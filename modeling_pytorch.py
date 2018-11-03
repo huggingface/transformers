84b7982137327fb0204213b03f522f2d2e2f1fd9
thomwolf <thomwolf@gmail.com>
2018-11-03 10:47:10 +0100

    comment on gelu function

--- modeling_pytorch.py
+++ modeling_pytorch.py
@@ -28,8 +28,8 @@ from torch.nn import CrossEntropyLoss
 
 def gelu(x):
     return x * 0.5 * (1.0 + torch.erf(x / math.sqrt(2.0)))
-    # OpenAI GPT gelu version :
-    # return 0.5 * x * (1 + torch.tanh(math.sqrt(2 / math.pi) * (x + 0.044715 * torch.pow(x, 3))))
+    # For information: OpenAI GPT gelu version is a bit different:
+    # 0.5 * x * (1 + torch.tanh(math.sqrt(2 / math.pi) * (x + 0.044715 * torch.pow(x, 3))))
 
 
 class BertConfig(object):
