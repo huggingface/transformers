6ec68dead3c47dee2ba8af430bb271542c8d6dac
thomwolf <thomwolf@gmail.com>
2018-11-04 12:07:38 +0100

    logging + update copyright

--- modeling.py
+++ modeling.py
@@ -1,5 +1,5 @@
 # coding=utf-8
-# Copyright 2018 The Google AI Language Team Authors.
+# Copyright 2018 The Google AI Language Team Authors and The HugginFace Inc. team.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,7 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Common utility functions related to TensorFlow."""
+"""PyTorch BERT model."""
 
 from __future__ import absolute_import
 from __future__ import division
