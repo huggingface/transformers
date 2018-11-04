c98f443206437be82bb78dbfc4a95f503bd34a1c
thomwolf <thomwolf@gmail.com>
2018-11-04 11:45:44 +0100

    what's in loss again

--- run_squad.py
+++ run_squad.py
@@ -873,7 +873,7 @@ def main():
                 logger.info("HHHHH Forward")
                 loss, _ = model(input_ids, segment_ids, input_mask, start_positions, end_positions)
                 model.zero_grad()
-                logger.info("HHHHH Backward")
+                logger.info("HHHHH Backward, loss: {}".format(loss))
                 loss.backward()
                 logger.info("HHHHH Loading data")
                 optimizer.step()
