6b2136a8a9f70f99f719a10e793e6a7f4ef26565
thomwolf <thomwolf@gmail.com>
2018-11-20 10:12:44 +0100

    fixing weights decay in run_squad example

--- examples/run_squad.py
+++ examples/run_squad.py
@@ -842,8 +842,8 @@ def main():
         param_optimizer = list(model.named_parameters())
     no_decay = ['bias', 'gamma', 'beta']
     optimizer_grouped_parameters = [
-        {'params': [p for n, p in param_optimizer if n not in no_decay], 'weight_decay_rate': 0.01},
-        {'params': [p for n, p in param_optimizer if n in no_decay], 'weight_decay_rate': 0.0}
+        {'params': [p for n, p in param_optimizer if not any(nd in n for nd in no_decay)], 'weight_decay_rate': 0.01},
+        {'params': [p for n, p in param_optimizer if any(nd in n for nd in no_decay)], 'weight_decay_rate': 0.0}
         ]
     optimizer = BertAdam(optimizer_grouped_parameters,
                          lr=args.learning_rate,
