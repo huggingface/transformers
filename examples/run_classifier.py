05053d163cbd6021e47487699e4e2de36c3b7720
thomwolf <thomwolf@gmail.com>
2018-11-26 10:45:13 +0100

    update cache_dir in readme and examples

--- examples/run_classifier.py
+++ examples/run_classifier.py
@@ -482,7 +482,8 @@ def main():
             len(train_examples) / args.train_batch_size / args.gradient_accumulation_steps * args.num_train_epochs)
 
     # Prepare model
-    model = BertForSequenceClassification.from_pretrained(args.bert_model, len(label_list))
+    model = BertForSequenceClassification.from_pretrained(args.bert_model, len(label_list),
+                cache_dir=PYTORCH_PRETRAINED_BERT_CACHE / 'distributed_{}'.format(args.local_rank))
     if args.fp16:
         model.half()
     model.to(device)
--- pytorch_pretrained_bert/__init__.py
+++ pytorch_pretrained_bert/__init__.py
@@ -3,3 +3,4 @@ from .modeling import (BertConfig, BertModel, BertForPreTraining,
                        BertForMaskedLM, BertForNextSentencePrediction,
                        BertForSequenceClassification, BertForQuestionAnswering)
 from .optimization import BertAdam
+from .file_utils import PYTORCH_PRETRAINED_BERT_CACHE
