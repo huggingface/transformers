bc659f86adfb26d7cf86e67fa4600b89e63ac07c
hzhwcmhf <hzhwcmhf@gmail.com>
2018-12-11 20:18:56 +0800

    fix compatibility with python 3.5.2; convert path to str

--- pytorch_pretrained_bert/file_utils.py
+++ pytorch_pretrained_bert/file_utils.py
@@ -23,8 +23,8 @@ import requests
 
 logger = logging.getLogger(__name__)  # pylint: disable=invalid-name
 
-PYTORCH_PRETRAINED_BERT_CACHE = Path(os.getenv('PYTORCH_PRETRAINED_BERT_CACHE',
-                                               Path.home() / '.pytorch_pretrained_bert'))
+PYTORCH_PRETRAINED_BERT_CACHE = str(Path(os.getenv('PYTORCH_PRETRAINED_BERT_CACHE',
+                                               Path.home() / '.pytorch_pretrained_bert')))
 
 
 def url_to_filename(url: str, etag: str = None) -> str:
