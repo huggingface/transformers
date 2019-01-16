b9b6b7cb22a0f965f3c54901facbc8f8c9e3e77b
thomwolf <thomwolf@gmail.com>
2019-01-16 11:18:19 +0100

    fix eval for wt103

--- examples/eval_transfo_xl.py
+++ examples/eval_transfo_xl.py
@@ -132,12 +132,12 @@ elif args.split == 'test':
     valid_loss = None
 
 def format_log(loss, split):
-    if args.dataset in ['enwik8', 'text8']:
-        log_str = '| {0} loss {1:5.2f} | {0} bpc {2:9.5f} '.format(
-            split, loss, loss / math.log(2))
-    else:
-        log_str = '| {0} loss {1:5.2f} | {0} ppl {2:9.3f} '.format(
-            split, loss, math.exp(loss))
+    # if args.dataset in ['enwik8', 'text8']:
+    #     log_str = '| {0} loss {1:5.2f} | {0} bpc {2:9.5f} '.format(
+    #         split, loss, loss / math.log(2))
+    # else:
+    log_str = '| {0} loss {1:5.2f} | {0} ppl {2:9.3f} '.format(
+        split, loss, math.exp(loss))
     return log_str
 
 log_str = ''
