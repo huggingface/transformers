5c85fc3977c4dcad526233a4706cffcd89e59eb0
thomwolf <thomwolf@gmail.com>
2019-03-06 10:05:21 +0100

    fix typo - logger info

--- pytorch_pretrained_bert/modeling_openai.py
+++ pytorch_pretrained_bert/modeling_openai.py
@@ -1,5 +1,5 @@
 # coding=utf-8
-# Copyright 2018 The OpenAI Team Authors and HugginFace Inc. team.
+# Copyright 2018 The OpenAI Team Authors and HuggingFace Inc. team.
 # Copyright (c) 2018, NVIDIA CORPORATION.  All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
