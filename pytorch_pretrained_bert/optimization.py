d7a3d3372b42ea393cb0953ad452f5497cf0c6ef
lukovnikov <lukovnikov@outlook.com>
2019-03-18 13:18:07 +0100

    branches, optim cosine fix

--- pytorch_pretrained_bert/optimization.py
+++ pytorch_pretrained_bert/optimization.py
@@ -26,7 +26,7 @@ logger = logging.getLogger(__name__)
 def warmup_cosine(x, warmup=0.002):
     if x < warmup:
         return x/warmup
-    x_ = (x - warmup) / (1 - warmup)  # progress after warmup
+    x_ = (x - warmup) / (1 - warmup)  # progress after warmup -
     return 0.5 * (1. + math.cos(math.pi * x_))
 
 def warmup_constant(x, warmup=0.002):
