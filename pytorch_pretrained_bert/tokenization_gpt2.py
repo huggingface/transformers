74f7906db460d81e4807249aaf73290db2b7d43c
Ben Mann <8enmann@gmail.com>
2019-04-30 19:48:22 -0700

    Fix #537

--- pytorch_pretrained_bert/tokenization_gpt2.py
+++ pytorch_pretrained_bert/tokenization_gpt2.py
@@ -221,7 +221,10 @@ class GPT2Tokenizer(object):
         """ Tokenize a string. """
         bpe_tokens = []
         for token in re.findall(self.pat, text):
-            token = ''.join(self.byte_encoder[ord(b)] for b in token)
+            if sys.version_info[0] == 2:
+                token = ''.join(self.byte_encoder[ord(b)] for b in token)
+            else:
+                token = ''.join(self.byte_encoder[b] for b in token.encode('utf-8'))
             bpe_tokens.extend(bpe_token for bpe_token in self.bpe(token).split(' '))
         return bpe_tokens
 
