8189d885506a6bcd275af9a4388f2e3858e50f0c
lukovnikov <lukovnikov@outlook.com>
2019-05-06 16:41:47 +0200

    run_classifier t_total fix

--- examples/lm_finetuning/simple_lm_finetuning.py
+++ examples/lm_finetuning/simple_lm_finetuning.py
@@ -22,6 +22,7 @@ import logging
 import os
 import random
 from io import open
+import math
 
 import numpy as np
 import torch
@@ -515,7 +516,7 @@ def main():
         train_dataset = BERTDataset(args.train_corpus, tokenizer, seq_len=args.max_seq_length,
                                     corpus_lines=None, on_memory=args.on_memory)
         num_train_optimization_steps = int(
-            len(train_dataset) / args.train_batch_size / args.gradient_accumulation_steps) * args.num_train_epochs
+            math.ceil(float(len(train_dataset)) / args.train_batch_size) / args.gradient_accumulation_steps) * args.num_train_epochs
         if args.local_rank != -1:
             num_train_optimization_steps = num_train_optimization_steps // torch.distributed.get_world_size()
 
--- examples/run_squad.py
+++ examples/run_squad.py
@@ -900,7 +900,7 @@ def main():
         train_examples = read_squad_examples(
             input_file=args.train_file, is_training=True, version_2_with_negative=args.version_2_with_negative)
         num_train_optimization_steps = int(
-            len(train_examples) / args.train_batch_size / args.gradient_accumulation_steps) * args.num_train_epochs
+            math.ceil(float(len(train_examples)) / args.train_batch_size) / args.gradient_accumulation_steps) * args.num_train_epochs
         if args.local_rank != -1:
             num_train_optimization_steps = num_train_optimization_steps // torch.distributed.get_world_size()
 
