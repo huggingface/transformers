62177222aefe5daaeb3d4eb858a5a1d8ac0cb8fc
Nathan Schucher <nathan@elementai.com>
2019-05-29 22:10:16 +0000

    We can keep the -ti flag after all

--- scripts/launch_docker.sh
+++ scripts/launch_docker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-nvidia-docker run --privileged --rm -v /home/hdvries/:/home/hdvries -v /home/nathan/:/home/nathan -v /dev/infiniband:/dev/infiniband \
+nvidia-docker run -ti --privileged --rm -v /home/hdvries/:/home/hdvries -v /home/nathan/:/home/nathan -v /dev/infiniband:/dev/infiniband \
     -w $(pwd) --ipc=host --network=host --dns 1.1.1.1 \
     images.borgy.elementai.lan/hdvries/bert_pytorch $1
