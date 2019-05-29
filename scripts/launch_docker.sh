b601d9aa665811a29c7d41e7235bb28231f9bbef
Nathan Schucher <nathan@elementai.com>
2019-05-29 22:04:54 +0000

    add scripts

--- scripts/launch_docker.sh
+++ scripts/launch_docker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-nvidia-docker run -ti --privileged --rm -v /home/hdvries/:/home/hdvries -v /home/nathan/:/home/nathan -v /dev/infiniband:/dev/infiniband \
-    --ipc=host --network=host --dns 1.1.1.1 \
-    -e NCCL_DEBUG=INFO -e NCCL_DEBUG_SUBSYS=ALL \
-    images.borgy.elementai.lan/hdvries/bert_pytorch bash
+
+nvidia-docker run --privileged --rm -v /home/hdvries/:/home/hdvries -v /home/nathan/:/home/nathan -v /dev/infiniband:/dev/infiniband \
+    -w $(pwd) --ipc=host --network=host --dns 1.1.1.1 \
+    images.borgy.elementai.lan/hdvries/bert_pytorch $1
