50e62a4cb4d503e3559b88838b8cf9f745fef516
LysandreJik <lysandre.debut@reseau.eseo.fr>
2019-07-11 16:50:21 -0400

    fix gpt/gpt-2 from pretrained

--- pytorch_transformers/modeling_gpt2.py
+++ pytorch_transformers/modeling_gpt2.py
@@ -423,7 +423,7 @@ class GPT2PreTrainedModel(PreTrainedModel):
         """
         num_special_tokens = kwargs.pop('num_special_tokens', None)
 
-        model = super(PreTrainedModel, cls).from_pretrained(pretrained_model_name_or_path, *inputs, **kwargs)
+        model = super().from_pretrained(pretrained_model_name_or_path, *inputs, **kwargs)
 
         # Add additional embeddings for special tokens if needed
         # This step also make sure we are still sharing the output and input embeddings after loading weights
