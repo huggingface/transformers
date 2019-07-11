93aef6d72d24d7c7350ccdec4d804cf120eab32f
LysandreJik <lysandre.debut@reseau.eseo.fr>
2019-07-11 12:38:21 -0400

    Added TorchScript disclaimer. CSS modifications.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -85,7 +85,9 @@ html_theme = 'sphinx_rtd_theme'
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {
+    'analytics_id': 'UA-83738774-2'
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
