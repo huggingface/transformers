53da85fe19cf7a252d89db61e4dfc9bf8538d8b3
LysandreJik <lysandre.debut@reseau.eseo.fr>
2019-07-11 20:16:28 -0400

    Byte order mark management in TSV glue reading.

--- examples/utils_glue.py
+++ examples/utils_glue.py
@@ -78,7 +78,7 @@ class DataProcessor(object):
     @classmethod
     def _read_tsv(cls, input_file, quotechar=None):
         """Reads a tab separated value file."""
-        with open(input_file, "r", encoding="utf-8") as f:
+        with open(input_file, "r", encoding="utf-8-sig") as f:
             reader = csv.reader(f, delimiter="\t", quotechar=quotechar)
             lines = []
             for line in reader:
