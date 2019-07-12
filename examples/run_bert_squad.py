762ded9b1c92f9cef2aa08c907e0f9b11b43e37a
thomwolf <thomwolf@gmail.com>
2019-07-12 11:28:52 +0200

    wip examples

--- examples/run_bert_squad.py
+++ examples/run_bert_squad.py
@@ -13,7 +13,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Run BERT on SQuAD."""
+""" Finetuning a question-answering Bert model on SQuAD."""
 
 from __future__ import absolute_import, division, print_function
 
