41f9d477bdc5089bbbc164989deca41d2f102de0
thomwolf <thomwolf@gmail.com>
2019-07-15 13:49:01 +0200

    clean up file_utils

--- pytorch_transformers/file_utils.py
+++ pytorch_transformers/file_utils.py
@@ -258,21 +258,3 @@ def get_from_cache(url, cache_dir=None):
             logger.info("removing temp file %s", temp_file.name)
 
     return cache_path
-
-
-def read_set_from_file(filename):
-    '''
-    Extract a de-duped collection (set) of text from a file.
-    Expected file format is one item per line.
-    '''
-    collection = set()
-    with open(filename, 'r', encoding='utf-8') as file_:
-        for line in file_:
-            collection.add(line.rstrip())
-    return collection
-
-
-def get_file_extension(path, dot=True, lower=True):
-    ext = os.path.splitext(path)[1]
-    ext = ext if dot else ext[1:]
-    return ext.lower() if lower else ext
