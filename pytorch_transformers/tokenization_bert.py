ba0d8c5f0b61006c60abb1ced2fa2e08ffdc2d05
Yiqing-Zhou <40547184+Yiqing-Zhou@users.noreply.github.com>
2019-07-23 20:41:26 +0800

    token[-1] -> token.rstrip('\n')

--- pytorch_transformers/tokenization_bert.py
+++ pytorch_transformers/tokenization_bert.py
@@ -69,7 +69,7 @@ def load_vocab(vocab_file):
     with open(vocab_file, "r", encoding="utf-8") as reader:
         tokens = reader.readlines()
     for index, token in enumerate(tokens):
-        token = token[:-1]
+        token = token.rstrip('\n')
         vocab[token] = index
     return vocab
 
