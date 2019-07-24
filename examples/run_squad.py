34aaf37dcf29668921f51448e4729c3d2a354736
Chi-Liang Liu <liangtaiwan1230@gmail.com>
2019-07-24 16:11:36 +0800

    fix squad v1 error (na_prob_file should be None)

--- examples/run_squad.py
+++ examples/run_squad.py
@@ -241,7 +241,10 @@ def evaluate(args, model, tokenizer, prefix=""):
     # Compute predictions
     output_prediction_file = os.path.join(args.output_dir, "predictions_{}.json".format(prefix))
     output_nbest_file = os.path.join(args.output_dir, "nbest_predictions_{}.json".format(prefix))
-    output_null_log_odds_file = os.path.join(args.output_dir, "null_odds_{}.json".format(prefix))
+    if args.version_2_with_negative:
+        output_null_log_odds_file = os.path.join(args.output_dir, "null_odds_{}.json".format(prefix))
+    else:
+        output_null_log_odds_file = None
 
     if args.model_type in ['xlnet', 'xlm']:
         # XLNet uses a more complex post-processing procedure
