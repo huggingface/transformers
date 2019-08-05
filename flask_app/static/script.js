c266b4d763108309a60ab4c24dfd5518de3ae639
snowblinkrx <ruixuerx@gmail.com>
2019-08-05 19:11:23 -0400

    dynamic ui update

--- flask_app/static/script.js
+++ flask_app/static/script.js
@@ -1,7 +1,8 @@
 $(function() {
     $('button#process').on('click', function() {
       $.getJSON($SCRIPT_ROOT + '/_input_helper', {
-        text_data: $('#text_data').val()
+        text_data: $('#text_data').val(),
+        question_data: $('#question_data').val()
       }, function(data) {
         $("#result").html(data.result);
       });
