901dde0e4583a00dc7e486aca6cda7acb647dea9
thomwolf <thomwolf@gmail.com>
2019-08-20 11:05:51 +0200

    fix #1014

--- pytorch_transformers/tokenization_bert.py
+++ pytorch_transformers/tokenization_bert.py
@@ -187,6 +187,8 @@ class BertTokenizer(PreTrainedTokenizer):
         index = 0
         if os.path.isdir(vocab_path):
             vocab_file = os.path.join(vocab_path, VOCAB_FILES_NAMES['vocab_file'])
+        else:
+            vocab_file = vocab_path
         with open(vocab_file, "w", encoding="utf-8") as writer:
             for token, token_index in sorted(self.vocab.items(), key=lambda kv: kv[1]):
                 if index != token_index:
