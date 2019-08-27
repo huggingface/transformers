c4a4711c36319e52b87e3db6ceca2db3439f9743
thomwolf <thomwolf@gmail.com>
2019-08-27 14:42:03 +0200

    updating docstring for AutoModel

--- pytorch_transformers/modeling_utils.py
+++ pytorch_transformers/modeling_utils.py
@@ -59,9 +59,20 @@ if not six.PY2:
             fn.__doc__ = ''.join(docstr) + fn.__doc__
             return fn
         return docstring_decorator
+
+    def add_end_docstrings(*docstr):
+        def docstring_decorator(fn):
+            fn.__doc__ = fn.__doc__ + ''.join(docstr)
+            return fn
+        return docstring_decorator
 else:
     # Not possible to update class docstrings on python2
     def add_start_docstrings(*docstr):
+        def docstring_decorator(fn):
+            return fn
+        return docstring_decorator
+
+    def add_end_docstrings(*docstr):
         def docstring_decorator(fn):
             return fn
         return docstring_decorator
