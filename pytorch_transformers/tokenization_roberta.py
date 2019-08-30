50e6daf83abec67964e56ad8fce1477df92a7a3c
thomwolf <thomwolf@gmail.com>
2019-08-30 11:27:43 +0200

    fix Roberta tokenizer __init__

--- pytorch_transformers/tokenization_roberta.py
+++ pytorch_transformers/tokenization_roberta.py
@@ -72,7 +72,8 @@ class RobertaTokenizer(GPT2Tokenizer):
 
     def __init__(self, vocab_file, merges_file, errors='replace', bos_token="<s>", eos_token="</s>", sep_token="</s>",
                  cls_token="<s>", unk_token="<unk>", pad_token='<pad>', mask_token='<mask>', **kwargs):
-        super(RobertaTokenizer, self).__init__(bos_token=bos_token, eos_token=eos_token, unk_token=unk_token,
+        super(RobertaTokenizer, self).__init__(vocab_file=vocab_file, merges_file=merges_file, errors=errors,
+                                               bos_token=bos_token, eos_token=eos_token, unk_token=unk_token,
                                                sep_token=sep_token, cls_token=cls_token, pad_token=pad_token,
                                                mask_token=mask_token, **kwargs)
 
