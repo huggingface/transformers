6a11e8f3e913faccfb40333373321c846e31f442
thomwolf <thomwolf@gmail.com>
2019-08-30 13:22:43 +0200

    python2 doesn't spark joy

--- pytorch_transformers/tests/tokenization_gpt2_test.py
+++ pytorch_transformers/tests/tokenization_gpt2_test.py
@@ -42,7 +42,7 @@ class GPT2TokenizationTest(CommonTestCases.CommonTokenizerTester):
         self.vocab_file = os.path.join(self.tmpdirname, VOCAB_FILES_NAMES['vocab_file'])
         self.merges_file = os.path.join(self.tmpdirname, VOCAB_FILES_NAMES['merges_file'])
         with open(self.vocab_file, "w", encoding="utf-8") as fp:
-            fp.write(json.dumps(vocab_tokens))
+            fp.write(json.dumps(vocab_tokens) + "\n")
         with open(self.merges_file, "w", encoding="utf-8") as fp:
             fp.write("\n".join(merges))
 
