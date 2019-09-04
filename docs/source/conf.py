0b52642d379bed155e8aa4f4088588bfd8ceaa88
LysandreJik <lysandre.debut@reseau.eseo.fr>
2019-09-04 11:03:32 -0400

    1.2.0 in docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ author = u'huggingface'
 # The short X.Y version
 version = u''
 # The full version, including alpha/beta/rc tags
-release = u'1.0.0'
+release = u'1.2.0'
 
 
 # -- General configuration ---------------------------------------------------
