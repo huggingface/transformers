204c34d3013a5a53c6f8c03778fa06d1541b6aa2
Richard Jackson <richard.jackson4@astrazeneca.com>
2019-09-11 19:13:02 +0100

    added docs

--- pytorch_transformers/tokenization_rbert.py
+++ pytorch_transformers/tokenization_rbert.py
@@ -24,9 +24,30 @@ logger = logging.getLogger(__name__)
 
 
 class RBertTokenizer(BertTokenizer):
+    r"""
+    Constructs a RBertTokenizer.
+    :class:`~pytorch_transformers.RBertTokenizer` runs end-to-end tokenization: punctuation splitting + wordpiece
+
+    Derived from the BertTokenizer, this class has a special encode method, encode_with_relationship, to implant the
+    special entity bounding characters around the entities of interest. It's intended use is with the
+    BertForRelationshipClassification head
+
+    Args:
+        same as BertTokenizer, apart crom
+        replace_conflicting_entity_offset_char: since the forward method of BertForRelationshipClassification detects
+                                                the special characters that bound each entity, any pre-existing characters
+                                                of this type will cause a conflict. To prevent this, we replace any affected
+                                                characters with this, prior to encoding. Defaults to '|'
+        ent1_sep_token: the char to delimit entity 1. Default is '$'
+        ent2_sep_token: the char to delimit entity 2. Default is '#'
+
+
+    """
+
     def __init__(self, vocab_file, do_lower_case=True, do_basic_tokenize=True, never_split=None,
                  unk_token="[UNK]", sep_token="[SEP]", pad_token="[PAD]", cls_token="[CLS]",
-                 mask_token="[MASK]", tokenize_chinese_chars=True, **kwargs):
+                 mask_token="[MASK]", tokenize_chinese_chars=True, replace_conflicting_entity_offset_char='|',
+                 ent1_sep_token="$", ent2_sep_token="#", **kwargs):
         super(RBertTokenizer, self).__init__(vocab_file,
                                              do_lower_case=do_lower_case,
                                              do_basic_tokenize=do_basic_tokenize,
@@ -38,8 +59,9 @@ class RBertTokenizer(BertTokenizer):
                                              mask_token=mask_token,
                                              tokenize_chinese_chars=tokenize_chinese_chars,
                                              **kwargs)
-        self.ent1_sep_token = "$"
-        self.ent2_sep_token = "#"
+        self.replace_conflicting_entity_offset_char = replace_conflicting_entity_offset_char
+        self.ent1_sep_token = ent1_sep_token
+        self.ent2_sep_token = ent2_sep_token
         self.entity_1_token_id = self.encode(self.ent1_sep_token)[0]
         self.entity_2_token_id = self.encode(self.ent2_sep_token)[0]
 
@@ -53,7 +75,9 @@ class RBertTokenizer(BertTokenizer):
         return self.encode(text=text, text_pair=text_pair, add_special_tokens=add_special_tokens)
 
     def _clean_special_chars(self, text: str):
-        return text.replace(self.ent1_sep_token, '|').replace(self.ent2_sep_token, '|')
+        return text.replace(self.ent1_sep_token,
+                            self.replace_conflicting_entity_offset_char).replace(self.ent2_sep_token,
+                                                                                 self.replace_conflicting_entity_offset_char)
 
     def _insert_relationship_special_chars(self, text, e1_offset_tup, e2_offset_tup):
         reverse_ordered_tups = sorted([e1_offset_tup, e2_offset_tup], reverse=True, key=lambda x: x[1])
