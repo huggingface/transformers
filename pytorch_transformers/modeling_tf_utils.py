2967de06f41af543b780e5c0ad912f8316a3f80e
thomwolf <thomwolf@gmail.com>
2019-09-25 22:08:38 +0200

    adding intialization to bert

--- pytorch_transformers/modeling_tf_utils.py
+++ pytorch_transformers/modeling_tf_utils.py
@@ -474,3 +474,12 @@ def shape_list(x):
     static = x.shape.as_list()
     dynamic = tf.shape(x)
     return [dynamic[i] if s is None else s for i, s in enumerate(static)]
+
+def get_initializer(initializer_range=0.02):
+  """Creates a `tf.initializers.truncated_normal` with the given range.
+  Args:
+    initializer_range: float, initializer range for stddev.
+  Returns:
+    TruncatedNormal initializer with stddev = `initializer_range`.
+  """
+  return tf.keras.initializers.TruncatedNormal(stddev=initializer_range)
