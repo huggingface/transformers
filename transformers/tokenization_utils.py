2964cd2853cc0cfbe5226fb2e4979a9862d1c226
LysandreJik <lysandre.debut@reseau.eseo.fr>
2019-10-04 14:27:38 -0400

    Decode documentaton

--- transformers/tokenization_utils.py
+++ transformers/tokenization_utils.py
@@ -912,6 +912,11 @@ class PreTrainedTokenizer(object):
         Converts a sequence of ids (integer) in a string, using the tokenizer and vocabulary
         with options to remove special tokens and clean up tokenization spaces.
         Similar to doing ``self.convert_tokens_to_string(self.convert_ids_to_tokens(token_ids))``.
+
+        Args:
+            token_ids: list of tokenized input ids. Can be obtained using the `encode` or `encode_plus` methods.
+            skip_special_tokens: if set to True, will replace special tokens.
+            clean_up_tokenization_spaces: if set to True, will clean up the tokenization spaces.
         """
         filtered_tokens = self.convert_ids_to_tokens(token_ids, skip_special_tokens=skip_special_tokens)
 
