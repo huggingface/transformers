111bf7ca0785fcce56db89043ec8941f89748b75
LysandreJik <lysandre.debut@reseau.eseo.fr>
2019-10-04 14:33:31 -0400

    Credit where it's due

--- examples/TPU/run_tpu_glue.py
+++ examples/TPU/run_tpu_glue.py
@@ -1,3 +1,23 @@
+# coding=utf-8
+# Copyright 2018 The Open AI Team Authors and The HuggingFace Inc. team.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+""" === Under active development === Script to fine-tune GLUE on a TPU
+
+Adapted from https://github.com/tensorflow/models
+Especially https://github.com/tensorflow/models/blob/master/official/modeling/model_training_utils.py
+"""
+
 from transformers import TFBertForSequenceClassification, BertTokenizer, BertConfig
 from tpu_utils import get_tpu
 from tpu_dataset import create_dataset
