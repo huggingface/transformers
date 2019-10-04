e836e505e10cacc59a205c519535c02e521d56bf
VictorSanh <victorsanh@gmail.com>
2019-10-04 17:16:10 -0400

    unecessary carriage return

--- examples/run_squad.py
+++ examples/run_squad.py
@@ -470,7 +470,6 @@ def main():
     tokenizer = tokenizer_class.from_pretrained(args.tokenizer_name if args.tokenizer_name else args.model_name_or_path, do_lower_case=args.do_lower_case)
     model = model_class.from_pretrained(args.model_name_or_path, from_tf=bool('.ckpt' in args.model_name_or_path), config=config)
 
-
     if args.local_rank == 0:
         torch.distributed.barrier()  # Make sure only the first process in distributed training will download model & vocab
 
