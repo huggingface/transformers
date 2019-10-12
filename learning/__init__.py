8594c26ef867dfa0a645b0f4747bc5c987dc68cb
hujunchao <1249952064@qq.com>
2019-10-12 17:37:48 +0800

    添加learning模块

---
+++ learning/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# @Time    : 10/12/19 5:29 PM
+# @Author  : hujunchao
+# @Email   : hujunchao@163.com
+# @File    : __init__.py.py
---
+++ learning/download_save.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# @Time    : 10/12/19 5:30 PM
+# @Author  : hujunchao
+# @Email   : hujunchao@163.com
+# @File    : download_save.py
+from transformers import DistilBertModel, DistilBertConfig, DistilBertTokenizer
+import os
+
+save_path_tokenizer = os.path.join('model', 'distilbert-base-uncased-tokenizer')
+save_path_model = os.path.join('model', 'distilbert-base-uncased-model')
+
+if not os.path.exists(save_path_tokenizer):
+    os.makedirs(save_path_tokenizer)
+
+if not os.path.exists(save_path_model):
+    os.makedirs(save_path_model)
+
+tokenizer = DistilBertTokenizer.from_pretrained('distilbert-base-uncased')
+tokenizer.save_pretrained(save_path_tokenizer)
+
+model = DistilBertModel.from_pretrained('distilbert-base-uncased')
+model.save_pretrained(save_path_model)
