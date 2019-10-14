be4fe8dbfd75479cc9e4a89b08868e9db4cc363b
qianliu0708 <qianliu0708@gmail.com>
2019-10-14 18:40:41 +0800

    Update gen_posrank_predwithsent.sh

--- gen_posrank_predwithsent.sh
+++ gen_posrank_predwithsent.sh
@@ -5,9 +5,9 @@ python ./examples/nq_posrank_sentences_generation.py \
 --example_pk_file /data/nieping/pytorch-transformers/models/wwm_train5piece/examples_${i}_.pk \
 --feature_pk_file /data/nieping/pytorch-transformers/models/wwm_train5piece/features_${i}_.pk \
 --results_pk_file /data/nieping/pytorch-transformers/models/wwm_train5piece/allresults_${i}_.pk \
---output_nbest_pk_file data/train_5_piece/train5piece_nbest_${i}.pk \
---output_pred_file data/train_5_piece/train5piece_pred_${i}.json \
---output_nbest_pred_with_sent_file data/train_5_piece/train5piece_nbest_predwithsent_${i}.pk
+--output_nbest_pk_file /data/nieping/pytorch-transformers/data/nq_sentence_selector/train_5_piece/train5piece_nbest_${i}.pk \
+--output_pred_file /data/nieping/pytorch-transformers/data/nq_sentence_selector/train_5_piece/train5piece_pred_${i}.json \
+--output_nbest_pred_with_sent_file /data/nieping/pytorch-transformers/data/nq_sentence_selector/train_5_piece/train5piece_nbest_predwithsent_${i}.pk
 echo Done ${i}
 } &
 done
