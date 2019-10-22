7d709e55ed54961ce3c84f53f1c14ee4f0c8a2e3
Lysandre <lysandre.debut@reseau.eseo.fr>
2019-10-22 14:12:33 -0400

    Remove

--- transformers/tests/tokenization_xlm_test.py
+++ transformers/tests/tokenization_xlm_test.py
@@ -69,8 +69,8 @@ class XLMTokenizationTest(CommonTestCases.CommonTokenizerTester):
     def test_sequence_builders(self):
         tokenizer = XLMTokenizer.from_pretrained("xlm-mlm-en-2048")
 
-        text = tokenizer.encode("sequence builders")
-        text_2 = tokenizer.encode("multi-sequence build")
+        text = tokenizer.encode("sequence builders", add_special_tokens=False)
+        text_2 = tokenizer.encode("multi-sequence build", add_special_tokens=False)
 
         encoded_sentence = tokenizer.build_inputs_with_special_tokens(text)
         encoded_pair = tokenizer.build_inputs_with_special_tokens(text, text_2)
