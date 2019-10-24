ae1d03fc51bb22ed59517ee6f92c560417fdb049
Matt Maybeno <mmaybeno@gmail.com>
2019-10-24 10:43:57 -0700

    Add roberta to doc

--- examples/run_ner.py
+++ examples/run_ner.py
@@ -13,7 +13,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-""" Fine-tuning the library models for named entity recognition on CoNLL-2003 (Bert). """
+""" Fine-tuning the library models for named entity recognition on CoNLL-2003 (Bert or Roberta). """
 
 from __future__ import absolute_import, division, print_function
 
