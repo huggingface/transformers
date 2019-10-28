cbd06962b3c843f5618addc84f8383935cb4aa3d
vfdev <vfdev.5@gmail.com>
2019-10-29 00:54:30 +0100

    [WIP] Raise error if larger sequences
    This is a WIP PR to inform user about potential error which can be hard to find if running on GPU.

--- transformers/modeling_bert.py
+++ transformers/modeling_bert.py
@@ -159,6 +159,8 @@ class BertEmbeddings(nn.Module):
     def forward(self, input_ids, token_type_ids=None, position_ids=None):
         seq_length = input_ids.size(1)
         if position_ids is None:
+            if seq_length > self.position_embeddings.num_embeddings:
+                raise ValueError("Sequence length is larger that number of positional embeddings")
             position_ids = torch.arange(seq_length, dtype=torch.long, device=input_ids.device)
             position_ids = position_ids.unsqueeze(0).expand_as(input_ids)
         if token_type_ids is None:
