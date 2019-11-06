7105c5bd7c51b9cfde4a54220ba234f11f98e38a
Julien Chaumond <chaumond@gmail.com>
2019-11-06 15:56:12 +0000

    [tests] Flag to test on cuda

--- transformers/tests/conftest.py
+++ transformers/tests/conftest.py
@@ -7,6 +7,9 @@ def pytest_addoption(parser):
     parser.addoption(
         "--runslow", action="store_true", default=False, help="run slow tests"
     )
+    parser.addoption(
+        "--use_cuda", action="store_true", default=False, help="run tests on gpu"
+    )
 
 
 def pytest_configure(config):
@@ -21,3 +24,8 @@ def pytest_collection_modifyitems(config, items):
     for item in items:
         if "slow" in item.keywords:
             item.add_marker(skip_slow)
+
+@pytest.fixture
+def use_cuda(request):
+    """ Run test on gpu """
+    return request.config.getoption("--use_cuda")
