fa736b7d814102c7b6b7df191e396e006801ceff
Lysandre <lysandre.debut@reseau.eseo.fr>
2019-11-11 10:15:14 -0500

    Fix #1784

--- transformers/tokenization_bert.py
+++ transformers/tokenization_bert.py
@@ -220,7 +220,7 @@ class BertTokenizer(PreTrainedTokenizer):
                 special tokens for the model
 
         Returns:
-            A list of integers in the range [0, 1]: 0 for a special token, 1 for a sequence token.
+            A list of integers in the range [0, 1]: 1 for a special token, 0 for a sequence token.
         """
 
         if already_has_special_tokens:
--- transformers/tokenization_utils.py
+++ transformers/tokenization_utils.py
@@ -951,7 +951,7 @@ class PreTrainedTokenizer(object):
                 special tokens for the model
 
         Returns:
-            A list of integers in the range [0, 1]: 0 for a special token, 1 for a sequence token.
+            A list of integers in the range [0, 1]: 1 for a special token, 0 for a sequence token.
         """
         return [0] * ((len(token_ids_1) if token_ids_1 else 0) + len(token_ids_0))
 
