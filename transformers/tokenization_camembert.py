5228d8db0486b17a878beb68c3f3ab530435931f
Julien Chaumond <chaumond@gmail.com>
2019-11-15 23:23:31 -0500

    [camembert] Upload to s3 + rename script

--- transformers/tokenization_camembert.py
+++ transformers/tokenization_camembert.py
@@ -25,7 +25,7 @@ VOCAB_FILES_NAMES = {'vocab_file': 'sentencepiece.bpe.model'}
 PRETRAINED_VOCAB_FILES_MAP = {
     'vocab_file':
     {
-    'camembert-base': "https://dl.fbaipublicfiles.com/camembert/camembert-base-v0-sentencepiece.bpe.model",
+    'camembert-base': "https://s3.amazonaws.com/models.huggingface.co/bert/camembert-base-sentencepiece.bpe.model",
     }
 }
 
