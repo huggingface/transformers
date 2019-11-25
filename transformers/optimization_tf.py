d576bb8ee60ef83dc8d6f3af822ad0da36a95dea
Julien Plu <julien.plu@schibsted.com>
2019-11-25 17:23:43 +0100

    Add TF WarmUp and AdamWeightDecay optimization classes for training.

--- transformers/optimization_tf.py
+++ transformers/optimization_tf.py
@@ -137,9 +137,9 @@ class AdamWeightDecay(tf.keras.optimizers.Adam):
           use_locking=self._use_locking)
     return tf.no_op()
 
-  def apply_gradients(self, grads_and_vars, clip_norm, name=None):
+  def apply_gradients(self, grads_and_vars, name=None):
     grads, tvars = list(zip(*grads_and_vars))
-    (grads, _) = tf.clip_by_global_norm(grads, clip_norm=clip_norm)
+    (grads, _) = tf.clip_by_global_norm(grads, clip_norm=1.0)
     return super(AdamWeightDecay, self).apply_gradients(zip(grads, tvars))
 
   def _get_lr(self, var_device, var_dtype, apply_state):
@@ -191,64 +191,3 @@ class AdamWeightDecay(tf.keras.optimizers.Adam):
         if re.search(r, param_name) is not None:
           return False
     return True
-
-
-## Inspired from https://github.com/OpenNMT/OpenNMT-tf/blob/master/opennmt/optimizers/utils.py
-class GradientAccumulator(object):
-    """Distribution strategies-aware gradient accumulation utility."""
-
-    def __init__(self):
-        """Initializes the accumulator."""
-        self._gradients = []
-        self._accum_steps = tf.Variable(
-            initial_value=0,
-            dtype=tf.int64,
-            trainable=False,
-            aggregation=tf.VariableAggregation.ONLY_FIRST_REPLICA)
-
-    @property
-    def step(self):
-        """Number of accumulated steps."""
-        return self._accum_steps.value()
-
-    @property
-    def gradients(self):
-        """The accumulated gradients."""
-        return list(gradient.value() if gradient is not None else gradient for gradient in self._get_replica_gradients())
-
-    def __call__(self, gradients):
-        """Accumulates :obj:`gradients`."""
-        if not self._gradients:
-            self._gradients.extend([tf.Variable(tf.zeros_like(gradient), trainable=False) if gradient is not None else gradient for gradient in gradients])
-
-        if len(gradients) != len(self._gradients):
-            raise ValueError("Expected %s gradients, but got %d" % (len(self._gradients), len(gradients)))
-
-        for accum_gradient, gradient in zip(self._get_replica_gradients(), gradients):
-            if accum_gradient is not None:
-                accum_gradient.assign_add(gradient)
-
-        self._accum_steps.assign_add(1)
-
-    def reset(self):
-        """Resets the accumulated gradients."""
-        if self._gradients:
-            self._accum_steps.assign(0)
-
-        for gradient in self._get_replica_gradients():
-            if gradient is not None:
-                gradient.assign(tf.zeros_like(gradient))
-
-    def _get_replica_gradients(self):
-        if tf.distribute.has_strategy():
-            # In a replica context, we want to accumulate gradients on each replica
-            # without synchronization, so we directly assign the value of the
-            # current replica.
-            replica_context = tf.distribute.get_replica_context()
-
-            if replica_context is None or tf.distribute.get_strategy().num_replicas_in_sync == 1:
-                return self._gradients
-
-            return (gradient.device_map.select_for_current_replica(gradient.values, replica_context) for gradient in self._gradients)
-        else:
-            return self._gradients
