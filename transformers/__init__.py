dd9f4e0565e38c82e77bc140d55dd2b0f600432d
Julien Plu <julien.plu@schibsted.com>
2019-11-26 15:36:44 +0100

    Add TFDistilBertForTokenClassification in the init file

--- transformers/__init__.py
+++ transformers/__init__.py
@@ -162,6 +162,7 @@ if is_tf_available():
     from .modeling_tf_distilbert import (TFDistilBertPreTrainedModel, TFDistilBertMainLayer,
                                          TFDistilBertModel, TFDistilBertForMaskedLM,
                                          TFDistilBertForSequenceClassification,
+                                         TFDistilBertForTokenClassification,
                                          TFDistilBertForQuestionAnswering,
                                          TF_DISTILBERT_PRETRAINED_MODEL_ARCHIVE_MAP)
 
