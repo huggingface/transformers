4b28d5d8f0058177647b7fac83ddbc71f05a002a
Lysandre <lysandre.debut@reseau.eseo.fr>
2019-11-25 19:22:21 -0500

    SQuAD v2 BERT + XLNet

--- transformers/data/__init__.py
+++ transformers/data/__init__.py
@@ -1,6 +1,6 @@
 from .processors import InputExample, InputFeatures, DataProcessor, SquadFeatures
 from .processors import glue_output_modes, glue_processors, glue_tasks_num_labels, glue_convert_examples_to_features
-from .processors import squad_convert_examples_to_features, SquadExample, read_squad_examples
+from .processors import squad_convert_examples_to_features, SquadExample
 
 from .metrics import is_sklearn_available
 if is_sklearn_available():
