0b84b9fd8a728ca46e4109aa38a11b25f87a09bf
Lysandre <lysandre.debut@reseau.eseo.fr>
2019-11-28 17:38:52 -0500

    Add processors to __init__

--- transformers/data/__init__.py
+++ transformers/data/__init__.py
@@ -1,6 +1,6 @@
 from .processors import InputExample, InputFeatures, DataProcessor, SquadFeatures
 from .processors import glue_output_modes, glue_processors, glue_tasks_num_labels, glue_convert_examples_to_features
-from .processors import squad_convert_examples_to_features, SquadExample
+from .processors import squad_convert_examples_to_features, SquadExample, SquadV1Processor, SquadV2Processor
 
 from .metrics import is_sklearn_available
 if is_sklearn_available():
