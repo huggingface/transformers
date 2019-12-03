c461129883be582964e93d74281602e55782dd8a
Julien Chaumond <chaumond@gmail.com>
2019-12-03 22:07:25 +0000

    [pplm] split classif head into its own file

--- examples/pplm/run_pplm_discrim_train.py
+++ examples/pplm/run_pplm_discrim_train.py
@@ -21,6 +21,7 @@ from torchtext import datasets
 from tqdm import tqdm, trange
 
 from transformers import GPT2Tokenizer, GPT2LMHeadModel
+from pplm_classification_head import ClassificationHead
 
 torch.manual_seed(0)
 np.random.seed(0)
@@ -29,22 +30,6 @@ example_sentence = "This is incredible! I love it, this is the best chicken I ha
 max_length_seq = 100
 
 
-class ClassificationHead(torch.nn.Module):
-    """Classification Head for  transformer encoders"""
-
-    def __init__(self, class_size, embed_size):
-        super(ClassificationHead, self).__init__()
-        self.class_size = class_size
-        self.embed_size = embed_size
-        # self.mlp1 = torch.nn.Linear(embed_size, embed_size)
-        # self.mlp2 = (torch.nn.Linear(embed_size, class_size))
-        self.mlp = torch.nn.Linear(embed_size, class_size)
-
-    def forward(self, hidden_state):
-        # hidden_state = F.relu(self.mlp1(hidden_state))
-        # hidden_state = self.mlp2(hidden_state)
-        logits = self.mlp(hidden_state)
-        return logits
 
 
 class Discriminator(torch.nn.Module):
