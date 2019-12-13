2d596c5e61060bc0d60a71d546ce45fca4a443ed
Aleksey Tikhonov <altsoph@gmail.com>
2019-12-13 22:27:19 +0300

    Update modeling_bert.py
    Added support for using load_tf_weights_in_bert with BertForSequenceClassification()

--- transformers/modeling_bert.py
+++ transformers/modeling_bert.py
@@ -82,7 +82,7 @@ def load_tf_weights_in_bert(model, config, tf_checkpoint_path):
         name = name.split('/')
         # adam_v and adam_m are variables used in AdamWeightDecayOptimizer to calculated m and v
         # which are not required for using pretrained model
-        if any(n in ["adam_v", "adam_m", "global_step"] for n in name):
+        if any(n in ["adam_v", "adam_m", "global_step", "bad_steps", "good_steps", "loss_scale"] for n in name):
             logger.info("Skipping {}".format("/".join(name)))
             continue
         pointer = model
@@ -94,9 +94,17 @@ def load_tf_weights_in_bert(model, config, tf_checkpoint_path):
             if l[0] == 'kernel' or l[0] == 'gamma':
                 pointer = getattr(pointer, 'weight')
             elif l[0] == 'output_bias' or l[0] == 'beta':
-                pointer = getattr(pointer, 'bias')
+                try:
+                    pointer = getattr(pointer, 'bias')
+                except:
+                    pointer = getattr(pointer, 'classifier')
+                    pointer = getattr(pointer, 'bias')
             elif l[0] == 'output_weights':
-                pointer = getattr(pointer, 'weight')
+                try:
+                    pointer = getattr(pointer, 'weight')
+                except:
+                    pointer = getattr(pointer, 'classifier')
+                    pointer = getattr(pointer, 'weight')
             elif l[0] == 'squad':
                 pointer = getattr(pointer, 'classifier')
             else:
