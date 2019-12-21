bfce049ca9125c6b05f4f2092004bb18fb6af0bc
Aymeric Augustin <aymeric.augustin@fractalideas.com>
2019-12-21 17:57:20 +0100

    Fix E265 flake8 warning (x1).

--- examples/contrib/run_swag.py
+++ examples/contrib/run_swag.py
@@ -356,7 +356,7 @@ def train(args, train_dataset, model, tokenizer):
             inputs = {
                 "input_ids": batch[0],
                 "attention_mask": batch[1],
-                #'token_type_ids':  None if args.model_type == 'xlm' else batch[2],
+                # 'token_type_ids':  None if args.model_type == 'xlm' else batch[2],
                 "token_type_ids": batch[2],
                 "labels": batch[3],
             }
