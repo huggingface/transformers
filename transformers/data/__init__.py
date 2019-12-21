a4f523e3f9e1d71694b69f545f643879fd153ece
Aymeric Augustin <aymeric.augustin@fractalideas.com>
2019-12-21 21:31:37 +0100

    Ignore F401 flake8 warning (x326 / 594).

--- transformers/data/__init__.py
+++ transformers/data/__init__.py
@@ -1,3 +1,7 @@
+# flake8: noqa
+# There's no way to ignore "F401 '...' imported but unused" warnings in this
+# module, but to preserve other warnings. So, don't check this module at all.
+
 from .metrics import is_sklearn_available
 from .processors import (
     DataProcessor,
