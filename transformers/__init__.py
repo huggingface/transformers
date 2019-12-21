654e051e2a602b822e6804a6199bb7d0e841dfdc
Aymeric Augustin <aymeric.augustin@fractalideas.com>
2019-12-21 21:31:37 +0100

    Ignore F401 flake8 warning (x326 / 594).

--- transformers/__init__.py
+++ transformers/__init__.py
@@ -1,3 +1,7 @@
+# flake8: noqa
+# There's no way to ignore "F401 '...' imported but unused" warnings in this
+# module, but to preserve other warnings. So, don't check this module at all.
+
 __version__ = "2.3.0"
 
 # Work around to update TensorFlow's absl.logging threshold which alters the
