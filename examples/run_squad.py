70d6fbe386ca2b9bbbaa0a4e0f34b36315dc52b7
thomwolf <thomwolf@gmail.com>
2019-12-21 14:49:54 +0100

    fix merge

--- examples/run_squad.py
+++ examples/run_squad.py
@@ -388,7 +388,7 @@ def evaluate(args, model, tokenizer, prefix=""):
         predictions = compute_predictions_logits(examples, features, all_results, args.n_best_size,
                         args.max_answer_length, args.do_lower_case, output_prediction_file,
                         output_nbest_file, output_null_log_odds_file, args.verbose_logging,
-                        args.version_2_with_negative, args.null_score_diff_threshold)
+                        args.version_2_with_negative, args.null_score_diff_threshold, tokenizer)
 
     # Compute the F1 and exact scores.
     results = squad_evaluate(examples, predictions)
