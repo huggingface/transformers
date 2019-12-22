d6eaf4e6d267049b0160b5de3ad6502a76b76bef
Aymeric Augustin <aymeric.augustin@fractalideas.com>
2019-12-22 18:22:29 +0100

    Update comments mentioning Python 2.

--- examples/run_squad.py
+++ examples/run_squad.py
@@ -185,7 +185,7 @@ def train(args, train_dataset, model, tokenizer):
     train_iterator = trange(
         epochs_trained, int(args.num_train_epochs), desc="Epoch", disable=args.local_rank not in [-1, 0]
     )
-    # Added here for reproductibility (even between python 2 and 3)
+    # Added here for reproductibility
     set_seed(args)
 
     for _ in train_iterator:
--- templates/adding_a_new_example_script/run_xxx.py
+++ templates/adding_a_new_example_script/run_xxx.py
@@ -155,7 +155,7 @@ def train(args, train_dataset, model, tokenizer):
     tr_loss, logging_loss = 0.0, 0.0
     model.zero_grad()
     train_iterator = trange(int(args.num_train_epochs), desc="Epoch", disable=args.local_rank not in [-1, 0])
-    set_seed(args)  # Added here for reproductibility (even between python 2 and 3)
+    set_seed(args)  # Added here for reproductibility
     for _ in train_iterator:
         epoch_iterator = tqdm(train_dataloader, desc="Iteration", disable=args.local_rank not in [-1, 0])
         for step, batch in enumerate(epoch_iterator):
