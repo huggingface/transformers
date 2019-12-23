5fca854dbcc8d1e8d7e879e2811580535f6aa5d2
Aymeric Augustin <aymeric.augustin@fractalideas.com>
2019-12-23 16:49:35 +0100

    Fix path to source code in docs config.
    This should fix API docs, which went AWOL with yesterday's changes.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../..'))
+sys.path.insert(0, os.path.abspath('../../src'))
 
 
 # -- Project information -----------------------------------------------------
