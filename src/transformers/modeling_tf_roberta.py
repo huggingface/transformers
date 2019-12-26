4bc9c2eacefeae9691dc7bd8cd5e996e6f714f47
Gautam <gautamrbharadwaj@gmail.com>
2019-12-26 19:34:29 +0530

    refactoring the code

--- src/transformers/modeling_tf_roberta.py
+++ src/transformers/modeling_tf_roberta.py
@@ -17,14 +17,12 @@
 
 
 import logging
-
 import tensorflow as tf
 
 from .configuration_roberta import RobertaConfig
 from .file_utils import add_start_docstrings
 from .modeling_tf_bert import TFBertEmbeddings, TFBertMainLayer, gelu
 from .modeling_tf_utils import TFPreTrainedModel, get_initializer, shape_list
-
 
 logger = logging.getLogger(__name__)
 
