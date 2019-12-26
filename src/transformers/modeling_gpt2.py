92c724d1d90230e2e9e3ea307ebd03a5b86d345b
Gautam <gautamrbharadwaj@gmail.com>
2019-12-26 19:27:22 +0530

    refactoring the code

--- src/transformers/modeling_gpt2.py
+++ src/transformers/modeling_gpt2.py
@@ -19,15 +19,13 @@
 import logging
 import math
 import os
-
 import torch
 import torch.nn as nn
-from torch.nn import CrossEntropyLoss
 
+from torch.nn import CrossEntropyLoss
 from .configuration_gpt2 import GPT2Config
 from .file_utils import add_start_docstrings
 from .modeling_utils import Conv1D, PreTrainedModel, SequenceSummary, prune_conv1d_layer
-
 
 logger = logging.getLogger(__name__)
 
