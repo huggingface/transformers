64813958dee268aca1427cf4ae944572989026f1
Gautam <gautamrbharadwaj@gmail.com>
2019-12-26 19:48:14 +0530

    refactoring the code

--- src/transformers/modeling_xlm.py
+++ src/transformers/modeling_xlm.py
@@ -19,13 +19,12 @@
 import itertools
 import logging
 import math
-
 import numpy as np
 import torch
+
 from torch import nn
 from torch.nn import CrossEntropyLoss, MSELoss
 from torch.nn import functional as F
-
 from .configuration_xlm import XLMConfig
 from .file_utils import add_start_docstrings
 from .modeling_utils import PreTrainedModel, SequenceSummary, SQuADHead, prune_linear_layer
