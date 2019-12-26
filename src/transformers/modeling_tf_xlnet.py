23ede2f85505b9694903b895d9fdfbe69492eff7
Gautam <gautamrbharadwaj@gmail.com>
2019-12-26 19:46:55 +0530

    refactoring the code

--- src/transformers/modeling_tf_xlnet.py
+++ src/transformers/modeling_tf_xlnet.py
@@ -18,14 +18,12 @@
 
 
 import logging
-
 import numpy as np
 import tensorflow as tf
 
 from .configuration_xlnet import XLNetConfig
 from .file_utils import add_start_docstrings
 from .modeling_tf_utils import TFPreTrainedModel, TFSequenceSummary, TFSharedEmbeddings, get_initializer, shape_list
-
 
 logger = logging.getLogger(__name__)
 
