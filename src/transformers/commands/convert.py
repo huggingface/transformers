7d5b907767a3ac041265ccfd4b4a84c11e0b3c43
Nauman Mustafa <nauman.mustafa@packagex.app>
2020-01-05 19:22:03 +0500

    [cli] Add support for T5 model conversion

--- src/transformers/commands/convert.py
+++ src/transformers/commands/convert.py
@@ -140,5 +140,11 @@ class ConvertCommand(BaseTransformersCLICommand):
             )
 
             convert_xlm_checkpoint_to_pytorch(self._tf_checkpoint, self._pytorch_dump_output)
+        elif self._model_type == 't5':
+            from transformers.convert_t5_original_tf_checkpoint_to_pytorch import (
+                convert_tf_checkpoint_to_pytorch
+            )
+            
+            convert_tf_checkpoint_to_pytorch(self._tf_checkpoint, self._config, self._pytorch_dump_output)
         else:
-            raise ValueError("--model_type should be selected in the list [bert, gpt, gpt2, transfo_xl, xlnet, xlm]")
+            raise ValueError("--model_type should be selected in the list [bert, gpt, gpt2, transfo_xl, xlnet, xlm, t5]")
