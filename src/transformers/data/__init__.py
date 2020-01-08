f0bd3ba931e26122d70adee460486c353d4890f7
meshidenn <mesitahiro@hotmail.com>
2020-01-08 05:29:25 +0000

    fix just forget save

--- src/transformers/data/__init__.py
+++ src/transformers/data/__init__.py
@@ -25,16 +25,3 @@ from .processors import (
     japanese_tasks_num_labels,
     japanese_convert_examples_to_features
 )
-
-
-if is_sklearn_available():
-    from .metrics import glue_compute_metrics, xnli_compute_metrics
-
-
-from .processors import InputExample, InputFeatures, DataProcessor
-from .processors import glue_output_modes, glue_processors, glue_tasks_num_labels, glue_convert_examples_to_features
-from .processors import japanese_output_modes, japanese_processors, japanese_tasks_num_labels, japanese_convert_examples_to_features
-
-from .metrics import is_sklearn_available
-if is_sklearn_available():
-    from .metrics import glue_compute_metrics, japanese_compute_metrics
