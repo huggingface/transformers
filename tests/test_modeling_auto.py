6496a816edc9ea093fcbdb1e8b9c6fba4c125ba8
Julien Chaumond <chaumond@gmail.com>
2020-01-10 17:40:02 +0000

    num_parameters helper

--- tests/test_modeling_auto.py
+++ tests/test_modeling_auto.py
@@ -100,3 +100,5 @@ class AutoModelTest(unittest.TestCase):
         logging.basicConfig(level=logging.INFO)
         model = AutoModelWithLMHead.from_pretrained(SMALL_MODEL_IDENTIFIER)
         self.assertIsInstance(model, BertForMaskedLM)
+        self.assertEqual(model.num_parameters(), 14830)
+        self.assertEqual(model.num_parameters(only_trainable=True), 14830)
