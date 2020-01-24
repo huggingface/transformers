d99326e2529d69627c4fe7d70fb2aae163090fa4
sshleifer <sshleifer@gmail.com>
2020-01-24 11:25:43 -0500

    before consider Roberta way

--- src/transformers/fairseq_utils.py
+++ src/transformers/fairseq_utils.py
@@ -2170,18 +2170,12 @@ class TransformerModel(FairseqEncoderDecoderModel):
         # make sure all arguments are present in older models
         #base_architecture(args) # mutates
 
-        if args.encoder_layers_to_keep:
-            args.encoder_layers = len(args.encoder_layers_to_keep.split(","))
-        if args.decoder_layers_to_keep:
-            args.decoder_layers = len(args.decoder_layers_to_keep.split(","))
-
         if getattr(args, 'max_source_positions', None) is None:
             args.max_source_positions = DEFAULT_MAX_SOURCE_POSITIONS
         if getattr(args, 'max_target_positions', None) is None:
             args.max_target_positions = DEFAULT_MAX_TARGET_POSITIONS
 
-        src_dict, tgt_dict = task.source_dictionary, task.target_dictionary
-
+        src_dict = task.source_dictionary
         def build_embedding(dictionary, embed_dim, path=None):
             num_embeddings = len(dictionary)
             padding_idx = dictionary.pad()
@@ -2193,30 +2187,23 @@ class TransformerModel(FairseqEncoderDecoderModel):
                 #utils.load_embedding(embed_dict, dictionary, emb)
             return emb
 
-        if args.share_all_embeddings:
-            if src_dict != tgt_dict:
-                raise ValueError('--share-all-embeddings requires a joined dictionary')
-            if args.encoder_embed_dim != args.decoder_embed_dim:
-                raise ValueError(
-                    '--share-all-embeddings requires --encoder-embed-dim to match --decoder-embed-dim')
-            if args.decoder_embed_path and (
-                    args.decoder_embed_path != args.encoder_embed_path):
-                raise ValueError('--share-all-embeddings not compatible with --decoder-embed-path')
-            encoder_embed_tokens = build_embedding(
-                src_dict, args.encoder_embed_dim, args.encoder_embed_path
-            )
-            decoder_embed_tokens = encoder_embed_tokens
-            args.share_decoder_input_output_embed = True
-        else:
-            encoder_embed_tokens = build_embedding(
-                src_dict, args.encoder_embed_dim, args.encoder_embed_path
-            )
-            decoder_embed_tokens = build_embedding(
-                tgt_dict, args.decoder_embed_dim, args.decoder_embed_path
-            )
+        #assert args.share_all_embeddings
+        # if src_dict != tgt_dict:
+        #     raise ValueError('--share-all-embeddings requires a joined dictionary')
+        if args.encoder_embed_dim != args.decoder_embed_dim:
+            raise ValueError(
+                '--share-all-embeddings requires --encoder-embed-dim to match --decoder-embed-dim')
+        #if args.decoder_embed_path and (
+        #        args.decoder_embed_path != args.encoder_embed_path):
+        #    raise ValueError('--share-all-embeddings not compatible with --decoder-embed-path')
+        encoder_embed_tokens = build_embedding(
+            src_dict, args.encoder_embed_dim, args.encoder_embed_path
+        )
+        decoder_embed_tokens = encoder_embed_tokens
+        args.share_decoder_input_output_embed = True
 
         encoder = cls.build_encoder(args, src_dict, encoder_embed_tokens)
-        decoder = cls.build_decoder(args, tgt_dict, decoder_embed_tokens)
+        decoder = cls.build_decoder(args, src_dict, decoder_embed_tokens)
         return cls(args, encoder, decoder)
 
     @classmethod
