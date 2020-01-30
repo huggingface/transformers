31795d64a54e7de81808a2f9bc6545ab5686b97a
Julien Chaumond <chaumond@gmail.com>
2020-01-30 11:13:22 -0500

    Extraneous doc

--- src/transformers/pipelines.py
+++ src/transformers/pipelines.py
@@ -364,7 +364,6 @@ class Pipeline(_ScikitCompat):
     def predict(self, X):
         """
         Scikit / Keras interface to transformers' pipelines. This method will forward to __call__().
-        Se
         """
         return self(X=X)
 
