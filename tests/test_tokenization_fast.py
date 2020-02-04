f806ec1add33e5e6163bf6d08428c0fd4999f778
Morgan Funtowicz <morgan@huggingface.co>
2020-02-04 17:53:56 +0100

    Black.
    Signed-off-by: Morgan Funtowicz <morgan@huggingface.co>

--- tests/test_tokenization_fast.py
+++ tests/test_tokenization_fast.py
@@ -2,8 +2,16 @@ import unittest
 
 import numpy as np
 
-from transformers import BertTokenizer, BertTokenizerFast, DistilBertTokenizer, GPT2Tokenizer, \
-    GPT2TokenizerFast, RobertaTokenizer, OpenAIGPTTokenizer, TransfoXLTokenizer
+from transformers import (
+    BertTokenizer,
+    BertTokenizerFast,
+    DistilBertTokenizer,
+    GPT2Tokenizer,
+    GPT2TokenizerFast,
+    OpenAIGPTTokenizer,
+    RobertaTokenizer,
+    TransfoXLTokenizer,
+)
 from transformers.tokenization_distilbert import DistilBertTokenizerFast
 from transformers.tokenization_openai import OpenAIGPTTokenizerFast
 from transformers.tokenization_roberta import RobertaTokenizerFast
@@ -11,9 +19,8 @@ from transformers.tokenization_transfo_xl import TransfoXLTokenizerFast
 
 
 class FastTokenizerMatchingTest(unittest.TestCase):
-
     def setUp(self) -> None:
-        with open('fixtures/sample_text.txt') as f_data:
+        with open("fixtures/sample_text.txt") as f_data:
             self._data = f_data.read().replace("\n\n", "\n").strip()
 
     def assert_sequence_almost_equals(self, a, b, threshold):
@@ -56,9 +63,9 @@ class FastTokenizerMatchingTest(unittest.TestCase):
         input_p = tokenizer_p.encode_plus(self._data, max_length=512, pad_to_max_length=True)
         input_r = tokenizer_r.encode_plus(self._data, max_length=512, pad_to_max_length=True)
 
-        self.assertSequenceEqual(input_p['input_ids'], input_r['input_ids'])
-        self.assertSequenceEqual(input_p['token_type_ids'], input_r['token_type_ids'])
-        self.assertSequenceEqual(input_p['attention_mask'], input_r['attention_mask'])
+        self.assertSequenceEqual(input_p["input_ids"], input_r["input_ids"])
+        self.assertSequenceEqual(input_p["token_type_ids"], input_r["token_type_ids"])
+        self.assertSequenceEqual(input_p["attention_mask"], input_r["attention_mask"])
 
         # Ensure truncation with stride match
         # input_p = tokenizer_p.encode_plus(self._data, max_length=512, stride=3, return_overflowing_tokens=True)
@@ -69,7 +76,7 @@ class FastTokenizerMatchingTest(unittest.TestCase):
         # self.assertSequenceEqual(input_p['attention_mask'], input_r['attention_mask'])
 
     def test_bert(self):
-        for tokenizer_name in BertTokenizer.pretrained_vocab_files_map['vocab_file'].keys():
+        for tokenizer_name in BertTokenizer.pretrained_vocab_files_map["vocab_file"].keys():
             tokenizer_p = BertTokenizer.from_pretrained(tokenizer_name)
             tokenizer_r = BertTokenizerFast.from_pretrained(tokenizer_name)
 
@@ -77,14 +84,14 @@ class FastTokenizerMatchingTest(unittest.TestCase):
             self.assert_tokenization_python_rust_almost_equals(tokenizer_p, tokenizer_r, 0.0)
 
     def test_transfoxl(self):
-        for tokenizer_name in TransfoXLTokenizer.pretrained_vocab_files_map['pretrained_vocab_file'].keys():
+        for tokenizer_name in TransfoXLTokenizer.pretrained_vocab_files_map["pretrained_vocab_file"].keys():
             tokenizer_p = TransfoXLTokenizer.from_pretrained(tokenizer_name)
             tokenizer_r = TransfoXLTokenizerFast.from_pretrained(tokenizer_name)
 
             self.assert_tokenization_python_rust_almost_equals(tokenizer_p, tokenizer_r, 0.0)
 
     def test_distilbert(self):
-        for tokenizer_name in DistilBertTokenizer.pretrained_vocab_files_map['vocab_file'].keys():
+        for tokenizer_name in DistilBertTokenizer.pretrained_vocab_files_map["vocab_file"].keys():
             tokenizer_p = DistilBertTokenizer.from_pretrained(tokenizer_name)
             tokenizer_r = DistilBertTokenizerFast.from_pretrained(tokenizer_name)
 
@@ -92,26 +99,26 @@ class FastTokenizerMatchingTest(unittest.TestCase):
             self.assert_tokenization_python_rust_almost_equals(tokenizer_p, tokenizer_r, 0.0)
 
     def test_gpt2(self):
-        for tokenizer_name in GPT2Tokenizer.pretrained_vocab_files_map['vocab_file'].keys():
+        for tokenizer_name in GPT2Tokenizer.pretrained_vocab_files_map["vocab_file"].keys():
             tokenizer_p = GPT2Tokenizer.from_pretrained(tokenizer_name)
             tokenizer_r = GPT2TokenizerFast.from_pretrained(tokenizer_name)
 
             self.assert_tokenization_python_rust_almost_equals(tokenizer_p, tokenizer_r, 0.05)
 
     def test_roberta(self):
-        for tokenizer_name in RobertaTokenizer.pretrained_vocab_files_map['vocab_file'].keys():
+        for tokenizer_name in RobertaTokenizer.pretrained_vocab_files_map["vocab_file"].keys():
             tokenizer_p = RobertaTokenizer.from_pretrained(tokenizer_name)
             tokenizer_r = RobertaTokenizerFast.from_pretrained(tokenizer_name)
 
             self.assert_tokenization_python_rust_almost_equals(tokenizer_p, tokenizer_r, 0.05)
 
     def test_openai(self):
-        for tokenizer_name in OpenAIGPTTokenizer.pretrained_vocab_files_map['vocab_file'].keys():
+        for tokenizer_name in OpenAIGPTTokenizer.pretrained_vocab_files_map["vocab_file"].keys():
             tokenizer_p = OpenAIGPTTokenizer.from_pretrained(tokenizer_name)
             tokenizer_r = OpenAIGPTTokenizerFast.from_pretrained(tokenizer_name)
 
             self.assert_tokenization_python_rust_almost_equals(tokenizer_p, tokenizer_r, 0.05)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
