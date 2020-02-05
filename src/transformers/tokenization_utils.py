8ec45eeef0c70b7984976f8e4e975cd12826d5ce
Morgan Funtowicz <morgan@huggingface.co>
2020-02-05 11:54:17 +0100

    Introduce tokenize() method on fast tokenizers.
    Signed-off-by: Morgan Funtowicz <morgan@huggingface.co>

--- src/transformers/tokenization_utils.py
+++ src/transformers/tokenization_utils.py
@@ -1645,6 +1645,9 @@ class PreTrainedTokenizerFast(PreTrainedTokenizer):
         self._update_special_tokens()
         return added
 
+    def tokenize(self, text, **kwargs):
+        return self.tokenizer._tokenizer.encode(text).tokens
+
     def encode_plus(
         self,
         text,
