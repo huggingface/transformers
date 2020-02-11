53815bd9e3721f57590650ece00885d21d5aa715
Sasha <arush@g.harvard.edu>
2020-02-11 13:29:17 -0500

    .

--- examples/transformer_base.py
+++ examples/transformer_base.py
@@ -33,31 +33,29 @@ def set_seed(args):
     if args.n_gpu > 0:
         torch.cuda.manual_seed_all(args.seed)
 
+# class ExampleLogger(LightningLoggerBase):
+#     @rank_zero_only
+#     def log_hyperparams(self, params):
+#         # params is an argparse.Namespace
+#         # your code to record hyperparameters goes here
+#         self.params = params
 
+#     @rank_zero_only
+#     def log_metrics(self, metrics, step):
+#         # metrics is a dictionary of metric names and values
+#         # your code to record metrics goes here
+#         # if step % self.params.logging_steps
+#         pass
 
-class ExampleLogger(LightningLoggerBase):
-    @rank_zero_only
-    def log_hyperparams(self, params):
-        # params is an argparse.Namespace
-        # your code to record hyperparameters goes here
-        self.params = params
+#     def save(self):
+#         # Optional. Any code necessary to save logger data goes here
+#         pass
 
-    @rank_zero_only
-    def log_metrics(self, metrics, step):
-        # metrics is a dictionary of metric names and values
-        # your code to record metrics goes here
-        # if step % self.params.logging_steps
-        pass
-
-    def save(self):
-        # Optional. Any code necessary to save logger data goes here
-        pass
-
-    @rank_zero_only
-    def finalize(self, status):
-        # Optional. Any code that needs to be run after training
-        # finishes goes here
-        pass
+#     @rank_zero_only
+#     def finalize(self, status):
+#         # Optional. Any code that needs to be run after training
+#         # finishes goes here
+#         pass
 
 class BaseTransformer(pl.LightningModule):
     def __init__(self, hparams, num_labels=None):
