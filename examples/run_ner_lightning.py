806fd5afc9ba1c2e2bfe06d29f7fc3caa7d87c77
Sasha <arush@g.harvard.edu>
2020-02-11 11:01:31 -0500

    .

--- examples/run_ner_lightning.py
+++ examples/run_ner_lightning.py
@@ -10,87 +10,18 @@ from seqeval.metrics import f1_score, precision_score, recall_score
 from torch.nn import CrossEntropyLoss
 from torch.utils.data import DataLoader, RandomSampler, SequentialSampler, TensorDataset
 from torch.utils.data.distributed import DistributedSampler
-from transformers import *
+
+from transformer_base import BaseTransformer, set_seed, add_generic_args, generic_train
 from utils_ner import convert_examples_to_features, get_labels, read_examples_from_file
 import pytorch_lightning as pl
 
 logger = logging.getLogger(__name__)
 
-ALL_MODELS = sum(
-    (
-        tuple(conf.pretrained_config_archive_map.keys())
-        for conf in (BertConfig, RobertaConfig, DistilBertConfig, CamembertConfig, XLMRobertaConfig)
-    ),
-    (),
-)
-
-MODEL_CLASSES = {
-    "bert": (BertConfig, BertForTokenClassification, BertTokenizer),
-    "roberta": (RobertaConfig, RobertaForTokenClassification, RobertaTokenizer),
-    "distilbert": (DistilBertConfig, DistilBertForTokenClassification, DistilBertTokenizer),
-    "camembert": (CamembertConfig, CamembertForTokenClassification, CamembertTokenizer),
-    "xlmroberta": (XLMRobertaConfig, XLMRobertaForTokenClassification, XLMRobertaTokenizer),
-}
 
 
 class NERTransformer(pl.LightningModule):
-
     def __init__(self, hparams):
-        "Initialize a model."
-
-        super(NERTransformer, self).__init__()
-        self.hparams = hparams
-        args = self.hparams
-        args.model_type = args.model_type.lower()
-
-        # Prepare CONLL-2003 task
-        labels = get_labels(args.labels)
-        num_labels = len(labels)
-        # Use cross entropy ignore index as padding label id so that only real label ids contribute to the loss later
-        self.pad_token_label_id = CrossEntropyLoss().ignore_index
-
-        config_class, model_class, tokenizer_class = MODEL_CLASSES[args.model_type]
-        config = config_class.from_pretrained(
-            args.config_name if args.config_name else args.model_name_or_path,
-            num_labels=num_labels,
-            cache_dir=args.cache_dir if args.cache_dir else None,
-        )
-        tokenizer = tokenizer_class.from_pretrained(
-            args.tokenizer_name if args.tokenizer_name else args.model_name_or_path,
-            do_lower_case=args.do_lower_case,
-            cache_dir=args.cache_dir if args.cache_dir else None,
-        )
-        model = model_class.from_pretrained(
-            args.model_name_or_path,
-            from_tf=bool(".ckpt" in args.model_name_or_path),
-            config=config,
-            cache_dir=args.cache_dir if args.cache_dir else None,
-        )
-        self.config, self.tokenizer, self.model = config, tokenizer, model
-
-
-    def configure_optimizers(self):
-        "Prepare optimizer and schedule (linear warmup and decay)"
-        args = self.hparams
-        model = self.model
-
-        t_total = len(self.train_dataloader()) // args.gradient_accumulation_steps * args.num_train_epochs
-        no_decay = ["bias", "LayerNorm.weight"]
-        optimizer_grouped_parameters = [
-            {
-                "params": [p for n, p in model.named_parameters()
-                           if not any(nd in n for nd in no_decay)],
-                "weight_decay": args.weight_decay,
-            },
-            {"params": [p for n, p in model.named_parameters()
-                        if any(nd in n for nd in no_decay)],
-             "weight_decay": 0.0},
-        ]
-        optimizer = AdamW(optimizer_grouped_parameters, lr=args.learning_rate, eps=args.adam_epsilon)
-        scheduler = get_linear_schedule_with_warmup(
-            optimizer, num_warmup_steps=args.warmup_steps, num_training_steps=t_total
-        )
-        return [optimizer], [scheduler]
+        super(NERTransformer, self).__init__(hparams)
 
     def forward(self, **inputs):
         return self.model(**inputs)
@@ -114,10 +45,11 @@ class NERTransformer(pl.LightningModule):
     def load_dataset(self, mode, batch_size):
         args = self.hparams
         labels = get_labels(args.labels)
+        pad_token_label_id = CrossEntropyLoss().ignore_index
         dataset = load_and_cache_examples(args,
                                           self.tokenizer,
                                           labels,
-                                          self.pad_token_label_id,
+                                          pad_token_label_id,
                                           mode=mode)
         sampler = RandomSampler(dataset)
         dataloader = DataLoader(dataset,
@@ -125,20 +57,6 @@ class NERTransformer(pl.LightningModule):
                                 batch_size=batch_size)
         return dataloader
 
-    @pl.data_loader
-    def train_dataloader(self):
-        args = self.hparams
-        return self.load_dataset("train", args.train_batch_size)
-
-    @pl.data_loader
-    def val_dataloader(self):
-        args = self.hparams
-        return self.load_dataset("dev", args.eval_batch_size)
-
-    @pl.data_loader
-    def test_dataloader(self):
-        args = self.hparams
-        return self.load_dataset("test", args.eval_batch_size)
 
     def validation_step(self, batch, batch_nb):
         inputs = {"input_ids": batch[0], "attention_mask": batch[1], "labels": batch[3]}
@@ -158,21 +76,8 @@ class NERTransformer(pl.LightningModule):
 
     @staticmethod
     def add_model_specific_args(parser, root_dir):
-        parser.add_argument(
-            "--model_type",
-            default=None,
-            type=str,
-            required=True,
-            help="Model type selected in the list: " + ", ".join(MODEL_CLASSES.keys()),
-        )
-        parser.add_argument(
-            "--model_name_or_path",
-            default=None,
-            type=str,
-            required=True,
-            help="Path to pre-trained model or shortcut name selected in the list: " + ", ".join(ALL_MODELS),
-        )
-
+        # Add base transformer options
+        BaseTransformer.add_model_specific_args(parser, root_dir)
         parser.add_argument(
             "--max_seq_length",
             default=128,
@@ -182,59 +87,10 @@ class NERTransformer(pl.LightningModule):
         )
 
         parser.add_argument(
-            "--config_name", default="", type=str, help="Pretrained config name or path if not the same as model_name"
-        )
-        parser.add_argument(
-            "--tokenizer_name",
-            default="",
-            type=str,
-            help="Pretrained tokenizer name or path if not the same as model_name",
-        )
-        parser.add_argument(
-            "--cache_dir",
-            default="",
-            type=str,
-            help="Where do you want to store the pre-trained models downloaded from s3",
-        )
-
-        parser.add_argument(
-            "--overwrite_output_dir", action="store_true", help="Overwrite the content of the output directory"
-        )
-        parser.add_argument(
-            "--overwrite_cache", action="store_true", help="Overwrite the cached training and evaluation sets"
-        )
-
-
-        parser.add_argument(
-            "--do_lower_case", action="store_true", help="Set this flag if you are using an uncased model."
-        )
-        parser.add_argument("--learning_rate", default=5e-5, type=float, help="The initial learning rate for Adam.")
-        parser.add_argument("--weight_decay", default=0.0, type=float, help="Weight decay if we apply some.")
-        parser.add_argument("--adam_epsilon", default=1e-8, type=float, help="Epsilon for Adam optimizer.")
-        parser.add_argument("--warmup_steps", default=0, type=int, help="Linear warmup over warmup_steps.")
-
-        # Other parameters
-        parser.add_argument(
             "--labels",
             default="",
             type=str,
             help="Path to a file containing all labels. If not specified, CoNLL-2003 labels are used.",
-        )
-        parser.add_argument(
-            "--max_steps",
-            default=-1,
-            type=int,
-            help="If > 0: set total number of training steps to perform. Override num_train_epochs.",
-        )
-        parser.add_argument(
-            "--train_batch_size",
-            default=32,
-            type=int
-        )
-        parser.add_argument(
-            "--eval_batch_size",
-            default=32,
-            type=int
         )
 
         parser.add_argument(
@@ -245,16 +101,13 @@ class NERTransformer(pl.LightningModule):
             help="The input data dir. Should contain the training files for the CoNLL-2003 NER task.",
         )
 
+        parser.add_argument(
+            "--overwrite_cache", action="store_true", help="Overwrite the cached training and evaluation sets"
+        )
 
         return parser
 
 
-def set_seed(args):
-    random.seed(args.seed)
-    np.random.seed(args.seed)
-    torch.manual_seed(args.seed)
-    if args.n_gpu > 0:
-        torch.cuda.manual_seed_all(args.seed)
 
 def load_and_cache_examples(args, tokenizer, labels, pad_token_label_id, mode):
     if args.local_rank not in [-1, 0] and not evaluate:
@@ -304,96 +157,10 @@ def load_and_cache_examples(args, tokenizer, labels, pad_token_label_id, mode):
     dataset = TensorDataset(all_input_ids, all_input_mask, all_segment_ids, all_label_ids)
     return dataset
 
-
-def main(hparams):
-    args = hparams
-    # init model
-    set_seed(args)
-
-    # Setup distant debugging if needed
-    if args.server_ip and args.server_port:
-        # Distant debugging - see https://code.visualstudio.com/docs/python/debugging#_attach-to-a-local-script
-        import ptvsd
-
-        print("Waiting for debugger attach")
-        ptvsd.enable_attach(address=(args.server_ip, args.server_port), redirect_output=True)
-        ptvsd.wait_for_attach()
-
-    checkpoint_callback = pl.callbacks.ModelCheckpoint(filepath=args.output_dir,
-                                                       prefix="checkpoint",
-                                                       period=args.save_steps)
-
-    model = NERTransformer(args)
-    trainer = pl.Trainer(accumulate_grad_batches=args.gradient_accumulation_steps,
-                         gpus=args.n_gpu,
-                         max_epochs=args.num_train_epochs,
-                         default_save_path=args.output_dir,
-                         use_amp=args.fp16,
-                         gradient_clip_val=args.max_grad_norm,
-                         checkpoint_callback=checkpoint_callback,
-                         log_save_interval=args.logging_steps
-    )
-    if args.do_train:
-        trainer.fit(model)
-
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description="parser for fast-neural-style")
-    parser.add_argument(
-            "--output_dir",
-            default=None,
-            type=str,
-            required=True,
-            help="The output directory where the model predictions and checkpoints will be written.",
-        )
-
-    parser.add_argument(
-        "--fp16",
-        action="store_true",
-        help="Whether to use 16-bit (mixed) precision (through NVIDIA apex) instead of 32-bit",
-    )
-
-    parser.add_argument(
-        "--num_train_epochs", default=3, type=int, help="Total number of training epochs to perform."
-    )
-
-    parser.add_argument(
-        "--n_gpu",
-        type=int, default=1
-    )
-    parser.add_argument("--max_grad_norm", default=1.0, type=float, help="Max gradient norm.")
-    # add model specific args
-    parser.add_argument("--do_train", action="store_true", help="Whether to run training.")
-    parser.add_argument("--do_eval", action="store_true", help="Whether to run eval on the dev set.")
-    parser.add_argument("--do_predict", action="store_true", help="Whether to run predictions on the test set.")
-    parser.add_argument(
-        "--gradient_accumulation_steps",
-        type=int,
-        default=1,
-        help="Number of updates steps to accumulate before performing a backward/update pass.",
-    )
-
-    parser.add_argument(
-        "--evaluate_during_training",
-        action="store_true",
-        help="Whether to run evaluation during training at each logging step.",
-    )
-
-
-    parser.add_argument("--logging_steps", type=int, default=500, help="Log every X updates steps.")
-    parser.add_argument("--save_steps", type=int, default=500, help="Save checkpoint every X updates steps.")
-    parser.add_argument(
-        "--eval_all_checkpoints",
-        action="store_true",
-        help="Evaluate all checkpoints starting with the same prefix as model_name ending and ending with step number",
-    )
-
-    parser.add_argument("--local_rank", type=int, default=-1, help="For distributed training: local_rank")
-    parser.add_argument("--server_ip", type=str, default="", help="For distant debugging.")
-    parser.add_argument("--server_port", type=str, default="", help="For distant debugging.")
-
-    parser.add_argument("--seed", type=int, default=42, help="random seed for initialization")
-
+    parser = argparse.ArgumentParser()
+    add_generic_args(parser, os.getcwd())
     parser = NERTransformer.add_model_specific_args(parser, os.getcwd())
     args = parser.parse_args()
-
-    main(args)
+    model = NERTransformer(args)
+    generic_train(model, args)
---
+++ examples/transformer_base.py
@@ -0,0 +1,244 @@
+import argparse
+import glob
+import logging
+import os
+import random
+import pytorch_lightning as pl
+
+from transformers import *
+
+ALL_MODELS = sum(
+    (
+        tuple(conf.pretrained_config_archive_map.keys())
+        for conf in (BertConfig, RobertaConfig, DistilBertConfig, CamembertConfig, XLMRobertaConfig)
+    ),
+    (),
+)
+
+MODEL_CLASSES = {
+    "bert": (BertConfig, BertForTokenClassification, BertTokenizer),
+    "roberta": (RobertaConfig, RobertaForTokenClassification, RobertaTokenizer),
+    "distilbert": (DistilBertConfig, DistilBertForTokenClassification, DistilBertTokenizer),
+    "camembert": (CamembertConfig, CamembertForTokenClassification, CamembertTokenizer),
+    "xlmroberta": (XLMRobertaConfig, XLMRobertaForTokenClassification, XLMRobertaTokenizer),
+}
+
+def set_seed(args):
+    random.seed(args.seed)
+    np.random.seed(args.seed)
+    torch.manual_seed(args.seed)
+    if args.n_gpu > 0:
+        torch.cuda.manual_seed_all(args.seed)
+
+
+class BaseTransformer(pl.LightningModule):
+    def __init__(self, hparams):
+        "Initialize a model."
+
+        super(BaseTransformer, self).__init__()
+        self.hparams = hparams
+        args = self.hparams
+        args.model_type = args.model_type.lower()
+
+        # Prepare CONLL-2003 task
+        labels = get_labels(args.labels)
+        num_labels = len(labels)
+
+
+        config_class, model_class, tokenizer_class = MODEL_CLASSES[args.model_type]
+        config = config_class.from_pretrained(
+            args.config_name if args.config_name else args.model_name_or_path,
+            num_labels=num_labels,
+            cache_dir=args.cache_dir if args.cache_dir else None,
+        )
+        tokenizer = tokenizer_class.from_pretrained(
+            args.tokenizer_name if args.tokenizer_name else args.model_name_or_path,
+            do_lower_case=args.do_lower_case,
+            cache_dir=args.cache_dir if args.cache_dir else None,
+        )
+        model = model_class.from_pretrained(
+            args.model_name_or_path,
+            from_tf=bool(".ckpt" in args.model_name_or_path),
+            config=config,
+            cache_dir=args.cache_dir if args.cache_dir else None,
+        )
+        self.config, self.tokenizer, self.model = config, tokenizer, model
+
+
+    def configure_optimizers(self):
+        "Prepare optimizer and schedule (linear warmup and decay)"
+        args = self.hparams
+        model = self.model
+
+        t_total = len(self.train_dataloader()) // args.gradient_accumulation_steps * args.num_train_epochs
+        no_decay = ["bias", "LayerNorm.weight"]
+        optimizer_grouped_parameters = [
+            {
+                "params": [p for n, p in model.named_parameters()
+                           if not any(nd in n for nd in no_decay)],
+                "weight_decay": args.weight_decay,
+            },
+            {"params": [p for n, p in model.named_parameters()
+                        if any(nd in n for nd in no_decay)],
+             "weight_decay": 0.0},
+        ]
+        optimizer = AdamW(optimizer_grouped_parameters, lr=args.learning_rate, eps=args.adam_epsilon)
+        scheduler = get_linear_schedule_with_warmup(
+            optimizer, num_warmup_steps=args.warmup_steps, num_training_steps=t_total
+        )
+        return [optimizer], [scheduler]
+
+
+    @pl.data_loader
+    def train_dataloader(self):
+        args = self.hparams
+        return self.load_dataset("train", args.train_batch_size)
+
+    @pl.data_loader
+    def val_dataloader(self):
+        args = self.hparams
+        return self.load_dataset("dev", args.eval_batch_size)
+
+    @pl.data_loader
+    def test_dataloader(self):
+        args = self.hparams
+        return self.load_dataset("test", args.eval_batch_size)
+
+    @staticmethod
+    def add_model_specific_args(parser, root_dir):
+        parser.add_argument(
+            "--model_type",
+            default=None,
+            type=str,
+            required=True,
+            help="Model type selected in the list: " + ", ".join(MODEL_CLASSES.keys()),
+        )
+        parser.add_argument(
+            "--model_name_or_path",
+            default=None,
+            type=str,
+            required=True,
+            help="Path to pre-trained model or shortcut name selected in the list: " + ", ".join(ALL_MODELS),
+        )
+        parser.add_argument(
+            "--config_name", default="", type=str, help="Pretrained config name or path if not the same as model_name"
+        )
+        parser.add_argument(
+            "--tokenizer_name",
+            default="",
+            type=str,
+            help="Pretrained tokenizer name or path if not the same as model_name",
+        )
+        parser.add_argument(
+            "--cache_dir",
+            default="",
+            type=str,
+            help="Where do you want to store the pre-trained models downloaded from s3",
+        )
+        parser.add_argument(
+            "--do_lower_case", action="store_true", help="Set this flag if you are using an uncased model."
+        )
+        parser.add_argument("--learning_rate", default=5e-5, type=float, help="The initial learning rate for Adam.")
+        parser.add_argument("--weight_decay", default=0.0, type=float, help="Weight decay if we apply some.")
+        parser.add_argument("--adam_epsilon", default=1e-8, type=float, help="Epsilon for Adam optimizer.")
+        parser.add_argument("--warmup_steps", default=0, type=int, help="Linear warmup over warmup_steps.")
+        parser.add_argument(
+            "--num_train_epochs", default=3, type=int, help="Total number of training epochs to perform."
+        )
+
+        parser.add_argument(
+            "--train_batch_size",
+            default=32,
+            type=int
+        )
+        parser.add_argument(
+            "--eval_batch_size",
+            default=32,
+            type=int
+        )
+
+def add_generic_args(parser, root_dir):
+    parser.add_argument(
+            "--output_dir",
+            default=None,
+            type=str,
+            required=True,
+            help="The output directory where the model predictions and checkpoints will be written.",
+        )
+
+    parser.add_argument(
+        "--fp16",
+        action="store_true",
+        help="Whether to use 16-bit (mixed) precision (through NVIDIA apex) instead of 32-bit",
+    )
+
+
+    parser.add_argument(
+        "--overwrite_output_dir", action="store_true", help="Overwrite the content of the output directory"
+    )
+
+    parser.add_argument(
+        "--n_gpu",
+        type=int, default=1
+    )
+    parser.add_argument("--max_grad_norm", default=1.0, type=float, help="Max gradient norm.")
+    # add model specific args
+    parser.add_argument("--do_train", action="store_true", help="Whether to run training.")
+    parser.add_argument("--do_eval", action="store_true", help="Whether to run eval on the dev set.")
+    parser.add_argument("--do_predict", action="store_true", help="Whether to run predictions on the test set.")
+    parser.add_argument(
+        "--gradient_accumulation_steps",
+        type=int,
+        default=1,
+        help="Number of updates steps to accumulate before performing a backward/update pass.",
+    )
+
+    parser.add_argument(
+        "--evaluate_during_training",
+        action="store_true",
+        help="Whether to run evaluation during training at each logging step.",
+    )
+
+
+    parser.add_argument("--logging_steps", type=int, default=500, help="Log every X updates steps.")
+    parser.add_argument("--save_steps", type=int, default=500, help="Save checkpoint every X updates steps.")
+    parser.add_argument(
+        "--eval_all_checkpoints",
+        action="store_true",
+        help="Evaluate all checkpoints starting with the same prefix as model_name ending and ending with step number",
+    )
+
+    parser.add_argument("--local_rank", type=int, default=-1, help="For distributed training: local_rank")
+    parser.add_argument("--server_ip", type=str, default="", help="For distant debugging.")
+    parser.add_argument("--server_port", type=str, default="", help="For distant debugging.")
+
+    parser.add_argument("--seed", type=int, default=42, help="random seed for initialization")
+
+def generic_train(model, args):
+    # init model
+    set_seed(args)
+
+    # Setup distant debugging if needed
+    if args.server_ip and args.server_port:
+        # Distant debugging - see https://code.visualstudio.com/docs/python/debugging#_attach-to-a-local-script
+        import ptvsd
+
+        print("Waiting for debugger attach")
+        ptvsd.enable_attach(address=(args.server_ip, args.server_port), redirect_output=True)
+        ptvsd.wait_for_attach()
+
+    checkpoint_callback = pl.callbacks.ModelCheckpoint(filepath=args.output_dir,
+                                                       prefix="checkpoint",
+                                                       period=args.save_steps)
+
+    trainer = pl.Trainer(accumulate_grad_batches=args.gradient_accumulation_steps,
+                         gpus=args.n_gpu,
+                         max_epochs=args.num_train_epochs,
+                         default_save_path=args.output_dir,
+                         use_amp=args.fp16,
+                         gradient_clip_val=args.max_grad_norm,
+                         checkpoint_callback=checkpoint_callback,
+                         log_save_interval=args.logging_steps
+    )
+    if args.do_train:
+        trainer.fit(model)
