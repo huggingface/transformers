e07af6413e3477472ab30690df64a83e738028d0
Morgan Funtowicz <morgan@huggingface.co>
2020-02-12 14:10:43 +0100

    Addressing review comment: Bump tokenizers dependency to 0.4.2
    Signed-off-by: Morgan Funtowicz <morgan@huggingface.co>

--- setup.py
+++ setup.py
@@ -89,7 +89,7 @@ setup(
     packages=find_packages("src"),
     install_requires=[
         "numpy",
-        "tokenizers == 0.4.0",
+        "tokenizers == 0.4.2",
         # accessing files from S3 directly
         "boto3",
         # filesystem locks e.g. to prevent parallel downloads
