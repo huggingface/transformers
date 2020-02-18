448b9ed02ea3b49f3040fa09f57cbb15246a362b
Sasha <arush@g.harvard.edu>
2020-02-18 16:56:28 -0500

    .

--- examples/ner/transformer_base.py
+++ examples/ner/transformer_base.py
@@ -5,6 +5,7 @@ import numpy as np
 import pytorch_lightning as pl
 import torch
 
+from torch.utils.data import RandomSampler
 from torch.utils.data.distributed import DistributedSampler
 from transformers import (
     AdamW,
