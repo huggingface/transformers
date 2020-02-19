4353c4b53340000e068361fcfa94ab75425e9443
Patrick von Platen <patrick.v.platen@gmail.com>
2020-02-19 14:20:10 +0100

    add slow language generation tests for pretrained models using hardcoded output with pytorch seed

--- tests/test_modeling_pretrained_lm_generation.py
+++ tests/test_modeling_pretrained_lm_generation.py
@@ -15,12 +15,14 @@
 
 
 import unittest
+
 import ipdb
 
 from transformers import is_torch_available
 
 from .utils import CACHE_DIR, require_torch, slow, torch_device
-from .utils_generation import GPT2_PRETRAINED_MODEL_GENERATION_TEST_CASES, GPT2_PADDING_TOKENS
+from .utils_generation import GPT2_PADDING_TOKENS, GPT2_PRETRAINED_MODEL_GENERATION_TEST_CASES
+
 
 if is_torch_available():
     import torch
@@ -45,7 +47,7 @@ class LMGenerationTest(unittest.TestCase):
             num_beams=1,
             bos_token_id=None,
             pad_token_id=None,
-            eos_token_id=None
+            eos_token_id=None,
         ):
             self.parent = parent
             self.max_length = max_length
@@ -57,9 +59,9 @@ class LMGenerationTest(unittest.TestCase):
             self.eos_token_id = eos_token_id
 
         def set_tokens(self, tokens_archieve):
-            self.bos_token_id = tokens_archieve['bos_token_id'] if 'bos_token_id' in tokens_archieve else None
-            self.pad_token_id = tokens_archieve['pad_token_id'] if 'pad_token_id' in tokens_archieve else None
-            self.eos_token_id = tokens_archieve['eos_token_id'] if 'eos_token_id' in tokens_archieve else None
+            self.bos_token_id = tokens_archieve["bos_token_id"] if "bos_token_id" in tokens_archieve else None
+            self.pad_token_id = tokens_archieve["pad_token_id"] if "pad_token_id" in tokens_archieve else None
+            self.eos_token_id = tokens_archieve["eos_token_id"] if "eos_token_id" in tokens_archieve else None
 
         def list_to_torch_tensor(self, tokens_list):
             return torch.tensor(tokens_list, dtype=torch.long, device=torch_device)
@@ -70,22 +72,30 @@ class LMGenerationTest(unittest.TestCase):
                     model = lm_model.from_pretrained(model_name, cache_dir=CACHE_DIR)
                     self.parent.assertIsNotNone(model)
                     model_test_config = model_generation_test_cases[model_name]
-                    torch_seed = model_test_config['seed']
-                    input_ids = self.list_to_torch_tensor(model_test_config['input'])
-                    expected_output_ids = model_test_config['exp_output']
+                    torch_seed = model_test_config["seed"]
+                    input_ids = self.list_to_torch_tensor(model_test_config["input"])
+                    expected_output_ids = model_test_config["exp_output"]
 
                     torch.manual_seed(torch_seed)
-                    output_ids = model.generate(input_ids, bos_token_id=self.bos_token_id, pad_token_id=self.pad_token_id, eos_token_ids=self.eos_token_id, max_length=self.max_length, do_sample=self.do_sample, num_return_sequences=self.num_return_sequences, num_beams=self.num_beams)
-                    ipdb.set_trace()
-                    self.parent.assertListEqual(
-                        list(output_ids[0]),
-                        expected_output_ids
+                    output_ids = model.generate(
+                        input_ids,
+                        bos_token_id=self.bos_token_id,
+                        pad_token_id=self.pad_token_id,
+                        eos_token_ids=self.eos_token_id,
+                        max_length=self.max_length,
+                        do_sample=self.do_sample,
+                        num_return_sequences=self.num_return_sequences,
+                        num_beams=self.num_beams,
                     )
+                    ipdb.set_trace()
+                    self.parent.assertListEqual(list(output_ids[0]), expected_output_ids)
 
     def setUp(self):
         self.model_tester = LMGenerationTest.LMGenerationTester(self)
 
-#    @slow
+    @slow
     def test_gpt2_generation(self):
         self.model_tester.set_tokens(GPT2_PADDING_TOKENS)
-        self.model_tester.check_language_generation(GPT2_PRETRAINED_MODEL_GENERATION_TEST_CASES, GPT2_PRETRAINED_MODEL_ARCHIVE_MAP, GPT2LMHeadModel)
+        self.model_tester.check_language_generation(
+            GPT2_PRETRAINED_MODEL_GENERATION_TEST_CASES, GPT2_PRETRAINED_MODEL_ARCHIVE_MAP, GPT2LMHeadModel,
+        )
