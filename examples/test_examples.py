26abb189a74818dd3af5ec792ae07162e555bbaa
Patrick von Platen <patrick.v.platen@gmail.com>
2020-02-21 18:10:00 +0100

    Improve special_token_id logic in run_generation.py and add tests (#2885)
    * improving generation
    
    * finalized special token behaviour for no_beam_search generation
    
    * solved modeling_utils merge conflict
    
    * solve merge conflicts in modeling_utils.py
    
    * add run_generation improvements from PR #2749
    
    * adapted language generation to not use hardcoded -1 if no padding token is available
    
    * remove the -1 removal as hard coded -1`s are not necessary anymore
    
    * add lightweight language generation testing for randomely initialized models - just checking whether no errors are thrown
    
    * add slow language generation tests for pretrained models using hardcoded output with pytorch seed
    
    * delete ipdb
    
    * check that all generated tokens are valid
    
    * renaming
    
    * renaming Generation -> Generate
    
    * make style
    
    * updated so that generate_beam_search has same token behavior than generate_no_beam_search
    
    * consistent return format for run_generation.py
    
    * deleted pretrain lm generate tests -> will be added in another PR
    
    * cleaning of unused if statements and renaming
    
    * run_generate will always return an iterable
    
    * make style
    
    * consistent renaming
    
    * improve naming, make sure generate function always returns the same tensor, add docstring
    
    * add slow tests for all lmhead models
    
    * make style and improve example comments modeling_utils
    
    * better naming and refactoring in modeling_utils
    
    * improving generation
    
    * finalized special token behaviour for no_beam_search generation
    
    * solved modeling_utils merge conflict
    
    * solve merge conflicts in modeling_utils.py
    
    * add run_generation improvements from PR #2749
    
    * adapted language generation to not use hardcoded -1 if no padding token is available
    
    * remove the -1 removal as hard coded -1`s are not necessary anymore
    
    * add lightweight language generation testing for randomely initialized models - just checking whether no errors are thrown
    
    * add slow language generation tests for pretrained models using hardcoded output with pytorch seed
    
    * delete ipdb
    
    * check that all generated tokens are valid
    
    * renaming
    
    * renaming Generation -> Generate
    
    * make style
    
    * updated so that generate_beam_search has same token behavior than generate_no_beam_search
    
    * consistent return format for run_generation.py
    
    * deleted pretrain lm generate tests -> will be added in another PR
    
    * cleaning of unused if statements and renaming
    
    * run_generate will always return an iterable
    
    * make style
    
    * consistent renaming
    
    * improve naming, make sure generate function always returns the same tensor, add docstring
    
    * add slow tests for all lmhead models
    
    * make style and improve example comments modeling_utils
    
    * better naming and refactoring in modeling_utils
    
    * changed fast random lm generation testing design to more general one
    
    * delete in old testing design in gpt2
    
    * correct old variable name
    
    * temporary fix for encoder_decoder lm generation tests - has to be updated when t5 is fixed
    
    * adapted all fast random generate tests to new design
    
    * better warning description in modeling_utils
    
    * better comment
    
    * better comment and error message
    
    Co-authored-by: Thomas Wolf <thomwolf@users.noreply.github.com>

--- examples/test_examples.py
+++ examples/test_examples.py
@@ -97,4 +97,4 @@ class ExamplesTests(unittest.TestCase):
         model_type, model_name = ("--model_type=openai-gpt", "--model_name_or_path=openai-gpt")
         with patch.object(sys, "argv", testargs + [model_type, model_name]):
             result = run_generation.main()
-            self.assertGreaterEqual(len(result), 10)
+            self.assertGreaterEqual(len(result[0]), 10)
