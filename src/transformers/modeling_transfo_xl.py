c370b829fd3a0b942a86c1ea52b97750877a26d2
andompesta <ando.cavallari@gmail.com>
2020-02-23 05:38:31 +0800

    fix transformer xl update memmory fn call

--- src/transformers/modeling_transfo_xl.py
+++ src/transformers/modeling_transfo_xl.py
@@ -681,7 +681,7 @@ class TransfoXLModel(TransfoXLPreTrainedModel):
         else:
             return None
 
-    def _update_mems(self, hids, mems, qlen, mlen):
+    def _update_mems(self, hids, mems, mlen, qlen):
         # does not deal with None
         if mems is None:
             return None
