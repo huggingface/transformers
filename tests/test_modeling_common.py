a2b4b6bdfc62d001486a8aaa6ac32a59faed1125
Patrick von Platen <patrick.v.platen@gmail.com>
2020-02-24 14:24:08 +0100

    merge conflicts

--- tests/test_modeling_common.py
+++ tests/test_modeling_common.py
@@ -22,7 +22,7 @@ import unittest
 
 from transformers import is_torch_available
 
-from .utils import require_torch, slow, torch_device
+from .utils import CACHE_DIR, require_torch, slow, torch_device
 
 
 if is_torch_available():
