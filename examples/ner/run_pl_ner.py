14d4f38e1f9b1f69803dc2777358835887be4241
Sasha <arush@g.harvard.edu>
2020-02-25 14:11:33 -0500

    .

--- examples/ner/run_pl_ner.py
+++ examples/ner/run_pl_ner.py
@@ -75,7 +75,7 @@ class NERTransformer(BaseTransformer):
                     pad_on_left=bool(args.model_type in ["xlnet"]),
                     pad_token=self.tokenizer.convert_tokens_to_ids([self.tokenizer.pad_token])[0],
                     pad_token_segment_id=4 if args.model_type in ["xlnet"] else 0,
-                    pad_token_label_id=pad_token_label_id,
+                    pad_token_label_id=self.pad_token_label_id,
                 )
                 logger.info("Saving features into cached file %s", cached_features_file)
                 torch.save(features, cached_features_file)
