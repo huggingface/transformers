f60be0cf3e87bb938a7554faec14d45cdc4d865c
sshleifer <sshleifer@gmail.com>
2020-02-27 14:22:38 -0500

    remove commented code

--- src/transformers/modeling_bart.py
+++ src/transformers/modeling_bart.py
@@ -822,7 +822,7 @@ class BartModel(PretrainedBartModel):
         if attention_mask is not None:
             assert attention_mask.dim() == 2
 
-            attention_mask = (1.0 - attention_mask.long()).bool()  # * -10000.0
+            attention_mask = (1.0 - attention_mask.long()).bool()
             assert attention_mask.max() <= 1
 
         # make masks if user doesn't supply
