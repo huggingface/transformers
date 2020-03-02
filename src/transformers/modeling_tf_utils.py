9ea0aa5a5b91150f0431648daed44157f867e164
Patrick von Platen <patrick.v.platen@gmail.com>
2020-03-02 18:02:34 +0100

    fix conflicts

--- src/transformers/modeling_tf_utils.py
+++ src/transformers/modeling_tf_utils.py
@@ -810,6 +810,84 @@ def set_tensor_by_indices_to_value(tensor, indices, value):
     return tf.where(indices, value_tensor, tensor)
 
 
+def tf_top_k_top_p_filtering(
+    logits, top_k=0, top_p=1.0, filter_value=-float("Inf"), min_tokens_to_keep=1
+):
+    """ Filter a distribution of logits using top-k and/or nucleus (top-p) filtering
+        Args:
+            logits: logits distribution shape (batch size, vocabulary size)
+            if top_k > 0: keep only top k tokens with highest probability (top-k filtering).
+            if top_p < 1.0: keep the top tokens with cumulative probability >= top_p (nucleus filtering).
+                Nucleus filtering is described in Holtzman et al. (http://arxiv.org/abs/1904.09751)
+            Make sure we keep at least min_tokens_to_keep per batch example in the output
+        From: https://gist.github.com/thomwolf/1a5a29f6962089e871b94cbd09daf317
+    """
+    logits_shape = logits.shape
+
+    if top_k > 0:
+        top_k = min(max(top_k, min_tokens_to_keep), logits_shape[-1])  # Safety check
+        # Remove all tokens with a probability less than the last token of the top-k
+        indices_to_remove = logits < tf.math.top_k(logits, k=top_k)[0][..., -1, None]
+        logits = set_tensor_by_indices_to_value(logits, indices_to_remove, filter_value)
+
+    if top_p < 1.0:
+        sorted_indices = tf.argsort(logits, direction="DESCENDING")
+        sorted_logits = tf.gather(
+            logits, sorted_indices, axis=-1, batch_dims=1
+        )  # expects logits to be of dim (batch_size, vocab_size)
+
+        cumulative_probs = tf.math.cumsum(
+            tf.nn.softmax(sorted_logits, axis=-1), axis=-1
+        )
+
+        # Remove tokens with cumulative probability above the threshold (token with 0 are kept)
+        sorted_indices_to_remove = cumulative_probs > top_p
+
+        if min_tokens_to_keep > 1:
+            # Keep at least min_tokens_to_keep (set to min_tokens_to_keep-1 because we add the first one below)
+            sorted_indices_to_remove = tf.concat(
+                [
+                    tf.zeros_like(sorted_indices_to_remove[:, :min_tokens_to_keep]),
+                    sorted_indices_to_remove[:, min_tokens_to_keep:],
+                ],
+                -1,
+            )
+
+        # Shift the indices to the right to keep also the first token above the threshold
+        sorted_indices_to_remove = tf.roll(sorted_indices_to_remove, 1, axis=-1)
+        sorted_indices_to_remove = tf.concat(
+            [
+                tf.zeros_like(sorted_indices_to_remove[:, :1]),
+                sorted_indices_to_remove[:, 1:],
+            ],
+            -1,
+        )
+        # scatter sorted tensors to original indexing
+        indices_to_remove = scatter_values_on_batch_indices(sorted_indices_to_remove, sorted_indices)
+        logits = set_tensor_by_indices_to_value(logits, indices_to_remove, filter_value)
+    return logits
+
+
+def scatter_values_on_batch_indices(values, batch_indices):
+    shape = batch_indices.shape
+    # broadcast batch dim to shape
+    broad_casted_batch_dims = tf.reshape(
+        tf.broadcast_to(tf.expand_dims(tf.range(shape[0]), axis=-1), shape), [1, -1]
+    )
+    # transform batch_indices to pair_indices
+    pair_indices = tf.transpose(
+        tf.concat([broad_casted_batch_dims, tf.reshape(batch_indices, [1, -1])], 0)
+    )
+    # scatter values to pair indices
+    return tf.scatter_nd(pair_indices, tf.reshape(values, [-1]), shape)
+
+
+def set_tensor_by_indices_to_value(tensor, indices, value):
+    # create value_tensor since tensor value assignment is not possible in TF
+    value_tensor = tf.zeros_like(tensor) + value
+    return tf.where(indices, value_tensor, tensor)
+
+
 class TFConv1D(tf.keras.layers.Layer):
     def __init__(self, nf, nx, initializer_range=0.02, **kwargs):
         """ TFConv1D layer as defined by Radford et al. for OpenAI GPT (and also used in GPT-2)
--- tests/test_torch_tf_conversion.py
+++ tests/test_torch_tf_conversion.py
@@ -57,3 +57,49 @@ if is_torch_available() and is_tf_available():
 
 def get_random_numpy_array(shape, upper_limit=1, lower_limit=-1):
     return (upper_limit - lower_limit) * np.random.random(shape) + lower_limit
+from transformers import is_torch_available
+from .utils import require_torch, torch_device
+import sys
+
+import tensorflow as tf
+import torch
+import numpy as np
+
+import unittest
+
+sys.path.insert(1, '../src')
+
+from transformers.modeling_utils import top_k_top_p_filtering  # noqa: E402
+from transformers.modeling_tf_utils import tf_top_k_top_p_filtering  # noqa: E402
+
+
+@require_torch
+class TorchTFConversionTest(unittest.TestCase):
+
+    def test_top_k_top_p_filtering(self):
+        np_logits = get_random_numpy_array((5, 100))
+        pt_logits = to_pt(np_logits)
+        tf_logits = to_tf(np_logits)
+
+        # check with all arguments used
+        top_k, top_p, min_tokens_to_keep = 10, 0.5, 6
+
+        filtered_logits_pt = top_k_top_p_filtering(pt_logits, top_k=top_k, top_p=top_p, min_tokens_to_keep=min_tokens_to_keep)
+        filtered_logits_tf = tf_top_k_top_p_filtering(tf_logits, top_k=top_k, top_p=top_p, min_tokens_to_keep=min_tokens_to_keep)
+
+        self.assertTrue(np.allclose(filtered_logits_pt, filtered_logits_tf))
+
+
+# TODO: think about dtype
+def get_random_numpy_array(shape, upper_limit=1, lower_limit=-1):
+    return (upper_limit - lower_limit) * np.random.random(shape) + lower_limit
+
+
+# TODO: think about dtype and device
+def to_pt(np_tensor):
+    return torch.from_numpy(np_tensor)
+
+
+# TODO: think about dtype
+def to_tf(np_tensor):
+    return tf.convert_to_tensor(np_tensor)
