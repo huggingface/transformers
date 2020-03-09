525b6b1c5492e6c60efb9ece388c07149983c549
Lysandre <lysandre.debut@reseau.eseo.fr>
2020-03-09 16:52:30 -0400

    TFQA pipeline marked as slow test

--- tests/test_pipelines.py
+++ tests/test_pipelines.py
@@ -291,7 +291,7 @@ class MultiColumnInputTestCase(unittest.TestCase):
             self._test_multicolumn_pipeline(nlp, valid_samples, invalid_samples, mandatory_output_keys)
 
     @require_tf
-    @unittest.skip("This test is failing intermittently. Skipping it until we resolve.")
+    @slow
     def test_tf_question_answering(self):
         mandatory_output_keys = {"score", "answer", "start", "end"}
         valid_samples = [
