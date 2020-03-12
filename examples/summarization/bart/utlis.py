6a6d314692bb76e5130c293083e4a5e53e0894cc
Andre Carrera <acarrera94@gmail.com>
2020-03-11 18:35:30 -0600

    added bart for finetuning

---
+++ examples/summarization/bart/utlis.py
@@ -0,0 +1,84 @@
+import logging
+from torch.utils.data import Dataset
+import os
+
+class CnnDailyMailDataset(Dataset):
+
+    def __init__(self, tokenizer, data_dir="./cnn-dailymail/cnn_dm/", type_path='train', block_size=768):
+        super(CnnDailyMailDataset, ).__init__()
+        self.tokenizer = tokenizer
+
+        self.source = []
+        self.target = []
+
+        print('loading ' + type_path + ' source.')
+
+        with open(os.path.join(data_dir, type_path + '.source'), 'r') as f:
+            for text in f.readlines():  # each text is a line and a full story
+                tokenized = tokenizer.batch_encode_plus([text], max_length=block_size, pad_to_max_length=True,
+                                                        return_tensors='pt')
+                self.source.append(tokenized)
+
+        print('loading ' + type_path + ' target.')
+
+
+        with open(os.path.join(data_dir, type_path + '.target'), 'r') as f:
+            for text in f.readlines():  # each text is a line and a summary
+                tokenized = tokenizer.batch_encode_plus([text], max_length=56, pad_to_max_length=True,
+                                                        return_tensors='pt')
+                self.target.append(tokenized)
+
+    def __len__(self):
+        return len(self.source)
+
+    def __getitem__(self, index):
+        source_ids = self.source[index]['input_ids'].squeeze()
+        target_ids = self.target[index]['input_ids'].squeeze()
+
+        src_mask = self.source[index]['attention_mask'].squeeze()  # might need to squeeze
+
+        return {'source_ids': source_ids,
+                'source_mask': src_mask,
+                'target_ids': target_ids,
+                'target_ids_y': target_ids}
+
+
+
+def add_generic_args(parser, root_dir):
+    parser.add_argument(
+        "--output_dir",
+        default=None,
+        type=str,
+        required=True,
+        help="The output directory where the model predictions and checkpoints will be written.",
+    )
+
+    parser.add_argument(
+        "--fp16",
+        action="store_true",
+        help="Whether to use 16-bit (mixed) precision (through NVIDIA apex) instead of 32-bit",
+    )
+
+    parser.add_argument(
+        "--fp16_opt_level",
+        type=str,
+        default="O1",
+        help="For fp16: Apex AMP optimization level selected in ['O0', 'O1', 'O2', and 'O3']."
+        "See details at https://nvidia.github.io/apex/amp.html",
+    )
+
+    parser.add_argument("--n_gpu", type=int, default=1)
+    parser.add_argument("--n_tpu_cores", type=int, default=0)
+    parser.add_argument("--max_grad_norm", default=1.0, type=float, help="Max gradient norm.")
+    parser.add_argument("--do_train", action="store_true", help="Whether to run training.")
+    parser.add_argument("--do_predict", action="store_true", help="Whether to run predictions on the test set.")
+    parser.add_argument(
+        "--gradient_accumulation_steps",
+        type=int,
+        default=1,
+        help="Number of updates steps to accumulate before performing a backward/update pass.",
+    )
+
+    parser.add_argument("--server_ip", type=str, default="", help="For distant debugging.")
+    parser.add_argument("--server_port", type=str, default="", help="For distant debugging.")
+    parser.add_argument("--seed", type=int, default=42, help="random seed for initialization")
