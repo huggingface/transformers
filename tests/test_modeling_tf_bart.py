6759d8394d02f672de1e2f88639dbdc43e268627
sshleifer <sshleifer@gmail.com>
2020-03-14 17:40:11 -0400

    broken test

--- tests/test_modeling_tf_bart.py
+++ tests/test_modeling_tf_bart.py
@@ -21,8 +21,10 @@ from transformers import is_tf_available
 
 from .test_configuration_common import ConfigTester
 from .test_modeling_tf_common import TFModelTesterMixin, ids_tensor, require_tf
-from .utils import CACHE_DIR, slow, torch_device
+from .utils import CACHE_DIR, slow
 
+# hack
+DEFAULT_DEVICE = 'cpu'
 
 if is_tf_available():
     import tensorflow as tf
@@ -127,7 +129,7 @@ class BARTModelTest(TFModelTesterMixin, unittest.TestCase):
         config, inputs_dict = self.model_tester.prepare_config_and_inputs_for_common()
         decoder_input_ids, decoder_attn_mask = _prepare_bart_decoder_inputs(config, inputs_dict["input_ids"])
         model = TFBartModel(config)
-        model.to(torch_device)
+        model.to(DEFAULT_DEVICE)
         model.eval()
         # test init
         self.assertTrue((model.encoder.embed_tokens.weight == model.shared.weight).all().item())
@@ -220,21 +222,19 @@ class TFBartHeadTests(unittest.TestCase):
 
     def test_sequence_classification_forward(self):
         config, input_ids, batch_size = self._get_config_and_data()
-        labels = _long_tensor([2] * batch_size).to(torch_device)
+        labels = _long_tensor([2] * batch_size).to(DEFAULT_DEVICE)
         model = TFBartForSequenceClassification(config)
-        model.to(torch_device)
-        outputs = model(input_ids=input_ids, decoder_input_ids=input_ids, labels=labels)
-        logits = outputs[1]
+        model.to(DEFAULT_DEVICE)
+        outputs = model(input_ids=input_ids, decoder_input_ids=input_ids)
+        logits = outputs[0]
         expected_shape = (batch_size, config.num_labels)
         self.assertEqual(logits.shape, expected_shape)
-        loss = outputs[0]
-        self.assertIsInstance(loss.item(), float)
 
     def test_lm_forward(self):
         config, input_ids, batch_size = self._get_config_and_data(output_past=False)
-        decoder_lm_labels = ids_tensor([batch_size, input_ids.shape[1]], self.vocab_size).to(torch_device)
+        decoder_lm_labels = ids_tensor([batch_size, input_ids.shape[1]], self.vocab_size).to(DEFAULT_DEVICE)
         lm_model = TFBartForConditionalGeneration(config)
-        lm_model.to(torch_device)
+        lm_model.to(DEFAULT_DEVICE)
         loss, logits, enc_features = lm_model.forward(
             input_ids=input_ids, lm_labels=decoder_lm_labels, decoder_input_ids=input_ids
         )
@@ -254,15 +254,15 @@ class TFBartHeadTests(unittest.TestCase):
             decoder_ffn_dim=32,
             max_position_embeddings=48,
         )
-        lm_model = TFBartForConditionalGeneration(config).to(torch_device)
-        context = tf.Tensor([[71, 82, 18, 33, 46, 91, 2], [68, 34, 26, 58, 30, 2, 1]]).long().to(torch_device)
-        summary = tf.Tensor([[82, 71, 82, 18, 2], [58, 68, 2, 1, 1]]).long().to(torch_device)
+        lm_model = TFBartForConditionalGeneration(config).to(DEFAULT_DEVICE)
+        context = tf.Tensor([[71, 82, 18, 33, 46, 91, 2], [68, 34, 26, 58, 30, 2, 1]]).long().to(DEFAULT_DEVICE)
+        summary = tf.Tensor([[82, 71, 82, 18, 2], [58, 68, 2, 1, 1]]).long().to(DEFAULT_DEVICE)
         loss, logits, enc_features = lm_model.forward(input_ids=context, decoder_input_ids=summary, lm_labels=summary)
         expected_shape = (*summary.shape, config.vocab_size)
         self.assertEqual(logits.shape, expected_shape)
 
     def test_generate_beam_search(self):
-        input_ids = tf.Tensor([[71, 82, 2], [68, 34, 2]]).long().to(torch_device)
+        input_ids = tf.Tensor([[71, 82, 2], [68, 34, 2]]).long().to(DEFAULT_DEVICE)
         config = BartConfig(
             vocab_size=self.vocab_size,
             d_model=24,
@@ -278,7 +278,7 @@ class TFBartHeadTests(unittest.TestCase):
             pad_token_id=1,
             bos_token_id=0,
         )
-        lm_model = TFBartForConditionalGeneration(config).to(torch_device)
+        lm_model = TFBartForConditionalGeneration(config).to(DEFAULT_DEVICE)
         lm_model.eval()
 
         max_length = 5
@@ -289,7 +289,7 @@ class TFBartHeadTests(unittest.TestCase):
         # TODO(SS): uneven length batches, empty inputs
 
     def test_shift_tokens_right(self):
-        input_ids = tf.Tensor([[71, 82, 18, 33, 2, 1, 1], [68, 34, 26, 58, 30, 82, 2]]).long()
+        input_ids =ids_tensor((2,7), vocab_size=99, values=[[71, 82, 18, 33, 2, 1, 1], [68, 34, 26, 58, 30, 82, 2]])
         shifted = shift_tokens_right(input_ids, 1)
         n_pad_before = input_ids.eq(1).float().sum()
         n_pad_after = shifted.eq(1).float().sum()
@@ -309,23 +309,23 @@ class TFBartHeadTests(unittest.TestCase):
             bart_toks = tokenizer.encode(ex, return_tensors="pt")
             _assert_tensors_equal(desired_result.long(), bart_toks, prefix=ex)
 
-    @unittest.skipIf(torch_device == "cpu", "Cant do half precision")
+    @unittest.skipIf(DEFAULT_DEVICE == "cpu", "Cant do half precision")
     def test_generate_fp16(self):
         config, input_ids, batch_size = self._get_config_and_data(output_past=True)
-        attention_mask = input_ids.ne(1).to(torch_device)
-        model = TFBartForConditionalGeneration(config).eval().to(torch_device).half()
+        attention_mask = input_ids.ne(1).to(DEFAULT_DEVICE)
+        model = TFBartForConditionalGeneration(config).eval().to(DEFAULT_DEVICE).half()
         model.generate(input_ids, attention_mask=attention_mask, do_sample=False, early_stopping=True)
 
-    @unittest.skipIf(torch_device == "cpu", "Cant do half precision")
+    @unittest.skipIf(DEFAULT_DEVICE == "cpu", "Cant do half precision")
     def test_base_model_fp16(self):
         config, input_ids, batch_size = self._get_config_and_data(output_past=False)
-        attention_mask = input_ids.ne(1).to(torch_device)
-        lm_model = TFBartForConditionalGeneration(config).eval().to(torch_device).half()
+        attention_mask = input_ids.ne(1).to(DEFAULT_DEVICE)
+        lm_model = TFBartForConditionalGeneration(config).eval().to(DEFAULT_DEVICE).half()
         lm_model(input_ids, attention_mask=attention_mask)
 
     def test_prepare_bart_decoder_inputs(self):
         config, *_ = self._get_config_and_data(output_past=False)
-        input_ids = _long_tensor(([4, 4, 2]))  # only used for .device if decoder_input_ids is passed
+        input_ids = _long_tensor(([4, 4, 2]))  # only used for .DEFAULT_DEVICE if decoder_input_ids is passed
         decoder_input_ids = _long_tensor([[26388, 2, config.pad_token_id]])
         ignore = LARGE_NEGATIVE
         decoder_input_ids, decoder_attn_mask = _prepare_bart_decoder_inputs(config, input_ids, decoder_input_ids)
@@ -372,25 +372,37 @@ def _assert_tensors_equal(a, b, atol=1e-12, prefix=""):
 
 
 def _long_tensor(tok_lst):
-    return tf.Tensor(tok_lst, dtype=torch.long, device=torch_device,)
+    return tf.constant(tok_lst, dtype=tf.int32)
+
+
+#output = tf.constant(values, shape=shape, dtype=dtype if dtype is not None else tf.int32)
 
 
 TOLERANCE = 1e-4
+import numpy as np
+
+def make_tensor(values):
+    arr = np.array(values)
+    shape = arr.shape
+    vocab_size = arr.max() + 1
+
+
+
 
 
 @require_tf
 class TFBartModelIntegrationTest(unittest.TestCase):
     @slow
     def test_inference_no_head(self):
-        model = TFBartModel.from_pretrained("bart-large").to(torch_device)
+        model = TFBartModel.from_pretrained("bart-large").to(DEFAULT_DEVICE)
         input_ids = _long_tensor([[0, 31414, 232, 328, 740, 1140, 12695, 69, 46078, 1588, 2]])
         inputs_dict = prepare_bart_inputs_dict(model.config, input_ids)
         with torch.no_grad():
-            output = model.forward(**inputs_dict)[0]
-        expected_shape = torch.Size((1, 11, 1024))
+            output = model(**inputs_dict)[0]
+        expected_shape = (1, 11, 1024)
         self.assertEqual(output.shape, expected_shape)
         expected_slice = tf.Tensor(
-            [[0.7144, 0.8143, -1.2813], [0.7144, 0.8143, -1.2813], [-0.0467, 2.5911, -2.1845]], device=torch_device
+            [[0.7144, 0.8143, -1.2813], [0.7144, 0.8143, -1.2813], [-0.0467, 2.5911, -2.1845]], device=DEFAULT_DEVICE
         )
         self.assertTrue(torch.allclose(output[:, :3, :3], expected_slice, atol=TOLERANCE))
 
@@ -401,15 +413,15 @@ class TFBartModelIntegrationTest(unittest.TestCase):
         input_ids = _long_tensor([[0, 31414, 232, 328, 740, 1140, 12695, 69, 46078, 1588, 2], example_b])
 
         model = TFAutoModelForSequenceClassification.from_pretrained("bart-large-mnli").to(
-            torch_device
+            DEFAULT_DEVICE
         )  # eval called in from_pre
         inputs_dict = prepare_bart_inputs_dict(model.config, input_ids)
         # Test that model hasn't changed
-        with torch.no_grad():
-            batched_logits, features = model.forward(**inputs_dict)
-        expected_shape = torch.Size((2, 3))
+
+        batched_logits, features = model(**inputs_dict)
+        expected_shape = (2, 3)
         self.assertEqual(batched_logits.shape, expected_shape)
-        expected_slice = tf.Tensor([[0.1907, 1.4342, -1.0289]]).to(torch_device)
+        expected_slice = tf.constant([[0.1907, 1.4342, -1.0289]]).to(DEFAULT_DEVICE)
         logits_arr = batched_logits[0].detach()
 
         # Test that padding does not change results
@@ -430,10 +442,10 @@ class TFBartModelIntegrationTest(unittest.TestCase):
 
     @slow
     def test_cnn_summarization_same_as_fairseq_easy(self):
-        hf = TFBartForConditionalGeneration.from_pretrained("bart-large-cnn", output_past=True,).to(torch_device)
+        hf = TFBartForConditionalGeneration.from_pretrained("bart-large-cnn", output_past=True,).to(DEFAULT_DEVICE)
         tok = BartTokenizer.from_pretrained("bart-large")
         text = " (CNN)The Palestinian Authority officially became the 123rd member of the International Criminal Court on Wednesday, a step that gives the court jurisdiction over alleged crimes in Palestinian"
-        tokens = tok.encode(text, return_tensors="pt").to(torch_device)
+        tokens = tok.encode(text, return_tensors="pt").to(DEFAULT_DEVICE)
         extra_len = 20
         gen_tokens = hf.generate(
             tokens,
@@ -448,7 +460,7 @@ class TFBartModelIntegrationTest(unittest.TestCase):
 
     @slow
     def test_cnn_summarization_same_as_fairseq_hard(self):
-        hf = TFBartForConditionalGeneration.from_pretrained("bart-large-cnn", output_past=True,).to(torch_device)
+        hf = TFBartForConditionalGeneration.from_pretrained("bart-large-cnn", output_past=True,).to(DEFAULT_DEVICE)
         tok = BartTokenizer.from_pretrained("bart-large")
 
         FRANCE_ARTICLE = ' Marseille, France (CNN)The French prosecutor leading an investigation into the crash of Germanwings Flight 9525 insisted Wednesday that he was not aware of any video footage from on board the plane. Marseille prosecutor Brice Robin told CNN that "so far no videos were used in the crash investigation." He added, "A person who has such a video needs to immediately give it to the investigators." Robin\'s comments follow claims by two magazines, German daily Bild and French Paris Match, of a cell phone video showing the harrowing final seconds from on board Germanwings Flight 9525 as it crashed into the French Alps. All 150 on board were killed. Paris Match and Bild reported that the video was recovered from a phone at the wreckage site. The two publications described the supposed video, but did not post it on their websites. The publications said that they watched the video, which was found by a source close to the investigation. "One can hear cries of \'My God\' in several languages," Paris Match reported. "Metallic banging can also be heard more than three times, perhaps of the pilot trying to open the cockpit door with a heavy object.  Towards the end, after a heavy shake, stronger than the others, the screaming intensifies. Then nothing." "It is a very disturbing scene," said Julian Reichelt, editor-in-chief of Bild online. An official with France\'s accident investigation agency, the BEA, said the agency is not aware of any such video. Lt. Col. Jean-Marc Menichini, a French Gendarmerie spokesman in charge of communications on rescue efforts around the Germanwings crash site, told CNN that the reports were "completely wrong" and "unwarranted." Cell phones have been collected at the site, he said, but that they "hadn\'t been exploited yet." Menichini said he believed the cell phones would need to be sent to the Criminal Research Institute in Rosny sous-Bois, near Paris, in order to be analyzed by specialized technicians working hand-in-hand with investigators. But none of the cell phones found so far have been sent to the institute, Menichini said. Asked whether staff involved in the search could have leaked a memory card to the media, Menichini answered with a categorical "no." Reichelt told "Erin Burnett: Outfront" that he had watched the video and stood by the report, saying Bild and Paris Match are "very confident" that the clip is real. He noted that investigators only revealed they\'d recovered cell phones from the crash site after Bild and Paris Match published their reports. "That is something we did not know before. ... Overall we can say many things of the investigation weren\'t revealed by the investigation at the beginning," he said. What was mental state of Germanwings co-pilot? German airline Lufthansa confirmed Tuesday that co-pilot Andreas Lubitz had battled depression years before he took the controls of Germanwings Flight 9525, which he\'s accused of deliberately crashing last week in the French Alps. Lubitz told his Lufthansa flight training school in 2009 that he had a "previous episode of severe depression," the airline said Tuesday. Email correspondence between Lubitz and the school discovered in an internal investigation, Lufthansa said, included medical documents he submitted in connection with resuming his flight training. The announcement indicates that Lufthansa, the parent company of Germanwings, knew of Lubitz\'s battle with depression, allowed him to continue training and ultimately put him in the cockpit. Lufthansa, whose CEO Carsten Spohr previously said Lubitz was 100% fit to fly, described its statement Tuesday as a "swift and seamless clarification" and said it was sharing the information and documents -- including training and medical records -- with public prosecutors. Spohr traveled to the crash site Wednesday, where recovery teams have been working for the past week to recover human remains and plane debris scattered across a steep mountainside. He saw the crisis center set up in Seyne-les-Alpes, laid a wreath in the village of Le Vernet, closer to the crash site, where grieving families have left flowers at a simple stone memorial. Menichini told CNN late Tuesday that no visible human remains were left at the site but recovery teams would keep searching. French President Francois Hollande, speaking Tuesday, said that it should be possible to identify all the victims using DNA analysis by the end of the week, sooner than authorities had previously suggested. In the meantime, the recovery of the victims\' personal belongings will start Wednesday, Menichini said. Among those personal belongings could be more cell phones belonging to the 144 passengers and six crew on board. Check out the latest from our correspondents . The details about Lubitz\'s correspondence with the flight school during his training were among several developments as investigators continued to delve into what caused the crash and Lubitz\'s possible motive for downing the jet. A Lufthansa spokesperson told CNN on Tuesday that Lubitz had a valid medical certificate, had passed all his examinations and "held all the licenses required." Earlier, a spokesman for the prosecutor\'s office in Dusseldorf, Christoph Kumpa, said medical records reveal Lubitz suffered from suicidal tendencies at some point before his aviation career and underwent psychotherapy before he got his pilot\'s license. Kumpa emphasized there\'s no evidence suggesting Lubitz was suicidal or acting aggressively before the crash. Investigators are looking into whether Lubitz feared his medical condition would cause him to lose his pilot\'s license, a European government official briefed on the investigation told CNN on Tuesday. While flying was "a big part of his life," the source said, it\'s only one theory being considered. Another source, a law enforcement official briefed on the investigation, also told CNN that authorities believe the primary motive for Lubitz to bring down the plane was that he feared he would not be allowed to fly because of his medical problems. Lubitz\'s girlfriend told investigators he had seen an eye doctor and a neuropsychologist, both of whom deemed him unfit to work recently and concluded he had psychological issues, the European government official said. But no matter what details emerge about his previous mental health struggles, there\'s more to the story, said Brian Russell, a forensic psychologist. "Psychology can explain why somebody would turn rage inward on themselves about the fact that maybe they weren\'t going to keep doing their job and they\'re upset about that and so they\'re suicidal," he said. "But there is no mental illness that explains why somebody then feels entitled to also take that rage and turn it outward on 149 other people who had nothing to do with the person\'s problems." Germanwings crash compensation: What we know . Who was the captain of Germanwings Flight 9525? CNN\'s Margot Haddad reported from Marseille and Pamela Brown from Dusseldorf, while Laura Smith-Spark wrote from London. CNN\'s Frederik Pleitgen, Pamela Boykoff, Antonia Mortensen, Sandrine Amiel and Anna-Maja Rappard contributed to this report.'  # @noqa
@@ -476,8 +488,8 @@ class TFBartModelIntegrationTest(unittest.TestCase):
 
         self.assertEqual(1024, dct["input_ids"].shape[1])
         hypotheses_batch = hf.generate(
-            input_ids=dct["input_ids"].to(torch_device),
-            attention_mask=dct["attention_mask"].to(torch_device),
+            input_ids=dct["input_ids"].to(DEFAULT_DEVICE),
+            attention_mask=dct["attention_mask"].to(DEFAULT_DEVICE),
             num_beams=4,
             length_penalty=2.0,
             max_length=max_length + 2,
