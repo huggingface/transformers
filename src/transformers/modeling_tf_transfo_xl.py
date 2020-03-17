f2cc11a46556b693823431f353dadcffa93c4e9f
Patrick von Platen <patrick.v.platen@gmail.com>
2020-03-17 16:25:59 +0100

    make style

--- src/transformers/modeling_tf_transfo_xl.py
+++ src/transformers/modeling_tf_transfo_xl.py
@@ -762,7 +762,9 @@ class TFTransfoXLLMHeadModel(TFTransfoXLPreTrainedModel):
         super().__init__(config)
         self.transformer = TFTransfoXLMainLayer(config, name="transformer")
         self.sample_softmax = config.sample_softmax
-        assert self.sample_softmax <= 0, "Sampling from the softmax is not implemented yet. Please look at issue: #3310: https://github.com/huggingface/transformers/issues/3310"
+        assert (
+            self.sample_softmax <= 0
+        ), "Sampling from the softmax is not implemented yet. Please look at issue: #3310: https://github.com/huggingface/transformers/issues/3310"
 
         self.crit = TFAdaptiveSoftmaxMask(
             config.vocab_size, config.d_embed, config.d_model, config.cutoffs, div_val=config.div_val, name="crit"
--- src/transformers/modeling_transfo_xl.py
+++ src/transformers/modeling_transfo_xl.py
@@ -810,7 +810,9 @@ class TransfoXLLMHeadModel(TransfoXLPreTrainedModel):
         self.transformer = TransfoXLModel(config)
         self.sample_softmax = config.sample_softmax
 
-        assert self.sample_softmax <= 0, "Sampling from the softmax is not implemented yet. Please look at issue: #3310: https://github.com/huggingface/transformers/issues/3310"
+        assert (
+            self.sample_softmax <= 0
+        ), "Sampling from the softmax is not implemented yet. Please look at issue: #3310: https://github.com/huggingface/transformers/issues/3310"
 
         self.crit = ProjectedAdaptiveLogSoftmax(
             config.vocab_size, config.d_embed, config.d_model, config.cutoffs, div_val=config.div_val
