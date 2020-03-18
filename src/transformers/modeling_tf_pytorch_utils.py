a48581447e498b36ba4b32135c116754335c2fdd
Patrick von Platen <patrick.v.platen@gmail.com>
2020-03-18 15:40:25 +0100

    remove ipdbs

--- src/transformers/modeling_tf_pytorch_utils.py
+++ src/transformers/modeling_tf_pytorch_utils.py
@@ -160,9 +160,7 @@ def load_pytorch_weights_in_tf2_model(tf_model, pt_state_dict, tf_inputs=None, a
         if name not in pt_state_dict:
             if allow_missing_keys:
                 continue
-            import ipdb
 
-            ipdb.set_trace()
             raise AttributeError("{} not found in PyTorch model".format(name))
 
         array = pt_state_dict[name].numpy()
@@ -291,9 +289,7 @@ def load_tf2_weights_in_pytorch_model(pt_model, tf_weights, allow_missing_keys=F
             if allow_missing_keys:
                 missing_keys_pt.append(pt_weight_name)
                 continue
-            import ipdb
 
-            ipdb.set_trace()
             raise AttributeError("{} not found in TF 2.0 model".format(pt_weight_name))
 
         array, transpose = tf_weights_map[pt_weight_name]
