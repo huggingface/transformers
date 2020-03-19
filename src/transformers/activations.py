0a6f022fe0bcd4215e8265588faab1638e68b74f
sshleifer <sshleifer@gmail.com>
2020-03-19 11:55:23 -0400

    make style

--- src/transformers/activations.py
+++ src/transformers/activations.py
@@ -44,8 +44,4 @@ def get_activation(activation_string):
     if activation_string in ACT2FN:
         return ACT2FN[activation_string]
     else:
-        raise KeyError(
-            "function {} not found in ACT2FN mapping {}".format(
-                activation_string, list(ACT2FN.keys())
-            )
-        )
+        raise KeyError("function {} not found in ACT2FN mapping {}".format(activation_string, list(ACT2FN.keys())))
