b628c1e900a01c017eebf8b729f2f9233eaad0f4
sshleifer <sshleifer@gmail.com>
2020-03-19 11:45:18 -0400

    fix err msg

--- src/transformers/activations.py
+++ src/transformers/activations.py
@@ -45,7 +45,7 @@ def get_activation(activation_string):
         return ACT2FN[activation_string]
     else:
         raise KeyError(
-            "function {} not found in ACT2FN mapping {} or torch.nn.functional".format(
+            "function {} not found in ACT2FN mapping {}".format(
                 activation_string, list(ACT2FN.keys())
             )
         )
