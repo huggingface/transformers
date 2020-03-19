77a2a4c5cc11811c33dc8f7890f6b13042f6ad71
LysandreJik <lysandre.debut@reseau.eseo.fr>
2020-03-19 13:11:10 -0400

    Export ALBERT main layer in TensorFlow

--- src/transformers/__init__.py
+++ src/transformers/__init__.py
@@ -434,6 +434,7 @@ if is_tf_available():
 
     from .modeling_tf_albert import (
         TFAlbertPreTrainedModel,
+        TFAlbertMainLayer,
         TFAlbertModel,
         TFAlbertForMaskedLM,
         TFAlbertForSequenceClassification,
