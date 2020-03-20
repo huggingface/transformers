00744b47fd31b0e54704b349fc13291eb6c93d48
Patrick von Platen <patrick.v.platen@gmail.com>
2020-03-20 21:41:04 +0100

    Clean special token init in modeling_....py (#3264)
    * make style
    
    * fix conflicts

--- tests/test_modeling_gpt2.py
+++ tests/test_modeling_gpt2.py
@@ -132,7 +132,7 @@ class GPT2ModelTest(ModelTesterMixin, unittest.TestCase):
                 # type_vocab_size=self.type_vocab_size,
                 # initializer_range=self.initializer_range
                 bos_token_id=self.bos_token_id,
-                eos_token_ids=self.eos_token_id,
+                eos_token_id=self.eos_token_id,
             )
 
             head_mask = ids_tensor([self.num_hidden_layers, self.num_attention_heads], 2)
