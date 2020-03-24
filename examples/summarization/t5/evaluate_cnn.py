aeb5483bc0285c14d7be7c0dd736e10c18b9749b
Patrick von Platen <patrick.v.platen@gmail.com>
2020-03-24 10:54:28 +0100

    add t5 summarization example

---
+++ examples/summarization/t5/evaluate_cnn.py
@@ -0,0 +1,58 @@
+import argparse
+from pathlib import Path
+
+from tqdm import tqdm
+
+from transformers import TFT5ForConditionalGeneration, T5Tokenizer
+
+
+def chunks(lst, n):
+    """Yield successive n-sized chunks from lst."""
+    for i in range(0, len(lst), n):
+        yield lst[i : i + n]
+
+
+def generate_summaries(lns, out_file, batch_size=8):
+    fout = Path(out_file).open("w")
+    model = TFT5ForConditionalGeneration.from_pretrained("t5-large")
+    tokenizer = T5Tokenizer.from_pretrained("t5-large")
+
+    max_length = 140
+    min_length = 55
+
+    for batch in tqdm(list(chunks(lns, batch_size))):
+        dct = tokenizer.batch_encode_plus(batch, max_length=1024, return_tensors="tf", pad_to_max_length=True)
+        summaries = model.generate(
+            input_ids=dct["input_ids"],
+            attention_mask=dct["attention_mask"],
+            num_beams=4,
+            length_penalty=0.6,
+            max_length=max_length,
+            min_length=min_length,
+            no_repeat_ngram_size=3,
+            early_stopping=True,
+        )
+        dec = [tokenizer.decode(g, skip_special_tokens=True, clean_up_tokenization_spaces=False) for g in summaries]
+        for hypothesis in dec:
+            fout.write(hypothesis + "\n")
+            fout.flush()
+
+
+def _run_generate():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "source_path", type=str, help="like cnn_dm/test.source",
+    )
+    parser.add_argument(
+        "output_path", type=str, help="where to save summaries",
+    )
+    parser.add_argument(
+        "--bs", type=int, default=8, required=False, help="batch size: how many to summarize at a time",
+    )
+    args = parser.parse_args()
+    lns = [" " + x.rstrip() for x in open(args.source_path).readlines()]
+    generate_summaries(lns, args.output_path, batch_size=args.bs, device=args.device)
+
+
+if __name__ == "__main__":
+    _run_generate()
