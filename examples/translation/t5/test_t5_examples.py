cd22ac2ba8170fc0ef7403049b2e79c6bd476b22
Patrick von Platen <patrick.v.platen@gmail.com>
2020-03-27 16:24:58 +0100

    force bleu

--- examples/translation/t5/test_t5_examples.py
+++ examples/translation/t5/test_t5_examples.py
@@ -8,7 +8,8 @@ from unittest.mock import patch
 from .evaluate_wmt import run_generate
 
 
-text = [" New York (CNN)When Liana Barrientos was 23 years old, she got married in Westchester County."]
+text = [" When Liana Barrientos was 23 years old, she got married in Westchester County."]
+translation = [" Als Liana Barrientos 23 Jahre alt war, heiratete sie in Westchester County."]
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -19,10 +20,17 @@ class TestT5Examples(unittest.TestCase):
     def test_t5_cli(self):
         stream_handler = logging.StreamHandler(sys.stdout)
         logger.addHandler(stream_handler)
-        tmp = Path(tempfile.gettempdir()) / "utest_generations.hypo"
-        with tmp.open("w") as f:
+
+        tmp_source = Path(tempfile.gettempdir()) / "utest_generations.hypo"
+        with tmp_source.open("w") as f:
             f.write("\n".join(text))
-        testargs = ["evaluate_cnn.py", str(tmp), "output.txt", str(tmp), "score.txt"]
+
+        tmp_target = Path(tempfile.gettempdir()) / "utest_generations.hypo"
+        with tmp_target.open("w") as f:
+            f.write("\n".join(text))
+
+        testargs = ["evaluate_cnn.py", str(tmp_source), "output.txt", str(tmp_target), "score.txt"]
+
         with patch.object(sys, "argv", testargs):
             run_generate()
             self.assertTrue(Path("output.txt").exists())
