9bc4b5803189dce07a989fcf59dd8a8dbc98404e
Morgan Funtowicz <funtowiczmo@gmail.com>
2020-03-28 20:58:26 +0100

    BertEncoder on par with PyTorch ! :D

--- src/transformers/modeling_flax_bert.py
+++ src/transformers/modeling_flax_bert.py
@@ -7,7 +7,6 @@ import torch
 from dataclasses import dataclass
 from typing import Tuple, Callable
 
-from flax.nn import Model
 from flax.serialization import from_state_dict
 from flax.traverse_util import unflatten_dict
 from jax.random import PRNGKey
@@ -23,11 +22,31 @@ ACT2FN = {
 }
 
 
+def gelu(x):
+    r"""Gaussian error linear unit activation function.
+
+    Computes the element-wise function:
+
+    .. math::
+      \mathrm{gelu}(x) = \frac{x}{2} \left(1 + \mathrm{tanh} \left(
+        \sqrt{\frac{2}{\pi}} \left(x + 0.044715 x^3 \right) \right) \right)
+
+    We explicitly use the approximation rather than the exact formulation for
+    speed. For more information, see `Gaussian Error Linear Units (GELUs)
+    <https://arxiv.org/abs/1606.08415>`_, section 2.
+    """
+    return x * 0.5 * (1. + jax.lax.erf(x / jnp.sqrt(2.)))
+
+
 @dataclass
 class BertConfig:
     max_length: int
     vocab_size: int
     hidden_size: int
+    num_layers: int
+    num_heads: int
+    head_size: int
+    intermediate_size: int
     initializer_range: Tuple[int, int] = (-1, 1)
 
 
@@ -107,19 +126,94 @@ class BertEmbeddings(nn.Module):
         return norm
 
 
+class BertAttention(nn.Module):
+
+    def apply(self, hidden_state, attention_mask, num_heads: int, head_size: int, output_size: int):
+        self_att = nn.attention.SelfAttention(
+            hidden_state,
+            num_heads=num_heads,
+            qkv_features=head_size,
+            name="self"
+        )
+
+        return BertLayerNorm(self_att + hidden_state, name="layer_norm")
+
+
+class BertIntermediate(nn.Module):
+
+    def apply(self, hidden_state, output_size: int):
+        # TODO: Had ACT2FN reference to change activation function
+        h = nn.Dense(hidden_state, features=output_size, name="dense")
+        return gelu(h)
+
+
+class BertOutput(nn.Module):
+    def apply(self, intermediate_output, attention_output):
+        h = nn.Dense(intermediate_output, attention_output.shape[-1], name="dense")
+        h = BertLayerNorm(h + attention_output, name="LayerNorm")
+
+        return h
+
+
+class BertLayer(nn.Module):
+
+    def apply(self, hidden_state, attention_mask, num_heads: int, head_size: int, intermediate_size: int):
+        attention = BertAttention(hidden_state, attention_mask, num_heads, head_size, intermediate_size, name="attention")
+        intermediate = BertIntermediate(attention, intermediate_size, name="intermediate")
+        output = BertOutput(intermediate, attention, name="output")
+
+        return output
+
+
+class BertLayerCollection(nn.Module):
+    """
+    Stores N BertLayer(s)
+    """
+    def apply(self, input, attention_mask, num_layers: int, num_heads: int, head_size: int, intermediate_size: int):
+        assert num_layers > 0, "num_layers should be >= 1, got ({})".format(num_layers)
+
+        # Initialize input / output
+        input_i = output_i = input
+
+        # Forward over all encoders
+        for i in range(num_layers):
+            output_i = BertLayer(input_i, attention_mask, num_heads, head_size, intermediate_size, name="{}".format(i))
+            input_i = output_i
+        # output_i = BertLayer(input_i, attention_mask, num_heads, head_size, intermediate_size, name="0")
+        return output_i
+
+
+class BertEncoder(nn.Module):
+
+    def apply(self, hidden_state, attention_mask, num_layers: int, num_heads: int, head_size: int, intermediate_size: int):
+        encodings = BertLayerCollection(hidden_state, attention_mask, num_layers, num_heads, head_size, intermediate_size, name="layer")
+        return encodings
+
+
 class BertModel(nn.Module):
 
     def apply(self, input_ids, token_type_ids, attention_mask,
-              vocab_size: int, hidden_size: int, type_vocab_size: int, max_length: int,
+              vocab_size: int, hidden_size: int, type_vocab_size: int,
+              max_length: int, num_encoder_layers: int, num_heads: int,
+              head_size: int, intermediate_size: int,
               emb_init: Callable[..., np.ndarray] = nn.initializers.normal(stddev=0.1)):
 
+        # Embedding
         embeddings = BertEmbeddings(
             input_ids, token_type_ids, attention_mask,
             vocab_size, hidden_size, type_vocab_size, max_length,
             name="embeddings"
         )
 
-        return embeddings
+        # N stacked encoding layers
+        encoder = BertEncoder(
+            embeddings, attention_mask, num_encoder_layers,
+            num_heads, head_size, intermediate_size,
+            name="encoder"
+        )
+
+        # TODO: Pooling
+        return encoder
 
 
 class FXBertModel:
@@ -138,13 +232,15 @@ class FXBertModel:
             hidden_size=self.config.hidden_size,
             type_vocab_size=2,
             max_length=self.config.max_length,
+            num_encoder_layers=self.config.num_layers,
+            num_heads=self.config.num_heads,
+            head_size=self.config.head_size,
+            intermediate_size=self.config.intermediate_size
         )
 
-        # inputs_shape = [(1, len(input_ids))] * 3
-        # _ = model_def.init_by_shape(self.key, inputs_shape)
-        bert = Model(model_def, self.state)
+        bert = nn.Model(model_def, self.state)
 
-        @jax.jit
+        # @jax.jit
         def predict(input_ids, token_type_ids, attention_mask):
             return bert(
                 jnp.array(input_ids, dtype='i4'),
@@ -161,15 +257,41 @@ class FXBertModel:
 
 if __name__ == '__main__':
     tokenizer = BertTokenizerFast.from_pretrained("bert-base-cased")
-    model = FXBertModel(BertConfig(512, 28996, 768))
-    model_pt = PTBertModel.from_pretrained('bert-base-cased')
+    model = FXBertModel(BertConfig(512, 28996, 768, 12, 12, 768, 3072))
+    model_pt = PTBertModel.from_pretrained('bert-base-cased', output_attentions=True)
     model_pt.eval()
 
     with open("/data/Downloads/bert-base-cased-pytorch_model.bin", 'rb') as model_f:
         state = torch.load(model_f)
-        state = unflatten_dict({tuple(k.split('.')[1:]): v.numpy() for k, v in state.items()})
+        state = {k: v.numpy() for k, v in state.items()}
+
+        # Need to change some parameters name to match Flax names so that we don't have to fork any layer
+        state = {k.replace("weight", "kernel") if {"dense", "query", "key", "value"} & set(k.split('.')) else k: v
+                 for k, v in state.items()}
+
+        # SelfAttention output is not a separate layer, remove one nesting
+        state = {k.replace("attention.output.dense", "attention.self.out"): v for k, v in state.items()}
+        state = {k.replace("attention.output.LayerNorm", "attention.layer_norm"): v for k, v in state.items()}
+
+        # SelfAttention.out
+        state = {k: v.T if v.shape == (3072, 768) or v.shape == (768, 3072) else v for k, v in state.items()}
+        state = {k: v.reshape((12, 64, 768)).transpose((2, 0, 1)) if v.shape == (768, 768) and "out" not in k else v for k, v in state.items()}
+
+        # Bias
+        state = {k: v.reshape((12, -1)) if v.shape == (768, ) and ("bias" in k) and ("out" not in k) else v for k, v in state.items()}
+
+        # Self Attention output projection
+        state = {k: v.reshape((768, 12, 64)).transpose(1, 2, 0) if "out.kernel" in k else v for k, v in state.items()}
+        state = unflatten_dict({tuple(k.split('.')[1:]): v for k, v in state.items()})
         model.from_pretrained(state)
 
-    out = model(**tokenizer.encode_plus("My name is Morgan"))
-    out_pt = model_pt.embeddings(tokenizer.encode_plus("My name is Morgan", return_tensors="pt")['input_ids'])
+    # Inputs
+    flax_input = tokenizer.encode_plus("My name is Morgan")
+    pt_input = tokenizer.encode_plus("My name is Morgan", return_tensors="pt")
+
+    # Forward
+    model_pt.eval()
+    pt_emb = model_pt.embeddings(pt_input['input_ids'])
+    pt_enc = model_pt.encoder(pt_emb, pt_input['attention_mask'], [None] * 12)
+    flax_enc = model(**flax_input)
     input()
