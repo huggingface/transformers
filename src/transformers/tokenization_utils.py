b06ed1bdb7e393c2264511c8870d68cc59732663
Patrick von Platen <patrick.v.platen@gmail.com>
2020-03-29 18:49:07 +0200

    fix egde gase for bert tokenization

--- src/transformers/tokenization_utils.py
+++ src/transformers/tokenization_utils.py
@@ -1396,7 +1396,13 @@ class PreTrainedTokenizer(SpecialTokensMixin):
 
         input_ids = []
         for ids_or_pair_ids in batch_text_or_text_pairs:
-            if isinstance(ids_or_pair_ids, (list, tuple)) and len(ids_or_pair_ids) == 2:
+            is_list_of_untokenized_strings = (
+                isinstance(ids_or_pair_ids, (list, tuple))
+                and len(ids_or_pair_ids) > 0
+                and not all([isinstance(x, str) and x == self.tokenize(x)[0] for x in ids_or_pair_ids])
+            )
+
+            if is_list_of_untokenized_strings and len(ids_or_pair_ids) == 2:
                 ids, pair_ids = ids_or_pair_ids
             else:
                 ids, pair_ids = ids_or_pair_ids, None
