4176564c785004fa69d4583a2108715c8dcf1818
Patrick von Platen <patrick.v.platen@gmail.com>
2020-04-02 18:56:24 +0200

    refactor + add reformer config

---
+++ src/transformers/configuration_reformer.py
@@ -0,0 +1,148 @@
+# coding=utf-8
+# Copyright 2018 The Google AI Language Team Authors and The HuggingFace Inc. team.
+# Copyright (c) 2018, NVIDIA CORPORATION.  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+""" Reformer model configuration """
+
+
+import logging
+
+from .configuration_utils import PretrainedConfig
+
+
+logger = logging.getLogger(__name__)
+
+REFORMER_PRETRAINED_CONFIG_ARCHIVE_MAP = {}
+
+
+class ReformerConfig(PretrainedConfig):
+    r"""
+        This is the configuration class to store the configuration of a :class:`~transformers.ReformerModel`.
+        It is used to instantiate an Reformer model according to the specified arguments, defining the model
+        architecture. Instantiating a configuration with the defaults will yield a similar configuration to that of
+        the Reformer `bert-base-uncased <https://huggingface.co/bert-base-uncased>`__ architecture.
+
+        Configuration objects inherit from  :class:`~transformers.PretrainedConfig` and can be used
+        to control the model outputs. Read the documentation from  :class:`~transformers.PretrainedConfig`
+        for more information.
+
+
+        Args:
+            vocab_size (:obj:`int`, optional, defaults to 30522):
+                Vocabulary size of the Reformer model. Defines the different tokens that
+                can be represented by the `inputs_ids` passed to the forward method of :class:`~transformers.ReformerModel`.
+            hidden_size (:obj:`int`, optional, defaults to 768):
+                Dimensionality of the encoder layers and the pooler layer.
+            num_hidden_layers (:obj:`int`, optional, defaults to 12):
+                Number of hidden layers in the Transformer encoder.
+            num_attention_heads (:obj:`int`, optional, defaults to 12):
+                Number of attention heads for each attention layer in the Transformer encoder.
+            num_buckets (:obj:`int`, optional, defaults to ):
+                TODO (PVP)
+            num_hashes (:obj:`int`, optional, defaults to ):
+                TODO (PVP)
+            chunk_length (:obj:`int`, optional, defaults to ):
+                TODO (PVP)
+            num_chunks_before (:obj:`int`, optional, defaults to ):
+                TODO (PVP)
+            num_chunks_after (:obj:`int`, optional, defaults to ):
+                TODO (PVP)
+            intermediate_size (:obj:`int`, optional, defaults to 3072):
+                Dimensionality of the "intermediate" (i.e., feed-forward) layer in the Transformer encoder.
+            hidden_act (:obj:`str` or :obj:`function`, optional, defaults to "gelu"):
+                The non-linear activation function (function or string) in the encoder and pooler.
+                If string, "gelu", "relu", "swish" and "gelu_new" are supported.
+            hidden_dropout_prob (:obj:`float`, optional, defaults to 0.1):
+                The dropout probabilitiy for all fully connected layers in the embeddings, encoder, and pooler.
+            attention_probs_dropout_prob (:obj:`float`, optional, defaults to 0.1):
+                The dropout ratio for the attention probabilities.
+            max_position_embeddings (:obj:`int`, optional, defaults to 512):
+                The maximum sequence length that this model might ever be used with.
+                Typically set this to something large just in case (e.g., 512 or 1024 or 2048).
+            type_vocab_size (:obj:`int`, optional, defaults to 2):
+                The vocabulary size of the `token_type_ids` passed into :class:`~transformers.ReformerModel`.
+            initializer_range (:obj:`float`, optional, defaults to 0.02):
+                The standard deviation of the truncated_normal_initializer for initializing all weight matrices.
+            layer_norm_eps (:obj:`float`, optional, defaults to 1e-12):
+                The epsilon used by the layer normalization layers.
+
+        Example::
+
+            from transformers import ReformerModel, ReformerConfig
+
+            # Initializing a Reformer bert-base-uncased style configuration
+            configuration = ReformerConfig()
+
+            # Initializing a model from the bert-base-uncased style configuration
+            model = ReformerModel(configuration)
+
+            # Accessing the model configuration
+            configuration = model.config
+
+        Attributes:
+            pretrained_config_archive_map (Dict[str, str]):
+                A dictionary containing all the available pre-trained checkpoints.
+    """
+    pretrained_config_archive_map = REFORMER_PRETRAINED_CONFIG_ARCHIVE_MAP
+    model_type = "bert"
+
+    def __init__(
+        self,
+        vocab_size=30522,
+        hidden_size=64,
+        num_hidden_layers=12,
+        num_attention_heads=2,
+        num_buckets=4,
+        num_hashes=2,
+        chunk_length=7,
+        num_chunks_before=1,
+        num_chunks_after=0,
+        intermediate_size=3072,
+        hidden_act="gelu",
+        hidden_dropout_prob=0.1,
+        attention_probs_dropout_prob=0.0,
+        max_position_embeddings=512,
+        type_vocab_size=2,
+        initializer_range=0.02,
+        layer_norm_eps=1e-12,
+        pad_token_id=0,
+        **kwargs
+    ):
+        super().__init__(pad_token_id=pad_token_id, **kwargs)
+
+#        TODO: not added yet
+#        predict_mem_len=2048
+#        predict_drop_len=256
+#        n_parallel_heads=1
+
+#        TO DELETE LATER:
+        self.seed = 0
+
+        self.vocab_size = vocab_size
+        self.hidden_size = hidden_size
+        self.num_hidden_layers = num_hidden_layers
+        self.num_attention_heads = num_attention_heads
+        self.num_hashes = num_hashes
+        self.num_buckets = num_buckets
+        self.chunk_length = chunk_length
+        self.num_chunks_after = num_chunks_after
+        self.num_chunks_before = num_chunks_before
+        self.hidden_act = hidden_act
+        self.intermediate_size = intermediate_size
+        self.hidden_dropout_prob = hidden_dropout_prob
+        self.attention_probs_dropout_prob = attention_probs_dropout_prob
+        self.max_position_embeddings = max_position_embeddings
+        self.type_vocab_size = type_vocab_size
+        self.initializer_range = initializer_range
+        self.layer_norm_eps = layer_norm_eps
--- tests/test_modeling_reformer.py
+++ tests/test_modeling_reformer.py
@@ -23,7 +23,7 @@ import jax
 from trax.layers.research.efficient_attention_v2 import (
     LSHSelfAttention as TraxLSHSelfAttention,
 )
-from transformers import LSHSelfAttention
+from transformers import LSHSelfAttention, ReformerConfig
 
 
 from transformers import is_torch_available  # noqa: F401
@@ -53,7 +53,7 @@ class TraxUtils(object):
              error message "<package> missing"
     """
 
-    def __init__(self, shape=(3, 32, 8)):
+    def __init__(self, shape):
         self._shape = shape
 
     def convert_to_jax_array(self, np_array):
@@ -68,39 +68,29 @@ class TraxUtils(object):
 
     def get_layer(
         self,
-        shape=None,
-        num_attention_heads=None,
-        hidden_size=None,
-        query_key_chunk_len=None,
-        num_chunks_before=None,
-        num_chunks_after=None,
-        num_hashes=None,
-        num_buckets=None,
+        config,
         use_reference_code=True,
-        attention_dropout=0.0,
         mode="train",
-        seed=None,
-        path_to_save_weights=None,
+        path_to_save_weights="/home/patrick/hugging_face/experiments/reformer/intermediate_weights",
         **kwargs
     ):
 
         with trax_math.use_backend("jax"):
-            if shape is None:
-                shape = self._shape
+            hidden_size_per_head = config.hidden_size // config.num_attention_heads
             layer = TraxLSHSelfAttention(
-                n_heads=num_attention_heads,
-                d_qk=hidden_size,
-                d_v=hidden_size,
+                n_heads=config.num_attention_heads,
+                d_qk=hidden_size_per_head,
+                d_v=hidden_size_per_head,
+                chunk_len=config.chunk_length,
+                n_chunks_before=config.num_chunks_before,
+                n_chunks_after=config.num_chunks_after,
+                n_hashes=config.num_hashes,
+                n_buckets=config.num_buckets,
+                attention_dropout=config.attention_probs_dropout_prob,
+                hash_seed=config.seed,
                 causal=False,
-                chunk_len=query_key_chunk_len,
-                n_chunks_before=num_chunks_before,
-                n_chunks_after=num_chunks_after,
-                n_hashes=num_hashes,
-                n_buckets=num_buckets,
                 use_reference_code=use_reference_code,
-                attention_dropout=attention_dropout,
                 mode=mode,
-                hash_seed=seed,
                 path_to_save_weights=path_to_save_weights
             )
 
@@ -109,15 +99,12 @@ class TraxUtils(object):
     def forward_layer(
         self,
         np_input_data,
-        layer=None,
+        layer,
         input_signature=None,
         random_number_generator=None,
     ):
         with trax_math.use_backend("jax"):
             input_data = self.convert_to_jax_array(np_input_data)
-
-            if layer is None:
-                layer = self.get_layer()
 
             if input_signature is None:
                 input_signature = self.get_input_signature()
@@ -137,70 +124,33 @@ class TraxUtils(object):
 @require_torch
 class ReformerIntegrationTests(unittest.TestCase):
 
-    hidden_size = 32
-    seq_len = 7
-
-    def _get_random_input(self, shape):
-        return np.random.rand(*shape)
-
-    def _get_trax_utils(self, shape):
-        return TraxUtils(shape)
-
-    def _create_config(
-        self,
-        input_size=8,
-        num_attention_heads=2,
-        num_hashes=2,
-        num_buckets=4,
-        num_chunks_before=1,
-        num_chunks_after=0,
-        seed=0,
-        path_to_save_weights="/home/patrick/hugging_face/experiments/reformer/intermediate_weights"
-    ):
-        return {
-            "input_size": self.hidden_size,
-            "num_attention_heads": num_attention_heads,
-            "hidden_size": self.hidden_size,
-            "hf_hidden_size": num_attention_heads * self.hidden_size,
-            "num_hashes": num_hashes,
-            "num_buckets": num_buckets,
-            "query_key_chunk_len": self.seq_len,
-            "num_chunks_before": num_chunks_before,
-            "num_chunks_after": num_chunks_after,
-            "seed": seed,
-            "path_to_save_weights": path_to_save_weights
-        }
-
-    def test_lsh_hashing(self):
-        shape = (3, self.seq_len, self.hidden_size)  # Batch x SeqLen x ModelDim
-
-        config_dict = self._create_config()
-
-        np_input = self._get_random_input(shape)
-
-        trax_utils = self._get_trax_utils(shape)
-
-        trax_layer = trax_utils.get_layer(**config_dict)
-        trax_output, trax_weights, trax_state = trax_utils.forward_layer(
-            np_input, layer=trax_layer
-        )  # noqa: F841
-        trax_torch_output = torch.tensor(np.asarray(trax_output))
-
-        hf_layer = LSHSelfAttention(config_dict)
-
+    def _set_weights_in_torch(self, weights, torch_layer, hidden_size_per_head):
         # set torch weights for 1-to-1 comparison
         with torch.no_grad():
-            np_query_key = np.asarray(trax_weights[0])
-            np_value = np.asarray(trax_weights[1])
-            np_dense = np.asarray(trax_weights[2])
+            np_query_key = np.asarray(weights[0])
+            np_value = np.asarray(weights[1])
+            np_dense = np.asarray(weights[2])
 
-            hf_layer.query_key.weight = torch.nn.Parameter(torch.tensor(np_query_key).transpose(1, 2).contiguous().view(-1, self.hidden_size))
-            hf_layer.value.weight = torch.nn.Parameter(torch.tensor(np_value).transpose(1, 2).contiguous().view(-1, self.hidden_size))
-            hf_layer.dense.weight = torch.nn.Parameter(torch.tensor(np_dense).view(-1, self.hidden_size).contiguous().transpose(0, 1))
+            torch_layer.query_key.weight = torch.nn.Parameter(torch.tensor(np_query_key).transpose(1, 2).contiguous().view(-1, hidden_size_per_head))
+            torch_layer.value.weight = torch.nn.Parameter(torch.tensor(np_value).transpose(1, 2).contiguous().view(-1, hidden_size_per_head))
+            torch_layer.dense.weight = torch.nn.Parameter(torch.tensor(np_dense).view(-1, hidden_size_per_head).contiguous().transpose(0, 1))
+
+    def test_lsh_hashing(self):
+        config = ReformerConfig()
+
+        hidden_size_per_head = config.hidden_size // config.num_attention_heads
+
+        shape = (3, 7, hidden_size_per_head)  # Batch x SeqLen x ModelDimPerHead
+        np_input = np.random.rand(*shape)
+
+        trax_utils = TraxUtils(shape)
+        trax_layer = trax_utils.get_layer(config)
+        trax_output, trax_weights, trax_state = trax_utils.forward_layer(np_input, layer=trax_layer)
+        trax_torch_output = torch.tensor(np.asarray(trax_output))
 
         hf_input = torch.tensor(np_input, dtype=torch.float)
+        hf_layer = LSHSelfAttention(config)
+        self._set_weights_in_torch(trax_weights, hf_layer, hidden_size_per_head)
         hf_output = hf_layer(hf_input)
 
-        assert torch.allclose(hf_output, trax_torch_output, atol=1e-6)
-
-        pass
+        self.assertTrue(torch.allclose(hf_output, trax_torch_output, atol=1e-6))
