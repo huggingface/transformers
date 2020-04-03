748bc3433c72117744a636b798091aeddde8098a
sshleifer <sshleifer@gmail.com>
2020-04-03 03:03:49 -0400

    done

--- src/transformers/tokenization_bart.py
+++ src/transformers/tokenization_bart.py
@@ -15,6 +15,8 @@
 
 import logging
 
+import torch
+
 from .tokenization_roberta import RobertaTokenizer
 from .tokenization_xlm_roberta import XLMRobertaTokenizer
 
@@ -47,3 +49,50 @@ class MBartTokenizer(XLMRobertaTokenizer):
     vocab_files_names = VOCAB_FILES_NAMES
     max_model_input_sizes = {m: 1024 for m in _all_mbart_models}
     pretrained_vocab_files_map = {"vocab_file": {m: SPM_URL for m in _all_mbart_models}}
+    lang_code_to_id = {
+        "ar_AR": 250001,
+        "cs_CZ": 250002,
+        "de_DE": 250003,
+        "en_XX": 250004,
+        "es_XX": 250005,
+        "et_EE": 250006,
+        "fi_FI": 250007,
+        "fr_XX": 250008,
+        "gu_IN": 250009,
+        "hi_IN": 250010,
+        "it_IT": 250011,
+        "ja_XX": 250012,
+        "kk_KZ": 250013,
+        "ko_KR": 250014,
+        "lt_LT": 250015,
+        "lv_LV": 250016,
+        "my_MM": 250017,
+        "ne_NP": 250018,
+        "nl_XX": 250019,
+        "ro_RO": 250020,
+        "ru_RU": 250021,
+        "si_LK": 250022,
+        "tr_TR": 250023,
+        "vi_VN": 250024,
+        "zh_CN": 250025,
+    }
+
+    def _encode_with_lang_code(self, raw_text: str, lang_code: int) -> list:
+        ids = self.convert_tokens_to_ids(self.tokenize(raw_text,))[: self.max_len_single_sentence]
+        return ids + [self.eos_token_id, self.lang_code_to_id[lang_code]]
+
+    def prepare_translation_example(self, src_text: str, src_lang="en_XX", tgt_text=None, tgt_lang="ro_RO"):
+        id_key = "input_ids"
+        src_ids = self._encode_with_lang_code(src_text, src_lang)
+        encoder_inputs = self.prepare_for_model(src_ids, return_tensors="pt")
+
+        if tgt_text is not None:
+            tgt_ids = self._encode_with_lang_code(tgt_text, tgt_lang)
+            decoder_input_ids = self.prepare_for_model(tgt_ids, return_tensors="pt")[id_key]
+        else:
+            decoder_input_ids = None
+        return {
+            id_key: encoder_inputs[id_key],
+            "attention_mask": encoder_inputs["attention_mask"],
+            "decoder_input_ids": decoder_input_ids,
+        }
