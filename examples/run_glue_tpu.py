54438f7263112b9c47ab40423ee5b0742a9cba56
LysandreJik <lysandre.debut@reseau.eseo.fr>
2020-04-08 15:57:10 -0400

    Style

--- examples/run_glue_tpu.py
+++ examples/run_glue_tpu.py
@@ -113,7 +113,7 @@ def train(args, train_dataset, model, tokenizer, disable_logging=False):
             "params": [p for n, p in model.named_parameters() if not any(nd in n for nd in no_decay)],
             "weight_decay": args.weight_decay,
         },
-        {"params": [p for n, p in model.named_parameters() if any(nd in n for nd in no_decay)], "weight_decay": 0.0, },
+        {"params": [p for n, p in model.named_parameters() if any(nd in n for nd in no_decay)], "weight_decay": 0.0},
     ]
     optimizer = AdamW(optimizer_grouped_parameters, lr=args.learning_rate, eps=args.adam_epsilon)
     scheduler = get_linear_schedule_with_warmup(
