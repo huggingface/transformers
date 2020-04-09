017c7f0e2d2b6ce0eb28fba546a1771d6da9d01f
TevenLeScao <teven.lescao@gmail.com>
2020-04-09 16:02:33 +0200

    black reformat

--- src/transformers/modeling_transfo_xl.py
+++ src/transformers/modeling_transfo_xl.py
@@ -663,7 +663,7 @@ class TransfoXLModel(TransfoXLPreTrainedModel):
             end_idx = mlen + max(0, qlen - 0 - self.ext_len)
             beg_idx = max(0, end_idx - self.mem_len)
             for i in range(len(hids)):
-                
+
                 cat = torch.cat([mems[i], hids[i]], dim=0)
                 new_mems.append(cat[beg_idx:end_idx].detach())
 
