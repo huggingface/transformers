b2c5ec494e39df51e614afe0a32acb5af3e2c3c9
Douwe Kiela <douwe.kiela@cl.cam.ac.uk>
2020-04-14 20:40:39 -0700

    Fix bug in GLUE example for models that do not require token_type_ids

--- examples/run_glue.py
+++ examples/run_glue.py
@@ -370,7 +370,8 @@ def load_and_cache_examples(args, task, tokenizer, evaluate=False):
     # Convert to Tensors and build dataset
     all_input_ids = torch.tensor([f.input_ids for f in features], dtype=torch.long)
     all_attention_mask = torch.tensor([f.attention_mask for f in features], dtype=torch.long)
-    all_token_type_ids = torch.tensor([f.token_type_ids for f in features], dtype=torch.long)
+    all_token_type_ids = torch.tensor([f.token_type_ids if args.model_type in \
+            ["bert", "xlnet", "albert"] else 0 for f in features], dtype=torch.long)
     if output_mode == "classification":
         all_labels = torch.tensor([f.label for f in features], dtype=torch.long)
     elif output_mode == "regression":
