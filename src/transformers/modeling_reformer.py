bae0700f36b2d828d1345e14a7432e70d03ef9b1
Patrick von Platen <patrick.v.platen@gmail.com>
2020-04-18 21:25:53 +0200

    make uploaded model forward pass work

--- src/transformers/modeling_reformer.py
+++ src/transformers/modeling_reformer.py
@@ -954,8 +954,6 @@ class ReformerPreTrainedModel(PreTrainedModel):
         if isinstance(module, nn.Linear) and module.bias is not None:
             module.bias.data.normal_(mean=0.0, std=1e-6)
             # TODO(PVP): discuss with Thom if necessary here to use different init
-
-
 #            module.bias.data.zero_()
 
 
