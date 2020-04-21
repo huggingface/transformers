283cc1982ecd48e9d9e2097e70fae8355588256a
Julien Chaumond <chaumond@gmail.com>
2020-04-21 10:49:00 -0400

    Fix Torch.hub + Integration test

--- src/transformers/tokenization_camembert.py
+++ src/transformers/tokenization_camembert.py
@@ -22,8 +22,7 @@ from typing import List, Optional
 
 import sentencepiece as spm
 
-from transformers.tokenization_utils import PreTrainedTokenizer
-
+from .tokenization_utils import PreTrainedTokenizer
 from .tokenization_xlnet import SPIECE_UNDERLINE
 
 
