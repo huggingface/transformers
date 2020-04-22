80a8445b5cbfb80592e21dcbf92736fd6ef11053
Deniz <dzorlu@medium.com>
2020-04-21 22:25:04 -0700

    set embeddings for generation head too

--- src/transformers/modeling_tf_t5.py
+++ src/transformers/modeling_tf_t5.py
@@ -806,6 +806,15 @@ class TFT5ForConditionalGeneration(TFT5PreTrainedModel):
     def get_output_embeddings(self):
         return self.shared
 
+    def set_input_embeddings(self, new_embeddings):
+        self.shared = new_embeddings
+        # retrieve correct absolute scope for embed token wrapper
+        with tf.compat.v1.variable_scope("shared") as shared_abs_scope_name:
+            pass
+        embed_tokens = _NoLayerEmbedTokens(self.shared, abs_scope_name=shared_abs_scope_name)
+        self.encoder.set_embed_tokens(embed_tokens)
+        self.decoder.set_embed_tokens(embed_tokens)
+
     def get_encoder(self):
         return self.encoder
 
