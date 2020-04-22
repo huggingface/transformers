13dd2acca4db270e196376c4defc1c9b62b799af
Anthony MOI <m.anthony.moi@gmail.com>
2020-04-22 11:02:29 -0400

    Bump tokenizers version to final 0.7.0 (#3898)

--- setup.py
+++ setup.py
@@ -96,7 +96,7 @@ setup(
     packages=find_packages("src"),
     install_requires=[
         "numpy",
-        "tokenizers == 0.7.0rc7",
+        "tokenizers == 0.7.0",
         # dataclasses for Python versions that don't have it
         "dataclasses;python_version<'3.7'",
         # accessing files from S3 directly
