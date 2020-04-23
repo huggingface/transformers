f83721e9c9143ed9ac02ef564619f23e095abee4
Patrick von Platen <patrick.v.platen@gmail.com>
2020-04-24 01:02:21 +0200

    make style

--- src/transformers/modeling_auto.py
+++ src/transformers/modeling_auto.py
@@ -181,7 +181,7 @@ MODEL_MAPPING = OrderedDict(
         (XLMConfig, XLMModel),
         (CTRLConfig, CTRLModel),
         (ElectraConfig, ElectraModel),
-        (ReformerConfig, ReformerModel)
+        (ReformerConfig, ReformerModel),
     ]
 )
 
