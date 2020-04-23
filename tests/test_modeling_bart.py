8eb30c01c991f94efb821cabbd81688b94fba659
sshleifer <sshleifer@gmail.com>
2020-04-22 23:36:08 -0400

    add to bart common tests

--- tests/test_modeling_bart.py
+++ tests/test_modeling_bart.py
@@ -36,6 +36,7 @@ if is_torch_available():
         BartConfig,
         BartTokenizer,
         MBartTokenizer,
+        MarianForConditionalGeneration,
     )
     from transformers.modeling_bart import (
         BART_PRETRAINED_MODEL_ARCHIVE_MAP,
@@ -109,7 +110,9 @@ class BARTModelTest(ModelTesterMixin, unittest.TestCase):
     all_model_classes = (
         (BartModel, BartForConditionalGeneration, BartForSequenceClassification) if is_torch_available() else ()
     )
-    all_generative_model_classes = (BartForConditionalGeneration,) if is_torch_available() else ()
+    all_generative_model_classes = (
+        (BartForConditionalGeneration, MarianForConditionalGeneration) if is_torch_available() else ()
+    )
     is_encoder_decoder = True
     # TODO(SS): fix the below in a separate PR
     test_pruning = False
