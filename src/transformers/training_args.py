a63aade15e0634af25ed425d6aa023f8dea2616e
Suraj <parmarsuraj99@gmail.com>
2020-04-24 11:41:54 +0530

    added explaination

--- src/transformers/training_args.py
+++ src/transformers/training_args.py
@@ -29,10 +29,12 @@ class TrainingArguments:
         metadata={"help": "The output directory where the model predictions and checkpoints will be written."}
     )
     overwrite_output_dir: bool = field(
-        default=False, 
+        default=False,
         metadata={
-        	"help": "Overwrite the content of the output directory." 
-        	"continue training if output_dir points to a checkpoint directory."
+            "help": (
+                "Overwrite the content of the output directory."
+                "continue training if output_dir points to a checkpoint directory."
+            )
         },
     )
 
@@ -40,14 +42,14 @@ class TrainingArguments:
     do_eval: bool = field(default=False, metadata={"help": "Whether to run eval on the dev set."})
     do_predict: bool = field(default=False, metadata={"help": "Whether to run predictions on the test set."})
     evaluate_during_training: bool = field(
-        default=False, metadata={"help": "Run evaluation during training at each logging step."}
+        default=False, metadata={"help": "Run evaluation during training at each logging step."},
     )
 
     per_gpu_train_batch_size: int = field(default=8, metadata={"help": "Batch size per GPU/CPU for training."})
     per_gpu_eval_batch_size: int = field(default=8, metadata={"help": "Batch size per GPU/CPU for evaluation."})
     gradient_accumulation_steps: int = field(
-        default=1, 
-        metadata={"help": "Number of updates steps to accumulate before performing a backward/update pass."}
+        default=1,
+        metadata={"help": "Number of updates steps to accumulate before performing a backward/update pass."},
     )
 
     learning_rate: float = field(default=5e-5, metadata={"help": "The initial learning rate for Adam."})
@@ -69,8 +71,10 @@ class TrainingArguments:
     save_total_limit: Optional[int] = field(
         default=None,
         metadata={
-            "help": "Limit the total amount of checkpoints" 
-            "delete the older checkpoints in the output_dir, does not delete by default"
+            "help": (
+                "Limit the total amount of checkpoints"
+                "delete the older checkpoints in the output_dir, does not delete by default"
+            )
         },
     )
     no_cuda: bool = field(default=False, metadata={"help": "Avoid using CUDA even if it is available"})
@@ -83,8 +87,10 @@ class TrainingArguments:
     fp16_opt_level: str = field(
         default="O1",
         metadata={
-            "help": "For fp16: Apex AMP optimization level selected in ['O0', 'O1', 'O2', and 'O3']."
-            "See details at https://nvidia.github.io/apex/amp.html"
+            "help": (
+                "For fp16: Apex AMP optimization level selected in ['O0', 'O1', 'O2', and 'O3']."
+                "See details at https://nvidia.github.io/apex/amp.html"
+            )
         },
     )
     local_rank: int = field(default=-1, metadata={"help": "For distributed training: local_rank"})
