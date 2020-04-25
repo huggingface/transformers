9d92a30ba8424e985927929bbaccf49b8319d9b2
monologg <adieujw@gmail.com>
2020-04-25 15:17:08 +0900

    Remove hard-coded pad token id in distilbert and albert

--- src/transformers/modeling_distilbert.py
+++ src/transformers/modeling_distilbert.py
@@ -61,7 +61,7 @@ def create_sinusoidal_embeddings(n_pos, dim, out):
 class Embeddings(nn.Module):
     def __init__(self, config):
         super().__init__()
-        self.word_embeddings = nn.Embedding(config.vocab_size, config.dim, padding_idx=0)
+        self.word_embeddings = nn.Embedding(config.vocab_size, config.dim, padding_idx=config.pad_token_id)
         self.position_embeddings = nn.Embedding(config.max_position_embeddings, config.dim)
         if config.sinusoidal_pos_embds:
             create_sinusoidal_embeddings(
