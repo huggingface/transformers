c511b7fd9a9d14c9f256b7e7d3c037130f291d3e
Hao Tan <airsplay@cs.unc.edu>
2020-04-26 15:20:09 -0400

    Fix the typos
    Fix the typos of "[0.0, 1.0[" --> "[0.0, 1.0]".

--- src/transformers/optimization.py
+++ src/transformers/optimization.py
@@ -108,9 +108,9 @@ class AdamW(Optimizer):
         if lr < 0.0:
             raise ValueError("Invalid learning rate: {} - should be >= 0.0".format(lr))
         if not 0.0 <= betas[0] < 1.0:
-            raise ValueError("Invalid beta parameter: {} - should be in [0.0, 1.0[".format(betas[0]))
+            raise ValueError("Invalid beta parameter: {} - should be in [0.0, 1.0]".format(betas[0]))
         if not 0.0 <= betas[1] < 1.0:
-            raise ValueError("Invalid beta parameter: {} - should be in [0.0, 1.0[".format(betas[1]))
+            raise ValueError("Invalid beta parameter: {} - should be in [0.0, 1.0]".format(betas[1]))
         if not 0.0 <= eps:
             raise ValueError("Invalid epsilon value: {} - should be >= 0.0".format(eps))
         defaults = dict(lr=lr, betas=betas, eps=eps, weight_decay=weight_decay, correct_bias=correct_bias)
