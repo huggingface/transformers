868cdf14c97b73e918d527167ec6929bb6478f08
Jonathan Chang <ttjtftx@gmail.com>
2020-04-26 15:29:46 +0800

    Make code clearer for next patch in split_on_token

--- src/transformers/tokenization_utils.py
+++ src/transformers/tokenization_utils.py
@@ -1244,12 +1244,12 @@ class PreTrainedTokenizer(SpecialTokensMixin):
             for i, sub_text in enumerate(split_text):
                 sub_text = sub_text.rstrip()
                 if i == 0 and not sub_text:
+                    if sub_text: # will never happen
+                        result += [sub_text]
                     result += [tok]
                 elif i == len(split_text) - 1:
                     if sub_text:
                         result += [sub_text]
-                    else:
-                        pass
                 else:
                     if sub_text:
                         result += [sub_text]
