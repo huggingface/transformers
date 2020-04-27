eeede11797c9aec7a8a00055bfda006e9569e05c
Antonis Maronikolakis <marakis.anthony@gmail.com>
2020-04-27 11:09:01 +0200

    save model to output_dir

--- examples/run_xnli.py
+++ examples/run_xnli.py
@@ -607,8 +607,8 @@ def main():
         model_to_save = (
             model.module if hasattr(model, "module") else model
         )  # Take care of distributed/parallel training
-        model_to_save.save_pretrained(args.model_name_or_path)
-        tokenizer.save_pretrained(args.tokenizer_name)
+        model_to_save.save_pretrained(args.output_dir)
+        tokenizer.save_pretrained(args.output_dir)
 
         # Good practice: save your training arguments together with the trained model
         torch.save(args, os.path.join(args.output_dir, "training_args.bin"))
