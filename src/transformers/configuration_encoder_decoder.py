818930cbd93815f54ecd7969a0c668371f873f83
Patrick von Platen <patrick.v.platen@gmail.com>
2020-04-27 21:41:56 +0200

    add docstring to config

--- src/transformers/configuration_encoder_decoder.py
+++ src/transformers/configuration_encoder_decoder.py
@@ -27,10 +27,8 @@ logger = logging.getLogger(__name__)
 
 class EncoderDecoderConfig(PretrainedConfig):
     r"""
-        This is the configuration class to store the configuration of a :class:`~transformers.BertModel`.
-        It is used to instantiate an BERT model according to the specified arguments, defining the model
-        architecture. Instantiating a configuration with the defaults will yield a similar configuration to that of
-        the BERT `bert-base-uncased <https://huggingface.co/bert-base-uncased>`__ architecture.
+        This is the configuration class to store the configuration of a :class:`~transformers.EncoderDecoderModel`.
+        It is used to instantiate an Encoder Decoder model according to the specified arguments, defining the encoder and decoder configs.
 
         Configuration objects inherit from  :class:`~transformers.PretrainedConfig` and can be used
         to control the model outputs. Read the documentation from  :class:`~transformers.PretrainedConfig`
@@ -38,61 +36,17 @@ class EncoderDecoderConfig(PretrainedConfig):
 
 
         Args:
-            vocab_size (:obj:`int`, optional, defaults to 30522):
-                Vocabulary size of the BERT model. Defines the different tokens that
-                can be represented by the `inputs_ids` passed to the forward method of :class:`~transformers.BertModel`.
-            hidden_size (:obj:`int`, optional, defaults to 768):
-                Dimensionality of the encoder layers and the pooler layer.
-            num_hidden_layers (:obj:`int`, optional, defaults to 12):
-                Number of hidden layers in the Transformer encoder.
-            num_attention_heads (:obj:`int`, optional, defaults to 12):
-                Number of attention heads for each attention layer in the Transformer encoder.
-            intermediate_size (:obj:`int`, optional, defaults to 3072):
-                Dimensionality of the "intermediate" (i.e., feed-forward) layer in the Transformer encoder.
-            hidden_act (:obj:`str` or :obj:`function`, optional, defaults to "gelu"):
-                The non-linear activation function (function or string) in the encoder and pooler.
-                If string, "gelu", "relu", "swish" and "gelu_new" are supported.
-            hidden_dropout_prob (:obj:`float`, optional, defaults to 0.1):
-                The dropout probabilitiy for all fully connected layers in the embeddings, encoder, and pooler.
-            attention_probs_dropout_prob (:obj:`float`, optional, defaults to 0.1):
-                The dropout ratio for the attention probabilities.
-            max_position_embeddings (:obj:`int`, optional, defaults to 512):
-                The maximum sequence length that this model might ever be used with.
-                Typically set this to something large just in case (e.g., 512 or 1024 or 2048).
-            type_vocab_size (:obj:`int`, optional, defaults to 2):
-                The vocabulary size of the `token_type_ids` passed into :class:`~transformers.BertModel`.
-            initializer_range (:obj:`float`, optional, defaults to 0.02):
-                The standard deviation of the truncated_normal_initializer for initializing all weight matrices.
-            layer_norm_eps (:obj:`float`, optional, defaults to 1e-12):
-                The epsilon used by the layer normalization layers.
-
-        Example::
-
-            from transformers import BertModel, BertConfig
-
-            # Initializing a BERT bert-base-uncased style configuration
-            configuration = BertConfig()
-
-            # Initializing a model from the bert-base-uncased style configuration
-            model = BertModel(configuration)
-
-            # Accessing the model configuration
-            configuration = model.config
-
-        Attributes:
-            pretrained_config_archive_map (Dict[str, str]):
-                A dictionary containing all the available pre-trained checkpoints.
+            encoder_config (:class:`PretrainedConfig`):
+                An instance of a configuration object that defines the encoder config.
+            decoder_config (:class:`PretrainedConfig`):
+                An instance of a configuration object that defines the decoder config.
     """
     model_type = "encoder_decoder"
 
-    def __init__(self, **kwargs):
-        assert (
-            "encoder" in kwargs and "decoder" in kwargs
-        ), "Config has to be initialized with encoder and decoder config"
-        super().__init__()
-        encoder_config = kwargs.pop("encoder")
+    def __init__(self, encoder_config=None, decoder_config=None, **kwargs):
+        assert (encoder_config is not None and decoder_config is not None), "Config has to be initialized with encoder and decoder config"
+        super().__init__(**kwargs)
         encoder_model_type = encoder_config.pop("model_type")
-        decoder_config = kwargs.pop("decoder")
         decoder_model_type = decoder_config.pop("model_type")
         self.encoder = AutoConfig.for_model(encoder_model_type, **encoder_config)
         self.decoder = AutoConfig.for_model(decoder_model_type, **decoder_config)
@@ -100,6 +54,12 @@ class EncoderDecoderConfig(PretrainedConfig):
 
     @classmethod
     def from_encoder_decoder_config(cls, encoder_config, decoder_config):
+        r"""
+        Instantiate a :class:`~transformers.EncoderDecoderConfig` (or a derived class) from a pre-trained encoder model configuration and decoder model configuration.
+
+        Returns:
+            :class:`EncoderDecoderConfig`: An instance of a configuration object
+        """
         return cls(encoder=encoder_config.to_dict(), decoder=decoder_config.to_dict())
 
     def to_dict(self):
