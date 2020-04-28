fa49b9afeab5545f14b3661b35195b829fcf8ef5
Patrick von Platen <patrick.v.platen@gmail.com>
2020-04-28 15:11:09 +0200

    Clean Encoder-Decoder models with Bart/T5-like API and add generate possibility (#3383)
    * change encoder decoder style to bart & t5 style
    
    * make encoder decoder generation dummy work for bert
    
    * make style
    
    * clean init config in encoder decoder
    
    * add tests for encoder decoder models
    
    * refactor and add last tests
    
    * refactor and add last tests
    
    * fix attn masks for bert encoder decoder
    
    * make style
    
    * refactor prepare inputs for Bert
    
    * refactor
    
    * finish encoder decoder
    
    * correct typo
    
    * add docstring to config
    
    * finish
    
    * add tests
    
    * better naming
    
    * make style
    
    * fix flake8
    
    * clean docstring
    
    * make style
    
    * rename

--- src/transformers/__init__.py
+++ src/transformers/__init__.py
@@ -41,6 +41,7 @@ from .configuration_camembert import CAMEMBERT_PRETRAINED_CONFIG_ARCHIVE_MAP, Ca
 from .configuration_ctrl import CTRL_PRETRAINED_CONFIG_ARCHIVE_MAP, CTRLConfig
 from .configuration_distilbert import DISTILBERT_PRETRAINED_CONFIG_ARCHIVE_MAP, DistilBertConfig
 from .configuration_electra import ELECTRA_PRETRAINED_CONFIG_ARCHIVE_MAP, ElectraConfig
+from .configuration_encoder_decoder import EncoderDecoderConfig
 from .configuration_flaubert import FLAUBERT_PRETRAINED_CONFIG_ARCHIVE_MAP, FlaubertConfig
 from .configuration_gpt2 import GPT2_PRETRAINED_CONFIG_ARCHIVE_MAP, GPT2Config
 from .configuration_mmbt import MMBTConfig
@@ -267,7 +268,7 @@ if is_torch_available():
         CamembertForQuestionAnswering,
         CAMEMBERT_PRETRAINED_MODEL_ARCHIVE_MAP,
     )
-    from .modeling_encoder_decoder import PreTrainedEncoderDecoder
+    from .modeling_encoder_decoder import EncoderDecoderModel
     from .modeling_t5 import (
         T5PreTrainedModel,
         T5Model,
