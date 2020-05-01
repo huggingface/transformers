6f90397971ac3fcfcb897101338225a88d98ede7
MFreidank <freidankm@gmail.com>
2020-05-01 22:27:04 +0200

    Introduce CLI support to package models for pytorch/serve
    chg: add subcommand for interfacing to pytorch/serve, for now
    only blueprint for a transformers-cli command.

---
+++ src/transformers/commands/torchserve.py
@@ -0,0 +1,37 @@
+import logging
+from argparse import ArgumentParser, Namespace
+from typing import Any, List, Optional
+
+from transformers import Pipeline
+from transformers.commands import BaseTransformersCLICommand
+from transformers.pipelines import SUPPORTED_TASKS, pipeline
+
+
+try:
+    _torchserve_dependencies_installed = True
+except (ImportError, AttributeError):
+    _torchserve_dependencies_installed = False
+
+
+logger = logging.getLogger("transformers-cli/torchserve")
+
+class TorchServeCommand(BaseTransformersCLICommand):
+    @staticmethod
+    def register_subcommand(parser: ArgumentParser):
+        torchserve_parser = parser.add_parser(
+            "torchserve",
+            help="CLI interface to package and version models for serving with pytorch/serve."
+        )
+        torchserve_parser.set_defaults(func=lambda args: TorchServeCommand(args))
+
+    def __init__(self, args: Namespace):
+        if not _torchserve_dependencies_installed:
+            raise RuntimeError(
+                "Using torchserve command requires torchserve and torch-model-archiver. "
+                'Please install transformers with [torchserve]: pip install "transformers[torchserve]".'
+                "Or install torchserve and torch-model-archiver separately."
+            )
+        self.args = args
+
+    def run(self):
+        raise NotImplementedError()
