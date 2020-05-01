47e5fc8a8636cfb40eca03da188d581918112505
Patrick von Platen <patrick.v.platen@gmail.com>
2020-05-02 01:37:42 +0200

    update copyright

--- src/transformers/configuration_reformer.py
+++ src/transformers/configuration_reformer.py
@@ -1,5 +1,5 @@
 # coding=utf-8
-# Copyright 2018 The Google AI Language Team Authors and The HuggingFace Inc. team.
+# Copyright 2020 The Trax Authors and The HuggingFace Inc. team.
 # Copyright (c) 2018, NVIDIA CORPORATION.  All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
