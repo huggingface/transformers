b6576c898b3521d1e132e1c0f9a79516d4ac6f07
Patrick von Platen <patrick.v.platen@gmail.com>
2020-05-02 01:40:06 +0200

    fix test path

--- tests/test_modeling_reformer.py
+++ tests/test_modeling_reformer.py
@@ -1106,8 +1106,8 @@ class ReformerIntegrationTests(unittest.TestCase):
 
     @slow
     def test_pretrained_generate_crime_and_punish(self):
-        model = ReformerModelWithLMHead.from_pretrained("patrickvonplaten/reformer-crime-and-punish").to(torch_device)
-        tokenizer = ReformerTokenizer.from_pretrained("patrickvonplaten/reformer-crime-and-punish")
+        model = ReformerModelWithLMHead.from_pretrained("google/reformer-crime-and-punish").to(torch_device)
+        tokenizer = ReformerTokenizer.from_pretrained("google/reformer-crime-and-punish")
         model.eval()
 
         input_ids = tokenizer.encode("A few months later", return_tensors="pt").to(torch_device)
