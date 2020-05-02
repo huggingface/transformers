e12b3d5597642981b8aa5b52e8e0991635485b61
Julien Chaumond <chaumond@gmail.com>
2020-05-02 10:49:21 -0400

    Update src/transformers/pipelines.py

--- src/transformers/pipelines.py
+++ src/transformers/pipelines.py
@@ -656,7 +656,7 @@ class TextClassificationPipeline(Pipeline):
 
     def __call__(self, *args, **kwargs):
         outputs = super().__call__(*args, **kwargs)
-        scores = np.exp(outputs) / np.exp(outputs).sum(-1).reshape(-1,1)
+        scores = np.exp(outputs) / np.exp(outputs).sum(-1, keepdims=True)
         return [{"label": self.model.config.id2label[item.argmax()], "score": item.max()} for item in scores]
 
 
