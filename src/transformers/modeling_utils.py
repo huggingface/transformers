54eb6293a42ac27c3885c6cab580da1fce3930a9
Patrick von Platen <patrick.v.platen@gmail.com>
2020-05-03 14:44:34 +0200

    remove wrong comment in utils

--- src/transformers/modeling_utils.py
+++ src/transformers/modeling_utils.py
@@ -13,7 +13,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""PyTorch BERT model."""
 
 import inspect
 import logging
