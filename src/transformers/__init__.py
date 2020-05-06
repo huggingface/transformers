f7885b6bab08be3e90592285b939ae20dfb38dae
kumapo <kumapo@users.noreply.github.com>
2020-05-06 16:10:22 +0900

    Include ElectraPreTrainedModel into __init__

--- src/transformers/__init__.py
+++ src/transformers/__init__.py
@@ -312,6 +312,7 @@ if is_torch_available():
         ElectraForPreTraining,
         ElectraForMaskedLM,
         ElectraForTokenClassification,
+        ElectraPreTrainedModel,
         ElectraModel,
         load_tf_weights_in_electra,
         ELECTRA_PRETRAINED_MODEL_ARCHIVE_MAP,
