23bb7b9449c93594bc160306c67f1bddb8ae5bf7
Morgan Funtowicz <funtowiczmo@gmail.com>
2020-05-07 13:24:15 +0200

    Avoid reshaping operations in Conv1D by using baddbmm

--- src/transformers/modeling_utils.py
+++ src/transformers/modeling_utils.py
@@ -1708,10 +1708,7 @@ class Conv1D(nn.Module):
         self.bias = nn.Parameter(torch.zeros(nf))
 
     def forward(self, x):
-        size_out = x.size()[:-1] + (self.nf,)
-        x = torch.addmm(self.bias, x.view(-1, x.size(-1)), self.weight)
-        x = x.view(*size_out)
-        return x
+        return torch.baddbmm(self.bias[None, None], x, self.weight[None])
 
 
 class PoolerStartLogits(nn.Module):
