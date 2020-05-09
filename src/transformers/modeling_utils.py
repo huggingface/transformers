c394156eda2b794617feb998fb74c49b10a34d77
Bijay Gurung <bijay@fusemachines.com>
2020-05-09 08:35:52 +0545

    Use typing.Iterable instead of Sequence

--- src/transformers/modeling_utils.py
+++ src/transformers/modeling_utils.py
@@ -17,7 +17,7 @@
 
 import logging
 import os
-from typing import Callable, Sequence, Tuple
+from typing import Callable, Iterable, Tuple
 
 import torch
 from torch import Tensor, device, dtype, nn
@@ -777,7 +777,7 @@ class PreTrainedModel(nn.Module, ModuleUtilsMixin):
         top_k: int = None,
         top_p: float = None,
         repetition_penalty: float = None,
-        bad_words_ids: Sequence = None,
+        bad_words_ids: Iterable = None,
         bos_token_id: int = None,
         pad_token_id: int = None,
         eos_token_id: int = None,
@@ -1567,7 +1567,7 @@ def calc_banned_ngram_tokens(prev_input_ids: Tensor, num_hypos: int, no_repeat_n
     return banned_tokens
 
 
-def calc_banned_bad_words_ids(prev_input_ids: Sequence, bad_words_ids: Sequence) -> Sequence:
+def calc_banned_bad_words_ids(prev_input_ids: Iterable, bad_words_ids: Iterable) -> Iterable:
     banned_tokens = []
 
     def _tokens_match(prev_tokens, tokens):
@@ -1604,12 +1604,12 @@ def calc_banned_bad_words_ids(prev_input_ids: Sequence, bad_words_ids: Sequence)
 
 
 def top_k_top_p_filtering(
-    logits: Sequence,
+    logits: Iterable,
     top_k: int = 0,
     top_p: float = 1.0,
     filter_value: float = -float("Inf"),
     min_tokens_to_keep: int = 1,
-) -> Sequence:
+) -> Iterable:
     """ Filter a distribution of logits using top-k and/or nucleus (top-p) filtering
         Args:
             logits: logits distribution shape (batch size, vocabulary size)
