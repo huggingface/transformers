14dbd5965a9097116018e60cc383a7bd3a89d48b
Lysandre <lysandre.debut@reseau.eseo.fr>
2020-05-11 17:59:50 -0400

    Test ELECTRA for sequence classification

--- src/transformers/__init__.py
+++ src/transformers/__init__.py
@@ -321,6 +321,7 @@ if is_torch_available():
         ElectraForMaskedLM,
         ElectraForTokenClassification,
         ElectraPreTrainedModel,
+        ElectraForSequenceClassification,
         ElectraModel,
         load_tf_weights_in_electra,
         ELECTRA_PRETRAINED_MODEL_ARCHIVE_MAP,
