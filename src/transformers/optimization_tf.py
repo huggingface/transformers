dbbda814a825cdf2811abf741fee2e4a85403320
jplu <plu.julien@gmail.com>
2020-05-11 15:11:27 +0200

    Fix the issue to properly run the accumulator with TF 2.2

--- src/transformers/optimization_tf.py
+++ src/transformers/optimization_tf.py
@@ -205,6 +205,7 @@ class GradientAccumulator(object):
         if self._accum_steps is None:
             self._accum_steps = tf.Variable(
                 tf.constant(0, dtype=tf.int64), trainable=False, synchronization=tf.VariableSynchronization.ON_READ,
+                aggregation=tf.VariableAggregation.ONLY_FIRST_REPLICA,
             )
 
         return self._accum_steps.value()
@@ -224,6 +225,7 @@ class GradientAccumulator(object):
                 [
                     tf.Variable(
                         tf.zeros_like(gradient), trainable=False, synchronization=tf.VariableSynchronization.ON_READ,
+                        aggregation=tf.VariableAggregation.ONLY_FIRST_REPLICA,
                     )
                     for gradient in gradients
                 ]
