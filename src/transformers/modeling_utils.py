7eef4f5a7575e05e822f8ef45d7f473a102671aa
Julien Chaumond <chaumond@gmail.com>
2020-05-14 14:45:45 -0400

    Update src/transformers/modeling_utils.py

--- src/transformers/modeling_utils.py
+++ src/transformers/modeling_utils.py
@@ -113,6 +113,7 @@ class ModuleUtilsMixin:
         try:
             return next(self.parameters()).device
         except StopIteration:
+            # For nn.DataParallel compatibility in PyTorch 1.5
 
             def find_tensor_attributes(module: nn.Module) -> List[Tuple[str, Tensor]]:
                 tuples = [(k, v) for k, v in module.__dict__.items() if torch.is_tensor(v)]
