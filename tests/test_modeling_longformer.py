7707ba6e70c1d85cc926970c1e0f067714eb964e
Iz Beltagy <beltagy@allenai.org>
2020-05-18 11:27:16 -0700

    increase unittest coverage

--- tests/test_modeling_longformer.py
+++ tests/test_modeling_longformer.py
@@ -217,10 +217,14 @@ class LongformerModelIntegrationTest(unittest.TestCase):
 
         # 'Hello world! ' repeated 1000 times
         input_ids = torch.tensor([[0] + [20920, 232, 328, 1437] * 1000 + [2]])  # long input
-        output = model(input_ids)[0]
-        expected_output_sum = torch.tensor(74518.2344)
-        expected_output_mean = torch.tensor(0.0242)
 
+        attention_mask = torch.ones(input_ids.shape, dtype=torch.long, device=input_ids.device)
+        attention_mask[:, [1, 4, 21]] = 2  # Set global attention on a few random positions
+
+        output = model(input_ids, attention_mask=attention_mask)[0]
+
+        expected_output_sum = torch.tensor(74585.8594)
+        expected_output_mean = torch.tensor(0.0243)
         self.assertTrue(torch.allclose(output.sum(), expected_output_sum, atol=1e-4))
         self.assertTrue(torch.allclose(output.mean(), expected_output_mean, atol=1e-4))
 
