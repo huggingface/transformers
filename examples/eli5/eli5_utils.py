199f7437103c356e93fcb9474f13062474f8d2f9
yjernite <yacine.jernite@gmail.com>
2020-05-19 16:37:13 -0400

    retriever_batch_checkpointing

--- examples/eli5/eli5_utils.py
+++ examples/eli5/eli5_utils.py
@@ -11,21 +11,23 @@ import sys
 
 from os.path import join as pjoin
 from random import choice, randint
-from torch.utils.data import DataLoader, Dataset, RandomSampler, SequentialSampler
 from time import time
 from tqdm import tqdm, trange
 
 from elasticsearch import Elasticsearch
 from elasticsearch.helpers import bulk, streaming_bulk
 
-import faiss
-
 import torch
+import torch.utils.checkpoint as checkpoint
+
+from torch.utils.data import DataLoader, Dataset, RandomSampler, SequentialSampler
+
+import faiss
+import nlp
 
 from transformers import AutoModel, AutoModelWithLMHead, AutoTokenizer
 from transformers import AdamW, get_linear_schedule_with_warmup
 
-import nlp
 
 ###############
 ### Defining nlp datasets for: ELI5, KILT-wikipedia original and snippets, Wiki40B snippets
@@ -505,22 +507,66 @@ class RetrievalQAEmbedder(torch.nn.Module):
     def __init__(self, sent_encoder, dim):
         super(RetrievalQAEmbedder, self).__init__()
         self.sent_encoder = sent_encoder
-        self.project_q = torch.nn.Linear(dim, 128, bias=False)
-        self.project_a = torch.nn.Linear(dim, 128, bias=False)
+        self.output_dim = 128
+        self.project_q = torch.nn.Linear(dim, self.output_dim, bias=False)
+        self.project_a = torch.nn.Linear(dim, self.output_dim, bias=False)
         self.ce_loss = torch.nn.CrossEntropyLoss(reduction='mean')
 
-    def embed_questions(self, q_ids, q_mask):
-        _, q_reps = self.sent_encoder(q_ids, attention_mask=q_mask)
+    def embed_sentences_checkpointed(self, input_ids, attention_mask, checkpoint_batch_size=-1):
+        # reproduces BERT forward pass with checkpointing
+        if checkpoint_batch_size < 0 or input_ids.shape[0] < checkpoint_batch_size:
+            return self.sent_encoder(input_ids, attention_mask=attention_mask)[1]
+        else:
+            # prepare implicit variables
+            device = input_ids.device
+            input_shape = input_ids.size()
+            token_type_ids = torch.zeros(input_shape, dtype=torch.long, device=device)
+            head_mask = [None] * self.sent_encoder.config.num_hidden_layers
+            extended_attention_mask: torch.Tensor = self.sent_encoder.get_extended_attention_mask(
+                attention_mask, input_shape, device
+            )
+            # define function for cehckpointing
+            def partial_encode(*inputs):
+                encoder_outputs = self.sent_encoder.encoder(
+                    inputs[0],
+                    attention_mask=inputs[1],
+                    head_mask=head_mask,
+                    encoder_hidden_states=None,
+                    encoder_attention_mask=None,
+                )
+                sequence_output = encoder_outputs[0]
+                pooled_output = self.sent_encoder.pooler(sequence_output)
+                return pooled_output
+            # run embedding layer on everything at once
+            embedding_output = self.sent_encoder.embeddings(
+                input_ids=input_ids,
+                position_ids=None,
+                token_type_ids=token_type_ids,
+                inputs_embeds=None
+            )
+            # run encoding and pooling on one mini-batch at a time
+            pooled_output_list = []
+            for b in range(math.ceil(input_ids.shape[0] / checkpoint_batch_size)):
+                b_embedding_output = embedding_output[b*checkpoint_batch_size:(b+1)*checkpoint_batch_size]
+                b_attention_mask = extended_attention_mask[b*checkpoint_batch_size:(b+1)*checkpoint_batch_size]
+                pooled_output = checkpoint.checkpoint(
+                    partial_encode, b_embedding_output, b_attention_mask
+                )
+                pooled_output_list.append(pooled_output)
+            return torch.cat(pooled_output_list, dim=0)
+
+    def embed_questions(self, q_ids, q_mask, checkpoint_batch_size=-1):
+        q_reps = self.embed_sentences_checkpointed(q_ids, q_mask, checkpoint_batch_size)
         return self.project_q(q_reps)
 
-    def embed_answers(self, a_ids, a_mask):
-        _, a_reps = self.sent_encoder(a_ids, attention_mask=a_mask)
+    def embed_answers(self, a_ids, a_mask, checkpoint_batch_size=-1):
+        a_reps = self.embed_sentences_checkpointed(a_ids, a_mask, checkpoint_batch_size)
         return self.project_a(a_reps)
 
-    def forward(self, q_ids, q_mask, a_ids, a_mask):
+    def forward(self, q_ids, q_mask, a_ids, a_mask, checkpoint_batch_size=-1):
         device = next(self.parameters()).device
-        q_reps = self.embed_questions(q_ids, q_mask)
-        a_reps = self.embed_answers(a_ids, a_mask)
+        q_reps = self.embed_questions(q_ids, q_mask, checkpoint_batch_size)
+        a_reps = self.embed_answers(a_ids, a_mask, checkpoint_batch_size)
         compare_scores = torch.mm(q_reps, a_reps.t())
         loss_qa = self.ce_loss(compare_scores, torch.arange(compare_scores.shape[1]).to(device))
         loss_aq = self.ce_loss(compare_scores.t(), torch.arange(compare_scores.shape[0]).to(device))
@@ -570,7 +616,10 @@ def train_qa_retriever_epoch(model, dataset, tokenizer, optimizer, scheduler, ar
     st_time = time()
     for step, batch in enumerate(epoch_iterator):
         q_ids, q_mask, a_ids, a_mask = batch
-        loss = model(q_ids, q_mask, a_ids, a_mask)
+        loss = model(
+            q_ids, q_mask, a_ids, a_mask,
+            checkpoint_batch_size=args.checkpoint_batch_size
+        )
         # optimizer
         loss.backward()
         optimizer.step()
@@ -725,16 +774,12 @@ def train_qa_s2s_epoch(model, dataset, tokenizer, optimizer, scheduler, args, e=
             loc_loss = 0
             loc_steps = 0
 
-
-
-
-
 ###############
 ### ELI5-trained retrieval model usage
 ###############
-def embed_passages_for_retrieval(passage_list, tokenizer, qa_embedder, device='cuda:0'):
+def embed_passages_for_retrieval(passage_list, tokenizer, qa_embedder, max_length=128, device='cuda:0'):
     a_ls = [p for p in passage_list['passage_text']]
-    a_toks = tokenizer.batch_encode_plus(a_ls, max_length=128, pad_to_max_length=True)
+    a_toks = tokenizer.batch_encode_plus(a_ls, max_length=max_length, pad_to_max_length=True)
     a_ids, a_mask = (
         torch.LongTensor(a_toks['input_ids']).to(device),
         torch.LongTensor(a_toks['attention_mask']).to(device),
@@ -754,13 +799,16 @@ def embed_question_for_retrieval(q_ls, tokenizer, qa_embedder, device='cuda:0'):
     return q_reps.numpy()
 
 def make_qa_dense_index(qa_embedder, tokenizer, passages_dset,
-                        batch_size=512, index_name='kilt_passages_reps.dat',
+                        batch_size=512, max_length=128, index_name='kilt_passages_reps.dat',
                         device='cuda:0'):
     st_time = time()
     fp = np.memmap(index_name, dtype='float16', mode='w+', shape=(wiki_passages.num_rows, 128))
-    n_batches = math.ceil(wiki_passages.num_rows / batch_size)
+    n_batches = math.ceil(passages_dset.num_rows / batch_size)
     for i in range(n_batches):
-        reps = embed_passages_for_retrieval(passages_dset[i * batch_size:(i+1) * batch_size], tokenizer, qa_embedder, device)
+        reps = embed_passages_for_retrieval(
+            passages_dset[i * batch_size:(i+1) * batch_size],
+            tokenizer, qa_embedder, max_length, device
+        )
         fp[i * batch_size:(i+1) * batch_size] = reps
         if i % 50 == 0:
             print(i, time() - st_time)
