3cebd56848ff3d0344d4903d755491005b15a48c
sshleifer <sshleifer@gmail.com>
2020-05-19 14:35:05 -0400

    boom boom

--- examples/summarization/bart/run_distiller.sh
+++ examples/summarization/bart/run_distiller.sh
@@ -1,5 +1,5 @@
 export CURRENT_DIR=${PWD}
-CNN_DIR = /home/shleifer/transformers_fork/examples/summarization/bart/cnn_dm
+#CNN_DIR = /home/shleifer/transformers_fork/examples/summarization/bart/cnn_dm
 
 # Make output directory if it doesn't exist
 mkdir -p $OUTPUT_DIR
