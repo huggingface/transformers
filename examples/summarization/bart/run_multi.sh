7a7dcd397fb439549772d9b296443724a4238b60
sshleifer <sshleifer@gmail.com>
2020-05-20 22:25:18 -0400

    l2copy[1] = 0

--- examples/summarization/bart/run_multi.sh
+++ examples/summarization/bart/run_multi.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 export CUDA_VISIBLE_DEVICES=1
-OUTPUT_DIR=dbart/logs/1l_nv300 ./run_distiller.sh --n_val 300 --num_train_epochs 2 --student_decoder_layers 1
-OUTPUT_DIR=dbart/logs/1l_nv300 ./run_distiller.sh --n_val 300 --num_train_epochs 2 --student_decoder_layers 1
+OUTPUT_DIR=dbart/logs/dl1 ./run_distiller.sh --n_val 500 --num_train_epochs 2 --student_decoder_layers 1
+OUTPUT_DIR=dbart/logs/dl3 ./run_distiller.sh --n_val 500 --num_train_epochs 2 --student_decoder_layers 3
+OUTPUT_DIR=dbart/logs/dl6_ckpt ./run_distiller.sh --n_val 500 --num_train_epochs 2 \
+  --student_decoder_layers 6 --resume_from_checkpoint=dbart/logs/run_6l/checkpointepoch=0.ckpt
 
