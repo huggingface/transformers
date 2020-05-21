70d3058aa154dcd631c0265f8b41fc6e06f7dbda
sshleifer <sshleifer@gmail.com>
2020-05-21 16:47:50 -0400

    style

--- src/transformers/pipelines.py
+++ src/transformers/pipelines.py
@@ -1628,7 +1628,7 @@ SUPPORTED_TASKS = {
         "impl": SummarizationPipeline,
         "tf": TFAutoModelWithLMHead if is_tf_available() else None,
         "pt": AutoModelWithLMHead if is_torch_available() else None,
-        "default": {"model": {"pt": "bart-large-cnn", "tf": "t5-small"}, "config": None, "tokenizer": None,},
+        "default": {"model": {"pt": "bart-large-cnn", "tf": "t5-small"}, "config": None, "tokenizer": None},
     },
     "translation_en_to_fr": {
         "impl": TranslationPipeline,
