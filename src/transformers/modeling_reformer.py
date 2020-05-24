21b32670bf529c831363f9fba0b888ea5f189460
patrickvonplaten <patrick.v.platen@gmail.com>
2020-05-24 21:05:31 +0200

    fix

--- src/transformers/modeling_reformer.py
+++ src/transformers/modeling_reformer.py
@@ -532,17 +532,17 @@ class LSHSelfAttention(nn.Module, EfficientAttentionMixin):
         return sorted_bucket_idx, undo_sorted_bucket_idx
 
     def _set_num_buckets(self, sequence_length):
-        # recommended `num_buckets` from paper
-        num_buckets = 2 * (sequence_length // self.chunk_length)
+        # `num_buckets` should be set to 2 * sequence_length // chunk_length as recommended in paper
+        num_buckets_pow_2 = (2 * (sequence_length // self.chunk_length)).bit_length() - 1
+        # make sure buckets are power of 2
+        num_buckets = 2 ** num_buckets_pow_2
 
         # factorize `num_buckets` if `num_buckets` becomes too large
         num_buckets_limit = 2 * max(
             int((self.max_position_embeddings // self.chunk_length) ** (0.5)), self.chunk_length,
         )
         if num_buckets > num_buckets_limit:
-            pow_2 = num_buckets.bit_length() - 1
-            # make sure buckets are power of 2
-            num_buckets = [2 ** (pow_2 // 2), 2 ** (pow_2 - pow_2 // 2)]
+            num_buckets = [2 ** (num_buckets_pow_2 // 2), 2 ** (num_buckets_pow_2 - num_buckets_pow_2 // 2)]
 
         logger.warning("config.num_buckets is not set. Setting config.num_buckets to {}...".format(num_buckets))
         self.num_buckets = num_buckets
