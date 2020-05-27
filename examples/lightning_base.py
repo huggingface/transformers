424fba4d8afd1f839b5d2f1cd9cc4b9b1236a9ba
Nate Raw <nxr9266@g.rit.edu>
2020-05-27 12:11:17 -0600

    :sparkles: update argparser and remove some junk

--- examples/lightning_base.py
+++ examples/lightning_base.py
@@ -66,9 +66,6 @@ class BaseTransformer(pl.LightningModule):
             cache_dir=cache_dir,
         )
 
-    def is_logger(self):
-        return self.trainer.proc_rank <= 0
-
     def configure_optimizers(self):
         "Prepare optimizer and schedule (linear warmup and decay)"
 
@@ -96,17 +93,6 @@ class BaseTransformer(pl.LightningModule):
         optimizer.zero_grad()
         self.lr_scheduler.step()
 
-    def get_tqdm_dict(self):
-        avg_loss = getattr(self.trainer, "avg_loss", 0.0)
-        tqdm_dict = {"loss": "{:.3f}".format(avg_loss), "lr": self.lr_scheduler.get_last_lr()[-1]}
-        return tqdm_dict
-
-    def test_step(self, batch, batch_nb):
-        return self.validation_step(batch, batch_nb)
-
-    def test_end(self, outputs):
-        return self.validation_end(outputs)
-
     def train_dataloader(self):
         train_batch_size = self.hparams.train_batch_size
         dataloader = self.load_dataset("train", train_batch_size)
@@ -123,7 +109,7 @@ class BaseTransformer(pl.LightningModule):
         return dataloader
 
     def val_dataloader(self):
-        return self.load_dataset("dev", self.hparams.eval_batch_size)
+        return self.load_dataset("validation", self.hparams.eval_batch_size)
 
     def test_dataloader(self):
         return self.load_dataset("test", self.hparams.eval_batch_size)
@@ -139,7 +125,9 @@ class BaseTransformer(pl.LightningModule):
         )
 
     @staticmethod
-    def add_model_specific_args(parser, root_dir):
+    def add_model_specific_args(parent_parser, root_dir):
+        parser = argparse.ArgumentParser(parents=[parent_parser], add_help=False)
+
         parser.add_argument(
             "--model_name_or_path",
             default=None,
@@ -166,18 +154,16 @@ class BaseTransformer(pl.LightningModule):
         parser.add_argument("--weight_decay", default=0.0, type=float, help="Weight decay if we apply some.")
         parser.add_argument("--adam_epsilon", default=1e-8, type=float, help="Epsilon for Adam optimizer.")
         parser.add_argument("--warmup_steps", default=0, type=int, help="Linear warmup over warmup_steps.")
-        parser.add_argument(
-            "--num_train_epochs", default=3, type=int, help="Total number of training epochs to perform."
-        )
-
         parser.add_argument("--train_batch_size", default=32, type=int)
         parser.add_argument("--eval_batch_size", default=32, type=int)
+        parser.add_argument("--num_workers", default=0, type=int)
+        return parser
 
 
 class LoggingCallback(pl.Callback):
     def on_validation_end(self, trainer: pl.Trainer, pl_module: pl.LightningModule):
         logger.info("***** Validation results *****")
-        if pl_module.is_logger():
+        if trainer.proc_rank <= 0:
             metrics = trainer.callback_metrics
             # Log results
             for key in sorted(metrics):
@@ -187,7 +173,7 @@ class LoggingCallback(pl.Callback):
     def on_test_end(self, trainer: pl.Trainer, pl_module: pl.LightningModule):
         logger.info("***** Test results *****")
 
-        if pl_module.is_logger():
+        if trainer.proc_rank <= 0:
             metrics = trainer.callback_metrics
 
             # Log and save results to file
@@ -197,84 +183,3 @@ class LoggingCallback(pl.Callback):
                     if key not in ["log", "progress_bar"]:
                         logger.info("{} = {}\n".format(key, str(metrics[key])))
                         writer.write("{} = {}\n".format(key, str(metrics[key])))
-
-
-def add_generic_args(parser, root_dir):
-    parser.add_argument(
-        "--output_dir",
-        default=None,
-        type=str,
-        required=True,
-        help="The output directory where the model predictions and checkpoints will be written.",
-    )
-
-    parser.add_argument(
-        "--fp16",
-        action="store_true",
-        help="Whether to use 16-bit (mixed) precision (through NVIDIA apex) instead of 32-bit",
-    )
-
-    parser.add_argument(
-        "--fp16_opt_level",
-        type=str,
-        default="O1",
-        help="For fp16: Apex AMP optimization level selected in ['O0', 'O1', 'O2', and 'O3']."
-        "See details at https://nvidia.github.io/apex/amp.html",
-    )
-
-    parser.add_argument("--n_gpu", type=int, default=1)
-    parser.add_argument("--n_tpu_cores", type=int, default=0)
-    parser.add_argument("--max_grad_norm", default=1.0, type=float, help="Max gradient norm.")
-    parser.add_argument("--do_train", action="store_true", help="Whether to run training.")
-    parser.add_argument("--do_predict", action="store_true", help="Whether to run predictions on the test set.")
-    parser.add_argument(
-        "--gradient_accumulation_steps",
-        type=int,
-        default=1,
-        help="Number of updates steps to accumulate before performing a backward/update pass.",
-    )
-
-    parser.add_argument("--seed", type=int, default=42, help="random seed for initialization")
-
-
-def generic_train(model: BaseTransformer, args: argparse.Namespace):
-    # init model
-    set_seed(args)
-
-    if os.path.exists(args.output_dir) and os.listdir(args.output_dir) and args.do_train:
-        raise ValueError("Output directory ({}) already exists and is not empty.".format(args.output_dir))
-
-    checkpoint_callback = pl.callbacks.ModelCheckpoint(
-        filepath=args.output_dir, prefix="checkpoint", monitor="val_loss", mode="min", save_top_k=5
-    )
-
-    train_params = dict(
-        accumulate_grad_batches=args.gradient_accumulation_steps,
-        gpus=args.n_gpu,
-        max_epochs=args.num_train_epochs,
-        early_stop_callback=False,
-        gradient_clip_val=args.max_grad_norm,
-        checkpoint_callback=checkpoint_callback,
-        callbacks=[LoggingCallback()],
-    )
-
-    if args.fp16:
-        train_params["use_amp"] = args.fp16
-        train_params["amp_level"] = args.fp16_opt_level
-
-    if args.n_tpu_cores > 0:
-        global xm
-        import torch_xla.core.xla_model as xm
-
-        train_params["num_tpu_cores"] = args.n_tpu_cores
-        train_params["gpus"] = 0
-
-    if args.n_gpu > 1:
-        train_params["distributed_backend"] = "ddp"
-
-    trainer = pl.Trainer(**train_params)
-
-    if args.do_train:
-        trainer.fit(model)
-
-    return trainer
