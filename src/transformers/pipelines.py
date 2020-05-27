2f4989a569abf3daaf71dfc5e1cd4d18e0bfd5bb
Julien Chaumond <chaumond@gmail.com>
2020-05-27 22:41:27 +0000

    FillMaskPipeline: also output token in string form

--- src/transformers/pipelines.py
+++ src/transformers/pipelines.py
@@ -808,7 +808,14 @@ class FillMaskPipeline(Pipeline):
                 tokens[masked_index] = p
                 # Filter padding out:
                 tokens = tokens[np.where(tokens != self.tokenizer.pad_token_id)]
-                result.append({"sequence": self.tokenizer.decode(tokens), "score": v, "token": p})
+                result.append(
+                    {
+                        "sequence": self.tokenizer.decode(tokens),
+                        "score": v,
+                        "token": p,
+                        "token_str": self.tokenizer.convert_ids_to_tokens(p),
+                    }
+                )
 
             # Append
             results += [result]
