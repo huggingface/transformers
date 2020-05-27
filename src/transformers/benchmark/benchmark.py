4bd7cfc30952922b6329ec2d7637f13729317014
Patrick von Platen <patrick.v.platen@gmail.com>
2020-05-27 21:47:07 +0200

    add better train memory handling

--- src/transformers/benchmark/benchmark.py
+++ src/transformers/benchmark/benchmark.py
@@ -46,52 +46,56 @@ class PyTorchBenchmark(Benchmark):
         return torch.__version__
 
     def train(self, model_name, batch_size, sequence_length, trace_memory=False):
-        config = self.config_dict[model_name]
-        model = MODEL_WITH_LM_HEAD_MAPPING[config.__class__](config)
-        model.to(self.args.device)
-        model.train()
+        try:
+            config = self.config_dict[model_name]
+            model = MODEL_WITH_LM_HEAD_MAPPING[config.__class__](config)
+            model.to(self.args.device)
+            model.train()
 
-        input_ids = torch.randint(
-            model.config.vocab_size, (batch_size, sequence_length), dtype=torch.long, device=self.args.device
-        )
+            input_ids = torch.randint(
+                model.config.vocab_size, (batch_size, sequence_length), dtype=torch.long, device=self.args.device
+            )
 
-        def compute_loss_and_backprob():
-            # TODO: Not all models call labels argument labels => this hack using the function signature should be corrected once all models have a common name for labels
-            function_argument_names = inspect.getfullargspec(model.forward).args
-            if "labels" in function_argument_names:
-                loss = model(input_ids, labels=input_ids)[0]
-            elif "lm_labels" in function_argument_names:
-                loss = model(input_ids, lm_labels=input_ids)[0]
-            elif "masked_lm_labels" in function_argument_names:
-                loss = model(input_ids, masked_lm_labels=input_ids)[0]
+            def compute_loss_and_backprob():
+                # TODO: Not all models call labels argument labels => this hack using the function signature should be corrected once all models have a common name for labels
+                function_argument_names = inspect.getfullargspec(model.forward).args
+                if "labels" in function_argument_names:
+                    loss = model(input_ids, labels=input_ids)[0]
+                elif "lm_labels" in function_argument_names:
+                    loss = model(input_ids, lm_labels=input_ids)[0]
+                elif "masked_lm_labels" in function_argument_names:
+                    loss = model(input_ids, masked_lm_labels=input_ids)[0]
+                else:
+                    NotImplementedError(f"{model_name} does not seem to allow training with labels")
+
+                loss.backward()
+                model.zero_grad()
+
+            if trace_memory is True:
+                if self.args.trace_memory_line_by_line or self.args.n_gpu == 0:
+                    trace = start_memory_tracing("transformers")
+                else:
+                    # clear cuda cache
+                    torch.cuda.empty_cache()
+                    torch.cuda.reset_peak_memory_stats()
+
+                # calculate loss and do backpropagation
+                compute_loss_and_backprob()
+
+                if self.args.trace_memory_line_by_line or self.args.n_gpu == 0:
+                    summary = stop_memory_tracing(trace)
+                    memory = summary.total
+                else:
+                    memory = Memory(torch.cuda.max_memory_reserved())
+
+                return memory
             else:
-                NotImplementedError(f"{model_name} does not seem to allow training with labels")
-
-            loss.backward()
-            model.zero_grad()
-
-        if trace_memory is True:
-            if self.args.trace_memory_line_by_line or self.args.n_gpu == 0:
-                trace = start_memory_tracing("transformers")
-            else:
-                # clear cuda cache
-                torch.cuda.empty_cache()
-                torch.cuda.reset_peak_memory_stats()
-
-            # calculate loss and do backpropagation
-            compute_loss_and_backprob()
-
-            if self.args.trace_memory_line_by_line or self.args.n_gpu == 0:
-                summary = stop_memory_tracing(trace)
-                memory = summary.total
-            else:
-                memory = Memory(torch.cuda.max_memory_reserved())
-
-            return memory
-        else:
-            # as written in https://docs.python.org/2/library/timeit.html#timeit.Timer.repeat, min should be taken rather than the average
-            runtimes = timeit.repeat(lambda: compute_loss_and_backprob(), repeat=self.args.repeat, number=10,)
-            return min(runtimes) / 10.0
+                # as written in https://docs.python.org/2/library/timeit.html#timeit.Timer.repeat, min should be taken rather than the average
+                runtimes = timeit.repeat(lambda: compute_loss_and_backprob(), repeat=self.args.repeat, number=10,)
+                return min(runtimes) / 10.0
+        except RuntimeError as e:
+            self.print_fn("Doesn't fit on GPU. {}".format(e))
+            return "N/A"
 
     def inference(self, model_name, batch_size, sequence_length, trace_memory=False):
         try:
