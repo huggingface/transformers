602830903a34bbd320fa10aece86e8e311143307
Victor SANH <victorsanh@gmail.com>
2020-05-27 18:24:23 -0400

    add sparsity modules

---
+++ examples/movement-pruning/emmental/__init__.py
@@ -0,0 +1,11 @@
+from .modules import *
+
+from .configuration_bert_masked import MaskedBertConfig
+
+from .modeling_bert_masked import (
+    MaskedBertModel,
+    MaskedBertForQuestionAnswering,
+    MaskedBertForSequenceClassification,
+    MaskedBertForTokenClassification,
+    MaskedBertForMultipleChoice,
+)
