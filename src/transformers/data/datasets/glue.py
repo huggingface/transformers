01a48df8d75434ce81bcfdb2e652be03f1d0a2f4
Julien Chaumond <chaumond@gmail.com>
2020-06-02 13:35:17 -0400

    Small Tweaks

--- src/transformers/data/datasets/glue.py
+++ src/transformers/data/datasets/glue.py
@@ -70,7 +70,7 @@ class GlueDataset(Dataset):
         tokenizer: PreTrainedTokenizer,
         limit_length: Optional[int] = None,
         mode: Union[str, Split] = Split.train,
-        cache_dir: str = None,
+        cache_dir: Optional[str] = None,
     ):
         self.args = args
         self.processor = glue_processors[args.task_name]()
@@ -82,7 +82,7 @@ class GlueDataset(Dataset):
                 raise KeyError("mode is not a valid split name")
         # Load data features from cache or dataset file
         cached_features_file = os.path.join(
-            args.data_dir if not cache_dir else cache_dir,
+            cache_dir if cache_dir is not None else args.data_dir,
             "cached_{}_{}_{}_{}".format(
                 mode.value, tokenizer.__class__.__name__, str(args.max_seq_length), args.task_name,
             ),
