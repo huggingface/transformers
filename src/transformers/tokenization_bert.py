f725a85dc06ce964b0d82311079703b63e9baad2
Morgan Funtowicz <funtowiczmo@gmail.com>
2020-06-02 14:36:38 +0200

    Ensure tokens in never_split are not splitted when using basic tokenizer before wordpiece.

--- src/transformers/tokenization_bert.py
+++ src/transformers/tokenization_bert.py
@@ -208,8 +208,12 @@ class BertTokenizer(PreTrainedTokenizer):
         split_tokens = []
         if self.do_basic_tokenize:
             for token in self.basic_tokenizer.tokenize(text, never_split=self.all_special_tokens):
-                for sub_token in self.wordpiece_tokenizer.tokenize(token):
-                    split_tokens.append(sub_token)
+
+                # If the token is part of the never_split set
+                if token in self.basic_tokenizer.never_split:
+                    split_tokens.append(token)
+                else:
+                    split_tokens += self.wordpiece_tokenizer.tokenize(token)
         else:
             split_tokens = self.wordpiece_tokenizer.tokenize(text)
         return split_tokens
