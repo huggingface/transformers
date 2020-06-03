8c6769ee41b373213103094519ad3f1856da4d43
sshleifer <sshleifer@gmail.com>
2020-06-03 13:04:02 +0000

    boom boom

---
+++ examples/summarization/bart/debug_distiller.sh
@@ -0,0 +1,20 @@
+export CURRENT_DIR=${PWD}
+#CNN_DIR = /home/shleifer/transformers_fork/examples/summarization/bart/cnn_dm
+
+# Make output directory if it doesn't exist
+mkdir -p $OUTPUT_DIR
+
+# Add parent directory to python path to access lightning_base.py
+export PYTHONPATH="../../":"${PYTHONPATH}"
+
+python -m pdb -c continue finetune.py \
+--data_dir=$CNN_DIR \
+--teacher=bart-large-cnn \
+--model_name_or_path=student \
+--learning_rate=3e-5 \
+--output_dir=$OUTPUT_DIR \
+--do_train \
+--do_predict \
+--fp16 \
+--val_check_interval 0.25 \
+$@
