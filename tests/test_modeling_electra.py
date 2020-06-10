929b83ff08a0f123a2135c8d412b27d0b34840d1
Suraj Patil <surajp815@gmail.com>
2020-06-10 22:47:45 +0530

    add test for electra qa model

--- tests/test_modeling_electra.py
+++ tests/test_modeling_electra.py
@@ -31,6 +31,7 @@ if is_torch_available():
         ElectraForTokenClassification,
         ElectraForPreTraining,
         ElectraForSequenceClassification,
+        ElectraForQuestionAnswering,
     )
     from transformers.modeling_electra import ELECTRA_PRETRAINED_MODEL_ARCHIVE_LIST
 
@@ -268,6 +269,36 @@ class ElectraModelTest(ModelTesterMixin, unittest.TestCase):
             self.parent.assertListEqual(list(result["logits"].size()), [self.batch_size, self.num_labels])
             self.check_loss_output(result)
 
+        def create_and_check_electra_for_question_answering(
+            self,
+            config,
+            input_ids,
+            token_type_ids,
+            input_mask,
+            sequence_labels,
+            token_labels,
+            choice_labels,
+            fake_token_labels,
+        ):
+            model = ElectraForQuestionAnswering(config=config)
+            model.to(torch_device)
+            model.eval()
+            loss, start_logits, end_logits = model(
+                input_ids,
+                attention_mask=input_mask,
+                token_type_ids=token_type_ids,
+                start_positions=sequence_labels,
+                end_positions=sequence_labels,
+            )
+            result = {
+                "loss": loss,
+                "start_logits": start_logits,
+                "end_logits": end_logits,
+            }
+            self.parent.assertListEqual(list(result["start_logits"].size()), [self.batch_size, self.seq_length])
+            self.parent.assertListEqual(list(result["end_logits"].size()), [self.batch_size, self.seq_length])
+            self.check_loss_output(result)
+
         def prepare_config_and_inputs_for_common(self):
             config_and_inputs = self.prepare_config_and_inputs()
             (
@@ -309,6 +340,11 @@ class ElectraModelTest(ModelTesterMixin, unittest.TestCase):
     def test_for_sequence_classification(self):
         config_and_inputs = self.model_tester.prepare_config_and_inputs()
         self.model_tester.create_and_check_electra_for_sequence_classification(*config_and_inputs)
+
+    def test_for_question_answering(self):
+        config_and_inputs = self.model_tester.prepare_config_and_inputs()
+        # self.model_tester.create_and_check_electra_for_question_answering(*config_and_inputs)
+        self.model_tester.create_and_check_electra_for_question_answering(*config_and_inputs)
 
     @slow
     def test_model_from_pretrained(self):
