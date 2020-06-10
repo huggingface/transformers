2e6b2d16bda19e7207cecb9330194fa6eef05794
Suraj Patil <surajp815@gmail.com>
2020-06-10 19:54:31 +0530

    update __init__

--- src/transformers/__init__.py
+++ src/transformers/__init__.py
@@ -243,6 +243,7 @@ if is_torch_available():
         BartForSequenceClassification,
         BartModel,
         BartForConditionalGeneration,
+        BartForQuestionAnswering,
         BART_PRETRAINED_MODEL_ARCHIVE_LIST,
     )
     from .modeling_marian import MarianMTModel
