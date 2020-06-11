f807de28f0ae9e3990e45de11ab0459f8d08bde3
Lysandre Debut <lysandre@huggingface.co>
2020-06-11 15:35:13 -0400

    Update src/transformers/modeling_mobilebert.py

--- src/transformers/modeling_mobilebert.py
+++ src/transformers/modeling_mobilebert.py
@@ -1,6 +1,6 @@
 # MIT License
 #
-# Copyright (c) 2020 lonePatient
+# Copyright (c) 2020  The Google AI Language Team Authors, The HuggingFace Inc. team and github/lonePatient
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
