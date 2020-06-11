4224ac999da59cd8a06a0771713b36156f7d1246
sshleifer <sshleifer@gmail.com>
2020-06-11 09:29:05 -0400

    boom boom

--- examples/summarization/bart/test_bart_examples.py
+++ examples/summarization/bart/test_bart_examples.py
@@ -281,6 +281,35 @@ class TestBartExamples(unittest.TestCase):
             fp16_opt_level="O1"
         )
         self._bart_distiller_cli(updates, check_contents=False)
+
+
+    def test_t5_base_fp16(self):
+        updates = dict(
+            model_type="t5",
+            model_name_or_path="t5-base",
+            do_train=True,
+            do_predict=True,
+            tokenizer_name="patrickvonplaten/t5-tiny-random",
+            no_teacher=True,
+            fp16=True,
+            gpus=1,
+            fp16_opt_level="O1"
+        )
+        self._bart_distiller_cli(updates, check_contents=False)
+    @unittest.skip('one sec')
+    def test_t5_base_eval_fp16_real_data(self):
+        updates = dict(
+            model_type="t5",
+            model_name_or_path="t5-base",
+            do_train=False,
+            do_predict=True,
+            tokenizer_name="patrickvonplaten/t5-tiny-random",
+            no_teacher=True,
+            fp16=True,
+            gpus=1,
+            fp16_opt_level="O1"
+        )
+        self._bart_distiller_cli(updates, check_contents=False)
     @unittest.skipUnless(False, "Not implemented")
     def test_bdc_mbart(self):
         pass
