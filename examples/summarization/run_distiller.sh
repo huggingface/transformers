9d99761b1bc1cb585e16a67af6548584c69291fa
sshleifer <sshleifer@gmail.com>
2020-06-11 22:55:51 -0400

    passing

--- examples/summarization/run_distiller.sh
+++ examples/summarization/run_distiller.sh
@@ -1,7 +1,7 @@
 #CNN_DIR = /home/shleifer/transformers_fork/examples/summarization/bart/cnn_dm
 
 # Add parent directory to python path to access lightning_base.py
-export PYTHONPATH="../../":"${PYTHONPATH}"
+export PYTHONPATH="../":"${PYTHONPATH}"
 
 python finetune.py \
 --learning_rate=3e-4 \
--- examples/summarization/run_eval.sh
+++ examples/summarization/run_eval.sh
@@ -1,5 +1,5 @@
 # Add parent directory to python path to access lightning_base.py
-export PYTHONPATH="../../":"${PYTHONPATH}"
+export PYTHONPATH="../":"${PYTHONPATH}"
 
 python finetune.py \
 --no_teacher \
