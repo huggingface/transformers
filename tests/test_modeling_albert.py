0a3bb4574a23342f5ee22da7ddf11a4bebac7467
aretius <amilaxelius@gmail.com>
2020-06-14 14:51:59 +0530

    fixed typo

--- tests/test_modeling_albert.py
+++ tests/test_modeling_albert.py
@@ -37,7 +37,7 @@ if is_torch_available():
     from transformers.modeling_albert import ALBERT_PRETRAINED_MODEL_ARCHIVE_LIST
 
 
-class AlbertModelTester():
+class AlbertModelTester:
     def __init__(
         self, parent,
     ):
@@ -212,16 +212,12 @@ class AlbertModelTester():
         model = AlbertForTokenClassification(config=config)
         model.to(torch_device)
         model.eval()
-        loss, logits = model(
-            input_ids, attention_mask=input_mask, token_type_ids=token_type_ids, labels=token_labels
-        )
+        loss, logits = model(input_ids, attention_mask=input_mask, token_type_ids=token_type_ids, labels=token_labels)
         result = {
             "loss": loss,
             "logits": logits,
         }
-        self.parent.assertListEqual(
-            list(result["logits"].size()), [self.batch_size, self.seq_length, self.num_labels]
-        )
+        self.parent.assertListEqual(list(result["logits"].size()), [self.batch_size, self.seq_length, self.num_labels])
         self.check_loss_output(result)
 
     def create_and_check_albert_for_multiple_choice(
