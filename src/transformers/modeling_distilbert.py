fdafefba4a94663297750a5faff3a7732f6f02fd
sgugger <sylvain.gugger@gmail.com>
2020-06-15 17:39:56 -0400

    Fix example

--- src/transformers/modeling_distilbert.py
+++ src/transformers/modeling_distilbert.py
@@ -923,8 +923,8 @@ class DistilBertForMultipleChoice(DistilBertPreTrainedModel):
         from transformers import DistilBertTokenizer, DistilBertForMultipleChoice
         import torch
 
-        tokenizer = DistilBertTokenizer.from_pretrained('bert-base-uncased')
-        model = DistilBertForMultipleChoice.from_pretrained('bert-base-uncased')
+        tokenizer = DistilBertTokenizer.from_pretrained('distilbert-base-cased')
+        model = DistilBertForMultipleChoice.from_pretrained('distilbert-base-cased')
 
         prompt = "In Italy, pizza served in formal settings, such as at a restaurant, is presented unsliced."
         choice0 = "It is eaten with a fork and a knife."
