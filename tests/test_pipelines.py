7de96853076e12c5362a04bfe79157bd8fad9ce3
Lorenzo Ampil <lorenzojulioampil@gmail.com>
2020-06-17 22:12:22 +0800

    Take out extra class accidentally added

--- tests/test_pipelines.py
+++ tests/test_pipelines.py
@@ -423,54 +423,6 @@ class QAPipelineTests(unittest.TestCase):
             self._test_qa_pipeline(nlp)
 
 
-class QAPipelineTests(unittest.TestCase):
-    def _test_qa_pipeline(self, nlp):
-        output_keys = {"score", "answer", "start", "end"}
-        valid_inputs = [
-            {"question": "Where was HuggingFace founded ?", "context": "HuggingFace was founded in Paris."},
-            {
-                "question": "In what field is HuggingFace working ?",
-                "context": "HuggingFace is a startup based in New-York founded in Paris which is trying to solve NLP.",
-            },
-        ]
-        invalid_inputs = [
-            {"question": "", "context": "This is a test to try empty question edge case"},
-            {"question": None, "context": "This is a test to try empty question edge case"},
-            {"question": "What is does with empty context ?", "context": ""},
-            {"question": "What is does with empty context ?", "context": None},
-        ]
-        self.assertIsNotNone(nlp)
-
-        mono_result = nlp(valid_inputs[0])
-        self.assertIsInstance(mono_result, dict)
-
-        for key in output_keys:
-            self.assertIn(key, mono_result)
-
-        multi_result = nlp(valid_inputs)
-        self.assertIsInstance(multi_result, list)
-        self.assertIsInstance(multi_result[0], dict)
-
-        for result in multi_result:
-            for key in output_keys:
-                self.assertIn(key, result)
-        for bad_input in invalid_inputs:
-            self.assertRaises(Exception, nlp, bad_input)
-        self.assertRaises(Exception, nlp, invalid_inputs)
-
-    @require_torch
-    def test_torch_question_answering(self):
-        for model_name in QA_FINETUNED_MODELS:
-            nlp = pipeline(task="question-answering", model=model_name, tokenizer=model_name)
-            self._test_qa_pipeline(nlp)
-
-    @require_tf
-    def test_tf_question_answering(self):
-        for model_name in QA_FINETUNED_MODELS:
-            nlp = pipeline(task="question-answering", model=model_name, tokenizer=model_name, framework="tf")
-            self._test_qa_pipeline(nlp)
-
-
 class PipelineCommonTests(unittest.TestCase):
 
     pipelines = SUPPORTED_TASKS.keys()
