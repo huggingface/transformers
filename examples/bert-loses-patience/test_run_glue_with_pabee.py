7ba1f4d59fcfdd93b6e94eeec8d753f02b09bb74
jetrunner <canwenxu@126.com>
2020-06-20 12:15:09 +0800

    Add a test

--- examples/bert-loses-patience/test_run_glue_with_pabee.py
+++ examples/bert-loses-patience/test_run_glue_with_pabee.py
@@ -33,8 +33,8 @@ class PabeeTests(unittest.TestCase):
             --do_train
             --do_eval
             --output_dir ./tests/fixtures/tests_samples/temp_dir
-            --per_device_train_batch_size=2
-            --per_device_eval_batch_size=1
+            --per_gpu_train_batch_size=2
+            --per_gpu_eval_batch_size=1
             --learning_rate=1e-4
             --max_steps=10
             --warmup_steps=2
