4d63b64f5cd33948ab0373b6118676e281e00030
i.boytsov <i.boytsov@MAC867.local>
2020-06-24 17:58:19 +0300

    add label smoothed loss

--- examples/summarization/utils.py
+++ examples/summarization/utils.py
@@ -246,3 +246,24 @@ def assert_not_all_frozen(model):
     model_grads: List[bool] = list(grad_status(model))
     npars = len(model_grads)
     assert any(model_grads), f"none of {npars} weights require grad"
+
+
+class LabelSmoothedCrossEntropyLoss(nn.Module):
+    """this loss performs label smoothing to compute cross-entropy with soft labels, when smoothing=0.0, this
+    is the same as torch.nn.CrossEntropyLoss"""
+
+    def __init__(self, n_classes, smoothing=0.0, dim=-1):
+        super().__init__()
+        self.confidence = 1.0 - smoothing
+        self.smoothing = smoothing
+        self.n_classes = n_classes
+        self.dim = dim
+
+    def forward(self, pred, target):
+        pred = pred.log_softmax(dim=self.dim)
+        with torch.no_grad():
+            true_dist = torch.zeros_like(pred)
+            true_dist.fill_(self.smoothing / (self.n_classes - 1))
+            true_dist.scatter_(1, target.data.unsqueeze(1), self.confidence)
+
+        return torch.mean(torch.sum(-true_dist * pred, dim=self.dim))
