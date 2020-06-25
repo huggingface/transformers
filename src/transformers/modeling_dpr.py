5a3f14e75222422454536e9db6efa40cca7b8838
Quentin Lhoest <lhoest.q@gmail.com>
2020-06-25 16:10:07 +0200

    minor

--- src/transformers/modeling_dpr.py
+++ src/transformers/modeling_dpr.py
@@ -12,11 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-""" PyTorch DPR model. """
-
-####################################################
-# In this template, replace all the DPR (various casings) with your model name
-####################################################
+""" PyTorch DPR model for Open Domain Question Answering."""
 
 
 import collections
