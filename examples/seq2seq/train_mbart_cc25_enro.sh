957212240b36e96d3824b7bd0aeebe848c31c4c8
sshleifer <sshleifer@gmail.com>
2020-07-02 20:08:41 +0000

    max_source_length

--- examples/seq2seq/train_mbart_cc25_enro.sh
+++ examples/seq2seq/train_mbart_cc25_enro.sh
@@ -11,7 +11,7 @@ python finetune.py \
     --n_val 500 \
     --num_train_epochs 2 --src_lang en_XX --tgt_lang ro_RO \
     --freeze_encoder --freeze_embeds --data_dir $ENRO_DIR \
-    --max_target_length 300 --val_max_target_length=300 --test_max_target_length 300 \
+    --max_source_length=300 --max_target_length 300 --val_max_target_length=300 --test_max_target_length 300 \
     --train_batch_size=$BS --eval_batch_size=$BS --gradient_accumulation_steps=$GAS \
     --model_name_or_path facebook/mbart-large-cc25 \
     --task translation \
