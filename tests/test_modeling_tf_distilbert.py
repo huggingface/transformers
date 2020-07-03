c75b9060767d6a1731b27e01f83a330a76237b81
Patrick von Platen <patrick.v.platen@gmail.com>
2020-07-03 12:22:51 +0200

    fix new test utils import

--- tests/test_modeling_tf_distilbert.py
+++ tests/test_modeling_tf_distilbert.py
@@ -17,11 +17,10 @@
 import unittest
 
 from transformers import DistilBertConfig, is_tf_available
-from transformers.testing_utils import require_tf
+from transformers.testing_utils import require_tf, slow
 
 from .test_configuration_common import ConfigTester
 from .test_modeling_tf_common import TFModelTesterMixin, ids_tensor
-from .utils import require_tf, slow
 
 
 if is_tf_available():
