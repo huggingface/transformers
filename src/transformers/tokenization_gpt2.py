d6b0b9d451e7ffe0da72a5f532cc9bec1563e801
Lysandre Debut <lysandre@huggingface.co>
2020-07-06 11:33:57 -0400

    GPT2 tokenizer should not output token type IDs (#5546)
    * GPT2 tokenizer should not output token type IDs
    
    * Same for OpenAIGPT

--- src/transformers/tokenization_gpt2.py
+++ src/transformers/tokenization_gpt2.py
@@ -137,6 +137,7 @@ class GPT2Tokenizer(PreTrainedTokenizer):
     vocab_files_names = VOCAB_FILES_NAMES
     pretrained_vocab_files_map = PRETRAINED_VOCAB_FILES_MAP
     max_model_input_sizes = PRETRAINED_POSITIONAL_EMBEDDINGS_SIZES
+    model_input_names = ["attention_mask"]
 
     def __init__(
         self,
@@ -330,6 +331,7 @@ class GPT2TokenizerFast(PreTrainedTokenizerFast):
     vocab_files_names = VOCAB_FILES_NAMES
     pretrained_vocab_files_map = PRETRAINED_VOCAB_FILES_MAP
     max_model_input_sizes = PRETRAINED_POSITIONAL_EMBEDDINGS_SIZES
+    model_input_names = ["attention_mask"]
 
     def __init__(
         self,
