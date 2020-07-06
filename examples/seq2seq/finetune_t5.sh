c38dcfa57e1b6922a2a4b1cd8024a1da6b679583
sshleifer <sshleifer@gmail.com>
2020-07-05 21:52:55 -0400

    t5 script

--- examples/seq2seq/finetune_t5.sh
+++ examples/seq2seq/finetune_t5.sh
@@ -1,18 +1,14 @@
-export OUTPUT_DIR_NAME=t5
-export CURRENT_DIR=${PWD}
-export OUTPUT_DIR=${CURRENT_DIR}/${OUTPUT_DIR_NAME}
-
-# Make output directory if it doesn't exist
-mkdir -p $OUTPUT_DIR
-
 # Add parent directory to python path to access lightning_base.py
 export PYTHONPATH="../":"${PYTHONPATH}"
 
 python finetune.py \
---data_dir=./cnn-dailymail/cnn_dm \
---model_name_or_path=t5-large \
+--data_dir=$CNN_DIR \
+--model_name_or_path=t5-base \
 --learning_rate=3e-5 \
---train_batch_size=4 \
---eval_batch_size=4 \
+--train_batch_size=$BS \
+--eval_batch_size=$BS \
 --output_dir=$OUTPUT_DIR \
---do_train  $@
+--max_source_length=512 \
+--freeze_encoder --freeze_embeds \
+--do_train --do_predict \
+ $@
