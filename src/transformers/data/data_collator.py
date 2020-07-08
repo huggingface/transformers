e8ff5c6fed8c85879a3d5522ecaec23a9e859bf0
Luna Fu <yfuyu@ebay.com>
2020-07-08 18:00:12 +0800

    add data collator with attention_mask feature

--- src/transformers/data/data_collator.py
+++ src/transformers/data/data_collator.py
@@ -137,6 +137,80 @@ class DataCollatorForLanguageModeling:
 
 
 @dataclass
+class DataCollatorForMaskedLanguageModeling:
+    """
+    Data collator used for masked language modeling.
+    - collates batches of tensors, honoring their tokenizer's pad_token
+    - preprocesses batches for masked language modeling
+    """
+
+    tokenizer: PreTrainedTokenizer
+    mlm: bool = True
+    mlm_probability: float = 0.15
+
+    def __call__(self, examples: List[List[torch.Tensor]]) -> Dict[str, torch.Tensor]:
+        input_ids = self._tensorize_batch([e[0] for e in examples], self.tokenizer.pad_token_id)
+        token_type_ids = self._tensorize_batch([e[1] for e in examples], 0)
+        attention_mask = self._tensorize_batch([e[2] for e in examples], 0)
+
+        if self.mlm:
+            input_ids, labels = self.mask_tokens(input_ids)
+        else:
+            labels = input_ids.clone().detach()
+            labels[labels == self.tokenizer.pad_token_id] = -100
+
+        return {"input_ids": input_ids, "token_type_ids": token_type_ids, "attention_mask": attention_mask, "labels": labels}
+
+    def _tensorize_batch(self, batch: List[torch.Tensor], pad_token: int) -> torch.Tensor:
+        length_of_first = batch[0].size(0)
+        are_tensors_same_length = all(x.size(0) == length_of_first for x in batch)
+        if are_tensors_same_length:
+            return torch.stack(batch, dim=0)
+        else:
+            if self.tokenizer._pad_token is None:
+                raise ValueError(
+                    "You are attempting to pad samples but the tokenizer you are using"
+                    f" ({self.tokenizer.__class__.__name__}) does not have one."
+                )
+            return pad_sequence(batch, batch_first=True, padding_value=pad_token)
+
+    def mask_tokens(self, inputs: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
+        """
+        Prepare masked tokens inputs/labels for masked language modeling: 80% MASK, 10% random, 10% original.
+        """
+
+        if self.tokenizer.mask_token is None:
+            raise ValueError(
+                "This tokenizer does not have a mask token which is necessary for masked language modeling. Remove the --mlm flag if you want to use this tokenizer."
+            )
+
+        labels = inputs.clone()
+        # We sample a few tokens in each sequence for masked-LM training (with probability args.mlm_probability defaults to 0.15 in Bert/RoBERTa)
+        probability_matrix = torch.full(labels.shape, self.mlm_probability)
+        special_tokens_mask = [
+            self.tokenizer.get_special_tokens_mask(val, already_has_special_tokens=True) for val in labels.tolist()
+        ]
+        probability_matrix.masked_fill_(torch.tensor(special_tokens_mask, dtype=torch.bool), value=0.0)
+        if self.tokenizer._pad_token is not None:
+            padding_mask = labels.eq(self.tokenizer.pad_token_id)
+            probability_matrix.masked_fill_(padding_mask, value=0.0)
+        masked_indices = torch.bernoulli(probability_matrix).bool()
+        labels[~masked_indices] = -100  # We only compute loss on masked tokens
+
+        # 80% of the time, we replace masked input tokens with tokenizer.mask_token ([MASK])
+        indices_replaced = torch.bernoulli(torch.full(labels.shape, 0.8)).bool() & masked_indices
+        inputs[indices_replaced] = self.tokenizer.convert_tokens_to_ids(self.tokenizer.mask_token)
+
+        # 10% of the time, we replace masked input tokens with random word
+        indices_random = torch.bernoulli(torch.full(labels.shape, 0.5)).bool() & masked_indices & ~indices_replaced
+        random_words = torch.randint(len(self.tokenizer), labels.shape, dtype=torch.long)
+        inputs[indices_random] = random_words[indices_random]
+
+        # The rest of the time (10% of the time) we keep the masked input tokens unchanged
+        return inputs, labels
+
+
+@dataclass
 class DataCollatorForPermutationLanguageModeling:
     """
     Data collator used for permutation language modeling.
