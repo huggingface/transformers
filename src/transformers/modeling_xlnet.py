98915edf4d1cc9524fcd48dadd8e9372f6c6fa8d
Teven <teven.lescao@gmail.com>
2020-07-10 17:38:36 +0200

    Fixed use of memories in XLNet (caching for language generation + warning when loading improper memoryless model) (#5632)
    * Pytorch gpu => cpu proper device
    
    * Memoryless XLNet warning + fixed memories during generation
    
    * Revert "Pytorch gpu => cpu proper device"
    
    This reverts commit 93489b36
    
    * made black happy
    
    * TF generation with memories
    
    * dim => axis
    
    * added padding_text to TF XL models
    
    * Added comment, added TF

--- src/transformers/modeling_xlnet.py
+++ src/transformers/modeling_xlnet.py
@@ -1261,7 +1261,16 @@ class XLNetLMHeadModel(XLNetPreTrainedModel):
 
         effective_batch_size = input_ids.shape[0]
         dummy_token = torch.zeros((effective_batch_size, 1), dtype=torch.long, device=input_ids.device)
-        input_ids = torch.cat([input_ids, dummy_token], dim=1)
+
+        # At every pass, the attention values for the new token and the two last generated tokens
+        # are computed, the rest is reloaded from the `past` cache. A purely auto-regressive model would have
+        # offset = 1; offset = 2 seems to have slightly better computation.
+        offset = 2
+
+        if past:
+            input_ids = torch.cat([input_ids[:, -offset:], dummy_token], dim=1)
+        else:
+            input_ids = torch.cat([input_ids, dummy_token], dim=1)
 
         # Build permutation mask so that previous tokens don't see last token
         sequence_length = input_ids.shape[1]
@@ -1285,7 +1294,7 @@ class XLNetLMHeadModel(XLNetPreTrainedModel):
 
         # if past is defined in model kwargs then use it for faster decoding
         if past:
-            inputs["mems"] = past
+            inputs["mems"] = tuple(layer_past[:-offset, :, :] for layer_past in past)
 
         return inputs
 
