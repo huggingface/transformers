fc906f7c65236d43d29a7dbf27b77f7bcd5ec4e4
Stas Bekman <stas@stason.org>
2020-07-09 18:17:54 -0700

    make isort happy

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,6 +4,7 @@
 import sys
 from os.path import abspath, dirname, join
 
+
 # allow having multiple repository checkouts and not needing to remember to rerun
 # 'pip install -e .[dev]' when switching between checkouts and running tests.
 git_repo_path = abspath(join(dirname(dirname(__file__)), "src"))
