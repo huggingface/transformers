78166e6dc3a5670db5e6114db691c19f4fb33d99
Patrick von Platen <patrick.v.platen@gmail.com>
2020-07-13 18:34:51 +0000

    revert wrong changes

--- src/transformers/generation_tf_utils.py
+++ src/transformers/generation_tf_utils.py
@@ -39,7 +39,6 @@ class TFGenerationMixin:
             return False
         return True
 
-    @tf.function
     def generate(
         self,
         input_ids=None,
@@ -438,8 +437,7 @@ class TFGenerationMixin:
 
         past = encoder_outputs  # defined for encoder-decoder models, None for decoder-only models
 
-        cur_len = tf.constant(cur_len, dtype=tf.int32)
-        while tf.less(cur_len, max_length):
+        while cur_len < max_length:
             model_inputs = self.prepare_inputs_for_generation(
                 input_ids, past=past, attention_mask=attention_mask, use_cache=use_cache
             )
@@ -621,8 +619,7 @@ class TFGenerationMixin:
         # done sentences
         done = [False for _ in range(batch_size)]
 
-        cur_len = tf.constant(cur_len, dtype=tf.int32)
-        while tf.less(cur_len, max_length):
+        while cur_len < max_length:
             model_inputs = self.prepare_inputs_for_generation(
                 input_ids, past=past, attention_mask=attention_mask, use_cache=use_cache
             )
