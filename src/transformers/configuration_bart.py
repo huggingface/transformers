3e933766c920fa41b677afb91c5013bbfbf21dbd
Sam Shleifer <sshleifer@gmail.com>
2020-07-13 10:20:50 -0400

    Fix is_valid_mbart

--- src/transformers/configuration_bart.py
+++ src/transformers/configuration_bart.py
@@ -141,9 +141,9 @@ class BartConfig(PretrainedConfig):
 
     def is_valid_mbart(self) -> bool:
         """Is the configuration aligned with the MBART paper."""
-        if self.normalize_before and self.add_final_layer_norm and self.scale_embedding:
+        if self.variant == 'prelayernorm' and self.add_final_layer_norm and self.scale_embedding:
             return True
-        if self.normalize_before or self.add_final_layer_norm or self.scale_embedding:
+        if self.variant == 'prelayernorm' or self.add_final_layer_norm or self.scale_embedding:
             logger.info("This configuration is a mixture of MBART and BART settings")
         return False
 
