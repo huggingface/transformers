11269ec97130d896e05748adec2cad2766b138cf
patrickvonplaten <patrick.v.platen@gmail.com>
2020-07-15 01:40:00 +0200

    fix use_cache for False case

--- src/transformers/modeling_reformer.py
+++ src/transformers/modeling_reformer.py
@@ -115,6 +115,23 @@ def _get_least_common_mult_chunk_len(config):
         )
 
 
+def _get_min_chunk_len(config):
+    attn_types = config.attn_layers
+    attn_types_set = set(attn_types)
+    if len(attn_types_set) == 1 and attn_types[0] == "lsh":
+        return config.lsh_attn_chunk_length
+    elif len(attn_types_set) == 1 and attn_types[0] == "local":
+        return config.local_attn_chunk_length
+    elif len(attn_types_set) == 2 and attn_types_set == set(["lsh", "local"]):
+        return min(config.lsh_attn_chunk_length, config.local_attn_chunk_length)
+    else:
+        raise NotImplementedError(
+            "Only attn layer types 'lsh' and 'local' exist, but `config.attn_layers`: {}. Select attn layer types from ['lsh', 'local'] only.".format(
+                config.attn_layers
+            )
+        )
+
+
 class AxialPositionEmbeddings(nn.Module):
     """Constructs axial position embeddings. Useful for very long input
     sequences to save memory and time.
@@ -512,7 +529,6 @@ class LSHSelfAttention(nn.Module, EfficientAttentionMixin):
             # cluster query key value vectors according to hashed buckets
             query_key_vectors = self._gather_by_expansion(query_key_vectors, sorted_bucket_idx_per_hash, num_hashes)
             value_vectors = self._gather_by_expansion(value_vectors, sorted_bucket_idx_per_hash, num_hashes)
-
             query_key_vectors = self._split_seq_length_dim_to(
                 query_key_vectors, -1, self.chunk_length, self.num_attention_heads, self.attention_head_size,
             )
@@ -1988,9 +2004,11 @@ class ReformerModel(ReformerPreTrainedModel):
 
         # if needs padding
         least_common_mult_chunk_length = _get_least_common_mult_chunk_len(self.config)
+        min_chunk_length = _get_min_chunk_len(self.config)
+
         must_pad_to_match_chunk_length = (
             input_shape[-1] % least_common_mult_chunk_length != 0
-            and input_shape[-1] > least_common_mult_chunk_length
+            and input_shape[-1] > min_chunk_length
             and cached_hidden_states_and_buckets is None
         )
 
