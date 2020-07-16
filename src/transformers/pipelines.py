92c042bb18a285f492b1716019233f0c993beef4
Guillaume B <guillaume.becquin@gmail.com>
2020-07-16 21:26:23 +0200

    - Updated attention_mask value for Tensorflow compatibility

--- src/transformers/pipelines.py
+++ src/transformers/pipelines.py
@@ -1907,7 +1907,9 @@ class ConversationalPipeline(Pipeline):
             outputs.append(input)
         max_len = max([len(item) for item in outputs])
         outputs = [output + [self.pad_token_id] * (max_len - len(output)) for output in outputs]
-        outputs = BatchEncoding({"input_ids": outputs, "attention_mask": []}, tensor_type=self.framework)
+        outputs = BatchEncoding(
+            {"input_ids": outputs, "attention_mask": [1] * len(outputs)}, tensor_type=self.framework
+        )
         return outputs
 
 
