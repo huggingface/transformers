7c2d5792d1a99c210cd56022f6e8756e7f3b0f46
Colanim <nicolas.remond@42maru.com>
2020-07-16 17:58:36 +0900

    :rotating_light: Fix black warning

--- src/transformers/training_args_tf.py
+++ src/transformers/training_args_tf.py
@@ -100,12 +100,9 @@ class TFTrainingArguments(TrainingArguments):
     tpu_name: str = field(
         default=None, metadata={"help": "Name of TPU"},
     )
-        
+
     poly_power: float = field(
-        default=1.0,
-        metadata={
-            "help": "Power for the Polynomial decay LR scheduler."
-        },
+        default=1.0, metadata={"help": "Power for the Polynomial decay LR scheduler."},
     )
 
     @cached_property
