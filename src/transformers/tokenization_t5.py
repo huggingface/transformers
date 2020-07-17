04f18d40e3cf165543fa5b0f456cc61bc156e427
Sam Shleifer <sshleifer@gmail.com>
2020-07-17 15:26:37 -0400

    No bos

--- src/transformers/tokenization_t5.py
+++ src/transformers/tokenization_t5.py
@@ -19,6 +19,7 @@ import logging
 import os
 import re
 from shutil import copyfile
+from typing import List, Optional
 
 from .tokenization_utils import PreTrainedTokenizer
 
@@ -58,7 +59,6 @@ PRETRAINED_POSITIONAL_EMBEDDINGS_SIZES = {
     "t5-11b": 512,
 }
 
-from typing import List, Optional
 
 class T5Tokenizer(PreTrainedTokenizer):
     """
@@ -146,14 +146,47 @@ class T5Tokenizer(PreTrainedTokenizer):
         vocab.update(self.added_tokens_encoder)
         return vocab
 
-    def build_inputs_with_special_tokens(self, token_ids_0: List[int], token_ids_1: Optional[List[int]] = None) -> List[int]:
+    def get_special_tokens_mask(
+        self, token_ids_0: List[int], token_ids_1: Optional[List[int]] = None, already_has_special_tokens: bool = False
+    ) -> List[int]:
+        """
+        Retrieves sequence ids from a token list that has no special tokens added. This method is called when adding
+        special tokens using the tokenizer ``prepare_for_model`` method.
+
+        Args:
+            token_ids_0 (:obj:`List[int]`):
+                List of ids.
+            token_ids_1 (:obj:`List[int]`, `optional`, defaults to :obj:`None`):
+                Optional second list of IDs for sequence pairs.
+            already_has_special_tokens (:obj:`bool`, `optional`, defaults to :obj:`False`):
+                Set to True if the token list is already formatted with special tokens for the model
+
+        Returns:
+            :obj:`List[int]`: A list of integers in the range [0, 1]: 1 for a special token, 0 for a sequence token.
+        """
+        if already_has_special_tokens:
+            if token_ids_1 is not None:
+                raise ValueError(
+                    "You should not supply a second sequence if the provided sequence of "
+                    "ids is already formatted with special tokens for the model."
+                )
+            return list(map(lambda x: 1 if x in [self.sep_token_id, self.cls_token_id] else 0, token_ids_0))
+
+        if token_ids_1 is None:
+            return ([0] * len(token_ids_0)) + [1]
+        return ([0] * len(token_ids_0)) + [1] + ([0] * len(token_ids_1)) + [1]
+
+    def build_inputs_with_special_tokens(
+        self, token_ids_0: List[int], token_ids_1: Optional[List[int]] = None
+    ) -> List[int]:
         """
         Build model inputs from a sequence or a pair of sequence for sequence classification tasks
         by concatenating and adding special tokens.
+        For some t5 tasks, model.config.prefix is specified. This must be used before tokenization.
         A sequence has the following format:
 
-        - single sequence: ``<s> X </s>``
-        - pair of sequences: ``<s> A </s></s> B </s>``
+        - single sequence: ``X </s>``
+        - pair of sequences: ``A </s> B </s>``
 
         Args:
             token_ids_0 (:obj:`List[int]`):
@@ -164,11 +197,10 @@ class T5Tokenizer(PreTrainedTokenizer):
         Returns:
             :obj:`List[int]`: list of `input IDs <../glossary.html#input-ids>`__ with the appropriate special tokens.
         """
-        bos = []#DELETE IF NOT NEEDED
         eos = [self.eos_token_id]
         if token_ids_1 is None:
-            return bos + token_ids_0 + eos
-        return bos + token_ids_0 + eos + eos + token_ids_1 + eos
+            return token_ids_0 + eos
+        return token_ids_0 + eos + token_ids_1 + eos
 
     def __getstate__(self):
         state = self.__dict__.copy()
