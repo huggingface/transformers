d0c53ad1f6e07fe6ec91f83ea2641e2b4b55736e
jetrunner <canwenxu@126.com>
2020-07-21 11:05:28 +0800

    readme

---
+++ src/transformers/theseus/README.md
@@ -0,0 +1,28 @@
+## transformers.theseus
+![BERT of Theseus](https://github.com/JetRunner/BERT-of-Theseus/blob/master/bert-of-theseus.png?raw=true)
+
+`transformers.theseus` is an implementation of [Theseus Compression](https://arxiv.org/abs/2002.02925).
+Theseus compression exploits module replacing to compress a large model to a small one.
+We implement [LayerDrop](https://arxiv.org/abs/1909.11556) and [Mixout](https://arxiv.org/abs/1909.11299) with the framework as well.
+
+### Run BERT-of-Theseus
+TBA.
+
+### Run LayerDrop
+TBA.
+
+### Run Mixout
+TBA.
+
+### Citation
+Please consider citing this paper for the `theseus` framework and BERT-of-Theseus:
+```bibtex
+@misc{xu2020bertoftheseus,
+    title={BERT-of-Theseus: Compressing BERT by Progressive Module Replacing},
+    author={Canwen Xu and Wangchunshu Zhou and Tao Ge and Furu Wei and Ming Zhou},
+    year={2020},
+    eprint={2002.02925},
+    archivePrefix={arXiv},
+    primaryClass={cs.CL}
+}
+```
--- tests/test_theseus.py
+++ tests/test_theseus.py
@@ -33,7 +33,7 @@ class TheseusTest(unittest.TestCase):
         self.assertTrue(prd_check)
         self.assertTrue(scc_check)
 
-    def test_theseus_list(self):
+    def test_theseus_list_one_to_one(self):
         prd_module_list = torch.nn.ModuleList()
         scc_module_list = torch.nn.ModuleList()
         theseus_module_list = theseus.TheseusList()
@@ -69,6 +69,35 @@ class TheseusTest(unittest.TestCase):
                     prd_num += 1
         self.assertLess(scc_num / (prd_num + scc_num), 1)
         self.assertGreater(scc_num / (prd_num + scc_num), 0)
+
+    def test_theseus_list_many_to_one(self):
+        scc_module_list = torch.nn.ModuleList()
+        theseus_module_list = theseus.TheseusList()
+        for i in range(10):
+            prd_module = torch.nn.ModuleList([torch.nn.Linear(1, i), torch.nn.Linear(2, i)])
+            scc_module = torch.nn.Linear(3, i)
+            theseus_module = theseus.TheseusModule(predecessor=prd_module, successor=scc_module)
+            scc_module_list.append(scc_module)
+            theseus_module_list.append(theseus_module)
+        for _ in range(10):
+            sampled_list = theseus_module_list.sample_and_pass()
+            self.assertEqual(len(sampled_list), 20)
+
+        theseus_module_list.set_replacing_rate(1)
+        for _ in range(10):
+            sampled_list = theseus_module_list.sample_and_pass()
+            self.assertEqual(len(sampled_list), 10)
+            for i in range(10):
+                self.assertEqual(scc_module_list[i], sampled_list[i])
+
+        theseus_module_list.set_replacing_rate(0.5)
+        len_sum = 0
+        for _ in range(100):
+            sampled_list = theseus_module_list.sample_and_pass()
+            len_sum += len(sampled_list)
+        len_avg = len_sum / 100
+        self.assertGreater(len_avg, 10)
+        self.assertLess(len_avg, 20)
 
     def test_layerdrop_list(self):
         layers = torch.nn.ModuleList([torch.nn.Linear(1, i) for i in range(12)])
