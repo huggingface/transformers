10f12b82c150231edeeed44f319597ec88154dde
Sam Shleifer <sshleifer@gmail.com>
2020-07-22 14:35:29 -0400

    simplify

--- examples/seq2seq/convert_model_to_fp16.py
+++ examples/seq2seq/convert_model_to_fp16.py
@@ -1,35 +1,20 @@
-import argparse
-from typing import Dict
+from typing import Union
 
+import fire
 import torch
 from tqdm import tqdm
 
 
-def convert(dct) -> Dict:
-    for k, v in tqdm(dct.items()):
-        try:
-            dct[k] = v.half()
-        except Exception:
-            continue
-        if isinstance(v, dict):
-            dct[k] = convert(v)
-        elif isinstance(v, list) or isinstance(v, tuple):
-            dct[k] = type(v)(convert(x) for x in v)
-        else:  # unconvertable
-            dct[k] = v
-    return dct
-
-
-def fp16_cli():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--src_path", type=str, help="like facebook/bart-large-cnn,t5-base, etc.")
-    parser.add_argument("--save_path", default=None, type=str)
-    parser.add_argument("--map_location", default="cpu", type=str)
-    args = parser.parse_args()
-    dct = convert(torch.load(args.src_path, map_location=args.map_location))
-    save_path = args.src_path if args.save_path is None else args.save_path
-    torch.save(dct, save_path)
+def convert(src_path: str, map_location: str = "cpu", save_path: Union[str, None] = None) -> None:
+    """Convert a pytorch_model.bin or model.pt file to torch.float16 for faster downloads, less disk space."""
+    state_dict = torch.load(src_path, map_location=map_location)
+    for k, v in tqdm(state_dict.items()):
+        if not isinstance(v, torch.Tensor):
+            raise TypeError("FP16 conversion only works on paths that are saved state dics, like pytorch_model.bin")
+        state_dict[k] = v.half()
+    if save_path is None:  # overwrite
+        torch.save(state_dict, src_path)
 
 
 if __name__ == "__main__":
-    fp16_cli()
+    fire.Fire(convert)
