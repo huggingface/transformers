c49cd927f7b59fc1308ff8073bde31ddeb15eed2
Sam Shleifer <sshleifer@gmail.com>
2020-07-28 18:29:35 -0400

    [Fix] position_ids tests again (#6100)

--- tests/test_modeling_auto.py
+++ tests/test_modeling_auto.py
@@ -88,9 +88,11 @@ class AutoModelTest(unittest.TestCase):
             model, loading_info = AutoModelForPreTraining.from_pretrained(model_name, output_loading_info=True)
             self.assertIsNotNone(model)
             self.assertIsInstance(model, BertForPreTraining)
+            # Only one value should not be initialized and in the missing keys.
+            missing_keys = loading_info.pop("missing_keys")
+            self.assertListEqual(["cls.predictions.decoder.bias"], missing_keys)
             for key, value in loading_info.items():
-                # Only one value should not be initialized and in the missing keys.
-                self.assertEqual(len(value), 1 if key == "missing_keys" else 0)
+                self.assertEqual(len(value), 0)
 
     @slow
     def test_lmhead_model_from_pretrained(self):
