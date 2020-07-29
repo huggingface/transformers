da9a24d77ec9192fd57c30557c70023b889c69dd
Sylvain Gugger <35901082+sgugger@users.noreply.github.com>
2020-07-29 12:28:12 -0400

    Use google style to document properties (#6130)
    * Use google style to document properties
    
    * Update src/transformers/configuration_utils.py
    
    Co-authored-by: Lysandre Debut <lysandre@huggingface.co>
    
    Co-authored-by: Lysandre Debut <lysandre@huggingface.co>

--- src/transformers/modeling_tf_utils.py
+++ src/transformers/modeling_tf_utils.py
@@ -238,10 +238,7 @@ class TFPreTrainedModel(tf.keras.Model, TFModelUtilsMixin, TFGenerationMixin):
     @property
     def dummy_inputs(self) -> Dict[str, tf.Tensor]:
         """
-        Dummy inputs to build the network.
-
-        Returns:
-            :obj:`Dict[str, tf.Tensor]`: The dummy inputs.
+        :obj:`Dict[str, tf.Tensor]`: Dummy inputs to build the network.
         """
         return {"input_ids": tf.constant(DUMMY_INPUTS)}
 
