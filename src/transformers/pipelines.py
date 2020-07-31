cfd9b5dda045e7389d132d5d9b9317d30465e3b0
sgugger <sylvain.gugger@gmail.com>
2020-07-31 10:57:01 -0400

    Work in progress

--- src/transformers/pipelines.py
+++ src/transformers/pipelines.py
@@ -711,6 +711,9 @@ class FeatureExtractionPipeline(Pipeline):
         )
 
     def __call__(self, *args, **kwargs):
+        """
+        
+        """
         return super().__call__(*args, **kwargs).tolist()
 
 
