4fbcde518f6c6a6b76a74c1e05e13b22f810e4e9
Bhashithe Abeysinghe <bhashithe@gmail.com>
2020-07-31 10:42:48 -0400

    Update examples/text-classification/run_pl_glue.py
    Co-authored-by: Sam Shleifer <sshleifer@gmail.com>

--- examples/text-classification/run_pl_glue.py
+++ examples/text-classification/run_pl_glue.py
@@ -76,7 +76,7 @@ class GLUETransformer(BaseTransformer):
                 logger.info("Saving features into cached file %s", cached_features_file)
                 torch.save(features, cached_features_file)
 
-    def get_dataloader(self, mode, batch_size):
+    def get_dataloader(self, mode: int, batch_size: int, shuffle: bool) -> DataLoader:
         "Load datasets. Called after prepare data."
 
         # We test on dev set to compare to benchmarks without having to submit to GLUE server
