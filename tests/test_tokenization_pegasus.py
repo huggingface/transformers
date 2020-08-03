5fab81717fbe1ffb346a00a1f6da2badc689ab23
Sam Shleifer <sshleifer@gmail.com>
2020-08-03 15:29:47 -0400

    8 tok tests failing

--- tests/test_tokenization_pegasus.py
+++ tests/test_tokenization_pegasus.py
@@ -16,14 +16,13 @@ class PegasusTokenizationTest(TokenizerTesterMixin, unittest.TestCase):
         #vocab = ["</s>", "<unk>", "▁This", "▁is", "▁a", "▁t", "est", "\u0120", "<pad>"]
         #vocab_tokens = dict(zip(vocab, range(len(vocab))))
         save_dir = Path(self.tmpdirname)
-        #save_json(vocab_tokens, save_dir / vocab_files_names["vocab"])
-        #save_json(mock_tokenizer_config, save_dir / vocab_files_names["tokenizer_config_file"])\
         spm_file = PegasusTokenizer.vocab_files_names["vocab_file"]
         if not (save_dir / spm_file).exists():
             copyfile(SAMPLE_SP, save_dir / spm_file)
+        tokenizer = PegasusTokenizer(spm_file)
 
-        tokenizer = PegasusTokenizer.from_pretrained('sshleifer/pegasus')
-        assert tokenizer.vocab_size == 96103
+        #tokenizer = PegasusTokenizer.from_pretrained('sshleifer/pegasus')
+        #assert tokenizer.vocab_size == 96103
         tokenizer.save_pretrained(self.tmpdirname)
 
     def get_tokenizer(self, **kwargs) -> PegasusTokenizer:
