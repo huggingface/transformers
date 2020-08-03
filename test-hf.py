6945fb15624178e75382d0b55207106b460040ac
Guillem Orellana Trullols <guillem.orellana@gmail.com>
2020-08-03 13:48:16 +0200

    Delete test-hf.py

--- test-hf.py
+++
@@ -1,9 +0,0 @@
-import transformers
-import tensorflow as tf
-
-model = transformers.TFT5ForConditionalGeneration.from_pretrained('t5-small')
-input_ids = tf.random.uniform([1, 512], maxval=1000, dtype=tf.int32)
-labels = tf.random.uniform([1, 512], maxval=1000, dtype=tf.int32)
-
-loss, *_ = model(dict(input_ids=input_ids, labels=labels))
-print(loss)
