eae4bc129df0633a3b5fdaf06feda5cdb4bd3fee
v-kasong <v-kasong@microsoft.com>
2020-08-05 11:44:13 +0000

    Update configuration

--- src/transformers/configuration_mpnet.py
+++ src/transformers/configuration_mpnet.py
@@ -1,12 +1,18 @@
 import logging
 
+from .configuration_utils import PretrainedConfig
 from .configuration_bert import BertConfig
 
 
 logger = logging.getLogger(__name__)
 
 
-class MPNetConfig(BertConfig):
+MPNET_PRETRAINED_CONFIG_ARCHIVE_MAP = {
+}
+
+
+
+class MPNetConfig(PretrainedConfig):
     r"""
         :class:`~transformers.MPNetConfig` is the configuration class to store the configuration of a
         `MPNetModel`.
@@ -34,13 +40,38 @@ class MPNetConfig(BertConfig):
                 initializing all weight matrices.
             layer_norm_eps: The epsilon used by LayerNorm.
     """
+    pretrained_config_archive_map = MPNET_PRETRAINED_CONFIG_ARCHIVE_MAP
     model_type = "mpnet"
 
     def __init__(
         self,
+        vocab_size=30527,
+        hidden_size=768,
+        num_hidden_layers=12,
+        num_attention_heads=12,
+        intermediate_size=3072,
+        hidden_act="gelu",
+        hidden_dropout_prob=0.1,
+        attention_probs_dropout_prob=0.1,
+        max_position_embeddings=512,
+        initializer_range=0.02,
+        layer_norm_eps=1e-12,
         pad_token_id=1,
         bos_token_id=0,
         eos_token_id=2,
         **kwargs
     ):
-        super().__init__(pad_token_id=pad_token_id, bos_token_id=bos_token_id, eos_token_id=eos_token_id, **kwargs)
+        super().__init__(pad_token_id=pad_token_id, bos_token_id=bos_token, eos_token_id=eos_token_id, **kwargs)
+
+        self.vocab_size = vocab_size
+        self.hidden_size = hidden_size
+        self.num_hidden_layers = num_hidden_layers
+        self.num_attention_heads = num_attention_heads
+        self.hidden_act = hidden_act
+        self.intermediate_size = intermediate_size
+        self.hidden_dropout_prob = hidden_dropout_prob
+        self.attention_probs_dropout_prob = attention_probs_dropout_prob
+        self.max_position_embeddings = max_position_embeddings
+        self.type_vocab_size = type_vocab_size
+        self.initializer_range = initializer_range
+        self.layer_norm_eps = layer_norm_eps
