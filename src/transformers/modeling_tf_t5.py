fe68f7d0b3efd64686f59573f8d914e1b5efd1bb
sgugger <sylvain.gugger@gmail.com>
2020-08-05 11:14:29 -0400

    Style

--- src/transformers/modeling_tf_t5.py
+++ src/transformers/modeling_tf_t5.py
@@ -1057,7 +1057,9 @@ class TFT5Model(TFT5PreTrainedModel):
             ],
             training=training,
         )
-        past = (encoder_outputs, decoder_outputs[1]) if cast_bool_to_primitive(use_cache, self.config.use_cache) else None
+        past = (
+            (encoder_outputs, decoder_outputs[1]) if cast_bool_to_primitive(use_cache, self.config.use_cache) else None
+        )
         if not return_dict:
             if past is not None:
                 decoder_outputs = decoder_outputs[:1] + (past,) + decoder_outputs[2:]
@@ -1274,7 +1276,9 @@ class TFT5ForConditionalGeneration(TFT5PreTrainedModel, TFCausalLanguageModeling
 
         loss = None if labels is None else self.compute_loss(labels, logits)
 
-        past = (encoder_outputs, decoder_outputs[1]) if cast_bool_to_primitive(use_cache, self.config.use_cache) else None
+        past = (
+            (encoder_outputs, decoder_outputs[1]) if cast_bool_to_primitive(use_cache, self.config.use_cache) else None
+        )
         if not return_dict:
             if past is not None:
                 decoder_outputs = decoder_outputs[:1] + (past,) + decoder_outputs[2:]
