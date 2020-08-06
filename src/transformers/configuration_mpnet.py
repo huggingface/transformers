18d7099f1a38c8498fdee689a29ae10328e454ac
v-kasong <v-kasong@microsoft.com>
2020-08-06 09:07:15 +0000

    Configuration for MPNet is Done

--- src/transformers/configuration_mpnet.py
+++ src/transformers/configuration_mpnet.py
@@ -8,8 +8,8 @@ logger = logging.getLogger(__name__)
 
 
 MPNET_PRETRAINED_CONFIG_ARCHIVE_MAP = {
+    "mpnet-base": "",
 }
-
 
 
 class MPNetConfig(PretrainedConfig):
@@ -18,7 +18,7 @@ class MPNetConfig(PretrainedConfig):
         `MPNetModel`.
 
         Arguments:
-            vocab_size: Vocabulary size of `inputs_ids` in `XxxModel`.
+            vocab_size: Vocabulary size of `inputs_ids` in `MPNetModel`.
             hidden_size: Size of the encoder layers and the pooler layer.
             num_hidden_layers: Number of hidden layers in the Transformer encoder.
             num_attention_heads: Number of attention heads for each attention layer in
@@ -34,11 +34,10 @@ class MPNetConfig(PretrainedConfig):
             max_position_embeddings: The maximum sequence length that this model might
                 ever be used with. Typically set this to something large just in case
                 (e.g., 512 or 1024 or 2048).
-            type_vocab_size: The vocabulary size of the `token_type_ids` passed into
-                `XxxModel`.
             initializer_range: The sttdev of the truncated_normal_initializer for
                 initializing all weight matrices.
             layer_norm_eps: The epsilon used by LayerNorm.
+            relative_attention_num_buckets: The buckets for relative positional embedding.
     """
     pretrained_config_archive_map = MPNET_PRETRAINED_CONFIG_ARCHIVE_MAP
     model_type = "mpnet"
@@ -56,12 +55,13 @@ class MPNetConfig(PretrainedConfig):
         max_position_embeddings=512,
         initializer_range=0.02,
         layer_norm_eps=1e-12,
+        relative_attention_num_buckets=32,
         pad_token_id=1,
         bos_token_id=0,
         eos_token_id=2,
-        **kwargs
+        **kwargs,
     ):
-        super().__init__(pad_token_id=pad_token_id, bos_token_id=bos_token, eos_token_id=eos_token_id, **kwargs)
+        super().__init__(pad_token_id=pad_token_id, bos_token_id=bos_token_id, eos_token_id=eos_token_id, **kwargs)
 
         self.vocab_size = vocab_size
         self.hidden_size = hidden_size
@@ -72,6 +72,6 @@ class MPNetConfig(PretrainedConfig):
         self.hidden_dropout_prob = hidden_dropout_prob
         self.attention_probs_dropout_prob = attention_probs_dropout_prob
         self.max_position_embeddings = max_position_embeddings
-        self.type_vocab_size = type_vocab_size
         self.initializer_range = initializer_range
         self.layer_norm_eps = layer_norm_eps
+        self.relative_attention_num_buckets = relative_attention_num_buckets
