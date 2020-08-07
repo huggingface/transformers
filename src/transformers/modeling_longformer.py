dfd729e259613e011cb36ab22573d4ad82c40f96
patrickvonplaten <patrick.v.platen@gmail.com>
2020-08-07 21:56:16 +0200

    reverse to assert statement because function will fail otherwise

--- src/transformers/modeling_longformer.py
+++ src/transformers/modeling_longformer.py
@@ -68,11 +68,9 @@ def _get_question_end_index(input_ids, sep_token_id):
     batch_size = input_ids.shape[0]
 
     assert sep_token_indices.shape[1] == 2, "`input_ids` should have two dimensions"
-    if sep_token_indices.shape[0] == 3 * batch_size:
-        logger.warning(
-            f"There should be exactly three separator tokens: {sep_token_id} in every sample for questions answering. You might also consider to set `global_attention_mask` manually in the forward function to avoid this error."
-        )
-
+    assert (
+        sep_token_indices.shape[0] == 3 * batch_size
+    ), f"There should be exactly three separator tokens: {sep_token_id} in every sample for questions answering. You might also consider to set `global_attention_mask` manually in the forward function to avoid this error."
     return sep_token_indices.view(batch_size, 3, 2)[:, 0, 1]
 
 
