7b30b74a6ed7d5faca21d22bfdf1908f876c588b
Tom Grek <tom@primer.ai>
2020-08-07 23:47:21 +0000

    Avoid call to torch.triu

--- src/transformers/modeling_bart.py
+++ src/transformers/modeling_bart.py
@@ -151,7 +151,11 @@ def _prepare_bart_decoder_inputs(
         decoder_padding_mask = make_padding_mask(decoder_input_ids, pad_token_id)
     else:
         decoder_padding_mask = invert_mask(decoder_padding_mask)
-    causal_mask = torch.triu(fill_with_neg_inf(torch.zeros(tgt_len, tgt_len)), 1).to(
+
+    tmp = fill_with_neg_inf(torch.zeros(tgt_len, tgt_len))
+    mask = torch.arange(tmp.size(-1))
+    tmp.masked_fill_(mask < (mask + 1).view(tmp.size(-1), 1), 0)
+    causal_mask = tmp.to(
         dtype=causal_mask_dtype, device=decoder_input_ids.device
     )
     return decoder_input_ids, decoder_padding_mask, causal_mask
