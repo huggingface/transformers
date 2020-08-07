9481d617624ab1203784e787434e0315d3f80f02
Stas Bekman <stas@stason.org>
2020-08-06 22:41:42 -0700

    more epochs?

--- examples/test_examples.py
+++ examples/test_examples.py
@@ -93,7 +93,7 @@ class ExamplesTests(unittest.TestCase):
             --learning_rate=2e-5
             --seed=42
             --max_seq_length=128
-            --num_train_epochs=2
+            --num_train_epochs=3
             """.split()
         with patch.object(sys, "argv", testargs):
             result = run_pl_glue.main()
