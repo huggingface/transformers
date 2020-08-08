ec1da907b6e5eaa9f41949fef54c9699fdc18975
Sam Shleifer <sshleifer@gmail.com>
2020-08-08 19:53:28 -0400

    fix bash

--- examples/seq2seq/finetune_pegasus_xsum.sh
+++ examples/seq2seq/finetune_pegasus_xsum.sh
@@ -12,11 +12,3 @@ python finetune.py \
     --max_source_length 512 --max_target_length 56 \
     --freeze_embeds --max_target_length 56 --label_smoothing 0.1 \
     $@
-
-export WANDB_PROJECT=pegasus_ft_v0
-export BS=16
-export GAS=16
-./finetune_pegasus_xsum.sh --data_dir $XSUM_DIR --output_dir peg_large_xsum_ft \
-  --num_train_epochs 10  --train_batch_size $BS --eval_batch_size $BS  \
-  --gradient_accumulation_steps $GAS \
-  --logger_name wandb --gpus 1
