a0215250b80e9e6f71c57f869d71c2167ca0a503
Sam Shleifer <sshleifer@gmail.com>
2020-08-08 00:59:31 -0400

    QOL improvements

--- examples/seq2seq/pack_dataset.py
+++ examples/seq2seq/pack_dataset.py
@@ -33,7 +33,7 @@ def pack_examples(tok, src_examples, tgt_examples, max_tokens=1024):
             new_src, new_tgt = src, tgt
         else:  # can fit, keep adding
             new_src, new_tgt = cand_src, cand_tgt
-        # import ipdb; ipdb.set_trace()
+        #
 
     # cleanup
     if new_src:
