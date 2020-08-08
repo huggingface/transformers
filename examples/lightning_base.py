cc260fd547316b88b433f38cc7fdbd697bc878f2
Sam Shleifer <sshleifer@gmail.com>
2020-08-08 19:52:02 -0400

    boom boom

--- examples/lightning_base.py
+++ examples/lightning_base.py
@@ -277,11 +277,6 @@ def add_generic_args(parser, root_dir) -> None:
         required=True,
         help="The output directory where the model predictions and checkpoints will be written.",
     )
-
-    parser.add_argument(
-        "--gpus", default=0, type=int, help="The number of GPUs allocated for this, it is by default 0 meaning none",
-    )
-
     parser.add_argument(
         "--fp16",
         action="store_true",
--- examples/seq2seq/finetune_pegasus_xsum.sh
+++ examples/seq2seq/finetune_pegasus_xsum.sh
@@ -2,7 +2,7 @@
 export PYTHONPATH="../":"${PYTHONPATH}"
 
 # From appendix C of paper https://arxiv.org/abs/1912.08777
-# Set --gradient_accumulation_steps  so that effective batch size is 256
+# Set --gradient_accumulation_steps  so that effective batch size is 256 (2*128, 4*64, 8*32, 16*16)
 python finetune.py \
     --learning_rate=1e-4 \
     --do_train \
@@ -12,3 +12,11 @@ python finetune.py \
     --max_source_length 512 --max_target_length 56 \
     --freeze_embeds --max_target_length 56 --label_smoothing 0.1 \
     $@
+
+export WANDB_PROJECT=pegasus_ft_v0
+export BS=16
+export GAS=16
+./finetune_pegasus_xsum.sh --data_dir $XSUM_DIR --output_dir peg_large_xsum_ft \
+  --num_train_epochs 10  --train_batch_size $BS --eval_batch_size $BS  \
+  --gradient_accumulation_steps $GAS \
+  --logger_name wandb --gpus 1
