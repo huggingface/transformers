c6cc3a9b4b016fc366e5588b891c3573b9d23977
Sam Shleifer <sshleifer@gmail.com>
2020-08-08 03:43:25 -0400

    Revert "no fusedlayernorm?"
    This reverts commit 995c0a2b12da150d3d28e7eaff96c8ff56249982.

--- src/transformers/modeling_bart.py
+++ src/transformers/modeling_bart.py
@@ -798,13 +798,13 @@ class LearnedPositionalEmbedding(nn.Embedding):
 
 
 def LayerNorm(normalized_shape, eps=1e-5, elementwise_affine=True):
-    # if torch.cuda.is_available():
-    #     try:
-    #         from apex.normalization import FusedLayerNorm
-    #
-    #         return FusedLayerNorm(normalized_shape, eps, elementwise_affine)
-    #     except ImportError:
-    #         pass
+    if torch.cuda.is_available():
+        try:
+            from apex.normalization import FusedLayerNorm
+
+            return FusedLayerNorm(normalized_shape, eps, elementwise_affine)
+        except ImportError:
+            pass
     return torch.nn.LayerNorm(normalized_shape, eps, elementwise_affine)
 
 
