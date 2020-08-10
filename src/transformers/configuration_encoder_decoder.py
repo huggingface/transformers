3425936643b157bda181af169b371dcf0a3ad3eb
Patrick von Platen <patrick.v.platen@gmail.com>
2020-08-10 19:46:48 +0200

    [EncoderDecoderModel] add a `add_cross_attention` boolean to config (#6377)
    * correct encoder decoder model
    
    * Apply suggestions from code review
    
    * apply sylvains suggestions

--- src/transformers/configuration_encoder_decoder.py
+++ src/transformers/configuration_encoder_decoder.py
@@ -58,6 +58,7 @@ class EncoderDecoderConfig(PretrainedConfig):
             >>> config_decoder  = model.config.decoder
             >>> # set decoder config to causal lm
             >>> config_decoder.is_decoder = True
+            >>> config_decoder.add_cross_attention = True
 
             >>> # Saving the model, including its configuration
             >>> model.save_pretrained('my-model')
@@ -94,8 +95,9 @@ class EncoderDecoderConfig(PretrainedConfig):
         Returns:
             :class:`EncoderDecoderConfig`: An instance of a configuration object
         """
-        logger.info("Set `config.is_decoder=True` for decoder_config")
+        logger.info("Set `config.is_decoder=True` and `config.add_cross_attention=True` for decoder_config")
         decoder_config.is_decoder = True
+        decoder_config.add_cross_attention = True
 
         return cls(encoder=encoder_config.to_dict(), decoder=decoder_config.to_dict())
 
--- tests/test_modeling_bert.py
+++ tests/test_modeling_bert.py
@@ -176,6 +176,7 @@ class BertModelTester:
         encoder_hidden_states,
         encoder_attention_mask,
     ):
+        config.add_cross_attention = True
         model = BertModel(config)
         model.to(torch_device)
         model.eval()
@@ -235,6 +236,7 @@ class BertModelTester:
         encoder_hidden_states,
         encoder_attention_mask,
     ):
+        config.add_cross_attention = True
         model = BertLMHeadModel(config=config)
         model.to(torch_device)
         model.eval()
