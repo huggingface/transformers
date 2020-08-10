203987a24c4db91820571eeba5485881f12ab0f4
Stas Bekman <stas@stason.org>
2020-08-10 15:03:12 -0700

    cleanup

--- examples/seq2seq/finetune.py
+++ examples/seq2seq/finetune.py
@@ -14,7 +14,7 @@ import torch
 from torch.utils.data import DataLoader
 
 from lightning_base import BaseTransformer, add_generic_args, generic_train
-from transformers import MarianTokenizer, MBartTokenizer, T5ForConditionalGeneration, get_linear_schedule_with_warmup
+from transformers import MarianTokenizer, MBartTokenizer, T5ForConditionalGeneration
 
 
 try:
