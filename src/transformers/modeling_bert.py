756f8b00b42aefc6ee5fb55d261a22207149d007
Patrick von Platen <patrick.v.platen@gmail.com>
2020-08-11 18:45:03 +0200

    fix typo

--- src/transformers/modeling_bert.py
+++ src/transformers/modeling_bert.py
@@ -771,6 +771,10 @@ class BertModel(BertPreTrainedModel):
             Mask values selected in ``[0, 1]``:
             ``1`` for tokens that are NOT MASKED, ``0`` for MASKED tokens.
         """
+        output_attentions = output_attentions if output_attentions is not None else self.config.output_attentions
+        output_hidden_states = (
+            output_hidden_states if output_hidden_states is not None else self.config.output_hidden_states
+        )
         return_dict = return_dict if return_dict is not None else self.config.use_return_dict
 
         if input_ids is not None and inputs_embeds is not None:
