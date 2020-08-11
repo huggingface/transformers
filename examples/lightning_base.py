cf798515d9d7a9053a48cd5b136b5009a42401fb
Sam Shleifer <sshleifer@gmail.com>
2020-08-10 22:44:01 -0400

    Dont delete setup

--- examples/lightning_base.py
+++ examples/lightning_base.py
@@ -149,22 +149,22 @@ class BaseTransformer(pl.LightningModule):
     def test_epoch_end(self, outputs):
         return self.validation_end(outputs)
 
-    @property
-    def total_steps(self) -> int:
-        """The number of total training steps that will be run. Used for lr scheduler purposes."""
-        if not hasattr(self, "train_loader"):
-            self.train_loader = self.get_dataloader("train", self.hparams.train_batch_size)
-        num_devices = max(1, self.hparams.gpus)  # TODO: consider num_tpu_cores
-        effective_batch_size = self.hparams.train_batch_size * self.hparams.accumulate_grad_batches * num_devices
-        return self.calc_total_steps(len(self.train_loader.dataset), effective_batch_size, self.hparams.max_epochs)
-
     @staticmethod
     def calc_total_steps(dataset_size, effective_batch_size, epochs):
         """Calculate the expected number of optimizer steps. (there can be multiple forward passes per optimizer step.)"""
-        return (dataset_size / effective_batch_size) * epochs
+        return
 
-    def setup(self):
-        pass
+    @property
+    def total_steps(self) -> int:
+        """The number of total training steps that will be run. Used for lr scheduler purposes."""
+        num_devices = max(1, self.hparams.gpus)  # TODO: consider num_tpu_cores
+        effective_batch_size = self.hparams.train_batch_size * self.hparams.accumulate_grad_batches * num_devices
+        dataset_size = len(self.train_loader.dataset)
+        return (dataset_size / effective_batch_size) * self.hparams.max_epochs
+
+    def setup(self, mode):
+        if mode == "fit":
+            self.train_loader = self.get_dataloader("train", self.hparams.train_batch_size, shuffle=True)
 
     def get_dataloader(self, type_path, batch_size, shuffle=False):
         raise NotImplementedError("You must implement this for your task")
