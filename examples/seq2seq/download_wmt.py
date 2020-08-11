693a2637a37f4374b21dff0f4e66599d62448d4f
Stas Bekman <stas@stason.org>
2020-08-10 20:41:56 -0700

    don't read the whole dataset into memory
    - rewrite not to load 100GB into RAM - wmt19 is huge! instead read/write in chunks
    - replaced the default dataset to wmt16 as it's much much smaller than wmt19 to experiment with
    - added lang defaults so it's easy to start experimenting with

--- examples/seq2seq/download_wmt.py
+++ examples/seq2seq/download_wmt.py
@@ -4,14 +4,14 @@ import fire
 from tqdm import tqdm
 
 
-def download_wmt_dataset(src_lang, tgt_lang, dataset="wmt19", save_dir=None) -> None:
+def download_wmt_dataset(src_lang="ro", tgt_lang="en", dataset="wmt16", save_dir=None) -> None:
     """Download a dataset using the nlp package and save it to the format expected by finetune.py
     Format of save_dir: train.source, train.target, val.source, val.target, test.source, test.target.
 
     Args:
         src_lang: <str> source language
         tgt_lang: <str> target language
-        dataset: <str> like wmt19 (if you don't know, try wmt19).
+        dataset: <str> like wmt16 (if you don't know, try wmt16).
         save_dir: <str>, where to save the datasets, defaults to f'{dataset}-{src_lang}-{tgt_lang}'
 
     Usage:
@@ -22,6 +22,7 @@ def download_wmt_dataset(src_lang, tgt_lang, dataset="wmt19", save_dir=None) -> 
     except (ModuleNotFoundError, ImportError):
         raise ImportError("run pip install nlp")
     pair = f"{src_lang}-{tgt_lang}"
+    print(f"Converting {dataset}-{pair}")
     ds = nlp.load_dataset(dataset, pair)
     if save_dir is None:
         save_dir = f"{dataset}-{pair}"
@@ -29,18 +30,29 @@ def download_wmt_dataset(src_lang, tgt_lang, dataset="wmt19", save_dir=None) -> 
     save_dir.mkdir(exist_ok=True)
 
     for split in tqdm(ds.keys()):
-        tr_list = list(ds[split])
-        data = [x["translation"] for x in tr_list]
+        print(f"Splitting {split} with {ds[split].num_rows} records")
+
+        # to save to val.source, val.target like summary datasets
+        fn = "val" if split == "validation" else split
+        src_path = save_dir.joinpath(f"{fn}.source")
+        src_fp = src_path.open("w+")
+        tgt_path = save_dir.joinpath(f"{fn}.target")
+        tgt_fp = tgt_path.open("w+")
+
         src, tgt = [], []
-        for example in data:
-            src.append(example[src_lang])
-            tgt.append(example[tgt_lang])
-        if split == "validation":
-            split = "val"  # to save to val.source, val.target like summary datasets
-        src_path = save_dir.joinpath(f"{split}.source")
-        src_path.open("w+").write("\n".join(src))
-        tgt_path = save_dir.joinpath(f"{split}.target")
-        tgt_path.open("w+").write("\n".join(tgt))
+        # read/write in chunks so not to consume potentially 100GB of RAM
+        for x in tqdm(ds[split]):
+            ex = x["translation"]
+            src.append(ex[src_lang])
+            tgt.append(ex[tgt_lang])
+            if len(src) == 100000:
+                src_fp.write("\n".join(src))
+                tgt_fp.write("\n".join(src))
+                src, tgt = [], []
+        # any leftovers
+        src_fp.write("\n".join(src))
+        tgt_fp.write("\n".join(src))
+
     print(f"saved dataset to {save_dir}")
 
 
