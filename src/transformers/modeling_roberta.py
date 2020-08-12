ed8414a38801f97cfb5d7de73f243a67ece6bcc7
Patrick von Platen <patrick.v.platen@gmail.com>
2020-08-12 18:04:14 +0200

    Apply suggestions from code review

--- src/transformers/modeling_roberta.py
+++ src/transformers/modeling_roberta.py
@@ -233,8 +233,6 @@ class RobertaForCausalLM(BertPreTrainedModel):
             Indices should be in ``[-100, 0, ..., config.vocab_size]`` (see ``input_ids`` docstring)
             Tokens with indices set to ``-100`` are ignored (masked), the loss is only computed for the tokens with labels
             in ``[0, ..., config.vocab_size]``
-        kwargs (:obj:`Dict[str, any]`, optional, defaults to `{}`):
-            Used to hide legacy arguments that have been deprecated.
 
     Returns:
 
