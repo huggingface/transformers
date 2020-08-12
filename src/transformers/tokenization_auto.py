f7d347df0e467a2a2f4797ec91d833e53d55aaf2
Mariama Drame <yamoundaw@gmail.com>
2020-08-12 22:32:47 +0200

    fix code style

--- src/transformers/tokenization_auto.py
+++ src/transformers/tokenization_auto.py
@@ -50,7 +50,7 @@ from .tokenization_albert import AlbertTokenizer
 from .tokenization_bart import BartTokenizer, MBartTokenizer
 from .tokenization_bert import BertTokenizer, BertTokenizerFast
 from .tokenization_bert_japanese import BertJapaneseTokenizer
-from .tokenization_blenderbot import BlenderbotSmallTokenizer, BlenderbotTokenizer
+from .tokenization_blenderbot import BlenderbotTokenizer
 from .tokenization_camembert import CamembertTokenizer
 from .tokenization_ctrl import CTRLTokenizer
 from .tokenization_distilbert import DistilBertTokenizer, DistilBertTokenizerFast
