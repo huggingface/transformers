22dc12f4bf15c793a842570d96ebd30ca7ba7825
Kevin Canwen Xu <canwenxu@126.com>
2020-08-13 14:41:40 +0800

    Update test_examples.py

--- examples/test_examples.py
+++ examples/test_examples.py
@@ -91,10 +91,10 @@ class ExamplesTests(unittest.TestCase):
             --do_train
             --do_predict
             --output_dir ./tests/fixtures/tests_samples/pl_temp_dir
-            --train_batch_size=2
+            --train_batch_size=32
             --learning_rate=1e-4
             --warmup_steps=3
-            --num_train_epochs=1
+            --num_train_epochs=4
             --seed=42
             --max_seq_length=128
             """.split()
