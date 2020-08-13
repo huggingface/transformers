3791efa561dc695df063f152a249c3b3cfc6eac6
Mariama Drame <yamoundaw@gmail.com>
2020-08-13 11:34:52 +0200

    remove trailling whitespace

--- src/transformers/tokenization_blenderbot.py
+++ src/transformers/tokenization_blenderbot.py
@@ -137,7 +137,7 @@ class BlenderbotSmallTokenizer(PreTrainedTokenizer):
             merges_file (:obj:`str`): Path to the merges file.
             bos_token (:obj:`string`, `optional`, defaults to "__start__"): The beginning of sentence token.
             eos_token (:obj:`string`, `optional`, defaults to "__end__"): The end of sentence token.
-            unk_token (:obj:`string`, `optional`, defaults to "<unk>"): The unknown token. A token that is not in the 
+            unk_token (:obj:`string`, `optional`, defaults to "<unk>"): The unknown token. A token that is not in the
                 vocabulary cannot be converted to an ID and is set to be this token instead.
     """
 
