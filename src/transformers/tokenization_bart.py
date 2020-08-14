b05ee8f033d46a8c21bea84be4aea635dfded3ce
Suraj Patil <surajp815@gmail.com>
2020-08-14 20:24:04 +0530

    fix docs

--- src/transformers/tokenization_bart.py
+++ src/transformers/tokenization_bart.py
@@ -75,6 +75,7 @@ class BartTokenizer(RobertaTokenizer):
                 If left unset or set to :obj:`None`, this will use the max_length value.
             padding (:obj:`bool`, :obj:`str` or :class:`~transformers.tokenization_utils_base.PaddingStrategy`, `optional`, defaults to :obj:`False`):
                 Activates and controls padding. Accepts the following values:
+
                 * :obj:`True` or :obj:`'longest'`: Pad to the longest sequence in the batch (or no padding if only a
                   single sequence if provided).
                 * :obj:`'max_length'`: Pad to a maximum length specified with the argument :obj:`max_length` or to the
@@ -83,11 +84,13 @@ class BartTokenizer(RobertaTokenizer):
                   different lengths).
             return_tensors (:obj:`str` or :class:`~transformers.tokenization_utils_base.TensorType`, `optional`, defaults to "pt"):
                 If set, will return tensors instead of list of python integers. Acceptable values are:
+
                 * :obj:`'tf'`: Return TensorFlow :obj:`tf.constant` objects.
                 * :obj:`'pt'`: Return PyTorch :obj:`torch.Tensor` objects.
                 * :obj:`'np'`: Return Numpy :obj:`np.ndarray` objects.
             truncation (:obj:`bool`, :obj:`str` or :class:`~transformers.tokenization_utils_base.TruncationStrategy`, `optional`, defaults to :obj:`True`):
                 Activates and controls truncation. Accepts the following values:
+
                 * :obj:`True` or :obj:`'longest_first'`: Truncate to a maximum length specified with the argument
                   :obj:`max_length` or to the maximum acceptable input length for the model if that argument is not
                   provided. This will truncate token by token, removing a token from the longest sequence in the pair
@@ -105,6 +108,7 @@ class BartTokenizer(RobertaTokenizer):
 
         Returns:
             :class:`~transformers.BatchEncoding`: A :class:`~transformers.BatchEncoding` with the following fields:
+
             - **input_ids** -- List of token ids to be fed to the encoder.
             - **attention_mask** -- List of indices specifying which tokens should be attended to by the model.
             - **decoder_input_ids** -- List of token ids to be fed to the decoder.
