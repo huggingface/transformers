b33cad7edd16721adb01dbc8387b07e881e3fb35
Mariama Drame <yamoundaw@gmail.com>
2020-08-14 09:21:04 +0200

    fix typos

--- tests/test_modeling_blenderbot.py
+++ tests/test_modeling_blenderbot.py
@@ -108,11 +108,11 @@ class BlenderbotModelTester:
 @require_torch
 class BlenderbotTesterMixin(ModelTesterMixin, unittest.TestCase):
     if is_torch_available:
-      all_generative_model_classes = (BlenderbotForConditionalGeneration,)
-      all_model_classes = (BlenderbotForConditionalGeneration,)
+        all_generative_model_classes = (BlenderbotForConditionalGeneration,)
+        all_model_classes = (BlenderbotForConditionalGeneration,)
     else:
-      all_generative_model_classes = ()
-      all_model_classes = ()
+        all_generative_model_classes = ()
+        all_model_classes = ()
 
     is_encoder_decoder = True
     test_head_masking = False
