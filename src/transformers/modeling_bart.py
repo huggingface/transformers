668da99299cea47d40873f1d294077115af4c582
Sam Shleifer <sshleifer@gmail.com>
2020-08-16 15:12:32 -0400

    WIP: dont force bos token

--- src/transformers/modeling_bart.py
+++ src/transformers/modeling_bart.py
@@ -1073,8 +1073,6 @@ class BartForConditionalGeneration(PretrainedBartModel):
         }
 
     def adjust_logits_during_generation(self, logits, cur_len, max_length):
-        if cur_len == 1:
-            self._force_token_ids_generation(logits, self.config.bos_token_id)
         if cur_len == max_length - 1 and self.config.eos_token_id is not None:
             self._force_token_ids_generation(logits, self.config.eos_token_id)
         return logits
