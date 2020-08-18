0f903a4c71e18cb054f4b772515f6ec86524bb19
hlz <hlz@pku.edu.cn>
2020-08-18 19:17:02 +0800

    Add TextDatasetForNSP for next sentence prediction task.

--- src/transformers/__init__.py
+++ src/transformers/__init__.py
@@ -443,12 +443,14 @@ if is_torch_available():
         DataCollator,
         DataCollatorForLanguageModeling,
         DataCollatorForPermutationLanguageModeling,
+        DataCollatorForNSP,
         DataCollatorWithPadding,
     )
     from .data.datasets import (
         GlueDataset,
         TextDataset,
         LineByLineTextDataset,
+        TextDatasetForNSP,
         GlueDataTrainingArguments,
         SquadDataset,
         SquadDataTrainingArguments,
