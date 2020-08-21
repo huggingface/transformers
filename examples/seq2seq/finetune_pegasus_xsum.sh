d4706c8bc852d22bf73b4ea9600b614a50238d7c
Sam Shleifer <sshleifer@gmail.com>
2020-08-21 21:16:58 +0000

    boom boom

--- examples/seq2seq/finetune_pegasus_xsum.sh
+++ examples/seq2seq/finetune_pegasus_xsum.sh
@@ -9,6 +9,6 @@ python finetune.py \
     --do_predict \
     --n_val 1000 \
     --val_check_interval 0.25 \
-    --max_source_length 512 --max_target_length 56 \
+    --max_source_length 512 \
     --freeze_embeds --max_target_length 56 --label_smoothing 0.1 \
     "$@"
