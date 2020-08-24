acfa63ba2d5f72faa0eb5d4a4e7104241cd2681f
Suraj Patil <surajp815@gmail.com>
2020-08-24 22:50:40 +0530

    add training code

---
+++ examples/seq2seq/finetune_trainer.py
@@ -0,0 +1,227 @@
+import dataclasses
+import logging
+import os
+import sys
+from dataclasses import dataclass, field
+from typing import Callable, Dict, Optional
+
+from transformers import AutoModelForSeq2SeqLM, AutoTokenizer, AutoConfig, MBartTokenizer, MarianTokenizer
+from transformers import (
+    HfArgumentParser,
+    Trainer,
+    TrainingArguments,
+    set_seed,
+)
+
+from .utils import (
+    freeze_params,
+    assert_all_frozen,
+    TranslationDataset,
+    Seq2SeqDataset,
+)
+
+logger = logging.getLogger(__name__)
+
+
+@dataclass
+class ModelArguments:
+    """
+    Arguments pertaining to which model/config/tokenizer we are going to fine-tune from.
+    """
+
+    model_name_or_path: str = field(
+        metadata={"help": "Path to pretrained model or model identifier from huggingface.co/models"}
+    )
+    config_name: Optional[str] = field(
+        default=None, metadata={"help": "Pretrained config name or path if not the same as model_name"}
+    )
+    tokenizer_name: Optional[str] = field(
+        default=None, metadata={"help": "Pretrained tokenizer name or path if not the same as model_name"}
+    )
+    cache_dir: Optional[str] = field(
+        default=None, metadata={"help": "Where do you want to store the pretrained models downloaded from s3"}
+    )
+    freeze_encoder: bool = field(
+        default=False, metadata={"help": "Freeze the encoder"}
+    )
+    freeze_embeds: bool = field(
+        default=False, metadata={"help": "Freeze the embeddings"}
+    )
+
+@dataclass
+class DataTrainingArguments:
+    """
+    Arguments pertaining to what data we are going to input our model for training and eval.
+    """
+
+    max_source_length: Optional[int] = field(
+        default=1024, metadata={"help": "The maximum total input sequence length after tokenization. Sequences longer "
+            "than this will be truncated, sequences shorter will be padded."}
+    )
+    max_target_length: Optional[int] = field(
+        default=56, metadata={"help": "The maximum total input sequence length after tokenization. Sequences longer "
+            "than this will be truncated, sequences shorter will be padded."}
+    )
+    val_max_target_length: Optional[int] = field(
+        default=142, metadata={"help": "The maximum total input sequence length after tokenization. Sequences longer "
+            "than this will be truncated, sequences shorter will be padded."}
+    )
+    test_max_target_length: Optional[int] = field(
+        default=142, metadata={"help": "The maximum total input sequence length after tokenization. Sequences longer "
+            "than this will be truncated, sequences shorter will be padded."}
+    )
+    sortish_sampler: bool = field(
+        default=False, metadata={"help": "use sortish sampler"}
+    )
+    n_train: Optional[int] = field(
+        default=-1, metadata={"help": "# examples. -1 means use all."}
+    )
+    n_val: Optional[int] = field(
+        default=-1, metadata={"help": "# examples. -1 means use all."}
+    )
+    n_test: Optional[int] = field(
+        default=-1, metadata={"help": "# examples. -1 means use all."}
+    )
+    src_lang: Optional[str] = field(
+        default=None, metadata={"help": "Source language for translation"}
+    )
+    tgt_lang: Optional[str] = field(
+        default=None, metadata={"help": "Target language for translation"}
+    )
+
+def main():
+    # See all possible arguments in src/transformers/training_args.py
+    # or by passing the --help flag to this script.
+    # We now keep distinct sets of args, for a cleaner separation of concerns.
+
+    parser = HfArgumentParser((ModelArguments, DataTrainingArguments, TrainingArguments))
+
+    if len(sys.argv) == 2 and sys.argv[1].endswith(".json"):
+        # If we pass only one argument to the script and it's the path to a json file,
+        # let's parse it to get our arguments.
+        model_args, data_args, training_args = parser.parse_json_file(json_file=os.path.abspath(sys.argv[1]))
+    else:
+        model_args, data_args, training_args = parser.parse_args_into_dataclasses()
+
+    if (
+        os.path.exists(training_args.output_dir)
+        and os.listdir(training_args.output_dir)
+        and training_args.do_train
+        and not training_args.overwrite_output_dir
+    ):
+        raise ValueError(
+            f"Output directory ({training_args.output_dir}) already exists and is not empty. Use --overwrite_output_dir to overcome."
+        )
+
+    # Setup logging
+    logging.basicConfig(
+        format="%(asctime)s - %(levelname)s - %(name)s -   %(message)s",
+        datefmt="%m/%d/%Y %H:%M:%S",
+        level=logging.INFO if training_args.local_rank in [-1, 0] else logging.WARN,
+    )
+    logger.warning(
+        "Process rank: %s, device: %s, n_gpu: %s, distributed training: %s, 16-bits training: %s",
+        training_args.local_rank,
+        training_args.device,
+        training_args.n_gpu,
+        bool(training_args.local_rank != -1),
+        training_args.fp16,
+    )
+    logger.info("Training/evaluation parameters %s", training_args)
+
+    # Set seed
+    set_seed(training_args.seed)
+
+    # Load pretrained model and tokenizer
+    #
+    # Distributed training:
+    # The .from_pretrained methods guarantee that only one local process can concurrently
+    # download model & vocab.
+
+    config = AutoConfig.from_pretrained(
+        model_args.config_name if model_args.config_name else model_args.model_name_or_path,
+        cache_dir=model_args.cache_dir,
+    )
+    tokenizer = AutoTokenizer.from_pretrained(
+        model_args.tokenizer_name if model_args.tokenizer_name else model_args.model_name_or_path,
+        cache_dir=model_args.cache_dir,
+    )
+    model = AutoModelForSeq2SeqLM.from_pretrained(
+        model_args.model_name_or_path,
+        from_tf=bool(".ckpt" in model_args.model_name_or_path),
+        config=config,
+        cache_dir=model_args.cache_dir,
+    )
+
+    def freeze_embeds(model):
+        """Freeze token embeddings and positional embeddings for bart, just token embeddings for t5."""
+        try:
+            freeze_params(model.model.shared)
+            for d in [model.model.encoder, model.model.decoder]:
+                freeze_params(d.embed_positions)
+                freeze_params(d.embed_tokens)
+        except AttributeError:
+            freeze_params(model.shared)
+            for d in [model.encoder, model.decoder]:
+                freeze_params(d.embed_tokens)
+    
+    if model_args.freeze_embeds:
+        freeze_embeds(model)
+    if model_args.freeze_encoder:
+        freeze_params(model.get_encoder())
+        assert_all_frozen(model.get_encoder())
+    
+    if isinstance(tokenizer, MBartTokenizer) or isinstance(tokenizer, MarianTokenizer):
+        dataset_class = TranslationDataset
+    else:
+        dataset_class = Seq2SeqDataset
+    
+    # Get datasets
+    train_dataset = (dataset_class(
+        tokenizer,
+        type_path="train",
+        data_dir=data_args.data_dir,
+        n_obs=data_args.n_train,
+        max_target_length=data_args.max_target_length,
+        max_source_length=data_args.max_source_length,
+        prefix=model.config.prefix or "",
+    ) if training_args.do_train else None)
+    eval_dataset = (dataset_class(
+        tokenizer,
+        type_path="val",
+        data_dir=data_args.data_dir,
+        n_obs=data_args.n_val,
+        max_target_length=data_args.val_max_target_length,
+        max_source_length=data_args.max_source_length,
+        prefix=model.config.prefix or "",
+    ) if training_args.do_eval else None)
+    test_dataset = (dataset_class(
+        tokenizer,
+        type_path="test",
+        data_dir=data_args.data_dir,
+        n_obs=data_args.n_test,
+        max_target_length=data_args.test_max_target_length,
+        max_source_length=data_args.max_source_length,
+        prefix=model.config.prefix or "",
+    ) if training_args.do_predict else None)
+
+    # Initialize our Trainer
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        train_dataset=train_dataset,
+        eval_dataset=eval_dataset,
+    )
+
+    # Training
+    if training_args.do_train:
+        trainer.train(
+            model_path=model_args.model_name_or_path if os.path.isdir(model_args.model_name_or_path) else None
+        )
+        trainer.save_model()
+        # For convenience, we also re-save the tokenizer to the same directory,
+        # so that you can share your model easily on huggingface.co/models =)
+        if trainer.is_world_master():
+            tokenizer.save_pretrained(training_args.output_dir)
+    
+    # TODO: compute_metrics
