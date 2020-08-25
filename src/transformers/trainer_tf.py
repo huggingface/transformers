c2ab0c00dea399ab5fd3b832ac17e37a549c4e71
jplu <plu.julien@gmail.com>
2020-08-25 12:59:58 +0200

    Apply style

--- src/transformers/trainer_tf.py
+++ src/transformers/trainer_tf.py
@@ -622,17 +622,22 @@ class TFTrainer:
             for _ in tf.range(self.args.gradient_accumulation_steps):
                 reduced_features = {
 <<<<<<< HEAD
+<<<<<<< HEAD
                     k: ft[: self.args.train_batch_size // self.args.n_replicas] for k, ft in features.items()
 =======
                     ft: features[ft][:self.args.train_batch_size // self.args.n_replicas]
                     for ft in features
 >>>>>>> Fix gradient accumulation training
+=======
+                    ft: features[ft][: self.args.train_batch_size // self.args.n_replicas] for ft in features
+>>>>>>> Apply style
                 }
                 reduced_labels = labels[: self.args.train_batch_size // self.args.n_replicas]
 
                 self.training_step(reduced_features, reduced_labels)
 
                 features = {
+<<<<<<< HEAD
 <<<<<<< HEAD
                     k: tf.concat(
                         [ft[self.args.train_batch_size // self.args.n_replicas :], reduced_features[k]],
@@ -651,6 +656,18 @@ class TFTrainer:
 
                 labels = tf.concat([labels[self.args.train_batch_size // self.args.n_replicas :], reduced_labels], axis=0)
 >>>>>>> Fix gradient accumulation training
+=======
+                    ft: tf.concat(
+                        [features[ft][self.args.train_batch_size // self.args.n_replicas :], reduced_features[ft]],
+                        axis=0,
+                    )
+                    for ft in features
+                }
+
+                labels = tf.concat(
+                    [labels[self.args.train_batch_size // self.args.n_replicas :], reduced_labels], axis=0
+                )
+>>>>>>> Apply style
 
             gradients = self.gradient_accumulator.gradients
             gradients = [
