fc954b1119ee8a449f320f18d582a9af0c119848
Nikolai Y <nikolai.yakovenko@point72.com>
2020-08-25 17:50:15 -0400

    remove double error class declare

--- tests/test_optimization.py
+++ tests/test_optimization.py
@@ -81,13 +81,6 @@ class OptimizationTest(unittest.TestCase):
             w.grad.zero_()
         self.assertListAlmostEqual(w.tolist(), [0.4, 0.2, -0.5], tol=1e-2)
 
-@require_torch
-class OptimizationTest(unittest.TestCase):
-    def assertListAlmostEqual(self, list1, list2, tol):
-        self.assertEqual(len(list1), len(list2))
-        for a, b in zip(list1, list2):
-            self.assertAlmostEqual(a, b, delta=tol)
-
     def test_adafactor(self):
         w = torch.tensor([0.1, -0.2, -0.1], requires_grad=True)
         target = torch.tensor([0.4, 0.2, -0.5])
