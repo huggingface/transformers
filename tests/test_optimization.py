706e5b2fdfebdc31fa72c880d874a9243b9560aa
Nikolai Y <nikolai.yakovenko@point72.com>
2020-08-25 16:44:35 -0400

    longer loop for adafactor test

--- tests/test_optimization.py
+++ tests/test_optimization.py
@@ -93,10 +93,10 @@ class OptimizationTest(unittest.TestCase):
         target = torch.tensor([0.4, 0.2, -0.5])
         criterion = torch.nn.MSELoss()
         # No warmup, constant schedule, no gradient clipping
-        optimizer = Adafactor(params=[w], lr=1e-3, eps=(1e-30, 1e-3), clip_threshold=1.0,
+        optimizer = Adafactor(params=[w], lr=1e-2, eps=(1e-30, 1e-3), clip_threshold=1.0,
             decay_rate=-0.8, beta1=None, weight_decay=0.0, relative_step=False,
             scale_parameter=False, warmup_init=False,)
-        for _ in range(100):
+        for _ in range(1000):
             loss = criterion(w, target)
             loss.backward()
             optimizer.step()
