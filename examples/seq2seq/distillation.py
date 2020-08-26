89e097b66e6523beb54d85d4fd8c23b96434a962
Sam Shleifer <sshleifer@gmail.com>
2020-08-26 16:32:02 -0400

    boom boom

--- examples/seq2seq/distillation.py
+++ examples/seq2seq/distillation.py
@@ -167,9 +167,6 @@ class BartSummarizationDistiller(SummarizationModule):
         if "labels" in batch:
             lm_labels = batch["labels"]
             decoder_input_ids = shift_tokens_right(lm_labels, pad_token_id)
-        elif isinstance(self.model, T5ForConditionalGeneration):
-            lm_labels = batch["decoder_input_ids"]
-            decoder_input_ids = self.model._shift_right(lm_labels)
         else:
             raise ValueError()
         # decoder_input_ids = y[:, :-1].contiguous()
@@ -189,8 +186,7 @@ class BartSummarizationDistiller(SummarizationModule):
         lm_logits, dec_hidden, enc_outputs, enc_hidden_state = outputs
 
         if not self.already_saved_batch:
-            batch['passed_labels'] = lm_labels
-            batch['passed_decoder_input_ids'] = decoder_input_ids
+            batch['decoder_input_ids'] = decoder_input_ids
             self.save_readable_batch(batch)
 
         if self.hparams.label_smoothing == 0:
@@ -278,7 +274,7 @@ def add_distill_args(parser):
 
 class BartTranslationDistiller(BartSummarizationDistiller):
     mode = "translation"
-    loss_names = ["loss"]
+    loss_names = ["loss", "ce_loss", "mlm_loss", "enc_mse_loss", "hid_loss_enc", "hid_loss_dec"]
     metric_names = ["bleu"]
     val_metric = "bleu"
 
--- examples/seq2seq/finetune.py
+++ examples/seq2seq/finetune.py
@@ -113,7 +113,9 @@ class SummarizationModule(BaseTransformer):
     def save_readable_batch(self, batch: Dict[str, torch.Tensor]) -> Dict[str, List[str]]:
         """A debugging utility"""
         readable_batch = {k: self.tokenizer.batch_decode(v) if 'mask' not in k else v.shape for k,v in batch.items()}
-        save_json(readable_batch,  Path(self.output_dir) / "batch.json")
+        save_json(readable_batch,  Path(self.output_dir) / "text_batch.json")
+        save_json({k: v.tolist() for k,v in batch.items()}, Path(self.output_dir) / "tok_batch.json")
+
         self.already_saved_batch = True
         return readable_batch
 
