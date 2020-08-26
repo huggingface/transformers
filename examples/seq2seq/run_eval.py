0b9f1cd8f1bc33352c74d3705d73def9f632b24f
Sam Shleifer <sshleifer@gmail.com>
2020-08-26 18:12:20 -0400

    style

--- examples/seq2seq/run_eval.py
+++ examples/seq2seq/run_eval.py
@@ -1,6 +1,7 @@
 import argparse
 import json
 import time
+import warnings
 from logging import getLogger
 from pathlib import Path
 from typing import Dict, List
@@ -9,7 +10,7 @@ import torch
 from tqdm import tqdm
 
 from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
-import warnings
+
 
 logger = getLogger(__name__)
 
@@ -105,7 +106,7 @@ def run_generate():
         examples = examples[: args.n_obs]
     Path(args.save_path).parent.mkdir(exist_ok=True)
     if args.reference_path is None and Path(args.score_path).exists():
-        warnings.warn(f'score_path {args.score_path} will be overwritten unless you type ctrl-c.')
+        warnings.warn(f"score_path {args.score_path} will be overwritten unless you type ctrl-c.")
     runtime_metrics = generate_summaries_or_translations(
         examples,
         args.save_path,
