0f58903bb62870342eae52f5a02c9105ec6f9b1e
Sam Shleifer <sshleifer@gmail.com>
2020-08-29 17:43:32 -0400

    Pegasus finetune script: add --adafactor (#6811)

--- tests/test_modeling_pegasus.py
+++ tests/test_modeling_pegasus.py
@@ -1,9 +1,10 @@
 import unittest
 
 from transformers import AutoConfig, AutoTokenizer, is_torch_available
-from transformers.configuration_pegasus import max_gen_length, max_model_length
+from transformers.configuration_pegasus import task_specific_params
 from transformers.file_utils import cached_property
 from transformers.testing_utils import require_torch, slow, torch_device
+from transformers.utils.logging import ERROR, set_verbosity
 
 from .test_modeling_bart import PGE_ARTICLE
 from .test_modeling_mbart import AbstractSeq2SeqIntegrationTest
@@ -13,6 +14,8 @@ if is_torch_available():
     from transformers import AutoModelForSeq2SeqLM
 
 XSUM_ENTRY_LONGER = """ The London trio are up for best UK act and best album, as well as getting two nominations in the best song category."We got told like this morning 'Oh I think you're nominated'", said Dappy."And I was like 'Oh yeah, which one?' And now we've got nominated for four awards. I mean, wow!"Bandmate Fazer added: "We thought it's best of us to come down and mingle with everyone and say hello to the cameras. And now we find we've got four nominations."The band have two shots at the best song prize, getting the nod for their Tynchy Stryder collaboration Number One, and single Strong Again.Their album Uncle B will also go up against records by the likes of Beyonce and Kanye West.N-Dubz picked up the best newcomer Mobo in 2007, but female member Tulisa said they wouldn't be too disappointed if they didn't win this time around."At the end of the day we're grateful to be where we are in our careers."If it don't happen then it don't happen - live to fight another day and keep on making albums and hits for the fans."Dappy also revealed they could be performing live several times on the night.The group will be doing Number One and also a possible rendition of the War Child single, I Got Soul.The charity song is a  re-working of The Killers' All These Things That I've Done and is set to feature artists like Chipmunk, Ironik and Pixie Lott.This year's Mobos will be held outside of London for the first time, in Glasgow on 30 September.N-Dubz said they were looking forward to performing for their Scottish fans and boasted about their recent shows north of the border."We just done Edinburgh the other day," said Dappy."We smashed up an N-Dubz show over there. We done Aberdeen about three or four months ago - we smashed up that show over there! Everywhere we go we smash it up!" """
+
+set_verbosity(ERROR)
 
 
 @require_torch
@@ -50,31 +53,25 @@ class PegasusXSUMIntegrationTest(AbstractSeq2SeqIntegrationTest):
 
 
 class PegasusConfigTests(unittest.TestCase):
-    def test_all_config_max_lengths(self):
+    @slow
+    def test_task_specific_params(self):
+        """Test that task_specific params['summarization_xsum'] == config['pegasus_xsum'] """
         failures = []
         pegasus_prefix = "google/pegasus"
-        for dataset, max_len in max_gen_length.items():
+        n_prefix_chars = len("summarization_")
+        for task, desired_settings in task_specific_params.items():
+            dataset = task[n_prefix_chars:]
             mname = f"{pegasus_prefix}-{dataset}"
             cfg = AutoConfig.from_pretrained(mname)
-
-            if cfg.max_length != max_len:
-                failures.append(f"config for {mname} had max_length: {cfg.max_length}, expected {max_len}")
-
-            if cfg.max_position_embeddings < max_model_length[dataset]:
-                # otherwise you get IndexError for e.g. position 513
-                # see https://github.com/huggingface/transformers/issues/6599
-                failures.append(
-                    f"config for {mname} had max_position_embeddings: {cfg.max_position_embeddings}, expected {max_model_length[dataset]}"
-                )
-
+            for k, v in desired_settings.items():
+                actual_value = getattr(cfg, k)
+                if actual_value != v:
+                    failures.append(f"config for {mname} had {k}: {actual_value}, expected {v}")
             tokenizer = AutoTokenizer.from_pretrained(mname)
-            if max_model_length[dataset] != tokenizer.model_max_length:
-                failures.append(
-                    f"tokenizer.model_max_length {tokenizer.model_max_length} expected {max_model_length[dataset]}"
-                )
+            n_pos_embeds = desired_settings["max_position_embeddings"]
+            if n_pos_embeds != tokenizer.model_max_length:
+                failures.append(f"tokenizer.model_max_length {tokenizer.model_max_length} expected {n_pos_embeds}")
 
-        if failures == []:
-            return
         # error
         all_fails = "\n".join(failures)
-        raise AssertionError(f"The following configs have unexpected settings: {all_fails}")
+        assert not failures, f"The following configs have unexpected settings: {all_fails}"
