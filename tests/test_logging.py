10092760e5028c2d27882cd23633a0dfd4c68088
Stas Bekman <stas@stason.org>
2020-08-31 13:31:48 -0700

    fix

--- tests/test_logging.py
+++ tests/test_logging.py
@@ -8,7 +8,7 @@ class HfArgumentParserTest(unittest.TestCase):
     def test_set_level(self):
         logger = logging.get_logger()
 
-        level_origin = logging.get_verbosity_error()
+        level_origin = logging.get_verbosity()
 
         logging.set_verbosity_error()
         self.assertEqual(logger.getEffectiveLevel(), logging.get_verbosity())
@@ -25,7 +25,6 @@ class HfArgumentParserTest(unittest.TestCase):
         # restore
         logging.set_verbosity(level_origin)
 
-
     def test_integration(self):
         import transformers.tokenization_bart
 
@@ -33,6 +32,7 @@ class HfArgumentParserTest(unittest.TestCase):
         msg = "Testing 1, 2, 3"
 
         # should be able to log info (default setting)
+        logging.set_verbosity_info()
         with CaptureLogger(logger) as cl:
             logger.info(msg)
         self.assertEqual(cl.out, msg + "\n")
