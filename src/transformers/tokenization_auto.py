ffe00c67aeaa066631207b268aee939b1d26f4b5
Jabin Huang <huangjipengnju@gmail.com>
2020-09-01 14:37:41 +0800

    Updata tokenization_auto.py
    Updata tokenization_auto.py to handle Inherited tokenizer

--- src/transformers/tokenization_auto.py
+++ src/transformers/tokenization_auto.py
@@ -213,7 +213,7 @@ class AutoTokenizer:
 
         use_fast = kwargs.pop("use_fast", False)
         for config_class, (tokenizer_class_py, tokenizer_class_fast) in TOKENIZER_MAPPING.items():
-            if isinstance(config, config_class):
+            if type(config) is config_class:
                 if tokenizer_class_fast and use_fast:
                     return tokenizer_class_fast.from_pretrained(pretrained_model_name_or_path, *inputs, **kwargs)
                 else:
