e45ca17e835d91ae499c5f53b48a8676d557b690
Lysandre Debut <lysandre@huggingface.co>
2020-09-01 09:15:43 +0200

    Update src/transformers/utils/logging.py

--- src/transformers/utils/logging.py
+++ src/transformers/utils/logging.py
@@ -74,7 +74,7 @@ def _reset_library_root_logger() -> None:
 
 def get_logger(name: Optional[str] = None) -> logging.Logger:
     """
-    eturn a logger with the specified name.
+    Return a logger with the specified name.
 
     This function is not supposed to be directly accessed unless you are writing a custom transformers module.
     """
