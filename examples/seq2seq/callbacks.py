a136279adc21cd9c901d30c74d7858da53126ea9
Sam Shleifer <sshleifer@gmail.com>
2020-09-04 04:07:40 +0000

    merged master

--- examples/seq2seq/callbacks.py
+++ examples/seq2seq/callbacks.py
@@ -91,13 +91,8 @@ def get_checkpoint_callback(output_dir, metric, save_top_k=1, lower_is_better=Fa
     checkpoint_callback = ModelCheckpoint(
         filepath=os.path.join(output_dir, exp),
         monitor=f"val_{metric}",
-<<<<<<< HEAD
-        mode="max",
-        save_top_k=5,
-=======
         mode="min" if "loss" in metric else "max",
         save_top_k=save_top_k,
->>>>>>> da5f790bf295fe4dc4cd6ce88f46278cae6cd877
         period=0,  # maybe save a checkpoint every time val is run, not just end of epoch.
     )
     return checkpoint_callback
