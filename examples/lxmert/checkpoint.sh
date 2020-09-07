98bba410d43aadc45583becb1d47addc9a9e7de7
Antonio V Mendoza <antonio36764@gmail.com>
2020-09-07 14:56:41 -0400

    Update examples/lxmert/checkpoint.sh
    Co-authored-by: Lysandre Debut <lysandre@huggingface.co>

--- examples/lxmert/checkpoint.sh
+++ examples/lxmert/checkpoint.sh
@@ -2,5 +2,5 @@
 
 wget http://nlp.cs.unc.edu/models/faster_rcnn_from_caffe_attr.pkl
 mv faster_rcnn_from_caffe_attr.pkl checkpoint.pkl
-https://raw.githubusercontent.com/airsplay/py-bottom-up-attention/master/demo/data/images/input.jpg
+wget https://raw.githubusercontent.com/airsplay/py-bottom-up-attention/master/demo/data/images/input.jpg
 mv input.jpg test_in.jpg
