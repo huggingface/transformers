db06984efdcf64d5dd255040b042f5b493e0b7c0
Patrick von Platen <patrick.v.platen@gmail.com>
2020-09-09 19:48:13 +0200

    correct slow test

--- tests/test_modeling_encoder_decoder.py
+++ tests/test_modeling_encoder_decoder.py
@@ -545,7 +545,7 @@ class BertForSeqGenerationEncoderDecoderModelTest(EncoderDecoderMixin, unittest.
 
         ARTICLE = """The problem is affecting people using the older versions of the PlayStation 3, called the "Fat" model.The problem isn't affecting the newer PS3 Slim systems that have been on sale since September last year.Sony have also said they are aiming to have the problem fixed shortly but is advising some users to avoid using their console for the time being."We hope to resolve this problem within the next 24 hours," a statement reads. "In the meantime, if you have a model other than the new slim PS3, we advise that you do not use your PS3 system, as doing so may result in errors in some functionality, such as recording obtained trophies, and not being able to restore certain data."We believe we have identified that this problem is being caused by a bug in the clock functionality incorporated in the system."The PlayStation Network is used by millions of people around the world.It allows users to play their friends at games like Fifa over the internet and also do things like download software or visit online stores."""
 
-        EXPECTED_SUMMARY = """sae was founded in 1856, five years before the civil war. the fraternity has had to work hard to change recently. the university of oklahoma president says the university's affiliation with the fraternity is permanently done. the sae has had a string of members in recent months."""
+        EXPECTED_SUMMARY = """Sony has said that a bug in its PlayStation 3 console is preventing them from using the machine as a computer."""
 
         input_ids = tokenizer(ARTICLE, return_tensors="pt").input_ids.to(torch_device)
         output_ids = model.generate(input_ids)
