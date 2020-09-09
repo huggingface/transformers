f841173ca9a0eefa2000fbd7a7e81af0bd6587ab
Sam Shleifer <sshleifer@gmail.com>
2020-09-09 10:38:02 -0400

    style

--- src/transformers/modeling_pegasus.py
+++ src/transformers/modeling_pegasus.py
@@ -57,4 +57,5 @@ class PegasusForConditionalGeneration(BartForConditionalGeneration):
         if cur_len == max_length - 1 and self.config.eos_token_id is not None:
             self._force_token_ids_generation(logits, self.config.eos_token_id)
         return logits
+
     # All the code is in src/transformers/modeling_bart.py
