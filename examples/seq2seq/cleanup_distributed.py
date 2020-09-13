b18e94a86f552a869471c1ba36ea8c5d2bb4476d
Sam Shleifer <sshleifer@gmail.com>
2020-09-13 16:57:04 -0400

    boom boom

--- examples/seq2seq/cleanup_distributed.py
+++ examples/seq2seq/cleanup_distributed.py
@@ -9,14 +9,17 @@ except ImportError:
     from utils import calculate_bleu, calculate_rouge, load_json, save_json, write_txt_file
 
 
-def cleanup_distributed_results(
+def combine_partial_results(
     result_dir: str, save_dir: str = None, save_prefix=None, calc_bleu=False, just_metrics=False
 ):
     """Write first n lines of each file f in src_dir to dest_dir/f """
     src_dir = Path(result_dir)
     save_dir = Path(save_dir)
     save_dir.mkdir(exist_ok=True)
-    records = sum([load_json(x) for x in src_dir.glob("*.json")])
+    records = []
+    for partial_result in src_dir.glob("*.json"):
+        records.extend(load_json(partial_result))
+
     preds = [x["pred"] for x in records]
     labels = [x["label"] for x in records]
     score_fn = calculate_bleu if calc_bleu else calculate_rouge
@@ -39,4 +42,4 @@ def cleanup_distributed_results(
 
 
 if __name__ == "__main__":
-    fire.Fire(cleanup_distributed_results)
+    fire.Fire(combine_partial_results)
