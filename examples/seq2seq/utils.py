8a81d1260231216c685029a8dd4bb710f3abc9aa
Sam Shleifer <sshleifer@gmail.com>
2020-09-14 12:58:14 -0400

    bug

--- examples/seq2seq/utils.py
+++ examples/seq2seq/utils.py
@@ -250,11 +250,10 @@ class DistributedSortishSampler(Sampler):
     def __iter__(self) -> Iterable:
         g = torch.Generator()
         g.manual_seed(self.epoch)
-        available_indices = self.available_indices()  # indices[self.rank: self.total_size: self.num_replicas]
 
-        sortish_data = [self.dataset.src_lens[i] for i in available_indices]
+        sortish_data = [self.dataset.src_lens[i] for i in self.available_indices]
         sortish_indices = sortish_sampler_indices(sortish_data, self.batch_size)
-        indices = [available_indices[i] for i in sortish_indices]
+        indices = [self.available_indices[i] for i in sortish_indices]
         assert len(indices) == self.num_samples
         return iter(indices)
 
