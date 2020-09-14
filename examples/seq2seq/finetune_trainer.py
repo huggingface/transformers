3ca5f1dd810e4f3cdef33a7f15067f4b1dcdcd6f
Suraj Patil <surajp815@gmail.com>
2020-09-14 23:26:41 +0530

    remove redundant  code

--- examples/seq2seq/finetune_trainer.py
+++ examples/seq2seq/finetune_trainer.py
@@ -3,7 +3,7 @@ import logging
 import os
 import sys
 from dataclasses import dataclass, field
-from typing import Callable, Dict, Optional
+from typing import Callable, Dict, List, Optional, Tuple
 
 import numpy as np
 import torch
@@ -279,25 +279,27 @@ def main():
         model.config.decoder_start_token_id = decoder_start_token_id
 
     def build_compute_metrics_fn(task_name: str) -> Callable[[EvalPrediction], Dict]:
-        def summarization_metrics(pred: EvalPrediction) -> Dict:
+        def non_pad_len(tokens: np.ndarray) -> int:
+            return np.count_nonzero(tokens != tokenizer.pad_token_type_id)
+
+        def decode_pred(pred: EvalPrediction) -> Tuple[List[str], List[str]]:
             pred_str = tokenizer.batch_decode(pred.predictions, skip_special_tokens=True)
             label_str = tokenizer.batch_decode(pred.label_ids, skip_special_tokens=True)
             pred_str = lmap(str.strip, pred_str)
             label_str = lmap(str.strip, label_str)
+            return pred_str, label_str
 
+        def summarization_metrics(pred: EvalPrediction) -> Dict:
+            pred_str, label_str = decode_pred(pred)
             rouge: Dict = calculate_rouge(pred_str, label_str)
             summ_len = np.mean(lmap(len, pred.predictions))
             rouge.update({"gen_len": summ_len})
             return rouge
 
         def translation_metrics(pred: EvalPrediction) -> Dict:
-            pred_str = tokenizer.batch_decode(pred.predictions, skip_special_tokens=True)
-            label_str = tokenizer.batch_decode(pred.label_ids, skip_special_tokens=True)
-            pred_str = lmap(str.strip, pred_str)
-            label_str = lmap(str.strip, label_str)
-
+            pred_str, label_str = decode_pred(pred)
             bleu: Dict = calculate_bleu(pred_str, label_str)
-            gen_len = np.mean(lmap(len, pred.predictions))
+            gen_len = np.mean(lmap(non_pad_len, pred.predictions))
             bleu.update({"gen_len": gen_len})
             return bleu
 
