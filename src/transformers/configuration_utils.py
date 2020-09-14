9c02c68636d5a4d6e15589da1e2c807dea3c7ee9
Kevin Canwen Xu <canwenxu@126.com>
2020-09-14 23:50:22 +0800

    Add Mirror Option for Downloads (#6679)
    * Add Tuna Mirror for Downloads from China
    
    * format fix
    
    * Use preset instead of hardcoding URL
    
    * Fix
    
    * make style
    
    * update the mirror option doc
    
    * update the mirror

--- src/transformers/configuration_utils.py
+++ src/transformers/configuration_utils.py
@@ -337,7 +337,9 @@ class PretrainedConfig(object):
         elif os.path.isfile(pretrained_model_name_or_path) or is_remote_url(pretrained_model_name_or_path):
             config_file = pretrained_model_name_or_path
         else:
-            config_file = hf_bucket_url(pretrained_model_name_or_path, filename=CONFIG_NAME, use_cdn=False)
+            config_file = hf_bucket_url(
+                pretrained_model_name_or_path, filename=CONFIG_NAME, use_cdn=False, mirror=None
+            )
 
         try:
             # Load from URL or cache if already cached
