9455493f1a2402b4d8fe35a633e63c2b7fbc452a
Sam Shleifer <sshleifer@gmail.com>
2020-09-14 14:14:46 -0400

    Catch racecondition of incomplete json file that exists

--- examples/seq2seq/run_distributed_eval.py
+++ examples/seq2seq/run_distributed_eval.py
@@ -1,8 +1,10 @@
 import argparse
+import shutil
+import time
+from json import JSONDecodeError
 from logging import getLogger
 from pathlib import Path
-from typing import Dict
-import time
+from typing import Dict, List, Tuple
 
 import torch
 from torch.utils.data import DataLoader
@@ -14,14 +16,30 @@ from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
 logger = getLogger(__name__)
 
 try:
-    from .utils import Seq2SeqDataset, parse_numeric_cl_kwargs, save_json, use_task_specific_params
-    from .aggregate_distributed_results import combine_partial_results
+    from .utils import (
+        Seq2SeqDataset,
+        calculate_bleu,
+        calculate_rouge,
+        lmap,
+        load_json,
+        parse_numeric_cl_kwargs,
+        save_json,
+        use_task_specific_params,
+        write_txt_file,
+    )
 except ImportError:
-    from utils import Seq2SeqDataset, parse_numeric_cl_kwargs, save_json, use_task_specific_params
-    from aggregate_distributed_results import combine_partial_results
+    from utils import (
+        Seq2SeqDataset,
+        calculate_bleu,
+        calculate_rouge,
+        lmap,
+        load_json,
+        parse_numeric_cl_kwargs,
+        save_json,
+        use_task_specific_params,
+        write_txt_file,
+    )
 
-DEFAULT_DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
-import time
 
 def eval_data_dir(
     data_dir,
@@ -32,7 +50,6 @@ def eval_data_dir(
     type_path="val",
     n_obs=None,
     fp16=False,
-    save_source=False,
     num_beams: int = 4,
     task="summarization",
     local_rank=None,
@@ -78,14 +95,9 @@ def eval_data_dir(
         preds = tokenizer.batch_decode(summaries, **dec_kwargs)
         labels = tokenizer.batch_decode(batch["labels"], **dec_kwargs)
         ids = batch["ids"]
-        if save_source:
-            docs = tokenizer.batch_decode(batch["input_ids"], **dec_kwargs)
         for i in range(len(labels)):
             label, pred = labels[i], preds[i]
-            if save_source:
-                results.append(dict(pred=pred, label=label, source=docs[i], id=ids[i]))
-            else:
-                results.append(dict(pred=pred, label=label, id=ids[i].item()))
+            results.append(dict(pred=pred, label=label, id=ids[i].item()))
     save_json(results, save_path)
     return results, sampler.num_replicas
 
@@ -117,16 +129,23 @@ def run_generate():
         "--n_obs", type=int, default=None, required=False, help="How many observations. Defaults to all."
     )
     parser.add_argument("--fp16", action="store_true")
-    parser.add_argument("--save_source", action="store_true")
-    real_start = time.time()
+    parser.add_argument("--debug", action="store_true")
+    start_time = time.time()
     args, rest = parser.parse_known_args()
     generate_kwargs = parse_numeric_cl_kwargs(rest)
     if generate_kwargs:
         print(f"parsed the following generate kwargs: {generate_kwargs}")
+    json_save_dir = Path(args.save_dir + "_tmp")
+    Path(json_save_dir).mkdir(exist_ok=True)  # this handles locking.
+    intermediate_files = list(json_save_dir.glob("rank_*.json"))
+    if intermediate_files:
+        raise ValueError(f"Found files at {json_save_dir} please move or remove them.")
+        # In theory, a node could finish and save before another node hits this. If this happens, we can address later.
+
     Path(args.save_dir).mkdir(exist_ok=True)
     results, num_replicas = eval_data_dir(
         args.data_dir,
-        args.save_dir,
+        json_save_dir,
         args.model_name,
         type_path=args.type_path,
         batch_size=args.bs,
@@ -134,25 +153,60 @@ def run_generate():
         task=args.task,
         local_rank=args.local_rank,
         n_obs=args.n_obs,
-        save_source=args.save_source,
         max_source_length=args.max_source_length,
         **generate_kwargs,
     )
-    save_dir = Path(args.save_dir)
+
     if args.local_rank <= 0:
-        wait_for_all_nodes_complete(num_replicas, save_dir)
-        combine_partial_results(args.save_dir, args.save_dir, save_prefix=args.type_path, just_metrics=False,
-                                calc_bleu='translation' in args.task)
+        save_dir = Path(args.save_dir)
+        save_dir.mkdir(exist_ok=True)
+        partial_results = gather_results_from_each_node(num_replicas, json_save_dir)
+        preds, labels = combine_partial_results(partial_results)
+
+        calc_bleu = "translation" in args.task
+        score_fn = calculate_bleu if calc_bleu else calculate_rouge
+        metric_name = "bleu" if calc_bleu else "rouge"
+        metrics: Dict = score_fn(preds, labels)
+        metrics["seconds_per_sample"] = metrics["n_obs"] / time.time() - start_time
+        # TODO(@stas00): add whatever metadata to metrics
+        metrics_save_path = save_dir.joinpath(f"{args.type_path}+_{metric_name}.json")
+        save_json(metrics, metrics_save_path)
+        write_txt_file(preds, save_dir.joinpath(f"{args.type_path}_generations.txt"))
+        if args.debug:
+            write_txt_file(labels, save_dir.joinpath(f"{args.type_path}.target"))
+        else:
+            shutil.rmtree(json_save_dir)
 
 
-def wait_for_all_nodes_complete(num_replicas, save_dir):
+def combine_partial_results(partial_results) -> Tuple[List, List]:
+    """Write first n lines of each file f in src_dir to dest_dir/f """
+    records = []
+    for partial_result in partial_results:
+        records.extend(partial_result)
+    records = list(sorted(records, key=lambda x: x["id"]))
+    preds = [x["pred"] for x in records]
+    labels = [x["label"] for x in records]
+    return preds, labels
+
+
+def gather_results_from_each_node(num_replicas, save_dir, timeout=120) -> List[Dict[str, List]]:
     # WAIT FOR lots of .json files
     start_wait = time.time()
-    timeout = 15  # make higher
+    logger.info("waiting for all nodes to finish")
+    json_data = None
     while (time.time() - start_wait) < timeout:
-        json_files = list(save_dir.glob('rank_*.json'))
-        if len(json_files) >= num_replicas:
-            break
+        json_files = list(save_dir.glob("rank_*.json"))
+        if len(json_files) < num_replicas:
+            continue
+        try:
+            # make sure all json files are fully saved
+            json_data = lmap(load_json, json_files)
+            return json_data
+        except JSONDecodeError:
+            continue
+    else:
+        raise TimeoutError("Rank 0 gave up on waiting for other processes")
+    # Unreachable
 
 
 if __name__ == "__main__":
