9b666b7f4dca2477cd4fab073a888ad261f26027
Thomas Wolf <thomwolf@users.noreply.github.com>
2020-09-16 08:48:26 +0200

    WIP tokenizers

--- src/transformers/tokenization_t5.py
+++ src/transformers/tokenization_t5.py
@@ -448,7 +448,6 @@ class T5TokenizerFast(PreTrainedTokenizerFast):
 
         sp = spm.SentencePieceProcessor()
         sp.Load(vocab_file)
-        vocab_filename = f"mymodel.json"
         vocab = [(sp.id_to_piece(i), sp.get_score(i)) for i in range(sp.piece_size())]
 
         super().__init__(
@@ -471,16 +470,6 @@ class T5TokenizerFast(PreTrainedTokenizerFast):
         self.vocab_file = vocab_file
         self._extra_ids = extra_ids
 
-    def _add_eos_if_not_present(self, token_ids: List[int]) -> List[int]:
-        """Do not add eos again if user already added it."""
-        if len(token_ids) > 0 and token_ids[-1] == self.eos_token_id:
-            warnings.warn(
-                f"This sequence already has {self.eos_token}. In future versions this behavior may lead to duplicated eos tokens being added."
-            )
-            return token_ids
-        else:
-            return token_ids + [self.eos_token_id]
-
     def build_inputs_with_special_tokens(
         self, token_ids_0: List[int], token_ids_1: Optional[List[int]] = None
     ) -> List[int]:
@@ -502,11 +491,11 @@ class T5TokenizerFast(PreTrainedTokenizerFast):
         Returns:
             :obj:`List[int]`: list of `input IDs <../glossary.html#input-ids>`__ with the appropriate special tokens.
         """
-        token_ids_0 = self._add_eos_if_not_present(token_ids_0)
+        token_ids_0 = token_ids_0 + [self.eos_token_id]
         if token_ids_1 is None:
             return self.prefix_tokens + token_ids_0
         else:
-            token_ids_1 = self._add_eos_if_not_present(token_ids_1)
+            token_ids_1 = token_ids_1 + [self.eos_token_id]
             return self.prefix_tokens + token_ids_0 + token_ids_1
 
     def prepare_seq2seq_batch(
