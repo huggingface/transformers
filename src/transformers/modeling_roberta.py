d67a95458e86edb918f9336761eaff3efdb71fff
sgugger <sylvain.gugger@gmail.com>
2020-09-17 16:24:34 -0400

    Fix docstring

--- src/transformers/modeling_roberta.py
+++ src/transformers/modeling_roberta.py
@@ -542,8 +542,21 @@ ROBERTA_INPUTS_DOCSTRING = r"""
 )
 class RobertaModel(RobertaPreTrainedModel):
     """
-    This class overrides :class:`~transformers.BertModel`. Please check the
-    superclass for the appropriate documentation alongside usage examples.
+
+    The model can behave as an encoder (with only self-attention) as well
+    as a decoder, in which case a layer of cross-attention is added between
+    the self-attention layers, following the architecture described in `Attention is all you need`_ by Ashish Vaswani,
+    Noam Shazeer, Niki Parmar, Jakob Uszkoreit, Llion Jones, Aidan N. Gomez, Lukasz Kaiser and Illia Polosukhin.
+
+    To behave as an decoder the model needs to be initialized with the
+    :obj:`is_decoder` argument of the configuration set to :obj:`True`.
+    To be used in a Seq2Seq model, the model needs to initialized with both :obj:`is_decoder`
+    argument and :obj:`add_cross_attention` set to :obj:`True`; an
+    :obj:`encoder_hidden_states` is then expected as an input to the forward pass.
+
+    .. _`Attention is all you need`:
+        https://arxiv.org/abs/1706.03762
+
     """
 
     # Copied from transformers.modeling_bert.BertModel.__init__ with Bert->Roberta
