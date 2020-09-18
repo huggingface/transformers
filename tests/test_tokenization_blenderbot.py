2715b25989c475bf13eb2c780e9ed75726901ed7
Sam Shleifer <sshleifer@gmail.com>
2020-09-18 00:42:32 -0400

    add semi-broken tokenizer test

--- tests/test_tokenization_blenderbot.py
+++ tests/test_tokenization_blenderbot.py
@@ -63,3 +63,11 @@ class BlenderbotSmallTokenizerTest(TokenizerTesterMixin, unittest.TestCase):
 
         input_bpe_tokens = [0, 1, 2, 3, 4, 5]
         self.assertListEqual(tokenizer.convert_tokens_to_ids(input_tokens), input_bpe_tokens)
+
+    def test_special_tokens_small_tok(self):
+        tok = BlenderbotSmallTokenizer.from_pretrained('facebook/blenderbot-90M')
+        src_text = 'I am a small frog.'
+        encoded = tok([src_text], padding=False, truncation=False)['input_ids']
+        decoded = tok.batch_decode(encoded, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0]
+        assert src_text != decoded  # I wish it did!
+        assert decoded == 'i am a small frog .'
