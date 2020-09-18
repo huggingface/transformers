b18a71cfb8cf5a92c5162e582d4ad483c012fa84
Sam Shleifer <sshleifer@gmail.com>
2020-09-18 16:31:06 -0400

    boom boom

--- tests/test_modeling_pegasus.py
+++ tests/test_modeling_pegasus.py
@@ -47,9 +47,16 @@ class PegasusXSUMIntegrationTest(AbstractSeq2SeqIntegrationTest):
         # Demonstrate fp16 issue, Contributions welcome!
         self.model.half()
         translated_tokens_fp16 = self.model.generate(**inputs, max_length=10)
-        decoded = self.tokenizer.batch_decode(translated_tokens_fp16, skip_special_tokens=True)
+        decoded_fp16 = self.tokenizer.batch_decode(translated_tokens_fp16, skip_special_tokens=True)
         bad_fp16_result = ["unk_7unk_7unk_7unk_7unk_7unk_7unk_7", "unk_7unk_7unk_7unk_7unk_7unk_7unk_7"]
+        print(f'decoded_fp16: {decoded_fp16}')
+        print(f'decoded_fp32: {decoded}')
+        import torch
+        encoder_outputs = self.model.model.encoder(**inputs)[0]
+        import torch
+        assert not torch.isnan(encoder_outputs).any().item()
         self.assertListEqual(decoded, bad_fp16_result)
+        assert decoded_fp16 == ["California's largest electricity provider has begun", 'N-Dubz have revealed they were']
 
 
 class PegasusConfigTests(unittest.TestCase):
