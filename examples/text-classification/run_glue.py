240650be727d85f304c3e94d8cabda7c4d9e6a0f
Suraj Patil <surajp815@gmail.com>
2020-09-19 18:56:36 +0530

    p.predictions -> preds

--- examples/text-classification/run_glue.py
+++ examples/text-classification/run_glue.py
@@ -152,10 +152,10 @@ def main():
         def compute_metrics_fn(p: EvalPrediction):
             if output_mode == "classification":
                 preds = p.predictions[0] if type(p.predictions) == tuple else p.predictions
-                preds = np.argmax(p.predictions, axis=1)
+                preds = np.argmax(preds, axis=1)
             elif output_mode == "regression":
                 preds = p.predictions[0] if type(p.predictions) == tuple else p.predictions
-                preds = np.squeeze(p.predictions)
+                preds = np.squeeze(preds)
             return glue_compute_metrics(task_name, preds, p.label_ids)
 
         return compute_metrics_fn
