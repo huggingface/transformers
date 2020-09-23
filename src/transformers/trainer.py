48c72a974c8019aa3deb2f94bc032a8958527d28
wissam antoun <wissam.antoun@gmail.com>
2020-09-23 17:42:14 +0300

    black formatting

--- src/transformers/trainer.py
+++ src/transformers/trainer.py
@@ -830,11 +830,11 @@ class Trainer:
                     break
             epoch_pbar.close()
             train_pbar.update(1)
-            
+
             if self.args.evaluation_strategy == EvaluationStrategy.EPOCH:
                 metrics = self.evaluate()
                 self._report_to_hp_search(trial, epoch, metrics)
-                
+
             if self.args.tpu_metrics_debug or self.args.debug:
                 if is_torch_tpu_available():
                     # tpu-comment: Logging debug metrics for PyTorch/XLA (compile, execute times, ops, etc.)
