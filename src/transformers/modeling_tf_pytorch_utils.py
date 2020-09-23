f64b0eb2a3dbef8355bc579fbef777c3927784da
patrickvonplaten <patrick.v.platen@gmail.com>
2020-09-24 00:55:59 +0200

    fix tests

--- src/transformers/modeling_tf_pytorch_utils.py
+++ src/transformers/modeling_tf_pytorch_utils.py
@@ -160,6 +160,10 @@ def load_pytorch_weights_in_tf2_model(tf_model, pt_state_dict, tf_inputs=None, a
             if allow_missing_keys:
                 missing_keys.append(name)
                 continue
+            elif tf_model.authorized_missing_keys is not None:
+                # authorized missing keys don't have to be loaded
+                if any(re.search(pat, name) is not None for pat in tf_model.authorized_missing_keys):
+                    continue
 
             raise AttributeError("{} not found in PyTorch model".format(name))
 
