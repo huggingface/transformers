b350e7d8467424031b9f105ecdb4e460fa700460
Patrick von Platen <patrick.v.platen@gmail.com>
2020-09-23 18:04:34 +0200

    make backwards compatible

--- src/transformers/benchmark/benchmark_args.py
+++ src/transformers/benchmark/benchmark_args.py
@@ -34,12 +34,32 @@ logger = logging.get_logger(__name__)
 
 @dataclass
 class PyTorchBenchmarkArguments(BenchmarkArguments):
+
+    deprecated_args = [
+        "no_inference",
+        "no_cuda",
+        "no_tpu",
+        "no_speed",
+        "no_memory",
+        "no_env_print",
+        "no_multi_process",
+    ]
+
     def __init__(self, **kwargs):
-        for deprecated_arg in ["no_inference", "no_cuda", "no_tpu", "no_speed", "no_memory", "no_env_print", "no_multi_process"]:
+        """This __init__ is there for legacy code. When removing
+        deprecated args completely, the class can simply be deleted
+        """
+        for deprecated_arg in self.deprecated_args:
             if deprecated_arg in kwargs:
                 positive_arg = deprecated_arg[3:]
-                kwargs[positive_arg] = not kwargs.pop(deprecated_arg)
-                logger.warning(f"{deprecated_arg} is depreciated. Please use --no-{positive_arg} or {positive_arg}={kwargs[positive_arg]}")
+                setattr(self, positive_arg, not kwargs.pop(deprecated_arg))
+                logger.warning(
+                    f"{deprecated_arg} is depreciated. Please use --no-{positive_arg} or {positive_arg}={kwargs[positive_arg]}"
+                )
+
+        self.torchscript = kwargs.pop("torchscript", self.torchscript)
+        self.torch_xla_tpu_print_metrics = kwargs.pop("torch_xla_tpu_print_metrics", self.torch_xla_tpu_print_metrics)
+        self.fp16_opt_level = kwargs.pop("fp16_opt_level", self.fp16_opt_level)
         super().__init__(**kwargs)
 
     torchscript: bool = field(default=False, metadata={"help": "Trace the models using torchscript"})
--- src/transformers/benchmark/benchmark_args_tf.py
+++ src/transformers/benchmark/benchmark_args_tf.py
@@ -32,12 +32,31 @@ logger = logging.get_logger(__name__)
 @dataclass
 class TensorFlowBenchmarkArguments(BenchmarkArguments):
 
+    deprecated_args = [
+        "no_inference",
+        "no_cuda",
+        "no_tpu",
+        "no_speed",
+        "no_memory",
+        "no_env_print",
+        "no_multi_process",
+    ]
+
     def __init__(self, **kwargs):
-        for deprecated_arg in ["no_inference", "no_cuda", "no_tpu", "no_speed", "no_memory", "no_env_print", "no_multi_process"]:
+        """This __init__ is there for legacy code. When removing
+        deprecated args completely, the class can simply be deleted
+        """
+        for deprecated_arg in self.deprecated_args:
             if deprecated_arg in kwargs:
                 positive_arg = deprecated_arg[3:]
                 kwargs[positive_arg] = not kwargs.pop(deprecated_arg)
-                logger.warning(f"{deprecated_arg} is depreciated. Please use --no-{positive_arg} or {positive_arg}={kwargs[positive_arg]}")
+                logger.warning(
+                    f"{deprecated_arg} is depreciated. Please use --no-{positive_arg} or {positive_arg}={kwargs[positive_arg]}"
+                )
+        self.tpu_name = kwargs.pop("tpu_name", self.tpu_name)
+        self.device_idx = kwargs.pop("device_idx", self.device_idx)
+        self.eager_mode = kwargs.pop("eager_mode", self.eager_mode)
+        self.use_xla = kwargs.pop("use_xla", self.use_xla)
         super().__init__(**kwargs)
 
     tpu_name: str = field(
