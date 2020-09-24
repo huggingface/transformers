cd540a506e82b362c245410d54257e661ca30fbf
Stas Bekman <stas00@users.noreply.github.com>
2020-09-24 14:10:26 -0700

    [fsmt] build/test scripts (#7257)
    Co-authored-by: Sam Shleifer <sshleifer@gmail.com>

---
+++ scripts/fsmt/tests-to-run.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# these scripts need to be run before any changes to FSMT-related code - it should cover all bases
+
+USE_CUDA=0 RUN_SLOW=1 pytest --disable-warnings tests/test_tokenization_fsmt.py tests/test_configuration_auto.py tests/test_modeling_fsmt.py examples/seq2seq/test_fsmt_bleu_score.py
+USE_CUDA=1 RUN_SLOW=1 pytest --disable-warnings tests/test_tokenization_fsmt.py tests/test_configuration_auto.py tests/test_modeling_fsmt.py examples/seq2seq/test_fsmt_bleu_score.py
