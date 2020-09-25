1a3b40a1e0ad8d1ea551c805d59523e2d2ff6151
Sam Shleifer <sshleifer@gmail.com>
2020-09-24 21:23:36 -0400

    add assert

--- src/transformers/modeling_bart.py
+++ src/transformers/modeling_bart.py
@@ -550,6 +550,9 @@ class BartDecoder(nn.Module):
         positions = self.embed_positions(input_ids, use_cache=use_cache)
 
         if use_cache:
+            assert (
+                input_ids.shape[1] == 1 or past_key_values is not None
+            ), "pass decoder_past_key_value_states to use_cache"
             input_ids = input_ids[:, -1:]
             positions = positions[:, -1:]  # happens after we embed them
             # assert input_ids.ne(self.padding_idx).any()
