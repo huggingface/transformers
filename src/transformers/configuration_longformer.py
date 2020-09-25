deb70752b1ac8a757210c4d39096e79843c6b13e
Patrick von Platen <patrick.v.platen@gmail.com>
2020-09-25 20:33:21 +0200

    [Longformer, Bert, Roberta, ...] Fix multi gpu training (#7272)
    * fix multi-gpu
    
    * fix longformer
    
    * force to delete unnecessary layers
    
    * fix notifications
    
    * fix warning
    
    * fix roberta
    
    * fix tests
    
    * remove hasattr
    
    * fix tests
    
    * fix roberta
    
    * merge and clean authorized keys

--- src/transformers/configuration_longformer.py
+++ src/transformers/configuration_longformer.py
@@ -67,6 +67,5 @@ class LongformerConfig(RobertaConfig):
     model_type = "longformer"
 
     def __init__(self, attention_window: Union[List[int], int] = 512, sep_token_id: int = 2, **kwargs):
-        super().__init__(**kwargs)
+        super().__init__(sep_token_id=sep_token_id, **kwargs)
         self.attention_window = attention_window
-        self.sep_token_id = sep_token_id
