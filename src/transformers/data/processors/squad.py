871128b5a8cd7480362d55116d89b2e442c4fd49
Malte Pietsch <malte.pietsch@deepset.ai>
2020-09-25 12:18:48 +0200

    fix squad tokenization for roberta & co

--- src/transformers/data/processors/squad.py
+++ src/transformers/data/processors/squad.py
@@ -107,9 +107,13 @@ def squad_convert_example_to_features(
     tok_to_orig_index = []
     orig_to_tok_index = []
     all_doc_tokens = []
+    tokenizer_type = type(tokenizer).__name__.replace("Tokenizer", "").lower()
     for (i, token) in enumerate(example.doc_tokens):
         orig_to_tok_index.append(len(all_doc_tokens))
-        sub_tokens = tokenizer.tokenize(token)
+        if tokenizer_type in ("roberta", "longformer", "bart"):
+            sub_tokens = tokenizer.tokenize(token, add_prefix_space=True)
+        else:
+            sub_tokens = tokenizer.tokenize(token)
         for sub_token in sub_tokens:
             tok_to_orig_index.append(i)
             all_doc_tokens.append(sub_token)
@@ -133,7 +137,6 @@ def squad_convert_example_to_features(
 
     # Tokenizers who insert 2 SEP tokens in-between <context> & <question> need to have special handling
     # in the way they compute mask of added tokens.
-    tokenizer_type = type(tokenizer).__name__.replace("Tokenizer", "").lower()
     sequence_added_tokens = (
         tokenizer.max_len - tokenizer.max_len_single_sentence + 1
         if tokenizer_type in MULTI_SEP_TOKENS_TOKENIZERS_SET
