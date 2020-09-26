d6f619f86aa176ed14bbf406c02fedef8bef2bfe
Sam Shleifer <sshleifer@gmail.com>
2020-09-26 14:51:33 -0400

    Update examples/seq2seq/make_student.py
    Co-authored-by: Suraj Patil <surajp815@gmail.com>

--- examples/seq2seq/make_student.py
+++ examples/seq2seq/make_student.py
@@ -83,7 +83,7 @@ def create_student_by_copying_alternating_layers(
     copy_first_teacher_layers=False,  # D
     **extra_config_kwargs
 ) -> Tuple[PreTrainedModel, List[int], List[int]]:
-    """Make a student by copying alternating layers from a teacher, save it to student_save_path.
+    """Make a student by copying alternating layers from a teacher, save it to save_path.
     Args:
         teacher: str or PreTrainedModel if str, this will call AutoModelForSeq2SeqLM.from_pretrained(teacher) before
         copying layers
