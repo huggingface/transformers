16c213820e3cbd5b59ca504cbf5684d8dbeefad3
Lysandre <lysandre.debut@reseau.eseo.fr>
2020-09-28 16:32:00 +0200

    Update docs to version v3.3.0

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -49,4 +49,5 @@ deploy_doc "10d7239" v2.10.0
 deploy_doc "b42586e" v2.11.0
 deploy_doc "7fb8bdf" v3.0.2
 deploy_doc "4b3ee9c" v3.1.0
-deploy_doc "3ebb1b3" # v3.2.0 Latest stable release
+deploy_doc "3ebb1b3" v3.2.0
+deploy_doc "0613f05" # v3.3.0 Latest stable release
