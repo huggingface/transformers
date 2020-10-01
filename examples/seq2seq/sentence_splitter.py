5f6bf945497885ff0d9d7e5aa7a48ac629f704a9
Sam Shleifer <sshleifer@gmail.com>
2020-10-01 12:37:19 -0400

    style

--- examples/seq2seq/sentence_splitter.py
+++ examples/seq2seq/sentence_splitter.py
@@ -20,4 +20,3 @@ def add_newline_to_end_of_each_sentence(x: str) -> str:
     re.sub("<n>", "", x)  # remove pegasus newline char
     assert NLTK_AVAILABLE, "nltk must be installed to separate newlines between sentences. (pip install nltk)"
     return "\n".join(nltk.sent_tokenize(x))
-
