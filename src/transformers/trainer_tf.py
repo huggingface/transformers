fad8a4a794ec058f3e5258e1bf8e09cc4287f80d
Paul_TraxRetail <paul@traxretail.com>
2020-10-01 14:24:55 +0300

    Since tf.gradients will not work in eager mode, it is best to use gradient tape, which flexibly tracks the operations within its block, and constructs a computation graph from them.

--- src/transformers/trainer_tf.py
+++ src/transformers/trainer_tf.py
@@ -630,12 +630,14 @@ class TFTrainer:
 
         Subclass and override to inject some custom behavior.
         """
-        per_example_loss, _ = self.run_model(features, labels, True)
-        scaled_loss = per_example_loss / tf.cast(nb_instances_in_global_batch, dtype=per_example_loss.dtype)
-        gradients = tf.gradients(scaled_loss, self.model.trainable_variables)
-        gradients = [
-            g if g is not None else tf.zeros_like(v) for g, v in zip(gradients, self.model.trainable_variables)
-        ]
+
+        with tf.GradientTape() as tape:
+            per_example_loss, _ = self.run_model(features, labels, True)
+            scaled_loss = per_example_loss / tf.cast(nb_instances_in_global_batch, dtype=per_example_loss.dtype)
+            gradients = tape.gradient(scaled_loss, self.model.trainable_variables)
+            gradients = [
+                g if g is not None else tf.zeros_like(v) for g, v in zip(gradients, self.model.trainable_variables)
+            ]
 
         if self.args.gradient_accumulation_steps > 1:
             self.gradient_accumulator(gradients)
