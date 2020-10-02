21917ecbadc41a0d6b431c5fb3415e3b8c8ba2cb
Sylvain Gugger <sylvain.gugger@gmail.com>
2020-10-02 09:33:07 -0400

    Format class name/obj name

--- src/transformers/retrieval_rag.py
+++ src/transformers/retrieval_rag.py
@@ -281,8 +281,8 @@ class RagRetriever:
     _init_retrieval = True
 
     def __init__(self, config, question_encoder_tokenizer, generator_tokenizer):
-        requires_datasets()
-        requires_faiss()
+        requires_datasets(self)
+        requires_faiss(self)
         super().__init__()
         self.index = (
             LegacyIndex(
@@ -311,8 +311,8 @@ class RagRetriever:
 
     @classmethod
     def from_pretrained(cls, retriever_name_or_path, **kwargs):
-        requires_datasets()
-        requires_faiss()
+        requires_datasets(cls)
+        requires_faiss(cls)
         config = RagConfig.from_pretrained(retriever_name_or_path, **kwargs)
         rag_tokenizer = RagTokenizer.from_pretrained(retriever_name_or_path, config=config)
         question_encoder_tokenizer = rag_tokenizer.question_encoder
