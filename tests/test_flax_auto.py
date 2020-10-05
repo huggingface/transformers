8fd6650ca1dcba7dfa4b603488d15911cfb83def
Morgan Funtowicz <funtowiczmo@gmail.com>
2020-10-05 18:40:41 +0200

    Rewrite FlaxAutoModel test to not rely on pretrained_model_archive_map
    Signed-off-by: Morgan Funtowicz <funtowiczmo@gmail.com>

--- tests/test_flax_auto.py
+++ tests/test_flax_auto.py
@@ -1,25 +1,37 @@
 import unittest
 
-from transformers import AutoConfig, is_flax_available
+from transformers import AutoConfig, is_flax_available, BertConfig
+from transformers.modeling_flax_bert import FlaxBertModel
+from transformers.modeling_flax_roberta import FlaxRobertaModel
 from transformers.testing_utils import require_flax, slow
 
 
 if is_flax_available():
-    from transformers.modeling_flax_auto import MODEL_MAPPING, FlaxAutoModel
+    from transformers.modeling_flax_auto import FlaxAutoModel
 
 
 @require_flax
 class FlaxAutoModelTest(unittest.TestCase):
     @slow
-    def test_model_from_pretrained(self):
-        for model_config, model_class in MODEL_MAPPING.items():
+    def test_bert_from_pretrained(self):
+        for model_name in ["bert-base-cased", "bert-large-uncased"]:
+            with self.subTest(model_name):
+                config = AutoConfig.from_pretrained(model_name)
+                self.assertIsNotNone(config)
+                self.assertIsInstance(config, BertConfig)
 
-            for model_name in model_class.pretrained_model_archive_map.keys():
-                with self.subTest(model_name):
-                    config = AutoConfig.from_pretrained(model_name)
-                    self.assertIsNotNone(config)
-                    self.assertIsInstance(config, model_config)
+                model = FlaxAutoModel.from_pretrained(model_name)
+                self.assertIsNotNone(model)
+                self.assertIsInstance(model, FlaxBertModel)
 
-                    model = FlaxAutoModel.from_pretrained(model_name)
-                    self.assertIsNotNone(model)
-                    self.assertIsInstance(model, model_class)
+    @slow
+    def test_roberta_from_pretrained(self):
+        for model_name in ["roberta-base-cased", "roberta-large-uncased"]:
+            with self.subTest(model_name):
+                config = AutoConfig.from_pretrained(model_name)
+                self.assertIsNotNone(config)
+                self.assertIsInstance(config, BertConfig)
+
+                model = FlaxAutoModel.from_pretrained(model_name)
+                self.assertIsNotNone(model)
+                self.assertIsInstance(model, FlaxRobertaModel)
