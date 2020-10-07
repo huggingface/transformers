81951c48a655de10e97cea987a49ee84335dd564
patrickvonplaten <patrick.v.platen@gmail.com>
2020-10-08 00:56:54 +0200

    add tokenizer tests

--- src/transformers/tokenization_prophetnet.py
+++ src/transformers/tokenization_prophetnet.py
@@ -194,7 +194,6 @@ class ProphetNetTokenizer(PreTrainedTokenizer):
         Returns:
             :obj:`List[int]`: A list of integers in the range [0, 1]: 1 for a special token, 0 for a sequence token.
         """
-
         if already_has_special_tokens:
             if token_ids_1 is not None:
                 raise ValueError(
@@ -203,9 +202,9 @@ class ProphetNetTokenizer(PreTrainedTokenizer):
                 )
             return list(map(lambda x: 1 if x in [self.sep_token_id, self.cls_token_id] else 0, token_ids_0))
 
-        if token_ids_1 is not None:
-            return [1] + ([0] * len(token_ids_0)) + [1] + ([0] * len(token_ids_1)) + [1]
-        return [1] + ([0] * len(token_ids_0)) + [1]
+        if token_ids_1 is None:
+            return ([0] * len(token_ids_0)) + [1]
+        return ([0] * len(token_ids_0)) + [1] + ([0] * len(token_ids_1)) + [1]
 
     def create_token_type_ids_from_sequences(
         self, token_ids_0: List[int], token_ids_1: Optional[List[int]] = None
