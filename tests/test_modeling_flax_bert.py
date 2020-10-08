958e1793c4c75f2db72421dccbac04200163a9b2
Morgan Funtowicz <funtowiczmo@gmail.com>
2020-10-08 11:45:30 +0200

    Update the unittest to use TensorType.JAX
    Signed-off-by: Morgan Funtowicz <funtowiczmo@gmail.com>

--- tests/test_modeling_flax_bert.py
+++ tests/test_modeling_flax_bert.py
@@ -2,7 +2,7 @@ import unittest
 
 from numpy import ndarray
 
-from transformers import is_flax_available, is_torch_available
+from transformers import is_flax_available, is_torch_available, TensorType
 from transformers.testing_utils import require_flax, require_torch
 from transformers.tokenization_bert import BertTokenizerFast
 
@@ -28,9 +28,10 @@ class FlaxBertModelTest(unittest.TestCase):
             pt_model = BertModel.from_pretrained("bert-base-cased")
 
             # Check for simple input
-            pt_inputs = tokenizer.encode_plus("This is a simple input", return_tensors="pt")
+            pt_inputs = tokenizer.encode_plus("This is a simple input", return_tensors=TensorType.PYTORCH)
+            fx_inputs = tokenizer.encode_plus("This is a simple input", return_tensors=TensorType.JAX)
             pt_outputs = pt_model(**pt_inputs)
-            fx_outputs = fx_model(**{k: v.numpy() for k, v in pt_inputs.items()})
+            fx_outputs = fx_model(**fx_inputs)
 
             self.assertEqual(len(fx_outputs), len(pt_outputs), "Output lengths differ between Flax and PyTorch")
 
--- tests/test_modeling_flax_roberta.py
+++ tests/test_modeling_flax_roberta.py
@@ -2,7 +2,7 @@ import unittest
 
 from numpy import ndarray
 
-from transformers import is_flax_available, is_torch_available
+from transformers import is_flax_available, is_torch_available, TensorType
 from transformers.testing_utils import require_flax, require_torch
 from transformers.tokenization_roberta import RobertaTokenizerFast
 
@@ -18,7 +18,7 @@ if is_torch_available():
 
 @require_flax
 @require_torch
-class FlaxBertModelTest(unittest.TestCase):
+class FlaxRobertaModelTest(unittest.TestCase):
     def test_from_pytorch(self):
         torch.set_grad_enabled(False)
 
@@ -28,9 +28,10 @@ class FlaxBertModelTest(unittest.TestCase):
             pt_model = RobertaModel.from_pretrained("roberta-base")
 
             # Check for simple input
-            pt_inputs = tokenizer.encode_plus("This is a simple input", return_tensors="pt")
+            pt_inputs = tokenizer.encode_plus("This is a simple input", return_tensors=TensorType.PYTORCH)
+            fx_inputs = tokenizer.encode_plus("This is a simple input", return_tensors=TensorType.JAX)
             pt_outputs = pt_model(**pt_inputs)
-            fx_outputs = fx_model(**{k: v.numpy() for k, v in pt_inputs.items()})
+            fx_outputs = fx_model(**fx_inputs)
 
             self.assertEqual(len(fx_outputs), len(pt_outputs), "Output lengths differ between Flax and PyTorch")
 
