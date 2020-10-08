8081439191896b04ebc1d24f86f2b755b1a89314
Patrick von Platen <patrick.v.platen@gmail.com>
2020-10-08 16:50:52 +0000

    remove unused import

--- tests/test_modeling_prophetnet.py
+++ tests/test_modeling_prophetnet.py
@@ -19,7 +19,6 @@ import tempfile
 import unittest
 
 from transformers import is_torch_available
-from transformers.file_utils import cached_property
 from transformers.testing_utils import require_torch, slow, torch_device
 
 from .test_configuration_common import ConfigTester
