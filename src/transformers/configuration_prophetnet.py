ca9be62abc14a94aa2d5621781e92be051a901b1
weizhen <weizhen@mail.ustc.edu.cn>
2020-10-11 00:48:08 +0800

    Descriptions for Prophetnet Config File

--- src/transformers/configuration_prophetnet.py
+++ src/transformers/configuration_prophetnet.py
@@ -29,7 +29,69 @@ PROPHETNET_PRETRAINED_CONFIG_ARCHIVE_MAP = {
 
 class ProphetNetConfig(PretrainedConfig):
     r"""
-    Configuration class for ProphetNet. Parameters are renamed from the fairseq implementation
+    This is the configuration class to store the configuration of a :class:`~transformers.ProphetNetModel`. It is used to
+    instantiate a ProphetNet model according to the specified arguments, defining the model architecture.
+
+    Configuration objects inherit from  :class:`~transformers.PretrainedConfig` and can be used
+    to control the model outputs. Read the documentation from  :class:`~transformers.PretrainedConfig`
+    for more information.
+
+    Args:
+        activation_dropout (:obj:`float`, `optional`, defaults to 0.1):
+            The dropout ratio for activations inside the fully connected layer.
+        activation_function (:obj:`str` or :obj:`function`, `optional`, defaults to :obj:`"gelu"`):
+            The non-linear activation function (function or string) in the encoder and pooler.
+            If string, :obj:`"gelu"`, :obj:`"relu"`, :obj:`"swish"` and :obj:`"gelu_new"` are supported.
+        vocab_size (:obj:`int`, `optional`, defaults to 30522):
+            Vocabulary size of the BERT model. Defines the number of different tokens that can be represented by the
+            :obj:`inputs_ids` passed when calling :class:`~transformers.ProphetNetModel`.
+        hidden_size (:obj:`int`, `optional`, defaults to 1024):
+            Dimensionality of the layers and the pooler layer.
+        encoder_ffn_dim (:obj:`int`, `optional`, defaults to 4096):
+            Dimensionality of the "intermediate" (often named feed-forward) layer in decoder.
+        num_encoder_layers (:obj:`int`, `optional`, defaults to 12):
+            Number of encoder layers.
+        num_encoder_attention_heads (:obj:`int`, `optional`, defaults to 16):
+            Number of attention heads for each attention layer in the Transformer encoder.
+        decoder_ffn_dim (:obj:`int`, `optional`, defaults to 4096):
+            Dimensionality of the "intermediate" (often named feed-forward) layer in decoder.
+        num_decoder_layers (:obj:`int`, `optional`, defaults to 12):
+            Number of decoder layers.
+        num_decoder_attention_heads (:obj:`int`, `optional`, defaults to 16):
+            Number of attention heads for each attention layer in the Transformer decoder.
+        attention_dropout (:obj:`float`, `optional`, defaults to 0.1):
+            The dropout ratio for the attention probabilities.
+        dropout (:obj:`float`, `optional`, defaults to 0.1):
+            The dropout probabilitiy for all fully connected layers in the embeddings, encoder, and pooler.
+        max_position_embeddings (:obj:`int`, `optional`, defaults to 512):
+            The maximum sequence length that this model might ever be used with.
+            Typically set this to something large just in case (e.g., 512 or 1024 or 2048).
+        init_std (:obj:`float`, `optional`, defaults to 0.02):
+            The standard deviation of the truncated_normal_initializer for initializing all weight matrices.
+        is_encoder_decoder (:obj:`bool`, `optional`, defaults to :obj:`True`):
+            Whether this is an encoder/decoder model.
+        pad_token_id (:obj:`int`, `optional`, defaults to 1)
+            Padding token id.
+        bos_token_id (:obj:`int`, `optional`, defaults to 0)
+            Beginning of stream token id.
+        eos_token_id (:obj:`int`, `optional`, defaults to 2)
+            End of stream token id.
+        ngram (:obj:`int`, `optional`, defaults to 2)
+            Number of future tokens to predict.
+            Set to 1 to be same as traditional Language model to predict next first token.
+        num_buckets (:obj:`int`, `optional`, defaults to 32)
+            The number of buckets to use for each attention layer.
+            This is for relative position calculation. See the `T5 paper
+            <see https://arxiv.org/abs/1910.10683>`__ for more details.
+        relative_max_distance (:obj:`int`, `optional`, defaults to 128)
+            Relative distances greater than this number will be put into the last same bucket.
+            This is for relative position calculation. See the `T5 paper
+            <see https://arxiv.org/abs/1910.10683>`__ for more details.
+        disable_ngram_loss (:obj:`bool`, `optional`, defaults to :obj:`False`):
+            Whether be trained predicting only the next first token.
+        eps (:obj:`float`, `optional`, defaults to 0.0):
+            Controls the ``epsilon`` parameter value for label
+            smoothing in the loss calculation. If set to 0, no label smoothing is performed.
     """
     model_type = "prophetnet"
 
@@ -45,8 +107,6 @@ class ProphetNetConfig(PretrainedConfig):
         decoder_ffn_dim=4096,
         num_decoder_layers=12,
         num_decoder_attention_heads=16,
-        encoder_layerdrop=0.1,
-        decoder_layerdrop=0.1,
         attention_dropout=0.1,
         dropout=0.1,
         max_position_embeddings=512,
@@ -74,8 +134,6 @@ class ProphetNetConfig(PretrainedConfig):
         self.encoder_ffn_dim = encoder_ffn_dim
         self.num_encoder_layers = num_encoder_layers
         self.num_encoder_attention_heads = num_encoder_attention_heads
-        self.encoder_layerdrop = encoder_layerdrop
-        self.decoder_layerdrop = decoder_layerdrop
         self.decoder_ffn_dim = decoder_ffn_dim
         self.num_decoder_layers = num_decoder_layers
         self.num_decoder_attention_heads = num_decoder_attention_heads
