ecb4e9f724bee6a819c3c82175a96623df625750
Noam Wies <noamwies@gmail.com>
2020-10-13 15:30:46 +0300

    return support for pytorch < 1.2

--- src/transformers/trainer.py
+++ src/transformers/trainer.py
@@ -101,6 +101,11 @@ else:
     _use_native_amp = True
     from torch.cuda.amp import autocast
 
+if version.parse(torch.__version__) < version.parse("1.2"):
+    _use_ddp_no_sync = False
+else:
+    _use_ddp_no_sync = True
+
 if is_datasets_available():
     import datasets
 
@@ -687,7 +692,11 @@ class Trainer:
                 if (step + 1) % self.args.gradient_accumulation_steps == 0:
                     self.control = self.callback_handler.on_step_begin(self.args, self.state, self.control)
 
-                if ((step + 1) % self.args.gradient_accumulation_steps != 0) and self.args.local_rank != -1:
+                if (
+                    ((step + 1) % self.args.gradient_accumulation_steps != 0)
+                    and self.args.local_rank != -1
+                    and _use_ddp_no_sync
+                ):
                     with model.no_sync():
                         tr_loss += self.training_step(model, inputs)
                 else:
