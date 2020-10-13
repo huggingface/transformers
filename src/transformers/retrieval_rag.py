829e6a3b7346e93ab5b8e98d558a1eab2df70d9b
Quentin Lhoest <lhoest.q@gmail.com>
2020-10-13 18:30:46 +0200

    docs

--- src/transformers/retrieval_rag.py
+++ src/transformers/retrieval_rag.py
@@ -329,17 +329,18 @@ class RagRetriever:
             The tokenizer used for the generator part of the RagModel.
 
     Examples::
-        To load the default "wiki_dpr" dataset with 21M passages from wikipedia (index name is 'compressed' or 'exact')
+
+        >>> # To load the default "wiki_dpr" dataset with 21M passages from wikipedia (index name is 'compressed' or 'exact')
         >>> from transformers import RagRetriever
         >>> retriever = RagRetriever.from_pretrained('facebook/dpr-ctx_encoder-single-nq-base', index_name='compressed')
 
-        To load your own indexed dataset built with the datasets library. More info on how to build the indexed dataset in examples/rag/use_own_knowledge_dataset.py
+        >>> # To load your own indexed dataset built with the datasets library. More info on how to build the indexed dataset in examples/rag/use_own_knowledge_dataset.py
         >>> from transformers import RagRetriever
         >>> dataset_path = "path/to/my/dataset"  # dataset saved via `dataset.save_to_disk(...)`
-        >>> index_path = "path/to/my/index.faiss  # faiss index saved via `dataset.get_index("embeddings").save(...)`
+        >>> index_path = "path/to/my/index.faiss"  # faiss index saved via `dataset.get_index("embeddings").save(...)`
         >>> retriever = RagRetriever.from_pretrained('facebook/dpr-ctx_encoder-single-nq-base', index_name='custom', dataset=dataset_path, index_path=index_path)
 
-        To load the legacy index built originally for Rag's paper
+        >>> # To load the legacy index built originally for Rag's paper
         >>> from transformers import RagRetriever
         >>> retriever = RagRetriever.from_pretrained('facebook/dpr-ctx_encoder-single-nq-base', index_name='legacy')
 
