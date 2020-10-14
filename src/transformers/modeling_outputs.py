a224fca6ff051e8e65f00a93088ea44492dddbd8
oriyor <39461788+oriyor@users.noreply.github.com>
2020-10-14 14:57:21 +0300

    multi task roberta
    add a RobertaForMultiTask model.
    
    the model has both RobertaLMHead and RobertaClassificationHead.
    when doing the forward pass, we provide an extra 'task' field.
    
    when doing the 'mlm' task, we use RobertaLMHead similarly to RobertaForMaskedLM.
    
    when doing the 'classification' task we use the RobertaClassificationHead, similarly to RobertaForSequenceClassification.

--- src/transformers/modeling_outputs.py
+++ src/transformers/modeling_outputs.py
@@ -391,6 +391,33 @@ class SequenceClassifierOutput(ModelOutput):
     hidden_states: Optional[Tuple[torch.FloatTensor]] = None
     attentions: Optional[Tuple[torch.FloatTensor]] = None
 
+@dataclass
+class MultiTaskOutput(ModelOutput):
+    """
+    Base class for outputs multitask models.
+
+    Args:
+        loss (:obj:`torch.FloatTensor` of shape :obj:`(1,)`, `optional`, returned when :obj:`labels` is provided):
+            Classification (or regression if config.num_labels==1) loss.
+        logits (:obj:`torch.FloatTensor` of shape :obj:`(batch_size, config.num_labels)`):
+            Classification (or regression if config.num_labels==1) scores (before SoftMax).
+        hidden_states (:obj:`tuple(torch.FloatTensor)`, `optional`, returned when ``output_hidden_states=True`` is passed or when ``config.output_hidden_states=True``):
+            Tuple of :obj:`torch.FloatTensor` (one for the output of the embeddings + one for the output of each layer)
+            of shape :obj:`(batch_size, sequence_length, hidden_size)`.
+
+            Hidden-states of the model at the output of each layer plus the initial embedding outputs.
+        attentions (:obj:`tuple(torch.FloatTensor)`, `optional`, returned when ``output_attentions=True`` is passed or when ``config.output_attentions=True``):
+            Tuple of :obj:`torch.FloatTensor` (one for each layer) of shape
+            :obj:`(batch_size, num_heads, sequence_length, sequence_length)`.
+
+            Attentions weights after the attention softmax, used to compute the weighted average in the self-attention
+            heads.
+    """
+
+    loss: Optional[torch.FloatTensor] = None
+    logits: torch.FloatTensor = None
+    hidden_states: Optional[Tuple[torch.FloatTensor]] = None
+    attentions: Optional[Tuple[torch.FloatTensor]] = None
 
 @dataclass
 class Seq2SeqSequenceClassifierOutput(ModelOutput):
--- src/transformers/modeling_roberta.py
+++ src/transformers/modeling_roberta.py
@@ -39,6 +39,7 @@ from .modeling_outputs import (
     QuestionAnsweringModelOutput,
     SequenceClassifierOutput,
     TokenClassifierOutput,
+    MultiTaskOutput,
 )
 from .modeling_utils import (
     PreTrainedModel,
@@ -914,6 +915,162 @@ class RobertaForMaskedLM(RobertaPreTrainedModel):
             hidden_states=outputs.hidden_states,
             attentions=outputs.attentions,
         )
+
+
+@add_start_docstrings("""RoBERTa Model with `language modeling` and  `binary classification` heads on top. """, ROBERTA_START_DOCSTRING)
+class RobertaForMultiTask(RobertaPreTrainedModel):
+
+    #todo predictions.decoder.bias and pooler don't appear for sequence classification, we need to verify what they should be in our case
+    #we need to verify if we need to remove them in that case
+    authorized_missing_keys = [r"position_ids", r"predictions.decoder.bias"]
+    authorized_unexpected_keys = [r"pooler"]
+
+    def __init__(self, config):
+        super().__init__(config)
+
+        #todo this is only in mlm
+        if config.is_decoder:
+            logger.warning(
+                "If you want to use `RobertaForMaskedLM` make sure `config.is_decoder=False` for "	
+                "bi-directional self-attention."
+            )
+
+        #shared
+        self.roberta = RobertaModel(config, add_pooling_layer=False)
+
+        #classification
+        self.num_labels = config.num_labels
+        self.classifier = RobertaClassificationHead(config)
+
+        #lm
+        self.lm_head = RobertaLMHead(config)
+        self.init_weights()
+
+    #todo this is only in mlm
+    def get_output_embeddings(self):
+        return self.lm_head.decoder
+
+    @add_start_docstrings_to_callable(ROBERTA_INPUTS_DOCSTRING.format("batch_size, sequence_length"))
+    @add_code_sample_docstrings(
+        tokenizer_class=_TOKENIZER_FOR_DOC,
+        checkpoint="roberta-base",
+        output_type=MaskedLMOutput,
+        config_class=_CONFIG_FOR_DOC,
+        mask="<mask>",
+    )
+    def forward(
+        self,
+        input_ids=None,
+        attention_mask=None,
+        token_type_ids=None,
+        position_ids=None,
+        head_mask=None,
+        inputs_embeds=None,
+        encoder_hidden_states=None,
+        encoder_attention_mask=None,
+        labels=None,
+        output_attentions=None,
+        output_hidden_states=None,
+        return_dict=None,
+        task='mlm',
+        **kwargs
+    ):
+        r"""
+        labels (:obj:`torch.LongTensor` of shape :obj:`(batch_size, sequence_length)`, `optional`):
+            Labels for computing the masked language modeling loss.
+            Indices should be in ``[-100, 0, ..., config.vocab_size]`` (see ``input_ids`` docstring)
+            Tokens with indices set to ``-100`` are ignored (masked), the loss is only computed for the tokens with labels
+            in ``[0, ..., config.vocab_size]``
+        kwargs (:obj:`Dict[str, any]`, optional, defaults to `{}`):
+            Used to hide legacy arguments that have been deprecated.
+        task determines which task are we doing in this batch: binary classification or mlm
+        """
+        #todo when calling the model, we need to specify which task are we currently doing: 'mlm' or 'cls'
+        #todo RobertaForSequenceClassification doesn't have encoder_hidden_states and encoder_attention_mask, we need to verify they are None in that case
+
+        #if we are doing mlm task do the same as RobertaForMaskedLM
+        if task == 'mlm':
+            if "masked_lm_labels" in kwargs:
+                warnings.warn(
+                    "The `masked_lm_labels` argument is deprecated and will be removed in a future version, use `labels` instead.",
+                    FutureWarning,
+                )
+                labels = kwargs.pop("masked_lm_labels")
+            assert kwargs == {}, f"Unexpected keyword arguments: {list(kwargs.keys())}."
+            return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+
+            outputs = self.roberta(
+                input_ids,
+                attention_mask=attention_mask,
+                token_type_ids=token_type_ids,
+                position_ids=position_ids,
+                head_mask=head_mask,
+                inputs_embeds=inputs_embeds,
+                encoder_hidden_states=encoder_hidden_states,
+                encoder_attention_mask=encoder_attention_mask,
+                output_attentions=output_attentions,
+                output_hidden_states=output_hidden_states,
+                return_dict=return_dict,
+            )
+            sequence_output = outputs[0]
+            prediction_scores = self.lm_head(sequence_output)
+
+            masked_lm_loss = None
+            if labels is not None:
+                loss_fct = CrossEntropyLoss()
+                masked_lm_loss = loss_fct(prediction_scores.view(-1, self.config.vocab_size), labels.view(-1))
+
+            if not return_dict:
+                output = (prediction_scores,) + outputs[2:]
+                return ((masked_lm_loss,) + output) if masked_lm_loss is not None else output
+
+            multi_task_output = MultiTaskOutput(
+                loss=masked_lm_loss,
+                logits=prediction_scores,
+                hidden_states=outputs.hidden_states,
+                attentions=outputs.attentions,
+            )
+
+        #if we are doing classification task, do the same as RobertaForSequenceClassification
+        if task == 'cls':
+
+            return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+            outputs = self.roberta(
+                input_ids,
+                attention_mask=attention_mask,
+                token_type_ids=token_type_ids,
+                position_ids=position_ids,
+                head_mask=head_mask,
+                inputs_embeds=inputs_embeds,
+                output_attentions=output_attentions,
+                output_hidden_states=output_hidden_states,
+                return_dict=return_dict,
+            )
+            sequence_output = outputs[0]
+            logits = self.classifier(sequence_output)
+
+            loss = None
+            if labels is not None:
+                if self.num_labels == 1:
+                    #  We are doing regression
+                    loss_fct = MSELoss()
+                    loss = loss_fct(logits.view(-1), labels.view(-1))
+                else:
+                    loss_fct = CrossEntropyLoss()
+                    loss = loss_fct(logits.view(-1, self.num_labels), labels.view(-1))
+
+            if not return_dict:
+                output = (logits,) + outputs[2:]
+                return ((loss,) + output) if loss is not None else output
+
+            multi_task_output = MultiTaskOutput(
+                loss=loss,
+                logits=logits,
+                hidden_states=outputs.hidden_states,
+                attentions=outputs.attentions
+            )
+
+        return multi_task_output
 
 
 class RobertaLMHead(nn.Module):
