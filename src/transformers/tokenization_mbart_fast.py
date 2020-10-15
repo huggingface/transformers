08f2a2360402ab122d6452c6c7151124c8bef49a
Thomas Wolf <thomwolf@users.noreply.github.com>
2020-10-15 13:33:01 +0200

    fix mbart

--- src/transformers/tokenization_mbart_fast.py
+++ src/transformers/tokenization_mbart_fast.py
@@ -101,8 +101,8 @@ class MBartTokenizerFast(XLMRobertaTokenizerFast):
     prefix_tokens: List[int] = []
     suffix_tokens: List[int] = []
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(self, *args, tokenizer_file=None, **kwargs):
+        super().__init__(*args, tokenizer_file=tokenizer_file, **kwargs)
 
         self.cur_lang_code = self.convert_tokens_to_ids("en_XX")
         self.set_src_lang_special_tokens(kwargs.get("src_lang", "en_XX"))
