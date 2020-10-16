13286cf255b009fafcfa7f34d9673ec6ea3b2be8
Raza Habib <rhabib01@gmail.com>
2020-10-16 18:38:19 +0100

    Remove duplicated mish activation function

--- src/transformers/activations.py
+++ src/transformers/activations.py
@@ -67,6 +67,3 @@ def get_activation(activation_string):
     else:
         raise KeyError("function {} not found in ACT2FN mapping {}".format(activation_string, list(ACT2FN.keys())))
 
-
-def mish(x):
-    return x * torch.tanh(torch.nn.functional.softplus(x))
