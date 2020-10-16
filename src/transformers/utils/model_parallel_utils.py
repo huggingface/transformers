6fc3849dce3d60d2291dee2ea3c7f11d19d1c60c
alexorona <alexorona@gmail.com>
2020-10-15 19:29:29 -0700

    Update model_parallel_utils.py

--- src/transformers/utils/model_parallel_utils.py
+++ src/transformers/utils/model_parallel_utils.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 
-def analyze_device_map(device_map, num_blocks):
+def assert_device_map(device_map, num_blocks):
     blocks = list(range(0, num_blocks))
     
     device_map_blocks = [item for sublist in list(device_map.values()) for item in sublist]
@@ -17,3 +17,8 @@ def analyze_device_map(device_map, num_blocks):
     assert len(duplicate_blocks) == 0, 'Duplicate attention blocks specified in device_map. Attention blocks must be specified to one device. These attention blocks were specified more than once: ' + str(duplicate_blocks)
     assert len(missing_blocks) == 0, 'There are attention blocks for this model that are not specified in the device_map. Add these attention_blocks to a device on the device_map:' + str(missing_blocks)
     assert len(extra_blocks) == 0, 'The device_map contains more attention blocks than this model has. Remove these from the device_map:' + str(extra_blocks)
+
+def get_device_map(n_layers, n_devices):
+    '''Returns a dictionary of layers distributed evenly across all devices.'''
+    device_map = {i: list(range(int(i * n_layers / n_devices), int((i+1) * n_layers / n_devices))) for i in range(n_devices)}
+    return device_map
