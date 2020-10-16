246e06957555cf2d2ee50dc243361363091b6ac9
Stas Bekman <stas00@users.noreply.github.com>
2020-10-15 20:23:01 -0700

    [testing] disable FutureWarning in examples tests
    same as tests/conftest.py, we can't resolve those warning, so turn the noise off.

--- examples/conftest.py
+++ examples/conftest.py
@@ -9,3 +9,7 @@ from os.path import abspath, dirname, join
 # 'pip install -e .[dev]' when switching between checkouts and running tests.
 git_repo_path = abspath(join(dirname(dirname(__file__)), "src"))
 sys.path.insert(1, git_repo_path)
+
+# silence FutureWarning warnings in tests since often we can't act on them until
+# they become normal warnings - i.e. the tests still need to test the current functionality
+warnings.simplefilter(action="ignore", category=FutureWarning)
