9fa2ad60c5dc8d2a01d029c68c3e3888ef2d47b3
Stas Bekman <stas00@users.noreply.github.com>
2020-10-15 21:22:43 -0700

    [seq2seq] get_git_info fails gracefully (#7843)
    Co-authored-by: Sam Shleifer <sshleifer@gmail.com>

--- examples/seq2seq/utils.py
+++ examples/seq2seq/utils.py
@@ -457,14 +457,22 @@ def load_json(path):
 
 
 def get_git_info():
-    repo = git.Repo(search_parent_directories=True)
-    repo_infos = {
-        "repo_id": str(repo),
-        "repo_sha": str(repo.head.object.hexsha),
-        "repo_branch": str(repo.active_branch),
-        "hostname": str(socket.gethostname()),
-    }
-    return repo_infos
+    try:
+        repo = git.Repo(search_parent_directories=True)
+        repo_infos = {
+            "repo_id": str(repo),
+            "repo_sha": str(repo.head.object.hexsha),
+            "repo_branch": str(repo.active_branch),
+            "hostname": str(socket.gethostname()),
+        }
+        return repo_infos
+    except TypeError:
+        return {
+            "repo_id": None,
+            "repo_sha": None,
+            "repo_branch": None,
+            "hostname": None,
+        }
 
 
 ROUGE_KEYS = ["rouge1", "rouge2", "rougeL", "rougeLsum"]
