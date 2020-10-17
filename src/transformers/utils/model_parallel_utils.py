d0be398f505e32b133fdd13fefc934d0b79d4741
alexorona <alexorona@gmail.com>
2020-10-16 22:05:49 -0700

    Update model_parallel_utils.py

--- src/transformers/utils/model_parallel_utils.py
+++ src/transformers/utils/model_parallel_utils.py
@@ -1,8 +1,9 @@
 # coding=utf-8
 
+
 def assert_device_map(device_map, num_blocks):
     blocks = list(range(0, num_blocks))
-    
+
     device_map_blocks = [item for sublist in list(device_map.values()) for item in sublist]
 
     # Duplicate check
@@ -13,12 +14,25 @@ def assert_device_map(device_map, num_blocks):
     # Missing blocks
     missing_blocks = [i for i in blocks if i not in device_map_blocks]
     extra_blocks = [i for i in device_map_blocks if i not in blocks]
-    
-    assert len(duplicate_blocks) == 0, 'Duplicate attention blocks specified in device_map. Attention blocks must be specified to one device. These attention blocks were specified more than once: ' + str(duplicate_blocks)
-    assert len(missing_blocks) == 0, 'There are attention blocks for this model that are not specified in the device_map. Add these attention_blocks to a device on the device_map:' + str(missing_blocks)
-    assert len(extra_blocks) == 0, 'The device_map contains more attention blocks than this model has. Remove these from the device_map:' + str(extra_blocks)
+
+    assert len(duplicate_blocks) == 0, (
+        "Duplicate attention blocks specified in device_map. Attention blocks must be specified to one device. These attention blocks were specified more than once: "
+        + str(duplicate_blocks)
+    )
+    assert len(missing_blocks) == 0, (
+        "There are attention blocks for this model that are not specified in the device_map. Add these attention_blocks to a device on the device_map:"
+        + str(missing_blocks)
+    )
+    assert (
+        len(extra_blocks) == 0
+    ), "The device_map contains more attention blocks than this model has. Remove these from the device_map:" + str(
+        extra_blocks
+    )
+
 
 def get_device_map(n_layers, n_devices):
-    '''Returns a dictionary of layers distributed evenly across all devices.'''
-    device_map = {i: list(range(int(i * n_layers / n_devices), int((i+1) * n_layers / n_devices))) for i in range(n_devices)}
+    """Returns a dictionary of layers distributed evenly across all devices."""
+    device_map = {
+        i: list(range(int(i * n_layers / n_devices), int((i + 1) * n_layers / n_devices))) for i in range(n_devices)
+    }
     return device_map
