8425fc6c99efad99a7978a7b344621bedafd8873
Raza Habib <rhabib01@gmail.com>
2020-10-17 15:10:48 +0100

    Update activations.py

--- src/transformers/activations.py
+++ src/transformers/activations.py
@@ -66,4 +66,3 @@ def get_activation(activation_string):
         return ACT2FN[activation_string]
     else:
         raise KeyError("function {} not found in ACT2FN mapping {}".format(activation_string, list(ACT2FN.keys())))
-
