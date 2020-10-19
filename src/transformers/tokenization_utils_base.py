406a49dfe4a7bced24cd63b5875f3192c72598dd
AndreaSottana <48888970+AndreaSottana@users.noreply.github.com>
2020-10-19 07:14:29 +0100

    Fix small type hinting error (#7820)
    * Fix small type hinting error
    
    * Update tokenization_utils_base.py
    
    * Update src/transformers/tokenization_utils_base.py
    
    Co-authored-by: Lysandre Debut <lysandre@huggingface.co>
    
    Co-authored-by: Lysandre Debut <lysandre@huggingface.co>

--- src/transformers/tokenization_utils_base.py
+++ src/transformers/tokenization_utils_base.py
@@ -602,7 +602,7 @@ class BatchEncoding(UserDict):
         return self
 
     @torch_required
-    def to(self, device: str) -> "BatchEncoding":
+    def to(self, device: Union[str, "torch.device"]) -> "BatchEncoding":
         """
         Send all values to device by calling :obj:`v.to(device)` (PyTorch only).
 
