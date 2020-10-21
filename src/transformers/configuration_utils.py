3d73fbf047a9703aa4ed0136f13f1b1fe3ba1f40
Patrick von Platen <patrick.v.platen@gmail.com>
2020-10-21 10:31:00 +0000

    fix config save

--- src/transformers/configuration_utils.py
+++ src/transformers/configuration_utils.py
@@ -476,11 +476,18 @@ class PretrainedConfig(object):
         # get the default config dict
         default_config_dict = PretrainedConfig().to_dict()
 
+        # get class specific config dict
+        class_config_dict = self.__class__().to_dict()
+
         serializable_config_dict = {}
 
         # only serialize values that differ from the default config
         for key, value in config_dict.items():
-            if key not in default_config_dict or value != default_config_dict[key]:
+            if (
+                key not in default_config_dict
+                or value != default_config_dict[key]
+                or (key in class_config_dict and value != class_config_dict[key])
+            ):
                 serializable_config_dict[key] = value
 
         return serializable_config_dict
