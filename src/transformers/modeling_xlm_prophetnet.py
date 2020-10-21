9b6610f7f6312bd7f8a88fbe55ff8a03550b721b
Patrick von Platen <patrick.v.platen@gmail.com>
2020-10-21 17:27:20 +0200

    [ProphetNet] Correct Doc string example (#7944)
    * correct xlm prophetnet auto model and examples
    
    * fix line-break docs

--- src/transformers/modeling_xlm_prophetnet.py
+++ src/transformers/modeling_xlm_prophetnet.py
@@ -144,14 +144,21 @@ class XLMProphetNetForCausalLM(ProphetNetForCausalLM):
         >>> logits = outputs.logits
 
         >>> # Model can also be used with EncoderDecoder framework
-        >>> from transformers import BertTokenizer, EncoderDecoderModel
+        >>> from transformers import EncoderDecoderModel, XLMProphetNetTokenizer, XLMRobertaTokenizer
         >>> import torch
 
-        >>> tokenizer = BertTokenizer.from_pretrained('bert-uncased-large')
-        >>> model = EncoderDecoderModel.from_encoder_decoder_pretrained("bert-uncased-large", "patrickvonplaten/xprophetnet-decoder-clm-large-uncased")
+        >>> tokenizer_enc = XLMRobertaTokenizer.from_pretrained('xlm-roberta-large')
+        >>> tokenizer_dec = XLMProphetNetTokenizer.from_pretrained('microsoft/xprophetnet-large-wiki100-cased')
+        >>> model = EncoderDecoderModel.from_encoder_decoder_pretrained("xlm-roberta-large", "patrickvonplaten/xprophetnet-decoder-clm-large-uncased")
 
-        >>> inputs = tokenizer("Hello, my dog is cute", return_tensors="pt")
-        >>> outputs = model(input_ids=inputs["input_ids"], labels=inputs["input_ids"])
+        >>> ARTICLE = (
+        ... "the us state department said wednesday it had received no "
+        ... "formal word from bolivia that it was expelling the us ambassador there "
+        ... "but said the charges made against him are `` baseless ."
+        ... )
+        >>> input_ids = tokenizer_enc(ARTICLE, return_tensors="pt").input_ids
+        >>> labels = tokenizer_dec("us rejects charges against its ambassador in bolivia", return_tensors="pt").input_ids
+        >>> outputs = model(input_ids=input_ids, decoder_input_ids=labels[:, :-1], labels=labels[:, 1:], return_dict=True)
 
         >>> loss = outputs.loss
     """
