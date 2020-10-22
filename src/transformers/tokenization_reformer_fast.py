891559cf64209df0727cbe39351d467fb8de4edb
patrickvonplaten <patrick.v.platen@gmail.com>
2020-10-22 22:50:25 +0200

    remove reformer pad_token_id

--- src/transformers/tokenization_reformer_fast.py
+++ src/transformers/tokenization_reformer_fast.py
@@ -102,7 +102,6 @@ class ReformerTokenizerFast(PreTrainedTokenizerFast):
         tokenizer_file=None,
         eos_token="</s>",
         unk_token="<unk>",
-        pad_token="<pad>",
         additional_special_tokens=[],
         **kwargs
     ):
@@ -111,7 +110,6 @@ class ReformerTokenizerFast(PreTrainedTokenizerFast):
             tokenizer_file=tokenizer_file,
             eos_token=eos_token,
             unk_token=unk_token,
-            pad_token=pad_token,
             additional_special_tokens=additional_special_tokens,
             **kwargs,
         )
