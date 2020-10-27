c2f7910bdf0da1d6d553c9d287ddfe4678a89b5f
Sylvain Gugger <sylvain.gugger@gmail.com>
2020-10-27 17:12:13 -0400

    More comments

--- examples/language-modeling/run_clm.py
+++ examples/language-modeling/run_clm.py
@@ -257,13 +257,15 @@ def main():
     else:
         block_size = min(data_args.block_size, tokenizer.max_len)
 
+    # Main function that will concatenate all texts from our dataset and generate chunks of block_size.
     def group_texts(examples):
         # Concatenate all texts.
         concatenated_examples = {k: sum(examples[k], []) for k in examples.keys()}
         total_length = len(concatenated_examples[list(examples.keys())[0]])
-        # We drop the small remainder
+        # We drop the small remainder, we could add padding if the model supported it instead of this drop, you can
+        # customize this part to your needs.
         total_length = (total_length // block_size) * block_size
-        # Split by chunks of max_len
+        # Split by chunks of max_len.
         result = {
             k: [t[i : i + block_size] for i in range(0, total_length, block_size)]
             for k, t in concatenated_examples.items()
@@ -271,6 +273,9 @@ def main():
         result["labels"] = result["input_ids"].copy()
         return result
 
+    # Note that with `batched=True`, this map processes 1,000 texts together, so group_texts throws away a remainder
+    # for each of those groups of 1,000 texts. You can adjust that batch_size here but a higher value might be slower
+    # to preprocess.
     lm_datasets = tokenized_datasets.map(group_texts, batched=True, load_from_cache_file=not data_args.overwrite_cache)
 
     # Initialize our Trainer
