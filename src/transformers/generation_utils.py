98a61abe67353936adc80380a58481515e1f81ea
Patrick von Platen <patrick.v.platen@gmail.com>
2020-10-27 18:13:33 +0000

    correct typo

--- src/transformers/generation_utils.py
+++ src/transformers/generation_utils.py
@@ -64,7 +64,9 @@ class GenerationMixin:
 
     def _prepare_attention_mask(self, input_ids: torch.Tensor, pad_token_id: int, eos_token_id: int):
         is_pad_token_in_inputs_ids = (pad_token_id is not None) and (pad_token_id in input_ids)
-        is_pad_token_not_equal_to_eos_token_id = (eos_token_id is not None) and (pad_token_id != eos_token_id)
+        is_pad_token_not_equal_to_eos_token_id = (eos_token_id is None) or (
+            (eos_token_id is not None) and (pad_token_id != eos_token_id)
+        )
         if is_pad_token_in_inputs_ids and is_pad_token_not_equal_to_eos_token_id:
             return input_ids.ne(pad_token_id).long()
         else:
