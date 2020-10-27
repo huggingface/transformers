2618ca433e9fa8c44ae948e0af654eb06ba6af55
Patrick von Platen <patrick.v.platen@gmail.com>
2020-10-27 18:07:47 +0000

    small fix

--- src/transformers/generation_utils.py
+++ src/transformers/generation_utils.py
@@ -62,8 +62,10 @@ class GenerationMixin:
             raise ValueError("`bos_token_id` has to be defined when no `input_ids` are provided.")
         return torch.ones((1, 1), dtype=torch.long, device=next(self.parameters()).device) * bos_token_id
 
-    def _prepare_attention_mask(self, input_ids: torch.Tensor, pad_token_id: int):
-        if (pad_token_id is not None) and (pad_token_id in input_ids):
+    def _prepare_attention_mask(self, input_ids: torch.Tensor, pad_token_id: int, eos_token_id: int):
+        is_pad_token_in_inputs_ids = (pad_token_id is not None) and (pad_token_id in input_ids)
+        is_pad_token_not_equal_to_eos_token_id = (eos_token_id is not None) and (pad_token_id != eos_token_id)
+        if is_pad_token_in_inputs_ids and is_pad_token_not_equal_to_eos_token_id:
             return input_ids.ne(pad_token_id).long()
         else:
             return input_ids.new_ones(input_ids.shape)
@@ -393,7 +395,7 @@ class GenerationMixin:
 
         if model_kwargs.get("attention_mask", None) is None:
             # init `attention_mask` depending on `pad_token_id`
-            model_kwargs["attention_mask"] = self._prepare_attention_mask(input_ids, pad_token_id)
+            model_kwargs["attention_mask"] = self._prepare_attention_mask(input_ids, pad_token_id, eos_token_id)
 
         # special case if pad_token_id is not defined
         if pad_token_id is None and eos_token_id is not None:
--- src/transformers/testing_utils.py
+++ src/transformers/testing_utils.py
@@ -88,8 +88,8 @@ def is_pipeline_test(test_case):
     """
     Decorator marking a test as a pipeline test.
 
-    Pipeline tests are skipped by default and we can run only them by setting RUN_PIPELINE_TEST environment variable to
-    a truthy value and selecting the is_pipeline_test pytest mark.
+    Pipeline tests are skipped by default and we can run only them by setting RUN_PIPELINE_TESTS environment variable
+    to a truthy value and selecting the is_pipeline_test pytest mark.
 
     """
     if not _run_pipeline_tests:
