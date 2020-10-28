37d40041bc0f7ae403da418251efae5d155cdb8b
Bram Vanroy <Bram.Vanroy@UGent.be>
2020-10-28 17:57:25 +0100

    Update wording for config

--- src/transformers/pipelines.py
+++ src/transformers/pipelines.py
@@ -2734,9 +2734,9 @@ def pipeline(
             identifier or an actual pretrained model configuration inheriting from
             :class:`~transformers.PretrainedConfig`.
 
-            If not provided, the default configuration file for the requested model will be used: either the given
-            :obj:`model`'s configuration, or - if no :obj:`model` was given - this :obj:`task`'s default model's
-            config.
+            If not provided, the default configuration file for the requested model will be used. That means that if
+            :obj:`model` is given, its default configuration will be used. However, if :obj:`model` is not supplied,
+            this :obj:`task`'s default model's config is used instead.
         tokenizer (:obj:`str` or :obj:`~transformers.PreTrainedTokenizer`, `optional`):
             The tokenizer that will be used by the pipeline to encode data for the model. This can be a model
             identifier or an actual pretrained tokenizer inheriting from :class:`~transformers.PreTrainedTokenizer`.
