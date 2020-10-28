b4cacb7a63473b527e3c723576bba81611bc3b16
Boris Dayma <boris.dayma@gmail.com>
2020-10-28 11:15:30 -0500

    fix(trainer_callback]: typo (#8121)

--- src/transformers/trainer_callback.py
+++ src/transformers/trainer_callback.py
@@ -148,7 +148,7 @@ class TrainerControl:
 
     def _new_step(self):
         """ Internal method that resets the variable for a new step. """
-        self.should_save_model = False
+        self.should_save = False
         self.should_evaluate = False
         self.should_log = False
 
