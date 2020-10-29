20f1fd32736a1de6eb7e786ce910bc02e7058714
Sam Shleifer <sshleifer@gmail.com>
2020-10-29 12:01:15 -0400

    [s2s] distillBART docs for paper replication (#8150)

--- examples/seq2seq/train_distilbart_cnn.sh
+++ examples/seq2seq/train_distilbart_cnn.sh
@@ -13,7 +13,7 @@ python finetune.py \
     --val_check_interval 0.25 \
     --n_val 500 \
     --num_train_epochs 2 \
-    --freeze_encoder --freeze_embeds --data_dir $CNN_DIR \
+    --freeze_encoder --freeze_embeds --data_dir cnn_dm \
     --max_target_length 142 --val_max_target_length=142 \
     --train_batch_size=$BS --eval_batch_size=$BS --gradient_accumulation_steps=$GAS \
     --model_name_or_path sshleifer/student_cnn_12_6 \
