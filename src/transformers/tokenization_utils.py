f245989b4af66fc0c8acef903965a4879d510f11
Santiago Castro <santi.1410@hotmail.com>
2020-10-29 02:57:52 -0400

    More fixes

--- src/transformers/tokenization_utils.py
+++ src/transformers/tokenization_utils.py
@@ -53,7 +53,7 @@ TOKENIZER_CONFIG_FILE = "tokenizer_config.json"
 
 def _is_whitespace(char):
     """Checks whether `char` is a whitespace character."""
-    # \t, \n, and \r are technically contorl characters but we treat them
+    # \t, \n, and \r are technically control characters but we treat them
     # as whitespace since they are generally considered as such.
     if char == " " or char == "\t" or char == "\n" or char == "\r":
         return True
