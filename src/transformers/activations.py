f6f137cc916e907b2eb36fbe2048d59896fb366c
TFUsers <howdygoop@gmail.com>
2020-10-29 19:50:25 -0700

    simplify optimized silu conditional and fix format

--- src/transformers/activations.py
+++ src/transformers/activations.py
@@ -39,19 +39,16 @@ def gelu_fast(x):
 
 def silu(x):
     """
-    See Gaussian Error Linear Units (Hendrycks et al., https://arxiv.org/abs/1606.08415) 
-    where the SiLU (Sigmoid Linear Unit) was originally introduced and coined,
-    and see Sigmoid-Weighted Linear Units for Neural Network Function Approximation 
-    in Reinforcement Learning (Elfwing et al., https://arxiv.org/abs/1702.03118) and Swish: 
-    a Self-Gated Activation Function (Ramachandran et al., https://arxiv.org/abs/1710.05941v1)
-    where the SiLU was experimented with later.
+    See Gaussian Error Linear Units (Hendrycks et al., https://arxiv.org/abs/1606.08415) where the SiLU (Sigmoid Linear
+    Unit) was originally introduced and coined, and see Sigmoid-Weighted Linear Units for Neural Network Function
+    Approximation in Reinforcement Learning (Elfwing et al., https://arxiv.org/abs/1702.03118) and Swish: a Self-Gated
+    Activation Function (Ramachandran et al., https://arxiv.org/abs/1710.05941v1) where the SiLU was experimented with
+    later.
     """
     return x * torch.sigmoid(x)
 
 
-if torch.__version__ < "1.7":
-    silu = silu
-else:
+if torch.__version__ >= "1.7":
     silu = F.silu
 
 
