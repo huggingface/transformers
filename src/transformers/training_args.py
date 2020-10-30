8f1c960ee75b0ebce4e94881ee2d5409a26a6239
Abi See <abigail.e.see@gmail.com>
2020-10-30 13:45:38 -0700

    Fix two bugs with --logging_first_step (#8193)
    * make sure that logging_first_step evaluates
    
    * fix bug with incorrect loss on logging_first_step
    
    * fix style
    
    * logging_first_step only logs, not evals

--- src/transformers/training_args.py
+++ src/transformers/training_args.py
@@ -250,7 +250,7 @@ class TrainingArguments:
     warmup_steps: int = field(default=0, metadata={"help": "Linear warmup over warmup_steps."})
 
     logging_dir: Optional[str] = field(default_factory=default_logdir, metadata={"help": "Tensorboard log dir."})
-    logging_first_step: bool = field(default=False, metadata={"help": "Log and eval the first global_step"})
+    logging_first_step: bool = field(default=False, metadata={"help": "Log the first global_step"})
     logging_steps: int = field(default=500, metadata={"help": "Log every X updates steps."})
     save_steps: int = field(default=500, metadata={"help": "Save checkpoint every X updates steps."})
     save_total_limit: Optional[int] = field(
