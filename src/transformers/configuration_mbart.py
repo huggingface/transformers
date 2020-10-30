00112c35393e4d81ef7593a3763dc626c0403e7b
TFUsers <25044281+TFUsers@users.noreply.github.com>
2020-10-30 12:09:10 -0700

    Replace swish with silu (#8166)
    * Replace swish with silu
    
    * revert nn.silu to nn.swish due to older version
    
    * simplify optimized silu conditional and fix format
    
    * Update activations.py
    
    * Update activations_tf.py
    
    * Update modeling_flax_utils.py
    
    * Update modeling_openai.py
    
    * add swish testcase
    
    * add pytorch swish testcase
    
    * Add more robust python version check
    
    * more formatting fixes
    
    Co-authored-by: TFUsers <TFUsers@gmail.com>

--- src/transformers/configuration_mbart.py
+++ src/transformers/configuration_mbart.py
@@ -55,7 +55,7 @@ class MBartConfig(BartConfig):
             Dimensionality of the "intermediate" (i.e., feed-forward) layer in decoder.
         activation_function (:obj:`str` or :obj:`function`, `optional`, defaults to :obj:`"gelu"`):
             The non-linear activation function (function or string) in the encoder and pooler. If string,
-            :obj:`"gelu"`, :obj:`"relu"`, :obj:`"swish"` and :obj:`"gelu_new"` are supported.
+            :obj:`"gelu"`, :obj:`"relu"`, :obj:`"silu"` and :obj:`"gelu_new"` are supported.
         dropout (:obj:`float`, `optional`, defaults to 0.1):
             The dropout probability for all fully connected layers in the embeddings, encoder, and pooler.
         attention_dropout (:obj:`float`, `optional`, defaults to 0.0):
--- src/transformers/configuration_squeezebert.py
+++ src/transformers/configuration_squeezebert.py
@@ -50,7 +50,7 @@ class SqueezeBertConfig(PretrainedConfig):
             Dimensionality of the "intermediate" (often named feed-forward) layer in the Transformer encoder.
         hidden_act (:obj:`str` or :obj:`Callable`, `optional`, defaults to :obj:`"gelu"`):
             The non-linear activation function (function or string) in the encoder and pooler. If string,
-            :obj:`"gelu"`, :obj:`"relu"`, :obj:`"swish"` and :obj:`"gelu_new"` are supported.
+            :obj:`"gelu"`, :obj:`"relu"`, :obj:`"silu"` and :obj:`"gelu_new"` are supported.
         hidden_dropout_prob (:obj:`float`, `optional`, defaults to 0.1):
             The dropout probability for all fully connected layers in the embeddings, encoder, and pooler.
         attention_probs_dropout_prob (:obj:`float`, `optional`, defaults to 0.1):
