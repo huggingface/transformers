e2f9761fb5bbdb073611366c3fd21cd98a93ac2f
Patrick von Platen <patrick.v.platen@gmail.com>
2020-11-02 12:41:37 +0100

    apply some more of sylvains and sams comments

--- src/transformers/generation_utils.py
+++ src/transformers/generation_utils.py
@@ -204,6 +204,15 @@ class GenerationMixin:
 
     @staticmethod
     def _reorder_cache(past: Tuple[torch.Tensor], beam_idx: torch.Tensor) -> Tuple[torch.Tensor]:
+        """
+        This function is used to re-order the :obj:`past_key_values` or :obj:`mems` cache if
+        :meth:`~transformers.PretrainedModel.beam_search` or :meth:`~transformers.PretrainedModel.beam_sample` is
+        called. This is required to match :obj:`past_key_values` or :obj:`mems` with the correct beam_idx at every
+        generation step.
+
+        For custom re-ordering of :obj:`past_key_values` or :obj:`mems`, the function should be implemented in
+        subclasses of :class:`~transformers.PreTrainedModel`.
+        """
         return tuple(layer_past.index_select(1, beam_idx) for layer_past in past)
 
     def _get_logits_warper(
@@ -1076,7 +1085,7 @@ class GenerationMixin:
             outputs = self(**model_inputs, return_dict=True)
             next_token_logits = outputs.logits[:, -1, :]
 
-            # adjust tokens for Bart, *e.g.*
+            # adjust token scores (a no-op by default)
             next_token_logits = self.adjust_logits_during_generation(
                 next_token_logits, cur_len=cur_len, max_length=max_length
             )
