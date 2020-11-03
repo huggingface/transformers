6ae9db7ff1d61117fef5187b08fe4a12ab77174d
PhilipMay <eniak.info@gmail.com>
2020-11-03 20:10:32 +0100

    fix line too long

--- src/transformers/training_args.py
+++ src/transformers/training_args.py
@@ -60,7 +60,8 @@ class TrainingArguments:
             See the `example scripts <https://github.com/huggingface/transformers/tree/master/examples>`___
             for more details.
         do_predict (:obj:`bool`, `optional`, defaults to :obj:`False`):
-            Whether to run predictions on the test set or not. This argument is not directly used by :class:`~transformers.Trainer`,
+            Whether to run predictions on the test set or not.
+            This argument is not directly used by :class:`~transformers.Trainer`,
             it's intended to be used by your training/evaluation scripts instead.
             See the `example scripts <https://github.com/huggingface/transformers/tree/master/examples>`___
             for more details.
