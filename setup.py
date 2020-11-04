b6e58db277bc8931678b79e71276639bcd933759
Sylvain Gugger <35901082+sgugger@users.noreply.github.com>
2020-11-04 11:51:21 -0500

    Speedup doc build (#8301)
    * Try -j option
    
    * Try other thing
    
    * Bigger machine
    
    * Test lower sphinx version
    
    * Remove trailing space

--- setup.py
+++ setup.py
@@ -105,7 +105,7 @@ extras["sentencepiece"] = ["sentencepiece==0.1.91"]
 extras["retrieval"] = ["faiss-cpu", "datasets"]
 extras["testing"] = ["pytest", "pytest-xdist", "timeout-decorator", "parameterized", "psutil"] + extras["retrieval"]
 # sphinx-rtd-theme==0.5.0 introduced big changes in the style.
-extras["docs"] = ["recommonmark", "sphinx", "sphinx-markdown-tables", "sphinx-rtd-theme==0.4.3", "sphinx-copybutton"]
+extras["docs"] = ["recommonmark", "sphinx==3.2.1", "sphinx-markdown-tables", "sphinx-rtd-theme==0.4.3", "sphinx-copybutton"]
 extras["quality"] = ["black >= 20.8b1", "isort >= 5.5.4", "flake8 >= 3.8.3"]
 
 
