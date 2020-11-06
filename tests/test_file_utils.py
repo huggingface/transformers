8d3cd55f64af92a0b518c17eb1d4b689d71978e9
Julien Chaumond <chaumond@gmail.com>
2020-11-06 13:29:39 +0100

    Add more tests and explain
    cc @sshleifer let me know if better
    
    Co-Authored-By: Sam Shleifer <sshleifer@gmail.com>

--- tests/test_file_utils.py
+++ tests/test_file_utils.py
@@ -6,9 +6,19 @@ from transformers.testing_utils import DUMMY_UNKWOWN_IDENTIFIER
 
 
 MODEL_ID = DUMMY_UNKWOWN_IDENTIFIER
-REVISION_ID = "main"
+# An actual model hosted on huggingface.co
+
+REVISION_ID_DEFAULT = "main"
+# Default branch name
+REVISION_ID_ONE_SPECIFIC_COMMIT = "432fec7515a022825ab3b79096838778e30aac17"
+# One particular commit
+REVISION_ID_INVALID = "aaaaaaa"
+# This commit does not exist, so we should 404.
+
 PINNED_SHA1 = "d9e9f15bc825e4b2c9249e9578f884bbcb5e3684"
+# Sha-1 of config.json on the top of `main`, for checking purposes
 PINNED_SHA256 = "4b243c475af8d0a7754e87d7d096c92e5199ec2fe168a2ee7998e3b8e9bcb1d3"
+# Sha-256 of pytorch_model.bin on the top of `main`, for checking purposes
 
 
 class GetFromCacheTests(unittest.TestCase):
@@ -20,19 +30,34 @@ class GetFromCacheTests(unittest.TestCase):
         with self.assertRaisesRegex(ValueError, "Connection error"):
             _ = get_from_cache(url)
 
-    def test_not_found(self):
+    def test_file_not_found(self):
+        # Valid revision (None) but missing file.
         url = hf_bucket_url(MODEL_ID, filename="missing.bin")
         with self.assertRaisesRegex(requests.exceptions.HTTPError, "404 Client Error"):
             _ = get_from_cache(url)
 
+    def test_revision_not_found(self):
+        # Valid file but missing revision
+        url = hf_bucket_url(MODEL_ID, filename=CONFIG_NAME, revision=REVISION_ID_INVALID)
+        with self.assertRaisesRegex(requests.exceptions.HTTPError, "404 Client Error"):
+            _ = get_from_cache(url)
+
     def test_standard_object(self):
-        url = hf_bucket_url(MODEL_ID, filename=CONFIG_NAME, revision=REVISION_ID)
+        url = hf_bucket_url(MODEL_ID, filename=CONFIG_NAME, revision=REVISION_ID_DEFAULT)
         filepath = get_from_cache(url, force_download=True)
         metadata = filename_to_url(filepath)
         self.assertEqual(metadata, (url, f'"{PINNED_SHA1}"'))
 
+    def test_standard_object(self):
+        # Same object, but different revision
+        url = hf_bucket_url(MODEL_ID, filename=CONFIG_NAME, revision=REVISION_ID_ONE_SPECIFIC_COMMIT)
+        filepath = get_from_cache(url, force_download=True)
+        metadata = filename_to_url(filepath)
+        self.assertNotEqual(metadata[1], f'"{PINNED_SHA1}"')
+        # Caution: check that the etag is *not* equal to the one from `test_standard_object`
+
     def test_lfs_object(self):
-        url = hf_bucket_url(MODEL_ID, filename=WEIGHTS_NAME, revision=REVISION_ID)
+        url = hf_bucket_url(MODEL_ID, filename=WEIGHTS_NAME, revision=REVISION_ID_DEFAULT)
         filepath = get_from_cache(url, force_download=True)
         metadata = filename_to_url(filepath)
         self.assertEqual(metadata, (url, f'"{PINNED_SHA256}"'))
