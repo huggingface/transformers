1b073be34e67c589b9563ba2b5f3f9af09422882
alexorona <alexorona@gmail.com>
2020-11-05 23:40:15 -0800

    Update model_parallel_utils.py

--- src/transformers/utils/model_parallel_utils.py
+++ src/transformers/utils/model_parallel_utils.py
@@ -1,10 +1,12 @@
 # coding=utf-8
-
+from math import ceil
 
 def assert_device_map(device_map, num_blocks):
     blocks = list(range(0, num_blocks))
 
-    device_map_blocks = [item for sublist in list(device_map.values()) for item in sublist]
+    device_map_blocks = [
+        item for sublist in list(device_map.values()) for item in sublist
+    ]
 
     # Duplicate check
     duplicate_blocks = []
@@ -23,16 +25,16 @@ def assert_device_map(device_map, num_blocks):
         "There are attention blocks for this model that are not specified in the device_map. Add these attention_blocks to a device on the device_map:"
         + str(missing_blocks)
     )
-    assert (
-        len(extra_blocks) == 0
-    ), "The device_map contains more attention blocks than this model has. Remove these from the device_map:" + str(
-        extra_blocks
+    assert len(extra_blocks) == 0, (
+        "The device_map contains more attention blocks than this model has. Remove these from the device_map:"
+        + str(extra_blocks)
     )
 
 
-def get_device_map(n_layers, n_devices):
+def get_device_map(n_layers: int, devices: list):
     """Returns a dictionary of layers distributed evenly across all devices."""
-    device_map = {
-        i: list(range(int(i * n_layers / n_devices), int((i + 1) * n_layers / n_devices))) for i in range(n_devices)
-    }
-    return device_map
+    layers = list(range(n_layers))
+    n_blocks = int(ceil(n_layers / len(devices)))
+    layers_list = list(layers[i : i + n_blocks] for i in range(0, n_layers, n_blocks))
+
+    return dict(zip(devices, layers_list))
