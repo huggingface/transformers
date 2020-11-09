bd0e1835a9b78edea069e2a924e9406d8ed7fedf
Patrick von Platen <patrick.v.platen@gmail.com>
2020-11-09 18:24:41 +0100

    [Tests] Add Common Test for Training + Fix a couple of bugs (#8415)
    * add training tests
    
    * correct longformer
    
    * fix docs
    
    * fix some tests
    
    * fix some more train tests
    
    * remove ipdb
    
    * fix multiple edge case model training
    
    * fix funnel and prophetnet
    
    * clean gpt models
    
    * undo renaming of albert

--- tests/test_modeling_albert.py
+++ tests/test_modeling_albert.py
@@ -24,7 +24,10 @@ from .test_modeling_common import ModelTesterMixin, ids_tensor, random_attention
 
 
 if is_torch_available():
+    import torch
+
     from transformers import (
+        MODEL_FOR_PRETRAINING_MAPPING,
         AlbertConfig,
         AlbertForMaskedLM,
         AlbertForMultipleChoice,
@@ -226,6 +229,20 @@ class AlbertModelTest(ModelTesterMixin, unittest.TestCase):
         if is_torch_available()
         else ()
     )
+
+    # special case for ForPreTraining model
+    def _prepare_for_class(self, inputs_dict, model_class, return_labels=False):
+        inputs_dict = super()._prepare_for_class(inputs_dict, model_class, return_labels=return_labels)
+
+        if return_labels:
+            if model_class in MODEL_FOR_PRETRAINING_MAPPING.values():
+                inputs_dict["labels"] = torch.zeros(
+                    (self.model_tester.batch_size, self.model_tester.seq_length), dtype=torch.long, device=torch_device
+                )
+                inputs_dict["sentence_order_label"] = torch.zeros(
+                    self.model_tester.batch_size, dtype=torch.long, device=torch_device
+                )
+        return inputs_dict
 
     def setUp(self):
         self.model_tester = AlbertModelTester(self)
