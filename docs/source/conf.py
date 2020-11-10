a3f53a053a2105bf8d271080cdb6877517295e21
Lysandre <lysandre.debut@reseau.eseo.fr>
2020-11-10 08:50:24 -0500

    Release: v3.5.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ author = u'huggingface'
 # The short X.Y version
 version = u''
 # The full version, including alpha/beta/rc tags
-release = u'3.4.0'
+release = u'3.5.0'
 
 
 # -- General configuration ---------------------------------------------------
