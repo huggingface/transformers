345e375570b24478815ac7f34f7c4f617e4e89e9
zeyuyun1 <chobitstian@berkeley.edu>
2020-11-11 13:22:21 -0800

    quick fix on concatenating text to support more datasets

--- examples/language-modeling/run_clm.py
+++ examples/language-modeling/run_clm.py
@@ -254,7 +254,7 @@ def main():
         tokenize_function,
         batched=True,
         num_proc=data_args.preprocessing_num_workers,
-        remove_columns=[text_column_name],
+        remove_columns=column_names,
         load_from_cache_file=not data_args.overwrite_cache,
     )
 
