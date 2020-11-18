1f5eeb88865cd0c2c75b2da80f745cc5ad678dee
patrickvonplaten <patrick.v.platen@gmail.com>
2020-11-18 18:15:09 +0000

    fix input embeds

--- src/transformers/models/longformer/modeling_tf_longformer.py
+++ src/transformers/models/longformer/modeling_tf_longformer.py
@@ -492,23 +492,17 @@ class TFLongformerEmbeddings(tf.keras.layers.Layer):
 
         super().build(input_shape)
 
-    def create_position_ids_from_input_ids(self, input_ids):
+    def create_position_ids_from_input_ids(self, x):
         """
         Replace non-padding symbols with their position numbers. Position numbers begin at padding_idx+1. Padding
         symbols are ignored. This is modified from fairseq's `utils.make_positions`.
 
         Args:
-            input_ids: tf.Tensor
+            x: tf.Tensor
 
         Returns: tf.Tensor
         """
-        input_ids_shape = shape_list(input_ids)
-
-        # multiple choice has 3 dimensions
-        if len(input_ids_shape) == 3:
-            input_ids = tf.reshape(input_ids, (input_ids_shape[0] * input_ids_shape[1], input_ids_shape[2]))
-
-        mask = tf.cast(tf.math.not_equal(input_ids, self.padding_idx), dtype=tf.int32)
+        mask = tf.cast(tf.math.not_equal(x, self.padding_idx), dtype=tf.int32)
         incremental_indices = tf.math.cumsum(mask, axis=1) * mask
 
         return incremental_indices + self.padding_idx
