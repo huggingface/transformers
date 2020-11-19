86173f3c09e6d7b17df6bb51cec1b25146a0b0aa
Julien Plu <plu.julien@gmail.com>
2020-11-19 10:49:35 +0100

    Address Patrick's comments

--- src/transformers/modeling_tf_utils.py
+++ src/transformers/modeling_tf_utils.py
@@ -252,7 +252,6 @@ def input_processing(func, input_ids, **kwargs):
     signature.pop("kwargs", None)
     parameter_names = list(signature.keys())
     output = {}
-    allowed_types = (tf.Tensor, bool, int, ModelOutput, tuple, list, dict)
 
     if "inputs" in kwargs["kwargs_call"]:
         warnings.warn(
@@ -275,7 +274,7 @@ def input_processing(func, input_ids, **kwargs):
         )
 
     for k, v in kwargs.items():
-        if isinstance(v, allowed_types) or v is None:
+        if isinstance(v, (tf.Tensor, bool, int, ModelOutput, tuple, list, dict)) or v is None:
             output[k] = v
         else:
             raise ValueError(f"Data of type {type(v)} is not allowed only tf.Tensor is accepted for {k}.")
@@ -294,7 +293,7 @@ def input_processing(func, input_ids, **kwargs):
                     raise ValueError(
                         f"The tensor named {input.name} does not belong to the authorized list of names {parameter_names}."
                     )
-            elif isinstance(input, allowed_types) or input is None:
+            elif isinstance(input, (tf.Tensor, bool, int, ModelOutput, tuple, list, dict)) or input is None:
                 output[parameter_names[i]] = input
             else:
                 raise ValueError(
@@ -317,108 +316,7 @@ def input_processing(func, input_ids, **kwargs):
             output["past_key_values"] = input_ids.pop("decoder_cached_states")
 
         for k, v in dict(input_ids).items():
-            if not isinstance(v, allowed_types):
-                raise ValueError(f"Data of type {type(v)} is not allowed only tf.Tensor is accepted for {k}.")
-            else:
-                output[k] = v
-    else:
-        if isinstance(input_ids, tf.Tensor) or input_ids is None:
-            output[parameter_names[0]] = input_ids
-        else:
-            raise ValueError(
-                f"Data of type {type(input_ids)} is not allowed only tf.Tensor is accepted for {parameter_names[0]}."
-            )
-
-    for name in parameter_names:
-        if name not in list(output.keys()) and name != "args":
-            output[name] = kwargs.pop(name, signature[name].default)
-
-    # When creating a SavedModel TF calls the method with LayerCall.__call__(args, **kwargs)
-    # So to respect the proper output we have to add this exception
-    if "args" in output:
-        if output["args"] is not None and type(output["args"]) == tf.Tensor:
-            tensor_name = output["args"].name.split(":")[0]
-            output[tensor_name] = output["args"]
-        else:
-            # `args` in this case is always the first parameter, then `input_ids`
-            output["input_ids"] = output["args"]
-
-        del output["args"]
-
-    if "kwargs" in output:
-        del output["kwargs"]
-
-    return output
-
-
-def input_processing(func, inputs, **kwargs):
-    signature = inspect.signature(func).parameters
-    parameter_names = list(signature.keys())
-    output = {}
-
-    if "inputs" in kwargs["kwargs_call"]:
-        warnings.warn(
-            "The `inputs` argument is deprecated and will be removed in a future version, use `input_ids` instead.",
-            FutureWarning,
-        )
-
-        output["input_ids"] = kwargs["kwargs_call"].pop("inputs")
-
-    if "decoder_cached_states" in kwargs["kwargs_call"]:
-        warnings.warn(
-            "The `decoder_cached_states` argument is deprecated and will be removed in a future version, use `past_key_values` instead.",
-            FutureWarning,
-        )
-        output["past_key_values"] = kwargs["kwargs_call"].pop("decoder_cached_states")
-
-    if len(kwargs["kwargs_call"]) > 0:
-        raise ValueError(
-            f"The following keyword arguments are not supported by this model: {list(kwargs['kwargs_call'].keys())}."
-        )
-
-    for k, v in kwargs.items():
-        if isinstance(v, (tf.Tensor, bool, ModelOutput, tuple, list, dict)) or v is None:
-            output[k] = v
-        else:
-            raise ValueError(f"Data of type {type(v)} is not allowed only tf.Tensor is accepted for {k}.")
-
-    if isinstance(input_ids, (tuple, list)):
-        for i, input in enumerate(input_ids):
-            if type(input) == tf.Tensor:
-                # Tensor names have always the pattern name:device_id then we check only the
-                # name and not the device id
-                tensor_name = input.name.split(":")[0]
-
-                if tensor_name in parameter_names:
-                    output[tensor_name] = input
-                else:
-                    raise ValueError(
-                        f"The tensor named {input.name} does not belong to the authorized list of names {parameter_names}."
-                    )
-            elif isinstance(input, (tf.Tensor, bool, ModelOutput, tuple, list, dict)) or input is None:
-                output[parameter_names[i]] = input
-            else:
-                raise ValueError(
-                    f"Data of type {type(input)} is not allowed only tf.Tensor is accepted for {parameter_names[i]}."
-                )
-    elif isinstance(input_ids, (dict, BatchEncoding)):
-        if "inputs" in input_ids:
-            warnings.warn(
-                "The `inputs` argument is deprecated and will be removed in a future version, use `input_ids` instead.",
-                FutureWarning,
-            )
-
-            output["input_ids"] = input_ids.pop("inputs")
-
-        if "decoder_cached_states" in input_ids:
-            warnings.warn(
-                "The `decoder_cached_states` argument is deprecated and will be removed in a future version, use `past_key_values` instead.",
-                FutureWarning,
-            )
-            output["past_key_values"] = input_ids.pop("decoder_cached_states")
-
-        for k, v in dict(input_ids).items():
-            if not isinstance(v, (tf.Tensor, bool, ModelOutput, tuple, list, dict)):
+            if not isinstance(v, (tf.Tensor, bool, int, ModelOutput, tuple, list, dict)):
                 raise ValueError(f"Data of type {type(v)} is not allowed only tf.Tensor is accepted for {k}.")
             else:
                 output[k] = v
--- src/transformers/models/bart/modeling_tf_bart.py
+++ src/transformers/models/bart/modeling_tf_bart.py
@@ -1200,3 +1200,15 @@ class TFBartForConditionalGeneration(TFPretrainedBartModel):
 
     def get_encoder(self):
         return self.model.encoder
+    
+    def compute_loss(self, labels, logits):
+        """CrossEntropyLoss that ignores pad tokens"""
+        loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(
+            from_logits=True,
+            reduction=tf.keras.losses.Reduction.NONE,
+        )
+        melted_labels = tf.reshape(labels, (-1,))
+        active_loss = tf.not_equal(melted_labels, self.config.pad_token_id)
+        reduced_logits = tf.boolean_mask(tf.reshape(logits, (-1, shape_list(logits)[2])), active_loss)
+        labels = tf.boolean_mask(melted_labels, active_loss)
+        return loss_fn(labels, reduced_logits)
