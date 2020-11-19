1037fb6272d54bc7222757ce6aa0682c35d0598a
Stas Bekman <stas@stason.org>
2020-11-18 18:31:47 -0800

    small tweaks

--- src/transformers/dependency_versions_check.py
+++ src/transformers/dependency_versions_check.py
@@ -3,8 +3,8 @@ from .utils.versions import require_version_core
 
 
 # define which module versions we always want to check at run time (usually the ones defined in `install_requires` in setup.py)
-pkg_to_check_at_runtime = "python tokenizers tqdm regex".split()
-for k in pkg_to_check_at_runtime:
+pkgs_to_check_at_runtime = "python tokenizers tqdm regex".split()
+for k in pkgs_to_check_at_runtime:
     if k in dep_versions:
         require_version_core(dep_versions[k])
     else:
