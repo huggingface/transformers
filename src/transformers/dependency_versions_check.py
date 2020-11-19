339a12fb9877e8cf87eb11c64c381658ea042fcd
Stas Bekman <stas@stason.org>
2020-11-18 17:22:34 -0800

    implement support for run-time dependency version checking

---
+++ src/transformers/dependency_versions_check.py
@@ -0,0 +1,11 @@
+from .dependency_versions_table import dep_versions
+from .utils.versions import require_version_core
+
+
+# define which module versions we always want to check at run time (usually the ones defined in `install_requires` in setup.py)
+pkg_to_check_at_runtime = "python tokenizers tqdm regex".split()
+for k in pkg_to_check_at_runtime:
+    if k in dep_versions:
+        require_version_core(dep_versions[k])
+    else:
+        raise ValueError(f"can't find {k} in {dep_versions.keys()}, check dependency_versions_table.py")
---
+++ src/transformers/utils/versions.py
@@ -0,0 +1,84 @@
+"""
+Utilities for working with package versions
+"""
+
+import operator
+import re
+import sys
+
+from packaging import version
+
+import pkg_resources
+
+
+ops = {
+    "<": operator.lt,
+    "<=": operator.le,
+    "==": operator.eq,
+    "!=": operator.ne,
+    ">=": operator.ge,
+    ">": operator.gt,
+}
+
+
+def require_version(requirement, hint=None):
+    """
+    Perform a runtime check of the dependency versions, using the exact same syntax used by pip.
+
+    The installed module version comes from the `site-packages` dir via `pkg_resources`.
+
+    Args:
+    - requirement: pip style definition, e.g.,  "tokenizers==0.9.4", "tqdm>=4.27",
+    - hint: what suggestion to print in case of requirements not being met
+    """
+
+    # note: while pkg_resources.require_version(requirement) is a much simpler way to do it, it
+    # fails if some of the dependencies of the dependencies are not matching, which is not necessarily
+    # bad, hence the more complicated check - which also should be faster, since it doesn't check
+    # dependencies of dependencies.
+
+    match = re.findall(r"^([^\!=<>\s]+)([\s\!=<>]{1,2})(.+)", requirement)
+    if not match:
+        raise ValueError(
+            f"requirement needs to be in the pip package format, .e.g., package_a==1.23, or package_b>=1.23, but got {requirement}"
+        )
+    pkg, op, want_ver = match[0]
+    if op not in ops:
+        raise ValueError(f"need one of {list(ops.keys())}, but got {op}")
+
+    # special case
+    if pkg == "python":
+        got_ver = ".".join([str(x) for x in sys.version_info[:3]])
+        if not ops[op](version.parse(got_ver), version.parse(want_ver)):
+            raise pkg_resources.VersionConflict(
+                f"{requirement} is required for a normal functioning of this module, but found {pkg}=={got_ver}."
+            )
+        return
+
+    try:
+        got_ver = pkg_resources.get_distribution(pkg).version
+    except pkg_resources.DistributionNotFound:
+        raise pkg_resources.DistributionNotFound(
+            f"{requirement} is required for a normal functioning of this module, but it is not installed. "
+            + ("\n" + hint if hint is not None else ""),
+            requirement,
+        )
+
+    if not ops[op](version.parse(got_ver), version.parse(want_ver)):
+        raise pkg_resources.VersionConflict(
+            f"{requirement} is required for a normal functioning of this module, but found {pkg}=={got_ver}. "
+            + "\n"
+            + hint
+            if hint is not None
+            else ""
+        )
+
+
+def require_version_core(requirement):
+    hint = 'Try: pip install transformers -U or pip install -e ".[dev]" if you\'re working with git master'
+    return require_version(requirement, hint)
+
+
+def require_version_examples(requirement):
+    hint = "Try: pip install -r examples/requirements.txt"
+    return require_version(requirement, hint)
