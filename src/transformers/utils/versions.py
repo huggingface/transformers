67fb1f7ac6b3cf0ed1ad0496867c3519a2e5c85f
Stas Bekman <stas@stason.org>
2020-11-18 18:32:57 -0800

    autoformatter worship

--- src/transformers/utils/versions.py
+++ src/transformers/utils/versions.py
@@ -76,7 +76,7 @@ def require_version(requirement, hint=None):
 
 def require_version_core(requirement):
     """ require_version wrapper which emits a core-specific hint on failure """
-    hint = 'Try: pip install transformers -U or pip install -e ".[dev]" if you\'re working with git master'
+    hint = "Try: pip install transformers -U or pip install -e '.[dev]' if you're working with git master"
     return require_version(requirement, hint)
 
 
