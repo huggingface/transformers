192c28fe5a70a32450c90dee396d1693a0dba5f7
alexorona <alexorona@gmail.com>
2020-11-19 23:02:36 -0800

    Update training_args.py

--- src/transformers/training_args.py
+++ src/transformers/training_args.py
@@ -438,8 +438,11 @@ class TrainingArguments:
     @torch_required
     def n_gpu(self):
         """
-        The number of GPUs used by this process. Note: This will only be greater than one when you have multiple GPUs
-        available but are not using distributed training. For distributed training, it will always be 1.
+        The number of GPUs used by this process.
+
+        Note:
+            This will only be greater than one when you have multiple GPUs available but are not using distributed
+            training. For distributed training, it will always be 1.
         """
         return self._setup_devices[1]
 
