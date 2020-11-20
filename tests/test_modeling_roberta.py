6ab4f8ed70704f3cfc73a0959c5b1eec7eb2a856
zhiheng <zhiheng@amazon.com>
2020-11-20 14:03:58 -0800

    [tiny] Remove unused import

--- tests/test_modeling_roberta.py
+++ tests/test_modeling_roberta.py
@@ -17,7 +17,7 @@
 import unittest
 
 from transformers import is_torch_available
-from transformers.testing_utils import require_sentencepiece, require_tokenizers, require_torch, slow, torch_device
+from transformers.testing_utils import require_torch, slow, torch_device
 
 from .test_configuration_common import ConfigTester
 from .test_generation_utils import GenerationTesterMixin
