300450f5c4480d8983be658796163db6e2c9fc5e
Stas Bekman <stas@stason.org>
2020-11-23 15:23:18 -0800

    tqdm must be checked before tokenizers

--- src/transformers/dependency_versions_check.py
+++ src/transformers/dependency_versions_check.py
@@ -1,14 +1,21 @@
 from .dependency_versions_table import deps
-from .file_utils import is_tokenizers_available
 from .utils.versions import require_version_core
 
 
 # define which module versions we always want to check at run time (usually the ones defined in `install_requires` in setup.py)
-pkgs_to_check_at_runtime = "python tokenizers tqdm regex numpy".split()
+#
+# order specific notes:
+# - tqdm must be checked before tokenizers
+
+pkgs_to_check_at_runtime = "python tqdm regex numpy tokenizers".split()
 for pkg in pkgs_to_check_at_runtime:
     if pkg in deps:
-        if pkg == "tokenizers" and not is_tokenizers_available():
-            continue  # not required, check version only if installed
+        if pkg == "tokenizers":
+            # must be loaded here, or else tqdm check may fail
+            from .file_utils import is_tokenizers_available
+
+            if not is_tokenizers_available():
+                continue  # not required, check version only if installed
         require_version_core(deps[pkg])
     else:
         raise ValueError(f"can't find {pkg} in {deps.keys()}, check dependency_versions_table.py")
