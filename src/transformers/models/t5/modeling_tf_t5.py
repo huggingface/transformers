3a6b0c5be38cf3212c66d9077b6253a15b98e922
Cong Duy Vu Hoang <vu.hoang@oracle.com>
2020-11-24 14:07:09 +1100

    Fix minor bug to handle dynamic sequence length with None (e.g., building serving models) for T5/mT5

--- src/transformers/models/t5/modeling_tf_t5.py
+++ src/transformers/models/t5/modeling_tf_t5.py
@@ -266,7 +266,7 @@ class TFT5Attention(tf.keras.layers.Layer):
             )
             real_seq_length += past_key_value[0].shape[2] if query_length is None else query_length
 
-        key_length = real_seq_length if key_value_states is None else key_value_states.shape[1]
+        key_length = real_seq_length if key_value_states is None or key_value_states.shape[1] is None else key_value_states.shape[1]
 
         def shape(hidden_states):
             """  projection """
