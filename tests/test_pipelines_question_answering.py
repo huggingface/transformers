b5607f5f567f60207b6cc30eeb53cdfe12a8aa74
Lysandre Debut <lysandre@huggingface.co>
2020-11-25 14:02:15 -0500

    Fix QA argument handler (#8765)
    * Fix QA argument handler
    
    * Attempt to get a better fix for QA (#8768)
    
    Co-authored-by: Nicolas Patry <patry.nicolas@protonmail.com>

--- tests/test_pipelines_question_answering.py
+++ tests/test_pipelines_question_answering.py
@@ -23,6 +23,17 @@ class QAPipelineTests(CustomInputPipelineCommonMixin, unittest.TestCase):
             "question": "In what field is HuggingFace working ?",
             "context": "HuggingFace is a startup based in New-York founded in Paris which is trying to solve NLP.",
         },
+        {
+            "question": ["In what field is HuggingFace working ?", "In what field is HuggingFace working ?"],
+            "context": "HuggingFace is a startup based in New-York founded in Paris which is trying to solve NLP.",
+        },
+        {
+            "question": ["In what field is HuggingFace working ?", "In what field is HuggingFace working ?"],
+            "context": [
+                "HuggingFace is a startup based in New-York founded in Paris which is trying to solve NLP.",
+                "HuggingFace is a startup based in New-York founded in Paris which is trying to solve NLP.",
+            ],
+        },
     ]
 
     def _test_pipeline(self, nlp: Pipeline):
@@ -78,6 +89,11 @@ class QAPipelineTests(CustomInputPipelineCommonMixin, unittest.TestCase):
         normalized = qa(question=Q, context=C)
         self.assertEqual(type(normalized), list)
         self.assertEqual(len(normalized), 1)
+        self.assertEqual({type(el) for el in normalized}, {SquadExample})
+
+        normalized = qa(question=[Q, Q], context=C)
+        self.assertEqual(type(normalized), list)
+        self.assertEqual(len(normalized), 2)
         self.assertEqual({type(el) for el in normalized}, {SquadExample})
 
         normalized = qa({"question": Q, "context": C})
@@ -158,6 +174,26 @@ class QAPipelineTests(CustomInputPipelineCommonMixin, unittest.TestCase):
             qa([{"question": Q, "context": C}, {"question": Q, "context": None}])
         with self.assertRaises(ValueError):
             qa([{"question": Q, "context": C}, {"question": Q, "context": ""}])
+
+        with self.assertRaises(ValueError):
+            qa(question={"This": "Is weird"}, context="This is a context")
+
+        with self.assertRaises(ValueError):
+            qa(question=[Q, Q], context=[C, C, C])
+
+        with self.assertRaises(ValueError):
+            qa(question=[Q, Q, Q], context=[C, C])
+
+    def test_argument_handler_old_format(self):
+        qa = QuestionAnsweringArgumentHandler()
+
+        Q = "Where was HuggingFace founded ?"
+        C = "HuggingFace was founded in Paris"
+        # Backward compatibility for this
+        normalized = qa(question=[Q, Q], context=[C, C])
+        self.assertEqual(type(normalized), list)
+        self.assertEqual(len(normalized), 2)
+        self.assertEqual({type(el) for el in normalized}, {SquadExample})
 
     def test_argument_handler_error_handling_odd(self):
         qa = QuestionAnsweringArgumentHandler()
