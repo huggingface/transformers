b4a3dc9c858bfd6b48a2b6a75fcec55c30b72102
Maksym Del <max.del.edu@gmail.com>
2020-11-27 11:13:38 +0200

    Resolve conflict

--- src/transformers/models/fsmt/modeling_fsmt.py
+++ src/transformers/models/fsmt/modeling_fsmt.py
@@ -693,12 +693,9 @@ class FSMTDecoder(nn.Module):
                 all_cross_attns += (layer_cross_attn,)
 
         # Convert to standard output format: (seq_len, BS, model_dim) -> (BS, seq_len, model_dim)
-<<<<<<< HEAD
         if output_hidden_states:
             all_hidden_states += (x,)
             all_hidden_states = tuple(hidden_state.transpose(0, 1) for hidden_state in all_hidden_states)
-=======
->>>>>>> cb7602b38df8d75809a4d8bd68530ff2fc045a2d
         x = x.transpose(0, 1)
         encoder_hidden_states = encoder_hidden_states.transpose(0, 1)
 
