9c93a752d8547b056655a242c5e7357f33cd1610
Charles Pierse <charlespierse@gmail.com>
2020-11-29 13:03:21 +0000

    add get_cached_models function

--- src/transformers/file_utils.py
+++ src/transformers/file_utils.py
@@ -956,6 +956,39 @@ def filename_to_url(filename, cache_dir=None):
     return url, etag
 
 
+def get_cached_models(cache_dir: Union[str, Path] = None) -> List[Tuple]:
+    """
+    Returns a list of tuples representing model binaries that are cached locally. Each tuple has shape `(model_url,
+    etag, size_MB)`. Filenames in `cache_dir` are use to get the metadata for each model, only urls ending with `.bin`
+    are added.
+
+    Args:
+        cache_dir (Union[str, Path], optional): Specify a cache directory to search for models within. Defaults to None.
+
+    Returns:
+        List[Tuple]: List of tuples each with shape `(model_url, etag, size_MB)`
+    """
+    if cache_dir is None:
+        cache_dir = TRANSFORMERS_CACHE
+
+    if isinstance(cache_dir, Path):
+        cache_dir = str(cache_dir)
+
+    cached_models = []
+    for file in os.listdir(cache_dir):
+        if file.endswith(".json"):
+            meta_path = os.path.join(cache_dir, file)
+            with open(meta_path, encoding="utf-8") as meta_file:
+                metadata = json.load(meta_file)
+                url = metadata["url"]
+                etag = metadata["etag"]
+                if url.endswith(".bin"):
+                    size_MB = os.path.getsize(meta_path.strip(".json")) / 1e6
+                    cached_models.append((url, etag, size_MB))
+
+    return cached_models
+
+
 def cached_path(
     url_or_filename,
     cache_dir=None,
