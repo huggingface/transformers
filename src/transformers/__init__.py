09aaf082ce927fa225fa1f23b767ec65008f7a37
LysandreJik <lysandre.debut@reseau.eseo.fr>
2020-11-30 12:07:43 -0500

    Release: v4.0.0

--- src/transformers/__init__.py
+++ src/transformers/__init__.py
@@ -2,7 +2,7 @@
 # There's no way to ignore "F401 '...' imported but unused" warnings in this
 # module, but to preserve other warnings. So, don't check this module at all.
 
-__version__ = "4.0.0-rc-1"
+__version__ = "4.1.0dev0"
 
 # Work around to update TensorFlow's absl.logging threshold which alters the
 # default Python logging output behavior when present.
