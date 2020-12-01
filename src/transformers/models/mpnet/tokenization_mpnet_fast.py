684fcd3bc6c35cc59b671fba55024f8127720485
StillKeepTry <sktsxy@gmail.com>
2020-12-02 00:25:11 +0800

    update tokenization_mpnet_fast

--- src/transformers/models/mpnet/tokenization_mpnet_fast.py
+++ src/transformers/models/mpnet/tokenization_mpnet_fast.py
@@ -20,6 +20,7 @@ from typing import List, Optional, Tuple
 
 from tokenizers import normalizers
 
+from ...tokenization_utils_fast import PreTrainedTokenizerFast
 from ...tokenization_utils import AddedToken
 from ...tokenization_utils_fast import PreTrainedTokenizerFast
 from ...utils import logging
