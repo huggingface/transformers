443f67e887a030d8254eba126e5f2cdb8b70eb63
Patrick von Platen <patrick.v.platen@gmail.com>
2020-12-02 19:19:50 +0100

    [PyTorch] Refactor Resize Token Embeddings (#8880)
    * fix resize tokens
    
    * correct mobile_bert
    
    * move embedding fix into modeling_utils.py
    
    * refactor
    
    * fix lm head resize
    
    * refactor
    
    * break lines to make sylvain happy
    
    * add news tests
    
    * fix typo
    
    * improve test
    
    * skip bart-like for now
    
    * check if base_model = get(...) is necessary
    
    * clean files
    
    * improve test
    
    * fix tests
    
    * revert style templates
    
    * Update templates/adding_a_new_model/cookiecutter-template-{{cookiecutter.modelname}}/modeling_{{cookiecutter.lowercase_modelname}}.py

--- src/transformers/models/squeezebert/modeling_squeezebert.py
+++ src/transformers/models/squeezebert/modeling_squeezebert.py
@@ -655,6 +655,9 @@ class SqueezeBertForMaskedLM(SqueezeBertPreTrainedModel):
     def get_output_embeddings(self):
         return self.cls.predictions.decoder
 
+    def set_output_embeddings(self, new_embeddings):
+        self.cls.predictions.decoder = new_embeddings
+
     @add_start_docstrings_to_model_forward(SQUEEZEBERT_INPUTS_DOCSTRING.format("(batch_size, sequence_length)"))
     @add_code_sample_docstrings(
         tokenizer_class=_TOKENIZER_FOR_DOC,
