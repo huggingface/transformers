d55309cbd2d5ff212b88b65b4696e3d3650986cd
Morgan Funtowicz <funtowiczmo@gmail.com>
2020-12-05 19:23:17 +0100

    Fix typo.
    Signed-off-by: Morgan Funtowicz <funtowiczmo@gmail.com>

--- examples/language-modeling/run_mlm_flax.py
+++ examples/language-modeling/run_mlm_flax.py
@@ -568,7 +568,7 @@ if __name__ == "__main__":
     # Create learning rate scheduler
     lr_scheduler_fn = create_learning_rate_scheduler(
         base_learning_rate=training_args.learning_rate,
-        warmup_steps=training_args.warmup_step
+        warmup_steps=training_args.warmup_steps
     )
 
     # Create parallel version of the training and evaluation steps
