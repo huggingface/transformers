e041a493d762af3b87482758cb12eafd2203e5b6
Sylvain Gugger <35901082+sgugger@users.noreply.github.com>
2020-12-07 18:36:34 -0500

    Copyright (#8970)
    * Add copyright everywhere missing
    
    * Style

--- scripts/fsmt/s3-move.sh
+++ scripts/fsmt/s3-move.sh
@@ -1,5 +1,18 @@
 
 # this is the process of uploading the updated models to s3. As I can't upload them directly to the correct orgs, this script shows how this is done
+# Copyright 2020 The HuggingFace Team. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 1. upload updated models to my account
 
--- tests/test_tokenization_lxmert.py
+++ tests/test_tokenization_lxmert.py
@@ -1,5 +1,5 @@
 # coding=utf-8
-# Copyright 2018 LXMERT Authors.
+# Copyright 2018 LXMERT Authors, The Hugging Face Team.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
