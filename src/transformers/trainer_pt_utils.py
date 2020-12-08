9f66366c0ae13f4b226736540a51d48e53f98498
Stas Bekman <stas00@users.noreply.github.com>
2020-12-07 18:42:00 -0800

    [training] SAVE_STATE_WARNING was removed in pytorch
    FYI `SAVE_STATE_WARNING` has been removed 3 days ago: pytorch/pytorch#46813
    
    Fixes: #8232
    
    @sgugger

--- src/transformers/trainer_pt_utils.py
+++ src/transformers/trainer_pt_utils.py
@@ -34,7 +34,8 @@ from .utils import logging
 if is_torch_tpu_available():
     import torch_xla.core.xla_model as xm
 
-if version.parse(torch.__version__) <= version.parse("1.4.1"):
+# this is used to supress an undesired warning emitted by pytorch versions 1.4.2-1.7.0
+if version.parse(torch.__version__) <= version.parse("1.4.1") or version.parse(torch.__version__) > version.parse("1.7.0"):
     SAVE_STATE_WARNING = ""
 else:
     from torch.optim.lr_scheduler import SAVE_STATE_WARNING
