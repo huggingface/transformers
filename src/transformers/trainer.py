4cc0f7f0e7741c8d57998afe5bc3e2459ebcb73e
Sylvain Gugger <35901082+sgugger@users.noreply.github.com>
2020-12-09 09:13:41 -0500

    Remove use of deprected method in Trainer HP search (#8996)

--- src/transformers/trainer.py
+++ src/transformers/trainer.py
@@ -543,7 +543,7 @@ class Trainer:
             self.args.output_dir = checkpoint_dir
             output_dir = os.path.join(self.args.output_dir, f"{PREFIX_CHECKPOINT_DIR}-{self.state.global_step}")
             self.save_model(output_dir)
-            if self.is_world_master():
+            if self.is_world_process_zero():
                 self.state.save_to_json(os.path.join(output_dir, "trainer_state.json"))
                 torch.save(self.optimizer.state_dict(), os.path.join(output_dir, "optimizer.pt"))
                 torch.save(self.lr_scheduler.state_dict(), os.path.join(output_dir, "scheduler.pt"))
