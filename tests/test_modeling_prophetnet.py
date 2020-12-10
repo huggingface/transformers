ec2a43a9928ad6f813ba7af5b499d9f950a2a882
Patrick von Platen <patrick.v.platen@gmail.com>
2020-12-10 16:48:20 +0000

    fix test

--- tests/test_modeling_prophetnet.py
+++ tests/test_modeling_prophetnet.py
@@ -19,7 +19,6 @@ import tempfile
 import unittest
 
 from transformers import is_torch_available
-from transformers.modeling_outputs import BaseModelOutput
 from transformers.testing_utils import require_torch, slow, torch_device
 
 from .test_configuration_common import ConfigTester
@@ -39,6 +38,7 @@ if is_torch_available():
         ProphetNetModel,
         ProphetNetTokenizer,
     )
+    from transformers.modeling_outputs import BaseModelOutput
 
 
 class ProphetNetModelTester:
