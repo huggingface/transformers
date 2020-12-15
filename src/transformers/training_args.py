8e8f36e321a36a82c63dd6ad4bc047fc15e856c7
Sylvain Gugger <35901082+sgugger@users.noreply.github.com>
2020-12-15 15:47:59 -0500

    Update src/transformers/training_args.py
    Co-authored-by: Stas Bekman <stas00@users.noreply.github.com>

--- src/transformers/training_args.py
+++ src/transformers/training_args.py
@@ -384,7 +384,7 @@ class TrainingArguments:
     )
     fp16_backend: str = field(
         default="auto",
-        metadata={"help": "The backend to used for mixed precision. Should be one of 'auto', 'amp' or 'apex'."},
+        metadata={"help": "The backend to be used for mixed precision. Should be one of 'auto', 'amp' or 'apex'."},
     )
 
     def __post_init__(self):
