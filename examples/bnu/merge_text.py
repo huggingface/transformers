6e463dc76341a00ed5566dcf4df3eed1676ca5f0
sky-snow <skysnow2017@gmail.com>
2020-12-16 20:14:18 +0800

    add merge_func

---
+++ examples/bnu/merge_text.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2020/12/16 19:37
+# @Author  : wuyan
+# @Email   :aerolandX@163.com
+# @FileName: merge_text.py
+# @Function:
+
+from concurrent.futures import ThreadPoolExecutor
+from chardet.universaldetector import UniversalDetector
+from pathlib import Path
+from typing import Any
+import logging
+
+logging.basicConfig(level=logging.INFO, format='[%(threadName)-9s] %(message)s')
+
+
+def get_encode_info(file):
+    with open(file, 'rb') as f:
+        detector = UniversalDetector()
+        for line in f.readlines():
+            detector.feed(line)
+            if detector.done:
+                break
+        detector.close()
+        return detector.result['encoding']
+
+
+def read_file(file):
+    with open(file, 'rb') as f:
+        return f.read()
+
+
+def write_file(content, file):
+    with open(file, 'wb') as f:
+        f.write(content)
+
+
+def convert_encode2utf8(file, original_encode, des_encode):
+    file_content = read_file(file)
+    file_decode = file_content.decode(original_encode, 'ignore')
+    file_encode = file_decode.encode(des_encode)
+    write_file(file_encode, file)
+
+
+def covert_all(file_path):
+    file_content = read_file(file_path)
+    encode_info = get_encode_info(file_path)
+    if encode_info != 'utf-8':
+        convert_encode2utf8(file_path, encode_info, 'utf-8')
+        return file_path
+
+
+def merge_text():
+    ...
+
+
+def multip_process(task: list):
+    executor = ThreadPoolExecutor()
+    for result in executor.map(covert_all, task):
+        logging.info('{} already processed'.format(result))
+
+
+if __name__ == '__main__':
+    root_path = r"/home/wuyan/usr/material/bert_corpus"
+    path = [str(x) for x in Path(root_path).glob("**/*.txt")]
--- examples/bnu/run_mlm_material.py
+++ examples/bnu/run_mlm_material.py
@@ -1,18 +1,25 @@
 from pathlib import Path
 from tokenizers import BertWordPieceTokenizer
+from tokenizers.processors import BertProcessing
 
-root_path = r"/home/wuyan/usr/material/bert_corpus"
-paths = [str(x) for x in Path(root_path).glob("**/*.txt")]
+# root_path = r"/home/wuyan/usr/material/bert_corpus"
+# paths = [str(x) for x in Path(root_path).glob("**/*.txt")]
+paths = [r'1.txt']
 
-tokenizer = BertWordPieceTokenizer()
-
-tokenizer.train(files=paths, vocab_size=52_000, min_frequency=2, special_tokens=[
-    "[UNK]",
-    "[SEP]",
-    "[CLS]",
-    "[PAD]",
-    "[MASK]",
-])
+tokenizer = BertWordPieceTokenizer(r'material-vocab.txt')
+# print(tokenizer)
+# tokenizer.train(files=paths, vocab_size=52_000, min_frequency=2, special_tokens=[
+#     "[UNK]",
+#     "[SEP]",
+#     "[CLS]",
+#     "[PAD]",
+#     "[MASK]",
+# ])
 
 # Save files to disk
-tokenizer.save_model(".", "material")
+# tokenizer.save_model(".", "material")
+
+tokenizer._tokenizer.post_processor = BertProcessing(('[SEP]',tokenizer.token_to_id('[SEP]')),('[CLS]',tokenizer.token_to_id('[CLS]')))
+tokenizer.enable_truncation(max_length=512)
+
+print(tokenizer.encode("hello i am wuyan . i am  a boy.").tokens)
