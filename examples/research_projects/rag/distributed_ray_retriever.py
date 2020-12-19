0e7769c4bb0c9fc2ff82c57a9c1f753121fa98de
Amog Kamsetty <amogkamsetty@yahoo.com>
2020-12-18 21:24:16 -0800

    addressing comments

--- examples/research_projects/rag/distributed_ray_retriever.py
+++ examples/research_projects/rag/distributed_ray_retriever.py
@@ -1,10 +1,10 @@
 import logging
 import random
-import ray
 
+import ray
 from transformers import RagConfig, RagRetriever, RagTokenizer
 from transformers.file_utils import requires_datasets, requires_faiss
-
+from transformers.models.rag.retrieval_rag import CustomHFIndex
 
 
 logger = logging.getLogger(__name__)
@@ -136,17 +136,13 @@ class RagRayDistributedRetriever(RagRetriever):
     def from_pretrained(cls, retriever_name_or_path, actor_handles, indexed_dataset=None, **kwargs):
         requires_datasets(cls)
         requires_faiss(cls)
-        config = kwargs.pop("config", None) or RagConfig.from_pretrained(
-            retriever_name_or_path,
-            **kwargs)
-        rag_tokenizer = RagTokenizer.from_pretrained(retriever_name_or_path,
-                                                     config=config)
+        config = kwargs.pop("config", None) or RagConfig.from_pretrained(retriever_name_or_path, **kwargs)
+        rag_tokenizer = RagTokenizer.from_pretrained(retriever_name_or_path, config=config)
         question_encoder_tokenizer = rag_tokenizer.question_encoder
         generator_tokenizer = rag_tokenizer.generator
         if indexed_dataset is not None:
             config.index_name = "custom"
-            index = CustomHFIndex(config.retrieval_vector_size,
-                                  indexed_dataset)
+            index = CustomHFIndex(config.retrieval_vector_size, indexed_dataset)
         else:
             index = cls._build_index(config)
         return cls(
--- src/transformers/models/rag/retrieval_rag.py
+++ src/transformers/models/rag/retrieval_rag.py
@@ -413,9 +413,7 @@ class RagRetriever:
     def from_pretrained(cls, retriever_name_or_path, indexed_dataset=None, **kwargs):
         requires_datasets(cls)
         requires_faiss(cls)
-        config = kwargs.pop("config", None) or RagConfig.from_pretrained(
-                            retriever_name_or_path,
-                                               **kwargs)
+        config = kwargs.pop("config", None) or RagConfig.from_pretrained(retriever_name_or_path, **kwargs)
         rag_tokenizer = RagTokenizer.from_pretrained(retriever_name_or_path, config=config)
         question_encoder_tokenizer = rag_tokenizer.question_encoder
         generator_tokenizer = rag_tokenizer.generator
