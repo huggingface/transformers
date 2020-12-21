1bb9acdc5de2314db20028fba5e01ee942339ace
Julien Plu <plu.julien@gmail.com>
2020-12-21 13:18:43 +0100

    Rework Funnel

--- src/transformers/models/funnel/modeling_tf_funnel.py
+++ src/transformers/models/funnel/modeling_tf_funnel.py
@@ -992,6 +992,20 @@ class TFFunnelMaskedLMHead(tf.keras.layers.Layer):
         self.bias = self.add_weight(shape=(self.vocab_size,), initializer="zeros", trainable=True, name="bias")
         super().build(input_shape)
 
+    def get_output_embeddings(self):
+        return self.input_embeddings.word_embeddings
+
+    def set_output_embeddings(self, value):
+        self.input_embeddings.word_embeddings = value
+        self.input_embeddings.vocab_size = shape_list(value)[0]
+
+    def get_bias(self):
+        return {"bias": self.bias}
+
+    def set_bias(self, value):
+        self.bias = value["bias"]
+        self.vocab_size = shape_list(value["bias"])[0]
+
     def call(self, hidden_states, training=False):
         hidden_states = self.input_embeddings(hidden_states, mode="linear")
         hidden_states = hidden_states + self.bias
@@ -1359,37 +1373,8 @@ class TFFunnelForMaskedLM(TFFunnelPreTrainedModel, TFMaskedLanguageModelingLoss)
         self.funnel = TFFunnelMainLayer(config, name="funnel")
         self.lm_head = TFFunnelMaskedLMHead(config, self.funnel.embeddings, name="lm_head")
 
-    def get_output_embeddings(self):
-        try:
-            return self.lm_head.input_embeddings.word_embeddings
-        except AttributeError:
-            self(self.dummy_inputs)
-            return self.lm_head.input_embeddings.word_embeddings
-
-    def set_output_embeddings(self, value):
-        if value is not None:
-            try:
-                self.lm_head.input_embeddings.word_embeddings = value
-            except AttributeError:
-                self(self.dummy_inputs)
-                self.lm_head.input_embeddings.word_embeddings = value
-            self.lm_head.input_embeddings.vocab_size = shape_list(value)[0]
-
-    def get_bias(self):
-        try:
-            return self.lm_head.bias
-        except AttributeError:
-            self(self.dummy_inputs)
-            return self.lm_head.bias
-
-    def set_bias(self, value):
-        if value is not None:
-            try:
-                self.lm_head.bias = value
-            except AttributeError:
-                self(self.dummy_inputs)
-                self.lm_head.bias = value
-            self.lm_head.vocab_size = shape_list(value)[0]
+    def get_lm_head(self):
+        return self.lm_head
 
     @add_start_docstrings_to_model_forward(FUNNEL_INPUTS_DOCSTRING.format("batch_size, sequence_length"))
     @add_code_sample_docstrings(
