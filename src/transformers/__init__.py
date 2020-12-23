6faddc9a0cdd0f7a2ad6bb88424c135a8f1fb45d
Ratthachat (Jung) <56621342+ratthachat@users.noreply.github.com>
2020-12-23 10:16:28 +0700

    __init__.py , import TFRagSequenceForGeneration

--- src/transformers/__init__.py
+++ src/transformers/__init__.py
@@ -878,7 +878,7 @@ if is_tf_available():
         TFOpenAIGPTPreTrainedModel,
     )
     from .models.pegasus import TFPegasusForConditionalGeneration
-    from .models.rag import TFRagModel, TFRagTokenForGeneration
+    from .models.rag import TFRagModel, TFRagTokenForGeneration, TFRagSequenceForGeneration
     from .models.roberta import (
         TF_ROBERTA_PRETRAINED_MODEL_ARCHIVE_LIST,
         TFRobertaForMaskedLM,
