89bbd62938ef5dfb4e1ccf70172e97a6794e33ae
Julien Plu <plu.julien@gmail.com>
2020-12-24 10:18:05 +0100

    Restore DPR

--- src/transformers/models/dpr/modeling_tf_dpr.py
+++ src/transformers/models/dpr/modeling_tf_dpr.py
@@ -161,12 +161,6 @@ class TFDPREncoderLayer(tf.keras.layers.Layer):
             self.encode_proj = tf.keras.layers.Dense(
                 config.projection_dim, kernel_initializer=get_initializer(config.initializer_range), name="encode_proj"
             )
-    
-    def serving(self, inputs):
-        pass
-    
-    def serving_output(self, output):
-        pass
 
     def call(
         self,
@@ -241,12 +235,6 @@ class TFDPRSpanPredictorLayer(tf.keras.layers.Layer):
         self.qa_classifier = tf.keras.layers.Dense(
             1, kernel_initializer=get_initializer(config.initializer_range), name="qa_classifier"
         )
-    
-    def serving(self, inputs):
-        pass
-    
-    def serving_output(self, output):
-        pass
 
     def call(
         self,
