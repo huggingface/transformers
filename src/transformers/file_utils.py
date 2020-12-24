6189ae99603bd5dc14c5631f1b4562f78e24d575
Jungwhan <53588015+jungwhank@users.noreply.github.com>
2020-12-24 17:18:33 +0900

    Fix typo in file_utils.py (#9289)

--- src/transformers/file_utils.py
+++ src/transformers/file_utils.py
@@ -760,7 +760,7 @@ PT_CAUSAL_LM_SAMPLE = r"""
         >>> from transformers import {tokenizer_class}, {model_class}
 
         >>> tokenizer = {tokenizer_class}.from_pretrained('{checkpoint}')
-        >>> model = {model_class}.from_pretrained('{checkpoint})
+        >>> model = {model_class}.from_pretrained('{checkpoint}')
 
         >>> inputs = tokenizer("Hello, my dog is cute", return_tensors="pt")
         >>> outputs = model(**inputs, labels=inputs["input_ids"])
