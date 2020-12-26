0784744f0d7bbd74d841ccf6b742ae649dfd24e5
sky-snow <skysnow2017@gmail.com>
2020-12-26 19:32:57 +0800

    vocab bug fix,rebuild vocab

--- examples/bnu/run_mlm_material.py
+++ examples/bnu/run_mlm_material.py
@@ -2,24 +2,24 @@ from pathlib import Path
 from tokenizers import BertWordPieceTokenizer
 from tokenizers.processors import BertProcessing
 
-# root_path = r"/home/wuyan/usr/material/bert_corpus"
-# paths = [str(x) for x in Path(root_path).glob("**/*.txt")]
-paths = [r'1.txt']
+root_path = r"/home/wuyan/usr/material/bert_corpus"
+paths = [str(x) for x in Path(root_path).glob("**/*.txt")]
+# paths = [r'1.txt']
 
-tokenizer = BertWordPieceTokenizer(r'material-vocab.txt')
+tokenizer = BertWordPieceTokenizer()
 # print(tokenizer)
-# tokenizer.train(files=paths, vocab_size=52_000, min_frequency=2, special_tokens=[
-#     "[UNK]",
-#     "[SEP]",
-#     "[CLS]",
-#     "[PAD]",
-#     "[MASK]",
-# ])
+tokenizer.train(files=paths, vocab_size=28_996, min_frequency=2, special_tokens=[
+    "[UNK]",
+    "[SEP]",
+    "[CLS]",
+    "[PAD]",
+    "[MASK]",
+])
 
 # Save files to disk
-# tokenizer.save_model(".", "material")
+tokenizer.save_model(".", "material")
 
-tokenizer._tokenizer.post_processor = BertProcessing(('[SEP]',tokenizer.token_to_id('[SEP]')),('[CLS]',tokenizer.token_to_id('[CLS]')))
-tokenizer.enable_truncation(max_length=512)
-
-print(tokenizer.encode("hello i am wuyan . i am  a boy.").tokens)
+# tokenizer._tokenizer.post_processor = BertProcessing(('[SEP]',tokenizer.token_to_id('[SEP]')),('[CLS]',tokenizer.token_to_id('[CLS]')))
+# tokenizer.enable_truncation(max_length=512)
+#
+# print(tokenizer.encode("hello i am wuyan . i am  a boy.").tokens)
