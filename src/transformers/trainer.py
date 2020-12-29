d73e5e6bad39a31fe23e77349efb77f81b379682
Stas Bekman <stas@stason.org>
2020-12-29 11:09:36 -0800

    --model_parallel hasn't been implemented for most models

--- src/transformers/trainer.py
+++ src/transformers/trainer.py
@@ -241,6 +241,14 @@ class Trainer:
         if model is None and model_init is not None:
             model = self.call_model_init()
 
+        if self.args.model_parallel:
+            # XXX: ideally this register should be maintained elsewhere so that the trainer could just do
+            # if model.model_parallel_is_supported()
+            mp_supported = ["gpt2", "t5"]
+            assert (
+                model.config.model_type in mp_supported
+            ), f"{model.config.model_type} implementation currently doesn't support model parallelism, therefore --model_parallel cl arg cannot be used"
+
         # Model parallel
         if model is not None and not self.args.model_parallel:
             model = model.to(args.device)
