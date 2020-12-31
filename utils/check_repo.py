1b020e2fcbe7449fe0f29346fc22a47529c483d1
Julien Plu <plu.julien@gmail.com>
2020-12-31 11:49:21 +0100

    Fix open

--- utils/check_repo.py
+++ utils/check_repo.py
@@ -315,7 +315,7 @@ def find_all_documented_objects():
     """ Parse the content of all doc files to detect which classes and functions it documents"""
     documented_obj = []
     for doc_file in Path(PATH_TO_DOC).glob("**/*.rst"):
-        with open(doc_file) as f:
+        with open(doc_file, "r", encoding="utf-8", newline="\n") as f:
             content = f.read()
         raw_doc_objs = re.findall(r"(?:autoclass|autofunction):: transformers.(\S+)\s+", content)
         documented_obj += [obj.split(".")[-1] for obj in raw_doc_objs]
