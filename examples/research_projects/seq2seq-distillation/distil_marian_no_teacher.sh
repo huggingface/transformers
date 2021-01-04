36c0563ebc2fa80d62834daeddd3668170ed5150
Sam Shleifer <sshleifer@gmail.com>
2021-01-04 00:51:24 -0500

    simplify marian distillation script (#9394)

--- examples/research_projects/seq2seq-distillation/distil_marian_no_teacher.sh
+++ examples/research_projects/seq2seq-distillation/distil_marian_no_teacher.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 export PYTHONPATH="../":"${PYTHONPATH}"
 export WANDB_PROJECT=dmar
-python distillation.py \
+export MAX_LEN=128
+python finetune.py \
   --learning_rate=3e-4 \
   --do_train \
   --do_predict \
-  --fp16 --no_teacher \
+  --fp16 \
   --val_check_interval 0.25 \
   --data_dir $ENRO_DIR \
   --max_source_length $MAX_LEN --max_target_length $MAX_LEN --val_max_target_length $MAX_LEN --test_max_target_length $MAX_LEN \
