bcb975f813ed8b1ffc3faa9ffd5fa3e75cf69b3b
Sugeeth <raghava.sugeeth@gmail.com>
2021-01-05 16:48:48 +0530

    Fix documentation links always pointing to master. (#9217)
    * Use extlinks to point hyperlink with the version of code
    
    * Point to version on release and master until then
    
    * Apply style
    
    * Correct links
    
    * Add missing backtick
    
    * Simple missing backtick after all.
    
    Co-authored-by: Raghavendra Sugeeth P S <raghav-5305@raghav-5305.csez.zohocorpin.com>
    Co-authored-by: Lysandre <lysandre.debut@reseau.eseo.fr>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,7 +27,10 @@ author = u'huggingface'
 version = u''
 # The full version, including alpha/beta/rc tags
 release = u'4.1.1'
-
+# Prefix link to point to master, comment this during version release and uncomment below line
+extlinks = {'prefix_link': ('https://github.com/huggingface/transformers/blob/master/%s', '')}
+# Prefix link to always point to corresponding version, uncomment this during version release
+# extlinks = {'prefix_link': ('https://github.com/huggingface/transformers/blob/v'+ release + '/%s', '')}
 
 # -- General configuration ---------------------------------------------------
 
@@ -40,6 +43,7 @@ release = u'4.1.1'
 # ones.
 extensions = [
     'sphinx.ext.autodoc',
+    'sphinx.ext.extlinks',
     'sphinx.ext.coverage',
     'sphinx.ext.napoleon',
     'recommonmark',
