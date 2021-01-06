3c746e0aa003dcd474ca779f910028fca44cbb98
Qbiwan <69753975+Qbiwan@users.noreply.github.com>
2021-01-06 22:13:43 +0800

    Improve documentation coverage for Herbert (#9428)
    * first commit
    
    * changed XLMTokenizer to HerbertTokenizer in code example

--- utils/check_repo.py
+++ utils/check_repo.py
@@ -402,9 +402,6 @@ SHOULD_HAVE_THEIR_OWN_PAGE = [
     "BertJapaneseTokenizer",
     "CharacterTokenizer",
     "MecabTokenizer",
-    # Herbert
-    "HerbertTokenizer",
-    "HerbertTokenizerFast",
     # Phoebus
     "PhobertTokenizer",
     # Benchmarks
