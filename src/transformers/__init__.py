940e412da8ad996df66f82fe6a854f4e72875bbc
Sylvain Gugger <sylvain.gugger@gmail.com>
2021-01-07 09:18:41 -0500

    Add documentation

--- src/transformers/__init__.py
+++ src/transformers/__init__.py
@@ -17,7 +17,10 @@
 # limitations under the License.
 
 # When adding a new object to this init, remember to add it twice: once inside the `_import_structure` dictionary and
-# once inside the `if TYPE_CHECKING` branch.
+# once inside the `if TYPE_CHECKING` branch. The `TYPE_CHECKING` should have import statements as usual, but they are
+# only there for type checking. The `_import_structure` is a dictionary submodule to list of object names, and is used
+# to defer the actual importing for when the objects are requested. This way `import transformers` provides the names
+# in the namespace without actually importing anything (and especially none of the backends).
 
 __version__ = "4.2.0dev0"
 
