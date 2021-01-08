b52405a249c4a7961f683134e251b8a525e37f85
Sylvain Gugger <35901082+sgugger@users.noreply.github.com>
2021-01-08 07:40:59 -0500

    Fast imports part 3 (#9474)
    * New intermediate inits
    
    * Update template
    
    * Avoid importing torch/tf/flax in tokenization unless necessary
    
    * Styling
    
    * Shutup flake8
    
    * Better python version check

--- src/transformers/models/ctrl/__init__.py
+++ src/transformers/models/ctrl/__init__.py
@@ -16,25 +16,71 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from ...file_utils import is_tf_available, is_torch_available
-from .configuration_ctrl import CTRL_PRETRAINED_CONFIG_ARCHIVE_MAP, CTRLConfig
-from .tokenization_ctrl import CTRLTokenizer
+from typing import TYPE_CHECKING
 
+from ...file_utils import _BaseLazyModule, is_tf_available, is_torch_available
+
+
+_import_structure = {
+    "configuration_ctrl": ["CTRL_PRETRAINED_CONFIG_ARCHIVE_MAP", "CTRLConfig"],
+    "tokenization_ctrl": ["CTRLTokenizer"],
+}
 
 if is_torch_available():
-    from .modeling_ctrl import (
-        CTRL_PRETRAINED_MODEL_ARCHIVE_LIST,
-        CTRLForSequenceClassification,
-        CTRLLMHeadModel,
-        CTRLModel,
-        CTRLPreTrainedModel,
-    )
+    _import_structure["modeling_ctrl"] = [
+        "CTRL_PRETRAINED_MODEL_ARCHIVE_LIST",
+        "CTRLForSequenceClassification",
+        "CTRLLMHeadModel",
+        "CTRLModel",
+        "CTRLPreTrainedModel",
+    ]
 
 if is_tf_available():
-    from .modeling_tf_ctrl import (
-        TF_CTRL_PRETRAINED_MODEL_ARCHIVE_LIST,
-        TFCTRLForSequenceClassification,
-        TFCTRLLMHeadModel,
-        TFCTRLModel,
-        TFCTRLPreTrainedModel,
-    )
+    _import_structure["modeling_tf_ctrl"] = [
+        "TF_CTRL_PRETRAINED_MODEL_ARCHIVE_LIST",
+        "TFCTRLForSequenceClassification",
+        "TFCTRLLMHeadModel",
+        "TFCTRLModel",
+        "TFCTRLPreTrainedModel",
+    ]
+
+
+if TYPE_CHECKING:
+    from .configuration_ctrl import CTRL_PRETRAINED_CONFIG_ARCHIVE_MAP, CTRLConfig
+    from .tokenization_ctrl import CTRLTokenizer
+
+    if is_torch_available():
+        from .modeling_ctrl import (
+            CTRL_PRETRAINED_MODEL_ARCHIVE_LIST,
+            CTRLForSequenceClassification,
+            CTRLLMHeadModel,
+            CTRLModel,
+            CTRLPreTrainedModel,
+        )
+
+    if is_tf_available():
+        from .modeling_tf_ctrl import (
+            TF_CTRL_PRETRAINED_MODEL_ARCHIVE_LIST,
+            TFCTRLForSequenceClassification,
+            TFCTRLLMHeadModel,
+            TFCTRLModel,
+            TFCTRLPreTrainedModel,
+        )
+
+else:
+    import importlib
+    import os
+    import sys
+
+    class _LazyModule(_BaseLazyModule):
+        """
+        Module class that surfaces all objects but only performs associated imports when the objects are requested.
+        """
+
+        __file__ = globals()["__file__"]
+        __path__ = [os.path.dirname(__file__)]
+
+        def _get_module(self, module_name: str):
+            return importlib.import_module("." + module_name, self.__name__)
+
+    sys.modules[__name__] = _LazyModule(__name__, _import_structure)
