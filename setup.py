8b8d5a0727d79fbad4d92fe3b9340e600a7fc4ca
Lysandre <lysandre.debut@reseau.eseo.fr>
2021-01-13 16:01:47 +0100

    Release: v4.2.0

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ To create the package for pypi.
 
 2. Commit these changes with the message: "Release: VERSION"
 
-3. Add a tag in git to mark the release: "git tag VERSION -m'Adds tag VERSION for pypi' "
+3. Add a tag in git to mark the release: "git tag VERSION -m 'Adds tag VERSION for pypi' "
    Push the tag to git: git push --tags origin master
 
 4. Build both the sources and the wheel. Do not change anything in setup.py between
@@ -248,7 +248,7 @@ install_requires = [
 
 setup(
     name="transformers",
-    version="4.2.0.dev0", # expected format is one of x.y.z.dev0, or x.y.z.rc1 or x.y.z (no to dashes, yes to dots)
+    version="4.2.0", # expected format is one of x.y.z.dev0, or x.y.z.rc1 or x.y.z (no to dashes, yes to dots)
     author="Thomas Wolf, Lysandre Debut, Victor Sanh, Julien Chaumond, Sam Shleifer, Patrick von Platen, Sylvain Gugger, Google AI Language Team Authors, Open AI team Authors, Facebook AI Authors, Carnegie Mellon University Authors",
     author_email="thomas@huggingface.co",
     description="State-of-the-art Natural Language Processing for TensorFlow 2.0 and PyTorch",
