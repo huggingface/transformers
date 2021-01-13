f4c36c916e941714f9c6c7931d3356ced4cdcce6
LSinev <LSinev@users.noreply.github.com>
2021-01-13 10:04:33 +0300

    make TopKLogitsWarper faster

--- src/transformers/generation_logits_process.py
+++ src/transformers/generation_logits_process.py
@@ -233,7 +233,7 @@ class TopKLogitsWarper(LogitsWarper):
         top_k = min(max(self.top_k, self.min_tokens_to_keep), scores.size(-1))  # Safety check
         # Remove all tokens with a probability less than the last token of the top-k
         indices_to_remove = scores < torch.topk(scores, top_k)[0][..., -1, None]
-        scores[indices_to_remove] = self.filter_value
+        scores = scores.masked_fill(indices_to_remove, self.filter_value)
         return scores
 
 
