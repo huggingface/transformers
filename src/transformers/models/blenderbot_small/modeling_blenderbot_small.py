c1159bdd864989486ff329f2dce80abc1a1955b1
Patrick von Platen <patrick.v.platen@gmail.com>
2021-01-18 13:08:56 +0100

    make style

--- src/transformers/models/blenderbot_small/modeling_blenderbot_small.py
+++ src/transformers/models/blenderbot_small/modeling_blenderbot_small.py
@@ -379,8 +379,8 @@ class BlenderbotSmallDecoderLayer(nn.Module):
             encoder_attention_mask (:obj:`torch.FloatTensor`): encoder attention mask of size
                 `(batch, 1, tgt_len, src_len)` where padding elements are indicated by very large negative values.
             layer_head_mask (:obj:`torch.FloatTensor`): mask for attention heads in a given layer of size
-                `(config.encoder_attention_heads,)`.
-                encoder_layer_head_mask (:obj:`torch.FloatTensor`): mask for encoder attention heads in a given layer of
+                `(config.encoder_attention_heads,)`. encoder_layer_head_mask (:obj:`torch.FloatTensor`): mask for
+                encoder attention heads in a given layer of
             size `(config.encoder_attention_heads,)`.
             past_key_value (:obj:`Tuple(torch.FloatTensor)`): cached past key and value projection states
             output_attentions (:obj:`bool`, `optional`):
