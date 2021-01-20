03fe5eb6c049e549b2f42333486ac1c2d9df84bd
Julien Plu <plu.julien@gmail.com>
2021-01-20 19:46:39 +0100

    add TF gelu approximate version

--- src/transformers/activations_tf.py
+++ src/transformers/activations_tf.py
@@ -80,7 +80,7 @@ ACT2FN = {
     "relu": tf.keras.activations.relu,
     "swish": tf.keras.activations.swish,
     "silu": tf.keras.activations.swish,
-    "gelu_new": tf.keras.layers.Activation(gelu_new),
+    "gelu_new": gelu_new,
     "mish": tf.keras.layers.Activation(mish),
     "tanh": tf.keras.activations.tanh,
     "gelu_fast": tf.keras.layers.Activation(gelu_fast),
