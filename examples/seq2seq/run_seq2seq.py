f1de9a51fa46f19d802dcc376f187197b9a89eb5
Sylvain Gugger <sylvain.gugger@gmail.com>
2021-01-21 12:23:39 -0500

    Add more defaults for summarization

--- examples/seq2seq/run_seq2seq.py
+++ examples/seq2seq/run_seq2seq.py
@@ -198,8 +198,17 @@ class DataTrainingArguments:
 
 
 summarization_name_mapping = {
+    "amazon_reviews_multi": ("review_body", "review_title"),
+    "big_patent": ("description", "abstract"),
     "cnn_dailymail": ("article", "highlights"),
+    "orange_sum": ("text", "summary"),
+    "pn_summary": ("article", "summary"),
+    "psc": ("extract_text", "summary_text"),
+    "samsum": ("dialogue", "summary"),
+    "thaisum": ("body", "summary"),
+    "xglue": ("news_body", "news_title"),
     "xsum": ("document", "summary"),
+    "wiki_summary": ("article", "highlights"),
 }
 
 
