da8e1955b19b970d9299a419b3fcba0653c829c8
Jesse Casey <jesse@mixtapeclub.com>
2021-01-26 19:42:22 -0500

    style fixes

--- src/transformers/trainer_utils.py
+++ src/transformers/trainer_utils.py
@@ -82,7 +82,11 @@ _re_checkpoint = re.compile(r"^" + PREFIX_CHECKPOINT_DIR + r"\-(\d+)$")
 
 def get_last_checkpoint(folder):
     content = os.listdir(folder)
-    checkpoints = [path for path in content if _re_checkpoint.search(path) is not None and os.path.isdir(os.path.join(folder, path))]
+    checkpoints = [
+        path
+        for path in content
+        if _re_checkpoint.search(path) is not None and os.path.isdir(os.path.join(folder, path))
+    ]
     if len(checkpoints) == 0:
         return
     return os.path.join(folder, max(checkpoints, key=lambda x: int(_re_checkpoint.search(x).groups()[0])))
