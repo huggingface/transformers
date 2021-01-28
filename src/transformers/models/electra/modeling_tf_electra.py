2ee9f9b69e67426aaed690f652f9cdd8b524b99d
Funtowicz Morgan <mfuntowicz@users.noreply.github.com>
2021-01-28 12:11:52 +0100

    Fix computation of attention_probs when head_mask is provided. (#9853)
    * Fix computation of attention_probs when head_mask is provided.
    
    Signed-off-by: Morgan Funtowicz <funtowiczmo@gmail.com>
    
    * Apply changes to the template
    
    Co-authored-by: Lysandre <lysandre.debut@reseau.eseo.fr>

--- src/transformers/models/electra/modeling_tf_electra.py
+++ src/transformers/models/electra/modeling_tf_electra.py
@@ -253,7 +253,7 @@ class TFElectraSelfAttention(tf.keras.layers.Layer):
 
         # Mask heads if we want to
         if head_mask is not None:
-            attention_scores = tf.multiply(attention_scores, head_mask)
+            attention_probs = tf.multiply(attention_probs, head_mask)
 
         attention_output = tf.einsum("acbe,aecd->abcd", attention_probs, value_layer)
         outputs = (attention_output, attention_probs) if output_attentions else (attention_output,)
