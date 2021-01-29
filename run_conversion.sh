066eb382e74c5ace7f1e615d58bc6a8f7847c9ca
Patrick von Platen <patrick.v.platen@gmail.com>
2021-01-29 17:31:05 +0300

    clean-up

--- run_conversion.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-eval "python src/transformers/models/wav2vec2/convert_wav2vec2_original_pytorch_checkpoint_to_pytorch.py --pytorch_dump_folder_path ../add_wav2vec/hf/wav2vec2 --checkpoint_path ../add_wav2vec/data/wav2vec2_vox_960h_new.pt --dict_path ../add_wav2vec/data"
--- tests/test_tokenization_small_blenderbot.py
+++ tests/test_tokenization_small_blenderbot.py
@@ -27,6 +27,7 @@ from .test_tokenization_common import TokenizerTesterMixin
 
 
 class BlenderbotSmallTokenizerTest(TokenizerTesterMixin, unittest.TestCase):
+
     tokenizer_class = BlenderbotSmallTokenizer
 
     def setUp(self):
