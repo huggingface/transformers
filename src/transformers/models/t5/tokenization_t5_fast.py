3162cfa09c98a045208c634e62babc2a94491738
Stas Bekman <stas@stason.org>
2021-01-29 18:16:50 -0800

    save fast tokenizer + add info logs

--- src/transformers/models/t5/tokenization_t5_fast.py
+++ src/transformers/models/t5/tokenization_t5_fast.py
@@ -148,6 +148,7 @@ class T5TokenizerFast(PreTrainedTokenizerFast):
         )
 
         self.vocab_file = vocab_file
+        self.tokenizer_file = tokenizer_file
         self._extra_ids = extra_ids
 
     def save_vocabulary(self, save_directory: str, filename_prefix: Optional[str] = None) -> Tuple[str]:
@@ -160,6 +161,15 @@ class T5TokenizerFast(PreTrainedTokenizerFast):
 
         if os.path.abspath(self.vocab_file) != os.path.abspath(out_vocab_file):
             copyfile(self.vocab_file, out_vocab_file)
+            logger.info(f"Copy vocab file to {out_vocab_file}")
+
+        out_tokenizer_file = os.path.join(
+            save_directory, (filename_prefix + "-" if filename_prefix else "") + VOCAB_FILES_NAMES["tokenizer_file"]
+        )
+
+        if os.path.abspath(self.tokenizer_file) != os.path.abspath(out_tokenizer_file):
+            copyfile(self.tokenizer_file, out_tokenizer_file)
+            logger.info(f"Copy tokenizer file to {out_tokenizer_file}")
 
         return (out_vocab_file,)
 
