c1f88c1b13f7825a0e16ce414843d12e0a568c99
Lewis Tunstall <lewis.c.tunstall@gmail.com>
2021-01-30 18:19:55 +0100

    Fix style

--- src/transformers/training_args.py
+++ src/transformers/training_args.py
@@ -149,8 +149,8 @@ class TrainingArguments:
             Whether to not use CUDA even when it is available or not.
         seed (:obj:`int`, `optional`, defaults to 42):
             Random seed that will be set at the beginning of training. To ensure reproducibility across runs, use the
-            :func:`~transformers.Trainer.model_init` function to instantiate the model if it has some randomly initialized 
-            parameters.
+            :func:`~transformers.Trainer.model_init` function to instantiate the model if it has some randomly
+            initialized parameters.
         fp16 (:obj:`bool`, `optional`, defaults to :obj:`False`):
             Whether to use 16-bit (mixed) precision training (through NVIDIA Apex) instead of 32-bit training.
         fp16_opt_level (:obj:`str`, `optional`, defaults to 'O1'):
