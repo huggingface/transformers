e640f85eba719215763f31a89fdf6b269690773a
patil-suraj <surajp815@gmail.com>
2021-02-01 23:12:58 +0530

    enable forced logit processor for all  generate methods

--- tests/test_generation_utils.py
+++ tests/test_generation_utils.py
@@ -81,7 +81,7 @@ class GenerationTesterMixin:
         diversity_penalty=None,
     ):
         process_kwargs = {
-            "min_length": input_length + 1,
+            "min_length": input_length,
             "bad_words_ids": [[1, 0]],
             "no_repeat_ngram_size": 2,
             "repetition_penalty": 1.2,
@@ -208,6 +208,8 @@ class GenerationTesterMixin:
         logits_process_kwargs, logits_processor = self._get_logits_processor_and_kwargs(
             input_ids.shape[-1],
             eos_token_id=model.config.eos_token_id,
+            forced_bos_token_id=model.config.forced_bos_token_id,
+            forced_eos_token_id=model.config.forced_eos_token_id,
             max_length=max_length,
         )
 
@@ -574,6 +576,9 @@ class GenerationTesterMixin:
             process_kwargs, logits_processor = self._get_logits_processor_and_kwargs(
                 input_ids.shape[-1],
                 model.config.eos_token_id,
+                forced_bos_token_id=model.config.forced_bos_token_id,
+                forced_eos_token_id=model.config.forced_eos_token_id,
+                max_length=max_length,
             )
             logits_warper_kwargs, logits_warper = self._get_warper_and_kwargs(num_beams=1)
 
@@ -617,6 +622,9 @@ class GenerationTesterMixin:
             process_kwargs, logits_processor = self._get_logits_processor_and_kwargs(
                 input_ids.shape[-1],
                 model.config.eos_token_id,
+                forced_bos_token_id=model.config.forced_bos_token_id,
+                forced_eos_token_id=model.config.forced_eos_token_id,
+                max_length=max_length,
             )
             logits_warper_kwargs, logits_warper = self._get_warper_and_kwargs(num_beams=1)
 
