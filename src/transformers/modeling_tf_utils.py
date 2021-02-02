01ce5d47c7dd5514c8f4093f16550f643a2a246c
Julien Plu <plu.julien@gmail.com>
2021-02-02 08:24:52 +0100

    Add documentation

--- src/transformers/modeling_tf_utils.py
+++ src/transformers/modeling_tf_utils.py
@@ -319,8 +319,10 @@ def input_processing(func, config, input_ids, **kwargs):
             The callable function of the TensorFlow model.
         config (:class:`~transformers.PretrainedConfig`):
             The config of the running model.
+        input_ids (:obj:`np.ndarray`, :obj:`tf.Tensor`, :obj:`List[tf.Tensor]` :obj:`Dict[str, tf.Tensor]` or :obj:`Dict[str, np.ndarray]`):
+            Indices of input sequence tokens in the vocabulary.
         **kwargs:
-            The inputs of the model.
+            The inputs of the model. May contain the `already_processed` argument to detect if an input has already been processed or not.
 
     Returns:
         Two lists, one for the missing layers, and another one for the unexpected layers.
