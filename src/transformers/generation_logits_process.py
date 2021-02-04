8a2920eb64e885e76290ee65aff7a829329c6f99
Nicolas Patry <patry.nicolas@protonmail.com>
2021-02-04 10:53:30 +0100

    Fixing to account for batch_size.

--- src/transformers/generation_logits_process.py
+++ src/transformers/generation_logits_process.py
@@ -314,14 +314,20 @@ class EncoderNoRepeatNGramLogitsProcessor(LogitsProcessor):
                 f"`encoder_ngram_size` has to be a strictly positive integer, but is {encoder_ngram_size}"
             )
         self.ngram_size = encoder_ngram_size
-        self.encoder_input_ids = encoder_input_ids
-        self.generated_ngrams = _get_ngrams(encoder_ngram_size, encoder_input_ids.unsqueeze(0), 1)[0]
+        if len(encoder_input_ids.shape) == 1:
+            encoder_input_ids = encoder_input_ids.unsqueeze(0)
+        self.batch_size = encoder_input_ids.shape[0]
+        self.generated_ngrams = _get_ngrams(encoder_ngram_size, encoder_input_ids, self.batch_size)
 
     def __call__(self, input_ids: torch.LongTensor, scores: torch.FloatTensor) -> torch.FloatTensor:
+        # B x num_beams
         num_hypos = scores.shape[0]
+        num_beams = num_hypos // self.batch_size
         cur_len = input_ids.shape[-1]
         banned_batch_tokens = [
-            _get_generated_ngrams(self.generated_ngrams, input_ids[hypo_idx], self.ngram_size, cur_len)
+            _get_generated_ngrams(
+                self.generated_ngrams[hypo_idx // num_beams], input_ids[hypo_idx], self.ngram_size, cur_len
+            )
             for hypo_idx in range(num_hypos)
         ]
 
