4ed763779e808591206b3ffb435a7ecf4063886f
abhishek thakur <abhishekkrthakur@users.noreply.github.com>
2021-02-09 12:07:56 +0100

    Fix example in Wav2Vec2 documentation (#10096)
    * Fix example in Wav2Vec2 documentation
    
    * fix style

--- src/transformers/models/wav2vec2/modeling_wav2vec2.py
+++ src/transformers/models/wav2vec2/modeling_wav2vec2.py
@@ -768,7 +768,8 @@ class Wav2Vec2ForCTC(Wav2Vec2PreTrainedModel):
 
         Example::
 
-            >>> from transformers import Wav2Vec2Tokenizer, Wav2Vec2Model
+            >>> import torch
+            >>> from transformers import Wav2Vec2Tokenizer, Wav2Vec2ForCTC
             >>> from datasets import load_dataset
             >>> import soundfile as sf
 
