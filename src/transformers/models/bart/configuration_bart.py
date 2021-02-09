1898ebd4e549ccfab099d9dbdb851dc839009fa3
patil-suraj <surajp815@gmail.com>
2021-02-09 11:15:51 +0530

    import warnings

--- src/transformers/models/bart/configuration_bart.py
+++ src/transformers/models/bart/configuration_bart.py
@@ -13,6 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """ BART model configuration """
+import warnings
 
 from ...configuration_utils import PretrainedConfig
 from ...utils import logging
