cdd138c87aad9b1a7e040ac39a1554d39aaa3027
Joe Davison <josephddavison@gmail.com>
2021-02-11 17:37:44 +0000

    add zero-shot distillation script

---
+++ examples/research_projects/zero-shot-distillation/README.md
@@ -0,0 +1,130 @@
+# Zero-shot classifier distillation
+
+This script provides a way to improve the speed and memory performance of a zero-shot classifier by training a more
+efficient student model from the zero-shot teacher's predictions over an unlabeled dataset.
+
+The zero-shot classification pipeline uses a model pre-trained on natural language inference (NLI) to determine the
+compatibility of a set of candidate class names with a given sequence. This serves as a convenient out-of-the-box
+classifier without the need for labeled training data. However, for a given sequence, the method requires each
+possible label to be fed through the large NLI model separately. This requirement slows results considerably,
+particularly for tasks with a large number of classes `K`.
+
+Given (1) an unlabeled corpus and (2) a set of candidate class names, this trains a student model with a standard
+classification head with `K` output dimensions. The resulting student model can then be used for classifying novel
+text instances over these `K` classes.
+
+### Usage
+
+A teacher NLI model can be distilled to a student model by running `distill_classifier.py` like so:
+
+```bash
+python distill_classifier.py --data_file <unlabeled_data.txt> --class_names_file <class_names.txt> --output_dir <output_dir>
+```
+
+`<unlabeled_data.txt>` should be a text file with a single unlabeled example per line. `<class_names.txt>` is a text file with one class name per line.
+
+Other optional arguments include:
+
+- `--teacher_name_or_path` (default: `roberta-large-mnli`): The name or path of the NLI teacher model.
+- `--student_name_or_path` (default: `distillbert-base-uncased`): The name or path of the student model which will
+be fine-tuned to copy the teacher predictions.
+- `--hypothesis_template` (default `"This example is {}."`): The template used to turn each label into an NLI-style
+hypothesis when generating teacher predictions. This template must include a `{}` or similar syntax for the
+candidate label to be inserted into the template. For example, the default template is `"This example is {}."` With
+the candidate label `"sports"`, this would be fed into the model like `"<cls> sequence to classify <sep> This
+example is sports . <sep>"`.
+- `--multi_class`: Whether or not multiple candidate labels can be true. By defeault, the scores are normalized such
+that the sum of the label likelihoods for each sequence is 1. If `--multi_class` is passed, the labels are
+considered independent and probabilities are normalized for each candidate by doing a softmax of the entailment
+score vs. the contradiction score. This is sometimes called "multi-class multi-label" classification.
+- `--temperature` (default: `1.0`): The temperature applied to the softmax of the teacher model predictions. A
+higher temperature results in a student with smoother (lower confidence) predictions than the teacher while a value
+`<1` resultings in a higher-confidence, peaked distribution. The default `1.0` is equivalent to no smoothing.
+- `--teacher_batch_size` (default: `32`): The batch size used for generating a single set of teacher predictions.
+Does not affect training. Use `--per_device_train_batch_size` to change the training batch size.
+
+Any of the arguments in the 🤗 Trainer's
+[`TrainingArguments`](https://huggingface.co/transformers/main_classes/trainer.html?#trainingarguments) can also be
+modified, such as `--learning_rate`, `--fp16`, `--no_cuda`, `--warmup_steps`, etc.
+
+### Example: Topic classification
+
+Let's say we're interested in classifying news articles into one of four topic categories: "the world", "sports",
+"business", or "science/tech". We have an unlabeled dataset, AG's News, which corresponds to this problem (in
+reality AG's News is annotated, but we will pretend it is not for the sake of the example).
+
+We can use an NLI model like `roberta-large-mnli` for zero-shot classification like so:
+
+```python
+class_names = ["the world", "sports", "business", "science/tech"]
+hypothesis_template = "This text is about {}."
+zero_shot_classifier = pipeline("zero-shot-classification", model="roberta-large-mnli")
+zero_shot_classifier("A new moon has been discovered in Jupiter's orbit", class_names)
+# {'sequence': "A new moon has been discovered in Jupiter's orbit",
+#  'labels': ['science/tech', 'the world', 'business', 'sports'],
+#  'scores': [0.80703204870224, 0.08598711341619492, 0.0719304010272026, 0.035050421953201294]}
+```
+
+But unfortunately inference is slow since each of our 4 class names must be fed through the large model for every
+example. But with our unlabeled data we can distill the model to a small distilbert classifier to make future
+inference much faster.
+
+We've put each training example from AG's News on its own line in `agnews/unlabeled.txt`, and each of the four class
+names above in the newline-separated `agnews/class_names.txt`. Then we can run distillation by running the following
+command:
+
+```bash
+python distill_classifier.py \
+--data_file ./agnews/unlabeled.txt \
+--class_names_files ./agnews/class_names.txt \
+--teacher_name_or_path roberta-large-mnli \
+--hypothesis_template "This text is about {}." \
+--output_dir ./agnews/distilled
+```
+
+The script will generate a set of soft zero-shot predictions from `roberta-large-mnli` for each example in
+`agnews/unlabeled.txt`. It will then train a student distilbert classifier on the teacher predictions and
+save the resulting model in `./agnews/distilled`.
+
+The resulting model can then be loaded and used like any other pre-trained classifier:
+
+```python
+from transformeres import AutoModel, AutoTokenizer
+model = AutoModel.from_pretrained("./agnews/distilled)
+tokenizer = AutoTokenizer.from_pretrained("./agnews/distilled)
+```
+
+and even used with a `TextClassificationPipeline`:
+
+```python
+distilled_classifier = TextClassificationPipeline(model=model, tokenizer=tokenizer, return_all_scores=True)
+distilled_classifier("A new moon has been discovered in Jupiter's orbit")
+# [[{'label': 'the world', 'score': 0.17189696431159973},
+#   {'label': 'sports', 'score': 0.027876097708940506},
+#   {'label': 'business', 'score': 0.06644482910633087},
+#   {'label': 'science/tech', 'score': 0.7337821125984192}]]
+```
+
+As we can see, the results of the student are roughly the same as the trainer. Now let's do a quick & dirty speed
+comparison:
+
+```python
+for _ in range(1000):
+    zero_shot_classifier(["A new moon has been discovered in Jupiter's orbit"] * 16, class_names)
+# runs in 1m 23s on a single V100 GPU
+```
+
+```python
+%%time
+for _ in range(1000):
+    distilled_classifier(["A new moon has been discovered in Jupiter's orbit"] * 16)
+# runs in 10.3s on a single V100 GPU
+```
+
+As we can see, the distilled student model runs an order of magnitude faster than its teacher NLI model. This is
+also a case where we only have `K=4` possible labels. The results will vary, but the more class names exist, the
+more drastic the speedup will be.
+
+Since we secretly have access to ground truth labels for AG's news, we can evaluate the accuracy of each model. The
+original zero-shot model `roberta-large-mnli` gets an accuracy of 70% on the test set. After training a student on
+the unlabeled training set, the distilled model gets 70%.
---
+++ examples/research_projects/zero-shot-distillation/distill_classifier.py
@@ -0,0 +1,233 @@
+from dataclasses import dataclass, field, asdict
+from typing import Optional
+from tqdm import tqdm
+import sys
+import logging
+
+from transformers import AutoModelForSequenceClassification, AutoTokenizer, Trainer, TrainingArguments, HfArgumentParser, default_data_collator
+from datasets import Dataset
+from diskcache import Cache
+import torch
+import numpy as np
+
+
+DESCRIPTION = """
+Distills an NLI-based zero-shot classifier to a smaller, more efficient model with a fixed set of candidate class
+names. Useful for speeding up zero-shot classification in cases where labeled training data is not available, but
+when only a single fixed set of classes is needed. Takes a teacher NLI model, student classifier model, unlabeled
+dataset, and set of K possible class names. Yields a single classifier with K outputs corresponding to the provided
+class names.
+"""
+
+
+@dataclass
+class TeacherModelArguments:
+    teacher_name_or_path: Optional[str] = field(
+        default="roberta-large-mnli",
+        metadata={"help": "The NLI/zero-shot teacher model to be distilled."}
+    )
+    hypothesis_template: Optional[str] = field(
+        default="This example is {}.",
+        metadata={
+            "help": (
+                "Template used to turn class names into mock hypotheses for teacher NLI model. Must include {{}}"
+                "where class name is inserted."
+            )
+        }
+    )
+    teacher_batch_size: Optional[int] = field(
+        default=32,
+        metadata={"help": "Batch size for generating teacher predictions."}
+    )
+    multi_class: Optional[bool] = field(
+        default=False,
+        metadata={
+            "help": (
+                "Allow multiple classes to be true rather than forcing them to sum to 1 (sometimes called"
+                "multi-class multi-label classification)."
+            )
+        }
+    )
+    temperature: Optional[float] = field(
+        default=1.,
+        metadata={"help": "Temperature applied to teacher softmax for distillation."}
+    )
+
+
+@dataclass
+class StudentModelArguments:
+    student_name_or_path: Optional[str] = field(
+        default="distilbert-base-uncased",
+        metadata={"help": "The NLI/zero-shot teacher model to be distilled."}
+    )
+
+
+@dataclass
+class DataTrainingArguments:
+    data_file: str = field(metadata={"help": "Text file with one unlabeled instance per line."})
+    class_names_file: str = field(metadata={"help": "Text file with one class name per line."})
+
+
+@dataclass
+class DistillTrainingArguments(TrainingArguments):
+    output_dir: Optional[str] = field(
+        default=None,
+        metadata={"help": "The output directory where the model predictions and checkpoints will be written."},
+    )
+    per_device_train_batch_size: int = field(
+        default=32, metadata={"help": "Batch size per GPU/TPU core/CPU for training."}
+    )
+    per_device_eval_batch_size: int = field(
+        default=128, metadata={"help": "Batch size per GPU/TPU core/CPU for evaluation."}
+    )
+    num_train_epochs: float = field(default=1.0, metadata={"help": "Total number of training epochs to perform."})
+    do_train: bool = field(default=True, metadata={"help": "Whether to run training."})
+
+
+class DistillationTrainer(Trainer):
+
+    def compute_loss(self, model, inputs, return_outputs=False):
+        target_p = inputs["labels"]
+        outputs = model(inputs["input_ids"], attention_mask=inputs["attention_mask"])
+        logits = outputs[0]
+
+        loss = -torch.sum(target_p * logits.log_softmax(dim=-1), axis=-1).mean()
+
+        if return_outputs:
+            return loss, outputs
+
+        return loss
+
+
+def read_lines(path):
+    lines = []
+    with open(path, "r") as f:
+        for line in f:
+            line = line.strip()
+            if len(line) > 0:
+                lines.append(line)
+    return lines
+
+def get_premise_hypothesis_pairs(examples, class_names, hypothesis_template):
+    premises = []
+    hypotheses = []
+    for example in examples:
+        for name in class_names:
+            premises.append(example)
+            hypotheses.append(hypothesis_template.format(name))
+    return premises, hypotheses
+
+def get_entailment_id(config):
+    for label, ind in config.label2id.items():
+        if label.lower().startswith("entail"):
+            return ind
+    logging.warning("Could not identify entailment dimension from teacher config label2id. Setting to -1.")
+    return -1
+
+def get_teacher_predictions(model_path, examples, class_names, hypothesis_template, batch_size, temperature, multi_class, device):
+    """
+    Instantiates a zero-shot model from model_path and reduces
+    """
+    model = AutoModelForSequenceClassification.from_pretrained(model_path).to(device)
+    tokenizer = AutoTokenizer.from_pretrained(model_path, use_fast=True)
+
+    premises, hypotheses = get_premise_hypothesis_pairs(examples, class_names, hypothesis_template)
+    logits = []
+
+    for i in tqdm(range(0, len(premises), batch_size)):
+        batch_premises = premises[i:i+batch_size]
+        batch_hypotheses = hypotheses[i:i+batch_size]
+
+        encodings = tokenizer(
+            batch_premises,
+            batch_hypotheses,
+            padding=True,
+            truncation="only_first",
+            return_tensors="pt",
+        )
+
+        with torch.no_grad():
+            outputs = model(**encodings.to(device))
+            logits.append(outputs.logits.detach().cpu())
+
+    entail_id = get_entailment_id(model.config)
+    contr_id = -1 if entail_id == 0 else 0
+    logits = torch.cat(logits, dim=0) # N*K x 3
+    nli_logits = logits.reshape(len(examples), len(class_names), -1)[..., [contr_id, entail_id]] # N x K x 2
+
+    if not multi_class:
+        nli_logp = nli_logits.log_softmax(-1) / temperature
+        nli_prob = nli_logp.softmax(1)
+    else:
+        nli_prob = (nli_logits / temperature).softmax(-1)
+    
+    return nli_prob[..., 1] # N x K
+
+def main():
+    # 0. parse arguments
+    parser = HfArgumentParser(
+        (DataTrainingArguments, TeacherModelArguments, StudentModelArguments, DistillTrainingArguments),
+        description=DESCRIPTION,
+    )
+    
+    if len(sys.argv) == 2 and sys.argv[1].endswith(".json"):
+        # If we pass only one argument to the script and it's the path to a json file,
+        # let's parse it to get our arguments.
+        data_args, teacher_args, student_args, training_args = parser.parse_json_file(json_file=os.path.abspath(sys.argv[1]))
+    else:
+        data_args, teacher_args, student_args, training_args = parser.parse_args_into_dataclasses()
+
+    # 1. read in data
+    examples = read_lines(data_args.data_file)
+    class_names = read_lines(data_args.class_names_file)
+
+    # 2. get teacher predictions and load into dataset
+    teacher_device = torch.device("cuda:0") if torch.cuda.is_available() and not training_args.no_cuda else torch.device("cpu")
+    teacher_soft_preds = get_teacher_predictions(
+        teacher_args.teacher_name_or_path,
+        examples,
+        class_names,
+        teacher_args.hypothesis_template,
+        teacher_args.teacher_batch_size,
+        teacher_args.temperature,
+        teacher_args.multi_class,
+        teacher_device,
+    )
+    dataset = Dataset.from_dict({
+        "text": examples,
+        "labels": teacher_soft_preds,
+    })
+
+    # 3. create student
+    model = AutoModelForSequenceClassification.from_pretrained(student_args.student_name_or_path, num_labels=len(class_names))
+    tokenizer = AutoTokenizer.from_pretrained(student_args.student_name_or_path, use_fast=True)
+    model.config.id2label = {i: label for i, label in enumerate(class_names)}
+    model.config.label2id = {label: i for i, label in enumerate(class_names)}
+
+    # 4. train student on teacher predictions
+    dataset = dataset.map(lambda ex: tokenizer(ex["text"]))
+    dataset.set_format("torch")
+
+    def compute_metrics(p, return_outputs=False):
+        preds = p.predictions.argmax(-1)
+        proxy_labels = p.label_ids.argmax(-1)
+        return {"agreement": (preds == proxy_labels).mean().item()}
+
+    trainer = DistillationTrainer(
+        model=model,
+        tokenizer=tokenizer,
+        args=training_args,
+        train_dataset=dataset,
+        compute_metrics=compute_metrics,
+    )
+
+    trainer.train()
+
+    agreement = trainer.evaluate(eval_dataset=dataset)['eval_agreement']
+    print(f"Agreement of student and teacher predictions: {agreement * 100:0.2f}%")
+
+    trainer.save_model()
+
+
+if __name__ == "__main__":
+    main()
