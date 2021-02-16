db821b18f05dd63182c5cba4337ef34ce0be3276
patil-suraj <surajp815@gmail.com>
2021-02-17 01:52:20 +0530

    remove input_embeds

--- src/transformers/models/speech_to_text_transformer/modeling_speech_to_text_transformer.py
+++ src/transformers/models/speech_to_text_transformer/modeling_speech_to_text_transformer.py
@@ -201,22 +201,6 @@ class SpeechToTextTransformerSinusoidalPositionalEmbedding(nn.Module):
 
         return self.weights.index_select(0, position_ids.view(-1)).view(bsz, seq_len, -1).detach()
 
-    def create_position_ids_from_inputs_embeds(self, inputs_embeds):
-        """
-        We are provided embeddings directly. We cannot infer which are padded so just generate sequential position ids.
-
-        Args:
-            inputs_embeds: torch.Tensor
-        Returns: torch.Tensor
-        """
-        input_shape = inputs_embeds.size()[:-1]
-        sequence_length = input_shape[1]
-
-        position_ids = torch.arange(
-            self.padding_idx + 1, sequence_length + self.padding_idx + 1, dtype=torch.long, device=inputs_embeds.device
-        )
-        return position_ids.unsqueeze(0).expand(input_shape)
-
 
 class SpeechToTextTransformerAttention(nn.Module):
     """Multi-headed attention from 'Attention Is All You Need' paper"""
