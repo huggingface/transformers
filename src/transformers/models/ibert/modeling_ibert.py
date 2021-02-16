ae031fff086043905065e238c925a149039522e9
Sehoon Kim <sehoonkim@berkeley.edu>
2021-02-15 17:48:56 -0800

    copyrights fixed

--- src/transformers/models/ibert/modeling_ibert.py
+++ src/transformers/models/ibert/modeling_ibert.py
@@ -1,6 +1,7 @@
 # coding=utf-8
-# Copyright 2018 The Google AI Language Team Authors and The HuggingFace Inc. team.
-# Copyright (c) 2018, NVIDIA CORPORATION.  All rights reserved.
+# Copyright 2021 The I-BERT Authors (Sehoon Kim, Amir Gholami, Zhewei Yao, 
+# Michael Mahoney, Kurt Keutzer - UC Berkeley) and The HuggingFace Inc. team.
+# Copyright (c) 20121, NVIDIA CORPORATION.  All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
