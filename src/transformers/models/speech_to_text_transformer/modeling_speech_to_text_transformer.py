5ac67b3688573e143d729e44a90f528f54bce44f
patil-suraj <surajp815@gmail.com>
2021-02-16 18:36:45 +0530

    expand embeddings if needed

--- src/transformers/models/speech_to_text_transformer/modeling_speech_to_text_transformer.py
+++ src/transformers/models/speech_to_text_transformer/modeling_speech_to_text_transformer.py
@@ -197,7 +197,13 @@ class SpeechToTextTransformerSinusoidalPositionalEmbedding(nn.Module):
         else:
             bsz, seq_len = inputs_embeds.size()[:-1]
             position_ids = self.create_position_ids_from_inputs_embeds(inputs_embeds)
+        
+        # expand embeddings if needed
+        max_pos = self.padding_idx + 1 + seq_len
+        if max_pos > self.weights.size(0):
+            self.weights = self.get_embedding(max_pos, self.embedding_dim, self.padding_idx)
         self.weights = self.weights.to(self._float_tensor)
+        
         return self.weights.index_select(0, position_ids.view(-1)).view(bsz, seq_len, -1).detach()
 
     def create_position_ids_from_inputs_embeds(self, inputs_embeds):
