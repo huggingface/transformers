610177104601d44e9f0d027b72df17936af13bcd
Sehoon Kim <sehoonkim@berkeley.edu>
2021-02-15 21:42:43 -0800

    configutration.py docstring fixed

--- src/transformers/models/ibert/configuration_ibert.py
+++ src/transformers/models/ibert/configuration_ibert.py
@@ -36,13 +36,12 @@ class IBertConfig(PretrainedConfig):
 
     Configuration objects inherit from :class:`~transformers.PretrainedConfig` and can be used to control the model
     outputs. Read the documentation from :class:`~transformers.PretrainedConfig` for more information. This code is
-    referred from :class:`~transformers.BertConfig` and :class:`~transformers.RobertaConfig`.
+    copied from both :class:`~transformers.IBertConfig` and :class:`~transformers.RobertaConfig`.
 
     Args:
         vocab_size (:obj:`int`, `optional`, defaults to 30522):
-            Vocabulary size of the BERT model. Defines the number of different tokens that can be represented by the
-            :obj:`inputs_ids` passed when calling :class:`~transformers.BertModel` or
-            :class:`~transformers.TFBertModel`.
+            Vocabulary size of the I-BERT model. Defines the number of different tokens that can be represented by the
+            :obj:`inputs_ids` passed when calling :class:`~transformers.IBertModel`
         hidden_size (:obj:`int`, `optional`, defaults to 768):
             Dimensionality of the encoder layers and the pooler layer.
         num_hidden_layers (:obj:`int`, `optional`, defaults to 12):
@@ -62,8 +61,7 @@ class IBertConfig(PretrainedConfig):
             The maximum sequence length that this model might ever be used with. Typically set this to something large
             just in case (e.g., 512 or 1024 or 2048).
         type_vocab_size (:obj:`int`, `optional`, defaults to 2):
-            The vocabulary size of the :obj:`token_type_ids` passed when calling :class:`~transformers.BertModel` or
-            :class:`~transformers.TFBertModel`.
+            The vocabulary size of the :obj:`token_type_ids` passed when calling :class:`~transformers.IBertModel`
         initializer_range (:obj:`float`, `optional`, defaults to 0.02):
             The standard deviation of the truncated_normal_initializer for initializing all weight matrices.
         layer_norm_eps (:obj:`float`, `optional`, defaults to 1e-12):
