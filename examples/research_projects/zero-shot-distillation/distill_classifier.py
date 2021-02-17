1efeca05bb535735cca6497d4b1cc63a27c5f2a9
Joe Davison <josephddavison@gmail.com>
2021-02-17 19:11:54 +0000

    add multi-gpu teacher inference plus tidying up more code

--- examples/research_projects/zero-shot-distillation/distill_classifier.py
+++ examples/research_projects/zero-shot-distillation/distill_classifier.py
@@ -1,12 +1,24 @@
+import sys
+import logging
+import os
 from dataclasses import dataclass, field, asdict
 from typing import Optional, List
 from tqdm.auto import tqdm
-import sys
-import logging
 
-from transformers import AutoModelForSequenceClassification, AutoTokenizer, Trainer, TrainingArguments, HfArgumentParser
-from datasets import Dataset
 import torch
+from torch import nn
+from datasets import Dataset
+
+from transformers import (
+    AutoModelForSequenceClassification,
+    AutoTokenizer,
+    Trainer,
+    TrainingArguments,
+    HfArgumentParser,
+    set_seed,
+)
+from transformers.trainer_utils import get_last_checkpoint, is_main_process
+from transformers import utils
 
 
 DESCRIPTION = """\
@@ -16,6 +28,8 @@ when only a single fixed set of classes is needed. Takes a teacher NLI model, st
 dataset, and set of K possible class names. Yields a single classifier with K outputs corresponding to the provided
 class names.
 """
+
+logger = logging.getLogger(__name__)
 
 
 @dataclass
@@ -136,19 +150,24 @@ def get_teacher_predictions(
         examples: List[str],
         class_names: List[str],
         hypothesis_template: str,
-        batch_size: int,
+        per_device_batch_size: int,
         temperature: float,
         multi_class: bool,
-        device: torch.device
+        no_cuda: bool
     ):
     """
-    Gets teachers by the same method as the zero-shot pipeline, but with more efficient mini-batching than pipelines
+    Gets predictions by the same method as the zero-shot pipeline but with DataParallel & more efficient batching
     """
-    model = AutoModelForSequenceClassification.from_pretrained(model_path).to(device)
+    model = AutoModelForSequenceClassification.from_pretrained(model_path)
+    if not no_cuda and torch.cuda.is_available():
+        model = model.cuda()
+    model = nn.DataParallel(model)
     tokenizer = AutoTokenizer.from_pretrained(model_path, use_fast=True)
 
     premises, hypotheses = get_premise_hypothesis_pairs(examples, class_names, hypothesis_template)
     logits = []
+
+    batch_size = per_device_batch_size * max(len(model.device_ids), 1)
 
     for i in tqdm(range(0, len(premises), batch_size)):
         batch_premises = premises[i:i+batch_size]
@@ -163,24 +182,25 @@ def get_teacher_predictions(
         )
 
         with torch.no_grad():
-            outputs = model(**encodings.to(device))
+            outputs = model(**encodings)
             logits.append(outputs.logits.detach().cpu())
 
-    entail_id = get_entailment_id(model.config)
+    entail_id = get_entailment_id(model.module.config)
     contr_id = -1 if entail_id == 0 else 0
     logits = torch.cat(logits, dim=0) # N*K x 3
     nli_logits = logits.reshape(len(examples), len(class_names), -1)[..., [contr_id, entail_id]] # N x K x 2
 
-    if not multi_class:
-        nli_logp = nli_logits.log_softmax(-1) / temperature
-        nli_prob = nli_logp.softmax(1)
-    else:
+    if multi_class:
+        # softmax over (contr, entail) logits for each class independently
         nli_prob = (nli_logits / temperature).softmax(-1)
+    else:
+        # softmax over entail logits across classes s.t. class probabilities sum to 1.
+        nli_prob = (nli_logits / temperature).softmax(1)
     
     return nli_prob[..., 1] # N x K
 
+
 def main():
-    # 0. parse arguments
     parser = HfArgumentParser(
         (DataTrainingArguments, TeacherModelArguments, StudentModelArguments, DistillTrainingArguments),
         description=DESCRIPTION,
@@ -193,13 +213,51 @@ def main():
     else:
         data_args, teacher_args, student_args, training_args = parser.parse_args_into_dataclasses()
 
+    # Detecting last checkpoint.
+    last_checkpoint = None
+    if os.path.isdir(training_args.output_dir) and training_args.do_train and not training_args.overwrite_output_dir:
+        last_checkpoint = get_last_checkpoint(training_args.output_dir)
+        if last_checkpoint is None and len(os.listdir(training_args.output_dir)) > 0:
+            raise ValueError(
+                f"Output directory ({training_args.output_dir}) already exists and is not empty. "
+                "Use --overwrite_output_dir to overcome."
+            )
+        elif last_checkpoint is not None:
+            logger.info(
+                f"Checkpoint detected, resuming training at {last_checkpoint}. To avoid this behavior, change "
+                "the `--output_dir` or add `--overwrite_output_dir` to train from scratch."
+            )
+
+    # Setup logging
+    logging.basicConfig(
+        format="%(asctime)s - %(levelname)s - %(name)s -   %(message)s",
+        datefmt="%m/%d/%Y %H:%M:%S",
+        handlers=[logging.StreamHandler(sys.stdout)],
+    )
+    logger.setLevel(logging.INFO if is_main_process(training_args.local_rank) else logging.WARN)
+
+    # Log on each process the small summary:
+    logger.warning(
+        f"Process rank: {training_args.local_rank}, device: {training_args.device}, n_gpu: {training_args.n_gpu}"
+        + f"distributed training: {bool(training_args.local_rank != -1)}, 16-bits training: {training_args.fp16}"
+    )
+    # Set the verbosity to info of the Transformers logger (on main process only):
+    if is_main_process(training_args.local_rank):
+        utils.logging.set_verbosity_info()
+        utils.logging.enable_default_handler()
+        utils.logging.enable_explicit_format()
+    logger.info(f"Training/evaluation parameters {training_args}")
+
+    # Set seed before initializing model.
+    set_seed(training_args.seed)
+
+
     # 1. read in data
     examples = read_lines(data_args.data_file)
     class_names = read_lines(data_args.class_names_file)
 
     # 2. get teacher predictions and load into dataset
     logging.info("Generating predictions from zero-shot teacher model")
-    teacher_device = torch.device("cuda:0") if torch.cuda.is_available() and not training_args.no_cuda else torch.device("cpu")
     teacher_soft_preds = get_teacher_predictions(
         teacher_args.teacher_name_or_path,
         examples,
@@ -208,7 +266,7 @@ def main():
         teacher_args.teacher_batch_size,
         teacher_args.temperature,
         teacher_args.multi_class,
-        teacher_device,
+        training_args.no_cuda,
     )
     dataset = Dataset.from_dict({
         "text": examples,
@@ -216,18 +274,19 @@ def main():
     })
 
     # 3. create student
+    logging.info("Initializing student model")
     model = AutoModelForSequenceClassification.from_pretrained(student_args.student_name_or_path, num_labels=len(class_names))
     tokenizer = AutoTokenizer.from_pretrained(student_args.student_name_or_path, use_fast=True)
     model.config.id2label = {i: label for i, label in enumerate(class_names)}
     model.config.label2id = {label: i for i, label in enumerate(class_names)}
 
     # 4. train student on teacher predictions
-    dataset = dataset.map(lambda ex: tokenizer(ex["text"]))
+    dataset = dataset.map(tokenizer, input_columns="text")
     dataset.set_format("torch")
 
     def compute_metrics(p, return_outputs=False):
         preds = p.predictions.argmax(-1)
-        proxy_labels = p.label_ids.argmax(-1)
+        proxy_labels = p.label_ids.argmax(-1) # "label_ids" are actually distributions
         return {"agreement": (preds == proxy_labels).mean().item()}
 
     trainer = DistillationTrainer(
