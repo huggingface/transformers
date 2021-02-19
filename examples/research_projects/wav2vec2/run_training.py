correct gradient checkpointing

examples/research_projects/wav2vec2/run_training.py
@@ -29,7 +29,7 @@ if version.parse(torch.__version__) >= version.parse("1.6"):
     from torch.cuda.amp import autocast
 
 
-model = Wav2Vec2ForCTC.from_pretrained("facebook/wav2vec2-large-lv60")
+model = Wav2Vec2ForCTC.from_pretrained("facebook/wav2vec2-large-lv60", gradient_checkpointing=True)
 tokenizer = Wav2Vec2Tokenizer.from_pretrained("facebook/wav2vec2-large-960h-lv60")
 
 train_dataset = datasets.load_dataset("librispeech_asr", "clean", split="train.100")
@@ -201,10 +201,10 @@ class CTCTrainer(Trainer):
 training_args = TrainingArguments(
     output_dir="./results",
     num_train_epochs=40,
-    per_device_train_batch_size=2,
-    per_device_eval_batch_size=4,
+    per_device_train_batch_size=16,
+    per_device_eval_batch_size=16,
     evaluation_strategy="steps",
-    gradient_accumulation_steps=16,
+    gradient_accumulation_steps=2,
     save_total_limit=3,
     save_steps=500,
     eval_steps=100,

src/transformers/models/wav2vec2/modeling_wav2vec2.py
@@ -472,7 +472,12 @@ class Wav2Vec2EncoderLayer(nn.Module):
         hidden_states = hidden_states + self.feed_forward(hidden_states)
         hidden_states = self.final_layer_norm(hidden_states)
 
-        return hidden_states, attn_weights
+        outputs = (hidden_states,)
+
+        if output_attentions:
+            outputs += (attn_weights,)
+
+        return outputs
 
 
 class Wav2Vec2EncoderLayerStableLayerNorm(nn.Module):
@@ -499,7 +504,12 @@ class Wav2Vec2EncoderLayerStableLayerNorm(nn.Module):
         hidden_states = attn_residual + hidden_states
         hidden_states = hidden_states + self.feed_forward(self.final_layer_norm(hidden_states))
 
-        return hidden_states, attn_weights
+        outputs = (hidden_states,)
+
+        if output_attentions:
+            outputs += (attn_weights,)
+
+        return outputs
 
 
 class Wav2Vec2Encoder(nn.Module):