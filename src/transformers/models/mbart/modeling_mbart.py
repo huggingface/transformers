6ad709c6104256ff4655e99ea3cf36d3d92df658
mingruimingrui <mingruimingrui@hotmail.com>
2021-02-19 15:37:34 +0800

    padding_idx fix for mbart

--- src/transformers/models/mbart/modeling_mbart.py
+++ src/transformers/models/mbart/modeling_mbart.py
@@ -114,12 +114,12 @@ class MBartLearnedPositionalEmbedding(nn.Embedding):
     This module learns positional embeddings up to a fixed maximum size.
     """
 
-    def __init__(self, num_embeddings: int, embedding_dim: int, padding_idx: int):
-        assert padding_idx is not None, "`padding_idx` should not be None, but of type int"
+    def __init__(self, num_embeddings: int, embedding_dim: int, padding_idx: Optional[int] = None):
         # MBart is set up so that if padding_idx is specified then offset the embedding ids by 2
         # and adjust num_embeddings appropriately. Other models dont have this hack
         self.offset = 2
         super().__init__(num_embeddings + self.offset, embedding_dim, padding_idx=padding_idx)
+        self.weight.data[: self.offset].fill_(0)
 
     def forward(self, input_ids_shape: torch.Size, past_key_values_length: int = 0):
         """`input_ids_shape` is expected to be [bsz x seqlen]."""
@@ -678,7 +678,6 @@ class MBartEncoder(MBartPreTrainedModel):
         self.embed_positions = MBartLearnedPositionalEmbedding(
             config.max_position_embeddings,
             embed_dim,
-            self.padding_idx,
         )
         self.layers = nn.ModuleList([MBartEncoderLayer(config) for _ in range(config.encoder_layers)])
         self.layernorm_embedding = nn.LayerNorm(embed_dim)
@@ -844,7 +843,6 @@ class MBartDecoder(MBartPreTrainedModel):
         self.embed_positions = MBartLearnedPositionalEmbedding(
             config.max_position_embeddings,
             config.d_model,
-            self.padding_idx,
         )
         self.layers = nn.ModuleList([MBartDecoderLayer(config) for _ in range(config.decoder_layers)])
         self.layernorm_embedding = nn.LayerNorm(config.d_model)
