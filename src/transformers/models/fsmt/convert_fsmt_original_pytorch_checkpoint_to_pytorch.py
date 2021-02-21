fb0d6bceb77b514f59a4acddaee1871072f7d6b9
tagucci <tagucci@users.noreply.github.com>
2021-02-22 00:54:27 +0900

    fix typo in conversion script (#10316)
    * fix typo in conversion script
    
    * style
    
    Co-authored-by: Stas Bekman <stas@stason.org>

--- src/transformers/models/fsmt/convert_fsmt_original_pytorch_checkpoint_to_pytorch.py
+++ src/transformers/models/fsmt/convert_fsmt_original_pytorch_checkpoint_to_pytorch.py
@@ -31,9 +31,9 @@ import torch
 from fairseq import hub_utils
 from fairseq.data.dictionary import Dictionary
 
-from transfomers.models.fsmt.tokenization_fsmt import VOCAB_FILES_NAMES
 from transformers import FSMTConfig, FSMTForConditionalGeneration
 from transformers.file_utils import WEIGHTS_NAME
+from transformers.models.fsmt.tokenization_fsmt import VOCAB_FILES_NAMES
 from transformers.tokenization_utils_base import TOKENIZER_CONFIG_FILE
 from transformers.utils import logging
 
