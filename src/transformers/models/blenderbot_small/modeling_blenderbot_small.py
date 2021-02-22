ed6d126fda2241f3fa66ae0aaeed519a9915ee3a
mingruimingrui <mingruimingrui@hotmail.com>
2021-02-22 15:45:09 +0800

    Remove accidentally added quotes

--- src/transformers/models/blenderbot_small/modeling_blenderbot_small.py
+++ src/transformers/models/blenderbot_small/modeling_blenderbot_small.py
@@ -82,7 +82,7 @@ def _make_causal_mask(input_ids_shape: torch.Size, dtype: torch.dtype, past_key_
     mask = torch.full((tgt_len, tgt_len), float("-inf"))
     mask_cond = torch.arange(mask.size(-1))
     mask.masked_fill_(mask_cond < (mask_cond + 1).view(mask.size(-1), 1), 0)
-    mask = mask.to(dtype)``
+    mask = mask.to(dtype)
 
     if past_key_values_length > 0:
         mask = torch.cat([torch.zeros(tgt_len, past_key_values_length, dtype=dtype), mask], dim=-1)
