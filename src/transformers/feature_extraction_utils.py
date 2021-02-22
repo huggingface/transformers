52466855b16935ae34d3363680e76b1f9ba52c3b
Patrick von Platen <patrick.v.platen@gmail.com>
2021-02-22 17:55:01 +0300

    Update src/transformers/feature_extraction_utils.py

--- src/transformers/feature_extraction_utils.py
+++ src/transformers/feature_extraction_utils.py
@@ -43,7 +43,6 @@ class PreTrainedFeatureExtractor:
         # in any way like Tokenizers are -> therefore all configuration params should be
         # stored in a json config
         self.sampling_rate = kwargs.get("sampling_rate", None)
-        self.pad_vector = kwargs.get("pad_vector", None)
         self.feature_dim = kwargs.get("feature_dim", None)  # this will be 1 for Wav2Vec2, but 768 for Speech2TextTransformers
 
     def pad(self, feature: BatchFeature):
