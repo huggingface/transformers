698e3b4ccb72d9b382c623934d7b0f088a1a4398
Patrick von Platen <patrick.v.platen@gmail.com>
2021-02-26 15:39:39 +0300

    delete conversion script

--- run_conversion.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-eval "python src/transformers/models/wav2vec2/convert_wav2vec2_original_pytorch_checkpoint_to_pytorch.py --pytorch_dump_folder_path ../add_wav2vec/hf/wav2vec2-base --checkpoint_path ../add_wav2vec/data/wav2vec_small.pt --dict_path ../add_wav2vec/data --not_finetuned"
