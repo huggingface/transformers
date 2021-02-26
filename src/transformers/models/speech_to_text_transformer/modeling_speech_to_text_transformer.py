79de9b51425074b9ad9b1f8f4bef613061048c93
patil-suraj <surajp815@gmail.com>
2021-02-26 15:20:16 +0530

    remove classification head

--- src/transformers/models/speech_to_text_transformer/modeling_speech_to_text_transformer.py
+++ src/transformers/models/speech_to_text_transformer/modeling_speech_to_text_transformer.py
@@ -521,30 +521,6 @@ class Speech2TextTransformerDecoderLayer(nn.Module):
         return outputs
 
 
-class Speech2TextTransformerClassificationHead(nn.Module):
-    """Head for sentence-level classification tasks."""
-
-    def __init__(
-        self,
-        input_dim: int,
-        inner_dim: int,
-        num_classes: int,
-        pooler_dropout: float,
-    ):
-        super().__init__()
-        self.dense = nn.Linear(input_dim, inner_dim)
-        self.dropout = nn.Dropout(p=pooler_dropout)
-        self.out_proj = nn.Linear(inner_dim, num_classes)
-
-    def forward(self, hidden_states: torch.Tensor):
-        hidden_states = self.dropout(hidden_states)
-        hidden_states = self.dense(hidden_states)
-        hidden_states = torch.tanh(hidden_states)
-        hidden_states = self.dropout(hidden_states)
-        hidden_states = self.out_proj(hidden_states)
-        return hidden_states
-
-
 class Speech2TextTransformerPreTrainedModel(PreTrainedModel):
     config_class = Speech2TextTransformerConfig
     base_model_prefix = "model"
