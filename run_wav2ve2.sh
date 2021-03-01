d3e2b4a95aeb8c9bd963a34407d48ad8cb118edf
Patrick von Platen <patrick.v.platen@gmail.com>
2021-03-01 11:56:45 +0300

    remove test file

--- run_wav2ve2.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-for i in {1..20}; do
-#	pytest -s tests/test_modeling_wav2vec2.py::Wav2Vec2RobustModelTest::test_retain_grad_hidden_states_attentions
-	pytest -s tests/test_modeling_wav2vec2.py
-done;
