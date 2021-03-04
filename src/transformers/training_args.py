3f212e8888db58132d0c4c4276aa5bea09c6cd0b
philschmid <schmidphilipp1995@gmail.com>
2021-03-04 16:57:10 +0100

    adjusted typing

--- src/transformers/training_args.py
+++ src/transformers/training_args.py
@@ -288,7 +288,7 @@ class TrainingArguments:
 
     """
 
-    output_dir: Optional[str] = field(
+    output_dir: str = field(
         metadata={"help": "The output directory where the model predictions and checkpoints will be written."},
     )
     overwrite_output_dir: bool = field(
