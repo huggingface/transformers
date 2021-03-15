6b23ad3683b98a141e4b4d27cdd1382ad1dd9093
Sylvain Gugger <sylvain.gugger@gmail.com>
2021-03-15 19:10:10 -0400

    Add helpful comment

--- examples/question-answering/run_qa.py
+++ examples/question-answering/run_qa.py
@@ -45,6 +45,7 @@ from transformers.utils import check_min_version
 from utils_qa import postprocess_qa_predictions
 
 
+# Will error if the minimal version of Transformers is not installed. Remove at your own risks.
 check_min_version("4.4.0.dev0")
 
 logger = logging.getLogger(__name__)
