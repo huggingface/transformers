f7e0de3bf626e0ae666b8b695c13be8b3e2a6b94
Patrick von Platen <patrick.v.platen@gmail.com>
2021-03-15 12:11:18 +0300

    Update src/transformers/models/wav2vec2/feature_extraction_wav2vec2.py

--- src/transformers/models/wav2vec2/feature_extraction_wav2vec2.py
+++ src/transformers/models/wav2vec2/feature_extraction_wav2vec2.py
@@ -33,7 +33,7 @@ class Wav2Vec2FeatureExtractor(SequenceFeatureExtractor):
     r"""
     Constructs a Wav2Vec2 feature extractor.
 
-    This feature extractor inherits from :class:`~transformers.SequenceFeatureExtractor` which contains most of the
+    This feature extractor inherits from :class:`~transformers.feature_extraction_sequence_utils.SequenceFeatureExtractor` which contains most of the
     main methods. Users should refer to this superclass for more information regarding those methods.
 
     Args:
