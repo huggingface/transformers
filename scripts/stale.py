cb8d9c8b3f51641866ce7d5b009e526e9446d0aa
Lysandre <lysandre.debut@reseau.eseo.fr>
2021-03-17 11:47:08 -0400

    Unleash the stalebot

--- scripts/stale.py
+++ scripts/stale.py
@@ -44,21 +44,21 @@ def main():
             and (dt.utcnow() - issue.created_at).days >= 30
             and not any(label.name.lower() in LABELS_TO_EXEMPT for label in issue.get_labels())
         ):
-            print(f"Would close issue {issue.number} since it has been 7 days of inactivity since bot mention.")
-            # issue.edit(state="closed")
+            # print(f"Would close issue {issue.number} since it has been 7 days of inactivity since bot mention.")
+            issue.edit(state="closed")
         elif (
             (dt.utcnow() - issue.updated_at).days > 23
             and (dt.utcnow() - issue.created_at).days >= 30
             and not any(label.name.lower() in LABELS_TO_EXEMPT for label in issue.get_labels())
         ):
-            print(f"Would add stale comment to {issue.number}")
-            # issue.create_comment(
-            #     "This issue has been automatically marked as stale because it has not had "
-            #     "recent activity. If you think this still needs to be addressed "
-            #     "please comment on this thread.\n\nPlease note that issues that do not follow the "
-            #     "[contributing guidelines](https://github.com/huggingface/transformers/blob/master/CONTRIBUTING.md) "
-            #     "are likely to be ignored."
-            # )
+            # print(f"Would add stale comment to {issue.number}")
+            issue.create_comment(
+                "This issue has been automatically marked as stale because it has not had "
+                "recent activity. If you think this still needs to be addressed "
+                "please comment on this thread.\n\nPlease note that issues that do not follow the "
+                "[contributing guidelines](https://github.com/huggingface/transformers/blob/master/CONTRIBUTING.md) "
+                "are likely to be ignored."
+            )
 
 
 if __name__ == "__main__":
