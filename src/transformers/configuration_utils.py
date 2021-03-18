da39d64a34eb1bd9e762978556d7164283ef2bda
Vimarsh Chaturvedi <vimarsh.c@gmail.com>
2021-03-18 22:21:42 +0530

    from_pretrained: check that the pretrained model is for the right model architecture (#10586)
    * Added check to ensure model name passed to from_pretrained and model are the same
    
    * Added test to check from_pretrained throws assert error when passed an incompatiable model name
    
    * Modified assert in from_pretrained with f-strings. Modified test to ensure desired assert message is being generated
    
    * Added check to ensure config and model has model_type
    
    * Fix FlauBERT heads
    
    Co-authored-by: vimarsh chaturvedi <vimarsh chaturvedi>
    Co-authored-by: Stas Bekman <stas@stason.org>
    Co-authored-by: Lysandre <lysandre.debut@reseau.eseo.fr>

--- src/transformers/configuration_utils.py
+++ src/transformers/configuration_utils.py
@@ -384,6 +384,11 @@ class PretrainedConfig(object):
 
         """
         config_dict, kwargs = cls.get_config_dict(pretrained_model_name_or_path, **kwargs)
+        if config_dict.get("model_type", False) and hasattr(cls, "model_type"):
+            assert (
+                config_dict["model_type"] == cls.model_type
+            ), f"You tried to initiate a model of type '{cls.model_type}' with a pretrained model of type '{config_dict['model_type']}'"
+
         return cls.from_dict(config_dict, **kwargs)
 
     @classmethod
