4785781f23bdd654f083f2924bfe1bf484ebac8a
Funtowicz Morgan <mfuntowicz@users.noreply.github.com>
2021-03-18 01:25:47 +0100

    Add support for detecting intel-tensorflow version (#10781)
    Signed-off-by: Morgan Funtowicz <funtowiczmo@gmail.com>

--- src/transformers/file_utils.py
+++ src/transformers/file_utils.py
@@ -102,8 +102,12 @@ if USE_TF in ENV_VARS_TRUE_AND_AUTO_VALUES and USE_TORCH not in ENV_VARS_TRUE_VA
                             try:
                                 _tf_version = importlib_metadata.version("tf-nightly-gpu")
                             except importlib_metadata.PackageNotFoundError:
-                                _tf_version = None
-                                _tf_available = False
+                                # Support for intel-tensorflow version
+                                try:
+                                    _tf_version = importlib_metadata.version("intel-tensorflow")
+                                except importlib_metadata.PackageNotFoundError:
+                                    _tf_version = None
+                                    _tf_available = False
     if _tf_available:
         if version.parse(_tf_version) < version.parse("2"):
             logger.info(f"TensorFlow found but with version {_tf_version}. Transformers requires version 2 minimum.")
