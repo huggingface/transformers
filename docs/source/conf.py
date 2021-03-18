9f43a425fe89cfc0e9b9aa7abd7dd44bcaccd79a
Sylvain Gugger <sylvain.gugger@gmail.com>
2021-03-18 15:09:04 -0400

    Release: v4.4.2

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,8 @@ author = u'huggingface'
 # The short X.Y version
 version = u''
 # The full version, including alpha/beta/rc tags
-release = u'4.4.1'
+release = u'4.4.2'
+
 
 
 # Prefix link to point to master, comment this during version release and uncomment below line
