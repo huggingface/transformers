ffb1ac2d81afef31810b251b0828b003ad8e2543
shubhendra <withshubh@gmail.com>
2021-03-22 13:10:24 +0530

    Refactor the comparison involving `not`
    Signed-off-by: shubhendra <withshubh@gmail.com>

--- src/transformers/optimization.py
+++ src/transformers/optimization.py
@@ -301,7 +301,7 @@ class AdamW(Optimizer):
             raise ValueError("Invalid beta parameter: {} - should be in [0.0, 1.0[".format(betas[0]))
         if not 0.0 <= betas[1] < 1.0:
             raise ValueError("Invalid beta parameter: {} - should be in [0.0, 1.0[".format(betas[1]))
-        if not 0.0 <= eps:
+        if 0.0 > eps:
             raise ValueError("Invalid epsilon value: {} - should be >= 0.0".format(eps))
         defaults = dict(lr=lr, betas=betas, eps=eps, weight_decay=weight_decay, correct_bias=correct_bias)
         super().__init__(params, defaults)
