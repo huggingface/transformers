1a7bf5d1aae24d350a185f8274a85b5532651eaf
Bhadresh Savani <bhadreshpsavani@gmail.com>
2021-03-23 22:53:29 +0530

    removed extra line
    Co-authored-by: Stas Bekman <stas00@users.noreply.github.com>

--- examples/text-classification/run_xnli.py
+++ examples/text-classification/run_xnli.py
@@ -377,7 +377,6 @@ def main():
         output_test_file = os.path.join(training_args.output_dir, "test_predictions.txt")
         if trainer.is_world_process_zero():
             with open(output_test_file, "w") as writer:
-                logger.info("***** Test results *****")
                 writer.write("index\tprediction\n")
                 for index, item in enumerate(predictions):
                     item = label_list[item]
