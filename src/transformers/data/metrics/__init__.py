2440095b6d7ab4e5cf34ed79f3152185c71ffa3f
skysnow <632157545@qq.com>
2021-03-23 15:59:25 +0800

    Update __init__.py

--- src/transformers/data/metrics/__init__.py
+++ src/transformers/data/metrics/__init__.py
@@ -88,6 +88,8 @@ def glue_compute_metrics(task_name, preds, labels):
         return {"acc": simple_accuracy(preds, labels)}
     elif task_name == "hans":
         return {"acc": simple_accuracy(preds, labels)}
+    elif task_name == "material":
+        return acc_and_f1(preds, labels)
     else:
         raise KeyError(task_name)
 
