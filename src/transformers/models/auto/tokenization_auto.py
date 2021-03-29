22ccf1491f32e7ab72489e813274c15977ac245b
Masatoshi Suzuki <sappukei.suzuki@gmail.com>
2021-03-29 23:26:15 +0900

    Ignore not initialized NO_CONFIG_TOKENIZERs (#10936)

--- src/transformers/models/auto/tokenization_auto.py
+++ src/transformers/models/auto/tokenization_auto.py
@@ -288,7 +288,7 @@ def tokenizer_class_from_name(class_name: str):
     all_tokenizer_classes = (
         [v[0] for v in TOKENIZER_MAPPING.values() if v[0] is not None]
         + [v[1] for v in TOKENIZER_MAPPING.values() if v[1] is not None]
-        + NO_CONFIG_TOKENIZER
+        + [v for v in NO_CONFIG_TOKENIZER if v is not None]
     )
     for c in all_tokenizer_classes:
         if c.__name__ == class_name:
