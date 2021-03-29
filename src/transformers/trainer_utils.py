8df5ad2f16559272d81aed4e84a0e9f2bb9d70a4
Stas Bekman <stas@stason.org>
2021-03-29 11:07:43 -0700

    restore

--- src/transformers/trainer_utils.py
+++ src/transformers/trainer_utils.py
@@ -239,7 +239,7 @@ def speed_metrics(split, start_time, num_samples=None):
     - num_samples: number of samples processed
     """
     runtime = time.time() - start_time
-    result = {f"{split}_runtime": runtime}
+    result = {f"{split}_runtime": round(runtime, 4)}
     if num_samples is not None:
         samples_per_second = 1 / (runtime / num_samples)
         result[f"{split}_samples_per_second"] = round(samples_per_second, 3)
