0d8d2bcac1c101adb674e0032bde1ffbf387ade3
patil-suraj <surajp815@gmail.com>
2021-03-29 12:51:11 +0530

    init hf config from mesh-tf config json file

--- src/transformers/models/gpt_neo/convert_gpt_neo_mesh_tf_to_pytorch.py
+++ src/transformers/models/gpt_neo/convert_gpt_neo_mesh_tf_to_pytorch.py
@@ -16,6 +16,7 @@
 
 
 import argparse
+import json
 
 from transformers import GPTNeoConfig, GPTNeoForCausalLM, load_tf_weights_in_gpt_neo
 from transformers.utils import logging
@@ -26,7 +27,17 @@ logging.set_verbosity_info()
 
 def convert_tf_checkpoint_to_pytorch(tf_checkpoint_path, config_file, pytorch_dump_path):
     # Initialise PyTorch model
-    config = GPTNeoConfig.from_json_file(config_file)
+    config_json = json.load(open(config_file, "r"))
+    config = GPTNeoConfig(
+        hidden_size=config_json["n_embd"],
+        num_layers=config_json["n_layer"],
+        num_heads=config_json["n_head"],
+        attention_types=config_json["attention_types"],
+        max_position_embeddings=config_json["n_ctx"],
+        resid_dropout=config_json["res_dropout"],
+        embed_dropout=config_json["embed_dropout"],
+        attention_dropout=config_json["attn_dropout"],
+    )
     print("Building PyTorch model from configuration: {}".format(str(config)))
     model = GPTNeoForCausalLM(config)
 
@@ -49,7 +60,7 @@ if __name__ == "__main__":
         default=None,
         type=str,
         required=True,
-        help="The config json file corresponding to the pre-trained T5 model. \n"
+        help="The config json file corresponding to the pre-trained mesh-tf model. \n"
         "This specifies the model architecture.",
     )
     parser.add_argument(
