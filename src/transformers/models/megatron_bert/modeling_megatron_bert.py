934bc8d4b09160507068201867dc4020233a7e4c
Julien Demouth <julien.demouth@gmail.com>
2021-04-01 17:57:23 +0200

    Update src/transformers/models/megatron_bert/modeling_megatron_bert.py
    Co-authored-by: Sylvain Gugger <35901082+sgugger@users.noreply.github.com>

--- src/transformers/models/megatron_bert/modeling_megatron_bert.py
+++ src/transformers/models/megatron_bert/modeling_megatron_bert.py
@@ -1750,7 +1750,7 @@ class MegatronBertForQuestionAnswering(MegatronBertPreTrainedModel):
     @add_start_docstrings_to_model_forward(MEGATRON_BERT_INPUTS_DOCSTRING.format("batch_size, sequence_length"))
     @add_code_sample_docstrings(
         tokenizer_class=_TOKENIZER_FOR_DOC,
-        checkpoint="nvidia/megatron-bert",
+        checkpoint=_CHECKPOINT_FOR_DOC,
         output_type=QuestionAnsweringModelOutput,
         config_class=_CONFIG_FOR_DOC,
     )
