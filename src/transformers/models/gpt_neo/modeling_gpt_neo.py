a58ae40eb2cc4c4aa52b5d6469a79209c6271717
Leo Gao <54557097+leogao2@users.noreply.github.com>
2021-04-05 23:31:42 -0600

    Switch to config.vocab_size

--- src/transformers/models/gpt_neo/modeling_gpt_neo.py
+++ src/transformers/models/gpt_neo/modeling_gpt_neo.py
@@ -114,7 +114,7 @@ def load_tf_weights_in_gpt_neo(model, config, gpt_neo_checkpoint_path):
 
         if name == ["wte"]:
             # if vocab is padded, then trim off the padding embeddings
-            array = array[:50257]
+            array = array[:config.vocab_size]
 
         try:
             assert (
