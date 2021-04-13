536233b187147e4f4ba5ac20636308ad46d3e0f3
Philipp Schmid <32632186+philschmid@users.noreply.github.com>
2021-04-13 18:35:18 +0200

    added cache_dir=model_args.cache_dir to all example with cache_dir arg (#11220)

--- examples/multiple-choice/run_swag.py
+++ examples/multiple-choice/run_swag.py
@@ -268,10 +268,10 @@ def main():
         if data_args.validation_file is not None:
             data_files["validation"] = data_args.validation_file
         extension = data_args.train_file.split(".")[-1]
-        datasets = load_dataset(extension, data_files=data_files)
+        datasets = load_dataset(extension, data_files=data_files, cache_dir=model_args.cache_dir)
     else:
         # Downloading and loading the swag dataset from the hub.
-        datasets = load_dataset("swag", "regular")
+        datasets = load_dataset("swag", "regular", cache_dir=model_args.cache_dir)
     # See more about loading any type of standard or custom dataset (from files, python dict, pandas DataFrame, etc) at
     # https://huggingface.co/docs/datasets/loading_datasets.html.
 
