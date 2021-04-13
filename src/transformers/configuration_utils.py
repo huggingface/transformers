0054d2d3f6fcb3b5b297ae5b71d85fd592d7820d
Sylvain Gugger <35901082+sgugger@users.noreply.github.com>
2021-04-13 10:33:52 -0400

    Replace error by warning when loading an architecture in another (#11207)
    * Replace error by warning when loading an architecture in another
    
    * Style
    
    * Style again
    
    * Add a test
    
    * Adapt old test

--- src/transformers/configuration_utils.py
+++ src/transformers/configuration_utils.py
@@ -399,10 +399,11 @@ class PretrainedConfig(object):
 
         """
         config_dict, kwargs = cls.get_config_dict(pretrained_model_name_or_path, **kwargs)
-        if config_dict.get("model_type", False) and hasattr(cls, "model_type"):
-            assert (
-                config_dict["model_type"] == cls.model_type
-            ), f"You tried to initiate a model of type '{cls.model_type}' with a pretrained model of type '{config_dict['model_type']}'"
+        if "model_type" in config_dict and hasattr(cls, "model_type") and config_dict["model_type"] != cls.model_type:
+            logger.warn(
+                f"You are using a model of type {config_dict['model_type']} to instantiate a model of type "
+                f"{cls.model_type}. This is not supported for all configurations of models and can yield errors."
+            )
 
         return cls.from_dict(config_dict, **kwargs)
 
