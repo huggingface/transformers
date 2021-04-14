926981d7f5b680926dd156d7495f0438ed4f426d
Sylvain Gugger <35901082+sgugger@users.noreply.github.com>
2021-04-14 11:47:54 -0400

    Fix #10128 (#11248)

--- src/transformers/trainer_pt_utils.py
+++ src/transformers/trainer_pt_utils.py
@@ -72,7 +72,7 @@ def torch_pad_and_concatenate(tensor1, tensor2, padding_index=-100):
 def numpy_pad_and_concatenate(array1, array2, padding_index=-100):
     """Concatenates `array1` and `array2` on first axis, applying padding on the second if necessary."""
     if len(array1.shape) == 1 or array1.shape[1] == array2.shape[1]:
-        return np.concatenate((array1, array2), dim=0)
+        return np.concatenate((array1, array2), axis=0)
 
     # Let's figure out the new shape
     new_shape = (array1.shape[0] + array2.shape[0], max(array1.shape[1], array2.shape[1])) + array1.shape[2:]
