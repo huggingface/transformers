4f280897a6511d09485465273836c7c0c7eb43ed
Sylvain Gugger <sylvain.gugger@gmail.com>
2021-04-21 19:12:58 -0400

    Add huggingface_hub dep for #11328

--- setup.py
+++ setup.py
@@ -99,6 +99,7 @@ _deps = [
     "flake8>=3.8.3",
     "flax>=0.3.2",
     "fugashi>=1.0",
+    "huggingface-hub>=0.0.8",
     "importlib_metadata",
     "ipadic>=1.0.0,<2.0",
     "isort>=5.5.4",
@@ -306,6 +307,7 @@ install_requires = [
     deps["dataclasses"] + ";python_version<'3.7'",  # dataclasses for Python versions that don't have it
     deps["importlib_metadata"] + ";python_version<'3.8'",  # importlib_metadata for Python versions that don't have it
     deps["filelock"],  # filesystem locks, e.g., to prevent parallel downloads
+    deps["huggingface-hub"],
     deps["numpy"],
     deps["packaging"],  # utilities from PyPA to e.g., compare versions
     deps["regex"],  # for OpenAI GPT
