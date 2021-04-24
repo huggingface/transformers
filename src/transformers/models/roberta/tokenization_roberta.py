ddc5f3dcaeffee91a6c7077c05e5b59337f4d505
cronoik <johannes.schaffrath@mail.de>
2021-04-24 02:00:42 +0200

    documentation linked to the parent class PreTrainedTokenizerFast but it should be the slow tokenizer

--- src/transformers/models/roberta/tokenization_roberta.py
+++ src/transformers/models/roberta/tokenization_roberta.py
@@ -81,7 +81,7 @@ class RobertaTokenizer(GPT2Tokenizer):
         When used with ``is_split_into_words=True``, this tokenizer will add a space before each word (even the first
         one).
 
-    This tokenizer inherits from :class:`~transformers.PreTrainedTokenizerFast` which contains most of the main
+    This tokenizer inherits from :class:`~transformers.PreTrainedTokenizer` which contains most of the main
     methods. Users should refer to this superclass for more information regarding those methods.
 
     Args:
