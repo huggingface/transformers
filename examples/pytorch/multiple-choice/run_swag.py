1d30ec95c7a3656972a394d28a86efdb6ca4cdab
Bhadresh Savani <bhadreshpsavani@gmail.com>
2021-04-26 17:24:31 +0100

    [Examples] Fixes inconsistency around eval vs val and predict vs test (#11380)
    * added changes for uniformity
    
    * modified files
    
    * corrected typo
    
    * fixed qa scripts
    
    * fix typos
    
    * fixed predict typo in qa no trainer
    
    * fixed test file
    
    * reverted trainer changes
    
    * reverted trainer changes in custom exmaples
    
    * updated readme
    
    * added changes in deepspeed test
    
    * added changes for predict and eval

--- examples/pytorch/multiple-choice/run_swag.py
+++ examples/pytorch/multiple-choice/run_swag.py
@@ -127,10 +127,10 @@ class DataTrainingArguments:
             "value if set."
         },
     )
-    max_val_samples: Optional[int] = field(
+    max_eval_samples: Optional[int] = field(
         default=None,
         metadata={
-            "help": "For debugging purposes or quicker training, truncate the number of validation examples to this "
+            "help": "For debugging purposes or quicker training, truncate the number of evaluation examples to this "
             "value if set."
         },
     )
@@ -363,8 +363,8 @@ def main():
         if "validation" not in datasets:
             raise ValueError("--do_eval requires a validation dataset")
         eval_dataset = datasets["validation"]
-        if data_args.max_val_samples is not None:
-            eval_dataset = eval_dataset.select(range(data_args.max_val_samples))
+        if data_args.max_eval_samples is not None:
+            eval_dataset = eval_dataset.select(range(data_args.max_eval_samples))
         eval_dataset = eval_dataset.map(
             preprocess_function,
             batched=True,
@@ -422,8 +422,8 @@ def main():
         logger.info("*** Evaluate ***")
 
         metrics = trainer.evaluate()
-        max_val_samples = data_args.max_val_samples if data_args.max_val_samples is not None else len(eval_dataset)
-        metrics["eval_samples"] = min(max_val_samples, len(eval_dataset))
+        max_eval_samples = data_args.max_eval_samples if data_args.max_eval_samples is not None else len(eval_dataset)
+        metrics["eval_samples"] = min(max_eval_samples, len(eval_dataset))
 
         trainer.log_metrics("eval", metrics)
         trainer.save_metrics("eval", metrics)
