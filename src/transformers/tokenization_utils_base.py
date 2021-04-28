7da43c1c6a393df5c23433d1d266673cbf69bd5e
Sylvain Gugger <sylvain.gugger@gmail.com>
2021-04-28 15:33:25 -0400

    Reformat to make code clearer

--- src/transformers/tokenization_utils_base.py
+++ src/transformers/tokenization_utils_base.py
@@ -2238,13 +2238,18 @@ class PreTrainedTokenizerBase(SpecialTokensMixin, PushToHubMixin):
         # Input type checking for clearer error
         def _is_valid_text_input(t):
             if isinstance(t, str):
+                # Strings are fine
                 return True
             elif isinstance(t, (list, tuple)):
+                # List are fine as long as they are...
                 if len(t) == 0:
+                    # ... empty
                     return True
                 elif isinstance(t[0], str):
+                    # ... list of strings
                     return True
                 elif isinstance(t[0], (list, tuple)):
+                    # ... list with an empty list or with a list of strings
                     return len(t[0]) == 0 or isinstance(t[0][0], str)
                 else:
                     return False
