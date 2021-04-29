4074384177fb60ded88d00585163ca9a76a9b165
Michael Benayoun <michael@huggingface.co>
2021-04-29 18:07:56 +0200

    solved coefficient issue for the TF version of gelu_fast

--- src/transformers/activations_tf.py
+++ src/transformers/activations_tf.py
@@ -57,8 +57,8 @@ def mish(x):
 
 def gelu_fast(x):
     x = tf.convert_to_tensor(x)
-    coeff1 = tf.cast(0.7978845608, x.dtype)
-    coeff2 = tf.cast(0.044715, x.dtype)
+    coeff1 = tf.cast(0.044715, x.dtype)
+    coeff2 = tf.cast(0.7978845608, x.dtype)
 
     return 0.5 * x * (1.0 + tf.tanh(x * coeff2 * (1.0 + coeff1 * x * x)))
 
