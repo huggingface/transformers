aace407ae02001e1d40beb543670ed18ed438952
Niels Rogge <niels.rogge1@gmail.com>
2021-05-03 09:25:25 +0200

    Add interpolation of patch embeddings

--- src/transformers/models/vit/modeling_vit.py
+++ src/transformers/models/vit/modeling_vit.py
@@ -92,18 +92,43 @@ class ViTEmbeddings(nn.Module):
         pos_embed = pos_embed.permute(0, 2, 3, 1).view(1, -1, dim)
         return torch.cat((class_emb.unsqueeze(0), pos_embed), dim=1)
     
-    def forward(self, pixel_values):
+    def forward(self, x, output_attentions):
         
-        batch_size = pixel_values.shape[0]
-        embeddings = self.patch_embeddings(pixel_values)
+        batch_size = x.shape[0]
+
+        if output_attentions:
+            N = self.position_embeddings.shape[1] - 1
+            x = self.patch_embeddings(x)
+
+            # interpolate patch embeddings
+            dim = x.shape[-1]
+            w0 = w // self.patch_embeddings.patch_size
+            h0 = h // self.patch_embeddings.patch_size
+            class_pos_embed = self.position_embeddings[:, 0]
+            patch_pos_embed = self.position_embeddings[:, 1:]
+            patch_pos_embed = nn.functional.interpolate(
+                patch_pos_embed.reshape(1, int(math.sqrt(N)), int(math.sqrt(N)), dim).permute(0, 3, 1, 2),
+                scale_factor=(w0 / math.sqrt(N), h0 / math.sqrt(N)),
+                mode='bicubic',
+            )
+            if w0 != patch_pos_embed.shape[-2]:
+                helper = torch.zeros(h0)[None, None, None, :].repeat(1, dim, w0 - patch_pos_embed.shape[-2], 1).to(x.device)
+                patch_pos_embed = torch.cat((patch_pos_embed, helper), dim=-2)
+            if h0 != patch_pos_embed.shape[-1]:
+                helper = torch.zeros(w0)[None, None, :, None].repeat(1, dim, 1, h0 - patch_pos_embed.shape[-1]).to(x.device)
+                pos_embed = torch.cat((patch_pos_embed, helper), dim=-1)
+            patch_pos_embed = patch_pos_embed.permute(0, 2, 3, 1).view(1, -1, dim)
+            pos_embed = torch.cat((class_pos_embed.unsqueeze(0), patch_pos_embed), dim=1)
+        else:
+            embeddings = self.patch_embeddings(x)
 
         cls_tokens = self.cls_token.expand(batch_size, -1, -1)
-        embeddings = torch.cat((cls_tokens, embeddings), dim=1)
+        x = torch.cat((cls_tokens, x), dim=1)
 
-        position_embeddings = self.interpolate_pos_encoding(embeddings, self.position_embeddings)
-        embeddings = embeddings + position_embeddings
-        embeddings = self.dropout(embeddings)
-        return embeddings
+        position_embeddings = self.interpolate_pos_encoding(x, self.position_embeddings)
+        x = x + position_embeddings
+        x = self.dropout(x)
+        return x
 
 
 # Based on timm implementation, which can be found here:
@@ -517,7 +542,7 @@ class ViTModel(ViTPreTrainedModel):
         # and head_mask is converted to shape [num_hidden_layers x batch x num_heads x seq_length x seq_length]
         head_mask = self.get_head_mask(head_mask, self.config.num_hidden_layers)
         
-        embedding_output = self.embeddings(pixel_values)
+        embedding_output = self.embeddings(pixel_values, output_attentions)
 
         encoder_outputs = self.encoder(
             embedding_output,
