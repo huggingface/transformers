49b58f8dd4379dc6f046196b8a603c17eb3d5031
Patrick von Platen <patrick.v.platen@gmail.com>
2021-05-04 19:57:59 +0200

    [FlaxRoberta] Add FlaxRobertaModels & adapt run_mlm_flax.py (#11470)
    * add flax roberta
    
    * make style
    
    * correct initialiazation
    
    * modify model to save weights
    
    * fix copied from
    
    * fix copied from
    
    * correct some more code
    
    * add more roberta models
    
    * Apply suggestions from code review
    
    * merge from master
    
    * finish
    
    * finish docs
    
    Co-authored-by: Patrick von Platen <patrick@huggingface.co>

--- src/transformers/utils/dummy_flax_objects.py
+++ src/transformers/utils/dummy_flax_objects.py
@@ -180,7 +180,61 @@ class FlaxBertPreTrainedModel:
         requires_backends(self, ["flax"])
 
 
+class FlaxRobertaForMaskedLM:
+    def __init__(self, *args, **kwargs):
+        requires_backends(self, ["flax"])
+
+    @classmethod
+    def from_pretrained(self, *args, **kwargs):
+        requires_backends(self, ["flax"])
+
+
+class FlaxRobertaForMultipleChoice:
+    def __init__(self, *args, **kwargs):
+        requires_backends(self, ["flax"])
+
+    @classmethod
+    def from_pretrained(self, *args, **kwargs):
+        requires_backends(self, ["flax"])
+
+
+class FlaxRobertaForQuestionAnswering:
+    def __init__(self, *args, **kwargs):
+        requires_backends(self, ["flax"])
+
+    @classmethod
+    def from_pretrained(self, *args, **kwargs):
+        requires_backends(self, ["flax"])
+
+
+class FlaxRobertaForSequenceClassification:
+    def __init__(self, *args, **kwargs):
+        requires_backends(self, ["flax"])
+
+    @classmethod
+    def from_pretrained(self, *args, **kwargs):
+        requires_backends(self, ["flax"])
+
+
+class FlaxRobertaForTokenClassification:
+    def __init__(self, *args, **kwargs):
+        requires_backends(self, ["flax"])
+
+    @classmethod
+    def from_pretrained(self, *args, **kwargs):
+        requires_backends(self, ["flax"])
+
+
 class FlaxRobertaModel:
+    def __init__(self, *args, **kwargs):
+        requires_backends(self, ["flax"])
+
+    @classmethod
+    def from_pretrained(self, *args, **kwargs):
+        requires_backends(self, ["flax"])
+
+
+class FlaxRobertaPreTrainedModel:
     def __init__(self, *args, **kwargs):
         requires_backends(self, ["flax"])
 
