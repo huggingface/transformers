c1780ce7a487162f44d74cb705b46ff42e7dfe0c
baeseongsu <subae94@gmail.com>
2021-05-06 15:18:02 +0900

    fix head_mask for albert encoder part(`AlbertTransformer`) (#11596)
    * fix head mask for albert encoder part
    
    * fix head_mask for albert encoder part

--- src/transformers/models/albert/modeling_albert.py
+++ src/transformers/models/albert/modeling_albert.py
@@ -450,6 +450,8 @@ class AlbertTransformer(nn.Module):
         all_hidden_states = (hidden_states,) if output_hidden_states else None
         all_attentions = () if output_attentions else None
 
+        head_mask = [None] * self.config.num_hidden_layers if head_mask is None else head_mask
+
         for i in range(self.config.num_hidden_layers):
             # Number of layers in a hidden group
             layers_per_group = int(self.config.num_hidden_layers / self.config.num_hidden_groups)
