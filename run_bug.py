5c0f49b1fe78fe5147dd1778250ec8b56cf94241
Patrick von Platen <patrick.v.platen@gmail.com>
2021-05-13 10:27:43 +0100

    del run bug

--- run_bug.py
+++
@@ -1,6 +0,0 @@
-from transformers import FlaxAutoModelForSequenceClassification, AutoConfig
-
-model_checkpoint = "bert-base-cased"
-
-config = AutoConfig.from_pretrained(model_checkpoint, num_labels=3, return_dict=False)
-model = FlaxAutoModelForSequenceClassification.from_pretrained(model_checkpoint, config=config)
