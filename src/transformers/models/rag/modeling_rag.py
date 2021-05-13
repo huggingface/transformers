ce9c207bbe23bb866d9185a79628c15ac08b39c1
Shamane Siriwardana <shamane@ahlab.org>
2021-05-13 23:57:40 +1200

    Update src/transformers/models/rag/modeling_rag.py
    Co-authored-by: Patrick von Platen <patrick.v.platen@gmail.com>

--- src/transformers/models/rag/modeling_rag.py
+++ src/transformers/models/rag/modeling_rag.py
@@ -738,7 +738,7 @@ class RagSequenceForGeneration(RagPreTrainedModel):
     def set_retriever(self, retriever: RagRetriever):
         self.rag.retriever = retriever
 
-    def set_context_encoder_for_training(self,ctx_encoder):
+    def set_context_encoder_for_training(self, ctx_encoder: PreTrainedModel):
         #used in end2end retriever training
         self.rag.context_encoder_training=True
         self.rag.ctx_encoder = ctx_encoder
