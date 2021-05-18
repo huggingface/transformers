91672d6b0f9207301d9669412e28670ef518761c
Gunjan Chhablani <chhablani.gunjan@gmail.com>
2021-05-18 20:07:31 +0530

    Update src/transformers/models/visual_bert/modeling_visual_bert.py
    Co-authored-by: Sylvain Gugger <35901082+sgugger@users.noreply.github.com>

--- src/transformers/models/visual_bert/modeling_visual_bert.py
+++ src/transformers/models/visual_bert/modeling_visual_bert.py
@@ -1215,10 +1215,10 @@ class VisualBertForQuestionAnswering(VisualBertPreTrainedModel):
             >>> visual_attention_mask = torch.ones(visual_embeds.shape[:-1], dtype=torch.float)
 
             >>> inputs.update({{
-                "visual_embeds": visual_embeds,
-                "visual_token_type_ids": visual_token_type_ids,
-                "visual_attention_mask": visual_attention_mask
-            }})
+            ...     "visual_embeds": visual_embeds,
+            ...     "visual_token_type_ids": visual_token_type_ids,
+            ...     "visual_attention_mask": visual_attention_mask
+            ... }})
 
             >>> labels = torch.tensor([[0.0,1.0]]).unsqueeze(0)  # Batch size 1, Num labels 2
 
