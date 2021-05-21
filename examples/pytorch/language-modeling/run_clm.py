b0f62cca307dba25d27477c56fc7102efe606311
Stas Bekman <stas@stason.org>
2021-05-21 09:02:43 -0700

    style

--- examples/pytorch/language-modeling/run_clm.py
+++ examples/pytorch/language-modeling/run_clm.py
@@ -230,7 +230,9 @@ def main():
     # download the dataset.
     if data_args.dataset_name is not None:
         # Downloading and loading a dataset from the hub.
-        datasets = load_dataset(data_args.dataset_name, data_args.dataset_config_name, cache_dir=model_args.cache_dir, keep_in_memory=False)
+        datasets = load_dataset(
+            data_args.dataset_name, data_args.dataset_config_name, cache_dir=model_args.cache_dir, keep_in_memory=False
+        )
         if "validation" not in datasets.keys():
             datasets["validation"] = load_dataset(
                 data_args.dataset_name,
