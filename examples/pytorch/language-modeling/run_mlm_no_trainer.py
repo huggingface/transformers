fix overflow in perplexity calc

examples/pytorch/language-modeling/run_mlm_no_trainer.py
@@ -486,7 +486,10 @@ def main():
 
         losses = torch.cat(losses)
         losses = losses[: len(eval_dataset)]
-        perplexity = math.exp(torch.mean(losses))
+        try:
+            perplexity = math.exp(metrics["eval_loss"])
+        except OverflowError:
+            perplexity = float("nan")
 
         logger.info(f"epoch {epoch}: perplexity: {perplexity}")
 