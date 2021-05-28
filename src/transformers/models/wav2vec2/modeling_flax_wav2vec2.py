81f2f94f917d197425c8a18886b56052a30e86cc
bhadreshpsavani <bhadreshpsavani@gmail.com>
2021-05-28 23:06:02 +0530

    added attention class

--- src/transformers/models/wav2vec2/modeling_flax_wav2vec2.py
+++ src/transformers/models/wav2vec2/modeling_flax_wav2vec2.py
@@ -25,7 +25,8 @@ import jax
 import jax.numpy as jnp
 import jaxlib.xla_extension as jax_xla
 from flax.core.frozen_dict import FrozenDict
-from flax.linen import dot_product_attention
+from flax.linen import combine_masks, dot_product_attention, make_causal_mask
+from flax.linen.attention import dot_product_attention_weights
 from jax import config, lax
 from jax.dtypes import dtype
 from jax.random import PRNGKey
@@ -54,7 +55,7 @@ from .configuration_wav2vec2 import Wav2Vec2Config
 logger = logging.get_logger(__name__)
 
 
-class Wav2Vec2NoLayerNormConvLayer(nn.Module):
+class FlaxWav2Vec2NoLayerNormConvLayer(nn.Module):
     config: Wav2Vec2Config
     layer_id: int = 0
     dtype: jnp.dtype = jnp.float32
@@ -79,7 +80,7 @@ class Wav2Vec2NoLayerNormConvLayer(nn.Module):
         return hidden_states
 
 
-class Wav2Vec2LayerNormConvLayer(nn.Module):
+class FlaxWav2Vec2LayerNormConvLayer(nn.Module):
     config: Wav2Vec2Config
     layer_id: int = 0
     dtype: jnp.dtype = jnp.float32
@@ -109,7 +110,7 @@ class Wav2Vec2LayerNormConvLayer(nn.Module):
         return hidden_states
 
 
-class Wav2Vec2GroupNormConvLayer(nn.Module):
+class FlaxWav2Vec2GroupNormConvLayer(nn.Module):
     config: Wav2Vec2Config
     layer_id: int = 0
     dtype: jnp.dtype = jnp.float32
@@ -137,7 +138,7 @@ class Wav2Vec2GroupNormConvLayer(nn.Module):
         return hidden_states
 
 
-class Wav2Vec2PositionalConvEmbedding(nn.Module):
+class FlaxWav2Vec2PositionalConvEmbedding(nn.Module):
     config: Wav2Vec2Config
     dtype: jnp.dtype = jnp.float32
 
@@ -165,20 +166,22 @@ class Wav2Vec2PositionalConvEmbedding(nn.Module):
         return hidden_states
 
 
-class Wav2Vec2FeatureExtractor(nn.Module):
+class FlaxWav2Vec2FeatureExtractor(nn.Module):
+    """Construct the featurs from raw audio waveform"""
+
     config: Wav2Vec2Config
     dtype: jnp.dtype = jnp.float32
 
     def setup(self):
         config = self.config
         if config.feat_extract_norm == "group":
-            self.conv_layers = [Wav2Vec2GroupNormConvLayer(config, layer_id=0, dtype=self.dtype)] + [
-                Wav2Vec2NoLayerNormConvLayer(config, layer_id=i + 1, dtype=self.dtype)
+            self.conv_layers = [FlaxWav2Vec2GroupNormConvLayer(config, layer_id=0, dtype=self.dtype)] + [
+                FlaxWav2Vec2NoLayerNormConvLayer(config, layer_id=i + 1, dtype=self.dtype)
                 for i in range(config.num_feat_extract_layers - 1)
             ]
         elif config.feat_extract_norm == "layer":
             self.conv_layers = [
-                Wav2Vec2LayerNormConvLayer(config, layer_id=i, dtype=self.dtype)
+                FlaxWav2Vec2LayerNormConvLayer(config, layer_id=i, dtype=self.dtype)
                 for i in range(config.num_feat_extract_layers)
             ]
         else:
@@ -193,45 +196,239 @@ class Wav2Vec2FeatureExtractor(nn.Module):
         return hidden_states
 
 
-class Wav2Vec2FeatureProjection(nn.Module):
+class FlaxWav2Vec2FeatureProjection(nn.Module):
+    config: Wav2Vec2Config
+    dtype: jnp.dtype = jnp.float32
+
+    def setup(self):
+        self.layer_norm = nn.LayerNorm(epsilon=self.config.layer_norm_eps, dtype=self.dtype)
+        self.projection = nn.Dense(
+            self.config.hidden_size,
+            kernel_init=jax.nn.initializers.normal(self.config.initializer_range, self.dtype),
+            dtype=self.dtype,
+        )
+        self.dropout = nn.Dropout(rate=self.config.feat_proj_dropout)
+
+    def __call__(self, hidden_states):
+        hidden_states = self.layer_norm(hidden_states)
+        hidden_states = self.projection(hidden_states)
+        hidden_states = self.dropout(hidden_states)
+        return hidden_states
+
+
+# Copied from transformers.models.bart.modeling_flax_bart.FlaxBartAttention with Bart->Wav2Vec2
+class FlaxWav2Vec2Attention(nn.Module):
+    config: Wav2Vec2Config
+    embed_dim: int
+    num_heads: int
+    dropout: float = 0.0
+    is_decoder: bool = False
+    causal: bool = False
+    bias: bool = True
+    dtype: jnp.dtype = jnp.float32  # the dtype of the computation
+
+    def setup(self) -> None:
+        self.head_dim = self.embed_dim // self.num_heads
+        assert (
+            self.head_dim * self.num_heads == self.embed_dim
+        ), f"embed_dim must be divisible by num_heads (got `embed_dim`: {self.embed_dim} and `num_heads`: {self.num_heads})."
+
+        self.k_proj = nn.Dense(
+            self.embed_dim,
+            use_bias=self.bias,
+            dtype=self.dtype,
+            kernel_init=jax.nn.initializers.normal(self.config.init_std, self.dtype),
+        )
+        self.v_proj = nn.Dense(
+            self.embed_dim,
+            use_bias=self.bias,
+            dtype=self.dtype,
+            kernel_init=jax.nn.initializers.normal(self.config.init_std, self.dtype),
+        )
+        self.q_proj = nn.Dense(
+            self.embed_dim,
+            use_bias=self.bias,
+            dtype=self.dtype,
+            kernel_init=jax.nn.initializers.normal(self.config.init_std, self.dtype),
+        )
+        self.out_proj = nn.Dense(
+            self.embed_dim,
+            use_bias=self.bias,
+            dtype=self.dtype,
+            kernel_init=jax.nn.initializers.normal(self.config.init_std, self.dtype),
+        )
+
+        self.dropout_layer = nn.Dropout(rate=self.dropout)
+
+        if self.causal:
+            self.causal_mask = make_causal_mask(
+                jnp.ones((1, self.config.max_position_embeddings), dtype="bool"), dtype="bool"
+            )
+
+    def _split_heads(self, hidden_states):
+        return hidden_states.reshape(hidden_states.shape[:2] + (self.num_heads, self.head_dim))
+
+    def _merge_heads(self, hidden_states):
+        return hidden_states.reshape(hidden_states.shape[:2] + (self.embed_dim,))
+
+    def __call__(
+        self,
+        hidden_states: jnp.ndarray,
+        key_value_states: Optional[jnp.ndarray] = None,
+        attention_mask: Optional[jnp.ndarray] = None,
+        layer_head_mask: Optional[jnp.ndarray] = None,
+        output_attentions: bool = False,
+        deterministic: bool = True,
+    ) -> Tuple[jnp.ndarray]:
+        """Input shape: Batch x Time x Channel"""
+
+        # if key_value_states are provided this layer is used as a cross-attention layer
+        # for the decoder
+        is_cross_attention = key_value_states is not None
+        batch_size, tgt_len, embed_dim = hidden_states.shape
+
+        # get query proj
+        query_states = self.q_proj(hidden_states)
+        # get key, value proj
+        if is_cross_attention:
+            # cross_attentions
+            key_states = self.k_proj(key_value_states)
+            value_states = self.v_proj(key_value_states)
+        else:
+            # self_attention
+            key_states = self.k_proj(hidden_states)
+            value_states = self.v_proj(hidden_states)
+
+        query_states = self._split_heads(query_states)
+        key_states = self._split_heads(key_states)
+        value_states = self._split_heads(value_states)
+
+        # prepare causal attention mask
+        if self.causal:
+            query_length, key_length = query_states.shape[1], key_states.shape[1]
+            causal_mask = self.causal_mask[:, :, :query_length, :key_length]
+            causal_mask = jnp.broadcast_to(causal_mask, (batch_size,) + causal_mask.shape[1:])
+
+        # combine masks if needed
+        if attention_mask is not None and self.causal:
+            attention_mask = jnp.broadcast_to(jnp.expand_dims(attention_mask, axis=(-3, -2)), causal_mask.shape)
+            attention_mask = combine_masks(attention_mask, causal_mask)
+        elif self.causal:
+            attention_mask = causal_mask
+        elif attention_mask is not None:
+            attention_mask = jnp.expand_dims(attention_mask, axis=(-3, -2))
+
+        # Convert the boolean attention mask to an attention bias.
+        if attention_mask is not None:
+            # attention mask in the form of attention bias
+            attention_bias = lax.select(
+                attention_mask > 0,
+                jnp.full(attention_mask.shape, 0.0).astype(self.dtype),
+                jnp.full(attention_mask.shape, float("-inf")).astype(self.dtype),
+            )
+        else:
+            attention_bias = None
+
+        dropout_rng = None
+        if not deterministic and self.dropout > 0.0:
+            dropout_rng = self.make_rng("dropout")
+
+        attn_weights = dot_product_attention_weights(
+            query_states,
+            key_states,
+            bias=attention_bias,
+            dropout_rng=dropout_rng,
+            dropout_rate=self.dropout,
+            broadcast_dropout=True,
+            deterministic=deterministic,
+            dtype=self.dtype,
+            precision=None,
+        )
+
+        # if layer_head_mask is not None:
+        #     assert layer_head_mask.shape == (
+        #         self.num_heads,
+        #     ), f"Head mask for a single layer should be of size {(self.num_heads,)}, but is {layer_head_mask.shape}"
+        #     attn_weights = layer_head_mask.reshape(1, -1, 1, 1) * attn_weights.reshape(
+        #         bsz, self.num_heads, tgt_len, src_len
+        #     )
+        #     attn_weights = attn_weights.reshape(bsz * self.num_heads, tgt_len, src_len)
+
+        attn_output = jnp.einsum("...hqk,...khd->...qhd", attn_weights, value_states)
+        attn_output = self._merge_heads(attn_output)
+        attn_output = self.out_proj(attn_output)
+
+        return attn_output, attn_weights
+
+
+class FlaxWav2Vec2FeedForward(nn.Module):
+    config: Wav2Vec2Config
+    dtype: jnp.dtype = jnp.float32
+
+    def setup(self):
+        self.intermediate_dropout = nn.Dropout(rate=self.config.activation_dropout)
+
+        self.intermediate_dense = nn.Dense(
+            self.config.intermediate_size,
+            kernel_init=jax.nn.initializers.normal(self.config.initializer_range, self.dtype),
+            dtype=self.dtype,
+        )
+        if isinstance(self.config.hidden_act, str):
+            self.intermediate_act_fn = ACT2FN[self.config.hidden_act]
+        else:
+            self.intermediate_act_fn = self.config.hidden_act
+
+        self.output_dense = nn.Dense(
+            self.config.hidden_size,
+            kernel_init=jax.nn.initializers.normal(self.config.initializer_range, self.dtype),
+            dtype=self.dtype,
+        )
+        self.output_dropout = nn.Dropout(rate=self.config.hidden_dropout)
+
+    def __call__(self, hidden_states):
+        hidden_states = self.intermediate_dense(hidden_states)
+        hidden_states = self.intermediate_act_fn(hidden_states)
+        hidden_states = self.intermediate_dropout(hidden_states)
+
+        hidden_states = self.output_dense(hidden_states)
+        hidden_states = self.output_dropout(hidden_states)
+        return hidden_states
+
+
+class FlaxWav2Vec2Output(nn.Module):
+    config: Wav2Vec2Config
+    dtype: jnp.dtype = jnp.float32
+
+    def setup(self):
+        config = self.config
+
+    def __call__(self, hidden_states, input_tensor):
+        return hidden_states
+
+
+class FlaxWav2Vec2EncoderLayer(nn.Module):
     pass
 
 
-class Wav2Vec2Attention(nn.Module):
+class FlaxWav2Vec2EncoderLayerStableLayerNorm(nn.Module):
     pass
 
 
-class Wav2Vec2FeedForward(nn.Module):
+class FlaxWav2Vec2Encoder(nn.Module):
     pass
 
 
-class Wav2Vec2Output(nn.Module):
+class FlaxWav2Vec2StableLayerNormEncoder(nn.Module):
     pass
 
 
-class Wav2Vec2EncoderLayer(nn.Module):
+class FlaxWav2Vec2PreTrainedModel(FlaxPreTrainedModel):
     pass
 
 
-class Wav2Vec2EncoderLayerStableLayerNorm(nn.Module):
+class FlaxWav2Vec2Model(FlaxWav2Vec2PreTrainedModel):
     pass
 
 
-class Wav2Vec2Encoder(nn.Module):
-    pass
-
-
-class Wav2Vec2StableLayerNormEncoder(nn.Module):
-    pass
-
-
-class Wav2Vec2PreTrainedModel(FlaxPreTrainedModel):
-    pass
-
-
-class Wav2Vec2Model(Wav2Vec2PreTrainedModel):
-    pass
-
-
-class Wav2Vec2ForCTC(Wav2Vec2PreTrainedModel):
+class FlaxWav2Vec2ForCTC(FlaxWav2Vec2PreTrainedModel):
     pass
