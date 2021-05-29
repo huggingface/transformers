1d5c4aaf99e9de84d23ee4665adf4c7ca6c391d6
monuminu <manoranjan.rajguru141@outlook.com>
2021-05-29 11:57:00 +0530

    https://github.com/huggingface/transformers/pull/11933

--- setup.py
+++ setup.py
@@ -149,7 +149,7 @@ _deps = [
     "unidic>=1.0.2",
     "unidic_lite>=1.0.7",
     "uvicorn",
-    "detectron2",
+    "git+https://github.com/facebookresearch/detectron2.git@v0.4#egg=detectron2",
 ]
 
 
