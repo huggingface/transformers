e02b5c8fc16515698be9af838681e476a857fb02
Suraj Patil <surajp815@gmail.com>
2021-06-01 09:44:31 +0530

    Add FlaxCLIP (#11883)
    * add flax CLIP
    
    * default input_shape
    
    * add tests
    
    * fix test
    
    * fix name
    
    * fix docs
    
    * fix shapes
    
    * attend at least 1 token
    
    * flax conv to torch conv
    
    * return floats
    
    * fix equivalence tests
    
    * fix import
    
    * return attention_weights and update tests
    
    * fix dosctrings
    
    * address patricks comments
    
    * input_shape arg
    
    * add tests for get_image_features and get_text_features methods
    
    * fix tests

--- src/transformers/modeling_flax_utils.py
+++ src/transformers/modeling_flax_utils.py
@@ -49,12 +49,17 @@ from .utils import logging
 logger = logging.get_logger(__name__)
 
 
+def quick_gelu(x):
+    return x * jax.nn.sigmoid(1.702 * x)
+
+
 ACT2FN = {
     "gelu": partial(nn.gelu, approximate=False),
     "relu": nn.relu,
     "silu": nn.swish,
     "swish": nn.swish,
     "gelu_new": partial(nn.gelu, approximate=True),
+    "quick_gelu": quick_gelu,
 }
 
 
