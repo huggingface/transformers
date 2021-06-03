880e8064f268d00bf3df655bb8d5890d305147ec
finetuneanon <82650881+finetuneanon@users.noreply.github.com>
2021-06-03 18:59:09 +0200

    Fix tail free sampling with batching

--- src/transformers/generation_logits_process.py
+++ src/transformers/generation_logits_process.py
@@ -203,21 +203,21 @@ class TailFreeSamplingLogitsWarper(LogitsWarper):
     def __call__(self, input_ids: torch.LongTensor, scores: torch.FloatTensor) -> torch.FloatTensor:
         sorted_logits, sorted_indices = torch.sort(scores, descending=True)
         d = sorted_logits.softmax(dim=-1)
-        d = d[..., 1:] - d[..., :-1]
-        d = d[..., 1:] - d[..., :-1]
+        d = d[:, 1:] - d[:, :-1]
+        d = d[:, 1:] - d[:, :-1]
         d = d.abs()
-        d = d / d.sum(dim=-1)
+        d = d / d.sum(dim=-1).view(1, -1).T
         cumulative_probs = d.cumsum(dim=-1)
 
         # Remove tokens with cumulative top_p above the threshold (token with 0 are kept)
         sorted_indices_to_remove = torch.zeros(sorted_indices.shape).bool().to(scores.device)
-        sorted_indices_to_remove[..., :-2] = (cumulative_probs > self.z)[..., :]
+        sorted_indices_to_remove[:, :-2] = (cumulative_probs > self.z)[:, :]
 
         # Shift the indices to the right to keep also the first token above the threshold
-        #sorted_indices_to_remove[..., 1:] = sorted_indices_to_remove[..., :-1].clone()
+        #sorted_indices_to_remove[:, 1:] = sorted_indices_to_remove[:, :-1].clone()
 
         # Always keep the first token
-        sorted_indices_to_remove[..., 0] = 0
+        sorted_indices_to_remove[:, 0] = 0
 
         # scatter sorted tensors to original indexing
         indices_to_remove = sorted_indices_to_remove.scatter(1, sorted_indices, sorted_indices_to_remove)
