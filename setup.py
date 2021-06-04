d8c82da9e8c49853c59f1479f9a6e3438ed5d182
monuminu <manoranjan.rajguru141@outlook.com>
2021-06-04 12:33:29 +0530

    fix for detectron2 installtion

--- setup.py
+++ setup.py
@@ -159,7 +159,7 @@ _deps = [
 #
 # some of the values are versioned whereas others aren't.
 deps = {b: a for a, b in (re.findall(r"^(([^!=<>]+)(?:[!=<>].*)?$)", x)[0] for x in _deps)}
-#deps["detectron2"] = "detectron2@git+https://github.com/facebookresearch/detectron2.git"
+deps["detectron2"] = "detectron2@git+https://github.com/facebookresearch/detectron2.git"
 
 # since we save this data in src/transformers/dependency_versions_table.py it can be easily accessed from
 # anywhere. If you need to quickly access the data from this table in a shell, you can do so easily with:
@@ -249,7 +249,7 @@ extras["integrations"] = extras["optuna"] + extras["ray"]
 
 extras["serving"] = deps_list("pydantic", "uvicorn", "fastapi", "starlette")
 extras["speech"] = deps_list("soundfile", "torchaudio")
-extras["vision"] = deps_list("Pillow")
+extras["vision"] = deps_list("Pillow","detectron2")
 
 extras["sentencepiece"] = deps_list("sentencepiece", "protobuf")
 extras["testing"] = (
@@ -309,7 +309,6 @@ extras["torchhub"] = deps_list(
     "torch",
     "tokenizers",
     "tqdm",
-    "detectron2@git+https://github.com/facebookresearch/detectron2.git"
 )
 
 # when modifying the following list, make sure to update src/transformers/dependency_versions_check.py
@@ -325,6 +324,7 @@ install_requires = [
     deps["sacremoses"],  # for XLM
     deps["tokenizers"],
     deps["tqdm"],  # progress bars in model download and training scripts
+    deps["detectron2"],  # detectron2 for image embedding
 ]
 
 setup(
