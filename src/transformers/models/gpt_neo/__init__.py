7906b575cee59304754c75bbc72e4ebd19ae467a
zanussbaum <zanussbaum@gmail.com>
2021-06-04 15:37:58 -0400

    Adding exports

--- src/transformers/models/gpt_neo/__init__.py
+++ src/transformers/models/gpt_neo/__init__.py
@@ -17,7 +17,13 @@
 # limitations under the License.
 from typing import TYPE_CHECKING
 
-from ...file_utils import _BaseLazyModule, is_torch_available
+from ...file_utils import  (
+    _BaseLazyModule,
+    is_flax_available,
+    is_tf_available,
+    is_tokenizers_available,
+    is_torch_available,
+)
 
 
 _import_structure = {
@@ -33,6 +39,8 @@ if is_torch_available():
         "load_tf_weights_in_gpt_neo",
     ]
 
+if is_flax_available():
+    _import_structure["modeling_flax_gpt_neo"] = ["FlaxGPTNeoLMHeadModel", "FlaxGPTNeoModel"]
 
 if TYPE_CHECKING:
     from .configuration_gpt_neo import GPT_NEO_PRETRAINED_CONFIG_ARCHIVE_MAP, GPTNeoConfig
@@ -45,6 +53,9 @@ if TYPE_CHECKING:
             GPTNeoPreTrainedModel,
             load_tf_weights_in_gpt_neo,
         )
+
+    if is_flax_available():
+        from .modeling_flax_gpt_neo import FlaxGPTNeoLMHeadModel, FlaxGPTNeoModel
 
 
 else:
--- src/transformers/models/gpt_neo/modeling_flax_gpt_neo.py
+++ src/transformers/models/gpt_neo/modeling_flax_gpt_neo.py
@@ -944,7 +944,44 @@ class FlaxGPTNeoLMHeadModule(nn.Module):
 
         return FlaxCausalLMOutput(logits=lm_logits, hidden_states=outputs.hidden_states, attentions=outputs.attentions)
 
+        
+class FlaxGPTNeoLMHeadModel(FlaxGPTNeoPreTrainedModel):
+    module_class = FlaxGPTNeoLMHeadModule
+
+    def prepare_inputs_for_generation(self, input_ids, past=None, **kwargs):
+        token_type_ids = kwargs.get("token_type_ids", None)
+        # only last token for inputs_ids if past is defined in kwargs
+        if past:
+            input_ids = jnp.expand_dims(input_ids[:, -1], -1)
+            if token_type_ids is not None:
+                token_type_ids = jnp.expand_dims(token_type_ids[:, -1], -1)
+
+        attention_mask = kwargs.get("attention_mask", None)
+        position_ids = kwargs.get("position_ids", None)
+
+        if attention_mask is not None and position_ids is None:
+            # create position_ids on the fly for batch generation
+            position_ids = attention_mask.long().cumsum(-1) - 1
+            position_ids.masked_fill_(attention_mask == 0, 1)
+            if past:
+                position_ids = jnp.expand_dims(position_ids[:, -1], -1)
+        else:
+            position_ids = None
+        return {
+            "input_ids": input_ids,
+            "past_key_values": past,
+            "use_cache": kwargs.get("use_cache"),
+            "position_ids": position_ids,
+            "attention_mask": attention_mask,
+            "token_type_ids": token_type_ids,
+        }
+
+    def update_inputs_for_generation(self, model_outputs, model_kwargs):
+        model_kwargs["past_key_values"] = model_outputs.past_key_values
+        model_kwargs["position_ids"] = model_kwargs["position_ids"][:, -1:] + 1
+        return model_kwargs
+
 
 append_call_sample_docstring(
-    FlaxGPTNeoLMHeadModule, _TOKENIZER_FOR_DOC, _CHECKPOINT_FOR_DOC, FlaxCausalLMOutput, _CONFIG_FOR_DOC
+    FlaxGPTNeoLMHeadModel, _TOKENIZER_FOR_DOC, _CHECKPOINT_FOR_DOC, FlaxCausalLMOutput, _CONFIG_FOR_DOC
 )
