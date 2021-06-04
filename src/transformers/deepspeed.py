202053704fa443ebbac2d1dfc4b53b60e66e48d6
Stas Bekman <stas@stason.org>
2021-06-03 19:04:07 -0700

    add mismatch validation + test

--- src/transformers/deepspeed.py
+++ src/transformers/deepspeed.py
@@ -20,6 +20,7 @@ import io
 import json
 import weakref
 from copy import deepcopy
+from functools import partialmethod
 
 from .dependency_versions_check import dep_version_check
 from .utils import logging
@@ -111,82 +112,65 @@ class HfTrainerDeepSpeedConfig(HfDeepSpeedConfig):
         super().__init__(config_file_or_dict)
         self.mismatches = []
 
-    def proc(self, config, ds_key, hf_val):
+    def proc(self, ds_key_long, hf_val, hf_key=None, must_match=False):
         """
-        Replace "auto" values with TrainingArguments value
+        A utility method that massages the config file and can optionally verify values.
 
-        Returns True if the replacement happened
-        """
-        if config is None:
-            return False
-
-        if config.get(ds_key) == "auto":
-            config[ds_key] = hf_val
-            return True
-
-    def proc_check(self, config, ds_key, hf_val, hf_key, ds_key_long=None):
-        """
         1. Replace "auto" values with TrainingArguments value
 
-        2. If it wasn't "auto" then check that DS config matches Trainer config values and if
-        mismatched add the entry to self.mismatched - will bail during trainer_config_finalize for one or more
-        mismatches
-
-        `ds_key_long` is to help to fully qualify the keys that otherwise are meaningless. e.g. "enabled" vs "fp16.enabled"
+        2. If it wasn't "auto" and ``must_match`` is true, then check that DS config matches Trainer
+        config values and if mismatched add the entry to self.mismatched - will bail during trainer_config_finalize for
+        one or more mismatches
 
         """
-        if config is None:
-            return False
 
-        print("hf1", hf_key, hf_val)
-        print("ds1", ds_key, config.get(ds_key))
+        c = self.config
 
-        if self.proc(config, ds_key, hf_val):
+        # find the config node of interest if it exists
+        stages = ds_key_long.split(".")
+        ds_key = stages.pop()
+        for stage in stages:
+            c = c.get(stage)
+            if c is None:
+                return
+
+        if c.get(ds_key) == "auto":
+            c[ds_key] = hf_val
             return
 
-        ds_val = config.get(ds_key)
-        print("ds2", ds_key, ds_val)
-        print()
+        if not must_match:
+            return
 
-        if ds_val != hf_val:
-            if ds_key_long is not None:
-                ds_key = ds_key_long
-            self.mismatches.append(f"- ds {ds_key}={ds_val} vs hf {hf_key}={hf_val}")
+        ds_val = c.get(ds_key)
+        if ds_val is not None and ds_val != hf_val:
+            self.mismatches.append(f"- ds {ds_key_long}={ds_val} vs hf {hf_key}={hf_val}")
+
+    proc_check = partialmethod(proc, must_match=True)
 
     def trainer_config_process(self, args):
         """
         Adjust the config with ``TrainingArguments`` values. This stage is run during ``TrainingArguments`` object
         creation.
         """
-        config = self.config
-
         # DeepSpeed does:
         # train_batch_size = world_size * train_micro_batch_size_per_gpu * gradient_accumulation_steps
         train_batch_size = args.world_size * args.per_device_train_batch_size * args.gradient_accumulation_steps
         self.proc_check(
-            config, "train_micro_batch_size_per_gpu", args.per_device_train_batch_size, "per_device_train_batch_size"
+            "train_micro_batch_size_per_gpu", args.per_device_train_batch_size, "per_device_train_batch_size"
         )
-        self.proc_check(
-            config, "gradient_accumulation_steps", args.gradient_accumulation_steps, "gradient_accumulation_steps"
-        )
-        self.proc_check(config, "train_batch_size", train_batch_size, "train_batch_size (calculated)")
-        self.proc_check(config, "gradient_clipping", args.max_grad_norm, "max_grad_norm")
+        self.proc_check("gradient_accumulation_steps", args.gradient_accumulation_steps, "gradient_accumulation_steps")
+        self.proc_check("train_batch_size", train_batch_size, "train_batch_size (calculated)")
+        self.proc_check("gradient_clipping", args.max_grad_norm, "max_grad_norm")
 
-        config_optim = config.get("optimizer", {})
-        if config_optim != {}:
-            config_optim_params = config_optim.get("params")
-            self.proc_check(config_optim_params, "lr", args.learning_rate, "learning_rate")
-            self.proc_check(config_optim_params, "betas", [args.adam_beta1, args.adam_beta2], "adam_beta1+adam_beta2")
-            self.proc_check(config_optim_params, "eps", args.adam_epsilon, "adam_epsilon")
-            self.proc_check(config_optim_params, "weight_decay", args.weight_decay, "weight_decay")
+        self.proc_check("optimizer.params.lr", args.learning_rate, "learning_rate")
+        self.proc_check("optimizer.params.betas", [args.adam_beta1, args.adam_beta2], "adam_beta1+adam_beta2")
+        self.proc_check("optimizer.params.eps", args.adam_epsilon, "adam_epsilon")
+        self.proc_check("optimizer.params.weight_decay", args.weight_decay, "weight_decay")
 
-        config_sched = config.get("scheduler", {})
-        if config_sched != {}:
-            config_sched_params = config_sched.get("params")
-            self.proc(config_sched_params, "warmup_min_lr", 0)
-            self.proc_check(config_sched_params, "warmup_max_lr", args.learning_rate, "learning_rate")
-            self.proc_check(config_sched_params, "warmup_num_steps", args.warmup_steps, "warmup_steps")
-            # total_num_steps - will get set in trainer_config_finalize
+        self.proc("scheduler.params.warmup_min_lr", 0)  # not a trainer arg
+        self.proc_check("scheduler.params.warmup_max_lr", args.learning_rate, "learning_rate")
+        self.proc_check("scheduler.params.warmup_num_steps", args.warmup_steps, "warmup_steps")
+        # total_num_steps - will get set in trainer_config_finalize
 
         # fp16
         if args.fp16:
@@ -196,14 +180,12 @@ class HfTrainerDeepSpeedConfig(HfDeepSpeedConfig):
 
         # amp: similar to the pytorch native amp - it has a bunch of optional params but we won't set
         # any here unless the user did the work
-        config_fp16 = config.get("fp16")
-        self.proc_check(config_fp16, "enabled", (fp16_backend == "amp"), "fp16+fp16_backend=amp")
+        self.proc_check("fp16.enabled", fp16_backend == "amp", "fp16+fp16_backend(amp)")
 
         # apex: delegates amp work to apex (which needs to be available), but it cannot be used with any
         # ZeRO features
-        config_amp = config.get("amp")
-        self.proc_check(config_amp, "enabled", fp16_backend == "apex", "fp16+fp16_backend=apex")
-        self.proc_check(config_amp, "opt_level", args.fp16_opt_level, "fp16_opt_level")
+        self.proc_check("amp.enabled", fp16_backend == "apex", "fp16+fp16_backend(apex)")
+        self.proc_check("amp.opt_level", args.fp16_opt_level, "fp16_opt_level")
 
     def trainer_config_finalize(self, args, model, num_training_steps):
         """
@@ -211,21 +193,16 @@ class HfTrainerDeepSpeedConfig(HfDeepSpeedConfig):
 
         Now we we can complete the configuration process.
         """
-        config = self.config
-
         # zero
-        config_zero = config.get("zero_optimization", {})
         if self.is_zero3():
             # automatically assign the optimal config values based on model config
             hidden_size = model.config.hidden_size
-            self.proc(config_zero, "reduce_bucket_size", hidden_size * hidden_size)
-            self.proc(config_zero, "stage3_prefetch_bucket_size", 0.9 * hidden_size * hidden_size)
-            self.proc(config_zero, "stage3_param_persistence_threshold", 10 * hidden_size)
+            self.proc("zero_optimization.reduce_bucket_size", hidden_size * hidden_size)
+            self.proc("zero_optimization.stage3_prefetch_bucket_size", 0.9 * hidden_size * hidden_size)
+            self.proc("zero_optimization.stage3_param_persistence_threshold", 10 * hidden_size)
 
         # scheduler
-        config_sched = config.get("scheduler", {})
-        config_sched_params = config_sched.get("params", {})
-        self.proc_check(config_sched_params, "total_num_steps", num_training_steps, "num_training_steps (calculated)")
+        self.proc_check("scheduler.params.total_num_steps", num_training_steps, "num_training_steps (calculated)")
 
         if len(self.mismatches) > 0:
             mismatches = "\n".join(self.mismatches)
