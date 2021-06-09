6a08733117a0a390329ebe8c68b9e9fbee1ff805
Stefan Schweter <stefan@schweter.it>
2021-06-09 15:07:27 +0200

    configuration: add new normalize_embeddings variable to RoBERTa config

--- src/transformers/models/roberta/configuration_roberta.py
+++ src/transformers/models/roberta/configuration_roberta.py
@@ -59,6 +59,12 @@ class RobertaConfig(BertConfig):
     """
     model_type = "roberta"
 
-    def __init__(self, pad_token_id=1, bos_token_id=0, eos_token_id=2, **kwargs):
+    def __init__(self, pad_token_id=1, bos_token_id=0, eos_token_id=2, normalize_embeddings=True, **kwargs):
         """Constructs RobertaConfig."""
-        super().__init__(pad_token_id=pad_token_id, bos_token_id=bos_token_id, eos_token_id=eos_token_id, **kwargs)
+        super().__init__(
+            pad_token_id=pad_token_id,
+            bos_token_id=bos_token_id,
+            eos_token_id=eos_token_id,
+            normalize_embeddings=normalize_embeddings,
+            **kwargs,
+        )
