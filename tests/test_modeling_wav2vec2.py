f1fe6ab046d0ec06a70f9b8104e823e7ac186c68
Patrick von Platen <patrick.v.platen@gmail.com>
2021-06-09 17:27:41 +0000

    fix flaky test

--- tests/test_modeling_wav2vec2.py
+++ tests/test_modeling_wav2vec2.py
@@ -341,8 +341,10 @@ class Wav2Vec2ModelTest(ModelTesterMixin, unittest.TestCase):
     def _mock_init_weights(self, module):
         if hasattr(module, "weight") and module.weight is not None:
             module.weight.data.fill_(3)
-        if hasattr(module, "weight_g") and module.weight is not None:
+        if hasattr(module, "weight_g") and module.weight_g is not None:
             module.weight_g.data.fill_(3)
+        if hasattr(module, "weight_v") and module.weight_v is not None:
+            module.weight_v.data.fill_(3)
         if hasattr(module, "bias") and module.bias is not None:
             module.bias.data.fill_(3)
         if hasattr(module, "codevectors") and module.codevectors is not None:
@@ -477,8 +479,10 @@ class Wav2Vec2RobustModelTest(ModelTesterMixin, unittest.TestCase):
     def _mock_init_weights(self, module):
         if hasattr(module, "weight") and module.weight is not None:
             module.weight.data.fill_(3)
-        if hasattr(module, "weight_g") and module.weight is not None:
+        if hasattr(module, "weight_g") and module.weight_g is not None:
             module.weight_g.data.fill_(3)
+        if hasattr(module, "weight_v") and module.weight_v is not None:
+            module.weight_v.data.fill_(3)
         if hasattr(module, "bias") and module.bias is not None:
             module.bias.data.fill_(3)
         if hasattr(module, "codevectors") and module.codevectors is not None:
