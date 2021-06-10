bebbdd0fc98c106411d64f45f62dbc235828c707
Matt <Rocketknight1@users.noreply.github.com>
2021-06-10 15:25:04 +0100

    Appending label2id and id2label to models to ensure inference works properly (#12102)

--- examples/pytorch/text-classification/run_glue.py
+++ examples/pytorch/text-classification/run_glue.py
@@ -370,6 +370,10 @@ def main():
     elif data_args.task_name is None and not is_regression:
         label_to_id = {v: i for i, v in enumerate(label_list)}
 
+    if label_to_id is not None:
+        model.config.label2id = label_to_id
+        model.config.id2label = {id: label for label, id in config.label2id.items()}
+
     if data_args.max_seq_length > tokenizer.model_max_length:
         logger.warning(
             f"The max_seq_length passed ({data_args.max_seq_length}) is larger than the maximum length for the"
