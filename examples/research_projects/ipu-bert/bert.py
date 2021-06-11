ec1e97cb63932ec24bebecd6c9bc58eb934ee107
James Briggs <jamesbr@graphcore.ai>
2021-06-11 15:59:28 +0100

    Fixed tab/space indent bug

--- examples/research_projects/ipu-bert/bert.py
+++ examples/research_projects/ipu-bert/bert.py
@@ -53,7 +53,7 @@ if __name__ == "__main__":
 
     # W&B
     if config.wandb:
-	import wandb
+        import wandb
         wandb.init(project="torch-bert")
         wandb.config.update(vars(config))
 
