ebfd88294ea9953733a1a3eded6716b15a2b8385
Stefan Schweter <stefan@schweter.it>
2021-06-11 08:48:41 +0200

    roberta: implement normalize before logic for RobertaSelfOutput from fairseq (see https://github.com/pytorch/fairseq/blob/4950c56f461db1646872159b2c470fe57ae72c69/fairseq/modules/transformer_layer.py\#L389)

--- src/transformers/models/roberta/modeling_roberta.py
+++ src/transformers/models/roberta/modeling_roberta.py
@@ -276,14 +276,21 @@ class RobertaSelfAttention(nn.Module):
 class RobertaSelfOutput(nn.Module):
     def __init__(self, config):
         super().__init__()
+        self.normalize_before = config.normalize_embeddings
         self.dense = nn.Linear(config.hidden_size, config.hidden_size)
         self.LayerNorm = nn.LayerNorm(config.hidden_size, eps=config.layer_norm_eps)
         self.dropout = nn.Dropout(config.hidden_dropout_prob)
 
     def forward(self, hidden_states, input_tensor):
+        if self.normalize_before:
+            hidden_states = self.LayerNorm(hidden_states + input_tensor)
+
         hidden_states = self.dense(hidden_states)
         hidden_states = self.dropout(hidden_states)
-        hidden_states = self.LayerNorm(hidden_states + input_tensor)
+
+        if not self.normalize_before:
+            hidden_states = self.LayerNorm(hidden_states + input_tensor)
+
         return hidden_states
 
 
