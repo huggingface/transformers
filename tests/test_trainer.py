cdee79a402c2b82e9e6b7c6da6fee6495e4b2115
Amog Kamsetty <amogkamsetty@yahoo.com>
2021-06-12 20:26:28 -0700

    formatting

--- tests/test_trainer.py
+++ tests/test_trainer.py
@@ -1337,6 +1337,6 @@ class TrainerHyperParameterRayIntegrationTest(unittest.TestCase):
         import ray
         from ray.util.client.ray_client_helpers import ray_start_client_server
 
-        with ray_start_client_server() as client:
+        with ray_start_client_server():
             assert ray.util.client.ray.is_connected()
             self.ray_hyperparameter_search()
