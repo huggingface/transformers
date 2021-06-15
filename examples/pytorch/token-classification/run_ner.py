7d7ceca3966fc5d46d55fdd022c12403f7a0c03e
Sylvain Gugger <35901082+sgugger@users.noreply.github.com>
2021-06-15 16:01:37 -0400

    Model card defaults (#12122)
    * [WIP] Model card defaults
    
    * finetuned_from default value
    
    * Add all mappings to the mapping file
    
    * Be more defensive on finetuned_from arg
    
    * Add default task tag
    
    * Separate tags from tasks
    
    * Edge case for dataset
    
    * Apply suggestions from code review
    
    Co-authored-by: Lysandre Debut <lysandre@huggingface.co>
    
    Co-authored-by: Lysandre Debut <lysandre@huggingface.co>

--- examples/pytorch/token-classification/run_ner.py
+++ examples/pytorch/token-classification/run_ner.py
@@ -522,7 +522,7 @@ def main():
                     writer.write(" ".join(prediction) + "\n")
 
     if training_args.push_to_hub:
-        kwargs = {"finetuned_from": model_args.model_name_or_path, "tags": "token-classification"}
+        kwargs = {"finetuned_from": model_args.model_name_or_path, "tasks": "token-classification"}
         if data_args.dataset_name is not None:
             kwargs["dataset_tags"] = data_args.dataset_name
             if data_args.dataset_config_name is not None:
