0d20bcd332ceca2aa7113696fc5e8239d36a3f5c
Andrew Romans <andrew.romans@hotmail.com>
2021-06-15 16:48:47 -0400

    Removing unused imports.
    Co-authored-by: Dalton Walker <dalton_walker@icloud.com>

--- src/transformers/data/data_tf_collator.py
+++ src/transformers/data/data_tf_collator.py
@@ -13,7 +13,7 @@
 # limitations under the License.
 
 from dataclasses import dataclass
-from typing import Any, Callable, Dict, List, NewType, Optional, Tuple, Union
+from typing import Dict, List, Optional, Tuple, Union
 
 import tensorflow as tf
 
