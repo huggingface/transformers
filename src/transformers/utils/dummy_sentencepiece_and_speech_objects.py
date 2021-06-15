d07b540a37afbe790a093183b3bb105c5ff5f7ec
Lysandre Debut <lysandre@huggingface.co>
2021-06-15 14:39:05 +0200

    Have dummy processors have a `from_pretrained` method (#12145)

--- src/transformers/utils/dummy_sentencepiece_and_speech_objects.py
+++ src/transformers/utils/dummy_sentencepiece_and_speech_objects.py
@@ -5,3 +5,7 @@ from ..file_utils import requires_backends
 class Speech2TextProcessor:
     def __init__(self, *args, **kwargs):
         requires_backends(self, ["sentencepiece", "speech"])
+
+    @classmethod
+    def from_pretrained(cls, *args, **kwargs):
+        requires_backends(cls, ["sentencepiece", "speech"])
