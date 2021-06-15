8adeef0b031d9b23c576771555cf1ebc998220fa
Andrew Romans <andrew.romans@hotmail.com>
2021-06-15 11:11:20 -0400

    Added TensorFlow_Probability.
    Co-authored-by: Dalton Walker <dalton_walker@icloud.com>

--- setup.py
+++ setup.py
@@ -224,7 +224,11 @@ extras["ja"] = deps_list("fugashi", "ipadic", "unidic_lite", "unidic")
 extras["sklearn"] = deps_list("scikit-learn")
 
 extras["tf"] = deps_list("tensorflow", "tensorflow-probability", "onnxconverter-common", "keras2onnx")
+<<<<<<< HEAD
 extras["tf-cpu"] = deps_list("tensorflow-cpu", "tensorflow-probability", "onnxconverter-common", "keras2onnx")
+=======
+extras["tf-cpu"] = deps_list("tensorflow-cpu", "onnxconverter-common", "keras2onnx")
+>>>>>>> Added TensorFlow_Probability.
 
 extras["torch"] = deps_list("torch")
 
