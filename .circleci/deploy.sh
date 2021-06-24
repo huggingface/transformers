186bc70137ff7d0ca4416baf2c7df1e4513f108d
Sylvain Gugger <sylvain.gugger@gmail.com>
2021-06-24 10:15:15 -0400

    Document patch release v4.8.1

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -65,4 +65,5 @@ deploy_doc "4906a29" v4.5.0
 deploy_doc "4bae96e" v4.5.1
 deploy_doc "25dee4a" v4.6.0
 deploy_doc "7a6c9fa" v4.7.0
-deploy_doc "9252a51"  # v4.8.0 Latest stable release
+deploy_doc "9252a51" v4.8.0
+deploy_doc "1366172"  # v4.8.1 Latest stable release
