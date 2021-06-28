88e2e350abd9795b57f0e2696e3feb3b7e7b5013
Patrick von Platen <patrick.v.platen@gmail.com>
2021-06-28 11:03:40 +0100

    Apply suggestions from code review
    Co-authored-by: SaulLu <55560583+SaulLu@users.noreply.github.com>

--- examples/flax/language-modeling/t5_tokenizer_model.py
+++ examples/flax/language-modeling/t5_tokenizer_model.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import json
 from typing import Iterator, List, Union
 
 from tokenizers import AddedToken, Regex, Tokenizer, decoders, normalizers, pre_tokenizers, trainers
@@ -11,11 +12,36 @@ class SentencePieceUnigramTokenizer(BaseTokenizer):
     """
     This class is a copy of `DeDLOC's tokenizer implementation <https://github.com/yandex-research/DeDLOC/blob/main/sahajbert/tokenizer/tokenizer_model.py>`__ .
 
-    Custom SentencePiece Unigram Tokenizer with lower, digits, punctuation and Bengali characters normalization
+    Custom SentencePiece Unigram Tokenizer with NMT, NKFC, spaces and lower-casing characters normalization
     Represents the Unigram algorithm, with the pretokenization used by SentencePiece
     """
 
-    def __init__(self, replacement: str = "▁", add_prefix_space: bool = True):
+    def __init__(
+        self,
+        replacement: str = "▁",
+        add_prefix_space: bool = True,
+        unk_token: Union[str, AddedToken]="<unk>",
+        sep_token: Union[str, AddedToken]="</s>",
+        pad_token: Union[str, AddedToken]="<pad>",
+    ):
+        self.special_tokens = {
+            "pad": {
+                "id": 0,
+                "token": pad_token
+            },"sep": {
+                "id": 1,
+                "token": sep_token
+            },
+            "unk": {
+                "id": 2,
+                "token": unk_token
+            }
+        }
+
+        self.special_tokens_list = [None] * len(self.special_tokens)
+        for token_dict in self.special_tokens.values():
+            self.special_tokens_list[token_dict["id"]] = token_dict["token"]
+
         tokenizer = Tokenizer(Unigram())
 
         tokenizer.normalizer = normalizers.Sequence(
@@ -36,8 +62,8 @@ class SentencePieceUnigramTokenizer(BaseTokenizer):
         tokenizer.decoder = decoders.Metaspace(replacement=replacement, add_prefix_space=add_prefix_space)
 
         tokenizer.post_processor = TemplateProcessing(
-            single="$A </s>",
-            special_tokens=[("<pad>", 0), ("</s>", 1), ("<unk>", 2)],
+            single=f"$A {self.special_tokens['sep']['token']}",
+            special_tokens=[(self.special_tokens["sep"]["token"], self.special_tokens["sep"]["id"])],
         )
 
         parameters = {
@@ -53,13 +79,12 @@ class SentencePieceUnigramTokenizer(BaseTokenizer):
         files: Union[str, List[str]],
         vocab_size: int = 8000,
         show_progress: bool = True,
-        special_tokens: List[Union[str, AddedToken]] = [],
     ):
         """Train the model using the given files"""
 
         trainer = trainers.UnigramTrainer(
             vocab_size=vocab_size,
-            special_tokens=special_tokens,
+            special_tokens=self.special_tokens_list,
             show_progress=show_progress,
         )
 
@@ -67,19 +92,28 @@ class SentencePieceUnigramTokenizer(BaseTokenizer):
             files = [files]
         self._tokenizer.train(files, trainer=trainer)
 
+        self.add_unk_id()
     def train_from_iterator(
         self,
         iterator: Union[Iterator[str], Iterator[Iterator[str]]],
         vocab_size: int = 8000,
         show_progress: bool = True,
-        special_tokens: List[Union[str, AddedToken]] = [],
     ):
         """Train the model using the given iterator"""
 
         trainer = trainers.UnigramTrainer(
             vocab_size=vocab_size,
-            special_tokens=special_tokens,
+            special_tokens=self.special_tokens_list,
             show_progress=show_progress,
         )
 
         self._tokenizer.train_from_iterator(iterator, trainer=trainer)
+        
+        self.add_unk_id()
+        
+    def add_unk_id(self):
+        tokenizer_json = json.loads(self._tokenizer.to_str())
+
+        tokenizer_json["model"]["unk_id"] = self.special_tokens["unk"]["id"]
+
+        self._tokenizer = Tokenizer.from_str(json.dumps(tokenizer_json))
