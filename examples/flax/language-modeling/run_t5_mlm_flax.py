122d7dc34fd0e397a08b8a584a632fc57d3fd5d0
Ibraheem Moosa <ibraheemmoosa1347@gmail.com>
2021-07-08 04:05:47 +0600

    Remove logging of GPU count etc logging. (#12569)
    Successfully logging this requires Pytorch. For the purposes of this script we are not using Pytorch.

--- examples/flax/language-modeling/run_t5_mlm_flax.py
+++ examples/flax/language-modeling/run_t5_mlm_flax.py
@@ -430,10 +430,6 @@ if __name__ == "__main__":
 
     # Log on each process the small summary:
     logger = logging.getLogger(__name__)
-    logger.warning(
-        f"Process rank: {training_args.local_rank}, device: {training_args.device}, n_gpu: {training_args.n_gpu}"
-        + f"distributed training: {bool(training_args.local_rank != -1)}, 16-bits training: {training_args.fp16}"
-    )
 
     # Set the verbosity to info of the Transformers logger (on main process only):
     logger.info(f"Training/evaluation parameters {training_args}")
