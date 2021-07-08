0a6b9048d1d5de5b965a8757dd56fef87ed9d7e1
Sylvain Gugger <35901082+sgugger@users.noreply.github.com>
2021-07-08 07:20:46 -0400

    Init pickle (#12567)
    * Try to pickle transformers
    
    * Deal with special objs better
    
    * Make picklable

--- src/transformers/models/byt5/__init__.py
+++ src/transformers/models/byt5/__init__.py
@@ -18,7 +18,7 @@
 
 from typing import TYPE_CHECKING
 
-from ...file_utils import _BaseLazyModule
+from ...file_utils import _LazyModule
 
 
 _import_structure = {
@@ -29,19 +29,6 @@ _import_structure = {
 if TYPE_CHECKING:
     from .tokenization_byt5 import ByT5Tokenizer
 else:
-    import importlib
-    import os
     import sys
 
-    class _LazyModule(_BaseLazyModule):
-        """
-        Module class that surfaces all objects but only performs associated imports when the objects are requested.
-        """
-
-        __file__ = globals()["__file__"]
-        __path__ = [os.path.dirname(__file__)]
-
-        def _get_module(self, module_name: str):
-            return importlib.import_module("." + module_name, self.__name__)
-
-    sys.modules[__name__] = _LazyModule(__name__, _import_structure)
+    sys.modules[__name__] = _LazyModule(__name__, globals()["__file__"], _import_structure)
