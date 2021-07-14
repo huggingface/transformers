b278e46bc278e92dbb3be6de1de531d33961fe92
Morgan <funtowiczmo@gmail.com>
2021-07-14 11:01:23 +0200

    Uniformize the style between hubert and wav2vec2

--- src/transformers/models/wav2vec2/modeling_wav2vec2.py
+++ src/transformers/models/wav2vec2/modeling_wav2vec2.py
@@ -1049,7 +1049,7 @@ class Wav2Vec2Model(Wav2Vec2PreTrainedModel):
             attention_mask = attention_mask.flip([-1]).cumsum(-1).flip([-1]).bool()
 
         hidden_states, extract_features = self.feature_projection(extract_features)
-        hidden_states = self._mask_hidden_states(hidden_states, mask_time_indices)
+        hidden_states = self._mask_hidden_states(hidden_states, mask_time_indices=mask_time_indices)
 
         encoder_outputs = self.encoder(
             hidden_states,
