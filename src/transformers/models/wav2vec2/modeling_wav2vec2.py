9b4b53f4581d289fb14f1ec302bb89eeaa273a6d
Funtowicz Morgan <mfuntowicz@users.noreply.github.com>
2021-07-14 16:30:19 +0200

    Fix uninitialized variables when `config.mask_feature_prob > 0` (#12705)

--- src/transformers/models/wav2vec2/modeling_wav2vec2.py
+++ src/transformers/models/wav2vec2/modeling_wav2vec2.py
@@ -961,13 +961,13 @@ class Wav2Vec2Model(Wav2Vec2PreTrainedModel):
         if not getattr(self.config, "apply_spec_augment", True):
             return hidden_states
 
+        # generate indices & apply SpecAugment along time axis
+        batch_size, sequence_length, hidden_size = hidden_states.size()
+
         if mask_time_indices is not None:
             # apply SpecAugment along time axis with given mask_time_indices
             hidden_states[mask_time_indices] = self.masked_spec_embed.to(hidden_states.dtype)
         elif self.config.mask_time_prob > 0 and self.training:
-            # generate indices & apply SpecAugment along time axis
-            batch_size, sequence_length, hidden_size = hidden_states.size()
-
             mask_time_indices = _compute_mask_indices(
                 (batch_size, sequence_length),
                 mask_prob=self.config.mask_time_prob,
