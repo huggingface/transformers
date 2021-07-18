075590b352354a420758f4302c7c2baf9908cb97
Niels Rogge <niels.rogge1@gmail.com>
2021-07-18 11:44:25 +0200

    Add print statement

--- src/transformers/image_utils.py
+++ src/transformers/image_utils.py
@@ -101,6 +101,10 @@ class ImageFeatureExtractionMixin:
         if rescale:
             image = image.astype(np.float32) / 255.0
 
+        print(image.shape)
+
+        print(image.dtype)
+        
         if channel_first:
             image = image.transpose(2, 0, 1)
 
