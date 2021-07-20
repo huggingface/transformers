bf8e000adeb749b9ccd7d542f7504725c8ca8c5a
patil-suraj <surajp815@gmail.com>
2021-07-20 17:42:25 +0530

    add and fix examples

--- src/transformers/models/clip/modeling_clip.py
+++ src/transformers/models/clip/modeling_clip.py
@@ -699,6 +699,18 @@ class CLIPTextModel(CLIPPreTrainedModel):
         r"""
         Returns:
 
+        Examples::
+
+            >>> from transformers import CLIPTokenizer, CLIPTextModel
+
+            >>> model = CLIPTextModel.from_pretrained("openai/clip-vit-base-patch32")
+            >>> tokenizer = CLIPTokenizer.from_pretrained("openai/clip-vit-base-patch32")
+
+            >>> inputs = tokenizer(["a photo of a cat", "a photo of a dog"],  padding=True, return_tensors="pt")
+
+            >>> outputs = model(**inputs)
+            >>> last_hidden_state = outputs.last_hidden_state
+            >>> pooled_output = outputs.pooled_output # pooled (EOS token) states
         """
         return self.text_model(
             input_ids=input_ids,
@@ -791,6 +803,23 @@ class CLIPVisionModel(CLIPPreTrainedModel):
         r"""
         Returns:
 
+        Examples::
+
+            >>> from PIL import Image
+            >>> import requests
+            >>> from transformers import CLIPProcessor, CLIPVisionModel
+
+            >>> model = CLIPVisionModel.from_pretrained("openai/clip-vit-base-patch32")
+            >>> processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
+
+            >>> url = "http://images.cocodataset.org/val2017/000000039769.jpg"
+            >>> image = Image.open(requests.get(url, stream=True).raw)
+
+            >>> inputs = processor(images=image, return_tensors="pt")
+
+            >>> outputs = model(**inputs)
+            >>> last_hidden_state = outputs.last_hidden_state
+            >>> pooled_output = outputs.pooled_output # pooled CLS states
         """
         return self.vision_model(
             pixel_values=pixel_values,
@@ -847,6 +876,16 @@ class CLIPModel(CLIPPreTrainedModel):
         Returns:
             text_features (:obj:`torch.FloatTensor` of shape :obj:`(batch_size, output_dim`): The text embeddings
             obtained by applying the projection layer to the pooled output of :class:`~transformers.CLIPTextModel`.
+
+        Examples::
+
+            >>> from transformers import CLIPTokenizer, CLIPModel
+
+            >>> model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
+            >>> tokenizer = CLIPTokenizer.from_pretrained("openai/clip-vit-base-patch32")
+
+            >>> inputs = tokenizer(["a photo of a cat", "a photo of a dog"],  padding=True, return_tensors="pt")
+            >>> text_features = model.get_text_features(**inputs)
         """
         text_outputs = self.text_model(
             input_ids=input_ids,
@@ -874,6 +913,22 @@ class CLIPModel(CLIPPreTrainedModel):
         Returns:
             image_features (:obj:`torch.FloatTensor` of shape :obj:`(batch_size, output_dim`): The image embeddings
             obtained by applying the projection layer to the pooled output of :class:`~transformers.CLIPVisionModel`.
+
+        Examples::
+
+            >>> from PIL import Image
+            >>> import requests
+            >>> from transformers import CLIPProcessor, CLIPModel
+
+            >>> model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
+            >>> processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
+
+            >>> url = "http://images.cocodataset.org/val2017/000000039769.jpg"
+            >>> image = Image.open(requests.get(url, stream=True).raw)
+
+            >>> inputs = processor(images=image, return_tensors="pt")
+
+            >>> image_features = model.get_image_features(**inputs)
         """
         vision_outputs = self.vision_model(
             pixel_values=pixel_values,
@@ -902,6 +957,24 @@ class CLIPModel(CLIPPreTrainedModel):
     ):
         r"""
         Returns:
+
+        Examples::
+
+            >>> from PIL import Image
+            >>> import requests
+            >>> from transformers import CLIPProcessor, CLIPModel
+
+            >>> model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
+            >>> processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
+
+            >>> url = "http://images.cocodataset.org/val2017/000000039769.jpg"
+            >>> image = Image.open(requests.get(url, stream=True).raw)
+
+            >>> inputs = processor(text=["a photo of a cat", "a photo of a dog"], images=image, return_tensors="pt", padding=True)
+
+            >>> outputs = model(**inputs)
+            >>> logits_per_image = outputs.logits_per_image # this is the image-text similarity score
+            >>> probs = logits_per_image.softmax(dim=1) # we can take the softmax to get the label probabilities
 
         """
         return_dict = return_dict if return_dict is not None else self.config.return_dict
