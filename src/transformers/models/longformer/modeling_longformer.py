e6d92edea72b8bef43b3ecc5e3aeb2e661909515
Patrick von Platen <patrick.v.platen@gmail.com>
2021-07-20 14:17:21 +0200

    [Longformer] Correct longformer docs (#12809)
    * fix_torch_device_generate_test
    
    * remove @
    
    * correct longformer docs
    
    Co-authored-by: Patrick von Platen <patrick@huggingface.co>

--- src/transformers/models/longformer/modeling_longformer.py
+++ src/transformers/models/longformer/modeling_longformer.py
@@ -1609,7 +1609,6 @@ class LongformerModel(LongformerPreTrainedModel):
             >>> SAMPLE_TEXT = ' '.join(['Hello world! '] * 1000)  # long input document
             >>> input_ids = torch.tensor(tokenizer.encode(SAMPLE_TEXT)).unsqueeze(0)  # batch of size 1
 
-            >>> # Attention mask values -- 0: no attention, 1: local attention, 2: global attention
             >>> attention_mask = torch.ones(input_ids.shape, dtype=torch.long, device=input_ids.device) # initialize to local attention
             >>> global_attention_mask = torch.zeros(input_ids.shape, dtype=torch.long, device=input_ids.device) # initialize to global attention to be deactivated for all tokens
             >>> global_attention_mask[:, [1, 4, 21,]] = 1  # Set global attention to random tokens for the sake of this example
