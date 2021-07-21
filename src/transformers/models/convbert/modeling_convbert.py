e70f66614f34de1e631bfca7b1372df950597299
PhilipMay <philip@may.la>
2021-07-21 18:12:15 +0200

    add classifier_dropout to ConvBERT

--- src/transformers/models/convbert/modeling_convbert.py
+++ src/transformers/models/convbert/modeling_convbert.py
@@ -936,7 +936,10 @@ class ConvBertClassificationHead(nn.Module):
     def __init__(self, config):
         super().__init__()
         self.dense = nn.Linear(config.hidden_size, config.hidden_size)
-        self.dropout = nn.Dropout(config.hidden_dropout_prob)
+        classifier_dropout = (
+            config.classifier_dropout if config.classifier_dropout is not None else config.hidden_dropout_prob
+        )
+        self.dropout = nn.Dropout(classifier_dropout)
         self.out_proj = nn.Linear(config.hidden_size, config.num_labels)
 
         self.config = config
@@ -1152,7 +1155,10 @@ class ConvBertForTokenClassification(ConvBertPreTrainedModel):
         self.num_labels = config.num_labels
 
         self.convbert = ConvBertModel(config)
-        self.dropout = nn.Dropout(config.hidden_dropout_prob)
+        classifier_dropout = (
+            config.classifier_dropout if config.classifier_dropout is not None else config.hidden_dropout_prob
+        )
+        self.dropout = nn.Dropout(classifier_dropout)
         self.classifier = nn.Linear(config.hidden_size, config.num_labels)
 
         self.init_weights()
