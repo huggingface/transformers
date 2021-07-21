236362fab6d41ef90408ccb12fa2c609eb717a77
Lysandre Debut <lysandre@huggingface.co>
2021-07-21 12:23:35 +0200

    Update src/transformers/models/funnel/modeling_funnel.py
    Co-authored-by: Sylvain Gugger <35901082+sgugger@users.noreply.github.com>

--- src/transformers/models/funnel/modeling_funnel.py
+++ src/transformers/models/funnel/modeling_funnel.py
@@ -911,7 +911,7 @@ class FunnelBaseModel(FunnelPreTrainedModel):
     @add_start_docstrings_to_model_forward(FUNNEL_INPUTS_DOCSTRING.format("batch_size, sequence_length"))
     @add_code_sample_docstrings(
         tokenizer_class=_TOKENIZER_FOR_DOC,
-        checkpoint=_CHECKPOINT_FOR_DOC,
+        checkpoint="funnel-transformer/small-base",
         output_type=BaseModelOutput,
         config_class=_CONFIG_FOR_DOC,
     )
