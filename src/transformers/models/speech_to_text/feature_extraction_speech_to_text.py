correct normalization for truncation

src/transformers/models/speech_to_text/feature_extraction_speech_to_text.py
@@ -93,22 +93,25 @@ class Speech2TextFeatureExtractor(SequenceFeatureExtractor):
 
     @staticmethod
     def utterance_cmvn(
-        x: np.ndarray, normalize_means: Optional[bool] = True, normalize_vars: Optional[bool] = True
+        x: np.ndarray, input_length: int, normalize_means: Optional[bool] = True, normalize_vars: Optional[bool] = True
     ) -> np.ndarray:
-        mean = x.mean(axis=0)
-        square_sums = (x ** 2).sum(axis=0)
+        mean = x[:input_length].mean(axis=0)
+        square_sums = (x[:input_length] ** 2).sum(axis=0)
 
         if normalize_means:
             x = np.subtract(x, mean)
         if normalize_vars:
-            var = square_sums / x.shape[0] - mean ** 2
+            var = square_sums / x[:input_length].shape[0] - mean ** 2
             std = np.sqrt(np.maximum(var, 1e-10))
             x = np.divide(x, std)
 
         return x
 
-    def normalize(self, input_values: List[np.ndarray]) -> List[np.ndarray]:
-        return [self.utterance_cmvn(x, self.normalize_means, self.normalize_vars) for x in input_values]
+    def normalize(self, input_values: List[np.ndarray], input_lengths: List[int]) -> List[np.ndarray]:
+        return [
+            self.utterance_cmvn(x, n, self.normalize_means, self.normalize_vars)
+            for x, n in zip(input_values, input_lengths)
+        ]
 
     def __call__(
         self,
@@ -202,10 +205,6 @@ class Speech2TextFeatureExtractor(SequenceFeatureExtractor):
         # extract fbank features
         features = [self._extract_fbank_features(waveform) for waveform in raw_speech]
 
-        # Utterance-level cepstral mean and variance normalization
-        if self.do_ceptral_normalize:
-            features = self.normalize(features)
-
         # convert into correct format for padding
         encoded_inputs = BatchFeature({"input_features": features})
 
@@ -216,8 +215,26 @@ class Speech2TextFeatureExtractor(SequenceFeatureExtractor):
             truncation=truncation,
             pad_to_multiple_of=pad_to_multiple_of,
             return_attention_mask=return_attention_mask,
-            return_tensors=return_tensors,
             **kwargs,
         )
+
+        if "attention_mask" in padded_inputs:
+            input_lengths = padded_inputs["attention_mask"].sum(-1)
+        else:
+            padded_input_values = padded_inputs["input_features"]
+            input_lengths = [padded_input_values.shape[-1] for _ in range(padded_input_values.shape[0])]
+
+        # Utterance-level cepstral mean and variance normalization
+        if self.do_ceptral_normalize:
+            input_features = padded_inputs["input_features"]
+
+            # make sure list is in array format
+            if isinstance(input_features[0], list):
+                input_features = [np.asarray(feature, dtype=np.float32) for feature in input_features]
+
+            padded_inputs["input_features"] = self.normalize(input_features, input_lengths=input_lengths)
+
+        if return_tensors is not None:
+            padded_inputs = padded_inputs.convert_to_tensors(return_tensors)
 
         return padded_inputs

src/transformers/models/wav2vec2/feature_extraction_wav2vec2.py
@@ -79,11 +79,15 @@ class Wav2Vec2FeatureExtractor(SequenceFeatureExtractor):
         self.do_normalize = do_normalize
 
     @staticmethod
-    def zero_mean_unit_var_norm(input_values: List[np.ndarray]) -> List[np.ndarray]:
+    def zero_mean_unit_var_norm(input_values: List[np.ndarray], input_lengths: List[int]) -> List[np.ndarray]:
         """
         Every array in the list is normalized to have zero mean and unit variance
         """
-        return [(x - np.mean(x)) / np.sqrt(np.var(x) + 1e-5) for x in input_values]
+        normed_input_values = [
+            (x - np.mean(x[:i], dtype=np.float32)) / np.sqrt(np.var(x[:i], dtype=np.float32) + 1e-5)
+            for x, i in zip(input_values, input_lengths)
+        ]
+        return normed_input_values
 
     def __call__(
         self,
@@ -171,17 +175,13 @@ class Wav2Vec2FeatureExtractor(SequenceFeatureExtractor):
 
         # make sure input is in list format
         if is_batched and not isinstance(raw_speech[0], np.ndarray):
-            raw_speech = [np.asarray(speech) for speech in raw_speech]
+            raw_speech = [np.asarray(speech, dtype=np.float32) for speech in raw_speech]
         elif not is_batched and not isinstance(raw_speech, np.ndarray):
-            raw_speech = np.asarray(raw_speech)
+            raw_speech = np.asarray(raw_speech, dtype=np.float32)
 
         # always return batch
         if not is_batched:
             raw_speech = [raw_speech]
-
-        # zero-mean and unit-variance normalization
-        if self.do_normalize:
-            raw_speech = self.zero_mean_unit_var_norm(raw_speech)
 
         # convert into correct format for padding
         encoded_inputs = BatchFeature({"input_values": raw_speech})
@@ -193,7 +193,21 @@ class Wav2Vec2FeatureExtractor(SequenceFeatureExtractor):
             truncation=truncation,
             pad_to_multiple_of=pad_to_multiple_of,
             return_attention_mask=return_attention_mask,
-            return_tensors=return_tensors,
         )
+
+        if "attention_mask" in padded_inputs:
+            input_lengths = padded_inputs["attention_mask"].sum(-1)
+        else:
+            padded_input_values = padded_inputs["input_values"]
+            input_lengths = [padded_input_values.shape[-1] for _ in range(padded_input_values.shape[0])]
+
+        # zero-mean and unit-variance normalization
+        if self.do_normalize:
+            padded_inputs["input_values"] = self.zero_mean_unit_var_norm(
+                padded_inputs["input_values"], input_lengths=input_lengths
+            )
+
+        if return_tensors is not None:
+            padded_inputs = padded_inputs.convert_to_tensors(return_tensors)
 
         return padded_inputs

tests/test_feature_extraction_wav2vec2.py
@@ -123,7 +123,7 @@ class Wav2Vec2FeatureExtractionTest(SequenceFeatureExtractionTestMixin, unittest
     def test_zero_mean_unit_variance_normalization(self):
         feat_extract = self.feature_extraction_class(**self.feat_extract_tester.prepare_feat_extract_dict())
         speech_inputs = [floats_list((1, x))[0] for x in range(800, 1400, 200)]
-        processed = feat_extract(speech_inputs, padding="longest")
+        processed = feat_extract(speech_inputs, padding="longest", return_tensors="np")
         input_values = processed.input_values
 
         def _check_zero_mean_unit_variance(input_vector):
@@ -132,6 +132,22 @@ class Wav2Vec2FeatureExtractionTest(SequenceFeatureExtractionTestMixin, unittest
 
         _check_zero_mean_unit_variance(input_values[0, :800])
         _check_zero_mean_unit_variance(input_values[1, :1000])
+        _check_zero_mean_unit_variance(input_values[2])
+
+    def test_zero_mean_unit_variance_normalization_trunc(self):
+        feat_extract = self.feature_extraction_class(**self.feat_extract_tester.prepare_feat_extract_dict())
+        speech_inputs = [floats_list((1, x))[0] for x in range(800, 1400, 200)]
+        processed = feat_extract(
+            speech_inputs, truncation=True, max_length=1000, padding="max_length", return_tensors="np"
+        )
+        input_values = processed.input_values
+
+        def _check_zero_mean_unit_variance(input_vector):
+            self.assertTrue(np.abs(np.mean(input_vector)) < 1e-3)
+            self.assertTrue(np.abs(np.var(input_vector) - 1) < 1e-3)
+
+        _check_zero_mean_unit_variance(input_values[0, :800])
+        _check_zero_mean_unit_variance(input_values[1])
         _check_zero_mean_unit_variance(input_values[2])
 
     @slow