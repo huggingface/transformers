d34fb772d463d4edf41f336eee1050861e74d1a4
cchen-dialpad <47165889+cchen-dialpad@users.noreply.github.com>
2021-07-27 05:37:46 -0700

    `Seq2SeqTrainer` set max_length and num_beams only when non None  (#12899)
    * set max_length and num_beams only when non None
    
    * fix instance variables
    
    * fix code style

--- src/transformers/trainer_seq2seq.py
+++ src/transformers/trainer_seq2seq.py
@@ -70,8 +70,10 @@ class Seq2SeqTrainer(Trainer):
             A dictionary containing the evaluation loss and the potential metrics computed from the predictions. The
             dictionary also contains the epoch number which comes from the training state.
         """
-        self._max_length = max_length
-        self._num_beams = num_beams
+        if max_length is not None or not hasattr(self, "_max_length"):
+            self._max_length = max_length
+        if num_beams is not None or not hasattr(self, "_num_beams"):
+            self._num_beams = num_beams
         return super().evaluate(eval_dataset, ignore_keys=ignore_keys, metric_key_prefix=metric_key_prefix)
 
     def predict(
@@ -117,8 +119,10 @@ class Seq2SeqTrainer(Trainer):
             - metrics (:obj:`Dict[str, float]`, `optional`): The potential dictionary of metrics (if the dataset
               contained labels).
         """
-        self._max_length = max_length
-        self._num_beams = num_beams
+        if max_length is not None or not hasattr(self, "_max_length"):
+            self._max_length = max_length
+        if num_beams is not None or not hasattr(self, "_num_beams"):
+            self._num_beams = num_beams
         return super().predict(test_dataset, ignore_keys=ignore_keys, metric_key_prefix=metric_key_prefix)
 
     def prediction_step(
