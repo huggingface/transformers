da779de39b6f751cd83c7a1824bfddf75f02606d
Ori Ram <ramori9@gmail.com>
2021-08-02 15:16:50 +0300

    Update src/transformers/models/splinter/modeling_splinter.py
    Co-authored-by: Sylvain Gugger <35901082+sgugger@users.noreply.github.com>

--- src/transformers/models/splinter/modeling_splinter.py
+++ src/transformers/models/splinter/modeling_splinter.py
@@ -639,7 +639,7 @@ SPLINTER_INPUTS_DOCSTRING = r"""
             - 1 indicates the head is **not masked**,
             - 0 indicates the head is **masked**.
 
-        inputs_embeds (:obj:`torch.FloatTensor` of shape :obj:`(batch_size, sequence_length, hidden_size)`, `optional`):
+        inputs_embeds (:obj:`torch.FloatTensor` of shape :obj:`({0}, hidden_size)`, `optional`):
             Optionally, instead of passing :obj:`input_ids` you can choose to directly pass an embedded representation.
             This is useful if you want more control over how to convert `input_ids` indices into associated vectors
             than the model's internal embedding lookup matrix.
