a6e8645cf74840f4aa1ed2ff2886b9b6bd0cec6e
PhilipMay <philip@may.la>
2021-08-02 18:06:49 +0200

    fix #12970

--- src/transformers/trainer.py
+++ src/transformers/trainer.py
@@ -1005,6 +1005,8 @@ class Trainer:
             kwargs:
                 Additional keyword arguments used to hide deprecated arguments
         """
+        # fix for https://github.com/huggingface/transformers/issues/12970
+        resume_from_checkpoint = None if not resume_from_checkpoint else resume_from_checkpoint
 
         # memory metrics - must set up as early as possible
         self._memory_tracker.start()
