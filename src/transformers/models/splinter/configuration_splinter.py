f3f92ad01929984cce57055f836f5a7e1fa41cfd
Ori Ram <ramori9@gmail.com>
2021-08-02 15:08:01 +0300

    Remove references to TF modeling from splinter

--- src/transformers/models/splinter/configuration_splinter.py
+++ src/transformers/models/splinter/configuration_splinter.py
@@ -41,8 +41,7 @@ class SplinterConfig(PretrainedConfig):
     Args:
         vocab_size (:obj:`int`, `optional`, defaults to 30522):
             Vocabulary size of the Splinter model. Defines the number of different tokens that can be represented by
-            the :obj:`inputs_ids` passed when calling :class:`~transformers.SplinterModel` or
-            :class:`~transformers.TFSplinterModel`.
+            the :obj:`inputs_ids` passed when calling :class:`~transformers.SplinterModel`.
         hidden_size (:obj:`int`, `optional`, defaults to 768):
             Dimension of the encoder layers and the pooler layer.
         num_hidden_layers (:obj:`int`, `optional`, defaults to 12):
@@ -62,8 +61,7 @@ class SplinterConfig(PretrainedConfig):
             The maximum sequence length that this model might ever be used with. Typically set this to something large
             just in case (e.g., 512 or 1024 or 2048).
         type_vocab_size (:obj:`int`, `optional`, defaults to 2):
-            The vocabulary size of the :obj:`token_type_ids` passed when calling :class:`~transformers.SplinterModel`
-            or :class:`~transformers.TFSplinterModel`.
+            The vocabulary size of the :obj:`token_type_ids` passed when calling :class:`~transformers.SplinterModel`.
         initializer_range (:obj:`float`, `optional`, defaults to 0.02):
             The standard deviation of the truncated_normal_initializer for initializing all weight matrices.
         layer_norm_eps (:obj:`float`, `optional`, defaults to 1e-12):
