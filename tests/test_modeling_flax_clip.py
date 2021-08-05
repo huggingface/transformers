5adbc39dc745b30ac6988014ba6e16732ef7fe31
Patrick von Platen <patrick.v.platen@gmail.com>
2021-08-05 18:38:50 +0200

    [Flax] Correct pt to flax conversion if from base to head (#13006)
    * finish PR
    
    * add tests
    
    * correct tests
    
    * finish
    
    * correct other flax tests
    
    * better naming
    
    * correct naming
    
    * finish
    
    * apply sylvains suggestions

--- tests/test_modeling_flax_clip.py
+++ tests/test_modeling_flax_clip.py
@@ -213,7 +213,18 @@ class FlaxCLIPVisionModelTest(FlaxModelTesterMixin, unittest.TestCase):
     def test_save_load_from_base(self):
         pass
 
+    # FlaxCLIPVisionModel does not have any base model
     def test_save_load_to_base(self):
+        pass
+
+    # FlaxCLIPVisionModel does not have any base model
+    @is_pt_flax_cross_test
+    def test_save_load_from_base_pt(self):
+        pass
+
+    # FlaxCLIPVisionModel does not have any base model
+    @is_pt_flax_cross_test
+    def test_save_load_to_base_pt(self):
         pass
 
     @slow
@@ -307,7 +318,18 @@ class FlaxCLIPTextModelTest(FlaxModelTesterMixin, unittest.TestCase):
     def test_save_load_from_base(self):
         pass
 
+    # FlaxCLIPVisionModel does not have any base model
     def test_save_load_to_base(self):
+        pass
+
+    # FlaxCLIPVisionModel does not have any base model
+    @is_pt_flax_cross_test
+    def test_save_load_from_base_pt(self):
+        pass
+
+    # FlaxCLIPVisionModel does not have any base model
+    @is_pt_flax_cross_test
+    def test_save_load_to_base_pt(self):
         pass
 
     @slow
