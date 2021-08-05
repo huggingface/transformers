98defb38590c26992b44405634fbe780612de303
patil-suraj <surajp815@gmail.com>
2021-08-05 14:09:01 +0530

    copyright

--- src/transformers/models/gptj/modeling_gptj.py
+++ src/transformers/models/gptj/modeling_gptj.py
@@ -1,5 +1,5 @@
 # coding=utf-8
-# Copyright 2021 The Eleuther AI and HuggingFace Inc. team. All rights reserved.
+# Copyright 2021 The mesh-transformer-jax authors, The Eleuther AI and HuggingFace Inc. team. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
