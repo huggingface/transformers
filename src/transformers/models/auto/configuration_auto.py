e9860e9c043b6ebf57a0e705044e9ec9ba2263bb
Stella Biderman <stellabiderman@gmail.com>
2021-08-05 16:21:44 -0400

    Update configuration_auto.py

--- src/transformers/models/auto/configuration_auto.py
+++ src/transformers/models/auto/configuration_auto.py
@@ -204,6 +204,7 @@ CONFIG_MAPPING = OrderedDict(
         ("mobilebert", MobileBertConfig),
         ("mpnet", MPNetConfig),
         ("mt5", MT5Config),
+        ("openai-gpt", OpenAIGPTConfig),
         ("pegasus", PegasusConfig),
         ("prophetnet", ProphetNetConfig),
         ("rag", RagConfig),
