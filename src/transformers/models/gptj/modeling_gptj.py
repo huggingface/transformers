f189b6d7d67eebb944961a70999c96cee16b79e8
Stella Biderman <stellabiderman@gmail.com>
2021-08-06 15:07:45 -0400

    Update src/transformers/models/gptj/modeling_gptj.py
    Co-authored-by: Suraj Patil <surajp815@gmail.com>

--- src/transformers/models/gptj/modeling_gptj.py
+++ src/transformers/models/gptj/modeling_gptj.py
@@ -811,9 +811,7 @@ class GPTJForCausalLM(GPTJPreTrainedModel):
         return self.lm_head
 
     def set_output_embeddings(self, new_embeddings):
-        if self.jax:
-            return
-        self.lm_head = new_embeddings
+        return
 
     def prepare_inputs_for_generation(self, input_ids, past=None, **kwargs):
         token_type_ids = kwargs.get("token_type_ids", None)
