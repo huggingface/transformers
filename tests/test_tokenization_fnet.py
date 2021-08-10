2bbf01a31ed2d33e3e50eaf08c623ce8569d39f5
Gunjan Chhablani <chhablani.gunjan@gmail.com>
2021-08-10 09:45:54 +0530

    Fix style

--- tests/test_tokenization_fnet.py
+++ tests/test_tokenization_fnet.py
@@ -17,7 +17,7 @@ import os
 import unittest
 
 from transformers import FNetTokenizer, FNetTokenizerFast
-from transformers.testing_utils import require_sentencepiece, require_tokenizers, slow
+from transformers.testing_utils import require_sentencepiece, require_tokenizers  # , slow
 
 from .test_tokenization_common import TokenizerTesterMixin
 
