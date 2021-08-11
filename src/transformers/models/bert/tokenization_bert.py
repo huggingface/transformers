0907c9b0eaa4c40c96fc9417b916c47521ff5636
Long Phan <44376091+justinphan3110@users.noreply.github.com>
2021-08-11 22:51:39 +0700

    add CellBertTokenizer

--- src/transformers/models/bert/tokenization_bert.py
+++ src/transformers/models/bert/tokenization_bert.py
@@ -553,3 +553,37 @@ class WordpieceTokenizer(object):
             else:
                 output_tokens.extend(sub_tokens)
         return output_tokens
+
+
+class CellBertTokenizer(BertTokenizer):
+    def __init__(
+        self,
+        vocab_file,
+        do_lower_case=True,
+        do_basic_tokenize=True,
+        never_split=None,
+        unk_token="[UNK]",
+        sep_token="[SEP]",
+        pad_token="[PAD]",
+        cls_token="[CLS]",
+        mask_token="[MASK]",
+        tokenize_chinese_chars=True,
+        strip_accents=None,
+        **kwargs
+    ):
+        super().__init__(
+            do_lower_case=do_lower_case,
+            do_basic_tokenize=do_basic_tokenize,
+            never_split=never_split,
+            unk_token=unk_token,
+            sep_token=sep_token,
+            pad_token=pad_token,
+            cls_token=cls_token,
+            mask_token=mask_token,
+            tokenize_chinese_chars=tokenize_chinese_chars,
+            strip_accents=strip_accents,
+            **kwargs,
+        )
+    
+    def _tokenize(self, text):
+        return text.split()
