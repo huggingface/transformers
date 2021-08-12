d59bab189b4339597ececa9190dc7946bd992056
patil-suraj <surajp815@gmail.com>
2021-08-12 16:57:35 +0530

    quality

--- tests/test_modeling_flax_common.py
+++ tests/test_modeling_flax_common.py
@@ -23,7 +23,6 @@ import numpy as np
 
 import transformers
 from huggingface_hub import HfApi
-from jax._src.dtypes import dtype
 from requests.exceptions import HTTPError
 from transformers import BertConfig, is_flax_available, is_torch_available
 from transformers.models.auto import get_values
