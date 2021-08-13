0eac3c58eb74ff50fdaac6c28717858ac844b09e
Gunjan Chhablani <chhablani.gunjan@gmail.com>
2021-08-14 00:29:32 +0530

    Remove bos and eos tokens from tokenizers

--- src/transformers/models/fnet/tokenization_fnet_fast.py
+++ src/transformers/models/fnet/tokenization_fnet_fast.py
@@ -70,20 +70,6 @@ class FNetTokenizerFast(PreTrainedTokenizerFast):
             Whether or not to strip the text when tokenizing (removing excess spaces before and after the string).
         keep_accents (:obj:`bool`, `optional`, defaults to :obj:`False`):
             Whether or not to keep accents when tokenizing.
-        bos_token (:obj:`str`, `optional`, defaults to :obj:`None`):
-            The beginning of sequence token that was used during pretraining. Can be used a sequence classifier token.
-
-            .. note::
-
-            When building a sequence using special tokens, this is not the token that is used for the beginning of
-            sequence. The token used is the :obj:`cls_token`.
-        eos_token (:obj:`str`, `optional`, defaults to :obj:`None`):
-            The end of sequence token.
-
-            .. note::
-
-            When building a sequence using special tokens, this is not the token that is used for the end of sequence.
-            The token used is the :obj:`sep_token`.
         unk_token (:obj:`str`, `optional`, defaults to :obj:`"<unk>"`):
             The unknown token. A token that is not in the vocabulary cannot be converted to an ID and is set to be this
             token instead.
@@ -113,8 +99,6 @@ class FNetTokenizerFast(PreTrainedTokenizerFast):
         do_lower_case=True,
         remove_space=True,
         keep_accents=False,
-        bos_token=None,
-        eos_token=None,
         unk_token="<unk>",
         sep_token="[SEP]",
         pad_token="<pad>",
@@ -131,8 +115,6 @@ class FNetTokenizerFast(PreTrainedTokenizerFast):
             do_lower_case=do_lower_case,
             remove_space=remove_space,
             keep_accents=keep_accents,
-            bos_token=bos_token,
-            eos_token=eos_token,
             unk_token=unk_token,
             sep_token=sep_token,
             pad_token=pad_token,
