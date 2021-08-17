be1eaa1e50952aafe3e37b44e17c321f1ef7b483
Lysandre <lysandre.debut@reseau.eseo.fr>
2021-08-17 11:27:52 +0200

    Fix test

--- tests/test_modeling_layoutlmv2.py
+++ tests/test_modeling_layoutlmv2.py
@@ -21,7 +21,7 @@ from transformers.file_utils import is_detectron2_available, is_torch_available
 from transformers.testing_utils import require_detectron2, require_torch, slow, torch_device
 
 from .test_configuration_common import ConfigTester
-from .test_modeling_common import ModelTesterMixin, ids_tensor, random_attention_mask
+from .test_modeling_common import ModelTesterMixin, _config_zero_init, ids_tensor, random_attention_mask
 
 
 if is_torch_available():
@@ -398,6 +398,23 @@ class LayoutLMv2ModelTest(ModelTesterMixin, unittest.TestCase):
         for model_name in LAYOUTLMV2_PRETRAINED_MODEL_ARCHIVE_LIST[:1]:
             model = LayoutLMv2Model.from_pretrained(model_name)
             self.assertIsNotNone(model)
+
+    def test_initialization(self):
+        config, inputs_dict = self.model_tester.prepare_config_and_inputs_for_common()
+
+        configs_no_init = _config_zero_init(config)
+        for model_class in self.all_model_classes:
+            model = model_class(config=configs_no_init)
+            for name, param in model.named_parameters():
+                if "backbone" in name or "visual_segment_embedding" in name:
+                    continue
+
+                if param.requires_grad:
+                    self.assertIn(
+                        ((param.data.mean() * 1e9).round() / 1e9).item(),
+                        [0.0, 1.0],
+                        msg=f"Parameter {name} of model {model_class} seems not properly initialized",
+                    )
 
 
 def prepare_layoutlmv2_batch_inputs():
