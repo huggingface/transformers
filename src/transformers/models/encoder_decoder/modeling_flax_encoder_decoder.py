d617f0e3f555c7fae9bb83d2d377a1bb7f5750a4
Yih-Dar <2521628+ydshieh@users.noreply.github.com>
2021-08-18 18:34:43 +0200

    Apply suggestions from code review
    Co-authored-by: Patrick von Platen <patrick.v.platen@gmail.com>

--- src/transformers/models/encoder_decoder/modeling_flax_encoder_decoder.py
+++ src/transformers/models/encoder_decoder/modeling_flax_encoder_decoder.py
@@ -1,5 +1,5 @@
 # coding=utf-8
-# Copyright 2020 The HuggingFace Inc. team.
+# Copyright 2021 The HuggingFace Inc. team.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,7 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-""" Classes to support Encoder-Decoder architectures """
+""" Classes to support Flax Encoder-Decoder architectures """
 
 
 import os
