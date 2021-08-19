bb3389d241d4b2ed8e98ca61a625701e76739b83
Ambesh Shekhar <ambesh.sinha@gmail.com>
2021-08-19 22:55:35 +0530

    Changing from BaseException to Exception

--- src/transformers/custom_exceptions.py
+++ src/transformers/custom_exceptions.py
@@ -3,7 +3,7 @@
 
 import os, sys
 
-class BatchSizeError(BaseException):
+class BatchSizeError(Exception):
     def __init__(self, *args: object) -> None:
         super(BatchSizeError,self).__init__(*args)
     
