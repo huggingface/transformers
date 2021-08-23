59cabb26e2a24d88c684467567e5b87aea73bcaa
Yih-Dar <2521628+ydshieh@users.noreply.github.com>
2021-08-23 17:57:29 +0200

    Make Flax GPT2 working with cross attention (#13008)
    * make flax gpt2 working with cross attention
    
    * Remove encoder->decoder projection layer
    
    * A draft (incomplete) for FlaxEncoderDecoderModel
    
    * Add the method from_encoder_decoder_pretrained + the docstrings
    
    * Fix the mistakes of using EncoderDecoderModel
    
    * Fix style
    
    * Add FlaxEncoderDecoderModel to the library
    
    * Fix cyclic imports
    
    * Add FlaxEncoderDecoderModel to modeling_flax_auto.py
    
    * Remove question comments
    
    * add tests for FlaxEncoderDecoderModel
    
    * add flax_encoder_decoder to the lists of ignored entries in check_repo.py
    
    * fix missing required positional arguments
    
    * Remove **kwargs when creating FlaxEncoderDecoderModel in from_encoder_decoder_pretrained()
    
    Also fix generation eos/pad tokens issue
    
    * Fix: Use sequences from the generated_output
    
    * Change a check from assert to raise ValueError
    
    * Fix examples and token ids issues
    
    * Fix missing all_cross_attentions when outputting tuple in modeling_gpt2
    
    * Remove the changes in configuration docstrings.
    
    * allow for bert 2 gpt2
    
    * make fix-copies
    
    * Apply suggestions from code review
    
    Co-authored-by: Patrick von Platen <patrick.v.platen@gmail.com>
    
    * Change remaining examples to bert2gpt2
    
    * Change the test to Bert2GPT2
    
    * Fix examples
    
    * Fix import
    
    * Fix unpack bug
    
    * Rename to FlaxEncoderDecoderModelTest and change the test to bert2gpt2
    
    * Apply suggestions from code review
    
    Co-authored-by: Patrick von Platen <patrick.v.platen@gmail.com>
    
    * Fix: NotImplentedError -> NotImplementedError
    
    * Apply suggestions from code review
    
    Co-authored-by: Patrick von Platen <patrick.v.platen@gmail.com>
    
    * up
    
    * finalize
    
    Co-authored-by: ydshieh <ydshieh@user.noreply>
    Co-authored-by: ydshieh <ydshieh@users.noreply.github.com>
    Co-authored-by: Patrick von Platen <patrick.v.platen@gmail.com>

--- tests/test_modeling_encoder_decoder.py
+++ tests/test_modeling_encoder_decoder.py
@@ -55,13 +55,13 @@ if is_torch_available():
 @require_torch
 class EncoderDecoderMixin:
     def get_encoder_decoder_model(self, config, decoder_config):
-        pass
+        raise NotImplementedError
 
     def prepare_config_and_inputs(self):
-        pass
+        raise NotImplementedError
 
     def get_pretrained_model(self):
-        pass
+        raise NotImplementedError
 
     def check_encoder_decoder_model_from_pretrained_configs(
         self,
@@ -775,6 +775,24 @@ class GPT2EncoderDecoderModelTest(EncoderDecoderMixin, unittest.TestCase):
 
     def test_encoder_decoder_model_shared_weights(self):
         pass
+
+    @slow
+    def test_bert2gpt2_summarization(self):
+        model = EncoderDecoderModel.from_pretrained("patrickvonplaten/bert2gpt2-cnn_dailymail-fp16")
+
+        model.to(torch_device)
+        tokenizer_in = AutoTokenizer.from_pretrained("bert-base-cased")
+        tokenizer_out = AutoTokenizer.from_pretrained("gpt2")
+
+        ARTICLE_STUDENTS = """(CNN)Sigma Alpha Epsilon is under fire for a video showing party-bound fraternity members singing a racist chant. SAE's national chapter suspended the students, but University of Oklahoma President David Boren took it a step further, saying the university's affiliation with the fraternity is permanently done. The news is shocking, but it's not the first time SAE has faced controversy. SAE was founded March 9, 1856, at the University of Alabama, five years before the American Civil War, according to the fraternity website. When the war began, the group had fewer than 400 members, of which "369 went to war for the Confederate States and seven for the Union Army," the website says. The fraternity now boasts more than 200,000 living alumni, along with about 15,000 undergraduates populating 219 chapters and 20 "colonies" seeking full membership at universities. SAE has had to work hard to change recently after a string of member deaths, many blamed on the hazing of new recruits, SAE national President Bradley Cohen wrote in a message on the fraternity's website. The fraternity's website lists more than 130 chapters cited or suspended for "health and safety incidents" since 2010. At least 30 of the incidents involved hazing, and dozens more involved alcohol. However, the list is missing numerous incidents from recent months. Among them, according to various media outlets: Yale University banned the SAEs from campus activities last month after members allegedly tried to interfere with a sexual misconduct investigation connected to an initiation rite. Stanford University in December suspended SAE housing privileges after finding sorority members attending a fraternity function were subjected to graphic sexual content. And Johns Hopkins University in November suspended the fraternity for underage drinking. "The media has labeled us as the 'nation's deadliest fraternity,' " Cohen said. In 2011, for example, a student died while being coerced into excessive alcohol consumption, according to a lawsuit. SAE's previous insurer dumped the fraternity. "As a result, we are paying Lloyd's of London the highest insurance rates in the Greek-letter world," Cohen said. Universities have turned down SAE's attempts to open new chapters, and the fraternity had to close 12 in 18 months over hazing incidents."""
+
+        EXPECTED_SUMMARY_STUDENTS = """SAS Alpha Epsilon suspended the students, but university president says it's permanent.\nThe fraternity has had to deal with a string of student deaths since 2010.\nSAS has more than 200,000 members, many of whom are students.\nA student died while being forced into excessive alcohol consumption."""
+
+        input_dict = tokenizer_in(ARTICLE_STUDENTS, return_tensors="pt")
+        output_ids = model.generate(input_dict["input_ids"].to(torch_device))
+        summary = tokenizer_out.batch_decode(output_ids, skip_special_tokens=True)
+
+        self.assertEqual(summary, [EXPECTED_SUMMARY_STUDENTS])
 
 
 @require_torch
