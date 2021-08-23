9b2875633e28e09c81e58c94c953cb24cce9ac00
ydshieh <ydshieh@users.noreply.github.com>
2021-08-23 13:11:12 +0200

    make style

--- src/transformers/models/layoutlm/modeling_tf_layoutlm.py
+++ src/transformers/models/layoutlm/modeling_tf_layoutlm.py
@@ -22,11 +22,7 @@ import numpy as np
 import tensorflow as tf
 
 from ...activations_tf import get_tf_activation
-from ...file_utils import (
-    add_start_docstrings,
-    add_start_docstrings_to_model_forward,
-    replace_return_docstrings,
-)
+from ...file_utils import add_start_docstrings, add_start_docstrings_to_model_forward, replace_return_docstrings
 from ...modeling_tf_outputs import (
     TFBaseModelOutputWithPastAndCrossAttentions,
     TFBaseModelOutputWithPoolingAndCrossAttentions,
