65f6c3899320765ea9d45ff6e228fb6a54cda20e
Bram Vanroy <Bram.Vanroy@UGent.be>
2021-08-26 10:32:57 +0200

    Add error message concerning revision (#13266)
    * add error message concerning revision
    
    * Update src/transformers/configuration_utils.py
    
    Co-authored-by: Lysandre Debut <lysandre@huggingface.co>
    
    * re-add double line endings
    
    * is not None instead of implicit bool casting
    
    Co-authored-by: Lysandre Debut <lysandre@huggingface.co>

--- src/transformers/modeling_utils.py
+++ src/transformers/modeling_utils.py
@@ -1265,8 +1265,12 @@ class PreTrainedModel(nn.Module, ModuleUtilsMixin, GenerationMixin, PushToHubMix
                 msg = (
                     f"Can't load weights for '{pretrained_model_name_or_path}'. Make sure that:\n\n"
                     f"- '{pretrained_model_name_or_path}' is a correct model identifier listed on 'https://huggingface.co/models'\n\n"
-                    f"- or '{pretrained_model_name_or_path}' is the correct path to a directory containing a file named one of {WEIGHTS_NAME}, {TF2_WEIGHTS_NAME}, {TF_WEIGHTS_NAME}.\n\n"
+                    f"- or '{pretrained_model_name_or_path}' is the correct path to a directory containing a file named one of {WEIGHTS_NAME}, {TF2_WEIGHTS_NAME}, {TF_WEIGHTS_NAME}\n\n"
                 )
+
+                if revision is not None:
+                    msg += f"- or '{revision}' is a valid git identifier (branch name, a tag name, or a commit id) that exists for this model name as listed on its model page on 'https://huggingface.co/models'\n\n"
+
                 raise EnvironmentError(msg)
 
             if resolved_archive_file == archive_file:
