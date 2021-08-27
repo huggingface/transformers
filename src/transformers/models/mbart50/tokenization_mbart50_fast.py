612f3a02c3a0cbb1f08af0fcff77d3ada80c5dcd
Nicolas Patry <patry.nicolas@protonmail.com>
2021-08-27 17:22:06 +0200

    Fixing mbart50 with `return_tensors` argument too. (#13301)
    * Fixing mbart50 with `return_tensors` argument too.
    
    * Adding mbart50 tokenization tests.

--- src/transformers/models/mbart50/tokenization_mbart50_fast.py
+++ src/transformers/models/mbart50/tokenization_mbart50_fast.py
@@ -245,12 +245,14 @@ class MBart50TokenizerFast(PreTrainedTokenizerFast):
             special_tokens=list(zip(prefix_tokens_str + suffix_tokens_str, self.prefix_tokens + self.suffix_tokens)),
         )
 
-    def _build_translation_inputs(self, raw_inputs, src_lang: Optional[str], tgt_lang: Optional[str], **extra_kwargs):
+    def _build_translation_inputs(
+        self, raw_inputs, return_tensors: str, src_lang: Optional[str], tgt_lang: Optional[str], **extra_kwargs
+    ):
         """Used by translation pipeline, to prepare inputs for the generate function"""
         if src_lang is None or tgt_lang is None:
             raise ValueError("Translation requires a `src_lang` and a `tgt_lang` for this model")
         self.src_lang = src_lang
-        inputs = self(raw_inputs, add_special_tokens=True, return_tensors="pt", **extra_kwargs)
+        inputs = self(raw_inputs, add_special_tokens=True, return_tensors=return_tensors, **extra_kwargs)
         tgt_lang_id = self.convert_tokens_to_ids(tgt_lang)
         inputs["forced_bos_token_id"] = tgt_lang_id
         return inputs
