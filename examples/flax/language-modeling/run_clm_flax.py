ca57527068f8df0dce0fbd39b2a3b83dff69e021
Stefan Schweter <stefan@schweter.it>
2021-08-28 16:22:29 +0200

    examples: only use keep_linebreaks when reading TXT files (#13320)
    * examples: only use keep_linebreaks when reading TXT files for all CLM examples
    
    * examples: only use keep_linebreaks when reading TXT files for all CLM examples
    
    * examples: only use keep_linebreaks when reading TXT files for all CLM examples

--- examples/flax/language-modeling/run_clm_flax.py
+++ examples/flax/language-modeling/run_clm_flax.py
@@ -157,7 +157,7 @@ class DataTrainingArguments:
         metadata={"help": "The number of processes to use for the preprocessing."},
     )
     keep_linebreaks: bool = field(
-        default=True, metadata={"help": "Whether to keep line breaks when using CSV/JSON/TXT files or not."}
+        default=True, metadata={"help": "Whether to keep line breaks when using TXT files or not."}
     )
 
     def __post_init__(self):
@@ -305,6 +305,7 @@ def main():
             )
     else:
         data_files = {}
+        dataset_args = {}
         if data_args.train_file is not None:
             data_files["train"] = data_args.train_file
         if data_args.validation_file is not None:
@@ -312,22 +313,23 @@ def main():
         extension = data_args.train_file.split(".")[-1]
         if extension == "txt":
             extension = "text"
-        dataset = load_dataset(extension, data_files=data_files, cache_dir=model_args.cache_dir)
+            dataset_args["keep_linebreaks"] = data_args.keep_linebreaks
+        dataset = load_dataset(extension, data_files=data_files, cache_dir=model_args.cache_dir, **dataset_args)
 
         if "validation" not in dataset.keys():
             dataset["validation"] = load_dataset(
                 extension,
-                keep_linebreaks=data_args.keep_linebreaks,
                 data_files=data_files,
                 split=f"train[:{data_args.validation_split_percentage}%]",
                 cache_dir=model_args.cache_dir,
+                **dataset_args,
             )
             dataset["train"] = load_dataset(
                 extension,
-                keep_linebreaks=data_args.keep_linebreaks,
                 data_files=data_files,
                 split=f"train[{data_args.validation_split_percentage}%:]",
                 cache_dir=model_args.cache_dir,
+                **dataset_args,
             )
     # See more about loading any type of standard or custom dataset (from files, python dict, pandas DataFrame, etc) at
     # https://huggingface.co/docs/datasets/loading_datasets.html.
