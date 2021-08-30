fa064ed441c4c55ca640b48e7c1aca79b74fe892
Stas Bekman <stas@stason.org>
2021-08-30 09:21:14 -0700

    style

--- src/transformers/trainer.py
+++ src/transformers/trainer.py
@@ -817,7 +817,8 @@ class Trainer:
 
     def create_scheduler(self, num_training_steps: int, optimizer: torch.optim.Optimizer = None):
         """
-        Setup the scheduler. The optimizer of the trainer must have been set up either before this method is called or passed as an argument.
+        Setup the scheduler. The optimizer of the trainer must have been set up either before this method is called or
+        passed as an argument.
 
         Args:
             num_training_steps (int): The number of training steps to do.
