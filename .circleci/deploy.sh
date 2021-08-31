80fcd60f14ef935a32d3b8b8d979a3b9f7821fd6
Lysandre <lysandre.debut@reseau.eseo.fr>
2021-08-31 16:02:31 +0200

    Docs for v4.10.0

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -70,4 +70,5 @@ deploy_doc "1366172" v4.8.1
 deploy_doc "96d1cfb" v4.8.2
 deploy_doc "72aee83" v4.9.0
 deploy_doc "bff1c71" v4.9.1
-deploy_doc "41981a2"  # v4.9.2 Latest stable release
+deploy_doc "41981a2" v4.9.2
+deploy_doc "d12bbe4"  # v4.10.0 Latest stable release
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,7 +27,8 @@ author = "huggingface"
 # The short X.Y version
 version = ""
 # The full version, including alpha/beta/rc tags
-release = "4.10.0"
+release = "4.11.0.dev0"
+
 
 
 
