39208178b5d158cc7425208a7a60908a4d34de94
Jonathan Chang <ttjtftx@gmail.com>
2021-09-01 15:49:03 +0800

    Add template for adding flax models (#12441)
    * Add option to add flax
    
    * Add flax template for __init__.py
    
    * Add flax template for .rst
    
    * Copy TF modeling template
    
    * Add a missing line in modeling_tf_... template
    
    * Update first half of modeling_flax_..
    
    * Update encoder flax template
    
    * Copy test_modeling_tf... as test_modeling_flax...
    
    * Replace some TF to Flax in test_modeling_flax_...
    
    * Replace tf to np
    
    some function might not work, like _assert_tensors_equal
    
    * Replace remaining tf to np (might not work)
    
    * Fix cookiecutter
    
    * Add Flax in to_replace_... template
    
    * Update transformers-cli add-new-model
    
    * Save generate_flax in configuration.json
    
    This will be read by transformers-cli
    
    * Fix to_replace_... and cli
    
    * Fix replace cli
    
    * Fix cookiecutter name
    
    * Move docstring earlier to avoid not defined error
    
    * Fix a missing Module
    
    * Add encoder-decoder flax template from bart
    
    * Fix flax test
    
    * Make style
    
    * Fix endif
    
    * Fix replace all "utf-8 -> unp-8"
    
    * Update comment
    
    * Fix flax template (add missing ..._DOCSTRING)
    
    * Use flax_bart imports in template (was t5)
    
    * Fix unp
    
    * Update templates/adding_a_new_model/tests
    
    * Revert "Fix unp"
    
    This reverts commit dc9002a41d902c4f9b07343eab1cb350c8b7fd57.
    
    * Remove one line of copied from to suppress CI error
    
    * Use generate_tensorflow_pytorch_and_flax
    
    * Add a missing part
    
    * fix typo
    
    * fix flax config
    
    * add examples for flax
    
    * small rename
    
    * correct modeling imports
    
    * correct auto loading
    
    * corrects some flax tests
    
    * correct small typo
    
    * correct as type
    
    * finish modif
    
    * correct more templates
    
    * final fixes
    
    * add file testers
    
    * up
    
    * make sure tests match template regex
    
    * correct pytorch
    
    * correct tf
    
    * correct more tf
    
    * correct imports
    
    * minor error
    
    * minor error
    
    * correct init
    
    * more fixes
    
    * correct more flax tests
    
    * correct flax test
    
    * more fixes
    
    * correct docs
    
    * update
    
    * fix
    
    Co-authored-by: Patrick von Platen <patrick.v.platen@gmail.com>

--- templates/adding_a_new_model/cookiecutter-template-{{cookiecutter.modelname}}/__init__.py
+++ templates/adding_a_new_model/cookiecutter-template-{{cookiecutter.modelname}}/__init__.py
@@ -17,13 +17,18 @@
 # limitations under the License.
 from typing import TYPE_CHECKING
 
-{%- if cookiecutter.generate_tensorflow_and_pytorch == "PyTorch & TensorFlow" %}
-from ...file_utils import _LazyModule, is_tf_available, is_torch_available, is_tokenizers_available
-{%- elif cookiecutter.generate_tensorflow_and_pytorch == "PyTorch" %}
-from ...file_utils import _LazyModule, is_torch_available, is_tokenizers_available
-{%- elif cookiecutter.generate_tensorflow_and_pytorch == "TensorFlow" %}
-from ...file_utils import _LazyModule, is_tf_available, is_tokenizers_available
+# rely on isort to merge the imports
+from ...file_utils import _LazyModule, is_tokenizers_available
+{%- if "TensorFlow" in cookiecutter.generate_tensorflow_pytorch_and_flax %}
+from ...file_utils import is_tf_available
 {% endif %}
+{%- if "PyTorch" in cookiecutter.generate_tensorflow_pytorch_and_flax %}
+from ...file_utils import is_torch_available
+{% endif %}
+{%- if "Flax" in cookiecutter.generate_tensorflow_pytorch_and_flax %}
+from ...file_utils import is_flax_available
+{% endif %}
+
 _import_structure = {
     "configuration_{{cookiecutter.lowercase_modelname}}": ["{{cookiecutter.uppercase_modelname}}_PRETRAINED_CONFIG_ARCHIVE_MAP", "{{cookiecutter.camelcase_modelname}}Config"],
     "tokenization_{{cookiecutter.lowercase_modelname}}": ["{{cookiecutter.camelcase_modelname}}Tokenizer"],
@@ -32,7 +37,7 @@ _import_structure = {
 if is_tokenizers_available():
     _import_structure["tokenization_{{cookiecutter.lowercase_modelname}}_fast"] = ["{{cookiecutter.camelcase_modelname}}TokenizerFast"]
 
-{%- if (cookiecutter.generate_tensorflow_and_pytorch == "PyTorch & TensorFlow" or cookiecutter.generate_tensorflow_and_pytorch == "PyTorch") %}
+{%- if "PyTorch" in cookiecutter.generate_tensorflow_pytorch_and_flax %}
 {% if cookiecutter.is_encoder_decoder_model == "False" %}
 if is_torch_available():
     _import_structure["modeling_{{cookiecutter.lowercase_modelname}}"] = [
@@ -61,7 +66,9 @@ if is_torch_available():
     ]
 {% endif %}
 {% endif %}
-{%- if (cookiecutter.generate_tensorflow_and_pytorch == "PyTorch & TensorFlow" or cookiecutter.generate_tensorflow_and_pytorch == "TensorFlow") %}
+
+
+{%- if "TensorFlow" in cookiecutter.generate_tensorflow_pytorch_and_flax %}
 {% if cookiecutter.is_encoder_decoder_model == "False" %}
 if is_tf_available():
     _import_structure["modeling_tf_{{cookiecutter.lowercase_modelname}}"] = [
@@ -87,6 +94,33 @@ if is_tf_available():
 {% endif %}
 
 
+{%- if "Flax" in cookiecutter.generate_tensorflow_pytorch_and_flax %}
+{% if cookiecutter.is_encoder_decoder_model == "False" %}
+if is_flax_available():
+    _import_structure["modeling_flax_{{cookiecutter.lowercase_modelname}}"] = [
+        "Flax{{cookiecutter.camelcase_modelname}}ForMaskedLM",
+        "Flax{{cookiecutter.camelcase_modelname}}ForCausalLM",
+        "Flax{{cookiecutter.camelcase_modelname}}ForMultipleChoice",
+        "Flax{{cookiecutter.camelcase_modelname}}ForQuestionAnswering",
+        "Flax{{cookiecutter.camelcase_modelname}}ForSequenceClassification",
+        "Flax{{cookiecutter.camelcase_modelname}}ForTokenClassification",
+        "Flax{{cookiecutter.camelcase_modelname}}Layer",
+        "Flax{{cookiecutter.camelcase_modelname}}Model",
+        "Flax{{cookiecutter.camelcase_modelname}}PreTrainedModel",
+    ]
+{% else %}
+if is_flax_available():
+    _import_structure["modeling_flax_{{cookiecutter.lowercase_modelname}}"] = [
+        "Flax{{cookiecutter.camelcase_modelname}}ForConditionalGeneration",
+        "Flax{{cookiecutter.camelcase_modelname}}ForQuestionAnswering",
+        "Flax{{cookiecutter.camelcase_modelname}}ForSequenceClassification",
+        "Flax{{cookiecutter.camelcase_modelname}}Model",
+        "Flax{{cookiecutter.camelcase_modelname}}PreTrainedModel",
+    ]
+{% endif %}
+{% endif %}
+
+
 if TYPE_CHECKING:
     from .configuration_{{cookiecutter.lowercase_modelname}} import {{cookiecutter.uppercase_modelname}}_PRETRAINED_CONFIG_ARCHIVE_MAP, {{cookiecutter.camelcase_modelname}}Config
     from .tokenization_{{cookiecutter.lowercase_modelname}} import {{cookiecutter.camelcase_modelname}}Tokenizer
@@ -94,7 +128,7 @@ if TYPE_CHECKING:
     if is_tokenizers_available():
         from .tokenization_{{cookiecutter.lowercase_modelname}}_fast import {{cookiecutter.camelcase_modelname}}TokenizerFast
 
-{%- if (cookiecutter.generate_tensorflow_and_pytorch == "PyTorch & TensorFlow" or cookiecutter.generate_tensorflow_and_pytorch == "PyTorch") %}
+{%- if "PyTorch" in cookiecutter.generate_tensorflow_pytorch_and_flax %}
 {% if cookiecutter.is_encoder_decoder_model == "False" %}
     if is_torch_available():
         from .modeling_{{cookiecutter.lowercase_modelname}} import (
@@ -123,7 +157,7 @@ if TYPE_CHECKING:
         )
 {% endif %}
 {% endif %}
-{%- if (cookiecutter.generate_tensorflow_and_pytorch == "PyTorch & TensorFlow" or cookiecutter.generate_tensorflow_and_pytorch == "TensorFlow") %}
+{%- if "TensorFlow" in cookiecutter.generate_tensorflow_pytorch_and_flax %}
 {% if cookiecutter.is_encoder_decoder_model == "False" %}
     if is_tf_available():
         from .modeling_tf_{{cookiecutter.lowercase_modelname}} import (
@@ -147,6 +181,32 @@ if TYPE_CHECKING:
         )
 {% endif %}
 {% endif %}
+{%- if "Flax" in cookiecutter.generate_tensorflow_pytorch_and_flax %}
+{% if cookiecutter.is_encoder_decoder_model == "False" %}
+    if is_flax_available():
+        from .modeling_{{cookiecutter.lowercase_modelname}} import (
+            Flax{{cookiecutter.camelcase_modelname}}ForMaskedLM,
+            Flax{{cookiecutter.camelcase_modelname}}ForCausalLM,
+            Flax{{cookiecutter.camelcase_modelname}}ForMultipleChoice,
+            Flax{{cookiecutter.camelcase_modelname}}ForQuestionAnswering,
+            Flax{{cookiecutter.camelcase_modelname}}ForSequenceClassification,
+            Flax{{cookiecutter.camelcase_modelname}}ForTokenClassification,
+            Flax{{cookiecutter.camelcase_modelname}}Layer,
+            Flax{{cookiecutter.camelcase_modelname}}Model,
+            Flax{{cookiecutter.camelcase_modelname}}PreTrainedModel,
+        )
+{% else %}
+    if is_flax_available():
+        from .modeling_{{cookiecutter.lowercase_modelname}} import (
+            Flax{{cookiecutter.camelcase_modelname}}ForConditionalGeneration,
+            Flax{{cookiecutter.camelcase_modelname}}ForQuestionAnswering,
+            Flax{{cookiecutter.camelcase_modelname}}ForSequenceClassification,
+            Flax{{cookiecutter.camelcase_modelname}}Model,
+            Flax{{cookiecutter.camelcase_modelname}}PreTrainedModel,
+        )
+{% endif %}
+{% endif %}
+
 else:
     import sys
 
