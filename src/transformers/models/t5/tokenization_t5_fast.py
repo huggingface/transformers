c4d78f01de30152f0e7a6f39d0901b85b0fd422d
SaulLu <55560583+SaulLu@users.noreply.github.com>
2021-09-01 16:32:56 +0200

    Fix tokenizer saving during training with `Trainer` (#12806)
    * add test in trainer and test tokenizer saving wi
    th trainer
    
    * quality
    
    * reverse trainer changes
    
    * replace test in test_trainer by a test for all the tokenizers
    
    * format
    
    * add can_save_slow_tokenizer attribute to all tokenizers
    
    * fix Herbert
    
    * format
    
    * Change comment in error
    
    * add comments and a new assert
    
    * Update src/transformers/models/albert/tokenization_albert_fast.py
    
    Co-authored-by: Sylvain Gugger <35901082+sgugger@users.noreply.github.com>
    
    * change ValueError barthez
    
    * change ValueError BigBird
    
    * change ValueError Camembert
    
    * change ValueError Mbart50
    
    * change ValueError Pegasus
    
    * change ValueError ReFormer
    
    * change ValueError T5
    
    * change ValueError RoBERTa
    
    * XLNET fast
    
    * Update tests/test_tokenization_common.py
    
    Co-authored-by: Sylvain Gugger <35901082+sgugger@users.noreply.github.com>
    
    * change `assert` into `self.assertIn`
    
    * format
    
    Co-authored-by: Sylvain Gugger <35901082+sgugger@users.noreply.github.com>

--- src/transformers/models/t5/tokenization_t5_fast.py
+++ src/transformers/models/t5/tokenization_t5_fast.py
@@ -137,9 +137,16 @@ class T5TokenizerFast(PreTrainedTokenizerFast):
         )
 
         self.vocab_file = vocab_file
+        self.can_save_slow_tokenizer = False if not self.vocab_file else True
         self._extra_ids = extra_ids
 
     def save_vocabulary(self, save_directory: str, filename_prefix: Optional[str] = None) -> Tuple[str]:
+        if not self.can_save_slow_tokenizer:
+            raise ValueError(
+                "Your fast tokenizer does not have the necessary information to save the vocabulary for a slow "
+                "tokenizer."
+            )
+
         if not os.path.isdir(save_directory):
             logger.error(f"Vocabulary path ({save_directory}) should be a directory")
             return
