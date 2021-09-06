92d4ef9ab038dfbbe02556375c4c3c14215b37d2
Anurag Kumar <mailanu98@gmail.com>
2021-09-07 03:02:24 +0530

    Update setup.py (#13421)

--- setup.py
+++ setup.py
@@ -369,6 +369,8 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Topic :: Scientific/Engineering :: Artificial Intelligence",
     ],
     cmdclass={"deps_table_update": DepsTableUpdateCommand},
