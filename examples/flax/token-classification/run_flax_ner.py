8fdbe88ebb858ad809674aceead52239644a22ac
Kamal Raj <kamalraj97@gmail.com>
2021-09-08 11:50:34 +0530

    added check_min_version

--- examples/flax/token-classification/run_flax_ner.py
+++ examples/flax/token-classification/run_flax_ner.py
@@ -44,10 +44,13 @@ from transformers import (
     HfArgumentParser,
     TrainingArguments,
 )
-from transformers.utils.versions import require_version
+from transformers.utils.versions import check_min_version, require_version
 
 
 logger = logging.getLogger(__name__)
+# Will error if the minimal version of Transformers is not installed. Remove at your own risks.
+check_min_version("4.11.0.dev0")
+
 require_version("datasets>=1.8.0", "To fix: pip install -r examples/pytorch/token-classification/requirements.txt")
 
 Array = Any
