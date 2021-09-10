f3fe5f11aaf2234215c4f606c6a62e56bf162e95
Patrick von Platen <patrick.v.platen@gmail.com>
2021-09-10 23:40:41 +0200

    new masking

--- src/transformers/models/wav2vec2/modeling_wav2vec2.py
+++ src/transformers/models/wav2vec2/modeling_wav2vec2.py
@@ -120,15 +120,80 @@ class Wav2Vec2ForPreTrainingOutput(ModelOutput):
     neg_is_pos: Optional[torch.FloatTensor] = None
 
 
-def _compute_mask_indices(
+def _random_spans_noise_mask(length, noise_density, mean_noise_span_length):
+    """This function is copy of `random_spans_helper <https://github.com/google-research/text-to-text-transfer-transformer/blob/84f8bcc14b5f2c03de51bd3587609ba8f6bbd1cd/t5/data/preprocessors.py#L2682>`__ .
+
+    Noise mask consisting of random spans of noise tokens.
+    The number of noise tokens and the number of noise spans and non-noise spans
+    are determined deterministically as follows:
+    num_noise_tokens = round(length * noise_density)
+    num_nonnoise_spans = num_noise_spans = round(num_noise_tokens / mean_noise_span_length)
+    Spans alternate between non-noise and noise, beginning with non-noise.
+    Subject to the above restrictions, all masks are equally likely.
+
+    Args:
+        length: an int32 scalar (length of the incoming token sequence)
+        noise_density: a float - approximate density of output mask
+        mean_noise_span_length: a number
+
+    Returns:
+        a boolean tensor with shape [length]
+    """
+
+    orig_length = length
+
+    num_noise_tokens = int(np.round(length * noise_density))
+    # avoid degeneracy by ensuring positive numbers of noise and nonnoise tokens.
+    num_noise_tokens = min(max(num_noise_tokens, 1), length - 1)
+    num_noise_spans = int(np.round(num_noise_tokens / mean_noise_span_length))
+
+    # avoid degeneracy by ensuring positive number of noise spans
+    num_noise_spans = max(num_noise_spans, 1)
+    num_nonnoise_tokens = length - num_noise_tokens
+
+    # pick the lengths of the noise spans and the non-noise spans
+    def _random_segmentation(num_items, num_segments):
+        """Partition a sequence of items randomly into non-empty segments.
+        Args:
+            num_items: an integer scalar > 0
+            num_segments: an integer scalar in [1, num_items]
+        Returns:
+            a Tensor with shape [num_segments] containing positive integers that add
+            up to num_items
+        """
+        mask_indices = np.arange(num_items - 1) < (num_segments - 1)
+        np.random.shuffle(mask_indices)
+        first_in_segment = np.pad(mask_indices, [[1, 0]])
+        segment_id = np.cumsum(first_in_segment)
+        # count length of sub segments assuming that list is sorted
+        _, segment_length = np.unique(segment_id, return_counts=True)
+        return segment_length
+
+    noise_span_lengths = _random_segmentation(num_noise_tokens, num_noise_spans)
+    nonnoise_span_lengths = _random_segmentation(num_nonnoise_tokens, num_noise_spans)
+
+    interleaved_span_lengths = np.reshape(
+        np.stack([nonnoise_span_lengths, noise_span_lengths], axis=1), [num_noise_spans * 2]
+    )
+    span_starts = np.cumsum(interleaved_span_lengths)[:-1]
+    span_start_indicator = np.zeros((length,), dtype=np.int8)
+    span_start_indicator[span_starts] = True
+    span_num = np.cumsum(span_start_indicator)
+    is_noise = np.equal(span_num % 2, 1)
+
+    return is_noise[:orig_length]
+
+
+def _compute_mask_indices(shape, mask_prob, mask_length) -> np.ndarray:
+    return np.array([_random_spans_noise_mask(shape[-1], noise_density=mask_prob, mean_noise_span_length=mask_length) for _ in range(shape[0])], dtype=np.bool)
+
+
+def _old_2_compute_mask_indices(
     shape: Tuple[int, int],
     mask_prob: float,
     mask_length: int,
     attention_mask: Optional[torch.tensor] = None,
     min_masks: int = 0,
-    mask_other: float = 0.0,
-    no_overlap: bool = False,
-    min_space: int = 0,
 ) -> np.ndarray:
 
     """
@@ -1091,7 +1156,6 @@ class Wav2Vec2Model(Wav2Vec2PreTrainedModel):
                 mask_prob=self.config.mask_time_prob,
                 mask_length=self.config.mask_time_length,
                 attention_mask=attention_mask,
-                min_masks=2,
             )
             mask_time_indices = torch.tensor(mask_time_indices, device=self.device)
             hidden_states[mask_time_indices] = self.masked_spec_embed.to(hidden_states.dtype)
@@ -1361,12 +1425,19 @@ class Wav2Vec2ForPreTraining(Wav2Vec2PreTrainedModel):
         # 2. quantize all (unmasked) extracted features and project to final vq dim
         extract_features = self.dropout_features(outputs[1])
 
+        if attention_mask is not None:
+            # compute reduced attention_mask correponding to feature vectors
+            attention_mask = self._get_feature_vector_attention_mask(extract_features.shape[1], attention_mask)
+
         # only use masked indices
         extract_features = extract_features[mask_time_indices].view(extract_features.size(0), -1, extract_features.size(-1))
         transformer_features = transformer_features[mask_time_indices].view(transformer_features.size(0), -1, transformer_features.size(-1))
+        attention_mask = attention_mask[mask_time_indices]
+
 
 #        quantized_features, codevector_perplexity = self.quantizer(extract_features, mask_time_indices)
-        quantized_features, codevector_perplexity = self.quantizer(extract_features)
+#        quantized_features, codevector_perplexity = self.quantizer(extract_features)
+        quantized_features, codevector_perplexity = self.quantizer(extract_features, attention_mask)
         quantized_features = self.project_q(quantized_features)
 
         loss = None
@@ -1374,13 +1445,9 @@ class Wav2Vec2ForPreTraining(Wav2Vec2PreTrainedModel):
             # for training, we sample negatives
             # 3. sample K negatives (distractors) quantized states for contrastive loss
             # if attention_mask is passed, make sure that padded feature vectors cannot be sampled
-#            if attention_mask is not None:
-                # compute reduced attention_mask correponding to feature vectors
-#                attention_mask = self._get_feature_vector_attention_mask(extract_features.shape[1], attention_mask)
-
             negative_quantized_features = self._sample_negatives(
-#                quantized_features, self.config.num_negatives, attention_mask=attention_mask
-                quantized_features, self.config.num_negatives
+                quantized_features, self.config.num_negatives, attention_mask=attention_mask
+#                quantized_features, self.config.num_negatives
             )
 
             # 4. compute logits, corresponding to `logs = sim(c_t, [q_t, \sim{q}_t]) / \kappa`
@@ -1402,12 +1469,16 @@ class Wav2Vec2ForPreTraining(Wav2Vec2PreTrainedModel):
             # 6. compute contrastive loss \mathbf{L}_m = cross_entropy(logs) =
             # -log(exp(sim(c_t, q_t)/\kappa) / \sum_{\sim{q}} exp(sim(c_t, \sim{q})/\kappa))
             preds = logits.transpose(0, 2).reshape(-1, logits.size(0))
+
+            target = ((1 - attention_mask.long()) * -100).transpose(0, 1).flatten()
 #            target = ((1 - mask_time_indices.long()) * -100).transpose(0, 1).flatten()
-            target = torch.zeros(preds.shape[0], device=preds.device, dtype=torch.long)
+#            target = torch.zeros(preds.shape[0], device=preds.device, dtype=torch.long)
+
             contrastive_loss = nn.functional.cross_entropy(preds.float(), target, reduction="sum")
             # 7. compute diversity loss: \mathbf{L}_d
             num_codevectors = self.config.num_codevectors_per_group * self.config.num_codevector_groups
-            diversity_loss = ((num_codevectors - codevector_perplexity) / num_codevectors) * mask_time_indices.sum()
+#            diversity_loss = ((num_codevectors - codevector_perplexity) / num_codevectors) * mask_time_indices.sum()
+            diversity_loss = ((num_codevectors - codevector_perplexity) / num_codevectors) * attention_mask.sum()
 
             # 8. \mathbf{L} = \mathbf{L}_m + \alpha * \mathbf{L}_d
             loss = contrastive_loss + self.config.diversity_loss_weight * diversity_loss
