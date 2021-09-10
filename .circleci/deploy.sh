4bccf02958b5d3ddb3224bfdb34800f4212f3750
patrickvonplaten <patrick.v.platen@gmail.com>
2021-09-10 16:45:19 +0200

    Docs for v4.10.1

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -71,4 +71,5 @@ deploy_doc "96d1cfb" v4.8.2
 deploy_doc "72aee83" v4.9.0
 deploy_doc "bff1c71" v4.9.1
 deploy_doc "41981a2" v4.9.2
-deploy_doc "39cb6f5"  # v4.10.0 Latest stable release
+deploy_doc "39cb6f5" v4.10.0
+deploy_doc "28e2787"  # v4.10.1 Latest stable release
