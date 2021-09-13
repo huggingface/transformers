584f930081d4ae268557365788d840acf8cde8a7
Kamal Raj <kamalraj97@gmail.com>
2021-09-13 22:10:34 +0530

    updated Copyright

--- src/transformers/models/beit/modeling_flax_beit.py
+++ src/transformers/models/beit/modeling_flax_beit.py
@@ -1,5 +1,5 @@
 # coding=utf-8
-# Copyright 2021 The Google Flax Team Authors and The HuggingFace Inc. team.
+# Copyright 2021 Microsoft Research and the HuggingFace Inc. team.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
