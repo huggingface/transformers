9c20f81b5399c66b7b4a48c4488c480ed7de6c64
elias@showk.me <elias@huggingface.co>
2021-09-14 16:44:08 +0200

    catch only EnvironmentError in push_to_hub

--- src/transformers/trainer.py
+++ src/transformers/trainer.py
@@ -2558,8 +2558,8 @@ class Trainer:
             self.create_model_card(model_name=model_name, **kwargs)
             try:
                 self.repo.push_to_hub(commit_message="update model card README.md")
-            except Exception as exc:
-                logger.error(f"Error pushing update to the model card. Please read logs and retry.\n{exc}")
+            except EnvironmentError as exc:
+                logger.error(f"Error pushing update to the model card. Please read logs and retry.\n${exc}")
 
         return git_head_commit_url
 
