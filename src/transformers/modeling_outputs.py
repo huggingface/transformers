e53b607f2366dbd926fed5495aaad3f60948dd25
fatcat-z <jiz@microsoft.com>
2021-09-15 23:17:57 +0800

    Restore modeling_outputs.py file.

--- src/transformers/modeling_outputs.py
+++ src/transformers/modeling_outputs.py
@@ -239,8 +239,7 @@ class BaseModelOutputWithPastAndCrossAttentions(ModelOutput):
     """
 
     last_hidden_state: torch.FloatTensor = None
-    # past_key_values: Optional[Tuple[Tuple[torch.FloatTensor]]] = None
-    past_key_values: Optional[List[torch.FloatTensor]] = None
+    past_key_values: Optional[Tuple[Tuple[torch.FloatTensor]]] = None
     hidden_states: Optional[Tuple[torch.FloatTensor]] = None
     attentions: Optional[Tuple[torch.FloatTensor]] = None
     cross_attentions: Optional[Tuple[torch.FloatTensor]] = None
