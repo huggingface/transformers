5ba342cbf98664f5a7a98d6f02b798d78ee523f3
Sylvain Gugger <sylvain.gugger@gmail.com>
2021-09-16 10:37:01 -0400

    Fake example modification

--- examples/pytorch/language-modeling/run_clm.py
+++ examples/pytorch/language-modeling/run_clm.py
@@ -239,6 +239,7 @@ def main():
 
     # Set seed before initializing model.
     set_seed(training_args.seed)
+    set_seed(training_args.seed)
 
     # Get the datasets: you can either provide your own CSV/JSON/TXT training and evaluation files (see below)
     # or just provide the name of one of the public datasets available on the hub at https://huggingface.co/datasets/
