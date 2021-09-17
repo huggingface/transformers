9e846746cc1c9155f01806774ab637b5304237fa
Yih-Dar <2521628+ydshieh@users.noreply.github.com>
2021-09-17 17:39:35 +0200

    fix some docstring in encoder-decoder models (#13611)
    Co-authored-by: ydshieh <ydshieh@users.noreply.github.com>

--- src/transformers/models/encoder_decoder/modeling_encoder_decoder.py
+++ src/transformers/models/encoder_decoder/modeling_encoder_decoder.py
@@ -53,7 +53,7 @@ ENCODER_DECODER_START_DOCSTRING = r"""
     general usage and behavior.
 
     Parameters:
-        config (:class:`~transformers.PretrainedConfig`): Model configuration class with all the parameters of the model.
+        config (:class:`~transformers.EncoderDecoderConfig`): Model configuration class with all the parameters of the model.
             Initializing with a config file does not load the weights associated with the model, only the
             configuration. Check out the :meth:`~transformers.PreTrainedModel.from_pretrained` method to load the model
             weights.
@@ -139,7 +139,7 @@ ENCODER_DECODER_INPUTS_DOCSTRING = r"""
 @add_start_docstrings(ENCODER_DECODER_START_DOCSTRING)
 class EncoderDecoderModel(PreTrainedModel):
     r"""
-    :class:`~transformers.EncoderDecoder` is a generic model class that will be instantiated as a transformer
+    :class:`~transformers.EncoderDecoderModel` is a generic model class that will be instantiated as a transformer
     architecture with one of the base model classes of the library as encoder and another one as decoder when created
     with the :meth`~transformers.AutoModel.from_pretrained` class method for the encoder and
     :meth`~transformers.AutoModelForCausalLM.from_pretrained` class method for the decoder.
