3786a3f1747e2773bb905c2b8be9d9b196d16200
Patrick von Platen <patrick.v.platen@gmail.com>
2021-09-17 16:43:55 +0200

    Update src/transformers/models/auto/tokenization_auto.py
    Co-authored-by: Lysandre Debut <lysandre@huggingface.co>

--- src/transformers/models/auto/tokenization_auto.py
+++ src/transformers/models/auto/tokenization_auto.py
@@ -348,7 +348,7 @@ class AutoTokenizer:
     def __init__(self):
         raise EnvironmentError(
             "AutoTokenizer is designed to be instantiated "
-            "using the `AutoTokenizer.from_pretrained(pretrained_model_name_or_path)` or `AutoTokenizer.from_config(config)` method."
+            "using the `AutoTokenizer.from_pretrained(pretrained_model_name_or_path)` or `AutoTokenizer.from_model_name(config)` method."
         )
 
     @classmethod
