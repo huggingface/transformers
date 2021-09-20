343842981817c985a95ae5535cc5538517f4c6c4
Sylvain Gugger <35901082+sgugger@users.noreply.github.com>
2021-09-20 15:58:25 -0400

    Update src/transformers/modeling_utils.py
    Co-authored-by: Stas Bekman <stas00@users.noreply.github.com>

--- src/transformers/modeling_utils.py
+++ src/transformers/modeling_utils.py
@@ -937,6 +937,8 @@ class PreTrainedModel(nn.Module, ModuleUtilsMixin, GenerationMixin, PushToHubMix
         """
         Activates or deactivates gradient checkpointing for the current model.
 
+        Note that in other frameworks this feature can be referred to as "activation checkpointing" or "checkpoint activations".
+
         Args:
             flag (:obj:`bool`, `optional`, defaults to :obj:`True`):
                 Will activate gradient checkpointing if :obj:`True`, deactivate it if :obj:`False`.
