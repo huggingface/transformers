e7e9995929c03754f9410124a3173cd768fdcf77
Michael Benayoun <mickbenayoun@gmail.com>
2021-09-22 14:36:57 +0200

    fixed issue with GraphModule deepcopy and added transformations compositiom helper function

--- src/transformers/utils/fx_transformations.py
+++ src/transformers/utils/fx_transformations.py
@@ -1,10 +1,11 @@
 import copy
 import functools
 import operator
-from typing import Any, Dict, Union
+from inspect import signature
+from typing import Any, Callable, Dict, Optional, Union
 
 import torch
-from torch.fx import GraphModule, Node
+from torch.fx import Graph, GraphModule, Node
 
 
 # Torch FX transformation convention:
@@ -30,8 +31,7 @@ def _cache_attributes(gm: GraphModule) -> Dict[str, Any]:
 
 def _restore_attributes_(gm: GraphModule, attributes: Dict[str, Any]):
     for name, attr in attributes.items():
-        if attr:
-            setattr(gm, name, attr)
+        setattr(gm, name, attr)
 
 
 def deepcopy_graph(gm: GraphModule) -> GraphModule:
@@ -39,9 +39,29 @@ def deepcopy_graph(gm: GraphModule) -> GraphModule:
     Performs a deepcopy of the GraphModule while also copying the relevant attributes to know whether the model was
     traced with dynamic axes, and what were the values if that is the case.
     """
+
+    # First, create a copy of the module without the graph.
+    graph = gm.__dict__.pop("_graph")
+    fake_mod = torch.nn.Module()
+    fake_mod.__dict__ = copy.deepcopy(gm.__dict__)
+    gm.__dict__["_graph"] = graph
+
+    # Then, copy the graph.
+    val_map = {}
+    graph_clone = Graph()
+    output_val = graph_clone.graph_copy(graph, val_map=val_map)
+    graph_clone.output(output_val)
+
+    # Finally create a new GraphModule (or a subclass of GraphModule) from the module and the graph copies.
+    # gm.__class__ is used to take into account that gm can be an instance of a subclass of GraphModule.
+    clone = gm.__class__(fake_mod, graph_clone)
+
+    # Restore the dynamic axes related attributes to the clone.
     attributes = _cache_attributes(gm)
-    clone = copy.deepcopy(gm)
+    attributes["dynamic2static"] = {val_map.get(k, k): v for k, v in attributes["dynamic2static"].items()}
+    attributes["static2dynamic"] = {v: k for k, v in attributes["dynamic2static"].items()}
     _restore_attributes_(clone, attributes)
+
     return clone
 
 
@@ -62,7 +82,51 @@ def transformation(func):
         new_kwargs = {k: map_fn(v) for k, v in kwargs.items()}
         return func(*new_args, **new_kwargs)
 
+    wrapper._is_transformation = True
+
     return wrapper
+
+
+def compose_transformations(
+    *args: Callable[[GraphModule], Optional[GraphModule]], inplace: bool = False
+) -> GraphModule:
+    """
+    Allows to compose transformations together and takes of:
+
+        1. Performing the transformations on a copy of the GraphModule if inplace is set to False, transformations that
+           are decorated with @transformation (which means that they are not modifying the original GraphModule) are
+           unwrapped to make them inplace.
+        2. Linting and recompiling only at the end of the composition for performance purposes.
+    """
+    args = list(args)
+    if not inplace:
+        args.insert(0, deepcopy_graph)
+
+    for i, transformation in enumerate(args[:-1]):
+        sig = signature(transformation)
+
+        # Unwrapping @transformation decorated transformations as performing the transformations inplace or on a copy is
+        # already handled by this function.
+        if getattr(transformation, "_is_transformation", False):
+            transformation = transformation.__wrapped__
+
+        # Linting and recompiling only after the last transformation applied to make composition efficient.
+        if "lint_and_recompile" in sig.parameters:
+            args[i] = functools.partial(transformation, lint_and_recompile=False)
+
+    def reduce_func(f, g):
+        def compose_f_and_g(gm):
+            output_g = g(gm)
+            if output_g is None:
+                output_g = gm
+            output_f = f(output_g)
+            if output_f is None:
+                output_f = gm
+            return output_f
+
+        return compose_f_and_g
+
+    return functools.reduce(reduce_func, reversed(args), lambda x: x)
 
 
 def _remove_unused_nodes_(gm: GraphModule, lint_and_recompile: bool = True):
@@ -290,9 +354,6 @@ def transform_to_dynamic_input_(gm: GraphModule, is_retracing: bool = False):
                 static2dynamic[gm.static_batch_size * gm.num_choices] = graph.call_function(
                     operator.mul, args=(batch_size_node, gm.num_choices)
                 )
-            # graph.lint()
-            # gm.recompile()
-
             # Useful when retracing for quantization.
             if hasattr(gm, "_qconfig_map"):
                 gm._qconfig_map[static2dynamic[gm.static_batch_size * gm.num_choices]] = None
