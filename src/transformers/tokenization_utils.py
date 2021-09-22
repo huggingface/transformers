37e960b1a2ba6115566ecf23cbdbfaa600c4f950
MocktaiLEngineer <kavinarasu22@gmail.com>
2021-09-22 18:30:26 +0530

    Raise exceptions instead of using assertions for control flow

--- src/transformers/tokenization_utils.py
+++ src/transformers/tokenization_utils.py
@@ -363,7 +363,7 @@ class PreTrainedTokenizer(PreTrainedTokenizerBase):
         tokens_to_add = []
         for token in new_tokens:
             if not isinstance(token, str):
-                raise TypeError(f"Token {token} is not a string but a {type(token)}")
+                raise TypeError(f"Token {token} is not a string but a {type(token)}.")
             if not special_tokens and hasattr(self, "do_lower_case") and self.do_lower_case:
                 token = token.lower()
             if (
