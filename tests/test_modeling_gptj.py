fix gradient checkpointing

tests/test_modeling_gptj.py
@@ -465,11 +465,12 @@ class GPTJModelLanguageGenerationTest(unittest.TestCase):
     def test_lm_generate_gptj(self):
         for checkpointing in [True, False]:
             model = GPTJForCausalLM.from_pretrained(
-                "EleutherAI/gpt-j-6B",
-                gradient_checkpointing=checkpointing,
-                revision="float16",
-                torch_dtype=torch.float16,
+                "EleutherAI/gpt-j-6B", revision="float16", torch_dtype=torch.float16
             )
+            if checkpointing:
+                model.gradient_checkpointing_enable()
+            else:
+                model.gradient_checkpointing_disable()
             model.to(torch_device)
             input_ids = torch.tensor([[464, 3290]], dtype=torch.long, device=torch_device)  # The dog
             # fmt: off