9a38c0b622f4e1e8943fdff950de7ff170f8a21d
sayef <msi.sayef@gmail.com>
2021-09-23 10:55:39 +0200

    Add installation dependencies for examples/research_projects/fsner.

--- examples/research_projects/fsner/setup.py
+++ examples/research_projects/fsner/setup.py
@@ -23,4 +23,5 @@ setuptools.setup(
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
     python_requires=">=3.6",
+    install_requires=["torch>=1.9.0", "transformers>=4.9.2"],
 )
