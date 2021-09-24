107c94eaef10323e38354eec1797ef27cdec3336
Yuta Hayashibe <yuta@hayashibe.jp>
2021-09-24 21:16:25 +0900

    Updated the waning message for pad_to_max_length

--- src/transformers/tokenization_utils_base.py
+++ src/transformers/tokenization_utils_base.py
@@ -2226,7 +2226,7 @@ class PreTrainedTokenizerBase(SpecialTokensMixin, PushToHubMixin):
                     if max_length is not None:
                         warnings.warn("`max_length` is ignored when `padding`=`True`.")
                     if old_pad_to_max_length is not False:
-                        warnings.warn("`pad_to_max_length` is ignored when `padding`=`True`.")
+                        warnings.warn("Though `pad_to_max_length` = `True`, it is ignored because `padding`=`True`.")
                 padding_strategy = PaddingStrategy.LONGEST  # Default to pad to the longest sequence in the batch
             elif not isinstance(padding, PaddingStrategy):
                 padding_strategy = PaddingStrategy(padding)
