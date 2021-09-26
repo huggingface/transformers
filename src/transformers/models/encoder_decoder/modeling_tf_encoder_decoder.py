6d654a1a87ab23f386ab49963c4b6e5bc0368775
ydshieh <ydshieh@users.noreply.github.com>
2021-09-26 15:41:19 +0200

    Fix style

--- src/transformers/models/encoder_decoder/modeling_tf_encoder_decoder.py
+++ src/transformers/models/encoder_decoder/modeling_tf_encoder_decoder.py
@@ -239,20 +239,21 @@ class TFEncoderDecoderModel(TFPreTrainedModel):
 
     @classmethod
     def from_pretrained(cls, pretrained_model_name_or_path, *model_args, **kwargs):
-        """Initializing `TFEncoderDecoderModel` from a pytorch checkpoint is not supported currently.
+        r"""
+        Initializing `TFEncoderDecoderModel` from a pytorch checkpoint is not supported currently.
 
-        If there are only pytorch checkpoints for a particular encoder-decoder model, a workaround is:
+        If there are only pytorch checkpoints for a particular encoder-decoder model, a workaround is::
 
-            # a workaround to load from pytorch checkpoint
-            _model = EncoderDecoderModel.from_pretrained("patrickvonplaten/bert2bert-cnn_dailymail-fp16")
-            _model.encoder.save_pretrained("./encoder")
-            _model.decoder.save_pretrained("./decoder")
-            model = TFEncoderDecoderModel.from_encoder_decoder_pretrained(
-                "./encoder", "./decoder", encoder_from_pt=True, decoder_from_pt=True
-            )
-            # This is only for copying some specific attributes of this particular model.
-            model.config = _model.config
-            
+            >>> # a workaround to load from pytorch checkpoint
+            >>> _model = EncoderDecoderModel.from_pretrained("patrickvonplaten/bert2bert-cnn_dailymail-fp16")
+            >>> _model.encoder.save_pretrained("./encoder")
+            >>> _model.decoder.save_pretrained("./decoder")
+            >>> model = TFEncoderDecoderModel.from_encoder_decoder_pretrained(
+            >>>     "./encoder", "./decoder", encoder_from_pt=True, decoder_from_pt=True
+            >>> )
+            >>> # This is only for copying some specific attributes of this particular model.
+            >>> model.config = _model.config
+
         """
 
         from_pt = kwargs.pop("from_pt", False)
