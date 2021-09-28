03efd1075f893b9ee391a5a72130462a11a0c772
matt <rocketknight1@gmail.com>
2021-09-28 16:33:12 +0100

    Adding print message to the final upload

--- src/transformers/keras_callbacks.py
+++ src/transformers/keras_callbacks.py
@@ -60,8 +60,9 @@ class PushToHubCallback(Callback):
 
     def on_train_end(self, logs=None):
         if self.last_job is not None and not self.last_job.is_done():
+            print("Waiting for existing upload to finish...")
             self.last_job.process.join()  # Wait for existing upload to finish to finish
         self.model.save_pretrained(self.output_dir)
-            if self.tokenizer is not None:
-                self.tokenizer.save_pretrained(self.output_dir)
+        if self.tokenizer is not None:
+            self.tokenizer.save_pretrained(self.output_dir)
         self.repo.push_to_hub(commit_message=f"End of training", blocking=True)
