c8592f40edd90722201266f887118df16dc57311
matt <rocketknight1@gmail.com>
2021-09-28 16:05:05 +0100

    Reworked the callback to use Repository

--- src/transformers/keras_callbacks.py
+++ src/transformers/keras_callbacks.py
@@ -1,36 +1,58 @@
-from typing import Optional
+from typing import Optional, Union, Any
+from pathlib import Path
 
 from tensorflow.keras.callbacks import Callback
+from transformers.file_utils import get_full_repo_name
+from huggingface_hub import Repository
 
 
 class PushToHubCallback(Callback):
     def __init__(
         self,
+        output_dir: Union[str, Path],
         save_strategy="epoch",
         save_steps: Optional[int] = None,
-        repo_path_or_name: Optional[str] = None,
-        repo_url: Optional[str] = None,
-        **push_to_hub_kwargs
+        tokenizer: Optional[Any] = None,
+        hub_model_id: Optional[str] = None,
+        hub_token: Optional[str] = None,
     ):
         super().__init__()
         self.save_strategy = save_strategy
         if self.save_strategy == "steps" and (not isinstance(save_steps, int) or save_steps <= 0):
             raise ValueError("Please supply a positive integer argument for save_steps when save_strategy == 'steps'!")
         self.save_steps = save_steps
-        if repo_path_or_name is None and repo_url is None:
-            raise ValueError("You need to specify a `repo_path_or_name` or a `repo_url`.")
-        self.repo_path_or_name = repo_path_or_name
-        self.repo_url = repo_url
-        self.push_to_hub_kwargs = push_to_hub_kwargs
+        if isinstance(output_dir, str):
+            output_dir = Path(output_dir)
+        if hub_model_id is None:
+            repo_name = get_full_repo_name(output_dir.name, token=hub_token)
+        else:
+            repo_name = hub_model_id
+        self.output_dir = output_dir
+        self.repo = Repository(str(output_dir), clone_from=repo_name)
+        self.tokenizer = tokenizer
+        self.last_job = None
 
     def on_train_batch_end(self, batch, logs=None):
         if self.save_strategy == "steps" and batch + 1 % self.save_steps == 0:
-            self.model.push_to_hub(
-                repo_path_or_name=self.repo_path_or_name, repo_url=self.repo_url, **self.push_to_hub_kwargs
-            )
+            if self.last_job is not None and not self.last_job.is_done():
+                return  # The last upload is still running, don't start another
+            self.model.save_pretrained(self.output_dir)
+            self.tokenizer.save_pretrained(self.output_dir)
+            _, self.last_job = self.repo.push_to_hub(
+                commit_message=f"Training in progress steps {batch}", blocking=False)
 
     def on_epoch_end(self, epoch, logs=None):
         if self.save_strategy == "epoch":
-            self.model.push_to_hub(
-                repo_path_or_name=self.repo_path_or_name, repo_url=self.repo_url, **self.push_to_hub_kwargs
-            )
+            if self.last_job is not None and not self.last_job.is_done():
+                return  # The last upload is still running, don't start another
+            self.model.save_pretrained(self.output_dir)
+            self.tokenizer.save_pretrained(self.output_dir)
+            _, self.last_job = self.repo.push_to_hub(
+                commit_message=f"Training in progress epoch {epoch}", blocking=False)
+
+    def on_train_end(self, logs=None):
+        if self.last_job is not None and not self.last_job.is_done():
+            self.last_job.process.join()  # Wait for existing upload to finish to finish
+        self.model.save_pretrained(self.output_dir)
+        self.tokenizer.save_pretrained(self.output_dir)
+        self.repo.push_to_hub(commit_message=f"End of training", blocking=True)
