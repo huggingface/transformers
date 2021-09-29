dc6d969dbee179b35cf5931d678b9e1b771c0ace
Sylvain Gugger <sylvain.gugger@gmail.com>
2021-09-29 11:28:25 -0400

    Add comments

--- tests/test_trainer_utils.py
+++ tests/test_trainer_utils.py
@@ -361,6 +361,8 @@ class TrainerUtilsTest(unittest.TestCase):
             for i in range(2)
         ]
 
+        # Build expected shards: each process will have batches of size 4 until there is not enough elements to
+        # form two full batches (so we stop at 96 = (100 // (4 * 2)) * 4)
         expected_shards = [[], []]
         current_shard = 0
         for i in range(0, 96, 4):
@@ -374,6 +376,7 @@ class TrainerUtilsTest(unittest.TestCase):
             IterableDatasetShard(list(range(100)), batch_size=4, drop_last=False, num_processes=2, process_index=i)
             for i in range(2)
         ]
+        # When drop_last=False, we get two last full batches by looping back to the beginning.
         expected_shards[0].extend(list(range(96, 100)))
         expected_shards[1].extend(list(range(0, 4)))
 
