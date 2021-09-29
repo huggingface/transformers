12d6ae0516eb0a8ededeabe5646a4a00d69f0e53
Stancl, Daniel <daniel.stancl@firma.seznam.cz>
2021-09-29 12:55:13 +0200

    (Nitpick) Fast tokenization -> Fast Tokenization in doc

--- src/transformers/models/blenderbot/tokenization_blenderbot_fast.py
+++ src/transformers/models/blenderbot/tokenization_blenderbot_fast.py
@@ -12,7 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Fast tokenization class for Blenderbot."""
+"""Fast Tokenization class for Blenderbot."""
 
 from typing import TYPE_CHECKING, List, Optional
 
