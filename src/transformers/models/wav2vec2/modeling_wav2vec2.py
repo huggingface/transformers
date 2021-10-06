correct gradient checkpoitning

src/transformers/models/wav2vec2/modeling_wav2vec2.py
@@ -398,8 +398,12 @@ class Wav2Vec2FeatureExtractor(nn.Module):
 
     def forward(self, input_values):
         hidden_states = input_values[:, None]
-        for conv_layer in self.conv_layers:
 
+        # make sure hidden_states require grad for gradient_checkpointing
+        if self.training:
+            hidden_states.requires_grad = True
+
+        for conv_layer in self.conv_layers:
             if self.gradient_checkpointing and self.training:
 
                 def create_custom_forward(module):