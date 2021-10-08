889a3a32852edf2c744e902f1a0fbb8ba2bfde75
Patrick von Platen <patrick.v.platen@gmail.com>
2021-10-08 17:28:18 +0200

    [Generation] Fix max_new_tokens (#13919)
    * up
    
    * Update src/transformers/generation_stopping_criteria.py
    
    * finish

--- src/transformers/generation_stopping_criteria.py
+++ src/transformers/generation_stopping_criteria.py
@@ -71,6 +71,12 @@ class MaxNewTokensCriteria(StoppingCriteria):
     """
 
     def __init__(self, start_length: int, max_new_tokens: int):
+        warnings.warn(
+            "The class `MaxNewTokensCriteria` is deprecated. "
+            f"Please use `MaxLengthCriteria(max_length={start_length + max_new_tokens})` "
+            "with `max_length = start_length + max_new_tokens` instead.",
+            FutureWarning,
+        )
         self.start_length = start_length
         self.max_new_tokens = max_new_tokens
         self.max_length = start_length + max_new_tokens
