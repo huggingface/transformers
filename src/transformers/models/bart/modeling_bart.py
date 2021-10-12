4fa8dbc4f8abd84a00247ed744cd171634d7dc21
Chi-Liang <liangtaiwan1230@gmail.com>
2021-10-12 16:11:22 +0800

    BartEnocder add get_input_embeddings

--- src/transformers/models/bart/modeling_bart.py
+++ src/transformers/models/bart/modeling_bart.py
@@ -694,6 +694,9 @@ class BartEncoder(BartPretrainedModel):
         self.init_weights()
         self.gradient_checkpointing = False
 
+    def get_input_embeddings(self):
+        return self.embed_tokens
+
     def set_input_embeddings(self, value):
         self.embed_tokens = value
 
