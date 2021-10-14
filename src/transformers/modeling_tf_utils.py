1d7052499a57123278920dddb8cefb624a797a0b
Li-Huai (Allan) Lin <qqaatw@gmail.com>
2021-10-14 09:28:11 +0800

    Remove wrong model_args supplied (#13937)
    * Remove wrong model_args of config.from_pretrained
    
    * Fix tf & flax

--- src/transformers/modeling_tf_utils.py
+++ src/transformers/modeling_tf_utils.py
@@ -1363,7 +1363,6 @@ class TFPreTrainedModel(tf.keras.Model, TFModelUtilsMixin, TFGenerationMixin, Pu
             config_path = config if config is not None else pretrained_model_name_or_path
             config, model_kwargs = cls.config_class.from_pretrained(
                 config_path,
-                *model_args,
                 cache_dir=cache_dir,
                 return_unused_kwargs=True,
                 force_download=force_download,
