1d53954a915edfbf25fc4603185b180715c340b3
King Yiu Suen <kingyiusuen@gmail.com>
2021-10-15 16:46:59 -0500

    Fix docstring

--- src/transformers/models/layoutlmv2/processing_layoutlmv2.py
+++ src/transformers/models/layoutlmv2/processing_layoutlmv2.py
@@ -12,9 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""
-Processor class for LayoutLMv2.
-"""
+""" Processor class for LayoutLMv2. """
 
 
 from typing import List, Optional, Union
