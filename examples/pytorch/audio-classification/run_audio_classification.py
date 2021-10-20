3b8e2f8ffafe953e32b659e34c82827e37d5f5dc
anton-l <aglozhkov@gmail.com>
2021-10-20 12:11:04 +0300

    Update datasets

--- examples/pytorch/audio-classification/run_audio_classification.py
+++ examples/pytorch/audio-classification/run_audio_classification.py
@@ -44,7 +44,7 @@ logger = logging.getLogger(__name__)
 # Will error if the minimal version of Transformers is not installed. Remove at your own risks.
 check_min_version("4.12.0.dev0")
 
-require_version("datasets>=1.13.3", "To fix: pip install -r examples/pytorch/audio-classification/requirements.txt")
+require_version("datasets>=1.14.0", "To fix: pip install -r examples/pytorch/audio-classification/requirements.txt")
 
 
 def random_subsample(wav: np.ndarray, max_length: float, sample_rate: int = 16000):
