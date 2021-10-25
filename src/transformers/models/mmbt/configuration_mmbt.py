bfa4fd7b1d7a99d7eae14e9b5aa3e283185c530c
Ahmed Mahran <ahmed.mahran@mashin.io>
2021-10-25 16:20:19 +0200

    Fix AttributeError: 'MMBTConfig' object has no attribute 'use_return_dict'
    Attributes are copied from transformers.PreTrainedConfig.__dict__ which does not contain object properties.
    use_return_dict is an object property that's not in object.__dict__

--- src/transformers/models/mmbt/configuration_mmbt.py
+++ src/transformers/models/mmbt/configuration_mmbt.py
@@ -37,6 +37,7 @@ class MMBTConfig(object):
 
     def __init__(self, config, num_labels=None, modal_hidden_size=2048):
         self.__dict__ = config.__dict__
+        self.use_return_dict = config.use_return_dict
         self.modal_hidden_size = modal_hidden_size
         if num_labels:
             self.num_labels = num_labels
