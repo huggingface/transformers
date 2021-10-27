5a59d6d8ea568027754f725d2ec3a26c4dce8f0f
Anton Lozhkov <aglozhkov@gmail.com>
2021-10-27 20:17:31 +0300

    Add DistilHuBERT  (#14174)
    * Add conversion
    
    * Rename
    
    * Add an integration test and remove layer_norm
    
    * Remove layer_norm from the converter
    
    * wording
    
    * Fix imports

--- src/transformers/models/hubert/modeling_hubert.py
+++ src/transformers/models/hubert/modeling_hubert.py
@@ -325,13 +325,16 @@ class HubertFeatureExtractor(nn.Module):
 class HubertFeatureProjection(nn.Module):
     def __init__(self, config):
         super().__init__()
-        self.layer_norm = nn.LayerNorm(config.conv_dim[-1], eps=config.layer_norm_eps)
+        self.feat_proj_layer_norm = config.feat_proj_layer_norm
+        if self.feat_proj_layer_norm:
+            self.layer_norm = nn.LayerNorm(config.conv_dim[-1], eps=config.layer_norm_eps)
         self.projection = nn.Linear(config.conv_dim[-1], config.hidden_size)
         self.dropout = nn.Dropout(config.feat_proj_dropout)
 
     def forward(self, hidden_states):
         # non-projected hidden states are needed for quantization
-        hidden_states = self.layer_norm(hidden_states)
+        if self.feat_proj_layer_norm:
+            hidden_states = self.layer_norm(hidden_states)
         hidden_states = self.projection(hidden_states)
         hidden_states = self.dropout(hidden_states)
         return hidden_states
