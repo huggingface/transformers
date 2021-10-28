b1e62249efcd859086d8bdba638cd47991233e4d
Nicolas Patry <patry.nicolas@protonmail.com>
2021-10-28 16:35:54 +0200

    Fixing tests on ibert, longformer, and roberta.

--- tests/test_pipelines_common.py
+++ tests/test_pipelines_common.py
@@ -22,7 +22,15 @@ from abc import abstractmethod
 from functools import lru_cache
 from unittest import skipIf
 
-from transformers import FEATURE_EXTRACTOR_MAPPING, TOKENIZER_MAPPING, AutoFeatureExtractor, AutoTokenizer, pipeline
+from transformers import (
+    FEATURE_EXTRACTOR_MAPPING,
+    TOKENIZER_MAPPING,
+    AutoFeatureExtractor,
+    AutoTokenizer,
+    IBertConfig,
+    RobertaConfig,
+    pipeline,
+)
 from transformers.pipelines.base import _pad
 from transformers.testing_utils import is_pipeline_test, require_torch
 
@@ -143,7 +151,11 @@ class PipelineTestCaseMeta(type):
                     try:
                         tokenizer = get_tiny_tokenizer_from_checkpoint(checkpoint)
                         # XLNet actually defines it as -1.
+<<<<<<< HEAD
                         if model.config.__class__.__name__ == "RobertaConfig":
+=======
+                        if isinstance(model.config, (RobertaConfig, IBertConfig)):
+>>>>>>> Fixing tests on ibert, longformer, and roberta.
                             tokenizer.model_max_length = model.config.max_position_embeddings - 2
                         elif (
                             hasattr(model.config, "max_position_embeddings")
