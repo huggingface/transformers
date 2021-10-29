9fc1951711e5377ffa1f06614ca37d4d5ad281a8
Lysandre <lysandre.debut@reseau.eseo.fr>
2021-10-29 14:51:05 -0400

    Docs for v4.12.2

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -78,4 +78,5 @@ deploy_doc "54f9d62" v4.11.1
 deploy_doc "7655f11" v4.11.2
 deploy_doc "65659a2" v4.11.3
 deploy_doc "62bf536" v4.12.0
-deploy_doc "e0a5154"  # v4.12.1 Latest stable release
+deploy_doc "e0a5154" v4.12.1
+deploy_doc "2191373"  # v4.12.2 Latest stable release
