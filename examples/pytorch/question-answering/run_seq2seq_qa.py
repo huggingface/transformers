2b344b205c095fa5aee31d3edf1d5499604bf139
Karthik Rangasai <karthikrangasai@gmail.com>
2021-10-30 02:33:34 +0530

    Fix code quality and update post processing.

--- examples/pytorch/question-answering/run_seq2seq_qa.py
+++ examples/pytorch/question-answering/run_seq2seq_qa.py
@@ -26,7 +26,6 @@ from typing import List, Optional, Tuple
 
 import datasets
 import nltk
-import numpy as np
 from datasets import load_dataset, load_metric
 
 import transformers
@@ -37,7 +36,6 @@ from transformers import (
     AutoTokenizer,
     DataCollatorForSeq2Seq,
     HfArgumentParser,
-    Seq2SeqTrainer,
     Seq2SeqTrainingArguments,
     set_seed,
 )
@@ -579,18 +577,22 @@ def main():
     def post_processing_function(
         examples: datasets.Dataset, features: datasets.Dataset, outputs: EvalLoopOutput, stage="eval"
     ):
-        preds, labels = outputs.predictions, outputs.label_ids
+
+        # Decode the predicted tokens.
+        preds = outputs.predictions
         if isinstance(preds, tuple):
             preds = preds[0]
         decoded_preds = tokenizer.batch_decode(preds, skip_special_tokens=True)
-        if data_args.ignore_pad_token_for_loss:
-            # Replace -100 in the labels as we can't decode them.
-            labels = np.where(labels != -100, labels, tokenizer.pad_token_id)
-        decoded_labels = tokenizer.batch_decode(labels, skip_special_tokens=True)
 
+        # Build a map example to its corresponding features.
+        example_id_to_index = {k: i for i, k in enumerate(examples["id"])}
+        feature_per_example = {example_id_to_index[feature["example_id"]]: i for i, feature in enumerate(features)}
         predictions = {}
-        for idx, example in enumerate(examples):
-            predictions[example["id"]] = decoded_preds[idx]
+        # Let's loop over all the examples!
+        for example_index, example in enumerate(examples):
+            # This is the index of the feature associated to the current example.
+            feature_index = feature_per_example[example_index]
+            predictions[example["id"]] = decoded_preds[feature_index]
 
         # Format the result to the format the metric expects.
         if data_args.version_2_with_negative:
