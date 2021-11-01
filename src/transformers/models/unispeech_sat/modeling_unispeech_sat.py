dcb0de8d8de4fc353d4738082ec9380069440797
Yih-Dar <2521628+ydshieh@users.noreply.github.com>
2021-11-01 11:52:36 +0100

    Add more missing models to models/__init__.py (#14177)
    * Add missing models to models/__init__.py
    
    * Fix issues previously undetected
    
    * Add UniSpeechSatForPreTraining to all_model_classes
    
    * fix unispeech sat
    
    * fix
    
    * Add check_model_list() to check_repo.py
    
    * Remove _ignore_models = ["bort"]
    
    Co-authored-by: ydshieh <ydshieh@users.noreply.github.com>
    Co-authored-by: patrickvonplaten <patrick.v.platen@gmail.com>

--- src/transformers/models/unispeech_sat/modeling_unispeech_sat.py
+++ src/transformers/models/unispeech_sat/modeling_unispeech_sat.py
@@ -1157,6 +1157,7 @@ class UniSpeechSatForPreTraining(UniSpeechSatPreTrainedModel):
 
         self.speaker_proj = nn.Linear(config.hidden_size, config.codevector_dim)
         self.label_embeddings_concat = nn.Parameter(torch.FloatTensor(config.num_clusters, config.codevector_dim))
+        self.label_embeddings_concat.data.zero_()
 
         self.layer_norm_for_extract = nn.LayerNorm(config.hidden_size, eps=config.layer_norm_eps)
         if self.config.do_stable_layer_norm:
@@ -1268,21 +1269,24 @@ class UniSpeechSatForPreTraining(UniSpeechSatPreTrainedModel):
         # quantize all (unmasked) extracted features and project to final vq dim
         extract_features = self.dropout_features(outputs[1])
 
-        # layer normalization (has no effect when `config.do_stable_layer_norm == False`)
-        extract_features = self.layer_norm_for_extract(extract_features)
-        quantized_features, codevector_perplexity = self.quantizer(extract_features)
-
-        # project quantized features twice
-        quantized_features = self.project_q(quantized_features)
-        quantized_features = self.project_hid(quantized_features)
-
         # TODO(PVP) - add pretraining logic and add to tests
-        loss = None
-        logits = None
+        logits = extract_features
+        loss = quantized_features = codevector_perplexity = None
+
+        # layer normalization (has no effect when `config.do_stable_layer_norm == False`)
+        #        extract_features = self.layer_norm_for_extract(extract_features)
+        #        quantized_features, codevector_perplexity = self.quantizer(extract_features)
+        #
+        # project quantized features twice
+        #        quantized_features = self.project_q(quantized_features)
+        #        quantized_features = self.project_hid(quantized_features)
+        #
+        #        loss = None
+        #        logits = quantized_features
         if not return_dict:
             if loss is not None:
-                return (loss, transformer_features, quantized_features, codevector_perplexity) + outputs[2:]
-            return (transformer_features, quantized_features, codevector_perplexity) + outputs[2:]
+                return (loss, logits, transformer_features, quantized_features, codevector_perplexity) + outputs[2:]
+            return (logits, transformer_features, quantized_features, codevector_perplexity) + outputs[2:]
 
         return UniSpeechSatForPreTrainingOutput(
             loss=loss,
