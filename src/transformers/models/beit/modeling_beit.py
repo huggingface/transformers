c1673c3d627754b7f41c7278f034aca37ff9dc84
Sylvain Gugger <35901082+sgugger@users.noreply.github.com>
2021-11-02 13:07:20 -0400

    Fixes Beit training for PyTorch 1.10+ (#14249)

--- src/transformers/models/beit/modeling_beit.py
+++ src/transformers/models/beit/modeling_beit.py
@@ -987,7 +987,7 @@ class BeitUperHead(nn.Module):
         used_backbone_levels = len(laterals)
         for i in range(used_backbone_levels - 1, 0, -1):
             prev_shape = laterals[i - 1].shape[2:]
-            laterals[i - 1] += nn.functional.interpolate(
+            laterals[i - 1] = laterals[i - 1] + nn.functional.interpolate(
                 laterals[i], size=prev_shape, mode="bilinear", align_corners=self.align_corners
             )
 
