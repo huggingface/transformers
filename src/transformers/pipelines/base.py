8c0e88fed454264f58239aa0c8e10a2d201693f0
Nicolas Patry <patry.nicolas@protonmail.com>
2021-11-02 15:27:11 +0100

    Enabling generators.

--- src/transformers/pipelines/base.py
+++ src/transformers/pipelines/base.py
@@ -975,7 +975,12 @@ class Pipeline(_ScikitCompat):
                 return outputs
             else:
                 return self.run_multi(inputs, preprocess_params, forward_params, postprocess_params)
-        elif Dataset is not None and isinstance(inputs, Dataset) or isinstance(inputs, types.GeneratorType):
+        elif (
+            Dataset is not None
+            and isinstance(inputs, Dataset)
+            or isinstance(inputs, types.GeneratorType)
+            and self.framework == "pt"
+        ):
             return self.get_iterator(
                 inputs, num_workers, batch_size, preprocess_params, forward_params, postprocess_params
             )
