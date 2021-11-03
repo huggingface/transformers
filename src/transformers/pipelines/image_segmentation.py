Put `load_image` function in `image_utils.py` & fix image rotation issue (#14062)
* Fix img load rotation

* Add `load_image` to `image_utils.py`

* Implement LoadImageTester

* Use hf-internal-testing dataset

* Add img utils comments

* Refactor LoadImageTester

* Import load_image under is_vision_available

src/transformers/pipelines/image_segmentation.py
@@ -1,11 +1,8 @@
 import base64
 import io
-import os
 from typing import Any, Dict, List, Union
 
 import numpy as np
-
-import requests
 
 from ..file_utils import add_end_docstrings, is_torch_available, is_vision_available, requires_backends
 from ..utils import logging
@@ -14,6 +11,8 @@ from .base import PIPELINE_INIT_ARGS, Pipeline
 
 if is_vision_available():
     from PIL import Image
+
+    from ..image_utils import load_image
 
 if is_torch_available():
     import torch
@@ -48,28 +47,6 @@ class ImageSegmentationPipeline(Pipeline):
 
         requires_backends(self, "vision")
         self.check_model_type(MODEL_FOR_IMAGE_SEGMENTATION_MAPPING)
-
-    @staticmethod
-    def load_image(image: Union[str, "Image.Image"]):
-        if isinstance(image, str):
-            if image.startswith("http://") or image.startswith("https://"):
-                # We need to actually check for a real protocol, otherwise it's impossible to use a local file
-                # like http_huggingface_co.png
-                image = Image.open(requests.get(image, stream=True).raw)
-            elif os.path.isfile(image):
-                image = Image.open(image)
-            else:
-                raise ValueError(
-                    f"Incorrect path or url, URLs must start with `http://` or `https://`, and {image} is not a valid path"
-                )
-        elif isinstance(image, Image.Image):
-            pass
-        else:
-            raise ValueError(
-                "Incorrect format used for image. Should be a URL linking to an image, a local path, or a PIL image."
-            )
-        image = image.convert("RGB")
-        return image
 
     def _sanitize_parameters(self, **kwargs):
         postprocess_kwargs = {}
@@ -118,7 +95,7 @@ class ImageSegmentationPipeline(Pipeline):
         return torch.no_grad
 
     def preprocess(self, image):
-        image = self.load_image(image)
+        image = load_image(image)
         target_size = torch.IntTensor([[image.height, image.width]])
         inputs = self.feature_extractor(images=[image], return_tensors="pt")
         inputs["target_size"] = target_size