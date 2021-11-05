c53e972f0ce907233c3641ba8ef8e94152f3ef73
Sylvain Gugger <35901082+sgugger@users.noreply.github.com>
2021-11-05 18:58:51 -0400

    Add new LFS prune API (#14294)

--- examples/pytorch/token-classification/run_ner_no_trainer.py
+++ examples/pytorch/token-classification/run_ner_no_trainer.py
@@ -590,7 +590,9 @@ def main():
             unwrapped_model.save_pretrained(args.output_dir, save_function=accelerator.save)
             if accelerator.is_main_process:
                 tokenizer.save_pretrained(args.output_dir)
-                repo.push_to_hub(commit_message=f"Training in progress epoch {epoch}", blocking=False)
+                repo.push_to_hub(
+                    commit_message=f"Training in progress epoch {epoch}", blocking=False, auto_lfs_prune=True
+                )
 
     if args.output_dir is not None:
         accelerator.wait_for_everyone()
@@ -599,7 +601,7 @@ def main():
         if accelerator.is_main_process:
             tokenizer.save_pretrained(args.output_dir)
             if args.push_to_hub:
-                repo.push_to_hub(commit_message="End of training")
+                repo.push_to_hub(commit_message="End of training", auto_lfs_prune=True)
 
 
 if __name__ == "__main__":
