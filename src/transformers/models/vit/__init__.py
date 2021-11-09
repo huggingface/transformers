9af911fcd3fb9d92d78f8ee397bc83266550d2c6
Yih-Dar <2521628+ydshieh@users.noreply.github.com>
2021-11-09 13:54:37 +0100

    Add TFViTModel (#13778)
    * Start the work for TFViTModel
    
    * Convert to TF code - need to check in the follow up commits
    
    * Clean up model code
    
    * Expose TFViTModel
    
    * make style
    
    * make quality
    
    * Add test
    
    * make style & quality
    
    * Fix some imports
    
    * fix wrong usage - *kwargs => ** kwargs
    
    * Fix Conv2D weight loading (PT->TF) issue
    
    * Add tests for images with different sizes + fix model
    
    * Fix some common tests for TFViTModel
    
    * Use inputs instead of input_ids in test_compile_tf_model
    
    * Add a comment about transpose and Conv2D in convert_tf_weight_name_to_pt_weight_name
    
    * Avoid transpose in TFViT call
    
    * Fix Conv2D issue in load_tf2_weights_in_pytorch_model
    
    * Use tf.keras.layers.Conv2D instead of tf.nn.conv2d
    
    * Using simpler heuristic to detect Conv2D layer
    
    * Change convert_tf_weight_name_to_pt_weight_name to return TransposeType
    
    * Check tf_weight_shape is not None before using it
    
    * Apply suggestions from code review
    
    Co-authored-by: Sylvain Gugger <35901082+sgugger@users.noreply.github.com>
    
    * fix missing comma
    
    * fix input dtype
    
    Co-authored-by: ydshieh <ydshieh@users.noreply.github.com>
    Co-authored-by: Sylvain Gugger <35901082+sgugger@users.noreply.github.com>

--- src/transformers/models/vit/__init__.py
+++ src/transformers/models/vit/__init__.py
@@ -17,7 +17,7 @@
 # limitations under the License.
 from typing import TYPE_CHECKING
 
-from ...file_utils import _LazyModule, is_flax_available, is_torch_available, is_vision_available
+from ...file_utils import _LazyModule, is_flax_available, is_tf_available, is_torch_available, is_vision_available
 
 
 _import_structure = {
@@ -35,6 +35,12 @@ if is_torch_available():
         "ViTPreTrainedModel",
     ]
 
+if is_tf_available():
+    _import_structure["modeling_tf_vit"] = [
+        "TFViTForImageClassification",
+        "TFViTModel",
+        "TFViTPreTrainedModel",
+    ]
 
 if is_flax_available():
     _import_structure["modeling_flax_vit"] = [
@@ -56,6 +62,9 @@ if TYPE_CHECKING:
             ViTModel,
             ViTPreTrainedModel,
         )
+
+    if is_tf_available():
+        from .modeling_tf_vit import TFViTForImageClassification, TFViTModel, TFViTPreTrainedModel
 
     if is_flax_available():
         from .modeling_flax_vit import FlaxViTForImageClassification, FlaxViTModel, FlaxViTPreTrainedModel
