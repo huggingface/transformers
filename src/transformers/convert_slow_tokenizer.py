50dfdfa11e8fb72fed1bf3ca6d6b66bc182dc8c5
qqaatw <qqaatw@gmail.com>
2021-11-11 20:19:59 +0800

    Directly use AlbertConverter

--- src/transformers/convert_slow_tokenizer.py
+++ src/transformers/convert_slow_tokenizer.py
@@ -526,34 +526,6 @@ class AlbertConverter(SpmConverter):
         )
 
 
-class FNetConverter(SpmConverter):
-    def normalizer(self, proto):
-        list_normalizers = [
-            normalizers.Replace("``", '"'),
-            normalizers.Replace("''", '"'),
-        ]
-        if not self.original_tokenizer.keep_accents:
-            list_normalizers.append(normalizers.NFKD())
-            list_normalizers.append(normalizers.StripAccents())
-        if self.original_tokenizer.do_lower_case:
-            list_normalizers.append(normalizers.Lowercase())
-
-        precompiled_charsmap = proto.normalizer_spec.precompiled_charsmap
-        list_normalizers.append(normalizers.Precompiled(precompiled_charsmap))
-        list_normalizers.append(normalizers.Replace(Regex(" {2,}"), " "))
-        return normalizers.Sequence(list_normalizers)
-
-    def post_processor(self):
-        return processors.TemplateProcessing(
-            single="[CLS]:0 $A:0 [SEP]:0",
-            pair="[CLS]:0 $A:0 [SEP]:0 $B:1 [SEP]:1",
-            special_tokens=[
-                ("[CLS]", self.original_tokenizer.convert_tokens_to_ids("[CLS]")),
-                ("[SEP]", self.original_tokenizer.convert_tokens_to_ids("[SEP]")),
-            ],
-        )
-
-
 class BarthezConverter(SpmConverter):
     def unk_id(self, proto):
         unk_id = 3
@@ -954,7 +926,7 @@ SLOW_TO_FAST_CONVERTERS = {
     "DPRQuestionEncoderTokenizer": BertConverter,
     "DPRContextEncoderTokenizer": BertConverter,
     "ElectraTokenizer": BertConverter,
-    "FNetTokenizer": FNetConverter,
+    "FNetTokenizer": AlbertConverter,
     "FunnelTokenizer": FunnelConverter,
     "GPT2Tokenizer": GPT2Converter,
     "HerbertTokenizer": HerbertConverter,
