3d1986e4399ff7a35735638443f9ff5b5951316e
qqaatw <qqaatw@gmail.com>
2021-11-11 15:28:48 +0800

    Add normalizer to FNetConverter

--- src/transformers/convert_slow_tokenizer.py
+++ src/transformers/convert_slow_tokenizer.py
@@ -527,6 +527,22 @@ class AlbertConverter(SpmConverter):
 
 
 class FNetConverter(SpmConverter):
+    def normalizer(self, proto):
+        list_normalizers = [
+            normalizers.Replace("``", '"'),
+            normalizers.Replace("''", '"'),
+        ]
+        if not self.original_tokenizer.keep_accents:
+            list_normalizers.append(normalizers.NFKD())
+            list_normalizers.append(normalizers.StripAccents())
+        if self.original_tokenizer.do_lower_case:
+            list_normalizers.append(normalizers.Lowercase())
+
+        precompiled_charsmap = proto.normalizer_spec.precompiled_charsmap
+        list_normalizers.append(normalizers.Precompiled(precompiled_charsmap))
+        list_normalizers.append(normalizers.Replace(Regex(" {2,}"), " "))
+        return normalizers.Sequence(list_normalizers)
+    
     def post_processor(self):
         return processors.TemplateProcessing(
             single="[CLS]:0 $A:0 [SEP]:0",
