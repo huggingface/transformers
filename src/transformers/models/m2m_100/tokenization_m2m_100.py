3f949c545a0b248e6a5398a73ce2af86a32c009d
Suraj Patil <surajp815@gmail.com>
2021-11-13 19:47:30 +0530

    Apply suggestions from code review
    Co-authored-by: Sylvain Gugger <35901082+sgugger@users.noreply.github.com>

--- src/transformers/models/m2m_100/tokenization_m2m_100.py
+++ src/transformers/models/m2m_100/tokenization_m2m_100.py
@@ -328,13 +328,13 @@ class M2M100Tokenizer(PreTrainedTokenizer):
         return super().prepare_seq2seq_batch(src_texts, tgt_texts, **kwargs)
 
     def _build_translation_inputs(
-        self, raw_inputs, return_tensors: str, src_lang: Optional[str], tgt_lang: Optional[str], **extra_kwargs
+        self, raw_inputs, src_lang: Optional[str], tgt_lang: Optional[str], **extra_kwargs
     ):
         """Used by translation pipeline, to prepare inputs for the generate function"""
         if src_lang is None or tgt_lang is None:
             raise ValueError("Translation requires a `src_lang` and a `tgt_lang` for this model")
         self.src_lang = src_lang
-        inputs = self(raw_inputs, add_special_tokens=True, return_tensors=return_tensors, **extra_kwargs)
+        inputs = self(raw_inputs, add_special_tokens=True, **extra_kwargs)
         tgt_lang_id = self.get_lang_id(tgt_lang)
         inputs["forced_bos_token_id"] = tgt_lang_id
         return inputs
