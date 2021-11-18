e66cedfd260ce7bd9ba063e30bfe48b1de17fabc
NielsRogge <48327001+NielsRogge@users.noreply.github.com>
2021-11-18 16:24:34 +0100

    Add ImageGPT (#14240)
    * First draft
    
    * More improvements
    
    * Improve conversion script
    
    * Fix init weights for layer norm
    
    * Fix correct model for conversion script
    
    * Don't tie input and output embeddings
    
    * Add print statements for debugging
    
    * Add print statements for debugging
    
    * Fix vocab size of model
    
    * Improve documentation, remove fast tokenizer
    
    * Add ImageGPTForImageClassification, improve docs
    
    * Fix docs issue
    
    * Set verbosity level back to info
    
    * Improve tests
    
    * Fix tests and add figure
    
    * Delete tokenizer file
    
    * Remove ImageGPTTokenizer from init files
    
    * Remove ImageGPTLayer from init files
    
    * Remove ImageGPT tokenizer from docs
    
    * First draft of ImageGPTFeatureExtractor
    
    * Fix typo
    
    * Fix bug
    
    * More improvements
    
    * Apply suggestions from code review, add tests for feature extractor
    
    * Fix layernorm
    
    * Update save_pretrained method
    
    * Fix issue
    
    * Make all tests of ImageGPTFeatureExtractor pass
    
    * Update code examples
    
    * Rename model inputs to pixel_values
    
    * Improve code examples
    
    * Update init_weights to post_init
    
    * Fix post_init

--- src/transformers/models/auto/configuration_auto.py
+++ src/transformers/models/auto/configuration_auto.py
@@ -30,6 +30,7 @@ logger = logging.get_logger(__name__)
 CONFIG_MAPPING_NAMES = OrderedDict(
     [
         # Add configs here
+        ("imagegpt", "ImageGPTConfig"),
         ("vision-encoder-decoder", "VisionEncoderDecoderConfig"),
         ("trocr", "TrOCRConfig"),
         ("fnet", "FNetConfig"),
@@ -111,6 +112,7 @@ CONFIG_MAPPING_NAMES = OrderedDict(
 CONFIG_ARCHIVE_MAP_MAPPING_NAMES = OrderedDict(
     [
         # Add archive maps here
+        ("imagegpt", "IMAGEGPT_PRETRAINED_CONFIG_ARCHIVE_MAP"),
         ("fnet", "FNET_PRETRAINED_CONFIG_ARCHIVE_MAP"),
         ("pegasus", "PEGASUS_PRETRAINED_CONFIG_ARCHIVE_MAP"),
         ("segformer", "SEGFORMER_PRETRAINED_CONFIG_ARCHIVE_MAP"),
@@ -182,6 +184,7 @@ CONFIG_ARCHIVE_MAP_MAPPING_NAMES = OrderedDict(
 MODEL_NAMES_MAPPING = OrderedDict(
     [
         # Add full (and cased) model names here
+        ("imagegpt", "ImageGPT"),
         ("vision-encoder-decoder", "Vision Encoder decoder"),
         ("trocr", "TrOCR"),
         ("fnet", "FNet"),
