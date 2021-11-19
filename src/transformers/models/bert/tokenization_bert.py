983e9e78fe1ea11eddfc9bf5b9c27c5e11176cc8
jacklanda <yonyonlau@gmail.com>
2021-11-19 23:39:34 +0800

    rm a redundant statement in function `whitespace_tokenize`

--- src/transformers/models/bert/tokenization_bert.py
+++ src/transformers/models/bert/tokenization_bert.py
@@ -108,10 +108,7 @@ def load_vocab(vocab_file):
 def whitespace_tokenize(text):
     """Runs basic whitespace cleaning and splitting on a piece of text."""
     text = text.strip()
-    if not text:
-        return []
-    tokens = text.split()
-    return tokens
+    return text.split()
 
 
 class BertTokenizer(PreTrainedTokenizer):
