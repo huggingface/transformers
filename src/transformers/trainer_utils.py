fd6f863ad60a066a7b523bbda2205852c8c1764a
Alexander Measure <ameasure@gmail.com>
2021-11-21 09:58:00 -0500

    add Tuple type hints to all label_ids outputs
    affects EvalLoopOutput and PredicctionOutput

--- src/transformers/trainer_utils.py
+++ src/transformers/trainer_utils.py
@@ -80,14 +80,14 @@ class EvalPrediction(NamedTuple):
 
 class EvalLoopOutput(NamedTuple):
     predictions: Union[np.ndarray, Tuple[np.ndarray]]
-    label_ids: Optional[np.ndarray]
+    label_ids: Optional[Union[np.ndarray, Tuple[np.ndarray]]]
     metrics: Optional[Dict[str, float]]
     num_samples: Optional[int]
 
 
 class PredictionOutput(NamedTuple):
     predictions: Union[np.ndarray, Tuple[np.ndarray]]
-    label_ids: Optional[np.ndarray]
+    label_ids: Optional[Union[np.ndarray, Tuple[np.ndarray]]]
     metrics: Optional[Dict[str, float]]
 
 
