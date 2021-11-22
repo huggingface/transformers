c0fd9385be15108356d26a7e46045239a6f996d3
Manuel R. Ciosici <manuelrciosici@gmail.com>
2021-11-22 08:27:50 -0500

    Add comment about AMP-bfloat16 incompatibility

--- src/transformers/training_args.py
+++ src/transformers/training_args.py
@@ -766,6 +766,7 @@ class TrainingArguments:
             )
 
         if (self.fp16 or self.fp16_full_eval or self.fp16_opt_level or self.fp16_backend) and self.bfloat16:
+            # This check should be modified once AMP supports bfloat16.
             raise ValueError(
                 "bfloat16 cannot be used together with AMP, APEX, or FP16."
             )
