65c8f551e787abad9bd96ea07763bc1eeb88022f
Stas Bekman <stas00@users.noreply.github.com>
2021-11-23 14:09:15 -0800

    [deepspeed] zero inference (#14253)
    * [deepspeed] zero inference
    
    * only z3 makes sense for inference
    
    * fix and style
    
    * docs
    
    * rework
    
    * fix test
    
    * Apply suggestions from code review
    
    Co-authored-by: Sylvain Gugger <35901082+sgugger@users.noreply.github.com>
    
    * responding to suggestions
    
    Co-authored-by: Sylvain Gugger <35901082+sgugger@users.noreply.github.com>

--- src/transformers/dependency_versions_table.py
+++ src/transformers/dependency_versions_table.py
@@ -8,7 +8,7 @@ deps = {
     "cookiecutter": "cookiecutter==1.7.2",
     "dataclasses": "dataclasses",
     "datasets": "datasets",
-    "deepspeed": "deepspeed>=0.5.3",
+    "deepspeed": "deepspeed>=0.5.7",
     "docutils": "docutils==0.16.0",
     "fairscale": "fairscale>0.3",
     "faiss-cpu": "faiss-cpu",
