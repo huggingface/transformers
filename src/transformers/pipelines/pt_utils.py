8675820a58ab8b419e55dc4c50176b58b52ba1b9
Nicolas Patry <patry.nicolas@protonmail.com>
2021-11-26 11:00:33 +0100

    `hidden_states` handling.

--- src/transformers/pipelines/pt_utils.py
+++ src/transformers/pipelines/pt_utils.py
@@ -77,8 +77,12 @@ class PipelineIterator(IterableDataset):
             # Batch data is assumed to be BaseModelOutput (or dict)
             loader_batched = {}
             for k, element in self._loader_batch_data.items():
-                if k == "past_key_values":
-                    # Past key values are never used by postprocessing and can be safely discarded
+                if k in {"hidden_states", "past_key_values", "attentions"} and isinstance(element, tuple):
+                    # Those are stored as lists of tensors so need specific unbatching.
+                    if isinstance(element[0], torch.Tensor):
+                        loader_batched[k] = tuple(el[self._loader_batch_index].unsqueeze(0) for el in element)
+                    elif isinstance(element[0], np.ndarray):
+                        loader_batched[k] = tuple(np.expand_dims(el[self._loader_batch_index], 0) for el in element)
                     continue
                 if isinstance(element[self._loader_batch_index], torch.Tensor):
                     # Take correct batch data, but make it looked like batch_size=1
