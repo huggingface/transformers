c468a87a69754b865575f115a2c8efa024c82cb5
Kamal Raj <kamalraj97@gmail.com>
2021-11-30 15:37:55 +0530

    Tapas tf (#13393)
    * TF Tapas first commit
    
    * updated docs
    
    * updated logger message
    
    * updated pytorch weight conversion
    script to support scalar array
    
    * added use_cache to tapas model config to
    work properly with tf input_processing
    
    * 1. rm embeddings_sum
    2. added # Copied
    3. + TFTapasMLMHead
    4. and lot other small fixes
    
    * updated docs
    
    * + test for tapas
    
    * updated testing_utils to check
    is_tensorflow_probability_available
    
    * converted model logits post processing using
    numpy to work with both PT and TF models
    
    * + TFAutoModelForTableQuestionAnswering
    
    * added TF support
    
    * added test for
    TFAutoModelForTableQuestionAnswering
    
    * added test for
    TFAutoModelForTableQuestionAnswering pipeline
    
    * updated auto model docs
    
    * fixed typo in import
    
    * added tensorflow_probability to run tests
    
    * updated MLM head
    
    * updated tapas.rst with TF  model docs
    
    * fixed optimizer import in docs
    
    * updated convert to np
    data from pt model is not
    `transformers.tokenization_utils_base.BatchEncoding`
    after pipeline upgrade
    
    * updated pipeline:
    1. with torch.no_gard removed, pipeline forward handles
    2. token_type_ids converted to numpy
    
    * updated docs.
    
    * removed `use_cache` from config
    
    * removed floats_tensor
    
    * updated code comment
    
    * updated Copyright Year and
    logits_aggregation Optional
    
    * updated docs and comments
    
    * updated docstring
    
    * fixed model weight loading
    
    * make fixup
    
    * fix indentation
    
    * added tf slow pipeline test
    
    * pip upgrade
    
    * upgrade python to 3.7
    
    * removed from_pt from tests
    
    * revert commit f18cfa9

--- src/transformers/models/tapas/tokenization_tapas.py
+++ src/transformers/models/tapas/tokenization_tapas.py
@@ -1897,9 +1897,9 @@ class TapasTokenizer(PreTrainedTokenizer):
             data (:obj:`dict`):
                 Dictionary mapping features to actual values. Should be created using
                 :class:`~transformers.TapasTokenizer`.
-            logits (:obj:`np.ndarray` of shape ``(batch_size, sequence_length)``):
+            logits (:obj:`torch.Tensor` or :obj:`tf.Tensor` of shape ``(batch_size, sequence_length)``):
                 Tensor containing the logits at the token level.
-            logits_agg (:obj:`np.ndarray` of shape ``(batch_size, num_aggregation_labels)``, `optional`):
+            logits_agg (:obj:`torch.Tensor` or :obj:`tf.Tensor` of shape ``(batch_size, num_aggregation_labels)``, `optional`):
                 Tensor containing the aggregation logits.
             cell_classification_threshold (:obj:`float`, `optional`, defaults to 0.5):
                 Threshold to be used for cell selection. All table cells for which their probability is larger than
@@ -1915,6 +1915,11 @@ class TapasTokenizer(PreTrainedTokenizer):
             - predicted_aggregation_indices (``List[int]``of length ``batch_size``, `optional`, returned when
               ``logits_aggregation`` is provided): Predicted aggregation operator indices of the aggregation head.
         """
+        # converting to numpy arrays to work with PT/TF
+        logits = logits.numpy()
+        if logits_agg is not None:
+            logits_agg = logits_agg.numpy()
+        data = {key: value.numpy() for key, value in data.items() if key != "training"}
         # input data is of type float32
         # np.log(np.finfo(np.float32).max) = 88.72284
         # Any value over 88.72284 will overflow when passed through the exponential, sending a warning
@@ -1975,7 +1980,7 @@ class TapasTokenizer(PreTrainedTokenizer):
         output = (predicted_answer_coordinates,)
 
         if logits_agg is not None:
-            predicted_aggregation_indices = logits_agg.argmax(dim=-1)
+            predicted_aggregation_indices = logits_agg.argmax(axis=-1)
             output = (predicted_answer_coordinates, predicted_aggregation_indices.tolist())
 
         return output
