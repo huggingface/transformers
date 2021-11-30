b6201f4514cd1841e4da58c41d38814a4d1a0004
Giacomo Snidero <giacomo.snidero@gmail.com>
2021-11-30 15:04:36 +0100

    update example docstring multilabel example

--- src/transformers/file_utils.py
+++ src/transformers/file_utils.py
@@ -835,7 +835,7 @@ PT_SEQUENCE_CLASSIFICATION_SAMPLE = r"""
         >>> model = {model_class}.from_pretrained('{checkpoint}')
 
         >>> inputs = tokenizer("Hello, my dog is cute", return_tensors="pt")
-        >>> labels = torch.tensor([1, 0]).unsqueeze(0)  # Batch size 1
+        >>> labels = torch.tensor([1]).unsqueeze(0)  # Batch size 1
         >>> outputs = model(**inputs, labels=labels)
         >>> loss = outputs.loss
         >>> logits = outputs.logits
