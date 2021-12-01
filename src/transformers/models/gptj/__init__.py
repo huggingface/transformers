7341a5914506f7b36685be6bc884f43bbb2efcde
Suraj Patil <surajp815@gmail.com>
2021-12-01 10:57:39 +0530

    FlaxGPTJ (#14396)
    * add flax gptj
    
    * no bias in attention dense
    
    * no wpe
    
    * fix rotary embeddings
    
    * fix rotary embeds
    
    * fix rotray embeds
    
    * quality
    
    * doc and quality
    
    * fix equivalence tests

--- src/transformers/models/gptj/__init__.py
+++ src/transformers/models/gptj/__init__.py
@@ -17,7 +17,7 @@
 # limitations under the License.
 from typing import TYPE_CHECKING
 
-from ...file_utils import _LazyModule, is_torch_available
+from ...file_utils import _LazyModule, is_flax_available, is_torch_available
 
 
 _import_structure = {
@@ -33,6 +33,13 @@ if is_torch_available():
         "GPTJPreTrainedModel",
     ]
 
+if is_flax_available():
+    _import_structure["modeling_flax_gptj"] = [
+        "FlaxGPTJForCausalLM",
+        "FlaxGPTJModel",
+        "FlaxGPTJPreTrainedModel",
+    ]
+
 
 if TYPE_CHECKING:
     from .configuration_gptj import GPTJ_PRETRAINED_CONFIG_ARCHIVE_MAP, GPTJConfig
@@ -45,6 +52,9 @@ if TYPE_CHECKING:
             GPTJModel,
             GPTJPreTrainedModel,
         )
+
+    if is_flax_available():
+        from .modeling_flax_gptj import FlaxGPTJForCausalLM, FlaxGPTJModel, FlaxGPTJPreTrainedModel
 
 else:
     import sys
