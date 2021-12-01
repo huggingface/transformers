504bf42181b05ca41b03d17ea3421d303b74d371
Jamie DeAntonis <33379057+JamesDeAntonis@users.noreply.github.com>
2021-11-30 21:00:47 -0500

    WIP: Support for Training with BF16 (#13207)
    * started bf16 integration
    
    * minor changes
    
    * code now runs
    
    * style
    
    * lay foundation for bf16 testing
    
    * lay foundation for bf16 testing
    
    * start the tests
    
    * better bf16 check
    
    * style
    
    * 2 separate checkers - one for bf16 support, another for bf16+autocast
    
    * Update src/transformers/training_args.py
    
    Co-authored-by: Stas Bekman <stas00@users.noreply.github.com>
    
    * a couple of comment resolutions
    
    * more comment resolutions
    
    * resolved a small bug
    
    * just some print statemtns
    
    * added todo marking
    
    * added a todo
    
    * adjust for API change s/fast_dtype/dtype/
    
    * fix style
    
    * merge 2 bf16 util functions
    
    * bf16 now does scaling too
    
    * Add support for bfloat16
    
    * Revert T5 layernorm to float32
    
    This is based on the comment at https://github.com/huggingface/transformers/pull/14448/files#r752660929 and the PyTorch PR https://github.com/pytorch/pytorch/pull/66920 .
    
    * Add comment about conversion to float32 before returning the numpy data
    
    * Add comment about AMP-bfloat16 incompatibility
    
    * Fix formatting
    
    * typo
    
    * reformer / bf16
    
    * cleanup
    
    * require at least pt-1.10
    
    * fix
    
    * will deal with deepspeed separately
    
    * cleanup
    
    * revert
    
    * cleanup
    
    * fp16_full_eval and bf16_full_eval are separate modes
    
    * proper deprecation
    
    * cleanup
    
    * test and fixes
    
    * spelling
    
    * cleanup
    
    * add a note that this API is experimental
    
    Co-authored-by: jamie <jamie@cortx.com>
    Co-authored-by: Stas Bekman <stas@stason.org>
    Co-authored-by: Stas Bekman <stas00@users.noreply.github.com>
    Co-authored-by: suriya <suriya@cortx.com>
    Co-authored-by: Manuel R. Ciosici <manuelrciosici@gmail.com>

--- src/transformers/file_utils.py
+++ src/transformers/file_utils.py
@@ -320,6 +320,37 @@ def is_torch_cuda_available():
         return False
 
 
+def is_torch_bf16_available():
+    if is_torch_available():
+        import torch
+
+        # since currently no utility function is available we build our own.
+        # some bits come from https://github.com/pytorch/pytorch/blob/2289a12f21c54da93bf5d696e3f9aea83dd9c10d/torch/testing/_internal/common_cuda.py#L51
+        # with additional check for torch version
+        # to succeed:
+        # 1. the hardware needs to support bf16 (arch >= Ampere)
+        # 2. torch >= 1.10 (1.9 should be enough for AMP API has changed in 1.10, so using 1.10 as minimal)
+        # 3. CUDA >= 11
+        # 4. torch.autocast exists
+        # XXX: one problem here is that it may give invalid results on mixed gpus setup, so it's
+        # really only correct for the 0th gpu (or currently set default device if different from 0)
+
+        if not torch.cuda.is_available() or torch.version.cuda is None:
+            return False
+        if torch.cuda.get_device_properties(torch.cuda.current_device()).major < 8:
+            return False
+        if int(torch.version.cuda.split(".")[0]) < 11:
+            return False
+        if not version.parse(torch.__version__) >= version.parse("1.10"):
+            return False
+        if not hasattr(torch, "autocast"):
+            return False
+
+        return True
+    else:
+        return False
+
+
 _torch_fx_available = _torch_onnx_dict_inputs_support_available = False
 if _torch_available:
     torch_version = version.parse(importlib_metadata.version("torch"))
