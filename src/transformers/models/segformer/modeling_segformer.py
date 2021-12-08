16870d114b652954bb0ccf069ad7433a93ac06fb
Yih-Dar <2521628+ydshieh@users.noreply.github.com>
2021-12-08 20:25:48 +0100

    Fix wrong checkpoint paths in doc examples (#14685)
    Co-authored-by: ydshieh <ydshieh@users.noreply.github.com>

--- src/transformers/models/segformer/modeling_segformer.py
+++ src/transformers/models/segformer/modeling_segformer.py
@@ -490,8 +490,8 @@ class SegformerModel(SegformerPreTrainedModel):
             >>> from PIL import Image
             >>> import requests
 
-            >>> feature_extractor = SegformerFeatureExtractor.from_pretrained("nvidia/segformer-b0")
-            >>> model = SegformerModel("nvidia/segformer-b0")
+            >>> feature_extractor = SegformerFeatureExtractor.from_pretrained("nvidia/segformer-b0-finetuned-ade-512-512")
+            >>> model = SegformerModel("nvidia/segformer-b0-finetuned-ade-512-512")
 
             >>> url = 'http://images.cocodataset.org/val2017/000000039769.jpg'
             >>> image = Image.open(requests.get(url, stream=True).raw)
@@ -726,7 +726,7 @@ class SegformerForSemanticSegmentation(SegformerPreTrainedModel):
             >>> import requests
 
             >>> feature_extractor = SegformerFeatureExtractor.from_pretrained("nvidia/segformer-b0-finetuned-ade-512-512")
-            >>> model = SegformerForSemanticSegmentation("nvidia/segformer-b0-finetuned-ade-512-512")
+            >>> model = SegformerForSemanticSegmentation.from_pretrained("nvidia/segformer-b0-finetuned-ade-512-512")
 
             >>> url = 'http://images.cocodataset.org/val2017/000000039769.jpg'
             >>> image = Image.open(requests.get(url, stream=True).raw)
