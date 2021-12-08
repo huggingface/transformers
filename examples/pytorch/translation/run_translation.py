66d18b3f10f1d6e09b4ea7d8dcc97a1e81c790f7
Gaurang Tandon <1gaurangtandon@gmail.com>
2021-12-08 08:22:15 +0530

    fix(run_translation.py): json/jsonl validation
    Both json and jsonl are to be accepted as valid jsonlines file extension

--- examples/pytorch/translation/run_translation.py
+++ examples/pytorch/translation/run_translation.py
@@ -216,12 +216,16 @@ class DataTrainingArguments:
         elif self.source_lang is None or self.target_lang is None:
             raise ValueError("Need to specify the source language and the target language.")
 
+        # accepting both json and jsonl file extensions, as
+        # many jsonlines files actually have a .json extension
+        valid_jsonl_file_extensions = ["json", "jsonl"]
+        
         if self.train_file is not None:
             extension = self.train_file.split(".")[-1]
-            assert extension == "jsonl", "`train_file` should be a jsonlines file."
+            assert extension in valid_jsonl_file_extensions, "`train_file` should be a jsonlines file."
         if self.validation_file is not None:
             extension = self.validation_file.split(".")[-1]
-            assert extension == "jsonl", "`validation_file` should be a jsonlines file."
+            assert extension in valid_jsonl_file_extensions, "`validation_file` should be a jsonlines file."
         if self.val_max_target_length is None:
             self.val_max_target_length = self.max_target_length
 
