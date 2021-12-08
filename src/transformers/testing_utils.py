cb0b94318e6e9ba36734508b7f78933d8784e327
Patrick von Platen <patrick.v.platen@gmail.com>
2021-12-08 12:07:54 +0100

    [Wav2Vec2] PyCTCDecode Integration to support language model boosted decoding (#14339)
    * up
    
    * up
    
    * up
    
    * make it cleaner
    
    * correct
    
    * make styhahalal
    
    * add more tests
    
    * finish
    
    * small fix
    
    * make style
    
    * up
    
    * tryout to solve cicrle ci
    
    * up
    
    * fix more tests
    
    * fix more tests
    
    * apply sylvains suggestions
    
    * fix import
    
    * correct docs
    
    * add pyctcdecode only to speech tests
    
    * fix more tests
    
    * add tf, flax and pt tests
    
    * add pt
    
    * fix last tests
    
    * fix more tests
    
    * Apply suggestions from code review
    
    * change lines
    
    * Apply suggestions from code review
    
    Co-authored-by: Anton Lozhkov <aglozhkov@gmail.com>
    
    * correct tests
    
    * correct tests
    
    * add doc string
    
    Co-authored-by: Anton Lozhkov <aglozhkov@gmail.com>

--- src/transformers/testing_utils.py
+++ src/transformers/testing_utils.py
@@ -36,8 +36,10 @@ from .file_utils import (
     is_faiss_available,
     is_flax_available,
     is_keras2onnx_available,
+    is_librosa_available,
     is_onnx_available,
     is_pandas_available,
+    is_pyctcdecode_available,
     is_pytesseract_available,
     is_pytorch_quantization_available,
     is_rjieba_available,
@@ -594,6 +596,26 @@ def require_deepspeed(test_case):
     """
     if not is_deepspeed_available():
         return unittest.skip("test requires deepspeed")(test_case)
+    else:
+        return test_case
+
+
+def require_pyctcdecode(test_case):
+    """
+    Decorator marking a test that requires pyctcdecode
+    """
+    if not is_pyctcdecode_available():
+        return unittest.skip("test requires pyctcdecode")(test_case)
+    else:
+        return test_case
+
+
+def require_librosa(test_case):
+    """
+    Decorator marking a test that requires librosa
+    """
+    if not is_librosa_available():
+        return unittest.skip("test requires librosa")(test_case)
     else:
         return test_case
 
