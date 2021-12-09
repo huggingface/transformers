5e2f388e11dbaf8bf1df5a83d997a1ef2ac22e7a
Lysandre <lysandre.debut@reseau.eseo.fr>
2021-12-09 16:55:21 +0100

    Release: v4.13.0

--- setup.py
+++ setup.py
@@ -354,7 +354,7 @@ install_requires = [
 
 setup(
     name="transformers",
-    version="4.13.0.dev0",  # expected format is one of x.y.z.dev0, or x.y.z.rc1 or x.y.z (no to dashes, yes to dots)
+    version="4.13.0",  # expected format is one of x.y.z.dev0, or x.y.z.rc1 or x.y.z (no to dashes, yes to dots)
     author="Thomas Wolf, Lysandre Debut, Victor Sanh, Julien Chaumond, Sam Shleifer, Patrick von Platen, Sylvain Gugger, Suraj Patil, Stas Bekman, Google AI Language Team Authors, Open AI team Authors, Facebook AI Authors, Carnegie Mellon University Authors",
     author_email="thomas@huggingface.co",
     description="State-of-the-art Natural Language Processing for TensorFlow 2.0 and PyTorch",
