e66224c05a91e259e8b500e065ebfe4db928670d
matt <rocketknight1@gmail.com>
2021-12-16 12:43:44 +0000

    Working on splitting out labels

--- src/transformers/keras_callbacks.py
+++ src/transformers/keras_callbacks.py
@@ -2,8 +2,10 @@ import logging
 import os
 from pathlib import Path
 from time import sleep
-from typing import Optional, Union
+from typing import Optional, Union, Callable
+import numpy as np
 
+import tensorflow as tf
 from tensorflow.keras.callbacks import Callback
 
 from huggingface_hub import Repository
@@ -15,6 +17,67 @@ from .modelcard import TrainingSummary
 
 logger = logging.getLogger(__name__)
 
+
+class KerasMetricCallback(Callback):
+    """
+    Callback to prompt metrics at the end of every epoch.
+
+    Args:
+        metric_fn: Metric function provided by the user.
+        val_dataset: Validation data to be used to evaluate the model at
+        the end of the epoch.
+        metric_name: Name of the metric calculated in metric_fn.
+        batch_size: Batch size.
+        labels: Labels.
+    """
+
+    def __init__(self, metric_fn: Callable,
+                 val_dataset: Union[tf.data.Dataset, np.ndarray, tf.Tensor, tuple, dict],
+                 metric_name: Optional[str],
+                 label_names: Optional[str],
+                 batch_size: Optional[int] = None):
+        super().__init__()
+        self.metric_fn = metric_fn
+        self.batch_size = batch_size
+        if not isinstance(val_dataset, tf.data.Dataset):
+            if batch_size is None:
+                raise ValueError("When passing data to KerasMetricCallback that is not a pre-batched tf.data.Dataset "
+                                 "the batch_size argument must be set.")
+            # Wrap a tf.data.Dataset around it
+            val_dataset = tf.data.Dataset.from_tensor_slices(val_dataset).batch(batch_size, drop_remainder=False)
+        self.val_dataset = val_dataset
+        self.metric_name = metric_name
+        self.label_names = "labels" if label_names is None else label_names
+
+    def on_epoch_end(self, epoch, logs=None):
+
+        prediction_list = []
+        label_list = []
+
+        for batch in self.val_dataset:
+
+            if isinstance(batch, tuple):
+                batch, labels = batch
+                labels = np.asarray(labels)
+
+            elif isinstance(batch, dict):
+                labels = np.asarray(batch["labels"])
+
+            predictions = self.model.predict(batch)
+            predictions_dict = dict(predictions)
+
+            for prediction in predictions_dict["logits"]:
+                prediction_list.append(predictions)
+
+            for label in labels:
+                label_list.append(label)
+
+        metric_value = self.metric_fn(predictions=np.asarray(prediction_list), labels=np.asarray(label_list))
+
+        if metric_name is not None:
+            print(f"{self.metric_name} for epoch {epoch} is {metric_value}")
+        else:
+            print(f"At epoch {epoch}: {metric_value}")
 
 class PushToHubCallback(Callback):
     def __init__(
@@ -29,32 +92,35 @@ class PushToHubCallback(Callback):
         **model_card_args
     ):
         """
-        output_dir (:obj:`str`):
-            The output directory where the model predictions and checkpoints will be written and synced with the
-            repository on the Hub.
-        save_strategy (:obj:`str` or :class:`~transformers.trainer_utils.IntervalStrategy`, `optional`, defaults to :obj:`"epoch"`):
-            The checkpoint save strategy to adopt during training. Possible values are:
+        Callback for pushing the model to the Hub after training.
 
-                * :obj:`"no"`: No save is done during training.
-                * :obj:`"epoch"`: Save is done at the end of each epoch.
-                * :obj:`"steps"`: Save is done every :obj:`save_steps`
-        save_steps (:obj:`int`, `optional`):
-            The number of steps between saves when using the "steps" save_strategy.
-        tokenizer (:obj:`PreTrainedTokenizerBase`, `optional`):
-            The tokenizer used by the model. If supplied, will be uploaded to the repo alongside the weights.
-        hub_model_id (:obj:`str`, `optional`):
-            The name of the repository to keep in sync with the local `output_dir`. It can be a simple model ID in
-            which case the model will be pushed in your namespace. Otherwise it should be the whole repository name,
-            for instance :obj:`"user_name/model"`, which allows you to push to an organization you are a member of with
-            :obj:`"organization_name/model"`.
+        Args:
+            output_dir (:obj:`str`):
+                The output directory where the model predictions and checkpoints will be written and synced with the
+                repository on the Hub.
+            save_strategy (:obj:`str` or :class:`~transformers.trainer_utils.IntervalStrategy`, `optional`, defaults to :obj:`"epoch"`):
+                The checkpoint save strategy to adopt during training. Possible values are:
 
-            Will default to to the name of :obj:`output_dir`.
-        hub_token (:obj:`str`, `optional`):
-            The token to use to push the model to the Hub. Will default to the token in the cache folder obtained with
-            :obj:`huggingface-cli login`.
-        checkpoint (:obj:`bool`, `optional`, defaults to :obj:`False`):
-            Whether to save full training checkpoints (including epoch and optimizer state) to allow training to be
-            resumed. Only usable when `save_strategy` is `epoch`.
+                    * :obj:`"no"`: No save is done during training.
+                    * :obj:`"epoch"`: Save is done at the end of each epoch.
+                    * :obj:`"steps"`: Save is done every :obj:`save_steps`
+            save_steps (:obj:`int`, `optional`):
+                The number of steps between saves when using the "steps" save_strategy.
+            tokenizer (:obj:`PreTrainedTokenizerBase`, `optional`):
+                The tokenizer used by the model. If supplied, will be uploaded to the repo alongside the weights.
+            hub_model_id (:obj:`str`, `optional`):
+                The name of the repository to keep in sync with the local `output_dir`. It can be a simple model ID in
+                which case the model will be pushed in your namespace. Otherwise it should be the whole repository name,
+                for instance :obj:`"user_name/model"`, which allows you to push to an organization you are a member of with
+                :obj:`"organization_name/model"`.
+
+                Will default to to the name of :obj:`output_dir`.
+            hub_token (:obj:`str`, `optional`):
+                The token to use to push the model to the Hub. Will default to the token in the cache folder obtained with
+                :obj:`huggingface-cli login`.
+            checkpoint (:obj:`bool`, `optional`, defaults to :obj:`False`):
+                Whether to save full training checkpoints (including epoch and optimizer state) to allow training to be
+                resumed. Only usable when `save_strategy` is `epoch`.
         """
         super().__init__()
         if checkpoint and save_strategy != "epoch":
