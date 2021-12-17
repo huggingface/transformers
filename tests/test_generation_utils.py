50dd447eed916fbd095e167da5a2bb96903dd258
Patrick von Platen <patrick.v.platen@gmail.com>
2021-12-17 14:57:36 +0000

    correct

--- tests/test_generation_utils.py
+++ tests/test_generation_utils.py
@@ -1725,20 +1725,24 @@ class GenerationIntegrationTests(unittest.TestCase):
         # need extrem generation values here to force this test
         # to fail when `attention_mask` is not correctly treated in generate
         model = BartForConditionalGeneration.from_pretrained(
-            "hf-internal-testing/tiny-random-bart", max_length=40, num_beams=3, do_sample=True
+            "hf-internal-testing/tiny-random-bart", max_length=50, num_beams=5, num_return_sequences=5
         ).to(torch_device)
 
         model.config.eos_token_id = None
         input_ids = tokenizer(articles[0], return_tensors="pt").input_ids.to(torch_device)
         input_ids_batched = tokenizer(articles, padding=True, return_tensors="pt").input_ids.to(torch_device)
 
-        torch.manual_seed(0)
-        output_sequences = model.generate(input_ids=input_ids)
-        torch.manual_seed(0)
-        output_sequences_batched = model.generate(input_ids=input_ids_batched)
+        output_sequences_batched = model.generate(
+            input_ids=input_ids_batched, return_dict_in_generate=True, output_scores=True
+        )
+        output_sequences = model.generate(input_ids=input_ids, return_dict_in_generate=True, output_scores=True)
 
-        # make sure model correctly creates attention_mask on the fly
-        self.assertListEqual(output_sequences[0].tolist(), output_sequences_batched[0].tolist())
+        batched_out = output_sequences_batched.sequences_scores
+        out = output_sequences.sequences_scores
+
+        diff = (batched_out[:5].sum() - out.sum()).abs()
+
+        self.assertTrue(diff < 1e-4)
 
     def test_decoder_generate_with_inputs_embeds(self):
         article = """I need input_ids to generate"""
