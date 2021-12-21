a21f4e67c0a4f674c2e086a62df3e6815aeb6065
Sylvain Gugger <sylvain.gugger@gmail.com>
2021-12-21 18:28:54 -0500

    Remove double returns

--- src/transformers/models/perceiver/modeling_perceiver.py
+++ src/transformers/models/perceiver/modeling_perceiver.py
@@ -1087,12 +1087,7 @@ class PerceiverForSequenceClassification(PerceiverPreTrainedModel):
         self.post_init()
 
     @add_start_docstrings_to_model_forward(PERCEIVER_INPUTS_DOCSTRING.format("batch_size, sequence_length"))
-    @add_code_sample_docstrings(
-        processor_class=_TOKENIZER_FOR_DOC,
-        checkpoint=_CHECKPOINT_FOR_DOC,
-        output_type=PerceiverClassifierOutput,
-        config_class=_CONFIG_FOR_DOC,
-    )
+    @replace_return_docstrings(output_type=PerceiverClassifierOutput, config_class=_CONFIG_FOR_DOC)
     def forward(
         self,
         inputs=None,
