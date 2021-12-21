f43218658cf3cf5b7fade4c41b343daad43d12e0
Stas Bekman <stas00@users.noreply.github.com>
2021-12-20 20:48:38 -0800

    [logging] implement warning_advice / TRANSFORMERS_NO_ADVISORY_WARNINGS (#14669)
    * [logging] implement warning_advice / TRANSFORMERS_NO_ADVISORY_WARNINGS
    
    * reword

--- src/transformers/utils/logging.py
+++ src/transformers/utils/logging.py
@@ -263,3 +263,17 @@ def reset_format() -> None:
 
     for handler in handlers:
         handler.setFormatter(None)
+
+
+def warning_advice(self, *args, **kwargs):
+    """
+    This method is identical to `logger.warning()`, but if env var TRANSFORMERS_NO_ADVISORY_WARNINGS=1 is set, this
+    warning will not be printed
+    """
+    no_advisory_warnings = os.getenv("TRANSFORMERS_NO_ADVISORY_WARNINGS", False)
+    if no_advisory_warnings:
+        return
+    self.warning(*args, **kwargs)
+
+
+logging.Logger.warning_advice = warning_advice
