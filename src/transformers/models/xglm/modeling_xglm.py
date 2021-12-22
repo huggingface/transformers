69a051b474dc23b4b5e006b2d24b54c105d53267
patil-suraj <surajp815@gmail.com>
2021-12-22 02:34:44 +0100

    typo

--- src/transformers/models/xglm/modeling_xglm.py
+++ src/transformers/models/xglm/modeling_xglm.py
@@ -985,7 +985,7 @@ class XGLMForCausalLM(XGLMPreTrainedModel):
         return_dict = return_dict if return_dict is not None else self.config.use_return_dict
 
         # decoder outputs consists of (dec_features, layer_state, dec_hidden, dec_attn)
-        outputs = self.model.decoder(
+        outputs = self.model(
             input_ids=input_ids,
             attention_mask=attention_mask,
             encoder_hidden_states=encoder_hidden_states,
