eed928cbea0eb3cec0435f92172093c1c68e47df
Stas Bekman <stas00@users.noreply.github.com>
2021-12-23 10:19:34 -0800

    [doc] post-porting (#14890)
    found a few oddities:
    
    1. https://huggingface.co/docs/transformers/main_classes/logging#transformers.utils.logging.enable_explicit_format
    has a :: - this PR fixes it
    
    2.  this looks borked too:
    https://huggingface.co/docs/transformers/main_classes/logging#transformers.utils.logging.set_verbosity
     has a <
    
    but I'm not sure where this one is coming from

--- src/transformers/utils/logging.py
+++ src/transformers/utils/logging.py
@@ -240,11 +240,9 @@ def enable_propagation() -> None:
 def enable_explicit_format() -> None:
     """
     Enable explicit formatting for every HuggingFace Transformers's logger. The explicit formatter is as follows:
-
-    ::
-
+    ```
         [LEVELNAME|FILENAME|LINE NUMBER] TIME >> MESSAGE
-
+    ```
     All handlers currently bound to the root logger are affected by this method.
     """
     handlers = _get_library_root_logger().handlers
