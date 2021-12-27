e32991f1fd9af25b775dacc2af373277b540a63c
Stas Bekman <stas@stason.org>
2021-12-27 13:54:51 -0800

    [doc] consistent True/False/None default format

--- src/transformers/training_args.py
+++ src/transformers/training_args.py
@@ -105,7 +105,7 @@ class TrainingArguments:
                 - `"steps"`: Evaluation is done (and logged) every `eval_steps`.
                 - `"epoch"`: Evaluation is done at the end of each epoch.
 
-        prediction_loss_only (`bool`, *optional*, defaults to *False*):
+        prediction_loss_only (`bool`, *optional*, defaults to `False`):
             When performing evaluation and generating predictions, only returns the loss.
         per_device_train_batch_size (`int`, *optional*, defaults to 8):
             The batch size per GPU/TPU core/CPU for training.
@@ -175,7 +175,7 @@ class TrainingArguments:
         logging_steps (`int`, *optional*, defaults to 500):
             Number of update steps between two logs if `logging_strategy="steps"`.
         logging_nan_inf_filter (`bool`, *optional*, defaults to `True`):
-            Whether to filter `nan` and `inf` losses for logging. If set to obj:*True* the loss of every step that is
+            Whether to filter `nan` and `inf` losses for logging. If set to obj:`True` the loss of every step that is
             `nan` or `inf` is filtered and the average loss of the current logging window is taken instead.
 
             <Tip>
