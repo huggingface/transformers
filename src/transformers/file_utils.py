b666594c300d1320fc58fbc5b96d25ff6d6cb44f
Sylvain Gugger <sylvain.gugger@gmail.com>
2021-12-27 16:10:37 -0500

    Fix add_end_docstring decorator

--- src/transformers/file_utils.py
+++ src/transformers/file_utils.py
@@ -855,7 +855,7 @@ def add_start_docstrings_to_model_forward(*docstr):
 
 def add_end_docstrings(*docstr):
     def docstring_decorator(fn):
-        fn.__doc__ = fn.__doc__ + "".join(docstr)
+        fn.__doc__ = (fn.__doc__ if fn.__doc__ is not None else "") + "".join(docstr)
         return fn
 
     return docstring_decorator
