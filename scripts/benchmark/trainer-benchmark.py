51929f047371cb0e7e75c1dca9106ee1e737414d
Stas Bekman <stas@stason.org>
2021-12-27 22:39:05 -0800

    massive rework/expansion

--- scripts/benchmark/trainer-benchmark.py
+++ scripts/benchmark/trainer-benchmark.py
@@ -7,7 +7,7 @@
 # The main idea is:
 # ./trainer-benchmark.py --base-cmd '<cmd args that don't change>' \
 # --dims '--tf32 0 --tf32 1' '--fp16 0 --fp16 1 --bf16 1' \
-# --metric-key train_samples_per_second
+# --target-metric-key train_samples_per_second
 #
 # --dims allows you to compare multiple dimensions.
 #
@@ -36,7 +36,7 @@
 # ' \
 # --dims '--tf32 0 --tf32 1' '--fp16 0 --fp16 1 --bf16 1' \
 # --base-dim '--tf32 0 --fp16 0' \
-# --metric-key train_samples_per_second --repeat-times 1
+# --target-metric-key train_samples_per_second --repeat-times 1
 #
 # and here is a possible output:
 #
@@ -56,13 +56,13 @@
 # Typically running each experiment once is enough, but if the environment is unstable you can
 # re-run each multiple times, e.g., 3 using --repeat-times 3 and it will report the averaged results.
 #
-# By default it'll use the worst result as the base line to use as 100% and then compare the rest to
+# By default it'll use the lowest result as the base line to use as 100% and then compare the rest to
 # it as can be seen from the table above, but you can also specify which combination is the one to use as
 # the baseline, e.g., to change to another entry use: --base-dim '--tf32 1 --fp16 0'
 #
-# --metric-key is there to tell the program which metrics to compare - the different metric keys are
+# --target-metric-key is there to tell the program which metrics to compare - the different metric keys are
 # inside output_dir/all_results.json. e.g., to measure eval performance instead of train use
-# --metric-key eval_samples_per_second
+# --target-metric-key eval_samples_per_second
 
 
 import argparse
@@ -73,8 +73,10 @@ import re
 import shlex
 import subprocess
 import sys
+from pathlib import Path
 from statistics import fmean
 
+import pandas as pd
 from tqdm import tqdm
 
 
@@ -94,36 +96,17 @@ def get_base_cmd(args, output_dir):
     return [sys.executable] + shlex.split(args.base_cmd)
 
 
-def process_run(id, cmd, opt, repeat_times, output_dir, metric_key, verbose):
-    results = []
-    preamble = f"{id}: {opt}"
-    outcome = f"{preamble}: "
-    for i in tqdm(range(repeat_times), desc=preamble, leave=False):
-        result = process_run_single(id, cmd, opt, output_dir, metric_key, verbose)
-        if result != -1:
-            results.append(result)
-            outcome += "✓"
-        else:
-            outcome += "✘"
-    outcome = f"\33[2K\r{outcome}"
-    if len(results):
-        mean_result = round(fmean(results), 2)
-        results_str = f"{outcome} {mean_result}"
-        if len(results) > 1:
-            results_str += f" ({[round(x, 2) for x in results]})"
-        print(results_str)
-        return mean_result
-    else:
-        print(outcome)
-        return -1
-
-
-def process_run_single(id, cmd, opt, output_dir, metric_key, verbose):
+def process_run_single(id, cmd, opt, output_dir, target_metric_key, metric_keys, verbose):
     # enable to debug everything but the run itself, to do it fast and see the progress
-    # from random import randint
-    # from time import sleep
-    # sleep(3)
-    # return randint(100, 300)
+    if 1:
+        import random
+        from random import randint
+        from time import sleep
+
+        sleep(0)
+        return dict(
+            {k: randint(1, 30) for k in metric_keys}, **{target_metric_key: random.choice([-1, 10, 100, 55, 222])}
+        )
 
     result = subprocess.run(cmd, capture_output=True, text=True)
 
@@ -131,42 +114,113 @@ def process_run_single(id, cmd, opt, output_dir, metric_key, verbose):
         print("STDOUT", result.stdout)
         print("STDERR", result.stderr)
 
+    # save the streams
+    prefix = opt.replace(" ", "-")
+    with open(Path(output_dir) / f"{prefix}.stdout.txt", "w") as f:
+        f.write(result.stdout)
+    with open(Path(output_dir) / f"{prefix}.stderr.txt", "w") as f:
+        f.write(result.stderr)
+
     if result.returncode != 0:
         if verbose:
             print("failed")
-        return -1
+        return {target_metric_key: -1}
 
-    filename = f"{output_dir}/all_results.json"
-    with io.open(filename, "r", encoding="utf-8") as f:
+    with io.open(f"{output_dir}/all_results.json", "r", encoding="utf-8") as f:
         metrics = json.load(f)
-    return metrics[metric_key]
+
+    # filter out just the keys we want
+    return {k: v for k, v in metrics.items() if k in metric_keys}
 
 
-def process_results(results, metric_key, base_dim):
-
-    print(f"\n*** Results: {metric_key}\n")
-
-    col_opt, col_result, col_relative = "Variations", "Result", "%"
-    width_opt = max(len(k) for k in list(results.keys()) + [col_opt])
-    width_metric = max(len(str(v)) for v in list(results.values()) + [col_result])
-    width_percent = 5
-
-    if base_dim is not None and base_dim in results:
-        sentinel_value = results[base_dim]
-    else:
-        # if no match, use the minimal value as the sentinel
-        sentinel_value = min(v for v in results.values() if v != -1)
-
-    print(f"| {col_opt:^{width_opt}} | {col_result:^{width_metric}} | {col_relative:^{width_percent}} |")
-    print(f"| {'-'*width_opt:{width_opt}} | {'-'*width_metric:{width_metric}} | {'-'*width_percent:{width_percent}} |")
-    for key, value in results.items():
-        if value != -1:
-            percent = f"{int(100*value/sentinel_value)}%"
-            value = f"{value:.02f}"
+def process_run(id, cmd, opt_key, opt, target_metric_key, report_metric_keys, repeat_times, output_dir, verbose):
+    metrics = []
+    preamble = f"{id}: {opt}"
+    outcome = f"{preamble}: "
+    metric_keys = set(report_metric_keys + [target_metric_key])
+    for i in tqdm(range(repeat_times), desc=preamble, leave=False):
+        single_run_metrics = process_run_single(id, cmd, opt, output_dir, target_metric_key, metric_keys, verbose)
+        result = single_run_metrics[target_metric_key]
+        if result != -1:
+            metrics.append(single_run_metrics)
+            outcome += "✓"
         else:
-            percent = "✘"
-            value = "✘"
-        print(f"| {key:{width_opt}} | {value:>{width_metric}} | {percent:>{width_percent}} |")
+            outcome += "✘"
+    outcome = f"\33[2K\r{outcome}"
+    successful_runs = len(metrics)
+    if successful_runs > 0:
+        mean_metrics = {k: fmean([metrics[i][k] for i in range(successful_runs)]) for k in metrics[0].keys()}
+        mean_target = round(mean_metrics[target_metric_key], 2)
+        results_str = f"{outcome} {mean_target}"
+        if successful_runs > 1:
+            results_str += f" ({[round(x, 2) for x in mean_metrics[target_metric_key]]})"
+        print(results_str)
+        mean_metrics[opt_key] = opt
+        return mean_metrics
+    else:
+        print(outcome)
+        return {opt_key: opt, target_metric_key: -1}
+
+
+def get_versions():
+    import datetime
+
+    import torch
+
+    import transformers
+
+    properties = torch.cuda.get_device_properties(torch.device("cuda"))
+    return f"""
+Datetime    : {datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}
+transformers: {transformers.__version__}
+torch       : {torch.__version__}
+cuda        : {torch.version.cuda}
+{torch.cuda.device_count()} GPUs      : {properties.name}, {properties.total_memory/2**30:0.2f}GB
+"""
+
+
+def process_results(results, target_metric_key, report_metric_keys, base_dim, table_format, output_dir):
+
+    df = pd.DataFrame(results)
+    variation_key = "variation"
+    diff_key = "diff_%"
+
+    sentinel_value = -1
+    if base_dim is not None and len(df[df.variation == base_dim]):
+        # this may still return -1
+        sentinel_value = df.loc[df.variation == base_dim][target_metric_key]
+    if sentinel_value == -1:
+        # as a fallback, use the minimal value as the sentinel
+        sentinel_value = df.loc[df[target_metric_key] != -1][target_metric_key].min()
+
+    # create diff column
+    if sentinel_value != -1:
+        df[diff_key] = df.apply(
+            lambda r: int(100 * r[target_metric_key] / sentinel_value) if r[target_metric_key] != -1 else "✘",
+            axis="columns",
+        )
+
+    # deal with failed runs
+    df[target_metric_key] = df.apply(
+        lambda r: r[target_metric_key] if r[target_metric_key] != -1 else "✘", axis="columns"
+    )
+
+    # re-order columns
+    cols = [variation_key, target_metric_key, diff_key, *report_metric_keys]
+    df = df.reindex(cols, axis="columns")  # reorder cols
+
+    # capitalize
+    df = df.rename(str.capitalize, axis="columns")
+
+    # make the cols as narrow as possible
+    linebreak = "<br>" if table_format == "github" else "\n"
+    df = df.rename(lambda c: c.replace("_", linebreak), axis="columns")
+
+    print("\n*** Results:\n")
+    print(df.to_markdown(index=False))
+    print(f"\nNote: each run's output is also logged under {output_dir}/*.std*.txt")
+    print("\nPlease include the following information with your benchmark post:")
+    print(get_versions())
 
 
 def main():
@@ -193,17 +247,31 @@ def main():
         help="Dimension base line arg. if None the minimal value will be used to compare against",
     )
     parser.add_argument(
-        "--metric-key",
+        "--target-metric-key",
         default=None,
         type=str,
         required=True,
-        help="Metric key in output_dir/all_results.json, e.g., train_samples_per_second",
+        help="Target metric key in output_dir/all_results.json, e.g., train_samples_per_second",
+    )
+    parser.add_argument(
+        "--report-metric-keys",
+        default="",
+        type=str,
+        help="Report metric keys - other metric keys from output_dir/all_results.json to report, e.g., train_loss. Use a single argument e.g., 'train_loss train_samples",
     )
     parser.add_argument(
         "--repeat-times",
         default=1,
         type=int,
         help="How many times to re-run each combination - an average will be reported",
+    )
+    # table_format_choices
+    parser.add_argument(
+        "--table-format",
+        default="console",
+        type=str,
+        choices=["github", "console"],
+        help="Format the results table to render best in the destination use",
     )
     parser.add_argument(
         "--verbose",
@@ -221,15 +289,31 @@ def main():
     # build a cartesian product of dimensions and convert those back into cmd-line arg strings
     opts = list(map(" ".join, itertools.product(*dims)))
 
+    # split wanted keys
+    report_metric_keys = args.report_metric_keys.split()
+
     print(f"\n*** Running {len(opts)} benchmarks:")
     print(f"Base command: {' '.join(base_cmd)}")
 
-    results = {}
+    opt_key = "variation"
+    results = []
     for id, opt in enumerate(tqdm(opts, desc="Total completion: ", leave=False)):
         cmd = base_cmd + opt.split()
-        results[opt] = process_run(id + 1, cmd, opt, args.repeat_times, output_dir, args.metric_key, args.verbose)
+        results.append(
+            process_run(
+                id + 1,
+                cmd,
+                opt_key,
+                opt,
+                args.target_metric_key,
+                report_metric_keys,
+                args.repeat_times,
+                output_dir,
+                args.verbose,
+            )
+        )
 
-    process_results(results, args.metric_key, args.base_dim)
+    process_results(results, args.target_metric_key, report_metric_keys, args.base_dim, args.table_format, output_dir)
 
 
 if __name__ == "__main__":
