5050232733c255fd5ea21dce41fb97a69979a951
Patrick von Platen <patrick.v.platen@gmail.com>
2021-12-28 12:41:18 +0100

    correct more in wav2vec2

--- src/transformers/models/wav2vec2/modeling_wav2vec2.py
+++ src/transformers/models/wav2vec2/modeling_wav2vec2.py
@@ -1221,14 +1221,14 @@ class Wav2Vec2Model(Wav2Vec2PreTrainedModel):
             "Please use the equivalent `freeze_feature_encoder` method instead.",
             FutureWarning,
         )
-        self.wav2vec2.feature_extractor._freeze_parameters()
+        self.feature_extractor._freeze_parameters()
 
     def freeze_feature_encoder(self):
         """
         Calling this function will disable the gradient computation for the feature encoder so that its parameter will
         not be updated during training.
         """
-        self.wav2vec2.feature_extractor._freeze_parameters()
+        self.feature_extractor._freeze_parameters()
 
     def _mask_hidden_states(
         self,
