85a5772ab3a3a255ca8c681735eca7f831c74515
Patrick von Platen <patrick.v.platen@gmail.com>
2021-12-30 09:56:43 +0100

    [AutoProcessor] Correct AutoProcessor and automatically add processor… (#14881)
    * [AutoProcessor] Correct AutoProcessor and automatically add processor class
    
    * up
    
    * up
    
    * up
    
    * up
    
    * up
    
    * up
    
    * up
    
    * up
    
    * continue tomorrow
    
    * up
    
    * up
    
    * up
    
    * make processor class private
    
    * fix loop

--- src/transformers/models/speech_to_text/processing_speech_to_text.py
+++ src/transformers/models/speech_to_text/processing_speech_to_text.py
@@ -69,8 +69,10 @@ class Speech2TextProcessor:
                 Directory where the feature extractor JSON file and the tokenizer files will be saved (directory will
                 be created if it does not exist).
         """
-
+        self.feature_extractor._set_processor_class(self.__class__.__name__)
         self.feature_extractor.save_pretrained(save_directory)
+
+        self.tokenizer._set_processor_class(self.__class__.__name__)
         self.tokenizer.save_pretrained(save_directory)
 
     @classmethod
