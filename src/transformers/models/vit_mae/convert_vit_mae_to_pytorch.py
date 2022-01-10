e00432f797cf0b5a7cd09600a6f9c0e1d2fc729b
Niels Rogge <niels.rogge1@gmail.com>
2022-01-10 12:13:41 +0100

    Add conversion script

--- src/transformers/models/vit_mae/convert_vit_mae_to_pytorch.py
+++ src/transformers/models/vit_mae/convert_vit_mae_to_pytorch.py
@@ -1,77 +1,96 @@
 import argparse
+
 import torch
-from transformers import ViTMAEConfig, ViTMAEFeatureExtractor, ViTMAEForPreTraining
+from PIL import Image
+
 import requests
-from PIL import Image  
+from transformers import ViTMAEConfig, ViTMAEFeatureExtractor, ViTMAEForPreTraining
+
 
 def rename_key(name):
-    if 'patch_embed.proj' in name:
-        name = name.replace('patch_embed.proj', 'embeddings.patch_embeddings.projection')
-    if 'patch_embed.norm' in name:
-        name = name.replace('patch_embed.norm', 'embeddings.norm')
-    if 'layers' in name:
-        name = 'encoder.' + name
-    if 'attn.proj' in name:
-        name = name.replace('attn.proj', 'attention.output.dense')
-    if 'attn' in name:
-        name = name.replace('attn', 'attention.self')
-    if 'norm1' in name:
-        name = name.replace('norm1', 'layernorm_before')
-    if 'norm2' in name:
-        name = name.replace('norm2', 'layernorm_after')
-    if 'mlp.fc1' in name:
-        name = name.replace('mlp.fc1', 'intermediate.dense')
-    if 'mlp.fc2' in name:
-        name = name.replace('mlp.fc2', 'output.dense')
-        
-    if name == 'norm.weight':
-        name = 'layernorm.weight'
-    if name == 'norm.bias':
-        name = 'layernorm.bias'
-     
-    if 'head' in name:
-        name = name.replace('head', 'classifier')
-    else:
-        name = 'vit.' + name
-    
+    if "cls_token" in name:
+        name = name.replace("cls_token", "vit.embeddings.cls_token")
+    if "mask_token" in name:
+        name = name.replace("mask_token", "decoder.mask_token")
+    if "decoder_pos_embed" in name:
+        name = name.replace("decoder_pos_embed", "decoder.decoder_pos_embed")
+    if "pos_embed" in name and "decoder" not in name:
+        name = name.replace("pos_embed", "vit.embeddings.position_embeddings")
+    if "patch_embed.proj" in name:
+        name = name.replace("patch_embed.proj", "vit.embeddings.patch_embeddings.projection")
+    if "patch_embed.norm" in name:
+        name = name.replace("patch_embed.norm", "vit.embeddings.norm")
+    if "decoder_blocks" in name:
+        name = name.replace("decoder_blocks", "decoder.decoder_layers")
+    if "blocks" in name:
+        name = name.replace("blocks", "vit.encoder.layer")
+    if "attn.proj" in name:
+        name = name.replace("attn.proj", "attention.output.dense")
+    if "attn" in name:
+        name = name.replace("attn", "attention.self")
+    if "norm1" in name:
+        name = name.replace("norm1", "layernorm_before")
+    if "norm2" in name:
+        name = name.replace("norm2", "layernorm_after")
+    if "mlp.fc1" in name:
+        name = name.replace("mlp.fc1", "intermediate.dense")
+    if "mlp.fc2" in name:
+        name = name.replace("mlp.fc2", "output.dense")
+    if "decoder_embed" in name:
+        name = name.replace("decoder_embed", "decoder.decoder_embed")
+    if "decoder_norm" in name:
+        name = name.replace("decoder_norm", "decoder.decoder_norm")
+    if "decoder_pred" in name:
+        name = name.replace("decoder_pred", "decoder.decoder_pred")
+    if "norm.weight" in name and "decoder" not in name:
+        name = name.replace("norm.weight", "vit.layernorm.weight")
+    if "norm.bias" in name and "decoder" not in name:
+        name = name.replace("norm.bias", "vit.layernorm.bias")
+
     return name
 
-def convert_state_dict(orig_state_dict, model):    
+
+def convert_state_dict(orig_state_dict, model):
     for key in orig_state_dict.copy().keys():
         val = orig_state_dict.pop(key)
 
-        if 'qkv' in key:
+        if "qkv" in key:
             # TODO
-            raise NotImplementedError("")
+            pass
         else:
             orig_state_dict[rename_key(key)] = val
-        
+
     return orig_state_dict
+
 
 def convert_vit_mae_checkpoint(checkpoint_url, pytorch_dump_folder_path):
     config = ViTMAEConfig()
     model = ViTMAEForPreTraining(config)
     model.eval()
-    
-    state_dict = torch.hub.load_from_url(checkpoint_url, map_location='cpu')
 
-    feature_extractor = ViTMAEFeatureExtractor(size = config.image_size)
-    
+    state_dict = torch.hub.load_state_dict_from_url(checkpoint_url, map_location="cpu")["model"]
+
+    feature_extractor = ViTMAEFeatureExtractor(size=config.image_size)
+
     new_state_dict = convert_state_dict(state_dict, model)
+
+    for k, v in new_state_dict.items():
+        print(k, v.shape)
+
     model.load_state_dict(new_state_dict)
-    
-    url = 'http://images.cocodataset.org/val2017/000000039769.jpg'
-    
+
+    url = "http://images.cocodataset.org/val2017/000000039769.jpg"
+
     image = Image.open(requests.get(url, stream=True).raw)
-    feature_extractor = ViTMAEFeatureExtractor(size = config.image_size)
+    feature_extractor = ViTMAEFeatureExtractor(size=config.image_size)
     inputs = feature_extractor(images=image, return_tensors="pt")
-    
+
     print(f"Saving model to {pytorch_dump_folder_path}")
     model.save_pretrained(pytorch_dump_folder_path)
-    
+
     print(f"Saving feature extractor to {pytorch_dump_folder_path}")
     feature_extractor.save_pretrained(pytorch_dump_folder_path)
-    
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -87,4 +106,4 @@ if __name__ == "__main__":
     )
 
     args = parser.parse_args()
-    convert_vit_mae_checkpoint(args.checkpoint_url, args.pytorch_dump_folder_path)  
+    convert_vit_mae_checkpoint(args.checkpoint_url, args.pytorch_dump_folder_path)
--- src/transformers/models/vit_mae/inference.py
+++ src/transformers/models/vit_mae/inference.py
@@ -1,11 +1,13 @@
-from transformers import ViTMAEConfig, ViTMAEForPreTraining
 import torch
+
+from transformers import ViTMAEConfig, ViTMAEForPreTraining
+
 
 model = ViTMAEForPreTraining(ViTMAEConfig())
 
 for name, param in model.named_parameters():
     print(name, param.shape)
 
-pixel_values = torch.randn((1,3,224,224))
+pixel_values = torch.randn((1, 3, 224, 224))
 
 outputs = model(pixel_values)
