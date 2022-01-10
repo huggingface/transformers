92d35c7769be8e34325699b5fc88dc1d679bb463
SaulLu <lucilesaul.com@gmail.com>
2022-01-10 12:52:30 +0100

    remove `errors` from docstring

--- src/transformers/models/clip/tokenization_clip_fast.py
+++ src/transformers/models/clip/tokenization_clip_fast.py
@@ -56,9 +56,6 @@ class CLIPTokenizerFast(PreTrainedTokenizerFast):
             Path to the vocabulary file.
         merges_file (`str`):
             Path to the merges file.
-        errors (`str`, *optional*, defaults to `"replace"`):
-            Paradigm to follow when decoding bytes to UTF-8. See
-            [bytes.decode](https://docs.python.org/3/library/stdtypes.html#bytes.decode) for more information.
         unk_token (`str`, *optional*, defaults to `<|endoftext|>`):
             The unknown token. A token that is not in the vocabulary cannot be converted to an ID and is set to be this
             token instead.
