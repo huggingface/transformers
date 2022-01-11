37f0fc88c51ec1c0208a46b653c0d9b99e8a8147
Niels Rogge <niels.rogge1@gmail.com>
2022-01-11 19:17:33 +0100

    Add support for other models in the conversion script

--- src/transformers/models/vit_mae/convert_vit_mae_to_pytorch.py
+++ src/transformers/models/vit_mae/convert_vit_mae_to_pytorch.py
@@ -88,6 +88,17 @@ def convert_state_dict(orig_state_dict, config):
 
 def convert_vit_mae_checkpoint(checkpoint_url, pytorch_dump_folder_path):
     config = ViTMAEConfig()
+    if "large" in checkpoint_url:
+        config.hidden_size = 1024
+        config.intermediate_size = 4096
+        config.num_hidden_layers = 24
+        config.num_attention_heads = 16
+    elif "huge" in checkpoint_url:
+        config.hidden_size = 1280
+        config.intermediate_size = 5120
+        config.num_hidden_layers = 32
+        config.num_attention_heads = 16
+
     model = ViTMAEForPreTraining(config)
 
     state_dict = torch.hub.load_state_dict_from_url(checkpoint_url, map_location="cpu")["model"]
@@ -110,11 +121,21 @@ def convert_vit_mae_checkpoint(checkpoint_url, pytorch_dump_folder_path):
     outputs = model(**inputs)
     logits = outputs.logits
 
-    assert torch.allclose(
-        logits[0, :3, :3],
-        torch.tensor([[-0.9192, -0.8481, -1.1259], [-1.1349, -1.0034, -1.2599], [-1.1757, -1.0429, -1.2726]]),
-        atol=1e-4,
-    )
+    if "large" in checkpoint_url:
+        expected_slice = torch.tensor(
+            [[-0.7309, -0.7128, -1.0169], [-1.0161, -0.9058, -1.1878], [-1.0478, -0.9411, -1.1911]]
+        )
+    elif "huge" in checkpoint_url:
+        expected_slice = torch.tensor(
+            [[-1.1599, -0.9199, -1.2221], [-1.1952, -0.9269, -1.2307], [-1.2143, -0.9337, -1.2262]]
+        )
+    else:
+        expected_slice = torch.tensor(
+            [[-0.9192, -0.8481, -1.1259], [-1.1349, -1.0034, -1.2599], [-1.1757, -1.0429, -1.2726]]
+        )
+
+    # verify logits
+    assert torch.allclose(logits[0, :3, :3], expected_slice, atol=1e-4)
 
     print(f"Saving model to {pytorch_dump_folder_path}")
     model.save_pretrained(pytorch_dump_folder_path)
