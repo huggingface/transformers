126ca724a6bf64b33e509b8cf550ff3ef631881f
Niels Rogge <niels.rogge1@gmail.com>
2022-01-12 17:58:55 +0100

    Fix archive_list

--- src/transformers/models/vit_mae/modeling_vit_mae.py
+++ src/transformers/models/vit_mae/modeling_vit_mae.py
@@ -44,9 +44,9 @@ logger = logging.get_logger(__name__)
 _CONFIG_FOR_DOC = "ViTMAEConfig"
 _CHECKPOINT_FOR_DOC = "facebook/vit-mae-base"
 
-VIT_PRETRAINED_MODEL_ARCHIVE_LIST = [
+VIT_MAE_PRETRAINED_MODEL_ARCHIVE_LIST = [
     "facebook/vit-mae-base",
-    # See all ViTMAE models at https://huggingface.co/models?filter=vit
+    # See all ViTMAE models at https://huggingface.co/models?filter=vit_mae
 ]
 
 
