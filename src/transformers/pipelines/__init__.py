374e00fe52212c9e11da0527c796c684efa383e8
Nicolas Patry <patry.nicolas@protonmail.com>
2022-01-12 09:28:19 +0100

    Pipeline ASR with LM. (#15071)
    * Pipeline ASR with LM.
    
    * Revamped into `self.decoder`.
    
    * Fixing.
    
    * 2nd fix.
    
    * Update src/transformers/pipelines/__init__.py
    
    Co-authored-by: Patrick von Platen <patrick.v.platen@gmail.com>
    
    * Fixing.
    
    Co-authored-by: Patrick von Platen <patrick.v.platen@gmail.com>

--- src/transformers/pipelines/__init__.py
+++ src/transformers/pipelines/__init__.py
@@ -611,6 +611,27 @@ def pipeline(
                 feature_extractor, revision=revision, _from_pipeline=task, **model_kwargs
             )
 
+            if (
+                feature_extractor._processor_class
+                and feature_extractor._processor_class.endswith("WithLM")
+                and isinstance(model_name, str)
+            ):
+                try:
+                    from pyctcdecode import BeamSearchDecoderCTC
+
+                    language_model_glob = os.path.join(BeamSearchDecoderCTC._LANGUAGE_MODEL_SERIALIZED_DIRECTORY, "*")
+                    alphabet_filename = BeamSearchDecoderCTC._ALPHABET_SERIALIZED_FILENAME
+                    allow_regex = [language_model_glob, alphabet_filename]
+
+                    decoder = BeamSearchDecoderCTC.load_from_hf_hub(
+                        pretrained_model_name_or_path, allow_regex=allow_regex
+                    )
+                    kwargs["decoder"] = decoder
+                except Exception as e:
+                    logger.warning(
+                        "Could not load the `decoder` for {model_name}. Defaulting to raw CTC. Try to install `pyctcdecode` and `kenlm`: (`pip install pyctcdecode`, `pip install https://github.com/kpu/kenlm/archive/master.zip`): Error: {e}"
+                    )
+
     if task == "translation" and model.config.task_specific_params:
         for key in model.config.task_specific_params:
             if key.startswith("translation"):
