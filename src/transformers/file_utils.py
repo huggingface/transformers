310d6d25dcb3a55f932ede034a44b69472a6c61c
Sylvain Gugger <sylvain.gugger@gmail.com>
2022-01-13 16:25:31 -0500

    See if this fixes the issue

--- src/transformers/file_utils.py
+++ src/transformers/file_utils.py
@@ -833,6 +833,8 @@ def requires_backends(obj, backends):
 
 class DummyObject(type):
     def __getattr__(cls, key):
+        if key.startswith("_"):
+            return super().__getattr__(cls, key)
         requires_backends(cls, cls._backends)
 
 
