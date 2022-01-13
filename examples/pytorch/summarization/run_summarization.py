96881729ce83cfc8e5fa04c903ee4296ad17cfbb
Sylvain Gugger <sylvain.gugger@gmail.com>
2022-01-13 17:34:41 -0500

    Remove assert on optional arg

--- examples/pytorch/summarization/run_summarization.py
+++ examples/pytorch/summarization/run_summarization.py
@@ -241,9 +241,6 @@ class DataTrainingArguments:
     def __post_init__(self):
         if self.dataset_name is None and self.train_file is None and self.validation_file is None:
             raise ValueError("Need either a dataset name or a training/validation file.")
-        elif self.lang is None:
-            raise ValueError("Need to specify the language.")
-
         else:
             if self.train_file is not None:
                 extension = self.train_file.split(".")[-1]
