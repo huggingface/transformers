b33300f815badb63c69bbe951b46c549b4d8f058
Jake Tae <jaesungtae@gmail.com>
2022-01-15 00:54:58 +0000

    style: run linter

--- src/transformers/file_utils.py
+++ src/transformers/file_utils.py
@@ -50,8 +50,8 @@ import requests
 from filelock import FileLock
 from huggingface_hub import HfFolder, Repository, create_repo, list_repo_files, whoami
 from requests.exceptions import HTTPError
-from transformers.utils.versions import importlib_metadata
 from transformers.utils.tqdm_utils import tqdm
+from transformers.utils.versions import importlib_metadata
 
 from . import __version__
 from .utils import logging
