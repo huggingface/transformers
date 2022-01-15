e0cec7e33902b9348a3bd7e187949c108dfe5b29
Jake Tae <jaesungtae@gmail.com>
2022-01-15 09:49:38 +0000

    refactor: use tiny model, run linter

--- tests/test_tqdm_utils.py
+++ tests/test_tqdm_utils.py
@@ -1,18 +1,22 @@
 from unittest.mock import patch
 
 from transformers import AutoModel
-from transformers.utils.tqdm_utils import set_progress_bar_enabled
 from transformers.testing_utils import require_torch
+from transformers.utils.tqdm_utils import set_progress_bar_enabled
+
+
+TINY_MODEL = "hf-internal-testing/tiny-random-distilbert"
+
 
 @require_torch
 def test_set_progress_bar_enabled():
     with patch("tqdm.auto.tqdm") as mock_tqdm:
         set_progress_bar_enabled(True)
-        _ = AutoModel.from_pretrained("distilbert-base-uncased", force_download=True)
+        _ = AutoModel.from_pretrained(TINY_MODEL, force_download=True)
         mock_tqdm.assert_called()
 
         mock_tqdm.reset_mock()
 
         set_progress_bar_enabled(False)
-        _ = AutoModel.from_pretrained("distilbert-base-uncased", force_download=True)
+        _ = AutoModel.from_pretrained(TINY_MODEL, force_download=True)
         mock_tqdm.assert_not_called()
