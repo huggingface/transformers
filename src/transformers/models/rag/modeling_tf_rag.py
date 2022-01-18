3f2bbca9738f4b36b4f4c21098fbc2f0e57e9a7e
Yih-Dar <2521628+ydshieh@users.noreply.github.com>
2022-01-18 13:16:30 +0100

    [Fix doc example] TFRagModel (#15187)
    * fix doc example - NameError: name 'PATH' is not defined
    
    * fix name 'TFRagModel' is not defined
    
    * correct TFRagRagSequenceForGeneration
    
    * fix name 'tf' is not defined
    
    * fix style
    
    Co-authored-by: ydshieh <ydshieh@users.noreply.github.com>

--- src/transformers/models/rag/modeling_tf_rag.py
+++ src/transformers/models/rag/modeling_tf_rag.py
@@ -293,7 +293,9 @@ class TFRagPreTrainedModel(TFPreTrainedModel):
         >>> model.save_pretrained("./rag")
 
         >>> # load retriever
-        >>> retriever = RagRetriever.from_pretrained(PATH, index_name="exact", use_dummy_dataset=True)
+        >>> retriever = RagRetriever.from_pretrained(
+        ...     "facebook/rag-token-base", index_name="exact", use_dummy_dataset=True
+        ... )
         >>> # load fine-tuned model with retriever
         >>> model = TFRagModel.from_pretrained("./rag", retriever=retriever)
         ```"""
@@ -559,7 +561,7 @@ class TFRagModel(TFRagPreTrainedModel):
         Example:
 
         ```python
-        >>> from transformers import RagTokenizer, RagRetriever, RagModel
+        >>> from transformers import RagTokenizer, RagRetriever, TFRagModel
         >>> import torch
 
         >>> tokenizer = RagTokenizer.from_pretrained("facebook/rag-token-base")
@@ -939,6 +941,7 @@ class TFRagTokenForGeneration(TFRagPreTrainedModel, TFCausalLanguageModelingLoss
         Example:
 
         ```python
+        >>> import tensorflow as tf
         >>> from transformers import RagTokenizer, RagRetriever, TFRagTokenForGeneration
 
         >>> tokenizer = RagTokenizer.from_pretrained("facebook/rag-token-nq")
@@ -1554,7 +1557,7 @@ class TFRagSequenceForGeneration(TFRagPreTrainedModel, TFCausalLanguageModelingL
         ...     "facebook/rag-sequence-nq", index_name="exact", use_dummy_dataset=True
         ... )
         >>> # initialize with RagRetriever to do everything in one forward call
-        >>> model = TFRagRagSequenceForGeneration.from_pretrained(
+        >>> model = TFRagSequenceForGeneration.from_pretrained(
         ...     "facebook/rag-sequence-nq", retriever=retriever, from_pt=True
         ... )
 
