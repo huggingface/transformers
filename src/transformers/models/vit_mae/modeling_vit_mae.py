5ad29881ae1d72e20d934c598b35e79fe6f448ad
Niels Rogge <niels.rogge1@gmail.com>
2022-01-18 10:26:21 +0100

    Remove unneccessary initialization of nn.Embedding

--- src/transformers/models/vit_mae/modeling_vit_mae.py
+++ src/transformers/models/vit_mae/modeling_vit_mae.py
@@ -580,10 +580,6 @@ class ViTMAEPreTrainedModel(PreTrainedModel):
             module.weight.data.normal_(mean=0.0, std=self.config.initializer_range)
             if module.bias is not None:
                 module.bias.data.zero_()
-        elif isinstance(module, nn.Embedding):
-            module.weight.data.normal_(mean=0.0, std=self.config.initializer_range)
-            if module.padding_idx is not None:
-                module.weight.data[module.padding_idx].zero_()
         elif isinstance(module, nn.LayerNorm):
             module.bias.data.zero_()
             module.weight.data.fill_(1.0)
