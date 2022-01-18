307651198c3f3ea6743aa31bffadbfcd9838de29
Jake Tae <jaesungtae@gmail.com>
2022-01-19 07:52:35 +0900

    Enable tqdm toggling (#15167)
    * feature: enable tqdm toggle
    
    * test: add tqdm unit test
    
    * style: run linter
    
    * Update tests/test_tqdm_utils.py
    
    Co-authored-by: Stas Bekman <stas00@users.noreply.github.com>
    
    * refactor: use tiny model, run linter
    
    * docs: add tqdm to logging
    
    * docs: add tqdm reference to `http_get`
    
    * style: run linter
    
    * Update docs/source/main_classes/logging.mdx
    
    Co-authored-by: Stas Bekman <stas00@users.noreply.github.com>
    
    * fix: use `AutoConfig` for framework agnostic testing
    
    * chore: mv tqdm test to `test_logging.py`
    
    * feature: implement enable/disable functions
    
    * docs: mv docstring to comment
    
    * chore: mv tqdm functions to `logging.py`
    
    * docs: update docs to reference `enable/disable` funcs
    
    * test: update test to use `enable/disable` func
    
    * chore: update function reference in comment
    
    Co-authored-by: Stas Bekman <stas00@users.noreply.github.com>

--- src/transformers/utils/logging.py
+++ src/transformers/utils/logging.py
@@ -28,6 +28,8 @@ from logging import WARN  # NOQA
 from logging import WARNING  # NOQA
 from typing import Optional
 
+from tqdm import auto as tqdm_lib
+
 
 _lock = threading.Lock()
 _default_handler: Optional[logging.Handler] = None
@@ -41,6 +43,8 @@ log_levels = {
 }
 
 _default_log_level = logging.WARNING
+
+_tqdm_active = True
 
 
 def _get_default_logging_level():
@@ -276,3 +280,65 @@ def warning_advice(self, *args, **kwargs):
 
 
 logging.Logger.warning_advice = warning_advice
+
+
+class EmptyTqdm:
+    """Dummy tqdm which doesn't do anything."""
+
+    def __init__(self, *args, **kwargs):  # pylint: disable=unused-argument
+        self._iterator = args[0] if args else None
+
+    def __iter__(self):
+        return iter(self._iterator)
+
+    def __getattr__(self, _):
+        """Return empty function."""
+
+        def empty_fn(*args, **kwargs):  # pylint: disable=unused-argument
+            return
+
+        return empty_fn
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, type_, value, traceback):
+        return
+
+
+class _tqdm_cls:
+    def __call__(self, *args, **kwargs):
+        if _tqdm_active:
+            return tqdm_lib.tqdm(*args, **kwargs)
+        else:
+            return EmptyTqdm(*args, **kwargs)
+
+    def set_lock(self, *args, **kwargs):
+        self._lock = None
+        if _tqdm_active:
+            return tqdm_lib.tqdm.set_lock(*args, **kwargs)
+
+    def get_lock(self):
+        if _tqdm_active:
+            return tqdm_lib.tqdm.get_lock()
+
+
+tqdm = _tqdm_cls()
+
+
+def is_progress_bar_enabled() -> bool:
+    """Return a boolean indicating whether tqdm progress bars are enabled."""
+    global _tqdm_active
+    return bool(_tqdm_active)
+
+
+def enable_progress_bar():
+    """Enable tqdm progress bar."""
+    global _tqdm_active
+    _tqdm_active = True
+
+
+def disable_progress_bar():
+    """Enable tqdm progress bar."""
+    global _tqdm_active
+    _tqdm_active = False
