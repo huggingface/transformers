58e1b6b45d5a882de000344b3869eea506d8911f
novice <44259234+novice03@users.noreply.github.com>
2022-01-19 11:41:17 +0530

    Update src/transformers/models/yoso/modeling_yoso.py
    Co-authored-by: Patrick von Platen <patrick.v.platen@gmail.com>

--- src/transformers/models/yoso/modeling_yoso.py
+++ src/transformers/models/yoso/modeling_yoso.py
@@ -1290,7 +1290,7 @@ class YosoForCausalLM(YosoPreTrainedModel):
         if past is not None:
             input_ids = input_ids[:, -1:]
 
-        return {"input_ids": input_ids, "attention_mask": attention_mask, "past_key_values": past}
+        return {"input_ids": input_ids, "attention_mask": attention_mask}
 
     def _reorder_cache(self, past, beam_idx):
         reordered_past = ()
