e509fd074b02071c12c6bf16ee5cafd432f57146
ydshieh <ydshieh@users.noreply.github.com>
2022-01-20 17:37:13 +0100

    Fix TF Causal LM models' returned logits

--- src/transformers/models/bert/modeling_tf_bert.py
+++ src/transformers/models/bert/modeling_tf_bert.py
@@ -1542,9 +1542,9 @@ class TFBertLMHeadModel(TFBertPreTrainedModel, TFCausalLanguageModelingLoss):
 
         if inputs["labels"] is not None:
             # shift labels to the left and cut last logit token
-            logits = logits[:, :-1]
+            shifted_logits = logits[:, :-1]
             labels = inputs["labels"][:, 1:]
-            loss = self.hf_compute_loss(labels=labels, logits=logits)
+            loss = self.hf_compute_loss(labels=labels, logits=shifted_logits)
 
         if not inputs["return_dict"]:
             output = (logits,) + outputs[2:]
