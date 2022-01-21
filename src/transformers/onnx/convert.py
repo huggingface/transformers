3adde33a85112356ab7b75f09bb8eadf9de6776d
Lewis Tunstall <lewis.c.tunstall@gmail.com>
2022-01-21 14:00:22 +0100

    Change upper bound on torch version to 1.10.99

--- src/transformers/onnx/convert.py
+++ src/transformers/onnx/convert.py
@@ -114,7 +114,7 @@ def export(
 
         # PyTorch deprecated the `enable_onnx_checker` and `use_external_data_format` arguments in v1.11,
         # so we check the torch version for backwards compatibility
-        if parse(torch.__version__) < parse("1.11.0.dev0"):
+        if parse(torch.__version__) <= parse("1.10.99"):
             # export can work with named args but the dict containing named args
             # has to be the last element of the args tuple.
             export(
