be47f7c84609a5843c5b6cd492ca00028644d5f2
Niels Rogge <niels.rogge1@gmail.com>
2022-01-24 11:39:17 +0100

    Add everything in new ops directory

--- src/transformers/models/deformable_detr/import_custom_layer.py
+++ src/transformers/models/deformable_detr/import_custom_layer.py
@@ -1,6 +1,4 @@
-from __future__ import absolute_import
-from __future__ import print_function
-from __future__ import division
+from __future__ import absolute_import, division, print_function
 
 import torch
 import torch.nn.functional as F
--- src/transformers/models/deformable_detr/inference.py
+++ src/transformers/models/deformable_detr/inference.py
@@ -1,11 +1,14 @@
-from transformers import DeformableDetrConfig, DeformableDetrModel
 import torch
-#import MultiScaleDeformableAttention as MSDA
+
+from transformers import DeformableDetrConfig, DeformableDetrModel
+
+
+# import MultiScaleDeformableAttention as MSDA
 
 config = DeformableDetrConfig()
 
 model = DeformableDetrModel(config)
 
-pixel_values = torch.randn((1,3,224,224))
+pixel_values = torch.randn((1, 3, 224, 224))
 
 outputs = model(pixel_values)
