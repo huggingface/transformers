3ecfebb04db09d9effe7967c5c5b625fb6a87077
Horace He <chilli@fb.com>
2022-01-25 02:08:17 +0000

    Updated with more models/dtypes

--- scripts/aot_albert.py
+++ scripts/aot_albert.py
@@ -1,4 +1,4 @@
-from transformers import AutoConfig, AutoModelForMaskedLM
+from transformers import AutoConfig, AutoModelForCausalLM, AutoModelForMaskedLM, AutoModelForSeq2SeqLM, ReformerConfig, BigBirdConfig, BertConfig
 import transformers
 import torch
 from functorch.compile import memory_efficient_fusion, aot_module, draw_graph_compile, nop
@@ -9,79 +9,106 @@ import torch.nn as nn
 from torch.nn.utils import _stateless
 import pandas as pd
 
-device = 'cuda'
+pytree._register_pytree_node(transformers.modeling_outputs.MaskedLMOutput, lambda x: ([x.loss, x.logits], None), lambda values, _: transformers.modeling_outputs.MaskedLMOutput(loss=values[0], logits=values[1]))
+
+pytree._register_pytree_node(transformers.modeling_outputs.Seq2SeqLMOutput, lambda x: ([x.loss, x.logits], None), lambda values, _: transformers.modeling_outputs.Seq2SeqLMOutput(loss=values[0], logits=values[1]))
+
+pytree._register_pytree_node(transformers.modeling_outputs.CausalLMOutputWithCrossAttentions, lambda x: ([x.loss, x.logits], None), lambda values, _: transformers.modeling_outputs.CausalLMOutputWithCrossAttentions(loss=values[0], logits=values[1]))
+
+pytree._register_pytree_node(transformers.models.longformer.modeling_longformer.LongformerMaskedLMOutput, lambda x: ([x.loss, x.logits], None), lambda values, _: transformers.models.longformer.modeling_longformer.LongformerMaskedLMOutput(loss=values[0], logits=values[1]))
+
 torch.manual_seed(42)
-config = AutoConfig.from_pretrained("albert-base-v2")
-config.classifier_dropout_prob = 1e-15 # To have the computation but not the stochasticity
-model = AutoModelForMaskedLM.from_config(config).to(device)
+benchmarks = [
+    (AutoConfig.from_pretrained("albert-base-v2"), AutoModelForMaskedLM, (8, 512), []),
+    (AutoConfig.from_pretrained("gpt2"), AutoModelForCausalLM, (4, 512), []),
+    (BertConfig(), AutoModelForMaskedLM, (4, 512), []),
+    (AutoConfig.from_pretrained("allenai/longformer-base-4096"), AutoModelForMaskedLM, (2, 1024), [torch.float16, torch.bfloat16]), # hmm, nans with float16
+    # (AutoConfig.from_pretrained("t5-small"), AutoModelForSeq2SeqLM, (4, 1024), [torch.float16, torch.bfloat16]), # Doesn't work with nn.utils._stateless for some reason...
+    # (AutoConfig.from_pretrained("facebook/bert-base"), AutoModelForSeq2SeqLM, (4, 512), []), # Doesn't work with nn.utils._stateless for some reason...
+    # (ReformerConfig(), AutoModelForMaskedLM, (8, 4096), []), # not sure...
+    # (BigBirdConfig(attention_type="block_sparse"), AutoModelForMaskedLM, (2, 1024), []), # not sure...
+    # (AutoConfig.from_pretrained("distilbert-base-uncased"),  AutoModelForMaskedLM, (8, 512), []), # encounters inf as a global value
+]
 
-input_ids = torch.randint(0, config.vocab_size, (8, 512)).to(device)
-decoder_ids = torch.randint(0, config.vocab_size, (8, 512)).to(device)
-
-
-train_inputs = {'input_ids': input_ids, 'labels': decoder_ids}
-
-
-pytree._register_pytree_node(transformers.modeling_outputs.MaskedLMOutput, lambda x: ([x.loss, x.logits], None), lambda values, _: transformers.modeling_outputs.MaskedLMOutput(loss=values[0], logits=values[1])) # Used for
-
-def get_cur_memory():
-    torch.cuda.synchronize()
-    import gc; gc.collect()
-    torch.cuda.empty_cache()
-    stats = torch.cuda.memory_stats()
-    peak_bytes_requirement = stats["allocated_bytes.all.current"]
-    print(f"Current memory requirement: {peak_bytes_requirement / 1024 ** 3:.2f} GB")
-    return peak_bytes_requirement
-
-def bench_model(name, mod):
-    m = None
-    for i in range(5):
-        out = mod(**train_inputs).loss.abs().sum()
-        if i == 4:
-            m = get_cur_memory()
-        out.backward()
-    iters = 20
-    torch.cuda.synchronize()
-    begin = time.time()
-    for _ in range(iters):
-        mod(**train_inputs).loss.sum().backward()
-    torch.cuda.synchronize()
-    t = (time.time()-begin)/iters
-    print(name, (time.time()-begin)/iters)
-    return t, m
+device = 'cuda'
 
 results = []
-aot_model = memory_efficient_fusion(model)
-t,m = bench_model("eager", model)
-nh, th, mh, tp, mp = "name", "time (s)", "mem (GB)", "time %", "mem %"
-results.append({nh:"eager", th:t, mh:m/2**30})
-with torch.jit.fuser("fuser2"):
-    t,m = bench_model("aot", aot_model)
-results.append({nh:"aot", th:t, mh:m/2**30})
+for config, model_type, input_size, not_supported_dtypes in benchmarks:
+    for dtype in [torch.float, torch.half, torch.bfloat16]:
+        if dtype in not_supported_dtypes:
+            continue
+        for attr in dir(config):
+            if 'drop' in attr:
+                setattr(config, attr, 1e-30) # So we can check for correct gradients without eliminating the dropout computation
+        model = model_type.from_config(config).to(device, dtype=dtype)
+        input_ids = torch.randint(0, config.vocab_size, input_size).to(device)
+        decoder_ids = torch.randint(0, config.vocab_size, input_size).to(device)
 
-# calculate relative improvements
-base_r = results[0]
-for r in results:
-    r[mp] = round(100 * (r[th] - base_r[th]) / base_r[th])
-    r[tp] = round(100 * (r[mh] - base_r[mh]) / base_r[mh])
+
+        train_inputs = {'input_ids': input_ids, 'labels': decoder_ids}
+
+        def get_cur_memory():
+            torch.cuda.synchronize()
+            import gc; gc.collect()
+            torch.cuda.empty_cache()
+            stats = torch.cuda.memory_stats()
+            peak_bytes_requirement = stats["allocated_bytes.all.current"]
+            print(f"Current memory requirement: {peak_bytes_requirement / 1024 ** 3:.2f} GB")
+            return peak_bytes_requirement
+
+        def bench_model(name, mod):
+            m = None
+            for i in range(5):
+                out = mod(**train_inputs).loss.abs().sum()
+                if i == 4:
+                    m = get_cur_memory()
+                out.backward()
+            iters = 20
+            torch.cuda.synchronize()
+            begin = time.time()
+            for _ in range(iters):
+                mod(**train_inputs).loss.sum().backward()
+            torch.cuda.synchronize()
+            t = (time.time()-begin)/iters
+            print(name, (time.time()-begin)/iters)
+            return t, m
+
+        aot_model = memory_efficient_fusion(model)
+        model_name = type(model).__name__
+        t,m = bench_model("eager", model)
+        model_header, dtype_header, nh, th, mh, tp, mp = "model", "dtype", "name", "time (s)", "mem (GB)", "time %", "mem %"
+        results.append({model_header: model_name, dtype_header: str(dtype), nh:"eager", th:t, mh:m/2**30})
+        with torch.jit.fuser("fuser2"):
+            t,m = bench_model("aot", aot_model)
+        results.append({model_header: model_name, dtype_header: str(dtype), nh:"aot", th:t, mh:m/2**30})
+
+        # calculate relative improvements
+        base_r = results[-2]
+        for r in results[-2:]:
+            r[tp] = round(100 * (r[th] - base_r[th]) / base_r[th])
+            r[mp] = round(100 * (r[mh] - base_r[mh]) / base_r[mh])
+        print(pd.DataFrame(results[-2:]).to_markdown(index=False, floatfmt=".3f"))
+
+        print()
+
+        ## Checking correctness
+        def clear_params(model):
+            for i in model.parameters(): i.grad = None
+
+        clear_params(model)
+        torch.manual_seed(0)
+        out1 = model(**train_inputs).loss
+        out1.sum().backward()
+        grad1 = [i.grad for i in model.parameters()]
+
+        clear_params(aot_model)
+        torch.manual_seed(0)
+        out2 = aot_model(**train_inputs).loss
+        out2.sum().backward()
+        grad2 = [i.grad for i in aot_model.parameters()]
+        print("Maximum output error: ", (out1 - out2).abs().max().item())
+        print("Maximum gradient error: ", max([(a-b).abs().max() for a, b in zip(grad1, grad2) if a is not None]).item())
+        print()
+
 
 print(pd.DataFrame(results).to_markdown(index=False, floatfmt=".3f"))
-
-print()
-## Checking correctness
-def clear_params(model):
-    for i in model.parameters(): i.grad = None
-
-clear_params(model)
-torch.manual_seed(0)
-out1 = model(**train_inputs).loss
-out1.sum().backward()
-grad1 = [i.grad for i in model.parameters()]
-
-clear_params(aot_model)
-torch.manual_seed(0)
-out2 = aot_model(**train_inputs).loss
-out2.sum().backward()
-grad2 = [i.grad for i in aot_model.parameters()]
-print((out1 - out2).abs().max())
-print(max([(a-b).abs().max() for a, b in zip(grad1, grad2)]))
