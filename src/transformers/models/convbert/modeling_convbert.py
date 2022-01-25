0b65ad3efc4f64793107beea08ca73c6ff97c178
ydshieh <ydshieh@users.noreply.github.com>
2022-01-25 21:35:36 +0100

    Same fix for ConvBertGeneratorPredictions

--- src/transformers/models/convbert/modeling_convbert.py
+++ src/transformers/models/convbert/modeling_convbert.py
@@ -865,7 +865,7 @@ class ConvBertGeneratorPredictions(nn.Module):
     def __init__(self, config):
         super().__init__()
 
-        self.LayerNorm = nn.LayerNorm(config.embedding_size)
+        self.LayerNorm = nn.LayerNorm(config.embedding_size, eps=config.layer_norm_eps)
         self.dense = nn.Linear(config.hidden_size, config.embedding_size)
 
     def forward(self, generator_hidden_states):
