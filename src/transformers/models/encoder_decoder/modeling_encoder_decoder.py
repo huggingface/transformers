173a0be1204d5fa5813ae1f37c5bef00dea6617e
Yih-Dar <2521628+ydshieh@users.noreply.github.com>
2022-01-26 10:14:46 +0100

    Fix encoder-decoder models when labels is passed (#15172)
    Co-authored-by: ydshieh <ydshieh@users.noreply.github.com>

--- src/transformers/models/encoder_decoder/modeling_encoder_decoder.py
+++ src/transformers/models/encoder_decoder/modeling_encoder_decoder.py
@@ -529,7 +529,7 @@ class EncoderDecoderModel(PreTrainedModel):
         loss = None
         if labels is not None:
             warnings.warn(DEPRECATION_WARNING, FutureWarning)
-            logits = decoder_outputs.logits if return_dict else decoder_outputs[1]
+            logits = decoder_outputs.logits if return_dict else decoder_outputs[0]
             loss_fct = CrossEntropyLoss()
             loss = loss_fct(logits.reshape(-1, self.decoder.config.vocab_size), labels.view(-1))
 
