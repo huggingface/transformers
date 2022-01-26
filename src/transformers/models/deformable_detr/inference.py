3512d694dd22200f63b580c55612971f0cfef401
NielsRogge <niels.rogge1@gmail.com>
2022-01-26 14:23:01 +0000

    Improve conversion script

--- src/transformers/models/deformable_detr/inference.py
+++ src/transformers/models/deformable_detr/inference.py
@@ -1,17 +1,21 @@
 import torch
-from torch.utils.cpp_extension import CUDA_HOME
+from PIL import Image
+from torchvision import transforms as T
 
+import requests
 from transformers import DeformableDetrConfig, DeformableDetrModel
 
-
-print(torch.cuda.is_available(), CUDA_HOME)
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 config = DeformableDetrConfig()
-model = DeformableDetrModel(config)
+model = DeformableDetrModel(config).eval()
 model.to(device)
 
-pixel_values = torch.randn((1, 3, 224, 224)).to(device)
+transform = T.Compose([T.Resize(800), T.ToTensor(), T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
+
+url = "http://images.cocodataset.org/val2017/000000039769.jpg"
+image = Image.open(requests.get(url, stream=True).raw)
+pixel_values = transform(image).unsqueeze(0).to(device)
 
 outputs = model(pixel_values)
