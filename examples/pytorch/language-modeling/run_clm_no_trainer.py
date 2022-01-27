0cd56b0699cd5585e85db3bc2b2296bd4cac93c5
dataaug <38125327+dataaug@users.noreply.github.com>
2022-01-27 15:53:22 +0800

    Fix bugs that may lead to training failure

--- examples/pytorch/language-modeling/run_clm_no_trainer.py
+++ examples/pytorch/language-modeling/run_clm_no_trainer.py
@@ -379,6 +379,12 @@ def main():
             for k, t in concatenated_examples.items()
         }
         result["labels"] = result["input_ids"].copy()
+        # padding if one group shorter than block_size
+        if total_length < block_size:
+            result = {
+            k: [t + [0] * (block_size - len(t))]
+            for k, t in result.items()
+        }
         return result
 
     # Note that with `batched=True`, this map processes 1,000 texts together, so group_texts throws away a remainder
