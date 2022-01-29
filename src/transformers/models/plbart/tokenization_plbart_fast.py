42243f3ae243a5b60f069156624b07a9c949e2d3
gchhablani <chhablani.gunjan@gmail.com>
2022-01-29 18:23:17 +0530

    Fix style

--- src/transformers/models/plbart/tokenization_plbart_fast.py
+++ src/transformers/models/plbart/tokenization_plbart_fast.py
@@ -110,7 +110,10 @@ class PLBartTokenizerFast(XLMRobertaTokenizerFast):
 
     ```python
     >>> from transformers import PLBartTokenizerFast
-    >>> tokenizer = PLBartTokenizerFast.from_pretrained('uclanlp/plbart-python-en_XX', src_lang="python", tgt_lang="en_XX")
+
+    >>> tokenizer = PLBartTokenizerFast.from_pretrained(
+    ...     "uclanlp/plbart-python-en_XX", src_lang="python", tgt_lang="en_XX"
+    ... )
     >>> example_python_phrase = "def maximum(a,b,c):NEW_LINE_INDENTreturn max([a,b,c])"
     >>> expected_translation_english = "Returns the maximum value of a b c."
     >>> inputs = tokenizer(example_python_phrase, return_tensors="pt")
