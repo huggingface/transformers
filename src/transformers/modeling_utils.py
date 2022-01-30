draft of chunking without returning attentions. currently fails due to passing wrong dimensions or data.

src/transformers/modeling_utils.py
@@ -2439,28 +2439,90 @@ def apply_chunking_to_forward(
 
     return forward_fn(*input_tensors)
 
-def attention(config, queries, keys, values, mask=None, bias=None, dropout=None, return_attentions=False):
+def attention(queries, keys, values, mask=None, bias=None, dropout=None, chunk_size_query=0, chunk_size_key=0, output_attentions=False, weights_dtype=torch.float32, scale=None, post_bias=None):
     '''
-        queries: [Batches, Queries, Heads, Features]
-        keys: [Batches, Keys, Heads, Features]
-        values: [Batches, Values, Heads, Features]
+        queries: [Batches, Heads, Queries, Features]
+        keys: [Batches, Heads, Keys, Features]
+        values: [Batches, Heads, Values, Features]
         mask: [Batches, Heads, Queries, Keys] of Bool
+                disables weights where false
+        bias: [Batches, Heads, Queries, Keys]
+                added to attention weights before softmax
+        dropout: torch.nn.Dropout
+                attention weights are passed through this before output is calculated
+        chunk_size_query:
+                size to process queries in. memory usage is queries * keys * heads * batches * dtypesize
+        chunk_size_key:
+                size to process keys in. memory usage is queries * keys * heads * batches * dtypesize
+        output_attentions: Bool
+                return attention weights if True. not implemented for chunking.
+        weights_dtype:
+                weights can be cast to this type during dot and softmax, if desired
+        scale:
+                if specified, weights are divided by this value before softmax
+        post_bias:
+                an additional bias to be multiplied after the softmax rather than summed before
 
-        returns [Batches, Values, Heads, Features]
+        returns: ([..., Values, Heads, Features], Attentions)
+                Attentions is None if output_attentions is not set
     '''
-    query_size = config.chunk_size_query if config.chunk_size_query != 0 else queries.shape[-3]
-    key_size = config.chunk_size_key if config.chunk_size_key != 0 else keys.shape[-3]
-    if dropout is not None:
-        maskbias_shape = (*queries.shape[:-3], queries.shape[-2], queries.shape[-3], keys.shape[-3])
-        if mask is None:
-            mask = True
-        mask &= dropout(torch.ones(maskbias_shape, device=queries.device)).to(torch.bool)
-        if bias is None:
-            bias = torch.zeros(maskbias_shape, dtype=queries.dtype, device=queries.device)
-        bias += torch.log(1 / (torch.tensor(1, dtype=bias.dtype, device=bias.device) - dropout.p))
-    return memory_efficient_attention.efficient_dot_product_attention_pt(
-            queries, keys, values, mask, bias, query_size, key_size, return_attentions)
+    if chunk_size_query > 0 or chunk_size_key > 0: # chunking to save memory
+        if output_attentions:
+            raise ValueError("output_attentions enabled but access when discarded by chunking is unimplemented")
+        queries = queries.permute(0,2,1,3)
+        keys = keys.permute(0,2,1,3)
+        values = values.permute(0,2,1,3)
+        query_size = chunk_size_query if chunk_size_query != 0 else queries.shape[-3]
+        key_size = chunk_size_key if chunk_size_key != 0 else keys.shape[-3]
+        if dropout is not None:
+            maskbias_shape = (*queries.shape[:-3], queries.shape[-2], queries.shape[-3], keys.shape[-3])
+            if mask is None:
+                mask = True
+            mask &= dropout(torch.ones(maskbias_shape, device=queries.device)).to(torch.bool)
+            if bias is None:
+                bias = torch.zeros(maskbias_shape, dtype=queries.dtype, device=queries.device)
+            bias = bias + torch.log(1 / (torch.tensor(1, dtype=bias.dtype, device=bias.device) - dropout.p))
+        if post_bias is not None:
+            bias = bias + torch.log(post_bias)
+        if scale is not None:
+            queries = queries * torch.tensor(keys.shape[-1], dtype=queries.dtype, device=queries.device).sqrt()  / scale
+        output = memory_efficient_attention.efficient_dot_product_attention_pt(
+            queries, keys, values, mask, bias, query_size, key_size)
+        return output.permute(0,2,1,3), None
+    else: # no chunking
+        if weights_dtype is not None:
+            queries = queries.to(torch.float32)
+            keys = keys.to(torch.float32)
 
+        weights = torch.matmul(queries, keys.transpose(-1, -2))
+
+        if bias is not None:
+            weights = weights + bias
+
+        if scale is not None:
+            weights = weights / scale
+        else:
+            weights = weights / torch.tensor(keys.shape[-1], dtype=queries.dtype, device=queries.device).sqrt()
+
+        if mask is not None:
+            big_neg = torch.finfo(weights.dtype).min
+            big_neg = torch.tensor(big_neg, dtype=weights.dtype)
+            weights = torch.where(mask, aweights, big_neg)
+
+        weights = nn.functional.softmax(weights, dim=-1)
+
+        if weights_dtype is not None:
+            weights = weights.to(values.dtype)
+
+        if dropout is not None:
+            weights = dropout(weights)
+
+        if post_bias is not None:
+            weights = weights * post_bias
+
+        output = torch.matmul(weights, values)
+
+        return output, (weights if output_attentions else None)
 
 def torch_int_div(tensor1, tensor2):
     """

src/transformers/models/gptj/modeling_gptj.py
@@ -133,11 +133,12 @@ class GPTJAttention(nn.Module):
         new_shape = tensor.size()[:-2] + (num_attention_heads * attn_head_size,)
         return tensor.view(new_shape)
 
-    def _new_attn(
+    def _attn(
         self,
         query,
         key,
         value,
+        output_attentions,
         attention_mask=None,
         head_mask=None,
     ):
@@ -156,45 +157,7 @@ class GPTJAttention(nn.Module):
         if head_mask is not None:
             bias += torch.log(head_mask)
 
-        attn_output, attn_weights = attention(self.config, query.permute(0,2,1,3), key.permute(0,2,1,3), value.permute(0,2,1,3), bias = bias, dropout = self.attn_dropout, return_attentions = True)
-
-        return attn_output.permute(0,2,1,3), attn_weights.permute(0,2,1,3)
-
-    def _old_attn(
-        self,
-        query,
-        key,
-        value,
-        attention_mask=None,
-        head_mask=None,
-    ):
-
-        # compute causal mask from causal mask buffer
-        query_length, key_length = query.size(-2), key.size(-2)
-        causal_mask = self.bias[:, :, key_length - query_length : key_length, :key_length].bool()
-
-        # Keep the attention weights computation in fp32 to avoid overflow issues
-        query = query.to(torch.float32)
-        key = key.to(torch.float32)
-
-        attn_weights = torch.matmul(query, key.transpose(-1, -2))
-        attn_weights = torch.where(causal_mask, attn_weights, self.masked_bias.to(attn_weights.dtype))
-
-        attn_weights = attn_weights / self.scale_attn
-
-        if attention_mask is not None:
-            # Apply the attention mask
-            attn_weights = attn_weights + attention_mask
-
-        attn_weights = nn.functional.softmax(attn_weights, dim=-1)
-        attn_weights = attn_weights.to(value.dtype)
-        attn_weights = self.attn_dropout(attn_weights)
-
-        # Mask heads if we want to
-        if head_mask is not None:
-            attn_weights = attn_weights * head_mask
-
-        attn_output = torch.matmul(attn_weights, value)
+        attn_output, attn_weights = attention(query, key, value, bias = bias, dropout = self.attn_dropout, chunk_size_query = self.config.chunk_size_query, chunk_size_key = self.config.chunk_size_key, output_attentions = output_attentions, scale = self.scale_attn)
 
         return attn_output, attn_weights
 
@@ -256,10 +219,7 @@ class GPTJAttention(nn.Module):
             present = None
 
         # compute self-attention: V x Softmax(QK^T)
-        if os.environ.get('NEW_ATTN'):
-            attn_output, attn_weights = self._new_attn(query, key, value, attention_mask, head_mask)
-        else:
-            attn_output, attn_weights = self._old_attn(query, key, value, attention_mask, head_mask)
+        attn_output, attn_weights = self._attn(query, key, value, attention_mask, head_mask)
 
         attn_output = self._merge_heads(attn_output, self.num_attention_heads, self.head_dim)
         attn_output = self.out_proj(attn_output)