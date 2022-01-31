aead6a7c7a70584fe937dea7dc3284f416b055f7
Matt <Rocketknight1@users.noreply.github.com>
2022-01-31 17:09:16 +0000

    Fix spurious warning in TF TokenClassification models (#15435)

--- src/transformers/modeling_tf_utils.py
+++ src/transformers/modeling_tf_utils.py
@@ -219,7 +219,7 @@ class TFTokenClassificationLoss:
         # make sure only labels that are not equal to -100
         # are taken into account as loss
         if tf.math.reduce_any(labels == -1):
-            warnings.warn("Using `-1` to mask the loss for the token is deprecated. Please use `-100` instead.")
+            tf.print("Using `-1` to mask the loss for the token is deprecated. Please use `-100` instead.")
             active_loss = tf.reshape(labels, (-1,)) != -1
         else:
             active_loss = tf.reshape(labels, (-1,)) != -100
