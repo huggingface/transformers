eed3ee276ad6e093e0759bdbae05d93ca99b230f
NielsRogge <niels.rogge1@gmail.com>
2022-02-01 15:49:05 +0000

    Improve decoder outputs

--- src/transformers/models/deformable_detr/configuration_deformable_detr.py
+++ src/transformers/models/deformable_detr/configuration_deformable_detr.py
@@ -74,8 +74,6 @@ class DeformableDetrConfig(PretrainedConfig):
         decoder_layerdrop: (`float`, *optional*, defaults to 0.0):
             The LayerDrop probability for the decoder. See the [LayerDrop paper](see https://arxiv.org/abs/1909.11556)
             for more details.
-        return_intermediate (`bool`, *optional*, defaults to `True`):
-            Whether to make the decoder return intermediate hidden states and reference points.
         auxiliary_loss (`bool`, *optional*, defaults to `False`):
             Whether auxiliary decoding losses (loss at each decoder layer) are to be used.
         position_embedding_type (`str`, *optional*, defaults to `"sine"`):
@@ -193,7 +191,6 @@ class DeformableDetrConfig(PretrainedConfig):
         self.init_xavier_std = init_xavier_std
         self.encoder_layerdrop = encoder_layerdrop
         self.decoder_layerdrop = decoder_layerdrop
-        self.return_intermediate = return_intermediate
         self.auxiliary_loss = auxiliary_loss
         self.position_embedding_type = position_embedding_type
         self.backbone = backbone
