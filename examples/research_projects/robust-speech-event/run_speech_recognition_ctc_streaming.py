ba85f0aec41ddad23b8e3a7fa35bef188b4f3b33
anton-l <aglozhkov@gmail.com>
2022-02-02 13:29:03 +0300

    Bump min version

--- examples/research_projects/robust-speech-event/run_speech_recognition_ctc_streaming.py
+++ examples/research_projects/robust-speech-event/run_speech_recognition_ctc_streaming.py
@@ -50,7 +50,7 @@ from transformers.utils.versions import require_version
 
 
 # Will error if the minimal version of Transformers is not installed. Remove at your own risk.
-check_min_version("4.16.0.dev0")
+check_min_version("4.17.0.dev0")
 
 require_version("datasets>=1.18.2", "To fix: pip install 'datasets>=1.18.2'")
 
