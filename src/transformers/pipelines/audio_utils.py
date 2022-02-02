f6f51d9b3935d2b3d1dfee13f311a92a9c281441
Nicolas Patry <patry.nicolas@protonmail.com>
2022-02-02 10:10:54 +0100

    Better docstrings.

--- src/transformers/pipelines/audio_utils.py
+++ src/transformers/pipelines/audio_utils.py
@@ -102,7 +102,34 @@ def ffmpeg_microphone_live(
 ):
     """
     Helper function to read audio from the microphone file through ffmpeg. This will output repeating/increasing chunks
-    until `chunk_length_s`. It will make use of striding to avoid errors on the "sides" of the various chunks.
+    starting from `stream_chunk_s` (if it is defined). until `chunk_length_s`. It will make use of striding to avoid
+    errors on the "sides" of the various chunks.
+
+    Arguments:
+        sampling_rate (`int`):
+            The sampling_rate to use when reading the data from the microphone. Try using the model's sampling_rate to
+            avoid resampling later.
+        chunk_length_s (`float` or `int`):
+            The length of the maximum chunk of audio to be sent returned. This includes the eventual striding.
+        stream_chunk_s (`float` or `int`)
+            The length of the minimal temporary audio to be returned.
+        stride_length_s (`float` or `int` or `(float, float)`, *optional*, defaults to `None`)
+            The length of the striding to be used. Stride is used to provide context to a model on the (left, right) of
+            an audio sample but without using that part to actually make the prediction. Setting this does not change
+            the length of the chunk.
+        format_for_conversion: (`str`, defalts to `f32le`)
+            The name of the format of the audio samples to be returned by ffmpeg. The standard is `f32le`, `s16le`
+            could also be used.
+    Return:
+        A generator yielding dictionaries of the following form
+
+        `{"sampling_rate": int, "raw": np.array(), "partial" bool}` With optionnally a `"stride" (int, int)` key if
+        `stride_length_s` is defined.
+
+        `stride` and `raw` are all expressed in `samples`, and `partial` is a boolean saying if the current yield item
+        is a whole chunk, or a partial temporary result to be later replaced by another larger chunk.
+
+
     """
     if stream_chunk_s is not None:
         chunk_s = stream_chunk_s
