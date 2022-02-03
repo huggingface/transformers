962cd49479f797218fb5e8ffb903ec972c1f9901
Yih-Dar <2521628+ydshieh@users.noreply.github.com>
2022-02-03 21:39:46 +0100

    Remove loss from some flax models docs & examples (#15492)
    * Remove return_loss from Flax models
    
    * fix more
    
    * fix
    
    Co-authored-by: ydshieh <ydshieh@users.noreply.github.com>

--- src/transformers/models/vision_text_dual_encoder/modeling_flax_vision_text_dual_encoder.py
+++ src/transformers/models/vision_text_dual_encoder/modeling_flax_vision_text_dual_encoder.py
@@ -561,9 +561,8 @@ VISION_TEXT_DUAL_ENCODER_MODEL_DOCSTRING = r"""
     ...     input_ids=inputs.input_ids,
     ...     attention_mask=inputs.attention_mask,
     ...     pixel_values=inputs.pixel_values,
-    ...     return_loss=True,
     ... )
-    >>> loss, logits_per_image = outputs.loss, outputs.logits_per_imag  # this is the image-text similarity score
+    >>> logits_per_image = outputs.logits_per_image  # this is the image-text similarity score
 
     >>> # save and load from pretrained
     >>> model.save_pretrained("vit-bert")
