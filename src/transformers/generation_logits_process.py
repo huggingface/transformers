33775ef677adf6628bc78a39451db3c36c4705d2
Clara Meister <cmeister7.47@gmail.com>
2022-02-04 09:17:48 +0100

    fix edge case where scores are same

--- src/transformers/generation_logits_process.py
+++ src/transformers/generation_logits_process.py
@@ -251,25 +251,22 @@ class TypicalLogitsWarper(LogitsWarper):
         # calculate entropy
         normalized = torch.nn.functional.log_softmax(scores, dim=-1)
         p = torch.exp(normalized)
-        ent = -(normalized * p).sum(-1, keepdim=True)
+        ent = -(normalized * p).nansum(-1, keepdim=True)
 
+        
         #shift and sort
-        shifted_scores = torch.abs((-ent) - normalized)
-        _, sorted_indices = torch.sort(shifted_scores, descending=False)
+        shifted_scores = torch.abs((-normalized) -ent)
+        sorted_scores, sorted_indices = torch.sort(shifted_scores, descending=False)
         sorted_logits = scores.gather(-1, sorted_indices)
         cumulative_probs = sorted_logits.softmax(dim=-1).cumsum(dim=-1)
 
-        # Remove tokens with cumulative top_p above the threshold (token with 0 are kept)
-        sorted_indices_to_remove = cumulative_probs > self.mass
+        # Remove tokens with cumulative mass above the threshold 
+        last_ind = (cumulative_probs < self.mass).sum(dim=1) 
+        last_ind[last_ind < 0] = 0
+        sorted_indices_to_remove = sorted_scores > sorted_scores.gather(1,last_ind.view(-1,1))
         if self.min_tokens_to_keep > 1:
             # Keep at least min_tokens_to_keep (set to min_tokens_to_keep-1 because we add the first one below)
-            sorted_indices_to_remove[..., : self.min_tokens_to_keep - 1] = 0
-        # Shift the indices to the right to keep also the first token above the threshold
-        sorted_indices_to_remove[...,
-                                 1:] = sorted_indices_to_remove[..., :-1].clone()
-        sorted_indices_to_remove[..., 0] = 0
-
-        # scatter sorted tensors to original indexing
+            sorted_indices_to_remove[..., : self.min_tokens_to_keep] = 0
         indices_to_remove = sorted_indices_to_remove.scatter(
             1, sorted_indices, sorted_indices_to_remove)
 
