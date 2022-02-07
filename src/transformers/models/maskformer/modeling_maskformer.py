b426cfff95e78a3bc6f29fb51c49819bd3f02761
fra <francesco.zuppichini@gmail.com>
2022-02-07 08:36:56 +0100

    removed global outputs variables

--- src/transformers/models/maskformer/modeling_maskformer.py
+++ src/transformers/models/maskformer/modeling_maskformer.py
@@ -57,8 +57,6 @@ MASKFORMER_PRETRAINED_MODEL_ARCHIVE_LIST = [
     # See all MaskFormer models at https://huggingface.co/models?filter=maskformer
 ]
 
-PREDICTIONS_MASKS_KEY = "masks_queries_logits"
-PREDICTIONS_LOGITS_KEY = "class_queries_logits"
 TARGETS_MASKS_KEY = "pixel"
 TARGETS_LABELS_KEY = "classes"
 
@@ -304,9 +302,9 @@ class MaskFormerHungarianMatcher(nn.Module):
 
         indices: List[Tuple[np.array]] = []
 
-        preds_masks: Tensor = outputs[PREDICTIONS_MASKS_KEY]
+        preds_masks: Tensor = outputs["masks_queries_logits"]
         labels_masks: Tensor = labels[TARGETS_MASKS_KEY]
-        preds_probs: Tensor = outputs[PREDICTIONS_LOGITS_KEY].softmax(dim=-1)
+        preds_probs: Tensor = outputs["class_queries_logits"].softmax(dim=-1)
         # downsample all masks in one go -> save memory
         labels_masks: Tensor = F.interpolate(labels_masks, size=preds_masks.shape[-2:], mode="nearest")
         # iterate through batch size
@@ -392,7 +390,7 @@ class MaskFormerLoss(nn.Module):
         [nb_target_masks]
         """
 
-        pred_logits: Tensor = outputs[PREDICTIONS_LOGITS_KEY]
+        pred_logits: Tensor = outputs["class_queries_logits"]
         b, q, _ = pred_logits.shape
 
         idx = self._get_src_permutation_idx(indices)
@@ -417,7 +415,7 @@ class MaskFormerLoss(nn.Module):
         """
         src_idx = self._get_src_permutation_idx(indices)
         tgt_idx = self._get_tgt_permutation_idx(indices)
-        pred_masks = outputs[PREDICTIONS_MASKS_KEY]  # shape [BATCH, NUM_QUERIES, H, W]
+        pred_masks = outputs["masks_queries_logits"]  # shape [BATCH, NUM_QUERIES, H, W]
         pred_masks = pred_masks[src_idx]  # shape [BATCH * NUM_QUERIES, H, W]
         target_masks = labels[TARGETS_MASKS_KEY]  # shape [BATCH, NUM_QUERIES, H, W]
         target_masks = target_masks[tgt_idx]  # shape [BATCH * NUM_QUERIES, H, W]
@@ -462,8 +460,8 @@ class MaskFormerLoss(nn.Module):
         """
         # TODO in theory here we can just take the `pred_masks` key
         outputs_without_aux = {
-            PREDICTIONS_MASKS_KEY: outputs[PREDICTIONS_MASKS_KEY],
-            PREDICTIONS_LOGITS_KEY: outputs[PREDICTIONS_LOGITS_KEY],
+            "masks_queries_logits": outputs["masks_queries_logits"],
+            "class_queries_logits": outputs["class_queries_logits"],
         }
 
         # Retrieve the matching between the outputs of the last layer and the labels
@@ -758,7 +756,7 @@ class MaskFormerSegmentationModule(nn.Module):
         if use_auxilary_loss:
             stacked_decoder_outputs: Tensor = torch.stack(decoder_outputs)
             classes: Tensor = self.class_predictor(stacked_decoder_outputs)
-            out.update({PREDICTIONS_LOGITS_KEY: classes[-1]})
+            out.update({"class_queries_logits": classes[-1]})
             # get the masks
             mask_embeddings: Tensor = self.mask_embedder(stacked_decoder_outputs)
             # sum up over the channels for each embedding
@@ -769,19 +767,19 @@ class MaskFormerSegmentationModule(nn.Module):
             # go til [:-1] because the last one is always used
             for aux_binary_masks, aux_classes in zip(binaries_masks[:-1], classes[:-1]):
                 auxilary_predictions.append(
-                    {PREDICTIONS_MASKS_KEY: aux_binary_masks, PREDICTIONS_LOGITS_KEY: aux_classes}
+                    {"masks_queries_logits": aux_binary_masks, "class_queries_logits": aux_classes}
                 )
             out.update({"auxilary_predictions": auxilary_predictions})
 
         else:
             last_decoder_output: Tensor = decoder_outputs[-1]
             classes: Tensor = self.class_predictor(last_decoder_output)
-            out.update({PREDICTIONS_LOGITS_KEY: classes})
+            out.update({"class_queries_logits": classes})
             # get the masks
             mask_embeddings: Tensor = self.mask_embedder(last_decoder_output)
             # sum up over the channels
             binary_masks: Tensor = torch.einsum("bqc,   bchw -> bqhw", mask_embeddings, pixel_embeddings)
-        out.update({PREDICTIONS_MASKS_KEY: binary_masks})
+        out.update({"masks_queries_logits": binary_masks})
         return out
 
 
@@ -885,120 +883,3 @@ class MaskFormerModel(MaskFormerPretrainedModel):
         outputs: Dict[str, Tensor] = self.segmentation_module(queries, pixel_embeddings, self.config.use_auxilary_loss)
 
         return MaskFormerOutput(**outputs)
-
-
-class PanopticSegmentationSegment(TypedDict):
-    id: int
-    category_id: int
-    is_thing: bool
-    label: str
-
-
-# NOTE this to be moved inside the feature extractor
-@add_start_docstrings(
-    """
-    MaskFormer for panoptic segmentation
-    """,
-    MASKFORMER_START_DOCSTRING,
-)
-class MaskFormerForPanopticSegmentation(MaskFormerForSemanticSegmentation):
-    def __init__(
-        self,
-        config: MaskFormerConfig,
-    ):
-        super().__init__(config)
-
-    def remove_low_and_no_objects(
-        self,
-        masks: Tensor,
-        scores: Tensor,
-        labels: Tensor,
-        object_mask_threshold: float,
-    ) -> Tuple[Tensor, Tensor, Tensor]:
-        if not (masks.shape[0] == scores.shape[0] == labels.shape[0]):
-            raise ValueError("mask, scores and labels must have the same shape!")
-
-        to_keep: Tensor = labels.ne(self.model.config.num_labels) & (scores > object_mask_threshold)
-
-        return masks[to_keep], scores[to_keep], labels[to_keep]
-
-    @add_start_docstrings_to_model_forward(MASKFORMER_INPUTS_DOCSTRING)
-    # @replace_return_docstrings(output_type=MaskFormerForPanopticSegmentationOutput, config_class=_CONFIG_FOR_DOC)
-    def forward(
-        self,
-        *args,
-        object_mask_threshold: Optional[float] = 0.8,
-        overlap_mask_area_threshold: Optional[float] = 0.8,
-        **kwargs
-    ):
-        outputs: MaskFormerOutput = self.model(*args, **kwargs)
-        preds_logits: Tensor = outputs.preds_logits
-        preds_masks: Tensor = outputs.preds_masks
-        # since all images are padded, they all have the same spatial dimensions
-        _, _, height, width = preds_masks.shape
-        # for each query, the best scores and their indeces
-        pred_scores, pred_labels = F.softmax(preds_logits, dim=-1).max(-1)
-        # pred_scores and pred_labels shape = [BATH,NUM_QUERIES]
-        mask_probs = preds_masks.sigmoid()
-        # mask probs has shape [BATCH, QUERIES, HEIGHT, WIDTH]
-        # now, we need to iterate over the batch size to correctly process the segmentation we got from the queries using our thresholds. Even if the original predicted masks have the same shape across the batch, they won't after thresholding so batch-wise operations are impossible
-        # TODO we need to store the segmentations and the segments in a list
-        for (mask_probs, pred_scores, pred_labels) in zip(mask_probs, pred_scores, pred_labels):
-
-            mask_probs, pred_scores, pred_labels = self.remove_low_and_no_objects(
-                mask_probs, pred_scores, pred_labels, object_mask_threshold
-            )
-            we_detect_something: bool = mask_probs.shape[0] > 0
-
-            segmentation: Tensor = torch.zeros((height, width), dtype=torch.int32, device=mask_probs.device)
-            segments: List[PanopticSegmentationSegment] = []
-
-            if we_detect_something:
-                current_segment_id: int = 0
-                # weight each mask by its score
-                mask_probs *= pred_scores.view(-1, 1, 1)
-                # find out for each pixel what is the most likely class to be there
-                mask_labels: Tensor = mask_probs.argmax(0)
-                # mask_labels shape = [H,W] where each pixel has a class label
-                stuff_memory_list: Dict[str, int] = {}
-                # this is a map between stuff and segments id, the used it to keep track of the instances of one class
-                for k in range(pred_labels.shape[0]):
-                    pred_class: int = pred_labels[k].item()
-                    # check if pred_class is not a "thing", so it can be merged with other instance. For example, class "sky" cannot have more then one instance
-                    class_spec: ClassSpec = self.model.config.dataset_metadata["classes"][pred_class]
-                    is_stuff = not class_spec["is_thing"]
-                    # get the mask associated with the k class
-                    mask_k: Tensor = mask_labels == k
-                    # create the area, since bool we just need to sum :)
-                    mask_k_area: Tensor = mask_k.sum()
-                    # this is the area of all the stuff in query k
-                    # TODO not 100%, why are the taking the k query here????
-                    original_area: Tensor = (mask_probs[k] >= 0.5).sum()
-
-                    mask_does_exist: bool = mask_k_area > 0 and original_area > 0
-
-                    if mask_does_exist:
-                        # find out how much of the all area mask_k is using
-                        area_ratio: float = mask_k_area / original_area
-                        mask_k_is_overlapping_enough: bool = area_ratio.item() > overlap_mask_area_threshold
-
-                        if mask_k_is_overlapping_enough:
-                            # merge stuff regions
-                            if pred_class in stuff_memory_list:
-                                current_segment_id = stuff_memory_list[pred_class]
-                            else:
-                                current_segment_id += 1
-                            # then we update out mask with the current segment
-                            segmentation[mask_k] = current_segment_id
-                            segments.append(
-                                {
-                                    "id": current_segment_id,
-                                    "category_id": pred_class,
-                                    "is_thing": not is_stuff,
-                                    "label": class_spec["label"],
-                                }
-                            )
-                            if is_stuff:
-                                stuff_memory_list[pred_class] = current_segment_id
-
-            return MaskFormerForPanopticSegmentationOutput(segmentation=segmentation, segments=segments, **outputs)
