d0936e874346f7a36e579d8a76d9237cc2764912
Eduardo Gonzalez Ponferrada <edugp91@gmail.com>
2022-02-07 15:13:14 -0800

    Update src/transformers/models/auto/configuration_auto.py
    Co-authored-by: Sylvain Gugger <35901082+sgugger@users.noreply.github.com>

--- src/transformers/models/auto/configuration_auto.py
+++ src/transformers/models/auto/configuration_auto.py
@@ -263,7 +263,7 @@ MODEL_NAMES_MAPPING = OrderedDict(
         ("reformer", "Reformer"),
         ("longformer", "Longformer"),
         ("roberta", "RoBERTa"),
-        ("data2vec", "data2vec"),
+        ("data2vec", "Data2Vec"),
         ("flaubert", "FlauBERT"),
         ("fsmt", "FairSeq Machine-Translation"),
         ("squeezebert", "SqueezeBERT"),
