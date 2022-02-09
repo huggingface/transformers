583769c722162fecf30f7c6ad05450996cf38f3c
Sayak Paul <spsayakpaul@gmail.com>
2022-02-09 07:23:05 +0530

    fix: sample code for the classification model.

--- src/transformers/models/convnext/modeling_tf_convnext.py
+++ src/transformers/models/convnext/modeling_tf_convnext.py
@@ -460,7 +460,7 @@ class TFConvNextForImageClassification(TFConvNextPreTrainedModel, TFSequenceClas
         >>> url = "http://images.cocodataset.org/val2017/000000039769.jpg"
         >>> image = Image.open(requests.get(url, stream=True).raw)
 
-        >>> feature_extractor = ViTFeatureExtractor.from_pretrained("facebook/convnext-tiny-224")
+        >>> feature_extractor = ConvNextFeatureExtractor.from_pretrained("facebook/convnext-tiny-224")
         >>> model = TFViTForImageClassification.from_pretrained("facebook/convnext-tiny-224")
 
         >>> inputs = feature_extractor(images=image, return_tensors="tf")
