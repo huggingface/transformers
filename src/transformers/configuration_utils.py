6cf06d198c72ab2796614bade00a50c777627512
Sylvain Gugger <35901082+sgugger@users.noreply.github.com>
2022-02-11 09:55:31 -0500

    Mark "code in the Hub" API as experimental (#15624)

--- src/transformers/configuration_utils.py
+++ src/transformers/configuration_utils.py
@@ -867,6 +867,12 @@ class PretrainedConfig(PushToHubMixin):
         Register this class with a given auto class. This should only be used for custom configurations as the ones in
         the library are already mapped with `AutoConfig`.
 
+        <Tip warning={true}>
+
+        This API is experimental and may have some slight breaking changes in the next releases.
+
+        </Tip>
+
         Args:
             auto_class (`str` or `type`, *optional*, defaults to `"AutoConfig"`):
                 The auto class to register this new configuration with.
