1d03d49d3bf3b8ff0d2495e89b6fb21eefb3f4be
Manuel R. Ciosici <manuelrciosici@gmail.com>
2022-02-11 07:27:07 -0500

    Fix package name

--- src/transformers/file_utils.py
+++ src/transformers/file_utils.py
@@ -476,7 +476,7 @@ def is_apex_available():
     return importlib.util.find_spec("apex") is not None
 
 def is_bnb_available():
-    return importlib.util.find_spec("bnb") is not None
+    return importlib.util.find_spec("bitsandbytes") is not None
 
 
 def is_faiss_available():
