db865a52d82b3f403a2fd81f1e18b3fe3dee0889
Niels Rogge <nielsrogge@Nielss-MacBook-Pro.local>
2022-02-15 17:34:55 +0100

    Fix most tokenizer tests

--- src/transformers/models/markuplm/test.py
+++ src/transformers/models/markuplm/test.py
@@ -1,9 +1,15 @@
 from transformers import MarkupLMTokenizer
 
+
 tokenizer = MarkupLMTokenizer.from_pretrained("microsoft/markuplm-base")
 
-encoding = tokenizer(["hello", "world"], xpaths=["/html/body/div/li[1]/div/span", "/html/body/div/li[1]/div/span"], 
-                      padding="max_length", max_length=20, return_tensors="pt")
+encoding = tokenizer(
+    ["hello", "world"],
+    xpaths=["/html/body/div/li[1]/div/span", "/html/body/div/li[1]/div/span"],
+    padding="max_length",
+    max_length=20,
+    return_tensors="pt",
+)
 
-for k,v in encoding.items():
+for k, v in encoding.items():
     print(k, v.shape)
