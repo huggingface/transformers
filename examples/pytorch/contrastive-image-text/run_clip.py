fix labels, handle attention mask

examples/pytorch/contrastive-image-text/run_clip.py
@@ -202,7 +202,8 @@ class Transform(torch.nn.Module):
 def collate_fn(examples):
     pixel_values = torch.stack([example["pixel_values"] for example in examples])
     labels = torch.tensor([example["labels"] for example in examples], dtype=torch.long)
-    return {"pixel_values": pixel_values, "labels": labels, "return_loss": True}
+    attention_mask = torch.tensor([example["attention_mask"] for example in examples], dtype=torch.long)
+    return {"pixel_values": pixel_values, "labels": labels, "attention_mask": attention_mask, "return_loss": True}
 
 
 def main():
@@ -359,9 +360,11 @@ def main():
         captions = [caption for caption in examples[caption_column]]
 
         # encode captions
-        examples["labels"] = tokenizer(
-            captions, max_length=data_args.max_seq_length, padding="max_length", truncation=True, return_tensors="pt"
+        text_inputs = tokenizer(
+            captions, max_length=data_args.max_seq_length, padding="max_length", truncation=True
         )
+        examples["labels"] = text_inputs.input_ids
+        examples["attention_mask"] = text_inputs.attention_mask
 
         # encode images
         def _transform_images(image_file):