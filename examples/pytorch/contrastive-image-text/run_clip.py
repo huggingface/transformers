c797ec773cf3399181f8f6bc065a7fbe2e3b0abb
patil-suraj <surajp815@gmail.com>
2022-02-18 15:23:06 +0100

    fix version message

--- examples/pytorch/contrastive-image-text/run_clip.py
+++ examples/pytorch/contrastive-image-text/run_clip.py
@@ -56,7 +56,7 @@ logger = logging.getLogger(__name__)
 # Will error if the minimal version of Transformers is not installed. Remove at your own risks.
 check_min_version("4.17.0.dev0")
 
-require_version("datasets>=1.8.0", "To fix: pip install -r examples/pytorch/image-classification/requirements.txt")
+require_version("datasets>=1.8.0", "To fix: pip install -r examples/pytorch/contrastive-image-text/requirements.txt")
 
 
 @dataclass
