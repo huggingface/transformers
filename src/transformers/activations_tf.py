31de348e7bcdbbb51601ef51c2093ffc693809e9
Morgan Funtowicz <funtowiczmo@gmail.com>
2022-02-21 10:34:13 +0100

    style style style style style style

--- src/transformers/activations_tf.py
+++ src/transformers/activations_tf.py
@@ -71,16 +71,14 @@ def quick_gelu(x):
 
 def gelu_10(x):
     """
-    Clip the range of possible GeLU outputs between [-10, 10].
-    This is especially useful for quantization purpose, as it allows mapping 2 negatives values in the GeLU spectrum.
-    For more information on this trick, please refer to https://arxiv.org/abs/2004.09602
+    Clip the range of possible GeLU outputs between [-10, 10]. This is especially useful for quantization purpose, as
+    it allows mapping 2 negatives values in the GeLU spectrum. For more information on this trick, please refer to
+    https://arxiv.org/abs/2004.09602
 
     Gaussian Error Linear Unit. Original Implementation of the gelu activation function in Google Bert repo when
     initially created. For information: OpenAI GPT's gelu is slightly different (and gives slightly different results):
     0.5 * x * (1 + torch.tanh(math.sqrt(2 / math.pi) * (x + 0.044715 * torch.pow(x, 3)))) Also see
-    https://arxiv.org/abs/1606.08415
-    :param x:
-    :return:
+    https://arxiv.org/abs/1606.08415 :param x: :return:
     """
     return tf.clip_by_value(_gelu(x), -10, 10)
 
