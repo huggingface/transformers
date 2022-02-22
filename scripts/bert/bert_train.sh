38117beaea38bf972591ea978338db54ab799fc7
Steven I Reeves <steven.reeves@amd.com>
2022-02-22 03:02:08 +0000

    Adding scripts for bert DLM.

---
+++ scripts/bert/bert_train.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e 
+set -x
+pip3 install transformers datasets
+
+
+cd ~ && git clone https://github.com/ROCmSoftwarePlatform/transformers
+# Script to train the small 117M model
+python3 transformers/scripts/bert/bert_train.py > log.txt
+cat log.txt | tail -n 1
+cat log.txt | tail -n 1 | awk '{ print "Accuracy: " $(NF) }'
+
