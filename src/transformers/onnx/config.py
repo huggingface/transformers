687d4366021cd779aa20378f922787bf25538614
Lewis Tunstall <lewis.c.tunstall@gmail.com>
2022-02-23 20:44:15 +0100

    Tweak docstring

--- src/transformers/onnx/config.py
+++ src/transformers/onnx/config.py
@@ -262,7 +262,7 @@ class OnnxConfig(ABC):
             is_pair (`bool`, *optional*, defaults to `False`):
                 Indicate if the input is a pair (sentence 1, sentence 2)
             framework (`TensorType`, *optional*, defaults to `None`):
-                The framework (optional) the tokenizer will generate tensor for.
+                The framework (PyTorch or TensorFlow) that the tokenizer will generate tensors for.
             num_channels (`int`, *optional*, defaults to 3):
                 The number of channels of the generated images.
             image_width (`int`, *optional*, defaults to 40):
