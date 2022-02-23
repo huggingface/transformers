64327bdad1a4149b415b2fcfb3c9cf5436a2d851
function2 <function2-llx@outlook.com>
2022-02-23 20:31:49 +0800

    Support PEP 563 for HfArgumentParser

--- tests/utils/test_hf_argparser.py
+++ tests/utils/test_hf_argparser.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from __future__ import annotations
+
 import argparse
 import unittest
 from argparse import Namespace
