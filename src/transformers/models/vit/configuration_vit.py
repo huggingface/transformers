31dd4f9575dd2a03e362fb8821cbd6f7c0356f69
Lewis Tunstall <lewis.c.tunstall@gmail.com>
2022-02-24 13:57:18 +0100

    Fix ViT torch version

--- src/transformers/models/vit/configuration_vit.py
+++ src/transformers/models/vit/configuration_vit.py
@@ -129,7 +129,7 @@ class ViTConfig(PretrainedConfig):
 
 class ViTOnnxConfig(OnnxConfig):
 
-    TORCH_ONNX_MINIMUM_VERSION = version.parse("1.12")
+    torch_onnx_minimum_version = version.parse("1.12")
 
     @property
     def inputs(self) -> Mapping[str, Mapping[int, str]]:
