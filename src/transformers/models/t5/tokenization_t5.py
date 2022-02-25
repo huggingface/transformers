bad0f43278b4abe6e72ae44f86e3cea605e30b21
Ben Eyal <ben.eyal.89@gmail.com>
2022-02-25 19:59:34 +0200

    Revert removal of space in convert_tokens_to_string

--- src/transformers/models/t5/tokenization_t5.py
+++ src/transformers/models/t5/tokenization_t5.py
@@ -288,7 +288,7 @@ class T5Tokenizer(PreTrainedTokenizer):
         for token in tokens:
             # make sure that special tokens are not decoded using sentencepiece model
             if token in self.all_special_tokens:
-                out_string += self.sp_model.decode(current_sub_tokens) + token
+                out_string += self.sp_model.decode(current_sub_tokens) + token + " "
                 current_sub_tokens = []
             else:
                 current_sub_tokens.append(token)
