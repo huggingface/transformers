4a866d95912523a5f6656d59b3917543a10a63c4
Edward Beeching <edbeeching@gmail.com>
2022-02-25 15:16:01 +0100

    added comments and argument descriptions to decision transformer file

--- src/transformers/models/decision_transformer/modeling_decision_transformer.py
+++ src/transformers/models/decision_transformer/modeling_decision_transformer.py
@@ -15,8 +15,6 @@
 """ PyTorch DecisionTransformer model. """
 
 
-
-
 import torch
 import torch.utils.checkpoint
 from torch import nn
@@ -33,9 +31,7 @@ from ...file_utils import (
 from ...modeling_outputs import (
     DecisionTransformerOutput,
 )
-from ...modeling_utils import (
-    PreTrainedModel
-)
+from ...modeling_utils import PreTrainedModel
 from ...utils import logging
 from .configuration_decision_transformer import DecisionTransformerConfig
 
@@ -51,6 +47,7 @@ DECISION_TRANSFORMER_PRETRAINED_MODEL_ARCHIVE_LIST = [
     # See all DecisionTransformer models at https://huggingface.co/models?filter=decision_transformer
 ]
 
+
 class DecisionTransformerPreTrainedModel(PreTrainedModel):
     """
     An abstract class to handle weights initialization and
@@ -58,30 +55,30 @@ class DecisionTransformerPreTrainedModel(PreTrainedModel):
     """
 
     config_class = DecisionTransformerConfig
-    #load_tf_weights = load_tf_weights_in_decision_transformer
+    # load_tf_weights = load_tf_weights_in_decision_transformer
     base_model_prefix = "decision_transformer"
     supports_gradient_checkpointing = True
     _keys_to_ignore_on_load_missing = [r"position_ids"]
 
     def _init_weights(self, module):
-        """ Initialize the weights """
+        """Initialize the weights"""
         if isinstance(module, nn.Linear):
             # Slightly different from the TF version which uses truncated_normal for initialization
             # cf https://github.com/pytorch/pytorch/pull/5617
-            module.weight.data.normal_(mean=0.0, std=self.config.initializer_range)
+            module.weight.data.normal_(
+                mean=0.0, std=self.config.initializer_range
+            )
             if module.bias is not None:
                 module.bias.data.zero_()
         elif isinstance(module, nn.Embedding):
-            module.weight.data.normal_(mean=0.0, std=self.config.initializer_range)
+            module.weight.data.normal_(
+                mean=0.0, std=self.config.initializer_range
+            )
             if module.padding_idx is not None:
                 module.weight.data[module.padding_idx].zero_()
         elif isinstance(module, nn.LayerNorm):
             module.bias.data.zero_()
             module.weight.data.fill_(1.0)
-
-    # def _set_gradient_checkpointing(self, module, value=False):
-    #     if isinstance(module, DecisionTransformerEncoder):
-    #         module.gradient_checkpointing = value
 
 
 DECISION_TRANSFORMER_START_DOCSTRING = r"""
@@ -97,72 +94,33 @@ DECISION_TRANSFORMER_START_DOCSTRING = r"""
 
 DECISION_TRANSFORMER_INPUTS_DOCSTRING = r"""
     Args:
-        input_ids (`torch.LongTensor` of shape `({0})`):
-            Indices of input sequence tokens in the vocabulary.
-
-            Indices can be obtained using [`DecisionTransformerTokenizer`].
-            See [`PreTrainedTokenizer.encode`] and
-            [`PreTrainedTokenizer.__call__`] for details.
-
-            [What are input IDs?](../glossary#input-ids)
-        attention_mask (`torch.FloatTensor` of shape `({0})`, *optional*):
-            Mask to avoid performing attention on padding token indices. Mask values selected in `[0, 1]`:
-
-            - 1 for tokens that are **not masked**,
-            - 0 for tokens that are **masked**.
-
-            [What are attention masks?](../glossary#attention-mask)
-        token_type_ids (`torch.LongTensor` of shape `({0})`, *optional*):
-            Segment token indices to indicate first and second portions of the inputs. Indices are selected in `[0, 1]`:
-
-            - 0 corresponds to a *sentence A* token,
-            - 1 corresponds to a *sentence B* token.
-
-            [What are token type IDs?](../glossary#token-type-ids)
-        position_ids (`torch.LongTensor` of shape `({0})`, *optional*):
-            Indices of positions of each input sequence tokens in the position embeddings.
-            Selected in the range `[0, config.max_position_embeddings - 1]`.
-
-            [What are position IDs?](../glossary#position-ids)
-        head_mask (`torch.FloatTensor` of shape `(num_heads,)` or `(num_layers, num_heads)`, *optional*):
-            Mask to nullify selected heads of the self-attention modules. Mask values selected in `[0, 1]`:
-
-            - 1 indicates the head is **not masked**,
-            - 0 indicates the head is **masked**.
-
-        inputs_embeds (`torch.FloatTensor` of shape `({0}, hidden_size)`, *optional*):
-            Optionally, instead of passing `input_ids` you can choose to directly pass an embedded representation.
-            This is useful if you want more control over how to convert *input_ids* indices into associated vectors
-            than the model's internal embedding lookup matrix.
-        output_attentions (`bool`, *optional*):
-            Whether or not to return the attentions tensors of all attention layers. See `attentions` under returned
-            tensors for more detail.
-        output_hidden_states (`bool`, *optional*):
-            Whether or not to return the hidden states of all layers. See `hidden_states` under returned tensors for
-            more detail.
-        return_dict (`bool`, *optional*):
-            Whether or not to return a [`~file_utils.ModelOutput`] instead of a plain tuple.
+        states (`torch.FloatTensor` of shape `(batch_size, episode_length, state_dim)`):
+            The states for each step in the trajectory
+        actions (`torch.FloatTensor` of shape `(batch_size, episode_length, act_dim)`):
+            The actions taken by the "expert" policy for the current state, these are masked for auto
+            regressive prediction
+        rewards (`torch.FloatTensor` of shape `(batch_size, episode_length, 1)`):
+            The rewards for each state, action
+        returns_to_go (`torch.FloatTensor` of shape `(batch_size, episode_length, 1)`):
+            The returns for each state in the trajectory
+        timesteps (`torch.LongTensor` of shape `(batch_size, episode_length)`):
+            The timestep for each step in the trajectory
+        attention_mask (`torch.LongTensor` of shape `(batch_size, episode_length)`):
+            Masking, used to mask the actions when performing autoregressive prediction
 """
 
 
 @add_start_docstrings(
-    "The bare DecisionTransformer Model transformer outputting raw hidden-states without any specific head on top.",
+    "The DecisionTransformer Model",
     DECISION_TRANSFORMER_START_DOCSTRING,
 )
 class DecisionTransformerModel(DecisionTransformerPreTrainedModel):
     """
 
-    The model can behave as an encoder (with only self-attention) as well
-    as a decoder, in which case a layer of cross-attention is added between
-    the self-attention layers, following the architecture described in [Attention is
-    all you need](https://arxiv.org/abs/1706.03762) by Ashish Vaswani,
-    Noam Shazeer, Niki Parmar, Jakob Uszkoreit, Llion Jones, Aidan N. Gomez, Lukasz Kaiser and Illia Polosukhin.
+    The model builds upon the GPT2 architecture to perform autoregressive prediction
+    of actions in an offline RL setting. Refer to the paper for more details:
+    https://arxiv.org/abs/2106.01345
 
-    To behave as an decoder the model needs to be initialized with the
-    `is_decoder` argument of the configuration set to `True`.
-    To be used in a Seq2Seq model, the model needs to initialized with both `is_decoder`
-    argument and `add_cross_attention` set to `True`; an
-    `encoder_hidden_states` is then expected as an input to the forward pass.
     """
 
     def __init__(self, config):
@@ -173,7 +131,9 @@ class DecisionTransformerModel(DecisionTransformerPreTrainedModel):
         # is that the positional embeddings are removed (since we'll add those ourselves)
         self.transformer = GPT2Model(config)
 
-        self.embed_timestep = nn.Embedding(config.max_ep_len, config.hidden_size)
+        self.embed_timestep = nn.Embedding(
+            config.max_ep_len, config.hidden_size
+        )
         self.embed_return = torch.nn.Linear(1, config.hidden_size)
         self.embed_state = torch.nn.Linear(config.state_dim, config.hidden_size)
         self.embed_action = torch.nn.Linear(config.act_dim, config.hidden_size)
@@ -181,40 +141,46 @@ class DecisionTransformerModel(DecisionTransformerPreTrainedModel):
         self.embed_ln = nn.LayerNorm(config.hidden_size)
 
         # note: we don't predict states or returns for the paper
-        self.predict_state = torch.nn.Linear(config.hidden_size, config.state_dim)
+        self.predict_state = torch.nn.Linear(
+            config.hidden_size, config.state_dim
+        )
         self.predict_action = nn.Sequential(
-            *([nn.Linear(config.hidden_size, config.act_dim)] + ([nn.Tanh()] if config.action_tanh else []))
+            *(
+                [nn.Linear(config.hidden_size, config.act_dim)]
+                + ([nn.Tanh()] if config.action_tanh else [])
+            )
         )
         self.predict_return = torch.nn.Linear(config.hidden_size, 1)
-        
-        
+
         # Initialize weights and apply final processing
         self.post_init()
 
-    @add_start_docstrings_to_model_forward(DECISION_TRANSFORMER_INPUTS_DOCSTRING.format("batch_size, sequence_length"))
+    @add_start_docstrings_to_model_forward(
+        DECISION_TRANSFORMER_INPUTS_DOCSTRING.format(
+            "batch_size, sequence_length"
+        )
+    )
     @add_code_sample_docstrings(
-        # processor_class=_TOKENIZER_FOR_DOC,
         checkpoint=_CHECKPOINT_FOR_DOC,
-        output_type=DecisionTransformerOutput, # TODO
+        output_type=DecisionTransformerOutput,
         config_class=_CONFIG_FOR_DOC,
     )
     def forward(
-        self, states=None, actions=None, rewards=None, returns_to_go=None, timesteps=None, attention_mask=None,
+        self,
+        states=None,
+        actions=None,
+        rewards=None,
+        returns_to_go=None,
+        timesteps=None,
+        attention_mask=None,
     ):
-        # r"""
-        # states    
-        # actions
-        # rewards
-        # returns_to_go
-        # timesteps
-        # attention_mask
- 
-        # """
         batch_size, seq_length = states.shape[0], states.shape[1]
 
         if attention_mask is None:
             # attention mask for GPT: 1 if can be attended to, 0 if not
-            attention_mask = torch.ones((batch_size, seq_length), dtype=torch.long)
+            attention_mask = torch.ones(
+                (batch_size, seq_length), dtype=torch.long
+            )
 
         # embed each modality with a different head
         state_embeddings = self.embed_state(states)
@@ -229,37 +195,49 @@ class DecisionTransformerModel(DecisionTransformerPreTrainedModel):
 
         # this makes the sequence look like (R_1, s_1, a_1, R_2, s_2, a_2, ...)
         # which works nice in an autoregressive sense since states predict actions
-        stacked_inputs = torch.stack(
-            (returns_embeddings, state_embeddings, action_embeddings), dim=1
-        ).permute(0, 2, 1, 3).reshape(batch_size, 3*seq_length, self.hidden_size)
+        stacked_inputs = (
+            torch.stack(
+                (returns_embeddings, state_embeddings, action_embeddings), dim=1
+            )
+            .permute(0, 2, 1, 3)
+            .reshape(batch_size, 3 * seq_length, self.hidden_size)
+        )
         stacked_inputs = self.embed_ln(stacked_inputs)
 
         # to make the attention mask fit the stacked inputs, have to stack it as well
-        stacked_attention_mask = torch.stack(
-            (attention_mask, attention_mask, attention_mask), dim=1
-        ).permute(0, 2, 1).reshape(batch_size, 3*seq_length)
+        stacked_attention_mask = (
+            torch.stack((attention_mask, attention_mask, attention_mask), dim=1)
+            .permute(0, 2, 1)
+            .reshape(batch_size, 3 * seq_length)
+        )
 
         # we feed in the input embeddings (not word indices as in NLP) to the model
         transformer_outputs = self.transformer(
             inputs_embeds=stacked_inputs,
             attention_mask=stacked_attention_mask,
         )
-        x = transformer_outputs['last_hidden_state']
+        x = transformer_outputs["last_hidden_state"]
 
         # reshape x so that the second dimension corresponds to the original
         # returns (0), states (1), or actions (2); i.e. x[:,1,t] is the token for s_t
-        x = x.reshape(batch_size, seq_length, 3, self.hidden_size).permute(0, 2, 1, 3)
+        x = x.reshape(batch_size, seq_length, 3, self.hidden_size).permute(
+            0, 2, 1, 3
+        )
 
         # get predictions
-        return_preds = self.predict_return(x[:,2])  # predict next return given state and action
-        state_preds = self.predict_state(x[:,2])    # predict next state given state and action
-        action_preds = self.predict_action(x[:,1])  # predict next action given state
+        return_preds = self.predict_return(
+            x[:, 2]
+        )  # predict next return given state and action
+        state_preds = self.predict_state(
+            x[:, 2]
+        )  # predict next state given state and action
+        action_preds = self.predict_action(
+            x[:, 1]
+        )  # predict next action given state
 
         return DecisionTransformerOutput(
-            state_preds=state_preds, 
-            action_preds=action_preds, 
-            return_preds=return_preds
+            last_hidden_state=transformer_outputs["last_hidden_state"],
+            state_preds=state_preds,
+            action_preds=action_preds,
+            return_preds=return_preds,
         )
-    
-    
-
