f325083f54af835efad40a5271f57a5736ff6b24
Nicolas Patry <patry.nicolas@protonmail.com>
2022-02-25 11:13:11 +0100

    Update src/transformers/pipelines/automatic_speech_recognition.py
    Co-authored-by: Patrick von Platen <patrick.v.platen@gmail.com>

--- src/transformers/pipelines/automatic_speech_recognition.py
+++ src/transformers/pipelines/automatic_speech_recognition.py
@@ -171,7 +171,7 @@ class AutomaticSpeechRecognitionPipeline(ChunkPipeline):
                 {"text": "i", "timestamps": (0.7, .9)}]`, then it means the model predicts that the letter "h" was pronounced
                 after `0.5` and before `0.6` seconds. If set to `"word"`, the pipeline will return `timestamps`
                 along the text for every word in the text. For instance if could get `[{"text": "Hi ", "timestamps":
-                (0.5,0.9), {"text": "there", "timestamps": (1.0, .1.5)}]`, then it means the model assumes the word
+                (0.5,0.9), {"text": "there", "timestamps": (1.0, .1.5)}]`, then it means the model predicts that the word
                 "hi" was pronounces before 0.5 and after 0.9 seconds.
 
         Return:
