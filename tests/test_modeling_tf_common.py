66d8b9c6e3dd291560c7dfff32edcb5c73eac485
ydshieh <ydshieh@users.noreply.github.com>
2022-02-26 21:11:24 +0100

    clean-up

--- tests/test_modeling_tf_common.py
+++ tests/test_modeling_tf_common.py
@@ -370,12 +370,7 @@ class TFModelTesterMixin:
 
         def check_outputs(tfo, pto, model_class, names):
 
-            # Some big issue (`about past_key_values`) to solve
-            """
-            E   AssertionError: 'TFPegasusForConditionalGeneration | len(tfo): 2 | len(pto): 5' != ''
-            E   - TFPegasusForConditionalGeneration | len(tfo): 2 | len(pto): 5
-            E   +
-            """
+            # Some big issue (`about past_key_values`) to solve (e.g. `TFPegasusForConditionalGeneration`)
             if names == "past_key_values":
                 return
                 # if type(tfo) == tuple and len(tfo) == 2 and isinstance(tfo[0], tf.Tensor) and type(tfo[1]) == tuple:
@@ -409,157 +404,153 @@ class TFModelTesterMixin:
 
         for model_class in self.all_model_classes:
 
-            # TODO: remove the for loop (so test once)
-            for idx in range(10):
+            config, inputs_dict = self.model_tester.prepare_config_and_inputs_for_common()
 
-                config, inputs_dict = self.model_tester.prepare_config_and_inputs_for_common()
+            # TODO: remove this block once the large negative value for attention masks is fixed.
+            for k in ["attention_mask", "encoder_attention_mask", "decoder_attention_mask"]:
+                if k in inputs_dict:
+                    attention_mask = inputs_dict[k]
+                    # (make sure no all 0s attention masks - to avoid failure at this moment)
+                    attention_mask = tf.ones_like(attention_mask, dtype=tf.int32)
+                    # (make the first sequence with all 0s attention mask -> to demonstrate the issue)
+                    # (this will fail for `TFWav2Vec2Model`)
+                    # attention_mask = tf.concat(
+                    #     [
+                    #         tf.zeros_like(attention_mask[:1], dtype=tf.int32),
+                    #         tf.cast(attention_mask[1:], dtype=tf.int32)
+                    #     ],
+                    #     axis=0
+                    # )
+                    inputs_dict[k] = attention_mask
 
-                # TODO: remove this block once the large negative value for attention masks is fixed.
-                for k in ["attention_mask", "encoder_attention_mask", "decoder_attention_mask"]:
-                    if k in inputs_dict:
-                        attention_mask = inputs_dict[k]
-                        # (make sure no all 0s attention masks - to avoid failure at this moment)
-                        attention_mask = tf.ones_like(attention_mask, dtype=tf.int32)
-                        # (make the first sequence with all 0s attention mask -> to demonstrate the issue)
-                        # (this will fail for `TFWav2Vec2Model`)
-                        # attention_mask = tf.concat(
-                        #     [
-                        #         tf.zeros_like(attention_mask[:1], dtype=tf.int32),
-                        #         tf.cast(attention_mask[1:], dtype=tf.int32)
-                        #     ],
-                        #     axis=0
-                        # )
-                        inputs_dict[k] = attention_mask
+            pt_model_class_name = model_class.__name__[2:]  # Skip the "TF" at the beginning
+            pt_model_class = getattr(transformers, pt_model_class_name)
 
-                pt_model_class_name = model_class.__name__[2:]  # Skip the "TF" at the beginning
-                pt_model_class = getattr(transformers, pt_model_class_name)
+            config.output_hidden_states = True
 
-                config.output_hidden_states = True
+            tf_model = model_class(config)
+            pt_model = pt_model_class(config)
 
-                tf_model = model_class(config)
-                pt_model = pt_model_class(config)
+            tf_inputs_dict = self._prepare_for_class(inputs_dict, model_class)
+            tf_inputs_dict_maybe_with_labels = self._prepare_for_class(
+                inputs_dict, model_class, return_labels=True
+            )
 
-                tf_inputs_dict = self._prepare_for_class(inputs_dict, model_class)
-                tf_inputs_dict_maybe_with_labels = self._prepare_for_class(
-                    inputs_dict, model_class, return_labels=True
-                )
+            # Check we can load pt model in tf and vice-versa with model => model functions
 
-                # Check we can load pt model in tf and vice-versa with model => model functions
+            tf_model = transformers.load_pytorch_model_in_tf2_model(tf_model, pt_model, tf_inputs=tf_inputs_dict)
+            pt_model = transformers.load_tf2_model_in_pytorch_model(pt_model, tf_model)
 
-                tf_model = transformers.load_pytorch_model_in_tf2_model(tf_model, pt_model, tf_inputs=tf_inputs_dict)
-                pt_model = transformers.load_tf2_model_in_pytorch_model(pt_model, tf_model)
+            # Check predictions on first output (logits/hidden-states) are close enough given low-level computational differences
 
-                # Check predictions on first output (logits/hidden-states) are close enough given low-level computational differences
+            pt_model.eval()
 
-                pt_model.eval()
+            pt_inputs_dict = prepare_pt_inputs_from_tf_inputs(tf_inputs_dict)
+            pt_inputs_dict_maybe_with_labels = prepare_pt_inputs_from_tf_inputs(tf_inputs_dict_maybe_with_labels)
 
-                pt_inputs_dict = prepare_pt_inputs_from_tf_inputs(tf_inputs_dict)
-                pt_inputs_dict_maybe_with_labels = prepare_pt_inputs_from_tf_inputs(tf_inputs_dict_maybe_with_labels)
+            # need to rename encoder-decoder "inputs" for PyTorch
+            if "inputs" in pt_inputs_dict and self.is_encoder_decoder:
+                pt_inputs_dict["input_ids"] = pt_inputs_dict.pop("inputs")
 
-                # need to rename encoder-decoder "inputs" for PyTorch
-                if "inputs" in pt_inputs_dict and self.is_encoder_decoder:
-                    pt_inputs_dict["input_ids"] = pt_inputs_dict.pop("inputs")
+            # Output all for aggressive testing
+            output_kwargs = {"output_hidden_states": True}
+            # Pure convolutional models have no attention
+            # TODO: use a better and general criteria
+            if "TFConvNext" not in model_class.__name__:
+                output_kwargs["output_attentions"] = True
 
-                # Output all for aggressive testing
-                output_kwargs = {"output_hidden_states": True}
-                # Pure convolutional models have no attention
-                # TODO: use a better and general criteria
-                if "TFConvNext" not in model_class.__name__:
-                    output_kwargs["output_attentions"] = True
+            # Original test: check without `labels`
+            with torch.no_grad():
+                pto = pt_model(**pt_inputs_dict, **output_kwargs)
+            tfo = tf_model(tf_inputs_dict, **output_kwargs)
 
-                # Original test: check without `labels`
+            tf_keys = [k for k, v in tfo.items() if v is not None]
+            pt_keys = [k for k, v in pto.items() if v is not None]
+
+            self.assertEqual(tf_keys, pt_keys)
+            check_outputs(tfo, pto, model_class, names=tf_keys)
+
+            # check the case where `labels` is passed
+            has_labels = any(
+                x in tf_inputs_dict_maybe_with_labels for x in ["labels", "next_sentence_label", "start_positions"]
+            )
+            if has_labels:
+
                 with torch.no_grad():
-                    pto = pt_model(**pt_inputs_dict, **output_kwargs)
-                tfo = tf_model(tf_inputs_dict, **output_kwargs)
+                    pto = pt_model(**pt_inputs_dict_maybe_with_labels, **output_kwargs)
+                tfo = tf_model(tf_inputs_dict_maybe_with_labels, **output_kwargs)
+
+                # Some models' output class don't have `loss` attribute despite `labels` is used.
+                # TODO: identify which models
+                tf_loss = getattr(tfo, "loss", None)
+                pt_loss = getattr(pto, "loss", None)
+
+                # Some PT models return loss while the corresponding TF models don't (i.e. `None` for `loss`).
+                #   - TFFlaubertWithLMHeadModel
+                #   - TFFunnelForPreTraining
+                #   - TFElectraForPreTraining
+                #   - TFXLMWithLMHeadModel
+                # TODO: Fix PT/TF diff -> remove this condition to fail the test if a diff occurs
+                if not ((tf_loss is None and pt_loss is None) or (tf_loss is not None and pt_loss is not None)):
+                    if model_class.__name__ not in [
+                        "TFFlaubertWithLMHeadModel",
+                        "TFFunnelForPreTraining",
+                        "TFElectraForPreTraining",
+                        "TFXLMWithLMHeadModel",
+                    ]:
+                        self.assertEqual(tf_loss is None, pt_loss is None)
 
                 tf_keys = [k for k, v in tfo.items() if v is not None]
                 pt_keys = [k for k, v in pto.items() if v is not None]
 
-                self.assertEqual(tf_keys, pt_keys)
-                check_outputs(tfo, pto, model_class, names=tf_keys)
-
-                # check the case where `labels` is passed
-                has_labels = any(
-                    x in tf_inputs_dict_maybe_with_labels for x in ["labels", "next_sentence_label", "start_positions"]
-                )
-                if has_labels:
-
-                    with torch.no_grad():
-                        pto = pt_model(**pt_inputs_dict_maybe_with_labels, **output_kwargs)
-                    tfo = tf_model(tf_inputs_dict_maybe_with_labels, **output_kwargs)
-
-                    # Some models' output class don't have `loss` attribute despite `labels` is used.
-                    # TODO: identify which models
-                    tf_loss = getattr(tfo, "loss", None)
-                    pt_loss = getattr(pto, "loss", None)
-
-                    # Some PT models return loss while the corresponding TF models don't (i.e. `None` for `loss`).
-                    #   - TFFlaubertWithLMHeadModel
-                    #   - TFFunnelForPreTraining
-                    #   - TFElectraForPreTraining
-                    #   - TFXLMWithLMHeadModel
-                    # TODO: Fix PT/TF diff -> remove this condition to fail the test if a diff occurs
-                    if not ((tf_loss is None and pt_loss is None) or (tf_loss is not None and pt_loss is not None)):
-                        if model_class.__name__ not in [
-                            "TFFlaubertWithLMHeadModel",
-                            "TFFunnelForPreTraining",
-                            "TFElectraForPreTraining",
-                            "TFXLMWithLMHeadModel",
-                        ]:
-                            self.assertEqual(tf_loss is None, pt_loss is None)
-
-                    tf_keys = [k for k, v in tfo.items() if v is not None]
-                    pt_keys = [k for k, v in pto.items() if v is not None]
-
-                    # TODO: remove these 2 conditions once the above TODOs (above loss) are implemented
-                    # (Also, `TFTransfoXLLMHeadModel` has no `loss` while `TransfoXLLMHeadModel` return `losses`)
-                    if tf_keys != pt_keys:
-                        if model_class.__name__ not in [
-                            "TFFlaubertWithLMHeadModel",
-                            "TFFunnelForPreTraining",
-                            "TFElectraForPreTraining",
-                            "TFXLMWithLMHeadModel",
-                        ] + ["TFTransfoXLLMHeadModel"]:
-                            self.assertEqual(tf_keys, pt_keys)
-
-                    # Since we deliberately make some tests pass above (regarding the `loss`), let's still try to test
-                    # some remaining attributes in the outputs.
-                    # TODO: remove this block of `index` computing once the above TODOs (above loss) are implemented
-                    # compute the 1st `index` where `tf_keys` and `pt_keys` is different
-                    index = 0
-                    for _ in range(min(len(tf_keys), len(pt_keys))):
-                        if tf_keys[index] == pt_keys[index]:
-                            index += 1
-                        else:
-                            break
-                    if tf_keys[:index] != pt_keys[:index]:
+                # TODO: remove these 2 conditions once the above TODOs (above loss) are implemented
+                # (Also, `TFTransfoXLLMHeadModel` has no `loss` while `TransfoXLLMHeadModel` return `losses`)
+                if tf_keys != pt_keys:
+                    if model_class.__name__ not in [
+                        "TFFlaubertWithLMHeadModel",
+                        "TFFunnelForPreTraining",
+                        "TFElectraForPreTraining",
+                        "TFXLMWithLMHeadModel",
+                    ] + ["TFTransfoXLLMHeadModel"]:
                         self.assertEqual(tf_keys, pt_keys)
 
-                    # 1. Some models require extra condition to return loss. For example, `BertForPreTraining` requires
-                    #    both`labels` and `next_sentence_label`.
-                    # TODO: remove this condition once the above TODOs (above loss) are implemented
-                    if tf_loss is not None and pt_loss is not None:
+                # Since we deliberately make some tests pass above (regarding the `loss`), let's still try to test
+                # some remaining attributes in the outputs.
+                # TODO: remove this block of `index` computing once the above TODOs (above loss) are implemented
+                # compute the 1st `index` where `tf_keys` and `pt_keys` is different
+                index = 0
+                for _ in range(min(len(tf_keys), len(pt_keys))):
+                    if tf_keys[index] == pt_keys[index]:
+                        index += 1
+                    else:
+                        break
+                if tf_keys[:index] != pt_keys[:index]:
+                    self.assertEqual(tf_keys, pt_keys)
 
-                        # check anything else than `loss`
-                        keys = [k for k in tf_keys]
-                        check_outputs(tfo[1:index], pto[1:index], model_class, names=keys[1:index])
+                # Some models require extra condition to return loss. For example, `(TF)BertForPreTraining` requires
+                # both`labels` and `next_sentence_label`.
+                if tf_loss is not None and pt_loss is not None:
 
-                        # check `loss`
+                    # check anything else than `loss`
+                    keys = [k for k in tf_keys]
+                    check_outputs(tfo[1:index], pto[1:index], model_class, names=keys[1:index])
 
-                        # tf models returned loss is usually a tensor rather than a scalar.
-                        # (see `hf_compute_loss`: it uses `tf.keras.losses.Reduction.NONE`)
-                        # Change it here to a scalar to match PyTorch models' loss
-                        tf_loss = tf.math.reduce_mean(tf_loss).numpy()
-                        pt_loss = pt_loss.numpy()
+                    # check `loss`
 
-                        tf_nans = np.copy(np.isnan(tf_loss))
-                        pt_nans = np.copy(np.isnan(pt_loss))
-                        # the 2 losses need to be both nan or both not nan
-                        self.assertEqual(tf_nans, pt_nans)
+                    # tf models returned loss is usually a tensor rather than a scalar.
+                    # (see `hf_compute_loss`: it uses `tf.keras.losses.Reduction.NONE`)
+                    # Change it here to a scalar to match PyTorch models' loss
+                    tf_loss = tf.math.reduce_mean(tf_loss).numpy()
+                    pt_loss = pt_loss.numpy()
 
-                        if not tf_nans:
-                            max_diff = np.amax(np.abs(tf_loss - pt_loss))
-                            self.assertLessEqual(max_diff, 1e-5)
+                    tf_nans = np.copy(np.isnan(tf_loss))
+                    pt_nans = np.copy(np.isnan(pt_loss))
+                    # the 2 losses need to be both nan or both not nan
+                    self.assertEqual(tf_nans, pt_nans)
+
+                    if not tf_nans:
+                        max_diff = np.amax(np.abs(tf_loss - pt_loss))
+                        self.assertLessEqual(max_diff, 1e-5)
 
     def test_compile_tf_model(self):
         config, inputs_dict = self.model_tester.prepare_config_and_inputs_for_common()
