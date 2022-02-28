e2a926ed6aa784d41fe881a42f1fb6b1aaa79904
Patrick von Platen <patrick.v.platen@gmail.com>
2022-02-28 17:11:27 +0100

    Apply suggestions from code review

--- src/transformers/models/wav2vec2_with_lm/processing_wav2vec2_with_lm.py
+++ src/transformers/models/wav2vec2_with_lm/processing_wav2vec2_with_lm.py
@@ -425,7 +425,7 @@ class Wav2Vec2ProcessorWithLM(ProcessorMixin):
         ...     logits = model(input_values).logits[0].cpu().numpy()
 
         >>> # retrieve word stamps (analogous commands for `output_char_offsets`)
-        >>> outputs = tokenizer.decode(pred_ids, output_word_offsets=True)
+        >>> outputs = tokenizer.decode(logits, output_word_offsets=True)
         >>> # compute `time_offset` in seconds as product of downsampling ratio and sampling_rate
         >>> time_offset = model.config.inputs_to_logits_ratio / feature_extractor.sampling_rate
 
