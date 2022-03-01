286fdc6b3c51cd09e57b29ba11603f585073aefd
NielsRogge <48327001+NielsRogge@users.noreply.github.com>
2022-03-01 18:09:52 +0100

    [vision] Add problem_type support (#15851)
    * Add problem_type to missing models
    
    * Fix deit test
    
    Co-authored-by: Niels Rogge <nielsrogge@Nielss-MacBook-Pro.local>

--- tests/test_modeling_common.py
+++ tests/test_modeling_common.py
@@ -1873,7 +1873,10 @@ class ModelTesterMixin:
         ]
 
         for model_class in self.all_model_classes:
-            if model_class not in get_values(MODEL_FOR_SEQUENCE_CLASSIFICATION_MAPPING):
+            if model_class not in [
+                *get_values(MODEL_FOR_SEQUENCE_CLASSIFICATION_MAPPING),
+                *get_values(MODEL_FOR_IMAGE_CLASSIFICATION_MAPPING),
+            ]:
                 continue
 
             for problem_type in problem_types:
