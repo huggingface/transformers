f4ea8bf2a826e47b0a3e1921297046e10c4df438
fra <francesco.zuppichini@gmail.com>
2022-03-02 11:44:53 +0100

    removed einops

--- src/transformers/models/cvt/modeling_cvt.py
+++ src/transformers/models/cvt/modeling_cvt.py
@@ -19,19 +19,16 @@ import collections.abc
 import math
 import warnings
 from dataclasses import dataclass
-from typing import Optional, OrderedDict, Tuple
+from typing import Optional, Tuple
 
 import torch
 import torch.utils.checkpoint
 from torch import nn
 from torch.nn import CrossEntropyLoss, MSELoss
 
-from einops import rearrange
-from einops.layers.torch import Rearrange
-
 from ...activations import ACT2FN
 from ...file_utils import add_code_sample_docstrings, add_start_docstrings, add_start_docstrings_to_model_forward
-from ...modeling_outputs import BaseModelOutput, ModelOutput, SequenceClassifierOutput
+from ...modeling_outputs import ModelOutput, SequenceClassifierOutput
 from ...modeling_utils import PreTrainedModel, find_pruneable_heads_and_indices, prune_linear_layer
 from ...utils import logging
 from .configuration_cvt import CvtConfig
@@ -156,11 +153,8 @@ def trunc_normal_(tensor, mean=0.0, std=1.0, a=-2.0, b=2.0):
     normal distribution. The values are effectively drawn from the normal distribution :math:`\mathcal{N}(\text{mean},
     \text{std}^2)` with values outside :math:`[a, b]` redrawn until they are within the bounds. The method used for
     generating the random values works best when :math:`a \leq \text{mean} \leq b`.
-        tensor: an n-dimensional `torch.Tensor`
-        mean: the mean of the normal distribution
-        std: the standard deviation of the normal distribution
-        a: the minimum cutoff value
-        b: the maximum cutoff value
+        tensor: an n-dimensional `torch.Tensor` mean: the mean of the normal distribution std: the standard deviation
+        of the normal distribution a: the minimum cutoff value b: the maximum cutoff value
     Examples:
         >>> w = torch.empty(3, 5) >>> nn.init.trunc_normal_(w)
     """
@@ -223,25 +217,26 @@ class ConvEmbeddings(nn.Module):
         pixel_values = pixel_values.permute(0, 2, 1).view(batch_size, num_channels, height, width)
         return pixel_values
 
+
 class CvtSelfAttentionConvProjection(nn.Sequential):
     def __init__(self, embed_dim, kernel_size, padding, stride):
         super().__init__()
-        self.conv = nn.Conv2d(embed_dim,
-                                embed_dim,
-                                kernel_size=kernel_size,
-                                padding=padding,
-                                stride=stride,
-                                bias=False,
-                                groups=embed_dim,
-                            )
+        self.conv = nn.Conv2d(
+            embed_dim,
+            embed_dim,
+            kernel_size=kernel_size,
+            padding=padding,
+            stride=stride,
+            bias=False,
+            groups=embed_dim,
+        )
         self.norm = nn.BatchNorm2d(embed_dim)
-        
 
 
 class CvtSelfAttentionLinearProjection(nn.Module):
     def __init__(self):
         super().__init__()
-    
+
     def forward(self, hidden_states):
         batch_size, num_channels, height, width = hidden_states.shape
         hidden_size = height * width
@@ -249,12 +244,13 @@ class CvtSelfAttentionLinearProjection(nn.Module):
         hidden_states = hidden_states.view(batch_size, num_channels, hidden_size).permute(0, 2, 1)
         return hidden_states
 
+
 class CvtSelfAttentionProjection(nn.Sequential):
-    def __init__(self, embed_dim, kernel_size, padding, stride, projection_method = 'dw_bn'):
+    def __init__(self, embed_dim, kernel_size, padding, stride, projection_method="dw_bn"):
         super().__init__()
-        if projection_method == 'dw_bn':
+        if projection_method == "dw_bn":
             self.conv_projection = CvtSelfAttentionConvProjection(embed_dim, kernel_size, padding, stride)
-        self.linear_projection  = CvtSelfAttentionLinearProjection()
+        self.linear_projection = CvtSelfAttentionLinearProjection()
 
 
 class CvtSelfAttention(nn.Module):
@@ -280,16 +276,17 @@ class CvtSelfAttention(nn.Module):
         self.num_heads = num_heads
 
         self.conv_projection_query = CvtSelfAttentionProjection(
-            embed_dim, kernel_size, padding_q,
-            stride_q, projection_method='linear' if qkv_projection_method == 'avg' else qkv_projection_method
+            embed_dim,
+            kernel_size,
+            padding_q,
+            stride_q,
+            projection_method="linear" if qkv_projection_method == "avg" else qkv_projection_method,
         )
         self.conv_projection_key = CvtSelfAttentionProjection(
-            embed_dim, kernel_size, padding_kv,
-            stride_kv,projection_method= qkv_projection_method
+            embed_dim, kernel_size, padding_kv, stride_kv, projection_method=qkv_projection_method
         )
         self.conv_projection_value = CvtSelfAttentionProjection(
-            embed_dim, kernel_size, padding_kv,
-            stride_kv, projection_method=qkv_projection_method
+            embed_dim, kernel_size, padding_kv, stride_kv, projection_method=qkv_projection_method
         )
 
         self.projection_query = nn.Linear(embed_dim, embed_dim, bias=qkv_bias)
@@ -299,11 +296,14 @@ class CvtSelfAttention(nn.Module):
         self.dropout = nn.Dropout(attention_drop_rate)
 
     def rearrange_for_multi_head_attention(self, inputs):
-        batch_size, hidden_size, _,  = inputs.shape
+        (
+            batch_size,
+            hidden_size,
+            _,
+        ) = inputs.shape
         head_dim = self.embed_dim // self.num_heads
         # rearrange 'b t (h d) -> b h t d'
         return inputs.view(batch_size, hidden_size, self.num_heads, head_dim).permute(0, 2, 1, 3)
-
 
     def forward(self, hidden_states, height, width, head_mask=None, output_attentions=False):
         if self.with_cls_token:
@@ -315,14 +315,14 @@ class CvtSelfAttention(nn.Module):
         key = self.conv_projection_key(hidden_states)
         query = self.conv_projection_query(hidden_states)
         value = self.conv_projection_value(hidden_states)
-        
+
         if self.with_cls_token:
             query = torch.cat((cls_token, query), dim=1)
             key = torch.cat((cls_token, key), dim=1)
             value = torch.cat((cls_token, value), dim=1)
 
         head_dim = self.embed_dim // self.num_heads
-     
+
         query = self.rearrange_for_multi_head_attention(self.projection_query(query))
         key = self.rearrange_for_multi_head_attention(self.projection_key(key))
         value = self.rearrange_for_multi_head_attention(self.projection_value(value))
@@ -838,7 +838,9 @@ class CvtForImageClassification(CvtPreTrainedModel):
         if self.config.cls_token[-1]:
             sequence_output = self.layernorm(cls_token)
         else:
-            sequence_output = rearrange(sequence_output, "b c h w -> b (h w) c")
+            batch_size, num_channels, height, width = sequence_output.shape
+            # rearrange "b c h w -> b (h w) c"
+            sequence_output = sequence_output.view(batch_size, num_channels, height * width).permute(0, 2, 1)
             sequence_output = self.layernorm(sequence_output)
 
         sequence_output_mean = sequence_output.mean(dim=1)
