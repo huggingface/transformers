cee809c7fc22cf3f6460b212ed1d558424bcdd19
Ben Eyal <ben.eyal.89@gmail.com>
2022-03-04 21:15:26 +0200

    Mimic Rust tokenizer behavior for tokenizers
    - Albert
    - Barthez
    - Camembert
    - MBart50
    - T5

--- src/transformers/models/mbart50/tokenization_mbart50.py
+++ src/transformers/models/mbart50/tokenization_mbart50.py
@@ -237,13 +237,18 @@ class MBart50Tokenizer(PreTrainedTokenizer):
         """Converts a sequence of tokens (string) in a single string."""
         current_sub_tokens = []
         out_string = ""
+        prev_is_special = False
         for token in tokens:
             # make sure that special tokens are not decoded using sentencepiece model
             if token in self.all_special_tokens:
+                if not prev_is_special:
+                    out_string += " "
                 out_string += self.sp_model.decode(current_sub_tokens) + token
+                prev_is_special = True
                 current_sub_tokens = []
             else:
                 current_sub_tokens.append(token)
+                prev_is_special = False
         out_string += self.sp_model.decode(current_sub_tokens)
         return out_string.strip()
 
