603684df24e3f0f8571884bb8d049a145c349adc
Niels Rogge <nielsrogge@Nielss-MacBook-Pro.local>
2022-03-10 10:47:20 +0100

    Apply suggestion from code review

--- tests/test_modeling_common.py
+++ tests/test_modeling_common.py
@@ -1045,7 +1045,7 @@ class ModelTesterMixin:
     def test_retain_grad_hidden_states_attentions(self):
         config, inputs_dict = self.model_tester.prepare_config_and_inputs_for_common()
         config.output_hidden_states = True
-        config.output_attentions = True if self.has_attentions else False
+        config.output_attentions = self.has_attentions
 
         # no need to test all models as different heads yield the same functionality
         model_class = self.all_model_classes[0]
