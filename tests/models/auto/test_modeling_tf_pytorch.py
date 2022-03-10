89422dedbc56425c52aa3312f79c8840dbde4411
ydshieh <ydshieh@users.noreply.github.com>
2022-03-10 11:57:25 +0100

    Remove tests that are not supposed to be

--- tests/models/auto/test_modeling_tf_pytorch.py
+++ tests/models/auto/test_modeling_tf_pytorch.py
@@ -110,9 +110,6 @@ class TFPTAutoModelTest(unittest.TestCase):
             self.assertIsInstance(config, GPT2Config)
 
             model = TFAutoModelForCausalLM.from_pretrained(model_name, from_pt=True)
-            model, loading_info = TFAutoModelForCausalLM.from_pretrained(
-                model_name, output_loading_info=True, from_pt=True
-            )
             self.assertIsNotNone(model)
             self.assertIsInstance(model, TFGPT2LMHeadModel)
 
@@ -146,9 +143,6 @@ class TFPTAutoModelTest(unittest.TestCase):
             self.assertIsInstance(config, BertConfig)
 
             model = TFAutoModelForMaskedLM.from_pretrained(model_name, from_pt=True)
-            model, loading_info = TFAutoModelForMaskedLM.from_pretrained(
-                model_name, output_loading_info=True, from_pt=True
-            )
             self.assertIsNotNone(model)
             self.assertIsInstance(model, TFBertForMaskedLM)
 
@@ -167,9 +161,6 @@ class TFPTAutoModelTest(unittest.TestCase):
             self.assertIsInstance(config, T5Config)
 
             model = TFAutoModelForSeq2SeqLM.from_pretrained(model_name, from_pt=True)
-            model, loading_info = TFAutoModelForSeq2SeqLM.from_pretrained(
-                model_name, output_loading_info=True, from_pt=True
-            )
             self.assertIsNotNone(model)
             self.assertIsInstance(model, TFT5ForConditionalGeneration)
 
