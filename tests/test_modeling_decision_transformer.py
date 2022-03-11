986f185561744577b6f3ceb222e4f03c8b61e685
Edward Beeching <edbeeching@gmail.com>
2022-03-11 16:27:37 +0100

    Updated tests to include many of the ModelTesterMixin tests.
    The following tests are skipped: test_generate_without_input_ids, test_pruning, test_resize_embeddings, test_head_masking, test_attention_outputs, test_hidden_states_output, test_inputs_embeds, test_model_common_attributes

--- tests/test_modeling_decision_transformer.py
+++ tests/test_modeling_decision_transformer.py
@@ -15,6 +15,7 @@
 """ Testing suite for the PyTorch DecisionTransformer model. """
 
 
+import inspect
 import unittest
 
 from transformers import DecisionTransformerConfig, is_torch_available
@@ -22,7 +23,7 @@ from transformers.testing_utils import require_torch, slow, torch_device
 
 from .test_configuration_common import ConfigTester
 from .test_generation_utils import GenerationTesterMixin
-from .test_modeling_common import floats_tensor, ids_tensor, random_attention_mask
+from .test_modeling_common import ModelTesterMixin, floats_tensor, ids_tensor, random_attention_mask
 
 
 if is_torch_available():
@@ -44,6 +45,7 @@ class DecisionTransformerModelTester:
         state_dim=17,
         hidden_size=23,
         max_length=11,
+        is_training=True,
     ):
         self.parent = parent
         self.batch_size = batch_size
@@ -52,12 +54,12 @@ class DecisionTransformerModelTester:
         self.state_dim = state_dim
         self.hidden_size = hidden_size
         self.max_length = max_length
+        self.is_training = is_training
 
     def prepare_config_and_inputs(self):
         states = floats_tensor((self.batch_size, self.seq_length, self.state_dim))
         actions = floats_tensor((self.batch_size, self.seq_length, self.act_dim))
         rewards = floats_tensor((self.batch_size, self.seq_length, 1))
-        # dones = ids_tensor((self.batch_size, self.seq_length, 1), vocab_size=2)
         returns_to_go = floats_tensor((self.batch_size, self.seq_length, 1))
         timesteps = ids_tensor((self.batch_size, self.seq_length), vocab_size=1000)
         attention_mask = random_attention_mask((self.batch_size, self.seq_length))
@@ -135,11 +137,23 @@ class DecisionTransformerModelTester:
 
 @require_torch
 class DecisionTransformerModelTest(  # ModelTesterMixin is removed as this model as the input / outputs of this model do not conform to a typical NLP model.
-    GenerationTesterMixin, unittest.TestCase
+    ModelTesterMixin, GenerationTesterMixin, unittest.TestCase
 ):
 
     all_model_classes = (DecisionTransformerModel,) if is_torch_available() else ()
     all_generative_model_classes = ()
+
+    # Ignoring of a failing test from GenerationTesterMixin, as the model does not use inputs_ids
+    test_generate_without_input_ids = False
+
+    # Ignoring of a failing tests from ModelTesterMixin, as the model does not implement these features
+    test_pruning = False
+    test_resize_embeddings = False
+    test_head_masking = False
+    test_attention_outputs = False
+    test_hidden_states_output = False
+    test_inputs_embeds = False
+    test_model_common_attributes = False
 
     def setUp(self):
         self.model_tester = DecisionTransformerModelTester(self)
@@ -152,12 +166,28 @@ class DecisionTransformerModelTest(  # ModelTesterMixin is removed as this model
         config_and_inputs = self.model_tester.prepare_config_and_inputs()
         self.model_tester.create_and_check_model(*config_and_inputs)
 
-    def test_generate_without_input_ids(self):
-        # Override of a failing test from GenerationTesterMixin, as the model does not use inputs_ids
-        pass
-
     @slow
     def test_model_from_pretrained(self):
         for model_name in DECISION_TRANSFORMER_PRETRAINED_MODEL_ARCHIVE_LIST[:1]:
             model = DecisionTransformerModel.from_pretrained(model_name)
             self.assertIsNotNone(model)
+
+    def test_forward_signature(self):
+        config, _ = self.model_tester.prepare_config_and_inputs_for_common()
+
+        for model_class in self.all_model_classes:
+            model = model_class(config)
+            signature = inspect.signature(model.forward)
+            # signature.parameters is an OrderedDict => so arg_names order is deterministic
+            arg_names = [*signature.parameters.keys()]
+
+            expected_arg_names = [
+                "states",
+                "actions",
+                "rewards",
+                "returns_to_go",
+                "timesteps",
+                "attention_mask",
+            ]
+
+            self.assertListEqual(arg_names[: len(expected_arg_names)], expected_arg_names)
