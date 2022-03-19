b2863a9993782cb71ed0eabf5c04b79c2353c517
Sayak Paul <spsayakpaul@gmail.com>
2022-03-19 09:02:28 +0530

    chore: resolved conflict.

--- src/transformers/models/vit_mae/modeling_vit_mae.py
+++ src/transformers/models/vit_mae/modeling_vit_mae.py
@@ -344,10 +344,7 @@ class ViTMAESelfAttention(nn.Module):
         self.dropout = nn.Dropout(config.attention_probs_dropout_prob)
 
     def transpose_for_scores(self, x):
-        new_x_shape = x.size()[:-1] + (
-            self.num_attention_heads,
-            self.attention_head_size,
-        )
+        new_x_shape = x.size()[:-1] + (self.num_attention_heads, self.attention_head_size)
         x = x.view(*new_x_shape)
         return x.permute(0, 2, 1, 3)
 
