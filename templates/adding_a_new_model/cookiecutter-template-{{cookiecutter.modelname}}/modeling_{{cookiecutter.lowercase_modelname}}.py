2c5cb1c82905f3ce35662a826fef8e0a26b1a9a6
matt <rocketknight1@gmail.com>
2022-03-22 18:45:25 +0000

    Adding types to template model

--- templates/adding_a_new_model/cookiecutter-template-{{cookiecutter.modelname}}/modeling_{{cookiecutter.lowercase_modelname}}.py
+++ templates/adding_a_new_model/cookiecutter-template-{{cookiecutter.modelname}}/modeling_{{cookiecutter.lowercase_modelname}}.py
@@ -25,6 +25,7 @@ import torch.utils.checkpoint
 from packaging import version
 from torch import nn
 from torch.nn import BCEWithLogitsLoss, CrossEntropyLoss, MSELoss
+from typing import Optional, Tuple, List, Union
 
 from ...activations import ACT2FN
 from ...file_utils import (
