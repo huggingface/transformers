0c55d47cded0c711a4359afb04d73ffb86b7d913
NielsRogge <48327001+NielsRogge@users.noreply.github.com>
2022-03-22 08:51:13 +0100

    Add GLPN (#16199)
    * First draft
    
    * Fix logits calculation
    
    * Improve tests
    
    * Add copied from statements
    
    * Fix base_model_prefix
    
    * Improve implementation, upload new models
    
    * Update design
    
    * Fix integration test
    
    * Add model to README and toctree
    
    * Add document image
    
    * Apply suggestions from code review
    
    * Apply suggestions from code review
    
    Co-authored-by: Sylvain Gugger <35901082+sgugger@users.noreply.github.com>
    
    * Add decoder_hidden_size attribute
    
    * Update design of decoder
    
    * Add DepthEstimatorOutput class
    
    * Rename in_index to head_in_index and add feature extractor tests
    
    * Apply suggestions from code review
    
    * Apply suggestions from code review
    
    * Update pretrained model name and add to doc tests
    
    * Remove test.py script
    
    * Update copied from statements and clean up
    
    Co-authored-by: Niels Rogge <nielsrogge@Nielss-MacBook-Pro.local>
    Co-authored-by: Sylvain Gugger <35901082+sgugger@users.noreply.github.com>

--- src/transformers/modeling_outputs.py
+++ src/transformers/modeling_outputs.py
@@ -878,3 +878,33 @@ class ImageClassifierOutput(ModelOutput):
     logits: torch.FloatTensor = None
     hidden_states: Optional[Tuple[torch.FloatTensor]] = None
     attentions: Optional[Tuple[torch.FloatTensor]] = None
+
+
+@dataclass
+class DepthEstimatorOutput(ModelOutput):
+    """
+    Base class for outputs of depth estimation models.
+
+    Args:
+        loss (`torch.FloatTensor` of shape `(1,)`, *optional*, returned when `labels` is provided):
+            Classification (or regression if config.num_labels==1) loss.
+        predicted_depth (`torch.FloatTensor` of shape `(batch_size, height, width)`):
+            Predicted depth for each pixel.
+
+        hidden_states (`tuple(torch.FloatTensor)`, *optional*, returned when `output_hidden_states=True` is passed or when `config.output_hidden_states=True`):
+            Tuple of `torch.FloatTensor` (one for the output of the embeddings + one for the output of each layer) of
+            shape `(batch_size, num_channels, height, width)`.
+
+            Hidden-states of the model at the output of each layer plus the initial embedding outputs.
+        attentions (`tuple(torch.FloatTensor)`, *optional*, returned when `output_attentions=True` is passed or when `config.output_attentions=True`):
+            Tuple of `torch.FloatTensor` (one for each layer) of shape `(batch_size, num_heads, patch_size,
+            sequence_length)`.
+
+            Attentions weights after the attention softmax, used to compute the weighted average in the self-attention
+            heads.
+    """
+
+    loss: Optional[torch.FloatTensor] = None
+    predicted_depth: torch.FloatTensor = None
+    hidden_states: Optional[Tuple[torch.FloatTensor]] = None
+    attentions: Optional[Tuple[torch.FloatTensor]] = None
