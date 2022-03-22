a439f1a0a6e2485c4d525c82b3e25bb127d02e53
Thomas Wolf <thomas@huggingface.co>
2022-03-22 14:59:35 +0100

    update conversion

--- src/transformers/models/bigscience176b/convert_bigscience176b_original_checkpoint_to_pytorch.py
+++ src/transformers/models/bigscience176b/convert_bigscience176b_original_checkpoint_to_pytorch.py
@@ -18,6 +18,8 @@
 import argparse
 
 import torch
+import os
+import re
 
 from transformers import BigScience176BConfig, BigScience176BModel
 from transformers.file_utils import CONFIG_NAME, WEIGHTS_NAME
@@ -25,6 +27,24 @@ from transformers.utils import logging
 
 
 logging.set_verbosity_info()
+
+def layer_name_mapping(key, file):
+    """ Convert Megatron-DeepSpeed TP/PP weights mapping in transformers PP only"""
+    # Handle first and last layers
+    if key == "word_embeddings.weight":
+        return key
+    if key == "word_embeddings.norm.weight":
+        return "word_embeddings_layernorm.weight"
+    if key == "word_embeddings.norm.bias":
+        return "word_embeddings_layernorm.bias"
+    if key == "weight":
+        return "ln_f.weight"
+    if key == "bias":
+        return "ln_f.bias"
+
+    # Handle transformer blocks
+    layer_number = int(re.match(r'.*layer_(\d*).*', file))[1]
+    return f'h.{layer_number}.' + key
 
 
 def convert_bigscience176b_checkpoint_to_pytorch(bigscience176b_checkpoint_path, bigscience176b_config_file, pytorch_dump_folder_path):
@@ -35,8 +55,28 @@ def convert_bigscience176b_checkpoint_to_pytorch(bigscience176b_checkpoint_path,
         config = BigScience176BConfig.from_json_file(bigscience176b_config_file)
     model = BigScience176BModel(config)
 
-    # Load weights from numpy
-    # load_tf_weights_in_bigscience176b(model, config, bigscience176b_checkpoint_path)
+    file_names = os.listdir(bigscience176b_checkpoint_path)
+    file_names = list(filter(lambda s: s.startswith('layer') and 'model_00' in s, file_names))
+
+    for file in file_names:
+        tensors = None
+        for i in range(config.pretraining_tp):
+            # load all TP files
+            f_name = file.replace('model_00', f'model_0{i}')
+            temp = torch.load(f_name, map_location='cpu')
+
+            # Rename keys in the transformers names
+            for key in temp.keys():
+                temp[layer_name_mapping(key, file)] = temp.pop(key)
+
+            if tensor is None:
+                tensor = temp
+            else:
+                for key in tensor.keys():
+                    cat_dim = 0
+                    tensor[key] = torch.cat([tensor[key], temp[key]], dim=cat_dim)
+        
+
 
     # Save pytorch-model
     pytorch_weights_dump_path = pytorch_dump_folder_path + "/" + WEIGHTS_NAME
@@ -61,7 +101,7 @@ if __name__ == "__main__":
         "--bigscience176b_config_file",
         default="",
         type=str,
-        help="An optional config json file corresponding to the pre-trained OpenAI model. \n"
+        help="An optional config json file corresponding to the pre-trained model. \n"
         "This specifies the model architecture.",
     )
     args = parser.parse_args()
