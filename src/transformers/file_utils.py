3ef38f0c33e496507619b8b08f1b55f5b88c06b1
Sylvain Gugger <Sylvain.gugger@gmail.com>
2022-03-23 16:10:16 -0400

    Revert unintentional change

--- src/transformers/file_utils.py
+++ src/transformers/file_utils.py
@@ -16,6 +16,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """
+File utilities: utilities related to download and cache models
+
 This module should not be update anymore and is only left for backward compatibility.
 """
 
