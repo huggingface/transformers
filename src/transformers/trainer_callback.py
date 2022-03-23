05f2aedb414f6db58503c856aabd5da09b8f83cc
OllieBroadhurst <46894149+OllieBroadhurst@users.noreply.github.com>
2022-03-23 20:18:51 +0200

    Update src/transformers/trainer_callback.py
    Co-authored-by: Sylvain Gugger <35901082+sgugger@users.noreply.github.com>

--- src/transformers/trainer_callback.py
+++ src/transformers/trainer_callback.py
@@ -419,7 +419,7 @@ class DefaultFlowCallback(TrainerCallback):
         if (
             args.evaluation_strategy == IntervalStrategy.STEPS
             and state.global_step % args.eval_steps == 0
-            and args.eval_delay < state.global_step
+            and args.eval_delay <= state.global_step
         ):
             control.should_evaluate = True
 
