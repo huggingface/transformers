6c49c3457d81198656bf1c9010487e31644b77d9
OllieBroadhurst <46894149+OllieBroadhurst@users.noreply.github.com>
2022-03-23 20:18:07 +0200

    Update src/transformers/trainer_callback.py
    Co-authored-by: Sylvain Gugger <35901082+sgugger@users.noreply.github.com>

--- src/transformers/trainer_callback.py
+++ src/transformers/trainer_callback.py
@@ -443,7 +443,7 @@ class DefaultFlowCallback(TrainerCallback):
             control.should_log = True
 
         # Evaluate
-        if args.evaluation_strategy == IntervalStrategy.EPOCH and args.eval_delay < state.epoch:
+        if args.evaluation_strategy == IntervalStrategy.EPOCH and args.eval_delay <= state.epoch:
             control.should_evaluate = True
 
         # Save
