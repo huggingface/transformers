adb8c8adbf335521b633f22ff72f3b664b1644af
arnaudstiegler <arnaud.stiegler@gmail.com>
2022-03-23 23:12:36 -0400

    clean version | passes tests

--- src/transformers/models/trocr/modeling_trocr.py
+++ src/transformers/models/trocr/modeling_trocr.py
@@ -891,33 +891,32 @@ class TrOCRForCausalLM(TrOCRPreTrainedModel):
         Example:
 
         ```python
-        >>> from transformers import VisionEncoderDecoderModel, TrOCRForCausalLM, ViTModel, TrOCRConfig, ViTConfig, TrOCRProcessor
-
-        >>> import numpy
+        >>> from transformers import TrOCRProcessor, VisionEncoderDecoderModel
+        >>> from transformers import VisionEncoderDecoderModel, TrOCRForCausalLM, ViTModel, TrOCRConfig, ViTConfig
+        >>> import requests
         >>> from PIL import Image
 
+        >>> processor = TrOCRProcessor.from_pretrained("microsoft/trocr-base-handwritten")
+
+        >>> # TrOCR is a decoder model and should be used within a VisionEncoderDecoderModel
         >>> encoder = ViTModel(ViTConfig())
         >>> decoder = TrOCRForCausalLM(TrOCRConfig())
-        # init vision2text model
-
         >>> model = VisionEncoderDecoderModel(encoder=encoder, decoder=decoder)
+
+        >>> # If you want to start from the pretrained model, load it from a VisionEncoderDecoderModel
+        >>> model = VisionEncoderDecoderModel.from_pretrained("microsoft/trocr-base-handwritten")
 
         >>> # load image from the IAM dataset
         >>> url = "https://fki.tic.heia-fr.ch/static/img/a01-122-02.jpg"
         >>> image = Image.open(requests.get(url, stream=True).raw).convert("RGB")
 
-        >>> # training
-        >>> model.config.decoder_start_token_id = processor.tokenizer.cls_token_id
-        >>> model.config.pad_token_id = processor.tokenizer.pad_token_id
-        >>> model.config.vocab_size = model.config.decoder.vocab_size
-
         >>> pixel_values = processor(image, return_tensors="pt").pixel_values
-        >>> text = "hello world"
-        >>> labels = processor.tokenizer(text, return_tensors="pt").input_ids
-        >>> outputs = model(pixel_values=pixel_values, labels=labels)
-        >>> loss = outputs.loss
+        >>> generated_ids = model.generate(pixel_values)
 
-        >>> model(pixel_values)
+        >>> generated_text = processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
+        >>> generated_text
+        'industry, " Mr. Brown commented icily. " Let us have a'
+
         ```"""
 
         output_attentions = output_attentions if output_attentions is not None else self.config.output_attentions
