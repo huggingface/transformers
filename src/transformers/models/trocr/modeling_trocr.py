3bcc3aa44ca61603025d4d833d28086d30ed7a9f
arnaudstiegler <arnaud.stiegler@gmail.com>
2022-03-24 17:52:28 -0400

    adding forward for training pass

--- src/transformers/models/trocr/modeling_trocr.py
+++ src/transformers/models/trocr/modeling_trocr.py
@@ -909,10 +909,19 @@ class TrOCRForCausalLM(TrOCRPreTrainedModel):
         >>> # load image from the IAM dataset
         >>> url = "https://fki.tic.heia-fr.ch/static/img/a01-122-02.jpg"
         >>> image = Image.open(requests.get(url, stream=True).raw).convert("RGB")
-
         >>> pixel_values = processor(image, return_tensors="pt").pixel_values
-        >>> generated_ids = model.generate(pixel_values)
 
+        >>> # training
+        >>> model.config.decoder_start_token_id = processor.tokenizer.cls_token_id
+        >>> model.config.pad_token_id = processor.tokenizer.pad_token_id
+        >>> model.config.vocab_size = model.config.decoder.vocab_size
+        >>> text = "hello world"
+        >>> labels = processor.tokenizer(text, return_tensors="pt").input_ids
+        >>> outputs = model(pixel_values, labels=labels)
+        >>> loss = outputs.loss
+
+        >>> # inference
+        >>> generated_ids = model.generate(pixel_values)
         >>> generated_text = processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
         >>> generated_text
         'industry, " Mr. Brown commented icily. " Let us have a'
