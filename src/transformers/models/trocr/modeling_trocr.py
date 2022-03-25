12f8e6955088ab47978eb4f9b4305a8e275b2c52
arnaudstiegler <arnaud.stiegler@gmail.com>
2022-03-25 09:44:40 -0400

    address comments

--- src/transformers/models/trocr/modeling_trocr.py
+++ src/transformers/models/trocr/modeling_trocr.py
@@ -891,31 +891,30 @@ class TrOCRForCausalLM(TrOCRPreTrainedModel):
         Example:
 
         ```python
-        >>> from transformers import TrOCRProcessor, VisionEncoderDecoderModel
-        >>> from transformers import VisionEncoderDecoderModel, TrOCRForCausalLM, ViTModel, TrOCRConfig, ViTConfig
+        >>> from transformers import TrOCRProcessor, VisionEncoderDecoderModel, TrOCRForCausalLM, ViTModel, TrOCRConfig, ViTConfig
         >>> import requests
         >>> from PIL import Image
 
-        >>> processor = TrOCRProcessor.from_pretrained("microsoft/trocr-base-handwritten")
-
         >>> # TrOCR is a decoder model and should be used within a VisionEncoderDecoderModel
+        >>> # init vision2text model with random weights
         >>> encoder = ViTModel(ViTConfig())
         >>> decoder = TrOCRForCausalLM(TrOCRConfig())
         >>> model = VisionEncoderDecoderModel(encoder=encoder, decoder=decoder)
 
         >>> # If you want to start from the pretrained model, load it from a VisionEncoderDecoderModel
+        >>> processor = TrOCRProcessor.from_pretrained("microsoft/trocr-base-handwritten")
         >>> model = VisionEncoderDecoderModel.from_pretrained("microsoft/trocr-base-handwritten")
 
         >>> # load image from the IAM dataset
         >>> url = "https://fki.tic.heia-fr.ch/static/img/a01-122-02.jpg"
         >>> image = Image.open(requests.get(url, stream=True).raw).convert("RGB")
+        >>> text = "hello world"
         >>> pixel_values = processor(image, return_tensors="pt").pixel_values
 
         >>> # training
         >>> model.config.decoder_start_token_id = processor.tokenizer.cls_token_id
         >>> model.config.pad_token_id = processor.tokenizer.pad_token_id
         >>> model.config.vocab_size = model.config.decoder.vocab_size
-        >>> text = "hello world"
         >>> labels = processor.tokenizer(text, return_tensors="pt").input_ids
         >>> outputs = model(pixel_values, labels=labels)
         >>> loss = outputs.loss
