168935203aeb4c52ed0be2f0859ee0c39e2926fd
arnaudstiegler <arnaud.stiegler@gmail.com>
2022-03-25 18:08:17 -0400

    remove additional blank

--- src/transformers/models/trocr/modeling_trocr.py
+++ src/transformers/models/trocr/modeling_trocr.py
@@ -931,7 +931,6 @@ class TrOCRForCausalLM(TrOCRPreTrainedModel):
         >>> generated_text = processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
         >>> generated_text
         'industry, " Mr. Brown commented icily. " Let us have a'
-
         ```"""
 
         output_attentions = output_attentions if output_attentions is not None else self.config.output_attentions
