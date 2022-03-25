2b9e1f189e61317489e62683ca02c64942a92909
arnaudstiegler <arnaud.stiegler@gmail.com>
2022-03-25 09:47:54 -0400

    fix doctest

--- src/transformers/models/trocr/modeling_trocr.py
+++ src/transformers/models/trocr/modeling_trocr.py
@@ -924,6 +924,7 @@ class TrOCRForCausalLM(TrOCRPreTrainedModel):
         >>> generated_text = processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
         >>> generated_text
         'industry, " Mr. Brown commented icily. " Let us have a'
+
         ```"""
 
         output_attentions = output_attentions if output_attentions is not None else self.config.output_attentions
