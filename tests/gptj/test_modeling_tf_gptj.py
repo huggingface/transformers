e90f4b31f0d274f94d246a5093b1f3880f59e1d4
ydshieh <ydshieh@users.noreply.github.com>
2022-03-28 19:15:01 +0200

    fix

--- tests/gptj/test_modeling_tf_gptj.py
+++ tests/gptj/test_modeling_tf_gptj.py
@@ -345,8 +345,10 @@ class TFGPTJModelTest(TFModelTesterMixin, TFCoreModelTesterMixin, unittest.TestC
                 assert name is None
 
     @slow
-    @unittest.skipIf(len(tf.config.list_physical_devices("GPU")) > 0, "skip testing on GPU for now to avoid GPU OOM.")
     def test_model_from_pretrained(self):
+        # skip testing on GPU for now to avoid GPU OOM.
+        if len(tf.config.list_physical_devices("GPU")) > 0:
+            return
         model = TFGPTJModel.from_pretrained("EleutherAI/gpt-j-6B", from_pt=True)
         self.assertIsNotNone(model)
 
