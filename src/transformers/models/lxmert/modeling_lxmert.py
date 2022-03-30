d7f7062d6acb351a638bb385a9f3ea93b3a527d0
Aditya Kane <64411306+AdityaKane2001@users.noreply.github.com>
2022-03-30 19:36:32 +0530

    Nit: MCSCOCO -> MS COCO (#16481)

--- src/transformers/models/lxmert/modeling_lxmert.py
+++ src/transformers/models/lxmert/modeling_lxmert.py
@@ -803,7 +803,7 @@ LXMERT_START_DOCSTRING = r"""
 
     The LXMERT model was proposed in [LXMERT: Learning Cross-Modality Encoder Representations from
     Transformers](https://arxiv.org/abs/1908.07490) by Hao Tan and Mohit Bansal. It's a vision and language transformer
-    model, pretrained on a variety of multi-modal datasets comprising of GQA, VQAv2.0, MCSCOCO captions, and Visual
+    model, pretrained on a variety of multi-modal datasets comprising of GQA, VQAv2.0, MSCOCO captions, and Visual
     genome, using a combination of masked language modeling, region of interest feature regression, cross entropy loss
     for question answering attribute prediction, and object tag prediction.
 
