9a1a6a4fa59657bf768e36d86c6954c0781a7c56
fra <francesco.zuppichini@gmail.com>
2022-03-30 14:00:43 +0200

    changed docstring in BaseModelOutput

--- src/transformers/modeling_outputs.py
+++ src/transformers/modeling_outputs.py
@@ -29,7 +29,7 @@ class BaseModelOutput(ModelOutput):
         last_hidden_state (`torch.FloatTensor` of shape `(batch_size, sequence_length, hidden_size)`):
             Sequence of hidden-states at the output of the last layer of the model.
         hidden_states (`tuple(torch.FloatTensor)`, *optional*, returned when `output_hidden_states=True` is passed or when `config.output_hidden_states=True`):
-            Tuple of `torch.FloatTensor` (one for the output of the embeddings + one for the output of each layer) of
+            Tuple of `torch.FloatTensor` (one for the output of the embeddings, if the model's has an embedding layer, + one for the output of each layer) of
             shape `(batch_size, sequence_length, hidden_size)`.
 
             Hidden-states of the model at the output of each layer plus the initial embedding outputs.
