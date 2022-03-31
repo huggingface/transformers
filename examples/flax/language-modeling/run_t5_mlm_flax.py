32d450552dd51729de1af336ab9705d9ca4df9e5
Ahmed Elnaggar <amit.najjar@gmail.com>
2022-03-31 21:08:10 +0200

    Fix t5 shard on TPU Pods
    The current script doesn't work properly on a TPU pod because the global batch is not divided correctly per host.
    This pull request fixes this issue by dividing the global batch to each host before it is shared on each host.

--- examples/flax/language-modeling/run_t5_mlm_flax.py
+++ examples/flax/language-modeling/run_t5_mlm_flax.py
@@ -702,6 +702,9 @@ def main():
     eval_batch_size = int(training_args.per_device_eval_batch_size) * jax.device_count()
 
     num_train_steps = len(tokenized_datasets["train"]) // train_batch_size * num_epochs
+    
+    num_of_hosts = jax.process_count()
+    current_host_idx = jax.process_index()
 
     # Create learning rate schedule
     warmup_fn = optax.linear_schedule(
@@ -817,9 +820,16 @@ def main():
         for step, batch_idx in enumerate(tqdm(train_batch_idx, desc="Training...", position=1)):
             samples = [tokenized_datasets["train"][int(idx)] for idx in batch_idx]
             model_inputs = data_collator(samples)
-
+            
+            local_host_model_inputs = {
+                key: np.split(model_inputs.data[key], num_of_hosts, axis=0)[
+                    current_host_idx
+                ]
+                for key, value in model_inputs.data.items()
+            }
+            
             # Model forward
-            model_inputs = shard(model_inputs.data)
+            model_inputs = shard(local_host_model_inputs)
             state, train_metric, dropout_rngs = p_train_step(state, model_inputs, dropout_rngs)
             train_metrics.append(train_metric)
 
