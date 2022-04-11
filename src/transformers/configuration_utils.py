12d2aceb8227449fab3ac06df567e8f891520d48
Sylvain Gugger <Sylvain.gugger@gmail.com>
2022-04-11 13:54:19 -0400

    Add defensive check for config num_labels and id2label

--- src/transformers/configuration_utils.py
+++ src/transformers/configuration_utils.py
@@ -304,7 +304,12 @@ class PretrainedConfig(PushToHubMixin):
         self.id2label = kwargs.pop("id2label", None)
         self.label2id = kwargs.pop("label2id", None)
         if self.id2label is not None:
-            kwargs.pop("num_labels", None)
+            num_labels = kwargs.pop("num_labels", None)
+            if num_labels is not None:
+                raise ValueError(
+                    f"You passed along `num_labels={num_labels}` with an incompatible id to label map: "
+                    f"{self.id2label}."
+                )
             self.id2label = dict((int(key), value) for key, value in self.id2label.items())
             # Keys are always strings in JSON so convert ids to int here.
         else:
@@ -678,6 +683,11 @@ class PretrainedConfig(PushToHubMixin):
             config.pruned_heads = dict((int(key), value) for key, value in config.pruned_heads.items())
 
         # Update config with kwargs if needed
+        if "num_labels" in kwargs and "id2label" in kwargs and len(kwargs["id2label"]) != kwargs["num_labels"]:
+            raise ValueError(
+                f"You passed along `num_labels={kwargs['num_labels']}` with an incompatible id to label map: "
+                f"{kwargs['id2label']}."
+            )
         to_remove = []
         for key, value in kwargs.items():
             if hasattr(config, key):
