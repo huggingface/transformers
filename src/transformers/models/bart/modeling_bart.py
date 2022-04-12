1e4ca5f0b2bdc2246522091bd0b3b6c4429d136f
Patrick von Platen <patrick.v.platen@gmail.com>
2022-04-12 10:19:49 +0200

    [Bart] correct doc test (#16722)

--- src/transformers/models/bart/modeling_bart.py
+++ src/transformers/models/bart/modeling_bart.py
@@ -563,7 +563,7 @@ BART_GENERATION_EXAMPLE = r"""
     >>> inputs = tokenizer([ARTICLE_TO_SUMMARIZE], max_length=1024, return_tensors="pt")
 
     >>> # Generate Summary
-    >>> summary_ids = model.generate(inputs["input_ids"], num_beams=2, max_length=20)
+    >>> summary_ids = model.generate(inputs["input_ids"], num_beams=2, min_length=0, max_length=20)
     >>> tokenizer.batch_decode(summary_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0]
     'PG&E scheduled the blackouts in response to forecasts for high winds amid dry conditions'
     ```
