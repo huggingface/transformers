d9cf563b44845a59f9d1e6546d741877014e6fbd
Anmol Joshi <anmolsjoshi@gmail.com>
2022-04-12 07:59:30 -0700

    Update src/transformers/models/ctrl/modeling_tf_ctrl.py
    Co-authored-by: Sylvain Gugger <35901082+sgugger@users.noreply.github.com>

--- src/transformers/models/ctrl/modeling_tf_ctrl.py
+++ src/transformers/models/ctrl/modeling_tf_ctrl.py
@@ -816,7 +816,7 @@ class TFCTRLForSequenceClassification(TFCTRLPreTrainedModel, TFSequenceClassific
                 batch_size, sequence_length = shape_list(input_ids)[:2]
             else:
                 batch_size, sequence_length = shape_list(inputs_embeds)[:2]
-            if self.config.pad_token_id is None or batch_size != 1:
+            if self.config.pad_token_id is None and batch_size != 1:
                 raise ValueError("Cannot handle batch sizes > 1 if no padding token is defined.")
 
             if not tf.is_tensor(sequence_lengths):
