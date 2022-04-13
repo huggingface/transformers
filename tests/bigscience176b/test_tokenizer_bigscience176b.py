919839d4a71c920a48016d9af2f53a67924e02de
younesbelkada <younesbelkada@gmail.com>
2022-04-13 15:16:22 +0200

    add descriptions + clearer code

--- tests/bigscience176b/test_tokenizer_bigscience176b.py
+++ tests/bigscience176b/test_tokenizer_bigscience176b.py
@@ -34,18 +34,18 @@ class BigScienceTokenizationTest(unittest.TestCase):
         self.path_tokenizer = "bigscience-catalogue-data-dev/byte-level-bpe-tokenizer-no-norm-250k-whitespace-and-eos-regex-alpha-v3-dedup-lines-articles"
         self.path_bin_data = "/home/thomwolf/bigscience/megatron-debug/preprocessed_dataset_text_document"
         self.path_json_dataset = "/home/thomwolf/bigscience/megatron-debug/train_dataset.jsonl"
-        self.path_new_tokenizer = "/home/younes/Desktop/Work/data/megatron-debug/bigscience176b-tokenizer"
+        self.path_local_tokenizer = "/home/younes/Desktop/Work/data/megatron-debug/bigscience176b-tokenizer"
         self.NB_SENTENCES = 2
     
     def file_exists(self):
         return os.path.isfile(self.path_bin_data+'.idx') and os.path.isfile(self.path_bin_data+'.bin') and os.path.isfile(self.path_json_dataset)
     
     def local_tokenizer_exists(self):
-        return os.path.isfile(os.path.join(self.path_new_tokenizer, 'tokenizer.json'))
+        return os.path.isfile(os.path.join(self.path_local_tokenizer, 'tokenizer.json'))
 
     def test_load_tokenizer(self):
         """
-            Assert that we can correctly load the tokenizer 
+            Assert that we can correctly load the tokenizer that is available on the Hub
         """
         try:
             _ = AutoTokenizer.from_pretrained(self.path_tokenizer)
@@ -53,19 +53,19 @@ class BigScienceTokenizationTest(unittest.TestCase):
             self.fail("Failed loading tokenizer")
     
     @unittest.skipUnless(local_tokenizer_exists, "requires tokenizer stored in the local machine!")
-    def test_load_new_tokenizer(self):
+    def test_load_local_tokenizer(self):
         """
-            Try the tokenizer from: https://huggingface.co/bigscience/tokenizer/ that I have copied in my local directory
+            Try to load the tokenizer from: https://huggingface.co/bigscience/tokenizer/ that I have copied in my local directory
         """
         try:
-            _ = AutoTokenizer.from_pretrained(self.path_new_tokenizer)
+            _ = AutoTokenizer.from_pretrained(self.path_local_tokenizer)
         except:
             self.fail("Failed loading tokenizer")
 
     @unittest.skipUnless(file_exists, "requires data stored in the local machine!")
     def test_load_custom_text_and_bin_data(self):
         """
-            Assert that we can correctly load the custom data (json and bin)
+            Assert that we can correctly load the bin and json data (Thomas Wolf's data)
         """
         with open(self.path_json_dataset) as f:
             json_data = [json.loads(line) for line in f]
@@ -81,7 +81,7 @@ class BigScienceTokenizationTest(unittest.TestCase):
     @unittest.skipUnless(file_exists, "requires data stored in the local machine!")
     def test_encodings_from_bin_data(self):
         """
-            Assert that the created tokens are the same than the one created statically
+            Assert that the created tokens are the same than the one created statically on the test data (Thomas Wolf's data)
         """
         tokenizer = AutoTokenizer.from_pretrained(self.path_tokenizer)
 
@@ -99,22 +99,31 @@ class BigScienceTokenizationTest(unittest.TestCase):
 
         
     @unittest.skipUnless(file_exists and local_tokenizer_exists, "requires data stored in the local machine!")
-    def test_new_tokenizer_with_bin_data(self):
-        new_tokenizer = AutoTokenizer.from_pretrained(self.path_new_tokenizer)
+    def test_local_tokenizer_with_bin_data(self):
+        """
+            Tests the tokenizer available on the hub:
+                - bigscience-catalogue-data-dev/byte-level-bpe-tokenizer-no-norm-250k-whitespace-and-eos-regex-alpha-v3-dedup-lines-articles
+            On the bin data (local test data from Thomas Wolf)
+        """
+        local_tokenizer = AutoTokenizer.from_pretrained(self.path_local_tokenizer)
 
         with open(self.path_json_dataset) as f:
-            input_text = [json.loads(line)['text']+new_tokenizer.eos_token for line in f][:self.NB_SENTENCES]
+            input_text = [json.loads(line)['text']+local_tokenizer.eos_token for line in f][:self.NB_SENTENCES]
         
         mmapdataset = MMapIndexedDataset(self.path_bin_data)
 
-        computed_tokens_with_new_tokenizer = list(map(new_tokenizer.encode, input_text))
-        _ = list(map(np.testing.assert_equal, mmapdataset[:self.NB_SENTENCES], computed_tokens_with_new_tokenizer))
+        computed_tokens_with_local_tokenizer = list(map(local_tokenizer.encode, input_text))
+        _ = list(map(np.testing.assert_equal, mmapdataset[:self.NB_SENTENCES], computed_tokens_with_local_tokenizer))
 
-        decoded_tokens_with_new_tokenizer = list(map(new_tokenizer.decode, mmapdataset[:self.NB_SENTENCES]))
-        self.assertListEqual(decoded_tokens_with_new_tokenizer, input_text)
+        decoded_tokens_with_local_tokenizer = list(map(local_tokenizer.decode, mmapdataset[:self.NB_SENTENCES]))
+        self.assertListEqual(decoded_tokens_with_local_tokenizer, input_text)
 
     def test_encodings_from_xnli_dataset(self):
-        # TODO: get a multilingual dataset and test the tokenizers
+        """
+            Tests the tokenizer downloaded from here:
+                - https://huggingface.co/bigscience/tokenizer/ 
+            On the bin data (local test data from Thomas Wolf)
+        """
         tokenizer = AutoTokenizer.from_pretrained(self.path_tokenizer)
         ds = load_dataset("xnli", "all_languages", split="test", streaming=True)
         
@@ -128,7 +137,12 @@ class BigScienceTokenizationTest(unittest.TestCase):
     
     @unittest.skipUnless(local_tokenizer_exists, "requires tokenizer stored in the local machine!")
     def test_encodings_from_xnli_dataset_with_local_tokenizer(self):
-        tokenizer = AutoTokenizer.from_pretrained(self.path_new_tokenizer)
+        """
+            Tests the tokenizer downloaded from here:
+                - https://huggingface.co/bigscience/tokenizer/ 
+            On one sample of xnli dataset, that contains sentences in various languages
+        """
+        tokenizer = AutoTokenizer.from_pretrained(self.path_local_tokenizer)
         ds = load_dataset("xnli", "all_languages", split="test", streaming=True)
         
         sample_data = next(iter(ds))['premise'] # pick up one data
@@ -141,22 +155,4 @@ class BigScienceTokenizationTest(unittest.TestCase):
 
 
 if __name__ == '__main__':
-    # TO DO: Merge argparse with unittest
-    # parser = argparse.ArgumentParser()
-    # Required parameters
-    # parser.add_argument(
-    #     "--path_bin_file", default="/home/thomwolf/bigscience/megatron-debug/preprocessed_dataset_text_document.bin", type=str, required=True, help="Path to the created bin file."
-    # )
-    # parser.add_argument(
-    #     "--path_json_dataset", default="/home/thomwolf/bigscience/megatron-debug/train_dataset.jsonl", type=str, required=True, help="Path to the json file dataset (json file that contains the dataset)."
-    # )
-    # parser.add_argument(
-    #     "--path_tokenizer",
-    #     default="_",
-    #     type=str,
-    #     required=True,
-    #     help="Name of the tokenizer that has been used \n"
-    #     "This specifies the model architecture.",
-    # )
-    # args = parser.parse_args()
     unittest.main()
