bb8247f0689f0aafed1d9f7690bc98310b3c2a86
allan <allan@bytedance.com>
2022-04-16 01:00:27 +0800

    reformat using make style

--- src/transformers/trainer.py
+++ src/transformers/trainer.py
@@ -397,7 +397,9 @@ class Trainer:
                 "Passing a `model_init` is incompatible with providing the `optimizers` argument. "
                 "You should subclass `Trainer` and override the `create_optimizer_and_scheduler` method."
             )
-        if (self.sharded_ddp is not None or args.deepspeed) and (self.optimizer is not None or self.lr_scheduler is not None):
+        if (self.sharded_ddp is not None or args.deepspeed) and (
+            self.optimizer is not None or self.lr_scheduler is not None
+        ):
             raise RuntimeError(
                 "Passing `optimizers` is not allowed if Fairscale or Deepspeed is enabled."
                 "You should subclass `Trainer` and override the `create_optimizer_and_scheduler` method."
