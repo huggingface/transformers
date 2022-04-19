b97e82bbfa9e6c91afd501101fe31a1949928316
haiming wang <765874234@qq.com>
2022-04-19 20:31:47 +0800

    clean up style

--- src/transformers/models/gpt2/modeling_tf_gpt2.py
+++ src/transformers/models/gpt2/modeling_tf_gpt2.py
@@ -655,8 +655,8 @@ GPT2_INPUTS_DOCSTRING = r"""
             - 1 for tokens that are **not masked**,
             - 0 for tokens that are **masked**.
             
-            If `past_key_values` is used, `attention_mask` needs to contain the masking strategy that was used for 
-            `past_key_values`. In other words, the attention_mask always has to have the length: 
+            If `past_key_values` is used, `attention_mask` needs to contain the masking strategy that was used for
+            `past_key_values`. In other words, the `attention_mask` always has to have the length:
             `len(past_key_values) + len(input_ids)`
 
             [What are attention masks?](../glossary#attention-mask)
