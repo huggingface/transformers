7e6a4a1fbeabdda953755489bafa4dbb9ea5211e
Nicolas Patry <patry.nicolas@protonmail.com>
2022-04-19 11:20:34 +0200

    Adding support for `array` key in raw dictionnaries in ASR pipeline.

--- src/transformers/pipelines/automatic_speech_recognition.py
+++ src/transformers/pipelines/automatic_speech_recognition.py
@@ -193,7 +193,14 @@ class AutomaticSpeechRecognitionPipeline(ChunkPipeline):
         extra = {}
         if isinstance(inputs, dict):
             stride = inputs.pop("stride", None)
-            _inputs = inputs.pop("raw")
+            # Accepting `"array"` which is the key defined in `datasets` for
+            # better integration
+            if not ("sampling_rate" in inputs and ("raw" in inputs or "array" in inputs)):
+                raise ValueError(
+                    'When passing a dictionnary to AutomaticSpeechRecognitionPipeline, the dict needs to contain a "raw" key containing the numpy array representing the audio and a "sampling_rate" key, containing the sampling_rate associated with that array'
+                )
+
+            _inputs = inputs.pop("raw", inputs.pop("array"))
             in_sampling_rate = inputs.pop("sampling_rate")
             extra = inputs
             inputs = _inputs
