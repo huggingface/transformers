edd1830813fcc1d2610d310ab718a8fb23b28f6c
Leon Derczynski <leonderczynski@gmail.com>
2022-04-22 19:00:35 +0200

    remove unused imports, let make style do its.. thing

--- tests/mobilebert/test_tokenization_mobilebert.py
+++ tests/mobilebert/test_tokenization_mobilebert.py
@@ -15,19 +15,20 @@
 """ Testing suite for the MobileBERT tokenizer. """
 
 
-
 import filecmp
-import requests
 import unittest
-
 from tempfile import NamedTemporaryFile
+
+import requests
 from transformers import MobileBertTokenizer, MobileBertTokenizerFast
-from transformers.testing_utils import require_tokenizers, slow
-from ..test_tokenization_common import TokenizerTesterMixin
+from transformers.testing_utils import require_tokenizers
+
 from ..bert.test_tokenization_bert import BertTokenizationTest
+
 
 # disable duplicate incorporation of tests from parent class in this module
 BertTokenizationTest.__test__ = False
+
 
 @require_tokenizers
 class MobileBertTokenizationTest(BertTokenizationTest, unittest.TestCase):
@@ -43,39 +44,27 @@ class MobileBertTokenizationTest(BertTokenizationTest, unittest.TestCase):
 
     pre_trained_model_path = "google/mobilebert-uncased"
 
-    # TODO: Check in `TokenizerTesterMixin` if other attributes need to be changed
-
     def setUp(self):
         super().setUp()
         self.tokenizers_list = [
-            (
-                tokenizer_def[0], 
-                self.pre_trained_model_path,  # else the 'google/' prefix is stripped
-                tokenizer_def[2]
-            ) 
+            (tokenizer_def[0], self.pre_trained_model_path, tokenizer_def[2])  # else the 'google/' prefix is stripped
             for tokenizer_def in self.tokenizers_list
         ]
 
     def test_mobilebert_tokenizer_uses_bert_params(self):
-        with \
-            NamedTemporaryFile(buffering=0) as bert_vocab_file, \
-            NamedTemporaryFile(buffering=0) as bert_merge_file, \
-            NamedTemporaryFile(buffering=0) as mobilebert_vocab_file, \
-            NamedTemporaryFile(buffering=0) as mobilebert_merge_file: # buffering=0 is shorter than four flush()es before cmp()
-            bert_merge_file.write(
-                requests.get("https://huggingface.co/bert-base-uncased/raw/main/merges.txt").content
-                )
+        with NamedTemporaryFile(buffering=0) as bert_vocab_file, NamedTemporaryFile(
+            buffering=0
+        ) as bert_merge_file, NamedTemporaryFile(buffering=0) as mobilebert_vocab_file, NamedTemporaryFile(
+            buffering=0
+        ) as mobilebert_merge_file:  # buffering=0 is shorter than four flush()es before cmp()
+            bert_merge_file.write(requests.get("https://huggingface.co/bert-base-uncased/raw/main/merges.txt").content)
             mobilebert_merge_file.write(
                 requests.get("https://huggingface.co/google/mobilebert-uncased/raw/main/merges.txt").content
-                )
+            )
             self.assertTrue(filecmp.cmp(bert_merge_file.name, mobilebert_merge_file.name), "Merge files don't match")
-            
-            bert_vocab_file.write(
-                requests.get("https://huggingface.co/bert-base-uncased/raw/main/vocab.json").content
-                )
+
+            bert_vocab_file.write(requests.get("https://huggingface.co/bert-base-uncased/raw/main/vocab.json").content)
             mobilebert_vocab_file.write(
                 requests.get("https://huggingface.co/google/mobilebert-uncased/raw/main/vocab.json").content
-                )
+            )
             self.assertTrue(filecmp.cmp(bert_vocab_file.name, mobilebert_vocab_file.name), "Vocab files don't match")
-
-
