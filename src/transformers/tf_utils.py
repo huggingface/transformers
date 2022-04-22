f8870d2162250f020cab30c3b3ab0c9254001d7b
Joao Gante <joaofranciscocardosogante@gmail.com>
2022-04-22 16:37:53 +0100

    Apply suggestions from code review
    Co-authored-by: Sylvain Gugger <35901082+sgugger@users.noreply.github.com>

--- src/transformers/tf_utils.py
+++ src/transformers/tf_utils.py
@@ -49,15 +49,15 @@ def shape_list(tensor: Union[tf.Tensor, np.ndarray]) -> List[int]:
 def stable_softmax(logits: tf.Tensor, axis: Optional[int] = None, name: Optional[str] = None) -> tf.Tensor:
     """
     Stable wrapper that returns the same output as `tf.nn.softmax`, but that works reliably with XLA on CPU. It is
-    meant as a workaround for the following issue (https://github.com/tensorflow/tensorflow/issues/55682), and will be
+    meant as a workaround for the [following issue](https://github.com/tensorflow/tensorflow/issues/55682), and will be
     removed after it gets fixed. The arguments and outputs are the same as `tf.nn.softmax`, and relies on the fact that
-    softmax(x) = softmax(x + c) (see https://ogunlao.github.io/2020/04/26/you_dont_really_know_softmax.html).
+    `softmax(x) = softmax(x + c)` (see https://ogunlao.github.io/2020/04/26/you_dont_really_know_softmax.html).
 
     Args:
         logits (`tf.Tensor`). Must be one of the following types: half, float32, float64.
         axis (`int`, *optional*). The dimension softmax would be performed on. The default is -1 which indicates the
             last dimension.
-        name (`str`, *optional*). A name for the operation (optional).
+        name (`str`, *optional*). A name for the operation.
 
     Returns:
         `tf.Tensor`: A Tensor. Has the same type and shape as logits.
