72728be3dbca26c70dddc8b724eb2c8d901e97dc
Patrick von Platen <patrick.v.platen@gmail.com>
2022-04-23 08:40:14 +0200

    [DocTests] Fix some doc tests (#16889)
    * [DocTests] Fix some doc tests
    
    * hacky fix
    
    * correct

--- src/transformers/models/beit/modeling_beit.py
+++ src/transformers/models/beit/modeling_beit.py
@@ -1210,14 +1210,14 @@ class BeitForSemanticSegmentation(BeitPreTrainedModel):
         Examples:
 
         ```python
-        >>> from transformers import BeitFeatureExtractor, BeitForSemanticSegmentation
+        >>> from transformers import AutoFeatureExtractor, BeitForSemanticSegmentation
         >>> from PIL import Image
         >>> import requests
 
         >>> url = "http://images.cocodataset.org/val2017/000000039769.jpg"
         >>> image = Image.open(requests.get(url, stream=True).raw)
 
-        >>> feature_extractor = BeitFeatureExtractor.from_pretrained("microsoft/beit-base-finetuned-ade-640-640")
+        >>> feature_extractor = AutoFeatureExtractor.from_pretrained("microsoft/beit-base-finetuned-ade-640-640")
         >>> model = BeitForSemanticSegmentation.from_pretrained("microsoft/beit-base-finetuned-ade-640-640")
 
         >>> inputs = feature_extractor(images=image, return_tensors="pt")
