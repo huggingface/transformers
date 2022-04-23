1eca8756e1b2ab50cf73273244427fb24469f3de
Arijit <arijitmukh007@gmail.com>
2022-04-23 19:00:57 +0530

    fix bash script

--- examples/research_projects/wav2vec2/run_alignment.sh
+++ examples/research_projects/wav2vec2/run_alignment.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 python alignment.py  \
 --model_name="arijitx/wav2vec2-xls-r-300m-bengali" \
---wav_dir="./wavs"
+--wav_dir="./wavs" \
 --text_file="script.txt" \
 --input_wavs_sr=48000 \
 --output_dir="./out_alignment" \
