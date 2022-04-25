37c6759ecebe4ba28a6ec3259d270e487afb22d4
Rushi Chaudhari <rushic24@gmail.com>
2022-04-25 14:50:45 -0400

    added deit onnx config (#16887)
    * added deit onnx config

--- tests/onnx/test_onnx_v2.py
+++ tests/onnx/test_onnx_v2.py
@@ -182,6 +182,7 @@ PYTORCH_EXPORT_MODELS = {
     ("xlm-roberta", "xlm-roberta-base"),
     ("layoutlm", "microsoft/layoutlm-base-uncased"),
     ("vit", "google/vit-base-patch16-224"),
+    ("deit", "facebook/deit-small-patch16-224"),
     ("beit", "microsoft/beit-base-patch16-224"),
     ("data2vec-text", "facebook/data2vec-text-base"),
 }
