fdcc3b8ed73b31d014d1daa6155460b386326e22
jianan-gu <jianan.gu@intel.com>
2022-04-25 09:47:07 +0800

    fix import ipex

--- src/transformers/utils/__init__.py
+++ src/transformers/utils/__init__.py
@@ -85,6 +85,7 @@ from .import_utils import (
     DummyObject,
     _LazyModule,
     is_apex_available,
+    is_ipex_available,
     is_bitsandbytes_available,
     is_coloredlogs_available,
     is_datasets_available,
