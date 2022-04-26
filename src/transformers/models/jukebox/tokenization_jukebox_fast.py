f296b11553e56df5162e0e99d1bfbedb80ce36a7
ArthurZucker <arthur.zucker@gmail.com>
2022-04-26 11:24:34 +0200

    add unchanged template jukebox_fast tokenizer

--- src/transformers/models/jukebox/tokenization_jukebox_fast.py
+++ src/transformers/models/jukebox/tokenization_jukebox_fast.py
@@ -1,5 +1,5 @@
 # coding=utf-8
-# Copyright 2022 The HuggingFace Team and The HuggingFace Inc. team. All rights reserved.
+# Copyright 2018 The Open AI Team Authors and The HuggingFace Inc. team.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,102 +12,177 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Tokenization classes for Jukebox."""
-from typing import List, Optional
+"""Tokenization classes for OpenAI GPT."""
 
-from tokenizers import ByteLevelBPETokenizer
 
+import json
+from typing import TYPE_CHECKING, List, Optional, Tuple
+
+from tokenizers import pre_tokenizers
+
+from ...tokenization_utils_base import BatchEncoding
 from ...tokenization_utils_fast import PreTrainedTokenizerFast
 from ...utils import logging
 from .tokenization_jukebox import JukeboxTokenizer
 
 
+if TYPE_CHECKING:
+    from transformers.pipelines.conversational import Conversation
+
+
 logger = logging.get_logger(__name__)
 
-VOCAB_FILES_NAMES = {"vocab_file": "vocab.txt", "tokenizer_file": "tokenizer.json"}
+VOCAB_FILES_NAMES = {"vocab_file": "vocab.json", "merges_file": "merges.txt", "tokenizer_file": "tokenizer.json"}
 
 PRETRAINED_VOCAB_FILES_MAP = {
     "vocab_file": {
-        "huggingface/jukebox-base-cased": "https://huggingface.co/huggingface/jukebox-base-cased/resolve/main/vocab.txt",
+        "jukebox": "https://huggingface.co/jukebox/resolve/main/vocab.json",
+        "jukebox-medium": "https://huggingface.co/jukebox-medium/resolve/main/vocab.json",
+        "jukebox-large": "https://huggingface.co/jukebox-large/resolve/main/vocab.json",
+        "jukebox-xl": "https://huggingface.co/jukebox-xl/resolve/main/vocab.json",
+        "distiljukebox": "https://huggingface.co/distiljukebox/resolve/main/vocab.json",
+    },
+    "merges_file": {
+        "jukebox": "https://huggingface.co/jukebox/resolve/main/merges.txt",
+        "jukebox-medium": "https://huggingface.co/jukebox-medium/resolve/main/merges.txt",
+        "jukebox-large": "https://huggingface.co/jukebox-large/resolve/main/merges.txt",
+        "jukebox-xl": "https://huggingface.co/jukebox-xl/resolve/main/merges.txt",
+        "distiljukebox": "https://huggingface.co/distiljukebox/resolve/main/merges.txt",
     },
     "tokenizer_file": {
-        "huggingface/jukebox-base-cased": "https://huggingface.co/huggingface/jukebox-base-cased/resolve/main/tokenizer.json",
+        "jukebox": "https://huggingface.co/jukebox/resolve/main/tokenizer.json",
+        "jukebox-medium": "https://huggingface.co/jukebox-medium/resolve/main/tokenizer.json",
+        "jukebox-large": "https://huggingface.co/jukebox-large/resolve/main/tokenizer.json",
+        "jukebox-xl": "https://huggingface.co/jukebox-xl/resolve/main/tokenizer.json",
+        "distiljukebox": "https://huggingface.co/distiljukebox/resolve/main/tokenizer.json",
     },
 }
 
 PRETRAINED_POSITIONAL_EMBEDDINGS_SIZES = {
-    "huggingface/jukebox-base-cased": 1024,
+    "jukebox": 1024,
+    "jukebox-medium": 1024,
+    "jukebox-large": 1024,
+    "jukebox-xl": 1024,
+    "distiljukebox": 1024,
 }
+
 
 class JukeboxTokenizerFast(PreTrainedTokenizerFast):
     """
-    Construct a "fast" Jukebox tokenizer (backed by HuggingFace's *tokenizers* library).
+    Construct a "fast" GPT-2 tokenizer (backed by HuggingFace's *tokenizers* library). Based on byte-level
+    Byte-Pair-Encoding.
+
+    This tokenizer has been trained to treat spaces like parts of the tokens (a bit like sentencepiece) so a word will
+    be encoded differently whether it is at the beginning of the sentence (without space) or not:
+
+    ```
+    >>> from transformers import JukeboxTokenizerFast
+    >>> tokenizer = JukeboxTokenizerFast.from_pretrained("jukebox")
+    >>> tokenizer("Hello world")['input_ids']
+    [15496, 995]
+    >>> tokenizer(" Hello world")['input_ids']
+    [18435, 995]
+    ```
+
+    You can get around that behavior by passing `add_prefix_space=True` when instantiating this tokenizer, but since
+    the model was not pretrained this way, it might yield a decrease in performance.
+
+    <Tip>
+
+    When used with `is_split_into_words=True`, this tokenizer needs to be instantiated with `add_prefix_space=True`.
+
+    </Tip>
+
+    This tokenizer inherits from [`PreTrainedTokenizerFast`] which contains most of the main methods. Users should
+    refer to this superclass for more information regarding those methods.
 
     Args:
         vocab_file (`str`):
             Path to the vocabulary file.
+        merges_file (`str`):
+            Path to the merges file.
+        errors (`str`, *optional*, defaults to `"replace"`):
+            Paradigm to follow when decoding bytes to UTF-8. See
+            [bytes.decode](https://docs.python.org/3/library/stdtypes.html#bytes.decode) for more information.
+        unk_token (`str`, *optional*, defaults to `<|endoftext|>`):
+            The unknown token. A token that is not in the vocabulary cannot be converted to an ID and is set to be this
+            token instead.
+        bos_token (`str`, *optional*, defaults to `<|endoftext|>`):
+            The beginning of sequence token.
+        eos_token (`str`, *optional*, defaults to `<|endoftext|>`):
+            The end of sequence token.
+        add_prefix_space (`bool`, *optional*, defaults to `False`):
+            Whether or not to add an initial space to the input. This allows to treat the leading word just as any
+            other word. (Jukebox tokenizer detect beginning of words by the preceding space).
+        trim_offsets (`bool`, *optional*, defaults to `True`):
+            Whether or not the post-processing step should trim offsets to avoid including whitespaces.
     """
 
     vocab_files_names = VOCAB_FILES_NAMES
     pretrained_vocab_files_map = PRETRAINED_VOCAB_FILES_MAP
     max_model_input_sizes = PRETRAINED_POSITIONAL_EMBEDDINGS_SIZES
+    model_input_names = ["input_ids", "attention_mask"]
     slow_tokenizer_class = JukeboxTokenizer
 
     def __init__(
-            self,
-            vocab_file,
-            merges_file,
-            unk_token="<|endoftext|>",
-            bos_token="<|endoftext|>",
-            eos_token="<|endoftext|>",
-            add_prefix_space=False,
-            trim_offsets=True,
-            **kwargs
+        self,
+        vocab_file=None,
+        merges_file=None,
+        tokenizer_file=None,
+        unk_token="<|endoftext|>",
+        bos_token="<|endoftext|>",
+        eos_token="<|endoftext|>",
+        add_prefix_space=False,
+        **kwargs
     ):
         super().__init__(
-            ByteLevelBPETokenizer(
-                vocab_file=vocab_file,
-                merges_file=merges_file,
-                add_prefix_space=add_prefix_space,
-                trim_offsets=trim_offsets,
-            ),
+            vocab_file,
+            merges_file,
+            tokenizer_file=tokenizer_file,
+            unk_token=unk_token,
             bos_token=bos_token,
             eos_token=eos_token,
-            unk_token=unk_token,
+            add_prefix_space=add_prefix_space,
             **kwargs,
         )
+
+        pre_tok_state = json.loads(self.backend_tokenizer.pre_tokenizer.__getstate__())
+        if pre_tok_state.get("add_prefix_space", add_prefix_space) != add_prefix_space:
+            pre_tok_class = getattr(pre_tokenizers, pre_tok_state.pop("type"))
+            pre_tok_state["add_prefix_space"] = add_prefix_space
+            self.backend_tokenizer.pre_tokenizer = pre_tok_class(**pre_tok_state)
+
         self.add_prefix_space = add_prefix_space
 
-    def build_inputs_with_special_tokens(self, token_ids_0, token_ids_1=None):
-        output = [self.bos_token_id] + token_ids_0 + [self.eos_token_id]
-        if token_ids_1 is None:
-            return output
+    def _batch_encode_plus(self, *args, **kwargs) -> BatchEncoding:
+        is_split_into_words = kwargs.get("is_split_into_words", False)
+        assert self.add_prefix_space or not is_split_into_words, (
+            f"You need to instantiate {self.__class__.__name__} with add_prefix_space=True "
+            "to use it with pretokenized inputs."
+        )
 
-        return output + [self.eos_token_id] + token_ids_1 + [self.eos_token_id]
+        return super()._batch_encode_plus(*args, **kwargs)
 
+    def _encode_plus(self, *args, **kwargs) -> BatchEncoding:
+        is_split_into_words = kwargs.get("is_split_into_words", False)
 
-    def create_token_type_ids_from_sequences(
-            self, token_ids_0: List[int], token_ids_1: Optional[List[int]] = None
-    ) -> List[int]:
-        """
-        Create a mask from the two sequences passed to be used in a sequence-pair classification task.
-        Jukebox does not make use of token type ids, therefore a list of zeros is returned.
+        assert self.add_prefix_space or not is_split_into_words, (
+            f"You need to instantiate {self.__class__.__name__} with add_prefix_space=True "
+            "to use it with pretokenized inputs."
+        )
 
-        Args:
-            token_ids_0 (`List[int]`):
-                List of IDs.
-            token_ids_1 (`List[int]`, *optional*):
-                Optional second list of IDs for sequence pairs.
+        return super()._encode_plus(*args, **kwargs)
 
-        Returns:
-            `List[int]`:  List of zeros.
-        """
-        sep = [self.sep_token_id]
-        cls = [self.cls_token_id]
+    def save_vocabulary(self, save_directory: str, filename_prefix: Optional[str] = None) -> Tuple[str]:
+        files = self._tokenizer.model.save(save_directory, name=filename_prefix)
+        return tuple(files)
 
-        if token_ids_1 is None:
-            return len(cls + token_ids_0 + sep) * [0]
-        return len(cls + token_ids_0 + sep + sep + token_ids_1 + sep) * [0]
+    def _build_conversation_input_ids(self, conversation: "Conversation") -> List[int]:
+        """This corresponds to DialoGPT variants of models."""
+        input_ids = []
+        for is_user, text in conversation.iter_texts():
+            input_ids.extend(self.encode(text, add_special_tokens=False) + [self.eos_token_id])
 
-
-
+        if len(input_ids) > self.model_max_length:
+            input_ids = input_ids[-self.model_max_length :]
+        return input_ids
