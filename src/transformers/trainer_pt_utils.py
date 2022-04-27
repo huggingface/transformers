1c4eeadf36c2c0db3f95f9586791ca5895d07a87
Antoni Baum <antoni.baum@protonmail.com>
2022-04-27 16:04:30 +0200

    Update trainer_pt_utils.py

--- src/transformers/trainer_pt_utils.py
+++ src/transformers/trainer_pt_utils.py
@@ -159,10 +159,9 @@ def distributed_concat(tensor: Any, num_total_examples: Optional[int] = None) ->
     try:
         if isinstance(tensor, (tuple, list)):
             return type(tensor)(distributed_concat(t, num_total_examples) for t in tensor)
-        output_tensors = [tensor.clone() for _ in range(dist.get_world_size())]
-        output_tensors = [t if len(t.shape) > 0 else t[None] for t in output_tensors]
         if len(tensor.shape) <= 0:
             tensor = tensor[None]
+        output_tensors = [tensor.clone() for _ in range(dist.get_world_size())]
         dist.all_gather(output_tensors, tensor)
         concat = torch.cat(output_tensors, dim=0)
 
