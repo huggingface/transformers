26470f83a3bc7dca5b7d08b3c2f396765a09da23
NielsRogge <48327001+NielsRogge@users.noreply.github.com>
2022-04-27 15:00:45 +0200

    [HF Argparser] Fix parsing of optional boolean arguments (#16946)
    * Add fix
    
    * Apply suggestion from code review
    
    Co-authored-by: Niels Rogge <nielsrogge@Nielss-MacBook-Pro.local>

--- src/transformers/hf_argparser.py
+++ src/transformers/hf_argparser.py
@@ -103,7 +103,7 @@ class HfArgumentParser(ArgumentParser):
                 kwargs["default"] = field.default
             else:
                 kwargs["required"] = True
-        elif field.type is bool or field.type is Optional[bool]:
+        elif field.type is bool or field.type == Optional[bool]:
             # Copy the currect kwargs to use to instantiate a `no_*` complement argument below.
             # We do not initialize it here because the `no_*` alternative must be instantiated after the real argument
             bool_kwargs = copy(kwargs)
@@ -140,7 +140,7 @@ class HfArgumentParser(ArgumentParser):
         # Order is important for arguments with the same destination!
         # We use a copy of earlier kwargs because the original kwargs have changed a lot before reaching down
         # here and we do not need those changes/additional keys.
-        if field.default is True and (field.type is bool or field.type is Optional[bool]):
+        if field.default is True and (field.type is bool or field.type == Optional[bool]):
             bool_kwargs["default"] = False
             parser.add_argument(f"--no_{field.name}", action="store_false", dest=field.name, **bool_kwargs)
 
