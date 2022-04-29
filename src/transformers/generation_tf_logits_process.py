fc9972c66df27a481680d32e92ff0afe194aa65a
Joao Gante <joao@huggingface.co>
2022-04-29 10:19:29 +0000

    typo

--- src/transformers/generation_tf_logits_process.py
+++ src/transformers/generation_tf_logits_process.py
@@ -330,7 +330,7 @@ class TFNoBadWordsLogitsProcessor(TFLogitsProcessor):
                 )
 
             def _len_greater_then_cur_len():
-                # Otherwise, if the bad sequence are longer than the current length they can't ever match
+                # Otherwise, if the bad sequence is longer than the current length they can't ever match
                 return tf.cond(
                     tf.math.greater(self.bad_word_seqs_len[bad_word_seq_number], row_input_ids.shape[0]),
                     lambda: tf.zeros((), dtype=tf.bool),
