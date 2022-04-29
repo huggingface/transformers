a3407c0ccb43d32b8887914e06e352e9963bd32c
Animesh Jain <anijain@umich.edu>
2022-04-29 00:06:19 +0000

    fixes

--- hf_dynamo_aot.py
+++ hf_dynamo_aot.py
@@ -1,3 +1,4 @@
+import argparse
 import copy
 import gc
 import time
@@ -16,19 +17,19 @@ from transformers import ReformerConfig
 import torchdynamo
 from torchdynamo.optimizations.training import aot_autograd_speedup_strategy
 from torchdynamo.testing import clone_me
+from torchdynamo.testing import collect_results
 from torchdynamo.testing import same
-from torchdynamo.utils import clone_inputs
 
 benchmarks = [
     (BertConfig(), AutoModelForMaskedLM, (4, 512), []),
-    (AutoConfig.from_pretrained("albert-base-v2"), AutoModelForMaskedLM, (8, 512), []),
-    (AutoConfig.from_pretrained("gpt2"), AutoModelForCausalLM, (4, 512), []),
-    (
-        AutoConfig.from_pretrained("allenai/longformer-base-4096"),
-        AutoModelForMaskedLM,
-        (2, 1024),
-        [torch.float16, torch.bfloat16],
-    ),  # hmm, nans with float16
+    # (AutoConfig.from_pretrained("albert-base-v2"), AutoModelForMaskedLM, (8, 512), []),
+    # (AutoConfig.from_pretrained("gpt2"), AutoModelForCausalLM, (4, 512), []),
+    # (
+    #     AutoConfig.from_pretrained("allenai/longformer-base-4096"),
+    #     AutoModelForMaskedLM,
+    #     (2, 1024),
+    #     [torch.float16, torch.bfloat16],
+    # ),  # hmm, nans with float16
     # (AutoConfig.from_pretrained("t5-small"), AutoModelForSeq2SeqLM, (4, 1024), [torch.float16, torch.bfloat16]), # Doesn't work with nn.utils._stateless for some reason...
     # (AutoConfig.from_pretrained("facebook/bert-base"), AutoModelForSeq2SeqLM, (4, 512), []), # Doesn't work with nn.utils._stateless for some reason...
     # (ReformerConfig(), AutoModelForMaskedLM, (8, 4096), []), # not sure...
@@ -62,18 +63,6 @@ def get_cur_memory():
 
 
 @torchdynamo.skip
-def collect_results(model, prediction, loss):
-    results = []
-    results.append(prediction)
-    results.append(loss)
-    grads = dict()
-    for name, param in model.named_parameters():
-        grads[name + ".grad"] = clone_me(param.grad)
-    results.append(grads)
-    return results
-
-
-@torchdynamo.skip
 def forward_pass(mod, inputs, collect_outputs=True):
     return mod(*inputs)
 
@@ -85,7 +74,7 @@ def forward_and_backward_pass(mod, inputs, collect_outputs=True):
     loss = pred.loss.mean()
     loss.backward()
     if collect_outputs:
-        return collect_results(mod, pred, loss)
+        return collect_results(mod, pred, loss, example_inputs=())
     return None
 
 
@@ -159,62 +148,81 @@ model_header, dtype_header, nh, th, mh, tp, mp, acc = (
 )
 
 
+def create_record(model_name, dtype, is_accurate, name, t, m):
+    return {
+        model_header: model_name,
+        dtype_header: str(dtype),
+        acc: is_accurate,
+        nh: name,
+        th: t,
+        mh: m / 2**30,
+    }
+
+
 numerical_diffs = []
 results = []
-for config, model_type, input_size, not_supported_dtypes in benchmarks:
-    for dtype in [torch.float, torch.half, torch.bfloat16]:
-        if dtype in not_supported_dtypes:
-            continue
-        for attr in dir(config):
-            if "drop" in attr:
-                setattr(
-                    config, attr, 1e-60
-                )  # So we can check for correct gradients without eliminating the dropout computation
-        model = model_type.from_config(config).to(device, dtype=dtype)
 
-        if USE_EVAL:
-            model.eval()
 
-        input_ids = torch.randint(0, config.vocab_size, input_size).to(device)
-        decoder_ids = torch.randint(0, config.vocab_size, input_size).to(device)
+def run_all():
+    for config, model_type, input_size, not_supported_dtypes in benchmarks:
+        for dtype in [torch.float, torch.half, torch.bfloat16]:
+            if dtype in not_supported_dtypes:
+                continue
+            for attr in dir(config):
+                if "drop" in attr:
+                    setattr(
+                        config, attr, 1e-60
+                    )  # So we can check for correct gradients without eliminating the dropout computation
+            model = model_type.from_config(config).to(device, dtype=dtype)
 
-        # train_inputs = (input_ids, decoder_ids)
-        train_inputs = {"input_ids": input_ids, "labels": decoder_ids}
+            if USE_EVAL:
+                model.eval()
 
-        is_accurate = check_correctness(model, train_inputs)
-        model_name = type(model).__name__
-        t, m = bench_model("eager", model, train_inputs)
-        results.append(
-            {
-                model_header: model_name,
-                dtype_header: str(dtype),
-                acc: is_accurate,
-                nh: "eager",
-                th: t,
-                mh: m / 2**30,
-            }
-        )
-        with torch.jit.fuser("fuser2"):
-            t, m = bench_model("dynamo_aot", model, train_inputs)
-        results.append(
-            {
-                model_header: model_name,
-                dtype_header: str(dtype),
-                nh: "dynamo_aot",
-                acc: is_accurate,
-                th: t,
-                mh: m / 2**30,
-            }
-        )
+            model_name = type(model).__name__
 
-        # calculate relative improvements
-        base_r = results[-2]
-        for r in results[-2:]:
-            r[tp] = round(100 * (r[th] - base_r[th]) / base_r[th])
-            r[mp] = round(100 * (r[mh] - base_r[mh]) / base_r[mh])
-        print(pd.DataFrame(results[-2:]).to_markdown(index=False, floatfmt=".3f"))
+            # Prepare inputs
+            input_ids = torch.randint(0, config.vocab_size, input_size).to(device)
+            decoder_ids = torch.randint(0, config.vocab_size, input_size).to(device)
+            train_inputs = {"input_ids": input_ids, "labels": decoder_ids}
 
-        print()
+            # Correctness check
+            is_accurate = check_correctness(model, train_inputs)
 
-print(results)
-print(pd.DataFrame(results).to_markdown(index=False, floatfmt=".3f"))
+            # Profile eager
+            t, m = bench_model("eager", model, train_inputs)
+            results.append(create_record(model_name, dtype, is_accurate, "eager", t, m))
+
+            # Profile Dynamo nvfuser
+            with torch.jit.fuser("fuser2"):
+                t, m = bench_model("dynamo_aot", model, train_inputs)
+            results.append(create_record(model_name, dtype, is_accurate, "dynamo_aot", t, m))
+
+            # calculate relative improvements
+            base_r = results[-2]
+            for r in results[-2:]:
+                r[tp] = round(100 * (r[th] - base_r[th]) / base_r[th])
+                r[mp] = round(100 * (r[mh] - base_r[mh]) / base_r[mh])
+            print(pd.DataFrame(results[-2:]).to_markdown(index=False, floatfmt=".3f"))
+
+    print("=== Final results ===")
+    print(pd.DataFrame(results).to_markdown(index=False, floatfmt=".3f"))
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--use-eval-mode",
+        action="store_true",
+        help="sets model.eval() to reduce randomness",
+    )
+
+    args = parser.parse_args()
+    if args.use_eval_mode:
+        global USE_EVAL
+        USE_EVAL = True
+
+    run_all()
+
+
+if __name__ == "__main__":
+    main()
