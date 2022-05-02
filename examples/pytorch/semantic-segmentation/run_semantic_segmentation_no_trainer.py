6aca3ff85629743f1f1b7cb3c67a3c5284eec657
Zachary Mueller <muellerzr@gmail.com>
2022-05-02 11:56:25 -0400

    Fix no_trainer examples to properly calculate the number of samples (#17046)
    * Update all examples to properly calculate progress bar

--- examples/pytorch/semantic-segmentation/run_semantic_segmentation_no_trainer.py
+++ examples/pytorch/semantic-segmentation/run_semantic_segmentation_no_trainer.py
@@ -475,6 +475,10 @@ def main():
         model, optimizer, train_dataloader, eval_dataloader, lr_scheduler
     )
 
+    # We need to recalculate our total training steps as the size of the training dataloader may have changed.
+    num_update_steps_per_epoch = math.ceil(len(train_dataloader) / args.gradient_accumulation_steps)
+    args.max_train_steps = args.num_train_epochs * num_update_steps_per_epoch
+
     # Instantiate metric
     metric = load_metric("mean_iou")
 
