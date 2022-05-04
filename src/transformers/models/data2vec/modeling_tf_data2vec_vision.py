fix: expand() on TF tensors with broadcast_to().

src/transformers/models/data2vec/modeling_tf_data2vec_vision.py
@@ -161,12 +161,12 @@ class TFData2VecVisionEmbeddings(tf.keras.layers.Layer):
     def call(self, pixel_values: tf.Tensor, bool_masked_pos: Optional[tf.Tensor] = None) -> tf.Tensor:
 
         embeddings = self.patch_embeddings(pixel_values)
-        batch_size, seq_len, _ = shape_list(embeddings)
+        batch_size, seq_len, projection_dim = shape_list(embeddings)
 
         cls_tokens = tf.tile(self.cls_token, (batch_size, 1, 1))
 
         if bool_masked_pos is not None:
-            mask_tokens = self.mask_token.expand(batch_size, seq_len, -1)
+            mask_tokens = tf.broadcast_to(self.mask_token, (batch_size, seq_len, projection_dim))
             # replace the masked visual tokens by mask_tokens
             w = bool_masked_pos[..., None]
             w = tf.cast(w, mask_tokens.dtype)