d0d6fec72d7a22497b6d8a1d952030591486a4ef
Jia Li <jia@cardiologs.com>
2022-05-05 19:42:38 +0200

    update style test

--- examples/research_projects/codeparrot/scripts/test_deduplicate.py
+++ examples/research_projects/codeparrot/scripts/test_deduplicate.py
@@ -1,11 +1,11 @@
 from unittest import TestCase
+
 from datasets import Dataset
 
 from deduplicate import make_duplicate_clusters
 
 
 class MakeDuplicateClustersTest(TestCase):
-
     def test_make_duplicate_clusters(self):
         data_dict = {
             "repo_name": ["test_repo1", "test_repo2"],
