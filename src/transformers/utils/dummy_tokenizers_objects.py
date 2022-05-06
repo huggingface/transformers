096eb74a6ed28b5bc6ba5169134fefb4eb6c5aea
ArthurZucker <arthur.zucker@gmail.com>
2022-05-06 12:24:45 +0200

    make repo consistency

--- src/transformers/utils/dummy_tokenizers_objects.py
+++ src/transformers/utils/dummy_tokenizers_objects.py
@@ -241,13 +241,6 @@ class OpenAIGPTTokenizerFast(metaclass=DummyObject):
         requires_backends(self, ["tokenizers"])
 
 
-class OPTTokenizerFast(metaclass=DummyObject):
-    _backends = ["tokenizers"]
-
-    def __init__(self, *args, **kwargs):
-        requires_backends(self, ["tokenizers"])
-
-
 class PegasusTokenizerFast(metaclass=DummyObject):
     _backends = ["tokenizers"]
 
