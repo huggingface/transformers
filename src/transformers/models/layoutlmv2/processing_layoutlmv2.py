657464468bea062ce7d511b3614b31b7a77086bf
ghlai9665 <glai9665@gmail.com>
2022-05-06 17:05:15 -0500

    add test for overflowing_tokens, change assert to ValueError, avoiding unrelated formatting changes

--- src/transformers/models/layoutlmv2/processing_layoutlmv2.py
+++ src/transformers/models/layoutlmv2/processing_layoutlmv2.py
@@ -138,8 +138,9 @@ class LayoutLMv2Processor(ProcessorMixin):
         for sample_idx in overflow_to_sample_mapping:
             images_with_overflow.append(images[sample_idx])
 
-        assert len(images_with_overflow) == len(
-            overflow_to_sample_mapping
-        ), f"Expected length of images to be the same as the length of overflow_to_sample_mapping, but got {len(images_with_overflow)} and {len(overflow_to_sample_mapping)}"
+        if len(images_with_overflow) != len(overflow_to_sample_mapping):
+            raise ValueError(
+                f"Expected length of images to be the same as the length of `overflow_to_sample_mapping`, but got {len(images_with_overflow)} and {len(overflow_to_sample_mapping)}"
+            )
 
         return images_with_overflow
