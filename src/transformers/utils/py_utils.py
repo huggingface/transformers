46716623921d53d7f7ac809cf6c874c1067b6867
Nicolas Brousse <nicolas@brousse.info>
2022-05-09 14:56:44 -0700

    ensure key is str

--- src/transformers/utils/py_utils.py
+++ src/transformers/utils/py_utils.py
@@ -21,7 +21,7 @@ def flatten_dict(d: MutableMapping, parent_key: str = '', delimiter: str = '.'):
     """Flatten a nested dict into a single level dict."""
     def _flatten_dict(d, parent_key="", delimiter="."):
         for k, v in d.items():
-            key = parent_key + delimiter + k if parent_key else k
+            key = str(parent_key) + delimiter + str(k) if parent_key else k
             if v and isinstance(v, MutableMapping):
                 yield from flatten_dict(v, key, delimiter=delimiter).items()
             else:
