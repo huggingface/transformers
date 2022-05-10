0e7020fb94cbf04360c640fd2957eb60a23ea128
Chan Woo Kim <chanwkim01@gmail.com>
2022-05-10 13:53:02 +0000

    how to avoid tf tests?

--- src/transformers/models/mctct/__init__.py
+++ src/transformers/models/mctct/__init__.py
@@ -20,9 +20,7 @@ from typing import TYPE_CHECKING
 from ...utils import (
     OptionalDependencyNotAvailable,
     _LazyModule,
-    is_sentencepiece_available,
     is_speech_available,
-    is_tf_available,
     is_torch_available,
 )
 
