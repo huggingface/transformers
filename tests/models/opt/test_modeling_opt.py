b01b78cfb25c686e3a1945fb0ec5cdcbde27f7e7
ArthurZucker <arthur.zucker@gmail.com>
2022-05-11 13:24:35 +0200

    clean up preapre opt input dict

--- tests/models/opt/test_modeling_opt.py
+++ tests/models/opt/test_modeling_opt.py
@@ -46,10 +46,6 @@ def prepare_opt_inputs_dict(
 ):
     if attention_mask is None:
         attention_mask = input_ids.ne(config.pad_token_id)
-    if decoder_attention_mask is None:
-        decoder_attention_mask = decoder_input_ids.ne(config.pad_token_id)
-    if decoder_head_mask is None:
-        decoder_head_mask = torch.ones(config.num_hidden_layers, config.num_attention_heads, device=torch_device)
     return {
         "input_ids": input_ids,
         "attention_mask": attention_mask,
