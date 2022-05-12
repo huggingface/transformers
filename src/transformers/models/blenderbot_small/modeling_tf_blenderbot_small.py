912e65d22e3cbdeef78a4148bd4a79ce5df16c77
matt <rocketknight1@gmail.com>
2022-05-12 13:51:20 +0100

    Add casts to blenderbot and blenderbot-small

--- src/transformers/models/blenderbot_small/modeling_tf_blenderbot_small.py
+++ src/transformers/models/blenderbot_small/modeling_tf_blenderbot_small.py
@@ -1265,7 +1265,7 @@ class TFBlenderbotSmallForConditionalGeneration(TFBlenderbotSmallPreTrainedModel
         if labels is not None:
             labels = tf.where(
                 labels == self.config.pad_token_id,
-                tf.fill(shape_list(labels), -100),
+                tf.cast(tf.fill(shape_list(labels), -100), labels.dtype),
                 labels,
             )
             use_cache = False
