c210216a02721f80c12c49fd7953a3560661ba6e
younesbelkada <younesbelkada@gmail.com>
2022-05-12 17:14:49 +0200

    fix style

--- tests/models/bigscience176b/test_embeddings_bigscience176b.py
+++ tests/models/bigscience176b/test_embeddings_bigscience176b.py
@@ -273,7 +273,7 @@ class BigScienceEmbeddingTest(unittest.TestCase):
 
         tensor_ids = torch.LongTensor([EXAMPLE_IDS])
         # position_ids = torch.LongTensor([[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]])
-        with torch.no_grad():            
+        with torch.no_grad():
             embeddings = model.word_embeddings(tensor_ids)
             embeddings_ln = model.word_embeddings_layernorm(embeddings)  #
         # first check the embeddings before LN
