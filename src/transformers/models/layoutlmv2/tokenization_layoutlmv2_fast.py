1ae4824889d882ee14d0d2571806e937981da251
Timothy J Laurent <timothyjlaurent@gmail.com>
2022-05-12 15:44:50 -0700

    Use word index for determining whether a token is a subword when adding word labels.

--- src/transformers/models/layoutlmv2/tokenization_layoutlmv2_fast.py
+++ src/transformers/models/layoutlmv2/tokenization_layoutlmv2_fast.py
@@ -581,16 +581,17 @@ class LayoutLMv2TokenizerFast(PreTrainedTokenizerFast):
                 else:
                     original_index = batch_index
                 labels_example = []
-                for id, offset, word_id in zip(
+                cur_word_id = -1
+                for id, word_id in zip(
                     sanitized_tokens["input_ids"][batch_index],
-                    sanitized_tokens["offset_mapping"][batch_index],
                     sanitized_encodings[batch_index].word_ids,
                 ):
                     if word_id is not None:
                         if self.only_label_first_subword:
-                            if offset[0] == 0:
+                            if word_id != cur_word_id:
                                 # Use the real label id for the first token of the word, and padding ids for the remaining tokens
                                 labels_example.append(word_labels[original_index][word_id])
+                                cur_word_id = word_id
                             else:
                                 labels_example.append(self.pad_token_label)
                         else:
