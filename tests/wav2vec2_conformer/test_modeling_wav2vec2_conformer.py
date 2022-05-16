ffd01e01d407bca87a1adafaee2b915816c42223
Patrick von Platen <patrick.v.platen@gmail.com>
2022-05-16 13:54:33 -0400

    finish

--- tests/wav2vec2_conformer/test_modeling_wav2vec2_conformer.py
+++ tests/wav2vec2_conformer/test_modeling_wav2vec2_conformer.py
@@ -815,9 +815,8 @@ class Wav2Vec2ConformerModelIntegrationTest(unittest.TestCase):
     def _load_datasamples(self, num_samples):
         ds = load_dataset("hf-internal-testing/librispeech_asr_dummy", "clean", split="validation")
         # automatic decoding with librispeech
-        speech_samples = ds.sort("id").filter(
-            lambda x: x["id"] in [f"1272-141231-000{i}" for i in range(num_samples)]
-        )[:num_samples]["audio"]
+        speech_samples = ds.sort("id").filter(lambda x: x["id"] in [f"1272-141231-000{i}" for i in range(num_samples)])
+        speech_samples = speech_samples[:num_samples]["audio"]
 
         return [x["array"] for x in speech_samples]
 
