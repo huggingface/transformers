2ae2834268ac6c3e0057723d2227bed17dd10a09
Sylvain Gugger <35901082+sgugger@users.noreply.github.com>
2022-05-16 14:55:46 -0400

    Better error in the Auto API when a dep is missing (#17289)

--- src/transformers/models/auto/feature_extraction_auto.py
+++ src/transformers/models/auto/feature_extraction_auto.py
@@ -84,6 +84,12 @@ def feature_extractor_class_from_name(class_name: str):
         if getattr(extractor, "__name__", None) == class_name:
             return extractor
 
+    # We did not fine the class, but maybe it's because a dep is missing. In that case, the class will be in the main
+    # init and we return the proper dummy to get an appropriate error message.
+    main_module = importlib.import_module("transformers")
+    if hasattr(main_module, class_name):
+        return getattr(main_module, class_name)
+
     return None
 
 
