fddbff19069ee0cddba5041dc5dfee1d14063e4d
AnugunjNaman <anugunjjha@gmail.com>
2022-05-17 16:31:02 +0530

    style

--- src/transformers/models/cvt/modeling_cvt.py
+++ src/transformers/models/cvt/modeling_cvt.py
@@ -637,7 +637,7 @@ class CvtModel(CvtPreTrainedModel):
         >>> from PIL import Image
         >>> import requests
 
-        >>> url = 'http://images.cocodataset.org/val2017/000000039769.jpg'
+        >>> url = "http://images.cocodataset.org/val2017/000000039769.jpg"
         >>> image = Image.open(requests.get(url, stream=True).raw)
 
         >>> feature_extractor = AutoFeatureExtractor.from_pretrained("anugunj/cvt-13")
@@ -723,7 +723,7 @@ class CvtForImageClassification(CvtPreTrainedModel):
         >>> from PIL import Image
         >>> import requests
 
-        >>> url = 'http://images.cocodataset.org/val2017/000000039769.jpg'
+        >>> url = "http://images.cocodataset.org/val2017/000000039769.jpg"
         >>> image = Image.open(requests.get(url, stream=True).raw)
 
         >>> feature_extractor = AutoFeatureExtractor.from_pretrained("anugunj/cvt-13")
