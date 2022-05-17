bb410bb0a39b8e1b5b7dc674357e1482cfd5b257
Carl <carl.cochet@gmail.com>
2022-05-17 23:42:00 +0200

    Remove tapex

--- src/transformers/models/auto/configuration_auto.py
+++ src/transformers/models/auto/configuration_auto.py
@@ -113,7 +113,6 @@ CONFIG_MAPPING_NAMES = OrderedDict(
         ("swin", "SwinConfig"),
         ("t5", "T5Config"),
         ("tapas", "TapasConfig"),
-        ("tapex", "BartConfig"),
         ("trajectory_transformer", "TrajectoryTransformerConfig"),
         ("transfo-xl", "TransfoXLConfig"),
         ("trocr", "TrOCRConfig"),
