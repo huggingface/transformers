0308cad2776c999133a8baf7251ab862372c7f0e
Patrick von Platen <patrick.v.platen@gmail.com>
2022-05-17 23:34:15 +0200

    Update src/transformers/modeling_tf_utils.py
    Co-authored-by: Sylvain Gugger <35901082+sgugger@users.noreply.github.com>

--- src/transformers/modeling_tf_utils.py
+++ src/transformers/modeling_tf_utils.py
@@ -1975,7 +1975,6 @@ class TFSharedEmbeddings(tf.keras.layers.Layer):
         Build shared token embedding layer Shared weights logic adapted from
         https://github.com/tensorflow/models/blob/a009f4fb9d2fc4949e32192a944688925ef78659/official/transformer/v2/embedding_layer.py#L24
         """
-
         self.weight = self.add_weight(
             "weight", shape=[self.vocab_size, self.hidden_size], initializer=get_initializer(self.initializer_range)
         )
