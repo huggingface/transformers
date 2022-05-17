a8c72e9ceb557a4ad95d3c85f25a9166522ae6e9
Anugunj Naman <anugunjjha@gmail.com>
2022-05-17 14:02:56 +0530

    Update src/transformers/models/cvt/convert_cvt_original_pytorch_checkpoint_to_pytorch.py
    Co-authored-by: NielsRogge <48327001+NielsRogge@users.noreply.github.com>

--- src/transformers/models/cvt/convert_cvt_original_pytorch_checkpoint_to_pytorch.py
+++ src/transformers/models/cvt/convert_cvt_original_pytorch_checkpoint_to_pytorch.py
@@ -12,7 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Convert Cvt checkpoints from the original repository.
+"""Convert CvT checkpoints from the original repository.
 
 URL: https://github.com/microsoft/CvT"""
 
