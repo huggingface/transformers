86ffa953be774c0ffb247e50b82a112f3f7634ef
Sijun HE <sijun.he@hotmail.com>
2022-05-17 22:29:23 +0800

    rebase

--- tests/pipelines/test_pipelines_visual_question_answering.py
+++ tests/pipelines/test_pipelines_visual_question_answering.py
@@ -16,10 +16,13 @@ import unittest
 
 from transformers import (
     MODEL_FOR_VISUAL_QUESTION_ANSWERING_MAPPING,
+    BertTokenizer,
+    ViltFeatureExtractor,
+    ViltForQuestionAnswering,
     PreTrainedTokenizer,
     is_vision_available,
 )
-from transformers.pipelines import VisualQuestionAnsweringPipeline, pipeline
+from transformers.pipelines import VisualQuestionAnsweringPipeline, VisualQuestionAnsweringArgumentHandler, pipeline
 from transformers.testing_utils import (
     is_pipeline_test,
     nested_simplify,
@@ -41,156 +44,93 @@ else:
         def open(*args, **kwargs):
             pass
 
+@is_pipeline_test
+class VisualQuestionAnsweringArgumentHandlerTests(unittest.TestCase):
+    def test_argument_handler(self):
+        qa = VisualQuestionAnsweringArgumentHandler()
+        image1 = Image.open("./tests/fixtures/tests_samples/COCO/000000039769.png")
+        image2 = "http://images.cocodataset.org/val2017/000000039769.jpg"
+        query = "How many cats are there?"
+
+        for image in [image1, image2]:
+            output = qa(images=image, queries=query)
+            self.assertEqual(type(output), list)
+            self.assertEqual(len(output), 1)
+            for item in output:
+                self.assertEqual(type(item["image"]), Image.Image)
+                self.assertEqual(type(item["query"]), str)
+
+        output = qa(images=[image1, image2], queries=[query, query])
+        self.assertEqual(type(output), list)
+        self.assertEqual(len(output), 2)
+        for item in output:
+            self.assertEqual(type(item["image"]), Image.Image)
+            self.assertEqual(type(item["query"]), str)
+
+    def test_argument_handler_error_handling(self):
+        qa = VisualQuestionAnsweringArgumentHandler()
+        image1 = Image.open("./tests/fixtures/tests_samples/COCO/000000039769.png")
+        image2 = "http://images.cocodataset.org/val2017/000000039769.jpg"
+        query = "How many cats are there?"
+
+        with self.assertRaises(ValueError):
+            qa(images=[image1, image2], queries=query)
+            qa(images=[image1, image2], queries=[query])
+            qa(images=image1, queries=[query, query])
+            qa(images=[image1], queries=[query, query])
+            qa(images=[image1, image2], queries=[query, query, query])
+
 
 @is_pipeline_test
-# @require_vision
+@require_vision
 class VisualQuestionAnsweringPipelineTests(unittest.TestCase, metaclass=PipelineTestCaseMeta):
     model_mapping = MODEL_FOR_VISUAL_QUESTION_ANSWERING_MAPPING
 
     def get_test_pipeline(self, model, tokenizer, feature_extractor):
-        vqa_pipeline = VisualQuestionAnsweringPipeline(model=model, tokenizer=tokenizer, feature_extractor=feature_extractor, top_k=2)
-        examples = [
-            Image.open("./tests/fixtures/tests_samples/COCO/000000039769.png"),
-            "http://images.cocodataset.org/val2017/000000039769.jpg",
-        ]
-        return vqa_pipeline, examples
+        vqa_pipeline = pipeline("visual-question-answering", model="dandelin/vilt-b32-finetuned-vqa", top_k=2)
+        images = Image.open("./tests/fixtures/tests_samples/COCO/000000039769.png")
+        queries = "How many cats are there?"
+        return vqa_pipeline, (images, queries)
 
-    # def run_pipeline_test(self, image_classifier, examples):
-    #     outputs = image_classifier("./tests/fixtures/tests_samples/COCO/000000039769.png")
-
-    #     self.assertEqual(
-    #         outputs,
-    #         [
-    #             {"score": ANY(float), "label": ANY(str)},
-    #             {"score": ANY(float), "label": ANY(str)},
-    #         ],
-    #     )
-
-    #     import datasets
-
-    #     dataset = datasets.load_dataset("hf-internal-testing/fixtures_image_utils", "image", split="test")
-
-    #     # Accepts URL + PIL.Image + lists
-    #     outputs = image_classifier(
-    #         [
-    #             Image.open("./tests/fixtures/tests_samples/COCO/000000039769.png"),
-    #             "http://images.cocodataset.org/val2017/000000039769.jpg",
-    #             # RGBA
-    #             dataset[0]["file"],
-    #             # LA
-    #             dataset[1]["file"],
-    #             # L
-    #             dataset[2]["file"],
-    #         ]
-    #     )
-    #     self.assertEqual(
-    #         outputs,
-    #         [
-    #             [
-    #                 {"score": ANY(float), "label": ANY(str)},
-    #                 {"score": ANY(float), "label": ANY(str)},
-    #             ],
-    #             [
-    #                 {"score": ANY(float), "label": ANY(str)},
-    #                 {"score": ANY(float), "label": ANY(str)},
-    #             ],
-    #             [
-    #                 {"score": ANY(float), "label": ANY(str)},
-    #                 {"score": ANY(float), "label": ANY(str)},
-    #             ],
-    #             [
-    #                 {"score": ANY(float), "label": ANY(str)},
-    #                 {"score": ANY(float), "label": ANY(str)},
-    #             ],
-    #             [
-    #                 {"score": ANY(float), "label": ANY(str)},
-    #                 {"score": ANY(float), "label": ANY(str)},
-    #             ],
-    #         ],
-    #     )
+    def run_pipeline_test(self, vqa_pipeline, examples):
+        images, queries = examples
+        outputs = vqa_pipeline(images, queries)
+        self.assertEqual(
+            outputs,
+            [
+                {"score": ANY(float), "label": ANY(str)},
+                {"score": ANY(float), "label": ANY(str)},
+            ],
+        )
 
     @require_torch
     def test_small_model_pt(self):
-        model = "dandelin/vilt-b32-finetuned-vqa"
-        vqa_pipeline = pipeline("visual-question-answering", model=model)
+        vqa_pipeline = pipeline("visual-question-answering", model="dandelin/vilt-b32-finetuned-vqa", top_k=2)
+        # model = ViltForQuestionAnswering.from_pretrained("dandelin/vilt-b32-finetuned-vqa")
+        # tokenizer = BertTokenizer.from_pretrained("dandelin/vilt-b32-finetuned-vqa")
+        # feature_extractor = ViltFeatureExtractor.from_pretrained("dandelin/vilt-b32-finetuned-vqa")
+        # vqa_pipeline = VisualQuestionAnsweringPipeline(model=model, tokenizer=tokenizer, feature_extractor=feature_extractor)
+        image = "./tests/fixtures/tests_samples/COCO/000000039769.png"
+        query = "How many cats are there?"
+        expected_output_top_2 = [{'score': 0.9444, 'label': '2'}, {'label': '1', 'score': 0.0542}]
 
-        outputs = vqa_pipeline("http://images.cocodataset.org/val2017/000000039769.jpg")
-        self.assertEqual(
-            nested_simplify(outputs, decimals=4),
-            [{"label": "LABEL_1", "score": 0.574}, {"label": "LABEL_0", "score": 0.426}],
-        )
+        outputs = vqa_pipeline(images=image, queries=query, top_k=2)
+        self.assertEqual(nested_simplify(outputs, decimals=4), expected_output_top_2)
 
-        outputs = image_classifier(
-            [
-                "http://images.cocodataset.org/val2017/000000039769.jpg",
-                "http://images.cocodataset.org/val2017/000000039769.jpg",
-            ],
-            top_k=2,
-        )
-        self.assertEqual(
-            nested_simplify(outputs, decimals=4),
-            [
-                [{"label": "LABEL_1", "score": 0.574}, {"label": "LABEL_0", "score": 0.426}],
-                [{"label": "LABEL_1", "score": 0.574}, {"label": "LABEL_0", "score": 0.426}],
-            ],
-        )
+        outputs = vqa_pipeline(images=[image], queries=query, top_k=2)
+        self.assertEqual(nested_simplify(outputs, decimals=4), expected_output_top_2)
+
+        outputs = vqa_pipeline(images=image, queries=[query], top_k=2)
+        self.assertEqual(nested_simplify(outputs, decimals=4), expected_output_top_2)
+
+        outputs = vqa_pipeline(images=[image], queries=[query], top_k=2)
+        self.assertEqual(nested_simplify(outputs, decimals=4), expected_output_top_2)
+
+        outputs = vqa_pipeline(images=[image, image], queries=[query, query], top_k=2)
+        self.assertEqual(nested_simplify(outputs, decimals=4), [expected_output_top_2] * 2)
     
     @require_tf
     @unittest.skip("Image segmentation not implemented in TF")
     def test_small_model_tf(self):
         pass
 
-    # def test_custom_tokenizer(self):
-    #     tokenizer = PreTrainedTokenizer()
-
-    #     # Assert that the pipeline can be initialized with a feature extractor that is not in any mapping
-    #     image_classifier = pipeline(
-    #         "image-classification", model="hf-internal-testing/tiny-random-vit", tokenizer=tokenizer
-    #     )
-
-    #     self.assertIs(image_classifier.tokenizer, tokenizer)
-
-    # @slow
-    # @require_torch
-    # def test_perceiver(self):
-    #     # Perceiver is not tested by `run_pipeline_test` properly.
-    #     # That is because the type of feature_extractor and model preprocessor need to be kept
-    #     # in sync, which is not the case in the current design
-    #     image_classifier = pipeline("image-classification", model="deepmind/vision-perceiver-conv")
-    #     outputs = image_classifier("http://images.cocodataset.org/val2017/000000039769.jpg")
-    #     self.assertEqual(
-    #         nested_simplify(outputs, decimals=4),
-    #         [
-    #             {"score": 0.4385, "label": "tabby, tabby cat"},
-    #             {"score": 0.321, "label": "tiger cat"},
-    #             {"score": 0.0502, "label": "Egyptian cat"},
-    #             {"score": 0.0137, "label": "crib, cot"},
-    #             {"score": 0.007, "label": "radiator"},
-    #         ],
-    #     )
-
-    #     image_classifier = pipeline("image-classification", model="deepmind/vision-perceiver-fourier")
-    #     outputs = image_classifier("http://images.cocodataset.org/val2017/000000039769.jpg")
-    #     self.assertEqual(
-    #         nested_simplify(outputs, decimals=4),
-    #         [
-    #             {"score": 0.5658, "label": "tabby, tabby cat"},
-    #             {"score": 0.1309, "label": "tiger cat"},
-    #             {"score": 0.0722, "label": "Egyptian cat"},
-    #             {"score": 0.0707, "label": "remote control, remote"},
-    #             {"score": 0.0082, "label": "computer keyboard, keypad"},
-    #         ],
-    #     )
-
-    #     image_classifier = pipeline("image-classification", model="deepmind/vision-perceiver-learned")
-    #     outputs = image_classifier("http://images.cocodataset.org/val2017/000000039769.jpg")
-    #     self.assertEqual(
-    #         nested_simplify(outputs, decimals=4),
-    #         [
-    #             {"score": 0.3022, "label": "tabby, tabby cat"},
-    #             {"score": 0.2362, "label": "Egyptian cat"},
-    #             {"score": 0.1856, "label": "tiger cat"},
-    #             {"score": 0.0324, "label": "remote control, remote"},
-    #             {"score": 0.0096, "label": "quilt, comforter, comfort, puff"},
-    #         ],
-    #     )
