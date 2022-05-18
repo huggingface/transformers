2501a9fd0bfc345e64be684919fe3dd846994a6c
NielsRogge <niels.rogge1@gmail.com>
2022-05-18 08:13:43 +0000

    Apply suggestions from code review

--- src/transformers/models/deformable_detr/test.py
+++ src/transformers/models/deformable_detr/test.py
@@ -3,15 +3,18 @@ import torch
 from transformers import DeformableDetrConfig, DeformableDetrForObjectDetection
 
 
-model = DeformableDetrForObjectDetection(DeformableDetrConfig()).to("cuda")
+cuda1 = torch.device("cuda:1")
 
-pixel_values = torch.randn(1, 3, 224, 224).to("cuda")
+config = DeformableDetrConfig(with_box_refine=True, two_stage=True)
 
-print("Dict output:")
-dict_outputs = model(pixel_values)
+model = DeformableDetrForObjectDetection(config).to(cuda1)
 
-print("tuple output:")
-tuple_outputs = model(pixel_values, return_dict=False)
+pixel_values = torch.randn(1, 3, 224, 224).to(cuda1)
+
+outputs = model(pixel_values)
+
+for k, v in outputs.items():
+    print(k, v.shape)
 
 # print("OUTPUTS + HIDDEN STATES")
 
