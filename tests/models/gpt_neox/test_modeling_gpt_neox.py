900079f0fb7e5a77ea2d36c5a0663b237d18169e
Jason Phang <jasonphang@nyu.edu>
2022-05-21 14:28:08 -0400

    test refactor

--- tests/models/gpt_neox/test_modeling_gpt_neox.py
+++ tests/models/gpt_neox/test_modeling_gpt_neox.py
@@ -17,10 +17,11 @@
 
 import unittest
 
-from tests.test_configuration_common import ConfigTester
-from tests.test_modeling_common import ModelTesterMixin, ids_tensor, random_attention_mask
 from transformers import GPTNeoXConfig, is_torch_available
 from transformers.testing_utils import require_torch, slow, torch_device
+
+from ...test_configuration_common import ConfigTester
+from ...test_modeling_common import ModelTesterMixin, ids_tensor, random_attention_mask
 
 
 if is_torch_available():
