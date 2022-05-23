c4f9470caf5ee8fc5752e92229aaa6f722fd2a34
Jiarui Xu <xvjiarui0826@gmail.com>
2022-05-23 11:56:43 -0700

    delete unused attr

--- src/transformers/models/groupvit/modeling_groupvit.py
+++ src/transformers/models/groupvit/modeling_groupvit.py
@@ -794,8 +794,6 @@ class GroupViTVisionOutput(nn.Module):
 class GroupViTVisionLayer(nn.Module):
     def __init__(self, config: GroupViTVisionConfig) -> None:
         super().__init__()
-        self.chunk_size_feed_forward = config.chunk_size_feed_forward
-        self.seq_len_dim = 1
         self.attention = GroupViTVisionAttention(config)
         self.intermediate = GroupViTIntermediate(config)
         self.output = GroupViTVisionOutput(config)
