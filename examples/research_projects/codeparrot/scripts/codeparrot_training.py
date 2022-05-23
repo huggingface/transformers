178ce667e7e05348f09775b301e71b8bf538772e
Loubna ben allal <loubnabenallal@gmail.com>
2022-05-23 09:56:21 +0000

    compute avg_loss in one line

--- examples/research_projects/codeparrot/scripts/codeparrot_training.py
+++ examples/research_projects/codeparrot/scripts/codeparrot_training.py
@@ -267,8 +267,7 @@ for step, batch in enumerate(train_dataloader, start=1):
     if args.resume_from_checkpoint and step < resume_step:
         continue  # we need to skip steps until we reach the resumed step
     loss = model(batch, labels=batch, use_cache=False).loss
-    avg_loss = loss.repeat(args.train_batch_size)
-    avg_loss = accelerator.gather(avg_loss).mean()
+    avg_loss = accelerator.gather(loss.repeat(args.train_batch_size)).mean()
     loss_tracking += avg_loss.item() / args.gradient_accumulation_steps
     log_metrics(step, {"samples": step * samples_per_step, "loss_per_step/train": loss.item()})
     loss = loss / args.gradient_accumulation_steps
