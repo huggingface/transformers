f3cd5768f442a7507e079730e56672cb4e475596
ydshieh <ydshieh@users.noreply.github.com>
2022-05-24 20:11:32 +0200

    update print_env_pt.py

--- utils/print_env_pt.py
+++ utils/print_env_pt.py
@@ -17,12 +17,30 @@
 
 # this script dumps information about the environment
 
-import torch
+import transformers
+print("transformers version:", transformers.__version__)
 
+try:
+    import torch
+    print("Torch version:", torch.__version__)
+    print("Cuda available:", torch.cuda.is_available())
+    print("Cuda version:", torch.version.cuda)
+    print("CuDNN version:", torch.backends.cudnn.version())
+    print("Number of GPUs available:", torch.cuda.device_count())
+    print("NCCL version:", torch.cuda.nccl.version())
+except ImportError:
+    print("Torch version:", None)
 
-print("Torch version:", torch.__version__)
-print("Cuda available:", torch.cuda.is_available())
-print("Cuda version:", torch.version.cuda)
-print("CuDNN version:", torch.backends.cudnn.version())
-print("Number of GPUs available:", torch.cuda.device_count())
-print("NCCL version:", torch.cuda.nccl.version())
+try:
+    import deepspeed
+    print("DeepSpeed version:", deepspeed.__version__)
+except ImportError:
+    print("DeepSpeed version:", None)
+
+try:
+    import tensorflow as tf
+    print("TensorFlow version:", tf.__version__)
+    print('TF GPUs available:', bool(tf.config.list_physical_devices('GPU')))
+    print('Number of TF GPUs available:', len(tf.config.list_physical_devices('GPU')))
+except ImportError:
+    print("TensorFlow version:", None)
