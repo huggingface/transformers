5c17918fe4cda80dae5b7ec8f0b2d23a813c4a05
Robert Dargavel Smith <teticio@gmail.com>
2022-06-03 10:08:35 +0100

    Allow from transformers import TypicalLogitsWarper (#17477)
    * Allow from transformers import TypicalLogitsWarper
    
    * Added TypicalLogitsWarper
    
    * Allow from transformers import TypicalLogitsWarper
    
    * Allow from transformers import TypicalLogitsWarper
    
    * Allow from transformers import TypicalLogitsWarper
    
    * Allow from transformers import TypicalLogitsWarper
    
    Added TypicalLogitsWarper
    
    Allow from transformers import TypicalLogitsWarper
    
    Allow from transformers import TypicalLogitsWarper
    
    Allow from transformers import TypicalLogitsWarper

--- src/transformers/utils/dummy_pt_objects.py
+++ src/transformers/utils/dummy_pt_objects.py
@@ -234,6 +234,13 @@ class TopPLogitsWarper(metaclass=DummyObject):
         requires_backends(self, ["torch"])
 
 
+class TypicalLogitsWarper(metaclass=DummyObject):
+    _backends = ["torch"]
+
+    def __init__(self, *args, **kwargs):
+        requires_backends(self, ["torch"])
+
+
 class MaxLengthCriteria(metaclass=DummyObject):
     _backends = ["torch"]
 
