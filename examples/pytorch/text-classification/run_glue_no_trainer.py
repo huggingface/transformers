71f4ba782d7e30cedbaf7202986947104bb4f910
bhuang <bofenghuang7@gmail.com>
2022-06-03 18:09:53 +0200

    Update run_glue_no_trainer.py

--- examples/pytorch/text-classification/run_glue_no_trainer.py
+++ examples/pytorch/text-classification/run_glue_no_trainer.py
@@ -270,7 +270,7 @@ def main():
             data_files["train"] = args.train_file
         if args.validation_file is not None:
             data_files["validation"] = args.validation_file
-        extension = (args.train_file if args.train_file is not None else args.valid_file).split(".")[-1]
+        extension = (args.train_file if args.train_file is not None else args.validation_file).split(".")[-1]
         raw_datasets = load_dataset(extension, data_files=data_files)
     # See more about loading any type of standard or custom dataset at
     # https://huggingface.co/docs/datasets/loading_datasets.html.
