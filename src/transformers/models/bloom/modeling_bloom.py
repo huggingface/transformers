8b481cee094b6823fa3bda4709ce02e124cc02e7
younesbelkada <younesbelkada@gmail.com>
2022-06-04 00:56:56 +0200

    make quality

--- src/transformers/models/bloom/modeling_bloom.py
+++ src/transformers/models/bloom/modeling_bloom.py
@@ -19,7 +19,7 @@ from typing import Tuple
 
 import torch
 import torch.utils.checkpoint
-from torch import Tensor, nn
+from torch import nn
 from torch.nn import CrossEntropyLoss, LayerNorm
 
 from ...file_utils import add_code_sample_docstrings, add_start_docstrings, add_start_docstrings_to_model_forward
