64174a2bd87c64409165f6faa0cae1e27ba18120
Sylvain Gugger <Sylvain.gugger@gmail.com>
2022-06-06 16:41:07 -0400

    Style

--- src/transformers/onnx/utils.py
+++ src/transformers/onnx/utils.py
@@ -18,7 +18,7 @@ from typing import TYPE_CHECKING, Optional, Union
 
 
 if TYPE_CHECKING:
-    from .. import AutoFeatureExtractor, AutoProcessor, AutoTokenizer # tests_ignore
+    from .. import AutoFeatureExtractor, AutoProcessor, AutoTokenizer  # tests_ignore
 
 
 class ParameterFormat(Enum):
@@ -82,7 +82,7 @@ def get_preprocessor(model_name: str) -> Optional[Union["AutoTokenizer", "AutoFe
             `None` if no preprocessor is found.
     """
     # Avoid circular imports by only importing this here.
-    from .. import AutoFeatureExtractor, AutoProcessor, AutoTokenizer # tests_ignore
+    from .. import AutoFeatureExtractor, AutoProcessor, AutoTokenizer  # tests_ignore
 
     try:
         return AutoProcessor.from_pretrained(model_name)
