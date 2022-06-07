2dc284f8ce4955133af73f11b9c9e17b3c0eb888
Sylvain Gugger <35901082+sgugger@users.noreply.github.com>
2022-06-07 11:57:52 -0400

    Add examples telemetry (#17552)
    * Add examples telemetry
    
    * Alternative approach
    
    * Add to all other examples
    
    * Add to templates as well
    
    * Put framework separately
    
    * Same for TensorFlow

--- examples/pytorch/multiple-choice/run_swag_no_trainer.py
+++ examples/pytorch/multiple-choice/run_swag_no_trainer.py
@@ -51,7 +51,7 @@ from transformers import (
     default_data_collator,
     get_scheduler,
 )
-from transformers.utils import PaddingStrategy, get_full_repo_name
+from transformers.utils import PaddingStrategy, get_full_repo_name, send_example_telemetry
 
 
 logger = get_logger(__name__)
@@ -272,6 +272,10 @@ class DataCollatorForMultipleChoice:
 
 def main():
     args = parse_args()
+
+    # Sending telemetry. Tracking the example usage helps us better allocate resources to maintain them. The
+    # information sent is the one passed as arguments along with your Python/PyTorch versions.
+    send_example_telemetry("run_swag_no_trainer", args)
 
     # Initialize the accelerator. We will let the accelerator handle device placement for us in this example.
     # If we're using tracking, we also need to initialize it here and it will by default pick up all supported trackers
