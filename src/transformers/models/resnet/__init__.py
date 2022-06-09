f812791e0627026cda3ca182712f22692e3fc43a
regisss <15324346+regisss@users.noreply.github.com>
2022-06-09 14:44:27 +0200

    Add ONNX support for ResNet (#17585)
    * Add ONNX support for ResNet
    
    * Add ONNX test
    
    * make fix-copies

--- src/transformers/models/resnet/__init__.py
+++ src/transformers/models/resnet/__init__.py
@@ -21,7 +21,9 @@ from typing import TYPE_CHECKING
 from ...utils import OptionalDependencyNotAvailable, _LazyModule, is_torch_available
 
 
-_import_structure = {"configuration_resnet": ["RESNET_PRETRAINED_CONFIG_ARCHIVE_MAP", "ResNetConfig"]}
+_import_structure = {
+    "configuration_resnet": ["RESNET_PRETRAINED_CONFIG_ARCHIVE_MAP", "ResNetConfig", "ResNetOnnxConfig"]
+}
 
 try:
     if not is_torch_available():
@@ -38,7 +40,7 @@ else:
 
 
 if TYPE_CHECKING:
-    from .configuration_resnet import RESNET_PRETRAINED_CONFIG_ARCHIVE_MAP, ResNetConfig
+    from .configuration_resnet import RESNET_PRETRAINED_CONFIG_ARCHIVE_MAP, ResNetConfig, ResNetOnnxConfig
 
     try:
         if not is_torch_available():
