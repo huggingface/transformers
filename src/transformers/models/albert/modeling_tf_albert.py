6cb109e1fb215c2c5f0337ffa70cdea38a2dcaa2
Aakash <aakashbakhle@gmail.com>
2022-06-11 23:46:37 +0530

    by fix-copies

--- src/transformers/models/albert/modeling_tf_albert.py
+++ src/transformers/models/albert/modeling_tf_albert.py
@@ -865,8 +865,11 @@ class TFAlbertForPreTraining(TFAlbertPreTrainedModel, TFAlbertPreTrainingLoss):
         >>> tokenizer = AlbertTokenizer.from_pretrained("albert-base-v2")
         >>> model = TFAlbertForPreTraining.from_pretrained("albert-base-v2")
 
-        >>> input_ids = tf.constant(tokenizer.encode("Hello, my dog is cute", add_special_tokens=True))[None, :]
-        >>> # Batch size 1
+
+        >>> input_ids = tf.constant(tokenizer.encode("Hello, my dog is cute", add_special_tokens=True))[
+        ...     None, :
+        ... ]  # Batch size 1
+        
         >>> outputs = model(input_ids)
 
         >>> prediction_logits = outputs.prediction_logits
