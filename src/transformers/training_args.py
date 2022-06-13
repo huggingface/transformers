457d4a324586133cadb3598697d9a5d5e37de8d2
Bram Vanroy <Bram.Vanroy@UGent.be>
2022-06-13 16:44:06 +0200

    Add Ray's scope to training arguments (#17629)
    * allow scope from trainer arg
    
    * add ray_scope to training args
    
    * escape double quotes
    
    * make style && quality
    
    * attempt to solve doc style issues
    
    * splitting up URLs for style
    
    * make fixup
    
    * Update src/transformers/training_args.py
    
    Co-authored-by: Antoni Baum <antoni.baum@protonmail.com>
    
    * make style
    
    Co-authored-by: Antoni Baum <antoni.baum@protonmail.com>

--- src/transformers/training_args.py
+++ src/transformers/training_args.py
@@ -456,6 +456,12 @@ class TrainingArguments:
         torchdynamo (`str`, *optional*):
             The token that is used to set the backend compiler for TorchDynamo. Possible choices are ["eager",
             "nvfuser]. This is an experimental API and subject to change.
+        ray_scope (`str`, *optional*, defaults to `"last"`):
+            The scope to use when doing hyperparameter search with Ray. By default, `"last"` will be used. Ray will
+            then use the last checkpoint of all trials, compare those, and select the best one. However, other options
+            are also available. See the [Ray documentation](
+            https://docs.ray.io/en/latest/tune/api_docs/analysis.html#ray.tune.ExperimentAnalysis.get_best_trial) for
+            more options.
     """
 
     output_dir: str = field(
@@ -914,6 +920,19 @@ class TrainingArguments:
                 " nvfuser path uses AOT Autograd and nvfuser compiler to optimize the models."
             ),
             "choices": ["eager", "nvfuser"],
+        },
+    )
+    ray_scope: Optional[str] = field(
+        default="last",
+        metadata={
+            "help": (
+                'The scope to use when doing hyperparameter search with Ray. By default, `"last"` will be used. Ray'
+                " will then use the last checkpoint of all trials, compare those, and select the best one. However,"
+                " other options are also available. See the Ray documentation"
+                " (https://docs.ray.io/en/latest/tune/api_docs/analysis.html"
+                "#ray.tune.ExperimentAnalysis.get_best_trial)"
+                " for more options."
+            )
         },
     )
 
