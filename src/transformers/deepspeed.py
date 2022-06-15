6dd70209dd196d25f0eaa3ba772a7be92727c8c4
Sourab Mangrulkar <13534540+pacman100@users.noreply.github.com>
2022-06-15 20:32:30 +0530

    addressing nits

--- src/transformers/deepspeed.py
+++ src/transformers/deepspeed.py
@@ -38,11 +38,13 @@ def is_deepspeed_available():
 
 class HfDeepSpeedConfig(DeepSpeedConfig):
     """
-    This object contains a DeepSpeed configuration dictionary and can be quickly queried for things like zero stage. A
-    `weakref` of this object is stored in the module's globals to be able to access the config from areas where things
+    This object contains a DeepSpeed configuration dictionary and can be quickly queried for things like zero stage.
+
+    A `weakref` of this object is stored in the module's globals to be able to access the config from areas where things
     like the Trainer object is not available (e.g. `from_pretrained` and `_get_resized_embeddings`). Therefore it's
-    important that this object remains alive while the program is still running. [`Trainer`] uses the
-    `HfTrainerDeepSpeedConfig` subclass instead. That subclass has logic to sync the configuration with values of
+    important that this object remains alive while the program is still running.
+
+    [`Trainer`] uses the `HfTrainerDeepSpeedConfig` subclass instead. That subclass has logic to sync the configuration with values of
     [`TrainingArguments`] by replacing special placeholder values: `"auto"`. Without this special logic the DeepSpeed
     configuration is not modified in any way.
 
@@ -51,6 +53,7 @@ class HfDeepSpeedConfig(DeepSpeedConfig):
     """
 
     def __init__(self, config_file_or_dict):
+        # set global weakref object
         set_hf_deepspeed_config(self)
         dep_version_check("deepspeed")
         super().__init__(config_file_or_dict)
