704c3367064dd6ea77b0419dee1d9d40ef3c5fb2
Santiago Castro <sacastro@umich.edu>
2022-06-15 08:29:54 -0700

    Update build.sh

--- .github/conda/build.sh
+++ .github/conda/build.sh
@@ -1,1 +1,1 @@
-$PYTHON setup.py install --single-version-externally-managed --record=record.txt  # Python command to install the script.
+$PYTHON setup.py install     # Python command to install the script.
