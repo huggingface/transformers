a0103140e1c77b810ffcb735192968bc03be3e1f
Lysandre Debut <lysandre.debut@reseau.eseo.fr>
2022-06-16 20:37:23 +0200

    pin hfh (revert before merge)

--- setup.py
+++ setup.py
@@ -115,7 +115,7 @@ _deps = [
     "fugashi>=1.0",
     "GitPython<3.1.19",
     "hf-doc-builder>=0.3.0",
-    "huggingface-hub>=0.1.0,<1.0",
+    "huggingface-hub==0.8.0.rc2",
     "importlib_metadata",
     "ipadic>=1.0.0,<2.0",
     "isort>=5.5.4",
