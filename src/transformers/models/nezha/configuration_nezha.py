f67d5789f9d239de77a28a077041bd9cf46a4d1a
Sijun He <sijun.he@hotmail.com>
2022-06-20 00:02:13 +0800

    fix doc

--- src/transformers/models/nezha/configuration_nezha.py
+++ src/transformers/models/nezha/configuration_nezha.py
@@ -6,59 +6,60 @@ NEZHA_PRETRAINED_CONFIG_ARCHIVE_MAP = {}
 
 class NeZhaConfig(PretrainedConfig):
     r"""
-    This is the configuration class to store the configuration of an :class:`~transformers.NeZhaModel`. It is used to
+    This is the configuration class to store the configuration of an [`NeZhaModel`]. It is used to
     instantiate an NeZha model according to the specified arguments, defining the model architecture. Instantiating a
     configuration with the defaults will yield a similar configuration to that of the NeZha base architecture.
 
-    Configuration objects inherit from :class:`~transformers.PretrainedConfig` and can be used to control the model
-    outputs. Read the documentation from :class:`~transformers.PretrainedConfig` for more information.
+    Configuration objects inherit from [`PretrainedConfig`] and can be used to control the model
+    outputs. Read the documentation from [`PretrainedConfig`] for more information.
 
 
     Args:
-        vocab_size (:obj:`int`, optional, defaults to 21128):
+        vocab_size (`int`, optional, defaults to 21128):
             Vocabulary size of the ALBERT model. Defines the different tokens that can be represented by the
-            `inputs_ids` passed to the forward method of :class:`~transformers.NeZhaModel`.
-        embedding_size (:obj:`int`, optional, defaults to 128):
+            *inputs_ids* passed to the forward method of [`NeZhaModel`].
+        embedding_size (`int`, optional, defaults to 128):
             Dimensionality of vocabulary embeddings.
-        hidden_size (:obj:`int`, optional, defaults to 768):
+        hidden_size (`int`, optional, defaults to 768):
             Dimensionality of the encoder layers and the pooler layer.
-        num_hidden_layers (:obj:`int`, optional, defaults to 12):
+        num_hidden_layers (`int`, optional, defaults to 12):
             Number of hidden layers in the Transformer encoder.
-        num_attention_heads (:obj:`int`, optional, defaults to 12):
+        num_attention_heads (`int`, optional, defaults to 12):
             Number of attention heads for each attention layer in the Transformer encoder.
-        intermediate_size (:obj:`int`, optional, defaults to 3072):
+        intermediate_size (`int`, optional, defaults to 3072):
             The dimensionality of the "intermediate" (i.e., feed-forward) layer in the Transformer encoder.
-        hidden_act (:obj:`str` or :obj:`function`, optional, defaults to "gelu"):
+        hidden_act (`str` or `function`, optional, defaults to "gelu"):
             The non-linear activation function (function or string) in the encoder and pooler.
-        hidden_dropout_prob (:obj:`float`, optional, defaults to 0.1):
+        hidden_dropout_prob (`float`, optional, defaults to 0.1):
             The dropout probability for all fully connected layers in the embeddings, encoder, and pooler.
-        attention_probs_dropout_prob (:obj:`float`, optional, defaults to 0.1):
+        attention_probs_dropout_prob (`float`, optional, defaults to 0.1):
             The dropout ratio for the attention probabilities.
-        max_position_embeddings (:obj:`int`, optional, defaults to 512):
+        max_position_embeddings (`int`, optional, defaults to 512):
             The maximum sequence length that this model might ever be used with. Typically set this to something large
             (e.g., 512 or 1024 or 2048).
-        type_vocab_size (:obj:`int`, optional, defaults to 2):
-            The vocabulary size of the `token_type_ids` passed into :class:`~transformers.NeZhaModel`.
-        initializer_range (:obj:`float`, optional, defaults to 0.02):
+        type_vocab_size (`int`, optional, defaults to 2):
+            The vocabulary size of the *token_type_ids* passed into [`NeZhaModel`].
+        initializer_range (`float`, optional, defaults to 0.02):
             The standard deviation of the truncated_normal_initializer for initializing all weight matrices.
-        layer_norm_eps (:obj:`float`, optional, defaults to 1e-12):
+        layer_norm_eps (`float`, optional, defaults to 1e-12):
             The epsilon used by the layer normalization layers.
-        classifier_dropout (:obj:`float`, optional, defaults to 0.1):
+        classifier_dropout (`float`, optional, defaults to 0.1):
             The dropout ratio for attached classifiers.
 
-    Example::
+    Example:
 
-        from transformers import NeZhaConfig, NeZhaModel # Initializing an NeZha configuration nezha_config =
-        NeZhaConfig()
+    ```python
+    >>> from transformers import NeZhaConfig, NeZhaModel
 
-        # Initializing a model from the NeZha-base style configuration model = NeZhaModel(nezha_config)
+    >>> # Initializing an NeZha configuration
+    >>> configuration = NeZhaConfig()
 
-        # Accessing the model configuration configuration = model.config
+    >>> # Initializing a model from the NeZha-base style configuration model
+    >>> model = NeZhaModel(configuration)
 
-    Attributes:
-        pretrained_config_archive_map (Dict[str, str]):
-            A dictionary containing all the available pre-trained checkpoints.
-    """
+    >>> # Accessing the model configuration
+    >>> configuration = model.config
+    ```"""
 
     pretrained_config_archive_map = NEZHA_PRETRAINED_CONFIG_ARCHIVE_MAP
     model_type = "nezha"
