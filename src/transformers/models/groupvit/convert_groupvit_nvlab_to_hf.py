0655a3052fd01b2c8a3d4e59b0a6ef1fa5839ba8
Niels Rogge <nielsrogge@Nielss-MacBook-Pro.local>
2022-06-20 21:12:43 +0200

    Simplify conversion script

--- src/transformers/models/groupvit/convert_groupvit_nvlab_to_hf.py
+++ src/transformers/models/groupvit/convert_groupvit_nvlab_to_hf.py
@@ -22,10 +22,10 @@ URL: https://github.com/NVlabs/GroupViT
 import argparse
 
 import torch
-import torch.nn.functional as F
+from PIL import Image
 
-from regex import E
-from transformers import GroupViTConfig, GroupViTModel
+import requests
+from transformers import CLIPProcessor, GroupViTConfig, GroupViTModel
 
 
 def rename_key(name):
@@ -35,16 +35,18 @@ def rename_key(name):
     if "img_encoder.patch_embed.proj" in name:
         name = name.replace("img_encoder.patch_embed.proj", "vision_model.embeddings.patch_embeddings.projection")
     if "img_encoder.patch_embed.norm" in name:
-        name = name.replace("img_encoder.patch_embed.norm", "vision_model.embeddings.patch_embeddings.layernorm")
+        name = name.replace("img_encoder.patch_embed.norm", "vision_model.embeddings.layernorm")
     if "img_encoder.layers" in name:
         name = name.replace("img_encoder.layers", "vision_model.encoder.stages")
     if "blocks" in name and "res" not in name:
         name = name.replace("blocks", "layers")
     if "attn" in name and "pre_assign" not in name:
         name = name.replace("attn", "self_attn")
+    if "proj" in name and "self_attn" in name and "text" not in name:
+        name = name.replace("proj", "out_proj")
     if "norm1" in name:
         name = name.replace("norm1", "layer_norm1")
-    if "norm2" in name:
+    if "norm2" in name and "pre_assign" not in name:
         name = name.replace("norm2", "layer_norm2")
     if "img_encoder.norm" in name:
         name = name.replace("img_encoder.norm", "vision_model.layernorm")
@@ -80,13 +82,52 @@ def rename_key(name):
     return name
 
 
-def convert_state_dict(orig_state_dict):
+def convert_state_dict(orig_state_dict, config):
     for key in orig_state_dict.copy().keys():
         val = orig_state_dict.pop(key)
 
-        # attention requires special treatment
         if "qkv" in key:
-            pass
+            # weights and biases of the key, value and query projections of vision encoder's attention layers require special treatment:
+            # we need to split them up into separate matrices/vectors
+            key_split = key.split(".")
+            stage_num, layer_num = int(key_split[2]), int(key_split[4])
+            dim = config.vision_config.hidden_size
+            if "weight" in key:
+                orig_state_dict[
+                    f"vision_model.encoder.stages.{stage_num}.layers.{layer_num}.self_attn.q_proj.weight"
+                ] = val[:dim, :]
+                orig_state_dict[
+                    f"vision_model.encoder.stages.{stage_num}.layers.{layer_num}.self_attn.k_proj.weight"
+                ] = val[dim : dim * 2, :]
+                orig_state_dict[
+                    f"vision_model.encoder.stages.{stage_num}.layers.{layer_num}.self_attn.v_proj.weight"
+                ] = val[-dim:, :]
+            else:
+                orig_state_dict[
+                    f"vision_model.encoder.stages.{stage_num}.layers.{layer_num}.self_attn.q_proj.bias"
+                ] = val[:dim]
+                orig_state_dict[
+                    f"vision_model.encoder.stages.{stage_num}.layers.{layer_num}.self_attn.k_proj.bias"
+                ] = val[dim : dim * 2]
+                orig_state_dict[
+                    f"vision_model.encoder.stages.{stage_num}.layers.{layer_num}.self_attn.v_proj.bias"
+                ] = val[-dim:]
+        elif "in_proj" in key:
+            # weights and biases of the key, value and query projections of text encoder's attention layers require special treatment:
+            # we need to split them up into separate matrices/vectors
+            key_split = key.split(".")
+            layer_num = int(key_split[3])
+            dim = config.text_config.hidden_size
+            if "weight" in key:
+                orig_state_dict[f"text_model.encoder.layers.{layer_num}.self_attn.q_proj.weight"] = val[:dim, :]
+                orig_state_dict[f"text_model.encoder.layers.{layer_num}.self_attn.k_proj.weight"] = val[
+                    dim : dim * 2, :
+                ]
+                orig_state_dict[f"text_model.encoder.layers.{layer_num}.self_attn.v_proj.weight"] = val[-dim:, :]
+            else:
+                orig_state_dict[f"text_model.encoder.layers.{layer_num}.self_attn.q_proj.bias"] = val[:dim]
+                orig_state_dict[f"text_model.encoder.layers.{layer_num}.self_attn.k_proj.bias"] = val[dim : dim * 2]
+                orig_state_dict[f"text_model.encoder.layers.{layer_num}.self_attn.v_proj.bias"] = val[-dim:]
         else:
             new_name = rename_key(key)
             # squeeze if necessary
@@ -103,8 +144,15 @@ def convert_state_dict(orig_state_dict):
     return orig_state_dict
 
 
+# We will verify our results on an image of cute cats
+def prepare_img():
+    url = "http://images.cocodataset.org/val2017/000000039769.jpg"
+    im = Image.open(requests.get(url, stream=True).raw)
+    return im
+
+
 @torch.no_grad()
-def convert_groupvit_checkpoint(checkpoint_path, pytorch_dump_folder_path):
+def convert_groupvit_checkpoint(checkpoint_path, pytorch_dump_folder_path, push_to_hub=False):
     """
     Copy/paste/tweak model's weights to the Transformers design.
     """
@@ -112,20 +160,38 @@ def convert_groupvit_checkpoint(checkpoint_path, pytorch_dump_folder_path):
     model = GroupViTModel(config).eval()
 
     state_dict = torch.load(checkpoint_path, map_location="cpu")["model"]
-    new_state_dict = convert_state_dict(state_dict)
-    del new_state_dict["multi_label_logit_scale"]
-    model.load_state_dict(new_state_dict)
+    new_state_dict = convert_state_dict(state_dict, config)
+    missing_keys, unexpected_keys = model.load_state_dict(new_state_dict, strict=False)
+    assert missing_keys == ["text_model.embeddings.position_ids"]
+    assert unexpected_keys == ["multi_label_logit_scale"]
 
-    expected_logits = torch.tensor([])
+    # verify result
+    processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
+    image = prepare_img()
+    inputs = processor(text=["a photo of a cat", "a photo of a dog"], images=image, padding=True, return_tensors="pt")
+
+    with torch.no_grad():
+        outputs = model(**inputs)
+
+    expected_logits = torch.tensor([[13.3523, 6.3629]])
+    assert torch.allclose(outputs.logits_per_image, expected_logits, atol=1e-3)
 
     model.save_pretrained(pytorch_dump_folder_path)
-    print("Saved model to", pytorch_dump_folder_path)
+    print("Successfully saved model to", pytorch_dump_folder_path)
+
+    if push_to_hub:
+        print("Pushing to the hub...")
+        model_name = "groupvit-gccyfcc"
+        model.push_to_hub(model_name, organization="nielsr")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--pytorch_dump_folder_path", default=None, type=str, help="Path to the output PyTorch model.")
     parser.add_argument("--checkpoint_path", default=None, type=str, help="Path to GroupViT checkpoint")
+    parser.add_argument(
+        "--push_to_hub", action="store_true", help="Whether or not to push the converted model to the 🤗 hub."
+    )
     args = parser.parse_args()
 
-    convert_groupvit_checkpoint(args.checkpoint_path, args.pytorch_dump_folder_path)
+    convert_groupvit_checkpoint(args.checkpoint_path, args.pytorch_dump_folder_path, args.push_to_hub)
