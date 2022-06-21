117d474056633137b444755b5c63eb7f9b1e40e2
Quentin <quentin.herreros@gmail.com>
2022-06-21 10:45:09 +0200

    add: check shapes

--- src/transformers/models/detr/modeling_detr.py
+++ src/transformers/models/detr/modeling_detr.py
@@ -1238,6 +1238,8 @@ class DetrModel(DetrPreTrainedModel):
         >>> inputs = feature_extractor(images=image, return_tensors="pt")
         >>> outputs = model(**inputs)
         >>> last_hidden_states = outputs.last_hidden_state
+        >>> list(last_hidden_states.shape)
+        [1, 100, 256]
         ```"""
         output_attentions = output_attentions if output_attentions is not None else self.config.output_attentions
         output_hidden_states = (
@@ -1561,8 +1563,14 @@ class DetrForSegmentation(DetrPreTrainedModel):
         >>> outputs = model(**inputs)
         >>> # model predicts COCO classes, bounding boxes, and masks
         >>> logits = outputs.logits
+        >>> list(logits.shape)
+        [1, 100, 251]
         >>> bboxes = outputs.pred_boxes
+        >>> list(bboxes.shape)
+        [1, 100, 4]
         >>> masks = outputs.pred_masks
+        >>> list(masks.shape)
+        [1, 100, 200, 267]
         ```"""
 
         return_dict = return_dict if return_dict is not None else self.config.use_return_dict
