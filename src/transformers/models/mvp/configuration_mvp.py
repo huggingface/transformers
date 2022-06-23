1c704fc42f13dc0bc77b2e963a0bdbde5add202d
Steven_Tang1998 <1020139164@qq.com>
2022-06-23 09:18:08 +0800

    Remove specifying

--- src/transformers/models/mvp/configuration_mvp.py
+++ src/transformers/models/mvp/configuration_mvp.py
@@ -85,9 +85,6 @@ class MvpConfig(PretrainedConfig):
             Scale embeddings by diving by sqrt(d_model).
         use_cache (`bool`, *optional*, defaults to `True`):
             Whether or not the model should return the last key/values attentions (not used by all models).
-        forced_bos_token_id (`int`, *optional*, defaults to 0):
-            The id of the token to force as the first generated token after the `decoder_start_token_id`. Usually set
-            to `bos_token_id`.
         forced_eos_token_id (`int`, *optional*, defaults to 2):
             The id of the token to force as the last generated token when `max_length` is reached. Usually set to
             `eos_token_id`.
@@ -143,7 +140,6 @@ class MvpConfig(PretrainedConfig):
         eos_token_id=2,
         is_encoder_decoder=True,
         decoder_start_token_id=2,
-        forced_bos_token_id=0,
         forced_eos_token_id=2,
         use_prompt=False,
         prompt_length=100,
@@ -182,10 +178,16 @@ class MvpConfig(PretrainedConfig):
             eos_token_id=eos_token_id,
             is_encoder_decoder=is_encoder_decoder,
             decoder_start_token_id=decoder_start_token_id,
-            forced_bos_token_id=forced_bos_token_id,
             forced_eos_token_id=forced_eos_token_id,
             **kwargs,
         )
+
+        if self.forced_bos_token_id is None and kwargs.get("force_bos_token_to_be_generated", False):
+            self.forced_bos_token_id = self.bos_token_id
+            warnings.warn(
+                f"Please make sure the config includes `forced_bos_token_id={self.bos_token_id}` in future versions. "
+                "The config can simply be saved and uploaded again to be fixed."
+            )
 
 
 class MvpOnnxConfig(OnnxSeq2SeqConfigWithPast):
