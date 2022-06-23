0c9eba2c73abe8e51998db2a1fd8a3d88c95d88c
Steven_Tang1998 <1020139164@qq.com>
2022-06-23 09:20:05 +0800

    Remove specifying

--- src/transformers/models/mvp/configuration_mvp.py
+++ src/transformers/models/mvp/configuration_mvp.py
@@ -13,6 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """ MVP model configuration"""
+import warnings
 from collections import OrderedDict
 from typing import Any, Mapping, Optional
 
