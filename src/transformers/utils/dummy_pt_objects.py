c21c0dd678ec6b23d63d3dd47dcad379d09a31c6
Jerry Jiarui XU <xvjiarui0826@gmail.com>
2022-06-28 11:51:47 -0700

    Adding GroupViT Models (#17313)
    * add group vit and fixed test (except slow)
    
    * passing slow test
    
    * addressed some comments
    
    * fixed test
    
    * fixed style
    
    * fixed copy
    
    * fixed segmentation output
    
    * fixed test
    
    * fixed relative path
    
    * fixed copy
    
    * add ignore non auto configured
    
    * fixed docstring, add doc
    
    * fixed copies
    
    * Apply suggestions from code review
    
    merge suggestions
    
    Co-authored-by: NielsRogge <48327001+NielsRogge@users.noreply.github.com>
    Co-authored-by: Sylvain Gugger <35901082+sgugger@users.noreply.github.com>
    
    * resolve comment, renaming model
    
    * delete unused attr
    
    * use fix copies
    
    * resolve comments
    
    * fixed attn
    
    * remove unused vars
    
    * refactor tests
    
    * resolve final comments
    
    * add demo notebook
    
    * fixed inconsitent default
    
    * Apply suggestions from code review
    
    Co-authored-by: NielsRogge <48327001+NielsRogge@users.noreply.github.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: NielsRogge <48327001+NielsRogge@users.noreply.github.com>
    
    * rename stage->stages
    
    * Create single GroupViTEncoderLayer class
    
    * Update conversion script
    
    * Simplify conversion script
    
    * Remove cross-attention class in favor of GroupViTAttention
    
    * Convert other model as well, add processor to conversion script
    
    * addressing final comment
    
    * fixed args
    
    * Update src/transformers/models/groupvit/modeling_groupvit.py
    
    Co-authored-by: Sylvain Gugger <35901082+sgugger@users.noreply.github.com>
    
    Co-authored-by: NielsRogge <48327001+NielsRogge@users.noreply.github.com>
    Co-authored-by: Sylvain Gugger <35901082+sgugger@users.noreply.github.com>
    Co-authored-by: Niels Rogge <nielsrogge@Nielss-MacBook-Pro.local>

--- src/transformers/utils/dummy_pt_objects.py
+++ src/transformers/utils/dummy_pt_objects.py
@@ -2283,6 +2283,37 @@ class GPTJPreTrainedModel(metaclass=DummyObject):
         requires_backends(self, ["torch"])
 
 
+GROUPVIT_PRETRAINED_MODEL_ARCHIVE_LIST = None
+
+
+class GroupViTModel(metaclass=DummyObject):
+    _backends = ["torch"]
+
+    def __init__(self, *args, **kwargs):
+        requires_backends(self, ["torch"])
+
+
+class GroupViTPreTrainedModel(metaclass=DummyObject):
+    _backends = ["torch"]
+
+    def __init__(self, *args, **kwargs):
+        requires_backends(self, ["torch"])
+
+
+class GroupViTTextModel(metaclass=DummyObject):
+    _backends = ["torch"]
+
+    def __init__(self, *args, **kwargs):
+        requires_backends(self, ["torch"])
+
+
+class GroupViTVisionModel(metaclass=DummyObject):
+    _backends = ["torch"]
+
+    def __init__(self, *args, **kwargs):
+        requires_backends(self, ["torch"])
+
+
 HUBERT_PRETRAINED_MODEL_ARCHIVE_LIST = None
 
 
