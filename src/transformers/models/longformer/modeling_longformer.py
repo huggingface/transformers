db20ebf8eaed7bd092cd96d2d70a8d98d6f7a72b
Clémentine Fourrier <22726840+clefourrier@users.noreply.github.com>
2022-06-28 15:37:55 +0200

    Now ignores removed (dead) dict key for backward comp

--- src/transformers/models/longformer/modeling_longformer.py
+++ src/transformers/models/longformer/modeling_longformer.py
@@ -1385,7 +1385,7 @@ class LongformerPreTrainedModel(PreTrainedModel):
     config_class = LongformerConfig
     base_model_prefix = "longformer"
     supports_gradient_checkpointing = True
-    _keys_to_ignore_on_load_missing = [r"position_ids"]
+    _keys_to_ignore_on_load_unexpected = [r"position_ids"]
 
     def _init_weights(self, module):
         """Initialize the weights"""
