19647846adc06aed11f286df7011853659b5300c
Wei Wei <wwei6@fb.com>
2022-06-29 16:43:50 -0700

    update

--- tests/trainer/test_trainer.py
+++ tests/trainer/test_trainer.py
@@ -1765,6 +1765,7 @@ class TrainerIntegrationTest(TestCasePlus, TrainerIntegrationCommon):
         # fp16 has accuracy accuracy degradation
         self.assertLess(np.max(np.abs(t1 - t2)), 1e-3)
 
+
     @require_torch_non_multi_gpu
     @require_torchdynamo
     @require_torch_tensorrt_fx
