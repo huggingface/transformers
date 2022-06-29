60c0db758d8ce596a41caa1f57bebfb7cfb93677
Dobatymo <Dobatymo@users.noreply.github.com>
2022-06-29 15:00:45 +0800

    only a stupid typo, but it can lead to confusion

--- src/transformers/integrations.py
+++ src/transformers/integrations.py
@@ -54,7 +54,7 @@ def is_wandb_available():
     # any value of WANDB_DISABLED disables wandb
     if os.getenv("WANDB_DISABLED", "").upper() in ENV_VARS_TRUE_VALUES:
         logger.warning(
-            "Using the `WAND_DISABLED` environment variable is deprecated and will be removed in v5. Use the "
+            "Using the `WANDB_DISABLED` environment variable is deprecated and will be removed in v5. Use the "
             "--report_to flag to control the integrations used for logging result (for instance --report_to none)."
         )
         return False
