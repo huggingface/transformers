a94c0e74d780f878d4218792835d5334d31835ab
muellerzr <muellerzr@gmail.com>
2022-06-29 09:56:45 -0400

    Start small, trainer_utils

--- src/transformers/utils/import_utils.py
+++ src/transformers/utils/import_utils.py
@@ -396,20 +396,20 @@ def is_ftfy_available():
     return _ftfy_available
 
 
-def is_torch_tpu_available(check_device=True):
+def is_torch_tpu_available(check_device=False):
     "Checks if `torch_xla` is installed and potentially if a TPU is in the environment"
     if not _torch_available:
         return False
     if importlib.util.find_spec("torch_xla") is not None:
-        # if check_device:
-        #     # We need to check if `xla_device` can be found, will raise a RuntimeError if not
-        #     try:
-        #         import torch_xla.core.xla_model as xm
+        if check_device:
+            # We need to check if `xla_device` can be found, will raise a RuntimeError if not
+            try:
+                import torch_xla.core.xla_model as xm
 
-        #         _ = xm.xla_device()
-        #         return True
-        #     except RuntimeError:
-        #         return False
+                _ = xm.xla_device()
+                return True
+            except RuntimeError:
+                return False
         return True
     return False
 
