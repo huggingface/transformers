2ebd575816d6416d6484e4477184f4e3019aa006
Kashif Rasul <kashif.rasul@gmail.com>
2022-06-30 17:37:33 +0200

    initial files

---
+++ src/transformers/models/time_series_transformer/modeling_time_series_transformer.py
@@ -0,0 +1,27 @@
+# coding=utf-8
+# Copyright 2022 The Trajectory Transformers paper authors and The HuggingFace Inc. team. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+""" PyTorch Time Series Transformer model."""
+
+import numpy as np
+import torch
+import torch.utils.checkpoint
+from torch import nn
+from torch.nn import functional as F
+
+from ...modeling_utils import PreTrainedModel
+
+
+class TimeSeriesTransformerPreTrainedModel(PreTrainedModel):
+    pass
