861c01b23a3f05dd3871eff07730452133763d9c
NielsRogge <niels.rogge1@gmail.com>
2022-07-07 08:22:03 +0000

    Remove img_metas default value

--- src/transformers/models/convnext_maskrcnn/convert_convnext_maskrcnn_to_pytorch.py
+++ src/transformers/models/convnext_maskrcnn/convert_convnext_maskrcnn_to_pytorch.py
@@ -21,6 +21,7 @@ import argparse
 import json
 from pathlib import Path
 
+import numpy as np
 import torch
 import torchvision.transforms as T
 from PIL import Image
@@ -131,10 +132,15 @@ def convert_convnext_maskrcnn_checkpoint(checkpoint_path, pytorch_dump_folder_pa
 
     pixel_values = transform(image).unsqueeze(0)
 
-    outputs = model(pixel_values, output_hidden_states=True)
+    img_metas = [
+        dict(
+            img_shape=(800, 1067, 3),
+            scale_factor=np.array([1.6671875, 1.6666666, 1.6671875, 1.6666666], dtype=np.float32),
+            ori_shape=(480, 640, 3),
+        )
+    ]
 
-    # for i in outputs.hidden_states[1:]:
-    #     print(i.shape)
+    outputs = model(pixel_values, img_metas=img_metas, output_hidden_states=True)
 
     expected_slice = torch.tensor(
         [[-0.0836, -0.1298, -0.1237], [-0.0743, -0.1090, -0.0873], [-0.0231, 0.0851, 0.0792]]
