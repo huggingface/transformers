95113d136508dfef192a29d23344e941735d1a1d
Yulv-git <34329208+Yulv-git@users.noreply.github.com>
2022-07-11 17:00:13 +0800

    Fix some typos. (#17560)
    * Fix some typos.
    
    Signed-off-by: Yulv-git <yulvchi@qq.com>
    
    * Fix typo.
    
    Signed-off-by: Yulv-git <yulvchi@qq.com>
    
    * make fixup.

--- src/transformers/models/led/modeling_tf_led.py
+++ src/transformers/models/led/modeling_tf_led.py
@@ -2330,7 +2330,7 @@ class TFLEDForConditionalGeneration(TFLEDPreTrainedModel):
         super().__init__(config, *inputs, **kwargs)
         self.led = TFLEDMainLayer(config, name="led")
         self.use_cache = config.use_cache
-        # final_bias_logits is registered as a buffer in pytorch, so not trainable for the the sake of consistency.
+        # final_bias_logits is registered as a buffer in pytorch, so not trainable for the sake of consistency.
         self.final_logits_bias = self.add_weight(
             name="final_logits_bias", shape=[1, config.vocab_size], initializer="zeros", trainable=False
         )
