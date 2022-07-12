e116e21b4d708c8a4bfb70566e0c7ec14416dc48
younesbelkada <younesbelkada@gmail.com>
2022-07-12 14:40:19 +0000

    revert bad formatting

--- src/transformers/__init__.py
+++ src/transformers/__init__.py
@@ -118,8 +118,8 @@ _import_structure = {
         "load_tf2_weights_in_pytorch_model",
     ],
     "models": [],
-    "models.albert": ["ALBERT_PRETRAINED_CONFIG_ARCHIVE_MAP", "AlbertConfig"],
     # Models
+    "models.albert": ["ALBERT_PRETRAINED_CONFIG_ARCHIVE_MAP", "AlbertConfig"],
     "models.auto": [
         "ALL_PRETRAINED_CONFIG_ARCHIVE_MAP",
         "CONFIG_MAPPING",
