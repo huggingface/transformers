3936a0ceb48267928747999d9deadfd6876d7e70
Amy Roberts <amyeroberts@users.noreply.github.com>
2022-07-13 16:16:44 +0100

    Fixup

--- src/transformers/models/deit/modeling_tf_deit.py
+++ src/transformers/models/deit/modeling_tf_deit.py
@@ -795,9 +795,7 @@ class TFDeiTForMaskedImageModeling(TFDeiTPreTrainedModel):
         >>> image = Image.open(requests.get(url, stream=True).raw)
 
         >>> feature_extractor = DeiTFeatureExtractor.from_pretrained("facebook/deit-base-distilled-patch16-224")
-        >>> model = TFDeiTForMaskedImageModeling.from_pretrained(
-        ...     "facebook/deit-base-distilled-patch16-224"
-        ... )
+        >>> model = TFDeiTForMaskedImageModeling.from_pretrained("facebook/deit-base-distilled-patch16-224")
 
         >>> num_patches = (model.config.image_size // model.config.patch_size) ** 2
         >>> pixel_values = feature_extractor(images=image, return_tensors="tf").pixel_values
@@ -924,9 +922,7 @@ class TFDeiTForImageClassification(TFDeiTPreTrainedModel, TFSequenceClassificati
         >>> # note: we are loading a TFDeiTForImageClassificationWithTeacher from the hub here,
         >>> # so the head will be randomly initialized, hence the predictions will be random
         >>> feature_extractor = DeiTFeatureExtractor.from_pretrained("facebook/deit-base-distilled-patch16-224")
-        >>> model = TFDeiTForImageClassification.from_pretrained(
-        ...     "facebook/deit-base-distilled-patch16-224"
-        ... )
+        >>> model = TFDeiTForImageClassification.from_pretrained("facebook/deit-base-distilled-patch16-224")
 
         >>> inputs = feature_extractor(images=image, return_tensors="tf")
         >>> outputs = model(**inputs)
