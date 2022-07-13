8581a798c0a48fca07b29ce2ca2ef55adcae8c7e
amyeroberts <amy@huggingface.co>
2022-07-13 18:04:08 +0100

    Add TF DeiT implementation (#17806)
    * Initial TF DeiT implementation
    
    * Fix copies naming issues
    
    * Fix up + docs
    
    * Properly same main layer
    
    * Name layers properly
    
    * Initial TF DeiT implementation
    
    * Fix copies naming issues
    
    * Fix up + docs
    
    * Properly same main layer
    
    * Name layers properly
    
    * Fixup
    
    * Fix import
    
    * Fix import
    
    * Fix import
    
    * Fix weight loading for tests whilst not on hub
    
    * Add doc tests and remove to_2tuple
    
    * Add back to_2tuple
    Removing to_2tuple results in many downstream changes needed because of the copies checks
    
    * Incorporate updates in Improve vision models #17731 PR
    
    * Don't hard code num_channels
    
    * Copy PyTorch DeiT embeddings and remove pytorch operations with mask
    
    * Fix patch embeddings & tidy up
    
    * Update PixelShuffle to move logic into class layer
    
    * Update doc strings - remove PT references
    
    * Use NHWC format in internal layers
    
    * Fix up
    
    * Use linear activation layer
    
    * Remove unused import
    
    * Apply suggestions from code review
    
    Co-authored-by: Sylvain Gugger <35901082+sgugger@users.noreply.github.com>
    Co-authored-by: NielsRogge <48327001+NielsRogge@users.noreply.github.com>
    
    Co-authored-by: NielsRogge <48327001+NielsRogge@users.noreply.github.com>
    Co-authored-by: Sylvain Gugger <35901082+sgugger@users.noreply.github.com>
    
    * Move dataclass to top of file
    
    * Remove from_pt now weights on hub
    
    * Fixup
    
    Co-authored-by: NielsRogge <48327001+NielsRogge@users.noreply.github.com>
    Co-authored-by: Sylvain Gugger <35901082+sgugger@users.noreply.github.com>
    Co-authored-by: Amy Roberts <amyeroberts@users.noreply.github.com>

--- src/transformers/modeling_tf_outputs.py
+++ src/transformers/modeling_tf_outputs.py
@@ -686,6 +686,33 @@ class TFSemanticSegmenterOutput(ModelOutput):
 
 
 @dataclass
+class TFImageClassifierOutput(ModelOutput):
+    """
+    Base class for outputs of image classification models.
+
+    Args:
+        loss (`tf.Tensor` of shape `(1,)`, *optional*, returned when `labels` is provided):
+            Classification (or regression if config.num_labels==1) loss.
+        logits (`tf.Tensor` of shape `(batch_size, config.num_labels)`):
+            Classification (or regression if config.num_labels==1) scores (before SoftMax).
+        hidden_states (`tuple(tf.Tensor)`, *optional*, returned when `output_hidden_states=True` is passed or when `config.output_hidden_states=True`):
+            Tuple of `tf.Tensor` (one for the output of the embeddings, if the model has an embedding layer, + one for
+            the output of each stage) of shape `(batch_size, sequence_length, hidden_size)`. Hidden-states (also called
+            feature maps) of the model at the output of each stage.
+        attentions (`tuple(tf.Tensor)`, *optional*, returned when `output_attentions=True` is passed or when `config.output_attentions=True`):
+            Tuple of `tf.Tensor` (one for each layer) of shape `(batch_size, num_heads, patch_size, sequence_length)`.
+
+            Attentions weights after the attention softmax, used to compute the weighted average in the self-attention
+            heads.
+    """
+
+    loss: Optional[tf.Tensor] = None
+    logits: tf.Tensor = None
+    hidden_states: Optional[Tuple[tf.Tensor]] = None
+    attentions: Optional[Tuple[tf.Tensor]] = None
+
+
+@dataclass
 class TFMultipleChoiceModelOutput(ModelOutput):
     """
     Base class for outputs of multiple choice models.
--- src/transformers/models/deit/__init__.py
+++ src/transformers/models/deit/__init__.py
@@ -17,7 +17,13 @@
 # limitations under the License.
 from typing import TYPE_CHECKING
 
-from ...utils import OptionalDependencyNotAvailable, _LazyModule, is_torch_available, is_vision_available
+from ...utils import (
+    OptionalDependencyNotAvailable,
+    _LazyModule,
+    is_tf_available,
+    is_torch_available,
+    is_vision_available,
+)
 
 
 _import_structure = {"configuration_deit": ["DEIT_PRETRAINED_CONFIG_ARCHIVE_MAP", "DeiTConfig", "DeiTOnnxConfig"]}
@@ -45,6 +51,21 @@ else:
         "DeiTPreTrainedModel",
     ]
 
+try:
+    if not is_tf_available():
+        raise OptionalDependencyNotAvailable()
+except OptionalDependencyNotAvailable:
+    pass
+else:
+    _import_structure["modeling_tf_deit"] = [
+        "TF_DEIT_PRETRAINED_MODEL_ARCHIVE_LIST",
+        "TFDeiTForImageClassification",
+        "TFDeiTForImageClassificationWithTeacher",
+        "TFDeiTForMaskedImageModeling",
+        "TFDeiTModel",
+        "TFDeiTPreTrainedModel",
+    ]
+
 
 if TYPE_CHECKING:
     from .configuration_deit import DEIT_PRETRAINED_CONFIG_ARCHIVE_MAP, DeiTConfig, DeiTOnnxConfig
@@ -70,6 +91,21 @@ if TYPE_CHECKING:
             DeiTForMaskedImageModeling,
             DeiTModel,
             DeiTPreTrainedModel,
+        )
+
+    try:
+        if not is_tf_available():
+            raise OptionalDependencyNotAvailable()
+    except OptionalDependencyNotAvailable:
+        pass
+    else:
+        from .modeling_tf_deit import (
+            TF_DEIT_PRETRAINED_MODEL_ARCHIVE_LIST,
+            TFDeiTForImageClassification,
+            TFDeiTForImageClassificationWithTeacher,
+            TFDeiTForMaskedImageModeling,
+            TFDeiTModel,
+            TFDeiTPreTrainedModel,
         )
 
 
