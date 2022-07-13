incorrect shape assertion for collator & function to remove excess nesting from DatasetDict

src/transformers/data/data_collator.py
@@ -21,7 +21,6 @@ from typing import Any, Callable, Dict, List, NewType, Optional, Tuple, Union
 from ..models.bert import BertTokenizer, BertTokenizerFast
 from ..tokenization_utils_base import PreTrainedTokenizerBase
 from ..utils import PaddingStrategy
-from ..utils.generic import remove_excess_nesting
 
 
 InputDataClass = NewType("InputDataClass", Any)
@@ -559,8 +558,8 @@ class DataCollatorForSeq2Seq:
         import numpy as np
 
         if return_tensors is None:
-            return_tensors = self.return_tensors
-        labels = [remove_excess_nesting(feature["labels"]) for feature in features] if "labels" in features[0].keys() else None
+            return_tensors = self.return_tensors            
+        labels = [feature["labels"] for feature in features] if "labels" in features[0].keys() else None
         # We have to pad the labels before calling `tokenizer.pad` as this method won't pad them and needs them of the
         # same length to return tensors.
         if labels is not None:
@@ -574,7 +573,14 @@ class DataCollatorForSeq2Seq:
 
             padding_side = self.tokenizer.padding_side
             for feature in features:
-                feature["labels"] = remove_excess_nesting(feature["labels"])
+
+                np_input_ids = np.array(feature["input_ids"])
+                assert np_input_ids.shape[0] != 1, f"""
+                Feature shape cannot be collated: expected shape=( > 1, ... ), found shape={str(np_input_ids.shape)}.
+                You may want to remove excessively nested layers from your features.
+                Or simply wrap you `DatasetDict` with `remove_excess_nesting` function from `transformers.utils.generic`.
+                """
+
                 remainder = [self.label_pad_token_id] * (max_label_length - len(feature["labels"]))
                 if isinstance(feature["labels"], list):
                     feature["labels"] = (

src/transformers/utils/generic.py
@@ -22,6 +22,7 @@ from contextlib import ExitStack
 from dataclasses import fields
 from enum import Enum
 from typing import Any, ContextManager, List, Tuple
+from datasets.dataset_dict import DatasetDict
 
 import numpy as np
 
@@ -326,11 +327,21 @@ def flatten_dict(d: MutableMapping, parent_key: str = "", delimiter: str = "."):
 
     return dict(_flatten_dict(d, parent_key, delimiter))
 
-def remove_excess_nesting(arr: List[Any]) -> List[Any]:
-    """Flattens any leading dimensions of shape 1"""
 
-    arr = np.array(arr)
-    for _ in range(arr.ndim-1):
-        arr = arr[0] if arr.shape[0] == 1 else arr
+def remove_excess_nesting(dataset: DatasetDict) -> DatasetDict:
+    """
+    Flattens any leading dimensions of shape 1 for all features 
+    in the dataset.
+    """
+    def clean_row_nesting(row):
+        row_keys = row.keys()
+        for key in row_keys:
+            arr = np.array(row[key])
+            for _ in range(arr.ndim-1):
+                arr = arr[0] if arr.shape[0] == 1 else arr
+            row[key] = arr.tolist()
+        return row
 
-    return arr.tolist()
+    claned_dataset = dataset.map(clean_row_nesting)
+
+    return claned_dataset