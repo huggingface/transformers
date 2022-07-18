fix causal_mask - avoid having both `-inf` and `dtype.min` in causal mask due to addition

src/transformers/models/bloom/modeling_bloom.py
@@ -595,11 +595,13 @@ class BloomModel(BloomPreTrainedModel):
 
         if attention_mask is not None:
             # [bsz, seq_len] -> [bsz, 1, tgt_seq_len, src_seq_len]
-            expanded_attn_mask = _expand_mask(attention_mask, inputs_embeds.dtype, tgt_len=input_shape[-1])
-            combined_attention_mask = (
-                expanded_attn_mask if combined_attention_mask is None else expanded_attn_mask + combined_attention_mask
+            if combined_attention_mask is None:
+                combined_attention_mask = _expand_mask(attention_mask, inputs_embeds.dtype, tgt_len=input_shape[-1])
+            else:
+                combined_attention_mask.masked_fill_(
+                    ~attention_mask[:, None, None, :].to(torch.bool).expand_as(combined_attention_mask),
+                    -torch.inf,
             )
-
         return combined_attention_mask
 
     def set_input_embeddings(self, new_embeddings):