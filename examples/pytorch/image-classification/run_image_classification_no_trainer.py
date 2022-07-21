de23b1b02b06fa3f9a1ad6230c030f92276d9920
Zach Mueller <muellerzr@gmail.com>
2022-07-21 07:59:55 -0400

    Start updating all no trainer examples

--- examples/pytorch/image-classification/run_image_classification_no_trainer.py
+++ examples/pytorch/image-classification/run_image_classification_no_trainer.py
@@ -209,7 +209,11 @@ def main():
     # If we're using tracking, we also need to initialize it here and it will by default pick up all supported trackers
     # in the environment
     accelerator = (
-        Accelerator(log_with=args.report_to, logging_dir=args.output_dir) if args.with_tracking else Accelerator()
+        Accelerator(
+            log_with=args.report_to, 
+            logging_dir=args.output_dir, 
+            gradient_accumulation_steps=args.gradient_accumulation_steps
+        ) if args.with_tracking else Accelerator(gradient_accumulation_steps=args.gradient_accumulation_steps)
     )
     logger.info(accelerator.state)
     # Make one log on every process with the configuration for debugging.
@@ -463,19 +467,19 @@ def main():
                 if resume_step is not None and step < resume_step:
                     completed_steps += 1
                     continue
-            outputs = model(**batch)
-            loss = outputs.loss
-            # We keep track of the loss at each epoch
-            if args.with_tracking:
-                total_loss += loss.detach().float()
-            loss = loss / args.gradient_accumulation_steps
-            accelerator.backward(loss)
-            if step % args.gradient_accumulation_steps == 0 or step == len(train_dataloader) - 1:
+            with accelerator.accumulate(model):
+                outputs = model(**batch)
+                loss = outputs.loss
+                # We keep track of the loss at each epoch
+                if args.with_tracking:
+                    total_loss += loss.detach().float()
+                accelerator.backward(loss)
                 optimizer.step()
                 lr_scheduler.step()
                 optimizer.zero_grad()
-                progress_bar.update(1)
-                completed_steps += 1
+                if step % args.gradient_accumulation_steps == 0 or step == len(train_dataloader) - 1:
+                    progress_bar.update(1)
+                    completed_steps += 1
 
             if isinstance(checkpointing_steps, int):
                 if completed_steps % checkpointing_steps == 0:
@@ -509,14 +513,7 @@ def main():
             with torch.no_grad():
                 outputs = model(**batch)
             predictions = outputs.logits.argmax(dim=-1)
-            predictions, references = accelerator.gather((predictions, batch["labels"]))
-            # If we are in a multiprocess environment, the last batch has duplicates
-            if accelerator.num_processes > 1:
-                if step == len(eval_dataloader) - 1:
-                    predictions = predictions[: len(eval_dataloader.dataset) - samples_seen]
-                    references = references[: len(eval_dataloader.dataset) - samples_seen]
-                else:
-                    samples_seen += references.shape[0]
+            predictions, references = accelerator.gather_for_metrics((predictions, batch["labels"]))
             metric.add_batch(
                 predictions=predictions,
                 references=references,
@@ -532,8 +529,7 @@ def main():
                     "train_loss": total_loss.item() / len(train_dataloader),
                     "epoch": epoch,
                     "step": completed_steps,
-                },
-                step=completed_steps,
+                }
             )
 
         if args.push_to_hub and epoch < args.num_train_epochs - 1:
