d9d72ebebf2ec0084d0496fc1f872740c67ecfbc
ydshieh <ydshieh@users.noreply.github.com>
2022-07-21 20:20:50 +0200

    with 64 models

---
+++ mem.py
@@ -0,0 +1,8 @@
+import os
+import time
+os.system("rm -rf mem.txt")
+while True:
+    os.system("ps aux --sort pmem >> mem.txt")
+    os.system("date >> mem.txt")
+    os.system('echo "================" >> mem.txt')
+    time.sleep(5)
