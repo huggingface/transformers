e47f5b4d86684f641124d386fcbbec8d9f44b0bb
ydshieh <ydshieh@users.noreply.github.com>
2022-07-21 20:28:08 +0200

    with 64 models

--- mem.py
+++ mem.py
@@ -3,6 +3,9 @@ import time
 os.system("rm -rf mem.txt")
 while True:
     os.system("ps aux --sort pmem >> mem.txt")
+    os.system('echo "----------" >> mem.txt')
+    os.system("ls -l reports/tests_torch/ >> mem.txt")
+    os.system('echo "----------" >> mem.txt')
     os.system("date >> mem.txt")
     os.system('echo "================" >> mem.txt')
     time.sleep(5)
