806e6bae94d16b513b4de24974131ed987872d9a
Raghavan <oneraghavan@gmail.com>
2022-07-21 17:59:42 +0530

    Fix command for after running doc tests locally

--- utils/prepare_for_doc_test.py
+++ utils/prepare_for_doc_test.py
@@ -36,7 +36,7 @@
 
     Afterwards you should revert the changes by running
 
-    ```python utils/prepare_for_doc_test.py src doc --remove_new_line```
+    ```python utils/prepare_for_doc_test.py src docs --remove_new_line```
 """
 
 import argparse
