8cd4c69e1b74aafe00fc2853a8f1127358d20b29
nandwalritik <ritiknandwal021@gmail.com>
2022-07-21 17:58:33 +0530

    Add suggested changes

--- src/transformers/models/swinv2/configuration_swinv2.py
+++ src/transformers/models/swinv2/configuration_swinv2.py
@@ -29,68 +29,68 @@ SWINV2_PRETRAINED_CONFIG_ARCHIVE_MAP = {
 
 class Swinv2Config(PretrainedConfig):
     r"""
-        This is the configuration class to store the configuration of a [`Swinv2Model`]. It is used to instantiate a
-        Swin v2 model according to the specified arguments, defining the model architecture. Instantiating a
-        configuration with the defaults will yield a similar configuration to that of the Swin v2
-        [microsoft/swinv2_tiny_patch4_windows8_256](https://huggingface.co/microsoft/swinv2_tiny_patch4_windows8_256)
-        architecture.
+    This is the configuration class to store the configuration of a [`Swinv2Model`]. It is used to instantiate a Swin
+    Transformer v2 model according to the specified arguments, defining the model architecture. Instantiating a
+    configuration with the defaults will yield a similar configuration to that of the Swin Transformer v2
+    [microsoft/swinv2_tiny_patch4_windows8_256](https://huggingface.co/microsoft/swinv2_tiny_patch4_windows8_256)
+    architecture.
 
-        Configuration objects inherit from [`PretrainedConfig`] and can be used to control the model outputs. Read the
-        documentation from [`PretrainedConfig`] for more information.
+    Configuration objects inherit from [`PretrainedConfig`] and can be used to control the model outputs. Read the
+    documentation from [`PretrainedConfig`] for more information.
 
-        Args:
-            image_size (`int`, *optional*, defaults to 224):
-                The size (resolution) of each image.
-            patch_size (`int`, *optional*, defaults to 4):
-                The size (resolution) of each patch.
-            num_channels (`int`, *optional*, defaults to 3):
-                The number of input channels.
-            embed_dim (`int`, *optional*, defaults to 96):
-                Dimensionality of patch embedding.
-            depths (`list(int)`, *optional*, defaults to [2, 2, 6, 2]):
-                Depth of each layer in the Transformer encoder.
-            num_heads (`list(int)`, *optional*, defaults to [3, 6, 12, 24]):
-                Number of attention heads in each layer of the Transformer encoder.
-            window_size (`int`, *optional*, defaults to 7):
-                Size of windows.
-            mlp_ratio (`float`, *optional*, defaults to 4.0):
-                Ratio of MLP hidden dimensionality to embedding dimensionality.
-            qkv_bias (`bool`, *optional*, defaults to True):
-                Whether or not a learnable bias should be added to the queries, keys and values.
-            hidden_dropout_prob (`float`, *optional*, defaults to 0.0):
-                The dropout probability for all fully connected layers in the embeddings and encoder.
-            attention_probs_dropout_prob (`float`, *optional*, defaults to 0.0):
-                The dropout ratio for the attention probabilities.
-            drop_path_rate (`float`, *optional*, defaults to 0.1):
-                Stochastic depth rate.
-            hidden_act (`str` or `function`, *optional*, defaults to `"gelu"`):
-                The non-linear activation function (function or string) in the encoder. If string, `"gelu"`, `"relu"`,
-                `"selu"` and `"gelu_new"` are supported.
-            use_absolute_embeddings (`bool`, *optional*, defaults to False):
-                Whether or not to add absolute position embeddings to the patch embeddings.
-            patch_norm (`bool`, *optional*, defaults to True):
-                Whether or not to add layer normalization after patch embedding.
-            initializer_range (`float`, *optional*, defaults to 0.02):
-                The standard deviation of the truncated_normal_initializer for initializing all weight matrices.
-            layer_norm_eps (`float`, *optional*, defaults to 1e-12):
-                The epsilon used by the layer normalization layers.
-            encoder_stride (`int`, `optional`, defaults to 32):
-                Factor to increase the spatial resolution by in the decoder head for masked image modeling.
+    Args:
+        image_size (`int`, *optional*, defaults to 224):
+            The size (resolution) of each image.
+        patch_size (`int`, *optional*, defaults to 4):
+            The size (resolution) of each patch.
+        num_channels (`int`, *optional*, defaults to 3):
+            The number of input channels.
+        embed_dim (`int`, *optional*, defaults to 96):
+            Dimensionality of patch embedding.
+        depths (`list(int)`, *optional*, defaults to [2, 2, 6, 2]):
+            Depth of each layer in the Transformer encoder.
+        num_heads (`list(int)`, *optional*, defaults to [3, 6, 12, 24]):
+            Number of attention heads in each layer of the Transformer encoder.
+        window_size (`int`, *optional*, defaults to 7):
+            Size of windows.
+        mlp_ratio (`float`, *optional*, defaults to 4.0):
+            Ratio of MLP hidden dimensionality to embedding dimensionality.
+        qkv_bias (`bool`, *optional*, defaults to True):
+            Whether or not a learnable bias should be added to the queries, keys and values.
+        hidden_dropout_prob (`float`, *optional*, defaults to 0.0):
+            The dropout probability for all fully connected layers in the embeddings and encoder.
+        attention_probs_dropout_prob (`float`, *optional*, defaults to 0.0):
+            The dropout ratio for the attention probabilities.
+        drop_path_rate (`float`, *optional*, defaults to 0.1):
+            Stochastic depth rate.
+        hidden_act (`str` or `function`, *optional*, defaults to `"gelu"`):
+            The non-linear activation function (function or string) in the encoder. If string, `"gelu"`, `"relu"`,
+            `"selu"` and `"gelu_new"` are supported.
+        use_absolute_embeddings (`bool`, *optional*, defaults to False):
+            Whether or not to add absolute position embeddings to the patch embeddings.
+        patch_norm (`bool`, *optional*, defaults to True):
+            Whether or not to add layer normalization after patch embedding.
+        initializer_range (`float`, *optional*, defaults to 0.02):
+            The standard deviation of the truncated_normal_initializer for initializing all weight matrices.
+        layer_norm_eps (`float`, *optional*, defaults to 1e-12):
+            The epsilon used by the layer normalization layers.
+        encoder_stride (`int`, `optional`, defaults to 32):
+            Factor to increase the spatial resolution by in the decoder head for masked image modeling.
 
-    Example:
+        Example:
 
-        ```python
-        >>> from transformers import Swinv2Config, Swinv2Model
+    ```python
+    >>> from transformers import Swinv2Config, Swinv2Model
 
-        >>> # Initializing a Swinv2 microsoft/swinv2_tiny_patch4_windows8_256 style configuration
-        >>> configuration = Swinv2Config()
+    >>> # Initializing a Swinv2 microsoft/swinv2_tiny_patch4_windows8_256 style configuration
+    >>> configuration = Swinv2Config()
 
-        >>> # Initializing a model from the microsoft/swinv2_tiny_patch4_windows8_256 style configuration
-        >>> model = Swinv2Model(configuration)
+    >>> # Initializing a model from the microsoft/swinv2_tiny_patch4_windows8_256 style configuration
+    >>> model = Swinv2Model(configuration)
 
-        >>> # Accessing the model configuration
-        >>> configuration = model.config
-        ```"""
+    >>> # Accessing the model configuration
+    >>> configuration = model.config
+    ```"""
     model_type = "swinv2"
 
     attribute_map = {
