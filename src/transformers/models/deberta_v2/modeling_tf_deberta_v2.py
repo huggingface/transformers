162f13d4034bd3c6e4e517d82fa3728f740bd5ef
matt <rocketknight1@gmail.com>
2022-07-22 13:53:16 +0100

    Change how `take_along_axis` is computed in DeBERTa to stop confusing XLA

--- src/transformers/models/deberta_v2/modeling_tf_deberta_v2.py
+++ src/transformers/models/deberta_v2/modeling_tf_deberta_v2.py
@@ -524,23 +524,23 @@ def pos_dynamic_expand(pos_index, p2c_att, key_layer):
 
 def take_along_axis(x, indices, gather_axis):
     if gather_axis < 0:
-        gather_axis = tf.rank(x) + gather_axis
+        gather_axis = x.shape.rank + gather_axis
 
-    if gather_axis != tf.rank(x) - 1:
-        pre_roll = tf.rank(x) - 1 - gather_axis
-        permutation = tf.roll(tf.range(tf.rank(x)), pre_roll, axis=0)
+    if gather_axis != x.shape.rank - 1:
+        pre_roll = x.shape.rank - 1 - gather_axis
+        permutation = tf.roll(tf.range(x.shape.rank), pre_roll, axis=0)
         x = tf.transpose(x, perm=permutation)
         indices = tf.transpose(indices, perm=permutation)
     else:
         pre_roll = 0
 
-    flat_x = tf.reshape(x, (-1, tf.shape(x)[-1]))
-    flat_indices = tf.reshape(indices, (-1, tf.shape(indices)[-1]))
+    flat_x = tf.reshape(x, (-1, x.shape[-1]))
+    flat_indices = tf.reshape(indices, (-1, indices.shape[-1]))
     gathered = tf.gather(flat_x, flat_indices, batch_dims=1)
-    gathered = tf.reshape(gathered, tf.shape(indices))
+    gathered = tf.reshape(gathered, indices.shape)
 
     if pre_roll != 0:
-        permutation = tf.roll(tf.range(tf.rank(x)), -pre_roll, axis=0)
+        permutation = tf.roll(tf.range(x.shape.rank), -pre_roll, axis=0)
         gathered = tf.transpose(gathered, perm=permutation)
 
     return gathered
