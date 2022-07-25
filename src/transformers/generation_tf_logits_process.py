346d79e27a8f3559b7fdb7c9af716e1109fff906
matt <rocketknight1@gmail.com>
2022-07-25 17:34:36 +0100

    Fix bad words filter in XLA generation

--- src/transformers/generation_tf_logits_process.py
+++ src/transformers/generation_tf_logits_process.py
@@ -281,6 +281,8 @@ class TFRepetitionPenaltyLogitsProcessor(TFLogitsProcessor):
 
         return scores
 
+import pdb
+import sys
 
 class TFNoBadWordsLogitsProcessor(TFLogitsProcessor):
     """
@@ -332,7 +334,7 @@ class TFNoBadWordsLogitsProcessor(TFLogitsProcessor):
             def _len_greater_than_cur_len():
                 # Otherwise, if the bad sequence is longer than the current length they can't ever match
                 return tf.cond(
-                    tf.math.greater(self.bad_word_seqs_len[bad_word_seq_number], row_input_ids.shape[0]),
+                    tf.math.greater(self.bad_word_seqs_len[bad_word_seq_number], tf.shape(row_input_ids)[0]),
                     lambda: tf.zeros((), dtype=tf.bool),
                     _match_found,
                 )
