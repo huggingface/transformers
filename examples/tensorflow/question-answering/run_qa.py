c89a592e87e1fe67f8c7d675e8029ef997651e8e
Lysandre <lysandre@huggingface.co>
2022-07-27 17:13:57 +0200

    Dev version

--- examples/tensorflow/question-answering/run_qa.py
+++ examples/tensorflow/question-answering/run_qa.py
@@ -46,7 +46,7 @@ from utils_qa import postprocess_qa_predictions
 
 
 # Will error if the minimal version of Transformers is not installed. Remove at your own risks.
-check_min_version("4.21.0.dev0")
+check_min_version("4.22.0.dev0")
 
 logger = logging.getLogger(__name__)
 
