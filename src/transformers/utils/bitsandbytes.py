9bd326b7eaf758e19f12166ec0bbba1906d53062
younesbelkada <younesbelkada@gmail.com>
2022-07-27 09:29:00 +0000

    add docstring
    - comment `replace_8bit_linear` function
    - fix weird formatting

--- src/transformers/utils/bitsandbytes.py
+++ src/transformers/utils/bitsandbytes.py
@@ -21,8 +21,8 @@ def set_module_8bit_tensor_to_device(module, tensor_name, device, value=None):
     Args:
         module (`torch.nn.Module`):
             The module in which the tensor we want to move lives.
-        tensor_name (`str`): The full
-            name of the parameter/buffer.
+        tensor_name (`str`):
+            The full name of the parameter/buffer.
         device (`int`, `str` or `torch.device`):
             The device on which to set the tensor.
         value (`torch.Tensor`, *optional*):
@@ -78,7 +78,24 @@ def set_module_8bit_tensor_to_device(module, tensor_name, device, value=None):
 
 
 def replace_8bit_linear(model, threshold=6.0):
+    """
+    A helper function to replace all `torch.nn.Linear` modules by `bnb.nn.Linear8bit` modules from the `bitsandbytes`
+    library. This will enable running your models using mixed int8 precision as described by the paper `GPT3.int8():
+    8-bit Matrix Multiplication for Transformers at Scale`. Make sure `bitsandbytes` compiled with the correct CUDA
+    version of your hardware is installed before running this function. `pip install -i https://test.pypi.org/simple/
+    bitsandbytes-cudaXXX` with `XXX` is your CUDA version (e.g., 11.6 = 116)
 
+    The function will be run recursively and replace all `torch.nn.Linear` modules except for the `lm_head` that should
+    be kept as a `torch.nn.Linear` module. The replacement is done under `init_empty_weights` context manager so no
+    CPU/GPU memory is required to run this function.
+
+    Parameters:
+        model (`torch.nn.Module`):
+            Input model or `torch.nn.Module` as the function is run recursively.
+        threshold (`float`, *optional*):
+            `int8_threshold` for outlier detection as described in the formentioned paper. This parameters is set to
+            `6.0` as described by the paper.
+    """
     for n, module in model.named_children():
         if len(list(module.children())) > 0:
             replace_8bit_linear(module, threshold)
