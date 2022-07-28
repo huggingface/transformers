1cabae4f30bf53c14e48e2e4a165ed3e60de3977
Sylvain Gugger <Sylvain.gugger@gmail.com>
2022-07-28 13:10:25 -0400

    Fix quality

--- tests/models/dpr/test_tokenization_dpr.py
+++ tests/models/dpr/test_tokenization_dpr.py
@@ -13,9 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
-import unittest
-
 from transformers import (
     DPRContextEncoderTokenizer,
     DPRContextEncoderTokenizerFast,
