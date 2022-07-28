0d1675076fb31afc8101477b3dd38db0f5314b92
Ikuya Yamada <ikuya@ikuya.net>
2022-07-28 23:20:05 +0900

    fix typos

--- src/transformers/models/luke/modeling_luke.py
+++ src/transformers/models/luke/modeling_luke.py
@@ -1829,7 +1829,7 @@ class LukeForSequenceClassification(LukePreTrainedModel):
             Labels for computing the sequence classification/regression loss. Indices should be in `[0, ...,
             config.num_labels - 1]`. If `config.num_labels == 1` a regression loss is computed (Mean-Square loss), If
             `config.num_labels > 1` a classification loss is computed (Cross-Entropy).
-        ```"""
+        """
         return_dict = return_dict if return_dict is not None else self.config.use_return_dict
 
         outputs = self.luke(
