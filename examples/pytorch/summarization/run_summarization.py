d052b2902664fe954af1288679cdd3424e6dd94d
Sylvain Gugger <35901082+sgugger@users.noreply.github.com>
2022-07-29 08:09:09 -0400

    Replace `as_target` context managers by direct calls (#18325)
    * Preliminary work on tokenizers
    
    * Quality + fix tests
    
    * Treat processors
    
    * Fix pad
    
    * Remove all uses of  in tests, docs and examples
    
    * Replace all as_target_tokenizer
    
    * Fix tests
    
    * Fix quality
    
    * Update examples/flax/image-captioning/run_image_captioning_flax.py
    
    Co-authored-by: amyeroberts <amy@huggingface.co>
    
    * Style
    
    Co-authored-by: amyeroberts <amy@huggingface.co>

--- examples/pytorch/summarization/run_summarization.py
+++ examples/pytorch/summarization/run_summarization.py
@@ -522,9 +522,8 @@ def main():
         inputs = [prefix + inp for inp in inputs]
         model_inputs = tokenizer(inputs, max_length=data_args.max_source_length, padding=padding, truncation=True)
 
-        # Setup the tokenizer for targets
-        with tokenizer.as_target_tokenizer():
-            labels = tokenizer(targets, max_length=max_target_length, padding=padding, truncation=True)
+        # Tokenize targets with the `text_target` keyword argument
+        labels = tokenizer(text_target=targets, max_length=max_target_length, padding=padding, truncation=True)
 
         # If we are padding here, replace all tokenizer.pad_token_id in the labels by -100 when we want to ignore
         # padding in the loss.
