75d8f4af7418d588c04f92ba3cc2116a0ed70879
Nicolas Patry <patry.nicolas@protonmail.com>
2022-08-01 17:00:36 +0200

    black again.

--- src/transformers/pipelines/__init__.py
+++ src/transformers/pipelines/__init__.py
@@ -673,7 +673,9 @@ def pipeline(
     # if it needs feature_extractor/tokenizer or not
     if not load_feature_extractor and task not in NO_FEATURE_EXTRACTOR_TASKS and task != "feature-extraction":
         raise EnvironmentError(
-            f"There is a problem in `transformers`. The task {task} requires a feature extractor, however the model {type(model_config)} seems to not support feature-extractors. This is likely a misconfiguration in the library, please report this issue."
+            f"There is a problem in `transformers`. The task {task} requires a feature extractor, however the model"
+            f" {type(model_config)} seems to not support feature-extractors. This is likely a misconfiguration in the"
+            " library, please report this issue."
         )
     if not load_tokenizer and task not in NO_TOKENIZER_TASKS and task != "feature-extraction":
         raise EnvironmentError(
