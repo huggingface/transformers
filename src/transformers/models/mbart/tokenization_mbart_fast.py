9240df81e827d76c73c569861cb540da1d99739a
ydshieh <ydshieh@users.noreply.github.com>
2022-08-01 10:30:48 +0200

    fix all ``...```

--- src/transformers/models/mbart/tokenization_mbart_fast.py
+++ src/transformers/models/mbart/tokenization_mbart_fast.py
@@ -68,8 +68,8 @@ class MBartTokenizerFast(PreTrainedTokenizerFast):
     This tokenizer inherits from [`PreTrainedTokenizerFast`] which contains most of the main methods. Users should
     refer to this superclass for more information regarding those methods.
 
-    The tokenization method is `<tokens> <eos> <language code>` for source language documents, and ``<language code>
-    <tokens> <eos>``` for target language documents.
+    The tokenization method is `<tokens> <eos> <language code>` for source language documents, and `<language code>
+    <tokens> <eos>` for target language documents.
 
     Examples:
 
