4f3328c1636c9bc4fabebe43b30ca89bf00b4fb5
ydshieh <ydshieh@users.noreply.github.com>
2022-08-02 11:59:13 +0200

    change audio to images in TROCR processor

--- src/transformers/models/trocr/processing_trocr.py
+++ src/transformers/models/trocr/processing_trocr.py
@@ -54,27 +54,23 @@ class TrOCRProcessor(ProcessorMixin):
         if self._in_target_context_manager:
             return self.current_processor(*args, **kwargs)
 
-        if "raw_speech" in kwargs:
-            warnings.warn("Using `raw_speech` as a keyword argument is deprecated. Use `audio` instead.")
-            audio = kwargs.pop("raw_speech")
-        else:
-            audio = kwargs.pop("audio", None)
+        images = kwargs.pop("images", None)
         text = kwargs.pop("text", None)
         if len(args) > 0:
-            audio = args[0]
+            images = args[0]
             args = args[1:]
 
-        if audio is None and text is None:
-            raise ValueError("You need to specify either an `audio` or `text` input to process.")
+        if images is None and text is None:
+            raise ValueError("You need to specify either an `images` or `text` input to process.")
 
-        if audio is not None:
-            inputs = self.feature_extractor(audio, *args, **kwargs)
+        if images is not None:
+            inputs = self.feature_extractor(images, *args, **kwargs)
         if text is not None:
             encodings = self.tokenizer(text, **kwargs)
 
         if text is None:
             return inputs
-        elif audio is None:
+        elif images is None:
             return encodings
         else:
             inputs["labels"] = encodings["input_ids"]
