8e13979e755bcc1689d9201a06fdce0bd42b8a0e
nandwalritik <ritiknandwal021@gmail.com>
2022-08-03 11:43:35 +0530

    Update no_trainer scripts for language-modeling examples

--- examples/pytorch/language-modeling/run_clm_no_trainer.py
+++ examples/pytorch/language-modeling/run_clm_no_trainer.py
@@ -597,7 +597,7 @@ def main():
                 outputs = model(**batch)
 
             loss = outputs.loss
-            losses.append(accelerator.gather(loss.repeat(args.per_device_eval_batch_size)))
+            losses.append(accelerator.gather_for_metrics(loss.repeat(args.per_device_eval_batch_size)))
 
         losses = torch.cat(losses)
         losses = losses[: len(eval_dataset)]
