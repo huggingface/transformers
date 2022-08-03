b8adae8ff086177a5da4b423d3fda719f41b1402
osanseviero <osanseviero@gmail.com>
2022-08-03 14:14:08 +0200

    Make style

--- src/transformers/dependency_versions_table.py
+++ src/transformers/dependency_versions_table.py
@@ -22,7 +22,7 @@ deps = {
     "fugashi": "fugashi>=1.0",
     "GitPython": "GitPython<3.1.19",
     "hf-doc-builder": "hf-doc-builder>=0.3.0",
-    "huggingface-hub": "huggingface-hub>=0.1.0,<1.0",
+    "huggingface-hub": "huggingface-hub>=0.8.1,<1.0",
     "importlib_metadata": "importlib_metadata",
     "ipadic": "ipadic>=1.0.0,<2.0",
     "isort": "isort>=5.5.4",
