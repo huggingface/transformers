58176d1c737df177ff1a4aafd7d225bc03d81a60
matt <rocketknight1@gmail.com>
2022-08-03 14:19:29 +0100

    Dodge a merge conflict

--- examples/tensorflow/question-answering/run_qa.py
+++ examples/tensorflow/question-answering/run_qa.py
@@ -27,8 +27,8 @@ from typing import Optional
 
 import tensorflow as tf
 from datasets import load_dataset
-from evaluate import load
 
+import evaluate
 import transformers
 from transformers import (
     AutoConfig,
@@ -600,7 +600,7 @@ def main():
         references = [{"id": ex["id"], "answers": ex[answer_column_name]} for ex in examples]
         return EvalPrediction(predictions=formatted_predictions, label_ids=references)
 
-    metric = load("squad_v2" if data_args.version_2_with_negative else "squad")
+    metric = evaluate.load("squad_v2" if data_args.version_2_with_negative else "squad")
 
     def compute_metrics(p: EvalPrediction):
         return metric.compute(predictions=p.predictions, references=p.label_ids)
