6e21b7d7e3ab2defd9d74906623c8635875f7885
Amy Roberts <22614925+amyeroberts@users.noreply.github.com>
2022-08-05 16:38:17 +0100

    Remove accidental clip changes

--- src/transformers/models/clip/feature_extraction_clip.py
+++ src/transformers/models/clip/feature_extraction_clip.py
@@ -108,15 +108,13 @@ class CLIPFeatureExtractor(FeatureExtractionMixin, ImageFeatureExtractionMixin):
                 tensor. In case of a NumPy array/PyTorch tensor, each image should be of shape (C, H, W), where C is a
                 number of channels, H and W are image height and width.
 
-            return_tensors (`str` or [`~utils.TensorType`], *optional*, defaults to `None`):
-                If set, will return a tensor of a particular framework.
+            return_tensors (`str` or [`~utils.TensorType`], *optional*, defaults to `'np'`):
+                If set, will return tensors of a particular framework. Acceptable values are:
 
-                Acceptable values are:
-                - `'tf'`: Return TensorFlow `tf.constant` object.
-                - `'pt'`: Return PyTorch `torch.Tensor` object.
-                - `'np'`: Return NumPy `np.ndarray` object.
-                - `'jax'`: Return JAX `jnp.ndarray` object.
-                - None: Return list of `np.ndarray` objects.
+                - `'tf'`: Return TensorFlow `tf.constant` objects.
+                - `'pt'`: Return PyTorch `torch.Tensor` objects.
+                - `'np'`: Return NumPy `np.ndarray` objects.
+                - `'jax'`: Return JAX `jnp.ndarray` objects.
 
         Returns:
             [`BatchFeature`]: A [`BatchFeature`] with the following fields:
@@ -157,15 +155,8 @@ class CLIPFeatureExtractor(FeatureExtractionMixin, ImageFeatureExtractionMixin):
             ]
         if self.do_center_crop and self.crop_size is not None:
             images = [self.center_crop(image, self.crop_size) for image in images]
-
-        # if do_normalize=False, the casting to a numpy array won't happen, so we need to do it here
-        make_channel_first = True if isinstance(images[0], Image.Image) else images[0].shape[-1] in (1, 3)
-        images = [self.to_numpy_array(image, rescale=False, channel_first=make_channel_first) for image in images]
-
         if self.do_normalize:
-            images = [
-                self.normalize(image=image, mean=self.image_mean, std=self.image_std, rescale=True) for image in images
-            ]
+            images = [self.normalize(image=image, mean=self.image_mean, std=self.image_std) for image in images]
 
         # return as BatchFeature
         data = {"pixel_values": images}
