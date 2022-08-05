8eabee9793b7c2ddf02d06843b2c98a828e8babb
Daniel Jones <jonesdaniel@microsoft.com>
2022-08-05 13:32:19 +0000

    removing copy inconsistencies via make fix-copies

--- src/transformers/models/mbart/modeling_mbart.py
+++ src/transformers/models/mbart/modeling_mbart.py
@@ -134,12 +134,14 @@ class MBartLearnedPositionalEmbedding(nn.Embedding):
         self.offset = 2
         super().__init__(num_embeddings + self.offset, embedding_dim)
 
-    def forward(self, input_ids_shape: torch.Size, past_key_values_length: int = 0):
-        """`input_ids_shape` is expected to be [bsz x seqlen]."""
-        bsz, seq_len = input_ids_shape[:2]
+    def forward(self, input_ids: torch.Tensor, past_key_values_length: int = 0):
+        """`input_ids' shape is expected to be [bsz x seqlen]."""
+
+        bsz, seq_len = input_ids.shape[:2]
         positions = torch.arange(
             past_key_values_length, past_key_values_length + seq_len, dtype=torch.long, device=self.weight.device
-        )
+        ).repeat(bsz, 1)
+
         return super().forward(positions + self.offset)
 
 
