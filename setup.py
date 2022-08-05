c7849d9efcf826021da0dd5721f380f377898980
Sylvain Gugger <35901082+sgugger@users.noreply.github.com>
2022-08-05 09:12:19 -0400

    Remove py.typed (#18485)

--- setup.py
+++ setup.py
@@ -412,7 +412,6 @@ setup(
     url="https://github.com/huggingface/transformers",
     package_dir={"": "src"},
     packages=find_packages("src"),
-    package_data={"transformers": ["py.typed"]},
     zip_safe=False,
     extras_require=extras,
     entry_points={"console_scripts": ["transformers-cli=transformers.commands.transformers_cli:main"]},
