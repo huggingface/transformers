586dcf6b2146ae9e2e8961b49c5e6706ae14f724
Nicolas Patry <patry.nicolas@protonmail.com>
2022-08-05 08:45:07 +0200

    Fixing issue where generic model types wouldn't load properly with the pipeline (#18392)
    * Adding a better error message when the model is improperly configured
    
    within transformers.
    
    * Update src/transformers/pipelines/__init__.py
    
    * Black version.
    
    * Overriding task aliases so that tokenizer+feature_extractor
    
    values are correct.
    
    * Fixing task aliases by overriding their names early
    
    * X.
    
    * Fixing feature-extraction.
    
    * black again.
    
    * Normalizing `translation` too.
    
    * Fixing last few corner cases.
    
    translation need to use its non normalized name (translation_XX_to_YY,
    so that the task_specific_params are correctly overloaded).
    This can be removed and cleaned up in a later PR.
    
    `speech-encode-decoder` actually REQUIRES to pass a `tokenizer` manually
    so the error needs to be discarded when the `tokenizer` is already
    there.
    
    * doc-builder fix.
    
    * Fixing the real issue.
    
    * Removing dead code.
    
    * Do not import the actual config classes.

--- src/transformers/pipelines/base.py
+++ src/transformers/pipelines/base.py
@@ -630,7 +630,6 @@ class PipedPipelineDataFormat(PipelineDataFormat):
         for line in sys.stdin:
             # Split for multi-columns
             if "\t" in line:
-
                 line = line.split("\t")
                 if self.column:
                     # Dictionary to map arguments
@@ -752,7 +751,6 @@ class Pipeline(_ScikitCompat):
         binary_output: bool = False,
         **kwargs,
     ):
-
         if framework is None:
             framework, model = infer_framework_load_model(model, config=model.config)
 
@@ -1123,18 +1121,19 @@ class PipelineRegistry:
         supported_task.sort()
         return supported_task
 
-    def check_task(self, task: str) -> Tuple[Dict, Any]:
+    def check_task(self, task: str) -> Tuple[str, Dict, Any]:
         if task in self.task_aliases:
             task = self.task_aliases[task]
         if task in self.supported_tasks:
             targeted_task = self.supported_tasks[task]
-            return targeted_task, None
+            return task, targeted_task, None
 
         if task.startswith("translation"):
             tokens = task.split("_")
             if len(tokens) == 4 and tokens[0] == "translation" and tokens[2] == "to":
                 targeted_task = self.supported_tasks["translation"]
-                return targeted_task, (tokens[1], tokens[3])
+                task = "translation"
+                return task, targeted_task, (tokens[1], tokens[3])
             raise KeyError(f"Invalid translation task {task}, use 'translation_XX_to_YY' format")
 
         raise KeyError(
