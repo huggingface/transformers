134e7a7e000d06238ce0db9a9159086db8262699
Amy Roberts <22614925+amyeroberts@users.noreply.github.com>
2022-08-05 14:44:15 +0100

    Cast images to numpy arrays in call to enable consistent behaviour with different configs

--- tests/models/clip/test_feature_extraction_clip.py
+++ tests/models/clip/test_feature_extraction_clip.py
@@ -18,6 +18,7 @@ import unittest
 
 import numpy as np
 
+from parameterized import parameterized
 from transformers.testing_utils import require_torch, require_vision
 from transformers.utils import is_torch_available, is_vision_available
 
@@ -292,3 +293,56 @@ class CLIPFeatureExtractionTestFourChannels(FeatureExtractionSavingTestMixin, un
                 self.feature_extract_tester.crop_size,
             ),
         )
+
+    @parameterized.expand(
+        [
+            ("do_resize_True_do_center_crop_True_do_normalize_True", True, True, True),
+            ("do_resize_True_do_center_crop_True_do_normalize_False", True, True, False),
+            ("do_resize_True_do_center_crop_False_do_normalize_True", True, False, True),
+            ("do_resize_True_do_center_crop_False_do_normalize_False", True, False, False),
+            ("do_resize_False_do_center_crop_True_do_normalize_True", False, True, True),
+            ("do_resize_False_do_center_crop_True_do_normalize_False", False, True, False),
+            ("do_resize_False_do_center_crop_False_do_normalize_True", False, False, True),
+            ("do_resize_False_do_center_crop_False_do_normalize_False", False, False, False),
+        ]
+    )
+    def test_call_flags(self, _, do_resize, do_center_crop, do_normalize):
+        # Initialize feature_extractor
+        feature_extractor = self.feature_extraction_class(**self.feat_extract_dict)
+        feature_extractor.do_center_crop = do_center_crop
+        feature_extractor.do_resize = do_resize
+        feature_extractor.do_normalize = do_normalize
+        # create random PIL images
+        image_inputs = self.feature_extract_tester.prepare_inputs(equal_resolution=False, torchify=True)
+
+        # expected_shapes = [(self.expected_encoded_image_num_channels, *x.size[::-1]) for x in image_inputs]
+        expected_shapes = [x.shape for x in image_inputs]
+        if do_resize:
+            # Same size logic inside resized
+            resized_shapes = []
+            for shape in expected_shapes:
+                c, h, w = shape
+                short, long = (w, h) if w <= h else (h, w)
+                min_size = self.feature_extract_tester.size
+                if short == min_size:
+                    resized_shapes.append((c, h, w))
+                else:
+                    short, long = min_size, int(long * min_size / short)
+                    resized_shape = (c, long, short) if w <= h else (c, short, long)
+                    resized_shapes.append(resized_shape)
+            expected_shapes = resized_shapes
+        if do_center_crop:
+            expected_shapes = [
+                (
+                    self.expected_encoded_image_num_channels,
+                    self.feature_extract_tester.crop_size,
+                    self.feature_extract_tester.crop_size,
+                )
+                for _ in range(self.feature_extract_tester.batch_size)
+            ]
+
+        pixel_values = feature_extractor(image_inputs, return_tensors=None)["pixel_values"]
+        self.assertEqual(len(pixel_values), self.feature_extract_tester.batch_size)
+        for idx, image in enumerate(pixel_values):
+            self.assertEqual(image.shape, expected_shapes[idx])
+            self.assertIsInstance(image, np.ndarray)
