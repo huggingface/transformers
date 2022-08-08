1a6f0eab45f5ceb558103d2f0c20f21e561904cb
JingyaHuang <huang_jingya@outlook.com>
2022-08-08 15:37:29 +0000

    Fix copies of sew-d

--- src/transformers/models/sew_d/modeling_sew_d.py
+++ src/transformers/models/sew_d/modeling_sew_d.py
@@ -791,6 +791,7 @@ class DisentangledSelfAttention(nn.Module):
         if "p2c" in self.pos_att_type:
             scale_factor += 1
 
+
         scale = torch.sqrt(torch.tensor(query_layer.size(-1), dtype=torch.float) * scale_factor)
         attention_scores = torch.bmm(query_layer, key_layer.transpose(-1, -2)) / torch.tensor(
             scale, dtype=query_layer.dtype
