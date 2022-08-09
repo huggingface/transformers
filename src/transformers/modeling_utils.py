0d5bc2b75c55bdd1eb8fedc8f72cd26878e5cfec
younesbelkada <younesbelkada@gmail.com>
2022-08-09 14:00:34 +0000

    modify installation guidelines

--- src/transformers/modeling_utils.py
+++ src/transformers/modeling_utils.py
@@ -1868,9 +1868,8 @@ class PreTrainedModel(nn.Module, ModuleUtilsMixin, GenerationMixin, PushToHubMix
             if not (is_accelerate_available() and is_bitsandbytes_available()):
                 raise ImportError(
                     "Using `load_in_8bit=True` requires Accelerate: `pip install accelerate` and the latest version of"
-                    " bitsandbytes `pip install -i https://test.pypi.org/simple/ bitsandbytes-cudaXXX` with `XXX`"
-                    " correspondingto your CUDA version (e.g. `pip install -i https://test.pypi.org/simple/"
-                    " bitsandbytes-cuda113` for CUDA 11.3)"
+                    " bitsandbytes `pip install -i https://test.pypi.org/simple/ bitsandbytes` or"
+                    " pip install bitsandbytes` "
                 )
             if torch_dtype == "auto" or torch_dtype != torch.float16:
                 torch_dtype = (
