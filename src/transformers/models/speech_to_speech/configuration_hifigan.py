b8b18df8c457ce93ba2cfa954c192084004f223b
sanchit-gandhi <sanchit@huggingface.co>
2022-08-10 11:46:43 +0100

    add CodeHiFiGAN config and model

--- src/transformers/models/speech_to_speech/configuration_hifigan.py
+++ src/transformers/models/speech_to_speech/configuration_hifigan.py
@@ -13,6 +13,7 @@ class HiFiGANConfig(PretrainedConfig):
         upsample_kernel_sizes=[16, 16, 4, 4],
         model_in_dim=80,
         sampling_rate=22050,
+        initializer_range=0.02,
         **kwargs,
     ):
         self.resblock_kernel_sizes = resblock_kernel_sizes
@@ -22,4 +23,56 @@ class HiFiGANConfig(PretrainedConfig):
         self.upsample_initial_channel = upsample_initial_channel
         self.upsample_kernel_sizes = upsample_kernel_sizes
         self.sampling_rate = sampling_rate
+        self.initializer_range = initializer_range
         super().__init__(**kwargs)
+
+
+class CodeHiFiGANConfig(PretrainedConfig):
+    def __init__(
+        self,
+        upsample_rates=[5,4,4,2,2],
+        upsample_kernel_sizes=[11,8,8,4,4],
+        upsample_initial_channel=512,
+        resblock_kernel_sizes=[3,7,11],
+        resblock_dilation_sizes=[[1,3,5], [1,3,5], [1,3,5]],
+        num_embeddings=1000,
+        embedding_dim=128,
+        model_in_dim=128,
+        f0=False,
+        f0_quant_num_bin=0,
+        dur_predictor=True,
+        encoder_embed_dim=128,
+        var_pred_hidden_dim=128,
+        var_pred_kernel_size=3,
+        var_pred_dropout=0.5,
+        sampling_rate=16000,
+        multispeaker=False,
+        embedder=False,
+        num_speakers=200,
+        embedder_dim=256,
+        initializer_range=0.02,
+        **kwargs,
+    ):
+        self.resblock_kernel_sizes = resblock_kernel_sizes
+        self.resblock_dilation_sizes = resblock_dilation_sizes
+        self.upsample_rates = upsample_rates
+        self.model_in_dim = model_in_dim
+        self.upsample_initial_channel = upsample_initial_channel
+        self.upsample_kernel_sizes = upsample_kernel_sizes
+        self.sampling_rate = sampling_rate
+        self.num_embeddings=num_embeddings
+        self.embedding_dim = embedding_dim
+        self.f0 = f0
+        self.f0_quant_num_bin = f0_quant_num_bin
+        self.dur_predictor = dur_predictor
+        self.encoder_embed_dim = encoder_embed_dim
+        self.var_pred_hidden_dim = var_pred_hidden_dim
+        self.var_pred_kernel_size = var_pred_kernel_size
+        self.var_pred_dropout = var_pred_dropout
+        self.multispeaker = multispeaker
+        self.embedder = embedder
+        self.num_speakers = num_speakers
+        self.embedder_dim = embedder_dim
+        self.initializer_range = initializer_range
+        super().__init__(**kwargs)
+
--- src/transformers/models/speech_to_speech/modeling_hifigan.py
+++ src/transformers/models/speech_to_speech/modeling_hifigan.py
@@ -5,7 +5,7 @@ from torch.nn import Conv1d, ConvTranspose1d
 from torch.nn.utils import remove_weight_norm, weight_norm
 from transformers import PreTrainedModel
 
-from .configuration_hifigan import HiFiGANConfig
+from configuration_hifigan import HiFiGANConfig, CodeHiFiGANConfig
 
 LRELU_SLOPE = 0.1
 
@@ -114,7 +114,7 @@ class ResBlock(torch.nn.Module):
 class HiFiGAN(PreTrainedModel):
     config_class = HiFiGANConfig
 
-    def __init__(self, config):
+    def __init__(self, config: HiFiGANConfig):
         super().__init__(config)
         self.num_kernels = len(config.resblock_kernel_sizes)
         self.num_upsamples = len(config.upsample_rates)
@@ -130,12 +130,12 @@ class HiFiGAN(PreTrainedModel):
 
         self.ups = nn.ModuleList()
         for i, (u, k) in enumerate(
-            zip(config.upsample_rates, config.upsample_kernel_sizes)
+                zip(config.upsample_rates, config.upsample_kernel_sizes)
         ):
             self.ups.append(
                 weight_norm(
                     ConvTranspose1d(
-                        config.upsample_initial_channel // (2**i),
+                        config.upsample_initial_channel // (2 ** i),
                         config.upsample_initial_channel // (2 ** (i + 1)),
                         k,
                         u,
@@ -148,13 +148,29 @@ class HiFiGAN(PreTrainedModel):
         for i in range(len(self.ups)):
             ch = config.upsample_initial_channel // (2 ** (i + 1))
             for k, d in zip(
-                config.resblock_kernel_sizes, config.resblock_dilation_sizes
+                    config.resblock_kernel_sizes, config.resblock_dilation_sizes
             ):
                 self.resblocks.append(ResBlock(ch, k, d))
 
         self.conv_post = weight_norm(Conv1d(ch, 1, 7, 1, padding=3))
         self.ups.apply(init_weights)
         self.conv_post.apply(init_weights)
+
+    def _init_weights(self, module):
+        """Initialize the weights."""
+        if isinstance(module, (nn.Linear, nn.Conv1d)):
+            # Slightly different from the TF version which uses truncated_normal for initialization
+            # cf https://github.com/pytorch/pytorch/pull/5617
+            module.weight.data.normal_(mean=0.0, std=self.config.initializer_range)
+            if module.bias is not None:
+                module.bias.data.zero_()
+        elif isinstance(module, nn.Embedding):
+            module.weight.data.normal_(mean=0.0, std=self.config.initializer_range)
+            if module.padding_idx is not None:
+                module.weight.data[module.padding_idx].zero_()
+        elif isinstance(module, nn.LayerNorm):
+            module.bias.data.zero_()
+            module.weight.data.fill_(1.0)
 
     def forward(self, x):
         x = self.conv_pre(x)
@@ -182,3 +198,128 @@ class HiFiGAN(PreTrainedModel):
             layer.remove_weight_norm()
         remove_weight_norm(self.conv_pre)
         remove_weight_norm(self.conv_post)
+
+
+class VariancePredictor(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.conv1 = nn.Sequential(
+            nn.Conv1d(
+                config.encoder_embed_dim,
+                config.var_pred_hidden_dim,
+                kernel_size=config.var_pred_kernel_size,
+                padding=(config.var_pred_kernel_size - 1) // 2,
+            ),
+            nn.ReLU(),
+        )
+        self.ln1 = nn.LayerNorm(config.var_pred_hidden_dim)
+        self.dropout_module = nn.Dropout(p=config.var_pred_dropout)
+        self.conv2 = nn.Sequential(
+            nn.Conv1d(
+                config.var_pred_hidden_dim,
+                config.var_pred_hidden_dim,
+                kernel_size=config.var_pred_kernel_size,
+                padding=1,
+            ),
+            nn.ReLU(),
+        )
+        self.ln2 = nn.LayerNorm(config.var_pred_hidden_dim)
+        self.proj = nn.Linear(config.var_pred_hidden_dim, 1)
+
+    def forward(self, x):
+        # Input: B x T x C; Output: B x T
+        x = self.conv1(x.transpose(1, 2)).transpose(1, 2)
+        x = self.dropout_module(self.ln1(x))
+        x = self.conv2(x.transpose(1, 2)).transpose(1, 2)
+        x = self.dropout_module(self.ln2(x))
+        return self.proj(x).squeeze(dim=2)
+
+
+class CodeHiFiGAN(HiFiGAN):
+    config_class = CodeHiFiGANConfig
+
+    def __init__(self, config):
+        super().__init__(config)
+        self.dict = nn.Embedding(config.num_embeddings, config.embedding_dim)
+        self.multispkr = config.multispeaker
+        self.embedder = config.embedder
+
+        if self.multispkr and not self.embedder:
+            self.spkr = nn.Embedding(config.num_speakers, config.embedding_dim)
+        elif self.embedder:
+            self.spkr = nn.Linear(config.embedder_dim, config.embedding_dim)
+
+        self.dur_predictor = None
+        if config.dur_predictor:
+            self.dur_predictor = VariancePredictor(config)
+
+        self.f0 = config.f0
+        n_f0_bin = config.f0_quant_num_bin
+        self.f0_quant_embed = (
+            None if n_f0_bin <= 0 else nn.Embedding(n_f0_bin, config.embedding_dim)
+        )
+
+    @staticmethod
+    def _upsample(signal, max_frames):
+        if signal.dim() == 3:
+            bsz, channels, cond_length = signal.size()
+        elif signal.dim() == 2:
+            signal = signal.unsqueeze(2)
+            bsz, channels, cond_length = signal.size()
+        else:
+            signal = signal.view(-1, 1, 1)
+            bsz, channels, cond_length = signal.size()
+
+        signal = signal.unsqueeze(3).repeat(1, 1, 1, max_frames // cond_length)
+
+        # pad zeros as needed (if signal's shape does not divide completely with max_frames)
+        reminder = (max_frames - signal.shape[2] * signal.shape[3]) // signal.shape[3]
+        if reminder > 0:
+            raise NotImplementedError(
+                "Padding condition signal - misalignment between condition features."
+            )
+
+        signal = signal.view(bsz, channels, max_frames)
+        return signal
+
+    def forward(self, **kwargs):
+        x = self.dict(kwargs["code"]).transpose(1, 2)
+
+        if self.dur_predictor and kwargs.get("dur_prediction", False):
+            assert x.size(0) == 1, "only support single sample"
+            log_dur_pred = self.dur_predictor(x.transpose(1, 2))
+            dur_out = torch.clamp(
+                torch.round((torch.exp(log_dur_pred) - 1)).long(), min=1
+            )
+            # B x C x T
+            x = torch.repeat_interleave(x, dur_out.view(-1), dim=2)
+
+        if self.f0:
+            if self.f0_quant_embed:
+                kwargs["f0"] = self.f0_quant_embed(kwargs["f0"].long()).transpose(1, 2)
+            else:
+                kwargs["f0"] = kwargs["f0"].unsqueeze(1)
+
+            if x.shape[-1] < kwargs["f0"].shape[-1]:
+                x = self._upsample(x, kwargs["f0"].shape[-1])
+            elif x.shape[-1] > kwargs["f0"].shape[-1]:
+                kwargs["f0"] = self._upsample(kwargs["f0"], x.shape[-1])
+            x = torch.cat([x, kwargs["f0"]], dim=1)
+
+        if self.multispkr:
+            assert (
+                    "spkr" in kwargs
+            ), 'require "spkr" input for multispeaker CodeHiFiGAN vocoder'
+            spkr = self.spkr(kwargs["spkr"]).transpose(1, 2)
+            spkr = self._upsample(spkr, x.shape[-1])
+            x = torch.cat([x, spkr], dim=1)
+
+        for k, feat in kwargs.items():
+            if k in ["spkr", "code", "f0", "dur_prediction"]:
+                continue
+
+            feat = self._upsample(feat, x.shape[-1])
+            x = torch.cat([x, feat], dim=1)
+
+        return super().forward(x)
+
