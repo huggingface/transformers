30289085e4ae40c4bed1ef60ef81f23b75646efb
Dhruv <k4r4n.dhruv@gmail.com>
2022-08-10 23:33:16 +0530

    fix copies

--- src/transformers/models/vision_text_dual_encoder/modeling_vision_text_dual_encoder.py
+++ src/transformers/models/vision_text_dual_encoder/modeling_vision_text_dual_encoder.py
@@ -154,7 +154,6 @@ def contrastive_loss(logits: torch.Tensor) -> torch.Tensor:
 # Copied from transformers.models.clip.modeling_clip.clip_loss
 def clip_loss(similarity: torch.Tensor) -> torch.Tensor:
     caption_loss = contrastive_loss(similarity)
-    # .T doesn't work while converting to onnx, aten::numpy_T operator is not supported yet
     image_loss = contrastive_loss(similarity.t())
     return (caption_loss + image_loss) / 2.0
 
