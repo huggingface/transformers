092359cc37e6bd2b1905531e86eeca8695324533
KMFODA <kmfoda@gmail.com>
2022-08-10 07:04:52 +0100

    Remove test_gradient_checkpointing

--- tests/models/t5/test_modeling_flax_t5.py
+++ tests/models/t5/test_modeling_flax_t5.py
@@ -244,30 +244,6 @@ class FlaxT5ModelTest(FlaxModelTesterMixin, FlaxGenerationTesterMixin, unittest.
         config_and_inputs = self.model_tester.prepare_config_and_inputs()
         self.model_tester.create_and_check_model(*config_and_inputs)
 
-    def test_gradient_checkpointing(self):
-        config, inputs_dict = self.model_tester.prepare_config_and_inputs_for_common()
-
-        for model_class in self.all_model_classes:
-            # prepare inputs
-            prepared_inputs_dict = self._prepare_for_class(inputs_dict, model_class)
-            # breakpoint()
-            model = model_class(config)
-            remat_model = model_class(config)
-            remat_model.enable_gradient_checkpointing()
-
-            outputs = model(**prepared_inputs_dict)
-            remat_outputs = remat_model(**prepared_inputs_dict)
-
-            # ensure that the dicts of outputs contain the same keys
-            self.assertEqual(outputs.keys(), remat_outputs.keys())
-
-            outputs = outputs.to_tuple()
-            remat_outputs = remat_outputs.to_tuple()
-
-            # ensure that the outputs remain precisely equal
-            for output, remat_output in zip(outputs, remat_outputs):
-                self.assertTrue((output == remat_output).all())
-
     def test_model_v1_1(self):
         config_and_inputs = self.model_tester.prepare_config_and_inputs()
         # check that gated gelu feed forward and different word embeddings work
