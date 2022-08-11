7c0d6bf6d62a99273b3c326e49c7c0b45ab4d0c8
iiLaurens <iiLaurens@users.noreply.github.com>
2022-08-11 13:30:06 +0200

    Implement suggestion
    Co-authored-by: Michael Benayoun <mickbenayoun@gmail.com>

--- src/transformers/models/deberta_v2/modeling_deberta_v2.py
+++ src/transformers/models/deberta_v2/modeling_deberta_v2.py
@@ -570,7 +570,7 @@ def build_relative_position(query_size, key_size, bucket_size=-1, max_position=-
     rel_pos_ids = q_ids[:, None] - k_ids[None,:]
     if bucket_size > 0 and max_position > 0:
         rel_pos_ids = make_log_bucket_position(rel_pos_ids, bucket_size, max_position)
-    rel_pos_ids = rel_pos_ids.type(torch.long)
+    rel_pos_ids = rel_pos_ids.to(torch.long)
     rel_pos_ids = rel_pos_ids[:query_size, :]
     rel_pos_ids = rel_pos_ids.unsqueeze(0)
     return rel_pos_ids
