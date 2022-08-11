4e51eb0488b75987e705e44b2ef2df38307914a2
Christoffer Øhrstrøm <2357447+ChrisFugl@users.noreply.github.com>
2022-08-11 11:15:16 +0200

    add encoder

--- src/transformers/models/layoutlmv3/modeling_tf_layoutlmv3.py
+++ src/transformers/models/layoutlmv3/modeling_tf_layoutlmv3.py
@@ -1,10 +1,12 @@
 import collections
+import math
 from typing import Dict
 from typing import Optional
 from typing import Union
 
 import tensorflow as tf
 
+from ...modeling_tf_outputs import TFBaseModelOutput
 from ...modeling_tf_utils import (
     TFPreTrainedModel,
     TFSequenceClassificationLoss,
@@ -280,7 +282,7 @@ class TFLayoutLMv3SelfAttention(tf.keras.layers.Layer):
         self.num_attention_heads = config.num_attention_heads
         self.attention_head_size = int(config.hidden_size / config.num_attention_heads)
         self.all_head_size = self.num_attention_heads * self.attention_head_size
-        self.attention_score_normaliser = sqrt(self.attention_head_size)
+        self.attention_score_normaliser = math.sqrt(self.attention_head_size)
 
         self.query = tf.keras.layers.Dense(
             self.all_head_size,
@@ -495,3 +497,149 @@ class TFLayoutLMv3Layer(tf.keras.layers.Layer):
         layer_output = self.bert_output(intermediate_output, attention_output, training=training)
         outputs = (layer_output,) + outputs
         return outputs
+
+
+class TFLayoutLMv3Encoder(tf.keras.layers.Layer):
+    def __init__(self, config: LayoutLMv3Config, **kwargs):
+        super().__init__(**kwargs)
+        self.config = config
+        self.layer = [TFLayoutLMv3Layer(config, name=f"layer.{i}") for i in range(config.num_hidden_layers)]
+
+        self.has_relative_attention_bias = config.has_relative_attention_bias
+        self.has_spatial_attention_bias = config.has_spatial_attention_bias
+
+        if self.has_relative_attention_bias:
+            self.rel_pos_bins = config.rel_pos_bins
+            self.max_rel_pos = config.max_rel_pos
+            self.rel_pos_bias = tf.keras.layers.Embedding(
+                self.rel_pos_bins,
+                config.num_attention_heads,
+                embeddings_initializer=get_initializer(config.initializer_range),
+                name="rel_pos_bias",
+            )
+
+        if self.has_spatial_attention_bias:
+            self.max_rel_2d_pos = config.max_rel_2d_pos
+            self.rel_2d_pos_bins = config.rel_2d_pos_bins
+            self.rel_pos_x_bias = tf.keras.layers.Embedding(
+                self.rel_2d_pos_bins,
+                config.num_attention_heads,
+                embeddings_initializer=get_initializer(config.initializer_range),
+                name="rel_pos_x_bias",
+            )
+            self.rel_pos_y_bias = tf.keras.layers.Embedding(
+                self.rel_2d_pos_bins,
+                config.num_attention_heads,
+                embeddings_initializer=get_initializer(config.initializer_range),
+                name="rel_pos_y_bias",
+            )
+
+    def relative_position_matrix(self, position_ids: tf.Tensor):
+        return tf.expand_dims(position_ids, axis=-2) - tf.expand_dims(position_ids, axis=-1)
+
+    def relative_position_bucket(self, relative_positions: tf.Tensor, num_buckets: int, max_distance: int):
+        # the negative relative positions are assigned to the interval [0, num_buckets / 2]
+        # we deal with this by assigning absolute relative positions to the interval [0, num_buckets / 2]
+        # and then offsetting the positive relative positions by num_buckets / 2 at the end
+        num_buckets = num_buckets // 2
+        buckets = tf.abs(relative_positions)
+
+        # half of the buckets are for exact increments in positions
+        max_exact_buckets = num_buckets // 2
+        is_small = buckets < max_exact_buckets
+
+        # the ofther half of the buckets are for logarithmically bigger bins in positions up to max_distance
+        buckets_log_ratio = tf.math.log(tf.cast(buckets, tf.float32) / max_exact_buckets)
+        distance_log_ratio = math.log(max_distance / max_exact_buckets)
+        buckets_big_offset = (
+            buckets_log_ratio / distance_log_ratio * (num_buckets - max_exact_buckets)
+        )  # scale is [0, num_buckets - max_exact_buckets]
+        buckets_big = max_exact_buckets + buckets_big_offset  # scale is [max_exact_buckets, num_buckets]
+        buckets_big = tf.cast(buckets_big, tf.int64)
+        buckets_big = tf.minimum(buckets_big, num_buckets - 1)
+
+        return (tf.cast(relative_positions > 0, tf.int64) * num_buckets) + tf.where(is_small, buckets, buckets_big)
+
+    def _cal_pos_emb(
+        self,
+        embedding_layer: tf.keras.layers.Embedding,
+        position_ids: tf.Tensor,
+        num_buckets: int,
+        max_distance: int,
+    ):
+        rel_pos_matrix = self.relative_position_matrix(position_ids)
+        rel_pos = self.relative_position_bucket(rel_pos_matrix, num_buckets, max_distance)
+        rel_pos = embedding_layer(rel_pos)
+        rel_pos = tf.transpose(rel_pos, [0, 3, 1, 2])  # B, N, N, H --> B, H, N, N
+        rel_pos = tf.cast(rel_pos, dtype=self.compute_dtype)
+        return rel_pos
+
+    def _cal_1d_pos_emb(self, position_ids: tf.Tensor):
+        return self._cal_pos_emb(self.rel_pos_bias, position_ids, self.rel_pos_bins, self.max_rel_pos)
+
+    def _cal_2d_pos_emb(self, bbox: tf.Tensor):
+        position_coord_x = bbox[:, :, 0]  # left
+        position_coord_y = bbox[:, :, 3]  # bottom
+        rel_pos_x = self._cal_pos_emb(
+            self.rel_pos_x_bias,
+            position_coord_x,
+            self.rel_2d_pos_bins,
+            self.max_rel_2d_pos,
+        )
+        rel_pos_y = self._cal_pos_emb(
+            self.rel_pos_y_bias,
+            position_coord_y,
+            self.rel_2d_pos_bins,
+            self.max_rel_2d_pos,
+        )
+        rel_2d_pos = rel_pos_x + rel_pos_y
+        return rel_2d_pos
+
+    def call(
+        self,
+        hidden_states: tf.Tensor,
+        bbox: Optional[tf.Tensor] = None,
+        attention_mask: Optional[tf.Tensor] = None,
+        head_mask: Optional[tf.Tensor] = None,
+        output_attentions: bool = False,
+        output_hidden_states: bool = False,
+        return_dict: bool = True,
+        position_ids: Optional[tf.Tensor] = None,
+        training: bool = False,
+    ):
+        all_hidden_states = () if output_hidden_states else None
+        all_self_attentions = () if output_attentions else None
+
+        rel_pos = self._cal_1d_pos_emb(position_ids) if self.has_relative_attention_bias else None
+        rel_2d_pos = self._cal_2d_pos_emb(bbox) if self.has_spatial_attention_bias else None
+
+        for i, layer_module in enumerate(self.layer):
+            if output_hidden_states:
+                all_hidden_states = all_hidden_states + (hidden_states,)
+
+            layer_head_mask = head_mask[i] if head_mask is not None else None
+
+            layer_outputs = layer_module(
+                hidden_states,
+                attention_mask,
+                layer_head_mask,
+                output_attentions,
+                rel_pos=rel_pos,
+                rel_2d_pos=rel_2d_pos,
+                training=training,
+            )
+
+            hidden_states = layer_outputs[0]
+            if output_attentions:
+                all_self_attentions = all_self_attentions + (layer_outputs[1],)
+
+        if return_dict:
+            return TFBaseModelOutput(
+                last_hidden_state=hidden_states,
+                hidden_states=all_hidden_states,
+                attentions=all_self_attentions,
+            )
+        else:
+            return tuple(
+                value for value in [hidden_states, all_hidden_states, all_self_attentions] if value is not None
+            )
