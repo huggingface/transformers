50507d40eb45f467f17d66d7ea7288cb5e7f63d4
Nicholas Broad <nicholas@nmbroad.com>
2022-08-17 14:46:52 -0700

    remove maskedlayernorm

--- src/transformers/models/deberta/modeling_deberta.py
+++ src/transformers/models/deberta/modeling_deberta.py
@@ -271,37 +271,6 @@ class DebertaLayerNorm(nn.Module):
         return y
 
 
-def MaskedLayerNorm(layerNorm, input, mask=None):
-    """
-    https://github.com/microsoft/DeBERTa/blob/master/DeBERTa/deberta/ops.py#L182
-
-    Masked LayerNorm which will apply mask over the output of LayerNorm to avoid inaccurate updatings to the LayerNorm module.
-
-    Args:
-      layernorm (:obj:`~DeBERTa.deberta.LayerNorm`): LayerNorm module or function
-      input (:obj:`torch.tensor`): The input tensor
-      mask (:obj:`torch.IntTensor`): The mask to applied on the output of LayerNorm where `0` indicate the output of that element will be ignored, i.e. set to `0`
-    Example::
-      # Create a tensor b x n x d
-      x = torch.randn([1,10,100])
-      m = torch.tensor([[1,1,1,0,0,0,0,0,0,0]], dtype=torch.int)
-      LayerNorm = DeBERTa.deberta.LayerNorm(100)
-      y = MaskedLayerNorm(LayerNorm, x, m)
-    """
-    output = layerNorm(input).to(input)
-
-    if mask is None:
-        return output
-    if mask.dim() != input.dim():
-        if mask.dim() == 4:
-            mask = mask.squeeze(1).squeeze(1)
-        mask = mask.unsqueeze(2)
-
-    mask = mask.to(output.dtype)
-
-    return output * mask
-
-
 class DebertaSelfOutput(nn.Module):
     def __init__(self, config):
         super().__init__()
@@ -1138,7 +1107,7 @@ class DebertaLMPredictionHead(nn.Module):
     def forward(self, hidden_states, embeddings):
         hidden_states = self.dense(hidden_states)
         hidden_states = self.transform_act_fn(hidden_states)
-        hidden_states = MaskedLayerNorm(self.LayerNorm, hidden_states)
+        hidden_states = self.LayerNorm(hidden_states) # original used MaskedLayerNorm, but passed no mask. This is equivalent.
         hidden_states = torch.matmul(hidden_states, embeddings.weight.t()) + self.bias
         return hidden_states
 
