affe0c4da72e36cc69a29c9f049068c762b4ac64
Leandro von Werra <lvwerra@users.noreply.github.com>
2022-08-22 17:41:19 +0200

    Fix GLUE MNLI when using `max_eval_samples`

--- examples/pytorch/text-classification/run_glue.py
+++ examples/pytorch/text-classification/run_glue.py
@@ -549,7 +549,11 @@ def main():
         eval_datasets = [eval_dataset]
         if data_args.task_name == "mnli":
             tasks.append("mnli-mm")
-            eval_datasets.append(raw_datasets["validation_mismatched"])
+            valid_mm_dataset = raw_datasets["validation_mismatched"]
+            if data_args.max_eval_samples is not None:
+                max_eval_samples = min(len(valid_mm_dataset), data_args.max_eval_samples)
+                valid_mm_dataset = valid_mm_dataset.select(range(max_eval_samples))
+            eval_datasets.append(valid_mm_dataset)
             combined = {}
 
         for eval_dataset, task in zip(eval_datasets, tasks):
