c8099483542f7919da011353cde79a44007c0ee1
Niels Rogge <nielsrogge@Nielss-MacBook-Pro.local>
2022-08-22 13:01:42 +0200

    Make height and width dynamic axes

--- src/transformers/models/segformer/configuration_segformer.py
+++ src/transformers/models/segformer/configuration_segformer.py
@@ -163,7 +163,7 @@ class SegformerOnnxConfig(OnnxConfig):
     def inputs(self) -> Mapping[str, Mapping[int, str]]:
         return OrderedDict(
             [
-                ("pixel_values", {0: "batch", 1: "num_channels"}),
+                ("pixel_values", {0: "batch", 1: "num_channels", 2: "height", 3: "width"}),
             ]
         )
 
