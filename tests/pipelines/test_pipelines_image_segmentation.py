53d49eabe88b5a255d6dbc46667aa53cf88c16bb
Amy Roberts <22614925+amyeroberts@users.noreply.github.com>
2022-08-23 11:18:15 +0100

    Use double quotes, like previous example

--- tests/pipelines/test_pipelines_image_segmentation.py
+++ tests/pipelines/test_pipelines_image_segmentation.py
@@ -255,12 +255,12 @@ class ImageSegmentationPipelineTests(unittest.TestCase, metaclass=PipelineTestCa
         self.assertEqual(
             nested_simplify(outputs, decimals=4),
             [
-                {'score': 0.9094, 'label': 'blanket', 'mask': '6500201749480f87154fd967783b2b97'},
-                {'score': 0.9941, 'label': 'cat', 'mask': 'f3a7f80220788acc0245ebc084df6afc'},
-                {'score': 0.9987, 'label': 'remote', 'mask': '7703408f54da1d0ebda47841da875e48'},
-                {'score': 0.9995, 'label': 'remote', 'mask': 'bd726918f10fed3efaef0091e11f923b'},
-                {'score': 0.9722, 'label': 'couch', 'mask': '226d6dcb98bebc3fbc208abdc0c83196'},
-                {'score': 0.9994, 'label': 'cat', 'mask': 'fa5d8d5c329546ba5339f3095641ef56'}
+                {"score": 0.9094, "label": "blanket", "mask": "6500201749480f87154fd967783b2b97"},
+                {"score": 0.9941, "label": "cat", "mask": "f3a7f80220788acc0245ebc084df6afc"},
+                {"score": 0.9987, "label": "remote", "mask": "7703408f54da1d0ebda47841da875e48"},
+                {"score": 0.9995, "label": "remote", "mask": "bd726918f10fed3efaef0091e11f923b"},
+                {"score": 0.9722, "label": "couch", "mask": "226d6dcb98bebc3fbc208abdc0c83196"},
+                {"score": 0.9994, "label": "cat", "mask": "fa5d8d5c329546ba5339f3095641ef56"}
             ]
         )
 
@@ -279,20 +279,20 @@ class ImageSegmentationPipelineTests(unittest.TestCase, metaclass=PipelineTestCa
             nested_simplify(outputs, decimals=4),
             [
                 [
-                    {'score': 0.9094, 'label': 'blanket', 'mask': '6500201749480f87154fd967783b2b97'},
-                    {'score': 0.9941, 'label': 'cat', 'mask': 'f3a7f80220788acc0245ebc084df6afc'},
-                    {'score': 0.9987, 'label': 'remote', 'mask': '7703408f54da1d0ebda47841da875e48'},
-                    {'score': 0.9995, 'label': 'remote', 'mask': 'bd726918f10fed3efaef0091e11f923b'},
-                    {'score': 0.9722, 'label': 'couch', 'mask': '226d6dcb98bebc3fbc208abdc0c83196'},
-                    {'score': 0.9994, 'label': 'cat', 'mask': 'fa5d8d5c329546ba5339f3095641ef56'}
+                    {"score": 0.9094, "label": "blanket", "mask": "6500201749480f87154fd967783b2b97"},
+                    {"score": 0.9941, "label": "cat", "mask": "f3a7f80220788acc0245ebc084df6afc"},
+                    {"score": 0.9987, "label": "remote", "mask": "7703408f54da1d0ebda47841da875e48"},
+                    {"score": 0.9995, "label": "remote", "mask": "bd726918f10fed3efaef0091e11f923b"},
+                    {"score": 0.9722, "label": "couch", "mask": "226d6dcb98bebc3fbc208abdc0c83196"},
+                    {"score": 0.9994, "label": "cat", "mask": "fa5d8d5c329546ba5339f3095641ef56"}
                 ],
                 [
-                    {'score': 0.9094, 'label': 'blanket', 'mask': '6500201749480f87154fd967783b2b97'},
-                    {'score': 0.9941, 'label': 'cat', 'mask': 'f3a7f80220788acc0245ebc084df6afc'},
-                    {'score': 0.9987, 'label': 'remote', 'mask': '7703408f54da1d0ebda47841da875e48'},
-                    {'score': 0.9995, 'label': 'remote', 'mask': 'bd726918f10fed3efaef0091e11f923b'},
-                    {'score': 0.9722, 'label': 'couch', 'mask': '226d6dcb98bebc3fbc208abdc0c83196'},
-                    {'score': 0.9994, 'label': 'cat', 'mask': 'fa5d8d5c329546ba5339f3095641ef56'}
+                    {"score": 0.9094, "label": "blanket", "mask": "6500201749480f87154fd967783b2b97"},
+                    {"score": 0.9941, "label": "cat", "mask": "f3a7f80220788acc0245ebc084df6afc"},
+                    {"score": 0.9987, "label": "remote", "mask": "7703408f54da1d0ebda47841da875e48"},
+                    {"score": 0.9995, "label": "remote", "mask": "bd726918f10fed3efaef0091e11f923b"},
+                    {"score": 0.9722, "label": "couch", "mask": "226d6dcb98bebc3fbc208abdc0c83196"},
+                    {"score": 0.9994, "label": "cat", "mask": "fa5d8d5c329546ba5339f3095641ef56"}
                 ]
             ]
         )
