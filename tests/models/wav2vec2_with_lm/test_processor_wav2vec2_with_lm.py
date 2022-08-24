aae900dba6d345ada3d22b20daa80b97b0bdbaa6
Patrick von Platen <patrick.v.platen@gmail.com>
2022-08-24 16:56:06 +0200

    remove generation

--- tests/models/wav2vec2_with_lm/test_processor_wav2vec2_with_lm.py
+++ tests/models/wav2vec2_with_lm/test_processor_wav2vec2_with_lm.py
@@ -435,21 +435,23 @@ class Wav2Vec2ProcessorWithLMTest(unittest.TestCase):
         self.assertEqual(" ".join(self.get_from_offsets(word_time_stamps, "word")), output.text)
 
         # output times
-        start_times = [round(x, 2) for x in self.get_from_offsets(word_time_stamps, "start_time")]
-        end_times = [round(x, 2) for x in self.get_from_offsets(word_time_stamps, "end_time")]
+        start_times = torch.tensor(self.get_from_offsets(word_time_stamps, "start_time"))
+        end_times = torch.tensor(self.get_from_offsets(word_time_stamps, "end_time"))
 
         # fmt: off
-        self.assertListEqual(
+        self.assertTrue(torch.allclose(
             start_times,
-            [
+            torch.tensor([
                 1.42, 1.64, 2.12, 2.26, 2.54, 3.0, 3.24, 3.6, 3.8, 4.1, 4.26, 4.94, 5.28, 5.66, 5.78, 5.94, 6.32, 6.54, 6.66,
-            ],
-        )
+            ]),
+            atol=0.01
+        ))
 
-        self.assertListEqual(
+        self.assertTrue(torch.allclose(
             end_times,
-            [
+            torch.tensor([
                 1.54, 1.88, 2.14, 2.46, 2.9, 3.18, 3.54, 3.72, 4.02, 4.18, 4.76, 5.16, 5.56, 5.7, 5.86, 6.2, 6.38, 6.62, 6.94,
-            ],
-        )
+            ]),
+            atol=0.01
+        ))
         # fmt: on
