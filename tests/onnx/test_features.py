6bd7477fb2ddb5932ac46b0f59a945bfe3290d12
rachthree <46288912+rachthree@users.noreply.github.com>
2022-08-23 20:32:25 -0400

    Moving torch_str/tf_str to self.framework_pt/tf

--- tests/onnx/test_features.py
+++ tests/onnx/test_features.py
@@ -16,6 +16,8 @@ class DetermineFrameworkTest(TestCase):
 
     def setUp(self):
         self.test_model = SMALL_MODEL_IDENTIFIER
+        self.framework_pt = "pt"
+        self.framework_tf = "tf"
 
     def _setup_pt_ckpt(self, save_dir):
         model_pt = AutoModel.from_pretrained(self.test_model)
@@ -54,20 +56,17 @@ class DetermineFrameworkTest(TestCase):
 
         For the functionality to execute, local checkpoints are provided but framework is not.
         """
-        torch_str = "pt"
-        tf_str = "tf"
-
         # PyTorch checkpoint
         with TemporaryDirectory() as local_pt_ckpt:
             self._setup_pt_ckpt(local_pt_ckpt)
             result = FeaturesManager.determine_framework(local_pt_ckpt)
-            self.assertEqual(result, torch_str)
+            self.assertEqual(result, self.framework_pt)
 
         # TensorFlow checkpoint
         with TemporaryDirectory() as local_tf_ckpt:
             self._setup_tf_ckpt(local_tf_ckpt)
             result = FeaturesManager.determine_framework(local_tf_ckpt)
-            self.assertEqual(result, tf_str)
+            self.assertEqual(result, self.framework_tf)
 
         # Invalid local checkpoint
         with TemporaryDirectory() as local_invalid_ckpt:
@@ -80,21 +79,18 @@ class DetermineFrameworkTest(TestCase):
 
         For the functionality to execute, framework and local checkpoints are not provided.
         """
-        torch_str = "pt"
-        tf_str = "tf"
-
         # Framework not provided, hub model is used (no local checkpoint directory)
         # TensorFlow not in environment -> use PyTorch
         mock_tf_available = MagicMock(return_value=False)
         with patch("transformers.onnx.features.is_tf_available", mock_tf_available):
             result = FeaturesManager.determine_framework(self.test_model)
-            self.assertEqual(result, torch_str)
+            self.assertEqual(result, self.framework_pt)
 
         # PyTorch not in environment -> use TensorFlow
         mock_torch_available = MagicMock(return_value=False)
         with patch("transformers.onnx.features.is_torch_available", mock_torch_available):
             result = FeaturesManager.determine_framework(self.test_model)
-            self.assertEqual(result, tf_str)
+            self.assertEqual(result, self.framework_tf)
 
         # Both in environment -> use PyTorch
         mock_tf_available = MagicMock(return_value=True)
@@ -103,7 +99,7 @@ class DetermineFrameworkTest(TestCase):
             "transformers.onnx.features.is_torch_available", mock_torch_available
         ):
             result = FeaturesManager.determine_framework(self.test_model)
-            self.assertEqual(result, torch_str)
+            self.assertEqual(result, self.framework_pt)
 
         # Both not in environment -> raise error
         mock_tf_available = MagicMock(return_value=False)
