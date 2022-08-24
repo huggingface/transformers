8a1d9fde1d48922da5a946d91aee423138800de5
Daniel Stancl <46073029+stancld@users.noreply.github.com>
2022-08-24 11:51:05 +0200

    Add TF implementation of `XGLMModel` (#16543)
    * Add TFXGLM models 
    
    * Add todo: self.supports_xla_generation = False
    
    Co-authored-by: Daniel Stancl <stancld@Daniels-MacBook-Pro.local>
    Co-authored-by: Daniel Stancl <stancld@daniels-mbp.home>
    Co-authored-by: Joao Gante <joaofranciscocardosogante@gmail.com>
    Co-authored-by: Daniel <daniel.stancl@rossum.ai>
    Co-authored-by: Patrick von Platen <patrick.v.platen@gmail.com>

--- src/transformers/models/auto/modeling_tf_auto.py
+++ src/transformers/models/auto/modeling_tf_auto.py
@@ -77,6 +77,7 @@ TF_MODEL_MAPPING_NAMES = OrderedDict(
         ("vit", "TFViTModel"),
         ("vit_mae", "TFViTMAEModel"),
         ("wav2vec2", "TFWav2Vec2Model"),
+        ("xglm", "TFXGLMModel"),
         ("xlm", "TFXLMModel"),
         ("xlm-roberta", "TFXLMRobertaModel"),
         ("xlnet", "TFXLNetModel"),
@@ -161,6 +162,7 @@ TF_MODEL_FOR_CAUSAL_LM_MAPPING_NAMES = OrderedDict(
         ("roberta", "TFRobertaForCausalLM"),
         ("roformer", "TFRoFormerForCausalLM"),
         ("transfo-xl", "TFTransfoXLLMHeadModel"),
+        ("xglm", "TFXGLMForCausalLM"),
         ("xlm", "TFXLMWithLMHeadModel"),
         ("xlnet", "TFXLNetLMHeadModel"),
     ]
