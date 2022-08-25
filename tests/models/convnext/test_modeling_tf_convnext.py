f812f04c2e3e392c24a964ce8baae220eecde099
Amy Roberts <22614925+amyeroberts@users.noreply.github.com>
2022-08-25 16:28:17 +0100

    Update comment - remove outdated ref to TF 2.8

--- tests/models/convnext/test_modeling_tf_convnext.py
+++ tests/models/convnext/test_modeling_tf_convnext.py
@@ -145,7 +145,7 @@ class TFConvNextModelTest(TFModelTesterMixin, unittest.TestCase):
 
     @unittest.skipIf(
         not is_tf_available() or len(tf.config.list_physical_devices("GPU")) == 0,
-        reason="TF (<=2.8) does not support backprop for grouped convolutions on CPU.",
+        reason="TF does not support backprop for grouped convolutions on CPU.",
     )
     def test_keras_fit(self):
         super().test_keras_fit()
@@ -176,7 +176,7 @@ class TFConvNextModelTest(TFModelTesterMixin, unittest.TestCase):
 
     @unittest.skipIf(
         not is_tf_available() or len(tf.config.list_physical_devices("GPU")) == 0,
-        reason="TF (<=2.8) does not support backprop for grouped convolutions on CPU.",
+        reason="TF does not support backprop for grouped convolutions on CPU.",
     )
     def test_dataset_conversion(self):
         super().test_dataset_conversion()
--- tests/models/regnet/test_modeling_tf_regnet.py
+++ tests/models/regnet/test_modeling_tf_regnet.py
@@ -138,7 +138,7 @@ class TFRegNetModelTest(TFModelTesterMixin, unittest.TestCase):
 
     @unittest.skipIf(
         not is_tf_available() or len(tf.config.list_physical_devices("GPU")) == 0,
-        reason="TF (<=2.8) does not support backprop for grouped convolutions on CPU.",
+        reason="TF does not support backprop for grouped convolutions on CPU.",
     )
     def test_keras_fit(self):
         super().test_keras_fit()
