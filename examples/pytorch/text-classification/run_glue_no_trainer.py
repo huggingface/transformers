d168112f66af1ed500495342506721b605ffd1e2
Rahul A R <rahular@users.noreply.github.com>
2022-08-25 11:00:38 -0400

    streamlining 'checkpointing_steps' parsing (#18755)

--- examples/pytorch/text-classification/run_glue_no_trainer.py
+++ examples/pytorch/text-classification/run_glue_no_trainer.py
@@ -451,12 +451,9 @@ def main():
     args.num_train_epochs = math.ceil(args.max_train_steps / num_update_steps_per_epoch)
 
     # Figure out how many steps we should save the Accelerator states
-    if hasattr(args.checkpointing_steps, "isdigit"):
-        checkpointing_steps = args.checkpointing_steps
-        if args.checkpointing_steps.isdigit():
-            checkpointing_steps = int(args.checkpointing_steps)
-    else:
-        checkpointing_steps = None
+    checkpointing_steps = args.checkpointing_steps
+    if checkpointing_steps is not None and checkpointing_steps.isdigit():
+        checkpointing_steps = int(checkpointing_steps)
 
     # We need to initialize the trackers we use, and also store our configuration.
     # The trackers initializes automatically on the main process.
