d4e040904cb0f04ca88b011d5af0ebe9b13dbc29
Ekagra Ranjan <ekagra.ranjan@gmail.com>
2022-08-27 21:38:02 +0530

    update dim of label in GPT2ForTokenClassification

--- src/transformers/models/gpt2/modeling_gpt2.py
+++ src/transformers/models/gpt2/modeling_gpt2.py
@@ -1519,7 +1519,7 @@ class GPT2ForTokenClassification(GPT2PreTrainedModel):
         return_dict: Optional[bool] = None,
     ) -> Union[Tuple, TokenClassifierOutput]:
         r"""
-        labels (`torch.LongTensor` of shape `(batch_size,)`, *optional*):
+        labels (`torch.LongTensor` of shape `(batch_size, sequence_length)`, *optional*):
             Labels for computing the sequence classification/regression loss. Indices should be in `[0, ...,
             config.num_labels - 1]`. If `config.num_labels == 1` a regression loss is computed (Mean-Square loss), If
             `config.num_labels > 1` a classification loss is computed (Cross-Entropy).
