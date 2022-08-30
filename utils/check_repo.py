992669bdbfb38e6d21bf68cee163b2c4950aec6f
NielsRogge <48327001+NielsRogge@users.noreply.github.com>
2022-08-30 14:15:36 +0200

    Remove ViltForQuestionAnswering from check_repo (#18762)
    Co-authored-by: Niels Rogge <nielsrogge@Nielss-MacBook-Pro.local>

--- utils/check_repo.py
+++ utils/check_repo.py
@@ -128,7 +128,6 @@ IGNORE_NON_AUTO_CONFIGURED = PRIVATE_MODELS.copy() + [
     "DPTForDepthEstimation",
     "DecisionTransformerGPT2Model",
     "GLPNForDepthEstimation",
-    "ViltForQuestionAnswering",
     "ViltForImagesAndTextClassification",
     "ViltForImageAndTextRetrieval",
     "ViltForTokenClassification",
