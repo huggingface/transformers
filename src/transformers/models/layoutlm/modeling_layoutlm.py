159d921e4b94cad968359e5bb4b9e78ca98aa7d0
Ankur Goyal <ankur@impira.com>
2022-09-01 09:33:30 -0700

    Shorten revisions

--- src/transformers/models/layoutlm/modeling_layoutlm.py
+++ src/transformers/models/layoutlm/modeling_layoutlm.py
@@ -1285,9 +1285,7 @@ class LayoutLMForQuestionAnswering(LayoutLMPreTrainedModel):
         >>> import torch
 
         >>> tokenizer = AutoTokenizer.from_pretrained("impira/layoutlm-document-qa", add_prefix_space=True)
-        >>> model = LayoutLMForQuestionAnswering.from_pretrained(
-        ...     "impira/layoutlm-document-qa", revision="1e3ebac"
-        ... )
+        >>> model = LayoutLMForQuestionAnswering.from_pretrained("impira/layoutlm-document-qa", revision="1e3ebac")
 
         >>> dataset = load_dataset("nielsr/funsd", split="train")
         >>> example = dataset[0]
