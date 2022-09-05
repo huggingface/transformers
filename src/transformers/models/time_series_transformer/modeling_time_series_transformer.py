9763a3e054353bf52462a75e73f7a7f38b4abeef
Kashif Rasul <kashif.rasul@gmail.com>
2022-09-05 13:36:46 +0200

    avoid _variable names

--- src/transformers/models/time_series_transformer/modeling_time_series_transformer.py
+++ src/transformers/models/time_series_transformer/modeling_time_series_transformer.py
@@ -101,10 +101,10 @@ class ParameterProjection(nn.Module):
 class LambdaLayer(nn.Module):
     def __init__(self, function):
         super().__init__()
-        self._func = function
+        self.function = function
 
     def forward(self, x, *args):
-        return self._func(x, *args)
+        return self.function(x, *args)
 
 
 class Output:
@@ -201,21 +201,21 @@ class FeatureEmbedder(nn.Module):
     ) -> None:
         super().__init__()
 
-        self._num_features = len(cardinalities)
-        self._embedders = nn.ModuleList([nn.Embedding(c, d) for c, d in zip(cardinalities, embedding_dims)])
+        self.num_features = len(cardinalities)
+        self.embedders = nn.ModuleList([nn.Embedding(c, d) for c, d in zip(cardinalities, embedding_dims)])
 
     def forward(self, features: torch.Tensor) -> torch.Tensor:
         if self._num_features > 1:
             # we slice the last dimension, giving an array of length
             # self._num_features with shape (N,T) or (N)
-            cat_feature_slices = torch.chunk(features, self._num_features, dim=-1)
+            cat_feature_slices = torch.chunk(features, self.num_features, dim=-1)
         else:
             cat_feature_slices = [features]
 
         return torch.cat(
             [
                 embed(cat_feature_slice.squeeze(-1))
-                for embed, cat_feature_slice in zip(self._embedders, cat_feature_slices)
+                for embed, cat_feature_slice in zip(self.embedders, cat_feature_slices)
             ],
             dim=-1,
         )
