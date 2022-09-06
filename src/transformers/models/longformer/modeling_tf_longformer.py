956c997b34e60441c0b1ee76c073c1b6c1f14c0a
matt <rocketknight1@gmail.com>
2022-09-06 19:46:51 +0100

    Do some more fixed casting in TFLongFormer

--- src/transformers/models/longformer/modeling_tf_longformer.py
+++ src/transformers/models/longformer/modeling_tf_longformer.py
@@ -2375,10 +2375,20 @@ class TFLongformerForSequenceClassification(TFLongformerPreTrainedModel, TFSeque
         training: Optional[bool] = False,
     ) -> Union[TFLongformerSequenceClassifierOutput, Tuple[tf.Tensor]]:
 
-        if not isinstance(attention_mask, tf.Tensor):
+        if input_ids is not None and not isinstance(input_ids, tf.Tensor):
+            input_ids = tf.convert_to_tensor(input_ids, dtype=tf.int64)
+        else:
+            input_ids = tf.cast(input_ids, tf.int64)
+
+        if attention_mask is not None and not isinstance(attention_mask, tf.Tensor):
             attention_mask = tf.convert_to_tensor(attention_mask, dtype=tf.int64)
         else:
             attention_mask = tf.cast(attention_mask, tf.int64)
+            
+        if global_attention_mask is not None and not isinstance(global_attention_mask, tf.Tensor):
+            global_attention_mask = tf.convert_to_tensor(global_attention_mask, dtype=tf.int64)
+        else:
+            global_attention_mask = tf.cast(global_attention_mask, tf.int64)
 
         if global_attention_mask is None and input_ids is not None:
             logger.info("Initializing global attention on CLS token...")
