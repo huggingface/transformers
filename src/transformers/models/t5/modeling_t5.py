f85acb4d73a84fe9bee5279068b0430fc391fb36
Ekagra Ranjan <ekagra.ranjan@gmail.com>
2022-09-06 17:42:26 +0530

    Fix decode_input_ids to bare T5Model and improve doc (#18791)
    * use tokenizer to output tensor
    
    * add preprocessing for decoder_input_ids for bare T5Model
    
    * add preprocessing to tf and flax
    
    * linting
    
    * linting
    
    * Update src/transformers/models/t5/modeling_flax_t5.py
    
    Co-authored-by: Patrick von Platen <patrick.v.platen@gmail.com>
    
    * Update src/transformers/models/t5/modeling_tf_t5.py
    
    Co-authored-by: Patrick von Platen <patrick.v.platen@gmail.com>
    
    * Update src/transformers/models/t5/modeling_t5.py
    
    Co-authored-by: Patrick von Platen <patrick.v.platen@gmail.com>
    
    Co-authored-by: Patrick von Platen <patrick.v.platen@gmail.com>

--- src/transformers/models/t5/modeling_t5.py
+++ src/transformers/models/t5/modeling_t5.py
@@ -1383,6 +1383,10 @@ class T5Model(T5PreTrainedModel):
         ... ).input_ids  # Batch size 1
         >>> decoder_input_ids = tokenizer("Studies show that", return_tensors="pt").input_ids  # Batch size 1
 
+        >>> # preprocess: Prepend decoder_input_ids with start token which is pad token for T5Model.
+        >>> # This is not needed for torch's T5ForConditionalGeneration as it does this internally using labels arg.
+        >>> decoder_input_ids = model._shift_right(decoder_input_ids)
+
         >>> # forward pass
         >>> outputs = model(input_ids=input_ids, decoder_input_ids=decoder_input_ids)
         >>> last_hidden_states = outputs.last_hidden_state
