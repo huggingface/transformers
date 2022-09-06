b20306f955c7dc453192ac033abed5708932700d
Shivalika Singh <73357305+shivalikasingh95@users.noreply.github.com>
2022-09-07 01:35:55 +0530

    Update src/transformers/models/auto/modeling_auto.py
    Co-authored-by: Alara Dirik <8944735+alaradirik@users.noreply.github.com>

--- src/transformers/models/auto/modeling_auto.py
+++ src/transformers/models/auto/modeling_auto.py
@@ -86,7 +86,7 @@ MODEL_MAPPING_NAMES = OrderedDict(
         ("m2m_100", "M2M100Model"),
         ("marian", "MarianModel"),
         ("maskformer", "MaskFormerModel"),
-        ("maskformer", "Mask2FormerModel"),
+        ("mask2former", "Mask2FormerModel"),
         ("mbart", "MBartModel"),
         ("mctct", "MCTCTModel"),
         ("megatron-bert", "MegatronBertModel"),
