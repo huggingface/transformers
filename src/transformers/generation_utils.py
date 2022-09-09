851e4891fe3642dcf19e24b07e6f412254e05d0e
Ekagra Ranjan <ekagra.ranjan@gmail.com>
2022-09-09 21:14:56 +0530

    Generate: Simplify is_pad_token_not_equal_to_eos_token_id (#18933)

--- src/transformers/generation_utils.py
+++ src/transformers/generation_utils.py
@@ -495,9 +495,8 @@ class GenerationMixin:
     ) -> torch.LongTensor:
         is_input_ids = len(inputs.shape) == 2 and inputs.dtype in [torch.int, torch.long]
         is_pad_token_in_inputs = (pad_token_id is not None) and (pad_token_id in inputs)
-        is_pad_token_not_equal_to_eos_token_id = (eos_token_id is None) or (
-            (eos_token_id is not None) and (pad_token_id != eos_token_id)
-        )
+        is_pad_token_not_equal_to_eos_token_id = (eos_token_id is None) or (pad_token_id != eos_token_id)
+
         # Check if input is input_ids and padded -> only then is attention_mask defined
         if is_input_ids and is_pad_token_in_inputs and is_pad_token_not_equal_to_eos_token_id:
             return inputs.ne(pad_token_id).long()
