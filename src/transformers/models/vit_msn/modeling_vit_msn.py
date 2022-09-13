5ba5e80e7beefb9ed864d3360c2d003dc81d3750
Sayak Paul <spsayakpaul@gmail.com>
2022-09-13 08:32:35 +0530

    fix: basemodeloutput.

--- src/transformers/models/vit_msn/modeling_vit_msn.py
+++ src/transformers/models/vit_msn/modeling_vit_msn.py
@@ -581,7 +581,7 @@ class ViTMSNModel(ViTMSNPreTrainedModel):
         sequence_output = self.layernorm(sequence_output)
 
         if not return_dict:
-            head_outputs = sequence_output
+            head_outputs = (sequence_output,)
             return head_outputs + encoder_outputs[1:]
 
         return BaseModelOutput(
@@ -679,8 +679,6 @@ class ViTMSNForImageClassification(ViTMSNPreTrainedModel):
                     loss = loss_fct(logits, labels)
             elif self.config.problem_type == "single_label_classification":
                 loss_fct = CrossEntropyLoss()
-                print(f"Logit shape: {logits.view(-1, self.num_labels).shape}")
-                print(f"Labels shape: {labels.view(-1).shape}")
                 loss = loss_fct(logits.view(-1, self.num_labels), labels.view(-1))
             elif self.config.problem_type == "multi_label_classification":
                 loss_fct = BCEWithLogitsLoss()
