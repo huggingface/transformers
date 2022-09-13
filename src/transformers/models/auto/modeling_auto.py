a5bb2fa31aaff4afc02cdcb65e5098e3b55e1dcf
Ankur Goyal <ankrgyl@gmail.com>
2022-09-13 07:04:03 -0700

    Update default revision for document-question-answering (#18938)
    Co-authored-by: Ankur Goyal <ankur@impira.com>

--- src/transformers/models/auto/modeling_auto.py
+++ src/transformers/models/auto/modeling_auto.py
@@ -920,7 +920,7 @@ class AutoModelForDocumentQuestionAnswering(_BaseAutoModelClass):
 AutoModelForDocumentQuestionAnswering = auto_class_update(
     AutoModelForDocumentQuestionAnswering,
     head_doc="document question answering",
-    checkpoint_for_example='impira/layoutlm-document-qa", revision="3dc6de3',
+    checkpoint_for_example='impira/layoutlm-document-qa", revision="52e01b3',
 )
 
 
