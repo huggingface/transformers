da872fda31ce071238052db1455afdc381e45939
SO0529 <gioca4.0529@gmail.com>
2022-09-14 02:47:18 +0000

    remove unused import from gpt-neox-japanese

--- src/transformers/models/gpt_neox_japanese/modeling_gpt_neox_japanese.py
+++ src/transformers/models/gpt_neox_japanese/modeling_gpt_neox_japanese.py
@@ -22,12 +22,7 @@ from torch import Tensor, nn
 from torch.nn import CrossEntropyLoss
 
 from ...activations import ACT2FN
-from ...file_utils import (
-    add_code_sample_docstrings,
-    add_start_docstrings,
-    add_start_docstrings_to_model_forward,
-    replace_return_docstrings,
-)
+from ...file_utils import add_start_docstrings, add_start_docstrings_to_model_forward, replace_return_docstrings
 from ...modeling_outputs import BaseModelOutputWithPast, CausalLMOutputWithPast
 from ...modeling_utils import PreTrainedModel
 from ...utils import logging
