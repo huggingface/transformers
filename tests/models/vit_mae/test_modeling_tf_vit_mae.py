9702477d39e15306f6a25b0d40af942d94ea4e9d
ydshieh <ydshieh@users.noreply.github.com>
2022-09-15 10:31:24 +0200

    Fix test_save_load for TFViTMAEModelTest

--- tests/models/vit_mae/test_modeling_tf_vit_mae.py
+++ tests/models/vit_mae/test_modeling_tf_vit_mae.py
@@ -375,7 +375,6 @@ class TFViTMAEModelTest(TFModelTesterMixin, unittest.TestCase):
 
     # overwrite from common since TFViTMAEForPretraining has random masking, we need to fix the noise
     # to generate masks during test
-    @slow
     def test_save_load(self):
         # make mask reproducible
         np.random.seed(2)
@@ -398,7 +397,7 @@ class TFViTMAEModelTest(TFModelTesterMixin, unittest.TestCase):
                 out_2[np.isnan(out_2)] = 0
 
             with tempfile.TemporaryDirectory() as tmpdirname:
-                model.save_pretrained(tmpdirname, saved_model=True)
+                model.save_pretrained(tmpdirname)
                 saved_model_dir = os.path.join(tmpdirname, "saved_model", "1")
                 model = tf.keras.models.load_model(saved_model_dir)
                 after_outputs = model(model_input, noise=noise)
