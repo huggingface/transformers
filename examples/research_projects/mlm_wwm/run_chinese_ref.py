60126f1bfa781ed5def29fe81735c01d3d5c4809
xyh1756 <1756221497@qq.com>
2022-09-15 15:56:02 +0800

    Fix: update ltp word segmentation call in mlm_wwm

--- examples/research_projects/mlm_wwm/run_chinese_ref.py
+++ examples/research_projects/mlm_wwm/run_chinese_ref.py
@@ -79,20 +79,21 @@ def prepare_ref(lines: List[str], ltp_tokenizer: LTP, bert_tokenizer: BertTokeni
     ltp_res = []
 
     for i in range(0, len(lines), 100):
-        res = ltp_tokenizer.pipeline(lines[i: i + 100], tasks=["cws"]).cws
+        res = ltp_tokenizer.pipeline(lines[i : i + 100], tasks=["cws"]).cws
         res = [get_chinese_word(r) for r in res]
         ltp_res.extend(res)
     assert len(ltp_res) == len(lines)
 
     bert_res = []
     for i in range(0, len(lines), 100):
-        res = bert_tokenizer(lines[i : i + 100], add_special_tokens=True, truncation=True, max_length=512)
+        res = bert_tokenizer(
+            lines[i : i + 100], add_special_tokens=True, truncation=True, max_length=512
+        )
         bert_res.extend(res["input_ids"])
     assert len(bert_res) == len(lines)
 
     ref_ids = []
     for input_ids, chinese_word in zip(bert_res, ltp_res):
-
         input_tokens = []
         for id in input_ids:
             token = bert_tokenizer._convert_id_to_token(id)
@@ -118,7 +119,9 @@ def main(args):
     # If we want to fine-tune these model, we have to use same tokenizer : LTP (https://github.com/HIT-SCIR/ltp)
     with open(args.file_name, "r", encoding="utf-8") as f:
         data = f.readlines()
-    data = [line.strip() for line in data if len(line) > 0 and not line.isspace()]  # avoid delimiter like '\u2029'
+    data = [
+        line.strip() for line in data if len(line) > 0 and not line.isspace()
+    ]  # avoid delimiter like '\u2029'
     ltp_tokenizer = LTP(args.ltp)  # faster in GPU device
     bert_tokenizer = BertTokenizer.from_pretrained(args.bert)
 
@@ -133,15 +136,32 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="prepare_chinese_ref")
     parser.add_argument(
         "--file_name",
+        required=False,
         type=str,
         default="./resources/chinese-demo.txt",
         help="file need process, same as training data in lm",
     )
     parser.add_argument(
-        "--ltp", type=str, default="./resources/ltp", help="resources for LTP tokenizer, usually a path"
+        "--ltp",
+        required=False,
+        type=str,
+        default="./resources/ltp",
+        help="resources for LTP tokenizer, usually a path",
     )
-    parser.add_argument("--bert", type=str, default="./resources/robert", help="resources for Bert tokenizer")
-    parser.add_argument("--save_path", type=str, default="./resources/ref.txt", help="path to save res")
+    parser.add_argument(
+        "--bert",
+        required=False,
+        type=str,
+        default="./resources/robert",
+        help="resources for Bert tokenizer",
+    )
+    parser.add_argument(
+        "--save_path",
+        required=False,
+        type=str,
+        default="./resources/ref.txt",
+        help="path to save res",
+    )
 
     args = parser.parse_args()
     main(args)
