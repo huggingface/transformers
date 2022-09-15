42d6d13e062e3f1e2b28080640a49cfe95d3a7e5
Yih-Dar <2521628+ydshieh@users.noreply.github.com>
2022-09-15 20:10:16 +0200

    Run `torchdynamo` tests (#19056)
    * Enable torchdynamo tests
    
    * make style
    
    Co-authored-by: ydshieh <ydshieh@users.noreply.github.com>

--- src/transformers/trainer.py
+++ src/transformers/trainer.py
@@ -638,14 +638,13 @@ class Trainer:
                 raise RuntimeError("Torchdynamo is not installed.")
             import torchdynamo
             from torchdynamo.optimizations import backends
-            from torchdynamo.optimizations.training import aot_autograd_speedup_strategy
 
             def get_ctx():
                 # Normal
                 if args.torchdynamo == "eager":
                     return torchdynamo.optimize("eager")
                 elif args.torchdynamo == "nvfuser":
-                    return torchdynamo.optimize(aot_autograd_speedup_strategy)
+                    return torchdynamo.optimize("aot_nvfuser")
                 # TensorRT
                 if args.torchdynamo in ["fx2trt-fp16", "fx2trt"]:
                     if not is_torch_tensorrt_fx_available():
