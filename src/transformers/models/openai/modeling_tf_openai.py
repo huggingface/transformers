76be3fddb3a8995f13ffcc725ca59d43110df960
Joao Gante <joao@huggingface.co>
2022-09-20 09:39:25 +0000

    correct typo in openai model

--- src/transformers/models/openai/modeling_tf_openai.py
+++ src/transformers/models/openai/modeling_tf_openai.py
@@ -315,7 +315,7 @@ class TFOpenAIGPTMainLayer(tf.keras.layers.Layer):
             # Note: tf.gather, on which the embedding layer is based, won't check positive out of bound
             # indices on GPU, returning zeros instead. This is a dangerous silent behavior.
             tf.debugging.assert_less(
-                input_ids,
+                token_type_ids,
                 tf.cast(self.vocab_size, dtype=input_ids.dtype),
                 message=(
                     "input_ids must be smaller than the embedding layer's input dimension (got"
