90d10eeffbc5a827e29820300e0eefdefb121f7e
younesbelkada <younesbelkada@gmail.com>
2022-09-20 16:08:47 +0000

    add `cached_property`

--- tests/models/vilt/test_modeling_vilt.py
+++ tests/models/vilt/test_modeling_vilt.py
@@ -30,6 +30,7 @@ from transformers.testing_utils import (
     slow,
     torch_device,
 )
+from transformers.utils import cached_property
 
 from ...test_configuration_common import ConfigTester
 from ...test_modeling_common import ModelTesterMixin, floats_tensor, ids_tensor, random_attention_mask
