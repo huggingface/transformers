ec83a22f26c2c840981f8df6d3d92b6c0093ef63
Arthur Zucker <arthur.zucker@gmail.com>
2022-09-22 14:07:14 +0000

    update processor for correct padding

--- src/transformers/models/whisper/processing_whisper.py
+++ src/transformers/models/whisper/processing_whisper.py
@@ -15,6 +15,8 @@
 """
 Speech processor class for Whisper
 """
+import warnings
+from contextlib import contextmanager
 
 from ...processing_utils import ProcessorMixin
 
@@ -34,45 +36,12 @@ class WhisperProcessor(ProcessorMixin):
             An instance of [`WhisperTokenizer`]. The tokenizer is a required input.
     """
     feature_extractor_class = "WhisperFeatureExtractor"
-    tokenizer_class = "WhisperTokenizer"
+    tokenizer_class = "GPT2Tokenizer"
 
     def __init__(self, feature_extractor, tokenizer):
         super().__init__(feature_extractor, tokenizer)
         self.current_processor = self.feature_extractor
         self._in_target_context_manager = False
-
-    def get_decoder_prompt_ids(self, task=None, language=None, no_timestamps=True):
-        forced_decoder_tokens = ""
-
-        if language is not None:
-            if f"<|{language}|>" not in self.tokenizer.additional_special_tokens:
-                raise ValueError(
-                    f"{language} is not supported. The language should be one of the following: '<|en|>',"
-                    " '<|zh|>', '<|de|>', '<|es|>', '<|ru|>', '<|ko|>', '<|fr|>', '<|ja|>', '<|pt|>', '<|tr|>',"
-                    " '<|pl|>', '<|ca|>', '<|nl|>', '<|ar|>', '<|sv|>', '<|it|>', '<|id|>', '<|hi|>', '<|fi|>',"
-                    " '<|vi|>', '<|iw|>', '<|uk|>', '<|el|>', '<|ms|>', '<|cs|>', '<|ro|>', '<|da|>', '<|hu|>',"
-                    " '<|ta|>', '<|no|>', '<|th|>', '<|ur|>', '<|hr|>', '<|bg|>', '<|lt|>', '<|la|>', '<|mi|>',"
-                    " '<|ml|>', '<|cy|>', '<|sk|>', '<|te|>', '<|fa|>', '<|lv|>', '<|bn|>', '<|sr|>', '<|az|>',"
-                    " '<|sl|>', '<|kn|>', '<|et|>', '<|mk|>', '<|br|>', '<|eu|>', '<|is|>', '<|hy|>', '<|ne|>',"
-                    " '<|mn|>', '<|bs|>', '<|kk|>', '<|sq|>', '<|sw|>', '<|gl|>', '<|mr|>', '<|pa|>', '<|si|>',"
-                    " '<|km|>', '<|sn|>', '<|yo|>', '<|so|>', '<|af|>', '<|oc|>', '<|ka|>', '<|be|>', '<|tg|>',"
-                    " '<|sd|>', '<|gu|>', '<|am|>', '<|yi|>', '<|lo|>', '<|uz|>', '<|fo|>', '<|ht|>', '<|ps|>',"
-                    " '<|tk|>', '<|nn|>', '<|mt|>', '<|sa|>', '<|lb|>', '<|my|>', '<|bo|>', '<|tl|>', '<|mg|>',"
-                    " '<|as|>', '<|tt|>', '<|haw|>', '<|ln|>', '<|ha|>', '<|ba|>', '<|jw|>', '<|su|>'"
-                )
-            forced_decoder_tokens += f"<|{language}|>"
-
-        if task is not None:
-            if f"<|{task}|>" not in self.tokenizer.additional_special_tokens:
-                raise ValueError(
-                    f"'{task}' is not supported. The language should be in : {{'transcribe', 'translate'}}"
-                )
-            forced_decoder_tokens += f"<|{task}|>"
-
-        forced_decoder_tokens += "<|notimestamps|>" if no_timestamps else ""
-        ids = self.tokenizer.encode(forced_decoder_tokens)
-        forced_decoder_ids = [(rank + 1, token) for rank, token in enumerate(ids)]
-        return forced_decoder_ids
 
     def __call__(self, *args, **kwargs):
         """
@@ -80,13 +49,16 @@ class WhisperProcessor(ProcessorMixin):
         [`~WhisperFeatureExtractor.__call__`] and returns its output. If used in the context
         [`~WhisperProcessor.as_target_processor`] this method forwards all its arguments to WhisperTokenizer's
         [`~WhisperTokenizer.__call__`]. Please refer to the doctsring of the above two methods for more information.
-
         """
         # For backward compatibility
         if self._in_target_context_manager:
             return self.current_processor(*args, **kwargs)
 
-        audio = kwargs.pop("audio", None)
+        if "raw_speech" in kwargs:
+            warnings.warn("Using `raw_speech` as a keyword argument is deprecated. Use `audio` instead.")
+            audio = kwargs.pop("raw_speech")
+        else:
+            audio = kwargs.pop("audio", None)
         text = kwargs.pop("text", None)
         if len(args) > 0:
             audio = args[0]
@@ -102,7 +74,6 @@ class WhisperProcessor(ProcessorMixin):
 
         if text is None:
             return inputs
-
         elif audio is None:
             return encodings
         else:
@@ -122,3 +93,20 @@ class WhisperProcessor(ProcessorMixin):
         the docstring of this method for more information.
         """
         return self.tokenizer.decode(*args, **kwargs)
+
+    @contextmanager
+    def as_target_processor(self):
+        """
+        Temporarily sets the tokenizer for processing the input. Useful for encoding the labels when fine-tuning
+        Whisper.
+        """
+        warnings.warn(
+            "`as_target_processor` is deprecated and will be removed in v5 of Transformers. You can process your "
+            "labels by using the argument `text` of the regular `__call__` method (either in the same call as "
+            "your audio inputs, or in a separate call."
+        )
+        self._in_target_context_manager = True
+        self.current_processor = self.tokenizer
+        yield
+        self.current_processor = self.feature_extractor
+        self._in_target_context_manager = False
