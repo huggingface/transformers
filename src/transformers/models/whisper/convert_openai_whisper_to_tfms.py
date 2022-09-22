974235f95b76a5ebbcab69518ba51be795d89ea7
Arthur Zucker <arthur.zucker@gmail.com>
2022-09-22 12:55:20 +0000

    copnversion for all models

--- src/transformers/models/whisper/convert_openai_whisper_to_tfms.py
+++ src/transformers/models/whisper/convert_openai_whisper_to_tfms.py
@@ -17,19 +17,14 @@ import argparse
 import torch
 from torch import nn
 
-from transformers import WhisperConfig, WhisperForConditionalGeneration
+from transformers import WhisperConfig, WhisperModel
 
 
 def remove_ignore_keys_(state_dict):
     ignore_keys = [
-        "encoder.version",
-        "decoder.version",
-        "model.encoder.version",
-        "model.decoder.version",
-        "decoder.output_projection.weight",
-        "_float_tensor",
-        "encoder.embed_positions._float_tensor",
-        "decoder.embed_positions._float_tensor",
+        "layers",
+        "blocks",
+        "proj_out.weight"
     ]
     for k in ignore_keys:
         state_dict.pop(k, None)
@@ -38,32 +33,39 @@ def remove_ignore_keys_(state_dict):
 WHISPER_MAPPING = {
     "blocks" : "layers",
     "mlp.0":"fc1",
-    "mlp:2":"fc2",
-    "attn_ln":"self_attn_layer_norm",
-    "attn.out":"self_attn.out_proj",
+    "mlp.2":"fc2",
     "mlp_ln":"final_layer_norm",
     "blocks":"layers",
-    "attn.query":"self_attn.q_proj",
-    "attn.key":"self_attn.k_proj",
-    "attn.value":"self_attn.v_proj"
-
+    ".attn.query":".self_attn.q_proj",
+    ".attn.key":".self_attn.k_proj",
+    ".attn.value":".self_attn.v_proj",
+    ".attn_ln":".self_attn_layer_norm",
+    ".attn.out":".self_attn.out_proj",
+    ".cross_attn.query":".encoder_attn.q_proj",
+    ".cross_attn.key":".encoder_attn.k_proj",
+    ".cross_attn.value":".encoder_attn.v_proj",
+    ".cross_attn_ln":".encoder_attn_layer_norm",
+    ".cross_attn.out":".encoder_attn.out_proj",
+    "decoder.ln.":"decoder.layer_norm.",
+    "encoder.ln.":"encoder.layer_norm.",
+    "token_embedding":"embed_tokens",
+    "encoder.positional_embedding":"encoder.embed_positions.weights",
+    "decoder.positional_embedding":"decoder.embed_positions.weight",
+    "ln_post":"layer_norm"
 }
 
 def rename_keys(s_dict):
     keys = list(s_dict.keys())
     for key in keys:
-        if "blocks" in key:
-            s_dict[key.replace("blocks", "layers")] = s_dict.pop(key)
-            s_dict[key.replace("attn.query", "self_attn.q_proj")] = s_dict.pop(key)
-            s_dict[key.replace("attn.key", "self_k.q_proj")] = s_dict.pop(key)
-            s_dict[key.replace("attn.value", "self_attn.v_proj")] = s_dict.pop(key)
+        new_key = key 
+        for k,v in WHISPER_MAPPING.items():
+            if k in key:
+                new_key = new_key.replace(k, v)
 
+        print(f"{key} -> {new_key}")
 
-
-
-        elif "subsample" in key:
-            s_dict[key.replace("subsample", "conv")] = s_dict.pop(key)
-
+        s_dict[new_key] = s_dict.pop(key)
+    return s_dict
 
 def make_linear_from_emb(emb):
     vocab_size, emb_size = emb.weight.shape
@@ -134,26 +136,99 @@ def convert_openai_whisper_to_tfms(checkpoint_path, pytorch_dump_folder_path):
 
     model.save_pretrained(pytorch_dump_folder_path)
 
+_MODELS = {
+    "tiny.en": "https://openaipublic.azureedge.net/main/whisper/models/d3dd57d32accea0b295c96e26691aa14d8822fac7d9d27d5dc00b4ca2826dd03/tiny.en.pt",
+    "tiny": "https://openaipublic.azureedge.net/main/whisper/models/65147644a518d12f04e32d6f3b26facc3f8dd46e5390956a9424a650c0ce22b9/tiny.pt",
+    "base.en": "https://openaipublic.azureedge.net/main/whisper/models/25a8566e1d0c1e2231d1c762132cd20e0f96a85d16145c3a00adf5d1ac670ead/base.en.pt",
+    "base": "https://openaipublic.azureedge.net/main/whisper/models/ed3a0b6b1c0edf879ad9b11b1af5a0e6ab5db9205f891f668f8b0e6c6326e34e/base.pt",
+    "small.en": "https://openaipublic.azureedge.net/main/whisper/models/f953ad0fd29cacd07d5a9eda5624af0f6bcf2258be67c92b79389873d91e0872/small.en.pt",
+    "small": "https://openaipublic.azureedge.net/main/whisper/models/9ecf779972d90ba49c06d968637d720dd632c55bbf19d441fb42bf17a411e794/small.pt",
+    "medium.en": "https://openaipublic.azureedge.net/main/whisper/models/d7440d1dc186f76616474e0ff0b3b6b879abc9d1a4926b7adfa41db2d497ab4f/medium.en.pt",
+    "medium": "https://openaipublic.azureedge.net/main/whisper/models/345ae4da62f9b3d59415adc60127b97c714f32e89e936602e85993674d08dcb1/medium.pt",
+    "large": "https://openaipublic.azureedge.net/main/whisper/models/e4b87e7e0bf463eb8e6956e646f1e277e901512310def2c24bf0e11bd3c28e9a/large.pt",
+}
+
+import hashlib
+import io
+import os
+import urllib
+import warnings
+from tqdm import tqdm
+
+def _download(url: str, root: str) -> bytes:
+    os.makedirs(root, exist_ok=True)
+    filename = os.path.basename(url)
+
+    expected_sha256 = url.split("/")[-2]
+    download_target = os.path.join(root, filename)
+
+    if os.path.exists(download_target) and not os.path.isfile(download_target):
+        raise RuntimeError(f"{download_target} exists and is not a regular file")
+
+    if os.path.isfile(download_target):
+        model_bytes = open(download_target, "rb").read()
+        if hashlib.sha256(model_bytes).hexdigest() == expected_sha256:
+            return model_bytes
+        else:
+            warnings.warn(f"{download_target} exists, but the SHA256 checksum does not match; re-downloading the file")
+
+    with urllib.request.urlopen(url) as source, open(download_target, "wb") as output:
+        with tqdm(total=int(source.info().get("Content-Length")), ncols=80, unit='iB', unit_scale=True, unit_divisor=1024) as loop:
+            while True:
+                buffer = source.read(8192)
+                if not buffer:
+                    break
+
+                output.write(buffer)
+                loop.update(len(buffer))
+
+    model_bytes = open(download_target, "rb").read()
+    if hashlib.sha256(model_bytes).hexdigest() != expected_sha256:
+        raise RuntimeError("Model has been downloaded but the SHA256 checksum does not not match. Please retry loading the model.")
+
+    return model_bytes
+
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    # Required parameters
-    parser.add_argument("--fairseq_path", type=str, help="Path to the fairseq model (.pt) file.")
-    parser.add_argument("--pytorch_dump_folder_path", default=None, type=str, help="Path to the output PyTorch model.")
-    args = parser.parse_args()
-
-    tiny_config = WhisperConfig(
-        vocab_size = 51865,
-        encoder_layers = 4, 
-        encoder_attention_heads = 6,
-        decoder_attention_heads = 6,
-        decoder_layers = 4,
-        d_model = 384, 
-    )
+    # parser = argparse.ArgumentParser()
+    # # Required parameters
+    # parser.add_argument("--fairseq_path", type=str, help="Path to the fairseq model (.pt) file.")
+    # parser.add_argument("--pytorch_dump_folder_path", default=None, type=str, help="Path to the output PyTorch model.")
+    # args = parser.parse_args()
+    from transformers import WhisperConfig, WhisperModel
+    import torch 
 
 
+    layers = [4,6,12,24,32]
+    width = [384,512,768,1024,1280]
+    heads = [6, 8, 12, 16, 20]
+    name = ["tiny","base", "small","medium","large"]
+    for l,w,h,n in zip(layers, width, heads, name):
+        config = WhisperConfig(
+            vocab_size = 51865,
+            encoder_layers =l, 
+            encoder_attention_heads = h,
+            decoder_attention_heads = h,
+            decoder_layers = l,
+            d_model = w, 
+        )
+        model = WhisperModel(config)
+
+
+        model_bytes = _download(_MODELS[n], "weights")
+        with io.BytesIO(model_bytes) as fp:
+            original = torch.load(fp, map_location="cpu")["model_state_dict"]
+
+        # original = torch.load(f"/home/arthur_huggingface_co/whisper/tiny.pt")
+        new = rename_keys(original.copy())
+
+
+        missing, unexpected = model.load_state_dict(new, strict = False)
+        if missing == ["proj_out.weight"]: 
+            print("succesfully loaded")
 
 
 
 
-    convert_fairseq_s2t_checkpoint_to_tfms(args.fairseq_path, args.pytorch_dump_folder_path)
+
+
