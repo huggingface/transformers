1dc67a6bbda7a7213bc5858448c45346d981ff50
matt <rocketknight1@gmail.com>
2022-09-23 18:31:16 +0100

    Clean up RotaryEmbedding docstring

--- src/transformers/models/esm/modeling_esm.py
+++ src/transformers/models/esm/modeling_esm.py
@@ -72,13 +72,8 @@ def apply_rotary_pos_emb(x, cos, sin):
 
 class RotaryEmbedding(torch.nn.Module):
     """
-    The rotary position embeddings from RoFormer_ (Su et. al). A crucial insight from the method is that the query and
-    keys are transformed by rotation matrices which depend on the relative positions. Other implementations are
-    available in the Rotary Transformer repo_ and in GPT-NeoX_, GPT-NeoX was an inspiration .. _RoFormer:
-    https://arxiv.org/abs/2104.09864 .. _repo: https://github.com/ZhuiyiTechnology/roformer .. _GPT-NeoX:
-    https://github.com/EleutherAI/gpt-neox .. warning: Please note that this embedding is not registered on purpose, as
-    it is transformative
-        (it does not create the embedding dimension) and will likely be picked up (imported) on a ad-hoc basis
+    Rotary position embeddings based on those in [RoFormer](https://huggingface.co/docs/transformers/model_doc/roformer).
+    Query and keys are transformed by rotation matrices which depend on their relative positions.
     """
 
     def __init__(self, dim: int):
