4a6d32ae957851b0dc934b3f80f12fddada73511
Alara Dirik <alaradirik@gmail.com>
2022-09-26 17:47:09 +0300

    fix bugs

--- src/transformers/models/detr/feature_extraction_detr.py
+++ src/transformers/models/detr/feature_extraction_detr.py
@@ -1157,22 +1157,22 @@ class DetrFeatureExtractor(FeatureExtractionMixin, ImageFeatureExtractionMixin):
                 mask_probs[i], pred_scores[i], pred_labels[i], object_mask_threshold, num_labels
             )
 
-            # Resize mask to corresponding target_size
-            if target_sizes is not None:
-                mask_probs_item = torch.nn.functional.interpolate(
-                    mask_probs_item.unsqueeze(0),
-                    size=target_sizes[i],
-                    mode="bilinear",
-                    align_corners=False,
-                )[0]
-
-            _, height, width = mask_probs_item.shape
-            object_detected = mask_probs_item.shape[0] > 0
-
+            height, width = target_sizes[i][0], target_sizes[i][1]
             segmentation = torch.zeros((height, width), dtype=torch.int32, device=mask_probs_item.device)
             segments: List[Dict] = []
 
+            object_detected = mask_probs_item.shape[0] > 0
+
             if object_detected:
+                # Resize mask to corresponding target_size
+                if target_sizes is not None:
+                    mask_probs_item = torch.nn.functional.interpolate(
+                        mask_probs_item.unsqueeze(0),
+                        size=target_sizes[i],
+                        mode="bilinear",
+                        align_corners=False,
+                    )[0]
+
                 current_segment_id = 0
 
                 # Weigh each mask by its prediction score
@@ -1211,6 +1211,8 @@ class DetrFeatureExtractor(FeatureExtractionMixin, ImageFeatureExtractionMixin):
                                 "label_id": pred_class,
                             }
                         )
+            else:
+                segmentation -= 1
 
             # Return segmentation map in run-length encoding (RLE) format
             if return_coco_format:
@@ -1347,6 +1349,8 @@ class DetrFeatureExtractor(FeatureExtractionMixin, ImageFeatureExtractionMixin):
                         )
                         if should_fuse:
                             stuff_memory_list[pred_class] = current_segment_id
+            else:
+                segmentation -= 1
 
             results.append({"segmentation": segmentation, "segments": segments})
         return results
