9353e3ab4762f00e1668b01cf15b1008c49769f1
Arthur Zucker <arthur.zucker@gmail.com>
2022-09-27 17:05:57 +0000

    update condfig to add second non supress tokens

--- src/transformers/models/whisper/configuration_whisper.py
+++ src/transformers/models/whisper/configuration_whisper.py
@@ -26,6 +26,17 @@ WHISPER_PRETRAINED_CONFIG_ARCHIVE_MAP = {
 
 # fmt: off
 NON_SPEECH_TOKENS = [
+        1, 2, 6, 7, 8, 9, 10, 12, 14, 25,
+        26, 27, 28, 29, 31, 58, 59, 60, 61, 62,
+        63, 90, 91, 92, 93, 357, 366, 438, 532, 685,
+        705, 796, 930, 1058, 1220, 1267, 1279, 1303, 1343, 1377,
+        1391, 1635, 1782, 1875, 2162, 2361, 2488, 3467, 4008, 4211,
+        4600, 4808, 5299, 5855, 6329, 7203, 9609, 9959, 10563, 10786,
+        11420, 11709, 11907, 13163, 13697, 13700, 14808, 15306, 16410, 16791,
+        17992, 19203, 19510, 20724, 22305, 22935, 27007, 30109, 30420, 33409,
+        34949, 40283, 40493, 40549, 47282, 49146
+]
+NON_SPEECH_TOKENS_MULTI = [
     1, 2, 6, 7, 8, 9, 10, 12, 14, 25,
     26, 27, 28, 29, 31, 58, 59, 60, 61, 62,
     63, 90, 91, 92, 93, 359, 503, 522, 542, 873,
@@ -37,7 +48,6 @@ NON_SPEECH_TOKENS = [
     42863, 47425, 49870, 50254
 ]
 # fmt: on
-
 
 class WhisperConfig(PretrainedConfig):
     r"""
@@ -139,7 +149,7 @@ class WhisperConfig(PretrainedConfig):
         encoder_ffn_dim=1536,
         encoder_layerdrop=0.0,
         decoder_layerdrop=0.0,
-        decoder_start_token_id=[50258, 50259, 50359],
+        decoder_start_token_id=50258,
         use_cache=True,
         is_encoder_decoder=True,
         activation_function="gelu",
