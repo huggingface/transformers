0e3eb972043e4777de176646be3f738db11aeec6
ydshieh <ydshieh@users.noreply.github.com>
2022-09-28 13:33:35 +0200

    Add status placeholder

--- utils/get_ci_error_statistics.py
+++ utils/get_ci_error_statistics.py
@@ -155,12 +155,12 @@ def reduce_by_model(logs, error_filter=None):
 
 
 def make_github_table(reduced_by_error):
-    header = "| no. | error |"
-    sep = "|-:|:-|"
+    header = "| no. | error | status |"
+    sep = "|-:|:-|:-|"
     lines = [header, sep]
     for error in reduced_by_error:
         count = reduced_by_error[error]["count"]
-        line = f"| {count} | {error[:100]} |"
+        line = f"| {count} | {error[:100]} |  |"
         lines.append(line)
 
     return "\n".join(lines)
