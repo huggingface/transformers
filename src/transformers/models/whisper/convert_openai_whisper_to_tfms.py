3d0db33e23eff0ad7dbc873425b93d926fee93bf
Arthur <48595927+ArthurZucker@users.noreply.github.com>
2022-09-29 16:59:08 +0200

    Update src/transformers/models/whisper/convert_openai_whisper_to_tfms.py
    Co-authored-by: NielsRogge <48327001+NielsRogge@users.noreply.github.com>

--- src/transformers/models/whisper/convert_openai_whisper_to_tfms.py
+++ src/transformers/models/whisper/convert_openai_whisper_to_tfms.py
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+"""Convert Whisper checkpoints from the original repository. URL: https://github.com/openai/whisper"""
 
 import argparse
 import hashlib
