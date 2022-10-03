b773c04d4f3d6be2a5b37c587659c31487eb3134
vumichien <vumichien1692@gmail.com>
2022-10-03 23:39:13 +0900

    audio classification script

--- src/transformers/models/trillsson_efficient/modeling_trillsson_efficient.py
+++ src/transformers/models/trillsson_efficient/modeling_trillsson_efficient.py
@@ -409,15 +409,12 @@ TRILLSSON_EFFICIENT_START_DOCSTRING = r"""
 
     Trillsson_efficient was proposed in [TRILLsson: Small, Universal Speech Representations for Paralinguistic Tasks]
     (https://arxiv.org/pdf/2203.00236) by Joel Shor, Subhashini Venugopalan.
-    
     This model inherits from [`PreTrainedModel`]. Check the superclass documentation for the generic methods the
     library implements for all its model (such as downloading or saving etc.).
-    
     This model is a PyTorch [torch.nn.Module](https:
         //pytorch.org/docs/stable/nn.html#torch.nn.Module) subclass. Use it
     as a regular PyTorch Module and refer to the PyTorch documentation for all matter related to general usage and
     behavior.
-    
     Parameters:
         config ([`Trillsson_efficientConfig`]): Model configuration class with all the parameters of the model.
             Initializing with a config file does not load the weights associated with the model, only the
