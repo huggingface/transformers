f57604171d41f04dc0894dde2096373d1c2a770b
Sylvain Gugger <Sylvain.gugger@gmail.com>
2022-10-03 13:23:11 -0400

    Try without sorting

--- utils/create_circleci_config.py
+++ utils/create_circleci_config.py
@@ -337,7 +337,7 @@ def create_circleci_config(folder=None):
         config["jobs"] = {j.job_name: j.to_dict() for j in jobs}
         config["workflows"] = {"version": 2, "run_tests": {"jobs": [j.job_name for j in jobs]}}
         with open(os.path.join(folder, "generated_config.yml"), "w") as f:
-            f.write(yaml.dump(config, indent=2, width=1000000))
+            f.write(yaml.dump(config, indent=2, width=1000000, sort_keys=False))
 
 
 if __name__ == "__main__":
