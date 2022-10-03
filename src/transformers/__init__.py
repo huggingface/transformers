ef58e771c622bd44ddf05f1d04082d70a7a34cc6
Arthur Zucker <arthur.zucker@gmail.com>
2022-10-03 19:05:18 +0000

    remove sentence piece dependency

--- src/transformers/__init__.py
+++ src/transformers/__init__.py
@@ -393,6 +393,8 @@ _import_structure = {
     "models.whisper": [
         "WHISPER_PRETRAINED_CONFIG_ARCHIVE_MAP",
         "WhisperConfig",
+        "WhisperFeatureExtractor",
+        "WhisperProcessor",
     ],
     "models.x_clip": [
         "XCLIP_PRETRAINED_CONFIG_ARCHIVE_MAP",
@@ -639,7 +641,6 @@ except OptionalDependencyNotAvailable:
 else:
     _import_structure["models.mctct"].append("MCTCTFeatureExtractor")
     _import_structure["models.speech_to_text"].append("Speech2TextFeatureExtractor")
-    _import_structure["models.whisper"].append("WhisperFeatureExtractor")
 
 # Tensorflow-text-specific objects
 try:
@@ -665,7 +666,6 @@ except OptionalDependencyNotAvailable:
     ]
 else:
     _import_structure["models.speech_to_text"].append("Speech2TextProcessor")
-    _import_structure["models.whisper"].append("WhisperProcessor")
 
 # Vision-specific objects
 try:
@@ -3358,7 +3358,12 @@ if TYPE_CHECKING:
     from .models.wav2vec2_phoneme import Wav2Vec2PhonemeCTCTokenizer
     from .models.wav2vec2_with_lm import Wav2Vec2ProcessorWithLM
     from .models.wavlm import WAVLM_PRETRAINED_CONFIG_ARCHIVE_MAP, WavLMConfig
-    from .models.whisper import WHISPER_PRETRAINED_CONFIG_ARCHIVE_MAP, WhisperConfig
+    from .models.whisper import (
+        WHISPER_PRETRAINED_CONFIG_ARCHIVE_MAP,
+        WhisperConfig,
+        WhisperFeatureExtractor,
+        WhisperProcessor,
+    )
     from .models.x_clip import (
         XCLIP_PRETRAINED_CONFIG_ARCHIVE_MAP,
         XCLIPConfig,
@@ -3585,7 +3590,6 @@ if TYPE_CHECKING:
     else:
         from .models.mctct import MCTCTFeatureExtractor
         from .models.speech_to_text import Speech2TextFeatureExtractor
-        from .models.whisper import WhisperFeatureExtractor
 
     try:
         if not is_tensorflow_text_available():
@@ -3602,7 +3606,6 @@ if TYPE_CHECKING:
         from .utils.dummy_sentencepiece_and_speech_objects import *
     else:
         from .models.speech_to_text import Speech2TextProcessor
-        from .models.whisper import WhisperProcessor
 
     try:
         if not is_vision_available():
