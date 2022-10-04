7152743d58ca87b623e9ea47cea365fc8242d30c
Kotmin <70173732+Kotmin@users.noreply.github.com>
2022-10-04 23:06:47 +0200

    cleaner code better format at docstrings

--- scripts/stale.py
+++ scripts/stale.py
@@ -32,12 +32,13 @@ LABELS_TO_EXEMPT = [
 
 
 def main():
-    g = Github(os.environ["GITHUB_TOKEN"])
-    repo = g.get_repo("huggingface/transformers")
+    git_handler = Github(os.environ["GITHUB_TOKEN"])
+    repo = git_handler.get_repo("huggingface/transformers")
     open_issues = repo.get_issues(state="open")
 
     for issue in open_issues:
-        comments = sorted([comment for comment in issue.get_comments()], key=lambda i: i.created_at, reverse=True)
+        comments = sorted([comment for comment in issue.get_comments()],
+                           key=lambda i: i.created_at, reverse=True)
         last_comment = comments[0] if len(comments) > 0 else None
         if (
             last_comment is not None and last_comment.user.login == "github-actions[bot]"
@@ -45,14 +46,17 @@ def main():
             and (dt.utcnow() - issue.created_at).days >= 30
             and not any(label.name.lower() in LABELS_TO_EXEMPT for label in issue.get_labels())
         ):
-            # print(f"Would close issue {issue.number} since it has been 7 days of inactivity since bot mention.")
+            ''' 
+            print(f"Would close issue {issue.number} since it has been 7 days of inactivity
+            since bot mention.")
+            '''
             issue.edit(state="closed")
         if (
             (dt.utcnow() - issue.updated_at).days > 23
             and (dt.utcnow() - issue.created_at).days >= 30
             and not any(label.name.lower() in LABELS_TO_EXEMPT for label in issue.get_labels())
         ):
-            # print(f"Would add stale comment to {issue.number}")
+            ''' print(f"Would add stale comment to {issue.number}") ''' 
             issue.create_comment(
                 "This issue has been automatically marked as stale because it has not had "
                 "recent activity. If you think this still needs to be addressed "
