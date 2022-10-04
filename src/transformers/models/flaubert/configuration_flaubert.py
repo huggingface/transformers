4f3bfa24fa04cb13552385f1e907a12db39a4311
Druhin Abrol <druhinabrol@gmail.com>
2022-10-05 03:30:56 +0530

    Update configuration_flaubert.py

--- src/transformers/models/flaubert/configuration_flaubert.py
+++ src/transformers/models/flaubert/configuration_flaubert.py
@@ -13,12 +13,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """ Flaubert configuration"""
-
 from collections import OrderedDict
 from typing import Mapping
 
 from ...configuration_utils import PretrainedConfig
-
 from ...onnx import OnnxConfig
 from ...utils import logging
 
