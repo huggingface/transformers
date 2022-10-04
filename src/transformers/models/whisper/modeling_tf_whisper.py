19766a6cf2389163130dd69dc0a55b2d58396ed1
Amy Roberts <22614925+amyeroberts@users.noreply.github.com>
2022-10-04 10:37:41 +0100

    Remove torch references

--- src/transformers/models/whisper/modeling_tf_whisper.py
+++ src/transformers/models/whisper/modeling_tf_whisper.py
@@ -492,19 +492,19 @@ class TFWhisperPreTrainedModel(TFPreTrainedModel):
 
 
 WHISPER_START_DOCSTRING = r"""
-    This model inherits from [`PreTrainedModel`]. Check the superclass documentation for the generic methods the
+    This model inherits from [`TFPreTrainedModel`]. Check the superclass documentation for the generic methods the
     library implements for all its model (such as downloading or saving, resizing the input embeddings, pruning heads
     etc.)
 
-    This model is also a PyTorch [torch.nn.Module](https://pytorch.org/docs/stable/nn.html#torch.nn.Module) subclass.
-    Use it as a regular PyTorch Module and refer to the PyTorch documentation for all matter related to general usage
-    and behavior.
+    This model is also a [tf.keras.Model](https://www.tensorflow.org/api_docs/python/tf/keras/Model) subclass. Use it
+    as a regular TF 2.0 Keras Model and refer to the TF 2.0 documentation for all matter related to general usage and
+    behavior.
 
     Parameters:
         config ([`WhisperConfig`]):
             Model configuration class with all the parameters of the model. Initializing with a config file does not
             load the weights associated with the model, only the configuration. Check out the
-            [`~PreTrainedModel.from_pretrained`] method to load the model weights.
+            [`~TFPreTrainedModel.from_pretrained`] method to load the model weights.
 """
 
 WHISPER_INPUTS_DOCSTRING = r"""
@@ -1131,7 +1131,7 @@ class TFWhisperModel(TFWhisperPreTrainedModel):
         Example:
 
          ```python
-         >>> import torch
+         >>> import tensorflow as tf
          >>> from transformers import TFWhisperModel, WhisperFeatureExtractor
          >>> from datasets import load_dataset
 
