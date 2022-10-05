d9e3621aedae4df0e64f6bf7435fc8e279356568
Alara Dirik <alaradirik@gmail.com>
2022-10-05 13:32:55 +0200

    fix bug

--- src/transformers/models/vit/modeling_vit.py
+++ src/transformers/models/vit/modeling_vit.py
@@ -68,14 +68,14 @@ class ViTEmbeddings(nn.Module):
         super().__init__()
 
         self.cls_token = nn.Parameter(
-            nn.init.trunc_normal_(torch.zeros(1, 1, config.hidden_size), mean=0.0, std=self.config.initializer_range)
+            nn.init.trunc_normal_(torch.zeros(1, 1, config.hidden_size), mean=0.0, std=config.initializer_range)
         )
         self.mask_token = nn.Parameter(torch.zeros(1, 1, config.hidden_size)) if use_mask_token else None
         self.patch_embeddings = ViTPatchEmbeddings(config)
         num_patches = self.patch_embeddings.num_patches
         self.position_embeddings = nn.Parameter(
             nn.init.trunc_normal_(
-                torch.zeros(1, num_patches + 1, config.hidden_size), mean=0.0, std=self.config.initializer_range
+                torch.zeros(1, num_patches + 1, config.hidden_size), mean=0.0, std=config.initializer_range
             )
         )
         self.dropout = nn.Dropout(config.hidden_dropout_prob)
