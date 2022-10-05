9eb67c6de3f77bb997f5ff7629d6fbf5f4dd1725
Vision <gitesh.ch.0912@gmail.com>
2022-10-05 21:09:29 +0530

    Update tokenization_convbert_fast.py

--- src/transformers/models/convbert/tokenization_convbert_fast.py
+++ src/transformers/models/convbert/tokenization_convbert_fast.py
@@ -20,7 +20,7 @@ from tokenizers import normalizers
 
 from ...tokenization_utils_fast import PreTrainedTokenizerFast
 from ...utils import logging
-from .tokenization_convbert import BertTokenizer, ConvBertTokenizer
+from .tokenization_convbert import ConvBertTokenizer
 
 
 logger = logging.get_logger(__name__)
@@ -50,10 +50,9 @@ PRETRAINED_INIT_CONFIGURATION = {
     "YituTech/conv-bert-small": {"do_lower_case": True},
 }
 
-"""Copied from transformers.models.bert.tokenization_bert.BertTokenizerFast"""
 
 # Copied from transformers.models.bert.tokenization_bert_fast.BertTokenizerFast
-class BertTokenizerFast(PreTrainedTokenizerFast):
+class ConvBertTokenizerFast(PreTrainedTokenizerFast):
     r"""
     Construct a "fast" BERT tokenizer (backed by HuggingFace's *tokenizers* library). Based on WordPiece.
 
@@ -197,19 +196,3 @@ class BertTokenizerFast(PreTrainedTokenizerFast):
     def save_vocabulary(self, save_directory: str, filename_prefix: Optional[str] = None) -> Tuple[str]:
         files = self._tokenizer.model.save(save_directory, name=filename_prefix)
         return tuple(files)
-
-
-class ConvBertTokenizerFast(BertTokenizerFast):
-    r"""
-    Construct a "fast" ConvBERT tokenizer (backed by HuggingFace's *tokenizers* library).
-
-    [`ConvBertTokenizerFast`] is identical to [`BertTokenizerFast`] and runs end-to-end tokenization: punctuation
-    splitting and wordpiece.
-
-    Refer to superclass [`BertTokenizerFast`] for usage examples and documentation concerning parameters.
-    """
-    vocab_files_names = VOCAB_FILES_NAMES
-    pretrained_vocab_files_map = PRETRAINED_VOCAB_FILES_MAP
-    max_model_input_sizes = PRETRAINED_POSITIONAL_EMBEDDINGS_SIZES
-    pretrained_init_configuration = PRETRAINED_INIT_CONFIGURATION
-    slow_tokenizer_class = ConvBertTokenizer
