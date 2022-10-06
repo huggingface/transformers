7fb03dc83a551c5afeaa20e5aa7755b9de8355e7
amyeroberts <22614925+amyeroberts@users.noreply.github.com>
2022-10-06 16:10:38 +0100

    Update src/transformers/models/whisper/modeling_tf_whisper.py

--- src/transformers/models/whisper/modeling_tf_whisper.py
+++ src/transformers/models/whisper/modeling_tf_whisper.py
@@ -633,7 +633,7 @@ class TFWhisperEncoder(tf.keras.layers.Layer):
     ):
         r"""
         Args:
-            input_features (`tf.Tensor` of shape `(batch_size, sequence_length, feature_size)`):
+            input_features (`tf.Tensor` of shape `(batch_size, feature_size, sequence_length)`):
                 Float values of fbank features extracted from the raw speech waveform. Raw speech waveform can be
                 obtained by loading a `.flac` or `.wav` audio file into an array of type `List[float]` or a
                 `numpy.ndarray`, *e.g.* via the soundfile library (`pip install soundfile`). To prepare the array into
