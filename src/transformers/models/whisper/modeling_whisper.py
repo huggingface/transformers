408259da3c3084fe7b822d572653aa856fe3a22c
amyeroberts <22614925+amyeroberts@users.noreply.github.com>
2022-10-07 15:55:03 +0100

    Remove added whitespace

--- src/transformers/models/whisper/modeling_whisper.py
+++ src/transformers/models/whisper/modeling_whisper.py
@@ -1101,7 +1101,7 @@ class WhisperForConditionalGeneration(WhisperPreTrainedModel):
 
     def get_decoder(self):
         return self.model.get_decoder()
-        
+
     def resize_token_embeddings(self, new_num_tokens: int) -> nn.Embedding:
         new_embeddings = super().resize_token_embeddings(new_num_tokens)
         return new_embeddings
