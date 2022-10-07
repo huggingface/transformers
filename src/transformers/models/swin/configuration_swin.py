3ca708c0de9072d7ae59b4ce90e70133e997572c
Bibhabasu Mohapatra <68384968+bibhabasumohapatra@users.noreply.github.com>
2022-10-07 14:17:54 +0530

    Updated image dimensions as dynamic
    Co-authored-by: lewtun <lewis.c.tunstall@gmail.com>

--- src/transformers/models/swin/configuration_swin.py
+++ src/transformers/models/swin/configuration_swin.py
@@ -161,7 +161,7 @@ class SwinOnnxConfig(OnnxConfig):
     def inputs(self) -> Mapping[str, Mapping[int, str]]:
         return OrderedDict(
             [
-                ("pixel_values", {0: "batch", 1: "num_channels"}),
+                ("pixel_values", {0: "batch", 1: "num_channels", 2: "height", 3: "width"}),
             ]
         )
 
