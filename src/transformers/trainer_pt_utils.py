188941ed199db49e6b8e1b977d2e86704ce0d6bf
Guillem Orellana Trullols <guillem.orellana@gmail.com>
2022-10-10 20:14:34 +0200

    Update src/transformers/trainer_pt_utils.py
    Co-authored-by: Sylvain Gugger <35901082+sgugger@users.noreply.github.com>

--- src/transformers/trainer_pt_utils.py
+++ src/transformers/trainer_pt_utils.py
@@ -349,7 +349,6 @@ def nested_truncate(tensors, limit):
     "Truncate `tensors` at `limit` (even if it's a nested list/tuple/dict of tensors)."
     if isinstance(tensors, (list, tuple)):
         return type(tensors)(nested_truncate(t, limit) for t in tensors)
-
     if isinstance(tensors, Mapping):
         return type(tensors)({k: nested_truncate(t, limit) for k, t in tensors.items()})
 
