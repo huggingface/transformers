8b6bba54a707a718149f11c2b0631e6ccd715fba
Yih-Dar <2521628+ydshieh@users.noreply.github.com>
2022-10-10 08:51:30 +0100

    Fix `ViTMSNForImageClassification` doctest (#19275)
    Co-authored-by: ydshieh <ydshieh@users.noreply.github.com>

--- src/transformers/models/vit_msn/modeling_vit_msn.py
+++ src/transformers/models/vit_msn/modeling_vit_msn.py
@@ -632,7 +632,7 @@ class ViTMSNForImageClassification(ViTMSNPreTrainedModel):
         >>> from PIL import Image
         >>> import requests
 
-        >>> torch.manual_seed(2)
+        >>> torch.manual_seed(2)  # doctest: +IGNORE_RESULT
 
         >>> url = "http://images.cocodataset.org/val2017/000000039769.jpg"
         >>> image = Image.open(requests.get(url, stream=True).raw)
