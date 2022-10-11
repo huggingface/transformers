e36451b120953559376a384a968528a7f85ab4e2
ydshieh <ydshieh@users.noreply.github.com>
2022-10-11 11:23:18 +0200

    BertConfig for doctest

--- src/transformers/models/bert/configuration_bert.py
+++ src/transformers/models/bert/configuration_bert.py
@@ -128,7 +128,7 @@ class BertConfig(PretrainedConfig):
     >>> # Initializing a BERT bert-base-uncased style configuration
     >>> configuration = BertConfig()
 
-    >>> # Initializing a model from the bert-base-uncased style configuration
+    >>> # Initializing a model (with random weights) from the bert-base-uncased style configuration
     >>> model = BertModel(configuration)
 
     >>> # Accessing the model configuration
