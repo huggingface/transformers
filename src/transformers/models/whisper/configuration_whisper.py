cb60f310aae1fbc576fc59302770944d36117ced
daspartho <parthodas6176@gmail.com>
2022-10-12 23:19:18 +0530

    restyle fix

--- src/transformers/models/whisper/configuration_whisper.py
+++ src/transformers/models/whisper/configuration_whisper.py
@@ -144,7 +144,6 @@ class WhisperConfig(PretrainedConfig):
 
     >>> # Accessing the model configuration
     >>> configuration = model.config
-
     ```"""
     model_type = "whisper"
     keys_to_ignore_at_inference = ["past_key_values"]
