c62284a200def524a512875465ab4872a33ea064
Vision <gitesh.ch.0912@gmail.com>
2022-10-12 22:04:13 +0530

    Update modeling_deberta_v2.py

--- src/transformers/models/deberta_v2/modeling_deberta_v2.py
+++ src/transformers/models/deberta_v2/modeling_deberta_v2.py
@@ -1616,15 +1616,15 @@ class DebertaV2ForMultipleChoice(DebertaV2PreTrainedModel):
     )
     def forward(
         self,
-        input_ids=None,
-        attention_mask=None,
-        token_type_ids=None,
-        position_ids=None,
-        inputs_embeds=None,
-        labels=None,
-        output_attentions=None,
-        output_hidden_states=None,
-        return_dict=None,
+        input_ids: Optional[torch.Tensor] = None,
+        attention_mask: Optional[torch.Tensor] = None,
+        token_type_ids: Optional[torch.Tensor] = None,
+        position_ids: Optional[torch.Tensor] = None,
+        inputs_embeds: Optional[torch.Tensor] = None,
+        labels: Optional[torch.Tensor] = None,
+        output_attentions: Optional[bool] = None,
+        output_hidden_states: Optional[bool] = None,
+        return_dict: Optional[bool] = None,
     ):
         r"""
         labels (`torch.LongTensor` of shape `(batch_size,)`, *optional*):
