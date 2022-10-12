06cfb5948033f29a2af30dde18109ff1e3e48a58
daspartho <parthodas6176@gmail.com>
2022-10-12 22:45:43 +0530

    YOLOS Config for doctest

--- src/transformers/models/yolos/configuration_yolos.py
+++ src/transformers/models/yolos/configuration_yolos.py
@@ -92,16 +92,17 @@ class YolosConfig(PretrainedConfig):
     Example:
 
     ```python
-    >>> from transformers import YolosModel, YolosConfig
+    >>> from transformers import YolosConfig, YolosModel
 
     >>> # Initializing a YOLOS hustvl/yolos-base style configuration
     >>> configuration = YolosConfig()
 
-    >>> # Initializing a model from the hustvl/yolos-base style configuration
+    >>> # Initializing a model (with random weights) from the hustvl/yolos-base style configuration
     >>> model = YolosModel(configuration)
 
     >>> # Accessing the model configuration
     >>> configuration = model.config
+
     ```"""
     model_type = "yolos"
 
