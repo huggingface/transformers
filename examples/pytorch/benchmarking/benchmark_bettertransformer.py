585d39cc1301b2ea046b2c9eaff93fa4426df51e
Hamid Shojanazeri <hamid.nazeri2010@gmail.com>
2022-10-12 22:27:08 +0000

    formatted

--- examples/pytorch/benchmarking/benchmark_bettertransformer.py
+++ examples/pytorch/benchmarking/benchmark_bettertransformer.py
@@ -1,18 +1,21 @@
-import os
-import torch
-import argparse 
+import argparse
 import logging
-from torch.profiler import profile, record_function, ProfilerActivity
+import os
+
+import torch
+from torch.profiler import ProfilerActivity, profile, record_function
+
 
 def str2bool(v):
     if isinstance(v, bool):
         return v
-    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+    if v.lower() in ("yes", "true", "t", "y", "1"):
         return True
-    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+    elif v.lower() in ("no", "false", "f", "n", "0"):
         return False
     else:
-        raise argparse.ArgumentTypeError('Boolean value expected.')
+        raise argparse.ArgumentTypeError("Boolean value expected.")
+
 
 def get_parser():
     parser = argparse.ArgumentParser()
@@ -77,12 +80,13 @@ def get_parser():
         help="num layers to run. if -1, then use all layers",
     )
     parser.add_argument(
-        "--benchmarks", 
-        nargs="+", # + allows multiple args as lst
-        default=["hf", "bt"], # all possible benchmarks available. 
+        "--benchmarks",
+        nargs="+",  # + allows multiple args as lst
+        default=["hf", "bt"],  # all possible benchmarks available.
         help="Choose choice of benchmarks.",
     )
     return parser
+
 
 def numerical_test(tensors1, tensors2, rtol, atol):
     """
@@ -101,20 +105,21 @@ def numerical_test(tensors1, tensors2, rtol, atol):
         max_diff = max(max_diff, torch.max(torch.abs(tensor1 - tensor2)))
         if not torch.allclose(tensor1, tensor2, atol=atol, rtol=rtol):
             n_failures += 1
-    
+
     num_tested = len(tensors1)
     return n_failures, max_diff, num_tested
+
 
 def benchmark_torch_function(is_cuda, f, inputs, masks, use_mask=True):
     """
     Benchmark torch on gpu or cpu
     """
-    if(use_mask and masks is not None):
+    if use_mask and masks is not None:
         assert len(inputs) == len(masks)
     if not use_mask:
         masks = [None for _ in range(len(masks))]
     iters = len(inputs)
-    if(is_cuda):
+    if is_cuda:
         f(inputs[0], attention_mask=masks[0])
         torch.cuda.synchronize()
         start_event = torch.cuda.Event(enable_timing=True)
@@ -127,6 +132,7 @@ def benchmark_torch_function(is_cuda, f, inputs, masks, use_mask=True):
         return (start_event.elapsed_time(end_event) * 1.0e-3) / iters
     else:
         import time
+
         f(inputs[0], attention_mask=masks[0])
         start_event = time.perf_counter()
         for x, mask in zip(inputs, masks):
@@ -134,9 +140,11 @@ def benchmark_torch_function(is_cuda, f, inputs, masks, use_mask=True):
         end_event = time.perf_counter()
         return (end_event - start_event) / iters
 
+
 def get_outputs(f, inputs, masks, use_mask=True):
     outputs = [f(x, attention_mask=mask if use_mask else None).logits for x, mask in zip(inputs, masks)]
     return outputs
+
 
 def get_batch(batch_size, avg_seqlen, max_sequence_length, seqlen_stdev, vocab_size, pad_idx=0):
     mean_tensor = torch.Tensor([avg_seqlen]).expand(batch_size)
@@ -161,29 +169,31 @@ def get_batch(batch_size, avg_seqlen, max_sequence_length, seqlen_stdev, vocab_s
         mask[i, : lengths[i]] = 1
     return tokens, lengths, mask
 
-def setup_logger(filename='log.csv'):
 
-   # create log file 
+def setup_logger(filename="log.csv"):
+
+    # create log file
     if os.path.exists(filename):
         os.remove(filename)
-        open(filename, 'w').close()
-    else :
-         open(filename, 'w').close()
+        open(filename, "w").close()
+    else:
+        open(filename, "w").close()
     # create logger
-    lgr = logging.getLogger('logger name')
-    lgr.setLevel(logging.DEBUG) # log all escalated at and above DEBUG
+    lgr = logging.getLogger("logger name")
+    lgr.setLevel(logging.DEBUG)  # log all escalated at and above DEBUG
     # add a file handler
     fh = logging.FileHandler(filename)
-    fh.setLevel(logging.DEBUG) # ensure all messages are logged to file
+    fh.setLevel(logging.DEBUG)  # ensure all messages are logged to file
 
     # create a formatter and set the formatter for the handler.
     # frmt = logging.Formatter('%(asctime)s,%(name)s,%(levelname)s,%(message)s')
-    frmt = logging.Formatter('%(message)s')
+    frmt = logging.Formatter("%(message)s")
     fh.setFormatter(frmt)
 
     # add the Handler to the logger
     lgr.addHandler(fh)
     return lgr
+
 
 def cut_layers(hf_encoder, n_layers):
     """
@@ -193,30 +203,46 @@ def cut_layers(hf_encoder, n_layers):
     hf_encoder.layer = torch.nn.ModuleList([hf_encoder.layer[i] for i in range(n_layers)])
     return hf_encoder
 
-def benchmark(num_batches, batch_size, avg_seqlen, max_seqlen, seqlen_stdev, is_cuda, is_large, is_half, benchmarks, use_mask, num_layers):
+
+def benchmark(
+    num_batches,
+    batch_size,
+    avg_seqlen,
+    max_seqlen,
+    seqlen_stdev,
+    is_cuda,
+    is_large,
+    is_half,
+    benchmarks,
+    use_mask,
+    num_layers,
+):
     from transformers import AutoModelForSequenceClassification
+
     assert avg_seqlen <= max_seqlen
 
-    log_file = 'log.csv'
+    log_file = "log.csv"
     lgr = setup_logger(filename=log_file)
 
     seed = 1234
     torch.manual_seed(seed)
-    setup_str = f"{num_batches} {batch_size} {avg_seqlen} {max_seqlen} {seqlen_stdev} {is_cuda} {is_large} {num_layers} {is_half} {benchmarks} {use_mask} {seed}"
+    setup_str = (
+        f"{num_batches} {batch_size} {avg_seqlen} {max_seqlen} {seqlen_stdev} {is_cuda} {is_large} {num_layers} {is_half} {benchmarks} {use_mask} {seed}"
+    )
     lgr.info(setup_str)
 
     vocab_size = 25000
     numtype = torch.float16 if is_half else torch.float32
-    model_name = 'bert-large-cased' if is_large else 'bert-base-cased'
-    device = 'cuda' if is_cuda else 'cpu' 
+    model_name = "bert-large-cased" if is_large else "bert-base-cased"
+    device = "cuda" if is_cuda else "cpu"
 
     model = AutoModelForSequenceClassification.from_pretrained(model_name).eval().to(device).to(numtype)
-    if(num_layers >= 0):
+    if num_layers >= 0:
         model.bert.encoder = cut_layers(model.bert.encoder, num_layers)
 
     batches = [get_batch(batch_size, avg_seqlen, max_seqlen, seqlen_stdev, vocab_size) for _ in range(num_batches)]
     eval_inputs = torch.stack([batch[0] for batch in batches]).to(device)
-    masks = torch.stack([batch[2] for batch in batches]).to(device) # 0 == pad, 1 == keep
+    masks = torch.stack([batch[2] for batch in batches]).to(device)  # 0 == pad, 1 == keep
 
     # DEBUG
     # lgr.info(f"DEBUG")
@@ -224,7 +250,7 @@ def benchmark(num_batches, batch_size, avg_seqlen, max_seqlen, seqlen_stdev, is_
     # masks = torch.Tensor([[[1, 1, 0]]]).to(device)
     # print(masks.shape, eval_inputs.shape)
 
-    if("hf" in benchmarks):
+    if "hf" in benchmarks:
         with torch.no_grad():
             hf_t = benchmark_torch_function(is_cuda, model, eval_inputs, masks, use_mask=use_mask)
             out = get_outputs(model, eval_inputs, masks, use_mask=use_mask)
@@ -232,15 +258,15 @@ def benchmark(num_batches, batch_size, avg_seqlen, max_seqlen, seqlen_stdev, is_
 
     model.bert.encoder.to_fast()
 
-    if("bt" in benchmarks):
+    if "bt" in benchmarks:
         with torch.no_grad():
             bt_t = benchmark_torch_function(is_cuda, model, eval_inputs, masks, use_mask=use_mask)
             out2 = get_outputs(model, eval_inputs, masks, use_mask=use_mask)
         lgr.info(f"BT time per batch {bt_t}")
 
-    if("hf" in benchmarks and "bt" in benchmarks):
+    if "hf" in benchmarks and "bt" in benchmarks:
         lgr.info(f"BT as prop of HF {hf_t / bt_t}")
-        n_failures, max_diff, n_tested = numerical_test(out, out2, 0, 1e-2) # absolute difference of 0.01 logit
+        n_failures, max_diff, n_tested = numerical_test(out, out2, 0, 1e-2)  # absolute difference of 0.01 logit
         if n_failures == 0:
             test_str = f"HF/BT PASS"
         else:
@@ -249,18 +275,20 @@ def benchmark(num_batches, batch_size, avg_seqlen, max_seqlen, seqlen_stdev, is_
 
     print(f"Logged to {log_file}")
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     parser = get_parser()
     args = parser.parse_args()
-    benchmark(num_batches = args.num_batches,
-        batch_size = args.batch_size,
-        avg_seqlen = args.avg_seqlen,
-        max_seqlen = args.max_seqlen,
-        seqlen_stdev = args.seqlen_stdev,
-        is_cuda = args.is_cuda,
-        is_large = args.is_large,
-        is_half = args.is_half,
+    benchmark(
+        num_batches=args.num_batches,
+        batch_size=args.batch_size,
+        avg_seqlen=args.avg_seqlen,
+        max_seqlen=args.max_seqlen,
+        seqlen_stdev=args.seqlen_stdev,
+        is_cuda=args.is_cuda,
+        is_large=args.is_large,
+        is_half=args.is_half,
         benchmarks=args.benchmarks,
         use_mask=args.use_mask,
-        num_layers=args.num_layers)
-    
+        num_layers=args.num_layers,
+    )
