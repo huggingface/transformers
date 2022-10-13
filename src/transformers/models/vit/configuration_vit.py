84bf95c942922348171f746378a5cfb76b5c62b9
daspartho <parthodas6176@gmail.com>
2022-10-13 12:33:50 +0530

    fix

--- src/transformers/models/vit/configuration_vit.py
+++ src/transformers/models/vit/configuration_vit.py
@@ -78,7 +78,7 @@ class ViTConfig(PretrainedConfig):
 
     ```python
     >>> from transformers import ViTConfig, ViTModel
-    
+
     >>> # Initializing a ViT vit-base-patch16-224 style configuration
     >>> configuration = ViTConfig()
 
