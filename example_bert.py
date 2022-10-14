145355c93f51b55e6e5717a8204561939863afd4
younesbelkada <younesbelkada@gmail.com>
2022-10-14 21:35:01 +0000

    remove unneeded file

--- example_bert.py
+++
@@ -1,6 +0,0 @@
-from transformers import AutoModelForSequenceClassification
-import torch
-
-model = AutoModelForSequenceClassification.from_pretrained("bert-base-uncased").eval()
-model.to_fast()
-print(model)
