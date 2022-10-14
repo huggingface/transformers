cc001b4acea59d195faa8938f57ca7d45a23b0ee
Pi Esposito <piero.skywalker@gmail.com>
2022-10-14 09:34:44 -0300

    reformat code with black to comply with code quality checks

--- tests/models/bert/test_tokenization_bert_tf.py
+++ tests/models/bert/test_tokenization_bert_tf.py
@@ -40,7 +40,9 @@ class BertTokenizationTest(unittest.TestCase):
     def setUp(self):
         super().setUp()
 
-        self.tokenizers = [BertTokenizer.from_pretrained(checkpoint) for checkpoint in (TOKENIZER_CHECKPOINTS * 2)] # repeat for fast_bert_tokenizer=false
+        self.tokenizers = [
+            BertTokenizer.from_pretrained(checkpoint) for checkpoint in (TOKENIZER_CHECKPOINTS * 2)
+        ]  # repeat for fast_bert_tokenizer=false
         self.tf_tokenizers = [TFBertTokenizer.from_pretrained(checkpoint) for checkpoint in TOKENIZER_CHECKPOINTS] + [
             TFBertTokenizer.from_pretrained(checkpoint, use_fast_bert_tokenizer=False)
             for checkpoint in TOKENIZER_CHECKPOINTS
