3adb6659022279b35dd702042083f6c831d6e7ec
= <erik.aho24@gmail.com>
2022-10-15 19:29:14 +0100

    Working sparse attention

--- src/transformers/models/big_bird/modeling_tf_big_bird.py
+++ src/transformers/models/big_bird/modeling_tf_big_bird.py
@@ -372,6 +372,8 @@ class TFBigBirdBlockSparseAttention(tf.keras.layers.Layer):
             output_attentions=output_attentions,
         )
 
+        context_layer = tf.reshape(context_layer, (batch_size, from_seq_length, -1))
+
         return context_layer, attention_probs
 
     def bigbird_block_sparse_attention(
@@ -431,7 +433,7 @@ class TFBigBirdBlockSparseAttention(tf.keras.layers.Layer):
         rand_attn = tf.expand_dims(rand_attn, axis=0)
         rand_attn = tf.concat([rand_attn for _ in range(batch_size)], axis=0)
 
-        rand_mask = self._create_rand_mask_from_inputs(
+        rand_mask = self.create_rand_mask_from_inputs(
             from_blocked_mask,
             to_blocked_mask,
             rand_attn,
@@ -497,12 +499,12 @@ class TFBigBirdBlockSparseAttention(tf.keras.layers.Layer):
             [
                 to_mask[:, :, :, : 3 * to_block_size],
                 to_mask[:, :, :, -to_block_size:],
-                tf.ones_like(rand_mask[:, :1, 0, :1]),
+                tf.ones_like(rand_mask[:, :1, 0, :1], dtype=tf.dtypes.float32),
             ],
             3,
         )
         second_rand_pad = tf.concat(
-            [tf.ones_like(second_product[:, :, :, : 4 * to_block_size]), rand_mask[:, :, 0]], 3
+            [tf.ones_like(second_product[:, :, :, : 4 * to_block_size], dtype=tf.dtypes.float32), rand_mask[:, :, 0]], 3,
         )
         second_product = tf.multiply(second_product, rsqrt_d)
         second_product += (1.0 - tf.minimum(second_seq_pad, second_rand_pad)) * attn_mask_penalty
@@ -637,8 +639,10 @@ class TFBigBirdBlockSparseAttention(tf.keras.layers.Layer):
         context_layer = tf.reshape(context_layer, (-1, n_heads, from_seq_len, attention_head_size)) * from_mask
         context_layer = tf.transpose(context_layer, (0, 2, 1, 3))
 
+        # combine all token representations
+
         # this is just for visualizing; forward pass doesn't depend on following code
-        attention_probs = tf.zeros(batch_size, n_heads, from_seq_len, to_seq_len)
+        attention_probs = tf.zeros((batch_size, n_heads, from_seq_len, to_seq_len), dtype=tf.float32)
 
         if not output_attentions:
             return context_layer, attention_probs
@@ -785,8 +789,8 @@ class TFBigBirdBlockSparseAttention(tf.keras.layers.Layer):
 
         return plan_from_length, plan_num_rand_blocks
 
-    @staticmethod
-    def _create_rand_mask_from_inputs(
+    def create_rand_mask_from_inputs(
+            self,
             from_blocked_mask,
             to_blocked_mask,
             rand_attn,
@@ -794,32 +798,34 @@ class TFBigBirdBlockSparseAttention(tf.keras.layers.Layer):
             num_rand_blocks,
             batch_size,
             from_seq_length,
-            from_block_size,
+            from_block_size
     ):
-        """
-        Create 3D attention mask from a 2D tensor mask.
-
+        """Create 4D attention mask from a 3D tensor mask.
         Args:
-            from_blocked_mask: 2D Tensor of shape [batch_size,
+          from_blocked_mask: 2D Tensor of shape [batch_size,
             from_seq_length//from_block_size, from_block_size].
-            to_blocked_mask: int32 Tensor of shape [batch_size,
+          to_blocked_mask: int32 Tensor of shape [batch_size,
             to_seq_length//to_block_size, to_block_size].
-            rand_attn: [batch_size, num_attention_heads,
+          rand_attn: [batch_size, num_attention_heads,
             from_seq_length//from_block_size-2, num_rand_blocks]
-            num_attention_heads: int. Number of attention heads.
-            num_rand_blocks: int. Number of random chunks per row.
-            batch_size: int. Batch size for computation.
-            from_seq_length: int. length of from sequence.
-            from_block_size: int. size of block in from sequence.
-
+          num_attention_heads: int. Number of attention heads.
+          num_rand_blocks: int. Number of random chunks per row.
+          batch_size: int. Number of batrc
+          from_seq_length: int. length of from sequence.
+          from_block_size: int. size of block in from sequence.
         Returns:
-            float Tensor of shape [batch_size, num_attention_heads, from_seq_length//from_block_size-2,
-            from_block_size, num_rand_blocks*to_block_size].
+          float Tensor of shape [batch_size, num_attention_heads,
+                                 from_seq_length//from_block_size-2,
+                                 from_block_size, num_rand_blocks*to_block_size].
         """
         num_windows = from_seq_length // from_block_size - 2
-        rand_mask = tf.stack([tf.reshape(tf.boolean_mask(i1, p1), [-1]) for p1, i1 in zip(to_blocked_mask, rand_attn)])
-        rand_mask = rand_mask.view(batch_size, num_attention_heads, num_windows, num_rand_blocks * from_block_size)
-        rand_mask = tf.einsum("blq,bhlk->bhlqk", from_blocked_mask[:, 1:-1], rand_mask)
+        rand_mask = tf.reshape(
+            tf.gather(to_blocked_mask, rand_attn, batch_dims=1), [
+                -1, num_attention_heads, num_windows,
+                num_rand_blocks * from_block_size
+            ])
+        rand_mask = tf.einsum("BLQ,BHLK->BHLQK", from_blocked_mask[:, 1:-1],
+                              rand_mask)
         return rand_mask
 
     @staticmethod
@@ -1116,7 +1122,7 @@ class TFBigBirdSelfOutput(tf.keras.layers.Layer):
     def call(self, hidden_states: tf.Tensor, input_ids: tf.Tensor, training: bool = False) -> tf.Tensor:
         hidden_states = self.dense(inputs=hidden_states)
         hidden_states = self.dropout(inputs=hidden_states, training=training)
-        hidden_states = self.LayerNorm(inputs=hidden_states + input_ids)
+        hidden_states = self.LayerNorm(inputs=(hidden_states + input_ids))
 
         return hidden_states
 
@@ -1178,14 +1184,6 @@ class TFBigBirdAttention(tf.keras.layers.Layer):
         training: bool = False,
     ):
 
-        # type compatibility
-        if band_mask is not None:
-            band_mask = tf.cast(band_mask, input_ids.dtype)
-        if from_mask is not None:
-            from_mask = tf.cast(from_mask, input_ids.dtype)
-        if to_mask is not None:
-            to_mask = tf.cast(to_mask, input_ids.dtype)
-
         if self.attention_type == "original_full":
             self_outputs = self.self(
                 input_ids,
@@ -1212,7 +1210,7 @@ class TFBigBirdAttention(tf.keras.layers.Layer):
                 training=training,
             )
         attention_output = self.dense_output(hidden_states=self_outputs[0], input_ids=input_ids, training=training)
-        outputs = (attention_output,) + self_outputs[1:]  # add attentions if we output them
+        outputs = (attention_output,) + self_outputs[1:]
         return outputs
 
 
@@ -1302,6 +1300,17 @@ class TFBigBirdLayer(tf.keras.layers.Layer):
         training: bool = False,
     ) -> Tuple[tf.Tensor]:
         # decoder uni-directional self-attention cached key/values tuple is at positions 1,2
+
+        # Ensure type safety
+        if band_mask is not None and band_mask.dtype is not tf.dtypes.float32:
+            band_mask = tf.cast(band_mask, tf.float32)
+        if from_mask is not None and from_mask.dtype is not tf.dtypes.float32:
+            from_mask = tf.cast(from_mask, tf.float32)
+        if to_mask is not None and to_mask.dtype is not tf.dtypes.float32:
+            to_mask = tf.cast(to_mask, tf.float32)
+        if blocked_encoder_mask is not None and blocked_encoder_mask.dtype is not tf.dtypes.float32:
+            blocked_encoder_mask = tf.cast(blocked_encoder_mask, tf.float32)
+
         self_attn_past_key_value = past_key_value[:2] if past_key_value is not None else None
         self_attention_outputs = self.attention(
             input_ids=hidden_states,
@@ -1658,8 +1667,8 @@ class TFBigBirdMainLayer(tf.keras.layers.Layer):
 
         return padding_len, input_ids, attention_mask, token_type_ids, position_ids, inputs_embeds
 
-    @staticmethod
-    def create_masks_for_block_sparse_attn(attention_mask: tf.Tensor, block_size: int):
+
+    def create_masks_for_block_sparse_attn(self, attention_mask: tf.Tensor, block_size: int):
 
         batch_size, seq_length = tf.shape(attention_mask)
         if seq_length % block_size != 0:
@@ -1668,29 +1677,27 @@ class TFBigBirdMainLayer(tf.keras.layers.Layer):
                 f" size is {block_size}."
             )
 
-        def create_band_mask_from_inputs(from_blocked_mask, to_blocked_mask):
-            """
-            Create 3D attention mask from a 2D tensor mask.
-
+        def _create_band_mask_from_inputs(from_blocked_mask, to_blocked_mask):
+            """Create 4D attention mask from a 3D blocked tensor mask.
             Args:
-                from_blocked_mask: 2D Tensor of shape [batch_size,
+              from_blocked_mask: 3D Tensor of shape [batch_size,
                 from_seq_length//from_block_size, from_block_size].
-                to_blocked_mask: int32 Tensor of shape [batch_size,
+              to_blocked_mask: 3D Tensor of shape [batch_size,
                 to_seq_length//to_block_size, to_block_size].
-
             Returns:
-                float Tensor of shape [batch_size, 1, from_seq_length//from_block_size-4, from_block_size,
-                3*to_block_size].
+              float Tensor of shape [batch_size, 1, from_seq_length//from_block_size-4,
+                                     from_block_size,  3*to_block_size].
             """
             exp_blocked_to_pad = tf.concat(
-                [to_blocked_mask[:, 1:-3], to_blocked_mask[:, 2:-2], to_blocked_mask[:, 3:-1]], axis=2
-            )
-            band_mask = tf.einsum("blq,blk->blqk", from_blocked_mask[:, 2:-2], exp_blocked_to_pad)
-            tf.expand_dims(band_mask, 1)
+                [to_blocked_mask[:, 1:-3], to_blocked_mask[:, 2:-2],
+                 to_blocked_mask[:, 3:-1]], 2)
+            band_mask = tf.einsum(
+                "BLQ,BLK->BLQK", from_blocked_mask[:, 2:-2], exp_blocked_to_pad)
+            band_mask = tf.expand_dims(band_mask, 1)
             return band_mask
 
         blocked_encoder_mask = tf.reshape(attention_mask, (batch_size, seq_length // block_size, block_size))
-        band_mask = create_band_mask_from_inputs(blocked_encoder_mask, blocked_encoder_mask)
+        band_mask = _create_band_mask_from_inputs(blocked_encoder_mask, blocked_encoder_mask)
 
         from_mask = tf.reshape(attention_mask, (batch_size, 1, seq_length, 1))
         to_mask = tf.reshape(attention_mask, (batch_size, 1, 1, seq_length))
