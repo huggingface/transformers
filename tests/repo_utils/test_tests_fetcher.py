62cee5597e8570782ef0ec37429b701aa0b7282d
Sylvain Gugger <Sylvain.gugger@gmail.com>
2022-10-17 15:41:16 -0400

    WiP
    WiP
    
    WiP
    
    WiP
    
    WiP
    
    WiP
    
    WiP
    
    WiP
    
    WiP

--- tests/repo_utils/test_tests_fetcher.py
+++ tests/repo_utils/test_tests_fetcher.py
@@ -15,12 +15,16 @@
 import os
 import sys
 import unittest
+from git import Repo
 
 
 git_repo_path = os.path.abspath(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
 sys.path.append(os.path.join(git_repo_path, "utils"))
 
-from tests_fetcher import clean_code  # noqa: E402
+transformers_path = os.path.join(git_repo_path, "src", "transformers")
+GIT_TEST_SHA = "07f6690206e39ed7a4d9dbc58824314f7089bb38"
+
+from tests_fetcher import checkout_commit, clean_code, get_module_dependencies  # noqa: E402
 
 
 class CheckDummiesTester(unittest.TestCase):
@@ -32,3 +36,26 @@ class CheckDummiesTester(unittest.TestCase):
         # Clean code removes all comments
         self.assertEqual(clean_code("code\n# Comment\ncode"), "code\ncode")
         self.assertEqual(clean_code("code  # inline comment\ncode"), "code  \ncode")
+
+    def test_checkout_commit(self):
+        repo = Repo(git_repo_path)
+        self.assertNotEqual(repo.head.commit.hexsha, GIT_TEST_SHA)
+        with checkout_commit(repo, GIT_TEST_SHA):
+            self.assertEqual(repo.head.commit.hexsha, GIT_TEST_SHA)
+        self.assertNotEqual(repo.head.commit.hexsha, GIT_TEST_SHA)
+
+    def test_get_module_dependencies(self):
+        bert_module = os.path.join(transformers_path, "models", "bert", "modeling_bert.py")
+        expected_deps = [
+            'activations.py',
+            'modeling_outputs.py',
+            'modeling_utils.py',
+            'pytorch_utils.py',
+            'models/bert/configuration_bert.py',
+        ]
+        expected_deps = set(os.path.join(transformers_path, f) for f in expected_deps)
+        repo = Repo(git_repo_path)
+        with checkout_commit(repo, GIT_TEST_SHA):
+            deps = get_module_dependencies(bert_module)
+        deps = set(os.path.expanduser(f) for f in deps)
+        self.assertEqual(deps, expected_deps)
