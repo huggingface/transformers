80450f1f277d90502e56c8826fa736843f4224db
Somasree <bishnupadamajumder32@gmail.com>
2022-10-18 20:15:11 +0530

    making the required changes

--- src/transformers/models/convnext/configuration_convnext.py
+++ src/transformers/models/convnext/configuration_convnext.py
@@ -71,8 +71,10 @@ class ConvNextConfig(PretrainedConfig):
 
     >>> # Initializing a ConvNext convnext-tiny-224 style configuration
     >>> configuration = ConvNextConfig()
+
     >>> # Initializing a model (with random weights) from the convnext-tiny-224 style configuration
     >>> model = ConvNextModel(configuration)
+
     >>> # Accessing the model configuration
     >>> configuration = model.config
     ```"""
