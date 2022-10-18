3e07196f896946f084ce65db852869981c533a98
Arthur <48595927+ArthurZucker@users.noreply.github.com>
2022-10-18 09:14:12 +0200

    check decoder_inputs_embeds is None before shifting labels (#19671)

--- src/transformers/models/whisper/modeling_tf_whisper.py
+++ src/transformers/models/whisper/modeling_tf_whisper.py
@@ -1293,7 +1293,7 @@ class TFWhisperForConditionalGeneration(TFWhisperPreTrainedModel, TFCausalLangua
         return_dict = return_dict if return_dict is not None else self.config.use_return_dict
 
         if labels is not None:
-            if decoder_input_ids is None:
+            if decoder_input_ids is None and decoder_inputs_embeds is None:
                 decoder_input_ids = shift_tokens_right(
                     labels, self.config.pad_token_id, self.config.decoder_start_token_id
                 )
