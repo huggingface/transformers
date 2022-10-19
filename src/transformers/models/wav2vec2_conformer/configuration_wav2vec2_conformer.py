34aaece34a167442ad8a840fbb15ac8d1f1b0c8a
JuanCarlosPi <juancopi_81@hotmail.com>
2022-10-18 23:47:41 -0500

    [Doctest] Add wav2vec2_conformer for doctest (#19734)

--- src/transformers/models/wav2vec2_conformer/configuration_wav2vec2_conformer.py
+++ src/transformers/models/wav2vec2_conformer/configuration_wav2vec2_conformer.py
@@ -193,12 +193,12 @@ class Wav2Vec2ConformerConfig(PretrainedConfig):
     Example:
 
     ```python
-    >>> from transformers import Wav2Vec2ConformerModel, Wav2Vec2ConformerConfig
+    >>> from transformers import Wav2Vec2ConformerConfig, Wav2Vec2ConformerModel
 
     >>> # Initializing a Wav2Vec2Conformer facebook/wav2vec2-conformer-rel-pos-large style configuration
     >>> configuration = Wav2Vec2ConformerConfig()
 
-    >>> # Initializing a model from the facebook/wav2vec2-conformer-rel-pos-large style configuration
+    >>> # Initializing a model (with random weights) from the facebook/wav2vec2-conformer-rel-pos-large style configuration
     >>> model = Wav2Vec2ConformerModel(configuration)
 
     >>> # Accessing the model configuration
