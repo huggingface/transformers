c9288af118ebee49f57da6b9b7ad77cfca069ba6
Cody Yu <comaniac0422@gmail.com>
2022-10-21 15:59:05 -0700

    Update pytorch_utils.py

--- src/transformers/pytorch_utils.py
+++ src/transformers/pytorch_utils.py
@@ -97,19 +97,26 @@ class Conv1D(nn.Module):
     Args:
         nf (`int`): The number of output features.
         nx (`int`): The number of input features.
+        bias (`bool`): If set to `False`, the layer will not learn an additive bias. Default: `True`
     """
 
-    def __init__(self, nf, nx):
+    def __init__(self, nf, nx, bias=True):
         super().__init__()
         self.nf = nf
         w = torch.empty(nx, nf)
         nn.init.normal_(w, std=0.02)
         self.weight = nn.Parameter(w)
-        self.bias = nn.Parameter(torch.zeros(nf))
+        if bias:
+            self.bias = nn.Parameter(torch.zeros(nf))
+        else:
+            self.register_parameter("bias", None)
 
     def forward(self, x):
         size_out = x.size()[:-1] + (self.nf,)
-        x = torch.addmm(self.bias, x.view(-1, x.size(-1)), self.weight)
+        if self.bias is not None:
+            x = torch.addmm(self.bias, x.view(-1, x.size(-1)), self.weight)
+        else:
+            x = torch.mm(x.view(-1, x.size(-1)), self.weight)
         x = x.view(size_out)
         return x
 
