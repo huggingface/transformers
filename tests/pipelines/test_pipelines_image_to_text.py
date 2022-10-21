259ef72b51fab76eccd2de3956c716623d1582d7
ydshieh <ydshieh@users.noreply.github.com>
2022-10-21 13:09:16 +0200

    fix

--- tests/pipelines/test_pipelines_image_to_text.py
+++ tests/pipelines/test_pipelines_image_to_text.py
@@ -56,7 +56,7 @@ class ImageToTextPipelineTests(unittest.TestCase, metaclass=PipelineTestCaseMeta
 
     @require_tf
     def test_small_model_tf(self):
-        pipe = pipeline("image-to-text", model="hf-internal-testing/tiny-random-vit-gpt2")
+        pipe = pipeline("image-to-text", model="hf-internal-testing/tiny-random-vit-gpt2", framework="tf")
         image = "./tests/fixtures/tests_samples/COCO/000000039769.png"
 
         outputs = pipe(image)
