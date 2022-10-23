3a1fcd8f2a1e046e8ede018b996f22084f395296
Aman K <7098967+amankhandelia@users.noreply.github.com>
2022-10-23 15:05:24 +0000

    add model to init

--- src/transformers/__init__.py
+++ src/transformers/__init__.py
@@ -3036,6 +3036,8 @@ else:
     )
     _import_structure["models.mbart"].extend(
         [
+            "FlaxMBartDecoderPreTrainedModel",
+            "FlaxMBartForCausalLM",
             "FlaxMBartForConditionalGeneration",
             "FlaxMBartForQuestionAnswering",
             "FlaxMBartForSequenceClassification",
@@ -5588,6 +5590,8 @@ if TYPE_CHECKING:
         from .models.longt5 import FlaxLongT5ForConditionalGeneration, FlaxLongT5Model, FlaxLongT5PreTrainedModel
         from .models.marian import FlaxMarianModel, FlaxMarianMTModel, FlaxMarianPreTrainedModel
         from .models.mbart import (
+            FlaxMBartDecoderPreTrainedModel,
+            FlaxMBartForCausalLM,
             FlaxMBartForConditionalGeneration,
             FlaxMBartForQuestionAnswering,
             FlaxMBartForSequenceClassification,
