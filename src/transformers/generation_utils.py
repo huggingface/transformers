15e003c62b0d8b99a2dd6b78b5bc85e02620f150
Nicolas Patry <patry.nicolas@protonmail.com>
2022-10-24 14:40:59 +0200

    1 - -> logical_not.

--- src/transformers/generation_utils.py
+++ src/transformers/generation_utils.py
@@ -1833,7 +1833,7 @@ class GenerationMixin:
             if eos_token_id is not None:
                 if pad_token_id is None:
                     raise ValueError("If `eos_token_id` is defined, make sure that `pad_token_id` is defined.")
-                next_tokens = next_tokens * unfinished_sequences + pad_token_id * (1 - unfinished_sequences)
+                next_tokens = next_tokens * unfinished_sequences + pad_token_id * (unfinished_sequences.logical_not())
 
             # update generated ids, model inputs, and length for next step
             input_ids = torch.cat([input_ids, next_tokens[:, None]], dim=-1)
