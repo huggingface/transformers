7a332359d1ec51eb4a256051c70c8b4008e65c87
regisss <regis@huggingface.co>
2022-10-24 10:07:02 +0200

    Display the number of trainable parameters when lauching a training

--- src/transformers/trainer.py
+++ src/transformers/trainer.py
@@ -1611,6 +1611,9 @@ class Trainer:
         logger.info(f"  Total train batch size (w. parallel, distributed & accumulation) = {total_train_batch_size}")
         logger.info(f"  Gradient Accumulation steps = {args.gradient_accumulation_steps}")
         logger.info(f"  Total optimization steps = {max_steps}")
+        logger.info(
+            f"  Number of trainable parameters = {sum(p.numel() for p in model.parameters() if p.requires_grad)}"
+        )
 
         self.state.epoch = 0
         start_time = time.time()
