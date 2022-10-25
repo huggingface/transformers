8ed019ddcd86a0be302dcaf38ab7ba1f8cded9e9
Somasree <bishnupadamajumder32@gmail.com>
2022-10-25 10:11:40 +0530

    Removing bertconfig fromRoberta

--- src/transformers/models/roberta/configuration_roberta.py
+++ src/transformers/models/roberta/configuration_roberta.py
@@ -19,7 +19,8 @@ from typing import Mapping
 
 from ...onnx import OnnxConfig
 from ...utils import logging
-from ..bert.configuration_bert import BertConfig
+
+from ...configuration_utils import PretrainedConfig
 
 
 logger = logging.get_logger(__name__)
@@ -34,7 +35,7 @@ ROBERTA_PRETRAINED_CONFIG_ARCHIVE_MAP = {
 }
 
 
-class RobertaConfig(BertConfig):
+class RobertaConfig(PretrainedConfig):
     r"""
     This is the configuration class to store the configuration of a [`RobertaModel`] or a [`TFRobertaModel`]. It is
     used to instantiate a RoBERTa model according to the specified arguments, defining the model architecture.
@@ -63,9 +64,46 @@ class RobertaConfig(BertConfig):
     ```"""
     model_type = "roberta"
 
-    def __init__(self, pad_token_id=1, bos_token_id=0, eos_token_id=2, **kwargs):
+    def __init__(
+                self,
+                vocab_size=30522,
+                hidden_size=768,
+                num_hidden_layers=12,
+                num_attention_heads=12,
+                intermediate_size=3072,
+                hidden_act="gelu",
+                hidden_dropout_prob=0.1,
+                attention_probs_dropout_prob=0.1,
+                max_position_embeddings=512,
+                type_vocab_size=2,
+                initializer_range=0.02,
+                layer_norm_eps=1e-12,
+                pad_token_id=1,
+                position_embedding_type="absolute",
+                use_cache=True,
+                classifier_dropout=None,
+                bos_token_id=0,
+                eos_token_id=2,
+                **kwargs):
         """Constructs RobertaConfig."""
-        super().__init__(pad_token_id=pad_token_id, bos_token_id=bos_token_id, eos_token_id=eos_token_id, **kwargs)
+        super().__init__(pad_token_id=pad_token_id, **kwargs)
+        self.vocab_size = vocab_size
+        self.hidden_size = hidden_size
+        self.num_hidden_layers = num_hidden_layers
+        self.num_attention_heads = num_attention_heads
+        self.hidden_act = hidden_act
+        self.intermediate_size = intermediate_size
+        self.hidden_dropout_prob = hidden_dropout_prob
+        self.attention_probs_dropout_prob = attention_probs_dropout_prob
+        self.max_position_embeddings = max_position_embeddings
+        self.type_vocab_size = type_vocab_size
+        self.initializer_range = initializer_range
+        self.layer_norm_eps = layer_norm_eps
+        self.position_embedding_type = position_embedding_type
+        self.use_cache = use_cache
+        self.classifier_dropout = classifier_dropout
+        self.bos_token_id=bos_token_id
+        self.eos_token_id=eos_token_id
 
 
 class RobertaOnnxConfig(OnnxConfig):
