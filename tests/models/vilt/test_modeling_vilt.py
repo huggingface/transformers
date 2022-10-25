0fccde5e14944c24da9e767131370211555de9f5
Lysandre Debut <lysandre.debut@reseau.eseo.fr>
2022-10-25 15:59:35 +0200

    [Past CI] Vilt only supports PT >= v1.10 (#19851)
    * Support for Vilt in v1.9
    
    * Skip if not higher or equal than 1.10
    
    * Move test :)
    
    * I am bad at python

--- tests/models/vilt/test_modeling_vilt.py
+++ tests/models/vilt/test_modeling_vilt.py
@@ -41,6 +41,9 @@ if is_torch_available():
         ViltModel,
     )
     from transformers.models.vilt.modeling_vilt import VILT_PRETRAINED_MODEL_ARCHIVE_LIST
+    from transformers.pytorch_utils import is_torch_greater_or_equal_than_1_10
+else:
+    is_torch_greater_or_equal_than_1_10 = False
 
 if is_vision_available():
     import PIL
@@ -214,6 +217,7 @@ class ViltModelTester:
 
 
 @require_torch
+@unittest.skipIf(not is_torch_greater_or_equal_than_1_10, "Vilt is only available in torch v1.10+")
 class ViltModelTest(ModelTesterMixin, unittest.TestCase):
     all_model_classes = (
         (
@@ -510,6 +514,7 @@ class ViltModelTest(ModelTesterMixin, unittest.TestCase):
 
 
 @require_torch
+@unittest.skipIf(not is_torch_greater_or_equal_than_1_10, "Vilt is only available in torch v1.10+")
 class ViltForImagesAndTextClassificationModelTest(ViltModelTest, unittest.TestCase):
     all_model_classes = (ViltForImagesAndTextClassification,) if is_torch_available() else ()
 
@@ -534,6 +539,7 @@ def prepare_img():
 
 @require_torch
 @require_vision
+@unittest.skipIf(not is_torch_greater_or_equal_than_1_10, "Vilt is only available in torch v1.10+")
 class ViltModelIntegrationTest(unittest.TestCase):
     @cached_property
     def default_processor(self):
