f8ac94398004b4633ad78378dc473f17b65c3de4
ydshieh <ydshieh@users.noreply.github.com>
2022-10-27 10:49:43 +0200

    Add pegasus_x

--- src/transformers/models/auto/tokenization_auto.py
+++ src/transformers/models/auto/tokenization_auto.py
@@ -204,7 +204,8 @@ else:
                     "PegasusTokenizer" if is_sentencepiece_available() else None,
                     "PegasusTokenizerFast" if is_tokenizers_available() else None,
                 ),
-            ),
+            ), ("pegasus_x", ("PegasusTokenizer" if is_sentencepiece_available() else None,
+                            "PegasusTokenizerFast" if is_tokenizers_available() else None,),),
             (
                 "perceiver",
                 (
