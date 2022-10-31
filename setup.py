b41b34cdd4757e58586ad0e89771852a9c3b4ab8
Yih-Dar <2521628+ydshieh@users.noreply.github.com>
2022-10-31 18:22:52 +0100

    Pin torch to < 1.13 temporarily (#19989)
    * pin torch to < 1.13
    
    * pin torch to < 1.13
    
    Co-authored-by: ydshieh <ydshieh@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -163,7 +163,7 @@ _deps = [
     "timeout-decorator",
     "timm",
     "tokenizers>=0.11.1,!=0.11.3,<0.14",
-    "torch>=1.7,!=1.12.0",
+    "torch>=1.7,!=1.12.0,<1.13.0",
     "torchaudio",
     "pyctcdecode>=0.4.0",
     "tqdm>=4.27",
