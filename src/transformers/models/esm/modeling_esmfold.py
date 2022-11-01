f6f4a2c42f3218e516c40c61e088dcc104fd42b7
Sylvain Gugger <Sylvain.gugger@gmail.com>
2022-10-31 21:00:29 -0400

    Flag example to write

--- src/transformers/models/esm/modeling_esmfold.py
+++ src/transformers/models/esm/modeling_esmfold.py
@@ -2092,19 +2092,7 @@ class EsmForProteinFolding(EsmPreTrainedModel):
 
         Example:
 
-        ```python
-        >>> from transformers import BertTokenizer, BertForPreTraining
-        >>> import torch
-
-        >>> tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
-        >>> model = BertForPreTraining.from_pretrained("bert-base-uncased")
-
-        >>> inputs = tokenizer("Hello, my dog is cute", return_tensors="pt")
-        >>> outputs = model(**inputs)
-
-        >>> prediction_logits = outputs.prediction_logits
-        >>> seq_relationship_logits = outputs.seq_relationship_logits
-        ```
+        TODO Matt
         """
         cfg = self.config.esmfold_config
 
