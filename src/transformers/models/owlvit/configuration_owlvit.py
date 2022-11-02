e223d60083a8ac598b93a7830e7ac82aa3469124
Yih-Dar <2521628+ydshieh@users.noreply.github.com>
2022-11-02 12:03:43 +0100

    clean up vision/text config dict arguments (#19954)
    * clean up
    
    * For backward compatibility
    
    * clean up
    
    * Same changes for more models
    
    Co-authored-by: ydshieh <ydshieh@users.noreply.github.com>

--- src/transformers/models/owlvit/configuration_owlvit.py
+++ src/transformers/models/owlvit/configuration_owlvit.py
@@ -260,9 +260,9 @@ class OwlViTConfig(PretrainedConfig):
     documentation from [`PretrainedConfig`] for more information.
 
     Args:
-        text_config_dict (`dict`, *optional*):
+        text_config (`dict`, *optional*):
             Dictionary of configuration options used to initialize [`OwlViTTextConfig`].
-        vision_config_dict (`dict`, *optional*):
+        vision_config (`dict`, *optional*):
             Dictionary of configuration options used to initialize [`OwlViTVisionConfig`].
         projection_dim (`int`, *optional*, defaults to 512):
             Dimensionality of text and vision projection layers.
@@ -285,15 +285,15 @@ class OwlViTConfig(PretrainedConfig):
         return_dict=True,
         **kwargs
     ):
-        super().__init__(text_config=text_config, vision_config=vision_config, **kwargs)
+        super().__init__(**kwargs)
 
         if text_config is None:
             text_config = {}
-            logger.info("text_config_dict is None. Initializing the OwlViTTextConfig with default values.")
+            logger.info("text_config is None. Initializing the OwlViTTextConfig with default values.")
 
         if vision_config is None:
             vision_config = {}
-            logger.info("vision_config_dict is None. initializing the OwlViTVisionConfig with default values.")
+            logger.info("vision_config is None. initializing the OwlViTVisionConfig with default values.")
 
         self.text_config = OwlViTTextConfig(**text_config)
         self.vision_config = OwlViTVisionConfig(**vision_config)
