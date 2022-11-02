9d61dbe4c6fef819598369bd0f03c001368fae8b
NielsRogge <niels.rogge1@gmail.com>
2022-11-02 13:58:54 +0000

    Improve postprocessing methods

--- src/transformers/models/convnext_maskrcnn/modeling_convnext_maskrcnn.py
+++ src/transformers/models/convnext_maskrcnn/modeling_convnext_maskrcnn.py
@@ -2421,60 +2421,6 @@ class ConvNextMaskRNNShared2FCBBoxHead(nn.Module):
 
         return cls_score, bbox_pred
 
-    def get_bboxes(self, rois, cls_score, bbox_pred, img_shape, scale_factor, rescale=False, cfg=None):
-        """Transform network output for a batch into bbox predictions.
-
-        Args:
-            rois (Tensor):
-                Boxes to be transformed. Has shape (num_boxes, 5). Last dimension 5 arrange as (batch_index, x1, y1,
-                x2, y2).
-            cls_score (Tensor):
-                Box scores, has shape (num_boxes, num_classes + 1).
-            bbox_pred (Tensor, optional):
-                Box energies / deltas. Has shape (num_boxes, num_classes * 4).
-            img_shape (Sequence[int], optional):
-                Maximum bounds for boxes, specifies (H, W, C) or (H, W).
-            scale_factor (ndarray):
-                Scale factor of the image arrange as (w_scale, h_scale, w_scale, h_scale).
-            rescale (bool):
-                If True, return boxes in original image space. Default: False.
-            cfg (obj:`ConfigDict`):
-                `test_cfg` of Bbox Head. Default: None
-
-        Returns:
-            tuple[Tensor, Tensor]:
-                First tensor is `det_bboxes`, has the shape (num_boxes, 5) and last dimension 5 represent (tl_x, tl_y,
-                br_x, br_y, score). Second tensor is the labels with shape (num_boxes, ).
-        """
-
-        # # some loss (Seesaw loss..) may have custom activation
-        # if self.custom_cls_channels:
-        #     scores = self.loss_cls.get_activation(cls_score)
-        # else:
-        scores = nn.functional.softmax(cls_score, dim=-1) if cls_score is not None else None
-        # bbox_pred would be None in some detector when with_reg is False,
-        # e.g. Grid R-CNN.
-        if bbox_pred is not None:
-            bboxes = self.bbox_coder.decode(rois[..., 1:], bbox_pred, max_shape=img_shape)
-        else:
-            bboxes = rois[:, 1:].clone()
-            if img_shape is not None:
-                img_shape = img_shape[-2:]
-                bboxes[:, [0, 2]].clamp_(min=0, max=img_shape[1])
-                bboxes[:, [1, 3]].clamp_(min=0, max=img_shape[0])
-
-        if rescale and bboxes.size(0) > 0:
-            scale_factor = bboxes.new_tensor(scale_factor)
-            bboxes = (bboxes.view(bboxes.size(0), -1, 4) / scale_factor).view(bboxes.size()[0], -1)
-
-        if cfg is None:
-            return bboxes, scores
-        else:
-            # it's here that we create a different amount of objects per image in a batch
-            det_bboxes, det_labels = multiclass_nms(bboxes, scores, cfg["score_thr"], cfg["nms"], cfg["max_per_img"])
-
-            return det_bboxes, det_labels
-
     def _get_target_single(self, pos_bboxes, neg_bboxes, pos_gt_bboxes, pos_gt_labels, cfg):
         """Calculate the ground truth for proposals in the single image
         Args:
@@ -2916,7 +2862,7 @@ class ConvNextMaskRCNNRoIHead(nn.Module):
             x (list[Tensor]):
                 list of multi-level img features.
             img_metas (list[dict]):
-                list of image info dict where each dict. Jas: 'img_shape', 'scale_factor', 'flip', and may also contain
+                list of image info dict where each dict. Has: 'img_shape', 'scale_factor', 'flip', and may also contain
                 'filename', 'ori_shape', 'pad_shape', and 'img_norm_cfg'. For details on the values of these keys see
                 `mmdet/datasets/pipelines/formatting.py:Collect`.
             proposals (list[Tensors]):
