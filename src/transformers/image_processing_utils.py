a6b77598805f4e3c24a47767d503dc6ea20d1381
amyeroberts <22614925+amyeroberts@users.noreply.github.com>
2022-11-02 11:57:36 +0000

    Add Image Processors (#19796)
    * Add CLIP image processor
    
    * Crop size as dict too
    
    * Update warning
    
    * Actually use logger this time
    
    * Normalize doesn't change dtype of input
    
    * Add perceiver image processor
    
    * Tidy up
    
    * Add DPT image processor
    
    * Add Vilt image processor
    
    * Tidy up
    
    * Add poolformer image processor
    
    * Tidy up
    
    * Add LayoutLM v2 and v3 imsge processors
    
    * Tidy up
    
    * Add Flava image processor
    
    * Tidy up
    
    * Add deit image processor
    
    * Tidy up
    
    * Add ConvNext image processor
    
    * Tidy up
    
    * Add levit image processor
    
    * Add segformer image processor
    
    * Add in post processing
    
    * Fix up
    
    * Add ImageGPT image processor
    
    * Fixup
    
    * Add mobilevit image processor
    
    * Tidy up
    
    * Add postprocessing
    
    * Fixup
    
    * Add VideoMAE image processor
    
    * Tidy up
    
    * Add ImageGPT image processor
    
    * Fixup
    
    * Add ViT image processor
    
    * Tidy up
    
    * Add beit image processor
    
    * Add mobilevit image processor
    
    * Tidy up
    
    * Add postprocessing
    
    * Fixup
    
    * Fix up
    
    * Fix flava and remove tree module
    
    * Fix image classification pipeline failing tests
    
    * Update feature extractor in trainer scripts
    
    * Update pad_if_smaller to accept tuple and int size
    
    * Update for image segmentation pipeline
    
    * Update src/transformers/models/perceiver/image_processing_perceiver.py
    
    Co-authored-by: Alara Dirik <8944735+alaradirik@users.noreply.github.com>
    
    * Update src/transformers/image_processing_utils.py
    
    Co-authored-by: NielsRogge <48327001+NielsRogge@users.noreply.github.com>
    
    * Update src/transformers/models/beit/image_processing_beit.py
    
    Co-authored-by: NielsRogge <48327001+NielsRogge@users.noreply.github.com>
    
    * PR comments - docstrings; remove accidentally added resize; var names
    
    * Update docstrings
    
    * Add exception if size is not in the right format
    
    * Fix exception check
    
    * Fix up
    
    * Use shortest_edge in tuple in script
    
    Co-authored-by: Alara Dirik <8944735+alaradirik@users.noreply.github.com>
    Co-authored-by: NielsRogge <48327001+NielsRogge@users.noreply.github.com>

--- src/transformers/image_processing_utils.py
+++ src/transformers/image_processing_utils.py
@@ -13,6 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from typing import Dict, Iterable, Optional, Union
+
 from .feature_extraction_utils import BatchFeature as BaseBatchFeature
 from .feature_extraction_utils import FeatureExtractionMixin
 from .utils import logging
@@ -48,7 +50,72 @@ class BaseImageProcessor(ImageProcessorMixin):
         super().__init__(**kwargs)
 
     def __call__(self, images, **kwargs) -> BatchFeature:
+        """Preprocess an image or a batch of images."""
         return self.preprocess(images, **kwargs)
 
     def preprocess(self, images, **kwargs) -> BatchFeature:
         raise NotImplementedError("Each image processor must implement its own preprocess method")
+
+
+def get_size_dict(
+    size: Union[int, Iterable[int], Dict[str, int]] = None,
+    max_size: Optional[int] = None,
+    height_width_order: bool = True,
+    default_to_square: bool = True,
+) -> dict:
+    """
+    Converts the old size parameter in the config into the new dict expected in the config. This is to ensure backwards
+    compatibility with the old feature extractor configs and removes ambiguity over whether the tuple is in (height,
+    width) or (width, height) format.
+
+    - If `size` is tuple, it is converted to `{"height": size[0], "width": size[1]}` or `{"height": size[1], "width":
+    size[0]}` if `height_width_order` is `False`.
+    - If `size` is an int, and `default_to_square` is `True`, it is converted to `{"height": size, "width": size}`.
+    - If `size` is an int and `default_to_square` is False, it is converted to `{"shortest_edge": size}`. If `max_size`
+      is set, it is added to the dict as `{"longest_edge": max_size}`.
+
+    Args:
+        size (`Union[int, Iterable[int], Dict[str, int]]`, *optional*):
+            The `size` parameter to be cast into a size dictionary.
+        max_size (`Optional[int]`, *optional*):
+            The `max_size` parameter to be cast into a size dictionary.
+        height_width_order (`bool`, *optional*, defaults to `True`):
+            If `size` is a tuple, whether it's in (height, width) or (width, height) order.
+        default_to_square (`bool`, *optional*, defaults to `True`):
+            If `size` is an int, whether to default to a square image or not.
+    """
+    # If a dict is passed, we check if it's a valid size dict and then return it.
+    if isinstance(size, dict):
+        size_keys = set(size.keys())
+        if (
+            size_keys != set(["height", "width"])
+            and size_keys != set(["shortest_edge"])
+            and size_keys != set(["shortest_edge", "longest_edge"])
+        ):
+            raise ValueError(
+                "The size dict must contain either the keys ('height', 'width') or ('shortest_edge')"
+                f"or ('shortest_edge', 'longest_edge') but got {size_keys}"
+            )
+        return size
+
+    # By default, if size is an int we assume it represents a tuple of (size, size).
+    elif isinstance(size, int) and default_to_square:
+        if max_size is not None:
+            raise ValueError("Cannot specify both size as an int, with default_to_square=True and max_size")
+        size_dict = {"height": size, "width": size}
+    # In other configs, if size is an int and default_to_square is False, size represents the length of the shortest edge after resizing.
+    elif isinstance(size, int) and not default_to_square:
+        if max_size is not None:
+            size_dict = {"shortest_edge": size, "longest_edge": max_size}
+        else:
+            size_dict = {"shortest_edge": size}
+    elif isinstance(size, (tuple, list)) and height_width_order:
+        size_dict = {"height": size[0], "width": size[1]}
+    elif isinstance(size, (tuple, list)) and not height_width_order:
+        size_dict = {"height": size[1], "width": size[0]}
+
+    logger.warning(
+        "The size parameter should be a dictionary with keys ('height', 'width'), ('shortest_edge', 'longest_edge')"
+        f" or ('shortest_edge',) got {size}. Setting as {size_dict}.",
+    )
+    return size_dict
