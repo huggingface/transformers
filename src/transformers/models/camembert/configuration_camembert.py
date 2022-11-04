4c94af5af6831d9ef9488f6211f17c502cd4d558
Saad Mahmud <shuvro.mahmud79@gmail.com>
2022-11-04 12:32:12 +0600

    swap RobertaConfig with PretrainedConfig

--- src/transformers/models/camembert/configuration_camembert.py
+++ src/transformers/models/camembert/configuration_camembert.py
@@ -20,7 +20,7 @@ from typing import Mapping
 
 from ...onnx import OnnxConfig
 from ...utils import logging
-from ..roberta.configuration_roberta import RobertaConfig
+from ...configuration_utils import PretrainedConfig
 
 
 logger = logging.get_logger(__name__)
@@ -36,7 +36,7 @@ CAMEMBERT_PRETRAINED_CONFIG_ARCHIVE_MAP = {
 }
 
 
-class CamembertConfig(RobertaConfig):
+class CamembertConfig(PretrainedConfig):
     """
     This class overrides [`RobertaConfig`]. Please check the superclass for the appropriate documentation alongside
     usage examples. Instantiating a configuration with the defaults will yield a similar configuration to that of the
@@ -58,6 +58,44 @@ class CamembertConfig(RobertaConfig):
     ```"""
 
     model_type = "camembert"
+
+    def __init__(
+        self,
+        vocab_size=30522,
+        hidden_size=768,
+        num_hidden_layers=12,
+        num_attention_heads=12,
+        intermediate_size=3072,
+        hidden_act="gelu",
+        hidden_dropout_prob=0.1,
+        attention_probs_dropout_prob=0.1,
+        max_position_embeddings=512,
+        type_vocab_size=2,
+        initializer_range=0.02,
+        layer_norm_eps=1e-12,
+        pad_token_id=0,
+        position_embedding_type="absolute",
+        use_cache=True,
+        classifier_dropout=None,
+        **kwargs
+    ):
+        super().__init__(pad_token_id=pad_token_id, **kwargs)
+
+        self.vocab_size = vocab_size
+        self.hidden_size = hidden_size
+        self.num_hidden_layers = num_hidden_layers
+        self.num_attention_heads = num_attention_heads
+        self.hidden_act = hidden_act
+        self.intermediate_size = intermediate_size
+        self.hidden_dropout_prob = hidden_dropout_prob
+        self.attention_probs_dropout_prob = attention_probs_dropout_prob
+        self.max_position_embeddings = max_position_embeddings
+        self.type_vocab_size = type_vocab_size
+        self.initializer_range = initializer_range
+        self.layer_norm_eps = layer_norm_eps
+        self.position_embedding_type = position_embedding_type
+        self.use_cache = use_cache
+        self.classifier_dropout = classifier_dropout
 
 
 class CamembertOnnxConfig(OnnxConfig):
