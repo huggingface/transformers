e2f6a842668a657f9eea6eead6b6c96656f33c50
Nicolas Patry <patry.nicolas@protonmail.com>
2022-11-08 15:48:30 +0100

    Remove extra pdb.

--- tests/test_modeling_common.py
+++ tests/test_modeling_common.py
@@ -2436,14 +2436,7 @@ class ModelTesterMixin:
 
                 for max_size in max_gpu_sizes:
                     max_memory = {0: max_size, "cpu": model_size * 2}
-                    try:
-                        new_model = model_class.from_pretrained(tmp_dir, device_map="auto", max_memory=max_memory)
-                    except Exception as e:
-                        print(e)
-                        import ipdb
-
-                        ipdb.set_trace()
-                        pass
+                    new_model = model_class.from_pretrained(tmp_dir, device_map="auto", max_memory=max_memory)
                     # Making sure part of the model will actually end up offloaded
                     self.assertSetEqual(set(new_model.hf_device_map.values()), {0, "cpu"})
 
