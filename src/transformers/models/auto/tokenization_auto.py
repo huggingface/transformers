f8943df46109995966a752c52e8c4575372c4cb9
Yih-Dar <2521628+ydshieh@users.noreply.github.com>
2022-11-09 20:58:56 +0100

    Add `RoCBertTokenizer` to `TOKENIZER_MAPPING_NAMES` (#20141)
    * Add RoCBertTokenizer to TOKENIZER_MAPPING_NAMES
    
    * fix
    
    Co-authored-by: ydshieh <ydshieh@users.noreply.github.com>

--- src/transformers/models/auto/tokenization_auto.py
+++ src/transformers/models/auto/tokenization_auto.py
@@ -248,6 +248,7 @@ else:
             ),
             ("retribert", ("RetriBertTokenizer", "RetriBertTokenizerFast" if is_tokenizers_available() else None)),
             ("roberta", ("RobertaTokenizer", "RobertaTokenizerFast" if is_tokenizers_available() else None)),
+            ("roc_bert", ("RoCBertTokenizer", None)),
             ("roformer", ("RoFormerTokenizer", "RoFormerTokenizerFast" if is_tokenizers_available() else None)),
             ("speech_to_text", ("Speech2TextTokenizer" if is_sentencepiece_available() else None, None)),
             ("speech_to_text_2", ("Speech2Text2Tokenizer", None)),
