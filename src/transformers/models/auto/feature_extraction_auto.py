050e8ebebc9b699d1c2b3ba31ac355343f6ad238
Nicolas Patry <patry.nicolas@protonmail.com>
2022-11-09 15:41:56 +0100

    Adding support for LayoutLMvX variants for `object-detection`.

--- src/transformers/models/auto/feature_extraction_auto.py
+++ src/transformers/models/auto/feature_extraction_auto.py
@@ -55,6 +55,7 @@ FEATURE_EXTRACTOR_MAPPING_NAMES = OrderedDict(
         ("groupvit", "CLIPFeatureExtractor"),
         ("hubert", "Wav2Vec2FeatureExtractor"),
         ("imagegpt", "ImageGPTFeatureExtractor"),
+        ("layoutlm", "LayoutLMv2FeatureExtractor"),
         ("layoutlmv2", "LayoutLMv2FeatureExtractor"),
         ("layoutlmv3", "LayoutLMv3FeatureExtractor"),
         ("levit", "LevitFeatureExtractor"),
