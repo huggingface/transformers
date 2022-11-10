9102887df4a68edd4a9bb8cf6966bfb0abdbbcce
Arthur Zucker <arthur.zucker@gmail.com>
2022-11-10 13:53:52 +0000

    update tokenization test

--- tests/models/whisper/test_modeling_tf_whisper.py
+++ tests/models/whisper/test_modeling_tf_whisper.py
@@ -652,7 +652,7 @@ def _test_large_logits_librispeech(in_queue, out_queue, timeout):
         processor = WhisperProcessor.from_pretrained("openai/whisper-large")
         processed_inputs = processor(audio=input_speech, return_tensors="tf")
         input_features = processed_inputs.input_features.to(torch_device)
-        labels = torch.tensor([[5723,   644,   295,   220,  3322,  6218]])
+        labels = tf.convert_to_tensor([[5723,   644,   295,   220,  3322,  6218]])
 
         logits = model(
             input_features,
