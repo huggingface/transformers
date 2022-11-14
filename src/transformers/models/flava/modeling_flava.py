9d81a8f0d96ea784c26bb3279d51ec5aaa347119
IMvision12 <88665786+IMvision12@users.noreply.github.com>
2022-11-14 18:09:18 +0530

    Pytorch type hints (#20112)
    * initial commit
    
    * Update modeling_whisper.py
    
    * Fixing Tests
    
    * modeling_vision_text_dual_encoder
    
    * modeling_vision_encoder_decoder
    
    * Update modeling_vit.py
    
    * Update modeling_vit_msn.py
    
    * Update modeling_trajectory_transformer.py
    
    * style
    
    * Update modeling_time_series_transformer.py
    
    * Update modeling_time_series_transformer.py
    
    * Update modeling_segformer.py
    
    * Update modeling_plbart.py
    
    * Update modeling_dpt.py
    
    * Update modeling_deit.py
    
    * Update modeling_dpt.py
    
    * Update modeling_esm.py
    
    * Update modeling_fnet.py
    
    * Update modeling_fnet.py
    
    * Update modeling_fnet.py
    
    * Update modeling_flava.py
    
    * Update modeling_flava.py
    
    * Update modeling_layoutlmv3.py
    
    * Update modeling_levit.py

--- src/transformers/models/flava/modeling_flava.py
+++ src/transformers/models/flava/modeling_flava.py
@@ -943,7 +943,7 @@ class FlavaImageModel(FlavaPreTrainedModel):
         output_attentions: Optional[bool] = None,
         output_hidden_states: Optional[bool] = None,
         return_dict: Optional[bool] = None,
-    ):
+    ) -> Union[tuple, BaseModelOutputWithPooling]:
         output_attentions = output_attentions if output_attentions is not None else self.config.output_attentions
         output_hidden_states = (
             output_hidden_states if output_hidden_states is not None else self.config.output_hidden_states
@@ -1039,7 +1039,7 @@ class FlavaTextModel(FlavaPreTrainedModel):
         output_attentions: Optional[bool] = None,
         output_hidden_states: Optional[bool] = None,
         return_dict: Optional[bool] = None,
-    ):
+    ) -> Union[tuple, BaseModelOutputWithPooling]:
         output_attentions = output_attentions if output_attentions is not None else self.config.output_attentions
         output_hidden_states = (
             output_hidden_states if output_hidden_states is not None else self.config.output_hidden_states
@@ -1142,7 +1142,7 @@ class FlavaMultimodalModel(FlavaPreTrainedModel):
         output_attentions: Optional[bool] = None,
         output_hidden_states: Optional[bool] = None,
         return_dict: Optional[bool] = None,
-    ):
+    ) -> Union[tuple, BaseModelOutputWithPooling]:
         output_attentions = output_attentions if output_attentions is not None else self.config.output_attentions
         output_hidden_states = (
             output_hidden_states if output_hidden_states is not None else self.config.output_hidden_states
