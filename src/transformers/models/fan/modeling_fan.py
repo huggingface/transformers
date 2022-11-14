7421b66eb24b275fd6ecaeea5b2494402e40732f
kiansierra <kiansierra90@gmail.com>
2022-11-14 18:26:53 +0100

    fixed output_attentions from config

--- src/transformers/models/fan/modeling_fan.py
+++ src/transformers/models/fan/modeling_fan.py
@@ -1947,6 +1947,8 @@ class FANForSemanticSegmentation(FANPreTrainedModel):
         [1, 150, 128, 128]
         ```"""
         return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+        output_attentions = output_attentions if output_attentions is not None else self.config.output_attentions
+
         output_hidden_states = (
             output_hidden_states if output_hidden_states is not None else self.config.output_hidden_states
         )
