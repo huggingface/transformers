c7b1afcef668773a22df958f95573556f395918e
Alara Dirik <alaradirik@gmail.com>
2022-11-14 18:20:34 +0300

    fix typos

--- src/transformers/models/owlvit/feature_extraction_owlvit.py
+++ src/transformers/models/owlvit/feature_extraction_owlvit.py
@@ -188,7 +188,7 @@ class OwlViTFeatureExtractor(FeatureExtractionMixin, ImageFeatureExtractionMixin
     def post_process_image_guided_detection(self, outputs, threshold=0.6, nms_threshold=0.3, target_sizes=None):
         """
         Converts the output of [`OwlViTForObjectDetection.image_guided_detection`] into the format expected by the
-        COCO: api.
+        COCO api.
 
         Args:
             outputs ([`OwlViTImageGuidedObjectDetectionOutput`]):
