2a66892483e29ec21ad217f0d059c6d338811f9f
Amy Roberts <22614925+amyeroberts@users.noreply.github.com>
2022-11-14 13:29:58 +0000

    Remove unused var desc in docstring

--- src/transformers/image_transforms.py
+++ src/transformers/image_transforms.py
@@ -171,8 +171,6 @@ def get_resize_output_image_size(
             If `size` is an int and `default_to_square` is `True`, then image will be resized to (size, size). If
             `size` is an int and `default_to_square` is `False`, then smaller edge of the image will be matched to this
             number. i.e, if height > width, then image will be rescaled to (size * height / width, size).
-        resample (`int`, *optional*, defaults to `PIL.Image.BILINEAR`):
-            The filter to user for resampling.
         default_to_square (`bool`, *optional*, defaults to `True`):
             How to convert `size` when it is a single int. If set to `True`, the `size` will be converted to a square
             (`size`,`size`). If set to `False`, will replicate
