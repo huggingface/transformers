25bbd3ed201f990557e0cea5503a7205ee9efa41
Nicolas Patry <patry.nicolas@protonmail.com>
2022-11-15 17:43:51 +0100

    Fixup.

--- src/transformers/pipelines/audio_classification.py
+++ src/transformers/pipelines/audio_classification.py
@@ -75,14 +75,15 @@ class AudioClassificationPipeline(Pipeline):
 
     ```python
     >>> from transformers import pipeline
+
     >>> classifier = pipeline(model="superb/wav2vec2-base-superb-ks")
     >>> result = classifier("https://huggingface.co/datasets/Narsil/asr_dummy/resolve/main/1.flac")
 
     >>> # Simplify results, different torch versions might alter the scores slightly.
     >>> from transformers.testing_utils import nested_simplify
+
     >>> nested_simplify(result)
     [{'score': 0.997, 'label': '_unknown_'}, {'score': 0.002, 'label': 'left'}, {'score': 0.0, 'label': 'yes'}, {'score': 0.0, 'label': 'down'}, {'score': 0.0, 'label': 'stop'}]
-
     ```
 
     [Using pipelines in a webserver or with a dataset](../pipeline_tutorial)
