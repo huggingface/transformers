b5e4e70532f0a4bc356efba8f93cc989efe2c56b
amyeroberts <22614925+amyeroberts@users.noreply.github.com>
2022-11-15 10:52:58 +0000

    Add param_name to size_dict logs & tidy (#20205)

--- src/transformers/models/mobilevit/image_processing_mobilevit.py
+++ src/transformers/models/mobilevit/image_processing_mobilevit.py
@@ -123,7 +123,7 @@ class MobileViTImageProcessor(BaseImageProcessor):
         size = size if size is not None else {"shortest_edge": 224}
         size = get_size_dict(size, default_to_square=False)
         crop_size = crop_size if crop_size is not None else {"height": 256, "width": 256}
-        crop_size = get_size_dict(crop_size)
+        crop_size = get_size_dict(crop_size, param_name="crop_size")
 
         self.do_resize = do_resize
         self.size = size
@@ -182,6 +182,8 @@ class MobileViTImageProcessor(BaseImageProcessor):
                 The channel dimension format of the image. If not provided, it will be the same as the input image.
         """
         size = get_size_dict(size)
+        if "height" not in size or "width" not in size:
+            raise ValueError(f"The `size` dictionary must contain the keys `height` and `width`. Got {size.keys()}")
         return center_crop(image, size=(size["height"], size["width"]), data_format=data_format, **kwargs)
 
     def rescale(
@@ -280,7 +282,7 @@ class MobileViTImageProcessor(BaseImageProcessor):
         size = size if size is not None else self.size
         size = get_size_dict(size, default_to_square=False)
         crop_size = crop_size if crop_size is not None else self.crop_size
-        crop_size = get_size_dict(crop_size)
+        crop_size = get_size_dict(crop_size, param_name="crop_size")
 
         if not is_batched(images):
             images = [images]
