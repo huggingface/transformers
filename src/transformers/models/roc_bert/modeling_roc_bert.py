9259ab18247969b153073d845416ec0c31dbff89
Arthur <48595927+ArthurZucker@users.noreply.github.com>
2022-11-15 10:46:34 +0100

    update relative positional embedding (#20203)
    * update relative positional embedding
    
    * make fix copies
    
    * add `use_cache` to list of arguments
    
    * fixup
    
    * 1line fucntion
    
    * add `test_decoder_model_past_with_large_inputs_relative_pos_emb`
    
    * add relative pos embedding test for more models
    
    * style

--- src/transformers/models/roc_bert/modeling_roc_bert.py
+++ src/transformers/models/roc_bert/modeling_roc_bert.py
@@ -331,6 +331,7 @@ class RoCBertSelfAttention(nn.Module):
 
         query_layer = self.transpose_for_scores(mixed_query_layer)
 
+        use_cache = past_key_value is not None
         if self.is_decoder:
             # if cross_attention save Tuple(torch.Tensor, torch.Tensor) of all cross attention key/value_states.
             # Further calls to cross_attention layer can then reuse all cross-attention
@@ -345,10 +346,16 @@ class RoCBertSelfAttention(nn.Module):
         attention_scores = torch.matmul(query_layer, key_layer.transpose(-1, -2))
 
         if self.position_embedding_type == "relative_key" or self.position_embedding_type == "relative_key_query":
-            seq_length = hidden_states.size()[1]
-            position_ids_l = torch.arange(seq_length, dtype=torch.long, device=hidden_states.device).view(-1, 1)
-            position_ids_r = torch.arange(seq_length, dtype=torch.long, device=hidden_states.device).view(1, -1)
+            query_length, key_length = query_layer.shape[2], key_layer.shape[2]
+            if use_cache:
+                position_ids_l = torch.tensor(key_length - 1, dtype=torch.long, device=hidden_states.device).view(
+                    -1, 1
+                )
+            else:
+                position_ids_l = torch.arange(query_length, dtype=torch.long, device=hidden_states.device).view(-1, 1)
+            position_ids_r = torch.arange(key_length, dtype=torch.long, device=hidden_states.device).view(1, -1)
             distance = position_ids_l - position_ids_r
+
             positional_embedding = self.distance_embedding(distance + self.max_position_embeddings - 1)
             positional_embedding = positional_embedding.to(dtype=query_layer.dtype)  # fp16 compatibility
 
