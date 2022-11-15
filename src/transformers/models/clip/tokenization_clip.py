83870654a9dfa2e90e3e3dd96a8fa3a37133521d
TilmannR <TilmannR@users.noreply.github.com>
2022-11-15 16:00:16 +0100

    Fix docstring of CLIPTokenizer(Fast) (#20233)

--- src/transformers/models/clip/tokenization_clip.py
+++ src/transformers/models/clip/tokenization_clip.py
@@ -275,7 +275,7 @@ class CLIPTokenizer(PreTrainedTokenizer):
         unk_token (`str`, *optional*, defaults to `<|endoftext|>`):
             The unknown token. A token that is not in the vocabulary cannot be converted to an ID and is set to be this
             token instead.
-        bos_token (`str`, *optional*, defaults to `<|endoftext|>`):
+        bos_token (`str`, *optional*, defaults to `<|startoftext|>`):
             The beginning of sequence token.
         eos_token (`str`, *optional*, defaults to `<|endoftext|>`):
             The end of sequence token.
