4442937d99ef747a42b9e271ec169f979b41b1ee
ydshieh <ydshieh@users.noreply.github.com>
2022-11-15 08:33:13 +0100

    rebase

--- tests/pipelines/test_pipelines_image_segmentation.py
+++ tests/pipelines/test_pipelines_image_segmentation.py
@@ -184,7 +184,6 @@ class ImageSegmentationPipelineTests(unittest.TestCase, metaclass=PipelineTestCa
         )
 
     @require_torch
-    @unittest.skip("This test is broken for now")
     def test_small_model_pt(self):
         model_id = "hf-internal-testing/tiny-detr-mobilenetsv3-panoptic"
 
