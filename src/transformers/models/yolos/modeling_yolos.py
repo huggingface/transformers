608e7b0e4779c6b1cfc76abbd34fce999561c464
IMvision12 <88665786+IMvision12@users.noreply.github.com>
2022-11-16 22:23:40 +0530

    remaining pytorch type hints (#20217)
    * Update modeling_flava.py
    
    * Update modeling_markuplm.py
    
    * Update modeling_glpn.py
    
    * Update modeling_roc_bert.py
    
    * Update modeling_segformer.py
    
    * Update modeling_tapas.py
    
    * Update modeling_tapas.py
    
    * Update modeling_tapas.py
    
    * Update modeling_tapas.py
    
    * Update modeling_trocr.py
    
    * Update modeling_videomae.py
    
    * Update modeling_videomae.py
    
    * Update modeling_videomae.py
    
    * Update modeling_yolos.py
    
    * Update modeling_wav2vec2.py
    
    * Update modeling_jukebox.py
    
    * Update modeling_jukebox.py
    
    * Update modeling_jukebox.py
    
    * Update modeling_jukebox.py

--- src/transformers/models/yolos/modeling_yolos.py
+++ src/transformers/models/yolos/modeling_yolos.py
@@ -641,7 +641,7 @@ class YolosModel(YolosPreTrainedModel):
         output_attentions: Optional[bool] = None,
         output_hidden_states: Optional[bool] = None,
         return_dict: Optional[bool] = None,
-    ):
+    ) -> Union[Tuple, BaseModelOutputWithPooling]:
         output_attentions = output_attentions if output_attentions is not None else self.config.output_attentions
         output_hidden_states = (
             output_hidden_states if output_hidden_states is not None else self.config.output_hidden_states
