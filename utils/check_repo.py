7b345a4c4e24ec969b6ea1e59c384e7b0974db11
NielsRogge <48327001+NielsRogge@users.noreply.github.com>
2022-11-17 15:43:20 +0100

    Add AutoBackbone + ResNetBackbone (#20229)
    * Add ResNetBackbone
    
    * Define channels and strides as property
    
    * Remove file
    
    * Add test for backbone
    
    * Update BackboneOutput class
    
    * Remove strides property
    
    * Fix docstring
    
    * Add backbones to SHOULD_HAVE_THEIR_OWN_PAGE
    
    * Fix auto mapping name
    
    * Add sanity check for out_features
    
    * Set stage names based on depths
    
    * Update to tuple
    
    Co-authored-by: Niels Rogge <nielsrogge@Nielss-MacBook-Pro.local>

--- utils/check_repo.py
+++ utils/check_repo.py
@@ -47,6 +47,7 @@ PRIVATE_MODELS = [
 # Being in this list is an exception and should **not** be the rule.
 IGNORE_NON_TESTED = PRIVATE_MODELS.copy() + [
     # models to ignore for not tested
+    "ResNetBackbone",  # Backbones have their own tests.
     "CLIPSegDecoder",  # Building part of bigger (tested) model.
     "TableTransformerEncoder",  # Building part of bigger (tested) model.
     "TableTransformerDecoder",  # Building part of bigger (tested) model.
@@ -668,6 +669,8 @@ SHOULD_HAVE_THEIR_OWN_PAGE = [
     "PyTorchBenchmarkArguments",
     "TensorFlowBenchmark",
     "TensorFlowBenchmarkArguments",
+    "ResNetBackbone",
+    "AutoBackbone",
 ]
 
 
