268b11636687317782322233bbbac83495916d9f
Joey Öhman <joeyoh@kth.se>
2022-11-17 17:08:35 +0100

    Tokenization progress

--- src/transformers/models/gpt_sw3/tokenization_gpt_sw3.py
+++ src/transformers/models/gpt_sw3/tokenization_gpt_sw3.py
@@ -14,6 +14,8 @@ logger = logging.get_logger(__name__)
 VOCAB_FILES_NAMES = {"vocab_file": "spm.model"}
 
 # TODO: rename spm.model -> spiece.model?
+# TODO: This does not seem to be used at all though except for some unit testing? The file is found with the above:
+#  VOCAB_FILES_NAMES, which is sufficient to find the tokenizer since there is one per model size
 PRETRAINED_VOCAB_FILES_MAP = {
     "vocab_file": {
         "AI-Sweden/gpt-sw3-126m": "https://huggingface.co/AI-Sweden/gpt-sw3-126m/resolve/main/spm.model",
@@ -24,6 +26,7 @@ PRETRAINED_VOCAB_FILES_MAP = {
     }
 }
 
+# TODO: This does not seem to be used except for prompting a warning when tokenizing sequences longer than these
 PRETRAINED_POSITIONAL_EMBEDDINGS_SIZES = {
     "AI-Sweden/gpt-sw3-126m": 2048,
     "AI-Sweden/gpt-sw3-350m": 2048,
@@ -32,100 +35,27 @@ PRETRAINED_POSITIONAL_EMBEDDINGS_SIZES = {
     "AI-Sweden/gpt-sw3-20b": 2048,
 }
 
-SPIECE_UNDERLINE = "▁"
+# SPIECE_UNDERLINE = "▁"
 
 
 class GptSw3Tokenizer(PreTrainedTokenizer):
-    """
-    Construct an ALBERT tokenizer. Based on [SentencePiece](https://github.com/google/sentencepiece).
-
-    This tokenizer inherits from [`PreTrainedTokenizer`] which contains most of the main methods. Users should refer to
-    this superclass for more information regarding those methods.
-
-    Args:
-        vocab_file (`str`):
-            [SentencePiece](https://github.com/google/sentencepiece) file (generally has a *.spm* extension) that
-            contains the vocabulary necessary to instantiate a tokenizer.
-        do_lower_case (`bool`, *optional*, defaults to `True`):
-            Whether or not to lowercase the input when tokenizing.
-        remove_space (`bool`, *optional*, defaults to `True`):
-            Whether or not to strip the text when tokenizing (removing excess spaces before and after the string).
-        keep_accents (`bool`, *optional*, defaults to `False`):
-            Whether or not to keep accents when tokenizing.
-        bos_token (`str`, *optional*, defaults to `"[CLS]"`):
-            The beginning of sequence token that was used during pretraining. Can be used a sequence classifier token.
-
-            <Tip>
-
-            When building a sequence using special tokens, this is not the token that is used for the beginning of
-            sequence. The token used is the `cls_token`.
-
-            </Tip>
-
-        eos_token (`str`, *optional*, defaults to `"[SEP]"`):
-            The end of sequence token.
-
-            <Tip>
-
-            When building a sequence using special tokens, this is not the token that is used for the end of sequence.
-            The token used is the `sep_token`.
-
-            </Tip>
-
-        unk_token (`str`, *optional*, defaults to `"<unk>"`):
-            The unknown token. A token that is not in the vocabulary cannot be converted to an ID and is set to be this
-            token instead.
-        sep_token (`str`, *optional*, defaults to `"[SEP]"`):
-            The separator token, which is used when building a sequence from multiple sequences, e.g. two sequences for
-            sequence classification or for a text and a question for question answering. It is also used as the last
-            token of a sequence built with special tokens.
-        pad_token (`str`, *optional*, defaults to `"<pad>"`):
-            The token used for padding, for example when batching sequences of different lengths.
-        cls_token (`str`, *optional*, defaults to `"[CLS]"`):
-            The classifier token which is used when doing sequence classification (classification of the whole sequence
-            instead of per-token classification). It is the first token of the sequence when built with special tokens.
-        mask_token (`str`, *optional*, defaults to `"[MASK]"`):
-            The token used for masking values. This is the token used when training this model with masked language
-            modeling. This is the token which the model will try to predict.
-        sp_model_kwargs (`dict`, *optional*):
-            Will be passed to the `SentencePieceProcessor.__init__()` method. The [Python wrapper for
-            SentencePiece](https://github.com/google/sentencepiece/tree/master/python) can be used, among other things,
-            to set:
-
-            - `enable_sampling`: Enable subword regularization.
-            - `nbest_size`: Sampling parameters for unigram. Invalid for BPE-Dropout.
-
-              - `nbest_size = {0,1}`: No sampling is performed.
-              - `nbest_size > 1`: samples from the nbest_size results.
-              - `nbest_size < 0`: assuming that nbest_size is infinite and samples from the all hypothesis (lattice)
-                using forward-filtering-and-backward-sampling algorithm.
-
-            - `alpha`: Smoothing parameter for unigram sampling, and dropout probability of merge operations for
-              BPE-dropout.
-
-    Attributes:
-        sp_model (`SentencePieceProcessor`):
-            The *SentencePiece* processor that is used for every conversion (string, tokens and IDs).
-    """
-
     vocab_files_names = VOCAB_FILES_NAMES
     pretrained_vocab_files_map = PRETRAINED_VOCAB_FILES_MAP
     max_model_input_sizes = PRETRAINED_POSITIONAL_EMBEDDINGS_SIZES
 
     def __init__(
-        self,
-        vocab_file,
-        do_lower_case=False,
-        remove_space=False,
-        keep_accents=False,
-        unk_token="<|endoftext|>",
-        bos_token="<|endoftext|>",
-        eos_token="<|endoftext|>",
-        sp_model_kwargs: Optional[Dict[str, Any]] = None,
-        **kwargs
+            self,
+            vocab_file,
+            do_lower_case=False,
+            remove_space=False,
+            keep_accents=False,
+            unk_token="<|endoftext|>",
+            bos_token="<|endoftext|>",
+            eos_token="<|endoftext|>",
+            sp_model_kwargs: Optional[Dict[str, Any]] = None,
+            **kwargs
     ) -> None:
 
-        print("INSIDE TOKENIZATION_GPT_SW3 CLASS INIT! :)")
         self.sp_model_kwargs = {} if sp_model_kwargs is None else sp_model_kwargs
 
         super().__init__(
@@ -150,85 +80,58 @@ class GptSw3Tokenizer(PreTrainedTokenizer):
         self.sp_model.Load(vocab_file)
 
     @property
-    def vocab_size(self):
+    def vocab_size(self) -> int:
         return len(self.sp_model)
 
-    def get_vocab(self):
-        vocab = {self.convert_ids_to_tokens(i): i for i in range(self.vocab_size)}
-        vocab.update(self.added_tokens_encoder)
-        return vocab
-
-    def __getstate__(self):
-        state = self.__dict__.copy()
-        state["sp_model"] = None
-        return state
-
-    def __setstate__(self, d):
-        self.__dict__ = d
-
-        # for backward compatibility
-        if not hasattr(self, "sp_model_kwargs"):
-            self.sp_model_kwargs = {}
-
-        self.sp_model = spm.SentencePieceProcessor(**self.sp_model_kwargs)
-        self.sp_model.Load(self.vocab_file)
-
     def preprocess_text(self, inputs):
-        if self.remove_space:
-            outputs = " ".join(inputs.strip().split())
-        else:
-            outputs = inputs
-
+        outputs = inputs
         # TODO: other normalization we used in data pipeline
         # outputs = outputs.replace("``", '"').replace("''", '"')
-
-        if not self.keep_accents:
-            # outputs = unicodedata.normalize("NFC", outputs)
-            # outputs = "".join([c for c in outputs if not unicodedata.combining(c)])
-            outputs = outputs
-        if self.do_lower_case:
-            outputs = outputs.lower()
-
+        # outputs = unicodedata.normalize("NFC", outputs)
         return outputs
 
-    def _tokenize(self, text: str) -> List[str]:
-        """Tokenize a string."""
+    def _tokenize(self, text, **kwargs):
+        """
+            Converts a string in a sequence of tokens (string), using the tokenizer. Split in words for word-based
+            vocabulary or sub-words for sub-word-based vocabularies (BPE/SentencePieces/WordPieces).
+
+            Do NOT take care of added tokens.
+        """
+
         text = self.preprocess_text(text)
-        # pieces = self.sp_model.encode(text, out_type=str)
-        # new_pieces = []
-        # for piece in pieces:
-        #     if len(piece) > 1 and piece[-1] == str(",") and piece[-2].isdigit():
-        #         cur_pieces = self.sp_model.EncodeAsPieces(piece[:-1].replace(SPIECE_UNDERLINE, ""))
-        #         if piece[0] != SPIECE_UNDERLINE and cur_pieces[0][0] == SPIECE_UNDERLINE:
-        #             if len(cur_pieces[0]) == 1:
-        #                 cur_pieces = cur_pieces[1:]
-        #             else:
-        #                 cur_pieces[0] = cur_pieces[0][1:]
-        #         cur_pieces.append(piece[-1])
-        #         new_pieces.extend(cur_pieces)
-        #     else:
-        #         new_pieces.append(piece)
-        #
-        # return new_pieces
         return self.sp_model.encode(text, out_type=str)
 
     def _convert_token_to_id(self, token):
         """Converts a token (str) in an id using the vocab."""
         return self.sp_model.PieceToId(token)
 
-    def _convert_id_to_token(self, index):
+    def _convert_id_to_token(self, index: int) -> str:
         """Converts an index (integer) in a token (str) using the vocab."""
         return self.sp_model.IdToPiece(index)
 
+    # NOT ABSTRACT!
     def convert_tokens_to_string(self, tokens: List[str]) -> str:
-        out_str = ""
-        for token in tokens:
-            if token[0] == SPIECE_UNDERLINE:
-                out_str += " " + token[1:]
-            else:
-                out_str += token
+        print(tokens)
+        return self.sp_model.decode(tokens)
 
-        return out_str.strip()
+    # NOT ABSTRACT!
+    # TODO: directly overriding decode() removes checks for datatype (e.g. if token_ids is a numpy array, TF/PT tensor
+    #  etc) but instead gives a speedup of factor 4. Meaning that its faster but does not support anything but python
+    #  lists
+    def _decode(
+            self,
+            token_ids: List[int],
+            skip_special_tokens: bool = False,
+            clean_up_tokenization_spaces: bool = True,
+            spaces_between_special_tokens: bool = True,
+            **kwargs
+    ) -> str:
+        return self.sp_model.decode(token_ids)
+
+    def get_vocab(self) -> Dict[str, int]:
+        vocab = {self.convert_ids_to_tokens(i): i for i in range(self.vocab_size)}
+        vocab.update(self.added_tokens_encoder)
+        return vocab
 
     def save_vocabulary(self, save_directory: str, filename_prefix: Optional[str] = None) -> Tuple[str]:
         if not os.path.isdir(save_directory):
--- src/transformers/models/gpt_sw3/tokenization_gpt_sw3_fast.py
+++ src/transformers/models/gpt_sw3/tokenization_gpt_sw3_fast.py
@@ -1,190 +1,97 @@
-# coding=utf-8
-# Copyright 2022 The Open AI Team Authors and The HuggingFace Inc. team.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-"""Tokenization classes for OpenAI GPT."""
+import os
+from shutil import copyfile
+from typing import List, Optional, Tuple, Dict, Any
 
+import sentencepiece as spm
 
-import json
-from typing import TYPE_CHECKING, List, Optional, Tuple
-
-from tokenizers import pre_tokenizers
-
-from ...tokenization_utils_base import BatchEncoding
+from ... import GptSw3Tokenizer
+from ...tokenization_utils import AddedToken
 from ...tokenization_utils_fast import PreTrainedTokenizerFast
 from ...utils import logging
-from .tokenization_gpt_sw3 import GptSw3Tokenizer
-
-
-if TYPE_CHECKING:
-    from transformers.pipelines.conversational import Conversation
 
 
 logger = logging.get_logger(__name__)
+# TODO: rename spm.model -> spiece.model?
+VOCAB_FILES_NAMES = {"vocab_file": "spm.model"}
 
-VOCAB_FILES_NAMES = {"vocab_file": "vocab.json", "merges_file": "merges.txt", "tokenizer_file": "tokenizer.json"}
-
+# TODO: rename spm.model -> spiece.model?
+# TODO: This does not seem to be used at all though except for some unit testing? The file is found with the above:
+#  VOCAB_FILES_NAMES, which is sufficient to find the tokenizer since there is one per model size
 PRETRAINED_VOCAB_FILES_MAP = {
     "vocab_file": {
-        "gpt_sw3": "https://huggingface.co/gpt_sw3/resolve/main/vocab.json",
-        "gpt_sw3-medium": "https://huggingface.co/gpt_sw3-medium/resolve/main/vocab.json",
-        "gpt_sw3-large": "https://huggingface.co/gpt_sw3-large/resolve/main/vocab.json",
-        "gpt_sw3-xl": "https://huggingface.co/gpt_sw3-xl/resolve/main/vocab.json",
-        "distilgpt_sw3": "https://huggingface.co/distilgpt_sw3/resolve/main/vocab.json",
-    },
-    "merges_file": {
-        "gpt_sw3": "https://huggingface.co/gpt_sw3/resolve/main/merges.txt",
-        "gpt_sw3-medium": "https://huggingface.co/gpt_sw3-medium/resolve/main/merges.txt",
-        "gpt_sw3-large": "https://huggingface.co/gpt_sw3-large/resolve/main/merges.txt",
-        "gpt_sw3-xl": "https://huggingface.co/gpt_sw3-xl/resolve/main/merges.txt",
-        "distilgpt_sw3": "https://huggingface.co/distilgpt_sw3/resolve/main/merges.txt",
-    },
-    "tokenizer_file": {
-        "gpt_sw3": "https://huggingface.co/gpt_sw3/resolve/main/tokenizer.json",
-        "gpt_sw3-medium": "https://huggingface.co/gpt_sw3-medium/resolve/main/tokenizer.json",
-        "gpt_sw3-large": "https://huggingface.co/gpt_sw3-large/resolve/main/tokenizer.json",
-        "gpt_sw3-xl": "https://huggingface.co/gpt_sw3-xl/resolve/main/tokenizer.json",
-        "distilgpt_sw3": "https://huggingface.co/distilgpt_sw3/resolve/main/tokenizer.json",
-    },
+        "AI-Sweden/gpt-sw3-126m": "https://huggingface.co/AI-Sweden/gpt-sw3-126m/resolve/main/spm.model",
+        "AI-Sweden/gpt-sw3-350m": "https://huggingface.co/AI-Sweden/gpt-sw3-350m/resolve/main/spm.model",
+        "AI-Sweden/gpt-sw3-1.6b": "https://huggingface.co/AI-Sweden/gpt-sw3-1.6b/resolve/main/spm.model",
+        "AI-Sweden/gpt-sw3-6.7b": "https://huggingface.co/AI-Sweden/gpt-sw3-6.7b/resolve/main/spm.model",
+        "AI-Sweden/gpt-sw3-20b": "https://huggingface.co/AI-Sweden/gpt-sw3-20b/resolve/main/spm.model",
+    }
 }
 
+# TODO: This does not seem to be used except for prompting a warning when tokenizing sequences longer than these
 PRETRAINED_POSITIONAL_EMBEDDINGS_SIZES = {
-    "gpt_sw3": 1024,
-    "gpt_sw3-medium": 1024,
-    "gpt_sw3-large": 1024,
-    "gpt_sw3-xl": 1024,
-    "distilgpt_sw3": 1024,
+    "AI-Sweden/gpt-sw3-126m": 2048,
+    "AI-Sweden/gpt-sw3-350m": 2048,
+    "AI-Sweden/gpt-sw3-1.6b": 2048,
+    "AI-Sweden/gpt-sw3-6.7b": 2048,
+    "AI-Sweden/gpt-sw3-20b": 2048,
 }
 
 
 class GptSw3TokenizerFast(PreTrainedTokenizerFast):
-    """
-    Construct a "fast" GPT-2 tokenizer (backed by HuggingFace's *tokenizers* library). Based on byte-level
-    Byte-Pair-Encoding.
-
-    This tokenizer has been trained to treat spaces like parts of the tokens (a bit like sentencepiece) so a word will
-    be encoded differently whether it is at the beginning of the sentence (without space) or not:
-
-    ```
-    >>> from transformers import GptSw3TokenizerFast
-    >>> tokenizer = GptSw3TokenizerFast.from_pretrained("gpt_sw3")
-    >>> tokenizer("Hello world")['input_ids']
-    [15496, 995]
-    >>> tokenizer(" Hello world")['input_ids']
-    [18435, 995]
-    ```
-
-    You can get around that behavior by passing `add_prefix_space=True` when instantiating this tokenizer, but since
-    the model was not pretrained this way, it might yield a decrease in performance.
-
-    <Tip>
-
-    When used with `is_split_into_words=True`, this tokenizer needs to be instantiated with `add_prefix_space=True`.
-
-    </Tip>
-
-    This tokenizer inherits from [`PreTrainedTokenizerFast`] which contains most of the main methods. Users should
-    refer to this superclass for more information regarding those methods.
-
-    Args:
-        vocab_file (`str`):
-            Path to the vocabulary file.
-        merges_file (`str`):
-            Path to the merges file.
-        errors (`str`, *optional*, defaults to `"replace"`):
-            Paradigm to follow when decoding bytes to UTF-8. See
-            [bytes.decode](https://docs.python.org/3/library/stdtypes.html#bytes.decode) for more information.
-        unk_token (`str`, *optional*, defaults to `<|endoftext|>`):
-            The unknown token. A token that is not in the vocabulary cannot be converted to an ID and is set to be this
-            token instead.
-        bos_token (`str`, *optional*, defaults to `<|endoftext|>`):
-            The beginning of sequence token.
-        eos_token (`str`, *optional*, defaults to `<|endoftext|>`):
-            The end of sequence token.
-        add_prefix_space (`bool`, *optional*, defaults to `False`):
-            Whether or not to add an initial space to the input. This allows to treat the leading word just as any
-            other word. (GptSw3 tokenizer detect beginning of words by the preceding space).
-        trim_offsets (`bool`, *optional*, defaults to `True`):
-            Whether or not the post-processing step should trim offsets to avoid including whitespaces.
-    """
-
     vocab_files_names = VOCAB_FILES_NAMES
     pretrained_vocab_files_map = PRETRAINED_VOCAB_FILES_MAP
     max_model_input_sizes = PRETRAINED_POSITIONAL_EMBEDDINGS_SIZES
-    model_input_names = ["input_ids", "attention_mask"]
     slow_tokenizer_class = GptSw3Tokenizer
 
     def __init__(
-        self,
-        vocab_file=None,
-        merges_file=None,
-        tokenizer_file=None,
-        unk_token="<|endoftext|>",
-        bos_token="<|endoftext|>",
-        eos_token="<|endoftext|>",
-        add_prefix_space=False,
-        **kwargs
-    ):
-        super().__init__(
+            self,
             vocab_file,
-            merges_file,
-            tokenizer_file=tokenizer_file,
-            unk_token=unk_token,
+            do_lower_case=False,
+            remove_space=False,
+            keep_accents=False,
+            unk_token="<|endoftext|>",
+            bos_token="<|endoftext|>",
+            eos_token="<|endoftext|>",
+            sp_model_kwargs: Optional[Dict[str, Any]] = None,
+            **kwargs
+    ) -> None:
+        self.sp_model_kwargs = {} if sp_model_kwargs is None else sp_model_kwargs
+
+        super().__init__(
+            do_lower_case=do_lower_case,
+            remove_space=remove_space,
+            keep_accents=keep_accents,
             bos_token=bos_token,
             eos_token=eos_token,
-            add_prefix_space=add_prefix_space,
+            unk_token=unk_token,
+            sp_model_kwargs=self.sp_model_kwargs,
             **kwargs,
         )
 
-        self.add_bos_token = kwargs.pop("add_bos_token", False)
+        self.do_lower_case = do_lower_case
+        self.remove_space = remove_space
+        self.keep_accents = keep_accents
+        self.vocab_file = vocab_file
 
-        pre_tok_state = json.loads(self.backend_tokenizer.pre_tokenizer.__getstate__())
-        if pre_tok_state.get("add_prefix_space", add_prefix_space) != add_prefix_space:
-            pre_tok_class = getattr(pre_tokenizers, pre_tok_state.pop("type"))
-            pre_tok_state["add_prefix_space"] = add_prefix_space
-            self.backend_tokenizer.pre_tokenizer = pre_tok_class(**pre_tok_state)
+        # print("MODEL NAME OR PATH")
+        # print(self.name_or_path)
+        self.sp_model = spm.SentencePieceProcessor(**self.sp_model_kwargs)
+        self.sp_model.Load(vocab_file)
 
-        self.add_prefix_space = add_prefix_space
-
-    def _batch_encode_plus(self, *args, **kwargs) -> BatchEncoding:
-        is_split_into_words = kwargs.get("is_split_into_words", False)
-        assert self.add_prefix_space or not is_split_into_words, (
-            f"You need to instantiate {self.__class__.__name__} with add_prefix_space=True "
-            "to use it with pretokenized inputs."
-        )
-
-        return super()._batch_encode_plus(*args, **kwargs)
-
-    def _encode_plus(self, *args, **kwargs) -> BatchEncoding:
-        is_split_into_words = kwargs.get("is_split_into_words", False)
-
-        assert self.add_prefix_space or not is_split_into_words, (
-            f"You need to instantiate {self.__class__.__name__} with add_prefix_space=True "
-            "to use it with pretokenized inputs."
-        )
-
-        return super()._encode_plus(*args, **kwargs)
 
     def save_vocabulary(self, save_directory: str, filename_prefix: Optional[str] = None) -> Tuple[str]:
-        files = self._tokenizer.model.save(save_directory, name=filename_prefix)
-        return tuple(files)
+        if not os.path.isdir(save_directory):
+            logger.error(f"Vocabulary path ({save_directory}) should be a directory")
+            return
+        out_vocab_file = os.path.join(
+            save_directory, (filename_prefix + "-" if filename_prefix else "") + VOCAB_FILES_NAMES["vocab_file"]
+        )
 
-    def _build_conversation_input_ids(self, conversation: "Conversation") -> List[int]:
-        """This corresponds to DialoGPT variants of models."""
-        input_ids = []
-        for is_user, text in conversation.iter_texts():
-            input_ids.extend(self.encode(text, add_special_tokens=False) + [self.eos_token_id])
+        if os.path.abspath(self.vocab_file) != os.path.abspath(out_vocab_file) and os.path.isfile(self.vocab_file):
+            copyfile(self.vocab_file, out_vocab_file)
+        elif not os.path.isfile(self.vocab_file):
+            with open(out_vocab_file, "wb") as fi:
+                content_spiece_model = self.sp_model.serialized_model_proto()
+                fi.write(content_spiece_model)
 
-        if len(input_ids) > self.model_max_length:
-            input_ids = input_ids[-self.model_max_length :]
-        return input_ids
+        return (out_vocab_file,)
