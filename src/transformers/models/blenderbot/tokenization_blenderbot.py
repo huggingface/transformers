1c0b37a8c38ea9c325b9357dbfc963a05e7e90c3
Joao Gante <joaofranciscocardosogante@gmail.com>
2022-11-17 12:33:44 +0000

    TF: add test for `PushToHubCallback` (#20231)
    * test hub tf callback
    
    * create repo before cloning it

--- src/transformers/models/blenderbot/tokenization_blenderbot.py
+++ src/transformers/models/blenderbot/tokenization_blenderbot.py
@@ -399,6 +399,7 @@ class BlenderbotTokenizer(PreTrainedTokenizer):
         Build model inputs from a sequence or a pair of sequence for sequence classification tasks by concatenating and
         adding special tokens. A Blenderbot sequence has the following format:
         - single sequence: ` X </s>`
+
         Args:
             token_ids_0 (`List[int]`):
                 List of IDs to which the special tokens will be added
