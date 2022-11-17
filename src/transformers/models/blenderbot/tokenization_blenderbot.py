1f508d8c6f2d8cae25070376b6d827832fedc0fd
Arthur Zucker <arthur.zucker@gmail.com>
2022-11-17 15:52:56 +0000

    fix the doc to specify that add_prefix_space = False

--- src/transformers/models/blenderbot/tokenization_blenderbot.py
+++ src/transformers/models/blenderbot/tokenization_blenderbot.py
@@ -99,6 +99,7 @@ class BlenderbotTokenizer(PreTrainedTokenizer):
     ```
     >>> from transformers import BlenderbotTokenizer
     >>> tokenizer = BlenderbotTokenizer.from_pretrained("facebook/blenderbot-3B")
+    >>> tokenizer.add_prefix_space = False
     >>> tokenizer("Hello world")['input_ids']
     [6950, 1085, 2]
     >>> tokenizer(" Hello world")['input_ids']
