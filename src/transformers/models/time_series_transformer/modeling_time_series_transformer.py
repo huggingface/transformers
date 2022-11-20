05d13d1052e3dbca33c44498cd57bb4d69e3b6f8
Kashif Rasul <kashif.rasul@gmail.com>
2022-11-20 18:23:15 +0100

    undid change as it does not help

--- src/transformers/models/time_series_transformer/modeling_time_series_transformer.py
+++ src/transformers/models/time_series_transformer/modeling_time_series_transformer.py
@@ -1585,23 +1585,16 @@ class TimeSeriesTransformerModel(TimeSeriesTransformerPreTrainedModel):
         embedded_cat = self.embedder(static_categorical_features)
 
         # static features
-        if self.config.scaling == "std":
-            log_loc = loc.abs().log1p() if self.config.input_size == 1 else loc.squeeze(1).abs().log1p()
-            log_scale = scale.log() if self.config.input_size == 1 else scale.squeeze(1).log()
-        else:
-            log_loc = (
-                context.std(dim=1, keepdim=True).log1p() if self.config.input_size == 1 else context.std(dim=1).log1p()
-            )
-            log_scale = scale.log() if self.config.input_size == 1 else scale.squeeze(1).log()
-
+        log_loc = loc.abs().log1p() if self.config.input_size == 1 else loc.squeeze(1).abs().log1p()
+        log_scale = scale.log() if self.config.input_size == 1 else scale.squeeze(1).log()
         static_feat = torch.cat((embedded_cat, static_real_features, log_loc, log_scale), dim=1)
         expanded_static_feat = static_feat.unsqueeze(1).expand(-1, time_feat.shape[1], -1)
 
         # all features
         features = torch.cat((expanded_static_feat, time_feat), dim=-1)
 
+        # lagged features
         lagged_sequence = self.get_lagged_subsequences(sequence=inputs, subsequences_length=subsequences_length)
-
         lags_shape = lagged_sequence.shape
         reshaped_lagged_sequence = lagged_sequence.reshape(lags_shape[0], lags_shape[1], -1)
 
