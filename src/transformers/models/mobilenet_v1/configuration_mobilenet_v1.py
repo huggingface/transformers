42976aecc7307c9f9555c13c22a51d57bc1188f1
Matthijs Hollemans <mail@hollance.com>
2022-11-21 16:21:28 +0100

    add MobileNetV1 model (#17799)
    * add model files etc for MobileNetV2
    
    rename files for MobileNetV1
    
    initial implementation of MobileNetV1
    
    fix conversion script
    
    cleanup
    
    write docs
    
    tweaks
    
    fix conversion script
    
    extract hidden states
    
    fix test cases
    
    make fixup
    
    fixup it all
    
    remove main from doc link
    
    fixes
    
    fix tests
    
    fix up
    
    use google org
    
    fix weird assert
    
    * fixup
    
    * use google organization for checkpoints

--- src/transformers/models/mobilenet_v1/configuration_mobilenet_v1.py
+++ src/transformers/models/mobilenet_v1/configuration_mobilenet_v1.py
@@ -12,7 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-""" MobileNetV2 model configuration"""
+""" MobileNetV1 model configuration"""
 
 from collections import OrderedDict
 from typing import Mapping
@@ -26,21 +26,19 @@ from ...utils import logging
 
 logger = logging.get_logger(__name__)
 
-MOBILENET_V2_PRETRAINED_CONFIG_ARCHIVE_MAP = {
-    "google/mobilenet_v2_1.4_224": "https://huggingface.co/google/mobilenet_v2_1.4_224/resolve/main/config.json",
-    "google/mobilenet_v2_1.0_224": "https://huggingface.co/google/mobilenet_v2_1.0_224/resolve/main/config.json",
-    "google/mobilenet_v2_0.75_160": "https://huggingface.co/google/mobilenet_v2_0.75_160/resolve/main/config.json",
-    "google/mobilenet_v2_0.35_96": "https://huggingface.co/google/mobilenet_v2_0.35_96/resolve/main/config.json",
-    # See all MobileNetV2 models at https://huggingface.co/models?filter=mobilenet_v2
+MOBILENET_V1_PRETRAINED_CONFIG_ARCHIVE_MAP = {
+    "google/mobilenet_v1_1.0_224": "https://huggingface.co/google/mobilenet_v1_1.0_224/resolve/main/config.json",
+    "google/mobilenet_v1_0.75_192": "https://huggingface.co/google/mobilenet_v1_0.75_192/resolve/main/config.json",
+    # See all MobileNetV1 models at https://huggingface.co/models?filter=mobilenet_v1
 }
 
 
-class MobileNetV2Config(PretrainedConfig):
+class MobileNetV1Config(PretrainedConfig):
     r"""
-    This is the configuration class to store the configuration of a [`MobileNetV2Model`]. It is used to instantiate a
-    MobileNetV2 model according to the specified arguments, defining the model architecture. Instantiating a
-    configuration with the defaults will yield a similar configuration to that of the MobileNetV2
-    [google/mobilenet_v2_1.0_224](https://huggingface.co/google/mobilenet_v2_1.0_224) architecture.
+    This is the configuration class to store the configuration of a [`MobileNetV1Model`]. It is used to instantiate a
+    MobileNetV1 model according to the specified arguments, defining the model architecture. Instantiating a
+    configuration with the defaults will yield a similar configuration to that of the MobileNetV1
+    [google/mobilenet_v1_1.0_224](https://huggingface.co/google/mobilenet_v1_1.0_224) architecture.
 
     Configuration objects inherit from [`PretrainedConfig`] and can be used to control the model outputs. Read the
     documentation from [`PretrainedConfig`] for more information.
@@ -53,22 +51,8 @@ class MobileNetV2Config(PretrainedConfig):
         depth_multiplier (`float`, *optional*, defaults to 1.0):
             Shrinks or expands the number of channels in each layer. Default is 1.0, which starts the network with 32
             channels. This is sometimes also called "alpha" or "width multiplier".
-        depth_divisible_by (`int`, *optional*, defaults to 8):
-            The number of channels in each layer will always be a multiple of this number.
         min_depth (`int`, *optional*, defaults to 8):
             All layers will have at least this many channels.
-        expand_ratio (`float`, *optional*, defaults to 6.0):
-            The number of output channels of the first layer in each block is input channels times expansion ratio.
-        output_stride (`int`, *optional*, defaults to 32):
-            The ratio between the spatial resolution of the input and output feature maps. By default the model reduces
-            the input dimensions by a factor of 32. If `output_stride` is 8 or 16, the model uses dilated convolutions
-            on the depthwise layers instead of regular convolutions, so that the feature maps never become more than 8x
-            or 16x smaller than the input image.
-        first_layer_is_expansion (`bool`, `optional`, defaults to `True`):
-            True if the very first convolution layer is also the expansion layer for the first expansion block.
-        finegrained_output (`bool`, `optional`, defaults to `True`):
-            If true, the number of output channels in the final convolution layer will stay large (1280) even if
-            `depth_multiplier` is less than 1.
         hidden_act (`str` or `function`, *optional*, defaults to `"relu6"`):
             The non-linear activation function (function or string) in the Transformer encoder and convolution layers.
         tf_padding (`bool`, `optional`, defaults to `True`):
@@ -79,42 +63,34 @@ class MobileNetV2Config(PretrainedConfig):
             The standard deviation of the truncated_normal_initializer for initializing all weight matrices.
         layer_norm_eps (`float`, *optional*, defaults to 0.001):
             The epsilon used by the layer normalization layers.
-        semantic_loss_ignore_index (`int`, *optional*, defaults to 255):
-            The index that is ignored by the loss function of the semantic segmentation model.
 
     Example:
 
     ```python
-    >>> from transformers import MobileNetV2Config, MobileNetV2Model
+    >>> from transformers import MobileNetV1Config, MobileNetV1Model
 
-    >>> # Initializing a "mobilenet_v2_1.0_224" style configuration
-    >>> configuration = MobileNetV2Config()
+    >>> # Initializing a "mobilenet_v1_1.0_224" style configuration
+    >>> configuration = MobileNetV1Config()
 
-    >>> # Initializing a model from the "mobilenet_v2_1.0_224" style configuration
-    >>> model = MobileNetV2Model(configuration)
+    >>> # Initializing a model from the "mobilenet_v1_1.0_224" style configuration
+    >>> model = MobileNetV1Model(configuration)
 
     >>> # Accessing the model configuration
     >>> configuration = model.config
     ```"""
-    model_type = "mobilenet_v2"
+    model_type = "mobilenet_v1"
 
     def __init__(
         self,
         num_channels=3,
         image_size=224,
         depth_multiplier=1.0,
-        depth_divisible_by=8,
         min_depth=8,
-        expand_ratio=6,
-        output_stride=32,
-        first_layer_is_expansion=True,
-        finegrained_output=True,
         hidden_act="relu6",
         tf_padding=True,
-        classifier_dropout_prob=0.8,
+        classifier_dropout_prob=0.999,
         initializer_range=0.02,
         layer_norm_eps=0.001,
-        semantic_loss_ignore_index=255,
         **kwargs
     ):
         super().__init__(**kwargs)
@@ -125,21 +101,15 @@ class MobileNetV2Config(PretrainedConfig):
         self.num_channels = num_channels
         self.image_size = image_size
         self.depth_multiplier = depth_multiplier
-        self.depth_divisible_by = depth_divisible_by
         self.min_depth = min_depth
-        self.expand_ratio = expand_ratio
-        self.output_stride = output_stride
-        self.first_layer_is_expansion = first_layer_is_expansion
-        self.finegrained_output = finegrained_output
         self.hidden_act = hidden_act
         self.tf_padding = tf_padding
         self.classifier_dropout_prob = classifier_dropout_prob
         self.initializer_range = initializer_range
         self.layer_norm_eps = layer_norm_eps
-        self.semantic_loss_ignore_index = semantic_loss_ignore_index
 
 
-class MobileNetV2OnnxConfig(OnnxConfig):
+class MobileNetV1OnnxConfig(OnnxConfig):
 
     torch_onnx_minimum_version = version.parse("1.11")
 
