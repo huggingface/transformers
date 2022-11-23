666e7f2dd6bcceb32e9526a60496a4ec1e7fee89
yangapku <yangapku@gmail.com>
2022-11-23 20:09:12 +0800

    fix refactor bug

--- src/transformers/models/chinese_clip/configuration_chinese_clip.py
+++ src/transformers/models/chinese_clip/configuration_chinese_clip.py
@@ -28,7 +28,6 @@ from ...configuration_utils import PretrainedConfig
 from ...onnx import OnnxConfig
 from ...utils import logging
 from ..bert.configuration_bert import BertConfig
-from ..clip.configuration_clip import CLIPVisionConfig
 
 
 logger = logging.get_logger(__name__)
