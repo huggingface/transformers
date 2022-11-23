674d0d9324bae4f90c4fbbb58bbaaafbb2f3d628
raghavanone <opensourcemaniacfreak@gmail.com>
2022-11-23 11:53:17 +0530

    Format file changes

--- tests/models/t5/test_tokenization_t5.py
+++ tests/models/t5/test_tokenization_t5.py
@@ -385,10 +385,8 @@ class T5TokenizationTest(TokenizerTesterMixin, unittest.TestCase):
         tokenizer = T5Tokenizer(SAMPLE_VOCAB, extra_ids=10)
         sentinel_tokens = tokenizer.get_sentinel_tokens()
         self.assertEquals(len(sentinel_tokens), 10)
-        self.assertListEqual(
-            sorted(sentinel_tokens), sorted([f"<extra_id_{str(i)}>" for i in range(0, 10)])
-        )
-        self.assertTrue([re.search("<extra_id_\d+>",token) is not None for token in sentinel_tokens])
+        self.assertListEqual(sorted(sentinel_tokens), sorted([f"<extra_id_{str(i)}>" for i in range(0, 10)]))
+        self.assertTrue([re.search("<extra_id_\d+>", token) is not None for token in sentinel_tokens])
 
     def test_get_sentinel_token_ids(self):
         tokenizer = T5Tokenizer(SAMPLE_VOCAB, extra_ids=10)
@@ -398,10 +396,8 @@ class T5TokenizationTest(TokenizerTesterMixin, unittest.TestCase):
         tokenizer = T5TokenizerFast(SAMPLE_VOCAB, extra_ids=10)
         sentinel_tokens = tokenizer.get_sentinel_tokens()
         self.assertEquals(len(sentinel_tokens), 10)
-        self.assertListEqual(
-            sorted(sentinel_tokens), sorted([f"<extra_id_{str(i)}>" for i in range(0, 10)])
-        )
-        self.assertTrue([re.search("<extra_id_\d+>",token) is not None for token in sentinel_tokens])
+        self.assertListEqual(sorted(sentinel_tokens), sorted([f"<extra_id_{str(i)}>" for i in range(0, 10)]))
+        self.assertTrue([re.search("<extra_id_\d+>", token) is not None for token in sentinel_tokens])
 
     def test_get_sentinel_token_ids_for_fasttokenizer(self):
         tokenizer = T5TokenizerFast(SAMPLE_VOCAB, extra_ids=10)
