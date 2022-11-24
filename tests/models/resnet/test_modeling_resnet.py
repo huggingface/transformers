f338daf5a0fabb59c477afa5956e29420947278d
Niels Rogge <nielsrogge@Nielss-MacBook-Pro.local>
2022-11-24 15:10:18 +0100

    Disable fx tests

--- tests/models/resnet/test_modeling_resnet.py
+++ tests/models/resnet/test_modeling_resnet.py
@@ -151,7 +151,7 @@ class ResNetModelTest(ModelTesterMixin, unittest.TestCase):
         else ()
     )
 
-    fx_compatible = True
+    fx_compatible = False
     test_pruning = False
     test_resize_embeddings = False
     test_head_masking = False
