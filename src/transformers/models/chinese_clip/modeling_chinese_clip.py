dc842f63822f23b83643cac6090a76241746535b
yangapku <yangapku@gmail.com>
2022-11-24 10:24:43 +0800

    fix refactor bug

--- src/transformers/models/chinese_clip/modeling_chinese_clip.py
+++ src/transformers/models/chinese_clip/modeling_chinese_clip.py
@@ -735,6 +735,38 @@ class ChineseCLIPTextPooler(nn.Module):
         return pooled_output
 
 
+class ChineseCLIPTextPreTrainedModel(PreTrainedModel):
+    """
+    An abstract class to handle weights initialization and a simple interface for downloading and loading pretrained
+    models.
+    """
+
+    config_class = ChineseCLIPTextConfig
+    base_model_prefix = "chinese_clip"
+    supports_gradient_checkpointing = True
+    _keys_to_ignore_on_load_missing = [r"position_ids"]
+
+    def _init_weights(self, module):
+        """Initialize the weights"""
+        if isinstance(module, nn.Linear):
+            # Slightly different from the TF version which uses truncated_normal for initialization
+            # cf https://github.com/pytorch/pytorch/pull/5617
+            module.weight.data.normal_(mean=0.0, std=self.config.initializer_range)
+            if module.bias is not None:
+                module.bias.data.zero_()
+        elif isinstance(module, nn.Embedding):
+            module.weight.data.normal_(mean=0.0, std=self.config.initializer_range)
+            if module.padding_idx is not None:
+                module.weight.data[module.padding_idx].zero_()
+        elif isinstance(module, nn.LayerNorm):
+            module.bias.data.zero_()
+            module.weight.data.fill_(1.0)
+
+    def _set_gradient_checkpointing(self, module, value=False):
+        if isinstance(module, ChineseCLIPTextEncoder):
+            module.gradient_checkpointing = value
+
+
 class ChineseCLIPPreTrainedModel(PreTrainedModel):
     """
     An abstract class to handle weights initialization and a simple interface for downloading and loading pretrained
@@ -1152,7 +1184,7 @@ class ChineseCLIPVisionTransformer(nn.Module):
     CHINESE_CLIP_START_DOCSTRING,
 )
 # Copied from transformers.models.bert.modeling_bert.BertModel with BERT->CHINESE_CLIP,Bert->ChineseCLIPText
-class ChineseCLIPTextModel(ChineseCLIPPreTrainedModel):
+class ChineseCLIPTextModel(ChineseCLIPTextPreTrainedModel):
     """
 
     The model can behave as an encoder (with only self-attention) as well as a decoder, in which case a layer of
