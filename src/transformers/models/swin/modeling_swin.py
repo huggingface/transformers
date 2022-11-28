9ef12babe152225131bc7525f9cd3008214b66ac
Felix Marty <9808326+fxmarty@users.noreply.github.com>
2022-11-28 17:30:31 +0100

    fix warnings

--- src/transformers/models/swin/modeling_swin.py
+++ src/transformers/models/swin/modeling_swin.py
@@ -209,7 +209,12 @@ def window_partition(input_feature, window_size):
     """
     batch_size, height, width, num_channels = input_feature.shape
     input_feature = input_feature.view(
-        batch_size, height // window_size, window_size, width // window_size, window_size, num_channels
+        batch_size,
+        torch.div(height, window_size, rounding_mode="trunc"),
+        window_size,
+        torch.div(width, window_size, rounding_mode="trunc"),
+        window_size,
+        num_channels,
     )
     windows = input_feature.permute(0, 1, 3, 2, 4, 5).contiguous().view(-1, window_size, window_size, num_channels)
     return windows
@@ -220,7 +225,23 @@ def window_reverse(windows, window_size, height, width):
     Merges windows to produce higher resolution features.
     """
     num_channels = windows.shape[-1]
-    windows = windows.view(-1, height // window_size, width // window_size, window_size, window_size, num_channels)
+    windows = windows.view(
+        -1,
+        torch.div(height, window_size, rounding_mode="trunc"),
+        torch.div(width, window_size, rounding_mode="trunc"),
+        window_size,
+        window_size,
+        num_channels,
+    )
+
+    window = windows.view(
+        -1,
+        torch.div(height, window_size, rounding_mode="trunc"),
+        torch.div(width, window_size, rounding_mode="trunc"),
+        window_size,
+        window_size,
+        num_channels,
+    )
     windows = windows.permute(0, 1, 3, 2, 4, 5).contiguous().view(-1, height, width, num_channels)
     return windows
 
@@ -478,7 +499,11 @@ class SwinSelfAttention(nn.Module):
             # Apply the attention mask is (precomputed for all layers in SwinModel forward() function)
             mask_shape = attention_mask.shape[0]
             attention_scores = attention_scores.view(
-                batch_size // mask_shape, mask_shape, self.num_attention_heads, dim, dim
+                torch.div(batch_size, mask_shape, rounding_mode="trunc"),
+                mask_shape,
+                self.num_attention_heads,
+                dim,
+                dim,
             )
             attention_scores = attention_scores + attention_mask.unsqueeze(1).unsqueeze(0)
             attention_scores = attention_scores.view(-1, self.num_attention_heads, dim, dim)
@@ -743,7 +768,8 @@ class SwinStage(nn.Module):
             hidden_states = layer_outputs[0]
 
         if self.downsample is not None:
-            height_downsampled, width_downsampled = (height + 1) // 2, (width + 1) // 2
+            height_downsampled = torch.div(height + 1, 2, rounding_mode="trunc")
+            width_downsampled = torch.div(width + 1, 2, rounding_mode="trunc")
             output_dimensions = (height, width, height_downsampled, width_downsampled)
             hidden_states = self.downsample(layer_outputs[0], input_dimensions)
         else:
