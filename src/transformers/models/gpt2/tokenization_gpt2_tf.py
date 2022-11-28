641dc5cc85da3fb2ee15464cc0f6d6af380f51ea
Pi Esposito <piero.skywalker@gmail.com>
2022-11-28 20:29:27 -0300

    add from config, get config and max length settings to address review

--- src/transformers/models/gpt2/tokenization_gpt2_tf.py
+++ src/transformers/models/gpt2/tokenization_gpt2_tf.py
@@ -4,6 +4,7 @@ from typing import Dict, List, Union
 import tensorflow as tf
 
 from keras_nlp.tokenizers import BytePairTokenizer
+from tensorflow_text import pad_model_inputs
 
 from .tokenization_gpt2 import GPT2Tokenizer
 
@@ -24,12 +25,16 @@ class TFGPT2Tokenizer(tf.keras.layers.Layer):
         merges (List[str]): Merges list for Byte Pair Tokenizer
     """
 
-    def __init__(self, vocab: Dict[str, int], merges: List[str]):
+    def __init__(self, vocab: Dict[str, int], merges: List[str], max_length: int = None, pad_token_id: int = None):
         super().__init__()
-        self.tf_tokenizer = BytePairTokenizer(vocab, merges)
+        self.pad_token_id = pad_token_id
+        self.max_length = max_length
+        self.vocab = vocab
+        self.merges = merges
+        self.tf_tokenizer = BytePairTokenizer(vocab, merges, sequence_length=max_length)
 
     @classmethod
-    def from_tokenizer(cls, tokenizer: GPT2Tokenizer):
+    def from_tokenizer(cls, tokenizer: GPT2Tokenizer, *args, **kwargs):
         """Creates TFGPT2Tokenizer from GPT2Tokenizer
 
         Args:
@@ -46,7 +51,7 @@ class TFGPT2Tokenizer(tf.keras.layers.Layer):
         """
         merges = [" ".join(m) for m in tokenizer.bpe_ranks.keys()]
         vocab = tokenizer.get_vocab()
-        return cls(vocab, merges)
+        return cls(vocab, merges, *args, **kwargs)
 
     @classmethod
     def from_pretrained(cls, pretrained_model_name_or_path: Union[str, os.PathLike], *init_inputs, **kwargs):
@@ -64,9 +69,36 @@ class TFGPT2Tokenizer(tf.keras.layers.Layer):
         ```
         """
         tokenizer = GPT2Tokenizer.from_pretrained(pretrained_model_name_or_path, *init_inputs, **kwargs)
-        return cls.from_tokenizer(tokenizer)
+        return cls.from_tokenizer(tokenizer, *init_inputs, **kwargs)
 
-    def call(self, x):
+    @classmethod
+    def from_config(cls, config):
+        """Creates TFGPT2Tokenizer from configurations
+
+        Args:
+            config (Dict): Dictionary with keys such as stated in `get_config`.
+        """
+        return cls(**config)
+
+    def get_config(self):
+        return {
+            "vocab": self.vocab,
+            "merges": self.merges,
+            "max_length": self.max_length,
+            "pad_token_id": self.pad_token_id,
+        }
+
+    def call(self, x, max_length: int = None):
         input_ids = self.tf_tokenizer(x)
         attention_mask = tf.ones_like(input_ids)
+
+        if self.pad_token_id is not None:
+            # pad the tokens up to max length
+            max_length = max_length if max_length is not None else self.max_length
+
+            if max_length is not None:
+                input_ids, attention_mask = pad_model_inputs(
+                    input_ids, max_seq_length=max_length, pad_value=self.pad_token_id
+                )
+
         return {"attention_mask": attention_mask, "input_ids": input_ids}
