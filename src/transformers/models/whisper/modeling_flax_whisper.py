03129935b7b73da97cd64f9bed8ff327352c4136
andyehrenberg <andyehrenberg@gmail.com>
2022-11-28 14:25:21 -0800

    remove unused imports

--- src/transformers/models/whisper/modeling_flax_whisper.py
+++ src/transformers/models/whisper/modeling_flax_whisper.py
@@ -24,8 +24,6 @@ from ...modeling_flax_utils import (
     ACT2FN,
     FlaxPreTrainedModel,
     append_call_sample_docstring,
-    append_replace_return_docstrings,
-    overwrite_call_docstring,
 )
 from ...utils import add_start_docstrings, add_start_docstrings_to_model_forward, logging, replace_return_docstrings
 from .configuration_whisper import WhisperConfig
