c2547bb5501038d0380be62dc05018b2feae1910
Arthur Zucker <arthur.zucker@gmail.com>
2022-11-29 10:02:48 +0000

    only send to device when used

--- tests/models/jukebox/test_modeling_jukebox.py
+++ tests/models/jukebox/test_modeling_jukebox.py
@@ -312,7 +312,7 @@ class Jukebox5bModelTester(unittest.TestCase):
 
     @slow
     def test_slow_sampling(self):
-        model = JukeboxModel.from_pretrained(self.model_id, min_duration=0).eval().to("cuda")
+        model = JukeboxModel.from_pretrained(self.model_id, min_duration=0).eval()
         labels = [i.cuda() for i in self.prepare_inputs(self.model_id)]
 
         set_seed(0)
