afc47add70b976cd60680676fb7ae33a11e359d7
Pi Esposito <piero.skywalker@gmail.com>
2022-11-29 12:02:40 -0300

    add in layer gpt2 tokenizer (#20421)
    * add minimal working gpt2 tokenizer
    
    * graph mode and output equivalence tests working
    
    * not today tensorflow. serialization test passing!
    
    * fix style, documentation, docstrings and all that jazz
    
    * passing consistency checks
    
    * move keras nlp to tf dependencies
    
    * fix tf modeling utils and gpt2 attention to enable compiling
    
    * fix (I hope) keras nlp dependencies
    
    * rever changes on generation
    
    * remove debug prints
    
    * remove redundant tf dummy objects
    
    * add from config, get config and max length settings to address review
    
    * let flake ignore the error on distillation you are welcome
    
    * test from config
    
    * add padding test
    
    * address sgugger review

--- src/transformers/__init__.py
+++ src/transformers/__init__.py
@@ -32,6 +32,7 @@ from .utils import (
     OptionalDependencyNotAvailable,
     _LazyModule,
     is_flax_available,
+    is_keras_nlp_available,
     is_sentencepiece_available,
     is_speech_available,
     is_tensorflow_text_available,
@@ -693,6 +694,19 @@ except OptionalDependencyNotAvailable:
     ]
 else:
     _import_structure["models.bert"].append("TFBertTokenizer")
+
+# keras-nlp-specific objects
+try:
+    if not is_keras_nlp_available():
+        raise OptionalDependencyNotAvailable()
+except OptionalDependencyNotAvailable:
+    from .utils import dummy_keras_nlp_objects
+
+    _import_structure["utils.dummy_keras_nlp_objects"] = [
+        name for name in dir(dummy_keras_nlp_objects) if not name.startswith("_")
+    ]
+else:
+    _import_structure["models.gpt2"].append("TFGPT2Tokenizer")
 
 try:
     if not (is_sentencepiece_available() and is_speech_available()):
@@ -3827,6 +3841,14 @@ if TYPE_CHECKING:
         from .utils.dummy_tensorflow_text_objects import *
     else:
         from .models.bert import TFBertTokenizer
+
+    try:
+        if not is_keras_nlp_available():
+            raise OptionalDependencyNotAvailable()
+    except OptionalDependencyNotAvailable:
+        from .utils.dummy_keras_nlp_objects import *
+    else:
+        from .models.gpt2 import TFGPT2Tokenizer
 
     try:
         if not (is_speech_available() and is_sentencepiece_available()):
