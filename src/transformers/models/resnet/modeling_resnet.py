a68445c41bb3c131e873ea525f12ff01a4c660c2
NielsRogge <48327001+NielsRogge@users.noreply.github.com>
2022-12-01 18:19:37 +0100

    Fix doctest (#20534)
    Co-authored-by: Niels Rogge <nielsrogge@Nielss-MacBook-Pro.local>

--- src/transformers/models/resnet/modeling_resnet.py
+++ src/transformers/models/resnet/modeling_resnet.py
@@ -474,11 +474,16 @@ class ResNetBackbone(ResNetPreTrainedModel):
         >>> image = Image.open(requests.get(url, stream=True).raw)
 
         >>> processor = AutoImageProcessor.from_pretrained("microsoft/resnet-50")
-        >>> model = AutoBackbone.from_pretrained("microsoft/resnet-50")
+        >>> model = AutoBackbone.from_pretrained(
+        ...     "microsoft/resnet-50", out_features=["stage1", "stage2", "stage3", "stage4"]
+        ... )
 
         >>> inputs = processor(image, return_tensors="pt")
 
         >>> outputs = model(**inputs)
+        >>> feature_maps = outputs.feature_maps
+        >>> list(feature_maps[-1].shape)
+        [1, 2048, 7, 7]
         ```"""
         return_dict = return_dict if return_dict is not None else self.config.use_return_dict
         output_hidden_states = (
