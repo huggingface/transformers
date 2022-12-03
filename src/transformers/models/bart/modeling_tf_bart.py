62be1265d226d72f97ca3b494f8654163a302a77
Cole Howard <uglyboxer@gmail.com>
2022-12-03 15:40:20 -0800

    add doc update

--- src/transformers/models/bart/modeling_tf_bart.py
+++ src/transformers/models/bart/modeling_tf_bart.py
@@ -1489,8 +1489,7 @@ class TFBartForConditionalGeneration(TFBartPretrainedModel, TFCausalLanguageMode
 @add_start_docstrings(
     """
         Bart model with a sequence classification/head on top (a linear layer on top of the pooled output) e.g. for
-        GLUE
-    tasks.
+        GLUE tasks.
     """,
     BART_START_DOCSTRING,
 )
