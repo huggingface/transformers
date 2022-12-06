082a9ea78953083401543d2018e6ed46bf23b147
Michael Benayoun <michael@huggingface.co>
2022-12-06 17:18:22 +0100

    Fix transformers.onnx cli

--- src/transformers/onnx/__main__.py
+++ src/transformers/onnx/__main__.py
@@ -18,13 +18,164 @@ from pathlib import Path
 
 from packaging import version
 
+from .. import AutoFeatureExtractor, AutoTokenizer, AutoProcessor
+from .convert import export, validate_model_outputs
 from ..utils import logging
 from ..utils.import_utils import is_optimum_available
+from .utils import get_preprocessor
+from .features import FeaturesManager
 
 
 MIN_OPTIMUM_VERSION = "1.5.0"
 
 ENCODER_DECODER_MODELS = ["vision-encoder-decoder"]
+
+
+def export_with_optimum(args):
+    if is_optimum_available():
+        from optimum.version import __version__ as optimum_version
+
+        parsed_optimum_version = version.parse(optimum_version)
+        if parsed_optimum_version < version.parse(MIN_OPTIMUM_VERSION):
+            raise RuntimeError(
+                f"transformers.onnx requires optimum >= {MIN_OPTIMUM_VERSION} but {optimum_version} is installed. You "
+                "can upgrade optimum by running: pip install -U optimum"
+            )
+    else:
+        raise RuntimeError(
+            "transformers.onnx requires optimum to run, you can install the library by running: pip install optimum"
+        )
+    cmd_line = [
+        sys.executable,
+        "-m",
+        "optimum.exporters.onnx",
+        f"--model {args.model}",
+        f"--task {args.feature}",
+        f"--framework {args.framework}" if args.framework is not None else "",
+        f"{args.output}",
+    ]
+    proc = subprocess.Popen(" ".join(cmd_line), stdout=subprocess.PIPE, shell=True)
+    proc.wait()
+
+    logger.info(
+        "The export was done by optimum.exporters.onnx, it is suggested to use this tool directly for the future, more"
+        " information here: https://huggingface.co/docs/optimum/exporters/onnx/usage_guides/export_a_model."
+    )
+
+
+def export_with_transformers(args):
+    args.output = args.output if args.output.is_file() else args.output.joinpath("model.onnx")
+    if not args.output.parent.exists():
+        args.output.parent.mkdir(parents=True)
+
+    # Allocate the model
+    model = FeaturesManager.get_model_from_feature(
+        args.feature, args.model, framework=args.framework, cache_dir=args.cache_dir
+    )
+
+    model_kind, model_onnx_config = FeaturesManager.check_supported_model_or_raise(model, feature=args.feature)
+    onnx_config = model_onnx_config(model.config)
+
+    if model_kind in ENCODER_DECODER_MODELS:
+        encoder_model = model.get_encoder()
+        decoder_model = model.get_decoder()
+
+        encoder_onnx_config = onnx_config.get_encoder_config(encoder_model.config)
+        decoder_onnx_config = onnx_config.get_decoder_config(
+            encoder_model.config, decoder_model.config, feature=args.feature
+        )
+
+        if args.opset is None:
+            args.opset = max(encoder_onnx_config.default_onnx_opset, decoder_onnx_config.default_onnx_opset)
+
+        if args.opset < min(encoder_onnx_config.default_onnx_opset, decoder_onnx_config.default_onnx_opset):
+            raise ValueError(
+                f"Opset {args.opset} is not sufficient to export {model_kind}. At least "
+                f" {min(encoder_onnx_config.default_onnx_opset, decoder_onnx_config.default_onnx_opset)} is required."
+            )
+
+        preprocessor = AutoFeatureExtractor.from_pretrained(args.model)
+
+        onnx_inputs, onnx_outputs = export(
+            preprocessor,
+            encoder_model,
+            encoder_onnx_config,
+            args.opset,
+            args.output.parent.joinpath("encoder_model.onnx"),
+        )
+
+        validate_model_outputs(
+            encoder_onnx_config,
+            preprocessor,
+            encoder_model,
+            args.output.parent.joinpath("encoder_model.onnx"),
+            onnx_outputs,
+            args.atol if args.atol else encoder_onnx_config.atol_for_validation,
+        )
+
+        preprocessor = AutoTokenizer.from_pretrained(args.model)
+
+        onnx_inputs, onnx_outputs = export(
+            preprocessor,
+            decoder_model,
+            decoder_onnx_config,
+            args.opset,
+            args.output.parent.joinpath("decoder_model.onnx"),
+        )
+
+        validate_model_outputs(
+            decoder_onnx_config,
+            preprocessor,
+            decoder_model,
+            args.output.parent.joinpath("decoder_model.onnx"),
+            onnx_outputs,
+            args.atol if args.atol else decoder_onnx_config.atol_for_validation,
+        )
+        logger.info(
+            f"All good, model saved at: {args.output.parent.joinpath('encoder_model.onnx').as_posix()},"
+            f" {args.output.parent.joinpath('decoder_model.onnx').as_posix()}"
+        )
+
+    else:
+        # Instantiate the appropriate preprocessor
+        if args.preprocessor == "auto":
+            preprocessor = get_preprocessor(args.model)
+        elif args.preprocessor == "tokenizer":
+            preprocessor = AutoTokenizer.from_pretrained(args.model)
+        elif args.preprocessor == "feature_extractor":
+            preprocessor = AutoFeatureExtractor.from_pretrained(args.model)
+        elif args.preprocessor == "processor":
+            preprocessor = AutoProcessor.from_pretrained(args.model)
+        else:
+            raise ValueError(f"Unknown preprocessor type '{args.preprocessor}'")
+
+        # Ensure the requested opset is sufficient
+        if args.opset is None:
+            args.opset = onnx_config.default_onnx_opset
+
+        if args.opset < onnx_config.default_onnx_opset:
+            raise ValueError(
+                f"Opset {args.opset} is not sufficient to export {model_kind}. "
+                f"At least  {onnx_config.default_onnx_opset} is required."
+            )
+
+        onnx_inputs, onnx_outputs = export(
+            preprocessor,
+            model,
+            onnx_config,
+            args.opset,
+            args.output,
+        )
+
+        if args.atol is None:
+            args.atol = onnx_config.atol_for_validation
+
+        validate_model_outputs(onnx_config, preprocessor, model, args.output, onnx_outputs, args.atol)
+        logger.info(f"All good, model saved at: {args.output.as_posix()}")
+        logger.info(
+            "The export was done by transformers.onnx, the recommended way is by using optimum.exporters.onnx, more "
+            "information here: https://huggingface.co/docs/optimum/exporters/onnx/usage_guides/export_a_model."
+        )
 
 
 def main():
@@ -61,40 +212,23 @@ def main():
         default="auto",
         help="Which type of preprocessor to use. 'auto' tries to automatically detect it.",
     )
-    args = parser.parse_args()
-
-    cmd_line = [
-        sys.executable,
-        "-m",
-        "optimum.exporters.onnx",
-        f"--model {args.model}",
-        f"--task {args.feature}",
-        f"--framework {args.framework}" if args.framework is not None else "",
-        f"{args.output}",
-    ]
-    proc = subprocess.Popen(" ".join(cmd_line), stdout=subprocess.PIPE, shell=True)
-    proc.wait()
-
-    logger.info(
-        "The export was done by optimum.exporters.onnx, it is suggested to use this tool directly for the future, more"
-        " information here: https://huggingface.co/docs/optimum/exporters/onnx/usage_guides/export_a_model"
+    parser.add_argument(
+        "--export_with_transformers",
+        action="store_true",
+        help=(
+            "Whether to use transformers.onnx instead of optimum.exporters.onnx to perform the ONNX export. It can be "
+            "when exporting a model supported in transformers but not in optimum, otherwise it is not recommended."
+        )
     )
+
+    args = parser.parse_args()
+    if args.export_with_transformers:
+        export_with_transformers(args)
+    else:
+        export_with_optimum(args)
 
 
 if __name__ == "__main__":
-    if is_optimum_available():
-        from optimum.version import __version__ as optimum_version
-
-        parsed_optimum_version = version.parse(optimum_version)
-        if parsed_optimum_version < version.parse(MIN_OPTIMUM_VERSION):
-            raise RuntimeError(
-                f"transformers.onnx requires optimum >= {MIN_OPTIMUM_VERSION} but {optimum_version} is installed. You "
-                "can upgrade optimum by running: pip install -U optimum"
-            )
-    else:
-        raise RuntimeError(
-            "transformers.onnx requires optimum to run, you can install the library by running: pip install optimum"
-        )
     logger = logging.get_logger("transformers.onnx")  # pylint: disable=invalid-name
     logger.setLevel(logging.INFO)
     main()
