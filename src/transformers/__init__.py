47e81f70583c7c28e12aaf9e645edc146fbe28ea
Cole Howard <uglyboxer@gmail.com>
2022-12-07 09:05:39 -0800

    Add TFBartForSequenceClassification (#20570)
    * read to load
    
    * base functionality
    
    * revert init
    
    * fix dummy data
    
    * moving right along
    
    * moving right along
    
    * finally
    
    * cleanup
    
    * pull out comment
    
    * add test
    
    * update docstring for main class
    
    * flake comments and rewriting copies from make repo-consistency`
    
    * remove irrelevant differences/accidental spaces
    
    * put copies back after space removals
    
    * mid
    
    * final test pass
    
    * stray comment
    
    * update test file
    
    * update test file
    
    * fixup
    
    * black
    
    * missed
    
    * black missed one more
    
    * sytle
    
    * add doc update
    
    * fix order of output class
    
    * comment
    
    * Revert "comment"
    
    This reverts commit 03f86b6948808461939cc8ad4ad74305dfb67700.
    
    * remove redundant function, and redundant reshape
    
    * move change out of common
    
    * style
    
    * put common spaces back
    
    * reorder kwargs in output
    
    * doc style

--- src/transformers/__init__.py
+++ src/transformers/__init__.py
@@ -2513,7 +2513,9 @@ else:
             "TFAutoModelWithLMHead",
         ]
     )
-    _import_structure["models.bart"].extend(["TFBartForConditionalGeneration", "TFBartModel", "TFBartPretrainedModel"])
+    _import_structure["models.bart"].extend(
+        ["TFBartForConditionalGeneration", "TFBartForSequenceClassification", "TFBartModel", "TFBartPretrainedModel"]
+    )
     _import_structure["models.bert"].extend(
         [
             "TF_BERT_PRETRAINED_MODEL_ARCHIVE_LIST",
@@ -5402,7 +5404,12 @@ if TYPE_CHECKING:
             TFAutoModelForVision2Seq,
             TFAutoModelWithLMHead,
         )
-        from .models.bart import TFBartForConditionalGeneration, TFBartModel, TFBartPretrainedModel
+        from .models.bart import (
+            TFBartForConditionalGeneration,
+            TFBartForSequenceClassification,
+            TFBartModel,
+            TFBartPretrainedModel,
+        )
         from .models.bert import (
             TF_BERT_PRETRAINED_MODEL_ARCHIVE_LIST,
             TFBertEmbeddings,
