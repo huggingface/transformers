e4be99974f3ad4939ae9ec6d3f4f2c099713d972
jongjyh <jongjyh0221@gmail.com>
2022-12-08 21:54:41 +0800

    recover

--- src/transformers/models/altclip/configuration_altclip.py
+++ src/transformers/models/altclip/configuration_altclip.py
@@ -17,8 +17,8 @@ import copy
 import os
 from typing import Union
 
-from transformers.configuration_utils import PretrainedConfig
-from transformers.utils import logging
+from ...configuration_utils import PretrainedConfig
+from ...utils import logging
 
 
 logger = logging.get_logger(__name__)
