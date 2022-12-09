02014405a042b78bcbf95a4ce0a28f4908ad2d83
Joey Öhman <joeyoh@kth.se>
2022-12-09 13:57:44 +0100

    make fix-copies

--- src/transformers/models/gpt_sw3/modeling_gpt_sw3.py
+++ src/transformers/models/gpt_sw3/modeling_gpt_sw3.py
@@ -871,7 +871,7 @@ class GPTSw3ForSequenceClassification(GPTSw3PreTrainedModel):
             sequence_lengths = -1
         else:
             if input_ids is not None:
-                sequence_lengths = torch.ne(input_ids, self.config.pad_token_id).sum(-1) - 1
+                sequence_lengths = (torch.ne(input_ids, self.config.pad_token_id).sum(-1) - 1).to(logits.device)
             else:
                 sequence_lengths = -1
                 logger.warning(
