62a41e8f68d1cdf2b757a3abe7308bd723943073
Joey Öhman <joeyoh@kth.se>
2022-12-09 10:59:06 +0100

    Make style, make quality

--- src/transformers/models/gpt_sw3/__init__.py
+++ src/transformers/models/gpt_sw3/__init__.py
@@ -18,7 +18,7 @@
 
 from typing import TYPE_CHECKING
 
-from ...utils import OptionalDependencyNotAvailable, _LazyModule, is_torch_available, is_sentencepiece_available
+from ...utils import OptionalDependencyNotAvailable, _LazyModule, is_sentencepiece_available, is_torch_available
 
 
 _import_structure = {
