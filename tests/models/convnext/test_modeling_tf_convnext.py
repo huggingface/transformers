be89955c857b0c7e94ad16a0640af55531467770
Niels Rogge <nielsrogge@Nielss-MacBook-Pro.local>
2022-12-09 11:35:12 +0100

    Improve TF tests

--- tests/models/convnext/test_modeling_tf_convnext.py
+++ tests/models/convnext/test_modeling_tf_convnext.py
@@ -170,10 +170,6 @@ class TFConvNextModelTest(TFModelTesterMixin, unittest.TestCase):
         config_and_inputs = self.model_tester.prepare_config_and_inputs()
         self.model_tester.create_and_check_model(*config_and_inputs)
 
-    @unittest.skip(reason="Model doesn't have attention layers")
-    def test_attention_outputs(self):
-        pass
-
     @unittest.skipIf(
         not is_tf_available() or len(tf.config.list_physical_devices("GPU")) == 0,
         reason="TF does not support backprop for grouped convolutions on CPU.",
