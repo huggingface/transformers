4968a662446b834c82ef377991fc988830d54bdc
younesbelkada <younesbelkada@gmail.com>
2022-12-13 20:19:11 +0000

    remove `token_type_ids`

--- src/transformers/models/blip/modeling_blip.py
+++ src/transformers/models/blip/modeling_blip.py
@@ -1249,7 +1249,6 @@ class BlipForQuestionAnswering(BlipPreTrainedModel):
         input_ids: torch.LongTensor,
         pixel_values: torch.FloatTensor,
         attention_mask: Optional[torch.LongTensor] = None,
-        token_type_ids: Optional[torch.LongTensor] = None,
         **generate_kwargs
     ) -> torch.LongTensor:
         r"""
@@ -1263,9 +1262,6 @@ class BlipForQuestionAnswering(BlipPreTrainedModel):
             attention_mask (*torch.LongTensor* of shape *(batch_size, sequence_length)*, *optional*):
                 Mask to avoid performing attention on padding token indices. Mask values selected in `[0, 1]`. `1` for
                 tokens that are NOT MASKED, `0` for MASKED tokens.
-            token_type_ids (*torch.LongTensor* of shape *(batch_size, sequence_length)*, *optional*):
-                Segment token indices to indicate first and second portions of the inputs. Not used but kept for
-                compatibility with the *BlipProcessor*
             **generate_kwargs:
                 Additional arguments passed to the *generate* function of the decoder
 
@@ -1372,7 +1368,6 @@ class BlipForImageTextRetrieval(BlipPreTrainedModel):
         pixel_values: torch.FloatTensor,
         use_itm_head: Optional[bool] = True,
         attention_mask: Optional[torch.LongTensor] = None,
-        token_type_ids: Optional[torch.LongTensor] = None,
         output_attentions: Optional[bool] = None,
         output_hidden_states: Optional[bool] = None,
         return_dict: Optional[bool] = None,
