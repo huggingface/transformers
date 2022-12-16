be7328a7d152fb34e1fd9cc76a5baf5c90e726b3
Niels Rogge <nielsrogge@Nielss-MacBook-Pro.local>
2022-12-16 14:32:27 +0100

    Improve docs

--- src/transformers/models/swin2sr/__init__.py
+++ src/transformers/models/swin2sr/__init__.py
@@ -71,7 +71,7 @@ if TYPE_CHECKING:
     except OptionalDependencyNotAvailable:
         pass
     else:
-        from .feature_extraction_swin2sr import Swin2SRImageProcessor
+        from .image_processing_swin2sr import Swin2SRImageProcessor
 
 
 else:
