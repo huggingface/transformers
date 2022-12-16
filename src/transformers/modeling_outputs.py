26dd041c6e45379141302e2d293ab4cd9cf805d4
NielsRogge <48327001+NielsRogge@users.noreply.github.com>
2022-12-16 16:24:01 +0100

    Add Swin2SR (#19784)
    * First draft
    
    * Add more improvements
    
    * Improve forward pass
    
    * Fix layernorm
    
    * Add upscaler
    
    * More improvements
    
    * More improvements
    
    * More improvements
    
    * Improve conversion script
    
    * Add preprocessing
    
    * Make output match original implementation
    
    * Add additional attributes
    
    * Add support for more models
    
    * Support more models
    
    * Add support for real world sr
    
    * Add initial Swin2SRFeatureExtractor
    
    * Add ImageSuperResolutionOutput
    
    * Make more tests pass
    
    * Use BaseModelOutput
    
    * Fix one more test
    
    * Fix more tests
    
    * Fix another test
    
    * Fix all tests
    
    * Rename to Swin2SRImageProcessor
    
    * Fix toctree
    
    * Fix toctree
    
    * Fix rebase
    
    * Improve Swin2SRImageProcessor
    
    * Remove feature extractor file
    
    * Improve model
    
    * Improve conversion script
    
    * Fix integration test
    
    * Fix init
    
    * Fix conversion script
    
    * Address comments
    
    * Improve upsampler
    
    * Add NearestConvUpsampler
    
    * Improve pixel shuffle upsampler
    
    * Improve auxiliary upsampler
    
    * Improve conversion script
    
    * Rename conv_last to final_convolution
    
    * Fix rebase
    
    * Improve upsample module
    
    * Add padding to image processor
    
    * Fix bug
    
    * Update padding
    
    * Remove print statement and fix integration test
    
    * Improve docs
    
    * Add image processor tests
    
    * Convert all checkpoints, fix testsé
    
    * Remove print statements
    
    * Fix import
    
    Co-authored-by: Niels Rogge <nielsrogge@Nielss-MacBook-Pro.local>

--- src/transformers/modeling_outputs.py
+++ src/transformers/modeling_outputs.py
@@ -1205,6 +1205,34 @@ class DepthEstimatorOutput(ModelOutput):
 
 
 @dataclass
+class ImageSuperResolutionOutput(ModelOutput):
+    """
+    Base class for outputs of image super resolution models.
+
+    Args:
+        loss (`torch.FloatTensor` of shape `(1,)`, *optional*, returned when `labels` is provided):
+            Reconstruction loss.
+        reconstruction (`torch.FloatTensor` of shape `(batch_size, num_channels, height, width)`):
+           Reconstructed images, possibly upscaled.
+        hidden_states (`tuple(torch.FloatTensor)`, *optional*, returned when `output_hidden_states=True` is passed or when `config.output_hidden_states=True`):
+            Tuple of `torch.FloatTensor` (one for the output of the embeddings, if the model has an embedding layer, +
+            one for the output of each stage) of shape `(batch_size, sequence_length, hidden_size)`. Hidden-states
+            (also called feature maps) of the model at the output of each stage.
+        attentions (`tuple(torch.FloatTensor)`, *optional*, returned when `output_attentions=True` is passed or when `config.output_attentions=True`):
+            Tuple of `torch.FloatTensor` (one for each layer) of shape `(batch_size, num_heads, patch_size,
+            sequence_length)`.
+
+            Attentions weights after the attention softmax, used to compute the weighted average in the self-attention
+            heads.
+    """
+
+    loss: Optional[torch.FloatTensor] = None
+    reconstruction: torch.FloatTensor = None
+    hidden_states: Optional[Tuple[torch.FloatTensor]] = None
+    attentions: Optional[Tuple[torch.FloatTensor]] = None
+
+
+@dataclass
 class Wav2Vec2BaseModelOutput(ModelOutput):
     """
     Base class for models that have been trained with the Wav2Vec2 loss objective.
