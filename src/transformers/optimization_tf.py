d0bfdd20f4b8d3d59c29ef41b9a836c80de7f1e1
Matt <Rocketknight1@users.noreply.github.com>
2022-12-20 13:40:45 +0000

    TF AdamWeightDecay fix for 2.11 (#20848)
    * Fix incorrect import for the base optimizer for AdamWeightDecay
    
    * Fix incorrect import for the base optimizer for AdamWeightDecay

--- src/transformers/optimization_tf.py
+++ src/transformers/optimization_tf.py
@@ -21,10 +21,10 @@ from typing import Callable, List, Optional, Union
 import tensorflow as tf
 
 
-if hasattr(tf.keras, "optimizer") and hasattr(tf.keras.optimizer, "legacy"):
-    Adam = tf.keras.optimizer.legacy.Adam
-else:
-    Adam = tf.keras.optimizers.Adam
+try:
+    from tensorflow.keras.optimizers.legacy import Adam
+except ImportError:
+    from tensorflow.keras.optimizers import Adam
 
 
 class WarmUp(tf.keras.optimizers.schedules.LearningRateSchedule):
