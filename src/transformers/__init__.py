b8795a02aa3ffca4e08fa5c41ee0009bfe04384e
Younes Belkada <49240599+younesbelkada@users.noreply.github.com>
2022-12-21 09:39:10 +0100

    Add BLIP (#20716)
    * add new model like
    
    * add v1
    
    * v1
    
    * v1
    
    * vision encoder logits match
    
    * v2
    
    * fix
    
    * add docstring
    
    * CI tests pass
    
    * fix tests
    
    * make fixup
    
    * add to `toctree`
    
    * fix processors
    
    * fix processors
    
    * fix doc
    
    * fill title
    
    * add content doc
    
    * remove from tokenization auto
    
    * fix config
    
    * change order
    
    * add `# Copied from`
    
    * few fixes
    
    - add correct license on modeling text
    - remove dummy argument
    
    * Apply suggestions from code review
    
    Co-authored-by: NielsRogge <48327001+NielsRogge@users.noreply.github.com>
    
    * replace name
    
    * refactor a bit
    
    * more refactor
    
    * remove unused arg
    
    * make fixup + remove some `# Adapted from ...`
    
    * Apply suggestions from code review
    
    Co-authored-by: NielsRogge <48327001+NielsRogge@users.noreply.github.com>
    
    * more `# Copied from`
    
    * Apply suggestions from code review
    
    Co-authored-by: NielsRogge <48327001+NielsRogge@users.noreply.github.com>
    
    * now `generate` supports no prefix
    
    * remove `FeatureExtractor`
    
    * fix path
    
    * correct dependency
    
    * fix tests
    
    * few fixes
    
    * add integration tests
    
    * add correct conversion script
    
    * Apply suggestions from code review
    
    Co-authored-by: Sylvain Gugger <35901082+sgugger@users.noreply.github.com>
    
    * add `blip` to tokenization auto
    
    * fix docstrings
    
    * fix test + add image
    
    * remove processor from uncorrect place
    
    * Apply suggestions from code review
    
    Co-authored-by: NielsRogge <48327001+NielsRogge@users.noreply.github.com>
    
    * clean up a bit
    
    * Apply suggestions from code review
    
    Co-authored-by: NielsRogge <48327001+NielsRogge@users.noreply.github.com>
    
    * clean pixel mask
    
    * clean pixel mask
    
    * fix `F`
    
    * Update src/transformers/models/blip/modeling_blip.py
    
    Co-authored-by: NielsRogge <48327001+NielsRogge@users.noreply.github.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: NielsRogge <48327001+NielsRogge@users.noreply.github.com>
    
    * fix output
    
    * Apply suggestions from code review
    
    Co-authored-by: NielsRogge <48327001+NielsRogge@users.noreply.github.com>
    
    * fix pad token id
    
    * remove `token_type_ids`
    
    * make fixup
    
    * Apply suggestions from code review
    
    Co-authored-by: NielsRogge <48327001+NielsRogge@users.noreply.github.com>
    
    * make fixup
    
    * Apply suggestions from code review
    
    Co-authored-by: NielsRogge <48327001+NielsRogge@users.noreply.github.com>
    
    * add comments
    
    * Update src/transformers/models/blip/modeling_blip.py
    
    Co-authored-by: NielsRogge <48327001+NielsRogge@users.noreply.github.com>
    
    * remove `token_type_ids`
    
    * make fixup
    
    * better name
    
    * replace with `image_attention_mask`
    
    * refactor
    
    * make fixup
    
    * better docstring
    
    * replace `answer_xx`
    
    * remove ununsed args
    
    * add `labels`
    
    * add `labels`
    
    * fix processing tests
    
    * make fixup
    
    * make fixup
    
    * put correct repo
    
    * remove `pad`
    
    * remove `crop` and `center_crop`
    
    * Update src/transformers/models/blip/image_processing_blip.py
    
    Co-authored-by: NielsRogge <48327001+NielsRogge@users.noreply.github.com>
    
    * fix
    
    * remove `size_divisor`
    
    * fix weights `init`
    
    * remove unneeded functions
    
    * add suggestions
    
    * minor changes
    
    - change slow test output for PT 1.13
    - docstring order
    
    * replace `feature_extractor` by `image_processor`
    
    * fix doctests
    
    * fix weight init order + add fp16 slow test
    
    * add `blip` to doctest
    
    * add correct repo name and fix test
    
    * Update src/transformers/models/blip/processing_blip.py
    
    Co-authored-by: NielsRogge <48327001+NielsRogge@users.noreply.github.com>
    
    * fix tests
    
    * use `convert_to_rgb` from `image_transforms`
    
    * make fixup
    
    * fix large loading issue
    
    Co-authored-by: NielsRogge <48327001+NielsRogge@users.noreply.github.com>
    Co-authored-by: Sylvain Gugger <35901082+sgugger@users.noreply.github.com>

--- src/transformers/__init__.py
+++ src/transformers/__init__.py
@@ -169,6 +169,13 @@ _import_structure = {
         "BlenderbotSmallConfig",
         "BlenderbotSmallTokenizer",
     ],
+    "models.blip": [
+        "BLIP_PRETRAINED_CONFIG_ARCHIVE_MAP",
+        "BlipConfig",
+        "BlipProcessor",
+        "BlipTextConfig",
+        "BlipVisionConfig",
+    ],
     "models.bloom": ["BLOOM_PRETRAINED_CONFIG_ARCHIVE_MAP", "BloomConfig"],
     "models.bort": [],
     "models.byt5": ["ByT5Tokenizer"],
@@ -754,6 +761,7 @@ else:
     _import_structure["image_utils"] = ["ImageFeatureExtractionMixin"]
     _import_structure["models.beit"].extend(["BeitFeatureExtractor", "BeitImageProcessor"])
     _import_structure["models.bit"].extend(["BitImageProcessor"])
+    _import_structure["models.blip"].extend(["BlipImageProcessor"])
     _import_structure["models.chinese_clip"].extend(["ChineseCLIPFeatureExtractor", "ChineseCLIPImageProcessor"])
     _import_structure["models.clip"].extend(["CLIPFeatureExtractor", "CLIPImageProcessor"])
     _import_structure["models.conditional_detr"].extend(
@@ -1093,6 +1101,18 @@ else:
             "BlenderbotSmallForConditionalGeneration",
             "BlenderbotSmallModel",
             "BlenderbotSmallPreTrainedModel",
+        ]
+    )
+    _import_structure["models.blip"].extend(
+        [
+            "BLIP_PRETRAINED_MODEL_ARCHIVE_LIST",
+            "BlipForConditionalGeneration",
+            "BlipForImageTextRetrieval",
+            "BlipForQuestionAnswering",
+            "BlipModel",
+            "BlipPreTrainedModel",
+            "BlipTextModel",
+            "BlipVisionModel",
         ]
     )
     _import_structure["models.bloom"].extend(
@@ -3491,6 +3511,13 @@ if TYPE_CHECKING:
         BlenderbotSmallConfig,
         BlenderbotSmallTokenizer,
     )
+    from .models.blip import (
+        BLIP_PRETRAINED_CONFIG_ARCHIVE_MAP,
+        BlipConfig,
+        BlipProcessor,
+        BlipTextConfig,
+        BlipVisionConfig,
+    )
     from .models.bloom import BLOOM_PRETRAINED_CONFIG_ARCHIVE_MAP, BloomConfig
     from .models.byt5 import ByT5Tokenizer
     from .models.camembert import CAMEMBERT_PRETRAINED_CONFIG_ARCHIVE_MAP, CamembertConfig
@@ -4010,6 +4037,7 @@ if TYPE_CHECKING:
         from .image_utils import ImageFeatureExtractionMixin
         from .models.beit import BeitFeatureExtractor, BeitImageProcessor
         from .models.bit import BitImageProcessor
+        from .models.blip import BlipImageProcessor
         from .models.chinese_clip import ChineseCLIPFeatureExtractor, ChineseCLIPImageProcessor
         from .models.clip import CLIPFeatureExtractor, CLIPImageProcessor
         from .models.conditional_detr import ConditionalDetrFeatureExtractor, ConditionalDetrImageProcessor
@@ -4298,6 +4326,16 @@ if TYPE_CHECKING:
             BlenderbotSmallForConditionalGeneration,
             BlenderbotSmallModel,
             BlenderbotSmallPreTrainedModel,
+        )
+        from .models.blip import (
+            BLIP_PRETRAINED_MODEL_ARCHIVE_LIST,
+            BlipForConditionalGeneration,
+            BlipForImageTextRetrieval,
+            BlipForQuestionAnswering,
+            BlipModel,
+            BlipPreTrainedModel,
+            BlipTextModel,
+            BlipVisionModel,
         )
         from .models.bloom import (
             BLOOM_PRETRAINED_MODEL_ARCHIVE_LIST,
