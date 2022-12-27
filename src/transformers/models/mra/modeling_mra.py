67762687aff486b20287ea158d8036c7d224557c
novice <pranavpulijala@gmail.com>
2022-12-27 19:44:11 +0530

    Changes from code review

--- src/transformers/models/mra/modeling_mra.py
+++ src/transformers/models/mra/modeling_mra.py
@@ -51,15 +51,21 @@ MRA_PRETRAINED_MODEL_ARCHIVE_LIST = [
     # See all MRA models at https://huggingface.co/models?filter=mra
 ]
 
+def load_cuda_kernels():
+    try:
+        curr_path = os.path.dirname(os.path.realpath(__file__))
+        src_files = ['cuda_kernel.cu', 'cuda_launch.cu', 'torch_extension.cpp']
+        src_files = [os.path.join(curr_path, file) for file in src_files]
+        cuda_kernel = load('cuda_kernel', src_files, verbose = True)
 
-curr_path = os.path.dirname(os.path.realpath(__file__))
-src_files = ['cuda_kernel.cu', 'cuda_launch.cu', 'torch_extension.cpp']
-src_files = [os.path.join(curr_path, file) for file in src_files]
-cuda_kernel = load('cuda_kernel', src_files, verbose = True)
+        import cuda_kernel
+    except Exception:
+        print("Failed to load CUDA kernels. MRA requires custom CUDA kernels. Please verify that compatible versions of PyTorch and CUDA Toolkit are installed.")
 
-import cuda_kernel
-
-def sparse_max_cuda(sparse_C, indices, A_num_block, B_num_block):
+def sparse_max(sparse_C, indices, A_num_block, B_num_block):
+    """
+    Computes maximum values for softmax stability. 
+    """
     assert len(sparse_C.size()) == 4
     assert len(indices.size()) == 2
     assert sparse_C.size(2) == 32
@@ -76,13 +82,10 @@ def sparse_max_cuda(sparse_C, indices, A_num_block, B_num_block):
     
     return max_vals, max_vals_scatter
 
-def sparse_max(sparse_C, indices, A_num_block, B_num_block):
-    return sparse_max_cuda(sparse_C, indices, A_num_block, B_num_block)
-
-def sparse_mask_B(mask, indices, block_size = 32):
-    # mask = [batch_size, num_block * block_size]
-    # indices = [batch_size, num_block]
-    
+def sparse_mask_B(mask, indices, block_size = 32): 
+    """
+    Converts attention mask to a sparse mask for high resolution logits.
+    """  
     assert len(mask.size()) == 2
     assert len(indices.size()) == 2
     assert mask.shape[0] == indices.shape[0]
@@ -96,19 +99,17 @@ def sparse_mask_B(mask, indices, block_size = 32):
     
     return mask
 
-def sparse_to_dense(sparse_A, indices, A_num_block, block_size = 32):
-    batch_size, _, _, _ = sparse_A.shape
-    eyes = torch.eye(A_num_block * block_size, device = sparse_A.device).repeat(batch_size, 1, 1)
-    eyes = eyes.reshape(batch_size, A_num_block, block_size, A_num_block * block_size).transpose(-1, -2)
-    dense_A = sparse_dense_mm_torch(sparse_A, indices, eyes, A_num_block)
-    dense_A = dense_A.transpose(-1, -2).reshape(batch_size, A_num_block * block_size, A_num_block * block_size)
-    return dense_A
+def mm_to_sparse(dense_A, dense_B, indices, block_size = 32):
+    """
+    Performs Sampled Dense Matrix Multiplication.
+    """
+    batch_size, A_size, dim = dense_A.size()
+    _, B_size, dim = dense_B.size()
+    assert A_size % block_size == 0
+    assert B_size % block_size == 0
 
-def mm_to_sparse_cuda(dense_A, dense_B, indices):
-
-    # dense_A = [batch_size, A_num_block, dim, 32]
-    # dense_B = [batch_size, B_num_block, dim, 32]
-    # indices = [batch_size, num_block]
+    dense_A = dense_A.reshape(batch_size, A_size // block_size, block_size, dim).transpose(-1, -2)
+    dense_B = dense_B.reshape(batch_size, B_size // block_size, block_size, dim).transpose(-1, -2)
 
     assert len(dense_A.size()) == 4
     assert len(dense_B.size()) == 4
@@ -132,44 +133,17 @@ def mm_to_sparse_cuda(dense_A, dense_B, indices):
 
     return cuda_kernel.mm_to_sparse(dense_A, dense_B, indices.int())
 
-def mm_to_sparse_torch(dense_A, dense_B, indices, block_size = 32):
+def sparse_dense_mm(sparse_A, indices, dense_B, A_num_block, block_size = 32):
+    """
+    Performs matrix multiplication of a sparse matrix with a dense matrix. 
+    """
+    batch_size, B_size, dim = dense_B.size()
 
-    # dense_A = [batch_size, A_num_block, dim, block_size]
-    # dense_B = [batch_size, B_num_block, dim, block_size]
-    # indices = [batch_size, num_block]
-
-    assert len(dense_A.size()) == 4
-    assert len(dense_B.size()) == 4
-    assert len(indices.size()) == 2
-
-    _, B_num_block, _, _ = dense_B.size()
-
-    batch_idx = torch.arange(indices.size(0), dtype = torch.long, device = indices.device)
-    dense_A = dense_A[batch_idx[:, None], torch.div(indices, B_num_block, rounding_mode = 'floor').long(), :, :]
-    dense_B = dense_B[batch_idx[:, None], (indices % B_num_block).long(), :, :]
-
-    sparse_C = torch.matmul(dense_B.transpose(-1, -2), dense_A)
-
-    return sparse_C
-
-def mm_to_sparse(dense_A, dense_B, indices, block_size = 32, cuda = True):
-    batch_size, A_size, dim = dense_A.size()
-    _, B_size, dim = dense_B.size()
-    assert A_size % block_size == 0
     assert B_size % block_size == 0
+    assert sparse_A.size(2) == block_size
+    assert sparse_A.size(3) == block_size
 
-    dense_A = dense_A.reshape(batch_size, A_size // block_size, block_size, dim).transpose(-1, -2)
     dense_B = dense_B.reshape(batch_size, B_size // block_size, block_size, dim).transpose(-1, -2)
-    if block_size == 32 and dim % 8 == 0 and cuda:
-        return mm_to_sparse_cuda(dense_A, dense_B, indices)
-    else:
-        return mm_to_sparse_torch(dense_A, dense_B, indices)
-
-def sparse_dense_mm_cuda(sparse_A, indices, dense_B, A_num_block):
-
-    # sparse_A = [batch_size, num_block, 32, 32]
-    # indices = [batch_size, num_block]
-    # dense_B = [batch_size, B_num_block, dim, 32]
 
     assert len(sparse_A.size()) == 4
     assert len(dense_B.size()) == 4
@@ -192,99 +166,9 @@ def sparse_dense_mm_cuda(sparse_A, indices, dense_B, A_num_block):
     assert indices.is_contiguous()
     assert dense_B.is_contiguous()
 
-    return cuda_kernel.sparse_dense_mm(sparse_A, indices, dense_B, A_num_block)
-
-def sparse_dense_mm_torch(sparse_A, indices, dense_B, A_num_block):
-
-    # sparse_A = [batch_size, num_block, block_size, block_size]
-    # indices = [batch_size, num_block]
-    # dense_B = [batch_size, B_num_block, dim, block_size]
-
-    assert len(sparse_A.size()) == 4
-    assert len(dense_B.size()) == 4
-    assert len(indices.size()) == 2
-
-    batch_size, num_block = indices.size()
-    _, B_num_block, dim, block_size = dense_B.size()
-
-    batch_idx = torch.arange(indices.size(0), dtype = torch.long, device = indices.device)
-    dense_B = dense_B[batch_idx[:, None], (indices % B_num_block).long(), :, :]
-
-    dense_C = torch.matmul(dense_B, sparse_A)
-    dense_C = dense_C.reshape(batch_size * num_block, dim * block_size)
-    global_idxes = (torch.div(indices, B_num_block, rounding_mode = 'floor').long() + batch_idx[:, None] * A_num_block).reshape(batch_size * num_block)
-    temp = torch.zeros((batch_size * A_num_block, dim * block_size), dtype = dense_C.dtype, device = dense_C.device)
-    dense_C = temp.index_add(0, global_idxes, dense_C).reshape(batch_size, A_num_block, dim, block_size)
-
-    return dense_C
-
-def sparse_dense_mm(sparse_A, indices, dense_B, A_num_block, block_size = 32, cuda = True):
-    batch_size, B_size, dim = dense_B.size()
-
-    assert B_size % block_size == 0
-    assert sparse_A.size(2) == block_size
-    assert sparse_A.size(3) == block_size
-
-    dense_B = dense_B.reshape(batch_size, B_size // block_size, block_size, dim).transpose(-1, -2)
-    if block_size == 32 and dim % 64 == 0 and cuda:
-        dense_C = sparse_dense_mm_cuda(sparse_A, indices, dense_B, A_num_block)
-    else:
-        dense_C = sparse_dense_mm_torch(sparse_A, indices, dense_B, A_num_block)
+    dense_C = cuda_kernel.sparse_dense_mm(sparse_A, indices, dense_B, A_num_block)
     dense_C = dense_C.transpose(-1, -2).reshape(batch_size, A_num_block * block_size, dim)
     return dense_C
-
-def reduce_sum_cuda(sparse_A, indices, A_num_block, B_num_block):
-
-    # sparse_A = [batch_size, num_block, 32, 32]
-    # indices = [batch_size, num_block]
-
-    assert len(sparse_A.size()) == 4
-    assert len(indices.size()) == 2
-    assert sparse_A.size(2) == 32
-    assert sparse_A.size(3) == 32
-
-    if not sparse_A.is_contiguous():
-        sparse_A = sparse_A.contiguous()
-
-    indices = indices.int()
-    if not indices.is_contiguous():
-        indices = indices.contiguous()
-
-    assert sparse_A.is_contiguous()
-    assert indices.is_contiguous()
-
-    return cuda_kernel.reduce_sum(sparse_A, indices, A_num_block, B_num_block)
-
-def reduce_sum_torch(sparse_A, indices, A_num_block, B_num_block):
-
-    # sparse_A = [batch_size, num_block, block_size, block_size]
-    # indices = [batch_size, num_block]
-
-    assert len(sparse_A.size()) == 4
-    assert len(indices.size()) == 2
-
-    _, _, block_size, _ = sparse_A.size()
-    batch_size, num_block = indices.size()
-
-    sparse_A = sparse_A.sum(dim = 2).reshape(batch_size * num_block, block_size)
-
-    batch_idx = torch.arange(indices.size(0), dtype = torch.long, device = indices.device)
-    global_idxes = (torch.div(indices, B_num_block, rounding_mode = 'floor').long() + batch_idx[:, None] * A_num_block).reshape(batch_size * num_block)
-    temp = torch.zeros((batch_size * A_num_block, block_size), dtype = sparse_A.dtype, device = sparse_A.device)
-    output = temp.index_add(0, global_idxes, sparse_A).reshape(batch_size, A_num_block, block_size)
-
-    return output
-
-def reduce_sum(sparse_A, indices, A_num_block, B_num_block, cuda = False):
-    batch_size, num_block, block_size, _ = sparse_A.size()
-
-    # if block_size == 32 and cuda:
-    #     output = reduce_sum_cuda(sparse_A, indices, A_num_block, B_num_block)
-    # else:
-    #     output = reduce_sum_torch(sparse_A, indices, A_num_block, B_num_block)
-    output = reduce_sum_torch(sparse_A, indices, A_num_block, B_num_block)
-    output = output.reshape(batch_size, A_num_block * block_size)
-    return output
 
 def transpose_indices(indices, dim_1_block, dim_2_block):
     return ((indices % dim_2_block) * dim_1_block + torch.div(indices, dim_2_block, rounding_mode='floor')).long()
@@ -337,9 +221,28 @@ class SparseDenseMM(torch.autograd.Function):
 class ReduceSum():
     @staticmethod
     def operator_call(sparse_A, indices, A_num_block, B_num_block):
-        return reduce_sum(sparse_A, indices, A_num_block, B_num_block)
+        batch_size, num_block, block_size, _ = sparse_A.size()
+
+        assert len(sparse_A.size()) == 4
+        assert len(indices.size()) == 2
+
+        _, _, block_size, _ = sparse_A.size()
+        batch_size, num_block = indices.size()
+
+        sparse_A = sparse_A.sum(dim = 2).reshape(batch_size * num_block, block_size)
+
+        batch_idx = torch.arange(indices.size(0), dtype = torch.long, device = indices.device)
+        global_idxes = (torch.div(indices, B_num_block, rounding_mode = 'floor').long() + batch_idx[:, None] * A_num_block).reshape(batch_size * num_block)
+        temp = torch.zeros((batch_size * A_num_block, block_size), dtype = sparse_A.dtype, device = sparse_A.device)
+        output = temp.index_add(0, global_idxes, sparse_A).reshape(batch_size, A_num_block, block_size)
+
+        output = output.reshape(batch_size, A_num_block * block_size)
+        return output
 
 def get_low_resolution_logit(Q, K, block_size, mask = None, V = None):
+    """
+    Compute low resolution approximation. 
+    """
     batch_size, seq_len, head_dim = Q.size()
 
     num_block_per_row = seq_len // block_size
@@ -368,6 +271,9 @@ def get_low_resolution_logit(Q, K, block_size, mask = None, V = None):
     return low_resolution_logit, token_count, low_resolution_logit_row_max, V_hat
 
 def get_block_idxes(low_resolution_logit, num_blocks, approx_mode, initial_prior_first_n_blocks, initial_prior_diagonal_n_blocks):
+    """
+    Compute the indices of the subset of components to be used in the approximation. 
+    """
     batch_size, total_blocks_per_row, _ = low_resolution_logit.shape
 
     if initial_prior_diagonal_n_blocks > 0:
@@ -400,7 +306,9 @@ def mra2_attention(
     initial_prior_first_n_blocks = 0,
     initial_prior_diagonal_n_blocks = 0
 ):
-
+    """
+    Use MRA to approximate self-attention. 
+    """
     batch_size, num_head, seq_len, head_dim = Q.size()
     meta_batch = batch_size * num_head
 
@@ -423,7 +331,7 @@ def mra2_attention(
         with torch.no_grad():
             low_resolution_logit, token_count, low_resolution_logit_row_max, _ = get_low_resolution_logit(Q, K, block_size, mask)
     else:
-        raise Exception()
+        raise Exception("config.approx_mode must be \"full\" or \"sparse\"")
 
     with torch.no_grad():
         low_resolution_logit_normalized = low_resolution_logit - low_resolution_logit_row_max
@@ -461,7 +369,7 @@ def mra2_attention(
     elif approx_mode == "sparse":
         attn = high_resolution_attn_out / (high_resolution_normalizer[:, :, None] + 1e-6)
     else:
-        raise Exception()
+        raise Exception("config.approx_mode must be \"full\" or \"sparse\"")
 
     if mask is not None:
         attn = attn * mask[:, :, None]
@@ -471,7 +379,6 @@ def mra2_attention(
     return attn
 
 
-# Copied from transformers.models.nystromformer.modeling_nystromformer.NystromformerEmbeddings
 class MRAEmbeddings(nn.Module):
     """Construct the embeddings from word, position and token_type embeddings."""
 
@@ -531,7 +438,6 @@ class MRAEmbeddings(nn.Module):
         return embeddings
 
 
-# Copied from transformers.models.yoso.modeling_yoso.YosoSelfAttention with Yoso->MRA
 class MRASelfAttention(nn.Module):
     def __init__(self, config, position_embedding_type=None):
         super().__init__()
@@ -946,6 +852,7 @@ MRA_INPUTS_DOCSTRING = r"""
 class MRAModel(MRAPreTrainedModel):
     def __init__(self, config):
         super().__init__(config)
+        load_cuda_kernels()
         self.config = config
 
         self.embeddings = MRAEmbeddings(config)
