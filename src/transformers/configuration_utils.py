0fe8ad98b36d41c14944eefd50880d9c979caea4
Matthew McDermott <mattmcdermott8@gmail.com>
2022-12-30 02:35:01 -0500

    Adds type checking to PreTrainedConfig. (#20926)

--- src/transformers/configuration_utils.py
+++ src/transformers/configuration_utils.py
@@ -732,7 +732,7 @@ class PretrainedConfig(PushToHubMixin):
         return json.loads(text)
 
     def __eq__(self, other):
-        return self.__dict__ == other.__dict__
+        return isinstance(other, PretrainedConfig) and (self.__dict__ == other.__dict__)
 
     def __repr__(self):
         return f"{self.__class__.__name__} {self.to_json_string()}"
