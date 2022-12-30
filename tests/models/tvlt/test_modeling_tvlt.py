c98be43a28df5511281e933c8f4e1396f136c811
Zineng Tang <82334223+zinengtang@users.noreply.github.com>
2022-12-30 10:09:42 -0500

    Update test_modeling_tvlt.py

--- tests/models/tvlt/test_modeling_tvlt.py
+++ tests/models/tvlt/test_modeling_tvlt.py
@@ -650,7 +650,7 @@ class TvltModelIntegrationTest(unittest.TestCase):
         # verify the logits
         expected_pixel_logits_shape = torch.Size([1, 1568, 768])
         expected_audio_logits_shape = torch.Size([1, 96, 256])
-        expected_matching_logits = torch.tensor([[-0.7540]], device=torch_device)
+        expected_matching_logits = torch.tensor([[-0.7539]], device=torch_device)
 
         self.assertEqual(outputs.pixel_logits.shape, expected_pixel_logits_shape)
         self.assertEqual(outputs.audio_logits.shape, expected_audio_logits_shape)
