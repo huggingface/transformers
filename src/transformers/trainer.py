cc888daa5ba2bd933842b8c6788693593a9fb0cb
radcheb <radcheb@users.noreply.github.com>
2023-01-03 21:16:12 +0100

    Fix race condition on cleaning checkpoints when save_total_limit set to 1 (#20989)
    * Update trainer.py
    
    * fix style
    
    Co-authored-by: Radhwane Chebaane <rchebaane.external@epo.org>

--- src/transformers/trainer.py
+++ src/transformers/trainer.py
@@ -1919,8 +1919,8 @@ class Trainer:
         run_dir = self._get_output_dir(trial)
         checkpoints_sorted = self._sorted_checkpoints(use_mtime=False, output_dir=run_dir)
 
-        # Delete the last checkpoint when save_total_limit=1 if it's different from the best checkpoint.
-        if self.state.best_model_checkpoint is not None and self.args.save_total_limit == 1:
+        # Delete the last checkpoint when save_total_limit=1 if it's different from the best checkpoint and process allowed to save.
+        if self.args.should_save and self.state.best_model_checkpoint is not None and self.args.save_total_limit == 1:
             for checkpoint in checkpoints_sorted:
                 if checkpoint != self.state.best_model_checkpoint:
                     logger.info(f"Deleting older checkpoint [{checkpoint}] due to args.save_total_limit")
