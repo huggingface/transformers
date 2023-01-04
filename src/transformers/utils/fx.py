efe05e33ac1aa3f65aa32f7b6ada7fd436321ff2
Jongjyh <37979232+jongjyh@users.noreply.github.com>
2023-01-04 16:18:57 +0800

    Add AltCLIP (#20446)
    * add altclip
    
    * update
    
    * fix wrong title
    
    * fix the copyright in readme
    
    * add altclip model
    
    * add altclip
    
    * fix test_gradient_checkpointing_enable_disable
    
    * code
    
    * add return class
    
    * add projection_state
    
    * "fix pretrained model bug"
    
    * delete print and fix 2 test instances.
    
    * delete token
    
    * rm xlmr
    
    * one model one file.
    
    * empty commit to trigger CI
    
    * Fix modeling_outputs.py
    
    * Fix __init__
    
    * Fix quality
    
    * Fix modeling file docstring
    
    * Fix README.md
    
    * Fix test file
    
    * add vision model
    
    * empty commit to trigger CI
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * del token in mdx file
    
    * fix
    
    * fix
    
    * fix
    
    * remove altrob from test list
    
    * add vision test
    
    * fix fx
    
    * fix
    
    * fix
    
    * fix
    
    * trigger CI
    
    * fix copies
    
    * fix tests
    
    * fix style
    
    * fix quality
    
    * update
    
    * recover import
    
    * recover
    
    * add ,
    
    * recover
    
    * fix copies
    
    * trigger CI
    
    * fix
    
    * some of review
    
    * update
    
    * remove import
    
    * last 2
    
    * fix
    
    * fix style
    
    * fix style
    
    * fix bug
    
    * fix uncomment
    
    * fix
    
    * update
    
    * fix
    
    * second review
    
    * empty commit to trigger CI
    
    * empty commit to trigger CI
    
    * fix position
    
    * fix
    
    * empty commit to trigger CI
    
    * empty commit to trigger CI
    
    * third comment
    
    * Update docs/source/en/model_doc/altclip.mdx
    
    Co-authored-by: Yih-Dar <2521628+ydshieh@users.noreply.github.com>
    
    * Update docs/source/en/model_doc/altclip.mdx
    
    Co-authored-by: Yih-Dar <2521628+ydshieh@users.noreply.github.com>
    
    * Update src/transformers/__init__.py
    
    Co-authored-by: Yih-Dar <2521628+ydshieh@users.noreply.github.com>
    
    * Update src/transformers/models/altclip/configuration_altclip.py
    
    Co-authored-by: Yih-Dar <2521628+ydshieh@users.noreply.github.com>
    
    * Update src/transformers/models/altclip/modeling_altclip.py
    
    Co-authored-by: Yih-Dar <2521628+ydshieh@users.noreply.github.com>
    
    * Update src/transformers/models/altclip/processing_altclip.py
    
    Co-authored-by: Yih-Dar <2521628+ydshieh@users.noreply.github.com>
    
    * Update src/transformers/models/altclip/modeling_altclip.py
    
    Co-authored-by: Yih-Dar <2521628+ydshieh@users.noreply.github.com>
    
    * fix merge
    
    * fix copies
    
    * update
    
    * update
    
    * empty commit to trigger CI
    
    * fix code example
    
    * empty commit to trigger CI
    
    * fix
    
    * empty commit to trigger CI
    
    * empty commit to trigger CI
    
    Co-authored-by: shunxing1234 <xw747777271@gmail.com>
    Co-authored-by: ydshieh <ydshieh@users.noreply.github.com>
    Co-authored-by: shunxing1234 <33774367+shunxing1234@users.noreply.github.com>
    Co-authored-by: Yih-Dar <2521628+ydshieh@users.noreply.github.com>

--- src/transformers/utils/fx.py
+++ src/transformers/utils/fx.py
@@ -101,6 +101,7 @@ def _generate_supported_model_class_names(
 
 
 _REGULAR_SUPPORTED_MODEL_NAMES_AND_TASKS = [
+    "altclip",
     "albert",
     "bart",
     "bert",
@@ -156,6 +157,8 @@ _SPECIAL_SUPPORTED_MODELS = [
     "CLIPTextModelWithProjection",
     "CLIPVisionModel",
     "CLIPVisionModelWithProjection",
+    "AltCLIPTextModel",
+    "AltCLIPVisionModel",
     "GitVisionModel",
     "GPT2DoubleHeadsModel",
     "Speech2Text2Decoder",
