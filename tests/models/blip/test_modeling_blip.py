b7cd172f1043c684bbb381bbc93a3a3e048f73f8
younesbelkada <younesbelkada@gmail.com>
2023-01-04 19:46:07 +0000

    cleanup

--- tests/models/blip/test_modeling_blip.py
+++ tests/models/blip/test_modeling_blip.py
@@ -847,7 +847,6 @@ class BlipModelIntegrationTest(unittest.TestCase):
 
         image = prepare_img()
         text = "A woman and her dog sitting in a beach"
-        # model.eval()
 
         inputs = processor(image, text, return_tensors="pt").to(torch_device)
 
