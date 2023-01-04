ce85686a1f425c8e60d9104522d8626395dd507d
Jongjyh <37979232+jongjyh@users.noreply.github.com>
2023-01-04 16:18:57 +0800

    Add AltCLIP (#20446)
    * add altclip
    
    * update
    
    * fix wrong title
    
    * fix the copyright in readme
    
    * add altclip model
    
    * add altclip
    
    * fix test_gradient_checkpointing_enable_disable
    
    * code
    
    * add return class
    
    * add projection_state
    
    * "fix pretrained model bug"
    
    * delete print and fix 2 test instances.
    
    * delete token
    
    * rm xlmr
    
    * one model one file.
    
    * empty commit to trigger CI
    
    * Fix modeling_outputs.py
    
    * Fix __init__
    
    * Fix quality
    
    * Fix modeling file docstring
    
    * Fix README.md
    
    * Fix test file
    
    * add vision model
    
    * empty commit to trigger CI
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * del token in mdx file
    
    * fix
    
    * fix
    
    * fix
    
    * remove altrob from test list
    
    * add vision test
    
    * fix fx
    
    * fix
    
    * fix
    
    * fix
    
    * trigger CI
    
    * fix copies
    
    * fix tests
    
    * fix style
    
    * fix quality
    
    * update
    
    * recover import
    
    * recover
    
    * add ,
    
    * recover
    
    * fix copies
    
    * trigger CI
    
    * fix
    
    * some of review
    
    * update
    
    * remove import
    
    * last 2
    
    * fix
    
    * fix style
    
    * fix style
    
    * fix bug
    
    * fix uncomment
    
    * fix
    
    * update
    
    * fix
    
    * second review
    
    * empty commit to trigger CI
    
    * empty commit to trigger CI
    
    * fix position
    
    * fix
    
    * empty commit to trigger CI
    
    * empty commit to trigger CI
    
    * third comment
    
    * Update docs/source/en/model_doc/altclip.mdx
    
    Co-authored-by: Yih-Dar <2521628+ydshieh@users.noreply.github.com>
    
    * Update docs/source/en/model_doc/altclip.mdx
    
    Co-authored-by: Yih-Dar <2521628+ydshieh@users.noreply.github.com>
    
    * Update src/transformers/__init__.py
    
    Co-authored-by: Yih-Dar <2521628+ydshieh@users.noreply.github.com>
    
    * Update src/transformers/models/altclip/configuration_altclip.py
    
    Co-authored-by: Yih-Dar <2521628+ydshieh@users.noreply.github.com>
    
    * Update src/transformers/models/altclip/modeling_altclip.py
    
    Co-authored-by: Yih-Dar <2521628+ydshieh@users.noreply.github.com>
    
    * Update src/transformers/models/altclip/processing_altclip.py
    
    Co-authored-by: Yih-Dar <2521628+ydshieh@users.noreply.github.com>
    
    * Update src/transformers/models/altclip/modeling_altclip.py
    
    Co-authored-by: Yih-Dar <2521628+ydshieh@users.noreply.github.com>
    
    * fix merge
    
    * fix copies
    
    * update
    
    * update
    
    * empty commit to trigger CI
    
    * fix code example
    
    * empty commit to trigger CI
    
    * fix
    
    * empty commit to trigger CI
    
    * empty commit to trigger CI
    
    Co-authored-by: shunxing1234 <xw747777271@gmail.com>
    Co-authored-by: ydshieh <ydshieh@users.noreply.github.com>
    Co-authored-by: shunxing1234 <33774367+shunxing1234@users.noreply.github.com>
    Co-authored-by: Yih-Dar <2521628+ydshieh@users.noreply.github.com>

--- src/transformers/__init__.py
+++ src/transformers/__init__.py
@@ -124,6 +124,13 @@ _import_structure = {
     "models": [],
     # Models
     "models.albert": ["ALBERT_PRETRAINED_CONFIG_ARCHIVE_MAP", "AlbertConfig"],
+    "models.altclip": [
+        "ALTCLIP_PRETRAINED_CONFIG_ARCHIVE_MAP",
+        "AltCLIPConfig",
+        "AltCLIPProcessor",
+        "AltCLIPTextConfig",
+        "AltCLIPVisionConfig",
+    ],
     "models.audio_spectrogram_transformer": [
         "AUDIO_SPECTROGRAM_TRANSFORMER_PRETRAINED_CONFIG_ARCHIVE_MAP",
         "ASTConfig",
@@ -921,7 +928,15 @@ else:
             "load_tf_weights_in_albert",
         ]
     )
-
+    _import_structure["models.altclip"].extend(
+        [
+            "ALTCLIP_PRETRAINED_MODEL_ARCHIVE_LIST",
+            "AltCLIPModel",
+            "AltCLIPPreTrainedModel",
+            "AltCLIPTextModel",
+            "AltCLIPVisionModel",
+        ]
+    )
     _import_structure["models.audio_spectrogram_transformer"].extend(
         [
             "AUDIO_SPECTROGRAM_TRANSFORMER_PRETRAINED_MODEL_ARCHIVE_LIST",
@@ -3482,6 +3497,13 @@ if TYPE_CHECKING:
         load_tf2_weights_in_pytorch_model,
     )
     from .models.albert import ALBERT_PRETRAINED_CONFIG_ARCHIVE_MAP, AlbertConfig
+    from .models.altclip import (
+        ALTCLIP_PRETRAINED_CONFIG_ARCHIVE_MAP,
+        AltCLIPConfig,
+        AltCLIPProcessor,
+        AltCLIPTextConfig,
+        AltCLIPVisionConfig,
+    )
     from .models.audio_spectrogram_transformer import (
         AUDIO_SPECTROGRAM_TRANSFORMER_PRETRAINED_CONFIG_ARCHIVE_MAP,
         ASTConfig,
@@ -4180,6 +4202,13 @@ if TYPE_CHECKING:
             AlbertModel,
             AlbertPreTrainedModel,
             load_tf_weights_in_albert,
+        )
+        from .models.altclip import (
+            ALTCLIP_PRETRAINED_MODEL_ARCHIVE_LIST,
+            AltCLIPModel,
+            AltCLIPPreTrainedModel,
+            AltCLIPTextModel,
+            AltCLIPVisionModel,
         )
         from .models.audio_spectrogram_transformer import (
             AUDIO_SPECTROGRAM_TRANSFORMER_PRETRAINED_MODEL_ARCHIVE_LIST,
--- src/transformers/models/auto/modeling_auto.py
+++ src/transformers/models/auto/modeling_auto.py
@@ -29,6 +29,7 @@ MODEL_MAPPING_NAMES = OrderedDict(
     [
         # Base model mapping
         ("albert", "AlbertModel"),
+        ("altclip", "AltCLIPModel"),
         ("audio-spectrogram-transformer", "ASTModel"),
         ("bart", "BartModel"),
         ("beit", "BeitModel"),
@@ -878,6 +879,7 @@ MODEL_FOR_AUDIO_XVECTOR_MAPPING_NAMES = OrderedDict(
 _MODEL_FOR_ZERO_SHOT_IMAGE_CLASSIFICATION_MAPPING_NAMES = OrderedDict(
     [
         # Model for Zero Shot Image Classification mapping
+        ("altclip", "AltCLIPModel"),
         ("blip", "BlipModel"),
         ("chinese_clip", "ChineseCLIPModel"),
         ("clip", "CLIPModel"),
