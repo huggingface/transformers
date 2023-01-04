8c1b4bee0b387a14238abb0c9525f07987715dc8
Sylvain Gugger <Sylvain.gugger@gmail.com>
2023-01-04 14:00:45 -0500

    Fix repo consistency

--- src/transformers/models/mt5/modeling_mt5.py
+++ src/transformers/models/mt5/modeling_mt5.py
@@ -862,7 +862,7 @@ class MT5Stack(MT5PreTrainedModel):
         # Set final layer norm to last device
         self.final_layer_norm = self.final_layer_norm.to(self.last_device)
 
-    @add_start_docstrings(PARALLELIZE_DOCSTRING)
+    @add_start_docstrings(DEPARALLELIZE_DOCSTRING)
     def deparallelize(self):
         self.model_parallel = False
         self.device_map = None
