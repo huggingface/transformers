53f686742293166ad218769748e5536c510aa08f
Vision <gitesh.ch.0912@gmail.com>
2023-01-11 12:13:51 +0530

    GRN

--- src/transformers/models/convnextv2/modeling_convnextv2.py
+++ src/transformers/models/convnextv2/modeling_convnextv2.py
@@ -146,6 +146,20 @@ class ConvNeXtV2Embeddings(nn.Module):
         return embeddings
 
 
+class GRN(nn.Module):
+    """ GRN (Global Response Normalization) layer
+    """
+    def __init__(self, dim):
+        super().__init__()
+        self.gamma = nn.Parameter(torch.zeros(1, 1, 1, dim))
+        self.beta = nn.Parameter(torch.zeros(1, 1, 1, dim))
+
+    def forward(self, x):
+        Gx = torch.norm(x, p=2, dim=(1,2), keepdim=True)
+        Nx = Gx / (Gx.mean(dim=-1, keepdim=True) + 1e-6)
+        return self.gamma * (x * Nx) + self.beta + x
+
+
 # Copied from transformers.models.convnext.modeling_convnext.ConvNextLayer with ConvNext->ConvNeXtV2
 class ConvNeXtV2Layer(nn.Module):
     """This corresponds to the `Block` class in the original implementation.
