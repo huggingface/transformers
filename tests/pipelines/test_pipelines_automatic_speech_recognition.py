7abab3c006ae15593f658c9a1fa9e9f2324ccb47
Arthur Zucker <arthur.zucker@gmail.com>
2023-01-13 17:52:41 +0000

    format sequences

--- tests/pipelines/test_pipelines_automatic_speech_recognition.py
+++ tests/pipelines/test_pipelines_automatic_speech_recognition.py
@@ -321,44 +321,11 @@ class AutomaticSpeechRecognitionPipelineTests(unittest.TestCase, metaclass=Pipel
         )
 
         # Merge when the previous sequence is a suffix of the next sequence
+        #fmt: off
         next_sequences_1 = [
-            [
-                50364,
-                295,
-                6177,
-                3391,
-                11,
-                19817,
-                3337,
-                507,
-                307,
-                406,
-                3163,
-                1953,
-                466,
-                13,
-                50614,
-                50614,
-                2812,
-                9836,
-                14783,
-                390,
-                6263,
-                538,
-                257,
-                1359,
-                11,
-                8199,
-                6327,
-                1090,
-                322,
-                702,
-                7443,
-                13,
-                50834,
-                50257,
-            ]
+            [50364, 295, 6177, 3391, 11, 19817, 3337, 507, 307, 406, 3163, 1953, 466, 13, 50614, 50614, 2812, 9836, 14783, 390, 6263, 538, 257, 1359, 11, 8199, 6327, 1090, 322, 702, 7443, 13, 50834, 50257]
         ]
+        #fmt: on
         self.assertEqual(
             processor.decode(next_sequences_1[0], output_offsets=True),
             {
@@ -384,36 +351,12 @@ class AutomaticSpeechRecognitionPipelineTests(unittest.TestCase, metaclass=Pipel
             max_source_positions,
         )
 
+        #fmt: off
         self.assertEqual(
             merge,
-            [
-                51492,
-                406,
-                3163,
-                1953,
-                466,
-                13,
-                51612,
-                51612,
-                2812,
-                9836,
-                14783,
-                390,
-                6263,
-                538,
-                257,
-                1359,
-                11,
-                8199,
-                6327,
-                1090,
-                322,
-                702,
-                7443,
-                13,
-                51832,
-            ],
+            [51492, 406, 3163, 1953, 466, 13, 51612, 51612, 2812, 9836, 14783, 390, 6263, 538, 257, 1359, 11, 8199, 6327, 1090, 322, 702, 7443, 13, 51832],
         )
+        #fmt: on
         self.assertEqual(
             processor.decode(merge, output_offsets=True),
             {
@@ -432,42 +375,11 @@ class AutomaticSpeechRecognitionPipelineTests(unittest.TestCase, metaclass=Pipel
         )
 
         # Merge when the sequence is in the middle of the 1st next sequence
+        #fmt: off
         next_sequences_2 = [
-            [
-                50364,
-                295,
-                6177,
-                3391,
-                11,
-                19817,
-                3337,
-                507,
-                307,
-                406,
-                3163,
-                1953,
-                466,
-                13,
-                2812,
-                9836,
-                14783,
-                390,
-                6263,
-                538,
-                257,
-                1359,
-                11,
-                8199,
-                6327,
-                1090,
-                322,
-                702,
-                7443,
-                13,
-                50834,
-                50257,
-            ]
+            [50364, 295, 6177, 3391, 11, 19817, 3337, 507, 307, 406, 3163, 1953, 466, 13, 2812, 9836, 14783, 390, 6263, 538, 257, 1359, 11, 8199, 6327, 1090, 322, 702, 7443, 13, 50834, 50257]
         ]
+        #fmt: on
         # {'text': ' of spectators, retrievality is not worth thinking about. His instant panic was followed by a small, sharp blow high on his chest.','timestamp': (0.0, 9.4)}
         merge = _find_timestamp_sequence(
             [[previous_sequence, (3000, 0, 0)], [next_sequences_2, (3000, 0, 0)]],
@@ -475,36 +387,12 @@ class AutomaticSpeechRecognitionPipelineTests(unittest.TestCase, metaclass=Pipel
             processor.feature_extractor,
             max_source_positions,
         )
+        #fmt: off
         self.assertEqual(
             merge,
-            [
-                51492,
-                406,
-                3163,
-                1953,
-                466,
-                13,
-                51612,
-                51612,
-                2812,
-                9836,
-                14783,
-                390,
-                6263,
-                538,
-                257,
-                1359,
-                11,
-                8199,
-                6327,
-                1090,
-                322,
-                702,
-                7443,
-                13,
-                51832,
-            ],
+            [51492, 406, 3163, 1953, 466, 13, 51612, 51612, 2812, 9836, 14783, 390, 6263, 538, 257, 1359, 11, 8199, 6327, 1090, 322, 702, 7443, 13, 51832],
         )
+        #fmt: on
         self.assertEqual(
             processor.decode(merge, output_offsets=True),
             {
@@ -523,64 +411,22 @@ class AutomaticSpeechRecognitionPipelineTests(unittest.TestCase, metaclass=Pipel
         )
 
         # Merge when the previous sequence is not included in the current sequence
-        next_sequences_3 = [
-            50364,
-            2812,
-            9836,
-            14783,
-            390,
-            6263,
-            538,
-            257,
-            1359,
-            11,
-            8199,
-            6327,
-            1090,
-            322,
-            702,
-            7443,
-            13,
-            50834,
-            50257,
-        ]
+        #fmt: off
+        next_sequences_3 = [50364, 2812, 9836,14783, 390, 6263, 538, 257, 1359, 11, 8199, 6327, 1090, 322, 702, 7443, 13, 50834, 50257]
+        #fmt: on
         # {'text': ' His instant panic was followed by a small, sharp blow high on his chest.','timestamp': (0.0, 9.4)}
         merge = _find_timestamp_sequence(
-            [[previous_sequence, (3000, 0, 0)], [next_sequences_2, (3000, 0, 0)]],
+            [[previous_sequence, (3000, 0, 0)], [next_sequences_3, (3000, 0, 0)]],
             processor.tokenizer,
             processor.feature_extractor,
             max_source_positions,
         )
+        #fmt: off
         self.assertEqual(
             merge,
-            [
-                51492,
-                406,
-                3163,
-                1953,
-                466,
-                13,
-                51612,
-                51612,
-                2812,
-                9836,
-                14783,
-                390,
-                6263,
-                538,
-                257,
-                1359,
-                11,
-                8199,
-                6327,
-                1090,
-                322,
-                702,
-                7443,
-                13,
-                51832,
-            ],
+            [51492, 406, 3163, 1953, 466, 13, 51612, 51612, 2812, 9836, 14783, 390, 6263, 538, 257, 1359, 11, 8199, 6327, 1090, 322, 702, 7443, 13, 51832],
         )
+        #fmt: on
         self.assertEqual(
             processor.decode(merge, output_offsets=True),
             {
