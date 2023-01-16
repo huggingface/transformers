e5cb2879b480192b9deb32f8df83d1daf448c067
Susnato Dhar <susnatodhar10@gmail.com>
2023-01-16 19:36:35 +0530

    Fixed issue #21053 (#21065)
    Co-authored-by: susnato <susnato@tensorflow123456@gmail.com>

--- src/transformers/models/gpt2/modeling_tf_gpt2.py
+++ src/transformers/models/gpt2/modeling_tf_gpt2.py
@@ -446,7 +446,7 @@ class TFGPT2MainLayer(tf.keras.layers.Layer):
             # indices on GPU, returning zeros instead. This is a dangerous silent behavior.
             tf.debugging.assert_less(
                 input_ids,
-                tf.cast(self.vocab_size, dtype=input_ids.dtype),
+                tf.cast(self.config.vocab_size, dtype=input_ids.dtype),
                 message=(
                     "input_ids must be smaller than the embedding layer's input dimension (got"
                     f" {tf.math.reduce_max(input_ids)} >= {self.vocab_size})"
