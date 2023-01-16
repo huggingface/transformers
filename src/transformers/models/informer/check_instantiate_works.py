27ebb7318777e79f35dfb018f26f2497ab946a0f
Eli Simhayev <elisimhayev@gmail.com>
2023-01-16 12:41:19 +0000

    adding informeConfig. need to remove FeatureEmbedder

--- src/transformers/models/informer/check_instantiate_works.py
+++ src/transformers/models/informer/check_instantiate_works.py
@@ -1,5 +1,8 @@
 from transformers import InformerModel, InformerConfig
+from gluonts.time_feature import get_lags_for_frequency
 
 if __name__ == '__main__':
+    freq = "h"
+    lags = get_lags_for_frequency(freq_str=freq)
     model = InformerModel(InformerConfig())
     print(model)
