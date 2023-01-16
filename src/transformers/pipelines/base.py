4e4cb16513c0329e58b24cdeab9c8177c70a3f3f
Nicolas Patry <patry.nicolas@protonmail.com>
2023-01-16 15:04:27 +0100

    Fixing batching pipelines on single items for ChunkPipeline (#21132)
    * Fixing #20783
    
    * Update src/transformers/pipelines/base.py
    
    * Fixing some tests.
    
    * Fixup.
    
    * Remove ffmpeg dep + a bit more relaxed for bigbird QA precision.
    
    * Better dataset.
    
    * Prevent failing on TF.
    
    * Better condition. We can't use `can_use_iterator` since we cannot use it
    directly.

--- src/transformers/pipelines/base.py
+++ src/transformers/pipelines/base.py
@@ -1072,6 +1072,14 @@ class Pipeline(_ScikitCompat):
             )
         elif is_iterable:
             return self.iterate(inputs, preprocess_params, forward_params, postprocess_params)
+        elif self.framework == "pt" and isinstance(self, ChunkPipeline):
+            return next(
+                iter(
+                    self.get_iterator(
+                        [inputs], num_workers, batch_size, preprocess_params, forward_params, postprocess_params
+                    )
+                )
+            )
         else:
             return self.run_single(inputs, preprocess_params, forward_params, postprocess_params)
 
