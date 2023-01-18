ed26b8fd92d8d403cef6d325368c3d207a29f29b
Joao Gante <joao@huggingface.co>
2023-01-18 13:49:32 +0000

    add fix to bart and reintroduce copied from in opt

--- src/transformers/models/opt/modeling_flax_opt.py
+++ src/transformers/models/opt/modeling_flax_opt.py
@@ -105,6 +105,7 @@ OPT_INPUTS_DOCSTRING = r"""
 """
 
 
+# Copied from transformers.models.bart.modeling_flax_bart.FlaxBartAttention with Bart->OPT
 class FlaxOPTAttention(nn.Module):
     config: OPTConfig
     embed_dim: int
