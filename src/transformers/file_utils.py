067ee05bb98d2c206b423e55e4890f58b24a1343
ydshieh <ydshieh@users.noreply.github.com>
2023-01-19 18:23:59 +0100

    revert Copyright 2023

--- src/transformers/file_utils.py
+++ src/transformers/file_utils.py
@@ -2,7 +2,7 @@
 # There's no way to ignore "F401 '...' imported but unused" warnings in this
 # module, but to preserve other warnings. So, don't check this module at all.
 
-# Copyright 2023 The HuggingFace Team. All rights reserved.
+# Copyright 2020 The HuggingFace Team. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
