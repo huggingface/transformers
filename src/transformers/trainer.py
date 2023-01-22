25cba9cdffe5362092f5dca2d4680ba50342131f
raghavanone <opensourcemaniacfreak@gmail.com>
2023-01-22 11:45:22 +0530

    Fix isort issue

--- src/transformers/trainer.py
+++ src/transformers/trainer.py
@@ -130,7 +130,6 @@ from .trainer_utils import (
     set_seed,
     speed_metrics,
 )
-
 from .training_args import OptimizerNames, ParallelMode, TrainingArguments
 from .utils import (
     CONFIG_NAME,
