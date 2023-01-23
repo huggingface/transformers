66459ce31967b5dfd955471e5e1ff295c94d5af8
amyeroberts <22614925+amyeroberts@users.noreply.github.com>
2023-01-23 13:48:30 +0000

    Add test_image_processing_common.py (#20785)
    * Add test_image_processing_common.py
    
    * Fix typo
    
    * Update imports and test fetcher
    
    * Revert but keep test fetcher update
    
    * Fix imports
    
    * Fix all imports
    
    * Formatting fix
    
    * Update tests/test_image_processing_common.py

--- utils/tests_fetcher.py
+++ utils/tests_fetcher.py
@@ -353,6 +353,7 @@ SPECIAL_MODULE_TO_TEST_MAP = {
     "feature_extraction_sequence_utils.py": "test_sequence_feature_extraction_common.py",
     "feature_extraction_utils.py": "test_feature_extraction_common.py",
     "file_utils.py": ["utils/test_file_utils.py", "utils/test_model_output.py"],
+    "image_processing_utils.py": ["test_image_processing_common.py", "utils/test_image_processing_utils.py"],
     "image_transforms.py": "test_image_transforms.py",
     "utils/generic.py": ["utils/test_file_utils.py", "utils/test_model_output.py", "utils/test_generic.py"],
     "utils/hub.py": "utils/test_hub_utils.py",
