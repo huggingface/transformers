53b5ba6dbd6d71f78c61eebba424ecb2c6481df6
leandro <leandro.vonwerra@spoud.io>
2023-01-23 12:21:52 +0100

    fix style

--- tests/models/gpt2mqa/test_modeling_gpt2mqa.py
+++ tests/models/gpt2mqa/test_modeling_gpt2mqa.py
@@ -518,7 +518,6 @@ class GPT2MQAModelTest(ModelTesterMixin, GenerationTesterMixin, unittest.TestCas
         config_and_inputs = self.model_tester.prepare_config_and_inputs(scale_attn_by_inverse_layer_idx=True)
         self.model_tester.create_and_check_forward_and_backwards(*config_and_inputs)
 
-
     @unittest.skip("GPT2MQA does not support head pruning.")
     def test_head_pruning(self):
         pass
@@ -534,7 +533,6 @@ class GPT2MQAModelTest(ModelTesterMixin, GenerationTesterMixin, unittest.TestCas
     @unittest.skip("GPT2MQA does not support head pruning.")
     def test_head_pruning_save_load_from_pretrained(self):
         pass
-
 
     def test_gpt2mqa_weight_initialization(self):
         config_and_inputs = self.model_tester.prepare_config_and_inputs()
