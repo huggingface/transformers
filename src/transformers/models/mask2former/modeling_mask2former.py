142a106996f0d95cf18c0eef74fba8b18d5fcf46
Niels Rogge <nielsrogge@Nielss-MacBook-Pro.local>
2023-01-24 17:46:04 +0100

    Fix merge

--- src/transformers/models/mask2former/modeling_mask2former.py
+++ src/transformers/models/mask2former/modeling_mask2former.py
@@ -2421,30 +2421,40 @@ class Mask2FormerForUniversalSegmentation(Mask2FormerPreTrainedModel):
         ... )[0]
         >>> print(pred_instance_map.shape)
         torch.Size([480, 640])
-        
+        ```
+
         Semantic segmentation example:
         ```python
-        >>> from transformers import AutoImageProcessor, Mask2FormerForUniversalSegmentation >>> from PIL import Image
-        >>> import requests >>> import torch
+        >>> from transformers import AutoImageProcessor, Mask2FormerForUniversalSegmentation
+        >>> from PIL import Image
+        >>> import requests
+        >>> import torch
 
-        >>> # Load Mask2Former trained on ADE20k semantic segmentation dataset >>> image_processor =
-        AutoImageProcessor.from_pretrained("facebook/mask2former-swin-small-ade-semantic") >>> model =
-        Mask2FormerForUniversalSegmentation.from_pretrained("facebook/mask2former-swin-small-ade-semantic")
+        >>> # Load Mask2Former trained on ADE20k semantic segmentation dataset
+        >>> image_processor = AutoImageProcessor.from_pretrained("facebook/mask2former-swin-small-ade-semantic")
+        >>> model = Mask2FormerForUniversalSegmentation.from_pretrained("facebook/mask2former-swin-small-ade-semantic")
 
-        >>> url = ( ...
-        "https://huggingface.co/datasets/hf-internal-testing/fixtures_ade20k/resolve/main/ADE_val_00000001.jpg" ... )
-        >>> image = Image.open(requests.get(url, stream=True).raw) >>> inputs = image_processor(image,
-        return_tensors="pt")
+        >>> url = (
+        ...     "https://huggingface.co/datasets/hf-internal-testing/fixtures_ade20k/resolve/main/ADE_val_00000001.jpg"
+        ... )
+        >>> image = Image.open(requests.get(url, stream=True).raw)
+        >>> inputs = image_processor(image, return_tensors="pt")
 
-        >>> with torch.no_grad(): ... outputs = model(**inputs)
+        >>> with torch.no_grad():
+        ...     outputs = model(**inputs)
 
-        >>> # Model predicts class_queries_logits of shape `(batch_size, num_queries)` >>> # and masks_queries_logits
-        of shape `(batch_size, num_queries, height, width)` >>> class_queries_logits = outputs.class_queries_logits >>>
-        masks_queries_logits = outputs.masks_queries_logits
+        >>> # Model predicts class_queries_logits of shape `(batch_size, num_queries)`
+        >>> # and masks_queries_logits of shape `(batch_size, num_queries, height, width)`
+        >>> class_queries_logits = outputs.class_queries_logits
+        >>> masks_queries_logits = outputs.masks_queries_logits
 
-        >>> # Perform post-processing to get semantic segmentation map >>> pred_semantic_map =
-        image_processor.post_process_semantic_segmentation( ... outputs, target_sizes=[image.size[::-1]] ... )[0] >>>
-        print(pred_semantic_map.shape) torch.Size([512, 683])
+        >>> # Perform post-processing to get semantic segmentation map
+        >>> pred_semantic_map = image_processor.post_process_semantic_segmentation(
+        ...     outputs, target_sizes=[image.size[::-1]]
+        ... )[0]
+        >>> print(pred_semantic_map.shape)
+        torch.Size([512, 683])
+        ```
 
         Panoptic segmentation example:
 
