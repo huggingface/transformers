0153d569d91d5a179d3e0dcfee9a12f33d56d574
ydshieh <ydshieh@users.noreply.github.com>
2023-01-24 12:36:13 +0100

    try to persiste files

--- .circleci/create_circleci_config.py
+++ .circleci/create_circleci_config.py
@@ -490,7 +490,7 @@ def create_circleci_config(folder=None):
         final_job = CircleCIJob("combine_test_reports", install_steps=[], pytest_num_workers=1)
         config["jobs"]["combine_test_reports"] = final_job.to_dict()
         config["workflows"] = {"version": 2, "run_tests": {"jobs": [j.job_name for j in jobs]}}
-        config["workflows"]["run_tests"]["jobs"].extend(final_job.job_name)
+        config["workflows"]["run_tests"]["jobs"].append(final_job.job_name)
         with open(os.path.join(folder, "generated_config.yml"), "w") as f:
             f.write(yaml.dump(config, indent=2, width=1000000, sort_keys=False))
 
