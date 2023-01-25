069889a0bf51036903e9865ed1ef9a625d7bed10
Zineng Tang <82334223+zinengtang@users.noreply.github.com>
2023-01-25 14:02:59 -0500

    Update src/transformers/models/tvlt/modeling_tvlt.py
    Co-authored-by: amyeroberts <22614925+amyeroberts@users.noreply.github.com>

--- src/transformers/models/tvlt/modeling_tvlt.py
+++ src/transformers/models/tvlt/modeling_tvlt.py
@@ -299,8 +299,8 @@ class TvltPixelPatchEmbeddings(nn.Module):
 
     def __init__(self, config):
         super().__init__()
-        image_size, patch_size = config.pixel_size, config.pixel_patch_size
-        num_channels, hidden_size = config.num_pixel_channels, config.hidden_size
+        image_size, patch_size = config.image_size, config.image_patch_size
+        num_channels, hidden_size = config.num_image_channels, config.hidden_size
 
         image_size = image_size if isinstance(image_size, collections.abc.Iterable) else (image_size, image_size)
         patch_size = patch_size if isinstance(patch_size, collections.abc.Iterable) else (patch_size, patch_size)
