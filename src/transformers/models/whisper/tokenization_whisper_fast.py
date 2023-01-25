21f69cf803e1e06a79cdca824b4418dc384a6e99
Jonatan Kłosko <jonatanklosko@gmail.com>
2023-01-25 13:32:34 +0100

    Sync copied changes from WhisperTokenizer

--- src/transformers/models/whisper/tokenization_whisper_fast.py
+++ src/transformers/models/whisper/tokenization_whisper_fast.py
@@ -199,6 +199,24 @@ class WhisperTokenizerFast(PreTrainedTokenizerFast):
 
         return super()._encode_plus(*args, **kwargs)
 
+    # Copied from transformers.models.whisper.tokenization_whisper.WhisperTokenizer._decode_with_timestamps
+    def _decode_with_timestamps(self, token_ids, time_precision=0.02) -> str:
+        """
+        Timestamp tokens are above the special tokens' id range and are ignored by `decode()`. This method decodes
+        given tokens with timestamps tokens annotated, e.g. "<|1.08|>".
+        """
+        timestamp_begin = self.all_special_ids[-1] + 1
+        outputs = [[]]
+        for token in token_ids:
+            if token >= timestamp_begin:
+                timestamp = f"<|{(token - timestamp_begin) * time_precision:.2f}|>"
+                outputs.append(timestamp)
+                outputs.append([])
+            else:
+                outputs[-1].append(token)
+        outputs = [s if isinstance(s, str) else self.decode(s) for s in outputs]
+        return "".join(outputs)
+
     # Copied from transformers.models.whisper.tokenization_whisper.WhisperTokenizer._compute_offsets
     def _compute_offsets(self, token_ids, time_precision=0.02):
         """
@@ -252,6 +270,7 @@ class WhisperTokenizerFast(PreTrainedTokenizerFast):
         clean_up_tokenization_spaces: bool = True,
         output_offsets: bool = False,
         time_precision=0.02,
+        decode_with_timestamps: bool = False,
         **kwargs
     ) -> str:
         """
@@ -269,7 +288,11 @@ class WhisperTokenizerFast(PreTrainedTokenizerFast):
                 Whether or not to clean up the tokenization spaces.
             kwargs (additional keyword arguments, *optional*):
                 Will be passed to the underlying model specific decode method.
-
+            output_offsets (`bool`, *optional*, defaults to `False`):
+                Whether or not to output the offsets of the tokens. This should only be set if the model predicted
+                timestamps.
+            decode_with_timestamps (`bool`, *optional*, defaults to `False`):
+                WHether or not to decode with timestamps included in the raw text.
         Returns:
             `str`: The decoded sentence.
         """
@@ -279,6 +302,8 @@ class WhisperTokenizerFast(PreTrainedTokenizerFast):
             clean_up_tokenization_spaces=clean_up_tokenization_spaces,
             **kwargs,
         )
+        if decode_with_timestamps:
+            text = self._decode_with_timestamps(token_ids, time_precision=time_precision)
         # retrieve offsets
         if output_offsets:
             offsets = None
