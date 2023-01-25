d07e5eea7713027f71422b96aa67105a8054982a
Zineng Tang <82334223+zinengtang@users.noreply.github.com>
2023-01-25 14:00:58 -0500

    Update src/transformers/models/tvlt/image_processing_tvlt.py
    Co-authored-by: amyeroberts <22614925+amyeroberts@users.noreply.github.com>

--- src/transformers/models/tvlt/image_processing_tvlt.py
+++ src/transformers/models/tvlt/image_processing_tvlt.py
@@ -95,7 +95,7 @@ class TvltImageProcessor(BaseImageProcessor):
         do_rescale (`bool`, *optional*, defaults to `True`):
             Whether to rescale the image by the specified scale `rescale_factor`. Can be overridden by the `do_rescale`
             parameter in the `preprocess` method.
-        rescale_factor (`int` or `float`, *optional*, defaults to `1/255`):
+        rescale_factor (`int` or `float`, *optional*, defaults to 1/255):
             Defines the scale factor to use if rescaling the image. Can be overridden by the `rescale_factor` parameter
             in the `preprocess` method.
         do_normalize (`bool`, *optional*, defaults to `True`):
