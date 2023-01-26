1b76089449d4328805cce27b8d9912e84921c575
altryne <git@alexw.me>
2023-01-26 11:34:39 -0700

    Update Hebrew language code to he per IANA registry (#21310)
    Here's my original PR into whisper that changes the same: 
    https://github.com/openai/whisper/pull/401
    
    Per [IANA registry](https://www.iana.org/assignments/language-subtag-registry/language-subtag-registry), `iw` was deprecated as the code for Hebrew in 1989 and the preferred code is `he`
    
    The correct subtag: 
    ```
    %%
    Type: language
    Subtag: he
    Description: Hebrew
    Added: 2005-10-16
    Suppress-Script: Hebr
    %%
    ``` 
    And the deprecation
    ```
    %%
    Type: language
    Subtag: iw
    Description: Hebrew
    Added: 2005-10-16
    Deprecated: 1989-01-01
    Preferred-Value: he
    Suppress-Script: Hebr
    %%
    ```

--- src/transformers/models/whisper/tokenization_whisper.py
+++ src/transformers/models/whisper/tokenization_whisper.py
@@ -112,7 +112,7 @@ LANGUAGES = {
     "hi": "hindi",
     "fi": "finnish",
     "vi": "vietnamese",
-    "iw": "hebrew",
+    "he": "hebrew",
     "uk": "ukrainian",
     "el": "greek",
     "ms": "malay",
