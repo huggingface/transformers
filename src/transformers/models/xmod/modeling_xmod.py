2d894a24d4cf54ec89b080a13d7031f094f3c9c5
Jannis Vamvas <vamvas@cl.uzh.ch>
2023-01-30 17:16:43 +0100

    Update copyright note

--- src/transformers/models/xmod/modeling_xmod.py
+++ src/transformers/models/xmod/modeling_xmod.py
@@ -1,5 +1,5 @@
 # coding=utf-8
-# Copyright 2023 Facebook AI Research and the HuggingFace Inc. team.
+# Copyright 2023 Meta AI Team and the HuggingFace Inc. team.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
