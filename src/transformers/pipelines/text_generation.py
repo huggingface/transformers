7589ed319521bc6c8989e7d0fea1ad61d1dedbe7
Motoki Wu <tokestermw@gmail.com>
2023-01-29 22:30:49 -0800

    remove import

--- src/transformers/pipelines/text_generation.py
+++ src/transformers/pipelines/text_generation.py
@@ -2,7 +2,6 @@ import enum
 import warnings
 
 from transformers import MODEL_FOR_CAUSAL_LM_MAPPING, TF_MODEL_FOR_CAUSAL_LM_MAPPING
-from ..generation.utils import GenerateOutput
 
 from ..utils import add_end_docstrings, is_tf_available
 from .base import PIPELINE_INIT_ARGS, Pipeline
