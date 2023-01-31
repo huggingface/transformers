e4c7875d3f419549845567a768c004c849277e20
Eli Simhayev <elisimhayev@gmail.com>
2023-01-31 12:56:31 +0000

    remove encoder decoder from the original informer

--- src/transformers/models/informer/modeling_informer.py
+++ src/transformers/models/informer/modeling_informer.py
@@ -1156,33 +1156,6 @@ class InformerEncoderLayer(nn.Module):
         return outputs
 
 
-# # source: https://github.com/zhouhaoyi/Informer2020/blob/main/models/encoder.py
-# class EncoderLayer(nn.Module):
-#     def __init__(self, attention, d_model, d_ff=None, dropout=0.1, activation="relu"):
-#         super(EncoderLayer, self).__init__()
-#         d_ff = d_ff or 4 * d_model
-#         self.attention = attention
-#         self.conv1 = nn.Conv1d(in_channels=d_model, out_channels=d_ff, kernel_size=1)
-#         self.conv2 = nn.Conv1d(in_channels=d_ff, out_channels=d_model, kernel_size=1)
-#         self.norm1 = nn.LayerNorm(d_model)
-#         self.norm2 = nn.LayerNorm(d_model)
-#         self.dropout = nn.Dropout(dropout)
-#         self.activation = F.relu if activation == "relu" else F.gelu
-
-#     def forward(self, x, attn_mask=None):
-#         # x [B, L, D]
-#         # x = x + self.dropout(self.attention(
-#         #     x, x, x,
-#         #     attn_mask = attn_mask
-#         # ))
-#         new_x, attn = self.attention(x, x, x, attn_mask=attn_mask)
-#         x = x + self.dropout(new_x)
-
-#         y = x = self.norm1(x)
-#         y = self.dropout(self.activation(self.conv1(y.transpose(-1, 1))))
-#         y = self.dropout(self.conv2(y).transpose(-1, 1))
-
-#         return self.norm2(x + y), attn
 
 
 class InformerDecoderLayer(nn.Module):
@@ -1308,42 +1281,6 @@ class InformerDecoderLayer(nn.Module):
             outputs += (present_key_value,)
 
         return outputs
-
-
-# # source: https://github.com/zhouhaoyi/Informer2020/blob/main/models/decoder.py
-# class DecoderLayer(nn.Module):
-#     def __init__(
-#         self,
-#         self_attention,
-#         cross_attention,
-#         d_model,
-#         d_ff=None,
-#         dropout=0.1,
-#         activation="relu",
-#     ):
-#         super(DecoderLayer, self).__init__()
-#         d_ff = d_ff or 4 * d_model
-#         self.self_attention = self_attention
-#         self.cross_attention = cross_attention
-#         self.conv1 = nn.Conv1d(in_channels=d_model, out_channels=d_ff, kernel_size=1)
-#         self.conv2 = nn.Conv1d(in_channels=d_ff, out_channels=d_model, kernel_size=1)
-#         self.norm1 = nn.LayerNorm(d_model)
-#         self.norm2 = nn.LayerNorm(d_model)
-#         self.norm3 = nn.LayerNorm(d_model)
-#         self.dropout = nn.Dropout(dropout)
-#         self.activation = F.relu if activation == "relu" else F.gelu
-
-#     def forward(self, x, cross, x_mask=None, cross_mask=None):
-#         x = x + self.dropout(self.self_attention(x, x, x, attn_mask=x_mask)[0])
-#         x = self.norm1(x)
-
-#         x = x + self.dropout(self.cross_attention(x, cross, cross, attn_mask=cross_mask)[0])
-
-#         y = x = self.norm2(x)
-#         y = self.dropout(self.activation(self.conv1(y.transpose(-1, 1))))
-#         y = self.dropout(self.conv2(y).transpose(-1, 1))
-
-#         return self.norm3(x + y)
 
 
 # Copied from transformers.models.time_series_transformer.modeling_time_series_transformer.TimeSeriesTransformerPreTrainedModel with TimeSeriesTransformer->Informer
