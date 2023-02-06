8521bcc3910d235113678c3e436eb42805336189
Saheru <adriansagerlaganga@gmail.com>
2023-02-06 20:40:24 +0100

    Updated src/transformers/__init__.py

--- src/transformers/__init__.py
+++ src/transformers/__init__.py
@@ -2594,6 +2594,7 @@ else:
         "get_cosine_with_hard_restarts_schedule_with_warmup",
         "get_linear_schedule_with_warmup",
         "get_polynomial_decay_schedule_with_warmup",
+        "get_inverse_sqrt_schedule",
         "get_scheduler",
     ]
     _import_structure["pytorch_utils"] = ["Conv1D", "apply_chunking_to_forward", "prune_layer"]
@@ -5665,6 +5666,7 @@ if TYPE_CHECKING:
             get_cosine_with_hard_restarts_schedule_with_warmup,
             get_linear_schedule_with_warmup,
             get_polynomial_decay_schedule_with_warmup,
+            get_inverse_sqrt_schedule,
             get_scheduler,
         )
         from .pytorch_utils import Conv1D, apply_chunking_to_forward, prune_layer
