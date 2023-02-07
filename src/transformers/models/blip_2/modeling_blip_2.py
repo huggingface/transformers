d7abc4e6d63e80c9950b2a496b1560e0350a453a
Niels Rogge <niels.rogge1@gmail.com>
2023-02-07 10:10:43 +0100

    Add doc tests

--- src/transformers/models/blip_2/modeling_blip_2.py
+++ src/transformers/models/blip_2/modeling_blip_2.py
@@ -1276,16 +1276,23 @@ class Blip2ForConditionalGeneration(Blip2PreTrainedModel):
         >>> from PIL import Image
         >>> import requests
         >>> from transformers import Blip2Processor, Blip2ForConditionalGeneration
+        >>> import torch
+
+        >>> device = "cuda" if torch.cuda.is_available() else "cpu"
 
         >>> processor = Blip2Processor.from_pretrained("nielsr/blip2-opt-2.7b")
-        >>> model = Blip2ForConditionalGeneration.from_pretrained("nielsr/blip2-opt-2.7b")
+        >>> model = Blip2ForConditionalGeneration.from_pretrained("nielsr/blip2-opt-2.7b", torch_dtype=torch.float16)
+        >>> model.to(device)  # doctest: +IGNORE_RESULT
 
         >>> url = "http://images.cocodataset.org/val2017/000000039769.jpg"
         >>> image = Image.open(requests.get(url, stream=True).raw)
 
-        >>> inputs = processor(images=image, return_tensors="pt")
+        >>> inputs = processor(images=image, return_tensors="pt").to(device, torch.float16)
 
         >>> generated_ids = model.generate(**inputs)
+        >>> generated_text = processor.batch_decode(generated_ids, skip_special_tokens=True)[0].strip()
+        >>> print(generated_text)
+        two cats laying on a couch
         ```"""
         return_dict = return_dict if return_dict is not None else self.config.use_return_dict
 
