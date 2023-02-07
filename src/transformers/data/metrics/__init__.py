67d074874d285e616393c65a0e670088e1b6b74a
Sylvain Gugger <35901082+sgugger@users.noreply.github.com>
2023-02-07 12:27:31 -0500

    Cleanup quality (#21493)
    * Remove mentions of flake8/isort
    
    * Clean up inits
    
    * Deall with all other inits
    
    * Last special rule for dummy files

--- src/transformers/data/metrics/__init__.py
+++ src/transformers/data/metrics/__init__.py
@@ -1,7 +1,3 @@
-# coding=utf-8
-# Copyright 2018 The Google AI Language Team Authors and The HuggingFace Inc. team.
-# Copyright (c) 2018, NVIDIA CORPORATION.  All rights reserved.
-#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
