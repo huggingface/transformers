aef6d95c49c0b1a8874d014b80bc50de5df2f8ad
Kashif Rasul <kashif.rasul@gmail.com>
2023-02-10 20:52:39 +0100

    use the actual num_static_real_features in feature_dim cal

--- src/transformers/models/time_series_transformer/configuration_time_series_transformer.py
+++ src/transformers/models/time_series_transformer/configuration_time_series_transformer.py
@@ -229,6 +229,6 @@ class TimeSeriesTransformerConfig(PretrainedConfig):
             sum(self.embedding_dimension)
             + self.num_dynamic_real_features
             + self.num_time_features
-            + max(1, self.num_static_real_features)  # there is at least one dummy static real feature
+            + self.num_static_real_features
             + self.input_size * 2  # the log1p(abs(loc)) and log(scale) features
         )
