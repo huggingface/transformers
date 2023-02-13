ad8219825454ac01fabc78886d2a2a19129ce73c
Arthur Zucker <arthur.zucker@gmail.com>
2023-02-13 17:30:44 +0000

    update modeling clap

--- src/transformers/models/clap/modeling_clap.py
+++ src/transformers/models/clap/modeling_clap.py
@@ -411,25 +411,17 @@ class ClapAudioPatchEmbed(nn.Module):
                 # local processing
                 local_hidden_states = hidden_states[is_longer_idx, 1:, :, :].contiguous()
                 batch_size, num_channels, height, width = local_hidden_states.shape
-
                 local_hidden_states = local_hidden_states.view(batch_size * num_channels, 1, height, width)
+                
                 local_hidden_states = self.mel_conv2d(local_hidden_states)
-                local_hidden_states = local_hidden_states.view(
-                    batch_size,
-                    num_channels,
-                    local_hidden_states.size(1),
-                    local_hidden_states.size(2),
-                    local_hidden_states.size(3),
-                )
+                
+                _, features, height, width = local_hidden_states.shape
+                local_hidden_states = local_hidden_states.view(batch_size,num_channels,features,height,width)
                 local_hidden_states = local_hidden_states.permute((0, 2, 3, 1, 4)).contiguous().flatten(3)
-
+                
                 local_width = local_hidden_states.size(-1)
-                if local_width < output_width:
-                    local_hidden_states = torch.nn.functional.pad(
-                        local_hidden_states, (0, output_width - local_width), "constant", 0
-                    )
-                else:
-                    local_hidden_states = local_hidden_states[:, :, :, :output_width]
+                local_hidden_states = torch.nn.functional.pad(local_hidden_states, (0, output_width - local_width), "constant", 0)
+
 
                 global_hidden_states[is_longer_idx] = self.fusion_model(
                     global_hidden_states[is_longer_idx], local_hidden_states
