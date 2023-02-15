0bd844aa65a7341136c23d899274a402bddd961a
Susnato Dhar <susnatodhar10@gmail.com>
2023-02-15 19:54:56 +0530

    Add Ernie-M Model to huggingface (#21349)
    * config and tokenization(fast too) changed and ErnieEncoder added
    
    * Slow Tokenization Added
    
    * Tokenizer(slow) is now working and Fast Tokenizer removed
    
    * Added Config code
    
    * Added Base Model and utils
    
    * ErnieMModel is now working
    
    * All added except tests
    
    * All tests passed except ErnieUIEM
    
    * All tests passed
    
    * all fixes done
    
    * all fixes done
    
    * fixed MAP
    
    * fixed check_code_quality
    
    * fixed Build PR Documentation issue
    
    * Added changes(comments) and also updated to the latest upstream/main
    
    * Added fixup
    
    * Added # Copied comments
    
    * Added fixup
    
    * Added more comments and some nits
    
    * Added fixup
    
    * Fixed README_hd.md
    
    * Added more fixes
    
    * ErnieMTokenizer (being sentencepiece) protected and other docs edited
    
    * Added code_quality fix
    
    * Fixed for
    
    * Added more fix
    
    * modified AZ
    
    * ernie-m tokenization test added!
    
    * attention mask part fixed(with 0->self.config.pad_token_id)
    
    * applied make fixup

--- src/transformers/utils/dummy_pt_objects.py
+++ src/transformers/utils/dummy_pt_objects.py
@@ -2415,6 +2415,58 @@ class ErniePreTrainedModel(metaclass=DummyObject):
         requires_backends(self, ["torch"])
 
 
+ERNIE_M_PRETRAINED_MODEL_ARCHIVE_LIST = None
+
+
+class ErnieMForInformationExtraction(metaclass=DummyObject):
+    _backends = ["torch"]
+
+    def __init__(self, *args, **kwargs):
+        requires_backends(self, ["torch"])
+
+
+class ErnieMForMultipleChoice(metaclass=DummyObject):
+    _backends = ["torch"]
+
+    def __init__(self, *args, **kwargs):
+        requires_backends(self, ["torch"])
+
+
+class ErnieMForQuestionAnswering(metaclass=DummyObject):
+    _backends = ["torch"]
+
+    def __init__(self, *args, **kwargs):
+        requires_backends(self, ["torch"])
+
+
+class ErnieMForSequenceClassification(metaclass=DummyObject):
+    _backends = ["torch"]
+
+    def __init__(self, *args, **kwargs):
+        requires_backends(self, ["torch"])
+
+
+class ErnieMForTokenClassification(metaclass=DummyObject):
+    _backends = ["torch"]
+
+    def __init__(self, *args, **kwargs):
+        requires_backends(self, ["torch"])
+
+
+class ErnieMModel(metaclass=DummyObject):
+    _backends = ["torch"]
+
+    def __init__(self, *args, **kwargs):
+        requires_backends(self, ["torch"])
+
+
+class ErnieMPreTrainedModel(metaclass=DummyObject):
+    _backends = ["torch"]
+
+    def __init__(self, *args, **kwargs):
+        requires_backends(self, ["torch"])
+
+
 ESM_PRETRAINED_MODEL_ARCHIVE_LIST = None
 
 
