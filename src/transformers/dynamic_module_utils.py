37a046c408fef912711e936d1f35233e2f670f66
ydshieh <ydshieh@users.noreply.github.com>
2023-02-15 18:59:27 +0100

    fix dynamic module import error

--- src/transformers/dynamic_module_utils.py
+++ src/transformers/dynamic_module_utils.py
@@ -148,9 +148,11 @@ def get_class_in_module(class_name, module_path):
     # copy to a temporary directory
     shutil.copytree(module_dir, module_dir_backup_temp)
 
-    # remove and recreate an empty directory
-    os.system(f"rm -rf {module_dir}")
-    os.makedirs(module_dir)
+    # remove `configuration.py`: this is necessary when we try to import modeling module, or other tokenizer/processor
+    # modules, while configuration module has been imported previously.
+    # TODO: This is only a simple heuristic. In general, we might need to consider any dynamic module that has been
+    #       imported. However, we don't have this information so far.
+    os.system(f"rm -rf {module_dir}/configuration.py")
 
     # copy back the target module file - and ONLY this single file
     # Without this hack, we may get error: `ModuleNotFoundError: No module named 'transformers_modules.local.modeling'`
@@ -159,26 +161,7 @@ def get_class_in_module(class_name, module_path):
 
     # import the module
     module_path = module_path.replace(os.path.sep, ".")
-
-    # up to the parent of the target module
-    parent_module_path = ".".join(module_path.split(".")[:-1])
-
-    try:
-        module = importlib.import_module(module_path)
-    except ModuleNotFoundError as e:
-        # 'transformers_modules.hf-internal-testing.test_dynamic_model_with_util.b731e5fae6d80a4a775461251c4388886fb7a249.util"
-        missing_module_path = str(e).replace("No module named ", "")[1:-1]
-
-        # anything after the parent of the target module
-        missing_module_path_short = missing_module_path.replace(parent_module_path + ".", "")
-        missing_module_file_path_short = missing_module_path_short.replace(".", os.path.sep)
-
-        # full path to the missing module
-        missing_module_file_path = module_dir / missing_module_file_path_short
-
-        missing_module_file_path_2 = Path(module_dir_backup_temp) / missing_module_file_path_short
-        shutil.copytree(os.path.join(missing_module_file_path_2, missing_module_file_path), module_dir)
-        module = importlib.import_module(module_path)
+    module = importlib.import_module(module_path)
 
     # copy the whole directory back
     os.system(f"rm -rf {module_dir}")
