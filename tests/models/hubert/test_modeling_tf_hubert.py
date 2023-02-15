8affeb2f4b8f812700dd814e90d46820d310daa1
amyeroberts <22614925+amyeroberts@users.noreply.github.com>
2023-02-15 16:00:50 +0000

    Skipping more high mem tests - Wav2Vec2 Hubert (#21647)
    Skipping more tests

--- tests/models/hubert/test_modeling_tf_hubert.py
+++ tests/models/hubert/test_modeling_tf_hubert.py
@@ -321,19 +321,15 @@ class TFHubertModelTest(TFModelTesterMixin, unittest.TestCase):
         model = TFHubertModel.from_pretrained("facebook/hubert-base-ls960")
         self.assertIsNotNone(model)
 
-    # We override here as passing a full batch of 13 samples results in OOM errors for CTC
+    @unittest.skip(reason="Fix me! Hubert hits OOM errors when loss is computed on full batch")
     def test_dataset_conversion(self):
-        default_batch_size = self.model_tester.batch_size
-        self.model_tester.batch_size = 2
-        super().test_dataset_conversion()
-        self.model_tester.batch_size = default_batch_size
+        # TODO: (Amy) - check whether skipping CTC model resolves this issue and possible resolutions for CTC
+        pass
 
-    # We override here as passing a full batch of 13 samples results in OOM errors for CTC
+    @unittest.skip(reason="Fix me! Hubert hits OOM errors when loss is computed on full batch")
     def test_keras_fit(self):
-        default_batch_size = self.model_tester.batch_size
-        self.model_tester.batch_size = 2
-        super().test_keras_fit()
-        self.model_tester.batch_size = default_batch_size
+        # TODO: (Amy) - check whether skipping CTC model resolves this issue and possible resolutions for CTC
+        pass
 
 
 @require_tf
