bfda32e651665ad96c8c0ebeb9c71a62fe3a7672
Connor Henderson <connor.henderson@talkiatry.com>
2023-02-16 11:32:32 -0500

    refactor: Make direct_transformers_import util (#21652)
    * refactor: Make direct_import util
    
    * edit direct import fn
    
    * add docstring
    
    * make import function specific to transformers only
    
    * edit doc string

--- utils/check_repo.py
+++ utils/check_repo.py
@@ -13,11 +13,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import importlib
 import inspect
 import os
 import re
-import sys
 import warnings
 from collections import OrderedDict
 from difflib import get_close_matches
@@ -25,7 +23,7 @@ from pathlib import Path
 
 from transformers import is_flax_available, is_tf_available, is_torch_available
 from transformers.models.auto import get_values
-from transformers.utils import ENV_VARS_TRUE_VALUES
+from transformers.utils import ENV_VARS_TRUE_VALUES, direct_transformers_import
 
 
 # All paths are set with the intent you should run this script from the root of the repo with the command
@@ -307,14 +305,7 @@ MODEL_TYPE_TO_DOC_MAPPING = OrderedDict(
 
 
 # This is to make sure the transformers module imported is the one in the repo.
-spec = importlib.util.spec_from_file_location(
-    "transformers",
-    os.path.join(PATH_TO_TRANSFORMERS, "__init__.py"),
-    submodule_search_locations=[PATH_TO_TRANSFORMERS],
-)
-transformers = importlib.util.module_from_spec(spec)
-spec.loader.exec_module(transformers)
-transformers = sys.modules["transformers"]
+transformers = direct_transformers_import(PATH_TO_TRANSFORMERS)
 
 
 def check_model_list():
