38e7c8fbc4b78c668ddbb83b3d7ec3e7ee193c34
AlexWertheim <90242206+AlexWertheim@users.noreply.github.com>
2023-02-20 00:06:23 -0800

    Enable PyTorch/XLA Fully Sharded Data Parallel (FSDP) (#21406)
    * Reinserted import statement accidentally removed during rebasing.
    
    * Added auto_wrap functionality, restructured XLA FSDP logic to more closely match PyTorch FSDP logic.
    
    * Fixed flag descriptions; changed several instances of fsdp_ to xla_fsdp_; pass in auto_wrap_policy and auto_wrapper_callable directly to avoid lambda saving.
    
    * Moved XLA FSDP logic to be adjacent to Fairscale FSDP logic in trainer.
    
    * Formatted changes in accordance with HF style requirements.
    
    * Added back in warning which was accidentally removed.
    
    * - Merged XLA FSDP training arguments into `fsdp_config`
    - Added `xla` boolean flag to `fsdp_config` to specify XLA FSDP wrapping
    - Merged XLA FSDP wrapping logic into FSDP wrapping logic within trainer
      class
    
    * Cleaned up errors, moved argument to fsdp_config
    
    - Set `xla` and `xla_fsdp_grad_ckpt` flags by default in fsdp_config
    - Added missing colons following conditionals
    - Moved `fsdp_transformer_layer_cls_to_wrap` to `fsdp_config`
    - Modified `fsdp_transformer_layer_cls_to_wrap` to be list of strings,
      not just one string
    - Changed Fairscale FSDP logic to allow for set of layer classes to wrap
    - Removed unnecessary checks for `xla_fsdp`
    
    * Corrected small errors, improved layer class flag
    
    - Correctly set default values for `xla` and `xla_fsdp_grad_ckpt`
      arguments
    - Made `fsdp_transformer_layer_cls_to_wrap` a list of strings instead of
      a single string
    - Added processing to ensure that `fsdp_transformer_layer_cls_to_wrap`
      works as expected if passed as a single string
    - Updated PyTorch FSDP logic to accept a list of layers to wrap, as done
      with XLA FSDP
    - Replaced instances of `getattr()` with `.get()` for dictionary
      retrievals with default values, including when setting
      `fsdp_min_num_params`
    - Corrected `self.fsdp is not None` to `len(self.fsdp) > 0`
    - Removed extraneous `xla_fsdp` argument descriptions from outside
      `fsdp_config`
    
    * Changed xla-fsdp-settings to be dictionary
    
    - Modified xla-fsdp-settings to be entered directly as dictionary
      instead of loaded through JSON file
    - Made small style corrections
    
    * Reverted unintentional local_rank TPU check
    
    * Do not block XLA FSDP if local rank is -1
    
    * Rebased and applied automatic formatting
    
    - Rebased
    - Applied automatic formatting changes via `make style`
    
    * Applied automatic formatting with latest version of black
    
    * Replaced  expression with
    
    * Reran black examples tests src utils
    ruff examples tests src utils --fix
    make autogenerate_code
    make[1]: Entering directory '/usr/local/google/home/awertheim/HF-FSDP-PR/transformers'
    make[1]: Leaving directory '/usr/local/google/home/awertheim/HF-FSDP-PR/transformers' after additional formatting changes
    
    * Additionall automatic formatting changes
    
    * Remove unnecessary whitespace characters from src/transformers/training_args.py
    
    Co-authored-by: Sylvain Gugger <35901082+sgugger@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Sylvain Gugger <35901082+sgugger@users.noreply.github.com>

--- src/transformers/trainer.py
+++ src/transformers/trainer.py
@@ -417,7 +417,7 @@ class Trainer:
                 raise ValueError(
                     "Using --fsdp xxx together with --deepspeed is not possible, deactivate one of those flags."
                 )
-            if args.local_rank == -1:
+            if not args.fsdp_config["xla"] and args.local_rank == -1:
                 raise ValueError("Using fsdp only works in distributed training.")
 
             # dep_version_check("torch>=1.12.0")
@@ -1419,55 +1419,110 @@ class Trainer:
                 ).to(self.args.device)
         # Distributed training using PyTorch FSDP
         elif self.fsdp is not None:
-            # PyTorch FSDP!
-            from torch.distributed.fsdp.fully_sharded_data_parallel import CPUOffload, MixedPrecision
-            from torch.distributed.fsdp.fully_sharded_data_parallel import FullyShardedDataParallel as FSDP
-            from torch.distributed.fsdp.wrap import size_based_auto_wrap_policy, transformer_auto_wrap_policy
+            if not self.args.fsdp_config["xla"]:
+                # PyTorch FSDP!
+                from torch.distributed.fsdp.fully_sharded_data_parallel import CPUOffload, MixedPrecision
+                from torch.distributed.fsdp.fully_sharded_data_parallel import FullyShardedDataParallel as FSDP
+                from torch.distributed.fsdp.wrap import size_based_auto_wrap_policy, transformer_auto_wrap_policy
 
-            if FSDPOption.OFFLOAD in self.args.fsdp:
-                cpu_offload = CPUOffload(offload_params=True)
+                if FSDPOption.OFFLOAD in self.args.fsdp:
+                    cpu_offload = CPUOffload(offload_params=True)
+                else:
+                    cpu_offload = CPUOffload(offload_params=False)
+
+                auto_wrap_policy = None
+
+                if FSDPOption.AUTO_WRAP in self.args.fsdp:
+                    if self.args.fsdp_config["fsdp_min_num_params"] > 0:
+                        auto_wrap_policy = functools.partial(
+                            size_based_auto_wrap_policy, min_num_params=self.args.fsdp_config["fsdp_min_num_params"]
+                        )
+                    elif self.args.fsdp_config.get("fsdp_transformer_layer_cls_to_wrap", None) is not None:
+                        transformer_cls_to_wrap = set()
+                        for layer_class in self.args.fsdp_config["fsdp_transformer_layer_cls_to_wrap"]:
+                            transformer_cls = get_module_class_from_name(model, layer_class)
+                            if transformer_cls is None:
+                                raise Exception("Could not find the transformer layer class to wrap in the model.")
+                            else:
+                                transformer_cls_to_wrap.add(transformer_cls)
+                        auto_wrap_policy = functools.partial(
+                            transformer_auto_wrap_policy,
+                            # Transformer layer class to wrap
+                            transformer_layer_cls=transformer_cls_to_wrap,
+                        )
+                mixed_precision_policy = None
+                dtype = None
+                if self.args.fp16:
+                    dtype = torch.float16
+                elif self.args.bf16:
+                    dtype = torch.bfloat16
+                if dtype is not None:
+                    mixed_precision_policy = MixedPrecision(param_dtype=dtype, reduce_dtype=dtype, buffer_dtype=dtype)
+                if type(model) != FSDP:
+                    # XXX: Breaking the self.model convention but I see no way around it for now.
+                    self.model = model = FSDP(
+                        model,
+                        sharding_strategy=self.fsdp,
+                        cpu_offload=cpu_offload,
+                        auto_wrap_policy=auto_wrap_policy,
+                        mixed_precision=mixed_precision_policy,
+                        device_id=self.args.device,
+                        backward_prefetch=self.backward_prefetch,
+                        forward_prefetch=self.forword_prefetch,
+                        limit_all_gathers=self.limit_all_gathers,
+                    )
             else:
-                cpu_offload = CPUOffload(offload_params=False)
-
-            auto_wrap_policy = None
-
-            if FSDPOption.AUTO_WRAP in self.args.fsdp:
+                try:
+                    from torch_xla.distributed.fsdp import XlaFullyShardedDataParallel as FSDP
+                    from torch_xla.distributed.fsdp import checkpoint_module
+                    from torch_xla.distributed.fsdp.wrap import (
+                        size_based_auto_wrap_policy,
+                        transformer_auto_wrap_policy,
+                    )
+                except ImportError:
+                    raise ImportError("Missing XLA FSDP related module; please make sure to use torch-xla >= 2.0.")
+                auto_wrap_policy = None
+                auto_wrapper_callable = None
                 if self.args.fsdp_config["fsdp_min_num_params"] > 0:
                     auto_wrap_policy = functools.partial(
                         size_based_auto_wrap_policy, min_num_params=self.args.fsdp_config["fsdp_min_num_params"]
                     )
-                elif self.args.fsdp_transformer_layer_cls_to_wrap is not None:
-                    transformer_cls_to_wrap = get_module_class_from_name(
-                        model, self.args.fsdp_transformer_layer_cls_to_wrap
-                    )
-                    if transformer_cls_to_wrap is None:
-                        raise Exception("Could not find the transformer layer class to wrap in the model.")
+                elif self.args.fsdp_config.get("fsdp_transformer_layer_cls_to_wrap", None) is not None:
+                    transformer_cls_to_wrap = set()
+                    for layer_class in self.args.fsdp_config["fsdp_transformer_layer_cls_to_wrap"]:
+                        transformer_cls = get_module_class_from_name(model, layer_class)
+                        if transformer_cls is None:
+                            raise Exception("Could not find the transformer layer class to wrap in the model.")
+                        else:
+                            transformer_cls_to_wrap.add(transformer_cls)
                     auto_wrap_policy = functools.partial(
                         transformer_auto_wrap_policy,
                         # Transformer layer class to wrap
-                        transformer_layer_cls={transformer_cls_to_wrap},
+                        transformer_layer_cls=transformer_cls_to_wrap,
                     )
-            mixed_precision_policy = None
-            dtype = None
-            if self.args.fp16:
-                dtype = torch.float16
-            elif self.args.bf16:
-                dtype = torch.bfloat16
-            if dtype is not None:
-                mixed_precision_policy = MixedPrecision(param_dtype=dtype, reduce_dtype=dtype, buffer_dtype=dtype)
-            if type(model) != FSDP:
-                # XXX: Breaking the self.model convention but I see no way around it for now.
+                fsdp_kwargs = self.args.xla_fsdp_config
+                if self.args.fsdp_config["xla_fsdp_grad_ckpt"]:
+                    # Apply gradient checkpointing to auto-wrapped sub-modules if specified
+                    def auto_wrapper_callable(m, *args, **kwargs):
+                        return FSDP(checkpoint_module(m), *args, **kwargs)
+
+                # Wrap the base model with an outer FSDP wrapper
                 self.model = model = FSDP(
                     model,
-                    sharding_strategy=self.fsdp,
-                    cpu_offload=cpu_offload,
                     auto_wrap_policy=auto_wrap_policy,
-                    mixed_precision=mixed_precision_policy,
-                    device_id=self.args.device,
-                    backward_prefetch=self.backward_prefetch,
-                    forward_prefetch=self.forword_prefetch,
-                    limit_all_gathers=self.limit_all_gathers,
+                    auto_wrapper_callable=auto_wrapper_callable,
+                    **fsdp_kwargs,
                 )
+
+                # Patch `xm.optimizer_step` should not reduce gradients in this case,
+                # as FSDP does not need gradient reduction over sharded parameters.
+                def patched_optimizer_step(optimizer, barrier=False, optimizer_args={}):
+                    loss = optimizer.step(**optimizer_args)
+                    if barrier:
+                        xm.mark_step()
+                    return loss
+
+                xm.optimizer_step = patched_optimizer_step
         elif is_sagemaker_dp_enabled():
             model = nn.parallel.DistributedDataParallel(
                 model, device_ids=[int(os.getenv("SMDATAPARALLEL_LOCAL_RANK"))]
