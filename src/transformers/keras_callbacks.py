cb13c783609b1b01a53403058797fa8864bc79d7
Matt <rocketknight1@gmail.com>
2023-02-21 14:34:18 +0000

    Add a debug check

--- src/transformers/keras_callbacks.py
+++ src/transformers/keras_callbacks.py
@@ -260,6 +260,7 @@ class KerasMetricCallback(Callback):
         # in the logs argument. Ordinarily, this is so the callback can read them, but in this case we write a bunch of
         # new keys in there, which will then get read by the History callback and treated like any other metric value.
         # I promise that I have it in writing from Chollet that this is okay.
+        print("DEBUG LINE: Here are the logs I'm seeing: ", logs)
         logs.update(metric_output)
 
 
