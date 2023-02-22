1c16c0158841db88397f5d0e124bf5eab0553bbc
ydshieh <ydshieh@users.noreply.github.com>
2023-02-22 10:34:00 +0100

    skip test_model_parallelism

--- tests/models/gptsan_japanese/test_modeling_gptsan_japanese.py
+++ tests/models/gptsan_japanese/test_modeling_gptsan_japanese.py
@@ -151,6 +151,12 @@ class GPTSanJapaneseTest(ModelTesterMixin, unittest.TestCase):
         config_and_inputs = self.model_tester.prepare_config_and_inputs()
         self.model_tester.create_and_check_model(*config_and_inputs)
 
+    @unittest.skip(
+        reason="skip for now as the computed `max_memory` by `model_split_percents` in the test method will be changed inside `from_pretrained`"
+    )
+    def test_model_parallelism(self):
+        super().test_model_parallelism()
+
 
 @require_torch
 class GPTSanJapaneseForConditionalGenerationTest(ModelTesterMixin, GenerationTesterMixin, unittest.TestCase):
