d76c6c2a0253b138c07908fa61bedb5cb4e163f6
arthur.zucker@gmail.com <arthur.zucker@gmail.com>
2023-02-24 15:58:49 +0000

    fix collate fn that was casting t float

--- src/transformers/pipelines/base.py
+++ src/transformers/pipelines/base.py
@@ -77,9 +77,12 @@ def _pad(items, key, padding_value, padding_side):
         # Others include `attention_mask` etc...
         shape = items[0][key].shape
         dim = len(shape)
-        if key in ["pixel_values", "image", "input_features"]:
+        if key in ["pixel_values", "image"]:
             # This is probable image so padding shouldn't be necessary
             # B, C, H, W
+            return torch.cat([item[key] for item in items], dim=0)
+        elif dim == 4 and key == "input_features":
+            # this is probably a mel spectrogram batched
             return torch.cat([item[key] for item in items], dim=0)
         max_length = max(item[key].shape[1] for item in items)
         min_length = min(item[key].shape[1] for item in items)
@@ -154,7 +157,7 @@ def pad_collate_fn(tokenizer, feature_extractor):
         for key in keys:
             if key in {"input_ids"}:
                 # ImageGPT uses a feature extractor
-                if feature_extractor is not None:
+                if tokenizer is None and feature_extractor is not None:
                     _padding_value = f_padding_value
                 else:
                     _padding_value = t_padding_value
