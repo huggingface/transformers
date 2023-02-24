d4bfa48082a263661931e650c726ba090583773f
“krypticmouse” <herumbshandilya123@gmail.com>
2023-02-24 23:36:12 +0530

    If applied, this commit fixes generate bug in gptj

--- src/transformers/models/gptj/modeling_gptj.py
+++ src/transformers/models/gptj/modeling_gptj.py
@@ -633,6 +633,12 @@ class GPTJModel(GPTJPreTrainedModel):
 
         output_shape = input_shape + (hidden_states.size(-1),)
 
+        if self.gradient_checkpointing and self.training:
+            if use_cache:
+                logger.warning(
+                    "`use_cache=True` is incompatible with gradient checkpointing. Setting `use_cache=False`..."
+                )
+                use_cache = False
         presents = () if use_cache else None
         all_self_attentions = () if output_attentions else None
         all_hidden_states = () if output_hidden_states else None
