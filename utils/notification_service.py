a94ff3a55159f9da5e00cbb7f38a2371b20a1009
ydshieh <ydshieh@users.noreply.github.com>
2023-02-24 20:05:09 +0100

    fix counting

--- utils/notification_service.py
+++ utils/notification_service.py
@@ -585,10 +585,10 @@ def get_job_links():
 
     try:
         jobs.update({job["name"]: job["html_url"] for job in result["jobs"]})
-        pages_to_iterate_over = math.ceil((result["total_count"] - 100) / 100)
+        pages_to_iterate_over = math.ceil((result["total_count"]) / 100)
 
         for i in range(pages_to_iterate_over):
-            result = requests.get(url + f"&page={i + 2}").json()
+            result = requests.get(url + f"&page={i + 1}").json()
             jobs.update({job["name"]: job["html_url"] for job in result["jobs"]})
 
         return jobs
