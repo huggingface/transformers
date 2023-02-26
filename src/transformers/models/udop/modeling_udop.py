a8b3165437ed7ba256c7e53ef6c45c4f2a8561c2
raghavanone <opensourcemaniacfreak@gmail.com>
2023-02-26 09:49:50 +0530

    Incorporate PR feedbacks

--- src/transformers/models/udop/modeling_udop.py
+++ src/transformers/models/udop/modeling_udop.py
@@ -366,43 +366,6 @@ class Attention(nn.Module):
         # x = self.proj_drop(x)
         return x
 
-
-class UdopCrossAttention(nn.Module):
-    """Copied from Xclip's cross attention"""
-
-    def __init__(self, dim, num_heads=8, qkv_bias=False, qk_scale=None, attn_drop=0.0, proj_drop=0.0):
-        super().__init__()
-        self.num_heads = num_heads
-        head_dim = dim // num_heads
-        # NOTE scale factor was wrong in my original version, can set manually to be compat with prev weights
-        self.scale = qk_scale or head_dim**-0.5
-
-        self.q = nn.Linear(dim, dim, bias=qkv_bias)
-        self.k = nn.Linear(dim, dim, bias=qkv_bias)
-        self.v = nn.Linear(dim, dim, bias=qkv_bias)
-
-        self.attn_drop = nn.Dropout(attn_drop)
-        self.proj = nn.Linear(dim, dim)
-        self.proj_drop = nn.Dropout(proj_drop)
-
-    def forward(self, x, context):
-        B, N, C = x.shape
-        _, N_context, _ = context.shape
-
-        q = self.q(x).reshape(B, N, self.num_heads, C // self.num_heads).permute(0, 2, 1, 3)
-        k = self.k(context).reshape(B, N_context, self.num_heads, C // self.num_heads).permute(0, 2, 1, 3)
-        v = self.v(context).reshape(B, N_context, self.num_heads, C // self.num_heads).permute(0, 2, 1, 3)
-
-        attn = (q @ k.transpose(-2, -1)) * self.scale
-        attn = attn.softmax(dim=-1)
-        attn = self.attn_drop(attn)
-
-        x = (attn @ v).transpose(1, 2).reshape(B, N, C)
-        x = self.proj(x)
-        x = self.proj_drop(x)
-        return x
-
-
 class UdopMaeBlock(nn.Module):
     def __init__(
         self,
