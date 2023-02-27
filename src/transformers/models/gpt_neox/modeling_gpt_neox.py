3a886a217d9523670a217b9b3c5200d8540b06af
Karim Foda <kmfoda@gmail.com>
2023-02-27 09:25:09 +0000

    Fix gradient checkpointing bug in gptneox

--- src/transformers/models/gpt_neox/modeling_gpt_neox.py
+++ src/transformers/models/gpt_neox/modeling_gpt_neox.py
@@ -500,6 +500,13 @@ class GPTNeoXModel(GPTNeoXPreTrainedModel):
 
         hidden_states = inputs_embeds
 
+        if self.gradient_checkpointing and self.training:
+            if use_cache:
+                logger.warning(
+                    "`use_cache=True` is incompatible with gradient checkpointing. Setting `use_cache=False`..."
+                )
+                use_cache = False
+
         presents = () if use_cache else None
         all_attentions = () if output_attentions else None
         all_hidden_states = () if output_hidden_states else None
