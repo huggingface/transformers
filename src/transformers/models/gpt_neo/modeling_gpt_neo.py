98ead44ef3601e89d5b2fb97ff9d31fd9286cf43
Stas Bekman <stas00@users.noreply.github.com>
2023-02-27 13:25:06 -0800

    introduce `logger.warning_once` and use it for grad checkpointing code (#21804)
    * logger.warning_once
    
    * style

--- src/transformers/models/gpt_neo/modeling_gpt_neo.py
+++ src/transformers/models/gpt_neo/modeling_gpt_neo.py
@@ -589,7 +589,7 @@ class GPTNeoModel(GPTNeoPreTrainedModel):
 
         if self.gradient_checkpointing and self.training:
             if use_cache:
-                logger.warning(
+                logger.warning_once(
                     "`use_cache=True` is incompatible with gradient checkpointing. Setting `use_cache=False`..."
                 )
                 use_cache = False
--- src/transformers/models/plbart/modeling_plbart.py
+++ src/transformers/models/plbart/modeling_plbart.py
@@ -1048,7 +1048,7 @@ class PLBartDecoder(PLBartPreTrainedModel):
 
             if self.gradient_checkpointing and self.training:
                 if use_cache:
-                    logger.warning(
+                    logger.warning_once(
                         "`use_cache=True` is incompatible with gradient checkpointing. Setting `use_cache=False`..."
                     )
                     use_cache = False
