a9b8a336eba4baae4dd2e6ddee116702be8fedfc
Yih-Dar <2521628+ydshieh@users.noreply.github.com>
2023-02-28 08:10:29 +0100

    Rename `MobileViTModelTest` to `TFMobileViTModelTest` (#21825)
    Let's give TF a bit more love ❤️ 🙏
    
    Co-authored-by: ydshieh <ydshieh@users.noreply.github.com>

--- tests/models/mobilevit/test_modeling_tf_mobilevit.py
+++ tests/models/mobilevit/test_modeling_tf_mobilevit.py
@@ -154,7 +154,7 @@ class TFMobileViTModelTester:
 
 
 @require_tf
-class MobileViTModelTest(TFModelTesterMixin, unittest.TestCase):
+class TFMobileViTModelTest(TFModelTesterMixin, unittest.TestCase):
     """
     Here we also overwrite some of the tests of test_modeling_common.py, as MobileViT does not use input_ids, inputs_embeds,
     attention_mask and seq_length.
