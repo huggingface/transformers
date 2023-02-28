a3ffc957577cf45b4bd29caf6ca84f89cddea544
Younes Belkada <49240599+younesbelkada@users.noreply.github.com>
2023-02-28 15:42:55 +0100

    [`Blip2`] Add `Blip2Model` (#21817)
    * add v1
    
    * add `Blip2Model`
    
    - add relevant functions
    - add tests
    - add on automapping
    
    * fix docs
    
    * fix doctest

--- src/transformers/utils/dummy_pt_objects.py
+++ src/transformers/utils/dummy_pt_objects.py
@@ -1221,6 +1221,13 @@ class Blip2ForConditionalGeneration(metaclass=DummyObject):
         requires_backends(self, ["torch"])
 
 
+class Blip2Model(metaclass=DummyObject):
+    _backends = ["torch"]
+
+    def __init__(self, *args, **kwargs):
+        requires_backends(self, ["torch"])
+
+
 class Blip2PreTrainedModel(metaclass=DummyObject):
     _backends = ["torch"]
 
