fix attention mask

src/transformers/models/cpmant/modeling_cpmant.py
@@ -734,7 +734,7 @@ class CPMAntModel(CPMAntPreTrainedModel):
             torch.tensor(list(range(seqlen - self.prompt_length))[::-1], device=device)[None, :].repeat(batch, 1)
             < length[:, None]
         )
-        mask_1d = torch.cat((torch.ones(batch, self.prompt_length).bool(), mask_1d), dim=1)
+        mask_1d = torch.cat((torch.ones(batch, self.prompt_length, device=device).bool(), mask_1d), dim=1)
         attention_mask = mask_1d.view(batch, seqlen, 1) & mask_1d.view(batch, 1, seqlen) & attention_mask
         return attention_mask
 