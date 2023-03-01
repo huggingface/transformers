359fb840ea8b233c057d99f037980f97dd055d71
raghavanone <opensourcemaniacfreak@gmail.com>
2023-03-01 15:05:52 +0530

    Revert unneccessary changes

--- tests/test_pipeline_mixin.py
+++ tests/test_pipeline_mixin.py
@@ -224,11 +224,11 @@ class PipelineTesterMixin:
         # TODO: Maybe not upload such problematic tiny models to Hub.
         if tokenizer is None and processor is None:
             self.skipTest(
-                f"{self.__class__.__name__}::test_pipeline_{task.replace('-', '_')} is skipped: Could not find or load"
-                f" any tokenizer / processor from `{repo_id}`."
+                f"{self.__class__.__name__}::test_pipeline_{task.replace('-', '_')} is skipped: Could not find or load "
+                f"any tokenizer / processor from `{repo_id}`."
             )
 
-        # TODO: We should check if a model file is on the Hub repo instead.
+        # TODO: We should check if a model file is on the Hub repo. instead.
         try:
             model = model_architecture.from_pretrained(repo_id)
         except Exception:
