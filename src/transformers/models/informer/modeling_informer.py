3bc616394e2c88e83ab02f97a3357cffda1f38b8
elisim <elisimhayev@gmail.com>
2023-03-01 19:51:55 +0700

    TimeSeriesTansformer->Informer in decoder's Copied from

--- src/transformers/models/informer/modeling_informer.py
+++ src/transformers/models/informer/modeling_informer.py
@@ -1576,7 +1576,7 @@ class InformerEncoder(InformerPreTrainedModel):
         )
 
 
-# Copied from transformers.models.time_series_transformer.modeling_time_series_transformer.TimeSeriesTransformerDecoder with TimeSeriesTransformer->Informer,TimeSeriesTransformerConfig->InformerConfig,TIME_SERIES_TRANSFORMER->INFORMER,time-series-transformer->informer,Transformer->Informer
+# Copied from transformers.models.time_series_transformer.modeling_time_series_transformer.TimeSeriesTransformerDecoder with TimeSeriesTransformer->Informer,TimeSeriesTransformerConfig->InformerConfig,TIME_SERIES_TRANSFORMER->INFORMER,time-series-transformer->informer,TimeSeriesTansformer->Informer
 class InformerDecoder(InformerPreTrainedModel):
     """
     Informer decoder consisting of *config.decoder_layers* layers. Each layer is a [`InformerDecoderLayer`]
