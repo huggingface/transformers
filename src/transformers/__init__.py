f7c618e3b0652a6a0f6e435c404288fc38d18dda
Matt <Rocketknight1@users.noreply.github.com>
2023-03-01 18:00:48 +0000

    Add TFVisionTextDualEncoder (#21873)
    * Temporary commit to stash everything so far
    
    * Temporary commit to stash everything so far
    
    * stash commit
    
    * Refactor from_pretrained
    
    * Fix final test, make fixup
    
    * Update dummies
    
    * Add model to TEST_FILES_WITH_NO_COMMON_TESTS
    
    * Update src/transformers/models/vision_text_dual_encoder/modeling_tf_vision_text_dual_encoder.py
    
    Co-authored-by: Joao Gante <joaofranciscocardosogante@gmail.com>
    
    * Update src/transformers/models/vision_text_dual_encoder/modeling_tf_vision_text_dual_encoder.py
    
    Co-authored-by: Joao Gante <joaofranciscocardosogante@gmail.com>
    
    * Update src/transformers/models/vision_text_dual_encoder/modeling_tf_vision_text_dual_encoder.py
    
    Co-authored-by: Joao Gante <joaofranciscocardosogante@gmail.com>
    
    * Update src/transformers/models/vision_text_dual_encoder/modeling_tf_vision_text_dual_encoder.py
    
    Co-authored-by: Joao Gante <joaofranciscocardosogante@gmail.com>
    
    * Add TFVisionTextDualEncoder to utils/documentation_tests.txt
    
    * make fixup
    
    ---------
    
    Co-authored-by: Joao Gante <joaofranciscocardosogante@gmail.com>

--- src/transformers/__init__.py
+++ src/transformers/__init__.py
@@ -3275,6 +3275,7 @@ else:
         ]
     )
     _import_structure["models.vision_encoder_decoder"].extend(["TFVisionEncoderDecoderModel"])
+    _import_structure["models.vision_text_dual_encoder"].extend(["TFVisionTextDualEncoderModel"])
     _import_structure["models.vit"].extend(
         [
             "TFViTForImageClassification",
@@ -6335,6 +6336,7 @@ if TYPE_CHECKING:
             TFTransfoXLPreTrainedModel,
         )
         from .models.vision_encoder_decoder import TFVisionEncoderDecoderModel
+        from .models.vision_text_dual_encoder import TFVisionTextDualEncoderModel
         from .models.vit import TFViTForImageClassification, TFViTModel, TFViTPreTrainedModel
         from .models.vit_mae import TFViTMAEForPreTraining, TFViTMAEModel, TFViTMAEPreTrainedModel
         from .models.wav2vec2 import (
