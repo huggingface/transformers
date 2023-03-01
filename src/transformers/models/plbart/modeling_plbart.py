98f5bc4c3a87265d35f57232da4da2f4baa92858
saswatmeher <35535056+saswatmeher@users.noreply.github.com>
2023-03-01 17:11:58 +0530

    Fix gradient checkpointing bug Bart (#21866)
    Co-authored-by: saswatmeher <saswatmeher@cse.iitb.ac.in>

--- src/transformers/models/plbart/modeling_plbart.py
+++ src/transformers/models/plbart/modeling_plbart.py
@@ -1021,6 +1021,13 @@ class PLBartDecoder(PLBartPreTrainedModel):
 
         hidden_states = nn.functional.dropout(hidden_states, p=self.dropout, training=self.training)
 
+        if self.gradient_checkpointing and self.training:
+            if use_cache:
+                logger.warning_once(
+                    "`use_cache=True` is incompatible with gradient checkpointing. Setting `use_cache=False`..."
+                )
+                use_cache = False
+
         # decoder layers
         all_hidden_states = () if output_hidden_states else None
         all_self_attns = () if output_attentions else None
@@ -1047,11 +1054,6 @@ class PLBartDecoder(PLBartPreTrainedModel):
             past_key_value = past_key_values[idx] if past_key_values is not None else None
 
             if self.gradient_checkpointing and self.training:
-                if use_cache:
-                    logger.warning_once(
-                        "`use_cache=True` is incompatible with gradient checkpointing. Setting `use_cache=False`..."
-                    )
-                    use_cache = False
 
                 def create_custom_forward(module):
                     def custom_forward(*inputs):
