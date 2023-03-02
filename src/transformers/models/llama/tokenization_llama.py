3be1e4aba928eaaa91a6fc0e9d5daaece86c113a
thomasw21 <24695242+thomasw21@users.noreply.github.com>
2023-03-02 16:27:44 +0100

    Test something on the tokenizer

--- src/transformers/models/llama/tokenization_llama.py
+++ src/transformers/models/llama/tokenization_llama.py
@@ -8,19 +8,9 @@ from ... import PreTrainedTokenizer
 from ...utils import logging
 
 
-if TYPE_CHECKING:
-    from transformers.pipelines.conversational import Conversation
-
-
 logger = logging.get_logger(__name__)
 
 VOCAB_FILES_NAMES = {"vocab_file": "tokenizer.model"}
-
-PRETRAINED_VOCAB_FILES_MAP = {
-}
-
-PRETRAINED_POSITIONAL_EMBEDDINGS_SIZES = {
-}
 
 
 class LLaMaTokenizer(PreTrainedTokenizer):
@@ -48,8 +38,6 @@ class LLaMaTokenizer(PreTrainedTokenizer):
     """
 
     vocab_files_names = VOCAB_FILES_NAMES
-    pretrained_vocab_files_map = PRETRAINED_VOCAB_FILES_MAP
-    max_model_input_sizes = PRETRAINED_POSITIONAL_EMBEDDINGS_SIZES
     model_input_names = ["input_ids", "attention_mask"]
 
     def __init__(
@@ -61,6 +49,20 @@ class LLaMaTokenizer(PreTrainedTokenizer):
         **kwargs
     ) -> None:
         self.sp_model = spm.SentencePieceProcessor(model_file=vocab_file)
+        self.special_tokens = {
+            bos_token: self.sp_model.bos_id(),
+            eos_token: self.sp_model.eos_id(),
+            unk_token: self.sp_model.unk_id()
+        }
+        for special_token, token_id in self.special_tokens.items():
+            # special token doesn't exist in the sentencepiece tokenizer
+            try:
+                self.sp_model.IdToPiece(special_token)
+                assert False
+            except IndexError:
+                pass
+            assert token_id >= 0
+
         # TODO @thomasw21: Understand if I need to have <bos> and such since they are not part of the official LLaMa model
         super().__init__(
             bos_token=bos_token,
@@ -85,12 +87,14 @@ class LLaMaTokenizer(PreTrainedTokenizer):
 
     def _tokenize(self, text: str) -> List[str]:
         """Take as input a string and return a list of strings (tokens) for words/sub-words"""
-        return [self.bos_token_id] + self.sp_model.encode(text, out_type=str)
+        return [self.bos_token] + self.sp_model.encode(text, out_type=str)
 
     def _convert_token_to_id(self, token: str):
         """Converts a token (str) in an id using the vocab."""
         # TODO @thomasw21: This means that you can't get the tokens from <bos>/<eos>/<unk>
         # The issue is that you can tokenizer <
+        if token in self.special_tokens:
+            return self.special_tokens
         return self.sp_model.piece_to_id(token)
 
     def _convert_id_to_token(self, index):
