e1769dcb3b9815e75813ee54efa589d648a3d9db
Kashif Rasul <kashif.rasul@gmail.com>
2023-03-02 14:49:26 +0100

    fix typo in Bart's attention (#21898)

--- src/transformers/models/time_series_transformer/modeling_time_series_transformer.py
+++ src/transformers/models/time_series_transformer/modeling_time_series_transformer.py
@@ -764,7 +764,7 @@ class TimeSeriesTransformerAttention(nn.Module):
 
         if attn_output.size() != (bsz * self.num_heads, tgt_len, self.head_dim):
             raise ValueError(
-                f"`attn_output` should be of size {(bsz, self.num_heads, tgt_len, self.head_dim)}, but is"
+                f"`attn_output` should be of size {(bsz * self.num_heads, tgt_len, self.head_dim)}, but is"
                 f" {attn_output.size()}"
             )
 
@@ -772,7 +772,7 @@ class TimeSeriesTransformerAttention(nn.Module):
         attn_output = attn_output.transpose(1, 2)
 
         # Use the `embed_dim` from the config (stored in the class) rather than `hidden_state` because `attn_output` can be
-        # partitioned aross GPUs when using tensor-parallelism.
+        # partitioned across GPUs when using tensor-parallelism.
         attn_output = attn_output.reshape(bsz, tgt_len, self.embed_dim)
 
         attn_output = self.out_proj(attn_output)
