d3465c825b138e4b6a9d83d16d5de9bcab31e7fb
elisim <elisimhayev@gmail.com>
2023-03-02 10:27:54 +0700

    added `AutoformerLayernorm` and `AutoformerSeriesDecomposition`

--- src/transformers/models/autoformer/modeling_autoformer.py
+++ src/transformers/models/autoformer/modeling_autoformer.py
@@ -536,6 +536,53 @@ class SampleTimeSeriesPredictionOutput(ModelOutput):
     sequences: torch.FloatTensor = None
 
 
+# Eli to Kashif: class based on
+# https://github.com/thuml/Autoformer/blob/c6a0694ff484753f2d986cc0bb1f99ee850fc1a8/layers/Autoformer_EncDec.py#L39
+# where AutoformerSeriesDecomposition is series_decomp + moving_avg
+class AutoformerSeriesDecomposition(nn.Module):
+    """
+    Series Decomposition block, to highlight the trend of time series:
+
+        x_trend = AvgPool(Padding(X))
+        x_seasonal = X - x_trend
+    """
+    def __init__(self, kernel_size):
+        super(AutoformerSeriesDecomposition, self).__init__()
+        self.kernel_size = kernel_size
+        self.avg = nn.AvgPool1d(kernel_size=kernel_size, stride=1, padding=0)
+
+    def forward(self, x):
+        # padding on the both ends of time series
+        front = x[:, 0:1, :].repeat(1, (self.kernel_size - 1) // 2, 1)
+        end = x[:, -1:, :].repeat(1, (self.kernel_size - 1) // 2, 1)
+        x = torch.cat([front, x, end], dim=1)
+
+        # pooling
+        x = self.avg(x.permute(0, 2, 1))
+        x_trend = x.permute(0, 2, 1)
+
+        # calculate x_seasonal
+        x_seasonal = x - x_trend
+        return x_seasonal, x_trend
+
+# Eli to Kashif: class based on
+# https://github.com/thuml/Autoformer/blob/c6a0694ff484753f2d986cc0bb1f99ee850fc1a8/layers/Autoformer_EncDec.py#L6
+# where AutoformerLayernorm is my_Layernorm
+class AutoformerLayernorm(nn.Module):
+    """
+    Special designed layer normalization for the seasonal part, calculated as:
+    AutoformerLayernorm(x) = nn.LayerNorm(x) - torch.mean(nn.LayerNorm(x))
+    """
+    def __init__(self, channels):
+        super(AutoformerLayernorm, self).__init__()
+        self.layernorm = nn.LayerNorm(channels)
+
+    def forward(self, x):
+        x_hat = self.layernorm(x)
+        bias = torch.mean(x_hat, dim=1).unsqueeze(1).repeat(1, x.shape[1], 1)
+        return x_hat - bias
+
+
 # Copied from transformers.models.bart.modeling_bart.BartAttention with Bart->Autoformer
 class AutoformerAttention(nn.Module):
     """Multi-headed attention from 'Attention Is All You Need' paper"""
