e17ed99fee06372b2413af314c89ca559acdda93
thomasw21 <24695242+thomasw21@users.noreply.github.com>
2023-03-03 17:03:10 +0100

    Missing save_vocabulary

--- src/transformers/models/llama/tokenization_llama.py
+++ src/transformers/models/llama/tokenization_llama.py
@@ -13,6 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """Tokenization classes for LLaMa."""
+import os
+from shutil import copyfile
 from typing import List, Optional, Union
 
 import sentencepiece as spm
@@ -161,3 +163,21 @@ class LLaMaTokenizer(PreTrainedTokenizer):
                 prev_is_special = False
         out_string += self.sp_model.decode(current_sub_tokens)
         return out_string.strip()
+
+    def save_vocabulary(self, save_directory: str, filename_prefix: Optional[str] = None) -> Tuple[str]:
+        if not os.path.isdir(save_directory):
+            logger.error(f"Vocabulary path ({save_directory}) should be a directory")
+            return
+        out_vocab_file = os.path.join(
+            save_directory, (filename_prefix + "-" if filename_prefix else "") + VOCAB_FILES_NAMES["vocab_file"]
+        )
+
+        if os.path.abspath(self.vocab_file) != os.path.abspath(out_vocab_file) and os.path.isfile(self.vocab_file):
+            copyfile(self.vocab_file, out_vocab_file)
+        elif not os.path.isfile(self.vocab_file):
+            with open(out_vocab_file, "wb") as fi:
+                content_spiece_model = self.sp_model.serialized_model_proto()
+                fi.write(content_spiece_model)
+
+        return (out_vocab_file,)
+
