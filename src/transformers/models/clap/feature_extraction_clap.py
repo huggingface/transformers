a6af8c2a8539b8a7c9d3d01ab67639480d9cc32f
Arthur <48595927+ArthurZucker@users.noreply.github.com>
2023-03-03 18:42:18 +0100

    [CLAP] Support batched inputs for CLAP. Fixes pipeline issues (#21931)
    * fix pipeline
    
    * fix feature_extraction clap
    
    * you can now batch the `is_longer` attribute
    
    * add tests
    
    * fixup
    
    * add expected scores
    
    * comment on is_longert

--- src/transformers/models/clap/feature_extraction_clap.py
+++ src/transformers/models/clap/feature_extraction_clap.py
@@ -347,6 +347,9 @@ class ClapFeatureExtractor(SequenceFeatureExtractor):
         if isinstance(input_mel[0], List):
             input_mel = [np.asarray(feature, dtype=np.float64) for feature in input_mel]
 
+        # is_longer is a list of bool
+        is_longer = [[longer] for longer in is_longer]
+
         input_features = {"input_features": input_mel, "is_longer": is_longer}
         input_features = BatchFeature(input_features)
 
