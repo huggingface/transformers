21c54c0cc52367c58a36a5ccb35614536c7dc5a3
Karim Foda <35491698+KMFODA@users.noreply.github.com>
2023-03-03 11:49:27 +0000

    Fix gradient checkpointing bug in MBart (#21918)

--- src/transformers/models/mbart/modeling_mbart.py
+++ src/transformers/models/mbart/modeling_mbart.py
@@ -1043,6 +1043,13 @@ class MBartDecoder(MBartPreTrainedModel):
 
         hidden_states = nn.functional.dropout(hidden_states, p=self.dropout, training=self.training)
 
+        if self.gradient_checkpointing and self.training:
+            if use_cache:
+                logger.warning_once(
+                    "`use_cache=True` is incompatible with gradient checkpointing`. Setting `use_cache=False`..."
+                )
+                use_cache = False
+
         # decoder layers
         all_hidden_states = () if output_hidden_states else None
         all_self_attns = () if output_attentions else None
@@ -1068,11 +1075,6 @@ class MBartDecoder(MBartPreTrainedModel):
             past_key_value = past_key_values[idx] if past_key_values is not None else None
 
             if self.gradient_checkpointing and self.training:
-                if use_cache:
-                    logger.warning_once(
-                        "`use_cache=True` is incompatible with gradient checkpointing`. Setting `use_cache=False`..."
-                    )
-                    use_cache = False
 
                 def create_custom_forward(module):
                     def custom_forward(*inputs):
