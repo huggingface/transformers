40b0b5d4955fec7664f3608b952b80ef49b12bed
Karim Foda <35491698+KMFODA@users.noreply.github.com>
2023-03-04 16:43:32 +0100

    Fix gradient checkpointing bug in Pegasus (#21944)

--- src/transformers/models/pegasus_x/modeling_pegasus_x.py
+++ src/transformers/models/pegasus_x/modeling_pegasus_x.py
@@ -1293,6 +1293,13 @@ class PegasusXDecoder(PegasusXPreTrainedModel):
 
         hidden_states = nn.functional.dropout(hidden_states, p=self.dropout, training=self.training)
 
+        if self.gradient_checkpointing and self.training:
+            if use_cache:
+                logger.warning_once(
+                    "`use_cache=True` is incompatible with gradient checkpointing. Setting `use_cache=False`..."
+                )
+                use_cache = False
+
         # decoder layers
         all_hidden_states = () if output_hidden_states else None
         all_self_attns = () if output_attentions else None
@@ -1310,11 +1317,6 @@ class PegasusXDecoder(PegasusXPreTrainedModel):
             past_key_value = past_key_values[idx] if past_key_values is not None else None
 
             if self.gradient_checkpointing and self.training:
-                if use_cache:
-                    logger.warning_once(
-                        "`use_cache=True` is incompatible with gradient checkpointing. Setting `use_cache=False`..."
-                    )
-                    use_cache = False
 
                 def create_custom_forward(module):
                     def custom_forward(*inputs):
