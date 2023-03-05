8205cdb6309de9a85758ce5e221371986dc6a622
novice <pranavpulijala@gmail.com>
2023-03-05 14:29:19 -0600

    Update compile.py

--- src/transformers/models/mra/compile.py
+++ src/transformers/models/mra/compile.py
@@ -6,6 +6,6 @@ curr_path = os.path.dirname(os.path.realpath(__file__))
 src_files = ["cuda_kernel.cu", "cuda_launch.cu", "torch_extension.cpp"]
 src_files = [os.path.join(curr_path, file) for file in src_files]
 
-setup(name = 'cuda_kernel', ext_modules=[cpp_extension.CUDAExtension(name = "kernel", sources = src_files)],
+setup(name = 'kernel_op', ext_modules=[cpp_extension.CUDAExtension(name = "cuda_kernel", sources = src_files)],
       cmdclass={'build_ext': cpp_extension.BuildExtension})
 
