a82e47c4d072624ff7c010f73c4e5dc10e11cf13
Stella Biderman <stellabiderman@gmail.com>
2023-03-06 21:14:27 -0500

    Update tokenization_llama.py

--- src/transformers/models/llama/tokenization_llama.py
+++ src/transformers/models/llama/tokenization_llama.py
@@ -1,5 +1,5 @@
 # coding=utf-8
-# Copyright 2022 The FAIR team of Meta AI and The HuggingFace Inc. team. All rights reserved.
+# Copyright 2022 EleutherAI and The HuggingFace Inc. team. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,6 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 """Tokenization classes for LLaMA."""
 import os
 import re
