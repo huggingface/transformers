4931cbb5bb3782b8937898dc90d2c18b761a8c3f
Somasree Majumder <56045049+soma2000-lang@users.noreply.github.com>
2023-03-09 00:51:38 +0530

    fixes the gradient checkpointing of whisper (#22019)
    * fixing
    
    * Update modeling_whisper.py
    
    * Update modeling_whisper.py
    
    * Update src/transformers/models/whisper/modeling_whisper.py
    
    ---------
    
    Co-authored-by: Joao Gante <joaofranciscocardosogante@gmail.com>

--- src/transformers/models/whisper/modeling_whisper.py
+++ src/transformers/models/whisper/modeling_whisper.py
@@ -1029,6 +1029,12 @@ class WhisperDecoder(WhisperPreTrainedModel):
         hidden_states = inputs_embeds + positions
         hidden_states = nn.functional.dropout(hidden_states, p=self.dropout, training=self.training)
 
+        if self.gradient_checkpointing and self.training:
+            if use_cache:
+                logger.warning_once(
+                    "`use_cache = True` is incompatible with gradient checkpointing. Setting `use_cache = False`..."
+                )
+                use_cache = False
         # decoder layers
         all_hidden_states = () if output_hidden_states else None
         all_self_attns = () if output_attentions else None
@@ -1053,12 +1059,6 @@ class WhisperDecoder(WhisperPreTrainedModel):
             past_key_value = past_key_values[idx] if past_key_values is not None else None
 
             if self.gradient_checkpointing and self.training:
-                if use_cache:
-                    logger.warning(
-                        "`use_cache = True` is incompatible with gradient checkpointing. Setting `use_cache ="
-                        " False`..."
-                    )
-                    use_cache = False
 
                 def create_custom_forward(module):
                     def custom_forward(*inputs):
