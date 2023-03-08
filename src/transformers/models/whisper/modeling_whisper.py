674c1d3d2b28f418d6f91d4948bfc01652300ca0
Joao Gante <joaofranciscocardosogante@gmail.com>
2023-03-08 18:52:32 +0000

    Update src/transformers/models/whisper/modeling_whisper.py

--- src/transformers/models/whisper/modeling_whisper.py
+++ src/transformers/models/whisper/modeling_whisper.py
@@ -1032,7 +1032,7 @@ class WhisperDecoder(WhisperPreTrainedModel):
         if self.gradient_checkpointing and self.training:
             if use_cache:
                 logger.warning_once(
-                    "`use_cache = True` is incompatible with gradient checkpointing. Setting `use_cache =" " False`..."
+                    "`use_cache = True` is incompatible with gradient checkpointing. Setting `use_cache = False`..."
                 )
                 use_cache = False
         # decoder layers
