0d08783b96591d3077bbe73860dd9be50f2898c4
Nipun Jindal <jindal.nipun@gmail.com>
2023-03-09 17:47:44 +0530

    [21737][T5]: Fix gradient checkpoint bug (#22036)
    * [21737][T5]: Fix gradient checkpoint bug
    
    * [21737][T5]: Fix gradient checkpoint bug
    
    * [21737][T5]: Fix gradient checkpoint bug
    
    * Update src/transformers/models/mt5/modeling_mt5.py
    
    * Update src/transformers/models/t5/modeling_t5.py
    
    ---------
    
    Co-authored-by: njindal <njindal@adobe.com>
    Co-authored-by: Joao Gante <joaofranciscocardosogante@gmail.com>

--- src/transformers/models/mt5/modeling_mt5.py
+++ src/transformers/models/mt5/modeling_mt5.py
@@ -978,6 +978,13 @@ class MT5Stack(MT5PreTrainedModel):
         else:
             encoder_extended_attention_mask = None
 
+        if self.gradient_checkpointing and self.training:
+            if use_cache:
+                logger.warning_once(
+                    "`use_cache=True` is incompatible with gradient checkpointing. Setting `use_cache=False`..."
+                )
+                use_cache = False
+
         # Prepare head mask if needed
         head_mask = self.get_head_mask(head_mask, self.config.num_layers)
         cross_attn_head_mask = self.get_head_mask(cross_attn_head_mask, self.config.num_layers)
@@ -1015,11 +1022,6 @@ class MT5Stack(MT5PreTrainedModel):
                 all_hidden_states = all_hidden_states + (hidden_states,)
 
             if self.gradient_checkpointing and self.training:
-                if use_cache:
-                    logger.warning_once(
-                        "`use_cache=True` is incompatible with gradient checkpointing. Setting `use_cache=False`..."
-                    )
-                    use_cache = False
 
                 def create_custom_forward(module):
                     def custom_forward(*inputs):
