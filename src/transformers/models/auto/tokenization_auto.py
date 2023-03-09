ead16dcb50089a0a142540b267e29770b9772454
younesbelakda <younesbelkada@gmail.com>
2023-03-09 13:08:01 +0000

    fix tokenizer

--- src/transformers/models/auto/tokenization_auto.py
+++ src/transformers/models/auto/tokenization_auto.py
@@ -246,7 +246,7 @@ else:
                 ),
             ),
             ("phobert", ("PhobertTokenizer", None)),
-            ("pix2struct", ("Pix2StructTokenizer", "Pix2StructTokenizerFast" if is_tokenizers_available() else None)),
+            ("pix2struct", ("T5Tokenizer", "T5TokenizerFast" if is_tokenizers_available() else None)),
             ("plbart", ("PLBartTokenizer" if is_sentencepiece_available() else None, None)),
             ("prophetnet", ("ProphetNetTokenizer", None)),
             ("qdqbert", ("BertTokenizer", "BertTokenizerFast" if is_tokenizers_available() else None)),
