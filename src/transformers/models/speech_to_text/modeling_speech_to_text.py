c52a7a22417153d4810958631eed12ffb85e8093
Karim Foda <35491698+KMFODA@users.noreply.github.com>
2023-03-10 12:30:42 +0100

    Fix gradient checkpointing bug in Speech2Text (#22079)
    * Fix gradient checkpointing bug in Speech2Text
    
    * Update modeling_speech_to_text.py
    
    * Update modeling_speech_to_text_2.py

--- src/transformers/models/speech_to_text/modeling_speech_to_text.py
+++ src/transformers/models/speech_to_text/modeling_speech_to_text.py
@@ -1024,6 +1024,13 @@ class Speech2TextDecoder(Speech2TextPreTrainedModel):
         hidden_states = inputs_embeds + positions
         hidden_states = nn.functional.dropout(hidden_states, p=self.dropout, training=self.training)
 
+        if self.gradient_checkpointing and self.training:
+            if use_cache:
+                logger.warning_once(
+                    "`use_cache = True` is incompatible with gradient checkpointing. Setting `use_cache =" " False`..."
+                )
+                use_cache = False
+
         # decoder layers
         all_hidden_states = () if output_hidden_states else None
         all_self_attns = () if output_attentions else None
@@ -1048,12 +1055,6 @@ class Speech2TextDecoder(Speech2TextPreTrainedModel):
             past_key_value = past_key_values[idx] if past_key_values is not None else None
 
             if self.gradient_checkpointing and self.training:
-                if use_cache:
-                    logger.warning(
-                        "`use_cache = True` is incompatible with gradient checkpointing. Setting `use_cache ="
-                        " False`..."
-                    )
-                    use_cache = False
 
                 def create_custom_forward(module):
                     def custom_forward(*inputs):
