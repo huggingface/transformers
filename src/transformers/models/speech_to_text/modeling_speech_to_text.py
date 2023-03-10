35dda5e3329169dcac5c0366dc74d9bb0d00d4cc
Karim Foda <35491698+KMFODA@users.noreply.github.com>
2023-03-10 12:12:38 +0100

    Update modeling_speech_to_text.py

--- src/transformers/models/speech_to_text/modeling_speech_to_text.py
+++ src/transformers/models/speech_to_text/modeling_speech_to_text.py
@@ -1026,7 +1026,7 @@ class Speech2TextDecoder(Speech2TextPreTrainedModel):
 
         if self.gradient_checkpointing and self.training:
             if use_cache:
-                logger.warning(
+                logger.warning_once(
                     "`use_cache = True` is incompatible with gradient checkpointing. Setting `use_cache =" " False`..."
                 )
                 use_cache = False
