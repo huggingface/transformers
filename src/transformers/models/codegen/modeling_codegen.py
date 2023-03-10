6c5285987e2e663c02e4b8c8a9710e70cc5e1769
Nick Hill <nickhill@us.ibm.com>
2023-03-10 09:38:01 -0800

    Fix "Copied from" comment referencing wrong function

--- src/transformers/models/codegen/modeling_codegen.py
+++ src/transformers/models/codegen/modeling_codegen.py
@@ -51,7 +51,7 @@ CODEGEN_PRETRAINED_MODEL_ARCHIVE_LIST = [
 ]
 
 
-# Copied from transformers.models.gptj.modeling_gptj.apply_rotary_pos_emb
+# Copied from transformers.models.gptj.modeling_gptj.create_sinusoidal_positions
 def create_sinusoidal_positions(num_pos: int, dim: int) -> torch.Tensor:
     inv_freq = 1.0 / (10000 ** (torch.arange(0, dim, 2) / dim))
     sinusoid_inp = torch.einsum("i , j -> i j", torch.arange(num_pos, dtype=torch.float), inv_freq).float()
