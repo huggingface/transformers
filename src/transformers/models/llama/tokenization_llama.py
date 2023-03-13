8eefcac63776fcd8f5a80038039725e46f138bb9
Jason Phang <jasonphang@nyu.edu>
2023-03-13 17:23:26 -0400

    fix vocab caching logic

--- src/transformers/models/llama/tokenization_llama.py
+++ src/transformers/models/llama/tokenization_llama.py
@@ -76,8 +76,8 @@ class LLaMATokenizer(PreTrainedTokenizer):
 
     @property
     def no_prefix_space_tokens(self):
-        vocab = self.convert_ids_to_tokens(list(range(self.vocab_size)))
         if self._no_prefix_space_tokens is None:
+            vocab = self.convert_ids_to_tokens(list(range(self.vocab_size)))
             self._no_prefix_space_tokens = {i for i, tok in enumerate(vocab) if not tok.startswith("▁")}
         return self._no_prefix_space_tokens
 
