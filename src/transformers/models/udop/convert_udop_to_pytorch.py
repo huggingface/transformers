5dc6455f8ec5bd6a512a0a8466260aa6df3ef21f
Niels <niels.rogge1@gmail.com>
2023-03-13 14:09:03 +0100

    Extend conversion script

--- src/transformers/models/udop/convert_udop_to_pytorch.py
+++ src/transformers/models/udop/convert_udop_to_pytorch.py
@@ -1,6 +1,22 @@
 import torch
+from huggingface_hub import hf_hub_download
+from PIL import Image
+from torchvision import transforms as T
 
-from transformers import UdopConfig, UdopForConditionalGeneration
+from transformers import T5Tokenizer, UdopConfig, UdopForConditionalGeneration
+
+
+def transform(image, image_size=224):
+    trans = T.Compose(
+        [
+            T.Resize([image_size, image_size]),
+            T.ToTensor(),
+            T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        ]
+    )
+
+    image = trans(image)  # copy to make it writeable
+    return image
 
 
 state_dict = torch.load("/Users/nielsrogge/Downloads/udop-unimodel-large-224/pytorch_model.bin", map_location="cpu")
@@ -30,9 +46,18 @@ print("Looks ok!")
 
 # single forward pass
 print("Testing single forward pass..")
-input_ids = torch.tensor([[101, 102]])
-seg_data = torch.tensor([[[0, 0, 0, 0], [1, 2, 3, 4]]]).float()
-image = torch.randn(1, 3, 224, 224)
+
+question = "Question answering. What is the title?"
+tokenizer = T5Tokenizer.from_pretrained("t5-base")
+input_ids = tokenizer(question, return_tensors="pt").input_ids
+# input_ids = torch.tensor([[101, 102]])
+
+seg_data = torch.tensor([[[0, 0, 0, 0] for _ in range(input_ids.shape[1])]]).float()
+filepath = hf_hub_download(
+    repo_id="hf-internal-testing/fixtures_docvqa", filename="document_2.png", repo_type="dataset"
+)
+image = Image.open(filepath).convert("RGB")
+image = transform(image).unsqueeze(0)
 decoder_input_ids = torch.tensor([[101]])
 
 with torch.no_grad():
@@ -42,3 +67,5 @@ with torch.no_grad():
 print("Testing generation...")
 model_kwargs = {"seg_data": seg_data, "image": image}
 outputs = model.generate(input_ids=input_ids, **model_kwargs, max_new_tokens=20)
+
+print("Generated:", tokenizer.batch_decode(outputs, skip_special_tokens=True))
