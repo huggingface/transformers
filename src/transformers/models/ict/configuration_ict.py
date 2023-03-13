6e8aa16288cfaf7618bca0c10aeecc720bd63dba
Sheon Han <sheon.han@gmail.com>
2023-03-12 23:04:32 -0700

    tweak configuration

--- src/transformers/models/ict/configuration_ict.py
+++ src/transformers/models/ict/configuration_ict.py
@@ -27,7 +27,7 @@ from ...utils import logging
 logger = logging.get_logger(__name__)
 
 ICT_PRETRAINED_CONFIG_ARCHIVE_MAP = {
-    "sheonhan/image-completion-transformer": "https://huggingface.co/sheonhan/image-completion-transformer/resolve/main/config.json",
+    "sheonhan/ict-imagenet-32": "https://huggingface.co/sheonhan/ict-imagenet-32/resolve/main/config.json",
 }
 
 
@@ -36,53 +36,53 @@ class ICTConfig(PretrainedConfig):
     r"""
     This is the configuration class to store the configuration of a [`ICTModel`]. It is used to instantiate an ICT
     model according to the specified arguments, defining the model architecture. Instantiating a configuration with the
-    defaults will yield a similar configuration to that of the ICT
-    [google/ict-base-patch16-224](https://huggingface.co/google/ict-base-patch16-224) architecture.
+    defaults will yield a similar configuration to that of the [ICT model trained with the ImageNet dataset](https://huggingface.co/sheonhan/ict-imagenet-32).
 
     Configuration objects inherit from [`PretrainedConfig`] and can be used to control the model outputs. Read the
     documentation from [`PretrainedConfig`] for more information.
 
 
     Args:
-        hidden_size (`int`, *optional*, defaults to 768):
-            Dimensionality of the encoder layers and the pooler layer.
-        num_hidden_layers (`int`, *optional*, defaults to 12):
+        vocab_size (`int`, *optional*, defaults to 512):
+            Vocabulary size of the ICT model. Defines the number of different tokens that can be represented by the
+            `pixel_values` passed when calling [`ICTModel`].
+        hidden_size (`int`, *optional*, defaults to 1024):
+            Dimensionality of the embeddings and hidden states.
+        num_hidden_layers (`int`, *optional*, defaults to 35):
             Number of hidden layers in the Transformer encoder.
-        num_attention_heads (`int`, *optional*, defaults to 12):
+        num_attention_heads (`int`, *optional*, defaults to 8):
             Number of attention heads for each attention layer in the Transformer encoder.
-        intermediate_size (`int`, *optional*, defaults to 3072):
+        intermediate_size (`int`, *optional*, defaults to 4096):
             Dimensionality of the "intermediate" (i.e., feed-forward) layer in the Transformer encoder.
-        hidden_act (`str` or `function`, *optional*, defaults to `"gelu"`):
-            The non-linear activation function (function or string) in the encoder and pooler. If string, `"gelu"`,
-            `"relu"`, `"selu"` and `"gelu_new"` are supported.
-        hidden_dropout_prob (`float`, *optional*, defaults to 0.1):
+        activation_function (`str`, *optional*, defaults to `"gelu"`):
+            Activation function (can be one of the activation functions defined in src/transformers/activations.py).
+            Defaults to "quick_gelu".
+        embedding_dropout_prob (`int`, *optional*, defaults to 0.0):
+            The dropout ratio for the embeddings.
+        residual_dropout_prob (`float`, *optional*, defaults to 0.0):
             The dropout probability for all fully connected layers in the embeddings, encoder, and pooler.
-        attention_probs_dropout_prob (`float`, *optional*, defaults to 0.1):
+        attention_probs_dropout_prob (`float`, *optional*, defaults to 0.0):
             The dropout ratio for the attention probabilities.
         initializer_range (`float`, *optional*, defaults to 0.02):
             The standard deviation of the truncated_normal_initializer for initializing all weight matrices.
         layer_norm_eps (`float`, *optional*, defaults to 1e-12):
             The epsilon used by the layer normalization layers.
-        image_size (`int`, *optional*, defaults to `224`):
+        image_size (`int`, *optional*, defaults to `32`):
             The size (resolution) of each image.
-        patch_size (`int`, *optional*, defaults to `16`):
-            The size (resolution) of each patch.
         num_channels (`int`, *optional*, defaults to `3`):
             The number of input channels.
-        qkv_bias (`bool`, *optional*, defaults to `True`):
+        qkv_bias (`bool`, *optional*, defaults to `False`):
             Whether to add a bias to the queries, keys and values.
-        encoder_stride (`int`, `optional`, defaults to 16):
-           Factor to increase the spatial resolution by in the decoder head for masked image modeling.
 
     Example:
 
     ```python
     >>> from transformers import ICTConfig, ICTModel
 
-    >>> # Initializing a ICT ict-base-patch16-224 style configuration
+    >>> # Initializing a ICT ict-imagenet-32 style configuration
     >>> configuration = ICTConfig()
 
-    >>> # Initializing a model (with random weights) from the ict-base-patch16-224 style configuration
+    >>> # Initializing a model (with random weights) from the ict-imagenet-32 style configuration
     >>> model = ICTModel(configuration)
 
     >>> # Accessing the model configuration
@@ -92,38 +92,39 @@ class ICTConfig(PretrainedConfig):
 
     def __init__(
         self,
+        vocab_size=512,
         hidden_size=768,
-        num_hidden_layers=12,
-        num_attention_heads=12,
-        intermediate_size=3072,
-        hidden_act="gelu",
-        hidden_dropout_prob=0.0,
+        num_hidden_layers=35,
+        num_attention_heads=8,
+        intermediate_size=4096,
+        activation_function="gelu",
+        embedding_dropout_prob=0.0,
+        residual_dropout_prob=0.0,
         attention_probs_dropout_prob=0.0,
         initializer_range=0.02,
         layer_norm_eps=1e-12,
-        image_size=224,
-        patch_size=16,
+        image_size=32,
         num_channels=3,
-        qkv_bias=True,
-        encoder_stride=16,
+        qkv_bias=False,
         **kwargs,
     ):
         super().__init__(**kwargs)
 
+        self.vocab_size = vocab_size
         self.hidden_size = hidden_size
         self.num_hidden_layers = num_hidden_layers
         self.num_attention_heads = num_attention_heads
         self.intermediate_size = intermediate_size
-        self.hidden_act = hidden_act
-        self.hidden_dropout_prob = hidden_dropout_prob
+        self.activation_function = activation_function
+        self.embedding_dropout_prob = embedding_dropout_prob
+        self.residual_dropout_prob = residual_dropout_prob
         self.attention_probs_dropout_prob = attention_probs_dropout_prob
         self.initializer_range = initializer_range
         self.layer_norm_eps = layer_norm_eps
         self.image_size = image_size
-        self.patch_size = patch_size
+        self.block_size = self.image_size * self.image_size
         self.num_channels = num_channels
         self.qkv_bias = qkv_bias
-        self.encoder_stride = encoder_stride
 
 
 class ICTOnnxConfig(OnnxConfig):
