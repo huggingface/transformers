1b1d17a6fcc02a43e492b35cff741ac3d8774ecd
Karim Foda <35491698+KMFODA@users.noreply.github.com>
2023-03-13 13:52:34 +0000

    Fix gradient checkpointing bug in xlm_roberta_xl (#22128)

--- src/transformers/models/xlm_roberta_xl/modeling_xlm_roberta_xl.py
+++ src/transformers/models/xlm_roberta_xl/modeling_xlm_roberta_xl.py
@@ -478,6 +478,12 @@ class XLMRobertaXLEncoder(nn.Module):
         output_hidden_states=False,
         return_dict=True,
     ):
+        if self.gradient_checkpointing and self.training:
+            if use_cache:
+                logger.warning_once(
+                    "`use_cache=True` is incompatible with gradient checkpointing. Setting `use_cache=False`..."
+                )
+                use_cache = False
         all_hidden_states = () if output_hidden_states else None
         all_self_attentions = () if output_attentions else None
         all_cross_attentions = () if output_attentions and self.config.add_cross_attention else None
@@ -491,11 +497,6 @@ class XLMRobertaXLEncoder(nn.Module):
             past_key_value = past_key_values[i] if past_key_values is not None else None
 
             if self.gradient_checkpointing and self.training:
-                if use_cache:
-                    logger.warning_once(
-                        "`use_cache=True` is incompatible with gradient checkpointing. Setting `use_cache=False`..."
-                    )
-                    use_cache = False
 
                 def create_custom_forward(module):
                     def custom_forward(*inputs):
