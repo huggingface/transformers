0b9b4649df6a687933c6bf6c8b558c816e0768f2
Tran Xuan Loc <loc.tran@moreh.com.vn>
2023-03-14 07:08:01 +0000

    add run scripts for image-pretraining example

---
+++ examples/pytorch/image-pretraining/mae.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+
+/usr/bin/env python3 run_mae.py \
+    --dataset_name cifar10 \
+    --output_dir ./vit-mae-demo \
+    --overwrite_output_dir \
+    --remove_unused_columns False \
+    --label_names pixel_values \
+    --mask_ratio 0.75 \
+    --norm_pix_loss \
+    --do_train \
+    --do_eval \
+    --base_learning_rate 1.5e-4 \
+    --lr_scheduler_type cosine \
+    --weight_decay 0.05 \
+    --num_train_epochs 5 \
+    --warmup_ratio 0.05 \
+    --per_device_train_batch_size 256 \
+    --per_device_eval_batch_size 256 \
+    --logging_strategy steps \
+    --logging_steps 100 \
+    --evaluation_strategy epoch \
+    --save_strategy epoch \
+    --load_best_model_at_end True \
+    --save_total_limit 3 \
+    --seed 42
---
+++ examples/pytorch/image-pretraining/mim.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+
+/usr/bin/env python3 run_mim.py \
+    --model_type vit \
+    --output_dir ./outputs \
+    --overwrite_output_dir \
+    --remove_unused_columns False \
+    --label_names bool_masked_pos \
+    --do_train \
+    --do_eval \
+    --learning_rate 2e-5 \
+    --weight_decay 0.05 \
+    --num_train_epochs 1 \
+    --per_device_train_batch_size 128 \
+    --per_device_eval_batch_size 128 \
+    --logging_strategy steps \
+    --logging_steps 100 \
+    --evaluation_strategy epoch \
+    --save_strategy epoch \
+    --load_best_model_at_end True \
+    --save_total_limit 3 \
+    --seed 42
