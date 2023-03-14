7f2f176128f391a0edce50ea2322134383e75a5d
Tran Xuan Loc <loc.tran@moreh.com.vn>
2023-03-14 07:06:41 +0000

    disable telemetry

--- examples/pytorch/image-pretraining/run_mae.py
+++ examples/pytorch/image-pretraining/run_mae.py
@@ -177,7 +177,7 @@ def main():
 
     # Sending telemetry. Tracking the example usage helps us better allocate resources to maintain them. The
     # information sent is the one passed as arguments along with your Python/PyTorch versions.
-    send_example_telemetry("run_mae", model_args, data_args)
+    # send_example_telemetry("run_mae", model_args, data_args)
 
     # Setup logging
     logging.basicConfig(
