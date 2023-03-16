0b5a8eabe2ceebde3a30bd650f809632c4bd3e32
Joao Gante <joao@huggingface.co>
2023-03-16 18:37:52 +0000

    remove breakpoint

--- src/transformers/models/bloom/modeling_bloom.py
+++ src/transformers/models/bloom/modeling_bloom.py
@@ -513,7 +513,6 @@ class BloomPreTrainedModel(PreTrainedModel):
         Standardizes the format of the cache so as to match most implementations, i.e. to tuple(tuple([batch_size,
         num_heads, ...]))
         """
-        breakpoint()
         batch_size_times_num_heads, head_dim, seq_length = past_key_value[0][0].shape
         num_heads = batch_size_times_num_heads // batch_size
         # key: [batch_size * num_heads, head_dim, seq_length] -> [batch_size, num_heads, head_dim, seq_length]
