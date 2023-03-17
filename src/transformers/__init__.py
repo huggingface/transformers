e45e6bc71a4661039c530ab6116abf9c1d5ddba1
lewtun <lewis.c.tunstall@gmail.com>
2023-03-17 14:39:26 +0100

    Add LlamaForSequenceClassification (#22209)
    * Add LlamaForSequenceClassification
    
    * Update src/transformers/models/llama/modeling_llama.py
    
    Co-authored-by: Younes Belkada <49240599+younesbelkada@users.noreply.github.com>
    
    * Update src/transformers/models/llama/modeling_llama.py
    
    Co-authored-by: Younes Belkada <49240599+younesbelkada@users.noreply.github.com>
    
    * Add docstring
    
    * Add test
    
    * Add input embedding getter and setter
    
    * Remove dead code
    
    ---------
    
    Co-authored-by: Younes Belkada <49240599+younesbelkada@users.noreply.github.com>

--- src/transformers/__init__.py
+++ src/transformers/__init__.py
@@ -1802,11 +1802,7 @@ else:
         ]
     )
     _import_structure["models.llama"].extend(
-        [
-            "LlamaForCausalLM",
-            "LlamaModel",
-            "LlamaPreTrainedModel",
-        ]
+        ["LlamaForCausalLM", "LlamaForSequenceClassification", "LlamaModel", "LlamaPreTrainedModel"]
     )
     _import_structure["models.longformer"].extend(
         [
@@ -5213,11 +5209,7 @@ if TYPE_CHECKING:
             LiltModel,
             LiltPreTrainedModel,
         )
-        from .models.llama import (
-            LlamaForCausalLM,
-            LlamaModel,
-            LlamaPreTrainedModel,
-        )
+        from .models.llama import LlamaForCausalLM, LlamaForSequenceClassification, LlamaModel, LlamaPreTrainedModel
         from .models.longformer import (
             LONGFORMER_PRETRAINED_MODEL_ARCHIVE_LIST,
             LongformerForMaskedLM,
