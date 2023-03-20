c3aea102914904fa95828ad00cdaaf4069352c4b
younesbelkada <younesbelkada@gmail.com>
2023-03-20 10:48:27 +0000

    make fixup

--- src/transformers/models/pix2struct/image_processing_pix2struct.py
+++ src/transformers/models/pix2struct/image_processing_pix2struct.py
@@ -201,7 +201,7 @@ class Pix2StructImageProcessor(BaseImageProcessor):
             The maximum number of patches to extract from the image as per the [Pix2Struct
             paper](https://arxiv.org/pdf/2210.03347.pdf).
         is_vqa (`bool`, *optional*, defaults to `False`):
-            Whether or not the image processor is for the VQA task. If `True` and `header_text` is passed in, text is 
+            Whether or not the image processor is for the VQA task. If `True` and `header_text` is passed in, text is
             rendered onto the input images.
     """
 
