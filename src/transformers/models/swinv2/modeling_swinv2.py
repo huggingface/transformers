c07a02a4b7892edfee22cbe57d3cdd9e10ae7a4d
amyeroberts <22614925+amyeroberts@users.noreply.github.com>
2023-03-20 20:06:16 +0000

    Update vision docstring bool masked pos (#22237)
    * Add bool_masked_pos to forward docstrings
    
    * Add note about mask ratio - videomae
    
    * Fix up
    
    * Fix indenting

--- src/transformers/models/swinv2/modeling_swinv2.py
+++ src/transformers/models/swinv2/modeling_swinv2.py
@@ -1055,6 +1055,10 @@ class Swinv2Model(Swinv2PreTrainedModel):
         output_hidden_states: Optional[bool] = None,
         return_dict: Optional[bool] = None,
     ) -> Union[Tuple, Swinv2ModelOutput]:
+        r"""
+        bool_masked_pos (`torch.BoolTensor` of shape `(batch_size, num_patches)`, *optional*):
+            Boolean masked positions. Indicates which patches are masked (1) and which aren't (0).
+        """
         output_attentions = output_attentions if output_attentions is not None else self.config.output_attentions
         output_hidden_states = (
             output_hidden_states if output_hidden_states is not None else self.config.output_hidden_states
