b9955aae3063cdc6dd8682dd0507a0e5591f6419
Yih-Dar <2521628+ydshieh@users.noreply.github.com>
2023-03-21 16:16:17 +0100

    fix more doctests (#22292)
    * fix more doctests
    
    * fix style
    
    ---------
    
    Co-authored-by: ydshieh <ydshieh@users.noreply.github.com>

--- src/transformers/models/roberta/tokenization_roberta_fast.py
+++ src/transformers/models/roberta/tokenization_roberta_fast.py
@@ -81,12 +81,14 @@ class RobertaTokenizerFast(PreTrainedTokenizerFast):
     This tokenizer has been trained to treat spaces like parts of the tokens (a bit like sentencepiece) so a word will
     be encoded differently whether it is at the beginning of the sentence (without space) or not:
 
-    ```
+    ```python
     >>> from transformers import RobertaTokenizerFast
+
     >>> tokenizer = RobertaTokenizerFast.from_pretrained("roberta-base")
-    >>> tokenizer("Hello world")['input_ids']
+    >>> tokenizer("Hello world")["input_ids"]
     [0, 31414, 232, 2]
-    >>> tokenizer(" Hello world")['input_ids']
+
+    >>> tokenizer(" Hello world")["input_ids"]
     [0, 20920, 232, 2]
     ```
 
