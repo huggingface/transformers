79e6c83007cc140294c0a50ac47f36b6494f421f
Arthur Zucker <arthur@huggingface.co>
2023-03-22 14:26:06 +0000

    fix the prefix tokens

--- src/transformers/models/nllb/tokenization_nllb.py
+++ src/transformers/models/nllb/tokenization_nllb.py
@@ -390,11 +390,11 @@ class NllbTokenizer(PreTrainedTokenizer):
     def set_src_lang_special_tokens(self, src_lang) -> None:
         """Reset the special tokens to the source lang setting. No prefix and suffix=[eos, src_lang_code]."""
         self.cur_lang_code = self.lang_code_to_id[src_lang]
-        self.prefix_tokens = []
-        self.suffix_tokens = [self.eos_token_id, self.cur_lang_code]
+        self.prefix_tokens = [self.cur_lang_code]
+        self.suffix_tokens = [self.eos_token_id]
 
     def set_tgt_lang_special_tokens(self, lang: str) -> None:
         """Reset the special tokens to the target language setting. No prefix and suffix=[eos, tgt_lang_code]."""
         self.cur_lang_code = self.lang_code_to_id[lang]
-        self.prefix_tokens = []
-        self.suffix_tokens = [self.eos_token_id, self.cur_lang_code]
+        self.prefix_tokens = [self.cur_lang_code]
+        self.suffix_tokens = [self.eos_token_id]
