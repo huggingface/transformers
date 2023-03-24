6fe19fb9bf5b476b43192c3d01f0f2ed56cd118c
Inderpreet Singh Chhabra <54892545+inderpreetsingh01@users.noreply.github.com>
2023-03-24 13:39:42 +0530

    Update src/transformers/models/seaformer/modeling_seaformer.py
    Co-authored-by: Alara Dirik <8944735+alaradirik@users.noreply.github.com>

--- src/transformers/models/seaformer/modeling_seaformer.py
+++ src/transformers/models/seaformer/modeling_seaformer.py
@@ -63,10 +63,6 @@ def _make_divisible(v, divisor, min_value=None):
     It ensures that all layers have a channel number that is divisible by 8
     It can be seen here:
     https://github.com/tensorflow/models/blob/master/research/slim/nets/mobilenet/mobilenet.py
-    :param v:
-    :param divisor:
-    :param min_value:
-    :return:
     """
     if min_value is None:
         min_value = divisor
