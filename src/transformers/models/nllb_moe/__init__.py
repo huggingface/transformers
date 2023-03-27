634dc4f873d8c39351c334d1c41d2c327449c44f
arthur.zucker@gmail.com <arthur.zucker@gmail.com>
2023-03-27 11:52:41 +0000

    cleanup

--- src/transformers/models/nllb_moe/__init__.py
+++ src/transformers/models/nllb_moe/__init__.py
@@ -14,15 +14,7 @@
 
 from typing import TYPE_CHECKING
 
-from ...utils import (
-    OptionalDependencyNotAvailable,
-    _LazyModule,
-    is_flax_available,
-    is_sentencepiece_available,
-    is_tf_available,
-    is_tokenizers_available,
-    is_torch_available,
-)
+from ...utils import OptionalDependencyNotAvailable, _LazyModule, is_torch_available
 
 
 _import_structure = {
