0bde905f927ea6203dcfb544e72ac24ffd812ee7
Niels <niels.rogge1@gmail.com>
2023-03-27 13:11:38 +0200

    Remove script

--- src/transformers/models/pix2struct/test.py
+++
@@ -1,5 +0,0 @@
-from transformers import Pix2StructConfig, Pix2StructTextConfig, Pix2StructForConditionalGeneration
-
-config = Pix2StructConfig(text_config=dict(tie_word_embeddings=False), tie_word_embeddings=False)
-
-model = Pix2StructForConditionalGeneration.from_pretrained("google/pix2struct-base", config=config)
