60862246a2af282d6a7444ce37bf6208753077ee
Matthijs Hollemans <mail@hollance.com>
2023-03-28 16:27:41 +0200

    clean up power_to_db

--- src/transformers/audio_utils.py
+++ src/transformers/audio_utils.py
@@ -206,37 +206,55 @@ def get_mel_filter_banks(
     return filterbank
 
 
-def power_to_db(mel_spectrogram, top_db=None, a_min=1e-10, ref=1.0):
+def power_to_db(
+    spectrogram: np.ndarray,
+    reference: float = 1.0,
+    min_value: float = 1e-10,
+    db_range: Optional[float] = None,
+) -> np.ndarray:
     """
-    Convert a mel spectrogram from power to db scale, this function is the numpy implementation of librosa.power_to_lb.
-    It computes `10 * log10(mel_spectrogram / ref)`, using basic log properties for stability.
+    Converts a power spectrogram to the decibel scale. This computes `10 * log10(spectrogram / reference)`, using basic
+    logarithm properties for numerical stability.
 
-    Tips:
-        - The motivation behind applying the log function on the mel spectrogram is that humans do not hear loudness on
-          a
-        linear scale. Generally to double the percieved volume of a sound we need to put 8 times as much energy into
-        it.
-        - This means that large variations in energy may not sound all that different if the sound is loud to begin
-        with. This compression operation makes the mel features match more closely what humans actually hear.
+    The motivation behind applying the log function on the (mel) spectrogram is that humans do not hear loudness on a
+    linear scale. Generally to double the perceived volume of a sound we need to put 8 times as much energy into it.
+    This means that large variations in energy may not sound all that different if the sound is loud to begin with.
+    This compression operation makes the (mel) spectrogram features match more closely what humans actually hear.
+
+    Based on the implementation of `librosa.power_to_db`.
 
     Args:
-        mel_spectrogram (`np.array`):
-            Input mel spectrogram.
-        top_db (`int`, *optional*):
-            The maximum decibel value.
-        a_min (`int`, *optional*, default to 1e-10):
-            Minimum value to use when cliping the mel spectrogram.
-        ref (`float`, *optional*, default to 1.0):
-            Maximum reference value used to scale the mel_spectrogram.
+        spectrogram (`np.ndarray`):
+            The input power (mel) spectrogram. Note that a power spectrogram has the amplitudes squared!
+        reference (`float`, *optional*, defaults to 1.0):
+            Sets the input spectrogram value that corresponds to 0 dB. For example, use `np.max(spectrogram)` to set
+            the loudest part to 0 dB. Must be greater than zero.
+        min_value (`float`, *optional*, defaults to `1e-10`):
+            The spectrogram will be clipped to this minimum value before conversion to decibels, to avoid taking
+            `log(0)`. The default of `1e-10` corresponds to a minimum of -100 dB. Must be greater than zero.
+        db_range (`float`, *optional*, defaults to `None`):
+            Sets the maximum dynamic range in decibels. For example, if `db_range = 80`, the difference between the
+            peak value and the smallest value will never be more than 80 dB. Must be greater than zero.
 
+    Returns:
+        `np.ndarray`: the spectrogram in decibels
     """
-    log_spec = 10 * np.log10(np.clip(mel_spectrogram, a_min=a_min, a_max=None))
-    log_spec -= 10.0 * np.log10(np.maximum(a_min, ref))
-    if top_db is not None:
-        if top_db < 0:
-            raise ValueError("top_db must be non-negative")
-        log_spec = np.clip(log_spec, min=np.maximum(log_spec) - top_db, max=np.inf)
-    return log_spec
+    if reference <= 0.0:
+        raise ValueError("reference must be greater than zero")
+    if min_value <= 0.0:
+        raise ValueError("min_value must be greater than zero")
+
+    reference = max(min_value, reference)
+
+    spectrogram = np.clip(spectrogram, a_min=min_value, a_max=None)
+    spectrogram = 10.0 * (np.log10(spectrogram) - np.log10(reference))
+
+    if db_range is not None:
+        if db_range <= 0.0:
+            raise ValueError("db_range must be greater than zero")
+        spectrogram = np.clip(spectrogram, a_min=spectrogram.max() - db_range, a_max=None)
+
+    return spectrogram
 
 
 # TODO @ArthurZucker: This method does not support batching yet as we are mainly focus on inference.
