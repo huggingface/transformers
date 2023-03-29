8c846fcdbd59e993b7b70ec426a037d754731e73
Matthijs Hollemans <mail@hollance.com>
2023-03-29 15:20:28 +0200

    tests for power_to_db and amplitude_to_db

--- tests/utils/test_audio_utils.py
+++ tests/utils/test_audio_utils.py
@@ -18,7 +18,7 @@ import unittest
 import numpy as np
 import pytest
 
-from transformers.audio_utils import hertz_to_mel, mel_to_hertz
+from transformers.audio_utils import amplitude_to_db, hertz_to_mel, mel_to_hertz, power_to_db
 
 
 class AudioUtilsFunctionTester(unittest.TestCase):
@@ -57,3 +57,82 @@ class AudioUtilsFunctionTester(unittest.TestCase):
 
         with pytest.raises(ValueError):
             mel_to_hertz(100, mel_scale=None)
+
+    def test_mel_filter_bank(self):
+        pass
+
+    def test_power_to_db(self):
+        spectrogram = np.zeros((2, 3))
+        spectrogram[0, 0] = 2.0
+        spectrogram[0, 1] = 0.5
+        spectrogram[0, 2] = 0.707
+        spectrogram[1, 1] = 1.0
+
+        output = power_to_db(spectrogram, reference=1.0)
+        expected = np.array([[3.01029996, -3.01029996, -1.50580586], [-100.0, 0.0, -100.0]])
+        self.assertTrue(np.allclose(output, expected))
+
+        output = power_to_db(spectrogram, reference=2.0)
+        expected = np.array([[0.0, -6.02059991, -4.51610582], [-103.01029996, -3.01029996, -103.01029996]])
+        self.assertTrue(np.allclose(output, expected))
+
+        output = power_to_db(spectrogram, min_value=1e-6)
+        expected = np.array([[3.01029996, -3.01029996, -1.50580586], [-60.0, 0.0, -60.0]])
+        self.assertTrue(np.allclose(output, expected))
+
+        output = power_to_db(spectrogram, db_range=80)
+        expected = np.array([[3.01029996, -3.01029996, -1.50580586], [-76.98970004, 0.0, -76.98970004]])
+        self.assertTrue(np.allclose(output, expected))
+
+        output = power_to_db(spectrogram, reference=2.0, db_range=80)
+        expected = np.array([[0.0, -6.02059991, -4.51610582], [-80.0, -3.01029996, -80.0]])
+        self.assertTrue(np.allclose(output, expected))
+
+        output = power_to_db(spectrogram, reference=2.0, min_value=1e-6, db_range=80)
+        expected = np.array([[0.0, -6.02059991, -4.51610582], [-63.01029996, -3.01029996, -63.01029996]])
+        self.assertTrue(np.allclose(output, expected))
+
+        with pytest.raises(ValueError):
+            power_to_db(spectrogram, reference=0.0)
+        with pytest.raises(ValueError):
+            power_to_db(spectrogram, min_value=0.0)
+        with pytest.raises(ValueError):
+            power_to_db(spectrogram, db_range=-80)
+
+    def test_amplitude_to_db(self):
+        spectrogram = np.zeros((2, 3))
+        spectrogram[0, 0] = 2.0
+        spectrogram[0, 1] = 0.5
+        spectrogram[0, 2] = 0.707
+        spectrogram[1, 1] = 1.0
+
+        output = amplitude_to_db(spectrogram, reference=1.0)
+        expected = np.array([[6.02059991, -6.02059991, -3.01161172], [-100.0, 0.0, -100.0]])
+        self.assertTrue(np.allclose(output, expected))
+
+        output = amplitude_to_db(spectrogram, reference=2.0)
+        expected = np.array([[0.0, -12.04119983, -9.03221164], [-106.02059991, -6.02059991, -106.02059991]])
+        self.assertTrue(np.allclose(output, expected))
+
+        output = amplitude_to_db(spectrogram, min_value=1e-3)
+        expected = np.array([[6.02059991, -6.02059991, -3.01161172], [-60.0, 0.0, -60.0]])
+        self.assertTrue(np.allclose(output, expected))
+
+        output = amplitude_to_db(spectrogram, db_range=80)
+        expected = np.array([[6.02059991, -6.02059991, -3.01161172], [-73.97940009, 0.0, -73.97940009]])
+        self.assertTrue(np.allclose(output, expected))
+
+        output = amplitude_to_db(spectrogram, reference=2.0, db_range=80)
+        expected = np.array([[0.0, -12.04119983, -9.03221164], [-80.0, -6.02059991, -80.0]])
+        self.assertTrue(np.allclose(output, expected))
+
+        output = amplitude_to_db(spectrogram, reference=2.0, min_value=1e-3, db_range=80)
+        expected = np.array([[0.0, -12.04119983, -9.03221164], [-66.02059991, -6.02059991, -66.02059991]])
+        self.assertTrue(np.allclose(output, expected))
+
+        with pytest.raises(ValueError):
+            amplitude_to_db(spectrogram, reference=0.0)
+        with pytest.raises(ValueError):
+            amplitude_to_db(spectrogram, min_value=0.0)
+        with pytest.raises(ValueError):
+            amplitude_to_db(spectrogram, db_range=-80)
