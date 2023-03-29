a3bf422dc0554230bb74995b6340ed415e03e485
upjabir <upjabir@gmail.com>
2023-03-29 20:28:10 +0530

    added biogpt token classifier

--- src/transformers/__init__.py
+++ src/transformers/__init__.py
@@ -1130,6 +1130,7 @@ else:
         [
             "BIOGPT_PRETRAINED_MODEL_ARCHIVE_LIST",
             "BioGptForCausalLM",
+            "BioGptForTokenClassification",
             "BioGptModel",
             "BioGptPreTrainedModel",
         ]
@@ -4691,6 +4692,7 @@ if TYPE_CHECKING:
         from .models.biogpt import (
             BIOGPT_PRETRAINED_MODEL_ARCHIVE_LIST,
             BioGptForCausalLM,
+            BioGptForTokenClassification,
             BioGptModel,
             BioGptPreTrainedModel,
         )
