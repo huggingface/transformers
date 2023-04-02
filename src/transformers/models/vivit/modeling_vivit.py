015c87b22cb29d0651aa7e59b505f414c5f9c2b0
Jegor Kitskerkin <jegor.kitskerkin@gmail.com>
2023-04-02 18:46:42 +0300

    Run fix-copies

--- src/transformers/models/vivit/modeling_vivit.py
+++ src/transformers/models/vivit/modeling_vivit.py
@@ -194,7 +194,6 @@ class VivitSelfOutput(nn.Module):
         self.dropout = nn.Dropout(config.hidden_dropout_prob)
 
     def forward(self, hidden_states: torch.Tensor, input_tensor: torch.Tensor) -> torch.Tensor:
-
         hidden_states = self.dense(hidden_states)
         hidden_states = self.dropout(hidden_states)
 
