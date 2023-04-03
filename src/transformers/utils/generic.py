18b091de755642077728c0e409fb0712d7910452
Xuehai Pan <XuehaiPan@pku.edu.cn>
2023-04-03 14:47:05 +0000

    reformat docstring by `doc-builder`

--- src/transformers/utils/generic.py
+++ src/transformers/utils/generic.py
@@ -60,9 +60,8 @@ class cached_property(property):
 def strtobool(val):
     """Convert a string representation of truth to true (1) or false (0).
 
-    True values are 'y', 'yes', 't', 'true', 'on', and '1'; false values
-    are 'n', 'no', 'f', 'false', 'off', and '0'.  Raises ValueError if
-    'val' is anything else.
+    True values are 'y', 'yes', 't', 'true', 'on', and '1'; false values are 'n', 'no', 'f', 'false', 'off', and '0'.
+    Raises ValueError if 'val' is anything else.
     """
     val = val.lower()
     if val in {"y", "yes", "t", "true", "on", "1"}:
