150f813023b17055215f25f9b8b6fbf01be5795a
Joao Gante <joao@huggingface.co>
2023-04-03 13:01:25 +0000

    make fixup

--- src/transformers/generation/__init__.py
+++ src/transformers/generation/__init__.py
@@ -17,7 +17,10 @@ from typing import TYPE_CHECKING
 from ..utils import OptionalDependencyNotAvailable, _LazyModule, is_flax_available, is_tf_available, is_torch_available
 
 
-_import_structure = {"configuration_utils": ["GenerationConfig"], "streamers": ["TextStreamer", "TextIteratorStreamer"]}
+_import_structure = {
+    "configuration_utils": ["GenerationConfig"],
+    "streamers": ["TextIteratorStreamer", "TextStreamer"],
+}
 
 try:
     if not is_torch_available():
