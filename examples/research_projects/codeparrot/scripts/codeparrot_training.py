475ed09063ac8cc3def9770c93ca0ee033709aff
Armel Zebaze <armelzebaze@Armels-MBP-2.localdomain>
2023-04-04 13:45:11 +0200

    fixing a bug about gradient accumulation in codeparrot_training

--- examples/research_projects/codeparrot/scripts/codeparrot_training.py
+++ examples/research_projects/codeparrot/scripts/codeparrot_training.py
@@ -195,7 +195,7 @@ parser = HfArgumentParser(TrainingArguments)
 args = parser.parse_args()
 
 # Accelerator
-accelerator = Accelerator(log_with=["wandb", "tensorboard"], logging_dir=f"{args.save_dir}/log")
+accelerator = Accelerator(gradient_accumulation_steps=args.gradient_accumulation_steps, log_with=["wandb", "tensorboard"], logging_dir=f"{args.save_dir}/log")
 acc_state = {str(k): str(v) for k, v in accelerator.state.__dict__.items()}
 
 args = Namespace(**vars(args), **acc_state)
@@ -264,42 +264,36 @@ completed_steps = 0
 t_start = time.time()
 loss_tracking = 0
 for step, batch in enumerate(train_dataloader, start=1):
-    if args.resume_from_checkpoint and step < resume_step:
-        continue  # we need to skip steps until we reach the resumed step
-    loss = model(batch, labels=batch, use_cache=False).loss
-    avg_loss = accelerator.gather(loss.repeat(args.train_batch_size)).mean()
-    loss_tracking += avg_loss.item() / args.gradient_accumulation_steps
-    log_metrics(step, {"samples": step * samples_per_step, "loss_per_step/train": loss.item()})
-    loss = loss / args.gradient_accumulation_steps
-    if step % args.gradient_accumulation_steps != 0:
-        # Prevent backward from doing gradient all_reduce in every step
-        if accelerator.distributed_type == DistributedType.MULTI_GPU:
-            with model.no_sync():
-                accelerator.backward(loss)
-        else:
-            accelerator.backward(loss)
-    else:
-        lr = get_lr()
-        accelerator.backward(loss)
+    with accelerator.accumulate(model):
+        if args.resume_from_checkpoint and step < resume_step:
+            continue  # we need to skip steps until we reach the resumed step
+        lr = get_lr()   
+        loss = model(batch, labels=batch, use_cache=False).loss
+        avg_loss = accelerator.gather(loss.repeat(args.train_batch_size)).mean()
+        loss_tracking += avg_loss.item() / args.gradient_accumulation_steps
+        log_metrics(step, {"samples": step * samples_per_step,
+                        "loss_per_step/train": loss.item()})
         accelerator.clip_grad_norm_(model.parameters(), 1.0)
+        accelerator.backward(loss)
         optimizer.step()
         lr_scheduler.step()
         optimizer.zero_grad()
-        elapsed_time = time.time() - t_start
-        tflops = compute_tflops(elapsed_time, accelerator, args)
-        log_metrics(
-            step,
-            {
-                "steps": completed_steps,
-                "loss/train": loss_tracking,
-                "lr": lr,
-                "tflops": tflops,
-                "time_per_iteration": elapsed_time,
-            },
-        )
-        t_start = time.time()
-        loss_tracking = 0
-        completed_steps += 1
+        if accelerator.gradient_state.sync_gradients:
+            elapsed_time = time.time() - t_start
+            tflops = compute_tflops(elapsed_time, accelerator, args)
+            log_metrics(
+                step,
+                {
+                    "steps": completed_steps,
+                    "loss/train": loss_tracking,
+                    "lr": lr,
+                    "tflops": tflops,
+                    "time_per_iteration": elapsed_time,
+                },
+            )
+            t_start = time.time()
+            loss_tracking = 0
+            completed_steps += 1
     if step % args.save_checkpoint_steps == 0:
         logger.info("Evaluating and saving model checkpoint")
         eval_loss, perplexity = evaluate(args)
