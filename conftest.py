6378d261f445211edaeb72ee38485d549a6a37d3
Joao Gante <joaofranciscocardosogante@gmail.com>
2023-04-05 11:30:51 +0100

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -42,7 +42,7 @@ def pytest_configure(config):
         "markers", "is_pipeline_test: mark test to run only when pipelines are tested"
     )
     config.addinivalue_line("markers", "is_staging_test: mark test to run only in the staging environment")
-    config.addinivalue_line("markers", "accelerate_tests: mark tests that require accelerate")
+    config.addinivalue_line("markers", "accelerate_tests: mark test that require accelerate")
 
 
 def pytest_addoption(parser):
