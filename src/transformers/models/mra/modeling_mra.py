e0e7a8f3530edb42780ba2fac82789fb9915a732
novice <pranavpulijala@gmail.com>
2023-04-09 18:59:49 -0500

    change cuda loading

--- src/transformers/models/mra/modeling_mra.py
+++ src/transformers/models/mra/modeling_mra.py
@@ -60,26 +60,25 @@ MRA_PRETRAINED_MODEL_ARCHIVE_LIST = [
 
 
 def load_cuda_kernels():
-    global cuda_kernel
-    curr_path = os.path.dirname(os.path.realpath(__file__))
-    src_files = ["cuda_kernel.cu", "cuda_launch.cu", "torch_extension.cpp"]
-    src_files = [os.path.join(curr_path, file) for file in src_files]
-    cuda_kernel = load("cuda_kernel", src_files, verbose=True)
-
-    import cuda_kernel
-
-
-if is_torch_cuda_available() and is_ninja_available():
     logger.info("Loading custom CUDA kernels...")
-    try:
-        load_cuda_kernels()
-    except Exception as e:
-        logger.warning(
-            "Failed to load CUDA kernels. MRA requires custom CUDA kernels. Please verify that compatible versions of"
-            f" PyTorch and CUDA Toolkit are installed: {e}"
-        )
-else:
-    pass
+
+    if is_torch_cuda_available() and is_ninja_available():
+        try:
+            global cuda_kernel
+            curr_path = os.path.dirname(os.path.realpath(__file__))
+            src_files = ["cuda_kernel.cu", "cuda_launch.cu", "torch_extension.cpp"]
+            src_files = [os.path.join(curr_path, file) for file in src_files]
+            cuda_kernel = load("cuda_kernel", src_files, verbose=True)
+
+            import cuda_kernel
+        except Exception as e:
+            logger.warning(
+                "Failed to load CUDA kernels. MRA requires custom CUDA kernels. Please verify that compatible versions of"
+                f" PyTorch and CUDA Toolkit are installed: {e}"
+            )
+            cuda_kernel = None
+    else:
+        None
 
 
 def sparse_max(sparse_C, indices, A_num_block, B_num_block):
@@ -353,7 +352,6 @@ def mra2_attention(
     """
     Use MRA to approximate self-attention.
     """
-    load_cuda_kernels()
     batch_size, num_head, seq_len, head_dim = Q.size()
     meta_batch = batch_size * num_head
 
@@ -910,6 +908,7 @@ MRA_INPUTS_DOCSTRING = r"""
 class MRAModel(MRAPreTrainedModel):
     def __init__(self, config):
         super().__init__(config)
+        load_cuda_kernels()
         self.config = config
 
         self.embeddings = MRAEmbeddings(config)
