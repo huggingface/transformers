89d3ae24f0ecc968acbdda4f53ac6a47e4ecd53b
novice <pranavpulijala@gmail.com>
2023-04-09 20:46:53 -0500

    formatting changes

--- src/transformers/models/mra/configuration_mra.py
+++ src/transformers/models/mra/configuration_mra.py
@@ -114,7 +114,7 @@ class MRAConfig(PretrainedConfig):
         pad_token_id=1,
         bos_token_id=0,
         eos_token_id=2,
-        **kwargs
+        **kwargs,
     ):
         super().__init__(pad_token_id=pad_token_id, bos_token_id=bos_token_id, eos_token_id=eos_token_id, **kwargs)
 
@@ -135,6 +135,3 @@ class MRAConfig(PretrainedConfig):
         self.approx_mode = approx_mode
         self.initial_prior_first_n_blocks = initial_prior_first_n_blocks
         self.initial_prior_diagonal_n_blocks = initial_prior_diagonal_n_blocks
-
-        self.num_block = (self.max_position_embeddings // 32) * self.block_per_row
-        self.num_block = min(self.num_block, int((self.max_position_embeddings // 32) ** 2))
