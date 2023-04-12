da619073c0428ad759ccde230c865b1695e5b40e
Sam Bean <sam.h.bean@gmail.com>
2023-04-12 16:49:35 -0400

    fixup

--- src/transformers/models/llama/configuration_llama.py
+++ src/transformers/models/llama/configuration_llama.py
@@ -19,11 +19,11 @@
 # limitations under the License.
 """ LLaMA model configuration"""
 from collections import OrderedDict
-from typing import Mapping, Any, List, Optional
+from typing import Any, List, Mapping, Optional
 
 from ... import PreTrainedTokenizer, TensorType, is_torch_available
 from ...configuration_utils import PretrainedConfig
-from ...onnx import OnnxConfig, OnnxConfigWithPast, PatchingSpec
+from ...onnx import OnnxConfigWithPast, PatchingSpec
 from ...utils import logging
 
 
