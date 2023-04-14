3d6c400b63e6ac2d568061fca4e71e5112d8bfa6
Shehan Munasinghe <shehanmunasinghe@gmail.com>
2023-04-14 20:28:58 +0530

    Update test_modeling_swiftformer.py

--- tests/models/swiftformer/test_modeling_swiftformer.py
+++ tests/models/swiftformer/test_modeling_swiftformer.py
@@ -20,9 +20,7 @@ import unittest
 
 from transformers import SwiftFormerConfig
 from transformers.testing_utils import (
-    require_accelerate,
     require_torch,
-    require_torch_gpu,
     require_vision,
     slow,
     torch_device,
@@ -38,14 +36,14 @@ if is_torch_available():
     import torch
     from torch import nn
 
-    from transformers import SwiftFormerForImageClassification, SwiftFormerForMaskedImageModeling, SwiftFormerModel
+    from transformers import SwiftFormerForImageClassification, SwiftFormerModel
     from transformers.models.swiftformer.modeling_swiftformer import SWIFTFORMER_PRETRAINED_MODEL_ARCHIVE_LIST
 
 
 if is_vision_available():
     from PIL import Image
 
-    from transformers import ViTFeatureExtractor
+    from transformers import ViTImageProcessor
 
 
 class SwiftFormerModelTester:
@@ -53,45 +51,46 @@ class SwiftFormerModelTester:
         self,
         parent,
         batch_size=13,
-        image_size=30,
-        patch_size=2,
         num_channels=3,
         is_training=True,
         use_labels=True,
-        hidden_size=32,
-        num_hidden_layers=5,
-        num_attention_heads=4,
-        intermediate_size=37,
-        hidden_act="gelu",
+        # hidden_size=32,
         hidden_dropout_prob=0.1,
         attention_probs_dropout_prob=0.1,
-        type_sequence_label_size=10,
-        initializer_range=0.02,
-        scope=None,
-        encoder_stride=2,
+        ###
+        layers=[3, 3, 6, 4],
+        embed_dims=[48, 56, 112, 220],
+        mlp_ratios=4,
+        downsamples=[True, True, True, True],
+        vit_num=1,
+        act_layer="gelu",
+        num_labels=1000,
+        down_patch_size=3,
+        down_stride=2,
+        down_pad=1,
+        drop_rate=0.0,
+        drop_path_rate=0.0,
+        use_layer_scale=True,
+        layer_scale_init_value=1e-5,
     ):
         self.parent = parent
+
         self.batch_size = batch_size
-        self.image_size = image_size
-        self.patch_size = patch_size
+
         self.num_channels = num_channels
         self.is_training = is_training
         self.use_labels = use_labels
-        self.hidden_size = hidden_size
-        self.num_hidden_layers = num_hidden_layers
-        self.num_attention_heads = num_attention_heads
-        self.intermediate_size = intermediate_size
-        self.hidden_act = hidden_act
+
         self.hidden_dropout_prob = hidden_dropout_prob
         self.attention_probs_dropout_prob = attention_probs_dropout_prob
-        self.type_sequence_label_size = type_sequence_label_size
-        self.initializer_range = initializer_range
-        self.scope = scope
-        self.encoder_stride = encoder_stride
+
+        self.type_sequence_label_size = num_labels
+
+        self.image_size = 224
+
+        # self.
 
         # in SwiftFormer, the seq length equals the number of patches + 1 (we add 1 for the [CLS] token)
-        num_patches = (image_size // patch_size) ** 2
-        self.seq_length = num_patches + 1
 
     def prepare_config_and_inputs(self):
         pixel_values = floats_tensor([self.batch_size, self.num_channels, self.image_size, self.image_size])
@@ -106,46 +105,28 @@ class SwiftFormerModelTester:
 
     def get_config(self):
         return SwiftFormerConfig(
-            image_size=self.image_size,
-            patch_size=self.patch_size,
-            num_channels=self.num_channels,
-            hidden_size=self.hidden_size,
-            num_hidden_layers=self.num_hidden_layers,
-            num_attention_heads=self.num_attention_heads,
-            intermediate_size=self.intermediate_size,
-            hidden_act=self.hidden_act,
-            hidden_dropout_prob=self.hidden_dropout_prob,
-            attention_probs_dropout_prob=self.attention_probs_dropout_prob,
-            is_decoder=False,
-            initializer_range=self.initializer_range,
-            encoder_stride=self.encoder_stride,
+            layers=[3, 3, 6, 4],
+            embed_dims=[48, 56, 112, 220],
+            mlp_ratios=4,
+            downsamples=[True, True, True, True],
+            vit_num=1,
+            act_layer="gelu",
+            num_labels=1000,
+            down_patch_size=3,
+            down_stride=2,
+            down_pad=1,
+            drop_rate=0.0,
+            drop_path_rate=0.0,
+            use_layer_scale=True,
+            layer_scale_init_value=1e-5,
         )
 
     def create_and_check_model(self, config, pixel_values, labels):
-        model = SwiftFormerModel(config=config)
+        model = SwiftFormerForImageClassification(config=config)
         model.to(torch_device)
         model.eval()
-        result = model(pixel_values)
-        self.parent.assertEqual(result.last_hidden_state.shape, (self.batch_size, self.seq_length, self.hidden_size))
-
-    def create_and_check_for_masked_image_modeling(self, config, pixel_values, labels):
-        model = SwiftFormerForMaskedImageModeling(config=config)
-        model.to(torch_device)
-        model.eval()
-        result = model(pixel_values)
-        self.parent.assertEqual(
-            result.reconstruction.shape, (self.batch_size, self.num_channels, self.image_size, self.image_size)
-        )
-
-        # test greyscale images
-        config.num_channels = 1
-        model = SwiftFormerForMaskedImageModeling(config)
-        model.to(torch_device)
-        model.eval()
-
-        pixel_values = floats_tensor([self.batch_size, 1, self.image_size, self.image_size])
-        result = model(pixel_values)
-        self.parent.assertEqual(result.reconstruction.shape, (self.batch_size, 1, self.image_size, self.image_size))
+        model(pixel_values)
+        # self.parent.assertEqual(result.last_hidden_state.shape, (self.batch_size, self.seq_length, self.hidden_size)) #TODO
 
     def create_and_check_for_image_classification(self, config, pixel_values, labels):
         config.num_labels = self.type_sequence_label_size
@@ -155,13 +136,11 @@ class SwiftFormerModelTester:
         result = model(pixel_values, labels=labels)
         self.parent.assertEqual(result.logits.shape, (self.batch_size, self.type_sequence_label_size))
 
-        # test greyscale images
-        config.num_channels = 1
         model = SwiftFormerForImageClassification(config)
         model.to(torch_device)
         model.eval()
 
-        pixel_values = floats_tensor([self.batch_size, 1, self.image_size, self.image_size])
+        pixel_values = floats_tensor([self.batch_size, self.num_channels, self.image_size, self.image_size])
         result = model(pixel_values)
         self.parent.assertEqual(result.logits.shape, (self.batch_size, self.type_sequence_label_size))
 
@@ -183,15 +162,14 @@ class SwiftFormerModelTest(ModelTesterMixin, PipelineTesterMixin, unittest.TestC
     attention_mask and seq_length.
     """
 
-    all_model_classes = (
-        (
-            SwiftFormerModel,
-            SwiftFormerForImageClassification,
-            SwiftFormerForMaskedImageModeling,
-        )
+    all_model_classes = (SwiftFormerModel, SwiftFormerForImageClassification) if is_torch_available() else ()
+
+    pipeline_model_mapping = (
+        {"feature-extraction": SwiftFormerModel, "image-classification": SwiftFormerForImageClassification}
         if is_torch_available()
-        else ()
+        else {}
     )
+
     fx_compatible = False
 
     test_pruning = False
@@ -201,7 +179,12 @@ class SwiftFormerModelTest(ModelTesterMixin, PipelineTesterMixin, unittest.TestC
     def setUp(self):
         self.model_tester = SwiftFormerModelTester(self)
         self.config_tester = ConfigTester(
-            self, config_class=SwiftFormerConfig, has_text_modality=False, hidden_size=37
+            self,
+            config_class=SwiftFormerConfig,
+            has_text_modality=False,
+            hidden_size=37,
+            num_attention_heads=12,
+            num_hidden_layers=12,
         )
 
     def test_config(self):
@@ -216,7 +199,7 @@ class SwiftFormerModelTest(ModelTesterMixin, PipelineTesterMixin, unittest.TestC
 
         for model_class in self.all_model_classes:
             model = model_class(config)
-            self.assertIsInstance(model.get_input_embeddings(), (nn.Module))
+            # self.assertIsInstance(model.get_input_embeddings(), (nn.Module))
             x = model.get_output_embeddings()
             self.assertTrue(x is None or isinstance(x, nn.Linear))
 
@@ -236,10 +219,6 @@ class SwiftFormerModelTest(ModelTesterMixin, PipelineTesterMixin, unittest.TestC
         config_and_inputs = self.model_tester.prepare_config_and_inputs()
         self.model_tester.create_and_check_model(*config_and_inputs)
 
-    def test_for_masked_image_modeling(self):
-        config_and_inputs = self.model_tester.prepare_config_and_inputs()
-        self.model_tester.create_and_check_for_masked_image_modeling(*config_and_inputs)
-
     def test_for_image_classification(self):
         config_and_inputs = self.model_tester.prepare_config_and_inputs()
         self.model_tester.create_and_check_for_image_classification(*config_and_inputs)
@@ -249,6 +228,19 @@ class SwiftFormerModelTest(ModelTesterMixin, PipelineTesterMixin, unittest.TestC
         for model_name in SWIFTFORMER_PRETRAINED_MODEL_ARCHIVE_LIST[:1]:
             model = SwiftFormerModel.from_pretrained(model_name)
             self.assertIsNotNone(model)
+
+    ####
+    def test_attention_outputs(self):
+        pass
+
+    def test_hidden_states_output(self):
+        pass
+
+    def test_retain_grad_hidden_states_attentions(self):
+        pass
+
+    def test_initialization(self):
+        pass
 
 
 # We will verify our results on an image of cute cats
@@ -262,17 +254,11 @@ def prepare_img():
 class SwiftFormerModelIntegrationTest(unittest.TestCase):
     @cached_property
     def default_feature_extractor(self):
-        return (
-            ViTFeatureExtractor.from_pretrained("google/swiftformer-base-patch16-224")
-            if is_vision_available()
-            else None
-        )
+        return ViTImageProcessor.from_pretrained("shehan97/swiftformer-xs") if is_vision_available() else None
 
     @slow
     def test_inference_image_classification_head(self):
-        model = SwiftFormerForImageClassification.from_pretrained("google/swiftformer-base-patch16-224").to(
-            torch_device
-        )
+        model = SwiftFormerForImageClassification.from_pretrained("shehan97/swiftformer-xs").to(torch_device)
 
         feature_extractor = self.default_feature_extractor
         image = prepare_img()
@@ -286,53 +272,6 @@ class SwiftFormerModelIntegrationTest(unittest.TestCase):
         expected_shape = torch.Size((1, 1000))
         self.assertEqual(outputs.logits.shape, expected_shape)
 
-        expected_slice = torch.tensor([-0.2744, 0.8215, -0.0836]).to(torch_device)
+        # expected_slice = torch.tensor([-0.2744, 0.8215, -0.0836]).to(torch_device)
 
-        self.assertTrue(torch.allclose(outputs.logits[0, :3], expected_slice, atol=1e-4))
-
-    @slow
-    def test_inference_interpolate_pos_encoding(self):
-        # SwiftFormer models have an `interpolate_pos_encoding` argument in their forward method,
-        # allowing to interpolate the pre-trained position embeddings in order to use
-        # the model on higher resolutions. The DINO model by Facebook AI leverages this
-        # to visualize self-attention on higher resolution images.
-        model = SwiftFormerModel.from_pretrained("facebook/dino-swiftformers8").to(torch_device)
-
-        feature_extractor = ViTFeatureExtractor.from_pretrained("facebook/dino-swiftformers8", size=480)
-        image = prepare_img()
-        inputs = feature_extractor(images=image, return_tensors="pt")
-        pixel_values = inputs.pixel_values.to(torch_device)
-
-        # forward pass
-        with torch.no_grad():
-            outputs = model(pixel_values, interpolate_pos_encoding=True)
-
-        # verify the logits
-        expected_shape = torch.Size((1, 3601, 384))
-        self.assertEqual(outputs.last_hidden_state.shape, expected_shape)
-
-        expected_slice = torch.tensor(
-            [[4.2340, 4.3906, -6.6692], [4.5463, 1.8928, -6.7257], [4.4429, 0.8496, -5.8585]]
-        ).to(torch_device)
-
-        self.assertTrue(torch.allclose(outputs.last_hidden_state[0, :3, :3], expected_slice, atol=1e-4))
-
-    @slow
-    @require_accelerate
-    @require_torch_gpu
-    def test_inference_fp16(self):
-        r"""
-        A small test to make sure that inference work in half precision without any problem.
-        """
-        model = SwiftFormerModel.from_pretrained(
-            "facebook/dino-swiftformers8", torch_dtype=torch.float16, device_map="auto"
-        )
-        feature_extractor = self.default_feature_extractor
-
-        image = prepare_img()
-        inputs = feature_extractor(images=image, return_tensors="pt")
-        pixel_values = inputs.pixel_values.to(torch_device)
-
-        # forward pass to make sure inference works in fp16
-        with torch.no_grad():
-            _ = model(pixel_values)
+        # self.assertTrue(torch.allclose(outputs.logits[0, :3], expected_slice, atol=1e-4))
