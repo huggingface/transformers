bb40cb01aec89821650014fd4cc1dfd7fa28ddc0
younesbelkada <younesbelkada@gmail.com>
2023-04-15 15:20:09 +0000

    some clean up

--- src/transformers/models/sam/modeling_sam.py
+++ src/transformers/models/sam/modeling_sam.py
@@ -492,35 +492,10 @@ class SamMaskDecoder(nn.Module):
         Returns:
           torch.Tensor: batched predicted masks torch.Tensor: batched predictions of mask quality
         """
-        masks, iou_pred = self.predict_masks(
-            image_embeddings=image_embeddings,
-            image_pe=image_pe,
-            sparse_prompt_embeddings=sparse_prompt_embeddings,
-            dense_prompt_embeddings=dense_prompt_embeddings,
-        )
-
-        # Select the correct mask or masks for outptu
-        if multimask_output:
-            mask_slice = slice(1, None)
-        else:
-            mask_slice = slice(0, 1)
-        masks = masks[:, mask_slice, :, :]
-        iou_pred = iou_pred[:, mask_slice]
-
-        # Prepare output
-        return masks, iou_pred
-
-    def predict_masks(
-        self,
-        image_embeddings: torch.Tensor,
-        image_pe: torch.Tensor,
-        sparse_prompt_embeddings: torch.Tensor,
-        dense_prompt_embeddings: torch.Tensor,
-    ) -> Tuple[torch.Tensor, torch.Tensor]:
-        """Predicts masks. See 'forward' for more details."""
         # Concatenate output tokens
         output_tokens = torch.cat([self.iou_token.weight, self.mask_tokens.weight], dim=0)
         output_tokens = output_tokens.unsqueeze(0).expand(sparse_prompt_embeddings.size(0), -1, -1)
+
         tokens = torch.cat((output_tokens, sparse_prompt_embeddings), dim=1)
         tokens = tokens.to(self.iou_token.weight.dtype)
 
@@ -528,7 +503,7 @@ class SamMaskDecoder(nn.Module):
         src = torch.repeat_interleave(image_embeddings, tokens.shape[0], dim=0)
         src = src + dense_prompt_embeddings
         pos_src = torch.repeat_interleave(image_pe, tokens.shape[0], dim=0)
-        b, c, h, w = src.shape
+        batch_size, num_channels, height, width = src.shape
 
         # Run the transformer
         hs, src = self.transformer(src, pos_src, tokens)
@@ -536,7 +511,7 @@ class SamMaskDecoder(nn.Module):
         mask_tokens_out = hs[:, 1 : (1 + self.num_mask_tokens), :]
 
         # Upscale mask embeddings and predict masks using the mask tokens
-        src = src.transpose(1, 2).view(b, c, h, w)
+        src = src.transpose(1, 2).view(batch_size, num_channels, height, width)
 
         upscaled_embedding = self.upscale_conv1(src)
         upscaled_embedding = self.activation(self.upscale_layer_norm(upscaled_embedding))
@@ -547,12 +522,21 @@ class SamMaskDecoder(nn.Module):
         for i in range(self.num_mask_tokens):
             hyper_in_list.append(self.output_hypernetworks_mlps[i](mask_tokens_out[:, i, :]))
         hyper_in = torch.stack(hyper_in_list, dim=1)
-        b, c, h, w = upscaled_embedding.shape
-        masks = (hyper_in @ upscaled_embedding.view(b, c, h * w)).view(b, -1, h, w)
+        batch_size, num_channels, height, width = upscaled_embedding.shape
+        masks = (hyper_in @ upscaled_embedding.view(batch_size, num_channels, height * width)).view(batch_size, -1, height, width)
 
         # Generate mask quality predictions
         iou_pred = self.iou_prediction_head(iou_token_out)
 
+        # Select the correct mask or masks for output
+        if multimask_output:
+            mask_slice = slice(1, None)
+        else:
+            mask_slice = slice(0, 1)
+        masks = masks[:, mask_slice, :, :]
+        iou_pred = iou_pred[:, mask_slice]
+
+        # Prepare output
         return masks, iou_pred
 
 
