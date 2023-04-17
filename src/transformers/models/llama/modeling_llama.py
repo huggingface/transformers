98b0ff3f0d5fd0c822bb4154c895d15838278a0d
Kunhao ZHENG <dyekuu@gmail.com>
2023-04-17 18:28:48 +0200

    Fix squeeze into torch 1.x compatible form in llama model (#22808)
    fix-squeeze-tuple

--- src/transformers/models/llama/modeling_llama.py
+++ src/transformers/models/llama/modeling_llama.py
@@ -132,8 +132,8 @@ def rotate_half(x):
 
 def apply_rotary_pos_emb(q, k, cos, sin, position_ids):
     # The first two dimensions of cos and sin are always 1, so we can `squeeze` them.
-    cos = cos.squeeze((0, 1))  # [seq_len, dim]
-    sin = sin.squeeze((0, 1))  # [seq_len, dim]
+    cos = cos.squeeze(1).squeeze(0)  # [seq_len, dim]
+    sin = sin.squeeze(1).squeeze(0)  # [seq_len, dim]
     cos = cos[position_ids].unsqueeze(1)  # [bs, 1, seq_len, dim]
     sin = sin[position_ids].unsqueeze(1)  # [bs, 1, seq_len, dim]
     q_embed = (q * cos) + (rotate_half(q) * sin)
