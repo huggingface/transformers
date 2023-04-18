18974172aca33342e934b2c5bd18d7e1286db66f
Sylvain Gugger <Sylvain.gugger@gmail.com>
2023-04-18 14:06:28 -0400

    PoC for some chaining API

---
+++ src/transformers/new_pipelines/chain.py
@@ -0,0 +1,38 @@
+class SimpleChainer:
+    """
+    Example:
+
+    ```py
+    from transformers import pipeline
+    from transformers.new_pipelines import SimpleChainer
+
+    chain = SimpleChainer(
+        pipeline("translation", model="facebook/nllb-200-distilled-600M", src_lang="fra_Latn", tgt_lang="eng_Latn"),
+        pipeline(model="gpt2"),
+    )
+    chain("Je m'appelle Lysandre et mon bagel préféré est")
+    ```
+    """
+
+    def __init__(self, *functions):
+        self.functions = functions
+
+    def __call__(self, *args, **kwargs):
+        output = self.functions[0](*args, **kwargs)
+        for func in self.functions[1:]:
+            if isinstance(output, (tuple, list)):
+                if len(output) == 1 and isinstance(output[0], dict):
+                    if len(output[0]) == 1:
+                        output = func(*list(output[0].values()))
+                    else:
+                        output = func(**output[0])
+                else:
+                    output = func(*output[0])
+            elif isinstance(output, dict):
+                if len(output) == 1:
+                    output = func(*list(output.values()))
+                else:
+                    output = func(**output)
+            else:
+                output = func(output)
+        return output
