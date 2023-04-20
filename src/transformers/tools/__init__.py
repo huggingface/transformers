73068409e9d215cd5300f730dbfe2c51b0beb4dd
Sylvain Gugger <Sylvain.gugger@gmail.com>
2023-04-20 14:48:32 -0400

    Basic python interpreter

--- src/transformers/tools/__init__.py
+++ src/transformers/tools/__init__.py
@@ -1,4 +1,4 @@
-from .base import Tool
+from .base import PipelineTool
 from .chain import SimpleChainer
 from .text_classification import TextClassificationTool
 from .text_to_speech import TextToSpeechTool
