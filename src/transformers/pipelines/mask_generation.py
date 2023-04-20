7d8b860e1aa5986d7be1c6af03236073c3952e70
younesbelkada <younesbelkada@gmail.com>
2023-04-20 15:49:27 +0000

    more reformat

--- src/transformers/pipelines/mask_generation.py
+++ src/transformers/pipelines/mask_generation.py
@@ -56,10 +56,10 @@ class MaskGenerationPipeline(ChunkPipeline):
             The tokenizer that will be used by the pipeline to encode data for the model. This object inherits from
             [`PreTrainedTokenizer`].
         feature_extractor ([`SequenceFeatureExtractor`]):
-            The feature extractor that will be used by the pipeline to encode waveform for the model.
+            The feature extractor that will be used by the pipeline to encode the input.
         points_per_batch (*optional*, int, default to 64):
-            Sets the number of points run simultaneously by the model. heightigher numbers may be faster but use more
-            GPU memory.
+            Sets the number of points run simultaneously by the model. Higher numbers may be faster but use more GPU
+            memory.
         output_bboxes_mask (`bool`, *optional*, default to `False`):
            Whether or not to output the bounding box predictions.
         output_rle_masks (`bool`, *optional*, default to `False`):
