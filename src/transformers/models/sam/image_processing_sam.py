796ec850f92250d6218b846ac1ae08a56461940c
arthur.zucker@gmail.com <arthur.zucker@gmail.com>
2023-04-20 11:38:35 +0000

    update docstrings

--- src/transformers/models/sam/image_processing_sam.py
+++ src/transformers/models/sam/image_processing_sam.py
@@ -442,8 +442,14 @@ class SamImageProcessor(BaseImageProcessor):
         Docstring to come
 
         Args:
+            masks (`torch.Tensor`):
+                Input masks.
+            iou_scores (`torch.Tensor`):
+                List of IoU scores.
+            original_size (`Tuple[int,int]`):
+                Size of the orginal image.
             cropped_box_image (``):
-                describe this input. Image in a crop box
+                Croping bounding boxes used to crop the images. 
 
         """
         requires_backends(self, ["torch"])
@@ -464,7 +470,7 @@ class SamImageProcessor(BaseImageProcessor):
         if pred_iou_thresh > 0.0:
             keep_mask = keep_mask & (iou_scores > pred_iou_thresh)
 
-        # Calculate stability score
+        # compute stability score
         if stability_score_thresh > 0.0:
             stability_scores = _compute_stability_score(masks, mask_threshold, stability_score_offset)
             keep_mask = keep_mask & (stability_scores > stability_score_thresh)
@@ -491,7 +497,7 @@ class SamImageProcessor(BaseImageProcessor):
         return masks, scores, converted_boxes
 
 
-def _compute_stability_score(masks, mask_threshold, stability_score_offset):
+def _compute_stability_score(masks:torch.Tensor, mask_threshold:float, stability_score_offset:int):
     # One mask is always contained inside the other.
     # Save memory by preventing unnecesary cast to torch.int64
     intersections = (
@@ -512,7 +518,7 @@ def _build_point_grid(n_per_side: int) -> np.ndarray:
     return points
 
 
-def _normalize_coordinates(target_size, coords: np.ndarray, original_size, is_bounding_box=False) -> np.ndarray:
+def _normalize_coordinates(target_size:int, coords: np.ndarray, original_size:Tuple[int,int], is_bounding_box=False) -> np.ndarray:
     """
     Expects a numpy array of length 2 in the final dimension. Requires the original image size in (height, width)
     format.
@@ -527,14 +533,12 @@ def _normalize_coordinates(target_size, coords: np.ndarray, original_size, is_bo
     coords = deepcopy(coords).astype(float)
 
     if is_bounding_box:
-        # reshape to .reshape(-1, 2, 2)
         coords = coords.reshape(-1, 2, 2)
 
     coords[..., 0] = coords[..., 0] * (new_width / old_width)
     coords[..., 1] = coords[..., 1] * (new_height / old_height)
 
     if is_bounding_box:
-        # reshape back to .reshape(-1, 4)
         coords = coords.reshape(-1, 4)
 
     return coords
@@ -542,7 +546,7 @@ def _normalize_coordinates(target_size, coords: np.ndarray, original_size, is_bo
 
 def _generate_crop_boxes(
     image,
-    target_size,
+    target_size:int, # Is it tuple here? 
     n_layers: int = 0,
     overlap_ratio: float = 512 / 1500,
     points_per_crop: Optional[int] = 32,
@@ -551,6 +555,7 @@ def _generate_crop_boxes(
 ) -> Tuple[List[List[int]], List[int]]:
     """
     Generates a list of crop boxes of different sizes. Each layer has (2**i)**2 boxes for the ith layer.
+    TODO doctring
     """
     if device is None:
         device = torch.device("cpu")
@@ -583,6 +588,7 @@ def _generate_crop_boxes(
 
 def _generate_per_layer_crops(n_layers, overlap_ratio, original_size):
     """
+    TODO more details
     Generates 2 ** (layers idx + 1) crops for each n_layers. Crops are in the XYWH format : The XYWH format consists of
     the following required indices:
         X: X coordinate of the left of the bounding box Y: Y coordinate of the top of the bounding box WIDTH: width of
@@ -615,6 +621,7 @@ def _generate_per_layer_crops(n_layers, overlap_ratio, original_size):
 
 def _generate_crop_images(crop_boxes, image, points_grid, layer_idxs, target_size, original_size):
     """
+    TODO 
     What is this doing? Takes as an input bounding boxes that are used to crop the image. Based in the crops, the
     corresponding points are also passed.
     """
@@ -665,6 +672,7 @@ def _is_box_near_crop_edge(boxes, crop_box, orig_box, atol=20.0):
 
 def _batched_mask_to_box(masks):
     """
+    TODO 
     Computes the bounding boxes around the given input masks. The bounding boxes are in the XYXY format which
     corresponds the following required indices:
         - LEFT: left hand side of the bounding box
