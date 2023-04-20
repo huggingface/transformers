7c47fa85a7af6f65eaf45e79802323e5fa1eca4f
Zach Mueller <muellerzr@gmail.com>
2023-04-20 14:19:40 +0000

    Change launcher

--- tests/deepspeed/test_deepspeed.py
+++ tests/deepspeed/test_deepspeed.py
@@ -123,7 +123,8 @@ def get_launcher(distributed=False):
     # results with mode gpus because we use very little data)
     num_gpus = min(2, get_gpu_count()) if distributed else 1
     master_port = get_master_port(real_launcher=True)
-    return f"deepspeed --num_nodes 1 --num_gpus {num_gpus} --master_port {master_port}".split()
+    return f"accelerate launch --use_deepspeed --num_processes {num_gpus} --main_process_port {master_port}".split()
+    # return f"deepspeed --num_nodes 1 --num_gpus {num_gpus} --master_port {master_port}".split()
 
 
 ZERO2 = "zero2"
@@ -1082,11 +1083,11 @@ class TestDeepSpeedWithLauncher(TestCasePlus):
             remove_args = remove_args_str.split()
             args = [x for x in args if x not in remove_args]
 
-        ds_args = f"--deepspeed {self.test_file_dir_str}/ds_config_{stage}.json".split()
+        ds_args = f"--deepspeed_config_file {self.test_file_dir_str}/ds_config_{stage}.json".split()
         script = [f"{self.examples_dir_str}/pytorch/translation/run_translation.py"]
         launcher = get_launcher(distributed)
 
-        cmd = launcher + script + args + ds_args
+        cmd = launcher + ds_args + script + args
         # keep for quick debug
         # print(" ".join([f"\nPYTHONPATH={self.src_dir_str}"] +cmd)); die
         execute_subprocess_async(cmd, env=self.get_env())
@@ -1124,7 +1125,7 @@ class TestDeepSpeedWithLauncher(TestCasePlus):
         script = [f"{self.examples_dir_str}/pytorch/language-modeling/run_clm.py"]
         launcher = get_launcher(distributed=True)
 
-        cmd = launcher + script + args + ds_args
+        cmd = launcher + ds_args + script + args
         # keep for quick debug
         # print(" ".join([f"\nPYTHONPATH={self.src_dir_str}"] +cmd)); die
         execute_subprocess_async(cmd, env=self.get_env())
@@ -1155,7 +1156,7 @@ class TestDeepSpeedWithLauncher(TestCasePlus):
         script = [f"{self.examples_dir_str}/pytorch/language-modeling/run_clm.py"]
         launcher = get_launcher(distributed=True)
 
-        cmd = launcher + script + args + ds_args
+        cmd = launcher + ds_args + script + args
         # keep for quick debug
         # print(" ".join([f"\nPYTHONPATH={self.src_dir_str}"] +cmd)); die
         with CaptureStderr() as cs:
