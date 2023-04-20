5a983f1af751ddea1a6ebef908ce37c5d01fb40d
LiangSong <sl12160010@gmail.com>
2023-04-20 22:57:58 +0800

    update stable embedding test

--- tests/models/open_llama/test_modeling_open_llama.py
+++ tests/models/open_llama/test_modeling_open_llama.py
@@ -22,7 +22,7 @@ from transformers.testing_utils import require_torch, torch_device
 
 from ...generation.test_utils import GenerationTesterMixin
 from ...test_configuration_common import ConfigTester
-from ...test_modeling_common import ModelTesterMixin, ids_tensor, random_attention_mask
+from ...test_modeling_common import ModelTesterMixin, _config_zero_init, ids_tensor, random_attention_mask
 from ...test_pipeline_mixin import PipelineTesterMixin
 
 
@@ -280,6 +280,22 @@ class OpenLlamaModelTest(ModelTesterMixin, GenerationTesterMixin, PipelineTester
 
     def test_config(self):
         self.config_tester.run_common_tests()
+
+    def test_initialization(self):
+        config, inputs_dict = self.model_tester.prepare_config_and_inputs_for_common()
+
+        configs_no_init = _config_zero_init(config)
+        for model_class in self.all_model_classes:
+            model = model_class(config=configs_no_init)
+            for name, param in model.named_parameters():
+                if config.use_stable_embedding and "embed_tokens" in name:
+                    continue
+                if param.requires_grad:
+                    self.assertIn(
+                        ((param.data.mean() * 1e9).round() / 1e9).item(),
+                        [0.0, 1.0],
+                        msg=f"Parameter {name} of model {model_class} seems not properly initialized",
+                    )
 
     def test_model(self):
         config_and_inputs = self.model_tester.prepare_config_and_inputs()
