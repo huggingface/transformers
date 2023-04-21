0ce8ba4e935f182d4f2177dab8ec811a6ec86e8d
ydshieh <ydshieh@users.noreply.github.com>
2023-04-21 22:37:49 +0200

    fix

--- tests/pipelines/test_pipelines_mask_generation.py
+++ tests/pipelines/test_pipelines_mask_generation.py
@@ -67,12 +67,12 @@ class MaskGenerationPipelineTests(unittest.TestCase):
         ]
 
     # TODO: Fix me @Arthur
-    def run_pipeline_test(self):
+    def run_pipeline_test(self, mask_generator, examples):
         pass
 
     @require_tf
     @unittest.skip("Image segmentation not implemented in TF")
-    def test_small_model_tf(self, mask_generator, examples):
+    def test_small_model_tf(self):
         pass
 
     @slow
