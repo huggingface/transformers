50671ef216d3f9c9a860c42f728dda0d1dd56fcb
Niels <niels.rogge1@gmail.com>
2023-04-24 22:06:35 +0200

    Delete files

--- src/transformers/models/mask_rcnn/test_pipeline.py
+++
@@ -1,8 +0,0 @@
-from transformers import MaskRCNNImageProcessor, pipeline
-
-
-pipe = pipeline(
-    task="object-detection", model="nielsr/convnext-tiny-maskrcnn", image_processor=MaskRCNNImageProcessor()
-)
-
-pipe("https://miro.medium.com/max/1000/0*w1s81z-Q72obhE_z")
