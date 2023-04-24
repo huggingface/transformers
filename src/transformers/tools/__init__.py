f95048dc70796837909fb194838e29e4aaab2417
Lysandre <hi@lyand.re>
2023-04-24 10:32:17 +0000

    captioning + s2t fixes

--- src/transformers/tools/__init__.py
+++ src/transformers/tools/__init__.py
@@ -2,6 +2,7 @@ from .agents import Agent, OpenAIAgent, OpenAssistantAgent
 from .base import PipelineTool
 from .chain import SimpleChainer
 from .generative_question_answering import GenerativeQuestionAnsweringTool
+from .image_captioning import ImageCaptioningTool
 from .language_identifier import LanguageIdentificationTool
 from .speech_to_text import SpeechToTextTool
 from .text_classification import TextClassificationTool
