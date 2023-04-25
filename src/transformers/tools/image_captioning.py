4cd8879812d2c8f9b3d528e1aeef2ed3e54a9b12
Sylvain Gugger <Sylvain.gugger@gmail.com>
2023-04-25 16:03:16 -0400

    More remote tools

--- src/transformers/tools/image_captioning.py
+++ src/transformers/tools/image_captioning.py
@@ -1,6 +1,8 @@
+import io
+
 from ..models.auto import AutoModelForVision2Seq, AutoProcessor
 from ..utils import is_vision_available
-from .base import PipelineTool
+from .base import PipelineTool, RemoteTool
 
 
 try:
@@ -33,3 +35,22 @@ class ImageCaptioningTool(PipelineTool):
 
     def decode(self, outputs):
         return self.pre_processor.batch_decode(outputs, skip_special_tokens=True)[0].strip()
+
+
+class RemoteImageCaptioningTool(RemoteTool):
+    default_checkpoint = "Salesforce/blip-image-captioning-large"
+    description = (
+        "This is a tool that generates a description of an image. It takes an input named `image` which should be the "
+        "image to caption, and returns a text that contains the description in English."
+    )
+
+    def extract_outputs(self, outputs):
+        return outputs[0]["generated_text"]
+
+    def prepare_inputs(self, image):
+        if isinstance(image, bytes):
+            return {"data": image}
+
+        byte_io = io.BytesIO()
+        image.save(byte_io, format="PNG")
+        return {"data": byte_io.getvalue()}
