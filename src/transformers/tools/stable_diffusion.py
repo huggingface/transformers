739b7c0d0ebe3c82593c17ada51266ba5236dddd
Sylvain Gugger <Sylvain.gugger@gmail.com>
2023-04-25 15:10:43 -0400

    Clean up remote tools

--- src/transformers/tools/stable_diffusion.py
+++ src/transformers/tools/stable_diffusion.py
@@ -1,23 +1,23 @@
-from transformers.tools.base import Tool
-from diffusers import DiffusionPipeline
 from accelerate.state import PartialState
+from diffusers import DiffusionPipeline
+
+from transformers.tools.base import Tool
 
 
 class StableDiffusionTool(Tool):
-
     default_checkpoint = "runwayml/stable-diffusion-v1-5"
     description = (
         "This is a tool that creates an image according to a text description. It takes an input named `text` which "
         "contains the image description and outputs an image."
     )
-    
+
     def __init__(self, device=None) -> None:
         super().__init__()
 
         self.device = device
         self.pipeline = None
 
-    def setup(self): 
+    def setup(self):
         if self.device is None:
             self.device = PartialState().default_device
 
@@ -31,6 +31,3 @@ class StableDiffusionTool(Tool):
             self.setup()
 
         return self.pipeline(text).images[0]
-
-
-
