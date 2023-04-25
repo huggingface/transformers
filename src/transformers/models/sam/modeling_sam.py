3863c2e5d74968239b6c9bf13291a403ef284b5d
younesbelkada <younesbelkada@gmail.com>
2023-04-25 11:35:19 +0000

    multiple fixes

--- src/transformers/models/sam/modeling_sam.py
+++ src/transformers/models/sam/modeling_sam.py
@@ -1281,7 +1281,7 @@ class SamModel(SamPreTrainedModel):
         >>> model = AutoModel.from_pretrained("facebook/sam-vit-base")
         >>> processor = AutoProcessor.from_pretrained("facebook/sam-vit-base")
 
-        >>> img_url = "https://huggingface.co/ybelkada/segment-anything/resolve/main/assets/car.png"
+        >>> img_url = "https://huggingface.co/datasets/huggingface/documentation-images/resolve/main/transformers/model_doc/sam-car.png"
         >>> raw_image = Image.open(requests.get(img_url, stream=True).raw).convert("RGB")
         >>> input_points = [[[400, 650]]]  # 2D location of a window on the car
         >>> inputs = processor(images=raw_image, input_points=input_points, return_tensors="pt")
@@ -1291,7 +1291,7 @@ class SamModel(SamPreTrainedModel):
 
         >>> # Postprocess masks
         >>> masks = processor.post_process_masks(
-        ...     outputs.pred_masks.cpu(), inputs["original_sizes"].cpu(), inputs["reshaped_input_sizes"].cpu()
+        ...     outputs.pred_masks, inputs["original_sizes"], inputs["reshaped_input_sizes"]
         ... )
         ```
         """
