b4ebe76fea430e56ad8e82f62ac7063a3bfd5f2c
Lysandre <hi@lyand.re>
2023-04-25 12:32:22 +0000

    Image segmentation

---
+++ src/transformers/tools/image_segmentation.py
@@ -0,0 +1,43 @@
+from typing import List
+from transformers import AutoProcessor, CLIPSegForImageSegmentation, is_vision_available
+
+from .base import PipelineTool
+
+
+try:
+    from PIL.Image import Image
+except ImportError:
+    pass
+
+
+class ImageSegmentationTool(PipelineTool):
+    pre_processor_class = AutoProcessor
+    model_class = CLIPSegForImageSegmentation
+
+    description = (
+        "This is a tool that generates a description of an image. It takes an input named `image` which should be the "
+        "image to caption, and returns a text that contains the description in English."
+    )
+    default_checkpoint = "CIDAS/clipseg-rd64-refined"
+
+    def __init__(self, *args, **kwargs):
+        if not is_vision_available():
+            raise ValueError("Pillow must be installed to use the ImageCaptioningTool.")
+
+        super().__init__(*args, **kwargs)
+
+    def encode(self, texts: List[str], image: "Image"):
+        return self.pre_processor(text=texts, images=[image] * len(texts), padding=True, return_tensors="pt")
+
+    def decode(self, outputs):
+        logits_array = outputs.logits
+        segmentation_maps = []
+
+        for logits in logits_array:
+            array = logits.cpu().detach().numpy()
+            array[array < 0] = 0
+            array[array >= 0] = 1
+            segmentation_maps.append(array)
+
+        return segmentation_maps
+
