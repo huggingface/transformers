908d7a5e6ead70a73a869fa27fb55b4d43176dcb
arthur.zucker@gmail.com <arthur.zucker@gmail.com>
2023-04-27 13:30:38 +0000

    don't  touch tee in this PR

--- .circleci/create_circleci_config.py
+++ .circleci/create_circleci_config.py
@@ -173,7 +173,7 @@ class CircleCIJob:
             test_command += " $(cat splitted_tests.txt)"
         if self.marker is not None:
             test_command += f" -m {self.marker}"
-        # test_command += " | tee tests_output.txt"
+        test_command += " | tee tests_output.txt"
         steps.append({"run": {"name": "Run tests", "command": test_command}})
         steps.append({"store_artifacts": {"path": "~/transformers/tests_output.txt"}})
         steps.append({"store_artifacts": {"path": "~/transformers/reports"}})
