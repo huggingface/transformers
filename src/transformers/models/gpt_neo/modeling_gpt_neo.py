889882237d3a15a44c8bb4e1ca892cd929ee4640
Prof. Peter Schneider-Kamp <jps@ordbogen.com>
2023-04-27 16:53:38 +0200

    classifier_dropout is always in config

--- src/transformers/models/gpt_neo/modeling_gpt_neo.py
+++ src/transformers/models/gpt_neo/modeling_gpt_neo.py
@@ -942,13 +942,7 @@ class GPTNeoForTokenClassification(GPTNeoPreTrainedModel):
         self.num_labels = config.num_labels
 
         self.transformer = GPTNeoModel(config)
-        if hasattr(config, "classifier_dropout") and config.classifier_dropout is not None:
-            classifier_dropout = config.classifier_dropout
-        elif hasattr(config, "hidden_dropout") and config.hidden_dropout is not None:
-            classifier_dropout = config.hidden_dropout
-        else:
-            classifier_dropout = 0.1
-        self.dropout = nn.Dropout(classifier_dropout)
+        self.dropout = nn.Dropout(config.classifier_dropout)
         self.classifier = nn.Linear(config.hidden_size, config.num_labels)
 
         # Initialize weights and apply final processing
