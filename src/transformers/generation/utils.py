b73fe3248f20f9d9f8ff83e0cc291d28b8d87b90
Joao Gante <joao@huggingface.co>
2023-04-28 17:53:25 +0000

    tmp commit

--- src/transformers/generation/utils.py
+++ src/transformers/generation/utils.py
@@ -4177,7 +4177,6 @@ class GenerationMixin:
         >>> tokenizer.batch_decode(outputs, skip_special_tokens=True)
         ["It might be possible to get a better understanding of the nature of the problem, but it's not"]
         ```"""
-        # NOTE: the code here is copy/paste from greedy search/sample, except when clearly stated in the comments
         # Assistant: initialize assistant-related variables
         if not hasattr(assistant_model, "max_assistant_tokens"):
             assistant_model.max_assistant_tokens = 5  # this value, which will be updated, persists across calls
@@ -4243,6 +4242,7 @@ class GenerationMixin:
             # need access to the assistant cache to secure strong speedups.
             candidate_input_ids = input_ids
             for _ in range(int(assistant_model.max_assistant_tokens)):
+
                 # 1.1. use the assistant model to obtain the next candidate logits
                 if "assistant_past_key_values" in model_kwargs:
                     prev_seq_len = model_kwargs["assistant_past_key_values"][0][0].shape[2]
@@ -4296,7 +4296,8 @@ class GenerationMixin:
             candidate_length = candidate_input_ids.shape[1] - input_ids.shape[1]
 
             # 2. Use the original model to obtain the next token logits given the candidate sequence. We obtain
-            # `candidate_length + 1` relevant logits from this process (see step 7 on why the +1)
+            # `candidate_length + 1` relevant logits from this process: in the event that all candidates are correct,
+            # we use this forward pass to also pick the subsequent logits in the original model.
 
             # 2.1. Run a forward pass on the candidate sequence
             if "past_key_values" in model_kwargs:
@@ -4343,19 +4344,17 @@ class GenerationMixin:
                 for i in range(candidate_length):
                     new_logits[:, i, :] = logits_warper(candidate_input_ids[:, : cur_len + i], new_logits[:, i, :])
 
-            # 3. Obtain the next tokens from the original model logits. If `do_sample` is True, use multinomial
-            # sampling, otherwise use argmax.
+            # 3. Obtain the next tokens from the original model logits.
             if do_sample:
-                probs = new_logits[:, -candidate_length - 1 :, :].softmax(dim=-1)
-                sampled_tokens = torch.multinomial(probs[0, :, :], num_samples=1).squeeze(1)[None, :]
-                next_tokens = sampled_tokens[:, :-1]
+                probs = new_logits[:, -candidate_length - 1:, :].softmax(dim=-1)
+                selected_tokens = torch.multinomial(probs[0, :, :], num_samples=1).squeeze(1)[None, :]
             else:
-                next_tokens = new_logits[:, -candidate_length - 1 : -1, :].argmax(dim=-1)
+                selected_tokens = new_logits[:, -candidate_length - 1:, :].argmax(dim=-1)
 
             # 4. Compare the argmax from the original model logits with the assistant forecasted tokens. We can keep
             # the assistant forecasted tokens until the first mismatch, or until the max length is reached.
             candidate_new_tokens = candidate_input_ids[:, -candidate_length:]
-            n_matches = ((~(candidate_new_tokens == next_tokens)).cumsum(dim=-1) < 1).sum()
+            n_matches = ((~(candidate_new_tokens == selected_tokens[:, :-1])).cumsum(dim=-1) < 1).sum()
 
             # 5. Adjust the max number of assistant tokens to use in the next iteration. This is a simple heuristic,
             # probably can be improved -- we want to balance the benefits of getting assistant tokens correct with the
@@ -4365,37 +4364,31 @@ class GenerationMixin:
             else:
                 assistant_model.max_assistant_tokens = max(1.0, assistant_model.max_assistant_tokens - 1.0)
 
-            # 6. Update variables according to the number of matching assistant tokens.
-            # 6.1. Ensure we don't generate beyond max_len or an EOS token (remember: one token will be added below)
-            n_matches = min(n_matches, max_len - cur_len - 1)
+            # 6. Update variables according to the number of matching assistant tokens. Remember: the token generated
+            # by the model after the last candidate match is also valid, as it is generated from a correct sequence.
+            # Because of this last token, assisted generation search reduces to a normal greedy search/sample if there
+            # is no match.
+
+            # 6.1. Ensure we don't generate beyond max_len or an EOS token
             if last_assistant_token_is_eos and n_matches == candidate_length:
                 n_matches -= 1
-            input_ids = candidate_input_ids[:, 0 : cur_len + n_matches]
-            new_cur_len = input_ids.shape[-1]
+            n_matches = min(n_matches, max_len - cur_len - 1)
+
+            # 6.2. Get the valid continuation, after the matching tokens
+            valid_tokens = selected_tokens[:, :n_matches + 1]
+            input_ids = torch.cat((input_ids, valid_tokens), dim=-1)
             if streamer is not None:
-                streamer.put(candidate_input_ids[:, cur_len : cur_len + n_matches])
+                streamer.put(valid_tokens)
+            new_cur_len = input_ids.shape[-1]
 
-            # 6.2. Discard past key values relative to unused assistant tokens
-            outputs.past_key_values = _crop_past_key_values(self, outputs.past_key_values, new_cur_len)
+            # 6.3. Discard past key values relative to unused assistant tokens
+            new_cache_size = new_cur_len - 1
+            outputs.past_key_values = _crop_past_key_values(self, outputs.past_key_values, new_cache_size)
             model_kwargs["assistant_past_key_values"] = _crop_past_key_values(
-                assistant_model, model_kwargs["assistant_past_key_values"], new_cur_len
-            )
+                assistant_model, model_kwargs["assistant_past_key_values"], new_cache_size - 1
+            )  # the assistant does not have the token after the last match, hence the -1
 
-            # 6.3. Extract the logits for the next token
-            next_token_scores = new_logits[:, n_matches, :]
-
-            # 7. Use the set of logits after the last matching assistant token to obtain the next token. Note that,
-            # because of this step, assisted generation search reduces to a normal greedy search/sample if there is no
-            # match.
-            if do_sample:
-                probs = probs[:, n_matches, :]
-                next_tokens = sampled_tokens[:, n_matches]
-            else:
-                next_tokens = torch.argmax(next_token_scores, dim=-1)
-
-            # Assistant: main logic end; Compared to greedy search/sample, the following (redundant) blocks were
-            # removed below: (1) model input preparation; (2) model forward pass; (3) score preparation; (4) model
-            # cache update.
+            # Assistant: main logic end
 
             if synced_gpus and this_peer_finished:
                 continue  # don't waste resources running the code we don't need
@@ -4441,15 +4434,6 @@ class GenerationMixin:
                             decoder_hidden_states, outputs.hidden_states, cur_len, last_matching_idx
                         )
 
-            # finished sentences should have their next token be a padding token
-            if eos_token_id is not None:
-                next_tokens = next_tokens * unfinished_sequences + pad_token_id * (1 - unfinished_sequences)
-
-            # update generated ids, model inputs, and length for next step
-            input_ids = torch.cat([input_ids, next_tokens[:, None]], dim=-1)
-            if streamer is not None:
-                streamer.put(next_tokens.cpu())
-
             model_kwargs = self._update_model_kwargs_for_generation(
                 outputs, model_kwargs, is_encoder_decoder=self.config.is_encoder_decoder
             )
@@ -4457,7 +4441,7 @@ class GenerationMixin:
             # if eos_token was found in one sentence, set sentence to finished
             if eos_token_id_tensor is not None:
                 unfinished_sequences = unfinished_sequences.mul(
-                    next_tokens.tile(eos_token_id_tensor.shape[0], 1).ne(eos_token_id_tensor.unsqueeze(1)).prod(dim=0)
+                    input_ids[:, -1].tile(eos_token_id_tensor.shape[0], 1).ne(eos_token_id_tensor.unsqueeze(1)).prod(dim=0)
                 )
 
                 # stop when each sentence is finished
