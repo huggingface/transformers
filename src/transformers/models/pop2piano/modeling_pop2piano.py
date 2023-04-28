4ff583f6b68b0c36f66ea315bf1e59e068883741
Susnato Dhar <susnatodhar10@gmail.com>
2023-04-29 00:43:57 +0530

    ruff changes

--- src/transformers/models/pop2piano/modeling_pop2piano.py
+++ src/transformers/models/pop2piano/modeling_pop2piano.py
@@ -1006,11 +1006,11 @@ Pop2Piano_START_DOCSTRING = r"""
     This model inherits from [`PreTrainedModel`]. Check the superclass documentation for the generic methods the
     library implements for all its model (such as downloading or saving, resizing the input embeddings, pruning heads
     etc.)
-    
+
     This model is also a PyTorch [torch.nn.Module](https://pytorch.org/docs/stable/nn.html#torch.nn.Module) subclass.
     Use it as a regular PyTorch Module and refer to the PyTorch documentation for all matter related to general usage
     and behavior.
-    
+
     Parameters:
         config ([`Pop2PianoConfig`]): Model configuration class with all the parameters of the model.
             Initializing with a config file does not load the weights associated with the model, only the
