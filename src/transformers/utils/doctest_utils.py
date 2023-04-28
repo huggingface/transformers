02fe69aec3f730089c46487d53207d2ea1c1bb88
arthur.zucker@gmail.com <arthur.zucker@gmail.com>
2023-04-28 14:34:43 +0000

    more cleaning

--- src/transformers/utils/doctest_utils.py
+++ src/transformers/utils/doctest_utils.py
@@ -13,16 +13,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """
-Utils to run the documentation tests without having to overwrite any files.s
+Utils to run the documentation tests without having to overwrite any files.
 
-The doc precossing function can be run on a list of files and/org directories of files. It will recursively check if
-the files have a python code snippet by looking for a ```python or ```py syntax. The script will add a new line before
-every python code ending ``` line to make the docstrings ready for pytest doctests. However, we don't want to have
-empty lines displayed in the official documentation which is why the new code is written to a temporary directory / is
-tested on the fly depending on the configuration.
+The `preprocess_string` function adds `# doctest: +IGNORE_RESULT` markers on the fly anywhere a `load_dataset` call is
+made as a print would otherwise fail the corresonding line.
 
-When debugging the doc tests locally, the script should automatically determine which files should be processed based
-on the modified files. It should also run the tests on the fly and delete the temp file when finished.
+To skip cuda tests, make sure to call `SKIP_CUDA_DOCTEST=1 pytest --doctest-modules <path_to_files_to_test>
 """
 import doctest
 import inspect
@@ -48,33 +44,30 @@ def preprocess_string(string, skip_cuda_tests):
     codeblock_pattern = r"(```(?:python|py)\s*\n\s*>>> )((?:.*?\n)*?.*?```)"
     codeblocks = re.split(re.compile(codeblock_pattern, flags=re.MULTILINE | re.DOTALL), string)
     for i, codeblock in enumerate(codeblocks):
-        # if ">>>" in codeblock and "```py" in codeblock:
-        #     # let's add everything we need here, but the DATASET_VERBOSITY should work.
-        #     finale_block = codeblock
-        #     finale_block += "import transformers;transformers.logging.set_verbosity_error();"
-        #     finale_block += (
-        #         "import datasets;datasets.logging.set_verbosity_error();from contextlib import redirect_stdout;"
-        #     )
-        #     finale_block += "import huggingface_hub;huggingface_hub.logging.set_verbosity_error();"
-        #     codeblocks[i] = finale_block
-        if "load_dataset(" in codeblock and not "# doctest: +IGNORE_RESULT" in codeblock:
-            # adding the ignore results is actually way better! 
-            codeblocks[i] = re.sub(
-                r"(>>> .*load_dataset\(.*)", r"\1 \# doctest: +IGNORE_RESULT", codeblock
-            )
+        if "load_dataset(" in codeblock and "# doctest: +IGNORE_RESULT" not in codeblock:
+            codeblocks[i] = re.sub(r"(>>> .*load_dataset\(.*)", r"\1 # doctest: +IGNORE_RESULT", codeblock)
 
-        if ">>>" in codeblock and re.match(r"( cuda | to(0) | device = 0)", codeblock) and skip_cuda_tests :
+        if ">>>" in codeblock and re.match(r"( cuda | to(0) | device = 0)", codeblock) and skip_cuda_tests:
             if 'device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")' in codeblock:
                 continue
             line_num = "".join(codeblocks[: i + 1]).count("\n")
             codeblocks[
                 i - 1
-            ] += f'from pytest import skip;skip("Codeblock line {line_num} uses `cuda`. Skipping",allow_module_level=True);'
+            ] += f'from pytest import skip;skip("Codeblock line {line_num} uses `cuda`. Skipping" \
+                                ,allow_module_level=True);'
     codeblocks = "".join(codeblocks)
     return codeblocks
 
 
 class HfDocTestParser(doctest.DocTestParser):
+    """
+    Overwrites the DocTestParser from doctest to properly parse the codeblocks that are formatted with black. This
+    means that there are no extra lines at the end of our snippets. The `# doctest: +IGNORE_RESULT` marker is also
+    added anywhere a `load_dataset` call is made as a print would otherwise fail the corresonding line.
+
+    Tests involving cuda are skipped base on a naive pattern that should be updated if it is not enough.
+    """
+
     # This regular expression is used to find doctest examples in a
     # string.  It defines three groups: `source` is the source code
     # (including leading indentation and prompts); `indent` is the
@@ -108,13 +101,16 @@ class HfDocTestParser(doctest.DocTestParser):
         Overwrites the `parse` method to incorporate a skip for CUDA tests, and remove logs and dataset prints before
         calling `super().parse`
         """
-        # for parsing INGORE RESULT? Should check the output checker
-        # string = string.replace("+IGNORE_RESULT", "+SKIP")  
         string = preprocess_string(string, self.skip_cuda_tests)
         return super().parse(string, name)
 
 
 class HfDoctestModule(Module):
+    """
+    Overwrites the `DoctestModule` of the pytest package to make sure the HFDocTestParser is used when discovering
+    tests.
+    """
+
     def collect(self) -> Iterable[DoctestItem]:
         class MockAwareDocTestFinder(doctest.DocTestFinder):
             """A hackish doctest finder that overrides stdlib internals to fix a stdlib bug.
@@ -171,7 +167,6 @@ class HfDoctestModule(Module):
                     raise
 
         # !!!!!!!!!!! HF Specific !!!!!!!!!!!
-        # The HF Parser has to be used here to properly find tests!
         finder = MockAwareDocTestFinder(parser=HfDocTestParser())
         # !!!!!!!!!!! HF Specific !!!!!!!!!!!
         optionflags = get_optionflags(self)
