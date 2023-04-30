007201df6c17989cef8ac31f8420ffdd62a9c86e
dewa <95997298+dewasahu2003@users.noreply.github.com>
2023-04-30 15:47:05 +0530

    error caused by riff tackled

--- src/transformers/models/graphormer/modeling_graphormer.py
+++ src/transformers/models/graphormer/modeling_graphormer.py
@@ -14,18 +14,21 @@
 # limitations under the License.
 """ PyTorch Graphormer model."""
 
-
 import math
-from typing import List, Optional, Tuple, Union, Iterable, Iterator
+from typing import Iterable, Iterator, List, Optional, Tuple, Union
 
 import torch
 import torch.nn as nn
 from torch.nn import BCEWithLogitsLoss, CrossEntropyLoss, MSELoss
 
 from ...activations import ACT2FN
-from ...modeling_outputs import BaseModelOutputWithNoAttention, SequenceClassifierOutput
+from ...modeling_outputs import (
+    BaseModelOutputWithNoAttention,
+    SequenceClassifierOutput,
+)
 from ...modeling_utils import PreTrainedModel
 from ...utils import logging
+
 from .configuration_graphormer import GraphormerConfig
 
 
