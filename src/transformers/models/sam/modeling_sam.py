7e9573688dbc9f0522c678d171456f1b9c4c322a
Michael Benayoun <mickbenayoun@gmail.com>
2023-05-02 16:01:06 +0200

    Resolve todo

--- src/transformers/models/sam/modeling_sam.py
+++ src/transformers/models/sam/modeling_sam.py
@@ -487,7 +487,6 @@ class SamMaskDecoder(nn.Module):
         output_tokens = torch.cat([self.iou_token.weight, self.mask_tokens.weight], dim=0)
         output_tokens = output_tokens.repeat(batch_size, point_batch_size, 1, 1)
 
-        # TODO: can we get rid of the if statement?
         if sparse_prompt_embeddings.sum().item() != 0:
             tokens = torch.cat((output_tokens, sparse_prompt_embeddings), dim=2)
         else:
