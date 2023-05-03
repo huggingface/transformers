8be9d02f359b7586995f4b987a2bfcff6b8b7e26
qywu <wilwu@ucdavis.edu>
2023-05-03 15:10:38 -0400

    fix style and quality

--- src/transformers/trainer.py
+++ src/transformers/trainer.py
@@ -672,7 +672,8 @@ class Trainer:
             self.label_smoother = None
 
         self.state = TrainerState(
-            is_local_process_zero=self.is_local_process_zero(), is_world_process_zero=self.is_world_process_zero(),
+            is_local_process_zero=self.is_local_process_zero(),
+            is_world_process_zero=self.is_world_process_zero(),
         )
 
         self.control = TrainerControl()
@@ -857,7 +858,10 @@ class Trainer:
                 )
             else:
                 return DistributedSampler(
-                    self.train_dataset, num_replicas=self.args.world_size, rank=self.args.process_index, seed=seed,
+                    self.train_dataset,
+                    num_replicas=self.args.world_size,
+                    rank=self.args.process_index,
+                    seed=seed,
                 )
 
     def get_train_dataloader(self) -> DataLoader:
@@ -1083,7 +1087,11 @@ class Trainer:
             optimizer_cls, optimizer_kwargs = Trainer.get_optimizer_cls_and_kwargs(self.args)
 
             if self.sharded_ddp == ShardedDDPOption.SIMPLE:
-                self.optimizer = OSS(params=optimizer_grouped_parameters, optim=optimizer_cls, **optimizer_kwargs,)
+                self.optimizer = OSS(
+                    params=optimizer_grouped_parameters,
+                    optim=optimizer_cls,
+                    **optimizer_kwargs,
+                )
             else:
                 self.optimizer = optimizer_cls(optimizer_grouped_parameters, **optimizer_kwargs)
                 if optimizer_cls.__name__ == "Adam8bit":
@@ -1424,7 +1432,10 @@ class Trainer:
                 if ShardedDDPOption.AUTO_WRAP in self.args.sharded_ddp:
                     model = auto_wrap(model)
                 self.model = model = FullyShardedDDP(
-                    model, mixed_precision=mixed_precision, reshard_after_forward=zero_3, cpu_offload=cpu_offload,
+                    model,
+                    mixed_precision=mixed_precision,
+                    reshard_after_forward=zero_3,
+                    cpu_offload=cpu_offload,
                 ).to(self.args.device)
         # Distributed training using PyTorch FSDP
         elif self.fsdp is not None:
