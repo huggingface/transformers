0936b07c059dbaf6c1e9af17deee414acc2b295d
Joao Gante <joao@huggingface.co>
2023-05-03 12:24:06 +0000

    better this way

--- src/transformers/pipelines/base.py
+++ src/transformers/pipelines/base.py
@@ -806,16 +806,9 @@ class Pipeline(_ScikitCompat):
         # Update config and generation_config with task specific parameters
         task_specific_params = self.model.config.task_specific_params
         if task_specific_params is not None and task in task_specific_params:
-            for attr_name, attr in task_specific_params.get(task).items():
-                if self.model.can_generate() and hasattr(self.model.generation_config, attr_name):
-                    setattr(self.model.generation_config, attr_name, attr)
-                else:
-                    setattr(self.model.config, attr_name, attr)
-
-        # If the model can generate, disables the flag for the legacy behavior check (generation can be parameterized
-        # from model.config), which can't happen in a pipeline
-        if self.model.can_generate():
-            self.model.generation_config._from_model_config = False
+            self.model.config.update(task_specific_params.get(task))
+            if self.model.can_generate():
+                self.model.generation_config.update(**task_specific_params.get(task))
 
         self.call_count = 0
         self._batch_size = kwargs.pop("batch_size", None)
