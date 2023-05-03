25197cfc01751390a542150962633d9aea64cc51
Matt <Rocketknight1@users.noreply.github.com>
2023-05-03 15:52:23 +0100

    Update tests/models/sam/test_modeling_tf_sam.py
    Co-authored-by: amyeroberts <22614925+amyeroberts@users.noreply.github.com>

--- tests/models/sam/test_modeling_tf_sam.py
+++ tests/models/sam/test_modeling_tf_sam.py
@@ -36,10 +36,6 @@ if is_tf_available():
     from transformers import SamProcessor, TFSamModel
     from transformers.models.sam.modeling_tf_sam import TF_SAM_PRETRAINED_MODEL_ARCHIVE_LIST
 
-if is_torch_available():
-    pass
-
-
 if is_vision_available():
     from PIL import Image
 
