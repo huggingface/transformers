ffb55f99d55b563a6d5de31bbb303d4d34e77d8d
younesbelkada <younesbelkada@gmail.com>
2023-05-04 14:09:35 +0000

    fix conversion script
    - fix sharded case
    - add new arguments

--- src/transformers/models/rwkv/convert_rwkv_checkpoint_to_hf.py
+++ src/transformers/models/rwkv/convert_rwkv_checkpoint_to_hf.py
@@ -12,7 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Convert a RKWV checkpoint form BlinkDL to the Hugging Face format."""
+"""Convert a RWKV checkpoint from BlinkDL to the Hugging Face format."""
 
 
 import argparse
@@ -24,7 +24,7 @@ import re
 import torch
 from huggingface_hub import hf_hub_download
 
-from transformers import PreTrainedTokenizerFast, RwkvConfig
+from transformers import AutoModelForCausalLM, AutoTokenizer, PreTrainedTokenizerFast, RwkvConfig
 from transformers.modeling_utils import WEIGHTS_INDEX_NAME, shard_checkpoint
 
 
@@ -78,15 +78,18 @@ def convert_state_dict(state_dict):
     return state_dict
 
 
-def convert_rmkv_checkpoint_to_hf_format(repo_id, checkpoint_file, output_dir, size=None, tokenizer_file=None):
+def convert_rmkv_checkpoint_to_hf_format(
+    repo_id, checkpoint_file, output_dir, size=None, tokenizer_file=None, push_to_hub=False, model_name=None
+):
     # 1. If possible, build the tokenizer.
     if tokenizer_file is None:
-        print("No `--tokenizer_file` provided, we will only convert the model.")
+        print("No `--tokenizer_file` provided, we will use the default tokenizer.")
         vocab_size = 50277
+        tokenizer = AutoTokenizer.from_pretrained("EleutherAI/gpt-neox-20b")
     else:
         tokenizer = PreTrainedTokenizerFast(tokenizer_file=tokenizer_file)
         vocab_size = len(tokenizer)
-        tokenizer.save_pretrained(output_dir)
+    tokenizer.save_pretrained(output_dir)
 
     # 2. Build the config
     possible_sizes = list(NUM_HIDDEN_LAYERS_MAPPING.keys())
@@ -136,8 +139,15 @@ def convert_rmkv_checkpoint_to_hf_format(repo_id, checkpoint_file, output_dir, s
         gc.collect()
 
         for shard_file in shard_files:
-            state_dict = torch.load(shard_file)
+            state_dict = torch.load(os.path.join(output_dir, shard_file))
             torch.save({k: v.cpu().clone() for k, v in state_dict.items()}, shard_file)
+
+    if push_to_hub:
+        if model_name is None:
+            raise ValueError("Please provide a `model_name` to push the model to the Hub.")
+        model = AutoModelForCausalLM.from_pretrained(output_dir)
+        model.push_to_hub(model_name, max_shard_size="2GB")
+        tokenizer.push_to_hub(model_name)
 
 
 if __name__ == "__main__":
@@ -164,8 +174,25 @@ if __name__ == "__main__":
         type=str,
         help="Size of the model. Will be inferred from the `checkpoint_file` if not passed.",
     )
+    parser.add_argument(
+        "--push_to_hub",
+        action="store_true",
+        help="Push to the Hub the converted model.",
+    )
+    parser.add_argument(
+        "--model_name",
+        default=None,
+        type=str,
+        help="Name of the pushed model on the Hub, including the username / organization.",
+    )
 
     args = parser.parse_args()
     convert_rmkv_checkpoint_to_hf_format(
-        args.repo_id, args.checkpoint_file, args.output_dir, size=args.size, tokenizer_file=args.tokenizer_file
+        args.repo_id,
+        args.checkpoint_file,
+        args.output_dir,
+        size=args.size,
+        tokenizer_file=args.tokenizer_file,
+        push_to_hub=args.push_to_hub,
+        model_name=args.model_name,
     )
