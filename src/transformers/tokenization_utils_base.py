67206ffab611818765b64f31f4c376b7e163a2fd
sanchit-gandhi <sanchit@huggingface.co>
2023-05-05 15:11:01 +0100

    check with tokenization changes

--- src/transformers/tokenization_utils_base.py
+++ src/transformers/tokenization_utils_base.py
@@ -705,7 +705,16 @@ class BatchEncoding(UserDict):
             as_tensor = jnp.array
             is_tensor = is_jax_tensor
         else:
-            as_tensor = np.asarray
+
+            def as_tensor(value):
+                value_lens = [len(val) for val in value]
+                if len(np.unique(value_lens)) != 1:
+                    # we have a ragged list so handle explicitly
+                    value = np.asarray([np.asarray(val) for val in value], dtype=object)
+                else:
+                    value = np.asarray(value)
+                return value
+
             is_tensor = is_numpy_array
 
         # Do the tensor conversion in batch
