8b2a9001d56d3f463f9f8a69720aa1f7d89c3e01
Sylvain Gugger <Sylvain.gugger@gmail.com>
2023-05-05 16:16:04 -0400

    Tool clean up

--- src/transformers/tools/__init__.py
+++ src/transformers/tools/__init__.py
@@ -5,13 +5,9 @@ from .generative_question_answering import GenerativeQuestionAnsweringTool, Remo
 from .image_captioning import ImageCaptioningTool, RemoteImageCaptioningTool
 from .image_question_answering import ImageQuestionAnsweringTool
 from .image_segmentation import ImageSegmentationTool
-from .image_transformation import ImageTransformationTool
 from .language_identifier import LanguageIdentificationTool
 from .speech_to_text import RemoteSpeechToTextTool, SpeechToTextTool
 from .text_classification import RemoteTextClassificationTool, TextClassificationTool
-from .text_download import TextDownloadTool
 from .text_summarization import TextSummarizationTool
-from .text_to_image import TextToImageTool
 from .text_to_speech import TextToSpeechTool
-from .text_to_video import TextToVideoTool
 from .translation import TranslationTool
