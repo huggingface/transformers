daf151bbd85aef1add7b0dc7b61662394c78fc2f
Eli Simhayev <elisimhayev@gmail.com>
2023-05-06 18:16:28 +0300

    Update src/transformers/models/autoformer/configuration_autoformer.py
    Co-authored-by: NielsRogge <48327001+NielsRogge@users.noreply.github.com>

--- src/transformers/models/autoformer/configuration_autoformer.py
+++ src/transformers/models/autoformer/configuration_autoformer.py
@@ -30,7 +30,7 @@ AUTOFORMER_PRETRAINED_CONFIG_ARCHIVE_MAP = {
 
 class AutoformerConfig(PretrainedConfig):
     r"""
-    This is the configuration class to store the configuration of a [`AutoformerModel`]. It is used to instantiate a
+    This is the configuration class to store the configuration of an [`AutoformerModel`]. It is used to instantiate an
     Autoformer model according to the specified arguments, defining the model architecture. Instantiating a
     configuration with the defaults will yield a similar configuration to that of the Time Series Transformer
     [huggingface/time-series-transformer-tourism-monthly](https://huggingface.co/huggingface/time-series-transformer-tourism-monthly)
