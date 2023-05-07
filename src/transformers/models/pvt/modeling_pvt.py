5a626512d7e3379b931f595a9890d9f90bd2d391
Rinat <51479797+Xrenya@users.noreply.github.com>
2023-05-07 21:24:24 +0900

    Update modeling_pvt.py

--- src/transformers/models/pvt/modeling_pvt.py
+++ src/transformers/models/pvt/modeling_pvt.py
@@ -436,26 +436,25 @@ class PvtEncoder(nn.Module):
         all_self_attentions = () if output_attentions else None
 
         batch_size = pixel_values.shape[0]
-
+        num_blocks = len(self.block)
         hidden_states = pixel_values
-        for idx, x in enumerate(zip(self.patch_embeddings, self.block)):
-            embedding_layer, block_layer = x
+        for idx, (embedding_layer, block_layer) in enumerate(zip(self.patch_embeddings, self.block)):
             # first, obtain patch embeddings
             hidden_states, height, width = embedding_layer(hidden_states)
             # second, send embeddings through blocks
-            for i, blk in enumerate(block_layer):
-                layer_outputs = blk(hidden_states, height, width, output_attentions)
+            for block in enumerate(block_layer):
+                layer_outputs = block(hidden_states, height, width, output_attentions)
                 hidden_states = layer_outputs[0]
                 if output_attentions:
                     all_self_attentions = all_self_attentions + (layer_outputs[1],)
                 if output_hidden_states:
                     all_hidden_states = all_hidden_states + (hidden_states,)
-            if idx != len(self.block) - 1:
+            if idx != num_blocks - 1:
                 hidden_states = hidden_states.reshape(batch_size, height, width, -1).permute(0, 3, 1, 2).contiguous()
         hidden_states = self.layer_norm(hidden_states)
         if output_hidden_states:
             all_hidden_states = all_hidden_states + (
-                self.layer_norm(hidden_states) if len(self.patch_embeddings) - 1 else hidden_states,
+                hidden_states if len(self.patch_embeddings) - 1 else hidden_states,
             )
         if not return_dict:
             return tuple(v for v in [hidden_states, all_hidden_states, all_self_attentions] if v is not None)
