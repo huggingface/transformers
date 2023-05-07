6f8a02844a0eeaf15f29acb7fe40126768936fba
Bartosz Szmelczynski <43574448+Bearnardd@users.noreply.github.com>
2023-05-08 00:55:04 +0200

    fix random attention for pytorch's bigbird/pegasus_bigbird (#23056)
    * fix random attention usage for bigbird and pegasus_bigbird
    
    * remove staticmethod, update tests target valus
    
    * revert style changes

--- tests/models/big_bird/test_modeling_flax_big_bird.py
+++ tests/models/big_bird/test_modeling_flax_big_bird.py
@@ -15,7 +15,7 @@
 import unittest
 
 from transformers import BigBirdConfig, is_flax_available
-from transformers.testing_utils import is_pt_flax_cross_test, require_flax, slow
+from transformers.testing_utils import require_flax, slow
 
 from ...test_modeling_flax_common import FlaxModelTesterMixin, ids_tensor, random_attention_mask
 
@@ -221,17 +221,3 @@ class FlaxBigBirdModelTest(FlaxModelTesterMixin, unittest.TestCase):
             return
         else:
             super().check_pt_flax_outputs(fx_outputs, pt_outputs, model_class, tol, name, attributes)
-
-    @is_pt_flax_cross_test
-    @unittest.skip(
-        reason="Current Pytorch implementation has bug with random attention -> it always uses it not matter if we are in eval/train mode"
-    )
-    def test_equivalence_flax_to_pt(self):
-        pass
-
-    @is_pt_flax_cross_test
-    @unittest.skip(
-        reason="Current Pytorch implementation has bug with random attention -> it always uses it not matter if we are in eval/train mode"
-    )
-    def test_equivalence_pt_to_flax(self):
-        pass
