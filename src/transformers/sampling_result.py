e6dd8f7309f51029988808a47d906776ee7cadc7
Niels <niels.rogge1@gmail.com>
2023-05-07 11:48:37 +0200

    Fix more docstrings

--- src/transformers/sampling_result.py
+++ src/transformers/sampling_result.py
@@ -5,15 +5,9 @@ import torch
 
 
 class SamplingResult:
-    """Bbox sampling result.
+    """Bounding box sampling result.
 
-    Example:
-        >>> # xdoctest: +IGNORE_WANT >>> from mmdet.core.bbox.samplers.sampling_result import * # NOQA >>> self =
-        SamplingResult.random(rng=10) >>> print(f'self = {self}') self = <SamplingResult({
-            'neg_bboxes': torch.Size([12, 4]), 'neg_indices': tensor([ 0, 1, 2, 4, 5, 6, 7, 8, 9, 10, 11, 12]), 'num_gts':
-            4, 'pos_assigned_gt_indices': tensor([], dtype=torch.int64), 'pos_bboxes': torch.Size([0, 4]), 'pos_indices':
-            tensor([], dtype=torch.int64), 'pos_is_gt': tensor([], dtype=torch.uint8)
-        })>
+    Source: https://github.com/open-mmlab/mmdetection/blob/ecac3a77becc63f23d9f6980b2a36f86acd00a8a/mmdet/models/task_modules/samplers/sampling_result.py#L51.
     """
 
     def __init__(self, pos_indices, neg_indices, bboxes, gt_bboxes, assign_result, gt_flags):
@@ -28,7 +22,8 @@ class SamplingResult:
 
         if gt_bboxes.numel() == 0:
             # hack for index error case
-            assert self.pos_assigned_gt_indices.numel() == 0
+            if self.pos_assigned_gt_indices.numel() != 0:
+                raise ValueError("No gt bboxes available to choose from")
             self.pos_gt_bboxes = torch.empty_like(gt_bboxes).view(-1, 4)
         else:
             if len(gt_bboxes.shape) < 2:
@@ -42,7 +37,6 @@ class SamplingResult:
             self.pos_gt_labels = None
 
     def __repr__(self):
-        """str: the string of the module"""
         try:
             nice = self.__nice__()
             classname = self.__class__.__name__
@@ -52,7 +46,6 @@ class SamplingResult:
             return object.__repr__(self)
 
     def __str__(self):
-        """str: the string of the module"""
         try:
             classname = self.__class__.__name__
             nice = self.__nice__()
@@ -68,10 +61,6 @@ class SamplingResult:
 
     def to(self, device):
         """Change the device of the data inplace.
-
-        Example:
-            >>> self = SamplingResult.random() >>> print(f'self = {self.to(None)}') >>> # xdoctest: +REQUIRES(--gpu)
-            >>> print(f'self = {self.to(0)}')
         """
         _dict = self.__dict__
         for key, value in _dict.items():
@@ -99,62 +88,3 @@ class SamplingResult:
             "num_gts": self.num_gts,
             "pos_assigned_gt_indices": self.pos_assigned_gt_indices,
         }
-
-    @classmethod
-    def random(cls, rng=None, **kwargs):
-        """
-        Args:
-            rng (None | int | numpy.random.RandomState): seed or state.
-            kwargs (keyword arguments):
-                - num_preds: number of predicted boxes
-                - num_gts: number of true boxes
-                - p_ignore (float): probability of a predicted box assigned to \
-                    an ignored truth.
-                - p_assigned (float): probability of a predicted box not being \
-                    assigned.
-                - p_use_label (float | bool): with labels or not.
-
-        Returns:
-            `SamplingResult`: Randomly generated sampling result.
-
-        Example:
-            >>> from mmdet.core.bbox.samplers.sampling_result import * # NOQA >>> self = SamplingResult.random() >>>
-            print(self.__dict__)
-        """
-        from mmdet.core.bbox import demodata
-        from mmdet.core.bbox.assigners.assign_result import AssignResult
-        from mmdet.core.bbox.samplers.random_sampler import RandomSampler
-
-        rng = demodata.ensure_rng(rng)
-
-        # make probabalistic?
-        num = 32
-        pos_fraction = 0.5
-        neg_pos_ub = -1
-
-        assign_result = AssignResult.random(rng=rng, **kwargs)
-
-        # Note we could just compute an assignment
-        bboxes = demodata.random_boxes(assign_result.num_preds, rng=rng)
-        gt_bboxes = demodata.random_boxes(assign_result.num_gts, rng=rng)
-
-        if rng.rand() > 0.2:
-            # sometimes algorithms squeeze their data, be robust to that
-            gt_bboxes = gt_bboxes.squeeze()
-            bboxes = bboxes.squeeze()
-
-        if assign_result.labels is None:
-            gt_labels = None
-        else:
-            gt_labels = None  # todo
-
-        if gt_labels is None:
-            add_gt_as_proposals = False
-        else:
-            add_gt_as_proposals = True  # make probabalistic?
-
-        sampler = RandomSampler(
-            num, pos_fraction, neg_pos_ub=neg_pos_ub, add_gt_as_proposals=add_gt_as_proposals, rng=rng
-        )
-        self = sampler.sample(assign_result, bboxes, gt_bboxes, gt_labels)
-        return self
