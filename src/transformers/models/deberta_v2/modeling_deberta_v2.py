b367defb824ad633bb00f21fbdfe298843d6fab0
Sebastian Lee <sebastian.lee@deepset.ai>
2023-05-07 14:46:46 +0200

    Updates to deberta modeling from microsoft repo

--- src/transformers/models/deberta_v2/modeling_deberta_v2.py
+++ src/transformers/models/deberta_v2/modeling_deberta_v2.py
@@ -589,7 +589,8 @@ def build_relative_position(query_size, key_size, bucket_size=-1, max_position=-
 
     q_ids = torch.arange(0, query_size, device=device)
     k_ids = torch.arange(0, key_size, device=device)
-    rel_pos_ids = q_ids[:, None] - k_ids[None, :]
+    # rel_pos_ids = q_ids[:, None] - k_ids[None, :]
+    rel_pos_ids = q_ids.view(-1, 1) - k_ids.view(1, -1)
     if bucket_size > 0 and max_position > 0:
         rel_pos_ids = make_log_bucket_position(rel_pos_ids, bucket_size, max_position)
     rel_pos_ids = rel_pos_ids.to(torch.long)
@@ -721,10 +722,10 @@ class DisentangledSelfAttention(nn.Module):
             scale_factor += 1
         if "p2c" in self.pos_att_type:
             scale_factor += 1
-        # scale = 1 / math.sqrt(query_layer.size(-1) * scale_factor)
-        # attention_scores = torch.bmm(query_layer, key_layer.transpose(-1, -2) * scale)
-        scale = torch.sqrt(torch.tensor(query_layer.size(-1), dtype=torch.float) * scale_factor)
-        attention_scores = torch.bmm(query_layer, key_layer.transpose(-1, -2)) / scale.to(dtype=query_layer.dtype)
+        scale = 1 / math.sqrt(query_layer.size(-1) * scale_factor)
+        attention_scores = torch.bmm(query_layer, key_layer.transpose(-1, -2) * scale)
+        # scale = torch.sqrt(torch.tensor(query_layer.size(-1), dtype=torch.float) * scale_factor)
+        # attention_scores = torch.bmm(query_layer, key_layer.transpose(-1, -2)) / scale.to(dtype=query_layer.dtype)
         if self.relative_attention:
             rel_embeddings = self.pos_dropout(rel_embeddings)
             rel_att = self.disentangled_attention_bias(
