ab6df253537455ebb8165900b9ae0b0d98dfd736
Sylvain Gugger <Sylvain.gugger@gmail.com>
2023-05-08 08:45:29 -0400

    New version of Accelerate for the Trainer

--- setup.py
+++ setup.py
@@ -102,7 +102,7 @@ if stale_egg_info.exists():
 # 2. once modified, run: `make deps_table_update` to update src/transformers/dependency_versions_table.py
 _deps = [
     "Pillow",
-    "accelerate>=0.17.0",
+    "accelerate>=0.19.0",
     "av==9.2.0",  # Latest version of PyAV (10.0.0) has issues with audio stream.
     "beautifulsoup4",
     "black~=23.1",
