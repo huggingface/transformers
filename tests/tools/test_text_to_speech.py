2ea83399447886aaa9c6c4368315a9779e8192b6
Lysandre <hi@lyand.re>
2023-05-09 22:35:57 +0000

    TTS tests

--- tests/tools/test_text_to_speech.py
+++ tests/tools/test_text_to_speech.py
@@ -16,23 +16,29 @@
 import unittest
 
 from transformers import load_tool
+from transformers.utils import is_torch_available
+
+if is_torch_available():
+    import torch
 
 from .test_tools_common import ToolTesterMixin
+from transformers.testing_utils import require_torch
 
 
+@require_torch
 class TextToSpeechToolTester(unittest.TestCase, ToolTesterMixin):
     def setUp(self):
         self.tool = load_tool("text-to-speech")
         self.tool.setup()
 
     def test_exact_match_arg(self):
+        # SpeechT5 isn't deterministic
+        torch.manual_seed(0)
         result = self.tool("hey")
-        print(result.shape)
-
-        # TODO check for real values
+        self.assertTrue(torch.allclose(result[:3], torch.tensor([-0.00040140701457858086, -0.0002551682700868696, -0.00010294507956132293])))
 
     def test_exact_match_kwarg(self):
+        # SpeechT5 isn't deterministic
+        torch.manual_seed(0)
         result = self.tool("hey")
-        print(result.shape)
-
-        # TODO check for real values
+        self.assertTrue(torch.allclose(result[:3], torch.tensor([-0.00040140701457858086, -0.0002551682700868696, -0.00010294507956132293])))
