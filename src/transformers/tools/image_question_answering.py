c53168d0106ddaeb6f3b108234424f167233de7e
Sylvain Gugger <Sylvain.gugger@gmail.com>
2023-05-09 16:23:50 -0400

    Quality

--- src/transformers/tools/image_question_answering.py
+++ src/transformers/tools/image_question_answering.py
@@ -14,6 +14,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import torch
+
 from ..models.auto import AutoModelForVisualQuestionAnswering, AutoProcessor
 from ..utils import is_vision_available
 from .base import PipelineTool
--- src/transformers/tools/image_segmentation.py
+++ src/transformers/tools/image_segmentation.py
@@ -15,6 +15,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import numpy as np
+import torch
 
 from ..models.clipseg import CLIPSegForImageSegmentation
 from ..utils import is_vision_available
