1662d73d160177f0c75eeaaa3e9c0997496a1bad
Rinat <51479797+Xrenya@users.noreply.github.com>
2023-05-10 09:09:46 +0900

    Update src/transformers/models/pvt/configuration_pvt.py
    Co-authored-by: amyeroberts <22614925+amyeroberts@users.noreply.github.com>

--- src/transformers/models/pvt/configuration_pvt.py
+++ src/transformers/models/pvt/configuration_pvt.py
@@ -51,19 +51,19 @@ class PvtConfig(PretrainedConfig):
             The number of input channels.
         num_encoder_blocks (`[int]`, *optional*., defaults to 4):
             The number of encoder blocks (i.e. stages in the Mix Transformer encoder).
-        depths (`List[int]`, *optional*, defaults to [2, 2, 2, 2]):
+        depths (`List[int]`, *optional*, defaults to `[2, 2, 2, 2]`):
             The number of layers in each encoder block.
-        sr_ratios (`List[int]`, *optional*, defaults to [8, 4, 2, 1]):
+        sr_ratios (`List[int]`, *optional*, defaults to `[8, 4, 2, 1]`):
             Sequence reduction ratios in each encoder block.
-        hidden_sizes (`List[int]`, *optional*, defaults to [64, 128, 320, 512]):
+        hidden_sizes (`List[int]`, *optional*, defaults to `[64, 128, 320, 512]`):
             Dimension of each of the encoder blocks.
-        patch_sizes (`List[int]`, *optional*, defaults to [4, 2, 2, 2]):
+        patch_sizes (`List[int]`, *optional*, defaults to `[4, 2, 2, 2]`):
             Patch size before each encoder block.
-        strides (`List[int]`, *optional*, defaults to [4, 2, 2, 2]):
+        strides (`List[int]`, *optional*, defaults to `[4, 2, 2, 2]`):
             Stride before each encoder block.
-        num_attention_heads (`List[int]`, *optional*, defaults to [1, 2, 5, 8]):
+        num_attention_heads (`List[int]`, *optional*, defaults to `[1, 2, 5, 8]`):
             Number of attention heads for each attention layer in each block of the Transformer encoder.
-        mlp_ratios (`List[int]`, *optional*, defaults to [8, 8, 4, 4]):
+        mlp_ratios (`List[int]`, *optional*, defaults to `[8, 8, 4, 4]`):
             Ratio of the size of the hidden layer compared to the size of the input layer of the Mix FFNs in the
             encoder blocks.
         hidden_act (`str` or `function`, *optional*, defaults to `"gelu"`):
