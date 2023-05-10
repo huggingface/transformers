6126da1b68524585b6b262b7c8ec29d25596abf4
jinho <jinhoparkseoul@gmail.com>
2023-05-10 23:44:38 +0900

    remove trailing period in copyright line

--- src/transformers/models/bros/__init__.py
+++ src/transformers/models/bros/__init__.py
@@ -1,4 +1,4 @@
-# Copyright 2020 The HuggingFace Team. All rights reserved.
+# Copyright 2022-present NAVER Corp, The Microsoft Research Asia LayoutLM Team Authors and the HuggingFace Inc. team.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
--- src/transformers/models/bros/tokenization_bros.py
+++ src/transformers/models/bros/tokenization_bros.py
@@ -1,5 +1,5 @@
 # coding=utf-8
-# Copyright 2022-present NAVER Corp, The Microsoft Research Asia LayoutLM Team Authors and the HuggingFace Inc. team..
+# Copyright 2022-present NAVER Corp, The Microsoft Research Asia LayoutLM Team Authors and the HuggingFace Inc. team.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
