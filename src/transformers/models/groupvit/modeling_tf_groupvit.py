1b0f3804e5ec27fc5b4e73761e70f22bea34a14b
Matt <rocketknight1@gmail.com>
2023-05-12 14:53:46 +0100

    import DUMMY_INPUTS from the right place

--- src/transformers/models/groupvit/modeling_tf_groupvit.py
+++ src/transformers/models/groupvit/modeling_tf_groupvit.py
@@ -28,7 +28,6 @@ import tensorflow as tf
 from ...activations_tf import get_tf_activation
 from ...modeling_tf_outputs import TFBaseModelOutput, TFBaseModelOutputWithPooling
 from ...modeling_tf_utils import (
-    DUMMY_INPUTS,
     TFModelInputType,
     TFPreTrainedModel,
     get_initializer,
@@ -37,6 +36,7 @@ from ...modeling_tf_utils import (
 )
 from ...tf_utils import check_embeddings_within_bounds, shape_list, stable_softmax
 from ...utils import (
+    DUMMY_INPUTS,
     ModelOutput,
     add_start_docstrings,
     add_start_docstrings_to_model_forward,
