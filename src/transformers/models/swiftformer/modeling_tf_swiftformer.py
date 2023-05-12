26e7f8507fadbbeb23055da7cee259cf66a7e243
joaocmd <joaodavid1999@hotmail.com>
2023-05-13 00:04:15 +0100

    convert TFSwiftFormerEncoderBlock

--- src/transformers/models/swiftformer/modeling_tf_swiftformer.py
+++ src/transformers/models/swiftformer/modeling_tf_swiftformer.py
@@ -322,7 +322,7 @@ class TFSwiftFormerLocalRepresentation(tf.keras.layers.Layer):
         return x
 
 
-class SwiftFormerEncoderBlock(nn.Module):
+class TFSwiftFormerEncoderBlock(tf.keras.layers.Layer):
     """
     SwiftFormer Encoder Block for SwiftFormer. It consists of (1) Local representation module, (2)
     SwiftFormerEfficientAdditiveAttention, and (3) MLP block.
@@ -338,38 +338,52 @@ class SwiftFormerEncoderBlock(nn.Module):
         layer_scale_init_value = config.layer_scale_init_value
         use_layer_scale = config.use_layer_scale
 
-        self.local_representation = SwiftFormerLocalRepresentation(config, dim=dim)
-        self.attn = SwiftFormerEfficientAdditiveAttention(config, dim=dim)
-        self.linear = SwiftFormerMlp(config, in_features=dim)
-        self.drop_path = SwiftFormerDropPath(drop_path) if drop_path > 0.0 else nn.Identity()
+        self.local_representation = TFSwiftFormerLocalRepresentation(config, dim=dim)
+        self.attn = TFSwiftFormerEfficientAdditiveAttention(config, dim=dim)
+        self.linear = TFSwiftFormerMlp(config, in_features=dim)
+        self.drop_path = SwiftFormerDropPath(drop_path) if drop_path > 0.0 else tf.keras.layers.Identity()
         self.use_layer_scale = use_layer_scale
         if use_layer_scale:
-            self.layer_scale_1 = nn.Parameter(
-                layer_scale_init_value * torch.ones(dim).unsqueeze(-1).unsqueeze(-1), requires_grad=True
-            )
-            self.layer_scale_2 = nn.Parameter(
-                layer_scale_init_value * torch.ones(dim).unsqueeze(-1).unsqueeze(-1), requires_grad=True
-            )
+            self.dim = dim
+            self.layer_scale_init_value = layer_scale_init_value
 
-    def forward(self, x):
-        x = self.local_representation(x)
+    def build(self, input_shape: tf.TensorShape):
+        self.layer_scale_1 = self.add_weight(
+            name="layer_scale_1",
+            shape=(self.dim), # FIXME
+            initializer=tf.keras.initializers.constant(self.layer_scale_init_value),
+            trainable=True,
+        )
+        self.layer_scale_2 = self.add_weight(
+            name="layer_scale_2",
+            shape=(self.dim), # FIXME
+            initializer=tf.keras.initializers.constant(self.layer_scale_init_value),
+            trainable=True,
+        )
+
+        super().build(input_shape)
+
+    def call(self, x: tf.Tensor, training: bool = False):
+        x = self.local_representation(x, training=training)
         batch_size, channels, height, width = x.shape
         if self.use_layer_scale:
             x = x + self.drop_path(
                 self.layer_scale_1
                 * self.attn(x.permute(0, 2, 3, 1).reshape(batch_size, height * width, channels))
                 .reshape(batch_size, height, width, channels)
-                .permute(0, 3, 1, 2)
+                .permute(0, 3, 1, 2),
+                training=training
             )
-            x = x + self.drop_path(self.layer_scale_2 * self.linear(x))
+            x = x + self.drop_path(self.layer_scale_2 * self.linear(x), training=training)
 
         else:
             x = x + self.drop_path(
                 self.attn(x.permute(0, 2, 3, 1).reshape(batch_size, height * width, channels))
                 .reshape(batch_size, height, width, channels)
-                .permute(0, 3, 1, 2)
+                .permute(0, 3, 1, 2),
+                training=training
             )
-            x = x + self.drop_path(self.linear(x))
+            x = x + self.drop_path(self.linear(x), training=training)
         return x
 
 
