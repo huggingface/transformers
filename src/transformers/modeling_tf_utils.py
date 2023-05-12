b867d7d5eb8cd9c84cd56b6151f64c63007c6902
Matt <rocketknight1@gmail.com>
2023-05-12 15:06:45 +0100

    Support cross-attention in the dummies

--- src/transformers/modeling_tf_utils.py
+++ src/transformers/modeling_tf_utils.py
@@ -1116,10 +1116,24 @@ class TFPreTrainedModel(tf.keras.Model, TFModelUtilsMixin, TFGenerationMixin, Pu
         input_sig = self.input_signature
         if not all(tensor_spec.shape[0] is None for tensor_spec in self.input_signature.values()):
             raise ValueError("The first dimension of the input tensors should be None.")
-        return {
+        dummies = {
             key: tf.keras.Input(shape=tensor.shape[1:], dtype=tensor.dtype, name=key)
             for key, tensor in input_sig.items()
         }
+        if self.config.add_cross_attention:
+            if (
+                "encoder_hidden_states" not in dummies
+                and "encoder_hidden_states" in inspect.signature(self.call).parameters
+                and self.main_input_name == "input_ids"
+            ):
+                dummies["encoder_hidden_states"] = tf.keras.Input(
+                    shape=(None, self.config.hidden_size), dtype=tf.float32, name="encoder_hidden_states"
+                )
+            else:
+                raise NotImplementedError(
+                    "Model has cross-attention but we couldn't infer the shape for the encoder hidden states. Please manually override dummy_inputs!"
+                )
+        return dummies
 
     @property
     def framework(self) -> str:
