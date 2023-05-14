24aa06416de9b7ce5ad1465b243e803879ed6f32
Niels <niels.rogge1@gmail.com>
2023-05-14 18:57:38 +0200

    Use channels first format

--- src/transformers/models/persam/modeling_persam.py
+++ src/transformers/models/persam/modeling_persam.py
@@ -640,13 +640,16 @@ class PerSamMaskEmbedding(nn.Module):
         self.conv1 = nn.Conv2d(1, self.mask_input_channels, kernel_size=2, stride=2)
         self.conv2 = nn.Conv2d(self.mask_input_channels, config.mask_input_channels, kernel_size=2, stride=2)
         self.conv3 = nn.Conv2d(config.mask_input_channels, config.hidden_size, kernel_size=1)
-        self.layer_norm1 = PerSamLayerNorm(self.mask_input_channels, config.layer_norm_eps)
-        self.layer_norm2 = PerSamLayerNorm(self.mask_input_channels * 4, config.layer_norm_eps)
+        self.layer_norm1 = PerSamLayerNorm(self.mask_input_channels, eps=config.layer_norm_eps, data_format="channels_first")
+        self.layer_norm2 = PerSamLayerNorm(self.mask_input_channels * 4, eps=config.layer_norm_eps, data_format="channels_first")
 
     def forward(self, masks):
         print("Shape of masks before embedding:", masks.shape)
 
         hidden_states = self.conv1(masks)
+
+        print("Shape of mask embeddings after conv1:", hidden_states.shape)
+
         hidden_states = self.layer_norm1(hidden_states)
         hidden_states = self.activation(hidden_states)
 
