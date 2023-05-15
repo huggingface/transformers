4328bf79029881dabe4ac0030d735a7bcb4ed4df
Matt <rocketknight1@gmail.com>
2023-05-15 19:21:24 +0100

    Fix T5's signature

--- src/transformers/models/t5/modeling_tf_t5.py
+++ src/transformers/models/t5/modeling_tf_t5.py
@@ -867,7 +867,7 @@ class TFT5PreTrainedModel(TFPreTrainedModel):
 
     @property
     def input_signature(self):
-        {
+        return {
             "input_ids": tf.TensorSpec((None, None), tf.int32, name="input_ids"),
             "attention_mask": tf.TensorSpec((None, None), tf.int32, name="attention_mask"),
             "decoder_input_ids": tf.TensorSpec((None, None), tf.int32, name="decoder_input_ids"),
