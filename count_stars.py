6bd60794e79c90d0bb4d238049b78066e784ce5c
ydshieh <ydshieh@users.noreply.github.com>
2023-05-17 08:52:13 +0200

    🤗 stars ⭐

--- count_stars.py
+++ count_stars.py
@@ -3,8 +3,7 @@ import time
 import requests
 from datetime import datetime
 
-
-def foo(token):
+def foo(token, old):
 
     headers = None
     if token is not None:
@@ -14,12 +13,19 @@ def foo(token):
 
     result = requests.get(url, headers=headers).json()
     count = result["stargazers_count"]
+    # print(f"⭐: {count}")
 
     now = datetime.today().strftime('%Y-%m-%d %H:%M:%S')
+
+    if count != old:
+        print("-" * 40)
     print(f"⭐: {count} | {now}")
+
+    return count
 
 
 if __name__ == "__main__":
+    old = None
     for i in range(10000):
-        time.sleep(5)
-        foo(token=os.environ["ACCESS_REPO_INFO_TOKEN"])
+        time.sleep(10)
+        foo(token=os.environ["ACCESS_REPO_INFO_TOKEN"], old=old)
