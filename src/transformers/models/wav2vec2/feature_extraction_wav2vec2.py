d07a04a8166890a3611972f4989963d2f2d64f59
Leon Wu <lwgithub@gmail.com>
2023-05-17 10:30:33 -0700

    oops, add other comment too

--- src/transformers/models/wav2vec2/feature_extraction_wav2vec2.py
+++ src/transformers/models/wav2vec2/feature_extraction_wav2vec2.py
@@ -117,7 +117,8 @@ class Wav2Vec2FeatureExtractor(SequenceFeatureExtractor):
         Args:
             raw_speech (`np.ndarray`, `List[float]`, `List[np.ndarray]`, `List[List[float]]`):
                 The sequence or batch of sequences to be padded. Each sequence can be a numpy array, a list of float
-                values, a list of numpy arrays or a list of list of float values.
+                values, a list of numpy arrays or a list of list of float values. Must be mono channel audio, not
+                stereo, i.e. single float per timestep.
             padding (`bool`, `str` or [`~utils.PaddingStrategy`], *optional*, defaults to `False`):
                 Select a strategy to pad the returned sequences (according to the model's padding side and padding
                 index) among:
