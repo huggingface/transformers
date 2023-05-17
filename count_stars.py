b922ec00353ee0f7eeca254fa04c7066b015d4df
ydshieh <ydshieh@users.noreply.github.com>
2023-05-17 07:52:59 +0200

    🤗 stars ⭐

--- count_stars.py
+++ count_stars.py
@@ -1,22 +1,24 @@
 import os
 import time
 
+
+def foo(s1):
+    print(f"🤗 stars ⭐: {s1}")
+
+
 if __name__ == "__main__":
 
     for i in range(86400):
-        time.sleep(5)
+        time.sleep(1)
 
         s1 = None
         s2 = None
 
-        os.system("rm -rf stars.txt")
         cmd = 'curl -s "https://api.github.com/repos/huggingface/transformers" | grep stargazers_count | cut -d : -f 2 | tee stars.txt'
         os.system(cmd)
-        print("cccc")
-        # with open("stars.txt") as fp:
-        #     s = fp.read()
-        #     print(s)
-        #     s1 = s[1:-1]
-        #     if s2 != s1:
-        #         print(f"🤗 stars ⭐: {s1}")
-        #         s2 = s1
+        with open("stars.txt") as fp:
+            s = fp.read()
+            s1 = s[1:-1]
+            if s2 != s1:
+                foo(s1)
+                s2 = s1
