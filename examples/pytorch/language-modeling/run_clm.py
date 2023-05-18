7d7e61b517d762936416e96ffa01745849feae4b
Boda Sadallah <32247544+BodaSadalla98@users.noreply.github.com>
2023-05-18 21:22:30 +0300

    fix bug in group_texts function, that was inserting short batches (#23429)
    * fix bug in group_texts function, that was inserting short batches
    
    * fully exclude short batches and return empty dict instead
    
    * fix style

--- examples/pytorch/language-modeling/run_clm.py
+++ examples/pytorch/language-modeling/run_clm.py
@@ -491,10 +491,9 @@ def main():
         # Concatenate all texts.
         concatenated_examples = {k: list(chain(*examples[k])) for k in examples.keys()}
         total_length = len(concatenated_examples[list(examples.keys())[0]])
-        # We drop the small remainder, we could add padding if the model supported it instead of this drop, you can
-        # customize this part to your needs.
-        if total_length >= block_size:
-            total_length = (total_length // block_size) * block_size
+        # We drop the small remainder, and if the total_length < block_size  we exclude this batch and return an empty dict.
+        # We could add padding if the model supported it instead of this drop, you can customize this part to your needs.
+        total_length = (total_length // block_size) * block_size
         # Split by chunks of max_len.
         result = {
             k: [t[i : i + block_size] for i in range(0, total_length, block_size)]
