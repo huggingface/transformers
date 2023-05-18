7379457e516e02c932aefc071d42b0c8b1af45c7
jinho <jinhoparkseoul@gmail.com>
2023-05-19 00:54:13 +0900

    set temporary value to hidden_size (32 is too low because of the of the Bros' positional embedding)

--- tests/models/bros/test_modeling_bros.py
+++ tests/models/bros/test_modeling_bros.py
@@ -49,7 +49,8 @@ class BrosModelTester:
             use_token_type_ids=True,
             use_labels=True,
             vocab_size=99,
-            hidden_size=32,
+            # hidden_size=32, # TODO
+            hidden_size=512,
             num_hidden_layers=5,
             num_attention_heads=4,
             intermediate_size=37,
