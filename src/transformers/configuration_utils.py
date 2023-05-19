f3daf60553d4bbe9410644e70156309a0532fe42
joaoareis <34096208+joaoareis@users.noreply.github.com>
2023-05-19 17:48:35 +0100

    Fix PretrainedConfig `min_length` docstring (#23471)

--- src/transformers/configuration_utils.py
+++ src/transformers/configuration_utils.py
@@ -119,7 +119,7 @@ class PretrainedConfig(PushToHubMixin):
 
         max_length (`int`, *optional*, defaults to 20):
             Maximum length that will be used by default in the `generate` method of the model.
-        min_length (`int`, *optional*, defaults to 10):
+        min_length (`int`, *optional*, defaults to 0):
             Minimum length that will be used by default in the `generate` method of the model.
         do_sample (`bool`, *optional*, defaults to `False`):
             Flag that will be used by default in the `generate` method of the model. Whether or not to use sampling ;
