1453f2fd7b0009dd0f5e893f68092469c1f1de61
younesbelkada <younesbelkada@gmail.com>
2023-05-19 11:00:42 +0000

    add comment

--- src/transformers/models/rwkv/modeling_rwkv.py
+++ src/transformers/models/rwkv/modeling_rwkv.py
@@ -709,6 +709,7 @@ class RwkvModel(RwkvPreTrainedModel):
                         block.attention.output.weight.mul_(2 ** int(block_id // self.config.rescale_every))
                         block.feed_forward.value.weight.mul_(2 ** int(block_id // self.config.rescale_every))
                     else:
+                        # Deal with quantization statistics
                         if hasattr(block.attention.output.weight, "SCB"):
                             block.attention.output.weight.SCB.div_(2 ** int(block_id // self.config.rescale_every))
                             block.feed_forward.value.weight.SCB.div_(2 ** int(block_id // self.config.rescale_every))
