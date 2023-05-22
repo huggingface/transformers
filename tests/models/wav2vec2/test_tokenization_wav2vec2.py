86d4840a769f702d7b854f25c7d65f0af35079a5
LWprogramming <LWprogramming@users.noreply.github.com>
2023-05-22 09:57:45 -0700

    Fix wav2vec2 is_batched check to include 2-D numpy arrays (#23223)
    * Fix wav2vec2 is_batched check to include 2-D numpy arrays
    
    * address comment
    
    * Add tests
    
    * oops
    
    * oops
    
    * Switch to np array
    
    Co-authored-by: Sanchit Gandhi <93869735+sanchit-gandhi@users.noreply.github.com>
    
    * Switch to np array
    
    * condition merge
    
    * Specify mono channel only in comment
    
    * oops, add other comment too
    
    * make style
    
    * Switch list check from falsiness to empty
    
    ---------
    
    Co-authored-by: Sanchit Gandhi <93869735+sanchit-gandhi@users.noreply.github.com>

--- tests/models/wav2vec2/test_tokenization_wav2vec2.py
+++ tests/models/wav2vec2/test_tokenization_wav2vec2.py
@@ -164,6 +164,14 @@ class Wav2Vec2TokenizerTest(unittest.TestCase):
         for enc_seq_1, enc_seq_2 in zip(encoded_sequences_1, encoded_sequences_2):
             self.assertTrue(np.allclose(enc_seq_1, enc_seq_2, atol=1e-3))
 
+        # Test 2-D numpy arrays are batched.
+        speech_inputs = [floats_list((1, x))[0] for x in (800, 800, 800)]
+        np_speech_inputs = np.asarray(speech_inputs)
+        encoded_sequences_1 = tokenizer(speech_inputs, return_tensors="np").input_values
+        encoded_sequences_2 = tokenizer(np_speech_inputs, return_tensors="np").input_values
+        for enc_seq_1, enc_seq_2 in zip(encoded_sequences_1, encoded_sequences_2):
+            self.assertTrue(np.allclose(enc_seq_1, enc_seq_2, atol=1e-3))
+
     def test_padding(self, max_length=50):
         def _input_values_have_equal_length(input_values):
             length = len(input_values[0])
