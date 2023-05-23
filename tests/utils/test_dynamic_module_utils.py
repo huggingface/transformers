a07a38bef2a6c5af4833d18dd785a7e4e78b77d0
Daniel King <daniel@mosaicml.com>
2023-05-23 15:19:03 -0700

    fixup

--- tests/utils/test_dynamic_module_utils.py
+++ tests/utils/test_dynamic_module_utils.py
@@ -1,7 +1,9 @@
 import os
+
 import pytest
 
 from transformers.dynamic_module_utils import get_imports
+
 
 TOP_LEVEL_IMPORT = """
 import os
@@ -68,14 +70,15 @@ CASES = [
     TOP_LEVEL_TRY_IMPORT,
     GENERIC_EXCEPT_IMPORT,
     MULTILINE_TRY_IMPORT,
-    MULTILINE_BOTH_IMPORT
+    MULTILINE_BOTH_IMPORT,
 ]
 
-@pytest.mark.parametrize('case', CASES)
+
+@pytest.mark.parametrize("case", CASES)
 def test_import_parsing(tmp_path, case):
-    tmp_file_path = os.path.join(tmp_path, 'test_file.py')
-    with open(tmp_file_path, 'w') as _tmp_file:
+    tmp_file_path = os.path.join(tmp_path, "test_file.py")
+    with open(tmp_file_path, "w") as _tmp_file:
         _tmp_file.write(case)
 
     parsed_imports = get_imports(tmp_file_path)
-    assert parsed_imports == ['os']
+    assert parsed_imports == ["os"]
