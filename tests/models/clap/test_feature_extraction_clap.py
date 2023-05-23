32c73cfaa524c8fac53ae28d8011a40887de4140
LWprogramming <LWprogramming@users.noreply.github.com>
2023-05-23 11:37:35 -0700

    is_batched fix for remaining 2-D numpy arrays (#23309)
    * Fix is_batched code to allow 2-D numpy arrays for audio
    
    * Tests
    
    * Fix typo
    
    * Incorporate comments from PR #23223

--- tests/models/clap/test_feature_extraction_clap.py
+++ tests/models/clap/test_feature_extraction_clap.py
@@ -139,6 +139,14 @@ class ClapFeatureExtractionTest(SequenceFeatureExtractionTestMixin, unittest.Tes
         for enc_seq_1, enc_seq_2 in zip(encoded_sequences_1, encoded_sequences_2):
             self.assertTrue(np.allclose(enc_seq_1, enc_seq_2, atol=1e-3))
 
+        # Test 2-D numpy arrays are batched.
+        speech_inputs = [floats_list((1, x))[0] for x in (800, 800, 800)]
+        np_speech_inputs = np.asarray(speech_inputs)
+        encoded_sequences_1 = feature_extractor(speech_inputs, return_tensors="np").input_features
+        encoded_sequences_2 = feature_extractor(np_speech_inputs, return_tensors="np").input_features
+        for enc_seq_1, enc_seq_2 in zip(encoded_sequences_1, encoded_sequences_2):
+            self.assertTrue(np.allclose(enc_seq_1, enc_seq_2, atol=1e-3))
+
     def test_double_precision_pad(self):
         import torch
 
