8774976f33ae9944a5685829723875b60b613afe
zspo <songpo.zhang@foxmail.com>
2023-05-24 02:50:40 +0800

    Fix some docs what layerdrop does (#23691)
    * Fix some docs what layerdrop does
    
    * Update src/transformers/models/data2vec/configuration_data2vec_audio.py
    
    Co-authored-by: Sylvain Gugger <35901082+sgugger@users.noreply.github.com>
    
    * Fix more docs
    
    ---------
    
    Co-authored-by: Sylvain Gugger <35901082+sgugger@users.noreply.github.com>

--- src/transformers/models/wavlm/configuration_wavlm.py
+++ src/transformers/models/wavlm/configuration_wavlm.py
@@ -62,6 +62,9 @@ class WavLMConfig(PretrainedConfig):
             The dropout ratio for the attention probabilities.
         final_dropout (`float`, *optional*, defaults to 0.1):
             The dropout probability for the final projection layer of [`WavLMForCTC`].
+        layerdrop (`float`, *optional*, defaults to 0.1):
+            The LayerDrop probability. See the [LayerDrop paper](see https://arxiv.org/abs/1909.11556) for more
+            details.
         initializer_range (`float`, *optional*, defaults to 0.02):
             The standard deviation of the truncated_normal_initializer for initializing all weight matrices.
         layer_norm_eps (`float`, *optional*, defaults to 1e-12):
