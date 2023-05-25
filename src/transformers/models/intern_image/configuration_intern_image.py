777c722dfb400f871fa131dd6bcb81c076e1dc93
Muhammad Hamza <m.waqas.hamza@gmail.com>
2023-05-25 19:21:24 +0500

    add config

--- src/transformers/models/intern_image/configuration_intern_image.py
+++ src/transformers/models/intern_image/configuration_intern_image.py
@@ -39,38 +39,38 @@ class InternImageConfig(PretrainedConfig):
 
 
     Args:
-        vocab_size (`int`, *optional*, defaults to 30522):
-            Vocabulary size of the internimage model. Defines the number of different tokens that can be represented by the
-            `inputs_ids` passed when calling [`~InternImageModel`] or
-            [`~TFInternImageModel`].
-        hidden_size (`int`, *optional*, defaults to 768):
-            Dimension of the encoder layers and the pooler layer.
-        num_hidden_layers (`int`, *optional*, defaults to 12):
-            Number of hidden layers in the Transformer encoder.
-        num_attention_heads (`int`, *optional*, defaults to 12):
-            Number of attention heads for each attention layer in the Transformer encoder.
-        intermediate_size (`int`, *optional*, defaults to 3072):
-            Dimension of the "intermediate" (i.e., feed-forward) layer in the Transformer encoder.
-        hidden_act (`str` or `function`, *optional*, defaults to `"gelu"`):
-            The non-linear activation function (function or string) in the encoder and pooler.
-            If string, `"gelu"`, `"relu"`, `"selu"` and `"gelu_new"` are supported.
-        hidden_dropout_prob (`float`, *optional*, defaults to 0.1):
-            The dropout probabilitiy for all fully connected layers in the embeddings, encoder, and pooler.
-        attention_probs_dropout_prob (`float`, *optional*, defaults to 0.1):
-            The dropout ratio for the attention probabilities.
-        max_position_embeddings (`int`, *optional*, defaults to 512):
-            The maximum sequence length that this model might ever be used with.
-            Typically set this to something large just in case (e.g., 512 or 1024 or 2048).
-        type_vocab_size (`int`, *optional*, defaults to 2):
-            The vocabulary size of the `token_type_ids` passed when calling [`~InternImageModel`] or
-            [`~TFInternImageModel`].
-        initializer_range (`float`, *optional*, defaults to 0.02):
-            The standard deviation of the truncated_normal_initializer for initializing all weight matrices.
-        layer_norm_eps (`float`, *optional*, defaults to 1e-12):
-            The epsilon used by the layer normalization layers.
-        use_cache (`bool`, *optional*, defaults to `True`):
-            Whether or not the model should return the last key/values attentions (not used by all models). Only
-            relevant if `config.is_decoder=True`.
+        # vocab_size (`int`, *optional*, defaults to 30522):
+        #     Vocabulary size of the internimage model. Defines the number of different tokens that can be represented by the
+        #     `inputs_ids` passed when calling [`~InternImageModel`] or
+        #     [`~TFInternImageModel`].
+        # hidden_size (`int`, *optional*, defaults to 768):
+        #     Dimension of the encoder layers and the pooler layer.
+        # num_hidden_layers (`int`, *optional*, defaults to 12):
+        #     Number of hidden layers in the Transformer encoder.
+        # num_attention_heads (`int`, *optional*, defaults to 12):
+        #     Number of attention heads for each attention layer in the Transformer encoder.
+        # intermediate_size (`int`, *optional*, defaults to 3072):
+        #     Dimension of the "intermediate" (i.e., feed-forward) layer in the Transformer encoder.
+        # hidden_act (`str` or `function`, *optional*, defaults to `"gelu"`):
+        #     The non-linear activation function (function or string) in the encoder and pooler.
+        #     If string, `"gelu"`, `"relu"`, `"selu"` and `"gelu_new"` are supported.
+        # hidden_dropout_prob (`float`, *optional*, defaults to 0.1):
+        #     The dropout probabilitiy for all fully connected layers in the embeddings, encoder, and pooler.
+        # attention_probs_dropout_prob (`float`, *optional*, defaults to 0.1):
+        #     The dropout ratio for the attention probabilities.
+        # max_position_embeddings (`int`, *optional*, defaults to 512):
+        #     The maximum sequence length that this model might ever be used with.
+        #     Typically set this to something large just in case (e.g., 512 or 1024 or 2048).
+        # type_vocab_size (`int`, *optional*, defaults to 2):
+        #     The vocabulary size of the `token_type_ids` passed when calling [`~InternImageModel`] or
+        #     [`~TFInternImageModel`].
+        # initializer_range (`float`, *optional*, defaults to 0.02):
+        #     The standard deviation of the truncated_normal_initializer for initializing all weight matrices.
+        # layer_norm_eps (`float`, *optional*, defaults to 1e-12):
+        #     The epsilon used by the layer normalization layers.
+        # use_cache (`bool`, *optional*, defaults to `True`):
+        #     Whether or not the model should return the last key/values attentions (not used by all models). Only
+        #     relevant if `config.is_decoder=True`.
         Example:
 
     ```python
@@ -84,49 +84,43 @@ class InternImageConfig(PretrainedConfig):
 
     >>> # Accessing the model configuration
     >>> configuration = model.config
-    ```
-"""
+    ```"""
     model_type = "intern_image"
-    
 
     def __init__(
         self,
-        vocab_size=30522,
-        hidden_size=768,
-        num_hidden_layers=12,
-        num_attention_heads=12,
-        intermediate_size=3072,
-        hidden_act="gelu",
-        hidden_dropout_prob=0.1,
-        attention_probs_dropout_prob=0.1,
-        max_position_embeddings=512,
-        type_vocab_size=2,
-        initializer_range=0.02,
-        layer_norm_eps=1e-12,
-        use_cache=True,
-        pad_token_id=1,
-        bos_token_id=0,
-        eos_token_id=2,
-        **kwargs
+        core_op="DCNv3_pytorch",
+        channels=64,
+        depths=(4, 4, 18, 4),
+        groups=(4, 8, 16, 32),
+        num_classes=1000,
+        mlp_ratio=4.0,
+        drop_rate=0.0,
+        drop_path_rate=0.1,
+        drop_path_type="linear",
+        act_layer="GELU",
+        norm_layer="LN",
+        layer_scale=None,
+        offset_scale=1.0,
+        post_norm=False,
+        cls_scale=1.5,
+        with_cp=False,
+        **kwargs,
     ):
-        self.vocab_size = vocab_size
-        self.max_position_embeddings = max_position_embeddings
-        self.hidden_size = hidden_size
-        self.num_hidden_layers = num_hidden_layers
-        self.num_attention_heads = num_attention_heads
-        self.intermediate_size = intermediate_size
-        self.hidden_act = hidden_act
-        self.hidden_dropout_prob = hidden_dropout_prob
-        self.attention_probs_dropout_prob = attention_probs_dropout_prob
-        self.initializer_range = initializer_range
-        self.type_vocab_size = type_vocab_size
-        self.layer_norm_eps = layer_norm_eps
-        self.use_cache = use_cache
-        super().__init__(
-            pad_token_id=pad_token_id,
-            bos_token_id=bos_token_id,
-            eos_token_id=eos_token_id,
-            **kwargs
-        )
-
-    
+        self.core_op = core_op
+        self.channels = channels
+        self.depths = depths
+        self.groups = groups
+        self.num_classes = num_classes
+        self.mlp_ratio = mlp_ratio
+        self.drop_rate = drop_rate
+        self.drop_path_rate = drop_path_rate
+        self.drop_path_type = drop_path_type
+        self.act_layer = act_layer
+        self.norm_layer = norm_layer
+        self.layer_scale = layer_scale
+        self.offset_scale = offset_scale
+        self.post_norm = post_norm
+        self.cls_scale = cls_scale
+        self.with_cp = with_cp
+        super().__init__(**kwargs)
