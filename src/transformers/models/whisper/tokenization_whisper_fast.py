0e9c5206bef471512a4f67340e4b98c4aa9f6795
Connor Henderson <connor.henderson@talkiatry.com>
2023-05-26 10:16:01 -0400

    add ' ' replacement for add_prefix_space

--- src/transformers/models/whisper/tokenization_whisper_fast.py
+++ src/transformers/models/whisper/tokenization_whisper_fast.py
@@ -494,7 +494,7 @@ class WhisperTokenizerFast(PreTrainedTokenizerFast):
     # Copied from transformers.models.whisper.tokenization_whisper.WhisperTokenizer.get_prompt_ids
     def get_prompt_ids(self, text: str, return_tensors="np"):
         """Converts prompt text to IDs that can be passed to [`~WhisperForConditionalGeneration.generate`]."""
-        batch_encoding = self("<|startofprev|>", text.strip(), add_prefix_space=True, add_special_tokens=False)
+        batch_encoding = self("<|startofprev|>", " " + text.strip(), add_special_tokens=False)
 
         # Check for special tokens
         prompt_text_ids = batch_encoding["input_ids"][1:]
