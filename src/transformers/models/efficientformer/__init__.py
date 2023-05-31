88f50a1e8974cb2f78a2cf9512ad20544dc72808
Denisa Roberts <denisa.roberts@denisaroberts.me>
2023-05-31 05:43:12 -0400

    Add TensorFlow implementation of  EfficientFormer (#22620)
    * Add tf code for efficientformer
    
    * Fix return dict bug - return last hidden state after last stage
    
    * Fix corresponding return dict bug
    
    * Override test tol
    
    * Change default values of training to False
    
    * Set training to default False X3
    
    * Rm axis from ln
    
    * Set init in dense projection
    
    * Rm debug stuff
    
    * Make style; all tests pass.
    
    * Modify year to 2023
    
    * Fix attention biases codes
    
    * Update the shape list logic
    
    * Add a batch norm eps config
    
    * Remove extract comments in test files
    
    * Add conditional attn and hidden states return for serving output
    
    * Change channel dim checking logic
    
    * Add exception for withteacher model in training mode
    
    * Revert layer count for now
    
    * Add layer count for conditional layer naming
    
    * Transpose for conv happens only in main layer
    
    * Make tests smaller
    
    * Make style
    
    * Update doc
    
    * Rm from_pt
    
    * Change to actual expect image class label
    
    * Remove stray print in tests
    
    * Update image processor test
    
    * Remove the old serving output logic
    
    * Make style
    
    * Make style
    
    * Complete test

--- src/transformers/models/efficientformer/__init__.py
+++ src/transformers/models/efficientformer/__init__.py
@@ -13,7 +13,13 @@
 # limitations under the License.
 from typing import TYPE_CHECKING
 
-from ...utils import OptionalDependencyNotAvailable, _LazyModule, is_torch_available, is_vision_available
+from ...utils import (
+    OptionalDependencyNotAvailable,
+    _LazyModule,
+    is_tf_available,
+    is_torch_available,
+    is_vision_available,
+)
 
 
 _import_structure = {
@@ -45,6 +51,20 @@ else:
         "EfficientFormerPreTrainedModel",
     ]
 
+try:
+    if not is_tf_available():
+        raise OptionalDependencyNotAvailable()
+except OptionalDependencyNotAvailable:
+    pass
+else:
+    _import_structure["modeling_tf_efficientformer"] = [
+        "TF_EFFICIENTFORMER_PRETRAINED_MODEL_ARCHIVE_LIST",
+        "TFEfficientFormerForImageClassification",
+        "TFEfficientFormerForImageClassificationWithTeacher",
+        "TFEfficientFormerModel",
+        "TFEfficientFormerPreTrainedModel",
+    ]
+
 if TYPE_CHECKING:
     from .configuration_efficientformer import EFFICIENTFORMER_PRETRAINED_CONFIG_ARCHIVE_MAP, EfficientFormerConfig
 
@@ -68,6 +88,19 @@ if TYPE_CHECKING:
             EfficientFormerForImageClassificationWithTeacher,
             EfficientFormerModel,
             EfficientFormerPreTrainedModel,
+        )
+    try:
+        if not is_tf_available():
+            raise OptionalDependencyNotAvailable()
+    except OptionalDependencyNotAvailable:
+        pass
+    else:
+        from .modeling_tf_efficientformer import (
+            TF_EFFICIENTFORMER_PRETRAINED_MODEL_ARCHIVE_LIST,
+            TFEfficientFormerForImageClassification,
+            TFEfficientFormerForImageClassificationWithTeacher,
+            TFEfficientFormerModel,
+            TFEfficientFormerPreTrainedModel,
         )
 
 else:
