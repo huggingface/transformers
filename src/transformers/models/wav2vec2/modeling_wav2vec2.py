9f1610ef5c3b8c3c08250f2d5db01775483a8ebb
Edward Z. Yang <ezyang@meta.com>
2023-06-05 18:33:59 -0700

    black
    Signed-off-by: Edward Z. Yang <ezyang@meta.com>

--- src/transformers/models/wav2vec2/modeling_wav2vec2.py
+++ src/transformers/models/wav2vec2/modeling_wav2vec2.py
@@ -377,7 +377,7 @@ class Wav2Vec2PositionalConvEmbedding(nn.Module):
         )
 
         weight_norm = nn.utils.weight_norm
-        if hasattr(nn.utils.parametrizations, 'weight_norm'):
+        if hasattr(nn.utils.parametrizations, "weight_norm"):
             weight_norm = nn.utils.parametrizations.weight_norm
 
         if is_deepspeed_zero3_enabled():
