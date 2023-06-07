72fc7eacc192d3baa4804f0bc7175838a3957426
arthur.zucker@gmail.com <arthur.zucker@gmail.com>
2023-06-07 20:14:41 +0000

    small tokenizer uses `__start__` and `__end__`

--- src/transformers/models/blenderbot_small/modeling_blenderbot_small.py
+++ src/transformers/models/blenderbot_small/modeling_blenderbot_small.py
@@ -536,9 +536,9 @@ BLENDERBOT_SMALL_GENERATION_EXAMPLE = r"""
     Human: I'm not sure
 
     >>> NEXT_UTTERANCE = (
-    ...     "My friends are cool but they eat too many carbs.</s> <s>what kind of carbs do they eat? "
-    ...     "i don't know much about carbs</s> "
-    ...     "<s> I'm not sure."
+    ...     "My friends are cool but they eat too many carbs.__end__ __start__what kind of carbs do they eat? "
+    ...     "i don't know much about carbs__end__ "
+    ...     "__start__ I'm not sure."
     ... )
     >>> inputs = tokenizer([NEXT_UTTERANCE], return_tensors="pt")
     >>> next_reply_ids = model.generate(**inputs)
