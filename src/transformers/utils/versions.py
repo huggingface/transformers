0ddb00cedce561c2e3bafb575f7c075d95eeac55
ydshieh <ydshieh@users.noreply.github.com>
2023-06-07 19:23:38 +0200

    fix

--- src/transformers/utils/versions.py
+++ src/transformers/utils/versions.py
@@ -15,19 +15,13 @@
 Utilities for working with package versions
 """
 
+import importlib.metadata as importlib_metadata
 import operator
 import re
 import sys
 from typing import Optional
 
 from packaging import version
-
-
-# The package importlib_metadata is in a different place, depending on the python version.
-if sys.version_info < (3, 8):
-    import importlib_metadata
-else:
-    import importlib.metadata as importlib_metadata
 
 
 ops = {
