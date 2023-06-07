476abb5339070ea0c2529f8c99f2f974960a9f33
Dong Hande <45357817+DongHande@users.noreply.github.com>
2023-06-07 15:29:14 +0800

    A minor change to fix a bug when using torch.compile()
    When using model = torch.compile(model), the class of new model is changed to OptimizedModule. The function, inspect.signature(self.model.forward), will return ["args", "kargs"], withou "input_ids", "labels", and so on. This will result in the dataset remove all columns and the data sample is a empty dict, and incurs bug when forward propagation. By using "self.model._orig_mod.forward", above problem can be fixed.

--- src/transformers/trainer.py
+++ src/transformers/trainer.py
@@ -752,7 +752,7 @@ class Trainer:
     def _set_signature_columns_if_needed(self):
         if self._signature_columns is None:
             # Inspect model forward signature to keep only the arguments it accepts.
-            signature = inspect.signature(self.model.forward)
+            signature = inspect.signature(self.model._orig_mod.forward if self.model.__class__.__name__ == 'OptimizedModule' else self.model.forward)
             self._signature_columns = list(signature.parameters.keys())
             # Labels may be named label or label_ids, the default data collator handles that.
             self._signature_columns += list(set(["label", "label_ids"] + self.label_names))
