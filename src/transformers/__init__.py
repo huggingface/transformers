e87ddb68f1148eb963419d577923b2eb1789f8a1
Sylvain Gugger <35901082+sgugger@users.noreply.github.com>
2023-06-07 16:34:53 -0400

    Add AzureOpenAiAgent (#24058)
    * Add AzureOpenAiAgent
    
    * quality
    
    * Update src/transformers/tools/agents.py
    
    Co-authored-by: Lysandre Debut <lysandre.debut@reseau.eseo.fr>
    
    ---------
    
    Co-authored-by: Lysandre Debut <lysandre.debut@reseau.eseo.fr>

--- src/transformers/__init__.py
+++ src/transformers/__init__.py
@@ -620,6 +620,7 @@ _import_structure = {
     ],
     "tools": [
         "Agent",
+        "AzureOpenAiAgent",
         "HfAgent",
         "LocalAgent",
         "OpenAiAgent",
@@ -4424,6 +4425,7 @@ if TYPE_CHECKING:
     # Tools
     from .tools import (
         Agent,
+        AzureOpenAiAgent,
         HfAgent,
         LocalAgent,
         OpenAiAgent,
