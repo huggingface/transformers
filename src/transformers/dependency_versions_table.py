f84e84617dd3a7d5497f3e60cbb950c257a2240c
Sylvain Gugger <35901082+sgugger@users.noreply.github.com>
2023-06-08 10:11:01 -0400

    Update the pin on Accelerate (#24110)

--- src/transformers/dependency_versions_table.py
+++ src/transformers/dependency_versions_table.py
@@ -3,7 +3,7 @@
 # 2. run `make deps_table_update``
 deps = {
     "Pillow": "Pillow",
-    "accelerate": "accelerate>=0.20.1",
+    "accelerate": "accelerate>=0.20.2",
     "av": "av==9.2.0",
     "beautifulsoup4": "beautifulsoup4",
     "black": "black~=23.1",
