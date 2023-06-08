3cd56779bff07b148bdeb1c40dda9b67b630c967
Serge Panev <spanev@nvidia.com>
2023-06-08 09:19:07 -0700

    Fix typo in Llama docstrings (#24020)
    * Fix typo in Llama docstrings
    
    Signed-off-by: Serge Panev <spanev@nvidia.com>
    
    * Update
    
    Signed-off-by: Serge Panev <spanev@nvidia.com>
    
    * make style
    
    Signed-off-by: Serge Panev <spanev@nvidia.com>
    
    ---------
    
    Signed-off-by: Serge Panev <spanev@nvidia.com>

--- src/transformers/models/opt/modeling_opt.py
+++ src/transformers/models/opt/modeling_opt.py
@@ -926,13 +926,13 @@ class OPTForCausalLM(OPTPreTrainedModel):
         >>> model = OPTForCausalLM.from_pretrained("facebook/opt-350m")
         >>> tokenizer = AutoTokenizer.from_pretrained("facebook/opt-350m")
 
-        >>> prompt = "Hey, are you consciours? Can you talk to me?"
+        >>> prompt = "Hey, are you conscious? Can you talk to me?"
         >>> inputs = tokenizer(prompt, return_tensors="pt")
 
         >>> # Generate
         >>> generate_ids = model.generate(inputs.input_ids, max_length=30)
         >>> tokenizer.batch_decode(generate_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0]
-        "Hey, are you consciours? Can you talk to me?\nI'm not consciours, but I can talk to you."
+        "Hey, are you conscious? Can you talk to me?\nI'm not conscious. I'm just a little bit of a weirdo."
         ```"""
 
         output_attentions = output_attentions if output_attentions is not None else self.config.output_attentions
