24afd51b16e5aa7641331d265d4cf6753de3d313
Ivan Reznikov <ivanreznikov@gmail.com>
2023-06-12 02:55:57 +0400

    multi-line fix

--- src/transformers/generation/utils.py
+++ src/transformers/generation/utils.py
@@ -1261,7 +1261,7 @@ class GenerationMixin:
                         " deprecated strategy to control generation and will be removed soon, in a future version."
                         " Please use a generation configuration file (see"
                         " https://huggingface.co/docs/transformers/main_classes/text_generation"
-                        ")"
+                        " )"
                     )
                     self.generation_config = new_generation_config
             generation_config = self.generation_config
