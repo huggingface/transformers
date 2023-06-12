92e179239ffcde7ff8d50293dc3200f0f975caa5
ydshieh <ydshieh@users.noreply.github.com>
2023-06-12 17:55:08 +0200

    fix

--- tests/models/rwkv/test_modeling_rwkv.py
+++ tests/models/rwkv/test_modeling_rwkv.py
@@ -261,7 +261,9 @@ class RwkvModelTester:
         return config, inputs_dict
 
 
-@unittest.skipIf(not is_torch_greater_or_equal_than_2_0, reason="See ")
+@unittest.skipIf(
+    not is_torch_greater_or_equal_than_2_0, reason="See https://github.com/huggingface/transformers/pull/24204"
+)
 @require_torch
 class RwkvModelTest(ModelTesterMixin, GenerationTesterMixin, PipelineTesterMixin, unittest.TestCase):
     all_model_classes = (RwkvModel, RwkvForCausalLM) if is_torch_available() else ()
@@ -422,7 +424,9 @@ class RwkvModelTest(ModelTesterMixin, GenerationTesterMixin, PipelineTesterMixin
             self.assertIsNotNone(model)
 
 
-@unittest.skipIf(not is_torch_greater_or_equal_than_2_0, reason="See ")
+@unittest.skipIf(
+    not is_torch_greater_or_equal_than_2_0, reason="See https://github.com/huggingface/transformers/pull/24204"
+)
 @slow
 class RWKVIntegrationTests(unittest.TestCase):
     def setUp(self):
