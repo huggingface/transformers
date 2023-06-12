1fede534911ce8375ca2f5da27a1761db48d6b70
Félix Marty <9808326+fxmarty@users.noreply.github.com>
2023-06-13 00:54:53 +0900

    copy

--- src/transformers/models/open_llama/modeling_open_llama.py
+++ src/transformers/models/open_llama/modeling_open_llama.py
@@ -111,7 +111,6 @@ class OpenLlamaRotaryEmbedding(torch.nn.Module):
         freqs = torch.einsum("i,j->ij", t, self.inv_freq)
         # Different from paper, but it uses a different permutation in order to obtain the same calculation
         emb = torch.cat((freqs, freqs), dim=-1)
-
         dtype = torch.get_default_dtype()
         self.register_buffer("cos_cached", emb.cos()[None, None, :, :].to(dtype), persistent=False)
         self.register_buffer("sin_cached", emb.sin()[None, None, :, :].to(dtype), persistent=False)
