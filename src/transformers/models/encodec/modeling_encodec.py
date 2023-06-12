fix scale computation

src/transformers/models/encodec/modeling_encodec.py
@@ -690,10 +690,10 @@ class EncodecModel(EncodecPreTrainedModel):
 
         scale = None
         if self.config.normalize:
-            # breakpoint()
-            unpadded_inputs = input_values[:, :, padding_mask[0]]
-            mono = unpadded_inputs.mean(dim=1, keepdim=True)
-            scale = mono.pow(2).mean(dim=2, keepdim=True).sqrt() + 1e-8
+            # if the padding is non zero
+            input_values = input_values * padding_mask
+            mono = torch.sum(input_values, 1, keepdim = True) / input_values.shape[1]
+            scale = mono.pow(2).mean(dim=-1, keepdim=True).sqrt() + 1e-8
             input_values = input_values / scale
 
         embeddings = self.encoder(input_values)

tests/models/encodec/test_modeling_encodec.py
@@ -453,7 +453,7 @@ class EncodecIntegrationTest(unittest.TestCase):
 
                 audio_codes, scales = encoder_outputs.to_tuple()
                 input_values_dec = model.decode(audio_codes, scales, padding_mask)[0]
-                input_values_enc_dec = model(input_values, bandwidth=float(bandwidth))[-1]
+                input_values_enc_dec = model(input_values, padding_mask, bandwidth=float(bandwidth))[-1]
 
             # make sure forward and decode gives same result
             self.assertTrue(torch.allclose(input_values_dec, input_values_enc_dec, atol=1e-3))