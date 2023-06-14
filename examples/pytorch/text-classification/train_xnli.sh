9c15f553a8b925851abcd2d7429ce30fde7f388f
HDThang <thang.huynh@moreh.com.vn>
2023-06-14 20:38:58 +0900

    Add trainign script for text-classification task

---
+++ examples/pytorch/text-classification/train_xnli.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+LOG_DIR="./logs"
+OUTPUT_DIR="./outputs"
+mkdir -p $LOG_DIR
+mkdir -p $OUTPUT_DIR
+##    SETTINGS     ## 
+MODEL=$1
+BATCH_SIZE=$2
+log_file="${LOG_DIR}/${model_name}.log"
+output_dir=${3:-"$OUTPUT_DIR/$model_name"}
+gpu_size=$4
+## END OF SETTINGS ## 
+
+export TRANSFORMERS_CACHE=/nas/huggingface_pretrained_models
+export HF_DATASETS_CACHE=/nas/common_data/huggingface
+
+args="
+--do_train \
+--do_eval \
+--learning_rate 3e-5 \
+--num_train_epochs 2 \
+--logging_strategy steps \
+--logging_steps 100 \
+--max_seq_length 384 \
+--doc_stride 128 \
+--overwrite_output_dir \
+--save_strategy epoch \
+--save_total_limit 2 \
+--seed 42
+"
+
+## Using moreh device
+moreh-switch-model --model $gpu_size
+
+python run_xnli.py \
+  --model_name_or_path $MODEL \
+  --language de \
+  --train_language en \
+  --per_device_train_batch_size $BATCH_SIZE \
+  --per_device_eval_batch_size $BATCH_SIZE \
+  --output_dir $output_dir \
+  $args \
+  2>&1 | tee $log_file
