391af089ee481f9bc3ed858168d7e048763b5678
Matt <rocketknight1@gmail.com>
2023-06-14 15:35:45 +0100

    Remove unnecessary skip

--- tests/models/groupvit/test_modeling_tf_groupvit.py
+++ tests/models/groupvit/test_modeling_tf_groupvit.py
@@ -692,11 +692,6 @@ class TFGroupViTModelTest(TFModelTesterMixin, PipelineTesterMixin, unittest.Test
     def test_saved_model_creation(self):
         pass
 
-    @unittest.skip(reason="Currently `saved_model` doesn't work with nested outputs.")
-    @slow
-    def test_saved_model_creation_extended(self):
-        pass
-
     @unittest.skip(reason="`saved_model` doesn't work with nested outputs so no preparation happens.")
     @slow
     def test_prepare_serving_output(self):
