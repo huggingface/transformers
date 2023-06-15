a611ac9b3f9493a80e2d0adf491f4868c71f71c5
JayL0321 <31190549+JayL0321@users.noreply.github.com>
2023-06-15 03:39:32 -0700

    remove unused is_decoder parameter in DetrAttention (#24226)
    * issue#24161 remove unused is_decoder parameter in DetrAttention
    
    * #24161 fix check_repository_consistency fail

--- src/transformers/models/table_transformer/modeling_table_transformer.py
+++ src/transformers/models/table_transformer/modeling_table_transformer.py
@@ -440,7 +440,6 @@ class TableTransformerAttention(nn.Module):
         embed_dim: int,
         num_heads: int,
         dropout: float = 0.0,
-        is_decoder: bool = False,
         bias: bool = True,
     ):
         super().__init__()
@@ -642,7 +641,6 @@ class TableTransformerDecoderLayer(nn.Module):
             embed_dim=self.embed_dim,
             num_heads=config.decoder_attention_heads,
             dropout=config.attention_dropout,
-            is_decoder=True,
         )
         self.dropout = config.dropout
         self.activation_fn = ACT2FN[config.activation_function]
@@ -653,7 +651,6 @@ class TableTransformerDecoderLayer(nn.Module):
             self.embed_dim,
             config.decoder_attention_heads,
             dropout=config.attention_dropout,
-            is_decoder=True,
         )
         self.encoder_attn_layer_norm = nn.LayerNorm(self.embed_dim)
         self.fc1 = nn.Linear(self.embed_dim, config.decoder_ffn_dim)
