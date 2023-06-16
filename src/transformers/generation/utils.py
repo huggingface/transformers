f1942219088e86d13db2ba691a4cf3e635a83617
blbadger <blbadger10@gmail.com>
2023-06-16 12:23:25 -0400

    input format debug

--- src/transformers/generation/utils.py
+++ src/transformers/generation/utils.py
@@ -2045,6 +2045,7 @@ class GenerationMixin:
                     # compute the candidate tokens by the language model and collect their hidden_states
                     next_model_inputs = self.prepare_inputs_for_generation(top_k_ids[:, i].unsqueeze(0), 
                                                 **model_kwargs)
+                    print (next_model_inputs)
                     outputs = self(
                         **next_model_inputs, 
                         return_dict=True, 
@@ -2086,6 +2087,7 @@ class GenerationMixin:
                 # compute the candidate tokens by the language model and collect their hidden_states
                 # assembles top_k_ids into batch of size k (leading to OOM for large models)
                 next_model_inputs = self.prepare_inputs_for_generation(top_k_ids.view(-1, 1), **model_kwargs)
+                print (next_model_inputs)
 
                 outputs = self(
                     **next_model_inputs, 
