6a76bb938a39c24b383e18192c19dfbb9a114a1f
Sanchit Gandhi <93869735+sanchit-gandhi@users.noreply.github.com>
2023-06-19 17:52:53 +0100

    Update src/transformers/models/musicgen/modeling_musicgen.py
    Co-authored-by: Patrick von Platen <patrick.v.platen@gmail.com>

--- src/transformers/models/musicgen/modeling_musicgen.py
+++ src/transformers/models/musicgen/modeling_musicgen.py
@@ -1427,11 +1427,6 @@ class MusicgenForCausalLM(MusicgenPreTrainedModel):
         else:
             return output_ids
 
-    def _reorder_cache(self, past_key_values, beam_idx):
-        reordered_past = ()
-        for layer_past in past_key_values:
-            reordered_past += (tuple(past_state.index_select(0, beam_idx) for past_state in layer_past),)
-        return reordered_past
 
 
 @add_start_docstrings(
