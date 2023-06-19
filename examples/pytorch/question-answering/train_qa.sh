e97f1a6316abe08b9db3461ca8238d5a6aa2f5d6
HDThang <thang.huynh@moreh.com.vn>
2023-06-19 18:08:58 +0900

    Modify format train script question answering task

--- examples/pytorch/question-answering/train_qa.sh
+++ examples/pytorch/question-answering/train_qa.sh
@@ -1,29 +1,24 @@
 #!/bin/bash
-LOG_DIR="./logs"
-OUTPUT_DIR="./outputs"
-mkdir -p $LOG_DIR
-mkdir -p $OUTPUT_DIR
-
 ##    SETTINGS     ## 
-# Defaul value
-output_dir="$OUTPUT_DIR"
-BATCH_SIZE=8
-gpu_size=2
 #Get the input arg
-while getopts m:b:o:g: flag
+while getopts m:b:g: flag
 do
     case "${flag}" in
-        m) MODEL=${OPTARG};;
-        b) BATCH_SIZE=${OPTARG};;
-        o) output_dir=${OPTARG};;
-        g) gpu_size=${OPTARG};;
+        m) model=${OPTARG};;
+        b) batch_size=${OPTARG};;
+        g) device_id=${OPTARG};;
     esac
 done
-log_file="${LOG_DIR}/train.log"
+
 ## END OF SETTINGS ## 
 
-export TRANSFORMERS_CACHE=/nas/huggingface_pretrained_models
-export HF_DATASETS_CACHE=/nas/common_data/huggingface
+LOG_DIR="./logs"
+OUTPUT_DIR="./outputs"
+log_file=$LOG_DIR/$model.log
+output_dir=$OUTPUT_DIR/$model
+
+mkdir -p "$(dirname $log_file)"
+mkdir -p "$(dirname $output_dir)"
 
 args="
 --do_train \
@@ -39,16 +34,15 @@ args="
 --save_total_limit 2 \
 --seed 42
 "
-
 ## Using moreh device
-moreh-switch-model --model $gpu_size
+export MOREH_VISIBLE_DEVICE=$device_id
 
 python run_qa.py \
-  --model_name_or_path $MODEL \
+  --model_name_or_path $model \
   --dataset_name squad_v2 \
   --version_2_with_negative \
-  --per_device_train_batch_size $BATCH_SIZE \
-  --per_device_eval_batch_size $BATCH_SIZE \
+  --per_device_train_batch_size $batch_size \
+  --per_device_eval_batch_size $batch_size \
   --output_dir $output_dir \
   $args \
   2>&1 | tee $log_file
--- examples/pytorch/question-answering/train_seq2seq.sh
+++ examples/pytorch/question-answering/train_seq2seq.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
-LOG_DIR="./logs"
-OUTPUT_DIR="./outputs"
-mkdir -p $LOG_DIR
-mkdir -p $OUTPUT_DIR
 ##    SETTINGS     ## 
-MODEL=$1
-BATCH_SIZE=$2
-log_file="${LOG_DIR}/${model_name}.log"
-output_dir=${3:-"$OUTPUT_DIR/$model_name"}
-gpu_size=$4
+#Get the input arg
+while getopts m:b:g: flag
+do
+    case "${flag}" in
+        m) model=${OPTARG};;
+        b) batch_size=${OPTARG};;
+        g) device_id=${OPTARG};;
+    esac
+done
+
 ## END OF SETTINGS ## 
 
-export TRANSFORMERS_CACHE=/nas/huggingface_pretrained_models
-export HF_DATASETS_CACHE=/nas/common_data/huggingface
+LOG_DIR="./logs"
+OUTPUT_DIR="./outputs"
+log_file=$LOG_DIR/$model.log
+output_dir=$OUTPUT_DIR/$model
+
+mkdir -p "$(dirname $log_file)"
+mkdir -p "$(dirname $output_dir)"
 
 args="
 --do_train \
@@ -32,15 +38,15 @@ args="
 --seed 42
 "
 
-## Using moreh device model
-moreh-switch-model --model $gpu_size
+## Using moreh device
+export MOREH_VISIBLE_DEVICE=$device_id
 
 python run_qa_beam_search.py \
-  --model_name_or_path $MODEL \
+  --model_name_or_path $model \
   --dataset_name squad_v2 \
   --version_2_with_negative \
-  --per_device_train_batch_size $BATCH_SIZE \
-  --per_device_eval_batch_size $BATCH_SIZE \
+  --per_device_train_batch_size $batch_size \
+  --per_device_eval_batch_size $batch_size \
   --output_dir $output_dir \
   $args \
   2>&1 | tee $log_file
