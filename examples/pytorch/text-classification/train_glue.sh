46da41e9bfae040d56196e7ef2c0fc1e563986ec
HDThang <thang.huynh@moreh.com.vn>
2023-06-19 17:45:22 +0900

    modify format train script for text-classification task

--- examples/pytorch/text-classification/train_glue.sh
+++ examples/pytorch/text-classification/train_glue.sh
@@ -1,32 +1,13 @@
-#!/bin/bash
-LOG_DIR="./logs"
-OUTPUT_DIR="./outputs"
-mkdir -p $LOG_DIR
-mkdir -p $OUTPUT_DIR
-
-export TRANSFORMERS_CACHE=/nas/huggingface_pretrained_models
-export HF_DATASETS_CACHE=/nas/common_data/huggingface
-export TASK_NAME=mrpc
-
-##    SETTINGS     ## 
-# Defaul value
-output_dir="$OUTPUT_DIR"
-BATCH_SIZE=8
-gpu_size=2
-#Get the input arg
-while getopts m:b:o:g:t: flag
+while getopts m:b:g:t: flag
 do
     case "${flag}" in
-        m) MODEL=${OPTARG};;
-        b) BATCH_SIZE=${OPTARG};;
-        o) output_dir=${OPTARG};;
-        g) gpu_size=${OPTARG};;
-        t) TASK_NAME=${OPTARG};;
+        m) model=${OPTARG};;
+        b) batch_size=${OPTARG};;
+        g) device_id=${OPTARG};;
+        t) task_name=${OPTARG};;
     esac
 done
-## END OF SETTINGS ## 
 
-## task list ##
 task_list=(
     "mrpc"
     "cola"
@@ -38,6 +19,17 @@ task_list=(
     "rte"
     "wnli"
 )
+
+log_file=$LOG_DIR/$model.log
+output_dir=$OUTPUT_DIR/$model
+
+mkdir -p "$(dirname $log_file)"
+mkdir -p "$(dirname $output_dir)"
+
+## Using moreh device
+export MOREH_VISIBLE_DEVICE=$device_id
+
+export TASK_NAME=$task_name
 
 args="
 --do_train \
@@ -53,14 +45,11 @@ args="
 --seed 42
 "
 
-## Using moreh device
-moreh-switch-model --model $gpu_size
-
 python run_glue.py \
-  --model_name_or_path $MODEL \
-  --task_name $TASK_NAME \
-  --per_device_train_batch_size $BATCH_SIZE \
-  --per_device_eval_batch_size $BATCH_SIZE \
+  --model_name_or_path $model \
+  --task_name ${TASK_NAME} \
+  --per_device_train_batch_size $batch_size \
+  --per_device_eval_batch_size $batch_size \
   --output_dir $output_dir \
   $args \
   2>&1 | tee $log_file
--- examples/pytorch/text-classification/train_xnli.sh
+++ examples/pytorch/text-classification/train_xnli.sh
@@ -1,28 +1,20 @@
-#!/bin/bash
-LOG_DIR="./logs"
-OUTPUT_DIR="./outputs"
-mkdir -p $LOG_DIR
-mkdir -p $OUTPUT_DIR
-
-export TRANSFORMERS_CACHE=/nas/huggingface_pretrained_models
-export HF_DATASETS_CACHE=/nas/common_data/huggingface
-
-##    SETTINGS     ## 
-# Defaul value
-output_dir="$OUTPUT_DIR"
-BATCH_SIZE=8
-gpu_size=2
-#Get the input arg
-while getopts m:b:o:g: flag
+while getopts m:b:g: flag
 do
     case "${flag}" in
-        m) MODEL=${OPTARG};;
-        b) BATCH_SIZE=${OPTARG};;
-        o) output_dir=${OPTARG};;
-        g) gpu_size=${OPTARG};;
+        m) model=${OPTARG};;
+        b) batch_size=${OPTARG};;
+        g) device_id=${OPTARG};;
     esac
 done
-## END OF SETTINGS ## 
+
+log_file=$LOG_DIR/$model.log
+output_dir=$OUTPUT_DIR/$model
+
+mkdir -p "$(dirname $log_file)"
+mkdir -p "$(dirname $output_dir)"
+
+## Using moreh device
+export MOREH_VISIBLE_DEVICE=$device_id
 
 args="
 --do_train \
@@ -32,21 +24,19 @@ args="
 --logging_strategy steps \
 --logging_steps 100 \
 --max_seq_length 384 \
+--doc_stride 128 \
 --overwrite_output_dir \
 --save_strategy epoch \
 --save_total_limit 2 \
 --seed 42
 "
 
-## Using moreh device
-moreh-switch-model --model $gpu_size
-
 python run_xnli.py \
-  --model_name_or_path $MODEL \
+  --model_name_or_path $model \
   --language de \
   --train_language en \
-  --per_device_train_batch_size $BATCH_SIZE \
-  --per_device_eval_batch_size $BATCH_SIZE \
+  --per_device_train_batch_size $batch_size \
+  --per_device_eval_batch_size $batch_size \
   --output_dir $output_dir \
   $args \
   2>&1 | tee $log_file
