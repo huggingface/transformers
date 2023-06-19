ac3e9c5ab9b370cc040b9ffad553e1b294940ebf
HDThang <thang.huynh@moreh.com.vn>
2023-06-19 17:57:39 +0900

    modifty format train script summarization task

--- examples/pytorch/summarization/train.sh
+++ examples/pytorch/summarization/train.sh
@@ -1,37 +1,25 @@
 #!/bin/bash
-LOG_DIR="./logs"
-OUTPUT_DIR="./outputs"
-mkdir -p $LOG_DIR
-mkdir -p $OUTPUT_DIR
-##    SETTINGS     ## 
-# Defaul value
-output_dir="$OUTPUT_DIR"
-BATCH_SIZE=8
-gpu_size=2
-#Get the input arg
-while getopts m:b:o:g: flag
+
+while getopts m:b:g: flag
 do
     case "${flag}" in
-        m) MODEL=${OPTARG};;
-        b) BATCH_SIZE=${OPTARG};;
-        o) output_dir=${OPTARG};;
-        g) gpu_size=${OPTARG};;
+        m) model=${OPTARG};;
+        b) batch_size=${OPTARG};;
+        g) device_id=${OPTARG};;
     esac
 done
-echo "MODEL: $MODEL ";
-echo "BATCH SIZE: $BATCH_SIZE";
-echo "OUTPUT DIR: $output_dir";
-echo "GPU SIZE: $gpu_size";
 
-log_file="${LOG_DIR}/train.log"
-# MODEL=$1
-# BATCH_SIZE=$2
-# output_dir=${3:-"$OUTPUT_DIR/$MODEL"}
-# gpu_size=${4:-2}
-## END OF SETTINGS ## 
+LOG_DIR="./logs"
+OUTPUT_DIR="./outputs"
+log_file=$LOG_DIR/$model.log
+output_dir=$OUTPUT_DIR/$model
 
-export TRANSFORMERS_CACHE=/nas/huggingface_pretrained_models
-export HF_DATASETS_CACHE=/nas/common_data/huggingface
+mkdir -p "$(dirname $log_file)"
+mkdir -p "$(dirname $output_dir)"
+
+## Using moreh device
+export MOREH_VISIBLE_DEVICE=$device_id
+
 
 args="
 --do_train \
@@ -47,15 +35,12 @@ args="
 --predict_with_generate
 "
 
-## Using moreh device
-moreh-switch-model --model $gpu_size
-
 python run_summarization.py \
-  --model_name_or_path $MODEL \
+  --model_name_or_path $model \
   --dataset_name xsum \
   --source_prefix "summarize: " \
-  --per_device_train_batch_size $BATCH_SIZE \
-  --per_device_eval_batch_size $BATCH_SIZE \
+  --per_device_train_batch_size $batch_size \
+  --per_device_eval_batch_size $batch_size \
   --output_dir $output_dir \
   $args \
   2>&1 | tee $log_file
