3db0b31b5a82d4af2cdcc7c244d32608c71a5f2a
arthur.zucker@gmail.com <arthur.zucker@gmail.com>
2023-06-20 10:21:34 +0000

    fixup

--- src/transformers/generation/logits_process.py
+++ src/transformers/generation/logits_process.py
@@ -546,8 +546,8 @@ class NoBadWordsLogitsProcessor(LogitsProcessor):
     Args:
         bad_words_ids (`List[List[int]]`):
             List of list of token ids that are not allowed to be generated. In order to get the token ids of the words
-            that should not appear in the generated text, make sure to set `add_prefix_space=True` when initializing the
-            tokenizer, and use `tokenizer(bad_words, add_special_tokens = False).input_ids`. The `add_prefix_space`
+            that should not appear in the generated text, make sure to set `add_prefix_space=True` when initializing
+            the tokenizer, and use `tokenizer(bad_words, add_special_tokens = False).input_ids`. The `add_prefix_space`
             argument is only supported for some slow tokenizers, as fast tokenizers's prefixing behaviours come from
             `pre tokenizers`. Read more [here](https://huggingface.co/docs/tokenizers/api/pre-tokenizers).
         eos_token_id (`Union[int, List[int]]`):
