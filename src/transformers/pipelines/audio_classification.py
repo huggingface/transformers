69f4a19ce5e16c8284b42400acc50009126188e2
Sanchit Gandhi <93869735+sanchit-gandhi@users.noreply.github.com>
2023-06-22 13:49:47 +0100

    Update src/transformers/pipelines/audio_classification.py
    Co-authored-by: Nicolas Patry <patry.nicolas@protonmail.com>

--- src/transformers/pipelines/audio_classification.py
+++ src/transformers/pipelines/audio_classification.py
@@ -162,7 +162,7 @@ class AudioClassificationPipeline(Pipeline):
             # better integration
             if not ("sampling_rate" in inputs and ("raw" in inputs or "array" in inputs)):
                 raise ValueError(
-                    "When passing a dictionary to AutomaticSpeechRecognitionPipeline, the dict needs to contain a "
+                    "When passing a dictionary to AudioClassificationPipeline, the dict needs to contain a "
                     '"raw" key containing the numpy array representing the audio and a "sampling_rate" key, '
                     "containing the sampling_rate associated with that array"
                 )
