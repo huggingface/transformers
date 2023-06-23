af05ec5451cd3c1511b11ca5f2b2816297011864
Yih-Dar <2521628+ydshieh@users.noreply.github.com>
2023-06-23 13:32:48 +0200

    Update src/transformers/models/whisper/modeling_tf_whisper.py
    Co-authored-by: amyeroberts <22614925+amyeroberts@users.noreply.github.com>

--- src/transformers/models/whisper/modeling_tf_whisper.py
+++ src/transformers/models/whisper/modeling_tf_whisper.py
@@ -1357,7 +1357,7 @@ class TFWhisperForConditionalGeneration(TFWhisperPreTrainedModel, TFCausalLangua
 
         Parameters:
             inputs (`tf.Tensor` of varying shape depending on the modality, *optional*):
-                The sequence used as a prompt for the generation or as model inputs to the encoder. If `None` the
+                The sequence used as a prompt for the generation or as model inputs to the encoder. If unset the
                 method initializes it with `bos_token_id` and a batch size of 1. For decoder-only models `inputs`
                 should of in the format of `input_ids`. For encoder-decoder models *inputs* can represent any of
                 `input_ids`, `input_values`, `input_features`, or `pixel_values`.
