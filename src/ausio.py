7e2f313b2c3524431f3f572f9185e0a2aecd44b5
shauray8 <shauray.209309012@muj.manipal.edu>
2023-06-25 22:33:01 +0530

    example script

--- src/ausio.py
+++ src/ausio.py
@@ -1,5 +1,6 @@
 from datasets import load_dataset, Audio
 from transformers import EncodecModel, AutoProcessor
+from transformers import Trainer
 librispeech_dummy = load_dataset("hf-internal-testing/librispeech_asr_dummy", "clean", split="validation")
 
 model = EncodecModel.from_pretrained("facebook/encodec_24khz")
@@ -8,7 +9,21 @@ librispeech_dummy = librispeech_dummy.cast_column("audio", Audio(sampling_rate=p
 audio_sample = librispeech_dummy[-1]["audio"]["array"]
 inputs = processor(raw_audio=audio_sample, sampling_rate=processor.sampling_rate, return_tensors="pt")
 
-encoder_outputs = model.encode(inputs["input_values"], inputs["padding_mask"])
-audio_values = model.decode(encoder_outputs.audio_codes, encoder_outputs.audio_scales, inputs["padding_mask"])[0]
-# or the equivalent with a forward pass
-audio_values = model(inputs["input_values"], inputs["padding_mask"]).audio_values
+class CustomTrainer(Trainer):
+  def compute_loss(self, model, inputs, return_outputs=False):
+      labels = inputs.get("labels")
+      # forward pass
+      outputs = model(inputs)
+      logits = outputs.get("logits")
+      # compute custom loss (suppose one has 3 labels with different weights)
+      loss_fct = nn.CrossEntropyLoss(weight=torch.tensor([1.0, 2.0, 3.0], device=model.device))
+      loss = loss_fct(logits.view(-1, self.model.config.num_labels), labels.view(-1))
+      return (loss, outputs) if return_outputs else loss
+
+#encoder_outputs = model.encode(inputs["input_values"], inputs["padding_mask"])
+#audio_values = model.decode(encoder_outputs.audio_codes, encoder_outputs.audio_scales, inputs["padding_mask"])[0]
+## or the equivalent with a forward pass
+#audio_values = model(inputs["input_values"], inputs["padding_mask"]).audio_values
+
+a = CustomTrainer()
+a.compute_loss(model, inputs)
