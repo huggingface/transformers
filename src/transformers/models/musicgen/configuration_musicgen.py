04c19d31418adcf15f5e282538f2e90646c331c3
Sanchit Gandhi <93869735+sanchit-gandhi@users.noreply.github.com>
2023-06-26 11:21:19 +0100

    Apply suggestions from code review
    Co-authored-by: Sylvain Gugger <35901082+sgugger@users.noreply.github.com>

--- src/transformers/models/musicgen/configuration_musicgen.py
+++ src/transformers/models/musicgen/configuration_musicgen.py
@@ -12,7 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-""" Musicgen model configuration"""
+""" MusicGen model configuration"""
 import copy
 
 from ...configuration_utils import PretrainedConfig
--- src/transformers/models/musicgen/convert_musicgen_transformers.py
+++ src/transformers/models/musicgen/convert_musicgen_transformers.py
@@ -12,7 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Convert Musicgen checkpoints from the original repository."""
+"""Convert MusicGen checkpoints from the original repository."""
 import argparse
 from pathlib import Path
 from typing import Dict, OrderedDict, Tuple
