07d960b3d02d4b566a2ec131a2c0c77616dc00f8
Sourab Mangrulkar <13534540+pacman100@users.noreply.github.com>
2023-06-27 14:23:11 +0530

    Update hyperparameter_search.py

--- src/transformers/hyperparameter_search.py
+++ src/transformers/hyperparameter_search.py
@@ -119,7 +119,9 @@ ALL_HYPERPARAMETER_SEARCH_BACKENDS = {
 
 
 def default_hp_search_backend() -> str:
-    available_backends = [backend for backend in ALL_HYPERPARAMETER_SEARCH_BACKENDS.values() if backend.is_available()]
+    available_backends = [
+        backend for backend in ALL_HYPERPARAMETER_SEARCH_BACKENDS.values() if backend().is_available()
+    ]
     if len(available_backends) > 0:
         name = available_backends[0].name
         if len(available_backends) > 1:
