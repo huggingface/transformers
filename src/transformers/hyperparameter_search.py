5218fdd26fb595ea8dcd75175b0edc4a4bc25b66
Sourab Mangrulkar <13534540+pacman100@users.noreply.github.com>
2023-06-27 18:42:15 +0530

    Update hyperparameter_search.py (#24515)
    * Update hyperparameter_search.py
    
    * resolve comments

--- src/transformers/hyperparameter_search.py
+++ src/transformers/hyperparameter_search.py
@@ -40,7 +40,8 @@ class HyperParamSearchBackendBase:
     name: str
     pip_package: str = None
 
-    def is_available(self):
+    @staticmethod
+    def is_available():
         raise NotImplementedError
 
     def run(self, trainer, n_trials: int, direction: str, **kwargs):
@@ -63,7 +64,8 @@ class HyperParamSearchBackendBase:
 class OptunaBackend(HyperParamSearchBackendBase):
     name = "optuna"
 
-    def is_available(self):
+    @staticmethod
+    def is_available():
         return is_optuna_available()
 
     def run(self, trainer, n_trials: int, direction: str, **kwargs):
@@ -77,7 +79,8 @@ class RayTuneBackend(HyperParamSearchBackendBase):
     name = "ray"
     pip_package = "'ray[tune]'"
 
-    def is_available(self):
+    @staticmethod
+    def is_available():
         return is_ray_available()
 
     def run(self, trainer, n_trials: int, direction: str, **kwargs):
@@ -90,7 +93,8 @@ class RayTuneBackend(HyperParamSearchBackendBase):
 class SigOptBackend(HyperParamSearchBackendBase):
     name = "sigopt"
 
-    def is_available(self):
+    @staticmethod
+    def is_available():
         return is_sigopt_available()
 
     def run(self, trainer, n_trials: int, direction: str, **kwargs):
@@ -103,7 +107,8 @@ class SigOptBackend(HyperParamSearchBackendBase):
 class WandbBackend(HyperParamSearchBackendBase):
     name = "wandb"
 
-    def is_available(self):
+    @staticmethod
+    def is_available():
         return is_wandb_available()
 
     def run(self, trainer, n_trials: int, direction: str, **kwargs):
