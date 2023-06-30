276bf6e8de817d4b3cb5fd3f14d97960aa975529
Sheon Han <sheon.han@gmail.com>
2023-06-30 02:13:07 -0700

    image_size to 32

--- src/transformers/models/ict/configuration_ict.py
+++ src/transformers/models/ict/configuration_ict.py
@@ -66,7 +66,7 @@ class IctConfig(PretrainedConfig):
             The standard deviation of the truncated_normal_initializer for initializing all weight matrices.
         layer_norm_eps (`float`, *optional*, defaults to 1e-12):
             The epsilon used by the layer normalization layers.
-        image_size (`int`, *optional*, defaults to 1024):
+        image_size (`int`, *optional*, defaults to 32):
             The size (resolution) of each image.
         num_channels (`int`, *optional*, defaults to 3):
             The number of input channels.
