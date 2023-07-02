c48a0466eeb8785a4b12bc4cb4624ac42ecdce48
Shahad Mahmud <shahad9381@gmail.com>
2023-07-02 17:05:48 +0600

    precompiled_charsmap checking for SPM tokenizer models - correct formatting

--- src/transformers/convert_slow_tokenizer.py
+++ src/transformers/convert_slow_tokenizer.py
@@ -551,10 +551,10 @@ class AlbertConverter(SpmConverter):
             list_normalizers.append(normalizers.Lowercase())
 
         precompiled_charsmap = proto.normalizer_spec.precompiled_charsmap
-        
+
         if precompiled_charsmap:
             list_normalizers.append(normalizers.Precompiled(precompiled_charsmap))
-        
+
         list_normalizers.append(normalizers.Replace(Regex(" {2,}"), " "))
         return normalizers.Sequence(list_normalizers)
 
@@ -842,10 +842,10 @@ class RemBertConverter(SpmConverter):
             list_normalizers.append(normalizers.Lowercase())
 
         precompiled_charsmap = proto.normalizer_spec.precompiled_charsmap
-        
+
         if precompiled_charsmap:
             list_normalizers.append(normalizers.Precompiled(precompiled_charsmap))
-        
+
         return normalizers.Sequence(list_normalizers)
 
     def post_processor(self):
