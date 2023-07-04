1fa1d594afb9e4d555b8f9b419b8fac63ed5fb19
tensorpro <tensorpro@gmail.com>
2023-07-03 22:38:47 -0400

    _set_token_in_kwargs to fix tests

--- src/transformers/models/clip_vip/configuration_clip_vip.py
+++ src/transformers/models/clip_vip/configuration_clip_vip.py
@@ -126,6 +126,7 @@ class CLIPViPTextConfig(PretrainedConfig):
 
     @classmethod
     def from_pretrained(cls, pretrained_model_name_or_path: Union[str, os.PathLike], **kwargs) -> "PretrainedConfig":
+        cls._set_token_in_kwargs(kwargs)
         config_dict, kwargs = cls.get_config_dict(pretrained_model_name_or_path, **kwargs)
 
         # get the text config dict if we are loading from CLIPViPConfig
@@ -234,6 +235,7 @@ class CLIPViPVisionConfig(PretrainedConfig):
 
     @classmethod
     def from_pretrained(cls, pretrained_model_name_or_path: Union[str, os.PathLike], **kwargs) -> "PretrainedConfig":
+        cls._set_token_in_kwargs(kwargs)
         config_dict, kwargs = cls.get_config_dict(pretrained_model_name_or_path, **kwargs)
 
         # get the vision config dict if we are loading from CLIPViPConfig
