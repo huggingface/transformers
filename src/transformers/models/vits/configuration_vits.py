7c5df8bbe8a8b36b1b5062eae8aba8ba01496123
sanchit-gandhi <sanchit@huggingface.co>
2023-07-05 09:43:51 +0100

    fix copyright: vits authors -> kakao enterprise

--- src/transformers/models/vits/configuration_vits.py
+++ src/transformers/models/vits/configuration_vits.py
@@ -1,5 +1,5 @@
 # coding=utf-8
-# Copyright 2023 The VITS Authors and the HuggingFace Inc. team. All rights reserved.
+# Copyright 2023 The Kakao Enterprise Authors and the HuggingFace Inc. team. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
