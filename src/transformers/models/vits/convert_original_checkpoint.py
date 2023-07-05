111a20ff8f66a135c054eb627c750295962d2afe
sanchit-gandhi <sanchit@huggingface.co>
2023-07-05 09:43:12 +0100

    remove redundant str -> list

--- src/transformers/models/vits/convert_original_checkpoint.py
+++ src/transformers/models/vits/convert_original_checkpoint.py
@@ -324,7 +324,7 @@ def convert_checkpoint(
         _punctuation = ';:,.!?¡¿—…"«»“” '
         _letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
         _letters_ipa = "ɑɐɒæɓʙβɔɕçɗɖðʤəɘɚɛɜɝɞɟʄɡɠɢʛɦɧħɥʜɨɪʝɭɬɫɮʟɱɯɰŋɳɲɴøɵɸθœɶʘɹɺɾɻʀʁɽʂʃʈʧʉʊʋⱱʌɣɤʍχʎʏʑʐʒʔʡʕʢǀǁǂǃˈˌːˑʼʴʰʱʲʷˠˤ˞↓↑→↗↘'̩'ᵻ"
-        symbols = [_pad] + list(_punctuation) + list(_letters) + list(_letters_ipa)
+        symbols = _pad + _punctuation + _letters + _letters_ipa
         symbol_to_id = {s: i for i, s in enumerate(symbols)}
         phonemize = True
     else:
