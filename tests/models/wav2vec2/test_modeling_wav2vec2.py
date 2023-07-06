d67b3489cc5802a0a26656bbaf596db8ee285e54
Kirandevraj <thekirandevraj@gmail.com>
2023-07-07 00:11:59 +0530

    updating _compute_mask_indices test cases by removing torch.from_numpy

--- tests/models/wav2vec2/test_modeling_wav2vec2.py
+++ tests/models/wav2vec2/test_modeling_wav2vec2.py
@@ -1245,7 +1245,7 @@ class Wav2Vec2UtilsTest(unittest.TestCase):
         mask_length = 1
 
         mask = _compute_mask_indices((batch_size, sequence_length), mask_prob, mask_length)
-        mask = torch.from_numpy(mask).to(torch_device)
+        mask = mask.to(torch_device)
 
         self.assertListEqual(mask.sum(axis=-1).tolist(), [mask_prob * sequence_length for _ in range(batch_size)])
 
@@ -1264,7 +1264,7 @@ class Wav2Vec2UtilsTest(unittest.TestCase):
 
         for _ in range(n_trials):
             mask = _compute_mask_indices((batch_size, sequence_length), mask_prob, mask_length)
-            mask = torch.from_numpy(mask).to(torch_device)
+            mask = mask.to(torch_device)
 
             num_masks = torch.sum(mask).item()
 
@@ -1286,7 +1286,7 @@ class Wav2Vec2UtilsTest(unittest.TestCase):
         mask_length = 4
 
         mask = _compute_mask_indices((batch_size, sequence_length), mask_prob, mask_length)
-        mask = torch.from_numpy(mask).to(torch_device)
+        mask = mask.to(torch_device)
 
         # because of overlap mask don't have to add up exactly to `mask_prob * sequence_length`, but have to be smaller or equal
         for batch_sum in mask.sum(axis=-1):
@@ -1304,7 +1304,7 @@ class Wav2Vec2UtilsTest(unittest.TestCase):
         mask = _compute_mask_indices(
             (batch_size, sequence_length), mask_prob, mask_length, attention_mask=attention_mask
         )
-        mask = torch.from_numpy(mask).to(torch_device)
+        mask = mask.to(torch_device)
 
         for batch_sum in mask.sum(axis=-1):
             self.assertTrue(int(batch_sum) <= mask_prob * sequence_length)
