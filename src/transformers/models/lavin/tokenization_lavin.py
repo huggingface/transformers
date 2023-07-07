dc4253dc9f6bb2ace14f11db9e984e482440fdec
shauray8 <shauray9@gmail.com>
2023-07-07 21:39:44 +0530

    cleaning, might work might not work

--- src/transformers/models/lavin/tokenization_lavin.py
+++ src/transformers/models/lavin/tokenization_lavin.py
@@ -100,31 +100,3 @@ class lavin_tokenizer(PreTrainedTokenizer):
     out_string += self.sp_model.decode(current_sub_tokens)
     return out_string
 
-
-
-    # reload tokenizer
-    assert os.path.isfile(model_path), model_path
-    self.sp_model = SentencePieceProcessor(model_file=model_path)
-    logger.info(f"Reloaded SentencePiece model from {model_path}")
-
-    # BOS / EOS token IDs
-    self.n_words: int = self.sp_model.vocab_size()
-    self.bos_id: int = self.sp_model.bos_id()
-    self.eos_id: int = self.sp_model.eos_id()
-    self.pad_id: int = self.sp_model.pad_id()
-    logger.info(
-        f"#words: {self.n_words} - BOS ID: {self.bos_id} - EOS ID: {self.eos_id}"
-    )
-    assert self.sp_model.vocab_size() == self.sp_model.get_piece_size()
-
-  def encode(self, s: str, bos: bool, eos: bool) -> List[int]:
-    assert type(s) is str
-    t = self.sp_model.encode(s)
-    if bos:
-      t = [self.bos_id] + t
-    if eos:
-      t = t + [self.eos_id]
-    return t
-
-  def decode(self, t: List[int]) -> str:
-    return self.sp_model.decode(t)
