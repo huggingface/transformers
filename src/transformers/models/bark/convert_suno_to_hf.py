face04a8a164a157996927a2ebbe0f870998d0cc
Yoach Lacombe <yoach.lacombe@gmail.com>
2023-07-10 10:15:15 +0000

    make style convert_suno

--- src/transformers/models/bark/convert_suno_to_hf.py
+++ src/transformers/models/bark/convert_suno_to_hf.py
@@ -7,25 +7,20 @@ import torch
 from bark.generation import _load_model as _bark_load_model
 from huggingface_hub import hf_hub_download
 
-from transformers import set_seed, EncodecModel, EncodecConfig
+from transformers import EncodecConfig, EncodecModel, set_seed
 from transformers.models.bark.configuration_bark import (
     BarkCoarseConfig,
+    BarkConfig,
     BarkFineConfig,
     BarkSemanticConfig,
-    BarkConfig,
 )
 from transformers.models.bark.generation_configuration_bark import (
     BarkCoarseGenerationConfig,
     BarkFineGenerationConfig,
-    BarkSemanticGenerationConfig,
     BarkGenerationConfig,
+    BarkSemanticGenerationConfig,
 )
-from transformers.models.bark.modeling_bark import (
-    BarkCoarseModel,
-    BarkFineModel,
-    BarkSemanticModel,
-    BarkModel
-)
+from transformers.models.bark.modeling_bark import BarkCoarseModel, BarkFineModel, BarkModel, BarkSemanticModel
 from transformers.utils import logging
 
 
@@ -221,24 +216,25 @@ def load_whole_bark_model(
     hub_path,
     folder_path,
 ):
-    pytorch_dump_folder_path = os.path.join(folder_path,append_text)
+    pytorch_dump_folder_path = os.path.join(folder_path, append_text)
 
-
-    semanticConfig = BarkSemanticConfig.from_pretrained(os.path.join(semantic_path,"config.json"))
-    coarseAcousticConfig = BarkCoarseConfig.from_pretrained(os.path.join(coarse_path,"config.json"))
-    fineAcousticConfig = BarkFineConfig.from_pretrained(os.path.join(fine_path,"config.json"))
+    semanticConfig = BarkSemanticConfig.from_pretrained(os.path.join(semantic_path, "config.json"))
+    coarseAcousticConfig = BarkCoarseConfig.from_pretrained(os.path.join(coarse_path, "config.json"))
+    fineAcousticConfig = BarkFineConfig.from_pretrained(os.path.join(fine_path, "config.json"))
     codecConfig = EncodecConfig.from_pretrained("facebook/encodec_24khz")
-
 
     semantic = BarkSemanticModel.from_pretrained(semantic_path)
     coarseAcoustic = BarkCoarseModel.from_pretrained(coarse_path)
     fineAcoustic = BarkFineModel.from_pretrained(fine_path)
     codec = EncodecModel.from_pretrained("facebook/encodec_24khz")
 
-    bark_config = BarkConfig.from_sub_model_configs(semanticConfig, coarseAcousticConfig, fineAcousticConfig, codecConfig)
+    bark_config = BarkConfig.from_sub_model_configs(
+        semanticConfig, coarseAcousticConfig, fineAcousticConfig, codecConfig
+    )
 
-    bark_generation_config = BarkGenerationConfig.from_sub_model_configs(semantic.generation_config, coarseAcoustic.generation_config, fineAcoustic.generation_config)
-
+    bark_generation_config = BarkGenerationConfig.from_sub_model_configs(
+        semantic.generation_config, coarseAcoustic.generation_config, fineAcoustic.generation_config
+    )
 
     bark = BarkModel(bark_config)
 
@@ -249,10 +245,10 @@ def load_whole_bark_model(
 
     bark.generation_config = bark_generation_config
 
-
     Path(pytorch_dump_folder_path).mkdir(exist_ok=True)
     bark.save_pretrained(pytorch_dump_folder_path, repo_id=hub_path, push_to_hub=True)
-   
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     # Required parameters
