d7176e2705465fcc9c2004c926f7d2fdd42a972d
Matt <rocketknight1@gmail.com>
2023-07-10 12:12:28 +0100

    Recompute the rotary embeddings much less often

--- src/transformers/models/falcon/modeling_falcon.py
+++ src/transformers/models/falcon/modeling_falcon.py
@@ -76,15 +76,15 @@ class FalconRotaryEmbedding(nn.Module):
         inv_freq = 1.0 / (base ** (torch.arange(0, head_dim, 2).float() / head_dim))
         self.register_buffer("inv_freq", inv_freq, persistent=False)
         self.head_dim = head_dim
-        self.seq_len_cached = None
-        self.batch_size_cached = None
+        self.seq_len_cached = -1
         self.cos_cached: torch.Tensor | None = None
         self.sin_cached: torch.Tensor | None = None
 
-    def cos_sin(self, seq_len: int, device="cpu", dtype=torch.bfloat16) -> torch.Tensor:
-        if seq_len != self.seq_len_cached:
-            self.seq_len_cached = seq_len
-            t = torch.arange(seq_len, device=device, dtype=self.inv_freq.dtype)
+    def cos_sin(self, seq_len: int, past_key_values_length: int, device="cpu", dtype=torch.bfloat16) -> torch.Tensor:
+        total_length = seq_len + past_key_values_length
+        if total_length > self.seq_len_cached:
+            self.seq_len_cached = total_length
+            t = torch.arange(total_length, device=device, dtype=self.inv_freq.dtype)
             freqs = torch.einsum("i,j->ij", t, self.inv_freq)
             emb = torch.cat((freqs, freqs), dim=-1).to(device)
 
@@ -97,12 +97,14 @@ class FalconRotaryEmbedding(nn.Module):
             self.cos_cached = self.cos_cached.type(dtype)
             self.sin_cached = self.sin_cached.type(dtype)
 
-        return self.cos_cached.to(device), self.sin_cached.to(device)
+        return (
+            self.cos_cached[:, past_key_values_length : seq_len + past_key_values_length],
+            self.sin_cached[:, past_key_values_length : seq_len + past_key_values_length],
+        )
 
     def forward(self, query, key, past_key_values_length=0):
         batch, seq_len, head_dim = query.shape
-        cos, sin = self.cos_sin(seq_len + past_key_values_length, query.device, query.dtype)
-        cos, sin = cos[:, past_key_values_length:], sin[:, past_key_values_length:]
+        cos, sin = self.cos_sin(seq_len, past_key_values_length, query.device, query.dtype)
         return (query * cos) + (rotate_half(query) * sin), (key * cos) + (rotate_half(key) * sin)
 
 
