2d0ee482a05e263261a00465fa84e1d67f88c4be
Yoach Lacombe <yoach.lacombe@gmail.com>
2023-07-10 10:12:26 +0000

    add loading whole BarkModel to convert_suno_to_hf

--- src/transformers/models/bark/convert_suno_to_hf.py
+++ src/transformers/models/bark/convert_suno_to_hf.py
@@ -205,6 +205,46 @@ def load_model(pytorch_dump_folder_path, use_small=False, model_type="text"):
     model.save_pretrained(pytorch_dump_folder_path)
 
 
+def load_whole_bark_model(
+    semantic_path,
+    coarse_path,
+    fine_path,
+    append_text,
+    hub_path,
+    folder_path,
+):
+    pytorch_dump_folder_path = os.path.join(folder_path,append_text)
+
+
+    semanticConfig = BarkSemanticConfig.from_pretrained(os.path.join(semantic_path,"config.json"))
+    coarseAcousticConfig = BarkCoarseConfig.from_pretrained(os.path.join(coarse_path,"config.json"))
+    fineAcousticConfig = BarkFineConfig.from_pretrained(os.path.join(fine_path,"config.json"))
+    codecConfig = EncodecConfig.from_pretrained("facebook/encodec_24khz")
+
+
+    semantic = BarkSemanticModel.from_pretrained(semantic_path)
+    coarseAcoustic = BarkCoarseModel.from_pretrained(coarse_path)
+    fineAcoustic = BarkFineModel.from_pretrained(fine_path)
+    codec = EncodecModel.from_pretrained("facebook/encodec_24khz")
+
+    bark_config = BarkConfig.from_sub_model_configs(semanticConfig, coarseAcousticConfig, fineAcousticConfig, codecConfig)
+
+    bark_generation_config = BarkGenerationConfig.from_sub_model_configs(semantic.generation_config, coarseAcoustic.generation_config, fineAcoustic.generation_config)
+
+
+    bark = BarkModel(bark_config)
+
+    bark.semantic = semantic
+    bark.coarse_acoustics = coarseAcoustic
+    bark.fine_acoustics = fineAcoustic
+    bark.codec_model = codec
+
+    bark.generation_config = bark_generation_config
+
+
+    Path(pytorch_dump_folder_path).mkdir(exist_ok=True)
+    bark.save_pretrained(pytorch_dump_folder_path, repo_id=hub_path, push_to_hub=True)
+   
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     # Required parameters
