23ab15708dd3fd171758fd6cd5c11591252c35c8
Arthur <48595927+ArthurZucker@users.noreply.github.com>
2023-07-11 13:39:23 +0200

    Apply suggestions from code review
    Co-authored-by: amyeroberts <22614925+amyeroberts@users.noreply.github.com>

--- src/transformers/models/llama/tokenization_llama.py
+++ src/transformers/models/llama/tokenization_llama.py
@@ -117,7 +117,7 @@ class LlamaTokenizer(PreTrainedTokenizer):
         )
         if legacy:
             logger.warning_once(
-                f"You are using the legacy behaviour of the {self.__class__}. This means that tokens that come after special tokens will not be properly handled. We recommand you to"
+                f"You are using the legacy behaviour of the {self.__class__}. This means that tokens that come after special tokens will not be properly handled. We recommend you to"
                 " read the related pull request available at https://github.com/huggingface/transformers/pull/24565"
             )
         self.legacy = legacy
