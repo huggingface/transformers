32c416629d0f8d85e4535b780cb6c85078810d06
Bauke Brenninkmeijer <bauke.brenninkmeijer@nl.abnamro.com>
2023-07-11 21:20:00 +0200

    reset formatting change

--- src/transformers/benchmark/benchmark_tf.py
+++ src/transformers/benchmark/benchmark_tf.py
@@ -24,10 +24,7 @@ from functools import wraps
 from typing import Callable, Optional
 
 from ..configuration_utils import PretrainedConfig
-from ..models.auto.modeling_tf_auto import (
-    TF_MODEL_MAPPING,
-    TF_MODEL_WITH_LM_HEAD_MAPPING,
-)
+from ..models.auto.modeling_tf_auto import TF_MODEL_MAPPING, TF_MODEL_WITH_LM_HEAD_MAPPING
 from ..utils import is_py3nvml_available, is_tf_available, logging
 from .benchmark_utils import (
     Benchmark,
