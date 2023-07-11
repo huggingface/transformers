304bf8bb7413d28f3934f472a1b8e4b7aa4bf1a6
Zach Mueller <muellerzr@gmail.com>
2023-07-11 19:54:43 +0000

    self

--- src/transformers/trainer.py
+++ src/transformers/trainer.py
@@ -689,7 +689,7 @@ class Trainer:
         self.control = self.callback_handler.on_init_end(self.args, self.state, self.control)
 
         # Internal variables to keep track of the original batch size
-        self._train_batch_size = args.train_batch_size
+        self._train_batch_size = self.args.train_batch_size
 
         # very last
         self._memory_tracker.stop_and_update_metrics()
@@ -1504,7 +1504,7 @@ class Trainer:
             raise TypeError(f"train() received got unexpected keyword arguments: {', '.join(list(kwargs.keys()))}.")
         # This might change the seed so needs to run first.
         self._hp_search_setup(trial)
-        self._train_batch_size = args.train_batch_size
+        self._train_batch_size = self.args.train_batch_size
 
         # Model re-init
         model_reloaded = False
