01afedc0ab88ecd44edb7512fcb3f588d9b55f88
Bauke Brenninkmeijer <bauke.brenninkmeijer@nl.abnamro.com>
2023-07-11 22:19:46 +0200

    don't touch this file

--- src/transformers/data/metrics/__init__.py
+++ src/transformers/data/metrics/__init__.py
@@ -60,8 +60,7 @@ def pearson_and_spearman(preds, labels):
 def glue_compute_metrics(task_name, preds, labels):
     warnings.warn(DEPRECATION_WARNING, FutureWarning)
     requires_backends(glue_compute_metrics, "sklearn")
-    if len(preds) != len(labels):
-        raise ValueError(f"Predictions and labels have mismatched lengths {len(preds)} and {len(labels)}")
+    assert len(preds) == len(labels), f"Predictions and labels have mismatched lengths {len(preds)} and {len(labels)}"
     if task_name == "cola":
         return {"mcc": matthews_corrcoef(labels, preds)}
     elif task_name == "sst-2":
