a371860f34fe4dfc258a5355b64b1b47596e8d82
Arthur <48595927+ArthurZucker@users.noreply.github.com>
2023-07-11 22:02:18 +0900

    [Patch-t5-tokenizer] Patches the changes on T5 to make sure previous behaviour is still valide for beginning of words (#24622)
    * patch `_tokenize` function
    
    * more tests
    
    * properly fix
    
    * fixup
    
    * Update src/transformers/models/t5/tokenization_t5.py
    
    Co-authored-by: amyeroberts <22614925+amyeroberts@users.noreply.github.com>
    
    * fix without ifs
    
    * update
    
    * protect import
    
    * add python processing
    
    * is first needed
    
    * add doc and update with lefacy
    
    * updaate
    
    * fix T5 SPM converter
    
    * styling
    
    * fix T5 warning
    
    * add is_seqio_available
    
    * remove is_first
    
    * revert some changes
    
    * more tests and update
    
    * update llama test batterie
    
    * fixup
    
    * refactor T5 spm common tests
    
    * draft the llama tests
    
    * update
    
    * uopdate test
    
    * nits
    
    * refine
    
    * name nit
    
    * fix t5 tests
    
    * fix T5
    
    * update
    
    * revert convert slow to fast changes that fail lots of tests
    
    * legacy support
    
    * fixup
    
    * nits is first not defined
    
    * don't use legacy behaviour for switch transformers
    
    * style
    
    * My attempt to check.
    
    * nits
    
    * fixes
    
    * update
    
    * fixup
    
    * Apply suggestions from code review
    
    Co-authored-by: amyeroberts <22614925+amyeroberts@users.noreply.github.com>
    
    * updates
    
    * fixup
    
    * add legacy warning
    
    * fixup
    
    * warning_once nit
    
    * update t5 documentation test
    
    * update llama tok documentation
    
    * add space to warning
    
    * nits
    
    * nit
    
    * Apply suggestions from code review
    
    Co-authored-by: amyeroberts <22614925+amyeroberts@users.noreply.github.com>
    
    * last nits
    
    ---------
    
    Co-authored-by: amyeroberts <22614925+amyeroberts@users.noreply.github.com>
    Co-authored-by: Nicolas Patry <patry.nicolas@protonmail.com>

--- src/transformers/utils/import_utils.py
+++ src/transformers/utils/import_utils.py
@@ -112,6 +112,7 @@ _sacremoses_available = _is_package_available("sacremoses")
 _safetensors_available = _is_package_available("safetensors")
 _scipy_available = _is_package_available("scipy")
 _sentencepiece_available = _is_package_available("sentencepiece")
+_is_seqio_available = _is_package_available("seqio")
 _sklearn_available = importlib.util.find_spec("sklearn") is not None
 if _sklearn_available:
     try:
@@ -505,6 +506,10 @@ def is_sklearn_available():
 
 def is_sentencepiece_available():
     return _sentencepiece_available
+
+
+def is_seqio_available():
+    return _is_seqio_available
 
 
 def is_protobuf_available():
