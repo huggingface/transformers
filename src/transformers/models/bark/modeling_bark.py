7651493def0858ec3588701fe185e187a20fc0e1
Yoach Lacombe <yoach.lacombe@gmail.com>
2023-07-13 14:05:30 +0000

    correct docstrings python snippet

--- src/transformers/models/bark/modeling_bark.py
+++ src/transformers/models/bark/modeling_bark.py
@@ -1410,8 +1410,8 @@ class BarkModel(BarkPreTrainedModel):
         ```python
         >>> from transformers import AutoProcessor, BarkModel
 
-        >>> processor = AutoProcessor.from_pretrained("suno/bark-small")
-        >>> model = BarkModel.from_pretrained("suno/bark-small")
+        >>> processor = AutoProcessor.from_pretrained("ylacombe/bark-small")
+        >>> model = BarkModel.from_pretrained("ylacombe/bark-small")
 
         >>> # To add a voice preset, you can pass `voice_preset` to `BarkProcessor.__call__(...)`
         >>> voice_preset = "v2/en_speaker_6"
