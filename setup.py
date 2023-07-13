08667050223a6f27b8cd3a057bc06585ff190ecf
Georgie Mathews <13368542+georgiemathews@users.noreply.github.com>
2023-07-13 09:56:43 -0700

    Update setup.py to be compatible with pipenv (#24789)

--- setup.py
+++ setup.py
@@ -444,7 +444,7 @@ setup(
     extras_require=extras,
     entry_points={"console_scripts": ["transformers-cli=transformers.commands.transformers_cli:main"]},
     python_requires=">=3.8.0",
-    install_requires=install_requires,
+    install_requires=list(install_requires),
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
