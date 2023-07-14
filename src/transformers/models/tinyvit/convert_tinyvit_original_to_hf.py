2d8bee30f72fc95b550ffa3fb259a50f07715e4a
Niels <niels.rogge1@gmail.com>
2023-07-14 17:07:12 +0200

    Improve conversion script

--- src/transformers/models/tinyvit/convert_tinyvit_original_to_hf.py
+++ src/transformers/models/tinyvit/convert_tinyvit_original_to_hf.py
@@ -1,6 +1,26 @@
+# coding=utf-8
+# Copyright 2023 The HuggingFace Inc. team.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Convert TinyViT checkpoints from the original repository.
+
+URL: https://github.com/microsoft/Cream/tree/main/TinyViT"""
+
 import argparse
+import json
 
 import torch
+from huggingface_hub import hf_hub_download
 from PIL import Image
 from torchvision.transforms import CenterCrop, Compose, InterpolationMode, Normalize, Resize, ToTensor
 
@@ -10,45 +30,75 @@ from transformers import TinyVitConfig, TinyVitForImageClassification
 def get_tinyvit_config(model_name):
     config = TinyVitConfig()
 
-    if model_name == "tinyvit-21m-224":
+    if "5m" in model_name:
+        hidden_sizes = [64, 128, 160, 320]
+        depths = [2, 2, 6, 2]
+        num_heads = [2, 4, 5, 10]
+        window_sizes = [7, 7, 14, 7]
+    elif "11m" in model_name:
+        hidden_sizes = [64, 128, 256, 448]
+        depths = [2, 2, 6, 2]
+        num_heads = [2, 4, 8, 14]
+        window_sizes = [7, 7, 14, 7]
+    elif "21m" in model_name:
         hidden_sizes = [96, 192, 384, 576]
         depths = [2, 2, 6, 2]
         num_heads = [3, 6, 12, 18]
         window_sizes = [7, 7, 14, 7]
     else:
-        raise NotImplementedError("To do")
+        raise ValueError("Model name should include either 5m, 11m or 21m")
 
-    # if "in22k" in tinyvit_name:
-    #     num_classes = 21841
-    # else:
-    #     num_classes = 1000
-    #     repo_id = "huggingface/label-files"
-    #     filename = "imagenet-1k-id2label.json"
-    #     id2label = json.load(open(hf_hub_download(repo_id, filename, repo_type="dataset"), "r"))
-    #     id2label = {int(k): v for k, v in id2label.items()}
-    #     config.id2label = id2label
-    #     config.label2id = {v: k for k, v in id2label.items()}
+    if model_name.replace("-distill", "").endswith("22k"):
+        num_labels = 21841
+        filename = "imagenet-22k-id2label.json"
+    elif model_name.replace("-distill", "").endswith("1k"):
+        num_labels = 1000
+        filename = "imagenet-1k-id2label.json"
+    else:
+        raise ValueError("Model name should include either 22k or 1k")
 
-    config.num_labels = 1000
+    repo_id = "huggingface/label-files"
+    id2label = json.load(open(hf_hub_download(repo_id, filename, repo_type="dataset"), "r"))
+    id2label = {int(k): v for k, v in id2label.items()}
+
+    if num_labels == 21841:
+        # this dataset contains 21843 labels but the model only has 21841
+        # we delete the classes as mentioned in https://github.com/google-research/big_transfer/issues/18
+        del id2label[9205]
+        del id2label[15027]
+
     config.hidden_sizes = hidden_sizes
     config.depths = depths
     config.num_heads = num_heads
     config.window_sizes = window_sizes
+    config.num_labels = num_labels
+    config.id2label = id2label
+    config.label2id = {v: k for k, v in id2label.items()}
 
     return config
+
+
+name_to_checkpoint_url = {
+    "tinyvit-5m-22k-distill": "https://github.com/wkcn/TinyViT-model-zoo/releases/download/checkpoints/tiny_vit_5m_22k_distill.pth",
+    "tinyvit-5m-22kto1k-distill": "https://github.com/wkcn/TinyViT-model-zoo/releases/download/checkpoints/tiny_vit_5m_22kto1k_distill.pth",
+    "tinyvit-11m-22k-distill": "https://github.com/wkcn/TinyViT-model-zoo/releases/download/checkpoints/tiny_vit_11m_22k_distill.pth",
+    "tinyvit-11m-22kto1k-distill": "https://github.com/wkcn/TinyViT-model-zoo/releases/download/checkpoints/tiny_vit_11m_22kto1k_distill.pth",
+    "tinyvit-21m-22k-distill": "https://github.com/wkcn/TinyViT-model-zoo/releases/download/checkpoints/tiny_vit_21m_22k_distill.pth",
+    "tinyvit-21m-22kto1k-distill": "https://github.com/wkcn/TinyViT-model-zoo/releases/download/checkpoints/tiny_vit_21m_22kto1k_distill.pth",
+    "tinyvit-21m-22kto1k-384-distill": "https://github.com/wkcn/TinyViT-model-zoo/releases/download/checkpoints/tiny_vit_21m_22kto1k_384_distill.pth",
+    "tinyvit-21m-22kto1k-512-distill": "https://github.com/wkcn/TinyViT-model-zoo/releases/download/checkpoints/tiny_vit_21m_22kto1k_512_distill.pth",
+    "tinyvit-5m-1k": "https://github.com/wkcn/TinyViT-model-zoo/releases/download/checkpoints/tiny_vit_5m_1k.pth",
+    "tinyvit-11m-1k": "https://github.com/wkcn/TinyViT-model-zoo/releases/download/checkpoints/tiny_vit_11m_1k.pth",
+    "tinyvit-21m-1k": "https://github.com/wkcn/TinyViT-model-zoo/releases/download/checkpoints/tiny_vit_21m_1k.pth",
+}
 
 
 def convert_tinyvit_checkpoint(model_name, pytorch_dump_folder_path, push_to_hub):
     config = get_tinyvit_config(model_name)
     model = TinyVitForImageClassification(config)
 
-    name_to_checkpoint_path = {
-        "tinyvit-21m-224": "/Users/nielsrogge/Documents/TinyViT/model.pth",
-    }
-
-    checkpoint_path = name_to_checkpoint_path[model_name]
-    state_dict = torch.load(checkpoint_path)
-    # new_state_dict = convert_state_dict(state_dict)
+    checkpoint_url = name_to_checkpoint_url[model_name]
+    state_dict = torch.hub.load_state_dict_from_url(checkpoint_url)["model"]
 
     for key, val in state_dict.copy().items():
         val = state_dict.pop(key)
@@ -91,10 +141,13 @@ def convert_tinyvit_checkpoint(model_name, pytorch_dump_folder_path, push_to_hub
     # image = Image.open(requests.get(url, stream=True).raw)
     # inputs = image_processor(images=image, return_tensors="pt")
 
-    logits = model(pixel_values).logits
+    with torch.no_grad():
+        logits = model(pixel_values).logits
+
     print("First values of logits:", logits[0, :3])
     print("Shape of logits:", logits.shape)
-    print(logits.argmax(-1).item())
+    predicted_class_idx = logits.argmax(-1).item()
+    print("Predicted class:", model.config.id2label[predicted_class_idx])
 
     expected_slice = torch.tensor([0.1848, -0.0826, -0.2939])
     assert torch.allclose(logits[0, :3], expected_slice, atol=1e-4)
@@ -106,7 +159,9 @@ def convert_tinyvit_checkpoint(model_name, pytorch_dump_folder_path, push_to_hub
         # image_processor.save_pretrained(pytorch_dump_folder_path)
 
     if push_to_hub:
-        raise NotImplementedError("To do")
+        print("Pushing model and processor to the 🤗 hub")
+        model.push_to_hub(f"nielsr/{model_name}")
+        # image_processor.push_to_hub(f"nielsr/{model_name}")
 
 
 if __name__ == "__main__":
@@ -114,9 +169,9 @@ if __name__ == "__main__":
     # Required parameters
     parser.add_argument(
         "--model_name",
-        default="tinyvit-21m-224",
+        default="tinyvit-21m-22kto1k-distill",
         type=str,
-        choices=["tinyvit-21m-224", "tinyvit-21m-384", "tinyvit-21m-512"],
+        choices=name_to_checkpoint_url.keys(),
         help="Name of the model.",
     )
     parser.add_argument(
