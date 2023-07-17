4b437b88708a61955b896bcc7883f6ff87cea5b6
Syed Salman Habeeb Quadri <quadrisyedsalmanhabeeb@gmail.com>
2023-07-17 12:17:27 +0000

    Changed incorrect error handling from AssertionError to ValueError

--- src/transformers/models/gpt2/modeling_gpt2.py
+++ src/transformers/models/gpt2/modeling_gpt2.py
@@ -114,7 +114,7 @@ def load_tf_weights_in_gpt2(model, config, gpt2_checkpoint_path):
                 raise ValueError(
                     f"Pointer shape {pointer.shape} and array shape {array.shape} mismatched"
                 )
-        except AssertionError as e:
+        except ValueError as e:
             e.args += (pointer.shape, array.shape)
             raise
         logger.info(f"Initialize PyTorch weight {name}")
