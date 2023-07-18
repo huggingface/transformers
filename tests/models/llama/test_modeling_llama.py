cf2ca90d4424c906f4f1979ef55d027cc2883883
Arthur Zucker <arthur.zucker@gmail.com>
2023-07-18 21:02:17 +0200

    sequential device map

--- tests/models/llama/test_modeling_llama.py
+++ tests/models/llama/test_modeling_llama.py
@@ -438,7 +438,7 @@ class LlamaIntegrationTest(unittest.TestCase):
         prompt = "Simply put, the theory of relativity states that "
         tokenizer = LlamaTokenizer.from_pretrained("meta-llama/Llama-2-13b-chat-hf")
         input_ids = tokenizer.encode(prompt, return_tensors="pt")
-        model = LlamaForCausalLM.from_pretrained("meta-llama/Llama-2-13b-chat-hf", device_map="auto")
+        model = LlamaForCausalLM.from_pretrained("meta-llama/Llama-2-13b-chat-hf", device_map="sequential", use_safetensors=False)
 
         # greedy generation outputs
         generated_ids = model.generate(input_ids, max_new_tokens=64, top_p=None, temperature=1, do_sample=False)
