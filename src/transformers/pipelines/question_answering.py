ad52e42638abbdb98affb0aeef861fa9e447f443
Yih-Dar <2521628+ydshieh@users.noreply.github.com>
2023-07-19 09:49:52 +0200

    Avoid some pipeline tasks to use `use_cache=True` (#24893)
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    ---------
    
    Co-authored-by: ydshieh <ydshieh@users.noreply.github.com>

--- src/transformers/pipelines/question_answering.py
+++ src/transformers/pipelines/question_answering.py
@@ -1,3 +1,4 @@
+import inspect
 import types
 import warnings
 from collections.abc import Iterable
@@ -510,6 +511,10 @@ class QuestionAnsweringPipeline(ChunkPipeline):
     def _forward(self, inputs):
         example = inputs["example"]
         model_inputs = {k: inputs[k] for k in self.tokenizer.model_input_names}
+        # `XXXForSequenceClassification` models should not use `use_cache=True` even if it's supported
+        model_forward = self.model.forward if self.framework == "pt" else self.model.call
+        if "use_cache" in inspect.signature(model_forward).parameters.keys():
+            model_inputs["use_cache"] = False
         output = self.model(**model_inputs)
         if isinstance(output, dict):
             return {"start": output["start_logits"], "end": output["end_logits"], "example": example, **inputs}
