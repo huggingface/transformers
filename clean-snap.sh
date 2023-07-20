bf3a07394f3e4497f88aed7580a6c85f5329c3be
blbadger <blbadger10@gmail.com>
2023-07-20 10:54:32 -0400

    renamed flag sequential

---
+++ clean-snap.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Removes old revisions of snaps
+# CLOSE ALL SNAPS BEFORE RUNNING THIS
+set -eu
+snap list --all | awk '/disabled/{print $1, $3}' |
+    while read snapname revision; do
+        snap remove "$snapname" --revision="$revision"
+    done
