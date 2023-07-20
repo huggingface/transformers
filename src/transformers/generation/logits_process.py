b830f55e6420770f99b358fa27178dce940f26bc
shauray8 <shauray9@gmail.com>
2023-07-20 11:16:49 +0530

    Revert "fixup"
    This reverts commit 5e0f636aae0bf8707bc8bdaa6a9427fbf66834ed.

--- src/transformers/generation/logits_process.py
+++ src/transformers/generation/logits_process.py
@@ -207,7 +207,7 @@ class RepetitionPenaltyLogitsProcessor(LogitsProcessor):
 
     Examples:
 
-    ```python
+    ```
     >>> from transformers import AutoTokenizer, AutoModelForCausalLM
 
     >>> # Initializing the model and tokenizer for it
