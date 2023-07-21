82aa0ee498842065f0c3daac6f78e5718d582681
Dong-Yong Lee <pfldy2850@gmail.com>
2023-07-21 19:08:10 +0900

    Fix beam search idx device when using model parallel

--- src/transformers/models/blenderbot_small/modeling_blenderbot_small.py
+++ src/transformers/models/blenderbot_small/modeling_blenderbot_small.py
@@ -1378,7 +1378,8 @@ class BlenderbotSmallForConditionalGeneration(BlenderbotSmallPreTrainedModel):
         for layer_past in past_key_values:
             # cached cross_attention states don't have to be reordered -> they are always the same
             reordered_past += (
-                tuple(past_state.index_select(0, beam_idx) for past_state in layer_past[:2]) + layer_past[2:],
+                tuple(past_state.index_select(0, beam_idx.to(past_state.device)) for past_state in layer_past[:2])
+                + layer_past[2:],
             )
         return reordered_past
 
@@ -1600,5 +1601,7 @@ class BlenderbotSmallForCausalLM(BlenderbotSmallPreTrainedModel):
     def _reorder_cache(past_key_values, beam_idx):
         reordered_past = ()
         for layer_past in past_key_values:
-            reordered_past += (tuple(past_state.index_select(0, beam_idx) for past_state in layer_past),)
+            reordered_past += (
+                tuple(past_state.index_select(0, beam_idx.to(past_state.device)) for past_state in layer_past),
+            )
         return reordered_past
