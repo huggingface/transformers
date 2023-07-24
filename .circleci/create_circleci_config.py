2e8f2a6432ceae372a8773c7f31b5b4140db6cf2
Quentin Lhoest <lhoest.q@gmail.com>
2023-07-24 12:16:00 +0200

    test datasets@main

--- .circleci/create_circleci_config.py
+++ .circleci/create_circleci_config.py
@@ -145,6 +145,7 @@ class CircleCIJob:
                 }
             }
         )
+        steps.append({"run": {"name": "Install `datasets@main`", "command": 'pip uninstall datasets -y && pip install "datasets @ git+https://github.com/huggingface/datasets@main#egg=datasets"'}})
         steps.append({"run": {"name": "Show installed libraries and their versions", "command": "pip freeze | tee installed.txt"}})
         steps.append({"store_artifacts": {"path": "~/transformers/installed.txt"}})
 
