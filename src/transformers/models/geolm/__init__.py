6ef46bbb1b6209c0b396391f114fd2ca37dbecf8
Zekun <li002666@umn.edu>
2023-07-24 18:33:16 +0000

    toponym

--- src/transformers/models/geolm/__init__.py
+++ src/transformers/models/geolm/__init__.py
@@ -18,7 +18,7 @@ from ...utils import (
     OptionalDependencyNotAvailable,
     _LazyModule,
     is_flax_available,
-    is_tensorflow_text_available,
+    is_tensorflow_text_available,   
     is_tf_available,
     is_torch_available,
 )
@@ -151,7 +151,6 @@ if TYPE_CHECKING:
     # except OptionalDependencyNotAvailable:
     #     pass
     # else:
-
     try:
         if not is_flax_available():
             raise OptionalDependencyNotAvailable()
