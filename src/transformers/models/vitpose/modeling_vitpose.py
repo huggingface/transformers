66b8d4a6b4bcc53d96d98a0cb3c4dfb2aa57726f
shauray8 <shauray9@gmail.com>
2023-07-25 15:56:02 +0530

    architecture kinda

--- src/transformers/models/vitpose/modeling_vitpose.py
+++ src/transformers/models/vitpose/modeling_vitpose.py
@@ -198,93 +198,145 @@ class ViTPoseBlock(nn.Module):
     pass
 
 
-a = ViTPoseBlock(config)
+class ViTBackbone(nn.Module):
+    """
+    Construct the CLS token, position and patch embeddings. Optionally, also the mask token.
+    """
+
+    def __init__(self, config: ViTPoseConfig, use_mask_token: bool = False) -> None:
+        super().__init__()
+
+        self.cls_token = nn.Parameter(torch.randn(1, 1, config.hidden_size))
+        self.mask_token = nn.Parameter(torch.zeros(1, 1, config.hidden_size)) if use_mask_token else None
+        self.patch_embeddings = ViTPatchEmbeddings(config)
+        num_patches = self.patch_embeddings.num_patches
+        self.position_embeddings = nn.Parameter(torch.randn(1, num_patches + 1, config.hidden_size))
+        self.dropout = nn.Dropout(config.hidden_dropout_prob)
+        self.config = config
+
+    def interpolate_pos_encoding(self, embeddings: torch.Tensor, height: int, width: int) -> torch.Tensor:
+        """
+        This method allows to interpolate the pre-trained position encodings, to be able to use the model on higher
+        resolution images.
+
+        Source:
+        https://github.com/facebookresearch/dino/blob/de9ee3df6cf39fac952ab558447af1fa1365362a/vision_transformer.py#L174
+        """
+
+        num_patches = embeddings.shape[1] - 1
+        num_positions = self.position_embeddings.shape[1] - 1
+        if num_patches == num_positions and height == width:
+            return self.position_embeddings
+        class_pos_embed = self.position_embeddings[:, 0]
+        patch_pos_embed = self.position_embeddings[:, 1:]
+        dim = embeddings.shape[-1]
+        h0 = height // self.config.patch_size
+        w0 = width // self.config.patch_size
+        # we add a small number to avoid floating point error in the interpolation
+        # see discussion at https://github.com/facebookresearch/dino/issues/8
+        h0, w0 = h0 + 0.1, w0 + 0.1
+        patch_pos_embed = patch_pos_embed.reshape(1, int(math.sqrt(num_positions)), int(math.sqrt(num_positions)), dim)
+        patch_pos_embed = patch_pos_embed.permute(0, 3, 1, 2)
+        patch_pos_embed = nn.functional.interpolate(
+            patch_pos_embed,
+            scale_factor=(h0 / math.sqrt(num_positions), w0 / math.sqrt(num_positions)),
+            mode="bicubic",
+            align_corners=False,
+        )
+        assert int(h0) == patch_pos_embed.shape[-2] and int(w0) == patch_pos_embed.shape[-1]
+        patch_pos_embed = patch_pos_embed.permute(0, 2, 3, 1).view(1, -1, dim)
+        return torch.cat((class_pos_embed.unsqueeze(0), patch_pos_embed), dim=1)
+
+    def forward(
+        self,
+        pixel_values: torch.Tensor,
+        bool_masked_pos: Optional[torch.BoolTensor] = None,
+        interpolate_pos_encoding: bool = False,
+    ) -> torch.Tensor:
+        batch_size, num_channels, height, width = pixel_values.shape
+        embeddings = self.patch_embeddings(pixel_values, interpolate_pos_encoding=interpolate_pos_encoding)
+
+        if bool_masked_pos is not None:
+            seq_length = embeddings.shape[1]
+            mask_tokens = self.mask_token.expand(batch_size, seq_length, -1)
+            # replace the masked visual tokens by mask_tokens
+            mask = bool_masked_pos.unsqueeze(-1).type_as(mask_tokens)
+            embeddings = embeddings * (1.0 - mask) + mask_tokens * mask
+
+        # add the [CLS] token to the embedded patch tokens
+        cls_tokens = self.cls_token.expand(batch_size, -1, -1)
+        embeddings = torch.cat((cls_tokens, embeddings), dim=1)
+
+        # add positional encoding to each token
+        if interpolate_pos_encoding:
+            embeddings = embeddings + self.interpolate_pos_encoding(embeddings, height, width)
+        else:
+            embeddings = embeddings + self.position_embeddings
+
+        embeddings = self.dropout(embeddings)
+
+        return embeddings
+
+
+class ViTBackbone(nn.Module):
+    def __init__(self, ):
+        super().__init__()
+        num_patches = (img_size // patch_size) ** 2
+        patch_dim = in_channels * (patch_size ** 2)
+        self.patch_embed = PatchEmbed(in_channels, embed_dim, patch_size)
+        self.cls_token = nn.Parameter(torch.zeros(1, 1, embed_dim))
+        self.pos_embed = nn.Parameter(torch.zeros(1, num_patches + 1, embed_dim))
+        self.dropout = nn.Dropout(dropout)
+        self.blocks = nn.ModuleList([Block(embed_dim, num_heads, mlp_ratio, dropout) for _ in range(depth)])
+        self.norm = nn.LayerNorm(embed_dim)
+
+    def forward(self, x):
+        B = x.shape[0]
+        x = self.patch_embed(x)
+        cls_token = self.cls_token.expand(B, -1, -1)
+        x = torch.cat((cls_token, x), dim=1)
+        x = x + self.pos_embed
+        x = self.dropout(x)
+
+        for block in self.blocks:
+            x = block(x)
+
+        x = self.norm(x)
+        return x
 
 
 class ViTPoseTopDownHeatMap(nn.Module):
     # keypoint head - mse loss]
     ## deconv layers and all the other remaining things with the final layer
-    pass
+    def __init__(self, embed_dim=hidden_dim, num_keypoints=17):
+        super().__init__()
+        self.deconv_layers = nn.Sequential(
+            nn.ConvTranspose2d(embed_dim, 256, kernel_size, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+            nn.ConvTranspose2d(256, 256, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+        )
+        self.final_layer = nn.Conv2d(256, num_keypoints, kernel_siz=1, stride=1)
+
+    def forward(self, x):
+        x = self.deconv_layers(x)
+        keypoints = self.final_layer(x)
+        return keypoints
+
+class TopDown(nn.Module):
+    def __init__(self, in_channels=3, img_size=224, patch_size=1, ):
+        super().__init__()
+        self.backbone = ViTBackbone(in_channels)
+        self.keypoint_head = ViTPoseTopDownHeatMap(embed_dim, num_keypoints)
+
+    def forward(self, x):
+        x = self.backbone(x)
+        keypoints = self.keypoint_head(x)
+        return keypoints
 
 #]
-#class ViTBackbone(nn.Module):
-#    """
-#    Construct the CLS token, position and patch embeddings. Optionally, also the mask token.
-#    """
-#
-#    def __init__(self, config: ViTPoseConfig, use_mask_token: bool = False) -> None:
-#        super().__init__()
-#
-#        self.cls_token = nn.Parameter(torch.randn(1, 1, config.hidden_size))
-#        self.mask_token = nn.Parameter(torch.zeros(1, 1, config.hidden_size)) if use_mask_token else None
-#        self.patch_embeddings = ViTPatchEmbeddings(config)
-#        num_patches = self.patch_embeddings.num_patches
-#        self.position_embeddings = nn.Parameter(torch.randn(1, num_patches + 1, config.hidden_size))
-#        self.dropout = nn.Dropout(config.hidden_dropout_prob)
-#        self.config = config
-#
-#    def interpolate_pos_encoding(self, embeddings: torch.Tensor, height: int, width: int) -> torch.Tensor:
-#        """
-#        This method allows to interpolate the pre-trained position encodings, to be able to use the model on higher
-#        resolution images.
-#
-#        Source:
-#        https://github.com/facebookresearch/dino/blob/de9ee3df6cf39fac952ab558447af1fa1365362a/vision_transformer.py#L174
-#        """
-#
-#        num_patches = embeddings.shape[1] - 1
-#        num_positions = self.position_embeddings.shape[1] - 1
-#        if num_patches == num_positions and height == width:
-#            return self.position_embeddings
-#        class_pos_embed = self.position_embeddings[:, 0]
-#        patch_pos_embed = self.position_embeddings[:, 1:]
-#        dim = embeddings.shape[-1]
-#        h0 = height // self.config.patch_size
-#        w0 = width // self.config.patch_size
-#        # we add a small number to avoid floating point error in the interpolation
-#        # see discussion at https://github.com/facebookresearch/dino/issues/8
-#        h0, w0 = h0 + 0.1, w0 + 0.1
-#        patch_pos_embed = patch_pos_embed.reshape(1, int(math.sqrt(num_positions)), int(math.sqrt(num_positions)), dim)
-#        patch_pos_embed = patch_pos_embed.permute(0, 3, 1, 2)
-#        patch_pos_embed = nn.functional.interpolate(
-#            patch_pos_embed,
-#            scale_factor=(h0 / math.sqrt(num_positions), w0 / math.sqrt(num_positions)),
-#            mode="bicubic",
-#            align_corners=False,
-#        )
-#        assert int(h0) == patch_pos_embed.shape[-2] and int(w0) == patch_pos_embed.shape[-1]
-#        patch_pos_embed = patch_pos_embed.permute(0, 2, 3, 1).view(1, -1, dim)
-#        return torch.cat((class_pos_embed.unsqueeze(0), patch_pos_embed), dim=1)
-#
-#    def forward(
-#        self,
-#        pixel_values: torch.Tensor,
-#        bool_masked_pos: Optional[torch.BoolTensor] = None,
-#        interpolate_pos_encoding: bool = False,
-#    ) -> torch.Tensor:
-#        batch_size, num_channels, height, width = pixel_values.shape
-#        embeddings = self.patch_embeddings(pixel_values, interpolate_pos_encoding=interpolate_pos_encoding)
-#
-#        if bool_masked_pos is not None:
-#            seq_length = embeddings.shape[1]
-#            mask_tokens = self.mask_token.expand(batch_size, seq_length, -1)
-#            # replace the masked visual tokens by mask_tokens
-#            mask = bool_masked_pos.unsqueeze(-1).type_as(mask_tokens)
-#            embeddings = embeddings * (1.0 - mask) + mask_tokens * mask
-#
-#        # add the [CLS] token to the embedded patch tokens
-#        cls_tokens = self.cls_token.expand(batch_size, -1, -1)
-#        embeddings = torch.cat((cls_tokens, embeddings), dim=1)
-#
-#        # add positional encoding to each token
-#        if interpolate_pos_encoding:
-#            embeddings = embeddings + self.interpolate_pos_encoding(embeddings, height, width)
-#        else:
-#            embeddings = embeddings + self.position_embeddings
-#
-#        embeddings = self.dropout(embeddings)
-#
-#        return embeddings
 #
 #class ViTLayer(nn.Module):
 #    """This corresponds to the Block class in the timm implementation."""
