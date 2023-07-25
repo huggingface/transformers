4873206da3c4b6291675e8baccae41df5e8ace72
Stas Bekman <stas@stason.org>
2023-07-25 09:11:35 -0700

    cleanup

--- src/transformers/models/idefics/image_processing_idefics.py
+++ src/transformers/models/idefics/image_processing_idefics.py
@@ -14,19 +14,14 @@
 # limitations under the License.
 """Image processor class for Idefics."""
 
-from typing import Any, Dict, Iterable, List, Optional, Tuple, Union
+from typing import Dict, List, Optional, Union
 
-import numpy as np
-
-from ...image_processing_utils import BaseImageProcessor, BatchFeature
-from ...image_transforms import PaddingMode, pad
+from ...image_processing_utils import BaseImageProcessor
 from ...image_utils import (
     IMAGENET_STANDARD_MEAN,
     IMAGENET_STANDARD_STD,
     ChannelDimension,
     ImageInput,
-    get_image_size,
-    infer_channel_dimension_format,
     make_list_of_images,
     valid_images,
 )
@@ -41,28 +36,6 @@ if is_vision_available():
     from PIL import Image
 
 logger = logging.get_logger(__name__)
-
-
-def max_across_indices(values: Iterable[Any]) -> List[Any]:
-    """
-    Return the maximum value across all indices of an iterable of values.
-    """
-    return [max(values_i) for values_i in zip(*values)]
-
-
-def get_max_height_width(images: List[np.ndarray]) -> List[int]:
-    """
-    Get the maximum height and width across all images in a batch.
-    """
-    input_channel_dimension = infer_channel_dimension_format(images[0])
-
-    if input_channel_dimension == ChannelDimension.FIRST:
-        _, max_height, max_width = max_across_indices([img.shape for img in images])
-    elif input_channel_dimension == ChannelDimension.LAST:
-        max_height, max_width, _ = max_across_indices([img.shape for img in images])
-    else:
-        raise ValueError(f"Invalid channel dimension format: {input_channel_dimension}")
-    return (max_height, max_width)
 
 
 def convert_to_rgb_transform(image):
@@ -93,9 +66,6 @@ class IdeficsImageProcessor(BaseImageProcessor):
             Standard deviation to use if normalizing the image. This is a float or list of floats the length of the
             number of channels in the image. Can be overridden by the `image_std` parameter in the `preprocess` method.
             Can be overridden by the `image_std` parameter in the `preprocess` method.
-        do_pad (`bool`, *optional*, defaults to `True`):
-            Whether to pad the image to the `(max_height, max_width)` of the images in the batch. Can be overridden by
-            the `do_pad` parameter in the `preprocess` method.
         eval_mode (`bool`, *optional*, defaults to `True`):
             Whether to prepare the image for evaluation or training. If `eval_mode` is `True` resize will be performed,
             otherwise a `RandomResizeCrop`
@@ -110,7 +80,6 @@ class IdeficsImageProcessor(BaseImageProcessor):
         image_size: int = 224,
         image_mean: Optional[Union[float, List[float]]] = IMAGENET_STANDARD_MEAN,
         image_std: Optional[Union[float, List[float]]] = IMAGENET_STANDARD_STD,
-        do_pad: bool = True,
         eval_mode: bool = True,
         **kwargs,
     ) -> None:
@@ -119,61 +88,6 @@ class IdeficsImageProcessor(BaseImageProcessor):
         self.image_size = image_size
         self.image_mean = image_mean
         self.image_std = image_std
-        self.do_pad = do_pad
-
-        print(f"{self.image_size=}")
-
-    def _pad_image(
-        self,
-        image: np.ndarray,
-        output_size: Tuple[int, int],
-        constant_values: Union[float, Iterable[float]] = 0,
-        data_format: Optional[ChannelDimension] = None,
-    ) -> np.ndarray:
-        """
-        Pad an image with zeros to the given size.
-        """
-        input_height, input_width = get_image_size(image)
-        output_height, output_width = output_size
-
-        pad_bottom = output_height - input_height
-        pad_right = output_width - input_width
-        padding = ((0, pad_bottom), (0, pad_right))
-        padded_image = pad(
-            image, padding, mode=PaddingMode.CONSTANT, constant_values=constant_values, data_format=data_format
-        )
-        return padded_image
-
-    def pad(
-        self,
-        images: List[np.ndarray],
-        return_tensors: Optional[Union[str, TensorType]] = None,
-        data_format: Optional[ChannelDimension] = None,
-    ) -> BatchFeature:
-        """
-        Pads a batch of images with zeros to the size of largest height and width in the batch and optionally returns
-        their corresponding pixel mask.
-
-        Args:
-            images (`List[np.ndarray]`):
-                Batch of images to pad.
-            return_tensors (`str` or `TensorType`, *optional*):
-                The type of tensors to return. Can be one of:
-                    - Unset: Return a list of `np.ndarray`.
-                    - `TensorType.TENSORFLOW` or `'tf'`: Return a batch of type `tf.Tensor`.
-                    - `TensorType.PYTORCH` or `'pt'`: Return a batch of type `torch.Tensor`.
-                    - `TensorType.NUMPY` or `'np'`: Return a batch of type `np.ndarray`.
-                    - `TensorType.JAX` or `'jax'`: Return a batch of type `jax.numpy.ndarray`.
-            data_format (`str` or `ChannelDimension`, *optional*):
-                The channel dimension format of the image. If not provided, it will be the same as the input image.
-        """
-        pad_size = get_max_height_width(images)
-        padded_images = [
-            self._pad_image(image=image, output_size=pad_size, data_format=data_format) for image in images
-        ]
-        data = {"pixel_values": padded_images}
-
-        return BatchFeature(data=data, tensor_type=return_tensors)
 
     def preprocess(
         self,
@@ -181,7 +95,6 @@ class IdeficsImageProcessor(BaseImageProcessor):
         image_size: Optional[Dict[str, int]] = None,
         image_mean: Optional[Union[float, List[float]]] = None,
         image_std: Optional[Union[float, List[float]]] = None,
-        do_pad: Optional[bool] = None,
         eval_mode: Optional[bool] = True,
         return_tensors: Optional[Union[str, TensorType]] = None,
         data_format: ChannelDimension = ChannelDimension.FIRST,
@@ -199,9 +112,6 @@ class IdeficsImageProcessor(BaseImageProcessor):
                 Image mean to normalize the image by if `do_normalize` is set to `True`.
             image_std (`float` or `List[float]`, *optional*, defaults to `self.image_std`):
                 Image standard deviation to normalize the image by if `do_normalize` is set to `True`.
-            do_pad (`bool`, *optional*, defaults to `self.do_pad`):
-                Whether to pad the image to the (max_height, max_width) in the batch. If `True`, a pixel mask is also
-                created and returned.
             eval_mode (`bool`, *optional*, defaults to `self.eval_mode`):
                 Whether to prepare the image for evaluation or training. If `eval_mode` is `True` resize will be
                 performed, otherwise a `RandomResizeCrop`
@@ -220,11 +130,6 @@ class IdeficsImageProcessor(BaseImageProcessor):
         image_size = image_size if image_size is not None else self.image_size
         image_mean = image_mean if image_mean is not None else self.image_mean
         image_std = image_std if image_std is not None else self.image_std
-        do_pad = do_pad if do_pad is not None else self.do_pad
-
-        # print(f"{image_size}")
-        # print(f"{image_mean}")
-        # die
 
         images = make_list_of_images(images)
 
@@ -255,16 +160,4 @@ class IdeficsImageProcessor(BaseImageProcessor):
 
         images = [image_transforms(image) for image in images if image is not None]
 
-        # XXX: self.pad() call was removed! is it on the other side?
-
         return images
-
-        # encoded_outputs = self.pad(images)#, return_tensors=return_tensors)
-        # return encoded_outputs
-
-        # if do_pad:
-        #     encoded_outputs = self.pad(images, return_tensors=return_tensors)
-        # else:
-        #     encoded_outputs = BatchFeature(data={"pixel_values": images}, tensor_type=return_tensors)
-
-        # return encoded_outputs
