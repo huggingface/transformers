255258f0a4cc4fb71760c25be93598bf970a3ba4
Arthur Zucker <arthur.zucker@gmail.com>
2023-07-25 17:03:20 +0200

    apply changes to other models

--- src/transformers/models/gptj/modeling_tf_gptj.py
+++ src/transformers/models/gptj/modeling_tf_gptj.py
@@ -866,8 +866,11 @@ class TFGPTJForSequenceClassification(TFGPTJPreTrainedModel, TFSequenceClassific
             sequence_lengths = -1
         else:
             if input_ids is not None:
-                sequence_lengths = tf.argmax(tf.cast(tf.math.equal(input_ids, self.config.pad_token_id), input_ids.dtype), axis=-1) - 1
-                sequence_lengths = tf.where(sequence_lengths >= 0, sequence_lengths, input_ids.shape[-1]-1)
+                sequence_lengths = (
+                    tf.argmax(tf.cast(tf.math.equal(input_ids, self.config.pad_token_id), input_ids.dtype), axis=-1)
+                    - 1
+                )
+                sequence_lengths = tf.where(sequence_lengths >= 0, sequence_lengths, input_ids.shape[-1] - 1)
                 in_logits = tf.gather(logits, sequence_lengths, batch_dims=1, axis=1)
             else:
                 sequence_lengths = -1
