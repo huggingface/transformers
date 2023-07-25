10bb20262c4efe89fa4100ec2f3f316a2448ffca
ydshieh <ydshieh@users.noreply.github.com>
2023-07-25 11:53:52 +0200

    update

--- src/transformers/models/kosmos2/tokenization_kosmos2_fast.py
+++ src/transformers/models/kosmos2/tokenization_kosmos2_fast.py
@@ -12,7 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License
-""" Tokenization classes for XLM-RoBERTa model."""
+""" Tokenization classes for Kosmos-2 model."""
 
 
 import os
