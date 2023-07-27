c4c6125b39104a122ed24e84cb4be461496a059d
Amy Roberts <22614925+amyeroberts@users.noreply.github.com>
2023-07-27 21:21:18 +0100

    PR comments

--- src/transformers/models/efficientnet/image_processing_efficientnet.py
+++ src/transformers/models/efficientnet/image_processing_efficientnet.py
@@ -147,9 +147,8 @@ class EfficientNetImageProcessor(BaseImageProcessor):
         size = get_size_dict(size)
         if "height" not in size or "width" not in size:
             raise ValueError(f"The `size` dictionary must contain the keys `height` and `width`. Got {size.keys()}")
-        return resize(
-            image, size=(size["height"], size["width"]), resample=resample, data_format=data_format, **kwargs
-        )
+        output_size = (size["height"], size["width"])
+        return resize(image, size=output_size, resample=resample, data_format=data_format, **kwargs)
 
     def rescale(
         self,
@@ -174,7 +173,7 @@ class EfficientNetImageProcessor(BaseImageProcessor):
                 Image to rescale.
             scale (`int` or `float`):
                 Scale to apply to the image.
-           offset (`bool`, *optional*):
+            offset (`bool`, *optional*):
                 Whether to scale the image in both negative and positive directions.
             data_format (`str` or `ChannelDimension`, *optional*):
                 The channel dimension format of the image. If not provided, it will be the same as the input image.
