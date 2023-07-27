09e411e16ab6c9073d9ebca550768e20d3d60b12
Younes Belkada <49240599+younesbelkada@users.noreply.github.com>
2023-07-27 18:18:03 +0200

    Update src/transformers/models/idefics/clip.py
    Co-authored-by: Stas Bekman <stas00@users.noreply.github.com>

--- src/transformers/models/idefics/clip.py
+++ src/transformers/models/idefics/clip.py
@@ -12,7 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-""" PyTorch IdeficsVision model - copy of CLIPVisionModel"""
+""" PyTorch IdeficsVision model - a copy of CLIPVisionModel using a new config object """
 
 
 from dataclasses import dataclass
