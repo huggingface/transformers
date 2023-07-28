6c44e6c590fbbd0a497b101a4cd766ac56fa3793
PETRASTORE <104895418+PETRASTORE@users.noreply.github.com>
2023-07-28 07:46:52 +0300

    Update configuration_glpn.py

--- src/transformers/models/glpn/configuration_glpn.py
+++ src/transformers/models/glpn/configuration_glpn.py
@@ -1,33 +1,4 @@
-# coding=utf-8
-# Copyright 2022 KAIST and The HuggingFace Inc. team. All rights reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-""" GLPN model configuration"""
 
-from ...configuration_utils import PretrainedConfig
-from ...utils import logging
-
-
-logger = logging.get_logger(__name__)
-
-GLPN_PRETRAINED_CONFIG_ARCHIVE_MAP = {
-    "vinvino02/glpn-kitti": "https://huggingface.co/vinvino02/glpn-kitti/resolve/main/config.json",
-    # See all GLPN models at https://huggingface.co/models?filter=glpn
-}
-
-
-class GLPNConfig(PretrainedConfig):
-    r"""
     This is the configuration class to store the configuration of a [`GLPNModel`]. It is used to instantiate an GLPN
     model according to the specified arguments, defining the model architecture. Instantiating a configuration with the
     defaults will yield a similar configuration to that of the GLPN
