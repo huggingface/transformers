823e44eadfdd4c78f3785d579d8e5432f0594a3e
ydshieh <ydshieh@users.noreply.github.com>
2023-07-29 10:44:19 +0200

    import Kosmos2PreTrainedModel in script

--- src/transformers/models/kosmos2/kosmos-2.py
+++ src/transformers/models/kosmos2/kosmos-2.py
@@ -49,67 +49,7 @@ from transformers.models.kosmos2.modeling_kosmos2 import Kosmos2VisionTransforme
 # Model class
 
 
-# Copied from transformers.models.bart.modeling_bart._expand_mask
-def _expand_mask(mask: torch.Tensor, dtype: torch.dtype, tgt_len: Optional[int] = None):
-    """
-    Expands attention_mask from `[bsz, seq_len]` to `[bsz, 1, tgt_seq_len, src_seq_len]`.
-    """
-    bsz, src_len = mask.size()
-    tgt_len = tgt_len if tgt_len is not None else src_len
-
-    expanded_mask = mask[:, None, None, :].expand(bsz, 1, tgt_len, src_len).to(dtype)
-
-    inverted_mask = 1.0 - expanded_mask
-
-    return inverted_mask.masked_fill(inverted_mask.to(torch.bool), torch.finfo(dtype).min)
-
-
-# Copied from transformers.models.bart.modeling_bart._make_causal_mask
-def _make_causal_mask(
-    input_ids_shape: torch.Size, dtype: torch.dtype, device: torch.device, past_key_values_length: int = 0
-):
-    """
-    Make causal mask used for bi-directional self-attention.
-    """
-    bsz, tgt_len = input_ids_shape
-    mask = torch.full((tgt_len, tgt_len), torch.tensor(torch.finfo(dtype).min, device=device), device=device)
-    mask_cond = torch.arange(mask.size(-1), device=device)
-    mask.masked_fill_(mask_cond < (mask_cond + 1).view(mask.size(-1), 1), 0)
-    mask = mask.to(dtype)
-
-    if past_key_values_length > 0:
-        mask = torch.cat([torch.zeros(tgt_len, past_key_values_length, dtype=dtype, device=device), mask], dim=-1)
-    return mask[None, None, :, :].expand(bsz, 1, tgt_len, tgt_len + past_key_values_length)
-
-
-# Copied from transformers.models.roberta.modeling_roberta.create_position_ids_from_input_ids
-def create_position_ids_from_input_ids(input_ids, padding_idx, past_key_values_length=0):
-    """
-    Replace non-padding symbols with their position numbers. Position numbers begin at padding_idx+1. Padding symbols
-    are ignored. This is modified from fairseq's `utils.make_positions`.
-
-    Args:
-        x: torch.Tensor x:
-
-    Returns: torch.Tensor
-    """
-    # The series of casts and type-conversions here are carefully balanced to both work with ONNX export and XLA.
-    mask = input_ids.ne(padding_idx).int()
-    incremental_indices = (torch.cumsum(mask, dim=1).type_as(mask) + past_key_values_length) * mask
-    return incremental_indices.long() + padding_idx
-
-
-
-from transformers.models.kosmos2.modeling_kosmos2 import Kosmos2TextSinusoidalPositionalEmbedding, KosmosTextAttention, Kosmos2TextFFN, Kosmos2TextBlock, Kosmos2TextTransformer
-
-
-class Kosmos2PreTrainedModel(PreTrainedModel):
-    """
-    An abstract class to handle weights initialization and a simple interface for downloading and loading pretrained
-    models.
-    """
-    config_class = Kosmos2Config
-    # base_model_prefix = "kosmos_2_text"
+from transformers.models.kosmos2.modeling_kosmos2 import Kosmos2TextSinusoidalPositionalEmbedding, KosmosTextAttention, Kosmos2TextFFN, Kosmos2TextBlock, Kosmos2TextTransformer, Kosmos2PreTrainedModel
 
 
 class Kosmos2TextModel(Kosmos2PreTrainedModel):
