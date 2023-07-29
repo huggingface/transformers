69881b80c84decb73826f283e8f62f8931f872c6
ydshieh <ydshieh@users.noreply.github.com>
2023-07-29 12:33:16 +0200

    remove Kosmos2VisionModel in script

--- src/transformers/models/kosmos2/kosmos-2.py
+++ src/transformers/models/kosmos2/kosmos-2.py
@@ -175,69 +175,6 @@ class Kosmos2TextForCausalLM(Kosmos2PreTrainedModel):
             "use_cache": use_cache,
         }
 
-
-# ==============================================================================================================
-# Vision model class
-# (completely copied from CLIPVision)
-
-@add_start_docstrings(
-    """The vision model from KOSMOS2 without any head or projection on top.""",
-    KOSMOS2_START_DOCSTRING,
-)
-class Kosmos2VisionModel(Kosmos2PreTrainedModel):
-    config_class = Kosmos2VisionConfig
-    main_input_name = "pixel_values"
-
-    def __init__(self, config: Kosmos2VisionConfig):
-        super().__init__(config)
-        self.vision_model = Kosmos2VisionTransformer(config)
-        # Initialize weights and apply final processing
-        self.post_init()
-
-    def get_input_embeddings(self) -> nn.Module:
-        return self.vision_model.embeddings.patch_embedding
-
-    @add_start_docstrings_to_model_forward(KOSMOS2_VISION_INPUTS_DOCSTRING)
-    @replace_return_docstrings(output_type=BaseModelOutputWithPooling, config_class=Kosmos2VisionConfig)
-    def forward(
-        self,
-        pixel_values: Optional[torch.FloatTensor] = None,
-        output_attentions: Optional[bool] = None,
-        output_hidden_states: Optional[bool] = None,
-        return_dict: Optional[bool] = None,
-    ) -> Union[Tuple, BaseModelOutputWithPooling]:
-        r"""
-        Returns:
-
-        Examples:
-
-        ```python
-        >>> from PIL import Image
-        >>> import requests
-        >>> from transformers import AutoProcessor, Kosmos2VisionModel
-
-        >>> model = Kosmos2VisionModel.from_pretrained("openai/clip-vit-base-patch32")
-        >>> processor = AutoProcessor.from_pretrained("openai/clip-vit-base-patch32")
-
-        >>> url = "http://images.cocodataset.org/val2017/000000039769.jpg"
-        >>> image = Image.open(requests.get(url, stream=True).raw)
-
-        >>> inputs = processor(images=image, return_tensors="pt")
-
-        >>> outputs = model(**inputs)
-        >>> last_hidden_state = outputs.last_hidden_state
-        >>> pooled_output = outputs.pooler_output  # pooled CLS states
-        ```"""
-        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
-
-        return self.vision_model(
-            pixel_values=pixel_values,
-            output_attentions=output_attentions,
-            output_hidden_states=output_hidden_states,
-            return_dict=return_dict,
-        )
-
-
 # ==============================================================================================================
 
 
