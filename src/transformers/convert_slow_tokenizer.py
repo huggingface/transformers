3c5d3f76662c33d89267ed28d85ed432fe2bbd06
ydshieh <ydshieh@users.noreply.github.com>
2023-07-30 20:09:47 +0200

    fix

--- src/transformers/convert_slow_tokenizer.py
+++ src/transformers/convert_slow_tokenizer.py
@@ -796,7 +796,7 @@ class Kosmos2Converter(SpmConverter):
             ("</phrase>", 0.0),
             ("<object>", 0.0),
             ("</object>", 0.0),
-            ("</delimiter_of_multi_objects>", 0.0),
+            ("</delimiter_of_multi_objects/>", 0.0),
             ("<grounding>", 0.0),
         ]
         # fmt: on
