6c1af43598dc15ceee4bed0fb2cadfbbbcf8f931
younesbelkada <younesbelkada@gmail.com>
2023-07-31 15:24:28 +0000

    let's raise an error.

--- src/transformers/pipelines/base.py
+++ src/transformers/pipelines/base.py
@@ -778,13 +778,14 @@ class Pipeline(_ScikitCompat):
         # `accelerate` device map
         hf_device_map = getattr(self.model, "hf_device_map", None)
 
+        if hf_device_map is not None and device is not None:
+            raise ValueError(
+                "The model has been loaded with `accelerate` and therefore cannot be moved to a specific device. Please "
+                "discard the `device` argument when creating your pipeline object."
+            )
+
         # We shouldn't call `model.to()` for models loaded with accelerate
-        if (
-            self.framework == "pt"
-            and device is not None
-            and not (isinstance(device, int) and device < 0)
-            and hf_device_map is None
-        ):
+        if self.framework == "pt" and device is not None and not (isinstance(device, int) and device < 0):
             self.model.to(device)
 
         if device is None:
