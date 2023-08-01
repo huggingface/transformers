53ed7d20c4bf5f599d2b73b5001981172b5fa1cf
shauray8 <shauray9@gmail.com>
2023-08-01 17:03:18 +0530

    prev

--- src/transformers/models/vitpose/modeling_vitpose.py
+++ src/transformers/models/vitpose/modeling_vitpose.py
@@ -411,92 +411,6 @@ VIT_INPUTS_DOCSTRING = r"""
 )
 
 
-#class ViTForImageClassification(ViTPreTrainedModel):
-#    def __init__(self, config: ViTConfig) -> None:
-#        super().__init__(config)
-#
-#        self.num_labels = config.num_labels
-#        self.vit = ViTModel(config, add_pooling_layer=False)
-#
-#        # Classifier head
-#        self.classifier = nn.Linear(config.hidden_size, config.num_labels) if config.num_labels > 0 else nn.Identity()
-#
-#        # Initialize weights and apply final processing
-#        self.post_init()
-#
-#    @add_start_docstrings_to_model_forward(VIT_INPUTS_DOCSTRING)
-#    @add_code_sample_docstrings(
-#        checkpoint=_IMAGE_CLASS_CHECKPOINT,
-#        output_type=ImageClassifierOutput,
-#        config_class=_CONFIG_FOR_DOC,
-#        expected_output=_IMAGE_CLASS_EXPECTED_OUTPUT,
-#    )
-#    def forward(
-#        self,
-#        pixel_values: Optional[torch.Tensor] = None,
-#        head_mask: Optional[torch.Tensor] = None,
-#        labels: Optional[torch.Tensor] = None,
-#        output_attentions: Optional[bool] = None,
-#        output_hidden_states: Optional[bool] = None,
-#        interpolate_pos_encoding: Optional[bool] = None,
-#        return_dict: Optional[bool] = None,
-#    ) -> Union[tuple, ImageClassifierOutput]:
-#        r"""
-#        labels (`torch.LongTensor` of shape `(batch_size,)`, *optional*):
-#            Labels for computing the image classification/regression loss. Indices should be in `[0, ...,
-#            config.num_labels - 1]`. If `config.num_labels == 1` a regression loss is computed (Mean-Square loss), If
-#            `config.num_labels > 1` a classification loss is computed (Cross-Entropy).
-#        """
-#        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
-#
-#        outputs = self.vit(
-#            pixel_values,
-#            head_mask=head_mask,
-#            output_attentions=output_attentions,
-#            output_hidden_states=output_hidden_states,
-#            interpolate_pos_encoding=interpolate_pos_encoding,
-#            return_dict=return_dict,
-#        )
-#
-#        sequence_output = outputs[0]
-#
-#        logits = self.classifier(sequence_output[:, 0, :])
-#
-#        loss = None
-#        if labels is not None:
-#            # move labels to correct device to enable model parallelism
-#            labels = labels.to(logits.device)
-#            if self.config.problem_type is None:
-#                if self.num_labels == 1:
-#                    self.config.problem_type = "regression"
-#                elif self.num_labels > 1 and (labels.dtype == torch.long or labels.dtype == torch.int):
-#                    self.config.problem_type = "single_label_classification"
-#                else:
-#                    self.config.problem_type = "multi_label_classification"
-#
-#            if self.config.problem_type == "regression":
-#                loss_fct = MSELoss()
-#                if self.num_labels == 1:
-#                    loss = loss_fct(logits.squeeze(), labels.squeeze())
-#                else:
-#                    loss = loss_fct(logits, labels)
-#            elif self.config.problem_type == "single_label_classification":
-#                loss_fct = CrossEntropyLoss()
-#                loss = loss_fct(logits.view(-1, self.num_labels), labels.view(-1))
-#            elif self.config.problem_type == "multi_label_classification":
-#                loss_fct = BCEWithLogitsLoss()
-#                loss = loss_fct(logits, labels)
-#
-#        if not return_dict:
-#            output = (logits,) + outputs[1:]
-#            return ((loss,) + output) if loss is not None else output
-#
-#        return ImageClassifierOutput(
-#            loss=loss,
-#            logits=logits,
-#            hidden_states=outputs.hidden_states,
-#            attentions=outputs.attentions,
-#        )
 
 class ViTPoseModel(ViTPosePreTrainedModel):
     def __init__(self, config: ViTPoseConfig, add_pooling_layer: bool = True, use_mask_token: bool = False):
@@ -576,6 +490,92 @@ class ViTPoseModel(ViTPosePreTrainedModel):
 
         return keypoint_outputs
 
+class ViTForImageClassification(ViTPreTrainedModel):
+    def __init__(self, config: ViTConfig) -> None:
+        super().__init__(config)
+
+        self.num_labels = config.num_labels
+        self.vitpose = ViTPoseModel(config, add_pooling_layer=False)
+
+        # Classifier head
+        self.classifier = nn.Linear(config.hidden_size, config.num_labels) if config.num_labels > 0 else nn.Identity()
+
+        # Initialize weights and apply final processing
+        self.post_init()
+
+    @add_start_docstrings_to_model_forward(VIT_INPUTS_DOCSTRING)
+    @add_code_sample_docstrings(
+        checkpoint=_IMAGE_CLASS_CHECKPOINT,
+        output_type=ImageClassifierOutput,
+        config_class=_CONFIG_FOR_DOC,
+        expected_output=_IMAGE_CLASS_EXPECTED_OUTPUT,
+    )
+    def forward(
+        self,
+        pixel_values: Optional[torch.Tensor] = None,
+        head_mask: Optional[torch.Tensor] = None,
+        labels: Optional[torch.Tensor] = None,
+        output_attentions: Optional[bool] = None,
+        output_hidden_states: Optional[bool] = None,
+        interpolate_pos_encoding: Optional[bool] = None,
+        return_dict: Optional[bool] = None,
+    ) -> Union[tuple, ImageClassifierOutput]:
+        r"""
+        labels (`torch.LongTensor` of shape `(batch_size,)`, *optional*):
+            Labels for computing the image classification/regression loss. Indices should be in `[0, ...,
+            config.num_labels - 1]`. If `config.num_labels == 1` a regression loss is computed (Mean-Square loss), If
+            `config.num_labels > 1` a classification loss is computed (Cross-Entropy).
+        """
+        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+
+        outputs = self.vit(
+            pixel_values,
+            head_mask=head_mask,
+            output_attentions=output_attentions,
+            output_hidden_states=output_hidden_states,
+            interpolate_pos_encoding=interpolate_pos_encoding,
+            return_dict=return_dict,
+        )
+
+        sequence_output = outputs[0]
+
+        logits = self.classifier(sequence_output[:, 0, :])
+
+        loss = None
+        if labels is not None:
+            # move labels to correct device to enable model parallelism
+            labels = labels.to(logits.device)
+            if self.config.problem_type is None:
+                if self.num_labels == 1:
+                    self.config.problem_type = "regression"
+                elif self.num_labels > 1 and (labels.dtype == torch.long or labels.dtype == torch.int):
+                    self.config.problem_type = "single_label_classification"
+                else:
+                    self.config.problem_type = "multi_label_classification"
+
+            if self.config.problem_type == "regression":
+                loss_fct = MSELoss()
+                if self.num_labels == 1:
+                    loss = loss_fct(logits.squeeze(), labels.squeeze())
+                else:
+                    loss = loss_fct(logits, labels)
+            elif self.config.problem_type == "single_label_classification":
+                loss_fct = CrossEntropyLoss()
+                loss = loss_fct(logits.view(-1, self.num_labels), labels.view(-1))
+            elif self.config.problem_type == "multi_label_classification":
+                loss_fct = BCEWithLogitsLoss()
+                loss = loss_fct(logits, labels)
+
+        if not return_dict:
+            output = (logits,) + outputs[1:]
+            return ((loss,) + output) if loss is not None else output
+
+        return ImageClassifierOutput(
+            loss=loss,
+            logits=logits,
+            hidden_states=outputs.hidden_states,
+            attentions=outputs.attentions,
+        )
 #]
 #
 #class ViTLayer(nn.Module):
