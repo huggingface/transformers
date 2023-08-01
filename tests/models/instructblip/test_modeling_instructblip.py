f1f31e9f9fb44b181bfd74b6ae3f72f3dd42ffaa
Younes Belkada <49240599+younesbelkada@users.noreply.github.com>
2023-08-01 12:20:34 +0200

    [`MPT`] Add  `require_bitsandbytes` on MPT integration tests (#25201)
    * add  `require_bitsandbytes` on MPT integration tests
    
    * add it on mpt as well

--- tests/models/instructblip/test_modeling_instructblip.py
+++ tests/models/instructblip/test_modeling_instructblip.py
@@ -29,7 +29,7 @@ from transformers import (
     InstructBlipQFormerConfig,
     InstructBlipVisionConfig,
 )
-from transformers.testing_utils import require_torch, require_vision, slow, torch_device
+from transformers.testing_utils import require_bitsandbytes, require_torch, require_vision, slow, torch_device
 from transformers.utils import is_torch_available, is_vision_available
 
 from ...test_configuration_common import ConfigTester
@@ -521,6 +521,7 @@ def prepare_img():
 @require_torch
 @slow
 class InstructBlipModelIntegrationTest(unittest.TestCase):
+    @require_bitsandbytes
     def test_inference_vicuna_7b(self):
         processor = InstructBlipProcessor.from_pretrained("Salesforce/instructblip-vicuna-7b")
         model = InstructBlipForConditionalGeneration.from_pretrained(
