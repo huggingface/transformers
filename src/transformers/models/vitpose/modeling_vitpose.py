f262f41742b0a24e029a84fc938ce69fb0de9ba6
shauray8 <shauray9@gmail.com>
2023-08-02 00:00:10 +0530

    more changes

--- src/transformers/models/vitpose/modeling_vitpose.py
+++ src/transformers/models/vitpose/modeling_vitpose.py
@@ -79,6 +79,7 @@ class ViTPosePatchEmbed(nn.Module):
         self.image_size = config.img_size
        # self.image_size = self.image_size if isinstance(self.image_size, collections.abc.Iterable) else (self.image_size, self.image_size)
         self.patch_size = self.patch_size if isinstance(self.patch_size, collections.abc.Iterable) else (self.patch_size, self.patch_size)
+        self.num_patches = (self.image_size[1] // self.patch_size[1]) * (self.image_size[0] // self.patch_size[0])
 
         self.projection = nn.Conv2d(self.num_channels, self.embed_dim, kernel_size=self.patch_size, stride=self.patch_size, padding = (2,2))
 
@@ -165,7 +166,7 @@ class ViTPoseAttention(nn.Module):
 
         return attention_output
 
-class mlp(nn.Module):
+class ViTPoseMLP(nn.Module):
     def __init__(self, config: ViTPoseConfig):
         super().__init__()
 
@@ -190,7 +191,7 @@ class ViTPoseBlock(nn.Module):
         self.attn = ViTPoseAttention(config)
         self.drop_path = DropPath(p = config.drop_path_rate)
         self.norm2 = nn.LayerNorm(config.embed_dim, eps=1e-06, elementwise_affine=True)
-        self.mlp = mlp(config)
+        self.mlp = ViTPoseMLP(config)
 
     def forward(self, x):
         x = x + self.drop_path(self.attn(self.norm1(x)))
@@ -214,84 +215,84 @@ class DropPath(nn.Module):
         return output
 
 ## to be changed
-#class ViTBackbone(nn.Module):
-#    """
-#    Construct the CLS token, position and patch embeddings. Optionally, also the mask token.
-#    """
-#
-#    def __init__(self, config: ViTPoseConfig, use_mask_token: bool = False) -> None:
-#        super().__init__()
-#
-#        self.cls_token = nn.Parameter(torch.randn(1, 1, config.hidden_size))
-#        self.mask_token = nn.Parameter(torch.zeros(1, 1, config.hidden_size)) if use_mask_token else None
-#        self.patch_embeddings = ViTPatchEmbed(config)
-#        num_patches = self.patch_embeddings.num_patches
-#        self.position_embeddings = nn.Parameter(torch.randn(1, num_patches + 1, config.hidden_size))
-#        self.dropout = nn.Dropout(config.dropout_p)
-#        self.config = config
-#
-#    def interpolate_pos_encoding(self, embeddings: torch.Tensor, height: int, width: int) -> torch.Tensor:
-#        """
-#        This method allows to interpolate the pre-trained position encodings, to be able to use the model on higher
-#        resolution images.
-#
-#        Source:
-#        https://github.com/facebookresearch/dino/blob/de9ee3df6cf39fac952ab558447af1fa1365362a/vision_transformer.py#L174
-#        """
-#
-#        num_patches = embeddings.shape[1] - 1
-#        num_positions = self.position_embeddings.shape[1] - 1
-#        if num_patches == num_positions and height == width:
-#            return self.position_embeddings
-#        class_pos_embed = self.position_embeddings[:, 0]
-#        patch_pos_embed = self.position_embeddings[:, 1:]
-#        dim = embeddings.shape[-1]
-#        h0 = height // self.config.patch_size
-#        w0 = width // self.config.patch_size
-#        # we add a small number to avoid floating point error in the interpolation
-#        # see discussion at https://github.com/facebookresearch/dino/issues/8
-#        h0, w0 = h0 + 0.1, w0 + 0.1
-#        patch_pos_embed = patch_pos_embed.reshape(1, int(math.sqrt(num_positions)), int(math.sqrt(num_positions)), dim)
-#        patch_pos_embed = patch_pos_embed.permute(0, 3, 1, 2)
-#        patch_pos_embed = nn.functional.interpolate(
-#            patch_pos_embed,
-#            scale_factor=(h0 / math.sqrt(num_positions), w0 / math.sqrt(num_positions)),
-#            mode="bicubic",
-#            align_corners=False,
-#        )
-#        assert int(h0) == patch_pos_embed.shape[-2] and int(w0) == patch_pos_embed.shape[-1]
-#        patch_pos_embed = patch_pos_embed.permute(0, 2, 3, 1).view(1, -1, dim)
-#        return torch.cat((class_pos_embed.unsqueeze(0), patch_pos_embed), dim=1)
-#
-#    def forward(
-#        self,
-#        pixel_values: torch.Tensor,
-#        bool_masked_pos: Optional[torch.BoolTensor] = None,
-#        interpolate_pos_encoding: bool = False,
-#    ) -> torch.Tensor:
-#        batch_size, num_channels, height, width = pixel_values.shape
-#        embeddings = self.patch_embeddings(pixel_values, interpolate_pos_encoding=interpolate_pos_encoding)
-#
-#        if bool_masked_pos is not None:
-#            seq_length = embeddings.shape[1]
-#            mask_tokens = self.mask_token.expand(batch_size, seq_length, -1)
-#            # replace the masked visual tokens by mask_tokens
-#            mask = bool_masked_pos.unsqueeze(-1).type_as(mask_tokens)
-#            embeddings = embeddings * (1.0 - mask) + mask_tokens * mask
-#
-#        # add the [CLS] token to the embedded patch tokens
-#        cls_tokens = self.cls_token.expand(batch_size, -1, -1)
-#        embeddings = torch.cat((cls_tokens, embeddings), dim=1)
-#
-#        # add positional encoding to each token
-#        if interpolate_pos_encoding:
-#            embeddings = embeddings + self.interpolate_pos_encoding(embeddings, height, width)
-#        else:
-#            embeddings = embeddings + self.position_embeddings
-#
-#        embeddings = self.dropout(embeddings)
-#
-#        return embeddings
+class ViTBackbone(nn.Module):
+    """
+    Construct the CLS token, position and patch embeddings. Optionally, also the mask token.
+    """
+
+    def __init__(self, config: ViTPoseConfig, use_mask_token: bool = False) -> None:
+        super().__init__()
+
+        self.cls_token = nn.Parameter(torch.randn(1, 1, config.embed_dim))
+        self.mask_token = nn.Parameter(torch.zeros(1, 1, config.embed_dim)) if use_mask_token else None
+        self.patch_embeddings = ViTPosePatchEmbed(config)
+        num_patches = self.patch_embeddings.num_patches
+        self.position_embeddings = nn.Parameter(torch.randn(1, num_patches + 1, config.embed_dim))
+        self.dropout = nn.Dropout(config.dropout_p)
+        self.config = config
+
+    def interpolate_pos_encoding(self, embeddings: torch.Tensor, height: int, width: int) -> torch.Tensor:
+        """
+        This method allows to interpolate the pre-trained position encodings, to be able to use the model on higher
+        resolution images.
+
+        Source:
+        https://github.com/facebookresearch/dino/blob/de9ee3df6cf39fac952ab558447af1fa1365362a/vision_transformer.py#L174
+        """
+
+        num_patches = embeddings.shape[1] - 1
+        num_positions = self.position_embeddings.shape[1] - 1
+        if num_patches == num_positions and height == width:
+            return self.position_embeddings
+        class_pos_embed = self.position_embeddings[:, 0]
+        patch_pos_embed = self.position_embeddings[:, 1:]
+        dim = embeddings.shape[-1]
+        h0 = height // self.config.patch_size
+        w0 = width // self.config.patch_size
+        # we add a small number to avoid floating point error in the interpolation
+        # see discussion at https://github.com/facebookresearch/dino/issues/8
+        h0, w0 = h0 + 0.1, w0 + 0.1
+        patch_pos_embed = patch_pos_embed.reshape(1, int(math.sqrt(num_positions)), int(math.sqrt(num_positions)), dim)
+        patch_pos_embed = patch_pos_embed.permute(0, 3, 1, 2)
+        patch_pos_embed = nn.functional.interpolate(
+            patch_pos_embed,
+            scale_factor=(h0 / math.sqrt(num_positions), w0 / math.sqrt(num_positions)),
+            mode="bicubic",
+            align_corners=False,
+        )
+        assert int(h0) == patch_pos_embed.shape[-2] and int(w0) == patch_pos_embed.shape[-1]
+        patch_pos_embed = patch_pos_embed.permute(0, 2, 3, 1).view(1, -1, dim)
+        return torch.cat((class_pos_embed.unsqueeze(0), patch_pos_embed), dim=1)
+
+    def forward(
+        self,
+        pixel_values: torch.Tensor,
+        bool_masked_pos: Optional[torch.BoolTensor] = None,
+        interpolate_pos_encoding: bool = False,
+    ) -> torch.Tensor:
+        batch_size, num_channels, height, width = pixel_values.shape
+        embeddings = self.patch_embeddings(pixel_values, interpolate_pos_encoding=interpolate_pos_encoding)
+
+        if bool_masked_pos is not None:
+            seq_length = embeddings.shape[1]
+            mask_tokens = self.mask_token.expand(batch_size, seq_length, -1)
+            # replace the masked visual tokens by mask_tokens
+            mask = bool_masked_pos.unsqueeze(-1).type_as(mask_tokens)
+            embeddings = embeddings * (1.0 - mask) + mask_tokens * mask
+
+        # add the [CLS] token to the embedded patch tokens
+        cls_tokens = self.cls_token.expand(batch_size, -1, -1)
+        embeddings = torch.cat((cls_tokens, embeddings), dim=1)
+
+        # add positional encoding to each token
+        if interpolate_pos_encoding:
+            embeddings = embeddings + self.interpolate_pos_encoding(embeddings, height, width)
+        else:
+            embeddings = embeddings + self.position_embeddings
+
+        embeddings = self.dropout(embeddings)
+
+        return embeddings
 
 
 class ViTPoseBackbone(nn.Module):
@@ -853,10 +854,7 @@ class ViTPoseModel(ViTPosePreTrainedModel):
 #)
 
 import numpy
-m = ViTPosePatchEmbed(ViTPoseConfig())
-model = ViTPoseBlock(ViTPoseConfig())
-print(m)
-for i in range(8):
-    print(model)
+model = ViTPoseBackbone(ViTPoseConfig())
+print(model)
 
 
