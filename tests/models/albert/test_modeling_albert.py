c11ae43c2b5a828a271d615750984046c6d73525
ydshieh <ydshieh@users.noreply.github.com>
2023-08-02 17:33:15 +0200

    CI with layers=2

--- tests/models/albert/test_modeling_albert.py
+++ tests/models/albert/test_modeling_albert.py
@@ -55,7 +55,8 @@ class AlbertModelTester:
         embedding_size=16,
         hidden_size=36,
         num_hidden_layers=2,
-        num_hidden_groups=6,
+        # this needs to be the same as `num_hidden_layers`!
+        num_hidden_groups=2,
         num_attention_heads=6,
         intermediate_size=37,
         hidden_act="gelu",
