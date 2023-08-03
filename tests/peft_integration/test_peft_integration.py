cd9943960f6bfe113f1f14000e0ff603006b778a
younesbelkada <younesbelkada@gmail.com>
2023-08-03 13:41:18 +0000

    fixup and nits

--- tests/peft_integration/test_peft_integration.py
+++ tests/peft_integration/test_peft_integration.py
@@ -28,18 +28,22 @@ if is_torch_available():
 @require_peft
 @require_torch
 class PeftTesterMixin:
-    peft_test_model_ids = ("peft-internal-testing/tiny-OPTForCausalLM-lora", )
-    transformers_test_model_ids = ("hf-internal-testing/tiny-random-OPTForCausalLM", )
+    peft_test_model_ids = ("peft-internal-testing/tiny-OPTForCausalLM-lora",)
+    transformers_test_model_ids = ("hf-internal-testing/tiny-random-OPTForCausalLM",)
     transformers_test_model_classes = (AutoModelForCausalLM, OPTForCausalLM)
 
 
 # TODO: run it with CI after PEFT release.
 @slow
 class PeftIntegrationTester(unittest.TestCase, PeftTesterMixin):
-    r"""
+    """
     A testing suite that makes sure that the PeftModel class is correctly integrated into the transformers library.
     """
+
     def _check_lora_correctly_converted(self, model):
+        """
+        Utility method to check if the model has correctly adapters injected on it.
+        """
         from peft.tuners.tuners_utils import BaseTunerLayer
 
         is_peft_loaded = False
@@ -48,12 +52,12 @@ class PeftIntegrationTester(unittest.TestCase, PeftTesterMixin):
             if isinstance(m, BaseTunerLayer):
                 is_peft_loaded = True
                 break
-                
+
         return is_peft_loaded
 
     def test_peft_from_pretrained(self):
-        r"""
-        Simple test that tests the basic usage of PEFT model through `from_pretrained`. 
+        """
+        Simple test that tests the basic usage of PEFT model through `from_pretrained`.
         This checks if we pass a remote folder that contains an adapter config and adapter weights, it
         should correctly load a model that has adapters injected on it.
         """
@@ -67,7 +71,7 @@ class PeftIntegrationTester(unittest.TestCase, PeftTesterMixin):
                 _ = peft_model.generate(input_ids=torch.LongTensor([[0, 1, 2, 3, 4, 5, 6, 7]]).to(torch_device))
 
     def test_peft_state_dict(self):
-        r"""
+        """
         Simple test that checks if the returned state dict of `get_adapter_state_dict()` method contains
         the expected keys.
         """
@@ -81,8 +85,8 @@ class PeftIntegrationTester(unittest.TestCase, PeftTesterMixin):
                     self.assertTrue("lora" in key)
 
     def test_peft_save_pretrained(self):
-        r"""
-        Test that checks various combinations of `save_pretrained` with a model that has adapters loaded 
+        """
+        Test that checks various combinations of `save_pretrained` with a model that has adapters loaded
         on it. This checks if the saved model contains the expected files (adapter weights and adapter config).
         """
         for model_id in self.peft_test_model_ids:
@@ -108,9 +112,8 @@ class PeftIntegrationTester(unittest.TestCase, PeftTesterMixin):
                     peft_model = transformers_class.from_pretrained(tmpdirname).to(torch_device)
                     self.assertTrue(self._check_lora_correctly_converted(peft_model))
 
-
     def test_peft_enable_disable_adapters(self):
-        r"""
+        """
         A test that checks if `enable_adapters` and `disable_adapters` methods work as expected.
         """
         from peft import LoraConfig
@@ -119,12 +122,9 @@ class PeftIntegrationTester(unittest.TestCase, PeftTesterMixin):
 
         for model_id in self.transformers_test_model_ids:
             for transformers_class in self.transformers_test_model_classes:
-                
                 peft_model = transformers_class.from_pretrained(model_id).to(torch_device)
 
-                peft_config = LoraConfig(
-                    init_lora_weights=False
-                )
+                peft_config = LoraConfig(init_lora_weights=False)
 
                 peft_model.add_adapter(peft_config)
 
@@ -142,7 +142,7 @@ class PeftIntegrationTester(unittest.TestCase, PeftTesterMixin):
                 self.assertFalse(torch.allclose(peft_logits_enabled, peft_logits_disabled, atol=1e-12, rtol=1e-12))
 
     def test_peft_add_adapter(self):
-        r"""
+        """
         Simple test that tests if `add_adapter` works as expected
         """
         from peft import LoraConfig
@@ -151,24 +151,21 @@ class PeftIntegrationTester(unittest.TestCase, PeftTesterMixin):
             for transformers_class in self.transformers_test_model_classes:
                 model = transformers_class.from_pretrained(model_id).to(torch_device)
 
-                peft_config = LoraConfig(
-                    init_lora_weights=False
-                )
+                peft_config = LoraConfig(init_lora_weights=False)
 
                 model.add_adapter(peft_config)
 
                 self.assertTrue(self._check_lora_correctly_converted(model))
                 # dummy generation
                 _ = model.generate(input_ids=torch.LongTensor([[0, 1, 2, 3, 4, 5, 6, 7]]).to(torch_device))
-    
 
     def test_peft_add_multi_adapter(self):
-        r"""
+        """
         Simple test that tests the basic usage of PEFT model through `from_pretrained`. This test tests if
         add_adapter works as expected in multi-adapter setting.
         """
-        from peft.tuners.tuners_utils import BaseTunerLayer
         from peft import LoraConfig
+        from peft.tuners.tuners_utils import BaseTunerLayer
 
         dummy_input = torch.LongTensor([[0, 1, 2, 3, 4, 5, 6, 7]]).to(torch_device)
 
@@ -179,9 +176,7 @@ class PeftIntegrationTester(unittest.TestCase, PeftTesterMixin):
 
                 logits_original_model = model(dummy_input).logits
 
-                peft_config = LoraConfig(
-                    init_lora_weights=False
-                )
+                peft_config = LoraConfig(init_lora_weights=False)
 
                 model.add_adapter(peft_config)
 
@@ -208,12 +203,14 @@ class PeftIntegrationTester(unittest.TestCase, PeftTesterMixin):
                 self.assertTrue(model.active_adapter() == "adapter-2")
 
                 # Logits comparison
-                self.assertFalse(torch.allclose(logits_adapter_1.logits, logits_adapter_2.logits, atol=1e-6, rtol=1e-6))
+                self.assertFalse(
+                    torch.allclose(logits_adapter_1.logits, logits_adapter_2.logits, atol=1e-6, rtol=1e-6)
+                )
                 self.assertFalse(torch.allclose(logits_original_model, logits_adapter_2.logits, atol=1e-6, rtol=1e-6))
 
     @require_torch_gpu
     def test_peft_from_pretrained_kwargs(self):
-        r"""
+        """
         Simple test that tests the basic usage of PEFT model through `from_pretrained` + additional kwargs
         and see if the integraiton behaves as expected.
         """
