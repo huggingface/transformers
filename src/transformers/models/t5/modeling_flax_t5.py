3eee23815f68e7634f9245d14b9ce096df26d160
mariecwhite <mariewhite@google.com>
2023-08-05 03:36:57 +1000

    Remove jnp.DeviceArray since it is deprecated. (#24875)
    * Remove jnp.DeviceArray since it is deprecated.
    
    * Replace all instances of jnp.DeviceArray with jax.Array
    
    * Update src/transformers/models/bert/modeling_flax_bert.py
    
    ---------
    
    Co-authored-by: Sanchit Gandhi <93869735+sanchit-gandhi@users.noreply.github.com>

--- src/transformers/models/t5/modeling_flax_t5.py
+++ src/transformers/models/t5/modeling_flax_t5.py
@@ -1740,8 +1740,8 @@ class FlaxT5ForConditionalGeneration(FlaxT5PreTrainedModel):
         self,
         decoder_input_ids,
         max_length,
-        attention_mask: Optional[jnp.DeviceArray] = None,
-        decoder_attention_mask: Optional[jnp.DeviceArray] = None,
+        attention_mask: Optional[jax.Array] = None,
+        decoder_attention_mask: Optional[jax.Array] = None,
         encoder_outputs=None,
         **kwargs,
     ):
