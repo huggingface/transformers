5a138e0ae533a98f5180cf63f419af5a152d8252
Niels <niels.rogge1@gmail.com>
2023-08-07 11:59:16 +0200

    More fixes

--- utils/check_repo.py
+++ utils/check_repo.py
@@ -273,8 +273,6 @@ IGNORE_NON_AUTO_CONFIGURED = PRIVATE_MODELS.copy() + [
     "SpeechT5ForSpeechToSpeech",
     "SpeechT5ForTextToSpeech",
     "SpeechT5HifiGan",
-    "MusicgenModel",
-    "MusicgenForConditionalGeneration",
     "VitMatteForImageMatting",
 ]
 
