65001cb1c8a7cfb431929f228be8efa7e99bc5a2
calpt <calpt@mail.de>
2023-08-07 16:31:15 +0200

    Loosen output shape restrictions on GPT-style models (#25188)
    * Loosen output shape restrictions on GPT-style models
    
    * Use more self-explanatory variables
    
    * Revert "Use more self-explanatory variables"
    
    This reverts commit 5fd9ab39119558b7e750f61aa4a19014dccc5ed5.

--- src/transformers/models/decision_transformer/modeling_decision_transformer.py
+++ src/transformers/models/decision_transformer/modeling_decision_transformer.py
@@ -603,7 +603,7 @@ class DecisionTransformerGPT2Model(DecisionTransformerGPT2PreTrainedModel):
 
         hidden_states = self.drop(hidden_states)
 
-        output_shape = input_shape + (hidden_states.size(-1),)
+        output_shape = (-1,) + input_shape[1:] + (hidden_states.size(-1),)
 
         if self.gradient_checkpointing and self.training:
             if use_cache:
