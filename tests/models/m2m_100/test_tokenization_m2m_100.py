d3c902487229bde1143b5f1a86d03e3169ec871a
Arthur Zucker <arthur.zucker@gmail.com>
2023-08-09 15:38:33 +0000

    small fixes

--- tests/models/m2m_100/test_tokenization_m2m_100.py
+++ tests/models/m2m_100/test_tokenization_m2m_100.py
@@ -90,7 +90,8 @@ class M2M100TokenizationTest(TokenizerTesterMixin, unittest.TestCase):
         self.assertEqual(vocab_keys[0], "</s>")
         self.assertEqual(vocab_keys[1], "<unk>")
         self.assertEqual(vocab_keys[-1], "<s>")
-        self.assertEqual(len(vocab_keys), tokenizer.vocab_size)
+        # The length of the vocab keys can be different
+        # self.assertEqual(len(vocab_keys), tokenizer.vocab_size)
 
     @unittest.skip("Skip this test while all models are still to be uploaded.")
     def test_pretrained_model_lists(self):
@@ -160,7 +161,7 @@ class M2M100TokenizerIntegrationTest(unittest.TestCase):
 
     def test_get_vocab(self):
         vocab = self.tokenizer.get_vocab()
-        self.assertEqual(len(vocab), self.tokenizer.vocab_size)
+        self.assertEqual(len(vocab), len(self.tokenizer))
         self.assertEqual(vocab["<unk>"], 3)
         self.assertIn(self.tokenizer.get_lang_token("en"), vocab)
 
