1158b6fab41d4d3387a2894b553d97d98986731f
ZHITENGLI <cnbgcnbg@126.com>
2023-08-10 07:33:23 +0000

    update documentation

--- src/transformers/models/egt/configuration_egt.py
+++ src/transformers/models/egt/configuration_egt.py
@@ -8,11 +8,11 @@ import torch.nn as nn
 
 logger = logging.get_logger(__name__)
 
-# GRAPHORMER_PRETRAINED_CONFIG_ARCHIVE_MAP = {
-#     # pcqm4mv1 now deprecated
-#     "graphormer-base": "https://huggingface.co/clefourrier/graphormer-base-pcqm4mv2/resolve/main/config.json",
-#     # See all Graphormer models at https://huggingface.co/models?filter=graphormer
-# }
+EGT_PRETRAINED_CONFIG_ARCHIVE_MAP = {
+    # pcqm4mv1 now deprecated
+    "graphormer-base": "https://huggingface.co/clefourrier/graphormer-base-pcqm4mv2/resolve/main/config.json",
+    # See all Graphormer models at https://huggingface.co/models?filter=graphormer
+}
 
 
 class EGTConfig(PretrainedConfig):
@@ -27,75 +27,32 @@ class EGTConfig(PretrainedConfig):
 
 
     Args:
+        feat_size (`int`, *optional*, defaults to 128):
+            Node feature size.
+        edge_feat_size (`int`, *optional*, defaults to 8):
+            Edge feature size.
+        num_heads (`int`, *optional*, defaults to 8):
+            Number of attention heads, by which :attr: `feat_size` is divisible.
+        num_layers (`int`, *optional*, defaults to 4):
+            Number of layers.
+        dropout (`float`, *optional*, defaults to 0.0):
+            Dropout probability.
+        attn_dropout (`float`, *optional*, defaults to 0.0):
+            Attention dropout probability.
+        activation (`nn.Module`, *optional*, defaults to nn.ELU()):
+            Activation function.
+        egt_simple (`bool`, *optional*, defaults to False):
+            If `False`, update the edge embedding.
+        upto_hop (`int`, *optional*, defaults to 16):
+            Maximum distance between nodes in the distance matrices.
+        mlp_ratios (`List[float]`, *optional*, defaults to [1., 1.]):
+            Ratios of inner dimensions with respect to the input dimension in MLP output block.
+        num_virtual_nodes (`int`, *optional*, defaults to 4):
+            Number of virtual nodes in EGT model, aggregated to graph embedding in the readout function.
+        svd_pe_size (`int`, *optional*, defaults to 8):
+            SVD positional encoding size.
         num_classes (`int`, *optional*, defaults to 1):
             Number of target classes or labels, set to n for binary classification of n tasks.
-        num_atoms (`int`, *optional*, defaults to 512*9):
-            Number of node types in the graphs.
-        num_edges (`int`, *optional*, defaults to 512*3):
-            Number of edges types in the graph.
-        num_in_degree (`int`, *optional*, defaults to 512):
-            Number of in degrees types in the input graphs.
-        num_out_degree (`int`, *optional*, defaults to 512):
-            Number of out degrees types in the input graphs.
-        num_edge_dis (`int`, *optional*, defaults to 128):
-            Number of edge dis in the input graphs.
-        multi_hop_max_dist (`int`, *optional*, defaults to 20):
-            Maximum distance of multi hop edges between two nodes.
-        spatial_pos_max (`int`, *optional*, defaults to 1024):
-            Maximum distance between nodes in the graph attention bias matrices, used during preprocessing and
-            collation.
-        edge_type (`str`, *optional*, defaults to multihop):
-            Type of edge relation chosen.
-        max_nodes (`int`, *optional*, defaults to 512):
-            Maximum number of nodes which can be parsed for the input graphs.
-        share_input_output_embed (`bool`, *optional*, defaults to `False`):
-            Shares the embedding layer between encoder and decoder - careful, True is not implemented.
-        num_layers (`int`, *optional*, defaults to 12):
-            Number of layers.
-        embedding_dim (`int`, *optional*, defaults to 768):
-            Dimension of the embedding layer in encoder.
-        ffn_embedding_dim (`int`, *optional*, defaults to 768):
-            Dimension of the "intermediate" (often named feed-forward) layer in encoder.
-        num_attention_heads (`int`, *optional*, defaults to 32):
-            Number of attention heads in the encoder.
-        self_attention (`bool`, *optional*, defaults to `True`):
-            Model is self attentive (False not implemented).
-        activation_function (`str` or `function`, *optional*, defaults to `"gelu"`):
-            The non-linear activation function (function or string) in the encoder and pooler. If string, `"gelu"`,
-            `"relu"`, `"silu"` and `"gelu_new"` are supported.
-        dropout (`float`, *optional*, defaults to 0.1):
-            The dropout probability for all fully connected layers in the embeddings, encoder, and pooler.
-        attention_dropout (`float`, *optional*, defaults to 0.1):
-            The dropout probability for the attention weights.
-        layerdrop (`float`, *optional*, defaults to 0.0):
-            The LayerDrop probability for the encoder. See the [LayerDrop paper](see https://arxiv.org/abs/1909.11556)
-            for more details.
-        bias (`bool`, *optional*, defaults to `True`):
-            Uses bias in the attention module - unsupported at the moment.
-        embed_scale(`float`, *optional*, defaults to None):
-            Scaling factor for the node embeddings.
-        num_trans_layers_to_freeze (`int`, *optional*, defaults to 0):
-            Number of transformer layers to freeze.
-        encoder_normalize_before (`bool`, *optional*, defaults to `False`):
-            Normalize features before encoding the graph.
-        pre_layernorm (`bool`, *optional*, defaults to `False`):
-            Apply layernorm before self attention and the feed forward network. Without this, post layernorm will be
-            used.
-        apply_graphormer_init (`bool`, *optional*, defaults to `False`):
-            Apply a custom graphormer initialisation to the model before training.
-        freeze_embeddings (`bool`, *optional*, defaults to `False`):
-            Freeze the embedding layer, or train it along the model.
-        encoder_normalize_before (`bool`, *optional*, defaults to `False`):
-            Apply the layer norm before each encoder block.
-        q_noise (`float`, *optional*, defaults to 0.0):
-            Amount of quantization noise (see "Training with Quantization Noise for Extreme Model Compression"). (For
-            more detail, see fairseq's documentation on quant_noise).
-        qn_block_size (`int`, *optional*, defaults to 8):
-            Size of the blocks for subsequent quantization with iPQ (see q_noise).
-        kdim (`int`, *optional*, defaults to None):
-            Dimension of the key in the attention, if different from the other values.
-        vdim (`int`, *optional*, defaults to None):
-            Dimension of the value in the attention, if different from the other values.
         use_cache (`bool`, *optional*, defaults to `True`):
             Whether or not the model should return the last key/values attentions (not used by all models).
         traceable (`bool`, *optional*, defaults to `False`):
@@ -120,39 +77,37 @@ class EGTConfig(PretrainedConfig):
 
     def __init__(
         self,
-        node_width: int = 128,
-        edge_width: int = 32,
+        feat_size: int = 128,
+        edge_feat_size: int = 32,
         num_heads: int = 8,
-        model_height: int = 4,
+        num_layers: int = 4,
         dropout: float = 0.,
         attn_dropout: float = 0.,
         activation: nn.Module = nn.ELU(),
-        ffn_multiplier: float = 1.,
         egt_simple: bool = False,
         upto_hop: int = 16,
         mlp_ratios: List[float] = [1., 1.],
         num_virtual_nodes: int = 4,
-        svd_encodings: int = 8,
-        output_dim: int = 1,
+        svd_pe_size: int = 8,
+        num_classes: int = 1,
         pad_token_id=0,
         bos_token_id=1,
         eos_token_id=2,
         **kwargs,
     ):
-        self.node_width = node_width
-        self.edge_width = edge_width
+        self.feat_size = feat_size
+        self.edge_feat_size = edge_feat_size
         self.num_heads = num_heads
-        self.model_height = model_height
+        self.num_layers = num_layers
         self.dropout = dropout
         self.attn_dropout = attn_dropout
         self.activation = activation
-        self.ffn_multiplier = ffn_multiplier
         self.egt_simple = egt_simple
         self.upto_hop = upto_hop
         self.mlp_ratios = mlp_ratios
         self.num_virtual_nodes = num_virtual_nodes
-        self.svd_encodings = svd_encodings
-        self.output_dim = output_dim
+        self.svd_pe_size = svd_pe_size
+        self.num_classes = num_classes
 
         super().__init__(
             pad_token_id=pad_token_id,
--- src/transformers/models/egt/modeling_egt.py
+++ src/transformers/models/egt/modeling_egt.py
@@ -30,13 +30,13 @@ class EGTLayer(nn.Module):
 
     Parameters
     ----------
-    ndim : int
-        Node embedding dimension.
-    edim : int
-        Edge embedding dimension.
+    feat_size : int
+        Node feature size.
+    edge_feat_size : int
+        Edge feature size.
     num_heads : int
-        Number of attention heads, by which :attr: `ndim` is divisible.
-    num_vns : int
+        Number of attention heads, by which :attr: `feat_size` is divisible.
+    num_virtual_nodes : int
         Number of virtual nodes.
     dropout : float, optional
         Dropout probability. Default: 0.0.
@@ -44,9 +44,6 @@ class EGTLayer(nn.Module):
         Attention dropout probability. Default: 0.0.
     activation : callable activation layer, optional
         Activation function. Default: nn.ELU().
-    ffn_multiplier : float, optional
-        Multiplier of the inner dimension in Feed Forward Network.
-        Default: 2.0.
     edge_update : bool, optional
         Whether to update the edge embedding. Default: True.
 
@@ -57,156 +54,163 @@ class EGTLayer(nn.Module):
 
     >>> batch_size = 16
     >>> num_nodes = 100
-    >>> ndim, edim = 128, 32
-    >>> nfeat = th.rand(batch_size, num_nodes, ndim)
-    >>> efeat = th.rand(batch_size, num_nodes, num_nodes, edim)
+    >>> feat_size, edge_feat_size = 128, 32
+    >>> nfeat = th.rand(batch_size, num_nodes, feat_size)
+    >>> efeat = th.rand(batch_size, num_nodes, num_nodes, edge_feat_size)
     >>> net = EGTLayer(
-            ndim=ndim,
-            edim=edim,
+            feat_size=feat_size,
+            edge_feat_size=edge_feat_size,
             num_heads=8,
-            num_vns=4,
+            num_virtual_nodes=4,
         )
     >>> out = net(nfeat, efeat)
     """
 
     def __init__(
         self,
-        ndim,
-        edim,
+        feat_size,
+        edge_feat_size,
         num_heads,
-        num_vns,
+        num_virtual_nodes,
         dropout=0,
         attn_dropout=0,
         activation=nn.ELU(),
-        ffn_multiplier=2.0,
         edge_update=True,
     ):
         super().__init__()
         self.num_heads = num_heads
-        self.num_vns = num_vns
+        self.num_virtual_nodes = num_virtual_nodes
         self.edge_update = edge_update
 
-        assert not (ndim % num_heads)
-        self.dot_dim = ndim // num_heads
-        self.mha_ln_h = nn.LayerNorm(ndim)
-        self.mha_ln_e = nn.LayerNorm(edim)
-        self.E = nn.Linear(edim, num_heads)
-        self.QKV = nn.Linear(ndim, ndim * 3)
-        self.G = nn.Linear(edim, num_heads)
+        assert (
+            feat_size % num_heads == 0
+        ), "feat_size must be divisible by num_heads"
+        self.dot_dim = feat_size // num_heads
+        self.mha_ln_h = nn.LayerNorm(feat_size)
+        self.mha_ln_e = nn.LayerNorm(edge_feat_size)
+        self.edge_input = nn.Linear(edge_feat_size, num_heads)
+        self.qkv_proj = nn.Linear(feat_size, feat_size * 3)
+        self.gate = nn.Linear(edge_feat_size, num_heads)
         self.attn_dropout = nn.Dropout(attn_dropout)
-        self.O_h = nn.Linear(ndim, ndim)
+        self.node_output = nn.Linear(feat_size, feat_size)
         self.mha_dropout_h = nn.Dropout(dropout)
 
-        node_inner_dim = round(ndim * ffn_multiplier)
         self.node_ffn = nn.Sequential(
-            nn.LayerNorm(ndim),
-            nn.Linear(ndim, node_inner_dim),
+            nn.LayerNorm(feat_size),
+            nn.Linear(feat_size, feat_size),
             activation,
-            nn.Linear(node_inner_dim, ndim),
+            nn.Linear(feat_size, feat_size),
             nn.Dropout(dropout),
         )
 
         if self.edge_update:
-            self.O_e = nn.Linear(num_heads, edim)
+            self.edge_output = nn.Linear(num_heads, edge_feat_size)
             self.mha_dropout_e = nn.Dropout(dropout)
-            edge_inner_dim = round(edim * ffn_multiplier)
             self.edge_ffn = nn.Sequential(
-                nn.LayerNorm(edim),
-                nn.Linear(edim, edge_inner_dim),
+                nn.LayerNorm(edge_feat_size),
+                nn.Linear(edge_feat_size, edge_feat_size),
                 activation,
-                nn.Linear(edge_inner_dim, edim),
+                nn.Linear(edge_feat_size, edge_feat_size),
                 nn.Dropout(dropout),
             )
 
-    def forward(self, h, e, mask=None):
-        """Forward computation. Note: :attr:`h` and :attr:`e` should be padded
-        with embedding of virtual nodes if :attr:`num_vns` > 0, while
-        :attr:`mask` should be padded with `0` values for virtual nodes.
+    def forward(self, nfeat, efeat, mask=None):
+        """Forward computation. Note: :attr:`nfeat` and :attr:`efeat` should be
+        padded with embedding of virtual nodes if :attr:`num_virtual_nodes` > 0,
+        while :attr:`mask` should be padded with `0` values for virtual nodes.
+        The padding should be put at the beginning.
 
         Parameters
         ----------
-        h : torch.Tensor
-            A 3D input tensor. Shape: (batch_size, N, :attr:`ndim`), where N
-            is the sum of maximum number of nodes and number of virtual nodes.
-        e : torch.Tensor
+        nfeat : torch.Tensor
+            A 3D input tensor. Shape: (batch_size, N, :attr:`feat_size`), where N
+            is the sum of the maximum number of nodes and the number of virtual nodes.
+        efeat : torch.Tensor
             Edge embedding used for attention computation and self update.
-            Shape: (batch_size, N, N, :attr:`edim`).
+            Shape: (batch_size, N, N, :attr:`edge_feat_size`).
         mask : torch.Tensor, optional
             The attention mask used for avoiding computation on invalid
-            positions, where invalid positions are indicated by `-inf` and
-            valid positions are indicated by `0`.
+            positions, where valid positions are indicated by `0` and
+            invalid positions are indicated by `-inf`.
             Shape: (batch_size, N, N). Default: None.
 
         Returns
         -------
-        h : torch.Tensor
-            The output node embedding. Shape: (batch_size, N, :attr:`ndim`).
-        e : torch.Tensor
-            The output edge embedding. Shape: (batch_size, N, N, :attr:`edim`).
+        nfeat : torch.Tensor
+            The output node embedding. Shape: (batch_size, N, :attr:`feat_size`).
+        efeat : torch.Tensor, optional
+            The output edge embedding. Shape: (batch_size, N, N, :attr:`edge_feat_size`).
+            It is returned only if :attr:`edge_update` is True.
         """
+        nfeat_r1 = nfeat
+        efeat_r1 = efeat
 
-        h_r1 = h
-        e_r1 = e
-
-        h_ln = self.mha_ln_h(h)
-        e_ln = self.mha_ln_e(e)
-        QKV = self.QKV(h_ln)
-        E = self.E(e_ln)
-        G = self.G(e_ln)
-        shp = QKV.shape
-        Q, K, V = QKV.view(shp[0], shp[1], -1, self.num_heads).split(
+        nfeat_ln = self.mha_ln_h(nfeat)
+        efeat_ln = self.mha_ln_e(efeat)
+        qkv = self.qkv_proj(nfeat_ln)
+        e_bias = self.edge_input(efeat_ln)
+        gates = self.gate(efeat_ln)
+        bsz, N, _ = qkv.shape
+        q_h, k_h, v_h = qkv.view(bsz, N, -1, self.num_heads).split(
             self.dot_dim, dim=2
         )
-        A_hat = torch.einsum("bldh,bmdh->blmh", Q, K)
-        H_hat = A_hat.clamp(-5, 5) + E
+        attn_hat = torch.einsum("bldh,bmdh->blmh", q_h, k_h)
+        attn_hat = attn_hat.clamp(-5, 5) + e_bias
 
         if mask is None:
-            gates = torch.sigmoid(G)
-            A_tild = F.softmax(H_hat, dim=2) * gates
+            gates = torch.sigmoid(gates)
+            attn_tild = F.softmax(attn_hat, dim=2) * gates
         else:
-            gates = torch.sigmoid(G + mask.unsqueeze(-1))
-            A_tild = F.softmax(H_hat + mask.unsqueeze(-1), dim=2) * gates
+            gates = torch.sigmoid(gates + mask.unsqueeze(-1))
+            attn_tild = F.softmax(attn_hat + mask.unsqueeze(-1), dim=2) * gates
 
-        A_tild = self.attn_dropout(A_tild)
-        V_attn = torch.einsum("blmh,bmkh->blkh", A_tild, V)
+        attn_tild = self.attn_dropout(attn_tild)
+        v_attn = torch.einsum("blmh,bmkh->blkh", attn_tild, v_h)
 
         # Scale the aggregated values by degree.
         degrees = torch.sum(gates, dim=2, keepdim=True)
         degree_scalers = torch.log(1 + degrees)
-        degree_scalers[:, : self.num_vns] = 1.0
-        V_attn = V_attn * degree_scalers
+        degree_scalers[:, : self.num_virtual_nodes] = 1.0
+        v_attn = v_attn * degree_scalers
 
-        V_attn = V_attn.reshape(shp[0], shp[1], self.num_heads * self.dot_dim)
-        h = self.O_h(V_attn)
+        v_attn = v_attn.reshape(bsz, N, self.num_heads * self.dot_dim)
+        nfeat = self.node_output(v_attn)
 
-        h = self.mha_dropout_h(h)
-        h.add_(h_r1)
-        h_r2 = h
-        h = self.node_ffn(h)
-        h.add_(h_r2)
+        nfeat = self.mha_dropout_h(nfeat)
+        nfeat.add_(nfeat_r1)
+        nfeat_r2 = nfeat
+        nfeat = self.node_ffn(nfeat)
+        nfeat.add_(nfeat_r2)
 
         if self.edge_update:
-            e = self.O_e(H_hat)
-            e = self.mha_dropout_e(e)
-            e.add_(e_r1)
-            e_r2 = e
-            e = self.edge_ffn(e)
-            e.add_(e_r2)
+            efeat = self.edge_output(attn_hat)
+            efeat = self.mha_dropout_e(efeat)
+            efeat.add_(efeat_r1)
+            efeat_r2 = efeat
+            efeat = self.edge_ffn(efeat)
+            efeat.add_(efeat_r2)
 
-        return h, e
+            return nfeat, efeat
+
+        return nfeat
 
 
 class VirtualNodes(nn.Module):
-    def __init__(self, node_width, edge_width, num_virtual_nodes = 1):
+    """
+    Generate node and edge features for virtual nodes in the graph
+    and pad the corresponding matrices.
+    """
+
+    def __init__(self, feat_size, edge_feat_size, num_virtual_nodes = 1):
         super().__init__()
-        self.node_width = node_width
-        self.edge_width = edge_width
+        self.feat_size = feat_size
+        self.edge_feat_size = edge_feat_size
         self.num_virtual_nodes = num_virtual_nodes
 
         self.vn_node_embeddings = nn.Parameter(torch.empty(num_virtual_nodes,
-                                                           self.node_width))
+                                                           self.feat_size))
         self.vn_edge_embeddings = nn.Parameter(torch.empty(num_virtual_nodes,
-                                                           self.edge_width))
+                                                           self.edge_feat_size))
         nn.init.normal_(self.vn_node_embeddings)
         nn.init.normal_(self.vn_edge_embeddings)
     
@@ -249,58 +253,64 @@ class EGTPreTrainedModel(PreTrainedModel):
 
 
 class EGTModel(EGTPreTrainedModel):
+    """The EGT model is a graph-encoder model.
+
+    It goes from a graph to its representation. If you want to use the model for a downstream classification task, use
+    EGTForGraphClassification instead. For any other downstream task, feel free to add a new class, or combine
+    this model with a downstream model of your choice, following the example in EGTForGraphClassification.
+    """
     def __init__(self, config: EGTConfig):
         super().__init__(config)
 
         self.layer_common_kwargs = dict(
-             ndim          = config.node_width            ,
-             edim          = config.edge_width            ,
+             feat_size          = config.feat_size            ,
+             edge_feat_size          = config.edge_feat_size           ,
              num_heads           = config.num_heads             ,
-             num_vns  = config.num_virtual_nodes,
+             num_virtual_nodes  = config.num_virtual_nodes,
              dropout    = config.dropout      ,
              attn_dropout        = config.attn_dropout          ,
              activation          = config.activation            ,
-             ffn_multiplier = config.ffn_multiplier   ,
         )        
+        self.edge_update = not config.egt_simple
         
         self.EGT_layers = nn.ModuleList([EGTLayer(**self.layer_common_kwargs, 
-                                                   edge_update=(not config.egt_simple))
-                                         for _ in range(config.model_height-1)])
+                                                   edge_update=self.edge_update)
+                                         for _ in range(config.num_layers-1)])
     
         self.EGT_layers.append(EGTLayer(**self.layer_common_kwargs, edge_update = False))
 
         self.upto_hop          = config.upto_hop
         self.num_virtual_nodes = config.num_virtual_nodes
-        self.svd_encodings     = config.svd_encodings
+        self.svd_pe_size     = config.svd_pe_size
         
         self.nodef_embed = nn.Embedding(NUM_NODE_FEATURES*NODE_FEATURES_OFFSET+1,
-                                        config.node_width, padding_idx=0)
-        if self.svd_encodings:
-            self.svd_embed = nn.Linear(self.svd_encodings*2, config.node_width)
+                                        config.feat_size, padding_idx=0)
+        if self.svd_pe_size:
+            self.svd_embed = nn.Linear(self.svd_pe_size*2, config.feat_size)
         
-        self.dist_embed = nn.Embedding(self.upto_hop+2, config.edge_width)
+        self.dist_embed = nn.Embedding(self.upto_hop+2, config.edge_feat_size)
         self.featm_embed = nn.Embedding(NUM_EDGE_FEATURES*EDGE_FEATURES_OFFSET+1,
-                                        config.edge_width, padding_idx=0)
+                                        config.edge_feat_size, padding_idx=0)
         
         if self.num_virtual_nodes > 0:
-            self.vn_layer = VirtualNodes(config.node_width, config.edge_width, 
+            self.vn_layer = VirtualNodes(config.feat_size, config.edge_feat_size, 
                                          self.num_virtual_nodes)
         
-        self.final_ln_h = nn.LayerNorm(config.node_width)
-        mlp_dims = [config.node_width * max(self.num_virtual_nodes, 1)]\
-                    +[round(config.node_width*r) for r in config.mlp_ratios]\
-                        +[config.output_dim]
+        self.final_ln_h = nn.LayerNorm(config.feat_size)
+        mlp_dims = [config.feat_size * max(self.num_virtual_nodes, 1)]\
+                    +[round(config.feat_size*r) for r in config.mlp_ratios]\
+                        +[config.num_classes]
         self.mlp_layers = nn.ModuleList([nn.Linear(mlp_dims[i],mlp_dims[i+1])
                                          for i in range(len(mlp_dims)-1)])
         self.mlp_fn = config.activation
      
-    def input_block(self, nodef, featm, dm, nodem, svd_encodings):
+    def input_block(self, nodef, featm, dm, nodem, svd_pe_size):
         dm = dm.long().clamp(min=0, max=self.upto_hop+1)  # (b,i,j)
         
         h = self.nodef_embed(nodef).sum(dim=2)      # (b,i,w,h) -> (b,i,h)
         
-        if self.svd_encodings:
-            h = h + self.svd_embed(svd_encodings)
+        if self.svd_pe_size:
+            h = h + self.svd_embed(svd_pe_size)
         
         e = self.dist_embed(dm)\
               + self.featm_embed(featm).sum(dim=3)  # (b,i,j,f,e) -> (b,i,j,e)
@@ -332,14 +342,19 @@ class EGTModel(EGTPreTrainedModel):
         featm: torch.LongTensor,
         dm: torch.LongTensor,
         attn_mask: torch.LongTensor,
-        svd_encodings: torch.Tensor,
+        svd_pe_size: torch.Tensor,
         return_dict: Optional[bool] = None,
         **unused,
     ) -> torch.Tensor:
-        h, e, mask = self.input_block(node_feat, featm, dm, attn_mask, svd_encodings)
-        
-        for layer in self.EGT_layers:
-            h, e = layer(h, e, mask)
+        h, e, mask = self.input_block(node_feat, featm, dm, attn_mask, svd_pe_size)
+
+        for layer in self.EGT_layers[:-1]:
+            if self.edge_update:
+                h, e = layer(h, e, mask)
+            else:
+                h = layer(h, e, mask)
+
+        h = self.EGT_layers[-1](h, e, mask)
         
         h = self.final_embedding(h, attn_mask)
         
@@ -365,7 +380,7 @@ class EGTForGraphClassification(EGTPreTrainedModel):
     def __init__(self, config: EGTConfig):
         super().__init__(config)
         self.model = EGTModel(config)
-        self.num_classes = config.output_dim
+        self.num_classes = config.num_classes
 
     def forward(
         self,
