300a2b38d2adc04bb3c605850d86423855a32e64
Arthur Zucker <arthur.zucker@gmail.com>
2023-08-17 15:30:19 +0000

    update

--- src/transformers/models/bloom/tokenization_bloom_fast.py
+++ src/transformers/models/bloom/tokenization_bloom_fast.py
@@ -128,12 +128,15 @@ class BloomTokenizerFast(PreTrainedTokenizerFast):
             clean_up_tokenization_spaces=clean_up_tokenization_spaces,
             **kwargs,
         )
-        # overwride add_prefix_space
         pre_tok_state = pickle.dumps(self.backend_tokenizer.pre_tokenizer)
+        decoder_state = pickle.dumps(self.backend_tokenizer.decoder)
+
         if add_prefix_space:
-            pre_tok_state = pre_tok_state.replace(b'"add_prefix_space":false',b'"add_prefix_space": true')
-            # TODO decoder should also be udpated to reflect this
+            pre_tok_state = pre_tok_state.replace(b'"add_prefix_space":false', b'"add_prefix_space": true')
+            decoder_state = pre_tok_state.replace(b'"add_prefix_space":false', b'"add_prefix_space": true')
         self.backend_tokenizer.pre_tokenizer = pickle.loads(pre_tok_state)
+        self.backend_tokenizer.decoder = pickle.loads(decoder_state)
+
         self.add_prefix_space = add_prefix_space
 
     def _batch_encode_plus(self, *args, **kwargs) -> BatchEncoding:
