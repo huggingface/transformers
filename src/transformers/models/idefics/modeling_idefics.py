46fa4b0a1066a9749dfcac9526d59daf1cdcb765
leot13 <leo.tronchon@gmail.com>
2023-08-18 13:12:29 +0200

    fix * + indentation + style

--- src/transformers/models/idefics/modeling_idefics.py
+++ src/transformers/models/idefics/modeling_idefics.py
@@ -104,31 +104,31 @@ class IdeficsCausalLMOutputWithPast(ModelOutput):
     Base class for causal language model (or autoregressive) outputs.
 
     Args:
-        loss (`torch.FloatTensor` of shape `(1,)`, *optional*, returned when `labels` is provided):
+        loss (`torch.FloatTensor` of shape `(1,)`, `optional`, returned when `labels` is provided):
             Language modeling loss (for next-token prediction).
         logits (`torch.FloatTensor` of shape `(batch_size, sequence_length, config.vocab_size)`):
             Prediction scores of the language modeling head (scores for each vocabulary token before SoftMax).
         past_key_values (:
-            obj:*List[torch.FloatTensor]*, *optional*, returned when `use_cache=True` is passed or when
+            obj:`List[torch.FloatTensor]`, `optional`, returned when `use_cache=True` is passed or when
             `config.use_cache=True`): List of `torch.FloatTensor` of length `config.n_layers`, with each tensor of
             shape `(2, batch_size, num_heads, sequence_length, embed_size_per_head)`).
 
             Contains pre-computed hidden-states (key and values in the attention blocks) that can be used (see
             `past_key_values` input) to speed up sequential decoding.
         hidden_states (:
-            obj:*tuple(torch.FloatTensor)*, *optional*, returned when `output_hidden_states=True` is passed or when
+            obj:`tuple(torch.FloatTensor)`, `optional`, returned when `output_hidden_states=True` is passed or when
             `config.output_hidden_states=True`): Tuple of `torch.FloatTensor` (one for the output of the embeddings +
             one for the output of each layer) of shape `(batch_size, sequence_length, hidden_size)`.
 
             Hidden-states of the model at the output of each layer plus the initial embedding outputs.
         attentions (:
-            obj:*tuple(torch.FloatTensor)*, *optional*, returned when `output_attentions=True` is passed or when
+            obj:`tuple(torch.FloatTensor)`, `optional`, returned when `output_attentions=True` is passed or when
             `config.output_attentions=True`): Tuple of `torch.FloatTensor` (one for each layer) of shape `(batch_size,
             num_heads, sequence_length, sequence_length)`.
 
             Attentions weights after the attention softmax, used to compute the weighted average in the self-attention
             heads.
-        image_hidden_states (`tuple(torch.FloatTensor)`, *optional*):
+        image_hidden_states (`tuple(torch.FloatTensor)`, `optional`):
             Tuple of `torch.FloatTensor` (one for the output of the image embeddings, `(batch_size, num_images,
             sequence_length, hidden_size)`.
 
@@ -1210,7 +1210,7 @@ class IdeficsModel(IdeficsPreTrainedModel):
             position_ids = position_ids.view(-1, seq_length).long()
 
         no_images = False
-if (pixel_values, image_encoder_embeddings, perceiver_embeddings).count(None) != 2:
+        if (pixel_values, image_encoder_embeddings, perceiver_embeddings).count(None) != 2:
             raise ValueError(
                 "Exactly 1 of pixel_values, image_encoder_embeddings or perceiver_embeddings has to be not-None."
             )
