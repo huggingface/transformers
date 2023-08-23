5812bda55185e9ff7ae8e99814056b3856d9670e
Joao Gante <joao@huggingface.co>
2023-08-23 17:25:55 +0000

    shorter example

--- src/transformers/generation/logits_process.py
+++ src/transformers/generation/logits_process.py
@@ -1017,39 +1017,29 @@ class NoBadWordsLogitsProcessor(SequenceBiasLogitsProcessor):
     >>> tokenizer = AutoTokenizer.from_pretrained("gpt2")
     >>> inputs = tokenizer(["In a word, the cake is a"], return_tensors="pt")
 
-    >>> summary_ids = model.generate(inputs["input_ids"], max_new_tokens=5, pad_token_id=tokenizer.eos_token_id)
-    >>> print(tokenizer.batch_decode(summary_ids, skip_special_tokens=True)[0])
+    >>> output_ids = model.generate(inputs["input_ids"], max_new_tokens=5, pad_token_id=tokenizer.eos_token_id)
+    >>> print(tokenizer.batch_decode(output_ids, skip_special_tokens=True)[0])
     In a word, the cake is a bit of a mess.
 
-    >>> # Now let's control generation taking the bad words out. Please note that the tokenizer is initialized differently
-
+    >>> # Now let's take the bad words out. Please note that the tokenizer is initialized differently
     >>> tokenizer_with_prefix_space = AutoTokenizer.from_pretrained("gpt2", add_prefix_space=True)
 
 
     >>> def get_tokens_as_list(word_list):
     ...     "Converts a sequence of words into a list of tokens"
     ...     tokens_list = []
-    ...     for word in word_list.split(" "):
+    ...     for word in word_list:
     ...         tokenized_word = tokenizer_with_prefix_space([word], add_special_tokens=False).input_ids[0]
     ...         tokens_list.append(tokenized_word)
     ...     return tokens_list
 
 
-    >>> word_list = "mess"
-    >>> bad_words_ids = get_tokens_as_list(word_list=word_list)
-
-    >>> badwords_ids = model.generate(
-    ...     inputs["input_ids"],
-    ...     max_new_tokens=5,
-    ...     bad_words_ids=bad_words_ids,
-    ...     eos_token_id=tokenizer_with_prefix_space.eos_token_id,
+    >>> bad_words_ids = get_tokens_as_list(word_list=["mess"])
+    >>> output_ids = model.generate(
+    ...     inputs["input_ids"], max_new_tokens=5, bad_words_ids=bad_words_ids, pad_token_id=tokenizer.eos_token_id
     ... )
-    >>> print(tokenizer.batch_decode(badwords_ids, skip_special_tokens=True)[0])
+    >>> print(tokenizer.batch_decode(output_ids, skip_special_tokens=True)[0])
     In a word, the cake is a bit of a surprise.
-
-    >>> badwords_ids = model.generate(inputs["input_ids"], max_new_tokens=4, num_beams=5, bad_words_ids=bad_words_ids)
-    >>> print(tokenizer.batch_decode(biased_ids, skip_special_tokens=True)[0])
-    In a word, the cake is a great way to start
     ```
     """
 
