2cc55fb56f82f837acf153a618104a378994810f
amyeroberts <22614925+amyeroberts@users.noreply.github.com>
2023-08-24 17:24:36 +0100

    ImageProcessor - check if input pixel values between 0-255 (#25688)
    * Check if pixel values between 0-255 and add doc clarification
    
    * Add missing docstrings
    
    * _is_scale_image -> is_scaled_image
    
    * Spelling is hard
    
    * Tidy up

--- src/transformers/models/mask2former/image_processing_mask2former.py
+++ src/transformers/models/mask2former/image_processing_mask2former.py
@@ -36,6 +36,7 @@ from ...image_utils import (
     get_image_size,
     infer_channel_dimension_format,
     is_batched,
+    is_scaled_image,
     to_numpy_array,
     valid_images,
 )
@@ -606,6 +607,11 @@ class Mask2FormerImageProcessor(BaseImageProcessor):
         """Preprocesses a single image."""
         # All transformations expect numpy arrays.
         image = to_numpy_array(image)
+        if is_scaled_image(image) and do_rescale:
+            logger.warning_once(
+                "It looks like you are trying to rescale already rescaled images. If the input"
+                " images have pixel values between 0 and 1, set `do_rescale=False` to avoid rescaling them again."
+            )
         if input_data_format is None:
             input_data_format = infer_channel_dimension_format(image)
         image = self._preprocess(
