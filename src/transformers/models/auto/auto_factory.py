2febd506149d039b51590f5dc7b45f0d8624819d
Sylvain Gugger <35901082+sgugger@users.noreply.github.com>
2023-08-24 14:56:11 +0200

    Fix number of minimal calls to the Hub with peft integration (#25715)
    * Fix number of minimal calls to the Hub with peft integration
    
    * Alternate design
    
    * And this way?
    
    * Revert
    
    * Address comments

--- src/transformers/models/auto/auto_factory.py
+++ src/transformers/models/auto/auto_factory.py
@@ -22,7 +22,16 @@ from collections import OrderedDict
 
 from ...configuration_utils import PretrainedConfig
 from ...dynamic_module_utils import get_class_from_dynamic_module, resolve_trust_remote_code
-from ...utils import copy_func, find_adapter_config_file, is_peft_available, logging, requires_backends
+from ...utils import (
+    CONFIG_NAME,
+    cached_file,
+    copy_func,
+    extract_commit_hash,
+    find_adapter_config_file,
+    is_peft_available,
+    logging,
+    requires_backends,
+)
 from .configuration_auto import AutoConfig, model_type_to_module_name, replace_list_option_in_docstrings
 
 
@@ -443,7 +452,6 @@ class _BaseAutoModelClass:
         kwargs["_from_auto"] = True
         hub_kwargs_names = [
             "cache_dir",
-            "code_revision",
             "force_download",
             "local_files_only",
             "proxies",
@@ -454,6 +462,8 @@ class _BaseAutoModelClass:
             "token",
         ]
         hub_kwargs = {name: kwargs.pop(name) for name in hub_kwargs_names if name in kwargs}
+        code_revision = kwargs.pop("code_revision", None)
+        commit_hash = kwargs.pop("_commit_hash", None)
 
         token = hub_kwargs.pop("token", None)
         use_auth_token = hub_kwargs.pop("use_auth_token", None)
@@ -470,12 +480,23 @@ class _BaseAutoModelClass:
         if token is not None:
             hub_kwargs["token"] = token
 
-        if is_peft_available():
-            revision = kwargs.get("revision", None)
-            subfolder = kwargs.get("subfolder", None)
+        if commit_hash is None:
+            if not isinstance(config, PretrainedConfig):
+                # We make a call to the config file first (which may be absent) to get the commit hash as soon as possible
+                resolved_config_file = cached_file(
+                    pretrained_model_name_or_path,
+                    CONFIG_NAME,
+                    _raise_exceptions_for_missing_entries=False,
+                    _raise_exceptions_for_connection_errors=False,
+                    **hub_kwargs,
+                )
+                commit_hash = extract_commit_hash(resolved_config_file, commit_hash)
+            else:
+                commit_hash = getattr(config, "_commit_hash", None)
 
+        if is_peft_available():
             maybe_adapter_path = find_adapter_config_file(
-                pretrained_model_name_or_path, revision=revision, token=token, subfolder=subfolder
+                pretrained_model_name_or_path, _commit_hash=commit_hash, **hub_kwargs
             )
 
             if maybe_adapter_path is not None:
@@ -499,6 +520,8 @@ class _BaseAutoModelClass:
                 pretrained_model_name_or_path,
                 return_unused_kwargs=True,
                 trust_remote_code=trust_remote_code,
+                code_revision=code_revision,
+                _commit_hash=commit_hash,
                 **hub_kwargs,
                 **kwargs,
             )
@@ -517,7 +540,7 @@ class _BaseAutoModelClass:
         if has_remote_code and trust_remote_code:
             class_ref = config.auto_map[cls.__name__]
             model_class = get_class_from_dynamic_module(
-                class_ref, pretrained_model_name_or_path, **hub_kwargs, **kwargs
+                class_ref, pretrained_model_name_or_path, code_revision=code_revision, **hub_kwargs, **kwargs
             )
             _ = hub_kwargs.pop("code_revision", None)
             if os.path.isdir(pretrained_model_name_or_path):
