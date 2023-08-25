015f8e110d270a0ad42de4ae5b98198d69eb1964
Arthur <48595927+ArthurZucker@users.noreply.github.com>
2023-08-25 18:57:40 +0200

    [`CodeLlama`] Add support for `CodeLlama` (#25740)
    * add all
    
    * Revert "Delete .github directory"
    
    This reverts commit 9b0ff7b052e2b20b629a26fb13606b78a42944d1.
    
    * make conversion script backward compatible
    
    * fixup
    
    * more styling
    
    * copy to llama changes
    
    * fix repo consistency
    
    * nits
    
    * document correct classes
    
    * updates
    
    * more fixes
    
    * nits
    
    * update auto mappings
    
    * add readmes
    
    * smallupdates
    
    * llama-code replace with llama_code
    
    * make fixup
    
    * updates to the testsing suite
    
    * fix fast nits
    
    * more small fixes
    
    * fix decode
    
    * fix template processing
    
    * properly reset the normalizer
    
    * nits processor
    
    * tokenization tests pass
    
    * styling
    
    * last tests
    
    * additional nits
    
    * one test is left
    
    * nits
    
    Co-authored-by faabian <faabian@users.noreply.github.com>
    
    * update failing test
    
    * fixup
    
    * remove decode infilling users should handle it on their onw after generation, padding can be a problem
    
    * update
    
    * make test slow and more meaningfull
    
    * fixup
    
    * doc update
    
    * fixup
    
    * Apply suggestions from code review
    
    * add kwargs doc
    
    * tokenizer requires `requires_backend`
    
    * type requires_backends
    
    * CodeLlama instead of LlamaCode
    
    * more name cahnges
    
    * nits
    
    * make doctests happy
    
    * small pipeline nits
    
    * last nit
    
    * Apply suggestions from code review
    
    Co-authored-by: Sylvain Gugger <35901082+sgugger@users.noreply.github.com>
    
    * update
    
    * add codellama to toctree
    
    ---------
    
    Co-authored-by: Sylvain Gugger <35901082+sgugger@users.noreply.github.com>

--- src/transformers/models/deprecated/open_llama/modeling_open_llama.py
+++ src/transformers/models/deprecated/open_llama/modeling_open_llama.py
@@ -246,18 +246,26 @@ class OpenLlamaAttention(nn.Module):
     def _init_rope(self):
         if self.config.rope_scaling is None:
             self.rotary_emb = OpenLlamaRotaryEmbedding(
-                self.head_dim, max_position_embeddings=self.max_position_embeddings
+                self.head_dim,
+                max_position_embeddings=self.max_position_embeddings,
+                base=self.rope_theta,
             )
         else:
             scaling_type = self.config.rope_scaling["type"]
             scaling_factor = self.config.rope_scaling["factor"]
             if scaling_type == "linear":
                 self.rotary_emb = OpenLlamaLinearScalingRotaryEmbedding(
-                    self.head_dim, max_position_embeddings=self.max_position_embeddings, scaling_factor=scaling_factor
+                    self.head_dim,
+                    max_position_embeddings=self.max_position_embeddings,
+                    scaling_factor=scaling_factor,
+                    base=self.rope_theta,
                 )
             elif scaling_type == "dynamic":
                 self.rotary_emb = OpenLlamaDynamicNTKScalingRotaryEmbedding(
-                    self.head_dim, max_position_embeddings=self.max_position_embeddings, scaling_factor=scaling_factor
+                    self.head_dim,
+                    max_position_embeddings=self.max_position_embeddings,
+                    scaling_factor=scaling_factor,
+                    base=self.rope_theta,
                 )
             else:
                 raise ValueError(f"Unknown RoPE scaling type {scaling_type}")
