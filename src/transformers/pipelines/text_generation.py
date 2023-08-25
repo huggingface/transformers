45f838e57934b2bd6306be9b654a71acf33b4fde
Arthur Zucker <arthur.zucker@gmail.com>
2023-08-25 15:14:56 +0000

    last nit

--- src/transformers/pipelines/text_generation.py
+++ src/transformers/pipelines/text_generation.py
@@ -283,6 +283,13 @@ class TextGenerationPipeline(Pipeline):
             if return_type == ReturnType.TENSORS:
                 record = {"generated_token_ids": sequence}
             elif return_type in {ReturnType.NEW_TEXT, ReturnType.FULL_TEXT}:
+                # Decode text
+                text = self.tokenizer.decode(
+                    sequence,
+                    skip_special_tokens=True,
+                    clean_up_tokenization_spaces=clean_up_tokenization_spaces,
+                )
+
                 # Remove PADDING prompt of the sequence if XLNet or Transfo-XL model is used
                 if input_ids is None:
                     prompt_length = 0
