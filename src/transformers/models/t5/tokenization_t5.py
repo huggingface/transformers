f5dc0cc31524c4ec2d60fcb00b5eed52a02c80b7
Arthur <48595927+ArthurZucker@users.noreply.github.com>
2023-08-25 14:41:04 +0200

    [`Sentencepiece`] make sure `legacy` do not require `protobuf` (#25684)
    make sure legacy does not require `protobuf`

--- src/transformers/models/t5/tokenization_t5.py
+++ src/transformers/models/t5/tokenization_t5.py
@@ -195,14 +195,17 @@ class T5Tokenizer(PreTrainedTokenizer):
 
     def get_spm_processor(self):
         tokenizer = spm.SentencePieceProcessor(**self.sp_model_kwargs)
+        if self.legacy:  # no dependency on protobuf
+            tokenizer.Load(self.vocab_file)
+            return tokenizer
+
         with open(self.vocab_file, "rb") as f:
             sp_model = f.read()
-            model_pb2 = import_protobuf()
+            model_pb2 = import_protobuf(f"The new behaviour of {self.__class__.__name__} (with `self.legacy = False`)")
             model = model_pb2.ModelProto.FromString(sp_model)
-            if not self.legacy:
-                normalizer_spec = model_pb2.NormalizerSpec()
-                normalizer_spec.add_dummy_prefix = False
-                model.normalizer_spec.MergeFrom(normalizer_spec)
+            normalizer_spec = model_pb2.NormalizerSpec()
+            normalizer_spec.add_dummy_prefix = False
+            model.normalizer_spec.MergeFrom(normalizer_spec)
             sp_model = model.SerializeToString()
             tokenizer.LoadFromSerializedProto(sp_model)
         return tokenizer
