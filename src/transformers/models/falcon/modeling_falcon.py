58848ab3cb47b172d096968f8f3244968dc2f161
younesbelkada <younesbelkada@gmail.com>
2023-09-04 10:01:09 +0000

    stype

--- src/transformers/models/falcon/modeling_falcon.py
+++ src/transformers/models/falcon/modeling_falcon.py
@@ -640,7 +640,9 @@ class FalconDecoderLayer(nn.Module):
         hidden_size = config.hidden_size
         self.num_heads = config.num_attention_heads
         self.self_attention = (
-            FalconAttention(config) if not getattr(config, "_use_flash_attn_2", False) else FalconFlashAttention(config)
+            FalconAttention(config)
+            if not getattr(config, "_use_flash_attn_2", False)
+            else FalconFlashAttention(config)
         )
         self.mlp = FalconMLP(config)
         self.hidden_dropout = config.hidden_dropout
