32359d8c780113d01ab54c8924043c714e98d4d5
Lysandre <lysandre@huggingface.co>
2023-09-06 07:04:20 -0400

    Cleaner

--- src/transformers/models/auto/configuration_auto.py
+++ src/transformers/models/auto/configuration_auto.py
@@ -1016,10 +1016,9 @@ class AutoConfig:
         kwargs["name_or_path"] = pretrained_model_name_or_path
         trust_remote_code = kwargs.pop("trust_remote_code", None)
         code_revision = kwargs.pop("code_revision", None)
-        revision = kwargs.pop("revision", None)
 
-        revision = sanitize_code_revision(pretrained_model_name_or_path, revision, trust_remote_code)
-        kwargs["revision"] = revision
+        revision = kwargs.pop("revision", None)
+        kwargs["revision"] = sanitize_code_revision(pretrained_model_name_or_path, revision, trust_remote_code)
 
         config_dict, unused_kwargs = PretrainedConfig.get_config_dict(pretrained_model_name_or_path, **kwargs)
         has_remote_code = "auto_map" in config_dict and "AutoConfig" in config_dict["auto_map"]
