49146c8cdc7177fce932410eccd6ce4aaf019c85
dumpmemory <64742282+dumpmemory@users.noreply.github.com>
2023-09-06 19:27:03 +0800

    Update src/transformers/trainer_pt_utils.py
    thanks @amyeroberts
    
    Co-authored-by: amyeroberts <22614925+amyeroberts@users.noreply.github.com>

--- src/transformers/trainer_pt_utils.py
+++ src/transformers/trainer_pt_utils.py
@@ -56,14 +56,10 @@ logger = logging.get_logger(__name__)
 
 
 def get_dataloader_sampler(dataloader):
-    # after accelerate.prepare function the wraped dataloader.sampler will be SequentialSampler instead of RandomSampler
-    if hasattr(dataloader, "sampler") and isinstance(dataloader.sampler, RandomSampler):
-        return dataloader.sampler
-    elif hasattr(dataloader, "batch_sampler") and dataloader.batch_sampler is not None:
+    if hasattr(dataloader, "batch_sampler") and dataloader.batch_sampler is not None:
         return get_dataloader_sampler(dataloader.batch_sampler)
     elif hasattr(dataloader, "sampler"):
         return dataloader.sampler
-    return None
 
 
 def atleast_1d(tensor_or_array: Union[torch.Tensor, np.ndarray]):
