2a28a11146285c8eb9ba797321f74146539ca537
Sylvain Gugger <35901082+sgugger@users.noreply.github.com>
2023-09-08 16:34:47 -0400

    Update utils/check_docstrings.py
    Co-authored-by: Arthur <48595927+ArthurZucker@users.noreply.github.com>

--- utils/check_docstrings.py
+++ utils/check_docstrings.py
@@ -1,5 +1,5 @@
 # coding=utf-8
-# Copyright 2020 The HuggingFace Inc. team.
+# Copyright 2023 The HuggingFace Inc. team.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
