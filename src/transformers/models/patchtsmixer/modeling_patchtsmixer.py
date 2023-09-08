058f8c33d7bb18963758b436a7b3449a071c44ff
Arindam Jati <arindam.jati@ibm.com>
2023-09-08 05:07:07 -0400

    functionality tests added

--- src/transformers/models/patchtsmixer/modeling_patchtsmixer.py
+++ src/transformers/models/patchtsmixer/modeling_patchtsmixer.py
@@ -1,7 +1,5 @@
 # coding=utf-8
-# Copyright (c) 2021 THUML @ Tsinghua University
-# Copyright 2023 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-# Copyright 2023 The HuggingFace Inc. team. All rights reserved.
+# Copyright 2023 IBM and HuggingFace Inc. team. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
