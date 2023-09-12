d73fac1914b4e2b9bd549d0a433814c7ab54f408
Arthur Zucker <arthur.zucker@gmail.com>
2023-09-12 21:04:54 +0000

    update

--- src/transformers/models/pegasus/tokenization_pegasus.py
+++ src/transformers/models/pegasus/tokenization_pegasus.py
@@ -157,8 +157,8 @@ class PegasusTokenizer(PreTrainedTokenizer):
             self._added_tokens_decoder[2] = AddedToken(mask_token_sent)
             self._added_tokens_decoder[3] = AddedToken(str(mask_token))
 
-        for i in range(len(additional_special_tokens_extended)+1, self.offset-1):
-            self._added_tokens_decoder[len(self._added_tokens_decoder)] = AddedToken(str(additional_special_tokens[i]))
+        for i in range(1, self.offset-1):
+            self._added_tokens_decoder[len(self._added_tokens_decoder)] = AddedToken(f"<unk_{i}>" )
 
         # update kwargs to this?
 
@@ -209,6 +209,10 @@ class PegasusTokenizer(PreTrainedTokenizer):
 
     def _convert_id_to_token(self, index: int) -> str:
         """Converts an index (integer) to a token (str) using the vocab."""
+        if index<self.offset:
+            print("Index erroring", index, self.sp_model.IdToPiece(index))
+
+            return self.sp_model.IdToPiece(index)
         token = self.sp_model.IdToPiece(index - self.offset)
         return token
 
