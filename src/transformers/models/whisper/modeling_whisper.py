95fe0f5d806ff1b981f1870f290a4d9aaa53a5d4
Joshua Lochner <admin@xenova.com>
2023-09-14 18:42:35 +0200

    [Whisper] Fix word-level timestamps for audio < 30 seconds (#25607)
    * Fix word-level timestamps for audio < 30 seconds
    
    * Fix code quality
    
    * fix unit tests
    
    * Fix unit tests
    
    * Fix unit test
    
    * temp: print out result
    
    * temp: set max diff to None
    
    * fix unit tests
    
    * fix typo
    
    * Fix typo
    
    Co-authored-by: Arthur <48595927+ArthurZucker@users.noreply.github.com>
    
    * Use generation config for `num_frames`
    
    * fix docs
    
    * Move `num_frames` to kwargs
    
    * compute stride/attn_mask once
    
    * mark test as slow
    
    ---------
    
    Co-authored-by: Arthur <48595927+ArthurZucker@users.noreply.github.com>
    Co-authored-by: sanchit-gandhi <sanchit@huggingface.co>

--- src/transformers/models/whisper/modeling_whisper.py
+++ src/transformers/models/whisper/modeling_whisper.py
@@ -1754,6 +1754,9 @@ class WhisperForConditionalGeneration(WhisperPreTrainedModel):
                     "See https://gist.github.com/hollance/42e32852f24243b748ae6bc1f985b13a on how to add this property to the generation config."
                 )
 
+            if kwargs.get("num_frames") is not None:
+                generation_config.num_frames = kwargs.pop("num_frames")
+
         outputs = super().generate(
             inputs,
             generation_config,
@@ -1765,7 +1768,10 @@ class WhisperForConditionalGeneration(WhisperPreTrainedModel):
         )
 
         if return_token_timestamps and hasattr(generation_config, "alignment_heads"):
-            outputs["token_timestamps"] = self._extract_token_timestamps(outputs, generation_config.alignment_heads)
+            num_frames = getattr(generation_config, "num_frames", None)
+            outputs["token_timestamps"] = self._extract_token_timestamps(
+                outputs, generation_config.alignment_heads, num_frames=num_frames
+            )
 
         return outputs
 
@@ -1799,10 +1805,11 @@ class WhisperForConditionalGeneration(WhisperPreTrainedModel):
             )
         return reordered_past
 
-    def _extract_token_timestamps(self, generate_outputs, alignment_heads, time_precision=0.02):
+    def _extract_token_timestamps(self, generate_outputs, alignment_heads, time_precision=0.02, num_frames=None):
         """
         Calculates token-level timestamps using the encoder-decoder cross-attentions and dynamic time-warping (DTW) to
-        map each output token to a position in the input audio.
+        map each output token to a position in the input audio. If `num_frames` is specified, the encoder-decoder
+        cross-attentions will be cropped before applying DTW.
 
         Returns:
             tensor containing the timestamps in seconds for each predicted token
@@ -1817,6 +1824,8 @@ class WhisperForConditionalGeneration(WhisperPreTrainedModel):
         # of shape (batch size, num selected, output length, input length).
         weights = torch.stack([cross_attentions[l][:, h] for l, h in alignment_heads])
         weights = weights.permute([1, 0, 2, 3])
+        if num_frames is not None:
+            weights = weights[..., : num_frames // 2]
 
         # Normalize and smoothen the weights.
         std, mean = torch.std_mean(weights, dim=-2, keepdim=True, unbiased=False)
