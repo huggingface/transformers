c3e0a1a68cc28058d3fd10eac12bff35283773c3
Sourab Mangrulkar <13534540+pacman100@users.noreply.github.com>
2023-09-15 12:31:11 +0530

    Update test_fsdp.py

--- tests/fsdp/test_fsdp.py
+++ tests/fsdp/test_fsdp.py
@@ -257,6 +257,7 @@ class TrainerIntegrationFSDP(TestCasePlus, TrainerIntegrationCommon):
 
         script = [f"{self.examples_dir_str}/pytorch/text-classification/run_glue.py"]
         cmd = launcher + script + args + fsdp_args
+        cmd = cmd.replace('"', "")
 
         # keep for quick debug
         # print(" ".join([f"\nPYTHONPATH={self.src_dir_str}"] +cmd)); die
