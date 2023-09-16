e0bab593ec22b8d641cecc479a6329f563bb5087
Félix Marty <9808326+fxmarty@users.noreply.github.com>
2023-09-16 10:07:18 +0200

    revert gpt_neox_japanese

--- src/transformers/models/gpt_neox_japanese/modeling_gpt_neox_japanese.py
+++ src/transformers/models/gpt_neox_japanese/modeling_gpt_neox_japanese.py
@@ -259,14 +259,14 @@ class RotaryEmbedding(torch.nn.Module):
         freqs = torch.einsum("i,j->ij", t, self.inv_freq)
         # Different from paper, but it uses a different permutation in order to obtain the same calculation
         emb = torch.cat((freqs, freqs), dim=-1)
-        self.cos_cached = emb.cos()
-        self.sin_cached = emb.sin()
+        self.cos_cached = emb.cos()[None, None, :, :]
+        self.sin_cached = emb.sin()[None, None, :, :]
 
     def forward(self, x, seq_len=None):
         # x: [bs, num_attention_heads, seq_len, head_size]
         if seq_len > self.max_seq_len_cached:
             self._set_cos_sin_cache(seq_len=seq_len, device=x.device)
-        return self.cos_cached[:seq_len].to(x.device), self.sin_cached[:seq_len].to(x.device)
+        return self.cos_cached[:seq_len, ...].to(x.device), self.sin_cached[:seq_len, ...].to(x.device)
 
 
 def rotate_half(x):
