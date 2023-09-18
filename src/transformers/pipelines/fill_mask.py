d1669f11c9c2c46e48a44423a95540d0dc204330
Nathan Cahill <nathan.m.cahill@gmail.com>
2023-09-18 11:49:03 -0700

    Adding tokenizer_kwargs to _sanitize_parameters

--- src/transformers/pipelines/fill_mask.py
+++ src/transformers/pipelines/fill_mask.py
@@ -93,6 +93,9 @@ class FillMaskPipeline(Pipeline):
     def preprocess(self, inputs, return_tensors=None, tokenizer_kwargs=None, **preprocess_parameters) -> Dict[str, GenericTensor]:
         if return_tensors is None:
             return_tensors = self.framework
+        if tokenizer_kwargs is None:
+            tokenizer_kwargs = {}
+
         model_inputs = self.tokenizer(inputs, return_tensors=return_tensors, **tokenizer_kwargs)
         self.ensure_exactly_one_mask_token(model_inputs)
         return model_inputs
@@ -198,9 +201,15 @@ class FillMaskPipeline(Pipeline):
         target_ids = np.array(target_ids)
         return target_ids
 
-    def _sanitize_parameters(self, top_k=None, targets=None):
-        postprocess_params = {}
+    def _sanitize_parameters(self, top_k=None, targets=None, tokenizer_kwargs=None):
 
+        preprocess_params = {}
+
+        if tokenizer_kwargs is not None:
+            preprocess_params['tokenizer_kwargs'] = tokenizer_kwargs
+
+        postprocess_params = {}
+        
         if targets is not None:
             target_ids = self.get_target_ids(targets, top_k)
             postprocess_params["target_ids"] = target_ids
@@ -208,11 +217,12 @@ class FillMaskPipeline(Pipeline):
         if top_k is not None:
             postprocess_params["top_k"] = top_k
 
+
         if self.tokenizer.mask_token_id is None:
             raise PipelineException(
                 "fill-mask", self.model.base_model_prefix, "The tokenizer does not define a `mask_token`."
             )
-        return {}, {}, postprocess_params
+        return preprocess_params, {}, postprocess_params
 
     def __call__(self, inputs, *args, **kwargs):
         """
