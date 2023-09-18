7569469111ac87d33e36161fcb3b6b1b45f6ca14
pphuc25 <phanphuc1100@gmail.com>
2023-09-19 02:19:07 +0700

    docs: rewrite some document

--- examples/pytorch/language-modeling/run_clm.py
+++ examples/pytorch/language-modeling/run_clm.py
@@ -350,11 +350,13 @@ def main():
             data_files["train"] = data_args.train_file
         if data_args.validation_file is not None:
             data_files["validation"] = data_args.validation_file
+
         extension = (
             data_args.train_file.split(".")[-1]
             if data_args.train_file is not None
             else data_args.validation_file.split(".")[-1]
         )
+
         if extension == "txt":
             extension = "text"
             dataset_args["keep_linebreaks"] = data_args.keep_linebreaks
@@ -365,6 +367,7 @@ def main():
             token=model_args.token,
             **dataset_args,
         )
+        
         # If no validation data is there, validation_split_percentage will be used to divide the dataset.
         if "validation" not in raw_datasets.keys():
             raw_datasets["validation"] = load_dataset(
