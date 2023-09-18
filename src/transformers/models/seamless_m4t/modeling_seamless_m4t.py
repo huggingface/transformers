fb85bb4b10bfe7566af0499d576e351b378e5c71
Yoach Lacombe <yoach.lacombe@gmail.com>
2023-09-18 08:51:47 +0200

    remove inputs_embeds from speechencoder

--- src/transformers/models/seamless_m4t/modeling_seamless_m4t.py
+++ src/transformers/models/seamless_m4t/modeling_seamless_m4t.py
@@ -1612,7 +1612,6 @@ class SeamlessM4TSpeechEncoder(SeamlessM4TPreTrainedModel):
     def forward(
         self,
         input_features: Optional[torch.Tensor],
-        inputs_embeds: Optional[torch.Tensor] = None,
         attention_mask: Optional[torch.Tensor] = None,
         output_attentions: Optional[bool] = None,
         output_hidden_states: Optional[bool] = None,
@@ -1624,8 +1623,6 @@ class SeamlessM4TSpeechEncoder(SeamlessM4TPreTrainedModel):
             output_hidden_states if output_hidden_states is not None else self.config.output_hidden_states
         )
         return_dict = return_dict if return_dict is not None else self.config.use_return_dict
-
-        input_features = input_features if input_features is not None else inputs_embeds
 
         if input_features is None:
             raise ValueError(
