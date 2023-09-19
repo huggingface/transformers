736133ff46bbe248af9b60c253cebec918532fb8
Sam Passaglia <8333102+passaglia@users.noreply.github.com>
2023-09-20 04:44:41 +0900

    [FIX] resize_token_embeddings (#26102)
    * fix roundup command
    
    * add test for resize_token_embeddings
    
    * Update tests/test_modeling_common.py
    
    Co-authored-by: Arthur <48595927+ArthurZucker@users.noreply.github.com>
    
    * style
    
    ---------
    
    Co-authored-by: Arthur <48595927+ArthurZucker@users.noreply.github.com>

--- tests/test_modeling_common.py
+++ tests/test_modeling_common.py
@@ -1437,6 +1437,11 @@ class ModelTesterMixin:
             model_embed = model.resize_token_embeddings(model_vocab_size + 13, pad_to_multiple_of=64)
             self.assertTrue(model_embed.weight.shape[0] // 64, 0)
 
+            # Check that resizing a model to a multiple of pad_to_multiple leads to a model of exactly that size
+            target_dimension = 128
+            model_embed = model.resize_token_embeddings(target_dimension, pad_to_multiple_of=64)
+            self.assertTrue(model_embed.weight.shape[0], target_dimension)
+
             with self.assertRaisesRegex(
                 ValueError,
                 "Asking to pad the embedding matrix to a multiple of `1.3`, which is not and integer. Please make sure to pass an integer",
