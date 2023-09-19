d0f82f401527b71cf2939b247021cc1f8b06656a
Yoach Lacombe <yoach.lacombe@gmail.com>
2023-09-19 12:35:14 +0000

    make style

--- src/transformers/convert_slow_tokenizer.py
+++ src/transformers/convert_slow_tokenizer.py
@@ -786,7 +786,8 @@ class SeamlessM4TConverter(SpmConverter):
         vocab += [(piece.piece, piece.score) for piece in proto.pieces[3:]]
         vocab += [
             # list of AddedToken, so need to get the content
-            (tok.content, 0.0) for tok in self.original_tokenizer._additional_special_tokens 
+            (tok.content, 0.0)
+            for tok in self.original_tokenizer._additional_special_tokens
         ]
         return vocab
 
