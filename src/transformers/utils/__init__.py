382ba670ed2376a9454c3c841fae4819118ec4f5
Sourab Mangrulkar <13534540+pacman100@users.noreply.github.com>
2023-09-20 10:26:16 +0530

    FSDP tests and checkpointing fixes (#26180)
    * add fsdp tests
    
    * Update test_fsdp.py
    
    * Update test_fsdp.py
    
    * fixes
    
    * checks
    
    * Update trainer.py
    
    * fix
    
    * fixes for saving/resuming checkpoints
    
    * fixes
    
    * add tests and delete debug statements
    
    * fixing tests
    
    * Update test_fsdp.py
    
    * fix tests
    
    * fix tests
    
    * minor nits
    
    * fix code style and quality
    
    * refactor and modularize test code
    
    * reduce the time of tests
    
    * reduce the test time
    
    * fix test
    
    * reduce test time
    
    * reduce test time
    
    * fix failing tests
    
    * fix
    
    * Apply suggestions from code review
    
    Co-authored-by: Arthur <48595927+ArthurZucker@users.noreply.github.com>
    
    * resolve comments
    
    ---------
    
    Co-authored-by: Arthur <48595927+ArthurZucker@users.noreply.github.com>

--- src/transformers/utils/__init__.py
+++ src/transformers/utils/__init__.py
@@ -115,6 +115,7 @@ from .import_utils import (
     is_essentia_available,
     is_faiss_available,
     is_flax_available,
+    is_fsdp_available,
     is_ftfy_available,
     is_in_notebook,
     is_ipex_available,
