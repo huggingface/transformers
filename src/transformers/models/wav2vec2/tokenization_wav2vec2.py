e67ef1fb73f49ae1eca13e82ddb50f670578f45b
Arthur <arthur.zucker@gmail.com>
2023-09-20 10:25:47 +0200

    nit

--- src/transformers/models/wav2vec2/tokenization_wav2vec2.py
+++ src/transformers/models/wav2vec2/tokenization_wav2vec2.py
@@ -232,7 +232,7 @@ class Wav2Vec2CTCTokenizer(PreTrainedTokenizer):
 
         # make sure that tokens made of several
         # characters are not split at tokenization
-        self.add_tokens([ tok for token in self.encoder.keys() if len(token)>1])
+        self.add_tokens([ token for token in self.encoder.keys() if len(token)>1])
 
 
     @property
