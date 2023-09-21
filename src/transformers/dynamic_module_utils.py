e71ab98a8470eb3e5b1ffa1bf0fa75d57298d5a9
Matt <Rocketknight1@users.noreply.github.com>
2023-09-21 15:10:31 +0100

    Update src/transformers/dynamic_module_utils.py
    Co-authored-by: Arthur <48595927+ArthurZucker@users.noreply.github.com>

--- src/transformers/dynamic_module_utils.py
+++ src/transformers/dynamic_module_utils.py
@@ -593,7 +593,7 @@ def resolve_trust_remote_code(trust_remote_code, model_name, has_local_code, has
                 signal.alarm(TIME_OUT_REMOTE_CODE)
                 while trust_remote_code is None:
                     answer = input(
-                        f"The repository for {model_name} contains custom code which must be executed to correctly\n"
+                        f"The repository for {model_name} contains custom code which must be executed to correctly"
                         f"load the model. You can inspect the repository content at https://hf.co/{model_name}.\n"
                         f"You can avoid this prompt in future by passing the argument `trust_remote_code=True`.\n\n"
                         f"Do you wish to run the custom code? [y/N] "
