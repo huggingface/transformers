209857299672bb1e56d4cf70774cdb9ce2b3b5ca
ydshieh <ydshieh@users.noreply.github.com>
2023-09-21 17:57:29 +0200

    fix docstring

--- src/transformers/models/kosmos2/processing_kosmos2.py
+++ src/transformers/models/kosmos2/processing_kosmos2.py
@@ -543,6 +543,7 @@ def extract_entities_with_patch_indices(text):
     ```python
     >>> text = "<grounding> An image of<phrase> a snowman</phrase><object><patch_index_0044><patch_index_0863></object> warming himself by<phrase> a fire</phrase><object><patch_index_0005><patch_index_0911></object>."
     >>> entities = extract_entities_with_patch_indices(text)
+    >>> entities
     [(' a snowman', (31, 41), [(44, 863)]), (' a fire', (130, 137), [(5, 911)])]
     ```"""
     # The regular expression pattern for matching the required formats
@@ -612,7 +613,7 @@ def clean_text_and_extract_entities_with_bboxes(text, num_patches_per_side=32):
     >>> text = "<grounding> An image of<phrase> a snowman</phrase><object><patch_index_0044><patch_index_0863></object> warming himself by<phrase> a fire</phrase><object><patch_index_0005><patch_index_0911></object>."
     >>> clean_text, entities = clean_text_and_extract_entities_with_bboxes(text)
     >>> clean_text
-    An image of a snowman warming himself by a fire.
+    'An image of a snowman warming himself by a fire.'
 
     >>> entities
     [('a snowman', (12, 21), [(0.390625, 0.046875, 0.984375, 0.828125)]), ('a fire', (41, 47), [(0.171875, 0.015625, 0.484375, 0.890625)])]
