6d60c2726b0fb501e691ee84b99d4992892f1766
Yun Dai <yundai424@gmail.com>
2023-09-22 12:21:59 -0700

    Update modeling_falcon.py

--- src/transformers/models/falcon/modeling_falcon.py
+++ src/transformers/models/falcon/modeling_falcon.py
@@ -907,6 +907,12 @@ class FalconModel(FalconPreTrainedModel):
 
         hidden_states = inputs_embeds
 
+        if self.gradient_checkpointing and self.training:
+            if use_cache:
+                logger.warning(
+                    "`use_cache=True` is incompatible with gradient checkpointing. Setting `use_cache=False`..."
+                )
+                use_cache = False
         presents = () if use_cache else None
         all_self_attentions = () if output_attentions else None
         all_hidden_states = () if output_hidden_states else None
@@ -944,13 +950,6 @@ class FalconModel(FalconPreTrainedModel):
                 all_hidden_states = all_hidden_states + (hidden_states,)
 
             if self.gradient_checkpointing and self.training:
-                if use_cache:
-                    logger.warning(
-                        "`use_cache=True` is incompatible with gradient checkpointing. Setting `use_cache=False`..."
-                    )
-                    use_cache = False
-                    presents = None
-
                 def create_custom_forward(module):
                     def custom_forward(*inputs):
                         # None for past_key_value
