f4873f0ea7995c027cad8a201300993bb68fb677
Omar Sanseviero <osanseviero@gmail.com>
2023-09-24 16:09:05 +0200

    Fix logging error

--- src/transformers/models/musicgen/modeling_musicgen.py
+++ src/transformers/models/musicgen/modeling_musicgen.py
@@ -1308,7 +1308,7 @@ class MusicgenForCausalLM(MusicgenPreTrainedModel):
         input_ids_seq_length = input_ids.shape[-1]
         has_default_max_length = kwargs.get("max_length") is None and generation_config.max_length is not None
         if has_default_max_length and generation_config.max_new_tokens is None and generation_config.max_length == 20:
-            logger.warning(
+            warnings.warn(
                 f"Using the model-agnostic default `max_length` (={generation_config.max_length}) "
                 "to control the generation length.  recommend setting `max_new_tokens` to control the maximum length of the generation.",
                 UserWarning,
@@ -2348,9 +2348,9 @@ class MusicgenForConditionalGeneration(PreTrainedModel):
         input_ids_seq_length = input_ids.shape[-1]
         has_default_max_length = kwargs.get("max_length") is None and generation_config.max_length is not None
         if has_default_max_length and generation_config.max_new_tokens is None:
-            logger.warning(
+            warnings.warn(
                 f"Using the model-agnostic default `max_length` (={generation_config.max_length}) "
-                "to control the generation length.  recommend setting `max_new_tokens` to control the maximum length of the generation.",
+                "to control the generation length. We recommend setting `max_new_tokens` to control the maximum length of the generation.",
                 UserWarning,
             )
         elif generation_config.max_new_tokens is not None:
