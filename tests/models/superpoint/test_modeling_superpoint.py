1e976bfe9a71398e92285829cb925d83d4d4b6b7
steven <steven.bucaille@gmail.com>
2023-09-25 23:16:39 +0200

    Added test skipping because model is not concerned

--- tests/models/superpoint/test_modeling_superpoint.py
+++ tests/models/superpoint/test_modeling_superpoint.py
@@ -142,6 +142,14 @@ class SuperPointModelTest(ModelTesterMixin, unittest.TestCase):
     def test_training_gradient_checkpointing(self):
         pass
 
+    @unittest.skip(reason="SuperPoint was not designed smaller.")
+    def test_model_is_small(self):
+        pass
+
+    @unittest.skip(reason="SuperPoint does not output any loss term in the forward pass")
+    def test_retain_grad_hidden_states_attentions(self):
+        pass
+
     def test_model(self):
         config_and_inputs = self.model_tester.prepare_config_and_inputs()
         self.model_tester.create_and_check_model(*config_and_inputs)
