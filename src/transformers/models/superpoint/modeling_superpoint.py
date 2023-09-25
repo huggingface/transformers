c2f72930dfe62eef125b180b0f307c1591169b8f
steven <steven.bucaille@gmail.com>
2023-09-25 21:33:53 +0200

    Added doc with example for InterestPointDescription

--- src/transformers/models/superpoint/modeling_superpoint.py
+++ src/transformers/models/superpoint/modeling_superpoint.py
@@ -444,6 +444,26 @@ class SuperPointModel(SuperPointPreTrainedModel):
 
 
 class SuperPointModelForInterestPointDescription(SuperPointPreTrainedModel):
+    """
+        Returns:
+
+        Examples:
+
+        ```python
+        >>> from transformers import AutoImageProcessor, AutoBackbone
+        >>> import torch
+        >>> from PIL import Image
+        >>> import requests
+
+        >>> url = "http://images.cocodataset.org/val2017/000000039769.jpg"
+        >>> image = Image.open(requests.get(url, stream=True).raw)
+
+        >>> processor = AutoImageProcessor.from_pretrained("stevenbucaille/superpoint")
+        >>> model = AutoBackbone.from_pretrained("stevenbucaille/superpoint")
+
+        >>> inputs = processor(image, return_tensors="pt")
+        >>> outputs = model(**inputs)
+        ```"""
     def __init__(self, config: SuperPointConfig):
         super().__init__(config)
 
