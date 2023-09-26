d75a510da13b308af276e7ff67da4b0d2281bfa6
Sachin Singh <sachinishu02@gmail.com>
2023-09-27 01:33:10 +0530

    Update generation_tf_utils.py

--- src/transformers/generation_tf_utils.py
+++ src/transformers/generation_tf_utils.py
@@ -14,14 +14,14 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import warnings
+import logging
 
 from .generation import TFGenerationMixin
 
 
 class TFGenerationMixin(TFGenerationMixin):
     # warning at import time
-    warnings.warn(
+    logging.warning(
         "Importing `TFGenerationMixin` from `src/transformers/generation_tf_utils.py` is deprecated and will "
         "be removed in Transformers v5. Import as `from transformers import TFGenerationMixin` instead.",
         FutureWarning,
