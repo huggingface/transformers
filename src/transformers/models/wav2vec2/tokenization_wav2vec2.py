7d62f6e331df58a9a993cc3508fb23a1ec4962fb
sanchit-gandhi <sanchit@huggingface.co>
2023-09-29 16:58:55 +0100

    revert since we need AddedTokens

--- src/transformers/models/wav2vec2/tokenization_wav2vec2.py
+++ src/transformers/models/wav2vec2/tokenization_wav2vec2.py
@@ -206,7 +206,9 @@ class Wav2Vec2CTCTokenizer(PreTrainedTokenizer):
 
         # make sure that tokens made of several
         # characters are not split at tokenization
-        self.add_tokens([token for token in self.encoder.keys() if len(token) > 1])
+        for token in self.encoder.keys():
+            if len(token) > 1:
+                self.add_tokens(AddedToken(token, rstrip=True, lstrip=True, normalized=False))
 
     def set_target_lang(self, target_lang: str):
         """
@@ -225,7 +227,9 @@ class Wav2Vec2CTCTokenizer(PreTrainedTokenizer):
 
         # make sure that tokens made of several
         # characters are not split at tokenization
-        self.add_tokens([token for token in self.encoder.keys() if len(token) > 1])
+        for token in self.encoder.keys():
+            if len(token) > 1:
+                self.add_tokens(AddedToken(token, rstrip=True, lstrip=True, normalized=False))
 
     @property
     def word_delimiter_token(self) -> str:
