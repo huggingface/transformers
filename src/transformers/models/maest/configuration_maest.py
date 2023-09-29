d68d9e460c4cc5888af8e495e7815da51016f7bc
palonso <pablo.alonso@upf.edu>
2023-09-29 20:07:44 +0200

    Add MAEST implementation
    The MAEST model is based on AST (audio_spectrogram_transformer) but it features a custom
    mel-spectrogram input representation optimized for musical purposes.
    While MAEST was trained using Essentia mel-spectrograms, this library does not support
    multi-threading, making it unsuitable for the implementation of the FeatureExtractor. For now, the
    implementation relies in Librosa, since similar mel-spectrogram configuration can be achieved,
    producing close to identical results.

---
+++ src/transformers/models/maest/configuration_maest.py
@@ -0,0 +1,124 @@
+# coding=utf-8
+# Copyright 2023 Google AI and The HuggingFace Inc. team. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+""" Audio Spectogram Transformer (MAEST) model configuration"""
+
+
+from ...configuration_utils import PretrainedConfig
+from ...utils import logging
+
+
+logger = logging.get_logger(__name__)
+
+MAEST_PRETRAINED_CONFIG_ARCHIVE_MAP = {
+    "mtg-upf/discogs-maest-30s-pw-129e": "https://huggingface.co/mtg-upf/discogs-maest-30s-pw-129e/resolve/main/config.json",
+}
+
+
+class MAESTConfig(PretrainedConfig):
+    r"""
+    This is the configuration class to store the configuration of a [`MAESTModel`]. It is used to instantiate an MAEST
+    model according to the specified arguments, defining the model architecture. Instantiating a configuration with the
+    defaults will yield a similar configuration to that of the MAEST
+    [mtg-upf/discogs-maest-30s-pw-129e](https://huggingface.co/mtg-upf/discogs-maest-30s-pw-129e)
+    architecture.
+
+    Configuration objects inherit from [`PretrainedConfig`] and can be used to control the model outputs. Read the
+    documentation from [`PretrainedConfig`] for more information.
+
+    Args:
+        hidden_size (`int`, *optional*, defaults to 768):
+            Dimensionality of the encoder layers and the pooler layer.
+        num_hidden_layers (`int`, *optional*, defaults to 12):
+            Number of hidden layers in the Transformer encoder.
+        num_attention_heads (`int`, *optional*, defaults to 12):
+            Number of attention heads for each attention layer in the Transformer encoder.
+        intermediate_size (`int`, *optional*, defaults to 3072):
+            Dimensionality of the "intermediate" (i.e., feed-forward) layer in the Transformer encoder.
+        hidden_act (`str` or `function`, *optional*, defaults to `"gelu"`):
+            The non-linear activation function (function or string) in the encoder and pooler. If string, `"gelu"`,
+            `"relu"`, `"selu"` and `"gelu_new"` are supported.
+        hidden_dropout_prob (`float`, *optional*, defaults to 0.1):
+            The dropout probability for all fully connected layers in the embeddings, encoder, and pooler.
+        attention_probs_dropout_prob (`float`, *optional*, defaults to 0.1):
+            The dropout ratio for the attention probabilities.
+        initializer_range (`float`, *optional*, defaults to 0.02):
+            The standard deviation of the truncated_normal_initializer for initializing all weight matrices.
+        layer_norm_eps (`float`, *optional*, defaults to 1e-12):
+            The epsilon used by the layer normalization layers.
+        patch_size (`int`, *optional*, defaults to `16`):
+            The size (resolution) of each patch.
+        qkv_bias (`bool`, *optional*, defaults to `True`):
+            Whether to add a bias to the queries, keys and values.
+        frequency_stride (`int`, *optional*, defaults to 10):
+            Frequency stride to use when patchifying the spectrograms.
+        time_stride (`int`, *optional*, defaults to 10):
+            Temporal stride to use when patchifying the spectrograms.
+        max_length (`int`, *optional*, defaults to 1024):
+            Temporal dimension of the spectrograms.
+        num_mel_bins (`int`, *optional*, defaults to 128):
+            Frequency dimension of the spectrograms (number of Mel-frequency bins).
+
+    Example:
+
+    ```python
+    >>> from transformers import MAESTConfig, MAESTModel
+
+    >>> # Initializing a MAEST mtg-upf/discogs-maest-30s-pw-129e style configuration
+    >>> configuration = MAESTConfig()
+
+    >>> # Initializing a model (with random weights) from the mtg-upf/discogs-maest-30s-pw-129e style configuration
+    >>> model = MAESTModel(configuration)
+
+    >>> # Accessing the model configuration
+    >>> configuration = model.config
+    ```"""
+    model_type = "maest"
+
+    def __init__(
+        self,
+        hidden_size=768,
+        num_hidden_layers=12,
+        num_attention_heads=12,
+        intermediate_size=3072,
+        hidden_act="gelu",
+        hidden_dropout_prob=0.0,
+        attention_probs_dropout_prob=0.0,
+        initializer_range=0.02,
+        layer_norm_eps=1e-6,
+        patch_size=16,
+        qkv_bias=True,
+        frequency_stride=10,
+        time_stride=10,
+        max_length=1876,
+        num_mel_bins=96,
+        **kwargs,
+    ):
+        super().__init__(**kwargs)
+
+        self.hidden_size = hidden_size
+        self.num_hidden_layers = num_hidden_layers
+        self.num_attention_heads = num_attention_heads
+        self.intermediate_size = intermediate_size
+        self.hidden_act = hidden_act
+        self.hidden_dropout_prob = hidden_dropout_prob
+        self.attention_probs_dropout_prob = attention_probs_dropout_prob
+        self.initializer_range = initializer_range
+        self.layer_norm_eps = layer_norm_eps
+        self.patch_size = patch_size
+        self.qkv_bias = qkv_bias
+        self.frequency_stride = frequency_stride
+        self.time_stride = time_stride
+        self.max_length = max_length
+        self.num_mel_bins = num_mel_bins
---
+++ src/transformers/models/maest/feature_extraction_maest.py
@@ -0,0 +1,149 @@
+# coding=utf-8
+# Copyright 2023 The HuggingFace Inc. team.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+Feature extractor class for Music Audio Efficient Spectrogram Transformer.
+"""
+
+
+import numpy as np
+import torch
+from librosa.feature import melspectrogram
+
+from ...utils import logging
+from ..audio_spectrogram_transformer import ASTFeatureExtractor
+
+
+logger = logging.get_logger(__name__)
+
+
+class MAESTFeatureExtractor(ASTFeatureExtractor):
+    r"""
+    Constructs a Music Audio Efficient Spectrogram Transformer (MAEST) feature extractor.
+
+    This feature extractor inherits from [`~feature_extraction_sequence_utils.SequenceFeatureExtractor`] which contains
+    most of the main methods. Users should refer to this superclass for more information regarding those methods.
+
+    This class extracts mel-filter bank features from raw audio, pads/truncates them to a fixed
+    length and normalizes them using a mean and standard deviation.
+
+    Args:
+        feature_size (`int`, *optional*, defaults to 1):
+            The feature dimension of the extracted features.
+        sampling_rate (`int`, *optional*, defaults to 16000):
+            The sampling rate at which the audio files should be digitalized expressed in hertz (Hz).
+        num_mel_bins (`int`, *optional*, defaults to 96):
+            Number of Mel-frequency bins.
+        max_length (`int`, *optional*, defaults to 1876):
+            Maximum length to which to pad/truncate the extracted features. Set to -1 to deactivate
+            the functionallity.
+        do_normalize (`bool`, *optional*, defaults to `True`):
+            Whether or not to normalize the log-Mel features using `mean` and `std`.
+        mean (`float`, *optional*, defaults to 2.06755686098554):
+            The mean value used to normalize the log-Mel features. Uses the Discogs20 mean by default.
+        std (`float`, *optional*, defaults to 1.268292820667291):
+            The standard deviation value used to normalize the log-Mel features. Uses the Discogs20 standard deviation
+            by default.
+        return_attention_mask (`bool`, *optional*, defaults to `False`):
+            Whether or not [`~ASTFeatureExtractor.__call__`] should return `attention_mask`.
+        n_fft (`int`, *optional*, defaults to 512):
+            Length of the FFT window.
+        hop_length (`int`, *optional*, defaults to 256):
+            Number of samples between successive frames.
+        log_compression (`str`, *optional*, defaults to `logC`):
+            Type of log compression to apply to the mel-spectrogram. Can be one of [`None`, `log`, `logC`].
+    """
+
+    model_input_names = ["input_values", "attention_mask"]
+
+    def __init__(
+        self,
+        feature_size=1,
+        sampling_rate=16000,
+        num_mel_bins=96,
+        max_length=1876,
+        padding_value=0.0,
+        do_normalize=True,
+        mean=2.06755686098554,
+        std=1.268292820667291,
+        return_attention_mask=False,
+        n_fft=512,
+        hop_length=256,
+        log_compression="logC",
+        **kwargs,
+    ):
+        super().__init__(
+            feature_size=feature_size,
+            sampling_rate=sampling_rate,
+            num_mel_bins=num_mel_bins,
+            max_length=max_length,
+            padding_value=padding_value,
+            do_normalize=True,
+            mean=mean,
+            std=std,
+            **kwargs,
+        )
+
+        self.n_fft = n_fft
+        self.hop_length = hop_length
+        self.log_compression = log_compression
+        self.num_mel_bins = num_mel_bins
+        self.max_length = max_length
+        self.do_normalize = do_normalize
+        self.mean = mean
+        self.std = std
+        self.return_attention_mask = return_attention_mask
+
+    def _extract_fbank_features(
+        self,
+        waveform: np.ndarray,
+        max_length: int,
+    ) -> np.ndarray:
+        """
+        Get mel-spectrogram features using Librosa.
+        """
+
+        melspec = melspectrogram(
+            y=waveform,
+            sr=self.sampling_rate,
+            n_fft=self.n_fft,
+            hop_length=self.hop_length,
+            n_mels=self.num_mel_bins,
+        ).T
+
+        if not self.log_compression:
+            pass
+        elif self.log_compression == "log":
+            melspec = np.log(melspec + np.finfo(float).eps)
+        elif self.log_compression == "logC":
+            melspec = np.log10(1 + melspec * 10000)
+        else:
+            raise ValueError(
+                f"`log_compression` can only be one of [None, 'log', 'logC'], but got: {self.log_compression}"
+            )
+
+        melspec = torch.Tensor(melspec)
+        n_frames = melspec.shape[0]
+
+        if max_length > 0:
+            difference = max_length - n_frames
+
+            # pad or truncate, depending on difference
+            if difference > 0:
+                pad_module = torch.nn.ZeroPad2d((0, 0, 0, difference))
+                melspec = pad_module(melspec)
+            elif difference < 0:
+                melspec = melspec[0:max_length, :]
+
+        return melspec.numpy()
