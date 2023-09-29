519e1ba1d235a0aec790b16d4e735a57ceac76a5
Pierre <pierre.le-meur@laposte.net>
2023-09-29 11:15:35 +0900

    Allow for scheduler kwargs

--- src/transformers/trainer.py
+++ src/transformers/trainer.py
@@ -1159,6 +1159,7 @@ class Trainer:
                 optimizer=self.optimizer if optimizer is None else optimizer,
                 num_warmup_steps=self.args.get_warmup_steps(num_training_steps),
                 num_training_steps=num_training_steps,
+                **self.args.lr_scheduler_kwargs,
             )
             self._created_lr_scheduler = True
         return self.lr_scheduler
