bf8391ffb163da89acefe31f080f25698e337e10
sanchit-gandhi <sanchit@huggingface.co>
2023-09-30 16:22:13 +0100

    up

--- tests/models/falcon/test_modeling_falcon.py
+++ tests/models/falcon/test_modeling_falcon.py
@@ -590,15 +590,19 @@ class FalconOverrideTest(unittest.TestCase):
 
     def test_config_with_remote_code(self):
         for supported_checkpoint in self.supported_checkpoints:
-            config = FalconConfig.from_pretrained(supported_checkpoint, trust_remote_code=True)
+            config1 = FalconConfig.from_pretrained(supported_checkpoint, trust_remote_code=True)
+            config2 = FalconConfig.from_pretrained(supported_checkpoint)
 
-            self.assertEqual(config.model_type, "falcon")
+            self.assertEqual(config1.model_type, "falcon")
+            self.assertEqual(config1.to_dict(), config2.to_dict())
 
     def test_auto_config_with_remote_code(self):
         for supported_checkpoint in self.supported_checkpoints:
-            config = AutoConfig.from_pretrained(supported_checkpoint, trust_remote_code=True)
+            config1 = AutoConfig.from_pretrained(supported_checkpoint, trust_remote_code=True)
+            config2 = FalconConfig.from_pretrained(supported_checkpoint)
 
-            self.assertEqual(config.model_type, "falcon")
+            self.assertEqual(config1.model_type, "falcon")
+            self.assertEqual(config1.to_dict(), config2.to_dict())
 
     def test_config_with_specific_revision(self):
         for supported_checkpoint in self.supported_checkpoints:
