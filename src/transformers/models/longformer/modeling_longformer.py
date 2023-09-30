781b00dd5870eee25484973be49ca686dd96eefb
archana53 <archanakutumbaka7@gmail.com>
2023-09-30 15:56:16 -0400

    Longformer with cross attention support

--- src/transformers/models/longformer/modeling_longformer.py
+++ src/transformers/models/longformer/modeling_longformer.py
@@ -493,6 +493,155 @@ class LongformerEmbeddings(nn.Module):
         )
         return position_ids.unsqueeze(0).expand(input_shape)
 
+# Copied from transformers.models.bert.modeling_bert.BertSelfAttention with Bert->Roberta->Longformer
+class LongDocumentCrossAttention(nn.Module):
+    def __init__(self, config, layer_id, position_embedding_type=None):
+        super().__init__()
+        if config.hidden_size % config.num_attention_heads != 0 and not hasattr(config, "embedding_size"):
+            raise ValueError(
+                f"The hidden size ({config.hidden_size}) is not a multiple of the number of attention "
+                f"heads ({config.num_attention_heads})"
+            )
+
+        self.num_attention_heads = config.num_attention_heads
+        self.attention_head_size = int(config.hidden_size / config.num_attention_heads)
+        self.all_head_size = self.num_attention_heads * self.attention_head_size
+
+        self.query = nn.Linear(config.hidden_size, self.all_head_size)
+        self.key = nn.Linear(config.hidden_size, self.all_head_size)
+        self.value = nn.Linear(config.hidden_size, self.all_head_size)
+
+        self.layer_id = layer_id
+        self.dropout = nn.Dropout(config.attention_probs_dropout_prob)
+        self.position_embedding_type = position_embedding_type or getattr(
+            config, "position_embedding_type", "absolute"
+        )
+        if self.position_embedding_type == "relative_key" or self.position_embedding_type == "relative_key_query":
+            self.max_position_embeddings = config.max_position_embeddings
+            self.distance_embedding = nn.Embedding(2 * config.max_position_embeddings - 1, self.attention_head_size)
+
+        self.is_decoder = False #not applicable for our case
+
+    def transpose_for_scores(self, x: torch.Tensor) -> torch.Tensor:
+        new_x_shape = x.size()[:-1] + (self.num_attention_heads, self.attention_head_size)
+        x = x.view(new_x_shape)
+        return x.permute(0, 2, 1, 3)
+
+    def forward(
+        self,
+        hidden_states: torch.Tensor,
+        attention_mask: Optional[torch.FloatTensor] = None,
+        head_mask: Optional[torch.FloatTensor] = None,
+        encoder_hidden_states: Optional[torch.FloatTensor] = None,
+        encoder_attention_mask: Optional[torch.FloatTensor] = None,
+        past_key_value: Optional[Tuple[Tuple[torch.FloatTensor]]] = None,
+        output_attentions: Optional[bool] = False,
+    ) -> Tuple[torch.Tensor]:
+        mixed_query_layer = self.query(hidden_states)
+
+        # If this is instantiated as a cross-attention module, the keys
+        # and values come from an encoder; the attention mask needs to be
+        # such that the encoder's padding tokens are not attended to.
+        is_cross_attention = encoder_hidden_states is not None
+
+        if is_cross_attention and past_key_value is not None:
+            # reuse k,v, cross_attentions
+            key_layer = past_key_value[0]
+            value_layer = past_key_value[1]
+            attention_mask = encoder_attention_mask
+        elif is_cross_attention:
+            key_layer = self.transpose_for_scores(self.key(encoder_hidden_states))
+            value_layer = self.transpose_for_scores(self.value(encoder_hidden_states))
+            attention_mask = encoder_attention_mask
+        elif past_key_value is not None:
+            key_layer = self.transpose_for_scores(self.key(hidden_states))
+            value_layer = self.transpose_for_scores(self.value(hidden_states))
+            key_layer = torch.cat([past_key_value[0], key_layer], dim=2)
+            value_layer = torch.cat([past_key_value[1], value_layer], dim=2)
+        else:
+            key_layer = self.transpose_for_scores(self.key(hidden_states))
+            value_layer = self.transpose_for_scores(self.value(hidden_states))
+
+        query_layer = self.transpose_for_scores(mixed_query_layer)
+
+        use_cache = past_key_value is not None
+        if self.is_decoder:
+            # if cross_attention save Tuple(torch.Tensor, torch.Tensor) of all cross attention key/value_states.
+            # Further calls to cross_attention layer can then reuse all cross-attention
+            # key/value_states (first "if" case)
+            # if uni-directional self-attention (decoder) save Tuple(torch.Tensor, torch.Tensor) of
+            # all previous decoder key/value_states. Further calls to uni-directional self-attention
+            # can concat previous decoder key/value_states to current projected key/value_states (third "elif" case)
+            # if encoder bi-directional self-attention `past_key_value` is always `None`
+            past_key_value = (key_layer, value_layer)
+
+        # Take the dot product between "query" and "key" to get the raw attention scores.
+        attention_scores = torch.matmul(query_layer, key_layer.transpose(-1, -2))
+
+        if self.position_embedding_type == "relative_key" or self.position_embedding_type == "relative_key_query":
+            query_length, key_length = query_layer.shape[2], key_layer.shape[2]
+            if use_cache:
+                position_ids_l = torch.tensor(key_length - 1, dtype=torch.long, device=hidden_states.device).view(
+                    -1, 1
+                )
+            else:
+                position_ids_l = torch.arange(query_length, dtype=torch.long, device=hidden_states.device).view(-1, 1)
+            position_ids_r = torch.arange(key_length, dtype=torch.long, device=hidden_states.device).view(1, -1)
+            distance = position_ids_l - position_ids_r
+
+            positional_embedding = self.distance_embedding(distance + self.max_position_embeddings - 1)
+            positional_embedding = positional_embedding.to(dtype=query_layer.dtype)  # fp16 compatibility
+
+            if self.position_embedding_type == "relative_key":
+                relative_position_scores = torch.einsum("bhld,lrd->bhlr", query_layer, positional_embedding)
+                attention_scores = attention_scores + relative_position_scores
+            elif self.position_embedding_type == "relative_key_query":
+                relative_position_scores_query = torch.einsum("bhld,lrd->bhlr", query_layer, positional_embedding)
+                relative_position_scores_key = torch.einsum("bhrd,lrd->bhlr", key_layer, positional_embedding)
+                attention_scores = attention_scores + relative_position_scores_query + relative_position_scores_key
+
+        attention_scores = attention_scores / math.sqrt(self.attention_head_size)
+        if attention_mask is not None:
+            # Apply the attention mask is (precomputed for all layers in RobertaModel forward() function)
+            attention_scores = attention_scores + attention_mask
+
+        # Normalize the attention scores to probabilities.
+        attention_probs = nn.functional.softmax(attention_scores, dim=-1)
+
+        # This is actually dropping out entire tokens to attend to, which might
+        # seem a bit unusual, but is taken from the original Transformer paper.
+        attention_probs = self.dropout(attention_probs)
+
+        # Mask heads if we want to
+        if head_mask is not None:
+            attention_probs = attention_probs * head_mask
+
+        context_layer = torch.matmul(attention_probs, value_layer)
+
+        context_layer = context_layer.permute(0, 2, 1, 3).contiguous()
+        new_context_layer_shape = context_layer.size()[:-2] + (self.all_head_size,)
+        context_layer = context_layer.view(new_context_layer_shape)
+
+        outputs = (context_layer, attention_probs) if output_attentions else (context_layer,)
+
+        if self.is_decoder:
+            outputs = outputs + (past_key_value,)
+        return outputs
+
+
+class LongDocumentCrossOutput(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.dense = nn.Linear(config.hidden_size, config.hidden_size)
+        self.LayerNorm = nn.LayerNorm(config.hidden_size, eps=config.layer_norm_eps)
+        self.dropout = nn.Dropout(config.hidden_dropout_prob)
+
+    def forward(self, hidden_states: torch.Tensor, input_tensor: torch.Tensor) -> torch.Tensor:
+        hidden_states = self.dense(hidden_states)
+        hidden_states = self.dropout(hidden_states)
+        hidden_states = self.LayerNorm(hidden_states + input_tensor)
+        return hidden_states
+
 
 class LongformerSelfAttention(nn.Module):
     def __init__(self, config, layer_id):
@@ -1129,6 +1278,7 @@ class LongformerSelfAttention(nn.Module):
         return global_attn_output, global_attn_probs
 
 
+
 # Copied from transformers.models.bert.modeling_bert.BertSelfOutput
 class LongformerSelfOutput(nn.Module):
     def __init__(self, config):
@@ -1145,10 +1295,14 @@ class LongformerSelfOutput(nn.Module):
 
 
 class LongformerAttention(nn.Module):
-    def __init__(self, config, layer_id=0):
+    def __init__(self, config, layer_id=0, cross_modality = False):
         super().__init__()
         self.self = LongformerSelfAttention(config, layer_id)
-        self.output = LongformerSelfOutput(config)
+        self.cross_modality = cross_modality
+        self.self_output = LongformerSelfOutput(config)
+        if cross_modality:
+            self.cross = LongDocumentCrossAttention(config, layer_id, position_embedding_type='')
+            self.cross_output = LongDocumentCrossOutput(config)
         self.pruned_heads = set()
 
     def prune_heads(self, heads):
@@ -1178,6 +1332,8 @@ class LongformerAttention(nn.Module):
         is_index_global_attn=None,
         is_global_attn=None,
         output_attentions=False,
+        cross_modality_inputs = None,
+        cross_modality_attention_masks = None
     ):
         self_outputs = self.self(
             hidden_states,
@@ -1188,8 +1344,25 @@ class LongformerAttention(nn.Module):
             is_global_attn=is_global_attn,
             output_attentions=output_attentions,
         )
-        attn_output = self.output(self_outputs[0], hidden_states)
+
+        attn_output = self.self_output(self_outputs[0], hidden_states)
         outputs = (attn_output,) + self_outputs[1:]
+
+
+        if self.cross_modality:
+
+            cross_outputs = self.cross(
+                hidden_states = outputs,
+                attention_mask=attention_mask,
+                layer_head_mask=layer_head_mask,
+                encoder_hidden_states = cross_modality_inputs,
+                encoder_attention_mask = cross_modality_attention_masks,
+                output_attentions=output_attentions,
+            )
+            attn_output = self.cross_output(cross_outputs[0], outputs)
+            outputs = (attn_output,) + outputs[1:]
+
+        
         return outputs
 
 
@@ -1225,9 +1398,9 @@ class LongformerOutput(nn.Module):
 
 
 class LongformerLayer(nn.Module):
-    def __init__(self, config, layer_id=0):
+    def __init__(self, config, layer_id=0, cross_modality= False):
         super().__init__()
-        self.attention = LongformerAttention(config, layer_id)
+        self.attention = LongformerAttention(config, layer_id, cross_modality=cross_modality)
         self.intermediate = LongformerIntermediate(config)
         self.output = LongformerOutput(config)
         self.chunk_size_feed_forward = config.chunk_size_feed_forward
@@ -1242,7 +1415,8 @@ class LongformerLayer(nn.Module):
         is_index_global_attn=None,
         is_global_attn=None,
         output_attentions=False,
-    ):
+        cross_modality_inputs = None,
+        cross_modality_attention_masks = None):
         self_attn_outputs = self.attention(
             hidden_states,
             attention_mask=attention_mask,
@@ -1251,6 +1425,8 @@ class LongformerLayer(nn.Module):
             is_index_global_attn=is_index_global_attn,
             is_global_attn=is_global_attn,
             output_attentions=output_attentions,
+            cross_modality_inputs = cross_modality_inputs,
+            cross_modality_attention_masks = cross_modality_attention_masks
         )
         attn_output = self_attn_outputs[0]
         outputs = self_attn_outputs[1:]
@@ -1268,10 +1444,10 @@ class LongformerLayer(nn.Module):
 
 
 class LongformerEncoder(nn.Module):
-    def __init__(self, config):
+    def __init__(self, config, cross_modality = False):
         super().__init__()
         self.config = config
-        self.layer = nn.ModuleList([LongformerLayer(config, layer_id=i) for i in range(config.num_hidden_layers)])
+        self.layer = nn.ModuleList([LongformerLayer(config, layer_id=i, cross_modality=cross_modality) for i in range(config.num_hidden_layers)])
         self.gradient_checkpointing = False
 
     def forward(
@@ -1283,6 +1459,8 @@ class LongformerEncoder(nn.Module):
         output_attentions=False,
         output_hidden_states=False,
         return_dict=True,
+        cross_modality_inputs = None,
+        cross_modality_attention_masks = None
     ):
         is_index_masked = attention_mask < 0
         is_index_global_attn = attention_mask > 0
@@ -1328,6 +1506,8 @@ class LongformerEncoder(nn.Module):
                     is_index_global_attn=is_index_global_attn,
                     is_global_attn=is_global_attn,
                     output_attentions=output_attentions,
+                    cross_modality_inputs = cross_modality_inputs,
+                    cross_modality_attention_masks = cross_modality_attention_masks
                 )
             hidden_states = layer_outputs[0]
 
@@ -1547,7 +1727,7 @@ class LongformerModel(LongformerPreTrainedModel):
 
     """
 
-    def __init__(self, config, add_pooling_layer=True):
+    def __init__(self, config, add_pooling_layer=True, cross_modality = False):
         super().__init__(config)
         self.config = config
 
@@ -1562,7 +1742,7 @@ class LongformerModel(LongformerPreTrainedModel):
             )
 
         self.embeddings = LongformerEmbeddings(config)
-        self.encoder = LongformerEncoder(config)
+        self.encoder = LongformerEncoder(config, cross_modality=cross_modality)
         self.pooler = LongformerPooler(config) if add_pooling_layer else None
 
         # Initialize weights and apply final processing
@@ -1658,6 +1838,8 @@ class LongformerModel(LongformerPreTrainedModel):
         output_attentions: Optional[bool] = None,
         output_hidden_states: Optional[bool] = None,
         return_dict: Optional[bool] = None,
+        cross_modality_inputs: Optional[torch.Tensor] = None,
+        cross_modality_attention_masks : Optional[torch.Tensor] = None
     ) -> Union[Tuple, LongformerBaseModelOutputWithPooling]:
         r"""
 
@@ -1752,6 +1934,8 @@ class LongformerModel(LongformerPreTrainedModel):
             output_attentions=output_attentions,
             output_hidden_states=output_hidden_states,
             return_dict=return_dict,
+            cross_modality_inputs = cross_modality_inputs,
+            cross_modality_attention_masks = cross_modality_attention_masks
         )
         sequence_output = encoder_outputs[0]
         pooled_output = self.pooler(sequence_output) if self.pooler is not None else None
