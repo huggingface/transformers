13f3f814e68df33e754ca3ea02b5e8e99fbff0e9
Niels <niels.rogge1@gmail.com>
2023-10-01 19:51:51 +0200

    Remove script

--- src/transformers/models/siglip/test.py
+++
@@ -1,9 +0,0 @@
-from transformers import SiglipConfig, SiglipModel
-
-
-config = SiglipConfig()
-
-model = SiglipModel(config)
-
-for name, param in model.named_parameters():
-    print(name, param.shape)
