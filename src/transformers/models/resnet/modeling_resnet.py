53668cc27dd1554710bd22822041f1a82c0354e1
Feng, Jiqing <jiqing.feng@intel.com>
2023-10-02 01:07:52 -0700

    fix param name

--- src/transformers/models/resnet/modeling_resnet.py
+++ src/transformers/models/resnet/modeling_resnet.py
@@ -123,7 +123,14 @@ class ResNetBasicLayer(nn.Module):
     A classic ResNet's residual layer composed by two `3x3` convolutions.
     """
 
-    def __init__(self, in_channels: int, out_channels: int, stride: int = 1, activation: str = "relu"):
+    def __init__(
+        self,
+        in_channels: int,
+        out_channels: int,
+        stride: int = 1,
+        activation: str = "relu",
+        downsample_in_bottleneck: bool = False,
+    ):
         super().__init__()
         should_apply_shortcut = in_channels != out_channels or stride != 1
         self.shortcut = (
@@ -159,7 +166,7 @@ class ResNetBottleNeckLayer(nn.Module):
         stride: int = 1,
         activation: str = "relu",
         reduction: int = 4,
-        reduce_first: bool = False,
+        downsample_in_bottleneck: bool = False,
     ):
         super().__init__()
         should_apply_shortcut = in_channels != out_channels or stride != 1
@@ -168,8 +175,10 @@ class ResNetBottleNeckLayer(nn.Module):
             ResNetShortCut(in_channels, out_channels, stride=stride) if should_apply_shortcut else nn.Identity()
         )
         self.layer = nn.Sequential(
-            ResNetConvLayer(in_channels, reduces_channels, kernel_size=1, stride=stride if reduce_first else 1),
-            ResNetConvLayer(reduces_channels, reduces_channels, stride=stride if not reduce_first else 1),
+            ResNetConvLayer(
+                in_channels, reduces_channels, kernel_size=1, stride=stride if downsample_in_bottleneck else 1
+            ),
+            ResNetConvLayer(reduces_channels, reduces_channels, stride=stride if not downsample_in_bottleneck else 1),
             ResNetConvLayer(reduces_channels, out_channels, kernel_size=1, activation=None),
         )
         self.activation = ACT2FN[activation]
@@ -200,24 +209,17 @@ class ResNetStage(nn.Module):
 
         layer = ResNetBottleNeckLayer if config.layer_type == "bottleneck" else ResNetBasicLayer
 
-        if config.layer_type == "bottleneck":
-            self.layers = nn.Sequential(
-                # downsampling is done in the first layer with stride of 2
-                layer(
-                    in_channels,
-                    out_channels,
-                    stride=stride,
-                    activation=config.hidden_act,
-                    reduce_first=config.reduce_first,
-                ),
-                *[layer(out_channels, out_channels, activation=config.hidden_act) for _ in range(depth - 1)],
-            )
-        else:
-            self.layers = nn.Sequential(
-                # downsampling is done in the first layer with stride of 2
-                layer(in_channels, out_channels, stride=stride, activation=config.hidden_act),
-                *[layer(out_channels, out_channels, activation=config.hidden_act) for _ in range(depth - 1)],
-            )
+        self.layers = nn.Sequential(
+            # downsampling is done in the first layer with stride of 2
+            layer(
+                in_channels,
+                out_channels,
+                stride=stride,
+                activation=config.hidden_act,
+                downsample_in_bottleneck=config.downsample_in_bottleneck,
+            ),
+            *[layer(out_channels, out_channels, activation=config.hidden_act) for _ in range(depth - 1)],
+        )
 
     def forward(self, input: Tensor) -> Tensor:
         hidden_state = input
