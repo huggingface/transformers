f07af7c29ef5202d6dde08a1dd145d41911f91a1
patil-suraj <surajp815@gmail.com>
2023-10-02 17:40:34 +0200

    remove unused masking function

--- src/transformers/models/siglip/modeling_siglip.py
+++ src/transformers/models/siglip/modeling_siglip.py
@@ -663,24 +663,6 @@ class SiglipEncoder(nn.Module):
         )
 
 
-# Copied from transformers.models.bart.modeling_bart._make_causal_mask
-def _make_causal_mask(
-    input_ids_shape: torch.Size, dtype: torch.dtype, device: torch.device, past_key_values_length: int = 0
-):
-    """
-    Make causal mask used for bi-directional self-attention.
-    """
-    bsz, tgt_len = input_ids_shape
-    mask = torch.full((tgt_len, tgt_len), torch.finfo(dtype).min, device=device)
-    mask_cond = torch.arange(mask.size(-1), device=device)
-    mask.masked_fill_(mask_cond < (mask_cond + 1).view(mask.size(-1), 1), 0)
-    mask = mask.to(dtype)
-
-    if past_key_values_length > 0:
-        mask = torch.cat([torch.zeros(tgt_len, past_key_values_length, dtype=dtype, device=device), mask], dim=-1)
-    return mask[None, None, :, :].expand(bsz, 1, tgt_len, tgt_len + past_key_values_length)
-
-
 class SiglipTextTransformer(nn.Module):
     def __init__(self, config: SiglipTextConfig):
         super().__init__()
