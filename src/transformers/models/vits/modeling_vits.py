b5044cab0ab073376daf709a339c342bccfcfa73
Yoach Lacombe <yoach.lacombe@gmail.com>
2023-10-02 12:34:32 +0000

    add discriminator

--- src/transformers/models/vits/modeling_vits.py
+++ src/transformers/models/vits/modeling_vits.py
@@ -444,6 +444,77 @@ class VitsPosteriorEncoder(nn.Module):
         sampled = (mean + torch.randn_like(mean) * torch.exp(log_stddev)) * padding_mask
         return sampled, mean, log_stddev
 
+class HifiGanDiscriminatorResidualBlock(nn.Module):
+    def __init__(self, period, kernel_size=5, stride=3, leaky_relu_slope=0.1):
+        super().__init__()
+        self.leaky_relu_slope = leaky_relu_slope
+        self.period = period
+        
+        convs = []
+        for in_channels, out_channels in [(1,32), (32,128), (128,512), (5012,1024), (1024,1024)]:
+            convs.append(nn.Conv2d(in_channels, out_channels, (kernel_size, 1), (stride, 1), padding = (self.get_padding(kernel_size, 1),0)))
+            
+        self.convs = nn.ModuleList([convs])
+        
+        self.final_conv = nn.Conv2d(1024, 1, (stride, 1), 1, padding = (1,0))
+
+    def get_padding(self, kernel_size, dilation=1):
+        return (kernel_size * dilation - dilation) // 2
+
+    def apply_weight_norm(self):
+        for layer in self.convs:
+            nn.utils.weight_norm(layer)
+        nn.utils.weight_norm(self.conv_post)
+
+    def remove_weight_norm(self):
+        for layer in self.convs:
+            nn.utils.remove_weight_norm(layer)
+        nn.utils.remove_weight_norm(self.conv_post)
+
+    def forward(self, hidden_states):
+        
+        fmap = []
+        
+        # from 1D to 2D
+        batch_size, channels, length = hidden_states.shape
+        if length % self.period != 0:
+            # pad first
+            n_pad = self.period - (length % self.period)
+            hidden_states = nn.functional.pad(hidden_states, (0,n_pad), "reflect")
+            length = length + n_pad
+        hidden_states = hidden_states.view(batch_size, channels, length//self.period, self.period)
+        
+        
+        for conv in self.convs:
+            hidden_states = conv(hidden_states)
+            hidden_states = nn.functional.leaky_relu(hidden_states, self.leaky_relu_slope)
+            fmap.append(hidden_states)
+            
+        hidden_states = self.final_conv(hidden_states)
+        fmap.append(hidden_states)
+        
+        hidden_states = torch.flatten(hidden_states, 1, -1)
+        return hidden_states, fmap
+    
+    
+class VitsDiscriminator(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        
+        
+        self.discriminators = nn.ModuleList([HifiGanDiscriminatorResidualBlock(period, config.discriminator_kernel_size, config.discriminator_stride, config.leaky_relu_slope)] for period in config.discriminator_periods)
+
+    def forward(self, hidden_states):
+        
+        fmaps = []
+        
+        for discriminator in self.discriminators:
+            hidden_states, fmap = discriminator(hidden_states)
+            fmaps.append(fmap)
+            
+        return hidden_states, fmaps
+
+
 
 # Copied from transformers.models.speecht5.modeling_speecht5.HifiGanResidualBlock
 class HifiGanResidualBlock(nn.Module):
