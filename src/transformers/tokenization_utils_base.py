ba6309024b2c4bf676f6e165b6b0bcf2020048c0
Matt <rocketknight1@gmail.com>
2023-10-03 18:21:52 +0100

    Add add_generation_prompt argument to apply_chat_template and update default templates

--- src/transformers/tokenization_utils_base.py
+++ src/transformers/tokenization_utils_base.py
@@ -1822,6 +1822,9 @@ class PreTrainedTokenizerBase(SpecialTokensMixin, PushToHubMixin):
             "{% for message in messages %}"
             "{{'<|im_start|>' + message['role'] + '\n' + message['content'] + '<|im_end|>' + '\n'}}"
             "{% endfor %}"
+            "{% if add_generation_prompt %}"
+            "{{ '<|im_start|>assistant\n' }}"
+            "{% endif %}"
         )
 
     @classmethod
