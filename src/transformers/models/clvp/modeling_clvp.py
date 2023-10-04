e5d306ac6a8d463b03a03d191072176bae3c6bc2
Susnato Dhar <susnatodhar10@gmail.com>
2023-10-04 16:49:46 +0530

    more comments

--- src/transformers/models/clvp/modeling_clvp.py
+++ src/transformers/models/clvp/modeling_clvp.py
@@ -1795,7 +1795,6 @@ class ClvpModelForConditionalGeneration(ClvpPreTrainedModel):
 
         speech_outputs = self.speech_encoder_model(
             input_ids=speech_ids,
-            attention_mask=None,
             output_attentions=output_attentions,
             output_hidden_states=output_hidden_states,
             return_dict=return_dict,
