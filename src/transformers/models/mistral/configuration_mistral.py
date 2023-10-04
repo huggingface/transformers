7dbec241b3d6cefbb79b6c917772e85fbd8c0d1e
sanchit-gandhi <sanchit@huggingface.co>
2023-10-04 15:50:19 +0100

    make style

--- src/transformers/models/mistral/configuration_mistral.py
+++ src/transformers/models/mistral/configuration_mistral.py
@@ -69,8 +69,7 @@ class MistralConfig(PretrainedConfig):
             The epsilon used by the rms normalization layers.
         use_cache (`bool`, *optional*, defaults to `True`):
             Whether or not the model should return the last key/values attentions (not used by all models). Only
-            relevant if `config.is_decoder=True`.
-            Whether to tie weight embeddings
+            relevant if `config.is_decoder=True`. Whether to tie weight embeddings
         pad_token_id (`int`, *optional*):
             The id of the padding token.
         bos_token_id (`int`, *optional*, defaults to 1):
