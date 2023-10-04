b6ec7da72c5c9b85f826fd0136dbce74cc512b02
Rockerz <64583161+rajveer43@users.noreply.github.com>
2023-10-04 06:47:57 +0000

    Add FA2 class for MPT

--- src/transformers/models/mpt/modeling_mpt.py
+++ src/transformers/models/mpt/modeling_mpt.py
@@ -220,8 +220,69 @@ class MptAttention(nn.Module):
         return attn_output, attn_weights, past_key_value
 
 #implement flash attention2 for Mosaic Pretrained transformer by inheriting the above MPTAttention class
-class MPTAttention2(MptAttention):
-    pass
+class MptFlashAttention2(MptAttention):
+    def __init__(self, config: MptConfig):
+        super().__init__(config)
+        self.alibi_bias_max = config.flash_attn_config.alibi_bias_max
+
+    def forward(
+        self,
+        hidden_states: torch.Tensor,
+        position_bias: torch.Tensor,
+        past_key_value: Optional[Tuple[torch.Tensor]] = None,
+        attention_mask: Optional[torch.Tensor] = None,
+        padding_mask: Optional[torch.Tensor] = None,
+        encoder_padding_mask: Optional[torch.LongTensor] = None,
+    ):
+        batch_size, seq_length = hidden_states.shape[:2]
+
+        mixed_qkv = self.Wqkv(hidden_states)
+        query_states, key_states, value_states = mixed_qkv.chunk(3, dim=2)
+        query_states = query_states.reshape(batch_size, seq_length, self.n_heads, self.head_dim).transpose(1, 2)
+        key_states = key_states.reshape(batch_size, seq_length, self.n_heads, self.head_dim).transpose(1, 2)
+        value_states = value_states.reshape(batch_size, seq_length, self.n_heads, self.head_dim).transpose(1, 2)
+
+        if past_key_value is not None:
+            if len(past_key_value) != 0:
+                key_states = torch.cat([past_key_value[0], key_states], dim=2)
+                value_states = torch.cat([past_key_value[1], value_states], dim=2)
+            past_key_value = (key_states, value_states)
+        else:
+            past_key_value = (key_states, value_states)
+
+        # Calculate FlashAttention2 Alibi tensor
+        alibi = build_mpt_alibi_tensor(self.n_heads, seq_length, alibi_bias_max=self.alibi_bias_max, device=query_states.device)
+
+        # Compute attention scores with Alibi tensor
+        attention_scores = torch.matmul(query_states, key_states.transpose(-1, -2)) * self.softmax_scale + alibi
+
+        query_length = seq_length if past_key_value is None else seq_length + past_key_value[0].shape[2]
+
+        if position_bias is not None:
+            if len(position_bias.shape) != 3:
+                raise ValueError(f"Expecting position_bias shape to be 3 dimensions, got {len(position_bias.shape)}")
+            key_length = key_states.shape[-2]
+
+            position_bias_query_index = max(0, position_bias.size(1) - query_length)
+            position_bias_key_index = max(0, position_bias.size(2) - key_length)
+
+            position_bias = position_bias[:, position_bias_query_index:, position_bias_key_index:]
+
+            attention_scores = attention_scores + position_bias
+
+        if attention_mask is not None:
+            attention_scores = attention_scores.masked_fill(attention_mask, torch.finfo(query_states.dtype).min)
+
+        # (batch_size, n_heads, seq_length, key_length)
+        attn_weights = nn.functional.softmax(attention_scores.float(), dim=-1).to(value_states.dtype)
+        attn_weights = nn.functional.dropout(attn_weights, p=self.attn_dropout_p, training=self.training)
+
+        context_states = torch.matmul(attn_weights, value_states)
+        context_states = context_states.permute(0, 2, 1, 3).contiguous().view(batch_size, seq_length, -1)
+        attn_output = self.out_proj(context_states)
+
+        return attn_output, attn_weights, past_key_value
+
 
 
 class MptMLP(nn.Module):
@@ -255,7 +316,11 @@ class MptBlock(nn.Module):
         self.norm_1.bias = None
 
         self.num_heads = config.n_heads
-        self.attn = MptAttention(config)
+        self.attn = (
+            MptAttention(config)
+            if not getattr(config, "_flash_attn_2_enabled", False)
+            else MptFlashAttention2(config)
+        )
 
         self.norm_2 = LayerNorm(hidden_size, eps=config.layer_norm_epsilon)
         # backward compatibility with weights on the Hub
@@ -274,6 +339,7 @@ class MptBlock(nn.Module):
         layer_past: Optional[Tuple[torch.Tensor, torch.Tensor]] = None,
         use_cache: bool = False,
         output_attentions: bool = False,
+        padding_mask: Optional[torch.LongTensor] = None,
     ):
         # hidden_states: [batch_size, seq_length, hidden_size]
         # Layer norm at the beginning of the transformer layer.
@@ -287,6 +353,7 @@ class MptBlock(nn.Module):
             position_bias=position_bias,
             attention_mask=attention_mask,
             past_key_value=layer_past,
+            padding_mask=padding_mask,
         )
 
         hidden_states = self.resid_attn_dropout(attn_outputs) + residual
