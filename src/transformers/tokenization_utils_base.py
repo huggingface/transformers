fafbbede8f3bf0d67caf8dae57fa8dc768a1f51d
Arthur <arthur.zucker@gmail.com>
2023-10-06 15:17:49 +0200

    how

--- src/transformers/tokenization_utils_base.py
+++ src/transformers/tokenization_utils_base.py
@@ -1179,7 +1179,7 @@ class SpecialTokensMixin:
         self._additional_special_tokens = [] if value is not None else None
         # 1. we are resetting the tokens that should not be special
         updated_tokens = []
-        for token in self._additional_special_token:
+        for token in self._additional_special_tokens:
             if token not in value or str(token) not in value:
                 token.special = False
                 updated_tokens.append(token)
@@ -1187,11 +1187,11 @@ class SpecialTokensMixin:
         # 2. The new additional special tokens should also be updated
         if value is not None:
             for token in value:
-                if str(value) not in self.special_tokens_map.values()
-                    self._additional_special_token.appemd(token)
+                if str(value) not in self.special_tokens_map.values():
+                    self._additional_special_tokens.append(token)
 
         # 3. Add everything to the tokenizer to overwrite
-        updated_tokens += self._additional_special_token
+        updated_tokens += self._additional_special_tokens
         self.add_tokens(updated_tokens, special_tokens = True)
 
     @property
