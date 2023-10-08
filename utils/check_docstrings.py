b84302ebd848dbbd94e9f269bd164f5d7d261daa
Isaac Chung <chungisaac1217@gmail.com>
2023-10-08 21:56:44 +0300

    fix docstring for CLIPImageProcessor

--- utils/check_docstrings.py
+++ utils/check_docstrings.py
@@ -118,7 +118,6 @@ OBJECTS_TO_IGNORE = [
     "BridgeTowerTextConfig",
     "BridgeTowerVisionConfig",
     "BrosModel",
-    "CLIPImageProcessor",
     "CLIPSegTextConfig",
     "CLIPSegVisionConfig",
     "CLIPTextConfig",
