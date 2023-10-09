5c9494bd699990f0ddedca59fb3e685f7cdd27d5
Lysandre <lysandre@huggingface.co>
2023-10-09 15:15:20 +0200

    Fix stale bot

--- scripts/stale.py
+++ scripts/stale.py
@@ -37,29 +37,30 @@ def main():
     open_issues = repo.get_issues(state="open")
 
     for issue in open_issues:
+        print(issue)
         comments = sorted([comment for comment in issue.get_comments()], key=lambda i: i.created_at, reverse=True)
         last_comment = comments[0] if len(comments) > 0 else None
         if (
             last_comment is not None and last_comment.user.login == "github-actions[bot]"
-            and (dt.utcnow() - issue.updated_at).days > 7
-            and (dt.utcnow() - issue.created_at).days >= 30
+            and (dt.utcnow() - issue.updated_at.replace(tzinfo=None)).days > 7
+            and (dt.utcnow() - issue.created_at.replace(tzinfo=None)).days >= 30
             and not any(label.name.lower() in LABELS_TO_EXEMPT for label in issue.get_labels())
         ):
-            # print(f"Would close issue {issue.number} since it has been 7 days of inactivity since bot mention.")
-            issue.edit(state="closed")
+            print(f"Would close issue {issue.number} since it has been 7 days of inactivity since bot mention.")
+            # issue.edit(state="closed")
         elif (
-            (dt.utcnow() - issue.updated_at).days > 23
-            and (dt.utcnow() - issue.created_at).days >= 30
+            (dt.utcnow() - issue.updated_at.replace(tzinfo=None)).days > 23
+            and (dt.utcnow() - issue.created_at.replace(tzinfo=None)).days >= 30
             and not any(label.name.lower() in LABELS_TO_EXEMPT for label in issue.get_labels())
         ):
-            # print(f"Would add stale comment to {issue.number}")
-            issue.create_comment(
-                "This issue has been automatically marked as stale because it has not had "
-                "recent activity. If you think this still needs to be addressed "
-                "please comment on this thread.\n\nPlease note that issues that do not follow the "
-                "[contributing guidelines](https://github.com/huggingface/transformers/blob/main/CONTRIBUTING.md) "
-                "are likely to be ignored."
-            )
+            print(f"Would add stale comment to {issue.number}")
+            # issue.create_comment(
+            #     "This issue has been automatically marked as stale because it has not had "
+            #     "recent activity. If you think this still needs to be addressed "
+            #     "please comment on this thread.\n\nPlease note that issues that do not follow the "
+            #     "[contributing guidelines](https://github.com/huggingface/transformers/blob/main/CONTRIBUTING.md) "
+            #     "are likely to be ignored."
+            # )
 
 
 if __name__ == "__main__":
