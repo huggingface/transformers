8d1fa608ebec57cf0ad12c1898d47dcf3491ed3a
shauray8 <shauray9@gmail.com>
2023-10-09 15:10:42 +0530

    works with both

--- src/transformers/models/llava/processing_llava.py
+++ src/transformers/models/llava/processing_llava.py
@@ -202,8 +202,11 @@ class LlavaProcessor(ProcessorMixin):
 
     # overwrite to load the Q-Former tokenizer from a separate folder
     @classmethod
-    def from_pretrained(self, cls, pretrained_model_name_or_path, **kwargs):
-        vision_model = CLIPVisionModel.from_pretrained(self.image_processor.model_path)
+    def from_pretrained(cls, pretrained_model_name_or_path, **kwargs):
+        if "1.5" in pretrained_model_name_or_path:
+            vision_model = CLIPVisionModel.from_pretrained("openai/clip-vit-large-patch14-336")
+        else:
+            vision_model = CLIPVisionModel.from_pretrained("openai/clip-vit-large-patch14")
         args = cls._get_arguments_from_pretrained(pretrained_model_name_or_path, **kwargs)
         args.append(vision_model)
         return cls(*args)
