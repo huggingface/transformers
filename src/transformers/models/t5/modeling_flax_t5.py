397fe6b74f4c8532126bc43e83e3ddb2cd582b04
Roy Hvaara <hvaara@google.com>
2023-10-09 19:40:30 +0000

    [JAX] Replace uses of `jnp.array` in types with `jnp.ndarray`.
    `jnp.array` is a function, not a type:
    https://jax.readthedocs.io/en/latest/_autosummary/jax.numpy.array.html
    so it never makes sense to use `jnp.array` in a type annotation. Presumably the intent was to write `jnp.ndarray` aka `jax.Array`.
    
    Co-authored-by: Peter Hawkins <phawkins@google.com>

--- src/transformers/models/t5/modeling_flax_t5.py
+++ src/transformers/models/t5/modeling_flax_t5.py
@@ -56,7 +56,7 @@ remat = nn_partitioning.remat
 
 
 # Copied from transformers.models.bart.modeling_flax_bart.shift_tokens_right
-def shift_tokens_right(input_ids: jnp.array, pad_token_id: int, decoder_start_token_id: int) -> jnp.ndarray:
+def shift_tokens_right(input_ids: jnp.ndarray, pad_token_id: int, decoder_start_token_id: int) -> jnp.ndarray:
     """
     Shift input ids one token to the right.
     """
