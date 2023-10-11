805f3e2c76be17c65f8fd073b20979e781fe3056
steven <steven.bucaille@gmail.com>
2023-10-11 21:24:37 +0200

    Moved convertt_to_grayscale from image_transforms.py to image_processing_superpoint.py

--- src/transformers/image_transforms.py
+++ src/transformers/image_transforms.py
@@ -763,42 +763,6 @@ def convert_to_rgb(image: ImageInput) -> ImageInput:
     return image
 
 
-def convert_to_grayscale(
-    image: ImageInput,
-    input_data_format: Optional[Union[str, ChannelDimension]] = None,
-) -> ImageInput:
-    """
-    Converts an image to grayscale format using the NTSC formula. Only support numpy and PIL Image. TODO support torch
-    and tensorflow grayscale conversion
-
-    This function is supposed to return a 1-channel image, but it returns a 3-channel image with the same value in each
-    channel, because of an issue that is discussed in :
-    https://github.com/huggingface/transformers/pull/25786#issuecomment-1730176446
-
-    Args:
-        image (Image):
-            The image to convert.
-    """
-    requires_backends(convert_to_grayscale, ["vision"])
-
-    if isinstance(image, np.ndarray):
-        if input_data_format == ChannelDimension.FIRST or input_data_format == "channels_first":
-            gray_image = image[0, ...] * 0.2989 + image[1, ...] * 0.5870 + image[2, ...] * 0.1140
-            # gray_image = gray_image[None, ...]
-            gray_image = np.stack([gray_image] * 3, axis=0)
-        elif input_data_format == ChannelDimension.LAST or input_data_format == "channels_last":
-            gray_image = image[..., 0] * 0.2989 + image[..., 1] * 0.5870 + image[..., 2] * 0.1140
-            # gray_image = gray_image[..., None]
-            gray_image = np.stack([gray_image] * 3, axis=-1)
-        return gray_image
-
-    if not isinstance(image, PIL.Image.Image):
-        return image
-
-    image = image.convert("L")
-    return image
-
-
 def flip_channel_order(
     image: np.ndarray,
     data_format: Optional[ChannelDimension] = None,
