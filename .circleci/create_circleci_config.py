d77d7f3ebc9377fca21de06d5360f0001f27eca9
ydshieh <ydshieh@users.noreply.github.com>
2023-10-12 16:23:11 +0200

    fix

--- .circleci/create_circleci_config.py
+++ .circleci/create_circleci_config.py
@@ -162,8 +162,8 @@ class CircleCIJob:
         self.tests_to_run = ["tests/models"]
 
         tests_to_run = [f"tests/models/{x}" for x in os.listdir("src/transformers/models/") if x != "deprecated"]
-        start = 32 * 2
-        end = 32 * 3
+        start = 16 * 2
+        end = 16 * 3
         self.tests_to_run = [x for x in tests_to_run[start:end] if not os.path.isfile(x)]
 
         if self.parallelism == 1:
