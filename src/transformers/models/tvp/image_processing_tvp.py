b93330a1309fbbe84219d2526a90fe7954028f8d
Feng, Jiqing <jiqing.feng@intel.com>
2023-10-13 00:09:16 -0700

    fix comments

--- src/transformers/models/tvp/image_processing_tvp.py
+++ src/transformers/models/tvp/image_processing_tvp.py
@@ -12,7 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND=, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Image processor class for Tvp."""
+"""Image processor class for TVP."""
 
 from typing import Dict, Iterable, List, Optional, Union
 
--- src/transformers/models/tvp/processing_tvp.py
+++ src/transformers/models/tvp/processing_tvp.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """
-Image/Text processor class for Tvp
+Processor class for TVP.
 """
 
 
