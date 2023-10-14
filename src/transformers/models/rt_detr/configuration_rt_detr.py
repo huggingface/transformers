69810cb510f75d061b9da6408f3e1c096679aacb
Rafael Padilla <rafael.padilla@huggingface.co>
2023-10-14 23:47:45 +0000

    modeling PResNet

--- src/transformers/models/rt_detr/configuration_rt_detr.py
+++ src/transformers/models/rt_detr/configuration_rt_detr.py
@@ -1,4 +1,5 @@
-# coding=utf-8
+
+        # coding=utf-8
 # Copyright 2023 Facebook AI Research and The HuggingFace Inc. team. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -141,129 +142,33 @@ class RT_DETRConfig(PretrainedConfig):
 
     def __init__(
         self,
-        use_timm_backbone=True,
-        backbone_config=None,
-        num_channels=3,
-        num_queries=100,
-        encoder_layers=6,
-        encoder_ffn_dim=2048,
-        encoder_attention_heads=8,
-        decoder_layers=6,
-        decoder_ffn_dim=2048,
-        decoder_attention_heads=8,
-        encoder_layerdrop=0.0,
-        decoder_layerdrop=0.0,
+        # PResNet config:
+        depth, 
+        variant='d', 
+        num_stages=4, 
+        return_idx=[0, 1, 2, 3], 
+        act='relu',
+        freeze_at=-1, 
+        freeze_norm=True, 
+        pretrained=True,
         is_encoder_decoder=True,
-        activation_function="relu",
-        d_model=256,
-        dropout=0.1,
-        attention_dropout=0.0,
-        activation_dropout=0.0,
-        init_std=0.02,
-        init_xavier_std=1.0,
-        auxiliary_loss=False,
-        position_embedding_type="sine",
-        backbone="resnet50",
-        use_pretrained_backbone=True,
-        dilation=False,
-        class_cost=1,
-        bbox_cost=5,
-        giou_cost=2,
-        mask_loss_coefficient=1,
-        dice_loss_coefficient=1,
-        bbox_loss_coefficient=5,
-        giou_loss_coefficient=2,
-        eos_coefficient=0.1,
+        block_nums = [3, 4, 6, 3], # TODO Rafael: OC depends on the depth
+    # 18: [2, 2, 2, 2],
+    # 34: [3, 4, 6, 3],
+    # 50: [3, 4, 6, 3],
+    # 101: [3, 4, 23, 3],
+    # 152: [3, 8, 36, 3],
         **kwargs,
     ):
-        if backbone_config is not None and use_timm_backbone:
-            raise ValueError("You can't specify both `backbone_config` and `use_timm_backbone`.")
-
-        if not use_timm_backbone:
-            if backbone_config is None:
-                logger.info("`backbone_config` is `None`. Initializing the config with the default `ResNet` backbone.")
-                backbone_config = CONFIG_MAPPING["resnet"](out_features=["stage4"])
-            elif isinstance(backbone_config, dict):
-                backbone_model_type = backbone_config.get("model_type")
-                config_class = CONFIG_MAPPING[backbone_model_type]
-                backbone_config = config_class.from_dict(backbone_config)
-            # set timm attributes to None
-            dilation, backbone, use_pretrained_backbone = None, None, None
-
-        self.use_timm_backbone = use_timm_backbone
-        self.backbone_config = backbone_config
-        self.num_channels = num_channels
-        self.num_queries = num_queries
-        self.d_model = d_model
-        self.encoder_ffn_dim = encoder_ffn_dim
-        self.encoder_layers = encoder_layers
-        self.encoder_attention_heads = encoder_attention_heads
-        self.decoder_ffn_dim = decoder_ffn_dim
-        self.decoder_layers = decoder_layers
-        self.decoder_attention_heads = decoder_attention_heads
-        self.dropout = dropout
-        self.attention_dropout = attention_dropout
-        self.activation_dropout = activation_dropout
-        self.activation_function = activation_function
-        self.init_std = init_std
-        self.init_xavier_std = init_xavier_std
-        self.encoder_layerdrop = encoder_layerdrop
-        self.decoder_layerdrop = decoder_layerdrop
-        self.num_hidden_layers = encoder_layers
-        self.auxiliary_loss = auxiliary_loss
-        self.position_embedding_type = position_embedding_type
-        self.backbone = backbone
-        self.use_pretrained_backbone = use_pretrained_backbone
-        self.dilation = dilation
-        # Hungarian matcher
-        self.class_cost = class_cost
-        self.bbox_cost = bbox_cost
-        self.giou_cost = giou_cost
-        # Loss coefficients
-        self.mask_loss_coefficient = mask_loss_coefficient
-        self.dice_loss_coefficient = dice_loss_coefficient
-        self.bbox_loss_coefficient = bbox_loss_coefficient
-        self.giou_loss_coefficient = giou_loss_coefficient
-        self.eos_coefficient = eos_coefficient
+        self.depth = depth
+        self.variant = variant
+        self.num_stages = num_stages
+        self.return_idx = return_idx
+        self.act, = act,
+        self.freeze_at = freeze_at
+        self.freeze_norm = freeze_norm
+        self.pretrained = pretrained
+        self.is_encoder_decoder = is_encoder_decoder
+        self.block_nums = block_nums
+        
         super().__init__(is_encoder_decoder=is_encoder_decoder, **kwargs)
-
-    @property
-    def num_attention_heads(self) -> int:
-        return self.encoder_attention_heads
-
-    @property
-    def hidden_size(self) -> int:
-        return self.d_model
-
-    @classmethod
-    def from_backbone_config(cls, backbone_config: PretrainedConfig, **kwargs):
-        """Instantiate a [`RT_DETRConfig`] (or a derived class) from a pre-trained backbone model configuration.
-
-        Args:
-            backbone_config ([`PretrainedConfig`]):
-                The backbone configuration.
-        Returns:
-            [`RT_DETRConfig`]: An instance of a configuration object
-        """
-        return cls(backbone_config=backbone_config, **kwargs)
-
-
-class RT_DETROnnxConfig(OnnxConfig):
-    torch_onnx_minimum_version = version.parse("1.11")
-
-    @property
-    def inputs(self) -> Mapping[str, Mapping[int, str]]:
-        return OrderedDict(
-            [
-                ("pixel_values", {0: "batch", 1: "num_channels", 2: "height", 3: "width"}),
-                ("pixel_mask", {0: "batch"}),
-            ]
-        )
-
-    @property
-    def atol_for_validation(self) -> float:
-        return 1e-5
-
-    @property
-    def default_onnx_opset(self) -> int:
-        return 12
