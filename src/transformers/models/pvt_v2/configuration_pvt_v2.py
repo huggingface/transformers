e95f751545c3080d19f5fb739fd22d3629f8d203
FoamoftheSea <indezera@gmail.com>
2023-10-14 14:43:15 -0700

    pvt_v2_b2_linear converted and working

--- src/transformers/models/pvt_v2/configuration_pvt_v2.py
+++ src/transformers/models/pvt_v2/configuration_pvt_v2.py
@@ -34,6 +34,7 @@ PVT_V2_PRETRAINED_CONFIG_ARCHIVE_MAP = {
     "pvt_v2_b0": "https://huggingface.co/FoamoftheSea/pvt_v2_b0",
     "pvt_v2_b1": "https://huggingface.co/FoamoftheSea/pvt_v2_b1",
     "pvt_v2_b2": "https://huggingface.co/FoamoftheSea/pvt_v2_b2",
+    "pvt_v2_b2_linear": "https://huggingface.co/FoamoftheSea/pvt_v2_b2_linear",
     "pvt_v2_b3": "https://huggingface.co/FoamoftheSea/pvt_v2_b3",
     "pvt_v2_b4": "https://huggingface.co/FoamoftheSea/pvt_v2_b4",
     "pvt_v2_b5": "https://huggingface.co/FoamoftheSea/pvt_v2_b5",
