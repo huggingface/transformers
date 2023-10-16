5c6b83cb69da2fbc58b3c25c0ed7ba354a3d4141
przemL <24912415+przemL@users.noreply.github.com>
2023-10-16 18:26:55 +0200

    [docstring] Fix bert generation tokenizer (#26820)
    * Remove BertGenerationTokenizer from objects to ignore
    
    The file BertGenerationTokenizer is removed from
    objects to ignore as a first step to fix docstring.
    
    * Docstrings fix for BertGenerationTokenizer
    
    Docstring fix is generated for BertGenerationTokenizer
    by using check_docstrings.py.
    
    * Fix docstring for BertGenerationTokenizer
    
    Added sep_token type and docstring in BertGenerationTokenizer.

--- utils/check_docstrings.py
+++ utils/check_docstrings.py
@@ -94,7 +94,6 @@ OBJECTS_TO_IGNORE = [
     "BarthezTokenizerFast",
     "BeitModel",
     "BertConfig",
-    "BertGenerationTokenizer",
     "BertJapaneseTokenizer",
     "BertModel",
     "BertTokenizerFast",
