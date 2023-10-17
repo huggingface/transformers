f9fd7b645ac3ca021f60252adf385ea6a712c04c
Alex McKinney <alex.f.mckinney@gmail.com>
2023-10-17 08:49:42 +0100

    mistral and persimmon test change -copy from llama

--- tests/models/mistral/test_modeling_mistral.py
+++ tests/models/mistral/test_modeling_mistral.py
@@ -106,7 +106,7 @@ class MistralModelTester:
 
         input_mask = None
         if self.use_input_mask:
-            input_mask = random_attention_mask([self.batch_size, self.seq_length])
+            input_mask = torch.tril(torch.ones(self.batch_size, self.seq_length))
 
         token_type_ids = None
         if self.use_token_type_ids:
