d933818d6729dcdb450a8ce6418102eb6c69d9bb
Matt <Rocketknight1@users.noreply.github.com>
2023-10-18 17:38:52 +0100

    Add default template warning (#26637)
    * Add default template warnings
    
    * make fixup
    
    * Move warnings to FutureWarning
    
    * Move warnings to FutureWarning
    
    * fix make fixup
    
    * Remove futurewarning

--- src/transformers/models/gpt_sw3/tokenization_gpt_sw3.py
+++ src/transformers/models/gpt_sw3/tokenization_gpt_sw3.py
@@ -321,6 +321,12 @@ class GPTSw3Tokenizer(PreTrainedTokenizer):
         This chat template formats messages like an instant messenger chat log, with "User:" and "Bot:" strings
         preceding messages. BOS tokens are added between all messages.
         """
+        logger.warning_once(
+            "\nNo chat template is defined for this tokenizer - using the default template "
+            f"for the {self.__class__.__name__} class. If the default is not appropriate for "
+            "your model, please set `tokenizer.chat_template` to an appropriate template. "
+            "See https://huggingface.co/docs/transformers/main/chat_templating for more information.\n"
+        )
         return (
             "{{ eos_token }}{{ bos_token }}"
             "{% for message in messages %}"
