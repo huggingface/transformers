81aab96a6bfe241ee935f0ffd996609e9a2e97b2
ArthurZucker <arthur.zucker@gmail.com>
2023-10-18 13:34:28 -0400

    fixup done

--- src/transformers/models/fuyu/configuration_fuyu.py
+++ src/transformers/models/fuyu/configuration_fuyu.py
@@ -89,9 +89,9 @@ class FuyuConfig(PretrainedConfig):
 
         pad_token_id (`int`, *optional*):
             The id of the *padding* token.
-        bos_token_id (`int`, *optional*):
+        bos_token_id (`int`, *optional*, defaults to 1):
             The id of the *beginning-of-sequence* token.
-        eos_token_id (`Union[int, List[int]]`, *optional*):
+        eos_token_id (`Union[int, List[int]]`, *optional*, defaults to 2):
             The id of the *end-of-sequence* token. Optionally, use a list to set multiple *end-of-sequence* tokens.
         text_config (`dict`, *optional*):
             Dictionary of configuration options used to initialize the `language``[`Aut`].
