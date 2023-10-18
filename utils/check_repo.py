caa0ff0bf104ee68ea46432b298a534e9a4a02a4
Pablo Montalvo <39954772+molbap@users.noreply.github.com>
2023-10-19 00:24:11 +0200

    Add fuyu model (#26911)
    * initial commit
    
    * add processor, add fuyu naming
    
    * add draft processor
    
    * fix processor
    
    * remove dropout to fix loading of weights
    
    * add image processing fixes from Pedro
    
    * fix
    
    * fix processor
    
    * add basic processing fuyu test
    
    * add documentation and TODO
    
    * address comments, add tests, add doc
    
    * replace assert with torch asserts
    
    * add Mixins and fix tests
    
    * clean imports
    
    * add model tester, clean imports
    
    * fix embedding test
    
    * add updated tests from pre-release model
    
    * Processor: return input_ids used for inference
    
    * separate processing and model tests
    
    * relax test tolerance for embeddings
    
    * add test for logit comparison
    
    * make sure fuyu image processor is imported in the init
    
    * fix formattingh
    
    * more formatting issues
    
    * and more
    
    * fixups
    
    * remove some stuff
    
    * nits
    
    * update init
    
    * remove the fuyu file
    
    * Update integration test with release model
    
    * Update conversion script.
    
    The projection is not used, as confirmed by the authors.
    
    * improve geenration
    
    * Remove duplicate function
    
    * Trickle down patches to model call
    
    * processing fuyu updates
    
    * remove things
    
    * fix prepare_inputs_for_generation to fix generate()
    
    * remove model_input
    
    * update
    
    * add generation tests
    
    * nits
    
    * draft leverage automodel and autoconfig
    
    * nits
    
    * fix dtype patch
    
    * address comments, update READMEs and doc, include tests
    
    * add working processing test, remove refs to subsequences
    
    * add tests, remove Sequence classification
    
    * processing
    
    * update
    
    * update the conversion script
    
    * more processing cleanup
    
    * safe import
    
    * take out ModelTesterMixin for early release
    
    * more cl;eanup
    
    * more cleanup
    
    * more cleanup
    
    * and more
    
    * register a buffer
    
    * nits
    
    * add postprocessing of generate output
    
    * nits
    
    * updates
    
    * add one working test
    
    * fix test
    
    * make fixup works
    
    * fixup
    
    * Arthur's updates
    
    * nits
    
    * update
    
    * update
    
    * fix processor
    
    * update tests
    
    * passe more fixups
    
    * fix
    
    * nits
    
    * don't import torch
    
    * skip fuyu config for now
    
    * fixup done
    
    * fixup
    
    * update
    
    * oups
    
    * nits
    
    * Use input embeddings
    
    * no buffer
    
    * update
    
    * styling processing fuyu
    
    * fix test
    
    * update licence
    
    * protect torch import
    
    * fixup and update not doctested
    
    * kwargs should be passed
    
    * udpates
    
    * update the impofixuprts in the test
    
    * protect import
    
    * protecting imports
    
    * protect imports in type checking
    
    * add testing decorators
    
    * protect top level import structure
    
    * fix typo
    
    * fix check init
    
    * move requires_backend to functions
    
    * Imports
    
    * Protect types
    
    ---------
    
    Co-authored-by: Pedro Cuenca <pedro@huggingface.co>
    Co-authored-by: ArthurZucker <arthur.zucker@gmail.com>
    Co-authored-by: Arthur <48595927+ArthurZucker@users.noreply.github.com>
    Co-authored-by: Lysandre <lysandre@huggingface.co>

--- utils/check_repo.py
+++ utils/check_repo.py
@@ -79,6 +79,7 @@ PRIVATE_MODELS = [
 # Being in this list is an exception and should **not** be the rule.
 IGNORE_NON_TESTED = PRIVATE_MODELS.copy() + [
     # models to ignore for not tested
+    "FuyuForCausalLM",  # Not tested fort now
     "InstructBlipQFormerModel",  # Building part of bigger (tested) model.
     "UMT5EncoderModel",  # Building part of bigger (tested) model.
     "Blip2QFormerModel",  # Building part of bigger (tested) model.
