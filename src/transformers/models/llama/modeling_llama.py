467096601e25e8096eeef8dc75d23d5597baf105
Tom Aarsen <Cubiegamedev@gmail.com>
2023-10-19 11:10:32 +0200

    Undo unnecessary newline change

--- src/transformers/models/llama/modeling_llama.py
+++ src/transformers/models/llama/modeling_llama.py
@@ -328,6 +328,7 @@ class LlamaAttention(nn.Module):
         padding_mask: Optional[torch.LongTensor] = None,
     ) -> Tuple[torch.Tensor, Optional[torch.Tensor], Optional[Tuple[torch.Tensor]]]:
         bsz, q_len, _ = hidden_states.size()
+
         if self.config.pretraining_tp > 1:
             key_value_slicing = (self.num_key_value_heads * self.head_dim) // self.config.pretraining_tp
             query_slices = self.q_proj.weight.split(
