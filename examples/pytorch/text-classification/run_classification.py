e49de734557ae3b3f8d1913d2a19675b1a7da9bf
YQ <ranchlai@163.com>
2023-10-20 21:00:56 +0800

    add comments

--- examples/pytorch/text-classification/run_classification.py
+++ examples/pytorch/text-classification/run_classification.py
@@ -721,9 +721,10 @@ def main():
         if is_regression:
             predictions = np.squeeze(predictions)
         elif is_multi_label:
-            predictions = np.array(
-                [np.where(p > 0, 1, 0) for p in predictions]
-            )  # convert logits to multi-hot encoding
+            # Convert logits to multi-hot encoding. We compare the logits to 0 instead of 0.5, because the sigmoid is not applied.
+            # You can aso pass `preprocess_logits_for_metrics=lambda logits, labels: nn.functional.sigmoid(logits)` to the Trainer
+            # and set p > 0.5 below (less efficient in this case)
+            predictions = np.array([np.where(p > 0, 1, 0) for p in predictions])
         else:
             predictions = np.argmax(predictions, axis=1)
         output_predict_file = os.path.join(training_args.output_dir, "predict_results.txt")
