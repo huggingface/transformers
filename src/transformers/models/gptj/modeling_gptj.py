4aa741aa75ab39f40deef186a2a5bec2cc2d7e0e
Félix Marty <9808326+fxmarty@users.noreply.github.com>
2023-10-20 09:22:07 +0200

    fix

--- src/transformers/models/gptj/modeling_gptj.py
+++ src/transformers/models/gptj/modeling_gptj.py
@@ -57,7 +57,7 @@ GPTJ_PRETRAINED_MODEL_ARCHIVE_LIST = [
 
 def create_sinusoidal_positions(num_pos: int, dim: int) -> torch.Tensor:
     inv_freq = 1.0 / (10000 ** (torch.arange(0, dim, 2) / dim))
-    sinusoid_inp = torch.einsum("i , j -> i j", torch.arange(num_pos, dtype=torch.float), inv_freq)
+    sinusoid_inp = torch.einsum("i , j -> i j", torch.arange(num_pos, dtype=torch.float), inv_freq).float()
     return torch.cat((torch.sin(sinusoid_inp), torch.cos(sinusoid_inp)), dim=1)
 
 
