a64c80f0dfc30692bb24653e7417c1e70730bae8
Samuel Hoffman <shoffman@ibm.com>
2023-10-20 11:40:46 -0700

    fix setup_and_quality tests

--- src/transformers/convert_slow_tokenizer.py
+++ src/transformers/convert_slow_tokenizer.py
@@ -24,7 +24,7 @@ from typing import Dict, List, Tuple
 
 from packaging import version
 from tokenizers import AddedToken, Regex, Tokenizer, decoders, normalizers, pre_tokenizers, processors
-from tokenizers.models import BPE, Unigram, WordPiece, WordLevel
+from tokenizers.models import BPE, Unigram, WordLevel, WordPiece
 
 from .utils import is_protobuf_available, requires_backends
 from .utils.import_utils import PROTOBUF_IMPORT_ERROR
--- tests/models/molformer/test_modeling_molformer.py
+++ tests/models/molformer/test_modeling_molformer.py
@@ -22,7 +22,7 @@ from transformers.testing_utils import require_torch, slow, torch_device
 
 from ...generation.test_utils import GenerationTesterMixin
 from ...test_configuration_common import ConfigTester
-from ...test_modeling_common import ModelTesterMixin, floats_tensor, ids_tensor, random_attention_mask
+from ...test_modeling_common import ModelTesterMixin, ids_tensor, random_attention_mask
 from ...test_pipeline_mixin import PipelineTesterMixin
 
 
