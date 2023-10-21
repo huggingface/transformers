42b46db3ca0ccaa260b641fe38352f33916b4992
Nathan Cooper <nathanacoop@gmail.com>
2023-10-21 11:25:54 -0500

    update docstring to have labels descript

--- src/transformers/models/fuyu/modeling_fuyu.py
+++ src/transformers/models/fuyu/modeling_fuyu.py
@@ -125,6 +125,10 @@ FUYU_INPUTS_DOCSTRING = r"""
             Optionally, instead of passing `input_ids` you can choose to directly pass an embedded representation. This
             is useful if you want more control over how to convert `input_ids` indices into associated vectors than the
             model's internal embedding lookup matrix.
+        labels (`torch.LongTensor` of shape `(batch_size, sequence_length)`, *optional*):
+                Labels for computing the masked language modeling loss. Indices should either be in `[0, ...,
+                config.vocab_size]` or -100 (see `input_ids` docstring). Tokens with indices set to `-100` are ignored
+                (masked), the loss is only computed for the tokens with labels in `[0, ..., config.vocab_size]`.
         use_cache (`bool`, *optional*):
             If set to `True`, `past_key_values` key value states are returned and can be used to speed up decoding (see
             `past_key_values`).
