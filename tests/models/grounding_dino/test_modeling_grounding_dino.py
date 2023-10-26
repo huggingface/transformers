eccaec95d0df167c098bdfe8e9c2ae86aa6c7637
EduardoPach <eduardo.pach@hotmail.com>
2023-10-26 17:29:51 -0300

    Fixed last test

--- tests/models/grounding_dino/test_modeling_grounding_dino.py
+++ tests/models/grounding_dino/test_modeling_grounding_dino.py
@@ -14,9 +14,10 @@
 # limitations under the License.
 """ Testing suite for the PyTorch Grounding DINO model. """
 
-
+import collections
 import inspect
 import math
+import re
 import unittest
 from typing import Dict, List, Tuple
 
@@ -41,6 +42,7 @@ if is_torch_available():
     import torch
 
     from transformers import GroundingDINOForObjectDetection, GroundingDINOModel
+    from transformers.pytorch_utils import id_tensor_storage
 
 
 if is_vision_available():
@@ -627,6 +629,41 @@ class GroundingDINOModelTest(ModelTesterMixin, GenerationTesterMixin, PipelineTe
         inputs = self._prepare_for_class(inputs_dict, model_class, return_labels=True)
         loss = model(**inputs).loss
         loss.backward()
+
+    def test_tied_weights_keys(self):
+        config, _ = self.model_tester.prepare_config_and_inputs_for_common()
+        config.tie_word_embeddings = True
+        for model_class in self.all_model_classes:
+            model_tied = model_class(config)
+
+            ptrs = collections.defaultdict(list)
+            for name, tensor in model_tied.state_dict().items():
+                ptrs[id_tensor_storage(tensor)].append(name)
+
+            # These are all the pointers of shared tensors.
+            tied_params = [names for _, names in ptrs.items() if len(names) > 1]
+
+            tied_weight_keys = model_tied._tied_weights_keys if model_tied._tied_weights_keys is not None else []
+            # Detect we get a hit for each key
+            for key in tied_weight_keys:
+                if not any(re.search(key, p) for group in tied_params for p in group):
+                    raise ValueError(f"{key} is not a tied weight key for {model_class}.")
+
+            # Removed tied weights found from tied params -> there should only be one left after
+            for key in tied_weight_keys:
+                for i in range(len(tied_params)):
+                    tied_params[i] = [p for p in tied_params[i] if re.search(key, p) is None]
+
+            # GroundingDINO when sharing weights also uses the shared ones in GroundingDINODecoder
+            # Therefore, differently from DeformableDetr, we expect the group lens to be 2
+            # one for self.bbox_embed in GroundingDINOForObejectDetection and another one
+            # in the decoder
+            tied_params = [group for group in tied_params if len(group) > 2] 
+            self.assertListEqual(
+                tied_params,
+                [],
+                f"Missing `_tied_weights_keys` for {model_class}: add all of {tied_params} except one.",
+            )
 
 
 TOLERANCE = 1e-4
