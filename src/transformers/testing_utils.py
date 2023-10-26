3070d1c07301c673d86402186c674eb0c66dfbaf
statelesshz <hzji210@gmail.com>
2023-10-26 23:30:47 +0800

    make fsdp test cases device agnostic

--- src/transformers/testing_utils.py
+++ src/transformers/testing_utils.py
@@ -628,6 +628,23 @@ def require_torch_multi_gpu(test_case):
     return unittest.skipUnless(torch.cuda.device_count() > 1, "test requires multiple GPUs")(test_case)
 
 
+def require_torch_multi_accelerator(test_case):
+    """
+    Decorator marking a test that requires a multi-accelerator (in PyTorch). These tests are skipped on a machine
+    without multiple accelerators.
+
+    To run *only* the multi_accelerator tests, assuming all test names contain multi_accelerator: $ pytest -sv ./tests
+    -k "multi_accelerator"
+    """
+    if not is_torch_available():
+        return unittest.skip("test requires PyTorch")(test_case)
+
+
+    return unittest.skipUnless(backend_device_count(torch_device) > 1, "test requires multiple accelerators")(
+        test_case
+    )
+
+
 def require_torch_non_multi_gpu(test_case):
     """
     Decorator marking a test that requires 0 or 1 GPU setup (in PyTorch).
