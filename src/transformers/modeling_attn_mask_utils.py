92f1c6ead400f6760b63b64e11da2d454a8a2541
Patrick von Platen <patrick.v.platen@gmail.com>
2023-10-27 16:03:58 +0200

    finish

--- src/transformers/modeling_attn_mask_utils.py
+++ src/transformers/modeling_attn_mask_utils.py
@@ -36,6 +36,11 @@ class AttentionMaskConverter:
         self.is_causal = is_causal
         self.sliding_window = sliding_window
 
+        if self.sliding_window <= 0:
+            raise ValueError(
+                f"Make sure that when passing `sliding_window` that its value is a strictly positive integer, not `{self.sliding_window}`"
+            )
+
     def to_causal_4d(
         self,
         batch_size: int,
