12a7a9a4ca5fabd8c95820695ce05694b58bba22
Patrick von Platen <patrick.v.platen@gmail.com>
2023-10-27 09:39:25 +0200

    Update src/transformers/models/idefics/modeling_idefics.py

--- src/transformers/models/idefics/modeling_idefics.py
+++ src/transformers/models/idefics/modeling_idefics.py
@@ -447,6 +447,7 @@ class IdeficsDecoupledLinear(nn.Linear):
 
 
 
+# this was adapted from LlamaRMSNorm
 class IdeficsRMSNorm(nn.Module):
     def __init__(self, hidden_size, eps=1e-6):
         """
