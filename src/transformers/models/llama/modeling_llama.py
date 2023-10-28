edcce345ba33f5ac01faa6a35a26dd52d45731da
Shashank Rajput <shashank.rajput@databricks.com>
2023-10-27 17:54:09 -0700

    Modified docstring

--- src/transformers/models/llama/modeling_llama.py
+++ src/transformers/models/llama/modeling_llama.py
@@ -193,7 +193,7 @@ def rotate_half(x):
 
 # Copied from transformers.models.gpt_neox.modeling_gpt_neox.apply_rotary_pos_emb
 def apply_rotary_pos_emb(q, k, cos, sin, position_ids, unsqueeze_dim=1):
-    """Applies Rotary Position Embedding to query and key tensors.
+    """Applies Rotary Position Embedding to the query and key tensors.
     
     Args:
         q (`torch.Tensor`): The query tensor.
