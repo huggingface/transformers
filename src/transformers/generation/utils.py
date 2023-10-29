fix attention mask

src/transformers/generation/utils.py
@@ -4440,63 +4440,20 @@ class GenerationMixin:
             # need access to the assistant cache to secure strong speedups.
             candidate_input_ids = input_ids
             assistant_attention_mask = model_kwargs.get("attention_mask", None)
-            if assistant_attention_mask is not None:
-                assistant_attention_mask = torch.cat(
-                    [
-                        assistant_attention_mask,
-                        torch.ones(
-                            [
-                                assistant_attention_mask.shape[0],
-                                input_ids.shape[-1] - assistant_attention_mask.shape[-1],
-                            ],
-                            dtype=assistant_attention_mask.dtype,
-                        ),
-                    ],
-                    dim=-1,
-                )
+            assistant_decoder_attention_mask = model_kwargs.get("decoder_attention_mask", None)
+            assistant_encoder_outputs = (model_kwargs.get("assistant_encoder_outputs", None),)
             for _ in range(int(num_assistant_tokens)):
                 # 1.1. use the assistant model to obtain the next candidate logits
-                assistant_past_key_values = model_kwargs.get("assistant_past_key_values", None)
-                assistant_position_ids = assistant_model.prepare_inputs_for_generation(
+                assistant_inputs = assistant_model.prepare_inputs_for_generation(
                     candidate_input_ids,
                     attention_mask=assistant_attention_mask,
-                    past_key_values=assistant_past_key_values,
-                ).get("position_ids", None)
-                if assistant_past_key_values is not None:
-                    prev_seq_len = assistant_past_key_values[0][assistant_kv_indexing].shape[-2]
-                    # `new_token_len` can be 1 or 2 (next token in assistant + last token picked by the larger model)
-                    new_token_len = candidate_input_ids.shape[1] - prev_seq_len
-                    assist_inputs = candidate_input_ids[:, -new_token_len:]
-                    # TODO (joao): make it compatible with models that use unconventional fwd pass logic, like blip2
-                    if assistant_model.config.is_encoder_decoder:
-                        assistant_model_outputs = assistant_model(
-                            decoder_input_ids=assist_inputs,
-                            attention_mask=assistant_attention_mask,
-                            position_ids=assistant_position_ids,
-                            past_key_values=assistant_past_key_values,
-                            encoder_outputs=model_kwargs["assistant_encoder_outputs"],
-                        )
-                    else:
-                        assistant_model_outputs = assistant_model(
-                            assist_inputs,
-                            attention_mask=assistant_attention_mask,
-                            position_ids=assistant_position_ids,
-                            past_key_values=assistant_past_key_values,
-                        )
-                else:
-                    if assistant_model.config.is_encoder_decoder:
-                        assistant_model_outputs = assistant_model(
-                            decoder_input_ids=candidate_input_ids,
-                            attention_mask=assistant_attention_mask,
-                            position_ids=assistant_position_ids,
-                            encoder_outputs=model_kwargs["assistant_encoder_outputs"],
-                        )
-                    else:
-                        assistant_model_outputs = assistant_model(
-                            candidate_input_ids,
-                            attention_mask=assistant_attention_mask,
-                            position_ids=assistant_position_ids,
-                        )
+                    decoder_attention_mask=assistant_decoder_attention_mask,
+                    encoder_outputs=assistant_encoder_outputs,
+                    past_key_values=model_kwargs.get("assistant_past_key_values", None),
+                )
+                assistant_model_outputs = assistant_model(
+                    **assistant_inputs,
+                )
 
                 # 1.2. greedily select the next candidate token
                 model_kwargs["assistant_past_key_values"] = assistant_model_outputs.past_key_values
@@ -4504,11 +4461,21 @@ class GenerationMixin:
                     assistant_model_outputs.logits[:, -1, :] = logits_processor(
                         candidate_input_ids, assistant_model_outputs.logits[:, -1, :]
                     )
+
                 new_token = assistant_model_outputs.logits[:, -1, :].argmax(dim=-1)
                 candidate_input_ids = torch.cat((candidate_input_ids, new_token[:, None]), dim=-1)
-                assistant_attention_mask = torch.cat(
-                    (assistant_attention_mask, torch.ones([1, 1], dtype=assistant_attention_mask.dtype)), dim=-1
-                )
+                if self.config.is_encoder_decoder and assistant_decoder_attention_mask is not None:
+                    assistant_decoder_attention_mask = torch.cat(
+                        (
+                            assistant_decoder_attention_mask,
+                            torch.ones([1, 1], dtype=assistant_decoder_attention_mask.dtype),
+                        ),
+                        dim=-1,
+                    )
+                elif not self.config.is_encoder_decoder and assistant_attention_mask is not None:
+                    assistant_attention_mask = torch.cat(
+                        (assistant_attention_mask, torch.ones([1, 1], dtype=assistant_attention_mask.dtype)), dim=-1
+                    )
 
                 # 1.3. stop assistant generation on EOS
                 if eos_token_id_tensor is not None: