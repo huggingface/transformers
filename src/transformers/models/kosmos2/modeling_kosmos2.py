07eac85c3954c80d7243a5fc53f1049a9cb2ee85
ydshieh <ydshieh@users.noreply.github.com>
2023-10-30 14:14:39 +0100

    fix

--- src/transformers/models/kosmos2/modeling_kosmos2.py
+++ src/transformers/models/kosmos2/modeling_kosmos2.py
@@ -660,7 +660,7 @@ class Kosmos2VisionEncoder(nn.Module):
             if output_hidden_states:
                 encoder_states = encoder_states + (hidden_states,)
             if self.gradient_checkpointing and self.training:
-                layer_outputs = self.gradient_checkpointing_func(
+                layer_outputs = self._gradient_checkpointing_func(
                     encoder_layer.__call__,
                     hidden_states,
                     attention_mask,
@@ -1268,7 +1268,7 @@ class Kosmos2TextTransformer(nn.Module):
             past_key_value = past_key_values[idx] if past_key_values is not None else None
 
             if self.gradient_checkpointing and self.training:
-                layer_outputs = self.gradient_checkpointing_func(
+                layer_outputs = self._gradient_checkpointing_func(
                     decoder_layer.__call__,
                     hidden_states,
                     attention_mask,
