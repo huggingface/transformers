d0d5f1ef7535b694d94c854a2e1945612fe4901a
Rafael <eng.rafaelpadilla@gmail.com>
2023-10-30 19:26:01 -0300

    transformation of bbox format + tests

--- src/transformers/bounding_box_utils.py
+++ src/transformers/bounding_box_utils.py
@@ -17,29 +17,61 @@ from typing import Union, Optional, Tuple
 import numpy as np
 import torch
 
-from .utils import (
+from transformers.utils import (
     ExplicitEnum,
 )
 
 class BoundingBoxFormat(ExplicitEnum):
-    XYXY = "xyxy"
-    XYWH = "xywh"
-    XCYCWH = "XCYCWH"
+    """Coordinate formats to represent a bounding box"""
+    XYXY = "xyxy"  # absolute coordinates
+    XYWH = "xywh"  # absolute coordinates
+    XCYCWH = "xcycwh"  # absolute coordinates
+    RELATIVE_XYXY = "relative_xyxy"  # relative coordinates
+    RELATIVE_XYWH  = "relative_xywh"  # relative coordinates
+    RELATIVE_XCYCWH = "relative_xcycwh"  # relative coordinates
+
+_relative_formats = (BoundingBoxFormat.RELATIVE_XYXY, BoundingBoxFormat.RELATIVE_XYWH, BoundingBoxFormat.RELATIVE_XCYCWH)
 
 def _infer_box_format(bbox: Union[torch.Tensor, np.ndarray]) -> BoundingBoxFormat:
     # check if bbox has 4 coordinates
     pass
 
-def _to_xyxy(bbox_xyxy):
-    pass
+def _xywh_to_xyxy(xywh: torch.Tensor, inplace: bool) -> torch.Tensor:
+    xyxy = xywh if inplace else xywh.clone()
+    xyxy[..., 2:].add_(xyxy[..., :2])
+    return xyxy
 
-def _to_xywh(bbox_xyxy):
-    pass
+def _xyxy_to_xywh(xyxy: torch.Tensor, inplace: bool) -> torch.Tensor:
+    xywh = xyxy if inplace else xyxy.clone()
+    xywh[..., 2:].sub_(xywh[..., :2])
+    return xywh
 
-def _to_xcycwh(bbox, orig_format):
-    pass
 
-def transform_box_format(bbox: Union[torch.Tensor, np.ndarray], dest_format: BoundingBoxFormat, orig_format: Optional[BoundingBoxFormat] = None, img_shape: Optional[Tuple[int, int]] = None):
+# adapted from https://github.com/pytorch/vision/blob/main/torchvision/transforms/v2/functional/_meta.py
+def _xcycwh_to_xyxy(xcycwh: torch.Tensor, inplace: bool) -> torch.Tensor:
+    xcycwh = xcycwh if inplace else xcycwh.clone()
+    # Trick to do fast division by 2 and ceil
+    rounding_mode = None if xcycwh.is_floating_point() else "floor"
+    half_wh = xcycwh[..., 2:].div(-2, rounding_mode=rounding_mode).abs_()
+    # (xc - width / 2) = x1 and (yc - height / 2) = x1
+    xcycwh[..., :2].sub_(half_wh)
+    # (x1 + width) = x2 and (y1 + height) = y2
+    xcycwh[..., 2:].add_(xcycwh[..., :2])
+    return xcycwh
+
+# adapted from https://github.com/pytorch/vision/blob/main/torchvision/transforms/v2/functional/_meta.py
+def _xyxy_to_xcycwh(xyxy: torch.Tensor, inplace: bool) -> torch.Tensor:
+    xyxy = xyxy if inplace else xyxy.clone()
+    # (x2 - x1) = width and (y2 - y1) = height
+    xyxy[..., 2:].sub_(xyxy[..., :2])  # => x, y, width, height
+    # cx and cy can be written with different terms
+    # (x1 * 2 + width)/2 = x1 + width/2 = x1 + (x2-x1)/2 = (x1 + x2)/2 = cy
+    # (y1 * 2 + height)/2 = y1 + height/2 = y1 + (y2-y1)/2 = (y1 + y2)/2 = cy
+    rounding_mode=None if xyxy.is_floating_point() else "floor"
+    xyxy[..., :2].mul_(2).add_(xyxy[..., 2:]).div_(2, rounding_mode=rounding_mode)
+    return xyxy
+
+def transform_box_format(bbox: Union[torch.Tensor, np.ndarray], dest_format: BoundingBoxFormat, orig_format: Optional[BoundingBoxFormat] = None, img_shape: Optional[Tuple[int, int]] = None, inplace: bool = False):
     if orig_format is None:
         orig_format = _infer_box_format(bbox)
     
@@ -47,27 +79,61 @@ def transform_box_format(bbox: Union[torch.Tensor, np.ndarray], dest_format: Bou
     if orig_format == dest_format:
         return bbox
     
-    if dest_format is BoundingBoxFormat.XCYCWH and img_shape is None:
+    if dest_format in _relative_formats and img_shape is None:
         raise ValueError(f"Image shape is required if the desired destination format is {dest_format}")
 
-    if orig_format is BoundingBoxFormat.XCYCWH and img_shape is None:
+    if orig_format in _relative_formats and img_shape is None:
         raise ValueError(f"Image shape is required if the input format format is {dest_format}")
 
-    if orig_format == BoundingBoxFormat.XYXY:
-        x, y, x2,y2 = bbox
-    elif orig_format == BoundingBoxFormat.XYWH:
-        x, y, w, h = bbox
-        x2, y2 = x+w, y+h
+    
+    # convert to xyxy
+    if orig_format == BoundingBoxFormat.XYWH:
+        bbox = _xywh_to_xyxy(bbox, inplace)
     elif orig_format == BoundingBoxFormat.XCYCWH:
-        cx, cy, w, h = bbox
-        x = cx - w/2
-        y = cy - h/2
-        x2 = cx + w/2
-        y2 = cy + h/2
-
-    if dest_format == BoundingBoxFormat.XYXY:
-        return x, y, x2, y2
-    elif dest_format == BoundingBoxFormat.XYWH:
-        return _to_xywh((x,y,x2,y2))
+        bbox = _xcycwh_to_xyxy(bbox, inplace)
+    # boxes are now in xyxy format
+    if dest_format == BoundingBoxFormat.XYWH:
+        bbox = _xyxy_to_xywh(bbox, inplace)
     elif dest_format == BoundingBoxFormat.XCYCWH:
-        return _to_xcycwh((x,y,x2,y2))
+        bbox = _xyxy_to_xcycwh(bbox, inplace)
+
+    return bbox
+
+# General transformations
+def horizontal_flip():
+    pass
+
+def vertical_flip():
+    pass
+
+def resize():
+    pass
+
+def crop():
+    pass
+
+boxes_xywh = torch.Tensor([[10, 20, 50, 60],
+            [25, 35, 40, 45],
+            [5, 5, 100, 100],
+            [15, 30, 55, 75],
+            [60, 70, 30, 20],
+            [0, 0, 50, 50],
+            [45, 10, 90, 80],
+            [100, 200, 120, 130],
+            [80, 90, 40, 60],
+            [110, 120, 70, 90]])
+
+# assert torch.equal(boxes_xywh, transform_box_format(boxes_xywh, dest_format = BoundingBoxFormat.XYWH, orig_format = BoundingBoxFormat.XYWH))
+
+# res = transform_box_format(boxes_xywh, dest_format = BoundingBoxFormat.XYXY, orig_format = BoundingBoxFormat.XYWH)
+# xywh = torch.Tensor([[10, 20, 60, 80],
+#         [25, 35, 65, 80],
+#         [5, 5, 105, 105],
+#         [15, 30, 70, 105],
+#         [60, 70, 90, 90],
+#         [0, 0, 50, 50],
+#         [45, 10, 135, 90],
+#         [100, 200, 220, 330],
+#         [80, 90, 120, 150],
+#         [110, 120, 180, 210]])
+# assert torch.equal(res, xywh)
