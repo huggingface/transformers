4d32902fd98404544a57b76e3f2eaab27ca92676
ydshieh <ydshieh@users.noreply.github.com>
2023-10-30 15:20:36 +0100

    fix

--- src/transformers/models/kosmos2/modeling_kosmos2.py
+++ src/transformers/models/kosmos2/modeling_kosmos2.py
@@ -1112,7 +1112,6 @@ class Kosmos2TextTransformer(nn.Module):
 
         self.gradient_checkpointing = False
 
-    # Copied from transformers.models.bart.modeling_bart.BartDecoder._prepare_decoder_attention_mask
     def _prepare_decoder_attention_mask(self, attention_mask, input_shape, inputs_embeds, past_key_values_length):
         # create causal mask
         # [bsz, seq_len] -> [bsz, 1, tgt_seq_len, src_seq_len]
