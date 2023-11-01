fed5e541ac7755a11a06ec649ed9fe35b006fe85
Lysandre <lysandre.debut@reseau.eseo.fr>
2023-11-01 13:32:29 +0100

    Fix disk offload tests + weight sharing issues

--- src/transformers/models/plbart/modeling_plbart.py
+++ src/transformers/models/plbart/modeling_plbart.py
@@ -1084,6 +1084,11 @@ class PLBartModel(PLBartPreTrainedModel):
         self.encoder.embed_tokens = self.shared
         self.decoder.embed_tokens = self.shared
 
+    def _tie_weights(self):
+        if self.config.tie_word_embeddings:
+            self._tie_or_clone_weights(self.encoder.embed_tokens, self.shared)
+            self._tie_or_clone_weights(self.decoder.embed_tokens, self.shared)
+
     def get_encoder(self):
         return self.encoder
 
