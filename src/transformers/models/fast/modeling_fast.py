51ec119b1f8a7178cd5c3078d9401913b99b1b55
raghavanone <opensourcemaniacfreak@gmail.com>
2023-11-01 17:50:10 +0530

    Fix build

--- src/transformers/models/fast/modeling_fast.py
+++ src/transformers/models/fast/modeling_fast.py
@@ -9,6 +9,7 @@ import torch.nn.functional as F
 from transformers import FastConfig, PreTrainedModel, add_start_docstrings
 from transformers.utils import ModelOutput, add_start_docstrings_to_model_forward, replace_return_docstrings
 
+
 _CONFIG_FOR_DOC = "FastConfig"
 
 FAST_START_DOCSTRING = r"""
@@ -66,7 +67,7 @@ def build_activation(act_func, inplace=True):
 
 class My2DLayer(nn.Module):
     def __init__(
-            self, in_channels, out_channels, use_bn=True, act_func="relu", dropout_rate=0, ops_order="weight_bn_act"
+        self, in_channels, out_channels, use_bn=True, act_func="relu", dropout_rate=0, ops_order="weight_bn_act"
     ):
         super(My2DLayer, self).__init__()
         self.in_channels = in_channels
@@ -163,19 +164,19 @@ class My2DLayer(nn.Module):
 
 class ConvLayer(nn.Module):
     def __init__(
-            self,
-            in_channels,
-            out_channels,
-            kernel_size=3,
-            stride=1,
-            dilation=1,
-            groups=1,
-            bias=False,
-            has_shuffle=False,
-            use_bn=True,
-            act_func="relu",
-            dropout_rate=0,
-            use_act=True,
+        self,
+        in_channels,
+        out_channels,
+        kernel_size=3,
+        stride=1,
+        dilation=1,
+        groups=1,
+        bias=False,
+        has_shuffle=False,
+        use_bn=True,
+        act_func="relu",
+        dropout_rate=0,
+        use_act=True,
     ):
         super().__init__()
 
@@ -449,48 +450,48 @@ class TextNet(nn.Module):
         # self.first_conv.apply(self._init_weights)
         stage1 = []
         for stage_config in zip(
-                config.backbone_stage1_in_channels,
-                config.backbone_stage1_out_channels,
-                config.backbone_stage1_kernel_size,
-                config.backbone_stage1_stride,
-                config.backbone_stage1_dilation,
-                config.backbone_stage1_groups,
+            config.backbone_stage1_in_channels,
+            config.backbone_stage1_out_channels,
+            config.backbone_stage1_kernel_size,
+            config.backbone_stage1_stride,
+            config.backbone_stage1_dilation,
+            config.backbone_stage1_groups,
         ):
             stage1.append(RepConvLayer(*stage_config))
         self.stage1 = nn.ModuleList(stage1)
 
         stage2 = []
         for stage_config in zip(
-                config.backbone_stage2_in_channels,
-                config.backbone_stage2_out_channels,
-                config.backbone_stage2_kernel_size,
-                config.backbone_stage2_stride,
-                config.backbone_stage2_dilation,
-                config.backbone_stage2_groups,
+            config.backbone_stage2_in_channels,
+            config.backbone_stage2_out_channels,
+            config.backbone_stage2_kernel_size,
+            config.backbone_stage2_stride,
+            config.backbone_stage2_dilation,
+            config.backbone_stage2_groups,
         ):
             stage2.append(RepConvLayer(*stage_config))
         self.stage2 = nn.ModuleList(stage2)
 
         stage3 = []
         for stage_config in zip(
-                config.backbone_stage3_in_channels,
-                config.backbone_stage3_out_channels,
-                config.backbone_stage3_kernel_size,
-                config.backbone_stage3_stride,
-                config.backbone_stage3_dilation,
-                config.backbone_stage3_groups,
+            config.backbone_stage3_in_channels,
+            config.backbone_stage3_out_channels,
+            config.backbone_stage3_kernel_size,
+            config.backbone_stage3_stride,
+            config.backbone_stage3_dilation,
+            config.backbone_stage3_groups,
         ):
             stage3.append(RepConvLayer(*stage_config))
         self.stage3 = nn.ModuleList(stage3)
 
         stage4 = []
         for stage_config in zip(
-                config.backbone_stage4_in_channels,
-                config.backbone_stage4_out_channels,
-                config.backbone_stage4_kernel_size,
-                config.backbone_stage4_stride,
-                config.backbone_stage4_dilation,
-                config.backbone_stage4_groups,
+            config.backbone_stage4_in_channels,
+            config.backbone_stage4_out_channels,
+            config.backbone_stage4_kernel_size,
+            config.backbone_stage4_stride,
+            config.backbone_stage4_dilation,
+            config.backbone_stage4_groups,
         ):
             stage4.append(RepConvLayer(*stage_config))
         self.stage4 = nn.ModuleList(stage4)
@@ -712,8 +713,7 @@ class FASTHead(nn.Module):
 
 
 def emb_loss(
-        emb, instance, kernel, training_mask, feature_dim=4, delta_v=0.5, delta_d=1.5, weights=(1.0, 1.0),
-        bg_sample=False
+    emb, instance, kernel, training_mask, feature_dim=4, delta_v=0.5, delta_d=1.5, weights=(1.0, 1.0), bg_sample=False
 ):
     training_mask = (training_mask > 0.5).long()
     kernel = (kernel > 0.5).long()
@@ -740,7 +740,7 @@ def emb_loss(
             continue
         ind = instance == lb
         emb_ = emb[:, ind]
-        dist = (emb_ - emb_mean[:, i: i + 1]).norm(p=2, dim=0)
+        dist = (emb_ - emb_mean[:, i : i + 1]).norm(p=2, dim=0)
         dist = F.relu(dist - delta_v) ** 2
         l_agg[i] = torch.mean(torch.log(dist + 1.0))
     l_agg = torch.mean(l_agg[1:])
@@ -772,7 +772,7 @@ def emb_loss(
                 for i, lb in enumerate(unique_labels):
                     if lb == 0:
                         continue
-                    dist = (emb_bg - emb_mean[:, i: i + 1]).norm(p=2, dim=0)
+                    dist = (emb_bg - emb_mean[:, i : i + 1]).norm(p=2, dim=0)
                     dist = F.relu(2 * delta_d - dist) ** 2
                     l_dis_bg = torch.mean(torch.log(dist + 1.0), 0, keepdim=True)
                     l_dis.append(l_dis_bg)
@@ -969,11 +969,11 @@ class FASTForImageCaptioning(FastPreTrainedModel):
     @add_start_docstrings_to_model_forward(FAST_FOR_CAPTIONING_INPUTS_DOCSTRING)
     @replace_return_docstrings(output_type=FASTForImageCaptioningOutput, config_class=_CONFIG_FOR_DOC)
     def forward(
-            self,
-            pixel_values: torch.FloatTensor,
-            output_hidden_states: Optional[bool] = True,
-            return_dict: Optional[bool] = None,
-            labels: Dict = None,
+        self,
+        pixel_values: torch.FloatTensor,
+        output_hidden_states: Optional[bool] = True,
+        return_dict: Optional[bool] = None,
+        labels: Dict = None,
     ):
         r"""
         labels (`torch.LongTensor` of shape `(batch_size, height, width)`, *optional*):
