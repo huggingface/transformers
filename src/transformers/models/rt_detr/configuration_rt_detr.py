1f41f668d2f2b4d951de42313572957e68badb5e
Rafael Padilla <rafael.padilla@huggingface.co>
2023-11-02 02:08:07 +0000

    completing config docstring

--- src/transformers/models/rt_detr/configuration_rt_detr.py
+++ src/transformers/models/rt_detr/configuration_rt_detr.py
@@ -25,7 +25,6 @@ RT_DETR_PRETRAINED_CONFIG_ARCHIVE_MAP = {
     "rafaelpadilla/porting_rt_detr": "https://huggingface.co/rafaelpadilla/porting_rt_detr/raw/main/config.json",
 }
 
-# TODO: Docstrings (Rafael)
 class RTDetrConfig(PretrainedConfig):
     r"""
     This is the configuration class to store the configuration of a [`RTDetrModel`]. It is used to instantiate a
@@ -37,25 +36,73 @@ class RTDetrConfig(PretrainedConfig):
     documentation from [`PretrainedConfig`] for more information.
 
     Args:
-        TODO: Missing docstrings
+        initializer_range (`float`, *optional*, defaults to 0.02):
+            The standard deviation of the truncated_normal_initializer for initializing all weight matrices.
+        backbone (`str`, *optional*, defaults to "resnet50d"):
+            Name of convolutional backbone to use.
+        out_indices (`List[int]`, *optional*, defaults to [2,3,4]): 
+            List of indices of features to output. Can be any of 0, 1, 2, etc. (depending on how
+            many stages the backbone has). 
+        freeze_batch_norm_2d (`bool`, *optional*, defaults to True):
+            If True, all `BatchNorm2d` and `SyncBatchNorm` layers of the backbone will be replaced by `FrozenBatchNorm2d`.
+        in_channels (`List[int]`, *optional*, defaults to [512, 1024, 2048]):
+            List of input channel sizes to be used in each block of the backbone's convolutional layers.
+        feat_strides (`List[int]`, *optional*, defaults to [8, 16, 32]):
+            Strides used in each feature map.
+        hidden_dim (`int`, *optional*, defaults to 256):
+            Dimension for hidden states in transformer encoder and decoder.
+        num_head (`int`, *optional*, defaults to 8):
+            Number of attention heads for each attention layer in the transformer encoder and decoder.
+        dim_feedforward (`int`, *optional*, defaults to 1024):
+            Dimension for feedforward network layer in transformer encoder and decoder.
+        dropout (`float`, *optional*, defaults to 0.):
+            The ratio for all dropout layers.
+        enc_act (`str`, *optional*, defaults to "gelu"): 
+            Activation function of the encoder used in the `TransformerEncoderLayer`.
+        use_encoder_idx (`List[int]`, *optional*, defaults to [2]):
+            Indexes of the projected layers to be used in the encoder.
+        num_encoder_layers (`int`, *optional*, defaults to 1):
+            Total of layers to be used by the encoder.
+        pe_temperature (`int`, *optional*, defaults to 10000):
+            The temperature parameter used to create the positional encodings.
+        expansion (`float`, *optional*, defaults to 1.):
+            Expansion factor used by the `CSPRepLayer` module.
+        depth_mult (`float`, *optional*, defaults to 1.):
+            Depth multiplicator factor used to create the `CSPRepLayer` module.
+        act_encoder (`str`, *optional*, defaults to "silu"):
+            Activation function of the encoder used in the top-down Feature Pyramid Network and the bottom-up Path Aggregation Network. 
+        eval_size (`Tuple[int, int]`, *optional*, defaults to None):
+            Height and width used to computes the effective height and width of the position embeddings after taking into account the stride.
         num_classes (`int`, *optional*, defaults to 80):
+            Number of target classes or labels used by the detector.
         num_queries (`int`, *optional*, defaults to 300):
+            Number of object queries.
         position_embed_type (`str`, *optional*, defaults to "sine"):
-        feat_channels (`List[int]`, *optional*, defaults to [512, 1024, 2048]):,
+            A string indicating the type of positional embedding to use. Supported values ["sine", "learned"]
+        feat_channels (`List[int]`, *optional*, defaults to [256, 256, 256]):
+            A list of integers representing the number of feature channels at various layers or stages of the network
         num_levels (`int`, *optional*, defaults to 3):
+            The number of feature levels used by the `RTDetrTransformers`.
         num_decoder_points (`int`, *optional*, defaults to 4):
+            Number of points used by the `TransformerDecoderLayer`.
         num_decoder_layers (`int`, *optional*, defaults to 6):
+            Number of layers of the decoder.
         act_decoder (`str`, *optional*, defaults to "relu"):
+            Activation function used by the decoder.
         num_denoising (`int`, *optional*, defaults to 100):
+            The total number of denoising tasks or queries to be used for contrastive denoising.
         label_noise_ratio (`float`, *optional*, defaults to 0.5):
+            The fraction of denoising labels to which random noise should be added.
         box_noise_scale (`float`, *optional*, defaults to 1.0):
+            Scale or magnitude of noise to be added to the bounding boxes.
         learnt_init_query (`bool`, *optional*, defaults to False):
-        eval_spatial_size  (`Tuple[int, int]`, *optional*, defaults to None):
+            Indicates whether the initial query embeddings for the decoder should be learned during training
+        eval_spatial_size  (`Tuple[int, int]`, *optional*, defaults to [640, 640]):
             Height and width used during evaluation to generate the bounding box anchors.
         eval_idx (`int`, *optional*, defaults to -1):
             Id of the decoder layer used to obtain the logits and bounding boxes.
         eps (`float`, *optional*, defaults to 1e-2):
-
+            A small positive value used to define the valid range for anchor coordinates. 
         matcher_alpha (`float`, *optional*, defaults to 0.25):
             Parameter alpha used by the Hungarian Matcher.
         matcher_gamma (`float`, *optional*, defaults to 2.0):
@@ -131,7 +178,7 @@ class RTDetrConfig(PretrainedConfig):
         num_classes=80,
         num_queries=300,
         position_embed_type="sine",
-        feat_channels=[512, 1024, 2048],
+        feat_channels=[256, 256, 256],
         num_levels=3,
         num_decoder_points=4,
         num_decoder_layers=6,
@@ -140,7 +187,7 @@ class RTDetrConfig(PretrainedConfig):
         label_noise_ratio=0.5,
         box_noise_scale=1.0,
         learnt_init_query=False,
-        eval_spatial_size=None,
+        eval_spatial_size=[640, 640],
         eval_idx=-1,
         eps=1e-2,
         # Loss
