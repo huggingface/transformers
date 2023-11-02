0bd7493ab196cc24e7024ad603000a630a13014d
Rafael Padilla <rafael.padilla@huggingface.co>
2023-11-02 02:06:57 +0000

    adding image processing in the AutoImageProcessor

--- src/transformers/models/auto/image_processing_auto.py
+++ src/transformers/models/auto/image_processing_auto.py
@@ -94,6 +94,7 @@ IMAGE_PROCESSOR_MAPPING_NAMES = OrderedDict(
         ("pvt", "PvtImageProcessor"),
         ("regnet", "ConvNextImageProcessor"),
         ("resnet", "ConvNextImageProcessor"),
+        ("rt_detr", "RTDetrImageProcessor"),
         ("sam", "SamImageProcessor"),
         ("segformer", "SegformerImageProcessor"),
         ("swiftformer", "ViTImageProcessor"),
