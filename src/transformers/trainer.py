6bec5f5fd3a4c4f97df5395cf131066d86d83f21
Komal K. Teru <komal.teru@gmail.com>
2023-11-02 12:33:23 -0400

    Changes to first unwrap the model then extract the base model name

--- src/transformers/trainer.py
+++ src/transformers/trainer.py
@@ -2752,10 +2752,11 @@ class Trainer:
             self._past = outputs[self.args.past_index]
 
         if labels is not None:
-            if is_peft_available() and isinstance(model, PeftModel):
-                model_name = unwrap_model(model.base_model.model)._get_name()
+            unwrapped_model = unwrap_model(model)
+            if is_peft_available() and isinstance(unwrapped_model, PeftModel):
+                model_name = unwrapped_model.base_model.model._get_name()
             else:
-                model_name = unwrap_model(model)._get_name()
+                model_name = unwrapped_model._get_name()
             if model_name in MODEL_FOR_CAUSAL_LM_MAPPING_NAMES.values():
                 loss = self.label_smoother(outputs, labels, shift_labels=True)
             else:
