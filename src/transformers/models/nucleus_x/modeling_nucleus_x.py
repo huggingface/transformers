9d8cf38bf8147e1731276c6c0f38e0b578b97dbc
Sehyun Choi <choisehyun98@gmail.com>
2023-11-04 01:34:30 +0900

    remove timm dependency and copy drop_path

--- src/transformers/models/nucleus_x/modeling_nucleus_x.py
+++ src/transformers/models/nucleus_x/modeling_nucleus_x.py
@@ -37,7 +37,6 @@ import numpy as np
 import torch
 import torch.nn.functional as F
 import torch.utils.checkpoint
-from timm.layers import drop_path
 from torch import nn
 from torch.nn import BCEWithLogitsLoss, CrossEntropyLoss, MSELoss
 
@@ -523,6 +522,26 @@ class NucleusXGLU(nn.Module):
         x = x.view(x_shape)
         x = self.dropout_module(x)
         return x
+
+
+# Copied from timm.layers.drop.drop_path
+def drop_path(x, drop_prob: float = 0.0, training: bool = False, scale_by_keep: bool = True):
+    """Drop paths (Stochastic Depth) per sample (when applied in main path of residual blocks).
+
+    This is the same as the DropConnect impl I created for EfficientNet, etc networks, however, the original name is
+    misleading as 'Drop Connect' is a different form of dropout in a separate paper... See discussion:
+    https://github.com/tensorflow/tpu/issues/494#issuecomment-532968956 ... I've opted for changing the layer and
+    argument names to 'drop path' rather than mix DropConnect as a layer name and use 'survival rate' as the argument.
+
+    """
+    if drop_prob == 0.0 or not training:
+        return x
+    keep_prob = 1 - drop_prob
+    shape = (x.shape[0],) + (1,) * (x.ndim - 1)  # work with diff dim tensors, not just 2D ConvNets
+    random_tensor = x.new_empty(shape).bernoulli_(keep_prob)
+    if keep_prob > 0.0 and scale_by_keep:
+        random_tensor.div_(keep_prob)
+    return x * random_tensor
 
 
 class NucleusXDropPath(nn.Module):
