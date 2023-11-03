Fix attention mask when padding mask is not null

src/transformers/modeling_attn_mask_utils.py
@@ -128,7 +128,7 @@ class AttentionMaskConverter:
         Make causal mask used for bi-directional self-attention.
         """
         bsz, tgt_len = input_ids_shape
-        mask = torch.full((tgt_len, tgt_len), torch.finfo(dtype).min, device=device)
+        mask = torch.full((tgt_len, tgt_len), float("-inf"), device=device)
         mask_cond = torch.arange(mask.size(-1), device=device)
         mask.masked_fill_(mask_cond < (mask_cond + 1).view(mask.size(-1), 1), 0)
 
@@ -142,7 +142,7 @@ class AttentionMaskConverter:
             diagonal = past_key_values_length - sliding_window + 1
 
             context_mask = 1 - torch.triu(torch.ones_like(mask, dtype=torch.int), diagonal=diagonal)
-            mask.masked_fill_(context_mask.bool(), torch.finfo(dtype).min)
+            mask.masked_fill_(context_mask.bool(), float("-inf"))
 
         return mask[None, None, :, :].expand(bsz, 1, tgt_len, tgt_len + past_key_values_length)
 
@@ -154,11 +154,11 @@ class AttentionMaskConverter:
         bsz, src_len = mask.size()
         tgt_len = tgt_len if tgt_len is not None else src_len
 
-        expanded_mask = mask[:, None, None, :].expand(bsz, 1, tgt_len, src_len).to(dtype)
+        expanded_mask = mask[:, None, None, -tgt_len:].expand(bsz, 1, src_len, tgt_len).transpose(-2, -1).to(dtype)
 
         inverted_mask = 1.0 - expanded_mask
 
-        return inverted_mask.masked_fill(inverted_mask.to(torch.bool), torch.finfo(dtype).min)
+        return inverted_mask.masked_fill(inverted_mask.to(torch.bool), float("-inf"))
 
 
 def _prepare_4d_causal_attention_mask(