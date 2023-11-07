e78684e97aa819171fbd1e994f05732fbffa7e0c
Jakob Steinn <jstoone@drk.sh>
2023-11-07 07:51:09 +0100

    Add large-v3 checkpoint.

--- src/transformers/models/whisper/convert_openai_to_hf.py
+++ src/transformers/models/whisper/convert_openai_to_hf.py
@@ -36,6 +36,7 @@ _MODELS = {
     "medium": "https://openaipublic.azureedge.net/main/whisper/models/345ae4da62f9b3d59415adc60127b97c714f32e89e936602e85993674d08dcb1/medium.pt",
     "large": "https://openaipublic.azureedge.net/main/whisper/models/e4b87e7e0bf463eb8e6956e646f1e277e901512310def2c24bf0e11bd3c28e9a/large.pt",
     "large-v2": "https://openaipublic.azureedge.net/main/whisper/models/81f7c96c852ee8fc832187b0132e569d6c3065a3252ed18e56effd0b6a73e524/large-v2.pt",
+    "large-v3": "https://openaipublic.azureedge.net/main/whisper/models/e5b1a55b89c1367dacf97e3e19bfd829a01529dbfdeefa8caeb59b3f1b81dadb/large-v3.pt",
 }
 
 
