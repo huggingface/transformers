6d7ab00f268ccd8cadec2c2bf1a7223bfd223e91
raghavanone <opensourcemaniacfreak@gmail.com>
2023-11-08 18:05:44 +0530

    Fix failures

--- src/transformers/models/textnet/modeling_textnet.py
+++ src/transformers/models/textnet/modeling_textnet.py
@@ -502,19 +502,19 @@ class TextNetBackbone(TextNetPreTrainedModel, BackboneMixin):
         Examples:
 
         ```python
-        >>> from transformers import AutoImageProcessor, AutoBackbone
-        >>> import torch
-        >>> from PIL import Image
-        >>> import requests
-
-        >>> url = "http://images.cocodataset.org/val2017/000000039769.jpg"
-        >>> image = Image.open(requests.get(url, stream=True).raw)
-
-        >>> processor = AutoImageProcessor.from_pretrained("google/resnetnv2-50")
-        >>> model = AutoBackbone.from_pretrained("google/resnetnv2-50")
-
-        >>> inputs = processor(image, return_tensors="pt")
-        >>> outputs = model(**inputs)
+        # >>> from transformers import AutoImageProcessor, AutoBackbone
+        # >>> import torch
+        # >>> from PIL import Image
+        # >>> import requests
+        #
+        # >>> url = "http://images.cocodataset.org/val2017/000000039769.jpg"
+        # >>> image = Image.open(requests.get(url, stream=True).raw)
+        #
+        # >>> processor = AutoImageProcessor.from_pretrained("google/resnetnv2-50")
+        # >>> model = AutoBackbone.from_pretrained("google/resnetnv2-50")
+        #
+        # >>> inputs = processor(image, return_tensors="pt")
+        # >>> outputs = model(**inputs)
         ```"""
         return_dict = return_dict if return_dict is not None else self.config.use_return_dict
         output_hidden_states = (
