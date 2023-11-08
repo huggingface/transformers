5790f2bfbcc3168a892eb6776adf57a72bc802a4
Rafael Padilla <31217453+rafaelpadilla@users.noreply.github.com>
2023-11-07 22:35:10 -0300

    Update src/transformers/models/rt_detr/modeling_rt_detr.py
    Co-authored-by: amyeroberts <22614925+amyeroberts@users.noreply.github.com>

--- src/transformers/models/rt_detr/modeling_rt_detr.py
+++ src/transformers/models/rt_detr/modeling_rt_detr.py
@@ -1511,7 +1511,7 @@ class HybridEncoder(RTDetrPreTrainedModel):
         # encoder
         if self.num_encoder_layers > 0:
             for i, enc_ind in enumerate(self.use_encoder_idx):
-                h, w = proj_feats[enc_ind].shape[2:]
+                height, weight = proj_feats[enc_ind].shape[2:]
                 # flatten [B, C, H, W] to [B, HxW, C]
                 src_flatten = proj_feats[enc_ind].flatten(2).permute(0, 2, 1)
                 if self.training or self.eval_size is None:
