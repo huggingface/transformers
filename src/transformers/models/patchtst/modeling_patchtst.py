7ab8d59233d42da87bcd1c37c35bf53bd6513f60
nnguyen <nnguyen@us.ibm.com>
2023-11-08 19:55:19 -0500

    add config as an argument to all the scaler classes

--- src/transformers/models/patchtst/modeling_patchtst.py
+++ src/transformers/models/patchtst/modeling_patchtst.py
@@ -206,7 +206,7 @@ class PatchTSTBatchNorm(nn.Module):
         d_model (`int`): model dimension
     """
 
-    def __init__(self, config):
+    def __init__(self, config: PatchTSTConfig):
         super().__init__()
         self.batchnorm = nn.BatchNorm1d(config.d_model)
 
@@ -1073,8 +1073,11 @@ class PatchTSTStdScaler(nn.Module):
     by subtracting from the mean and dividing by the standard deviation.
     """
 
-    def __init__(self):
+    def __init__(self, config: PatchTSTConfig):
         super().__init__()
+        self.dim = 1 if config.scaling_dim is None else config.scaling_dim
+        self.keepdim = True if config.keepdim is None else config.keepdim
+        self.minimum_scale = 1e-10 if config.minimum_scale is None else config.minimum_scale
 
     @torch.no_grad()
     def forward(self, data: torch.Tensor, observed_indicator: torch.Tensor
@@ -1090,12 +1093,12 @@ class PatchTSTStdScaler(nn.Module):
                 (`(batch_size, sequence_length, num_input_channels)`,`(batch_size, 1, num_input_channels)`,
                 `(batch_size, 1, num_input_channels)`)
         """
-        denominator = observed_indicator.sum(dim=1, keepdim=True)
+        denominator = weights.sum(self.dim, keepdim=self.keepdim)
         denominator = denominator.clamp_min(1.0)
-        loc = (data * observed_indicator).sum(dim=1, keepdim=True) / denominator
+        loc = (data * weights).sum(self.dim, keepdim=self.keepdim) / denominator
 
-        variance = (((data - loc) * observed_indicator) ** 2).sum(dim=1, keepdim=True) / denominator
-        scale = torch.sqrt(variance + 1e-10)
+        variance = (((data - loc) * weights) ** 2).sum(self.dim, keepdim=self.keepdim) / denominator
+        scale = torch.sqrt(variance + self.minimum_scale)
         return (data - loc) / scale, loc, scale
 
 
@@ -1105,8 +1108,12 @@ class PatchTSTMeanScaler(nn.Module):
     Computes a scaling factor as the weighted average absolute value along the first dimension, and scales the data
     accordingly.
     """
-    def __init__(self):
+    def __init__(self, config: PatchTSTConfig):
         super().__init__()
+        self.dim = 1 if config.scaling_dim is None else config.scaling_dim
+        self.keepdim = True if config.keepdim is None else config.keepdim
+        self.minimum_scale = 1e-10 if config.minimum_scale is None else config.minimum_scale
+        self.default_scale = config.default_scale if config.default_scale else None
 
     @torch.no_grad()
     def forward(
@@ -1123,21 +1130,30 @@ class PatchTSTMeanScaler(nn.Module):
                 (`(batch_size, sequence_length, num_input_channels)`,`(batch_size, 1, num_input_channels)`,
                 `(batch_size, 1, num_input_channels)`)
         """
-        ts_sum = (data * observed_indicator).abs().sum(dim=1, keepdim=True)
-        num_observed = observed_indicator.sum(dim=1, keepdim=True)
+        ts_sum = (data * observed_indicator).abs().sum(self.dim, keepdim=True)
+        num_observed = observed_indicator.sum(self.dim, keepdim=True)
+
         scale = ts_sum / torch.clamp(num_observed, min=1)
 
-        # use the scale of the batch.
-        batch_sum = ts_sum.sum(dim=0)
-        batch_observations = torch.clamp(num_observed.sum(0), min=1)
-        default_scale = torch.squeeze(batch_sum / batch_observations)
+        # If `default_scale` is provided, we use it, otherwise we use the scale
+        # of the batch.
+        if self.default_scale is None:
+            batch_sum = ts_sum.sum(dim=0)
+            batch_observations = torch.clamp(num_observed.sum(0), min=1)
+            default_scale = torch.squeeze(batch_sum / batch_observations)
+        else:
+            default_scale = self.default_scale * torch.ones_like(scale)
 
         # apply default scale where there are no observations
         scale = torch.where(num_observed > 0, scale, default_scale)
 
-        # ensure the scale is at least 1e-10
-        scale = torch.clamp(scale, min=1e-10)
+        # ensure the scale is at least `self.minimum_scale`
+        scale = torch.clamp(scale, min=self.minimum_scale)
         scaled_data = data / scale
+
+        if not self.keepdim:
+            scale = scale.squeeze(dim=self.dim)
+
         return scaled_data, torch.zeros_like(scale), scale
 
 
@@ -1147,8 +1163,10 @@ class PatchTSTNOPScaler(nn.Module):
     Assigns a scaling factor equal to 1 along the first dimension, and therefore applies no scaling to the input data.
     """
 
-    def __init__(self):
+    def __init__(self, config: PatchTSTConfig):
         super().__init__()
+        self.dim = 1 if config.scaling_dim is None else config.scaling_dim
+        self.keepdim = True if config.keepdim is None else config.keepdim
 
     def forward(
         self, data: torch.Tensor, observed_indicator: torch.Tensor=None
@@ -1162,8 +1180,8 @@ class PatchTSTNOPScaler(nn.Module):
                 (`(batch_size, sequence_length, num_input_channels)`,`(batch_size, 1, num_input_channels)`,
                 `(batch_size, 1, num_input_channels)`)
         """
-        scale = torch.ones_like(data, requires_grad=False).mean(dim=1, keepdim=True)
-        loc = torch.zeros_like(data, requires_grad=False).mean(dim=1, keepdim=True)
+        scale = torch.ones_like(data, requires_grad=False).mean(dim=self.dim, keepdim=self.keepdim)
+        loc = torch.zeros_like(data, requires_grad=False).mean(dim=self.dim, keepdim=self.keepdim)
         return data, loc, scale
 
 
@@ -1171,11 +1189,11 @@ class PatchTSTScaler(nn.Module):
     def __init__(self, config: PatchTSTConfig):
         super().__init__()
         if config.scaling == "mean" or config.scaling is True:
-            self.scaler = PatchTSTMeanScaler()
+            self.scaler = PatchTSTMeanScaler(config)
         elif config.scaling == "std":
-            self.scaler = PatchTSTStdScaler()
+            self.scaler = PatchTSTStdScaler(config)
         else:
-            self.scaler = PatchTSTNOPScaler()
+            self.scaler = PatchTSTNOPScaler(config)
 
     def forward(
         self, data: torch.Tensor, observed_indicator: torch.Tensor
@@ -1266,7 +1284,7 @@ class PatchTSTMaskPretrainHead(nn.Module):
     Pretraining head for mask modelling
     """
 
-    def __init__(self, config):
+    def __init__(self, config: PatchTSTConfig):
         super().__init__()
         self.dropout = nn.Dropout(config.dropout)
         self.linear = nn.Linear(config.d_model, config.patch_length)
