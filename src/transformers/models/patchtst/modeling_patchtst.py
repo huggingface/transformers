01294fd0e9302e467e8e928fb5ec3aae9d89806b
Kashif Rasul <kashif.rasul@gmail.com>
2023-11-13 11:21:13 +0100

    style

--- src/transformers/models/patchtst/modeling_patchtst.py
+++ src/transformers/models/patchtst/modeling_patchtst.py
@@ -770,7 +770,9 @@ class PatchTSTEncoder(PatchTSTPreTrainedModel):
             `BaseModelOutput`
         """
         output_attentions = output_attentions if output_attentions is not None else self.config.output_attentions
-        output_hidden_states = output_hidden_states if output_hidden_states is not None else self.config.output_hidden_states
+        output_hidden_states = (
+            output_hidden_states if output_hidden_states is not None else self.config.output_hidden_states
+        )
 
         # Input embedding
         patch_input = self.embedder(patch_input)
