8f7e1a2d1b890fe4bf866c6e735274d332e8d9cb
ArthurZucker <arthur.zucker@gmail.com>
2023-11-15 05:00:57 -0500

    nit current path

--- src/transformers/models/udop/convert_udop_to_pytorch.py
+++ src/transformers/models/udop/convert_udop_to_pytorch.py
@@ -83,7 +83,7 @@ def prepare_dummy_inputs(tokenizer, image_processor):
 
 def convert_udop_checkpoint(model_name, pytorch_dump_folder_path=None, push_to_hub=False):
     model_name_to_checkpoint_path = {
-        "udop-large": "/Users/nielsrogge/Downloads/udop-unimodel-large-224/pytorch_model.bin",
+        "udop-large": "/home/arthur/transformers/udop-unimodel-large-224/pytorch_model.bin",
         "udop-dual-large": "",
     }
 
