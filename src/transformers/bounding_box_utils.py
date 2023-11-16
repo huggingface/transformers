a622cfc0353d5d4f4edd75cf07c87d455248a852
Rafael Padilla <rafael.padilla@huggingface.co>
2023-11-16 21:17:39 +0000

    finishing pytests and transformations with rounding

--- src/transformers/bounding_box_utils.py
+++ src/transformers/bounding_box_utils.py
@@ -15,12 +15,15 @@
 
 from typing import Optional, Tuple, Union
 
-import numpy as np
-import torch
-
 from transformers.utils import (
     ExplicitEnum,
 )
+
+from .utils import is_torch_available
+
+
+if is_torch_available():
+    import torch
 
 
 class BoundingBoxFormat(ExplicitEnum):
@@ -73,9 +76,9 @@ def _xyxy_to_xywh(xyxy: torch.Tensor, inplace: bool) -> torch.Tensor:
     Returns:
         torch.Tensor: The bounding box in XYWH format.
     """
-    xywh = xyxy if inplace else xyxy.clone()
-    xywh[..., 2:].sub_(xywh[..., :2])
-    return xywh
+    xyxy = xyxy if inplace else xyxy.clone()
+    xyxy[..., 2:].sub_(xyxy[..., :2])
+    return xyxy
 
 
 # adapted from https://github.com/pytorch/vision/blob/main/torchvision/transforms/v2/functional/_meta.py
@@ -137,43 +140,57 @@ def _relxywh_to_xyxy(relxywh: torch.Tensor, img_shape: Tuple[int, int], inplace:
         torch.Tensor: The bounding box in absolute XYXY format.
     """
     relxywh = relxywh if inplace else relxywh.clone()
+    # convert to relative_xyxy
+    relxywh[..., 2:].add_(relxywh[..., :2])
     # convert to xywh
     relxywh.multiply_(img_shape.repeat(2).flip(0))
-    # convert to xyxy
-    relxywh[..., 2:].add_(relxywh[..., :2])
     return relxywh
 
 
 def _relxcycwh_to_xyxy(relxcycwh: torch.Tensor, img_shape: Tuple[int, int], inplace: bool) -> torch.Tensor:
-    # TODO
-    pass
+    relxcycwh = relxcycwh if inplace else relxcycwh.clone()
+    # convert to relative_xyxy
+    relxyxy = _xcycwh_to_xyxy(relxcycwh, inplace)
+    # convert to xyxy
+    relxyxy.multiply_(img_shape.repeat(2).flip(0))
+    return relxyxy
 
 
 def _xyxy_to_relxywh(xyxy: torch.Tensor, img_shape: Tuple[int, int], inplace: bool) -> torch.Tensor:
-    # TODO
-    pass
+    xyxy = xyxy if inplace else xyxy.clone()
+    # to xywh
+    xyxy = _xyxy_to_xywh(xyxy, inplace)
+    # divide by (height, width) to make coordinates in relative format (rel_xywh)
+    xyxy.divide_(img_shape.repeat(2).flip(0))
+    return xyxy
 
 
 def _xyxy_to_relxcycwh(xyxy: torch.Tensor, img_shape: Tuple[int, int], inplace: bool) -> torch.Tensor:
-    # TODO
-    pass
+    xyxy = xyxy if inplace else xyxy.clone()
+    # to xcycwh
+    xyxy = _xyxy_to_xcycwh(xyxy, inplace)
+    # divide by (height, width) to make coordinates in relative format (rel_xcycwh)
+    xyxy.divide_(img_shape.repeat(2).flip(0))
+    return xyxy
 
 
 def transform_box_format(
-    bbox: Union[torch.Tensor, np.ndarray],
+    bbox: torch.Tensor,
     orig_format: BoundingBoxFormat,
     dest_format: BoundingBoxFormat,
-    img_shape: Optional[Tuple[int, int]] = None,
+    img_shape: Optional[Union[Tuple[int, int], torch.Tensor]] = None,
     inplace: bool = False,
+    do_round: bool = False,
 ):
     """
     Transform a bounding box from one format to another.
 
-    Args:s
-        bbox (Union[torch.Tensor, np.ndarray]): The bounding box to transform. orig_format (BoundingBoxFormat): The
+    Args:
+        bbox (torch.Tensor): The bounding box to transform. orig_format (BoundingBoxFormat): The
         original format of the bounding box. dest_format (BoundingBoxFormat): The desired destination format of the
         bounding box. img_shape (Optional[Tuple[int, int]]): The shape of the image (height, width), required for
         relative formats. inplace (bool): If True, perform operation in-place.
+        do_round (bool): If True, and the destination format is not a relative format, the coordinates of boxes are rounded.
 
     Returns:
         Union[torch.Tensor, np.ndarray]: The transformed bounding box.
@@ -185,11 +202,26 @@ def transform_box_format(
     if orig_format == dest_format:
         return bbox
 
-    if _is_relative_format(orig_format) and img_shape is None:
-        raise ValueError(f"Image shape (height, width) is required if the input format format is {dest_format}")
+    bbx_format_members = BoundingBoxFormat.__members__.values()
+    if orig_format not in bbx_format_members:
+        raise ValueError("orig_format is not a valid BoundingBoxFormat.")
+    if dest_format not in bbx_format_members:
+        raise ValueError("dest_format is not a valid BoundingBoxFormat.")
 
-    if _is_relative_format(dest_format) and img_shape is None:
-        raise ValueError(f"Image shape (height, width) is required if the desired destination format is {dest_format}")
+    if _is_relative_format(orig_format):
+        bbox = bbox.type(torch.float32)
+        if img_shape is None:
+            raise ValueError(f"Image shape (height, width) is required if the input format format is {dest_format}")
+        if not isinstance(img_shape, torch.Tensor):
+            img_shape = torch.Tensor(img_shape)
+
+    if _is_relative_format(dest_format):
+        if img_shape is None:
+            raise ValueError(
+                f"Image shape (height, width) is required if the desired destination format is {dest_format}"
+            )
+        if not isinstance(img_shape, torch.Tensor):
+            img_shape = torch.Tensor(img_shape)
 
     bbox = bbox.type(torch.float)
     # convert to xyxy
@@ -200,7 +232,7 @@ def transform_box_format(
     elif orig_format == BoundingBoxFormat.RELATIVE_XYWH:
         bbox = _relxywh_to_xyxy(bbox, img_shape, inplace)
     elif orig_format == BoundingBoxFormat.RELATIVE_XCYCWH:
-        bbox = _relxcycwh_to_xyxy(bbox, inplace)
+        bbox = _relxcycwh_to_xyxy(bbox, img_shape, inplace)
 
     # boxes are now in xyxy format
 
@@ -209,8 +241,12 @@ def transform_box_format(
     elif dest_format == BoundingBoxFormat.XCYCWH:
         bbox = _xyxy_to_xcycwh(bbox, inplace)
     elif dest_format == BoundingBoxFormat.RELATIVE_XYWH:
-        bbox = _xyxy_to_relxywh(bbox, inplace)
+        bbox = _xyxy_to_relxywh(bbox, img_shape, inplace)
     elif dest_format == BoundingBoxFormat.RELATIVE_XCYCWH:
-        bbox = _xyxy_to_relxcycwh(bbox, inplace)
+        bbox = _xyxy_to_relxcycwh(bbox, img_shape, inplace)
+
+    # If rounded is requested and destination format is not a relative format, round coordinates.
+    if do_round and not _is_relative_format(dest_format):
+        bbox = torch.round(bbox, decimals=1)
 
     return bbox
--- tests/test_bounding_box_utils.py
+++ tests/test_bounding_box_utils.py
@@ -15,168 +15,86 @@
 
 import unittest
 
-from transformers import is_torch_available, is_torchvision_available
-from transformers.bounding_box_utils import transform_box_format
+from transformers import is_torch_available
+from transformers.bounding_box_utils import BoundingBoxFormat, transform_box_format
 from transformers.testing_utils import require_torch
 
 
 if is_torch_available():
     import torch
 
-if is_torchvision_available():
-    # TODO: (Rafa) GOOD RESOURCE
-    # Using torchvision as reference
-    # >>> box_convert(boxes_xywh, in_fmt="xywh", out_fmt="xywh")
-    from torchvision.ops.boxes import box_convert
-
-_EPS = 1e-8
-
 
 @require_torch
 class UtilBoundingBoxConverters(unittest.TestCase):
-    def generate_rand_xy(self, image_height, image_width, total_samples, init_val=0):
-        # Random generated starting points (X, Y) for the bounding boxes
-        x = torch.randint(init_val, image_width, (total_samples, 1))
-        y = torch.randint(init_val, image_height, (total_samples, 1))
-        return x, y
+    def test_enumerators(self):
+        self.assertEqual(BoundingBoxFormat.XYXY, "xyxy")
+        self.assertEqual(BoundingBoxFormat.XYWH, "xywh")
+        self.assertEqual(BoundingBoxFormat.XCYCWH, "xcycwh")
+        self.assertEqual(BoundingBoxFormat.RELATIVE_XYWH, "relative_xywh")
+        self.assertEqual(BoundingBoxFormat.RELATIVE_XCYCWH, "relative_xcycwh")
 
-    def generate_rand_xywh(self, image_height, image_width, total_samples):
-        # Get x and y
-        x, y = self.generate_rand_xy(image_height, image_width, total_samples)
-        # Random generated widths and heights for the bounding boxes
-        w = torch.zeros(total_samples, 1, dtype=torch.int)
-        h = torch.zeros(total_samples, 1, dtype=torch.int)
-        for idx, (x_val, y_val) in enumerate(zip(x, y)):
-            w[idx] = torch.randint(image_width - x_val.item(), (1,))
-            h[idx] = torch.randint(image_height - y_val.item(), (1,))
-        # Concatenate them to get the bounding boxes
-        bboxes = torch.cat((x, y, w, h), dim=1)
-        # guarantees that boxes are within image boundaries
-        self.assertTrue(torch.all(bboxes[..., 0] + bboxes[..., 2] < image_width))
-        self.assertTrue(torch.all(bboxes[..., 1] + bboxes[..., 3] < image_width))
-        return bboxes
+    def test_conversion_cases(self):
+        # Identical bounding boxes differing in format only
+        testing_cases = {
+            "xywh": [
+                [387, 441, 44, 9],
+                [134, 57, 434, 383],
+                [306, 274, 36, 154],
+                [22, 252, 479, 56],
+                [498, 349, 120, 79],
+            ],
+            "xyxy": [
+                [387, 441, 431, 450.0],
+                [134, 57, 568, 440.0],
+                [306, 274, 342, 428.0],
+                [22, 252, 501, 308.0],
+                [498, 349, 618, 428],
+            ],
+            "xcycwh": [
+                [409.0, 445.5, 44.0, 9.0],
+                [351.0, 248.5, 434.0, 383.0],
+                [324.0, 351.0, 36.0, 154.0],
+                [261.5, 280.0, 479.0, 56.0],
+                [558.0, 388.5, 120.0, 79.0],
+            ],
+            "relative_xywh": [
+                [0.6047, 0.9587, 0.0688, 0.0196],
+                [0.2094, 0.1239, 0.6781, 0.8326],
+                [0.4781, 0.5957, 0.05625, 0.3348],
+                [0.0344, 0.5478, 0.7484, 0.1217],
+                [0.7781, 0.7587, 0.1875, 0.1717],
+            ],
+            "relative_xcycwh": [
+                [0.6391, 0.9685, 0.0688, 0.0196],
+                [0.5484, 0.5402, 0.6781, 0.8326],
+                [0.5063, 0.7630, 0.0563, 0.3348],
+                [0.4086, 0.6087, 0.7484, 0.1217],
+                [0.8719, 0.8446, 0.1875, 0.1717],
+            ],
+        }
+        # Resolution of the image where the boxes belong to
+        original_img_shape = [460, 640]
 
-    def generate_rand_xyxy(self, image_height, image_width, total_samples):
-        # Get x and y
-        x, y = self.generate_rand_xy(image_height, image_width, total_samples)
-        # Random generated x2 and y2 for the bounding boxes
-        x2 = torch.zeros(total_samples, 1, dtype=torch.int)
-        y2 = torch.zeros(total_samples, 1, dtype=torch.int)
-        for idx, (x_val, y_val) in enumerate(zip(x, y)):
-            x2[idx] = torch.randint(x_val.item(), image_width, (1,))
-            y2[idx] = torch.randint(y_val.item(), image_height, (1,))
-        # Concatenate them to get the bounding boxes
-        bboxes = torch.cat((x, y, x2, y2), dim=1)
-        # guarantees that boxes are within image boundaries
-        self.assertTrue(torch.all(bboxes[..., 2] < image_width))
-        self.assertTrue(torch.all(bboxes[..., 3] < image_height))
-        return bboxes
+        # Define which formats are relative and absolute
+        rel_formats = ("relative_xywh", "relative_xcycwh")
 
-    def generate_rand_xcycwh(self, image_height, image_width, total_samples):
-        # Random generate center points (Xc, Yc) for the bounding boxes
-        # Making init_val=1 ensures that the boxes will have a width and height
-        xc, yc = self.generate_rand_xy(image_height, image_width, total_samples, init_val=1)
-        # Randomly generated widths and heights ensuring they don't exceed twice the distance from center to image borders
-        w = torch.zeros(total_samples, 1, dtype=torch.int)
-        h = torch.zeros(total_samples, 1, dtype=torch.int)
-        for idx, (x_val, y_val) in enumerate(zip(xc, yc)):
-            x_val, y_val = x_val.item(), y_val.item()
-            w[idx] = 1 + torch.randint(1, 1 + min(x_val, image_width - x_val), (1,))
-            h[idx] = 1 + torch.randint(1, 1 + min(y_val, image_height - y_val), (1,))  # add 1 to be inclusive
-        bboxes = torch.cat((xc, yc, w, h), dim=1)
-        # guarantees that boxes are within image boundaries
-        self.assertTrue(torch.all(bboxes[..., 0] < image_width))  # xc lays within image width
-        self.assertTrue(torch.all(bboxes[..., 1] < image_height))  # yc lays within image height
-        self.assertTrue(torch.all(bboxes[..., 2] <= image_width))  # box width is le than image width
-        self.assertTrue(torch.all(bboxes[..., 3] <= image_height))  # box height is le than image height
-        return bboxes
+        # Loop through each format in the testing cases
+        for origin_format, bboxes in testing_cases.items():
+            origin_format = BoundingBoxFormat(origin_format)
+            bboxes = torch.Tensor(bboxes)
 
-    def generate_rand_relxywh(self, image_height, image_width, total_samples):
-        # create xywh
-        bboxes = self.generate_rand_xywh(image_height, image_width, total_samples)
-        # make it relative
-        img_shape = torch.tensor((image_width, image_height, image_width, image_height))
-        bboxes = bboxes.add_(_EPS).divide(img_shape)
-        return bboxes
+            # Loop through all formats to be used as destination formats
+            for dest_format, expected_bboxes in testing_cases.items():
+                dest_format = BoundingBoxFormat(dest_format)
+                expected_bboxes = torch.Tensor(expected_bboxes)
+                img_shape = None
+                # If original or destination formats are relative, the image shape is required
+                if dest_format in rel_formats or origin_format in rel_formats:
+                    img_shape = original_img_shape
 
-    def test_converters_xywh(self):
-        # Assuming 10 bounding boxes are within an image of size 640x480
-        (image_width, image_height) = 640, 480
-        total_samples = 10
-        # Create bounding boxes in the format XYWH
-        bboxes_xywh = self.generate_rand_xywh(image_height, image_width, total_samples)
-        # Apply transformation
-        transf_boxes_xywh = transform_box_format(bboxes_xywh, orig_format="xywh", dest_format="xywh")
-        transf_boxes_xyxy = transform_box_format(
-            bboxes_xywh,
-            orig_format="xywh",
-            dest_format="xyxy",
-        )
-        transf_boxes_xcycwh = transform_box_format(
-            bboxes_xywh,
-            orig_format="xywh",
-            dest_format="xcycwh",
-        )
-        # Test transformations
-        self.assertTrue(torch.equal(bboxes_xywh, transf_boxes_xywh))
-        self.assertFalse(torch.equal(bboxes_xywh, transf_boxes_xyxy))
-        self.assertFalse(torch.equal(bboxes_xywh, transf_boxes_xcycwh))
-        # Test with torch transformations
-        torch_transf_boxes_xywh = box_convert(bboxes_xywh, out_fmt="xywh", in_fmt="xywh")
-        torch_transf_boxes_xyxy = box_convert(bboxes_xywh, out_fmt="xyxy", in_fmt="xywh")
-        torch_transf_boxes_xcycwh = box_convert(bboxes_xywh, out_fmt="cxcywh", in_fmt="xywh")
-        assert torch.equal(transf_boxes_xywh, torch_transf_boxes_xywh)
-        assert torch.equal(transf_boxes_xyxy, torch_transf_boxes_xyxy)
-        assert torch.equal(transf_boxes_xcycwh, torch_transf_boxes_xcycwh)
-
-    def test_converters_xyxy(self):
-        # Assuming 10 bounding boxes are within an image of size 640x480
-        (image_width, image_height) = 640, 480
-        total_samples = 10
-        # Create bounding boxes in the format XYXY
-        bboxes_xyxy = self.generate_rand_xyxy(image_height, image_width, total_samples)
-        # Apply transformation
-        transf_boxes_xyxy = transform_box_format(bboxes_xyxy, orig_format="xyxy", dest_format="xyxy")
-        transf_boxes_xywh = transform_box_format(
-            bboxes_xyxy,
-            orig_format="xyxy",
-            dest_format="xywh",
-        )
-        transf_boxes_xcycwh = transform_box_format(
-            bboxes_xyxy,
-            orig_format="xyxy",
-            dest_format="xcycwh",
-        )
-        # Test transformations
-        self.assertTrue(torch.equal(bboxes_xyxy, transf_boxes_xyxy))
-        self.assertFalse(torch.equal(bboxes_xyxy, transf_boxes_xywh))
-        self.assertFalse(torch.equal(bboxes_xyxy, transf_boxes_xcycwh))
-        # Test with torch transformations
-        torch_transf_boxes_xyxy = box_convert(bboxes_xyxy, out_fmt="xyxy", in_fmt="xyxy")
-        torch_transf_boxes_xywh = box_convert(bboxes_xyxy, out_fmt="xywh", in_fmt="xyxy")
-        torch_transf_boxes_xcycwh = box_convert(bboxes_xyxy, out_fmt="cxcywh", in_fmt="xyxy")
-        assert torch.equal(transf_boxes_xywh, torch_transf_boxes_xywh)
-        assert torch.equal(transf_boxes_xyxy, torch_transf_boxes_xyxy)
-        assert torch.equal(transf_boxes_xcycwh, torch_transf_boxes_xcycwh)
-
-    def test_converters_xcycwh(self):
-        # Assuming 10 bounding boxes are within an image of size 640x480
-        (image_width, image_height) = 640, 480
-        total_samples = 10
-        # Create bounding boxes in the format XCYCWH
-        bboxes_xcycwh = self.generate_rand_xcycwh(image_height, image_width, total_samples)
-        # Apply transformation
-        transf_boxes_xcycwh = transform_box_format(bboxes_xcycwh, orig_format="xcycwh", dest_format="xcycwh")
-        transf_boxes_xyxy = transform_box_format(bboxes_xcycwh, orig_format="xcycwh", dest_format="xyxy")
-        transf_boxes_xywh = transform_box_format(bboxes_xcycwh, orig_format="xcycwh", dest_format="xywh")
-        # Test transformations
-        self.assertTrue(torch.equal(bboxes_xcycwh, transf_boxes_xcycwh))
-        self.assertFalse(torch.equal(bboxes_xcycwh, transf_boxes_xyxy))
-        self.assertFalse(torch.equal(bboxes_xcycwh, transf_boxes_xywh))
-        # Test with torch transformations
-        torch_transf_boxes_xcycwh = box_convert(bboxes_xcycwh, out_fmt="cxcywh", in_fmt="cxcywh")
-        torch_transf_boxes_xyxy = box_convert(bboxes_xcycwh, out_fmt="xyxy", in_fmt="cxcywh")
-        torch_transf_boxes_xywh = box_convert(bboxes_xcycwh, out_fmt="xywh", in_fmt="cxcywh")
-        assert torch.equal(transf_boxes_xcycwh, torch_transf_boxes_xcycwh)
-        assert torch.equal(transf_boxes_xyxy, torch_transf_boxes_xyxy)
-        assert torch.equal(transf_boxes_xywh, torch_transf_boxes_xywh)
+                # Apply transformation
+                result = transform_box_format(
+                    bboxes, orig_format=origin_format, dest_format=dest_format, img_shape=img_shape, do_round=True
+                )
+                # Compare results
+                self.assertTrue(torch.allclose(result, expected_bboxes, atol=1e-4))
