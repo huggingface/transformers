4989e73e2f86f991a7c2aec4852d873903c71c14
Matt <Rocketknight1@users.noreply.github.com>
2023-11-16 13:47:43 +0000

    Update the TF pin for 2.15 (#27375)
    * Move the TF pin for 2.15
    
    * make fixup

--- setup.py
+++ setup.py
@@ -127,7 +127,7 @@ _deps = [
     "jieba",
     "kenlm",
     # Keras pin - this is to make sure Keras 3 doesn't destroy us. Remove or change when we have proper support.
-    "keras<2.15",
+    "keras<2.16",
     "keras-nlp>=0.3.1",
     "librosa",
     "nltk",
@@ -169,9 +169,9 @@ _deps = [
     "sudachidict_core>=20220729",
     "tensorboard",
     # TensorFlow pin. When changing this value, update examples/tensorflow/_tests_requirements.txt accordingly
-    "tensorflow-cpu>=2.6,<2.15",
-    "tensorflow>=2.6,<2.15",
-    "tensorflow-text<2.15",
+    "tensorflow-cpu>=2.6,<2.16",
+    "tensorflow>=2.6,<2.16",
+    "tensorflow-text<2.16",
     "tf2onnx",
     "timeout-decorator",
     "timm",
