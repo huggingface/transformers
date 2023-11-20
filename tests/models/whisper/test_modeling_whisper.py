0593495bdff7d443c89a94e736d9bf1f2776a3e7
Patrick von Platen <patrick.v.platen@gmail.com>
2023-11-20 13:56:03 +0000

    add comments to explain the code better

--- tests/models/whisper/test_modeling_whisper.py
+++ tests/models/whisper/test_modeling_whisper.py
@@ -65,6 +65,78 @@ if is_torch_available():
     )
     from transformers.models.whisper.modeling_whisper import WhisperDecoder, WhisperEncoder, sinusoids
 
+    class DummyTimestampLogitProcessor(LogitsProcessor):
+        """This processor fakes the correct timestamps tokens pattern [TOK_1] [TOK_2] ... [TOK_N] [TIME_STAMP_TOK_1] [TIME_STAMP_TOK_2] [TOK_N+1] ..."""
+
+        def __init__(
+            self, timestamp_begin, vocab_size, batch_size, max_length, min_space=3, seed=0, is_length_ascending=True
+        ):
+            self.timestamp_begin = timestamp_begin
+            self.vocab_size = vocab_size
+
+            self.min_space_between_timestamps = min_space
+            self.timestamp_tokens = torch.arange(self.timestamp_begin, self.vocab_size)
+            self.timestamp_tokens.to(torch_device)
+            self.is_length_ascending = is_length_ascending
+
+            self.no_time_stamp_counter = batch_size * [0]
+            self.prev_highest_timestamp = batch_size * [0]
+            self.batch_size = batch_size
+            self.max_length = max_length
+            self.count = 0
+
+            self.let_pass = [[] for _ in range(batch_size)]
+            for k in range(batch_size):
+                random.seed(seed + k)
+                for _ in range(10000):
+                    self.let_pass[k].append(random.randint(1, 10) <= 3)
+
+        def __call__(self, input_ids: torch.LongTensor, scores: torch.FloatTensor) -> torch.FloatTensor:
+            # we don't want to randomely sample timestamp tokens
+            if input_ids.shape[-1] > 1:
+                scores[:, self.timestamp_begin :] = -float("inf")
+
+            self.no_time_stamp_counter = [x + 1 for x in self.no_time_stamp_counter]
+            for k in range(input_ids.shape[0]):
+                # make sure to use correct index if a batch was removed
+                if self.is_length_ascending and input_ids.shape[0] < self.batch_size:
+                    prev_k = k + self.batch_size - input_ids.shape[0]
+                else:
+                    prev_k = k
+
+                if input_ids[k, -1] == self.timestamp_begin:
+                    self.no_time_stamp_counter[prev_k] = 0
+
+                can_produce = self.no_time_stamp_counter[prev_k] > self.min_space_between_timestamps
+                must_produce = (
+                    input_ids[k][2:].le(self.timestamp_begin).all() and input_ids.shape[-1] == self.max_length - 1
+                )
+                # produce timestamp with 30%
+                if (can_produce and self.let_pass[prev_k][self.count]) or must_produce:
+                    self.no_time_stamp_counter[prev_k] = 0
+                    self.prev_highest_timestamp[prev_k] = max(input_ids[k].max() + 1, self.timestamp_tokens[0].item())
+
+                    # force a timestamp
+                    scores[k, :] = -float("inf")
+                    scores[k, self.prev_highest_timestamp[prev_k]] = 10.0
+
+                if (
+                    input_ids.shape[-1] > 3
+                    and input_ids[k, -1].item() in self.timestamp_tokens
+                    and input_ids[k, -2].item() not in self.timestamp_tokens
+                ):
+                    # force the same as before
+                    scores[k, :] = -float("inf")
+                    scores[k, input_ids[k, -1].item()] = 10.0
+
+            self.count += 1
+
+            if torch.isinf(scores).all():
+                raise ValueError("Dummy logit processor is incorrectly set up. Scores should not be all inf.")
+
+            return scores
+
+
 if is_flax_available():
     import jax.numpy as jnp
 
@@ -101,78 +173,6 @@ def prepare_whisper_inputs_dict(
         "decoder_head_mask": decoder_head_mask,
         "cross_attn_head_mask": cross_attn_head_mask,
     }
-
-
-class DummyTimestampLogitProcessor(LogitsProcessor):
-    """This processor fakes the correct timestamps tokens pattern [TOK_1] [TOK_2] ... [TOK_N] [TIME_STAMP_TOK_1] [TIME_STAMP_TOK_2] [TOK_N+1] ..."""
-
-    def __init__(
-        self, timestamp_begin, vocab_size, batch_size, max_length, min_space=3, seed=0, is_length_ascending=True
-    ):
-        self.timestamp_begin = timestamp_begin
-        self.vocab_size = vocab_size
-
-        self.min_space_between_timestamps = min_space
-        self.timestamp_tokens = torch.arange(self.timestamp_begin, self.vocab_size)
-        self.timestamp_tokens.to(torch_device)
-        self.is_length_ascending = is_length_ascending
-
-        self.no_time_stamp_counter = batch_size * [0]
-        self.prev_highest_timestamp = batch_size * [0]
-        self.batch_size = batch_size
-        self.max_length = max_length
-        self.count = 0
-
-        self.let_pass = [[] for _ in range(batch_size)]
-        for k in range(batch_size):
-            random.seed(seed + k)
-            for _ in range(10000):
-                self.let_pass[k].append(random.randint(1, 10) <= 3)
-
-    def __call__(self, input_ids: torch.LongTensor, scores: torch.FloatTensor) -> torch.FloatTensor:
-        # we don't want to randomely sample timestamp tokens
-        if input_ids.shape[-1] > 1:
-            scores[:, self.timestamp_begin :] = -float("inf")
-
-        self.no_time_stamp_counter = [x + 1 for x in self.no_time_stamp_counter]
-        for k in range(input_ids.shape[0]):
-            # make sure to use correct index if a batch was removed
-            if self.is_length_ascending and input_ids.shape[0] < self.batch_size:
-                prev_k = k + self.batch_size - input_ids.shape[0]
-            else:
-                prev_k = k
-
-            if input_ids[k, -1] == self.timestamp_begin:
-                self.no_time_stamp_counter[prev_k] = 0
-
-            can_produce = self.no_time_stamp_counter[prev_k] > self.min_space_between_timestamps
-            must_produce = (
-                input_ids[k][2:].le(self.timestamp_begin).all() and input_ids.shape[-1] == self.max_length - 1
-            )
-            # produce timestamp with 30%
-            if (can_produce and self.let_pass[prev_k][self.count]) or must_produce:
-                self.no_time_stamp_counter[prev_k] = 0
-                self.prev_highest_timestamp[prev_k] = max(input_ids[k].max() + 1, self.timestamp_tokens[0].item())
-
-                # force a timestamp
-                scores[k, :] = -float("inf")
-                scores[k, self.prev_highest_timestamp[prev_k]] = 10.0
-
-            if (
-                input_ids.shape[-1] > 3
-                and input_ids[k, -1].item() in self.timestamp_tokens
-                and input_ids[k, -2].item() not in self.timestamp_tokens
-            ):
-                # force the same as before
-                scores[k, :] = -float("inf")
-                scores[k, input_ids[k, -1].item()] = 10.0
-
-        self.count += 1
-
-        if torch.isinf(scores).all():
-            raise ValueError("Dummy logit processor is incorrectly set up. Scores should not be all inf.")
-
-        return scores
 
 
 @require_torch
