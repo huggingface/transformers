da43b79faa70b4c34a2f6a9c5be4ef930badf8f4
Niels <niels.rogge1@gmail.com>
2023-11-20 12:05:08 +0100

    Improve conversion script

--- src/transformers/models/siglip/convert_siglip_to_hf.py
+++ src/transformers/models/siglip/convert_siglip_to_hf.py
@@ -29,7 +29,7 @@ from huggingface_hub import hf_hub_download
 from numpy import load
 from PIL import Image
 
-from transformers import SiglipConfig, SiglipModel
+from transformers import SiglipConfig, SiglipModel, SiglipTokenizer
 from transformers.utils import logging
 
 
@@ -211,7 +211,7 @@ def flatten_nested_dict(params, parent_key="", sep="/"):
 
 
 @torch.no_grad()
-def convert_siglip_checkpoint(model_name, pytorch_dump_folder_path, push_to_hub=False):
+def convert_siglip_checkpoint(model_name, vocab_file, pytorch_dump_folder_path, push_to_hub=False):
     """
     Copy/paste/tweak model's weights to our SigLIP structure.
     """
@@ -247,12 +247,58 @@ def convert_siglip_checkpoint(model_name, pytorch_dump_folder_path, push_to_hub=
     filepath = hf_hub_download(repo_id="nielsr/test-image", filename="pixel_values_siglip.npy", repo_type="dataset")
     pixel_values = np.load(filepath)
     pixel_values = torch.from_numpy(pixel_values).permute(0, 3, 1, 2)
+
+    # create tokenizer
+    tokenizer = SiglipTokenizer(vocab_file=vocab_file)
+    texts = [
+        "an apple",
+        "a picture of an apple",
+        "an ipod",
+        "granny smith",
+        'an apple with a note saying "ipod"',
+        "a cold drink on a hot day",
+        "a hot drink on a cold day",
+        "a photo of a cold drink on a hot day",
+        "a photo of a hot drink on a cold day",
+        #
+        "a photo of two guys in need of caffeine",
+        "a photo of two guys in need of water",
+        "a photo of the SigLIP authors",
+        "a photo of a rock band",
+        "a photo of researchers at Google Brain",
+        "a photo of researchers at OpenAI",
+        #
+        "a robot on a sign",
+        "a photo of a robot on a sign",
+        "an empty street",
+        "autumn in Toronto",
+        "a photo of autumn in Toronto",
+        "a photo of Toronto in autumn",
+        "a photo of Toronto in summer",
+        "autumn in Singapore",
+        #
+        "cow",
+        "a cow in a tuxedo",
+        "a cow on the beach",
+        "a cow in the prairie",
+        #
+        "the real mountain view",
+        "Zürich",
+        "San Francisco",
+        "a picture of a laptop with the lockscreen on, a cup of cappucino, salt and pepper grinders. The view through the window reveals lake Zürich and the Alps in the background of the city.",
+    ]
+    input_ids = tokenizer(texts, return_tensors="pt", padding="max_length").input_ids
+
+    # verify input_ids against original ones
     filepath = hf_hub_download(repo_id="nielsr/test-image", filename="input_ids_siglip.npy", repo_type="dataset")
-    input_ids = np.load(filepath)
-    input_ids = torch.from_numpy(input_ids)
+    original_input_ids = np.load(filepath)
+    original_input_ids = torch.from_numpy(original_input_ids)
+    assert input_ids.tolist() == original_input_ids.tolist()
 
     with torch.no_grad():
         outputs = model(input_ids=input_ids, pixel_values=pixel_values)
+
+    print("Logits per image:", outputs.logits_per_image[:3, :3])
 
     # assert values
     expected_slice = torch.tensor(
@@ -284,6 +330,13 @@ if __name__ == "__main__":
         help="Name of the model you'd like to convert.",
     )
     parser.add_argument(
+        "--vocab_file",
+        default="/Users/nielsrogge/Documents/SigLIP/sentencepiece.model",
+        type=str,
+        required=False,
+        help="Path to the vocabulary file.",
+    )
+    parser.add_argument(
         "--pytorch_dump_folder_path", default=None, type=str, help="Path to the output PyTorch model directory."
     )
     parser.add_argument(
@@ -291,4 +344,4 @@ if __name__ == "__main__":
     )
 
     args = parser.parse_args()
-    convert_siglip_checkpoint(args.model_name, args.pytorch_dump_folder_path, args.push_to_hub)
+    convert_siglip_checkpoint(args.model_name, args.vocab_file, args.pytorch_dump_folder_path, args.push_to_hub)
