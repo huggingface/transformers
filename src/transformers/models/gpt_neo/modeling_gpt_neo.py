087e1ee1d1bbcfde2ce4892413cdb7c0d8207530
Amy Roberts <22614925+amyeroberts@users.noreply.github.com>
2023-11-20 21:53:14 +0000

    Fix

--- src/transformers/models/gpt_neo/modeling_gpt_neo.py
+++ src/transformers/models/gpt_neo/modeling_gpt_neo.py
@@ -56,7 +56,7 @@ if is_flash_attn_2_available():
 # This makes `_prepare_4d_causal_attention_mask` a leaf function in the FX graph.
 # It means that the function will not be traced through and simply appear as a node in the graph.
 if is_torch_fx_available():
-    if not is_torch_greater_or_equal_than_1_13():
+    if not is_torch_greater_or_equal_than_1_13:
         import torch.fx
 
     _prepare_4d_causal_attention_mask = torch.fx.wrap(_prepare_4d_causal_attention_mask)
