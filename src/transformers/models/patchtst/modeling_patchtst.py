027d092b852a1e720d68ddd3ca31e62a12a55842
Kashif Rasul <kashif.rasul@gmail.com>
2023-11-21 13:20:52 +0100

    move examples to the forward's docstrings

--- src/transformers/models/patchtst/modeling_patchtst.py
+++ src/transformers/models/patchtst/modeling_patchtst.py
@@ -1191,31 +1191,6 @@ class PatchTSTScaler(nn.Module):
     PATCHTST_START_DOCSTRING,
 )
 class PatchTSTModel(PatchTSTPreTrainedModel):
-    """
-    Examples:
-
-    ```python
-    >>> from huggingface_hub import hf_hub_download
-    >>> import torch
-    >>> from transformers import PatchTSTModel
-
-    >>> file = hf_hub_download(
-    ...     repo_id="hf-internal-testing/etth1-hourly-batch", filename="train-batch.pt", repo_type="dataset"
-    ... )
-    >>> batch = torch.load(file)
-
-    >>> model = PatchTSTModel.from_pretrained("huggingface/patchtst-etth1-pretrain")
-
-    >>> # during training, one provides both past and future values
-    >>> outputs = model(
-    ...     past_values=batch["past_values"],
-    ...     future_values=batch["future_values"],
-    ... )
-
-    >>> last_hidden_state = outputs.last_hidden_state
-    ```
-    """
-
     def __init__(self, config: PatchTSTConfig):
         super().__init__(config)
 
@@ -1243,7 +1218,7 @@ class PatchTSTModel(PatchTSTPreTrainedModel):
         output_attentions: Optional[bool] = None,
         return_dict: Optional[bool] = None,
     ) -> Union[Tuple, PatchTSTModelOutput]:
-        """
+        r"""
         Parameters:
             past_values (`torch.Tensor` of shape `(bs, sequence_length, num_input_channels)`, *required*):
                 Input sequence to the model
@@ -1265,7 +1240,29 @@ class PatchTSTModel(PatchTSTPreTrainedModel):
         Returns:
             `PatchTSTModelOutput` or tuple of `torch.Tensor` (if `return_dict`=False or `config.return_dict`=False)
 
-        """
+        Examples:
+
+        ```python
+        >>> from huggingface_hub import hf_hub_download
+        >>> import torch
+        >>> from transformers import PatchTSTModel
+
+        >>> file = hf_hub_download(
+        ...     repo_id="hf-internal-testing/etth1-hourly-batch", filename="train-batch.pt", repo_type="dataset"
+        ... )
+        >>> batch = torch.load(file)
+
+        >>> model = PatchTSTModel.from_pretrained("huggingface/patchtst-etth1-pretrain")
+
+        >>> # during training, one provides both past and future values
+        >>> outputs = model(
+        ...     past_values=batch["past_values"],
+        ...     future_values=batch["future_values"],
+        ... )
+
+        >>> last_hidden_state = outputs.last_hidden_state
+        ```"""
+
         return_dict = return_dict if return_dict is not None else self.config.use_return_dict
         output_attentions = output_attentions if output_attentions is not None else self.config.output_attentions
 
@@ -1335,54 +1332,6 @@ class PatchTSTMaskPretrainHead(nn.Module):
     PATCHTST_START_DOCSTRING,
 )
 class PatchTSTForPretraining(PatchTSTPreTrainedModel):
-    """
-    Mask pretrain model: PatchTST model + pretrain head
-
-    Examples:
-
-    ```python
-    >>> from huggingface_hub import hf_hub_download
-    >>> import torch
-    >>> from transformers import PatchTSTConfig, PatchTSTForPretraining
-
-    >>> file = hf_hub_download(
-    ...     repo_id="hf-internal-testing/etth1-hourly-batch", filename="train-batch.pt", repo_type="dataset"
-    ... )
-    >>> batch = torch.load(file)
-
-    >>> # Config for random mask pretraining
-    >>> config = PatchTSTConfig(
-    ...     num_input_channels=7,
-    ...     context_length=512,
-    ...     patch_length = 12,
-    ...     stride = 12,
-    ...     mask_type = 'random',
-    ...     random_mask_ratio = 0.4,
-    ...     use_cls_token = True,
-    ... )
-    >>> # Config for forecast mask pretraining
-    >>> config = PatchTSTConfig(
-    ...     num_input_channels=7,
-    ...     context_length=512,
-    ...     patch_length = 12,
-    ...     stride = 12,
-    ...     mask_type = 'forecast',
-    ...     num_forecast_mask_patches = 5,
-    ...     use_cls_token = True,
-    ... )
-    >>> model = PatchTSTForPretraining(config)
-
-    >>> # during training, one provides both past and future values
-    >>> outputs = model(
-    ...     past_values=batch["past_values"],
-    ...     future_values=batch["future_values"],
-    ... )
-
-    >>> loss = outputs.loss
-    >>> loss.backward()
-    ```
-    """
-
     def __init__(self, config: PatchTSTConfig):
         super().__init__(config)
 
@@ -1401,7 +1350,7 @@ class PatchTSTForPretraining(PatchTSTPreTrainedModel):
         output_attentions: Optional[bool] = None,
         return_dict: Optional[bool] = None,
     ) -> Union[Tuple, PatchTSTForPretrainingOutput]:
-        """
+        r"""
         Parameters:
             past_values (`torch.Tensor` of shape `(bs, sequence_length, num_input_channels)`, *required*):
                 Input sequence to the model
@@ -1421,7 +1370,49 @@ class PatchTSTForPretraining(PatchTSTPreTrainedModel):
             `PatchTSTForPretrainingOutput` or tuple of `torch.Tensor` (if `return_dict`=False or
             `config.return_dict`=False)
 
-        """
+        Examples:
+
+        ```python
+        >>> from huggingface_hub import hf_hub_download
+        >>> import torch
+        >>> from transformers import PatchTSTConfig, PatchTSTForPretraining
+
+        >>> file = hf_hub_download(
+        ...     repo_id="hf-internal-testing/etth1-hourly-batch", filename="train-batch.pt", repo_type="dataset"
+        ... )
+        >>> batch = torch.load(file)
+
+        >>> # Config for random mask pretraining
+        >>> config = PatchTSTConfig(
+        ...     num_input_channels=7,
+        ...     context_length=512,
+        ...     patch_length = 12,
+        ...     stride = 12,
+        ...     mask_type = 'random',
+        ...     random_mask_ratio = 0.4,
+        ...     use_cls_token = True,
+        ... )
+        >>> # Config for forecast mask pretraining
+        >>> config = PatchTSTConfig(
+        ...     num_input_channels=7,
+        ...     context_length=512,
+        ...     patch_length = 12,
+        ...     stride = 12,
+        ...     mask_type = 'forecast',
+        ...     num_forecast_mask_patches = 5,
+        ...     use_cls_token = True,
+        ... )
+        >>> model = PatchTSTForPretraining(config)
+
+        >>> # during training, one provides both past and future values
+        >>> outputs = model(
+        ...     past_values=batch["past_values"],
+        ...     future_values=batch["future_values"],
+        ... )
+
+        >>> loss = outputs.loss
+        >>> loss.backward()
+        ```"""
 
         return_dict = return_dict if return_dict is not None else self.config.use_return_dict
 
@@ -1495,32 +1486,6 @@ class PatchTSTClassificationHead(nn.Module):
     PATCHTST_START_DOCSTRING,
 )
 class PatchTSTForClassification(PatchTSTPreTrainedModel):
-    """
-    PatchTST model for classification. The model contains PatchTST model + classification head
-
-    Examples:
-
-    ```python
-    >>> from transformers import PatchTSTConfig, PatchTSTForClassification
-
-    >>> # classification task with two input channel2 and 3 classes
-    >>> config = PatchTSTConfig(
-    ...     num_input_channels=2,
-    ...     num_targets=3,
-    ...     context_length=512,
-    ...     patch_length = 12,
-    ...     stride = 12,
-    ...     use_cls_token = True,
-    ... )
-    >>> model = PatchTSTForClassification.from_pretrained("huggingface/patchtst-etth1-pretrain", config=config)
-
-    >>> # during inference, one only provides past values
-    >>> past_values = torch.randn(20, 512, 2)
-    >>> outputs = model(past_values=past_values)
-    >>> labels = outputs.prediction_logits
-    ```
-    """
-
     def __init__(self, config: PatchTSTConfig):
         super().__init__(config)
 
@@ -1544,7 +1509,7 @@ class PatchTSTForClassification(PatchTSTPreTrainedModel):
         output_attentions: Optional[bool] = None,
         return_dict: Optional[bool] = None,
     ) -> Union[tuple, PatchTSTForClassificationOutput]:
-        """
+        r"""
         Parameters:
             past_values (`torch.Tensor` of shape `(bs, sequence_length, num_input_channels)`, *required*):
                 Input sequence to the model
@@ -1567,7 +1532,28 @@ class PatchTSTForClassification(PatchTSTPreTrainedModel):
             `PatchTSTForClassificationOutput` or tuple of `torch.Tensor` (if `return_dict`=False or
             `config.return_dict`=False)
 
-        """
+        Examples:
+
+        ```python
+        >>> from transformers import PatchTSTConfig, PatchTSTForClassification
+
+        >>> # classification task with two input channel2 and 3 classes
+        >>> config = PatchTSTConfig(
+        ...     num_input_channels=2,
+        ...     num_targets=3,
+        ...     context_length=512,
+        ...     patch_length = 12,
+        ...     stride = 12,
+        ...     use_cls_token = True,
+        ... )
+        >>> model = PatchTSTForClassification.from_pretrained("huggingface/patchtst-etth1-pretrain", config=config)
+
+        >>> # during inference, one only provides past values
+        >>> past_values = torch.randn(20, 512, 2)
+        >>> outputs = model(past_values=past_values)
+        >>> labels = outputs.prediction_logits
+        ```"""
+
         return_dict = return_dict if return_dict is not None else self.config.use_return_dict
 
         model_output = self.model(
@@ -1694,50 +1680,6 @@ class PatchTSTPredictionHead(nn.Module):
     PATCHTST_START_DOCSTRING,
 )
 class PatchTSTForPrediction(PatchTSTPreTrainedModel):
-    """
-    PatchTST for forecasting. The model contains PatchTST model + Forecasting head
-
-    Examples:
-
-    ```python
-    >>> from huggingface_hub import hf_hub_download
-    >>> import torch
-    >>> from transformers import PatchTSTConfig, PatchTSTForPrediction
-
-    >>> file = hf_hub_download(
-    ...     repo_id="hf-internal-testing/etth1-hourly-batch", filename="train-batch.pt", repo_type="dataset"
-    ... )
-    >>> batch = torch.load(file)
-
-    >>> # Prediction task with 7 input channels and prediction length is 96
-    >>> config = PatchTSTConfig(
-    ...     num_input_channels=7,
-    ...     context_length=512,
-    ...     patch_length = 12,
-    ...     stride = 12,
-    ...     use_cls_token = True,
-    ...     prediction_length = 96,
-    ... )
-    >>> model = PatchTSTForPrediction.from_pretrained("huggingface/patchtst-etth1-pretrain", config=config)
-
-    >>> # during training, one provides both past and future values
-    >>> outputs = model(
-    ...     past_values=batch["past_values"],
-    ...     future_values=batch["future_values"],
-    ... )
-
-    >>> loss = outputs.loss
-    >>> loss.backward()
-
-    >>> # during inference, one only provides past values, the model generates future values
-    >>> outputs = model.generate(
-    ...     past_values=batch["past_values"],
-    ... )
-
-    >>> mean_prediction = outputs.sequences.mean(dim=1)
-    ```
-    """
-
     def __init__(self, config: PatchTSTConfig):
         super().__init__(config)
 
@@ -1774,7 +1716,7 @@ class PatchTSTForPrediction(PatchTSTPreTrainedModel):
         output_attentions: Optional[bool] = None,
         return_dict: Optional[bool] = None,
     ) -> Union[Tuple, PatchTSTForPredictionOutput]:
-        """
+        r"""
         Parameters:
             past_values (`torch.Tensor` of shape `(bs, sequence_length, num_input_channels)`, *required*):
                 Input sequence to the model
@@ -1797,7 +1739,46 @@ class PatchTSTForPrediction(PatchTSTPreTrainedModel):
             `PatchTSTForPredictionOutput` or tuple of `torch.Tensor` (if `return_dict`=False or
             `config.return_dict`=False)
 
-        """
+        Examples:
+
+        ```python
+        >>> from huggingface_hub import hf_hub_download
+        >>> import torch
+        >>> from transformers import PatchTSTConfig, PatchTSTForPrediction
+
+        >>> file = hf_hub_download(
+        ...     repo_id="hf-internal-testing/etth1-hourly-batch", filename="train-batch.pt", repo_type="dataset"
+        ... )
+        >>> batch = torch.load(file)
+
+        >>> # Prediction task with 7 input channels and prediction length is 96
+        >>> config = PatchTSTConfig(
+        ...     num_input_channels=7,
+        ...     context_length=512,
+        ...     patch_length = 12,
+        ...     stride = 12,
+        ...     use_cls_token = True,
+        ...     prediction_length = 96,
+        ... )
+        >>> model = PatchTSTForPrediction.from_pretrained("huggingface/patchtst-etth1-pretrain", config=config)
+
+        >>> # during training, one provides both past and future values
+        >>> outputs = model(
+        ...     past_values=batch["past_values"],
+        ...     future_values=batch["future_values"],
+        ... )
+
+        >>> loss = outputs.loss
+        >>> loss.backward()
+
+        >>> # during inference, one only provides past values, the model generates future values
+        >>> outputs = model.generate(
+        ...     past_values=batch["past_values"],
+        ... )
+
+        >>> mean_prediction = outputs.sequences.mean(dim=1)
+        ```"""
+
         return_dict = return_dict if return_dict is not None else self.config.use_return_dict
 
         # get model output
@@ -1945,35 +1926,6 @@ class PatchTSTRegressionHead(nn.Module):
 
 
 class PatchTSTForRegression(PatchTSTPreTrainedModel):
-    """
-    PatchTST model + Regression head
-
-    Examples:
-
-    ```python
-    >>> from transformers import PatchTSTConfig, PatchTSTForRegression
-
-    >>> # Regression task with 6 input channels and regress 2 targets
-    >>> config = PatchTSTConfig(
-    ...     num_input_channels=6,
-    ...     num_targets=2,
-    ...     context_length=512,
-    ...     patch_length = 12,
-    ...     stride = 12,
-    ...     use_cls_token = True,
-    ... )
-    >>> model = PatchTSTForRegression.from_pretrained("huggingface/patchtst-etth1-pretrain", config=config)
-
-    >>> # during inference, one only provides past values, the model generates future values
-    >>> past_values = torch.randn(20, 512, 6)
-    >>> outputs = model.generate(
-    ...     past_values=past_values
-    ... )
-
-    >>> mean_prediction = outputs.sequences.mean(dim=1)
-    ```
-    """
-
     def __init__(self, config: PatchTSTConfig):
         super().__init__(config)
 
@@ -2009,7 +1961,7 @@ class PatchTSTForRegression(PatchTSTPreTrainedModel):
         output_attentions: Optional[bool] = None,
         return_dict: Optional[bool] = None,
     ) -> Union[tuple, PatchTSTForRegressionOutput]:
-        """
+        r"""
         Parameters:
             past_values (`torch.Tensor` of shape `(bs, sequence_length, num_input_channels)`, *required*):
                 Input sequence to the model
@@ -2032,7 +1984,31 @@ class PatchTSTForRegression(PatchTSTPreTrainedModel):
             `PatchTSTForRegressionOutput` or tuple of `torch.Tensor` (if `return_dict`=False or
             `config.return_dict`=False)
 
-        """
+        Examples:
+
+        ```python
+        >>> from transformers import PatchTSTConfig, PatchTSTForRegression
+
+        >>> # Regression task with 6 input channels and regress 2 targets
+        >>> config = PatchTSTConfig(
+        ...     num_input_channels=6,
+        ...     num_targets=2,
+        ...     context_length=512,
+        ...     patch_length = 12,
+        ...     stride = 12,
+        ...     use_cls_token = True,
+        ... )
+        >>> model = PatchTSTForRegression.from_pretrained("huggingface/patchtst-etth1-pretrain", config=config)
+
+        >>> # during inference, one only provides past values, the model generates future values
+        >>> past_values = torch.randn(20, 512, 6)
+        >>> outputs = model.generate(
+        ...     past_values=past_values
+        ... )
+
+        >>> mean_prediction = outputs.sequences.mean(dim=1)
+        ```"""
+
         return_dict = return_dict if return_dict is not None else self.config.use_return_dict
 
         model_output = self.model(
