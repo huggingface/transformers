cde53efe376ef47ba49fe27c92b722161fd52d88
younesbelkada <younesbelkada@gmail.com>
2023-11-21 17:34:48 +0100

    split up into 3 different private methods

--- src/transformers/integrations/awq.py
+++ src/transformers/integrations/awq.py
@@ -178,93 +178,151 @@ def fuse_awq_modules(model, quantization_config):
         from awq.modules.fused.attn import QuantAttentionFused
         from awq.modules.fused.mlp import QuantFusedMLP
         from awq.modules.fused.norm import FasterTransformerRMSNorm
-        from awq.modules.linear import WQLinear_GEMM, WQLinear_GEMV
     else:
         raise ValueError("Fusing is only supported for the AutoAWQ backend")
 
     for name, module in model.named_modules():
         # Replace layer norms
-        for module_name in modules_to_fuse["layernorm"]:
-            if hasattr(module, module_name):
-                old_module = getattr(module, module_name)
-                module._modules[module_name] = FasterTransformerRMSNorm(
-                    old_module.weight,
-                    old_module.variance_epsilon,
-                ).to(old_module.weight.device)
-                del old_module
+        _fuse_awq_layernorm(modules_to_fuse["layernorm"], module, FasterTransformerRMSNorm)
+
         # Replace MLP layers
-        if hasattr(module, modules_to_fuse["mlp"][0]):
-            gate_proj = getattr(module, modules_to_fuse["mlp"][0])
-            up_proj = getattr(module, modules_to_fuse["mlp"][1])
-            down_proj = getattr(module, modules_to_fuse["mlp"][2])
-
-            previous_device = gate_proj.qweight.device
-            activation_fn = ACT2FN[model.config.hidden_act]
-            new_module = QuantFusedMLP(gate_proj, down_proj, up_proj, activation_fn)
-
-            parent_name, child_name = name.rsplit(".", 1)
-            parent = model.get_submodule(parent_name)
-            setattr(parent, child_name, new_module.to(previous_device))
-
-            del gate_proj, up_proj, down_proj
+        _fuse_awq_mlp(model, name, modules_to_fuse["mlp"], module, QuantFusedMLP)
 
         # Replace attention layers
-        # inside modules_to_fuse["attention"] we should have (in correct order): q, k, v, o layer
-        if hasattr(module, modules_to_fuse["attention"][0]):
-            # First, we pack the QKV layers together
-            q_proj = getattr(module, modules_to_fuse["attention"][0])
-            previous_device = q_proj.qweight.device
-
-            if isinstance(q_proj, WQLinear_GEMV):
-                target_cls = WQLinear_GEMV
-                cat_dim = 0
-            elif isinstance(q_proj, WQLinear_GEMM):
-                target_cls = WQLinear_GEMM
-                cat_dim = 1
-            else:
-                raise ValueError("Unsupported q_proj type: {type(q_proj)}")
-
-            k_proj = getattr(module, modules_to_fuse["attention"][1])
-            v_proj = getattr(module, modules_to_fuse["attention"][2])
-            o_proj = getattr(module, modules_to_fuse["attention"][3])
-
-            bias = torch.cat([q_proj.bias, k_proj.bias, v_proj.bias], dim=0) if q_proj.bias is not None else None
-
-            qkv_layer = target_cls(
-                q_proj.w_bit,
-                q_proj.group_size,
-                q_proj.in_features,
-                q_proj.out_features + k_proj.out_features + v_proj.out_features,
-                q_proj.bias is not None,
-                next(iter(module.state_dict().values())).device,
-            )
-
-            qkv_layer.qweight = torch.cat([q_proj.qweight, k_proj.qweight, v_proj.qweight], dim=cat_dim)
-            qkv_layer.qzeros = torch.cat([q_proj.qzeros, k_proj.qzeros, v_proj.qzeros], dim=cat_dim)
-            qkv_layer.scales = torch.cat([q_proj.scales, k_proj.scales, v_proj.scales], dim=cat_dim)
-
-            if isinstance(qkv_layer, WQLinear_GEMV):
-                qkv_layer.split_k_iters = q_proj.split_k_iters
-
-            qkv_layer.bias = bias
-
-            fused_attention_layer = QuantAttentionFused(
-                modules_to_fuse["hidden_size"],
-                modules_to_fuse["num_attention_heads"],
-                modules_to_fuse["num_key_value_heads"],
-                qkv_layer,
-                o_proj,
-                previous_device,
-                modules_to_fuse["max_seq_len"],
-                use_alibi=modules_to_fuse["use_alibi"],
-            )
-
-            fused_attention_layer.is_hf_transformers = True
-
-            parent_name, child_name = name.rsplit(".", 1)
-            parent = model.get_submodule(parent_name)
-            setattr(parent, child_name, fused_attention_layer.to(previous_device))
-
-            del q_proj, k_proj, v_proj, o_proj
-
+        _fuse_awq_attention_layers(model, module, modules_to_fuse, name, QuantAttentionFused)
     return model
+
+
+def _fuse_awq_layernorm(fuse_module_names, module, target_cls):
+    """
+    Fuse the LayerNorm layers into a target class using autoawq
+
+    Args:
+        fuse_module_names (`List[str]`):
+            The list of module names to fuse
+        module (`nn.Module`):
+            The pytorch parent module that has layernorm modules to fuse
+        target_cls (`~autoawq.FasterTransformerRMSNorm`):
+            The `FasterTransformerRMSNorm` class as it only supports that class
+            for now.
+    """
+    for module_name in fuse_module_names:
+        if hasattr(module, module_name):
+            old_module = getattr(module, module_name)
+            module._modules[module_name] = target_cls(
+                old_module.weight,
+                old_module.variance_epsilon,
+            ).to(old_module.weight.device)
+            del old_module
+
+
+def _fuse_awq_mlp(model, current_module_name, fuse_module_names, module, target_cls):
+    """
+    Fuse the MLP layers into a target class using autoawq
+
+    Args:
+        model (`~PreTrainedModel`):
+            The input pretrained model
+        current_module_name (`str`):
+            The current submodule name
+        fuse_module_names (`List[str]`):
+            The list of module names to fuse. For the MLP layers it has to be an array
+            of length 3 that consists of the 3 MLP layers in the order (gate (dense layer post-attention) / up / down layers)
+        module (`nn.Module`):
+            The pytorch parent module that has layernorm modules to fuse
+        target_cls (`~autoawq.QuantFusedMLP`):
+            The `QuantFusedMLP` class as it only supports that class
+            for now.
+    """
+    if hasattr(module, fuse_module_names[0]):
+        gate_proj = getattr(module, fuse_module_names[0])
+        up_proj = getattr(module, fuse_module_names[1])
+        down_proj = getattr(module, fuse_module_names[2])
+
+        previous_device = gate_proj.qweight.device
+        activation_fn = ACT2FN[model.config.hidden_act]
+        new_module = target_cls(gate_proj, down_proj, up_proj, activation_fn)
+
+        parent_name, child_name = current_module_name.rsplit(".", 1)
+        parent = model.get_submodule(parent_name)
+        setattr(parent, child_name, new_module.to(previous_device))
+
+        del gate_proj, up_proj, down_proj
+
+
+def _fuse_awq_attention_layers(model, module, modules_to_fuse, current_module_name, target_cls):
+    """
+    Fuse the Attention layers into a target class using autoawq
+
+    Args:
+        model (`~PreTrainedModel`):
+            The input pretrained model
+        module (`nn.Module`):
+            The pytorch parent module that has layernorm modules to fuse
+        modules_to_fuse (`List[str]`):
+            The module fusing mapping. The dictionary has to contain a field `attention` with attention module names
+            in the correct order: q, k, v, o layer
+        current_module_name (`str`):
+            The current submodule name
+        target_cls (`~autoawq.QuantAttentionFused`):
+            The `QuantAttentionFused` class as it only supports that class
+            for now.
+    """
+    from awq.modules.linear import WQLinear_GEMM, WQLinear_GEMV
+
+    if hasattr(module, modules_to_fuse["attention"][0]):
+        # First, we pack the QKV layers together
+        q_proj = getattr(module, modules_to_fuse["attention"][0])
+        previous_device = q_proj.qweight.device
+
+        if isinstance(q_proj, WQLinear_GEMV):
+            linear_target_cls = WQLinear_GEMV
+            cat_dim = 0
+        elif isinstance(q_proj, WQLinear_GEMM):
+            linear_target_cls = WQLinear_GEMM
+            cat_dim = 1
+        else:
+            raise ValueError("Unsupported q_proj type: {type(q_proj)}")
+
+        k_proj = getattr(module, modules_to_fuse["attention"][1])
+        v_proj = getattr(module, modules_to_fuse["attention"][2])
+        o_proj = getattr(module, modules_to_fuse["attention"][3])
+
+        bias = torch.cat([q_proj.bias, k_proj.bias, v_proj.bias], dim=0) if q_proj.bias is not None else None
+
+        qkv_layer = linear_target_cls(
+            q_proj.w_bit,
+            q_proj.group_size,
+            q_proj.in_features,
+            q_proj.out_features + k_proj.out_features + v_proj.out_features,
+            q_proj.bias is not None,
+            next(iter(module.state_dict().values())).device,
+        )
+
+        qkv_layer.qweight = torch.cat([q_proj.qweight, k_proj.qweight, v_proj.qweight], dim=cat_dim)
+        qkv_layer.qzeros = torch.cat([q_proj.qzeros, k_proj.qzeros, v_proj.qzeros], dim=cat_dim)
+        qkv_layer.scales = torch.cat([q_proj.scales, k_proj.scales, v_proj.scales], dim=cat_dim)
+
+        if isinstance(qkv_layer, WQLinear_GEMV):
+            qkv_layer.split_k_iters = q_proj.split_k_iters
+
+        qkv_layer.bias = bias
+
+        fused_attention_layer = target_cls(
+            modules_to_fuse["hidden_size"],
+            modules_to_fuse["num_attention_heads"],
+            modules_to_fuse["num_key_value_heads"],
+            qkv_layer,
+            o_proj,
+            previous_device,
+            modules_to_fuse["max_seq_len"],
+            use_alibi=modules_to_fuse["use_alibi"],
+        )
+
+        fused_attention_layer.is_hf_transformers = True
+
+        parent_name, child_name = current_module_name.rsplit(".", 1)
+        parent = model.get_submodule(parent_name)
+        setattr(parent, child_name, fused_attention_layer.to(previous_device))
+
+        del q_proj, k_proj, v_proj, o_proj
