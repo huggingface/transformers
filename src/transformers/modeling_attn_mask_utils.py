f0840401e7f8c6d4363d82606a9ef0d48946301e
fxmarty <9808326+fxmarty@users.noreply.github.com>
2023-11-22 02:13:18 +0900

    Update src/transformers/modeling_attn_mask_utils.py
    Co-authored-by: Arthur <48595927+ArthurZucker@users.noreply.github.com>

--- src/transformers/modeling_attn_mask_utils.py
+++ src/transformers/modeling_attn_mask_utils.py
@@ -342,7 +342,7 @@ def _prepare_4d_causal_attention_mask_for_sdpa(
                 attention_mask = None
             else:
                 # Unfortunately, for query_length > 1 and key_value_length != query_length, we cannot generally ignore the attention mask, as SDPA causal mask generation
-                # may be wrong. We will set is_causal=False in SDPA and rely on Transformers attention_mask instead, hence not setting it to None here.
+                # may be wrong. We will set `is_causal=False` in SDPA and rely on Transformers attention_mask instead, hence not setting it to None here.
                 # Reference: https://github.com/pytorch/pytorch/issues/108108
                 pass
 
