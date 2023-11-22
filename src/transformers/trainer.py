b09af9463697b2c135cb978b4d192e5b6f085cc6
DESKTOP-42S1K65 <muellerzr@gmail.com>
2023-11-22 12:09:17 -0500

    Only do it if in log

--- src/transformers/trainer.py
+++ src/transformers/trainer.py
@@ -2655,7 +2655,9 @@ class Trainer:
             logs["num_input_tokens_seen"] = self.state.num_input_tokens_seen
 
         output = {**logs, **{"step": self.state.global_step}}
-        output["learning_rate"] = "{:.2e}".format(logs["learning_rate"])
+        if "learning_rate" in logs:
+            # Conver to scientific notation for space
+            output["learning_rate"] = "{:.2e}".format(logs["learning_rate"])
         self.state.log_history.append(output)
         self.control = self.callback_handler.on_log(self.args, self.state, self.control, logs)
 
