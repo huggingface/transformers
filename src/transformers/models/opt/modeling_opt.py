e4a18852318b64b723b28f3b008bad6a38536087
Félix Marty <9808326+fxmarty@users.noreply.github.com>
2023-11-23 14:46:25 +0100

    update opt

--- src/transformers/models/opt/modeling_opt.py
+++ src/transformers/models/opt/modeling_opt.py
@@ -35,6 +35,7 @@ from ...utils import (
     add_start_docstrings,
     add_start_docstrings_to_model_forward,
     is_flash_attn_2_available,
+    is_flash_attn_greater_or_equal_210,
     logging,
     replace_return_docstrings,
 )
@@ -288,6 +289,14 @@ class OptFlashAttention2(OPTAttention):
     attention and deal with padding tokens in case the input contains any of them.
     """
 
+    # Copied from transformers.models.llama.modeling_llama.LlamaFlashAttention2.__init__
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # flash_attn<2.1 generates top-left aligned causal mask, while what is needed here is bottom-right alignement, that was made default for flash_attn>=2.1. This attribute is used
+        # to handle this difference.
+        self.flash_attn_uses_top_left_mask = not is_flash_attn_greater_or_equal_210()
+
     def forward(
         self,
         hidden_states: torch.Tensor,
@@ -404,6 +413,12 @@ class OptFlashAttention2(OPTAttention):
             softmax_scale (`float`, *optional*):
                 The scaling of QK^T before applying softmax. Default to 1 / sqrt(head_dim)
         """
+        if not self.flash_attn_uses_top_left_mask:
+            causal = self.is_causal
+        else:
+            # NOTE: `causal=self.is_causal and query_length != 1` is required for compatibility with flash_attn>=2.0,<2.1, reference: https://github.com/Dao-AILab/flash-attention/releases/tag/v2.1.0. We need to remove this check `query_length != 1` once Flash Attention for RoCm is bumped to 2.1. With flash_attn<2.1, using q_seqlen != k_seqlen (except for the case q_seqlen ==1) produces a wrong mask (top-left).
+            causal = self.is_causal and query_length != 1
+
         # Contains at least one padding token in the sequence
         if attention_mask is not None:
             batch_size = query_states.shape[0]
@@ -424,13 +439,18 @@ class OptFlashAttention2(OPTAttention):
                 max_seqlen_k=max_seqlen_in_batch_k,
                 dropout_p=dropout,
                 softmax_scale=softmax_scale,
-                causal=self.is_causal,
+                causal=causal,
             )
 
             attn_output = pad_input(attn_output_unpad, indices_q, batch_size, query_length)
         else:
             attn_output = flash_attn_func(
-                query_states, key_states, value_states, dropout, softmax_scale=softmax_scale, causal=self.is_causal
+                query_states,
+                key_states,
+                value_states,
+                dropout,
+                softmax_scale=softmax_scale,
+                causal=causal,
             )
 
         return attn_output
