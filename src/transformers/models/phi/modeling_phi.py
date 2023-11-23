a33fa732475c3b266b043ef9833a3d34f84cf366
Susnato Dhar <susnatodhar10@gmail.com>
2023-11-23 18:18:43 +0530

    comment

--- src/transformers/models/phi/modeling_phi.py
+++ src/transformers/models/phi/modeling_phi.py
@@ -460,7 +460,7 @@ class PhiFlashAttention2(PhiAttention):
         # therefore the input hidden states gets silently casted in float32. Hence, we need
         # cast them back in the correct dtype just to be sure everything works as expected.
         # This might slowdown training & inference so it is recommended to not cast the LayerNorms
-        # in fp32. (LlamaRMSNorm handles it correctly)
+        # in fp32.
 
         if query_states.dtype == torch.float32:
             # Handle the case where the model is quantized
