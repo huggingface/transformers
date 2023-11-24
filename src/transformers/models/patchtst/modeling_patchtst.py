b704fdbe1c754f073a5da42e9d1343c0f951960e
nnguyen <nnguyen@us.ibm.com>
2023-11-23 22:31:07 -0500

    add output_hidden_states and config.num_patches

--- src/transformers/models/patchtst/modeling_patchtst.py
+++ src/transformers/models/patchtst/modeling_patchtst.py
@@ -386,8 +386,8 @@ class PatchTSTPatchify(nn.Module):
             )
 
         # get the number of patches
-        self.num_patches = (max(self.sequence_length, self.patch_length) - self.patch_length) // self.patch_stride + 1
-        new_sequence_length = self.patch_length + self.patch_stride * (self.num_patches - 1)
+        config.num_patches = (max(self.sequence_length, self.patch_length) - self.patch_length) // self.patch_stride + 1
+        new_sequence_length = self.patch_length + self.patch_stride * (config.num_patches - 1)
         self.sequence_start = self.sequence_length - new_sequence_length
 
     def forward(self, past_values: torch.Tensor):
@@ -1271,6 +1271,7 @@ class PatchTSTModel(PatchTSTPreTrainedModel):
 
         return_dict = return_dict if return_dict is not None else self.config.use_return_dict
         output_attentions = output_attentions if output_attentions is not None else self.config.output_attentions
+        output_hidden_states = output_hidden_states if output_hidden_states is not None else self.config.output_hidden_states
 
         if past_observed_mask is None:
             past_observed_mask = torch.ones_like(past_values)
