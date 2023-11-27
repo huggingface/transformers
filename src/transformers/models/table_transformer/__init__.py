572d70ae02f16dd85c16fb5bc2f2f4b0d32a69ff
Niels <niels.rogge1@gmail.com>
2023-11-27 11:39:44 +0100

    Remove is_vision_available

--- src/transformers/models/table_transformer/__init__.py
+++ src/transformers/models/table_transformer/__init__.py
@@ -14,7 +14,7 @@
 
 from typing import TYPE_CHECKING
 
-from ...utils import OptionalDependencyNotAvailable, _LazyModule, is_torch_available, is_vision_available
+from ...utils import OptionalDependencyNotAvailable, _LazyModule, is_torch_available
 
 
 _import_structure = {
