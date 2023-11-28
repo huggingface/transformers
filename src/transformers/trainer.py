2ca73e5ee320078a275e40a95ad32f040a389d39
Charbel Abi Daher <45701489+CharbelAD@users.noreply.github.com>
2023-11-28 08:33:45 +0100

    Fixed passing scheduler-specific kwargs via TrainingArguments lr_scheduler_kwargs (#27595)
    * Fix passing scheduler-specific kwargs through TrainingArguments `lr_scheduler_kwargs`
    
    * Added test for lr_scheduler_kwargs

--- src/transformers/trainer.py
+++ src/transformers/trainer.py
@@ -1111,7 +1111,7 @@ class Trainer:
                 optimizer=self.optimizer if optimizer is None else optimizer,
                 num_warmup_steps=self.args.get_warmup_steps(num_training_steps),
                 num_training_steps=num_training_steps,
-                **self.args.lr_scheduler_kwargs,
+                scheduler_specific_kwargs=self.args.lr_scheduler_kwargs,
             )
             self._created_lr_scheduler = True
         return self.lr_scheduler
