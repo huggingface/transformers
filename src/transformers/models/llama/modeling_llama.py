1510746437e1b782651cf6840bee11bbd473befe
Joao Gante <joaofranciscocardosogante@gmail.com>
2023-11-30 17:47:50 +0000

    Apply suggestions from code review
    Co-authored-by: Patrick von Platen <patrick.v.platen@gmail.com>
    Co-authored-by: Tom Aarsen <37621491+tomaarsen@users.noreply.github.com>

--- src/transformers/models/llama/modeling_llama.py
+++ src/transformers/models/llama/modeling_llama.py
@@ -394,8 +394,8 @@ class LlamaAttention(nn.Module):
         if past_key_value is not None:
             if self.layer_idx is None:
                 raise ValueError(
-                    f"The cache structure has changed since version v4.36. If you are using {self.__class__.__name__}"
-                    "for auto-regressive decoding with k/v caching, please make sure to initialize the attention class"
+                    f"The cache structure has changed since version v4.36. If you are using {self.__class__.__name__} "
+                    "for auto-regressive decoding with k/v caching, please make sure to initialize the attention class "
                     "with a layer index."
                 )
             kv_seq_len += past_key_value.get_seq_length(self.layer_idx)
