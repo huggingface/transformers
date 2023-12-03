56943d0c89f2c65028b00a2769a609da4a66f70b
blbadger <blbadger10@gmail.com>
2023-12-03 15:32:29 -0500

    debugged

--- src/transformers/modeling_utils.py
+++ src/transformers/modeling_utils.py
@@ -2138,7 +2138,8 @@ class PreTrainedModel(nn.Module, ModuleUtilsMixin, GenerationMixin, PushToHubMix
                     ptrs[id(tensor)].append(name)
 
             # These are all the pointers of shared tensors.
-            shared_ptrs = {ptr: names for ptr, names in ptrs.items() if len(names) > 1 and ptr.device.type != 'meta'}
+            shared_ptrs = {ptr: names for ptr, names in ptrs.items() if len(names) > 1 and ptr[0].device.type != 'meta'}
+            print (shared_ptrs)
             warn_names = set()
             for names in shared_ptrs.values():
                 # Removing the keys which are declared as known duplicates on
