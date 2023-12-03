Fix beam score calculation issue for JAX

src/transformers/generation/flax_utils.py
@@ -123,6 +123,7 @@ class BeamSearchState:
     sequences: jnp.ndarray
     scores: jnp.ndarray
     is_sent_finished: jnp.ndarray
+    decoder_prompt_len: int
     model_kwargs: Dict[str, jnp.ndarray]
 
 
@@ -809,6 +810,8 @@ class FlaxGenerationMixin:
         pad_token_id = jnp.array(pad_token_id, dtype=jnp.int32)
         cur_len = jnp.array(cur_len)
 
+        decoder_prompt_len = input_ids.shape[-1]
+
         # per batch,beam-item holding current token in loop.
         sequences = jnp.full((batch_size, num_beams, max_length), pad_token_id, dtype=jnp.int32)
         running_sequences = jnp.full((batch_size, num_beams, max_length), pad_token_id, dtype=jnp.int32)
@@ -845,6 +848,7 @@ class FlaxGenerationMixin:
             sequences=sequences,
             scores=scores,
             is_sent_finished=is_sent_finished,
+            decoder_prompt_len=decoder_prompt_len,
             model_kwargs=model_kwargs,
         )
 
@@ -861,9 +865,17 @@ class FlaxGenerationMixin:
             # early_stopping == "never" -> compute the best score from max_length or cur_len, depending on the sign of
             #   length_penalty. Positive length_penalty favors longer sequences, thus we use max_length there.
             if early_stopping == "never" and length_penalty > 0.0:
-                best_running_score = state.running_scores[:, :1] / (max_length**length_penalty)
+                if max_length <= state.decoder_prompt_len:
+                    raise ValueError("max_length is not larger than the prompt length of decoder")
+                best_running_score = state.running_scores[:, :1] / (
+                    (max_length - state.decoder_prompt_len) ** length_penalty
+                )
             else:
-                best_running_score = state.running_scores[:, :1] / (state.cur_len**length_penalty)
+                if state.cur_len <= state.decoder_prompt_len:
+                    raise ValueError("current length of sequence is not larger than the prompt length of decoder")
+                best_running_score = state.running_scores[:, :1] / (
+                    (state.cur_len - state.decoder_prompt_len) ** length_penalty
+                )
             worst_finished_score = jnp.where(
                 state.is_sent_finished, jnp.min(state.scores, axis=1, keepdims=True), np.array(-1.0e7)
             )
@@ -953,7 +965,7 @@ class FlaxGenerationMixin:
             # - add length penalty
             # - make sure no scores can be added anymore if beam is full
             # - make sure still running sequences cannot be chosen as finalized beam
-            topk_log_probs = topk_log_probs / (state.cur_len**length_penalty)
+            topk_log_probs = topk_log_probs / ((state.cur_len + 1 - state.decoder_prompt_len) ** length_penalty)
             beams_in_batch_are_full = jnp.broadcast_to(
                 state.is_sent_finished.all(axis=-1, keepdims=True), did_topk_just_finished.shape
             ) & (early_stopping is True)
@@ -987,6 +999,7 @@ class FlaxGenerationMixin:
                 scores=next_scores,
                 sequences=next_sequences,
                 is_sent_finished=next_is_sent_finished,
+                decoder_prompt_len=state.decoder_prompt_len,
                 model_kwargs=next_model_kwargs,
             )
 