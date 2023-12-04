8ddbb29827fdb337dc7865bb3bfa798e615f48e5
fxmarty <9808326+fxmarty@users.noreply.github.com>
2023-12-04 21:05:07 +0900

    Update src/transformers/modeling_utils.py
    Co-authored-by: amyeroberts <22614925+amyeroberts@users.noreply.github.com>

--- src/transformers/modeling_utils.py
+++ src/transformers/modeling_utils.py
@@ -1292,7 +1292,7 @@ class PreTrainedModel(nn.Module, ModuleUtilsMixin, GenerationMixin, PushToHubMix
                 flash_attention_version = version.parse(importlib.metadata.version("flash_attn"))
                 if flash_attention_version < version.parse("2.1.0"):
                     raise ImportError(
-                        f"{preface} you need flash_attn package version to be greater or equal than 2.1.0. Make sure to have that version installed - detected version {flash_attention_version}. {install_message}"
+                        f"{preface} you need flash_attn package version to be greater or equal than 2.1.0. Detected version {flash_attention_version}. {install_message}"
                     )
                 else:
                     raise ImportError(f"{preface} Flash Attention 2 is not available. {install_message}")
