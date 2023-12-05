a28ec0655b368edb4c464d767aae39877425a445
younesbelkada <younesbelkada@gmail.com>
2023-12-05 09:00:31 +0100

    update conversion script

--- src/transformers/models/llava/convert_llava_weights_to_hf.py
+++ src/transformers/models/llava/convert_llava_weights_to_hf.py
@@ -63,25 +63,28 @@ def convert_llava_llama_to_hf(text_model_id, vision_model_id, output_hub_path, o
     with torch.device("meta"):
         model = LlavaForVisionText2Text(config)
 
+    # Pad to 64 for performance reasons
+    pad_shape = 64
+
     state_dict_path = hf_hub_download(old_state_dict_id, "model_state_dict.bin")
 
     state_dict = torch.load(state_dict_path, map_location="cpu")
     state_dict = convert_state_dict_to_hf(state_dict)
-
-    embed_tokens = state_dict["language_model.model.embed_tokens.weight"]
-    lm_head = state_dict["language_model.lm_head.weight"]
-
-    new_embed_tokens = torch.zeros((embed_tokens.shape[0] + 1, embed_tokens.shape[1]), dtype=embed_tokens.dtype)
-    new_embed_tokens.data[: config.text_config.vocab_size - 1, :] = embed_tokens
-
-    state_dict["language_model.model.embed_tokens.weight"] = new_embed_tokens
-
-    new_lm_head = torch.zeros((lm_head.shape[0] + 1, lm_head.shape[1]), dtype=lm_head.dtype)
-    new_lm_head.data[: config.text_config.vocab_size - 1, :] = lm_head
-
-    state_dict["language_model.lm_head.weight"] = new_lm_head
-
     model.load_state_dict(state_dict, strict=True, assign=True)
+
+    pre_expansion_embeddings = model.language_model.model.embed_tokens.weight.data
+    mu = torch.mean(pre_expansion_embeddings, dim=0).float()
+    n = pre_expansion_embeddings.size()[0]
+    sigma = ((pre_expansion_embeddings - mu).T @ (pre_expansion_embeddings - mu)) / n
+    dist = torch.distributions.multivariate_normal.MultivariateNormal(mu, covariance_matrix=1e-5 * sigma)
+
+    # We add an image token so we resize the model
+    model.resize_token_embeddings(config.text_config.vocab_size + 2, pad_shape)
+    model.language_model.model.embed_tokens.weight.data[32000:] = torch.stack(
+        tuple((dist.sample() for _ in range(model.language_model.model.embed_tokens.weight.data[32000:].shape[0]))),
+        dim=0,
+    )
+    model.language_model.lm_head.weight.data = model.language_model.model.embed_tokens.weight.data.T
 
     model.push_to_hub(output_hub_path)
     processor.push_to_hub(output_hub_path)
