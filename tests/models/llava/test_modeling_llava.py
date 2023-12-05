c205929f4759c279b43921d48f44a34a80c16b47
Arthur Zucker <arthur.zucker@gmail.com>
2023-12-05 11:43:11 +0100

    cleanup

--- tests/models/llava/test_modeling_llava.py
+++ tests/models/llava/test_modeling_llava.py
@@ -192,7 +192,7 @@ class LlavaForConditionalGenerationModelTest(ModelTesterMixin, unittest.TestCase
 @require_torch
 class LlavaForConditionalGenerationIntegrationTest(unittest.TestCase):
     def setUp(self):
-        self.processor = AutoProcessor.from_pretrained("ArthurZ/llava-1.5-7b")
+        self.processor = AutoProcessor.from_pretrained("llava-hf/bakLlava-v1-hf")
 
     def test_large_model_integration_test(self):
         # Let' s make sure we test the preprocessing to replace what is used
@@ -227,7 +227,7 @@ class LlavaForConditionalGenerationIntegrationTest(unittest.TestCase):
 
     def test_small_model_integration_test(self):
         # Let' s make sure we test the preprocessing to replace what is used
-        model = LlavaForConditionalGeneration.from_pretrained("ArthurZ/llava-1.5-7b")
+        model = LlavaForConditionalGeneration.from_pretrained("llava-hf/bakLlava-v1-hf")
         EXPECTED_OUTPUTS = torch.tensor([[1, 32000, 29871, 13, 11889, 29901, 1724, 526, 278, 2712, 306, 881, 367, 274, 1300, 2738, 1048, 746, 306, 6493, 445, 2058, 29973, 13, 22933, 9047, 13566, 29901, 1932, 6493, 292, 445, 2058, 29892, 607, 5692, 304, 367, 263, 9307, 470, 23630, 23771, 964, 263, 3573, 310, 4094, 29892, 727, 526, 263, 2846, 2712, 304, 367, 274, 1300, 2738, 1048, 29889, 3824, 29892, 367, 9543, 310, 278, 4094, 10809, 322, 738, 7037, 447, 29920, 3163, 29892, 1316, 408, 1014, 1050, 3192, 23150, 470, 316, 1182, 275, 29889, 6440, 29892, 367, 3458, 1319, 310, 278, 14826, 5855, 29892, 408, 8327, 3620, 297, 14826, 508, 1207, 278, 9307, 470, 23630, 25110, 304, 6686, 373, 29889, 18008, 29892, 367, 274, 1300, 2738, 310, 278, 18830, 5177, 29892, 408, 727, 1795, 367, 8775, 19264, 470, 916, 7037, 447, 29920, 3163, 297, 278, 4038, 29889, 9208, 368, 29892, 367, 9543, 310, 738, 1887, 1072, 8250, 470, 1410, 10652, 1475, 363, 278, 671, 310, 278, 9307, 470, 23630, 29892, 408, 777, 10161, 1122, 505, 2702, 6865, 470, 25091, 297, 2058, 29889, 2,]])  # fmt: skip
         prompt = "<image>\nUSER: What are the things I should be cautious about when I visit this place?\nASSISTANT:"
         image_file = "https://llava-vl.github.io/static/images/view.jpg"
@@ -245,7 +245,7 @@ class LlavaForConditionalGenerationIntegrationTest(unittest.TestCase):
 
     def test_small_model_integration_test_batch(self):
         # Let' s make sure we test the preprocessing to replace what is used
-        model = LlavaForConditionalGeneration.from_pretrained("ArthurZ/llava-1.5-7b")
+        model = LlavaForConditionalGeneration.from_pretrained("llava-hf/bakLlava-v1-hf")
         EXPECTED_OUTPUTS = torch.tensor([[1, 32000, 29871, 13, 11889, 29901, 1724, 526, 278, 2712, 306, 881, 367, 274, 1300, 2738, 1048, 746, 306, 6493, 445, 2058, 29973, 13, 22933, 9047, 13566, 29901, 1932, 6493, 292, 445, 2058, 29892, 607, 5692, 304, 367, 263, 9307, 470, 23630, 23771, 964, 263, 3573, 310, 4094, 29892, 727, 526, 263, 2846, 2712, 304, 367, 274, 1300, 2738, 1048, 29889, 3824, 29892, 367, 9543, 310, 278, 4094, 10809, 322, 738, 7037, 447, 29920, 3163, 29892, 1316, 408, 1014, 1050, 3192, 23150, 470, 316, 1182, 275, 29889, 6440, 29892, 367, 3458, 1319, 310, 278, 14826, 5855, 29892, 408, 8327, 3620, 297, 14826, 508, 1207, 278, 9307, 470, 23630, 25110, 304, 6686, 373, 29889, 18008, 29892, 367, 274, 1300, 2738, 310, 278, 18830, 5177, 29892, 408, 727, 1795, 367, 8775, 19264, 470, 916, 7037, 447, 29920, 3163, 297, 278, 4038, 29889, 9208, 368, 29892, 367, 9543, 310, 738, 1887, 1072, 8250, 470, 1410, 10652, 1475, 363, 278, 671, 310, 278, 9307, 470, 23630, 29892, 408, 777, 10161, 1122, 505, 2702, 6865, 470, 25091, 297, 2058, 29889, 2,]])  # fmt: skip
         # The first batch is longer in terms of text, but only has 1 image. The second batch will be padded in text, but the first will be padded because images take more space!.
         prompts = [
@@ -268,19 +268,3 @@ class LlavaForConditionalGenerationIntegrationTest(unittest.TestCase):
 
         # EXPECTED_DECODED_TEXT = """\nUSER: What are the things I should be cautious about when I visit this place?\nASSISTANT: When visiting this place, which appears to be a pier or dock extending into a body of water, there are a few things to be cautious about. First, be aware of the water depth and any potential hazards, such as submerged rocks or debris. Second, be mindful of the weather conditions, as sudden changes in weather can make the pier or dock unsafe to walk on. Third, be cautious of the surrounding environment, as there might be wildlife or other potential hazards in the area. Lastly, be aware of any local regulations or guidelines for the use of the pier or dock, as some areas may have specific rules or restrictions in place.""" # fmt: skip
         # self.assertEqual(self.processor.decode(output[0][2:], skip_special_tokens=True), EXPECTED_DECODED_TEXT)
-
-    def test_small_model_logits_test(self):
-        # Let' s make sure we test the preprocessing to replace what is used
-        pass
-
-    def test_image_text_pre_processing(self):
-        # Let' s make sure we test the preprocessing to replace what is used
-        pass
-
-    def test_prepare_inputs_labels_for_multimodal(self):
-        # Pretty much the most important function
-        pass
-
-    def test_tokenizer_image_token(self):
-        # prompt = "Make sure to always answer in a truthful manner"
-        pass
