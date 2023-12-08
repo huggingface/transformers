85a217afef88d9217617a96c1af0a28145faf105
BBuf <1182563586@qq.com>
2023-12-08 09:21:23 +0800

    format

--- src/transformers/models/rwkv5/modeling_rwkv5.py
+++ src/transformers/models/rwkv5/modeling_rwkv5.py
@@ -470,7 +470,6 @@ class Rwkv5Model(Rwkv5PreTrainedModel):
         output_hidden_states: Optional[bool] = None,
         return_dict: Optional[bool] = None,
     ) -> Union[Tuple, Rwkv5Output]:
-
         output_attentions = output_attentions if output_attentions is not None else self.config.output_attentions
         output_hidden_states = (
             output_hidden_states if output_hidden_states is not None else self.config.output_hidden_states
