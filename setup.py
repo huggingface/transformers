f9da44479d67c36022a4c4861933b9733b828944
younesbelkada <younesbelkada@gmail.com>
2023-12-08 13:08:48 +0100

    test

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
-# limitations under the License.
+# limitations under the License. 
 
 """
 Simple check list from AllenNLP repo: https://github.com/allenai/allennlp/blob/main/setup.py
