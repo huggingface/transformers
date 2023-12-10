58ad11eb7a0f4d031f92a82e8d51cacb7e5363e0
Younes Belkada <49240599+younesbelkada@users.noreply.github.com>
2023-12-10 22:04:44 +0100

    Apply suggestions from code review

--- src/transformers/models/auto/processing_auto.py
+++ src/transformers/models/auto/processing_auto.py
@@ -86,7 +86,7 @@ PROCESSOR_MAPPING_NAMES = OrderedDict(
         ("unispeech", "Wav2Vec2Processor"),
         ("unispeech-sat", "Wav2Vec2Processor"),
         ("vilt", "ViltProcessor"),
-        ("vipllava", "VipLlavaProcessor"),
+        ("vipllava", "LlavaProcessor"),
         ("vision-text-dual-encoder", "VisionTextDualEncoderProcessor"),
         ("wav2vec2", "Wav2Vec2Processor"),
         ("wav2vec2-conformer", "Wav2Vec2Processor"),
