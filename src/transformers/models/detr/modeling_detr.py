077f8455aa5b140e231bfb25e29f1e108268c720
NielsRogge <48327001+NielsRogge@users.noreply.github.com>
2023-12-11 19:03:42 +0100

    Update bounding box format everywhere (#27944)
    Update formats

--- src/transformers/models/detr/modeling_detr.py
+++ src/transformers/models/detr/modeling_detr.py
@@ -1535,7 +1535,7 @@ class DetrForObjectDetection(DetrPreTrainedModel):
         >>> inputs = image_processor(images=image, return_tensors="pt")
         >>> outputs = model(**inputs)
 
-        >>> # convert outputs (bounding boxes and class logits) to COCO API
+        >>> # convert outputs (bounding boxes and class logits) to Pascal VOC format (xmin, ymin, xmax, ymax)
         >>> target_sizes = torch.tensor([image.size[::-1]])
         >>> results = image_processor.post_process_object_detection(outputs, threshold=0.9, target_sizes=target_sizes)[
         ...     0
