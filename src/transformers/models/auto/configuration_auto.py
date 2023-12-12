f4e02ab0a05398e4314324b79e4aa038c9f8541d
Matt <rocketknight1@gmail.com>
2023-12-12 14:21:01 +0000

    make fixup

--- src/transformers/models/auto/configuration_auto.py
+++ src/transformers/models/auto/configuration_auto.py
@@ -1098,9 +1098,11 @@ class AutoConfig:
             try:
                 config_class = CONFIG_MAPPING[config_dict["model_type"]]
             except KeyError:
-                raise ValueError(f"The checkpoint you are trying to load has model type `{config_dict['model_type']}` "
-                               "but Transformers does not recognize this architecture. This could be because of an "
-                               "issue with the checkpoint, or because your version of Transformers is out of date.")
+                raise ValueError(
+                    f"The checkpoint you are trying to load has model type `{config_dict['model_type']}` "
+                    "but Transformers does not recognize this architecture. This could be because of an "
+                    "issue with the checkpoint, or because your version of Transformers is out of date."
+                )
             return config_class.from_dict(config_dict, **unused_kwargs)
         else:
             # Fallback: use pattern matching on the string.
