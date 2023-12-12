5b7d5bd2908acb9270c24fedef90b1385da568c1
fxmarty <9808326+fxmarty@users.noreply.github.com>
2023-12-12 16:33:46 +0100

    Fix SDPA correctness following torch==2.1.2 regression (#27973)
    * fix sdpa with non-contiguous inputs for gpt_bigcode
    
    * fix other archs
    
    * add currently comment
    
    * format

--- src/transformers/models/idefics/modeling_idefics.py
+++ src/transformers/models/idefics/modeling_idefics.py
@@ -688,6 +688,13 @@ class IdeficsAttention(nn.Module):
                     f"Attention mask should be of size {(bsz, 1, q_len, kv_seq_len)}, but is {attention_mask.size()}"
                 )
 
+        # SDPA with memory-efficient backend is currently (torch==2.1.2) bugged with non-contiguous inputs with custom attn_mask,
+        # Reference: https://github.com/pytorch/pytorch/issues/112577.
+        if query_states.device.type == "cuda" and attention_mask is not None:
+            query_states = query_states.contiguous()
+            key_states = key_states.contiguous()
+            value_states = value_states.contiguous()
+
         attn_output = nn.functional.scaled_dot_product_attention(
             query_states,
             key_states,
