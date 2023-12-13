f0dbb0723165268a4d26d58d1e6964887a1b703a
chi <iamchi@skiff.com>
2023-12-13 19:03:58 +0530

    solve check-code-quality error

--- src/transformers/models/llava/modeling_llava.py
+++ src/transformers/models/llava/modeling_llava.py
@@ -247,7 +247,7 @@ class LlavaForConditionalGeneration(LlavaPreTrainedModel):
     @property
     def output_embeddings(self):
         return self.language_model.get_output_embeddings()
-    
+
     @output_embeddings.setter
     def output_embeddings(self, new_embeddings):
         self.language_model.set_output_embeddings(new_embeddings)
