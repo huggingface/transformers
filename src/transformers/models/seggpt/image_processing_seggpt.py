c38de071c5e7f38db01e860c18fc7756a706f05e
Eduardo Pacheco <69953243+EduardoPach@users.noreply.github.com>
2023-12-13 00:08:17 -0300

    Update src/transformers/models/seggpt/image_processing_seggpt.py
    Co-authored-by: NielsRogge <48327001+NielsRogge@users.noreply.github.com>

--- src/transformers/models/seggpt/image_processing_seggpt.py
+++ src/transformers/models/seggpt/image_processing_seggpt.py
@@ -12,7 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Image processor class for ViT."""
+"""Image processor class for SegGPT."""
 
 from typing import Dict, List, Optional, Tuple, Union
 
