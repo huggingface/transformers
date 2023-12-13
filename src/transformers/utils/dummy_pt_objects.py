c7f076a00ee54f777b3d3322c91bc11489a47950
Younes Belkada <49240599+younesbelkada@users.noreply.github.com>
2023-12-13 10:42:24 +0100

    Adds VIP-llava to transformers (#27932)
    * v1
    
    * add-new-model-like
    
    * revert
    
    * fix forward and conversion script
    
    * revert
    
    * fix copies
    
    * fixup
    
    * fix
    
    * Update docs/source/en/index.md
    
    * Apply suggestions from code review
    
    * push
    
    * fix
    
    * fixes here and there
    
    * up
    
    * fixup and fix tests
    
    * Apply suggestions from code review
    
    * add docs
    
    * fixup
    
    * fixes
    
    * docstring
    
    * add docstring
    
    * fixup
    
    * docstring
    
    * fixup
    
    * nit
    
    * docs
    
    * more copies
    
    * fix copies
    
    * nit
    
    * update test

--- src/transformers/utils/dummy_pt_objects.py
+++ src/transformers/utils/dummy_pt_objects.py
@@ -8320,6 +8320,23 @@ class ViltPreTrainedModel(metaclass=DummyObject):
         requires_backends(self, ["torch"])
 
 
+VIPLLAVA_PRETRAINED_MODEL_ARCHIVE_LIST = None
+
+
+class VipLlavaForConditionalGeneration(metaclass=DummyObject):
+    _backends = ["torch"]
+
+    def __init__(self, *args, **kwargs):
+        requires_backends(self, ["torch"])
+
+
+class VipLlavaPreTrainedModel(metaclass=DummyObject):
+    _backends = ["torch"]
+
+    def __init__(self, *args, **kwargs):
+        requires_backends(self, ["torch"])
+
+
 class VisionEncoderDecoderModel(metaclass=DummyObject):
     _backends = ["torch"]
 
