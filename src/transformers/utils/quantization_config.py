94229b9e39b584bee2393a5036d4ad0a51089a2f
Marc Sun <57196510+SunMarc@users.noreply.github.com>
2023-12-13 12:19:16 -0500

    Update src/transformers/utils/quantization_config.py
    Co-authored-by: amyeroberts <22614925+amyeroberts@users.noreply.github.com>

--- src/transformers/utils/quantization_config.py
+++ src/transformers/utils/quantization_config.py
@@ -384,8 +384,9 @@ class GPTQConfig(QuantizationConfigMixin):
             List of list of module names to quantize in the specified block. This argument is useful to exclude certain linear modules from being quantized.
             The block to quantize can be specified by setting `block_name_to_quantize`. We will quantize each list sequentially. If not set, we will quantize all linear layers.
             Example: `modules_in_block_to_quantize =[["self_attn.k_proj", "self_attn.v_proj", "self_attn.q_proj"], ["self_attn.o_proj"]]`.
-            In this example, we will first quantize the q,k,v layers simultaneously since they are independent. Then, we will quantize `self_attn.o_proj` layer with the q,k,v layers quantized.
-            This way, we will get better results since it reflects the real input `self_attn.o_proj` will get when the model is quantized.
+            In this example, we will first quantize the q,k,v layers simultaneously since they are independent. 
+            Then, we will quantize `self_attn.o_proj` layer with the q,k,v layers quantized. This way, we will get
+            better results since it reflects the real input `self_attn.o_proj` will get when the model is quantized.
     """
 
     def __init__(
