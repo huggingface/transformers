73de5108e172112bc620cfc0ceebfd27730dba11
Younes Belkada <49240599+younesbelkada@users.noreply.github.com>
2023-12-14 12:19:45 +0100

    [`core` / `modeling`] Fix training bug with PEFT + GC (#28031)
    fix trainign bug

--- src/transformers/models/deprecated/open_llama/modeling_open_llama.py
+++ src/transformers/models/deprecated/open_llama/modeling_open_llama.py
@@ -578,6 +578,13 @@ class OpenLlamaModel(OpenLlamaPreTrainedModel):
         seq_length_with_past = seq_length
         past_key_values_length = 0
 
+        if self.gradient_checkpointing and self.training:
+            if use_cache:
+                logger.warning_once(
+                    "`use_cache=True` is incompatible with gradient checkpointing. Setting `use_cache=False`..."
+                )
+                use_cache = False
+
         if past_key_values is not None:
             past_key_values_length = past_key_values[0][0].shape[2]
             seq_length_with_past = seq_length_with_past + past_key_values_length
@@ -607,13 +614,6 @@ class OpenLlamaModel(OpenLlamaPreTrainedModel):
         )
 
         hidden_states = inputs_embeds
-
-        if self.gradient_checkpointing and self.training:
-            if use_cache:
-                logger.warning_once(
-                    "`use_cache=True` is incompatible with gradient checkpointing. Setting `use_cache=False`..."
-                )
-                use_cache = False
 
         # decoder layers
         all_hidden_states = () if output_hidden_states else None
