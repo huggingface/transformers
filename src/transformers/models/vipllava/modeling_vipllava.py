ad10440e9b4ef676f9e2c52547665e90f35b0bef
Younes Belkada <49240599+younesbelkada@users.noreply.github.com>
2023-12-18 13:46:30 +0100

    [`Llava` / `Vip-Llava`] Add SDPA into llava (#28107)
    add SDPA into llava

--- src/transformers/models/vipllava/modeling_vipllava.py
+++ src/transformers/models/vipllava/modeling_vipllava.py
@@ -162,6 +162,14 @@ class VipLlavaPreTrainedModel(PreTrainedModel):
             if module.padding_idx is not None:
                 module.weight.data[module.padding_idx].zero_()
 
+    @property
+    def _supports_sdpa(self):
+        """
+        Retrieve language_model's attribute to check whether the model supports
+        SDPA or not.
+        """
+        return self.language_model._supports_sdpa
+
 
 VIPLLAVA_INPUTS_DOCSTRING = r"""
     Args:
