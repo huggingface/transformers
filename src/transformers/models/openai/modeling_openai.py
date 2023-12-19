2823b47b421c9c6c697626dde32e498c04f4ab2d
Dean Wyatte <dwyatte@squareup.com>
2023-12-19 18:26:29 +0000

    use modulo instead

--- src/transformers/models/openai/modeling_openai.py
+++ src/transformers/models/openai/modeling_openai.py
@@ -814,11 +814,10 @@ class OpenAIGPTForSequenceClassification(OpenAIGPTPreTrainedModel):
             sequence_lengths = -1
         else:
             if input_ids is not None:
-                # if no pad token found, use input_ids.shape[-1] - 1 instead of reverse indexing for ONNX compatibility
+                # if no pad token found, use modulo instead of reverse indexing for ONNX compatibility
                 sequence_lengths = torch.eq(input_ids, self.config.pad_token_id).int().argmax(-1) - 1
-                sequence_lengths = torch.where(sequence_lengths >= 0, sequence_lengths, input_ids.shape[-1] - 1).to(
-                    logits.device
-                )
+                sequence_lengths = sequence_lengths % input_ids.shape[-1]
+                sequence_lengths = sequence_lengths.to(logits.device)
             else:
                 sequence_lengths = -1
                 logger.warning(
