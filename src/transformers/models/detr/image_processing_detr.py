1d7773594754457ed4a79cf6d98bcaabea5bff51
amyeroberts <22614925+amyeroberts@users.noreply.github.com>
2023-12-20 20:55:51 +0000

    Fix yolos resizing (#27663)
    * Fix yolos resizing
    
    * Update tests
    
    * Add a test

--- src/transformers/models/detr/image_processing_detr.py
+++ src/transformers/models/detr/image_processing_detr.py
@@ -82,6 +82,7 @@ logger = logging.get_logger(__name__)  # pylint: disable=invalid-name
 SUPPORTED_ANNOTATION_FORMATS = (AnnotationFormat.COCO_DETECTION, AnnotationFormat.COCO_PANOPTIC)
 
 
+# From the original repo: https://github.com/facebookresearch/detr/blob/3af9fa878e73b6894ce3596450a8d9b89d918ca9/datasets/transforms.py#L76
 def get_size_with_aspect_ratio(image_size, size, max_size=None) -> Tuple[int, int]:
     """
     Computes the output image size given the input image size and the desired output size.
