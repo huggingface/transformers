9792399cfabd5d30d1e0c424b11a84f352f12c34
Yoach Lacombe <yoach.lacombe@gmail.com>
2023-12-21 10:44:14 +0000

    remove function not used

--- src/transformers/models/wav2vec2_conformer/modeling_wav2vec2_conformer.py
+++ src/transformers/models/wav2vec2_conformer/modeling_wav2vec2_conformer.py
@@ -268,35 +268,6 @@ def _sample_negative_indices(
 
     return sampled_negative_indices
 
-
-# Copied from transformers.models.seamless_m4t_v2.modeling_seamless_m4t_v2._compute_new_attention_mask
-def _compute_new_attention_mask(hidden_states: torch.Tensor, seq_lens: torch.Tensor):
-    """
-    Computes an attention mask of the form `(batch, seq_len)` with an attention for each element in the batch that
-    stops at the corresponding element in `seq_lens`.
-
-    Args:
-        hidden_states (`torch.FloatTensor` of shape `(batch, seq_len, *)`):
-            The sequences to mask, where `*` is any number of sequence-specific dimensions including none.
-        seq_lens (`torch.Tensor` of shape `(batch)`:
-            Each element represents the length of the sequence at the same index in `hidden_states`
-
-    Returns:
-        `torch.FloatTensor`: The float attention mask of shape `(batch, seq_len)`
-    """
-    batch_size, mask_seq_len = hidden_states.shape[:2]
-
-    indices = torch.arange(mask_seq_len, device=seq_lens.device).expand(batch_size, -1)
-
-    bool_mask = indices >= seq_lens.unsqueeze(1).expand(-1, mask_seq_len)
-
-    mask = hidden_states.new_ones((batch_size, mask_seq_len))
-
-    mask = mask.masked_fill(bool_mask, 0)
-
-    return mask
-
-
 # Copied from transformers.models.wav2vec2.modeling_wav2vec2.Wav2Vec2NoLayerNormConvLayer with Wav2Vec2->Wav2Vec2Conformer
 class Wav2Vec2ConformerNoLayerNormConvLayer(nn.Module):
     def __init__(self, config, layer_id=0):
