d5a123a87f17fd732397eb70204829f324139ad8
Niels <niels.rogge1@gmail.com>
2023-12-21 10:46:32 +0100

    Skip test

--- tests/models/udop/test_modeling_udop.py
+++ tests/models/udop/test_modeling_udop.py
@@ -306,6 +306,10 @@ class UdopModelTest(ModelTesterMixin, PipelineTesterMixin, unittest.TestCase):
         config_and_inputs = self.model_tester.prepare_config_and_inputs()
         self.model_tester.create_and_check_model_fp16_forward(*config_and_inputs)
 
+    @unittest.skip("Gradient checkpointing is not supported by this model")
+    def test_training_gradient_checkpointing(self):
+        pass
+
     @unittest.skip(
         reason="This architecure seem to not compute gradients properly when using GC, check: https://github.com/huggingface/transformers/pull/27124"
     )
