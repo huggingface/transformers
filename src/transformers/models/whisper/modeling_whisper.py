b28a922d717cbfa18d1ff6c704ec273f149fbdad
Yoach Lacombe <yoach.lacombe@gmail.com>
2023-12-21 14:21:34 +0000

    write clearer comments

--- src/transformers/models/whisper/modeling_whisper.py
+++ src/transformers/models/whisper/modeling_whisper.py
@@ -2559,12 +2559,13 @@ class WhisperForConditionalGeneration(WhisperPreTrainedModel):
 
         if "beam_indices" in generate_outputs:
             # If beam search has been used, the output sequences may have been generated for more timesteps than their sequence_lengths
-            # Since the beam search strategy chooses the most probable sequences at the end of the search
-            # In that case, we make sure that the weights have the right output_length
+            # since the beam search strategy chooses the most probable sequences at the end of the search.
+            # In that case, the cross_attentions weights are too long and we have to make sure that they have the right output_length
             weight_length = (generate_outputs.beam_indices != -1).sum(-1).max()
             weights = weights[:, :, :weight_length]
 
             # If beam index is still -1, it means that the associated token id is EOS
+            # We need to replace the index with 0 since index_select gives an error if any of the indexes is -1.
             beam_indices = generate_outputs.beam_indices[:, :weight_length]
             beam_indices = beam_indices.masked_fill(beam_indices == -1, 0)
 
