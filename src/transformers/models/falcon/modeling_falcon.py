6dfc2e9705c7f319ee9ac8d0ea62b2b1dc06f500
Sourab Mangrulkar <13534540+pacman100@users.noreply.github.com>
2023-12-22 17:17:04 +0530

    fix FA2 when using quantization

--- src/transformers/models/falcon/modeling_falcon.py
+++ src/transformers/models/falcon/modeling_falcon.py
@@ -617,11 +617,11 @@ class FalconFlashAttention2(FalconAttention):
         # cast them back in float16 just to be sure everything works as expected.
         input_dtype = query_layer.dtype
         if input_dtype == torch.float32:
-            # Handle the case where the model is quantized
-            if hasattr(self.config, "_pre_quantization_dtype"):
-                target_dtype = self.config._pre_quantization_dtype
-            elif torch.is_autocast_enabled():
+            if torch.is_autocast_enabled():
                 target_dtype = torch.get_autocast_gpu_dtype()
+            # Handle the case where the model is quantized
+            elif hasattr(self.config, "_pre_quantization_dtype"):
+                target_dtype = self.config._pre_quantization_dtype
             else:
                 target_dtype = self.query_key_value.weight.dtype
 
