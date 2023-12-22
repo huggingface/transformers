64e0cd6be01afa0d91154301aafed51dc34ebce7
Niels Rogge <niels.rogge1@gmail.com>
2023-12-22 09:40:56 +0100

    Remove print statements

--- src/transformers/generation/utils.py
+++ src/transformers/generation/utils.py
@@ -3138,7 +3138,6 @@ class GenerationMixin:
         this_peer_finished = False  # used by synced_gpus only
 
         decoder_prompt_len = input_ids.shape[-1]  # record the prompt length of decoder
-        step = 0
         while True:
             if synced_gpus:
                 # Under synced_gpus the `forward` call must continue until all gpus complete their sequence.
@@ -3158,7 +3157,6 @@ class GenerationMixin:
                 output_attentions=output_attentions,
                 output_hidden_states=output_hidden_states,
             )
-            step += 1
 
             if synced_gpus and this_peer_finished:
                 cur_len = cur_len + 1
