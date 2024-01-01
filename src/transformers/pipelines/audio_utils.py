736d5ae2c83bbecb9b01834c8fbeb3ebde76ae1d
Teapack1 <ondrej.major@gmail.com>
2024-01-01 22:17:34 +0100

    Fixed getting mic on Windows systems by adding _get_microphone_name() function.

--- src/transformers/pipelines/audio_utils.py
+++ src/transformers/pipelines/audio_utils.py
@@ -232,13 +232,15 @@ def _get_microphone_name():
     """
     Retrieve the microphone name in Windows .
     """
-    command = ["ffmpeg", "-list_devices", "true", "-f", "dshow", "-i", "dummy"]
+    command = ["ffmpeg", "-list_devices", "true", "-f", "dshow", "-i", ""]
 
     try:
-        ffmpeg_devices = subprocess.run(command, text=True, stderr=subprocess.PIPE)
+        ffmpeg_devices = subprocess.run(command, text=True, stderr=subprocess.PIPE, encoding="utf-8")
         microphone_lines = [line for line in ffmpeg_devices.stderr.splitlines() if "(audio)" in line]
+
         if microphone_lines:
             microphone_name = microphone_lines[0].split('"')[1]
+            print(f"Using microphone: {microphone_name}")
             return f"audio={microphone_name}"
     except FileNotFoundError:
         print("ffmpeg was not found. Please install it or make sure it is in your system PATH.")
