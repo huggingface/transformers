700124f9288cae33b0bf864d5d0392d7499694d6
Marco Carosi <ercaronte@gmail.com>
2024-01-01 15:52:14 +0100

    Update modeling_whisper.py sorting the import block
    Sorting the utils import block

--- src/transformers/models/whisper/modeling_whisper.py
+++ src/transformers/models/whisper/modeling_whisper.py
@@ -43,9 +43,9 @@ from ...utils import (
     add_start_docstrings_to_model_forward,
     is_flash_attn_2_available,
     is_flash_attn_greater_or_equal_2_10,
+    is_torch_mps_available,
     logging,
     replace_return_docstrings,
-    is_torch_mps_available,
 )
 from .configuration_whisper import WhisperConfig
 from .tokenization_whisper import TASK_IDS, TO_LANGUAGE_CODE
