fix M4T FE error when no attention mask

src/transformers/models/seamless_m4t/feature_extraction_seamless_m4t.py
@@ -271,14 +271,16 @@ class SeamlessM4TFeatureExtractor(SequenceFeatureExtractor):
         remainder = num_frames % self.stride
         if remainder != 0:
             input_features = input_features[:, :num_frames, :]
-            attention_mask = attention_mask[:, :num_frames]
+            if return_attention_mask:
+                attention_mask = attention_mask[:, :num_frames]
 
         input_features = np.reshape(
             input_features, (batch_size, num_frames // self.stride, num_channels * self.stride)
         )
 
-        indices = np.arange(0, num_frames)
-        attention_mask = attention_mask[:, indices % self.stride == 1]
+        if return_attention_mask:
+            indices = np.arange(0, num_frames)
+            attention_mask = attention_mask[:, indices % self.stride == 1]
 
         padded_inputs["input_features"] = input_features
         padded_inputs["attention_mask"] = attention_mask