444ed0d9c9616d1a2c7603e4cb0715ceaf9effdc
Susnato Dhar <susnatodhar10@gmail.com>
2024-01-04 15:46:40 +0530

    fix fa2 autocasting when using quantization

--- src/transformers/models/distilbert/modeling_distilbert.py
+++ src/transformers/models/distilbert/modeling_distilbert.py
@@ -322,11 +322,13 @@ class DistilBertFlashAttention2(MultiHeadSelfAttention):
         # in fp32. (LlamaRMSNorm handles it correctly)
 
         if query_states.dtype == torch.float32:
+            if torch.is_autocast_enabled():
+                target_dtype = torch.get_autocast_gpu_dtype()
             # Handle the case where the model is quantized
-            if hasattr(self.config, "_pre_quantization_dtype"):
+            elif hasattr(self.config, "_pre_quantization_dtype"):
                 target_dtype = self.config._pre_quantization_dtype
             else:
-                target_dtype = self.q_lin.weight.dtype
+                target_dtype = self.q_proj.weight.dtype
 
             logger.warning_once(
                 f"The input hidden states seems to be silently casted in float32, this might be related to"
