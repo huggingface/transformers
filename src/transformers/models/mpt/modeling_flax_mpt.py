8bc5b9e8dc434883610953fe6661acc540e861b2
Anshuman Mishra <anshuman@flip.space>
2024-01-04 17:09:57 +0530

    styles

--- src/transformers/models/mpt/modeling_flax_mpt.py
+++ src/transformers/models/mpt/modeling_flax_mpt.py
@@ -4,16 +4,21 @@ from typing import Optional, Tuple, Union
 
 import flax.linen as nn
 import jax
-from jax import lax
 import jax.numpy as jnp
-from transformers.modeling_flax_outputs import FlaxBaseModelOutputWithPastAndCrossAttentions, FlaxCausalLMOutputWithCrossAttentions, FlaxQuestionAnsweringModelOutput, FlaxTokenClassifierOutput
+from jax import lax
 
+from transformers.modeling_flax_outputs import (
+    FlaxBaseModelOutputWithPastAndCrossAttentions,
+    FlaxCausalLMOutputWithCrossAttentions,
+    FlaxQuestionAnsweringModelOutput,
+    FlaxTokenClassifierOutput,
+)
 from transformers.modeling_flax_utils import FlaxPreTrainedModel
 from transformers.models.mpt.configuration_mpt import MptConfig
-from transformers.utils.doc import add_start_docstrings
 
 
 logger = logging.getLogger(__name__)
+
 
 class AttentionMaskConverter:
     def __init__(self, is_causal, sliding_window=None):
@@ -34,6 +39,7 @@ class AttentionMaskConverter:
         if self.sliding_window is not None:
             mask_4d = jnp.maximum(mask_4d, self.sliding_window_mask(query_length, key_value_length, dtype))
         return mask_4d
+
 
 def _prepare_4d_causal_attention_mask(
     attention_mask,
@@ -71,9 +77,7 @@ def _prepare_4d_causal_attention_mask(
     elif attention_mask is not None and len(attention_mask.shape) == 4:
         expected_shape = (input_shape[0], 1, input_shape[1], key_value_length)
         if attention_mask.shape != expected_shape:
-            raise ValueError(
-                f"Incorrect 4D attention_mask shape: {attention_mask.shape}; expected: {expected_shape}."
-            )
+            raise ValueError(f"Incorrect 4D attention_mask shape: {attention_mask.shape}; expected: {expected_shape}.")
         else:
             # if the 4D mask has the correct shape - invert it and fill with negative infinity
             inverted_mask = 1.0 - attention_mask
@@ -84,6 +88,7 @@ def _prepare_4d_causal_attention_mask(
         )
 
     return attention_mask
+
 
 class FlaxMptAttention(nn.module):
     config: MptConfig
@@ -406,7 +411,8 @@ class FlaxMptModel(FlaxMptPreTrainedModel):
             hidden_states=all_hidden_states,
             attentions=all_self_attentions,
         )
-    
+
+
 class FlaxMptForCausalLMModule(nn.Module):
     config: MptConfig
     dtype: jnp.dtype = jnp.float32
@@ -455,6 +461,7 @@ class FlaxMptForCausalLMModule(nn.Module):
             attentions=outputs.attentions,
         )
 
+
 class FlaxMptForCausalLM(FlaxMptPreTrainedModel):
     module_class = FlaxMptForCausalLMModule
 
@@ -485,85 +492,6 @@ class FlaxMptForCausalLM(FlaxMptPreTrainedModel):
         model_kwargs["position_ids"] = model_kwargs["position_ids"][:, -1:] + 1
         return model_kwargs
 
-class FlaxMptForSequenceClassificationModule(nn.Module):
-    config: MptConfig
-    dtype: jnp.dtype = jnp.float32
-
-    def setup(self):
-        self.transformer = FlaxMptModel(self.config, dtype=self.dtype)
-        self.score = nn.Dense(
-            self.config.num_labels,
-            use_bias=False,
-            dtype=self.dtype,
-            kernel_init=jax.nn.initializers.normal(stddev=self.config.initializer_range),
-        )
-
-    def __call__(
-        self,
-        input_ids,
-        past_key_values=None,
-        attention_mask=None,
-        inputs_embeds=None,
-        labels=None,
-        use_cache=None,
-        output_attentions=None,
-        output_hidden_states=None,
-        return_dict=None,
-    ):
-        outputs = self.transformer(
-            input_ids,
-            past_key_values=past_key_values,
-            attention_mask=attention_mask,
-            inputs_embeds=inputs_embeds,
-            use_cache=use_cache,
-            output_attentions=output_attentions,
-            output_hidden_states=output_hidden_states,
-            return_dict=return_dict,
-        )
-
-        hidden_states = outputs[0]
-        logits = self.score(hidden_states)
-
-        if not return_dict:
-            return (logits,) + outputs[1:]
-
-        return FlaxSequenceClassifierOutputWithPast(
-            logits=logits,
-            past_key_values=outputs.past_key_values,
-            hidden_states=outputs.hidden_states,
-            attentions=outputs.attentions,
-        )
-
-
-class FlaxMptForSequenceClassification(FlaxMptPreTrainedModel):
-    module_class = FlaxMptForSequenceClassificationModule
-
-    def prepare_inputs_for_generation(self, input_ids, max_length, attention_mask: Optional[jnp.Array] = None):
-        # initializing the cache
-        batch_size, seq_length = input_ids.shape
-
-        past_key_values = self.init_cache(batch_size, max_length)
-
-        # Note that usually one would have to put 0's in the attention_mask for x > input_ids.shape[-1] and x < cache_length.
-        # But since Mpt uses a causal mask, those positions are masked anyways.
-        # Thus we can create a single static attention_mask here, which is more efficient for compilation
-        extended_attention_mask = jnp.ones((batch_size, max_length), dtype="i4")
-        if attention_mask is not None:
-            position_ids = attention_mask.cumsum(axis=-1) - 1
-            extended_attention_mask = lax.dynamic_update_slice(extended_attention_mask, attention_mask, (0, 0))
-        else:
-            position_ids = jnp.broadcast_to(jnp.arange(seq_length, dtype="i4")[None, :], (batch_size, seq_length))
-
-        return {
-            "past_key_values": past_key_values,
-            "attention_mask": extended_attention_mask,
-            "position_ids": position_ids,
-        }
-
-    def update_inputs_for_generation(self, model_outputs, model_kwargs):
-        model_kwargs["past_key_values"] = model_outputs.past_key_values
-        model_kwargs["position_ids"] = model_kwargs["position_ids"][:, -1:] + 1
-        return model_kwargs
 
 class FlaxMptForTokenClassificationModule(nn.Module):
     config: MptConfig
@@ -620,7 +548,10 @@ class FlaxMptForTokenClassificationModule(nn.Module):
             labels = labels.astype(logits.dtype)
             batch_size, seq_length = labels.shape
             loss_fct = jax.nn.softmax_cross_entropy_with_logits
-            loss = loss_fct(logits.reshape((batch_size * seq_length, self.config.num_labels)), labels.reshape((batch_size * seq_length,)))
+            loss = loss_fct(
+                logits.reshape((batch_size * seq_length, self.config.num_labels)),
+                labels.reshape((batch_size * seq_length,)),
+            )
 
         if not return_dict:
             return (logits,) + outputs[2:]
@@ -648,108 +579,6 @@ class FlaxMptForTokenClassification(FlaxMptPreTrainedModel):
         extended_attention_mask = jnp.ones((batch_size, max_length), dtype="i4")
         if attention_mask is not None:
             position_ids = attention_mask.cumsum(axis=-1) - 1
-            extended_attention_mask = lax.dynamic_update_slice(extended_attention_mask, attention_mask, (0, 0))
-        else:
-            position_ids = jnp.broadcast_to(jnp.arange(seq_length, dtype="i4")[None, :], (batch_size, seq_length))
-
-        return {
-            "past_key_values": past_key_values,
-            "attention_mask": extended_attention_mask,
-            "position_ids": position_ids,
-        }
-
-    def update_inputs_for_generation(self, model_outputs, model_kwargs):
-        model_kwargs["past_key_values"] = model_outputs.past_key_values
-        model_kwargs["position_ids"] = model_kwargs["position_ids"][:, -1:] + 1
-        return model_kwargs
-
-class FlaxMptForQuestionAnsweringModule(nn.Module):
-    config: MptConfig
-    dtype: jnp.dtype = jnp.float32
-
-    def setup(self):
-        self.transformer = FlaxMptModel(self.config, dtype=self.dtype)
-        self.qa_outputs = nn.Dense(
-            2,
-            use_bias=True,
-            dtype=self.dtype,
-            kernel_init=jax.nn.initializers.xavier_uniform(),
-        )
-
-    def __call__(
-        self,
-        input_ids=None,
-        attention_mask=None,
-        inputs_embeds=None,
-        start_positions=None,
-        end_positions=None,
-        output_attentions=None,
-        output_hidden_states=None,
-        return_dict=None,
-    ):
-        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
-
-        outputs = self.transformer(
-            input_ids,
-            attention_mask=attention_mask,
-            inputs_embeds=inputs_embeds,
-            output_attentions=output_attentions,
-            output_hidden_states=output_hidden_states,
-            return_dict=return_dict,
-        )
-
-        sequence_output = outputs[0]
-
-        logits = self.qa_outputs(sequence_output)
-        start_logits, end_logits = jnp.split(logits, indices_or_sections=2, axis=-1)
-        start_logits = start_logits.squeeze(-1).contiguous()
-        end_logits = end_logits.squeeze(-1).contiguous()
-
-        total_loss = None
-        if start_positions is not None and end_positions is not None:
-            # If we are on multi-GPU, add a dimension
-            if start_positions.ndim > 1:
-                start_positions = start_positions.squeeze(-1)
-            if end_positions.ndim > 1:
-                end_positions = end_positions.squeeze(-1)
-            # Sometimes the start/end positions are outside our model inputs, we ignore these terms
-            ignored_index = start_logits.shape[1]
-            start_positions = jnp.clip(start_positions, 0, ignored_index)
-            end_positions = jnp.clip(end_positions, 0, ignored_index)
-
-            loss_fct = jax.nn.softmax_cross_entropy_with_logits
-            start_loss = jnp.mean(loss_fct(start_logits, start_positions))
-            end_loss = jnp.mean(loss_fct(end_logits, end_positions))
-            total_loss = (start_loss + end_loss) / 2.0
-
-        if not return_dict:
-            output = (start_logits, end_logits) + outputs[2:]
-            return ((total_loss,) + output) if total_loss is not None else output
-
-        return FlaxQuestionAnsweringModelOutput(
-            loss=total_loss,
-            start_logits=start_logits,
-            end_logits=end_logits,
-            hidden_states=outputs.hidden_states,
-            attentions=outputs.attentions,
-        )
-
-
-class FlaxMptForQuestionAnswering(FlaxMptPreTrainedModel):
-    module_class = FlaxMptForQuestionAnsweringModule
-
-    def prepare_inputs_for_generation(self, input_ids, max_length, attention_mask: Optional[jnp.Array] = None):
-        # Initializing the cache
-        batch_size, seq_length = input_ids.shape
-
-        past_key_values = self.init_cache(batch_size, max_length)
-
-        # Note that usually one would have to put 0's in the attention_mask for x > input_ids.shape[-1] and x < cache_length.
-        # But since Mpt uses a causal mask, those positions are masked anyways.
-        # Thus we can create a single static attention_mask here, which is more efficient for compilation
-        extended_attention_mask = jnp.ones((batch_size, max_length), dtype="i4")
-        if attention_mask is not None:
-            position_ids = jnp.cumsum(attention_mask, axis=-1) - 1
             extended_attention_mask = lax.dynamic_update_slice(extended_attention_mask, attention_mask, (0, 0))
         else:
             position_ids = jnp.broadcast_to(jnp.arange(seq_length, dtype="i4")[None, :], (batch_size, seq_length))
