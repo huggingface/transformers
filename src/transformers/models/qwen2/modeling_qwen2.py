82992662811183ace40160ec632546c5184400e3
JustinLin610 <justinlin930319@hotmail.com>
2024-01-11 00:21:49 +0800

    update team name

--- src/transformers/models/qwen2/modeling_qwen2.py
+++ src/transformers/models/qwen2/modeling_qwen2.py
@@ -1,5 +1,5 @@
 # coding=utf-8
-# Copyright 2024 The Qwen Team and the HuggingFace Inc. team. All rights reserved.
+# Copyright 2024 The Qwen team, Alibaba Group and the HuggingFace Inc. team. All rights reserved.
 #
 # This code is based on EleutherAI's GPT-NeoX library and the GPT-NeoX
 # and OPT implementations in this library. It has been modified from its
