24543b4d357eb3ac261c7b5cf2581bc62d00ea49
Matt <rocketknight1@gmail.com>
2024-01-11 18:52:50 +0000

    Fix TF Regnet docstring

--- src/transformers/models/regnet/modeling_tf_regnet.py
+++ src/transformers/models/regnet/modeling_tf_regnet.py
@@ -460,7 +460,7 @@ REGNET_START_DOCSTRING = r"""
     [tf.keras.layers.Layer](https://www.tensorflow.org/api_docs/python/tf/keras/layers/Layer) sub-class. Use it as a
     regular Tensorflow Module and refer to the Tensorflow documentation for all matter related to general usage and
     behavior.
-    
+
     Parameters:
         config ([`RegNetConfig`]): Model configuration class with all the parameters of the model.
             Initializing with a config file does not load the weights associated with the model, only the
