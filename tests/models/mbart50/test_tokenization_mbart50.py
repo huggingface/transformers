f38d8874a7f3866b4fd3fda4cef0ad9372edf41b
hi-sushanta <iamchi@skiff.com>
2024-01-14 06:21:39 +0530

    My mistake to not run ruff command to chack if it's perfectly solve issue, And now it's solved

--- tests/models/mbart50/test_tokenization_mbart50.py
+++ tests/models/mbart50/test_tokenization_mbart50.py
@@ -307,8 +307,8 @@ class MBart50OneToManyIntegrationTest(unittest.TestCase):
 
     def test_inconsistent_decoding(self):
         text = "<s>[special_token] normal text"
-        fast_tokens = self.tokenizer(text,use_fast=True)["input_ids"]
-        slow_tokens = self.tokenizer(text,use_fast=False)["input_ids"]
+        fast_tokens = self.tokenizer(text, use_fast=True)["input_ids"]
+        slow_tokens = self.tokenizer(text, use_fast=False)["input_ids"]
         decoded_fast = self.tokenizer.decode(fast_tokens)
         decoded_slow = self.tokenizer.decode(slow_tokens)
         assert decoded_fast == decoded_slow == "en_XX<s> [special_token] normal text</s>"
