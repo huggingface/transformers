af3a2c6448545af8588ec8461a6e6689e8241557
ncoop57 <nathan.cooper@stability.ai>
2024-01-14 19:42:36 +0000

    Refactor Fuyu model code

--- src/transformers/models/fuyu/modeling_fuyu.py
+++ src/transformers/models/fuyu/modeling_fuyu.py
@@ -22,7 +22,12 @@ from torch import nn
 from ...modeling_outputs import CausalLMOutputWithPast
 from ...modeling_utils import PreTrainedModel
 from ...models.auto.modeling_auto import AutoModelForCausalLM
-from ...utils import add_start_docstrings, add_start_docstrings_to_model_forward, logging, replace_return_docstrings
+from ...utils import (
+    add_start_docstrings,
+    add_start_docstrings_to_model_forward,
+    logging,
+    replace_return_docstrings,
+)
 from .configuration_fuyu import FuyuConfig
 
 
@@ -126,10 +131,6 @@ FUYU_INPUTS_DOCSTRING = r"""
             Optionally, instead of passing `input_ids` you can choose to directly pass an embedded representation. This
             is useful if you want more control over how to convert `input_ids` indices into associated vectors than the
             model's internal embedding lookup matrix.
-        labels (`torch.LongTensor` of shape `(batch_size, sequence_length)`, *optional*):
-                Labels for computing the masked language modeling loss. Indices should either be in `[0, ...,
-                config.vocab_size]` or -100 (see `input_ids` docstring). Tokens with indices set to `-100` are ignored
-                (masked), the loss is only computed for the tokens with labels in `[0, ..., config.vocab_size]`.
         use_cache (`bool`, *optional*):
             If set to `True`, `past_key_values` key value states are returned and can be used to speed up decoding (see
             `past_key_values`).
@@ -156,7 +157,8 @@ class FuyuForCausalLM(FuyuPreTrainedModel):
         self.language_model = AutoModelForCausalLM.from_config(config.text_config)
 
         self.vision_embed_tokens = nn.Linear(
-            config.patch_size * config.patch_size * config.num_channels, config.hidden_size
+            config.patch_size * config.patch_size * config.num_channels,
+            config.hidden_size,
         )
 
         self.gradient_checkpointing = False
@@ -198,7 +200,9 @@ class FuyuForCausalLM(FuyuPreTrainedModel):
         for batch_idx in range(word_embeddings.shape[0]):
             # First, find the positions of all the non-negative values in image_patch_input_indices, those are the
             # positions in word_embeddings that we want to replace with content from continuous_embeddings.
-            dst_indices = torch.nonzero(image_patch_input_indices[batch_idx] >= 0, as_tuple=True)[0]
+            dst_indices = torch.nonzero(
+                image_patch_input_indices[batch_idx] >= 0, as_tuple=True
+            )[0]
             # Next look up those indices in image_patch_input_indices to find the indices in continuous_embeddings that we
             # want to use to replace the values in word_embeddings.
             src_indices = image_patch_input_indices[batch_idx][dst_indices]
@@ -208,11 +212,15 @@ class FuyuForCausalLM(FuyuPreTrainedModel):
                     f"Number of continuous embeddings {continuous_embeddings[batch_idx].shape=} does not match "
                     f"number of continuous token ids {src_indices.shape=} in batch element {batch_idx}."
                 )
-            output_embeddings[batch_idx, dst_indices] = continuous_embeddings[batch_idx][src_indices]
+            output_embeddings[batch_idx, dst_indices] = continuous_embeddings[
+                batch_idx
+            ][src_indices]
         return output_embeddings
 
     @add_start_docstrings_to_model_forward(FUYU_INPUTS_DOCSTRING)
-    @replace_return_docstrings(output_type=CausalLMOutputWithPast, config_class=_CONFIG_FOR_DOC)
+    @replace_return_docstrings(
+        output_type=CausalLMOutputWithPast, config_class=_CONFIG_FOR_DOC
+    )
     def forward(
         self,
         input_ids: torch.LongTensor = None,
@@ -222,7 +230,6 @@ class FuyuForCausalLM(FuyuPreTrainedModel):
         position_ids: Optional[torch.LongTensor] = None,
         past_key_values: Optional[List[torch.FloatTensor]] = None,
         inputs_embeds: Optional[torch.FloatTensor] = None,
-        labels: Optional[torch.LongTensor] = None,
         use_cache: Optional[bool] = None,
         labels: Optional[torch.Tensor] = None,
         output_attentions: Optional[bool] = None,
@@ -260,16 +267,26 @@ class FuyuForCausalLM(FuyuPreTrainedModel):
         'A bus parked on the side of a road.'
         ```"""
 
-        output_attentions = output_attentions if output_attentions is not None else self.config.output_attentions
+        output_attentions = (
+            output_attentions
+            if output_attentions is not None
+            else self.config.output_attentions
+        )
         output_hidden_states = (
-            output_hidden_states if output_hidden_states is not None else self.config.output_hidden_states
+            output_hidden_states
+            if output_hidden_states is not None
+            else self.config.output_hidden_states
         )
         use_cache = use_cache if use_cache is not None else self.config.use_cache
 
-        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+        return_dict = (
+            return_dict if return_dict is not None else self.config.use_return_dict
+        )
 
         if input_ids is not None and inputs_embeds is not None:
-            raise ValueError("You cannot specify both input_ids and inputs_embeds at the same time")
+            raise ValueError(
+                "You cannot specify both input_ids and inputs_embeds at the same time"
+            )
         elif input_ids is not None:
             batch_size, seq_length = input_ids.shape
         elif inputs_embeds is not None:
@@ -287,7 +304,10 @@ class FuyuForCausalLM(FuyuPreTrainedModel):
         if position_ids is None:
             device = input_ids.device if input_ids is not None else inputs_embeds.device
             position_ids = torch.arange(
-                past_key_values_length, seq_length + past_key_values_length, dtype=torch.long, device=device
+                past_key_values_length,
+                seq_length + past_key_values_length,
+                dtype=torch.long,
+                device=device,
             )
             position_ids = position_ids.unsqueeze(0)
 
@@ -295,7 +315,9 @@ class FuyuForCausalLM(FuyuPreTrainedModel):
             inputs_embeds = self.language_model.get_input_embeddings()(input_ids)
             if image_patches is not None and past_key_values is None:
                 patch_embeddings = [
-                    self.vision_embed_tokens(patch.to(self.vision_embed_tokens.weight.dtype)).squeeze(0)
+                    self.vision_embed_tokens(
+                        patch.to(self.vision_embed_tokens.weight.dtype)
+                    ).squeeze(0)
                     for patch in image_patches
                 ]
                 inputs_embeds = self.gather_continuous_embeddings(
@@ -306,7 +328,6 @@ class FuyuForCausalLM(FuyuPreTrainedModel):
 
         outputs = self.language_model(
             inputs_embeds=inputs_embeds,
-            labels=labels,
             attention_mask=attention_mask,
             position_ids=position_ids,
             past_key_values=past_key_values,
@@ -355,7 +376,9 @@ class FuyuForCausalLM(FuyuPreTrainedModel):
                 "past_key_values": past_key_values,
                 "use_cache": kwargs.get("use_cache"),
                 "attention_mask": attention_mask,
-                "image_patches_indices": image_patches_indices if past_key_values is None else None,
+                "image_patches_indices": image_patches_indices
+                if past_key_values is None
+                else None,
                 "image_patches": image_patches if past_key_values is None else None,
             }
         )
