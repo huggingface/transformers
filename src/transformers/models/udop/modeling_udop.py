c10bcd91c7d66dee0cb4998addebe61b7ea3d0b7
NielsRogge <48327001+NielsRogge@users.noreply.github.com>
2024-01-15 17:27:45 +0100

    Apply suggestions from code review
    Co-authored-by: Arthur <48595927+ArthurZucker@users.noreply.github.com>

--- src/transformers/models/udop/modeling_udop.py
+++ src/transformers/models/udop/modeling_udop.py
@@ -1,5 +1,5 @@
 # coding=utf-8
-# Copyright 2023 Microsoft Research and HuggingFace Inc. team.
+# Copyright 2024 Microsoft Research and HuggingFace Inc. team.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
