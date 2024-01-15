21271b229205dcb965bab5bbb96e81300150ca82
Félix Marty <9808326+fxmarty@users.noreply.github.com>
2024-01-15 11:28:22 +0100

    simplification

--- src/transformers/configuration_utils.py
+++ src/transformers/configuration_utils.py
@@ -901,9 +901,6 @@ class PretrainedConfig(PushToHubMixin):
 
         if hasattr(self, "model_type"):
             output["model_type"] = self.model_type
-        elif hasattr(self.__class__, "model_type"):
-            output["model_type"] = self.__class__.model_type
-
         if "_auto_class" in output:
             del output["_auto_class"]
         if "_commit_hash" in output:
