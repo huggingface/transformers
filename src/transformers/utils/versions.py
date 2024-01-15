f87546eead0ce97157a86481b72c0e55125282b1
Timothy Cronin <40186632+4imothy@users.noreply.github.com>
2024-01-15 13:36:40 -0500

    improve dev setup comments and hints (#28495)
    * improve dev setup comments and hints
    
    * fix tests for new dev setup hints

--- src/transformers/utils/versions.py
+++ src/transformers/utils/versions.py
@@ -113,5 +113,5 @@ def require_version(requirement: str, hint: Optional[str] = None) -> None:
 
 def require_version_core(requirement):
     """require_version wrapper which emits a core-specific hint on failure"""
-    hint = "Try: pip install transformers -U or pip install -e '.[dev]' if you're working with git main"
+    hint = "Try: `pip install transformers -U` or `pip install -e '.[dev]'` if you're working with git main"
     return require_version(requirement, hint)
