1cded97ace1a85af8c39a64cee368e70c656ec6c
Arthur <arthur.zucker@gmail.com>
2024-01-16 09:22:33 +0100

    Add copied from and fix the `convert_tokens_to_string` to match the fast decoding scheme

--- src/transformers/models/speecht5/tokenization_speecht5.py
+++ src/transformers/models/speecht5/tokenization_speecht5.py
@@ -177,17 +177,23 @@ class SpeechT5Tokenizer(PreTrainedTokenizer):
         token = self.sp_model.IdToPiece(index)
         return token
 
+    # Copied from transformers.models.albert.tokenization_albert.AlbertTokenizer.convert_tokens_to_string
     def convert_tokens_to_string(self, tokens):
         """Converts a sequence of tokens (string) in a single string."""
         current_sub_tokens = []
         out_string = ""
+        prev_is_special = False
         for token in tokens:
             # make sure that special tokens are not decoded using sentencepiece model
             if token in self.all_special_tokens:
+                if not prev_is_special:
+                    out_string += " "
                 out_string += self.sp_model.decode(current_sub_tokens) + token
+                prev_is_special = True
                 current_sub_tokens = []
             else:
                 current_sub_tokens.append(token)
+                prev_is_special = False
         out_string += self.sp_model.decode(current_sub_tokens)
         return out_string.strip()
 
