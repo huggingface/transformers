f74b89ccf81289f578192b6be30ed08d5a90ac6e
Arthur Zucker <arthur.zucker@gmail.com>
2024-01-17 15:53:58 +0100

    nits

--- tests/models/nllb/test_tokenization_nllb.py
+++ tests/models/nllb/test_tokenization_nllb.py
@@ -364,6 +364,13 @@ class NllbDistilledIntegrationTest(unittest.TestCase):
     def test_mask_token(self):
         self.assertListEqual(self.tokenizer.convert_tokens_to_ids(["<mask>", "ar_AR"]), [256203, 3])
 
+    def test_special_tokens_unaffacted_by_save_load(self):
+        tmpdirname = tempfile.mkdtemp()
+        original_special_tokens = self.tokenizer.fairseq_tokens_to_ids
+        self.tokenizer.save_pretrained(tmpdirname)
+        new_tok = NllbTokenizer.from_pretrained(tmpdirname)
+        self.assertDictEqual(new_tok.fairseq_tokens_to_ids, original_special_tokens)
+
     @require_torch
     def test_enro_tokenizer_prepare_batch(self):
         batch = self.tokenizer(
