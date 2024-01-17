71609062b4ca80e92f6606e02fb58de5978699fc
Yoach Lacombe <yoach.lacombe@gmail.com>
2024-01-17 13:41:17 +0000

    fix typo

--- tests/models/wav2vec2_bert/test_processor_wav2vec2_bert.py
+++ tests/models/wav2vec2_bert/test_processor_wav2vec2_bert.py
@@ -1,7 +1,7 @@
 # Copyright 2024 The HuggingFace Team. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file excenp in compliance with the License.
+# you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
 #     http://www.apache.org/licenses/LICENSE-2.0
