de2f27adf7d5eb3f687a7ec478103f17a342ed1b
Biswajit <biswajit2902@gmail.com>
2024-01-17 11:49:13 +0530

    added option for inital_prompt for whisper API

--- src/transformers/pipelines/automatic_speech_recognition.py
+++ src/transformers/pipelines/automatic_speech_recognition.py
@@ -574,18 +574,18 @@ class AutomaticSpeechRecognitionPipeline(ChunkPipeline):
 
             if self.type == "seq2seq_whisper" and inputs.shape[-1] > self.feature_extractor.nb_max_frames:
                 generate_kwargs["input_features"] = inputs
-                if self.processor:
-                    # Added initial prompt for whisper
-                    if "initial_prompt" in generate_kwargs:
-                        initial_prompt = generate_kwargs.pop("initial_prompt")
-                        generate_kwargs["prompt_ids"] = self.processor.get_prompt_ids(initial_prompt)
-                else:
-                    if "initial_prompt" in generate_kwargs:
-                        initial_prompt = generate_kwargs.pop("initial_prompt")
-                    RuntimeWarning("No defined processor for Whisper Prompting. `initial_ptompt` will be ignored.")
             else:
                 generate_kwargs["encoder_outputs"] = encoder(inputs, attention_mask=attention_mask)
 
+            if self.processor:
+                # Added initial prompt for whisper
+                if "initial_prompt" in generate_kwargs:
+                    initial_prompt = generate_kwargs.pop("initial_prompt")
+                    generate_kwargs["prompt_ids"] = self.processor.get_prompt_ids(initial_prompt)
+            else:
+                if "initial_prompt" in generate_kwargs:
+                    initial_prompt = generate_kwargs.pop("initial_prompt")
+                RuntimeWarning("No defined processor for Whisper Prompting. `initial_ptompt` will be ignored.")
             print(generate_kwargs)
             tokens = self.model.generate(
                 attention_mask=attention_mask,
