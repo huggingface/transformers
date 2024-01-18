02fb40812ba757e2c7224bd542be06b8f1bea64f
Alexander Kwako <31602350+akwako@users.noreply.github.com>
2024-01-17 18:27:16 -0800

    Make style correction
    Co-authored-by: amyeroberts <22614925+amyeroberts@users.noreply.github.com>

--- src/transformers/trainer.py
+++ src/transformers/trainer.py
@@ -1614,12 +1614,11 @@ class Trainer:
             )
 
         # Ensure save_steps < max_steps
-        if self.args.save_strategy == "steps":
-            if max_steps < self.args.save_steps:
-                raise ValueError(
-                    'args.save_steps must be less than max_steps. Either set args.save_strategy to "no" or "epoch", '
-                    'or increase number of training steps.'
-                )
+        if self.args.save_strategy == "steps" and max_steps < self.args.save_steps:
+            raise ValueError(
+                'args.save_steps must be less than max_steps. Either set args.save_strategy to "no" or "epoch", '
+                'or increase number of training steps.'
+            )
 
         if DebugOption.UNDERFLOW_OVERFLOW in self.args.debug:
             if self.args.n_gpu > 1:
