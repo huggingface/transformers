1279cb8b3b8d68cf801c2a0e9b5f78bb8dd573d7
Ayenem <moub.ahmed@hotmail.com>
2024-01-17 23:06:14 -0500

    batched generation + isolated token healing

--- examples/pytorch/token-healing/run_token_healing.py
+++ examples/pytorch/token-healing/run_token_healing.py
@@ -1,16 +1,14 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer, GenerationConfig
 
-def generate(prompt, model, tokenizer, token_healing):
-    input_ids = tokenizer(prompt, return_tensors='pt').input_ids.cuda()
+def generate(inputs, model, tokenizer, token_healing):
+    input_ids = tokenizer(inputs, return_tensors='pt', padding=True).input_ids.cuda()
     generation_config = GenerationConfig(
-        max_new_tokens=16,
-        temperature=0.7, do_sample=True, top_p=0.95, top_k=40,
-        pad_token_id=model.config.pad_token_id,
+        max_new_tokens=8, pad_token_id=model.config.pad_token_id, repetition_penalty=1.1,
     )
     output = model.generate(
         inputs=input_ids, token_healing=token_healing, generation_config=generation_config
     )
-    return tokenizer.decode(output[0], skip_special_tokens=True)
+    return tokenizer.batch_decode(output, skip_special_tokens=True)
 
 model_name_or_path = 'TheBloke/deepseek-llm-7B-base-GPTQ'
 completion_model = AutoModelForCausalLM.from_pretrained(
@@ -23,18 +21,25 @@ completion_model = AutoModelForCausalLM.from_pretrained(
 tokenizer = AutoTokenizer.from_pretrained(model_name_or_path, use_fast=True)
 
 test_prompts = [
+    'An example ["like this"] and another example [',
     'The link is <a href="http:',
     'The link is <a href="http', # test aggressive healing http->https
     'I read a book about ', # test trailing whitespace
-    'I read a book about', # test no-op
-    'I', # test single-token
-    'An example ["like this"] and another example [',
+    'I read a book about', # test nothing to heal
 ]
-for prompt in test_prompts:
-    print(f'\nOriginal prompt:\n{prompt}\n')
 
-    healed_output = generate(prompt, completion_model, tokenizer, token_healing=True)
-    print(f'Generation without token healing:\n{healed_output}\n')
+raw_output = generate(test_prompts, completion_model, tokenizer, token_healing=False)
+healed_output = generate(test_prompts, completion_model, tokenizer, token_healing=True)
 
-    output = generate(prompt, completion_model, tokenizer, token_healing=False)
-    print(f'Generation without token healing:\n{output}\n')
+for p, a, b in zip(test_prompts, raw_output, healed_output):
+    print(f'\nPrompt: {p}\nWithout healing:\n{a}\nWith healing:\n{b}')
+
+# You can also use token healing in isolation
+# This can be useful if you have other work to do before the generation
+# Or if you want to delegate generation to another process
+input_ids = tokenizer(test_prompts, return_tensors='pt', padding=True).input_ids.cuda()
+healed_ids = completion_model.heal_tokens(input_ids)
+healed_prompts = tokenizer.batch_decode(healed_ids, skip_special_tokens=True)
+print('\nhealed prompts:')
+for p in healed_prompts:
+    print(p)
