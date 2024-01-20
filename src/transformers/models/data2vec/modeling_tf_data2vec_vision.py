2b0d3abfdcb2744e89ab277ffc31e234a55769a7
MadElf1337 <madelf1337@gmail.com>
2024-01-19 23:41:25 -0800

    fixing in_channels error

--- src/transformers/models/data2vec/modeling_tf_data2vec_vision.py
+++ src/transformers/models/data2vec/modeling_tf_data2vec_vision.py
@@ -1363,7 +1363,6 @@ class TFData2VecVisionFCNHead(tf.keras.layers.Layer):
         convs = []
         convs.append(
             TFData2VecVisionConvModule(
-                in_channels=self.in_channels,
                 out_channels=self.channels,
                 kernel_size=kernel_size,
                 padding="same",
@@ -1374,7 +1373,6 @@ class TFData2VecVisionFCNHead(tf.keras.layers.Layer):
         for i in range(self.num_convs - 1):
             convs.append(
                 TFData2VecVisionConvModule(
-                    in_channels=self.channels,
                     out_channels=self.channels,
                     kernel_size=kernel_size,
                     padding="same",
@@ -1388,11 +1386,7 @@ class TFData2VecVisionFCNHead(tf.keras.layers.Layer):
             self.convs = convs
         if self.concat_input:
             self.conv_cat = TFData2VecVisionConvModule(
-                self.in_channels + self.channels,
-                out_channels=self.channels,
-                kernel_size=kernel_size,
-                padding="same",
-                name="conv_cat",
+                 out_channels=self.channels, kernel_size=kernel_size, padding="same", name="conv_cat"
             )
 
         self.classifier = tf.keras.layers.Conv2D(config.num_labels, kernel_size=1, name="classifier")
@@ -1407,17 +1401,6 @@ class TFData2VecVisionFCNHead(tf.keras.layers.Layer):
             output = self.conv_cat(tf.concat([hidden_states, output], axis=-1))
         output = self.classifier(output)
         return output
-
-    def build(self, input_shape=None):
-        if self.built:
-            return
-        self.built = True
-        if getattr(self, "classifier", None) is not None:
-            with tf.name_scope(self.classifier.name):
-                self.classifier.build([None, None, None, self.channels])
-        if getattr(self, "conv_cat", None) is not None:
-            with tf.name_scope(self.conv_cat.name):
-                self.conv_cat.build(None)
 
 
 @add_start_docstrings(
