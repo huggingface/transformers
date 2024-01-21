cb801996e72389c8a6aecbee0966575ce46c6df7
Arthur Zucker <arthur.zucker@gmail.com>
2024-01-21 21:25:18 +0100

    update conversion script

--- src/transformers/models/golden_gate/convert_golden_gate_weights_to_hf.py
+++ src/transformers/models/golden_gate/convert_golden_gate_weights_to_hf.py
@@ -12,10 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import argparse
-import gc
-import json
 import os
-import shutil
 import warnings
 
 import torch
@@ -53,141 +50,72 @@ Important note: you need to be able to host the whole model in RAM to execute th
 come in several checkpoints they each contain a part of each weight of the model, so we need to load them all in RAM).
 """
 
-NUM_SHARDS = {
-    "2B": 1,
-    "7B": 1,
+golden_gate_2b_config = GoldenGateConfig(
+    hidden_size=2048,
+    intermediate_size=16384,
+    num_attention_heads=8,
+    num_hidden_layers=18,
+    rms_norm_eps=1e-6,
+    num_key_value_heads=1,
+    vocab_size=256128,
+    head_dim=256,
+    max_position_embeddings=8192,
+    rope=10000,
+    hidden_act = "gelu"
+)
+
+golden_gate_7b_config = GoldenGateConfig(
+    hidden_size=3072,
+    intermediate_size=24576,
+    num_attention_heads=16,
+    num_hidden_layers=28,
+    rms_norm_eps= 1e-6,
+    num_key_value_heads=16,
+    vocab_size=256128,
+    head_dim=256,
+    max_position_embeddings=8192,
+    rope=10000,
+    hidden_act = "gelu"
+)
+
+
+LAYER_NAME_MAPPING = {
+    "model.embed_tokens.weight": "model.embedder.weight",
+    "lm_head.weight": "model.embedder.weight",
 }
 
-
-def compute_intermediate_size(n, ffn_dim_multiplier=1, multiple_of=256):
-    return multiple_of * ((int(ffn_dim_multiplier * int(8 * n / 3)) + multiple_of - 1) // multiple_of)
-
-
-def read_json(path):
-    with open(path, "r") as f:
-        return json.load(f)
-
-
-def write_json(text, path):
-    with open(path, "w") as f:
-        json.dump(text, f)
-
-
-def write_model(model_path, input_base_path, config, tokenizer_path=None, safe_serialization=True):
-
-    os.makedirs(model_path, exist_ok=True)
-    tmp_model_path = os.path.join(model_path, "tmp")
-    os.makedirs(tmp_model_path, exist_ok=True)
-
-    config = config.get("model", config)
-    n_layers = config["n_layers"]
-    n_heads = config["n_heads"]
-    n_heads_per_shard = n_heads // num_shards
-    dim = config["dim"]
-    dims_per_head = dim // n_heads
-    base = config.get("rope_theta", 10000.0)
-    inv_freq = 1.0 / (base ** (torch.arange(0, dims_per_head, 2).float() / dims_per_head))
-    if base > 10000.0:
-        max_position_embeddings = 16384
-    else:
-        max_position_embeddings = 2048
-
-    tokenizer_class = LlamaTokenizer if LlamaTokenizerFast is None else LlamaTokenizerFast
-    if tokenizer_path is not None:
-        tokenizer = tokenizer_class(tokenizer_path)
-        tokenizer.save_pretrained(model_path)
-    vocab_size = tokenizer.vocab_size if tokenizer_path is not None else 32000
-
-    if config.get("n_kv_heads", None) is not None:
-        num_key_value_heads = config.n_kv_heads  # for GQA / MQA
-        num_local_key_value_heads = n_heads_per_shard // num_key_value_heads
-        key_value_dim = dim // num_key_value_heads
-    else:  # compatibility with other checkpoints
-        num_key_value_heads = n_heads
-        num_local_key_value_heads = n_heads_per_shard
-        key_value_dim = dim
+def write_model(save_path, input_base_path, config, tokenizer_path=None, safe_serialization=True):
+    num_attn_heads = config.num_attention_heads
+    num_kv_heads = config.num_key_value_heads
+    head_dim = config.head_dim
+    hidden_size = config.hidden_size
 
     # permute for sliced rotary
-    def permute(w, n_heads=n_heads, dim1=dim, dim2=dim):
+    def permute(w, n_heads=num_attn_heads, dim1=hidden_size, dim2=hidden_size):
         return w.view(n_heads, dim1 // n_heads // 2, 2, dim2).transpose(1, 2).reshape(dim1, dim2)
 
     print(f"Fetching all parameters from the checkpoint at {input_base_path}.")
-    loaded = torch.load(os.path.join(input_base_path, "consolidated.00.pth"), map_location="cpu")
+    model_state_dict = torch.load(os.path.join(input_base_path), map_location="cpu")
 
-    param_count = 0
-    index_dict = {"weight_map": {}}
-    for layer_i in range(n_layers):
-        filename = f"pytorch_model-{layer_i + 1}-of-{n_layers + 1}.bin"
-        if num_shards == 1:
-            # Unsharded
-            state_dict = {
-                f"model.layers.{layer_i}.self_attn.q_proj.weight": permute(
-                    loaded[f"layers.{layer_i}.attention.wq.weight"]
-                ),
-                f"model.layers.{layer_i}.self_attn.k_proj.weight": permute(
-                    loaded[f"layers.{layer_i}.attention.wk.weight"]
-                ),
-                f"model.layers.{layer_i}.self_attn.v_proj.weight": loaded[f"layers.{layer_i}.attention.wv.weight"],
-                f"model.layers.{layer_i}.self_attn.o_proj.weight": loaded[f"layers.{layer_i}.attention.wo.weight"],
-                f"model.layers.{layer_i}.mlp.gate_proj.weight": loaded[f"layers.{layer_i}.feed_forward.w1.weight"],
-                f"model.layers.{layer_i}.mlp.down_proj.weight": loaded[f"layers.{layer_i}.feed_forward.w2.weight"],
-                f"model.layers.{layer_i}.mlp.up_proj.weight": loaded[f"layers.{layer_i}.feed_forward.w3.weight"],
-                f"model.layers.{layer_i}.input_layernorm.weight": loaded[f"layers.{layer_i}.attention_norm.weight"],
-                f"model.layers.{layer_i}.post_attention_layernorm.weight": loaded[f"layers.{layer_i}.ffn_norm.weight"],
-            }
-
-        state_dict[f"model.layers.{layer_i}.self_attn.rotary_emb.inv_freq"] = inv_freq
-        for k, v in state_dict.items():
-            index_dict["weight_map"][k] = filename
-            param_count += v.numel()
-        torch.save(state_dict, os.path.join(tmp_model_path, filename))
-
-    filename = f"pytorch_model-{n_layers + 1}-of-{n_layers + 1}.bin"
-
-    # Unsharded
-    state_dict = {
-        "model.embed_tokens.weight": loaded["tok_embeddings.weight"],
-        "model.norm.weight": loaded["norm.weight"],
-        "lm_head.weight": loaded["output.weight"],
-    }
-
-
-    for k, v in state_dict.items():
-        index_dict["weight_map"][k] = filename
-        param_count += v.numel()
-    torch.save(state_dict, os.path.join(tmp_model_path, filename))
-
-    # Write configs
-    index_dict["metadata"] = {"total_size": param_count * 2}
-    write_json(index_dict, os.path.join(tmp_model_path, "pytorch_model.bin.index.json"))
-
-    config = GoldenGateConfig(
-        hidden_size=config.hidden_size,
-        intermediate_size=config.intermediate_size,
-        num_attention_heads=config.num_attention_heads,
-        num_hidden_layers=config.num_hidden_layers,
-        rms_norm_eps=config.rms_norm_eps,
-        num_key_value_heads=num_key_value_heads,
-        vocab_size=config.model_vocab_size,
-        head_dim=config.head_dim,
-        rope_theta=base,
-        max_position_embeddings=config.max_position_embeddings,
-    )
-    config.save_pretrained(tmp_model_path)
-
-    # Make space so we can load the model properly now.
-    del state_dict
-    del loaded
-    gc.collect()
+    state_dict = {}
+    for k,v in model_state_dict:
+        if "qkv_proj" in k:
+            q_proj, k_proj , v_proj = v.split(3)
+            state_dict[k.replace("qkv_proj", "q_proj")] = permute(q_proj)
+            state_dict[k.replace("qkv_proj", "k_proj")] = permute(k_proj)
+            state_dict[k.replace("qkv_proj", "v_proj")] = permute(v_proj)
+        if k in LAYER_NAME_MAPPING:
+            state_dict[LAYER_NAME_MAPPING[k]] = v
+        else:
+            state_dict[k] = v
 
     print("Loading the checkpoint in a GoldenGate model.")
-    model = GoldenGateForCausalLM.from_pretrained(tmp_model_path, torch_dtype=torch.float32, low_cpu_mem_usage=True)
-    # Avoid saving this as part of the config.
+    with torch.device("meta"):
+        model = GoldenGateForCausalLM(config)
+    model.load_state_dict(state_dict, assign=True, strict=True)
     del model.config._name_or_path
-    model.config.torch_dtype = torch.float16
     print("Saving in the Transformers format.")
-    model.save_pretrained(model_path, safe_serialization=safe_serialization)
-    shutil.rmtree(tmp_model_path)
+    model.save_pretrained(save_path, safe_serialization=safe_serialization)
 
 
 def write_tokenizer(tokenizer_path, input_tokenizer_path):
