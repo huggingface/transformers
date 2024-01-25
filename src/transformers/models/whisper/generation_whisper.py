5a2eac04e44c133bdf627b8bbfdabe691bc6c3ff
Patrick von Platen <patrick.v.platen@gmail.com>
2024-01-25 11:46:04 +0000

    Correct more

--- src/transformers/models/whisper/generation_whisper.py
+++ src/transformers/models/whisper/generation_whisper.py
@@ -993,15 +993,7 @@ class WhisperGenerationMixin:
 
     @staticmethod
     def _set_return_timestamps(return_timestamps, is_shortform, generation_config):
-        if return_timestamps is True:
-            if not hasattr(generation_config, "no_timestamps_token_id"):
-                raise ValueError(
-                    "You are trying to return timestamps, but the generation config is not properly set. "
-                    "Make sure to initialize the generation config with the correct attributes that are needed such as `no_timestamps_token_id`. "
-                    "For more details on how to generate the approtiate config, refer to https://github.com/huggingface/transformers/issues/21878#issuecomment-1451902363"
-                )
-            generation_config.return_timestamps = True
-        elif not is_shortform:
+        if not is_shortform:
             if return_timestamps is False:
                 raise ValueError(
                     "You have passed more than 3000 mel input features (> 30 seconds) which automatically enables long-form generation which "
@@ -1009,9 +1001,16 @@ class WhisperGenerationMixin:
                 )
 
             logger.info("Setting `return_timestamps=True` for long-form generation.")
-            generation_config.return_timestamps = True
-        else:
-            generation_config.return_timestamps = False
+            return_timestamps = True
+
+        if return_timestamps and not hasattr(generation_config, "no_timestamps_token_id"):
+            raise ValueError(
+                "You are trying to return timestamps, but the generation config is not properly set. "
+                "Make sure to initialize the generation config with the correct attributes that are needed such as `no_timestamps_token_id`. "
+                "For more details on how to generate the approtiate config, refer to https://github.com/huggingface/transformers/issues/21878#issuecomment-1451902363"
+            )
+
+        generation_config.return_timestamps = return_timestamps
 
     @staticmethod
     def _set_language_and_task(language, task, is_multilingual, generation_config):
