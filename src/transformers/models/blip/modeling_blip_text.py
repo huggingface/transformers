bdc243deb4feb6bd72f3a39bc68cf7943ec92465
Nilesh Kokane <nilesh.kokane360@gmail.com>
2024-01-25 21:35:05 +0530

    Fixed nll with label_smoothing to nll

--- src/transformers/models/blip/modeling_blip_text.py
+++ src/transformers/models/blip/modeling_blip_text.py
@@ -889,7 +889,7 @@ class BlipTextLMHeadModel(BlipTextPreTrainedModel):
             # we are doing next-token prediction; shift prediction scores and input ids by one
             shifted_prediction_scores = prediction_scores[:, :-1, :].contiguous()
             labels = labels[:, 1:].contiguous().to(shifted_prediction_scores.device)
-            loss_fct = CrossEntropyLoss(reduction=reduction, label_smoothing=0.1)
+            loss_fct = CrossEntropyLoss(reduction=reduction, label_smoothing=0.0)
             lm_loss = loss_fct(shifted_prediction_scores.view(-1, self.config.vocab_size), labels.view(-1))
             if reduction == "none":
                 lm_loss = lm_loss.view(prediction_scores.size(0), -1).sum(1)
