c1dcaa3eb8a1d065fcd95363e08763bc9dfa9270
younesbelkada <younesbelkada@gmail.com>
2024-01-25 13:19:11 +0000

    fix

--- src/transformers/quantizers/__init__.py
+++ src/transformers/quantizers/__init__.py
@@ -12,3 +12,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 from .auto import AutoHFQuantizer, AutoQuantizationConfig
+from .base import HFQuantizer
