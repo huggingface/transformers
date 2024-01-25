4efab00acb679cae31d1c3caee0e6a1ebc6d3685
Wesley Gifford <79663411+wgifford@users.noreply.github.com>
2024-01-25 15:38:31 -0500

    Update tests/models/patchtsmixer/test_modeling_patchtsmixer.py
    Co-authored-by: amyeroberts <22614925+amyeroberts@users.noreply.github.com>

--- tests/models/patchtsmixer/test_modeling_patchtsmixer.py
+++ tests/models/patchtsmixer/test_modeling_patchtsmixer.py
@@ -260,7 +260,6 @@ class PatchTSMixerModelTest(ModelTesterMixin, PipelineTesterMixin, unittest.Test
         if model_class in get_values(MODEL_FOR_TIME_SERIES_CLASSIFICATION_MAPPING):
             rng = random.Random(self.model_tester.seed_number)
             labels = ids_tensor([self.model_tester.batch_size], self.model_tester.num_targets, rng=rng)
-            # inputs_dict["labels"] = labels
             inputs_dict["target_values"] = labels
             inputs_dict.pop("future_values")
         elif model_class in get_values(MODEL_FOR_TIME_SERIES_REGRESSION_MAPPING):
