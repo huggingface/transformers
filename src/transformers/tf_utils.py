8eb0489f9783310f914688aa37aec54f3dce55bb
Matt <rocketknight1@gmail.com>
2024-01-26 18:51:18 +0000

    Convert BatchFeature too (thanks Amy)

--- src/transformers/tf_utils.py
+++ src/transformers/tf_utils.py
@@ -17,6 +17,7 @@ from typing import List, Optional, Union
 import numpy as np
 import tensorflow as tf
 
+from .feature_extraction_utils import BatchFeature
 from .tokenization_utils_base import BatchEncoding
 from .utils import logging
 
@@ -257,10 +258,10 @@ def expand_1d(data):
 
 
 def convert_batch_encoding(*args, **kwargs):
-    # Convert HF BatchEncoding objects in the inputs to dicts that Keras understands
-    if args and isinstance(args[0], BatchEncoding):
+    # Convert HF BatchEncoding/BatchFeature objects in the inputs to dicts that Keras understands
+    if args and isinstance(args[0], (BatchEncoding, BatchFeature)):
         args = list(args)
         args[0] = dict(args[0])
-    elif "x" in kwargs and isinstance(kwargs["x"], BatchEncoding):
+    elif "x" in kwargs and isinstance(kwargs["x"], (BatchEncoding, BatchFeature)):
         kwargs["x"] = dict(kwargs["x"])
     return args, kwargs
