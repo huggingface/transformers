ddae6f5aebea8f4129cff6c40b9cf9e89927f27a
steven <steven.bucaille@gmail.com>
2024-01-29 11:04:36 +0100

    Added copyright header

--- src/transformers/models/superpoint/configuration_superpoint.py
+++ src/transformers/models/superpoint/configuration_superpoint.py
@@ -1,3 +1,16 @@
+# Copyright 2023 The HuggingFace Team. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 from typing import List
 
 from ...configuration_utils import PretrainedConfig
@@ -48,6 +61,7 @@ class SuperPointConfig(PretrainedConfig):
     >>> # Accessing the model configuration
     >>> configuration = model.config
     ```"""
+
     model_type = "superpoint"
 
     def __init__(
