ce1d84378b5d14bb6c2643d57f04f67d94210359
steven <steven.bucaille@gmail.com>
2024-01-29 11:09:40 +0100

    Changed from Copyright 2023 to 2024 and added other headers

--- src/transformers/models/superpoint/modeling_superpoint.py
+++ src/transformers/models/superpoint/modeling_superpoint.py
@@ -1,4 +1,4 @@
-# Copyright 2023 The HuggingFace Team. All rights reserved.
+# Copyright 2024 The HuggingFace Team. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -373,7 +373,7 @@ SUPERPOINT_START_DOCSTRING = r"""
 SUPERPOINT_INPUTS_DOCSTRING = r"""
 Args:
     pixel_values (`torch.FloatTensor` of shape `(batch_size, num_channels, height, width)`):
-        Pixel values. Pixel values can be obtained using [`SuperPointImageProcessor`]. See 
+        Pixel values. Pixel values can be obtained using [`SuperPointImageProcessor`]. See
         [`SuperPointImageProcessor.__call__`] for details.
     output_hidden_states (`bool`, *optional*):
         Whether or not to return the hidden states of all layers. See `hidden_states` under returned tensors for
