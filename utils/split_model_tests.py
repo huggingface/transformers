95b4ec58e0eb6400d45065e2489f834dce388e2f
ydshieh <ydshieh@users.noreply.github.com>
2024-01-29 16:54:57 +0100

    remove unused file

--- utils/split_model_tests.py
+++ utils/split_model_tests.py
@@ -6,4 +6,12 @@ d1 = sorted(list(filter(os.path.isdir, os.listdir(tests))))
 d2 = sorted(list(filter(os.path.isdir, [f"models/{x}" for x in model_tests])))
 d1.remove("models")
 d = d2[1:3] + d1[:1]
-print(d)
+
+num_jobs = len(d)
+num_splits = int(os.getenv("NUM_SLICES", 2))
+num_jobs_per_splits = num_jobs // num_splits + int(num_jobs % num_splits > 0)
+
+model_splits = []
+for idx in range(num_splits):
+    model_splits.append(d[(num_jobs_per_splits * idx):(num_jobs_per_splits * (idx + 1))])
+print(model_splits)
