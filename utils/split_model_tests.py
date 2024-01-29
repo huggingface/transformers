6728173b759b2e949fff272fe350319ecad23220
ydshieh <ydshieh@users.noreply.github.com>
2024-01-29 18:51:56 +0100

    fix

--- utils/split_model_tests.py
+++ utils/split_model_tests.py
@@ -7,15 +7,16 @@ if __name__ == "__main__":
     d1 = sorted(list(filter(os.path.isdir, os.listdir(tests))))
     d2 = sorted(list(filter(os.path.isdir, [f"models/{x}" for x in model_tests])))
     d1.remove("models")
-    d = d2[1:3] + d1[:1]
+    d = d2[1:5] + d1[:2]
 
     num_jobs = len(d)
     num_splits = int(os.getenv("NUM_SLICES", 2))
     num_jobs_per_splits = num_jobs // num_splits
 
     model_splits = []
+    end = 0
     for idx in range(num_splits):
-        start = num_jobs_per_splits * idx
-        end = start + num_jobs_per_splits if idx < num_splits - 1 else num_jobs
+        start = end
+        end = start + num_jobs_per_splits + (1 if idx < num_jobs % num_splits else 0)
         model_splits.append(d[start:end])
     print(model_splits)
