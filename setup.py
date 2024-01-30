9410491a633bf133e33c5a059dec674df3b8eb74
Matt <rocketknight1@gmail.com>
2024-01-30 19:18:01 +0000

    twiddle it back

--- setup.py
+++ setup.py
@@ -160,7 +160,7 @@ _deps = [
     "sacremoses",
     "safetensors>=0.4.1",
     "sagemaker>=2.31.0",
-    "scikit-lear",
+    "scikit-learn",
     "sentencepiece>=0.1.91,!=0.1.92",
     "sigopt",
     "starlette",
