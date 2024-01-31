717c731e21ee9b0b64645146230894bd1633b328
ydshieh <ydshieh@users.noreply.github.com>
2024-01-31 16:40:46 +0100

    add doc

--- utils/split_model_tests.py
+++ utils/split_model_tests.py
@@ -1,3 +1,37 @@
+# Copyright 2024 The HuggingFace Team. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+This script is used to get the list of folders under `tests/models` and split the list into `NUM_SLICES` splits.
+The main use case is a GitHub Actions workflow file calling this script to get the (nested) list of folders allowing it
+to split the list of jobs to run into multiple slices each containing a smaller number of jobs. This way, we can bypass
+the maximum of 256 jobs in a matrix.
+
+See the `setup` and `run_tests_gpu` jobs defined in the workflow file `.github/workflows/self-scheduled.yml` for more
+details.
+
+Usage:
+
+This script is required to be run under `tests` folder of `transformers` root directory.
+
+Assume we are under `transformers` root directory:
+```bash
+cd tests
+python ../utils/split_model_tests.py
+```
+"""
+
 import os
 
 
@@ -10,7 +44,7 @@ if __name__ == "__main__":
     d = d2 + d1
 
     num_jobs = len(d)
-    num_splits = int(os.getenv("NUM_SLICES", 2))
+    num_splits = int(os.getenv("NUM_SLICES", 1))
     num_jobs_per_splits = num_jobs // num_splits
 
     model_splits = []
