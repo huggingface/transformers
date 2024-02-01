42bc4eab1e8c3e2f579bcdb0dfd9bd6f06c6c048
Derk Mus <derkmus@gmail.com>
2024-02-01 18:35:19 +0100

    remove tokenization mamba and load tf weights

--- src/transformers/models/vmamba/__init__.py
+++ src/transformers/models/vmamba/__init__.py
@@ -13,52 +13,31 @@
 # limitations under the License.
 from typing import TYPE_CHECKING
 
-from ...utils import OptionalDependencyNotAvailable, _LazyModule, is_tokenizers_available, is_torch_available
+from ...utils import OptionalDependencyNotAvailable, _LazyModule, is_torch_available
 
 
 _import_structure = {
     "configuration_vmamba": ["VMAMBA_PRETRAINED_CONFIG_ARCHIVE_MAP", "VMambaConfig"],
-    "tokenization_vmamba": ["VMambaTokenizer"],
 }
 
-try:
-    if not is_tokenizers_available():
-        raise OptionalDependencyNotAvailable()
-except OptionalDependencyNotAvailable:
-    pass
-else:
-    _import_structure["tokenization_vmamba_fast"] = ["VMambaTokenizerFast"]
-
-try:
-    if not is_torch_available():
-        raise OptionalDependencyNotAvailable()
-except OptionalDependencyNotAvailable:
-    pass
-else:
+if is_torch_available():
     _import_structure["modeling_vmamba"] = [
         "VMAMBA_PRETRAINED_MODEL_ARCHIVE_LIST",
         "VMambaForImageClassification",
         "VMambaModel",
         "VMambaPreTrainedModel",
-        "load_tf_weights_in_vmamba",
     ]
 
 
 if TYPE_CHECKING:
     from .configuration_vmamba import VMAMBA_PRETRAINED_CONFIG_ARCHIVE_MAP, VMambaConfig
 
-    try:
-        if not is_torch_available():
-            raise OptionalDependencyNotAvailable()
-    except OptionalDependencyNotAvailable:
-        pass
-    else:
+    if is_torch_available():
         from .modeling_vmamba import (
             VMAMBA_PRETRAINED_MODEL_ARCHIVE_LIST,
             VMambaForImageClassification,
             VMambaModel,
             VMambaPreTrainedModel,
-            load_tf_weights_in_vmamba,
         )
 
 
