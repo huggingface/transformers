a3fc357f30566f875fba5f5b772dac52f040d14a
Karl Hajjar <karl.hajjar@polytechnique.edu>
2024-02-02 10:22:49 +0100

    Fixed formatting issues. Fixed missing argument in write_model.

--- src/transformers/models/llama/convert_llama_weights_to_hf.py
+++ src/transformers/models/llama/convert_llama_weights_to_hf.py
@@ -80,7 +80,9 @@ def write_json(text, path):
         json.dump(text, f)
 
 
-def write_model(model_path, input_base_path, model_size, tokenizer_path=None, safe_serialization=True):
+def write_model(
+    model_path, input_base_path, model_size, tokenizer_path=None, safe_serialization=True, llama_version=1
+):
     # for backward compatibility, before you needed the repo to be called `my_repo/model_size`
     if not os.path.isfile(os.path.join(input_base_path, "params.json")):
         input_base_path = os.path.join(input_base_path, model_size)
@@ -103,14 +105,14 @@ def write_model(model_path, input_base_path, model_size, tokenizer_path=None, sa
         max_position_embeddings = 16384
     else:
         if llama_version == 1:
-                    max_position_embeddings = 2048
+            max_position_embeddings = 2048
         elif llama_version == 2:
             max_position_embeddings = 4096
         else:
             raise NotImplementedError(
                 f"Version {llama_version} of llama is not supported yet. "
-                 "Current supported versions of llama are [1, 2]."
-                 )
+                "Current supported versions of llama are [1, 2]."
+            )
 
     tokenizer_class = LlamaTokenizer if LlamaTokenizerFast is None else LlamaTokenizerFast
     if tokenizer_path is not None:
