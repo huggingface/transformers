464ab65d23668cdafaf757784bb43216a5fea6c2
Karsten Martiny <karsten.martiny@sri.com>
2024-02-05 11:33:29 -0800

    alternative method to create decoder_attention_mask in modeling_encoder_decoder to prevent torch warning

--- src/transformers/models/encoder_decoder/modeling_encoder_decoder.py
+++ src/transformers/models/encoder_decoder/modeling_encoder_decoder.py
@@ -617,7 +617,7 @@ class EncoderDecoderModel(PreTrainedModel):
                 labels, self.config.pad_token_id, self.config.decoder_start_token_id
             )
             if decoder_attention_mask is None:
-                decoder_attention_mask = decoder_input_ids.new_tensor(decoder_input_ids != self.config.pad_token_id)
+                decoder_attention_mask = torch.not_equal(decoder_input_ids, self.config.pad_token_id)
 
         # Decode
         decoder_outputs = self.decoder(
