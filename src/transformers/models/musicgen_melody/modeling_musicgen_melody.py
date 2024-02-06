4eface6bb390a9f025d895e2bc0ba4a176d702d1
Yoach Lacombe <yoach.lacombe@gmail.com>
2024-02-06 15:26:36 +0000

    change warning for chroma length

--- src/transformers/models/musicgen_melody/modeling_musicgen_melody.py
+++ src/transformers/models/musicgen_melody/modeling_musicgen_melody.py
@@ -1807,7 +1807,11 @@ class MusicgenMelodyForConditionalGeneration(PreTrainedModel):
                     n_repeat = int(math.ceil(self.config.chroma_length / audio_hidden_states.shape[1]))
                     audio_hidden_states = audio_hidden_states.repeat(1, n_repeat, 1)
                 else:
-                    logger.warning("The conditional audio signal is too long and will be truncated.")
+                    logger.warning(
+                        f"The conditional audio signal is of length {audio_hidden_states.shape[1]}, which exceeds"
+                        f"the maximum chroma duration of {self.config.chroma_length}."
+                        f"The audio will be truncated to {self.config.chroma_length} frames."
+                    )
                 audio_hidden_states = audio_hidden_states[:, : self.config.chroma_length]
 
                 if encoder_hidden_states is not None:
