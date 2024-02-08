52cde6bd9d07e9bc45b29dc5b29eafbfa01badf5
Mamou, Jonathan <jonathan.mamou@intel.com>
2024-02-08 04:57:32 -0800

    fix docstring

--- src/transformers/generation/configuration_utils.py
+++ src/transformers/generation/configuration_utils.py
@@ -248,7 +248,7 @@ class GenerationConfig(PushToHubMixin):
             Defines the schedule at which max assistant tokens shall be changed during inference.
             - `"heuristic"`: When all speculative tokens are correct, increase `num_assistant_tokens` by 2 else
               reduce by 1. `num_assistant_tokens` value is persistent over multiple generation calls with the same assistant model.
-            - `"heuristic_transient`: Same as `"heuristic` but `num_assistant_tokens` is reset to its initial value after each generation call.
+            - `"heuristic_transient"`: Same as `"heuristic"` but `num_assistant_tokens` is reset to its initial value after each generation call.
             - `"constant"`: `num_assistant_tokens` stays unchanged during generation
 
         > Wild card
