88db53f3bb4113164ab3f09b122986cef66ac5b4
Eduardo Pacheco <eduardo.pacheco@limehome.com>
2024-02-10 06:44:18 +0100

    Added docstrings to SegGptLoss

--- src/transformers/models/seggpt/modeling_seggpt.py
+++ src/transformers/models/seggpt/modeling_seggpt.py
@@ -870,9 +870,31 @@ class SegGptLoss(nn.Module):
         labels: torch.FloatTensor,
         bool_masked_pos: torch.BoolTensor,
     ):
+        """Computes the L1 loss between the predicted masks and the ground truth masks.
+
+        Args:
+            pixel_values (`torch.FloatTensor` of shape `(batch_size, num_channels, 2*height, width)`):
+                Concatenated pixel values from prompt and input images.
+
+            prompt_pixel_values (`torch.FloatTensor` of shape `(batch_size, num_channels, 2*height, width)`):
+                Concatenated pixel values from mask prompt.
+
+            pred_masks (`torch.FloatTensor` of shape `(batch_size, num_channels, 2*height, width)`):
+                Predicted masks.
+
+            labels (`torch.FloatTensor` of shape `(batch_size, num_channels, height, width)`):
+                Ground truth mask for input images.
+
+            bool_masked_pos (`torch.BoolTensor` of shape `(batch_size, num_patches)`):
+                Boolean masked positions. Indicates which patches are masked (1) and which aren't (0).
+
+        Returns:
+            _type_: _description_
+        """
         mask = bool_masked_pos[:, :, None].repeat(1, 1, self.patch_size**2 * 3)
         mask = unpatchify(mask, pixel_values.shape[1] // self.patch_size, pixel_values.shape[2] // self.patch_size)
         # Changing dummy mask in prompt_pixel_values to labels values
+        prompt_pixel_values = prompt_pixel_values.clone()
         prompt_pixel_values[:, :, prompt_pixel_values.shape[2] // 2 :, :] = labels
         loss = F.smooth_l1_loss(pred_masks, prompt_pixel_values, reduction="none", beta=self.beta)
         loss = (loss * mask).sum() / mask.sum()  # mean loss on removed patches
