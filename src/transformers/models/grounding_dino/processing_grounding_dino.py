09ae5c1708be994896cf8dc791abd3b0bac71e5d
Eduardo Pacheco <eduardo.pacheco@limehome.com>
2024-02-10 20:39:50 +0100

    Updated post_process_grounded_object_detection

--- src/transformers/models/grounding_dino/processing_grounding_dino.py
+++ src/transformers/models/grounding_dino/processing_grounding_dino.py
@@ -18,6 +18,7 @@ Processor class for Grounding DINO.
 
 from typing import List, Optional, Tuple, Union
 
+from ...image_transforms import center_to_corners_format
 from ...image_utils import ImageInput
 from ...processing_utils import ProcessorMixin
 from ...tokenization_utils_base import BatchEncoding, PaddingStrategy, PreTokenizedInput, TextInput, TruncationStrategy
@@ -192,18 +193,57 @@ class GroundingDinoProcessor(ProcessorMixin):
         target_sizes: Union[TensorType, List[Tuple]] = None,
     ):
         """
-        Post-process the output of the model to get the grounded object detection results.
+        Converts the raw output of [`GroundingDinoForObjectDetection`] into final bounding boxes in (top_left_x, top_left_y,
+        bottom_right_x, bottom_right_y) format and get the associated text label.
+
+        Args:
+            outputs ([`GroundingDinoObjectDetectionOutput`]):
+                Raw outputs of the model.
+            input_ids (`torch.LongTensor` of shape `(batch_size, sequence_length)`):
+                The token ids of the input text.
+            box_threshold (`float`, *optional*):
+                Score threshold to keep object detection predictions.
+            text_threshold (`float`, *optional*):
+                Score threshold to keep text detection predictions.
+            target_sizes (`torch.Tensor` or `List[Tuple[int, int]]`, *optional*):
+                Tensor of shape `(batch_size, 2)` or list of tuples (`Tuple[int, int]`) containing the target size
+                `(height, width)` of each image in the batch. If unset, predictions will not be resized.
+        Returns:
+            `List[Dict]`: A list of dictionaries, each dictionary containing the scores, labels and boxes for an image
+            in the batch as predicted by the model.
         """
-        results = self.image_processor.post_process_object_detection(outputs, box_threshold, target_sizes)
+        logits, boxes = outputs.logits, outputs.pred_boxes
 
-        probs = torch.sigmoid(outputs.logits)  # (batch_size, num_queries, 256)
+        if target_sizes is not None:
+            if len(logits) != len(target_sizes):
+                raise ValueError(
+                    "Make sure that you pass in as many target sizes as the batch dimension of the logits"
+                )
 
-        for idx, (result, prob) in enumerate(zip(results, probs)):
-            labels = result["labels"]
-            # Assuming that selected bboxes are sorted by confidence due to Hungarian matching loss in training
-            prob = prob[: len(labels)]  # len(labels) , 256
-            token_ids = get_phrases_from_posmap(prob > text_threshold, input_ids[idx])
-            # overrides result labels key
-            result["labels"] = self.batch_decode(token_ids)
+        probs = torch.sigmoid(logits)  # (batch_size, num_queries, 256)
+        scores = torch.max(probs, dim=-1)[0]  # (batch_size, num_queries)
+
+        # Convert to [x0, y0, x1, y1] format
+        boxes = center_to_corners_format(boxes)
+
+        # Convert from relative [0, 1] to absolute [0, height] coordinates
+        if target_sizes is not None:
+            if isinstance(target_sizes, List):
+                img_h = torch.Tensor([i[0] for i in target_sizes])
+                img_w = torch.Tensor([i[1] for i in target_sizes])
+            else:
+                img_h, img_w = target_sizes.unbind(1)
+
+            scale_fct = torch.stack([img_w, img_h, img_w, img_h], dim=1).to(boxes.device)
+            boxes = boxes * scale_fct[:, None, :]
+
+        results = []
+        for idx, (s, b, p) in enumerate(zip(scores, boxes, probs)):
+            score = s[s > box_threshold]
+            box = b[s > box_threshold]
+            prob = p[s > box_threshold]
+            label_ids = get_phrases_from_posmap(prob > text_threshold, input_ids[idx])
+            label = self.batch_decode(label_ids)
+            results.append({"scores": score, "labels": label, "boxes": box})
 
         return results
