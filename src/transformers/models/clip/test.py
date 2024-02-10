3a0663af410aab0ecc0048a56806a45f70b5bbc2
Niels <niels.rogge1@gmail.com>
2024-02-10 12:45:36 +0100

    Remove scripts

--- src/transformers/models/clip/test.py
+++
@@ -1,4 +0,0 @@
-from transformers import CLIPForImageClassification
-
-
-model = CLIPForImageClassification.from_pretrained("openai/clip-vit-base-patch16")
--- src/transformers/models/siglip/test.py
+++
@@ -1,4 +0,0 @@
-from transformers import SiglipForImageClassification
-
-
-model = SiglipForImageClassification.from_pretrained("google/siglip-base-patch16-256-multilingual")
