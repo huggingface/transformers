4f8193a078abe3cf6be2af282d3fb272ec118073
hi-sushanta <iamchi@skiff.com>
2024-02-10 06:24:11 +0530

    Run make fix-copies

--- src/transformers/models/mbart50/tokenization_mbart50.py
+++ src/transformers/models/mbart50/tokenization_mbart50.py
@@ -236,21 +236,18 @@ class MBart50Tokenizer(PreTrainedTokenizer):
         current_sub_tokens = []
         out_string = ""
         prev_is_special = False
-        for i,token in enumerate(tokens):
+        for token in tokens:
             # make sure that special tokens are not decoded using sentencepiece model
             if token in self.all_special_tokens:
                 if not prev_is_special:
-                    out_string += " "  
-                if token != "</s>" and (i != len(tokens)):   
-                    out_string += self.sp_model.decode(current_sub_tokens) + " " + token
-                else:
-                    out_string += self.sp_model.decode(current_sub_tokens) + token
-                prev_is_special = True
+                    out_string += " "
+                out_string += self.sp_model.decode(current_sub_tokens) + token
+                prev_is_special = True 
                 current_sub_tokens = []
             else:
                 current_sub_tokens.append(token)
                 prev_is_special = False
-        out_string += " " + self.sp_model.decode(current_sub_tokens)
+        out_string += self.sp_model.decode(current_sub_tokens)
         return out_string.strip()
 
     def save_vocabulary(self, save_directory: str, filename_prefix: Optional[str] = None) -> Tuple[str]:
