8af6774f52d2a37eb99d40abf12173f8a1d87cfe
ydshieh <ydshieh@users.noreply.github.com>
2024-02-12 15:52:40 +0100

    002

--- src/transformers/benchmark/benchmark_utils_generic.py
+++ src/transformers/benchmark/benchmark_utils_generic.py
@@ -14,29 +14,65 @@
 """
 Benchmark
 """
-
-
+import json
 import timeit
 
 
 class BenchMark:
 
-    def run(self, measure_kwargs, prep_kwargs, input_kwargs):
+    def measure(self, func, **measure_kwargs):
+        raise NotImplementedError
 
-        return self.measure(self.prepare_target(**prep_kwargs), **measure_kwargs)(**input_kwargs)
+    def target(self, **target_kwargs):
+        raise NotImplementedError
 
-    def report(self):
-        pass
+    def inputs(self, **inputs_kwargs):
+        return {}
+
+    def _convert_to_json(self, report):
+        if isinstance(report, list):
+            return [self._convert_to_json(x) for x in report]
+        if isinstance(report, dict):
+            return {k: self._convert_to_json(v) for k, v in report.items()}
+        if isinstance(report, type):
+            return report.__name__
+        return report
+
+    def report(self, result, run_info, only_result=False, output_path=None):
+
+        report = {"result": result}
+        if not only_result:
+            report["run_kwargs"] = run_info
+
+        report = self._convert_to_json(report)
+
+        return report
+
+    def run(self, measure_kwargs, target_kwargs, inputs_kwargs, report_kwargs):
+        target = self.target(**target_kwargs)
+
+        all_inputs_kwargs = [inputs_kwargs] if isinstance(inputs_kwargs, dict) else inputs_kwargs
+        results = []
+        for _inputs_kwargs in all_inputs_kwargs:
+
+            inputs = self.inputs(**_inputs_kwargs)
+            result = self.measure(target, **measure_kwargs)(**inputs)
+            results.append(result)
+
+        run_info = {
+            "measure_kwargs": measure_kwargs,
+            "target_kwargs": target_kwargs,
+            "inputs_kwargs": inputs_kwargs,
+            "report_kwargs": report_kwargs,
+        }
+
+        if isinstance(inputs_kwargs, dict):
+            results = results[0]
+
+        return self.report(results, run_info, **report_kwargs)
 
 
-class FromPretrainedBenchMark(BenchMark):
-
-    def prepare_target(self, model_class, repo_id):
-
-        def target():
-            _ = model_class.from_pretrained(repo_id)
-
-        return target
+class SpeedBenchMark(BenchMark):
 
     def measure(self, func, number=3, repeat=1):
 
@@ -54,17 +90,55 @@ class FromPretrainedBenchMark(BenchMark):
         return wrapper
 
 
+class SpeedBenchMark(BenchMark):
+
+    def measure(self, func, number=3, repeat=1):
+
+        def wrapper():
+
+            # as written in https://docs.python.org/2/library/timeit.html#timeit.Timer.repeat, min should be taken rather than the average
+            runtimes = timeit.repeat(
+                func,
+                repeat=repeat,
+                number=number,
+            )
+
+            return {"time": min(runtimes) / number}
+
+        return wrapper
+
+
+class FromPretrainedBenchMark(BenchMark):
+
+    def target(self, model_class, repo_id):
+
+        def target():
+            _ = model_class.from_pretrained(repo_id)
+
+        return target
+
+
+class FromPretrainedSpeedBenchMark(SpeedBenchMark, FromPretrainedBenchMark):
+    pass
+
+
 if __name__ == "__main__":
 
     from transformers import AutoModel
     repo_id = "bert-base-uncased"
 
-    benchmakr = FromPretrainedBenchMark()
+    benchmakr = FromPretrainedSpeedBenchMark()
 
     run_kwargs = {
         "measure_kwargs": {"number": 2, "repeat": 3},
-        "prep_kwargs": {"model_class": AutoModel, "repo_id": repo_id},
-        "input_kwargs": {},
+        "target_kwargs": {"model_class": AutoModel, "repo_id": repo_id},
+        "inputs_kwargs": [{}],
+        "report_kwargs": {},
+        # "report_kwargs": {"output_path": None, "keys_to_keep": None}
     }
     result = benchmakr.run(**run_kwargs)
-    print(result)
+    print(json.dumps(result, indent=4))
+
+    run_kwargs["report_kwargs"]["only_result"] = True
+    result = benchmakr.run(**run_kwargs)
+    print(json.dumps(result, indent=4))
