10cc68f3d68c6a8ced4d349475bdb427e751eefb
Arthur Zucker <arthur.zucker@gmail.com>
2024-02-12 09:50:46 +0100

    some nits to make sure genration works with static cache uncompiled

--- src/transformers/cache_utils.py
+++ src/transformers/cache_utils.py
@@ -396,7 +396,7 @@ class StaticCache(Cache):
 
     def get_seq_length(self, layer_idx: Optional[int] = 0) -> int:
         """Returns the sequence length of the cached states that were seen by the model. `layer_idx` kept for BC"""
-        return 0
+        return self.seen_tokens
 
     def get_usable_length(self, new_sequence_length=None, layer_idx: Optional[int] = 0) -> int:
         return self.seen_tokens
