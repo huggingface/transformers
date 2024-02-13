1547befece9b9f2762ec79d946db036af248ebea
ydshieh <ydshieh@users.noreply.github.com>
2024-02-13 16:52:16 +0100

    012

--- src/transformers/benchmark/static_cache_benchmark.py
+++ src/transformers/benchmark/static_cache_benchmark.py
@@ -111,7 +111,7 @@ class StaticCacheBenchMark(BenchMark):
                     with torch.backends.cuda.sdp_kernel(enable_flash=False, enable_mem_efficient=False, enable_math=True):
                         next_token = decode_one_tokens_compiled(self.model, next_token.clone(), None, cache_position)
                         generated_ids.index_copy_(1, cache_position, next_token)
-                    torch.cuda.synchronize()
+                    # torch.cuda.synchronize()
 
                     # _total_time = time.perf_counter() - _start
                     # print(_total_time, end="\t")
