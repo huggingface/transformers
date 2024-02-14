fedc5633542e9ef3583cd5747bf9a96b1e15b9fb
Arthur Zucker <arthur.zucker@gmail.com>
2024-02-14 10:08:55 +0900

    fix-copies

--- src/transformers/models/persimmon/modeling_persimmon.py
+++ src/transformers/models/persimmon/modeling_persimmon.py
@@ -823,7 +823,6 @@ class PersimmonForCausalLM(PersimmonPreTrainedModel):
             attentions=outputs.attentions,
         )
 
-    # Copied from transformers.models.llama.modeling_llama.LlamaForCausalLM.prepare_inputs_for_generation
     def prepare_inputs_for_generation(
         self, input_ids, past_key_values=None, attention_mask=None, inputs_embeds=None, **kwargs
     ):
