d8c195fbdf486015cf758b656842f8b5c7e05da3
Arthur Zucker <arthur.zucker@gmail.com>
2024-02-15 16:45:16 +0900

    nits

--- src/transformers/models/mamba/modeling_mamba.py
+++ src/transformers/models/mamba/modeling_mamba.py
@@ -486,6 +486,30 @@ class MambaPreTrainedModel(PreTrainedModel):
         # # Our initialization would set all Linear.bias to zero, need to mark this one as _no_reinit
         # self.dt_proj.bias._no_reinit = True
 
+        # if isinstance(module, nn.Linear):
+        #     if module.bias is not None:
+        #         if not getattr(module.bias, "_no_reinit", False):
+        #             nn.init.zeros_(module.bias)
+        # elif isinstance(module, nn.Embedding):
+        #     nn.init.normal_(module.weight, std=initializer_range)
+        #
+        # if rescale_prenorm_residual:
+        #     # Reinitialize selected weights subject to the OpenAI GPT-2 Paper Scheme:
+        #     #   > A modified initialization which accounts for the accumulation on the residual path with model depth. Scale
+        #     #   > the weights of residual layers at initialization by a factor of 1/√N where N is the # of residual layers.
+        #     #   >   -- GPT-2 :: https://openai.com/blog/better-language-models/
+        #     #
+        #     # Reference (Megatron-LM): https://github.com/NVIDIA/Megatron-LM/blob/main/megatron/model/gpt_model.py
+        #     for name, p in module.named_parameters():
+        #         if name in ["out_proj.weight", "fc2.weight"]:
+        #             # Special Scaled Initialization --> There are 2 Layer Norms per Transformer Block
+        #             # Following Pytorch init, except scale by 1/sqrt(2 * n_layer)
+        #             # We need to reinit p since this code could be called multiple times
+        #             # Having just p *= scale would repeatedly scale it down
+        #             nn.init.kaiming_uniform_(p, a=math.sqrt(5))
+        #             with torch.no_grad():
+        #                 p /= math.sqrt(n_residuals_per_layer * n_layer)
+
 
 @dataclass
 class MambaOutput(ModelOutput):
