3164035847224b24f352864befb41038f86ec118
Matt <rocketknight1@gmail.com>
2024-02-15 14:21:51 +0000

    Add type hint

--- src/transformers/pipelines/text_generation.py
+++ src/transformers/pipelines/text_generation.py
@@ -1,5 +1,6 @@
 import enum
 import warnings
+from typing import Dict
 
 from ..utils import add_end_docstrings, is_tf_available, is_torch_available
 from .base import Pipeline, build_pipeline_init_args
@@ -25,10 +26,10 @@ class Chat:
     to this format because the rest of the pipeline code tends to assume that lists of messages are
     actually a batch of samples rather than messages in the same conversation."""
 
-    def __init__(self, messages):
+    def __init__(self, messages: Dict):
         for message in messages:
             if not ("role" in message and "content" in message):
-                raise ValueError("When passing chat dicts as input, each chat must have a 'role' and 'content' key.")
+                raise ValueError("When passing chat dicts as input, each dict must have a 'role' and 'content' key.")
         self.messages = messages
 
 
