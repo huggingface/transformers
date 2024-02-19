c98cb874279dfd88878035a0d7b00588886cbdbe
ydshieh <ydshieh@users.noreply.github.com>
2024-02-19 20:19:35 +0800

    change cache number

--- .circleci/create_circleci_config.py
+++ .circleci/create_circleci_config.py
@@ -52,7 +52,7 @@ class CircleCIJob:
     name: str
     additional_env: Dict[str, Any] = None
     cache_name: str = None
-    cache_version: str = "0.8"
+    cache_version: str = "0.8.1"
     docker_image: List[Dict[str, str]] = None
     install_steps: List[str] = None
     marker: Optional[str] = None
