c2c770967f14530e61a3db7064b960470b6ff8e3
Arthur Zucker <arthur.zucker@gmail.com>
2024-02-19 14:08:00 +0100

    Let's start working on the CIs

--- src/transformers/models/mamba/configuration_mamba.py
+++ src/transformers/models/mamba/configuration_mamba.py
@@ -128,6 +128,6 @@ class MambaConfig(PretrainedConfig):
         self.residual_in_fp32 = residual_in_fp32
         self.tie_word_embeddings = tie_word_embeddings
         self.dt_rank = dt_rank
-        self.use_cache=use_cache
+        self.use_cache = use_cache
 
         super().__init__(bos_token_id=bos_token_id, eos_token_id=eos_token_id, **kwargs)
