0bb9376f8af65bf3a69c3d2aeb8b96b1ca802789
Merve Noyan <merveenoyan@gmail.com>
2024-02-22 13:13:28 +0300

    Update tests/models/convnextv2/test_modeling_convnextv2.py
    Co-authored-by: amyeroberts <22614925+amyeroberts@users.noreply.github.com>

--- tests/models/convnextv2/test_modeling_convnextv2.py
+++ tests/models/convnextv2/test_modeling_convnextv2.py
@@ -19,10 +19,7 @@ import unittest
 
 from transformers import ConvNextV2Config
 from transformers.models.auto import get_values
-from transformers.models.auto.modeling_auto import (
-    MODEL_FOR_BACKBONE_MAPPING_NAMES,
-    MODEL_MAPPING_NAMES,
-)
+from transformers.models.auto.modeling_auto import MODEL_FOR_BACKBONE_MAPPING_NAMES, MODEL_MAPPING_NAMES
 from transformers.testing_utils import require_torch, require_vision, slow, torch_device
 from transformers.utils import cached_property, is_torch_available, is_vision_available
 
