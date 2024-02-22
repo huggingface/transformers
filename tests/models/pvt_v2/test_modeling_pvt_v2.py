a64e7795c6727ffa24ac5e386d13bcb42cc16966
FoamoftheSea <indezera@gmail.com>
2024-02-21 17:28:26 -0800

    Fixed outdated decorators on test_inference_fp16

--- tests/models/pvt_v2/test_modeling_pvt_v2.py
+++ tests/models/pvt_v2/test_modeling_pvt_v2.py
@@ -24,9 +24,10 @@ from transformers.models.auto import get_values
 from transformers.testing_utils import (
     require_accelerate,
     require_torch,
-    require_torch_gpu,
     slow,
     torch_device,
+    require_torch_accelerator,
+    require_torch_fp16,
 )
 
 from ...test_backbone_common import BackboneTesterMixin
@@ -364,7 +365,8 @@ class PvtV2ModelIntegrationTest(unittest.TestCase):
 
     @slow
     @require_accelerate
-    @require_torch_gpu
+    @require_torch_accelerator
+    @require_torch_fp16
     def test_inference_fp16(self):
         r"""
         A small test to make sure that inference work in half precision without any problem.
