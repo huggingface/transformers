a6e099298f86672028bcf9d5c496db74c5cdba1a
younesbelkada <younesbelkada@gmail.com>
2024-02-23 03:14:41 +0000

    fix copyright

--- src/transformers/models/starcoder2/modeling_starcoder2.py
+++ src/transformers/models/starcoder2/modeling_starcoder2.py
@@ -1,5 +1,5 @@
 # coding=utf-8
-# Copyright 2024 Starcoder2 AI and the HuggingFace Inc. team. All rights reserved.
+# Copyright 2024 BigCode and the HuggingFace Inc. team. All rights reserved.
 #
 # This code is based on EleutherAI's GPT-NeoX library and the GPT-NeoX
 # and OPT implementations in this library. It has been modified from its
