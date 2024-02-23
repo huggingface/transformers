232da2a1e71427dd01b1a66aeeff4e939e7576b5
Joao Gante <joao@huggingface.co>
2024-02-23 19:36:27 +0000

    bc nit

--- src/transformers/models/llama/modeling_llama.py
+++ src/transformers/models/llama/modeling_llama.py
@@ -1254,7 +1254,7 @@ class LlamaForCausalLM(LlamaPreTrainedModel):
         if attention_mask is not None and position_ids is None:
             # create position_ids on the fly for batch generation
             position_ids = attention_mask.long().cumsum(-1) - 1
-            position_ids.masked_fill_(attention_mask == 0, 0)
+            position_ids.masked_fill_(attention_mask == 0, 1)
             if past_key_values:
                 position_ids = position_ids[:, -input_ids.shape[1] :]
 
