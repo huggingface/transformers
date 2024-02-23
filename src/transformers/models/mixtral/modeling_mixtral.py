7ee1e517a72c9cafc9b0ab0c816334b618f6f37c
Leonardo Emili <leonardo.emili3@gmail.com>
2024-02-23 14:38:54 +0000

    Set output_router_logits=False in prepare_inputs_for_generation for mixtral

--- src/transformers/models/mixtral/modeling_mixtral.py
+++ src/transformers/models/mixtral/modeling_mixtral.py
@@ -1467,6 +1467,7 @@ class MixtralForCausalLM(MixtralPreTrainedModel):
                 "past_key_values": past_key_values,
                 "use_cache": kwargs.get("use_cache"),
                 "attention_mask": attention_mask,
+                "output_router_logits": False,
             }
         )
         return model_inputs
