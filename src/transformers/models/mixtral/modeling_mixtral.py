8b84d6858794454a769ac4d2d95f846795af5a07
ehuaa <ehuamail@163.com>
2024-02-24 14:10:47 +0800

    revert non-contigous tensor modification

--- src/transformers/models/mixtral/modeling_mixtral.py
+++ src/transformers/models/mixtral/modeling_mixtral.py
@@ -761,11 +761,7 @@ class MixtralSdpaAttention(MixtralAttention):
 
         # SDPA with memory-efficient backend is currently (torch==2.1.2) bugged with non-contiguous inputs with custom attn_mask,
         # Reference: https://github.com/pytorch/pytorch/issues/112577.
-        if (
-            not _is_torch_version_greater_or_equal_than_2_2_0
-            and query_states.device.type == "cuda"
-            and attention_mask is not None
-        ):
+        if query_states.device.type == "cuda" and attention_mask is not None:
             query_states = query_states.contiguous()
             key_states = key_states.contiguous()
             value_states = value_states.contiguous()
