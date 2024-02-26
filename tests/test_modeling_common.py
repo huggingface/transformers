8b150596490406c38150eff207fcc48f56b69073
Raushan Turganbay <raushan.turganbay@alumni.nu.edu.kz>
2024-02-27 00:27:51 +0500

    Update tests/test_modeling_common.py
    Co-authored-by: Joao Gante <joaofranciscocardosogante@gmail.com>

--- tests/test_modeling_common.py
+++ tests/test_modeling_common.py
@@ -716,7 +716,7 @@ class ModelTesterMixin:
                 return
             else:
                 # indexing the first element does not always work
-                # e.g. models that output similarity scores pf size (N, M) would need to index [0, 0]
+                # e.g. models that output similarity scores of size (N, M) would need to index [0, 0]
                 slice_ids = [slice(0, index) for index in single_row_object.shape]
                 batched_row = batched_object[slice_ids]
                 self.assertFalse(
