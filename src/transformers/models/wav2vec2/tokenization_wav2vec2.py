faa316a36d0167203c5be13189dcfa284d36b8b0
Matt <matt@rtzr.ai>
2024-02-27 15:18:38 +0900

    Fix skip_special_tokens process for Wav2Vec2CTCTokenizer._decode

--- src/transformers/models/wav2vec2/tokenization_wav2vec2.py
+++ src/transformers/models/wav2vec2/tokenization_wav2vec2.py
@@ -895,7 +895,7 @@ class Wav2Vec2Tokenizer(PreTrainedTokenizer):
 
         result = []
         for token in filtered_tokens:
-            if skip_special_tokens and token in self.all_special_ids:
+            if skip_special_tokens and (token in self.all_special_ids or token in self.all_special_tokens):
                 continue
             result.append(token)
 
