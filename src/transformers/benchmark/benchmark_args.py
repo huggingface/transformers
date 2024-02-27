3f37d19a2d7360d112044540d4f71a93e0708043
Lin, Fanli <fanli.lin@intel.com>
2024-02-26 22:17:43 -0800

    fix style

--- src/transformers/benchmark/benchmark_args.py
+++ src/transformers/benchmark/benchmark_args.py
@@ -17,7 +17,14 @@
 from dataclasses import dataclass, field
 from typing import Tuple
 
-from ..utils import cached_property, is_torch_available, is_torch_tpu_available, is_torch_xpu_available, logging, requires_backends
+from ..utils import (
+    cached_property,
+    is_torch_available,
+    is_torch_tpu_available,
+    is_torch_xpu_available,
+    logging,
+    requires_backends,
+)
 from .benchmark_args_utils import BenchmarkArguments
 
 
