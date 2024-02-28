5b2dea1bf026da15fa88b50f5b3c36c3d902205f
jiqing-feng <107918818+jiqing-feng@users.noreply.github.com>
2024-02-28 22:56:25 +0800

    check if position_ids exists before using it (#29306)
    Co-authored-by: Joao Gante <joao@huggingface.co>

--- src/transformers/models/gemma/modeling_gemma.py
+++ src/transformers/models/gemma/modeling_gemma.py
@@ -1168,7 +1168,9 @@ class GemmaForCausalLM(GemmaPreTrainedModel):
 
         # TODO @gante we should only keep a `cache_position` in generate, and do +=1.
         # same goes for position ids. Could also help with continued generation.
-        cache_position = torch.arange(past_length, past_length + position_ids.shape[-1], device=position_ids.device)
+        input_length = position_ids.shape[-1] if position_ids is not None else input_ids.shape[-1]
+        cache_position = torch.arange(past_length, past_length + input_length, device=input_ids.device)
+        position_ids = position_ids.contiguous() if position_ids is not None else None
 
         # if `inputs_embeds` are passed, we only want to use them in the 1st generation step
         if inputs_embeds is not None and past_key_values is None:
@@ -1181,7 +1183,7 @@ class GemmaForCausalLM(GemmaPreTrainedModel):
 
         model_inputs.update(
             {
-                "position_ids": position_ids.contiguous(),
+                "position_ids": position_ids,
                 "cache_position": cache_position,
                 "past_key_values": past_key_values,
                 "use_cache": kwargs.get("use_cache"),
