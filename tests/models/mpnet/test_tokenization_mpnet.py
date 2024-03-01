8d3d377d13ac9a539acb82600bdc1d7222a348dc
ydshieh <ydshieh@users.noreply.github.com>
2024-03-01 17:32:53 +0800

    fix mpent cache

--- tests/models/mpnet/test_tokenization_mpnet.py
+++ tests/models/mpnet/test_tokenization_mpnet.py
@@ -13,6 +13,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import shutil
+shutil.rmtree('/mnt/cache/hub/models--microsoft--mpnet-base', ignore_errors=True)
+
 
 import os
 import unittest
