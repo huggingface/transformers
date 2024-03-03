51c8efdef84b89f8198c522bf47b62ec74d8435e
Glen Taggart <glemtaggart@gmail.com>
2024-03-02 20:46:11 -0800

    try to fix gemma mem use

--- src/transformers/models/gemma/modeling_gemma.py
+++ src/transformers/models/gemma/modeling_gemma.py
@@ -971,9 +971,10 @@ class GemmaModel(GemmaPreTrainedModel):
 
         # We use the current dtype to avoid any overflows
         min_dtype = torch.finfo(dtype).min
-        causal_mask = self.causal_mask[None, None, :, :].repeat(batch_size, 1, 1, 1).to(dtype) * min_dtype
+        # causal_mask = self.causal_mask[None, None, :, :].repeat(batch_size, 1, 1, 1).to(dtype) * min_dtype
 
-        causal_mask = causal_mask.to(dtype=dtype, device=device)
+        # causal_mask = causal_mask.to(dtype=dtype, device=device)
+        causal_mask = (min_dtype * self.causal_mask[None, None, :, :]).to(dtype=dtype, device=device).expand(batch_size, 1, -1, -1)
         if attention_mask is not None and attention_mask.dim() == 2:
             mask_length = attention_mask.shape[-1]
             padding_mask = causal_mask[..., :mask_length].eq(0.0) * attention_mask[:, None, None, :].eq(0.0)
