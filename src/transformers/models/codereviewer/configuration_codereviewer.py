8d198c077d4d19392b8fb05d4d8d570fe5b0d5f5
fasterinnerlooper <shafiq.jetha@outlook.com>
2024-03-03 14:14:08 -0700

    fix error

--- src/transformers/models/codereviewer/configuration_codereviewer.py
+++ src/transformers/models/codereviewer/configuration_codereviewer.py
@@ -52,22 +52,22 @@ class CodeReviewerConfig(PretrainedConfig):
             Number of hidden layers in the Transformer encoder.
         num_decoder_layers (`int`, *optional*):
             Number of hidden layers in the Transformer decoder. Will use the same value as `num_layers` if not set.
+        num_heads (`int`, *optional*, defaults to 8):
+            Number of attention heads for each attention layer in the Transformer encoder.
         relative_attention_num_buckets (`int`, *optional*, defaults to 32):
             The number of buckets to use for each attention layer.
         relative_attention_max_distance (`int`, *optional*, defaults to 128):
             The maximum distance of the longer sequences for the bucket separation.
         dropout_rate (`float`, *optional*, defaults to 0.1):
             The ratio for all dropout layers.
-        layer_norm_epsilon (`float`, *optional*, defaults to 1e-06):
+        layer_norm_epsilon (`float`, *optional*, defaults to 1e-05):
             The epsilon to use in the layer normalization layers.
         initializer_factor (`float`, *optional*, defaults to 1.0):
             A factor for initializing all weight matrices (should be kept to 1, used internally for initialization
             testing).
-        is_encoder_decoder (`<fill_type>`, *optional*, defaults to `True`): <fill_docstring>
         use_cache (`bool`, *optional*, defaults to `True`):
             Whether or not the model should return the last key/values attentions (not used by all models).
-        pad_token_id (`<fill_type>`, *optional*, defaults to 0): <fill_docstring>
-        eos_token_id (`int`, *optional*, defaults to 1):
+        eos_token_id (`int`, *optional*, defaults to 50256):
             End of stream token id.
         classifier_dropout (`float`, *optional*, defaults to 0.0):
             The dropout ratio for classifier.
@@ -85,6 +85,7 @@ class CodeReviewerConfig(PretrainedConfig):
         d_ff=2048,
         num_layers=6,
         num_decoder_layers=None,
+        num_heads=8,
         relative_attention_num_buckets=32,
         relative_attention_max_distance=128,
         dropout_rate=0.1,
