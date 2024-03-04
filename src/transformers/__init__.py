1d287abc97c63f828704a9448f541de51a5169b1
NielsRogge <48327001+NielsRogge@users.noreply.github.com>
2024-03-04 18:49:02 +0100

    Add UDOP (#22940)
    * First draft
    
    * More improvements
    
    * More improvements
    
    * More fixes
    
    * Fix copies
    
    * More improvements
    
    * More fixes
    
    * More improvements
    
    * Convert checkpoint
    
    * More improvements, set up tests
    
    * Fix more tests
    
    * Add UdopModel
    
    * More improvements
    
    * Fix equivalence test
    
    * More fixes
    
    * Redesign model
    
    * Extend conversion script
    
    * Use real inputs for conversion script
    
    * Add image processor
    
    * Improve conversion script
    
    * Add UdopTokenizer
    
    * Add fast tokenizer
    
    * Add converter
    
    * Update README's
    
    * Add processor
    
    * Add fully fledged tokenizer
    
    * Add fast tokenizer
    
    * Use processor in conversion script
    
    * Add tokenizer tests
    
    * Fix one more test
    
    * Fix more tests
    
    * Fix tokenizer tests
    
    * Enable fast tokenizer tests
    
    * Fix more tests
    
    * Fix additional_special_tokens of fast tokenizer
    
    * Fix tokenizer tests
    
    * Fix more tests
    
    * Fix equivalence test
    
    * Rename image to pixel_values
    
    * Rename seg_data to bbox
    
    * More renamings
    
    * Remove vis_special_token
    
    * More improvements
    
    * Add docs
    
    * Fix copied from
    
    * Update slow tokenizer
    
    * Update fast tokenizer design
    
    * Make text input optional
    
    * Add first draft of processor tests
    
    * Fix more processor tests
    
    * Fix decoder_start_token_id
    
    * Fix test_initialization
    
    * Add integration test
    
    * More improvements
    
    * Improve processor, add test
    
    * Add more copied from
    
    * Add more copied from
    
    * Add more copied from
    
    * Add more copied from
    
    * Remove print statement
    
    * Update README and auto mapping
    
    * Delete files
    
    * Delete another file
    
    * Remove code
    
    * Fix test
    
    * Fix docs
    
    * Remove asserts
    
    * Add doc tests
    
    * Include UDOP in exotic model tests
    
    * Add expected tesseract decodings
    
    * Add sentencepiece
    
    * Use same design as T5
    
    * Add UdopEncoderModel
    
    * Add UdopEncoderModel to tests
    
    * More fixes
    
    * Fix fast tokenizer
    
    * Fix one more test
    
    * Remove parallelisable attribute
    
    * Fix copies
    
    * Remove legacy file
    
    * Copy from T5Tokenizer
    
    * Fix rebase
    
    * More fixes, copy from T5
    
    * More fixes
    
    * Fix init
    
    * Use ArthurZ/udop for tests
    
    * Make all model tests pass
    
    * Remove UdopForConditionalGeneration from auto mapping
    
    * Fix more tests
    
    * fixups
    
    * more fixups
    
    * fix the tokenizers
    
    * remove un-necessary changes
    
    * nits
    
    * nits
    
    * replace truncate_sequences_boxes with truncate_sequences for fix-copies
    
    * nit current path
    
    * add a test for input ids
    
    * ids that we should get taken from c9f7a32f57440d90ff79890270d376a1cc0acb68
    
    * nits converting
    
    * nits
    
    * apply ruff
    
    * nits
    
    * nits
    
    * style
    
    * fix slow order of addition
    
    * fix udop fast range as well
    
    * fixup
    
    * nits
    
    * Add docstrings
    
    * Fix gradient checkpointing
    
    * Update code examples
    
    * Skip tests
    
    * Update integration test
    
    * Address comment
    
    * Make fixup
    
    * Remove extra ids from tokenizer
    
    * Skip test
    
    * Apply suggestions from code review
    
    Co-authored-by: Arthur <48595927+ArthurZucker@users.noreply.github.com>
    
    * Update year
    
    * Address comment
    
    * Address more comments
    
    * Address comments
    
    * Add copied from
    
    * Update CI
    
    * Rename script
    
    * Update model id
    
    * Add AddedToken, skip tests
    
    * Update CI
    
    * Fix doc tests
    
    * Do not use Tesseract for the doc tests
    
    * Remove kwargs
    
    * Add original inputs
    
    * Update casting
    
    * Fix doc test
    
    * Update question
    
    * Update question
    
    * Use LayoutLMv3ImageProcessor
    
    * Update organization
    
    * Improve docs
    
    * Update forward signature
    
    * Make images optional
    
    * Remove deprecated device argument
    
    * Add comment, add add_prefix_space
    
    * More improvements
    
    * Remove kwargs
    
    ---------
    
    Co-authored-by: ArthurZucker <arthur.zucker@gmail.com>
    Co-authored-by: Arthur <48595927+ArthurZucker@users.noreply.github.com>

--- src/transformers/__init__.py
+++ src/transformers/__init__.py
@@ -856,6 +856,11 @@ _import_structure = {
         "TvpConfig",
         "TvpProcessor",
     ],
+    "models.udop": [
+        "UDOP_PRETRAINED_CONFIG_ARCHIVE_MAP",
+        "UdopConfig",
+        "UdopProcessor",
+    ],
     "models.umt5": ["UMT5Config"],
     "models.unispeech": [
         "UNISPEECH_PRETRAINED_CONFIG_ARCHIVE_MAP",
@@ -1135,6 +1140,7 @@ else:
     _import_structure["models.speech_to_text"].append("Speech2TextTokenizer")
     _import_structure["models.speecht5"].append("SpeechT5Tokenizer")
     _import_structure["models.t5"].append("T5Tokenizer")
+    _import_structure["models.udop"].append("UdopTokenizer")
     _import_structure["models.xglm"].append("XGLMTokenizer")
     _import_structure["models.xlm_prophetnet"].append("XLMProphetNetTokenizer")
     _import_structure["models.xlm_roberta"].append("XLMRobertaTokenizer")
@@ -1214,6 +1220,7 @@ else:
     _import_structure["models.splinter"].append("SplinterTokenizerFast")
     _import_structure["models.squeezebert"].append("SqueezeBertTokenizerFast")
     _import_structure["models.t5"].append("T5TokenizerFast")
+    _import_structure["models.udop"].append("UdopTokenizerFast")
     _import_structure["models.whisper"].append("WhisperTokenizerFast")
     _import_structure["models.xglm"].append("XGLMTokenizerFast")
     _import_structure["models.xlm_roberta"].append("XLMRobertaTokenizerFast")
@@ -3410,6 +3417,15 @@ else:
             "TvpModel",
             "TvpPreTrainedModel",
         ]
+    )
+    _import_structure["models.udop"].extend(
+        [
+            "UDOP_PRETRAINED_MODEL_ARCHIVE_LIST",
+            "UdopEncoderModel",
+            "UdopForConditionalGeneration",
+            "UdopModel",
+            "UdopPreTrainedModel",
+        ],
     )
     _import_structure["models.umt5"].extend(
         [
@@ -5640,6 +5656,7 @@ if TYPE_CHECKING:
         TvpConfig,
         TvpProcessor,
     )
+    from .models.udop import UDOP_PRETRAINED_CONFIG_ARCHIVE_MAP, UdopConfig, UdopProcessor
     from .models.umt5 import UMT5Config
     from .models.unispeech import (
         UNISPEECH_PRETRAINED_CONFIG_ARCHIVE_MAP,
@@ -5915,6 +5932,7 @@ if TYPE_CHECKING:
         from .models.speech_to_text import Speech2TextTokenizer
         from .models.speecht5 import SpeechT5Tokenizer
         from .models.t5 import T5Tokenizer
+        from .models.udop import UdopTokenizer
         from .models.xglm import XGLMTokenizer
         from .models.xlm_prophetnet import XLMProphetNetTokenizer
         from .models.xlm_roberta import XLMRobertaTokenizer
@@ -5987,6 +6005,7 @@ if TYPE_CHECKING:
         from .models.splinter import SplinterTokenizerFast
         from .models.squeezebert import SqueezeBertTokenizerFast
         from .models.t5 import T5TokenizerFast
+        from .models.udop import UdopTokenizerFast
         from .models.whisper import WhisperTokenizerFast
         from .models.xglm import XGLMTokenizerFast
         from .models.xlm_roberta import XLMRobertaTokenizerFast
@@ -7826,6 +7845,13 @@ if TYPE_CHECKING:
             TvpForVideoGrounding,
             TvpModel,
             TvpPreTrainedModel,
+        )
+        from .models.udop import (
+            UDOP_PRETRAINED_MODEL_ARCHIVE_LIST,
+            UdopEncoderModel,
+            UdopForConditionalGeneration,
+            UdopModel,
+            UdopPreTrainedModel,
         )
         from .models.umt5 import (
             UMT5EncoderModel,
