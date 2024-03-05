4beb1f6837e7e2bee8c5accc127b488ec9d2b82a
steven <steven.bucaillle@gmail.com>
2024-03-05 22:19:29 +0100

    Moved last_hidden_state to be the first returned

--- src/transformers/models/superpoint/modeling_superpoint.py
+++ src/transformers/models/superpoint/modeling_superpoint.py
@@ -89,6 +89,8 @@ class ImagePointDescriptionOutput(ModelOutput):
     and which are padding.
 
     Args:
+        last_hidden_state (`torch.FloatTensor` of shape `(batch_size, sequence_length, hidden_size)`):
+            Sequence of hidden-states at the output of the last layer of the decoder of the model.
         keypoints (`torch.FloatTensor` of shape `(batch_size, num_keypoints, 2)`):
             Relative (x, y) coordinates of predicted keypoints in a given image.
         scores (`torch.FloatTensor` of shape `(batch_size, num_keypoints)`):
@@ -97,8 +99,6 @@ class ImagePointDescriptionOutput(ModelOutput):
             Descriptors of predicted keypoints.
         mask (`torch.BoolTensor` of shape `(batch_size, num_keypoints)`):
             Mask indicating which values in keypoints, scores and descriptors are keypoint information.
-        last_hidden_state (`torch.FloatTensor` of shape `(batch_size, sequence_length, hidden_size)`):
-            Sequence of hidden-states at the output of the last layer of the decoder of the model.
         hidden_states (`tuple(torch.FloatTensor)`, *optional*, returned when `output_hidden_states=True` is passed or
         when `config.output_hidden_states=True`):
             Tuple of `torch.FloatTensor` (one for the output of the embeddings, if the model has an embedding layer, +
@@ -106,11 +106,11 @@ class ImagePointDescriptionOutput(ModelOutput):
             (also called feature maps) of the model at the output of each stage.
     """
 
+    last_hidden_state: torch.FloatTensor = None
     keypoints: Optional[torch.IntTensor] = None
     scores: Optional[torch.FloatTensor] = None
     descriptors: Optional[torch.FloatTensor] = None
     mask: Optional[torch.BoolTensor] = None
-    last_hidden_state: torch.FloatTensor = None
     hidden_states: Optional[Tuple[torch.FloatTensor]] = None
 
 
