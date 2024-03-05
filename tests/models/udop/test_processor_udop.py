75bdb484e62ffabb13a9fb8f81222eaa738c048e
Arthur Zucker <arthur.zucker@gmail.com>
2024-03-05 18:34:55 +0900

    revert with RP

--- tests/models/udop/test_processor_udop.py
+++ tests/models/udop/test_processor_udop.py
@@ -22,6 +22,8 @@ from typing import List
 import numpy as np
 
 from transformers import PreTrainedTokenizer, PreTrainedTokenizerBase, PreTrainedTokenizerFast
+from transformers.models.udop import UdopTokenizer, UdopTokenizerFast
+
 from transformers.testing_utils import (
     require_pytesseract,
     require_sentencepiece,
@@ -34,9 +36,6 @@ from transformers.utils import FEATURE_EXTRACTOR_NAME, cached_property, is_pytes
 
 if is_torch_available():
     import torch
-
-    from transformers import UdopTokenizer, UdopTokenizerFast
-
 
 if is_pytesseract_available():
     from PIL import Image
