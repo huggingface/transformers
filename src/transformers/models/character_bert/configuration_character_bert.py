99f1a9b5cf0c747dfb51e55194ffadb6784dac4e
Hicham EL BOUKKOURI <36409068+helboukkouri@users.noreply.github.com>
2024-03-06 17:04:01 +0100

    Update src/transformers/models/character_bert/configuration_character_bert.py
    Co-authored-by: Arthur <48595927+ArthurZucker@users.noreply.github.com>

--- src/transformers/models/character_bert/configuration_character_bert.py
+++ src/transformers/models/character_bert/configuration_character_bert.py
@@ -174,17 +174,3 @@ class CharacterBertConfig(PretrainedConfig):
         self.classifier_dropout = classifier_dropout
 
 
-class CharacterBertOnnxConfig(OnnxConfig):
-    @property
-    def inputs(self) -> Mapping[str, Mapping[int, str]]:
-        if self.task == "multiple-choice":
-            dynamic_axis = {0: "batch", 1: "choice", 2: "sequence"}
-        else:
-            dynamic_axis = {0: "batch", 1: "sequence"}
-        return OrderedDict(
-            [
-                ("input_ids", dynamic_axis),
-                ("attention_mask", dynamic_axis),
-                ("token_type_ids", dynamic_axis),
-            ]
-        )
