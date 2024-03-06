7c126f6d61bf0623dbc69742ec078560210d5ecf
younesbelkada <younesbelkada@gmail.com>
2024-03-06 05:51:36 +0000

    attempt to fix failing tests

--- src/transformers/models/llama/modeling_llama.py
+++ src/transformers/models/llama/modeling_llama.py
@@ -55,7 +55,6 @@ if is_flash_attn_2_available():
     from flash_attn.bert_padding import index_first_axis, pad_input, unpad_input  # noqa
 
 
-
 logger = logging.get_logger(__name__)
 
 _CONFIG_FOR_DOC = "LlamaConfig"
