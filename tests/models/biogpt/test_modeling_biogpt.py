6b5262569cedc49c446ba7b0af223f34b3aa9a62
Lin, Fanli <fanli.lin@intel.com>
2024-03-07 12:16:33 -0500

    make less skipped

--- tests/models/biogpt/test_modeling_biogpt.py
+++ tests/models/biogpt/test_modeling_biogpt.py
@@ -17,8 +17,8 @@
 import math
 import unittest
 
-from transformers import BioGptConfig, is_torch_available
-from transformers.testing_utils import require_sacremoses, require_torch, slow, torch_device
+from transformers import BioGptConfig, is_sacremoses_available, is_torch_available
+from transformers.testing_utils import require_torch, slow, torch_device
 
 from ...generation.test_utils import GenerationTesterMixin
 from ...test_configuration_common import ConfigTester
@@ -278,7 +278,6 @@ class BioGptModelTester:
         return config, inputs_dict
 
 
-@require_sacremoses
 @require_torch
 class BioGptModelTest(ModelTesterMixin, GenerationTesterMixin, PipelineTesterMixin, unittest.TestCase):
     all_model_classes = (
@@ -295,7 +294,7 @@ class BioGptModelTest(ModelTesterMixin, GenerationTesterMixin, PipelineTesterMix
             "token-classification": BioGptForTokenClassification,
             "zero-shot": BioGptForSequenceClassification,
         }
-        if is_torch_available()
+        if is_torch_available() and is_sacremoses_available()
         else {}
     )
     test_pruning = False
