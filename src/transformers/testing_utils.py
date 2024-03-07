e4e32e07cad61669b43f9436eb6cd7559953d85b
Lin, Fanli <fanli.lin@intel.com>
2024-03-06 21:31:58 -0800

    add CUDA as well

--- src/transformers/testing_utils.py
+++ src/transformers/testing_utils.py
@@ -800,6 +800,10 @@ if is_torch_available():
 
     if "TRANSFORMERS_TEST_DEVICE" in os.environ:
         torch_device = os.environ["TRANSFORMERS_TEST_DEVICE"]
+        if torch_device == "cuda" and not torch.cuda.is_available():
+            raise ValueError(
+                f"TRANSFORMERS_TEST_DEVICE={torch_device}, but CUDA is unavailable. Please double-check your testing environment."
+            )
         if torch_device == "xpu" and not is_torch_xpu_available():
             raise ValueError(
                 f"TRANSFORMERS_TEST_DEVICE={torch_device}, but XPU is unavailable. Please double-check your testing environment."
