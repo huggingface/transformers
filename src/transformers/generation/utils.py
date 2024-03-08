ce093c109eb92334ff375350c0c7f24b6e39253d
Raushan Turganbay <raushan.turganbay@alumni.nu.edu.kz>
2024-03-08 18:34:30 +0500

    Update src/transformers/generation/utils.py
    Co-authored-by: Joao Gante <joaofranciscocardosogante@gmail.com>

--- src/transformers/generation/utils.py
+++ src/transformers/generation/utils.py
@@ -4845,7 +4845,7 @@ def _speculative_sampling(
         valid_tokens = new_candidate_input_ids[:, : n_matches + 1]
     else:
         # Next token selection: if there is a rejection, adjust the distribution from the main model before sampling.
-        gamma = min(candidate_logits.shape[1], n_matches)
+        gamma = candidate_logits.shape[1]
         p_n_plus_1 = p[:, n_matches, :]
         if n_matches < gamma:
             q_n_plus_1 = q[:, n_matches, :]
