c147fb229c55e02d318c47e489e551819d30fa05
a8nova <a8nova@gmail.com>
2024-03-09 22:43:24 -0800

    init stuff for importing tf version

--- src/transformers/__init__.py
+++ src/transformers/__init__.py
@@ -4286,6 +4286,15 @@ else:
             "TFFunnelPreTrainedModel",
         ]
     )
+    _import_structure["models.gemma"].extend(
+        [
+            "TFGemmaForCausalLM",
+            "TFGemmaForSequenceClassification",
+            "TFGemmaModel",
+            "TFGemmaPreTrainedModel",
+        ]
+    )
+
     _import_structure["models.gpt2"].extend(
         [
             "TF_GPT2_PRETRAINED_MODEL_ARCHIVE_LIST",
@@ -8736,6 +8745,12 @@ if TYPE_CHECKING:
             TFFunnelForTokenClassification,
             TFFunnelModel,
             TFFunnelPreTrainedModel,
+        )
+        from .models.gemma import (
+            TFGemmaForCausalLM,
+            TFGemmaForSequenceClassification,
+            TFGemmaModel,
+            TFGemmaPreTrainedModel,
         )
         from .models.gpt2 import (
             TF_GPT2_PRETRAINED_MODEL_ARCHIVE_LIST,
