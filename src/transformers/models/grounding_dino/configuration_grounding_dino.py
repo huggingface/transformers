538e88f83b217327d42bd65ced809ea5d6d20fca
Eduardo Pacheco <eduardo.pach@hotmail.com>
2024-03-10 19:17:16 +0100

    Fix copies

--- src/transformers/models/grounding_dino/configuration_grounding_dino.py
+++ src/transformers/models/grounding_dino/configuration_grounding_dino.py
@@ -165,9 +165,9 @@ class GroundingDinoConfig(PretrainedConfig):
             Name of backbone to use when `backbone_config` is `None`. If `use_pretrained_backbone` is `True`, this
             will load the corresponding pretrained weights from the timm or transformers library. If `use_pretrained_backbone`
             is `False`, this loads the backbone's config and uses that to initialize the backbone with random weights.
-        use_pretrained_backbone (`bool`, *optional*, `False`):
+        use_pretrained_backbone (`bool`, *optional*, defaults to `False`):
             Whether to use pretrained weights for the backbone.
-        use_timm_backbone (`bool`, *optional*, `False`):
+        use_timm_backbone (`bool`, *optional*, defaults to `False`):
             Whether to load `backbone` from the timm library. If `False`, the backbone is loaded from the transformers
             library.
         backbone_kwargs (`dict`, *optional*):
