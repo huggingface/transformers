c35924bdd75f4c9246bea6f5c6c8f5d1dcad9f00
ydshieh <ydshieh@users.noreply.github.com>
2024-03-12 15:39:36 +0100

    update

--- tests/models/flava/test_modeling_flava.py
+++ tests/models/flava/test_modeling_flava.py
@@ -1341,7 +1341,7 @@ class FlavaForPreTrainingIntegrationTest(unittest.TestCase):
         self.assertTrue(torch.allclose(outputs.contrastive_logits_per_image, expected_logits, atol=1e-3))
         self.assertAlmostEqual(outputs.loss_info.mmm_text.item(), 2.0727925, places=4)
         self.assertAlmostEqual(outputs.loss_info.mmm_image.item(), 7.0282096, places=4)
-        self.assertAlmostEqual(outputs.loss.item(), 11.37761, places=4)
+        self.assertAlmostEqual(outputs.loss.item(), 11.3792324, places=4)
 
     @slow
     def test_inference_with_itm_labels(self):
@@ -1392,4 +1392,4 @@ class FlavaForPreTrainingIntegrationTest(unittest.TestCase):
         self.assertTrue(torch.allclose(outputs.contrastive_logits_per_image, expected_logits, atol=1e-3))
         self.assertAlmostEqual(outputs.loss_info.mmm_text.item(), 2.0727925, places=4)
         self.assertAlmostEqual(outputs.loss_info.mmm_image.item(), 6.8965902, places=4)
-        self.assertAlmostEqual(outputs.loss.item(), 9.6090, places=4)
+        self.assertAlmostEqual(outputs.loss.item(), 9.6084213, places=4)
