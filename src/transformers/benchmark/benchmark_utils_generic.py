37e178847a847d500c09477d62a08d46cafa5d1e
Yih-Dar <2521628+ydshieh@users.noreply.github.com>
2024-03-13 19:38:48 +0100

    Update src/transformers/benchmark/benchmark_utils_generic.py
    Co-authored-by: amyeroberts <22614925+amyeroberts@users.noreply.github.com>

--- src/transformers/benchmark/benchmark_utils_generic.py
+++ src/transformers/benchmark/benchmark_utils_generic.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """
-Benchmark
+Benchmark utilities - module containing base classes for benchmarking
 """
 import json
 import timeit
