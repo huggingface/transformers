2bf324b79a15c6e05ded60a0ed08d6a07c19ec8a
Zach Mueller <muellerzr@gmail.com>
2024-03-15 01:52:18 -0400

    Update docstring

--- src/transformers/trainer_callback.py
+++ src/transformers/trainer_callback.py
@@ -156,7 +156,7 @@ class ExportableState:
     Example:
 
     ```python
-    class EarlyStoppingCallback(StatefulTrainerCallback):
+    class EarlyStoppingCallback(TrainerCallback, ExportableState):
         def __init__(self, early_stopping_patience: int = 1, early_stopping_threshold: Optional[float] = 0.0):
             self.early_stopping_patience = early_stopping_patience
             self.early_stopping_threshold = early_stopping_threshold
