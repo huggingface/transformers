3c7ffdfda3706349236914051992a6f746aa48b0
bhack <bhack@users.noreply.github.com>
2024-03-15 09:15:06 +0000

    Revert "Restore package_exists"
    This reverts commit 66fd2cd5c33d1b9a26a8f3e8adef2e6ec1214868.

--- src/transformers/utils/import_utils.py
+++ src/transformers/utils/import_utils.py
@@ -46,7 +46,6 @@ def _is_package_available(pkg_name: str, return_version: bool = False) -> Union[
         try:
             # Primary method to get the package version
             package_version = importlib.metadata.version(pkg_name)
-            package_exists = True
         except importlib.metadata.PackageNotFoundError:
             # Fallback method: Only for "torch" and versions containing "dev"
             if pkg_name == "torch":
