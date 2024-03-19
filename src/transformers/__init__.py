3f84b1a029314cde16fdd13c26cf53cdbcec7c82
StevenBucaille <steven.bucaille@gmail.com>
2024-03-19 15:43:02 +0100

    Implementation of SuperPoint and AutoModelForKeypointDetection (#28966)
    * Added SuperPoint docs
    
    * Added tests
    
    * Removed commented part
    
    * Commit to create and fix add_superpoint branch with a new branch
    
    * Fixed dummy_pt_objects
    
    * Committed missing files
    
    * Fixed README.md
    
    * Apply suggestions from code review
    
    Fixed small changes
    
    Co-authored-by: amyeroberts <22614925+amyeroberts@users.noreply.github.com>
    
    * Moved ImagePointDescriptionOutput from modeling_outputs.py to modeling_superpoint.py
    
    * Removed AutoModelForKeypointDetection and related stuff
    
    * Fixed inconsistencies in image_processing_superpoint.py
    
    * Moved infer_on_model logic simply in test_inference
    
    * Fixed bugs, added labels to forward method with checks whether it is properly a None value, also added tests about this logic in test_modeling_superpoint.py
    
    * Added tests to SuperPointImageProcessor to ensure that images are properly converted to grayscale
    
    * Removed remaining mentions of MODEL_FOR_KEYPOINT_DETECTION_MAPPING
    
    * Apply suggestions from code review
    
    Co-authored-by: amyeroberts <22614925+amyeroberts@users.noreply.github.com>
    
    * Fixed from (w, h) to (h, w) as input for tests
    
    * Removed unnecessary condition
    
    * Moved last_hidden_state to be the first returned
    
    * Moved last_hidden_state to be the first returned (bis)
    
    * Moved last_hidden_state to be the first returned (ter)
    
    * Switched image_width and image_height in tests to match recent changes
    
    * Added config as first SuperPointConvBlock init argument
    
    * Reordered README's after merge
    
    * Added missing first config argument to SuperPointConvBlock instantiations
    
    * Removed formatting error
    
    * Added SuperPoint to README's de, pt-br, ru, te and vi
    
    * Checked out README_fr.md
    
    * Fixed README_fr.md
    
    * Test fix README_fr.md
    
    * Test fix README_fr.md
    
    * Last make fix-copies !
    
    * Updated checkpoint path
    
    * Removed unused SuperPoint doc
    
    * Added missing image
    
    * Update src/transformers/models/superpoint/modeling_superpoint.py
    
    Co-authored-by: amyeroberts <22614925+amyeroberts@users.noreply.github.com>
    
    * Removed unnecessary import
    
    * Update src/transformers/models/superpoint/modeling_superpoint.py
    
    Co-authored-by: amyeroberts <22614925+amyeroberts@users.noreply.github.com>
    
    * Added SuperPoint to _toctree.yml
    
    ---------
    
    Co-authored-by: steven <steven.bucaillle@gmail.com>
    Co-authored-by: amyeroberts <22614925+amyeroberts@users.noreply.github.com>
    Co-authored-by: Steven Bucaille <steven.bucaille@buawei.com>

--- src/transformers/__init__.py
+++ src/transformers/__init__.py
@@ -819,6 +819,7 @@ _import_structure = {
     ],
     "models.stablelm": ["STABLELM_PRETRAINED_CONFIG_ARCHIVE_MAP", "StableLmConfig"],
     "models.starcoder2": ["STARCODER2_PRETRAINED_CONFIG_ARCHIVE_MAP", "Starcoder2Config"],
+    "models.superpoint": ["SUPERPOINT_PRETRAINED_CONFIG_ARCHIVE_MAP", "SuperPointConfig"],
     "models.swiftformer": [
         "SWIFTFORMER_PRETRAINED_CONFIG_ARCHIVE_MAP",
         "SwiftFormerConfig",
@@ -1339,6 +1340,7 @@ else:
     _import_structure["models.segformer"].extend(["SegformerFeatureExtractor", "SegformerImageProcessor"])
     _import_structure["models.seggpt"].extend(["SegGptImageProcessor"])
     _import_structure["models.siglip"].append("SiglipImageProcessor")
+    _import_structure["models.superpoint"].extend(["SuperPointImageProcessor"])
     _import_structure["models.swin2sr"].append("Swin2SRImageProcessor")
     _import_structure["models.tvlt"].append("TvltImageProcessor")
     _import_structure["models.tvp"].append("TvpImageProcessor")
@@ -3334,6 +3336,13 @@ else:
             "Starcoder2ForSequenceClassification",
             "Starcoder2Model",
             "Starcoder2PreTrainedModel",
+        ]
+    )
+    _import_structure["models.superpoint"].extend(
+        [
+            "SUPERPOINT_PRETRAINED_MODEL_ARCHIVE_LIST",
+            "SuperPointModel",
+            "SuperPointPreTrainedModel",
         ]
     )
     _import_structure["models.swiftformer"].extend(
@@ -5670,6 +5679,7 @@ if TYPE_CHECKING:
     )
     from .models.stablelm import STABLELM_PRETRAINED_CONFIG_ARCHIVE_MAP, StableLmConfig
     from .models.starcoder2 import STARCODER2_PRETRAINED_CONFIG_ARCHIVE_MAP, Starcoder2Config
+    from .models.superpoint import SUPERPOINT_PRETRAINED_CONFIG_ARCHIVE_MAP, SuperPointConfig
     from .models.swiftformer import (
         SWIFTFORMER_PRETRAINED_CONFIG_ARCHIVE_MAP,
         SwiftFormerConfig,
@@ -6182,6 +6192,7 @@ if TYPE_CHECKING:
         from .models.segformer import SegformerFeatureExtractor, SegformerImageProcessor
         from .models.seggpt import SegGptImageProcessor
         from .models.siglip import SiglipImageProcessor
+        from .models.superpoint import SuperPointImageProcessor
         from .models.swin2sr import Swin2SRImageProcessor
         from .models.tvlt import TvltImageProcessor
         from .models.tvp import TvpImageProcessor
@@ -7838,6 +7849,11 @@ if TYPE_CHECKING:
             Starcoder2ForSequenceClassification,
             Starcoder2Model,
             Starcoder2PreTrainedModel,
+        )
+        from .models.superpoint import (
+            SUPERPOINT_PRETRAINED_MODEL_ARCHIVE_LIST,
+            SuperPointModel,
+            SuperPointPreTrainedModel,
         )
         from .models.swiftformer import (
             SWIFTFORMER_PRETRAINED_MODEL_ARCHIVE_LIST,
