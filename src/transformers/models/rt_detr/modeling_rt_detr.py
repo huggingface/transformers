92d800c78a8ec94ba5e98739f6c058413dfe1706
sangbumchoi <danielsejong55@gmail.com>
2024-03-19 08:21:11 +0000

    minor fix

--- src/transformers/models/rt_detr/modeling_rt_detr.py
+++ src/transformers/models/rt_detr/modeling_rt_detr.py
@@ -2228,6 +2228,7 @@ class RTDetrForObjectDetection(RTDetrPreTrainedModel):
     # When using clones, all layers > 0 will be clones, but layer 0 *is* required
     _tied_weights_keys = ["bbox_embed", "class_embed"]
     # We can't initialize the model on meta device as some weights are modified during the initialization
+    _no_split_modules = None
 
     def __init__(self, config: RTDetrConfig):
         super().__init__(config)
