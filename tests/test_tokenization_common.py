aa3f2b2ec0510203f5bbefeca7ac5788732ea724
Arthur Zucker <arthur.zucker@gmail.com>
2024-03-19 19:15:26 +0900

    oups

--- tests/test_tokenization_common.py
+++ tests/test_tokenization_common.py
@@ -202,7 +202,7 @@ class TokenizerTesterMixin:
     def setUp(self) -> None:
         # Tokenizer.filter makes it possible to filter which Tokenizer to case based on all the
         # information available in Tokenizer (name, rust class, python class, vocab key name)
-        self.from_pretrained_id = list(self.from_pretrained_id)
+        self.from_pretrained_id = [self.from_pretrained_id] if isinstance(self.from_pretrained_id, str) else self.from_pretrained_id
 
         self.tokenizers_list = []
         if self.test_rust_tokenizer:
