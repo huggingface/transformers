ba49e89a14502e38319ea31773caa4187f2fbd1b
Zach Mueller <muellerzr@gmail.com>
2024-03-20 11:11:28 -0400

    Clean

--- tests/trainer/test_trainer_seq2seq.py
+++ tests/trainer/test_trainer_seq2seq.py
@@ -22,7 +22,7 @@ from transformers import (
     Seq2SeqTrainingArguments,
     T5Tokenizer,
 )
-from transformers.testing_utils import TestCasePlus, require_torch, slow, require_sentencepiece
+from transformers.testing_utils import TestCasePlus, require_sentencepiece, require_torch, slow
 from transformers.utils import is_datasets_available
 
 
