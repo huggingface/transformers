69eba6c7dd53443ed6739e37e7ddf51a51172732
sangbumchoi <danielsejong55@gmail.com>
2024-03-20 14:35:33 +0000

    set lru_cache with max_size 32

--- src/transformers/models/rt_detr/modeling_rt_detr.py
+++ src/transformers/models/rt_detr/modeling_rt_detr.py
@@ -1656,7 +1656,7 @@ class RTDetrModel(RTDetrPreTrainedModel):
         for param in self.backbone.parameters():
             param.requires_grad_(True)
 
-    @lru_cache()
+    @lru_cache(max_size=32)
     def generate_anchors(self, spatial_shapes=None, grid_size=0.05, dtype=torch.float32, device="cpu"):
         if spatial_shapes is None:
             spatial_shapes = [
