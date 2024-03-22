9df16c5b4a02f80cf6b5f044c98ec1bbdfd9e623
Younes Belkada <younesbelkada@gmail.com>
2024-03-22 10:11:31 +0000

    fix

--- tests/test_modeling_common.py
+++ tests/test_modeling_common.py
@@ -3245,6 +3245,7 @@ class ModelTesterMixin:
     @require_torch_gpu
     @mark.flash_attn_test
     @slow
+    @is_flaky
     def test_flash_attn_2_inference(self):
         for model_class in self.all_model_classes:
             if not model_class._supports_flash_attn_2:
@@ -3340,6 +3341,7 @@ class ModelTesterMixin:
     @require_torch_gpu
     @mark.flash_attn_test
     @slow
+    @is_flaky
     def test_flash_attn_2_inference_padding_right(self):
         for model_class in self.all_model_classes:
             if not model_class._supports_flash_attn_2:
