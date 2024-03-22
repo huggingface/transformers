f4f55a47f94ffcc1a5e71e287553de4d4bbe5c38
Younes Belkada <younesbelkada@gmail.com>
2024-03-22 11:49:00 +0000

    revert change in gemma

--- src/transformers/models/gemma/modeling_gemma.py
+++ src/transformers/models/gemma/modeling_gemma.py
@@ -14,6 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """ PyTorch Gemma model."""
+
 import math
 import warnings
 from typing import List, Optional, Tuple, Union
