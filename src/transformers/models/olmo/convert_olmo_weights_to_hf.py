7cc2a9aa6e392cbf7629d16965d90a9f7cab68c5
Shane A <shanea@allenai.org>
2024-03-26 14:46:49 -0700

    Implement OLMo to HF conversion script

--- src/transformers/models/olmo/convert_olmo_weights_to_hf.py
+++ src/transformers/models/olmo/convert_olmo_weights_to_hf.py
@@ -12,327 +12,164 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import argparse
-import gc
-import json
 import os
 import shutil
-import warnings
+from pathlib import Path
 
-import torch
+import yaml
+from tokenizers import Tokenizer
 
-from transformers import OLMoConfig, OLMoForCausalLM, OLMoTokenizer
+from transformers import OLMoConfig, OLMoForCausalLM, OLMoTokenizerFast
+from transformers.models.olmo.configuration_olmo import ActivationType, BlockType, InitFnType, LayerNormType
 
 
-try:
-    from transformers import OLMoTokenizerFast
-except ImportError as e:
-    warnings.warn(e)
-    warnings.warn(
-        "The converted tokenizer will be the `slow` tokenizer. To use the fast, update your `tokenizers` library and re-run the tokenizer conversion"
-    )
-    OLMoTokenizerFast = None
-
-"""
+USAGE = """
 Sample usage:
 
 ```
 python src/transformers/models/olmo/convert_olmo_weights_to_hf.py \
-    --input_dir /path/to/downloaded/olmo/weights --model_size 7B --output_dir /output/path
+    --input_dir /path/to/downloaded/olmo/weights --output_dir /output/path --tokenizer_json_path /path/to/downloaded/olmo/tokenizer/file.json
 ```
 
 Thereafter, models can be loaded via:
 
 ```py
-from transformers import OLMoForCausalLM, OLMoTokenizer
+from transformers import OLMoForCausalLM, OLMoTokenizerFast
 
 model = OLMoForCausalLM.from_pretrained("/output/path")
-tokenizer = OLMoTokenizer.from_pretrained("/output/path")
+tokenizer = OLMoTokenizerFast.from_pretrained("/output/path")
 ```
 
 Important note: you need to be able to host the whole model in RAM to execute this script (even if the biggest versions
 come in several checkpoints they each contain a part of each weight of the model, so we need to load them all in RAM).
 """
 
-NUM_SHARDS = {
-    "7B": 1,
-    "7Bf": 1,
-    "13B": 2,
-    "13Bf": 2,
-    "34B": 4,
-    "30B": 4,
-    "65B": 8,
-    "70B": 8,
-    "70Bf": 8,
-}
+
+def _get_olmo_config(config_path: Path) -> OLMoConfig:
+    # Default OLMo checkpoint settings. These may not match OLMoConfig, which defaults specifically to OLMo-7B.
+    olmo_config = {
+        "d_model": 768,
+        "n_heads": 12,
+        "n_layers": 12,
+        "mlp_ratio": 4,
+        "mlp_hidden_size": None,
+        "activation_type": ActivationType.swiglu,
+        "block_type": BlockType.sequential,
+        "alibi": False,
+        "alibi_bias_max": 8.0,
+        "rope": False,
+        "rope_full_precision": True,
+        "flash_attention": False,
+        "attention_dropout": 0.1,
+        "multi_query_attention": False,
+        "attention_layer_norm": False,
+        "residual_dropout": 0.1,
+        "embedding_dropout": 0.1,
+        "layer_norm_type": LayerNormType.default,
+        "layer_norm_with_affine": True,
+        "attention_layer_norm_with_affine": True,
+        "max_sequence_length": 1024,
+        "include_bias": True,
+        "bias_for_layer_norm": None,
+        "scale_logits": False,
+        "vocab_size": 50304,
+        "weight_tying": True,
+        "eos_token_id": 50256,
+        "pad_token_id": 50256,
+        "init_device": None,
+        "init_fn": InitFnType.normal,
+        "init_std": 0.02,
+        "init_cutoff_factor": None,
+        "precision": None,
+    }
+
+    olmo_config.update(yaml.safe_load(config_path.read_text())["model"])
+
+    # OLMo considers 'vocab size' (the number of different tokens the tokenizer can produce)
+    # and 'embedding size' (the number of embeddings) to be different and separately configurable.
+    # HF does not. We set `vocab_size` to `embedding_size` and set `embedding_size` to `None`
+    # so that HF OLMo models always have the same vocab and embedding sizes, thus avoiding this problem.
+    if "embedding_size" in olmo_config:
+        olmo_config["vocab_size"] = olmo_config["embedding_size"]
+        del olmo_config["embedding_size"]
+
+    return OLMoConfig(**olmo_config)
 
 
-def compute_intermediate_size(n, ffn_dim_multiplier=1, multiple_of=256):
-    return multiple_of * ((int(ffn_dim_multiplier * int(8 * n / 3)) + multiple_of - 1) // multiple_of)
+def _write_tokenizer(output_path: Path, config: OLMoConfig, input_tokenizer_path: Path) -> None:
+    print(f"Saving a {OLMoTokenizerFast.__name__} to {output_path}.")
 
+    base_tokenizer = Tokenizer.from_file(str(input_tokenizer_path))
 
-def read_json(path):
-    with open(path, "r") as f:
-        return json.load(f)
+    eos_token_id = config.eos_token_id if config.eos_token_id is not None else base_tokenizer.get_vocab_size() - 1
+    pad_token_id = config.pad_token_id if config.pad_token_id is not None else eos_token_id
 
+    tokenizer = OLMoTokenizerFast(
+        tokenizer_object=base_tokenizer,
+        eos_token=base_tokenizer.decode([eos_token_id], skip_special_tokens=False),
+        pad_token=base_tokenizer.decode([pad_token_id], skip_special_tokens=False),
+    )
 
-def write_json(text, path):
-    with open(path, "w") as f:
-        json.dump(text, f)
+    tokenizer.save_pretrained(output_path)
 
 
 def write_model(
-    model_path, input_base_path, model_size, tokenizer_path=None, safe_serialization=True, olmo_version=1
+    output_path: str,
+    input_base_path: str,
+    safe_serialization: bool = True,
+    input_tokenizer_path: str | None = None,
 ):
-    # for backward compatibility, before you needed the repo to be called `my_repo/model_size`
-    if not os.path.isfile(os.path.join(input_base_path, "params.json")):
-        input_base_path = os.path.join(input_base_path, model_size)
+    os.makedirs(output_path, exist_ok=True)
+    tmp_output_path = os.path.join(output_path, "tmp")
+    os.makedirs(tmp_output_path, exist_ok=True)
 
-    os.makedirs(model_path, exist_ok=True)
-    tmp_model_path = os.path.join(model_path, "tmp")
-    os.makedirs(tmp_model_path, exist_ok=True)
+    config_path = Path(input_base_path) / "config.yaml"
+    hf_config = _get_olmo_config(config_path)
 
-    params = read_json(os.path.join(input_base_path, "params.json"))
-    num_shards = NUM_SHARDS[model_size]
-    params = params.get("model", params)
-    n_layers = params["n_layers"]
-    n_heads = params["n_heads"]
-    n_heads_per_shard = n_heads // num_shards
-    dim = params["dim"]
-    dims_per_head = dim // n_heads
-    base = params.get("rope_theta", 10000.0)
-    inv_freq = 1.0 / (base ** (torch.arange(0, dims_per_head, 2).float() / dims_per_head))
-    if base > 10000.0:
-        max_position_embeddings = 16384
-    else:
-        # Depending on the OLMo version, the default max_position_embeddings has different values.
-        if olmo_version == 1:
-            max_position_embeddings = 2048
-        elif olmo_version == 2:
-            max_position_embeddings = 4096
-        else:
-            raise NotImplementedError(
-                f"Version {olmo_version} of olmo is not supported yet. "
-                "Current supported versions of olmo are [1, 2]."
-            )
+    # `PreTrainedModel.from_pretrained` does not automatically initialize meta device tensors, so use cpu instead
+    hf_config.init_device = "cpu" if hf_config.init_device == "meta" else hf_config.init_device
 
-    tokenizer_class = OLMoTokenizer if OLMoTokenizerFast is None else OLMoTokenizerFast
-    if tokenizer_path is not None:
-        tokenizer = tokenizer_class(tokenizer_path)
-        tokenizer.save_pretrained(model_path)
-    vocab_size = tokenizer.vocab_size if tokenizer_path is not None else 32000
+    hf_config.save_pretrained(tmp_output_path)
 
-    if params.get("n_kv_heads", None) is not None:
-        num_key_value_heads = params["n_kv_heads"]  # for GQA / MQA
-        num_local_key_value_heads = n_heads_per_shard // num_key_value_heads
-        key_value_dim = dim // num_key_value_heads
-    else:  # compatibility with other checkpoints
-        num_key_value_heads = n_heads
-        num_local_key_value_heads = n_heads_per_shard
-        key_value_dim = dim
-
-    # permute for sliced rotary
-    def permute(w, n_heads=n_heads, dim1=dim, dim2=dim):
-        return w.view(n_heads, dim1 // n_heads // 2, 2, dim2).transpose(1, 2).reshape(dim1, dim2)
-
-    print(f"Fetching all parameters from the checkpoint at {input_base_path}.")
-    # Load weights
-    if num_shards == 1:
-        # Not sharded
-        # (The sharded implementation would also work, but this is simpler.)
-        loaded = torch.load(os.path.join(input_base_path, "consolidated.00.pth"), map_location="cpu")
-    else:
-        # Sharded
-        loaded = [
-            torch.load(os.path.join(input_base_path, f"consolidated.{i:02d}.pth"), map_location="cpu")
-            for i in range(num_shards)
-        ]
-    param_count = 0
-    index_dict = {"weight_map": {}}
-    for layer_i in range(n_layers):
-        filename = f"pytorch_model-{layer_i + 1}-of-{n_layers + 1}.bin"
-        if num_shards == 1:
-            # Unsharded
-            state_dict = {
-                f"model.layers.{layer_i}.self_attn.q_proj.weight": permute(
-                    loaded[f"layers.{layer_i}.attention.wq.weight"]
-                ),
-                f"model.layers.{layer_i}.self_attn.k_proj.weight": permute(
-                    loaded[f"layers.{layer_i}.attention.wk.weight"]
-                ),
-                f"model.layers.{layer_i}.self_attn.v_proj.weight": loaded[f"layers.{layer_i}.attention.wv.weight"],
-                f"model.layers.{layer_i}.self_attn.o_proj.weight": loaded[f"layers.{layer_i}.attention.wo.weight"],
-                f"model.layers.{layer_i}.mlp.gate_proj.weight": loaded[f"layers.{layer_i}.feed_forward.w1.weight"],
-                f"model.layers.{layer_i}.mlp.down_proj.weight": loaded[f"layers.{layer_i}.feed_forward.w2.weight"],
-                f"model.layers.{layer_i}.mlp.up_proj.weight": loaded[f"layers.{layer_i}.feed_forward.w3.weight"],
-                f"model.layers.{layer_i}.input_layernorm.weight": loaded[f"layers.{layer_i}.attention_norm.weight"],
-                f"model.layers.{layer_i}.post_attention_layernorm.weight": loaded[f"layers.{layer_i}.ffn_norm.weight"],
-            }
-        else:
-            # Sharded
-            # Note that attention.w{q,k,v,o}, feed_fordward.w[1,2,3], attention_norm.weight and ffn_norm.weight share
-            # the same storage object, saving attention_norm and ffn_norm will save other weights too, which is
-            # redundant as other weights will be stitched from multiple shards. To avoid that, they are cloned.
-
-            state_dict = {
-                f"model.layers.{layer_i}.input_layernorm.weight": loaded[0][
-                    f"layers.{layer_i}.attention_norm.weight"
-                ].clone(),
-                f"model.layers.{layer_i}.post_attention_layernorm.weight": loaded[0][
-                    f"layers.{layer_i}.ffn_norm.weight"
-                ].clone(),
-            }
-            state_dict[f"model.layers.{layer_i}.self_attn.q_proj.weight"] = permute(
-                torch.cat(
-                    [
-                        loaded[i][f"layers.{layer_i}.attention.wq.weight"].view(n_heads_per_shard, dims_per_head, dim)
-                        for i in range(num_shards)
-                    ],
-                    dim=0,
-                ).reshape(dim, dim)
-            )
-            state_dict[f"model.layers.{layer_i}.self_attn.k_proj.weight"] = permute(
-                torch.cat(
-                    [
-                        loaded[i][f"layers.{layer_i}.attention.wk.weight"].view(
-                            num_local_key_value_heads, dims_per_head, dim
-                        )
-                        for i in range(num_shards)
-                    ],
-                    dim=0,
-                ).reshape(key_value_dim, dim),
-                num_key_value_heads,
-                key_value_dim,
-                dim,
-            )
-            state_dict[f"model.layers.{layer_i}.self_attn.v_proj.weight"] = torch.cat(
-                [
-                    loaded[i][f"layers.{layer_i}.attention.wv.weight"].view(
-                        num_local_key_value_heads, dims_per_head, dim
-                    )
-                    for i in range(num_shards)
-                ],
-                dim=0,
-            ).reshape(key_value_dim, dim)
-
-            state_dict[f"model.layers.{layer_i}.self_attn.o_proj.weight"] = torch.cat(
-                [loaded[i][f"layers.{layer_i}.attention.wo.weight"] for i in range(num_shards)], dim=1
-            )
-            state_dict[f"model.layers.{layer_i}.mlp.gate_proj.weight"] = torch.cat(
-                [loaded[i][f"layers.{layer_i}.feed_forward.w1.weight"] for i in range(num_shards)], dim=0
-            )
-            state_dict[f"model.layers.{layer_i}.mlp.down_proj.weight"] = torch.cat(
-                [loaded[i][f"layers.{layer_i}.feed_forward.w2.weight"] for i in range(num_shards)], dim=1
-            )
-            state_dict[f"model.layers.{layer_i}.mlp.up_proj.weight"] = torch.cat(
-                [loaded[i][f"layers.{layer_i}.feed_forward.w3.weight"] for i in range(num_shards)], dim=0
-            )
-
-        state_dict[f"model.layers.{layer_i}.self_attn.rotary_emb.inv_freq"] = inv_freq
-        for k, v in state_dict.items():
-            index_dict["weight_map"][k] = filename
-            param_count += v.numel()
-        torch.save(state_dict, os.path.join(tmp_model_path, filename))
-
-    filename = f"pytorch_model-{n_layers + 1}-of-{n_layers + 1}.bin"
-    if num_shards == 1:
-        # Unsharded
-        state_dict = {
-            "model.embed_tokens.weight": loaded["tok_embeddings.weight"],
-            "model.norm.weight": loaded["norm.weight"],
-            "lm_head.weight": loaded["output.weight"],
-        }
-    else:
-        state_dict = {
-            "model.norm.weight": loaded[0]["norm.weight"],
-            "model.embed_tokens.weight": torch.cat(
-                [loaded[i]["tok_embeddings.weight"] for i in range(num_shards)], dim=1
-            ),
-            "lm_head.weight": torch.cat([loaded[i]["output.weight"] for i in range(num_shards)], dim=0),
-        }
-
-    for k, v in state_dict.items():
-        index_dict["weight_map"][k] = filename
-        param_count += v.numel()
-    torch.save(state_dict, os.path.join(tmp_model_path, filename))
-
-    # Write configs
-    index_dict["metadata"] = {"total_size": param_count * 2}
-    write_json(index_dict, os.path.join(tmp_model_path, "pytorch_model.bin.index.json"))
-    ffn_dim_multiplier = params["ffn_dim_multiplier"] if "ffn_dim_multiplier" in params else 1
-    multiple_of = params["multiple_of"] if "multiple_of" in params else 256
-    config = OLMoConfig(
-        hidden_size=dim,
-        intermediate_size=compute_intermediate_size(dim, ffn_dim_multiplier, multiple_of),
-        num_attention_heads=params["n_heads"],
-        num_hidden_layers=params["n_layers"],
-        rms_norm_eps=params["norm_eps"],
-        num_key_value_heads=num_key_value_heads,
-        vocab_size=vocab_size,
-        rope_theta=base,
-        max_position_embeddings=max_position_embeddings,
-    )
-    config.save_pretrained(tmp_model_path)
-
-    # Make space so we can load the model properly now.
-    del state_dict
-    del loaded
-    gc.collect()
+    model_path = Path(input_base_path) / "model.pt"
+    shutil.copy(model_path, Path(tmp_output_path) / "pytorch_model.bin")
 
     print("Loading the checkpoint in a OLMo model.")
-    model = OLMoForCausalLM.from_pretrained(tmp_model_path, torch_dtype=torch.bfloat16, low_cpu_mem_usage=True)
+    model = OLMoForCausalLM.from_pretrained(tmp_output_path, low_cpu_mem_usage=True)
     # Avoid saving this as part of the config.
     del model.config._name_or_path
-    model.config.torch_dtype = torch.float16
     print("Saving in the Transformers format.")
-    model.save_pretrained(model_path, safe_serialization=safe_serialization)
-    shutil.rmtree(tmp_model_path)
+    model.save_pretrained(output_path, safe_serialization=safe_serialization)
+    shutil.rmtree(tmp_output_path)
 
-
-def write_tokenizer(tokenizer_path, input_tokenizer_path):
-    # Initialize the tokenizer based on the `spm` model
-    tokenizer_class = OLMoTokenizer if OLMoTokenizerFast is None else OLMoTokenizerFast
-    print(f"Saving a {tokenizer_class.__name__} to {tokenizer_path}.")
-    tokenizer = tokenizer_class(input_tokenizer_path)
-    tokenizer.save_pretrained(tokenizer_path)
+    if input_tokenizer_path is not None:
+        _write_tokenizer(Path(output_path), hf_config, Path(input_tokenizer_path))
 
 
 def main():
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(usage=USAGE)
     parser.add_argument(
         "--input_dir",
-        help="Location of OLMo weights, which contains tokenizer.model and model folders",
-    )
-    parser.add_argument(
-        "--model_size",
-        choices=["7B", "7Bf", "13B", "13Bf", "30B", "34B", "65B", "70B", "70Bf", "tokenizer_only"],
-        help="'f' models correspond to the finetuned versions, and are specific to the OLMo2 official release. For more details on OLMo2, checkout the original repo: https://huggingface.co/meta-olmo",
+        required=True,
+        help="Location of OLMo config weights (config.yaml and model.pt)",
     )
     parser.add_argument(
         "--output_dir",
+        required=True,
         help="Location to write HF model and tokenizer",
     )
-    parser.add_argument("--safe_serialization", type=bool, help="Whether or not to save using `safetensors`.")
-    # Different OLMo versions used different default values for max_position_embeddings, hence the need to be able to specify which version is being used.
     parser.add_argument(
-        "--olmo_version",
-        choices=[1, 2],
-        default=1,
-        type=int,
-        help="Version of the OLMo model to convert. Currently supports OLMo1 and OLMo2. Controls the context size",
+        "--tokenizer_json_path",
+        help="Location of the tokenizer json file used by OLMo's original tokenizer. When supplied, this script also creates HF tokenizer files.",
     )
+    parser.add_argument("--safe_serialization", type=bool, help="Whether or not to save using `safetensors`.")
     args = parser.parse_args()
-    spm_path = os.path.join(args.input_dir, "tokenizer.model")
-    if args.model_size != "tokenizer_only":
-        write_model(
-            model_path=args.output_dir,
-            input_base_path=args.input_dir,
-            model_size=args.model_size,
-            safe_serialization=args.safe_serialization,
-            tokenizer_path=spm_path,
-            olmo_version=args.olmo_version,
-        )
-    else:
-        write_tokenizer(args.output_dir, spm_path)
+    write_model(
+        output_path=args.output_dir,
+        input_base_path=args.input_dir,
+        input_tokenizer_path=args.tokenizer_json_path,
+        safe_serialization=args.safe_serialization if args.safe_serialization is not None else True,
+    )
 
 
 if __name__ == "__main__":
