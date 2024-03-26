66c3b77a6293c206c53369c5f8e10cf16eb5921e
steven <steven.bucaillle@gmail.com>
2024-03-26 23:29:03 +0100

    Multiple changes : - Added keypoint detection config to SuperGlueConfig - Completed convert_superglue_to_pytorch and succesfully run inference

--- src/transformers/models/superglue/convert_superglue_to_pytorch.py
+++ src/transformers/models/superglue/convert_superglue_to_pytorch.py
@@ -1,17 +1,25 @@
 import argparse
 
+import numpy as np
+import requests
 import torch
+from PIL import Image
+import cv2
 
-from transformers import SuperGlueConfig, SuperGlueModel
+from transformers import SuperGlueConfig, SuperGlueModel, SuperPointImageProcessor, AutoImageProcessor, AutoModel, \
+    AutoConfig, SuperPointConfig, AutoModelForKeypointDetection
+from transformers.models.superglue.modeling_superglue import ImageMatchingOutput
 
 
 def get_superglue_config(checkpoint_url):
+    keypoint_detection_config = AutoConfig.from_pretrained("magic-leap-community/superpoint")
     config = SuperGlueConfig(
         descriptor_dim=256,
         keypoint_encoder_sizes=[32, 64, 128, 256],
         gnn_layers_types=['self', 'cross'] * 9,
         sinkhorn_iterations=100,
         matching_threshold=0.2,
+        keypoint_detector_config=keypoint_detection_config
     )
 
     if "superglue_indoor" in checkpoint_url:
@@ -66,6 +74,66 @@ def rename_key(dct, old, new):
     dct[new] = val
 
 
+def process_resize(w, h, resize):
+    assert (len(resize) > 0 and len(resize) <= 2)
+    if len(resize) == 1 and resize[0] > -1:
+        scale = resize[0] / max(h, w)
+        w_new, h_new = int(round(w * scale)), int(round(h * scale))
+    elif len(resize) == 1 and resize[0] == -1:
+        w_new, h_new = w, h
+    else:  # len(resize) == 2:
+        w_new, h_new = resize[0], resize[1]
+
+    # Issue warning if resolution is too small or too large.
+    if max(w_new, h_new) < 160:
+        print('Warning: input resolution is very small, results may vary')
+    elif max(w_new, h_new) > 2000:
+        print('Warning: input resolution is very large, results may vary')
+
+    return w_new, h_new
+
+
+def frame2tensor(frame, device):
+    return torch.from_numpy(frame / 255.).float()[None, None].to(device)
+
+
+def read_image(path, device, resize, rotation, resize_float):
+    image = cv2.imread(str(path), cv2.IMREAD_GRAYSCALE)
+    if image is None:
+        return None, None, None
+    w, h = image.shape[1], image.shape[0]
+    w_new, h_new = process_resize(w, h, resize)
+    scales = (float(w) / float(w_new), float(h) / float(h_new))
+
+    if resize_float:
+        image = cv2.resize(image.astype('float32'), (w_new, h_new))
+    else:
+        image = cv2.resize(image, (w_new, h_new)).astype('float32')
+
+    if rotation != 0:
+        image = np.rot90(image, k=rotation)
+        if rotation % 2:
+            scales = scales[::-1]
+
+    inp = frame2tensor(image, device)
+    return inp
+
+
+def prepare_imgs_as_original():
+    url = "tests/fixtures/tests_samples/image_matching/london_bridge_78916675_4568141288.jpg"
+    im1 = read_image(url, 'cuda', [640, 480], 0, False)
+    url = "tests/fixtures/tests_samples/image_matching/london_bridge_19481797_2295892421.jpg"
+    im2 = read_image(url, 'cuda', [640, 480], 0, False)
+    return torch.cat([im1, im2], dim=0)
+
+
+def prepare_imgs_for_image_processor():
+    url = "tests/fixtures/tests_samples/image_matching/london_bridge_78916675_4568141288.jpg"
+    im1 = Image.open(url)
+    url = "tests/fixtures/tests_samples/image_matching/london_bridge_19481797_2295892421.jpg"
+    im2 = Image.open(url)
+    return [im1, im2]
+
 @torch.no_grad()
 def convert_superglue_checkpoint(checkpoint_url, pytorch_dump_folder_path, save_model, push_to_hub):
     """
@@ -84,16 +152,28 @@ def convert_superglue_checkpoint(checkpoint_url, pytorch_dump_folder_path, save_
     for src, dest in rename_keys:
         rename_key(new_state_dict, src, dest)
 
-    for key in new_state_dict.copy().keys():
-        val = new_state_dict.pop(key)
-        if not key.startswith("superglue"):
-            key = "superglue." + key
-        new_state_dict[key] = val
-
     model = SuperGlueModel(config)
-    model.load_state_dict(new_state_dict)
+    model.load_state_dict(new_state_dict, strict=False)
+    model.to("cuda")
     model.eval()
     print("Successfully loaded weights in the model")
+
+    ## USE REGULAR IMAGE PROCESSOR FOR INFERENCE
+    images = prepare_imgs_for_image_processor()
+    preprocessor = AutoImageProcessor.from_pretrained("magic-leap-community/superpoint")
+    inputs = preprocessor(images=images, return_tensors="pt")
+    inputs.to("cuda")
+    output: ImageMatchingOutput = model(**inputs, return_dict=True)
+    print("Number of matching keypoints using image processor")
+    print(torch.sum(output.image0_matches != -1))
+    print(torch.sum(output.image1_matches != -1))
+
+    images = prepare_imgs_as_original()
+    output: ImageMatchingOutput = model(pixel_values=images, return_dict=True)
+    print("Number of matching keypoints using original reading code")
+    print(torch.sum(output.image0_matches != -1))
+    print(torch.sum(output.image1_matches != -1))
+    # output: ImageMatchingOutput = model(pixel_values=images, return_dict=True)
 
 
 
@@ -102,7 +182,7 @@ if __name__ == "__main__":
     # Required parameters
     parser.add_argument(
         "--checkpoint_url",
-        default="https://github.com/magicleap/SuperGluePretrainedNetwork/raw/master/models/weights/superglue_indoor.pth",
+        default="https://github.com/magicleap/SuperGluePretrainedNetwork/raw/master/models/weights/superglue_outdoor.pth",
         type=str,
         help="URL of the original SuperGlue checkpoint you'd like to convert.",
     )
