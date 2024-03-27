309f0af05e41f5631a80bafc9f46be2483b91508
Hovnatan Karapetyan <hovnatan@gmail.com>
2024-03-27 08:01:53 +0400

    Move create_sinusoidal_embeddings to _init_weights

--- src/transformers/models/distilbert/modeling_distilbert.py
+++ src/transformers/models/distilbert/modeling_distilbert.py
@@ -106,10 +106,6 @@ class Embeddings(nn.Module):
         super().__init__()
         self.word_embeddings = nn.Embedding(config.vocab_size, config.dim, padding_idx=config.pad_token_id)
         self.position_embeddings = nn.Embedding(config.max_position_embeddings, config.dim)
-        if config.sinusoidal_pos_embds:
-            create_sinusoidal_embeddings(
-                n_pos=config.max_position_embeddings, dim=config.dim, out=self.position_embeddings.weight
-            )
 
         self.LayerNorm = nn.LayerNorm(config.dim, eps=1e-12)
         self.dropout = nn.Dropout(config.dropout)
@@ -621,12 +617,6 @@ class DistilBertPreTrainedModel(PreTrainedModel):
 
     def _init_weights(self, module: nn.Module):
         """Initialize the weights."""
-        modules_to_skip_init = []
-        if self.config.sinusoidal_pos_embds:
-            main_module = self.distilbert if hasattr(self, "distilbert") else self
-            modules_to_skip_init.append(main_module.embeddings.position_embeddings)
-        if module in modules_to_skip_init:
-            return
         if isinstance(module, nn.Linear):
             # Slightly different from the TF version which uses truncated_normal for initialization
             # cf https://github.com/pytorch/pytorch/pull/5617
@@ -640,6 +630,10 @@ class DistilBertPreTrainedModel(PreTrainedModel):
         elif isinstance(module, nn.LayerNorm):
             module.bias.data.zero_()
             module.weight.data.fill_(1.0)
+        elif isinstance(module, Embeddings) and self.config.sinusoidal_pos_embds:
+            create_sinusoidal_embeddings(
+                self.config.max_position_embeddings, self.config.dim, module.position_embeddings.weight
+            )
 
 
 DISTILBERT_START_DOCSTRING = r"""
