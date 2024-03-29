57929ac96bece96f2bfc46f9f0f70f71c0cdc3f7
Joao Gante <joao@huggingface.co>
2024-03-29 16:02:51 +0000

    beam search

--- src/transformers/generation/utils.py
+++ src/transformers/generation/utils.py
@@ -1724,6 +1724,7 @@ class GenerationMixin:
                 logits_processor=prepared_logits_processor,
                 stopping_criteria=prepared_stopping_criteria,
                 pad_token_id=generation_config.pad_token_id,
+                eos_token_id=generation_config.eos_token_id,
                 output_attentions=generation_config.output_attentions,
                 output_hidden_states=generation_config.output_hidden_states,
                 output_scores=generation_config.output_scores,
@@ -1765,6 +1766,7 @@ class GenerationMixin:
                 logits_warper=logits_warper,
                 stopping_criteria=prepared_stopping_criteria,
                 pad_token_id=generation_config.pad_token_id,
+                eos_token_id=generation_config.eos_token_id,
                 output_attentions=generation_config.output_attentions,
                 output_hidden_states=generation_config.output_hidden_states,
                 output_scores=generation_config.output_scores,
@@ -1800,6 +1802,7 @@ class GenerationMixin:
                 logits_processor=prepared_logits_processor,
                 stopping_criteria=prepared_stopping_criteria,
                 pad_token_id=generation_config.pad_token_id,
+                eos_token_id=generation_config.eos_token_id,
                 output_attentions=generation_config.output_attentions,
                 output_hidden_states=generation_config.output_hidden_states,
                 output_scores=generation_config.output_scores,
@@ -1875,6 +1878,7 @@ class GenerationMixin:
                 logits_processor=prepared_logits_processor,
                 stopping_criteria=prepared_stopping_criteria,
                 pad_token_id=generation_config.pad_token_id,
+                eos_token_id=generation_config.eos_token_id,
                 output_attentions=generation_config.output_attentions,
                 output_hidden_states=generation_config.output_hidden_states,
                 output_scores=generation_config.output_scores,
@@ -1912,7 +1916,7 @@ class GenerationMixin:
         penalty_alpha: float,
         logits_processor: LogitsProcessorList,
         stopping_criteria: StoppingCriteriaList,
-        pad_token_id: int,
+        pad_token_id: Optional[int],
         output_attentions: bool,
         output_hidden_states: bool,
         output_scores: bool,
@@ -1940,7 +1944,7 @@ class GenerationMixin:
             stopping_criteria (`StoppingCriteriaList`):
                 An instance of [`StoppingCriteriaList`]. List of instances of class derived from [`StoppingCriteria`]
                 used to tell if the generation loop should stop.
-            pad_token_id (`int`):
+            pad_token_id (`int`, *optional*):
                 The id of the *padding* token.
             output_attentions (`bool`):
                 Whether or not to return the attentions tensors of all attention layers. See `attentions` under
@@ -2283,7 +2287,7 @@ class GenerationMixin:
         input_ids: torch.LongTensor,
         logits_processor: LogitsProcessorList,
         stopping_criteria: StoppingCriteriaList,
-        pad_token_id: int,
+        pad_token_id: Optional[int],
         output_attentions: bool,
         output_hidden_states: bool,
         output_scores: bool,
@@ -2306,7 +2310,7 @@ class GenerationMixin:
             stopping_criteria (`StoppingCriteriaList`):
                 An instance of [`StoppingCriteriaList`]. List of instances of class derived from [`StoppingCriteria`]
                 used to tell if the generation loop should stop.
-            pad_token_id (`int`):
+            pad_token_id (`int`, *optional*):
                 The id of the *padding* token.
             output_attentions (`bool`):
                 Whether or not to return the attentions tensors of all attention layers. See `attentions` under
@@ -2454,7 +2458,7 @@ class GenerationMixin:
         logits_processor: LogitsProcessorList,
         stopping_criteria: StoppingCriteriaList,
         logits_warper: LogitsProcessorList,
-        pad_token_id: int,
+        pad_token_id: Optional[int],
         output_attentions: bool,
         output_hidden_states: bool,
         output_scores: bool,
@@ -2481,7 +2485,7 @@ class GenerationMixin:
                 An instance of [`LogitsProcessorList`]. List of instances of class derived from [`LogitsWarper`] used
                 to warp the prediction score distribution of the language modeling head applied before multinomial
                 sampling at each generation step.
-            pad_token_id (`int`):
+            pad_token_id (`int`, *optional*):
                 The id of the *padding* token.
             output_attentions (`bool`):
                 Whether or not to return the attentions tensors of all attention layers. See `attentions` under
@@ -2655,18 +2659,17 @@ class GenerationMixin:
         self,
         input_ids: torch.LongTensor,
         beam_scorer: BeamScorer,
-        logits_processor: Optional[LogitsProcessorList] = None,
-        stopping_criteria: Optional[StoppingCriteriaList] = None,
-        max_length: Optional[int] = None,
-        pad_token_id: Optional[int] = None,
-        eos_token_id: Optional[Union[int, List[int]]] = None,
-        output_attentions: Optional[bool] = None,
-        output_hidden_states: Optional[bool] = None,
-        output_scores: Optional[bool] = None,
-        output_logits: Optional[bool] = None,
-        return_dict_in_generate: Optional[bool] = None,
-        synced_gpus: bool = False,
-        sequential: Optional[bool] = None,
+        logits_processor: LogitsProcessorList,
+        stopping_criteria: StoppingCriteriaList,
+        pad_token_id: Optional[int],
+        eos_token_id: Optional[Union[int, List[int]]],
+        output_attentions: bool,
+        output_hidden_states: bool,
+        output_scores: bool,
+        output_logits: bool,
+        return_dict_in_generate: bool,
+        synced_gpus: bool,
+        sequential: Optional[bool],
         **model_kwargs,
     ) -> Union[GenerateBeamOutput, torch.LongTensor]:
         r"""
@@ -2679,35 +2682,32 @@ class GenerationMixin:
             beam_scorer (`BeamScorer`):
                 An derived instance of [`BeamScorer`] that defines how beam hypotheses are constructed, stored and
                 sorted during generation. For more information, the documentation of [`BeamScorer`] should be read.
-            logits_processor (`LogitsProcessorList`, *optional*):
+            logits_processor (`LogitsProcessorList`):
                 An instance of [`LogitsProcessorList`]. List of instances of class derived from [`LogitsProcessor`]
                 used to modify the prediction scores of the language modeling head applied at each generation step.
-            stopping_criteria (`StoppingCriteriaList`, *optional*):
+            stopping_criteria (`StoppingCriteriaList`:
                 An instance of [`StoppingCriteriaList`]. List of instances of class derived from [`StoppingCriteria`]
                 used to tell if the generation loop should stop.
-            max_length (`int`, *optional*, defaults to 20):
-                **DEPRECATED**. Use `logits_processor` or `stopping_criteria` directly to cap the number of generated
-                tokens. The maximum length of the sequence to be generated.
             pad_token_id (`int`, *optional*):
                 The id of the *padding* token.
             eos_token_id (`Union[int, List[int]]`, *optional*):
                 The id of the *end-of-sequence* token. Optionally, use a list to set multiple *end-of-sequence* tokens.
-            output_attentions (`bool`, *optional*, defaults to `False`):
+            output_attentions (`bool`):
                 Whether or not to return the attentions tensors of all attention layers. See `attentions` under
                 returned tensors for more details.
-            output_hidden_states (`bool`, *optional*, defaults to `False`):
+            output_hidden_states (`bool`):
                 Whether or not to return the hidden states of all layers. See `hidden_states` under returned tensors
                 for more details.
-            output_logits (`bool`, *optional*, defaults to `False`):
+            output_logits (`bool`):
                 Whether or not to return the raw prediction logit scores. See `logits` under returned tensors for
                 more details.
-            output_scores (`bool`, *optional*, defaults to `False`):
+            output_scores (`bool`):
                 Whether or not to return the prediction scores. See `scores` under returned tensors for more details.
-            return_dict_in_generate (`bool`, *optional*, defaults to `False`):
+            return_dict_in_generate (`bool`):
                 Whether or not to return a [`~utils.ModelOutput`] instead of a plain tuple.
-            synced_gpus (`bool`, *optional*, defaults to `False`):
+            synced_gpus (`bool`):
                 Whether to continue running the while loop until max_length (needed for ZeRO stage 3)
-            sequential (`bool`, defaults to `False`):
+            sequential (`bool`, *optional*):
                 By default, beam search has `batch_size * num_beams` as effective batch size (see `beam_search()` for
                 more details). This flag will avoid parallelizing the beam search and will instead run beam search
                 sequentially.
@@ -2723,53 +2723,8 @@ class GenerationMixin:
             `model.config.is_encoder_decoder=True`.
         """
         # init values
-        logits_processor = logits_processor if logits_processor is not None else LogitsProcessorList()
-        stopping_criteria = stopping_criteria if stopping_criteria is not None else StoppingCriteriaList()
-        sequential = sequential if sequential is not None else self.generation_config.low_memory
-        if max_length is not None:
-            warnings.warn(
-                "`max_length` is deprecated in this function, use"
-                " `stopping_criteria=StoppingCriteriaList([MaxLengthCriteria(max_length=max_length)])` instead.",
-                UserWarning,
-            )
-            stopping_criteria = validate_stopping_criteria(stopping_criteria, max_length)
-        if len(stopping_criteria) == 0:
-            warnings.warn("You don't have defined any stopping_criteria, this will likely loop forever", UserWarning)
-        pad_token_id = pad_token_id if pad_token_id is not None else self.generation_config.pad_token_id
-        if eos_token_id is not None:
-            logger.warning_once(
-                "`eos_token_id` is deprecated in this function and will be removed in v4.41, use"
-                " `stopping_criteria=StoppingCriteriaList([EosTokenCriteria(eos_token_id=eos_token_id)])` instead."
-                " Otherwise make sure to set `model.generation_config.eos_token_id`",
-                FutureWarning,
-            )
-            stopping_criteria.append(EosTokenCriteria(eos_token_id=eos_token_id))
-        else:
-            # TODO remove when the method is totally private and beam scorer refactored
-            # need to get `eos_token_id` and add stopping criteria, so that generation does not go forever
-            eos_token_id = [
-                criteria.eos_token_id.tolist() for criteria in stopping_criteria if hasattr(criteria, "eos_token_id")
-            ]
-            eos_token_id = eos_token_id[0] if eos_token_id else None
-            if eos_token_id is None and self.generation_config.eos_token_id is not None:
-                eos_token_id = self.generation_config.eos_token_id
-                stopping_criteria.append(EosTokenCriteria(eos_token_id=eos_token_id))
-
         if isinstance(eos_token_id, int):
             eos_token_id = [eos_token_id]
-        output_scores = output_scores if output_scores is not None else self.generation_config.output_scores
-        output_logits = output_logits if output_logits is not None else self.generation_config.output_logits
-        output_attentions = (
-            output_attentions if output_attentions is not None else self.generation_config.output_attentions
-        )
-        output_hidden_states = (
-            output_hidden_states if output_hidden_states is not None else self.generation_config.output_hidden_states
-        )
-        return_dict_in_generate = (
-            return_dict_in_generate
-            if return_dict_in_generate is not None
-            else self.generation_config.return_dict_in_generate
-        )
 
         batch_size = len(beam_scorer._beam_hyps)
         num_beams = beam_scorer.num_beams
