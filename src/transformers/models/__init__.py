bd84f1bc3b38c3e1ae964882385c71ebb7090962
steven <steven.bucaillle@gmail.com>
2024-03-31 00:49:29 +0100

    Multiple changes :  - Added SuperGlue to a bunch of places  - Divided SuperGlue into SuperGlueForImageMatching and SuperGlueModel  - Added testing images

--- src/transformers/models/__init__.py
+++ src/transformers/models/__init__.py
@@ -13,7 +13,6 @@
 # limitations under the License.
 
 from . import (
-    superglue,
     albert,
     align,
     altclip,
@@ -193,7 +192,6 @@ from . import (
     regnet,
     rembert,
     resnet,
-    superglue,
     roberta,
     roberta_prelayernorm,
     roc_bert,
