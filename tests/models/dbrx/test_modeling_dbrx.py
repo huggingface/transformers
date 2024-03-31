0ed3675314b43c6aa1be31a5be752639360ef0fe
Eitan Turok <eitan.turok@databricks.com>
2024-03-31 17:09:03 +0000

    init ConfigTester differently so tests pass

--- tests/models/dbrx/test_modeling_dbrx.py
+++ tests/models/dbrx/test_modeling_dbrx.py
@@ -303,7 +303,9 @@ class DbrxModelTest(ModelTesterMixin, unittest.TestCase):
 
     def setUp(self):
         self.model_tester = DbrxModelTester(self)
-        self.config_tester = ConfigTester(self, config_class=DbrxConfig, hidden_size=37)
+        self.config_tester = ConfigTester(
+            self, config_class=DbrxConfig, ffn_config={"ffn_hidden_size": 37, "model_type": ""}
+        )
 
     def test_config(self):
         self.config_tester.run_common_tests()
