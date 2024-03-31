a08b27dcf4accfa5eb6f41b4674cf700d48d10ad
Eitan Turok <eitan.turok@databricks.com>
2024-03-31 17:14:30 +0000

    remove multiple choice test

--- tests/models/dbrx/test_modeling_dbrx.py
+++ tests/models/dbrx/test_modeling_dbrx.py
@@ -324,10 +324,6 @@ class DbrxModelTest(ModelTesterMixin, unittest.TestCase):
         config_and_inputs = self.model_tester.prepare_config_and_inputs()
         self.model_tester.create_and_check_for_masked_lm(*config_and_inputs)
 
-    def test_for_multiple_choice(self):
-        config_and_inputs = self.model_tester.prepare_config_and_inputs()
-        self.model_tester.create_and_check_for_multiple_choice(*config_and_inputs)
-
     def test_decoder_model_past_with_large_inputs(self):
         config_and_inputs = self.model_tester.prepare_config_and_inputs_for_decoder()
         self.model_tester.create_and_check_decoder_model_past_large_inputs(*config_and_inputs)
