580a8ee162722c5f4a0a5d95a3ee357e07593f5f
Yoach Lacombe <52246514+ylacombe@users.noreply.github.com>
2024-04-01 08:51:00 +0100

    Fix FA2 tests (#29909)
    * fix FA2 tests
    
    * refactor inference test name

--- tests/models/qwen2/test_modeling_qwen2.py
+++ tests/models/qwen2/test_modeling_qwen2.py
@@ -477,7 +477,7 @@ class Qwen2ModelTest(ModelTesterMixin, GenerationTesterMixin, PipelineTesterMixi
     @require_torch_gpu
     @pytest.mark.flash_attn_test
     @slow
-    def test_flash_attn_2_inference_padding_right(self):
+    def test_flash_attn_2_inference_equivalence_right_padding(self):
         self.skipTest("Qwen2 flash attention does not support right padding")
 
 
