d9cb2ce7f92df9ad21abb8ea9cba82eba0eabce3
Arthur Zucker <arthur.zucker@gmail.com>
2024-04-01 11:47:40 +0200

    nit

--- utils/check_copies.py
+++ utils/check_copies.py
@@ -314,9 +314,11 @@ def split_code_into_blocks(
             rf"^{' ' * (indent - 4)}((class|def)\s+\S+)(\(|\:)", lines[start_index]
         ).groups()[0]
     except Exception:
+        start_context = min(start_index-10, 0)
+        end_context = min(end_index+10, len(lines))
         raise ValueError(
             f"Tried to split a class or function. It did not work. Error comes from line {start_index}: \n```\n"
-            + "".join(lines[start_index - 10 : end_index + 10])
+            + "".join(lines[start_context:end_context])
             + "```\n"
         )
 
