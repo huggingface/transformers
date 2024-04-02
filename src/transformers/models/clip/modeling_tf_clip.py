bfa8da0b9ef578e402cfa0f0f02230efc703aeb4
Matthew Denton <mdenton@snapchat.com>
2024-04-02 12:48:28 -0400

    [WIP] Add CLIP Vision with Projection for TensorFlow

--- src/transformers/models/clip/modeling_tf_clip.py
+++ src/transformers/models/clip/modeling_tf_clip.py
@@ -1024,6 +1024,81 @@ class TFCLIPMainLayer(keras.layers.Layer):
             vision_model_output=vision_outputs,
         )
 
+@keras_serializable
+class TFCLIPVisionProjectionLayer(keras.layers.Layer):
+    config_class = CLIPConfig
+
+    def __init__(self, config: CLIPConfig, **kwargs):
+        super().__init__(**kwargs)
+
+        if not isinstance(config, CLIPVisionConfig):
+            raise ValueError(
+                "config.vision_config is expected to be of type CLIPVisionConfig but is of type"
+                f" {type(config.vision_config)}."
+            )
+
+        self.config = config
+
+        vision_config = config#.vision_config
+
+        self.projection_dim = config.projection_dim
+
+        self.vision_model = TFCLIPVisionTransformer(vision_config, name="vision_model")
+
+        self.visual_projection = keras.layers.Dense(
+            units=self.projection_dim,
+            kernel_initializer=get_initializer(vision_config.hidden_size**-0.5 * self.config.initializer_factor),
+            use_bias=False,
+            name="visual_projection",
+        )
+
+        self.vision_embed_dim = vision_config.hidden_size
+
+    def build(self, input_shape: tf.TensorShape = None):
+        #self.logit_scale = self.add_weight(
+        #    shape=(1,),
+        #    initializer=keras.initializers.Constant(self.config.logit_scale_init_value),
+        #    trainable=True,
+        #    name="logit_scale",
+        #)
+
+        if self.built:
+            return
+        self.built = True
+        if getattr(self, "vision_model", None) is not None:
+            with tf.name_scope(self.vision_model.name):
+                self.vision_model.build(None)
+        if getattr(self, "visual_projection", None) is not None:
+            with tf.name_scope(self.visual_projection.name):
+                self.visual_projection.build([None, None, self.vision_embed_dim])
+
+    
+    @unpack_inputs
+    def call(
+        self,
+        pixel_values: TFModelInputType | None = None,
+        output_attentions: Optional[bool] = None,
+        output_hidden_states: Optional[bool] = None,
+        return_dict: Optional[bool] = None,
+        training: bool = False,
+    ) -> tf.Tensor:
+        if pixel_values is None:
+            raise ValueError("You have to specify pixel_values")
+
+        vision_outputs = self.vision_model(
+            pixel_values=pixel_values,
+            output_attentions=output_attentions,
+            output_hidden_states=output_hidden_states,
+            return_dict=return_dict,
+            training=training,
+        )
+
+        pooled_output = vision_outputs[1]  # pooled_output
+        image_features = self.visual_projection(inputs=pooled_output)
+
+        image_features = image_features / tf.norm(tensor=image_features, ord="euclidean", axis=-1, keepdims=True)
+
+        return image_features
 
 class TFCLIPPreTrainedModel(TFPreTrainedModel):
     """
@@ -1301,6 +1376,47 @@ class TFCLIPVisionModel(TFCLIPPreTrainedModel):
                 self.clip.build(None)
 
 
+class TFCLIPVisionProjectionModel(TFCLIPPreTrainedModel):
+    config_class = CLIPVisionConfig
+    main_input_name = "pixel_values"
+
+    def __init__(self, config: CLIPVisionConfig, *inputs, **kwargs):
+        super().__init__(config, *inputs, **kwargs)
+
+        self.clip = TFCLIPVisionProjectionLayer(config, name="clip")
+
+    
+    #@add_start_docstrings_to_model_forward(CLIP_VISION_INPUTS_DOCSTRING)
+    #@replace_return_docstrings(output_type=TFBaseModelOutputWithPooling, config_class=CLIPVisionConfig)
+    @unpack_inputs
+    def call(
+        self,
+        pixel_values: TFModelInputType | None = None,
+        output_attentions: Optional[bool] = None,
+        output_hidden_states: Optional[bool] = None,
+        return_dict: Optional[bool] = None,
+        training: Optional[bool] = False,
+    ) -> tf.Tensor:
+
+        outputs = self.clip(
+            pixel_values=pixel_values,
+            output_attentions=output_attentions,
+            output_hidden_states=output_hidden_states,
+            return_dict=return_dict,
+            training=training,
+        )
+
+        return outputs
+
+    def build(self, input_shape=None):
+        if self.built:
+            return
+        self.built = True
+        if getattr(self, "clip", None) is not None:
+            with tf.name_scope(self.clip.name):
+                self.clip.build(None)
+
+
 @add_start_docstrings(CLIP_START_DOCSTRING)
 class TFCLIPModel(TFCLIPPreTrainedModel):
     config_class = CLIPConfig
@@ -1310,8 +1426,9 @@ class TFCLIPModel(TFCLIPPreTrainedModel):
 
         self.clip = TFCLIPMainLayer(config, name="clip")
 
+    
+    #@add_start_docstrings_to_model_forward(CLIP_TEXT_INPUTS_DOCSTRING.format("batch_size, sequence_length"))
     @unpack_inputs
-    @add_start_docstrings_to_model_forward(CLIP_TEXT_INPUTS_DOCSTRING.format("batch_size, sequence_length"))
     def get_text_features(
         self,
         input_ids: TFModelInputType | None = None,
