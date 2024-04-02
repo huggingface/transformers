170f9ec780de0bb2466b98227e98b57da8c925b7
YasserdahouML <yasser.dahoudjilali2@mail.dcu.ie>
2024-04-02 12:22:35 +0000

    correcting the config

--- src/transformers/models/falcon_vlm/configuration_falcon_vlm.py
+++ src/transformers/models/falcon_vlm/configuration_falcon_vlm.py
@@ -79,7 +79,6 @@ class FalconVLMConfig(PretrainedConfig):
     ):
         self.image_token_index = image_token_index
         self.projector_hidden_act = projector_hidden_act
-
         if isinstance(vision_config, dict):
             vision_config["model_type"] = (
                 vision_config["model_type"] if "model_type" in vision_config else "clip_vision_model"
@@ -100,10 +99,10 @@ class FalconVLMConfig(PretrainedConfig):
         self.vision_config = vision_config
 
         if isinstance(text_config, dict):
-            text_config["model_type"] = text_config["model_type"] if "model_type" in text_config else "llama"
+            text_config["model_type"] = text_config["model_type"] if "model_type" in text_config else "falcon"
             text_config = CONFIG_MAPPING[text_config["model_type"]](**text_config)
         elif text_config is None:
-            text_config = CONFIG_MAPPING["llama"]()
+            text_config = CONFIG_MAPPING["falcon"]()
 
         self.text_config = text_config
 
