de23710134d8b93033915bcde86a121bfd04821e
Jonathan Tow <41410219+jon-tow@users.noreply.github.com>
2024-04-08 17:51:58 -0400

    [`StableLm`] Add QK normalization and Parallel Residual Support (#29745)
    * init: add StableLm 2 support
    
    * add integration test for parallel residual and qk layernorm
    
    * update(modeling): match qk norm naming for consistency with phi/persimmon
    
    * fix(tests): run fwd/bwd on random init test model to jitter norm weights off identity
    
    * `use_parallel_residual`: add copy pointer to `GPTNeoXLayer.forward`
    
    * refactor: rename head states var in `StableLmLayerNormPerHead`
    
    * tests: update test model and add generate check

--- src/transformers/models/stablelm/configuration_stablelm.py
+++ src/transformers/models/stablelm/configuration_stablelm.py
@@ -83,6 +83,11 @@ class StableLmConfig(PretrainedConfig):
             is an experimental feature, subject to breaking API changes in future versions.
         use_qkv_bias (`bool`, *optional*, defaults to `False`):
             Whether or not the model should use bias for qkv layers.
+        qk_layernorm (`bool`, *optional*, defaults to `False`):
+            Whether or not to normalize, per head, the Queries and Keys after projecting the hidden states.
+        use_parallel_residual (`bool`, *optional*, defaults to `False`):
+            Whether to use a "parallel" formulation in each Transformer layer, which can provide a slight training
+            speedup at large scales.
         hidden_dropout (`float`, *optional*, defaults to 0.0):
             The dropout ratio after applying the MLP to the hidden states.
         attention_dropout (`float`, *optional*, defaults to 0.0):
@@ -123,6 +128,8 @@ class StableLmConfig(PretrainedConfig):
         rope_theta=10_000,
         rope_scaling=None,
         use_qkv_bias=False,
+        qk_layernorm=False,
+        use_parallel_residual=False,
         hidden_dropout=0.0,
         attention_dropout=0.0,
         partial_rotary_factor=0.25,
@@ -146,6 +153,8 @@ class StableLmConfig(PretrainedConfig):
         self.rope_theta = rope_theta
         self.rope_scaling = rope_scaling
         self.use_qkv_bias = use_qkv_bias
+        self.qk_layernorm = qk_layernorm
+        self.use_parallel_residual = use_parallel_residual
         self.hidden_dropout = hidden_dropout
         self.attention_dropout = attention_dropout
         self.partial_rotary_factor = partial_rotary_factor
