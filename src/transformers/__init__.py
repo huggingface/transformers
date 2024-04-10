0cabfbe5009fa02e18c538a8928cc101ac6b8727
Arthur <48595927+ArthurZucker@users.noreply.github.com>
2024-04-10 16:59:13 +0200

    Add recurrent gemma (#30143)
    * Fork.
    
    * RecurrentGemma initial commit.
    
    * Updating __init__.py.
    
    * Minor modification to how we initialize the cache.
    Changing how the config specifies the architecture.
    
    * Reformat code to 4 spaces.
    Fixed a few typos.
    
    * Fixed the forward pass.
    Still unclear on the cache?
    
    * Fixed the RecurrentGemmaForCausalLM
    
    * Minor comment that we might not need attention_mask and output_attention arguments.
    
    * Now cache should work as well.
    
    * Adding a temporary example to check whether the model generation works.
    
    * Adding the tests and updating imports.
    
    * Adding the example file missing in the previous commit.
    
    * First working example.
    
    * Removing .gitignore and reverting parts of __init__.
    
    * Re-add .gitignore.
    
    * Addressing comments for configuration.
    
    * Move mask creation to `_prepare_inputs_for_generation`.
    
    * First try at integration tests:
    1. AttributeError: 'GriffinCausalLMOutput' object has no attribute 'attentions'.
    2. `cache_position` not passed
    
    * Transfoering between machines.
    
    * Running normal tests.
    
    * Minor fix.
    
    * More fixes.
    
    * Addressing more comments.
    
    * Minor fixes.
    
    * first stab at cleanup
    
    * more refactoring
    
    * fix copies and else
    
    * renaming and get init to work
    
    * fix causal mask creation
    
    * update
    
    * nit
    
    * fix a hell lot of things
    
    * updates
    
    * update conversion script
    
    * make all keys importable
    
    * nits
    
    * add auto mappings
    
    * properly convert ffw_up and down
    
    * add scaling
    
    * fix generations
    
    * for recurrent dtype
    
    * update
    
    * fix going beyong window
    
    * fixup
    
    * add missing files
    
    * current updates to remove last einops
    
    * finish modeling refactor
    
    * TADA
    
    * fix compile
    
    * fix most failing testt ? ?
    
    * update tests
    
    * refactor and update
    
    * update
    
    * nits, fixup and update tests
    
    * more fixup
    
    * nits
    
    * fix imports
    
    * test format
    
    * fixups
    
    * nits
    
    * tuple typing
    
    * fix code quality
    
    * add model card
    
    * fix doc
    
    * skip most generation tests
    
    * nits
    
    * style
    
    * doc fixes
    
    * fix pr and check_copies?
    
    * last nit
    
    * oupsy
    
    * Apply suggestions from code review
    
    Co-authored-by: Lysandre Debut <hi@lysand.re>
    
    * update
    
    * Update src/transformers/models/recurrent_gemma/convert_recurrent_gemma_to_hf.py
    
    Co-authored-by: amyeroberts <22614925+amyeroberts@users.noreply.github.com>
    
    * Update tests/models/recurrent_gemma/test_modeling_recurrent_gemma.py
    
    Co-authored-by: amyeroberts <22614925+amyeroberts@users.noreply.github.com>
    
    * Update tests/models/recurrent_gemma/test_modeling_recurrent_gemma.py
    
    Co-authored-by: amyeroberts <22614925+amyeroberts@users.noreply.github.com>
    
    * Update tests/models/recurrent_gemma/test_modeling_recurrent_gemma.py
    
    Co-authored-by: amyeroberts <22614925+amyeroberts@users.noreply.github.com>
    
    * Update tests/models/recurrent_gemma/test_modeling_recurrent_gemma.py
    
    Co-authored-by: amyeroberts <22614925+amyeroberts@users.noreply.github.com>
    
    * update based on review
    
    * doc nit
    
    * fix quality
    
    * quality
    
    * fix slow test model path
    
    * update default dype
    
    * ignore attributes that can be safely ignored in check config attributes
    
    * 0lallalala come on
    
    * save nit
    
    * style
    
    * remove to dict update
    
    * make sure we can also run in float16
    
    * style
    
    ---------
    
    Co-authored-by: Pablo Montalvo <39954772+molbap@users.noreply.github.com>
    Co-authored-by: Aleksandar Botev <botev@google.com>
    Co-authored-by: Leonard Berrada <lberrada@users.noreply.github.com>
    Co-authored-by: anushanf <anushanf@google.com>
    Co-authored-by: botev <botevmg@gmail.com>
    Co-authored-by: Lysandre Debut <hi@lysand.re>
    Co-authored-by: amyeroberts <22614925+amyeroberts@users.noreply.github.com>

--- src/transformers/__init__.py
+++ src/transformers/__init__.py
@@ -743,6 +743,7 @@ _import_structure = {
         "RealmConfig",
         "RealmTokenizer",
     ],
+    "models.recurrent_gemma": ["RecurrentGemmaConfig"],
     "models.reformer": ["REFORMER_PRETRAINED_CONFIG_ARCHIVE_MAP", "ReformerConfig"],
     "models.regnet": ["REGNET_PRETRAINED_CONFIG_ARCHIVE_MAP", "RegNetConfig"],
     "models.rembert": ["REMBERT_PRETRAINED_CONFIG_ARCHIVE_MAP", "RemBertConfig"],
@@ -3113,6 +3114,13 @@ else:
             "RealmRetriever",
             "RealmScorer",
             "load_tf_weights_in_realm",
+        ]
+    )
+    _import_structure["models.recurrent_gemma"].extend(
+        [
+            "RecurrentGemmaForCausalLM",
+            "RecurrentGemmaModel",
+            "RecurrentGemmaPreTrainedModel",
         ]
     )
     _import_structure["models.reformer"].extend(
@@ -5625,6 +5633,7 @@ if TYPE_CHECKING:
         RealmConfig,
         RealmTokenizer,
     )
+    from .models.recurrent_gemma import RecurrentGemmaConfig
     from .models.reformer import REFORMER_PRETRAINED_CONFIG_ARCHIVE_MAP, ReformerConfig
     from .models.regnet import REGNET_PRETRAINED_CONFIG_ARCHIVE_MAP, RegNetConfig
     from .models.rembert import REMBERT_PRETRAINED_CONFIG_ARCHIVE_MAP, RemBertConfig
@@ -7686,6 +7695,11 @@ if TYPE_CHECKING:
             RealmRetriever,
             RealmScorer,
             load_tf_weights_in_realm,
+        )
+        from .models.recurrent_gemma import (
+            RecurrentGemmaForCausalLM,
+            RecurrentGemmaModel,
+            RecurrentGemmaPreTrainedModel,
         )
         from .models.reformer import (
             REFORMER_PRETRAINED_MODEL_ARCHIVE_LIST,
