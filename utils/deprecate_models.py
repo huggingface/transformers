3fb3f832cfa04e18cb9a1d49af04d6be8d7a8f35
Amy Roberts <22614925+amyeroberts@users.noreply.github.com>
2024-04-11 12:21:29 +0200

    Fix logger

--- utils/deprecate_models.py
+++ utils/deprecate_models.py
@@ -18,7 +18,7 @@ REPO_PATH = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
 repo = Repo(REPO_PATH)
 
 
-logger = logging.get_logger(__name__)
+logger = logger.get_logger(__name__)
 
 
 # # FIXME - more robust way of finding the model doc - find the model class form the model's init file and then find the model doc
@@ -102,12 +102,12 @@ def extract_model_info(model):
     model_path = f"src/transformers/models/{model}"
 
     if model_doc_path is None:
-        logging.info(f"Model doc path does not exist for {model}")
+        logger.info(f"Model doc path does not exist for {model}")
         return None
     model_info["model_doc_path"] = model_doc_path
 
     if not os.path.exists(model_path):
-        logging.info(f"Model path does not exist for {model}")
+        logger.info(f"Model path does not exist for {model}")
         return None
     model_info["model_path"] = model_path
 
@@ -372,7 +372,7 @@ def deprecate_models(models):
     # Filter out skipped models
     models = [model for model in models if model not in skipped_models]
 
-    logging.info(f"Skipped models: {skipped_models} as the model doc or model path could not be found.")
+    logger.info(f"Skipped models: {skipped_models} as the model doc or model path could not be found.")
     logger.info(f"Models to deprecate: {models}")
 
 
@@ -380,30 +380,30 @@ def deprecate_models(models):
 
     for model, model_info in model_info.items():
         # Add the tip message to the model doc page directly underneath the title
-        logging.info(f"Adding tip message to model doc page for model: {model}")
+        logger.info(f"Adding tip message to model doc page for model: {model}")
         insert_tip_to_model_doc(model_info["model_doc_path"], tip_message)
 
         # Move the model file to deprecated: src/transfomers/models/model -> src/transformers/models/deprecated/model
-        logging.info(f"Moving model files to deprecated for model: {model}")
+        logger.info(f"Moving model files to deprecated for model: {model}")
         move_model_files_to_deprecated(model)
 
         # Delete the model tests: tests/models/model
-        logging.info(f"Deleting model tests for model: {model}")
+        logger.info(f"Deleting model tests for model: {model}")
         delete_model_tests(model)
 
     # We do the following with all models passed at once to avoid having to re-write the file multiple times
 
     # Update the __init__.py file to point to the deprecated model.
-    logging.info(f"Updating __init__.py file to point to the deprecated models")
+    logger.info(f"Updating __init__.py file to point to the deprecated models")
     update_init_file("src/transformers/__init__.py", models)
 
     # Remove model references from other files
-    logging.info(f"Removing model references from other files")
+    logger.info(f"Removing model references from other files")
     remove_model_references_from_file("src/transformers/models/__init__.py", models, lambda line, model: model == line.strip().strip(","))
     remove_model_references_from_file("utils/slow_documentation_tests.txt", models, lambda line, model: "/" + model + "/" in line)
 
     # Remove model config classes from config check
-    logging.info(f"Removing model config classes from config checks")
+    logger.info(f"Removing model config classes from config checks")
     model_config_classes = [CONFIG_MAPPING[model_name].__name__ for model_name in models]
     remove_model_config_classes_from_config_check("src/transformers/configuration_utils.py", model_config_classes)
 
