71e8fc979b61bd8e9883768816bc4dd01636ebf1
Sangbum Daniel Choi <34004152+SangbumChoi@users.noreply.github.com>
2024-04-12 08:53:22 +0900

    Update src/transformers/models/rt_detr/image_processing_rt_detr.py
    Co-authored-by: NielsRogge <48327001+NielsRogge@users.noreply.github.com>

--- src/transformers/models/rt_detr/image_processing_rt_detr.py
+++ src/transformers/models/rt_detr/image_processing_rt_detr.py
@@ -12,7 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Image processor class for Deformable DETR."""
+"""Image processor class for RT-DETR."""
 
 import pathlib
 from typing import Any, Callable, Dict, Iterable, List, Optional, Tuple, Union
