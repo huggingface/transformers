e2607062396a841d32c1a5108c6bbafd6d9a4886
steven <steven.bucaillle@gmail.com>
2024-04-11 23:26:01 +0200

    Changed SuperGlue to support batching pairs of images and modified ImageMatchingOutput in consequences

--- src/transformers/utils/dummy_pt_objects.py
+++ src/transformers/utils/dummy_pt_objects.py
@@ -8153,13 +8153,6 @@ class SuperGlueForImageMatching(metaclass=DummyObject):
         requires_backends(self, ["torch"])
 
 
-class SuperGlueModel(metaclass=DummyObject):
-    _backends = ["torch"]
-
-    def __init__(self, *args, **kwargs):
-        requires_backends(self, ["torch"])
-
-
 class SuperGluePreTrainedModel(metaclass=DummyObject):
     _backends = ["torch"]
 
