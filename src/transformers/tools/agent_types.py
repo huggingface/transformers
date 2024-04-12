8c18841d7ee7dc1cab72880a324c31338764cc75
Aymeric <aymeric.roucher@gmail.com>
2024-04-12 15:54:09 +0200

    Change tool initialization logic

--- src/transformers/tools/agent_types.py
+++ src/transformers/tools/agent_types.py
@@ -223,55 +223,9 @@ def handle_agent_inputs(*args, **kwargs):
     return args, kwargs
 
 
-def handle_agent_outputs(outputs, output_types=None):
-    if isinstance(outputs, dict):
-        decoded_outputs = {}
-        for i, (k, v) in enumerate(outputs.items()):
-            if output_types is not None:
-                # If the class has defined outputs, we can map directly according to the class definition
-                if output_types[i] in AGENT_TYPE_MAPPING:
-                    decoded_outputs[k] = AGENT_TYPE_MAPPING[output_types[i]](v)
-                else:
-                    decoded_outputs[k] = AgentType(v)
-
-            else:
-                # If the class does not have defined output, then we map according to the type
-                for _k, _v in INSTANCE_TYPE_MAPPING.items():
-                    if isinstance(v, _k):
-                        decoded_outputs[k] = _v(v)
-                if k not in decoded_outputs:
-                    decoded_outputs[k] = AgentType[v]
-
-    elif isinstance(outputs, (list, tuple)):
-        decoded_outputs = type(outputs)()
-        for i, v in enumerate(outputs):
-            if output_types is not None:
-                # If the class has defined outputs, we can map directly according to the class definition
-                if output_types[i] in AGENT_TYPE_MAPPING:
-                    decoded_outputs.append(AGENT_TYPE_MAPPING[output_types[i]](v))
-                else:
-                    decoded_outputs.append(AgentType(v))
-            else:
-                # If the class does not have defined output, then we map according to the type
-                found = False
-                for _k, _v in INSTANCE_TYPE_MAPPING.items():
-                    if isinstance(v, _k):
-                        decoded_outputs.append(_v(v))
-                        found = True
-
-                if not found:
-                    decoded_outputs.append(AgentType(v))
-
+def handle_agent_outputs(output, output_type):
+    # If the class has defined outputs, we can map directly according to the class definition
+    if output_type in AGENT_TYPE_MAPPING:
+        return AGENT_TYPE_MAPPING[output_type](v)
     else:
-        if output_types[0] in AGENT_TYPE_MAPPING:
-            # If the class has defined outputs, we can map directly according to the class definition
-            decoded_outputs = AGENT_TYPE_MAPPING[output_types[0]](outputs)
-
-        else:
-            # If the class does not have defined output, then we map according to the type
-            for _k, _v in INSTANCE_TYPE_MAPPING.items():
-                if isinstance(outputs, _k):
-                    return _v(outputs)
-            return AgentType(outputs)
-
-    return decoded_outputs
+        return AgentType(output)
