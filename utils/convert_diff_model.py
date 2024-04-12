1fa297cf1f0d04ed90771e938320b3a4082886cc
Arthur Zucker <arthur.zucker@gmail.com>
2024-04-12 09:16:02 +0200

    push the conversion file

---
+++ utils/convert_diff_model.py
@@ -0,0 +1,55 @@
+from transformers.models.cohere.diff_cohere import CohereConverter
+import regex as re
+import inspect
+pattern = re.compile(r'(class|def|XXXConverter\.register)\s+[\w.()]+\s*:(\s*(?:[^class|def|XXXConverter\.register]|\n)+)', re.MULTILINE)
+import inspect
+
+
+from transformers.models.llama.modeling_llama import *
+from transformers.models.cohere.diff_cohere import *
+
+# TODO in order to get everything from LLAMA we need to copy each line from Llama
+# only updating the attention classes. 
+# Need to keep the order correct
+
+
+# 1. all the imports from the original file should be copied until end of header? __HEADER__
+# with open(CohereConverter.original_file, 'r') as file, open("result.py", "w+") as modeling:
+#         pass
+
+with open(CohereConverter.diff_file, 'r') as file, open("result.py", "w+") as modeling:
+    function_set = {}
+    for line in file: 
+            if "Converter.register" in line: # TODO use map() to map lines to this 
+                # write the code of the original model
+                class_to_use, old_class = re.search(r'Converter\.register\(\"(.*?)\", (.*?)\)', line).groups()
+                model_identifier_camel = re.findall(r'[A-Z][a-z0-9]*', class_to_use)[0]
+                old_model_identifier_camel = re.findall(r'[A-Z][a-z0-9]*', old_class)[0]
+                source_code = inspect.getsource(CohereConverter.registered_classes[class_to_use])
+                source_code = source_code.replace(old_class, class_to_use)
+                source_code = source_code.replace(old_model_identifier_camel, model_identifier_camel)
+                modeling.write(source_code)
+            elif match:=re.match(r"class (\w+)\((\w+)\):", line):
+                class_name, parent_class = match.groups()
+                # TODO import from the module
+                parent_class_def = inspect.getsource(eval(parent_class))
+
+                modeling.write(parent_class_def.split('\n')[0].replace(parent_class,class_name)+"\n")
+                # pattern = r"((    [\s\S]*?)\n\n(?=    \S))|((    [\s\S]*?)(?=\Z))"
+                pattern = re.compile(r"((    [\s\S]*?)\n(\n)?(?=    \S))|((    [\s\S]*?)(?=\Z))", re.MULTILINE)
+                
+                matches = pattern.finditer(parent_class_def)
+                function_set = {}
+                for match in matches:
+                    full_function = match.group()
+                    function_set[full_function.split("(")[0]] = full_function
+
+                class_def = inspect.getsource(eval(class_name))
+                matches = pattern.finditer(class_def)
+                for match in matches:
+                        full_function = match.group()
+                        function_set[full_function.split("(")[0]] = full_function
+
+                modeling.write("".join(function_set.values())) # TODO we wrote the code, next lines shall be ignored
+            elif line not in "".join(function_set.values()) or line=="\n":
+                modeling.write(line) 
