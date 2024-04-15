fcc457cc2952730be2eaa79900cd1ada3df308e8
Shane A <shanea@allenai.org>
2024-04-15 16:18:50 -0700

    Try fix incorrect characters in OLMoForCausalLM.forward doct test

--- src/transformers/models/olmo/modeling_olmo.py
+++ src/transformers/models/olmo/modeling_olmo.py
@@ -1155,7 +1155,7 @@ OLMO_GENERATION_EXAMPLE = r"""
     >>> # Generate
     >>> generate_ids = model.generate(inputs.input_ids, max_length=30)
     >>> tokenizer.batch_decode(generate_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0]
-    "Hey, are you conscious? Can you talk to me?\nI’m not sure if you’re conscious of this, but I’m"
+    "Hey, are you conscious? Can you talk to me?\nI'm not sure if you're conscious of this, but I'm"
     ```
 """
 
