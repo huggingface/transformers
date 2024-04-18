bafb694b9da50f8e6dd6407adee24bb4ceb72425
Jinlin <jinlinwang@stu.scu.edu.cn>
2024-04-18 18:30:55 +0800

    code_quality & repository_consistency

--- src/transformers/models/musicgen_melody/modeling_musicgen_melody.py
+++ src/transformers/models/musicgen_melody/modeling_musicgen_melody.py
@@ -12,7 +12,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-""" PyTorch Musicgen Melody model."""
+"""PyTorch Musicgen Melody model."""
+
 import copy
 import inspect
 import math
