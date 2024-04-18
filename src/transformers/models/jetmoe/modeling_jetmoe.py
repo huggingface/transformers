3aff0697bb463d2aa1b4a812ddd3b305a7b3b436
Yikang Shen <yikang.shn@gmail.com>
2024-04-17 22:33:55 -0400

    Update src/transformers/models/jetmoe/modeling_jetmoe.py
    Co-authored-by: Arthur <48595927+ArthurZucker@users.noreply.github.com>

--- src/transformers/models/jetmoe/modeling_jetmoe.py
+++ src/transformers/models/jetmoe/modeling_jetmoe.py
@@ -656,7 +656,7 @@ def rotate_half(x):
     return torch.cat((-x2, x1), dim=-1)
 
 
-# copied from transformers.models.llama.modeling_llama.apply_rotary_pos_emb
+# Copied from transformers.models.llama.modeling_llama.apply_rotary_pos_emb
 def apply_rotary_pos_emb(q, k, cos, sin, position_ids, unsqueeze_dim=2):
     """Applies Rotary Position Embedding to the query and key tensors.
 
