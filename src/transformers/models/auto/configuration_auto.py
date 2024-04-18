d04c87022ea7b7a724bced198e2bf89d55372b74
Eitan Turok <150733043+eitanturok@users.noreply.github.com>
2024-04-18 08:05:22 -0400

    fix configuration_auto.py

--- src/transformers/models/auto/configuration_auto.py
+++ src/transformers/models/auto/configuration_auto.py
@@ -21,19 +21,14 @@ from collections import OrderedDict
 from typing import List, Union
 
 from ...configuration_utils import PretrainedConfig
-from ...dynamic_module_utils import (
-    get_class_from_dynamic_module,
-    resolve_trust_remote_code,
-)
+from ...dynamic_module_utils import get_class_from_dynamic_module, resolve_trust_remote_code
 from ...utils import CONFIG_NAME, logging
 
 
 logger = logging.get_logger(__name__)
 
 
-from ..deprecated._archive_maps import (  # noqa: F401, E402
-    CONFIG_ARCHIVE_MAP_MAPPING_NAMES,
-)
+from ..deprecated._archive_maps import CONFIG_ARCHIVE_MAP_MAPPING_NAMES  # noqa: F401, E402
 
 
 CONFIG_MAPPING_NAMES = OrderedDict(
