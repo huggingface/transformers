745dc4720c344e5f5900e540e6c3ba3c41c1d77e
Eitan Turok <eitan.turok@databricks.com>
2024-04-18 02:51:21 +0000

    add PipelineTesterMixin

--- tests/models/dbrx/test_modeling_dbrx.py
+++ tests/models/dbrx/test_modeling_dbrx.py
@@ -25,6 +25,7 @@ from transformers.testing_utils import require_torch, slow, torch_device
 from ...generation.test_utils import GenerationTesterMixin
 from ...test_configuration_common import ConfigTester
 from ...test_modeling_common import ModelTesterMixin, ids_tensor, random_attention_mask
+from ...test_pipeline_mixin import PipelineTesterMixin
 
 
 if is_torch_available():
@@ -322,9 +323,10 @@ class DbrxModelTester:
 
 
 @require_torch
-class DbrxModelTest(ModelTesterMixin, GenerationTesterMixin, unittest.TestCase):
+class DbrxModelTest(ModelTesterMixin, GenerationTesterMixin, PipelineTesterMixin, unittest.TestCase):
     all_model_classes = (DbrxModel, DbrxForCausalLM) if is_torch_available() else ()
     all_generative_model_classes = (DbrxForCausalLM,) if is_torch_available() else ()
+    pipeline_model_mapping = {"text-generation": DbrxForCausalLM} if is_torch_available() else {}
     test_headmasking = False
     test_pruning = False
 
