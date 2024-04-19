9f7c43639aa638ff3397ef0cdb3e87d600ec6d9a
Matt <rocketknight1@gmail.com>
2024-04-19 14:12:12 +0100

    Fix doctests

--- src/transformers/pipelines/text_generation.py
+++ src/transformers/pipelines/text_generation.py
@@ -58,7 +58,7 @@ class TextGenerationPipeline(Pipeline):
     >>> from transformers import pipeline
 
     >>> generator = pipeline(model="HuggingFaceH4/zephyr-7b-beta")
-    >>> # Zephyr-beta is a conversational model, so let's pass it a chat instead of a single message
+    >>> # Zephyr-beta is a conversational model, so let's pass it a chat instead of a single string
     >>> generator([{"role": "user", "content": "What is the capital of France? Answer in one word."}], do_sample=False, max_new_tokens=2)
     [{'generated_text': [{'role': 'user', 'content': 'What is the capital of France? Answer in one word.'}, {'role': 'assistant', 'content': 'Paris'}]}]
     ```
