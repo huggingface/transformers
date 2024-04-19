629b87b9a778214a6171312f6a3a2c905910b71e
Raushan Turganbay <raushan.turganbay@alumni.nu.edu.kz>
2024-04-20 01:42:18 +0500

    Update src/transformers/generation/configuration_utils.py
    Co-authored-by: Joao Gante <joaofranciscocardosogante@gmail.com>

--- src/transformers/generation/configuration_utils.py
+++ src/transformers/generation/configuration_utils.py
@@ -475,7 +475,7 @@ class GenerationConfig(PushToHubMixin):
         if self.pad_token_id is not None and self.pad_token_id < 0:
             warnings.warn(
                 f"`pad_token_id` should be positive but got {self.pad_token_id}. This will cause errors when batch generating, if there is padding. "
-                "Please set `pas_token_id` explicitly by `model.generation_config.pad_token_id=PAD_TOKEN_ID` to avoid errors in generation"
+                "Please set `pas_token_id` explicitly by `model.generation_config.pad_token_id=PAD_TOKEN_ID` to avoid errors in generation, and ensure your `input_ids` input does not have negative values."
             )
 
         # Validation of attribute relations:
