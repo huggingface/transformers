755ba276681b4516a487ef6c0c1bca48e738e784
Raushan Turganbay <raushan@huggingface.co>
2024-04-19 21:32:52 +0500

    Do not remove half seq length in generation tests (#30016)
    * remove seq length from generation tests
    
    * style and quality
    
    * [test_all] & PR suggestion
    
    Co-authored-by: Joao Gante <joaofranciscocardosogante@gmail.com>
    
    * Update tests/generation/test_utils.py
    
    Co-authored-by: Arthur <48595927+ArthurZucker@users.noreply.github.com>
    
    * [test all] remove unused variables
    
    ---------
    
    Co-authored-by: Joao Gante <joaofranciscocardosogante@gmail.com>
    Co-authored-by: Arthur <48595927+ArthurZucker@users.noreply.github.com>

--- tests/models/speech_to_text/test_modeling_speech_to_text.py
+++ tests/models/speech_to_text/test_modeling_speech_to_text.py
@@ -285,7 +285,7 @@ class Speech2TextModelTest(ModelTesterMixin, GenerationTesterMixin, PipelineTest
     input_name = "input_features"
 
     def _get_input_ids_and_config(self, batch_size=2):
-        config, input_ids, attention_mask, max_length = GenerationTesterMixin._get_input_ids_and_config(self)
+        config, input_ids, attention_mask = GenerationTesterMixin._get_input_ids_and_config(self)
 
         # `input_ids` is actually `input_features` which is a 3D tensor.
         # We must overwrite the mask to make it 2D since the original `_get_input_ids_and_config` creates an
@@ -294,7 +294,7 @@ class Speech2TextModelTest(ModelTesterMixin, GenerationTesterMixin, PipelineTest
             sequence_length = input_ids.shape[1]
             attention_mask = torch.ones((batch_size, sequence_length), dtype=torch.long, device=attention_mask.device)
 
-        return config, input_ids, attention_mask, max_length
+        return config, input_ids, attention_mask
 
     def setUp(self):
         self.model_tester = Speech2TextModelTester(self)
