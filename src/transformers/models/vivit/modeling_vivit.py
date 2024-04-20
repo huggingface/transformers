c8a2e442be93c31be78bad54cf27196b92b48afc
Jacky Lee <jla524@sfu.ca>
2024-04-20 00:38:20 +0000

    feat: support for vivit

--- src/transformers/models/vivit/modeling_vivit.py
+++ src/transformers/models/vivit/modeling_vivit.py
@@ -387,6 +387,7 @@ class VivitPreTrainedModel(PreTrainedModel):
     base_model_prefix = "vivit"
     main_input_name = "pixel_values"
     supports_gradient_checkpointing = True
+    _no_split_modules = []
 
     def _init_weights(self, module):
         """Initialize the weights"""
