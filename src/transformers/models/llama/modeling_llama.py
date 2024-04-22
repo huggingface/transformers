2d5533945b72d903bfc1322e8e716ba0c657f791
ArthurZucker <arthur.zucker@gmail.com>
2024-04-22 03:54:17 -0400

    fix other models

--- src/transformers/models/llama/modeling_llama.py
+++ src/transformers/models/llama/modeling_llama.py
@@ -1328,7 +1328,7 @@ class LlamaForCausalLM(LlamaPreTrainedModel):
                 "position_ids": position_ids,
                 "cache_position": cache_position,
                 "past_key_values": past_key_values,
-                "use_cache": kwargs.get("use_cache"),
+                "use_cache": use_cache,
                 "attention_mask": attention_mask,
             }
         )
