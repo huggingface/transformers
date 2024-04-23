b37bb87a6027a8ccbcc4d80837c7fef84fa4008e
yikangshen <yikang.shn@gmail.com>
2024-04-23 16:44:11 -0400

    remove debug code

--- tests/test_modeling_common.py
+++ tests/test_modeling_common.py
@@ -4036,7 +4036,6 @@ class ModelTesterMixin:
                 else:
                     _ = model(dummy_input)
                     # with attention mask
-                    print(dummy_attention_mask)
                     _ = model(dummy_input, attention_mask=dummy_attention_mask)
 
     @is_pt_tf_cross_test
