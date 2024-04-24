0192b03b81e03aeee56330dc8601435f90e107b4
mobicham <hicham@mobiuslabs.com>
2024-04-24 14:59:22 +0000

    add force_hooks check in modeling_utils.py

--- src/transformers/modeling_utils.py
+++ src/transformers/modeling_utils.py
@@ -3744,6 +3744,9 @@ class PreTrainedModel(nn.Module, ModuleUtilsMixin, GenerationMixin, PushToHubMix
             }
             if "skip_keys" in inspect.signature(dispatch_model).parameters:
                 device_map_kwargs["skip_keys"] = model._skip_keys_device_placement
+            # For HQQ method we force-set the hooks for single GPU envs
+            if "force_hooks" in inspect.signature(dispatch_model).parameters and hf_quantizer is not None and hf_quantizer.quantization_config.quant_method == QuantizationMethod.HQQ:
+                device_map_kwargs["force_hooks"] = True
             if not is_fsdp_enabled() and not is_deepspeed_zero3_enabled():
                 dispatch_model(model, **device_map_kwargs)
 
