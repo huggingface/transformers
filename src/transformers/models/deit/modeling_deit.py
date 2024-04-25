1197b8d99bc2783a4d8b81a505926b9e1c54a19a
Jacky Lee <jla524@sfu.ca>
2024-04-25 00:49:54 +0000

    fix: other copies

--- src/transformers/models/deit/modeling_deit.py
+++ src/transformers/models/deit/modeling_deit.py
@@ -314,7 +314,7 @@ class DeiTLayer(nn.Module):
         outputs = self_attention_outputs[1:]  # add self attentions if we output attention weights
 
         # first residual connection
-        hidden_states = attention_output + hidden_states
+        hidden_states = attention_output.to(hidden_states.device) + hidden_states
 
         # in DeiT, layernorm is also applied after self-attention
         layer_output = self.layernorm_after(hidden_states)
