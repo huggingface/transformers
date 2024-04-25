de88c2afa17048f4c7faab31319d7e06bf9e0056
mobicham <hicham@mobiuslabs.com>
2024-04-25 16:03:52 +0000

    delete hqq_utils.py

--- src/transformers/utils/hqq_utils.py
+++ src/transformers/utils/hqq_utils.py
@@ -19,52 +19,52 @@ if is_torch_available():
     import torch
 
 
-# checks if a module is a leaf: doesn't have another module inside
-def is_leaf_module(module):
-    return len(module._modules) == 0
+# # checks if a module is a leaf: doesn't have another module inside
+# def is_leaf_module(module):
+#     return len(module._modules) == 0
 
 
-# Returns layers to ignores. These layers are typically not leaves we are interested in for storage and loading
-def get_ignore_layers(model):
-    layers = {""}
-    for name, module in model.named_modules():
-        if not is_leaf_module(module):
-            layers.add(name)
-    return list(layers)
+# # Returns layers to ignores. These layers are typically not leaves we are interested in for storage and loading
+# def get_ignore_layers(model):
+#     layers = {""}
+#     for name, module in model.named_modules():
+#         if not is_leaf_module(module):
+#             layers.add(name)
+#     return list(layers)
 
 
-# Checks if a quant config is an HQQ quant config
-def check_if_hqq_quant_config(quant_config):
-    if quant_config is None:
-        return False
-    q_keys = list(quant_config.keys())
-    q_vals = [quant_config[k] for k in quant_config][0]
-    if isinstance(q_vals, dict):
-        q_keys = q_keys + list([quant_config[k] for k in quant_config][0].keys())
-    return "weight_quant_params" in q_keys
+# # Checks if a quant config is an HQQ quant config
+# def check_if_hqq_quant_config(quant_config):
+#     if quant_config is None:
+#         return False
+#     q_keys = list(quant_config.keys())
+#     q_vals = [quant_config[k] for k in quant_config][0]
+#     if isinstance(q_vals, dict):
+#         q_keys = q_keys + list([quant_config[k] for k in quant_config][0].keys())
+#     return "weight_quant_params" in q_keys
 
 
-# Returns a new module from a dummy (meta) module and a dictionary of module name -> state_dict
-def load_hqq_module(module, weights, compute_dtype, device):
-    if is_hqq_available():
-        from hqq.core.quantize import HQQLinear
+# # Returns a new module from a dummy (meta) module and a dictionary of module name -> state_dict
+# def load_hqq_module(module, weights, compute_dtype, device):
+#     if is_hqq_available():
+#         from hqq.core.quantize import HQQLinear
 
-    if module.name not in weights:
-        try:
-            return module.to(dtype=compute_dtype, device=device)
-        except Exception:
-            return module
+#     if module.name not in weights:
+#         try:
+#             return module.to(dtype=compute_dtype, device=device)
+#         except Exception:
+#             return module
 
-    state_dict = weights[module.name]
-    if ("W_q" in state_dict) and ("meta" in state_dict):
-        module = HQQLinear(linear_layer=None, quant_config=None, compute_dtype=compute_dtype, device=device)
-        module.load_state_dict(state_dict)
-    else:
-        for key in state_dict:
-            setattr(
-                module,
-                key,
-                torch.nn.Parameter(state_dict[key].to(device=device, dtype=compute_dtype), requires_grad=False),
-            )
+#     state_dict = weights[module.name]
+#     if ("W_q" in state_dict) and ("meta" in state_dict):
+#         module = HQQLinear(linear_layer=None, quant_config=None, compute_dtype=compute_dtype, device=device)
+#         module.load_state_dict(state_dict)
+#     else:
+#         for key in state_dict:
+#             setattr(
+#                 module,
+#                 key,
+#                 torch.nn.Parameter(state_dict[key].to(device=device, dtype=compute_dtype), requires_grad=False),
+#             )
 
-    return module
+#     return module
