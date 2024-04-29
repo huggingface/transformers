d1eefa3cc4d2220fa459f62a01c96e99fdaced24
Arthur Zucker <arthur.zucker@gmail.com>
2024-04-29 10:33:57 +0200

    nits

--- .circleci/create_circleci_config.py
+++ .circleci/create_circleci_config.py
@@ -193,8 +193,8 @@ class CircleCIJob:
         steps.append({"run": {"name": "Run tests", "command": test_command}})
 
         steps.append({"run": {"name": "Show skip reasons", "command": f"python3 .circleci/parse_test_outputs.py --file ~/transformers/tests_output.txt --skip"}})
-        steps.append({"run": {"name": "Show fail reasons", "command": f"python3 .circleci/parse_test_outputs.py --file reports/{self.job_name}/failures_short.txt --fail"}})
-        steps.append({"run": {"name": "Show errors",       "command": f"python3 .circleci/parse_test_outputs.py --file reports/{self.job_name}/errors.txt --errors"}})
+        steps.append({"run": {"name": "Show fail reasons", "command": f"python3 .circleci/parse_test_outputs.py --file ~/transformers/tests_output.txt --fail"}})
+        steps.append({"run": {"name": "Show errors",       "command": f"python3 .circleci/parse_test_outputs.py --file ~/transformers/tests_output.txt --errors"}})
 
         steps.append({"store_test_results": {"path": "test-results"}})
         steps.append({"store_artifacts": {"path": "~/transformers/tests_output.txt"}})
