491198bbbe69c789ef61cc728fbfb2b872fc1cc4
Jacky Lee <jla524@sfu.ca>
2024-04-30 06:58:35 +0000

    remove: extra space

--- src/transformers/models/dpt/modeling_dpt.py
+++ src/transformers/models/dpt/modeling_dpt.py
@@ -865,7 +865,6 @@ DPT_INPUTS_DOCSTRING = r"""
     DPT_START_DOCSTRING,
 )
 class DPTModel(DPTPreTrainedModel):
-
     def __init__(self, config, add_pooling_layer=True):
         super().__init__(config)
         self.config = config
