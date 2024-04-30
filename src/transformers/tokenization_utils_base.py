66118772aa52a89450cae088cfbd43222cf4dabe
Younes Belkada <younesbelkada@gmail.com>
2024-04-30 15:33:11 +0200

    add comment for clarity

--- src/transformers/tokenization_utils_base.py
+++ src/transformers/tokenization_utils_base.py
@@ -2127,6 +2127,9 @@ class PreTrainedTokenizerBase(SpecialTokensMixin, PushToHubMixin):
         from_slow = kwargs.get("from_slow", False)
         from_gguf = kwargs.get("from_gguf", False)
         has_tokenizer_file = resolved_vocab_files.get("tokenizer_file", None) is not None
+        
+        # If one passes a GGUF file path to `from_gguf` there is no need for this check as the tokenizer will be
+        # loaded directly from the GGUF file.
         if (from_slow or not has_tokenizer_file) and cls.slow_tokenizer_class is not None and not from_gguf:
             slow_tokenizer = (cls.slow_tokenizer_class)._from_pretrained(
                 copy.deepcopy(resolved_vocab_files),
