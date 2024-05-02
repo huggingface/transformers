a13706712d0d59fe7ec7b3744453f642fdf65939
Arthur Zucker <arthur.zucker@gmail.com>
2024-05-02 10:27:10 +0200

    tapas as well

--- src/transformers/models/tapas/modeling_tf_tapas.py
+++ src/transformers/models/tapas/modeling_tf_tapas.py
@@ -71,6 +71,15 @@ if is_tensorflow_probability_available():
             "It seems you have `tensorflow_probability` installed with the wrong tensorflow version. "
             "Please try to reinstall it following the instructions here: https://github.com/tensorflow/probability."
         )
+else:
+    try:
+        import tensorflow_probability as tfp
+
+        # On the first call, check whether a compatible version of TensorFlow is installed
+        # TensorFlow Probability depends on a recent stable release of TensorFlow
+        _ = tfp.distributions.Normal(loc=0.0, scale=1.0) 
+    except:
+        pass
 
 _CONFIG_FOR_DOC = "TapasConfig"
 _CHECKPOINT_FOR_DOC = "google/tapas-base"
