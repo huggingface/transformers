0f6ac580342c4ffed0ce7bd49c7af30f83ddd0ef
Joao Gante <joao@huggingface.co>
2024-05-03 10:08:29 +0000

    missing not

--- src/transformers/generation/logits_process.py
+++ src/transformers/generation/logits_process.py
@@ -1171,7 +1171,7 @@ class NoBadWordsLogitsProcessor(SequenceBiasLogitsProcessor):
         self._validate_arguments()
 
         # Filter EOS token from bad_words_ids
-        if eos_token_id is None:
+        if eos_token_id is not None:
             if not isinstance(eos_token_id, torch.Tensor):
                 if isinstance(eos_token_id, int):
                     eos_token_id = [eos_token_id]
