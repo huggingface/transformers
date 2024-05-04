df93669255e1c4c616a37fb4f7341505caad0bbf
eigen2017 <wobushiliu2@gmail.com>
2024-05-04 20:45:43 +0800

    fix ci, ruff format again

--- src/transformers/models/qwen2_moe/modeling_qwen2_moe.py
+++ src/transformers/models/qwen2_moe/modeling_qwen2_moe.py
@@ -45,6 +45,7 @@ from ...utils import (
 )
 from .configuration_qwen2_moe import Qwen2MoeConfig
 
+
 if is_flash_attn_2_available():
     from flash_attn import flash_attn_func, flash_attn_varlen_func
     from flash_attn.bert_padding import index_first_axis, pad_input, unpad_input  # noqa
