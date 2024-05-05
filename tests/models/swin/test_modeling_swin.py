05c82cfa9b1dee13a5dee9c66a7ac51c9a4c4436
the-neural-networker <abhirooptejomay@gmail.com>
2024-05-04 23:39:59 -0400

    add interpolation of positional encoding support to swin

--- tests/models/swin/test_modeling_swin.py
+++ tests/models/swin/test_modeling_swin.py
@@ -493,6 +493,32 @@ class SwinModelIntegrationTest(unittest.TestCase):
         expected_slice = torch.tensor([-0.0948, -0.6454, -0.0921]).to(torch_device)
         self.assertTrue(torch.allclose(outputs.logits[0, :3], expected_slice, atol=1e-4))
 
+    @slow
+    def test_inference_interpolate_pos_encoding(self):
+        # ViT models have an `interpolate_pos_encoding` argument in their forward method,
+        # allowing to interpolate the pre-trained position embeddings in order to use
+        # the model on higher resolutions. 
+        model = SwinModel.from_pretrained("microsoft/swin-tiny-patch4-window7-224").to(torch_device)
+
+        image_processor = AutoImageProcessor.from_pretrained("microsoft/swin-tiny-patch4-window7-224", size=480)
+        image = Image.open("./tests/fixtures/tests_samples/COCO/000000039769.png")
+        inputs = image_processor(images=image, return_tensors="pt")
+        pixel_values = inputs.pixel_values.to(torch_device)
+    
+        # forward pass
+        with torch.no_grad():
+            outputs = model(pixel_values, interpolate_pos_encoding=True)
+
+        # verify the logits
+        expected_shape = torch.Size((1, 225, 768))
+        self.assertEqual(outputs.last_hidden_state.shape, expected_shape)
+
+        expected_slice = torch.tensor(
+            [[0.3269,  0.2602, -0.3718], [-0.6080, -1.2802, -0.4047], [0.0146, -0.0850,  0.0518]]
+        ).to(torch_device)
+
+        self.assertTrue(torch.allclose(outputs.last_hidden_state[0, :3, :3], expected_slice, atol=1e-4))
+
 
 @require_torch
 class SwinBackboneTest(unittest.TestCase, BackboneTesterMixin):
