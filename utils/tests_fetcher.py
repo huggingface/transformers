57761189ee4149bacb726d38b7a3896d38de1814
Arthur Zucker <arthur.zucker@gmail.com>
2024-05-07 18:55:30 +0200

    update

--- utils/tests_fetcher.py
+++ utils/tests_fetcher.py
@@ -1279,6 +1279,13 @@ if __name__ == "__main__":
             print("main branch detected, fetching tests against last commit.")
             diff_with_last_commit = True
 
+        infer_tests_to_run(
+            args.output_file,
+            diff_with_last_commit=diff_with_last_commit,
+            json_output_file=args.json_output_file,
+            filter_models=(not (commit_flags["no_filter"] or is_main_branch)),
+        )
+
         if not commit_flags["test_all"]:
             try:
                 infer_tests_to_run(
