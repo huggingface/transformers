19a47968fe08bfb71d3d0b35df4a35d4bcb6d611
Arthur Zucker <arthur.zucker@gmail.com>
2024-05-07 22:59:11 +0200

    update

--- .circleci/create_circleci_config.py
+++ .circleci/create_circleci_config.py
@@ -87,7 +87,7 @@ class CircleCIJob:
         if self.parallelism is None:
             self.parallelism = 1
         else:
-            self.parallelism = min(len(self.tests_to_run) // self.num_test_files_per_worker, 16)
+            self.parallelism = min(len(self.tests_to_run) // self.num_test_files_per_worker, 8)
 
     def to_dict(self):
         env = COMMON_ENV_VARIABLES.copy()
