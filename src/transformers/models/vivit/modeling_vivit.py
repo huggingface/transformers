23d2812898d1d51fd564c3472ac1001c60d100eb
Jacky Lee <39754370+jla524@users.noreply.github.com>
2024-05-08 09:51:51 -0700

    Update src/transformers/models/vivit/modeling_vivit.py
    Co-authored-by: amyeroberts <22614925+amyeroberts@users.noreply.github.com>

--- src/transformers/models/vivit/modeling_vivit.py
+++ src/transformers/models/vivit/modeling_vivit.py
@@ -670,7 +670,7 @@ class VivitForVideoClassification(VivitPreTrainedModel):
         labels: Optional[torch.LongTensor] = None,
         output_attentions: Optional[bool] = None,
         output_hidden_states: Optional[bool] = None,
-        interpolate_pos_encoding: Optional[bool] = None,
+        interpolate_pos_encoding: bool = False,
         return_dict: Optional[bool] = None,
     ) -> Union[Tuple[torch.FloatTensor], ImageClassifierOutput]:
         r"""
