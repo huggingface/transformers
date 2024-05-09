ba6a6b0fb72c9f271a748dfba9d0725414ae6449
ariG23498 <aritra.born2fly@gmail.com>
2024-05-09 21:37:48 +0530

    fix docstring

--- src/transformers/models/mistral/modeling_tf_mistral.py
+++ src/transformers/models/mistral/modeling_tf_mistral.py
@@ -865,8 +865,8 @@ class TFMistralForCausalLM(TFMistralPreTrainedModel, TFCausalLanguageModelingLos
         r"""
         Args:
             labels (`tf.Tensor` of shape `(batch_size, sequence_length)`, *optional*):
-                Labels for computing the masked language modeling loss. Indices should either be in `[0, ...,
-                config.vocab_size]` or -100 (see `input_ids` docstring). Tokens with indices set to `-100` are ignored
+                Labels for computing the masked language modeling loss. Indices should either be in `[0, ..., config.vocab_size]`
+                or -100 (see `input_ids` docstring). Tokens with indices set to `-100` are ignored
                 (masked), the loss is only computed for the tokens with labels in `[0, ..., config.vocab_size]`.
         ```"""
 
