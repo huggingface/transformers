7290bc11cea5ea140965438afaf9316c6d258739
raushan <raushan@huggingface.co>
2024-05-10 10:56:22 +0200

    forgot `melody` version

--- src/transformers/models/musicgen_melody/modeling_musicgen_melody.py
+++ src/transformers/models/musicgen_melody/modeling_musicgen_melody.py
@@ -1689,7 +1689,7 @@ class MusicgenMelodyForCausalLM(MusicgenMelodyPreTrainedModel):
 
         elif is_sample_gen_mode:
             # 11. prepare logits warper
-            logits_warper = self._get_logits_warper(generation_config)
+            logits_warper = self._get_logits_warper(generation_config, device=input_ids.device)
 
             # expand input_ids with `num_return_sequences` additional sequences per batch
             input_ids, model_kwargs = self._expand_inputs_for_generation(
@@ -2705,7 +2705,7 @@ class MusicgenMelodyForConditionalGeneration(PreTrainedModel):
 
         elif is_sample_gen_mode:
             # 11. prepare logits warper
-            logits_warper = self._get_logits_warper(generation_config)
+            logits_warper = self._get_logits_warper(generation_config, device=input_ids.device)
 
             # expand input_ids with `num_return_sequences` additional sequences per batch
             input_ids, model_kwargs = self._expand_inputs_for_generation(
