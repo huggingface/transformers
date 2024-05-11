f80667df8c5928e04d25566c0e6e95483953e408
Lin, Fanli <fanli.lin@intel.com>
2024-05-11 09:30:53 -0400

    change test names and add more cases

--- tests/pipelines/test_pipelines_common.py
+++ tests/pipelines/test_pipelines_common.py
@@ -521,23 +521,36 @@ class PipelineUtilsTest(unittest.TestCase):
         self.assertEqual(expected_output, actual_output)
 
     @require_torch_accelerator
-    def test_pipeline_should_not_move_model(self):
-        # Test when model is already on device, pipeline shouldn't move model
+    def test_pipeline_no_device(self):
+        # Test when no device is passed to pipeline
         import torch
 
         from transformers import AutoModelForCausalLM
 
         tokenizer = AutoTokenizer.from_pretrained("hf-internal-testing/tiny-random-bert")
+        # Case 1: Model is manually moved to device
         model = AutoModelForCausalLM.from_pretrained(
             "hf-internal-testing/tiny-random-bert", torch_dtype=torch.float16
         ).to(torch_device)
         model_device = model.device
-        # No device passed to pipeline
         pipe = pipeline("text-generation", model=model, tokenizer=tokenizer)
         self.assertEqual(pipe.model.device, model_device)
+        # Case 2: Model is loaded by accelerate
+        model = AutoModelForCausalLM.from_pretrained(
+            "hf-internal-testing/tiny-random-bert", device_map=torch_device, torch_dtype=torch.float16
+        )
+        model_device = model.device
+        pipe = pipeline("text-generation", model=model, tokenizer=tokenizer)
+        self.assertEqual(pipe.model.device, model_device)
+        # Case 3: device_map is passed to model and device is passed to pipeline
+        model = AutoModelForCausalLM.from_pretrained(
+            "hf-internal-testing/tiny-random-bert", device_map=torch_device, torch_dtype=torch.float16
+        )
+        with self.assertRaises(ValueError):
+            pipe = pipeline("text-generation", model=model, device="cpu", tokenizer=tokenizer)
 
     @require_torch_multi_accelerator
-    def test_pipeline_should_move_model(self):
+    def test_pipeline_device_not_equal_model_device(self):
         # Test when device ids are different, pipeline should move the model to the passed device id
         import torch
 
@@ -549,7 +562,7 @@ class PipelineUtilsTest(unittest.TestCase):
             "hf-internal-testing/tiny-random-bert", torch_dtype=torch.float16
         ).to(model_device)
         target_device = f"{torch_device}:0"
-        assert model_device != model_device
+        assert model_device != target_device
         pipe = pipeline("text-generation", model=model, device=target_device, tokenizer=tokenizer)
         assert pipe.model.device == torch.device(target_device)
 
