d7c435940bd57e7bcbb93a375a8bbddcad363f1a
Cyril Vallez <cyril.vallez@gmail.com>
2024-05-13 20:04:12 +0200

    correct type hint

--- src/transformers/cache_utils.py
+++ src/transformers/cache_utils.py
@@ -748,7 +748,7 @@ class EfficientDynamicCache(Cache):
     @classmethod
     def from_legacy_cache(
         cls,
-        past_key_values: Optional[Tuple[Tuple[torch.Tensor]], Tuple[Tuple[List[torch.Tensor]]]] = None,
+        past_key_values: Optional[Union[Tuple[Tuple[torch.Tensor]], Tuple[Tuple[List[torch.Tensor]]]]] = None,
         restack_limit: Optional[int] = None,
     ) -> "EfficientDynamicCache":
         """Converts a cache in the legacy cache format Tuple[Tuple[torch.Tensor]] or Tuple[Tuple[List[torch.Tensor]]] into an equivalent `EfficientDynamicCache`."""
