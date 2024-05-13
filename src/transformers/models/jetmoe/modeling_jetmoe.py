1b8ed083c7466af40192844fa2a6a54e2aa875de
yikangshen <yikang.shn@gmail.com>
2024-05-13 13:55:49 -0400

    fix format

--- src/transformers/models/jetmoe/modeling_jetmoe.py
+++ src/transformers/models/jetmoe/modeling_jetmoe.py
@@ -1160,7 +1160,7 @@ class JetMoeModel(JetMoePreTrainedModel):
         next_cache = next_decoder_cache if use_cache else None
         if return_legacy_cache:
             next_cache = next_cache.to_legacy_cache()
-            
+
         if not return_dict:
             return tuple(v for v in [hidden_states, next_cache, all_hidden_states, all_self_attns] if v is not None)
         return MoeModelOutputWithPast(
