c824771418e20682c98da5be5820f36872c5f39a
Pablo <pablo.montalvo.leroux@gmail.com>
2024-05-14 18:24:03 +0200

    use cache_position

--- src/transformers/models/paligemma/modeling_paligemma.py
+++ src/transformers/models/paligemma/modeling_paligemma.py
@@ -407,13 +407,14 @@ class PaliGemmaForConditionalGeneration(PaliGemmaPreTrainedModel):
                 if past_key_values is not None and pixel_values is not None and input_ids.shape[1] == 1:
                     # Retrieve the first layer to inspect the logits and mask out the hidden states
                     # that are set to 0
+                    # TODO @molbap this will only work for dynamic cache.
                     first_layer_past_key_value = past_key_values[0][0][:, :, :, 0]
 
                     # Sum all dimensions of head_dim (-2) to avoid random errors such as: https://github.com/huggingface/transformers/pull/28032#issuecomment-1863691941
                     batch_index, non_attended_tokens = torch.where(first_layer_past_key_value.float().sum(-2) == 0)
 
                     # Get the target length
-                    target_seqlen = first_layer_past_key_value.shape[-1] + 1
+                    target_seqlen = cache_position[-1] + 1
 
                     extended_attention_mask = torch.ones(
                         (attention_mask.shape[0], target_seqlen - attention_mask.shape[1]),
