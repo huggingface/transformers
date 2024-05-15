709429a14124d9277311a67b1a2c2bbc47b822c6
Arthur Zucker <arthur.zucker@gmail.com>
2024-05-15 17:21:40 +0200

    updates

--- src/transformers/models/gemma/diff_gemma.py
+++ src/transformers/models/gemma/diff_gemma.py
@@ -29,7 +29,6 @@ from torch import nn
 from ...activations import ACT2FN
 from ...cache_utils import Cache
 
-from ...modeling_outputs import BaseModelOutputWithPast
 from ...pytorch_utils import ALL_LAYERNORM_LAYERS 
 from ...utils import (
     logging,
@@ -229,26 +228,66 @@ GemmaConverter.register("GemmaDecoderLayer", LlamaDecoderLayer)
 GemmaConverter.register("GemmaPreTrainedModel", LlamaPreTrainedModel)
 
 class GemmaModel(LlamaModel):
+    
+    @add_start_docstrings_to_model_forward("")
+    @replace_return_docstrings(output_type=CausalLMOutputWithPast, config_class="""_CONFIG_FOR_DOC""")
     def forward(
         self,
         input_ids: torch.LongTensor = None,
         attention_mask: Optional[torch.Tensor] = None,
         position_ids: Optional[torch.LongTensor] = None,
-        past_key_values: Optional[List[torch.FloatTensor]] = None,
+        past_key_values: Optional[Union[Cache, List[torch.FloatTensor]]] = None,
         inputs_embeds: Optional[torch.FloatTensor] = None,
+        labels: Optional[torch.LongTensor] = None,
         use_cache: Optional[bool] = None,
         output_attentions: Optional[bool] = None,
         output_hidden_states: Optional[bool] = None,
         return_dict: Optional[bool] = None,
         cache_position: Optional[torch.LongTensor] = None,
-    ) -> Union[Tuple, BaseModelOutputWithPast]:
-    
-        if inputs_embeds is None:
-            inputs_embeds = self.embed_tokens(input_ids)
-        hidden_states = inputs_embeds
-        normalizer = torch.tensor(self.config.hidden_size**0.5, dtype=hidden_states.dtype)
-        hidden_states = hidden_states * normalizer
-        return super().forward(None, attention_mask, position_ids, past_key_values, inputs_embeds, use_cache, output_attentions, output_hidden_states, return_dict, cache_position)
+    ) -> Union[Tuple, CausalLMOutputWithPast]:
+        output_attentions = output_attentions | self.config.output_attentions
+        output_hidden_states = output_hidden_states| self.config.output_hidden_states
+        return_dict = return_dict | self.config.use_return_dict
 
+        outputs = self.model(
+            input_ids=input_ids,
+            attention_mask=attention_mask,
+            position_ids=position_ids,
+            past_key_values=past_key_values,
+            inputs_embeds=inputs_embeds,
+            use_cache=use_cache,
+            output_attentions=output_attentions,
+            output_hidden_states=output_hidden_states,
+            return_dict=return_dict,
+            cache_position=cache_position,
+        )
+
+        hidden_states = outputs[0]
+        logits = self.lm_head(hidden_states)
+        logits = logits.float()
+        loss = None
+        if labels is not None:
+            # Shift so that tokens < n predict n
+            shift_logits = logits[..., :-1, :].contiguous()
+            shift_labels = labels[..., 1:].contiguous()
+            # Flatten the tokens
+            loss_fct = CrossEntropyLoss()
+            shift_logits = shift_logits.view(-1, self.config.vocab_size)
+            shift_labels = shift_labels.view(-1)
+            # Enable model parallelism
+            shift_labels = shift_labels.to(shift_logits.device)
+            loss = loss_fct(shift_logits, shift_labels)
+
+        if not return_dict:
+            output = (logits,) + outputs[1:]
+            return (loss,) + output if loss is not None else output
+
+        return CausalLMOutputWithPast(
+            loss=loss,
+            logits=logits,
+            past_key_values=outputs.past_key_values,
+            hidden_states=outputs.hidden_states,
+            attentions=outputs.attentions,
+        )
 
 GemmaConverter.register("GemmaForCausalLM", LlamaForCausalLM)
