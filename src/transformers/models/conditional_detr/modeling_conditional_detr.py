33b6aabd08cd0eed404c3c12df6e56d69a869db2
David <37246112+conditionedstimulus@users.noreply.github.com>
2024-05-15 12:19:43 +0200

    Remove unused module DETR based models (#30823)
    * removing heads for classification from DETR models.
    
    * quality fix

--- src/transformers/models/conditional_detr/modeling_conditional_detr.py
+++ src/transformers/models/conditional_detr/modeling_conditional_detr.py
@@ -1091,25 +1091,6 @@ class ConditionalDetrDecoderLayer(nn.Module):
         return outputs
 
 
-# Copied from transformers.models.detr.modeling_detr.DetrClassificationHead with Detr->ConditionalDetr
-class ConditionalDetrClassificationHead(nn.Module):
-    """Head for sentence-level classification tasks."""
-
-    def __init__(self, input_dim: int, inner_dim: int, num_classes: int, pooler_dropout: float):
-        super().__init__()
-        self.dense = nn.Linear(input_dim, inner_dim)
-        self.dropout = nn.Dropout(p=pooler_dropout)
-        self.out_proj = nn.Linear(inner_dim, num_classes)
-
-    def forward(self, hidden_states: torch.Tensor):
-        hidden_states = self.dropout(hidden_states)
-        hidden_states = self.dense(hidden_states)
-        hidden_states = torch.tanh(hidden_states)
-        hidden_states = self.dropout(hidden_states)
-        hidden_states = self.out_proj(hidden_states)
-        return hidden_states
-
-
 # Copied from transformers.models.detr.modeling_detr.DetrMLPPredictionHead with DetrMLPPredictionHead->MLP
 class MLP(nn.Module):
     """
