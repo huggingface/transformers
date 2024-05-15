215bcfe194081e4fcb4269759560f64c970c1eb4
RUFFY-369 <prakarshkaushik369@gmail.com>
2024-05-16 02:07:28 +0530

    fix:bug for integration tests;test:add test batching equivalence in iris tests

--- tests/models/iris/test_modeling_iris.py
+++ tests/models/iris/test_modeling_iris.py
@@ -262,7 +262,7 @@ class IrisModelTest(ModelTesterMixin, GenerationTesterMixin, PipelineTesterMixin
                 dict_output = model(**dict_inputs, return_dict=True, **additional_kwargs).to_tuple()
                 tuple_dummy = ()
                 dict_dummy = ()
-                #loss(specially actor critic loss) and actor critic logit actions are not deterministic so, squeeze them out for the test
+                #losses(specially actor critic loss) and actor critic logit actions are not deterministic so, squeeze them out for the test
                 for i, output in enumerate(tuple_output): tuple_dummy = tuple_dummy+(output,) if i!= 1 and i !=2 else tuple_dummy
                 for i, output in enumerate(tuple_output): dict_dummy = dict_dummy+(output,) if i!= 1 and i !=2 else dict_dummy
                 tuple_output = tuple_dummy
@@ -330,11 +330,110 @@ class IrisModelTest(ModelTesterMixin, GenerationTesterMixin, PipelineTesterMixin
                 check_equivalence(
                     model, tuple_inputs, dict_inputs, {"output_hidden_states": True, "output_attentions": True}
                 )
-    
-    @unittest.skip("IRIS model is not deterministic.")
+    @is_flaky(max_attempts=3, description="`hidden states, total losses, logits actions` are flaky.")
     def test_batching_equivalence(self):
-        pass
+        """
+        Tests that the model supports batching and that the output is the nearly the same for the same input in
+        different batch sizes.
+        (Why "nearly the same" not "exactly the same"? Batching uses different matmul shapes, which often leads to
+        different results: https://github.com/huggingface/transformers/issues/25420#issuecomment-1775317535)
+        """
 
+        def get_tensor_equivalence_function(batched_input):
+            # models operating on continuous spaces have higher abs difference than LMs
+            # instead, we can rely on cos distance for image/speech models, similar to `diffusers`
+            if "input_ids" not in batched_input:
+                return lambda tensor1, tensor2: (
+                    1.0 - F.cosine_similarity(tensor1.float().flatten(), tensor2.float().flatten(), dim=0, eps=1e-38)
+                )
+            return lambda tensor1, tensor2: torch.max(torch.abs(tensor1 - tensor2))
+
+        def recursive_check(batched_object, single_row_object, model_name, key):
+            if isinstance(batched_object, (list, tuple)):
+                for batched_object_value, single_row_object_value in zip(batched_object, single_row_object):
+                    recursive_check(batched_object_value, single_row_object_value, model_name, key)
+            elif isinstance(batched_object, dict):
+                for batched_object_value, single_row_object_value in zip(
+                    batched_object.values(), single_row_object.values()
+                ):
+                    recursive_check(batched_object_value, single_row_object_value, model_name, key)
+            # do not compare returned loss (0-dim tensor) / codebook ids (int) / caching objects
+            elif batched_object is None or not isinstance(batched_object, torch.Tensor):
+                return
+            elif batched_object.dim() == 0:
+                return
+            else:
+                # indexing the first element does not always work
+                # e.g. models that output similarity scores of size (N, M) would need to index [0, 0]
+                slice_ids = [slice(0, index) for index in single_row_object.shape]
+                batched_row = batched_object[slice_ids]
+                self.assertFalse(
+                    torch.isnan(batched_row).any(), f"Batched output has `nan` in {model_name} for key={key}"
+                )
+                self.assertFalse(
+                    torch.isinf(batched_row).any(), f"Batched output has `inf` in {model_name} for key={key}"
+                )
+                self.assertFalse(
+                    torch.isnan(single_row_object).any(), f"Single row output has `nan` in {model_name} for key={key}"
+                )
+                self.assertFalse(
+                    torch.isinf(single_row_object).any(), f"Single row output has `inf` in {model_name} for key={key}"
+                )
+                #hidden states are also non deterministic(tight but generous tolerance for the test)
+                TOLERANCE = 1e-01 if key == "hidden_states" else 1e-03
+                    
+                self.assertTrue(
+                    (equivalence(batched_row, single_row_object)) <= TOLERANCE,
+                    msg=(
+                        f"Batched and Single row outputs are not equal in {model_name} for key={key}. "
+                        f"Difference={equivalence(batched_row, single_row_object)}."
+                    ),
+                )
+
+        config, batched_input = self.model_tester.prepare_config_and_inputs_for_common()
+        equivalence = get_tensor_equivalence_function(batched_input)
+
+        for model_class in self.all_model_classes:
+            config.output_hidden_states = True
+
+            model_name = model_class.__name__
+            if hasattr(self.model_tester, "prepare_config_and_inputs_for_model_class"):
+                config, batched_input = self.model_tester.prepare_config_and_inputs_for_model_class(model_class)
+            batched_input_prepared = self._prepare_for_class(batched_input, model_class)
+            model = model_class(config).to(torch_device).eval()
+
+            batch_size = self.model_tester.batch_size
+            single_row_input = {}
+            for key, value in batched_input_prepared.items():
+                if isinstance(value, torch.Tensor) and value.shape[0] % batch_size == 0:
+                    # e.g. musicgen has inputs of size (bs*codebooks). in most cases value.shape[0] == batch_size
+                    single_batch_shape = value.shape[0] // batch_size
+                    single_row_input[key] = value[:single_batch_shape]
+                else:
+                    single_row_input[key] = value
+
+            with torch.no_grad():
+                model_batched_output = model(**batched_input_prepared)
+                model_row_output = model(**single_row_input)
+                #losses(specially actor critic loss) and actor critic logit actions are not deterministic so, zero them out for this test to pass
+                model_batched_output["losses"] = torch.tensor([1e-03])
+                model_row_output["losses"] = torch.tensor([1e-03])
+                model_batched_output["action_preds"] = torch.tensor([1e-03])
+                model_row_output["action_preds"] = torch.tensor([1e-03])
+                
+
+            if isinstance(model_batched_output, torch.Tensor):
+                model_batched_output = {"model_output": model_batched_output}
+                model_row_output = {"model_output": model_row_output}
+
+            for key in model_batched_output:
+                # DETR starts from zero-init queries to decoder, leading to cos_similarity = `nan`
+                if hasattr(self, "zero_init_hidden_state") and "decoder_hidden_states" in key:
+                    model_batched_output[key] = model_batched_output[key][1:]
+                    model_row_output[key] = model_row_output[key][1:]
+                recursive_check(model_batched_output[key], model_row_output[key], model_name, key)
+
+    
     @unittest.skip("Cannot configure Iris to output a smaller backbone and there is no tiny model available")
     def test_model_is_small(self):
         pass
@@ -350,52 +449,60 @@ class IrisModelIntegrationTest(unittest.TestCase):
         """
 
         NUM_STEPS = 1  # number of steps of prediction with all the three components i.e., tokenizer, world model & actor critic we will perform
+        batch_size = 1
         model = IrisModel.from_pretrained("ruffy369/iris-breakout")
         model = model.to(torch_device)
+        model.eval()
         config = model.config
         torch.manual_seed(0)
         # state = torch.randn(1, 1, config.state_dim).to(device=torch_device, dtype=torch.float32)  # env.reset()
 
-        expected_outputs = torch.tensor([[[-2.1720,  0.1622, -4.3326,  4.6579]],
-                                        [[-3.4466, -5.1955,  4.0899,  3.4191]],
-                                        [[-1.3850,  1.0004, -3.9815,  3.1493]],
-                                        [[-3.1353, -5.3218, -2.6076, 10.3681]]], device=torch_device)
+        expected_outputs = torch.tensor([[[-56.9865,   6.1326,  -5.7514],[-27.8220,   0.2719,   0.0750],
+                                        [-46.6914,   6.2149,  -6.2242],[-22.3273,   3.4510,  -3.3912],
+                                        [-56.9804,   8.9555,  -9.1860],[-58.9221,   8.7978,  -8.8263],
+                                        [-55.0845,   8.2940,  -8.3846],[-49.0554,   7.8225,  -7.8627],
+                                        [-21.2931,   2.8445,  -2.9540],[-56.0253,   8.2314,  -8.4516],
+                                        [-45.9430,   7.0588,  -7.3609],[-36.2316,   5.1116,  -5.3988],
+                                        [-53.2926,   6.6223,  -6.8106],[-23.5454,   2.6830,  -2.9352],
+                                        [-42.4132,   5.0499,  -5.4652],[-46.1090,   5.9608,  -6.1060],
+                                        [-61.1128,   8.9312,  -9.0944],[-71.0932,   9.6851,  -9.7979],
+                                        [-65.7601,   8.2317,  -8.2783],[-45.1447,   6.7321,  -6.9647]]], device=torch_device)
 
-        observations_tokenizer = floats_tensor((3,1,config.in_channels,config.resolution,config.resolution))
-        actions_tokenizer = ids_tensor((3,1),vocab_size =4).long()
-        rewards_tokenizer = ids_tensor((3,1),vocab_size =8)
+        observations_tokenizer = torch.randn(batch_size,1,config.in_channels,config.resolution,config.resolution).to(device=torch_device, dtype=torch.float32)
+        actions_tokenizer = torch.randint(0,4,(batch_size,1)).to(device=torch_device, dtype=torch.long)
+        rewards_tokenizer = torch.randint(0,8,(batch_size,1))
         zeros = torch.zeros_like(rewards_tokenizer)
         # Rewards are given depending on which color brick is broken in 'Breakout' Atari env
         zeros[((rewards_tokenizer==7)|( rewards_tokenizer==4)|(rewards_tokenizer==1))]=1
-        rewards_tokenizer = torch.mul(zeros,rewards_tokenizer).float()
-        ends_tokenizer = torch.zeros(3,1).long()
-        for i in range(3):
-            ends_tokenizer[i,ids_tensor((1,),vocab_size=1).item()]=1 if floats_tensor((1,)).item()<0.5 else 0
-        mask_padding_tokenizer = torch.ones(3,1).bool()
+        rewards_tokenizer = torch.mul(zeros,rewards_tokenizer).to(device=torch_device, dtype=torch.float32)
+        ends_tokenizer = torch.zeros(batch_size,1).to(device=torch_device, dtype=torch.long)
+        for i in range(batch_size):
+            ends_tokenizer[i,torch.randint(0,1,(1,)).item()]=1 if torch.randn(1,).item()<0.5 else 0
+        mask_padding_tokenizer = torch.ones(batch_size,1).bool().to(device=torch_device, dtype=torch.bool)
 
-        observations_world_model = floats_tensor((3,20,config.in_channels,config.resolution,config.resolution))
-        actions_world_model = ids_tensor((3,20),vocab_size =4).long()
-        rewards_world_model = ids_tensor((3,20),vocab_size =8)
+        observations_world_model = torch.randn(batch_size,20,config.in_channels,config.resolution,config.resolution).to(device=torch_device, dtype=torch.float32)
+        actions_world_model = torch.randint(0,4,(batch_size,20)).to(device=torch_device, dtype=torch.long)
+        rewards_world_model = torch.randint(0,8,(batch_size,20))
         zeros = torch.zeros_like(rewards_world_model)
         # Rewards are given depending on which color brick is broken in 'Breakout' Atari env
         zeros[((rewards_world_model==7)|( rewards_world_model==4)|(rewards_world_model==1))]=1
-        rewards_world_model = torch.mul(zeros,rewards_world_model).float()
-        ends_world_model = torch.zeros(3,20).long()
-        for i in range(3):
-            ends_world_model[i,ids_tensor((1,),vocab_size=20).item()]=1 if floats_tensor((1,)).item()<0.5 else 0
-        mask_padding_world_model = torch.ones(4,20).bool()
+        rewards_world_model = torch.mul(zeros,rewards_world_model).to(device=torch_device, dtype=torch.float32)
+        ends_world_model = torch.zeros(batch_size,20).to(device=torch_device, dtype=torch.long)
+        for i in range(batch_size):
+            ends_world_model[i,torch.randint(0,20,(1,)).item()]=1 if torch.randn(1,).item()<0.5 else 0
+        mask_padding_world_model = torch.ones(batch_size,20).bool().to(device=torch_device, dtype=torch.bool)
 
-        observations_actor_critic = floats_tensor((3,21,config.in_channels,config.resolution,config.resolution))
-        actions_actor_critic = ids_tensor((3,21),vocab_size =4).long()
-        rewards_actor_critic = ids_tensor((3,21),vocab_size =8)
+        observations_actor_critic = torch.randn(batch_size,21,config.in_channels,config.resolution,config.resolution).to(device=torch_device, dtype=torch.float32)
+        actions_actor_critic = torch.randint(0,4,(batch_size,21)).to(device=torch_device, dtype=torch.long)
+        rewards_actor_critic = torch.randint(0,8,(batch_size,21))
         zeros = torch.zeros_like(rewards_actor_critic)
         # Rewards are given depending on which color brick is broken in 'Breakout' Atari env
         zeros[((rewards_actor_critic==7)|( rewards_actor_critic==4)|(rewards_actor_critic==1))]=1
-        rewards_actor_critic = torch.mul(zeros,rewards_actor_critic).float()
-        ends_actor_critic = torch.zeros(3,21).long()
-        for i in range(3):
-            ends_actor_critic[i,ids_tensor((1,),vocab_size=21).item()]=1 if floats_tensor((1,)).item()<0.5 else 0
-        mask_padding_actor_critic = torch.ones(3,21).bool()
+        rewards_actor_critic = torch.mul(zeros,rewards_actor_critic).to(device=torch_device, dtype=torch.float32)
+        ends_actor_critic = torch.zeros(batch_size,21).to(device=torch_device, dtype=torch.long)
+        for i in range(batch_size):
+            ends_actor_critic[i,torch.randint(0,21,(1,)).item()]=1 if torch.randn(1,).item()<0.5 else 0
+        mask_padding_actor_critic = torch.ones(batch_size,21).bool().to(device=torch_device, dtype=torch.bool)
         
         observations = [observations_tokenizer,observations_world_model,observations_actor_critic]
         actions = [actions_tokenizer,actions_world_model,actions_actor_critic]
@@ -418,7 +525,7 @@ class IrisModelIntegrationTest(unittest.TestCase):
                     return_dict = True,
                 )
             
-            act_pred_expected_shape = torch.Size((3,1, config.num_actions))
-            self.assertEqual(model_pred.action_preds.shape, act_pred_expected_shape)
-            self.assertTrue(torch.allclose(model_pred.action_preds, expected_outputs[step], atol=1e-4))
+            rewards_expected_shape = torch.Size((batch_size,config.max_blocks,3))
+            self.assertEqual(model_pred.reward_preds.shape, rewards_expected_shape)
+            self.assertTrue(torch.allclose(model_pred.reward_preds, expected_outputs[step], atol=1e-4))
             
