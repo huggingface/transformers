b66e01e55f5e31d4c0479cac4bcacc0f123dc9d2
Younes Belkada <younesbelkada@gmail.com>
2024-05-17 10:57:48 +0200

    test commit

--- src/transformers/models/llama/modeling_llama.py
+++ src/transformers/models/llama/modeling_llama.py
@@ -17,6 +17,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+# Test
 """PyTorch LLaMA model."""
 
 import math
