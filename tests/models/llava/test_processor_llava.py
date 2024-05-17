c5eee5b79fc3562346822c24c3aa956d30acf4f4
Arthur Zucker <arthur.zucker@gmail.com>
2024-05-17 16:35:39 +0200

    bug so let's just use class for now

--- tests/models/llava/test_processor_llava.py
+++ tests/models/llava/test_processor_llava.py
@@ -27,4 +27,4 @@ class LlavaProcessorTest(unittest.TestCase):
         for checkpoint in ["Intel/llava-gemma-2b", "llava-hf/llava-1.5-7b-hf"]:
             processor = LlavaProcessor.from_pretrained(checkpoint)
             tokenizer = AutoTokenizer.from_pretrained(checkpoint)
-            self.assertDictEqual(processor.tokenizer.__dict__, tokenizer.__dict__)
+            self.assertDictEqual(processor.tokenizer.__class___, tokenizer.__class__)
