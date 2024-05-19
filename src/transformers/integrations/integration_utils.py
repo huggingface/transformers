d425921476cab62897397769744a00c833d9833e
mgerstgrasser <matthias@gerstgrasser.net>
2024-05-18 20:44:22 -0700

    fix wandb always uploading initial model

--- src/transformers/integrations/integration_utils.py
+++ src/transformers/integrations/integration_utils.py
@@ -813,7 +813,6 @@ class WandbCallback(TrainerCallback):
                         "initial_model": True,
                     },
                 )
-                model.save_pretrained(temp_dir)
                 # add the architecture to a separate text file
                 save_model_architecture_to_file(model, temp_dir)
 
