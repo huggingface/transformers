55ebde0abe6cbe3cf4d81aa5b9a52e62e9ba2517
Pavel Iakubovskii <qubvel@gmail.com>
2024-05-21 17:43:54 +0000

    Memory leak debugging

--- examples/pytorch/instance-segmentation/run_instance_segmentation.py
+++ examples/pytorch/instance-segmentation/run_instance_segmentation.py
@@ -1,8 +1,15 @@
 import numpy as np
 import pandas as pd
 from datasets import load_dataset
-from transformers import Mask2FormerImageProcessor, Mask2FormerForUniversalSegmentation, Mask2FormerConfig, MaskFormerImageProcessor
-from transformers import AutoModelForInstanceSegmentation, Mask2FormerForUniversalSegmentation
+from transformers import (
+    Mask2FormerImageProcessor,
+    Mask2FormerForUniversalSegmentation,
+    Mask2FormerConfig,
+    MaskFormerImageProcessor,
+    MaskFormerForInstanceSegmentation,
+)
+
+from transformers import AutoModelForInstanceSegmentation, AutoImageProcessor
 
 
 import logging
@@ -29,7 +36,7 @@ from transformers import (
 )
 from transformers.image_processing_utils import BatchFeature
 from transformers.image_transforms import center_to_corners_format
-from transformers.trainer import EvalPrediction
+from transformers.trainer import EvalPrediction, nested_numpify
 from transformers.trainer_utils import get_last_checkpoint
 from transformers.utils import check_min_version, send_example_telemetry
 from transformers.utils.versions import require_version
@@ -96,37 +103,50 @@ class ModelOutput:
     masks_queries_logits: torch.Tensor
 
 
-@torch.no_grad()
-def compute_metrics(
-    evaluation_results: EvalPrediction,
-    image_processor: Mask2FormerImageProcessor,
-    threshold: float = 0.5,
-    id2label: Optional[Mapping[int, str]] = None,
-) -> Mapping[str, float]:
-    """
-    Compute mean average mAP, mAR and their variants for the object detection task.
+def nested_cpu(tensors):
+    if isinstance(tensors, (list, tuple)):
+        return type(tensors)(nested_cpu(t) for t in tensors)
+    if isinstance(tensors, Mapping):
+        return type(tensors)({k: nested_cpu(t) for k, t in tensors.items()})
+    t = tensors.cpu().detach()
+    return t
 
-    Args:
-        evaluation_results (EvalPrediction): Predictions and targets from evaluation.
-        threshold (float, optional): Threshold to filter predicted boxes by confidence. Defaults to 0.0.
-        id2label (Optional[dict], optional): Mapping from class id to class name. Defaults to None.
 
-    Returns:
-        Mapping[str, float]: Metrics in a form of dictionary {<metric_name>: <metric_value>}
-    """
+class Evaluator:
 
-    predictions, targets = evaluation_results.predictions, evaluation_results.label_ids
+    def __init__(self, image_processor: Mask2FormerImageProcessor, id2label: Mapping[int, str], threshold: float = 0.5):
+        self.image_processor = image_processor
+        self.id2label = id2label
+        self.threshold = threshold
+        self.metric = self.get_metric()
 
-    # For metric computation we need to provide:
-    #  - targets in a form of list of dictionaries with keys "masks", "labels"
-    #  - predictions in a form of list of dictionaries with keys "masks", "labels", "scores"
+    def get_metric(self):
+        return MeanAveragePrecision(iou_type="segm", class_metrics=True)
 
-    post_processed_targets = []
-    post_processed_predictions = []
+    @torch.no_grad()
+    def __call__(self, evaluation_results: EvalPrediction, compute_result: bool = False) -> Mapping[str, float]:
+        """
+        Compute mean average mAP, mAR and their variants for the object detection task.
 
-    # Collect targets in the required format for metric computation
-    for target_batch, prediction_batch in zip(targets, predictions):
-        
+        Args:
+            evaluation_results (EvalPrediction): Predictions and targets from evaluation.
+            threshold (float, optional): Threshold to filter predicted boxes by confidence. Defaults to 0.0.
+            id2label (Optional[dict], optional): Mapping from class id to class name. Defaults to None.
+
+        Returns:
+            Mapping[str, float]: Metrics in a form of dictionary {<metric_name>: <metric_value>}
+        """
+
+        prediction_batch = nested_cpu(evaluation_results.predictions)
+        target_batch = nested_cpu(evaluation_results.label_ids)
+        del evaluation_results
+
+        # For metric computation we need to provide:
+        #  - targets in a form of list of dictionaries with keys "masks", "labels"
+        #  - predictions in a form of list of dictionaries with keys "masks", "labels", "scores"
+
+        post_processed_targets = []
+        post_processed_predictions = []
         target_sizes = []
 
         # Collect targets
@@ -144,9 +164,9 @@ def compute_metrics(
         class_queries_logits = torch.tensor(prediction_batch[0])
         masks_queries_logits = torch.tensor(prediction_batch[1])
         model_output = ModelOutput(class_queries_logits=class_queries_logits, masks_queries_logits=masks_queries_logits)
-        post_processed_output = image_processor.post_process_instance_segmentation(
+        post_processed_output = self.image_processor.post_process_instance_segmentation(
             model_output,
-            threshold=threshold,
+            threshold=self.threshold,
             target_sizes=target_sizes,
             return_binary_maps=True,
         )
@@ -167,97 +187,104 @@ def compute_metrics(
                 }
             post_processed_predictions.append(post_processed_image_prediction)
 
-    # Compute metrics
-    metric = MeanAveragePrecision(iou_type="segm", class_metrics=True)
-    metric.update(post_processed_predictions, post_processed_targets)
-    metrics = metric.compute()
+        # Compute metrics
+        self.metric.update(post_processed_predictions, post_processed_targets)
 
-    # Replace list of per class metrics with separate metric for each class
-    classes = metrics.pop("classes")
-    map_per_class = metrics.pop("map_per_class")
-    mar_100_per_class = metrics.pop("mar_100_per_class")
-    for class_id, class_map, class_mar in zip(classes, map_per_class, mar_100_per_class):
-        class_name = id2label[class_id.item()] if id2label is not None else class_id.item()
-        metrics[f"map_{class_name}"] = class_map
-        metrics[f"mar_100_{class_name}"] = class_mar
-    
-    metrics = {k: round(v.item(), 4) for k, v in metrics.items()}
+        if not compute_result:
+            return
 
-    return metrics
+        metrics = self.metric.compute()
+        self.metric = self.get_metric()
 
+        # Replace list of per class metrics with separate metric for each class
+        classes = metrics.pop("classes")
+        map_per_class = metrics.pop("map_per_class")
+        mar_100_per_class = metrics.pop("mar_100_per_class")
+        for class_id, class_map, class_mar in zip(classes, map_per_class, mar_100_per_class):
+            class_name = self.id2label[class_id.item()] if id2label is not None else class_id.item()
+            metrics[f"map_{class_name}"] = class_map
+            metrics[f"mar_100_{class_name}"] = class_mar
+        
+        metrics = {k: round(v.item(), 4) for k, v in metrics.items()}
 
-# Dataset
-dataset = load_dataset("qubvel-hf/ade20k-mini")
-label2id = dataset["train"][0]["semantic_class_to_id"]
-
-
-checkpoint = "facebook/mask2former-swin-tiny-coco-instance"
+        return metrics
 
 
-# Image transformations
-image_processor = Mask2FormerImageProcessor.from_pretrained(
-    checkpoint,
-    do_resize=True,
-    size={"height": 512, "width": 512},
-    reduce_labels=True,
-)
+if __name__ == "__main__":
 
-augmentation_transform = A.Compose([])
-
-train_augment_and_transform_batch = partial(augment_and_transform_batch, transform=augmentation_transform, image_processor=image_processor)
-valid_transform_batch = partial(augment_and_transform_batch, transform=augmentation_transform, image_processor=image_processor)
-dataset["train"] = dataset["train"].with_transform(train_augment_and_transform_batch)
-dataset["validation"] = dataset["validation"].with_transform(valid_transform_batch)
-
-# dataset["validation"] = dataset["train"].select(range(16))
-# dataset["train"] = dataset["train"].select(list(range(16)) * 10)
+    # Dataset
+    dataset = load_dataset("qubvel-hf/ade20k-mini")
+    label2id = dataset["train"][0]["semantic_class_to_id"]
 
 
-# Model
-if image_processor.reduce_labels:
-    label2id.pop("background")
-    label2id = {k: i - 1 for k, i in label2id.items()}
+    # checkpoint = "facebook/mask2former-swin-tiny-coco-instance"
+    checkpoint = "facebook/maskformer-swin-tiny-ade"
 
-id2label = {v: k for k, v in label2id.items()}
+    # Image transformations
+    image_processor = MaskFormerImageProcessor.from_pretrained(
+        checkpoint,
+        do_resize=True,
+        size={"height": 256, "width": 256},
+        do_reduce_labels=True,
+    )
 
-model = Mask2FormerForUniversalSegmentation.from_pretrained(
-    "facebook/mask2former-swin-tiny-ade-semantic",
-    label2id=label2id,
-    id2label=id2label,
-    ignore_mismatched_sizes=True,
-)
+    augmentation_transform = A.Compose([])
 
-args = TrainingArguments(
-    output_dir="finetune-instance-segmentation-ade20k-mini",
-    num_train_epochs=40,
-    do_train=True,
-    do_eval=True,
-    fp16=True,
-    dataloader_num_workers=4,
-    dataloader_persistent_workers=True,
-    per_device_train_batch_size=8,
-    gradient_accumulation_steps=1,
-    remove_unused_columns=False,
-    eval_do_concat_batches=False,
-    eval_strategy="epoch",
-    save_strategy="epoch",
-    learning_rate=1e-5,
-    lr_scheduler_type="constant",
-    load_best_model_at_end=True,
-    save_total_limit=2,
-    # max_steps=1,
-)
+    train_augment_and_transform_batch = partial(augment_and_transform_batch, transform=augmentation_transform, image_processor=image_processor)
+    valid_transform_batch = partial(augment_and_transform_batch, transform=augmentation_transform, image_processor=image_processor)
+    dataset["train"] = dataset["train"].with_transform(train_augment_and_transform_batch)
+    dataset["validation"] = dataset["validation"].with_transform(valid_transform_batch)
 
-eval_compute_metrics = partial(compute_metrics, image_processor=image_processor, id2label=id2label)
+    # dataset["validation"] = dataset["train"].select(range(16))
+    dataset["train"] = dataset["train"].select(list(range(8)))
 
-trainer = Trainer(
-    model=model,
-    args=args,
-    train_dataset=dataset["train"],
-    eval_dataset=dataset["validation"], # overfit for debugging
-    data_collator=collate_fn,
-    compute_metrics=eval_compute_metrics,
-)
 
-trainer.train()
-trainer.evaluate()
+    # Model
+    if image_processor.do_reduce_labels:
+        label2id.pop("background")
+        label2id = {k: i - 1 for k, i in label2id.items()}
+
+    id2label = {v: k for k, v in label2id.items()}
+
+    model = MaskFormerForInstanceSegmentation.from_pretrained(
+        checkpoint,
+        label2id=label2id,
+        id2label=id2label,
+        ignore_mismatched_sizes=True,
+    )
+
+    args = TrainingArguments(
+        output_dir="finetune-instance-segmentation-ade20k-mini-maskformer",
+        num_train_epochs=40,
+        do_train=True,
+        do_eval=True,
+        fp16=True,
+        dataloader_num_workers=4,
+        dataloader_persistent_workers=True,
+        per_device_train_batch_size=8,
+        gradient_accumulation_steps=1,
+        remove_unused_columns=False,
+        eval_do_concat_batches=False,
+        eval_strategy="epoch",
+        save_strategy="epoch",
+        learning_rate=1e-5,
+        lr_scheduler_type="constant",
+        load_best_model_at_end=True,
+        save_total_limit=2,
+        batch_eval_metrics=True,
+        # max_steps=1,
+    )
+
+    eval_compute_metrics = Evaluator(image_processor=image_processor, id2label=id2label, threshold=0.0)
+
+    trainer = Trainer(
+        model=model,
+        args=args,
+        train_dataset=dataset["train"],
+        eval_dataset=dataset["validation"], # overfit for debugging
+        data_collator=collate_fn,
+        compute_metrics=dummy_compute_metrics, #eval_compute_metrics,
+    )
+
+    trainer.train()
+    trainer.evaluate()
