44e37f4cedea278e5fb34196a21b3d352f5f1cc9
Cyril Vallez <cyril.vallez@gmail.com>
2024-05-21 15:19:54 +0200

    Apply coding style with new circleCI ruff version (0.4.4)

--- src/transformers/models/dbrx/modeling_dbrx.py
+++ src/transformers/models/dbrx/modeling_dbrx.py
@@ -12,7 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-""" PyTorch DBRX model. """
+"""PyTorch DBRX model."""
 
 import math
 from typing import Any, Optional, Tuple, Union
