1e2b873a4f37ea279bb2d952e77a14943f515d4a
raushan <raushan@huggingface.co>
2024-05-22 14:18:03 +0200

    should not have changed

--- src/transformers/models/llava/processing_llava.py
+++ src/transformers/models/llava/processing_llava.py
@@ -41,8 +41,8 @@ class LlavaProcessor(ProcessorMixin):
     """
 
     attributes = ["image_processor", "tokenizer"]
-    image_processor_class = "CLIPImageProcessor"
-    tokenizer_class = ("LlamaTokenizer", "LlamaTokenizerFast")
+    image_processor_class = "AutoImageProcessor"
+    tokenizer_class = "AutoTokenizer"
 
     def __init__(self, image_processor=None, tokenizer=None):
         super().__init__(image_processor, tokenizer)
