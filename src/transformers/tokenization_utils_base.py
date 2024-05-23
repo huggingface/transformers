b2563c218252aba2691b872047e148cd06532b78
Matt <Rocketknight1@users.noreply.github.com>
2024-05-23 19:46:34 +0100

    Update src/transformers/tokenization_utils_base.py
    Co-authored-by: amyeroberts <22614925+amyeroberts@users.noreply.github.com>

--- src/transformers/tokenization_utils_base.py
+++ src/transformers/tokenization_utils_base.py
@@ -1707,19 +1707,21 @@ class PreTrainedTokenizerBase(SpecialTokensMixin, PushToHubMixin):
         Args:
             conversation (Union[List[Dict[str, str]], List[List[Dict[str, str]]]]): A list of dicts
                 with "role" and "content" keys, representing the chat history so far.
-            tools (List[Dict], *optional*): A list of tools (callable functions) that will be accessible
-                to the model. If the template does not support function calling, this argument will have no effect.
-                Each tool should be passed as a JSON Schema, giving the name, description and argument types
-                for the tool. See our [chat templating guide](https://huggingface.co/docs/transformers/main/en/chat_templating#automated-function-conversion-for-tool-use)
+            tools (`List[Dict]`, *optional*): 
+                A list of tools (callable functions) that will be accessible to the model. If the template does not
+                support function calling, this argument will have no effect. Each tool should be passed as a JSON Schema,
+                giving the name, description and argument types for the tool. See our 
+                [chat templating guide](https://huggingface.co/docs/transformers/main/en/chat_templating#automated-function-conversion-for-tool-use)
                 for more information.
-            documents (List[Dict[str, str]], *optional*): A list of dicts representing documents that will be accessible
-                to the model if it is performing RAG (retrieval-augmented generation). If the template does not support
-                RAG, this argument will have no effect. We recommend that each document should be a dict containing
-                "title" and "text" keys. Please see the RAG section of the
-                [chat templating guide](https://huggingface.co/docs/transformers/main/en/chat_templating#arguments-for-RAG)
+            documents (`List[Dict[str, str]]`, *optional*):
+                A list of dicts representing documents that will be accessible to the model if it is performing RAG
+                (retrieval-augmented generation). If the template does not support RAG, this argument will have no
+                effect. We recommend that each document should be a dict containing "title" and "text" keys. Please
+                see the RAG section of the [chat templating guide](https://huggingface.co/docs/transformers/main/en/chat_templating#arguments-for-RAG)
                 for examples of passing documents with chat templates.
-            chat_template (str, *optional*): A Jinja template to use for this conversion. It is usually not necessary
-                to pass anything to this argument, as the model's template will be used by default.
+            chat_template (`str`, *optional*): 
+                A Jinja template to use for this conversion. It is usually not necessary to pass anything to this
+                argument, as the model's template will be used by default.
             add_generation_prompt (bool, *optional*): Whether to end the prompt with the token(s) that indicate
                 the start of an assistant message. This is useful when you want to generate a response from the model.
                 Note that this argument will be passed to the chat template, and so it must be supported in the
