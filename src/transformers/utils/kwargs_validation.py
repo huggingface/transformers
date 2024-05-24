42fed61acaa873027eb805636080c7a936a6fa95
Pavel Iakubovskii <qubvel@gmail.com>
2024-05-24 15:49:20 +0000

    fixup

--- src/transformers/utils/kwargs_validation.py
+++ src/transformers/utils/kwargs_validation.py
@@ -1,7 +1,6 @@
-import inspect
 import functools
+import inspect
 import warnings
-
 from typing import Optional
 
 
@@ -12,14 +11,12 @@ def filter_out_non_signature_kwargs(extra: Optional[list] = None):
     extra_params_to_pass = set(extra)
 
     def decorator(func):
-
         sig = inspect.signature(func)
         function_named_args = set(sig.parameters.keys())
         valid_kwargs_to_pass = function_named_args.union(extra_params_to_pass)
 
         @functools.wraps(func)
         def wrapper(*args, **kwargs):
-            
             valid_kwargs = {}
             invalid_kwargs = {}
 
@@ -28,15 +25,16 @@ def filter_out_non_signature_kwargs(extra: Optional[list] = None):
                     valid_kwargs[k] = v
                 else:
                     invalid_kwargs[k] = v
-            
+
             if invalid_kwargs:
-                invalid_kwargs_names = ', '.join(invalid_kwargs.keys())
+                invalid_kwargs_names = ", ".join(invalid_kwargs.keys())
                 warnings.warn(
                     f"The following named arguments are not valid for function `{func.__name__}`"
                     f" and were ignored: {invalid_kwargs_names}"
                 )
-            
+
             return func(*args, **valid_kwargs)
-        
+
         return wrapper
+
     return decorator
