f851298789010c7aeb4f395ec5c04bc545d7a200
raushan <raushan@huggingface.co>
2024-05-27 15:22:32 +0200

    incorrect compare sign

--- src/transformers/cache_utils.py
+++ src/transformers/cache_utils.py
@@ -14,7 +14,7 @@ from .utils import is_hqq_available, is_quanto_available, logging
 
 if is_quanto_available():
     quanto_version = version.parse(importlib.metadata.version("quanto"))
-    if quanto_version < version.parse("0.2.0"):
+    if quanto_version >= version.parse("0.2.0"):
         from quanto import AffineQuantizer, MaxOptimizer, qint2, qint4
 
 if is_hqq_available():
