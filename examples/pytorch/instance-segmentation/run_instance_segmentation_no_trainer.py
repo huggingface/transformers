fa7dd211842996d76c6d08ea2510144d385994de
Pavel Iakubovskii <qubvel@gmail.com>
2024-05-28 12:43:11 +0100

    Update examples/pytorch/instance-segmentation/run_instance_segmentation_no_trainer.py

--- examples/pytorch/instance-segmentation/run_instance_segmentation_no_trainer.py
+++ examples/pytorch/instance-segmentation/run_instance_segmentation_no_trainer.py
@@ -415,7 +415,7 @@ def main():
 
     # Sending telemetry. Tracking the example usage helps us better allocate resources to maintain them. The
     # information sent is the one passed as arguments along with your Python/PyTorch versions.
-    # send_example_telemetry("run_instance_segmentation_no_trainer", args)
+    send_example_telemetry("run_instance_segmentation_no_trainer", args)
 
     # Initialize the accelerator. We will let the accelerator handle device placement for us in this example.
     # If we're using tracking, we also need to initialize it here and it will by default pick up all supported trackers
