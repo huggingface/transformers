e1eb7b334c32de257affd6a8f5a522c69633e2eb
Pavel Iakubovskii <qubvel@gmail.com>
2024-05-28 12:43:16 +0100

    Update examples/pytorch/instance-segmentation/run_instance_segmentation.py

--- examples/pytorch/instance-segmentation/run_instance_segmentation.py
+++ examples/pytorch/instance-segmentation/run_instance_segmentation.py
@@ -414,7 +414,7 @@ def main():
         do_resize=True,
         size={"height": args.image_height, "width": args.image_width},
         do_reduce_labels=args.do_reduce_labels,
-        reduce_labels=args.do_reduce_labels,  # TODO: remove in the future
+        reduce_labels=args.do_reduce_labels,  # TODO: remove when mask2former support `do_reduce_labels`
         token=args.token,
     )
 
