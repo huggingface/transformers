539fe0e070e515d0eb40593eba846397fa1818b6
Omar Salman <omar.salman@arbisoft.com>
2024-05-29 16:36:25 +0500

    Revert change

--- tests/test_modeling_common.py
+++ tests/test_modeling_common.py
@@ -3156,7 +3156,7 @@ class ModelTesterMixin:
 
                 for max_size in max_gpu_sizes:
                     max_memory = {0: max_size, 1: model_size * 2, "cpu": model_size * 2}
-                    new_model = model_class.from_pretrained(tmp_dir, device_map="sequential", max_memory=max_memory)
+                    new_model = model_class.from_pretrained(tmp_dir, device_map="auto", max_memory=max_memory)
                     # Making sure part of the model will actually end up offloaded
                     self.assertSetEqual(set(new_model.hf_device_map.values()), {0, 1})
                     self.check_device_map_is_respected(new_model, new_model.hf_device_map)
