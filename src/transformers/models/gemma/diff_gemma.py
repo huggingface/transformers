d7355db6ae0c73fb269185f74bd799bf6615508e
Arthur Zucker <arthur.zucker@gmail.com>
2024-05-30 16:48:48 +0200

    nit

--- src/transformers/models/gemma/diff_gemma.py
+++ src/transformers/models/gemma/diff_gemma.py
@@ -20,6 +20,7 @@ import torch
 import torch.utils.checkpoint
 from torch import nn
 from torch.nn import CrossEntropyLoss
+
 from transformers import PretrainedConfig
 from transformers.models.llama.modeling_llama import (
     LlamaForCausalLM,
