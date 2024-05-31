40afb0d7fef6ccda3212c143c8e3694c9fbd4ba1
Wing Lian <wing.lian@gmail.com>
2024-05-31 14:53:16 -0400

    fixes and update docs

--- src/transformers/training_args.py
+++ src/transformers/training_args.py
@@ -179,7 +179,7 @@ class OptimizerNames(ExplicitEnum):
     ADALOMO = "adalomo"
     GROKADAMW = "grokadamw"
     SCHEDULE_FREE_ADAMW_32BIT = "schedule_free_adamw_32bit"
-    SCHEDULE_FREE_SGD_32BIT = "schedule_free_adamw_32bit"
+    SCHEDULE_FREE_SGD_32BIT = "schedule_free_sgd_32bit"
 
 
 # Sometimes users will pass in a `str` repr of a dict in the CLI
