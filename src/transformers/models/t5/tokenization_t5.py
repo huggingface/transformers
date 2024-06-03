b6de569868af8d935c563fa996eea24ff4732a19
Ita Zaporozhets <ita.zaporozhets@huggingface.co>
2024-06-03 15:44:18 +0200

    t5 mod

--- src/transformers/models/t5/tokenization_t5.py
+++ src/transformers/models/t5/tokenization_t5.py
@@ -210,8 +210,8 @@ class T5Tokenizer(PreTrainedTokenizer):
             model_pb2 = import_protobuf(f"The new behaviour of {self.__class__.__name__} (with `self.legacy = False`)")
             model = model_pb2.ModelProto.FromString(sp_model)
             normalizer_spec = model_pb2.NormalizerSpec()
-            normalizer_spec.add_dummy_prefix = False
             self.add_prefix_space = normalizer_spec.add_dummy_prefix if self.add_prefix_space is None else self.add_prefix_space
+            normalizer_spec.add_dummy_prefix = False
             model.normalizer_spec.MergeFrom(normalizer_spec)
             sp_model = model.SerializeToString()
             tokenizer.LoadFromSerializedProto(sp_model)
