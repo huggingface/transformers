7cb9925a7cac499308515249a05ccf1beb477708
Pablo Montalvo <pablo.montalvo.leroux@gmail.com>
2024-06-03 15:02:38 +0200

    update processor to generic kwargs + test

--- src/transformers/models/altclip/processing_altclip.py
+++ src/transformers/models/altclip/processing_altclip.py
@@ -20,8 +20,73 @@ import warnings
 from typing import List, Union
 
 from ...image_utils import ChannelDimension, ImageInput
-from ...processing_utils import ProcessingKwargs, ProcessorMixin
+from ...processing_utils import (
+    CommonKwargs,
+    ImagesKwargs,
+    ProcessingKwargs,
+    ProcessorMixin,
+    TextKwargs,
+)
 from ...tokenization_utils_base import BatchEncoding, PreTokenizedInput, TextInput
+from ...utils import is_torch_available, is_vision_available
+
+
+# TODO (@molbap) This is a bother, forward references from TypedDict are resolved and need this to work
+if is_vision_available():
+    import PIL  # noqa: F401
+if is_torch_available():
+    import torch  # noqa: F401
+
+
+class AltClipProcessorKwargs(ProcessingKwargs, total=False):
+    """
+    Inherits from `ProcessingKwargs` to provide:
+        1) Additional keys that this model requires to process inputs.
+        2) Default values for extra keys.
+    New keys have to be defined as follows to ensure type hinting is done correctly.
+
+    ```python
+    common_kwargs: CommonKwargs = {
+            **CommonKwargs.__annotations__,
+        }
+        text_kwargs: TextKwargs = {
+            **TextKwargs.__annotations__,
+            "a_new_text_boolean_key": Optional[bool],
+        }
+        images_kwargs: ImagesKwargs = {
+            **ImagesKwargs.__annotations__,
+            "a_new_image_processing_key": Optional[int]
+        }
+    ```
+
+    """
+
+    common_kwargs: CommonKwargs = {
+        **CommonKwargs.__annotations__,
+    }
+    text_kwargs: TextKwargs = {
+        **TextKwargs.__annotations__,
+    }
+    images_kwargs: ImagesKwargs = {
+        **ImagesKwargs.__annotations__,
+    }
+
+    _defaults = {
+        "text_kwargs": {
+            "add_special_tokens": True,
+            "padding": False,
+            "stride": 0,
+            "is_split_into_words": False,
+            "return_overflowing_tokens": False,
+            "return_special_tokens_mask": False,
+            "return_offsets_mapping": False,
+            "return_length": False,
+            "verbose": True,
+        },
+        "images_kwargs": {
+            "data_format": ChannelDimension.FIRST,
+        },
+    }
 
 
 class AltCLIPProcessor(ProcessorMixin):
@@ -59,40 +124,6 @@ class AltCLIPProcessor(ProcessorMixin):
             raise ValueError("You need to specify a `tokenizer`.")
 
         super().__init__(image_processor, tokenizer)
-        self.processing_kwargs: ProcessingKwargs = {
-            "common_kwargs": {"return_tensors": None},
-            "text_kwargs": {
-                "add_special_tokens": True,
-                "padding": False,
-                "truncation": None,
-                "max_length": None,
-                "stride": 0,
-                "is_split_into_words": False,
-                "pad_to_multiple_of": None,
-                "return_token_type_ids": None,
-                "return_attention_mask": None,
-                "return_overflowing_tokens": False,
-                "return_special_tokens_mask": False,
-                "return_offsets_mapping": False,
-                "return_length": False,
-                "verbose": True,
-            },
-            "images_kwargs": {
-                "do_resize": None,
-                "size": None,
-                "resample": None,
-                "do_thumbnail": None,
-                "do_align_long_axis": None,
-                "do_pad": None,
-                "do_rescale": None,
-                "rescale_factor": None,
-                "do_normalize": None,
-                "image_mean": None,
-                "image_std": None,
-                "data_format": ChannelDimension.FIRST,
-                "input_data_format": None,
-            },
-        }
 
     def __call__(
         self,
@@ -100,7 +131,10 @@ class AltCLIPProcessor(ProcessorMixin):
         text: Union[TextInput, PreTokenizedInput, List[TextInput], List[PreTokenizedInput]] = None,
         audio=None,
         videos=None,
-        **kwargs,
+        text_kwargs: AltClipProcessorKwargs.text_kwargs = None,
+        images_kwargs: AltClipProcessorKwargs.images_kwargs = None,
+        common_kwargs: AltClipProcessorKwargs.common_kwargs = None,
+        **kwargs: AltClipProcessorKwargs,
     ) -> BatchEncoding:
         """
         Main method to prepare for the model one or several sequences(s) and image(s). This method forwards the `text`
@@ -134,20 +168,62 @@ class AltCLIPProcessor(ProcessorMixin):
             - **pixel_values** -- Pixel values to be fed to a model. Returned when `images` is not `None`.
         """
 
-        text_kwargs = {**self.processing_kwargs["text_kwargs"], **self.processing_kwargs["common_kwargs"], **kwargs}
-        images_kwargs = {
-            **self.processing_kwargs["images_kwargs"],
-            **self.processing_kwargs["common_kwargs"],
-            **kwargs,
-        }
+        if text is None and images is None:
+            raise ValueError("You must specify either text or images.")
 
-        if text:
+        # set kwargs as empty dicts to avoid default mutable
+        if text_kwargs is None:
+            text_kwargs = {}
+        if images_kwargs is None:
+            images_kwargs = {}
+        if common_kwargs is None:
+            common_kwargs = {}
+        # Init with default values if they exist
+        default_text_kwargs = AltClipProcessorKwargs._defaults.get("text_kwargs", {}).copy()
+        default_images_kwargs = AltClipProcessorKwargs._defaults.get("images_kwargs", {}).copy()
+
+        # then override with tokenizer-level arguments passed
+        default_text_kwargs.update(
+            {k: v for k, v in self.tokenizer.init_kwargs.items() if k in AltClipProcessorKwargs.text_kwargs}
+        )
+        # then get passed per-modality dictionaries if they exist
+        text_kwargs = {**default_text_kwargs, **text_kwargs, **kwargs.pop("text_kwargs", {})}
+        images_kwargs = {**default_images_kwargs, **images_kwargs, **kwargs.pop("images_kwargs", {})}
+        common_kwargs.update(kwargs.pop("common_kwargs", {}))
+
+        # then merge kwargs by name
+        for text_key in AltClipProcessorKwargs.text_kwargs.keys():
+            text_kwarg_value = kwargs.pop(text_key, None)
+            if text_kwarg_value is not None:
+                text_kwargs[text_key] = text_kwarg_value
+
+        for images_key in AltClipProcessorKwargs.images_kwargs.keys():
+            images_kwarg_value = kwargs.pop(images_key, None)
+            if images_kwarg_value is not None:
+                images_kwargs[images_key] = images_kwarg_value
+        # if something remains in kwargs, it belongs to common
+        common_kwargs.update(kwargs)
+
+        # all modality-specific kwargs are updated with common kwargs
+        text_kwargs.update(common_kwargs)
+        images_kwargs.update(common_kwargs)
+
+        if text is not None:
             encoding = self.tokenizer(text, **text_kwargs)
-        if images:
+        if images is not None:
             image_features = self.image_processor(images, **images_kwargs)
-            encoding.update(image_features)
 
-        return encoding
+        # BC for explicit return_tensors
+        if "return_tensors" in common_kwargs:
+            return_tensors = common_kwargs.pop("return_tensors", None)
+
+        if text is not None and images is not None:
+            encoding["pixel_values"] = image_features.pixel_values
+            return encoding
+        elif text is not None:
+            return encoding
+        else:
+            return BatchEncoding(data=dict(**image_features), tensor_type=return_tensors)
 
     def batch_decode(self, *args, **kwargs):
         """
---
+++ tests/models/altclip/test_processor_altclip.py
@@ -0,0 +1,83 @@
+
+import json
+import os
+import shutil
+import tempfile
+import unittest
+
+import numpy as np
+import pytest
+
+from transformers import XLMRobertaTokenizer, XLMRobertaTokenizerFast
+from transformers.models.bert.tokenization_bert import VOCAB_FILES_NAMES
+from transformers.testing_utils import require_torch, require_vision
+from transformers.utils import IMAGE_PROCESSOR_NAME, is_vision_available
+
+
+if is_vision_available():
+    from PIL import Image
+
+    from transformers import AltCLIPProcessor, CLIPImageProcessor
+
+
+@require_vision
+class AltClipProcessorTest(unittest.TestCase):
+    def setUp(self):
+        self.model_id = "BAAI/AltCLIP"
+        pass
+
+    def get_tokenizer(self, **kwargs):
+        return XLMRobertaTokenizer.from_pretrained(self.model_id, **kwargs)
+
+    def get_rust_tokenizer(self, **kwargs):
+        return XLMRobertaTokenizerFast.from_pretrained(self.model_id, **kwargs)
+
+    def get_image_processor(self, **kwargs):
+        return CLIPImageProcessor.from_pretrained(self.model_id, **kwargs)
+
+    def tearDown(self):
+        pass
+
+    def prepare_image_inputs(self):
+        """This function prepares a list of PIL images, or a list of numpy arrays if one specifies numpify=True,
+        or a list of PyTorch tensors if one specifies torchify=True.
+        """
+
+        image_inputs = [np.random.randint(255, size=(3, 30, 400), dtype=np.uint8)]
+        image_inputs = [Image.fromarray(np.moveaxis(x, 0, -1)) for x in image_inputs]
+        return image_inputs
+
+    def test_defaults_preserved(self):
+        image_processor = self.get_image_processor()
+        tokenizer = self.get_tokenizer(model_max_length=117)
+        processor = AltCLIPProcessor(tokenizer=tokenizer, image_processor=image_processor)
+
+        input_str = "lower newer"
+        image_input = self.prepare_image_inputs()
+
+        inputs = processor(text=input_str, images=image_input, padding="max_length")
+
+        self.assertEqual(len(inputs["input_ids"]), 117)
+
+    @require_torch
+    def test_structured_kwargs(self):
+        image_processor = self.get_image_processor()
+        tokenizer = self.get_tokenizer()
+
+        processor = AltCLIPProcessor(tokenizer=tokenizer, image_processor=image_processor)
+
+        input_str = "lower newer"
+        image_input = self.prepare_image_inputs()
+
+        # Define the kwargs for each modality
+        common_kwargs = {"return_tensors": "pt"}
+        images_kwargs = {"crop_size": {"height": 214, "width": 214}}
+        text_kwargs = {"padding": "max_length", "max_length": 76}
+
+        # Combine them into a single dictionary
+        all_kwargs = {"images_kwargs": images_kwargs, "text_kwargs": text_kwargs, "common_kwargs": common_kwargs}
+
+        inputs = processor(text=input_str, images=image_input, **all_kwargs)
+        self.assertEquals(inputs["pixel_values"].shape[2], 214)
+
+        self.assertEqual(len(inputs["input_ids"][0]), 76)
