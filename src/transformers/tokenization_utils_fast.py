a8694bca84a02fe07bdb40e2c1005662b74cf40b
Ita Zaporozhets <ita.zaporozhets@huggingface.co>
2024-06-04 11:00:22 +0200

    add eos and bos token arg support

--- src/transformers/tokenization_utils_fast.py
+++ src/transformers/tokenization_utils_fast.py
@@ -189,6 +189,12 @@ class PreTrainedTokenizerFast(PreTrainedTokenizerBase):
         tokens_to_add += [
             token for token in self.all_special_tokens_extended if token not in encoder and token not in tokens_to_add
         ]
+
+        if type(self) == PreTrainedTokenizerFast and all(item in kwargs for item in ["add_bos_token", "add_eos_token", "eos_token", "bos_token"]):
+            self.add_bos_token = kwargs.get("add_bos_token")
+            self.add_eos_token = kwargs.get("add_eos_token")
+            self.update_post_processor_pretrained()
+
         if len(tokens_to_add) > 0:
             # super hack: if a token.special is set, tokenizer ignores it for now so FIXME @ArthurZ
             # Accumulate added tokens into batches of special/non-special tokens, because calling add_tokens() for
@@ -897,8 +903,6 @@ class PreTrainedTokenizerFast(PreTrainedTokenizerBase):
                 return
             prepend_normalizer = [n for n in curr_normalizer['normalizers'] if n['type'] == 'Prepend']
             if prepend_normalizer:
-                prepend_normalizer = prepend_normalizer[0]
-                replacement = prepend_normalizer['prepend']
                 self.add_prefix_space = True
             else:
                 return
@@ -912,3 +916,31 @@ class PreTrainedTokenizerFast(PreTrainedTokenizerBase):
         self._tokenizer.pre_tokenizer = pre_tokenizers.Metaspace(replacement="▁", prepend_scheme=prepend_scheme,
                                                                  split=False)
         self.update_normalizer()
+
+
+
+    def update_post_processor(self):
+        """
+        Updates the underlying post processor with the current `bos_token` and `eos_token`.
+        """
+        bos = self.bos_token
+        bos_token_id = self.bos_token_id
+        if bos is None and self.add_bos_token:
+            raise ValueError("add_bos_token = True but bos_token = None")
+
+        eos = self.eos_token
+        eos_token_id = self.eos_token_id
+        if eos is None and self.add_eos_token:
+            raise ValueError("add_eos_token = True but eos_token = None")
+
+        single = f"{(bos+':0 ') if self.add_bos_token else ''}$A:0{(' '+eos+':0') if self.add_eos_token else ''}"
+        pair = f"{single}{(' '+bos+':1') if self.add_bos_token else ''} $B:1{(' '+eos+':1') if self.add_eos_token else ''}"
+
+        special_tokens = []
+        if self.add_bos_token:
+            special_tokens.append((bos, bos_token_id))
+        if self.add_eos_token:
+            special_tokens.append((eos, eos_token_id))
+        self._tokenizer.post_processor = processors.TemplateProcessing(
+            single=single, pair=pair, special_tokens=special_tokens
+        )
