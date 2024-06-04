00e74221c2439f08f7aaf6e77ffb454a2d4d6563
Ita Zaporozhets <ita.zaporozhets@huggingface.co>
2024-06-04 17:19:20 +0200

    depreciate cleanup_tokenization_spaces

--- src/transformers/commands/serving.py
+++ src/transformers/commands/serving.py
@@ -198,15 +198,13 @@ class ServeCommand(BaseTransformersCLICommand):
         self,
         tokens_ids: List[int] = Body(None, embed=True),
         skip_special_tokens: bool = Body(False, embed=True),
-        cleanup_tokenization_spaces: bool = Body(True, embed=True),
     ):
         """
         Detokenize the provided tokens ids to readable text: - **tokens_ids**: List of tokens ids -
-        **skip_special_tokens**: Flag indicating to not try to decode special tokens - **cleanup_tokenization_spaces**:
-        Flag indicating to remove all leading/trailing spaces and intermediate ones.
+        **skip_special_tokens**: Flag indicating to not try to decode special tokens
         """
         try:
-            decoded_str = self._pipeline.tokenizer.decode(tokens_ids, skip_special_tokens, cleanup_tokenization_spaces)
+            decoded_str = self._pipeline.tokenizer.decode(tokens_ids, skip_special_tokens)
             return ServeDeTokenizeResult(model="", text=decoded_str)
         except Exception as e:
             raise HTTPException(status_code=500, detail={"model": "", "error": str(e)})
