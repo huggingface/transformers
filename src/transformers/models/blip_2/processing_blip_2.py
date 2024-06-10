674f16e6620efe9733101dc02189e22ca02d5dc6
raushan <raushan@huggingface.co>
2024-06-10 09:57:04 +0200

    update blip-2

--- src/transformers/models/blip_2/processing_blip_2.py
+++ src/transformers/models/blip_2/processing_blip_2.py
@@ -20,8 +20,18 @@ from typing import List, Optional, Union
 
 from ...image_utils import ImageInput
 from ...processing_utils import ProcessorMixin
-from ...tokenization_utils_base import BatchEncoding, PaddingStrategy, PreTokenizedInput, TextInput, TruncationStrategy
-from ...utils import TensorType
+from ...tokenization_utils_base import (
+    AddedToken,
+    BatchEncoding,
+    PaddingStrategy,
+    PreTokenizedInput,
+    TextInput,
+    TruncationStrategy,
+)
+from ...utils import TensorType, logging
+
+
+logger = logging.get_logger(__name__)
 
 
 class Blip2Processor(ProcessorMixin):
@@ -43,10 +53,15 @@ class Blip2Processor(ProcessorMixin):
     tokenizer_class = "AutoTokenizer"
 
     # Copied from transformers.models.blip.processing_blip.BlipProcessor.__init__
-    def __init__(self, image_processor, tokenizer):
+    def __init__(self, image_processor, tokenizer, num_query_tokens=None):
         tokenizer.return_token_type_ids = False
+        self.current_processor = image_processor
+        self.image_token = AddedToken("<image>", normalized=False, special=True)
+        tokens_to_add = {"additional_special_tokens": [self.image_token]}
+        tokenizer.add_special_tokens(tokens_to_add)
+        self.num_query_tokens = num_query_tokens
+
         super().__init__(image_processor, tokenizer)
-        self.current_processor = self.image_processor
 
     # Copied from transformers.models.blip.processing_blip.BlipProcessor.__call__
     def __call__(
@@ -105,6 +120,15 @@ class Blip2Processor(ProcessorMixin):
         encoding_image_processor = self.image_processor(images, return_tensors=return_tensors)
 
         if text is not None:
+            # if we know how many query tokens, expand text inside processor
+            if self.num_query_tokens is not None:
+                text = self.image_token.content * self.num_query_tokens + text
+            else:
+                logger.warning_once(
+                    "Expanding inputs for image tokens in BLIP-2 should be done in processing. "
+                    "Please follow instruction here (https://gist.github.com/zucchini-nlp/e9f20b054fa322f84ac9311d9ab67042) to update your BLIP-2 model. "
+                    "Using processors without these attributes in the config is deprecated and will throw an error in v4.44."
+                )
             text_encoding = self.tokenizer(
                 text=text,
                 add_special_tokens=add_special_tokens,
