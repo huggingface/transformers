f6f1dacd76f02cd9d272323bbd64fa9048526924
Pablo Montalvo <pablo.montalvo.leroux@gmail.com>
2024-06-12 18:47:56 +0200

    protect imports

--- src/transformers/image_utils.py
+++ src/transformers/image_utils.py
@@ -83,12 +83,12 @@ ImageInput = Union[
 
 VideoInput = Union[
     List["PIL.Image.Image"],
-    np.ndarray,
+    "np.ndarray",
     "torch.Tensor",
-    List[np.ndarray],
+    List["np.ndarray"],
     List["torch.Tensor"],
     List[List["PIL.Image.Image"]],
-    List[List[np.ndarrray]],
+    List[List["np.ndarrray"]],
     List[List["torch.Tensor"]],
 ]  # noqa
 
