2e054a7af0d5f0b39ed24a6ab5f94a4944c5394f
Ita Zaporozhets <ita.zaporozhets@huggingface.co>
2024-06-13 11:57:41 +0200

    added llama and t5 tests for legacy prefix space in fast tokenizer

--- tests/models/t5/test_tokenization_t5.py
+++ tests/models/t5/test_tokenization_t5.py
@@ -464,7 +464,9 @@ class T5TokenizationTest(TokenizerTesterMixin, unittest.TestCase):
         fast_ = self.rust_tokenizer_class.from_pretrained(
             pretrained_name, add_prefix_space=False, legacy=False, from_slow=True
         )
+        legacy_fast_ = self.rust_tokenizer_class.from_pretrained(pretrained_name, add_prefix_space=False)
         self.assertEqual(slow_.encode(inputs), EXPECTED_WO_SPACE)
+        self.assertEqual(slow_.encode(inputs), legacy_fast_.encode(inputs))
         self.assertEqual(slow_.encode(inputs), fast_.encode(inputs))
         self.assertEqual(slow_.tokenize(inputs), ["He", "y", "▁how", "▁are", "▁you", "▁doing"])
         self.assertEqual(slow_.decode(EXPECTED_WO_SPACE, skip_special_tokens=True), inputs)
