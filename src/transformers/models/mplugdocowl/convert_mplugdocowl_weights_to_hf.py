fix: attention mask handling is fixed, .forward works

src/transformers/models/mplugdocowl/convert_mplugdocowl_weights_to_hf.py
@@ -21,9 +21,7 @@ from transformers import (
     AddedToken,
     AutoConfig,
     AutoTokenizer,
-    CLIPImageProcessor,
     MPLUGDocOwlConfig,
-    LlamaConfig,
     MPLUGDocOwlForConditionalGeneration,
     MPLUGDocOwlProcessor,
 )
@@ -99,7 +97,7 @@ def convert_mplugdocowl_llama_to_hf(text_model_id, vision_model_id, output_hub_p
     config = MPLUGDocOwlConfig(text_config=text_config)
     config.pad_token_id = 32001
 
-    with torch.device("cuda:1"):
+    with torch.device("cuda:3"):
         model = MPLUGDocOwlForConditionalGeneration(config)
 
     # Pad to 64 for performance reasons
@@ -139,7 +137,7 @@ def convert_mplugdocowl_llama_to_hf(text_model_id, vision_model_id, output_hub_p
     query = "<image>Recognize text in the image."
     output = processor(images=image, text=query)
     breakpoint()
-    device = torch.device("cuda:1")
+    device = torch.device("cuda:3")
     output.to(device)
     model.to(device)
     try:

src/transformers/models/mplugdocowl/image_processing_mplugdocowl.py
@@ -182,7 +182,11 @@ def shape_adaptive_cropping(image_patches: ImageInput,
             np.repeat(np.arange(anchor[0])[:, np.newaxis], anchor[1], axis=1)[:, :, np.newaxis],
             np.repeat(np.arange(anchor[1])[np.newaxis, :], anchor[0], axis=0)[:, :, np.newaxis]
         ], axis=2)
-        patch_position = patch_position.reshape(-1, 2)  # num_patch, (ph, pw)
+    
+        patch_position = patch_position.reshape(-1, 2)
+        if add_global_img:
+            patch_position = np.vstack((np.ones((1, 2), dtype=np.int64) * anchor_max, patch_position))
+          # num_patch, (ph, pw)
         return image_patches_list, patch_position, patch_position.shape[0], anchor_max
 
 class MPLUGDocOwlImageProcessor(BaseImageProcessor):

src/transformers/models/mplugdocowl/language_modeling_mplugdocowl.py
@@ -31,7 +31,7 @@ from torch.nn import BCEWithLogitsLoss, CrossEntropyLoss, MSELoss
 
 from ...activations import ACT2FN
 from ...cache_utils import Cache, DynamicCache, StaticCache
-from ...modeling_attn_mask_utils import AttentionMaskConverter
+from ...modeling_attn_mask_utils import AttentionMaskConverter, _prepare_4d_causal_attention_mask, _prepare_4d_attention_mask
 from ...modeling_outputs import (
     BaseModelOutputWithPast,
     CausalLMOutputWithPast,
@@ -353,7 +353,8 @@ class MultiwayAttention(nn.Module):
         kv_seq_len = key_states.shape[-2]
         if past_key_value is not None:
             kv_seq_len += past_key_value[0].shape[-2]
-        cos, sin = self.rotary_emb(value_states, seq_len=kv_seq_len)
+        #cos, sin = self.rotary_emb(value_states, seq_len=kv_seq_len)
+        cos, sin = self.rotary_emb(value_states, position_ids)
         query_states, key_states = apply_rotary_pos_emb(query_states, key_states, cos, sin, position_ids)
 
         if past_key_value is not None:
@@ -394,7 +395,8 @@ class MultiwayAttention(nn.Module):
         attn_output = attn_output.transpose(1, 2).contiguous()
 
         attn_output = attn_output.reshape(bsz, q_len, self.hidden_size)
-
+        #FIXME look here
+        attn_output = self.o_proj(attn_output)
         if not output_attentions:
             attn_weights = None
 
@@ -614,13 +616,52 @@ class MPLUGDocOwlModel(MPLUGDocOwlPreTrainedModel):
 
         # Initialize weights and apply final processing
         self.post_init()
-
+   
     def get_input_embeddings(self):
         return self.embed_tokens
 
     def set_input_embeddings(self, value):
         self.embed_tokens = value
+    '''
+    def _make_causal_mask(
+        input_ids_shape: torch.Size, dtype: torch.dtype, device: torch.device, past_key_values_length: int = 0
+    ):
+        """
+        Make causal mask used for bi-directional self-attention.
+        """
+        bsz, tgt_len = input_ids_shape
+        mask = torch.full((tgt_len, tgt_len), torch.finfo(dtype).min, device=device)
+        mask_cond = torch.arange(mask.size(-1), device=device)
+        mask.masked_fill_(mask_cond < (mask_cond + 1).view(mask.size(-1), 1), 0)
+        mask = mask.to(dtype)
 
+        if past_key_values_length > 0:
+            mask = torch.cat([torch.zeros(tgt_len, past_key_values_length, dtype=dtype, device=device), mask], dim=-1)
+        return mask[None, None, :, :].expand(bsz, 1, tgt_len, tgt_len + past_key_values_length)
+
+    def _prepare_decoder_attention_mask(self, attention_mask, input_shape, inputs_embeds, past_key_values_length):
+        # create causal mask
+        # [bsz, seq_len] -> [bsz, 1, tgt_seq_len, src_seq_len]
+        combined_attention_mask = None
+        if input_shape[-1] > 1:
+            combined_attention_mask = self._make_causal_mask(
+                input_shape,
+                inputs_embeds.dtype,
+                device=inputs_embeds.device,
+                past_key_values_length=past_key_values_length,
+            )
+
+        if attention_mask is not None:
+            # [bsz, seq_len] -> [bsz, 1, tgt_seq_len, src_seq_len]
+            expanded_attn_mask = self._expand_mask(attention_mask, inputs_embeds.dtype, tgt_len=input_shape[-1]).to(
+                inputs_embeds.device
+            )
+            combined_attention_mask = (
+                expanded_attn_mask if combined_attention_mask is None else expanded_attn_mask + combined_attention_mask
+            )
+
+        return combined_attention_mask
+    '''
     @add_start_docstrings_to_model_forward(MPLUGDocOwl_INPUTS_DOCSTRING)
     def forward(
         self,
@@ -675,9 +716,11 @@ class MPLUGDocOwlModel(MPLUGDocOwlPreTrainedModel):
             attention_mask = torch.ones(
                 (batch_size, seq_length_with_past), dtype=torch.bool, device=inputs_embeds.device
             )
-        attention_mask = self._prepare_decoder_attention_mask(
-            attention_mask, (batch_size, seq_length), inputs_embeds, past_key_values_length
-        )
+        #breakpoint()
+       # attention_mask = self._prepare_decoder_attention_mask(
+       #     attention_mask, (batch_size, seq_length), inputs_embeds, past_key_values_length
+       # )
+        attention_mask = _prepare_4d_attention_mask(attention_mask, dtype=torch.float32)
 
         hidden_states = inputs_embeds
 

src/transformers/models/mplugdocowl/modeling_mplugdocowl.py
@@ -476,7 +476,7 @@ class MPLUGDocOwlForConditionalGeneration(MPLUGDocOwlPreTrainedModel):
         # Compute the maximum embed dimension
         max_embed_dim = (num_special_image_tokens.max() * (num_image_patches - 1)) + sequence_length
         batch_indices, non_image_indices = torch.where(input_ids != self.config.image_token_index)
-
+        modality_indicators = torch.zeros((batch_size, max_embed_dim), dtype=torch.long, device=inputs_embeds.device)
         # 2. Compute the positions where text should be written
         # Calculate new positions for text tokens in merged image-text sequence.
         # `special_image_token_mask` identifies image tokens. Each image token will be replaced by `nb_text_tokens_per_images - 1` text tokens.
@@ -487,7 +487,7 @@ class MPLUGDocOwlForConditionalGeneration(MPLUGDocOwlPreTrainedModel):
         if left_padding:
             new_token_positions += nb_image_pad[:, None]  # offset for left padding
         text_to_overwrite = new_token_positions[batch_indices, non_image_indices]
-
+        
         # 3. Create the full embedding, already padded to the maximum position
         final_embedding = torch.zeros(
             batch_size, max_embed_dim, embed_dim, dtype=inputs_embeds.dtype, device=inputs_embeds.device
@@ -513,10 +513,11 @@ class MPLUGDocOwlForConditionalGeneration(MPLUGDocOwlPreTrainedModel):
         # we need to index copy on [0, 577, 578, 579] for the text and [1:576] for the image features
         final_embedding[batch_indices, text_to_overwrite] = inputs_embeds[batch_indices, non_image_indices]
         final_attention_mask[batch_indices, text_to_overwrite] = attention_mask[batch_indices, non_image_indices]
+        #modality_indicators[batch_indices, text_to_overwrite] = 0
         if labels is not None:
             final_labels[batch_indices, text_to_overwrite] = labels[batch_indices, non_image_indices]
 
-        # 5. Fill the embeddings corresponding to the images. Anything that is not `text_positions` needs filling (#29835)
+        # 5. Fill the embeddings corresponding to the images. Anything that is n.....≥≥.≥≥≥≥≥≥≥≥≥≥≥≥≥≥≥≥≥≥≥≥≥≥≥≥≥≥≥≥≥≥≥≥≥≥≥≥≥≥≥≥≥≥≥≥≥≥≥≥≥≥≥≥≥≥≥≥≥.≥ot `text_positions` needs filling (#29835)
         image_to_overwrite = torch.full(
             (batch_size, max_embed_dim), True, dtype=torch.bool, device=inputs_embeds.device
         )
@@ -531,6 +532,7 @@ class MPLUGDocOwlForConditionalGeneration(MPLUGDocOwlPreTrainedModel):
 
         final_embedding[image_to_overwrite] = image_features.contiguous().reshape(-1, embed_dim).to(target_device)
         final_attention_mask |= image_to_overwrite
+        modality_indicators[image_to_overwrite] = 1
         position_ids = (final_attention_mask.cumsum(-1) - 1).masked_fill_((final_attention_mask == 0), 1)
 
         # 6. Mask out the embedding at padding positions, as we later use the past_key_value value to determine the non-attended tokens.
@@ -541,8 +543,8 @@ class MPLUGDocOwlForConditionalGeneration(MPLUGDocOwlPreTrainedModel):
 
         if labels is None:
             final_labels = None
-
-        return final_embedding, final_attention_mask, final_labels, position_ids
+        breakpoint()
+        return final_embedding, final_attention_mask, final_labels, position_ids, modality_indicators
 
     @add_start_docstrings_to_model_forward(MPLUGDOCOWL_INPUTS_DOCSTRING)
     @replace_return_docstrings(output_type=MPLUGDocOwlCausalLMOutputWithPast, config_class=_CONFIG_FOR_DOC)
@@ -642,10 +644,10 @@ class MPLUGDocOwlForConditionalGeneration(MPLUGDocOwlPreTrainedModel):
                  #    input_ids, image_features, attention_mask, past_key_values, labels
                 #)
                 #FIXME old call is commented below
-                inputs_embeds, attention_mask, labels, position_ids = self._merge_input_ids_with_image_features(
+                inputs_embeds, attention_mask, labels, position_ids, modality_indicators = self._merge_input_ids_with_image_features(
                     image_features, inputs_embeds, input_ids, attention_mask, labels
                 )
-
+                breakpoint()
             # In case input_ids.shape[1] == 1 & pixel_values==None & past_key_values != None, we are in the case of
             # generation with cache
             elif past_key_values is not None and pixel_values is not None and input_ids.shape[1] == 1:
@@ -681,7 +683,7 @@ class MPLUGDocOwlForConditionalGeneration(MPLUGDocOwlPreTrainedModel):
 
         outputs = self.language_model(
             attention_mask=attention_mask,
-            #modality_indicators=modality_indicators,
+            modality_indicators=modality_indicators,
             position_ids=position_ids,
             past_key_values=past_key_values,
             inputs_embeds=inputs_embeds,