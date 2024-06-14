b745ab9f71dfd7b040a1bfc1af31161877c3b7e9
Wang, Yi <yi.a.wang@intel.com>
2024-06-14 08:12:46 -0400

    fix comment
    Signed-off-by: Wang, Yi <yi.a.wang@intel.com>

--- src/transformers/trainer.py
+++ src/transformers/trainer.py
@@ -3438,8 +3438,8 @@ class Trainer:
             self.tokenizer.save_pretrained(output_dir)
 
     def _save(self, output_dir: Optional[str] = None, state_dict=None):
-        # If we are executing this function, we are the process zero, so we don't check for that.
         output_dir = output_dir if output_dir is not None else self.args.output_dir
+        # For deepspeed and FSDP case, this function will be executed in each rank, so we should check should_save here.
         if self.args.should_save:
             os.makedirs(output_dir, exist_ok=True)
         logger.info(f"Saving model checkpoint to {output_dir}")
