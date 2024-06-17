10d727b112f48bae475be8450555e23cfc8708c1
Pablo Montalvo <pablo.montalvo.leroux@gmail.com>
2024-06-17 10:26:52 +0200

    fix tokenizer test

--- tests/models/altclip/test_processor_altclip.py
+++ tests/models/altclip/test_processor_altclip.py
@@ -3,7 +3,7 @@ import unittest
 import numpy as np
 
 from transformers import XLMRobertaTokenizer, XLMRobertaTokenizerFast
-from transformers.testing_utils import require_vision
+from transformers.testing_utils import require_torch, require_vision
 from transformers.utils import is_vision_available
 
 from ...test_processing_common import ProcessorTesterMixin
@@ -44,14 +44,27 @@ class AltClipProcessorTest(ProcessorTesterMixin, unittest.TestCase):
         image_inputs = [Image.fromarray(np.moveaxis(x, 0, -1)) for x in image_inputs]
         return image_inputs
 
-    def test_defaults_preserved(self):
-        image_processor = self.get_image_processor()
-        tokenizer = self.get_tokenizer(model_max_length=117)
-        processor = AltCLIPProcessor(tokenizer=tokenizer, image_processor=image_processor)
+    @require_torch
+    @require_vision
+    def test_unstructured_kwargs_batched(self):
+        if "image_processor" not in self.processor_class.attributes:
+            self.skipTest(f"image_processor attribute not present in {self.processor_class}")
+        image_processor = self.get_component("image_processor")
+        tokenizer = self.get_component("tokenizer")
 
-        input_str = "lower newer"
-        image_input = self.prepare_image_inputs()
+        processor = self.processor_class(tokenizer=tokenizer, image_processor=image_processor)
+        self.skip_processor_without_typed_kwargs(processor)
 
-        inputs = processor(text=input_str, images=image_input, padding="max_length")
+        input_str = ["lower newer", "upper older longer string"]
+        image_input = self.prepare_image_inputs() * 2
+        inputs = processor(
+            text=input_str,
+            images=image_input,
+            return_tensors="pt",
+            crop_size={"height": 214, "width": 214},
+            padding="longest",
+            max_length=76,
+        )
+        self.assertEqual(inputs["pixel_values"].shape[2], 214)
 
-        self.assertEqual(len(inputs["input_ids"]), 117)
+        self.assertEqual(len(inputs["input_ids"][0]), 7)
