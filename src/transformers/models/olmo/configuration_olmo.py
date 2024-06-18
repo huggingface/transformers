8d40a4d075e8b754684fdb6c51bf18855b29e1ff
Quentin Gallouédec <45557362+qgallouedec@users.noreply.github.com>
2024-06-18 17:09:50 +0200

    Fix documentation typos (#31476)
    Fix doc typo

--- src/transformers/models/olmo/configuration_olmo.py
+++ src/transformers/models/olmo/configuration_olmo.py
@@ -51,7 +51,7 @@ class OlmoConfig(PretrainedConfig):
         num_key_value_heads (`int`, *optional*):
             This is the number of key_value heads that should be used to implement Grouped Query Attention. If
             `num_key_value_heads=num_attention_heads`, the model will use Multi Head Attention (MHA), if
-            `num_key_value_heads=1 the model will use Multi Query Attention (MQA) otherwise GQA is used. When
+            `num_key_value_heads=1` the model will use Multi Query Attention (MQA) otherwise GQA is used. When
             converting a multi-head checkpoint to a GQA checkpoint, each group key and value head should be constructed
             by meanpooling all the original heads within that group. For more details checkout [this
             paper](https://arxiv.org/pdf/2305.13245.pdf). If it is not specified, will default to
