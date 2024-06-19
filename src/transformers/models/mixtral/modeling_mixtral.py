2aca410bdecc886973fa4085ca99120270136900
Luke20000429 <liuxs@umich.edu>
2024-06-19 10:25:18 -0700

    to meet formatting requirement

--- src/transformers/models/mixtral/modeling_mixtral.py
+++ src/transformers/models/mixtral/modeling_mixtral.py
@@ -444,10 +444,7 @@ class MixtralFlashAttention2(MixtralAttention):
             kv_seq_len += past_key_value.get_usable_length(kv_seq_len, self.layer_idx)
 
         # Because the input can be padded, the absolute sequence length depends on the max position id.
-        if position_ids is not None:
-            rotary_seq_len = max(kv_seq_len, position_ids[:, -1].max().item()+1)
-        else:
-            rotary_seq_len = kv_seq_len
+        rotary_seq_len = max(kv_seq_len, position_ids[:, -1].max().item() + 1) if position_ids is not None else kv_seq_len
         cos, sin = self.rotary_emb(value_states, seq_len=rotary_seq_len)
 
         query_states, key_states = apply_rotary_pos_emb(query_states, key_states, cos, sin, position_ids)
