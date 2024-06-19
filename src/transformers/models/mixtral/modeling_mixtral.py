f546a416d37fc2d15ccfba323df3794897cc8030
Luke20000429 <liuxs@umich.edu>
2024-06-19 10:35:04 -0700

    remove white space

--- src/transformers/models/mixtral/modeling_mixtral.py
+++ src/transformers/models/mixtral/modeling_mixtral.py
@@ -447,7 +447,7 @@ class MixtralFlashAttention2(MixtralAttention):
         rotary_seq_len = max(
             kv_seq_len, position_ids[:, -1].max().item() + 1
         ) if position_ids is not None else kv_seq_len
-        
+
         cos, sin = self.rotary_emb(value_states, seq_len=rotary_seq_len)
 
         query_states, key_states = apply_rotary_pos_emb(query_states, key_states, cos, sin, position_ids)
