686f2e0017ca720c277f43aa2f7c6a026c686490
amyeroberts <22614925+amyeroberts@users.noreply.github.com>
2024-06-19 10:18:08 +0100

    Use self.config_tester.run_common_tests() (#31431)
    * First testing updating config tests
    
    * Use run_common_tests

--- tests/models/upernet/test_modeling_upernet.py
+++ tests/models/upernet/test_modeling_upernet.py
@@ -161,15 +161,16 @@ class UperNetModelTest(ModelTesterMixin, PipelineTesterMixin, unittest.TestCase)
 
     def setUp(self):
         self.model_tester = UperNetModelTester(self)
-        self.config_tester = ConfigTester(self, config_class=UperNetConfig, has_text_modality=False, hidden_size=37)
+        self.config_tester = ConfigTester(
+            self,
+            config_class=UperNetConfig,
+            has_text_modality=False,
+            hidden_size=37,
+            common_properties=["hidden_size"],
+        )
 
     def test_config(self):
-        self.config_tester.create_and_test_config_to_json_string()
-        self.config_tester.create_and_test_config_to_json_file()
-        self.config_tester.create_and_test_config_from_and_save_pretrained()
-        self.config_tester.create_and_test_config_with_num_labels()
-        self.config_tester.check_config_can_be_init_without_params()
-        self.config_tester.check_config_arguments_init()
+        self.config_tester.run_common_tests()
 
     def test_for_semantic_segmentation(self):
         config_and_inputs = self.model_tester.prepare_config_and_inputs()
