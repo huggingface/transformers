492d30f2373469e8d1d4bb4d039f46fc13183926
Dave <69651599+D4ve-R@users.noreply.github.com>
2024-06-20 16:25:54 +0200

    fix florence2 config, make fixup

--- src/transformers/models/florence/configuration_florence2.py
+++ src/transformers/models/florence/configuration_florence2.py
@@ -11,11 +11,10 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+"""Florence-2 configuration"""
+
 import warnings
-
-
-""" Florence-2 configuration"""
-
 
 from ...configuration_utils import PretrainedConfig
 from ...utils import logging
