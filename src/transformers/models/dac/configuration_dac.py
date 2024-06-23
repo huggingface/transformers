6b52abe49afcf5cac52df1ae8db1fd8aab453aa2
kamilakesbi <kamil@huggingface.co>
2024-06-23 15:05:51 +0200

    up

--- src/transformers/models/dac/configuration_dac.py
+++ src/transformers/models/dac/configuration_dac.py
@@ -24,38 +24,68 @@ logger = logging.get_logger(__name__)
 
 class DacConfig(PretrainedConfig):
     r"""
-    """
+    This is the configuration class to store the configuration of an [`DacModel`]. It is used to instantiate a
+    Dac model according to the specified arguments, defining the model architecture. Instantiating a configuration
+    with the defaults will yield a similar configuration to that of the
+    [descript/dac_16khz](https://huggingface.co/descript/dac_16khz) architecture.
 
+    Configuration objects inherit from [`PretrainedConfig`] and can be used to control the model outputs. Read the
+    documentation from [`PretrainedConfig`] for more information.
+
+    Args:
+        encoder_hidden_size (`int`, *optional*, defaults to 64):
+            Intermediate representation dimension for the encoder.
+        downsampling_ratios (`List[int]`, *optional*, defaults to `[2, 4, 8, 8]`):
+            Ratios for downsampling in the encoder. These are used in reverse order for upsampling in the decoder.
+        decoder_hidden_size (`int`, *optional*, defaults to 1536):
+            Intermediate representation dimension for the decoder.
+        n_codebooks (`int`, *optional*, defaults to 9):
+            Number of codebooks in the VQVAE.
+        codebook_size (`int`, *optional*, defaults to 1024):
+            Number of discrete codes in each codebook.
+        codebook_dim (`int`, *optional*, defaults to 8):
+            Dimension of the codebook vectors. If not defined, uses `encoder_hidden_size`.
+        quantizer_dropout (`bool`, *optional*, defaults to `False`):
+            Whether to apply dropout to the quantizer.
+
+    Example:
+
+    ```python
+    >>> from transformers import DacModel, DacConfig
+
+    >>> # Initializing a "descript/dac_16khz" style configuration
+    >>> configuration = DacConfig()
+
+    >>> # Initializing a model (with random weights) from the "descript/dac_16khz" style configuration
+    >>> model = DacModel(configuration)
+
+    >>> # Accessing the model configuration
+    >>> configuration = model.config
+    """
     model_type = "dac"
 
     def __init__(
         self,
-        encoder_dim=64,
-        encoder_rates=[2, 4, 8, 8],
-        latent_dim=None,
-        decoder_dim=1536,
-        decoder_rates=[8, 8, 4, 2],
+        encoder_hidden_size=64,
+        downsampling_ratios=[2, 4, 8, 8],
+        decoder_hidden_size=1536,
         n_codebooks=9,
         codebook_size=1024,
         codebook_dim=8,
-        quantizer_dropout=False,
+        quantizer_dropout=0,
         **kwargs,
     ):
-        self.encoder_dim = encoder_dim
-        self.encoder_rates = encoder_rates
-        self.latent_dim = latent_dim
-        self.decoder_dim = decoder_dim
-        self.decoder_rates = decoder_rates
+        self.encoder_hidden_size = encoder_hidden_size
+        self.downsampling_ratios = downsampling_ratios
+        self.decoder_hidden_size = decoder_hidden_size
+        self.upsampling_ratios = downsampling_ratios[::-1]
         self.n_codebooks = n_codebooks
         self.codebook_size = codebook_size
         self.codebook_dim = codebook_dim
         self.quantizer_dropout = quantizer_dropout
 
-        if latent_dim is None:
-            latent_dim = encoder_dim * (2 ** len(encoder_rates))
+        self.hidden_size = encoder_hidden_size * (2 ** len(downsampling_ratios))
 
-        self.latent_dim = latent_dim
-
-        self.hop_length = int(np.prod(encoder_rates))
+        self.hop_length = int(np.prod(downsampling_ratios))
 
         super().__init__(**kwargs)
