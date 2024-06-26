a1d3866ce822cd6e9289872e44fe44c5946555b4
Arthur Zucker <arthur.zucker@gmail.com>
2024-06-26 09:18:43 +0200

    more init cleanups

--- src/transformers/models/starcoder2/modeling_starcoder2.py
+++ src/transformers/models/starcoder2/modeling_starcoder2.py
@@ -296,15 +296,6 @@ class Starcoder2FlashAttention2(Starcoder2Attention):
     flash attention and deal with padding tokens in case the input contains any of them.
     """
 
-    # Copied from transformers.models.llama.modeling_llama.LlamaFlashAttention2.__init__
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        # TODO: Should be removed once Flash Attention for RoCm is bumped to 2.1.
-        # flash_attn<2.1 generates top-left aligned causal mask, while what is needed here is bottom-right alignement, that was made default for flash_attn>=2.1. This attribute is used to handle this difference. Reference: https://github.com/Dao-AILab/flash-attention/releases/tag/v2.1.0.
-        # Beware that with flash_attn<2.1, using q_seqlen != k_seqlen (except for the case q_seqlen == 1) produces a wrong mask (top-left).
-        self._flash_attn_uses_top_left_mask = not is_flash_attn_greater_or_equal_2_10()
-
     # Ignore copy
     def forward(
         self,
@@ -340,7 +331,6 @@ class Starcoder2FlashAttention2(Starcoder2Attention):
         cos, sin = self.rotary_emb(value_states, seq_len=rotary_seq_len)
 
         query_states, key_states = apply_rotary_pos_emb(query_states, key_states, cos, sin, position_ids)
-
 
         if past_key_value is not None:
             # Activate slicing cache only if the config has a value `sliding_windows` attribute
@@ -411,8 +401,8 @@ class Starcoder2FlashAttention2(Starcoder2Attention):
             attention_mask,
             q_len,
             dropout=dropout_rate,
-            sliding_windows=getattr(self.config, "sliding_window", None) ,
-            cache_position=kv_seq_len
+            sliding_windows=getattr(self.config, "sliding_window", None),
+            cache_position=kv_seq_len,
         )
 
         attn_output = attn_output.reshape(bsz, q_len, self.hidden_size).contiguous()
