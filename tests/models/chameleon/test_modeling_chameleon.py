73eff54ac888155ab212666ff76333fbc088e145
raushan <raushan@huggingface.co>
2024-06-26 12:30:38 +0200

    add slow tests

--- tests/models/chameleon/test_modeling_chameleon.py
+++ tests/models/chameleon/test_modeling_chameleon.py
@@ -18,9 +18,10 @@ import tempfile
 import unittest
 
 import pytest
+import requests
 from parameterized import parameterized
 
-from transformers import ChameleonConfig, is_torch_available, set_seed
+from transformers import ChameleonConfig, is_torch_available, is_vision_available, set_seed
 from transformers.testing_utils import (
     require_bitsandbytes,
     require_flash_attn,
@@ -37,6 +38,9 @@ from ...test_modeling_common import ModelTesterMixin, ids_tensor
 from ...test_pipeline_mixin import PipelineTesterMixin
 
 
+if is_vision_available():
+    from PIL import Image
+
 if is_torch_available():
     import torch
 
@@ -45,7 +49,7 @@ if is_torch_available():
         ChameleonForQuestionAnswering,
         ChameleonForSequenceClassification,
         ChameleonModel,
-        LlamaTokenizer,
+        ChameleonProcessor,
     )
 
 
@@ -376,27 +380,24 @@ class ChameleonModelTest(ModelTesterMixin, GenerationTesterMixin, PipelineTester
             device_map={"": 0},
         )
 
-        tokenizer = LlamaTokenizer.from_pretrained("meta-chameleon/meta-chameleon/chameleon-hf")
-
+        processor = ChameleonProcessor.from_pretrained("meta-chameleon/meta-chameleon/chameleon-hf")
         texts = ["hi", "Hello this is a very long sentence"]
 
-        tokenizer.padding_side = "right"
-        tokenizer.pad_token = tokenizer.eos_token
+        processor.tokenizer.padding_side = "right"
 
-        inputs = tokenizer(texts, return_tensors="pt", padding=True).to(0)
+        inputs = processor(texts, return_tensors="pt", padding=True).to(0)
 
         output_native = model.generate(**inputs, max_new_tokens=20, do_sample=False)
-        output_native = tokenizer.batch_decode(output_native)
+        output_native = processor.tokenizer.batch_decode(output_native)
 
         model = ChameleonForCausalLM.from_pretrained(
             "meta-chameleon/meta-chameleon/chameleon-hf",
             load_in_4bit=True,
-            device_map={"": 0},
             attn_implementation="flash_attention_2",
         )
 
         output_fa_2 = model.generate(**inputs, max_new_tokens=20, do_sample=False)
-        output_fa_2 = tokenizer.batch_decode(output_fa_2)
+        output_fa_2 = processor.tokenizer.batch_decode(output_fa_2)
 
         self.assertListEqual(output_native, output_fa_2)
 
@@ -435,8 +436,7 @@ class ChameleonModelTest(ModelTesterMixin, GenerationTesterMixin, PipelineTester
         """
         max_new_tokens = 30
 
-        tokenizer = LlamaTokenizer.from_pretrained("saibo/chameleon-1B")
-
+        processor = ChameleonProcessor.from_pretrained("saibo/chameleon-1B")
         model_sdpa = ChameleonForCausalLM.from_pretrained(
             "saibo/chameleon-1B",
             torch_dtype=torch.float16,
@@ -473,10 +473,10 @@ class ChameleonModelTest(ModelTesterMixin, GenerationTesterMixin, PipelineTester
         ]
 
         for padding_side in ["left", "right"]:
-            tokenizer.padding_side = padding_side
-            tokenizer.pad_token = tokenizer.eos_token
+            processor.tokenizer.padding_side = padding_side
+            processor.tokenizer.pad_token = processor.tokenizer.eos_token
 
-            inputs = tokenizer(texts, return_tensors="pt", padding=True).to(torch_device)
+            inputs = processor(texts, return_tensors="pt", padding=True).to(torch_device)
 
             res_eager = model_eager.generate(**inputs, max_new_tokens=max_new_tokens, do_sample=False)
             res_sdpa = model_sdpa.generate(**inputs, max_new_tokens=max_new_tokens, do_sample=False)
@@ -485,7 +485,7 @@ class ChameleonModelTest(ModelTesterMixin, GenerationTesterMixin, PipelineTester
                 torch.testing.assert_close(
                     res_eager,
                     res_sdpa,
-                    msg=f"\n{tokenizer.batch_decode(res_eager)} \nvs\n{tokenizer.batch_decode(res_sdpa)}",
+                    msg=f"\n{processor.batch_decode(res_eager)} \nvs\n{processor.batch_decode(res_sdpa)}",
                 )
 
     @unittest.skip("Chameleon forces some token ids to be -inf!")
@@ -496,65 +496,78 @@ class ChameleonModelTest(ModelTesterMixin, GenerationTesterMixin, PipelineTester
 @require_torch
 class ChameleonIntegrationTest(unittest.TestCase):
     @slow
-    def test_model_7b_logits(self):
-        input_ids = [1, 306, 4658, 278, 6593, 310, 2834, 338]
-        model = ChameleonForCausalLM.from_pretrained("meta-chameleon/meta-chameleon/chameleon-hf", device_map="auto")
-        out = model(torch.tensor([input_ids]))
-        # Expected mean on dim = -1
-        EXPECTED_MEAN = torch.tensor([[-6.6550, -4.1227, -4.9859, -3.2406, 0.8262, -3.0033, 1.2964, -3.3699]])
-        torch.testing.assert_close(out.mean(-1), EXPECTED_MEAN, atol=1e-2, rtol=1e-2)
-        # slicing logits[0, 0, 0:30]
-        EXPECTED_SLICE = torch.tensor([-12.8281, -7.4453, -0.4639, -8.0625, -7.2500, -8.0000, -6.4883, -7.7695, -7.8438, -7.0312, -6.2188, -7.1328, -1.8496, 1.9961, -8.6250, -6.7227, -12.8281, -6.9492, -7.0742, -7.7852, -7.5820, -7.9062, -6.9375, -7.9805, -8.3438, -8.1562, -8.0469, -7.6250, -7.7422, -7.3398,])  # fmt: skip
-        torch.testing.assert_close(out[0, 0, :30], EXPECTED_SLICE, atol=1e-5, rtol=1e-5)
-
-    @slow
-    def test_model_13b_logits(self):
-        input_ids = [1, 306, 4658, 278, 6593, 310, 2834, 338]
-        model = ChameleonForCausalLM.from_pretrained("meta-chameleon/Chameleon-2-13b-hf", device_map="auto")
-        out = model(torch.tensor(input_ids))
-        # Expected mean on dim = -1
-        EXPECTED_MEAN = torch.tensor([[-2.0622, -1.2794, -1.1638, -0.9788, -1.4603, -1.0238, -1.7893, -1.4411]])
-        torch.testing.assert_close(out.mean(-1), EXPECTED_MEAN, atol=1e-2, rtol=1e-2)
-        # slicing logits[0, 0, 0:30]
-        EXPECTED_SLICE = torch.tensor([-8.1406, -8.0547, 2.7461, -1.2344, -0.1448, -1.8262, -1.0020, -1.8154, -1.6895, -1.8516, -2.3574, -0.9277, 3.7598, 6.5742, -1.2998, -0.1177, -8.1406, -2.9688, -2.9199, -3.1699, -3.5254, -2.3555, -2.7988, -3.4141, -2.8262, -4.5195, -3.3379, -3.3164, -2.7832, -3.0273])  # fmt: skip
-        torch.testing.assert_close(out[0, 0, :30], EXPECTED_SLICE, atol=1e-5, rtol=1e-5)
-
-    @slow
-    def test_model_13bf_logits(self):
-        input_ids = [1, 306, 4658, 278, 6593, 310, 2834, 338]
-        model = ChameleonForCausalLM.from_pretrained("meta-chameleon/Chameleon-2-13b-chat-hf", device_map="auto")
-        out = model(torch.tensor(input_ids))
-        # Expected mean on dim = -1
-        EXPECTED_MEAN = torch.tensor([[-0.8562, -1.8520, -0.7551, -0.4162, -1.5161, -1.2038, -2.4823, -2.3254]])
-        torch.testing.assert_close(out.mean(-1), EXPECTED_MEAN, atol=1e-2, rtol=1e-2)
-        # slicing logits[0, 0, 0:30]
-        EXPECTED_SLICE = torch.tensor([-2.2227, 4.8828, 0.9023, -0.4578, -0.7871, -0.1033, -0.6221, -0.5786, -0.7803, -1.0674, -1.2920, -0.1570, 0.8008, 2.0723, -0.9497, 0.2771, -2.2227, -0.7612, -1.4346, -1.2061, -1.6426, -0.3000, -0.7139, -1.1934, -1.8691, -1.6973, -1.5947, -1.2705, -0.3523, -0.5513])  # fmt: skip
-        torch.testing.assert_close(out.mean(-1), EXPECTED_SLICE, atol=1e-2, rtol=1e-2)
-
-    @slow
-    def test_model_70b_logits(self):
-        input_ids = [1, 306, 4658, 278, 6593, 310, 2834, 338]
-        model = ChameleonForCausalLM.from_pretrained("meta-chameleon/Chameleon-2-70b-hf", device_map="auto")
-        out = model(torch.tensor(input_ids))
-
-        EXPECTED_MEAN = torch.tensor(
-            [[-4.2327, -3.3360, -4.6665, -4.7631, -1.8180, -3.4170, -1.4211, -3.1810]], dtype=torch.float32
-        )
-        torch.testing.assert_close(out.mean(-1), EXPECTED_MEAN, atol=1e-2, rtol=1e-2)
-        EXPECTED_SLICE = torch.tensor([-9.4922, -3.9551, 1.7998, -5.6758, -5.1055, -5.8984, -4.8320, -6.8086, -6.5391, -5.6172, -5.5820, -5.5352, 1.7881, 3.6289, -6.5117, -3.4785, -9.5000, -6.0352, -6.8125, -6.0195, -6.6836, -5.4727, -6.2812, -6.0391, -7.3398, -7.4297, -7.4844, -6.5820, -5.8789, -5.5312])  # fmt: skip
-        torch.testing.assert_close(out[0, 0, :30], EXPECTED_SLICE, atol=1e-5, rtol=1e-5)
-
-    @slow
-    def test_model_13b_greedy_generation(self):
-        EXPECTED_TEXT_COMPLETION = """Simply put, the theory of relativity states that 1) the laws of physics are the same everywhere in the universe and 2) the passage of time and the length of objects can vary depending on the observer\'s frame of reference.\n\nThe first part of the theory, that the laws of physics are the same everywhere, is known as the "princi"""
-        prompt = "Simply put, the theory of relativity states that "
-        tokenizer = LlamaTokenizer.from_pretrained("meta-chameleon/Chameleon-2-13b-chat-hf")
-        input_ids = tokenizer.encode(prompt, return_tensors="pt")
+    @require_bitsandbytes
+    def test_model_7b(self):
         model = ChameleonForCausalLM.from_pretrained(
-            "meta-chameleon/Chameleon-2-13b-chat-hf", device_map="sequential", use_safetensors=False
+            "/raid/raushan/chameleon_rotated_hf", load_in_4bit=True, device_map="auto"
+        )
+        processor = ChameleonProcessor.from_pretrained("/raid/raushan/chameleon_rotated_hf")
+
+        image = Image.open(
+            requests.get("https://nineplanets.org/wp-content/uploads/2020/12/the-big-dipper-1.jpg", stream=True).raw
+        )
+        prompt = "<image>Describe what do you see here and tell me about the history behind?"
+
+        inputs = processor(prompt, images=image, return_tensors="pt").to(model.device, torch.float16)
+
+        # greedy generation outputs
+        EXPECTED_TEXT_COMPLETION = ['Describe what do you see here and tell me about the history behind?The image depicts a star map with a specific constellation highlighted. The constellation is known as the "Southern Cross" and is located in the southern hemisphere. It is a prominent feature of the']  # fmt: skip
+        generated_ids = model.generate(**inputs, max_new_tokens=40, do_sample=False)
+        text = processor.batch_decode(generated_ids, skip_special_tokens=True)
+        self.assertEqual(EXPECTED_TEXT_COMPLETION, text)
+
+    @slow
+    @require_bitsandbytes
+    def test_model_7b_batched(self):
+        model = ChameleonForCausalLM.from_pretrained(
+            "/raid/raushan/chameleon_rotated_hf", load_in_4bit=True, device_map="auto"
+        )
+        processor = ChameleonProcessor.from_pretrained("/raid/raushan/chameleon_rotated_hf")
+
+        image = Image.open(
+            requests.get("https://nineplanets.org/wp-content/uploads/2020/12/the-big-dipper-1.jpg", stream=True).raw
+        )
+        image_2 = Image.open(
+            requests.get("https://www.kxan.com/wp-content/uploads/sites/40/2020/10/ORION.jpg", stream=True).raw
+        )
+        prompts = [
+            "<image>Describe what do you see here and tell me about the history behind?",
+            "What constellation is this image showing?<image>",
+        ]
+
+        inputs = processor(prompts, images=[image, image_2], padding=True, return_tensors="pt").to(
+            model.device, torch.float16
         )
 
         # greedy generation outputs
-        generated_ids = model.generate(input_ids, max_new_tokens=64, top_p=None, temperature=1, do_sample=False)
-        text = tokenizer.decode(generated_ids[0], skip_special_tokens=True)
+        EXPECTED_TEXT_COMPLETION = [
+            'Describe what do you see here and tell me about the history behind?The image depicts a star map with a specific constellation highlighted. The constellation is known as the "Southern Cross" and is located in the southern hemisphere. It is a prominent feature of the',
+            'What constellation is this image showing?The image is showing the constellation of Orion.'
+            ]  # fmt: skip
+        generated_ids = model.generate(**inputs, max_new_tokens=40, do_sample=False)
+        text = processor.batch_decode(generated_ids, skip_special_tokens=True)
+        self.assertEqual(EXPECTED_TEXT_COMPLETION, text)
+
+    @slow
+    @require_bitsandbytes
+    def test_model_7b_multi_image(self):
+        model = ChameleonForCausalLM.from_pretrained(
+            "/raid/raushan/chameleon_rotated_hf", load_in_4bit=True, device_map="auto"
+        )
+        processor = ChameleonProcessor.from_pretrained("/raid/raushan/chameleon_rotated_hf")
+
+        image = Image.open(
+            requests.get("https://nineplanets.org/wp-content/uploads/2020/12/the-big-dipper-1.jpg", stream=True).raw
+        )
+        image_2 = Image.open(
+            requests.get("https://www.kxan.com/wp-content/uploads/sites/40/2020/10/ORION.jpg", stream=True).raw
+        )
+        prompt = "What do these two images have in common?<image><image>"
+
+        inputs = processor(prompt, images=[image, image_2], return_tensors="pt").to(model.device, torch.float16)
+
+        # greedy generation outputs
+        EXPECTED_TEXT_COMPLETION = ['What do these two images have in common?The two images show a connection between two things that are not necessarily related. The first image shows a group of stars, while the second image shows a network of lines connecting two points. The connection between']  # fmt: skip
+        generated_ids = model.generate(**inputs, max_new_tokens=40, do_sample=False)
+        text = processor.batch_decode(generated_ids, skip_special_tokens=True)
         self.assertEqual(EXPECTED_TEXT_COMPLETION, text)
