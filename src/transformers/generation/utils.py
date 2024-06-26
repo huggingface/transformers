66f8b693f736e107eebcc5411ec8bd8d28049386
Joao Gante <joaofranciscocardosogante@gmail.com>
2024-06-26 18:24:04 +0100

    Generate: fix assisted generation with `past_key_values` passed as kwargs (#31644)

--- src/transformers/generation/utils.py
+++ src/transformers/generation/utils.py
@@ -3697,11 +3697,10 @@ class GenerationMixin:
         model_kwargs = self._get_initial_cache_position(input_ids, model_kwargs)
 
         # This is needed if return_dict_in_generate is True
+        start_from_empty_dynamic_cache = False
         if isinstance(model_kwargs.get("past_key_values", None), DynamicCache):
             if len(model_kwargs["past_key_values"]) == 0:
                 start_from_empty_dynamic_cache = True
-        else:
-            start_from_empty_dynamic_cache = False
 
         this_peer_finished = False
         while self._has_unfinished_sequences(this_peer_finished, synced_gpus, device=input_ids.device):
