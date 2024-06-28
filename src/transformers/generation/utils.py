1fc3e1e3590778d2263a9db9d4c8e7672a276b61
sanchit-gandhi <sanchit@huggingface.co>
2024-06-28 14:07:46 +0100

    [generate] fix eos/pad id check on mps devices

--- src/transformers/generation/utils.py
+++ src/transformers/generation/utils.py
@@ -1510,7 +1510,7 @@ class GenerationMixin:
             logger.warning(f"Setting `pad_token_id` to `eos_token_id`:{pad_token_id} for open-end generation.")
 
         # we can't infer attn mask if pad token is set to be eos token in model's generation config
-        if eos_token_id is not None and torch.isin(elements=eos_token_id, test_elements=pad_token_id).any():
+        if eos_token_id is not None and pad_token_id in eos_token_id:
             if kwargs_has_attention_mask is not None and not kwargs_has_attention_mask:
                 logger.warning_once(
                     "The attention mask is not set and cannot be inferred from input because pad token is same as eos token."
