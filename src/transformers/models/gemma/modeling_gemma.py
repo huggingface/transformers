b6f30f537655e5f32fd752eb995b68b2abe537ee
ydshieh <ydshieh@users.noreply.github.com>
2024-07-04 17:27:38 +0200

    fix

--- src/transformers/models/gemma/modeling_gemma.py
+++ src/transformers/models/gemma/modeling_gemma.py
@@ -907,9 +907,10 @@ class GemmaModel(GemmaPreTrainedModel):
         if cache_length is None:
             past_seen_tokens = past_key_values.get_seq_length() if past_key_values is not None else 0
             cache_length = past_seen_tokens + inputs_embeds.shape[1]
-            cache_position = torch.arange(
-                past_seen_tokens, cache_length, device=inputs_embeds.device
-            )
+
+        cache_position = torch.arange(
+            past_seen_tokens, cache_length, device=inputs_embeds.device
+        )
 
         if position_ids is None:
             position_ids = cache_position.unsqueeze(0)
