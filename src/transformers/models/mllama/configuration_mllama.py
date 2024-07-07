3e031b70f390dffbaf2613b5ac285a558a43172f
Pablo <pablo.montalvo.leroux@gmail.com>
2024-07-07 22:58:21 +0000

    add vision and text config

--- src/transformers/models/mllama/configuration_mllama.py
+++ src/transformers/models/mllama/configuration_mllama.py
@@ -18,11 +18,195 @@ import warnings
 from ...configuration_utils import PretrainedConfig
 from ...utils import logging
 from ..auto import CONFIG_MAPPING
-
+import os
+from typing import Union
 
 logger = logging.get_logger(__name__)
 
 
+class MllamaCrossAttentionVisionConfig(PretrainedConfig):
+    # TODO fix config docstring
+    r"""
+    This is the configuration class to store the configuration of a [`MllamaForConditionalGeneration`]. It is used to instantiate an
+    Mllama model according to the specified arguments, defining the model architecture. Instantiating a configuration
+    with the defaults will yield a similar configuration to that of the Mllama-9B.
+
+    e.g. [mllama-hf/mllama-9b](https://huggingface.co/mllama-hf/mllama-9b)
+
+    Configuration objects inherit from [`PretrainedConfig`] and can be used to control the model outputs. Read the
+    documentation from [`PretrainedConfig`] for more information.
+
+    Args:
+        vision_config (`Union[AutoConfig, dict]`,  *optional*, defaults to `CLIPVisionConfig`):
+            The config object or dictionary of the vision backbone.
+        text_config (`Union[AutoConfig, dict]`, *optional*, defaults to `LlamaConfig`):
+            The config object or dictionary of the text backbone.
+        ignore_index (`int`, *optional*, defaults to -100):
+            The ignore index for the loss function.
+        image_token_index (`int`, *optional*, defaults to 32000):
+            The image token index to encode the image prompt.
+        projector_hidden_act (`str`, *optional*, defaults to `"gelu"`):
+            The activation function used by the multimodal projector.
+        vision_feature_select_strategy (`str`, *optional*, defaults to `"default"`):
+            The feature selection strategy used to select the vision feature from the vision backbone.
+            Can be one of `"default"` or `"full"`.
+        vision_feature_layer (`int`, *optional*, defaults to -2):
+        The index of the layer to select the vision feature.
+
+    Example:
+
+    ```python
+    >>> from transformers import MllamaForConditionalGeneration, MllamaConfig, CLIPVisionConfig, LlamaConfig
+
+    >>> # Initializing a CLIP-vision config
+    >>> vision_config = CLIPVisionConfig()
+
+    >>> # Initializing a Llama config
+    >>> text_config = LlamaConfig()
+
+    >>> # Initializing a Mllama mllama-1.5-7b style configuration
+    >>> configuration = MllamaConfig(vision_config, text_config)
+
+    >>> # Initializing a model from the mllama-1.5-7b style configuration
+    >>> model = MllamaForConditionalGeneration(configuration)
+
+    >>> # Accessing the model configuration
+    >>> configuration = model.config
+    ```"""
+    model_type = "mllama"
+    def __init__(
+        self,
+        n_heads=16,
+        vision_chunk_size=560,
+        vision_max_num_chunks=4,
+        patch_size=14,
+        projection_dim=8192,
+        vision_input_dim=1280,
+        return_intermediate="3,7,15,23,30",
+        global_vision_layers=8,
+        max_num_tiles=4, # same as vision max num chunks?
+        **kwargs,
+    ):
+        self.n_heads=n_heads
+        self.vision_chunk_size = vision_chunk_size
+        self.vision_max_num_chunks = vision_max_num_chunks
+        self.patch_size = patch_size
+        self.projection_dim = projection_dim
+        self.vision_input_dim = vision_input_dim
+        self.return_intermediate = return_intermediate
+        self.global_vision_layers = global_vision_layers
+        self.max_num_tiles = max_num_tiles
+        super().__init__(**kwargs)
+
+
+    @classmethod
+    def from_pretrained(cls, pretrained_model_name_or_path: Union[str, os.PathLike], **kwargs) -> "PretrainedConfig":
+        cls._set_token_in_kwargs(kwargs)
+
+        config_dict, kwargs = cls.get_config_dict(pretrained_model_name_or_path, **kwargs)
+
+        if config_dict.get("model_type") == "mllama":
+            config_dict = config_dict["vision_config"]
+
+        if "model_type" in config_dict and hasattr(cls, "model_type") and config_dict["model_type"] != cls.model_type:
+            logger.warning(
+                f"You are using a model of type {config_dict['model_type']} to instantiate a model of type "
+                f"{cls.model_type}. This is not supported for all configurations of models and can yield errors."
+            )
+
+        return cls.from_dict(config_dict, **kwargs)
+
+
+
+class MllamaCrossAttentionTextConfig(PretrainedConfig):
+    # TODO fix config docstring
+    r"""
+    This is the configuration class to store the configuration of a [`MllamaForConditionalGeneration`]. It is used to instantiate an
+    Mllama model according to the specified arguments, defining the model architecture. Instantiating a configuration
+    with the defaults will yield a similar configuration to that of the Mllama-9B.
+
+    e.g. [mllama-hf/mllama-9b](https://huggingface.co/mllama-hf/mllama-9b)
+
+    Configuration objects inherit from [`PretrainedConfig`] and can be used to control the model outputs. Read the
+    documentation from [`PretrainedConfig`] for more information.
+
+    Args:
+        vision_config (`Union[AutoConfig, dict]`,  *optional*, defaults to `CLIPVisionConfig`):
+            The config object or dictionary of the vision backbone.
+        text_config (`Union[AutoConfig, dict]`, *optional*, defaults to `LlamaConfig`):
+            The config object or dictionary of the text backbone.
+        ignore_index (`int`, *optional*, defaults to -100):
+            The ignore index for the loss function.
+        image_token_index (`int`, *optional*, defaults to 32000):
+            The image token index to encode the image prompt.
+        projector_hidden_act (`str`, *optional*, defaults to `"gelu"`):
+            The activation function used by the multimodal projector.
+        vision_feature_select_strategy (`str`, *optional*, defaults to `"default"`):
+            The feature selection strategy used to select the vision feature from the vision backbone.
+            Can be one of `"default"` or `"full"`.
+        vision_feature_layer (`int`, *optional*, defaults to -2):
+        The index of the layer to select the vision feature.
+
+    Example:
+
+    ```python
+    >>> from transformers import MllamaForConditionalGeneration, MllamaConfig, CLIPVisionConfig, LlamaConfig
+
+    >>> # Initializing a CLIP-vision config
+    >>> vision_config = CLIPVisionConfig()
+
+    >>> # Initializing a Llama config
+    >>> text_config = LlamaConfig()
+
+    >>> # Initializing a Mllama mllama-1.5-7b style configuration
+    >>> configuration = MllamaConfig(vision_config, text_config)
+
+    >>> # Initializing a model from the mllama-1.5-7b style configuration
+    >>> model = MllamaForConditionalGeneration(configuration)
+
+    >>> # Accessing the model configuration
+    >>> configuration = model.config
+    ```"""
+    model_type = "mllama"
+    def __init__(
+        self,
+        vocab_size=128256,
+        n_layers=80,
+        dim=8192,
+        n_heads=64,
+        n_kv_heads=8,
+        max_seq_len=512,
+        ffn_dim_multiplier= 1.3,
+        norm_eps= 1.0e-5,
+        rope_theta= 500000,
+        use_scaled_rope=True, # TODO finish up text config
+        **kwargs,
+    ):
+        self.vocab_size = vocab_size
+        self.n_layers = n_layers
+        self.dim = dim
+        self.n_heads = n_heads
+        self.n_kv_heads = n_kv_heads
+        self.max_seq_len = max_seq_len
+        super().__init__(**kwargs)
+
+
+    @classmethod
+    def from_pretrained(cls, pretrained_model_name_or_path: Union[str, os.PathLike], **kwargs) -> "PretrainedConfig":
+        cls._set_token_in_kwargs(kwargs)
+
+        config_dict, kwargs = cls.get_config_dict(pretrained_model_name_or_path, **kwargs)
+
+        if config_dict.get("model_type") == "mllama":
+            config_dict = config_dict["text_config"]
+
+        if "model_type" in config_dict and hasattr(cls, "model_type") and config_dict["model_type"] != cls.model_type:
+            logger.warning(
+                f"You are using a model of type {config_dict['model_type']} to instantiate a model of type "
+                f"{cls.model_type}. This is not supported for all configurations of models and can yield errors."
+            )
+
+        return cls.from_dict(config_dict, **kwargs)
 class MllamaConfig(PretrainedConfig):
     r"""
     This is the configuration class to store the configuration of a [`MllamaForConditionalGeneration`]. It is used to instantiate an
@@ -49,7 +233,7 @@ class MllamaConfig(PretrainedConfig):
             The feature selection strategy used to select the vision feature from the vision backbone.
             Can be one of `"default"` or `"full"`.
         vision_feature_layer (`int`, *optional*, defaults to -2):
-            The index of the layer to select the vision feature.
+        The index of the layer to select the vision feature.
 
     Example:
 
@@ -78,7 +262,6 @@ class MllamaConfig(PretrainedConfig):
     def __init__(
         self,
         vision_config=None,
-        global_vision_config=None,
         text_config=None,
         **kwargs,
     ):
@@ -93,11 +276,11 @@ class MllamaConfig(PretrainedConfig):
                 intermediate_size=4096,
                 hidden_size=1024,
                 patch_size=14,
-                image_size=336,
+                image_size=448,
                 num_hidden_layers=32,
                 num_attention_heads=16,
-                vocab_size=32000,
-                projection_dim=768,
+                vision_chunk_size= 560,
+                vision_max_num_chunks= 4,
             )
 
         self.vision_config = vision_config
@@ -107,7 +290,7 @@ class MllamaConfig(PretrainedConfig):
             text_config = CONFIG_MAPPING[text_config["model_type"]](**text_config)
         elif text_config is None:
             text_config = CONFIG_MAPPING["llama"](
-                
+                vision_num_cross_attention_layers=20, # Should be maybe in vision config?
             )
 
         self.text_config = text_config
@@ -115,19 +298,19 @@ class MllamaConfig(PretrainedConfig):
 
         super().__init__(**kwargs)
 
-    @property
-    def vocab_size(self):
-        warnings.warn(
-            "The `vocab_size` attribute is deprecated and will be removed in v4.42, Please use `text_config.vocab_size` instead.",
-            FutureWarning,
-        )
-        return self._vocab_size
+        @property
+        def vocab_size(self):
+            warnings.warn(
+                "The `vocab_size` attribute is deprecated and will be removed in v4.42, Please use `text_config.vocab_size` instead.",
+                FutureWarning,
+            )
+            return self._vocab_size
 
-    @vocab_size.setter
-    def vocab_size(self, value):
-        self._vocab_size = value
+        @vocab_size.setter
+        def vocab_size(self, value):
+            self._vocab_size = value
 
-    def to_dict(self):
-        output = super().to_dict()
-        output.pop("_vocab_size", None)
-        return output
+        def to_dict(self):
+            output = super().to_dict()
+            output.pop("_vocab_size", None)
+            return output
