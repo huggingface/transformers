d5e5610fbe0620e9341c78e03756a53e28137dff
ydshieh <ydshieh@users.noreply.github.com>
2024-07-10 11:21:42 +0200

    fix

--- src/transformers/trainer_seq2seq.py
+++ src/transformers/trainer_seq2seq.py
@@ -113,7 +113,7 @@ class Seq2SeqTrainer(Trainer):
         # an exception if there are warnings at validation time.
 
         from transformers.utils.logging import _get_library_root_logger, get_logger, captureWarnings
-        captureWarnings(True)
+        captureWarnings(False)
 
         assert _get_library_root_logger().level == 30
         assert str(logger) == "<Logger transformers.trainer_seq2seq (WARNING)>"
