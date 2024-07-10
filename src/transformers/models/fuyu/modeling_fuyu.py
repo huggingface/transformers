8e6a86215ab0de824f3d205ae01e7d8f68f65dad
Isotr0py <2037008807@qq.com>
2024-07-10 13:49:45 +0800

    fix a typo

--- src/transformers/models/fuyu/modeling_fuyu.py
+++ src/transformers/models/fuyu/modeling_fuyu.py
@@ -172,7 +172,7 @@ class FuyuForCausalLM(FuyuPreTrainedModel):
         return self.language_model.get_output_embeddings()
     
     def set_output_embeddings(self, new_embeddings):
-        self.language_model.set_input_embeddings(new_embeddings)
+        self.language_model.set_output_embeddings(new_embeddings)
 
     def set_decoder(self, decoder):
         self.language_model.set_decoder(decoder)
