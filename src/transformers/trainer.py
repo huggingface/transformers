b70c877ef4d1a6e084856e8abbf16f2a299203d2
apoorvkh <mail@apoorvkh.com>
2024-07-11 13:41:52 -0400

    Revert `get_optimizer_cls_and_kwargs` to staticmethod

--- src/transformers/trainer.py
+++ src/transformers/trainer.py
@@ -1078,7 +1078,7 @@ class Trainer:
                 },
             ]
 
-            optimizer_cls, optimizer_kwargs = self.get_optimizer_cls_and_kwargs(opt_model)
+            optimizer_cls, optimizer_kwargs = self.get_optimizer_cls_and_kwargs(self.args, opt_model)
 
             # Overwrite `params` in case it's created by `get_optimizer_cls_and_kwargs`
             # e.g. for GaLore optimizer.
@@ -1146,7 +1146,10 @@ class Trainer:
                     return group
         return [group["params"] for group in self.optimizer.param_groups]
 
-    def get_optimizer_cls_and_kwargs(self, model: Optional[PreTrainedModel] = None) -> Tuple[Any, Any]:
+    @staticmethod
+    def get_optimizer_cls_and_kwargs(
+        args: TrainingArguments, model: Optional[PreTrainedModel] = None
+    ) -> Tuple[Any, Any]:
         """
         Returns the optimizer class and optimizer parameters based on the training arguments.
 
@@ -1158,33 +1161,33 @@ class Trainer:
 
         # parse args.optim_args
         optim_args = {}
-        if self.args.optim_args:
-            for mapping in self.args.optim_args.replace(" ", "").split(","):
+        if args.optim_args:
+            for mapping in args.optim_args.replace(" ", "").split(","):
                 key, value = mapping.split("=")
                 optim_args[key] = value
 
-        optimizer_kwargs = {"lr": self.args.learning_rate}
+        optimizer_kwargs = {"lr": args.learning_rate}
 
         adam_kwargs = {
-            "betas": (self.args.adam_beta1, self.args.adam_beta2),
-            "eps": self.args.adam_epsilon,
+            "betas": (args.adam_beta1, args.adam_beta2),
+            "eps": args.adam_epsilon,
         }
-        if self.args.optim == OptimizerNames.ADAFACTOR:
+        if args.optim == OptimizerNames.ADAFACTOR:
             optimizer_cls = Adafactor
             optimizer_kwargs.update({"scale_parameter": False, "relative_step": False})
-        elif self.args.optim == OptimizerNames.ADAMW_HF:
+        elif args.optim == OptimizerNames.ADAMW_HF:
             from .optimization import AdamW
 
             optimizer_cls = AdamW
             optimizer_kwargs.update(adam_kwargs)
-        elif self.args.optim in [OptimizerNames.ADAMW_TORCH, OptimizerNames.ADAMW_TORCH_FUSED]:
+        elif args.optim in [OptimizerNames.ADAMW_TORCH, OptimizerNames.ADAMW_TORCH_FUSED]:
             from torch.optim import AdamW
 
             optimizer_cls = AdamW
             optimizer_kwargs.update(adam_kwargs)
-            if self.args.optim == OptimizerNames.ADAMW_TORCH_FUSED:
+            if args.optim == OptimizerNames.ADAMW_TORCH_FUSED:
                 optimizer_kwargs.update({"fused": True})
-        elif self.args.optim == OptimizerNames.ADAMW_TORCH_XLA:
+        elif args.optim == OptimizerNames.ADAMW_TORCH_XLA:
             try:
                 from torch_xla.amp.syncfree import AdamW
 
@@ -1192,7 +1195,7 @@ class Trainer:
                 optimizer_kwargs.update(adam_kwargs)
             except ImportError:
                 raise ValueError("Trainer failed to import syncfree AdamW from torch_xla.")
-        elif self.args.optim == OptimizerNames.ADAMW_TORCH_NPU_FUSED:
+        elif args.optim == OptimizerNames.ADAMW_TORCH_NPU_FUSED:
             try:
                 from torch_npu.optim import NpuFusedAdamW
 
@@ -1200,7 +1203,7 @@ class Trainer:
                 optimizer_kwargs.update(adam_kwargs)
             except ImportError:
                 raise ValueError("Trainer failed to import FusedAdamW from torch_npu.")
-        elif self.args.optim == OptimizerNames.ADAMW_APEX_FUSED:
+        elif args.optim == OptimizerNames.ADAMW_APEX_FUSED:
             try:
                 from apex.optimizers import FusedAdam
 
@@ -1208,7 +1211,7 @@ class Trainer:
                 optimizer_kwargs.update(adam_kwargs)
             except ImportError:
                 raise ValueError("Trainer tried to instantiate apex FusedAdam but apex is not installed!")
-        elif self.args.optim in [
+        elif args.optim in [
             OptimizerNames.ADAMW_BNB,
             OptimizerNames.ADAMW_8BIT,
             OptimizerNames.PAGED_ADAMW,
@@ -1228,23 +1231,23 @@ class Trainer:
                 optim_bits = 32
                 optimizer_cls = None
                 additional_optim_kwargs = adam_kwargs
-                if "paged" in self.args.optim:
+                if "paged" in args.optim:
                     is_paged = True
-                if "8bit" in self.args.optim:
+                if "8bit" in args.optim:
                     optim_bits = 8
-                if "adam" in self.args.optim:
+                if "adam" in args.optim:
                     optimizer_cls = AdamW
-                elif "lion" in self.args.optim:
+                elif "lion" in args.optim:
                     optimizer_cls = Lion
-                    additional_optim_kwargs = {"betas": (self.args.adam_beta1, self.args.adam_beta2)}
-                elif "rmsprop" in self.args.optim:
+                    additional_optim_kwargs = {"betas": (args.adam_beta1, args.adam_beta2)}
+                elif "rmsprop" in args.optim:
                     optimizer_cls = RMSprop
                     # Above we pass all `adam_kwargs` to the optimizer, here
                     # we only pass `optim_args` which can be passed by the user.
                     additional_optim_kwargs = optim_args
 
                 bnb_kwargs = {"optim_bits": optim_bits}
-                if "rmsprop" not in self.args.optim:
+                if "rmsprop" not in args.optim:
                     bnb_kwargs["is_paged"] = is_paged
 
                 optimizer_kwargs.update(additional_optim_kwargs)
@@ -1258,7 +1261,7 @@ class Trainer:
                     "You are using 8-bit optimizers with a version of `bitsandbytes` < 0.41.1. "
                     "It is recommended to update your version as a major bug has been fixed in 8-bit optimizers."
                 )
-        elif self.args.optim == OptimizerNames.ADAMW_ANYPRECISION:
+        elif args.optim == OptimizerNames.ADAMW_ANYPRECISION:
             try:
                 from torchdistx.optimizers import AnyPrecisionAdamW
 
@@ -1278,13 +1281,13 @@ class Trainer:
                 )
             except ImportError:
                 raise ValueError("Please install https://github.com/pytorch/torchdistx")
-        elif self.args.optim == OptimizerNames.SGD:
+        elif args.optim == OptimizerNames.SGD:
             optimizer_cls = torch.optim.SGD
-        elif self.args.optim == OptimizerNames.ADAGRAD:
+        elif args.optim == OptimizerNames.ADAGRAD:
             optimizer_cls = torch.optim.Adagrad
-        elif self.args.optim == OptimizerNames.RMSPROP:
+        elif args.optim == OptimizerNames.RMSPROP:
             optimizer_cls = torch.optim.RMSprop
-        elif self.args.optim in [
+        elif args.optim in [
             OptimizerNames.GALORE_ADAMW,
             OptimizerNames.GALORE_ADAMW_8BIT,
             OptimizerNames.GALORE_ADAFACTOR,
@@ -1299,8 +1302,8 @@ class Trainer:
                 )
             from galore_torch import GaLoreAdafactor, GaLoreAdamW, GaLoreAdamW8bit
 
-            is_layerwise = self.args.optim.lower().endswith("layerwise")
-            if is_layerwise and self.args.parallel_mode == ParallelMode.DISTRIBUTED:
+            is_layerwise = args.optim.lower().endswith("layerwise")
+            if is_layerwise and args.parallel_mode == ParallelMode.DISTRIBUTED:
                 raise NotImplementedError("Layer-wise GaLore does not support DDP at this time")
 
             optimizer_mapping = {
@@ -1312,16 +1315,16 @@ class Trainer:
                 OptimizerNames.GALORE_ADAFACTOR_LAYERWISE: GaLoreAdafactor,
             }
 
-            optimizer_cls = optimizer_mapping[self.args.optim]
+            optimizer_cls = optimizer_mapping[args.optim]
 
-            if self.args.optim_target_modules is None:
+            if args.optim_target_modules is None:
                 raise ValueError(
                     "You need to define a `optim_target_modules` in order to properly use GaLore optimizers"
                 )
 
-            if not isinstance(self.args.optim_target_modules, (list, str)):
+            if not isinstance(args.optim_target_modules, (list, str)):
                 raise ValueError(
-                    f"`optim_target_modules` has to be a list of strings, a string corresponding to a regex, or a specific module or 'all-linear', you passed {self.args.optim_target_modules}"
+                    f"`optim_target_modules` has to be a list of strings, a string corresponding to a regex, or a specific module or 'all-linear', you passed {args.optim_target_modules}"
                 )
 
             if model is None:
@@ -1332,15 +1335,15 @@ class Trainer:
             )
 
             all_linear = (
-                isinstance(self.args.optim_target_modules, str)
-                and self.args.optim_target_modules.replace("_", "-") == "all-linear"
+                isinstance(args.optim_target_modules, str)
+                and args.optim_target_modules.replace("_", "-") == "all-linear"
             )
 
             galore_params = []
             galore_params_names = []
             for module_name, module in model.named_modules():
                 target_module_exists, is_regex = check_target_module_exists(
-                    self.args.optim_target_modules, module_name, return_is_regex=True
+                    args.optim_target_modules, module_name, return_is_regex=True
                 )
 
                 if not isinstance(module, nn.Linear):
@@ -1360,7 +1363,7 @@ class Trainer:
 
             if len(galore_params) == 0:
                 raise ValueError(
-                    f"None of the target modules were found! ({self.args.optim_target_modules}). Please make sure to pass a valid `target_modules`."
+                    f"None of the target modules were found! ({args.optim_target_modules}). Please make sure to pass a valid `target_modules`."
                 )
 
             non_galore_params = [p for n, p in model.named_parameters() if n not in galore_params_names]
@@ -1384,7 +1387,7 @@ class Trainer:
                 # create a dummy optimizer that will perform no-ops in the Trainer.
                 # See the original implementation or the nice implementation from @hiyouga
                 # here: https://github.com/hiyouga/LLaMA-Factory/commit/8664262cde3919e10eaecbd66e8c5d356856362e#diff-ebe08ab14496dfb9e06075f0fdd36799ef6d1535cc4dd4715b74c4e3e06fe3ba
-                if self.args.gradient_accumulation_steps != 1:
+                if args.gradient_accumulation_steps != 1:
                     raise ValueError("Layerwise GaLoRE optimizer do not support gradient accumulation !")
 
                 optimizer_dict = {}
@@ -1409,9 +1412,9 @@ class Trainer:
 
             optimizer_kwargs.update({"params": param_groups})
 
-            if self.args.optim == OptimizerNames.GALORE_ADAFACTOR:
+            if args.optim == OptimizerNames.GALORE_ADAFACTOR:
                 optimizer_kwargs.update({"scale_parameter": False, "relative_step": False})
-        elif self.args.optim in [OptimizerNames.LOMO, OptimizerNames.ADALOMO]:
+        elif args.optim in [OptimizerNames.LOMO, OptimizerNames.ADALOMO]:
             if not is_lomo_available():
                 raise ImportError(
                     "You need to install `lomo_optim` in order to use LOMO optimizers"
@@ -1425,14 +1428,14 @@ class Trainer:
 
             from lomo_optim import AdaLomo, Lomo
 
-            if "ada" in self.args.optim:
+            if "ada" in args.optim:
                 optimizer_cls = AdaLomo
             else:
                 optimizer_cls = Lomo
 
             optimizer_kwargs.update({"model": model})
         else:
-            raise ValueError(f"Trainer cannot instantiate unsupported optimizer: {self.args.optim}")
+            raise ValueError(f"Trainer cannot instantiate unsupported optimizer: {args.optim}")
         return optimizer_cls, optimizer_kwargs
 
     def create_scheduler(self, num_training_steps: int, optimizer: torch.optim.Optimizer = None):
