ccf47d6481b928a6e914e1c2f1d2fed6a1b4e4a8
Dattu Sharma <venkatadattasainimmaturi@gmail.com>
2024-07-12 16:25:45 +0000

    Fixup ruff linter
    Signed-off-by: Dattu Sharma <venkatadattasainimmaturi@gmail.com>

--- src/transformers/optimization.py
+++ src/transformers/optimization.py
@@ -20,17 +20,17 @@ from functools import partial
 from typing import Callable, Iterable, Optional, Tuple, Union
 
 import torch
+import torch.distributed as dist
 from torch import nn
+from torch.distributed._tensor import Replicate
 from torch.optim import Optimizer
 from torch.optim.lr_scheduler import LambdaLR, ReduceLROnPlateau
-import torch.distributed as dist
-from torch.distributed._tensor import Replicate
 
+from .modeling_utils import PreTrainedModel
 from .trainer_pt_utils import LayerWiseDummyOptimizer, LayerWiseDummyScheduler
 from .trainer_utils import SchedulerType
 from .utils import logging
 from .utils.versions import require_version
-from .modeling_utils import PreTrainedModel
 
 
 logger = logging.get_logger(__name__)
@@ -941,7 +941,7 @@ class AdafactorSchedule(LambdaLR):
         if len(lrs) == 0:
             lrs = self.base_lrs  # if called before stepping
         return lrs
-    
+
 
 class AdamMini(Optimizer):
     """
@@ -984,7 +984,6 @@ class AdamMini(Optimizer):
         n_head: int = 32,
         n_kv_head: int = None,
     ):
-
         if lr < 0.0:
             raise ValueError(f"Invalid learning rate: {lr} - should be >= 0.0")
         if not 0.0 <= betas[0] < 1.0:
@@ -995,7 +994,7 @@ class AdamMini(Optimizer):
             raise ValueError(f"Invalid weight_decay value: {weight_decay} - should be >= 0.0")
         if not 0.0 <= eps:
             raise ValueError(f"Invalid epsilon value: {eps} - should be >= 0.0")
-        
+
         self.n_feature = n_feature
         self.n_head = n_head
         if n_kv_head is not None:
@@ -1019,20 +1018,20 @@ class AdamMini(Optimizer):
                 dic = {}
                 dic["name"] = name
                 dic["params"] = param
-                if ("norm" in name or "ln_f" in name):
+                if "norm" in name or "ln_f" in name:
                     dic["weight_decay"] = 0.0
                 else:
                     dic["weight_decay"] = weight_decay
 
-                if ("embed" in name or "wte" in name or "embd" in name):
+                if "embed" in name or "wte" in name or "embd" in name:
                     count_embd += 1
 
-                if ("lm_head.weight" in name or "output.weight" in name):
+                if "lm_head.weight" in name or "output.weight" in name:
                     count_output += 1
 
-                if ("q_proj.weight" in name or "wq.weight" in name):
+                if "q_proj.weight" in name or "wq.weight" in name:
                     count_q += 1
-                    
+
                     if self.n_feature is None:
                         raise ValueError(
                             "n_feature is None. Should be specified for transformer models and"
@@ -1048,7 +1047,7 @@ class AdamMini(Optimizer):
                     if (self.n_feature * self.n_feature % self.n_head) != 0:
                         raise ValueError("'n_feature * n_feature' is not a multiple of n_head")
 
-                if ("k_proj.weight" in name or "wk.weight" in name):
+                if "k_proj.weight" in name or "wk.weight" in name:
                     count_k += 1
 
                     if self.n_feature is None:
@@ -1098,13 +1097,9 @@ class AdamMini(Optimizer):
             )
 
         # embd_blocks, including embd and output layers. Use normal adamW updates for these blocks
-        self.embd_blocks = {
-            "embed", "embd", "wte", "lm_head.weight", "output.weight"
-        }
+        self.embd_blocks = {"embed", "embd", "wte", "lm_head.weight", "output.weight"}
         # Query and Keys, will  assign lrs by heads
-        self.qk_blocks = {
-            "k_proj.weight", "q_proj.weight", "wq.weight", "wk.weight"
-        }
+        self.qk_blocks = {"k_proj.weight", "q_proj.weight", "wq.weight", "wk.weight"}
 
         defaults = {"lr": lr, "betas": betas, "eps": eps, "weight_decay": weight_decay}
 
@@ -1124,146 +1119,145 @@ class AdamMini(Optimizer):
             loss = closure()
 
         for group in self.param_groups:
-                beta1 = group["beta1"]
-                beta2 = group["beta2"]
-                lr = group["lr"]
-                name = group["name"]
-                eps = group["eps"]
+            beta1 = group["beta1"]
+            beta2 = group["beta2"]
+            lr = group["lr"]
+            name = group["name"]
+            eps = group["eps"]
 
-                for p in group["params"]:
-                    state = self.state[p]
+            for p in group["params"]:
+                state = self.state[p]
 
-                    if any(block in name for block in self.embd_blocks):
-                        if p.grad is None:
-                            continue
-                        if len(state) == 0:
-                            state["m"] = torch.zeros_like(p.data).to(torch.float32)
-                            state["iteration"] = 0
-                            state["v"] = torch.zeros_like(p.data).to(torch.float32)
+                if any(block in name for block in self.embd_blocks):
+                    if p.grad is None:
+                        continue
+                    if len(state) == 0:
+                        state["m"] = torch.zeros_like(p.data).to(torch.float32)
+                        state["iteration"] = 0
+                        state["v"] = torch.zeros_like(p.data).to(torch.float32)
 
-                        grad = p.grad.data.to(torch.float32)
+                    grad = p.grad.data.to(torch.float32)
 
-                        state["v"].mul_(beta2).addcmul_(grad, grad.conj(), value=1 - beta2)
-                        state["iteration"] += 1
-                        if group["weight_decay"] != 0:
-                            p.data.mul_(1 - lr * group["weight_decay"])
+                    state["v"].mul_(beta2).addcmul_(grad, grad.conj(), value=1 - beta2)
+                    state["iteration"] += 1
+                    if group["weight_decay"] != 0:
+                        p.data.mul_(1 - lr * group["weight_decay"])
 
-                        state["m"].lerp_(grad, 1 - beta1)
+                    state["m"].lerp_(grad, 1 - beta1)
 
-                        bias_correction_1 = 1 - beta1 ** state["iteration"]
-                        bias_correction_2 = 1 - beta2 ** state["iteration"]
-                        bias_correction_2_sqrt = math.sqrt(bias_correction_2)
+                    bias_correction_1 = 1 - beta1 ** state["iteration"]
+                    bias_correction_2 = 1 - beta2 ** state["iteration"]
+                    bias_correction_2_sqrt = math.sqrt(bias_correction_2)
 
-                        h = (state["v"].sqrt() / bias_correction_2_sqrt).add_(eps)
-                        stepsize = lr / bias_correction_1
-                        p.addcdiv_(state["m"], h, value=-stepsize)
+                    h = (state["v"].sqrt() / bias_correction_2_sqrt).add_(eps)
+                    stepsize = lr / bias_correction_1
+                    p.addcdiv_(state["m"], h, value=-stepsize)
 
-                    elif any(block in name for block in self.qk_blocks):
+                elif any(block in name for block in self.qk_blocks):
+                    if p.grad is None:
+                        continue
 
-                        if p.grad is None:
-                            continue
+                    dim = group["parameter_per_head"]
 
-                        dim = group["parameter_per_head"]
+                    if len(state) == 0:
+                        state["m"] = torch.zeros_like(p.data).to(torch.float32)
 
-                        if (len(state) == 0):
-                            state["m"] = torch.zeros_like(p.data).to(torch.float32)
+                        state["m"] = state["m"].view(-1, dim)
+                        state["head"] = state["m"].shape[0]
+                        state["iteration"] = 0
 
-                            state["m"] = state["m"].view(-1, dim)
-                            state["head"] = state['m'].shape[0]
-                            state["iteration"] = 0
+                        state["vmean"] = torch.zeros_like(state["m"][0 : state["head"], 0:1]).to(self.device)
 
-                            state["vmean"] = torch.zeros_like(state["m"][0:state["head"], 0:1]).to(self.device)
+                    grad = p.grad.data.to(torch.float32)
+                    head = state["head"]
+                    grad = grad.view(head, dim)
 
-                        grad = p.grad.data.to(torch.float32)
-                        head = state['head']
-                        grad = grad.view(head, dim)
+                    tmp_lr = torch.mean(grad * grad, dim=1).unsqueeze(1).to(self.device)
 
-                        tmp_lr = torch.mean(grad * grad, dim=1).unsqueeze(1).to(self.device)
+                    state["vmean"].mul_(beta2).add_(tmp_lr, alpha=1 - beta2)
+                    v = state["vmean"]
 
-                        state["vmean"].mul_(beta2).add_(tmp_lr, alpha=1 - beta2)
-                        v = state["vmean"]
+                    state["iteration"] += 1
+                    if group["weight_decay"] != 0:
+                        p.data.mul_(1 - lr * group["weight_decay"])
 
-                        state["iteration"] += 1
-                        if group["weight_decay"] != 0:
-                            p.data.mul_(1 - lr * group["weight_decay"])
+                    state["m"].lerp_(grad, 1 - beta1)
 
-                        state["m"].lerp_(grad, 1 - beta1)
+                    bias_correction_1 = 1 - beta1 ** state["iteration"]
+                    bias_correction_2 = 1 - beta2 ** state["iteration"]
+                    bias_correction_2_sqrt = math.sqrt(bias_correction_2)
 
-                        bias_correction_1 = 1 - beta1 ** state["iteration"]
-                        bias_correction_2 = 1 - beta2 ** state["iteration"]
-                        bias_correction_2_sqrt = math.sqrt(bias_correction_2)
+                    h = (v.sqrt() / bias_correction_2_sqrt).add_(eps)
+                    stepsize = ((1 / bias_correction_1) / h).view(head, 1)
 
-                        h = (v.sqrt() / bias_correction_2_sqrt).add_(eps)
-                        stepsize = ((1 / bias_correction_1) / h).view(head, 1)
+                    update = state["m"] * (stepsize.to(state["m"].device))
 
-                        update = state["m"] * (stepsize.to(state['m'].device))
-
-                        if p.dim() > 1:
-                            d0, d1 = p.size()
-                            update = update.view(d0, d1)
-                        else:
-                            update = update.view(-1)
-
-                        update.mul_(lr)
-                        p.add_(-update)
-
+                    if p.dim() > 1:
+                        d0, d1 = p.size()
+                        update = update.view(d0, d1)
                     else:
-                        if (len(state) == 0):
-                            dimension = torch.tensor(p.data.numel()).to(self.device).to(torch.float32)
-                            reduced = False
-                            if (self.world_size > 1) and (self.model_sharding is True):
-                                tensor_list = [torch.zeros_like(dimension) for _ in range(self.world_size)]
+                        update = update.view(-1)
 
-                                dist.all_gather(tensor_list, dimension)
-                                s = 0
-                                dimension = 0
-                                for d in tensor_list:
-                                    if (d > 0):
-                                        s = s + 1
-                                    dimension = dimension + d
-                                if (s >= 2):
-                                    reduced = True
+                    update.mul_(lr)
+                    p.add_(-update)
 
-                            state["m"] = torch.zeros_like(p.data).to(torch.float32)
-                            state["iteration"] = 0
-                            state["reduced"] = reduced
+                else:
+                    if len(state) == 0:
+                        dimension = torch.tensor(p.data.numel()).to(self.device).to(torch.float32)
+                        reduced = False
+                        if (self.world_size > 1) and (self.model_sharding is True):
+                            tensor_list = [torch.zeros_like(dimension) for _ in range(self.world_size)]
 
-                            state["vmean"] = torch.zeros_like(torch.sum(p.data * p.data)).to(self.device)
-                            state["dimension"] = dimension.item()
-                        if p.grad is None:
-                            tmp_lr = torch.zeros_like(torch.sum(p.data * p.data)).to(self.device)
+                            dist.all_gather(tensor_list, dimension)
+                            s = 0
+                            dimension = 0
+                            for d in tensor_list:
+                                if d > 0:
+                                    s = s + 1
+                                dimension = dimension + d
+                            if s >= 2:
+                                reduced = True
+
+                        state["m"] = torch.zeros_like(p.data).to(torch.float32)
+                        state["iteration"] = 0
+                        state["reduced"] = reduced
+
+                        state["vmean"] = torch.zeros_like(torch.sum(p.data * p.data)).to(self.device)
+                        state["dimension"] = dimension.item()
+                    if p.grad is None:
+                        tmp_lr = torch.zeros_like(torch.sum(p.data * p.data)).to(self.device)
+                    else:
+                        grad = p.grad.data.to(torch.float32)
+                        tmp_lr = torch.sum(grad * grad).to(self.device)
+
+                    if state["reduced"]:
+                        if "device_mesh" in dir(tmp_lr):
+                            lr_local = tmp_lr.to_local()
+                            dist.all_reduce(lr_local, op=dist.ReduceOp.SUM)
+                            tmp_lr.redistribute(placements=[Replicate()])
                         else:
-                            grad = p.grad.data.to(torch.float32)
-                            tmp_lr = torch.sum(grad * grad).to(self.device)
+                            dist.all_reduce(tmp_lr, op=dist.ReduceOp.SUM)
 
-                        if (state["reduced"]):
-                            if "device_mesh" in dir(tmp_lr):
-                                lr_local = tmp_lr.to_local()
-                                dist.all_reduce(lr_local, op=dist.ReduceOp.SUM)
-                                tmp_lr.redistribute(placements=[Replicate()])
-                            else:
-                                dist.all_reduce(tmp_lr, op=dist.ReduceOp.SUM)
+                    if p.grad is None:
+                        continue
+                    tmp_lr = tmp_lr / (state["dimension"])
+                    tmp_lr = tmp_lr.to(grad.device)
 
-                        if (p.grad is None):
-                            continue
-                        tmp_lr = tmp_lr / (state["dimension"])
-                        tmp_lr = tmp_lr.to(grad.device)
+                    if group["weight_decay"] != 0:
+                        p.data.mul_(1 - lr * group["weight_decay"])
+                    state["iteration"] += 1
+                    state["m"].lerp_(grad, 1 - beta1)
 
-                        if group["weight_decay"] != 0:
-                            p.data.mul_(1 - lr * group["weight_decay"])
-                        state["iteration"] += 1
-                        state["m"].lerp_(grad, 1 - beta1)
+                    bias_correction_1 = 1 - beta1 ** state["iteration"]
+                    bias_correction_2 = 1 - beta2 ** state["iteration"]
+                    bias_correction_2_sqrt = math.sqrt(bias_correction_2)
+                    state["vmean"] = (1 - beta2) * tmp_lr + beta2 * state["vmean"]
+                    h = (state["vmean"].sqrt() / bias_correction_2_sqrt).add_(eps)
 
-                        bias_correction_1 = 1 - beta1 ** state["iteration"]
-                        bias_correction_2 = 1 - beta2 ** state["iteration"]
-                        bias_correction_2_sqrt = math.sqrt(bias_correction_2)
-                        state["vmean"] = (1 - beta2) * tmp_lr + beta2 * state["vmean"]
-                        h = (state["vmean"].sqrt() / bias_correction_2_sqrt).add_(eps)
-
-                        stepsize = (1 / bias_correction_1) / h
-                        update = state["m"] * (stepsize.to(state['m'].device))
-                        update.mul_(lr)
-                        p.add_(-update)
+                    stepsize = (1 / bias_correction_1) / h
+                    update = state["m"] * (stepsize.to(state["m"].device))
+                    update.mul_(lr)
+                    p.add_(-update)
 
         return loss
 
