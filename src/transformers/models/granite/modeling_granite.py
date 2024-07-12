cfa8210c5158da23389defa3f8c813795d33b922
Mayank Mishra <mayank31398@gmail.com>
2024-07-12 17:29:43 -0400

    drop legacy cache

--- src/transformers/models/granite/modeling_granite.py
+++ src/transformers/models/granite/modeling_granite.py
@@ -804,15 +804,6 @@ class GraniteModel(GranitePreTrainedModel):
         if self.m_emb is not None:
             inputs_embeds = inputs_embeds * self.m_emb
 
-        return_legacy_cache = False
-        if use_cache and not isinstance(past_key_values, Cache):  # kept for BC (non `Cache` `past_key_values` inputs)
-            return_legacy_cache = True
-            past_key_values = DynamicCache.from_legacy_cache(past_key_values)
-            logger.warning_once(
-                "We detected that you are passing `past_key_values` as a tuple and this is deprecated and will be removed in v4.43. "
-                "Please use an appropriate `Cache` class (https://huggingface.co/docs/transformers/v4.41.3/en/internal/generation_utils#transformers.Cache)"
-            )
-
         if cache_position is None:
             past_seen_tokens = past_key_values.get_seq_length() if past_key_values is not None else 0
             cache_position = torch.arange(
@@ -874,8 +865,6 @@ class GraniteModel(GranitePreTrainedModel):
             all_hidden_states += (hidden_states,)
 
         next_cache = next_decoder_cache if use_cache else None
-        if return_legacy_cache:
-            next_cache = next_cache.to_legacy_cache()
 
         if not return_dict:
             return tuple(v for v in [hidden_states, next_cache, all_hidden_states, all_self_attns] if v is not None)
