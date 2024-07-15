f772da865806446810c1545d35d2e07ece9d071c
ydshieh <ydshieh@users.noreply.github.com>
2024-07-15 12:37:48 +0200

    remove delete

--- tests/utils/test_image_processing_utils.py
+++ tests/utils/test_image_processing_utils.py
@@ -71,22 +71,22 @@ class ImageProcessorPushToHubTester(unittest.TestCase):
         cls._token = TOKEN
         HfFolder.save_token(TOKEN)
 
-    @classmethod
-    def tearDownClass(cls):
-        try:
-            delete_repo(token=cls._token, repo_id="test-image-processor")
-        except HTTPError:
-            pass
-
-        try:
-            delete_repo(token=cls._token, repo_id="valid_org/test-image-processor-org")
-        except HTTPError:
-            pass
-
-        try:
-            delete_repo(token=cls._token, repo_id="test-dynamic-image-processor")
-        except HTTPError:
-            pass
+    # @classmethod
+    # def tearDownClass(cls):
+    #     try:
+    #         delete_repo(token=cls._token, repo_id="test-image-processor")
+    #     except HTTPError:
+    #         pass
+    #
+    #     try:
+    #         delete_repo(token=cls._token, repo_id="valid_org/test-image-processor-org")
+    #     except HTTPError:
+    #         pass
+    #
+    #     try:
+    #         delete_repo(token=cls._token, repo_id="test-dynamic-image-processor")
+    #     except HTTPError:
+    #         pass
 
     def test_push_to_hub(self):
         image_processor = ViTImageProcessor.from_pretrained(SAMPLE_IMAGE_PROCESSING_CONFIG_DIR)
