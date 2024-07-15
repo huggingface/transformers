09866057710b7ed57dd1452d017b00e53c2a13e2
Moshe Raboh Moshiko.Raboh@ibm.com <moshiko@cccxl012.pok.ibm.com>
2024-07-15 16:53:50 -0400

    adding option for scaled_dot_product with and witout position bias

--- src/transformers/models/t5/modeling_t5.py
+++ src/transformers/models/t5/modeling_t5.py
@@ -805,14 +805,14 @@ class T5Attention(nn.Module):
 
             attn_output = self.unshape(torch.matmul(attn_weights, value_states), batch_size=batch_size, inner_dim=self.inner_dim)  # (batch_size, seq_length, dim)
         
-        elif self.memory_efficient_attention in ["torch_scaled_dot_product_attention", "torch_scaled_dot_product_attention_force_flash"]:
+        elif self.memory_efficient_attention in ["torch_scaled_dot_product_attention", "torch_scaled_dot_product_attention_force_flash", "torch_scaled_dot_product_attention_with_position_bias"]:
             scale = 1 / math.sqrt(K) if self.scale == None else self.scale
             is_causal = self.is_decoder and not self.cross_attention
-            if self.memory_efficient_attention == "torch_scaled_dot_product_attention_force_flash":
+            if self.memory_efficient_attention in ["torch_scaled_dot_product_attention_force_flash", "torch_scaled_dot_product_attention"]:
                 with torch.backends.cuda.sdp_kernel(
                     enable_flash=True, 
-                    enable_math=False, 
-                    enable_mem_efficient=False
+                    enable_math=self.memory_efficient_attention != "torch_scaled_dot_product_attention_force_flash",
+                    enable_mem_efficient=self.memory_efficient_attention != "torch_scaled_dot_product_attention_force_flash",
                 ):
                     attn_output = F.scaled_dot_product_attention(query_states,key_states, value_states, attn_mask=None, dropout_p=self.dropout, is_causal=is_causal, scale=scale)
             else:
@@ -1278,7 +1278,7 @@ class T5Stack(T5PreTrainedModel):
         position_ids_dict='default', #None here means the user wants NO pos embedding of any kind.  'default' has the default T5 behavior
     ):
         if position_ids_dict == 'default':
-            position_ids_dict = {'default': (None, ["default"])} # TODO: ask why it was build as a dict of tuple of list (most of it is not used)
+            position_ids_dict = {'default': (None, ["default"])} 
         # Model parallel
         if self.model_parallel:
             torch.cuda.set_device(self.first_device)
