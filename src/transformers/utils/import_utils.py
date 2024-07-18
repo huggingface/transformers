4529d8ef98d238406038a7f0faf27c99c7a3c98c
Alexandre TL <alextorresleguet@icloud.com>
2024-07-18 13:56:17 +0200

    Update import_utils.py

--- src/transformers/utils/import_utils.py
+++ src/transformers/utils/import_utils.py
@@ -396,6 +396,8 @@ def is_causal_conv1d_available():
 
 def is_mambapy_available():
     if is_torch_available():
+        import torch
+        
         return _is_package_available("mambapy")
     return False
 
