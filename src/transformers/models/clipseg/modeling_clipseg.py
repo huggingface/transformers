fde2c794bbfc083f6518a350ad3292600257b332
tahaakbarialvanagh <tahaakbarialvanagh@gmail.com>
2024-07-24 16:18:16 +0330

    Bug fix

--- src/transformers/models/clipseg/modeling_clipseg.py
+++ src/transformers/models/clipseg/modeling_clipseg.py
@@ -226,7 +226,7 @@ class CLIPSegTextEmbeddings(nn.Module):
         if seq_length > self.max_position_embeddings:
             raise ValueError(
                 f"Sequence length must be less than max_position_embeddings (got `sequence length`: "
-                f"{seq_length} and max_position_embeddings: {max_position_embedding}"
+                f"{seq_length} and max_position_embeddings: {self.max_position_embedding}"
             )
 
         if position_ids is None:
