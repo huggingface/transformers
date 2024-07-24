3df6afda4e4da93ed7f10ffc7b02ec6a6de04079
ydshieh <ydshieh@users.noreply.github.com>
2024-07-24 19:06:28 +0200

    fix 15

--- tests/utils/test_feature_extraction_utils.py
+++ tests/utils/test_feature_extraction_utils.py
@@ -68,23 +68,6 @@ class FeatureExtractorPushToHubTester(unittest.TestCase):
         except:  # noqa E722
             pass
 
-    @classmethod
-    def tearDownClass(cls):
-        try:
-            delete_repo(token=cls._token, repo_id="test-feature-extractor")
-        except HTTPError:
-            pass
-
-        try:
-            delete_repo(token=cls._token, repo_id="valid_org/test-feature-extractor-org")
-        except HTTPError:
-            pass
-
-        try:
-            delete_repo(token=cls._token, repo_id="test-dynamic-feature-extractor")
-        except HTTPError:
-            pass
-
     def test_push_to_hub(self):
         with tempfile.TemporaryDirectory() as tmp_dir:
             try:
@@ -102,12 +85,10 @@ class FeatureExtractorPushToHubTester(unittest.TestCase):
     def test_push_to_hub_via_save_pretrained(self):
         with tempfile.TemporaryDirectory() as tmp_dir:
             try:
-                tmp_repo = f"{USER}/test-tokenizer-{Path(tmp_dir).name}"
+                tmp_repo = f"{USER}/test-feature-extractor-{Path(tmp_dir).name}"
                 feature_extractor = Wav2Vec2FeatureExtractor.from_pretrained(SAMPLE_FEATURE_EXTRACTION_CONFIG_DIR)
                 # Push to hub via save_pretrained
-                feature_extractor.save_pretrained(
-                    tmp_dir, repo_id=tmp_repo, push_to_hub=True, token=self._token
-                )
+                feature_extractor.save_pretrained(tmp_dir, repo_id=tmp_repo, push_to_hub=True, token=self._token)
 
                 new_feature_extractor = Wav2Vec2FeatureExtractor.from_pretrained(tmp_repo)
                 for k, v in feature_extractor.__dict__.items():
@@ -118,7 +99,7 @@ class FeatureExtractorPushToHubTester(unittest.TestCase):
     def test_push_to_hub_in_organization(self):
         with tempfile.TemporaryDirectory() as tmp_dir:
             try:
-                tmp_repo = f"valid_org/test-tokenizer-{Path(tmp_dir).name}"
+                tmp_repo = f"valid_org/test-feature-extractor-{Path(tmp_dir).name}"
                 feature_extractor = Wav2Vec2FeatureExtractor.from_pretrained(SAMPLE_FEATURE_EXTRACTION_CONFIG_DIR)
                 feature_extractor.push_to_hub(tmp_repo, token=self._token)
 
@@ -131,12 +112,10 @@ class FeatureExtractorPushToHubTester(unittest.TestCase):
     def test_push_to_hub_in_organization_via_save_pretrained(self):
         with tempfile.TemporaryDirectory() as tmp_dir:
             try:
-                tmp_repo = f"valid_org/test-tokenizer-{Path(tmp_dir).name}"
+                tmp_repo = f"valid_org/test-feature-extractor-{Path(tmp_dir).name}"
                 feature_extractor = Wav2Vec2FeatureExtractor.from_pretrained(SAMPLE_FEATURE_EXTRACTION_CONFIG_DIR)
                 # Push to hub via save_pretrained
-                feature_extractor.save_pretrained(
-                    tmp_dir, repo_id=tmp_repo, push_to_hub=True, token=self._token
-                )
+                feature_extractor.save_pretrained(tmp_dir, repo_id=tmp_repo, push_to_hub=True, token=self._token)
 
                 new_feature_extractor = Wav2Vec2FeatureExtractor.from_pretrained(tmp_repo)
                 for k, v in feature_extractor.__dict__.items():
@@ -145,19 +124,22 @@ class FeatureExtractorPushToHubTester(unittest.TestCase):
                 self._try_delete_repo(repo_id=tmp_repo, token=self._token)
 
     def test_push_to_hub_dynamic_feature_extractor(self):
-        CustomFeatureExtractor.register_for_auto_class()
-        feature_extractor = CustomFeatureExtractor.from_pretrained(SAMPLE_FEATURE_EXTRACTION_CONFIG_DIR)
+        with tempfile.TemporaryDirectory() as tmp_dir:
+            try:
+                tmp_repo = f"{USER}/test-dynamic-feature-extractor-{Path(tmp_dir).name}"
+                CustomFeatureExtractor.register_for_auto_class()
+                feature_extractor = CustomFeatureExtractor.from_pretrained(SAMPLE_FEATURE_EXTRACTION_CONFIG_DIR)
 
-        feature_extractor.push_to_hub("test-dynamic-feature-extractor", token=self._token)
+                feature_extractor.push_to_hub(tmp_repo, token=self._token)
 
-        # This has added the proper auto_map field to the config
-        self.assertDictEqual(
-            feature_extractor.auto_map,
-            {"AutoFeatureExtractor": "custom_feature_extraction.CustomFeatureExtractor"},
-        )
+                # This has added the proper auto_map field to the config
+                self.assertDictEqual(
+                    feature_extractor.auto_map,
+                    {"AutoFeatureExtractor": "custom_feature_extraction.CustomFeatureExtractor"},
+                )
 
-        new_feature_extractor = AutoFeatureExtractor.from_pretrained(
-            f"{USER}/test-dynamic-feature-extractor", trust_remote_code=True
-        )
-        # Can't make an isinstance check because the new_feature_extractor is from the CustomFeatureExtractor class of a dynamic module
-        self.assertEqual(new_feature_extractor.__class__.__name__, "CustomFeatureExtractor")
+                new_feature_extractor = AutoFeatureExtractor.from_pretrained(tmp_repo, trust_remote_code=True)
+                # Can't make an isinstance check because the new_feature_extractor is from the CustomFeatureExtractor class of a dynamic module
+                self.assertEqual(new_feature_extractor.__class__.__name__, "CustomFeatureExtractor")
+            finally:
+                self._try_delete_repo(repo_id=tmp_repo, token=self._token)
