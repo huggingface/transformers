52440ad9b16356eeb7abb89e3aaf6a7dc9b07e4e
Mayank Mishra <mayank31398@gmail.com>
2024-07-23 21:14:31 -0400

    drop unused imports

--- src/transformers/models/granite/modeling_granite.py
+++ src/transformers/models/granite/modeling_granite.py
@@ -17,7 +17,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import math
 from typing import List, Optional, Tuple, Union
 
 import torch
