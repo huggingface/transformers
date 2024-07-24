8d2534c4d0ab94a97a72d2ce6bb9ccd201abadb3
Arthur <48595927+ArthurZucker@users.noreply.github.com>
2024-07-24 16:06:39 +0200

    let's not warn when someone is running a forward  (#32176)
    * let's not warn when someone is running a foward without cache + self.training
    
    * more models
    
    * fixup

--- src/transformers/models/olmo/modeling_olmo.py
+++ src/transformers/models/olmo/modeling_olmo.py
@@ -811,7 +811,9 @@ class OlmoModel(OlmoPreTrainedModel):
             inputs_embeds = self.embed_tokens(input_ids)
 
         return_legacy_cache = False
-        if use_cache and not isinstance(past_key_values, Cache):  # kept for BC (non `Cache` `past_key_values` inputs)
+        if (
+            use_cache and not isinstance(past_key_values, Cache) and not self.training
+        ):  # kept for BC (non `Cache` `past_key_values` inputs)
             return_legacy_cache = True
             past_key_values = DynamicCache.from_legacy_cache(past_key_values)
             logger.warning_once(
