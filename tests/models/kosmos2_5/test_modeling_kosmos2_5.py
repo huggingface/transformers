ec82032bd9f222cc752e66078c7a0ad5c917c79a
ydshieh <ydshieh@users.noreply.github.com>
2024-07-25 19:36:30 +0200

    [ydshieh] update FA2 ocr expected outputs

--- tests/models/kosmos2_5/test_modeling_kosmos2_5.py
+++ tests/models/kosmos2_5/test_modeling_kosmos2_5.py
@@ -785,6 +785,6 @@ class Kosmos2_5ModelIntegrationTest(unittest.TestCase):
             prompt, image, model, processor
         )
         EXPECTED_TEXT = [
-            "<md>- **1 \\[REG\\] BLACK SAKURA** 45,455\n- **1 COOKIE DOH SAUCES** 0\n- **1 NATA DE COCO** 0\n- **Sub Total** 45,455\n- **PB1 (10%)** 4,545\n- **Rounding** 0\n- **Total** **50,000**\n"
+            "<md>- **1 \\[REG\\] BLACK SAKURA** 45,455\n- **1 COOKIE DOH SAUCES** 0\n- **1 NATA DE COCO** 0\n\n<table>\n<thead>\n<tr>\n<th>\nSub Total\n</th>\n<th>\n45,455\n</th>\n</tr>\n</thead>\n<tbody>\n<tr>\n<td>\nPB1 (10%)\n</td>\n<td>\n4,545\n</td>\n</tr>\n<tr>\n<td>\nRounding\n</td>\n<td>\n0\n</td>\n</tr>\n<tr>\n<td>\n<strong>\nTotal\n</strong>\n</td>\n<td>\n<strong>\n50,000\n</strong>\n</td>\n</tr>\n</tbody>\n</table>\n\nCard Payment 50,000"
         ]
         self.assertListEqual(generated_text, EXPECTED_TEXT)
