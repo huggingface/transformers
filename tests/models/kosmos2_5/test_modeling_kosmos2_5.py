b7be077f043aeb5cd32d6e74a0e3410c41dc4af5
ydshieh <ydshieh@users.noreply.github.com>
2024-07-25 20:17:48 +0200

    [ydshieh] fix

--- tests/models/kosmos2_5/test_modeling_kosmos2_5.py
+++ tests/models/kosmos2_5/test_modeling_kosmos2_5.py
@@ -808,4 +808,4 @@ class Kosmos2_5ModelIntegrationTest(unittest.TestCase):
             "<md>- **1 \\[REG\\] BLACK SAKURA** 45,455\n- **1 COOKIE DOH SAUCES** 0\n- **1 NATA DE COCO** 0\n\n<table>\n<thead>\n<tr>\n<th>\nSub Total\n</th>\n<th>\n45,455\n</th>\n</tr>\n</thead>\n<tbody>\n<tr>\n<td>\nPB1 (10%)\n</td>\n<td>\n4,545\n</td>\n</tr>\n<tr>\n<td>\nRounding\n</td>\n<td>\n0\n</td>\n</tr>\n<tr>\n<td>\n<strong>\nTotal\n</strong>\n</td>\n<td>\n<strong>\n50,000\n</strong>\n</td>\n</tr>\n</tbody>\n</table>\n\nCard Payment 50,000",
             "<md>- **1 \\[REG\\] BLACK SAKURA** 45,455\n- **1 COOKIE DOH SAUCES** 0\n- **1 NATA DE COCO** 0\n- **Sub Total** 45,455\n- **PB1 (10%)** 4,545\n- **Rounding** 0\n- **Total** **50,000**\n",
         ]
-        self.assertIn(generated_text, EXPECTED_TEXT)
+        self.assertIn(generated_text[0], EXPECTED_TEXT)
