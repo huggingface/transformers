1a86c116737f9c778dba77c4a6136c40170f7786
faychu <faychu@qq.com>
2024-07-26 11:14:05 +0800

    add to PRIVATE_MODELS in check_repo.py; add to MODEL_NAMES_TO_IGNORE in check_table.py

--- utils/check_table.py
+++ utils/check_table.py
@@ -173,7 +173,7 @@ MODEL_NAMES_WITH_SAME_CONFIG = {
     "XLS-R": "Wav2Vec2",
     "XLSR-Wav2Vec2": "Wav2Vec2",
 }
-MODEL_NAMES_TO_IGNORE = ["CLIPVisionModel", "SiglipVisionModel", "ChineseCLIPVisionModel"]
+MODEL_NAMES_TO_IGNORE = ["CLIPVisionModel", "SiglipVisionModel", "ChineseCLIPVisionModel", "Qwen2AudioEncoder"]
 
 
 def get_model_table_from_auto_modules() -> str:
