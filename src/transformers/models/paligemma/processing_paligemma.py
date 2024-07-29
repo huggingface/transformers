a07bbc8f243a8df90e30373e27a05d2f83c51348
yonigozlan <yoni.gozlan@huggingface.co>
2024-07-29 14:42:28 +0000

    Fix Paligemma prompts with image token

--- src/transformers/models/paligemma/processing_paligemma.py
+++ src/transformers/models/paligemma/processing_paligemma.py
@@ -17,6 +17,7 @@ Processor class for PaliGemma.
 """
 
 import logging
+import warnings
 from typing import List, Optional, Union
 
 from ...feature_extraction_utils import BatchFeature
@@ -70,6 +71,11 @@ def build_string_from_input(prompt, bos_token, image_seq_len, image_token):
         image_seq_len (`int`): The length of the image sequence.
         image_token (`str`): The image token.
     """
+    if image_token in prompt:
+        warnings.warn(
+            f"The image token {image_token} is already present in the prompt. This may lead to unexpected behavior."
+        )
+        prompt = prompt.replace(image_token, "")
     return f"{image_token * image_seq_len}{bos_token}{prompt}\n"
 
 
