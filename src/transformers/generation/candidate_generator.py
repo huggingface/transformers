029fc827dcc89589e28ce74cbbdbddc00ed2dfa6
Raushan Turganbay <raushan@huggingface.co>
2024-07-29 10:58:59 +0500

    🚨 Bloom support for cache class (#31445)
    * bloom dynamic cache
    
    * bloom follows standard cache format
    
    * no skips for bloom anymore
    
    * use cache position when possible
    
    * clean up
    
    * codestyle
    
    * Update src/transformers/models/bloom/modeling_bloom.py
    
    Co-authored-by: amyeroberts <22614925+amyeroberts@users.noreply.github.com>
    
    * Update src/transformers/models/bloom/modeling_bloom.py
    
    Co-authored-by: amyeroberts <22614925+amyeroberts@users.noreply.github.com>
    
    * Update src/transformers/models/bloom/modeling_bloom.py
    
    Co-authored-by: amyeroberts <22614925+amyeroberts@users.noreply.github.com>
    
    * pr comments
    
    * isinstance fix
    
    * address comments
    
    * make musicgen test happy
    
    * [run-slow] bloom
    
    ---------
    
    Co-authored-by: amyeroberts <22614925+amyeroberts@users.noreply.github.com>

--- src/transformers/generation/candidate_generator.py
+++ src/transformers/generation/candidate_generator.py
@@ -378,19 +378,7 @@ def _crop_past_key_values(model, past_key_values, max_length):
                 )
             )
         past_key_values = tuple(new_past)
-    # bloom is special
-    elif "bloom" in model.__class__.__name__.lower() or (
-        model.config.architectures is not None and "bloom" in model.config.architectures[0].lower()
-    ):
-        for idx in range(len(past_key_values)):
-            new_past.append(
-                (
-                    past_key_values[idx][0][:, :, :max_length],
-                    past_key_values[idx][1][:, :max_length, :],
-                )
-            )
-        past_key_values = tuple(new_past)
-    # gptbigcode is too
+    # gptbigcode is special and stores kv in shape (batch_size, seq_len, dim), if it's a multi_query model
     elif "gptbigcode" in model.__class__.__name__.lower() or (
         model.config.architectures is not None and "gptbigcode" in model.config.architectures[0].lower()
     ):
@@ -402,7 +390,6 @@ def _crop_past_key_values(model, past_key_values, max_length):
                 past_key_values[idx] = past_key_values[idx][:, :, :max_length, :]
     elif isinstance(past_key_values, DynamicCache):
         past_key_values.crop(max_length)
-
     elif past_key_values is not None:
         for idx in range(len(past_key_values)):
             new_past.append(
