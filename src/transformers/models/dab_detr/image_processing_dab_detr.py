8ae2e1b68e6f03da6a2935c1936ebb81805358f6
David <37246112+conditionedstimulus@users.noreply.github.com>
2024-07-29 19:14:38 +0200

    Update src/transformers/models/dab_detr/image_processing_dab_detr.py
    Co-authored-by: Pavel Iakubovskii <qubvel@gmail.com>

--- src/transformers/models/dab_detr/image_processing_dab_detr.py
+++ src/transformers/models/dab_detr/image_processing_dab_detr.py
@@ -742,10 +742,9 @@ class DABDETRImageProcessor(BaseImageProcessor):
         rescale_factor (`int` or `float`, *optional*, defaults to `1/255`):
             Scale factor to use if rescaling the image. Can be overridden by the `rescale_factor` parameter in the
             `preprocess` method.
+        do_normalize (`bool`, *optional*, defaults to `True`):
             Controls whether to normalize the image. Can be overridden by the `do_normalize` parameter in the
             `preprocess` method.
-        do_normalize (`bool`, *optional*, defaults to `True`):
-            Whether to normalize the image.
         image_mean (`float` or `List[float]`, *optional*, defaults to `IMAGENET_DEFAULT_MEAN`):
             Mean values to use when normalizing the image. Can be a single value or a list of values, one for each
             channel. Can be overridden by the `image_mean` parameter in the `preprocess` method.
