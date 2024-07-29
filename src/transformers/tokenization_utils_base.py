84564ea9b8abe05f39de079179844614303e1804
Hanna Yukhymenko <49597980+ayukh@users.noreply.github.com>
2024-07-29 16:15:26 +0200

    Fix error message for non-string tokenizer device

--- src/transformers/tokenization_utils_base.py
+++ src/transformers/tokenization_utils_base.py
@@ -2664,9 +2664,7 @@ class PreTrainedTokenizerBase(SpecialTokensMixin, PushToHubMixin):
             tokenizer_config.pop("special_tokens_map_file", None)
             tokenizer_config.pop("tokenizer_file", None)
         if "device_map" in tokenizer_config and not isinstance(tokenizer_config["device_map"], str):
-            raise ValueError(
-                    "`device_map` is not in str type. Please set the value in str format ('cpu'/'cuda') or do not pass it into tokenizer (if you are using TPU/XLA)."
-                )
+            raise ValueError("'device_map' is not in str type. Please set the value in str format (e.g. 'auto') or do not pass it into tokenizer (for TPU).")
 
         with open(tokenizer_config_file, "w", encoding="utf-8") as f:
             out_str = json.dumps(tokenizer_config, indent=2, sort_keys=True, ensure_ascii=False) + "\n"
