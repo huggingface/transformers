c027a98a3ccef6a2ef954aaf13970591b1899fdd
kirp@umich.edu <tic-top>
2024-07-30 10:05:57 +0000

    fix error

--- src/transformers/models/kosmos2_5/image_processing_kosmos2_5.py
+++ src/transformers/models/kosmos2_5/image_processing_kosmos2_5.py
@@ -308,7 +308,7 @@ class Kosmos2_5ImageProcessor(BaseImageProcessor):
             images = [self.normalize(image=image, input_data_format=input_data_format) for image in images]
 
         # convert to torch tensor and permute
-        images, width, height, rows, cols = [
+        images, width, height, rows, cols = zip(*[
             self.extract_flattened_patches(
                 image=image,
                 max_patches=max_patches,
@@ -316,7 +316,7 @@ class Kosmos2_5ImageProcessor(BaseImageProcessor):
                 input_data_format=input_data_format,
             )
             for image in images
-        ]
+        ])
 
         # create attention mask in numpy
         attention_masks = [(image.sum(axis=-1) != 0).astype(np.float32) for image in images]
