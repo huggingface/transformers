0db3048bf441055435479a0e39f4e62ab8ee7a5d
Sai-Suraj-27 <sai.suraj.27.729@gmail.com>
2024-08-01 00:16:42 +0530

    Fixed staticmethods with self as first argument.

--- src/transformers/models/deberta/modeling_deberta.py
+++ src/transformers/models/deberta/modeling_deberta.py
@@ -103,19 +103,21 @@ class XSoftmax(torch.autograd.Function):
     >>> y = XSoftmax.apply(x, mask, dim)
     ```"""
 
-    def forward(self, input, mask, dim):
-        self.dim = dim
+    @staticmethod
+    def forward(ctx, input, mask, dim):
+        ctx.dim = dim
         rmask = ~(mask.to(torch.bool))
 
         output = input.masked_fill(rmask, torch.tensor(torch.finfo(input.dtype).min))
         output = torch.softmax(output, self.dim)
         output.masked_fill_(rmask, 0)
-        self.save_for_backward(output)
+        ctx.save_for_backward(output)
         return output
 
-    def backward(self, grad_output):
-        (output,) = self.saved_tensors
-        inputGrad = softmax_backward_data(self, grad_output, output, self.dim, output)
+    @staticmethod
+    def backward(ctx, grad_output):
+        (output,) = ctx.saved_tensors
+        inputGrad = softmax_backward_data(ctx, grad_output, output, ctx.dim, output)
         return inputGrad, None, None
 
     @staticmethod
