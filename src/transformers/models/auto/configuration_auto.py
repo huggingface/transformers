61a9414360218327a1b8ad7944db4023a8178aeb
Yoach Lacombe <yoach.lacombe@gmail.com>
2024-08-01 11:13:20 +0200

    fix module path

--- src/transformers/models/auto/configuration_auto.py
+++ src/transformers/models/auto/configuration_auto.py
@@ -643,6 +643,7 @@ SPECIAL_MODEL_TYPE_TO_MODULE_NAME = OrderedDict(
         ("siglip_vision_model", "siglip"),
         ("chinese_clip_vision_model", "chinese_clip"),
         ("rt_detr_resnet", "rt_detr"),
+        ("fastspeech2_conformer_with_hifigan", "fastspeech2_conformer"),
     ]
 )
 
