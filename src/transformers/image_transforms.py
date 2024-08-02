1b439e20be2c07527ea89a54e4e0a749facad6be
sangbumchoi <danielsejong55@gmail.com>
2024-08-02 01:21:48 +0000

    make box_to_center more applicable

--- src/transformers/image_transforms.py
+++ src/transformers/image_transforms.py
@@ -539,7 +539,10 @@ def _center_to_corners_format_tf(bboxes_center: "tf.Tensor") -> "tf.Tensor":
     return bboxes_corners
 
 
-def box_to_center_and_scale(box: Union[Tuple, List], image_width: int, image_height: int):
+# inspired by https://github.com/ViTAE-Transformer/ViTPose/blob/d5216452796c90c6bc29f5c5ec0bdba94366768a/mmpose/datasets/datasets/base/kpt_2d_sview_rgb_img_top_down_dataset.py#L132
+def box_to_center_and_scale(
+    box: Union[Tuple, List], image_width: int, image_height: int, pixel_std: float = 200.0, padding: float = 1.25
+):
     """
     Encodes a bounding box in COCO format into (center, scale).
 
@@ -550,6 +553,10 @@ def box_to_center_and_scale(box: Union[Tuple, List], image_width: int, image_hei
             Image width.
         image_height (`int`):
             Image height.
+        pixel_std (`float`):
+            Width and height scale factor.
+        padding (`float`):
+            Bounding box padding factor.
 
     Returns:
         tuple: A tuple containing center and scale.
@@ -568,8 +575,8 @@ def box_to_center_and_scale(box: Union[Tuple, List], image_width: int, image_hei
         width = height * aspect_ratio
 
     # pixel std is 200.0
-    scale = np.array([width / 200.0, height / 200.0], dtype=np.float32)
-    scale = scale * 1.25
+    scale = np.array([width / pixel_std, height / pixel_std], dtype=np.float32)
+    scale = scale * padding
 
     return center, scale
 
