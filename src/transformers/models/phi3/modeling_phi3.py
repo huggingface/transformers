48fc3807e025777e778f051277201f1cd07e9ca4
Xueshen Liu <liuxs@umich.edu>
2024-08-03 14:07:55 -0400

    MixtralFlashAttention2: put "plus 1" inside parentheses when calculating rotary_seq_len, allowing None position_ids input. (#31500)
    * Mixtral: remove unnecessary plus 1 when calculating rotary_seq_len, allowing position_ids=None (no auto position_ids generation could be unsafe)
    
    * fix typo [:-1] to [:, -1]
    
    * to meet formatting requirement
    
    * to meet formatting requirement
    
    * remove white space
    
    * MixtralFlashAttention2: put "+ 1" inside parentheses when calculating rotary_seq_len, allowing None position_ids input. Fix format/style issue.
    
    * propagate to startcoder2, phi3, mixtral and qwen2
    
    * update qwen2_moe

--- src/transformers/models/phi3/modeling_phi3.py
+++ src/transformers/models/phi3/modeling_phi3.py
@@ -536,8 +536,11 @@ class Phi3FlashAttention2(Phi3Attention):
             kv_seq_len += past_key_value.get_usable_length(kv_seq_len, self.layer_idx)
 
         # Because the input can be padded, the absolute sequence length depends on the max position id.
-        rotary_seq_len = max(kv_seq_len, position_ids[:, -1].max().item()) + 1
-        cos, sin = self.rotary_emb(value_states, position_ids, seq_len=rotary_seq_len)
+        rotary_seq_len = (
+            max(kv_seq_len, position_ids[:, -1].max().item() + 1) if position_ids is not None else kv_seq_len
+        )
+
+        cos, sin = self.rotary_emb(value_states, seq_len=rotary_seq_len)
 
         query_states, key_states = apply_rotary_pos_emb(query_states, key_states, cos, sin, position_ids)
 
