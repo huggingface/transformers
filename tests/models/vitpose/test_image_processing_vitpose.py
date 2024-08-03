741f07bc0f55427a1ac8cb5b95e4c74d2597a90b
sangbumchoi <danielsejong55@gmail.com>
2024-08-03 07:49:46 +0000

    import torch should be above ImageProcessor

--- tests/models/vitpose/test_image_processing_vitpose.py
+++ tests/models/vitpose/test_image_processing_vitpose.py
@@ -24,13 +24,14 @@ from transformers.utils import is_torch_available, is_vision_available
 from ...test_image_processing_common import ImageProcessingTestMixin, prepare_image_inputs
 
 
+if is_torch_available():
+    import torch
+    
+
 if is_vision_available():
     from PIL import Image
 
     from transformers import ViTPoseImageProcessor
-
-if is_torch_available():
-    import torch
 
 
 class ViTPoseImageProcessingTester(unittest.TestCase):
