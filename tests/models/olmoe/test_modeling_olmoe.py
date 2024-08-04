474f8e82b1eb78fd405853187fc6124e7f779cd4
Muennighoff <n.muennighoff@gmail.com>
2024-08-04 04:34:09 +0000

    Format

--- tests/models/olmoe/test_modeling_olmoe.py
+++ tests/models/olmoe/test_modeling_olmoe.py
@@ -75,7 +75,7 @@ class OlmoeModelTester:
         norm_topk_prob=False,
         output_router_logits=False,
         router_aux_loss_coef=0.001,
-        intermediate_size=12,                        
+        intermediate_size=12,
     ):
         self.parent = parent
         self.batch_size = batch_size
