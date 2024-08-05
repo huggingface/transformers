774f2c80215a0b9a9c6b2f305d607707ef2abdc7
coco58323 <a923602148@163.com>
2024-08-05 22:45:04 +0800

    Fix: tensor tolist()

--- src/transformers/models/qwen2_moe/modeling_qwen2_moe.py
+++ src/transformers/models/qwen2_moe/modeling_qwen2_moe.py
@@ -719,7 +719,7 @@ class Qwen2MoeSparseMoeBlock(nn.Module):
         expert_mask = torch.nn.functional.one_hot(selected_experts, num_classes=self.num_experts).permute(2, 1, 0)
 
         # Loop over all available experts in the model and perform the computation on each expert
-        expert_hitted = torch.where(expert_mask.sum(dim=-1).sum(dim=-1) > 0)[0]
+        expert_hitted = torch.where(expert_mask.sum(dim=-1).sum(dim=-1) > 0)[0].tolist()
         for expert_idx in expert_hitted:
             expert_layer = self.experts[expert_idx]
             idx, top_x = torch.where(expert_mask[expert_idx])
