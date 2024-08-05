6b568c8a925dd7e35fc7a8bd637b46f83c5b6bbd
Ita Zaporozhets <ita.zaporozhets@huggingface.co>
2024-08-05 11:34:03 +0200

    trying isort to fix ruff error

--- src/transformers/convert_slow_tokenizer.py
+++ src/transformers/convert_slow_tokenizer.py
@@ -23,13 +23,13 @@ import warnings
 from typing import Dict, List, Tuple
 
 from packaging import version
-
-from tokenizers import AddedToken, Regex, Tokenizer, decoders, normalizers, pre_tokenizers, processors
 from tokenizers.models import BPE, Unigram, WordPiece
+
+from tokenizers import (AddedToken, Regex, Tokenizer, decoders, normalizers,
+                        pre_tokenizers, processors)
 
 from .utils import is_protobuf_available, logging, requires_backends
 from .utils.import_utils import PROTOBUF_IMPORT_ERROR
-
 
 logger = logging.get_logger(__name__)
 
@@ -41,7 +41,8 @@ def import_protobuf(error_message=""):
         if version.parse(google.protobuf.__version__) < version.parse("4.0.0"):
             from transformers.utils import sentencepiece_model_pb2
         else:
-            from transformers.utils import sentencepiece_model_pb2_new as sentencepiece_model_pb2
+            from transformers.utils import \
+                sentencepiece_model_pb2_new as sentencepiece_model_pb2
         return sentencepiece_model_pb2
     else:
         raise ImportError(PROTOBUF_IMPORT_ERROR.format(error_message))
