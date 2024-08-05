e40a376d04fed41d155b5b233246dc60b5ed3ecd
faychu <faychu@qq.com>
2024-08-05 16:30:57 +0800

    [run_slow] qwen2_audio

--- tests/models/qwen2_audio/test_modeling_qwen2_audio.py
+++ tests/models/qwen2_audio/test_modeling_qwen2_audio.py
@@ -174,7 +174,7 @@ class Qwen2AudioForConditionalGenerationModelTest(ModelTesterMixin, unittest.Tes
 @require_torch
 class Qwen2AudioForConditionalGenerationIntegrationTest(unittest.TestCase):
     def setUp(self):
-        self.processor = AutoProcessor.from_pretrained("Qwen/Qwen2-Audio-7B")
+        self.processor = AutoProcessor.from_pretrained("faychu/test")
 
     def tearDown(self):
         gc.collect()
@@ -183,21 +183,70 @@ class Qwen2AudioForConditionalGenerationIntegrationTest(unittest.TestCase):
     @slow
     def test_small_model_integration_test_single(self):
         # Let' s make sure we test the preprocessing to replace what is used
-        model = Qwen2AudioForConditionalGeneration.from_pretrained("Qwen/Qwen2-Audio-7B")
+        model = Qwen2AudioForConditionalGeneration.from_pretrained("faychu/test")
 
-        prompt = "<|audio_bos|><|AUDIO|><|audio_eos|>Detect the language and recognize the speech:"
-        url = "https://qianwen-res.oss-cn-beijing.aliyuncs.com/Qwen2-Audio/audio/1272-128104-0000.flac"
+        url = "https://qianwen-res.oss-cn-beijing.aliyuncs.com/Qwen2-Audio/audio/glass-breaking-151256.mp3"
+        messages = [
+            {
+                "role": "user",
+                "content": [
+                    {"type": "audio", "audio_url": url},
+                    {"type": "text", "text": "What's that sound?"},
+                ],
+            }
+        ]
 
         raw_audio, _ = librosa.load(BytesIO(urlopen(url).read()), sr=self.processor.feature_extractor.sampling_rate)
 
-        inputs = self.processor(text=prompt, audios=raw_audio, return_tensors="pt")
+        formatted_prompt = self.processor.apply_chat_template(messages, add_generation_prompt=True)
 
-        EXPECTED_INPUT_IDS = torch.tensor([[151647, 151646, 151648, 57193, 279, 4128, 323, 15282, 279, 8806, 25]])
-        self.assertTrue(torch.equal(inputs["input_ids"], EXPECTED_INPUT_IDS))
-
+        inputs = self.processor(text=formatted_prompt, audios=[raw_audio], return_tensors="pt", padding=True)
+        print(inputs["input_ids"])
         output = model.generate(**inputs, max_new_tokens=32)
         print(self.processor.decode(output[0], skip_special_tokens=False))
-        EXPECTED_DECODED_TEXT = "<|audio_bos|><|AUDIO|><|audio_eos|>Detect the language and recognize the speech:<|en|>mister quilter is the apostle of the middle classes and we are glad to welcome his gospel<|endoftext|>"
+
+        EXPECTED_INPUT_IDS = torch.tensor(
+            [
+                [
+                    151644,
+                    8948,
+                    198,
+                    2610,
+                    525,
+                    264,
+                    10950,
+                    17847,
+                    13,
+                    151645,
+                    198,
+                    151644,
+                    872,
+                    198,
+                    14755,
+                    220,
+                    16,
+                    25,
+                    220,
+                    151647,
+                    151646,
+                    151648,
+                    198,
+                    3838,
+                    594,
+                    429,
+                    5112,
+                    30,
+                    151645,
+                    198,
+                    151644,
+                    77091,
+                    198,
+                ]
+            ]
+        )
+        self.assertTrue(torch.equal(inputs["input_ids"], EXPECTED_INPUT_IDS))
+
+        EXPECTED_DECODED_TEXT = "<|im_start|>system\nYou are a helpful assistant.<|im_end|>\n<|im_start|>user\nAudio 1: <|audio_bos|><|AUDIO|><|audio_eos|>\nWhat's that sound?<|im_end|>\n<|im_start|>assistant\nIt is the sound of glass breaking.<|im_end|>"
 
         self.assertEqual(
             self.processor.decode(output[0], skip_special_tokens=False),
@@ -207,28 +256,88 @@ class Qwen2AudioForConditionalGenerationIntegrationTest(unittest.TestCase):
     @slow
     def test_small_model_integration_test_batch(self):
         # Let' s make sure we test the preprocessing to replace what is used
-        model = Qwen2AudioForConditionalGeneration.from_pretrained("Qwen/Qwen2-Audio-7B")
+        model = Qwen2AudioForConditionalGeneration.from_pretrained("faychu/test")
 
-        prompts = [
-            "<|audio_bos|><|AUDIO|><|audio_eos|>Detect the language and recognize the speech:",
-            "<|audio_bos|><|AUDIO|><|audio_eos|>Generate the caption in English:",
-            "<|audio_bos|><|AUDIO|><|audio_eos|>Classify the human vocal sound to VocalSound in English:",
+        conversation1 = [
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "audio",
+                        "audio_url": "https://qianwen-res.oss-cn-beijing.aliyuncs.com/Qwen2-Audio/audio/glass-breaking-151256.mp3",
+                    },
+                    {"type": "text", "text": "What's that sound?"},
+                ],
+            },
+            {"role": "assistant", "content": "It is the sound of glass shattering."},
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "audio",
+                        "audio_url": "https://qianwen-res.oss-cn-beijing.aliyuncs.com/Qwen2-Audio/audio/f2641_0_throatclearing.wav",
+                    },
+                    {"type": "text", "text": "What can you hear?"},
+                ],
+            },
         ]
-        url1 = "https://qianwen-res.oss-cn-beijing.aliyuncs.com/Qwen2-Audio/audio/1272-128104-0000.flac"
-        audio1, _ = librosa.load(BytesIO(urlopen(url1).read()), sr=self.processor.feature_extractor.sampling_rate)
-        url2 = "https://qianwen-res.oss-cn-beijing.aliyuncs.com/Qwen2-Audio/audio/glass-breaking-151256.mp3"
-        audio2, _ = librosa.load(BytesIO(urlopen(url2).read()), sr=self.processor.feature_extractor.sampling_rate)
-        url3 = "https://qianwen-res.oss-cn-beijing.aliyuncs.com/Qwen2-Audio/audio/f2641_0_throatclearing.wav"
-        audio3, _ = librosa.load(BytesIO(urlopen(url3).read()), sr=self.processor.feature_extractor.sampling_rate)
 
-        inputs = self.processor(text=prompts, audios=[audio1, audio2, audio3], return_tensors="pt", padding=True)
+        conversation2 = [
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "audio",
+                        "audio_url": "https://qianwen-res.oss-cn-beijing.aliyuncs.com/Qwen2-Audio/audio/1272-128104-0000.flac",
+                    },
+                    {"type": "text", "text": "What does the person say?"},
+                ],
+            },
+        ]
+
+        conversations = [conversation1, conversation2]
+
+        text = [
+            self.processor.apply_chat_template(conversation, add_generation_prompt=True, tokenize=False)
+            for conversation in conversations
+        ]
+
+        audios = []
+        for conversation in conversations:
+            for message in conversation:
+                if isinstance(message["content"], list):
+                    for ele in message["content"]:
+                        if ele["type"] == "audio":
+                            audios.append(
+                                librosa.load(
+                                    BytesIO(urlopen(ele["audio_url"]).read()),
+                                    sr=self.processor.feature_extractor.sampling_rate,
+                                )[0]
+                            )
+
+        inputs = self.processor(text=text, audios=audios, return_tensors="pt", padding=True)
+
+        # prompts = [
+        #     "<|audio_bos|><|AUDIO|><|audio_eos|>Detect the language and recognize the speech:",
+        #     "<|audio_bos|><|AUDIO|><|audio_eos|>Generate the caption in English:",
+        #     "<|audio_bos|><|AUDIO|><|audio_eos|>Classify the human vocal sound to VocalSound in English:",
+        # ]
+        # url1 = "https://qianwen-res.oss-cn-beijing.aliyuncs.com/Qwen2-Audio/audio/1272-128104-0000.flac"
+        # audio1, _ = librosa.load(BytesIO(urlopen(url1).read()), sr=self.processor.feature_extractor.sampling_rate)
+        # url2 = "https://qianwen-res.oss-cn-beijing.aliyuncs.com/Qwen2-Audio/audio/glass-breaking-151256.mp3"
+        # audio2, _ = librosa.load(BytesIO(urlopen(url2).read()), sr=self.processor.feature_extractor.sampling_rate)
+        # url3 = "https://qianwen-res.oss-cn-beijing.aliyuncs.com/Qwen2-Audio/audio/f2641_0_throatclearing.wav"
+        # audio3, _ = librosa.load(BytesIO(urlopen(url3).read()), sr=self.processor.feature_extractor.sampling_rate)
+
+        # inputs = self.processor(text=prompts, audios=[audio1, audio2, audio3], return_tensors="pt", padding=True)
 
         output = model.generate(**inputs, max_new_tokens=32)
 
+        print(self.processor.batch_decode(output, skip_special_tokens=True))
+
         EXPECTED_DECODED_TEXT = [
-            "Detect the language and recognize the speech:mister quilter is the apostle of the middle classes and we are glad to welcome his gospel",
-            "Generate the caption in English: Glass is breaking.",
-            "Classify the human vocal sound to VocalSound in English: Throat clearing",
+            "system\nYou are a helpful assistant.\nuser\nAudio 1: \nWhat's that sound?\nassistant\nIt is the sound of glass shattering.\nuser\nAudio 2: \nWhat can you hear?\nassistant\ncough and throat clearing.",
+            "system\nYou are a helpful assistant.\nuser\nAudio 1: \nWhat does the person say?\nassistant\nThe original content of this audio is: 'Mister Quiller is the apostle of the middle classes and we are glad to welcome his gospel.'",
         ]
         self.assertEqual(
             self.processor.batch_decode(output, skip_special_tokens=True),
@@ -238,7 +347,7 @@ class Qwen2AudioForConditionalGenerationIntegrationTest(unittest.TestCase):
     @slow
     def test_small_model_integration_test_multiturn(self):
         # Let' s make sure we test the preprocessing to replace what is used
-        model = Qwen2AudioForConditionalGeneration.from_pretrained("Qwen/Qwen2-Audio-7B-Instruct")
+        model = Qwen2AudioForConditionalGeneration.from_pretrained("faychu/test")
 
         messages = [
             {"role": "system", "content": "You are a helpful assistant."},
