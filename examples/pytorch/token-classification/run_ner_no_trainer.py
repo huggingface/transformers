26a9443dae41737e665910fbb617173e17a0cd18
Arthur Zucker <arthur.zucker@gmail.com>
2024-08-06 18:33:18 +0200

    dev version 4.45.0

--- examples/pytorch/token-classification/run_ner_no_trainer.py
+++ examples/pytorch/token-classification/run_ner_no_trainer.py
@@ -56,7 +56,7 @@ from transformers.utils.versions import require_version
 
 
 # Will error if the minimal version of Transformers is not installed. Remove at your own risks.
-check_min_version("4.44.0.dev0")
+check_min_version("4.45.0.dev0")
 
 logger = get_logger(__name__)
 require_version("datasets>=1.8.0", "To fix: pip install -r examples/pytorch/token-classification/requirements.txt")
