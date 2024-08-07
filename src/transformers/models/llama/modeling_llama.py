b7569b76c4bb9a778216bfac1235b5055ac97734
Yoshi Suhara <ysuhara@nvidia.com>
2024-08-07 01:22:24 -0700

    Fix o_proj

--- src/transformers/models/llama/modeling_llama.py
+++ src/transformers/models/llama/modeling_llama.py
@@ -362,11 +362,11 @@ class LlamaAttention(nn.Module):
         self.v_proj = nn.Linear(self.hidden_size, self.num_key_value_heads * self.head_dim, bias=config.attention_bias)
 
         # If kv_channels is None, the output projection is hidden_size -> hidden_size
-        # Otherwise it is done kv_channels * num_key_value_heads -> hidden_size
+        # Otherwise it is done num_heads * head_dim (= kv_channels) -> hidden_size
         if config.kv_channels is None:
             self.o_proj = nn.Linear(self.hidden_size, self.hidden_size, bias=config.attention_bias)
         else:
-            self.o_proj = nn.Linear(self.hidden_size, config.kv_channels * self.num_key_value_heads, bias=config.attention_bias)
+            self.o_proj = nn.Linear(self.num_heads * self.head_dim, self.hidden_size, bias=config.attention_bias)
 
         # TODO (joao): remove in v4.45 (RoPE is computed in the model, not in the decoder layers)
         self.rotary_emb = LlamaRotaryEmbedding(config=self.config)
