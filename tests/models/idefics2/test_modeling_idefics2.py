4c9f8945b47c0fc70576ff4261d1bec8bdd35033
Raushan Turganbay <raushan.turganbay@alumni.nu.edu.kz>
2024-08-08 14:54:08 +0500

    Update tests/models/idefics2/test_modeling_idefics2.py
    Co-authored-by: amyeroberts <22614925+amyeroberts@users.noreply.github.com>

--- tests/models/idefics2/test_modeling_idefics2.py
+++ tests/models/idefics2/test_modeling_idefics2.py
@@ -405,7 +405,7 @@ class Idefics2ForConditionalGenerationModelTest(GenerationTesterMixin, ModelTest
     is_multimodal = True
     # We define this flag here because in VLMs these flags depend on which LM/vision models are used
     # So we can't know if SDPA is supported before starting to load the model
-    # This flag is used by tests and is set to False because LM/vision models used in tests dont support SDPA
+    # This flag is used by tests and is set to False because LM/vision models used in tests don't support SDPA
     supports_sdpa = False
 
     def setUp(self):
