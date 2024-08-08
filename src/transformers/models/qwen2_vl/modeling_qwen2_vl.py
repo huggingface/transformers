fdb4afdaff689e4e6d311b2b2f2ca8d1a600d28a
simonJJJ <821898965@qq.com>
2024-08-08 19:47:17 +0800

    update-rotary_seq_len

--- src/transformers/models/qwen2_vl/modeling_qwen2_vl.py
+++ src/transformers/models/qwen2_vl/modeling_qwen2_vl.py
@@ -710,7 +710,9 @@ class Qwen2VLFlashAttention2(Qwen2VLAttention):
             kv_seq_len += past_key_value.get_usable_length(kv_seq_len, self.layer_idx)
 
         # Because the input can be padded, the absolute sequence length depends on the max position id.
-        rotary_seq_len = max(kv_seq_len, position_ids[:, -1].max().item()) + 1
+        rotary_seq_len = (
+            max(kv_seq_len, position_ids[:, -1].max().item() + 1) if position_ids is not None else kv_seq_len
+        )
         cos, sin = self.rotary_emb(value_states, seq_len=rotary_seq_len)
 
         if self.rope_scaling and "mrope_section" in self.rope_scaling:
