1b75471a397efe9542698ac25d85a99a038f0283
simonJJJ <821898965@qq.com>
2024-08-08 16:56:06 +0800

    remove-reorder_cache

--- src/transformers/models/qwen2_vl/modeling_qwen2_vl.py
+++ src/transformers/models/qwen2_vl/modeling_qwen2_vl.py
@@ -1568,12 +1568,3 @@ class Qwen2VLForConditionalGeneration(Qwen2VLPreTrainedModel):
             }
         )
         return model_inputs
-
-    @staticmethod
-    def _reorder_cache(past_key_values, beam_idx):
-        reordered_past = ()
-        for layer_past in past_key_values:
-            reordered_past += (
-                tuple(past_state.index_select(0, beam_idx.to(past_state.device)) for past_state in layer_past),
-            )
-        return reordered_past
