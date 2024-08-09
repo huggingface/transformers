e72c03df5b0239eb42d3514c4d009570aca94a79
raushan <raushan@huggingface.co>
2024-08-09 10:40:49 +0200

    skip tests

--- tests/models/musicgen/test_modeling_musicgen.py
+++ tests/models/musicgen/test_modeling_musicgen.py
@@ -270,6 +270,10 @@ class MusicgenDecoderTest(ModelTesterMixin, GenerationTesterMixin, PipelineTeste
     def test_inputs_embeds_matches_input_ids(self):
         pass
 
+    @unittest.skip(reason="MusicGen does not use inputs_embeds")
+    def test_inputs_embeds_matches_input_ids_with_generate(self):
+        pass
+
     @unittest.skip(reason="MusicGen does not support all arguments tested")
     def test_model_outputs_equivalence(self):
         pass
