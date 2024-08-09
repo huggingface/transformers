90b20fc1dbd056c999501e5363fec90eb12f2315
Pablo Montalvo <pablo.montalvo.leroux@gmail.com>
2024-08-09 11:13:17 +0200

    add Gemma2 for causal LM to test classes

--- tests/models/gemma2/test_modeling_gemma2.py
+++ tests/models/gemma2/test_modeling_gemma2.py
@@ -59,7 +59,7 @@ class Gemma2ModelTest(GemmaModelTest, unittest.TestCase):
         if is_torch_available()
         else ()
     )
-    all_generative_model_classes = ()
+    all_generative_model_classes = (Gemma2ForCausalLM,) if is_torch_available else ()
     pipeline_model_mapping = (
         {
             "feature-extraction": Gemma2Model,
