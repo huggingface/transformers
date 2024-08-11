df5a0356934c87e1fb90c97084732126f7ad54db
Sean (Seok-Won) Yi <seantuokc@gmail.com>
2024-08-11 17:15:04 +0900

    Undo metric_for_best_model update.

--- src/transformers/trainer.py
+++ src/transformers/trainer.py
@@ -767,9 +767,6 @@ class Trainer:
         )
         self.processing_class = processing_class
 
-        if not self.args.metric_for_best_model:
-            self.args.metric_for_best_model = "loss"
-
     def _activate_neftune(self, model):
         r"""
         Activates the neftune as presented in this code: https://github.com/neelsjain/NEFTune and paper:
