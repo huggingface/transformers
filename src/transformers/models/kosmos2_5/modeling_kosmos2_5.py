210ccb198960d15bfe12fcd9b20be8b4a4a89521
ydshieh <ydshieh@users.noreply.github.com>
2024-08-13 10:32:00 +0200

    [ydshieh] update _init_weights

--- src/transformers/models/kosmos2_5/modeling_kosmos2_5.py
+++ src/transformers/models/kosmos2_5/modeling_kosmos2_5.py
@@ -1681,9 +1681,10 @@ class Kosmos2_5PreTrainedModel(PreTrainedModel):
 
     def _init_weights(self, module):
         """Initialize the weights"""
-        # vision config's `self.config.vision_config.initializer_range` is not used
-        if isinstance(self, (Kosmos2_5VisionModel, Kosmos2_5TextModel, Kosmos2_5TextForCausalLM)):
+        if isinstance(self, Kosmos2_5VisionModel):
             factor = self.config.initializer_factor
+            std = self.config.initializer_range
+        elif isinstance(self, (Kosmos2_5TextModel, Kosmos2_5TextForCausalLM)):
             std = self.config.init_std
         elif isinstance(self, (Kosmos2_5Model, Kosmos2_5ForConditionalGeneration)):
             factor = self.config.vision_config.initializer_factor
