8e15beaaa1d120890fd715b361b5e9d07c3a1340
Joao Gante <joao@huggingface.co>
2024-08-14 12:20:31 +0000

    remove custom bart config

--- tests/models/blenderbot/test_modeling_blenderbot.py
+++ tests/models/blenderbot/test_modeling_blenderbot.py
@@ -116,12 +116,6 @@ class BlenderbotModelTester:
         self.pad_token_id = pad_token_id
         self.bos_token_id = bos_token_id
 
-        # forcing a certain token to be generated, sets all other tokens to -inf
-        # if however the token to be generated is already at -inf then it can lead token
-        # `nan` values and thus break generation
-        self.forced_bos_token_id = None
-        self.forced_eos_token_id = None
-
     def prepare_config_and_inputs(self):
         input_ids = ids_tensor([self.batch_size, self.seq_length], self.vocab_size).clamp(
             3,
@@ -150,8 +144,6 @@ class BlenderbotModelTester:
             eos_token_id=self.eos_token_id,
             bos_token_id=self.bos_token_id,
             pad_token_id=self.pad_token_id,
-            forced_bos_token_id=self.forced_bos_token_id,
-            forced_eos_token_id=self.forced_eos_token_id,
         )
 
     def get_pipeline_config(self):
