0ddecd3069e68f3de93b465345b6dc780081e958
Michael Kamerath <mkamerath@gmail.com>
2024-08-15 13:58:37 -0600

    Add option for running ffmpeg_microphone_live as a background process

--- src/transformers/pipelines/audio_utils.py
+++ src/transformers/pipelines/audio_utils.py
@@ -51,6 +51,7 @@ def ffmpeg_microphone(
     chunk_length_s: float,
     format_for_conversion: str = "f32le",
     ffmpeg_input_device: Optional[str] = None,
+    background_arg: Optional[str] = None
 ):
     """
     Helper function to read audio from a microphone using ffmpeg. The default input device will be used unless another
@@ -70,6 +71,10 @@ def ffmpeg_microphone(
             The indentifier of the input device to be used by ffmpeg (i.e. ffmpeg's '-i' argument). If unset,
             the default input device will be used. See `https://www.ffmpeg.org/ffmpeg-devices.html#Input-Devices`
             for how to specify and list input devices.
+        background_arg (`str`, *optional*):
+            The argument passed to ffmpeg that determines whether or not the process is run in the background
+            or not. Default is the empty string and the only other option is -nostdin
+
     Returns:
         A generator yielding audio chunks of `chunk_length_s` seconds as `bytes` objects of length
         `int(round(sampling_rate * chunk_length_s)) * size_of_sample`.
@@ -95,6 +100,8 @@ def ffmpeg_microphone(
         format_ = "dshow"
         input_ = ffmpeg_input_device or _get_microphone_name()
 
+    stdin_arg = str(background_arg or "")
+
     ffmpeg_command = [
         "ffmpeg",
         "-f",
@@ -113,6 +120,7 @@ def ffmpeg_microphone(
         "-loglevel",
         "quiet",
         "pipe:1",
+        stdin_arg
     ]
     chunk_len = int(round(sampling_rate * chunk_length_s)) * size_of_sample
     iterator = _ffmpeg_stream(ffmpeg_command, chunk_len)
@@ -127,6 +135,7 @@ def ffmpeg_microphone_live(
     stride_length_s: Optional[Union[Tuple[float, float], float]] = None,
     format_for_conversion: str = "f32le",
     ffmpeg_input_device: Optional[str] = None,
+    run_in_background: Optional[bool] = False
 ):
     """
     Helper function to read audio from a microphone using ffmpeg. This will output `partial` overlapping chunks starting
@@ -153,6 +162,11 @@ def ffmpeg_microphone_live(
             The identifier of the input device to be used by ffmpeg (i.e. ffmpeg's '-i' argument). If unset,
             the default input device will be used. See `https://www.ffmpeg.org/ffmpeg-devices.html#Input-Devices`
             for how to specify and list input devices.
+        run_in_background (`bool`, *optional*):
+            The boolean to determine whether or not to run the ffmpeg command in the background. If set to true,
+            ffmpeg will not use STDIN as its input (useful for when other processes are using STDIN). If unset,
+            ffmpeg will run using STDIN.
+
     Return:
         A generator yielding dictionaries of the following form
 
@@ -167,9 +181,20 @@ def ffmpeg_microphone_live(
     else:
         chunk_s = chunk_length_s
 
-    microphone = ffmpeg_microphone(
-        sampling_rate, chunk_s, format_for_conversion=format_for_conversion, ffmpeg_input_device=ffmpeg_input_device
-    )
+    if run_in_background:
+        microphone = ffmpeg_microphone(
+            sampling_rate, chunk_s,
+            format_for_conversion=format_for_conversion,
+            ffmpeg_input_device=ffmpeg_input_device,
+            background_arg="-nostdin"
+        )
+    else:
+        microphone = ffmpeg_microphone(
+            sampling_rate, chunk_s,
+            format_for_conversion=format_for_conversion,
+            ffmpeg_input_device=ffmpeg_input_device
+        )
+
     if format_for_conversion == "s16le":
         dtype = np.int16
         size_of_sample = 2
