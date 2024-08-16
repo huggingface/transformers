258814458d5fdbf3282a86b46c4a794ca9ccb8b4
Franz Louis Cesista <franzlouiscesista@gmail.com>
2024-08-16 15:41:52 +0800

    fix linter nit

--- src/transformers/models/chameleon/processing_chameleon.py
+++ src/transformers/models/chameleon/processing_chameleon.py
@@ -24,6 +24,7 @@ from ...image_utils import ImageInput
 from ...processing_utils import ProcessingKwargs, ProcessorMixin, TextKwargs
 from ...tokenization_utils_base import PreTokenizedInput, TextInput
 
+
 if sys.version_info >= (3, 11):
     from typing import Unpack
 else:
