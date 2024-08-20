5d718f0a52a9d56180818e00ef133aa79931dd98
Ita Zaporozhets <ita.zaporozhets@huggingface.co>
2024-08-20 14:03:08 +0200

    add tiktoken changes back [build-ci-image]

--- setup.py
+++ setup.py
@@ -99,7 +99,7 @@ _deps = [
     "accelerate>=0.21.0",
     "av==9.2.0",  # Latest version of PyAV (10.0.0) has issues with audio stream.
     "beautifulsoup4",
-    "blobfile==2.1.1"
+    "blobfile==2.1.1",
     "codecarbon==1.2.0",
     "cookiecutter==1.7.3",
     "dataclasses",
