de040509ed66b5d083e6cbc5c9674e94bff80f0b
Niels <niels.rogge1@gmail.com>
2024-08-22 18:20:46 +0200

    Rebase

--- src/transformers/models/siglip/tokenization_siglip_fast.py
+++ src/transformers/models/siglip/tokenization_siglip_fast.py
@@ -12,8 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-""" Fast tokenization class for SigLIP."""
-
+"""Fast tokenization class for SigLIP."""
 
 import os
 from shutil import copyfile
