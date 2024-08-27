d41a4403121467fec3fba5671a963cf6cd37181f
Hannan Komari <Komari.Hannan@gmail.com>
2024-08-27 21:30:22 +0200

    Update tokenization_whisper_fast.py
    just use len of token_ids

--- src/transformers/models/whisper/tokenization_whisper_fast.py
+++ src/transformers/models/whisper/tokenization_whisper_fast.py
@@ -580,8 +580,8 @@ class WhisperTokenizerFast(PreTrainedTokenizerFast):
             token_ids = self._convert_to_list(token_ids)
 
         # handle case of empty token_ids for decoding with timestamps.
-        # at this point token_ids is a list, so it is safe to use if not check.
-        if token_ids is None or len(token_ids) == 0:
+        # at this point token_ids is a list, so it is safe to check length of list.
+        if len(token_ids) == 0:
             return token_ids
 
         has_prompt = token_ids[0] == prompt_token_id
