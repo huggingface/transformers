a19baede79ba03866fc43dbd886e0d731dc54603
Avishai Elmakies <avishai.elma@cs.huji.ac.il>
2024-08-28 11:37:14 +0300

    fix 2 PR comments

--- src/transformers/models/opt/modeling_opt.py
+++ src/transformers/models/opt/modeling_opt.py
@@ -65,7 +65,8 @@ def _attention_to_position_ids(attention_mask: torch.Tensor) -> torch.Tensor:
     Converts an attention mask to position ids.
     """
 
-    return (torch.cumsum(attention_mask, dim=1) * attention_mask).long() - 1
+    position_ids = torch.cumsum(attention_mask, dim=1)
+    return (position_ids * attention_mask - 1).long()
 
 
 class OPTLearnedPositionalEmbedding(nn.Embedding):
@@ -1101,7 +1102,10 @@ class OPTForCausalLM(OPTPreTrainedModel):
             if past_key_values:
                 position_ids = position_ids[:, -input_ids.shape[1] :]
 
-                # This `clone` call is needed to avoid recapturing cuda graphs with `torch.compile`'s  `mode="reduce-overhead`, as otherwise the input `position_ids` would have various stride during the decoding. Here, simply using `.contiguous()` is not sufficient as in the batch size = 1 case, `position_ids` is already contiguous but with varying stride which retriggers a capture.
+                # This `clone` call is needed to avoid recapturing cuda graphs with `torch.compile`'s  `mode="reduce-overhead`,
+                # as otherwise the input `position_ids` would have various stride during the decoding.
+                # Here, simply using `.contiguous()` is not sufficient as in the batch size = 1 case,
+                # position_ids` is already contiguous but with varying stride which retriggers a capture.
                 position_ids = position_ids.clone(memory_format=torch.contiguous_format)
 
         # if `inputs_embeds` are passed, we only want to use them in the 1st generation step
