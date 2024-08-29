7e01cfb0eab9a06d7d3b471218ccdb29dfbde5b5
jiqing-feng <107918818+jiqing-feng@users.noreply.github.com>
2024-08-29 13:22:20 +0800

    Update src/transformers/integrations/integration_utils.py
    Co-authored-by: Arthur <48595927+ArthurZucker@users.noreply.github.com>

--- src/transformers/integrations/integration_utils.py
+++ src/transformers/integrations/integration_utils.py
@@ -230,7 +230,8 @@ def _validate_bnb_multi_backend_availability(raise_exception):
                 else device
                 for device in bnb_supported_devices
             )
-            err_msg = f"None of the available devices `available_devices = {available_devices or None}` are supported by the bitsandbytes version you have installed: `bnb_supported_devices = {bnb_supported_devices_with_info}`. Please check the docs to see if the backend you intend to use is available and how to install it: https://huggingface.co/docs/bitsandbytes/main/en/installation#multi-backend"
+            err_msg = f"None of the available devices `available_devices = {available_devices or None}` are supported by the bitsandbytes version you have installed: `bnb_supported_devices = {bnb_supported_devices_with_info}`. 
+            Please check the docs to see if the backend you intend to use is available and how to install it: https://huggingface.co/docs/bitsandbytes/main/en/installation#multi-backend"
 
             logger.error(err_msg)
             raise RuntimeError(err_msg)
