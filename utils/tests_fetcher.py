9180e62fe59eae942b35c0d1d1e2887e57dcfeae
Arthur Zucker <arthur.zucker@gmail.com>
2024-08-29 17:36:35 +0200

    fix

--- utils/tests_fetcher.py
+++ utils/tests_fetcher.py
@@ -1131,13 +1131,13 @@ def parse_commit_message(commit_message: str) -> Dict[str, bool]:
 
 
 JOB_TO_TEST_FILE = {
-    "torch_and_tf": r"tests/models/.*/test_modeling_(?=tf_|!flax]).*",
-    "torch_and_flax": r"tests/models/.*/test_modeling_(?=flax_|!tf).*",
+    "torch_and_tf": r"tests/models/.*/test_modeling_((?=tf_)|(?!flax)).*",
+    "torch_and_flax": r"tests/models/.*/test_modeling_((?=flax)|(?!tf)).*",
     "tf": r"tests/models/.*/test_modeling_tf_.*",
     "torch": r"tests/models/.*/test_modeling_(?!(flax_|tf_)).*",
     "generate": r"tests/models/.*/test_modeling_(?!(flax_|tf_)).*",
     "tokenization": r"tests/models/.*/test_tokenization.*",
-    "processors": r"tests/models/.*/test_(?!(modeling_|tokenization_).*",  # takes feature extractors, image processors, processors
+    "processors": r"tests/models/.*/test_(?!(modeling_|tokenization_)).*",  # takes feature extractors, image processors, processors
     "examples_torch": r"examples/pytorch/.*test_.*",
     "examples_tensorflow": r"examples/tensorflow/.*test_.*",
     "examples_flax": r"examples/flax/.*test.*",
@@ -1145,9 +1145,9 @@ JOB_TO_TEST_FILE = {
     "custom_tokenizers": r"tests/models/.*/test_tokenization_(?=bert_japanese|openai|clip).*",
     # "repo_utils": r"tests/[^models].*test.*", TODO later on we might want to do
     "pipeline_tf": r"tests/models/.*/test_modeling_tf_.*",
-    "pipeline_torch": r"tests/models/.*/test_modeling_(?!(flax_|tf_))].*",
+    "pipeline_torch": r"tests/models/.*/test_modeling_(?!(flax_|tf_)).*",
     "hub": r"tests/.*(?!(flax_|tf_)).*",
-    "onnx": r"tests/models/.*/test_modeling_(?=tf_|!flax)).*",
+    "onnx": r"tests/models/.*/test_modeling_((?=tf_)|(?!flax)).*",
 }
 
 
