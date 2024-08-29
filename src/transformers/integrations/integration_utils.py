496c0467a4f401312b78e94c91056a5a109da4bb
jiqing-feng <jiqing.feng@intel.com>
2024-08-29 09:43:18 -0400

    make quality

--- src/transformers/integrations/integration_utils.py
+++ src/transformers/integrations/integration_utils.py
@@ -230,8 +230,10 @@ def _validate_bnb_multi_backend_availability(raise_exception):
                 else device
                 for device in bnb_supported_devices
             )
-            err_msg = f"None of the available devices `available_devices = {available_devices or None}` are supported by the bitsandbytes version you have installed: `bnb_supported_devices = {bnb_supported_devices_with_info}`." \
-            + "\nPlease check the docs to see if the backend you intend to use is available and how to install it: https://huggingface.co/docs/bitsandbytes/main/en/installation#multi-backend"
+            err_msg = (
+                f"None of the available devices `available_devices = {available_devices or None}` are supported by the bitsandbytes version you have installed: `bnb_supported_devices = {bnb_supported_devices_with_info}`. "
+                "Please check the docs to see if the backend you intend to use is available and how to install it: https://huggingface.co/docs/bitsandbytes/main/en/installation#multi-backend"
+            )
 
             logger.error(err_msg)
             raise RuntimeError(err_msg)
@@ -245,8 +247,10 @@ def _validate_bnb_multi_backend_availability(raise_exception):
 
 def _validate_bnb_cuda_backend_availability(raise_exception):
     if not torch.cuda.is_available():
-        log_msg = "CUDA is required but not available for bitsandbytes. Please consider installing the multi-platform enabled version of bitsandbytes, which is currently a work in progress." \
-        + "\nPlease check currently supported platforms and installation instructions at https://huggingface.co/docs/bitsandbytes/main/en/installation#multi-backend"
+        log_msg = (
+            "CUDA is required but not available for bitsandbytes. Please consider installing the multi-platform enabled version of bitsandbytes, which is currently a work in progress. "
+            "Please check currently supported platforms and installation instructions at https://huggingface.co/docs/bitsandbytes/main/en/installation#multi-backend"
+        )
         if raise_exception:
             logger.error(log_msg)
             raise RuntimeError(log_msg)
