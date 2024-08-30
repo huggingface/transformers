74e448e6ae8a1efd308be9507e2119958e542b7c
Joao Gante <joao@huggingface.co>
2024-08-30 11:27:39 +0000

    skip InstructBlipForConditionalGenerationDecoderOnlyTest

--- tests/models/instructblip/test_modeling_instructblip.py
+++ tests/models/instructblip/test_modeling_instructblip.py
@@ -454,6 +454,9 @@ class InstructBlipForConditionalGenerationDecoderOnlyModelTester:
 @require_torch
 class InstructBlipForConditionalGenerationDecoderOnlyTest(ModelTesterMixin, GenerationTesterMixin, unittest.TestCase):
     all_model_classes = (InstructBlipForConditionalGeneration,) if is_torch_available() else ()
+    # InstructBlipForConditionalGeneration has a custom input structure, would need custom input preparation in the
+    # generation tests
+    all_generative_model_classes = ()
     fx_compatible = False
     test_head_masking = False
     test_pruning = False
