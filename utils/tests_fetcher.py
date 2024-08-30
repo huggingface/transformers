2633cf267b81eab0cb33b7d909d171a3639cbddd
Joao Gante <joao@huggingface.co>
2024-08-30 20:07:54 +0000

    dbg

--- utils/tests_fetcher.py
+++ utils/tests_fetcher.py
@@ -1153,7 +1153,6 @@ JOB_TO_TEST_FILE = {
 
 def create_test_list_from_filter(full_test_list, out_path):
     all_test_files = "\n".join(full_test_list)
-    breakpoint()
     for job_name, _filter in JOB_TO_TEST_FILE.items():
         file_name = os.path.join(out_path, f"{job_name}_test_list.txt")
         if job_name == "tests_hub":
@@ -1161,8 +1160,8 @@ def create_test_list_from_filter(full_test_list, out_path):
         else:
             files_to_test = list(re.findall(_filter, all_test_files))
         print(job_name, file_name)
-        with open(file_name, "w") as f:
-            f.write("\n".join(files_to_test))
+        # with open(file_name, "w") as f:
+        #     f.write("\n".join(files_to_test))
 
 
 if __name__ == "__main__":
