346fce80f488cd3fcda6aa5ae74001e0a35fcdf6
Arthur Zucker <arthur.zucker@gmail.com>
2024-08-30 13:51:03 +0200

    up

--- .circleci/create_circleci_config.py
+++ .circleci/create_circleci_config.py
@@ -351,7 +351,7 @@ def create_circleci_config(folder=None):
 
     if len(jobs) == 0:
         jobs = [EmptyJob()]
-
+    print({j.job_name + "_test_list":{"type":"string", "default":''} for j in jobs})
     config = {
         "version": "2.1",
         "parameters": {
