a4bd3ee249c8738e7343f6e8ad15f025d66d8f62
Jeongseok Kang <jskang@lablup.com>
2024-09-02 20:40:34 +0900

    docs: Replace package abbreviations with full name(`bitsandbytes`) in docstrings (#33230)
    * docs: Provide fullname for `bitsandbytes` package
    
    * docs: Provide fullname for `bitsandbytes` package (2)

--- src/transformers/trainer.py
+++ src/transformers/trainer.py
@@ -1274,7 +1274,7 @@ class Trainer:
                 optimizer_kwargs.update(additional_optim_kwargs)
                 optimizer_kwargs.update(bnb_kwargs)
             except ImportError:
-                raise ValueError("Trainer tried to instantiate bnb optimizer but bnb is not installed!")
+                raise ValueError("Trainer tried to instantiate bnb optimizer but `bitsandbytes` is not installed!")
             if is_bitsandbytes_available() and version.parse(
                 importlib.metadata.version("bitsandbytes")
             ) < version.parse("0.41.1"):
