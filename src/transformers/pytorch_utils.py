b3d1c1167c151d3ab1260ddff4118fc1da84b099
NielsRogge <48327001+NielsRogge@users.noreply.github.com>
2024-09-02 09:39:39 +0200

    Add GraniteRMSNorm (#33177)
    * Add GraniteRMSNorm
    
    * [run_slow] granite

--- src/transformers/pytorch_utils.py
+++ src/transformers/pytorch_utils.py
@@ -24,7 +24,7 @@ from torch import nn
 from .utils import is_torch_xla_available, logging
 
 
-ALL_LAYERNORM_LAYERS = [nn.LayerNorm, nn.RMSNorm]
+ALL_LAYERNORM_LAYERS = [nn.LayerNorm]
 
 logger = logging.get_logger(__name__)
 
