ca3a51b13cbbdbc5428d9aa5a37e54362ab89af5
Arthur Zucker <arthur.zucker@gmail.com>
2024-09-02 14:26:27 +0200

    correct one example

--- examples/diff-conversion/diff_new_model.py
+++ examples/diff-conversion/diff_new_model.py
@@ -26,8 +26,9 @@ class NewModelConfig(GemmaConfig):
         rope_theta=10000.0,
         attention_bias=False,
         attention_dropout=0.0,
+        **kwargs
     ):
-        super().__init__(self)
+        super().__init__(self, **kwargs)
 
     @property
     def num_heads(self):
