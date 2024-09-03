955cb6de6057346d9ff2571a4f780f3782d8eeab
Yoach Lacombe <yoach.lacombe@gmail.com>
2024-09-03 10:52:05 +0200

    make fixup

--- utils/check_table.py
+++ utils/check_table.py
@@ -174,7 +174,14 @@ MODEL_NAMES_WITH_SAME_CONFIG = {
     "XLSR-Wav2Vec2": "Wav2Vec2",
 }
 
-MODEL_NAMES_TO_IGNORE = ["CLIPVisionModel", "SiglipVisionModel", "ChineseCLIPVisionModel", "Qwen2AudioEncoder", "FastSpeech2ConformerWithHifiGan"]
+MODEL_NAMES_TO_IGNORE = [
+    "CLIPVisionModel",
+    "SiglipVisionModel",
+    "ChineseCLIPVisionModel",
+    "Qwen2AudioEncoder",
+    "FastSpeech2ConformerWithHifiGan",
+]
+
 
 def get_model_table_from_auto_modules() -> str:
     """
