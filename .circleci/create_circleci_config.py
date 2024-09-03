baba0271d7ba273b3510b69a608bf7d3226bf29f
Arthur Zucker <arthur.zucker@gmail.com>
2024-09-03 16:21:23 +0200

    print config

--- .circleci/create_circleci_config.py
+++ .circleci/create_circleci_config.py
@@ -365,6 +365,7 @@ def create_circleci_config(folder=None):
         "jobs" : {j.job_name: j.to_dict() for j in jobs},
         "workflows": {"version": 2, "run_tests": {"jobs": [j.job_name for j in jobs]}}
     }
+    print(config)
     with open(os.path.join(folder, "generated_config.yml"), "w") as f:
         f.write(yaml.dump(config, sort_keys=False, default_flow_style=False))
 
