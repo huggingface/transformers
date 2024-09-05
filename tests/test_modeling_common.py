d5a36f3e4836a6103a7127c6df8a5871ed6ddb5c
Wang, Yi <yi.a.wang@intel.com>
2024-09-05 07:25:16 -0400

    split test case
    Signed-off-by: Wang, Yi <yi.a.wang@intel.com>

--- tests/test_modeling_common.py
+++ tests/test_modeling_common.py
@@ -382,28 +382,60 @@ class ModelTesterMixin:
             self.assertFalse(model.is_gradient_checkpointing)
 
             # check enable works
-            for _hf_peft_config_loaded in [True, False]:
-                model._hf_peft_config_loaded = _hf_peft_config_loaded
-                model.gradient_checkpointing_enable()
-                self.assertTrue(model.is_gradient_checkpointing)
+            model.gradient_checkpointing_enable()
+            self.assertTrue(model.is_gradient_checkpointing)
 
-                # Loop over all modules and check that relevant modules have gradient_checkpointing set to True
-                for n, m in model.named_modules():
-                    if hasattr(m, "gradient_checkpointing"):
-                        self.assertTrue(
-                            m.gradient_checkpointing, f"Module {n} does not have gradient_checkpointing set to True"
-                        )
+            # Loop over all modules and check that relevant modules have gradient_checkpointing set to True
+            for n, m in model.named_modules():
+                if hasattr(m, "gradient_checkpointing"):
+                    self.assertTrue(
+                        m.gradient_checkpointing, f"Module {n} does not have gradient_checkpointing set to True"
+                    )
 
-                # check disable works
-                model.gradient_checkpointing_disable()
-                self.assertFalse(model.is_gradient_checkpointing)
+            # check disable works
+            model.gradient_checkpointing_disable()
+            self.assertFalse(model.is_gradient_checkpointing)
 
-                # Loop over all modules and check that relevant modules have gradient_checkpointing set to False
-                for n, m in model.named_modules():
-                    if hasattr(m, "gradient_checkpointing"):
-                        self.assertFalse(
-                            m.gradient_checkpointing, f"Module {n} does not have gradient_checkpointing set to False"
-                        )
+            # Loop over all modules and check that relevant modules have gradient_checkpointing set to False
+            for n, m in model.named_modules():
+                if hasattr(m, "gradient_checkpointing"):
+                    self.assertFalse(
+                        m.gradient_checkpointing, f"Module {n} does not have gradient_checkpointing set to False"
+                    )
+
+    def test_peft_gradient_checkpointing_enable_disable(self):
+        config, inputs_dict = self.model_tester.prepare_config_and_inputs_for_common()
+
+        for model_class in self.all_model_classes:
+            if not model_class.supports_gradient_checkpointing:
+                continue
+
+            # at init model should have gradient checkpointing disabled
+            model = model_class(config)
+            self.assertFalse(model.is_gradient_checkpointing)
+
+            # check enable works
+            model._hf_peft_config_loaded = True
+            model.gradient_checkpointing_enable()
+            self.assertTrue(model.is_gradient_checkpointing)
+
+            # Loop over all modules and check that relevant modules have gradient_checkpointing set to True
+            for n, m in model.named_modules():
+                if hasattr(m, "gradient_checkpointing"):
+                    self.assertTrue(
+                        m.gradient_checkpointing, f"Module {n} does not have gradient_checkpointing set to True"
+                    )
+
+            # check disable works
+            model.gradient_checkpointing_disable()
+            self.assertFalse(model.is_gradient_checkpointing)
+
+            # Loop over all modules and check that relevant modules have gradient_checkpointing set to False
+            for n, m in model.named_modules():
+                if hasattr(m, "gradient_checkpointing"):
+                    self.assertFalse(
+                        m.gradient_checkpointing, f"Module {n} does not have gradient_checkpointing set to False"
+                    )
 
     @is_flaky(description="low likelihood of failure, reason not yet discovered")
     def test_save_load_fast_init_from_base(self):
