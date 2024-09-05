34dfb103f5d964b64e042b0218964c590b122d6f
Jade Choghari <78852495+jadechoghari@users.noreply.github.com>
2024-09-05 23:08:50 +0200

    remove forward pass logic

--- src/transformers/models/sam/convert_sam_to_hf.py
+++ src/transformers/models/sam/convert_sam_to_hf.py
@@ -172,33 +172,20 @@ def convert_sam_checkpoint(model_name, checkpoint_path, pytorch_dump_folder, pus
             scores = output.iou_scores.squeeze()
 
     elif model_name == "sam_vit_h_4b8939":
-        # inputs = processor(
-        #     images=np.array(raw_image), input_points=[input_points], input_labels=input_labels, return_tensors="pt"
-        # ).to(device)
+        inputs = processor(
+            images=np.array(raw_image), input_points=[input_points], input_labels=input_labels, return_tensors="pt"
+        ).to(device)
 
-        # with torch.no_grad():
-        #     output = hf_model(**inputs)
-        #     scores = output.iou_scores.squeeze()
+        input_boxes = [[[75.0, 275.0, 1725.0, 850.0]]]
+        inputs = processor(images=np.array(raw_image), input_boxes=input_boxes, return_tensors="pt").to(device)
 
-        # input_boxes = [[[75.0, 275.0, 1725.0, 850.0]]]
+        # Test with 2 points and 1 image.
+        input_points = [[[400, 650], [800, 650]]]
+        input_labels = [[1, 1]]
 
-        # inputs = processor(images=np.array(raw_image), input_boxes=input_boxes, return_tensors="pt").to(device)
-
-        # with torch.no_grad():
-        #     output = hf_model(**inputs)
-        #     scores = output.iou_scores.squeeze()
-
-        # # Test with 2 points and 1 image.
-        # input_points = [[[400, 650], [800, 650]]]
-        # input_labels = [[1, 1]]
-
-        # inputs = processor(
-        #     images=np.array(raw_image), input_points=[input_points], input_labels=input_labels, return_tensors="pt"
-        # ).to(device)
-
-        # with torch.no_grad():
-        #     output = hf_model(**inputs)
-        #     scores = output.iou_scores.squeeze()
+        inputs = processor(
+            images=np.array(raw_image), input_points=[input_points], input_labels=input_labels, return_tensors="pt"
+        ).to(device)
 
 
     if pytorch_dump_folder is not None:
