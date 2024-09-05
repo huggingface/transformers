2494e646e7510c6417447e4c9b6327797a844f1b
younesbelkada <younesbelkada@gmail.com>
2024-09-05 07:20:45 +0000

    add comment

--- src/transformers/kernels/falcon_mamba/selective_scan_with_ln_interface.py
+++ src/transformers/kernels/falcon_mamba/selective_scan_with_ln_interface.py
@@ -12,6 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+# Original code from: https://github.com/state-spaces/mamba/blob/main/mamba_ssm/ops/selective_scan_interface.py
 
 import torch
 import torch.nn.functional as F
