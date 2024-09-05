3116d78cf90ad7366820ec182bdd1c13320a1f5d
raushan <raushan@huggingface.co>
2024-09-05 14:36:48 +0200

    working draft for TF `generate()` w/o cache

--- src/transformers/models/mllama/configuration_mllama.py
+++ src/transformers/models/mllama/configuration_mllama.py
@@ -308,22 +308,4 @@ class MllamaConfig(PretrainedConfig):
         elif isinstance(text_config, MllamaCrossAttentionTextConfig):
             self.text_config = text_config
 
-        self._vocab_size = self.text_config.vocab_size
         super().__init__(**kwargs)
-
-        @property
-        def vocab_size(self):
-            warnings.warn(
-                "The `vocab_size` attribute is deprecated and will be removed in v4.42, Please use `text_config.vocab_size` instead.",
-                FutureWarning,
-            )
-            return self._vocab_size
-
-        @vocab_size.setter
-        def vocab_size(self, value):
-            self._vocab_size = value
-
-        def to_dict(self):
-            output = super().to_dict()
-            output.pop("_vocab_size", None)
-            return output
