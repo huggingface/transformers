d245749b71a8caa6fea2823b7e98bbecfcfd8982
pglorio <85982602+pglorio@users.noreply.github.com>
2024-09-06 12:54:45 -0700

    Update src/transformers/models/zamba/modeling_zamba.py
    Co-authored-by: Arthur <48595927+ArthurZucker@users.noreply.github.com>

--- src/transformers/models/zamba/modeling_zamba.py
+++ src/transformers/models/zamba/modeling_zamba.py
@@ -79,16 +79,6 @@ logger = logging.get_logger(__name__)
 _CONFIG_FOR_DOC = "ZambaConfig"
 
 
-def _get_unpad_data(attention_mask):
-    seqlens_in_batch = attention_mask.sum(dim=-1, dtype=torch.int32)
-    indices = torch.nonzero(attention_mask.flatten(), as_tuple=False).flatten()
-    max_seqlen_in_batch = seqlens_in_batch.max().item()
-    cu_seqlens = F.pad(torch.cumsum(seqlens_in_batch, dim=0, dtype=torch.int32), (1, 0))
-    return (
-        indices,
-        cu_seqlens,
-        max_seqlen_in_batch,
-    )
 
 
 # Copied from transformers.models.llama.modeling_llama.LlamaRMSNorm with Llama->Zamba
