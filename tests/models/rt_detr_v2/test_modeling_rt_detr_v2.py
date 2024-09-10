a2116ea8795402fa1b9bf47dacddfb3fa44109f7
sangbumchoi <danielsejong55@gmail.com>
2024-09-10 05:19:08 +0000

    make to v2

--- tests/models/rt_detr_v2/test_modeling_rt_detr_v2.py
+++ tests/models/rt_detr_v2/test_modeling_rt_detr_v2.py
@@ -12,7 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Testing suite for the PyTorch RT_DETR model."""
+"""Testing suite for the PyTorch RT_DETRV2 model."""
 
 import inspect
 import math
