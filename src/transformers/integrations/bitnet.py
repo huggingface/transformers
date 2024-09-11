93c993d3cf5f5b1a2473c926c58d9090b73cb8ce
MekkCyber <mekk.cyber@gmail.com>
2024-09-11 10:19:55 +0000

    adding doc to tree

--- src/transformers/integrations/bitnet.py
+++ src/transformers/integrations/bitnet.py
@@ -13,7 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from ..utils import is_accelerate_available, is_torch_available, logging
+from ..utils import is_accelerate_available, is_torch_available,logging
+
 
 if is_accelerate_available():
     from accelerate import init_empty_weights
