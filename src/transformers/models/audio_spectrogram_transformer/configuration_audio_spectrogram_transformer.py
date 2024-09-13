6b6ca571a61cf6015e52aee40359c0136d1a9d24
Joao Gante <joao@huggingface.co>
2024-09-13 16:41:27 +0000

    missing this one

--- src/transformers/models/audio_spectrogram_transformer/configuration_audio_spectrogram_transformer.py
+++ src/transformers/models/audio_spectrogram_transformer/configuration_audio_spectrogram_transformer.py
@@ -14,7 +14,7 @@
 # limitations under the License.
 """Audio Spectogram Transformer (AST) model configuration"""
 
-from typing import Any, List, Tuple
+from typing import Any, Dict
 
 from ...configuration_utils import PretrainedConfig
 from ...utils import logging
@@ -124,5 +124,5 @@ class ASTConfig(PretrainedConfig):
     # Overwritten from the parent class: AST is not compatible with `generate`, but has a config parameter sharing the
     # same name (`max_length`). Sharing the same name triggers checks regarding the config -> generation_config
     # generative parameters deprecation cycle, overwriting this function prevents this from happening.
-    def _get_non_default_generation_parameters(self) -> List[Tuple[str, Any, Any]]:
-        return []
+    def _get_non_default_generation_parameters(self) -> Dict[str, Any]:
+        return {}
