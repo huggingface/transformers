9af7fd4cafab9401b40a732ac400bcc04adfff5f
Matt <rocketknight1@gmail.com>
2024-09-17 16:18:22 +0100

    Cleanup return_text and return_full_text options in TextGenerationPipeline

--- src/transformers/pipelines/text_generation.py
+++ src/transformers/pipelines/text_generation.py
@@ -174,14 +174,15 @@ class TextGenerationPipeline(Pipeline):
 
         postprocess_params = {}
         if return_full_text is not None and return_type is None:
-            if return_text is not None:
-                raise ValueError("`return_text` is mutually exclusive with `return_full_text`")
             if return_tensors is not None:
                 raise ValueError("`return_full_text` is mutually exclusive with `return_tensors`")
-            return_type = ReturnType.FULL_TEXT if return_full_text else ReturnType.NEW_TEXT
-        if return_tensors is not None and return_type is None:
-            if return_text is not None:
+            return_type = ReturnType.FULL_TEXT
+        elif return_text is not None and return_type is None:
+            if return_tensors is not None:
                 raise ValueError("`return_text` is mutually exclusive with `return_tensors`")
+            return_type = ReturnType.NEW_TEXT
+        if return_tensors is not None and return_type is None:
+            # No incompatibility error needed here because it should already be caught above
             return_type = ReturnType.TENSORS
         if return_type is not None:
             postprocess_params["return_type"] = return_type
@@ -223,13 +224,13 @@ class TextGenerationPipeline(Pipeline):
                 of dicts with "role" and "content" keys, can be passed, or a list of such chats. When chats are passed,
                 the model's chat template will be used to format them before passing them to the model.
             return_tensors (`bool`, *optional*, defaults to `False`):
-                Whether or not to return the tensors of predictions (as token indices) in the outputs. If set to
+                Returns the tensors of predictions (as token indices) in the outputs. If set to
                 `True`, the decoded text is not returned.
             return_text (`bool`, *optional*, defaults to `True`):
-                Whether or not to return the decoded texts in the outputs.
+                Returns the decoded texts in the outputs.
             return_full_text (`bool`, *optional*, defaults to `True`):
-                If set to `False` only added text is returned, otherwise the full text is returned. Only meaningful if
-                *return_text* is set to True.
+                Returns the full text, including the prompt and the decoded output. This option
+                overrides `return_text`.
             clean_up_tokenization_spaces (`bool`, *optional*, defaults to `True`):
                 Whether or not to clean up the potential extra spaces in the text output.
             continue_final_message( `bool`, *optional*): This indicates that you want the model to continue the
