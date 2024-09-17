4ae1bd8e1b3037b605191ef71d8d0fa30f8773ba
Cheng Luo <451714048@qq.com>
2024-09-16 23:01:12 -0700

    Update modeling_gemma2.py

--- src/transformers/models/gemma2/modeling_gemma2.py
+++ src/transformers/models/gemma2/modeling_gemma2.py
@@ -201,7 +201,32 @@ class Gemma2MLP(nn.Module):
         self.down_proj = nn.Linear(self.intermediate_size, self.hidden_size, bias=False)
         self.act_fn = ACT2FN[config.hidden_activation]
 
+        self.mini_s = 8
+        self.chunk_size = 4096
+        self.chunk_mode = True
+
     def forward(self, x):
+        
+        bsz, q_len, _ = x.size()
+
+        if self.chunk_mode:
+            chunk_size = self.chunk_size
+        else:
+            chunk_size = math.ceil(q_len / self.mini_s)
+
+        x_list = list(x.split(chunk_size, dim=1))
+            
+        output_list = [None for _ in range(len(x_list))]
+
+        for i in range(len(x_list)):
+            output = self.minis_forward(x_list[i])
+            output_list[i] = output
+            
+        down_proj = torch.cat(output_list, dim=1)
+
+        return down_proj 
+
+    def minis_forward(self, x):
         return self.down_proj(self.act_fn(self.gate_proj(x)) * self.up_proj(x))
 
 
@@ -942,6 +967,85 @@ class Gemma2Model(Gemma2PreTrainedModel):
         )
         return causal_mask
 
+import torch.nn.functional as F
+
+class _LM_head(torch.autograd.Function):
+
+    @classmethod
+    def forward(cls, ctx, hidden_states, indices, weights):
+        logits = F.linear(hidden_states, weights).float()
+        loss_fct = torch.nn.CrossEntropyLoss(reduction="sum")
+        loss_i = loss_fct(logits, indices)
+
+        weights.count += 1
+
+        ctx.save_for_backward(hidden_states, indices, weights)
+        
+        return loss_i
+
+    @classmethod
+    def backward(cls, ctx, dneg_logprobs):
+        """We know d(-log(p[i])/dlogit[k] = -id_mat[i,k] + p[k]
+        so we initialize the gradient as neg_logprobs, so we can just exponentiate
+        to get p[k], which is most of what we need...  neg_logprobs will be
+        modified in place to become the gradient we want
+        """
+        # load saved tensors
+        hidden_states, indices, weights = ctx.saved_tensors
+        weights.count -= 1
+
+        ignore_index = -100
+        mask = indices != ignore_index
+        reverse_mask = indices == ignore_index
+
+        if not mask.any():
+            # If all indices are -100, return zero gradients
+            return torch.zeros_like(hidden_states), None, None
+
+        logits = F.linear(hidden_states, weights).float()
+
+        grad_input = F.softmax(logits, dim=-1)
+        logits = None
+        grad_input[mask, indices[mask]] -= 1
+        # grad_input[mask] /= batch_size
+        grad_input[reverse_mask] = 0
+        grad_input *= dneg_logprobs
+        grad_input = grad_input.to(hidden_states.dtype)
+        
+        if hasattr(weights, 'grad') and weights.grad != None:
+            torch.addmm(
+                    weights.grad,
+                    grad_input.T,
+                    hidden_states,
+                    out=weights.grad,
+                )
+        else:
+            weights.grad = grad_input.T @ hidden_states
+            
+        grad_input = grad_input @ weights
+
+        if weights.count == 0:
+            return grad_input, None, weights.grad
+        else:
+            return grad_input, None, None
+
+class LMheadWarpper(nn.Module):
+    def __init__(
+        self,
+        original_weight = None
+    ):
+        super().__init__()
+        if original_weight is None:
+            self.LM_head_weight = nn.Parameter(torch.empty(hidden_size, vocab_size))
+        else:
+            self.LM_head_weight = original_weight
+        self.LM_head = _LM_head.apply
+        self.LM_head_weight.count = 0
+
+    def forward(self, hidden_states, labels):
+        ignore_index = -100
+        loss = self.LM_head(hidden_states, labels, self.LM_head_weight)
+        return loss
 
 class Gemma2ForCausalLM(Gemma2PreTrainedModel):
     _tied_weights_keys = ["lm_head.weight"]
@@ -972,6 +1076,46 @@ class Gemma2ForCausalLM(Gemma2PreTrainedModel):
 
     def get_decoder(self):
         return self.model
+
+    def minis_processing(self, hidden_states, labels):
+        bsz, q_len, hidden_size = hidden_states.size()
+        chunk_size = max(q_len // self.mini_s, 64)
+
+        if labels is None:
+            hidden_states = hidden_states[..., -1:, :]
+            logits = self.lm_head(hidden_states)
+            logits = logits.float()
+            return logits, None
+
+        hidden_states = hidden_states[..., :-1, :]
+
+        labels = labels[..., 1:].contiguous()
+        labels = labels.to(hidden_states.device)
+
+        LMhead = LMheadWarpper(self.lm_head.weight)
+
+        hidden_states_list = list(hidden_states.split(chunk_size, dim=1))
+        labels_list = list(labels.split(chunk_size, dim=1))
+        
+        loss = None
+        for i in range(len(hidden_states_list)):
+
+
+            shift_hidden_states = hidden_states_list[i].contiguous()
+            shift_hidden_states = shift_hidden_states.view(-1, hidden_size)
+            shift_labels = labels_list[i].contiguous()
+            shift_labels = shift_labels.view(-1)
+
+            loss_i = LMhead(shift_hidden_states, shift_labels)
+
+            if not torch.isnan(loss_i):
+                if loss is None:
+                    loss = loss_i
+                else:
+                    loss = loss + loss_i
+
+        loss = loss / torch.sum(torch.ne(labels, -100))
+        return None, loss
 
     @add_start_docstrings_to_model_forward(GEMMA2_INPUTS_DOCSTRING)
     @replace_return_docstrings(output_type=CausalLMOutputWithPast, config_class=_CONFIG_FOR_DOC)
@@ -1045,33 +1189,37 @@ class Gemma2ForCausalLM(Gemma2PreTrainedModel):
         )
 
         hidden_states = outputs[0]
-        if labels is None and not is_torchdynamo_compiling():
-            logger.warning_once(
-                "Starting from v4.46, the `logits` model output will have the same type as the model (except at train time, where it will always be FP32)"
-            )
-        # Only compute necessary logits, and do not upcast them to float if we are not computing the loss
-        logits = self.lm_head(hidden_states[:, -num_logits_to_keep:, :])
-        if self.config.final_logit_softcapping is not None:
-            logits = logits / self.config.final_logit_softcapping
-            logits = torch.tanh(logits)
-            logits = logits * self.config.final_logit_softcapping
 
-        # TODO: remove the float() operation in v4.46
-        logits = logits.float()
-        loss = None
-        if labels is not None:
-            # Upcast to float if we need to compute the loss to avoid potential precision issues
-            logits = logits.float()
-            # Shift so that tokens < n predict n
-            shift_logits = logits[..., :-1, :].contiguous()
-            shift_labels = labels[..., 1:].contiguous()
-            # Flatten the tokens
-            loss_fct = CrossEntropyLoss()
-            shift_logits = shift_logits.view(-1, self.config.vocab_size)
-            shift_labels = shift_labels.view(-1)
-            # Enable model parallelism
-            shift_labels = shift_labels.to(shift_logits.device)
-            loss = loss_fct(shift_logits, shift_labels)
+        self.mini_s = 128
+        logits, loss = self.minis_processing(hidden_states, labels)
+
+        # if labels is None and not is_torchdynamo_compiling():
+        #     logger.warning_once(
+        #         "Starting from v4.46, the `logits` model output will have the same type as the model (except at train time, where it will always be FP32)"
+        #     )
+        # # Only compute necessary logits, and do not upcast them to float if we are not computing the loss
+        # logits = self.lm_head(hidden_states[:, -num_logits_to_keep:, :])
+        # if self.config.final_logit_softcapping is not None:
+        #     logits = logits / self.config.final_logit_softcapping
+        #     logits = torch.tanh(logits)
+        #     logits = logits * self.config.final_logit_softcapping
+
+        # # TODO: remove the float() operation in v4.46
+        # logits = logits.float()
+        # loss = None
+        # if labels is not None:
+        #     # Upcast to float if we need to compute the loss to avoid potential precision issues
+        #     logits = logits.float()
+        #     # Shift so that tokens < n predict n
+        #     shift_logits = logits[..., :-1, :].contiguous()
+        #     shift_labels = labels[..., 1:].contiguous()
+        #     # Flatten the tokens
+        #     loss_fct = CrossEntropyLoss()
+        #     shift_logits = shift_logits.view(-1, self.config.vocab_size)
+        #     shift_labels = shift_labels.view(-1)
+        #     # Enable model parallelism
+        #     shift_labels = shift_labels.to(shift_logits.device)
+        #     loss = loss_fct(shift_logits, shift_labels)
 
         if not return_dict:
             output = (logits,) + outputs[1:]
