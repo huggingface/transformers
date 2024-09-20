ff16af0165678c3942cc9009e9cde2240f4bd6c2
Cyril Vallez <cyril.vallez@gmail.com>
2024-09-20 18:25:25 +0200

    update cache conversion in test

--- tests/generation/test_utils.py
+++ tests/generation/test_utils.py
@@ -1816,7 +1816,10 @@ class GenerationTesterMixin:
                         )
 
             new_cache = new_results.past_key_values
-            legacy_cache_converted = cache_cls.from_legacy_cache(legacy_results.past_key_values)
+            if cache_cls == DynamicSlidingWindowCache:
+                legacy_cache_converted = cache_cls.from_legacy_cache(config.sliding_window, legacy_results.past_key_values)
+            else:
+                legacy_cache_converted = cache_cls.from_legacy_cache(legacy_results.past_key_values)
             for layer_idx in range(len(new_cache)):
                 for kv_idx in range(len(new_cache[layer_idx])):
                     # TODO: @raushan, please look into this for new cache format
