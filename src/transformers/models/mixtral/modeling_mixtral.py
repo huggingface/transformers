87e49be3b3c7a44560ddc8e9df238311ecd382f2
PhilipMay <philip@may.la>
2024-09-21 09:06:15 +0200

    fix return type

--- src/transformers/models/mixtral/modeling_mixtral.py
+++ src/transformers/models/mixtral/modeling_mixtral.py
@@ -124,7 +124,7 @@ def _prepare_4d_causal_attention_mask_with_cache_position(
 
 def load_balancing_loss_func(
     gate_logits: torch.Tensor, num_experts: torch.Tensor = None, top_k=2, attention_mask: Optional[torch.Tensor] = None
-) -> float:
+) -> torch.Tensor:
     r"""
     Computes auxiliary load balancing loss as in Switch Transformer - implemented in Pytorch.
 
