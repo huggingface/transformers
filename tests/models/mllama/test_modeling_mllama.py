Fix signature, but get error with None gradient

tests/models/mllama/test_modeling_mllama.py
@@ -392,23 +392,23 @@ class MllamaForConditionalGenerationModelTest(ModelTesterMixin, GenerationTester
                 out_embeds = model(inputs_embeds=inputs_embeds, **inputs)[0]
             self.assertTrue(torch.allclose(out_embeds, out_ids))
 
-    @unittest.skip(
-        reason="This architecure seem to not compute gradients properly when using GC, check: https://github.com/huggingface/transformers/pull/27124"
-    )
-    def test_training_gradient_checkpointing(self):
-        pass
+    # @unittest.skip(
+    #     reason="This architecure seem to not compute gradients properly when using GC, check: https://github.com/huggingface/transformers/pull/27124"
+    # )
+    # def test_training_gradient_checkpointing(self):
+    #     pass
 
-    @unittest.skip(
-        reason="This architecure seem to not compute gradients properly when using GC, check: https://github.com/huggingface/transformers/pull/27124"
-    )
-    def test_training_gradient_checkpointing_use_reentrant(self):
-        pass
+    # @unittest.skip(
+    #     reason="This architecure seem to not compute gradients properly when using GC, check: https://github.com/huggingface/transformers/pull/27124"
+    # )
+    # def test_training_gradient_checkpointing_use_reentrant(self):
+    #     pass
 
-    @unittest.skip(
-        reason="This architecure seem to not compute gradients properly when using GC, check: https://github.com/huggingface/transformers/pull/27124"
-    )
-    def test_training_gradient_checkpointing_use_reentrant_false(self):
-        pass
+    # @unittest.skip(
+    #     reason="This architecure seem to not compute gradients properly when using GC, check: https://github.com/huggingface/transformers/pull/27124"
+    # )
+    # def test_training_gradient_checkpointing_use_reentrant_false(self):
+    #     pass
 
     @unittest.skip(reason="Mllama has only SDPA layers and returns no attention outputs")
     def test_attention_outputs(self):