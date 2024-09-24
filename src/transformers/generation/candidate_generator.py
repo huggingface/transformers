9796f308be200634c7010e740f1c73f5e8858372
Korat, Daniel <daniel.korat@intel.com>
2024-09-24 09:21:52 -0700

    fix prev_tokens branching

--- src/transformers/generation/candidate_generator.py
+++ src/transformers/generation/candidate_generator.py
@@ -422,54 +422,45 @@ class AssistedCandidateGeneratorDifferentTokenizers(AssistedCandidateGenerator):
         }
         remove_from_pkv = 0
 
-        if self.prev_tokens is None:
+        if self.prev_tokens is not None and self.prev_target_ids.shape[1] > self.target_lookbehind:
+            # input_ids contains all target prompt input ids and some new target input ids
+            start_index_in_target_window = self.prev_target_ids.shape[1] - target_lookbehind
+
+            new_assistant_ids = self.convert_token_ids(
+                input_ids[:, start_index_in_target_window:], **convert_kwargs
+            )
+            prompt_use_length = new_assistant_ids.shape[1]
+            prompt_use = self.prev_assistant_ids[:, -prompt_use_length:]
+
+            discrepancy_length, new_tokens_only, discrepancy_only = (
+                AssistedCandidateGeneratorDifferentTokenizers._get_tokens_diag(prompt_use, new_assistant_ids)
+            )
+            assistant_input_ids = self.prev_assistant_ids
+
+            if new_tokens_only is not None:
+                if discrepancy_length > 0 and discrepancy_only.shape[1] > 0:
+                    if discrepancy_length == discrepancy_only.shape[1]:
+                        assistant_input_ids[:, -discrepancy_length:] = discrepancy_only
+
+                    elif discrepancy_length > discrepancy_only.shape[1]:
+                        discrepancy_length_diff = discrepancy_length - discrepancy_only.shape[1]
+                        assistant_input_ids = assistant_input_ids[:, :-discrepancy_length_diff]
+                        assistant_input_ids[:, -discrepancy_only.shape[1] :] = discrepancy_only
+
+                    remove_from_pkv = discrepancy_length
+
+                if new_tokens_only.shape[1] > 0:
+                    assistant_input_ids = torch.cat([assistant_input_ids, new_tokens_only], dim=-1)
+            else:
+                # edge case: in case of no intersection between prompt and new_assistant_ids
+                assistant_input_ids = torch.cat([assistant_input_ids, new_assistant_ids], dim=-1)
+
+        else:
             assistant_input_ids = self.convert_token_ids(input_ids, **convert_kwargs)
             self.prev_target_ids = input_ids
-            self.prev_assistant_ids = assistant_input_ids
-            new_cur_len = assistant_input_ids.shape[-1]
-        else:
-            # input_ids contains all target prompt input ids and some new target input ids
-            if self.prev_target_ids.shape[1] > target_lookbehind:
-                start_index_in_target_window = self.prev_target_ids.shape[1] - target_lookbehind
-
-                new_assistant_ids = self.convert_token_ids(
-                    input_ids[:, start_index_in_target_window:], **convert_kwargs
-                )
-                prompt_use_length = new_assistant_ids.shape[1]
-                prompt_use = self.prev_assistant_ids[:, -prompt_use_length:]
-
-                discrepancy_length, new_tokens_only, discrepancy_only = (
-                    AssistedCandidateGeneratorDifferentTokenizers._get_tokens_diag(prompt_use, new_assistant_ids)
-                )
-                assistant_input_ids = self.prev_assistant_ids
-
-                if new_tokens_only is not None:
-                    if discrepancy_length > 0 and discrepancy_only.shape[1] > 0:
-                        if discrepancy_length == discrepancy_only.shape[1]:
-                            assistant_input_ids[:, -discrepancy_length:] = discrepancy_only
-
-                        elif discrepancy_length > discrepancy_only.shape[1]:
-                            discrepancy_length_diff = discrepancy_length - discrepancy_only.shape[1]
-                            assistant_input_ids = assistant_input_ids[:, :-discrepancy_length_diff]
-                            assistant_input_ids[:, -discrepancy_only.shape[1] :] = discrepancy_only
-
-                        remove_from_pkv = discrepancy_length
-
-                    if new_tokens_only.shape[1] > 0:
-                        assistant_input_ids = torch.cat([assistant_input_ids, new_tokens_only], dim=-1)
-                else:
-                    # edge case: in case of no intersection between prompt and new_assistant_ids
-                    assistant_input_ids = torch.cat([assistant_input_ids, new_assistant_ids], dim=-1)
-
-                self.prev_assistant_ids = assistant_input_ids
-            else:
-                assistant_input_ids = self.convert_token_ids(input_ids, **convert_kwargs)
-                assistant_input_ids = torch.cat(
-                    [self.prev_assistant_ids, assistant_input_ids[:, self.prev_assistant_ids.shape[1] :]], dim=1
-                )
-
-            new_cur_len = assistant_input_ids.shape[-1]
-
+            
+        self.prev_assistant_ids = assistant_input_ids
+        new_cur_len = assistant_input_ids.shape[-1]
         min_new_tokens = max(min(max_new_tokens, self.main_model_min_length - new_cur_len), 0)
 
         # 1. If it is not the first round of candidate generation, prepare the inputs based on the input_ids length
