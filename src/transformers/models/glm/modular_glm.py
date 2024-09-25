644300433c26ea7989012bd8838ae2e1e4aaa0aa
Cyril Vallez <cyril.vallez@gmail.com>
2024-09-25 15:51:20 +0200

    Add all attentions modules

--- src/transformers/models/glm/modular_glm.py
+++ src/transformers/models/glm/modular_glm.py
@@ -13,26 +13,20 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from typing import Optional, Tuple, Union
+from typing import Optional, Tuple
 import math
 
 import torch
 import torch.nn as nn
 import torch.utils.checkpoint
-from torch.nn import CrossEntropyLoss
 
-from ...activations import ACT2FN
-from ...cache_utils import Cache, HybridCache
+from ...cache_utils import Cache
+
 from ...configuration_utils import PretrainedConfig
-from ...modeling_outputs import (
-    BaseModelOutputWithPast,
-    CausalLMOutputWithPast,
-)
+
 from ...utils import (
     is_flash_attn_2_available,
-    is_flash_attn_greater_or_equal,
     is_flash_attn_greater_or_equal_2_10,
-    is_torchdynamo_compiling,
     logging,
 )
 from ..llama.modeling_llama import (
@@ -44,9 +38,7 @@ from ..llama.modeling_llama import (
     repeat_kv,
 )
 from ..phi3.modeling_phi3 import (
-    Phi3Config,
     Phi3MLP,
-    Phi3Attention,
     Phi3DecoderLayer
 )
 
@@ -58,7 +50,68 @@ if is_flash_attn_2_available():
 logger = logging.get_logger(__name__)
 
 
-class GlmConfig(Phi3Config):
+# class GlmConfig(Phi3Config):
+#     model_type = "glm"
+
+#     def __init__(
+#         self,
+#         # Phi3 args
+#         vocab_size=151552,
+#         hidden_size=4096,
+#         intermediate_size=13696,
+#         num_hidden_layers=40,
+#         num_attention_heads=32,
+#         num_key_value_heads=2,
+#         resid_pdrop=0.0,
+#         attention_dropout=0.0,
+#         hidden_act="silu",
+#         max_position_embeddings=131072,
+#         initializer_range=0.02,
+#         rms_norm_eps=1e-5,
+#         use_cache=True,
+#         tie_word_embeddings=False,
+#         rope_theta=10000.0,
+#         rope_scaling={"rope_type": "linear", "factor": 1.,},
+#         pad_token_id=151329,
+#         eos_token_id=[151329, 151336, 151338],
+#         bos_token_id=None,
+#         # Additionnal args
+#         head_dim=128,
+#         partial_rotary_factor=0.5,
+#         attention_bias=True,
+#         **kwargs,
+#     ):
+#         super().__init__(
+#             vocab_size=vocab_size,
+#             hidden_size=hidden_size,
+#             intermediate_size=intermediate_size,
+#             num_hidden_layers=num_hidden_layers,
+#             num_attention_heads=num_attention_heads,
+#             num_key_value_heads=num_key_value_heads,
+#             resid_pdrop=resid_pdrop,
+#             attention_dropout=attention_dropout,
+#             hidden_act=hidden_act,
+#             max_position_embeddings=max_position_embeddings,
+#             initializer_range=initializer_range,
+#             rms_norm_eps=rms_norm_eps,
+#             use_cache=use_cache,
+#             tie_word_embeddings=tie_word_embeddings,
+#             rope_theta=rope_theta,
+#             rope_scaling=rope_scaling,
+#             pad_token_id=pad_token_id,
+#             bos_token_id=bos_token_id,
+#             eos_token_id=eos_token_id,
+#             **kwargs,
+#         )
+#         self.head_dim = head_dim
+#         self.partial_rotary_factor = partial_rotary_factor
+#         self.attention_bias = attention_bias
+#         del self.embd_pdrop
+#         del self.original_max_mposition_embeddings
+#         del self.sliding_window
+
+
+class GlmConfig(PretrainedConfig):
     model_type = "glm"
 
     def __init__(
@@ -80,9 +133,9 @@ class GlmConfig(Phi3Config):
         tie_word_embeddings=False,
         rope_theta=10000.0,
         rope_scaling={"rope_type": "linear", "factor": 1.,},
-        pad_token_id=0,
-        eos_token_id=1,
-        bos_token_id=2,
+        pad_token_id=151329,
+        eos_token_id=[151329, 151336, 151338],
+        bos_token_id=None,
         # Additionnal args
         head_dim=128,
         partial_rotary_factor=0.5,
@@ -90,34 +143,33 @@ class GlmConfig(Phi3Config):
         **kwargs,
     ):
         super().__init__(
-            vocab_size=vocab_size,
-            hidden_size=hidden_size,
-            intermediate_size=intermediate_size,
-            num_hidden_layers=num_hidden_layers,
-            num_attention_heads=num_attention_heads,
-            num_key_value_heads=num_key_value_heads,
-            resid_pdrop=resid_pdrop,
-            attention_dropout=attention_dropout,
-            hidden_act=hidden_act,
-            max_position_embeddings=max_position_embeddings,
-            initializer_range=initializer_range,
-            rms_norm_eps=rms_norm_eps,
-            use_cache=use_cache,
             tie_word_embeddings=tie_word_embeddings,
-            rope_theta=rope_theta,
-            rope_scaling=rope_scaling,
             pad_token_id=pad_token_id,
             bos_token_id=bos_token_id,
             eos_token_id=eos_token_id,
             **kwargs,
         )
+        self.vocab_size = vocab_size
+        self.hidden_size=hidden_size
+        self.intermediate_size=intermediate_size
+        self.num_hidden_layers=num_hidden_layers
+        self.num_attention_heads=num_attention_heads
+        self.num_key_value_heads=num_key_value_heads
+        self.resid_pdrop=resid_pdrop
+        self.attention_dropout=attention_dropout
+        self.hidden_act=hidden_act
+        self.max_position_embeddings=max_position_embeddings
+        self.initializer_range=initializer_range
+        self.rms_norm_eps=rms_norm_eps
+        self.use_cache=use_cache
+        self.initializer_range=initializer_range
+        self.rms_norm_eps=rms_norm_eps
+        self.use_cache=use_cache
+        self.rope_theta=rope_theta
+        self.rope_scaling=rope_scaling
         self.head_dim = head_dim
         self.partial_rotary_factor = partial_rotary_factor
         self.attention_bias = attention_bias
-        del self.embd_pdrop
-        del self.original_max_mposition_embeddings
-        del self.sliding_window
-
 
 
 
@@ -134,7 +186,7 @@ class GlmMLP(Phi3MLP):
     pass
 
 
-class GlmAttention(Phi3Attention):
+class GlmAttention(nn.Module):
 
     def __init__(self, config: GlmConfig, layer_idx: Optional[int] = None):
         super().__init__()
@@ -170,6 +222,7 @@ class GlmAttention(Phi3Attention):
         self,
         hidden_states: torch.Tensor,
         attention_mask: Optional[torch.Tensor] = None,
+        position_ids: Optional[torch.LongTensor] = None,
         past_key_value: Optional[Cache] = None,
         output_attentions: bool = False,
         use_cache: bool = False,
@@ -207,7 +260,7 @@ class GlmAttention(Phi3Attention):
             causal_mask = attention_mask[:, :, :, : key_states.shape[-2]]
             attn_weights += causal_mask
 
-        # MAYBE upcast attention to fp32
+        # upcast attention to fp32
         attn_weights = nn.functional.softmax(attn_weights, dim=-1, dtype=torch.float32).to(value_states.dtype)
         attn_weights = nn.functional.dropout(attn_weights, p=self.attention_dropout, training=self.training)
 
@@ -230,9 +283,207 @@ class GlmAttention(Phi3Attention):
         return attn_output, attn_weights, past_key_value
     
 
+class GlmFlashAttention2(GlmAttention):
+    """
+    GLM flash attention module. This module inherits from `GlmAttention` as the weights of the module stays
+    untouched. The only required change would be on the forward pass where it needs to correctly call the public API of
+    flash attention and deal with padding tokens in case the input contains any of them.
+    """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # TODO: Should be removed once Flash Attention for RoCm is bumped to 2.1.
+        # flash_attn<2.1 generates top-left aligned causal mask, while what is needed here is bottom-right alignement, that was made default for flash_attn>=2.1. This attribute is used to handle this difference. Reference: https://github.com/Dao-AILab/flash-attention/releases/tag/v2.1.0.
+        # Beware that with flash_attn<2.1, using q_seqlen != k_seqlen (except for the case q_seqlen == 1) produces a wrong mask (top-left).
+        self._flash_attn_uses_top_left_mask = not is_flash_attn_greater_or_equal_2_10()
+
+    def forward(
+        self,
+        hidden_states: torch.Tensor,
+        attention_mask: Optional[torch.Tensor] = None,
+        position_ids: Optional[torch.LongTensor] = None,
+        past_key_value: Optional[Cache] = None,
+        output_attentions: bool = False,
+        use_cache: bool = False,
+        cache_position: Optional[torch.LongTensor] = None,
+        position_embeddings: Optional[Tuple[torch.Tensor, torch.Tensor]] = None,
+    ) -> Tuple[torch.Tensor, Optional[torch.Tensor], Optional[Tuple[torch.Tensor]]]:
+
+        output_attentions = False
+
+        bsz, q_len, _ = hidden_states.size()
+
+        qkv = self.qkv_proj(hidden_states)
+        query_pos = self.num_heads * self.head_dim
+        query_states = qkv[..., :query_pos]
+        key_states = qkv[..., query_pos : query_pos + self.num_key_value_heads * self.head_dim]
+        value_states = qkv[..., query_pos + self.num_key_value_heads * self.head_dim :]
+
+        # Flash attention requires the input to have the shape
+        # batch_size x seq_length x head_dim x hidden_dim
+        # therefore we just need to keep the original shape
+        query_states = query_states.view(bsz, q_len, self.num_heads, self.head_dim).transpose(1, 2)
+        key_states = key_states.view(bsz, q_len, self.num_key_value_heads, self.head_dim).transpose(1, 2)
+        value_states = value_states.view(bsz, q_len, self.num_key_value_heads, self.head_dim).transpose(1, 2)
+
+        cos, sin = position_embeddings
+        query_states, key_states = apply_rotary_pos_emb(query_states, key_states, cos, sin)
+
+        if past_key_value is not None:
+            cache_kwargs = {"sin": sin, "cos": cos, "cache_position": cache_position}  # Specific to RoPE models
+            key_states, value_states = past_key_value.update(key_states, value_states, self.layer_idx, cache_kwargs)
+
+        # repeat k/v heads if n_kv_heads < n_heads
+        key_states = repeat_kv(key_states, self.num_key_value_groups)
+        value_states = repeat_kv(value_states, self.num_key_value_groups)
+
+        attn_dropout = self.attention_dropout if self.training else 0.0
+
+        # In PEFT, usually we cast the layer norms in float32 for training stability reasons
+        # therefore the input hidden states gets silently casted in float32. Hence, we need
+        # cast them back in the correct dtype just to be sure everything works as expected.
+        # This might slowdown training & inference so it is recommended to not cast the LayerNorms
+        # in fp32.
+
+        if query_states.dtype == torch.float32:
+            if torch.is_autocast_enabled():
+                target_dtype = torch.get_autocast_gpu_dtype()
+            # Handle the case where the model is quantized
+            elif hasattr(self.config, "_pre_quantization_dtype"):
+                target_dtype = self.config._pre_quantization_dtype
+            else:
+                target_dtype = self.qkv_proj.weight.dtype
+
+            logger.warning_once(
+                f"The input hidden states seems to be silently casted in float32, this might be related to"
+                f" the fact you have upcasted embedding or layer norm layers in float32. We will cast back the input in"
+                f" {target_dtype}."
+            )
+
+            query_states = query_states.to(target_dtype)
+            key_states = key_states.to(target_dtype)
+            value_states = value_states.to(target_dtype)
+
+        # Reashape to the expected shape for Flash Attention
+        query_states = query_states.transpose(1, 2)
+        key_states = key_states.transpose(1, 2)
+        value_states = value_states.transpose(1, 2)
+
+        attn_output = _flash_attention_forward(
+            query_states,
+            key_states,
+            value_states,
+            attention_mask,
+            q_len,
+            position_ids=position_ids,
+            dropout=attn_dropout,
+            sliding_window=getattr(self.config, "sliding_window", None),
+            use_top_left_mask=self._flash_attn_uses_top_left_mask,
+            is_causal=self.is_causal,
+        )
+
+        attn_output = attn_output.reshape(bsz, q_len, self.hidden_size).contiguous()
+        attn_output = self.o_proj(attn_output)
+
+        if not output_attentions:
+            attn_weights = None
+
+        return attn_output, attn_weights, past_key_value
+    
+
+
+class GlmSdpaAttention(GlmAttention):
+    """
+    GLM attention module using torch.nn.functional.scaled_dot_product_attention. This module inherits from
+    `GlmAttention` as the weights of the module stays untouched. The only changes are on the forward pass to adapt to
+    SDPA API.
+    """
+
+    def forward(
+        self,
+        hidden_states: torch.Tensor,
+        attention_mask: Optional[torch.Tensor] = None,
+        position_ids: Optional[torch.LongTensor] = None,
+        past_key_value: Optional[Cache] = None,
+        output_attentions: bool = False,
+        use_cache: bool = False,
+        cache_position: Optional[torch.LongTensor] = None,
+        position_embeddings: Optional[Tuple[torch.Tensor, torch.Tensor]] = None,
+    ) -> Tuple[torch.Tensor, Optional[torch.Tensor], Optional[Tuple[torch.Tensor]]]:
+        if output_attentions:
+            logger.warning_once(
+                "GlmModel is using GlmSdpaAttention, but `torch.nn.functional.scaled_dot_product_attention` does not support `output_attentions=True`. Falling back to the manual attention implementation, "
+                'but specifying the manual implementation will be required from Transformers version v5.0.0 onwards. This warning can be removed using the argument `attn_implementation="eager"` when loading the model.'
+            )
+            return super().forward(
+                hidden_states=hidden_states,
+                attention_mask=attention_mask,
+                position_ids=position_ids,
+                past_key_value=past_key_value,
+                output_attentions=output_attentions,
+                use_cache=use_cache,
+            )
+
+        bsz, q_len, _ = hidden_states.size()
+
+        qkv = self.qkv_proj(hidden_states)
+        query_pos = self.num_heads * self.head_dim
+        query_states = qkv[..., :query_pos]
+        key_states = qkv[..., query_pos : query_pos + self.num_key_value_heads * self.head_dim]
+        value_states = qkv[..., query_pos + self.num_key_value_heads * self.head_dim :]
+
+        query_states = query_states.view(bsz, q_len, self.num_heads, self.head_dim).transpose(1, 2)
+        key_states = key_states.view(bsz, q_len, self.num_key_value_heads, self.head_dim).transpose(1, 2)
+        value_states = value_states.view(bsz, q_len, self.num_key_value_heads, self.head_dim).transpose(1, 2)
+
+        cos, sin = position_embeddings
+        query_states, key_states = apply_rotary_pos_emb(query_states, key_states, cos, sin, position_ids)
+
+        if past_key_value is not None:
+            cache_kwargs = {"sin": sin, "cos": cos, "cache_position": cache_position}  # Specific to RoPE models
+            key_states, value_states = past_key_value.update(key_states, value_states, self.layer_idx, cache_kwargs)
+
+        key_states = repeat_kv(key_states, self.num_key_value_groups)
+        value_states = repeat_kv(value_states, self.num_key_value_groups)
+
+        causal_mask = attention_mask
+        if attention_mask is not None:
+            causal_mask = attention_mask[:, :, :, : key_states.shape[-2]]
+
+        # SDPA with memory-efficient backend is currently (torch==2.1.2) bugged with non-contiguous inputs with custom attn_mask,
+        # Reference: https://github.com/pytorch/pytorch/issues/112577.
+        if query_states.device.type == "cuda" and attention_mask is not None:
+            query_states = query_states.contiguous()
+            key_states = key_states.contiguous()
+            value_states = value_states.contiguous()
+
+        # We dispatch to SDPA's Flash Attention or Efficient kernels via this `is_causal` if statement instead of an inline conditional assignment
+        # in SDPA to support both torch.compile's dynamic shapes and full graph options. An inline conditional prevents dynamic shapes from compiling.
+        # The q_len > 1 is necessary to match with AttentionMaskConverter.to_causal_4d that does not create a causal mask in case q_len == 1.
+        is_causal = True if causal_mask is None and q_len > 1 else False
+
+        attn_output = torch.nn.functional.scaled_dot_product_attention(
+            query_states,
+            key_states,
+            value_states,
+            attn_mask=causal_mask,
+            dropout_p=self.attention_dropout if self.training else 0.0,
+            is_causal=is_causal,
+        )
+
+        attn_output = attn_output.transpose(1, 2).contiguous()
+        attn_output = attn_output.view(bsz, q_len, self.hidden_size)
+
+        attn_output = self.o_proj(attn_output)
+
+        return attn_output, None, past_key_value
+    
 
 GLM_ATTENTION_CLASSES = {
-"eager": GlmAttention,
+    "eager": GlmAttention,
+    "flash_attention_2": GlmFlashAttention2,
+    "sdpa": GlmSdpaAttention,
 }
 
 
@@ -318,7 +569,7 @@ class GlmDecoderLayer(Phi3DecoderLayer):
             outputs += (present_key_value,)
 
         return outputs
-    
+
 
 class GlmModel(LlamaModel):
 
