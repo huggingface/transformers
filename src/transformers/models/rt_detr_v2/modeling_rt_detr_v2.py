d45aed5736121562927b5a629e878d417c4e852b
Sangbum Daniel Choi <34004152+SangbumChoi@users.noreply.github.com>
2024-09-25 21:19:20 +0900

    Update src/transformers/models/rt_detr_v2/modeling_rt_detr_v2.py
    Co-authored-by: amyeroberts <22614925+amyeroberts@users.noreply.github.com>

--- src/transformers/models/rt_detr_v2/modeling_rt_detr_v2.py
+++ src/transformers/models/rt_detr_v2/modeling_rt_detr_v2.py
@@ -12,7 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""PyTorch RT-DETR model."""
+"""PyTorch RT-DETR v2 model."""
 
 import math
 import os
