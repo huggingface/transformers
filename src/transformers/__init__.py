541a992572705f945e88ff6e6ae8251935eec3ff
Andrés Marafioti <andimarafioti@gmail.com>
2024-09-25 21:28:49 +0200

    Add Idefics 3! (#32473)
    * Add Idefics 3!
    
    * fixes to make both pipelines identical
    
    * fix for quantized models
    
    * First pass at the review
    
    * remove vocab size from the main config (it's still in the text_config)
    
    * hot fix for merve
    
    * Apply suggestions from code review
    
    Co-authored-by: amyeroberts <22614925+amyeroberts@users.noreply.github.com>
    
    * re-add model_type for text_config
    
    * remove support for old_cache
    
    * remove hidden_size from main config
    
    * rename idefics3 HF repo
    
    * few changes suggested in the PR
    
    * fix to input_data_format computation
    
    * remove overwrite of _autoset_attn_implementation following @zucchini-nlp suggestion
    
    * improve example
    
    * few improvements from amy's review
    
    * big change to enable processing input images as numpy arrays
    
    * Changes to the code to uniformize processor kwargs
    
    * image processing tests
    
    * image processing tests fixes and some bugs they discovered
    
    * addressed review comments from Yoni
    
    * fix modeling tests
    
    * remove special tokens that are not special
    
    * fixes tests
    
    * skip failing tests - they also fail for idefics2
    
    * added paper and readded the tests with multi gpu, who knows
    
    * Update docs/source/en/model_doc/idefics3.md
    
    Co-authored-by: amyeroberts <22614925+amyeroberts@users.noreply.github.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: amyeroberts <22614925+amyeroberts@users.noreply.github.com>
    
    * review amy until image_processing_idefics3
    
    * last comments from Amy
    
    * review amy
    
    * Update src/transformers/models/idefics3/image_processing_idefics3.py
    
    Co-authored-by: amyeroberts <22614925+amyeroberts@users.noreply.github.com>
    
    * Update src/transformers/models/idefics3/modeling_idefics3.py
    
    Co-authored-by: amyeroberts <22614925+amyeroberts@users.noreply.github.com>
    
    * Update docs/source/en/model_doc/idefics3.md
    
    Co-authored-by: amyeroberts <22614925+amyeroberts@users.noreply.github.com>
    
    * doc improvement - amy review
    
    * fix runtime error during fine-tuning
    
    * amy's review
    
    * Update src/transformers/models/idefics3/image_processing_idefics3.py
    
    Co-authored-by: amyeroberts <22614925+amyeroberts@users.noreply.github.com>
    
    * Update src/transformers/models/idefics3/image_processing_idefics3.py
    
    Co-authored-by: amyeroberts <22614925+amyeroberts@users.noreply.github.com>
    
    * Update src/transformers/models/idefics3/modeling_idefics3.py
    
    Co-authored-by: amyeroberts <22614925+amyeroberts@users.noreply.github.com>
    
    * ruff
    
    * amy's comment on the order
    
    * ruff ruff
    
    * fix copies
    
    * square images when they are not splitted
    
    * ruff :(
    
    * Update src/transformers/models/idefics3/image_processing_idefics3.py
    
    Co-authored-by: amyeroberts <22614925+amyeroberts@users.noreply.github.com>
    
    * Update tests/models/idefics3/test_processing_idefics3.py
    
    Co-authored-by: amyeroberts <22614925+amyeroberts@users.noreply.github.com>
    
    * fix small bug introduced in refactor
    
    * amy's image processing changes
    
    * fixes peft tests and ruff
    
    * modify to_pil_image from transformers. and review from emanuele.
    
    * add modified to_pil_image
    
    ---------
    
    Co-authored-by: amyeroberts <22614925+amyeroberts@users.noreply.github.com>

--- src/transformers/__init__.py
+++ src/transformers/__init__.py
@@ -481,6 +481,7 @@ _import_structure = {
     "models.ibert": ["IBertConfig"],
     "models.idefics": ["IdeficsConfig"],
     "models.idefics2": ["Idefics2Config"],
+    "models.idefics3": ["Idefics3Config"],
     "models.imagegpt": ["ImageGPTConfig"],
     "models.informer": ["InformerConfig"],
     "models.instructblip": [
@@ -1191,6 +1192,7 @@ else:
     _import_structure["models.grounding_dino"].extend(["GroundingDinoImageProcessor"])
     _import_structure["models.idefics"].extend(["IdeficsImageProcessor"])
     _import_structure["models.idefics2"].extend(["Idefics2ImageProcessor"])
+    _import_structure["models.idefics3"].extend(["Idefics3ImageProcessor"])
     _import_structure["models.imagegpt"].extend(["ImageGPTFeatureExtractor", "ImageGPTImageProcessor"])
     _import_structure["models.instructblipvideo"].extend(["InstructBlipVideoImageProcessor"])
     _import_structure["models.layoutlmv2"].extend(["LayoutLMv2FeatureExtractor", "LayoutLMv2ImageProcessor"])
@@ -2418,6 +2420,14 @@ else:
             "Idefics2Model",
             "Idefics2PreTrainedModel",
             "Idefics2Processor",
+        ]
+    )
+    _import_structure["models.idefics3"].extend(
+        [
+            "Idefics3ForConditionalGeneration",
+            "Idefics3Model",
+            "Idefics3PreTrainedModel",
+            "Idefics3Processor",
         ]
     )
     _import_structure["models.imagegpt"].extend(
@@ -5289,6 +5299,7 @@ if TYPE_CHECKING:
         IdeficsConfig,
     )
     from .models.idefics2 import Idefics2Config
+    from .models.idefics3 import Idefics3Config
     from .models.imagegpt import ImageGPTConfig
     from .models.informer import InformerConfig
     from .models.instructblip import (
@@ -6037,6 +6048,7 @@ if TYPE_CHECKING:
         from .models.grounding_dino import GroundingDinoImageProcessor
         from .models.idefics import IdeficsImageProcessor
         from .models.idefics2 import Idefics2ImageProcessor
+        from .models.idefics3 import Idefics3ImageProcessor
         from .models.imagegpt import ImageGPTFeatureExtractor, ImageGPTImageProcessor
         from .models.instructblipvideo import InstructBlipVideoImageProcessor
         from .models.layoutlmv2 import (
@@ -7070,6 +7082,12 @@ if TYPE_CHECKING:
             Idefics2Model,
             Idefics2PreTrainedModel,
             Idefics2Processor,
+        )
+        from .models.idefics3 import (
+            Idefics3ForConditionalGeneration,
+            Idefics3Model,
+            Idefics3PreTrainedModel,
+            Idefics3Processor,
         )
         from .models.imagegpt import (
             ImageGPTForCausalImageModeling,
