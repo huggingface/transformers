6bbbc7b675f2ed9531b1d7fc0b5dd1db15a645a0
Yoni Gozlan <74535834+yonigozlan@users.noreply.github.com>
2024-09-25 13:26:28 -0400

    Add OmDet-Turbo (#31843)
    * Add template with add-new-model-like
    
    * Add rough OmDetTurboEncoder and OmDetTurboDecoder
    
    * Add working OmDetTurbo convert to hf
    
    * Change OmDetTurbo encoder to RT-DETR encoder
    
    * Add swin timm backbone as default, add always partition fix for swin timm
    
    * Add labels and tasks caching
    
    * Fix make fix-copies
    
    * Format omdet_turbo
    
    * fix Tokenizer tests
    
    * Fix style and quality
    
    * Reformat omdet_turbo
    
    * Fix quality, style, copies
    
    * Standardize processor kwargs
    
    * Fix style
    
    * Add output_hidden_states and ouput_attentions
    
    * Add personalize multi-head attention, improve docstrings
    
    * Add integrated test and fix copy, style, quality
    
    * Fix unprotected import
    
    * Cleanup comments and fix unprotected imports
    
    * Add fix different prompts in batch (key_padding_mask)
    
    * Add key_padding_mask to custom multi-head attention module
    
    * Replace attention_mask by key_padding_mask
    
    * Remove OmDetTurboModel and refactor
    
    * Refactor processing of classes and abstract use of timm backbone
    
    * Add testing, fix output attentions and hidden states, add cache for anchors generation
    
    * Fix copies, style, quality
    
    * Add documentation, conver key_padding_mask to attention_mask
    
    * revert changes to backbone_utils
    
    * Fic docstrings rst
    
    * Fix unused argument in config
    
    * Fix image link documentation
    
    * Reorder config and cleanup
    
    * Add tokenizer_init_kwargs in merge_kwargs of the processor
    
    * Change AutoTokenizer to CLIPTokenizer in convert
    
    * Fix init_weights
    
    * Add ProcessorMixin tests, Fix convert while waiting on uniform kwargs
    
    * change processor kwargs and make task input optional
    
    * Fix omdet docs
    
    * Remove unnecessary tests for processor kwargs
    
    * Replace nested BatchEncoding output of the processor by a flattened BatchFeature
    
    * Make modifications from Pavel review
    
    * Add changes Amy review
    
    * Remove unused param
    
    * Remove normalize_before param, Modify processor call docstring
    
    * Remove redundant decoder class, add gradient checkpointing for decoder
    
    * Remove commented out code
    
    * Fix inference in fp16 and add fp16 integrated test
    
    * update omdet md doc
    
    * Add OmdetTurboModel
    
    * fix caching and nit
    
    * add OmDetTurboModel to tests
    
    * nit change repeated key test
    
    * Improve inference speed in eager mode
    
    * fix copies
    
    * Fix nit
    
    * remove OmdetTurboModel
    
    * [run-slow] omdet_turbo
    
    * [run-slow] omdet_turbo
    
    * skip dataparallel test
    
    * [run-slow] omdet_turbo
    
    * update weights to new path
    
    * remove unnecessary config in class
    
    ---------
    
    Co-authored-by: Ubuntu <ubuntu@ip-172-31-91-248.ec2.internal>

--- src/transformers/__init__.py
+++ src/transformers/__init__.py
@@ -609,6 +609,10 @@ _import_structure = {
     "models.nystromformer": ["NystromformerConfig"],
     "models.olmo": ["OlmoConfig"],
     "models.olmoe": ["OlmoeConfig"],
+    "models.omdet_turbo": [
+        "OmDetTurboConfig",
+        "OmDetTurboProcessor",
+    ],
     "models.oneformer": [
         "OneFormerConfig",
         "OneFormerProcessor",
@@ -2859,6 +2863,12 @@ else:
             "OlmoeForCausalLM",
             "OlmoeModel",
             "OlmoePreTrainedModel",
+        ]
+    )
+    _import_structure["models.omdet_turbo"].extend(
+        [
+            "OmDetTurboForObjectDetection",
+            "OmDetTurboPreTrainedModel",
         ]
     )
     _import_structure["models.oneformer"].extend(
@@ -5407,6 +5417,10 @@ if TYPE_CHECKING:
     )
     from .models.olmo import OlmoConfig
     from .models.olmoe import OlmoeConfig
+    from .models.omdet_turbo import (
+        OmDetTurboConfig,
+        OmDetTurboProcessor,
+    )
     from .models.oneformer import (
         OneFormerConfig,
         OneFormerProcessor,
@@ -7382,6 +7396,10 @@ if TYPE_CHECKING:
             OlmoeForCausalLM,
             OlmoeModel,
             OlmoePreTrainedModel,
+        )
+        from .models.omdet_turbo import (
+            OmDetTurboForObjectDetection,
+            OmDetTurboPreTrainedModel,
         )
         from .models.oneformer import (
             OneFormerForUniversalSegmentation,
--- src/transformers/utils/dummy_pt_objects.py
+++ src/transformers/utils/dummy_pt_objects.py
@@ -6587,6 +6587,20 @@ class OlmoePreTrainedModel(metaclass=DummyObject):
         requires_backends(self, ["torch"])
 
 
+class OmDetTurboForObjectDetection(metaclass=DummyObject):
+    _backends = ["torch"]
+
+    def __init__(self, *args, **kwargs):
+        requires_backends(self, ["torch"])
+
+
+class OmDetTurboPreTrainedModel(metaclass=DummyObject):
+    _backends = ["torch"]
+
+    def __init__(self, *args, **kwargs):
+        requires_backends(self, ["torch"])
+
+
 class OneFormerForUniversalSegmentation(metaclass=DummyObject):
     _backends = ["torch"]
 
