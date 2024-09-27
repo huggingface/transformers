c63a30237488c77227ae73b961bef2e6cd7a9a9d
Tony Wu <28306721+tonywu71@users.noreply.github.com>
2024-09-27 19:03:35 +0200

    feat: make ColPaliModel inehrit from PaliGemmaForConditionalGeneration

--- src/transformers/models/colpali/modular_colpali.py
+++ src/transformers/models/colpali/modular_colpali.py
@@ -14,7 +14,7 @@
 # limitations under the License.
 
 from dataclasses import dataclass
-from typing import ClassVar, List, Optional, Union
+from typing import ClassVar, List, Optional, Tuple, Union
 
 import torch
 import torch.utils.checkpoint
@@ -24,7 +24,6 @@ from torch import nn
 from ...cache_utils import Cache
 from ...feature_extraction_utils import BatchFeature
 from ...utils import (
-    ModelOutput,
     add_start_docstrings,
     add_start_docstrings_to_model_forward,
     is_flash_attn_2_available,
@@ -34,7 +33,6 @@ from ...utils import (
 from ..paligemma import (
     PaliGemmaConfig,
     PaliGemmaForConditionalGeneration,
-    PaliGemmaPreTrainedModel,
     PaliGemmaProcessor,
 )
 
@@ -185,7 +183,7 @@ class ColPaliProcessor(PaliGemmaProcessor):
 
 
 @dataclass
-class ColPaliModelOutput(ModelOutput):
+class ColPaliModelOutput(PaliGemmaForConditionalGeneration):
     """
     Base class for ColPali embeddings output.
 
@@ -194,7 +192,7 @@ class ColPaliModelOutput(ModelOutput):
             The embeddings of the model.
     """
 
-    embeddings: torch.Tensor
+    embeddings: torch.Tensor = None
 
 
 @add_start_docstrings(
@@ -210,16 +208,15 @@ class ColPaliModelOutput(ModelOutput):
     Adapted from colpali-engine==0.3.0: https://github.com/illuin-tech/colpali.
     """
 )
-class ColPaliForRetrieval(PaliGemmaPreTrainedModel):
+class ColPaliForRetrieval(PaliGemmaForConditionalGeneration):
     main_input_name: ClassVar[str] = "doc_input_ids"  # transformers-related
 
     def __init__(self, config: PaliGemmaConfig):
         super().__init__(config=config)
 
-        model = PaliGemmaForConditionalGeneration(config=config)
-        if model.language_model._tied_weights_keys is not None:
-            self._tied_weights_keys = [f"model.language_model.{k}" for k in model.language_model._tied_weights_keys]
-        self.model = model
+        if self.language_model._tied_weights_keys is not None:
+            self._tied_weights_keys = [f"model.language_model.{k}" for k in self.language_model._tied_weights_keys]
+        self.model = self
 
         self.dim = 128
         self.custom_text_proj = nn.Linear(self.model.config.text_config.hidden_size, self.dim)
@@ -258,9 +255,9 @@ class ColPaliForRetrieval(PaliGemmaPreTrainedModel):
     @replace_return_docstrings(output_type=ColPaliModelOutput, config_class="ColPaliConfig")
     def forward(
         self,
-        input_ids: torch.LongTensor,
-        pixel_values: torch.FloatTensor,
-        attention_mask: torch.Tensor,
+        input_ids: torch.LongTensor = None,
+        pixel_values: torch.FloatTensor = None,
+        attention_mask: Optional[torch.Tensor] = None,
         position_ids: Optional[torch.LongTensor] = None,
         past_key_values: Optional[Union[List[torch.FloatTensor], Cache]] = None,
         token_type_ids: Optional[torch.LongTensor] = None,
@@ -269,56 +266,41 @@ class ColPaliForRetrieval(PaliGemmaPreTrainedModel):
         labels: Optional[torch.LongTensor] = None,
         use_cache: Optional[bool] = None,
         output_attentions: Optional[bool] = None,
+        output_hidden_states: Optional[bool] = None,
+        return_dict: Optional[bool] = None,
         num_logits_to_keep: int = 0,
-    ) -> ColPaliModelOutput:
+    ) -> Union[Tuple, ColPaliModelOutput]:
         r"""
         Returns:
         """
-        outputs = self.model(
-            input_ids,
-            pixel_values,
-            attention_mask,
-            position_ids,
-            past_key_values,
-            token_type_ids,
-            cache_position,
-            inputs_embeds,
-            labels,
-            use_cache,
-            output_attentions,
-            num_logits_to_keep,
+        outputs = super().forward(
+            input_ids=input_ids,
+            pixel_values=pixel_values,
+            attention_mask=attention_mask,
+            position_ids=position_ids,
+            past_key_values=past_key_values,
+            token_type_ids=token_type_ids,
+            cache_position=cache_position,
+            inputs_embeds=inputs_embeds,
+            labels=labels,
+            use_cache=use_cache,
+            output_attentions=output_attentions,
             output_hidden_states=True,
+            return_dict=False,
+            num_logits_to_keep=num_logits_to_keep,
         )
         last_hidden_states = outputs.hidden_states[-1]  # (batch_size, sequence_length, hidden_size)
         proj = self.custom_text_proj(last_hidden_states)  # (batch_size, sequence_length, dim)
 
         # L2 normalization
-        proj = proj / proj.norm(dim=-1, keepdim=True)  # (batch_size, sequence_length, dim)
+        embeddings = proj / proj.norm(dim=-1, keepdim=True)  # (batch_size, sequence_length, dim)
 
-        proj = proj * attention_mask.unsqueeze(-1)  # (batch_size, sequence_length, dim)
+        embeddings = embeddings * attention_mask.unsqueeze(-1)  # (batch_size, sequence_length, dim)
 
-        return ColPaliModelOutput(embeddings=proj)
+        if not return_dict:
+            return (embeddings,)
 
-    def get_input_embeddings(self):
-        return self.model.language_model.get_input_embeddings()
-
-    def set_input_embeddings(self, value):
-        self.model.language_model.set_input_embeddings(value)
-
-    def get_output_embeddings(self):
-        return self.model.language_model.get_output_embeddings()
-
-    def set_output_embeddings(self, new_embeddings):
-        self.model.language_model.set_output_embeddings(new_embeddings)
-
-    def set_decoder(self, decoder):
-        self.model.language_model.set_decoder(decoder)
-
-    def get_decoder(self):
-        return self.model.language_model.get_decoder()
-
-    def tie_weights(self):
-        return self.model.language_model.tie_weights()
+        return ColPaliModelOutput(embeddings=embeddings)
 
     def resize_token_embeddings(
         self,
