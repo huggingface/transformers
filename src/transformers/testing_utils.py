74780f63c8b7da0453adc73e37d9777b10d54dbd
Matt <rocketknight1@gmail.com>
2024-09-27 17:42:15 +0100

    Full error reporting

--- src/transformers/testing_utils.py
+++ src/transformers/testing_utils.py
@@ -2614,11 +2614,27 @@ if is_torch_available():
 
 
 def compare_pipeline_output_to_hub_spec(output, hub_spec):
+    missing_keys = []
+    unexpected_keys = []
+    all_field_names = {field.name for field in fields(hub_spec)}
+    matching_keys = sorted([key for key in output.keys() if key in all_field_names])
+
     for field in fields(hub_spec):
         if field.default is MISSING and field.name not in output:
+            missing_keys.append(field.name)
             raise KeyError(f"Pipeline output is missing mandatory field in Hub spec: {field.name}")
 
-    all_field_names = {field.name for field in fields(hub_spec)}
     for output_key in output:
         if output_key not in all_field_names:
+            unexpected_keys.append(output_key)
             raise KeyError(f"Pipeline output contains field not present in Hub spec: {output_key}")
+
+    if missing_keys or unexpected_keys:
+        error = ["Pipeline output does not match Hub spec!"]
+        if matching_keys:
+            error.append(f"Matching keys: {matching_keys}")
+        if missing_keys:
+            error.append(f"Missing mandatory keys in pipeline output: {missing_keys}")
+        if unexpected_keys:
+            error.append(f"Keys in pipeline output that do not exist in Hub spec: {unexpected_keys}")
+        raise KeyError("\n".join(error))
