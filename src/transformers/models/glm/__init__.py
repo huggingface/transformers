fc5cd413f8ece96027647f4a1b1f8d85cb83bbc7
Cyril Vallez <cyril.vallez@gmail.com>
2024-09-27 17:04:17 +0200

    Update __init__.py

--- src/transformers/models/glm/__init__.py
+++ src/transformers/models/glm/__init__.py
@@ -1,4 +1,4 @@
-# Copyright 2024 The HuggingFace Inc. team. All rights reserved.
+# Copyright 2020 The HuggingFace Team. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,49 +13,15 @@
 # limitations under the License.
 from typing import TYPE_CHECKING
 
-from ...utils import (
-    OptionalDependencyNotAvailable,
-    _LazyModule,
-    is_torch_available,
-)
+from ...utils import _LazyModule
+from ...utils.import_utils import define_import_structure
 
-
-_import_structure = {
-    "configuration_glm": ["GlmConfig"],
-}
-
-try:
-    if not is_torch_available():
-        raise OptionalDependencyNotAvailable()
-except OptionalDependencyNotAvailable:
-    pass
-else:
-    _import_structure["modeling_glm"] = [
-        "GlmForCausalLM",
-        "GlmModel",
-        "GlmPreTrainedModel",
-        "GlmForSequenceClassification",
-        "GlmForTokenClassification",
-    ]
 
 if TYPE_CHECKING:
-    from .configuration_glm import GlmConfig
-
-    try:
-        if not is_torch_available():
-            raise OptionalDependencyNotAvailable()
-    except OptionalDependencyNotAvailable:
-        pass
-    else:
-        from .modeling_glm import (
-            GlmForCausalLM,
-            GlmForSequenceClassification,
-            GlmForTokenClassification,
-            GlmModel,
-            GlmPreTrainedModel,
-        )
-
+    from .configuration_glm import *
+    from .modeling_glm import *
 else:
     import sys
 
-    sys.modules[__name__] = _LazyModule(__name__, globals()["__file__"], _import_structure, module_spec=__spec__)
+    _file = globals()["__file__"]
+    sys.modules[__name__] = _LazyModule(__name__, _file, define_import_structure(_file), module_spec=__spec__)
