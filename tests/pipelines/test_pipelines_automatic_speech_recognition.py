9dfcfc73c8155c8a289f877cda9bfc434053dba8
Matt <rocketknight1@gmail.com>
2024-09-27 17:06:18 +0100

    Add ASR pipeline to testing

--- tests/pipelines/test_pipelines_automatic_speech_recognition.py
+++ tests/pipelines/test_pipelines_automatic_speech_recognition.py
@@ -14,11 +14,12 @@
 
 import time
 import unittest
+from dataclasses import fields
 
 import numpy as np
 import pytest
 from datasets import Audio, load_dataset
-from huggingface_hub import hf_hub_download, snapshot_download
+from huggingface_hub import AutomaticSpeechRecognitionOutput, hf_hub_download, snapshot_download
 
 from transformers import (
     MODEL_FOR_CTC_MAPPING,
@@ -85,6 +86,10 @@ class AutomaticSpeechRecognitionPipelineTests(unittest.TestCase):
         audio = np.zeros((34000,))
         outputs = speech_recognizer(audio)
         self.assertEqual(outputs, {"text": ANY(str)})
+
+        spec_output_keys = {field.name for field in fields(AutomaticSpeechRecognitionOutput)}
+        output_keys = set(outputs.keys())
+        self.assertEqual(spec_output_keys, output_keys, msg="Pipeline output keys do not match HF Hub spec!")
 
         # Striding
         audio = {"raw": audio, "stride": (0, 4000), "sampling_rate": speech_recognizer.feature_extractor.sampling_rate}
