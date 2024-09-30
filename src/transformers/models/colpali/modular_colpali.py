965693b460068229507061e9084d075f245de827
Tony Wu <28306721+tonywu71@users.noreply.github.com>
2024-09-30 22:38:35 +0200

    fix: ColPaliModelOutput inherits from ModelOutput instead of PaliGemmaCausalLMOutputWithPast

--- src/transformers/models/colpali/modular_colpali.py
+++ src/transformers/models/colpali/modular_colpali.py
@@ -22,6 +22,8 @@ import torch.utils.checkpoint
 from PIL import Image
 from torch import nn
 
+from transformers.utils.generic import ModelOutput
+
 from ...cache_utils import Cache
 from ...feature_extraction_utils import BatchFeature
 from ...utils import (
@@ -36,7 +38,6 @@ from ..paligemma import (
     PaliGemmaForConditionalGeneration,
     PaliGemmaProcessor,
 )
-from ..paligemma.modeling_paligemma import PaliGemmaCausalLMOutputWithPast
 
 
 if is_flash_attn_2_available():
@@ -262,16 +263,43 @@ class ColPaliProcessor(PaliGemmaProcessor):
 
 
 @dataclass
-class ColPaliModelOutput(PaliGemmaCausalLMOutputWithPast):
+class ColPaliModelOutput(ModelOutput):
     """
     Base class for ColPali embeddings output.
 
     Args:
         embeddings (`torch.FloatTensor` of shape `(batch_size, sequence_length, hidden_size)`):
             The embeddings of the model.
+        loss (`torch.FloatTensor` of shape `(1,)`, *optional*, returned when `labels` is provided):
+            Language modeling loss (for next-token prediction).
+        past_key_values (`tuple(tuple(torch.FloatTensor))`, *optional*, returned when `use_cache=True` is passed or when `config.use_cache=True`):
+            Tuple of `tuple(torch.FloatTensor)` of length `config.n_layers`, with each tuple having 2 tensors of shape
+            `(batch_size, num_heads, sequence_length, embed_size_per_head)`)
+
+            Contains pre-computed hidden-states (key and values in the self-attention blocks) that can be used (see
+            `past_key_values` input) to speed up sequential decoding.
+        hidden_states (`tuple(torch.FloatTensor)`, *optional*, returned when `output_hidden_states=True` is passed or when `config.output_hidden_states=True`):
+            Tuple of `torch.FloatTensor` (one for the output of the embeddings, if the model has an embedding layer, +
+            one for the output of each layer) of shape `(batch_size, sequence_length, hidden_size)`.
+
+            Hidden-states of the model at the output of each layer plus the optional initial embedding outputs.
+        attentions (`tuple(torch.FloatTensor)`, *optional*, returned when `output_attentions=True` is passed or when `config.output_attentions=True`):
+            Tuple of `torch.FloatTensor` (one for each layer) of shape `(batch_size, num_heads, sequence_length,
+            sequence_length)`.
+
+            Attentions weights after the attention softmax, used to compute the weighted average in the self-attention
+            heads.
+        image_hidden_states (`torch.FloatTensor`, *optional*):
+            A `torch.FloatTensor` of size `(batch_size, num_images, sequence_length, hidden_size)`.
+            image_hidden_states of the model produced by the vision encoder after projecting last hidden state.
     """
 
     embeddings: torch.Tensor = None
+    loss: Optional[torch.FloatTensor] = None
+    past_key_values: Optional[Union[List[torch.FloatTensor], Cache]] = None
+    hidden_states: Optional[Tuple[torch.FloatTensor]] = None
+    attentions: Optional[Tuple[torch.FloatTensor]] = None
+    image_hidden_states: Optional[torch.FloatTensor] = None
 
 
 @add_start_docstrings(
