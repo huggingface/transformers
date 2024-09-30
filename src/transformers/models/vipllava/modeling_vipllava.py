fc076402af1447977df4fc81443dc1699a0181ed
laurentd-lunit <laurent.dillard@lunit.io>
2024-09-30 06:09:15 +0000

    [fix] token dimension for check

--- src/transformers/models/vipllava/modeling_vipllava.py
+++ src/transformers/models/vipllava/modeling_vipllava.py
@@ -507,7 +507,7 @@ class VipLlavaForConditionalGeneration(VipLlavaPreTrainedModel, GenerationMixin)
             # TODO: @raushan retain only the new behavior after v4.47
             else:
                 n_image_tokens = (input_ids == self.config.image_token_index).sum().item()
-                n_image_features = image_features.shape[0]
+                n_image_features = image_features.shape[1]
                 if n_image_tokens != n_image_features:
                     raise ValueError(
                         f"Image features and image tokens do not match: tokens: {n_image_tokens}, features {n_image_features}"
