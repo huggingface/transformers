b915673fba9746597e1299bace06bc47f4901670
MekkCyber <mekk.cyber@gmail.com>
2024-10-01 15:19:22 +0000

    fix style

--- src/transformers/trainer.py
+++ src/transformers/trainer.py
@@ -404,15 +404,16 @@ class Trainer:
         self.args = args
         # Seed must be set before instantiating the model when using model
         enable_full_determinism(self.args.seed) if self.args.full_determinism else set_seed(self.args.seed)
-        
 
         generator = torch.Generator()
         if self.args.data_seed is not None:
             # TODO change min version of accelerate
             if self.args.accelerator_config.use_seedable_sampler and is_accelerate_available(min_version="0.34.0"):
-                raise RuntimeError("data_seed requires accelerate >= 0.34.0, please install the latest version of accelerate")
+                raise RuntimeError(
+                    "data_seed requires accelerate >= 0.34.0, please install the latest version of accelerate"
+                )
             generator.manual_seed(self.args.data_seed)
-        else: 
+        else:
             generator.manual_seed(self.args.seed)
         self.data_sampler_generator = generator
 
@@ -904,7 +905,7 @@ class Trainer:
             )
 
         else:
-            return RandomSampler(self.train_dataset, generator=self.data_sampler_generator)       
+            return RandomSampler(self.train_dataset, generator=self.data_sampler_generator)
 
     def get_train_dataloader(self) -> DataLoader:
         """
