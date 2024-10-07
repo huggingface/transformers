5d16f6aacc47daeeaa3c3efc92d170eaeef17f95
Marc Sun <marc@huggingface.co>
2024-10-07 18:34:35 +0200

    remove unnecessary line

--- src/transformers/configuration_utils.py
+++ src/transformers/configuration_utils.py
@@ -1036,7 +1036,6 @@ class PretrainedConfig(PushToHubMixin):
             if decoder_config is not self:
                 default_config = decoder_config.__class__()
             else:
-                default_config = None
                 decoder_config = None
 
         # If it is a composite model, we want to check the subconfig that will be used for generation
