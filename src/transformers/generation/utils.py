9ef5359017c9d1c12d5a6e3072958c753617abe2
Tianshu Wang <wang@tianshu.me>
2024-10-07 21:15:20 +0800

    Fix pad_token_tensor is None in warning

--- src/transformers/generation/utils.py
+++ src/transformers/generation/utils.py
@@ -1758,8 +1758,8 @@ class GenerationMixin:
                         "The attention mask and the pad token id were not set. As a consequence, you may observe "
                         "unexpected behavior. Please pass your input's `attention_mask` to obtain reliable results."
                     )
-                logger.warning(f"Setting `pad_token_id` to `eos_token_id`:{pad_token_tensor} for open-end generation.")
             pad_token_tensor = eos_token_tensor[0]
+            logger.warning(f"Setting `pad_token_id` to `eos_token_id`:{pad_token_tensor} for open-end generation.")
 
         # Sanity checks/warnings
         if self.config.is_encoder_decoder and decoder_start_token_tensor is None:
