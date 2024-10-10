d30482309c7c3e208442dc866ec421d89060da22
Cyril Vallez <cyril.vallez@gmail.com>
2024-10-10 16:09:06 +0200

    Add required arg in prepare_inoput

--- src/transformers/models/mistral/modeling_mistral.py
+++ src/transformers/models/mistral/modeling_mistral.py
@@ -1195,6 +1195,7 @@ class MistralForCausalLM(MistralPreTrainedModel, GenerationMixin):
                 attention_mask,
                 sequence_length=sequence_length,
                 target_length=past_key_values.get_max_cache_shape(),
+                initial_mask_position=0,
                 dtype=self.lm_head.weight.dtype,
                 device=device,
                 cache_position=cache_position,
