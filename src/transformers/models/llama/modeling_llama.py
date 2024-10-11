d2796f6f12702ae9688fc4771040aed7a3eb973f
Abhishek <maurya.abhishek@ibm.com>
2024-10-10 20:03:14 -0400

    PR changes
    Signed-off-by: Abhishek <maurya.abhishek@ibm.com>

--- src/transformers/models/llama/modeling_llama.py
+++ src/transformers/models/llama/modeling_llama.py
@@ -877,6 +877,7 @@ class LlamaModel(LlamaPreTrainedModel):
         output_attentions: Optional[bool] = None,
         output_hidden_states: Optional[bool] = None,
         return_dict: Optional[bool] = None,
+        cache_position: Optional[torch.LongTensor] = None,
         **flash_attn_kwargs: Unpack[FlashAttentionKwargs],
     ) -> Union[Tuple, BaseModelOutputWithPast]:
         output_attentions = output_attentions if output_attentions is not None else self.config.output_attentions
