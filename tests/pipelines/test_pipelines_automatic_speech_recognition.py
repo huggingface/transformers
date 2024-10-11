1eb9b6282ae1b393bcdca890e7f83fc081bb1502
root <victor.b.nilsson@if.se>
2024-10-11 10:10:35 +0000

    remove unused import

--- tests/pipelines/test_pipelines_automatic_speech_recognition.py
+++ tests/pipelines/test_pipelines_automatic_speech_recognition.py
@@ -18,7 +18,7 @@ import unittest
 import numpy as np
 import pytest
 from datasets import Audio, load_dataset
-from huggingface_hub import AutomaticSpeechRecognitionOutput, AutomaticSpeechRecognitionInput, hf_hub_download, snapshot_download
+from huggingface_hub import AutomaticSpeechRecognitionOutput, hf_hub_download, snapshot_download
 
 from transformers import (
     MODEL_FOR_CTC_MAPPING,
