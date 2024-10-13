936087cfe05885b1553be8d9d276e0d0848a1069
Tibor Reiss <tibor.reiss@gmail.com>
2024-10-13 12:00:20 +0200

    Make some minor improvements

--- src/transformers/modeling_utils.py
+++ src/transformers/modeling_utils.py
@@ -378,7 +378,7 @@ def check_support_param_buffer_assignment(model_to_load, state_dict, start_prefi
         return False
 
     # If the model does, the incoming `state_dict` and the `model_to_load` must be the same dtype
-    first_key = list(model_to_load.state_dict().keys())[0]
+    first_key = next(iter(model_to_load.state_dict().keys()))
     if start_prefix + first_key in state_dict:
         return state_dict[start_prefix + first_key].dtype == model_to_load.state_dict()[first_key].dtype
 
