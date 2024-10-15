aaf7299862d697f5bc7c554719120e2d61614c3f
yonigozlan <yoni.gozlan@huggingface.co>
2024-10-15 15:07:37 +0000

    nit fix wrong llava onevision name in tokenization auto

--- src/transformers/models/auto/tokenization_auto.py
+++ src/transformers/models/auto/tokenization_auto.py
@@ -258,7 +258,7 @@ else:
                 ),
             ),
             ("llava", ("LlamaTokenizer", "LlamaTokenizerFast" if is_tokenizers_available() else None)),
-            ("llava-onevision", ("LlamaTokenizer", "LlamaTokenizerFast" if is_tokenizers_available() else None)),
+            ("llava_onevision", ("LlamaTokenizer", "LlamaTokenizerFast" if is_tokenizers_available() else None)),
             ("llava_next", ("LlamaTokenizer", "LlamaTokenizerFast" if is_tokenizers_available() else None)),
             ("llava_next_video", ("LlamaTokenizer", "LlamaTokenizerFast" if is_tokenizers_available() else None)),
             ("longformer", ("LongformerTokenizer", "LongformerTokenizerFast" if is_tokenizers_available() else None)),
