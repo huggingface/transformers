5dfc51c9b8cde540b1e41d9e5b61881268d4f6a6
Arthur Zucker <arthur.zucker@gmail.com>
2024-10-16 11:22:59 +0200

    ?

--- src/transformers/modeling_utils.py
+++ src/transformers/modeling_utils.py
@@ -4988,7 +4988,8 @@ class PreTrainedModel(nn.Module, ModuleUtilsMixin, GenerationMixin, PushToHubMix
         else:
             loss_type = self.__class__.__name__
             if loss_type not in LOSS_MAPPING:
-                loss_type = re.findall(rf"({"|".join(LOSS_MAPPING)})", self.__class__.__name__)
+                loss_groups = f"({"|".join(LOSS_MAPPING)})"
+                loss_type = re.findall(loss_groups, self.__class__.__name__)
                 if len(loss_type) > 0:
                     loss_type = loss_type[0]
                 else:
