3c7e57a252de0b881e957209b9360dc05528dca9
Cyril Vallez <cyril.vallez@huggingface.co>
2024-10-18 17:41:12 +0200

    add Glm (#33823)
    * Create modular_glm.py
    
    * Update modular_glm.py
    
    * Finalize architecture without all attentions
    
    * Add all attentions modules
    
    * Finalize modular
    
    * Update given last version
    
    * Last update
    
    * Finalize model
    
    * Finalize converter
    
    * Update convert_glm_weights_to_hf.py
    
    * style
    
    * style
    
    * Create __init__.py
    
    * Aff all inits
    
    * Update convert_glm_weights_to_hf.py
    
    * Update convert_glm_weights_to_hf.py
    
    * Update convert_glm_weights_to_hf.py
    
    * Update convert_glm_weights_to_hf.py
    
    * Update convert_glm_weights_to_hf.py
    
    * Update convert_glm_weights_to_hf.py
    
    * Update convert_glm_weights_to_hf.py
    
    * Update convert_glm_weights_to_hf.py
    
    * Update convert_glm_weights_to_hf.py
    
    * Correct the rotary embeddings
    
    * Remove apply_residual_connection_post_layernorm (always false)
    
    * remove use_rms_norm (always true)
    
    * remove past_layer_norm (always true)
    
    * Update __init__.py
    
    * Update config and license
    
    * start adding tests and doc
    
    * Add doc + style
    
    * Update test_modeling_glm.py
    
    * Add dummies
    
    * Apply correct modeling
    
    * Refactor attention to follow llama
    
    * Update __init__.py
    
    * Update convert_glm_weights_to_hf.py
    
    * Correct bias
    
    * remove linear_bias and pdrop (never used)
    
    * apply modular
    
    * Simplify converter
    
    * remove dummies + style
    
    * add model_input_names
    
    * Add pretraining_tp to config for when eager attention is used
    
    * Update modular to remove all pretraining_tp
    
    * Update test_modeling_glm.py
    
    * Update the __all__
    
    * Update __all__
    
    * Update __init__.py
    
    * Update test_modeling_glm.py
    
    * add revisions
    
    * Add the correct repos and revisions
    
    * style
    
    * Update __init__.py
    
    * update exports
    
    * remove import of modular files
    
    * style
    
    * Apply Llama changes + refine converter
    
    * Update convert_glm_weights_to_hf.py
    
    * Update convert_glm_weights_to_hf.py
    
    * Update convert_glm_weights_to_hf.py
    
    * Update convert_glm_weights_to_hf.py
    
    * Update convert_glm_weights_to_hf.py
    
    * Update convert_glm_weights_to_hf.py
    
    * Update convert_glm_weights_to_hf.py
    
    * Update convert_glm_weights_to_hf.py
    
    * style
    
    * Use new modular converter
    
    * add pretrainedmodel to init
    
    * style
    
    * Update test_modeling_glm.py
    
    * Move config outside modular to please CI about docstrings
    
    * Add dummies to please CI
    
    * Update glm.md
    
    * Update glm.md

--- src/transformers/__init__.py
+++ src/transformers/__init__.py
@@ -454,6 +454,7 @@ _import_structure = {
         "GitProcessor",
         "GitVisionConfig",
     ],
+    "models.glm": ["GlmConfig"],
     "models.glpn": ["GLPNConfig"],
     "models.gpt2": [
         "GPT2Config",
@@ -2292,6 +2293,15 @@ else:
             "GitModel",
             "GitPreTrainedModel",
             "GitVisionModel",
+        ]
+    )
+    _import_structure["models.glm"].extend(
+        [
+            "GlmForCausalLM",
+            "GlmForSequenceClassification",
+            "GlmForTokenClassification",
+            "GlmModel",
+            "GlmPreTrainedModel",
         ]
     )
     _import_structure["models.glpn"].extend(
@@ -5304,6 +5314,7 @@ if TYPE_CHECKING:
         GitProcessor,
         GitVisionConfig,
     )
+    from .models.glm import GlmConfig
     from .models.glpn import GLPNConfig
     from .models.gpt2 import (
         GPT2Config,
@@ -7023,6 +7034,13 @@ if TYPE_CHECKING:
             GitModel,
             GitPreTrainedModel,
             GitVisionModel,
+        )
+        from .models.glm import (
+            GlmForCausalLM,
+            GlmForSequenceClassification,
+            GlmForTokenClassification,
+            GlmModel,
+            GlmPreTrainedModel,
         )
         from .models.glpn import (
             GLPNForDepthEstimation,
