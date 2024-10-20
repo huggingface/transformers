6dc6f81c094b73313447203c43e5972814758c23
松本和真 <96107127+weak-kajuma@users.noreply.github.com>
2024-10-20 20:45:59 +0900

    adding groupnorm
    Co-authored-by: Minho Ryu <ryumin93@gmail.com>

--- src/transformers/models/diffllama/modeling_diffllama.py
+++ src/transformers/models/diffllama/modeling_diffllama.py
@@ -403,6 +403,8 @@ class DiffLlamaAttention(nn.Module):
         attn_output = attn_output.transpose(1, 2).contiguous()
 
         attn_output = attn_output.reshape(bsz, q_len, -1)
+        
+        attn_output = (1 - self.lambda_init) * self.groupnorm(attn_output)
 
         if self.config.pretraining_tp > 1:
             attn_output = attn_output.split(self.hidden_size // self.config.pretraining_tp, dim=2)
