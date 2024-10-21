4d4d4fc6b258b39a8eda43cf090fcbc1d61d4e06
Tony Wu <28306721+tonywu71@users.noreply.github.com>
2024-10-21 21:49:54 +0200

    docs: update ColPali model doc

--- src/transformers/models/colpali/modular_colpali.py
+++ src/transformers/models/colpali/modular_colpali.py
@@ -350,8 +350,6 @@ class ColPaliForRetrievalOutput(ModelOutput):
     - A blog post detailing ColPali, a vision retrieval model, can be found [here](https://huggingface.co/blog/manu/colpali). 📝
     - The code for training ColPali and for the `colpali-engine` package can be found [here](https://github.com/illuin-tech/colpali). 🌎
     - Cookbooks to fine-tune ColPali (with optional quantization), generate similarity maps, ... can be found [here](https://github.com/tonywu71/colpali-cookbooks). 📚
-
-    Adapted from [`colpali-engine==0.3.0`](https://github.com/illuin-tech/colpali/releases/tag/v0.3.0).
     """
 )
 class ColPaliForRetrieval(PaliGemmaForConditionalGeneration):
