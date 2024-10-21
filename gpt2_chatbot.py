7c3b7bd412b7316a9d5c266ec7ab6d622bec6df8
grish-ma <144985869+grish-ma@users.noreply.github.com>
2024-10-21 13:34:52 -0700

    Update gpt2_chatbot.py

--- gpt2_chatbot.py
+++ gpt2_chatbot.py
@@ -1,5 +1,12 @@
-# from transformers import GPT2LMHeadModel, GPT2Tokenizer
+## FROM VIDEO
+from transformers import pipeline
 
+classifier = pipeline("sentiment-analysis")
+res = classifier("I've been waiting for a HuggingFace course my whole life.")
+print(res)
+
+## FROM CHAT GPT
+# from transformers import GPT2LMHeadModel, GPT2Tokenizer
 # # Load pre-trained model and tokenizer
 # model_name = "gpt2"  # You can use "gpt2", "gpt2-medium", "gpt2-large", or "gpt2-xl"
 # model = GPT2LMHeadModel.from_pretrained(model_name)
@@ -19,10 +26,13 @@
 #     response = tokenizer.decode(output[0], skip_special_tokens=True)
 #     print(f"GPT-2: {response}")
 
-from transformers import AutoTokenizer, AutoModel
 
-tokenizer = AutoTokenizer.from_pretrained("google-bert/bert-base-uncased")
-model = AutoModel.from_pretrained("google-bert/bert-base-uncased")
 
-inputs = tokenizer("Hello world!", return_tensors="pt")
-outputs = model(**inputs)
+##FROM README FILE
+# from transformers import AutoTokenizer, AutoModel
+
+# tokenizer = AutoTokenizer.from_pretrained("gpt2")
+# model = AutoModel.from_pretrained("gpt2")
+
+# inputs = tokenizer("Hello world!", return_tensors="pt")
+# outputs = model(**inputs)
