1d3a3d7b7cf8f1a382985cfe194763e084ae08fe
Tony Wu <28306721+tonywu71@users.noreply.github.com>
2024-10-21 17:10:38 +0200

    refactor: remove unused `get_torch_device`

--- src/transformers/models/colpali/modular_colpali.py
+++ src/transformers/models/colpali/modular_colpali.py
@@ -155,27 +155,6 @@ class ColPaliProcessor(PaliGemmaProcessor):
             in a chat into a tokenizable string.
     """
 
-    @staticmethod
-    def get_torch_device(device: str = "auto") -> str:
-        """
-        Returns the device (string) to be used by PyTorch.
-
-        `device` arg defaults to "auto" which will use:
-        - "cuda:0" if available
-        - else "mps" if available
-        - else "cpu".
-        """
-
-        if device == "auto":
-            if torch.cuda.is_available():
-                device = "cuda:0"
-            elif torch.backends.mps.is_available():  # for Apple Silicon
-                device = "mps"
-            else:
-                device = "cpu"
-
-        return device
-
     def __call__(
         self,
         images: ImageInput = None,
