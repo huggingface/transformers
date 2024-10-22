1f7c75bc925b2f338d90707a524870e8d79c8e69
HALLOUARD <57447861+YHallouard@users.noreply.github.com>
2024-10-22 15:14:07 +0200

    [RT-DETR] Fix onnx inference bug for Optype (Where) (#33877)
    * feat: [RT-DETR] Add onnx runtime config and fix onnx inference bug Optype (Where)
    
    * fix lint
    
    * use dtype istead of torch.float32
    
    * add doc
    
    * remove onnx config
    
    * use dtype info
    
    * use tensor to fix lint

--- src/transformers/models/rt_detr/modeling_rt_detr.py
+++ src/transformers/models/rt_detr/modeling_rt_detr.py
@@ -1752,7 +1752,7 @@ class RTDetrModel(RTDetrPreTrainedModel):
         anchors = torch.concat(anchors, 1)
         valid_mask = ((anchors > eps) * (anchors < 1 - eps)).all(-1, keepdim=True)
         anchors = torch.log(anchors / (1 - anchors))
-        anchors = torch.where(valid_mask, anchors, torch.inf)
+        anchors = torch.where(valid_mask, anchors, torch.tensor(torch.finfo(dtype).max, dtype=dtype, device=device))
 
         return anchors, valid_mask
 
