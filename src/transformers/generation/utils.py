722595a38d912d475fc70d2d56bdecf00f51a050
Raushan Turganbay <raushan@huggingface.co>
2024-10-22 08:23:53 +0200

    T5 compile compatibilty (#34089)
    * this worked in normal generation, needs more tests
    
    * fix almost all tests in t5
    
    * nit
    
    * longt5, umt5, mt5
    
    * style
    
    * udop, pix2struct
    
    * more models
    
    * fix some tests
    
    * fix onnx tests
    
    * tracing tests fixed
    
    * compile enabled and tested for t5 models
    
    * fix small bug in slow tests
    
    * [run-slow] t5
    
    * uncomment
    
    * style
    
    * update with new generation refactoring
    
    * nit
    
    * fix copies
    
    * this is the fix, had to change t5 to fix copies
    
    * update
    
    * [run-slow] t5
    
    * [run-slow] t5
    
    * update
    
    * add test for encoder only T5
    
    * clean up after rebase
    
    * fix pop2piano
    
    * add comment
    
    * style
    
    * fix copies after rebase
    
    * fix copies  missed this one

--- src/transformers/generation/utils.py
+++ src/transformers/generation/utils.py
@@ -1535,8 +1535,12 @@ class GenerationMixin:
     def _get_initial_cache_position(self, input_ids, model_kwargs):
         """Calculates `cache_position` for the pre-fill stage based on `input_ids` and optionally past length"""
         # `torch.compile`-friendly `torch.arange` from a shape -- the lines below are equivalent to `torch.arange`
-        if "inputs_embeds" in model_kwargs:
+        if "inputs_embeds" in model_kwargs and not self.config.is_encoder_decoder:
             cache_position = torch.ones_like(model_kwargs["inputs_embeds"][0, :, 0], dtype=torch.int64).cumsum(0) - 1
+        elif "decoder_inputs_embeds" in model_kwargs and self.config.is_encoder_decoder:
+            cache_position = (
+                torch.ones_like(model_kwargs["decoder_inputs_embeds"][0, :, 0], dtype=torch.int64).cumsum(0) - 1
+            )
         else:
             cache_position = torch.ones_like(input_ids[0, :], dtype=torch.int64).cumsum(0) - 1
 
@@ -1633,7 +1637,7 @@ class GenerationMixin:
 
             cache_kwargs = {
                 "config": self.config.get_text_config(),
-                "max_batch_size": batch_size,
+                "batch_size": batch_size,
                 "max_cache_len": max_cache_len,
                 "device": device,
                 "dtype": cache_dtype,
