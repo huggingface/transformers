f8782dcc4a765e4c1789e8b80939591e60b5663c
Raushan Turganbay <raushan@huggingface.co>
2024-10-22 06:54:44 +0200

    Attn implementation for composite models (#32238)
    * first try
    
    * codestyle
    
    * idefics2 is happy
    
    * [run-slow] llava, llava_next, video_llava, vipllava, llava_next_video, idefics, idefics2, kosmos2, fuyu, blip, blip_2, instructblip, instructblipvideo, paligemma
    
    * fix-copies
    
    * [run-slow] llava, llava_next, video_llava, vipllava, llava_next_video, idefics, idefics2, kosmos2, fuyu, blip, blip_2, instructblip, instructblipvideo
    
    * blip-2 needs to init vision from config
    
    * when was this removed O_o
    
    * minor fix
    
    * tests
    
    * this way?
    
    * tests
    
    * model-agnostic code
    
    * codestyle
    
    * add tests for idefics
    
    * modify general test for VLMs
    
    * no generation test for vlm yet!
    
    * no generation test here also
    
    * wanr in VIT-SDPA if output attn
    
    * add more tests
    
    * user can pass dict as attn impl
    
    * repo consistency
    
    * update
    
    * muicgen
    
    * no prints
    
    * forgot speech enc-dec and clip
    
    * how many composite models we have?
    
    * musicgen meelody is same as mudicgen
    
    * +siglip
    
    * fix tests + add some more
    
    * remove idefics custom overriden code
    
    * make idefics2 automappable
    
    * nits
    
    * skip tests
    
    * doctests
    
    * Update src/transformers/models/idefics2/configuration_idefics2.py
    
    Co-authored-by: amyeroberts <22614925+amyeroberts@users.noreply.github.com>
    
    * Update tests/models/clip/test_modeling_clip.py
    
    Co-authored-by: amyeroberts <22614925+amyeroberts@users.noreply.github.com>
    
    * Update tests/models/idefics2/test_modeling_idefics2.py
    
    Co-authored-by: amyeroberts <22614925+amyeroberts@users.noreply.github.com>
    
    * Update tests/models/idefics2/test_modeling_idefics2.py
    
    Co-authored-by: amyeroberts <22614925+amyeroberts@users.noreply.github.com>
    
    * Update src/transformers/configuration_utils.py
    
    Co-authored-by: amyeroberts <22614925+amyeroberts@users.noreply.github.com>
    
    * major update, no need for automap
    
    * clean up
    
    * add FA2 test
    
    * more tests
    
    * style
    
    * skip tests
    
    * why did these started failing now?
    
    * no attributes for FA2 needed
    
    * one tiny test
    
    * address comment about FA2 false warning
    
    * style
    
    * add new models and resolve conflicts
    
    * fix copies
    
    * let it be this way for now, come back tomorrow to review
    
    * some more fixes
    
    * update
    
    * more updates
    
    * update
    
    * fix copies
    
    * style and tests
    
    * another big update
    
    * fix tests
    
    * fix tests
    
    * update
    
    * another update
    
    * fix tests
    
    * fix copies
    
    * fix tests
    
    ---------
    
    Co-authored-by: amyeroberts <22614925+amyeroberts@users.noreply.github.com>

--- src/transformers/models/musicgen_melody/configuration_musicgen_melody.py
+++ src/transformers/models/musicgen_melody/configuration_musicgen_melody.py
@@ -250,20 +250,3 @@ class MusicgenMelodyConfig(PretrainedConfig):
     # This is a property because you might want to change the codec model on the fly
     def sampling_rate(self):
         return self.audio_encoder.sampling_rate
-
-    @property
-    def _attn_implementation(self):
-        # This property is made private for now (as it cannot be changed and a PreTrainedModel.use_attn_implementation method needs to be implemented.)
-        if hasattr(self, "_attn_implementation_internal"):
-            if self._attn_implementation_internal is None:
-                # `config.attn_implementation` should never be None, for backward compatibility.
-                return "eager"
-            else:
-                return self._attn_implementation_internal
-        else:
-            return "eager"
-
-    @_attn_implementation.setter
-    def _attn_implementation(self, value):
-        self._attn_implementation_internal = value
-        self.decoder._attn_implementation = value
