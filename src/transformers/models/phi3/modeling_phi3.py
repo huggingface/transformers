d9f733625c43158f3fa52377f2f8bf49350160f3
Zach Mueller <muellerzr@gmail.com>
2024-10-23 11:24:57 -0400

    Enable Gradient Accumulation fix across all models + trainer fully in forward() (#34283)
    * Enable grad accum fix across all models + trainer fully in forward()
    
    * handle peft case
    
    * Account for DDP: need to run scale tests
    
    * Use accelerator state
    
    * Quality
    
    * Guard
    
    * Experiment w/ only fairseq fix
    
    * Fairseq only
    
    * Revert multiply_grads fix
    
    * Mult by grad accum to fully bring back solution
    
    * Style
    
    * Good to go now
    
    * Skip fx tests for now
    
    * Bookmark
    
    * Working now

--- src/transformers/models/phi3/modeling_phi3.py
+++ src/transformers/models/phi3/modeling_phi3.py
@@ -1209,6 +1209,7 @@ class Phi3ForCausalLM(Phi3PreTrainedModel, GenerationMixin):
         return_dict: Optional[bool] = None,
         cache_position: Optional[torch.LongTensor] = None,
         num_logits_to_keep: int = 0,
+        **loss_kwargs,
     ) -> Union[Tuple, CausalLMOutputWithPast]:
         r"""
         Args:
@@ -1275,7 +1276,7 @@ class Phi3ForCausalLM(Phi3PreTrainedModel, GenerationMixin):
 
         loss = None
         if labels is not None:
-            loss = self.loss_function(logits, labels, self.vocab_size)
+            loss = self.loss_function(logits, labels, self.vocab_size, **loss_kwargs)
 
         if not return_dict:
             output = (logits,) + outputs[1:]
