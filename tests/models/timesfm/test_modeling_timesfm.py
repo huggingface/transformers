18439762ec5eff4510c248d32bbaee7bec08b6c6
Jinan Zhou <jinan.zhou@nutanix.com>
2024-10-24 09:50:18 -0700

    fixing unit tests

--- tests/models/timesfm/test_modeling_timesfm.py
+++ tests/models/timesfm/test_modeling_timesfm.py
@@ -13,37 +13,36 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
-import numpy as np
+import inspect
 import unittest
 from typing import List
 
+import numpy as np
+import torch
+
 from transformers import TimesFMConfig, is_torch_available
 from transformers.testing_utils import (
-    require_accelerate,
-    require_sentencepiece,
-    require_tokenizers,
     require_torch,
-    slow,
     torch_device,
 )
-from transformers.utils import cached_property, is_torch_fx_available
+from transformers.utils import is_torch_fx_available
 
-from ...generation.test_utils import GenerationTesterMixin
+# from ...generation.test_utils import GenerationTesterMixin
+# define our own GenerationTesters
 from ...test_configuration_common import ConfigTester
 from ...test_modeling_common import ModelTesterMixin
-from ...test_pipeline_mixin import PipelineTesterMixin
+
+
+# from ...test_pipeline_mixin import PipelineTesterMixin
 
 
 if is_torch_fx_available():
-    from transformers.utils.fx import symbolic_trace
+    pass
 
 
 if is_torch_available():
-    import torch
 
     from transformers import (
-        AutoTokenizer,
         TimesFMModel,
     )
 
@@ -68,6 +67,7 @@ class TimesFMModelTester:
         use_positional_embedding: bool = True,
         per_core_batch_size: int = 32,
         initializer_factor: float = 1.0,
+        is_training: bool = False,
     ):
         self.parent = parent
         self.patch_len = patch_len
@@ -78,14 +78,15 @@ class TimesFMModelTester:
         self.freq_size = freq_size
         self.model_dim = model_dim
         self.head_dim = head_dim
-        self.num_layers = num_layers
-        self.num_heads = num_heads
+        self.num_hidden_layers = num_layers
+        self.num_attention_heads = num_heads
         self.dropout_rate = dropout_rate
         self.tolerance = tolerance
         self.rms_norm_eps = rms_norm_eps
         self.use_positional_embedding = use_positional_embedding
         self.per_core_batch_size = per_core_batch_size
         self.initializer_factor = initializer_factor
+        self.is_training = is_training
 
     def get_large_model_config(self):
         return TimesFMConfig.from_pretrained("google/timesfm-1.0-200m-pytorch")
@@ -100,8 +101,8 @@ class TimesFMModelTester:
             freq_size=self.freq_size,
             model_dim=self.model_dim,
             head_dim=self.head_dim,
-            num_layers=self.num_layers,
-            num_heads=self.num_heads,
+            num_layers=self.num_hidden_layers,
+            num_heads=self.num_attention_heads,
             dropout_rate=self.dropout_rate,
             tolerance=self.tolerance,
             rms_norm_eps=self.rms_norm_eps,
@@ -145,7 +146,7 @@ class TimesFMModelTester:
 
 @require_torch
 class TimesFMModelTest(
-    ModelTesterMixin, GenerationTesterMixin, PipelineTesterMixin, unittest.TestCase
+    ModelTesterMixin, unittest.TestCase
 ):
     all_model_classes = (TimesFMModel,) if is_torch_available() else ()
     all_generative_model_classes = (TimesFMModel,) if is_torch_available() else ()
@@ -155,6 +156,7 @@ class TimesFMModelTest(
     test_resize_embeddings = False
     test_model_parallel = False
     is_encoder_decoder = False
+    test_inputs_embeds = False
 
     def setUp(self):
         self.model_tester = TimesFMModelTester(self)
@@ -165,5 +167,89 @@ class TimesFMModelTest(
         model = TimesFMModel(config)
         model.to(torch_device)
         model.eval()
-        results = model.run_model(**inputs_dict)
-        assert results
+        results = model(**inputs_dict)
+        assert results.mean_predictions is not None
+
+    def test_attention_outputs(self):
+        if not self.has_attentions:
+            self.skipTest(reason="Model does not output attentions")
+
+        config, inputs_dict = self.model_tester.prepare_config_and_inputs_for_common()
+        config.return_dict = True
+
+        seq_len = getattr(self.model_tester, "seq_length", None)
+        decoder_seq_length = getattr(self.model_tester, "decoder_seq_length", seq_len)
+        encoder_seq_length = getattr(self.model_tester, "encoder_seq_length", seq_len)
+        decoder_key_length = getattr(self.model_tester, "decoder_key_length", decoder_seq_length)
+        encoder_key_length = getattr(self.model_tester, "key_length", encoder_seq_length)
+        chunk_length = getattr(self.model_tester, "chunk_length", None)
+        if chunk_length is not None and hasattr(self.model_tester, "num_hashes"):
+            encoder_seq_length = encoder_seq_length * self.model_tester.num_hashes
+
+        for model_class in self.all_model_classes:
+            inputs_dict["output_attentions"] = True
+            inputs_dict["output_hidden_states"] = False
+            config.return_dict = True
+            model = model_class(config)
+            model.to(torch_device)
+            model.eval()
+            with torch.no_grad():
+                outputs = model(**self._prepare_for_class(inputs_dict, model_class))
+            attentions = outputs.encoder_attentions if config.is_encoder_decoder else outputs.attentions
+            self.assertEqual(len(attentions), self.model_tester.num_hidden_layers)
+
+            # check that output_attentions also work using config
+            del inputs_dict["output_attentions"]
+            config.output_attentions = True
+            model = model_class(config)
+            model.to(torch_device)
+            model.eval()
+            with torch.no_grad():
+                outputs = model(**self._prepare_for_class(inputs_dict, model_class))
+            attentions = outputs.encoder_attentions if config.is_encoder_decoder else outputs.attentions
+            self.assertEqual(len(attentions), self.model_tester.num_hidden_layers)
+
+            out_len = len(outputs)
+
+             # Check attention is always last and order is fine
+            inputs_dict["output_attentions"] = True
+            inputs_dict["output_hidden_states"] = True
+            model = model_class(config)
+            model.to(torch_device)
+            model.eval()
+            with torch.no_grad():
+                outputs = model(**self._prepare_for_class(inputs_dict, model_class))
+
+            if hasattr(self.model_tester, "num_hidden_states_types"):
+                added_hidden_states = self.model_tester.num_hidden_states_types
+            elif self.is_encoder_decoder:
+                added_hidden_states = 2
+            else:
+                added_hidden_states = 1
+
+            self.assertEqual(out_len + added_hidden_states, len(outputs))
+
+            self_attentions = outputs.encoder_attentions if config.is_encoder_decoder else outputs.attentions
+
+            self.assertEqual(len(self_attentions), self.model_tester.num_hidden_layers)
+            if chunk_length is not None:
+                self.assertListEqual(
+                    list(self_attentions[0].shape[-4:]),
+                    [self.model_tester.num_attention_heads, encoder_seq_length, chunk_length, encoder_key_length],
+                )
+            else:
+                self.assertListEqual(
+                    list(self_attentions[0].shape[-3:]),
+                    [self.model_tester.num_attention_heads, encoder_seq_length, encoder_key_length],
+                )
+
+    @unittest.skip(reason="Model does not have input embeddings")
+    def test_model_get_set_embeddings(self):
+        pass
+
+    # the main input name is `inputs`
+    def test_model_main_input_name(self):
+        model_signature = inspect.signature(getattr(TimesFMModel, "forward"))
+        # The main input is the name of the argument after `self`
+        observed_main_input_name = list(model_signature.parameters.keys())[1]
+        self.assertEqual(TimesFMModel.main_input_name, observed_main_input_name)
