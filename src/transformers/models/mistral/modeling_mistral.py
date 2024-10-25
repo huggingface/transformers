86468adad0968312caa5c858d5cb241b3e8fd942
Ilyas Moutawwakil <57442720+IlyasMoutawwakil@users.noreply.github.com>
2024-10-25 09:44:09 +0200

    Fix onnx non-expotable inplace aten op (#34376)
    * fix onnx non-expotable inplace op
    
    * mistral, qwen2, qwen2_vl, starcoder2
    
    * fixup copies

--- src/transformers/models/mistral/modeling_mistral.py
+++ src/transformers/models/mistral/modeling_mistral.py
@@ -961,7 +961,7 @@ class MistralModel(MistralPreTrainedModel):
                     sliding_attend_mask = torch.arange(target_length, device=device) <= (
                         cache_position.reshape(-1, 1) - config.sliding_window
                     )
-                    diagonal_attend_mask |= sliding_attend_mask
+                    diagonal_attend_mask.bitwise_or_(sliding_attend_mask)
             causal_mask *= diagonal_attend_mask
             causal_mask = causal_mask[None, None, :, :].expand(batch_size, 1, -1, -1)
             if attention_mask is not None:
