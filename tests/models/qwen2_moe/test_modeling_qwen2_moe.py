186b8dc190481032892d0a5d68b3db64f4ad4543
Joao Gante <joaofranciscocardosogante@gmail.com>
2024-10-25 11:55:07 +0100

    Tests: upgrade `test_eager_matches_sdpa_generate` (#34386)

--- tests/models/qwen2_moe/test_modeling_qwen2_moe.py
+++ tests/models/qwen2_moe/test_modeling_qwen2_moe.py
@@ -368,12 +368,6 @@ class Qwen2MoeModelTest(ModelTesterMixin, GenerationTesterMixin, PipelineTesterM
     ):
         return True
 
-    # Ignore copy
-    @require_torch_sdpa
-    @slow
-    def test_eager_matches_sdpa_generate(self):
-        super().test_eager_matches_sdpa_generate()
-
     def setUp(self):
         self.model_tester = Qwen2MoeModelTester(self)
         self.config_tester = ConfigTester(self, config_class=Qwen2MoeConfig, hidden_size=37)
