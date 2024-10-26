97d5a444c81448d70b1f5ac351962a42c9e83805
Chengcheng Pei <chengchengpei@outlook.com>
2024-10-25 18:31:19 -0700

    fix docstrings

--- src/transformers/cache_utils.py
+++ src/transformers/cache_utils.py
@@ -1871,7 +1871,8 @@ class OffloadedStaticCache(StaticCache):
         offload_device (`Union[str, torch.device]`, *optional*, defaults to `cpu`):
             The device to offload to. Defaults to CPU.
         max_batch_size (`int`, *optional*): The max batch size with which the model will be used.
-        layer_device_map(`Dict[int, Union[str, torch.device, int]]]`, `optional`): Mapping between the layers and its device.
+        layer_device_map(`Dict[int, Union[str, torch.device, int]]]`, `optional`):
+            Mapping between the layers and its device.
 
     Attributes:
         key_cache (`List[torch.Tensor]`):
@@ -1890,6 +1891,8 @@ class OffloadedStaticCache(StaticCache):
             The device used to offload to.
         dtype (`torch.dtype`):
             The `dtype` used to initializing the cache.
+        layer_device_map(`Dict[int, Union[str, torch.device, int]]]`):
+            Mapping between the layers and its device.
 
     Example:
 
