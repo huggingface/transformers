5efa0577bb0e19e8856d773cda7f97679bb0be80
Cyril Vallez <cyril.vallez@gmail.com>
2024-11-01 10:23:37 +0100

    Update modeling_longt5.py

--- src/transformers/models/longt5/modeling_longt5.py
+++ src/transformers/models/longt5/modeling_longt5.py
@@ -1450,7 +1450,7 @@ class LongT5Stack(LongT5PreTrainedModel):
             # it messes indexing later in decoder-stack because cache object is modified in-place
             past_key_values = None
 
-        past_key_values_length = past_key_values.get_seq_length() if past_key_values is not None else 0
+        past_key_values_length = past_key_values.get_past_seen_tokens() if past_key_values is not None else 0
         if cache_position is None:
             cache_position = torch.arange(
                 past_key_values_length, past_key_values_length + seq_length, device=inputs_embeds.device
