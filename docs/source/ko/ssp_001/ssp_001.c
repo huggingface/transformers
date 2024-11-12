cad7a0bd8d94e1f9a80e6c010d5120697435ad07
Max <naturale@hufs.ac.kr>
2024-11-12 14:30:33 +0900

    updated _toctree.yml

---
+++ docs/source/ko/ssp_001/ssp_001.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
+void alarm_handler() {
+    puts("TIME OUT");
+    exit(-1);
+}
+void initialize() {
+    setvbuf(stdin, NULL, _IONBF, 0);
+    setvbuf(stdout, NULL, _IONBF, 0);
+    signal(SIGALRM, alarm_handler);
+    alarm(30);
+}
+void get_shell() {
+    system("/bin/sh");
+}
+void print_box(unsigned char *box, int idx) {
+    printf("Element of index %d is : %02x\n", idx, box[idx]);
+}
+void menu() {
+    puts("[F]ill the box");
+    puts("[P]rint the box");
+    puts("[E]xit");
+    printf("> ");
+}
+int main(int argc, char *argv[]) {
+    unsigned char box[0x40] = {};
+    char name[0x40] = {};
+    char select[2] = {};
+    int idx = 0, name_len = 0;
+    initialize();
+    while(1) {
+        menu();
+        read(0, select, 2);
+        switch( select[0] ) {
+            case 'F':
+                printf("box input : ");
+                read(0, box, sizeof(box));
+                break;
+            case 'P':
+                printf("Element index : ");
+                scanf("%d", &idx);
+                print_box(box, idx);
+                break;
+            case 'E':
+                printf("Name Size : ");
+                scanf("%d", &name_len);
+                printf("Name : ");
+                read(0, name, name_len);
+                return 0;
+            default:
+                break;
+        }
+    }
+}
