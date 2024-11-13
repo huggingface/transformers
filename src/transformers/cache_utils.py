debb5ad66348d444edf0412f8c96875c80ba9524
Shirin Yamani <yamani.shirin@ucalgary.ca>
2024-11-12 17:01:11 -0700

    ruff errors solved

--- src/transformers/cache_utils.py
+++ src/transformers/cache_utils.py
@@ -1,15 +1,15 @@
 import copy
 import importlib.metadata
 import json
-import os
 import math
+import os
 from dataclasses import dataclass
 from typing import Any, Dict, List, Optional, Tuple, Union
 
 import torch
 import torch.nn as nn
-from torch.nn import functional as F
 from packaging import version
+from torch.nn import functional as F
 
 from .configuration_utils import PretrainedConfig
 from .utils import (
@@ -2124,7 +2124,8 @@ class OffloadedStaticCache(StaticCache):
         self._device_key_cache[layer_idx & 1].copy_(self.key_cache[layer_idx], non_blocking=True)
         self._device_value_cache[layer_idx & 1].copy_(self.value_cache[layer_idx], non_blocking=True)
 
-#------------------SnapKV Cache--------------------------------------
+
+# ------------------SnapKV Cache--------------------------------------
 # Copied from transformers.models.llama.modeling_llama.repeat_kv
 def repeat_kv(hidden_states: torch.Tensor, n_rep: int) -> torch.Tensor:
     """
@@ -2137,15 +2138,16 @@ def repeat_kv(hidden_states: torch.Tensor, n_rep: int) -> torch.Tensor:
     hidden_states = hidden_states[:, :, None, :, :].expand(batch, num_key_value_heads, n_rep, slen, head_dim)
     return hidden_states.reshape(batch, num_key_value_heads * n_rep, slen, head_dim)
 
-class SnapKVCluster():
-    def __init__(self, window_size = 64, max_capacity_prompt = 256 + 64, kernel_size = 5, pooling = 'avgpool'):
+
+class SnapKVCluster:
+    def __init__(self, window_size=64, max_capacity_prompt=256 + 64, kernel_size=5, pooling="avgpool"):
         self.window_size = window_size
         self.max_capacity_prompt = max_capacity_prompt
         assert self.max_capacity_prompt - self.window_size > 0
         self.kernel_size = kernel_size
         self.pooling = pooling
 
-    def reset(self, window_size = 64, max_capacity_prompt = 256 + 64, kernel_size = 5, pooling = 'avgpool'):
+    def reset(self, window_size=64, max_capacity_prompt=256 + 64, kernel_size=5, pooling="avgpool"):
         self.window_size = window_size
         self.max_capacity_prompt = max_capacity_prompt
         assert self.max_capacity_prompt - self.window_size > 0
@@ -2159,47 +2161,56 @@ class SnapKVCluster():
         if q_len < self.max_capacity_prompt:
             return key_states, value_states
         else:
-            attn_weights = torch.matmul(query_states[..., -self.window_size:, :], key_states.transpose(2, 3)) / math.sqrt(head_dim)
-            mask = torch.full((self.window_size, self.window_size), torch.finfo(attn_weights.dtype).min, device=attn_weights.device)
+            attn_weights = torch.matmul(
+                query_states[..., -self.window_size :, :], key_states.transpose(2, 3)
+            ) / math.sqrt(head_dim)
+            mask = torch.full(
+                (self.window_size, self.window_size), torch.finfo(attn_weights.dtype).min, device=attn_weights.device
+            )
             mask_cond = torch.arange(mask.size(-1), device=attn_weights.device)
             mask.masked_fill_(mask_cond < (mask_cond + 1).view(mask.size(-1), 1), 0)
             mask = mask.to(attn_weights.device)
             attention_mask = mask[None, None, :, :]
 
-            attn_weights[:, :, -self.window_size:, -self.window_size:] += attention_mask
+            attn_weights[:, :, -self.window_size :, -self.window_size :] += attention_mask
 
             attn_weights = nn.functional.softmax(attn_weights, dim=-1, dtype=torch.float32).to(query_states.dtype)
-            attn_weights_sum = attn_weights[:, :, -self.window_size:, : -self.window_size].sum(dim = -2)
-            if self.pooling == 'avgpool':
-                attn_cache = F.avg_pool1d(attn_weights_sum, kernel_size = self.kernel_size, padding=self.kernel_size//2, stride=1)
-            elif self.pooling == 'maxpool':
-                attn_cache = F.max_pool1d(attn_weights_sum, kernel_size = self.kernel_size, padding=self.kernel_size//2, stride=1)
+            attn_weights_sum = attn_weights[:, :, -self.window_size :, : -self.window_size].sum(dim=-2)
+            if self.pooling == "avgpool":
+                attn_cache = F.avg_pool1d(
+                    attn_weights_sum, kernel_size=self.kernel_size, padding=self.kernel_size // 2, stride=1
+                )
+            elif self.pooling == "maxpool":
+                attn_cache = F.max_pool1d(
+                    attn_weights_sum, kernel_size=self.kernel_size, padding=self.kernel_size // 2, stride=1
+                )
             else:
-                raise ValueError('Pooling method not supported')
+                raise ValueError("Pooling method not supported")
             indices = attn_cache.topk(self.max_capacity_prompt - self.window_size, dim=-1).indices
             indices = indices.unsqueeze(-1).expand(-1, -1, -1, head_dim)
-            k_past_compress = key_states[:, :, :-self.window_size, :].gather(dim = 2, index = indices)
-            v_past_compress = value_states[:, :, :-self.window_size, :].gather(dim = 2, index = indices)
-            k_cur = key_states[:, :, -self.window_size:, :]
-            v_cur = value_states[:, :, -self.window_size:, :]
-            key_states = torch.cat([k_past_compress, k_cur], dim = 2)
-            value_states = torch.cat([v_past_compress, v_cur], dim = 2)
+            k_past_compress = key_states[:, :, : -self.window_size, :].gather(dim=2, index=indices)
+            v_past_compress = value_states[:, :, : -self.window_size, :].gather(dim=2, index=indices)
+            k_cur = key_states[:, :, -self.window_size :, :]
+            v_cur = value_states[:, :, -self.window_size :, :]
+            key_states = torch.cat([k_past_compress, k_cur], dim=2)
+            value_states = torch.cat([v_past_compress, v_cur], dim=2)
             return key_states, value_states
 
-#initiate snapkv with window_size, etc if not given
+
+# initiate snapkv with window_size, etc if not given
 def init_snapkv(self):
     if not hasattr(self, "kv_cluster"):
-        if not hasattr(self.config, 'window_size'):
+        if not hasattr(self.config, "window_size"):
             self.config.window_size = 32
-        if not hasattr(self.config, 'max_capacity_prompt'):
+        if not hasattr(self.config, "max_capacity_prompt"):
             self.config.max_capacity_prompt = 2048
-        if not hasattr(self.config, 'kernel_size'):
+        if not hasattr(self.config, "kernel_size"):
             self.config.kernel_size = 5
-        if not hasattr(self.config, 'pooling'):
-            self.config.pooling = 'avgpool'
+        if not hasattr(self.config, "pooling"):
+            self.config.pooling = "avgpool"
     self.kv_cluster = SnapKVCluster(
-        window_size = self.config.window_size,
-        max_capacity_prompt = self.config.max_capacity_prompt,
-        kernel_size = self.config.kernel_size,
-        pooling = self.config.pooling
-        )
+        window_size=self.config.window_size,
+        max_capacity_prompt=self.config.max_capacity_prompt,
+        kernel_size=self.config.kernel_size,
+        pooling=self.config.pooling,
+    )
