30d0b3d1a54a33ab5e11e01910eebbf336ee26b5
Your GitHub Username <pratgeesh1999@gmail.com>
2024-11-12 19:01:44 -0500

    Add Kannada translations for documentation

--- src/transformers/models/pegasus/tokenization_pegasus_fast.py
+++ src/transformers/models/pegasus/tokenization_pegasus_fast.py
@@ -1,18 +1,16 @@
-# coding=utf-8
-# Copyright 2020 Google and The HuggingFace Inc. team.
+# ಕೋಡಿಂಗ್=utf-8
+# ಕೃತಿಸ್ವಾಮ್ಯ 2020 ಗೂಗಲ್ ಮತ್ತು ಹಗಿಂಗ್ ಫೇಸ್ ಇಂಕ್ ತಂಡ.
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
+# ಈ ಫೈಲನ್ನು ಅನುಮೋದಿತ ಪರವಾನಗಿಯ ಅಡಿಯಲ್ಲಿ ಮಾತ್ರ ಬಳಸಬಹುದು
+# ಪರವಾನಗಿಯ ಪ್ರತಿಯನ್ನು ನೀವು ಈ ಲಿಂಕ್‌ನಲ್ಲಿ ಪಡೆಯಬಹುದು:
 #
 #     http://www.apache.org/licenses/LICENSE-2.0
 #
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-"""Tokenization class for model PEGASUS."""
+# ತಿಳುವಳಿಕೆ ಅಥವಾ ಬರವಣಿಗೆಯಲ್ಲಿ ಒಪ್ಪಿಗೆಯಾದ ಹೊರತು ಈ ಸಾಫ್ಟ್‌ವೇರ್
+# ಪರವಾನಗಿಯ ಅಡಿಯಲ್ಲಿ ವಿತರಿಸಲ್ಪಡುತ್ತದೆ "ಯಥಾಸ್ಥಿತಿ" ಆಧಾರದಲ್ಲಿ,
+# ಯಾವುದೇ ಖಾತರಿ ಅಥವಾ ಷರತ್ತುಗಳಿಲ್ಲದೆ. ಅನುಮತಿಗಳು ಮತ್ತು
+# ಪರವಾನಗಿಯ ನಿಯಮಗಳನ್ನು ನಿಯಂತ್ರಿಸುವ ಕಾನೂನುಗಳ ಪ್ರಕಾರ ನೋಡಿ.
+"""ಪೆಗಾಸಸ್ ಮಾದರಿಗೆ ಟೋಕನೈಜೇಶನ್ ವರ್ಗ."""
 
 import os
 from shutil import copyfile
@@ -38,47 +36,40 @@ VOCAB_FILES_NAMES = {"vocab_file": "spiece.model", "tokenizer_file": "tokenizer.
 
 class PegasusTokenizerFast(PreTrainedTokenizerFast):
     r"""
-    Construct a "fast" PEGASUS tokenizer (backed by HuggingFace's *tokenizers* library). Based on
-    [Unigram](https://huggingface.co/docs/tokenizers/python/latest/components.html?highlight=unigram#models).
+    "ಫಾಸ್ಟ್" ಪೆಗಾಸಸ್ ಟೋಕನೈಜರ್ ನಿರ್ಮಿಸು (ಹಗಿಂಗ್ ಫೇಸ್ನ ಟೋಕನೈಜರ್ಸ್ ಗ್ರಂಥಾಲಯದ ಬೆಂಬಲದೊಂದಿಗೆ). ಆಧಾರಿತವಾಗಿದೆ
+    [ಯುನಿಗ್ರಾಮ್](https://huggingface.co/docs/tokenizers/python/latest/components.html?highlight=unigram#models).
 
-    This tokenizer inherits from [`PreTrainedTokenizerFast`] which contains most of the main methods. Users should
-    refer to this superclass for more information regarding those methods.
+    ಈ ಟೋಕನೈಜರ್ ಈ ಮೇಲ್ವಿಚಾರಕ ವರ್ಗದಿಂದ ಪಾರಂಪರಿಕ ವಿಧಾನಗಳನ್ನು ಹೊಂದಿದೆ [`PreTrainedTokenizerFast`]. ಬಳಕೆದಾರರು ಈ
+    ಮುಖ್ಯ ವಿಧಾನಗಳಿಗಾಗಿ ಈ ಮೇಲ್ವಿಚಾರಕ ವರ್ಗವನ್ನು ಉಲ್ಲೇಖಿಸಬಹುದು.
 
-    Args:
+    ಹೊಂದಿಸಿದ ವಿಧಾನಗಳು:
         vocab_file (`str`):
-            [SentencePiece](https://github.com/google/sentencepiece) file (generally has a *.spm* extension) that
-            contains the vocabulary necessary to instantiate a tokenizer.
-        pad_token (`str`, *optional*, defaults to `"<pad>"`):
-            The token used for padding, for example when batching sequences of different lengths.
-        eos_token (`str`, *optional*, defaults to `"</s>"`):
-            The end of sequence token.
+            [ಸೆಂಟೆನ್ಸ್‌ಪೀಸ್](https://github.com/google/sentencepiece) ಫೈಲ್ (ಸಾಮಾನ್ಯವಾಗಿ *.spm* ವಿಸ್ತರಣೆಯನ್ನು ಹೊಂದಿದೆ) ಅದು
+            ಟೋಕನೈಜರ್ ಉದ್ಭವಗೊಳ್ಳಲು ಅಗತ್ಯವಿರುವ ಶಬ್ದಸಂಗ್ರಹವನ್ನು ಒಳಗೊಂಡಿದೆ.
+        pad_token (`str`, *ಐಚ್ಛಿಕ*, ಡೀಫಾಲ್ಟ್ ಗೆ `"<pad>"`):
+            ಉದಾಹರಣೆಗೆ ವಿವಿಧ ಉದ್ದಗಳ ಸರಣಿಗಳನ್ನು ಬ್ಯಾಚ್ ಮಾಡುವಾಗ ಬಳಸುವ ಪ್ಯಾಡ್ ಟೋಕನ್.
+        eos_token (`str`, *ಐಚ್ಛಿಕ*, ಡೀಫಾಲ್ಟ್ ಗೆ `"</s>"`):
+            ಸರಣಿಯ ಕೊನೆಯ ಟೋಕನ್.
 
-            <Tip>
+            <ಸಲಹೆ>
 
-            When building a sequence using special tokens, this is not the token that is used for the end of sequence.
-            The token used is the `sep_token`.
+            ವಿಶೇಷ ಟೋಕನ್ಗಳನ್ನು ಬಳಸುವಾಗ ಈ ಟೋಕನ್ ಸರಣಿಯ ಕೊನೆಗೆ ಬಳಸುವ ಟೋಕನ್ ಅಲ್ಲ.
+            ಬಳಸುವ ಟೋಕನ್ `sep_token`.
 
-            </Tip>
+            </ಸಲಹೆ>
 
-        unk_token (`str`, *optional*, defaults to `"<unk>"`):
-            The unknown token. A token that is not in the vocabulary cannot be converted to an ID and is set to be this
-            token instead.
-        mask_token (`str`, *optional*, defaults to `"<mask_2>"`):
-            The token used for masking single token values. This is the token used when training this model with masked
-            language modeling (MLM). This is the token that the PEGASUS encoder will try to predict during pretraining.
-            It corresponds to *[MASK2]* in [PEGASUS: Pre-training with Extracted Gap-sentences for Abstractive
-            Summarization](https://arxiv.org/pdf/1912.08777.pdf).
-        mask_token_sent (`str`, *optional*, defaults to `"<mask_1>"`):
-            The token used for masking whole target sentences. This is the token used when training this model with gap
-            sentences generation (GSG). This is the sentence that the PEGASUS decoder will try to predict during
-            pretraining. It corresponds to *[MASK1]* in [PEGASUS: Pre-training with Extracted Gap-sentences for
-            Abstractive Summarization](https://arxiv.org/pdf/1912.08777.pdf).
-        additional_special_tokens (`List[str]`, *optional*):
-            Additional special tokens used by the tokenizer. If no additional_special_tokens are provided <mask_2> and
-            <unk_2, ..., unk_102> are used as additional special tokens corresponding to the [original PEGASUS
-            tokenizer](https://github.com/google-research/pegasus/blob/939830367bcf411193d2b5eca2f2f90f3f9260ca/pegasus/ops/pretrain_parsing_ops.cc#L66)
-            that uses the tokens 2 - 104 only for pretraining
-    """
+        unk_token (`str`, *ಐಚ್ಛಿಕ*, ಡೀಫಾಲ್ಟ್ ಗೆ `"<unk>"`):
+            ಅಜ್ಞಾತ ಟೋಕನ್. ಶಬ್ದಸಂಗ್ರಹದಲ್ಲಿಲ್ಲದ ಟೋಕನ್ನು ID ಗೆ ಪರಿವರ್ತಿಸಲಾಗದು ಮತ್ತು ಇದನ್ನು ಈ ಟೋಕನ್ ಎಂದು ಹೊಂದಿಸಲಾಗುತ್ತದೆ.
+        mask_token (`str`, *ಐಚ್ಛಿಕ*, ಡೀಫಾಲ್ಟ್ ಗೆ `"<mask_2>"`):
+            ಮಾಸ್ಕ್ ಮಾಡಲು ಬಳಸುವ ಟೋಕನ್. ಇದು ಮಾಸ್ಕ್ಡ್ ಭಾಷಾ ಮಾದರಿಕರಣ (MLM) ಜೊತೆಗೆ ಈ ಮಾದರಿಯನ್ನು ತರಬೇತಿ ಮಾಡುವಾಗ ಬಳಸಲಾಗುತ್ತದೆ.
+            ಇದು [ಪೆಗಾಸಸ್: ಸಾರಾಂಶಕ್ಕಾಗಿ ತೆಗೆದುಕೊಂಡ ಗ್ಯಾಪ್-ವಾಕ್ಯಗಳೊಂದಿಗೆ ಪೂರ್ವ-ತರಬೇತಿ](https://arxiv.org/pdf/1912.08777.pdf)ನಲ್ಲಿ *[MASK2]*ಗೆ ಹೊಂದಿಸಲಾಗಿದೆ.
+        mask_token_sent (`str`, *ಐಚ್ಛಿಕ*, ಡೀಫಾಲ್ಟ್ ಗೆ `"<mask_1>"`):
+            ಗುರಿ ವಾಕ್ಯಗಳನ್ನು ಮಾಸ್ಕ್ ಮಾಡಲು ಬಳಸುವ ಟೋಕನ್. ಇದು ಗ್ಯಾಪ್ ವಾಕ್ಯಗಳ ಉತ್ಪಾದನೆ (GSG) ಜೊತೆಗೆ ಈ ಮಾದರಿಯನ್ನು ತರಬೇತಿ ಮಾಡುವಾಗ ಬಳಸಲಾಗುತ್ತದೆ.
+            ಇದು [ಪೆಗಾಸಸ್: ಸಾರಾಂಶಕ್ಕಾಗಿ ತೆಗೆದುಕೊಂಡ ಗ್ಯಾಪ್-ವಾಕ್ಯಗಳೊಂದಿಗೆ ಪೂರ್ವ-ತರಬೇತಿ](https://arxiv.org/pdf/1912.08777.pdf)ನಲ್ಲಿ *[MASK1]*ಗೆ ಹೊಂದಿಸಲಾಗಿದೆ.
+        additional_special_tokens (`List[str]`, *ಐಚ್ಛಿಕ*):
+            ಟೋಕನೈಜರ್ ಬಳಸುವ ಹೆಚ್ಚುವರಿ ವಿಶೇಷ ಟೋಕನ್ಗಳು. ಹೆಚ್ಚುವರಿ ವಿಶೇಷ ಟೋಕನ್ಗಳನ್ನು ಒದಗಿಸದಿದ್ದರೆ, <mask_2> ಮತ್ತು <unk_2, ..., unk_102> ಗಳನ್ನು ಬಳಸಲಾಗುತ್ತದೆ.
+            ಪ್ರಿಟ್ರೇನಿಂಗ್ಗಾಗಿ ಟೋಕನ್ಗಳು 2 - 104 ಅನ್ನು ಮಾತ್ರ ಬಳಸುವ [ಮೂಲ ಪೆಗಾಸಸ್ ಟೋಕನೈಜರ್](https://github.com/google-research/pegasus/blob/939830367bcf411193d2b5eca2f2f90f3f9260ca/pegasus/ops/pretrain_parsing_ops.cc#L66) ಗೆ ಹೊಂದಿಸಲಾಗಿದೆ.
+   """
 
     vocab_files_names = VOCAB_FILES_NAMES
     slow_tokenizer_class = PegasusTokenizer
@@ -94,7 +85,7 @@ class PegasusTokenizerFast(PreTrainedTokenizerFast):
         mask_token="<mask_2>",
         mask_token_sent="<mask_1>",
         additional_special_tokens=None,
-        offset=103,  # entries 2 - 104 are only used for pretraining
+        offset=103,   # ಪ್ರಿಟ್ರೇನಿಂಗ್ಗಾಗಿ ಮಾತ್ರ 2 - 104 ಎಂಟ್ರಿಗಳನ್ನು ಬಳಸಲಾಗುತ್ತದೆ
         **kwargs,
     ):
         self.offset = offset
@@ -111,7 +102,7 @@ class PegasusTokenizerFast(PreTrainedTokenizerFast):
                 if mask_token_sent not in additional_special_tokens and mask_token_sent is not None
                 else additional_special_tokens
             )
-            # fill additional tokens with ..., <unk_token_102> in case not all additional tokens are already taken
+            # ಎಲ್ಲಾ ಹೆಚ್ಚುವರಿ ಟೋಕನ್ಗಳನ್ನು ತುಂಬಿಸಲು..., <unk_token_102> ಬಳಸಲಾಗುತ್ತದೆ
             additional_special_tokens_extended += [
                 f"<unk_{i}>" for i in range(len(additional_special_tokens_extended), self.offset - 1)
             ]
@@ -126,8 +117,8 @@ class PegasusTokenizerFast(PreTrainedTokenizerFast):
             additional_special_tokens = [mask_token_sent] if mask_token_sent is not None else []
             additional_special_tokens += [f"<unk_{i}>" for i in range(2, self.offset)]
 
-        # pegasus was design to support changing the index of the first tokens. If one of the padding/eos/unk/mask token
-        # is different from default, we must rebuild the vocab
+        # ಪೆಗಾಸಸ್ ಮೊದಲ ಟೋಕನ್ಗಳ ಸೂಚ್ಯಂಕವನ್ನು ಬದಲಾವಣೆ ಮಾಡಲು ವಿನ್ಯಾಸವಾಗಿದೆ. ಪ್ಯಾಡ್/ಇಓಎಸ್/ಅಂಕ್/ಮಾಸ್ಕ್ ಟೋಕನ್
+        # ಡೀಫಾಲ್ಟ್ ಗಿಂತ ಬೇರೆಯಾಗಿದ್ದರೆ, ನಾವು ಶಬ್ದಸಂಗ್ರಹವನ್ನು ಮರುನಿರ್ಮಾಣ ಮಾಡಬೇಕು
         from_slow = kwargs.pop("from_slow", None)
         from_slow = from_slow or str(pad_token) != "<pad>" or str(eos_token) != "</s>" or str(unk_token) != "<unk>"
 
@@ -153,8 +144,8 @@ class PegasusTokenizerFast(PreTrainedTokenizerFast):
         return os.path.isfile(self.vocab_file) if self.vocab_file else False
 
     def _special_token_mask(self, seq):
-        all_special_ids = set(self.all_special_ids)  # call it once instead of inside list comp
-        all_special_ids.remove(self.unk_token_id)  # <unk> is only sometimes special
+        all_special_ids = set(self.all_special_ids)   # ಒಮ್ಮೆ ಕರೆಯಲಾಗುವುದು ಬದಲು ಲಿಸ್ಟ್ ಕಂಪ್ರಹೆನ್ಶನ್ನಲ್ಲಿ
+        all_special_ids.remove(self.unk_token_id)   # ಒಮ್ಮೆ ಕರೆಯಲಾಗುವುದು ಬದಲು ಲಿಸ್ಟ್ ಕಂಪ್ರಹೆನ್ಶನ್ನಲ್ಲಿ
 
         if all_special_ids != set(range(len(self.additional_special_tokens) + 3)):
             raise ValueError(
@@ -177,23 +168,23 @@ class PegasusTokenizerFast(PreTrainedTokenizerFast):
 
     def build_inputs_with_special_tokens(self, token_ids_0, token_ids_1=None) -> List[int]:
         """
-        Build model inputs from a sequence by adding eos to the end. no bos token is added to the front.
+        ಒಂದು ಸರಣಿಯಿಂದ ಮಾದರಿ ನಿರ್ಮಾಣಗಳನ್ನು ಕೊನೆಯ ಟೋಕನ್ ಸೇರಿಸುವ ಮೂಲಕ ಕಟ್ಟುವುದು. ಮುಂದಿನ ಬಾಸ್ ಟೋಕನ್ ಸೇರ್ಪಡೆಯಾಗಿಲ್ಲ.
 
-        - single sequence: `X </s>`
-        - pair of sequences: `A B </s>` (not intended use)
+        - ಏಕ ಸರಣಿ: `X </s>`
+        - ಜೋಡಿ ಸರಣಿಗಳು: `A B </s>` (ಉದ್ದೇಶಿತ ಬಳಕೆಯಲ್ಲಿಲ್ಲ)
 
-        Args:
+        ಹೊಂದಿಸಿದ ವಿಧಾನಗಳು:
             token_ids_0 (`List[int]`):
-                List of IDs to which the special tokens will be added
-            token_ids_1 (`List[int]`, *optional*):
-                Optional second list of IDs for sequence pairs.
+                ವಿಶೇಷ ಟೋಕನ್ಗಳನ್ನು ಸೇರಿಸಲಾಗುವ IDಗಳ ಪಟ್ಟಿ
+            token_ids_1 (`List[int]`, *ಐಚ್ಛಿಕ*):
+                ಜೋಡಿ ಸರಣಿಗಳಿಗಾಗಿ ಎರಡನೇ ಐಚ್ಛಿಕ IDಗಳ ಪಟ್ಟಿ.
 
-        Returns:
-            `List[int]`: list of [input IDs](../glossary#input-ids) with the appropriate special tokens.
+        ಮರುಪಡೆಯುವಿಕೆಗಳು:
+            `List[int]`: ಸೂಕ್ತ ವಿಶೇಷ ಟೋಕನ್ಗಳೊಂದಿಗೆ [input IDs](../glossary#input-ids) ಪಟ್ಟಿ.
         """
         if token_ids_1 is None:
             return token_ids_0 + [self.eos_token_id]
-        # We don't expect to process pairs, but leave the pair logic for API consistency
+        # ಜೋಡಿ ಸರಣಿಗಳನ್ನು ಸಂಸ್ಕರಿಸುವುದನ್ನು ನಾವು ನಿರೀಕ್ಷಿಸುತ್ತಿಲ್ಲ, ಆದರೆ API ಸಮರ್ಥನೆಗಾಗಿ ಜೋಡಿ ತರ್ಕವನ್ನು ಬಿಟ್ಟಿದ್ದೇವೆ
         return token_ids_0 + token_ids_1 + [self.eos_token_id]
 
     def save_vocabulary(self, save_directory: str, filename_prefix: Optional[str] = None) -> Tuple[str]:
