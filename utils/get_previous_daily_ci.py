756c2edae2e7c11b1a0abae82f80688fc2629505
ydshieh <ydshieh@users.noreply.github.com>
2024-11-15 22:01:07 +0100

    run_ci_manually

--- utils/get_previous_daily_ci.py
+++ utils/get_previous_daily_ci.py
@@ -30,15 +30,8 @@ def get_daily_ci_runs(token, num_runs=7):
 
 
 def get_last_daily_ci_runs(token):
-    """Get the last completed workflow run id of the scheduled (daily) CI."""
-    workflow_runs = get_daily_ci_runs(token)
-    workflow_run_id = None
-    for workflow_run in workflow_runs:
-        if workflow_run["status"] == "completed":
-            workflow_run_id = workflow_run["id"]
-            break
+    return "11752574759"
 
-    return workflow_run_id
 
 
 def get_last_daily_ci_run_commit(token):
