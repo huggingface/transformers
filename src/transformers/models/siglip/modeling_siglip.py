3ba189414b60006d078465f16c294cbc59066dd2
Merve Noyan <mervenoyan@Merve-MacBook-Pro.local>
2024-11-17 22:44:10 +0100

    final version

--- src/transformers/models/siglip/modeling_siglip.py
+++ src/transformers/models/siglip/modeling_siglip.py
@@ -926,11 +926,7 @@ class SiglipTextTransformer(nn.Module):
         self.embeddings = SiglipTextEmbeddings(config)
         self.encoder = SiglipEncoder(config)
         self.final_layer_norm = nn.LayerNorm(embed_dim, eps=config.layer_norm_eps)
-        self.head = (
-            nn.Linear(embed_dim, embed_dim, bias=False)
-            if config.has_head
-            else lambda x: x @ torch.eye(embed_dim, device=x.device)
-        )
+        self.head = nn.Linear(embed_dim, embed_dim)
         self._use_flash_attention_2 = config._attn_implementation == "flash_attention_2"
 
     @add_start_docstrings_to_model_forward(SIGLIP_TEXT_INPUTS_DOCSTRING)
