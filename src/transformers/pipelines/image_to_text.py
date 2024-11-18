4bf47d22c8fbad2d4c77549d9afea28ccf4997b0
Spycsh <sihan.chen@intel.com>
2024-11-17 23:02:46 -0800

    remove padding default

--- src/transformers/pipelines/image_to_text.py
+++ src/transformers/pipelines/image_to_text.py
@@ -161,7 +161,7 @@ class ImageToTextPipeline(Pipeline):
                     model_inputs = model_inputs.to(self.torch_dtype)
 
             elif model_type == "qwen2_vl":
-                model_inputs = self.processor(text=prompt, images=[image], padding=True, return_tensors=self.framework)
+                model_inputs = self.processor(text=prompt, images=[image], return_tensors=self.framework)
                 if self.framework == "pt":
                     model_inputs = model_inputs.to(self.torch_dtype)
 
