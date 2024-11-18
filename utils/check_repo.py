febc45c3c14594796d07f9679071e846dfef2d57
Jonathan Flynn <jon.flynn@guardian.co.uk>
2024-11-18 19:27:48 +0000

    add discriminator to IGNORE_NON_AUTO_CONFIGURED in check_repo.py

--- utils/check_repo.py
+++ utils/check_repo.py
@@ -186,6 +186,7 @@ IGNORE_NON_AUTO_CONFIGURED = PRIVATE_MODELS.copy() + [
     "BlipForQuestionAnswering",
     "BlipVisionModel",
     "BlipTextLMHeadModel",
+    "EncodecDiscriminator",
     "BlipTextModel",
     "BrosSpadeEEForTokenClassification",
     "BrosSpadeELForTokenClassification",
