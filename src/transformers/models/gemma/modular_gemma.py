8dfa29aae16fff2b9049609675e1bc989d432766
Joao Gante <joao@huggingface.co>
2024-11-18 17:33:14 +0000

    [test all] make fix-copies :D

--- src/transformers/models/gemma/modular_gemma.py
+++ src/transformers/models/gemma/modular_gemma.py
@@ -886,7 +886,7 @@ class GemmaModel(LlamaModel):
         all_self_attns = () if output_attentions else None
         next_decoder_cache = None
 
-        for decoder_layer in self.layers:
+        for decoder_layer in self.layers[: self.num_hidden_layers]:
             if output_hidden_states:
                 all_hidden_states += (hidden_states,)
 
