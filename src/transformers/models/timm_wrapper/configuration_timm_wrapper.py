dede4e45ad2a2051d9212f3d1297ccb157141492
Pavel Iakubovskii <qubvel@gmail.com>
2024-11-19 22:54:42 +0000

    Managing num_labels <-> num_classes attributes

--- src/transformers/models/timm_wrapper/configuration_timm_wrapper.py
+++ src/transformers/models/timm_wrapper/configuration_timm_wrapper.py
@@ -15,6 +15,8 @@
 
 """Configuration for Backbone models"""
 
+from typing import Any, Dict
+
 from ...configuration_utils import PretrainedConfig
 from ...utils import logging
 
@@ -54,7 +56,28 @@ class TimmWrapperConfig(PretrainedConfig):
 
     def __init__(self, initializer_range: float = 0.02, **kwargs):
         self.initializer_range = initializer_range
-
-        # timm models have `num_classes` instead of `num_labels`
-        kwargs["num_labels"] = kwargs.get("num_labels", kwargs.get("num_classes", 2))
         super().__init__(**kwargs)
+
+    @classmethod
+    def from_dict(cls, config_dict: Dict[str, Any], **kwargs) -> "PretrainedConfig":
+        # timm config stores `num_classes` attribute in root of config and in "pretrained_cfg" dict of config.
+        # We are removing this attributes in order to have native `transformers` num_labels attribute in config
+        # and not to have duplicated attributes
+
+        num_labels_in_kwargs = kwargs.pop("num_labels", None)
+        num_labels_in_dict = config_dict.pop("num_classes", None)
+
+        # passed num_labels has priority over num_classes in config_dict
+        kwargs["num_labels"] = num_labels_in_kwargs or num_labels_in_dict
+
+        # pop num_classes from "pretrained_cfg",
+        # it is not necessary no have it, only root one is used in timm
+        if "pretrained_cfg" in config_dict and "num_classes" in config_dict["pretrained_cfg"]:
+            config_dict["pretrained_cfg"].pop("num_classes", None)
+
+        return super().from_dict(config_dict, **kwargs)
+
+    def to_dict(self) -> Dict[str, Any]:
+        output = super().to_dict()
+        output["num_classes"] = self.num_labels
+        return output
