e58395c150119a28f8f98feab90c5de064008fd1
Pavel Iakubovskii <qubvel@gmail.com>
2024-11-20 23:05:23 +0000

    Bump Replicate guard to torch 2.5

--- src/transformers/pytorch_utils.py
+++ src/transformers/pytorch_utils.py
@@ -30,6 +30,7 @@ logger = logging.get_logger(__name__)
 
 parsed_torch_version_base = version.parse(version.parse(torch.__version__).base_version)
 
+is_torch_greater_or_equal_than_2_5 = parsed_torch_version_base >= version.parse("2.5")
 is_torch_greater_or_equal_than_2_4 = parsed_torch_version_base >= version.parse("2.4")
 is_torch_greater_or_equal_than_2_3 = parsed_torch_version_base >= version.parse("2.3")
 is_torch_greater_or_equal_than_2_2 = parsed_torch_version_base >= version.parse("2.2")
@@ -39,7 +40,7 @@ is_torch_greater_or_equal_than_1_13 = parsed_torch_version_base >= version.parse
 is_torch_greater_or_equal_than_1_12 = parsed_torch_version_base >= version.parse("1.12")
 
 
-if is_torch_greater_or_equal_than_2_4:
+if is_torch_greater_or_equal_than_2_5:
     from torch.distributed.tensor import Replicate
     from torch.distributed.tensor.parallel import (
         ColwiseParallel,
