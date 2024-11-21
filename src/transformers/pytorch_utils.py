b73fe9689c000a92a254c08bfbc35b6d4b12e8c0
Logan Adams <loadams@microsoft.com>
2024-11-21 09:06:54 -0800

    Update PR with feedback

--- src/transformers/pytorch_utils.py
+++ src/transformers/pytorch_utils.py
@@ -21,7 +21,7 @@ from packaging import version
 from safetensors.torch import storage_ptr, storage_size
 from torch import nn
 
-from .utils import is_torch_xla_available, logging
+from .utils import is_torch_xla_available, logging, is_torch_greater_or_equal
 
 
 ALL_LAYERNORM_LAYERS = [nn.LayerNorm]
@@ -40,7 +40,7 @@ is_torch_greater_or_equal_than_1_13 = parsed_torch_version_base >= version.parse
 is_torch_greater_or_equal_than_1_12 = parsed_torch_version_base >= version.parse("1.12")
 
 
-if is_torch_greater_or_equal_than_2_5:
+if is_torch_greater_or_equal("2.5"):
     from torch.distributed.tensor import Replicate
     from torch.distributed.tensor.parallel import (
         ColwiseParallel,
