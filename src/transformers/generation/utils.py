09cc04a36adf84c4a682e16907f6818ff53a796a
ydshieh <ydshieh@users.noreply.github.com>
2024-11-23 18:06:42 +0100

    try 1

--- src/transformers/generation/utils.py
+++ src/transformers/generation/utils.py
@@ -3225,14 +3225,20 @@ class GenerationMixin:
         unfinished_sequences = torch.ones(batch_size, dtype=torch.long, device=input_ids.device)
         model_kwargs = self._get_initial_cache_position(input_ids, model_kwargs)
 
-        def model_forward(model, *args, **kwargs):
-            return model.forward(*args, **kwargs)
+        model_forward = None
+        def foo(model, model_inputs):
 
-        if isinstance(model_kwargs.get("past_key_values"), StaticCache):
-            if self.device.type == "cuda":
-                logger.warning_once("Using `torch.compile`.")
-                os.environ["TOKENIZERS_PARALLELISM"] = "0"
-                model_forward = torch.compile(model_forward, mode="reduce-overhead", fullgraph=True)
+            def model_forward(model, *args, **kwargs):
+                return model.forward(*args, **kwargs)
+
+            if isinstance(model_kwargs.get("past_key_values"), StaticCache):
+                if model.device.type == "cuda":
+                    logger.warning_once("Using `torch.compile`.")
+                    os.environ["TOKENIZERS_PARALLELISM"] = "0"
+                    model_forward = torch.compile(model_forward, mode="reduce-overhead", fullgraph=True)
+
+            outputs = model_forward(model, return_dict=True, **model_inputs)
+            return model_forward
 
         i = 0
         while self._has_unfinished_sequences(
@@ -3249,7 +3255,15 @@ class GenerationMixin:
                 outputs = self(**model_inputs, return_dict=True)
                 i += 1
             else:
-                outputs = model_forward(self, return_dict=True, **model_inputs)
+                if i == 1:
+                    import threading
+                    t = threading.Thread(target=foo, args=(self, model_inputs))
+                    t.start()
+                    t.join()
+                if model_forward is not None:
+                    outputs = model_forward(self, return_dict=True, **model_inputs)
+                else:
+                    outputs = self(**model_inputs, return_dict=True)
 
             # synced_gpus: don't waste resources running the code we don't need; kwargs must be updated before skipping
             model_kwargs = self._update_model_kwargs_for_generation(
