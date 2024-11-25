c873667b6097b6dbc869b728a2da732b7f7a1227
Arthur Zucker <arthur.zucker@gmail.com>
2024-11-25 11:38:07 +0100

    values

--- src/transformers/trainer.py
+++ src/transformers/trainer.py
@@ -620,7 +620,7 @@ class Trainer:
             else unwrapped_model.get_base_model().forward
         )
         forward_params = inspect.signature(model_forward).parameters
-        self.model_accepts_loss_kwargs = any(k.kind == inspect.Parameter.VAR_KEYWORD for k in forward_params)
+        self.model_accepts_loss_kwargs = any(k.kind == inspect.Parameter.VAR_KEYWORD for k in forward_params.values())
 
         self.neftune_noise_alpha = args.neftune_noise_alpha
 
