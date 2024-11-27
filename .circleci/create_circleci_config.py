af2ddf2575672160aa889467423377dcd93ec277
ydshieh <ydshieh@users.noreply.github.com>
2024-11-27 23:38:39 +0100

    try 1

--- .circleci/create_circleci_config.py
+++ .circleci/create_circleci_config.py
@@ -399,10 +399,6 @@ def create_circleci_config(folder=None):
     collection_job.job_name = "collection_job"
     jobs = jobs + [collection_job]
 
-    _jobs = []
-    for job in jobs:
-        _jobs.append(job.job_name)
-
     config = {
         "version": "2.1",
         "parameters": {
@@ -413,7 +409,7 @@ def create_circleci_config(folder=None):
             **{j.job_name + "_parallelism":{"type":"integer", "default":1} for j in jobs},
         },
         "jobs": {j.job_name: j.to_dict() for j in jobs},
-        "workflows": {"version": 2, "run_tests": {"jobs": _jobs}},
+        "workflows": {"version": 2, "run_tests": {"jobs": [j.job_name for j in jobs]}}
     }
     with open(os.path.join(folder, "generated_config.yml"), "w") as f:
         f.write(yaml.dump(config, sort_keys=False, default_flow_style=False).replace("' << pipeline", " << pipeline").replace(">> '", " >>"))
