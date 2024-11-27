ee0fbf5fa257617d664e869460b38cd25b3f18da
Sigbjørn Skjæret <sigbjorn.skjaeret@scala.com>
2024-11-27 15:54:24 +0100

    reorder imports

--- tests/generation/test_streamers.py
+++ tests/generation/test_streamers.py
@@ -13,8 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import unittest
 import pytest
+import unittest
 from queue import Empty
 from threading import Thread
 
