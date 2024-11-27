ab1cb6915c3ea5eb47d9e9c459429c0f823f2462
Elvir Crncevic <elvircrn@gmail.com>
2024-11-27 17:01:38 +0100

    Isort

--- src/transformers/__init__.py
+++ src/transformers/__init__.py
@@ -991,8 +991,8 @@ _import_structure = {
         "GPTQConfig",
         "HqqConfig",
         "QuantoConfig",
-        "TorchAoConfig",
         "SpQRConfig",
+        "TorchAoConfig",
     ],
 }
 
