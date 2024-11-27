777ff0960b7b7ebd01e314170b66c4757086973c
Steven Bucaille <steven.bucaille@buawei.com>
2024-11-27 13:47:09 +0000

    chore: changed do_grayscale SuperGlueImageProcessing default value to True

--- src/transformers/models/superglue/image_processing_superglue.py
+++ src/transformers/models/superglue/image_processing_superglue.py
@@ -267,7 +267,7 @@ class SuperGlueImageProcessor(BaseImageProcessor):
         rescale_factor (`int` or `float`, *optional*, defaults to `1/255`):
             Scale factor to use if rescaling the image. Can be overriden by `rescale_factor` in the `preprocess`
             method.
-        do_grayscale (`bool`, *optional*, defaults to `False`):
+        do_grayscale (`bool`, *optional*, defaults to `True`):
             Whether to convert the image to grayscale. Can be overriden by `do_grayscale` in the `preprocess` method.
     """
 
@@ -280,7 +280,7 @@ class SuperGlueImageProcessor(BaseImageProcessor):
         resample: PILImageResampling = PILImageResampling.BILINEAR,
         do_rescale: bool = True,
         rescale_factor: float = 1 / 255,
-        do_grayscale: bool = False,
+        do_grayscale: bool = True,
         **kwargs,
     ) -> None:
         super().__init__(**kwargs)
