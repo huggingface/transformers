f7ab4300a8bb942ec97819d84780b523933ab7c6
ydshieh <ydshieh@users.noreply.github.com>
2024-11-28 10:15:54 +0100

    try 1

--- utils/process_circleci_workflow_test_reports.py
+++ utils/process_circleci_workflow_test_reports.py
@@ -1,6 +1,8 @@
+import argparse
 import json
 import os
-import argparse
+import requests
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -8,62 +10,50 @@ if __name__ == '__main__':
     args = parser.parse_args()
     workflow_id = args.workflow_id
 
-    command = f'curl -o workflow_jobs.json --location --request GET "https://circleci.com/api/v2/workflow/{workflow_id}/job" --header "Circle-Token: $CIRCLE_TOKEN"'
-    os.system(command)
+    r = requests.get(f"https://circleci.com/api/v2/workflow/{workflow_id}/job", headers={"Circle-Token": os.environ.get('CIRCLE_TOKEN', "")})
+    jobs = r.json()["items"]
 
-    # os.system("tail -1000 workflow_jobs.json")
-
-    with open("workflow_jobs.json") as fp:
-        jobs = json.load(fp)["items"]
-
-    os.system(f'mkdir outputs')
+    os.makedirs("outputs", exist_ok=True)
 
     workflow_summary = {}
     # for each job, download artifacts
     for job in jobs:
 
-        project_slug = f'gh/{os.environ["CIRCLE_PROJECT_USERNAME"]}/{os.environ["CIRCLE_PROJECT_REPONAME"]}'
+        project_slug = job["project_slug"]
         if job["name"].startswith(("tests_", "examples_", "pipelines_")):
 
             url = f'https://circleci.com/api/v2/project/{project_slug}/{job["job_number"]}/artifacts'
-            os.system(f'curl -o {job["name"]}_artifacts.json {url} --header "Circle-Token: $CIRCLE_TOKEN"')
-            with open(f'{job["name"]}_artifacts.json') as fp:
-                job_artifacts = json.load(fp)["items"]
+            r = requests.get(url, headers={"Circle-Token": os.environ.get('CIRCLE_TOKEN', "")})
+            job_artifacts = r.json()["items"]
 
-                os.system(f'mkdir {job["name"]}')
-                os.system(f'mkdir outputs/{job["name"]}')
+            os.makedirs(job["name"], exist_ok=True)
+            os.makedirs(f'outputs/{job["name"]}', exist_ok=True)
 
-                job_test_summaries = {}
-                for artifact in job_artifacts:
-                    if artifact["path"].startswith("reports/") and artifact["path"].endswith("/summary_short.txt"):
-                        node_index = artifact["node_index"]
-                        url = artifact["url"]
-                        fn = f'{job["name"]}/test_summary_{node_index}.txt'
-                        # -L is necessary
-                        command = f'curl -L -o {fn} {url} --header "Circle-Token: $CIRCLE_TOKEN"'
-                        os.system(command)
+            job_test_summaries = {}
+            for artifact in job_artifacts:
+                if artifact["path"].startswith("reports/") and artifact["path"].endswith("/summary_short.txt"):
+                    node_index = artifact["node_index"]
+                    url = artifact["url"]
+                    r = requests.get(url, headers={"Circle-Token": os.environ.get('CIRCLE_TOKEN', "")})
+                    test_summary = r.text
+                    job_test_summaries[node_index] = test_summary
 
-                        # add each
-                        with open(fn) as fp:
-                            test_summary = fp.read()
-                            job_test_summaries[node_index] = test_summary
+            summary = {}
+            for node_index, node_test_summary in job_test_summaries.items():
+                for line in node_test_summary.splitlines():
+                    if line.startswith("PASSED "):
+                        test = line[len("PASSED "):]
+                        summary[test] = "passed"
+                    elif line.startswith("FAILED "):
+                        test = line[len("FAILED "):].split()[0]
+                        summary[test] = "failed"
+            # failed before passed
+            summary = {k: v for k, v in sorted(summary.items(), key=lambda x: (x[1], x[0]))}
+            workflow_summary[job["name"]] = summary
 
-                summary = {}
-                for node_index, node_test_summary in job_test_summaries.items():
-                    for line in node_test_summary.splitlines():
-                        if line.startswith("PASSED "):
-                            test = line[len("PASSED "):]
-                            summary[test] = "passed"
-                        elif line.startswith("FAILED "):
-                            test = line[len("FAILED "):].split()[0]
-                            summary[test] = "failed"
-                # failed before passed
-                summary = {k: v for k, v in sorted(summary.items(), key=lambda x: (x[1], x[0]))}
-                workflow_summary[job["name"]] = summary
-
-                # collected version
-                with open(f'outputs/{job["name"]}/test_summary.json', "w") as fp:
-                    json.dump(summary, fp, indent=4)
+            # collected version
+            with open(f'outputs/{job["name"]}/test_summary.json', "w") as fp:
+                json.dump(summary, fp, indent=4)
 
     new_workflow_summary = {}
     for job_name, job_summary in workflow_summary.items():
