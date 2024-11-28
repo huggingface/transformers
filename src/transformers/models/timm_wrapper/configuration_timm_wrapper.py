672cc6d3fa82cd379c1d8168d5fdace5a77c7a03
Pavel Iakubovskii <qubvel@gmail.com>
2024-11-28 20:07:40 +0000

    Fix header

--- src/transformers/models/timm_wrapper/configuration_timm_wrapper.py
+++ src/transformers/models/timm_wrapper/configuration_timm_wrapper.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Configuration for Backbone models"""
+"""Configuration for TimmWrapper models"""
 
 from typing import Any, Dict
 
