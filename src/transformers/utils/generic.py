185fab8c5d309930c6e093b6adbbd120f986c0a1
Pavel Iakubovskii <qubvel@gmail.com>
2024-11-28 19:54:28 +0000

    fix for is_timm_checkpoint

--- src/transformers/utils/generic.py
+++ src/transformers/utils/generic.py
@@ -891,24 +891,18 @@ def is_timm_checkpoint(pretrained_model_name_or_path: str) -> bool:
     if pretrained_model_name_or_path is None:
         return False
 
-    from . import IMAGE_PROCESSOR_NAME
-
     is_file = os.path.isfile(pretrained_model_name_or_path)
     is_dir = os.path.isdir(pretrained_model_name_or_path)
     is_local = is_file or is_dir
 
-    if is_dir and os.path.exists(os.path.join(pretrained_model_name_or_path, IMAGE_PROCESSOR_NAME)):
-        # timm models don't have a preprocessor_config.json file saved out
-        return False
-
     # pretrained_model_name_or_path is a file
-    if is_file:
+    if is_file and pretrained_model_name_or_path.endswith(".json"):
         with open(pretrained_model_name_or_path, "r") as f:
             config = json.load(f)
         return "pretrained_cfg" in config
 
     # pretrained_model_name_or_path is a directory with a config.json
-    if is_file and os.path.exists(os.path.join(pretrained_model_name_or_path, "config.json")):
+    if is_dir and os.path.exists(os.path.join(pretrained_model_name_or_path, "config.json")):
         with open(os.path.join(pretrained_model_name_or_path, "config.json"), "r") as f:
             config = json.load(f)
         return "pretrained_cfg" in config
