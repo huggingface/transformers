5582beb1d6117e5fdd2fede94053352e5b2fac17
Elvir Crncevic <elvircrn@gmail.com>
2024-12-02 13:52:17 +0100

    Docs and config update

--- src/transformers/utils/quantization_config.py
+++ src/transformers/utils/quantization_config.py
@@ -1537,5 +1537,6 @@ class SpQRConfig(QuantizationConfigMixin):
             raise TypeError("beta1 must be an int")
         if not isinstance(self.beta2, int):
             raise TypeError("beta2 must be an int")
+        # TODO: Validate
         if not isinstance(self.shapes, dict):
             raise TypeError("shapes must be a dict")
