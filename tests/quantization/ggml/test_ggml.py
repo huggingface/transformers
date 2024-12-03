2b4484a15709938f74b4691ed8e380dcf28b8fb9
Isotr0py <2037008807@qq.com>
2024-12-03 21:57:09 +0800

    port remaining tests
    Signed-off-by: Isotr0py <2037008807@qq.com>

--- tests/quantization/ggml/test_ggml.py
+++ tests/quantization/ggml/test_ggml.py
@@ -38,10 +38,15 @@ if is_gguf_available():
 @require_torch_gpu
 @slow
 class GgufQuantizationTests(unittest.TestCase):
-    model_id = ("duyntnet/TinyLlama-1.1B-Chat-v1.0-imatrix-GGUF",)
-    gguf_filename_format = (lambda x: f"TinyLlama-1.1B-Chat-v1.0-{x.upper()}.gguf",)
+    """
+    Test cases for the dequantization workflow of the GGUF models.
+    """
 
+    model_id = "duyntnet/TinyLlama-1.1B-Chat-v1.0-imatrix-GGUF"
     example_text = "Hello"
+
+    def get_gguf_filename_format(self, quant_type: QuantType) -> str:
+        return f"TinyLlama-1.1B-Chat-v1.0-{quant_type.name.upper()}.gguf"
 
     @parameterized.expand(
         [
@@ -54,30 +59,119 @@ class GgufQuantizationTests(unittest.TestCase):
             # k-quants
             (QuantType.Q2_K, "Hello, World!\n\n[10:0"),
             (QuantType.Q3_K, "Hello, World!\n\n```\n<|user"),
-            (QuantType.Q5_K, "Hello, World!\n\nStep 3: Add"),
             (QuantType.Q4_K, "Hello, World!\n\n5. Python:\n"),
+            (QuantType.Q5_K, "Hello, World!\n\nStep 3: Add"),
             (QuantType.Q6_K, "Hello, World!\n\nStep 3: Add"),
             # i-matrix
             (QuantType.IQ1_S, "Hello, I'm a friend of mine, I"),
             (QuantType.IQ1_M, "Hello, I am interested in purching a copy of"),
-            (QuantType.IQ2_S, "Hello World!\n\n```\n<|user|"),
-            (QuantType.IQ2_XS, "Hello World!\n\n```\n<|user|"),
             (QuantType.IQ2_XXS, "Hello, I'm a software engineer. I'"),
-            (QuantType.IQ3_S, "Hello, World!\n\n5. Python:\n"),
+            (QuantType.IQ2_XS, "Hello World!\n\n```\n<|user|"),
+            (QuantType.IQ2_S, "Hello World!\n\n```\n<|user|"),
             (QuantType.IQ3_XXS, "Hello, I am interested in your product. Can you"),
             (QuantType.IQ4_XS, "Hello, world!\n\n5. Using a loop"),
+            (QuantType.IQ3_S, "Hello, World!\n\n5. Python:\n"),
             (QuantType.IQ4_NL, "Hello, world!\n\n5. Using a loop"),
         ]
     )
     def test_quantization_types(self, quant_type: QuantType, expected_text: str):
-        gguf_model_id = self.gguf_filename_format(quant_type.name)
-        tokenizer = AutoTokenizer.from_pretrained(self.model_id, gguf_file=gguf_model_id)
-        model = AutoModelForCausalLM.from_pretrained(self.model_id, gguf_file=gguf_model_id).to(torch_device)
+        gguf_filename = self.get_gguf_filename_format(quant_type)
+        tokenizer = AutoTokenizer.from_pretrained(self.model_id, gguf_file=gguf_filename)
+        model = AutoModelForCausalLM.from_pretrained(self.model_id, gguf_file=gguf_filename).to(torch_device)
 
         text = tokenizer(self.example_text, return_tensors="pt").to(torch_device)
         out = model.generate(**text, max_new_tokens=10)
 
         self.assertEqual(tokenizer.decode(out[0], skip_special_tokens=True), expected_text)
+
+    # TODO(Isotr0py): Fix these broken tests
+    def test_tokenization_xnli(self):
+        import tqdm
+        from datasets import load_dataset
+
+        gguf_tokenizer = AutoTokenizer.from_pretrained(self.model_id, gguf_file=self.q8_0_gguf_model_id)
+        original_tokenizer = AutoTokenizer.from_pretrained(self.original_model_id)
+
+        dataset = load_dataset("google/code_x_glue_ct_code_to_text", "go")
+        for item in tqdm.tqdm(dataset["validation"]):
+            string = item["code"]
+            encoded1 = gguf_tokenizer.encode(string)
+            encoded2 = original_tokenizer.encode(string)
+
+            self.assertEqual(encoded1, encoded2)
+
+            decoded1 = gguf_tokenizer.decode(encoded1, skip_special_tokens=True)
+            decoded2 = original_tokenizer.decode(encoded2, skip_special_tokens=True)
+
+            self.assertEqual(decoded1, decoded2)
+
+        dataset = load_dataset("facebook/xnli", "all_languages")
+
+        for i, item in enumerate(tqdm.tqdm(dataset["train"].select(range(100)))):
+            for string in item["premise"].values():
+                encoded1 = gguf_tokenizer.encode(string)
+                encoded2 = original_tokenizer.encode(string)
+
+                self.assertEqual(encoded1, encoded2)
+
+                decoded1 = gguf_tokenizer.decode(encoded1, skip_special_tokens=True)
+                decoded2 = original_tokenizer.decode(encoded2, skip_special_tokens=True)
+
+                self.assertEqual(decoded1, decoded2)
+
+        # With special tokens
+        gguf_tokenizer = AutoTokenizer.from_pretrained(self.model_id, gguf_file=self.q8_0_gguf_model_id)
+        original_tokenizer = AutoTokenizer.from_pretrained(self.original_model_id)
+
+        gguf_tokenizer.add_special_tokens(
+            {"additional_special_tokens": [AddedToken("<token>", rstrip=False, lstrip=False)]}
+        )
+        original_tokenizer.add_special_tokens(
+            {"additional_special_tokens": [AddedToken("<token>", rstrip=False, lstrip=False)]}
+        )
+
+        text = "Hello <token>. <token> Hello"
+
+        encoded1 = gguf_tokenizer.encode(text)
+        encoded2 = original_tokenizer.encode(text)
+
+        self.assertEqual(encoded1, encoded2)
+
+        decoded1 = gguf_tokenizer.decode(encoded1, skip_special_tokens=True)
+        decoded2 = original_tokenizer.decode(encoded2, skip_special_tokens=True)
+
+        self.assertEqual(decoded1, decoded2)
+
+    def test_q2_k_serialization(self):
+        tokenizer = AutoTokenizer.from_pretrained(self.model_id, gguf_file=self.q2_k_gguf_model_id)
+        model = AutoModelForCausalLM.from_pretrained(self.model_id, gguf_file=self.q2_k_gguf_model_id).to(torch_device)
+
+        with tempfile.TemporaryDirectory() as tmpdirname:
+            model.save_pretrained(tmpdirname)
+            tokenizer.save_pretrained(tmpdirname)
+
+            model = AutoModelForCausalLM.from_pretrained(tmpdirname).to(torch_device)
+            tokenizer = AutoTokenizer.from_pretrained(tmpdirname)
+
+            text = tokenizer(self.example_text, return_tensors="pt").to(torch_device)
+            out = model.generate(**text, max_new_tokens=10)
+
+        EXPECTED_TEXT = "Hello, World!\n\n[10:0"
+        self.assertEqual(tokenizer.decode(out[0], skip_special_tokens=True), EXPECTED_TEXT)
+
+    def test_q6_k_fp16(self):
+        tokenizer = AutoTokenizer.from_pretrained(self.model_id, gguf_file=self.q6_k_gguf_model_id)
+        model = AutoModelForCausalLM.from_pretrained(
+            self.model_id, gguf_file=self.q6_k_gguf_model_id, torch_dtype=torch.float16
+        ).to(torch_device)
+
+        self.assertTrue(model.lm_head.weight.dtype == torch.float16)
+
+        text = tokenizer(self.example_text, return_tensors="pt").to(torch_device)
+        out = model.generate(**text, max_new_tokens=10)
+
+        EXPECTED_TEXT = "Hello, World!\n\nStep 3: Add"
+        self.assertEqual(tokenizer.decode(out[0], skip_special_tokens=True), EXPECTED_TEXT)
 
 
 @require_gguf
@@ -115,28 +209,6 @@ class GgufIntegrationTests(unittest.TestCase):
     nemotron_original_model_id = "nvidia/Nemotron-Mini-4B-Instruct"
     nemotron_model_id = "bartowski/Nemotron-Mini-4B-Instruct-GGUF"
 
-    # standard quants
-    q4_0_gguf_model_id = "tinyllama-1.1b-chat-v1.0.Q4_0.gguf"
-    q5_0_gguf_model_id = "tinyllama-1.1b-chat-v1.0.Q5_0.gguf"
-    q8_0_gguf_model_id = "tinyllama-1.1b-chat-v1.0.Q8_0.gguf"
-    # k-quants
-    q2_k_gguf_model_id = "tinyllama-1.1b-chat-v1.0.Q2_K.gguf"
-    q3_k_gguf_model_id = "tinyllama-1.1b-chat-v1.0.Q3_K_L.gguf"
-    q4_k_gguf_model_id = "tinyllama-1.1b-chat-v1.0.Q4_K_M.gguf"
-    q5_k_gguf_model_id = "tinyllama-1.1b-chat-v1.0.Q5_K_M.gguf"
-    q6_k_gguf_model_id = "tinyllama-1.1b-chat-v1.0.Q6_K.gguf"
-    q4_k_m_stablelm_model_id = "stablelm-3b-4e1t.q4_k_m.gguf"
-    # imatrix
-    iq1_m_gguf_model_id = "TinyLlama-1.1B-Chat-v1.0-IQ1_M.gguf"
-    iq1_s_gguf_model_id = "TinyLlama-1.1B-Chat-v1.0-IQ1_S.gguf"
-    iq2_s_gguf_model_id = "TinyLlama-1.1B-Chat-v1.0-IQ2_S.gguf"
-    iq2_xs_gguf_model_id = "TinyLlama-1.1B-Chat-v1.0-IQ2_XS.gguf"
-    iq2_xxs_gguf_model_id = "TinyLlama-1.1B-Chat-v1.0-IQ2_XXS.gguf"
-    iq3_s_gguf_model_id = "TinyLlama-1.1B-Chat-v1.0-IQ3_S.gguf"
-    iq3_xxs_gguf_model_id = "TinyLlama-1.1B-Chat-v1.0-IQ3_XXS.gguf"
-    iq4_xs_gguf_model_id = "TinyLlama-1.1B-Chat-v1.0-IQ4_XS.gguf"
-    iq4_nl_gguf_model_id = "TinyLlama-1.1B-Chat-v1.0-IQ4_NL.gguf"
-
     q4_0_phi3_model_id = "Phi-3-mini-4k-instruct-q4.gguf"
     q4_0_mistral_model_id = "mistral-7b-instruct-v0.2.Q4_0.gguf"
     q4_0_qwen2_model_id = "qwen1_5-0_5b-chat-q4_0.gguf"
@@ -163,37 +235,6 @@ class GgufIntegrationTests(unittest.TestCase):
     fp16_nemotron_model_id = "Nemotron-Mini-4B-Instruct-f16.gguf"
 
     example_text = "Hello"
-
-    def test_q2_k_serialization(self):
-        tokenizer = AutoTokenizer.from_pretrained(self.model_id, gguf_file=self.q2_k_gguf_model_id)
-        model = AutoModelForCausalLM.from_pretrained(self.model_id, gguf_file=self.q2_k_gguf_model_id).to(torch_device)
-
-        with tempfile.TemporaryDirectory() as tmpdirname:
-            model.save_pretrained(tmpdirname)
-            tokenizer.save_pretrained(tmpdirname)
-
-            model = AutoModelForCausalLM.from_pretrained(tmpdirname).to(torch_device)
-            tokenizer = AutoTokenizer.from_pretrained(tmpdirname)
-
-            text = tokenizer(self.example_text, return_tensors="pt").to(torch_device)
-            out = model.generate(**text, max_new_tokens=10)
-
-        EXPECTED_TEXT = "Hello, World!\n\n[10:0"
-        self.assertEqual(tokenizer.decode(out[0], skip_special_tokens=True), EXPECTED_TEXT)
-
-    def test_q6_k_fp16(self):
-        tokenizer = AutoTokenizer.from_pretrained(self.model_id, gguf_file=self.q6_k_gguf_model_id)
-        model = AutoModelForCausalLM.from_pretrained(
-            self.model_id, gguf_file=self.q6_k_gguf_model_id, torch_dtype=torch.float16
-        ).to(torch_device)
-
-        self.assertTrue(model.lm_head.weight.dtype == torch.float16)
-
-        text = tokenizer(self.example_text, return_tensors="pt").to(torch_device)
-        out = model.generate(**text, max_new_tokens=10)
-
-        EXPECTED_TEXT = "Hello, World!\n\nStep 3: Add"
-        self.assertEqual(tokenizer.decode(out[0], skip_special_tokens=True), EXPECTED_TEXT)
 
     def test_mistral_q4_0(self):
         tokenizer = AutoTokenizer.from_pretrained(self.mistral_model_id, gguf_file=self.q4_0_mistral_model_id)
@@ -682,60 +723,3 @@ class GgufIntegrationTests(unittest.TestCase):
 
         EXPECTED_TEXT = "'Hello. hotmail.com.'"
         self.assertEqual(tokenizer.decode(out[0], skip_special_tokens=True), EXPECTED_TEXT)
-
-    def test_tokenization_xnli(self):
-        import tqdm
-        from datasets import load_dataset
-
-        gguf_tokenizer = AutoTokenizer.from_pretrained(self.model_id, gguf_file=self.q8_0_gguf_model_id)
-        original_tokenizer = AutoTokenizer.from_pretrained(self.original_model_id)
-
-        dataset = load_dataset("google/code_x_glue_ct_code_to_text", "go")
-        for item in tqdm.tqdm(dataset["validation"]):
-            string = item["code"]
-            encoded1 = gguf_tokenizer.encode(string)
-            encoded2 = original_tokenizer.encode(string)
-
-            self.assertEqual(encoded1, encoded2)
-
-            decoded1 = gguf_tokenizer.decode(encoded1, skip_special_tokens=True)
-            decoded2 = original_tokenizer.decode(encoded2, skip_special_tokens=True)
-
-            self.assertEqual(decoded1, decoded2)
-
-        dataset = load_dataset("facebook/xnli", "all_languages")
-
-        for i, item in enumerate(tqdm.tqdm(dataset["train"].select(range(100)))):
-            for string in item["premise"].values():
-                encoded1 = gguf_tokenizer.encode(string)
-                encoded2 = original_tokenizer.encode(string)
-
-                self.assertEqual(encoded1, encoded2)
-
-                decoded1 = gguf_tokenizer.decode(encoded1, skip_special_tokens=True)
-                decoded2 = original_tokenizer.decode(encoded2, skip_special_tokens=True)
-
-                self.assertEqual(decoded1, decoded2)
-
-        # With special tokens
-        gguf_tokenizer = AutoTokenizer.from_pretrained(self.model_id, gguf_file=self.q8_0_gguf_model_id)
-        original_tokenizer = AutoTokenizer.from_pretrained(self.original_model_id)
-
-        gguf_tokenizer.add_special_tokens(
-            {"additional_special_tokens": [AddedToken("<token>", rstrip=False, lstrip=False)]}
-        )
-        original_tokenizer.add_special_tokens(
-            {"additional_special_tokens": [AddedToken("<token>", rstrip=False, lstrip=False)]}
-        )
-
-        text = "Hello <token>. <token> Hello"
-
-        encoded1 = gguf_tokenizer.encode(text)
-        encoded2 = original_tokenizer.encode(text)
-
-        self.assertEqual(encoded1, encoded2)
-
-        decoded1 = gguf_tokenizer.decode(encoded1, skip_special_tokens=True)
-        decoded2 = original_tokenizer.decode(encoded2, skip_special_tokens=True)
-
-        self.assertEqual(decoded1, decoded2)
