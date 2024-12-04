7d8100cf4c289c1bc729e63892a55a3a1c7ef5d8
yonigozlan <yoni.gozlan@huggingface.co>
2024-12-04 06:13:42 +0000

    add fast img blip

--- src/transformers/models/blip/image_processing_blip_fast.py
+++ src/transformers/models/blip/image_processing_blip_fast.py
@@ -15,12 +15,15 @@
 """Fast Image processor class for BLIP."""
 
 from ...image_processing_utils_fast import BaseImageProcessorFast
+from ...image_utils import OPENAI_CLIP_MEAN, OPENAI_CLIP_STD, PILImageResampling
 
 
 class BlipImageProcessorFast(BaseImageProcessorFast):
-    # To be implemented
-    resample = None
-    image_mean = None
-    image_std = None
-    size = {"height": None, "width": None}
-    crop_size = {"height": None, "width": None}
+    resample = PILImageResampling.BICUBIC
+    image_mean = OPENAI_CLIP_MEAN
+    image_std = OPENAI_CLIP_STD
+    size = {"height": 384, "width": 384}
+    do_resize = True
+    do_rescale = True
+    do_normalize = True
+    do_convert_rgb = True
