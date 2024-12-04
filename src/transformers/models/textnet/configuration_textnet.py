2a12e6648b5a42ae957ef20a86315e1b5a6abfa7
Jade Choghari <chogharijade@gmail.com>
2024-12-04 15:31:29 -0500

    Update src/transformers/models/textnet/configuration_textnet.py
    Co-authored-by: Pavel Iakubovskii <qubvel@gmail.com>

--- src/transformers/models/textnet/configuration_textnet.py
+++ src/transformers/models/textnet/configuration_textnet.py
@@ -31,7 +31,7 @@ class TextNetConfig(BackboneConfigMixin, PretrainedConfig):
     This is the configuration class to store the configuration of a [`TextNextModel`]. It is used to instantiate a
     TextNext model according to the specified arguments, defining the model architecture. Instantiating a configuration
     with the defaults will yield a similar configuration to that of the
-    [Raghavan/textnet-base](https://huggingface.co/Raghavan/textnet-base)Configuration objects inherit from
+    [Raghavan/textnet-base](https://huggingface.co/Raghavan/textnet-base). Configuration objects inherit from
     [`PretrainedConfig`] and can be used to control the model outputs.Read the documentation from [`PretrainedConfig`]
     for more information.
 
