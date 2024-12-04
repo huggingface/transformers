a6e47ed3782d619caa881796ab2aeef0bd3ab506
Arthur <48595927+ArthurZucker@users.noreply.github.com>
2024-12-04 14:43:26 +0100

    Update src/transformers/models/qwen2_moe/configuration_qwen2_moe.py

--- src/transformers/models/qwen2_moe/configuration_qwen2_moe.py
+++ src/transformers/models/qwen2_moe/configuration_qwen2_moe.py
@@ -150,7 +150,7 @@ class Qwen2MoeConfig(PretrainedConfig):
     model_type = "qwen2_moe"
     keys_to_ignore_at_inference = ["past_key_values"]
 
-    # Default tensor parallel plan for base model `Qwe2Moe`
+    # Default tensor parallel plan for base model `Qwen2Moe`
     base_model_tp_plan = {
         "layers.*.self_attn.q_proj": "colwise",
         "layers.*.self_attn.k_proj": "colwise",
