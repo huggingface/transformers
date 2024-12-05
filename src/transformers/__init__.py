53e2358d1743027b944b9fb0308348231af78dfa
João Marcelo <jmttozato@gmail.com>
2024-12-05 16:14:46 +0100

    Add I-JEPA (#33125)
    * first draft
    
    * add IJepaEmbeddings class
    
    * fix copy-from for IJepa model
    
    * add weight conversion script
    
    * update attention class names in IJepa model
    
    * style changes
    
    * Add push_to_hub option to convert_ijepa_checkpoint function
    
    * add initial tests for I-JEPA
    
    * minor style changes to conversion script
    
    * make fixup related
    
    * rename conversion script
    
    * Add I-JEPA to sdpa docs
    
    * minor fixes
    
    * adjust conversion script
    
    * update conversion script
    
    * adjust sdpa docs
    
    * [run_slow] ijepa
    
    * [run-slow] ijepa
    
    * [run-slow] ijepa
    
    * [run-slow] ijepa
    
    * [run-slow] ijepa
    
    * [run-slow] ijepa
    
    * formatting issues
    
    * adjust modeling to modular code
    
    * add IJepaModel to objects to ignore in docstring checks
    
    * [run-slow] ijepa
    
    * fix formatting issues
    
    * add usage instruction snippet to docs
    
    * change pos encoding, add checkpoint for doc
    
    * add verify logits for all models
    
    * [run-slow] ijepa
    
    * update docs to include image feature extraction instructions
    
    * remove pooling layer from IJepaModel in image classification class
    
    * [run-slow] ijepa
    
    * remove pooling layer from IJepaModel constructor
    
    * update docs
    
    * [run-slow] ijepa
    
    * [run-slow] ijepa
    
    * small changes
    
    * [run-slow] ijepa
    
    * style adjustments
    
    * update copyright in init file
    
    * adjust modular ijepa
    
    * [run-slow] ijepa

--- src/transformers/__init__.py
+++ src/transformers/__init__.py
@@ -485,6 +485,7 @@ _import_structure = {
     "models.idefics": ["IdeficsConfig"],
     "models.idefics2": ["Idefics2Config"],
     "models.idefics3": ["Idefics3Config"],
+    "models.ijepa": ["IJepaConfig"],
     "models.imagegpt": ["ImageGPTConfig"],
     "models.informer": ["InformerConfig"],
     "models.instructblip": [
@@ -2460,6 +2461,13 @@ else:
             "Idefics3Model",
             "Idefics3PreTrainedModel",
             "Idefics3Processor",
+        ]
+    )
+    _import_structure["models.ijepa"].extend(
+        [
+            "IJepaForImageClassification",
+            "IJepaModel",
+            "IJepaPreTrainedModel",
         ]
     )
     _import_structure["models.imagegpt"].extend(
@@ -5368,6 +5376,7 @@ if TYPE_CHECKING:
     )
     from .models.idefics2 import Idefics2Config
     from .models.idefics3 import Idefics3Config
+    from .models.ijepa import IJepaConfig
     from .models.imagegpt import ImageGPTConfig
     from .models.informer import InformerConfig
     from .models.instructblip import (
@@ -7180,6 +7189,11 @@ if TYPE_CHECKING:
             Idefics3Model,
             Idefics3PreTrainedModel,
             Idefics3Processor,
+        )
+        from .models.ijepa import (
+            IJepaForImageClassification,
+            IJepaModel,
+            IJepaPreTrainedModel,
         )
         from .models.imagegpt import (
             ImageGPTForCausalImageModeling,
