935127bbb85489baf7c53cc7a013ab5097357c0d
Wing Lian <wing@axolotl.ai>
2024-12-06 15:37:07 -0500

    fix check for loss_kwargs/GA

--- src/transformers/trainer.py
+++ src/transformers/trainer.py
@@ -623,7 +623,9 @@ class Trainer:
             else unwrapped_model.get_base_model().forward
         )
         forward_params = inspect.signature(model_forward).parameters
-        self.model_accepts_loss_kwargs = any(k.kind == inspect.Parameter.VAR_KEYWORD for k in forward_params.values())
+        self.model_accepts_loss_kwargs = (
+                "num_items_in_batch"  in forward_params and forward_params["loss_kwargs"].kind == inspect.Parameter.VAR_KEYWORD
+        )
 
         self.neftune_noise_alpha = args.neftune_noise_alpha
 
