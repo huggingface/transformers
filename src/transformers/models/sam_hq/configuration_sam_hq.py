a284572d71c2ba328505d186a3ee17b307d1780b
sushmanthreddy <sushmanthreddymereddy@gmail.com>
2024-12-08 14:41:23 +0530

    added the configuartion for sam_hq

---
+++ src/transformers/models/sam_hq/configuration_sam_hq.py
@@ -0,0 +1,185 @@
+# coding=utf-8
+# Copyright 2023 The HuggingFace Inc. team. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""SAM-HQ configuration."""
+
+from ...configuration_utils import PretrainedConfig
+from ...utils import logging
+
+
+logger = logging.get_logger(__name__)
+
+class SamHQPromptEncoderConfig(PretrainedConfig):
+    r"""
+    This is the configuration class to store the configuration of a [`SamHQPromptEncoderModel`].The [`SamHQPromptEncoderModel`]
+    module is used to encode the input 2D points and bounding boxes. Instantiating a configuration defaults will yield a 
+    similar configuration to that of the SAM_HQ model. The configuration is used to store the configuration of the model.
+    [Uminosachi/sam-hq](https://huggingface.co/Uminosachi/sam-hq) architecture.
+
+    Configuration objects inherit from [`PretrainedConfig`] and can be used to control the model's output.Read the documentation from
+    [`PretrainedConfig`] for more information.
+
+    Args:
+        hidden_size (`int`, *optional*, defaults to 256):
+            Dimensionality of the hidden states.
+        image_size (`int`, *optional*, defaults to 1024):
+            The expected output resolution of the image.
+        patch_size (`int`, *optional*, defaults to 16):
+            The size (resolution) of each patch.
+        mask_input_channels (`int`, *optional*, defaults to 16):
+            The number of channels to be fed to the `MaskDecoder` module.
+        num_point_embeddings (`int`, *optional*, defaults to 4):
+            The number of point embeddings to be used.
+        hidden_act (`str`, *optional*, defaults to `"gelu"`):
+            The non-linear activation function in the encoder and pooler.
+    """
+    def __init__(
+            self, 
+            hidden_size=256,
+            image_size=1024,
+            patch_size=16,
+            mask_input_channels=16,
+            num_point_embeddings=4,
+            hidden_act="gelu",
+            layer_norm_eps=1e-6,
+            **kwargs):
+        super().__init__(**kwargs)
+        self.hidden_size = hidden_size
+        self.image_size = image_size
+        self.patch_size = patch_size
+        self.image_embeddings_size = image_size // patch_size
+        self.mask_input_channels = mask_input_channels
+        self.num_point_embeddings = num_point_embeddings
+        self.hidden_act = hidden_act
+        self.layer_norm_eps = layer_norm_eps
+
+
+class SamHQVisionConfig(PretrainedConfig):
+    r"""
+    This is the configuration class to store the configuration of a [`SamVisionModel`]. It is used to instantiate a SAM-HQ model
+    vision encoder according to the specified arguments, defining the model architecture.Instantiating a configuration
+    defaults will yield a similar configuration to that of the SAM_HQ model. 
+
+    [Uminosachi/sam-hq](https://huggingface.co/Uminosachi/sam-hq) architecture.
+
+    
+    Configuration objects inherit from [`PretrainedConfig`] and can be used to control the model outputs. Read the
+    documentation from [`PretrainedConfig`] for more information.
+    """
+    def __init__(
+        self,
+        hidden_size=768,
+        output_channels=256,
+        num_hidden_layers=12,
+        num_attention_heads=12,
+        num_channels=3,
+        image_size=1024,
+        patch_size=16,
+        hidden_act="gelu",
+        layer_norm_eps=1e-6,
+        attention_dropout=0.0,
+        initializer_range=1e-10,
+        qkv_bias=True,
+        mlp_ratio=4.0,
+        use_abs_pos = True,
+        use_rel_pos = True,
+        window_size = 14,
+        global_attn_indexes = [2, 5, 8, 11],
+        num_pos_feats = 128,
+        mlp_dim = None,
+        **kwargs):
+        super().__init__(**kwargs)
+        self.hidden_size = hidden_size
+        self.output_channels = output_channels
+        self.num_hidden_layers = num_hidden_layers
+        self.num_attention_heads = num_attention_heads
+        self.num_channels = num_channels
+        self.image_size = image_size
+        self.patch_size = patch_size
+        self.hidden_act = hidden_act
+        self.layer_norm_eps = layer_norm_eps
+        self.attention_dropout = attention_dropout
+        self.initializer_range = initializer_range
+        self.qkv_bias = qkv_bias
+        self.mlp_ratio = mlp_ratio
+        self.use_abs_pos = use_abs_pos
+        self.use_rel_pos = use_rel_pos
+        self.window_size = window_size
+        self.global_attn_indexes = global_attn_indexes
+        self.num_pos_feats = num_pos_feats
+        self.mlp_dim = int(hidden_size * mlp_ratio) if mlp_dim is None else mlp_dim
+
+
+
+class SamHQMaskDecoderConfig(PretrainedConfig):
+
+    def __init__(
+        self,
+        hidden_size=256,
+        hidden_act="relu",
+        mlp_dim=2048,
+        num_hidden_layers=2,
+        num_attention_heads=8,
+        attention_downsample_rate=2,
+        num_multimask_outputs=3,
+        iou_head_depth=3,
+        iou_head_hidden_dim=256,
+        layer_norm_eps=1e-6,
+        vit_dim = 1024,
+        **kwargs,
+    ):
+        super().__init__(**kwargs)
+        self.hidden_size = hidden_size
+        self.hidden_act = hidden_act
+        self.mlp_dim = mlp_dim
+        self.num_hidden_layers = num_hidden_layers
+        self.num_attention_heads = num_attention_heads
+        self.attention_downsample_rate = attention_downsample_rate
+        self.num_multimask_outputs = num_multimask_outputs
+        self.iou_head_depth = iou_head_depth
+        self.iou_head_hidden_dim = iou_head_hidden_dim
+        self.layer_norm_eps = layer_norm_eps
+        self.vit_dim = vit_dim
+
+
+
+
+class SamHQConfig(PretrainedConfig):
+    model_type = "sam-hq"
+
+    def __init__(
+        self,
+        vision_config=None,
+        prompt_encoder_config=None,
+        intializer_range=0.02,
+        **kwargs,
+    ):
+        super().__init__(**kwargs)
+        self.vision_config = vision_config if vision_config is not None else {}
+        self.prompt_encoder_config = prompt_encoder_config if prompt_encoder_config is not None else {}
+
+
+        if isinstance(vision_config, SamHQVisionConfig):
+            vision_config = vision_config.to_dict()
+        if isinstance(prompt_encoder_config, SamHQPromptEncoderConfig):
+            prompt_encoder_config = prompt_encoder_config.to_dict()
+        if isinstance(mask_decoder_config, SamHQMaskDecoder):
+            mask_decoder_config = mask_decoder_config.to_dict()
+
+
+        
+        self.vision_config = SamHQVisionConfig(**vision_config)
+        self.prompt_encoder_config = SamHQPromptEncoderConfig(**prompt_encoder_config)
+        self.intializer_range = intializer_range
