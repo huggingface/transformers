92df994aec90fa96d0cfc6e381767016cfa3c6df
Wing Lian <wing@axolotl.ai>
2024-12-09 18:35:44 -0500

    flash attention forward doesn't play well with torch.compile

--- src/transformers/modeling_flash_attention_utils.py
+++ src/transformers/modeling_flash_attention_utils.py
@@ -228,6 +228,7 @@ flash_241 = is_flash_attn_greater_or_equal("2.4.1")
 deterministic_g = os.environ.get("FLASH_ATTENTION_DETERMINISTIC", "0") == "1"
 
 
+@torch.compiler.disable(recursive=True)
 def _flash_attention_forward(
     query_states: torch.Tensor,
     key_states: torch.Tensor,
