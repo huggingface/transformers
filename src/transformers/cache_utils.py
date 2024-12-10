0ea61c0946281bf0a75e90def0fb4dcdf94882c5
raushan <raushan@huggingface.co>
2024-12-10 12:44:32 +0100

    fix copies

--- src/transformers/cache_utils.py
+++ src/transformers/cache_utils.py
@@ -1942,7 +1942,7 @@ class OffloadedStaticCache(StaticCache):
             The default `dtype` to use when initializing the cache.
         offload_device (`Union[str, torch.device]`, *optional*, defaults to `cpu`):
             The device to offload to. Defaults to CPU.
-        layer_device_map(`Dict[int, Union[str, torch.device, int]]]`, *optional*):
+        layer_device_map (`Dict[int, Union[str, torch.device, int]]]`, *optional*):
             Mapping between the layers and its device. This is required if no `device` is passed, because the cache needs to prefetch future layers
             in their corresponding devices. You can know which layers mapped to which device by checking the associated device_map: `model.hf_device_map`.
 
