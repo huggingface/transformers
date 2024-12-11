55305f64e6b79f129026c1235ef231943a4d9ac4
xiangdeping1 <xiangdeping1@jd.com>
2024-12-11 21:04:10 +0800

    fix  to calculate num_items_in_batch correctly

--- src/transformers/trainer.py
+++ src/transformers/trainer.py
@@ -3698,6 +3698,9 @@ class Trainer:
                 scaled_loss.backward()
         else:
             self.accelerator.backward(loss, **kwargs)
+            # Finally we need to normalize the loss for reporting
+            if num_items_in_batch is None:
+                return loss.detach() / self.args.gradient_accumulation_steps
             return loss.detach()
 
     def compute_loss(self, model, inputs, return_outputs=False, num_items_in_batch=None):
