3bbae395394259523ae319c9aa3bd17490c351d2
Arthur Zucker <arthur.zucker@gmail.com>
2024-12-11 17:24:56 +0100

    remove tanh

--- src/transformers/integrations/flex_attention.py
+++ src/transformers/integrations/flex_attention.py
@@ -1,29 +1,17 @@
-import torch
-
 from ..utils import is_torch_greater_or_equal
 
 
 if is_torch_greater_or_equal("2.5"):
     from torch.nn.attention.flex_attention import flex_attention
 
-def flex_attention_forward(config, query, key, value, mask, output_attentions=False, **_kwargs):
-    def tanh_softcap(score, b, h, q_idx, kv_idx):
-        soft_cap = config.attn_logit_softcapping
-        score = soft_cap * torch.tanh(score / soft_cap)
-        if mask is not None:
-            return score + mask[b][0][q_idx][kv_idx]
-        return score
+def flex_attention_forward(module, query, key, value, attention_mask, output_attentions=False, **_kwargs):
 
-    attn_output = flex_attention(
+    attn_output, attention_weights = flex_attention(
         query,
         key,
         value,
-        score_mod=tanh_softcap,
         enable_gqa=True,
-        scale=config.scaling,
-        return_lse=output_attentions,
+        scale=module.scaling,
+        return_lse=True,
     )
-    if not output_attentions:
-        return attn_output, None
-    else:
-        return attn_output[0], attn_output[1]
+    return attn_output, attention_weights
