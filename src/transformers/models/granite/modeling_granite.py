a4ad30109b28d5b32687a6ee1b3af99d4d1e9338
Alex-Brooks <Alex.Brooks@ibm.com>
2024-12-12 07:25:41 -0500

    Add num logits to keep to granite llms
    Formatting
    
    Signed-off-by: Alex-Brooks <Alex.Brooks@ibm.com>

--- src/transformers/models/granite/modeling_granite.py
+++ src/transformers/models/granite/modeling_granite.py
@@ -1052,6 +1052,7 @@ class GraniteForCausalLM(GranitePreTrainedModel, GenerationMixin):
         output_hidden_states: Optional[bool] = None,
         return_dict: Optional[bool] = None,
         cache_position: Optional[torch.LongTensor] = None,
+        num_logits_to_keep=0,
     ) -> Union[Tuple, CausalLMOutputWithPast]:
         r"""
         Args:
@@ -1059,6 +1060,11 @@ class GraniteForCausalLM(GranitePreTrainedModel, GenerationMixin):
                 Labels for computing the masked language modeling loss. Indices should either be in `[0, ...,
                 config.vocab_size]` or -100 (see `input_ids` docstring). Tokens with indices set to `-100` are ignored
                 (masked), the loss is only computed for the tokens with labels in `[0, ..., config.vocab_size]`.
+
+            num_logits_to_keep (`int`, *optional*):
+                Calculate logits for the last `num_logits_to_keep` tokens. If `0`, calculate logits for all
+                `input_ids` (special case). Only last token logits are needed for generation, and calculating them only for that
+                token can save memory, which becomes pretty significant for long sequences or large vocabulary size.
 
         Returns:
 
@@ -1099,7 +1105,7 @@ class GraniteForCausalLM(GranitePreTrainedModel, GenerationMixin):
         )
 
         hidden_states = outputs[0]
-        logits = self.lm_head(hidden_states)
+        logits = self.lm_head(hidden_states[:, -num_logits_to_keep:, :])
         logits = logits / self.config.logits_scaling
 
         loss = None
