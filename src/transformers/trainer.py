29a693f3cf200f10df3d5ec5d556a8a05ace491d
Wing Lian <wing@axolotl.ai>
2024-12-13 13:23:00 -0500

    don't use no_sync when deepspeed doesn't support it for certain zero stages (#35157)
    * don't use no_sync when deepspeed doesn't support it for certain zero stages
    
    * chore: lint
    
    * fix no_sync context for deepspeed across all zero types
    
    * chore: lint

--- src/transformers/trainer.py
+++ src/transformers/trainer.py
@@ -2517,6 +2517,7 @@ class Trainer:
                     context = (
                         functools.partial(self.accelerator.no_sync, model=model)
                         if i != len(batch_samples) - 1
+                        and self.accelerator.distributed_type != DistributedType.DEEPSPEED
                         else contextlib.nullcontext
                     )
                     with context():
