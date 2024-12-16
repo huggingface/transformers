461259527752424ab6f8d2ec6084ee81a7492f99
Cyril Vallez <cyril.vallez@gmail.com>
2024-12-16 15:14:19 +0100

    style

--- src/transformers/models/llama/modeling_llama.py
+++ src/transformers/models/llama/modeling_llama.py
@@ -489,7 +489,7 @@ class LlamaModel(LlamaPreTrainedModel):
         self.norm = LlamaRMSNorm(config.hidden_size, eps=config.rms_norm_eps)
         self.rotary_emb = LlamaRotaryEmbedding(config=config)
         self.gradient_checkpointing = False
-        
+
         # Initialize weights and apply final processing
         self.post_init()
 
