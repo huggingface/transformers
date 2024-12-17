55944fcf8762785f915aedeae16ecbf9a8f83fe4
ydshieh <ydshieh@users.noreply.github.com>
2024-12-17 15:19:54 +0100

    temp

--- tests/models/kosmos2_5/test_modeling_kosmos2_5.py
+++ tests/models/kosmos2_5/test_modeling_kosmos2_5.py
@@ -374,18 +374,6 @@ class Kosmos2_5ModelTest(ModelTesterMixin, GenerationTesterMixin, PipelineTester
         config_and_inputs = self.model_tester.prepare_config_and_inputs()
         self.model_tester.create_and_check_model(*config_and_inputs)
 
-    def test_forward_signature(self):
-        config, _ = self.model_tester.prepare_config_and_inputs_for_common()
-
-        for model_class in self.all_model_classes:
-            model = model_class(config)
-            signature = inspect.signature(model.forward)
-            # signature.parameters is an OrderedDict => so arg_names order is deterministic
-            arg_names = [*signature.parameters.keys()]
-
-            expected_arg_names = ["flattened_patches"]
-            self.assertListEqual(arg_names[:1], expected_arg_names)
-
     def test_load_save_without_tied_weights(self):
         config, _ = self.model_tester.prepare_config_and_inputs_for_common()
         config.text_config.tie_word_embeddings = False
