9b29aacce38221fdaa019ef011687c52f0b1edf2
ydshieh <ydshieh@users.noreply.github.com>
2024-12-17 16:18:37 +0100

    temp

--- src/transformers/models/kosmos2_5/modeling_kosmos2_5.py
+++ src/transformers/models/kosmos2_5/modeling_kosmos2_5.py
@@ -1556,9 +1556,6 @@ class Kosmos2_5TextTransformer(nn.Module):
             all_hidden_states += (hidden_states,)
 
         next_cache = next_decoder_cache if use_cache else None
-        if return_legacy_cache:
-            next_cache = next_cache.to_legacy_cache()
-
         if not return_dict:
             return tuple(v for v in [hidden_states, next_cache, all_hidden_states, all_self_attns] if v is not None)
         return BaseModelOutputWithPast(
