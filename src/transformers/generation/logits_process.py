77f3c21ae78b5171833f9c10d600f25829e0ebfc
Vukasin Toroman <vukasin@toroman.name>
2024-12-19 16:22:05 +0100

    use torch.full in stead of np

--- src/transformers/generation/logits_process.py
+++ src/transformers/generation/logits_process.py
@@ -1366,7 +1366,7 @@ class PrefixConstrainedLogitsProcessor(LogitsProcessor):
 
     @add_start_docstrings(LOGITS_PROCESSOR_INPUTS_DOCSTRING)
     def __call__(self, input_ids: torch.LongTensor, scores: torch.FloatTensor) -> torch.FloatTensor:
-        mask = np.full(scores.shape, -np.inf)
+        mask = torch.full_like(scores, -math.inf, device=torch.cpu.current_device())
         for batch_id, beam_sent in enumerate(input_ids.view(-1, self._num_beams, input_ids.shape[-1])):
             for beam_id, sent in enumerate(beam_sent):
                 prefix_allowed_tokens = self._prefix_allowed_tokens_fn(batch_id, sent)
@@ -1378,7 +1378,7 @@ class PrefixConstrainedLogitsProcessor(LogitsProcessor):
                     )
                 mask[batch_id * self._num_beams + beam_id, prefix_allowed_tokens] = 0
 
-        scores_processed = scores + torch.tensor(mask, device=scores.device)
+        scores_processed = scores + mask.to(scores.device)
         return scores_processed
 
 
