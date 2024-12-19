aa40889ea26d83025f7be759d6212f2bcc8e38f1
Arthur Zucker <arthur.zucker@gmail.com>
2024-12-19 17:34:21 +0100

    [test-all]

--- utils/tests_fetcher.py
+++ utils/tests_fetcher.py
@@ -998,6 +998,7 @@ def infer_tests_to_run(
     output_file: str,
     diff_with_last_commit: bool = False,
     filter_models: bool = False,
+    test_all: bool = False
 ):
     """
     The main function called by the test fetcher. Determines the tests to run from the diff.
@@ -1018,7 +1019,10 @@ def infer_tests_to_run(
             Whether or not to filter the tests to core models only, when a file modified results in a lot of model
             tests.
     """
-    modified_files = get_modified_python_files(diff_with_last_commit=diff_with_last_commit)
+    if not test_all:
+        modified_files = get_modified_python_files(diff_with_last_commit=diff_with_last_commit)
+    else:
+        modified_files = PATH_TO_TESTS.glob("*")
     print(f"\n### MODIFIED FILES ###\n{_print_list(modified_files)}")
 
     # Create the map that will give us all impacted modules.
@@ -1230,5 +1234,6 @@ if __name__ == "__main__":
             args.output_file,
             diff_with_last_commit=diff_with_last_commit,
             filter_models=False,
+            test_all=args.fetch_all
         )
         filter_tests(args.output_file, ["repo_utils"])
