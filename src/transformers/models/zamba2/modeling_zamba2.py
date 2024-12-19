b993a7895e7e42be6a3f9647759a19320f6bd73e
pglorio <paolomorphic@gmail.com>
2024-12-19 04:23:09 +0000

    Fix torch_forward in mamba2 layer

--- src/transformers/models/zamba2/modeling_zamba2.py
+++ src/transformers/models/zamba2/modeling_zamba2.py
@@ -1019,6 +1019,9 @@ class Zamba2MambaMixer(nn.Module):
         if cache_params is not None and cache_params.has_previous_state:
             projected_states =  self.in_proj(input_states.squeeze(1))
         else:
+            if attention_mask is not None and not torch.all(attention_mask==1):
+                    # tune out hidden states for pad tokens, see https://github.com/state-spaces/mamba/issues/66
+                    input_states = (input_states * attention_mask[:, :, None]).to(dtype)
             projected_states =  self.in_proj(input_states)
         d_mlp = (projected_states.shape[-1] - 2 * self.intermediate_size -  2 * self.n_groups * self.ssm_state_size- self.num_heads) // 2
         _, _, gate, hidden_states, dt = projected_states.split(
@@ -1041,10 +1044,6 @@ class Zamba2MambaMixer(nn.Module):
                     hidden_states += self.conv1d.bias
                 hidden_states = self.act(hidden_states).to(dtype)[:, None, ...]         # [batch, 1, intermediate_size] : decoding
             else:
-                if attention_mask is not None and not torch.all(attention_mask==1):
-                    dtype = hidden_states.dtype
-                    # tune out hidden states for pad tokens, see https://github.com/state-spaces/mamba/issues/66
-                    hidden_states = (hidden_states * attention_mask[:, :, None]).to(dtype)
                 hidden_states = hidden_states.transpose(1,2)
                 conv_state = nn.functional.pad(
                     hidden_states,
