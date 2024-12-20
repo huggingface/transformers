87263faa484a63617769f3fff90db1fa4b7322ac
Niels <niels.rogge1@gmail.com>
2024-12-20 18:06:31 +0100

    Simplify drop path

--- src/transformers/models/dinov2_with_registers/modeling_dinov2_with_registers.py
+++ src/transformers/models/dinov2_with_registers/modeling_dinov2_with_registers.py
@@ -338,6 +338,25 @@ class Dinov2WithRegistersLayerScale(nn.Module):
         return hidden_state * self.lambda1
 
 
+def drop_path(input: torch.Tensor, drop_prob: float = 0.0, training: bool = False) -> torch.Tensor:
+    """
+    Drop paths (Stochastic Depth) per sample (when applied in main path of residual blocks).
+    Comment by Ross Wightman: This is the same as the DropConnect impl I created for EfficientNet, etc networks,
+    however, the original name is misleading as 'Drop Connect' is a different form of dropout in a separate paper...
+    See discussion: https://github.com/tensorflow/tpu/issues/494#issuecomment-532968956 ... I've opted for changing the
+    layer and argument names to 'drop path' rather than mix DropConnect as a layer name and use 'survival rate' as the
+    argument.
+    """
+    if drop_prob == 0.0 or not training:
+        return input
+    keep_prob = 1 - drop_prob
+    shape = (input.shape[0],) + (1,) * (input.ndim - 1)  # work with diff dim tensors, not just 2D ConvNets
+    random_tensor = keep_prob + torch.rand(shape, dtype=input.dtype, device=input.device)
+    random_tensor.floor_()  # binarize
+    output = input.div(keep_prob) * random_tensor
+    return output
+
+
 class Dinov2WithRegistersDropPath(nn.Module):
     """Drop paths (Stochastic Depth) per sample (when applied in main path of residual blocks)."""
 
@@ -345,26 +364,8 @@ class Dinov2WithRegistersDropPath(nn.Module):
         super().__init__()
         self.drop_prob = drop_prob
 
-    def drop_path(input: torch.Tensor, drop_prob: float = 0.0, training: bool = False) -> torch.Tensor:
-        """
-        Drop paths (Stochastic Depth) per sample (when applied in main path of residual blocks).
-        Comment by Ross Wightman: This is the same as the DropConnect impl I created for EfficientNet, etc networks,
-        however, the original name is misleading as 'Drop Connect' is a different form of dropout in a separate paper...
-        See discussion: https://github.com/tensorflow/tpu/issues/494#issuecomment-532968956 ... I've opted for changing the
-        layer and argument names to 'drop path' rather than mix DropConnect as a layer name and use 'survival rate' as the
-        argument.
-        """
-        if drop_prob == 0.0 or not training:
-            return input
-        keep_prob = 1 - drop_prob
-        shape = (input.shape[0],) + (1,) * (input.ndim - 1)  # work with diff dim tensors, not just 2D ConvNets
-        random_tensor = keep_prob + torch.rand(shape, dtype=input.dtype, device=input.device)
-        random_tensor.floor_()  # binarize
-        output = input.div(keep_prob) * random_tensor
-        return output
-
     def forward(self, hidden_states: torch.Tensor) -> torch.Tensor:
-        return self.drop_path(hidden_states, self.drop_prob, self.training)
+        return drop_path(hidden_states, self.drop_prob, self.training)
 
     def extra_repr(self) -> str:
         return "p={}".format(self.drop_prob)
