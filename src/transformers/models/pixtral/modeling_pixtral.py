436104d6fa58d16c22df49c3bceabc304c5b6579
Matt <rocketknight1@gmail.com>
2024-12-20 21:30:42 +0000

    Break copied from in pixtral

--- src/transformers/models/pixtral/modeling_pixtral.py
+++ src/transformers/models/pixtral/modeling_pixtral.py
@@ -126,7 +126,6 @@ def rotate_half(x):
     return torch.cat((-x2, x1), dim=-1)
 
 
-# Copied from transformers.models.llama.modeling_llama.apply_rotary_pos_emb
 def apply_rotary_pos_emb(q, k, cos, sin, position_ids=None, unsqueeze_dim=1):
     """Applies Rotary Position Embedding to the query and key tensors.
 
