e8a5b972972657f8f3b9725265218fc7f62e1f3a
Jade Choghari <chogharijade@gmail.com>
2024-12-21 16:26:26 +0300

    Update src/transformers/models/textnet/modeling_textnet.py
    Co-authored-by: Pavel Iakubovskii <qubvel@gmail.com>

--- src/transformers/models/textnet/modeling_textnet.py
+++ src/transformers/models/textnet/modeling_textnet.py
@@ -426,10 +426,10 @@ class TextNetBackbone(TextNetPreTrainedModel, BackboneMixin):
         Examples:
 
         ```python
-        >>> from transformers import AutoImageProcessor, AutoBackbone
         >>> import torch
-        >>> from PIL import Image
         >>> import requests
+        >>> from PIL import Image
+        >>> from transformers import AutoImageProcessor, AutoBackbone
 
         >>> url = "http://images.cocodataset.org/val2017/000000039769.jpg"
         >>> image = Image.open(requests.get(url, stream=True).raw)
@@ -438,7 +438,8 @@ class TextNetBackbone(TextNetPreTrainedModel, BackboneMixin):
         >>> model = AutoBackbone.from_pretrained("Raghavan/textnet-base")
 
         >>> inputs = processor(image, return_tensors="pt")
-        >>> outputs = model(**inputs)
+        >>> with torch.no_grad():
+        >>>     outputs = model(**inputs)
         ```"""
         return_dict = return_dict if return_dict is not None else self.config.use_return_dict
         output_hidden_states = (
