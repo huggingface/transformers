93aafdc620d39b9ec714ffecf015a085ea221282
Yoni Gozlan <74535834+yonigozlan@users.noreply.github.com>
2024-12-23 13:12:45 -0500

    Add compile test for fast image processor (#35184)
    * add compile test for fast image processor
    
    * override pixtral test

--- src/transformers/models/pixtral/image_processing_pixtral_fast.py
+++ src/transformers/models/pixtral/image_processing_pixtral_fast.py
@@ -346,4 +346,7 @@ class PixtralImageProcessorFast(BaseImageProcessorFast):
             batch_images.append(images)
             batch_image_sizes.append(image_sizes)
 
-        return BatchMixFeature(data={"pixel_values": batch_images, "image_sizes": batch_image_sizes}, tensor_type=None)
+        return BatchMixFeature(
+            data={"pixel_values": batch_images, "image_sizes": batch_image_sizes},
+            tensor_type=None,
+        )
