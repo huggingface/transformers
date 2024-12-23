099b5ddf163575e34c724a22ac4b15086742e6e2
Wing Lian <wing@axolotl.ai>
2024-12-23 07:10:00 -0500

    make LlamaModel._update_causal_mask torch compilable (#35187)
    * make LlamaModel._update_causal_mask torch compilable
    
    * chore: lint (make fix-copies)
    
    * fix-copies
    
    ---------
    
    Co-authored-by: Arthur Zucker <arthur.zucker@gmail.com>

--- src/transformers/models/olmo2/modeling_olmo2.py
+++ src/transformers/models/olmo2/modeling_olmo2.py
@@ -609,7 +609,7 @@ class Olmo2Model(Olmo2PreTrainedModel):
         output_attentions: bool,
     ):
         if self.config._attn_implementation == "flash_attention_2":
-            if attention_mask is not None and 0.0 in attention_mask:
+            if attention_mask is not None and (attention_mask == 0.0).any():
                 return attention_mask
             return None
 
