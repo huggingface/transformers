46065d4e0290ea74f4733da8bcfaa3ced5ea9577
Benjamin Warner <me@benjaminwarner.dev>
2024-12-23 14:11:28 -0600

    organize imports

--- src/transformers/models/modernbert/modular_modernbert.py
+++ src/transformers/models/modernbert/modular_modernbert.py
@@ -13,8 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from contextlib import nullcontext
 import math
+from contextlib import nullcontext
 from typing import Dict, Literal, Optional, Tuple, Union
 
 import torch
