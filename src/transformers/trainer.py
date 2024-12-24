7633c80365481114467387d9f251f1e3a3ca476e
Hafedh Hichri <70411813+not-lain@users.noreply.github.com>
2024-12-24 16:55:25 +0100

    Update src/transformers/trainer.py
    Co-authored-by: Marc Sun <57196510+SunMarc@users.noreply.github.com>

--- src/transformers/trainer.py
+++ src/transformers/trainer.py
@@ -302,7 +302,7 @@ logger = logging.get_logger(__name__)
 
 # Name of the files used for checkpointing
 TRAINING_ARGS_NAME = "training_args.json"
-DEPRECATED_ARGS_NAME = "trainer_state.bin"
+DEPRECATED_ARGS_NAME = "training_args.bin"
 TRAINER_STATE_NAME = "trainer_state.json"
 OPTIMIZER_NAME = "optimizer.pt"
 OPTIMIZER_NAME_BIN = "optimizer.bin"
