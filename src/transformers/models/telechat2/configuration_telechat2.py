7e48f0c39897c2d3844c017029598b3982c40898
xiangw2 <xiangw2@chinatelecom.cn>
2024-12-24 11:19:29 +0800

    fix

--- src/transformers/models/telechat2/configuration_telechat2.py
+++ src/transformers/models/telechat2/configuration_telechat2.py
@@ -143,13 +143,12 @@ class TeleChat2Config(PretrainedConfig):
     keys_to_ignore_at_inference = ["past_key_values"]
     # Default tensor parallel plan for base model `TeleChat2Model`
     base_model_tp_plan = {
-        "layers.*.self_attn.q_proj": "colwise",
-        "layers.*.self_attn.k_proj": "colwise",
-        "layers.*.self_attn.v_proj": "colwise",
-        "layers.*.self_attn.o_proj": "rowwise",
-        "layers.*.mlp.gate_proj": "colwise",
-        "layers.*.mlp.up_proj": "colwise",
-        "layers.*.mlp.down_proj": "rowwise",
+        "h.*.self_attention.query": "colwise",
+        "h.*.self_attention.key_value": "colwise",
+        "h.*.self_attention.dense": "rowwise",
+        "h.*.mlp.gate_proj": "colwise",
+        "h.*.mlp.up_proj": "colwise",
+        "h.*.mlp.down_proj": "rowwise",
     }
 
     def __init__(
