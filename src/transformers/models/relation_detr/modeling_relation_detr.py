99c647b4912d0d53c3938aa5b6c457f344216efc
xiuqhou <xiuqhou@outlook.com>
2024-12-26 18:28:16 +0800

    Remove no_grad context for replace_batch_norm

--- src/transformers/models/relation_detr/modeling_relation_detr.py
+++ src/transformers/models/relation_detr/modeling_relation_detr.py
@@ -537,8 +537,7 @@ class RelationDetrConvEncoder(nn.Module):
         backbone = load_backbone(config)
 
         # replace batch norm by frozen batch norm
-        with torch.no_grad():
-            replace_batch_norm(backbone)
+        replace_batch_norm(backbone)
         self.model = backbone
         self.intermediate_channel_sizes = self.model.channels
 
