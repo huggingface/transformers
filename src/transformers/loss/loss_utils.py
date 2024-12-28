a9d7c6c7a38f12796423e88a8a2111805a8e3d01
Ronald Rogers <rrogers@bloombergindustry.com>
2024-12-28 00:02:42 -0500

    fix accidentally removed import

--- src/transformers/loss/loss_utils.py
+++ src/transformers/loss/loss_utils.py
@@ -16,6 +16,7 @@ import torch
 import torch.nn as nn
 from torch.nn import BCEWithLogitsLoss, MSELoss
 
+from .loss_deformable_detr import DeformableDetrForObjectDetectionLoss, DeformableDetrForSegmentationLoss
 from .loss_for_object_detection import ForObjectDetectionLoss, ForSegmentationLoss
 from .loss_rt_detr import RTDetrForObjectDetectionLoss
 
