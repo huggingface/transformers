d0287458c52b901b0e7a6ee57c678cdb8f95da37
Kevin Rauwolf <sweetpea-git@tentacle.net>
2024-12-27 21:34:35 -0800

    Check whether rescale is requested before checking is_scaled_image

--- src/transformers/models/idefics2/image_processing_idefics2.py
+++ src/transformers/models/idefics2/image_processing_idefics2.py
@@ -529,7 +529,7 @@ class Idefics2ImageProcessor(BaseImageProcessor):
         # All transformations expect numpy arrays.
         images_list = [[to_numpy_array(image) for image in images] for images in images_list]
 
-        if is_scaled_image(images_list[0][0]) and do_rescale:
+        if do_rescale and is_scaled_image(images_list[0][0]):
             logger.warning_once(
                 "It looks like you are trying to rescale already rescaled images. If the input"
                 " images have pixel values between 0 and 1, set `do_rescale=False` to avoid rescaling them again."
--- src/transformers/models/vitmatte/image_processing_vitmatte.py
+++ src/transformers/models/vitmatte/image_processing_vitmatte.py
@@ -223,7 +223,7 @@ class VitMatteImageProcessor(BaseImageProcessor):
         images = [to_numpy_array(image) for image in images]
         trimaps = [to_numpy_array(trimap) for trimap in trimaps]
 
-        if is_scaled_image(images[0]) and do_rescale:
+        if do_rescale and is_scaled_image(images[0]):
             logger.warning_once(
                 "It looks like you are trying to rescale already rescaled images. If the input"
                 " images have pixel values between 0 and 1, set `do_rescale=False` to avoid rescaling them again."
