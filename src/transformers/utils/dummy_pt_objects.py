a3f45b068ab9ad7de8976fc420d2f32f5f1c4a81
Vladislav <vladislav.bronzov@gmail.com>
2025-01-04 16:54:12 +0100

    add init, some fixes

--- src/transformers/utils/dummy_pt_objects.py
+++ src/transformers/utils/dummy_pt_objects.py
@@ -3572,6 +3572,20 @@ class DepthProPreTrainedModel(metaclass=DummyObject):
         requires_backends(self, ["torch"])
 
 
+class DFineModel(metaclass=DummyObject):
+    _backends = ["torch"]
+
+    def __init__(self, *args, **kwargs):
+        requires_backends(self, ["torch"])
+
+
+class DFineModelForObjectDetection(metaclass=DummyObject):
+    _backends = ["torch"]
+
+    def __init__(self, *args, **kwargs):
+        requires_backends(self, ["torch"])
+
+
 class DetrForObjectDetection(metaclass=DummyObject):
     _backends = ["torch"]
 
