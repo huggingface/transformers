838940a48ecb4836a15a24626c4d5f04c1057ae0
Lysandre <hi@lysand.re>
2025-01-05 18:04:22 +0100

    [test_all] TFGPT2Tokenizer

--- src/transformers/models/gpt2/__init__.py
+++ src/transformers/models/gpt2/__init__.py
@@ -24,6 +24,7 @@ if TYPE_CHECKING:
     from .modeling_tf_gpt2 import *
     from .tokenization_gpt2 import *
     from .tokenization_gpt2_fast import *
+    from .tokenization_gpt2_tf import *
 else:
     import sys
 
