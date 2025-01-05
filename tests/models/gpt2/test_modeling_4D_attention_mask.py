32d9db230b7186bc261f024ce1d0f080ce0d099d
sambhavnoobcoder <indosambahv@gmail.com>
2025-01-06 03:14:13 +0530

    setup basic class for testing attention mask for gpt2

--- tests/models/gpt2/test_modeling_4D_attention_mask.py
+++ tests/models/gpt2/test_modeling_4D_attention_mask.py
@@ -3,3 +3,9 @@ import torch
 from transformers import AutoTokenizer
 from transformers.models.gpt2.modeling_gpt2 import GPT2LMHeadModel
 
+class TestAttentionMaskIssue(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.model_name = "gpt2"
+        cls.model = GPT2LMHeadModel.from_pretrained(cls.model_name)
+        cls.tokenizer = AutoTokenizer.from_pretrained(cls.model_name)
