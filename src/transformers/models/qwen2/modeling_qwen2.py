708464b299dd53d0af98195f83bc901cfeb296fc
Yonghye Kwon <developer.0hye@gmail.com>
2025-01-07 21:29:30 +0900

    Update src/transformers/models/qwen2/modeling_qwen2.py
    Co-authored-by: Pavel Iakubovskii <qubvel@gmail.com>

--- src/transformers/models/qwen2/modeling_qwen2.py
+++ src/transformers/models/qwen2/modeling_qwen2.py
@@ -329,10 +329,8 @@ class Qwen2RotaryEmbedding(nn.Module):
             self._dynamic_frequency_update(position_ids, device=x.device)
 
         # Core RoPE block
-        if self.inv_freq.device != x.device:
-            self.inv_freq = self.inv_freq.to(x.device)
-
-        inv_freq_expanded = self.inv_freq[None, :, None].float().expand(position_ids.shape[0], -1, 1)
+        inv_freq = self.inv_freq.to(x.device)
+        inv_freq_expanded = inv_freq[None, :, None].float().expand(position_ids.shape[0], -1, 1)
         position_ids_expanded = position_ids[:, None, :].float()
         # Force float32 (see https://github.com/huggingface/transformers/pull/29285)
         device_type = x.device.type
