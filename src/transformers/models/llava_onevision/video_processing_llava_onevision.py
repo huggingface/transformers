5c952348486832e23a300a7e1108570ca0277821
yonigozlan <yoni.gozlan@huggingface.co>
2025-01-07 23:28:21 +0000

    move make_flat_list_of_images and make_batched_videos to image_utils

--- src/transformers/models/llava_onevision/video_processing_llava_onevision.py
+++ src/transformers/models/llava_onevision/video_processing_llava_onevision.py
@@ -16,6 +16,8 @@
 
 from typing import Dict, List, Optional, Union
 
+import numpy as np
+
 from ...image_processing_utils import BaseImageProcessor, BatchFeature, get_size_dict
 from ...image_transforms import (
     convert_to_rgb,
@@ -31,35 +33,15 @@ from ...image_utils import (
     VideoInput,
     infer_channel_dimension_format,
     is_scaled_image,
-    is_valid_image,
+    make_batched_videos,
     to_numpy_array,
     valid_images,
     validate_preprocess_arguments,
 )
-from ...utils import TensorType, is_vision_available, logging
+from ...utils import TensorType, logging
 
 
 logger = logging.get_logger(__name__)
-
-
-if is_vision_available():
-    from PIL import Image
-
-
-def make_batched_videos(videos) -> List[VideoInput]:
-    if isinstance(videos, (list, tuple)) and isinstance(videos[0], (list, tuple)) and is_valid_image(videos[0][0]):
-        return videos
-
-    elif isinstance(videos, (list, tuple)) and is_valid_image(videos[0]):
-        if isinstance(videos[0], Image.Image) or len(videos[0].shape) == 3:
-            return [videos]
-        elif len(videos[0].shape) == 4:
-            return [list(video) for video in videos]
-
-    elif is_valid_image(videos) and len(videos.shape) == 4:
-        return [list(videos)]
-
-    raise ValueError(f"Could not make batched video from {videos}")
 
 
 class LlavaOnevisionVideoProcessor(BaseImageProcessor):
@@ -138,7 +120,7 @@ class LlavaOnevisionVideoProcessor(BaseImageProcessor):
         do_convert_rgb: bool = None,
         data_format: Optional[ChannelDimension] = ChannelDimension.FIRST,
         input_data_format: Optional[Union[str, ChannelDimension]] = None,
-    ) -> Image.Image:
+    ) -> list[np.ndarray]:
         """
         Args:
             images (`ImageInput`):
