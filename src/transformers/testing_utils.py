2cd007b34bd3fccd62c7ff6b97325afbacab587d
ydshieh <ydshieh@users.noreply.github.com>
2025-01-08 17:55:58 +0100

    oh no!!!

--- src/transformers/testing_utils.py
+++ src/transformers/testing_utils.py
@@ -1426,8 +1426,14 @@ def set_model_tester_for_less_flaky_test(test_case):
     # For Albert
     if hasattr(test_case.model_tester, "num_hidden_groups"):
         test_case.model_tester.num_hidden_groups = test_case.model_tester.num_hidden_layers
+    # For DPT
     if hasattr(test_case.model_tester, "neck_hidden_sizes"):
         test_case.model_tester.neck_hidden_sizes = test_case.model_tester.neck_hidden_sizes[
+            : test_case.model_tester.num_hidden_layers
+        ]
+    # For DPT auto backbone
+    if hasattr(test_case.model_tester, "out_features"):
+        test_case.model_tester.out_features = test_case.model_tester.out_features[
             : test_case.model_tester.num_hidden_layers
         ]
 
