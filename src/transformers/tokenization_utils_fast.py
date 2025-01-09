e8738e5d3857e876852e41e2b165f8323f1566de
Tom Aarsen <Cubiegamedev@gmail.com>
2025-01-09 15:35:19 +0100

    Ensure that add_prefix_space is propagated to backend_tokenizer.pre_tokenizer
    in PreTrainedTokenizerFast, rather than relying on subclasses to take care of this.

--- src/transformers/tokenization_utils_fast.py
+++ src/transformers/tokenization_utils_fast.py
@@ -102,6 +102,7 @@ class PreTrainedTokenizerFast(PreTrainedTokenizerBase):
         fast_tokenizer_file = kwargs.pop("tokenizer_file", None)
         from_slow = kwargs.pop("from_slow", False)
         added_tokens_decoder = kwargs.pop("added_tokens_decoder", {})
+        add_prefix_space = kwargs.get("add_prefix_space", False)
 
         if from_slow and slow_tokenizer is None and self.slow_tokenizer_class is None:
             raise ValueError(
@@ -205,6 +206,14 @@ class PreTrainedTokenizerFast(PreTrainedTokenizerBase):
                 tokens.append(token)
             if tokens:
                 self.add_tokens(tokens)
+
+        pre_tok_state = json.loads(self.backend_tokenizer.pre_tokenizer.__getstate__())
+        if pre_tok_state.get("add_prefix_space", add_prefix_space) != add_prefix_space:
+            pre_tok_class = getattr(pre_tokenizers_fast, pre_tok_state.pop("type"))
+            pre_tok_state["add_prefix_space"] = add_prefix_space
+            self.backend_tokenizer.pre_tokenizer = pre_tok_class(**pre_tok_state)
+
+        self.add_prefix_space = add_prefix_space
 
     @property
     def is_fast(self) -> bool:
