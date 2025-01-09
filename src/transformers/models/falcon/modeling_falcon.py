965a2fb3204375492787d7bb2fbec76964cef692
Cyril Vallez <cyril.vallez@huggingface.co>
2025-01-09 11:09:09 +0100

    More model refactoring! (#35359)
    * cohere
    
    * style
    
    * phi3
    
    * style
    
    * small fix
    
    * small fix
    
    * phi3 longrope
    
    * oups
    
    * Update rope (only for phi3 still)
    
    * Update test_modeling_rope_utils.py
    
    * Update modeling_phi3.py
    
    * fix
    
    * fix copies
    
    * style
    
    * Fix copied from bad renaming

--- src/transformers/models/falcon/modeling_falcon.py
+++ src/transformers/models/falcon/modeling_falcon.py
@@ -110,11 +110,7 @@ def apply_rotary_pos_emb(q, k, cos, sin, position_ids=None, unsqueeze_dim=1):
 
 # Copied from transformers.models.llama.modeling_llama.LlamaRotaryEmbedding with Llama->Falcon
 class FalconRotaryEmbedding(nn.Module):
-    def __init__(
-        self,
-        config: FalconConfig,
-        device=None,
-    ):
+    def __init__(self, config: FalconConfig, device=None):
         super().__init__()
         self.rope_kwargs = {}
         # BC: "rope_type" was originally "type"
