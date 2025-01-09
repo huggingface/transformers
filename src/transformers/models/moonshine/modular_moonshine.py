02a1f1f8e07f422daa47b881d5154f40136b0ea7
Eustache Le Bihan <eulebihan@gmail.com>
2025-01-09 12:51:40 +0100

    update config

--- src/transformers/models/moonshine/modular_moonshine.py
+++ src/transformers/models/moonshine/modular_moonshine.py
@@ -52,32 +52,32 @@ class MoonshineConfig(PretrainedConfig):
             `inputs_ids` passed when calling [`MoonshineModel`].
         hidden_size (`int`, *optional*, defaults to 288):
             Dimension of the hidden representations.
-        intermediate_size (`int`, *optional*):
+        intermediate_size (`int`, *optional*, defaults to 1152):
             Dimension of the MLP representations.
-        conv1_kernel_size (`int`, *optional*, defaults to 127):
-            Kernel size of the first convolutional layer.
-        conv1_stride (`int`, *optional*, defaults to 64):
-            Stride of the first convolutional layer.
-        conv2_kernel_size (`int`, *optional*, defaults to 7):
-            Kernel size of the second convolutional layer.
-        conv2_stride (`int`, *optional*, defaults to 3):
-            Stride of the second convolutional layer.
-        conv3_kernel_size (`int`, *optional*, defaults to 3):
-            Kernel size of the third convolutional layer.
-        conv3_stride (`int`, *optional*, defaults to 2):
-            Stride of the third convolutional layer.
-        num_hidden_layers (`int`, *optional*, defaults to 6):
-            Number of hidden layers in the Transformer encoder and decoder.
-        num_attention_heads (`int`, *optional*, defaults to 8):
-            Number of attention heads for each attention layer in the Transformer encoder and decoder.
-        num_key_value_heads (`int`, *optional*):
+        encoder_num_hidden_layers (`int`, *optional*, defaults to 6):
+            Number of hidden layers in the Transformer encoder.
+        decoder_num_hidden_layers (`int`, *optional*, defaults to 6):
+            Number of hidden layers in the Transformer decoder.
+        encoder_num_attention_heads (`int`, *optional*, defaults to 8):
+            Number of attention heads for each attention layer in the Transformer encoder.
+        decoder_num_attention_heads (`int`, *optional*, defaults to 8):
+            Number of attention heads for each attention layer in the Transformer decoder.
+        encoder_num_key_value_heads (`int`, *optional*):
             This is the number of key_value heads that should be used to implement Grouped Query Attention. If
-            `num_key_value_heads=num_attention_heads`, the model will use Multi Head Attention (MHA), if
-            `num_key_value_heads=1` the model will use Multi Query Attention (MQA) otherwise GQA is used. When
+            `encoder_num_key_value_heads=encoder_num_attention_heads`, the model will use Multi Head Attention (MHA), if
+            `encoder_num_key_value_heads=1` the model will use Multi Query Attention (MQA) otherwise GQA is used. When
             converting a multi-head checkpoint to a GQA checkpoint, each group key and value head should be constructed
             by meanpooling all the original heads within that group. For more details checkout [this
             paper](https://arxiv.org/pdf/2305.13245.pdf). If it is not specified, will default to
             `num_attention_heads`.
+        decoder_num_key_value_heads (`int`, *optional*):
+            This is the number of key_value heads that should be used to implement Grouped Query Attention. If
+            `decoder_num_key_value_heads=decoder_num_attention_heads`, the model will use Multi Head Attention (MHA), if
+            `decoder_num_key_value_heads=1` the model will use Multi Query Attention (MQA) otherwise GQA is used. When
+            converting a multi-head checkpoint to a GQA checkpoint, each group key and value head should be constructed
+            by meanpooling all the original heads within that group. For more details checkout [this
+            paper](https://arxiv.org/pdf/2305.13245.pdf). If it is not specified, will default to
+            `decoder_num_attention_heads`.
         encoder_hidden_act (`str` or `function`, *optional*, defaults to `"gelu"`):
             The non-linear activation function (function or string) in the encoder.
         decoder_hidden_act (`str` or `function`, *optional*, defaults to `"silu"`):
@@ -86,8 +86,6 @@ class MoonshineConfig(PretrainedConfig):
             The maximum sequence length that this model might ever be used with.
         initializer_range (`float`, *optional*, defaults to 0.02):
             The standard deviation of the truncated_normal_initializer for initializing all weight matrices.
-        layer_norm_eps (`float`, *optional*, defaults to 1e-05):
-            The epsilon used by the layer normalization layers.
         decoder_start_token_id (`int`, *optional*, defaults to 1):
             Corresponds to the "<|startoftranscript|>" token, which is automatically used when no `decoder_input_ids`
             are provided to the `generate` function. It is used to guide the model`s generation process depending on
@@ -96,55 +94,55 @@ class MoonshineConfig(PretrainedConfig):
             Whether or not the model should return the last key/values attentions (not used by all models).
         rope_theta (`float`, *optional*, defaults to 10000.0):
             The base period of the RoPE embeddings.
+        rope_scaling (`Dict`, *optional*):
+            Dictionary containing the scaling configuration for the RoPE embeddings. NOTE: if you apply new rope type
+            and you expect the model to work on longer `max_position_embeddings`, we recommend you to update this value
+            accordingly.
+            Expected contents:
+                `rope_type` (`str`):
+                    The sub-variant of RoPE to use. Can be one of ['default', 'linear', 'dynamic', 'yarn', 'longrope',
+                    'llama3'], with 'default' being the original RoPE implementation.
+                `factor` (`float`, *optional*):
+                    Used with all rope types except 'default'. The scaling factor to apply to the RoPE embeddings. In
+                    most scaling types, a `factor` of x will enable the model to handle sequences of length x *
+                    original maximum pre-trained length.
+                `original_max_position_embeddings` (`int`, *optional*):
+                    Used with 'dynamic', 'longrope' and 'llama3'. The original max position embeddings used during
+                    pretraining.
+                `attention_factor` (`float`, *optional*):
+                    Used with 'yarn' and 'longrope'. The scaling factor to be applied on the attention
+                    computation. If unspecified, it defaults to value recommended by the implementation, using the
+                    `factor` field to infer the suggested value.
+                `beta_fast` (`float`, *optional*):
+                    Only used with 'yarn'. Parameter to set the boundary for extrapolation (only) in the linear
+                    ramp function. If unspecified, it defaults to 32.
+                `beta_slow` (`float`, *optional*):
+                    Only used with 'yarn'. Parameter to set the boundary for interpolation (only) in the linear
+                    ramp function. If unspecified, it defaults to 1.
+                `short_factor` (`List[float]`, *optional*):
+                    Only used with 'longrope'. The scaling factor to be applied to short contexts (<
+                    `original_max_position_embeddings`). Must be a list of numbers with the same length as the hidden
+                    size divided by the number of attention heads divided by 2
+                `long_factor` (`List[float]`, *optional*):
+                    Only used with 'longrope'. The scaling factor to be applied to long contexts (<
+                    `original_max_position_embeddings`). Must be a list of numbers with the same length as the hidden
+                    size divided by the number of attention heads divided by 2
+                `low_freq_factor` (`float`, *optional*):
+                    Only used with 'llama3'. Scaling factor applied to low frequency components of the RoPE
+                `high_freq_factor` (`float`, *optional*):
+                    Only used with 'llama3'. Scaling factor applied to high frequency components of the RoPE
+        partial_rotary_factor (`float`, *optional*, defaults to 0.9):
+            Percentage of the query and keys which will have rotary embedding.
         is_encoder_decoder (`bool`, *optional*, defaults to `True`):
             Whether the model is used as an encoder/decoder or not.
-        min_rotary_ndims (`int`, *optional*, defaults to 32):
-            The minimum number of dimensions of the RoPE.
         attention_bias (`bool`, *optional*, defaults to `False`):
             Whether to use a bias in the query, key, value and output projection layers during self-attention.
         attention_dropout (`float`, *optional*, defaults to 0.0):
             The dropout ratio for the attention probabilities.
-        ff_mult (`int`, *optional*, defaults to 4):
-            Factor by which to scale the intermediate size.
         bos_token_id (`int`, *optional*, defaults to 1):
             Denotes beginning of sequences token id.
         eos_token_id (`int`, *optional*, defaults to 2):
             Denotes end of sequences token id.
-        apply_spec_augment (`bool`, *optional*, defaults to `False`):
-            Whether to apply *SpecAugment* data augmentation to the outputs of the feature encoder. For reference see
-            [SpecAugment: A Simple Data Augmentation Method for Automatic Speech
-            Recognition](https://arxiv.org/abs/1904.08779).
-        mask_time_prob (`float`, *optional*, defaults to 0.05):
-            Percentage (between 0 and 1) of all feature vectors along the time axis which will be masked. The masking
-            procecure generates `mask_time_prob*len(time_axis)/mask_time_length` independent masks over the axis. If
-            reasoning from the propability of each feature vector to be chosen as the start of the vector span to be
-            masked, *mask_time_prob* should be `prob_vector_start*mask_time_length`. Note that overlap may decrease the
-            actual percentage of masked vectors. This is only relevant if `apply_spec_augment == True`.
-        mask_time_length (`int`, *optional*, defaults to 10):
-            Length of vector span along the time axis.
-            The minimum number of masks of length `mask_feature_length` generated along the time axis, each time step,
-            irrespectively of `mask_feature_prob`. Only relevant if ''mask_time_prob*len(time_axis)/mask_time_length <
-            mask_time_min_masks''
-        mask_time_min_masks (`int`, *optional*, defaults to 2):
-            The minimum number of masks of length `mask_feature_length` generated along the time axis, each time step,
-            irrespectively of `mask_feature_prob`. Only relevant if ''mask_time_prob*len(time_axis)/mask_time_length <
-            mask_time_min_masks''
-        mask_feature_prob (`float`, *optional*, defaults to 0.0):
-            Percentage (between 0 and 1) of all feature vectors along the feature axis which will be masked. The
-            masking procecure generates `mask_feature_prob*len(feature_axis)/mask_time_length` independent masks over
-            the axis. If reasoning from the propability of each feature vector to be chosen as the start of the vector
-            span to be masked, *mask_feature_prob* should be `prob_vector_start*mask_feature_length`. Note that overlap
-            may decrease the actual percentage of masked vectors. This is only relevant if `apply_spec_augment is
-            True`.
-        mask_feature_length (`int`, *optional*, defaults to 10):
-            Length of vector span along the feature axis.
-            The minimum number of masks of length `mask_feature_length` generated along the feature axis, each time
-            step, irrespectively of `mask_feature_prob`. Only relevant if
-            `mask_feature_prob*len(feature_axis)/mask_feature_length < mask_feature_min_masks`.
-        mask_feature_min_masks (`int`, *optional*, defaults to 0):
-            The minimum number of masks of length `mask_feature_length` generated along the feature axis, each time
-            step, irrespectively of `mask_feature_prob`. Only relevant if
-            `mask_feature_prob*len(feature_axis)/mask_feature_length < mask_feature_min_masks`.
 
     Example:
 
@@ -168,78 +166,60 @@ class MoonshineConfig(PretrainedConfig):
         self,
         vocab_size=32768,
         hidden_size=288,
-        intermediate_size=None,
-        conv1_kernel_size=127,
-        conv1_stride=64,
-        conv2_kernel_size=7,
-        conv2_stride=3,
-        conv3_kernel_size=3,
-        conv3_stride=2,
-        num_hidden_layers=6,
-        num_attention_heads=8,
-        num_key_value_heads=None,
+        intermediate_size=1152,
+        encoder_num_hidden_layers=6,
+        decoder_num_hidden_layers=6,
+        encoder_num_attention_heads=8,
+        decoder_num_attention_heads=8,
+        encoder_num_key_value_heads=None,
+        decoder_num_key_value_heads=None,
         encoder_hidden_act="gelu",
         decoder_hidden_act="silu",
         max_position_embeddings=2048,
         initializer_range=0.02,
-        layer_norm_eps=1e-5,
         decoder_start_token_id=1,
         use_cache=True,
         rope_theta=10000.0,
+        rope_scaling=None,
+        partial_rotary_factor=0.9,
         is_encoder_decoder=True,
-        min_rotary_ndims=32,
         attention_bias=False,
         attention_dropout=0.0,
-        ff_mult=4,
         bos_token_id=1,
         eos_token_id=2,
-        apply_spec_augment=False,
-        mask_time_prob=0.05,
-        mask_time_length=10,
-        mask_time_min_masks=2,
-        mask_feature_prob=0.0,
-        mask_feature_length=10,
-        mask_feature_min_masks=0,
         **kwargs,
     ):
         self.vocab_size = vocab_size
         self.hidden_size = hidden_size
         self.intermediate_size = intermediate_size
-        self.conv1_kernel_size = conv1_kernel_size
-        self.conv1_stride = conv1_stride
-        self.conv2_kernel_size = conv2_kernel_size
-        self.conv2_stride = conv2_stride
-        self.conv3_kernel_size = conv3_kernel_size
-        self.conv3_stride = conv3_stride
-        self.num_hidden_layers = num_hidden_layers
-        self.num_attention_heads = num_attention_heads
+        self.encoder_num_hidden_layers = encoder_num_hidden_layers
+        self.decoder_num_hidden_layers = decoder_num_hidden_layers
+        self.encoder_num_attention_heads = encoder_num_attention_heads
+        self.decoder_num_attention_heads = decoder_num_attention_heads
 
-        if num_key_value_heads is None:
-            num_key_value_heads = num_attention_heads
+        if encoder_num_key_value_heads is None:
+            encoder_num_key_value_heads = encoder_num_attention_heads
+        self.encoder_num_key_value_heads = encoder_num_key_value_heads
 
-        self.num_key_value_heads = num_key_value_heads
+        if decoder_num_key_value_heads is None:
+            decoder_num_key_value_heads = decoder_num_attention_heads
+        self.decoder_num_key_value_heads = decoder_num_key_value_heads
+
         self.encoder_hidden_act = encoder_hidden_act
         self.decoder_hidden_act = decoder_hidden_act
         self.max_position_embeddings = max_position_embeddings
         self.initializer_range = initializer_range
-        self.layer_norm_eps = layer_norm_eps
         self.decoder_start_token_id = decoder_start_token_id
         self.use_cache = use_cache
         self.rope_theta = rope_theta
+        self.rope_scaling = rope_scaling
+        self.partial_rotary_factor = partial_rotary_factor
         self.is_encoder_decoder = is_encoder_decoder
-        self.min_rotary_ndims = min_rotary_ndims
         self.attention_bias = attention_bias
         self.attention_dropout = attention_dropout
-        self.ff_mult = ff_mult
 
-        # fine-tuning config parameters for SpecAugment: https://arxiv.org/abs/1904.08779
-        self.apply_spec_augment = apply_spec_augment
-        self.mask_time_prob = mask_time_prob
-        self.mask_time_length = mask_time_length
-        self.mask_time_min_masks = mask_time_min_masks
-        self.mask_feature_prob = mask_feature_prob
-        self.mask_feature_length = mask_feature_length
-        self.mask_feature_min_masks = mask_feature_min_masks
+        # Validate the correctness of rotary position embeddings parameters
+        rope_config_validation(self)
 
         super().__init__(
             bos_token_id=bos_token_id,
