ddbac918c333836f5ff42fe32427ef6d97c1000f
Joel Koch <joel@bitcrowd.net>
2025-01-10 14:59:00 +0100

    Uncomment warn_if_padding_and_no_attention_mask

--- src/transformers/models/jina_bert/modular_jina_bert.py
+++ src/transformers/models/jina_bert/modular_jina_bert.py
@@ -890,7 +890,7 @@ class JinaBertModel(BertModel):
         if input_ids is not None and inputs_embeds is not None:
             raise ValueError("You cannot specify both input_ids and inputs_embeds at the same time")
         elif input_ids is not None:
-            # self.warn_if_padding_and_no_attention_mask(input_ids, attention_mask)
+            self.warn_if_padding_and_no_attention_mask(input_ids, attention_mask)
             input_shape = input_ids.size()
         elif inputs_embeds is not None:
             input_shape = inputs_embeds.size()[:-1]
