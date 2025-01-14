b2c6a35f89086b7c62cf300b1c0afe952327bfd1
xiuqhou <xiuqhou@outlook.com>
2025-01-14 21:13:14 +0800

    Update copyright year to 2025

--- src/transformers/models/relation_detr/image_processing_relation_detr.py
+++ src/transformers/models/relation_detr/image_processing_relation_detr.py
@@ -1,5 +1,5 @@
 # coding=utf-8
-# Copyright 2024 The HuggingFace Inc. team. All rights reserved.
+# Copyright 2025 The HuggingFace Inc. team. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
