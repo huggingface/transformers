88aac166dbf3720c3ba127d35954576e1c3b1ffe
Matt <rocketknight1@gmail.com>
2025-01-14 17:42:33 +0000

    Make set_initialized_submodules O(kN + log(N)) instead of O(N^2), where k << N

--- src/transformers/modeling_utils.py
+++ src/transformers/modeling_utils.py
@@ -32,6 +32,7 @@ from functools import partial, wraps
 from threading import Thread
 from typing import Any, Callable, Dict, List, Optional, Set, Tuple, Type, TypeVar, Union
 from zipfile import is_zipfile
+from bisect import bisect_left, bisect_right
 
 import torch
 from huggingface_hub import split_torch_state_dict_into_shards
@@ -565,9 +566,22 @@ def set_initialized_submodules(model, state_dict_keys):
     Sets the `_is_hf_initialized` flag in all submodules of a given model when all its weights are in the loaded state
     dict.
     """
+    state_dict_keys = sorted(state_dict_keys)  # So we can do binary search on it - this becomes important when it's big
+
     not_initialized_submodules = {}
     for module_name, module in model.named_modules():
-        loaded_keys = {k.replace(f"{module_name}.", "") for k in state_dict_keys if k.startswith(f"{module_name}.")}
+        # loaded_keys is the set of keys that are in state_dict_keys and start with module_name + "."
+        prefix = module_name + "."
+        loaded_keys = set()
+        # Use binary search to find the start of the keys that have this prefix
+        i = bisect_left(state_dict_keys, prefix)
+        while i < len(state_dict_keys) and state_dict_keys[i].startswith(prefix):
+            # Iterate until we reach the end of the keys with this prefix
+            loaded_keys.add(state_dict_keys[i].removeprefix(prefix))
+            i += 1
+        # This next block is just for debug testing and should be removed before merging
+        old_loaded_keys = {k.replace(f"{module_name}.", "") for k in state_dict_keys if k.startswith(f"{module_name}.")}
+        assert old_loaded_keys == loaded_keys
         # When checking if the root module is loaded all state_dict_keys must be used.
         if module_name == "":
             loaded_keys = set(state_dict_keys)
