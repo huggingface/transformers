d10963b995857eacadfe6b888543a8d4d831b06e
Joao Gante <joao@huggingface.co>
2025-01-15 14:01:16 +0000

    augment test as suggested

--- tests/utils/test_modeling_utils.py
+++ tests/utils/test_modeling_utils.py
@@ -37,6 +37,7 @@ from transformers import (
     AutoModel,
     AutoModelForImageClassification,
     AutoModelForSequenceClassification,
+    DynamicCache,
     LlavaForConditionalGeneration,
     OwlViTForObjectDetection,
     PretrainedConfig,
@@ -1790,10 +1791,10 @@ class ModelUtilsTest(TestCasePlus):
         )
         self.assertTrue(check_models_equal(model, model_loaded))
 
-    def test_training_with_cache(self):
+    def test_cache_at_train_time(self):
         """
         Some fine-tuning methods require the use of cache, like prefix tuning in PEFT. This test checks that a cache
-        is used if we request it. Related issue: #35648
+        is at train time used if we request it. Related issue: #35648
         """
         model = AutoModelForCausalLM.from_pretrained(TINY_MISTRAL)
         tokenizer = AutoTokenizer.from_pretrained(TINY_MISTRAL)
@@ -1805,12 +1806,26 @@ class ModelUtilsTest(TestCasePlus):
 
         # If we set `use_cache=True` while training, then a cache is returned
         model_outputs = model(**model_inputs, use_cache=True)
-        self.assertTrue(model_outputs.past_key_values is not None)
+        self.assertIsInstance(model_outputs.past_key_values, DynamicCache)
+        self.assertTrue(model.training)
+
+        # simulate injecting virtual tokens like in prefix tuning
+        num_virtual_tokens = 3
+        past_key_values = [torch.randn(2, 1, 2, num_virtual_tokens, 8)] * 2
+        past_key_values = DynamicCache.from_legacy_cache(past_key_values)
+        model_inputs["attention_mask"] = torch.cat(
+            (
+                model_inputs["attention_mask"],
+                torch.ones(1, num_virtual_tokens).to(model_inputs["attention_mask"].device),
+            ),
+            dim=1,
+        )
+        model_outputs = model(**model_inputs, past_key_values=past_key_values, use_cache=True)
         self.assertTrue(model.training)
 
         # We can also disable the cache to skip a few operations, if the training loop doesn't need cache
         model_outputs = model(**model_inputs, use_cache=False)
-        self.assertTrue(model_outputs.past_key_values is None)
+        self.assertIsNone(model_outputs.past_key_values)
         self.assertTrue(model.training)
 
 
