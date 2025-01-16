d6fc3524ef06473804374541f3b20d48dd3369bf
sambhavnoobcoder <indosambahv@gmail.com>
2025-01-16 22:38:46 +0530

    Test output_dir default to 'tmp_trainer' when  unspecified.

--- tests/test_training_args.py
+++ tests/test_training_args.py
@@ -4,3 +4,8 @@ import os
 from transformers import TrainingArguments
 
 
+class TestTrainingArguments(unittest.TestCase):
+    def test_default_output_dir(self):
+        """Test that output_dir defaults to 'tmp_trainer' when not specified."""
+        args = TrainingArguments(output_dir=None)
+        self.assertEqual(args.output_dir, "tmp_trainer")
