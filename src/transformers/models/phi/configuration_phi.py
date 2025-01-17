ef0b5e279c697e3d0714e02cfa2c842f1d0a8a0e
Arthur Zucker <arthur.zucker@gmail.com>
2025-01-17 11:27:13 +0100

    add more TP support

--- src/transformers/models/phi/configuration_phi.py
+++ src/transformers/models/phi/configuration_phi.py
@@ -138,6 +138,15 @@ class PhiConfig(PretrainedConfig):
 
     model_type = "phi"
     keys_to_ignore_at_inference = ["past_key_values"]
+    base_model_tp_plan = {
+        "layers.*.self_attn.q_proj": "colwise",
+        "layers.*.self_attn.k_proj": "colwise",
+        "layers.*.self_attn.v_proj": "colwise",
+        "layers.*.self_attn.o_proj": "rowwise",
+        "layers.*.mlp.gate_proj": "colwise",
+        "layers.*.mlp.up_proj": "colwise",
+        "layers.*.mlp.down_proj": "rowwise",
+    }
 
     def __init__(
         self,
