29b7858e7f9762c980c05344020158140f842244
Fabian Perez *-* <nelsonfabiancs8@gmail.com>
2025-01-17 23:59:04 -0500

    handle is_timm kwarg

--- src/transformers/image_processing_base.py
+++ src/transformers/image_processing_base.py
@@ -411,6 +411,7 @@ class ImageProcessingMixin(PushToHubMixin):
         """
         image_processor_dict = image_processor_dict.copy()
         return_unused_kwargs = kwargs.pop("return_unused_kwargs", False)
+        is_timm = kwargs.pop("is_timm", False)
 
         # The `size` parameter is a dict and was previously an int or tuple in feature extractors.
         # We set `size` here directly to the `image_processor_dict` so that it is converted to the appropriate
@@ -420,7 +421,10 @@ class ImageProcessingMixin(PushToHubMixin):
         if "crop_size" in kwargs and "crop_size" in image_processor_dict:
             image_processor_dict["crop_size"] = kwargs.pop("crop_size")
 
-        image_processor = cls(**image_processor_dict)
+        if is_timm:
+            image_processor = cls(image_processor_dict)
+        else:
+            image_processor = cls(**image_processor_dict)
 
         # Update image_processor with kwargs if needed
         to_remove = []
