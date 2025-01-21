1f5aaf3082386ed8ab46b90992a97af868f7a7fb
Wauplin <lucainp@gmail.com>
2025-01-21 11:17:08 +0100

    Remove deprecated get_cached_models

--- src/transformers/utils/hub.py
+++ src/transformers/utils/hub.py
@@ -165,45 +165,6 @@ def is_remote_url(url_or_filename):
     return parsed.scheme in ("http", "https")
 
 
-# TODO: remove this once fully deprecated
-# TODO? remove from './examples/research_projects/lxmert/utils.py' as well
-# TODO? remove from './examples/research_projects/visual_bert/utils.py' as well
-@_deprecate_method(version="4.39.0", message="This method is outdated and does not support the new cache system.")
-def get_cached_models(cache_dir: Union[str, Path] = None) -> List[Tuple]:
-    """
-    Returns a list of tuples representing model binaries that are cached locally. Each tuple has shape `(model_url,
-    etag, size_MB)`. Filenames in `cache_dir` are use to get the metadata for each model, only urls ending with *.bin*
-    are added.
-
-    Args:
-        cache_dir (`Union[str, Path]`, *optional*):
-            The cache directory to search for models within. Will default to the transformers cache if unset.
-
-    Returns:
-        List[Tuple]: List of tuples each with shape `(model_url, etag, size_MB)`
-    """
-    if cache_dir is None:
-        cache_dir = TRANSFORMERS_CACHE
-    elif isinstance(cache_dir, Path):
-        cache_dir = str(cache_dir)
-    if not os.path.isdir(cache_dir):
-        return []
-
-    cached_models = []
-    for file in os.listdir(cache_dir):
-        if file.endswith(".json"):
-            meta_path = os.path.join(cache_dir, file)
-            with open(meta_path, encoding="utf-8") as meta_file:
-                metadata = json.load(meta_file)
-                url = metadata["url"]
-                etag = metadata["etag"]
-                if url.endswith(".bin"):
-                    size_MB = os.path.getsize(meta_path.strip(".json")) / 1e6
-                    cached_models.append((url, etag, size_MB))
-
-    return cached_models
-
-
 def define_sagemaker_information():
     try:
         instance_data = requests.get(os.environ["ECS_CONTAINER_METADATA_URI"]).json()
