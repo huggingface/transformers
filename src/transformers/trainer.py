59e10153daab4a164d298e0909c21a29661ad758
[[ -z $EMAIL ]] && read -e -p "Enter your email (for git configuration): " EMAIL <muellerzr@gmail.com>
2025-01-21 09:24:17 -0500

    Document what's happening in the code

--- src/transformers/trainer.py
+++ src/transformers/trainer.py
@@ -1477,12 +1477,13 @@ class Trainer:
 
         len_dataloader = None
         num_train_tokens = None
+
+        # Since the actual `max_steps` depends on if we have a dataloader length, number of epochs, or max_steps,
+        # we keep it defined here first as the "base case" if we don't have a dataloader length later
         max_steps = args.max_steps
+        # If max_steps is negative, we use the number of epochs to determine the number of total steps later
         epoch_based = max_steps < 0
-        if args.include_tokens_per_second:
-            num_train_tokens = self.num_tokens(train_dataloader, None if epoch_based else max_steps)
-            if epoch_based:
-                num_train_tokens *= args.gradient_accumulation_steps
+
         # Setting a very large number of epochs so we go as many times as necessary over the iterator.
         num_train_epochs = math.ceil(args.num_train_epochs) if epoch_based else sys.maxsize
         num_update_steps_per_epoch = max_steps
@@ -1491,16 +1492,24 @@ class Trainer:
         # but it's the best we can do
         num_train_samples = max_steps * total_train_batch_size
 
+        # Case 2: We have a dataloader length and can extrapolate
         if has_length(train_dataloader):
             len_dataloader = len(train_dataloader)
             num_update_steps_per_epoch = max(len_dataloader // args.gradient_accumulation_steps, 1)
             num_examples = self.num_examples(train_dataloader)
             if epoch_based:
+                # Since we have a dataloader length and are utilizing epochs, we can extrapolate the number of steps
                 max_steps = math.ceil(args.num_train_epochs * num_update_steps_per_epoch)
             else:
+                # Otherwise, we calculate the number of epochs from max_steps & update steps
                 num_train_epochs = max_steps // num_update_steps_per_epoch + int(
                     max_steps % num_update_steps_per_epoch > 0
                 )
+
+        if args.include_tokens_per_second:
+            num_train_tokens = self.num_tokens(train_dataloader, None if epoch_based else max_steps)
+            if epoch_based:
+                num_train_tokens *= args.gradient_accumulation_steps
 
         # Can this be a callback?
         if DebugOption.UNDERFLOW_OVERFLOW in self.args.debug:
