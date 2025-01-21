6de9d1e2e417df1e02f01cbc4355acd1bc311426
sambhavnoobcoder <indosambahv@gmail.com>
2025-01-22 03:08:43 +0530

    base class functinoal steup

--- tests/pipelines/test_mask_crop_pipeline.py
+++ tests/pipelines/test_mask_crop_pipeline.py
@@ -10,3 +10,13 @@ from transformers.testing_utils import require_torch, require_vision, slow
 from transformers.utils.import_utils import is_vision_available
 
 from .test_pipelines_common import ANY
+
+@require_vision
+@require_torch
+class MaskCropPipelineTests(unittest.TestCase):
+    def get_test_pipeline(self):
+        pipe = pipeline("mask-generation", model="facebook/sam-vit-base")
+        return pipe
+    
+    def get_image_processor(self):
+        return AutoImageProcessor.from_pretrained("facebook/sam-vit-base")
