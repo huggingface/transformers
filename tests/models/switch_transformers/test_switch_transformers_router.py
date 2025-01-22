864b3bdb07d5eff4d80f19611d677e80ff3da65c
sambhavnoobcoder <indosambahv@gmail.com>
2025-01-22 23:59:15 +0530

    setup for class

--- tests/models/switch_transformers/test_switch_transformers_router.py
+++ tests/models/switch_transformers/test_switch_transformers_router.py
@@ -5,3 +5,13 @@ from transformers.models.switch_transformers.modeling_switch_transformers import
     SwitchTransformersTop1Router,
     SwitchTransformersSparseMLP
 )
+
+class SwitchTransformersRouterTest(unittest.TestCase):
+    def setUp(self):
+        self.config = SwitchTransformersConfig(
+            num_experts=2,
+            hidden_size=32,
+            d_ff=16,
+            jitter_noise=0.2,
+            expert_capacity=4
+        )
