ff35a57a09e5665efde301b1859f39a6fcebb9b6
Matt <rocketknight1@gmail.com>
2025-01-22 17:10:14 +0000

    make fixup

--- src/transformers/__init__.py
+++ src/transformers/__init__.py
@@ -2995,11 +2995,11 @@ else:
     _import_structure["models.modernbert"].extend(
         [
             "ModernBertForMaskedLM",
+            "ModernBertForQuestionAnswering",
             "ModernBertForSequenceClassification",
             "ModernBertForTokenClassification",
             "ModernBertModel",
             "ModernBertPreTrainedModel",
-            "ModernBertForQuestionAnswering",
         ]
     )
     _import_structure["models.moonshine"].extend(
