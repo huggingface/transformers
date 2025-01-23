0122d98aa4f3ad96dfcec2a7458f1cf6257bcf04
sambhavnoobcoder <indosambahv@gmail.com>
2025-01-24 00:48:19 +0530

    class setup

--- tests/models/paligemma/test_attention_mask.py
+++ tests/models/paligemma/test_attention_mask.py
@@ -4,3 +4,15 @@ import requests
 from PIL import Image
 from transformers import PaliGemmaForConditionalGeneration, PaliGemmaProcessor
 from transformers.testing_utils import require_torch, require_vision, slow, torch_device
+
+@require_torch
+@require_vision
+class TestPaliGemmaAttentionMask(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.processor = PaliGemmaProcessor.from_pretrained("google/paligemma-3b-pt-224")
+        cls.model = PaliGemmaForConditionalGeneration.from_pretrained(
+            "google/paligemma-3b-pt-224",
+            torch_dtype=torch.float16,
+            device_map="auto"
+        )
