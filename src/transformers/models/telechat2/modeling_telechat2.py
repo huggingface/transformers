6c4ffc64950e0dc80fb432803eb330c73f68e7e7
shunxing12345 <747777271@qq.com>
2025-01-23 13:52:39 +0800

    fix
    Signed-off-by: shunxing12345 <747777271@qq.com>

--- src/transformers/models/telechat2/modeling_telechat2.py
+++ src/transformers/models/telechat2/modeling_telechat2.py
@@ -46,8 +46,8 @@ class TeleChat2MLP(nn.Module):
         super().__init__()
         self.hidden_size = config.hidden_size
         self.intermediate_size = config.intermediate_size
-        self.gate_proj = nn.Linear(self.hidden_size, self.intermediate_size, bias=False)
-        self.up_proj = nn.Linear(self.hidden_size, self.intermediate_size, bias=False)
+        self.gate_proj = nn.Linear(self.hidden_size, self.intermediate_size, bias=config.mlp_bias)
+        self.up_proj = nn.Linear(self.hidden_size, self.intermediate_size, bias=config.mlp_bias)
         self.down_proj = nn.Linear(self.intermediate_size, self.hidden_size, bias=True)
         self.hidden_dropout = config.hidden_dropout
 
