3a411a3fcb166096bbc52c1352b5b9df9dd0b16d
ydshieh <ydshieh@users.noreply.github.com>
2025-01-24 18:24:20 +0100

    fix: change self.attention_dropout to self.dropout

--- src/transformers/models/kosmos2_5/modeling_kosmos2_5.py
+++ src/transformers/models/kosmos2_5/modeling_kosmos2_5.py
@@ -1009,7 +1009,7 @@ class Kosmos2_5TextAttention(nn.Module):
             key_states,
             value_states,
             attention_mask,
-            dropout=0.0 if not self.training else self.attention_dropout,
+            dropout=0.0 if not self.training else self.dropout,
             scaling=self.scaling,
             **kwargs,
         )
