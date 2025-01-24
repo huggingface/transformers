cb16afb91f3daf8c94f1012de4e0ac5250e79c0d
LRL-ModelCloud <165116337+LRL-ModelCloud@users.noreply.github.com>
2025-01-24 09:25:37 +0800

    Update tokenization_utils_base.py

--- src/transformers/tokenization_utils_base.py
+++ src/transformers/tokenization_utils_base.py
@@ -2465,9 +2465,7 @@ class PreTrainedTokenizerBase(SpecialTokensMixin, PushToHubMixin):
         # Add tokenizer class to the tokenizer config to be able to reload it with from_pretrained
         tokenizer_class = self.__class__.__name__
         # Remove the Fast at the end if we can save the slow tokenizer
-        if tokenizer_class.endswith("Fast") and (
-            hasattr(self, "can_save_slow_tokenizer") and self.can_save_slow_tokenizer
-        ):
+        if tokenizer_class.endswith("Fast") and getattr(self, "can_save_slow_tokenizer", False):
             tokenizer_class = tokenizer_class[:-4]
         tokenizer_config["tokenizer_class"] = tokenizer_class
         if getattr(self, "_auto_map", None) is not None:
