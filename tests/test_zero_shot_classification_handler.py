0189b43acb80c41f786cff9cdd4b8c6f005f7d08
sambhavnoobcoder <indosambahv@gmail.com>
2025-01-25 17:19:19 +0530

    test valid hypothesis

--- tests/test_zero_shot_classification_handler.py
+++ tests/test_zero_shot_classification_handler.py
@@ -1,3 +1,13 @@
 import pytest
 from transformers.pipelines import ZeroShotClassificationArgumentHandler
 
+def test_valid_hypothesis_template():
+    handler = ZeroShotClassificationArgumentHandler()
+    sequences = ["I love this movie"]
+    labels = ["positive", "negative"]
+    template = "This movie is {}."
+    
+    # Should work fine with simple template
+    sequence_pairs, _ = handler(sequences, labels, template)
+    assert len(sequence_pairs) == 2
+    assert sequence_pairs[0][1] == "This movie is positive."
