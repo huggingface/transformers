d8ab0e8eff2a630648a2c86744399fa887762fd4
AlanPonnachan <allenktr2001@gmail.com>
2025-01-25 13:36:00 +0530

    modified init

--- src/transformers/models/aimv2/__init__.py
+++ src/transformers/models/aimv2/__init__.py
@@ -1,4 +1,4 @@
-# Copyright 2025 The HuggingFace Inc. team. All rights reserved.
+# Copyright 2025 The HuggingFace Team. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,44 +11,17 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 from typing import TYPE_CHECKING
 
-from transformers.utils import (
-    OptionalDependencyNotAvailable,
-    _LazyModule,
-    is_torch_available,
-)
+from ...utils import _LazyModule
+from ...utils.import_utils import define_import_structure
 
-
-_import_structure = {
-    "configuration_aimv2": ["AIMv2Config"],
-    "modeling_aimv2": ["AIMv2Model", "AIMv2PreTrainedModel"],
-}
-
-try:
-    if not is_torch_available():
-        raise OptionalDependencyNotAvailable()
-except OptionalDependencyNotAvailable:
-    pass
-else:
-    _import_structure["modeling_aimv2"] = [
-        "AIMv2Model",
-        "AIMv2PreTrainedModel",
-    ]
 
 if TYPE_CHECKING:
-    from .configuration_aimv2 import AIMv2Config
-
-    try:
-        if not is_torch_available():
-            raise OptionalDependencyNotAvailable()
-    except OptionalDependencyNotAvailable:
-        pass
-    else:
-        from .modeling_aimv2 import AIMv2Model, AIMv2PreTrainedModel
-
+    from .configuration_aimv2 import *
+    from .modeling_aimv2 import *
 else:
     import sys
 
-    sys.modules[__name__] = _LazyModule(__name__, globals()["__file__"], _import_structure, module_spec=__spec__)
+    _file = globals()["__file__"]
+    sys.modules[__name__] = _LazyModule(__name__, _file, define_import_structure(_file), module_spec=__spec__)
