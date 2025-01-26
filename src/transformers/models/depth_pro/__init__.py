18984594e728a5ddb877dff43ac0318c2693f5e5
geetu040 <raoarmaghanshakir040@gmail.com>
2025-01-26 16:01:14 +0500

    update __init__ to latest format

--- src/transformers/models/depth_pro/__init__.py
+++ src/transformers/models/depth_pro/__init__.py
@@ -13,60 +13,17 @@
 # limitations under the License.
 from typing import TYPE_CHECKING
 
-from ...file_utils import _LazyModule, is_torch_available, is_vision_available
-from ...utils import OptionalDependencyNotAvailable
-
-
-_import_structure = {"configuration_depth_pro": ["DepthProConfig"]}
-
-try:
-    if not is_vision_available():
-        raise OptionalDependencyNotAvailable()
-except OptionalDependencyNotAvailable:
-    pass
-else:
-    _import_structure["image_processing_depth_pro"] = ["DepthProImageProcessor"]
-    _import_structure["image_processing_depth_pro_fast"] = ["DepthProImageProcessorFast"]
-
-try:
-    if not is_torch_available():
-        raise OptionalDependencyNotAvailable()
-except OptionalDependencyNotAvailable:
-    pass
-else:
-    _import_structure["modeling_depth_pro"] = [
-        "DepthProForDepthEstimation",
-        "DepthProModel",
-        "DepthProPreTrainedModel",
-    ]
+from ...utils import _LazyModule
+from ...utils.import_utils import define_import_structure
 
 
 if TYPE_CHECKING:
-    from .configuration_depth_pro import DepthProConfig
-
-    try:
-        if not is_vision_available():
-            raise OptionalDependencyNotAvailable()
-    except OptionalDependencyNotAvailable:
-        pass
-    else:
-        from .image_processing_depth_pro import DepthProImageProcessor
-        from .image_processing_depth_pro_fast import DepthProImageProcessorFast
-
-    try:
-        if not is_torch_available():
-            raise OptionalDependencyNotAvailable()
-    except OptionalDependencyNotAvailable:
-        pass
-    else:
-        from .modeling_depth_pro import (
-            DepthProForDepthEstimation,
-            DepthProModel,
-            DepthProPreTrainedModel,
-        )
-
-
+    from .configuration_depth_pro import *
+    from .image_processing_depth_pro import *
+    from .image_processing_depth_pro_fast import *
+    from .modeling_depth_pro import *
 else:
     import sys
 
-    sys.modules[__name__] = _LazyModule(__name__, globals()["__file__"], _import_structure, module_spec=__spec__)
+    _file = globals()["__file__"]
+    sys.modules[__name__] = _LazyModule(__name__, _file, define_import_structure(_file), module_spec=__spec__)
