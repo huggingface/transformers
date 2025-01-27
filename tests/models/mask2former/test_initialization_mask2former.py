077a14c6ce6a0043c9a173ab68ff01b6b5e8eae6
Sambhav Dixit <94298612+sambhavnoobcoder@users.noreply.github.com>
2025-01-27 15:55:55 +0530

    base setup

--- tests/models/mask2former/test_initialization_mask2former.py
+++ tests/models/mask2former/test_initialization_mask2former.py
@@ -5,3 +5,9 @@ from transformers.models.mask2former.modeling_mask2former import (
     Mask2FormerMaskedAttentionDecoderLayer,
     Mask2FormerPixelDecoderEncoderMultiscaleDeformableAttention
 )
+
+class TestMask2FormerInitialization(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.config = Mask2FormerConfig()
+        cls.model = Mask2FormerModel(cls.config)
