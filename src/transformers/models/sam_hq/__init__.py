1b58321a72ff925fa7d06022dc35a1ea6408b65b
sushmanthreddy <sushmanthreddymereddy@gmail.com>
2025-01-27 19:17:36 +0530

    added the easy loading to init file

--- src/transformers/models/sam_hq/__init__.py
+++ src/transformers/models/sam_hq/__init__.py
@@ -1,71 +1,25 @@
+#Copyright 2025 The HuggingFace Team. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 from typing import TYPE_CHECKING
-
-from ...utils import (
-    OptionalDependencyNotAvailable,
-    _LazyModule,
-    is_torch_available,
-    is_vision_available,
-)
-
-
-__import_structure = {
-    "configuration_sam_hq": [
-        "SamHQConfig",
-        "SamHQMaskDecoderConfig",
-        "SamHQPromptEncoderConfig",
-        "SamHQVisionConfig",
-    ],
-    "processing_samhq": ["SamHQProcessor"],
-}
-
-
-try:
-    if not is_torch_available():
-        raise OptionalDependencyNotAvailable()
-except OptionalDependencyNotAvailable:
-    pass
-else:
-    __import_structure["modeling_sam_hq"] = [
-        "SamHQModel",
-        "SamHQPreTrainedModel",
-    ]
-
-try:
-    if not is_vision_available():
-        raise OptionalDependencyNotAvailable()
-except OptionalDependencyNotAvailable:
-    pass
-else:
-    __import_structure["image_processing_sam"] = ["SamImageProcessor"]
-
-
+from ...utils import _LazyModule
+from ...utils.import_utils import define_import_structure
 if TYPE_CHECKING:
-    from .configuration_sam_hq import (
-        SamHQConfig,
-        SamHQMaskDecoderConfig,
-        SamHQPromptEncoderConfig,
-        SamHQVisionConfig,
-    )
-    from .processing_samhq import SamHQProcessor
-
-    try:
-        if not is_torch_available():
-            raise OptionalDependencyNotAvailable()
-    except OptionalDependencyNotAvailable:
-        pass
-    else:
-        from .modeling_sam_hq import SamHQModel, SamHQPreTrainedModel
-
-    try:
-        if not is_vision_available():
-            raise OptionalDependencyNotAvailable()
-    except OptionalDependencyNotAvailable:
-        pass
-    else:
-        from transformers.models.sam.image_processing_sam import SamImageProcessor
-
-
+    from .configuration_sam_hq import *
+    from .modeling_sam_hq import *
+    from .processing_samhq import *
+    from ..sam.image_processing_sam import *
 else:
     import sys
-
-    sys.modules[__name__] = _LazyModule(__name__, globals()["__file__"], __import_structure, module_spec=__spec__)
+    _file = globals()["__file__"]
+    sys.modules[__name__] = _LazyModule(__name__, _file, define_import_structure(_file), module_spec=__spec__)
