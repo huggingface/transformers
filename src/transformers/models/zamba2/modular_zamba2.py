be7d81ac7b64519c77322f515424f2a14527a045
pglorio <paolomorphic@gmail.com>
2025-01-28 21:42:30 +0000

    set _supports_sdpa = True

--- src/transformers/models/zamba2/modular_zamba2.py
+++ src/transformers/models/zamba2/modular_zamba2.py
@@ -915,7 +915,7 @@ class Zamba2PreTrainedModel(PreTrainedModel):
     _skip_keys_device_placement = "past_key_values"
     _supports_flash_attn_2 = True
     _supports_flex_attn = True
-    _supports_sdpa = False
+    _supports_sdpa = True
     _supports_cache_class = True  # Note: only supports Zamba2HybridDynamicCache
     _is_stateful = True
 
