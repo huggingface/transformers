de6c7e7fd20f1800a3e875dcc7f83ef95a3c4c29
keetrap <parteekkamboj112@gmail.com>
2025-01-28 17:18:44 +0530

    Updated Changes

--- src/transformers/modeling_utils.py
+++ src/transformers/modeling_utils.py
@@ -3641,9 +3641,9 @@ class PreTrainedModel(nn.Module, ModuleUtilsMixin, GenerationMixin, PushToHubMix
             device_map = hf_quantizer.update_device_map(device_map)
 
             # In order to ensure popular quantization methods are supported. Can be disable with `disable_telemetry`
-            try:
+            if hasattr(hf_quantizer.quantization_config.quant_method, "value"):
                 user_agent["quant"] = hf_quantizer.quantization_config.quant_method.value
-            except Exception:
+            else:
                 user_agent["quant"] = hf_quantizer.quantization_config.quant_method
             # Force-set to `True` for more mem efficiency
             if low_cpu_mem_usage is None:
