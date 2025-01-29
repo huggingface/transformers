ac7c5e06cfabaa65674b98dd8b46073243725545
ydshieh <ydshieh@users.noreply.github.com>
2025-01-29 10:28:40 +0100

    fix

--- tests/optimization/test_optimization.py
+++ tests/optimization/test_optimization.py
@@ -59,7 +59,7 @@ def unwrap_and_save_reload_schedule(scheduler, num_steps=10):
                 file_name = os.path.join(tmpdirname, "schedule.bin")
                 torch.save(scheduler.state_dict(), file_name)
 
-                state_dict = torch.load(file_name)
+                state_dict = torch.load(file_name, weights_only=True)
                 scheduler.load_state_dict(state_dict)
     return lrs
 
