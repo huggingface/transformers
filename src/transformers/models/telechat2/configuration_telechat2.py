36fb72d85f88ff376735ded9ca229f895f140de2
shunxing12345 <747777271@qq.com>
2025-01-30 10:36:26 +0800

    fix
    Signed-off-by: shunxing12345 <747777271@qq.com>

--- src/transformers/models/telechat2/configuration_telechat2.py
+++ src/transformers/models/telechat2/configuration_telechat2.py
@@ -18,6 +18,16 @@ from ..llama.configuration_llama import LlamaConfig
 
 
 class TeleChat2Config(LlamaConfig):
+    r"""
+    This is the configuration class to store the configuration of a [`TeleChat2Model`]. It is used to instantiate an TeleChat2
+    model according to the specified arguments, defining the model architecture. Instantiating a configuration with the
+    defaults will yield a similar configuration to that of the TeleChat2-7B.
+
+    Configuration objects inherit from [`PretrainedConfig`] and can be used to control the model outputs. Read the
+    documentation from [`PretrainedConfig`] for more information.
+    """
+
+
     model_type = "telechat2"
     keys_to_ignore_at_inference = ["past_key_values"]
     # Default tensor parallel plan for base model `TeleChat2Model`
