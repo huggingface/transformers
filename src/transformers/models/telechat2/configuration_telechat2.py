289bd56330f2307b8519f7a4a96bfc0737324a82
shunxing12345 <747777271@qq.com>
2025-01-30 10:25:57 +0800

    fix
    Signed-off-by: shunxing12345 <747777271@qq.com>

--- src/transformers/models/telechat2/configuration_telechat2.py
+++ src/transformers/models/telechat2/configuration_telechat2.py
@@ -30,32 +30,5 @@ class TeleChat2Config(LlamaConfig):
         "h.*.mlp.down_proj": "rowwise",
     }
 
-    def __init__(
-        self,
-        vocab_size=32000,
-        hidden_size=4096,
-        intermediate_size=11008,
-        num_hidden_layers=32,
-        num_attention_heads=32,
-        num_key_value_heads=None,
-        hidden_act="silu",
-        max_position_embeddings=2048,
-        initializer_range=0.02,
-        rms_norm_eps=1e-6,
-        use_cache=True,
-        pad_token_id=None,
-        bos_token_id=1,
-        eos_token_id=2,
-        pretraining_tp=1,
-        tie_word_embeddings=False,
-        rope_theta=10000.0,
-        rope_scaling=None,
-        attention_dropout=0.0,
-        head_dim=None,
-        **kwargs,
-    ):
-        del self.mlp_bias
-        del self.attention_bias
-
 
 __all__ = ["TeleChat2Config"]
