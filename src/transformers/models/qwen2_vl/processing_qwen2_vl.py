0d7af3cc2e96f74ebd6e4c7743345f6a7d64e8ad
Ye Liu <yeliudev@outlook.com>
2025-02-01 00:24:21 +0800

    Use `AutoImageProcessor` instead
    Co-authored-by: Yoni Gozlan <74535834+yonigozlan@users.noreply.github.com>

--- src/transformers/models/qwen2_vl/processing_qwen2_vl.py
+++ src/transformers/models/qwen2_vl/processing_qwen2_vl.py
@@ -57,7 +57,7 @@ class Qwen2VLProcessor(ProcessorMixin):
 
     attributes = ["image_processor", "tokenizer"]
     valid_kwargs = ["chat_template"]
-    image_processor_class = ("Qwen2VLImageProcessor", "Qwen2VLImageProcessorFast")
+    image_processor_class = "AutoImageProcessor"
     tokenizer_class = ("Qwen2Tokenizer", "Qwen2TokenizerFast")
 
     def __init__(self, image_processor=None, tokenizer=None, chat_template=None, **kwargs):
