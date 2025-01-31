9646ba762d93832e543cc43c01467dbf190ca196
Ye Liu <yeliudev@outlook.com>
2025-01-31 20:12:03 +0800

    Add `Qwen2VLImageProcessorFast` into `Qwen2VLProcessor`

--- src/transformers/models/qwen2_vl/processing_qwen2_vl.py
+++ src/transformers/models/qwen2_vl/processing_qwen2_vl.py
@@ -57,7 +57,7 @@ class Qwen2VLProcessor(ProcessorMixin):
 
     attributes = ["image_processor", "tokenizer"]
     valid_kwargs = ["chat_template"]
-    image_processor_class = "Qwen2VLImageProcessor"
+    image_processor_class = ("Qwen2VLImageProcessor", "Qwen2VLImageProcessorFast")
     tokenizer_class = ("Qwen2Tokenizer", "Qwen2TokenizerFast")
 
     def __init__(self, image_processor=None, tokenizer=None, chat_template=None, **kwargs):
