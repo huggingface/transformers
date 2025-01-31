30976a855489f824655198a6445e880c254d695e
Vladislav <vladislav.bronzov@gmail.com>
2025-01-31 13:57:28 +0100

    small refact for convert script

--- src/transformers/models/d_fine/convert_d_fine_original_pytorch_checkpoint_to_hf.py
+++ src/transformers/models/d_fine/convert_d_fine_original_pytorch_checkpoint_to_hf.py
@@ -815,7 +815,7 @@ def convert_rt_detr_checkpoint(model_name, pytorch_dump_folder_path, push_to_hub
     # Pass image by the model
     outputs = model(pixel_values)
 
-    if model_name == "rtdetr_r18vd":
+    if model_name == "dfine_x_coco":
         expected_slice_logits = torch.tensor(
             [
                 [-4.3364253, -6.465683, -3.6130402],
@@ -828,111 +828,6 @@ def convert_rt_detr_checkpoint(model_name, pytorch_dump_folder_path, push_to_hub
                 [0.16868353, 0.19833282, 0.21182671],
                 [0.25559652, 0.55121744, 0.47988364],
                 [0.7698693, 0.4124569, 0.46036878],
-            ]
-        )
-    elif model_name == "rtdetr_r34vd":
-        expected_slice_logits = torch.tensor(
-            [
-                [-4.3727384, -4.7921476, -5.7299604],
-                [-4.840536, -8.455345, -4.1745796],
-                [-4.1277084, -5.2154565, -5.7852697],
-            ]
-        )
-        expected_slice_boxes = torch.tensor(
-            [
-                [0.258278, 0.5497808, 0.4732004],
-                [0.16889669, 0.19890057, 0.21138911],
-                [0.76632994, 0.4147879, 0.46851268],
-            ]
-        )
-    elif model_name == "rtdetr_r50vd_m":
-        expected_slice_logits = torch.tensor(
-            [
-                [-4.319764, -6.1349025, -6.094794],
-                [-5.1056995, -7.744766, -4.803956],
-                [-4.7685347, -7.9278393, -4.5751696],
-            ]
-        )
-        expected_slice_boxes = torch.tensor(
-            [
-                [0.2582739, 0.55071366, 0.47660282],
-                [0.16811174, 0.19954777, 0.21292639],
-                [0.54986024, 0.2752091, 0.0561416],
-            ]
-        )
-    elif model_name == "rtdetr_r50vd":
-        expected_slice_logits = torch.tensor(
-            [
-                [-4.6476398, -5.001154, -4.9785104],
-                [-4.1593494, -4.7038546, -5.946485],
-                [-4.4374595, -4.658361, -6.2352347],
-            ]
-        )
-        expected_slice_boxes = torch.tensor(
-            [
-                [0.16880608, 0.19992264, 0.21225442],
-                [0.76837635, 0.4122631, 0.46368608],
-                [0.2595386, 0.5483334, 0.4777486],
-            ]
-        )
-    elif model_name == "rtdetr_r101vd":
-        expected_slice_logits = torch.tensor(
-            [
-                [-4.6162, -4.9189, -4.6656],
-                [-4.4701, -4.4997, -4.9659],
-                [-5.6641, -7.9000, -5.0725],
-            ]
-        )
-        expected_slice_boxes = torch.tensor(
-            [
-                [0.7707, 0.4124, 0.4585],
-                [0.2589, 0.5492, 0.4735],
-                [0.1688, 0.1993, 0.2108],
-            ]
-        )
-    elif model_name == "rtdetr_r18vd_coco_o365":
-        expected_slice_logits = torch.tensor(
-            [
-                [-4.8726, -5.9066, -5.2450],
-                [-4.8157, -6.8764, -5.1656],
-                [-4.7492, -5.7006, -5.1333],
-            ]
-        )
-        expected_slice_boxes = torch.tensor(
-            [
-                [0.2552, 0.5501, 0.4773],
-                [0.1685, 0.1986, 0.2104],
-                [0.7692, 0.4141, 0.4620],
-            ]
-        )
-    elif model_name == "rtdetr_r50vd_coco_o365":
-        expected_slice_logits = torch.tensor(
-            [
-                [-4.6491, -3.9252, -5.3163],
-                [-4.1386, -5.0348, -3.9016],
-                [-4.4778, -4.5423, -5.7356],
-            ]
-        )
-        expected_slice_boxes = torch.tensor(
-            [
-                [0.2583, 0.5492, 0.4747],
-                [0.5501, 0.2754, 0.0574],
-                [0.7693, 0.4137, 0.4613],
-            ]
-        )
-    elif model_name == "rtdetr_r101vd_coco_o365":
-        expected_slice_logits = torch.tensor(
-            [
-                [-4.5152, -5.6811, -5.7311],
-                [-4.5358, -7.2422, -5.0941],
-                [-4.6919, -5.5834, -6.0145],
-            ]
-        )
-        expected_slice_boxes = torch.tensor(
-            [
-                [0.7703, 0.4140, 0.4583],
-                [0.1686, 0.1991, 0.2107],
-                [0.2570, 0.5496, 0.4750],
             ]
         )
     else:
