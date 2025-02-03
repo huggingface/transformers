cc9c0b759a422b785091331a1a1fc7fddc671b8e
Haotong LIN <haotongl@outlook.com>
2025-02-03 23:26:30 +0800

    Update src/transformers/models/prompt_depth_anything/modular_prompt_depth_anything.py
    Co-authored-by: Pavel Iakubovskii <qubvel@gmail.com>

--- src/transformers/models/prompt_depth_anything/modular_prompt_depth_anything.py
+++ src/transformers/models/prompt_depth_anything/modular_prompt_depth_anything.py
@@ -277,10 +277,10 @@ class PromptDepthAnythingForDepthEstimation(DepthAnythingForDepthEstimation):
     def forward(
         self,
         pixel_values: torch.FloatTensor,
+        prompt_depth: Optional[torch.FloatTensor] = None,
         labels: Optional[torch.LongTensor] = None,
         output_attentions: Optional[bool] = None,
         output_hidden_states: Optional[bool] = None,
-        prompt_depth: Optional[torch.FloatTensor] = None,
         return_dict: Optional[bool] = None,
     ) -> Union[Tuple[torch.Tensor], DepthEstimatorOutput]:
         r"""
