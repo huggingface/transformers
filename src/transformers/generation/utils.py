1097d4b3f8dd6440add50f87a2784b03cc0b4c85
Joao Gante <joao@huggingface.co>
2025-02-04 18:05:32 +0000

    retrive vocab size correctly

--- src/transformers/generation/utils.py
+++ src/transformers/generation/utils.py
@@ -3442,7 +3442,7 @@ class GenerationMixin:
 
         batch_size_unflattened, cur_len = input_ids.shape
         batch_size = batch_size_unflattened // num_beams
-        vocab_size = self.config.vocab_size
+        vocab_size = self.config.get_text_config().vocab_size
         decoder_prompt_len = cur_len
         this_peer_finished = False
 
