0c0f2ef191b0030bb9cc3070964501b8e66454e9
Arthur Zucker <arthur.zucker@gmail.com>
2025-02-04 09:04:57 +0100

    nits

--- examples/training/distributed_training.py
+++ examples/training/distributed_training.py
@@ -6,39 +6,42 @@ import torch.distributed as dist
 
 
 # Environment variables set by torch.distributed.launch
-LOCAL_RANK = int(os.environ['LOCAL_RANK'])
-WORLD_SIZE = int(os.environ['WORLD_SIZE'])
-WORLD_RANK = int(os.environ['RANK'])
+LOCAL_RANK = int(os.environ["LOCAL_RANK"])
+WORLD_SIZE = int(os.environ["WORLD_SIZE"])
+WORLD_RANK = int(os.environ["RANK"])
 
-LOCAL_RANK = int(os.environ['OMPI_COMM_WORLD_LOCAL_RANK'])
-WORLD_SIZE = int(os.environ['OMPI_COMM_WORLD_SIZE'])
-WORLD_RANK = int(os.environ['OMPI_COMM_WORLD_RANK'])
+LOCAL_RANK = int(os.environ["OMPI_COMM_WORLD_LOCAL_RANK"])
+WORLD_SIZE = int(os.environ["OMPI_COMM_WORLD_SIZE"])
+WORLD_RANK = int(os.environ["OMPI_COMM_WORLD_RANK"])
 
 
 def run(backend):
     tensor = torch.zeros(1)
     # Need to put tensor on a GPU device for nccl backend
-    if backend == 'nccl':
+    if backend == "nccl":
         device = torch.device("cuda:{}".format(LOCAL_RANK))
         tensor = tensor.to(device)
 
     if WORLD_RANK == 0:
         for rank_recv in range(1, WORLD_SIZE):
             dist.send(tensor=tensor, dst=rank_recv)
-            print('worker_{} sent data to Rank {}\n'.format(0, rank_recv))
+            print("worker_{} sent data to Rank {}\n".format(0, rank_recv))
     else:
         dist.recv(tensor=tensor, src=0)
-        print('worker_{} has received data from rank {}\n'.format(WORLD_RANK, 0))
+        print("worker_{} has received data from rank {}\n".format(WORLD_RANK, 0))
+
 
 def init_processes(backend):
     dist.init_process_group(backend, rank=WORLD_RANK, world_size=WORLD_SIZE)
     run(backend)
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--local_rank", type=int, help="Local rank. Necessary for using the torch.distributed.launch utility.")
-    parser.add_argument("--backend", type=str, default="nccl", choices=['nccl', 'gloo'])
+    parser.add_argument(
+        "--local_rank", type=int, help="Local rank. Necessary for using the torch.distributed.launch utility."
+    )
+    parser.add_argument("--backend", type=str, default="nccl", choices=["nccl", "gloo"])
     args = parser.parse_args()
 
     init_processes(backend=args.backend)
