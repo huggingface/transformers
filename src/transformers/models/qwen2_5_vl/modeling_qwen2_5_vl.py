73ec7d61302c58bbc56f5348382d3f57bf8a1146
raushan <raushan@huggingface.co>
2025-02-05 16:29:21 +0100

    fix copies

--- src/transformers/models/qwen2_5_vl/modeling_qwen2_5_vl.py
+++ src/transformers/models/qwen2_5_vl/modeling_qwen2_5_vl.py
@@ -1261,7 +1261,7 @@ class Qwen2_5_VLModel(Qwen2_5_VLPreTrainedModel):
         if (
             self.config._attn_implementation == "sdpa"
             and attention_mask is not None
-            and attention_mask.device.type == "cuda"
+            and attention_mask.device.type in ["cuda", "xpu"]
             and not output_attentions
         ):
             # Attend to all tokens in fully masked rows in the causal_mask, for example the relevant first rows when
