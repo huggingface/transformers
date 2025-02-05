4d8f927a86ce6ad4fcde12daeb88ec7052d2749a
geetu040 <raoarmaghanshakir040@gmail.com>
2025-02-05 13:38:15 +0500

    ruff formatting

--- tests/models/depth_pro/test_image_processing_depth_pro.py
+++ tests/models/depth_pro/test_image_processing_depth_pro.py
@@ -16,7 +16,7 @@
 
 import unittest
 
-from transformers.testing_utils import is_flaky, require_torch, require_vision
+from transformers.testing_utils import require_torch, require_vision
 from transformers.utils import is_torchvision_available, is_vision_available
 
 from ...test_image_processing_common import ImageProcessingTestMixin, prepare_image_inputs
