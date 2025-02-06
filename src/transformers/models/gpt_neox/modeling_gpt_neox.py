376ae30c800bc578fd9ba0d3c8622d880af546f2
Cyril Vallez <cyril.vallez@gmail.com>
2025-02-06 11:30:16 +0100

    simplify at the same time

--- src/transformers/models/gpt_neox/modeling_gpt_neox.py
+++ src/transformers/models/gpt_neox/modeling_gpt_neox.py
@@ -927,11 +927,7 @@ class GPTNeoXForSequenceClassification(GPTNeoXPreTrainedModel):
         hidden_states = outputs[0]
         logits = self.score(hidden_states)
 
-        if input_ids is not None:
-            batch_size = input_ids.shape[0]
-        else:
-            batch_size = inputs_embeds.shape[0]
-
+        batch_size = logits.shape[0]
         if self.config.pad_token_id is None and batch_size != 1:
             raise ValueError("Cannot handle batch sizes > 1 if no padding token is defined.")
         if self.config.pad_token_id is None:
