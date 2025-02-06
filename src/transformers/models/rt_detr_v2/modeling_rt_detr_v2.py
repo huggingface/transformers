ade4e3f4eb111bfc4c645e99444db1817dc0df10
qubvel <qubvel@gmail.com>
2025-02-06 18:09:50 +0000

    Fix RT-DETR loss

--- src/transformers/models/rt_detr_v2/modeling_rt_detr_v2.py
+++ src/transformers/models/rt_detr_v2/modeling_rt_detr_v2.py
@@ -2071,9 +2071,8 @@ class RTDetrV2ForObjectDetection(RTDetrV2PreTrainedModel):
 
         loss, loss_dict, auxiliary_outputs, enc_topk_logits, enc_topk_bboxes = None, None, None, None, None
         if labels is not None:
-            if self.training and denoising_meta_values is not None:
-                enc_topk_logits = outputs.enc_topk_logits if return_dict else outputs[-5]
-                enc_topk_bboxes = outputs.enc_topk_bboxes if return_dict else outputs[-4]
+            enc_topk_logits = outputs.enc_topk_logits if return_dict else outputs[-5]
+            enc_topk_bboxes = outputs.enc_topk_bboxes if return_dict else outputs[-4]
             loss, loss_dict, auxiliary_outputs = self.loss_function(
                 logits,
                 labels,
