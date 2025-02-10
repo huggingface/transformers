81c11859be51e28cc387b1f9af52cdaacbe916c7
Harry Mellor <19981378+hmellor@users.noreply.github.com>
2025-02-10 15:47:03 +0100

    Add pp plan for Qwen 2, 2 MoE, 2 VL and 2.5 VL
    Signed-off-by: Harry Mellor <19981378+hmellor@users.noreply.github.com>

--- src/transformers/models/qwen2/modeling_qwen2.py
+++ src/transformers/models/qwen2/modeling_qwen2.py
@@ -735,6 +735,7 @@ class KwargsForCausalLM(FlashAttentionKwargs, LossKwargs): ...
 class Qwen2ForCausalLM(Qwen2PreTrainedModel, GenerationMixin):
     _tied_weights_keys = ["lm_head.weight"]
     _tp_plan = {"lm_head": "colwise_rep"}
+    _pp_plan = {"lm_head": [["hidden_states"], ["logits"]]}
 
     def __init__(self, config):
         super().__init__(config)
