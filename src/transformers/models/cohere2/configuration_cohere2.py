57a2aa57f56d8e05d8c469019199bef21675c3a3
Harry Mellor <19981378+hmellor@users.noreply.github.com>
2025-02-10 15:10:26 +0100

    Add pp plan to Cohere 1 & 2
    Signed-off-by: Harry Mellor <19981378+hmellor@users.noreply.github.com>

--- src/transformers/models/cohere2/configuration_cohere2.py
+++ src/transformers/models/cohere2/configuration_cohere2.py
@@ -148,6 +148,11 @@ class Cohere2Config(PretrainedConfig):
         "layers.*.mlp.up_proj": "colwise",
         "layers.*.mlp.down_proj": "rowwise",
     }
+    base_model_pp_plan = {
+        "embed_tokens": [["input_ids"], ["inputs_embeds"]],
+        "layers": [["hidden_states", "attention_mask"], ["hidden_states"]],
+        "norm": [["hidden_states"], ["hidden_states"]],
+    }
 
     def __init__(
         self,
