eea68ad0431f6d1c5d4e9a91d876bca2bbe3c9ee
MekkCyber <mekk.cyber@gmail.com>
2025-02-11 08:29:34 +0000

    update

--- src/transformers/utils/quantization_config.py
+++ src/transformers/utils/quantization_config.py
@@ -1561,7 +1561,7 @@ class FineGrainedFP8Config(QuantizationConfigMixin):
             A list of module names that should not be converted during quantization.
         activation_scheme (`str`, defaults to `"dynamic"`):
             The scheme used for activation, the defaults and only support scheme for now is "dynamic".
-        weight_block_size (`typing.Tuple[int, int]`, default to `(128, 128)`):
+        weight_block_size (`typing.Tuple[int, int]`, defaults to `(128, 128)`):
             The size of the weight blocks for quantization, default is (128, 128).
     """
 
