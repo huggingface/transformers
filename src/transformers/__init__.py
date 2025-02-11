340f87d5c9f8b707680041d41310d4e0684e32a3
Matt <rocketknight1@gmail.com>
2025-02-11 15:44:16 +0000

    Test commit to fix import structure

--- src/transformers/__init__.py
+++ src/transformers/__init__.py
@@ -697,7 +697,6 @@ _import_structure = {
     "models.phi3": ["Phi3Config"],
     "models.phi3_5": [
         "Phi3VConfig",
-        "Phi3VProcessor",
     ],
     "models.phimoe": ["PhimoeConfig"],
     "models.phobert": ["PhobertTokenizer"],
@@ -1289,7 +1288,7 @@ else:
     _import_structure["models.owlv2"].append("Owlv2ImageProcessor")
     _import_structure["models.owlvit"].extend(["OwlViTFeatureExtractor", "OwlViTImageProcessor"])
     _import_structure["models.perceiver"].extend(["PerceiverFeatureExtractor", "PerceiverImageProcessor"])
-    _import_structure["models.phi3_5"].extend(["Phi3VImageProcessor"])
+    _import_structure["models.phi3_5"].extend(["Phi3VImageProcessor", "Phi3VProcessor"])
     _import_structure["models.pix2struct"].extend(["Pix2StructImageProcessor"])
     _import_structure["models.pixtral"].append("PixtralImageProcessor")
     _import_structure["models.poolformer"].extend(["PoolFormerFeatureExtractor", "PoolFormerImageProcessor"])
@@ -5884,7 +5883,6 @@ if TYPE_CHECKING:
     from .models.phi3 import Phi3Config
     from .models.phi3_5 import (
         Phi3VConfig,
-        Phi3VProcessor,
     )
     from .models.phimoe import PhimoeConfig
     from .models.phobert import PhobertTokenizer
@@ -6501,7 +6499,7 @@ if TYPE_CHECKING:
         from .models.owlv2 import Owlv2ImageProcessor
         from .models.owlvit import OwlViTFeatureExtractor, OwlViTImageProcessor
         from .models.perceiver import PerceiverFeatureExtractor, PerceiverImageProcessor
-        from .models.phi3_5 import Phi3VImageProcessor
+        from .models.phi3_5 import Phi3VImageProcessor, Phi3VProcessor
         from .models.pix2struct import Pix2StructImageProcessor
         from .models.pixtral import PixtralImageProcessor
         from .models.poolformer import (
