4b4a083d5d56482127b2e0922e9057c82b1b1f43
Arthur Zucker <arthur.zucker@gmail.com>
2025-02-12 12:51:19 +0100

    nits

--- src/transformers/models/data2vec/modeling_data2vec_vision.py
+++ src/transformers/models/data2vec/modeling_data2vec_vision.py
@@ -284,7 +284,7 @@ class Data2VecVisionSelfAttention(nn.Module):
         self.config = config
         if config.hidden_size % config.num_attention_heads != 0 and not hasattr(config, "embedding_size"):
             raise ValueError(
-                f"The hidden size {config.hidden_size,} is not a multiple of the number of attention "
+                f"The hidden size {(config.hidden_size,)} is not a multiple of the number of attention "
                 f"heads {config.num_attention_heads}."
             )
 
