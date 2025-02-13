5a933d06594377c5aabc24d33813676147905581
isotr0py <2037008807@qq.com>
2025-02-13 19:45:10 +0800

    revert qwen2vl fast image processor
    Signed-off-by: isotr0py <2037008807@qq.com>

--- src/transformers/models/qwen2_vl/image_processing_qwen2_vl_fast.py
+++ src/transformers/models/qwen2_vl/image_processing_qwen2_vl_fast.py
@@ -19,7 +19,6 @@
 # limitations under the License.
 """Fast Image processor class for Qwen2-VL."""
 
-import math
 from typing import Dict, List, Optional, Union
 
 from ...image_processing_utils import BatchFeature
@@ -53,6 +52,7 @@ from ...utils import (
     is_vision_available,
     logging,
 )
+from .image_processing_qwen2_vl import smart_resize
 
 
 if is_torch_available():
@@ -67,37 +67,6 @@ elif is_torchvision_available():
     from torchvision.transforms import functional as F
 
 logger = logging.get_logger(__name__)
-
-
-def smart_resize(
-    height: int, width: int, factor: int = 28, min_pixels: int = 56 * 56, max_pixels: int = 14 * 14 * 4 * 1280
-):
-    """Rescales the image so that the following conditions are met:
-
-    1. Both dimensions (height and width) are divisible by 'factor'.
-
-    2. The total number of pixels is within the range ['min_pixels', 'max_pixels'].
-
-    3. The aspect ratio of the image is maintained as closely as possible.
-
-    """
-    if height < factor or width < factor:
-        raise ValueError(f"height:{height} or width:{width} must be larger than factor:{factor}")
-    elif max(height, width) / min(height, width) > 200:
-        raise ValueError(
-            f"absolute aspect ratio must be smaller than 200, got {max(height, width) / min(height, width)}"
-        )
-    h_bar = round(height / factor) * factor
-    w_bar = round(width / factor) * factor
-    if h_bar * w_bar > max_pixels:
-        beta = math.sqrt((height * width) / max_pixels)
-        h_bar = math.floor(height / beta / factor) * factor
-        w_bar = math.floor(width / beta / factor) * factor
-    elif h_bar * w_bar < min_pixels:
-        beta = math.sqrt(min_pixels / (height * width))
-        h_bar = math.ceil(height * beta / factor) * factor
-        w_bar = math.ceil(width * beta / factor) * factor
-    return h_bar, w_bar
 
 
 class Qwen2VLFastImageProcessorInitKwargs(DefaultFastImageProcessorInitKwargs):
