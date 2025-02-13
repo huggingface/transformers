6630d6248662126bd163c947eec3548649001731
Matt <rocketknight1@gmail.com>
2025-02-13 18:41:45 +0000

    Fix modular conversion CI

--- utils/modular_model_converter.py
+++ utils/modular_model_converter.py
@@ -12,6 +12,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+from __future__ import annotations
+
 import argparse
 import glob
 import importlib
