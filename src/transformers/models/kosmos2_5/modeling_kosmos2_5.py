6c320a6f9dead5752ce427170723102ff6053279
ydshieh <ydshieh@users.noreply.github.com>
2025-02-13 11:23:04 +0100

    fix copie

--- src/transformers/models/kosmos2_5/modeling_kosmos2_5.py
+++ src/transformers/models/kosmos2_5/modeling_kosmos2_5.py
@@ -1046,7 +1046,7 @@ class Kosmos2_5TextTransformer(nn.Module):
         if (
             self.config._attn_implementation == "sdpa"
             and attention_mask is not None
-            and attention_mask.device.type == "cuda"
+            and attention_mask.device.type in ["cuda", "xpu"]
             and not output_attentions
         ):
             # Attend to all tokens in fully masked rows in the causal_mask, for example the relevant first rows when
