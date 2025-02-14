fe927130005725d414dce55dcae71e08645e7830
xiuqhou <xiuqhou@outlook.com>
2025-02-14 10:07:17 +0800

    Fix `Copied` in test_image_processing_relation_detr

--- tests/models/relation_detr/test_image_processing_relation_detr.py
+++ tests/models/relation_detr/test_image_processing_relation_detr.py
@@ -206,7 +206,7 @@ class RelationDetrImageProcessingTest(ImageProcessingTestMixin, unittest.TestCas
             self.assertTrue(torch.allclose(encoding["labels"][0]["size"], expected_size))
 
     @slow
-    # Copied from tests.models.detr.test_image_processing_detr.DetrImageProcessingTest.test_batched_coco_detection_annotations with Detr->RelationDetr
+    # Modified from tests.models.detr.test_image_processing_detr.DetrImageProcessingTest.test_batched_coco_detection_annotations with Detr->RelationDetr
     def test_batched_coco_detection_annotations(self):
         image_0 = Image.open("./tests/fixtures/tests_samples/COCO/000000039769.png")
         image_1 = Image.open("./tests/fixtures/tests_samples/COCO/000000039769.png").resize((800, 800))
