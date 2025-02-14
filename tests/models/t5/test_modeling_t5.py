6c6416fa66438a678e7bdf9ce5cc932bc587eb4e
Raushan Turganbay <raushan@huggingface.co>
2025-02-14 15:23:49 +0100

    🔴 VLM: compile compatibility (#35724)
    * llavas
    
    * add mroe models
    
    * fix `compile_forward` test for all models
    
    * fix copies
    
    * make style
    
    * also doesn't support cache class
    
    * fix some tests
    
    * not copied from
    
    * ci green?
    
    * fix tests
    
    * fix copies
    
    * fix tests
    
    * check with `numel` and remove `item`
    
    * fix copies
    
    * fix copies
    
    * Update src/transformers/models/cohere2/modeling_cohere2.py
    
    Co-authored-by: Arthur <48595927+ArthurZucker@users.noreply.github.com>
    
    * opt remove cross attn
    
    * gemma2
    
    * fixup
    
    * fixup
    
    * fix newly added test
    
    * maybe fixed?
    
    * green please?
    
    ---------
    
    Co-authored-by: Arthur <48595927+ArthurZucker@users.noreply.github.com>

--- tests/models/t5/test_modeling_t5.py
+++ tests/models/t5/test_modeling_t5.py
@@ -545,7 +545,6 @@ class T5ModelTester:
             "attention_mask": attention_mask,
             "decoder_input_ids": decoder_input_ids,
             "decoder_attention_mask": decoder_attention_mask,
-            "use_cache": False,
         }
         return config, inputs_dict
 
