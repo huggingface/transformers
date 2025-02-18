3425edcbdb97e25831332972dd8489783f61e734
Vladislav <vladislav.bronzov@gmail.com>
2025-02-18 14:52:24 +0100

    remove unused import

--- src/transformers/__init__.py
+++ src/transformers/__init__.py
@@ -403,7 +403,7 @@ _import_structure = {
     "models.depth_anything": ["DepthAnythingConfig"],
     "models.depth_pro": ["DepthProConfig"],
     "models.detr": ["DetrConfig"],
-    "models.d_fine": ["DFineConfig", "DFineResNetConfig", "DFineResNetStageConfig"],
+    "models.d_fine": ["DFineConfig", "DFineResNetConfig"],
     "models.dialogpt": [],
     "models.diffllama": ["DiffLlamaConfig"],
     "models.dinat": ["DinatConfig"],
@@ -5439,7 +5439,7 @@ if TYPE_CHECKING:
         CTRLTokenizer,
     )
     from .models.cvt import CvtConfig
-    from .models.d_fine import DFineConfig, DFineResNetConfig, DFineResNetStageConfig
+    from .models.d_fine import DFineConfig, DFineResNetConfig
     from .models.dab_detr import (
         DabDetrConfig,
     )
