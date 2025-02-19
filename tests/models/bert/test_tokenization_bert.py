19433278bdf248f1084f0f531533c7760b308e2f
KimBomm <springnode@gmail.com>
2025-02-19 20:02:19 +0900

    Update test_tokenization_bert.py

--- tests/models/bert/test_tokenization_bert.py
+++ tests/models/bert/test_tokenization_bert.py
@@ -206,7 +206,7 @@ class BertTokenizationTest(TokenizerTesterMixin, unittest.TestCase):
 
     def test_bidirectional_wordpiece_tokenizer(self):
         vocab_tokens = ["[UNK]", "[CLS]", "[SEP]", "want", "##want", "##ed", "wa", "un", "runn", "##ing"]
-        
+
         vocab = {}
         for i, token in enumerate(vocab_tokens):
             vocab[token] = i
@@ -214,9 +214,9 @@ class BertTokenizationTest(TokenizerTesterMixin, unittest.TestCase):
         tokenizer = BidirectionalWordpieceTokenizer(vocab=vocab, unk_token="[UNK]")
 
         self.assertListEqual(tokenizer.tokenize(""), [])
-        
+
         self.assertListEqual(tokenizer.tokenize("unwanted running"), ["un", "##want", "##ed", "runn", "##ing"])
-        
+
         self.assertListEqual(tokenizer.tokenize("unwantedX running"), ["[UNK]", "runn", "##ing"])
 
     def test_is_whitespace(self):
