9c1c2134b5be65ad4fbdf565fc782e68e347387b
Rohan Potdar <66227218+Rohan138@users.noreply.github.com>
2025-02-19 13:47:55 -0600

    Remove ORT from transformers (#66)
    * Tear out ort completely

--- src/transformers/trainer.py
+++ src/transformers/trainer.py
@@ -1916,12 +1916,7 @@ class Trainer:
 
         # train/eval could be run multiple-times - if already wrapped, don't re-wrap it again
         if self.accelerator.unwrap_model(model) is not model:
-            if self.args.ort:
-                from torch_ort import ORTModule
-                if type(model) is not ORTModule:
-                    return model
-            else:
-                return model
+            return model
 
         # Mixed precision training with apex (torch < 1.6)
         if self.use_apex and training:
