ec4fa2d18fc8d06f4e3132d78e79d001c05dee45
jadechoghari <chogharijade@gmail.com>
2025-02-21 16:14:31 +0100

    update copyright

--- src/transformers/loss/loss_fast.py
+++ src/transformers/loss/loss_fast.py
@@ -1,4 +1,5 @@
-# Copyright 2025 The HuggingFace Team. All rights reserved.
+# coding=utf-8
+# Copyright 2025 the Fast authors and HuggingFace Inc. team.  All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
