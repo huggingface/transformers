8d32560bb7cb8e730c887a46550b547409109206
geetu040 <raoarmaghanshakir040@gmail.com>
2025-02-24 09:30:29 +0500

    cleaning

--- src/transformers/models/deepseek_vl/configuration_deepseek_vl.py
+++ src/transformers/models/deepseek_vl/configuration_deepseek_vl.py
@@ -16,26 +16,30 @@
 
 from ...configuration_utils import PretrainedConfig
 from ...utils import logging
-from ..sam.configuration_sam import SamVisionConfig
-from ..siglip.configuration_siglip import SiglipVisionConfig
-from ..llama.configuration_llama import LlamaConfig
+from ..sam import SamVisionConfig
+from ..siglip import SiglipVisionConfig
+from ..llama import LlamaConfig
 
 
 logger = logging.get_logger(__name__)
 
 
 class DeepseekVLVisionConfig(PretrainedConfig):
+    r"""
+    This class handles the configuration for the vision component of the DeepseekVL model.
+    Instantiating a configuration with the defaults will yield a similar configuration to that of the model of the DeepseekVL
+    [geetu040/deepseek-vl-7b-chat](https://huggingface.co/geetu040/deepseek-vl-7b-chat) architecture.
+    This class wraps the 2 vision model configurations: `SiglipVisionConfig` and `SamVisionConfig`.
+
+    Args:
+
+    """
+
     model_type = "deepseek_vl_vision_model"
     base_config_key = "vision_config"
     sub_configs = {"low_res_config": SiglipVisionConfig, "high_res_config": SamVisionConfig}
 
-    def __init__(
-            self,
-            use_high_res=False,
-            low_res_config=None,
-            high_res_config=None,
-            **kwargs
-        ):
+    def __init__(self, use_high_res=False, low_res_config=None, high_res_config=None, **kwargs):
         super().__init__(**kwargs)
 
         if low_res_config is None:
@@ -52,6 +56,32 @@ class DeepseekVLVisionConfig(PretrainedConfig):
 
 
 class DeepseekVLConfig(PretrainedConfig):
+    r"""
+    This is the configuration class to store the configuration of a [`DeepseekVLModel`]. It is used to instantiate a
+    DeepseekVL model according to the specified arguments, defining the model architecture. Instantiating a configuration
+    with the defaults will yield a similar configuration to that of the DeepseekVL
+    [geetu040/deepseek-vl-7b-chat](https://huggingface.co/geetu040/deepseek-vl-7b-chat) architecture.
+
+    Configuration objects inherit from [`PretrainedConfig`] and can be used to control the model outputs. Read the
+    documentation from [`PretrainedConfig`] for more information.
+
+    Args:
+
+    Example:
+
+    ```python
+    >>> from transformers import DeepseekVLConfig, DeepseekVLModel
+
+    >>> # Initializing a DeepseekVL geetu040/deepseek-vl-7b-chat style configuration
+    >>> configuration = DeepseekVLConfig()
+
+    >>> # Initializing a model (with random weights) from the geetu040/deepseek-vl-7b-chat style configuration
+    >>> model = DeepseekVLModel(configuration)
+
+    >>> # Accessing the model configuration
+    >>> configuration = model.config
+    ```"""
+
     model_type = "deepseek_vl"
     sub_configs = {
         "text_config": LlamaConfig,
@@ -75,7 +105,7 @@ class DeepseekVLConfig(PretrainedConfig):
     @classmethod
     def from_text_vision_configs(cls, text_config: LlamaConfig, vision_config: DeepseekVLVisionConfig, **kwargs):
         r"""
-        Instantiate a [`SiglipConfig`] (or a derived class) from siglip text model configuration and siglip vision
+        Instantiate a [`DeepseekVLConfig`] from llama text model configuration and DeepseekVLVisionModel
         model configuration.
 
         Returns:
