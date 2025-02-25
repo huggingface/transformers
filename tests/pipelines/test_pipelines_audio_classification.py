a37156e603ed2c338abbf99c22d94f675bfc55a9
Yih-Dar <2521628+ydshieh@users.noreply.github.com>
2025-02-25 14:33:30 +0100

    Update tests/pipelines/test_pipelines_audio_classification.py

--- tests/pipelines/test_pipelines_audio_classification.py
+++ tests/pipelines/test_pipelines_audio_classification.py
@@ -145,7 +145,7 @@ class AudioClassificationPipelineTests(unittest.TestCase):
         output = audio_classifier(audio, top_k=4)
 
         # Expected outputs are collected running the test on torch 2.6 in few scenarios.
-        # Running on CUDA A100 and on XPU PVC (note: using stock torch xpu, NOT using IPEX):
+        # Running on CUDA T4/A100 and on XPU PVC (note: using stock torch xpu, NOT using IPEX):
         EXPECTED_OUTPUT = [
             {"score": 0.0833, "label": "go"},
             {"score": 0.0833, "label": "off"},
