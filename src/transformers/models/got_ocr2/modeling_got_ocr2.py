f286eb467265693af3239ce7437e3270290b09f1
Cyril Vallez <cyril.vallez@huggingface.co>
2025-02-25 11:09:01 +0100

    Fix doc formatting in forward passes & modular (#36243)
    * fix indentation issues + modular without magic keyword
    
    * style
    
    * Update doc.py
    
    * style
    
    * Fix all decorators indentation
    
    * all models
    
    * style
    
    * style
    
    * Update doc.py
    
    * fix
    
    * general fix
    
    * style

--- src/transformers/models/got_ocr2/modeling_got_ocr2.py
+++ src/transformers/models/got_ocr2/modeling_got_ocr2.py
@@ -769,7 +769,6 @@ class GotOcr2ForConditionalGeneration(GotOcr2PreTrainedModel, GenerationMixin):
         logits_to_keep: Union[int, torch.Tensor] = 0,
     ) -> Union[Tuple, GotOcr2CausalLMOutputWithPast]:
         r"""
-        Args:
             labels (`torch.LongTensor` of shape `(batch_size, sequence_length)`, *optional*):
                 Labels for computing the masked language modeling loss. Indices should either be in `[0, ...,
                 config.vocab_size]` or -100 (see `input_ids` docstring). Tokens with indices set to `-100` are ignored
