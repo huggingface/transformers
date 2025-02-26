e846b1c595a3c57b7d6d38ed3815339af5b8b7d0
Sung Ching Liu <sunny19981005@outlook.com>
2025-02-26 14:05:40 -0500

    fix bugs in gathering state dict

--- src/transformers/modeling_utils.py
+++ src/transformers/modeling_utils.py
@@ -3667,7 +3667,8 @@ class PreTrainedModel(nn.Module, ModuleUtilsMixin, PushToHubMixin, PeftAdapterMi
         for key, value in state_dict.items():
             if hasattr(value, "_local_tensor"):
                 gathered_state_dict[key] = value.to_local()
-            gathered_state_dict[key] = value
+            else:
+                gathered_state_dict[key] = value
 
         del state_dict
         state_dict = gathered_state_dict
