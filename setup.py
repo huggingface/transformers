8889da38664d484692c4f4988da426457ace56eb
ydshieh <ydshieh@users.noreply.github.com>
2025-02-27 15:07:06 +0100

    trigger

--- setup.py
+++ setup.py
@@ -74,7 +74,7 @@ from pathlib import Path
 
 from setuptools import Command, find_packages, setup
 
-a = 2
+a = 3
 
 # Remove stale transformers.egg-info directory to avoid https://github.com/pypa/pip/issues/5466
 stale_egg_info = Path(__file__).parent / "transformers.egg-info"
