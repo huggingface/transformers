fix wrong shape of qk norm: megatron uses head_dim

src/transformers/models/swissai/modeling_swissai.py
@@ -161,8 +161,8 @@ class SwissAIAttention(nn.Module):
             config.num_attention_heads * self.head_dim, config.hidden_size, bias=config.attention_bias
         )
         if self.config.qk_norm:
-            self.q_norm = SwissAIRMSNorm(config.num_attention_heads * self.head_dim, config.rms_norm_eps)
-            self.k_norm = SwissAIRMSNorm(config.num_key_value_heads * self.head_dim, config.rms_norm_eps)
+            self.q_norm = SwissAIRMSNorm(self.head_dim, config.rms_norm_eps)
+            self.k_norm = SwissAIRMSNorm(self.head_dim, config.rms_norm_eps)
         else:
             self.q_norm = nn.Identity()
             self.k_norm = nn.Identity()
@@ -179,12 +179,14 @@ class SwissAIAttention(nn.Module):
         input_shape = hidden_states.shape[:-1]
         hidden_shape = (*input_shape, -1, self.head_dim)
 
-        query_states = self.q_norm(self.q_proj(hidden_states))
-        key_states = self.k_norm(self.k_proj(hidden_states))
-        value_states = self.v_proj(hidden_states)
-
+        query_states = self.q_proj(hidden_states)
         query_states = query_states.view(hidden_shape).transpose(1, 2)
+        query_states = self.q_norm(query_states)
+        key_states = self.k_proj(hidden_states)
         key_states = key_states.view(hidden_shape).transpose(1, 2)
+        key_states = self.k_norm(key_states)
+        
+        value_states = self.v_proj(hidden_states)
         value_states = value_states.view(hidden_shape).transpose(1, 2)
 
         cos, sin = position_embeddings