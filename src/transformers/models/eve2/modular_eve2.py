c485b999f6ab3f5c339501a9856943870d50a7fc
Steven Bucaille <steven.bucaille@buawei.com>
2025-03-03 16:04:08 +0000

    draft commit with divide and conquer

--- src/transformers/models/eve2/modular_eve2.py
+++ src/transformers/models/eve2/modular_eve2.py
@@ -7,11 +7,14 @@ from ...cache_utils import Cache
 from ...modeling_outputs import CausalLMOutputWithPast
 from ..clip.image_processing_clip import CLIPImageProcessor
 from ..qwen2.configuration_qwen2 import Qwen2Config
-from ..qwen2.modeling_qwen2 import KwargsForCausalLM, Qwen2ForCausalLM, Qwen2Model
+from ..qwen2.modeling_qwen2 import KwargsForCausalLM, Qwen2Attention, Qwen2DecoderLayer, Qwen2ForCausalLM, Qwen2Model
 
 
-class EVEV2Config(Qwen2Config):
-    model_type = "evev2"
+_CONFIG_FOR_DOC = "Eve2Config"
+
+
+class Eve2Config(Qwen2Config):
+    model_type = "eve2"
 
     def __init__(
         self,
@@ -65,12 +68,25 @@ class EVEV2Config(Qwen2Config):
         self.dense_stride = dense_stride
 
 
-class EVEV2ImageProcessor(CLIPImageProcessor):
+class Eve2ImageProcessor(CLIPImageProcessor):
     pass
 
 
-class EVEV2VisionTokenizer(nn.Module):
-    def __init__(self, config: EVEV2Config):
+class Eve2DivideAndConquerModule(nn.Module):
+    def __init__(self, module: nn.Module, **kwargs):
+        self.visual_module = module(**kwargs)
+        self.textual_module = module(**kwargs)
+
+    def forward(self, hidden_states, visual_token_mask):
+        visual_projection = self.visual_projection(hidden_states)
+        textual_projection = self.textual_projection(hidden_states)
+        visual_projection = visual_projection * visual_token_mask
+        textual_projection = textual_projection * (1 - visual_token_mask)
+        return visual_projection + textual_projection
+
+
+class Eve2VisionEmbeddings(nn.Module):
+    def __init__(self, config: Eve2Config):
         super().__init__()
 
         input_size = config.mm_hidden_size
@@ -88,36 +104,73 @@ class EVEV2VisionTokenizer(nn.Module):
 
     def forward(self, pixel_values):
         patch_embeds = []
+        target_dtype = self.patch_embedding[0].weight.dtype
         for i in range(len(pixel_values)):
-            pixel_value = pixel_values[i].to(dtype=self.dtype)
+            pixel_value = pixel_values[i].to(dtype=target_dtype)
             patch_embed = self.patch_embedding(pixel_value.unsqueeze(0))[0]
             split_embed = self.split_embedding[:, None, None].repeat(1, patch_embed.shape[1], 1)
-            patch_embed = torch.cat([patch_embed, split_embed.to(dtype=self.dtype)], dim=-1)
-
-            class_embed = self.class_embedding[None, :].to(dtype=self.dtype)
+            patch_embed = torch.cat([patch_embed, split_embed.to(dtype=target_dtype)], dim=-1)
+            class_embed = self.class_embedding[None, :].to(dtype=target_dtype)
             patch_embeds.append(torch.cat([class_embed, patch_embed.flatten(1).transpose(0, 1)], dim=0))
 
         return patch_embeds
 
-    @property
-    def dtype(self):
-        return self.patch_embedding[0].weight.dtype
 
-    @property
-    def device(self):
-        return self.patch_embedding[0].weight.device
+class Eve2Attention(Qwen2Attention):
+    def __init__(self, config: Eve2Config, layer_idx: int):
+        super(Qwen2Attention, self).__init__(config, layer_idx)
+        self.q_proj = Eve2DivideAndConquerModule(
+            nn.Linear,
+            in_features=config.hidden_size,
+            out_features=config.num_key_value_heads * self.head_dim,
+            bias=True,
+        )
+        self.k_proj = Eve2DivideAndConquerModule(
+            nn.Linear,
+            in_features=config.hidden_size,
+            out_features=config.num_key_value_heads * self.head_dim,
+            bias=True,
+        )
+        self.v_proj = Eve2DivideAndConquerModule(
+            nn.Linear,
+            in_features=config.hidden_size,
+            out_features=config.num_key_value_heads * self.head_dim,
+            bias=True,
+        )
+        self.o_proj = Eve2DivideAndConquerModule(
+            nn.Linear,
+            in_features=config.num_attention_heads * self.head_dim,
+            out_features=config.hidden_size,
+            bias=False,
+        )
 
 
-class EVEV2Model(Qwen2Model):
-    config_class = EVEV2Config
+class Eve2DecoderLayer(Qwen2DecoderLayer):
+    def __init__(self, config: Eve2Config, layer_idx: int):
+        super(Qwen2DecoderLayer, self).__init__(config)
+        self.mlp = Eve2DivideAndConquerModule(Eve2MLP, config=config)
+        self.input_layernorm = Eve2DivideAndConquerModule(
+            Eve2RMSNorm, hidden_size=config.hidden_size, eps=config.rms_norm_eps
+        )
+        self.post_attention_layernorm = Eve2DivideAndConquerModule(
+            Eve2RMSNorm, hidden_size=config.hidden_size, eps=config.rms_norm_eps
+        )
 
-    def __init__(self, config: EVEV2Config):
+
+class Eve2Model(Qwen2Model):
+    config_class = Eve2Config
+
+    def __init__(self, config: Eve2Config):
         super().__init__(config)
 
-        self.vision_tower = EVEV2VisionTokenizer(config)
+        self.vision_embeddings = Eve2VisionEmbeddings(config)
 
 
-class EVEV2ForCausalLM(Qwen2ForCausalLM):
+class Eve2ForCausalLM(Qwen2ForCausalLM):
+    def __init__(self, config: Eve2Config):
+        super().__init__(config)
+        self.model = Eve2Model(config)
+
     def forward(
         self,
         input_ids: torch.LongTensor = None,
@@ -191,19 +244,23 @@ class EVEV2ForCausalLM(Qwen2ForCausalLM):
             attentions=outputs.attentions,
         )
 
-    def prepare_inputs_for_generation(self, input_ids, past_key_values=None, inputs_embeds=None, attention_mask=None,
-                                      **kwargs):
+    def prepare_inputs_for_generation(
+        self, input_ids, past_key_values=None, inputs_embeds=None, attention_mask=None, **kwargs
+    ):
         pixel_values = kwargs.pop("pixel_values", None)
         _inputs = super().prepare_inputs_for_generation(
-            input_ids, past_key_values=past_key_values, inputs_embeds=inputs_embeds, attention_mask=attention_mask,
-            **kwargs
+            input_ids,
+            past_key_values=past_key_values,
+            inputs_embeds=inputs_embeds,
+            attention_mask=attention_mask,
+            **kwargs,
         )
         if pixel_values is not None:
-            _inputs['pixel_values'] = pixel_values
+            _inputs["pixel_values"] = pixel_values
         return _inputs
 
     def get_transfer_tensor(self, tmp_tensor):
-        return tmp_tensor.to(dtype=self.get_model().dtype, device=self.get_model().device)
+        return tmp_tensor.to(dtype=self.model.dtype, device=self.model.device)
 
     def prepare_inputs_labels_for_multimodal(
         self, input_ids, position_ids, attention_mask, past_key_values, labels, images
@@ -212,14 +269,24 @@ class EVEV2ForCausalLM(Qwen2ForCausalLM):
         IMAGE_TOKEN_INDEX = -200
         vision_tower = self.get_vision_tower()
         if vision_tower is None or images is None or input_ids.shape[1] == 1:
-            if past_key_values is not None and vision_tower is not None and images is not None and input_ids.shape[
-                1] == 1:
+            if (
+                past_key_values is not None
+                and vision_tower is not None
+                and images is not None
+                and input_ids.shape[1] == 1
+            ):
                 target_shape = past_key_values[-1][-1].shape[-2] + 1
-                attention_mask = torch.cat((attention_mask, torch.ones(
-                    (attention_mask.shape[0], target_shape - attention_mask.shape[1]),
-                    dtype=attention_mask.dtype,
-                    device=attention_mask.device
-                )), dim=1)
+                attention_mask = torch.cat(
+                    (
+                        attention_mask,
+                        torch.ones(
+                            (attention_mask.shape[0], target_shape - attention_mask.shape[1]),
+                            dtype=attention_mask.dtype,
+                            device=attention_mask.device,
+                        ),
+                    ),
+                    dim=1,
+                )
                 position_ids = torch.sum(attention_mask, dim=1).unsqueeze(-1) - 1
             visual_token_mask = self.get_transfer_tensor(torch.zeros_like(input_ids).unsqueeze(-1))
             return input_ids, position_ids, attention_mask, past_key_values, None, labels, visual_token_mask
@@ -245,8 +312,9 @@ class EVEV2ForCausalLM(Qwen2ForCausalLM):
         input_ids_temp = input_ids  # points to the actual input_ids tensor
 
         # remove the padding using attention_mask -- TODO: double check
-        input_ids = [cur_input_ids[cur_attention_mask] for cur_input_ids, cur_attention_mask in
-                     zip(input_ids, attention_mask)]
+        input_ids = [
+            cur_input_ids[cur_attention_mask] for cur_input_ids, cur_attention_mask in zip(input_ids, attention_mask)
+        ]
         labels = [cur_labels[cur_attention_mask] for cur_labels, cur_attention_mask in zip(labels, attention_mask)]
 
         # -- TODO: better implementation?
@@ -269,14 +337,15 @@ class EVEV2ForCausalLM(Qwen2ForCausalLM):
                 visual_token_mask.append(self.get_transfer_tensor(torch.zeros_like(cur_input_ids).unsqueeze(-1)))
                 continue
 
-            image_token_indices = [-1] + torch.where(cur_input_ids == IMAGE_TOKEN_INDEX)[0].tolist() + [
-                cur_input_ids.shape[0]]
+            image_token_indices = (
+                [-1] + torch.where(cur_input_ids == IMAGE_TOKEN_INDEX)[0].tolist() + [cur_input_ids.shape[0]]
+            )
             cur_input_ids_noim = []
             cur_labels = labels[batch_idx]
             cur_labels_noim = []
             for i in range(len(image_token_indices) - 1):
-                cur_input_ids_noim.append(cur_input_ids[image_token_indices[i] + 1:image_token_indices[i + 1]])
-                cur_labels_noim.append(cur_labels[image_token_indices[i] + 1:image_token_indices[i + 1]])
+                cur_input_ids_noim.append(cur_input_ids[image_token_indices[i] + 1 : image_token_indices[i + 1]])
+                cur_labels_noim.append(cur_labels[image_token_indices[i] + 1 : image_token_indices[i + 1]])
             split_sizes = [x.shape[0] for x in cur_labels_noim]
             cur_input_embeds = self.get_model().embed_tokens(torch.cat(cur_input_ids_noim))
             cur_input_embeds_no_im = torch.split(cur_input_embeds, split_sizes, dim=0)
@@ -286,24 +355,27 @@ class EVEV2ForCausalLM(Qwen2ForCausalLM):
             for i in range(num_images + 1):
                 cur_new_input_embeds.append(cur_input_embeds_no_im[i])
                 cur_new_labels.append(cur_labels_noim[i])
-                cur_visual_mask.append(
-                    self.get_transfer_tensor(torch.zeros_like(cur_labels_noim[i]).unsqueeze(-1)))
+                cur_visual_mask.append(self.get_transfer_tensor(torch.zeros_like(cur_labels_noim[i]).unsqueeze(-1)))
                 if i < num_images:
                     cur_image_features = image_features[cur_image_idx]
                     cur_image_idx += 1
                     cur_new_input_embeds.append(cur_image_features)
                     cur_new_labels.append(
-                        torch.full((cur_image_features.shape[0],), IGNORE_INDEX, device=cur_labels.device,
-                                   dtype=cur_labels.dtype))
-                    cur_visual_mask.append(
-                        self.get_transfer_tensor(torch.ones(cur_image_features.shape[0], 1)))
+                        torch.full(
+                            (cur_image_features.shape[0],),
+                            IGNORE_INDEX,
+                            device=cur_labels.device,
+                            dtype=cur_labels.dtype,
+                        )
+                    )
+                    cur_visual_mask.append(self.get_transfer_tensor(torch.ones(cur_image_features.shape[0], 1)))
 
             new_input_embeds.append(torch.cat(cur_new_input_embeds))
             new_labels.append(torch.cat(cur_new_labels))
             visual_token_mask.append(torch.cat(cur_visual_mask))
 
         # Truncate sequences to max length as image embeddings can make the sequence longer
-        tokenizer_model_max_length = getattr(self.config, 'tokenizer_model_max_length', None)
+        tokenizer_model_max_length = getattr(self.config, "tokenizer_model_max_length", None)
         if tokenizer_model_max_length is not None:
             new_input_embeds = [x[:tokenizer_model_max_length] for x in new_input_embeds]
             new_labels = [x[:tokenizer_model_max_length] for x in new_labels]
@@ -314,26 +386,37 @@ class EVEV2ForCausalLM(Qwen2ForCausalLM):
         batch_size = len(new_input_embeds)
 
         new_input_embeds_padded = []
-        new_labels_padded = torch.full((batch_size, max_len), IGNORE_INDEX, dtype=new_labels[0].dtype,
-                                       device=new_labels[0].device)
+        new_labels_padded = torch.full(
+            (batch_size, max_len), IGNORE_INDEX, dtype=new_labels[0].dtype, device=new_labels[0].device
+        )
         visual_token_mask_padded = self.get_transfer_tensor(torch.zeros_like(new_labels_padded).unsqueeze(-1))
         attention_mask = torch.zeros((batch_size, max_len), dtype=attention_mask.dtype, device=attention_mask.device)
         position_ids = torch.zeros((batch_size, max_len), dtype=position_ids.dtype, device=position_ids.device)
 
         for i, (cur_new_embed, cur_new_label, cur_new_mask) in enumerate(
-                zip(new_input_embeds, new_labels, visual_token_mask)):
+            zip(new_input_embeds, new_labels, visual_token_mask)
+        ):
             cur_len = cur_new_embed.shape[0]
-            new_input_embeds_padded.append(torch.cat((
-                cur_new_embed,
-                torch.zeros((max_len - cur_len, cur_new_embed.shape[1]), dtype=cur_new_embed.dtype,
-                            device=cur_new_embed.device)
-            ), dim=0))
+            new_input_embeds_padded.append(
+                torch.cat(
+                    (
+                        cur_new_embed,
+                        torch.zeros(
+                            (max_len - cur_len, cur_new_embed.shape[1]),
+                            dtype=cur_new_embed.dtype,
+                            device=cur_new_embed.device,
+                        ),
+                    ),
+                    dim=0,
+                )
+            )
             if cur_len > 0:
                 new_labels_padded[i, :cur_len] = cur_new_label
                 visual_token_mask_padded[i, :cur_len] = cur_new_mask
                 attention_mask[i, :cur_len] = True
-                position_ids[i, :cur_len] = torch.arange(0, cur_len, dtype=position_ids.dtype,
-                                                         device=position_ids.device)
+                position_ids[i, :cur_len] = torch.arange(
+                    0, cur_len, dtype=position_ids.dtype, device=position_ids.device
+                )
 
         new_input_embeds = torch.stack(new_input_embeds_padded, dim=0)
 
@@ -350,4 +433,12 @@ class EVEV2ForCausalLM(Qwen2ForCausalLM):
         if _position_ids is None:
             position_ids = None
 
-        return None, position_ids, attention_mask, past_key_values, new_input_embeds, new_labels, visual_token_mask_padded
+        return (
+            None,
+            position_ids,
+            attention_mask,
+            past_key_values,
+            new_input_embeds,
+            new_labels,
+            visual_token_mask_padded,
+        )
