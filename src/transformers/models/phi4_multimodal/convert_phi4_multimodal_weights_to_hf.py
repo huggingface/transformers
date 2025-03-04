1bbb2987969ede9d5feb535710bce0e51fec9570
Cyril Vallez <cyril.vallez@gmail.com>
2025-03-04 14:03:11 +0100

    add lora extraction to converter

--- src/transformers/models/phi4_multimodal/convert_phi4_multimodal_weights_to_hf.py
+++ src/transformers/models/phi4_multimodal/convert_phi4_multimodal_weights_to_hf.py
@@ -17,7 +17,7 @@ import os
 import re
 
 import torch
-from safetensors.torch import load_file
+from safetensors.torch import load_file, save_file
 
 from transformers import (
     Phi4MultimodalAudioConfig,
@@ -26,6 +26,7 @@ from transformers import (
     Phi4MultimodalProcessor,
     Phi4MultimodalVisionConfig,
 )
+from peft import LoraConfig
 
 
 # fmt: off
@@ -168,6 +169,42 @@ def convert_and_save_processor(input_dir: str, output_dir: str):
     processor.save_pretrained(output_dir)
 
 
+def extract_adapters_data(input_dir: str, output_dir: str):
+    """Extract adapters data from the state dict and save weights and configs."""
+    speech_lora = {}
+    vision_lora = {}
+    shards = [file for file in os.listdir(input_dir) if file.endswith(".safetensors")]
+    for shard_file in shards:
+        original_state_dict = load_file(os.path.join(input_dir, shard_file))
+        for k, v in original_state_dict.items():
+            if "lora" in k:
+                if "speech" in k:
+                    speech_lora[k] = v
+                elif "vision" in k:
+                    vision_lora[k] = v
+
+    # Create and save the lora configs
+    speech_lora_config = LoraConfig(
+        r=320,
+        lora_alpha=640,
+        target_modules=r"model.layers.\d+.((self_attn.(qkv|o)_proj)|(mlp.(gate_up|down)_proj))",
+        lora_dropout=0.01,
+        task_type="CAUSAL_LM",
+    )
+    speech_lora_config.save_pretrained(os.path.join(output_dir, "speech-lora"))
+    vision_lora_config = LoraConfig(
+        r=256,
+        lora_alpha=512,
+        target_modules=r"model.layers.\d+.((self_attn.(qkv|o)_proj)|(mlp.(gate_up|down)_proj))",
+        lora_dropout=0.0,
+        task_type="CAUSAL_LM",
+    )
+    vision_lora_config.save_pretrained(os.path.join(output_dir, "vision-lora"))
+
+    save_file(speech_lora, os.path.join(output_dir, "speech-lora", "adapter_model.safetensors"))
+    save_file(vision_lora, os.path.join(output_dir, "vision-lora", "adapter_model.safetensors"))
+
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument(
@@ -183,6 +220,7 @@ def main():
     # Convert
     convert_and_write_model(args.input_dir, args.output_dir)
     convert_and_save_processor(args.input_dir, args.output_dir)
+    extract_adapters_data(args.input_dir, args.output_dir)
 
 
 if __name__ == "__main__":
