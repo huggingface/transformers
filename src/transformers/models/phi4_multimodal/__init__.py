923b2d09e8274faff2f618437d206064ccb49d84
Cyril Vallez <cyril.vallez@gmail.com>
2025-03-04 14:39:33 +0100

    Update __init__.py

--- src/transformers/models/phi4_multimodal/__init__.py
+++ src/transformers/models/phi4_multimodal/__init__.py
@@ -22,7 +22,7 @@ from ...utils.import_utils import define_import_structure
 if TYPE_CHECKING:
     from .configuration_phi4_multimodal import *
     from .feature_extraction_phi4_multimodal import *
-    from .image_processing_phi4_multimodal import *
+    from .image_processing_phi4_multimodal_fast import *
     from .modeling_phi4_multimodal import *
     from .processing_phi4_multimodal import *
 else:
