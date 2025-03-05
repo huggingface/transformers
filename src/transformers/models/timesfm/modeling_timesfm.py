b0354f048be7ae9475048d497e8953d12ad82257
Jinan Zhou <jinan.zhou@nutanix.com>
2025-03-05 00:44:48 -0800

    unwrap sequential class

--- src/transformers/models/timesfm/modeling_timesfm.py
+++ src/transformers/models/timesfm/modeling_timesfm.py
@@ -117,18 +117,16 @@ class TimesFmResidualBlock(nn.Module):
         self.output_dims = output_dims
 
         # Hidden Layer
-        self.hidden_layer = nn.Sequential(
-            nn.Linear(input_dims, hidden_dims),
-            nn.SiLU(),
-        )
-
+        self.input_layer = nn.Linear(input_dims, hidden_dims)
+        self.activation = nn.SiLU()
         # Output Layer
         self.output_layer = nn.Linear(hidden_dims, output_dims)
         # Residual Layer
         self.residual_layer = nn.Linear(input_dims, output_dims)
 
     def forward(self, x):
-        hidden = self.hidden_layer(x)
+        hidden = self.input_layer(x)
+        hidden = self.activation(hidden)
         output = self.output_layer(hidden)
         residual = self.residual_layer(x)
         return output + residual
