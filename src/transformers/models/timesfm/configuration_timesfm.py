df913603af5d3ddcd5ce6b3e01012eb7cae17e74
Kashif Rasul <kashif.rasul@gmail.com>
2025-03-05 11:30:58 +0100

    remove TimesFmOnnxConfig

--- src/transformers/models/timesfm/configuration_timesfm.py
+++ src/transformers/models/timesfm/configuration_timesfm.py
@@ -14,10 +14,9 @@
 # limitations under the License.
 """TimesFM model configuration"""
 
-from typing import List, Mapping
+from typing import List
 
 from ...configuration_utils import PretrainedConfig
-from ...onnx import OnnxSeq2SeqConfigWithPast
 from ...utils import logging
 
 
@@ -130,37 +129,6 @@ class TimesFmConfig(PretrainedConfig):
             is_encoder_decoder=self.is_encoder_decoder,
             **kwargs,
         )
-
-
-class TimesFmOnnxConfig(OnnxSeq2SeqConfigWithPast):
-    @property
-    def inputs(self) -> Mapping[str, Mapping[int, str]]:
-        common_inputs = {
-            "input_ids": {0: "batch", 1: "encoder_sequence"},
-            "attention_mask": {0: "batch", 1: "encoder_sequence"},
-        }
-        if self.use_past:
-            common_inputs["attention_mask"][1] = "past_encoder_sequence + sequence"
-            common_inputs["decoder_input_ids"] = {0: "batch"}
-            common_inputs["decoder_attention_mask"] = {
-                0: "batch",
-                1: "past_decoder_sequence + sequence",
-            }
-        else:
-            common_inputs["decoder_input_ids"] = {0: "batch", 1: "decoder_sequence"}
-            common_inputs["decoder_attention_mask"] = {
-                0: "batch",
-                1: "decoder_sequence",
-            }
-
-        if self.use_past:
-            self.fill_with_past_key_values_(common_inputs, direction="inputs")
-
-        return common_inputs
-
-    @property
-    def default_onnx_opset(self) -> int:
-        return 13
 
 
 __all__ = ["TimesFmConfig"]
