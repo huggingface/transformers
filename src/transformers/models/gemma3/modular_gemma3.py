fixes for embedding table overflow and missing image_soft_token_mask from Gemma3Processor

src/transformers/models/gemma3/modular_gemma3.py
@@ -574,8 +574,9 @@ class Gemma3Processor(ProcessorMixin):
             #   supports 896x896 images.
             #
             # So we concat all images across all batches into a single flat list prior to sending it to the
-            # `Gemma3ForConditionalGeneration.vision_model` for ecnoding and use `torch.masked_scatter()` to
-            # sequentially update the text embeddings wth the pooled vision embdeddings.
+            # `Gemma3ForConditionalGeneration.vision_model` for ecnoding and use `torch.masked_scatter()` in that the
+            # `Gemma3ForConditionalGeneration` model class to sequentially update the language model embeddings wth the
+            # pooled vision embdeddings.
             pixel_values = torch.cat([
                 self.image_processor(prompt_images, **output_kwargs["images_kwargs"])["pixel_values"]
                 for prompt_images in batch_flattened_images
@@ -589,9 +590,14 @@ class Gemma3Processor(ProcessorMixin):
         )
 
         if pixel_values is not None:
+            input_ids = batched_input["input_ids"].clone()
+            image_soft_token_mask = input_ids == self.image_soft_token_id
+            input_ids[image_soft_token_mask] = self.tokenizer.convert_tokens_to_ids("<pad>")
+
             batched_input.update(
+                input_ids=input_ids,
                 pixel_values=pixel_values,
-                image_soft_token_mask=batched_input["input_ids"] == self.image_soft_token_id,
+                image_soft_token_mask=image_soft_token_mask,
             )
 
         return batched_input
@@ -1772,7 +1778,7 @@ class Gemma3ForConditionalGeneration(PreTrainedModel, GenerationMixin):
         self,
         input_ids: Optional[torch.LongTensor] = None,
         attention_mask: Optional[torch.Tensor] = None,
-        pixel_values: Optional[Sequence[torch.FloatTensor]] = None,
+        pixel_values: Optional[torch.FloatTensor] = None,
         image_soft_token_mask: Optional[torch.BoolTensor] = None,
         position_ids: Optional[torch.LongTensor] = None,
         past_key_values: Optional[Union[list[torch.FloatTensor], Cache]] = None,
@@ -1851,9 +1857,6 @@ class Gemma3ForConditionalGeneration(PreTrainedModel, GenerationMixin):
 
         is_training = token_type_ids is not None and labels is not None
 
-        if input_ids is not None and image_soft_token_mask is None:
-            image_soft_token_mask = input_ids == self.config.text_config.mm_soft_token_id
-
         if inputs_embeds is None:
             inputs_embeds = self.get_input_embeddings()(input_ids)
 
@@ -1871,14 +1874,23 @@ class Gemma3ForConditionalGeneration(PreTrainedModel, GenerationMixin):
             position_ids = (cache_position.unsqueeze(0) + 1)
 
         # Merge text and images
-        if pixel_values is not None and image_soft_token_mask is not None:
+        if pixel_values is not None:
+            if image_soft_token_mask is None:
+                raise ValueError(
+                    "Cannot join vision and language embeddings wihtout an `image_soft_token_mask`. "
+                    "Use Gemma3Processor to create one."
+                )
+
             image_features = self.get_image_features(pixel_values).to(inputs_embeds.device, inputs_embeds.dtype)
 
             image_mask = image_soft_token_mask.unsqueeze(-1)
             image_mask = image_mask.expand_as(inputs_embeds).to(inputs_embeds.device)
 
-            if inputs_embeds[image_mask].numel() != image_features.numel():
-                raise ValueError("Number of images does not match number of special image tokens in the input text. ")
+            if (emb_nel := inputs_embeds[image_mask].numel()) != (img_nel := image_features.numel()):
+                raise ValueError(
+                    f"Number of image features ({img_nel}) does not match number of special image tokens in the input "
+                    f"text ({emb_nel}). "
+                )
 
             inputs_embeds = inputs_embeds.masked_scatter(image_mask, image_features)
 
@@ -1963,6 +1975,7 @@ class Gemma3ForConditionalGeneration(PreTrainedModel, GenerationMixin):
         cache_position=None,
         position_ids=None,
         pixel_values=None,
+        image_soft_token_mask=None,
         attention_mask=None,
         token_type_ids=None,
         use_cache=True,
@@ -1987,10 +2000,15 @@ class Gemma3ForConditionalGeneration(PreTrainedModel, GenerationMixin):
         # position_ids in Paligemma are 1-indexed
         if model_inputs.get("position_ids") is not None:
             model_inputs["position_ids"] += 1
-        # If we're in cached decoding stage, pixel values should be None because input ids do not contain special image token anymore
-        # Otherwise we need pixel values to be passed to model. NOTE: use_cache=False needs pixel_values always
+
+        # If we're in cached decoding stage, pixel values should be None because
+        # input ids do not contain special image tokens anymore. Otherwise we
+        # need pixel values to be passed to model.
+        # NOTE: use_cache=False needs pixel_values always
         if cache_position[0] == 0:
             model_inputs["pixel_values"] = pixel_values
+            model_inputs["image_soft_token_mask"] = image_soft_token_mask
+
         is_training = token_type_ids is not None and labels is not None
         if cache_position[0] == 0 and isinstance(past_key_values, HybridCache):
             input_tensor = inputs_embeds if inputs_embeds is not None else input_ids

src/transformers/models/gemma3/processing_gemma3.py
@@ -235,8 +235,9 @@ class Gemma3Processor(ProcessorMixin):
             #   supports 896x896 images.
             #
             # So we concat all images across all batches into a single flat list prior to sending it to the
-            # `Gemma3ForConditionalGeneration.vision_model` for ecnoding and use `torch.masked_scatter()` to
-            # sequentially update the text embeddings wth the pooled vision embdeddings.
+            # `Gemma3ForConditionalGeneration.vision_model` for ecnoding and use `torch.masked_scatter()` in that the
+            # `Gemma3ForConditionalGeneration` model class to sequentially update the language model embeddings wth the
+            # pooled vision embdeddings.
             pixel_values = torch.cat(
                 [
                     self.image_processor(prompt_images, **output_kwargs["images_kwargs"])["pixel_values"]
@@ -250,9 +251,14 @@ class Gemma3Processor(ProcessorMixin):
         batched_input = self._process_text(text=text, batched_images=batched_images, **output_kwargs["text_kwargs"])
 
         if pixel_values is not None:
+            input_ids = batched_input["input_ids"].clone()
+            image_soft_token_mask = input_ids == self.image_soft_token_id
+            input_ids[image_soft_token_mask] = self.tokenizer.convert_tokens_to_ids("<pad>")
+
             batched_input.update(
+                input_ids=input_ids,
                 pixel_values=pixel_values,
-                image_soft_token_mask=batched_input["input_ids"] == self.image_soft_token_id,
+                image_soft_token_mask=image_soft_token_mask,
             )
 
         return batched_input