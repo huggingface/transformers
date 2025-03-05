04176066c6aa9cffb5a9894c09dac4abf68ef2a4
innerNULL <lyt697@outlook.com>
2025-03-05 15:45:10 +0800

    dbg: Remove logics and another test failed which successed before, so should be the stablibility issue

--- src/transformers/trainer.py
+++ src/transformers/trainer.py
@@ -3985,9 +3985,8 @@ class Trainer:
             and hasattr(self.data_collator, "tokenizer")
             and self.data_collator.tokenizer is not None
         ):
-            #logger.info("Saving Trainer.data_collator.tokenizer by default as Trainer.processing_class is `None`")
-            #self.data_collator.tokenizer.save_pretrained(output_dir)
-            pass
+            logger.info("Saving Trainer.data_collator.tokenizer by default as Trainer.processing_class is `None`")
+            self.data_collator.tokenizer.save_pretrained(output_dir)
 
         # Good practice: save your training arguments together with the trained model
         torch.save(self.args, os.path.join(output_dir, TRAINING_ARGS_NAME))
