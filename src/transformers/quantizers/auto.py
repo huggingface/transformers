93d8480388d81713d34e2cd7c8e49d6d899442d2
Felix Marty <felmarty@amd.com>
2025-03-10 14:20:30 +0100

    update copyright headers

--- src/transformers/quantizers/auto.py
+++ src/transformers/quantizers/auto.py
@@ -1,5 +1,5 @@
 # Copyright 2024 The HuggingFace Inc. team. All rights reserved.
-# Modifications Copyright (C) 2024, Advanced Micro Devices, Inc. All rights reserved.
+# Modifications Copyright (C) 2025, Advanced Micro Devices, Inc. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
