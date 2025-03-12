221a8f41b886ba0956a8478945deabbc50ec4649
Vladislav <vladislav.bronzov@gmail.com>
2025-03-12 17:12:33 +0100

    fix dummies

--- src/transformers/utils/dummy_pt_objects.py
+++ src/transformers/utils/dummy_pt_objects.py
@@ -2819,6 +2819,13 @@ class DeepseekV2ForCausalLM(metaclass=DummyObject):
         requires_backends(self, ["torch"])
 
 
+class DeepseekV2ForSequenceClassification(metaclass=DummyObject):
+    _backends = ["torch"]
+
+    def __init__(self, *args, **kwargs):
+        requires_backends(self, ["torch"])
+
+
 class DeepseekV2Model(metaclass=DummyObject):
     _backends = ["torch"]
 
