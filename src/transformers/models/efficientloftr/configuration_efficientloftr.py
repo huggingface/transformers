14dcd0e7e1a63d78c79888bb1be8e2e4e3a9f836
steven <steven.bucaille@gmail.com>
2025-03-12 22:24:34 +0100

    fix: model repo typo in config

--- src/transformers/models/efficientloftr/configuration_efficientloftr.py
+++ src/transformers/models/efficientloftr/configuration_efficientloftr.py
@@ -28,7 +28,7 @@ class EfficientLoFTRConfig(PretrainedConfig):
     This is the configuration class to store the configuration of a [`EffientLoFTRFromKeypointMatching`].
     It is used to instantiate a EfficientLoFTR model according to the specified arguments, defining the model
     architecture. Instantiating a configuration with the defaults will yield a similar configuration to that of the
-    EfficientLoFTR [stevenbucaille/efficientloftr](https://huggingface.co/stevenbucaille/efficient_loftr) architecture.
+    EfficientLoFTR [stevenbucaille/efficientloftr](https://huggingface.co/stevenbucaille/efficientloftr) architecture.
 
     Configuration objects inherit from [`PretrainedConfig`] and can be used to control the model outputs. Read the
     documentation from [`PretrainedConfig`] for more information.
