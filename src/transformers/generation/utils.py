d0bbb3396f93ff4f5cf1747798e47a96adbc51c2
xadupre <xadupre@microsoft.com>
2025-03-12 09:26:05 +0100

    style

--- src/transformers/generation/utils.py
+++ src/transformers/generation/utils.py
@@ -414,15 +414,19 @@ class GenerationMixin:
                     #             input_ids = input_ids[:, cache_position]
                     def branch_1(inputs_embeds, cache_position):
                         return inputs_embeds[:, -cache_position.shape[0] :]
+
                     def branch_2(input_ids, cache_position):
                         return input_ids[:, -cache_position.shape[0] :]
+
                     def branch_3(input_ids, cache_position):
                         return input_ids[:, cache_position]
+
                     inputs_embeds, input_ids = torch.cond(
                         input_ids.shape[1] == 0,
                         (
                             lambda input_ids, inputs_embeds, cache_position: (
-                                branch_1(inputs_embeds, cache_position), input_ids
+                                branch_1(inputs_embeds, cache_position),
+                                input_ids,
                             )
                         ),
                         (
@@ -439,20 +443,17 @@ class GenerationMixin:
                                             [input_ids, cache_position],
                                         )
                                     ),
-                                    [input_ids, cache_position]
-                                )
+                                    [input_ids, cache_position],
+                                ),
                             )
                         ),
-                        [input_ids, inputs_embeds, cache_position]
+                        [input_ids, inputs_embeds, cache_position],
                     )
             elif inputs_embeds is not None and input_ids.shape[1] == 0:  # Exception 4
                 inputs_embeds = inputs_embeds[:, -cache_position.shape[0] :]
             elif (
                 inputs_embeds is not None  # Exception 1
-                or (
-                    is_torchdynamo_compiling()
-                    or cache_position[-1] >= input_ids.shape[1]
-                )  # Exception 3
+                or (is_torchdynamo_compiling() or cache_position[-1] >= input_ids.shape[1])  # Exception 3
             ):
                 input_ids = input_ids[:, -cache_position.shape[0] :]
             elif input_ids.shape[1] != cache_position.shape[0]:  # Default case (the "else", a no op, is Exception 2)
