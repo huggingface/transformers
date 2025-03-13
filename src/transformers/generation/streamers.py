3d22f71fe336904e23926eab1ef1fadee4e90794
Muspi Merol <me@promplate.dev>
2025-03-14 06:54:25 +0800

    Simplify string sliding

--- src/transformers/generation/streamers.py
+++ src/transformers/generation/streamers.py
@@ -112,7 +112,7 @@ class TextStreamer(BaseStreamer):
         # Otherwise, print all text except the last character to avoid showing incomplete char during streaming,
         # which may change with the subsequent token -- there are probably smarter ways to do this!)
         else:
-            printable_text = text[self.print_len : len(text) - 1]  # the incomplete char cannot be longer than 1
+            printable_text = text[self.print_len : -1]  # the incomplete char cannot be longer than 1
             self.print_len += len(printable_text)
 
         self.on_finalized_text(printable_text)
