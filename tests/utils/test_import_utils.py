7eb35ed6cafa8d6a02fdaf5ce284adedd1208759
sambhavnoobcoder <indosambhav@gmail.com>
2025-03-14 22:43:24 +0530

    make fixup

--- tests/utils/test_import_utils.py
+++ tests/utils/test_import_utils.py
@@ -7,7 +7,6 @@ from transformers.testing_utils import run_test_using_subprocess
 from transformers.utils.import_utils import clear_import_cache
 
 
-
 @pytest.mark.skipif(
     "PYTEST_XDIST_WORKER" in os.environ,
     reason="This test should not run under pytest-xdist workers",
@@ -32,5 +31,6 @@ def test_clear_import_cache():
 
     # Import and verify module exists
     from transformers.models.auto import modeling_auto
+
     assert "transformers.models.auto.modeling_auto" in sys.modules
     assert modeling_auto.__name__ == "transformers.models.auto.modeling_auto"
