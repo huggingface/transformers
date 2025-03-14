ba05f2ebcaed3ed436953a3d401f1ecf14f76ef1
JJJYmmm <92386084+JJJYmmm@users.noreply.github.com>
2025-03-14 22:28:06 +0800

    remove redundant import

--- src/transformers/models/auto/image_processing_auto.py
+++ src/transformers/models/auto/image_processing_auto.py
@@ -24,7 +24,7 @@ from typing import TYPE_CHECKING, Dict, Optional, Tuple, Union
 # Build the list of all image processors
 from ...configuration_utils import PretrainedConfig
 from ...dynamic_module_utils import get_class_from_dynamic_module, resolve_trust_remote_code
-from ...image_processing_utils import BaseImageProcessor, ImageProcessingMixin
+from ...image_processing_utils import ImageProcessingMixin
 from ...image_processing_utils_fast import BaseImageProcessorFast
 from ...utils import (
     CONFIG_NAME,
