21e0900543d035bc91bec9d037c58a2573a35c38
sambhavnoobcoder <indosambhav@gmail.com>
2025-03-15 03:58:13 +0530

    rm redundat skipif + style changes

--- tests/utils/test_import_utils.py
+++ tests/utils/test_import_utils.py
@@ -1,16 +1,9 @@
-import os
 import sys
-
-import pytest
 
 from transformers.testing_utils import run_test_using_subprocess
 from transformers.utils.import_utils import clear_import_cache
 
 
-@pytest.mark.skipif(
-    "PYTEST_XDIST_WORKER" in os.environ,
-    reason="This test should not run under pytest-xdist workers",
-)
 @run_test_using_subprocess
 def test_clear_import_cache():
     """Test the clear_import_cache function."""
@@ -28,5 +21,6 @@ def test_clear_import_cache():
 
     # Import and verify module exists
     from transformers.models.auto import modeling_auto
+
     assert "transformers.models.auto.modeling_auto" in sys.modules
     assert modeling_auto.__name__ == "transformers.models.auto.modeling_auto"
