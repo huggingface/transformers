3c087177b0b95a5ffdc03bd31c283b9b0bd9b1bd
Alex-Brooks <Alex.brooks@ibm.com>
2025-03-17 04:23:56 +0000

    Add dummy objects for torch/torchaudio

--- src/transformers/utils/dummy_pt_objects.py
+++ src/transformers/utils/dummy_pt_objects.py
@@ -5066,6 +5066,20 @@ class GraniteMoeSharedPreTrainedModel(metaclass=DummyObject):
         requires_backends(self, ["torch"])
 
 
+class GraniteSpeechPreTrainedModel(metaclass=DummyObject):
+    _backends = ["torch"]
+
+    def __init__(self, *args, **kwargs):
+        requires_backends(self, ["torch"])
+
+
+class GraniteSpeechForConditionalGeneration(metaclass=DummyObject):
+    _backends = ["torch"]
+
+    def __init__(self, *args, **kwargs):
+        requires_backends(self, ["torch"])
+
+
 class GroundingDinoForObjectDetection(metaclass=DummyObject):
     _backends = ["torch"]
 
