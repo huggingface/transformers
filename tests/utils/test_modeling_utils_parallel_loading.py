27f36f2432425ba8ae4e742a52e0aaa50de262eb
Aaron Vogler <aaronvogler@gmail.com>
2025-03-18 21:28:55 -0400

    Update the tests for parallel loading

--- tests/utils/test_modeling_utils_parallel_loading.py
+++ tests/utils/test_modeling_utils_parallel_loading.py
@@ -1,25 +1,12 @@
 import multiprocessing
 import os
 
-from .test_modeling_utils import ModelUtilsTest
 
+# Set the env variable to enable parallel loading
+os.environ["ENABLE_PARALLEL_LOADING"] = "true"
 
-original_setUp = ModelUtilsTest.setUp
+# Set multiprocessing to spawn, which is required due to torch contraints
+multiprocessing.set_start_method("spawn", force=True)
 
-
-# We're monkey patching the original tests, as we want to run them, but now with the parallel loader enabled
-def patched_setUp(self):
-    # Call the original setUp first
-    original_setUp(self)
-
-    # Set the env variable to enable parallel loading
-    os.environ.setdefault("ENABLE_PARALLEL_LOADING", "true")
-    # Set multiprocessing to spawn, which is required due to torch contraints
-    multiprocessing.set_start_method("spawn", force=True)
-
-
-try:
-    # Monkey patch the setUp method
-    ModelUtilsTest.setUp = patched_setUp
-finally:
-    ModelUtilsTest.setUp = original_setUp
+# Declare the normal model_utils.py test as a sideffect of importing the module
+from .test_modeling_utils import ModelUtilsTest # noqa
