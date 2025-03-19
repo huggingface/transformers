33b3e0f04b72319549e6c74790caa5a37f8b39d4
Aaron V <inf3rnus@users.noreply.github.com>
2025-03-19 14:43:37 -0400

    Edit comment in test_modeling_utils_parallel_loading.py

--- tests/utils/test_modeling_utils_parallel_loading.py
+++ tests/utils/test_modeling_utils_parallel_loading.py
@@ -5,7 +5,7 @@ import os
 # Set the env variable to enable parallel loading
 os.environ["HF_HF_ENABLE_PARALLEL_LOADING"] = "true"
 
-# Set multiprocessing to spawn, which is required due to torch contraints
+# Set multiprocessing to spawn, which is required due to cuda constraints
 multiprocessing.set_start_method("spawn", force=True)
 
 # Declare the normal model_utils.py test as a sideffect of importing the module
