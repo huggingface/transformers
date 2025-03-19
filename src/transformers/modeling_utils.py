0e22c047f18bc3f47b1c32969bc89c08f07c7d33
Aaron Vogler <aaronvogler@gmail.com>
2025-03-19 15:05:37 -0400

    Make sure HF_PARALLEL_LOADING_WORKERS is spelled correctly in modeling_utils.py

--- src/transformers/modeling_utils.py
+++ src/transformers/modeling_utils.py
@@ -4983,7 +4983,7 @@ class PreTrainedModel(nn.Module, ModuleUtilsMixin, GenerationMixin, PushToHubMix
 
         # Use multiprocessing Pool for parallel execution, off by default
         if json.loads(os.environ.get("HF_ENABLE_PARALLEL_LOADING", "false")):
-            num_workers = json.loads(os.environ.get("PARALLEL_LOADING_WORKERS", "8"))
+            num_workers = json.loads(os.environ.get("HF_PARALLEL_LOADING_WORKERS", "8"))
             logger.info(f"Loading model weights in parallel with {num_workers} workers...")
             state_dict_modules_list = []
 
