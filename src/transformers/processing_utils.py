99330e19823646e2c640b0e99340e87580f71e1e
Matt <rocketknight1@gmail.com>
2025-03-20 16:11:05 +0000

    Revert changes now we don't have empty templates

--- src/transformers/processing_utils.py
+++ src/transformers/processing_utils.py
@@ -877,7 +877,6 @@ class ProcessorMixin(PushToHubMixin):
         elif resolved_chat_template_file is not None:
             with open(resolved_chat_template_file, encoding="utf-8") as reader:
                 chat_template_json = json.loads(reader.read())
-            if "chat_template" in chat_template_json:  # These files are sometimes empty in tests
                 default_chat_template = chat_template_json["chat_template"]
         chat_templates = {
             template_name: open(template_file, "r", encoding="utf-8").read()
