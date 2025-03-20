ca804ea9c17d095e9bd2d8d0e2ef990f6761909a
NieCha <936810980@qq.com>
2025-03-20 15:37:38 +0800

    add_new_model: Long_Vita

--- src/transformers/models/long_vita/modular_long_vita.py
+++ src/transformers/models/long_vita/modular_long_vita.py
@@ -25,7 +25,6 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.utils.checkpoint
-from timm.models.layers import DropPath
 
 from transformers import Qwen2ForCausalLM, Qwen2Model
 from transformers.cache_utils import Cache, DynamicCache
@@ -307,6 +306,26 @@ def init_weights(m):
         torch.nn.init.zeros_(m.bias)
 
 
+def drop_path(x, drop_prob: float = 0., training: bool = False, scale_by_keep: bool = True):
+    """Drop paths (Stochastic Depth) per sample (when applied in main path of residual blocks).
+
+    This is the same as the DropConnect impl I created for EfficientNet, etc networks, however,
+    the original name is misleading as 'Drop Connect' is a different form of dropout in a separate paper...
+    See discussion: https://github.com/tensorflow/tpu/issues/494#issuecomment-532968956 ... I've opted for
+    changing the layer and argument names to 'drop path' rather than mix DropConnect as a layer name and use
+    'survival rate' as the argument.
+
+    """
+    if drop_prob == 0. or not training:
+        return x
+    keep_prob = 1 - drop_prob
+    shape = (x.shape[0],) + (1,) * (x.ndim - 1)  # work with diff dim tensors, not just 2D ConvNets
+    random_tensor = x.new_empty(shape).bernoulli_(keep_prob)
+    if keep_prob > 0.0 and scale_by_keep:
+        random_tensor.div_(keep_prob)
+    return x * random_tensor
+
+
 class ResamplerProjector(nn.Module):
     def __init__(self, config, vision_model_config):
         super().__init__()
@@ -338,6 +357,20 @@ NORM2FN = {
     "rms_norm": InternRMSNorm,
     "layer_norm": nn.LayerNorm,
 }
+
+class DropPath(nn.Module):
+    """Drop paths (Stochastic Depth) per sample  (when applied in main path of residual blocks).
+    """
+    def __init__(self, drop_prob: float = 0., scale_by_keep: bool = True):
+        super(DropPath, self).__init__()
+        self.drop_prob = drop_prob
+        self.scale_by_keep = scale_by_keep
+
+    def forward(self, x):
+        return drop_path(x, self.drop_prob, self.training, self.scale_by_keep)
+
+    def extra_repr(self):
+        return f'drop_prob={round(self.drop_prob,3):0.3f}'
 
 
 class InternVisionEncoderLayer(nn.Module):
