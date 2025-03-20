cbca4477b61d37f0d9bbedc00412751bd4daa02b
NieCha <936810980@qq.com>
2025-03-20 15:26:18 +0800

    add_new_model: Long_Vita

--- src/transformers/models/long_vita/modeling_long_vita.py
+++ src/transformers/models/long_vita/modeling_long_vita.py
@@ -5,7 +5,7 @@
 #                          modular_long_vita.py file directly. One of our CI enforces this.
 #                🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨
 # coding=utf-8
-# Copyright 2025 The Qwen Team and The HuggingFace Inc. team. All rights reserved.
+# Copyright 2025 The Vita Team and The HuggingFace Inc. team. All rights reserved.
 #
 # This code is based on EleutherAI's GPT-NeoX library and the GPT-NeoX
 # and OPT implementations in this library. It has been modified from its
