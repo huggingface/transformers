23c17dd510e0d721bcd6c9013f502871facaf9ae
Thishyaketh <thishyakethabimalla@gmail.com>
2025-03-22 13:09:39 +0530

    Add NGen3

--- src/transformers/models/auto/modeling_auto.py
+++ src/transformers/models/auto/modeling_auto.py
@@ -198,6 +198,7 @@ MODEL_MAPPING_NAMES = OrderedDict(
         ("nat", "NatModel"),
         ("nemotron", "NemotronModel"),
         ("nezha", "NezhaModel"),
+        ("ngen3", "NGen3")
         ("nllb-moe", "NllbMoeModel"),
         ("nystromformer", "NystromformerModel"),
         ("olmo", "OlmoModel"),
@@ -370,6 +371,7 @@ MODEL_FOR_PRETRAINING_MAPPING_NAMES = OrderedDict(
         ("mra", "MraForMaskedLM"),
         ("mvp", "MvpForConditionalGeneration"),
         ("nezha", "NezhaForPreTraining"),
+        ("ngen3", "NGEN3ForCasualLM")
         ("nllb-moe", "NllbMoeForConditionalGeneration"),
         ("openai-gpt", "OpenAIGPTLMHeadModel"),
         ("paligemma", "PaliGemmaForConditionalGeneration"),
@@ -459,6 +461,7 @@ MODEL_WITH_LM_HEAD_MAPPING_NAMES = OrderedDict(
         ("mra", "MraForMaskedLM"),
         ("mvp", "MvpForConditionalGeneration"),
         ("nezha", "NezhaForMaskedLM"),
+        ("ngen3", "NGEN3ForCasualLM")
         ("nllb-moe", "NllbMoeForConditionalGeneration"),
         ("nystromformer", "NystromformerForMaskedLM"),
         ("openai-gpt", "OpenAIGPTLMHeadModel"),
@@ -556,6 +559,7 @@ MODEL_FOR_CAUSAL_LM_MAPPING_NAMES = OrderedDict(
         ("musicgen_melody", "MusicgenMelodyForCausalLM"),
         ("mvp", "MvpForCausalLM"),
         ("nemotron", "NemotronForCausalLM"),
+        ("ngen3", "NGen3ForCasualLM"),
         ("olmo", "OlmoForCausalLM"),
         ("olmo2", "Olmo2ForCausalLM"),
         ("olmoe", "OlmoeForCausalLM"),
---
+++ src/transformers/models/ngen3/_init_.py
@@ -0,0 +1,5 @@
+# __init__.py
+from .configuration_ngen3 import NGEN3Config
+from .modeling_ngen3 import NGEN3ForCausalLM
+
+__all__ = ["NGEN3Config", "NGEN3ForCausalLM"]
