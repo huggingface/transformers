d40175f2c3b3f48cd3aa1670c888c6b5917549ef
Alex-Brooks <Alex.brooks@ibm.com>
2025-03-24 05:52:21 +0000

    Add set input/output embeddings for granite speech

--- src/transformers/models/granite_speech/modeling_granite_speech.py
+++ src/transformers/models/granite_speech/modeling_granite_speech.py
@@ -1165,6 +1165,12 @@ class GraniteSpeechForConditionalGeneration(GraniteSpeechPretrainedModel, Genera
 
         self.post_init()
 
+    def set_input_embeddings(self, value):
+        self.language_model.set_input_embeddings(value)
+
+    def set_output_embeddings(self, new_embeddings):
+        self.language_model.set_output_embeddings(new_embeddings)
+
     def get_input_embeddings(self):
         return self.language_model.get_input_embeddings()
 
