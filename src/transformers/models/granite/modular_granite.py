8f6e3e9f3605bc1da44a9a2486ca23d01ad84ebd
qubvel <qubvel@gmail.com>
2025-03-24 11:43:12 +0000

    Fix attribute Granite

--- src/transformers/models/granite/modular_granite.py
+++ src/transformers/models/granite/modular_granite.py
@@ -263,7 +263,7 @@ class GraniteForCausalLM(LlamaForCausalLM):
             **kwargs,
         )
 
-        hidden_states = outputs[0]
+        hidden_states = outputs.last_hidden_state
         # Only compute necessary logits, and do not upcast them to float if we are not computing the loss
         slice_indices = slice(-logits_to_keep, None) if isinstance(logits_to_keep, int) else logits_to_keep
         logits = self.lm_head(hidden_states[:, slice_indices, :])
