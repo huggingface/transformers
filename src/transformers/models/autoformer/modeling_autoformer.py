d0918bb64a19704375973bfaa4eb4a9fa554fe33
Cyril Vallez <cyril.vallez@gmail.com>
2025-03-26 00:07:56 +0100

    remove grad

--- src/transformers/models/autoformer/modeling_autoformer.py
+++ src/transformers/models/autoformer/modeling_autoformer.py
@@ -370,13 +370,12 @@ class AutoformerSinusoidalPositionalEmbedding(nn.Embedding):
         position_enc = np.array(
             [[pos / np.power(10000, 2 * (j // 2) / dim) for j in range(dim)] for pos in range(n_pos)]
         )
-        out = torch.empty(n_pos, dim, dtype=self.weight.dtype)
-        out.requires_grad = False  # set early to avoid an error in pytorch-1.8+
+        out = torch.empty(n_pos, dim, dtype=self.weight.dtype, requires_grad=False)
         sentinel = dim // 2 if dim % 2 == 0 else (dim // 2) + 1
         out[:, 0:sentinel] = torch.FloatTensor(np.sin(position_enc[:, 0::2]))
         out[:, sentinel:] = torch.FloatTensor(np.cos(position_enc[:, 1::2]))
         out.detach_()
-        self.weight = nn.Parameter(out)
+        self.weight = nn.Parameter(out, requires_grad=False)
 
     @torch.no_grad()
     def forward(self, input_ids_shape: torch.Size, past_key_values_length: int = 0) -> torch.Tensor:
