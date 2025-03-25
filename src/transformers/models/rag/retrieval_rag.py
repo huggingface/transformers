fb9462670fcd035b9641985503bbf10fccb17c46
ydshieh <ydshieh@users.noreply.github.com>
2025-03-25 15:33:37 +0100

    update

--- src/transformers/models/rag/retrieval_rag.py
+++ src/transformers/models/rag/retrieval_rag.py
@@ -156,9 +156,9 @@ class LegacyIndex(Index):
             )
         with open(resolved_meta_path, "rb") as metadata_file:
             self.index_id_to_db_id = pickle.load(metadata_file)
-        assert (
-            len(self.index_id_to_db_id) == self.index.ntotal
-        ), "Deserialized index_id_to_db_id should match faiss index size"
+        assert len(self.index_id_to_db_id) == self.index.ntotal, (
+            "Deserialized index_id_to_db_id should match faiss index size"
+        )
 
     def is_initialized(self):
         return self._index_initialized
