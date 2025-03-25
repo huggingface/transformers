3a972c86b9ad94c75ed258cfe93dd6c3c25a756a
Vladislav <vladislav.bronzov@gmail.com>
2025-03-25 15:20:45 +0100

    small fix for converted modeling

--- src/transformers/models/d_fine/modeling_d_fine.py
+++ src/transformers/models/d_fine/modeling_d_fine.py
@@ -1831,7 +1831,6 @@ class DFineForObjectDetection(DFinePreTrainedModel):
         output_hidden_states = (
             output_hidden_states if output_hidden_states is not None else self.config.output_hidden_states
         )
-
         return_dict = return_dict if return_dict is not None else self.config.use_return_dict
 
         outputs = self.model(
