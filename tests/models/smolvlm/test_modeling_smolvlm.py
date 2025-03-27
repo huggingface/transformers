6cf7ee3af1a8805fcec4fed9037610edcf3bc321
Yih-Dar <2521628+ydshieh@users.noreply.github.com>
2025-03-27 10:59:47 +0100

    Mark 2 tests as flaky for now (#37038)
    * fix
    
    * fix
    
    * fix
    
    ---------
    
    Co-authored-by: ydshieh <ydshieh@users.noreply.github.com>

--- tests/models/smolvlm/test_modeling_smolvlm.py
+++ tests/models/smolvlm/test_modeling_smolvlm.py
@@ -29,6 +29,7 @@ from transformers import (
 )
 from transformers.testing_utils import (
     cleanup,
+    is_flaky,
     require_torch,
     require_torch_sdpa,
     slow,
@@ -372,6 +373,11 @@ class SmolVLMForConditionalGenerationModelTest(GenerationTesterMixin, ModelTeste
     )
     def test_prompt_lookup_decoding_matches_greedy_search(self):
         pass
+
+    @pytest.mark.generate
+    @is_flaky(description="TODO: check why flaky")
+    def test_generate_methods_with_logits_to_keep(self):
+        super().test_generate_methods_with_logits_to_keep()
 
     @unittest.skip(reason=" FlashAttention only support fp16 and bf16 data type")
     def test_flash_attn_2_fp32_ln(self):
