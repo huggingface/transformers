faf6078a4d4f659e58dc40820719d33783f5f9ed
Cyril Vallez <cyril.vallez@gmail.com>
2025-03-27 01:17:32 +0100

    style

--- src/transformers/models/dinov2_with_registers/modular_dinov2_with_registers.py
+++ src/transformers/models/dinov2_with_registers/modular_dinov2_with_registers.py
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import Optional
+from typing import Optional, Union
 
 import torch
 import torch.utils.checkpoint
@@ -305,7 +305,7 @@ class Dinov2WithRegistersPreTrainedModel(Dinov2PreTrainedModel):
 
             module.mask_token.data.zero_()
             module.register_tokens.data.zero_()
-        elif isinstance(module, Dinov2WithRegistersLayerScale):
+        elif isinstance(module, Dinov2WithRegistersLayerScale):  # noqa: F821
             module.lambda1.data.fill_(self.config.layerscale_value)
 
 
