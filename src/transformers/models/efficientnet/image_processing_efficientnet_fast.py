9456d3a4c431783df48d6cb49d4759d673316043
Zeeshan Khan Suri <zshn25@gmail.com>
2025-03-27 22:23:59 +0100

    ruff formatted

--- src/transformers/models/efficientnet/image_processing_efficientnet_fast.py
+++ src/transformers/models/efficientnet/image_processing_efficientnet_fast.py
@@ -32,7 +32,6 @@ if is_torch_available():
     import torch
 
 if is_torchvision_available():
-
     if is_torchvision_v2_available():
         from torchvision.transforms.v2 import functional as F
     else:
