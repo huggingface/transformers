207bb98ee79d43251e2b1399ef1f5caa9146231f
Korbinian Poeppel <korbinian.poeppel@nx-ai.com>
2025-03-27 09:28:12 +0000

    Chore: Remove unnecessary import.

--- src/transformers/models/xlstm/modeling_xlstm.py
+++ src/transformers/models/xlstm/modeling_xlstm.py
@@ -16,7 +16,6 @@ from ...utils import (
     add_code_sample_docstrings,
     add_start_docstrings,
     add_start_docstrings_to_model_forward,
-    is_torchdynamo_compiling,
     is_xlstm_available,
 )
 from .configuration_xlstm import xLSTMConfig
