bcbaf8a7a55b1c80e7bb8d4afb4122a47b2ad290
sushmanthreddy <sushmanthreddymereddy@gmail.com>
2025-03-28 15:31:36 +0530

    added some code

--- src/transformers/__init__.py
+++ src/transformers/__init__.py
@@ -1280,7 +1280,7 @@ else:
     _import_structure["models.got_ocr2"].extend(["GotOcr2ImageProcessor"])
     _import_structure["models.grounding_dino"].extend(["GroundingDinoImageProcessor"])
     _import_structure["models.idefics"].extend(["IdeficsImageProcessor"])
-    _import_structure["models.idefics2"].extend(["Idefics2ImageProcessor"])
+    _import_structure["models.idefics2"].extend(["Idefics2ImageProcessor", "Idefics2ImageProcessorFast"])
     _import_structure["models.idefics3"].extend(["Idefics3ImageProcessor"])
     _import_structure["models.imagegpt"].extend(["ImageGPTFeatureExtractor", "ImageGPTImageProcessor"])
     _import_structure["models.instructblipvideo"].extend(["InstructBlipVideoImageProcessor"])
@@ -6528,7 +6528,7 @@ if TYPE_CHECKING:
         from .models.got_ocr2 import GotOcr2ImageProcessor
         from .models.grounding_dino import GroundingDinoImageProcessor
         from .models.idefics import IdeficsImageProcessor
-        from .models.idefics2 import Idefics2ImageProcessor
+        from .models.idefics2 import Idefics2ImageProcessor, Idefics2ImageProcessorFast
         from .models.idefics3 import Idefics3ImageProcessor
         from .models.imagegpt import ImageGPTFeatureExtractor, ImageGPTImageProcessor
         from .models.instructblipvideo import InstructBlipVideoImageProcessor
