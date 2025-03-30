4bc2123e2b845045f19d8375003f51a4dd5c77d3
Avihu Dekel <avihu.dekel@ibm.com>
2025-03-30 06:41:30 -0400

    bugfix: input_feature_mask fix to get the exact number tokens.

--- src/transformers/models/granite_speech/feature_extraction_granite_speech.py
+++ src/transformers/models/granite_speech/feature_extraction_granite_speech.py
@@ -83,7 +83,7 @@ class GraniteSpeechFeatureExtractor(FeatureExtractionMixin):
         audio_embed_sizes = self._get_num_audio_features(audio_lengths)
         speech_inputs["audio_embed_sizes"] = audio_embed_sizes
         # todo: input_features_mask is not a great name, because input_features and input_features mask have different shapes (before/after the projector)
-        speech_inputs["input_features_mask"] = torch.arange(max(audio_embed_sizes)).view(1, -1) <= torch.tensor(
+        speech_inputs["input_features_mask"] = torch.arange(max(audio_embed_sizes)).view(1, -1) < torch.tensor(
             audio_embed_sizes
         ).view(-1, 1)
         return BatchFeature(data=speech_inputs)
