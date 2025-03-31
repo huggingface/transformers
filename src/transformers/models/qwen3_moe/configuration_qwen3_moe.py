6acd5aecb34b579729c29a26aa470f92081d88fa
Bo Zheng <bzheng@ir.hit.edu.cn>
2025-03-31 15:50:49 +0800

    Adding Qwen3 and Qwen3MoE (#36878)
    * Initial commit for Qwen3
    
    * fix and add tests for qwen3 & qwen3_moe
    
    * rename models for tests.
    
    * fix
    
    * fix
    
    * fix and add docs.
    
    * fix model name in docs.
    
    * simplify modular and fix configuration issues
    
    * Fix the red CI: ruff was updated
    
    * revert ruff, version was wrong
    
    * fix qwen3moe.
    
    * fix
    
    * make sure MOE can load
    
    * fix copies
    
    ---------
    
    Co-authored-by: Arthur Zucker <arthur.zucker@gmail.com>

--- src/transformers/models/qwen3_moe/configuration_qwen3_moe.py
+++ src/transformers/models/qwen3_moe/configuration_qwen3_moe.py
@@ -12,7 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Qwen2MoE model configuration"""
+"""Qwen3MoE model configuration"""
 
 from ...configuration_utils import PretrainedConfig
 from ...modeling_rope_utils import rope_config_validation
@@ -22,11 +22,11 @@ from ...utils import logging
 logger = logging.get_logger(__name__)
 
 
-class Qwen2MoeConfig(PretrainedConfig):
+class Qwen3MoeConfig(PretrainedConfig):
     r"""
-    This is the configuration class to store the configuration of a [`Qwen2MoeModel`]. It is used to instantiate a
-    Qwen2MoE model according to the specified arguments, defining the model architecture. Instantiating a configuration
-    with the defaults will yield a similar configuration to that of [Qwen/Qwen1.5-MoE-A2.7B](https://huggingface.co/Qwen/Qwen1.5-MoE-A2.7B).
+    This is the configuration class to store the configuration of a [`Qwen3MoeModel`]. It is used to instantiate a
+    Qwen3MoE model according to the specified arguments, defining the model architecture. Instantiating a configuration
+    with the defaults will yield a similar configuration to that of [Qwen/Qwen3-MoE-15B-A2B](https://huggingface.co/Qwen/Qwen3-15B-A2B).
 
     Configuration objects inherit from [`PretrainedConfig`] and can be used to control the model outputs. Read the
     documentation from [`PretrainedConfig`] for more information.
@@ -34,17 +34,17 @@ class Qwen2MoeConfig(PretrainedConfig):
 
     Args:
         vocab_size (`int`, *optional*, defaults to 151936):
-            Vocabulary size of the Qwen2MoE model. Defines the number of different tokens that can be represented by the
-            `inputs_ids` passed when calling [`Qwen2MoeModel`]
+            Vocabulary size of the Qwen3MoE model. Defines the number of different tokens that can be represented by the
+            `inputs_ids` passed when calling [`Qwen3MoeModel`]
         hidden_size (`int`, *optional*, defaults to 2048):
             Dimension of the hidden representations.
-        intermediate_size (`int`, *optional*, defaults to 5632):
+        intermediate_size (`int`, *optional*, defaults to 6144):
             Dimension of the MLP representations.
         num_hidden_layers (`int`, *optional*, defaults to 24):
             Number of hidden layers in the Transformer encoder.
-        num_attention_heads (`int`, *optional*, defaults to 16):
+        num_attention_heads (`int`, *optional*, defaults to 32):
             Number of attention heads for each attention layer in the Transformer encoder.
-        num_key_value_heads (`int`, *optional*, defaults to 16):
+        num_key_value_heads (`int`, *optional*, defaults to 4):
             This is the number of key_value heads that should be used to implement Grouped Query Attention. If
             `num_key_value_heads=num_attention_heads`, the model will use Multi Head Attention (MHA), if
             `num_key_value_heads=1` the model will use Multi Query Attention (MQA) otherwise GQA is used. When
@@ -103,6 +103,8 @@ class Qwen2MoeConfig(PretrainedConfig):
                     Only used with 'llama3'. Scaling factor applied to low frequency components of the RoPE
                 `high_freq_factor` (`float`, *optional*):
                     Only used with 'llama3'. Scaling factor applied to high frequency components of the RoPE
+        attention_bias (`bool`, defaults to `False`, *optional*, defaults to `False`):
+            Whether to use a bias in the query, key, value and output projection layers during self-attention.
         use_sliding_window (`bool`, *optional*, defaults to `False`):
             Whether to use sliding window attention.
         sliding_window (`int`, *optional*, defaults to 4096):
@@ -113,44 +115,41 @@ class Qwen2MoeConfig(PretrainedConfig):
             The dropout ratio for the attention probabilities.
         decoder_sparse_step (`int`, *optional*, defaults to 1):
             The frequency of the MoE layer.
-        moe_intermediate_size (`int`, *optional*, defaults to 1408):
+        moe_intermediate_size (`int`, *optional*, defaults to 768):
             Intermediate size of the routed expert.
-        shared_expert_intermediate_size (`int`, *optional*, defaults to 5632):
-            Intermediate size of the shared expert.
-        num_experts_per_tok (`int`, *optional*, defaults to 4):
+        num_experts_per_tok (`int`, *optional*, defaults to 8):
             Number of selected experts.
-        num_experts (`int`, *optional*, defaults to 60):
+        num_experts (`int`, *optional*, defaults to 128):
             Number of routed experts.
         norm_topk_prob (`bool`, *optional*, defaults to `False`):
             Whether to normalize the topk probabilities.
         output_router_logits (`bool`, *optional*, defaults to `False`):
-            Whether or not the router logits should be returned by the model. Enabling this will also
+            Whether or not the router logits should be returned by the model. Enabeling this will also
             allow the model to output the auxiliary loss, including load balancing loss and router z-loss.
         router_aux_loss_coef (`float`, *optional*, defaults to 0.001):
             The aux loss factor for the total loss.
         mlp_only_layers (`List[int]`, *optional*, defaults to `[]`):
-            Indicate which layers use Qwen2MoeMLP rather than Qwen2MoeSparseMoeBlock
+            Indicate which layers use Qwen3MoeMLP rather than Qwen3MoeSparseMoeBlock
             The list contains layer index, from 0 to num_layers-1 if we have num_layers layers
             If `mlp_only_layers` is empty, `decoder_sparse_step` is used to determine the sparsity.
-        qkv_bias (`bool`, *optional*, defaults to `True`):
-            Whether to add a bias to the queries, keys and values.
+
     ```python
-    >>> from transformers import Qwen2MoeModel, Qwen2MoeConfig
+    >>> from transformers import Qwen3MoeModel, Qwen3MoeConfig
 
-    >>> # Initializing a Qwen2MoE style configuration
-    >>> configuration = Qwen2MoeConfig()
+    >>> # Initializing a Qwen3MoE style configuration
+    >>> configuration = Qwen3MoeConfig()
 
-    >>> # Initializing a model from the Qwen1.5-MoE-A2.7B" style configuration
-    >>> model = Qwen2MoeModel(configuration)
+    >>> # Initializing a model from the Qwen3-15B-A2B" style configuration
+    >>> model = Qwen3MoeModel(configuration)
 
     >>> # Accessing the model configuration
     >>> configuration = model.config
     ```"""
 
-    model_type = "qwen2_moe"
+    model_type = "qwen3_moe"
     keys_to_ignore_at_inference = ["past_key_values"]
 
-    # Default tensor parallel plan for base model `Qwen2Moe`
+    # Default tensor parallel plan for base model `Qwen3Moe`
     base_model_tp_plan = {
         "layers.*.self_attn.q_proj": "colwise",
         "layers.*.self_attn.k_proj": "colwise",
@@ -170,10 +169,10 @@ class Qwen2MoeConfig(PretrainedConfig):
         self,
         vocab_size=151936,
         hidden_size=2048,
-        intermediate_size=5632,
+        intermediate_size=6144,
         num_hidden_layers=24,
-        num_attention_heads=16,
-        num_key_value_heads=16,
+        num_attention_heads=32,
+        num_key_value_heads=4,
         hidden_act="silu",
         max_position_embeddings=32768,
         initializer_range=0.02,
@@ -182,20 +181,19 @@ class Qwen2MoeConfig(PretrainedConfig):
         tie_word_embeddings=False,
         rope_theta=10000.0,
         rope_scaling=None,
+        attention_bias=False,
         use_sliding_window=False,
         sliding_window=4096,
         max_window_layers=28,
         attention_dropout=0.0,
         decoder_sparse_step=1,
-        moe_intermediate_size=1408,
-        shared_expert_intermediate_size=5632,
-        num_experts_per_tok=4,
-        num_experts=60,
+        moe_intermediate_size=768,
+        num_experts_per_tok=8,
+        num_experts=128,
         norm_topk_prob=False,
         output_router_logits=False,
         router_aux_loss_coef=0.001,
         mlp_only_layers=None,
-        qkv_bias=True,
         **kwargs,
     ):
         self.vocab_size = vocab_size
@@ -215,6 +213,7 @@ class Qwen2MoeConfig(PretrainedConfig):
         self.use_cache = use_cache
         self.rope_theta = rope_theta
         self.rope_scaling = rope_scaling
+        self.attention_bias = attention_bias
         self.attention_dropout = attention_dropout
         # Validate the correctness of rotary position embeddings parameters
         # BC: if there is a 'type' field, move it to 'rope_type'.
@@ -225,14 +224,12 @@ class Qwen2MoeConfig(PretrainedConfig):
         # MoE arguments
         self.decoder_sparse_step = decoder_sparse_step
         self.moe_intermediate_size = moe_intermediate_size
-        self.shared_expert_intermediate_size = shared_expert_intermediate_size
         self.num_experts_per_tok = num_experts_per_tok
         self.num_experts = num_experts
         self.norm_topk_prob = norm_topk_prob
         self.output_router_logits = output_router_logits
         self.router_aux_loss_coef = router_aux_loss_coef
         self.mlp_only_layers = [] if mlp_only_layers is None else mlp_only_layers
-        self.qkv_bias = qkv_bias
 
         super().__init__(
             tie_word_embeddings=tie_word_embeddings,
@@ -240,4 +237,4 @@ class Qwen2MoeConfig(PretrainedConfig):
         )
 
 
-__all__ = ["Qwen2MoeConfig"]
+__all__ = ["Qwen3MoeConfig"]
