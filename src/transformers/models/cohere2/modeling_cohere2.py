db059c6353b77b0b3dbd001ed5fe9f06e87ebd96
efsotr <104755879+efsotr@users.noreply.github.com>
2025-03-31 16:53:02 +0800

    Support passing flash_attn_kwargs when gradient_checkpointing is enabled (#37037)
    * support passing flash_attn_kwargs when gradient_checkpointing is enabled
    
    * make modeling_deepspeek_v3.py consistent with modular_deepseek_v3.py

--- src/transformers/models/cohere2/modeling_cohere2.py
+++ src/transformers/models/cohere2/modeling_cohere2.py
@@ -19,6 +19,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+from functools import partial
 from typing import Callable, List, Optional, Tuple, Union
 
 import torch
@@ -634,7 +635,7 @@ class Cohere2Model(Cohere2PreTrainedModel):
 
             if self.gradient_checkpointing and self.training:
                 layer_outputs = self._gradient_checkpointing_func(
-                    decoder_layer.__call__,
+                    partial(decoder_layer.__call__, **flash_attn_kwargs),
                     hidden_states,
                     position_embeddings,
                     causal_mask,
