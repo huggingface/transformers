2a53bf493d36cde48dd8a6fb7d53ea52df665879
ydshieh <ydshieh@users.noreply.github.com>
2025-04-01 16:52:57 +0200

    cls

--- utils/fetch_hub_objects_for_ci.py
+++ utils/fetch_hub_objects_for_ci.py
@@ -6,3 +6,4 @@ if __name__ == "__main__":
         import datasets
 
         _ = datasets.load_dataset("hf-internal-testing/librispeech_asr_dummy", "clean", split="validation")
+        _ = datasets.load_dataset("hf-internal-testing/fixtures_image_utils", split="test", revision="refs/pr/1")
