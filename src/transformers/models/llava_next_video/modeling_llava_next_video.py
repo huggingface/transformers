55b99c5e515fc01051e4d555ca517728883dfdaa
Cyril Vallez <cyril.vallez@gmail.com>
2025-04-01 10:16:47 +0200

    test

--- src/transformers/models/llava_next_video/modeling_llava_next_video.py
+++ src/transformers/models/llava_next_video/modeling_llava_next_video.py
@@ -171,18 +171,14 @@ class LlavaNextVideoPreTrainedModel(PreTrainedModel):
             if hasattr(self.config, "initializer_range")
             else self.config.text_config.initializer_range
         )
-
-        if hasattr(module, "class_embedding"):
-            module.class_embedding.data.normal_(mean=0.0, std=std)
-
-        if isinstance(module, (nn.Linear, nn.Conv2d)):
+        if module in self.vision_tower.modules():
+            self.vision_tower._init_weights(module)
+        elif module in self.language_model.modules():
+            self.language_model._init_weights(module)
+        elif isinstance(module, nn.Linear):
             module.weight.data.normal_(mean=0.0, std=std)
             if module.bias is not None:
                 module.bias.data.zero_()
-        elif isinstance(module, nn.Embedding):
-            module.weight.data.normal_(mean=0.0, std=std)
-            if module.padding_idx is not None:
-                module.weight.data[module.padding_idx].zero_()
 
 
 class LlavaNextVideoMultiModalProjector(nn.Module):
