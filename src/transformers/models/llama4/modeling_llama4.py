ed6cba8756617812fd51242ffcde5fe83491d32a
Marc Sun <marc@huggingface.co>
2025-04-03 11:42:03 +0000

    rm

--- src/transformers/models/llama4/modeling_llama4.py
+++ src/transformers/models/llama4/modeling_llama4.py
@@ -876,7 +876,7 @@ class Llama4ForCausalLM(Llama4PreTrainedModel, GenerationMixin):
     _tp_plan = {"lm_head": "colwise_rep"}
     config_class = Llama4TextConfig
 
-    def __init__(self, config: Llama4TextConfig):
+    def __init__(self, config):
         super().__init__(config)
         self.model = Llama4TextModel(config)
         self.vocab_size = config.vocab_size
