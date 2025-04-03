5adc0a9783ad52d3529aa3582188b97c08a9e2a6
Alex-Brooks <Alex.Brooks@ibm.com>
2025-04-03 12:39:29 +0000

    Update init structure to mirror llama

--- src/transformers/models/granite_speech/__init__.py
+++ src/transformers/models/granite_speech/__init__.py
@@ -1,4 +1,4 @@
-# Copyright 2025 EleutherAI and The HuggingFace Inc. team. All rights reserved.
+# Copyright 2025 The HuggingFace Team. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,76 +13,17 @@
 # limitations under the License.
 from typing import TYPE_CHECKING
 
-from ...utils import (
-    OptionalDependencyNotAvailable,
-    _LazyModule,
-    is_torch_available,
-    is_torchaudio_available,
-)
+from ...utils import _LazyModule
 from ...utils.import_utils import define_import_structure
 
 
-_import_structure = {
-    "configuration_granite_speech": [
-        "GraniteSpeechConfig",
-        "GraniteSpeechEncoderConfig",
-        "GraniteSpeechProjectorConfig",
-    ],
-}
-
-try:
-    if not is_torch_available():
-        raise OptionalDependencyNotAvailable()
-except OptionalDependencyNotAvailable:
-    pass
-else:
-    _import_structure["modeling_granite_speech"] = [
-        "GraniteSpeechForConditionalGeneration",
-        "GraniteSpeechPreTrainedModel",
-        "GraniteSpeechEncoderProjectorPreTrainedModel",
-        "GraniteSpeechQFormerModel",
-    ]
-
-try:
-    if not is_torchaudio_available():
-        raise OptionalDependencyNotAvailable()
-except OptionalDependencyNotAvailable:
-    pass
-else:
-    _import_structure["feature_extraction_granite_speech"] = ["GraniteSpeechFeatureExtractor"]
-    _import_structure["processing_granite_speech"] = ["GraniteSpeechProcessor"]
-
-
 if TYPE_CHECKING:
-    from .configuration_granite_speech import (
-        GraniteSpeechConfig,
-        GraniteSpeechEncoderConfig,
-        GraniteSpeechProjectorConfig,
-    )
-
-    try:
-        if not is_torch_available():
-            raise OptionalDependencyNotAvailable()
-    except OptionalDependencyNotAvailable:
-        pass
-    else:
-        from .modeling_granite_speech import (
-            GraniteSpeechEncoderProjectorPreTrainedModel,
-            GraniteSpeechForConditionalGeneration,
-            GraniteSpeechPreTrainedModel,
-            GraniteSpeechQFormerModel,
-        )
-
-    try:
-        if not is_torchaudio_available():
-            raise OptionalDependencyNotAvailable()
-    except OptionalDependencyNotAvailable:
-        pass
-    else:
-        from .feature_extraction_granite_speech import GraniteSpeechFeatureExtractor
-        from .processing_granite_speech import GraniteSpeechProcessor
+    from .configuration_granite_speech import *
+    from .feature_extraction_granite_speech import *
+    from .modeling_granite_speech import *
+    from .processing_granite_speech import *
 else:
     import sys
 
     _file = globals()["__file__"]
-    sys.modules[__name__] = _LazyModule(__name__, _file, _import_structure, module_spec=__spec__)
+    sys.modules[__name__] = _LazyModule(__name__, _file, define_import_structure(_file), module_spec=__spec__)
