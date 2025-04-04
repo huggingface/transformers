ab4c80d6995fde32cf0f60305841d8ad9b9388c9
byi8220 <byi8220@gmail.com>
2025-04-04 14:11:37 -0400

    Disable delay_optimizer_creation in `Trainer` to support fsdp2 (#37147)
    * github why you do this
    
    * fix
    
    * make fixup
    
    * disable cpu offload test
    
    * fixup
    
    * tmp reworks
    
    * git branch movement
    
    * make fixup
    
    * add require_fsdp_v2_version
    
    * dep issues
    
    * update ruff and fixup

--- src/transformers/trainer.py
+++ src/transformers/trainer.py
@@ -2313,6 +2313,11 @@ class Trainer:
 
         delay_optimizer_creation = is_sagemaker_mp_enabled() or self.is_fsdp_xla_enabled or self.is_fsdp_enabled
 
+        # Can't delay optimizer creation when using FSDP2: https://github.com/huggingface/accelerate/blob/3f636d626063ffcf9a337c7d3624d61b7d187d59/src/accelerate/accelerator.py#L1404
+        is_fsdp2 = self.is_fsdp_enabled and (getattr(self.accelerator.state.fsdp_plugin, "fsdp_version", 1) == 2)
+        if is_fsdp2:
+            delay_optimizer_creation = False
+
         # We need to reset the scheduler, as its parameters may be different on subsequent calls
         if self._created_lr_scheduler:
             self.lr_scheduler = None
