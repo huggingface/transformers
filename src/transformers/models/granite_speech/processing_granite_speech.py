ff5869f1dd53aa55ba7d2b8b5c9cd6563fd43c7f
Alex-Brooks <Alex.Brooks@ibm.com>
2025-04-04 06:06:55 +0000

    Fix typos, add license

--- src/transformers/models/granite_speech/processing_granite_speech.py
+++ src/transformers/models/granite_speech/processing_granite_speech.py
@@ -12,9 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""
-Processor class for Granite Speech.
-"""
+"""Processor class for Granite Speech."""
 
 from typing import List, Union
 
