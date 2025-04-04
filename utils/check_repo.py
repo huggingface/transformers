d8aceb2f66b48f462dfab7c4a58d64176bd8b002
Joao Gante <joaofranciscocardosogante@gmail.com>
2025-04-04 14:27:28 +0100

    [RoPE] abstract dynamic RoPE update under a decorator ✨  (#37249)
    * dynamic rope decorator
    
    * longrope; shorter fwd pass
    
    * propper docstring
    
    * make fixup

--- utils/check_repo.py
+++ utils/check_repo.py
@@ -1083,8 +1083,7 @@ def check_all_objects_are_documented():
     undocumented_objs = [c for c in objects if c not in documented_objs and not ignore_undocumented(c)]
     if len(undocumented_objs) > 0:
         raise Exception(
-            "The following objects are in the public init so should be documented:\n - "
-            + "\n - ".join(undocumented_objs)
+            "The following objects are in the public init, but not in the docs:\n - " + "\n - ".join(undocumented_objs)
         )
     check_model_type_doc_match()
     check_public_method_exists(documented_methods_map)
