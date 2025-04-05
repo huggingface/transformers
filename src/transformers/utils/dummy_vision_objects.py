d1b92369ca193da49f9f7ecd01b08ece45c2c9aa
Lysandre <hi@lysand.re>
2025-04-05 22:04:21 +0200

    v4.52.0.dev0

--- src/transformers/utils/dummy_vision_objects.py
+++ src/transformers/utils/dummy_vision_objects.py
@@ -408,13 +408,6 @@ class LevitImageProcessor(metaclass=DummyObject):
         requires_backends(self, ["vision"])
 
 
-class Llama4ImageProcessor(metaclass=DummyObject):
-    _backends = ["vision"]
-
-    def __init__(self, *args, **kwargs):
-        requires_backends(self, ["vision"])
-
-
 class LlavaImageProcessor(metaclass=DummyObject):
     _backends = ["vision"]
 
