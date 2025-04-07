dd06245d94d2f0074d0fb7654b8aeaf0b5564d99
Wing Lian <wing@axolotl.ai>
2025-04-07 06:44:42 -0400

    use target_length instead of guearded past_key_values

--- src/transformers/models/llama4/modeling_llama4.py
+++ src/transformers/models/llama4/modeling_llama4.py
@@ -768,7 +768,7 @@ class Llama4TextModel(Llama4PreTrainedModel):
         )
 
         if past_key_values is not None and past_key_values.is_compileable:
-            target_length = past_key_values.get_max_cache_shape
+            target_length = past_key_values.get_max_cache_shape()
         else:
             target_length = attention_mask.shape[-1] if attention_mask is not None else sequence_length
 
@@ -781,7 +781,7 @@ class Llama4TextModel(Llama4PreTrainedModel):
                 attention_mask = make_flex_block_causal_mask(
                     attention_mask,
                     query_length=sequence_length,
-                    key_length=past_key_values.get_max_cache_shape() if past_key_values else sequence_length,
+                    key_length=target_length,
                     offsets=None if sequence_length != 1 else (first_cache_position, 0),
                 )
                 return attention_mask, chunked_attention_mask
