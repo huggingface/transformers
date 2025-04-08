dff2e53afda4e240fbab8f4cb46911e85815398f
Vladislav <vladislav.bronzov@gmail.com>
2025-04-08 13:53:55 +0200

    skip multi gpu for d-fine

--- tests/models/d_fine/test_modeling_d_fine.py
+++ tests/models/d_fine/test_modeling_d_fine.py
@@ -331,6 +331,10 @@ class DFineModelTest(ModelTesterMixin, PipelineTesterMixin, unittest.TestCase):
         config_and_inputs = self.model_tester.prepare_config_and_inputs()
         self.model_tester.create_and_check_d_fine_object_detection_head_model(*config_and_inputs)
 
+    @unittest.skip(reason="DFine doesn't work well with `nn.DataParallel")
+    def test_multi_gpu_data_parallel_forward(self):
+        pass
+
     @unittest.skip(reason="DFine does not use inputs_embeds")
     def test_inputs_embeds(self):
         pass
