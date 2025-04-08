cc8a050924623e1617b2a9e9d8c194770941639e
Arthur Zucker <arthur.zucker@gmail.com>
2025-04-08 08:58:28 +0000

    final sytle

--- src/transformers/integrations/flex_attention.py
+++ src/transformers/integrations/flex_attention.py
@@ -66,7 +66,6 @@ class WrappedFlexAttention:
             # cause errors. The suggested fix is to compile with "max-autotune-no-cudagraphs"
             # see https://github.com/pytorch/pytorch/issues/146260 for training
             if _torch_version == "2.6.0" and training:
-              
                 self._compiled_flex_attention = torch.compile(
                     flex_attention, dynamic=False, mode="max-autotune-no-cudagraphs"
                 )
