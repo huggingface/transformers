4333431fd2158809691956a37dc2c0c2b570afe4
MekkCyber <mekk.cyber@gmail.com>
2025-04-09 16:37:25 +0000

    rm print

--- src/transformers/quantizers/quantizer_fbgemm_fp8.py
+++ src/transformers/quantizers/quantizer_fbgemm_fp8.py
@@ -272,10 +272,8 @@ class FbgemmFp8HfQuantizer(HfQuantizer):
             "layers.*.feed_forward.experts.down_proj": "local_colwise",
         }
         if config.get_text_config() is not None:
-            print("config.get_text_config(). ", config.get_text_config().base_model_tp_plan)
             config.get_text_config().base_model_tp_plan = text_plan
         else:
-            print("config.base_model_tp_plan", config.base_model_tp_plan)
             config.base_model_tp_plan = text_plan
         return config
 
