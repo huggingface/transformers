ecc920e0fe09abcde4444eb7cd675ef34e0d01c2
raushan <raushan@huggingface.co>
2025-04-11 17:43:46 +0200

    fixup

--- src/transformers/models/qwen2_5_omni/modeling_qwen2_5_omni.py
+++ src/transformers/models/qwen2_5_omni/modeling_qwen2_5_omni.py
@@ -2101,7 +2101,7 @@ class Qwen2_5OmniThinkerTextModel(Qwen2_5OmniPreTrainedModel):
         if (
             self.config._attn_implementation == "sdpa"
             and attention_mask is not None
-            and attention_mask.device.type in ["cuda", "xpu"]
+            and attention_mask.device.type in ["cuda", "xpu", "npu"]
             and not output_attentions
         ):
             # Attend to all tokens in fully masked rows in the causal_mask, for example the relevant first rows when
@@ -2813,7 +2813,7 @@ class Qwen2_5OmniTalkerModel(Qwen2_5OmniPreTrainedModel):
         if (
             self.config._attn_implementation == "sdpa"
             and attention_mask is not None
-            and attention_mask.device.type in ["cuda", "xpu"]
+            and attention_mask.device.type in ["cuda", "xpu", "npu"]
             and not output_attentions
         ):
             # Attend to all tokens in fully masked rows in the causal_mask, for example the relevant first rows when
