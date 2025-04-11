6f888dcf44968efc12804b5733ba4367574f422e
steven <steven.bucaille@gmail.com>
2025-04-11 22:21:09 +0200

    chore: changed convert_lightglue_to_hf script to comply with new standard

--- src/transformers/models/lightglue/convert_lightglue_to_hf.py
+++ src/transformers/models/lightglue/convert_lightglue_to_hf.py
@@ -12,320 +12,182 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import argparse
-from pathlib import Path
-from typing import Tuple
+import gc
+import os
+import re
+from typing import List
 
 import torch
-from datasets import load_dataset
 
+from tests.models.lightglue.test_modeling_lightglue import prepare_imgs
 from transformers import (
-    AutoConfig,
     AutoModelForKeypointDetection,
     LightGlueForKeypointMatching,
     LightGlueImageProcessor,
 )
-
-# SuperGlueConfig,
-# SuperGlueForKeypointMatching,
-# SuperGlueImageProcessor,
 from transformers.models.lightglue.configuration_lightglue import LightGlueConfig
-from transformers.models.superpoint.modeling_superpoint import SuperPointKeypointDescriptionOutput
-from transformers.utils import is_flash_attn_2_available
 
 
-def get_lightglue_config():
-    config = LightGlueConfig()
+def verify_model_outputs(model):
+    images = prepare_imgs()
+    preprocessor = LightGlueImageProcessor()
+    inputs = preprocessor(images=images, return_tensors="pt").to("cuda")
+    model.to("cuda")
+    with torch.no_grad():
+        outputs = model(**inputs, output_hidden_states=True, output_attentions=True)
 
-    return config
+    predicted_matches_values = outputs.matches[0, 0, 20:30]
+    predicted_matching_scores_values = outputs.matching_scores[0, 0, 20:30]
 
+    predicted_number_of_matches = torch.sum(outputs.matches[0][0] != -1).item()
 
-def create_rename_keys(config, state_dict):
-    rename_keys = []
+    expected_max_number_keypoints = 866
+    expected_matches_shape = torch.Size((len(images), 2, expected_max_number_keypoints))
+    expected_matching_scores_shape = torch.Size((len(images), 2, expected_max_number_keypoints))
 
-    # positional encoder
-    rename_keys.append(
-        (
-            "posenc.Wr.weight",
-            "positional_encoder.projector.weight",
-        )
+    expected_matches_values = torch.tensor([-1, -1, -1, -1, -1, -1, -1, -1, 540, -1], dtype=torch.int64).to("cuda")
+    expected_matching_scores_values = torch.tensor([0, 0, 0.0167, 0.0304, 0.0328, 0, 0, 0.0095, 0.2964, 0.0352]).to(
+        "cuda"
     )
 
-    for i in range(config.num_layers):
-        # self attention blocks
-        rename_keys.append(
-            (
-                f"self_attn.{i}.Wqkv.weight",
-                f"transformer_layers.{i}.self_attention_block.Wqkv.weight",
-            )
-        )
-        rename_keys.append(
-            (
-                f"self_attn.{i}.Wqkv.bias",
-                f"transformer_layers.{i}.self_attention_block.Wqkv.bias",
-            )
-        )
-        rename_keys.append(
-            (
-                f"self_attn.{i}.out_proj.weight",
-                f"transformer_layers.{i}.self_attention_block.output_projection.weight",
-            )
-        )
-        rename_keys.append(
-            (
-                f"self_attn.{i}.out_proj.bias",
-                f"transformer_layers.{i}.self_attention_block.output_projection.bias",
-            )
-        )
-        for j in [0, 1, 3]:
-            rename_keys.append(
-                (
-                    f"self_attn.{i}.ffn.{j}.weight",
-                    f"transformer_layers.{i}.self_attention_block.ffn.{j}.weight",
-                )
-            )
-            rename_keys.append(
-                (
-                    f"self_attn.{i}.ffn.{j}.bias",
-                    f"transformer_layers.{i}.self_attention_block.ffn.{j}.bias",
-                )
-            )
+    expected_number_of_matches = 127
 
-        # cross attention blocks
-        rename_keys.append(
-            (
-                f"cross_attn.{i}.to_qk.weight",
-                f"transformer_layers.{i}.cross_attention_block.to_qk.weight",
-            )
-        )
-        rename_keys.append(
-            (
-                f"cross_attn.{i}.to_qk.bias",
-                f"transformer_layers.{i}.cross_attention_block.to_qk.bias",
-            )
-        )
-        rename_keys.append(
-            (
-                f"cross_attn.{i}.to_v.weight",
-                f"transformer_layers.{i}.cross_attention_block.to_v.weight",
-            )
-        )
-        rename_keys.append(
-            (
-                f"cross_attn.{i}.to_v.bias",
-                f"transformer_layers.{i}.cross_attention_block.to_v.bias",
-            )
-        )
-        rename_keys.append(
-            (
-                f"cross_attn.{i}.to_out.weight",
-                f"transformer_layers.{i}.cross_attention_block.to_out.weight",
-            )
-        )
-        rename_keys.append(
-            (
-                f"cross_attn.{i}.to_out.bias",
-                f"transformer_layers.{i}.cross_attention_block.to_out.bias",
-            )
-        )
+    assert outputs.matches.shape == expected_matches_shape
+    assert outputs.matching_scores.shape == expected_matching_scores_shape
 
-        for j in [0, 1, 3]:
-            rename_keys.append(
-                (
-                    f"cross_attn.{i}.ffn.{j}.weight",
-                    f"transformer_layers.{i}.cross_attention_block.ffn.{j}.weight",
-                )
-            )
-            rename_keys.append(
-                (
-                    f"cross_attn.{i}.ffn.{j}.bias",
-                    f"transformer_layers.{i}.cross_attention_block.ffn.{j}.bias",
-                )
-            )
+    assert torch.allclose(predicted_matches_values, expected_matches_values, atol=1e-4)
+    assert torch.allclose(predicted_matching_scores_values, expected_matching_scores_values, atol=1e-4)
 
-        # Match assignment layers
-        rename_keys.append(
-            (
-                f"log_assignment.{i}.matchability.weight",
-                f"match_assignment_layers.{i}.matchability.weight",
-            )
-        )
-        rename_keys.append(
-            (
-                f"log_assignment.{i}.matchability.bias",
-                f"match_assignment_layers.{i}.matchability.bias",
-            )
-        )
-        rename_keys.append(
-            (
-                f"log_assignment.{i}.final_proj.weight",
-                f"match_assignment_layers.{i}.final_projection.weight",
-            )
-        )
-        rename_keys.append(
-            (
-                f"log_assignment.{i}.final_proj.bias",
-                f"match_assignment_layers.{i}.final_projection.bias",
-            )
-        )
-        if i < config.num_layers - 1:
-            rename_keys.append(
-                (
-                    f"token_confidence.{i}.token.0.weight",
-                    f"token_confidence.{i}.token.weight",
-                )
-            )
-            rename_keys.append(
-                (
-                    f"token_confidence.{i}.token.0.bias",
-                    f"token_confidence.{i}.token.bias",
-                )
-            )
-
-    return rename_keys
+    assert predicted_number_of_matches == expected_number_of_matches
 
 
-def rename_key(dct, old, new):
-    val = dct.pop(old)
-    dct[new] = val
+ORIGINAL_TO_CONVERTED_KEY_MAPPING = {
+    r"posenc.Wr.weight": r"positional_encoder.projector.weight",
+    r"self_attn.(\d+).Wqkv.weight": r"transformer_layers.\1.self_attention_block.Wqkv.weight",
+    r"self_attn.(\d+).Wqkv.bias": r"transformer_layers.\1.self_attention_block.Wqkv.bias",
+    r"self_attn.(\d+).out_proj.weight": r"transformer_layers.\1.self_attention_block.output_projection.weight",
+    r"self_attn.(\d+).out_proj.bias": r"transformer_layers.\1.self_attention_block.output_projection.bias",
+    r"self_attn.(\d+).ffn.(\d+).weight": r"transformer_layers.\1.self_attention_block.ffn.\2.weight",
+    r"self_attn.(\d+).ffn.(\d+).bias": r"transformer_layers.\1.self_attention_block.ffn.\2.bias",
+    r"cross_attn.(\d+).to_qk.weight": r"transformer_layers.\1.cross_attention_block.to_qk.weight",
+    r"cross_attn.(\d+).to_qk.bias": r"transformer_layers.\1.cross_attention_block.to_qk.bias",
+    r"cross_attn.(\d+).to_v.weight": r"transformer_layers.\1.cross_attention_block.to_v.weight",
+    r"cross_attn.(\d+).to_v.bias": r"transformer_layers.\1.cross_attention_block.to_v.bias",
+    r"cross_attn.(\d+).to_out.weight": r"transformer_layers.\1.cross_attention_block.to_out.weight",
+    r"cross_attn.(\d+).to_out.bias": r"transformer_layers.\1.cross_attention_block.to_out.bias",
+    r"cross_attn.(\d+).ffn.(\d+).weight": r"transformer_layers.\1.cross_attention_block.ffn.\2.weight",
+    r"cross_attn.(\d+).ffn.(\d+).bias": r"transformer_layers.\1.cross_attention_block.ffn.\2.bias",
+    r"log_assignment.(\d+).matchability.weight": r"match_assignment_layers.\1.matchability.weight",
+    r"log_assignment.(\d+).matchability.bias": r"match_assignment_layers.\1.matchability.bias",
+    r"log_assignment.(\d+).final_proj.weight": r"match_assignment_layers.\1.final_projection.weight",
+    r"log_assignment.(\d+).final_proj.bias": r"match_assignment_layers.\1.final_projection.bias",
+    r"token_confidence.(\d+).token.0.weight": r"token_confidence.\1.token.weight",
+    r"token_confidence.(\d+).token.0.bias": r"token_confidence.\1.token.bias",
+}
 
 
-def add_keypoint_detector_state_dict(lightglue_state_dict, keypoint_detector_state_dict):
+def convert_old_keys_to_new_keys(state_dict_keys: List[str]):
+    """
+    This function should be applied only once, on the concatenated keys to efficiently rename using
+    the key mappings.
+    """
+    output_dict = {}
+    if state_dict_keys is not None:
+        old_text = "\n".join(state_dict_keys)
+        new_text = old_text
+        for pattern, replacement in ORIGINAL_TO_CONVERTED_KEY_MAPPING.items():
+            if replacement is None:
+                new_text = re.sub(pattern, "", new_text)  # an empty line
+                continue
+            new_text = re.sub(pattern, replacement, new_text)
+        output_dict = dict(zip(old_text.split("\n"), new_text.split("\n")))
+    return output_dict
+
+
+def add_keypoint_detector_state_dict(lightglue_state_dict):
+    keypoint_detector = AutoModelForKeypointDetection.from_pretrained("magic-leap-community/superpoint")
+    keypoint_detector_state_dict = keypoint_detector.state_dict()
     for k, v in keypoint_detector_state_dict.items():
         lightglue_state_dict[f"keypoint_detector.{k}"] = v
     return lightglue_state_dict
 
 
-def prepare_imgs_for_image_processor():
-    dataset = load_dataset("stevenbucaille/image_matching_fixtures", split="train")
-    return [[dataset[0]["image"], dataset[1]["image"]]]
-
-
-def extract_keypoint_information_from_image_point_description_output(
-    output: SuperPointKeypointDescriptionOutput, i: int
-) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
-    indices = torch.nonzero(output.mask[i]).squeeze()
-    keypoints = torch.unsqueeze(output.keypoints[i][indices], dim=0)
-    descriptors = torch.unsqueeze(output.descriptors[i][indices], dim=0)
-    scores = torch.unsqueeze(output.scores[i][indices], dim=0)
-    return keypoints, descriptors, scores
-
-
 @torch.no_grad()
-def convert_lightglue_checkpoint(checkpoint_url, pytorch_dump_folder_path, save_model, push_to_hub):
-    """
-    Copy/paste/tweak model's weights to our SuperPoint structure.
-    Also test the model with the image processor and other methods of reading the images.
-    """
-    torch.set_printoptions(precision=10)
-    keypoint_detector_config = AutoConfig.from_pretrained("magic-leap-community/superpoint")
-    keypoint_detector_config.max_keypoints = 2048
-    keypoint_detector_config.keypoint_threshold = 0.0005
-    lightglue_config = get_lightglue_config()
-    lightglue_config.keypoint_detector_config = keypoint_detector_config
+def write_model(
+    model_path,
+    checkpoint_url,
+    safe_serialization=True,
+    push_to_hub=False,
+):
+    os.makedirs(model_path, exist_ok=True)
 
-    keypoint_detector = AutoModelForKeypointDetection.from_pretrained("magic-leap-community/superpoint")
-    keypoint_detector.config = keypoint_detector_config
-    keypoint_detector.to("cuda")
-    keypoint_detector.eval()
-    keypoint_detector_state_dict = keypoint_detector.state_dict()
+    # ------------------------------------------------------------
+    # SuperGlue config
+    # ------------------------------------------------------------
 
-    print("Downloading original model from checkpoint...")
-    original_lightglue_state_dict = torch.hub.load_state_dict_from_url(checkpoint_url)
+    config = LightGlueConfig(
+        descriptor_dim=256,
+        num_layers=9,
+        num_heads=4,
+    )
+    config.architectures = ["LightGlueForKeypointMatching"]
+    config.save_pretrained(model_path)
+    print("Model config saved successfully...")
 
-    print("Converting model parameters...")
-    rename_keys = create_rename_keys(lightglue_config, original_lightglue_state_dict)
-    new_lightglue_state_dict = original_lightglue_state_dict.copy()
-    for src, dest in rename_keys:
-        rename_key(new_lightglue_state_dict, src, dest)
-    new_lightglue_state_dict = add_keypoint_detector_state_dict(new_lightglue_state_dict, keypoint_detector_state_dict)
+    # ------------------------------------------------------------
+    # Convert weights
+    # ------------------------------------------------------------
 
-    expected_number_of_matches = 264
+    print(f"Fetching all parameters from the checkpoint at {checkpoint_url}...")
+    original_state_dict = torch.hub.load_state_dict_from_url(checkpoint_url)
 
-    # eager mode
-    lightglue_config._attn_implementation = "eager"
-    model = LightGlueForKeypointMatching(lightglue_config)
-    model.load_state_dict(new_lightglue_state_dict, strict=False, assign=False)
-    model.to("cuda")
-    model.eval()
-    print("Successfully loaded weights in the model")
+    print("Converting model...")
+    all_keys = list(original_state_dict.keys())
+    new_keys = convert_old_keys_to_new_keys(all_keys)
 
-    ## USE REGULAR IMAGE PROCESSOR FOR INFERENCE
-    images = prepare_imgs_for_image_processor()
-    preprocessor = LightGlueImageProcessor()
-    inputs = preprocessor(images=images, return_tensors="pt")
-    inputs.to("cuda")
-    torch.save(inputs["pixel_values"], "lightglue_input.pth")
+    state_dict = {}
+    for key in all_keys:
+        new_key = new_keys[key]
+        state_dict[new_key] = original_state_dict.pop(key).contiguous().clone()
 
-    output = model(**inputs, return_dict=True)
+    del original_state_dict
+    gc.collect()
+    state_dict = add_keypoint_detector_state_dict(state_dict)
 
-    print("Number of matching keypoints using image processor")
-    print(torch.sum(output.matches[0][0] != -1))
-    print(output.matches[0][0][:10])
-    assert expected_number_of_matches == torch.sum(output.matches[0][0] != -1)
+    print("Loading the checkpoint in a SuperGlue model...")
+    with torch.device("cuda"):
+        model = LightGlueForKeypointMatching(config)
+    model.load_state_dict(state_dict, strict=False)
+    print("Checkpoint loaded successfully...")
+    del model.config._name_or_path
 
-    if save_model:
-        Path(pytorch_dump_folder_path).mkdir(exist_ok=True)
-        print(f"Saving model to {pytorch_dump_folder_path}")
-        model.save_pretrained(pytorch_dump_folder_path)
-        preprocessor.save_pretrained(pytorch_dump_folder_path)
+    model_name = "lightglue"
+    print("Checking the model outputs...")
+    verify_model_outputs(model)
+    print("Model outputs verified successfully.")
 
-        if push_to_hub:
-            print("Pushing model to the hub...")
-            model_name = "lightglue"
+    organization = "stevenbucaille"
+    if push_to_hub:
+        print("Pushing model to the hub...")
+        model.push_to_hub(
+            repo_id=f"{organization}/{model_name}",
+            commit_message="Add model",
+        )
+        config.push_to_hub(repo_id=f"{organization}/{model_name}", commit_message="Add config")
 
-            model.push_to_hub(
-                repo_id=f"stevenbucaille/{model_name}",
-                commit_message="Add model",
-            )
-            preprocessor.push_to_hub(model_name)
+    write_image_processor(model_path, model_name, organization, push_to_hub=push_to_hub)
 
-    # sdpa mode
-    lightglue_config._attn_implementation = "sdpa"
-    model = LightGlueForKeypointMatching(lightglue_config)
-    model.load_state_dict(new_lightglue_state_dict, strict=False, assign=False)
-    model.to("cuda")
-    model.eval()
-    print("Successfully loaded weights in the model")
 
-    ## USE REGULAR IMAGE PROCESSOR FOR INFERENCE
-    images = prepare_imgs_for_image_processor()
-    preprocessor = LightGlueImageProcessor()
-    inputs = preprocessor(images=images, return_tensors="pt")
-    inputs.to("cuda")
-    torch.save(inputs["pixel_values"], "lightglue_input.pth")
+def write_image_processor(save_dir, model_name, organization, push_to_hub=False):
+    image_processor = LightGlueImageProcessor()
+    image_processor.save_pretrained(save_dir)
 
-    output = model(**inputs, return_dict=True)
-
-    print("Number of matching keypoints using image processor")
-    print(torch.sum(output.matches[0][0] != -1))
-    print(output.matches[0][0][:10])
-
-    assert expected_number_of_matches == torch.sum(output.matches[0][0] != -1)
-
-    if is_flash_attn_2_available():
-        # flash attention mode
-        lightglue_config._attn_implementation = "flash_attention_2"
-        with torch.autocast(device_type="cuda"):
-            model = LightGlueForKeypointMatching(lightglue_config)
-            model.load_state_dict(new_lightglue_state_dict, strict=False, assign=False)
-            model.to("cuda")
-            model.eval()
-            print("Successfully loaded weights in the model")
-
-            ## USE REGULAR IMAGE PROCESSOR FOR INFERENCE
-            images = prepare_imgs_for_image_processor()
-            preprocessor = LightGlueImageProcessor()
-            inputs = preprocessor(images=images, return_tensors="pt")
-            inputs.to("cuda")
-            torch.save(inputs["pixel_values"], "lightglue_input.pth")
-            with torch.backends.cuda.sdp_kernel(enable_flash=True, enable_math=False, enable_mem_efficient=False):
-                output = model(**inputs, return_dict=True)
-
-            print("Number of matching keypoints using image processor")
-            print(torch.sum(output.matches[0][0] != -1))
-            print(output.matches[0][0][:10])
-
-            assert expected_number_of_matches == torch.sum(output.matches[0][0] != -1)
+    if push_to_hub:
+        print("Pushing image processor to the hub...")
+        image_processor.push_to_hub(
+            repo_id=f"{organization}/{model_name}",
+            commit_message="Add image processor",
+        )
 
 
 if __name__ == "__main__":
@@ -352,9 +214,6 @@ if __name__ == "__main__":
     )
 
     args = parser.parse_args()
-    convert_lightglue_checkpoint(
-        args.checkpoint_url,
-        args.pytorch_dump_folder_path,
-        args.save_model,
-        args.push_to_hub,
+    write_model(
+        args.pytorch_dump_folder_path, args.checkpoint_url, safe_serialization=True, push_to_hub=args.push_to_hub
     )
