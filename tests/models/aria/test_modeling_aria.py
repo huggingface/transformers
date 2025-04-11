6a79f356de63d0950ced2c62a423e9d958b603f5
jiqing-feng <jiqing.feng@intel.com>
2025-04-11 08:04:02 -0400

    add cuda tests
    Signed-off-by: jiqing-feng <jiqing.feng@intel.com>

--- tests/models/aria/test_modeling_aria.py
+++ tests/models/aria/test_modeling_aria.py
@@ -467,6 +467,10 @@ class AriaForConditionalGenerationIntegrationTest(unittest.TestCase):
             "cpu": [
                 "<|im_start|>user\n<fim_prefix><fim_suffix> <image>\n <image>\n USER: What's the difference of two images?\n ASSISTANT:<fim_prefix><fim_suffix> <image>\n USER: Describe the image.\n ASSISTANT:<|im_end|>\n <|im_start|>assistant\n The first image features a cute, light-colored puppy sitting on a paved surface with",
                 "<|im_start|>user\n<fim_prefix><fim_suffix> <image>\n USER: Describe the image.\n ASSISTANT:<|im_end|>\n <|im_start|>assistant\n The image shows a young alpaca standing on a grassy hill. The alpaca has",
+            ],
+            "cuda": [
+                "<|im_start|>user\n<fim_prefix><fim_suffix> <image>\n <image>\n USER: What's the difference of two images?\n ASSISTANT:<fim_prefix><fim_suffix> <image>\n USER: Describe the image.\n ASSISTANT:<|im_end|>\n <|im_start|>assistant\n The first image features a cute, light-colored puppy sitting on a paved surface with",
+                "<|im_start|>user\n<fim_prefix><fim_suffix> <image>\n USER: Describe the image.\n ASSISTANT:<|im_end|>\n <|im_start|>assistant\n The image shows a young alpaca standing on a patch of ground with some dry grass. The",
             ]
         }
         generate_ids = model.generate(**inputs, max_new_tokens=20)
