442d356aa5a32bc90ac78132e0dd95207409ed1e
Arthur <48595927+ArthurZucker@users.noreply.github.com>
2025-04-11 11:56:22 +0200

    Add moe kernels (#37376)
    * the fix that did not get in
    
    * add kernels
    
    * full graph does not work
    
    * simpler is better
    
    * Update src/transformers/integrations/hub_kernels.py
    
    Co-authored-by: Daniël de Kok <me@danieldk.eu>
    
    * Update src/transformers/integrations/fbgemm_fp8.py
    
    Co-authored-by: Daniël de Kok <me@danieldk.eu>
    
    * Update src/transformers/integrations/hub_kernels.py
    
    Co-authored-by: Daniël de Kok <me@danieldk.eu>
    
    * fixup
    
    ---------
    
    Co-authored-by: Daniël de Kok <me@danieldk.eu>

--- src/transformers/models/llama4/modeling_llama4.py
+++ src/transformers/models/llama4/modeling_llama4.py
@@ -27,6 +27,7 @@ from transformers.models.llama4.configuration_llama4 import Llama4VisionConfig
 from ...activations import ACT2FN
 from ...cache_utils import Cache, HybridChunkedCache
 from ...generation import GenerationMixin
+from ...integrations.hub_kernels import use_kernel_forward_from_hub
 from ...modeling_attn_mask_utils import AttentionMaskConverter
 from ...modeling_flash_attention_utils import FlashAttentionKwargs
 from ...modeling_outputs import (
@@ -144,6 +145,7 @@ class Llama4TextRMSNorm(nn.Module):
         return f"{tuple(self.weight.shape)}, eps={self.eps}"
 
 
+@use_kernel_forward_from_hub("Llama4TextMoe")
 class Llama4TextMoe(nn.Module):
     def __init__(self, config):
         super().__init__()
