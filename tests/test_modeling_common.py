dbccd894dd0374be9a1a035cb8d2b0564a875412
Cyril Vallez <cyril.vallez@gmail.com>
2025-04-14 23:38:23 +0200

    Update test_modeling_common.py

--- tests/test_modeling_common.py
+++ tests/test_modeling_common.py
@@ -4499,7 +4499,7 @@ class ModelTesterMixin:
                     buffer.device for buffer in new_model.buffers()
                 }
 
-            # set back the correct device to avoid polluting other tests
+            # set back the correct device
             torch.set_default_device(default_device)
 
             self.assertEqual(
