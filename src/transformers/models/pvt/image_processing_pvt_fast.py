33f103e23a890657a6ef5ab7c35e233f9e582903
keetrap <parteekkamboj112@gmail.com>
2025-04-14 20:54:40 +0530

    Update image_processing_pvt_fast.py

--- src/transformers/models/pvt/image_processing_pvt_fast.py
+++ src/transformers/models/pvt/image_processing_pvt_fast.py
@@ -18,10 +18,8 @@ from ...image_processing_utils_fast import (
     BASE_IMAGE_PROCESSOR_FAST_DOCSTRING,
     BaseImageProcessorFast,
     DefaultFastImageProcessorKwargs,
-    get_size_dict,
 )
 from ...image_utils import IMAGENET_DEFAULT_MEAN, IMAGENET_DEFAULT_STD, PILImageResampling
-from ...processing_utils import Unpack
 from ...utils import add_start_docstrings
 
 
@@ -38,7 +36,7 @@ class PvtImageProcessorFast(BaseImageProcessorFast):
     image_mean = IMAGENET_DEFAULT_MEAN
     image_std = IMAGENET_DEFAULT_STD
     size = {"height": 224, "width": 224}
-    default_to_square = None
+    default_to_square = True
     crop_size = None
     do_resize = True
     do_center_crop = None
@@ -46,14 +44,6 @@ class PvtImageProcessorFast(BaseImageProcessorFast):
     do_normalize = True
     do_convert_rgb = None
     model_input_names = ["pixel_values"]
-
-    def __init__(self, **kwargs: Unpack[PvtFastImageProcessorKwargs]) -> None:
-        size = kwargs.pop("size", None)
-        size = size if size is not None else {"height": 224, "width": 224}
-        if isinstance(size, (tuple, list)):
-            size = size[::-1]
-        self.size = get_size_dict(size)
-        super().__init__(**kwargs)
 
 
 __all__ = ["PvtImageProcessorFast"]
