037332e9a9055e3452d7520a02e33a6f1e2f45bd
Pablo <pablo.montalvo.leroux@gmail.com>
2025-04-15 19:05:09 +0200

    recover FA2 training for CLIP + handle quantization

--- src/transformers/models/clip/modeling_clip.py
+++ src/transformers/models/clip/modeling_clip.py
@@ -357,13 +357,25 @@ class CLIPAttention(nn.Module):
         values = values.view(batch_size, seq_length, self.num_heads, self.head_dim).transpose(1, 2)
         # CLIP text model uses both `causal_attention_mask` and `attention_mask`
         # in case FA2 kernel is called, `is_causal` should be inferred from `causal_attention_mask`
-        if self.config._attn_implementation != "flash_attention_2":
+        if self.config._attn_implementation == "flash_attention_2":
+            self.is_causal = causal_attention_mask is not None
+            input_dtype = queries.dtype
+            if input_dtype == torch.float32:
+                if torch.is_autocast_enabled():
+                    target_dtype = torch.get_autocast_gpu_dtype()
+                # Handle the case where the model is quantized
+                elif hasattr(self.config, "_pre_quantization_dtype"):
+                    target_dtype = self.config._pre_quantization_dtype
+                else:
+                    target_dtype = self.q_proj.weight.dtype
+                queries = queries.to(target_dtype)
+                keys = keys.to(target_dtype)
+                values = values.to(target_dtype)
+        else:
             if attention_mask is not None and causal_attention_mask is not None:
                 attention_mask = attention_mask + causal_attention_mask
             elif causal_attention_mask is not None:
                 attention_mask = causal_attention_mask
-        else:
-            self.is_causal = causal_attention_mask is not None
 
         attention_interface: Callable = eager_attention_forward
         if self.config._attn_implementation != "eager":
