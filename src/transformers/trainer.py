af37917d295c2f1cb385ada9e9ff7e7a208aa836
Matt <rocketknight1@gmail.com>
2025-04-15 14:49:10 +0100

    Add TODO

--- src/transformers/trainer.py
+++ src/transformers/trainer.py
@@ -3689,6 +3689,9 @@ class Trainer:
         arguments, depending on the situation.
         """
         if self.use_cpu_amp:
+            # TODO Matt: This syntax is deprecated and the preferred version is
+            #      torch.amp.autocast("cpu", cache_enabled=cache_enabled, dtype=self.amp_dtype)
+            #      but this is unavailable on Torch 2.1 or earlier. We can change this when we stop supporting 2.1.
             ctx_manager = torch.cpu.amp.autocast(cache_enabled=cache_enabled, dtype=self.amp_dtype)
         else:
             ctx_manager = contextlib.nullcontext()
