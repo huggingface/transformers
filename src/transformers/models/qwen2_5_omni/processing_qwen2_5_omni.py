d308f221df8405dd5195d61ead02126fb52da66d
weedge <weege007@gmail.com>
2025-04-15 10:35:49 +0800

    fix: qwen2.5 omni apply_chat_template system content check
    Signed-off-by: weedge <weege007@gmail.com>

--- src/transformers/models/qwen2_5_omni/processing_qwen2_5_omni.py
+++ src/transformers/models/qwen2_5_omni/processing_qwen2_5_omni.py
@@ -328,10 +328,14 @@ class Qwen2_5OmniProcessor(ProcessorMixin):
         if isinstance(conversations[0], dict):
             conversations = [conversations]
         for conversation in conversations:
+            sys_content = ""
+            if isinstance(conversation[0]["content"],str):
+                sys_content = conversation[0]["content"]
+            elif isinstance(conversation[0]["content"],list) and len(conversation[0]["content"])>0:
+                sys_content = conversation[0]["content"][0].get("text","")
             if (
                 conversation[0]["role"] != "system"
-                or conversation[0]["content"][0]["text"]
-                != "You are Qwen, a virtual human developed by the Qwen Team, Alibaba Group, capable of perceiving auditory and visual inputs, as well as generating text and speech."
+                or sys_content != "You are Qwen, a virtual human developed by the Qwen Team, Alibaba Group, capable of perceiving auditory and visual inputs, as well as generating text and speech."
             ):
                 logging.warning(
                     "System prompt modified, audio output may not work as expected. "
