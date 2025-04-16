66098665c87e85a71051b96a087e3ebcd2572b7d
Yih-Dar <2521628+ydshieh@users.noreply.github.com>
2025-04-16 18:56:17 +0200

    Update tests/test_modeling_common.py
    Co-authored-by: Cyril Vallez <cyril.vallez@huggingface.co>

--- tests/test_modeling_common.py
+++ tests/test_modeling_common.py
@@ -4550,7 +4550,8 @@ class ModelTesterMixin:
             self.assertEqual(
                 unique_devices, {device}, f"All parameters should be on {device}, but found {unique_devices}."
             )
-
+    # here we need to run with a subprocess as otherwise setting back the default device to the default value ("cpu")
+    # may bring unwanted consequences on other tests
     @run_test_using_subprocess
     @require_torch_accelerator
     def test_can_load_with_global_device_set(self):
