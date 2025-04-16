c029cf0bc764c38bb7c2b1b1e65000ede4ae6d8d
Chih-Chieh-Yang <7364402+cyang49@users.noreply.github.com>
2025-04-16 12:05:59 -0400

    Add comment

--- tests/models/mamba2/test_modeling_mamba2.py
+++ tests/models/mamba2/test_modeling_mamba2.py
@@ -240,6 +240,7 @@ class Mamba2ModelTest(ModelTesterMixin, GenerationTesterMixin, PipelineTesterMix
 
     # This test adjusts n_groups to half the original setting and effectively
     # creates a grouped SSD configuration in the mamba2 layers
+    # See https://github.com/huggingface/transformers/pull/37533/
     def test_mamba2_slow_vs_fast_forward_grouped(self):
         config_and_inputs = self.model_tester.prepare_config_and_inputs()
         config_and_inputs[0].n_groups //= 2
