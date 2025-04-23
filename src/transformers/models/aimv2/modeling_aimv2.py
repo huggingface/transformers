cae9b80297a2760ce33c3ca616a6c46056003e8a
yaswant19 <yaswanthgali8@gmail.com>
2025-04-24 05:29:22 +0530

    nit

--- src/transformers/models/aimv2/modeling_aimv2.py
+++ src/transformers/models/aimv2/modeling_aimv2.py
@@ -537,6 +537,8 @@ AIMV2_VISION_INPUTS_DOCSTRING = r"""
             more detail.
         interpolate_pos_encoding (`bool`, *optional*, defaults `False`):
             Whether to interpolate the pre-trained position encodings.
+        return_dict (`bool`, *optional*):
+            Whether or not to return a [`~utils.ModelOutput`] instead of a plain tuple.
 """
 
 
