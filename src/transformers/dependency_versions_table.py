af6d2756d9ba7d83388f617b612009242ca46681
Joao Gante <joaofranciscocardosogante@gmail.com>
2025-04-24 16:18:25 +0100

    [deps] pin max `torch` version  (#37760)
    pin max pt version :(

--- src/transformers/dependency_versions_table.py
+++ src/transformers/dependency_versions_table.py
@@ -91,7 +91,7 @@ deps = {
     "tiktoken": "tiktoken",
     "timm": "timm<=1.0.11",
     "tokenizers": "tokenizers>=0.21,<0.22",
-    "torch": "torch>=2.1",
+    "torch": "torch>=2.1,<2.7",
     "torchaudio": "torchaudio",
     "torchvision": "torchvision",
     "pyctcdecode": "pyctcdecode>=0.4.0",
