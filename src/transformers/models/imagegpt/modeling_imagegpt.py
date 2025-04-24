9e3c7f57c48d86211589a69a9b857e391fee249a
kkew3 <kps6326@hotmail.com>
2025-04-24 09:59:29 +0800

    Fix possibly wrong labels shape in doc
    Supported by ImageGPTModel.forward, line 665--666:
    
    src/transformers/models/imagegpt/modeling_imagegpt.py:
       665	            Labels for language modeling. Note that the labels **are shifted** inside the model, i.e. you can set
       666	            `labels = input_ids` Indices are selected in `[-100, 0, ..., config.vocab_size]` All labels set to `-100`
    
    This commit is introduced due to commit 8e594a4143cca79f165b99e4ed4c9f3a90047bf3.

--- src/transformers/models/imagegpt/modeling_imagegpt.py
+++ src/transformers/models/imagegpt/modeling_imagegpt.py
@@ -661,7 +661,7 @@ class ImageGPTModel(ImageGPTPreTrainedModel):
         **kwargs: Any,
     ) -> Union[Tuple, BaseModelOutputWithPastAndCrossAttentions]:
         r"""
-        labels (`torch.LongTensor` of shape `(batch_size, sequence_length)`, *optional*):
+        labels (`torch.LongTensor` of shape `(batch_size, input_ids_length)`, *optional*):
             Labels for language modeling. Note that the labels **are shifted** inside the model, i.e. you can set
             `labels = input_ids` Indices are selected in `[-100, 0, ..., config.vocab_size]` All labels set to `-100`
             are ignored (masked), the loss is only computed for labels in `[0, ..., config.vocab_size]`
