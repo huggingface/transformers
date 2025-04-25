7b798ab1e61aa1804cf587ac65bec95f734eb30c
Nandakishor <nandumpilicode@gmail.com>
2025-04-25 23:54:03 +0530

    new

--- .history/src/transformers/models/__init___20250425235345.py
+++ .history/src/transformers/models/__init___20250425235345.py
@@ -134,7 +134,9 @@ if TYPE_CHECKING:
     from .helium import *
     from .herbert import *
     from .hiera import *
+    # --- !!! ADD YOUR MODEL IMPORT HERE (for type checking) !!! ---
     from .hindi_causal_lm import *
+    # --- !!! END ADDITION !!! ---
     from .hubert import *
     from .ibert import *
     from .idefics import *
