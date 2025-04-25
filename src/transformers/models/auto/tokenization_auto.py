3d60e5b04643282d1ac6d662b0feed377b7f2c16
Shuming Hu <shumingh@gmail.com>
2025-04-25 23:23:56 +0000

    plm template

--- src/transformers/models/auto/tokenization_auto.py
+++ src/transformers/models/auto/tokenization_auto.py
@@ -311,6 +311,7 @@ else:
                 ),
             ),
             ("llava", ("LlamaTokenizer", "LlamaTokenizerFast" if is_tokenizers_available() else None)),
+            ("perception_lm", ("PerceptionLMTokenizer", "PerceptionLMTokenizerFast" if is_tokenizers_available() else None)),
             ("llava_next", ("LlamaTokenizer", "LlamaTokenizerFast" if is_tokenizers_available() else None)),
             ("llava_next_video", ("LlamaTokenizer", "LlamaTokenizerFast" if is_tokenizers_available() else None)),
             ("llava_onevision", ("LlamaTokenizer", "LlamaTokenizerFast" if is_tokenizers_available() else None)),
