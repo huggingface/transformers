da684631fe70b5b1b7522b1129307ecbca7146da
Cyril Vallez <cyril.vallez@gmail.com>
2025-04-28 15:50:47 +0200

    Update modular_test_detr.py

--- examples/modular-transformers/modular_test_detr.py
+++ examples/modular-transformers/modular_test_detr.py
@@ -2,6 +2,6 @@ from transformers.models.deformable_detr.modeling_deformable_detr import Deforma
 
 
 # Here, the old and new model have by essence a common "detr" suffix. Make sure everything is correctly named
-# in this case
+# in this case (i.e., we do not wrongly detect `Detr` as part of a suffix to remove)
 class TestDetrModel(DeformableDetrModel):
     pass
