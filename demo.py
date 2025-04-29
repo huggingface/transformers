37e38277b6ed59989a473ca6e6fd7c6b5614b8f8
ariG23498 <aritra.born2fly@gmail.com>
2025-04-29 04:02:57 +0200

    chore: delete demo.py file

--- demo.py
+++
@@ -1,7 +0,0 @@
-from transformers import BeitImageProcessor, BeitImageProcessorFast
-
-im_pro = BeitImageProcessor(size={"height":20, "width":20})
-im_pro_fast = BeitImageProcessorFast(size={"height":20, "width": 20})
-
-print(im_pro)
-print(im_pro_fast)
