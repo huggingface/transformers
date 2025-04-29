6c59d3e03427f4b31cc90d3e14f92b90b74463cf
sushmanthreddy <sushmanthreddymereddy@gmail.com>
2025-04-30 01:23:49 +0530

    added base code to work from dfine model

--- src/transformers/models/auto/modeling_auto.py
+++ src/transformers/models/auto/modeling_auto.py
@@ -81,6 +81,7 @@ MODEL_MAPPING_NAMES = OrderedDict(
         ("ctrl", "CTRLModel"),
         ("cvt", "CvtModel"),
         ("d_fine", "DFineModel"),
+        ("deim", "DeimModel"),
         ("dab-detr", "DabDetrModel"),
         ("dac", "DacModel"),
         ("data2vec-audio", "Data2VecAudioModel"),
@@ -949,6 +950,7 @@ MODEL_FOR_OBJECT_DETECTION_MAPPING_NAMES = OrderedDict(
         # Model for Object Detection mapping
         ("conditional_detr", "ConditionalDetrForObjectDetection"),
         ("d_fine", "DFineForObjectDetection"),
+        ("deim", "DeimForObjectDetection"),
         ("dab-detr", "DabDetrForObjectDetection"),
         ("deformable_detr", "DeformableDetrForObjectDetection"),
         ("deta", "DetaForObjectDetection"),
