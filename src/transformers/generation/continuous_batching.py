fix: attention mask

src/transformers/generation/continuous_batching.py
@@ -124,7 +124,6 @@ class PagedAttentionCache(Cache):
             self.key_cache.append(torch.zeros(cache_shape, dtype=self.dtype, device=layer_device))
             self.value_cache.append(torch.zeros(cache_shape, dtype=self.dtype, device=layer_device))
 
-        self._lock = threading.Lock()
         self._free_blocks = deque(range(num_blocks))
         # Maps request_id to list of block numbers used by that request
         self._block_tables: Dict[str, List[int]] = {}
@@ -145,27 +144,23 @@ class PagedAttentionCache(Cache):
 
     def free_blocks(self, request_id: str) -> None:
         """Frees all blocks associated with a request_id."""
-        with self._lock:
-            if request_id in self._block_tables:
-                blocks_to_free = self._block_tables.pop(request_id)
-                self._free_blocks.extend(blocks_to_free)  # Add back to the deque
-            else:
-                logger.warning(f"Attempted to free blocks for non-existent request_id: {request_id}")
+        if request_id in self._block_tables:
+            blocks_to_free = self._block_tables.pop(request_id)
+            self._free_blocks.extend(blocks_to_free)  # Add back to the deque
+        else:
+            logger.warning(f"Attempted to free blocks for non-existent request_id: {request_id}")
 
     def get_num_free_blocks(self) -> int:
-        with self._lock:
-            return len(self._free_blocks)
+        return len(self._free_blocks)
 
     def get_block_table(self, request_id: str) -> List[int]:
-        with self._lock:
-            return self._block_tables.get(request_id, [])
+        return self._block_tables.get(request_id, [])
 
     def _get_physical_indices(self, request_id: str, logical_indices: List[int]) -> List[int]:
         """Maps logical sequence indices to physical cache indices using the block table."""
-        with self._lock:
-            block_table = self._block_tables.get(request_id)
-            if not block_table:
-                raise ValueError(f"No block table found for request {request_id}")
+        block_table = self._block_tables.get(request_id)
+        if not block_table:
+            raise ValueError(f"No block table found for request {request_id}")
 
         physical_indices = []
         for idx in logical_indices:
@@ -669,6 +664,7 @@ class ContinuousBatchProcessor:
 
         return selected_requests
 
+    # TODO: use tensors direclty instead of lists
     def prepare_next_batch(self):
         """Prepares tensors and metadata for the next model forward pass."""
         self._get_new_requests()
@@ -1103,8 +1099,6 @@ class ContinuousBatchingManager:
                         )
                 except Exception as e:
                     logger.error(f"Model forward pass failed: {e}", exc_info=True)
-                    # How to handle? Mark involved requests as failed? Stop manager?
-                    # Mark requests in the failed batch as failed
                     failed_ids = batch_processor.requests_to_process_next
                     for req_id in failed_ids:
                         if req_id in batch_processor.active_requests:

src/transformers/integrations/sdpa_attention.py
@@ -24,32 +24,35 @@ def sdpa_attention_forward(
     dropout: float = 0.0,
     scaling: Optional[float] = None,
     is_causal: Optional[bool] = None,
-    cache=None,
-    cumulative_seqlens_q=None,
-    cumulative_seqlens_k=None,
     **kwargs,
 ) -> Tuple[torch.Tensor, None]:
-    key, value = cache.update(key, value, module.layer_idx, cumulative_seqlens_k, **kwargs)
-    attention_mask_ = torch.full(
-        [1, 1, query.shape[2], key.shape[2] + 1], torch.finfo(query.dtype).min, device=query.device, dtype=query.dtype
-    )
-    attention_mask_[0, 0, cumulative_seqlens_q[0], 0 : cumulative_seqlens_k[0]] = 0
-    for i in range(1, len(cumulative_seqlens_k)):
-        attention_mask_[
-            ...,
-            cumulative_seqlens_q[i - 1] : cumulative_seqlens_q[i],
-            cumulative_seqlens_k[i - 1] : cumulative_seqlens_k[i],
-        ] = 0
-    attention_mask_[..., cumulative_seqlens_q[i] :, cumulative_seqlens_k[i] :] = 0
+    cache = kwargs.pop("cache", None)
+    if cache is not None:
+        cumulative_seqlens_q = kwargs.pop("cumulative_seqlens_q")
+        cumulative_seqlens_k = kwargs.pop("cumulative_seqlens_k")
+        key, value = cache.update(key, value, module.layer_idx, cumulative_seqlens_k, **kwargs)
+        attention_mask_ = torch.full(
+            [1, 1, query.shape[2], key.shape[2] + 1], torch.finfo(query.dtype).min, device=query.device, dtype=query.dtype
+        )
+        for i in range(len(cumulative_seqlens_k) - 1):
+            attention_mask_[
+                ...,
+                cumulative_seqlens_q[i] : cumulative_seqlens_q[i + 1],
+                cumulative_seqlens_k[i] : cumulative_seqlens_k[i + 1],
+            ] = 0
 
-    if attention_mask.shape == attention_mask_.shape:
-        attention_mask_.masked_fill_(attention_mask != 0, torch.finfo(query.dtype).min)
+        if attention_mask.shape == attention_mask_.shape:
+            attention_mask_.masked_fill_(attention_mask != 0, torch.finfo(query.dtype).min)
+
+        causal_mask = attention_mask_
+    else:
+        causal_mask = attention_mask
+
     if hasattr(module, "num_key_value_groups"):
         key = repeat_kv(key, module.num_key_value_groups)
         value = repeat_kv(value, module.num_key_value_groups)
 
-    causal_mask = attention_mask_
-    if attention_mask is not None and causal_mask.ndim == 4:
+    if causal_mask is not None and causal_mask.ndim == 4:
         causal_mask = causal_mask[:, :, :, : key.shape[-2]]
 
     # SDPA with memory-efficient backend is bugged with non-contiguous inputs and custom attn_mask for some torch versions

src/transformers/models/llama/modeling_llama.py
@@ -197,31 +197,37 @@ def eager_attention_forward(
     dropout: float = 0.0,
     **kwargs,
 ):
-    cache = kwargs.pop("cache")
-    cumulative_seqlens_k = kwargs.pop("cumulative_seqlens_k")
-    cumulative_seqlens_q = kwargs.pop("cumulative_seqlens_q")
-    key, value = cache.update(key, value, module.layer_idx, cumulative_seqlens_k, **kwargs)
-    attention_mask_ = torch.full(
-            [1, 1, query.shape[2],key.shape[2]+1], torch.finfo(query.dtype).min, device=query.device, dtype=query.dtype
-    )
-    cuq = cumulative_seqlens_q
-    cuk = cumulative_seqlens_k
-    for i in range(len(cuk) - 1):
-        attention_mask_[..., cuq[i] : cuq[i + 1], cuk[i] : cuk[i + 1]] = 0
+    cache = kwargs.pop("cache", None)
+    if cache is not None:
+        cumulative_seqlens_q = kwargs.pop("cumulative_seqlens_q")
+        cumulative_seqlens_k = kwargs.pop("cumulative_seqlens_k")
+        key, value = cache.update(key, value, module.layer_idx, cumulative_seqlens_k, **kwargs)
+        attention_mask_ = torch.full(
+                [1, 1, query.shape[2],key.shape[2]+1], torch.finfo(query.dtype).min, device=query.device, dtype=query.dtype
+        )
+        cuq = cumulative_seqlens_q
+        cuk = cumulative_seqlens_k
+        # if module.layer_idx  == 0:
+        #     print("bob")
+        for i in range(len(cuk) - 1):
+            attention_mask_[..., cuq[i] : cuq[i + 1], cuk[i] : cuk[i + 1]] = 0
 
-    if attention_mask.shape == attention_mask_.shape:
-        attention_mask_.masked_fill_(attention_mask!=0, torch.finfo(query.dtype).min)
+        if attention_mask.shape == attention_mask_.shape:
+            attention_mask_.masked_fill_(attention_mask!=0, torch.finfo(query.dtype).min)
+        attention_mask = attention_mask_
 
     key_states = repeat_kv(key, module.num_key_value_groups)
     value_states = repeat_kv(value, module.num_key_value_groups)
 
     attn_weights = torch.matmul(query, key_states.transpose(2, 3)) * scaling
     if attention_mask is not None:
-        attention_mask_ = attention_mask_[:, :, :, : key_states.shape[2]]
-        attn_weights = attn_weights + attention_mask_
+        causal_mask = attention_mask[:, :, :, : key_states.shape[2]]
+        attn_weights = attn_weights + causal_mask
 
     attn_weights = nn.functional.softmax(attn_weights, dim=-1, dtype=torch.float32).to(query.dtype)
     attn_weights = nn.functional.dropout(attn_weights, p=dropout, training=module.training)
+    if cache is not None:
+        attn_weights = attn_weights.masked_fill(causal_mask!=0, 0)
     attn_output = torch.matmul(attn_weights, value_states)
     attn_output = attn_output.transpose(1, 2).contiguous()
 