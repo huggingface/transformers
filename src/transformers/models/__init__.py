4abeb50f6e7b5ca7e63f80686cdd74e7b6b03e63
Vladislav Bronzov <58587565+VladOS95-cyber@users.noreply.github.com>
2025-04-29 13:17:55 +0200

    Add D-FINE Model into Transformers (#36261)
    * copy the last changes from broken PR
    
    * small format
    
    * some fixes and refactoring after review
    
    * format
    
    * add config attr for loss
    
    * some fixes and refactoring
    
    * fix copies
    
    * fix style
    
    * add test for d-fine resnet
    
    * fix decoder layer prop
    
    * fix dummies
    
    * format init
    
    * remove extra print
    
    * refactor modeling, move resnet into separate folder
    
    * fix resnet config
    
    * change resnet on hgnet_v2, add clamp into decoder
    
    * fix init
    
    * fix config doc
    
    * fix init
    
    * fix dummies
    
    * fix config docs
    
    * fix hgnet_v2 config typo
    
    * format modular
    
    * add image classification for hgnet, some refactoring
    
    * format tests
    
    * fix dummies
    
    * fix init
    
    * fix style
    
    * fix init for hgnet v2
    
    * fix index.md, add init rnage for hgnet
    
    * fix conversion
    
    * add missing attr to encoder
    
    * add loss for d-fine, add additional output for rt-detr decoder
    
    * tests and docs fixes
    
    * fix rt_detr v2 conversion
    
    * some fixes for loos and decoder output
    
    * some fixes for loss
    
    * small fix for converted modeling
    
    * add n model config, some todo comments for modular
    
    * convert script adjustments and fixes, small refact
    
    * remove extra output for rt_detr
    
    * make some outputs optionsl, fix conversion
    
    * some posr merge fixes
    
    * small fix
    
    * last field fix
    
    * fix not split for hgnet_v2
    
    * disable parallelism test for hgnet_v2 image classification
    
    * skip multi gpu for d-fine
    
    * adjust after merge init
    
    * remove extra comment
    
    * fix repo name references
    
    * small fixes for tests
    
    * Fix checkpoint path
    
    * Fix consistency
    
    * Fixing docs
    
    ---------
    
    Co-authored-by: Pavel Iakubovskii <qubvel@gmail.com>

--- src/transformers/models/__init__.py
+++ src/transformers/models/__init__.py
@@ -70,6 +70,7 @@ if TYPE_CHECKING:
     from .cpmant import *
     from .ctrl import *
     from .cvt import *
+    from .d_fine import *
     from .dab_detr import *
     from .dac import *
     from .data2vec import *
@@ -133,6 +134,7 @@ if TYPE_CHECKING:
     from .groupvit import *
     from .helium import *
     from .herbert import *
+    from .hgnet_v2 import *
     from .hiera import *
     from .hubert import *
     from .ibert import *
