e093e20634297015eea22df517fd2022215a064b
Shuming Hu <shumingh@gmail.com>
2025-05-01 00:28:55 +0000

    Simplify and fix tie_word_embeddings

--- src/transformers/models/perception_lm/modeling_perception_lm.py
+++ src/transformers/models/perception_lm/modeling_perception_lm.py
@@ -64,6 +64,7 @@ class PerceptionEncoder(PE):
             use_abs_posemb=config.use_abs_posemb,
             use_ln_post=config.use_ln_post,
             ls_init_value=config.ls_init_value,
+            drop_path=config.drop_path,
             output_dim=config.width,
             use_attn_pool=False,
             use_proj=False,
@@ -365,8 +366,6 @@ class PerceptionLMForConditionalGeneration(
     def get_image_features(
         self,
         pixel_values: torch.FloatTensor,
-        vision_feature_layer: Union[int, List[int]],
-        vision_feature_select_strategy: str,
         **kwargs,
     ):
         """
@@ -385,18 +384,8 @@ class PerceptionLMForConditionalGeneration(
         Returns:
             image_features (`torch.Tensor`): Image feature tensor of shape `(num_images, image_length, embed_dim)`).
         """
-        if vision_feature_select_strategy not in ["default", "full"]:
-            raise ValueError(
-                f"Unexpected select feature strategy: {self.config.vision_feature_select_strategy}"
-            )
-
-        kwargs = {k: v for k, v in kwargs.items() if v is not None}
-        # this is not memory efficient at all (output_hidden_states=True) will save all the hidden states.
         print("pixel_values shape: ", pixel_values.shape)
         image_outputs = self.vision_model(pixel_values[0])
-        # image_features = torch.load(
-        #     "/checkpoint/vision_encoder/smhu/debug/0/h_img_dump_0.pt"
-        # ).to(pixel_values.device).to(torch.bfloat16)
         print("image_outputs shape: ", image_outputs.shape)
         image_features = self.multi_modal_projector(image_outputs)
         return image_features
@@ -476,17 +465,6 @@ class PerceptionLMForConditionalGeneration(
         return_dict = (
             return_dict if return_dict is not None else self.config.use_return_dict
         )
-        vision_feature_layer = (
-            vision_feature_layer
-            if vision_feature_layer is not None
-            else self.config.vision_feature_layer
-        )
-        vision_feature_select_strategy = (
-            vision_feature_select_strategy
-            if vision_feature_select_strategy is not None
-            else self.config.vision_feature_select_strategy
-        )
-
         if (input_ids is None) ^ (inputs_embeds is not None):
             raise ValueError(
                 "You must specify exactly one of input_ids or inputs_embeds"
@@ -503,33 +481,18 @@ class PerceptionLMForConditionalGeneration(
         if pixel_values is not None:
             image_features = self.get_image_features(
                 pixel_values=pixel_values.to(inputs_embeds),
-                vision_feature_layer=vision_feature_layer,
-                vision_feature_select_strategy=vision_feature_select_strategy,
-                image_sizes=image_sizes,
             )
 
             special_image_mask = (input_ids == self.config.image_token_id).unsqueeze(-1)
             special_image_mask = special_image_mask.expand_as(inputs_embeds).to(
                 inputs_embeds.device
             )
-            # if not is_torchdynamo_compiling() and inputs_embeds[special_image_mask].numel() != image_features.numel():
-            #     n_image_tokens = (input_ids == self.config.image_token_id).sum()
-            #     n_image_features = image_features.shape[0] * image_features.shape[1]
-            #     raise ValueError(
-            #         f"Image features and image tokens do not match: tokens: {n_image_tokens}, features {n_image_features}"
-            #     )
             image_features = image_features.to(
                 inputs_embeds.device, inputs_embeds.dtype
             )
             inputs_embeds = inputs_embeds.masked_scatter(
                 special_image_mask, image_features
             )
-            # print(inputs_embeds.shape)
-            # occhi_embeds = torch.load(
-            #     "/checkpoint/vision_encoder/smhu/debug/0/h_post_stitching_dump_0.pt"
-            # )
-            # print(occhi_embeds.shape)
-            # inputs_embeds = occhi_embeds
 
         outputs = self.language_model(
             attention_mask=attention_mask,
