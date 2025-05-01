0cf1c5428d250e276ebf14825011cffafc4c82e4
Joao Gante <joao@huggingface.co>
2025-05-01 18:20:51 +0000

    reuse cuda memory

--- src/transformers/modeling_utils.py
+++ src/transformers/modeling_utils.py
@@ -1284,6 +1284,12 @@ def _get_device_map(
             max_memory = get_max_memory(max_memory)
         if hf_quantizer is not None:
             max_memory = hf_quantizer.adjust_max_memory(max_memory)
+
+        # There may be unused reserved memory in the GPU, which we can use to allocate parameters.
+        for device_name in max_memory.keys():
+            if isinstance(device_name, int):  # it's a GPU device
+                unused_memory = torch.cuda.memory_reserved(device_name) - torch.cuda.memory_allocated(device_name)
+                max_memory[device_name] += unused_memory
         device_map_kwargs["max_memory"] = max_memory
 
         device_map = infer_auto_device_map(model, dtype=target_dtype, **device_map_kwargs)
@@ -5978,8 +5984,12 @@ def caching_allocator_warmup(model: PreTrainedModel, expanded_device_map: Dict, 
             # Note that we use an absolute value instead of device proportion here, as a 8GiB device could still allocate too much
             # if using e.g. 90% of device size, while a 140GiB device would allocate too little
             byte_count = min(byte_count, max(0, int(device_memory - 1.2 * 1024**3)))
+            # If there is unused cuda memory, we can skip/reduce the allocation.
+            unused_memory = torch.cuda.memory_reserved() - torch.cuda.memory_allocated()
+            byte_count = max(0, byte_count - unused_memory)
         # Allocate memory
-        _ = torch.empty(byte_count // factor, dtype=torch.float16, device=device, requires_grad=False)
+        if byte_count > 0:
+            _ = torch.empty(byte_count // factor, dtype=torch.float16, device=device, requires_grad=False)
 
 
 def get_disk_only_shard_files(device_map, weight_map):
