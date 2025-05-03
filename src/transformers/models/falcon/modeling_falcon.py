fix graph break for torch.compile in attention mask check

src/transformers/models/falcon/modeling_falcon.py
@@ -998,7 +998,7 @@ class FalconModel(FalconPreTrainedModel):
         # `fullgraph=True`. See more context in https://github.com/huggingface/transformers/pull/29114
 
         if self.config._attn_implementation == "flash_attention_2":
-            if attention_mask is not None and 0.0 in attention_mask:
+            if attention_mask is not None and torch.any(attention_mask == 0.0):
                 return attention_mask
             return None
 

src/transformers/models/gpt2/modeling_gpt2.py
@@ -1104,7 +1104,7 @@ class GPT2Model(GPT2PreTrainedModel):
         output_attentions: bool,
     ):
         if self.config._attn_implementation == "flash_attention_2":
-            if attention_mask is not None and 0.0 in attention_mask:
+            if attention_mask is not None and torch.any(attention_mask == 0.0):
                 return attention_mask
             return None
 

src/transformers/models/jamba/modeling_jamba.py
@@ -1354,7 +1354,7 @@ class JambaModel(JambaPreTrainedModel):
 
     def _update_causal_mask(self, attention_mask, input_tensor, cache_position):
         if self.config._attn_implementation == "flash_attention_2":
-            if attention_mask is not None and 0.0 in attention_mask:
+            if attention_mask is not None and torch.any(attention_mask == 0.0):
                 return attention_mask
             return None
 

src/transformers/models/moshi/modeling_moshi.py
@@ -1282,7 +1282,7 @@ class MoshiDepthDecoder(MoshiPreTrainedModel, GenerationMixin):
                         " this may lead to unexpected behaviour for Flash Attention version of Moshi. Make sure to "
                         " call `tokenizer.padding_side  = 'left'` before tokenizing the input. "
                     )
-            if attention_mask is not None and 0.0 in attention_mask:
+            if attention_mask is not None and torch.any(attention_mask == 0.0):
                 return attention_mask
             return None
         if self.config._attn_implementation == "flex_attention":
@@ -1598,7 +1598,7 @@ class MoshiModel(MoshiPreTrainedModel):
                         " this may lead to unexpected behaviour for Flash Attention version of Moshi. Make sure to "
                         " call `tokenizer.padding_side  = 'left'` before tokenizing the input. "
                     )
-            if attention_mask is not None and 0.0 in attention_mask:
+            if attention_mask is not None and torch.any(attention_mask == 0.0):
                 return attention_mask
             return None
         if self.config._attn_implementation == "flex_attention":

src/transformers/models/qwen2_5_omni/modeling_qwen2_5_omni.py
@@ -2059,7 +2059,7 @@ class Qwen2_5OmniThinkerTextModel(Qwen2_5OmniPreTrainedModel):
                         " this may lead to unexpected behaviour for Flash Attention version of Qwen25OmniThinkerText. Make sure to "
                         " call `tokenizer.padding_side  = 'left'` before tokenizing the input. "
                     )
-            if attention_mask is not None and 0.0 in attention_mask:
+            if attention_mask is not None and torch.any(attention_mask == 0.0):
                 return attention_mask
             return None
         if self.config._attn_implementation == "flex_attention":
@@ -2775,7 +2775,7 @@ class Qwen2_5OmniTalkerModel(Qwen2_5OmniPreTrainedModel):
                         " this may lead to unexpected behaviour for Flash Attention version of Qwen25OmniTalker. Make sure to "
                         " call `tokenizer.padding_side  = 'left'` before tokenizing the input. "
                     )
-            if attention_mask is not None and 0.0 in attention_mask:
+            if attention_mask is not None and torch.any(attention_mask == 0.0):
                 return attention_mask
             return None
         if self.config._attn_implementation == "flex_attention":