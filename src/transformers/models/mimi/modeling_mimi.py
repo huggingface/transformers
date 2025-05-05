aa00fa63aeb41fac3f62afffafe0ae523b0331a0
Eustache Le Bihan <eulebihan@gmail.com>
2025-05-05 17:33:41 +0200

    simplify encoded length computation

--- src/transformers/models/mimi/modeling_mimi.py
+++ src/transformers/models/mimi/modeling_mimi.py
@@ -216,7 +216,7 @@ class MimiConv1d(nn.Module):
         end = padded.shape[-1] - extra_pad
         return padded[..., :end]
 
-    def _get_output_length(self, input_length: int) -> int:
+    def _get_output_length(self, input_length: torch.LongTensor) -> torch.LongTensor:
         """
         Return the length of the output of the MimiConv1d.
         """
@@ -357,21 +357,28 @@ class MimiEncoder(nn.Module):
         model = [MimiConv1d(config, config.audio_channels, config.num_filters, config.kernel_size)]
         scaling = 1
 
+        # keep track of MimiConv1d submodule layer names for easy encoded length computation
+        mimiconv1d_layer_names = ["layers.0"]
+
         # Downsample to raw audio scale
         for ratio in reversed(config.upsampling_ratios):
             current_scale = scaling * config.num_filters
             # Add residual layers
             for j in range(config.num_residual_layers):
+                mimiconv1d_layer_names.extend([f"layers.{len(model)}.block.1", f"layers.{len(model)}.block.3"])
                 model += [MimiResnetBlock(config, current_scale, [config.dilation_growth_rate**j, 1])]
             # Add downsampling layers
             model += [nn.ELU()]
+            mimiconv1d_layer_names.append(f"layers.{len(model)}")
             model += [MimiConv1d(config, current_scale, current_scale * 2, kernel_size=ratio * 2, stride=ratio)]
             scaling *= 2
 
         model += [nn.ELU()]
+        mimiconv1d_layer_names.append(f"layers.{len(model)}")
         model += [MimiConv1d(config, scaling * config.num_filters, config.hidden_size, config.last_kernel_size)]
 
         self.layers = nn.ModuleList(model)
+        self._mimiconv1d_layer_names = mimiconv1d_layer_names
 
     # Copied from transformers.models.encodec.modeling_encodec.EncodecEncoder.forward
     def forward(self, hidden_states):
@@ -1593,20 +1600,15 @@ class MimiModel(MimiPreTrainedModel):
         codes = codes.transpose(0, 1)
         return codes, past_key_values
 
-    def get_encoded_length(self, input_length: int) -> int:
+    def get_encoded_length(self, input_length: torch.LongTensor) -> torch.LongTensor:
         """
         Return the number of frames of the encoded audio waveform.
         """
         output_length = input_length
 
         # encoder
-        for layer in self.encoder.layers:
-            if isinstance(layer, MimiConv1d):
-                output_length = layer._get_output_length(output_length)
-            elif isinstance(layer, MimiResnetBlock):
-                for el in layer.block:
-                    if isinstance(el, MimiConv1d):
-                        output_length = el._get_output_length(output_length)
+        for layer_name in self.encoder._mimiconv1d_layer_names:
+            output_length = self.encoder.get_submodule(layer_name)._get_output_length(output_length)
 
         # downsample
         output_length = self.downsample._get_output_length(output_length)
@@ -1617,8 +1619,7 @@ class MimiModel(MimiPreTrainedModel):
         """
         Get the mask for the audio codes from the original padding mask.
         """
-        audio_lengths = padding_mask.sum(dim=-1).cpu().tolist()
-        encoded_lengths = torch.tensor([self.get_encoded_length(length) for length in audio_lengths])
+        encoded_lengths = self.get_encoded_length(padding_mask.sum(dim=-1))
 
         audio_codes_mask = torch.arange(encoded_lengths.max(), device=encoded_lengths.device).expand(
             len(encoded_lengths), -1
