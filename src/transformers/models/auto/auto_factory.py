0330210e5eae2008a4525acf91bddc0ac7914233
Matt <rocketknight1@gmail.com>
2025-05-06 17:57:09 +0100

    Start adding _auto_class to models

--- src/transformers/models/auto/auto_factory.py
+++ src/transformers/models/auto/auto_factory.py
@@ -561,7 +561,7 @@ class _BaseAutoModelClass:
             )
             _ = hub_kwargs.pop("code_revision", None)
             cls.register(config.__class__, model_class, exist_ok=True)
-            model_class._auto_class = cls.__name__
+            model_class.register_for_auto_class(auto_class=cls)
             model_class = add_generation_mixin_to_remote_model(model_class)
             return model_class.from_pretrained(
                 pretrained_model_name_or_path, *model_args, config=config, **hub_kwargs, **kwargs
