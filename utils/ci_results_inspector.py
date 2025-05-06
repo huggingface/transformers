8a7d80d20999f80b2c867e86073ca75619ac1983
Ivar Flakstad <69173633+ivarflakstad@users.noreply.github.com>
2025-05-06 13:32:10 +0200

    Take all required arguments as input

--- utils/ci_results_inspector.py
+++ utils/ci_results_inspector.py
@@ -24,10 +24,21 @@ def get_unique_failures_from_reports(*, reports: dict[str, dict[str, str]]) -> d
                                            unique_failures[line] = trace
     return unique_failures
 
+
+DEFAULT_ARTIFACTS = [
+    "ci_results_run_models_gpu",
+    "ci_results_run_examples_gpu",
+    "ci_results_run_pipelines_torch_gpu",
+    "ci_results_run_torch_cuda_extensions_gpu",
+]
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     # Required parameters
-    parser.add_argument("--workflow_id", type=str, required=True, help="A GitHub Actions workflow run id.")
+    parser.add_argument("-a", "--workflow-a", type=str, required=True, help="GitHub Actions workflow run id A")
+    parser.add_argument("-b", "--workflow-b", type=str, required=True, help="GitHub Actions workflow run id B")
+    parser.add_argument("--artifacts", type=str, nargs='+', default=DEFAULT_ARTIFACTS, help="Artifact names to process")
+    parser.add_argument("-op", "--operation", type=str, choices=['|', '&'], default='|', help="Artifact names to process")
     parser.add_argument(
         "--output_dir",
         type=str,
@@ -37,37 +48,25 @@ if __name__ == "__main__":
     parser.add_argument("--token", default=None, type=str, help="A token that has actions:read permission.")
     args = parser.parse_args()
 
-    artifact_names = [
-        "ci_results_run_models_gpu",
-        #"ci_results_run_examples_gpu",
-        #"ci_results_run_pipelines_torch_gpu",
-        #"ci_results_run_torch_cuda_extensions_gpu",
-    ]
-    reports = get_workflow_run_reports(
-        workflow_run_id=args.workflow_id, artifact_names=artifact_names, output_dir=args.output_dir, token=args.token
+    a_reports = get_workflow_run_reports(
+        workflow_run_id=args.workflow_a, artifact_names=args.artifacts, output_dir=args.output_dir, token=args.token
     )
+    a_unique_failures = get_unique_failures_from_reports(reports=a_reports)
 
-    unique_failures = get_unique_failures_from_reports(reports=reports)
-
-
-    other_output_dir = "/Users/ivar/dev/hf/transformers-ci-error-statistics/test2"
-    workflow_run_id = 13755547860
-    other_reports = get_workflow_run_reports(
-        workflow_run_id=workflow_run_id, artifact_names=artifact_names, output_dir=other_output_dir, token=args.token
+    b_reports = get_workflow_run_reports(
+        workflow_run_id=args.workflow_b, artifact_names=args.artifacts, output_dir=args.output_dir, token=args.token
     )
-    other_unique_failures = get_unique_failures_from_reports(reports=other_reports)
+    b_unique_failures = get_unique_failures_from_reports(reports=b_reports)
 
 
-    #diff = unique_failures - other_unique_failures
-    #print("unique_failures - other_unique_failures")
-    #pprint(diff)
-    #print()
+    result = {}
+    for key, val in b_unique_failures.items():
+        if args.operation == '|' and key not in a_unique_failures.keys():
+            result[key] = val
 
-    diff = {}
-    for key, val in other_unique_failures.items():
-        if key not in unique_failures.keys():
-            diff[key] = val
+        if args.operation == '&' and key in a_unique_failures.keys():
+            result[key] = val
 
-    print("other_unique_failures - unique_failures")
-    for key, val in sorted(diff.items(), key=lambda x: x[0]):
+
+    for key, val in sorted(result.items(), key=lambda x: x[0]):
         print(key, val)
