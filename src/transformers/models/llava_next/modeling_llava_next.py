61c54f23ac2668ec1cc094865ab543606534c92d
youngrok cha <line0930@gmail.com>
2025-05-06 20:11:26 +0900

    [llava] one pixel is missing from padding when length is odd (#37819)
    * [fix] one pixel should be added when length is odd
    
    * [fix] add vision_aspect_ratio args & typo
    
    * [fix] style
    
    * [fix] do not fix fast file directly
    
    * [fix] convert using modular
    
    * remove duplicate codes
    
    * match unpad logic with pad logic
    
    * test odd-sized images for llava & aria
    
    * test unpad odd-sized padding for llava family
    
    * fix style
    
    * add kwarg to onvision modular
    
    * move vision_aspect_ratio from image_processor to processor
    (llava_onevision)

--- src/transformers/models/llava_next/modeling_llava_next.py
+++ src/transformers/models/llava_next/modeling_llava_next.py
@@ -139,14 +139,14 @@ def unpad_image(tensor, original_size):
 
     if original_aspect_ratio > current_aspect_ratio:
         scale_factor = current_width / original_width
-        new_height = int(round(original_height * scale_factor, 7))
-        padding = (current_height - new_height) // 2
-        unpadded_tensor = tensor[:, padding : current_height - padding, :]
+        new_height = min(math.ceil(original_height * scale_factor), current_height)
+        padding, r = divmod(current_height - new_height, 2)
+        unpadded_tensor = tensor[:, padding : current_height - (padding + r), :]
     else:
         scale_factor = current_height / original_height
-        new_width = int(round(original_width * scale_factor, 7))
-        padding = (current_width - new_width) // 2
-        unpadded_tensor = tensor[:, :, padding : current_width - padding]
+        new_width = min(math.ceil(original_width * scale_factor), current_width)
+        padding, r = divmod(current_width - new_width, 2)
+        unpadded_tensor = tensor[:, :, padding : current_width - (padding + r)]
 
     return unpadded_tensor
 
