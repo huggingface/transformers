1f8228aad779386c20b380aeccac4d028047c571
ydshieh <ydshieh@users.noreply.github.com>
2025-05-07 12:58:57 +0200

    fix ruff check

--- src/transformers/models/kosmos2_5/modeling_kosmos2_5.py
+++ src/transformers/models/kosmos2_5/modeling_kosmos2_5.py
@@ -23,7 +23,7 @@ import torch.utils.checkpoint
 from torch import nn
 
 from ...activations import ACT2FN
-from ...cache_utils import Cache, DynamicCache, StaticCache
+from ...cache_utils import Cache, DynamicCache
 from ...generation import GenerationMixin
 from ...modeling_attn_mask_utils import AttentionMaskConverter
 from ...modeling_flash_attention_utils import FlashAttentionKwargs
