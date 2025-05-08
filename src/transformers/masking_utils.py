8f3a2a030fe6024aaa1727d4741bd79425b6617f
Cyril Vallez <cyril.vallez@gmail.com>
2025-05-08 13:53:14 +0200

    Update masking_utils.py

--- src/transformers/masking_utils.py
+++ src/transformers/masking_utils.py
@@ -649,7 +649,7 @@ def get_causal_masks(
         )
         masks.append(causal_mask)
 
-    # Map back to each layer
+    # Map back to each layer (note that this does not incur any copy of the masks)
     mask_per_layers = [masks[layer_to_mask_mapping[i]] for i in range(num_layers)]
     return mask_per_layers
 
