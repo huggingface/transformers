88149dfcbcb6eaf3004a8966475c26957006dffe
Peter St. John <pstjohn@nvidia.com>
2025-05-09 09:53:08 -0700

    update extended_attention_mask for fa2
    Signed-off-by: Peter St. John <pstjohn@nvidia.com>

--- src/transformers/models/esm/modeling_esm.py
+++ src/transformers/models/esm/modeling_esm.py
@@ -931,9 +931,13 @@ class EsmModel(EsmPreTrainedModel):
         if attention_mask is None:
             attention_mask = torch.ones(((batch_size, seq_length + past_key_values_length)), device=device)
 
-        # We can provide a self-attention mask of dimensions [batch_size, from_seq_length, to_seq_length]
-        # ourselves in which case we just need to make it broadcastable to all heads.
-        extended_attention_mask: torch.Tensor = self.get_extended_attention_mask(attention_mask, input_shape)
+        if self.config._attn_implementation == "flash_attention_2":
+            extended_attention_mask = attention_mask
+
+        else:
+            # We can provide a self-attention mask of dimensions [batch_size, from_seq_length, to_seq_length]
+            # ourselves in which case we just need to make it broadcastable to all heads.
+            extended_attention_mask: torch.Tensor = self.get_extended_attention_mask(attention_mask, input_shape)
 
         # If a 2D or 3D attention mask is provided for the cross-attention
         # we need to make broadcastable to [batch_size, num_heads, seq_length, seq_length]
