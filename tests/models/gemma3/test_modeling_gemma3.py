ff23f1deb74dce334924df623314ea6e8e667418
Matt <rocketknight1@gmail.com>
2025-05-12 19:28:10 +0100

    Revert Gemma3 changes since it's a VLM

--- tests/models/gemma3/test_modeling_gemma3.py
+++ tests/models/gemma3/test_modeling_gemma3.py
@@ -37,8 +37,8 @@ from transformers.testing_utils import (
     torch_device,
 )
 
-from ...causal_lm_tester import CausalLMModelTest, CausalLMModelTester
 from ...generation.test_utils import GenerationTesterMixin
+from ...models.gemma.test_modeling_gemma import GemmaModelTester
 from ...test_configuration_common import ConfigTester
 from ...test_modeling_common import ModelTesterMixin, floats_tensor, ids_tensor
 
@@ -56,25 +56,17 @@ if is_torch_available():
     from transformers.pytorch_utils import is_torch_greater_or_equal
 
 
-class Gemma3ModelTester(CausalLMModelTester):
+class Gemma3ModelTester(GemmaModelTester):
     if is_torch_available():
         config_class = Gemma3TextConfig
-        base_model_class = Gemma3TextModel
-        causal_lm_class = Gemma3ForCausalLM
-    pipeline_model_mapping = (
-        {
-            "feature-extraction": Gemma3TextModel,
-            "text-generation": Gemma3ForCausalLM,
-        }
-        if is_torch_available()
-        else {}
-    )
+        model_class = Gemma3TextModel
+        for_causal_lm_class = Gemma3ForCausalLM
 
 
 @require_torch
-class Gemma3ModelTest(CausalLMModelTest, unittest.TestCase):
-    model_tester_class = Gemma3ModelTester
+class Gemma3ModelTest(ModelTesterMixin, GenerationTesterMixin, unittest.TestCase):
     all_model_classes = (Gemma3TextModel, Gemma3ForCausalLM) if is_torch_available() else ()
+    all_generative_model_classes = (Gemma3ForCausalLM,) if is_torch_available() else ()
     test_headmasking = False
     test_pruning = False
     _is_stateful = True
