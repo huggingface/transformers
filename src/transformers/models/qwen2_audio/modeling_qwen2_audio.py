dccabeb4117520aba2e1e0404eac5013004460e0
Vasqu <antonprogamer@gmail.com>
2025-05-14 17:06:19 +0200

    remove unnecessary _reshape function and remove copy to whisper

--- src/transformers/models/qwen2_audio/modeling_qwen2_audio.py
+++ src/transformers/models/qwen2_audio/modeling_qwen2_audio.py
@@ -127,7 +127,6 @@ class Qwen2AudioAttention(nn.Module):
         self.q_proj = nn.Linear(embed_dim, embed_dim, bias=bias)
         self.out_proj = nn.Linear(embed_dim, embed_dim, bias=bias)
 
-    # Copied from transformers.models.bart.modeling_bart.BartAttention._shape with BART->whisper
     def _shape(self, tensor: torch.Tensor, seq_len: int, bsz: int):
         return tensor.view(bsz, seq_len, self.num_heads, self.head_dim).transpose(1, 2).contiguous()
 
