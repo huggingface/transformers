f3fb6164f2597481f73b60b39734ec9dcc82cdae
itazap <ita.zaporozhets@huggingface.co>
2025-05-15 15:10:32 +0200

    rm build_input function for fast, don't test prepare_for_model for fast as we don't use it

--- tests/test_tokenization_common.py
+++ tests/test_tokenization_common.py
@@ -3020,16 +3020,17 @@ class TokenizerTesterMixin:
                     self.assertEqual(output[key], output_sequence[key])
 
     def test_prepare_for_model(self):
-        tokenizers = self.get_tokenizers(do_lower_case=False)
-        for tokenizer in tokenizers:
-            with self.subTest(f"{tokenizer.__class__.__name__}"):
-                string_sequence = "Testing the prepare_for_model method."
-                ids = tokenizer.encode(string_sequence, add_special_tokens=False)
-                prepared_input_dict = tokenizer.prepare_for_model(ids, add_special_tokens=True)
+        tokenizer = self.get_tokenizer(do_lower_case=False)
+        if hasattr(tokenizer, "is_fast") and tokenizer.is_fast:
+            self.skipTest(f"{tokenizer.__class__.__name__} is fast")
 
-                input_dict = tokenizer.encode_plus(string_sequence, add_special_tokens=True)
+        string_sequence = "Testing the prepare_for_model method."
+        ids = tokenizer.encode(string_sequence, add_special_tokens=False)
+        prepared_input_dict = tokenizer.prepare_for_model(ids, add_special_tokens=True)
 
-                self.assertEqual(input_dict, prepared_input_dict)
+        input_dict = tokenizer.encode_plus(string_sequence, add_special_tokens=True)
+
+        self.assertEqual(input_dict, prepared_input_dict)
 
     def test_batch_encode_plus_overflowing_tokens(self):
         tokenizers = self.get_tokenizers(do_lower_case=False)
