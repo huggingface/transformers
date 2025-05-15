09d32dfdc51b6da136d8eb0dfb64396c07077316
Cyril Vallez <cyril.vallez@gmail.com>
2025-05-15 11:59:42 +0200

    typo

--- src/transformers/models/gemma2/modeling_gemma2.py
+++ src/transformers/models/gemma2/modeling_gemma2.py
@@ -454,7 +454,7 @@ class Gemma2Model(Gemma2PreTrainedModel):
             # Prepare mask arguments
             mask_kwargs = {
                 "config": self.config,
-                "inputs_embeds": inputs_embeds,
+                "input_embeds": inputs_embeds,
                 "attention_mask": attention_mask,
                 "cache_position": cache_position,
                 "past_key_values": past_key_values,
