2f86c5980de8eadef33e83f39e66011e5d3bc27a
Matt <rocketknight1@gmail.com>
2025-05-16 17:53:26 +0100

    Fix values for RoPE testing

--- tests/causal_lm_tester.py
+++ tests/causal_lm_tester.py
@@ -75,7 +75,7 @@ class CausalLMModelTester:
         vocab_size=99,
         hidden_size=32,
         num_hidden_layers=2,
-        num_attention_heads=4,
+        num_attention_heads=2,
         num_key_value_heads=2,
         intermediate_size=37,
         hidden_act="gelu",
