f945d079264191a56452b3fde736e2c7eb60c434
ydshieh <ydshieh@users.noreply.github.com>
2025-05-19 10:49:02 +0200

    get info

--- utils/notification_service.py
+++ utils/notification_service.py
@@ -1151,12 +1151,11 @@ if __name__ == "__main__":
     # Remove some entries in `additional_files` if they are not concerned.
     test_name = None
     job_name = os.getenv("CI_TEST_JOB")
-    report_repo_subfolder = os.getenv("REPORT_REPO_SUBFOLDER", "")
-    if report_repo_subfolder == "":
-        if os.getenv("GITHUB_EVENT_NAME") != "schedule":
-            # use workflow run id (if it is not a scheduled run)
-            report_repo_subfolder = f"{os.getenv('GITHUB_RUN_NUMBER')}-{os.getenv('GITHUB_RUN_ID')}"
-            report_repo_subfolder = f"runs/{report_repo_subfolder}"
+    report_repo_subfolder = ""
+    if os.getenv("GITHUB_EVENT_NAME") != "schedule":
+        # use workflow run id (if it is not a scheduled run)
+        report_repo_subfolder = f"{os.getenv('GITHUB_RUN_NUMBER')}-{os.getenv('GITHUB_RUN_ID')}"
+        report_repo_subfolder = f"runs/{report_repo_subfolder}"
 
     # TODO: better way
     os.system(f"curl https://api.github.com/repos/huggingface/transformers/actions/runs/{os.getenv('GITHUB_RUN_ID')} > workflow_run.json")
--- utils/process_bad_commit_report.py
+++ utils/process_bad_commit_report.py
@@ -68,12 +68,11 @@ if __name__ == "__main__":
         new_data_full[author] = {k: v for k, v in _data.items() if len(v) > 0}
 
     # Upload to Hub and get the url
-    report_repo_subfolder = os.getenv("REPORT_REPO_SUBFOLDER", "")
-    if report_repo_subfolder == "":
-        if os.getenv("GITHUB_EVENT_NAME") != "schedule":
-            # use workflow run id (if it is not a scheduled run)
-            report_repo_subfolder = f"{os.getenv('GITHUB_RUN_NUMBER')}-{os.getenv('GITHUB_RUN_ID')}"
-            report_repo_subfolder = f"runs/{report_repo_subfolder}"
+    report_repo_subfolder = ""
+    if os.getenv("GITHUB_EVENT_NAME") != "schedule":
+        # use workflow run id (if it is not a scheduled run)
+        report_repo_subfolder = f"{os.getenv('GITHUB_RUN_NUMBER')}-{os.getenv('GITHUB_RUN_ID')}"
+        report_repo_subfolder = f"runs/{report_repo_subfolder}"
 
     # TODO: better way
     os.system(f"curl https://api.github.com/repos/huggingface/transformers/actions/runs/{os.getenv('GITHUB_RUN_ID')} > workflow_run.json")
