e60701c350e19315362b502a6528a42f6a739698
Matt <rocketknight1@gmail.com>
2025-05-20 15:26:38 +0100

    make fixup

--- examples/pytorch/test_accelerate_examples.py
+++ examples/pytorch/test_accelerate_examples.py
@@ -28,7 +28,6 @@ from accelerate.utils import write_basic_config
 from transformers.testing_utils import (
     TestCasePlus,
     backend_device_count,
-    is_flaky,
     run_command,
     slow,
     torch_device,
