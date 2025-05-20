dd5129766f1cd046f4d43ec68b27803610abe75a
Matt <rocketknight1@gmail.com>
2025-05-20 16:52:36 +0100

    Revert bits of JetMOE

--- tests/models/jetmoe/test_modeling_jetmoe.py
+++ tests/models/jetmoe/test_modeling_jetmoe.py
@@ -49,9 +49,59 @@ class JetMoeModelTester(CausalLMModelTester):
         causal_lm_class = JetMoeForCausalLM
         sequence_class = JetMoeForSequenceClassification
 
-    def __init__(self, parent, hidden_act="silu", kv_channels=8, **kwargs):
-        super().__init__(parent, hidden_act=hidden_act, **kwargs)
+    def __init__(
+        self,
+        parent,
+        batch_size=13,
+        seq_length=7,
+        is_training=True,
+        use_input_mask=True,
+        use_token_type_ids=False,
+        use_labels=True,
+        vocab_size=99,
+        hidden_size=32,
+        num_hidden_layers=2,
+        num_key_value_heads=2,
+        kv_channels=8,
+        intermediate_size=37,
+        hidden_act="silu",
+        num_local_experts=4,
+        num_experts_per_tok=2,
+        max_position_embeddings=512,
+        type_vocab_size=16,
+        type_sequence_label_size=2,
+        initializer_range=0.02,
+        num_labels=3,
+        num_choices=4,
+        pad_token_id=0,
+        scope=None,
+    ):
+        super().__init__(parent)
+        self.parent = parent
+        self.batch_size = batch_size
+        self.seq_length = seq_length
+        self.is_training = is_training
+        self.use_input_mask = use_input_mask
+        self.use_token_type_ids = use_token_type_ids
+        self.use_labels = use_labels
+        self.vocab_size = vocab_size
+        self.hidden_size = hidden_size
+        self.num_hidden_layers = num_hidden_layers
         self.kv_channels = kv_channels
+        self.num_attention_heads = num_key_value_heads * num_experts_per_tok
+        self.num_key_value_heads = num_key_value_heads
+        self.intermediate_size = intermediate_size
+        self.hidden_act = hidden_act
+        self.num_local_experts = num_local_experts
+        self.num_experts_per_tok = num_experts_per_tok
+        self.max_position_embeddings = max_position_embeddings
+        self.type_vocab_size = type_vocab_size
+        self.type_sequence_label_size = type_sequence_label_size
+        self.initializer_range = initializer_range
+        self.num_labels = num_labels
+        self.num_choices = num_choices
+        self.pad_token_id = pad_token_id
+        self.scope = scope
 
 
 @require_torch
