3b9f4587fcb1a21aadc8d7f7a376d727f96e6491
Aaron V <inf3rnus@users.noreply.github.com>
2025-05-22 12:34:59 -0400

    Delete tests/utils/test_modeling_utils_parallel_loading.py

--- tests/utils/test_modeling_utils_parallel_loading.py
+++
@@ -1,9 +0,0 @@
-import os
-
-
-# Set the env variable to enable parallel loading
-os.environ["HF_ENABLE_PARALLEL_LOADING"] = "true"
-
-
-# Declare the normal model_utils.py test as a sideffect of importing the module
-from .test_modeling_utils import ModelUtilsTest  # noqa
