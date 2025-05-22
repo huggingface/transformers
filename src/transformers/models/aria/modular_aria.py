0704e51aced10767cae20b020c5e99698afd3ccc
yonigozlan <yoni.gozlan@huggingface.co>
2025-05-22 15:41:56 +0000

    fix modular aria

--- src/transformers/models/aria/modular_aria.py
+++ src/transformers/models/aria/modular_aria.py
@@ -914,7 +914,6 @@ class AriaProcessor(ProcessorMixin):
     """
 
     attributes = ["image_processor", "tokenizer"]
-    valid_kwargs = ["chat_template", "size_conversion"]
     image_processor_class = "AriaImageProcessor"
     tokenizer_class = "AutoTokenizer"
 
