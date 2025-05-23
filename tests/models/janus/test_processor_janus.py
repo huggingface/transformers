22284a3c2e50e48379e2953a4d578faa71f9dad7
yaswant19 <yaswanthgali8@gmail.com>
2025-05-23 08:35:29 +0530

    Cleanup janus testcase

--- tests/models/janus/test_processor_janus.py
+++ tests/models/janus/test_processor_janus.py
@@ -20,7 +20,6 @@ import unittest
 import numpy as np
 
 from transformers import AutoProcessor, AutoTokenizer, JanusProcessor
-from transformers.models.janus.convert_janus_weights_to_hf import CHAT_TEMPLATE
 from transformers.utils import is_vision_available
 
 from ...test_processing_common import ProcessorTesterMixin
@@ -53,11 +52,12 @@ class JanusProcessorTest(ProcessorTesterMixin, unittest.TestCase):
     def get_image_processor(self, **kwargs):
         return AutoProcessor.from_pretrained(self.tmpdirname, **kwargs).image_processor
 
-    def prepare_processor_dict(self):
-        # similar to Emu3 and Qwen2VLProcessorTest, but keep the template in the convert script to avoid duplicated code
-        return {
-            "chat_template": CHAT_TEMPLATE,
-        }
+    def get_processor(self):
+        processor = AutoProcessor.from_pretrained(self.tmpdirname)
+        # Set the processor to use the default system prompt to False as it's used based on input modality.
+        # Hence set to False to avoid any issues in the test irrespective of inputs.
+        processor.use_default_system_prompt = False
+        return processor
 
     def test_chat_template_single(self):
         """
