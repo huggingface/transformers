89deddaf6558152b4d4dbc844bf4a86dc4650677
Arthur Zucker <arthur.zucker@gmail.com>
2025-05-23 10:57:18 +0200

    fix wrong merge commit

--- tests/generation/test_utils.py
+++ tests/generation/test_utils.py
@@ -1157,8 +1157,8 @@ class GenerationTesterMixin:
                 self.skipTest(reason=f"{model_class.__name__} doesn't support caching")
 
             config.is_decoder = True
-            model = model_class._from_config(config, attn_implementation="eager")
-            config = model.config.to(torch_device).eval()
+            model = model_class._from_config(config, attn_implementation="eager").to(torch_device).eval()
+            config = model.config
             # Sets assisted generation arguments such that:
             # a) no EOS is generated, to ensure generation doesn't break early
             # b) the assistant model always generates two tokens when it is called, to ensure the input preparation of
@@ -1369,8 +1369,8 @@ class GenerationTesterMixin:
                 self.skipTest(reason=f"{model_class.__name__} doesn't support caching")
 
             config.is_decoder = True
-            model = model_class._from_config(config, attn_implementation="eager")
-            config = model.config.to(torch_device).eval()
+            model = model_class._from_config(config, attn_implementation="eager").to(torch_device).eval()
+            config = model.config
             # Sets assisted generation arguments such that:
             # a) no EOS is generated, to ensure generation doesn't break early
             # b) the assistant model always generates two tokens when it is called, to ensure the input preparation of
