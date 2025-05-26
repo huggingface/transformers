bf2eb6abbdb15e464203a652e3cf0c4f57b18453
Arthur Zucker <arthur.zucker@gmail.com>
2025-05-26 10:50:05 +0200

    nit

--- src/transformers/models/llama/modeling_llama.py
+++ src/transformers/models/llama/modeling_llama.py
@@ -323,14 +323,13 @@ class LlamaPreTrainedModel(PreTrainedModel):
     base_model_prefix = "model"
     supports_gradient_checkpointing = True
     _no_split_modules = ["LlamaDecoderLayer"]
-    _skip_keys_device_placement = ["past_key_values"]
-    _supports_flash_attn_2 = True
-    _supports_sdpa = True
-    _supports_flex_attn = True
-    _supports_cache_class = True
-    _supports_quantized_cache = True
-    _supports_static_cache = True
-    _supports_attention_backend = True
+    # _supports_flash_attn_2 = True  TODO find a way to not require this here
+    # _supports_sdpa = True
+    # _supports_flex_attn = True
+    # _supports_cache_class = True
+    # _supports_quantized_cache = True
+    # _supports_static_cache = True
+    # _supports_attention_backend = True
 
 
 
