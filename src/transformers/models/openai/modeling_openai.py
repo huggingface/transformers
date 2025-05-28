a5db3468ec0dd06363eddedeeee9207ce797d094
Arthur Zucker <arthur.zucker@gmail.com>
2025-05-28 18:21:42 +0000

    push simplifcations

--- src/transformers/models/openai/modeling_openai.py
+++ src/transformers/models/openai/modeling_openai.py
@@ -116,8 +116,6 @@ def openai_flex_attention_forward(
     dropout: float = 0.0,
     **kwargs,
 ):
-    key_states = repeat_kv(key, module.num_key_value_groups)
-    value_states = repeat_kv(value, module.num_key_value_groups)
     sink = module.sink
     def attention_sink(score, b, h, q_idx, kv_idx):
         score = torch.cat([score, sink], dim=-1)
@@ -126,8 +124,8 @@ def openai_flex_attention_forward(
     return flex_attention_forward(
         module,
         query,
-        key_states,
-        value_states,
+        key,
+        value,
         attention_mask,
         scaling=scaling,
         dropout=dropout,
@@ -136,65 +134,17 @@ def openai_flex_attention_forward(
         **kwargs,
     )
 
+ALL_ATTENTION_FUNCTIONS.register(
+    "openai_flex_attention", openai_flex_attention_forward
+)
+
 
 class OpenaiAttention(LlamaAttention):
     """Multi-headed attention from 'Attention Is All You Need' paper"""
 
     def __init__(self, config: OpenaiConfig, layer_idx: int):
         super().__init__()
-        self.sinks = torch.empty(
-            config.num_attention_heads, device=device, dtype=torch.bfloat16
-        )
-
-    def forward(
-        self,
-        hidden_states: torch.Tensor,
-        position_embeddings: Tuple[torch.Tensor, torch.Tensor],
-        attention_mask: Optional[torch.Tensor],
-        past_key_value: Optional[Cache] = None,
-        cache_position: Optional[torch.LongTensor] = None,
-        **kwargs: Unpack[FlashAttentionKwargs],
-    ) -> Tuple[torch.Tensor, Optional[torch.Tensor], Optional[Tuple[torch.Tensor]]]:
-        input_shape = hidden_states.shape[:-1]
-        hidden_shape = (*input_shape, -1, self.head_dim)
-
-        query_states = self.q_proj(hidden_states).view(hidden_shape).transpose(1, 2)
-        key_states = self.k_proj(hidden_states).view(hidden_shape).transpose(1, 2)
-        value_states = self.v_proj(hidden_states).view(hidden_shape).transpose(1, 2)
-
-        cos, sin = position_embeddings
-        query_states, key_states = apply_rotary_pos_emb(query_states, key_states, cos, sin)
-
-        if past_key_value is not None:
-            # sin and cos are specific to RoPE models; cache_position needed for the static cache
-            cache_kwargs = {"sin": sin, "cos": cos, "cache_position": cache_position}
-            key_states, value_states = past_key_value.update(key_states, value_states, self.layer_idx, cache_kwargs)
-
-        attention_interface: Callable = eager_attention_forward
-
-        if self.config._attn_implementation != "eager":
-            if self.config._attn_implementation == "sdpa" and kwargs.get("output_attentions", False):
-                logger.warning_once(
-                    "`torch.nn.functional.scaled_dot_product_attention` does not support `output_attentions=True`. Falling back to "
-                    'eager attention. This warning can be removed using the argument `attn_implementation="eager"` when loading the model.'
-                )
-            else:
-                attention_interface = ALL_ATTENTION_FUNCTIONS[self.config._attn_implementation]
-
-        attn_output, attn_weights = attention_interface(
-            self,
-            query_states,
-            key_states,
-            value_states,
-            attention_mask,
-            dropout=0.0 if not self.training else self.attention_dropout,
-            scaling=self.scaling,
-            **kwargs,
-        )
-
-        attn_output = attn_output.reshape(*input_shape, -1).contiguous()
-        attn_output = self.o_proj(attn_output)
-        return attn_output, attn_weights
+        self.sinks = torch.empty(config.num_attention_heads)
 
 
 # Copied from transformers.models.llama.modeling_llama.LlamaDecoderLayer with Llama->Openai
