37c18fb4c07e8bad7ebf452edb7fdb55d7b649df
Aryan Chauhan <chauhanaryan31801@gmail.com>
2025-05-29 01:34:42 +0000

    Apply ruff formatting to image_processing_glpn_fast.py

--- src/transformers/models/glpn/image_processing_glpn_fast.py
+++ src/transformers/models/glpn/image_processing_glpn_fast.py
@@ -6,9 +6,10 @@ class GLPNImageProcessorFast(BaseImageProcessorFast):
     """
     Fast image processor for GLPN using torch/torchvision backend.
     """
+
     resample = PILImageResampling.BICUBIC
     image_mean = IMAGENET_STANDARD_MEAN  # [0.485, 0.456, 0.406]
-    image_std = IMAGENET_STANDARD_STD    # [0.229, 0.224, 0.225]
+    image_std = IMAGENET_STANDARD_STD  # [0.229, 0.224, 0.225]
     size = {"height": 480, "width": 640}
     do_resize = True
     do_rescale = True
