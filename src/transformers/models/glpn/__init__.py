9a2f5db6ac56ba989a4c07015f9262cab8d41bdc
Aryan Chauhan <chauhanaryan31801@gmail.com>
2025-05-29 01:03:14 +0000

    Fix formatting and imports in GLPN __init__.py

--- src/transformers/models/glpn/__init__.py
+++ src/transformers/models/glpn/__init__.py
@@ -17,23 +17,11 @@ from ...utils import _LazyModule
 from ...utils.import_utils import define_import_structure
 
 
-# if TYPE_CHECKING:
-#     from .configuration_glpn import *
-#     from .feature_extraction_glpn import *
-#     from .image_processing_glpn import *
-#     from .modeling_glpn import *
-#     from .image_processing_glpn_fast import GLPNImageProcessorFast
-# else:
-#     import sys
-
-#     _file = globals()["__file__"]
-#     sys.modules[__name__] = _LazyModule(__name__, _file, define_import_structure(_file), module_spec=__spec__)
-
 import_structure = {
     "configuration_glpn": ["GLPNConfig"],
     "feature_extraction_glpn": ["GLPNFeatureExtractor"],
     "image_processing_glpn": ["GLPNImageProcessor"],
-    "image_processing_glpn_fast": ["GLPNImageProcessorFast"], 
+    "image_processing_glpn_fast": ["GLPNImageProcessorFast"],
     "modeling_glpn": [
         "GLPNModel",
         "GLPNForDepthEstimation",
@@ -47,13 +35,13 @@ if TYPE_CHECKING:
     from .image_processing_glpn import GLPNImageProcessor
     from .image_processing_glpn_fast import GLPNImageProcessorFast
     from .modeling_glpn import (
-        GLPNModel,
         GLPNForDepthEstimation,
+        GLPNModel,
         GLPNPreTrainedModel,
     )
 else:
-    import sys
     import os
+    import sys
 
     _file = globals()["__file__"]
     sys.modules[__name__] = _LazyModule(__name__, _file, import_structure, module_spec=__spec__)
