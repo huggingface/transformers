7d245b566610c1868b638c492c6dc1d410d5d8a4
ydshieh <ydshieh@users.noreply.github.com>
2025-05-31 10:21:46 +0200

    fix more: regex 15

--- src/transformers/models/kosmos2/modeling_kosmos2.py
+++ src/transformers/models/kosmos2/modeling_kosmos2.py
@@ -1493,7 +1493,7 @@ class Kosmos2TextForCausalLM(Kosmos2PreTrainedModel, GenerationMixin):
         lm_logits = self.lm_head(outputs[0])
 
         loss = None
-        if labels is not None:
+        if labels is not None or kwargs.get("shift_labels", None) is not None:
             loss = self.loss_function(logits=lm_logits, labels=labels, vocab_size=self.config.vocab_size, **kwargs)
 
         return CausalLMOutputWithCrossAttentions(
