371fea18cbbf0b5132894daab683b87d23dd9d4c
ydshieh <ydshieh@users.noreply.github.com>
2025-05-31 22:37:02 +0200

    fix 12: _supports_sdpa=False

--- src/transformers/models/blip_2/modeling_blip_2.py
+++ src/transformers/models/blip_2/modeling_blip_2.py
@@ -1876,6 +1876,7 @@ class Blip2VisionModelWithProjection(Blip2PreTrainedModel):
 class Blip2ForConditionalGeneration(Blip2PreTrainedModel, GenerationMixin):
     config_class = Blip2Config
     main_input_name = "pixel_values"
+    _supports_sdpa = False
     _supports_cache_class = True
     _supports_static_cache = True
     _supports_quantized_cache = False  # not all LM bacbones support (e.g. T5)
