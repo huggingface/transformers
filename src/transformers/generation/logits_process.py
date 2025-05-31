616e70a03c6c1be99953b5a390bb55b0c5577337
Jaeyong Sung <jaeyong0201@gmail.com>
2025-05-31 10:52:54 -0400

    make RepetitionPenaltyLogitsProcessor flexible
    - can accept 3D scores like (batch_size, channel, vocab)

--- src/transformers/generation/logits_process.py
+++ src/transformers/generation/logits_process.py
@@ -360,12 +360,12 @@ class RepetitionPenaltyLogitsProcessor(LogitsProcessor):
         if self.prompt_ignore_length:
             input_ids = input_ids[:, self.prompt_ignore_length :]
 
-        score = torch.gather(scores, 1, input_ids)
+        score = torch.gather(scores, -1, input_ids)
 
         # if score < 0 then repetition penalty has to be multiplied to reduce the token probabilities
         score = torch.where(score < 0, score * self.penalty, score / self.penalty)
 
-        scores_processed = scores.scatter(1, input_ids, score)
+        scores_processed = scores.scatter(-1, input_ids, score)
         return scores_processed
 
 
