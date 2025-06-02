52199d360777c82d9c466473ce16194417f4816d
Manuel de Prada Corral <6536835+manueldeprada@users.noreply.github.com>
2025-06-02 12:18:06 +0200

    Update src/transformers/cache_utils.py

--- src/transformers/cache_utils.py
+++ src/transformers/cache_utils.py
@@ -12,8 +12,6 @@ from packaging import version
 from transformers.pytorch_utils import is_torch_greater_or_equal_than_2_6
 
 from .configuration_utils import PretrainedConfig
-
-# For backwards compatibility
 from .utils import is_hqq_available, is_optimum_quanto_available, is_torch_greater_or_equal, logging
 
 
