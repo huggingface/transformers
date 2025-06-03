feefeab28c6d170df922b0ea6f8e1031815d3b22
Korbinian Poeppel <korbinian.poeppel@nx-ai.com>
2025-06-03 09:28:31 +0000

    Chore: Fix style.

--- src/transformers/models/auto/tokenization_auto.py
+++ src/transformers/models/auto/tokenization_auto.py
@@ -54,7 +54,7 @@ else:
 logger = logging.get_logger(__name__)
 
 
-            
+
 # Explicit rather than inferred generics to significantly improves completion suggestion performance for language servers.
 TOKENIZER_MAPPING_NAMES = OrderedDict[str, tuple[Optional[str], Optional[str]]](
     [
