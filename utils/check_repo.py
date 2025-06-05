ca8ae9cbeaad38f2e982f27988dfdf022284fe94
zRzRzRzRzRzRzR <2448370773@qq.com>
2025-06-05 16:33:34 +0800

    update with remove

--- utils/check_repo.py
+++ utils/check_repo.py
@@ -142,7 +142,6 @@ IGNORE_NON_TESTED = (
         "SeamlessM4TCodeHifiGan",  # Building part of bigger (tested) model.
         "SeamlessM4TTextToUnitForConditionalGeneration",  # Building part of bigger (tested) model.
         "ChameleonVQVAE",  # VQVAE here is used only for encoding (discretizing) and is tested as part of bigger model
-        "Glm4vModel",  # Building part of bigger (tested) model. Tested implicitly through Glm4vForConditionalGeneration.
         "Qwen2VLModel",  # Building part of bigger (tested) model. Tested implicitly through Qwen2VLForConditionalGeneration.
         "Qwen2_5_VLModel",  # Building part of bigger (tested) model. Tested implicitly through Qwen2_5_VLForConditionalGeneration.
         "Qwen2_5OmniForConditionalGeneration",  # Not a regular model. Testted in Qwen2_5OmniModelIntergrationTest
