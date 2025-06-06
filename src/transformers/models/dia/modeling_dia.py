d46de542cfddba6ca3d93d26712d707a886086b9
Vasqu <antonprogamer@gmail.com>
2025-06-06 19:57:18 +0200

    proof of concept rope

--- src/transformers/models/dia/modeling_dia.py
+++ src/transformers/models/dia/modeling_dia.py
@@ -345,17 +345,16 @@ class DiaRMSNorm(nn.Module):
         return f"{tuple(self.weight.shape)}, eps={self.variance_epsilon}"
 
 
-def apply_rotary_pos_emb(
+"""def apply_rotary_pos_emb(
     tensor: torch.Tensor, cos: torch.Tensor, sin: torch.Tensor, unsqueeze_dim: int = 1
 ) -> torch.Tensor:
     first_half, second_half = torch.chunk(tensor.to(torch.float32), 2, dim=-1)
     first_part = first_half * cos - second_half * sin
     second_part = second_half * cos + first_half * sin
     return torch.cat((first_part.to(tensor.dtype), second_part.to(tensor.dtype)), dim=-1)
+"""
 
-
-# TODO: refactor RoPE to transformers rope?
-class DiaRotaryEmbedding(nn.Module):
+class DiaRotaryEmbedding2(nn.Module):
     def __init__(self, config: Union[DiaEncoderConfig, DiaDecoderConfig], device: Optional[torch.device] = None):
         super().__init__()
         self.embedding_dims = config.head_dim
@@ -375,6 +374,67 @@ class DiaRotaryEmbedding(nn.Module):
         )
         cos, sin = full_freqs.cos(), full_freqs.sin()
         return cos, sin
+
+
+# TODO: move fully to transformers RoPE, proof of concept
+class DiaRotaryEmbedding(nn.Module):
+    def __init__(self, config, device=None):
+        super().__init__()
+        self.config = config
+
+        inv_freq = 1.0 / config.rope_max_timescale ** (torch.arange(0, config.head_dim, 2, dtype=torch.int64).to(dtype=torch.float) / config.head_dim)
+        self.register_buffer("inv_freq", inv_freq, persistent=False)
+        self.original_inv_freq = self.inv_freq
+
+    @torch.no_grad()
+    def forward(self, x, position_ids):
+        inv_freq_expanded = self.inv_freq[None, :, None].float().expand(position_ids.shape[0], -1, 1).to(x.device)
+        position_ids_expanded = position_ids[:, None, :].float()
+
+        device_type = x.device.type if isinstance(x.device.type, str) and x.device.type != "mps" else "cpu"
+        with torch.autocast(device_type=device_type, enabled=False):  # Force float32
+            freqs = (inv_freq_expanded.float() @ position_ids_expanded.float()).transpose(1, 2)
+            emb = torch.cat((freqs, freqs), dim=-1)
+            cos = emb.cos()
+            sin = emb.sin()
+
+        return cos.to(dtype=x.dtype), sin.to(dtype=x.dtype)
+
+
+# Copied from transformers.models.llama.modeling_llama.rotate_half
+def rotate_half(x):
+    """Rotates half the hidden dims of the input."""
+    x1 = x[..., : x.shape[-1] // 2]
+    x2 = x[..., x.shape[-1] // 2 :]
+    return torch.cat((-x2, x1), dim=-1)
+
+
+# Copied from transformers.models.llama.modeling_llama.apply_rotary_pos_emb
+def apply_rotary_pos_emb(q, k, cos, sin, position_ids=None, unsqueeze_dim=1):
+    """Applies Rotary Position Embedding to the query and key tensors.
+
+    Args:
+        q (`torch.Tensor`): The query tensor.
+        k (`torch.Tensor`): The key tensor.
+        cos (`torch.Tensor`): The cosine part of the rotary embedding.
+        sin (`torch.Tensor`): The sine part of the rotary embedding.
+        position_ids (`torch.Tensor`, *optional*):
+            Deprecated and unused.
+        unsqueeze_dim (`int`, *optional*, defaults to 1):
+            The 'unsqueeze_dim' argument specifies the dimension along which to unsqueeze cos[position_ids] and
+            sin[position_ids] so that they can be properly broadcasted to the dimensions of q and k. For example, note
+            that cos[position_ids] and sin[position_ids] have the shape [batch_size, seq_len, head_dim]. Then, if q and
+            k have the shape [batch_size, heads, seq_len, head_dim], then setting unsqueeze_dim=1 makes
+            cos[position_ids] and sin[position_ids] broadcastable to the shapes of q and k. Similarly, if q and k have
+            the shape [batch_size, seq_len, heads, head_dim], then set unsqueeze_dim=2.
+    Returns:
+        `tuple(torch.Tensor)` comprising of the query and key tensors rotated using the Rotary Position Embedding.
+    """
+    cos = cos.unsqueeze(unsqueeze_dim)
+    sin = sin.unsqueeze(unsqueeze_dim)
+    q_embed = (q * cos) + (rotate_half(q) * sin)
+    k_embed = (k * cos) + (rotate_half(k) * sin)
+    return q_embed, k_embed
 
 
 # Copied from transformers.models.llama.modeling_llama.repeat_kv
@@ -447,13 +507,12 @@ class DiaSelfAttention(nn.Module):  # Modular : LlamaAttentions
         input_shape = hidden_states.shape[:-1]
         hidden_shape = (*input_shape, -1, self.head_dim)
 
-        query_states = self.q_proj(hidden_states).view(hidden_shape)
-        key_states = self.k_proj(hidden_states).view(hidden_shape)
+        query_states = self.q_proj(hidden_states).view(hidden_shape).transpose(1, 2)
+        key_states = self.k_proj(hidden_states).view(hidden_shape).transpose(1, 2)
         value_states = self.v_proj(hidden_states).view(hidden_shape).transpose(1, 2)
 
         cos, sin = position_embeddings
-        query_states = apply_rotary_pos_emb(query_states, cos, sin, -2).transpose(1, 2)
-        key_states = apply_rotary_pos_emb(key_states, cos, sin, -2).transpose(1, 2)
+        query_states, key_states = apply_rotary_pos_emb(query_states, key_states, cos, sin)
 
         if past_key_values is not None:
             # sin and cos are specific to RoPE models; cache_position needed for the static cache
@@ -615,10 +674,8 @@ class DiaEncoder(DiaPreTrainedModel):
         # RoPE
         # Note: We expect right padding and hence always generate
         # the position ids on the fly to reduce preparation overhead
-        position_ids = torch.arange(input_ids.shape[-1], device=input_ids.device)[None, :].expand(
-            input_ids.shape[0], -1
-        )
-        position_embeddings = self.rotary_embeddings(position_ids)
+        position_ids = torch.arange(input_ids.shape[-1], device=input_ids.device)[None, :]
+        position_embeddings = self.rotary_embeddings(hidden_states, position_ids)
 
         attention_mask = self._update_full_mask(
             attention_mask,
@@ -747,11 +804,11 @@ class DiaDecoder(DiaPreTrainedModel):
                 past_key_values_length, past_key_values_length + seq_length, device=self.device
             )
         if position_ids is None:
-            position_ids = cache_position[None, :].expand(batch_size, -1)
+            position_ids = cache_position[None, :]
 
         # RoPE
         hidden_states = self.embeddings(input_ids)
-        position_embeddings = self.rotary_embeddings(position_ids)
+        position_embeddings = self.rotary_embeddings(hidden_states, position_ids)
 
         if attention_mask is None and not is_torchdynamo_compiling():
             # required mask seq length can be calculated via length of past cache
