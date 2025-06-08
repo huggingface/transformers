03898267a77c869395a023dfb1a8b33766088d79
ydshieh <ydshieh@users.noreply.github.com>
2025-06-08 13:59:50 +0200

    ruff

--- tests/models/aya_vision/test_processor_aya_vision.py
+++ tests/models/aya_vision/test_processor_aya_vision.py
@@ -17,7 +17,7 @@ import tempfile
 import unittest
 
 from transformers import AutoProcessor, AutoTokenizer, AyaVisionProcessor
-from transformers.testing_utils import require_read_token, require_torch, require_vision
+from transformers.testing_utils import require_torch, require_vision
 from transformers.utils import is_torch_available, is_vision_available
 
 from ...test_processing_common import ProcessorTesterMixin
