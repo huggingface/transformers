6a27b39d3a0b400ddd8e4ce339166a7ed60bc2a2
Arynz-C <alifrp111@gmail.com>
2025-06-09 13:36:33 +0700

    Register FirefliesModel in MODEL_MAPPING_NAMES

--- src/transformers/models/fireflies/configuration_fireflies.py
+++ src/transformers/models/fireflies/configuration_fireflies.py
@@ -1,20 +1,10 @@
-from transformers.configuration_utils import PretrainedConfig
+from transformers import PretrainedConfig
 
 class FirefliesConfig(PretrainedConfig):
     model_type = "fireflies"
 
-    def __init__(
-        self,
-        vocab_size=50257,
-        d_model=1024,
-        n_heads=16,
-        num_layers=24,
-        pad_token_id=0,
-        bos_token_id=1,
-        eos_token_id=2,
-        **kwargs
-    ):
-        super().__init__(pad_token_id=pad_token_id, bos_token_id=bos_token_id, eos_token_id=eos_token_id, **kwargs)
+    def __init__(self, vocab_size=50257, d_model=1024, n_heads=19, num_layers=13, **kwargs):
+        super().__init__(**kwargs)
         self.vocab_size = vocab_size
         self.d_model = d_model
         self.n_heads = n_heads
