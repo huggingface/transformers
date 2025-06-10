47a71f78e92a2fc63435c3a26084460cb4583cd5
Mehant Kammakomati <mehant.kammakomati2@ibm.com>
2025-06-10 12:41:17 +0530

    refactor: use slice API
    Signed-off-by: Mehant Kammakomati <mehant.kammakomati2@ibm.com>

--- src/transformers/integrations/tensor_parallel.py
+++ src/transformers/integrations/tensor_parallel.py
@@ -348,6 +348,8 @@ def get_tensor_shard(param, empty_param, device_mesh, rank, dim):
 
     shard_size = math.ceil(empty_param.shape[dim] / world_size)
     start = rank * shard_size
+
+    # Construct slicing index dynamically
     end = min(start + shard_size, empty_param.shape[dim])
     slice_indices = [slice(None)] * param_dim
     if start < empty_param.shape[dim]:
