bb2cff87e373821872414f834462a8af0e43a40a
Guang Yang <guangyang@meta.com>
2025-06-11 10:19:55 -0700

    use static cache if has layer_types but no sliding_window

--- src/transformers/integrations/executorch.py
+++ src/transformers/integrations/executorch.py
@@ -56,19 +56,15 @@ class TorchExportableModuleForDecoderOnlyLM(torch.nn.Module):
         if not hasattr(model.config, "use_cache") or model.config.use_cache is False:
             raise ValueError("The model must have caching enabled to be performant.")
 
-        if not hasattr(model.config, "cache_implementation"):
-            # If `cache_implementation` is not specified explicitly in the config, `DynamicCache` will
-            # be used by default, so export will use `StaticCache` by default.
-            logging.info("Using `StaticCache` for export as `cache_implementation` is not specified in the config.")
-            self.model = TorchExportableModuleWithStaticCache(model)
+        if hasattr(model.config, "layer_types") and getattr(model.config, "sliding_window", None) is not None:
+            self.model = TorchExportableModuleWithHybridCache(model, max_batch_size, max_cache_len)
         else:
-            if model.config.cache_implementation == "hybrid":
-                self.model = TorchExportableModuleWithHybridCache(model, max_batch_size, max_cache_len)
-            else:
-                raise ValueError(
-                    f"Unsupported cache implementation: {model.config.cache_implementation}. "
-                    "Please use `hybrid` or `static`."
-                )
+            # If `layer_types` is not specified explicitly in the config or `sliding_window` is null,
+            # there is only 1 type of layers, so export will use `StaticCache` by default.
+            logging.info(
+                "Using `StaticCache` for export as `layer_types` is not specified or `sliding_window` is `null` in the config."
+            )
+            self.model = TorchExportableModuleWithStaticCache(model)
 
     def forward(
         self,
@@ -405,12 +401,6 @@ class TorchExportableModuleWithHybridCache(torch.nn.Module):
         # Verify the model is configured for HybridCache
         if not self.model.config.use_cache:
             raise AssertionError("Model must have caching enabled")
-
-        if (
-            not hasattr(self.model.config, "cache_implementation")
-            or self.model.config.cache_implementation != "hybrid"
-        ):
-            raise AssertionError("Model must use 'hybrid' cache implementation")
 
         # Initialize the HybridCache
         self.cache = HybridCache(
@@ -813,6 +803,7 @@ def sdpa_mask_without_vmap(
         raise ValueError("Cannot use both `sliding_window` and `attention_chunk_size`")
 
     # Simplest and most efficient way to obtain a causal mask
+    kv_arange = kv_arange.view(1, -1)
     causal_mask = kv_arange <= reshaped_cache_position
     # If using sliding window, add the sliding mask
     if sliding_window is not None:
