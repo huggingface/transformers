70abb06151c82275bfeab512675d4453959c6f7e
Shuming Hu <shumingh@gmail.com>
2025-06-12 21:16:35 +0000

    Image processing refactoring to use more common parts.

--- src/transformers/models/perception_lm/image_processing_perception_lm_fast.py
+++ src/transformers/models/perception_lm/image_processing_perception_lm_fast.py
@@ -33,33 +33,37 @@ from ...utils import (
     add_start_docstrings,
     is_torch_available,
 )
+from ...image_utils import PILImageResampling, IMAGENET_STANDARD_MEAN, IMAGENET_STANDARD_STD
 
 if is_torch_available():
     import torch
 
 class PerceptionLMFastImageProcessorKwargs(DefaultFastImageProcessorKwargs):
     vision_input_type: str = "thumb+tile"
-    image_res: int = 448
     max_num_tiles: int = 36
-    normalize_img: bool = True
+
 
 @add_start_docstrings(
     "Constructs a fast PerceptionLM image processor.",
-    """
-        do_pad (`bool`, *optional*, defaults to `self.do_pad`):
-            Whether to pad the image to a square based on the longest edge. Can be overridden by the `do_pad` parameter
-    """,
 )
 class PerceptionLMImageProcessorFast(BaseImageProcessorFast):
+    resample = PILImageResampling.BICUBIC
+    image_mean = IMAGENET_STANDARD_MEAN
+    image_std = IMAGENET_STANDARD_STD
+    do_resize = True
+    do_center_crop = False
+    do_rescale = True
+    do_normalize = True
+    do_convert_rgb = True
     valid_kwargs = PerceptionLMFastImageProcessorKwargs
+
 
     def __init__(self, **kwargs: Unpack[PerceptionLMFastImageProcessorKwargs]) -> None:
         super().__init__(**kwargs)
         self.image_transform = get_image_transform(
             vision_input_type=kwargs.get("vision_input_type", "thumb+tile"),
-            image_res=kwargs.get("image_res", 448),
+            image_res=kwargs.get("tile_size", 448),
             max_num_tiles=kwargs.get("max_num_tiles", 36),
-            normalize_img=kwargs.get("normalize_img", True),
         )
 
     def to_dict(self):
@@ -70,6 +74,12 @@ class PerceptionLMImageProcessorFast(BaseImageProcessorFast):
     def _preprocess(
         self,
         images: List["torch.Tensor"],
+        do_resize: bool,
+        do_rescale: Optional[bool],
+        rescale_factor: Optional[Union[int, float]],
+        do_normalize: Optional[bool],
+        image_mean: Optional[Union[float, list[float]]],
+        image_std: Optional[Union[float, list[float]]],
         return_tensors: Optional[Union[str, TensorType]],
         **kwargs: Unpack[PerceptionLMFastImageProcessorKwargs]
     ) -> BatchFeature:
@@ -77,11 +87,23 @@ class PerceptionLMImageProcessorFast(BaseImageProcessorFast):
         del kwargs
         if images:
             grouped_images, grouped_images_index = group_images_by_shape(images)
+            resized_images_grouped = {}
+            for shape, stacked_images in grouped_images.items():
+                if do_resize:
+                    stacked_images, _ = self.image_transform(stacked_images)
+                resized_images_grouped[shape] = stacked_images
+            resized_images = reorder_images(resized_images_grouped, grouped_images_index)
+                
+            grouped_images, grouped_images_index = group_images_by_shape(resized_images)
             processed_images_grouped = {}
             for shape, stacked_images in grouped_images.items():
-                stacked_images, _ = self.image_transform(stacked_images)
+                # Fused rescale and normalize
+                stacked_images = self.rescale_and_normalize(
+                    stacked_images, do_rescale, rescale_factor, do_normalize, image_mean, image_std
+                )
                 processed_images_grouped[shape] = stacked_images
             processed_images = reorder_images(processed_images_grouped, grouped_images_index)
+            
             processed_images = torch.stack(processed_images, dim=0) if return_tensors else processed_images
             return BatchFeature(data={"pixel_values": processed_images}, tensor_type=return_tensors)
         else:
