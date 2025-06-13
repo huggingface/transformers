0f4e4076637a6e1c0edd85230ff63c2190706691
Shuming Hu <shumingh@gmail.com>
2025-06-13 04:39:12 +0000

    update tests to use model from hub

--- tests/models/perception_lm/test_image_processing_perception_lm.py
+++ tests/models/perception_lm/test_image_processing_perception_lm.py
@@ -1,4 +1,4 @@
-# Copyright 2025 HuggingFace Inc.
+# Copyright 2024 HuggingFace Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,28 +12,25 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
 import unittest
-from typing import Union
 
 import numpy as np
 
+from transformers.image_utils import IMAGENET_STANDARD_MEAN, IMAGENET_STANDARD_STD, ChannelDimension
 from transformers.testing_utils import require_torch, require_vision
-from transformers.utils import is_torchvision_available, is_vision_available
+from transformers.utils import is_torch_available, is_torchvision_available, is_vision_available
 
 from ...test_image_processing_common import ImageProcessingTestMixin, prepare_image_inputs
 
 
+if is_torch_available():
+    import torch
+
 if is_vision_available():
     from PIL import Image
 
-    from transformers import PerceptionLMImageProcessor
-
     if is_torchvision_available():
-        from torchvision.transforms import functional as F
-
         from transformers import PerceptionLMImageProcessorFast
-
 
 class PerceptionLMImageProcessingTester:
     def __init__(
@@ -44,30 +41,23 @@ class PerceptionLMImageProcessingTester:
         image_size=18,
         min_resolution=30,
         max_resolution=400,
-        do_pad=True,
         do_resize=True,
-        size=None,
-        do_center_crop=True,
-        crop_size=None,
+        tile_size=None,
         do_normalize=True,
-        image_mean=[0.48145466, 0.4578275, 0.40821073],
-        image_std=[0.26862954, 0.26130258, 0.27577711],
+        image_mean=IMAGENET_STANDARD_MEAN,
+        image_std=IMAGENET_STANDARD_STD,
         do_convert_rgb=True,
     ):
         super().__init__()
         size = size if size is not None else {"shortest_edge": 20}
-        crop_size = crop_size if crop_size is not None else {"height": 18, "width": 18}
         self.parent = parent
         self.batch_size = batch_size
         self.num_channels = num_channels
         self.image_size = image_size
         self.min_resolution = min_resolution
         self.max_resolution = max_resolution
-        self.do_pad = do_pad
         self.do_resize = do_resize
-        self.size = size
-        self.do_center_crop = do_center_crop
-        self.crop_size = crop_size
+        self.tile_size = size
         self.do_normalize = do_normalize
         self.image_mean = image_mean
         self.image_std = image_std
@@ -75,11 +65,8 @@ class PerceptionLMImageProcessingTester:
 
     def prepare_image_processor_dict(self):
         return {
-            "do_pad": self.do_pad,
             "do_resize": self.do_resize,
-            "size": self.size,
-            "do_center_crop": self.do_center_crop,
-            "crop_size": self.crop_size,
+            "tile_size": self.size,
             "do_normalize": self.do_normalize,
             "image_mean": self.image_mean,
             "image_std": self.image_std,
@@ -89,6 +76,7 @@ class PerceptionLMImageProcessingTester:
     def expected_output_image_shape(self, images):
         return self.num_channels, self.crop_size["height"], self.crop_size["width"]
 
+    # Copied from tests.models.clip.test_image_processing_clip.CLIPImageProcessingTester.prepare_image_inputs
     def prepare_image_inputs(self, equal_resolution=False, numpify=False, torchify=False):
         return prepare_image_inputs(
             batch_size=self.batch_size,
@@ -103,33 +91,30 @@ class PerceptionLMImageProcessingTester:
 
 @require_torch
 @require_vision
-# Copied from tests.models.clip.test_image_processing_clip.CLIPImageProcessingTest with CLIP->PerceptionLM
 class PerceptionLMImageProcessingTest(ImageProcessingTestMixin, unittest.TestCase):
-    image_processing_class = PerceptionLMImageProcessor if is_vision_available() else None
     fast_image_processing_class = PerceptionLMImageProcessorFast if is_torchvision_available() else None
 
+    # Copied from tests.models.clip.test_image_processing_clip.CLIPImageProcessingTest.setUp with CLIP->LlavaNext
     def setUp(self):
         super().setUp()
         self.image_processor_tester = PerceptionLMImageProcessingTester(self)
 
     @property
+    # Copied from tests.models.clip.test_image_processing_clip.CLIPImageProcessingTest.image_processor_dict
     def image_processor_dict(self):
         return self.image_processor_tester.prepare_image_processor_dict()
 
-    # Ignore copy
     def test_image_processor_properties(self):
         for image_processing_class in self.image_processor_list:
             image_processing = image_processing_class(**self.image_processor_dict)
-            self.assertTrue(hasattr(image_processing, "do_pad"))
             self.assertTrue(hasattr(image_processing, "do_resize"))
-            self.assertTrue(hasattr(image_processing, "size"))
-            self.assertTrue(hasattr(image_processing, "do_center_crop"))
-            self.assertTrue(hasattr(image_processing, "center_crop"))
+            self.assertTrue(hasattr(image_processing, "tile_size"))
             self.assertTrue(hasattr(image_processing, "do_normalize"))
             self.assertTrue(hasattr(image_processing, "image_mean"))
             self.assertTrue(hasattr(image_processing, "image_std"))
             self.assertTrue(hasattr(image_processing, "do_convert_rgb"))
 
+    # Copied from tests.models.clip.test_image_processing_clip.CLIPImageProcessingTest.test_image_processor_from_dict_with_kwargs
     def test_image_processor_from_dict_with_kwargs(self):
         for image_processing_class in self.image_processor_list:
             image_processor = image_processing_class.from_dict(self.image_processor_dict)
@@ -140,97 +125,120 @@ class PerceptionLMImageProcessingTest(ImageProcessingTestMixin, unittest.TestCas
             self.assertEqual(image_processor.size, {"shortest_edge": 42})
             self.assertEqual(image_processor.crop_size, {"height": 84, "width": 84})
 
-    # Ignore copy
-    def test_padding(self):
-        """
-        LLaVA needs to pad images to square size before processing as per orig implementation.
-        Checks that image processor pads images correctly given different background colors.
-        """
-
-        # taken from original implementation: https://github.com/haotian-liu/LLaVA/blob/c121f0432da27facab705978f83c4ada465e46fd/perception_lm/mm_utils.py#L152
-        def pad_to_square_original(
-            image: Image.Image, background_color: Union[int, tuple[int, int, int]] = 0
-        ) -> Image.Image:
-            width, height = image.size
-            if width == height:
-                return image
-            elif width > height:
-                result = Image.new(image.mode, (width, width), background_color)
-                result.paste(image, (0, (width - height) // 2))
-                return result
-            else:
-                result = Image.new(image.mode, (height, height), background_color)
-                result.paste(image, ((height - width) // 2, 0))
-                return result
-
-        for i, image_processing_class in enumerate(self.image_processor_list):
-            image_processor = image_processing_class.from_dict(self.image_processor_dict)
-            numpify = i == 0
-            torchify = i == 1
-            image_inputs = self.image_processor_tester.prepare_image_inputs(
-                equal_resolution=False, numpify=numpify, torchify=torchify
-            )
-
-            # test with images in channel-last and channel-first format (only channel-first for torch)
+    def test_call_pil(self):
+        for image_processing_class in self.image_processor_list:
+            # Initialize image_processing
+            image_processing = image_processing_class(**self.image_processor_dict)
+            # create random PIL images
+            image_inputs = self.image_processor_tester.prepare_image_inputs(equal_resolution=True)
             for image in image_inputs:
-                padded_image = image_processor.pad_to_square(image)
-                if i == 0:
-                    padded_image_original = pad_to_square_original(Image.fromarray(image))
-                    padded_image_original = np.array(padded_image_original)
+                self.assertIsInstance(image, Image.Image)
 
-                    np.testing.assert_allclose(padded_image, padded_image_original)
+            # Test not batched input
+            encoded_images = image_processing(image_inputs[0], return_tensors="pt").pixel_values
+            expected_output_image_shape = (1, 1445, 3, 18, 18)
+            self.assertEqual(tuple(encoded_images.shape), expected_output_image_shape)
 
-                    padded_image = image_processor.pad_to_square(
-                        image.transpose(2, 0, 1), input_data_format="channels_first"
-                    )
-                    padded_image = padded_image.transpose(1, 2, 0)
+            # Test batched
+            encoded_images = image_processing(image_inputs, return_tensors="pt").pixel_values
+            expected_output_image_shape = (7, 1445, 3, 18, 18)
+            self.assertEqual(tuple(encoded_images.shape), expected_output_image_shape)
 
-                    np.testing.assert_allclose(padded_image, padded_image_original)
-                else:
-                    padded_image_original = pad_to_square_original(F.to_pil_image(image))
-                    padded_image = padded_image.permute(1, 2, 0)
-                    np.testing.assert_allclose(padded_image, padded_image_original)
-
-            # test background color
-            background_color = (122, 116, 104)
+    def test_call_numpy(self):
+        for image_processing_class in self.image_processor_list:
+            # Initialize image_processing
+            image_processing = image_processing_class(**self.image_processor_dict)
+            # create random numpy tensors
+            image_inputs = self.image_processor_tester.prepare_image_inputs(equal_resolution=True, numpify=True)
             for image in image_inputs:
-                padded_image = image_processor.pad_to_square(image, background_color=background_color)
-                if i == 0:
-                    padded_image_original = pad_to_square_original(
-                        Image.fromarray(image), background_color=background_color
-                    )
-                else:
-                    padded_image_original = pad_to_square_original(
-                        F.to_pil_image(image), background_color=background_color
-                    )
-                    padded_image = padded_image.permute(1, 2, 0)
-                padded_image_original = np.array(padded_image_original)
+                self.assertIsInstance(image, np.ndarray)
 
-                np.testing.assert_allclose(padded_image, padded_image_original)
+            # Test not batched input
+            encoded_images = image_processing(image_inputs[0], return_tensors="pt").pixel_values
+            expected_output_image_shape = (1, 1445, 3, 18, 18)
+            self.assertEqual(tuple(encoded_images.shape), expected_output_image_shape)
 
-            background_color = 122
+            # Test batched
+            encoded_images = image_processing(image_inputs, return_tensors="pt").pixel_values
+            expected_output_image_shape = (7, 1445, 3, 18, 18)
+            self.assertEqual(tuple(encoded_images.shape), expected_output_image_shape)
+
+    def test_call_pytorch(self):
+        for image_processing_class in self.image_processor_list:
+            # Initialize image_processing
+            image_processing = image_processing_class(**self.image_processor_dict)
+            # create random PyTorch tensors
+            image_inputs = self.image_processor_tester.prepare_image_inputs(equal_resolution=True, torchify=True)
+
             for image in image_inputs:
-                padded_image = image_processor.pad_to_square(image, background_color=background_color)
-                if i == 0:
-                    padded_image_original = pad_to_square_original(
-                        Image.fromarray(image), background_color=background_color
-                    )
-                else:
-                    padded_image_original = pad_to_square_original(
-                        F.to_pil_image(image), background_color=background_color
-                    )
-                    padded_image = padded_image.permute(1, 2, 0)
-                padded_image_original = np.array(padded_image_original)
-                np.testing.assert_allclose(padded_image, padded_image_original)
+                self.assertIsInstance(image, torch.Tensor)
 
-            # background color length should match channel length
-            with self.assertRaises(ValueError):
-                padded_image = image_processor.pad_to_square(image_inputs[0], background_color=(122, 104))
+            # Test not batched input
+            encoded_images = image_processing(image_inputs[0], return_tensors="pt").pixel_values
+            expected_output_image_shape = (1, 1445, 3, 18, 18)
+            self.assertEqual(tuple(encoded_images.shape), expected_output_image_shape)
 
-            with self.assertRaises(ValueError):
-                padded_image = image_processor.pad_to_square(image_inputs[0], background_color=(122, 104, 0, 0))
+            # Test batched
+            encoded_images = image_processing(image_inputs, return_tensors="pt").pixel_values
+            expected_output_image_shape = (7, 1445, 3, 18, 18)
+            self.assertEqual(tuple(encoded_images.shape), expected_output_image_shape)
 
-    @unittest.skip(reason="PerceptionLM does not support 4 channel images yet")
-    # Ignore copy
+    @unittest.skip(
+        reason="LlavaNextImageProcessor doesn't treat 4 channel PIL and numpy consistently yet"
+    )  # FIXME Amy
     def test_call_numpy_4_channels(self):
         pass
+
+    def test_nested_input(self):
+        for image_processing_class in self.image_processor_list:
+            image_processing = image_processing_class(**self.image_processor_dict)
+            image_inputs = self.image_processor_tester.prepare_image_inputs(equal_resolution=True)
+
+            # Test batched as a list of images
+            encoded_images = image_processing(image_inputs, return_tensors="pt").pixel_values
+            expected_output_image_shape = (7, 1445, 3, 18, 18)
+            self.assertEqual(tuple(encoded_images.shape), expected_output_image_shape)
+
+            # Test batched as a nested list of images, where each sublist is one batch
+            image_inputs_nested = [image_inputs[:3], image_inputs[3:]]
+            encoded_images_nested = image_processing(image_inputs_nested, return_tensors="pt").pixel_values
+            expected_output_image_shape = (7, 1445, 3, 18, 18)
+            self.assertEqual(tuple(encoded_images_nested.shape), expected_output_image_shape)
+
+            # Image processor should return same pixel values, independently of ipnut format
+            self.assertTrue((encoded_images_nested == encoded_images).all())
+
+    def test_pad_for_patching(self):
+        for image_processing_class in self.image_processor_list:
+            if image_processing_class == self.fast_image_processing_class:
+                numpify = False
+                torchify = True
+                input_data_format = image_processing_class.data_format
+            else:
+                numpify = True
+                torchify = False
+                input_data_format = ChannelDimension.LAST
+            image_processing = image_processing_class(**self.image_processor_dict)
+            # Create odd-sized images
+            image_input = self.image_processor_tester.prepare_image_inputs(
+                equal_resolution=True,
+                numpify=numpify,
+                torchify=torchify,
+            )[0]
+            self.assertIn(image_input.shape, [(3, 400, 400), (400, 400, 3)])
+
+            # Test odd-width
+            image_shape = (400, 601)
+            encoded_images = image_processing._pad_for_patching(image_input, image_shape, input_data_format)
+            encoded_image_shape = (
+                encoded_images.shape[:-1] if input_data_format == ChannelDimension.LAST else encoded_images.shape[1:]
+            )
+            self.assertEqual(encoded_image_shape, image_shape)
+
+            # Test odd-height
+            image_shape = (503, 400)
+            encoded_images = image_processing._pad_for_patching(image_input, image_shape, input_data_format)
+            encoded_image_shape = (
+                encoded_images.shape[:-1] if input_data_format == ChannelDimension.LAST else encoded_images.shape[1:]
+            )
+            self.assertEqual(encoded_image_shape, image_shape)
