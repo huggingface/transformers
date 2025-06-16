d63cab40516cffba6ca648e1e7105b6607ef41f6
Pranav <veldurthipranav@gmail.com>
2025-06-16 20:25:28 +0000

    fix copyright year

--- src/transformers/models/arcee/__init__.py
+++ src/transformers/models/arcee/__init__.py
@@ -1,9 +1,4 @@
-# Copyright 2022 EleutherAI and the HuggingFace Inc. team. All rights reserved.
-#
-# This code is based on EleutherAI's GPT-NeoX library and the GPT-NeoX
-# and OPT implementations in this library. It has been modified from its
-# original forms to accommodate minor architectural differences compared
-# to GPT-NeoX and OPT used by the Meta AI team that trained the model.
+# Copyright 2025 Arcee AI and the HuggingFace Inc. team. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
