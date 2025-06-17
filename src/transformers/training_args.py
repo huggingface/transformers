dcb059a032aaf704746829b19467c9da5c8e5ed1
YAO Matrix <matrix.yao@intel.com>
2025-06-17 00:40:56 +0000

    enable misc test cases on XPU
    Signed-off-by: YAO Matrix <matrix.yao@intel.com>

--- src/transformers/training_args.py
+++ src/transformers/training_args.py
@@ -391,7 +391,7 @@ class TrainingArguments:
             installation](https://github.com/intel/intel-extension-for-pytorch).
         bf16 (`bool`, *optional*, defaults to `False`):
             Whether to use bf16 16-bit (mixed) precision training instead of 32-bit training. Requires Ampere or higher
-            NVIDIA architecture or using CPU (use_cpu) or Ascend NPU. This is an experimental API and it may change.
+            NVIDIA architecture or Intel XPU or using CPU (use_cpu) or Ascend NPU. This is an experimental API and it may change.
         fp16 (`bool`, *optional*, defaults to `False`):
             Whether to use fp16 16-bit (mixed) precision training instead of 32-bit training.
         fp16_opt_level (`str`, *optional*, defaults to 'O1'):
