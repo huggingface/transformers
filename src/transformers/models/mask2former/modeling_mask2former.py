5a95ed5ca0826c867e35e52f698db4d8fc907bcb
Cyril Vallez <cyril.vallez@huggingface.co>
2025-06-18 09:46:22 +0200

    🚨🚨 Fix initialization of Mask2Former (#38864)
    * Correctly fix init
    
    Co-authored-by: BUI Van Tuan <buivantuan07@gmail.com>
    
    * add back the block, breaking BC but this is correct author's code
    
    * override the test for params needing it
    
    ---------
    
    Co-authored-by: BUI Van Tuan <buivantuan07@gmail.com>

--- src/transformers/models/mask2former/modeling_mask2former.py
+++ src/transformers/models/mask2former/modeling_mask2former.py
@@ -2127,29 +2127,19 @@ class Mask2FormerPreTrainedModel(PreTrainedModel):
             for p in module.parameters():
                 if p.dim() > 1:
                     nn.init.xavier_uniform_(p, gain=xavier_std)
-
-        elif isinstance(module, Mask2FormerPixelLevelModule):
-            for submodule in module.modules():
-                if isinstance(submodule, (nn.Conv2d, nn.Linear)):
-                    submodule.weight.data.normal_(mean=0.0, std=std)
-                    if submodule.bias is not None:
-                        submodule.bias.data.zero_()
+            module.cross_attn.in_proj_bias.data.zero_()
 
         elif isinstance(module, Mask2FormerPixelDecoder):
-            for p in module.parameters():
-                if p.dim() > 1:
-                    nn.init.xavier_uniform_(p)
             nn.init.normal_(module.level_embed, std=0)
-
-        elif isinstance(module, Mask2FormerPixelDecoderEncoderOnly):
-            for p in module.parameters():
-                if p.dim() > 1:
-                    nn.init.xavier_uniform_(p)
 
         elif isinstance(module, (nn.Linear, nn.Conv2d, nn.BatchNorm2d)):
             module.weight.data.normal_(mean=0.0, std=std)
             if module.bias is not None:
                 module.bias.data.zero_()
+
+        elif isinstance(module, (nn.LayerNorm, nn.GroupNorm)):
+            module.weight.data.fill_(1.0)
+            module.bias.data.zero_()
 
         elif isinstance(module, nn.Embedding):
             module.weight.data.normal_(mean=0.0, std=std)
