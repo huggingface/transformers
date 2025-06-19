a436e29caee6c91f5ebc5d9c34c95b4f919469d9
ydshieh <ydshieh@users.noreply.github.com>
2025-06-19 16:40:07 +0200

    try

---
+++ utils/get_runner_map.py
@@ -0,0 +1,2 @@
+runner_map = {"models/vit": {"single-gpu": "aws-g4dn-4xlarge-cache", "multi-gpu": "aws-g4dn-12xlarge-cache"}, "models/clip": {"single-gpu": "aws-g5-4xlarge-cache", "multi-gpu": "aws-g5-12xlarge-cache"}}
+print(runner_map)
