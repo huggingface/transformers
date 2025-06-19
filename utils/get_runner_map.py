69f5fa194055419a73f0acd926fb41442493211f
ydshieh <ydshieh@users.noreply.github.com>
2025-06-19 17:27:22 +0200

    try

--- utils/get_runner_map.py
+++ utils/get_runner_map.py
@@ -1,2 +1,6 @@
-runner_map = {"models/vit": {"single-gpu": "aws-g4dn-4xlarge-cache", "multi-gpu": "aws-g4dn-12xlarge-cache"}, "models/clip": {"single-gpu": "aws-g5-4xlarge-cache", "multi-gpu": "aws-g5-12xlarge-cache"}}
+import requests
+
+response = requests.get("https://huggingface.co/datasets/hf-internal-testing/transformers_daily_ci/resolve/main/runner_map.json")
+runner_map = response.json()
+
 print(runner_map)
