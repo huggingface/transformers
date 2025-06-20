eb20826e0b53e48190e2075556d193bdff97dce1
qubvel <qubvel@gmail.com>
2025-06-20 15:05:19 +0000

    fix blip2

--- src/transformers/models/blip_2/modeling_blip_2.py
+++ src/transformers/models/blip_2/modeling_blip_2.py
@@ -992,11 +992,11 @@ class Blip2QFormerEncoder(nn.Module):
                 hidden_states,
                 attention_mask,
                 layer_head_mask,
-                encoder_hidden_states,
-                encoder_attention_mask,
-                past_key_value,
-                output_attentions,
-                query_length,
+                encoder_hidden_states,  # as a positional argument for gradient checkpointing
+                encoder_attention_mask=encoder_attention_mask,
+                past_key_value=past_key_value,
+                output_attentions=output_attentions,
+                query_length=query_length,
             )
 
             hidden_states = layer_outputs[0]
