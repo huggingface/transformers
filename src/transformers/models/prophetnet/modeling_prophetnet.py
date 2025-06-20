73d56148648e228e69827d9a23bb3f41d7fa304e
qubvel <qubvel@gmail.com>
2025-06-20 11:47:13 +0000

    fix prophetnet

--- src/transformers/models/prophetnet/modeling_prophetnet.py
+++ src/transformers/models/prophetnet/modeling_prophetnet.py
@@ -1389,8 +1389,8 @@ class ProphetNetDecoder(ProphetNetPreTrainedModel):
 
             layer_outputs = decoder_layer(
                 hidden_states,
-                attention_mask=extended_attention_mask,
-                encoder_hidden_states=encoder_hidden_states,
+                extended_attention_mask,
+                encoder_hidden_states,  # as a positional argument for gradient checkpointing
                 encoder_attn_mask=extended_encoder_attention_mask,
                 layer_head_mask=(head_mask[idx] if head_mask is not None else None),
                 cross_attn_layer_head_mask=(cross_attn_head_mask[idx] if cross_attn_head_mask is not None else None),
