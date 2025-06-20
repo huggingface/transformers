fc140147e4b91b6ab78361c74b31640d97a21ec2
qubvel <qubvel@gmail.com>
2025-06-20 13:21:50 +0000

    fix m2m_100

--- src/transformers/models/m2m_100/modeling_m2m_100.py
+++ src/transformers/models/m2m_100/modeling_m2m_100.py
@@ -1136,8 +1136,8 @@ class M2M100Decoder(M2M100PreTrainedModel):
 
                 layer_outputs = decoder_layer(
                     hidden_states,
-                    attention_mask=attention_mask,
-                    encoder_hidden_states=encoder_hidden_states,
+                    attention_mask,
+                    encoder_hidden_states,
                     encoder_attention_mask=encoder_attention_mask,
                     layer_head_mask=(head_mask[idx] if head_mask is not None else None),
                     cross_attn_layer_head_mask=(
