72c341fa799a7c2a70ec8abedffce322aafc1f5a
Marc Sun <marc@huggingface.co>
2025-06-20 16:17:59 +0200

    fix

--- src/transformers/modeling_utils.py
+++ src/transformers/modeling_utils.py
@@ -4307,9 +4307,12 @@ class PreTrainedModel(nn.Module, ModuleUtilsMixin, PushToHubMixin, PeftAdapterMi
             )
 
         if device_map == "auto" and int(os.environ.get("WORLD_SIZE", 0)):
-            logger.info("You've set device_map=`auto` while triggering a distributed run with torchrun. This might lead to unexpected behavior. "
-                           "If your plan is to load the model on each device, you should set device_map={"": PartialState().process_index} where PartialState comes from accelerate library")
-            
+            logger.info(
+                "You've set device_map=`auto` while triggering a distributed run with torchrun. This might lead to unexpected behavior. "
+                "If your plan is to load the model on each device, you should set device_map={"
+                ": PartialState().process_index} where PartialState comes from accelerate library"
+            )
+
         # We need to correctly dispatch the model on the current process device. The easiest way for this is to use a simple
         # `device_map` pointing to the correct device
         if tp_plan is not None:
@@ -4320,7 +4323,6 @@ class PreTrainedModel(nn.Module, ModuleUtilsMixin, PushToHubMixin, PeftAdapterMi
                 if device_mesh.ndim == 1:
                     raise ValueError("device_mesh must be 1 dimensional and will be used for TP")
                 device_map = torch.device(device_mesh.device_type, int(os.environ["LOCAL_RANK"]))
-
 
         if use_auth_token is not None:
             warnings.warn(
