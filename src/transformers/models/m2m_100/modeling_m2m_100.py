f2cc8652fcde9a88840b36ac2418597cf7a27a9e
qubvel <qubvel@gmail.com>
2025-06-20 13:22:21 +0000

    add comment

--- src/transformers/models/m2m_100/modeling_m2m_100.py
+++ src/transformers/models/m2m_100/modeling_m2m_100.py
@@ -1137,7 +1137,7 @@ class M2M100Decoder(M2M100PreTrainedModel):
                 layer_outputs = decoder_layer(
                     hidden_states,
                     attention_mask,
-                    encoder_hidden_states,
+                    encoder_hidden_states,  # as a positional argument for gradient checkpointing
                     encoder_attention_mask=encoder_attention_mask,
                     layer_head_mask=(head_mask[idx] if head_mask is not None else None),
                     cross_attn_layer_head_mask=(
