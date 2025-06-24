275d648fc6efdca25a3fc6b3eafed132f571f67d
MekkCyber <mekk.cyber@gmail.com>
2025-06-24 14:59:18 +0000

    revert

--- src/transformers/masking_utils.py
+++ src/transformers/masking_utils.py
@@ -684,8 +684,8 @@ def create_causal_mask(
             useful to easily overlay another mask on top of the causal one, for example for image tokens handling.
     """
     # If we have an HybridCache structure, here we want to create the mask for the full layers
-    if past_key_values is not None and hasattr(past_key_values, "is_sliding"):
-        layer_idx = next(i for i, is_sliding in enumerate(past_key_values.is_sliding) if not is_sliding)
+    if hasattr(past_key_values, "is_sliding") and False in past_key_values.is_sliding:
+        layer_idx = past_key_values.is_sliding.index(False)
     else:
         layer_idx = 0
 
