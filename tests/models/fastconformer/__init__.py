b5ef0c18cb26e41ce28f0c3554795f221a0392e6
nithinraok <nithinrao.koluguri@gmail.com>
2025-06-26 11:28:21 -0700

    update docs and improve style
    Signed-off-by: nithinraok <nithinrao.koluguri@gmail.com>

--- tests/models/fastconformer/__init__.py
+++ tests/models/fastconformer/__init__.py
@@ -1,4 +1,4 @@
-# Copyright 2024 The HuggingFace Inc. team. All rights reserved.
+# Copyright 2025 The HuggingFace Inc. team. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -10,4 +10,4 @@
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
-# limitations under the License. 
+# limitations under the License.
