15e89476ab6df565fd0affbecc812408d9bf1f8e
ydshieh <ydshieh@users.noreply.github.com>
2025-06-26 19:20:01 +0200

    fix

--- tests/models/layoutlmv3/test_image_processing_layoutlmv3.py
+++ tests/models/layoutlmv3/test_image_processing_layoutlmv3.py
@@ -11,6 +11,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+
 import unittest
 
 from transformers.testing_utils import require_pytesseract, require_torch
