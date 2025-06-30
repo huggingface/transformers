1303470aa46541ff8c2483e9720e0cb936f1ffc3
Arthur <arthur.zucker@gmail.com>
2025-06-30 18:01:58 +0200

    remove output attentions

--- src/transformers/models/gemma/modular_gemma.py
+++ src/transformers/models/gemma/modular_gemma.py
@@ -422,7 +422,6 @@ class GemmaModel(LlamaModel):
                 attention_mask=causal_mask,
                 position_ids=position_ids,
                 past_key_value=past_key_values,
-                output_attentions=output_attentions,
                 use_cache=use_cache,
                 cache_position=cache_position,
                 position_embeddings=position_embeddings,
