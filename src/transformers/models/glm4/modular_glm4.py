15a8ff4fe9637febf9727c64fee1edf029c4444a
Arthur <arthur.zucker@gmail.com>
2025-07-01 14:20:56 +0200

    update

--- src/transformers/models/glm4/modular_glm4.py
+++ src/transformers/models/glm4/modular_glm4.py
@@ -68,7 +68,6 @@ class Glm4DecoderLayer(GradientCheckpointingLayer):
             attention_mask=attention_mask,
             position_ids=position_ids,
             past_key_value=past_key_value,
-            output_attentions=output_attentions,
             use_cache=use_cache,
             cache_position=cache_position,
             position_embeddings=position_embeddings,
