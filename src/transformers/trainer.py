b0150b0abe4f3210c38a34465de0dbf3489d327a
Wing Lian <wing@axolotl.ai>
2025-07-01 10:30:18 -0400

    when delaying optimizer creation only prepare the model

--- src/transformers/trainer.py
+++ src/transformers/trainer.py
@@ -2357,7 +2357,7 @@ class Trainer:
                     model = self.accelerator.prepare(self.model)
                 else:
                     if delay_optimizer_creation:
-                        self.optimizer = self.accelerator.prepare(self.optimizer)
+                        model = self.accelerator.prepare(self.model)
                     else:
                         model, self.optimizer = self.accelerator.prepare(self.model, self.optimizer)
             else:
