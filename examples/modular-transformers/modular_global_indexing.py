8ef983a2833df019c467f9127608c723f98d2c94
Cyril Vallez <cyril.vallez@gmail.com>
2025-07-02 17:50:29 +0200

    style

--- examples/modular-transformers/modular_global_indexing.py
+++ examples/modular-transformers/modular_global_indexing.py
@@ -6,9 +6,11 @@ def custom_flex(x, **kwargs):
     """Dummy function."""
     return x
 
+
 ALL_ATTENTION_FUNCTIONS = AttentionInterface()
 # This indexing statement and associated function should be exported correctly!
 ALL_ATTENTION_FUNCTIONS["flex_attention"] = custom_flex
+
 
 class GlobalIndexingAttention(LlamaAttention):
     pass
