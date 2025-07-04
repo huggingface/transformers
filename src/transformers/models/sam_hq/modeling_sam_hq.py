90e36aae7ce206598774f6bd12325b2143c9c5da
Arthur <arthur.zucker@gmail.com>
2025-07-04 16:39:02 +0200

    fix copies for sam_hq

--- src/transformers/models/sam_hq/modeling_sam_hq.py
+++ src/transformers/models/sam_hq/modeling_sam_hq.py
@@ -29,12 +29,12 @@ import torch.nn.functional as F
 from torch import Tensor, nn
 
 from transformers.modeling_outputs import ModelOutput
-from transformers.modeling_utils import PreTrainedModel
 from transformers.utils.generic import OutputRecorder, TransformersKwargs, check_model_inputs
 
 from ...activations import ACT2FN
 from ...modeling_layers import GradientCheckpointingLayer
 from ...modeling_outputs import BaseModelOutput
+from ...modeling_utils import PreTrainedModel
 from ...processing_utils import Unpack
 from ...utils import auto_docstring, logging
 from .configuration_sam_hq import SamHQConfig, SamHQMaskDecoderConfig, SamHQPromptEncoderConfig, SamHQVisionConfig
