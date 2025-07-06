22226c34a8dc380077a9d2e26e629e8cedb94460
steven <steven.bucaille@gmail.com>
2025-07-06 17:28:33 +0200

    refactor: removed logger and updated attention forward

--- src/transformers/models/efficientloftr/modeling_efficientloftr.py
+++ src/transformers/models/efficientloftr/modeling_efficientloftr.py
@@ -33,9 +33,6 @@ from ...utils import (
 from .configuration_efficientloftr import EfficientLoFTRConfig
 
 
-logger = logging.get_logger(__name__)
-
-
 def create_meshgrid(
     height: int,
     width: int,
@@ -544,13 +541,7 @@ class EfficientLoFTRAttention(nn.Module):
 
         attention_interface: Callable = eager_attention_forward
         if self.config._attn_implementation != "eager":
-            if self.config._attn_implementation == "sdpa" and kwargs.get("output_attentions", False):
-                logger.warning_once(
-                    "`torch.nn.functional.scaled_dot_product_attention` does not support `output_attentions=True`. Falling back to "
-                    'eager attention. This warning can be removed using the argument `attn_implementation="eager"` when loading the model.'
-                )
-            else:
-                attention_interface = ALL_ATTENTION_FUNCTIONS[self.config._attn_implementation]
+            attention_interface = ALL_ATTENTION_FUNCTIONS[self.config._attn_implementation]
         attn_output, attn_weights = attention_interface(
             self,
             query_states,
