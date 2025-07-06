e729ba3fa34f3f3c12d074f3eae7ff16fcc5e85f
steven <steven.bucaille@gmail.com>
2025-07-06 17:30:40 +0200

    refactor: update type imports

--- src/transformers/models/efficientloftr/modeling_efficientloftr.py
+++ src/transformers/models/efficientloftr/modeling_efficientloftr.py
@@ -25,9 +25,9 @@ from ...modeling_utils import ALL_ATTENTION_FUNCTIONS, PreTrainedModel
 from ...processing_utils import Unpack
 from ...utils import (
     ModelOutput,
-    add_start_docstrings,
-    add_start_docstrings_to_model_forward,
-    logging,
+    TransformersKwargs,
+    auto_docstring,
+    can_return_tuple,
     torch_int,
 )
 from .configuration_efficientloftr import EfficientLoFTRConfig
