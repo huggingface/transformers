c62a0d0dd9b34b6862a03fb01e6916c87cf330bf
molbap <pablo.montalvo.leroux@gmail.com>
2025-07-07 17:10:11 +0200

    fixup

--- utils/modular_model_converter.py
+++ utils/modular_model_converter.py
@@ -365,6 +365,7 @@ class SuperTransformer(cst.CSTTransformer):
             return updated_node.with_changes(body=new_body, params=updated_node.params)
         return updated_node
 
+
 def find_all_dependencies(
     dependency_mapping: dict[str, set],
     start_entity: Optional[str] = None,
