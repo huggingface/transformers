dd8aace1747580983a42b85a706c2b8e6feafcfe
Deep-unlearning <steven@huggingface.co>
2025-07-07 14:19:38 +0000

    xcodec2 use EncodecFeatureExtractor

--- src/transformers/models/auto/feature_extraction_auto.py
+++ src/transformers/models/auto/feature_extraction_auto.py
@@ -112,7 +112,7 @@ FEATURE_EXTRACTOR_MAPPING_NAMES = OrderedDict(
         ("wavlm", "Wav2Vec2FeatureExtractor"),
         ("whisper", "WhisperFeatureExtractor"),
         ("xclip", "CLIPFeatureExtractor"),
-        ("xcodec2", "XCodec2FeatureExtractor"),
+        ("xcodec2", "EncodecFeatureExtractor"),
         ("yolos", "YolosFeatureExtractor"),
     ]
 )
