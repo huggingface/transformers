716baa6109b0eb62f7ebcc53bb5be68297772766
Eric B <ebezzam@gmail.com>
2025-07-09 17:51:43 +0200

    Fix DAC conversion.

--- src/transformers/models/dac/convert_dac_checkpoint.py
+++ src/transformers/models/dac/convert_dac_checkpoint.py
@@ -16,6 +16,7 @@ import argparse
 import fnmatch
 import re
 
+import numpy as np
 import torch
 
 from transformers import (
@@ -207,6 +208,7 @@ def convert_checkpoint(
     config.upsampling_ratios = metadata["decoder_rates"]
     config.quantizer_dropout = float(metadata["quantizer_dropout"])
     config.sampling_rate = sample_rate
+    config.hop_length = int(np.prod(config.downsampling_ratios))
 
     model = DacModel(config)
     feature_extractor = DacFeatureExtractor()
