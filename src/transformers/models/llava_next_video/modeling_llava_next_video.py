53e7b1aff5a189aff53fd6c5672edc5af9640202
raushan <raushan@huggingface.co>
2025-07-09 08:43:22 +0200

    flash attn 3 flag

--- src/transformers/models/llava_next_video/modeling_llava_next_video.py
+++ src/transformers/models/llava_next_video/modeling_llava_next_video.py
@@ -170,6 +170,7 @@ class LlavaNextVideoPreTrainedModel(PreTrainedModel):
     _skip_keys_device_placement = "past_key_values"
     _supports_cache_class = True
     _supports_flash_attn_2 = True
+    _supports_flash_attn_3 = True
     _supports_sdpa = True
     _supports_quantized_cache = True
     _supports_static_cache = True
