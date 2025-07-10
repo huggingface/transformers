31acdb0a0bbf44fe396cc7c98d6f3197145c5038
eromomon <edgar.romo.montiel@intel.com>
2025-07-10 19:25:03 +0000

    use do_reducer_labels in AutoImageProcessor

--- examples/pytorch/semantic-segmentation/run_semantic_segmentation_no_trainer.py
+++ examples/pytorch/semantic-segmentation/run_semantic_segmentation_no_trainer.py
@@ -324,7 +324,7 @@ def main():
         args.model_name_or_path, id2label=id2label, label2id=label2id, trust_remote_code=args.trust_remote_code
     )
     image_processor = AutoImageProcessor.from_pretrained(
-        args.model_name_or_path, trust_remote_code=args.trust_remote_code
+        args.model_name_or_path, trust_remote_code=args.trust_remote_code, do_reduce_labels=args.do_reduce_labels
     )
     model = AutoModelForSemanticSegmentation.from_pretrained(
         args.model_name_or_path,
