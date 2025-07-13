2bbfbb92417f9d367bd0776bd8b0cbb6ffbf5d34
cyy <cyyever@outlook.com>
2025-07-13 17:31:23 +0800

    Fix inefficient sequence tests
    Signed-off-by: cyy <cyyever@outlook.com>

--- src/transformers/modeling_utils.py
+++ src/transformers/modeling_utils.py
@@ -5830,7 +5830,7 @@ def caching_allocator_warmup(model: PreTrainedModel, expanded_device_map: dict, 
     accelerator_device_map = {
         param: torch.device(device) for param, device in expanded_device_map.items() if is_accelerator_device(device)
     }
-    if not len(accelerator_device_map):
+    if not accelerator_device_map:
         return
 
     tp_plan_regex = (
