be0cbe069373bd28c1c9677d9b318c5bd1f282fd
Matt <rocketknight1@gmail.com>
2025-07-14 18:25:01 +0100

    stash commit

--- src/transformers/models/llama/modeling_llama.py
+++ src/transformers/models/llama/modeling_llama.py
@@ -185,18 +185,6 @@ class LlamaMLP(nn.Module):
         return down_proj
 
 
-def repeat_kv(hidden_states: torch.Tensor, n_rep: int) -> torch.Tensor:
-    """
-    This is the equivalent of torch.repeat_interleave(x, dim=1, repeats=n_rep). The hidden states go from (batch,
-    num_key_value_heads, seqlen, head_dim) to (batch, num_attention_heads, seqlen, head_dim)
-    """
-    batch, num_key_value_heads, slen, head_dim = hidden_states.shape
-    if n_rep == 1:
-        return hidden_states
-    hidden_states = hidden_states[:, :, None, :, :].expand(batch, num_key_value_heads, n_rep, slen, head_dim)
-    return hidden_states.reshape(batch, num_key_value_heads * n_rep, slen, head_dim)
-
-
 def eager_attention_forward(
     module: nn.Module,
     query: torch.Tensor,
@@ -207,18 +195,29 @@ def eager_attention_forward(
     dropout: float = 0.0,
     **kwargs: Unpack[TransformersKwargs],
 ):
-    key_states = repeat_kv(key, module.num_key_value_groups)
-    value_states = repeat_kv(value, module.num_key_value_groups)
+    if multi_head_attention := key.shape[1] != query.shape[1]:
+        query_states = query.view(query.shape[0], key.shape[1], -1, *query.shape[2:])
+        attn_weights = torch.einsum("bkgjd, bksd -> bkgjs", query_states, key)
+        attn_weights = attn_weights.view(*query.shape[:3], query.shape[3]) * scaling
+    else:
+        attn_weights = torch.einsum("bhsd, bhjd -> bhjs", query, key) * scaling
 
-    attn_weights = torch.matmul(query, key_states.transpose(2, 3)) * scaling
     if attention_mask is not None:
-        causal_mask = attention_mask[:, :, :, : key_states.shape[-2]]
+        if multi_head_attention:
+            causal_mask = attention_mask[:, :, None, :, : key.shape[-2]]
+        else:
+            causal_mask = attention_mask[:, :, :, : key.shape[-2]]
         attn_weights = attn_weights + causal_mask
 
     attn_weights = nn.functional.softmax(attn_weights, dim=-1, dtype=torch.float32).to(query.dtype)
     attn_weights = nn.functional.dropout(attn_weights, p=dropout, training=module.training)
-    attn_output = torch.matmul(attn_weights, value_states)
-    attn_output = attn_output.transpose(1, 2).contiguous()
+    if multi_head_attention:
+        # Can we fold the transpose into the einsum?
+        attn_output = torch.einsum("bkgjs, bksd -> bkgjd", attn_weights, value).flatten(1, 2).transpose(1, 2).contiguous()
+        attn_weights = attn_weights.flatten(1, 2)
+    else:
+        attn_output = torch.matmul(attn_weights, value)
+        attn_output = attn_output.transpose(1, 2).contiguous()
 
     return attn_output, attn_weights
 
