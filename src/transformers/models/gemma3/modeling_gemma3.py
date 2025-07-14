8b4624d213c9641ab7671cb0d6b102b0c0b19820
Matt <rocketknight1@gmail.com>
2025-07-14 19:52:55 +0100

    Performance improvements

--- src/transformers/models/gemma3/modeling_gemma3.py
+++ src/transformers/models/gemma3/modeling_gemma3.py
@@ -291,10 +291,10 @@ def eager_attention_forward(
     attn_weights = nn.functional.softmax(attn_weights, dim=-1, dtype=torch.float32).to(query.dtype)
     attn_weights = nn.functional.dropout(attn_weights, p=dropout, training=module.training)
     if multi_head_attention:
-        attn_output = torch.einsum("bkgjs, bksd -> bjkgd", attn_weights, value).flatten(2, 3)
+        attn_output = torch.einsum("bkgjs, bksd -> bkgjd", attn_weights, value).flatten(1, 2).transpose(1, 2)
         attn_weights = attn_weights.flatten(1, 2)
     else:
-        attn_output = torch.einsum("bhjs, bhsd -> bjhd", attn_weights, value)
+        attn_output = torch.einsum("bhjs, bhsd -> bhjd", attn_weights, value).transpose(1, 2)
 
     return attn_output, attn_weights
 
