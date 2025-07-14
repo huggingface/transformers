9e648b3a75eef533a8c4e3c60be92efb37e7c2e2
zRzRzRzRzRzRzR <2448370773@qq.com>
2025-07-14 12:58:22 +0800

    Update check_config_docstrings.py

--- utils/check_config_docstrings.py
+++ utils/check_config_docstrings.py
@@ -50,7 +50,7 @@ CONFIG_CLASSES_TO_IGNORE_FOR_DOCSTRING_CHECKPOINT_CHECK = {
     "GraniteMoeHybridConfig",
     "Qwen3MoeConfig",
     "GraniteSpeechConfig",
-    "Glm4MoeConfig"
+    "Glm4MoeConfig",
 }
 
 
