c5a3676c4934c13191e343f0bcd61a5196a7788d
Pablo <pablo.montalvo.leroux@gmail.com>
2025-07-17 15:48:24 +0200

    remodularise some stuff

--- src/transformers/models/minimax/modeling_minimax.py
+++ src/transformers/models/minimax/modeling_minimax.py
@@ -1099,11 +1099,6 @@ class MiniMaxForQuestionAnswering(MiniMaxPreTrainedModel):
         # Initialize weights and apply final processing
         self.post_init()
 
-    def get_input_embeddings(self):
-        return self.transformer.embed_tokens
-
-    def set_input_embeddings(self, value):
-        self.transformer.embed_tokens = value
 
     @can_return_tuple
     @auto_docstring
