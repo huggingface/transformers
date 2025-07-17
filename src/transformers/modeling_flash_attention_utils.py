5a7ae11308ca515f4bb7220acdf0881cc470f32c
Arthur <arthur.zucker@gmail.com>
2025-07-17 16:46:09 +0200

    bring the licence back

--- src/transformers/modeling_flash_attention_utils.py
+++ src/transformers/modeling_flash_attention_utils.py
@@ -1,3 +1,16 @@
+# Copyright 2024 The Fairseq Authors and the HuggingFace Inc. team. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 import inspect
 import os
 import warnings
@@ -5,6 +18,7 @@ from typing import Optional, TypedDict
 
 import torch
 import torch.nn.functional as F
+
 from .utils import (
     is_flash_attn_2_available,
     is_flash_attn_3_available,
@@ -13,6 +27,7 @@ from .utils import (
     is_torch_npu_available,
     logging,
 )
+
 
 logger = logging.get_logger(__name__)
 
