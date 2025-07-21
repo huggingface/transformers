9ec4170939d9b023588abd1f0107d4b71c346da6
st81 <m59rt@yahoo.co.jp>
2025-07-21 11:45:55 +0900

    Fix Docstring of BarkProcessor

--- src/transformers/models/bark/processing_bark.py
+++ src/transformers/models/bark/processing_bark.py
@@ -255,8 +255,9 @@ class BarkProcessor(ProcessorMixin):
                 - `'np'`: Return NumPy `np.ndarray` objects.
 
         Returns:
-            Tuple([`BatchEncoding`], [`BatchFeature`]): A tuple composed of a [`BatchEncoding`], i.e the output of the
-            `tokenizer` and a [`BatchFeature`], i.e the voice preset with the right tensors type.
+            [`BatchEncoding`]: A [`BatchEncoding`] object containing the output of the `tokenizer`.
+            If a voice preset is provided, the returned object will include a `"history_prompt"` key
+            containing a [`BatchFeature`] , i.e the voice preset with the right tensors type.
         """
         if voice_preset is not None and not isinstance(voice_preset, dict):
             if (
