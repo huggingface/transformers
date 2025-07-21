0e35c0bd1e03205df2193358cf0fe1513829ead1
Vasqu <antonprogamer@gmail.com>
2025-07-21 20:04:02 +0200

    fix repo consistency

--- src/transformers/models/ernie4_5_moe/modeling_ernie4_5_moe.py
+++ src/transformers/models/ernie4_5_moe/modeling_ernie4_5_moe.py
@@ -516,12 +516,6 @@ class Ernie4_5_MoEModel(Ernie4_5_MoEPreTrainedModel):
         # Initialize weights and apply final processing
         self.post_init()
 
-    def get_input_embeddings(self):
-        return self.embed_tokens
-
-    def set_input_embeddings(self, value):
-        self.embed_tokens = value
-
     @check_model_inputs
     @auto_docstring
     def forward(
@@ -686,18 +680,6 @@ class Ernie4_5_MoEForCausalLM(Ernie4_5_MoEPreTrainedModel, GenerationMixin):
 
         # Initialize weights and apply final processing
         self.post_init()
-
-    def get_input_embeddings(self):
-        return self.model.embed_tokens
-
-    def set_input_embeddings(self, value):
-        self.model.embed_tokens = value
-
-    def get_output_embeddings(self):
-        return self.lm_head
-
-    def set_output_embeddings(self, new_embeddings):
-        self.lm_head = new_embeddings
 
     def set_decoder(self, decoder):
         self.model = decoder
