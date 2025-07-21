18f68e52df84c56c12cde74f848e40797415be16
qubvel <qubvel@gmail.com>
2025-07-21 14:09:52 +0000

    fixup

--- src/transformers/models/mm_grounding_dino/modular_mm_grounding_dino.py
+++ src/transformers/models/mm_grounding_dino/modular_mm_grounding_dino.py
@@ -183,7 +183,6 @@ class MMGroundingDinoContrastiveEmbedding(GroundingDinoContrastiveEmbedding):
 
 
 class MMGroundingDinoPreTrainedModel(GroundingDinoPreTrainedModel):
-    
     def _init_weights(self, module):
         super()._init_weights(module)
         if isinstance(module, MMGroundingDinoContrastiveEmbedding):
