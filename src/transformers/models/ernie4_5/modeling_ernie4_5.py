049a674e681181c2616fec4124086ec0ec55ed2d
Anton Vlasjuk <73884904+vasqu@users.noreply.github.com>
2025-07-21 20:56:24 +0200

    [`CI`] Fix post merge ernie 4.5 (#39561)
    fix repo consistency

--- src/transformers/models/ernie4_5/modeling_ernie4_5.py
+++ src/transformers/models/ernie4_5/modeling_ernie4_5.py
@@ -350,12 +350,6 @@ class Ernie4_5Model(Ernie4_5PreTrainedModel):
         # Initialize weights and apply final processing
         self.post_init()
 
-    def get_input_embeddings(self):
-        return self.embed_tokens
-
-    def set_input_embeddings(self, value):
-        self.embed_tokens = value
-
     @check_model_inputs
     @auto_docstring
     def forward(
@@ -431,18 +425,6 @@ class Ernie4_5ForCausalLM(Ernie4_5PreTrainedModel, GenerationMixin):
 
         # Initialize weights and apply final processing
         self.post_init()
-
-    def get_input_embeddings(self):
-        return self.model.embed_tokens
-
-    def set_input_embeddings(self, value):
-        self.model.embed_tokens = value
-
-    def get_output_embeddings(self):
-        return self.lm_head
-
-    def set_output_embeddings(self, new_embeddings):
-        self.lm_head = new_embeddings
 
     def set_decoder(self, decoder):
         self.model = decoder
