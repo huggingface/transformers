1c8bf235ecd24dfba76c8d6bd990114911e61eeb
geetu040 <raoarmaghanshakir040@gmail.com>
2025-07-22 15:00:00 +0500

    Revert "sam: fix _init_weigths"
    This reverts commit db625d0c68956c0dad45edd7a469b6a074905c27.

--- src/transformers/models/sam_hq/modeling_sam_hq.py
+++ src/transformers/models/sam_hq/modeling_sam_hq.py
@@ -499,7 +499,7 @@ class SamHQPreTrainedModel(PreTrainedModel):
             if module.use_rel_pos:
                 module.rel_pos_h.data.zero_()
                 module.rel_pos_w.data.zero_()
-        elif isinstance(module, SamHQVisionEncoder):
+        if isinstance(module, SamHQVisionEncoder):
             if module.pos_embed is not None:
                 module.pos_embed.data.zero_()
 
--- src/transformers/models/sam_hq/modular_sam_hq.py
+++ src/transformers/models/sam_hq/modular_sam_hq.py
@@ -186,7 +186,11 @@ class SamHQVisionLayer(SamVisionLayer):
 
 
 class SamHQPreTrainedModel(SamPreTrainedModel):
-    pass
+    def _init_weights(self, module):
+        super()._init_weights(module)
+        if isinstance(module, SamHQVisionEncoder):
+            if module.pos_embed is not None:
+                module.pos_embed.data.zero_()
 
 
 class SamHQVisionEncoder(SamVisionEncoder, SamHQPreTrainedModel):
