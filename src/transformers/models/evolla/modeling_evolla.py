c752949e919d6a01dc9fa74347471eeac375d9df
Xibin Bayes Zhou <zhouxibin@westlake.edu.cn>
2025-07-23 16:29:30 +0800

    fill in docstrings

--- src/transformers/models/evolla/modeling_evolla.py
+++ src/transformers/models/evolla/modeling_evolla.py
@@ -1594,18 +1594,18 @@ class EvollaModel(EvollaPreTrainedModel):
         **kwargs,
     ) -> Union[tuple, BaseModelOutputWithPast]:
         r"""
-        protein_input_ids (<fill_type>):
-            <fill_docstring>
-        protein_attention_mask (<fill_type>):
-            <fill_docstring>
-        structure_feats (<fill_type>):
-            <fill_docstring>
-        msa_feats (<fill_type>):
-            <fill_docstring>
-        structure_batch_mask (<fill_type>):
-            <fill_docstring>
-        msa_batch_mask (<fill_type>):
-            <fill_docstring>
+        protein_input_ids (torch.LongTensor):
+            The input IDs for the protein sequence in structure-aware tokens. Should be of shape `(batch_size, protein_seq_length)` and type `torch.LongTensor`.
+        protein_attention_mask (torch.Tensor):
+            The attention mask for the protein sequence. Should be of shape `(batch_size, protein_seq_length)` and type `torch.Tensor`.
+        structure_feats (torch.FloatTensor):
+            The input IDs for purely structure-based features. Should be of shape `(batch_size, structure_seq_length, structure_feat_dim)` and type `torch.FloatTensor`. Dummy input for now.
+        msa_feats (torch.FloatTensor):
+            The input IDs for purely MSA-based features. Should be of shape `(batch_size, msa_seq_length, msa_feat_dim)` and type `torch.FloatTensor`. Dummy input for now.
+        structure_batch_mask (torch.Tensor):
+            The batch mask to decide which protein sequences are purely structure-based. Should be of shape `(batch_size)` and type `torch.Tensor`. Should be paired with `structure_feats`. Dummpy input for now.
+        msa_batch_mask (torch.Tensor):
+            The batch mask to decide which protein sequences are purely MSA-based. Should be of shape `(batch_size)` and type `torch.Tensor`. Should be paired with `msa_feats`. Dummpy input for now.
         """
         # If not provided `protein_feats`, use the `protein_encoder` to get the protein features
         if protein_input_ids is not None and protein_attention_mask is not None:
@@ -1704,10 +1704,10 @@ class EvollaForProteinText2Text(EvollaPreTrainedModel, GenerationMixin):
         **kwargs,
     ):
         r"""
-        protein_input_ids (<fill_type>):
-            <fill_docstring>
-        protein_attention_mask (<fill_type>):
-            <fill_docstring>
+        protein_input_ids (torch.LongTensor):
+            The input IDs for the protein sequence. Should be of shape `(batch_size, protein_seq_length)` and type `torch.LongTensor`.
+        protein_attention_mask (torch.Tensor):
+            The attention mask for the protein sequence. Should be of shape `(batch_size, protein_seq_length)` and type `torch.Tensor`.
 
         Example:
 
