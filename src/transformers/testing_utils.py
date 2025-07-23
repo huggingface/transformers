bada82fa9ab79dbc2846e3fcfcda4d302af8cf37
Andrei Panferov <andrei@panferov.org>
2025-07-23 11:41:10 +0200

    FP-Quant support (#38696)
    * quartet
    
    * quartet qat -> quartet
    
    * format
    
    * bf16 backward
    
    * interfaces
    
    * forward_method
    
    * quartet -> fp_quant
    
    * style
    
    * List -> list
    
    * list typing
    
    * fixed format and annotations
    
    * test_fp_quant
    
    * docstrings and default dtypes
    
    * better docstring and removed noop checks
    
    * docs
    
    * pseudoquantization support to test on non-blackwell
    
    * pseudoquant
    
    * Pseudoquant docs
    
    * Update docs/source/en/quantization/fp_quant.md
    
    Co-authored-by: Marc Sun <57196510+SunMarc@users.noreply.github.com>
    
    * Update docs/source/en/quantization/fp_quant.md
    
    * Update docs/source/en/quantization/fp_quant.md
    
    * Update src/transformers/utils/quantization_config.py
    
    Co-authored-by: Mohamed Mekkouri <93391238+MekkCyber@users.noreply.github.com>
    
    * Update tests/quantization/fp_quant_integration/test_fp_quant.py
    
    Co-authored-by: Mohamed Mekkouri <93391238+MekkCyber@users.noreply.github.com>
    
    * Update tests/quantization/fp_quant_integration/test_fp_quant.py
    
    Co-authored-by: Marc Sun <57196510+SunMarc@users.noreply.github.com>
    
    * small test fixes
    
    * dockerfile update
    
    * spec link
    
    * removed `_process_model_after_weight_loading`
    
    * toctree
    
    ---------
    
    Co-authored-by: Marc Sun <57196510+SunMarc@users.noreply.github.com>
    Co-authored-by: Mohamed Mekkouri <93391238+MekkCyber@users.noreply.github.com>

--- src/transformers/testing_utils.py
+++ src/transformers/testing_utils.py
@@ -90,6 +90,7 @@ from .utils import (
     is_flash_attn_3_available,
     is_flax_available,
     is_flute_available,
+    is_fp_quant_available,
     is_fsdp_available,
     is_ftfy_available,
     is_g2p_en_available,
@@ -127,6 +128,7 @@ from .utils import (
     is_pytest_available,
     is_pytorch_quantization_available,
     is_quark_available,
+    is_qutlass_available,
     is_rjieba_available,
     is_sacremoses_available,
     is_safetensors_available,
@@ -1465,6 +1467,20 @@ def require_flute_hadamard(test_case):
     return unittest.skipUnless(
         is_flute_available() and is_hadamard_available(), "test requires flute and fast_hadamard_transform"
     )(test_case)
+
+
+def require_fp_quant(test_case):
+    """
+    Decorator marking a test that requires fp_quant and qutlass
+    """
+    return unittest.skipUnless(is_fp_quant_available(), "test requires fp_quant")(test_case)
+
+
+def require_qutlass(test_case):
+    """
+    Decorator marking a test that requires qutlass
+    """
+    return unittest.skipUnless(is_qutlass_available(), "test requires qutlass")(test_case)
 
 
 def require_phonemizer(test_case):
