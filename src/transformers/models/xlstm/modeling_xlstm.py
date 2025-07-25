bf3574497fa3ada43e7a59ba19a57ba425bb452e
Cyril Vallez <cyril.vallez@gmail.com>
2025-07-25 15:31:56 +0200

    style

--- src/transformers/models/xlstm/modeling_xlstm.py
+++ src/transformers/models/xlstm/modeling_xlstm.py
@@ -11,8 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-
 """PyTorch xLSTM Model."""
 
 from dataclasses import dataclass
