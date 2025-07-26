13f536b521eccabe4158b32104665558da46d763
ducviet00 <vietyb00@gmail.com>
2025-07-27 00:56:25 +0700

    make fixup

--- src/transformers/models/florence2/modeling_florence2.py
+++ src/transformers/models/florence2/modeling_florence2.py
@@ -669,18 +669,18 @@ class Florence2Seq2SeqLMOutput(Seq2SeqLMOutput):
 
 @auto_docstring
 class Florence2PreTrainedModel(PreTrainedModel):
-    config_class = Florence2Config
+    config: Florence2Config
     base_model_prefix = ""
     supports_gradient_checkpointing = True
     _skip_keys_device_placement = "past_key_values"
-    _supports_cache_class = True
-    _supports_flash_attn_2 = True
-    _supports_flash_attn_3 = True
+
+    _supports_flash_attn = True
     _supports_sdpa = True
-    _supports_quantized_cache = True
-    _supports_static_cache = True
+
+    _can_compile_fullgraph = True
     _supports_flex_attn = True
     _supports_attention_backend = True
+    config_class = Florence2Config
 
     def _init_weights(self, module):
         std = self.config.vision_config.initializer_range
