3baf4835dff61d34a558efab1ce03d072c7b5b1c
Mingji Han <mingjihan@tencent.com>
2025-07-28 11:53:32 +0800

    update

--- src/transformers/models/hunyuan_v1_dense/modeling_hunyuan_v1_dense.py
+++ src/transformers/models/hunyuan_v1_dense/modeling_hunyuan_v1_dense.py
@@ -5,14 +5,14 @@
 
 import math
 import warnings
-from typing import Optional, Union
+from typing import Optional, Union, Unpack
 
 import torch
 import torch.nn.functional as F
 import torch.utils.checkpoint
 from torch import nn
 from torch.nn import BCEWithLogitsLoss, CrossEntropyLoss, MSELoss
-
+from ...utils import TransformersKwargs
 from transformers.activations import ACT2FN
 from transformers.cache_utils import Cache, DynamicCache
 from transformers.modeling_attn_mask_utils import (
@@ -1329,6 +1329,7 @@ class HunYuanDenseV1ForCausalLM(HunYuanDenseV1PreTrainedModel, GenerationMixin):
         output_attentions: Optional[bool] = None,
         output_hidden_states: Optional[bool] = None,
         return_dict: Optional[bool] = None,
+        **super_kwargs: Unpack[TransformersKwargs]
     ) -> Union[tuple, CausalLMOutputWithPast]:
         r"""
         Args:
