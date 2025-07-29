567cf0e288fcd7d4c32f16678fcd4f18db1be91e
notkisk <salahxd99@gmail.com>
2025-07-29 14:10:24 +0000

    Fix linting issues in GLM4V modular file
    - Add missing imports for Any and ModelOutput type hints
    - Remove trailing whitespace from blank line
    - Maintain GLM4V rope_deltas override functionality

--- src/transformers/models/glm4v/modular_glm4v.py
+++ src/transformers/models/glm4v/modular_glm4v.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import itertools
-from typing import Callable, Optional, Union
+from typing import Any, Callable, Optional, Union
 
 import numpy as np
 import torch
@@ -30,7 +30,7 @@ from ...image_utils import ImageInput
 from ...masking_utils import create_causal_mask
 from ...modeling_flash_attention_utils import FlashAttentionKwargs
 from ...modeling_layers import GradientCheckpointingLayer
-from ...modeling_outputs import BaseModelOutputWithPast
+from ...modeling_outputs import BaseModelOutputWithPast, ModelOutput
 from ...modeling_rope_utils import rope_config_validation
 from ...modeling_utils import ALL_ATTENTION_FUNCTIONS
 from ...processing_utils import ImagesKwargs, Unpack
@@ -1313,7 +1313,6 @@ class Glm4vCausalLMOutputWithPast(Qwen2_5_VLCausalLMOutputWithPast):
 
 class Glm4vForConditionalGeneration(Qwen2_5_VLForConditionalGeneration):
     _checkpoint_conversion_mapping = {}
-    
     # Override to prevent rope_deltas inheritance from Qwen2.5VL - GLM4V doesn't need this
     def _update_model_kwargs_for_generation(
         self,
