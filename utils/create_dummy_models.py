093fa8ea46b63052aa27e8188c917ea67b76ec26
ydshieh <ydshieh@users.noreply.github.com>
2025-07-31 10:33:40 +0200

    check custom 140:280

--- utils/create_dummy_models.py
+++ utils/create_dummy_models.py
@@ -1400,7 +1400,7 @@ def create_tiny_models(
         for c, models_to_create in list(to_create.items()):
             all_build_args.append((c, models_to_create, os.path.join(output_path, c.model_type)))
 
-        all_build_args = all_build_args[0:140]
+        all_build_args = all_build_args[140:280]
         with multiprocessing.Pool() as pool:
             results = pool.starmap(build, all_build_args)
             results = {buid_args[0].__name__: result for buid_args, result in zip(all_build_args, results)}
