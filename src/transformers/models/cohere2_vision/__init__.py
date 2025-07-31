69469c37eebf9124c1930ce1611760e6109d82ad
raushan <raushan@huggingface.co>
2025-07-31 12:32:25 +0200

    oh come on, fixup

--- src/transformers/models/cohere2_vision/__init__.py
+++ src/transformers/models/cohere2_vision/__init__.py
@@ -19,9 +19,9 @@ from ...utils.import_utils import define_import_structure
 
 if TYPE_CHECKING:
     from .configuration_cohere2_vision import *
+    from .image_processing_cohere2_vision_fast import *
     from .modeling_cohere2_vision import *
     from .processing_cohere2_vision import *
-    from .image_processing_cohere2_vision_fast import *
 else:
     import sys
 
