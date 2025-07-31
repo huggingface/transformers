c0ce44687bd01b4ca14bbd64c4c71daf5b62a938
Cyril Vallez <cyril.vallez@gmail.com>
2025-07-31 19:39:58 +0200

    fix mamba caches

--- src/transformers/models/lfm2/modular_lfm2.py
+++ src/transformers/models/lfm2/modular_lfm2.py
@@ -17,7 +17,6 @@ import torch
 import torch.nn.functional as F
 from torch import nn
 
-from ...cache_utils import DynamicCache
 from ...masking_utils import create_causal_mask
 from ...modeling_layers import GradientCheckpointingLayer
 from ...modeling_outputs import BaseModelOutputWithPast
@@ -81,7 +80,7 @@ class Lfm2MLP(nn.Module):
         return self.w2(F.silu(self.w1(x)) * self.w3(x))
 
 
-class Lfm2HybridConvCache(DynamicCache):
+class Lfm2HybridConvCache:
     """
     Attention and conv cache for Lfm2.
 
@@ -215,13 +214,6 @@ class Lfm2HybridConvCache(DynamicCache):
 
     def __getitem__(self, layer_idx: int) -> tuple[torch.Tensor, torch.Tensor]:
         return self.key_cache[layer_idx], self.value_cache[layer_idx]
-
-    def to_legacy_cache(self) -> tuple[tuple[torch.Tensor], tuple[torch.Tensor]]:
-        raise NotImplementedError("Lfm2HybridConvCache does not have a legacy cache equivalent.")
-
-    @classmethod
-    def from_legacy_cache(cls, past_key_values: Optional[tuple[tuple[torch.FloatTensor]]] = None) -> "DynamicCache":
-        raise NotImplementedError("Lfm2HybridConvCache does not have a legacy cache equivalent.")
 
     def reset(self):
         for layer_idx in range(len(self.conv_cache)):
