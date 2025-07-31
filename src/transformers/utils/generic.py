fee1b70d0822efa8978a55379fc55014274a46d6
raushan <raushan@huggingface.co>
2025-07-31 11:32:17 +0200

    check model outputs workaround

--- src/transformers/utils/generic.py
+++ src/transformers/utils/generic.py
@@ -1049,16 +1049,19 @@ def check_model_inputs(func):
                 for specs in layer_specs:
                     if not isinstance(specs, OutputRecorder):
                         index = 0 if "hidden_states" in key else 1
-                        layer_name = specs.__name__ if not isinstance(specs, str) else specs
+                        layer_name = None if not isinstance(specs, str) else specs
                         target_class = specs if not isinstance(specs, str) else None
                         specs = OutputRecorder(target_class=target_class, index=index, layer_name=layer_name)
                     capture_tasks.append((key, specs))
 
             for name, module in self.named_modules():
                 for key, specs in capture_tasks:
+                    # The second check is for multimodals where only backbone layer suffix is available
                     if (specs.target_class is not None and isinstance(module, specs.target_class)) or (
-                        specs.layer_name is None and specs.layer_name in name
+                        specs.target_class is None and specs.layer_name is not None and name.endswith(specs.layer_name)
                     ):
+                        if specs.layer_name is not None and specs.layer_name not in name:
+                            continue
                         # Monkey patch forward
                         original_forward = module.forward
                         module.forward = make_capture_wrapper(module, original_forward, key, specs.index)
