191682468ad5e33cd5137a49919c350311a37c99
ydshieh <ydshieh@users.noreply.github.com>
2025-08-06 16:08:47 +0200

    temp

---
+++ run_and_fix.py
@@ -0,0 +1,19 @@
+import os
+import json
+
+fn = "new_failures_with_bad_commit_grouped_by_authors"
+
+with open(fn) as fp:
+    data = json.load(fp)
+
+tests = []
+for model in data["ydshieh"]:
+    for test in data["ydshieh"][model]["single-gpu"]:
+        tests.append(test["test"])
+
+print(len(tests))
+
+for test in tests[:3]:
+    print(test)
+    command = f"HF_HOME=/mnt/cache RUN_SLOW=1 python3 -m pytest -v {test}"
+    os.system(command)
