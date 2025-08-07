e6a49cfe265054dd4836fa766983872d455747d5
ydshieh <ydshieh@users.noreply.github.com>
2025-08-07 14:48:24 +0200

    fix

--- run_and_fix.py
+++ run_and_fix.py
@@ -11,9 +11,28 @@ for model in data["ydshieh"]:
     for test in data["ydshieh"][model]["single-gpu"]:
         tests.append(test["test"])
 
-print(len(tests))
+fn2 = "fixed.json"
 
-for test in tests:
+with open(fn2) as fp:
+    fixed = json.load(fp)
+
+to_fix = [x for x in tests if x not in fixed]
+
+print(len(tests))
+print(len(fixed))
+print(len(to_fix))
+
+fn3 = "to_fix.json"
+
+with open(fn3, "w") as fp:
+    json.dump(to_fix, fp, indent=4)
+
+fn4 = "to_fix.txt"
+
+with open(fn4, "w") as fp:
+    fp.write("\n".join(to_fix))
+
+for test in to_fix:
     print(test)
     command = f"HF_HOME=/mnt/cache RUN_SLOW=1 python3 -m pytest -v {test}"
     os.system(command)
