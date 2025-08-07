a5d81c9e205a0affd3dcc0ee03ae2c5c9b2af25c
Yuanyuan Chen <cyyever@outlook.com>
2025-08-08 01:25:40 +0800

    Fix default values of getenv (#39867)
    Signed-off-by: cyy <cyyever@outlook.com>

--- src/transformers/training_args.py
+++ src/transformers/training_args.py
@@ -123,7 +123,7 @@ def default_logdir() -> str:
 def get_int_from_env(env_keys, default):
     """Returns the first positive env value found in the `env_keys` list or the default."""
     for e in env_keys:
-        val = int(os.environ.get(e, -1))
+        val = int(os.environ.get(e, "-1"))
         if val >= 0:
             return val
     return default
