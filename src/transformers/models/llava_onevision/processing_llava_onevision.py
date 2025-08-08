0956ec75d007bbd738bb09c6a8d5c3d3de147ad2
cyr0930 <line0930@gmail.com>
2025-08-08 02:28:33 +0000

    [fix] llava onevision batch inference

--- src/transformers/models/llava_onevision/processing_llava_onevision.py
+++ src/transformers/models/llava_onevision/processing_llava_onevision.py
@@ -214,15 +214,16 @@ class LlavaOnevisionProcessor(ProcessorMixin):
         prompt_strings = []
         max_num_vision_tokens = 0
         for sample in text:
+            num_images = next(batch_num_images)     # should consume iterable
             if special_token in sample:
-                is_multi_image = next(batch_num_images) != 1
+                is_multi_image = num_images != 1
             else:
                 is_multi_image = False
             while special_token in sample:
+                original_size = next(image_sizes)   # should consume iterable
                 if is_multi_image:
                     num_image_tokens = self.num_image_tokens + 1  # one for image_newline
                 else:
-                    original_size = next(image_sizes)
                     if not isinstance(original_size, (list, tuple)):
                         # cast to list to avoid numerical precision errors when calculating unpadding
                         original_size = original_size.tolist()
