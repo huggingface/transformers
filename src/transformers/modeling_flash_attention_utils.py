5f7d937b0769ff16f313aeddeba18c7ad5056f35
vasqu <antonprogamer@gmail.com>
2025-08-08 13:32:56 +0200

    small fix

--- src/transformers/modeling_flash_attention_utils.py
+++ src/transformers/modeling_flash_attention_utils.py
@@ -385,14 +385,13 @@ def _is_packed_sequence(position_ids, batch_size):
         2. Flattened sequences only are supported
         3. Compile-friendly `not (torch.diff(position_ids, dim=-1) >= 0).all()`, i.e. we have multiple increasing sequences
     """
+    if position_ids is None:
+        return False
+
     increasing_position_sequences = (
         torch.arange(position_ids.shape[1], device=position_ids.device) + position_ids.min()
     )
-    return (
-        position_ids is not None
-        and batch_size == 1
-        and (increasing_position_sequences - position_ids).abs().sum().bool()
-    )
+    return batch_size == 1 and (increasing_position_sequences - position_ids).abs().sum().bool()
 
 
 def fa_peft_integration_check(
