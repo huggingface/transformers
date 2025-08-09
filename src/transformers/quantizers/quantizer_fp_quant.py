69e176e1279eab4eab085ee99dcdddc81be29d6d
Andrei Panferov <andrei@panferov.org>
2025-08-09 22:14:31 +0200

    cpu if pseudoquant

--- src/transformers/quantizers/quantizer_fp_quant.py
+++ src/transformers/quantizers/quantizer_fp_quant.py
@@ -63,12 +63,16 @@ class FPQuantHfQuantizer(HfQuantizer):
         if not is_fp_quant_available():
             raise ImportError("Using `fp_quant` quantization requires fp_quant: `pip install fp_quant`")
 
-        if device_map is None:
+        if device_map is None and not self.quantization_config.pseudoquantization:
             raise ValueError(
                 "You are attempting to load a FPQuant model without setting device_map."
                 " Please set device_map comprised of 'cuda' devices."
             )
-        elif isinstance(device_map, dict) and ("cpu" in device_map.values() or "disk" in device_map.values()):
+        elif (
+            isinstance(device_map, dict)
+            and ("cpu" in device_map.values() or "disk" in device_map.values())
+            and not self.quantization_config.pseudoquantization
+        ):
             raise ValueError(
                 "You are attempting to load a FPQuant model with a device_map that contains a CPU or disk device."
                 " This is not supported. Please remove the CPU or disk device from the device_map."
