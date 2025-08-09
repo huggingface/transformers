8af285314353d32b9474be3b628301b877b2b994
Wing Lian <wing@axolotl.ai>
2025-08-09 08:22:18 -0400

    fix the check around meta+device_mesh

--- src/transformers/modeling_utils.py
+++ src/transformers/modeling_utils.py
@@ -4668,9 +4668,10 @@ class PreTrainedModel(nn.Module, EmbeddingAccessMixin, ModuleUtilsMixin, PushToH
         if tp_plan is not None and tp_plan != "auto":
             # TODO: we can relax this check when we support taking tp_plan from a json file, for example.
             raise ValueError(f"tp_plan supports 'auto' only for now but got {tp_plan}.")
-        if tp_plan is not None and device_map is not None and device_mesh is not None:
+        if tp_plan is not None and device_map is not None and device_map != "meta" and device_mesh is None:
             raise ValueError(
-                "`tp_plan` and `device_map` are mutually exclusive. Choose either one for parallelization."
+                "`tp_plan` and `device_map` are mutually exclusive. "
+                "Choose either one for parallelization or include a `device_mesh`."
             )
 
         if device_map == "auto" and int(os.environ.get("WORLD_SIZE", "0")):
