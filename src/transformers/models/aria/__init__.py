aa84fa4360bb5a25e7ba28025ba5598f6d1c9c4e
Prajwal A <prajwal.a@radius.ai>
2025-08-14 02:16:11 +0530

    Add fast image processor for Aria

--- src/transformers/models/aria/__init__.py
+++ src/transformers/models/aria/__init__.py
@@ -20,6 +20,7 @@ from ...utils.import_utils import define_import_structure
 if TYPE_CHECKING:
     from .configuration_aria import *
     from .image_processing_aria import *
+    from .image_processing_aria_fast import *
     from .modeling_aria import *
     from .processing_aria import *
 
