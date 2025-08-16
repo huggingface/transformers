2a08c49ab4b1060e1e187522fa838d4f9b2ed325
ydshieh <ydshieh@users.noreply.github.com>
2025-08-16 23:18:04 +0200

    vm 1

--- .circleci/create_circleci_config.py
+++ .circleci/create_circleci_config.py
@@ -141,7 +141,7 @@ class CircleCIJob:
 
         job = {
             "machine": {
-                "image": "ubuntu-2004:current"
+                "image": "ubuntu-2004:2024.01.1"
             },
             "environment": env,
         }
