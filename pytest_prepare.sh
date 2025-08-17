97ace3954589f2dc7ae2b00a49626dabac97f341
ydshieh <ydshieh@users.noreply.github.com>
2025-08-17 20:15:51 +0200

    vm 1

--- pytest_prepare.sh
+++ pytest_prepare.sh
@@ -4,9 +4,31 @@ export PYTHONDONTWRITEBYTECODE=1 && export PYTHONUNBUFFERED=1 && export OMP_NUM_
 git clone https://github.com/huggingface/transformers.git
 cd transformers/
 git checkout debug_too_long_no_output
+
+
 python3 -V
-echo $UV_PYTHON
 python3 -c "import sys; print(sys.executable)"
+
+
+apt-get update
+apt-get install -y gdb
+apt-get remove --purge needrestart -y
+echo 'deb http://deb.debian.org/debian bullseye main' >> /etc/apt/sources.list
+apt-get update
+apt-get install -y python3.9-dbg
+sed -i '/bullseye/d' /etc/apt/sources.list
+
+
+which python3
+which python3.9-dbg
+ls -la /usr/bin/python*
+
+
+python3 -V
+python3 -c "import sys; print(sys.executable)"
+
+
+echo $UV_PYTHON
 
 uv pip install git+https://github.com/ydshieh/pytest.git@8.3.5-ydshieh
 uv pip install .
