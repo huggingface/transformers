1a61210d43c05a17d124b360e596d2b1c306cc16
ydshieh <ydshieh@users.noreply.github.com>
2025-08-17 21:51:28 +0200

    vm 1

--- pytest_prepare.sh
+++ pytest_prepare.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export PYTHONDONTWRITEBYTECODE=1 && export PYTHONUNBUFFERED=1 && export OMP_NUM_THREADS=1 && export TRANSFORMERS_IS_CI=true && export PYTEST_TIMEOUT=120 && export RUN_PIPELINE_TESTS=false && export RUN_FLAKY=true && UV_PYTHON=/usr/bin/python3.9-dbg
+export PYTHONDONTWRITEBYTECODE=1 && export PYTHONUNBUFFERED=1 && export OMP_NUM_THREADS=1 && export TRANSFORMERS_IS_CI=true && export PYTEST_TIMEOUT=120 && export RUN_PIPELINE_TESTS=false && export RUN_FLAKY=true
 
 git clone https://github.com/huggingface/transformers.git
 cd transformers/
@@ -21,18 +21,18 @@ apt-get install -y python3.9-dbg
 sed -i '/bullseye/d' /etc/apt/sources.list
 
 
-which python3
-which python3.9-dbg
-ls -la /usr/bin/python*
+#which python3
+#which python3.9-dbg
+#ls -la /usr/bin/python*
+#
+#
+#python3 -V
+#python3 -c "import sys; print(sys.executable)"
 
 
-python3 -V
-python3 -c "import sys; print(sys.executable)"
-
-
-# Enable aliases in script
-shopt -s expand_aliases
-alias python3='python3.9-dbg'
+## Enable aliases in script
+#shopt -s expand_aliases
+#alias python3='python3.9-dbg'
 
 
 which python3
@@ -48,22 +48,22 @@ echo $UV_PYTHON
 
 
 
-echo 000
-curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-python3.9-dbg get-pip.py
-python3.9-dbg -m pip --version
-
-echo 111
-python3 -m pip --no-cache-dir install uv && uv pip install --no-cache-dir -U pip setuptools
-
-echo 222
-uv pip install --no-cache-dir 'torch' 'torchaudio' 'torchvision' 'torchcodec' --index-url https://download.pytorch.org/whl/cpu
-
-echo 333
-uv pip install --no-deps timm accelerate --extra-index-url https://download.pytorch.org/whl/cpu
-
-echo 444
-uv pip install --no-cache-dir librosa "git+https://github.com/huggingface/transformers.git@${REF}#egg=transformers[sklearn,sentencepiece,vision,testing,tiktoken,num2words,video]"
+#echo 000
+#curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+#python3.9-dbg get-pip.py
+#python3.9-dbg -m pip --version
+#
+#echo 111
+#python3 -m pip --no-cache-dir install uv && uv pip install --no-cache-dir -U pip setuptools
+#
+#echo 222
+#uv pip install --no-cache-dir 'torch' 'torchaudio' 'torchvision' 'torchcodec' --index-url https://download.pytorch.org/whl/cpu
+#
+#echo 333
+#uv pip install --no-deps timm accelerate --extra-index-url https://download.pytorch.org/whl/cpu
+#
+#echo 444
+#uv pip install --no-cache-dir librosa "git+https://github.com/huggingface/transformers.git@${REF}#egg=transformers[sklearn,sentencepiece,vision,testing,tiktoken,num2words,video]"
 
 echo 555
 uv pip install git+https://github.com/ydshieh/pytest.git@8.3.5-ydshieh
