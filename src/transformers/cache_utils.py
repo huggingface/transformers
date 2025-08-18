9baf489a62e4fd082dac39ef39ab17ed8edd1236
Manuel de Prada Corral <manueldeprada@gmail.com>
2025-08-18 12:36:34 +0200

    cyril review

--- src/transformers/cache_utils.py
+++ src/transformers/cache_utils.py
@@ -1064,7 +1064,7 @@ class DynamicCache(Cache):
         """
         cache = cls()
         if past_key_values is None:
-            logger.warning_once("past_key_values should be None in from_legacy_cache()")
+            logger.warning_once("past_key_values should not be None in from_legacy_cache()")
         if past_key_values is not None:
             for layer_idx in range(len(past_key_values)):
                 key_states, value_states = past_key_values[layer_idx]
