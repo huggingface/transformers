cbc8208180b6ecc339ab7a8450d846806cef4288
luoxiaoc <xiaochuan.luo@intel.com>
2025-08-18 16:01:12 +0800

    pass in kwargs for loss_func at qwen2_vl and qwen2_5_vl
    Signed-off-by: luoxiaoc <xiaochuan.luo@intel.com>

--- src/transformers/models/qwen2_vl/modeling_qwen2_vl.py
+++ src/transformers/models/qwen2_vl/modeling_qwen2_vl.py
@@ -1405,7 +1405,8 @@ class Qwen2VLForConditionalGeneration(Qwen2VLPreTrainedModel, GenerationMixin):
 
         loss = None
         if labels is not None:
-            loss = self.loss_function(logits=logits, labels=labels, vocab_size=self.config.text_config.vocab_size)
+            loss = self.loss_function(logits=logits, labels=labels, vocab_size=self.config.text_config.vocab_size,
+                                      **kwargs)
 
         return Qwen2VLCausalLMOutputWithPast(
             loss=loss,
