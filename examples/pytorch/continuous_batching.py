9495e0e196ec27b733d50159df5badc9f92d1d74
remi-or <remi.pierre_o@orange.fr>
2025-08-18 14:39:03 +0000

    Rework of the CB example

--- examples/pytorch/continuous_batching.py
+++ examples/pytorch/continuous_batching.py
@@ -1,5 +1,5 @@
 import time
-
+import argparse
 import datasets
 import torch
 
@@ -7,71 +7,118 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 from transformers.generation import GenerationConfig
 
 
-torch.set_float32_matmul_precision("high")
+def batch_generate(
+    model: AutoModelForCausalLM,
+    simple_batch_inputs: list,
+    generation_config: GenerationConfig,
+    tokenizer: AutoTokenizer,
+    displayed_samples: int = 0, # -1: no display, 0: display stats, >0: display inputs and some outputs
+) -> tuple[float, float]:
 
-model_id = "meta-llama/Llama-3.2-3b-Instruct"
-model = (
-    AutoModelForCausalLM.from_pretrained(
-        model_id,
-        attn_implementation="paged_attention|kernels-community/flash-attn",
-        dtype=torch.bfloat16,
+    # Actual batch generation
+    if displayed_samples >= 0:
+        print("--- Running CB Generation Example ---")
+    start_time_simple = time.time()
+    batch_outputs = model.generate_batch(
+        inputs=simple_batch_inputs,
+        generation_config=generation_config,
     )
-    .eval()
-    .cuda()
-)
-tokenizer = AutoTokenizer.from_pretrained(model_id, padding_side="left")
+    end_time_simple = time.time()
+    if displayed_samples >= 0:
+        print("Done with batch generation.")
 
-generation_config = GenerationConfig(
-    max_new_tokens=512,
-    # use_cuda_graph=False,
-    eos_token_id=tokenizer.eos_token_id,
-    pad_token_id=tokenizer.pad_token_id,
-    do_sample=False,
-)
+    # Decode outputs
+    token_count = 0
+    for i, request in enumerate(batch_outputs):
+        input_text = tokenizer.decode(batch_outputs[request].prompt_ids, skip_special_tokens=False)
+        try:
+            output_text = tokenizer.decode(batch_outputs[request].generated_tokens, skip_special_tokens=False)
+            token_count += len(batch_outputs[request].generated_tokens[1:])
+        except Exception as e:
+            print(f"Decoding failed for request {request}: {e}")
+            token_count += len(batch_outputs[request].generated_tokens[1:])
+            output_text = tokenizer.decode(batch_outputs[request].generated_tokens[1:], skip_special_tokens=False)
+        if i < displayed_samples:
+            if len(output_text) > 0:
+                print("-" * 20)
+                print(f"{request} Input:  {input_text}")
+                print(f"{request} Output: {output_text}")
+            else:
+                print(f"{request} Input:  {input_text}")
+                print("[WARN]")
+                print(f"{request} Output was empty!")
 
-train_dataset = datasets.load_dataset("openai/gsm8k", "socratic", split="test")
-train_dataset = train_dataset.select(range(500))  # Use only 5 examples for the simple version
-print("--- Running CB Generation Example ---")
-
-
-def tokenize_function(examples):
-    return tokenizer(examples["question"])
-
-
-tokenized_datasets = train_dataset.map(tokenize_function, batched=True)
-simple_batch_inputs = [item["input_ids"] for item in tokenized_datasets]
-
-start_time_simple = time.time()
-model.forward = torch.compile(model.forward, mode="max-autotune-no-cudagraphs")
-batch_outputs = model.generate_batch(
-    inputs=simple_batch_inputs,
-    generation_config=generation_config,
-)
-end_time_simple = time.time()
-token_count = 0
-for request in batch_outputs:
-    input_text = tokenizer.decode(batch_outputs[request].prompt_ids, skip_special_tokens=False)
-    try:
-        output_text = tokenizer.decode(batch_outputs[request].generated_tokens, skip_special_tokens=False)
-        token_count += len(batch_outputs[request].generated_tokens[1:])
-    except Exception as e:
-        print(f"Decoding failed for request {request}: {e}")
-        token_count += len(batch_outputs[request].generated_tokens[1:])
-        output_text = tokenizer.decode(batch_outputs[request].generated_tokens[1:], skip_special_tokens=False)
-    if len(output_text) > 0:
+    # Compute stats and maybe print them
+    gen_time = end_time_simple - start_time_simple
+    tok_per_sec = token_count / gen_time
+    if displayed_samples >= 0:
         print("-" * 20)
-        print(f"{request} Input:  {input_text}")
-        print(f"{request} Output: {output_text}")
-    else:
-        print("", end="\r\r\r\r")
-print("-" * 20)
-print("--- Finished CB Generation Example ---\n\n")
+        print("--- Finished CB Generation Example ---\n")
+        print(f"CB generation took: {gen_time:.2f} seconds for {token_count} tokens. {tok_per_sec:.2f}tok/s")
+    return gen_time, tok_per_sec
 
 
-print(
-    f"CB generation took: {end_time_simple - start_time_simple:.2f} seconds for {token_count} tokens. {token_count / (end_time_simple - start_time_simple)}tok/s"
-)
+if __name__ == "__main__":
 
+    # Parse args
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--attn-implementation", type=str, default="paged_attention|kernels-community/flash-attn")
+    parser.add_argument("--matmul-precision", type=str, default="high") # set to "none" to disable
+    parser.add_argument("--samples", type=int, default=500)
+    parser.add_argument("--use-cuda-graph", action="store_true")
+    args = parser.parse_args()
+
+    # Set matmul precision
+    if args.matmul_precision != "none":
+        torch.set_float32_matmul_precision(args.matmul_precision)
+
+    # Prepare model
+    model_id = "meta-llama/Llama-3.2-3b-Instruct"
+    model = AutoModelForCausalLM.from_pretrained(
+        model_id,
+        attn_implementation=args.attn_implementation,
+        dtype=torch.bfloat16,
+        torch_dtype=torch.bfloat16,
+    )
+    model = model.cuda().eval()
+    model.forward = torch.compile(model.forward, mode="max-autotune-no-cudagraphs")
+
+    # Prepare tokenizer and dataset
+    tokenizer = AutoTokenizer.from_pretrained(model_id, padding_side="left")
+    dataset = datasets.load_dataset("openai/gsm8k", "socratic", split="test")
+    dataset = dataset.select(range(args.samples))  # Use only 5 examples for the simple version
+    tokenized_datasets = dataset.map(lambda x: tokenizer(x["question"]), batched=True)
+    simple_batch_inputs = [item["input_ids"] for item in tokenized_datasets]
+
+    # Prepare generation config
+    generation_config = GenerationConfig(
+        max_new_tokens=512,
+        use_cuda_graph=args.use_cuda_graph,
+        eos_token_id=tokenizer.eos_token_id,
+        pad_token_id=tokenizer.pad_token_id,
+        do_sample=False,
+    )
+
+    # Run warmup batch generation
+    batch_generate(
+        model,
+        simple_batch_inputs[:min(5, args.samples)],
+        generation_config,
+        tokenizer,
+        displayed_samples=-1,
+    )
+
+    # Run batch generation
+    gen_time, tok_per_sec = batch_generate(
+        model,
+        simple_batch_inputs,
+        generation_config,
+        tokenizer,
+        displayed_samples=5,
+    )
+
+
+# TODO: remove this or incorporate it into the script above
 
 # train_dataset = train_dataset.select(range(5))  # Use only 5 examples for the simple version
 
