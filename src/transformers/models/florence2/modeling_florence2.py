2561b2b1e4e89baf5a2b48ea589e7edb4e26e1a8
ducviet00-h2 <viet.d.hoang@h2corporation.jp>
2025-08-18 23:06:21 +0700

    fix ci

--- src/transformers/models/florence2/modeling_florence2.py
+++ src/transformers/models/florence2/modeling_florence2.py
@@ -835,7 +835,7 @@ class Florence2ForConditionalGeneration(Florence2PreTrainedModel, GenerationMixi
     def get_image_features(self, pixel_values: torch.Tensor, **kwargs):
         return self.model.get_image_features(pixel_values=pixel_values, **kwargs)
 
-    # Make modules available throught conditional class for BC
+    # Make modules available through conditional class for BC
     @property
     def language_model(self):
         return self.model.language_model
