8f1c0817cad29563602344c34de5481926174fff
EduardDurech <39579228+EduardDurech@users.noreply.github.com>
2025-08-19 22:34:29 +0200

    Update license
    Co-authored-by: Cyril Vallez <cyril.vallez@gmail.com>

--- src/transformers/models/apertus/modular_apertus.py
+++ src/transformers/models/apertus/modular_apertus.py
@@ -1,9 +1,6 @@
 # coding=utf-8
-# Copyright 2025 EleutherAI, the HuggingFace Inc. team, and the Swiss AI Initiative. All rights reserved.
+# Copyright 2025 the HuggingFace Inc. team and the Swiss AI Initiative. All rights reserved.
 #
-# This code is based on HuggingFace's LLaMA implementation in this library.
-# It has been modified from its original forms to accommodate the architectural
-# differences made by the Swiss AI Initiative that trained the model.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
