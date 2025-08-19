1fb826cda62b5c7c93c11f64587c6f0d1f113f55
raushan <raushan@huggingface.co>
2025-08-19 18:31:37 +0200

    typo

--- src/transformers/models/sam/image_processing_sam_fast.py
+++ src/transformers/models/sam/image_processing_sam_fast.py
@@ -54,7 +54,7 @@ if is_torch_available():
 if is_torchvision_available():
     from torchvision.ops.boxes import batched_nms
 
-    from .image_utils import pil_torch_interpolation_mapping
+    from ...image_utils import pil_torch_interpolation_mapping
 
     if is_torchvision_v2_available():
         from torchvision.transforms.v2 import functional as F_t
