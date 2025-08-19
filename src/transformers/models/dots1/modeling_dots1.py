49941ad27696e0bdc792236c4efb66132e4abb7b
Cyril Vallez <cyril.vallez@gmail.com>
2025-08-19 11:15:42 +0200

    rename test

--- src/transformers/models/dots1/modeling_dots1.py
+++ src/transformers/models/dots1/modeling_dots1.py
@@ -422,8 +422,7 @@ class Dots1PreTrainedModel(PreTrainedModel):
     _supports_flash_attn = True
     _supports_sdpa = True
     _supports_flex_attn = True
-
-    _can_compile_fullgraph = True
+    _can_compile_fullgraph = False
     _supports_attention_backend = True
     _can_record_outputs = {
         "hidden_states": Dots1DecoderLayer,
