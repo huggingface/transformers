34c88ccd2f141f66fd5cae80ba5c2a9d061376ce
ydshieh <ydshieh@users.noreply.github.com>
2025-08-20 16:55:09 +0200

    trigger FA2

--- utils/split_model_tests.py
+++ utils/split_model_tests.py
@@ -62,5 +62,5 @@ if __name__ == "__main__":
         start = end
         end = start + num_jobs_per_splits + (1 if idx < num_jobs % args.num_splits else 0)
         model_splits.append(d[start:end])
-
+    model_splits = [["models/vit"]]
     print(model_splits)
