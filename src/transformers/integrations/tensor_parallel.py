82a02b34a590d2e42b795411edbb498b537284fb
Arthur <48595927+ArthurZucker@users.noreply.github.com>
2025-08-20 13:42:47 +0200

    Update src/transformers/integrations/tensor_parallel.py

--- src/transformers/integrations/tensor_parallel.py
+++ src/transformers/integrations/tensor_parallel.py
@@ -1013,7 +1013,7 @@ def shard_and_distribute_module(
 
     """
     param_name, param_type = parameter_name.rsplit(".", 1) if "." in parameter_name else parameter_name
-    tp_plan = model.tp_plan() or {}
+    tp_plan = model.tp_plan or {}
     tp_plan.update(getattr(type(model), "tp_plan", None) or {})
     module_to_tp = model.get_submodule(param_name)  # TODO: can i loop over modules?
     rank = int(rank)
