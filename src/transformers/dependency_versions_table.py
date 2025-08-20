514073cc1bc3a6243d549549518ba18ffde66469
Matt <rocketknight1@gmail.com>
2025-08-20 14:11:13 +0100

    update versions table

--- src/transformers/dependency_versions_table.py
+++ src/transformers/dependency_versions_table.py
@@ -92,7 +92,7 @@ deps = {
     "tiktoken": "tiktoken",
     "timm": "timm<=1.0.19,!=1.0.18",
     "tokenizers": "tokenizers>=0.21,<0.22",
-    "torch": "torch>=2.1",
+    "torch": "torch>=2.2",
     "torchaudio": "torchaudio",
     "torchvision": "torchvision",
     "pyctcdecode": "pyctcdecode>=0.4.0",
