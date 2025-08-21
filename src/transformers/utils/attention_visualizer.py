9b129982acb1f81fe8b6b35e1faa10129d2f9c62
molbap <pablo.montalvo.leroux@gmail.com>
2025-08-21 12:00:51 +0200

    try with flush

--- src/transformers/utils/attention_visualizer.py
+++ src/transformers/utils/attention_visualizer.py
@@ -229,16 +229,17 @@ class AttentionMaskVisualizer:
             len(f"Attention visualization for {self.config.model_type} | {self.mapped_cls}") + 4
         )  # Box width adjusted to text length
         side_border = "##"
-        print(f"\n{top_bottom_border}")
+        print(f"\n{top_bottom_border}", flush=True)
         print(
             "##"
             + f"  Attention visualization for \033[1m{self.config.model_type}:{self.repo_id}\033[0m {self.mapped_cls.__name__}".center(
                 len(top_bottom_border)
             )
             + "    "
-            + side_border
+            + side_border,
+            flush=True,
         )
-        print(f"{top_bottom_border}")
+        print(f"{top_bottom_border}", flush=True)
         f_string = generate_attention_matrix_from_mask(
             tokens,
             attention_mask,
@@ -247,5 +248,5 @@ class AttentionMaskVisualizer:
             token_type_ids=kwargs.get("token_type_ids"),
             image_seq_length=image_seq_length,
         )
-        print(f_string)
-        print(f"{top_bottom_border}")
+        print(f_string, flush=True)
+        print(f"{top_bottom_border}", flush=True)
