e3de183e58525eeddb069932ac38735ddf8a0574
yuekaiz <yuekaiz@mgmt1-login.cm.cluster>
2025-08-21 17:10:16 +0800

    remove try except

--- src/transformers/models/qwen2_5_omni/processing_qwen2_5_omni.py
+++ src/transformers/models/qwen2_5_omni/processing_qwen2_5_omni.py
@@ -195,16 +195,17 @@ class Qwen2_5OmniProcessor(ProcessorMixin):
         if not isinstance(text, list):
             text = [text]
 
-        text = self.replace_multimodal_special_tokens(
-            text,
-            audio_lengths,
-            image_grid_thw,
-            video_grid_thw,
-            video_second_per_grid=video_second_per_grid,
-            use_audio_in_video=use_audio_in_video,
-            position_id_per_seconds=position_id_per_seconds,
-            seconds_per_chunk=seconds_per_chunk,
-        )
+        if images is not None or videos is not None or audio is not None:
+            text = self.replace_multimodal_special_tokens(
+                text,
+                audio_lengths,
+                image_grid_thw,
+                video_grid_thw,
+                video_second_per_grid=video_second_per_grid,
+                use_audio_in_video=use_audio_in_video,
+                position_id_per_seconds=position_id_per_seconds,
+                seconds_per_chunk=seconds_per_chunk,
+            )
 
         texts_inputs = self.tokenizer(text, **output_kwargs["text_kwargs"])
 
@@ -238,54 +239,45 @@ class Qwen2_5OmniProcessor(ProcessorMixin):
 
             for _, special_token in positions:
                 if special_token == self.audio_token:
-                    try:
-                        sample = sample.replace(self.audio_token, "<|audio_placeholder|>" * next(audio_lengths), 1)
-                    except StopIteration:
-                        logging.warning(f"Audio token `{self.audio_token}` found in text but no audio is provided.")
+                    sample = sample.replace(self.audio_token, "<|audio_placeholder|>" * next(audio_lengths), 1)
                 elif special_token == self.image_token:
-                    try:
-                        image_seq_length = next(image_grid_thw).prod() // merge_length_image
-                        sample = sample.replace(self.image_token, "<|image_placeholder|>" * image_seq_length, 1)
-                    except StopIteration:
-                        logging.warning(f"Image token `{self.image_token}` found in text but no image is provided.")
+                    image_seq_length = next(image_grid_thw).prod() // merge_length_image
+                    sample = sample.replace(self.image_token, "<|image_placeholder|>" * image_seq_length, 1)
                 elif special_token == self.video_token:
-                    try:
-                        if not use_audio_in_video:
-                            video_seq_length = next(video_grid_thw).prod() // merge_length_video
-                            sample = sample.replace(self.video_token, "<|video_placeholder|>" * video_seq_length, 1)
-                        else:
-                            audio_token_indices = np.arange(next(audio_lengths))
-                            curr_video_grid_thw = next(video_grid_thw)
-                            height = curr_video_grid_thw[1] // self.video_processor.merge_size
-                            width = curr_video_grid_thw[2] // self.video_processor.merge_size
-                            video_token_indices = np.arange(curr_video_grid_thw[0]).reshape(-1, 1, 1)
-                            video_token_indices = np.broadcast_to(
-                                video_token_indices, (video_token_indices.shape[0], height, width)
-                            ).reshape(-1)
-                            video_token_indices = (
-                                video_token_indices * next(video_second_per_grid) * position_id_per_seconds
-                            )
+                    if not use_audio_in_video:
+                        video_seq_length = next(video_grid_thw).prod() // merge_length_video
+                        sample = sample.replace(self.video_token, "<|video_placeholder|>" * video_seq_length, 1)
+                    else:
+                        audio_token_indices = np.arange(next(audio_lengths))
+                        curr_video_grid_thw = next(video_grid_thw)
+                        height = curr_video_grid_thw[1] // self.video_processor.merge_size
+                        width = curr_video_grid_thw[2] // self.video_processor.merge_size
+                        video_token_indices = np.arange(curr_video_grid_thw[0]).reshape(-1, 1, 1)
+                        video_token_indices = np.broadcast_to(
+                            video_token_indices, (video_token_indices.shape[0], height, width)
+                        ).reshape(-1)
+                        video_token_indices = (
+                            video_token_indices * next(video_second_per_grid) * position_id_per_seconds
+                        )
 
-                            tokens_per_chunk = int(position_id_per_seconds * seconds_per_chunk)
-                            video_chunk_indexes = self.get_chunked_index(video_token_indices, tokens_per_chunk)
-                            audio_chunk_indexes = self.get_chunked_index(audio_token_indices, tokens_per_chunk)
+                        tokens_per_chunk = int(position_id_per_seconds * seconds_per_chunk)
+                        video_chunk_indexes = self.get_chunked_index(video_token_indices, tokens_per_chunk)
+                        audio_chunk_indexes = self.get_chunked_index(audio_token_indices, tokens_per_chunk)
 
-                            placeholder_string = self.vision_bos_token + self.audio_bos_token
-                            for j in range(max(len(video_chunk_indexes), len(audio_chunk_indexes))):
-                                if j < len(video_chunk_indexes):
-                                    video_seq_length = video_chunk_indexes[j][1] - video_chunk_indexes[j][0]
-                                    placeholder_string += "<|video_placeholder|>" * video_seq_length
-                                if j < len(audio_chunk_indexes):
-                                    audio_seq_length = audio_chunk_indexes[j][1] - audio_chunk_indexes[j][0]
-                                    placeholder_string += "<|audio_placeholder|>" * audio_seq_length
-                            placeholder_string += self.audio_eos_token + self.vision_eos_token
-                            sample = sample.replace(
-                                self.vision_bos_token + self.video_token + self.vision_eos_token,
-                                placeholder_string,
-                                1,
-                            )
-                    except StopIteration:
-                        logging.warning(f"Video token `{self.video_token}` found in text but no video is provided.")
+                        placeholder_string = self.vision_bos_token + self.audio_bos_token
+                        for j in range(max(len(video_chunk_indexes), len(audio_chunk_indexes))):
+                            if j < len(video_chunk_indexes):
+                                video_seq_length = video_chunk_indexes[j][1] - video_chunk_indexes[j][0]
+                                placeholder_string += "<|video_placeholder|>" * video_seq_length
+                            if j < len(audio_chunk_indexes):
+                                audio_seq_length = audio_chunk_indexes[j][1] - audio_chunk_indexes[j][0]
+                                placeholder_string += "<|audio_placeholder|>" * audio_seq_length
+                        placeholder_string += self.audio_eos_token + self.vision_eos_token
+                        sample = sample.replace(
+                            self.vision_bos_token + self.video_token + self.vision_eos_token,
+                            placeholder_string,
+                            1,
+                        )
 
             sample = sample.replace("<|audio_placeholder|>", self.audio_token)
             sample = sample.replace("<|image_placeholder|>", self.image_token)
