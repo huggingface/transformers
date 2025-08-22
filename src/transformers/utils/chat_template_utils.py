b87b036aadd35e3ff6b56ea7d2c2ea132d565bc3
Your Name <you@example.com>
2025-08-22 05:06:40 +0000

    Add fromjson filter to jinja2 environment

--- src/transformers/utils/chat_template_utils.py
+++ src/transformers/utils/chat_template_utils.py
@@ -450,6 +450,11 @@ def _compile_jinja_template(chat_template):
         # We also expose some options like custom indents and separators
         return json.dumps(x, ensure_ascii=ensure_ascii, indent=indent, separators=separators, sort_keys=sort_keys)
 
+    def fromjson(val):
+        if isinstance(val, str):
+            return json.loads(val)
+        return val
+
     def strftime_now(format):
         return datetime.now().strftime(format)
 
@@ -457,6 +462,7 @@ def _compile_jinja_template(chat_template):
         trim_blocks=True, lstrip_blocks=True, extensions=[AssistantTracker, jinja2.ext.loopcontrols]
     )
     jinja_env.filters["tojson"] = tojson
+    jinja_env.filters["fromjson"] = fromjson
     jinja_env.globals["raise_exception"] = raise_exception
     jinja_env.globals["strftime_now"] = strftime_now
     return jinja_env.from_string(chat_template)
