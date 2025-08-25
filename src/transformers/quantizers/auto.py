dfc9ef3d6eb410ad5d09c24d9e7d6023367f9fac
Marc Sun <marc@huggingface.co>
2025-08-25 16:09:31 +0200

    style

--- src/transformers/quantizers/auto.py
+++ src/transformers/quantizers/auto.py
@@ -295,9 +295,7 @@ def register_quantizer(name: str):
     return register_quantizer_fn
 
 
-def get_hf_quantizer(
-    config, quantization_config, dtype, from_tf, from_flax, device_map, weights_only, user_agent
-):
+def get_hf_quantizer(config, quantization_config, dtype, from_tf, from_flax, device_map, weights_only, user_agent):
     pre_quantized = hasattr(config, "quantization_config")
     if pre_quantized and not AutoHfQuantizer.supports_quant_method(config.quantization_config):
         pre_quantized = False
