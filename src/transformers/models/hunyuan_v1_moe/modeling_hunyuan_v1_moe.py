ba095d387dde8a5fe7574ebbb2671dbb0991b215
Pablo Montalvo <39954772+molbap@users.noreply.github.com>
2025-08-25 10:57:56 +0200

    :broom: :broom: :broom: Get set decoder cleanup (#39509)
    * simplify common get/set
    
    * remove some noise
    
    * change some 5 years old modeling utils
    
    * update examples
    
    * fix copies
    
    * revert some changes
    
    * fixes, gah
    
    * format
    
    * move to Mixin
    
    * remove smolvlm specific require grad
    
    * skip
    
    * force defaults
    
    * remodularise some stuff
    
    * remodularise more stuff
    
    * add safety for audio models
    
    * style
    
    * have a correct fallback, you daft donkey
    
    * remove this argh
    
    * change heuristic for audio models
    
    * fixup
    
    * revert
    
    * this works
    
    * this should be explicit
    
    * fix Nth ESM exception
    
    * tryout decoder
    
    * this as well
    
    * revert again
    
    * 🧠
    
    * aaah ESM has two modelings aaah
    
    * broom broom
    
    * format
    
    * wrong copies
    
    * copies
    
    * modular cleanups
    
    * format
    
    * modularities
    
    * wrong mergefix
    
    * seriously
    
    * align with new model
    
    * new model

--- src/transformers/models/hunyuan_v1_moe/modeling_hunyuan_v1_moe.py
+++ src/transformers/models/hunyuan_v1_moe/modeling_hunyuan_v1_moe.py
@@ -512,12 +512,6 @@ class HunYuanMoEV1ForCausalLM(HunYuanMoEV1PreTrainedModel, GenerationMixin):
         # Initialize weights and apply final processing
         self.post_init()
 
-    def set_decoder(self, decoder):
-        self.model = decoder
-
-    def get_decoder(self):
-        return self.model
-
     @can_return_tuple
     @auto_docstring
     def forward(
