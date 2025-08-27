acb4b0c5906e7bc748183cf4c55e197a600adf59
raushan <raushan@huggingface.co>
2025-08-27 15:08:34 +0200

    fixup

--- src/transformers/models/qwen2_5_omni/modeling_qwen2_5_omni.py
+++ src/transformers/models/qwen2_5_omni/modeling_qwen2_5_omni.py
@@ -1586,7 +1586,7 @@ class Qwen2_5OmniThinkerTextModel(Qwen2_5OmniPreTrainedModel):
             text_position_ids = position_ids[0]
             position_ids = position_ids[1:]
         else:
-            text_position_ids = position_ids[0]
+            text_position_ids = None
 
         # It may already have been prepared by e.g. `generate`
         if not isinstance(causal_mask_mapping := attention_mask, dict):
@@ -2161,7 +2161,7 @@ class Qwen2_5OmniTalkerModel(Qwen2_5OmniPreTrainedModel):
             text_position_ids = position_ids[0]
             position_ids = position_ids[1:]
         else:
-            text_position_ids = position_ids[0]
+            text_position_ids = None
 
         # It may already have been prepared by e.g. `generate`
         if not isinstance(causal_mask_mapping := attention_mask, dict):
