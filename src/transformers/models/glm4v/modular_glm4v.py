b2e8bf0da3c6b052ce83463047780fa89f8345e0
Yuanyuan Chen <cyyever@outlook.com>
2025-08-29 19:25:33 +0800

    Fix typos (#40511)
    Signed-off-by: cyy <cyyever@outlook.com>

--- src/transformers/models/glm4v/modular_glm4v.py
+++ src/transformers/models/glm4v/modular_glm4v.py
@@ -1154,7 +1154,7 @@ class Glm4vModel(Qwen2_5_VLModel):
         video_features: torch.FloatTensor = None,
     ):
         """
-        Obtains multimodal placeholdr mask from `input_ids` or `inputs_embeds`, and checks that the placeholder token count is
+        Obtains multimodal placeholder mask from `input_ids` or `inputs_embeds`, and checks that the placeholder token count is
         equal to the length of multimodal features. If the lengths are different, an error is raised.
         """
         if input_ids is None:
