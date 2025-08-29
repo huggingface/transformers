80afd1e861f04e5f0136159ea96c94b2231a6a7f
Arthur <arthur.zucker@gmail.com>
2025-08-29 12:54:01 +0200

    fix bump!

--- src/transformers/dependency_versions_table.py
+++ src/transformers/dependency_versions_table.py
@@ -91,7 +91,7 @@ deps = {
     "timeout-decorator": "timeout-decorator",
     "tiktoken": "tiktoken",
     "timm": "timm<=1.0.19,!=1.0.18",
-    "tokenizers": "tokenizers==0.22.0",
+    "tokenizers": "tokenizers>=0.22.0,<=0.23.0",
     "torch": "torch>=2.2",
     "torchaudio": "torchaudio",
     "torchvision": "torchvision",
