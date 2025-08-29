a4349357a6631319d90209981ab60122b3faedc7
Marc Sun <marc@huggingface.co>
2025-08-29 14:23:28 +0000

    fix

--- src/transformers/models/gpt_oss/configuration_gpt_oss.py
+++ src/transformers/models/gpt_oss/configuration_gpt_oss.py
@@ -108,9 +108,7 @@ class GptOssConfig(PretrainedConfig):
         # BC: if there is a 'type' field, copy it it to 'rope_type'.
         if self.rope_scaling is not None and "type" in self.rope_scaling:
             self.rope_scaling["rope_type"] = self.rope_scaling["type"]
-        print("hi")
         rope_config_validation(self)
-        print('end')
 
         super().__init__(
             tie_word_embeddings=tie_word_embeddings,
