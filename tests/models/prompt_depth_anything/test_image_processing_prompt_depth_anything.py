1e8a9bd579ce9723a176191ecfe334d97b9c77db
Samuel Barry <samuel.barry@mistral.ai>
2025-09-01 15:57:58 -0700

    Test & import setup

--- tests/models/prompt_depth_anything/test_image_processing_prompt_depth_anything.py
+++ tests/models/prompt_depth_anything/test_image_processing_prompt_depth_anything.py
@@ -17,7 +17,7 @@ import unittest
 
 import numpy as np
 
-from transformers.file_utils import is_vision_available
+from transformers.utils import is_torchvision_available, is_vision_available
 from transformers.testing_utils import require_torch, require_vision
 
 from ...test_image_processing_common import ImageProcessingTestMixin, prepare_image_inputs
@@ -25,6 +25,9 @@ from ...test_image_processing_common import ImageProcessingTestMixin, prepare_im
 
 if is_vision_available():
     from transformers import PromptDepthAnythingImageProcessor
+
+    if is_torchvision_available():
+        from transformers import PromptDepthAnythingImageProcessorFast
 
 
 class PromptDepthAnythingImageProcessingTester(unittest.TestCase):
@@ -84,6 +87,7 @@ class PromptDepthAnythingImageProcessingTester(unittest.TestCase):
 @require_vision
 class PromptDepthAnythingImageProcessingTest(ImageProcessingTestMixin, unittest.TestCase):
     image_processing_class = PromptDepthAnythingImageProcessor if is_vision_available() else None
+    fast_image_processing_class = PromptDepthAnythingImageProcessorFast if is_torchvision_available() else None
 
     def setUp(self):
         super().setUp()
@@ -94,45 +98,49 @@ class PromptDepthAnythingImageProcessingTest(ImageProcessingTestMixin, unittest.
         return self.image_processor_tester.prepare_image_processor_dict()
 
     def test_image_processor_properties(self):
-        image_processing = self.image_processing_class(**self.image_processor_dict)
-        self.assertTrue(hasattr(image_processing, "image_mean"))
-        self.assertTrue(hasattr(image_processing, "image_std"))
-        self.assertTrue(hasattr(image_processing, "do_normalize"))
-        self.assertTrue(hasattr(image_processing, "do_resize"))
-        self.assertTrue(hasattr(image_processing, "size"))
-        self.assertTrue(hasattr(image_processing, "do_rescale"))
-        self.assertTrue(hasattr(image_processing, "rescale_factor"))
-        self.assertTrue(hasattr(image_processing, "do_pad"))
-        self.assertTrue(hasattr(image_processing, "size_divisor"))
-        self.assertTrue(hasattr(image_processing, "prompt_scale_to_meter"))
+        for image_processing_class in self.image_processor_list:
+            image_processing = image_processing_class(**self.image_processor_dict)
+            self.assertTrue(hasattr(image_processing, "image_mean"))
+            self.assertTrue(hasattr(image_processing, "image_std"))
+            self.assertTrue(hasattr(image_processing, "do_normalize"))
+            self.assertTrue(hasattr(image_processing, "do_resize"))
+            self.assertTrue(hasattr(image_processing, "size"))
+            self.assertTrue(hasattr(image_processing, "do_rescale"))
+            self.assertTrue(hasattr(image_processing, "rescale_factor"))
+            self.assertTrue(hasattr(image_processing, "do_pad"))
+            self.assertTrue(hasattr(image_processing, "size_divisor"))
+            self.assertTrue(hasattr(image_processing, "prompt_scale_to_meter"))
 
     def test_image_processor_from_dict_with_kwargs(self):
-        image_processor = self.image_processing_class.from_dict(self.image_processor_dict)
-        self.assertEqual(image_processor.size, {"height": 18, "width": 18})
+        for image_processing_class in self.image_processor_list:
+            image_processor = image_processing_class.from_dict(self.image_processor_dict)
+            self.assertEqual(image_processor.size, {"height": 18, "width": 18})
 
-        image_processor = self.image_processing_class.from_dict(self.image_processor_dict, size=42)
-        self.assertEqual(image_processor.size, {"height": 42, "width": 42})
+            image_processor = image_processing_class.from_dict(self.image_processor_dict, size=42)
+            self.assertEqual(image_processor.size, {"height": 42, "width": 42})
 
     def test_keep_aspect_ratio(self):
         size = {"height": 512, "width": 512}
-        image_processor = PromptDepthAnythingImageProcessor(size=size, keep_aspect_ratio=True, ensure_multiple_of=32)
+        for image_processing_class in self.image_processor_list:
+            image_processor = image_processing_class(size=size, keep_aspect_ratio=True, ensure_multiple_of=32)
 
-        image = np.zeros((489, 640, 3))
+            image = np.zeros((489, 640, 3))
 
-        pixel_values = image_processor(image, return_tensors="pt").pixel_values
+            pixel_values = image_processor(image, return_tensors="pt").pixel_values
 
-        self.assertEqual(list(pixel_values.shape), [1, 3, 512, 672])
+            self.assertEqual(list(pixel_values.shape), [1, 3, 512, 672])
 
     def test_prompt_depth_processing(self):
         size = {"height": 756, "width": 756}
-        image_processor = PromptDepthAnythingImageProcessor(size=size, keep_aspect_ratio=True, ensure_multiple_of=32)
+        for image_processing_class in self.image_processor_list:
+            image_processor = image_processing_class(size=size, keep_aspect_ratio=True, ensure_multiple_of=32)
 
-        image = np.zeros((756, 1008, 3))
-        prompt_depth = np.random.random((192, 256))
+            image = np.zeros((756, 1008, 3))
+            prompt_depth = np.random.random((192, 256))
 
-        outputs = image_processor(image, prompt_depth=prompt_depth, return_tensors="pt")
-        pixel_values = outputs.pixel_values
-        prompt_depth_values = outputs.prompt_depth
+            outputs = image_processor(image, prompt_depth=prompt_depth, return_tensors="pt")
+            pixel_values = outputs.pixel_values
+            prompt_depth_values = outputs.prompt_depth
 
-        self.assertEqual(list(pixel_values.shape), [1, 3, 768, 1024])
-        self.assertEqual(list(prompt_depth_values.shape), [1, 1, 192, 256])
+            self.assertEqual(list(pixel_values.shape), [1, 3, 768, 1024])
+            self.assertEqual(list(prompt_depth_values.shape), [1, 1, 192, 256])
