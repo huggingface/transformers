7f2d029c4b6646fc2f826e52793ab2bc26bf158d
ydshieh <ydshieh@users.noreply.github.com>
2025-09-01 17:56:25 +0200

    add to fetch script

--- utils/fetch_hub_objects_for_ci.py
+++ utils/fetch_hub_objects_for_ci.py
@@ -14,6 +14,10 @@ URLS_FOR_TESTING_DATA = [
     "https://huggingface.co/datasets/raushan-testing-hf/audio-test/resolve/main/f2641_0_throatclearing.wav",
     "https://www.ilankelman.org/stopsigns/australia.jpg",
     "http://images.cocodataset.org/val2017/000000039769.jpg",
+    "https://cdn.britannica.com/61/93061-050-99147DCE/Statue-of-Liberty-Island-New-York-Bay.jpg",
+    "https://thumbs.dreamstime.com/b/golden-gate-bridge-san-francisco-purple-flowers-california-echium-candicans-36805947.jpg",
+    "https://llava-vl.github.io/static/images/view.jpg",
+    "https://huggingface.co/datasets/hf-internal-testing/fixtures_videos/resolve/main/tennis.mp4",
 ]
 
 
