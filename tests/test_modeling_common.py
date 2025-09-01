35acb8d1b76fa472d040c7fecfa76e40c8676b32
Cyril Vallez <cyril.vallez@gmail.com>
2025-09-01 16:13:30 +0200

    style

--- tests/test_modeling_common.py
+++ tests/test_modeling_common.py
@@ -3529,7 +3529,9 @@ class ModelTesterMixin:
                 if model.main_input_name != "pixel_values" and "pixel_values" in inputs_dict:
                     first_inputs["pixel_values"] = inputs_dict["pixel_values"][:1].to(torch.bfloat16)
                 if model.config.is_encoder_decoder:
-                    first_inputs["decoder_input_ids"] = inputs_dict.get("decoder_input_ids", first_inputs["input_ids"])[:1]
+                    first_inputs["decoder_input_ids"] = inputs_dict.get(
+                        "decoder_input_ids", first_inputs["input_ids"]
+                    )[:1]
 
                 # Create attention mask with padding
                 dummy_attention_mask = inputs_dict.get("attention_mask", None)
