29685493b31fe8a1e54a97cc3178c38d43b57e6f
Lasha <26011196+lashahub@users.noreply.github.com>
2025-09-02 13:58:15 -0400

    Change year

--- src/transformers/models/audioflamingo3/__init__.py
+++ src/transformers/models/audioflamingo3/__init__.py
@@ -1,4 +1,4 @@
-# Copyright 2024 NVIDIA CORPORATION and The HuggingFace Team. All rights reserved.
+# Copyright 2025 NVIDIA CORPORATION and The HuggingFace Team. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
