db1a3840693d3cc98c4f3889d97ba50a320a60ba
ydshieh <ydshieh@users.noreply.github.com>
2025-09-02 18:25:41 +0200

    fix

--- utils/fetch_hub_objects_for_ci.py
+++ utils/fetch_hub_objects_for_ci.py
@@ -2,7 +2,12 @@ import os
 
 import requests
 from huggingface_hub import Repository, hf_hub_download
-from mistral_common.tokens.tokenizers.mistral import MistralTokenizer
+
+from transformers import is_mistral_common_available
+
+
+if is_mistral_common_available():
+    from mistral_common.tokens.tokenizers.mistral import MistralTokenizer
 
 from transformers.testing_utils import _run_pipeline_tests, _run_staging
 
@@ -52,19 +57,20 @@ if __name__ == "__main__":
 
     # For `tests/test_tokenization_mistral_common.py:TestMistralCommonTokenizer`, which eventually calls
     # `mistral_common.tokens.tokenizers.utils.download_tokenizer_from_hf_hub` which (probably) doesn't have the cache.
-    from mistral_common.tokens.tokenizers.mistral import MistralTokenizer
+    if is_mistral_common_available():
+        from mistral_common.tokens.tokenizers.mistral import MistralTokenizer
 
-    from transformers import AutoTokenizer
-    from transformers.tokenization_mistral_common import MistralCommonTokenizer
+        from transformers import AutoTokenizer
+        from transformers.tokenization_mistral_common import MistralCommonTokenizer
 
-    repo_id = "hf-internal-testing/namespace-mistralai-repo_name-Mistral-Small-3.1-24B-Instruct-2503"
-    AutoTokenizer.from_pretrained(repo_id, tokenizer_type="mistral")
-    MistralCommonTokenizer.from_pretrained(repo_id)
-    MistralTokenizer.from_hf_hub(repo_id)
+        repo_id = "hf-internal-testing/namespace-mistralai-repo_name-Mistral-Small-3.1-24B-Instruct-2503"
+        AutoTokenizer.from_pretrained(repo_id, tokenizer_type="mistral")
+        MistralCommonTokenizer.from_pretrained(repo_id)
+        MistralTokenizer.from_hf_hub(repo_id)
 
-    repo_id = "mistralai/Voxtral-Mini-3B-2507"
-    AutoTokenizer.from_pretrained(repo_id)
-    MistralTokenizer.from_hf_hub(repo_id)
+        repo_id = "mistralai/Voxtral-Mini-3B-2507"
+        AutoTokenizer.from_pretrained(repo_id)
+        MistralTokenizer.from_hf_hub(repo_id)
 
     # Download files from URLs to local directory
     for url in URLS_FOR_TESTING_DATA:
