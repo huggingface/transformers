2febeea00aea1c5f0eb273dd82bab5d90ce62c02
remi-or <remi.pierre_o@orange.fr>
2025-09-03 21:21:34 +0000

    Small fixes

--- examples/pytorch/continuous_batching.py
+++ examples/pytorch/continuous_batching.py
@@ -27,8 +27,8 @@ from transformers.generation import GenerationConfig
 
 
 # MODEL_ID = "Qwen/Qwen3-4B-Instruct-2507"
-MODEL_ID = "google/gemma-2-2b-it"
-SLIDIND_WINDOW = 256
+SLIDIND_WINDOW = 0
+MODEL_ID = "google/gemma-2-2b-it" if SLIDIND_WINDOW > 0 else "Qwen/Qwen3-4B-Instruct-2507"
 FORCE_MAX_LENGTH = False # should be False unless you are debugging sliding window features
 
 
