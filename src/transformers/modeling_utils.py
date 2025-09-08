dc262ee6f57f2154f5233e53482da14dbe3be834
Wing Lian <wing@axolotl.ai>
2025-09-08 08:52:31 -0400

    remove FSDP prefix when using save_pretrained with FSDP2 (#40207)
    * remove FSDP prefix when using save_pretrained with FSDP2
    
    * Fix: use removeprefix correctly
    
    ---------
    
    Co-authored-by: Matej Sirovatka <54212263+S1ro1@users.noreply.github.com>
    Co-authored-by: S1ro1 <matej.sirovatka@gmail.com>

--- src/transformers/modeling_utils.py
+++ src/transformers/modeling_utils.py
@@ -4022,7 +4022,8 @@ class PreTrainedModel(nn.Module, EmbeddingAccessMixin, ModuleUtilsMixin, PushToH
         model_to_save.config.dtype = str(dtype).split(".")[1]
 
         # Attach architecture to the config
-        model_to_save.config.architectures = [model_to_save.__class__.__name__]
+        # When using FSDP2, unwrapping is a noop, so the model name doesn't change back to the original model name
+        model_to_save.config.architectures = [model_to_save.__class__.__name__.removeprefix("FSDP")]
 
         # If we have a custom model, we copy the file defining it in the folder and set the attributes so it can be
         # loaded from the Hub.
