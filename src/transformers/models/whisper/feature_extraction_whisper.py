617a421e85a41f6387d413f9d5a9e01ed4d4b9d1
Eustache Le Bihan <eulebihan@gmail.com>
2025-09-10 09:50:48 +0200

    make style

--- src/transformers/models/whisper/feature_extraction_whisper.py
+++ src/transformers/models/whisper/feature_extraction_whisper.py
@@ -329,7 +329,7 @@ class WhisperFeatureExtractor(SequenceFeatureExtractor):
             rescaled_attention_mask = padded_inputs["attention_mask"][:, :: self.hop_length]
             if padded_inputs["attention_mask"].shape[1] % self.hop_length != 0:
                 rescaled_attention_mask = rescaled_attention_mask[:, :-1]
-            padded_inputs["attention_mask"] = rescaled_attention_mask         
+            padded_inputs["attention_mask"] = rescaled_attention_mask
 
         if return_token_timestamps is not None:
             logger.warning_once(
