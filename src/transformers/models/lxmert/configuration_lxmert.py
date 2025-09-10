035442046d0228f07f4cc4c8fcb95b3e3a548f76
Cyril Vallez <cyril.vallez@gmail.com>
2025-09-10 15:17:53 +0200

    fix configs

--- src/transformers/models/lxmert/configuration_lxmert.py
+++ src/transformers/models/lxmert/configuration_lxmert.py
@@ -66,8 +66,6 @@ class LxmertConfig(PretrainedConfig):
             The vocabulary size of the *token_type_ids* passed into [`BertModel`].
         initializer_range (`float`, *optional*, defaults to 0.02):
             The standard deviation of the truncated_normal_initializer for initializing all weight matrices.
-        layer_norm_eps (`float`, *optional*, defaults to 1e-12):
-            The epsilon used by the layer normalization layers.
         l_layers (`int`, *optional*, defaults to 9):
             Number of hidden layers in the Transformer language encoder.
         x_layers (`int`, *optional*, defaults to 5):
@@ -119,7 +117,6 @@ class LxmertConfig(PretrainedConfig):
         max_position_embeddings=512,
         type_vocab_size=2,
         initializer_range=0.02,
-        layer_norm_eps=1e-12,
         l_layers=9,
         x_layers=5,
         r_layers=5,
@@ -145,7 +142,6 @@ class LxmertConfig(PretrainedConfig):
         self.max_position_embeddings = max_position_embeddings
         self.type_vocab_size = type_vocab_size
         self.initializer_range = initializer_range
-        self.layer_norm_eps = layer_norm_eps
         self.num_qa_labels = num_qa_labels
         self.num_object_labels = num_object_labels
         self.num_attr_labels = num_attr_labels
