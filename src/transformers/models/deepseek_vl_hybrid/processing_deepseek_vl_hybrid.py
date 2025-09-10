08edec9f7da1ed3d43f277a870cd8cf5305df4b1
Raushan Turganbay <raushan@huggingface.co>
2025-09-10 10:37:19 +0200

    [processors] Unbloating simple processors (#40377)
    * modularize processor - step 1
    
    * typos
    
    * why raise error, super call check it also
    
    * tiny update
    
    * fix copies
    
    * fix style and test
    
    * lost an import / fix copies
    
    * fix tests
    
    * oops deleted accidentally

--- src/transformers/models/deepseek_vl_hybrid/processing_deepseek_vl_hybrid.py
+++ src/transformers/models/deepseek_vl_hybrid/processing_deepseek_vl_hybrid.py
@@ -78,7 +78,7 @@ class DeepseekVLHybridProcessor(ProcessorMixin):
         """
         Main method to prepare for the model one or several sequences(s) and image(s). This method forwards the `text`
         and `kwargs` arguments to LlamaTokenizerFast's [`~LlamaTokenizerFast.__call__`] if `text` is not `None` to encode
-        the text. To prepare the image(s), this method forwards the `images` and `kwrags` arguments to
+        the text. To prepare the image(s), this method forwards the `images` and `kwargs` arguments to
         DeepseekVLHybridImageProcessor's [`~DeepseekVLHybridImageProcessor.__call__`] if `images` is not `None`. Please refer to the doctsring
         of the above two methods for more information.
 
