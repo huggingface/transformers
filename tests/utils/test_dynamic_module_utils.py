bc43bade505d98bf2dfe93b56797576cdffe9eff
Matt <rocketknight1@gmail.com>
2025-09-10 15:22:10 +0100

    make fixup

--- tests/utils/test_dynamic_module_utils.py
+++ tests/utils/test_dynamic_module_utils.py
@@ -16,9 +16,7 @@ import os
 
 import pytest
 
-from transformers import AutoModel
 from transformers.dynamic_module_utils import get_imports
-from transformers.testing_utils import require_torch
 
 
 TOP_LEVEL_IMPORT = """
