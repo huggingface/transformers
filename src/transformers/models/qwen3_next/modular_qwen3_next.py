7cac277ed13aa1fa67cf18f0a81fc10a74e46470
Cyril Vallez <cyril.vallez@gmail.com>
2025-09-11 12:34:11 +0200

    remove unused attr

--- src/transformers/models/qwen3_next/modular_qwen3_next.py
+++ src/transformers/models/qwen3_next/modular_qwen3_next.py
@@ -282,7 +282,6 @@ def torch_chunk_gated_delta_rule(
 ):
     initial_dtype = query.dtype
     if use_qk_l2norm_in_kernel:
-        head_dim = query.size(-1)
         inv_scale = query.size(-1)**-0.5
         query = F.rms_norm(query, (head_dim,), eps=1e-6) * inv_scale
         key = F.rms_norm(key, (head_dim,), eps=1e-6) * inv_scale
@@ -356,7 +355,6 @@ def torch_recurrent_gated_delta_rule(
 ):
     initial_dtype = query.dtype
     if use_qk_l2norm_in_kernel:
-        head_dim = query.size(-1)
         inv_scale = query.size(-1)**-0.5
         query = F.rms_norm(query, (head_dim,), eps=1e-6) * inv_scale
         key = F.rms_norm(key, (head_dim,), eps=1e-6) * inv_scale
