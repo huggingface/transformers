04893a43ce413f1c1ba45f9e411d4725e69492aa
remi-or <remi.pierre_o@orange.fr>
2025-09-11 16:44:18 +0000

    Style

--- tests/generation/test_continuous_batching.py
+++ tests/generation/test_continuous_batching.py
@@ -165,7 +165,6 @@ class ContinuousBatchingTest(unittest.TestCase):
                         f"Exp:{repr(expected_output)}\nOut:{repr(cb_decoded_output)}",
                     )
 
-
     # Eager tests
     @require_torch_gpu
     @slow
@@ -178,7 +177,7 @@ class ContinuousBatchingTest(unittest.TestCase):
                 "req_1": " 3 bolts of blue fiber and 1.5 bolts of white fiber. The total number of bolts is 4.5. The total number of bolts is 4.5. The total",
                 "req_2": " $50,000. This is because the value of the house increased by 150%, which means that the value of the house increased by $50,000. This is because the value of the"
             }
-        }).get_expectation() # fmt: skip
+        }).get_expectation()  # fmt: skip
         self._test_continuous_batching_parity("meta-llama/Llama-3.1-8B", "eager_paged", expected_outputs)
 
     @require_torch_gpu
@@ -192,7 +191,7 @@ class ContinuousBatchingTest(unittest.TestCase):
                 "req_0": "\n\n**$12**\n\n**Here's how to solve it:**\n\n* **Eggs eaten:** 3\n* **Eggs left:** 16 - 3 = 13",
                 "req_1": " \n \n 2 + 1 = 3 bolts \n \n \n \n \n \n \n \n \n \n \n \n \n "
             }
-        }).get_expectation() # fmt: skip
+        }).get_expectation()  # fmt: skip
         self._test_continuous_batching_parity("google/gemma-2-2b-it", "eager_paged", expected_outputs)
 
     @require_torch_gpu
@@ -209,9 +208,8 @@ class ContinuousBatchingTest(unittest.TestCase):
                 "req_1": " 2.5 bolts. The question: \"What is the name of the puzzle that involves a robe taking 2 bolts of blue fiber and half that much white fiber?\" The answer: \"The",
                 "req_2": " 50%.\"\n\nWe need to parse: He buys a house for $80,000. He puts in $50,000 in repairs. This increased the value of the house by 150%."
             }
-        }).get_expectation() # fmt: skip
+        }).get_expectation()  # fmt: skip
         self._test_continuous_batching_parity("openai/gpt-oss-20b", "eager_paged", expected_outputs)
-
 
     # SDPA tests
     @require_torch_gpu
@@ -221,7 +219,7 @@ class ContinuousBatchingTest(unittest.TestCase):
             ("rocm", (9, 4)): {
                 "req_2": " $50,000. This is because the value of the house increased by 150%, which means that the value of the house increased by $50,000. This is because the value of the"
             }
-        }).get_expectation() # fmt: skip
+        }).get_expectation()  # fmt: skip
         self._test_continuous_batching_parity("meta-llama/Llama-3.1-8B", "sdpa_paged", expected_outputs)
 
     @require_torch_gpu
@@ -231,7 +229,7 @@ class ContinuousBatchingTest(unittest.TestCase):
             ("cuda", (9, 0)): {
                 "req_1": " \n\n**Answer:** 3 bolts\n\n**Solution:**\n\n* **White fiber:** The robe needs half as much white fiber as blue fiber, so it needs 2 bolts / 2 =",
             }
-        }).get_expectation() # fmt: skip
+        }).get_expectation()  # fmt: skip
         self._test_continuous_batching_parity("google/gemma-2-2b-it", "sdpa_paged", expected_outputs)
 
     @require_torch_gpu
@@ -242,7 +240,6 @@ class ContinuousBatchingTest(unittest.TestCase):
 
     # GPT-OSS is not compatible with SDPA because it has an attention sink. TODO: is this fixable?
 
-
     # Flash attention test
     @require_torch_gpu
     @require_kernels
@@ -252,7 +249,7 @@ class ContinuousBatchingTest(unittest.TestCase):
             ("cuda", (9, 0)): {
                 "req_1": " 3 bolts of blue fiber and 1.5 bolts of white fiber. The total number of bolts is 4.5 bolts. The total number of bolts is 4.5 bolts.",
             }
-        }).get_expectation() # fmt: skip
+        }).get_expectation()  # fmt: skip
         self._test_continuous_batching_parity(
             "meta-llama/Llama-3.1-8B", "paged_attention|kernels-community/flash-attn", expected_outputs
         )
@@ -265,22 +262,29 @@ class ContinuousBatchingTest(unittest.TestCase):
             ("cuda", (9, 0)): {
                 "req_1": " \n \n 2 + 1 = 3 bolts \n \n \n \n \n \n \n \n \n \n \n \n \n ",
             }
-        }).get_expectation() # fmt: skip
-        self._test_continuous_batching_parity("google/gemma-2-2b-it", "paged_attention|kernels-community/flash-attn", expected_outputs)
+        }).get_expectation()  # fmt: skip
+        self._test_continuous_batching_parity(
+            "google/gemma-2-2b-it", "paged_attention|kernels-community/flash-attn", expected_outputs
+        )
 
     @require_torch_gpu
     @require_kernels
     @slow
     def test_continuous_batching_parity_qwen_flash(self) -> None:
         expected_outputs = {}
-        self._test_continuous_batching_parity("Qwen/Qwen3-4B-Instruct-2507", "paged_attention|kernels-community/flash-attn", expected_outputs)
+        self._test_continuous_batching_parity(
+            "Qwen/Qwen3-4B-Instruct-2507", "paged_attention|kernels-community/flash-attn", expected_outputs
+        )
 
     @require_torch_gpu
     @require_kernels
     @slow
     def test_continuous_batching_parity_gpt_oss_flash(self) -> None:
         expected_outputs = {}
-        self._test_continuous_batching_parity("openai/gpt-oss-20b", "paged_attention|kernels-community/flash-attn", expected_outputs)
+        self._test_continuous_batching_parity(
+            "openai/gpt-oss-20b", "paged_attention|kernels-community/flash-attn", expected_outputs
+        )
 
-# FIXME: the gemma test seem broken, there is a message about cuda graphs and the sdpa and flash expecteations are 
+
+# FIXME: the gemma test seem broken, there is a message about cuda graphs and the sdpa and flash expecteations are
 # inverted on CUDA. On AMD they do fine.
