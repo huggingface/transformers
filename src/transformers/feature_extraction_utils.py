bda0c3d1abd63fe55a19144969c3ba1867a93013
Yuanyuan Chen <cyyever@outlook.com>
2025-09-11 13:57:01 +0800

    Improve typing
    Signed-off-by: Yuanyuan Chen <cyyever@outlook.com>

--- src/transformers/feature_extraction_utils.py
+++ src/transformers/feature_extraction_utils.py
@@ -47,9 +47,6 @@ from .utils.hub import cached_file
 if TYPE_CHECKING:
     from .feature_extraction_sequence_utils import SequenceFeatureExtractor
 
-    if is_torch_available():
-        import torch  # noqa
-
 
 logger = logging.get_logger(__name__)
 
@@ -112,7 +109,7 @@ class BatchFeature(UserDict):
         if tensor_type == TensorType.PYTORCH:
             if not is_torch_available():
                 raise ImportError("Unable to convert output to PyTorch tensors format, PyTorch is not installed.")
-            import torch  # noqa
+            import torch
 
             def as_tensor(value):
                 if isinstance(value, (list, tuple)) and len(value) > 0:
