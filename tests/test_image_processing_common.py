f61cec1e8864e9da23b7646556f4fd43e383d0ae
yonigozlan <yoni.gozlan@huggingface.co>
2025-09-12 18:46:01 +0000

    make center_crop fast equivalent to slow

--- tests/test_image_processing_common.py
+++ tests/test_image_processing_common.py
@@ -200,11 +200,6 @@ class ImageProcessingTestMixin:
         if self.image_processing_class is None or self.fast_image_processing_class is None:
             self.skipTest(reason="Skipping slow/fast equivalence test as one of the image processors is not defined")
 
-        if hasattr(self.image_processor_tester, "do_center_crop") and self.image_processor_tester.do_center_crop:
-            self.skipTest(
-                reason="Skipping as do_center_crop is True and center_crop functions are not equivalent for fast and slow processors"
-            )
-
         dummy_images = self.image_processor_tester.prepare_image_inputs(equal_resolution=False, torchify=True)
         image_processor_slow = self.image_processing_class(**self.image_processor_dict)
         image_processor_fast = self.fast_image_processing_class(**self.image_processor_dict)
