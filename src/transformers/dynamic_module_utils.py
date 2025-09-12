d6eb6da3b32545a4a6480c5b7ef4a5acdd438c7d
Xuehai Pan <XuehaiPan@pku.edu.cn>
2025-09-12 13:17:17 +0800

    Add notes for sanitization rule in docstring

--- src/transformers/dynamic_module_utils.py
+++ src/transformers/dynamic_module_utils.py
@@ -49,14 +49,22 @@ logger = logging.get_logger(__name__)  # pylint: disable=invalid-name
 
 
 def _sanitize_module_name(name: str) -> str:
-    """
+    r"""
     Tries to sanitize a module name so that it can be used as a Python module.
+
+    The following transformations are applied:
 
     1. Replace `.` in module names with `__dot__`.
     2. Replace `-` in module names with `__hyphen__`.
     3. If the module name starts with a digit, prepend it with `__`.
     4. Warn if the sanitized name is a Python reserved keyword or not a valid identifier.
+
+    If the input name is already a valid identifier, it is returned unchanged.
     """
+    # We not replacing `\W` characters with `_` to avoid collisions. Because `_` is a very common
+    # separator used in module names, replacing `\W` with `_` would create too many collisions.
+    # Once a module is imported, it is cached in `sys.modules` and the second import would return
+    # the first module, which might not be the expected behavior if name collisions happen.
     new_name = name.replace(".", "__dot__").replace("-", "__hyphen__")
     if new_name and new_name[0].isdigit():
         new_name = f"__{new_name}"
