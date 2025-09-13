2183cddb6cfe467ad59d8d7f4e73f43f4013f60e
S1ro1 <matej.sirovatka@gmail.com>
2025-09-13 14:27:14 +0000

    smth

--- src/transformers/models/qwen3_moe/modeling_qwen3_moe.py
+++ src/transformers/models/qwen3_moe/modeling_qwen3_moe.py
@@ -231,11 +231,10 @@ class Qwen3MoeSparseMoeBlock(nn.Module):
         self.top_k = config.num_experts_per_tok
         self.norm_topk_prob = config.norm_topk_prob
 
-        # gating
-        self.gate = nn.Linear(config.hidden_size, config.num_experts, bias=False)
         import os
 
         if os.environ.get("USE_NEW_MOE", "false") == "false":
+            self.gate = nn.Linear(config.hidden_size, config.num_experts, bias=False)
             self.experts = nn.ModuleList(
                 [Qwen3MoeMLP(config, intermediate_size=config.moe_intermediate_size) for _ in range(self.num_experts)]
             )
@@ -456,6 +455,7 @@ class Qwen3MoeDecoderLayer(GradientCheckpointingLayer):
         ):
             self.mlp = Qwen3MoeSparseMoeBlock(config)
         else:
+            self.gate = nn.Linear(config.hidden_size, config.num_experts, bias=False)
             self.mlp = Qwen3MoeMLP(config, intermediate_size=config.intermediate_size)
 
         self.input_layernorm = Qwen3MoeRMSNorm(config.hidden_size, eps=config.rms_norm_eps)
@@ -520,7 +520,7 @@ class Qwen3MoeDecoderLayer(GradientCheckpointingLayer):
             b, s, h = hidden_states.shape
             hidden_states = hidden_states.view(-1, h)
 
-            router_logits = self.mlp.gate(hidden_states)
+            router_logits = self.gate(hidden_states)
             routing_weights = F.softmax(router_logits, dim=1, dtype=torch.float)
             routing_weights, selected_experts = torch.topk(routing_weights, self.mlp.top_k, dim=-1)
 
