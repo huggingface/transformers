9cb5d4fa1827a8fc7c44e28c1524eba36d7d7f9e
yaswanth <yaswanthgali8@gmail.com>
2025-09-15 18:11:31 +0530

    revrt one change

--- src/transformers/models/llama4/modeling_llama4.py
+++ src/transformers/models/llama4/modeling_llama4.py
@@ -1273,6 +1273,11 @@ class Llama4ForConditionalGeneration(Llama4PreTrainedModel, GenerationMixin):
             output_hidden_states if output_hidden_states is not None else self.config.output_hidden_states
         )
         return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+        vision_feature_layer = (
+            vision_feature_layer
+            if vision_feature_layer is not None
+            else self.config.vision_config.vision_feature_layer
+        )
         vision_feature_select_strategy = (
             vision_feature_select_strategy
             if vision_feature_select_strategy is not None
