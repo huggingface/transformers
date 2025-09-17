8b8b35398960a094d43687522a8638f0258beb85
Raushan Turganbay <raushan@huggingface.co>
2025-09-17 18:40:25 +0200

    Consistent naming for images kwargs (#40834)
    * use consistent naming for padding
    
    * no validation on pad size
    
    * add warnings
    
    * fix
    
    * fox copies
    
    * another fix
    
    * fix some tests
    
    * fix more tests
    
    * fix lasts tests
    
    * fix copies
    
    * better docstring
    
    * delete print

--- src/transformers/models/smolvlm/video_processing_smolvlm.py
+++ src/transformers/models/smolvlm/video_processing_smolvlm.py
@@ -98,7 +98,8 @@ def get_resize_output_image_size(
 
 
 class SmolVLMVideoProcessorInitKwargs(VideosKwargs):
-    max_image_size: dict[str, int] = None
+    max_image_size: Optional[dict[str, int]]
+    do_pad: Optional[bool]
 
 
 class SmolVLMVideoProcessor(BaseVideoProcessor):
