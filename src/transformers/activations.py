48c53ac9804cd170ce70c578010822aae7d27a4e
MekkCyber <mekk.cyber@gmail.com>
2025-09-17 09:11:26 +0000

    rm print

--- src/transformers/activations.py
+++ src/transformers/activations.py
@@ -47,7 +47,6 @@ class NewGELUActivation(nn.Module):
     """
 
     def forward(self, input: Tensor) -> Tensor:
-        print("NewGELUActivation")
         return 0.5 * input * (1.0 + torch.tanh(math.sqrt(2.0 / math.pi) * (input + 0.044715 * torch.pow(input, 3.0))))
 
 
