beffdc1f8435d04c781e7a2ab367c903cfdc57ad
Lasha <26011196+lashahub@users.noreply.github.com>
2025-09-17 21:57:17 -0400

    Add audio_token_id to config

--- src/transformers/models/audioflamingo3/configuration_audioflamingo3.py
+++ src/transformers/models/audioflamingo3/configuration_audioflamingo3.py
@@ -47,23 +47,23 @@ class AudioFlamingo3EncoderConfig(PretrainedConfig):
         encoder_ffn_dim (`int`, *optional*, defaults to 5120):
             Dimensionality of the "intermediate" (often named feed-forward) layer in encoder.
         encoder_layerdrop (`float`, *optional*, defaults to 0.0):
-            The LayerDrop probability for the encoder. See the [LayerDrop paper](see https://huggingface.co/papers/1909.11556)
+            The LayerDrop probability for the encoder. See the [LayerDrop paper](https://huggingface.co/papers/1909.11556)
             for more details.
+        activation_function (`str`, *optional*, defaults to `"gelu"`):
+            The non-linear activation function (function or string) in the encoder and pooler. If string, `"gelu"`,
+            `"relu"`, `"silu"` and `"gelu_new"` are supported.
         d_model (`int`, *optional*, defaults to 1280):
             Dimensionality of the layers.
         dropout (`float`, *optional*, defaults to 0.0):
             The dropout probability for all fully connected layers in the embeddings, encoder, and pooler.
         attention_dropout (`float`, *optional*, defaults to 0.0):
             The dropout ratio for the attention probabilities.
-        activation_function (`str`, *optional*, defaults to `"gelu"`):
-            The non-linear activation function (function or string) in the encoder and pooler. If string, `"gelu"`,
-            `"relu"`, `"silu"` and `"gelu_new"` are supported.
         activation_dropout (`float`, *optional*, defaults to 0.0):
             The dropout ratio for activations inside the fully connected layer.
+        init_std (`float`, *optional*, defaults to 0.02):
+            The standard deviation of the truncated_normal_initializer for initializing all weight matrices.
         scale_embedding (`bool`, *optional*, defaults to `False`):
             Scale embeddings by dividing by sqrt(d_model).
-        initializer_range (`float`, *optional*, defaults to 0.02):
-            The standard deviation of the truncated_normal_initializer for initializing all weight matrices.
         max_source_positions (`int`, *optional*, defaults to 1500):
             The maximum sequence length of log-mel filter-bank features that this model might ever be used with.
         avg_pool_kernel_size (`int`, *optional*, defaults to 2):
@@ -95,13 +95,13 @@ class AudioFlamingo3EncoderConfig(PretrainedConfig):
         encoder_attention_heads: int = 20,
         encoder_ffn_dim: int = 5120,
         encoder_layerdrop: float = 0.0,
+        activation_function: str = "gelu",
         d_model: int = 1280,
         dropout: float = 0.0,
         attention_dropout: float = 0.0,
-        activation_function: str = "gelu",
         activation_dropout: float = 0.0,
+        init_std: float = 0.02,
         scale_embedding: bool = False,
-        initializer_range: float = 0.02,
         max_source_positions: int = 1500,
         avg_pool_kernel_size: int = 2,
         avg_pool_stride: int = 2,
@@ -116,11 +116,11 @@ class AudioFlamingo3EncoderConfig(PretrainedConfig):
         self.encoder_ffn_dim = encoder_ffn_dim
         self.dropout = dropout
         self.attention_dropout = attention_dropout
-        self.activation_function = activation_function
         self.activation_dropout = activation_dropout
+        self.activation_function = activation_function
+        self.init_std = init_std
         self.encoder_layerdrop = encoder_layerdrop
         self.num_hidden_layers = encoder_layers
-        self.initializer_range = initializer_range
         self.scale_embedding = scale_embedding  # scale factor will be sqrt(d_model) if True
         self.max_source_positions = max_source_positions
         self.avg_pool_kernel_size = avg_pool_kernel_size
@@ -137,12 +137,12 @@ class AudioFlamingo3Config(PretrainedConfig):
     documentation from [`PretrainedConfig`] for more information.
 
     Args:
-        audio_config (`Union[AutoConfig, dict]`,  *optional*, defaults to `CLIPVisionConfig`):
+        audio_config (`Union[AudioFlamingo3EncoderConfig, dict]`, *optional*, defaults to `AudioFlamingo3EncoderConfig`):
             The config object or dictionary of the audio backbone.
-        text_config (`Union[AutoConfig, dict]`, *optional*, defaults to `LlamaConfig`):
+        text_config (`Union[AutoConfig, dict]`, *optional*, defaults to `Qwen2Config`):
             The config object or dictionary of the text backbone.
-        audio_token_index (`int`, *optional*, defaults to 151646):
-            The image token index to encode the image prompt.
+        audio_token_id (`int`, *optional*, defaults to 151669):
+            The audio token index to encode the audio prompt.
 
     Example:
 
@@ -167,16 +167,19 @@ class AudioFlamingo3Config(PretrainedConfig):
 
     model_type = "audioflamingo3"
     sub_configs = {
-        "text_config": AutoConfig,
         "audio_config": AudioFlamingo3EncoderConfig,
+        "text_config": AutoConfig,
     }
 
     def __init__(
         self,
-        text_config: Optional[Union[AutoConfig, dict[str, Any]]] = None,
         audio_config: Optional[Union[AudioFlamingo3EncoderConfig, dict[str, Any]]] = None,
+        text_config: Optional[Union[AutoConfig, dict[str, Any]]] = None,
+        audio_token_id: int = 151669,
         **kwargs,
     ):
+        self.audio_token_id = audio_token_id
+
         if isinstance(audio_config, dict):
             audio_config["model_type"] = audio_config.get("model_type", "audioflamingo3_encoder")
             audio_config = CONFIG_MAPPING[audio_config["model_type"]](**audio_config)
