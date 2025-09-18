d010fbc39956455adc2cee1409ea899471304d50
Wauplin <lucainp@gmail.com>
2025-09-18 16:47:53 +0200

    no proxies

--- src/transformers/utils/hub.py
+++ src/transformers/utils/hub.py
@@ -680,7 +680,6 @@ def has_file(
             hf_hub_url(path_or_repo, filename=filename, revision=revision, repo_type=repo_type),
             headers=build_hf_headers(token=token, user_agent=http_user_agent()),
             follow_redirects=False,
-            proxies=proxies,
             timeout=10,
         )
     except httpx.ProxyError:
