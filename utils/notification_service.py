dfe9a45d3e9b1e937634f922cf868155eaa9c864
ydshieh <ydshieh@users.noreply.github.com>
2025-09-18 17:00:44 +0200

    trigger and check killed jobs

--- utils/notification_service.py
+++ utils/notification_service.py
@@ -244,7 +244,7 @@ class Message:
                 "type": "plain_text",
                 "text": (
                     f"There were {self.n_failures} failures, out of {self.n_tests} tests.\n"
-                    f"There were {self.n_jobs_errored_out} jobs errored out without producing expected output files.\n"
+                    f"🚨 There were {self.n_jobs_errored_out} jobs errored out (not producing test output files). 🚨\n"
                     f"The suite ran in {self.time}."
                 ),
                 "emoji": True,
