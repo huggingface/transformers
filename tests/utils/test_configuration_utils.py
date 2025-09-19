e7c94d58764ad63d3b8c192443b866d1b07c18f8
Wauplin <lucainp@gmail.com>
2025-09-19 16:07:49 +0200

    fix others

--- tests/utils/test_configuration_utils.py
+++ tests/utils/test_configuration_utils.py
@@ -220,7 +220,9 @@ class ConfigTestUtils(unittest.TestCase):
         response_mock = mock.Mock()
         response_mock.status_code = 500
         response_mock.headers = {}
-        response_mock.raise_for_status.side_effect = httpx.HTTPError("failed")
+        response_mock.raise_for_status.side_effect = httpx.HTTPStatusError(
+            "failed", request=mock.Mock(), response=mock.Mock()
+        )
         response_mock.json.return_value = {}
 
         # Download this model to make sure it's in the cache.
--- tests/utils/test_modeling_utils.py
+++ tests/utils/test_modeling_utils.py
@@ -1145,7 +1145,9 @@ class ModelUtilsTest(TestCasePlus):
         response_mock = mock.Mock()
         response_mock.status_code = 500
         response_mock.headers = {}
-        response_mock.raise_for_status.side_effect = httpx.HTTPError("failed")
+        response_mock.raise_for_status.side_effect = httpx.HTTPStatusError(
+            "failed", request=mock.Mock(), response=mock.Mock()
+        )
         response_mock.json.return_value = {}
 
         # Download this model to make sure it's in the cache.
