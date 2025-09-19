d5e56bbd2cceb0512ab91e0647fa7c54a68e90a9
itazap <ita.zaporozhets@huggingface.co>
2025-09-19 16:22:17 +0200

    move update post processor and add bos eos properties

--- src/transformers/tokenization_utils_fast.py
+++ src/transformers/tokenization_utils_fast.py
@@ -24,6 +24,7 @@ from collections.abc import Iterable
 from typing import Any, Optional, Union
 
 import tokenizers.pre_tokenizers as pre_tokenizers_fast
+from tokenizers import processors
 from tokenizers import Encoding as EncodingFast
 from tokenizers import Tokenizer as TokenizerFast
 from tokenizers.decoders import Decoder as DecoderFast
@@ -236,6 +237,57 @@ class PreTrainedTokenizerFast(PreTrainedTokenizerBase):
             return False
         else:
             return True
+
+    def update_post_processor(self):
+        """
+        Updates the underlying post processor with the current `bos_token` and `eos_token`.
+        """
+        bos = self.bos_token
+        bos_token_id = self.bos_token_id
+        if bos is None and self.add_bos_token:
+            raise ValueError("add_bos_token = True but bos_token = None")
+
+        eos = self.eos_token
+        eos_token_id = self.eos_token_id
+        if eos is None and self.add_eos_token:
+            raise ValueError("add_eos_token = True but eos_token = None")
+
+        single = f"{(bos + ':0 ') if self.add_bos_token else ''}$A:0{(' ' + eos + ':0') if self.add_eos_token else ''}"
+        pair = f"{single}{(' ' + bos + ':1') if self.add_bos_token else ''} $B:1{(' ' + eos + ':1') if self.add_eos_token else ''}"
+
+        special_tokens = []
+        if self.add_bos_token:
+            special_tokens.append((bos, bos_token_id))
+        if self.add_eos_token:
+            special_tokens.append((eos, eos_token_id))
+
+        new_processor = processors.TemplateProcessing(
+            single=single, pair=pair, special_tokens=special_tokens
+        )
+        if isinstance(self._tokenizer.post_processor, processors.Sequence):
+            self._tokenizer.post_processor += [new_processor]
+        elif self._tokenizer.post_processor is not None:
+            self._tokenizer.post_processor = processors.Sequence([self._tokenizer.post_processor, new_processor])
+        else:
+            self._tokenizer.post_processor = new_processor
+
+    @property
+    def add_eos_token(self):
+        return self._add_eos_token
+
+    @property
+    def add_bos_token(self):
+        return self._add_bos_token
+
+    @add_eos_token.setter
+    def add_eos_token(self, value):
+        self._add_eos_token = value
+        self.update_post_processor()
+
+    @add_bos_token.setter
+    def add_bos_token(self, value):
+        self._add_bos_token = value
+        self.update_post_processor()
 
     @property
     def vocab_size(self) -> int:
