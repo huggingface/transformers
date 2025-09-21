8caac2618e4d6b692a99e18a80963a1ec8a2f774
BakerBunker <17872844+BakerBunker@users.noreply.github.com>
2025-09-21 16:46:27 -0500

    Adding support for Qwen3Omni (#41025)
    * Add Qwen3Omni
    
    * make fix-copies, import properly
    
    * nit
    
    * fix wrong setup. Why was audio_token_id renamed ?
    
    * upds
    
    * more processing fixes
    
    * yup
    
    * fix more generation tests
    
    * down to 1?
    
    * fix import issue
    
    * style, update check repo
    
    * up
    
    * fix quality at my best
    
    * final quality?
    
    * fix doc building
    
    * FINAL COMMIT: SKIP IMPORTANT BUT FAILING TESTS FOR MERGE
    
    * SKIP THE TEMPLATE ONE
    
    ---------
    
    Co-authored-by: lvyuanjun.lyj <lvyuanjun.lyj@alibaba-inc.com>
    Co-authored-by: Arthur <arthur.zucker@gmail.com>

--- src/transformers/models/__init__.py
+++ src/transformers/models/__init__.py
@@ -283,6 +283,7 @@ if TYPE_CHECKING:
     from .qwen3 import *
     from .qwen3_moe import *
     from .qwen3_next import *
+    from .qwen3_omni_moe import *
     from .qwen3_vl import *
     from .qwen3_vl_moe import *
     from .rag import *
