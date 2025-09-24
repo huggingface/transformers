234f233392461b5040163290c95d5f2dd32b0c31
raushan <raushan@huggingface.co>
2025-09-24 18:47:19 +0200

    fix copies

--- src/transformers/models/t5gemma/modular_t5gemma.py
+++ src/transformers/models/t5gemma/modular_t5gemma.py
@@ -31,6 +31,7 @@ from ...modeling_outputs import (
     SequenceClassifierOutput,
     TokenClassifierOutput,
 )
+from ...modeling_rope_utils import RopeParameters
 from ...modeling_utils import ALL_ATTENTION_FUNCTIONS, PreTrainedModel
 from ...processing_utils import Unpack
 from ...utils import (
