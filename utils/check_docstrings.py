bd0874cc9e8e830f33dd3c391b5b481695333822
ryan.u <ryan.u@kakaocorp.com>
2025-09-24 16:47:01 +0900

    make fix-up

--- utils/check_docstrings.py
+++ utils/check_docstrings.py
@@ -283,6 +283,7 @@ OBJECTS_TO_IGNORE = {
     "MegatronBertConfig",
     "MegatronBertForPreTraining",
     "MegatronBertModel",
+    "MiniCPM3Config",
     "MLCDVisionConfig",
     "MobileBertConfig",
     "MobileBertModel",
