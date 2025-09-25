671764ac7be984de6fed64ed333613b281be207f
Liu, Kaixuan <kaixuan.liu@intel.com>
2025-09-25 04:40:42 +0000

    use get_kernel
    Signed-off-by: Liu, Kaixuan <kaixuan.liu@intel.com>

--- src/transformers/integrations/hub_kernels.py
+++ src/transformers/integrations/hub_kernels.py
@@ -25,7 +25,6 @@ try:
         LayerRepository,
         Mode,
         get_kernel,
-        load_kernel,
         register_kernel_mapping,
         replace_kernel_forward_from_hub,
         use_kernel_forward_from_hub,
