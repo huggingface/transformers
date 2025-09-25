ceaa289ed5c81f18e8094d01a2fac3f9b1f2fb80
vasqu <antonprogamer@gmail.com>
2025-09-25 13:09:49 +0200

    fix

--- tests/models/conditional_detr/test_modeling_conditional_detr.py
+++ tests/models/conditional_detr/test_modeling_conditional_detr.py
@@ -430,8 +430,7 @@ class ConditionalDetrModelTest(ModelTesterMixin, PipelineTesterMixin, unittest.T
             arg_names = [*signature.parameters.keys()]
 
             if model.config.is_encoder_decoder:
-                expected_arg_names = ["pixel_values", "pixel_mask"]
-                expected_arg_names.extend(["encoder_outputs"])
+                expected_arg_names = ["pixel_values", "pixel_mask", "decoder_attention_mask"]
                 self.assertListEqual(arg_names[: len(expected_arg_names)], expected_arg_names)
             else:
                 expected_arg_names = ["pixel_values", "pixel_mask"]
