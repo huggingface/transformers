7a0893a22822837a42217b013513501071d5bf80
ydshieh <ydshieh@users.noreply.github.com>
2025-09-26 14:56:20 +0200

    does this works?

--- upload_hub_cache.py
+++ upload_hub_cache.py
@@ -8,4 +8,5 @@ api.upload_file(
     path_in_repo="huggingface-cache.tar.gz",
     repo_id="hf-transformers-bot/ci_artifacts_temp",
     token=os.environ["TRANSFORMERS_CI_RESULTS_UPLOAD_TOKEN"],
+    repo_type="dataset",
 )
