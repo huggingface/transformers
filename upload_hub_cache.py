0bff89b08c02094fd7db7f7b70965d8dc13bf61a
ydshieh <ydshieh@users.noreply.github.com>
2025-09-26 15:54:42 +0200

    does this works?

--- upload_hub_cache.py
+++ upload_hub_cache.py
@@ -6,7 +6,7 @@ api = HfApi()
 api.upload_file(
     path_or_fileobj="huggingface-cache.tar.gz",
     path_in_repo="huggingface-cache.tar.gz",
-    repo_id="hf-transformers-bot/ci_artifacts_temp",
+    repo_id="hf-internal-testing/hf_hub_cache",
     token=os.environ["TRANSFORMERS_CI_RESULTS_UPLOAD_TOKEN"],
     repo_type="dataset",
 )
