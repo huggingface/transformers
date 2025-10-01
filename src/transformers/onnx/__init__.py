9d1d331df07652a58dc40a1c03bbf6f20898c675
Yuanyuan Chen <cyyever@outlook.com>
2025-10-01 20:17:04 +0800

    [V5] Remove deprecated transformers.onnx (#41214)
    * Remove deprecated transformers.onnx
    
    Signed-off-by: Yuanyuan Chen <cyyever@outlook.com>
    
    * Remove onnx docs
    
    Signed-off-by: Yuanyuan Chen <cyyever@outlook.com>
    
    ---------
    
    Signed-off-by: Yuanyuan Chen <cyyever@outlook.com>
    Co-authored-by: Yih-Dar <2521628+ydshieh@users.noreply.github.com>

--- src/transformers/onnx/__init__.py
+++ src/transformers/onnx/__init__.py
@@ -25,8 +25,6 @@ _import_structure = {
         "OnnxSeq2SeqConfigWithPast",
         "PatchingSpec",
     ],
-    "convert": ["export", "validate_model_outputs"],
-    "features": ["FeaturesManager"],
     "utils": ["ParameterFormat", "compute_serialized_parameters_size"],
 }
 
@@ -39,8 +37,6 @@ if TYPE_CHECKING:
         OnnxSeq2SeqConfigWithPast,
         PatchingSpec,
     )
-    from .convert import export, validate_model_outputs
-    from .features import FeaturesManager
     from .utils import ParameterFormat, compute_serialized_parameters_size
 
 else:
