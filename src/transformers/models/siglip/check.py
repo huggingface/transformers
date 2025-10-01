dcddb970176382c0fcf4521b0c0e6fc15894dfe0
Pepijn <pepijn@huggingface.co>
2025-10-01 11:15:41 +0200

    allow '4.53.3'

--- src/transformers/models/siglip/check.py
+++ src/transformers/models/siglip/check.py
@@ -1,4 +1,4 @@
 import transformers
 
 def check_whether_transformers_replace_is_installed_correctly():
-    return transformers.__version__ == "4.53.2"
+    return transformers.__version__ == "4.53.2" or transformers.__version__ == "4.53.3"
