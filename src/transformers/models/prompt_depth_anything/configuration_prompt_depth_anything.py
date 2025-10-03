09cb9db6c294b284e3e1bbe7448a183acb5f3a54
raushan <raushan@huggingface.co>
2025-10-03 11:09:41 +0200

    fix copies

--- src/transformers/models/prompt_depth_anything/configuration_prompt_depth_anything.py
+++ src/transformers/models/prompt_depth_anything/configuration_prompt_depth_anything.py
@@ -17,7 +17,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
 from ...configuration_utils import PretrainedConfig
 from ...utils import logging
 from ...utils.backbone_utils import verify_backbone_config_arguments
