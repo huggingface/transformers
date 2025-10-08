9c815871526477e59dab3eaeff0aeca632e519d4
Marc Sun <57196510+SunMarc@users.noreply.github.com>
2025-10-08 12:04:26 +0200

    [v5] Bump min version of bitsandbytes to 0.46.1  (#41283)
    * bump bitsandbytes to 0.46.1
    
    * huge cleanup
    
    * style
    
    * fix
    
    * req
    
    * fix
    
    * importerror
    
    * fix

--- src/transformers/trainer.py
+++ src/transformers/trainer.py
@@ -1454,70 +1454,57 @@ class Trainer:
             OptimizerNames.RMSPROP_8BIT,
             OptimizerNames.RMSPROP_32BIT,
         ]:
-            try:
-                from bitsandbytes.optim import AdamW, Lion, RMSprop
-
-                is_paged = False
-                optim_bits = 32
-                optimizer_cls = None
-                additional_optim_kwargs = adam_kwargs
-                if "paged" in args.optim:
-                    is_paged = True
-                if "8bit" in args.optim:
-                    optim_bits = 8
-                if "adam" in args.optim:
-                    optimizer_cls = AdamW
-                elif "lion" in args.optim:
-                    optimizer_cls = Lion
-                    additional_optim_kwargs = {"betas": (args.adam_beta1, args.adam_beta2)}
-                elif "rmsprop" in args.optim:
-                    optimizer_cls = RMSprop
-                    # Above we pass all `adam_kwargs` to the optimizer, here
-                    # we only pass `optim_args` which can be passed by the user.
-                    additional_optim_kwargs = optim_args
-                elif "ademamix" in args.optim:
-                    if is_bitsandbytes_available() and version.parse(
-                        importlib.metadata.version("bitsandbytes")
-                    ) < version.parse("0.44.0"):
-                        raise ValueError(
-                            "The AdEMAMix optimizer is not supported by your current version of `bitsandbytes`. "
-                            "Please install `bitsandbytes` >= 0.44.0."
-                        )
-
-                    from bitsandbytes.optim import AdEMAMix
-
-                    optimizer_cls = AdEMAMix
-                    additional_optim_kwargs = {
-                        "betas": (
-                            float(optim_args.get("beta1", args.adam_beta1)),
-                            float(optim_args.get("beta2", args.adam_beta2)),
-                            float(optim_args.get("beta3", 0.9999)),
-                        ),
-                        "alpha": float(optim_args.get("alpha", 5.0)),
-                        "eps": float(optim_args.get("eps", args.adam_epsilon)),
-                    }
-
-                    if "t_alpha" in optim_args:
-                        additional_optim_kwargs["t_alpha"] = int(optim_args["t_alpha"])
-
-                    if "t_beta3" in optim_args:
-                        additional_optim_kwargs["t_beta3"] = int(optim_args["t_beta3"])
-
-                bnb_kwargs = {"optim_bits": optim_bits}
-                if "rmsprop" not in args.optim:
-                    bnb_kwargs["is_paged"] = is_paged
-
-                optimizer_kwargs.update(additional_optim_kwargs)
-                optimizer_kwargs.update(bnb_kwargs)
-            except ImportError:
-                raise ValueError("Trainer tried to instantiate bnb optimizer but `bitsandbytes` is not installed!")
-            if is_bitsandbytes_available() and version.parse(
-                importlib.metadata.version("bitsandbytes")
-            ) < version.parse("0.41.1"):
-                logger.warning(
-                    "You are using 8-bit optimizers with a version of `bitsandbytes` < 0.41.1. "
-                    "It is recommended to update your version as a major bug has been fixed in 8-bit optimizers."
+            if not is_bitsandbytes_available():
+                raise ImportError(
+                    "You need to install `bitsandbytes` in order to use bitsandbytes optimizers: `pip install -U bitsandbytes`"
                 )
+
+            from bitsandbytes.optim import AdamW, Lion, RMSprop
+
+            is_paged = False
+            optim_bits = 32
+            optimizer_cls = None
+            additional_optim_kwargs = adam_kwargs
+            if "paged" in args.optim:
+                is_paged = True
+            if "8bit" in args.optim:
+                optim_bits = 8
+            if "adam" in args.optim:
+                optimizer_cls = AdamW
+            elif "lion" in args.optim:
+                optimizer_cls = Lion
+                additional_optim_kwargs = {"betas": (args.adam_beta1, args.adam_beta2)}
+            elif "rmsprop" in args.optim:
+                optimizer_cls = RMSprop
+                # Above we pass all `adam_kwargs` to the optimizer, here
+                # we only pass `optim_args` which can be passed by the user.
+                additional_optim_kwargs = optim_args
+            elif "ademamix" in args.optim:
+                from bitsandbytes.optim import AdEMAMix
+
+                optimizer_cls = AdEMAMix
+                additional_optim_kwargs = {
+                    "betas": (
+                        float(optim_args.get("beta1", args.adam_beta1)),
+                        float(optim_args.get("beta2", args.adam_beta2)),
+                        float(optim_args.get("beta3", 0.9999)),
+                    ),
+                    "alpha": float(optim_args.get("alpha", 5.0)),
+                    "eps": float(optim_args.get("eps", args.adam_epsilon)),
+                }
+
+                if "t_alpha" in optim_args:
+                    additional_optim_kwargs["t_alpha"] = int(optim_args["t_alpha"])
+
+                if "t_beta3" in optim_args:
+                    additional_optim_kwargs["t_beta3"] = int(optim_args["t_beta3"])
+
+            bnb_kwargs = {"optim_bits": optim_bits}
+            if "rmsprop" not in args.optim:
+                bnb_kwargs["is_paged"] = is_paged
+
+            optimizer_kwargs.update(additional_optim_kwargs)
+            optimizer_kwargs.update(bnb_kwargs)
         elif args.optim == OptimizerNames.ADAMW_ANYPRECISION:
             try:
                 from torchdistx.optimizers import AnyPrecisionAdamW
