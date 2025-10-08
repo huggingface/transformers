af8f34ed3bee5254f30cab6e0eee2e834dff1aee
Kamila Luchay <kamilaluczaj24@gmail.com>
2025-10-08 17:17:00 +0200

    Fixed documentation

--- src/transformers/models/fast_vlm/modular_fast_vlm.py
+++ src/transformers/models/fast_vlm/modular_fast_vlm.py
@@ -221,6 +221,17 @@ class FastVlmModel(LlavaModel):
         image_features = list(image_features)
         return image_features
     
+    def forward(self, **super_kwargs):
+        r"""
+        vision_feature_select_strategy (`str`, *optional*):
+            The feature selection strategy used to select the vision feature from the vision backbone. Can only be `"full"`.
+
+        vision_feature_layer (`Union[int, list[int], NoneType]`, *optional*):
+            The index of the layer to select the vision feature. If multiple indices are provided, the vision feature of the 
+            corresponding indices will be concatenated to form the vision features. Must be negative.
+        """
+        super().forward(**super_kwargs)
+    
 class FastVlmForConditionalGeneration(LlavaForConditionalGeneration):
     def forward(self, **super_kwargs):
         r"""
@@ -228,6 +239,13 @@ class FastVlmForConditionalGeneration(LlavaForConditionalGeneration):
             Labels for computing the masked language modeling loss. Indices should either be in `[0, ...,
             config.vocab_size]` or -100 (see `input_ids` docstring). Tokens with indices set to `-100` are ignored
             (masked), the loss is only computed for the tokens with labels in `[0, ..., config.vocab_size]`.
+
+        vision_feature_select_strategy (`str`, *optional*):
+            The feature selection strategy used to select the vision feature from the vision backbone. Can only be `"full"`.
+
+        vision_feature_layer (`Union[int, list[int], NoneType]`, *optional*):
+            The index of the layer to select the vision feature. If multiple indices are provided, the vision feature of the 
+            corresponding indices will be concatenated to form the vision features. Must be negative.
 
         Example:
 
