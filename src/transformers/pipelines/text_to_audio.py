7b2a0b6f495948d099e70289111592ca1b1907d8
Cyril Vallez <cyril.vallez@gmail.com>
2025-10-08 10:48:42 +0200

    order

--- src/transformers/pipelines/text_to_audio.py
+++ src/transformers/pipelines/text_to_audio.py
@@ -80,11 +80,11 @@ class TextToAudioPipeline(Pipeline):
     See the list of available models on [huggingface.co/models](https://huggingface.co/models?filter=text-to-speech).
     """
 
+    _pipeline_calls_generate = True
     _load_processor = False
     _load_image_processor = False
     _load_feature_extractor = False
     _load_tokenizer = True
-    _pipeline_calls_generate = True
 
     # Make sure the docstring is updated when the default generation config is changed
     _default_generation_config = GenerationConfig(
