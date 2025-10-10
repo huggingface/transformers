e4f7e7133806dc8acb42d1094e8893960a8e641c
ydshieh <ydshieh@users.noreply.github.com>
2025-10-10 15:53:25 +0200

    hate pickle

--- tests/models/llama/test_tokenization_llama.py
+++ tests/models/llama/test_tokenization_llama.py
@@ -299,10 +299,6 @@ class LlamaTokenizationTest(TokenizerTesterMixin, unittest.TestCase):
         pickle.loads(pickled_tokenizer)
 
     @unittest.skip(reason="worker 'gw4' crashed on CI, passing locally.")
-    def test_pickle_subword_regularization_tokenizer(self):
-        pass
-
-    @unittest.skip(reason="worker 'gw4' crashed on CI, passing locally.")
     def test_subword_regularization_tokenizer(self):
         pass
 
