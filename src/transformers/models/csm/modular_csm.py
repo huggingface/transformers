6ab98c0cb44a0c92f4957d86f0521f503c0d884c
raushan <raushan@huggingface.co>
2025-10-10 15:10:31 +0200

    style

--- src/transformers/models/csm/modular_csm.py
+++ src/transformers/models/csm/modular_csm.py
@@ -125,7 +125,6 @@ class CsmPreTrainedModel(PreTrainedModel):
     config: CsmConfig
     base_model_prefix = "model"
     input_modalities = ["audio", "text"]
-    input_modalities = "audio"
     supports_gradient_checkpointing = True
     _no_split_modules = ["CsmDecoderLayer"]
     _skip_keys_device_placement = ["past_key_values"]
