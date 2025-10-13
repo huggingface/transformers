4f3c38dc86419e78285c2fe26dee58123c569c11
romit <romit@ibm.com>
2025-10-13 08:56:21 +0000

    Added kernels from kernel hub for Bamba model

--- src/transformers/utils/import_utils.py
+++ src/transformers/utils/import_utils.py
@@ -1166,6 +1166,10 @@ def is_matplotlib_available() -> bool:
 def is_mistral_common_available() -> bool:
     return _is_package_available("mistral_common")
 
+@lru_cache
+def is_einops_available() -> bool:
+    return _is_package_available("einops")
+
 
 def check_torch_load_is_safe() -> None:
     if not is_torch_greater_or_equal("2.6"):
