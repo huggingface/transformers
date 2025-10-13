98051951a5fc25feb542c236fab1adf3676c49bc
Wauplin <lucainp@gmail.com>
2025-10-13 15:16:53 +0200

    fix quality

--- tests/cli/test_serving.py
+++ tests/cli/test_serving.py
@@ -11,16 +11,13 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import asyncio
 import os
 import time
 import unittest
 from threading import Thread
 from unittest.mock import patch
 
-import aiohttp.client_exceptions
 import httpx
-import pytest
 import uvicorn
 from huggingface_hub import ChatCompletionStreamOutput, InferenceClient
 from parameterized import parameterized
