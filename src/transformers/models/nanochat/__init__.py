5977a78509902500a612b0b0b57ebe3dd281d08b
Ben Burtenshaw <ben.burtenshaw@gmail.com>
2025-10-16 11:31:48 +0000

    protect imports with lazy

--- src/transformers/models/nanochat/__init__.py
+++ src/transformers/models/nanochat/__init__.py
@@ -1,9 +1,14 @@
-from .configuration_nanochat import NanoChatConfig
-from .modeling_nanochat import NanoChatForCausalLM, NanoChatModel
+from typing import TYPE_CHECKING
+
+from ...utils import _LazyModule
+from ...utils.import_utils import define_import_structure
 
 
-__all__ = [
-    "NanoChatConfig",
-    "NanoChatModel",
-    "NanoChatForCausalLM",
-]
+if TYPE_CHECKING:
+    from .configuration_nanochat import *
+    from .modeling_nanochat import *
+else:
+    import sys
+
+    _file = globals()["__file__"]
+    sys.modules[__name__] = _LazyModule(__name__, _file, define_import_structure(_file), module_spec=__spec__)
