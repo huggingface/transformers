806087073cb6618c0fca2c51f2e4ecbb64ac1824
Justin Chu <justinchuby@users.noreply.github.com>
2025-10-16 11:15:24 -0700

    Fix check
    Signed-off-by: Justin Chu <justinchuby@users.noreply.github.com>

--- src/transformers/integrations/executorch.py
+++ src/transformers/integrations/executorch.py
@@ -1233,10 +1233,7 @@ def sdpa_attention_forward_for_export(
     if attention_mask is not None and attention_mask.ndim == 4:
         attention_mask = attention_mask[:, :, :, : key.shape[-2]]
         # torch._check used to inform torch.export of the shape relationship
-        torch._check(
-            attention_mask.shape[-1] == query.shape[-2],
-            lambda: "attention_mask.shape[-1] == query.shape[-2] should be True",
-        )
+        torch._check(attention_mask.shape[-1] == query.shape[-2])
 
     # We dispatch to SDPA's Flash Attention or Efficient kernels via this `is_causal` if statement instead of an inline conditional assignment
     # in SDPA to support both torch.compile's dynamic shapes and full graph options. An inline conditional prevents dynamic shapes from compiling.
