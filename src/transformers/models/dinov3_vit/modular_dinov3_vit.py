a14f3ce9b54832eed76dde91c8a2565702ab0ea2
dimidagd <46669905+dimidagd@users.noreply.github.com>
2025-10-16 12:42:49 -0300

    Update src/transformers/models/dinov3_vit/modular_dinov3_vit.py
    Co-authored-by: Pablo Montalvo <39954772+molbap@users.noreply.github.com>

--- src/transformers/models/dinov3_vit/modular_dinov3_vit.py
+++ src/transformers/models/dinov3_vit/modular_dinov3_vit.py
@@ -452,7 +452,6 @@ class DINOv3ViTForImageClassification(DINOv3ViTPreTrainedModel):
     def forward(
         self,
         pixel_values: Optional[torch.Tensor] = None,
-        head_mask: Optional[torch.Tensor] = None,
         labels: Optional[torch.Tensor] = None,
         **kwargs: Unpack[TransformersKwargs],
     ) -> ImageClassifierOutput:
@@ -462,7 +461,7 @@ class DINOv3ViTForImageClassification(DINOv3ViTPreTrainedModel):
             config.num_labels - 1]`. If `config.num_labels == 1` a regression loss is computed (Mean-Square loss), If
             `config.num_labels > 1` a classification loss is computed (Cross-Entropy).
         """
-        outputs: BaseModelOutputWithPooling = self.dinov3(pixel_values, head_mask=head_mask, **kwargs)
+        outputs: BaseModelOutputWithPooling = self.dinov3(pixel_values, **kwargs)
 
         sequence_output = outputs.last_hidden_state  # batch_size, sequence_length, hidden_size
         cls_token = sequence_output[:, 0]
