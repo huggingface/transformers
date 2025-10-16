557cfe201fbc02319bc9662529b9b131c97d3328
Ben Burtenshaw <ben.burtenshaw@gmail.com>
2025-10-16 11:09:22 +0000

    remove custom tokenizer

--- src/transformers/models/nanochat/__init__.py
+++ src/transformers/models/nanochat/__init__.py
@@ -1,11 +1,9 @@
 from .configuration_nanochat import NanoChatConfig
 from .modeling_nanochat import NanoChatForCausalLM, NanoChatModel
-from .tokenizer_nanochat import NanoChatTokenizer
 
 
 __all__ = [
     "NanoChatConfig",
     "NanoChatModel",
     "NanoChatForCausalLM",
-    "NanoChatTokenizer",
 ]
