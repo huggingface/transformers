4774877ad594a80466aab69c8938e5e96fb5ea55
yonigozlan <yoni.gozlan@huggingface.co>
2025-10-16 19:14:13 +0000

    remove comment

--- src/transformers/models/efficientloftr/modular_efficientloftr.py
+++ src/transformers/models/efficientloftr/modular_efficientloftr.py
@@ -1,7 +1,6 @@
 from ..superglue.image_processing_superglue_fast import SuperGlueImageProcessorFast
 
 
-# use modular for BC
 class EfficientLoFTRImageProcessorFast(SuperGlueImageProcessorFast):
     pass
 
