6126f3f6120c34a1317abd459060b86bd1fd3ed8
Ben Burtenshaw <ben.burtenshaw@gmail.com>
2025-10-18 11:57:37 +0000

    delete excess modelling file and regenerate modeling

--- src/transformers/models/nanochat/modeling_nanochat.py
+++ src/transformers/models/nanochat/modeling_nanochat.py
@@ -460,6 +460,23 @@ class NanoChatForCausalLM(NanoChatPreTrainedModel, GenerationMixin):
         logits_to_keep: Union[int, torch.Tensor] = 0,
         **kwargs: Unpack[TransformersKwargs],
     ) -> CausalLMOutputWithPast:
+        r"""
+        Example:
+
+        ```python
+        >>> from transformers import AutoTokenizer, NanoChatForCausalLM
+
+        >>> model = NanoChatForCausalLM.from_pretrained("meta-nanochat/NanoChat-2-7b-hf")
+        >>> tokenizer = AutoTokenizer.from_pretrained("meta-nanochat/NanoChat-2-7b-hf")
+
+        >>> prompt = "Hey, are you conscious? Can you talk to me?"
+        >>> inputs = tokenizer(prompt, return_tensors="pt")
+
+        >>> # Generate
+        >>> generate_ids = model.generate(inputs.input_ids, max_length=30)
+        >>> tokenizer.batch_decode(generate_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0]
+        "Hey, are you conscious? Can you talk to me?\nI'm not conscious, but I can talk to you."
+        ```"""
         outputs: BaseModelOutputWithPast = self.model(
             input_ids=input_ids,
             attention_mask=attention_mask,
