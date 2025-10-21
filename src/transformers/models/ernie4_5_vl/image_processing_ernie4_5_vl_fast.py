33a0319abd85b7cecdf7216cbd182a389ca77c29
vasqu <antonprogamer@gmail.com>
2025-10-21 17:22:57 +0200

    vid processor is properly separated now

--- src/transformers/models/ernie4_5_vl/image_processing_ernie4_5_vl_fast.py
+++ src/transformers/models/ernie4_5_vl/image_processing_ernie4_5_vl_fast.py
@@ -35,12 +35,8 @@ from ...image_utils import (
     SizeDict,
 )
 from ...processing_utils import Unpack
-from ...utils import TensorType, auto_docstring, logging
-from ...video_utils import VideoInput, make_batched_videos
+from ...utils import TensorType, auto_docstring
 from .image_processing_ernie4_5_vl import Ernie4_5_VLImageProcessorKwargs
-
-
-logger = logging.get_logger(__name__)
 
 
 def smart_resize(
@@ -88,7 +84,7 @@ class Ernie4_5_VLImageProcessorFast(BaseImageProcessorFast):
     min_pixels = None
     max_pixels = None
     valid_kwargs = Ernie4_5_VLImageProcessorKwargs
-    model_input_names = ["pixel_values", "image_grid_thw", "pixel_values_videos", "video_grid_thw"]
+    model_input_names = ["pixel_values", "image_grid_thw"]
 
     def __init__(self, **kwargs: Unpack[Ernie4_5_VLImageProcessorKwargs]):
         size = kwargs.pop("size", None)
@@ -134,15 +130,13 @@ class Ernie4_5_VLImageProcessorFast(BaseImageProcessorFast):
     def preprocess(
         self,
         images: ImageInput,
-        videos: Optional[VideoInput] = None,
         **kwargs: Unpack[Ernie4_5_VLImageProcessorKwargs],
     ) -> BatchFeature:
-        return super().preprocess(images, videos, **kwargs)
+        return super().preprocess(images, **kwargs)
 
     def _preprocess_image_like_inputs(
         self,
         images: ImageInput,
-        videos: VideoInput,
         do_convert_rgb: bool,
         input_data_format: ChannelDimension,
         device: Optional[Union[str, "torch.device"]] = None,
@@ -155,27 +149,10 @@ class Ernie4_5_VLImageProcessorFast(BaseImageProcessorFast):
         """
         # Prepare input images
         batch_feature = BatchFeature()
-        if images is not None:
-            images = self._prepare_image_like_inputs(
-                images=images, do_convert_rgb=do_convert_rgb, input_data_format=input_data_format, device=device
-            )
-            batch_feature = self._preprocess(images, **kwargs)
-        if videos is not None:
-            logger.warning(
-                "`Ernie4_5_VLImageProcessorFast` works only with image inputs and doesn't process videos anymore. "
-                "This is a deprecated behavior and will be removed in v5.0. "
-                "Your videos should be forwarded to `Ernie4_5_VLVideoProcessor`. "
-            )
-            # Can't change _prepare_images_structure to work with videos because it also needs to work with images.
-            videos = make_batched_videos(videos)
-            videos = [
-                torch.stack(self._prepare_image_like_inputs(video, do_convert_rgb, input_data_format, device))
-                for video in videos
-            ]
-            video_outputs = self._preprocess(videos, **kwargs)
-            batch_feature.update(
-                {"pixel_values_videos": video_outputs.pixel_values, "video_grid_thw": video_outputs.image_grid_thw}
-            )
+        images = self._prepare_image_like_inputs(
+            images=images, do_convert_rgb=do_convert_rgb, input_data_format=input_data_format, device=device
+        )
+        batch_feature = self._preprocess(images, **kwargs)
         return batch_feature
 
     def _preprocess(
