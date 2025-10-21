bf3118d9e2dc9c177b65f3bd169914730ee9c269
Steven Bucaille <steven.bucaille@gmail.com>
2025-10-21 00:10:51 -0400

    fix: added missing attention mask argument

--- src/transformers/models/lw_detr/modeling_lw_detr_vit.py
+++ src/transformers/models/lw_detr/modeling_lw_detr_vit.py
@@ -93,6 +93,7 @@ class LwDetrViTSelfAttention(nn.Module):
             query_layer,
             key_layer,
             value_layer,
+            None,
             is_causal=self.is_causal,
             scaling=self.scaling,
             dropout=0.0 if not self.training else self.dropout_prob,
