80159641bc72a12ac8118bd8f5be33f6a88e5dda
vasqu <antonprogamer@gmail.com>
2025-10-30 14:06:14 +0100

    fix

--- utils/check_repo.py
+++ utils/check_repo.py
@@ -394,6 +394,7 @@ IGNORE_NON_AUTO_CONFIGURED = PRIVATE_MODELS.copy() + [
     "Qwen3OmniMoeTalkerModel",  # Building part of a bigger model
     "Qwen3OmniMoeThinkerForConditionalGeneration",  # Building part of a bigger model
     "Qwen3OmniMoeThinkerTextModel",  # Building part of a bigger model
+    "Ernie4_5_VLTextModel",  # Building part of a bigger model
 ]
 
 
