7d9bd805f1e4f96a4cdb9b53f63ad49c2480a647
yonigozlan <yoni.gozlan@huggingface.co>
2025-11-04 20:13:46 +0000

    Add pix2struct fast image processor

--- src/transformers/models/pix2struct/__init__.py
+++ src/transformers/models/pix2struct/__init__.py
@@ -20,6 +20,7 @@ from ...utils.import_utils import define_import_structure
 if TYPE_CHECKING:
     from .configuration_pix2struct import *
     from .image_processing_pix2struct import *
+    from .image_processing_pix2struct_fast import *
     from .modeling_pix2struct import *
     from .processing_pix2struct import *
 else:
