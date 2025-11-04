7a9e3f7722396af2575856e0db24f37ae029b90c
IlyasMoutawwakil <moutawwakil.ilyas.tsi@gmail.com>
2025-11-04 17:29:33 +0100

    disable torch export for some models using custom caches

--- tests/models/mamba/test_modeling_mamba.py
+++ tests/models/mamba/test_modeling_mamba.py
@@ -236,6 +236,7 @@ class MambaModelTest(ModelTesterMixin, GenerationTesterMixin, PipelineTesterMixi
     all_model_classes = (MambaModel, MambaForCausalLM) if is_torch_available() else ()
     has_attentions = False  # Mamba does not support attentions
     test_missing_keys = False
+    test_torch_exportable = False
 
     pipeline_model_mapping = (
         {"feature-extraction": MambaModel, "text-generation": MambaForCausalLM} if is_torch_available() else {}
