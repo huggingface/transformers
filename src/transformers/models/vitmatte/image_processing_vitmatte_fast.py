bd710b780db55f1eded08b95e40f70b0852759e3
yaswanth19 <yaswanthgali8@gmail.com>
2025-11-06 00:12:15 +0530

    Update trimaps logic

--- src/transformers/models/vitmatte/image_processing_vitmatte_fast.py
+++ src/transformers/models/vitmatte/image_processing_vitmatte_fast.py
@@ -152,7 +152,8 @@ class VitMatteImageProcessorFast(BaseImageProcessorFast):
         processed_images_grouped = {}
         for shape in grouped_images:
             stacked_images = grouped_images[shape]
-            stacked_trimaps = grouped_trimaps[shape]
+            trimaps_shape = torch.Size([1, *shape[1:]]) # Trimaps have singel channel
+            stacked_trimaps = grouped_trimaps[trimaps_shape]
             # Fused rescale and normalize
             stacked_images = self.rescale_and_normalize(
                 stacked_images, do_rescale, rescale_factor, do_normalize, image_mean, image_std
