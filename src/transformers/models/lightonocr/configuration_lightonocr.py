fde46aa4f3650f9f23da6ed1f3a14204145fbe5e
Baptiste Aubertin <bapt120@hotmail.fr>
2025-11-06 11:17:04 +0100

    fixup

--- src/transformers/models/lightonocr/configuration_lightonocr.py
+++ src/transformers/models/lightonocr/configuration_lightonocr.py
@@ -73,7 +73,7 @@ class LightOnOCRVisionConfig(PreTrainedConfig):
         patch_size: Optional[int] = 16,
         hidden_act: Optional[str] = "gelu",
         attention_dropout: Optional[float] = 0.0,
-        rope_parameters: Optional[RopeParameters | dict[RopeParameters]] = None,
+        rope_parameters: Optional[RopeParameters | dict[str, RopeParameters]] = None,
         initializer_range: Optional[float] = 0.02,
         **kwargs,
     ):
@@ -210,7 +210,7 @@ class LightOnOCRTextConfig(PreTrainedConfig):
         rms_norm_eps: Optional[int] = 1e-6,
         use_cache: Optional[bool] = True,
         tie_word_embeddings: Optional[bool] = False,
-        rope_parameters: Optional[RopeParameters | dict[RopeParameters]] = None,
+        rope_parameters: Optional[RopeParameters | dict[str, RopeParameters]] = None,
         attention_bias: Optional[bool] = False,
         use_sliding_window: Optional[bool] = False,
         sliding_window: Optional[int] = 4096,
