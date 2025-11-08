f11b7acd32f1cbc6d6d042593d873e1848a30ef7
IlyasMoutawwakil <moutawwakil.ilyas.tsi@gmail.com>
2025-11-08 12:38:12 +0100

    skip some models with highly data-dependent helper methods

--- tests/models/colqwen2/test_modeling_colqwen2.py
+++ tests/models/colqwen2/test_modeling_colqwen2.py
@@ -202,6 +202,7 @@ class ColQwen2ForRetrievalModelTest(ModelTesterMixin, unittest.TestCase):
     all_model_classes = (ColQwen2ForRetrieval,) if is_torch_available() else ()
 
     test_resize_embeddings = True
+    test_torch_exportable = False  # the qwen2_vl model's get_rope_index method is highly data-dependent
 
     def setUp(self):
         self.model_tester = ColQwen2ForRetrievalModelTester(self)
