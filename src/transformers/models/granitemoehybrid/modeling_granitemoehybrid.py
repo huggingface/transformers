982aba52d01e83055d119a0f25db10363e210a5f
IlyasMoutawwakil <moutawwakil.ilyas.tsi@gmail.com>
2025-11-08 11:08:28 +0100

    fix granitemoehybrid

--- src/transformers/models/granitemoehybrid/modeling_granitemoehybrid.py
+++ src/transformers/models/granitemoehybrid/modeling_granitemoehybrid.py
@@ -1306,7 +1306,9 @@ class GraniteMoeHybridModel(GraniteMoeHybridPreTrainedModel):
             2. Attending to all inputs
         """
         mamba_mask = attention_mask
-        if cache_position[0] > 0 or (attention_mask is not None and torch.all(attention_mask == 1)):
+        if not torch.compiler.is_exporting() and (
+            cache_position[0] > 0 or (attention_mask is not None and torch.all(attention_mask == 1))
+        ):
             mamba_mask = None
         return mamba_mask
 
