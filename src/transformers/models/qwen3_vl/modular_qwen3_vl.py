f5630f9b1afb01db38219e574e641d455edcab4e
Qile Xu <xql438814395@gmail.com>
2025-11-10 16:23:43 +0800

    Fix return metadata checking logic (#42108)
    fix return_metadata_checking_logic

--- src/transformers/models/qwen3_vl/modular_qwen3_vl.py
+++ src/transformers/models/qwen3_vl/modular_qwen3_vl.py
@@ -1346,7 +1346,7 @@ class Qwen3VLProcessor(Qwen2VLProcessor):
             videos_inputs = self.video_processor(videos=videos, **output_kwargs["videos_kwargs"])
             video_grid_thw = videos_inputs["video_grid_thw"]
             # If user has not requested video metadata, pop it
-            if "return_metadata" not in kwargs:
+            if not kwargs.get("return_metadata"):
                 video_metadata = videos_inputs.pop("video_metadata")
             else:
                 video_metadata = videos_inputs["video_metadata"]
