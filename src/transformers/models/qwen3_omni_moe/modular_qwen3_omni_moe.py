eaea27bff6a598b7bc1e8677d54abb23d5caad49
Diego Akel <diegoakel1@gmail.com>
2025-11-11 12:45:22 +0100

    missing self.training

--- src/transformers/models/qwen3_omni_moe/modular_qwen3_omni_moe.py
+++ src/transformers/models/qwen3_omni_moe/modular_qwen3_omni_moe.py
@@ -1454,7 +1454,7 @@ class Qwen3OmniMoeThinkerForConditionalGeneration(Qwen2_5OmniThinkerForCondition
             )
 
         aux_loss = None
-        if output_router_logits and labels is not None:
+        if output_router_logits and self.training:
             aux_loss = load_balancing_loss_func(
                 outputs.router_logits,
                 self.num_experts,
