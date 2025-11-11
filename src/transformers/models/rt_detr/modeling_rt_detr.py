ade8dab4e5b3782e3e41fe3b478a5bf9913a42c6
Arthur <arthur.zucker@gmail.com>
2025-11-11 23:05:07 +0100

    upsy

--- src/transformers/models/rt_detr/modeling_rt_detr.py
+++ src/transformers/models/rt_detr/modeling_rt_detr.py
@@ -1028,7 +1028,7 @@ class RTDetrPreTrainedModel(PreTrainedModel):
             for i in range(module.n_points):
                 grid_init[:, :, i, :] *= i + 1
 
-            if not getattr(module.sampling_offsets.bias, "_is_hf_initialized") :
+            if not getattr(module.sampling_offsets.bias, "_is_hf_initialized", False) :
                 module.sampling_offsets.bias = nn.Parameter(grid_init.view(-1))
             nn.init.constant_(module.attention_weights.weight, 0.0)
             nn.init.constant_(module.attention_weights.bias, 0.0)
