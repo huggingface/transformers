a43c606bbb77f79c0d92eb5eb6500336d23f9956
zRzRzRzRzRzRzR <2448370773@qq.com>
2025-11-11 11:06:16 +0800

    21

--- src/transformers/models/glm46v/processing_glm46v.py
+++ src/transformers/models/glm46v/processing_glm46v.py
@@ -60,12 +60,6 @@ class Glm46vProcessor(ProcessorMixin):
             in a chat into a tokenizable string.
     """
 
-    attributes = ["image_processor", "tokenizer", "video_processor"]
-    image_processor_class = "AutoImageProcessor"
-    video_processor_class = "AutoVideoProcessor"
-
-    tokenizer_class = ("PreTrainedTokenizer", "PreTrainedTokenizerFast")
-
     def __init__(self, image_processor=None, tokenizer=None, video_processor=None, chat_template=None, **kwargs):
         self.image_token = "<|image|>" if not hasattr(tokenizer, "image_token") else tokenizer.image_token
         self.video_token = "<|video|>" if not hasattr(tokenizer, "video_token") else tokenizer.video_token
