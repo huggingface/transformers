e20ed0019499c042fcd42b948c9ec241fc3fc485
Marc Sun <marc@huggingface.co>
2025-11-12 14:57:33 +0100

    recover orignal parameter/buffer using _original

--- src/transformers/modeling_utils.py
+++ src/transformers/modeling_utils.py
@@ -4576,13 +4576,14 @@ class PreTrainedModel(nn.Module, EmbeddingAccessMixin, ModuleUtilsMixin, PushToH
         # Replace the loaded parameters class back to nn.Parameter (they were changed to easily skip initialization
         # when performed in-place on the tensors)
         for name, p in list(self.named_parameters()) + list(self.named_buffers()):
-            if hasattr(p, "_original_cls"):
+            # We get back the original parameter that we stored in _original. This attribute was created when we initialized LoadedParam when loading the checkpoints.
+            if hasattr(p, "_original"):
                 if '.' in name:
                     module, name = name.rsplit(".", 1)
                     module = self.get_submodule(module)
                 else:
                     module = self
-                setattr(module, name, p._original_cls(p.data))
+                setattr(module, name, p._original)
 
 
     def _adjust_missing_and_unexpected_keys(
