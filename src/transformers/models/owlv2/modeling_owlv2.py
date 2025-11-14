9b9020aadc136096909a3e577901f5d7faf96844
Aravind-11 <arvindhant01@gmail.com>
2025-11-13 18:07:59 -0700

    fixes

--- src/transformers/models/owlv2/modeling_owlv2.py
+++ src/transformers/models/owlv2/modeling_owlv2.py
@@ -500,8 +500,7 @@ class Owlv2EncoderLayer(GradientCheckpointingLayer):
         self,
         hidden_states: torch.Tensor,
         attention_mask: torch.Tensor,
-        causal_attention_mask: torch.Tensor,
-        output_attentions: Optional[bool] = False,
+        **kwargs,
     ) -> tuple[torch.FloatTensor]:
         """
         Args:
@@ -519,8 +518,6 @@ class Owlv2EncoderLayer(GradientCheckpointingLayer):
         hidden_states, attn_weights = self.self_attn(
             hidden_states=hidden_states,
             attention_mask=attention_mask,
-            causal_attention_mask=causal_attention_mask,
-            output_attentions=output_attentions,
         )
         hidden_states = residual + hidden_states
 
@@ -531,8 +528,7 @@ class Owlv2EncoderLayer(GradientCheckpointingLayer):
 
         outputs = (hidden_states,)
 
-        if output_attentions:
-            outputs += (attn_weights,)
+       
 
         return outputs
 
