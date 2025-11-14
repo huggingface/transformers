5b8e7924efc856a4b1df3c4b184462ce7ff000ee
ydshieh <ydshieh@users.noreply.github.com>
2025-11-14 16:15:02 +0100

    fix

--- tests/models/pegasus_x/test_modeling_pegasus_x.py
+++ tests/models/pegasus_x/test_modeling_pegasus_x.py
@@ -66,7 +66,7 @@ class PegasusXModelTester:
     def __init__(
         self,
         parent,
-        batch_size=13,
+        batch_size=3,
         seq_length=7,
         is_training=True,
         use_labels=False,
