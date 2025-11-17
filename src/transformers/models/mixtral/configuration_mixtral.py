e4b1d1cdb07d4ac41138a07cbd704e0a5fef2332
Cyril Vallez <cyril.vallez@huggingface.co>
2025-11-17 12:43:59 +0100

    Fix TP plans for MoE models (#42236)
    * start
    
    * more fixes

--- src/transformers/models/mixtral/configuration_mixtral.py
+++ src/transformers/models/mixtral/configuration_mixtral.py
@@ -115,16 +115,14 @@ class MixtralConfig(PreTrainedConfig):
     model_type = "mixtral"
     keys_to_ignore_at_inference = ["past_key_values"]
     base_model_tp_plan = {
-        "layers.*.self_attn.q_proj": "local_colwise",
-        "layers.*.self_attn.k_proj": "local_colwise",
-        "layers.*.self_attn.v_proj": "local_colwise",
-        "layers.*.self_attn.o_proj": "local_rowwise",
-        "layers.*.self_attn": "gather",
+        "layers.*.self_attn.q_proj": "colwise",
+        "layers.*.self_attn.k_proj": "colwise",
+        "layers.*.self_attn.v_proj": "colwise",
+        "layers.*.self_attn.o_proj": "rowwise",
         "layers.*.mlp.gate": "ep_router",  # we need to replicate here to correctly route experts
         "layers.*.mlp.experts.gate_up_proj": "local_colwise",
         "layers.*.mlp.experts.down_proj": "local_rowwise",
         "layers.*.mlp.experts": "gather",
-        # "layers.*.mlp.experts.gate_up_proj": "local_packed_rowwise" ? if you load from
     }
     base_model_pp_plan = {
         "embed_tokens": (["input_ids"], ["inputs_embeds"]),
