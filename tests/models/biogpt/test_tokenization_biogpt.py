6795515d8aaa02d9745df30dbc275322d014c58e
itazap <ita.zaporozhets@huggingface.co>
2025-11-17 22:13:34 +0100

    biogpt

--- tests/models/biogpt/test_tokenization_biogpt.py
+++ tests/models/biogpt/test_tokenization_biogpt.py
@@ -15,6 +15,8 @@
 
 import json
 import os
+import shutil
+import tempfile
 import unittest
 
 from transformers.models.biogpt.tokenization_biogpt import VOCAB_FILES_NAMES, BioGptTokenizer
@@ -62,10 +64,6 @@ class BioGptTokenizationTest(TokenizerTesterMixin, unittest.TestCase):
 
         cls.vocab_file = os.path.join(cls.tmpdirname, VOCAB_FILES_NAMES["vocab_file"])
         cls.merges_file = os.path.join(cls.tmpdirname, VOCAB_FILES_NAMES["merges_file"])
-        with open(cls.vocab_file, "w") as fp:
-            fp.write(json.dumps(vocab_tokens))
-        with open(cls.merges_file, "w") as fp:
-            fp.write("\n".join(merges))
 
     def get_input_output_texts(self, tokenizer):
         input_text = "lower newer"
@@ -74,7 +72,44 @@ class BioGptTokenizationTest(TokenizerTesterMixin, unittest.TestCase):
 
     def test_full_tokenizer(self):
         """Adapted from Sennrich et al. 2015 and https://github.com/rsennrich/subword-nmt"""
-        tokenizer = BioGptTokenizer(self.vocab_file, self.merges_file)
+
+        vocab = [
+            "l",
+            "o",
+            "w",
+            "e",
+            "r",
+            "s",
+            "t",
+            "i",
+            "d",
+            "n",
+            "w</w>",
+            "r</w>",
+            "t</w>",
+            "lo",
+            "low",
+            "er</w>",
+            "low</w>",
+            "lowest</w>",
+            "newer</w>",
+            "wider</w>",
+            "<unk>",
+        ]
+        vocab_tokens = dict(zip(vocab, range(len(vocab))))
+        merges = ["l o 123", "lo w 1456", "e r</w> 1789", ""]
+
+
+        with tempfile.TemporaryDirectory() as tmpdir:
+            vocab_file = os.path.join(tmpdir, VOCAB_FILES_NAMES["vocab_file"])
+            merges_file = os.path.join(tmpdir, VOCAB_FILES_NAMES["merges_file"])
+            shutil.copy(self.vocab_file, vocab_file)
+            shutil.copy(self.merges_file, merges_file)
+            with open(vocab_file, "w") as fp:
+                fp.write(json.dumps(vocab_tokens))
+            with open(merges_file, "w") as fp:
+                fp.write("\n".join(merges))
+            tokenizer = BioGptTokenizer(vocab_file, merges_file)
 
         text = "lower"
         bpe_tokens = ["low", "er</w>"]
