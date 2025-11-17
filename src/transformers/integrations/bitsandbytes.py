bf51aa16f2c2ff24b462f704e5bfa8f2d791aa97
Marc Sun <57196510+SunMarc@users.noreply.github.com>
2025-11-17 17:16:06 +0100

    Apply suggestions from code review
    Co-authored-by: Arthur <48595927+ArthurZucker@users.noreply.github.com>

--- src/transformers/integrations/bitsandbytes.py
+++ src/transformers/integrations/bitsandbytes.py
@@ -37,6 +37,9 @@ class Bnb4bitQuantize(ConversionOps):
         self.hf_quantizer = hf_quantizer
 
     def convert(self, input_dict: torch.Tensor, model: Optional[torch.nn.Module] = None, missing_keys = None, **kwargs) -> dict[str, torch.Tensor]:
+    """
+    we need to store some parameters to create the quantized weight. For example, bnb requires 6 values that are stored in the checkpoint to recover the quantized weight. So we store them in a dict that it stored in hf_quantizer for now as we can't save it in the op since we create an op per tensor.
+    """
         target_key, value = tuple(input_dict.items())[0]
         value = value[0] if isinstance(value, list) else value
 
