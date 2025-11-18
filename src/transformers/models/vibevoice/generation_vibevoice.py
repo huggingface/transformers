4c1068dd888d0c9b544dd817e3556bd4aa56346e
Eric Bezzam <ebezzam@gmail.com>
2025-11-18 16:30:34 +0100

    Add blank line

--- src/transformers/models/vibevoice/generation_vibevoice.py
+++ src/transformers/models/vibevoice/generation_vibevoice.py
@@ -13,6 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+
 from collections.abc import Callable
 from dataclasses import dataclass
 from queue import Queue
