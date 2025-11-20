wrong order whoops

src/transformers/models/ernie4_5_vl/modeling_ernie4_5_vl.py
@@ -427,7 +427,7 @@ class Ernie4_5_VLSparseMoeBlock(nn.Module):
     ) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]:
         hidden_states = hidden_states.view(-1, self.hidden_dim)
 
-        selected_experts, routing_weights, router_logits = self.gate(hidden_states)
+        routing_weights, selected_experts, router_logits = self.gate(hidden_states)
         final_hidden_states = self.experts(hidden_states, selected_experts, routing_weights)
 
         # moe results are changed to a flattened shape to ease the modality isolated assigning of results