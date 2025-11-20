8f85d673e997c6c336c6b87a7b397d8030e99f95
vasqu <antonprogamer@gmail.com>
2025-11-20 18:47:47 +0100

    fix

--- src/transformers/models/nanochat/modeling_nanochat.py
+++ src/transformers/models/nanochat/modeling_nanochat.py
@@ -444,7 +444,7 @@ class NanoChatModel(NanoChatPreTrainedModel):
 
 @auto_docstring
 class NanoChatForCausalLM(NanoChatPreTrainedModel, GenerationMixin):
-    _tied_weights_keys = ["lm_head.weight"]
+    _tied_weights_keys = {"lm_head.weight": "model.embed_tokens.weight"}
     _tp_plan = {"lm_head": "colwise_rep"}
     _pp_plan = {"lm_head": (["hidden_states"], ["logits"])}
 
