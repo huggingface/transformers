691cf16c695a1c65704a4a3f74b0f4426208409d
Matt <rocketknight1@gmail.com>
2025-11-20 15:42:36 +0000

    Allow LayoutLMV3Processor to accept rescale_factor

--- src/transformers/models/layoutlmv3/image_processing_layoutlmv3.py
+++ src/transformers/models/layoutlmv3/image_processing_layoutlmv3.py
@@ -188,10 +188,7 @@ class LayoutLMv3ImageProcessor(BaseImageProcessor):
         self.size = size
         self.resample = resample
         self.do_rescale = do_rescale
-        if "rescale_factor" in kwargs:
-            self.rescale_factor = kwargs["rescale_factor"]
-        else:
-            self.rescale_factor = rescale_value
+        self.rescale_factor = kwargs.get("rescale_factor", rescale_value)
         self.do_normalize = do_normalize
         self.image_mean = image_mean if image_mean is not None else IMAGENET_STANDARD_MEAN
         self.image_std = image_std if image_std is not None else IMAGENET_STANDARD_STD
