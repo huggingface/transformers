b8135b4e18127b4237ffb6a322c54748263a631f
IlyasMoutawwakil <moutawwakil.ilyas.tsi@gmail.com>
2025-11-21 09:19:39 +0100

    more vlms

--- src/transformers/models/deepseek_vl/modeling_deepseek_vl.py
+++ src/transformers/models/deepseek_vl/modeling_deepseek_vl.py
@@ -174,12 +174,12 @@ class DeepseekVLModel(DeepseekVLPreTrainedModel):
             special_image_mask = input_ids == self.config.image_token_id
 
         n_image_tokens = special_image_mask.sum()
+        n_image_features = image_features.shape[0] * image_features.shape[1]
         special_image_mask = special_image_mask.unsqueeze(-1).expand_as(inputs_embeds).to(inputs_embeds.device)
-        if inputs_embeds[special_image_mask].numel() != image_features.numel():
-            n_image_features = image_features.shape[0] * image_features.shape[1]
-            raise ValueError(
-                f"Image features and image tokens do not match: tokens: {n_image_tokens}, features {n_image_features}"
-            )
+        torch._check(
+            inputs_embeds[special_image_mask].numel() == image_features.numel(),
+            lambda: f"Image features and image tokens do not match: tokens: {n_image_tokens}, features {n_image_features}",
+        )
         return special_image_mask
 
     @can_return_tuple
