bf2662cd7a9e4f25f61bd525e98e0a0de7606a69
vasqu <antonprogamer@gmail.com>
2025-11-21 17:01:06 +0100

    style

--- tests/models/efficientloftr/test_image_processing_efficientloftr.py
+++ tests/models/efficientloftr/test_image_processing_efficientloftr.py
@@ -129,7 +129,7 @@ class EfficientLoFTRImageProcessingTest(SuperGlueImageProcessingTest, unittest.T
             fast_time, slow_time * 1.2, "Fast processor should not be significantly slower than slow processor"
         )
 
-    # TODO: FIXME @yonigozlan 
+    # TODO: FIXME @yonigozlan
     @unittest.skip(reason="failing after #42018")
     def test_post_processing_keypoint_matching_with_padded_match_indices(self):
         pass
