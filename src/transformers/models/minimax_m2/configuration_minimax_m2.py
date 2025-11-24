ef9a1f9895b5677b9caa6e972a6d15990376732d
xuebi <xuebi@minimaxi.com>
2025-11-24 19:40:35 +0800

    update: fix docs

--- src/transformers/models/minimax_m2/configuration_minimax_m2.py
+++ src/transformers/models/minimax_m2/configuration_minimax_m2.py
@@ -38,66 +38,68 @@ class MiniMaxM2Config(PreTrainedConfig):
 
 
     Args:
-        vocab_size (`int`, *optional*, defaults to 32000):
-            Vocabulary size of the MiniMaxM2 model. Defines the number of different tokens that can be represented by the
-            `inputs_ids` passed when calling [`MiniMaxM2Model`]
-        hidden_size (`int`, *optional*, defaults to 4096):
-            Dimension of the hidden representations.
-        intermediate_size (`int`, *optional*, defaults to 14336):
-            Dimension of the MLP representations.
-        num_hidden_layers (`int`, *optional*, defaults to 32):
-            Number of hidden layers in the Transformer encoder.
-        num_attention_heads (`int`, *optional*, defaults to 32):
-            Number of attention heads for each attention layer in the Transformer encoder.
-        num_key_value_heads (`int`, *optional*, defaults to 8):
-            This is the number of key_value heads that should be used to implement Grouped Query Attention. If
-            `num_key_value_heads=num_attention_heads`, the model will use Multi Head Attention (MHA), if
-            `num_key_value_heads=1` the model will use Multi Query Attention (MQA) otherwise GQA is used. When
-            converting a multi-head checkpoint to a GQA checkpoint, each group key and value head should be constructed
-            by meanpooling all the original heads within that group. For more details, check out [this
-            paper](https://huggingface.co/papers/2305.13245). If it is not specified, will default to `8`.
-        head_dim (`int`, *optional*, defaults to `hidden_size // num_attention_heads`):
-            The attention head dimension.
-        hidden_act (`str` or `function`, *optional*, defaults to `"silu"`):
-            The non-linear activation function (function or string) in the decoder.
-        max_position_embeddings (`int`, *optional*, defaults to `4096*32`):
-            The maximum sequence length that this model might ever be used with. MiniMaxM2's sliding window attention
-            allows sequence of up to 4096*32 tokens.
-        initializer_range (`float`, *optional*, defaults to 0.02):
-            The standard deviation of the truncated_normal_initializer for initializing all weight matrices.
-        rms_norm_eps (`float`, *optional*, defaults to 1e-05):
-            The epsilon used by the rms normalization layers.
-        use_cache (`bool`, *optional*, defaults to `True`):
-            Whether or not the model should return the last key/values attentions (not used by all models). Only
-            relevant if `config.is_decoder=True`.
-        pad_token_id (`int`, *optional*):
-            The id of the padding token.
-        bos_token_id (`int`, *optional*, defaults to 1):
-            The id of the "beginning-of-sequence" token.
-        eos_token_id (`int`, *optional*, defaults to 2):
-            The id of the "end-of-sequence" token.
-        tie_word_embeddings (`bool`, *optional*, defaults to `False`):
-            Whether the model's input and output word embeddings should be tied.
-        sliding_window (`int`, *optional*):
-            Sliding window attention window size. If not specified, will default to `4096`.
-        attention_dropout (`float`, *optional*, defaults to 0.0):
-            The dropout ratio for the attention probabilities.
-        num_experts_per_tok (`int`, *optional*, defaults to 2):
-            The number of experts to route per-token, can be also interpreted as the `top-k` routing
-            parameter
-        num_local_experts (`int`, *optional*, defaults to 8):
-            Number of experts per Sparse MLP layer.
-        output_router_logits (`bool`, *optional*, defaults to `False`):
-            Whether or not the router logits should be returned by the model. Enabling this will also
-            allow the model to output the auxiliary loss. See [here]() for more details
-        router_aux_loss_coef (`float`, *optional*, defaults to 0.001):
-            The aux loss factor for the total loss.
-        router_jitter_noise (`float`, *optional*, defaults to 0.0):
-            Amount of noise to add to the router.
-        rope_parameters (`RopeParameters`, *optional*):
-            Dictionary containing the configuration parameters for the RoPE embeddings. The dictionaty should contain
-            a value for `rope_theta` and optionally parameters used for scaling in case you want to use RoPE
-            with longer `max_position_embeddings`.
+            vocab_size (`int`, *optional*, defaults to 32000):
+                Vocabulary size of the MiniMaxM2 model. Defines the number of different tokens that can be represented by the
+                `inputs_ids` passed when calling [`MiniMaxM2Model`]
+            hidden_size (`int`, *optional*, defaults to 4096):
+                Dimension of the hidden representations.
+            intermediate_size (`int`, *optional*, defaults to 14336):
+                Dimension of the MLP representations.
+            num_hidden_layers (`int`, *optional*, defaults to 32):
+                Number of hidden layers in the Transformer encoder.
+            num_attention_heads (`int`, *optional*, defaults to 32):
+                Number of attention heads for each attention layer in the Transformer encoder.
+            num_key_value_heads (`int`, *optional*, defaults to 8):
+                This is the number of key_value heads that should be used to implement Grouped Query Attention. If
+                `num_key_value_heads=num_attention_heads`, the model will use Multi Head Attention (MHA), if
+                `num_key_value_heads=1` the model will use Multi Query Attention (MQA) otherwise GQA is used. When
+                converting a multi-head checkpoint to a GQA checkpoint, each group key and value head should be constructed
+                by meanpooling all the original heads within that group. For more details, check out [this
+                paper](https://huggingface.co/papers/2305.13245). If it is not specified, will default to `8`.
+            head_dim (`int`, *optional*, defaults to `hidden_size // num_attention_heads`):
+                The attention head dimension.
+            hidden_act (`str` or `function`, *optional*, defaults to `"silu"`):
+                The non-linear activation function (function or string) in the decoder.
+            max_position_embeddings (`int`, *optional*, defaults to `4096*32`):
+                The maximum sequence length that this model might ever be used with. MiniMaxM2's sliding window attention
+                allows sequence of up to 4096*32 tokens.
+            initializer_range (`float`, *optional*, defaults to 0.02):
+                The standard deviation of the truncated_normal_initializer for initializing all weight matrices.
+            rms_norm_eps (`float`, *optional*, defaults to 1e-05):
+                The epsilon used by the rms normalization layers.
+            use_cache (`bool`, *optional*, defaults to `True`):
+                Whether or not the model should return the last key/values attentions (not used by all models). Only
+                relevant if `config.is_decoder=True`.
+            pad_token_id (`int`, *optional*):
+                The id of the padding token.
+            bos_token_id (`int`, *optional*, defaults to 1):
+                The id of the "beginning-of-sequence" token.
+            eos_token_id (`int`, *optional*, defaults to 2):
+                The id of the "end-of-sequence" token.
+            tie_word_embeddings (`bool`, *optional*, defaults to `False`):
+                Whether the model's input and output word embeddings should be tied.
+            sliding_window (`int`, *optional*):
+                Sliding window attention window size. If not specified, will default to `4096`.
+            attention_dropout (`float`, *optional*, defaults to 0.0):
+                The dropout ratio for the attention probabilities.
+            num_experts_per_tok (`int`, *optional*, defaults to 2):
+                The number of experts to route per-token, can be also interpreted as the `top-k` routing
+                parameter
+            num_local_experts (`int`, *optional*, defaults to 8):
+                Number of experts per Sparse MLP layer.
+            output_router_logits (`bool`, *optional*, defaults to `False`):
+                Whether or not the router logits should be returned by the model. Enabling this will also
+                allow the model to output the auxiliary loss. See [here]() for more details
+            router_aux_loss_coef (`float`, *optional*, defaults to 0.001):
+                The aux loss factor for the total loss.
+            router_jitter_noise (`float`, *optional*, defaults to 0.0):
+                Amount of noise to add to the router.
+            rope_theta (`float`, *optional*, defaults to 10000.0):
+                The base period of the RoPE embeddings.
+            rope_parameters (`RopeParameters`, *optional*):
+                Dictionary containing the configuration parameters for the RoPE embeddings. The dictionaty should contain
+                a value for `rope_theta` and optionally parameters used for scaling in case you want to use RoPE
+                with longer `max_position_embeddings`.
 
     ```python
     >>> from transformers import MiniMaxM2Model, MiniMaxM2Config
