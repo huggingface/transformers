631553d2ed4ae3112b526230b3c2c262ee4f1b85
Kamila Luchay <kamilaluczaj24@gmail.com>
2025-11-24 18:58:05 +0100

    Consistency fixed

--- src/transformers/models/fast_vlm/modeling_fast_vlm.py
+++ src/transformers/models/fast_vlm/modeling_fast_vlm.py
@@ -58,7 +58,7 @@ class FastVlmMultiModalProjector(nn.Module):
 @auto_docstring
 class FastVlmPreTrainedModel(PreTrainedModel):
     config: FastVlmConfig
-    base_model_prefix = ""
+    base_model_prefix = "model"
     input_modalities = ["image", "text"]
     supports_gradient_checkpointing = True
     _skip_keys_device_placement = "past_key_values"
@@ -113,12 +113,6 @@ class FastVlmModel(FastVlmPreTrainedModel):
 
     def set_input_embeddings(self, value):
         self.language_model.set_input_embeddings(value)
-
-    def set_decoder(self, decoder):
-        self.language_model = decoder
-
-    def get_decoder(self):
-        return self.language_model
 
     def get_image_features(
         self,
@@ -291,7 +285,7 @@ class FastVlmCausalLMOutputWithPast(ModelOutput):
 )
 class FastVlmForConditionalGeneration(FastVlmPreTrainedModel, GenerationMixin):
     _checkpoint_conversion_mapping = {}
-    _tied_weights_keys = ["lm_head.weight"]
+    _tied_weights_keys = {"lm_head.weight": "model.language_model.embed_tokens.weight"}
 
     def __init__(self, config: FastVlmConfig):
         super().__init__(config)
@@ -308,12 +302,6 @@ class FastVlmForConditionalGeneration(FastVlmPreTrainedModel, GenerationMixin):
     def get_output_embeddings(self) -> nn.Module:
         return self.lm_head
 
-    def set_decoder(self, decoder):
-        self.model.set_decoder(decoder)
-
-    def get_decoder(self):
-        return self.model.get_decoder()
-
     def get_image_features(
         self,
         pixel_values: torch.FloatTensor,
@@ -327,19 +315,6 @@ class FastVlmForConditionalGeneration(FastVlmPreTrainedModel, GenerationMixin):
             vision_feature_select_strategy=vision_feature_select_strategy,
             **kwargs,
         )
-
-    # Make modules available through conditional class for BC
-    @property
-    def language_model(self):
-        return self.model.language_model
-
-    @property
-    def vision_tower(self):
-        return self.model.vision_tower
-
-    @property
-    def multi_modal_projector(self):
-        return self.model.multi_modal_projector
 
     @can_return_tuple
     @auto_docstring
