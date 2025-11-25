f0d58aab33fc42c8c894b474400130e34cd96a08
Steven Bucaille <steven.bucaille@gmail.com>
2025-11-25 10:37:34 -0500

    fix: rebased on lw_detr branch

--- src/transformers/models/rf_detr/modeling_rf_detr_dinov2.py
+++ src/transformers/models/rf_detr/modeling_rf_detr_dinov2.py
@@ -11,6 +11,7 @@ from typing import Optional, Union
 import torch
 from torch import nn
 
+from ... import initialization as init
 from ...activations import ACT2FN
 from ...modeling_layers import GradientCheckpointingLayer
 from ...modeling_outputs import BackboneOutput, BaseModelOutput
@@ -452,35 +453,23 @@ class RfDetrDinov2PreTrainedModel(PreTrainedModel):
         "attentions": RfDetrDinov2SelfAttention,
     }
 
+    @torch.no_grad()
     def _init_weights(self, module: Union[nn.Linear, nn.Conv2d, nn.LayerNorm]) -> None:
         """Initialize the weights"""
         if isinstance(module, (nn.Linear, nn.Conv2d)):
-            # Upcast the input in `fp32` and cast it back to desired `dtype` to avoid
-            # `trunc_normal_cpu` not implemented in `half` issues
-            module.weight.data = nn.init.trunc_normal_(
-                module.weight.data.to(torch.float32), mean=0.0, std=self.config.initializer_range
-            ).to(module.weight.dtype)
+            init.trunc_normal_(module.weight, mean=0.0, std=self.config.initializer_range)
             if module.bias is not None:
-                module.bias.data.zero_()
+                init.zeros_(module.bias)
         elif isinstance(module, nn.LayerNorm):
-            module.bias.data.zero_()
-            module.weight.data.fill_(1.0)
+            init.zeros_(module.bias)
+            init.ones_(module.weight)
         elif isinstance(module, RfDetrDinov2Embeddings):
-            module.position_embeddings.data = nn.init.trunc_normal_(
-                module.position_embeddings.data.to(torch.float32),
-                mean=0.0,
-                std=self.config.initializer_range,
-            ).to(module.position_embeddings.dtype)
-
-            module.cls_token.data = nn.init.trunc_normal_(
-                module.cls_token.data.to(torch.float32),
-                mean=0.0,
-                std=self.config.initializer_range,
-            ).to(module.cls_token.dtype)
-
-            module.mask_token.data.zero_()
-        elif isinstance(module, RfDetrDinov2LayerScale):  # noqa: F821
-            module.lambda1.data.fill_(self.config.layerscale_value)
+            init.trunc_normal_(module.position_embeddings, mean=0.0, std=self.config.initializer_range)
+            init.trunc_normal_(module.cls_token, mean=0.0, std=self.config.initializer_range)
+            if self.config.use_mask_token:
+                init.zeros_(module.mask_token)
+        elif isinstance(module, RfDetrDinov2LayerScale):
+            init.constant_(module.lambda1, self.config.layerscale_value)
 
 
 def window_unpartition(
