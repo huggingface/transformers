bca09a92ae0489fb78567d1d0eb3d4a591dce9e1
Arthur <arthur.zucker@gmail.com>
2025-11-26 10:46:18 +0100

    up

--- src/transformers/models/deberta_v2/tokenization_deberta_v2.py
+++ src/transformers/models/deberta_v2/tokenization_deberta_v2.py
@@ -114,7 +114,7 @@ class DebertaV2Tokenizer(TokenizersBackend):
             ]
 
         else:
-            self._vocab = [tuple(item) if not isinstance(item, tuple) else item for item in vocab]
+            self._vocab = list(vocab.items()) if isinstance(vocab, dict) else vocab
             for i, (piece, _score) in enumerate(self._vocab):
                 if piece == str(unk_token):
                     unk_id = i
