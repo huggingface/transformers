bc9b7c150d0c3cd2e696568f7af5fe6d7702544a
3outeille <ferdinand.mom@epita.fr>
2025-11-26 16:50:45 +0000

    remove zero scatter

--- src/transformers/models/qwen3_next/modeling_qwen3_next.py
+++ src/transformers/models/qwen3_next/modeling_qwen3_next.py
@@ -877,7 +877,7 @@ class Qwen3NextTopKRouter(nn.Module):
         if self.norm_topk_prob:
             router_top_value /= router_top_value.sum(dim=-1, keepdim=True)
         router_top_value = router_top_value.to(router_logits.dtype)
-        router_scores = torch.zeros_like(router_logits).scatter_(1, router_indices, router_top_value)
+        router_scores = router_top_value
         return router_scores, router_indices
 
 
