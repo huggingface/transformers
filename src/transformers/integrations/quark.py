7b535410cae1adbc76db21f93d533fc331762f2b
medmekk <mekk.cyber@gmail.com>
2025-11-27 15:35:12 +0000

    explain

--- src/transformers/integrations/quark.py
+++ src/transformers/integrations/quark.py
@@ -35,14 +35,21 @@ class QuarkDeserialize(ConversionOps):
         full_layer_name: str | None = None,
         **kwargs,
     ) -> dict[str, torch.Tensor]:
+        # target_key should be in the form of weight_scale, bias_scale, input_scale, output_scale, weight_zero_point, bias_zero_point, input_zero_point, output_zero_point
         target_key, value = tuple(input_dict.items())[0]
         value = value[0] if isinstance(value, list) else value
         # this will get the param name : weight, input, bias or output
         param = target_key.split("_", 1)[0]
+        # quant_state should be in the form of scale, or zero_point
         quant_state = target_key.split("_", 1)[-1]
 
+        # here we change the name for example from the form of : 
+        # model.layers.0.mlp.down_proj.weight_scale to model.layers.0.mlp.down_proj.weight_quantizer.scale to fit within
+        # the QParamsLinear module of quark
         sub_module_state = full_layer_name.rsplit(".", 1)[0] + "." + param + "_quantizer" + "." + quant_state
 
+        # since quark module was expecting keys in the form of model.layers.0.mlp.down_proj.weight_scale, 
+        # we need to remove it from the missing_keys list
         missing_keys.discard(full_layer_name)
 
         return {sub_module_state: value}
