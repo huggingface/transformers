e95a2eed37271e4c28c35c8467e5e8429bd1ae56
YangKai0616 <kai.yang@intel.com>
2025-11-27 07:13:50 +0000

    Make quality

--- src/transformers/integrations/finegrained_fp8.py
+++ src/transformers/integrations/finegrained_fp8.py
@@ -13,7 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import re
 from collections.abc import Sequence
 from typing import Any
 
