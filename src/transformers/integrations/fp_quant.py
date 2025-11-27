edbfae5172b0c2af2aaa3183bc31a694c15ed955
medmekk <mekk.cyber@gmail.com>
2025-11-27 15:57:10 +0000

    initial

--- src/transformers/integrations/fp_quant.py
+++ src/transformers/integrations/fp_quant.py
@@ -17,13 +17,78 @@ from ..utils import (
     is_fp_quant_available,
 )
 
+import torch
+from typing import Optional
 
 if is_fp_quant_available():
     from fp_quant import FPQuantConfig as FPQuantLinearConfig
     from fp_quant import FPQuantDtype
 
 from transformers.utils.quantization_config import FPQuantConfig
+from ..quantizers.quantizers_utils import get_module_from_name
+from ..core_model_loading import ConversionOps
 
+class FpQuantQuantize(ConversionOps):
+    def __init__(self, hf_quantizer):
+        self.hf_quantizer = hf_quantizer
+
+    def convert(self, input_dict: torch.Tensor, model: Optional[torch.nn.Module] = None, missing_keys: Optional[list[str]] = None, **kwargs) -> dict[str, torch.Tensor]:
+        target_key, value = tuple(input_dict.items())[0]
+        value = value[0] if isinstance(value, list) else value
+
+        module, _ = get_module_from_name(model, target_key)
+
+        # TODO: check if we need this or not, commented for now
+        # if target_device == "cpu" and param_name.endswith("weight"):
+        #     # Works agains hard-coded missing key dispatch to CPU
+        #     return
+        # The module holds either:
+        #  * `weight` when `store_master_weights=True`
+        #  * `qweight` and `scales` when `store_master_weights=False` and `pseudoquantization=False`
+        #  * `dqweight` when `store_master_weights=False` and `pseudoquantization=True`
+        if target_key.endswith(".qweight"):
+            # Loading a real quantized checkpoint without master weights
+            qweight = torch.nn.Parameter(
+                value,
+                requires_grad=False,
+            )
+            
+            weight_key = target_key.rsplit(".", 1)[0] + ".weight"
+            dqweight_key = target_key.rsplit(".", 1)[0] + ".dqweight"
+
+            return {f"{target_key}": qweight,
+                    }
+
+        if target_key.endswith(".dqweight"):
+            # Loading a pseudo-quantized checkpoint without master weights
+            dqweight = torch.nn.Parameter(value)
+            
+            weight_key = target_key.rsplit(".", 1)[0] + ".weight"
+            dqweight_key = target_key.rsplit(".", 1)[0] + ".dqweight"
+            scales_key = target_key.rsplit(".", 1)[0] + ".scales"
+            
+            return {
+                f"{target_key}": dqweight,
+                f"{weight_key}": torch.nn.Parameter(torch.empty(0)),
+                f"{dqweight_key}": torch.nn.Parameter(torch.empty(0)),
+                f"{scales_key}": torch.nn.Parameter(torch.empty(0))
+                }
+
+        # Loading master weights or an unquantized checkpoint
+        weight = torch.nn.Parameter(value)
+        module.weight = weight
+        # Let pre-forward handle the quantization and set None where necessary
+        module.pre_forward()
+        
+        prefix_target_key = target_key.rsplit(".", 1)[0]
+
+        return {target_key: weight,
+                f"{prefix_target_key}.act_global_scale": module.act_global_scale,
+                f"{prefix_target_key}.backward_hadamard_matrix": module.backward_hadamard_matrix,
+                f"{prefix_target_key}.forward_hadamard_matrix": module.forward_hadamard_matrix,
+                f"{prefix_target_key}.qweight": module.qweight,
+                f"{prefix_target_key}.scales": module.scales
+                }
 
 def adapt_fp_quant_config(config: FPQuantConfig):
     if config.forward_dtype == "mxfp4":
