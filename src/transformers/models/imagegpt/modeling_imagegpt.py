b770b2f168ca2231e04346c61647822451737559
Dhruv Deshmukh <dhruva4000@gmail.com>
2025-11-29 16:32:18 +0000

    fix: use input_shape and device instead of input_emdeds properties for imagegpt

--- src/transformers/models/imagegpt/modeling_imagegpt.py
+++ src/transformers/models/imagegpt/modeling_imagegpt.py
@@ -493,9 +493,7 @@ class ImageGPTModel(ImageGPTPreTrainedModel):
 
         if cache_position is None:
             past_seen_tokens = past_key_values.get_seq_length() if past_key_values is not None else 0
-            cache_position: torch.Tensor = (
-                torch.arange(inputs_embeds.shape[1], device=inputs_embeds.device) + past_seen_tokens
-            )
+            cache_position: torch.Tensor = torch.arange(input_shape[-1], device=device) + past_seen_tokens
 
         if position_ids is None:
             position_ids = cache_position.unsqueeze(0)
