069e35e8626ad24a837c665cf6af9bf02b8c02ad
ydshieh <ydshieh@users.noreply.github.com>
2025-12-01 16:15:43 +0100

    run

--- .circleci/create_circleci_config.py
+++ .circleci/create_circleci_config.py
@@ -189,7 +189,7 @@ class CircleCIJob:
             {"run": {"name": "download and unzip hub cache", "command": 'curl -L -o huggingface-cache.tar.gz https://huggingface.co/datasets/hf-internal-testing/hf_hub_cache/resolve/main/huggingface-cache.tar.gz && apt-get install pigz && tar --use-compress-program="pigz -d -p 8" -xf huggingface-cache.tar.gz && mv -n hub/* /root/.cache/huggingface/hub/ && ls -la /root/.cache/huggingface/hub/'}},
             {"run": {
                 "name": "Run tests",
-                "command": f"({timeout_cmd} python3 -m pytest {marker_cmd} -n {self.pytest_num_workers} {junit_flags} {repeat_on_failure_flags} {' '.join(pytest_flags)} tests/models/bert/test_tokenization_bert.py tests/models/distilbert/test_tokenization_distilbert.py tests/models/mluke/test_tokenization_mluke.py tests/models/splinter/test_tokenization_splinter.py tests/models/pop2piano/test_tokenization_pop2piano.py | tee tests_output.txt)"}
+                "command": f"({timeout_cmd} python3 -m pytest {marker_cmd} -n {self.pytest_num_workers} {junit_flags} {repeat_on_failure_flags} {' '.join(pytest_flags)} tests/models/mluke/test_tokenization_mluke.py | tee tests_output.txt)"}
             },
             {"run":
                 {
