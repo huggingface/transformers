b2618b3d50da602530c84966e23bd6b4c18224a7
Pablo Montalvo <pablo.montalvo.leroux@gmail.com>
2025-12-02 19:09:42 +0100

    this is needed likely

--- src/transformers/models/layoutlmv3/modeling_layoutlmv3.py
+++ src/transformers/models/layoutlmv3/modeling_layoutlmv3.py
@@ -883,6 +883,12 @@ class LayoutLMv3ForTokenClassification(LayoutLMv3PreTrainedModel):
 
         self.post_init()
 
+    def get_input_embeddings(self):
+        return self.layoutlmv3.get_input_embeddings()
+
+    def set_input_embeddings(self, value):
+        self.layoutlmv3.set_input_embeddings(value)
+
     @auto_docstring
     def forward(
         self,
@@ -981,6 +987,12 @@ class LayoutLMv3ForQuestionAnswering(LayoutLMv3PreTrainedModel):
         self.qa_outputs = LayoutLMv3ClassificationHead(config, pool_feature=False)
 
         self.post_init()
+
+    def get_input_embeddings(self):
+        return self.layoutlmv3.get_input_embeddings()
+
+    def set_input_embeddings(self, value):
+        self.layoutlmv3.set_input_embeddings(value)
 
     @auto_docstring
     def forward(
@@ -1100,6 +1112,12 @@ class LayoutLMv3ForSequenceClassification(LayoutLMv3PreTrainedModel):
         self.classifier = LayoutLMv3ClassificationHead(config, pool_feature=False)
 
         self.post_init()
+
+    def get_input_embeddings(self):
+        return self.layoutlmv3.get_input_embeddings()
+
+    def set_input_embeddings(self, value):
+        self.layoutlmv3.set_input_embeddings(value)
 
     @auto_docstring
     def forward(
