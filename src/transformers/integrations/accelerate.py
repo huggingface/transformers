08fd64d0c0dc120dd9618700607078ecf036b0a5
Cyril Vallez <cyril.vallez@huggingface.co>
2025-12-05 18:33:51 +0100

    [loading] Correctly load params during offloading & careful memory considerations (#42632)
    * do not load everything in advance
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix memory leaks during conversion
    
    * oupsi
    
    * fix device_map
    
    * add doc
    
    * fix
    
    * doc
    
    * make it a method
    
    * doc
    
    * first shot at test
    
    * fix test
    
    * fix
    
    * revert test: cpu mem too hard to track correctly
    
    * fix

--- src/transformers/integrations/accelerate.py
+++ src/transformers/integrations/accelerate.py
@@ -392,6 +392,15 @@ def _get_device_map(
             )
         else:
             inferred_max_memory = get_max_memory(max_memory)
+
+        # If the user does not provide `max_memory`, accelerate sets the WHOLE cpu available memory as available.
+        # This is unwanted, as we don't want to set extremely tight bound and pressure for cpu if we are memory-constrained,
+        # especially if the model uses WeightConverter (because there will be some uncontrollable cpu memory spikes during
+        # the conversions before we resave the weights). In those cases, it's better to offload to disk a bit more
+        # if we were in-between, as otherwise we blow-up cpu memory
+        if max_memory is None:
+            inferred_max_memory["cpu"] *= 0.90
+
         if hf_quantizer is not None:
             inferred_max_memory = hf_quantizer.adjust_max_memory(inferred_max_memory)
 
