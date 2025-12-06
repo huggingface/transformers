1c4e9ad5c3c53a7910be9d3e6c4df281739fdf54
ydshieh <ydshieh@users.noreply.github.com>
2025-12-06 20:06:08 +0100

    run : all

--- run_failed_tests.py
+++ run_failed_tests.py
@@ -49,7 +49,7 @@ failed_tests = [
 ]
 
 import os
-for idx, failed_test in enumerate(failed_tests[21:]):
+for idx, failed_test in enumerate(failed_tests[:]):
     print(f"Running test {idx}: {failed_test}")
     output_dir = "captured"
     os.makedirs(output_dir, exist_ok=True)
