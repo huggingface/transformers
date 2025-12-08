4ec42008c3771c821c13d541602aad5e336e27fd
Arthur <arthur.zucker@gmail.com>
2025-12-08 17:01:51 +0100

    style

--- src/transformers/processing_utils.py
+++ src/transformers/processing_utils.py
@@ -1472,7 +1472,9 @@ class ProcessorMixin(PushToHubMixin):
 
                 from .models.llama import LlamaTokenizer
 
-                tokenizer = LlamaTokenizer.from_pretrained(pretrained_model_name_or_path, add_bos_token=False, **kwargs)
+                tokenizer = LlamaTokenizer.from_pretrained(
+                    pretrained_model_name_or_path, add_bos_token=False, **kwargs
+                )
                 tokenizer._tokenizer.pre_tokenizer = pre_tokenizers.Sequence(
                     [pre_tokenizers.ByteLevel(False), tokenizer._tokenizer.pre_tokenizer]
                 )
