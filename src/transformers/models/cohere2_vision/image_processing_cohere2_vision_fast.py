c9758341eb1e58e7a1d189dccd8f52643eab2b1b
dongluw <108290936+dongluw@users.noreply.github.com>
2025-12-10 08:31:27 -0500

    Command-a-vision fix (#42642)
    * Add test case and update image processing
    
    * Apply suggestions from code review
    
    * improve naming

--- src/transformers/models/cohere2_vision/image_processing_cohere2_vision_fast.py
+++ src/transformers/models/cohere2_vision/image_processing_cohere2_vision_fast.py
@@ -93,8 +93,9 @@ def get_optimal_tiled_canvas(
     patch_size_height, patch_size_width = target_tile_size  # (height == width)
 
     candidate_resolutions = np.array(possible_resolutions) * patch_size_height
-    original_size = np.stack([image_height, image_width])
-    required_scales = candidate_resolutions / original_size
+    # tiles following (width, height) order to align with aspect ratio convention
+    tile_size = np.stack([image_width, image_height])
+    required_scales = candidate_resolutions / tile_size
     required_scale = np.min(required_scales, axis=-1, keepdims=True)  # [n_resolutions, 1]
     if np.all(required_scale < 1):
         # We are forced to downscale, so try to minimize the amount of downscaling
@@ -103,7 +104,7 @@ def get_optimal_tiled_canvas(
         # Pick the resolution that required the least upscaling so that it most closely fits the image
         required_scale = np.where(required_scale < 1.0, 10e9, required_scale)
         best_grid = possible_resolutions[np.argmin(required_scale)]
-    return best_grid
+    return best_grid  # (width, height)
 
 
 @auto_docstring
