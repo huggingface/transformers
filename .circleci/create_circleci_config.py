4c062ae32a4c38c930d845e698298d112ca208d1
3outeille <ferdinand.mom@epita.fr>
2025-12-10 11:13:28 +0000

    update naming

--- .circleci/create_circleci_config.py
+++ .circleci/create_circleci_config.py
@@ -327,12 +327,12 @@ training_ci_job = CircleCIJob(
     parallelism=6,
 )
 
-distributed_training_ci_job = CircleCIJob(
-    "distributed_training_ci",
+training_distributed_ci_job = CircleCIJob(
+    "training_distributed_ci",
     additional_env={"RUN_TRAINING_TESTS": True},
     docker_image=[{"image": "huggingface/transformers-torch-light"}],
     install_steps=["uv pip install ."],
-    marker="is_distributed_training_test",
+    marker="is_training_distributed_test",
     parallelism=6,
 )
 
@@ -365,7 +365,7 @@ EXAMPLES_TESTS = [examples_torch_job]
 PIPELINE_TESTS = [pipelines_torch_job]
 REPO_UTIL_TESTS = [repo_utils_job]
 DOC_TESTS = [doc_test_job]
-TRAINING_CI_TESTS = [training_ci_job, distributed_training_ci_job]
+TRAINING_CI_TESTS = [training_ci_job, training_distributed_ci_job]
 ALL_TESTS = REGULAR_TESTS + EXAMPLES_TESTS + PIPELINE_TESTS + REPO_UTIL_TESTS + DOC_TESTS + [custom_tokenizers_job] + [exotic_models_job] + TRAINING_CI_TESTS  # fmt: skip
 
 
