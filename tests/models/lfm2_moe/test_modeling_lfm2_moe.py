8962ff162da2700dd7256800e411c127fb55ee1f
YangKai0616 <kai.yang@intel.com>
2025-12-11 02:21:27 +0000

    Update flm2 tests GT for XPU

--- tests/models/lfm2_moe/test_modeling_lfm2_moe.py
+++ tests/models/lfm2_moe/test_modeling_lfm2_moe.py
@@ -177,7 +177,7 @@ class Lfm2MoeIntegrationTest(unittest.TestCase):
         EXPECTED_MEANS = Expectations(
             {
                 ("cuda", None): torch.tensor([[-1.3855, -0.5123, -1.3143, -1.2144, -1.0791, -1.2117, -1.4704, -0.7648, -0.6175, -1.2402, -1.1459, -1.0083, -1.0247, -0.8830, -1.5643, -1.7266, -1.6254,]]),
-                ("xpu", None): torch.tensor([[-1.3863, -0.4653, -1.3246, -1.3199, -1.0940, -1.2254, -1.4716, -0.8852, -0.5920, -1.2182, -1.1782, -1.0268, -1.0114, -0.8816, -1.5774, -1.7408, -1.6147,]]),
+                ("xpu", None): torch.tensor([[-1.3879, -0.4730, -1.3193, -1.3139, -1.0826, -1.2129, -1.4744, -0.7485, -0.6004, -1.2353, -1.1602, -1.0432, -1.0180, -0.9099, -1.5949, -1.7487, -1.5991]]),
             }
         )
         # fmt: on
@@ -189,7 +189,7 @@ class Lfm2MoeIntegrationTest(unittest.TestCase):
         EXPECTED_SLICES = Expectations(
             {
                 ("cuda", None): torch.tensor([-1.2656, 2.4844, 5.5000, -1.3359, -1.3203, -1.3438, 1.9375, 5.8438, -0.6523, -1.2891]),
-                ("xpu", None): torch.tensor([-1.2656, 2.4531, 5.4375, -1.3438, -1.3203, -1.3516, 1.9297, 5.7812, -0.6719, -1.3203]),
+                ("xpu", None): torch.tensor([-1.2734,  2.4531, 5.4688, -1.3438, -1.3281, -1.3516, 1.9297, 5.7812, -0.6719, -1.3125]),
             }
         )
         # fmt: on
@@ -223,10 +223,10 @@ class Lfm2MoeIntegrationTest(unittest.TestCase):
                                  "Complete the text: Lorem ipsum dolor ipsum dolor ipsum dolor ipsum dolor ipsum dolor",
                                  "The Meji Restoration in Japan ended or the Meiji Restoration (1868–1912) marked a pivotal",
                                 ],
-                ("xpu", None): ['Who are you? (AI) designed to assist?  \nI am an AI assistant developed to',
-                                'Complete the text: Lorem ipsum dolor ipsum dolor ipsum dolor ipsum dolor ipsum dolor',
-                                'The Meji Restoration in Japan ended**  \n* **Key Event:** The overthrow of the Tokugawa'
-                               ],
+                ("xpu", None): ["Who are you? (AI) designed to assist?  \nI am an AI language model developed",
+                                "Complete the text: Lorem ipsum dolor ipsum dolor ipsum dolor ipsum dolor ipsum dolor",
+                                "The Meji Restoration in Japan ended, which occurred in 1868, marked the:  \nA) Establish"
+                                ],
             }
         )
         # fmt: on
