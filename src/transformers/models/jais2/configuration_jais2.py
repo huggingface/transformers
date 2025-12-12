506d4fdd8cdf08b56cc85719c91e68342154816c
sarathc-cerebras <sarath.chandran@cerebras.net>
2025-12-13 02:22:57 +0400

    updates layernorm setup

--- src/transformers/models/jais2/configuration_jais2.py
+++ src/transformers/models/jais2/configuration_jais2.py
@@ -28,9 +28,8 @@ from ...modeling_rope_utils import RopeParameters
 class Jais2Config(PreTrainedConfig):
     r"""
     This is the configuration class to store the configuration of a [`Jais2Model`]. It is used to instantiate a Jais2
-    model according to the specified arguments, defining the model architecture. Instantiating a configuration with the
-    defaults will yield a similar configuration to that of the Jais2
-    [inceptionai/Jais-2-8B-Chat](https://huggingface.co/inceptionai/Jais-2-8B-Chat) architecture.
+    model according to the specified arguments, defining the model architecture.
+    [inceptionai/Jais-2-8B-Chat](https://huggingface.co/inceptionai/Jais-2-8B-Chat).
 
     Configuration objects inherit from [`PreTrainedConfig`] and can be used to control the model outputs. Read the
     documentation from [`PreTrainedConfig`] for more information.
@@ -129,9 +128,6 @@ class Jais2Config(PreTrainedConfig):
         # If rope_parameters not provided, create default with rope_theta
         if rope_parameters is None:
             rope_parameters = RopeParameters(rope_theta=rope_theta)
-
-        # Define rms_norm_eps for the parent init to use
-        rms_norm_eps = layer_norm_eps
         self.vocab_size = vocab_size
         self.max_position_embeddings = max_position_embeddings
         self.hidden_size = hidden_size
@@ -146,7 +142,6 @@ class Jais2Config(PreTrainedConfig):
         self.num_key_value_heads = num_key_value_heads
         self.hidden_act = hidden_act
         self.initializer_range = initializer_range
-        self.rms_norm_eps = rms_norm_eps
         self.pretraining_tp = pretraining_tp
         self.use_cache = use_cache
         self.attention_bias = attention_bias
@@ -162,7 +157,7 @@ class Jais2Config(PreTrainedConfig):
             tie_word_embeddings=tie_word_embeddings,
             **kwargs,
         )
-        self.layer_norm_eps = self.rms_norm_eps
+        self.layer_norm_eps = layer_norm_eps
 
 
 __all__ = ["Jais2Config"]
