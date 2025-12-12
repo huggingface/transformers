b777f866cc2875e1a53eff4d7db9e5558553bb96
anaslari23 <anaslari610@gmail.com>
2025-12-12 06:27:37 +0530

    Format file to satisfy HuggingFace style rules

--- src/transformers/models/audio_spectrogram_transformer/feature_extraction_audio_spectrogram_transformer.py
+++ src/transformers/models/audio_spectrogram_transformer/feature_extraction_audio_spectrogram_transformer.py
@@ -151,7 +151,6 @@ class ASTFeatureExtractor(SequenceFeatureExtractor):
         return_tensors: Optional[Union[str, TensorType]] = None,
         **kwargs,
     ) -> BatchFeature:
-
         if sampling_rate is not None:
             if sampling_rate != self.sampling_rate:
                 raise ValueError(
