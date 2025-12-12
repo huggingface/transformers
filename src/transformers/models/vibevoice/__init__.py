91b638052ce6a63914a6a2b032966ba42d39889e
anaslari23 <anaslari610@gmail.com>
2025-12-12 18:46:10 +0530

    fix: Convert vibevoice/__init__.py to use define_import_structure pattern
    - Replaced manual _import_structure dictionary with define_import_structure(_file)
    - Simplified TYPE_CHECKING imports to use wildcard imports
    - This fixes check_repo.py detection of vibevoice module

--- src/transformers/models/vibevoice/__init__.py
+++ src/transformers/models/vibevoice/__init__.py
@@ -13,63 +13,17 @@
 # limitations under the License.
 from typing import TYPE_CHECKING
 
-from ...utils import (
-    OptionalDependencyNotAvailable,
-    _LazyModule,
-    is_torch_available,
-)
-
-
-_import_structure = {
-    "configuration_vibevoice": [
-        "VibeVoiceAcousticTokenizerConfig",
-        "VibeVoiceConfig",
-        "VibeVoiceDiffusionHeadConfig",
-        "VibeVoiceSemanticTokenizerConfig",
-    ],
-    "processing_vibevoice": ["VibeVoiceProcessor"],
-    "tokenization_vibevoice": ["VibeVoiceTokenizer", "VibeVoiceTokenizerFast"],
-}
-
-try:
-    if not is_torch_available():
-        raise OptionalDependencyNotAvailable()
-except OptionalDependencyNotAvailable:
-    pass
-else:
-    _import_structure["modeling_vibevoice"] = [
-        "VibeVoiceAcousticTokenizerModel",
-        "VibeVoiceForConditionalGeneration",
-        "VibeVoiceModel",
-        "VibeVoicePreTrainedModel",
-    ]
+from ...utils import _LazyModule
+from ...utils.import_utils import define_import_structure
 
 
 if TYPE_CHECKING:
-    from .configuration_vibevoice import (
-        VibeVoiceAcousticTokenizerConfig,
-        VibeVoiceConfig,
-        VibeVoiceDiffusionHeadConfig,
-        VibeVoiceSemanticTokenizerConfig,
-    )
-    from .processing_vibevoice import VibeVoiceProcessor
-    from .tokenization_vibevoice import VibeVoiceTokenizer, VibeVoiceTokenizerFast
-
-    try:
-        if not is_torch_available():
-            raise OptionalDependencyNotAvailable()
-    except OptionalDependencyNotAvailable:
-        pass
-    else:
-        from .modeling_vibevoice import (
-            VibeVoiceAcousticTokenizerModel,
-            VibeVoiceForConditionalGeneration,
-            VibeVoiceModel,
-            VibeVoicePreTrainedModel,
-        )
-
-
+    from .configuration_vibevoice import *
+    from .modeling_vibevoice import *
+    from .processing_vibevoice import *
+    from .tokenization_vibevoice import *
 else:
     import sys
 
-    sys.modules[__name__] = _LazyModule(__name__, globals()["__file__"], _import_structure, module_spec=__spec__)
+    _file = globals()["__file__"]
+    sys.modules[__name__] = _LazyModule(__name__, _file, define_import_structure(_file), module_spec=__spec__)
