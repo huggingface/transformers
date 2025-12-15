64bd20b44a2b85ab5b5fde46df4393c6faee1c0b
vasqu <antonprogamer@gmail.com>
2025-12-15 11:44:01 +0100

    fix expectation from dtype casting

--- tests/models/ernie4_5_moe/test_modeling_ernie4_5_moe.py
+++ tests/models/ernie4_5_moe/test_modeling_ernie4_5_moe.py
@@ -189,7 +189,7 @@ class Ernie4_5_MoeIntegrationTest(unittest.TestCase):
 
     def test_shortened_model_generation(self):
         # This is gibberish which is expected as the model are the first x layers of the original 28B model
-        EXPECTED_TEXT_COMPLETION = 'User: Hey, are you conscious? Can you talk to me?\nAssistant: 不了的شم尔斯graveyard效应osm osmos乎哉哉哉哉 bargaining程度level打好莱坞制片制片amme瑙瑙��eka地步chansesaurian'  # fmt: skip
+        EXPECTED_TEXT_COMPLETION = 'User: Hey, are you conscious? Can you talk to me?\nAssistant: 不了的 tongues说话 dagat绵席裹着头phones<mask:11>odikèkèk<mask:11><mask:11>bun褶席席地说起来这么说的话的话retti upside upsideolate疡疡疡'  # fmt: skip
 
         model = self.get_small_model()
         tokenizer = AutoTokenizer.from_pretrained("baidu/ERNIE-4.5-21B-A3B-PT")
