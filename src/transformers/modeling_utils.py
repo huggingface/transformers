4b84c54bcdd320d9976666e8355c44acfda85f56
Marc Sun <57196510+SunMarc@users.noreply.github.com>
2025-12-16 12:35:27 +0100

    Update src/transformers/modeling_utils.py
    Co-authored-by: Mohamed Mekkouri <93391238+MekkCyber@users.noreply.github.com>

--- src/transformers/modeling_utils.py
+++ src/transformers/modeling_utils.py
@@ -4001,7 +4001,7 @@ class PreTrainedModel(nn.Module, EmbeddingAccessMixin, ModuleUtilsMixin, PushToH
 
         if hf_quantizer is not None:
             model.hf_quantizer = hf_quantizer
-            hf_quantizer.postprocess_model(model)  # usually a no-op but sometimes needed
+            hf_quantizer.postprocess_model(model)  # usually a no-op but sometimes needed, e.g to remove the quant config when dequantizing 
 
         if _adapter_model_path is not None:
             adapter_kwargs["key_mapping"] = key_mapping
