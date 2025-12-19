67d14937e5d0481f243db90c49c70f9b4cd41f2d
priyankaz-ml <0509priyankasharma@gmail.com>
2025-12-19 23:30:33 +0530

    Refactor evaluation method docstring
    Updated docstring for evaluation method to clarify arguments and usage.

--- src/transformers/trainer.py
+++ src/transformers/trainer.py
@@ -4231,44 +4231,24 @@ class Trainer:
         ignore_keys: list[str] | None = None,
         metric_key_prefix: str = "eval",
     ) -> dict[str, float]:
-        """
-        Run evaluation and returns metrics.
+       """
+    Run evaluation and returns metrics.
 
-        The calling script will be responsible for providing a method to compute metrics, as they are task-dependent
-        (pass it to the init `compute_metrics` argument).
+    The calling script will be responsible for providing a method to compute metrics, as they are task-dependent
+    (pass it to the init `compute_metrics` argument).
 
-        You can also subclass and override this method to inject custom behavior.
+    You can also subclass and override this method to inject custom behavior.
 
-        Args:
-            eval_dataset (Union[`Dataset`, dict[str, `Dataset`]], *optional*):
-                Pass a dataset if you wish to override `self.eval_dataset`. If it is a [`~datasets.Dataset`], columns
-                not accepted by the `model.forward()` method are automatically removed. If it is a dictionary, it will
-                evaluate on each dataset, prepending the dictionary key to the metric name. Datasets must implement the
-                `__len__` method.
-
-                <Tip>
-
-                If you pass a dictionary with names of datasets as keys and datasets as values, evaluate will run
-                separate evaluations on each dataset. This can be useful to monitor how training affects other
-                datasets or simply to get a more fine-grained evaluation.
-                When used with `load_best_model_at_end`, make sure `metric_for_best_model` references exactly one
-                of the datasets. If you, for example, pass in `{"data1": data1, "data2": data2}` for two datasets
-                `data1` and `data2`, you could specify `metric_for_best_model="eval_data1_loss"` for using the
-                loss on `data1` and `metric_for_best_model="eval_data2_loss"` for the loss on `data2`.
-
-                </Tip>
-
-            ignore_keys (`list[str]`, *optional*):
-                A list of keys in the output of your model (if it is a dictionary) that should be ignored when
-                gathering predictions.
-            metric_key_prefix (`str`, *optional*, defaults to `"eval"`):
-                An optional prefix to be used as the metrics key prefix. For example the metrics "bleu" will be named
-                "eval_bleu" if the prefix is "eval" (default)
-
-        Returns:
-            A dictionary containing the evaluation loss and the potential metrics computed from the predictions. The
-            dictionary also contains the epoch number which comes from the training state.
-        """
+    Args:
+        eval_dataset (Union[Dataset, dict[str, Dataset]], *optional*):
+            Pass a dataset if you wish to override `self.eval_dataset`. If it is a [`~datasets.Dataset`], columns not
+            accepted by the `model.forward()` method are automatically removed. If it is a dictionary, it will evaluate
+            on each dataset, prepending the dictionary key to the metric name. Datasets must implement the
+            `__len__` method.
+        max_eval_steps (`int`, *optional*):
+            If set, limits the number of batches used during evaluation. Evaluation will stop once this threshold
+            is reached, even if the dataset has more batches.
+    """
         # handle multiple eval datasets
         override = eval_dataset is not None
         eval_dataset = eval_dataset if override else self.eval_dataset
