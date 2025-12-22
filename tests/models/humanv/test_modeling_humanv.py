414a7ff8b2e505a724f06f5fc822de56fe8c3bb2
ZagrosLLMModel <yrah7430@gmail.com>
2025-12-22 15:17:00 +0330

    Add HumanV tests for all model classes

--- tests/models/humanv/test_modeling_humanv.py
+++ tests/models/humanv/test_modeling_humanv.py
@@ -1,11 +1,26 @@
 import unittest
 
-from transformers import HumanVConfig, HumanVForCausalLM
+from transformers import (
+    HumanVConfig,
+    HumanVForCausalLM,
+    HumanVForQuestionAnswering,
+    HumanVForSequenceClassification,
+    HumanVForTokenClassification,
+    HumanVModel,
+)
 from transformers.testing_utils import require_torch
 from transformers.tests.test_modeling_common import ModelTesterMixin, ids_tensor
 
 
-all_model_classes = (HumanVForCausalLM,)
+all_model_classes = (
+    HumanVModel,
+    HumanVForCausalLM,
+    HumanVForSequenceClassification,
+    HumanVForTokenClassification,
+    HumanVForQuestionAnswering,
+)
+
+all_generative_model_classes = (HumanVForCausalLM,)
 
 
 class HumanVModelTester:
@@ -21,6 +36,7 @@ class HumanVModelTester:
         self.num_key_value_heads = 4
         self.head_dim = 8
         self.max_position_embeddings = 64
+        self.num_labels = 3
 
     def get_config(self):
         return HumanVConfig(
@@ -34,6 +50,7 @@ class HumanVModelTester:
             max_position_embeddings=self.max_position_embeddings,
             layer_types=["full_attention"] * self.num_hidden_layers,
             use_cache=False,
+            num_labels=self.num_labels,
             bos_token_id=0,
             eos_token_id=1,
             pad_token_id=1,
@@ -44,22 +61,52 @@ class HumanVModelTester:
         input_ids = ids_tensor([self.batch_size, self.seq_length], config.vocab_size)
         return config, input_ids
 
-    def create_and_check_model(self, config, input_ids):
-        model = HumanVForCausalLM(config).eval()
-        outputs = model(input_ids=input_ids)
-        self.parent.assertEqual(outputs.logits.shape, (self.batch_size, self.seq_length, config.vocab_size))
-
 
 @require_torch
 class HumanVModelTest(ModelTesterMixin, unittest.TestCase):
     all_model_classes = all_model_classes
+    all_generative_model_classes = all_generative_model_classes
 
     def setUp(self):
         self.model_tester = HumanVModelTester(self)
 
-    def test_model(self):
+    def test_base_model_output_shape(self):
         config, input_ids = self.model_tester.prepare_config_and_inputs()
-        self.model_tester.create_and_check_model(config, input_ids)
+        model = HumanVModel(config).eval()
+        outputs = model(input_ids=input_ids)
+        self.assertEqual(
+            outputs.last_hidden_state.shape,
+            (self.model_tester.batch_size, self.model_tester.seq_length, config.hidden_size),
+        )
+
+    def test_causal_lm_output_shape(self):
+        config, input_ids = self.model_tester.prepare_config_and_inputs()
+        model = HumanVForCausalLM(config).eval()
+        outputs = model(input_ids=input_ids)
+        self.assertEqual(
+            outputs.logits.shape, (self.model_tester.batch_size, self.model_tester.seq_length, config.vocab_size)
+        )
+
+    def test_sequence_classification_output_shape(self):
+        config, input_ids = self.model_tester.prepare_config_and_inputs()
+        model = HumanVForSequenceClassification(config).eval()
+        outputs = model(input_ids=input_ids)
+        self.assertEqual(outputs.logits.shape, (self.model_tester.batch_size, config.num_labels))
+
+    def test_token_classification_output_shape(self):
+        config, input_ids = self.model_tester.prepare_config_and_inputs()
+        model = HumanVForTokenClassification(config).eval()
+        outputs = model(input_ids=input_ids)
+        self.assertEqual(
+            outputs.logits.shape, (self.model_tester.batch_size, self.model_tester.seq_length, config.num_labels)
+        )
+
+    def test_question_answering_output_shape(self):
+        config, input_ids = self.model_tester.prepare_config_and_inputs()
+        model = HumanVForQuestionAnswering(config).eval()
+        outputs = model(input_ids=input_ids)
+        self.assertEqual(outputs.start_logits.shape, (self.model_tester.batch_size, self.model_tester.seq_length))
+        self.assertEqual(outputs.end_logits.shape, (self.model_tester.batch_size, self.model_tester.seq_length))
 
     def test_generate(self):
         config, input_ids = self.model_tester.prepare_config_and_inputs()
