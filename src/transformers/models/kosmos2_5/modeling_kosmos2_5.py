ccc7d90bcb4a94cd0888925c6abac941f3b8151d
Raushan Turganbay <raushan@huggingface.co>
2025-12-23 08:35:51 +0100

    Prefill-related logic in input preparation for generation (#42088)
    * add prefill arg in generation
    
    * add a slow test
    
    * fix copies
    
    * can be like this but checking special tokens isn't good
    
    * ig this solves the issue with assisted_gen+prefill
    
    * update overwritten `prepare_inpits_for_generation`
    
    * prefill is actually when we have no cache at all.. Try this for now
    
    * first iteration is not always techincally same as prefill
    
    * fix?
    
    * fix now?
    
    * update bloom
    
    * fix smth
    
    * make style
    
    * fix copies and skip test
    
    * fix copies
    
    * tiny updates after a review
    
    * fix other slow tests
    
    * fix copies
    
    * do not pass the same kwargs twice in prefill
    
    * oops
    
    * have to revert? prob fails only on dgx
    
    * adjust slow test again
    
    * address comments
    
    * fix copies

--- src/transformers/models/kosmos2_5/modeling_kosmos2_5.py
+++ src/transformers/models/kosmos2_5/modeling_kosmos2_5.py
@@ -1608,6 +1608,7 @@ class Kosmos2_5TextForCausalLM(Kosmos2_5PreTrainedModel):
         use_cache=None,
         cache_position=None,
         position_ids=None,
+        is_first_iteration=False,
         **model_kwargs,
     ):
         input_shape = input_ids.shape
@@ -1812,6 +1813,7 @@ class Kosmos2_5ForConditionalGeneration(Kosmos2_5PreTrainedModel, GenerationMixi
         use_cache=None,
         cache_position=None,
         position_ids=None,
+        is_first_iteration=False,
         **model_kwargs,
     ):
         # Overwritten -- in specific circumstances we don't want to forward image inputs to the model
@@ -1825,10 +1827,11 @@ class Kosmos2_5ForConditionalGeneration(Kosmos2_5PreTrainedModel, GenerationMixi
             use_cache=use_cache,
             cache_position=cache_position,
             position_ids=position_ids,
+            is_first_iteration=is_first_iteration,
             **model_kwargs,
         )
 
-        if cache_position[0] == 0:
+        if is_first_iteration or not use_cache:
             # If we're in cached decoding stage, `flattened_patches` should be `None` because `input_ids` do not contain special image token anymore
             # Otherwise we need `flattened_patches` to be passed to model
             model_inputs["flattened_patches"] = flattened_patches
