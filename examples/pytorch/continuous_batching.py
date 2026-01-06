d949fc4e23a4f484151c384a7f537f8d5f236b85
remi-or <remi.pierre_o@orange.fr>
2026-01-06 15:00:22 +0000

    QOL for debugging

--- examples/pytorch/continuous_batching.py
+++ examples/pytorch/continuous_batching.py
@@ -14,6 +14,7 @@
 import argparse
 import contextlib
 import json
+import logging
 import os
 import time
 from itertools import cycle
@@ -323,14 +324,15 @@ if __name__ == "__main__":
             f"runs/cb/{args.num_blocks}_{args.max_batch_tokens}_{attn}_{args.matmul_precision}_{args.samples}.json"
         )
 
-    # Run warmup batch generation # TODO: understand why warmup incurs a large overhead during cache creation
-    batch_generate(
-        model,
-        batched_inputs[: min(5, args.samples)],
-        generation_cfg,
-        tokenizer,
-        displayed_samples=-1,
-    )
+    # Run warmup batch generation if log level is above DEBUG # TODO: understand why warmup incurs a large overhead during cache creation
+    if logger.level > logging.DEBUG:
+        batch_generate(
+            model,
+            batched_inputs[: min(5, args.samples)],
+            generation_cfg,
+            tokenizer,
+            displayed_samples=-1,
+        )
 
     if args.profile is not None:
         cm = profile(activities=[ProfilerActivity.CPU, ProfilerActivity.CUDA], record_shapes=True)
