50c20f13e7cd6ecd9881b767d2602d28064c8924
Arthur <arthur.zucker@gmail.com>
2026-01-07 16:48:00 +0100

    style

--- tests/models/auto/test_tokenization_auto.py
+++ tests/models/auto/test_tokenization_auto.py
@@ -11,7 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from numba.tests.test_globals import tmp
 
 import importlib
 import json
@@ -617,4 +616,3 @@ class NopConfig(PreTrainedConfig):
 
         tok = AutoTokenizer.from_pretrained("HuggingFaceTB/SmolLM2-135M-Instruct")
         self.assertTrue(tok.__class__ == TokenizersBackend)
-
