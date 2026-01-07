a621ad52bebd897bf10180ef37fda2765d890696
IlyasMoutawwakil <moutawwakil.ilyas.tsi@gmail.com>
2026-01-07 10:53:13 +0100

    info once

--- src/transformers/generation/utils.py
+++ src/transformers/generation/utils.py
@@ -2199,7 +2199,7 @@ class GenerationMixin(ContinuousMixin):
     def _optimize_model_for_decode(self):
         original_experts_implementation = self.config._experts_implementation
         if original_experts_implementation == "grouped_mm":
-            logger.warning_once(
+            logger.info_once(
                 "We will be switching to 'batched_mm' for the decoding stage as it is much more performant than 'grouped_mm' on smaller inputs. "
                 "If you experience any issues with this, please open an issue on the Hugging Face Transformers GitHub repository.",
             )
