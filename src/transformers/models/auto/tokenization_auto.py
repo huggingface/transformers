014919a60d7f7d3821e3049e569448f527f62cf8
LuJunru <junrulu@tencent.com>
2026-01-08 20:00:42 +0800

    correct unnecessary tokenizer annotation

--- src/transformers/models/auto/tokenization_auto.py
+++ src/transformers/models/auto/tokenization_auto.py
@@ -309,7 +309,6 @@ TOKENIZER_MAPPING_NAMES = OrderedDict[str, str | None](
         ("xlstm", "GPTNeoXTokenizer" if is_tokenizers_available() else None),
         ("xmod", "XLMRobertaTokenizer" if is_tokenizers_available() else None),
         ("yoso", "AlbertTokenizer" if is_tokenizers_available() else None),
-        ("youtu_llm", "TokenizersBackend" if is_tokenizers_available() else None),
     ]
 )
 
