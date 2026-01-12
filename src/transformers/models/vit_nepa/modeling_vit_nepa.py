fix: attention masking position

src/transformers/models/vit_nepa/modeling_vit_nepa.py
@@ -344,7 +344,7 @@ def eager_attention_forward(
     query: torch.Tensor,
     key: torch.Tensor,
     value: torch.Tensor,
-    attention_mask: Optional[torch.Tensor],
+    attention_mask: Optional[torch.BoolTensor] = None,
     scaling: Optional[float] = None,
     is_causal: bool = False,
     dropout: float = 0.0,
@@ -363,16 +363,16 @@ def eager_attention_forward(
         causal_mask = torch.triu(causal_mask, diagonal=1)
         attn_weights = attn_weights + causal_mask
 
+    if attention_mask is not None:
+        attention_mask = attention_mask[:, :, :, : key.shape[-2]]
+        attn_weights = attn_weights + attention_mask
+
     # Normalize the attention scores to probabilities.
     attn_weights = nn.functional.softmax(attn_weights, dim=-1, dtype=torch.float32).to(query.dtype)
 
     # This is actually dropping out entire tokens to attend to, which might
     # seem a bit unusual, but is taken from the original Transformer paper.
     attn_weights = nn.functional.dropout(attn_weights, p=dropout, training=module.training)
-
-    # Mask heads if we want to
-    if attention_mask is not None:
-        attn_weights = attn_weights * attention_mask
 
     attn_output = torch.matmul(attn_weights, value)
     attn_output = attn_output.transpose(1, 2).contiguous()