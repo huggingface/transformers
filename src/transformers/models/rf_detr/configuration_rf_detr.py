6fb11f1feaec23021eca8a89f88f7fd5a6dd5f5f
Steven Bucaille <steven.bucaille@gmail.com>
2026-01-13 22:35:11 +0100

    style: make style

--- src/transformers/models/rf_detr/configuration_rf_detr.py
+++ src/transformers/models/rf_detr/configuration_rf_detr.py
@@ -18,6 +18,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+
 from ...configuration_utils import PreTrainedConfig
 from ...utils import logging
 from ...utils.backbone_utils import (
