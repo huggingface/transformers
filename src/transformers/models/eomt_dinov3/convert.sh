9e24c23bda2d7037686ff4a5973748e43a828e2e
Niels Rogge <niels.rogge1@gmail.com>
2026-01-15 13:42:17 +0100

    Remove script

--- src/transformers/models/eomt_dinov3/convert.sh
+++
@@ -1,5 +0,0 @@
-python convert_eomt_dinov3_to_hf.py \
-    --model-id tue-mps/coco_panoptic_eomt_small_640_dinov3 \
-    --output-dir . \
-    --verify \
-    --original-repo-path /Users/nielsrogge/Documents/python_projects/eomt
