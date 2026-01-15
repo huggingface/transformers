5a8f8621d7876cc2c5b91616f512b1063e645082
oesni <inseo@upstage.ai>
2026-01-15 02:27:17 +0000

    chore: update lincse year -> 2026

--- src/transformers/models/solar_open/configuration_solar_open.py
+++ src/transformers/models/solar_open/configuration_solar_open.py
@@ -4,7 +4,7 @@
 #             the file from the modular. If any change should be done, please apply the change to the
 #                          modular_solar_open.py file directly. One of our CI enforces this.
 #                🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨
-# Copyright 2025 Upstage and HuggingFace Inc. team.
+# Copyright 2026 Upstage and HuggingFace Inc. team.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
