f20ec335de32dcf3151fca4833bad32bc6320b14
Tarek Ziade <tarek@ziade.org>
2026-01-22 08:53:21 +0100

    remove unecessary ignores

--- src/transformers/utils/type_validators.py
+++ src/transformers/utils/type_validators.py
@@ -92,20 +92,20 @@ def video_metadata_validator(value: VideoMetadataType | None = None):
 
     valid_keys = ["total_num_frames", "fps", "width", "height", "duration", "video_backend", "frames_indices"]
 
-    def check_dict_keys(d: dict) -> bool:
+    def check_dict_keys(d: dict[str, Any]) -> bool:
         return all(key in valid_keys for key in d.keys())
 
     if isinstance(value, Sequence) and isinstance(value[0], Sequence) and isinstance(value[0][0], dict):
         for sublist in value:
             for item in sublist:
-                if not check_dict_keys(item):  # type: ignore[arg-type]
+                if not check_dict_keys(item):
                     raise ValueError(
                         f"Invalid keys found in video metadata. Valid keys: {valid_keys} got: {list(item.keys())}"
                     )
 
     elif isinstance(value, Sequence) and isinstance(value[0], dict):
         for item in value:
-            if not check_dict_keys(item):  # type: ignore[arg-type]
+            if not check_dict_keys(item):
                 raise ValueError(
                     f"Invalid keys found in video metadata. Valid keys: {valid_keys} got: {list(item.keys())}"
                 )
