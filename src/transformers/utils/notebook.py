7d653909209cfdddc8ecad81282186d785bf781f
Tarek Ziade <tarek@ziade.org>
2026-01-22 09:45:16 +0100

    explicitely assert training_tracker, do not silently ignore

--- src/transformers/utils/notebook.py
+++ src/transformers/utils/notebook.py
@@ -307,12 +307,12 @@ class NotebookProgressCallback(TrainerCallback):
 
     def on_step_end(self, args, state, control, **kwargs):
         epoch = int(state.epoch) if int(state.epoch) == state.epoch else f"{state.epoch:.2f}"
-        if self.training_tracker is not None:
-            self.training_tracker.update(
-                state.global_step + 1,
-                comment=f"Epoch {epoch}/{state.num_train_epochs}",
-                force_update=self._force_next_update,
-            )
+        assert self.training_tracker is not None, "on_train_begin must be called before on_step_end"
+        self.training_tracker.update(
+            state.global_step + 1,
+            comment=f"Epoch {epoch}/{state.num_train_epochs}",
+            force_update=self._force_next_update,
+        )
         self._force_next_update = False
 
     def on_prediction_step(self, args, state, control, eval_dataloader=None, **kwargs):
@@ -338,50 +338,50 @@ class NotebookProgressCallback(TrainerCallback):
             values = {"Training Loss": logs["loss"]}
             # First column is necessarily Step sine we're not in epoch eval strategy
             values["Step"] = state.global_step
-            if self.training_tracker is not None:
-                self.training_tracker.write_line(values)
+            assert self.training_tracker is not None, "on_train_begin must be called before on_log"
+            self.training_tracker.write_line(values)
 
     def on_evaluate(self, args, state, control, metrics=None, **kwargs):
-        if self.training_tracker is not None:
-            values = {"Training Loss": "No log", "Validation Loss": "No log"}
-            for log in reversed(state.log_history):
-                if "loss" in log:
-                    values["Training Loss"] = log["loss"]
-                    break
+        assert self.training_tracker is not None, "on_train_begin must be called before on_evaluate"
+        values = {"Training Loss": "No log", "Validation Loss": "No log"}
+        for log in reversed(state.log_history):
+            if "loss" in log:
+                values["Training Loss"] = log["loss"]
+                break
 
-            if self.first_column == "Epoch":
-                values["Epoch"] = int(state.epoch)
-            else:
-                values["Step"] = state.global_step
-            if metrics is None:
-                metrics = {}
-            metric_key_prefix = "eval"
-            for k in metrics:
-                if k.endswith("_loss"):
-                    metric_key_prefix = re.sub(r"\_loss$", "", k)
-            _ = metrics.pop("total_flos", None)
-            _ = metrics.pop("epoch", None)
-            _ = metrics.pop(f"{metric_key_prefix}_runtime", None)
-            _ = metrics.pop(f"{metric_key_prefix}_samples_per_second", None)
-            _ = metrics.pop(f"{metric_key_prefix}_steps_per_second", None)
-            for k, v in metrics.items():
-                splits = k.split("_")
-                name = " ".join([part.capitalize() for part in splits[1:]])
-                if name == "Loss":
-                    # Single dataset
-                    name = "Validation Loss"
-                values[name] = v
-            self.training_tracker.write_line(values)
-            self.training_tracker.remove_child()
-            self.prediction_bar = None
-            # Evaluation takes a long time so we should force the next update.
-            self._force_next_update = True
+        if self.first_column == "Epoch":
+            values["Epoch"] = int(state.epoch)
+        else:
+            values["Step"] = state.global_step
+        if metrics is None:
+            metrics = {}
+        metric_key_prefix = "eval"
+        for k in metrics:
+            if k.endswith("_loss"):
+                metric_key_prefix = re.sub(r"\_loss$", "", k)
+        _ = metrics.pop("total_flos", None)
+        _ = metrics.pop("epoch", None)
+        _ = metrics.pop(f"{metric_key_prefix}_runtime", None)
+        _ = metrics.pop(f"{metric_key_prefix}_samples_per_second", None)
+        _ = metrics.pop(f"{metric_key_prefix}_steps_per_second", None)
+        for k, v in metrics.items():
+            splits = k.split("_")
+            name = " ".join([part.capitalize() for part in splits[1:]])
+            if name == "Loss":
+                # Single dataset
+                name = "Validation Loss"
+            values[name] = v
+        self.training_tracker.write_line(values)
+        self.training_tracker.remove_child()
+        self.prediction_bar = None
+        # Evaluation takes a long time so we should force the next update.
+        self._force_next_update = True
 
     def on_train_end(self, args, state, control, **kwargs):
-        if self.training_tracker is not None:
-            self.training_tracker.update(
-                state.global_step,
-                comment=f"Epoch {int(state.epoch)}/{state.num_train_epochs}",
-                force_update=True,
-            )
-            self.training_tracker = None
+        assert self.training_tracker is not None, "on_train_begin must be called before on_train_end"
+        self.training_tracker.update(
+            state.global_step,
+            comment=f"Epoch {int(state.epoch)}/{state.num_train_epochs}",
+            force_update=True,
+        )
+        self.training_tracker = None
