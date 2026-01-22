85452c0f732d9d3f5e0402456ef1434bc40961a8
Matt <rocketknight1@gmail.com>
2026-01-22 17:07:15 +0000

    Update copyright

--- tests/vlm_tester.py
+++ tests/vlm_tester.py
@@ -1,4 +1,4 @@
-# Copyright 2025 HuggingFace Inc.
+# Copyright 2026 HuggingFace Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
