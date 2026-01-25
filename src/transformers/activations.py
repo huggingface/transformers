93109de4f3206945cd6d7d40799115fb20c38636
Joshua Lochner <26504141+xenova@users.noreply.github.com>
2026-01-24 22:23:53 -0500

    Fix meta tensor loading
    NotImplementedError: Cannot copy out of meta tensor; no data!

--- src/transformers/activations.py
+++ src/transformers/activations.py
@@ -247,8 +247,8 @@ class XIELUActivation(nn.Module):
         self.register_buffer("eps", torch.tensor(eps, dtype=dtype))
         self.with_vector_loads = with_vector_loads
         # Temporary until xIELU CUDA fully implemented
-        self._beta_scalar = float(self.beta.detach().cpu().float().item())
-        self._eps_scalar = float(self.eps.detach().cpu().float().item())
+        self._beta_scalar = float(beta)
+        self._eps_scalar = float(eps)
 
         self._xielu_cuda_obj = None
         try:
