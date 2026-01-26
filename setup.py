6301b5034d4e14043a543b3228860331eea773a6
Arthur <arthur.zucker@gmail.com>
2026-01-26 16:39:05 +0100

    push dev version 5.0.1.dev0

--- setup.py
+++ setup.py
@@ -380,7 +380,7 @@ install_requires = [
 
 setup(
     name="transformers",
-    version="5.0.0.dev0",  # expected format is one of x.y.z.dev0, or x.y.z.rc1 or x.y.z (no to dashes, yes to dots)
+    version="5.0.1.dev0",  # expected format is one of x.y.z.dev0, or x.y.z.rc1 or x.y.z (no to dashes, yes to dots)
     author="The Hugging Face team (past and future) with the help of all our contributors (https://github.com/huggingface/transformers/graphs/contributors)",
     author_email="transformers@huggingface.co",
     description="Transformers: the model-definition framework for state-of-the-art machine learning models in text, vision, audio, and multimodal models, for both inference and training.",
