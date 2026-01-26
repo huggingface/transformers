9b68ca129b81b66bf8399fafa1ed7535677f4d72
Harisha P C <reach.harishapc@gmail.com>
2026-01-27 02:16:40 +0530

    Simplify __init__.py and fix formatting - final cleanup for PR submission

--- src/transformers/models/rish_ai/__init__.py
+++ src/transformers/models/rish_ai/__init__.py
@@ -11,67 +11,19 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 from typing import TYPE_CHECKING
 
-from transformers.utils import (
-    OptionalDependencyNotAvailable,
-    _LazyModule,
-    is_tokenizers_available,
-    is_torch_available,
-)
-
-
-_import_structure = {
-    "configuration_rish_ai": ["RishAIConfig"],
-}
-
-try:
-    if not is_torch_available():
-        raise OptionalDependencyNotAvailable()
-except OptionalDependencyNotAvailable:
-    pass
-else:
-    _import_structure["modeling_rish_ai"] = [
-        "RishAICausalLM",
-        "RishAIModel",
-        "RishAIPreTrainedModel",
-    ]
-
-try:
-    if not is_tokenizers_available():
-        raise OptionalDependencyNotAvailable()
-except OptionalDependencyNotAvailable:
-    pass
-else:
-    _import_structure["tokenization_rish_ai"] = ["RishAITokenizer"]
+from ...utils import _LazyModule
+from ...utils.import_utils import define_import_structure
 
 if TYPE_CHECKING:
-    from .configuration_rish_ai import RishAIConfig
-
-    try:
-        if not is_torch_available():
-            raise OptionalDependencyNotAvailable()
-    except OptionalDependencyNotAvailable:
-        pass
-    else:
-        from .modeling_rish_ai import (
-            RishAICausalLM,
-            RishAIModel,
-            RishAIPreTrainedModel,
-        )
-
-    try:
-        if not is_tokenizers_available():
-            raise OptionalDependencyNotAvailable()
-    except OptionalDependencyNotAvailable:
-        pass
-    else:
-        from .tokenization_rish_ai import RishAITokenizer
-
+    from .configuration_rish_ai import *  # noqa: F401, F403
+    from .modeling_rish_ai import *  # noqa: F401, F403
+    from .tokenization_rish_ai import *  # noqa: F401, F403
 else:
     import sys
 
+    _file = globals()["__file__"]
     sys.modules[__name__] = _LazyModule(
-        __name__, globals()["__file__"], _import_structure, module_spec=__spec__
+        __name__, _file, define_import_structure(_file), module_spec=__spec__
     )
--- src/transformers/models/rish_ai/tokenization_rish_ai.py
+++ src/transformers/models/rish_ai/tokenization_rish_ai.py
@@ -19,7 +19,6 @@ import json
 from transformers.tokenization_utils_base import PreTrainedTokenizerBase
 from transformers.utils import add_end_docstrings, logging
 
-
 logger = logging.get_logger(__name__)
 
 
