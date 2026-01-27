huggingface/transformers

commit 8dd9c999a6262d6ceb48f4a2da7acaccfa80e3bc

Use timm-side buffers initialization (#43124)
* make the switch

* pin version

* don't protect import if it is used without protection....

* add batchnorm2d

* fix

* fix

* oupsi

* use public API

* fix typo

src/transformers/models/timm_backbone/modeling_timm_backbone.py
@@ -16,22 +16,18 @@
 from typing import Optional, Union
 
 import torch
+from torch import Tensor, nn
 
+from ... import initialization as init
 from ...modeling_outputs import BackboneOutput
 from ...modeling_utils import PreTrainedModel
-from ...utils import is_timm_available, is_torch_available, requires_backends
+from ...utils import is_timm_available, requires_backends
 from ...utils.backbone_utils import BackboneMixin
 from .configuration_timm_backbone import TimmBackboneConfig
 
 
 if is_timm_available():
     import timm
-
-    from ...integrations.timm import _maybe_reinit_non_persistent_buffer
-
-
-if is_torch_available():
-    from torch import Tensor
 
 
 class TimmBackbone(PreTrainedModel, BackboneMixin):
@@ -121,7 +117,12 @@ class TimmBackbone(PreTrainedModel, BackboneMixin):
     def _init_weights(self, module):
         """We need to at least re-init the non-persistent buffers if the model was initialized on meta device (we
         assume weights and persistent buffers will be part of checkpoint as we have no way to control timm inits)"""
-        _maybe_reinit_non_persistent_buffer(module)
+        if hasattr(module, "init_non_persistent_buffers"):
+            module.init_non_persistent_buffers()
+        elif isinstance(module, nn.BatchNorm2d) and getattr(module, "running_mean", None) is not None:
+            init.zeros_(module.running_mean)
+            init.ones_(module.running_var)
+            init.zeros_(module.num_batches_tracked)
 
     def forward(
         self,

src/transformers/models/timm_wrapper/modeling_timm_wrapper.py
@@ -29,8 +29,6 @@ from .configuration_timm_wrapper import TimmWrapperConfig
 if is_timm_available():
     import timm
 
-    from ...integrations.timm import _maybe_reinit_non_persistent_buffer
-
 
 @dataclass
 @auto_docstring(
@@ -117,7 +115,12 @@ class TimmWrapperPreTrainedModel(PreTrainedModel):
             if module.bias is not None:
                 init.zeros_(module.bias)
         # Also, reinit all non-persistemt buffers if any!
-        _maybe_reinit_non_persistent_buffer(module)
+        if hasattr(module, "init_non_persistent_buffers"):
+            module.init_non_persistent_buffers()
+        elif isinstance(module, nn.BatchNorm2d) and getattr(module, "running_mean", None) is not None:
+            init.zeros_(module.running_mean)
+            init.ones_(module.running_var)
+            init.zeros_(module.num_batches_tracked)
 
     def _timm_model_supports_gradient_checkpointing(self):
         """

setup.py
@@ -140,7 +140,7 @@ _deps = [
     "tensorboard",
     "timeout-decorator",
     "tiktoken",
-    "timm>=1.0.20",
+    "timm>=1.0.23",
     "tokenizers>=0.22.0,<=0.23.0",
     "torch>=2.2",
     "torchaudio",

src/transformers/dependency_versions_table.py
@@ -75,7 +75,7 @@ deps = {
     "tensorboard": "tensorboard",
     "timeout-decorator": "timeout-decorator",
     "tiktoken": "tiktoken",
-    "timm": "timm>=1.0.20",
+    "timm": "timm>=1.0.23",
     "tokenizers": "tokenizers>=0.22.0,<=0.23.0",
     "torch": "torch>=2.2",
     "torchaudio": "torchaudio",

src/transformers/integrations/timm.py
@@ -1,216 +1,0 @@
-# coding=utf-8
-# Copyright 2025 The HuggingFace Inc. team. All rights reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-"""
-This integration has for unique goal to allow re-initialization of the non-persistent buffers of `timm` models.
-Indeed, as we load models fully on meta by default, we need a way to get back the correct value of non-persistent buffers.
-We assume that everything else, i.e. parameters and persistent buffers, will correctly reside in model checkpoints, so we
-won't need to reinit them.
-Do not rely on it, as we will work to integrate it directly in `timm`, to then remove this file without warning.
-"""
-
-from math import comb
-
-import torch
-
-from .. import initialization as init
-from ..utils import is_timm_available
-
-
-if is_timm_available():
-    from timm.layers import ndgrid
-    from timm.layers.blur_pool import BlurPool2d
-    from timm.layers.lambda_layer import LambdaLayer, rel_pos_indices
-    from timm.layers.pos_embed_rel import (
-        RelPosBias,
-        RelPosBiasTf,
-        RelPosMlp,
-        gen_relative_log_coords,
-        gen_relative_position_index,
-        generate_lookup_tensor,
-    )
-    from timm.layers.pos_embed_sincos import (
-        FourierEmbed,
-        RotaryEmbedding,
-        RotaryEmbeddingCat,
-        RotaryEmbeddingDinoV3,
-        RotaryEmbeddingMixed,
-        freq_bands,
-        pixel_freq_bands,
-    )
-    from timm.models.beit import Attention
-    from timm.models.beit import gen_relative_position_index as beit_gen_relative_position_index
-    from timm.models.efficientformer_v2 import Attention2d, Attention2dDownsample
-    from timm.models.eva import EvaAttention
-    from timm.models.levit import AttentionDownsample
-    from timm.models.swin_transformer import SwinTransformerBlock, get_relative_position_index
-    from timm.models.swin_transformer import WindowAttention as SwinWindowAttention
-    from timm.models.swin_transformer_v2 import SwinTransformerV2Block
-    from timm.models.swin_transformer_v2 import WindowAttention as Swin2WindowAttention
-    from timm.models.swin_transformer_v2_cr import SwinTransformerV2CrBlock, WindowMultiHeadAttention
-    from timm.models.vision_transformer import ParallelScalingBlock
-
-    # This one is very recent and is not necesarily in all versions we support (we require timm>=1.0.20)
-    try:
-        from timm.models.csatv2 import _DCT_MEAN, _DCT_VAR, LearnableDct2d
-    except Exception:
-        _DCT_MEAN, _DCT_VAR, LearnableDct2d = None, None, type(None)
-
-
-def _maybe_reinit_non_persistent_buffer(module):
-    """Reinit the non-persistent buffers of `module` if it matches any timm Module which has any."""
-    # This is a loooong list of hardcoded combinations from timm, as the modules do not provide a nice way to do
-    # it natively
-    if isinstance(module, FourierEmbed):
-        init.copy_(module.bands, pixel_freq_bands(module.max_res, module.num_bands))
-    elif isinstance(module, RotaryEmbedding):
-        if module.bands is not None:
-            bands = (
-                pixel_freq_bands(module.dim // 4, float(module.max_res), linear_bands=module.linear_bands)
-                if module.in_pixels
-                else freq_bands(module.dim // 4, temperature=module.temperature, step=1)
-            )
-            init.copy_(module.bands, bands)
-        elif module.pos_embed_sin is not None:
-            emb_sin, emb_cos = module._get_pos_embed_values(module.feat_shape)
-            init.copy_(module.pos_embed_sin, emb_sin)
-            init.copy_(module.pos_embed_cos, emb_cos)
-    elif isinstance(module, RotaryEmbeddingCat):
-        if module.bands is not None:
-            bands = (
-                pixel_freq_bands(module.dim // 4, float(module.max_res), linear_bands=module.linear_bands)
-                if module.in_pixels
-                else freq_bands(module.dim // 4, temperature=module.temperature, step=1)
-            )
-            init.copy_(module.bands, bands)
-        elif module.pos_embed is not None:
-            init.copy_(module.pos_embed, module._get_pos_embed_values(feat_shape=module.feat_shape))
-    elif isinstance(module, RotaryEmbeddingMixed):
-        if module.t_x is not None:
-            t_x, t_y = module._get_grid_values(module.feat_shape)
-            init.copy_(module.t_x, t_x)
-            init.copy(module.t_y, t_y)
-    elif isinstance(module, RotaryEmbeddingDinoV3):
-        init.copy_(module.periods, module._compute_periods())
-        if module.pos_embed_cached is not None:
-            init.copy_(module.pos_embed_cached, module._create_embed(module.feat_shape, no_aug=True))
-    elif isinstance(module, RelPosBias):
-        has_class_token = module.relative_position_bias_table.shape[0] > (2 * module.window_size[0] - 1) * (
-            2 * module.window_size[1] - 1
-        )
-        init.copy_(
-            module.relative_position_index,
-            gen_relative_position_index(module.window_size, class_token=has_class_token).view(-1),
-        )
-    elif isinstance(module, RelPosMlp):
-        init.copy_(module.relative_position_index, gen_relative_position_index(module.window_size).view(-1))
-        # This one is supposed to pass args `pretrained_window_size` as well to `gen_relative_log_coords`, but it's
-        # not recorded as class attributes in `__init__` and we have no way to infer its value back as we do for `mode` here...
-        # Let's hope it's always default value
-        mode = "cr" if module.bias_gain is None else "swin"
-        init.copy_(module.rel_coords_log, gen_relative_log_coords(module.window_size, mode=mode))
-    elif isinstance(module, RelPosBiasTf):
-        init.copy_(module.height_lookup, generate_lookup_tensor(module.window_size[0]))
-        init.copy_(module.width_lookup, generate_lookup_tensor(module.window_size[1]))
-    elif isinstance(module, LearnableDct2d):
-        init.copy_(module.mean, torch.tensor(_DCT_MEAN))
-        init.copy_(module.var, torch.tensor(_DCT_VAR))
-        init.copy_(module.imagenet_mean, torch.tensor([0.485, 0.456, 0.406]).view(3, 1, 1))
-        init.copy_(module.imagenet_std, torch.tensor([0.229, 0.224, 0.225]).view(3, 1, 1))
-    elif isinstance(module, LambdaLayer):
-        if module.rel_pos_indices is not None:
-            rel_size = module.pos_enb.shape[:2]
-            feat_size = [(s + 1) // 2 for s in rel_size]
-            init.copy_(module.rel_pos_indices, rel_pos_indices(feat_size))
-    elif isinstance(module, AttentionDownsample):
-        k_pos = torch.stack(
-            ndgrid(
-                torch.arange(module.resolution[0], dtype=torch.long),
-                torch.arange(module.resolution[1], dtype=torch.long),
-            )
-        ).flatten(1)
-        q_pos = torch.stack(
-            ndgrid(
-                torch.arange(0, module.resolution[0], step=module.stride, dtype=torch.long),
-                torch.arange(0, module.resolution[1], step=module.stride, dtype=torch.long),
-            )
-        ).flatten(1)
-        rel_pos = (q_pos[..., :, None] - k_pos[..., None, :]).abs()
-        rel_pos = (rel_pos[0] * module.resolution[1]) + rel_pos[1]
-        init.copy_(module.attention_bias_idxs, rel_pos)
-    elif isinstance(
-        module,
-        EvaAttention,
-    ):
-        if module.k_bias is not None:
-            init.zeros_(module.k_bias)
-    elif isinstance(module, ParallelScalingBlock):
-        if module.qkv_bias is not None:
-            init.zeros_(module.qkv_bias)
-    elif isinstance(module, Attention):
-        if module.k_bias is not None:
-            init.zeros_(module.k_bias)
-        if module.relative_position_index is not None:
-            init.copy_(module.relative_position_index, beit_gen_relative_position_index(module.window_size))
-    elif isinstance(module, SwinTransformerV2CrBlock):
-        if module.attn_mask is not None:
-            init.copy_(module.attn_mask, module.get_attn_mask())
-    elif isinstance(module, WindowMultiHeadAttention):
-        module._make_pair_wise_relative_positions()
-    elif isinstance(module, BlurPool2d):
-        coeffs = torch.tensor(
-            [comb(module.filt_size - 1, k) for k in range(module.filt_size)], dtype=torch.float32
-        ) / (2 ** (module.filt_size - 1))
-        blur_filter = (coeffs[:, None] * coeffs[None, :])[None, None, :, :]
-        if module.channels is not None:
-            blur_filter = blur_filter.repeat(module.channels, 1, 1, 1)
-        init.copy_(module.filt, blur_filter)
-    elif isinstance(module, Swin2WindowAttention):
-        module._make_pair_wise_relative_positions()
-        if module.k_bias is not None:
-            init.zeros_(module.k_bias)
-    elif isinstance(module, SwinTransformerV2Block):
-        if module.attn_mask is not None:
-            init.copy_(module.attn_mask, module.get_attn_mask())
-    elif isinstance(module, SwinWindowAttention):
-        init.copy_(module.relative_position_index, get_relative_position_index(*module.window_size))
-    elif isinstance(module, SwinTransformerBlock):
-        if module.attn_mask is not None:
-            init.copy_(module.attn_mask, module.get_attn_mask())
-    elif isinstance(module, Attention2d):
-        pos = torch.stack(
-            ndgrid(
-                torch.arange(module.resolution[0], dtype=torch.long),
-                torch.arange(module.resolution[1], dtype=torch.long),
-            )
-        ).flatten(1)
-        rel_pos = (pos[..., :, None] - pos[..., None, :]).abs()
-        rel_pos = (rel_pos[0] * module.resolution[1]) + rel_pos[1]
-        init.copy_(module.attention_bias_idxs, rel_pos)
-    elif isinstance(module, Attention2dDownsample):
-        k_pos = torch.stack(
-            ndgrid(
-                torch.arange(module.resolution[0], dtype=torch.long),
-                torch.arange(module.resolution[1], dtype=torch.long),
-            )
-        ).flatten(1)
-        q_pos = torch.stack(
-            ndgrid(
-                torch.arange(0, module.resolution[0], step=2, dtype=torch.long),
-                torch.arange(0, module.resolution[1], step=2, dtype=torch.long),
-            )
-        ).flatten(1)
-        rel_pos = (q_pos[..., :, None] - k_pos[..., None, :]).abs()
-        rel_pos = (rel_pos[0] * module.resolution[1]) + rel_pos[1]
-        init.copy_(module.attention_bias_idxs, rel_pos)

tests/models/gemma3n/test_processing_gemma3n.py
@@ -14,15 +14,11 @@
 
 import unittest
 
-from transformers import is_speech_available
+from transformers.models.gemma3n import Gemma3nProcessor
 from transformers.testing_utils import require_sentencepiece, require_torch, require_torchaudio, require_vision
 
 from ...test_processing_common import ProcessorTesterMixin
 from .test_feature_extraction_gemma3n import floats_list
-
-
-if is_speech_available():
-    from transformers.models.gemma3n import Gemma3nProcessor
 
 
 # TODO: omni-modal processor can't run tests from `ProcessorTesterMixin`

tests/models/timm_backbone/test_modeling_timm_backbone.py
@@ -135,8 +135,12 @@ class TimmBackboneModelTest(ModelTesterMixin, BackboneTesterMixin, PipelineTeste
     def test_hidden_states_output(self):
         pass
 
-    @unittest.skip(reason="TimmBackbone initialization is managed on the timm side")
+    @unittest.skip(reason="TimmBackbone uses a pretrained model initialization in __init__, not random weights")
     def test_can_init_all_missing_weights(self):
+        pass
+
+    @unittest.skip(reason="TimmBackbone uses a pretrained model initialization in __init__, not random weights")
+    def test_init_weights_can_init_buffers(self):
         pass
 
     @unittest.skip(reason="TimmBackbone models doesn't have inputs_embeds")

tests/test_modeling_common.py
@@ -1157,13 +1157,12 @@ class ModelTesterMixin:
         addition_year = 0  # if we cannot find it, set it to 0 (i.e. oldest)
         if match_object := re.search(r"^# Copyright (\d{4})", source_code, re.MULTILINE | re.IGNORECASE):
             addition_year = int(match_object.group(1))
+        # For now, skip everything older than 2023 and "important models" (too many models to patch otherwise)
+        # TODO: relax this as we patch more and more models
+        if addition_year < 2023:
+            self.skipTest(reason="Not a prioritized model for now.")
 
         for model_class in self.all_model_classes:
-            # For now, skip everything older than 2023 and "important models" (too much models to patch otherwise)
-            # TODO: relax this as we patch more and more models
-            if addition_year < 2023:
-                self.skipTest(reason=f"{model_class} is not a prioritized model for now.")
-
             # This context manager makes sure that we get the same results deterministically for random new weights
             with seeded_weight_init():
                 # First, initialize the model from __init__ -> this ensure everything is correctly initialized, even if
@@ -1282,14 +1281,6 @@ class ModelTesterMixin:
                 buf_name, immediate_parent_class, pretrained_parent_class = find_parent_traceback(
                     buffer, model_from_init
                 )
-
-                # We cannot control timm model weights initialization, so skip in this case
-                if (pretrained_parent_class == "TimmWrapperPreTrainedModel" and "timm_model." in buffer) or (
-                    pretrained_parent_class == "TimmBackbone" and "_backbone." in buffer
-                ):
-                    different_buffers.discard(buffer)
-                    continue
-
                 # Add it to the traceback
                 traceback = (
                     f"`{buf_name}` in module `{immediate_parent_class}` called from `{pretrained_parent_class}`\n"

commit 9e0224d34b0ef22b2eddb1d7b8c107bb958cf6b9

fix: initialize BatchNorm2d buffers onnly on meta
Commit 8dd9c999a6262d6ceb48f4a2da7acaccfa80e3bc introduced a regression by unconditionally reinitializing BatchNorm2d buffers (running_mean, running_var, num_batches_tracked)
in the _init_weights() method.

The problem: When loading pretrained timm models, the flow is:
1. timm.create_model(pretrained=True) correctly loads pretrained BatchNorm statistics
2. post_init() calls _init_weights() on all modules
3. _init_weights() overwrites the pretrained BatchNorm buffers with zeros/ones
4. Model produces incorrect results because normalization uses wrong statistics

The error shows a mismatch at index (0, 178, 0, 0) with a difference of 80224.0 on CircleCI on AVX 512 CPUs

The fix: added a check to only initialize BatchNorm2d buffers when they're on the meta device (uninitialized):
This ensures pretrained buffers are preserved while still handling the meta device initialization case.

src/transformers/models/timm_backbone/modeling_timm_backbone.py
@@ -117,7 +117,12 @@ class TimmBackbone(PreTrainedModel, BackboneMixin):
         assume weights and persistent buffers will be part of checkpoint as we have no way to control timm inits)"""
         if hasattr(module, "init_non_persistent_buffers"):
             module.init_non_persistent_buffers()
-        elif isinstance(module, nn.BatchNorm2d) and getattr(module, "running_mean", None) is not None:
+        elif (
+            isinstance(module, nn.BatchNorm2d)
+            and getattr(module, "running_mean", None) is not None
+            and module.running_mean.device.type == "meta"
+        ):
+            # Only initialize if on meta device (uninitialized), not if already loaded from pretrained
             init.zeros_(module.running_mean)
             init.ones_(module.running_var)
             init.zeros_(module.num_batches_tracked)

src/transformers/models/timm_wrapper/modeling_timm_wrapper.py
@@ -115,7 +115,12 @@ class TimmWrapperPreTrainedModel(PreTrainedModel):
         # Also, reinit all non-persistemt buffers if any!
         if hasattr(module, "init_non_persistent_buffers"):
             module.init_non_persistent_buffers()
-        elif isinstance(module, nn.BatchNorm2d) and getattr(module, "running_mean", None) is not None:
+        elif (
+            isinstance(module, nn.BatchNorm2d)
+            and getattr(module, "running_mean", None) is not None
+            and module.running_mean.device.type == "meta"
+        ):
+            # Only initialize if on meta device (uninitialized), not if already loaded from pretrained
             init.zeros_(module.running_mean)
             init.ones_(module.running_var)
             init.zeros_(module.num_batches_tracked)