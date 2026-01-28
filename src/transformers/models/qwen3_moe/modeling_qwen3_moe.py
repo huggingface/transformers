d54df94e76ac9c8dfd08c90e789845c2b68786d8
ITcarrot <luo-yb25@mails.tsinghua.edu.cn>
2026-01-28 11:51:16 +0800

    fix: output router capture wrong router logits

--- src/transformers/models/qwen3_moe/modeling_qwen3_moe.py
+++ src/transformers/models/qwen3_moe/modeling_qwen3_moe.py
@@ -266,8 +266,8 @@ class Qwen3MoeTopKRouter(nn.Module):
     def forward(self, hidden_states):
         hidden_states = hidden_states.reshape(-1, self.hidden_dim)
         router_logits = F.linear(hidden_states, self.weight)  # (seq_len, num_experts)
-        router_logits = torch.nn.functional.softmax(router_logits, dtype=torch.float, dim=-1)
-        router_top_value, router_indices = torch.topk(router_logits, self.top_k, dim=-1)  # (seq_len, top_k)
+        routing_weights = torch.nn.functional.softmax(router_logits, dtype=torch.float, dim=-1)
+        router_top_value, router_indices = torch.topk(routing_weights, self.top_k, dim=-1)  # (seq_len, top_k)
         if self.norm_topk_prob:
             router_top_value /= router_top_value.sum(dim=-1, keepdim=True)
         router_top_value = router_top_value.to(router_logits.dtype)
