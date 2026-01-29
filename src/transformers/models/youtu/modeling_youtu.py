ab98ee5c08894c100638064c092d6b997200eaa1
3outeille <ferdinand.mom@epita.fr>
2026-01-29 15:07:52 +0000

    fixing remaining modular

--- src/transformers/models/youtu/modeling_youtu.py
+++ src/transformers/models/youtu/modeling_youtu.py
@@ -543,7 +543,7 @@ class YoutuModel(YoutuPreTrainedModel):
 @auto_docstring
 class YoutuForCausalLM(YoutuPreTrainedModel, GenerationMixin):
     _tied_weights_keys = {"lm_head.weight": "model.embed_tokens.weight"}
-    _tp_plan = {"lm_head": "colwise_rep"}
+    _tp_plan = {"lm_head": "colwise_gather_output"}
     _pp_plan = {"lm_head": (["hidden_states"], ["logits"])}
 
     def __init__(self, config):
