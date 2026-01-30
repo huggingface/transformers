52a0b09c9a8938e5c78e2f5256d212f66468e530
Tarek Ziade <tarek@ziade.org>
2026-01-30 11:31:20 +0100

    time flies

--- utils/aggregate_failure_reports.py
+++ utils/aggregate_failure_reports.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# Copyright 2025 The HuggingFace Inc. team.
+# Copyright 2026 The HuggingFace Inc. team.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
