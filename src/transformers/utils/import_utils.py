396a24cf6e6d900cb9ba1094dbfedb3b8328dc60
Tarek Ziade <tarek@ziade.org>
2026-02-06 10:05:39 +0100

    not needed anymore

--- src/transformers/utils/import_utils.py
+++ src/transformers/utils/import_utils.py
@@ -114,7 +114,7 @@ def is_torch_available() -> bool:
         is_available, torch_version = _is_package_available("torch", return_version=True)
         parsed_version = version.parse(torch_version)
         if is_available and parsed_version < version.parse("2.4.0"):
-            logger.warning_once(f"Disabling PyTorch because PyTorch >= 2.4 is required but found {torch_version}")  # type: ignore
+            logger.warning_once(f"Disabling PyTorch because PyTorch >= 2.4 is required but found {torch_version}")
         return is_available and version.parse(torch_version) >= version.parse("2.4.0")
     except packaging.version.InvalidVersion:
         return False
