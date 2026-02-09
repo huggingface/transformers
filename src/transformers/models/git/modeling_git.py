bef3e2bea200662ba68bab575f7902e599c7b5c7
vasqu <antonprogamer@gmail.com>
2026-02-09 16:34:42 +0100

    style

--- src/transformers/models/git/modeling_git.py
+++ src/transformers/models/git/modeling_git.py
@@ -13,6 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """PyTorch GIT model."""
+
 import math
 from collections.abc import Callable
 from dataclasses import dataclass
