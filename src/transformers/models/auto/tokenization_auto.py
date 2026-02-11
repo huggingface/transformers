6ca31e88387905b4e95019a83858f6f50f95c07a
Pavel Esir <pavel.esir@gmail.com>
2026-02-11 11:52:28 +0100

    add Llama to mapping names in tokenization_auto.py
    Without this like `AutoTokenizer.from_pretrained(...)` does not create LlamaTokenizer object.

--- src/transformers/models/auto/tokenization_auto.py
+++ src/transformers/models/auto/tokenization_auto.py
@@ -170,6 +170,7 @@ TOKENIZER_MAPPING_NAMES = OrderedDict[str, str | None](
         ("led", "LEDTokenizer" if is_tokenizers_available() else None),
         ("lighton_ocr", "Qwen2TokenizerFast" if is_tokenizers_available() else None),
         ("lilt", "RobertaTokenizer" if is_tokenizers_available() else None),
+        ("llama", "LlamaTokenizer" if is_tokenizers_available() else None),        
         ("longformer", "RobertaTokenizer" if is_tokenizers_available() else None),
         ("longt5", "T5Tokenizer" if is_tokenizers_available() else None),
         ("luke", "LukeTokenizer"),
