772739fd1bf8ef59339c2ec10aaa78552c1fe4e3
Arthur <arthur.zucker@gmail.com>
2026-02-11 14:39:57 +0100

    last nit

--- src/transformers/models/edgetam/modeling_edgetam.py
+++ src/transformers/models/edgetam/modeling_edgetam.py
@@ -311,15 +311,7 @@ class EdgeTamPreTrainedModel(PreTrainedModel):
     _supports_sdpa = True
     _supports_flash_attn = True
     _supports_attention_backend = True
-    _keys_to_ignore_on_load_unexpected = [
-        r"^memory_.*",
-        r"^mask_downsample.*",
-        r"^object_pointer_proj.*",
-        r"^temporal_positional_encoding_projection_layer.*",
-        "no_memory_positional_encoding",
-        "no_object_pointer",
-        "occlusion_spatial_embedding_parameter",
-    ]
+    _keys_to_ignore_on_load_unexpected = None
 
     @torch.no_grad()
     def _init_weights(self, module):
