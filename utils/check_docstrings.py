a423c6c4e62807b820a8adc79c5ee27824e2e97c
Eustache Le Bihan <eulebihan@gmail.com>
2026-02-15 21:57:50 +0100

    fix-repo

--- utils/check_docstrings.py
+++ utils/check_docstrings.py
@@ -521,6 +521,7 @@ OBJECTS_TO_IGNORE = {
     "BltPatcherConfig",
     "MoonshineStreamingConfig",
     "MoonshineStreamingEncoderConfig",
+    "VoxtralRealtimeFeatureExtractor",
 }
 # In addition to the objects above, we also ignore objects with certain prefixes. If you add an item to the list
 # below, make sure to add a comment explaining why.
