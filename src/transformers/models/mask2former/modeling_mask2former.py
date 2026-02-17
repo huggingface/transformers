5ce8206bd2a2618af56662daf430be4caa9787b4
Yuanyuan Chen <cyyever@outlook.com>
2026-02-17 14:44:17 +0800

    Use torch.isfinite
    Signed-off-by: Yuanyuan Chen <cyyever@outlook.com>

--- src/transformers/models/mask2former/modeling_mask2former.py
+++ src/transformers/models/mask2former/modeling_mask2former.py
@@ -1060,7 +1060,7 @@ class Mask2FormerPixelDecoderEncoderLayer(nn.Module):
         hidden_states = self.final_layer_norm(hidden_states)
 
         if self.training:
-            if torch.isinf(hidden_states).any() or torch.isnan(hidden_states).any():
+            if not torch.isfinite(hidden_states).all():
                 clamp_value = torch.finfo(hidden_states.dtype).max - 1000
                 hidden_states = torch.clamp(hidden_states, min=-clamp_value, max=clamp_value)
 
