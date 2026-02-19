7d925efc214b52a656fe434b6405549cd8798af6
Logan Hallee <lhallee@udel.edu>
2026-02-19 16:42:26 -0500

    Update modeling_esm.py

--- src/transformers/models/esm/modeling_esm.py
+++ src/transformers/models/esm/modeling_esm.py
@@ -644,6 +644,8 @@ class EsmModel(EsmPreTrainedModel):
             raise ValueError("You must specify exactly one of input_ids or inputs_embeds")
 
         if inputs_embeds is None:
+            # Important, attention_mask must be passed to the embedding class
+            # This effects how the token_dropout is calculated
             inputs_embeds = self.embeddings(
                 input_ids=input_ids,
                 attention_mask=attention_mask,
