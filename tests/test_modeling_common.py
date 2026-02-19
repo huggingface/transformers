44cc8abb39c96a48961dff3f61e0c0f5768849b0
vasqu <antonprogamer@gmail.com>
2026-02-19 16:55:25 +0100

    fixup skip condition

--- tests/test_modeling_common.py
+++ tests/test_modeling_common.py
@@ -1763,7 +1763,7 @@ class ModelTesterMixin:
         """Helper function to recursively set a config attr to a given value"""
         for k in config.sub_configs:
             if (
-                self._is_composite and k == "vision_config"
+                self._is_composite and attribute_name == "output_attentions" and k == "vision_config"
             ):  # skip because it's not needed and causes errors e.g with Timm
                 continue
             if getattr(config, k) is not None:
