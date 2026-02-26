775da67d914c05c3b24ff8a54c7f325f2d34b1dd
Matt <rocketknight1@gmail.com>
2026-02-26 17:31:11 +0000

    Update check_repo

--- utils/check_repo.py
+++ utils/check_repo.py
@@ -718,22 +718,28 @@ def find_tested_models(test_file: str) -> set[str]:
             else:
                 tested_class = model_name + _COMMON_MODEL_NAMES_MAP[test_class_type]
             model_tested.add(tested_class)
-    elif "VLMModelTester" in content:
-        base_model_class = re.findall(r"base_model_class\s+=.*", content)  # Required attribute
+    # Same as above, but for VLMModelTester. We scope the search to the VLMModelTester subclass body, as some
+    # files may contain both a CausalLMModelTester and a VLMModelTester (e.g. gemma3).
+    vlm_class_match = re.search(r"class \w+\(VLMModelTester\)", content)
+    if vlm_class_match is not None:
+        vlm_content = content[vlm_class_match.start() :]
+        base_model_class = re.findall(r"base_model_class\s+=.*", vlm_content)  # Required attribute
         base_class = base_model_class[0].split("=")[1].strip()
         model_tested.add(base_class)
 
         model_name = base_class.replace("Model", "")
         # Optional attributes: if not set explicitly, the tester will attempt to infer and use the corresponding class
         for test_class_type in [
-            "causal_lm_class",
             "conditional_generation_class",
+            "sequence_classification_class",
         ]:
-            tested_class = re.findall(rf"{test_class_type}\s+=.*", content)
+            tested_class = re.findall(rf"{test_class_type}\s+=.*", vlm_content)
             if tested_class:
                 tested_class = tested_class[0].split("=")[1].strip()
-            else:
+            elif test_class_type in _VLM_COMMON_MODEL_NAMES_MAP:
                 tested_class = model_name + _VLM_COMMON_MODEL_NAMES_MAP[test_class_type]
+            else:
+                continue
             model_tested.add(tested_class)
 
     return model_tested
