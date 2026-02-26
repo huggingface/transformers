98afc6e5fc92257324119f73133791a857238131
Steven Bucaille <steven.bucaille@gmail.com>
2026-02-26 18:05:09 -0500

    test: fix segmentation tests

--- src/transformers/models/rf_detr/configuration_rf_detr.py
+++ src/transformers/models/rf_detr/configuration_rf_detr.py
@@ -17,9 +17,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+from ...backbone_utils import BackboneConfigMixin
 from ...configuration_utils import PreTrainedConfig
 from ...utils import logging
-from ...utils.backbone_utils import BackboneConfigMixin, get_aligned_output_features_output_indices
 from ..auto import CONFIG_MAPPING, AutoConfig
 
 
@@ -152,9 +152,7 @@ class RfDetrDinov2Config(BackboneConfigMixin, PreTrainedConfig):
         self.drop_path_rate = drop_path_rate
         self.use_swiglu_ffn = use_swiglu_ffn
         self.stage_names = ["stem"] + [f"stage{idx}" for idx in range(1, num_hidden_layers + 1)]
-        self._out_features, self._out_indices = get_aligned_output_features_output_indices(
-            out_features=out_features, out_indices=out_indices, stage_names=self.stage_names
-        )
+        self.set_output_features_output_indices(out_indices=out_indices, out_features=out_features)
         self.apply_layernorm = apply_layernorm
         self.reshape_hidden_states = reshape_hidden_states
         self.use_mask_token = use_mask_token
