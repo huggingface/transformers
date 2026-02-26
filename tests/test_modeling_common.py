2c8e97d8ecccc1fb835647bbf6b3b351b61dee74
IlyasMoutawwakil <moutawwakil.ilyas.tsi@gmail.com>
2026-02-26 10:48:28 +0100

    skip pretraining as it  always computes loss

--- tests/test_modeling_common.py
+++ tests/test_modeling_common.py
@@ -4166,6 +4166,9 @@ class ModelTesterMixin:
 
         for model_class in self.all_model_classes:
             with self.subTest(model_class.__name__):
+                if model_class.__name__.endswith("ForPreTraining"):
+                    continue  # we skip ForPreTraining models as they compute a loss which is not exportable
+
                 if hasattr(self.model_tester, "prepare_config_and_inputs_for_model_class"):
                     config, inputs_dict = self.model_tester.prepare_config_and_inputs_for_model_class(model_class)
                 else:
