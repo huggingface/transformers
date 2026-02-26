6cd59969a74633c1da9a102591b06b08774f8494
Vasqu <antonprogamer@gmail.com>
2026-02-26 13:00:55 +0000

    try

--- src/transformers/models/ernie4_5_vl_moe/image_processing_ernie4_5_vl_moe.py
+++ src/transformers/models/ernie4_5_vl_moe/image_processing_ernie4_5_vl_moe.py
@@ -61,21 +61,6 @@ class Ernie4_5_VLMoeImageProcessorKwargs(ImagesKwargs, total=False):
     merge_size: int
 
 
-class Ernie45VLMoeImageProcessorKwargs(ImagesKwargs, total=False):
-    """
-    patch_size (`int`, *optional*, defaults to 14):
-        The spatial patch size of the vision encoder.
-    temporal_patch_size (`int`, *optional*, defaults to 2):
-        The temporal patch size of the vision encoder.
-    merge_size (`int`, *optional*, defaults to 2):
-        The merge size of the vision encoder to llm encoder.
-    """
-
-    patch_size: int
-    temporal_patch_size: int
-    merge_size: int
-
-
 def smart_resize(
     height: int, width: int, factor: int = 28, min_pixels: int = 56 * 56, max_pixels: int = 14 * 14 * 4 * 1280
 ):
@@ -138,7 +123,7 @@ class Ernie4_5_VLMoeImageProcessor(BaseImageProcessor):
     """
 
     model_input_names = ["pixel_values", "image_grid_thw"]
-    valid_kwargs = Ernie45VLMoeImageProcessorKwargs
+    valid_kwargs = Ernie4_5_VLMoeImageProcessorKwargs
 
     def __init__(
         self,
--- src/transformers/models/ernie4_5_vl_moe/image_processing_ernie4_5_vl_moe_fast.py
+++ src/transformers/models/ernie4_5_vl_moe/image_processing_ernie4_5_vl_moe_fast.py
@@ -28,7 +28,7 @@ from ...image_processing_utils_fast import BaseImageProcessorFast, group_images_
 from ...image_utils import OPENAI_CLIP_MEAN, OPENAI_CLIP_STD, ImageInput, PILImageResampling, SizeDict
 from ...processing_utils import Unpack
 from ...utils import TensorType, auto_docstring
-from .image_processing_ernie4_5_vl_moe import Ernie45VLMoeImageProcessorKwargs
+from .image_processing_ernie4_5_vl_moe import Ernie4_5_VLMoeImageProcessorKwargs
 
 
 def smart_resize(
@@ -73,10 +73,10 @@ class Ernie4_5_VLMoeImageProcessorFast(BaseImageProcessorFast):
     patch_size = 14
     temporal_patch_size = None  # Unused
     merge_size = 2
-    valid_kwargs = Ernie45VLMoeImageProcessorKwargs
+    valid_kwargs = Ernie4_5_VLMoeImageProcessorKwargs
     model_input_names = ["pixel_values", "image_grid_thw"]
 
-    def __init__(self, **kwargs: Unpack[Ernie45VLMoeImageProcessorKwargs]):
+    def __init__(self, **kwargs: Unpack[Ernie4_5_VLMoeImageProcessorKwargs]):
         super().__init__(**kwargs)
         if self.size is not None and (
             self.size.get("shortest_edge", None) is None or self.size.get("longest_edge", None) is None
@@ -223,7 +223,7 @@ class Ernie4_5_VLMoeImageProcessorFast(BaseImageProcessorFast):
     def preprocess(
         self,
         images: ImageInput,
-        **kwargs: Unpack[Ernie45VLMoeImageProcessorKwargs],
+        **kwargs: Unpack[Ernie4_5_VLMoeImageProcessorKwargs],
     ) -> BatchFeature:
         return super().preprocess(images, **kwargs)
 
