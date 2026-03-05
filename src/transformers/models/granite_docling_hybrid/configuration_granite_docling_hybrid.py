4c596a6b1c42b728acb860fda97176d101f6f630
Gabe Goodhart <ghart@us.ibm.com>
2026-03-05 09:48:07 -0700

    style: Fix copyright headers year
    Branch: GraniteDoclingHybrid
    AI-usage: none
    Signed-off-by: Gabe Goodhart <ghart@us.ibm.com>

--- src/transformers/models/granite_docling_hybrid/configuration_granite_docling_hybrid.py
+++ src/transformers/models/granite_docling_hybrid/configuration_granite_docling_hybrid.py
@@ -1,4 +1,4 @@
-# Copyright 2024 the HuggingFace Inc. team. All rights reserved.
+# Copyright 2026 the HuggingFace Inc. team. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
