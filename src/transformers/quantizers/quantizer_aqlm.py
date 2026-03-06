d7e87d5798cc3747c360d2051aff123d33d5e751
Tarek Ziade <tarek@ziade.org>
2026-03-06 16:33:59 +0100

    simplify call
    Co-authored-by: vasqu

--- src/transformers/quantizers/quantizer_aqlm.py
+++ src/transformers/quantizers/quantizer_aqlm.py
@@ -54,10 +54,9 @@ class AqlmHfQuantizer(HfQuantizer):
         model: "PreTrainedModel",
         **kwargs,
     ):
-        modules_to_not_convert = self.quantization_config.linear_weights_not_to_quantize
         replace_with_aqlm_linear(
             model,
-            modules_to_not_convert=modules_to_not_convert,
+            modules_to_not_convert=self.quantization_config.linear_weights_not_to_quantize,
             quantization_config=self.quantization_config,
         )
 
