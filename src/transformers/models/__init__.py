14dea527607a4da62429812e9e0a5ab237b9373b
vandit <vanditashah12@gmail.com>
2026-03-06 23:26:43 -0500

    Add qwen3_tts to models and auto registrations, add documentation

--- src/transformers/models/__init__.py
+++ src/transformers/models/__init__.py
@@ -349,6 +349,7 @@ if TYPE_CHECKING:
     from .qwen3_moe import *
     from .qwen3_next import *
     from .qwen3_omni_moe import *
+    from .qwen3_tts import *
     from .qwen3_vl import *
     from .qwen3_vl_moe import *
     from .rag import *
