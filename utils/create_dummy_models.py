39305e7f84f500119b1f4ede486203d58beb071e
ydshieh <ydshieh@users.noreply.github.com>
2026-03-08 15:16:24 +0100

    check why so many messages

--- utils/create_dummy_models.py
+++ utils/create_dummy_models.py
@@ -1755,7 +1755,7 @@ def create_tiny_models(
     config_classes = [x for x in config_classes if x.__name__ not in deprecated_models]
     config_classes = [x for x in config_classes if x.__name__ not in config_without_meaningful_model_class]
 
-    config_classes = config_classes[50:200]
+    config_classes = config_classes[50:125]
 
     # import random
     # for i in range(100):
