873eb45490415c0d991ce3f0a1d6d9a642032a69
Cyril Vallez <cyril.vallez@gmail.com>
2026-03-09 15:50:23 +0100

    add a bit more explanations in comment

--- src/transformers/modeling_utils.py
+++ src/transformers/modeling_utils.py
@@ -4551,6 +4551,9 @@ class PreTrainedModel(nn.Module, EmbeddingAccessMixin, ModuleUtilsMixin, PushToH
         if is_fsdp_enabled() and not is_local_dist_rank_0():
             # Handle FSDP edge case when using cpu ram efficient loading to ensure it is marked as initialized
             # since it will get its weights broadcasted from rank0
+            # We actually need to do that only because we want to re-initialize non-persistent buffers with correct values.
+            # Everything else in the state_dict will be gathered from rank0, so we don't need re-initialization.
+            # We could simply early return after buffer inits if we had a way to init only the non-persistent buffers
             for key in self.state_dict():
                 try:
                     param_or_buffer = self.get_parameter_or_buffer(key)
