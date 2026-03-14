c368e139aade3ee7cdfa29387f3249168a912e5c
Anton Vlasjuk <73884904+vasqu@users.noreply.github.com>
2026-03-14 18:17:39 +0100

    [`FA`] Fix fa detection (#44703)
    * fix fa2 and fa4 detection
    
    * should be there as well
    
    * also adapt fa3 to same pattern

--- src/transformers/modeling_flash_attention_utils.py
+++ src/transformers/modeling_flash_attention_utils.py
@@ -74,7 +74,7 @@ FLASH_ATTENTION_COMPATIBILITY_MATRIX = {
         "flash_attn_version": 2,
         "general_availability_check": is_flash_attn_2_available,
         "pkg_availability_check": lambda *args, **kwargs: importlib.util.find_spec("flash_attn") is not None
-        and "flash_attn" in PACKAGE_DISTRIBUTION_MAPPING["flash_attn"],
+        and "flash-attn" in [pkg.replace("_", "-") for pkg in PACKAGE_DISTRIBUTION_MAPPING["flash_attn"]],
         "supported_devices": (
             (is_torch_cuda_available, "cuda"),
             (is_torch_mlu_available, "mlu"),
@@ -92,7 +92,8 @@ FLASH_ATTENTION_COMPATIBILITY_MATRIX = {
     3: {
         "flash_attn_version": 3,
         "general_availability_check": is_flash_attn_3_available,
-        "pkg_availability_check": lambda *args, **kwargs: importlib.util.find_spec("flash_attn_3") is not None,
+        "pkg_availability_check": lambda *args, **kwargs: importlib.util.find_spec("flash_attn_interface") is not None
+        and "flash-attn-3" in [pkg.replace("_", "-") for pkg in PACKAGE_DISTRIBUTION_MAPPING["flash_attn_interface"]],
         "supported_devices": ((is_torch_cuda_available, "cuda"),),
         "cuda_min_major_version": 8,  # Ampere
     },
@@ -100,7 +101,7 @@ FLASH_ATTENTION_COMPATIBILITY_MATRIX = {
         "flash_attn_version": 4,
         "general_availability_check": is_flash_attn_4_available,
         "pkg_availability_check": lambda *args, **kwargs: importlib.util.find_spec("flash_attn") is not None
-        and importlib.util.find_spec("flash_attn.cute") is not None,
+        and "flash-attn-4" in [pkg.replace("_", "-") for pkg in PACKAGE_DISTRIBUTION_MAPPING["flash_attn"]],
         "supported_devices": ((is_torch_cuda_available, "cuda"),),
         "cuda_min_major_version": 9,  # Hopper
     },
