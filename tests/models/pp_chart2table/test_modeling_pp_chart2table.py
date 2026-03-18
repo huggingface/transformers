ac2bc662aaf705282081f03cc85917bb9d0d9772
XingweiDeng <aaadddiy@163.com>
2026-03-18 11:57:50 +0800

    update

--- tests/models/pp_chart2table/test_modeling_pp_chart2table.py
+++ tests/models/pp_chart2table/test_modeling_pp_chart2table.py
@@ -17,21 +17,13 @@ import unittest
 
 from transformers import (
     AutoProcessor,
-    PPChart2TableConfig,
     is_torch_available,
     is_vision_available,
 )
 from transformers.testing_utils import cleanup, require_torch, slow, torch_device
 
-from ...generation.test_utils import GenerationTesterMixin
-from ...test_configuration_common import ConfigTester
-from ...test_modeling_common import ModelTesterMixin, floats_tensor, ids_tensor
-from ...test_pipeline_mixin import PipelineTesterMixin
-
 
 if is_torch_available():
-    import torch
-
     from transformers import (
         PPChart2TableForConditionalGeneration,
     )
@@ -39,117 +31,6 @@ if is_torch_available():
 
 if is_vision_available():
     from transformers.image_utils import load_image
-
-
-class PPChart2TableVisionText2TextModelTester:
-    def __init__(
-        self,
-        parent,
-        batch_size=3,
-        seq_length=7,
-        num_channels=3,
-        ignore_index=-100,
-        image_size=64,
-        image_token_index=1,
-        model_type="pp_chart2table",
-        is_training=True,
-        text_config={
-            "model_type": "qwen2",
-            "vocab_size": 99,
-            "hidden_size": 32,
-            "intermediate_size": 37,
-            "num_hidden_layers": 2,
-            "num_attention_heads": 2,
-            "num_key_value_heads": 2,
-            "output_channels": 32,
-            "hidden_act": "silu",
-            "max_position_embeddings": 512,
-            "rope_theta": 10000,
-            "tie_word_embeddings": True,
-            "bos_token_id": 2,
-            "eos_token_id": 3,
-            "pad_token_id": 4,
-        },
-        vision_config={
-            "num_hidden_layers": 2,
-            "output_channels": 32,
-            "hidden_act": "quick_gelu",
-            "hidden_size": 32,
-            "mlp_dim": 64,
-            "num_attention_heads": 2,
-            "patch_size": 2,
-            "image_size": 64,
-        },
-    ):
-        self.parent = parent
-        self.ignore_index = ignore_index
-        self.bos_token_id = text_config["bos_token_id"]
-        self.eos_token_id = text_config["eos_token_id"]
-        self.pad_token_id = text_config["pad_token_id"]
-        self.image_token_index = image_token_index
-        self.model_type = model_type
-        self.text_config = text_config
-        self.vision_config = vision_config
-        self.batch_size = batch_size
-        self.num_channels = num_channels
-        self.image_size = image_size
-        self.is_training = is_training
-        self.num_image_tokens = 64
-        self.seq_length = seq_length + self.num_image_tokens
-
-        self.num_hidden_layers = text_config["num_hidden_layers"]
-        self.vocab_size = text_config["vocab_size"]
-        self.hidden_size = text_config["hidden_size"]
-        self.num_attention_heads = text_config["num_attention_heads"]
-
-    def get_config(self):
-        return PPChart2TableConfig(
-            text_config=self.text_config,
-            vision_config=self.vision_config,
-            model_type=self.model_type,
-            image_token_index=self.image_token_index,
-        )
-
-    def prepare_config_and_inputs(self):
-        config = self.get_config()
-        pixel_values = floats_tensor([self.batch_size, self.num_channels, self.image_size, self.image_size])
-
-        return config, pixel_values
-
-    def prepare_config_and_inputs_for_common(self):
-        config_and_inputs = self.prepare_config_and_inputs()
-        config, pixel_values = config_and_inputs
-        input_ids = ids_tensor([self.batch_size, self.seq_length], self.vocab_size)
-        attention_mask = torch.ones(input_ids.shape, dtype=torch.long, device=torch_device)
-
-        input_ids[input_ids == self.image_token_index] = self.pad_token_id
-        input_ids[:, : self.num_image_tokens] = self.image_token_index
-
-        inputs_dict = {
-            "pixel_values": pixel_values,
-            "input_ids": input_ids,
-            "attention_mask": attention_mask,
-        }
-        return config, inputs_dict
-
-
-@require_torch
-class PPChart2TableModelTest(ModelTesterMixin, GenerationTesterMixin, PipelineTesterMixin, unittest.TestCase):
-    all_model_classes = (PPChart2TableForConditionalGeneration,) if is_torch_available() else ()
-    pipeline_model_mapping = (
-        {
-            "image-text-to-text": PPChart2TableForConditionalGeneration,
-        }
-        if is_torch_available()
-        else {}
-    )
-
-    def setUp(self):
-        self.model_tester = PPChart2TableVisionText2TextModelTester(self)
-        self.config_tester = ConfigTester(self, config_class=PPChart2TableConfig, has_text_modality=False)
-
-    def test_config(self):
-        self.config_tester.run_common_tests()
 
 
 @slow
