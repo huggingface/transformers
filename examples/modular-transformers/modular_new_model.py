185ba25e41b1cec76495711986a4450e6bde1cfb
Cyril Vallez <cyril.vallez@gmail.com>
2026-03-18 17:34:04 +0100

    fix and rerun modular

--- examples/modular-transformers/modular_new_model.py
+++ examples/modular-transformers/modular_new_model.py
@@ -4,32 +4,28 @@ from transformers.models.gemma.configuration_gemma import GemmaConfig
 
 
 class NewModelConfig(GemmaConfig):
-    def __init__(
-        self,
-        vocab_size=256030,
-        hidden_size=64,
-        intermediate_size=90,
-        num_hidden_layers=28,
-        num_attention_heads=16,
-        num_key_value_heads=16,
-        head_dim=256,
-        hidden_act="gelu_pytorch_tanh",
-        hidden_activation=None,
-        max_position_embeddings=1500,
-        initializer_range=0.02,
-        rms_norm_eps=1e-6,
-        use_cache=True,
-        pad_token_id=0,
-        eos_token_id=1,
-        bos_token_id=2,
-        tie_word_embeddings=True,
-        rope_parameters=None,
-        attention_bias=False,
-        attention_dropout=0.0,
-        use_bidirectional_attention=False,
-        **kwargs,
-    ):
-        super().__init__(self, **kwargs)
+
+    vocab_size: int = 256030
+    hidden_size: int = 64
+    intermediate_size: int = 90
+    num_hidden_layers: int = 28
+    num_attention_heads: int = 16
+    num_key_value_heads: int = 16
+    head_dim: int = 256
+    hidden_act: str = "gelu_pytorch_tanh"
+    hidden_activation: str | None = None
+    max_position_embeddings: int = 1500
+    initializer_range: float = 0.02
+    rms_norm_eps: float = 1e-6
+    use_cache: bool = True
+    pad_token_id: int = 0
+    eos_token_id: int = 1
+    bos_token_id: int = 2
+    tie_word_embeddings: bool = True
+    rope_parameters: dict | None = None
+    attention_bias: bool = False
+    attention_dropout: float = 0.0
+    use_bidirectional_attention: bool = False
 
     @property
     def num_heads(self):
