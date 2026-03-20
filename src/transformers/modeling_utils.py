b9f3135a80e6c8396a60c075b4c9b5f24e2cf01d
Cyril Vallez <cyril.vallez@gmail.com>
2026-03-20 12:39:28 +0100

    fix

--- src/transformers/modeling_utils.py
+++ src/transformers/modeling_utils.py
@@ -259,7 +259,8 @@ def get_state_dict_dtype(state_dict):
     Returns the first found floating dtype in `state_dict` if there is one, otherwise returns the first dtype.
     """
     for t in state_dict.values():
-        if t.is_floating_point():
+        # We cannot instantiate a whole model under float8_xxx dtypes (torch does not allow setting them as default dtype)
+        if t.is_floating_point() and not "float8_" in str(t.dtype):
             return t.dtype
 
     # if no floating dtype was found return whatever the first dtype is
