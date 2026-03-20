419394c190a723b7e5b1f8165d79f0536d6f092d
itazap <ita.zaporozhets@huggingface.co>
2026-03-20 12:29:40 +0100

    fix config type

--- src/transformers/models/llama4/configuration_llama4.py
+++ src/transformers/models/llama4/configuration_llama4.py
@@ -166,7 +166,7 @@ class Llama4TextConfig(PreTrainedConfig):
     no_rope_layers: list[int] | None = None
     no_rope_layer_interval: int = 4
     attention_chunk_size: int = 8192
-    layer_types: list[int] | None = None
+    layer_types: list[str] | None = None
     attn_temperature_tuning: bool = True
     floor_scale: int = 8192
     attn_scale: float = 0.1
