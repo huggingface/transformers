ba0275dca1541729410e826eccccdd414d3e27a4
Tarek Ziade <tarek@ziade.org>
2026-03-23 10:01:12 +0100

    APLv2 headers everywhere

--- utils/mlinter/mlinter.py
+++ utils/mlinter/mlinter.py
@@ -1,5 +1,4 @@
-#!/usr/bin/env python
-# Copyright 2025 The HuggingFace Inc. team.
+# Copyright 2026 The HuggingFace Team. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 import argparse
 import ast
 import hashlib
