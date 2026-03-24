7b4ca4f394058168dfa17ed0814f66fd15c3609e
BADAOUI Abdennacer <106801897+Abdennacer-Badaoui@users.noreply.github.com>
2026-03-24 17:09:15 +0100

    fix

--- tests/models/qwen2/test_modeling_qwen2.py
+++ tests/models/qwen2/test_modeling_qwen2.py
@@ -189,7 +189,7 @@ class Qwen2IntegrationTest(unittest.TestCase):
                 ("rocm", (9, 4)): "My favourite condiment is 100% natural, organic, gluten-free, vegan, and vegetarian. I have been making",
                 ("xpu", 3): "My favourite condiment is 100% natural, organic, gluten-free, vegan, and vegetarian. I have been making",
             }
-        )  # fmt: skip
+        ).get_expectation()  # fmt: skip
         prompt = "My favourite condiment is "
         tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen2-0.5B", use_fast=False)
         model = Qwen2ForCausalLM.from_pretrained("Qwen/Qwen2-0.5B", device_map="auto", dtype=torch.float16)
@@ -202,7 +202,7 @@ class Qwen2IntegrationTest(unittest.TestCase):
             input_ids, max_new_tokens=20, do_sample=True, temperature=0.3, assistant_model=assistant_model
         )
         text = tokenizer.decode(generated_ids[0], skip_special_tokens=True)
-        self.assertEqual(EXPECTED_TEXT_COMPLETION.get_expectation(), text)
+        self.assertEqual(EXPECTED_TEXT_COMPLETION, text)
 
         del model
         backend_empty_cache(torch_device)
