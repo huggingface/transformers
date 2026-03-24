103ef93a5253d846935248183cbddb307c23e0fc
raushan <raushan@huggingface.co>
2026-03-24 15:58:45 +0100

    fix style

--- src/transformers/models/aria/modular_aria.py
+++ src/transformers/models/aria/modular_aria.py
@@ -11,7 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import numpy as np
 import torch
 from huggingface_hub.dataclasses import strict
 from torch import nn
