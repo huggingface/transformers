2d63f8897da718a6c62ece4df1a158ec316cecaa
ita.zaporozhets@huggingface.co <ita_zaporozhets@ip-26-0-160-40.ec2.internal>
2026-03-24 14:43:30 +0000

    Add modular_sarvam.py skeleton based on deepseek_v3

---
+++ src/transformers/models/sarvam/modular_sarvam.py
@@ -0,0 +1,60 @@
+# Copyright 2024 The HuggingFace Team. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Auto-generated skeleton by modular_model_detector. Base model: deepseek_v3
+
+from ..deepseek_v3.modeling_deepseek_v3 import (
+    DeepseekV3MLP,
+    DeepseekV3MoE,
+    DeepseekV3Attention,
+    DeepseekV3DecoderLayer,
+)
+
+
+class SarvamMLAMLP(DeepseekV3MLP):
+    pass  # score 0.9719
+
+
+class MoEGate(DeepseekV3MoE):
+    pass  # score 0.8995
+
+
+class SarvamMLAMoE(DeepseekV3MoE):
+    pass  # score 0.8891
+
+
+class SarvamMLAAttention(DeepseekV3Attention):
+    pass  # score 0.9092
+
+
+class SarvamMLADecoderLayer(DeepseekV3DecoderLayer):
+    pass  # score 0.9330
+
+
+# TODO: SarvamMLARMSNorm has no close match in 'deepseek_v3' — implement from scratch
+
+
+# TODO: SarvamMLARotaryEmbedding has no close match in 'deepseek_v3' — implement from scratch
+
+
+# TODO: SarvamMLAYarnRotaryEmbedding has no close match in 'deepseek_v3' — implement from scratch
+
+
+# TODO: SarvamMLAPreTrainedModel has no close match in 'deepseek_v3' — implement from scratch
+
+
+# TODO: SarvamMLAModel has no close match in 'deepseek_v3' — implement from scratch
+
+
+# TODO: SarvamMLAForCausalLM has no close match in 'deepseek_v3' — implement from scratch
