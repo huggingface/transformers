a3fad8ca11152611e931a8801a89b92c29adec49
Ilyas Moutawwakil <ilyas.moutawwakil@gmail.com>
2026-03-24 18:26:44 +0000

    final fix that works for cpu builds as well

--- src/transformers/integrations/moe.py
+++ src/transformers/integrations/moe.py
@@ -17,7 +17,7 @@ from functools import wraps
 
 from ..utils import logging
 from ..utils.generic import GeneralInterface
-from ..utils.import_utils import is_torch_available, is_torchdynamo_compiling
+from ..utils.import_utils import is_torch_available, is_torch_less_or_equal, is_torchdynamo_compiling
 
 
 if is_torch_available():
@@ -262,7 +262,10 @@ def _can_use_grouped_mm(input: torch.Tensor, weight: torch.Tensor, offs: torch.T
         `bool`: True if grouped_mm can be used, False otherwise.
     """
     if (is_torchdynamo_compiling() and weight.dtype != torch.bfloat16) or (
-        weight.data_ptr() % 16 != 0 or input.data_ptr() % 16 != 0
+        weight.device.type == "cpu"
+        and is_torch_less_or_equal("2.10.0", accept_dev=True)
+        # accept_dev=True is necessary for "+cpu" builds as well
+        and (weight.data_ptr() % 16 != 0 or input.data_ptr() % 16 != 0)
     ):
         # Under the following conditions we cannot use torch.grouped_mm and have to fall back:
         # 1. torch.grouped_mm is not supported in torch.compile / inductor with dtypes other than bf16
