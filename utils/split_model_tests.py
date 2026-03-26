d1b998987387e4939bace3cbf9e73e16edc9137e
ydshieh <ydshieh@users.noreply.github.com>
2026-03-26 16:00:36 +0100

    fix

--- utils/split_model_tests.py
+++ utils/split_model_tests.py
@@ -85,4 +85,5 @@ if __name__ == "__main__":
         if len(d[start:end]) > 0:
             model_splits.append(d[start:end])
 
+    model_splits = [["models/qwen3_omni_moe"]]
     print(model_splits)
