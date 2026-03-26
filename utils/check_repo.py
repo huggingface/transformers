914159cc94063ddcc5b6760a164ecaf13b3ced31
Tarek Ziade <tarek@ziade.org>
2026-03-26 08:07:31 +0100

    Remove check_repo cache changes from speedup branch

--- utils/check_repo.py
+++ utils/check_repo.py
@@ -31,7 +31,6 @@ It has no auto-fix mode.
 """
 
 import ast
-import functools
 import os
 import re
 import types
@@ -566,7 +565,6 @@ def check_model_list():
 
 # If some modeling modules should be ignored for all checks, they should be added in the nested list
 # _ignore_modules of this function.
-@functools.lru_cache(maxsize=1)
 def get_model_modules() -> list[str]:
     """Get all the model modules inside the transformers library (except deprecated models)."""
     _ignore_modules = [
@@ -1341,11 +1339,11 @@ def check_models_have_kwargs():
             with open(modeling_file, "r", encoding="utf-8") as f:
                 tree = ast.parse(f.read())
 
-            # Map all classes in the file to their base classes (only top-level classes)
+            # Map all classes in the file to their base classes
             class_bases = {}
             all_class_nodes = {}
 
-            for node in tree.body:
+            for node in ast.walk(tree):
                 if isinstance(node, ast.ClassDef):
                     # We only care about base classes that are simple names
                     bases = [b.id for b in node.bases if isinstance(b, ast.Name)]
