461fb5256e9795c19207290de36b4c28304c59b4
ydshieh <ydshieh@users.noreply.github.com>
2026-03-26 09:06:01 +0100

    fix

--- utils/tests_fetcher.py
+++ utils/tests_fetcher.py
@@ -1106,16 +1106,27 @@ JOB_TO_TEST_FILE = {
 def create_test_list_from_filter(full_test_list, out_path):
     os.makedirs(out_path, exist_ok=True)
     all_test_files = "\n".join(full_test_list)
+
+    to_output = []
     for job_name, _filter in JOB_TO_TEST_FILE.items():
         file_name = os.path.join(out_path, f"{job_name}_test_list.txt")
         if job_name == "tests_hub":
             files_to_test = ["tests"]
         else:
             files_to_test = list(re.findall(_filter, all_test_files))
-        print(job_name, file_name)
+
+        print(job_name, file_name, len(files_to_test))
+
         if len(files_to_test) > 0:  # No tests -> no file with test list
-            with open(file_name, "w") as f:
-                f.write("\n".join(files_to_test))
+            to_output.append((job_name, file_name, files_to_test))
+
+    # Only get `tests_hub`, which means we have 0 test file for all other jobs --> No job at all to run.
+    if len(to_output) == 1:
+        to_output = []
+
+    for _, file_name, files_to_test in to_output:
+        with open(file_name, "w") as f:
+            f.write("\n".join(files_to_test))
 
 
 if __name__ == "__main__":
