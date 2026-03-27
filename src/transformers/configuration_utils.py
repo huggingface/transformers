d74992b33e78b025acd97fb2bd016aea052d6e4c
raushan <raushan@huggingface.co>
2026-03-27 09:44:07 +0100

    fix

--- src/transformers/configuration_utils.py
+++ src/transformers/configuration_utils.py
@@ -263,6 +263,12 @@ class PreTrainedConfig(PushToHubMixin, RotaryEmbeddingConfigMixin):
         # BC for rotary embeddings. We will pop out legacy keys from kwargs and rename to new format
         if hasattr(self, "rope_parameters"):
             kwargs = self.convert_rope_params_to_dict(**kwargs)
+        elif "rope_scaling" in kwargs and "rope_theta" in kwargs:
+            logger.warning(
+                f"{self.__class__.__name__} got `key=rope_scaling` in kwargs but hasn't set it as attribute. "
+                "For RoPE standardization you need to set `self.rope_parameters` in model's config. "
+            )
+            kwargs = self.convert_rope_params_to_dict(**kwargs)
 
         # Parameters for sequence generation saved in the config are popped instead of loading them.
         for parameter_name in GenerationConfig._get_default_generation_params().keys():
