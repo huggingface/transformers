d1a7aacd92073f803300a8c5fff310b5d23c15a5
Sachin Singh <sachinkumarsingh092@gmail.com>
2026-04-01 00:31:05 +0530

    Initial scaffolding

---
+++ src/transformers/models/voxtral_tts/modular_voxtral_tts.py
@@ -0,0 +1,87 @@
+# Copyright 2026 the HuggingFace Team. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from ..mistral.configuration_mistral import MistralConfig
+from ..mistral.modeling_mistral import (
+    MistralAttention,
+    MistralDecoderLayer,
+    MistralForCausalLM,
+    MistralForQuestionAnswering,
+    MistralForSequenceClassification,
+    MistralForTokenClassification,
+    MistralMLP,
+    MistralModel,
+    MistralPreTrainedModel,
+    MistralRMSNorm,
+    MistralRotaryEmbedding,
+)
+
+
+class VoxtralTtsConfig(MistralConfig):
+    pass
+
+
+class VoxtralTtsMLP(MistralMLP):
+    pass
+
+
+class VoxtralTtsAttention(MistralAttention):
+    pass
+
+
+class VoxtralTtsRMSNorm(MistralRMSNorm):
+    pass
+
+
+class VoxtralTtsDecoderLayer(MistralDecoderLayer):
+    pass
+
+
+class VoxtralTtsPreTrainedModel(MistralPreTrainedModel):
+    pass
+
+
+class VoxtralTtsRotaryEmbedding(MistralRotaryEmbedding):
+    pass
+
+
+class VoxtralTtsModel(MistralModel):
+    pass
+
+
+class VoxtralTtsForCausalLM(MistralForCausalLM):
+    pass
+
+
+class VoxtralTtsForTokenClassification(MistralForTokenClassification):
+    pass
+
+
+class VoxtralTtsForSequenceClassification(MistralForSequenceClassification):
+    pass
+
+
+class VoxtralTtsForQuestionAnswering(MistralForQuestionAnswering):
+    pass
+
+
+__all__ = [
+    "VoxtralTtsConfig",
+    "VoxtralTtsForCausalLM",
+    "VoxtralTtsForQuestionAnswering",
+    "VoxtralTtsModel",
+    "VoxtralTtsPreTrainedModel",
+    "VoxtralTtsForSequenceClassification",
+    "VoxtralTtsForTokenClassification",
+]
