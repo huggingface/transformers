ab6e7110e6e6f4256b6b02f5f34d74dfbc7bcce1
Harry Mellor <19981378+hmellor@users.noreply.github.com>
2026-03-31 14:36:51 +0200

    `make style`
    Signed-off-by: Harry Mellor <19981378+hmellor@users.noreply.github.com>

--- src/transformers/models/auto/configuration_auto.py
+++ src/transformers/models/auto/configuration_auto.py
@@ -1465,7 +1465,9 @@ class AutoConfig:
         config_dict, unused_kwargs = PreTrainedConfig.get_config_dict(pretrained_model_name_or_path, **kwargs)
         has_remote_code = "auto_map" in config_dict and "AutoConfig" in config_dict["auto_map"]
         has_local_code = "model_type" in config_dict and config_dict["model_type"] in CONFIG_MAPPING
-        explicit_local_code = has_local_code and not CONFIG_MAPPING[config_dict["model_type"]].__module__.startswith("transformers.")
+        explicit_local_code = has_local_code and not CONFIG_MAPPING[config_dict["model_type"]].__module__.startswith(
+            "transformers."
+        )
         if has_remote_code:
             class_ref = config_dict["auto_map"]["AutoConfig"]
             if "--" in class_ref:
