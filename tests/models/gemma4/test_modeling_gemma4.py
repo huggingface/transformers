dec020b91bf09761a594d78840db8afe4904f0e9
Cyril Vallez <cyril.vallez@gmail.com>
2026-04-08 14:51:48 +0200

    style

--- tests/models/gemma4/test_modeling_gemma4.py
+++ tests/models/gemma4/test_modeling_gemma4.py
@@ -495,16 +495,16 @@ class Gemma4IntegrationTest(unittest.TestCase):
 
         EXPECTED_TEXTS = Expectations(
             {
-                ("cuda", (8, 0)):
-                    [
-                        'This image shows a **brown and white cow** standing on a **sandy beach** with the **ocean and a blue sky** in the background',
-                        'No, these images are not identical.\n\nThe first image is a photograph of a **cow** standing on a beach under a blue sky.\n\n'
-                    ],
-                ("cuda", (8, 6)):
-                    ['This image shows a **brown and white cow** standing on a **sandy beach** with the **ocean and a blue sky** in the background',
-                     'No, these images are not identical.\n\nThe first image is a photograph of a **brown and white cow standing on a beach** under a blue']
+                ("cuda", (8, 0)): [
+                    "This image shows a **brown and white cow** standing on a **sandy beach** with the **ocean and a blue sky** in the background",
+                    "No, these images are not identical.\n\nThe first image is a photograph of a **cow** standing on a beach under a blue sky.\n\n",
+                ],
+                ("cuda", (8, 6)): [
+                    "This image shows a **brown and white cow** standing on a **sandy beach** with the **ocean and a blue sky** in the background",
+                    "No, these images are not identical.\n\nThe first image is a photograph of a **brown and white cow standing on a beach** under a blue",
+                ],
             }
-        )  # fmt: skip
+        )
         EXPECTED_TEXT = EXPECTED_TEXTS.get_expectation()
         self.assertEqual(output_text, EXPECTED_TEXT)
 
