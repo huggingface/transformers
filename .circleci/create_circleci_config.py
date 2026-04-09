f40bd7179c4873543379a9a975bf66174337e37c
Tarek Ziade <tarek@ziade.org>
2026-04-09 10:27:23 +0200

    bumping up more timeouts

--- .circleci/create_circleci_config.py
+++ .circleci/create_circleci_config.py
@@ -243,7 +243,7 @@ class CircleCIJob:
             {
                 "run": {
                     "name": "Run tests",
-                    "no_output_timeout": "15m",
+                    "no_output_timeout": "30m",
                     "command": f"({timeout_cmd} python3 -m pytest {marker_cmd} -n {self.pytest_num_workers} {junit_flags} {repeat_on_failure_flags} {' '.join(pytest_flags)} $(cat splitted_tests.txt) | tee tests_output.txt)",
                 }
             },
