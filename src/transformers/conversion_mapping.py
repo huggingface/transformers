324a0db44b1d13a3d9ee5af8745a6af9dac3d115
Cyril Vallez <cyril.vallez@gmail.com>
2026-04-10 12:25:57 +0200

    comment

--- src/transformers/conversion_mapping.py
+++ src/transformers/conversion_mapping.py
@@ -159,8 +159,8 @@ def _build_checkpoint_conversion_mapping():
             WeightRenaming("feedforward_layer_norm", "post_attention_layernorm"),
         ],
         "qwen3_5_text": [
-            # Note: the lookbehind on the target is to avoid replacing bigger matches when the model is a submodel of
-            # the ForConditionalGeneration model
+            # Note: the lookbehind on the target is to avoid replacing bigger matches during saving when the model is
+            # a submodel of the ForConditionalGeneration model
             WeightRenaming(
                 source_patterns=r"^model.language_model.", target_patterns=r"^model.(?!(?:language_model.|visual.))"
             ),
