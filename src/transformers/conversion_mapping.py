5be79157c9223b92636d7a16170ab48d21c614eb
Cyril Vallez <cyril.vallez@gmail.com>
2026-04-10 12:46:38 +0200

    remove gemma3n - should never have been there

--- src/transformers/conversion_mapping.py
+++ src/transformers/conversion_mapping.py
@@ -77,7 +77,6 @@ _MODEL_TO_CONVERSION_PATTERN = {
     "qwen2_5_vl": "qwen2_vl",
     "sam3_tracker_video": "sam3_tracker",
     "pp_chart2table": "llava",
-    "gemma3n_text": "qwen3_5_text",
     "qwen3_5_moe_text": "qwen3_5_text",
 }
 
