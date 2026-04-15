4711af6acb5c918dc3b180894a1961fdd0dcb2b8
IlyasMoutawwakil <moutawwakil.ilyas.tsi@gmail.com>
2026-04-15 16:22:10 +0200

    style

--- src/transformers/models/qwen2_5_omni/modular_qwen2_5_omni.py
+++ src/transformers/models/qwen2_5_omni/modular_qwen2_5_omni.py
@@ -1365,7 +1365,6 @@ class Qwen2_5OmniAudioEncoder(Qwen2_5OmniPreTrainedModel):
             pool_indices = get_pool_indices(feature_lens)
 
         # Derive masks from chunk_lengths (traceable arithmetic + arange broadcasting)
-
         padded_feature = padded_feature.to(self.conv1.weight.dtype)
         padded_mask = (
             (torch.arange(padded_feature.shape[2], device=padded_feature.device) < chunk_lengths.unsqueeze(1))
