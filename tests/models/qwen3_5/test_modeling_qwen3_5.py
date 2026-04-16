cd2ec5817c3ff2d4b972fdb879c634b85794923d
raushan <raushan@huggingface.co>
2026-04-16 14:33:17 +0200

    add the test

--- tests/models/qwen3_5/test_modeling_qwen3_5.py
+++ tests/models/qwen3_5/test_modeling_qwen3_5.py
@@ -44,6 +44,7 @@ if is_torch_available():
         Qwen3_5ForSequenceClassification,
         Qwen3_5Model,
         Qwen3_5TextConfig,
+        Qwen3_5TextForSequenceClassification,
         Qwen3_5TextModel,
     )
 
@@ -52,7 +53,7 @@ class Qwen3_5TextModelTester(CausalLMModelTester):
     if is_torch_available():
         base_model_class = Qwen3_5TextModel
         causal_lm_class = Qwen3_5ForCausalLM
-        sequence_classification_class = Qwen3_5ForSequenceClassification
+        sequence_classification_class = Qwen3_5TextForSequenceClassification
 
     def __init__(self, parent):
         super().__init__(parent=parent)
