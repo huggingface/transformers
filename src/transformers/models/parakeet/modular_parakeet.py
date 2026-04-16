ensure correct loss computation

src/transformers/models/parakeet/modular_parakeet.py
@@ -778,7 +778,6 @@ class ParakeetTDTDecoder(nn.Module):
         return decoder_output
 
 
-
 class ParakeetTDTJointNetwork(nn.Module):
     """Joint network that combines encoder and decoder outputs to predict tokens and durations."""
 
@@ -915,9 +914,9 @@ class ParakeetForTDT(ParakeetPreTrainedModel, ParakeetTDTGenerationMixin):
 
         decoder_hidden_states = self.decoder(decoder_input_ids, cache=decoder_cache)
         logits = self.joint(
-            encoder_hidden_states=encoder_outputs.pooler_output,
-            decoder_hidden_states=decoder_hidden_states,
-        )
+            encoder_hidden_states=encoder_outputs.pooler_output[:, :, None, :],
+            decoder_hidden_states=decoder_hidden_states[:, None, :, :],
+        ).squeeze(2)
 
         loss = None
         if labels is not None: