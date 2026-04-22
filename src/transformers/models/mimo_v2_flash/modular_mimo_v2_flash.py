ed35fafef7763c7a35d6f02354bd118e990076b5
casinca <47400729+casinca@users.noreply.github.com>
2026-04-22 19:34:00 +0200

    docs: added comment
    Co-authored-by: Anton Vlasjuk <73884904+vasqu@users.noreply.github.com>

--- src/transformers/models/mimo_v2_flash/modular_mimo_v2_flash.py
+++ src/transformers/models/mimo_v2_flash/modular_mimo_v2_flash.py
@@ -354,7 +354,7 @@ class MiMoV2FlashAttention(nn.Module):
             dropout=0.0 if not self.training else self.attention_dropout,
             scaling=self.scaling,
             sliding_window=self.sliding_window,
-            s_aux=self.sinks,
+            s_aux=self.sinks,  # Optional sinks, only when in SWA layer
             **kwargs,
         )
 
