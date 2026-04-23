7e435bef05ce0f699f56ce58d54fd3320c220a43
Cyril Vallez <cyril.vallez@huggingface.co>
2026-04-23 10:45:59 +0200

    Align latest model attention function dispatch (#45598)
    align

--- src/transformers/models/gemma4/modeling_gemma4.py
+++ src/transformers/models/gemma4/modeling_gemma4.py
@@ -1235,9 +1235,9 @@ class Gemma4TextAttention(nn.Module):
         if self.store_full_length_kv:
             shared_kv_states[self.layer_idx] = key_states, value_states
 
-        attention_interface: Callable = eager_attention_forward
-        if self.config._attn_implementation != "eager":
-            attention_interface = ALL_ATTENTION_FUNCTIONS[self.config._attn_implementation]
+        attention_interface: Callable = ALL_ATTENTION_FUNCTIONS.get_interface(
+            self.config._attn_implementation, eager_attention_forward
+        )
 
         attn_output, attn_weights = attention_interface(
             self,
