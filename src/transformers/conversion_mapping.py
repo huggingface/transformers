a18eb334fe419efccceda86e11d6333c3dd05edf
Harry Mellor <19981378+hmellor@users.noreply.github.com>
2026-04-23 18:02:12 +0000

    remove double newline
    Signed-off-by: Harry Mellor <19981378+hmellor@users.noreply.github.com>

--- src/transformers/conversion_mapping.py
+++ src/transformers/conversion_mapping.py
@@ -599,10 +599,8 @@ def _build_checkpoint_conversion_mapping():
         WeightRenaming(source_patterns=r"mlp\.expert_bias", target_patterns="mlp.e_score_correction_bias"),
         WeightRenaming(source_patterns=r"mlp\.shared_mlp\.", target_patterns="mlp.shared_experts."),
     ]
-
     mapping["qwen3_5_moe_text"] = mapping["qwen3_5_text"].copy()
     mapping["qwen3_5_moe_text"] += mapping["qwen2_moe"].copy()
-
 
     for model_type, base_pattern in _MODEL_TO_CONVERSION_PATTERN.items():
         if model_type in mapping:
