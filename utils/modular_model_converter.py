82ba8df8783f774ef317e2fb1166a665625647da
Tarek Ziade <tarek@ziade.org>
2026-04-27 08:32:57 +0200

    make the change closer to main

--- utils/modular_model_converter.py
+++ utils/modular_model_converter.py
@@ -1189,11 +1189,30 @@ def append_new_import_node(
         imports_to_keep.append(new_node)
 
 
-def _build_needed_imports(all_imports: list[cst.CSTNode], unused_imports: set[str]) -> list[cst.CSTNode]:
-    """Build the final import block after pruning the unused names."""
+def get_needed_imports(body: dict[str, dict], all_imports: list[cst.CSTNode]) -> list[cst.CSTNode]:
+    """Get all the imports needed in the `body`, from the list of `all_imports`.
+    `body` is a dict with the following structure `{str: {"insert_idx": int, "node": cst.CSTNode}}`.
+    Note: we need to use `isinstance` on scope assignments, m.matches apparently does not work here yet!
+    """
+    new_body = [k[1]["node"] for k in sorted(body.items(), key=lambda x: x[1]["insert_idx"])]
+    wrapper = MetadataWrapper(cst.Module(body=all_imports + new_body), unsafe_skip_copy=True)
+    scopes = set(wrapper.resolve(ScopeProvider).values())
+    unused_imports = set()
+    import_ref_count = defaultdict(lambda: 0)
+    for scope in scopes:
+        for assignment in scope.assignments:
+            node = assignment.node
+            if isinstance(assignment, cst.metadata.Assignment) and isinstance(node, (cst.Import, cst.ImportFrom)):
+                ref_count = len(assignment.references)
+                name = assignment.name
+                import_ref_count[name] = max(ref_count, import_ref_count[name])
+    # Similar imports may be redefined, and only used between their 1st and 2nd definition so if we already have
+    # a ref count > 0 at any point, the imports is actually used
+    unused_imports = {name for name, count in import_ref_count.items() if count <= 0 or name in body}
+
     imports_to_keep = []
-    # We need to keep track of which names were already imported, because some import may be duplicated from multiple
-    # sources or be both protected and unprotected due to inconsistency between models.
+    # We need to keep track of which names were already imported, because some import may be duplicated from multiple sources
+    # or be both protected and unprotected due to inconsistency between models
     added_names = set()
     existing_protected_statements = set()  # str repr of the import nodes - does not work with the nodes directly
     for node in all_imports:
@@ -1214,27 +1233,6 @@ def _build_needed_imports(all_imports: list[cst.CSTNode], unused_imports: set[st
 
     # Protected imports always appear at the end of all imports
     return usual_import_nodes + protected_import_nodes
-
-
-def get_needed_imports(body: dict[str, dict], all_imports: list[cst.CSTNode]) -> list[cst.CSTNode]:
-    """Get all the imports needed in the `body`, from the list of `all_imports`.
-    `body` is a dict with the following structure `{str: {"insert_idx": int, "node": cst.CSTNode}}`.
-    """
-    new_body = [k[1]["node"] for k in sorted(body.items(), key=lambda x: x[1]["insert_idx"])]
-    wrapper = MetadataWrapper(cst.Module(body=all_imports + new_body), unsafe_skip_copy=True)
-    scopes = set(wrapper.resolve(ScopeProvider).values())
-    import_ref_count = defaultdict(lambda: 0)
-    for scope in scopes:
-        for assignment in scope.assignments:
-            node = assignment.node
-            if isinstance(assignment, cst.metadata.Assignment) and isinstance(node, (cst.Import, cst.ImportFrom)):
-                ref_count = len(assignment.references)
-                name = assignment.name
-                import_ref_count[name] = max(ref_count, import_ref_count[name])
-    # Similar imports may be redefined, and only used between their 1st and 2nd definition so if we already have
-    # a ref count > 0 at any point, the imports is actually used
-    unused_imports = {name for name, count in import_ref_count.items() if count <= 0 or name in body}
-    return _build_needed_imports(all_imports, unused_imports)
 
 
 def _ensure_utils_availability_imports(imports: list[cst.CSTNode], needed: set[str]) -> list[cst.CSTNode]:
