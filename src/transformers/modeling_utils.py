37c106b6f3038132dd4c949d899b6594b617ea83
aminediro <amine.dirhoussi@huggingface.co>
2026-04-27 22:51:53 +0000

    Fix import ordering

--- src/transformers/modeling_utils.py
+++ src/transformers/modeling_utils.py
@@ -32,15 +32,15 @@ from threading import Thread
 from typing import Any, Optional, TypeVar, get_type_hints
 from zipfile import is_zipfile
 
+import torch
 from huggingface_hub import create_repo, is_offline_mode, split_torch_state_dict_into_shards
 from packaging import version
 from safetensors import safe_open
 from safetensors.torch import load as _safe_load_bytes
 from safetensors.torch import save_file as safe_save_file
-import torch
 from torch import Tensor, nn
-from torch.distributions import constraints
 from torch.distributed.tensor import DTensor, Shard
+from torch.distributions import constraints
 from torch.utils.checkpoint import checkpoint
 
 from . import initialization as init
