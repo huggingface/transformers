dcf9519e42219745733f716cd90233cf9ea58c46
eustlb <94853470+eustlb@users.noreply.github.com>
2026-04-28 14:56:37 +0900

    glmasr should be in AutoModelForMultimodalLM

--- src/transformers/models/auto/modeling_auto.py
+++ src/transformers/models/auto/modeling_auto.py
@@ -1213,6 +1213,7 @@ MODEL_FOR_SPEECH_SEQ_2_SEQ_MAPPING_NAMES = OrderedDict(
     [
         ("cohere_asr", "CohereAsrForConditionalGeneration"),
         ("dia", "DiaForConditionalGeneration"),
+        ("glmasr", "GlmAsrForConditionalGeneration"),
         ("granite_speech", "GraniteSpeechForConditionalGeneration"),
         ("kyutai_speech_to_text", "KyutaiSpeechToTextForConditionalGeneration"),
         ("moonshine", "MoonshineForConditionalGeneration"),
