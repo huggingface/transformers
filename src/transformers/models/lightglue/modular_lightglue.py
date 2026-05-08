ed006461aa7d989344ebedf368d9abe76e2bf920
vasqu <antonprogamer@gmail.com>
2026-05-08 17:53:37 +0200

    fix lightglue

--- src/transformers/models/lightglue/modular_lightglue.py
+++ src/transformers/models/lightglue/modular_lightglue.py
@@ -748,6 +748,8 @@ class LightGlueForKeypointMatching(LightGluePreTrainedModel):
                     config=self.config,
                     input_embeds=descriptors[:, 0:1, :],  # force q_len == 1
                     attention_mask=mask,
+                    # Model is too sensitive to the FA backend --> force mask to avoid the backend
+                    and_mask_function=lambda *args: torch.tensor(True, dtype=torch.bool),
                 )
             else:
                 extended_attention_mask = torch.ones((batch_size, descriptors.size()[-2]), device=keypoints.device)
