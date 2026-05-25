82cd959eff58850be52dcc0ad83abf54d30aca94
Cyril Vallez <cyril.vallez@huggingface.co>
2026-05-25 16:36:19 +0900

    Update release tool (#46193)
    update

--- utils/release.py
+++ utils/release.py
@@ -128,7 +128,8 @@ def global_version_update(version: str, patch: bool = False):
     """
     for pattern, fname in REPLACE_FILES.items():
         update_version_in_file(fname, version, pattern)
-    update_version_in_examples(version, patch=patch)
+    # REMOVED AFTER v5! Uncomment to start updating the version of the examples again
+    # update_version_in_examples(version, patch=patch)
 
 
 def remove_conversion_scripts():
