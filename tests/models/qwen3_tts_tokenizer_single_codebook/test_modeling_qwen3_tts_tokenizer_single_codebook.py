1958ce1a31016f0c82367db25107ce8d2eefd6b6
vandit <vanditashah12@gmail.com>
2026-05-25 00:55:03 -0400

    Update qwen3_tts docs: replace V1/V2 with MC/SC tokenizer class names, add missing autodoc entries

--- tests/models/qwen3_tts_tokenizer_single_codebook/test_modeling_qwen3_tts_tokenizer_single_codebook.py
+++ tests/models/qwen3_tts_tokenizer_single_codebook/test_modeling_qwen3_tts_tokenizer_single_codebook.py
@@ -16,6 +16,7 @@ import unittest
 from transformers import is_torch_available
 from transformers.testing_utils import require_torch
 
+
 if is_torch_available():
     from transformers import (
         Qwen3TTSTokenizerSingleCodebookDecoder,
