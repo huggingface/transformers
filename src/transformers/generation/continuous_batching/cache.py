4cbc618a0e89dab03d22207f5839318b410ccd0a
remi-or <remi.pierre_o@orange.fr>
2026-05-28 06:25:23 +0000

    Core

--- src/transformers/generation/continuous_batching/cache.py
+++ src/transformers/generation/continuous_batching/cache.py
@@ -504,6 +504,26 @@ class PagedAttentionCache:
         # FIXME: consolidate the cache into a single tensor of shape (group_size, 2, *self.k_or_v_cache_shape)
         # This will allow for  better .update and a single copy instead of one per cache tensor
 
+    def compute_max_num_forks(self, source_request_id: str) -> int:
+        """Computes the maximum number of children requests that can be forked from the source request."""
+        # Compute the number of blocks needed per fork
+        blocks_needed_per_fork = 0
+        for cm in self.group_cache_managers:
+            block_ids = cm.block_table[source_request_id]
+            will_be_shared = 0
+            if cm.uses_block_sharing:
+                for block_id in block_ids:
+                    if self._block_manager._id_to_block[block_id].is_complete:
+                        will_be_shared += 1
+                    else:
+                        break
+            blocks_needed_per_fork += len(block_ids) - will_be_shared
+        # Infer, out of the requested number of children, how many can actually be created using a fork
+        if blocks_needed_per_fork == 0:
+            return 2 ** 31  # absurdly large number, virtually infinite number of forks
+        max_num_forks = self.get_num_free_blocks() // blocks_needed_per_fork
+        return max_num_forks
+
     def fork_request(self, source_request_id: str, destination_request_ids: list[str]) -> tuple[list[int], list[int]]:
         """Fork the cache of a request (state) into the one of a list of requests with the given (dst_request_ids)."""
         # These lists will be the accumulators for the source and destination blocks for the cache copy
