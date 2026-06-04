9b77e500564c04c2ba4777de0c2a0b3d2d3829ac
Arthur <48595927+ArthurZucker@users.noreply.github.com>
2026-06-04 15:06:53 +0200

    Apply suggestion from @ArthurZucker

--- setup.py
+++ setup.py
@@ -147,7 +147,7 @@ _deps = [
     "tomli",
     "tiktoken",
     "timm>=1.0.23",
-    "tokenizers>=0.22.0,<=0.23.1",
+    "tokenizers>=0.23.1,<0.24.0",
     "torch>=2.4",
     "torchaudio",
     "torchvision",
