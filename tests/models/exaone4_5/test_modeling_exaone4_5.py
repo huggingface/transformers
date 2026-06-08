6e6707f96aaeffe4f01a185d49687b9dca5f01a7
kaixuanliu <kaixuan.liu@intel.com>
2026-06-08 11:04:01 +0000

    update
    Signed-off-by: kaixuanliu <kaixuan.liu@intel.com>

--- tests/models/exaone4_5/test_modeling_exaone4_5.py
+++ tests/models/exaone4_5/test_modeling_exaone4_5.py
@@ -145,10 +145,6 @@ class Exaone4_5_ModelTest(VLMModelTest, unittest.TestCase):
     def test_model_parallelism(self):
         pass
 
-    # @unittest.skip("Beam search with model parallel auto device_map is not stable for EXAONE 4.5 VLM yet.")
-    # def test_model_parallel_beam_search(self):
-    #     pass
-
 
 @require_torch
 class Exaone4_5_IntegrationTest(unittest.TestCase):
