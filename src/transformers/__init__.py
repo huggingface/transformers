89eb876fdd9eca53400fe06e6c1e267dedf2d554
vasqu <antonprogamer@gmail.com>
2026-06-15 18:10:16 +0200

    v5.10.3

--- src/transformers/__init__.py
+++ src/transformers/__init__.py
@@ -18,7 +18,7 @@
 # to defer the actual importing for when the objects are requested. This way `import transformers` provides the names
 # in the namespace without actually importing anything (and especially none of the backends).
 
-__version__ = "5.10.3"
+__version__ = "5.10.4"
 
 import importlib
 import sys
