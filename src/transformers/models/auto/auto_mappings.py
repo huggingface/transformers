0886a02a720baca08d1704ecb38ae1dec69eb51d
MAHIR DAIYAN <mahir15daiyan@gmail.com>
2026-06-16 17:39:46 +0000

    nits

--- src/transformers/models/auto/auto_mappings.py
+++ src/transformers/models/auto/auto_mappings.py
@@ -922,7 +922,6 @@ VIDEO_PROCESSOR_MAPPING_NAMES = OrderedDict(
         ("video_llava", "VideoLlavaVideoProcessor"),
         ("videomae", "VideoMAEVideoProcessor"),
         ("videomt", "VideomtVideoProcessor"),
-        ("videoprism", "LlavaOnevisionVideoProcessor"),
         ("vivit", "VivitVideoProcessor"),
         ("vjepa2", "VJEPA2VideoProcessor"),
     ]
