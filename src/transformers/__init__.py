1960d0b74d05235511f88ab749ef019c376d426e
primordialomegazero <devilswithin13@gmail.com>
2026-06-19 06:55:38 +0800

    Remove phi_utils imports from __init__ to fix import complexity

--- src/transformers/__init__.py
+++ src/transformers/__init__.py
@@ -1,16 +1,8 @@
 # ... existing imports ...
 
-# Phi-optimized generation
-    PhiRecursiveGenerator,
-    phi_max_tokens,
-    phi_repetition_penalty,
-    phi_temperature,
-    phi_top_k,
-    phi_top_p,
 )
 
 # ... rest of file ...
 
-# Phi-optimized generation (lazy import to reduce complexity)
 def PhiRecursiveGenerator(*args, **kwargs):
     return _PhiRecursiveGenerator(*args, **kwargs)
