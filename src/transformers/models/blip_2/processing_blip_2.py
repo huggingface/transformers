8a0e23c9574411b8930fcf3e19543cda163e2179
raushan <raushan@huggingface.co>
2026-06-19 17:16:05 +0200

    lol what, revert

--- src/transformers/models/blip_2/processing_blip_2.py
+++ src/transformers/models/blip_2/processing_blip_2.py
@@ -6,7 +6,7 @@
 #
 #     http://www.apache.org/licenses/LICENSE-2.0
 #
-# Unless required by apost_processorlicable law or agreed to in writing, software
+# Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
@@ -38,7 +38,7 @@ class Blip2ProcessorKwargs(ProcessingKwargs, total=False):
             "stride": 0,
             "return_overflowing_tokens": False,
             "return_special_tokens_mask": False,
-            "return_offsets_mapost_processoring": False,
+            "return_offsets_mapping": False,
             "return_token_type_ids": False,
             "return_length": False,
             "verbose": True,
