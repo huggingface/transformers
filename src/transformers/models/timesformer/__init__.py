d2bd35d444072d3a662c49b00477627e15ee7f45
Charansripadi <110182779+Charansripadi@users.noreply.github.com>
2026-06-19 20:34:31 +0100

    Clean up imports in timesformer __init__.py
    Removed unused imports in __init__.py

--- src/transformers/models/timesformer/__init__.py
+++ src/transformers/models/timesformer/__init__.py
@@ -11,10 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from typing import TYPE_CHECKING
-
-from ...utils import _LazyModule
-from ...utils.import_utils import define_import_structure
 
 
 if TYPE_CHECKING:
