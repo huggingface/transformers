bffafd0a55cc2233e445e3bd8221ff494b842d5f
guarin <43336610+guarin@users.noreply.github.com>
2026-06-24 08:45:53 +0200

    update

--- src/transformers/models/unlimited_ocr/modular_unlimited_ocr.py
+++ src/transformers/models/unlimited_ocr/modular_unlimited_ocr.py
@@ -351,11 +351,10 @@ class UnlimitedOcrConfig(DeepseekOcr2Config):
             self.vision_config = self.sub_configs["vision_config"](**self.vision_config)
 
         if self.text_config is None:
-            text_cls = self.sub_configs["text_config"]
-            text_keys = text_cls().to_dict().keys()
-            text_kwargs = {key: kwargs.pop(key) for key in text_keys if key in kwargs}
-            text_kwargs = {}
-            self.text_config = text_cls(**text_kwargs)
+            text_config_cls = self.sub_configs["text_config"]
+            text_keys = text_config_cls().to_dict().keys()
+            text_kwargs = {key: kwargs[key] for key in text_keys if key in kwargs}
+            self.text_config = text_config_cls(**text_kwargs)
         elif isinstance(self.text_config, dict):
             self.text_config = self.sub_configs["text_config"](**self.text_config)
         PretrainedConfig.__post_init__(self, **kwargs)
