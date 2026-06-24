367aa62bd0b4515d6df63a1c21d1eeb6d6662d28
eustlb <94853470+eustlb@users.noreply.github.com>
2026-06-24 11:56:01 +0200

    update

--- src/transformers/models/nemotron3_5_asr/modular_nemotron3_5_asr.py
+++ src/transformers/models/nemotron3_5_asr/modular_nemotron3_5_asr.py
@@ -1,5 +1,4 @@
 # Copyright 2026 The HuggingFace Inc. team. All rights reserved.
-# Copyright (c) 2026, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
