d867606f9bdf6f85d6349978360378f7e9b8e0ad
Cyril Vallez <cyril.vallez@gmail.com>
2026-06-26 21:06:14 +0900

    mark fa test

--- tests/models/llama/test_modeling_llama.py
+++ tests/models/llama/test_modeling_llama.py
@@ -286,6 +286,7 @@ class LlamaIntegrationTest(unittest.TestCase):
             ep_generated_text = tokenizer.batch_decode(ep_generated_ids, skip_special_tokens=True)
             self.assertEqual(EXPECTED_TEXT_COMPLETION, ep_generated_text)
 
+    @pytest.mark.flash_attn_test
     @require_flash_attn
     def test_flash_with_static_cache(self):
         """Test that FA works correctly with a StaticCache"""
