a6e379c4dd07f91696e2b73e20c426df856c68bc
Vikas Sharma <vikasharmadev8@gmail.com>
2026-06-26 17:05:46 +0530

    Use do_shift_labels flag: only skip shift when shift_tokens_right() was called (per reviewer)

--- src/transformers/models/florence2/modeling_florence2.py
+++ src/transformers/models/florence2/modeling_florence2.py
@@ -872,6 +872,7 @@ class Florence2ForConditionalGeneration(Florence2PreTrainedModel, GenerationMixi
         >>> processor.batch_decode(generate_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0]
         "A green car parked in front of a yellow building."
         ```"""
+        do_shift_labels = False
         if labels is not None:
             if use_cache:
                 logger.warning("The `use_cache` argument is changed to `False` since `labels` is provided.")
@@ -880,6 +881,7 @@ class Florence2ForConditionalGeneration(Florence2PreTrainedModel, GenerationMixi
                 decoder_input_ids = shift_tokens_right(
                     labels, self.config.text_config.pad_token_id, self.config.text_config.decoder_start_token_id
                 )
+                do_shift_labels = True
 
         outputs = self.model(
             input_ids=input_ids,
@@ -902,11 +904,14 @@ class Florence2ForConditionalGeneration(Florence2PreTrainedModel, GenerationMixi
 
         loss = None
         if labels is not None:
+            shift_labels = kwargs.pop("shift_labels", None)
+            if do_shift_labels and shift_labels is None:
+                shift_labels = labels
             loss = self.loss_function(
                 logits=logits,
                 labels=labels,
                 vocab_size=self.config.text_config.vocab_size,
-                shift_labels=labels,
+                shift_labels=shift_labels,
                 **kwargs,
             )
 
