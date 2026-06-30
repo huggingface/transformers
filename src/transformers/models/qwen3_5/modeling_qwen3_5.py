dc02de4425bad382127742d45f8d6946634a74fe
Cyril Vallez <cyril.vallez@huggingface.co>
2026-06-30 16:21:09 +0900

    Remove default dtype in FusedRMSNormGated modules (#46953)
    remove default dtype

--- src/transformers/models/qwen3_5/modeling_qwen3_5.py
+++ src/transformers/models/qwen3_5/modeling_qwen3_5.py
@@ -412,7 +412,6 @@ class Qwen3_5GatedDeltaNet(nn.Module):
                 self.head_v_dim,
                 eps=self.layer_norm_epsilon,
                 activation=self.activation,
-                dtype=config.dtype if config.dtype is not None else torch.get_default_dtype(),
             )
         )
 
