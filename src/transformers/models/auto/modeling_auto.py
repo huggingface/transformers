d1a7a41f7fbdb2ea96225f40a9ee3f5a7ba2beec
RaphaelKreft <rkreft@ethz.ch>
2026-07-18 17:38:10 +0200

    feat(apertus1p5): finalize composite model and text backbone

--- src/transformers/models/auto/modeling_auto.py
+++ src/transformers/models/auto/modeling_auto.py
@@ -49,6 +49,7 @@ MODEL_MAPPING_NAMES = OrderedDict(
         ("altclip", "AltCLIPModel"),
         ("apertus", "ApertusModel"),
         ("apertus1p5", "Apertus1p5Model"),
+        ("apertus1p5_text", "Apertus1p5TextModel"),
         ("arcee", "ArceeModel"),
         ("aria", "AriaModel"),
         ("aria_text", "AriaTextModel"),
@@ -709,7 +710,7 @@ MODEL_FOR_CAUSAL_LM_MAPPING_NAMES = OrderedDict(
         # Model for Causal LM mapping
         ("afmoe", "AfmoeForCausalLM"),
         ("apertus", "ApertusForCausalLM"),
-        ("apertus1p5", "Apertus1p5ForCausalLM"),
+        ("apertus1p5_text", "Apertus1p5TextForCausalLM"),
         ("arcee", "ArceeForCausalLM"),
         ("aria_text", "AriaTextForCausalLM"),
         ("axk1", "AXK1ForCausalLM"),
@@ -1982,7 +1983,6 @@ MODEL_FOR_KEYPOINT_MATCHING_MAPPING_NAMES = OrderedDict(
 MODEL_FOR_TEXT_ENCODING_MAPPING_NAMES = OrderedDict(
     [
         ("albert", "AlbertModel"),
-        ("apertus1p5", "Apertus1p5TextModel"),
         ("bert", "BertModel"),
         ("big_bird", "BigBirdModel"),
         ("clip_text_model", "CLIPTextModel"),
