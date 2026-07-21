63b80ba6fc28f4720084f20ed7c0c104210b22a7
Cyril Vallez <cyril.vallez@gmail.com>
2026-07-21 18:22:05 +0900

    more flags

--- src/transformers/models/lfm2_moe/modeling_lfm2_moe.py
+++ src/transformers/models/lfm2_moe/modeling_lfm2_moe.py
@@ -550,7 +550,6 @@ class Lfm2MoePreTrainedModel(PreTrainedModel):
         "hidden_states": Lfm2MoeDecoderLayer,
         "attentions": Lfm2MoeAttention,
     }
-    _is_stateful = True
 
     @torch.no_grad()
     def _init_weights(self, module):
