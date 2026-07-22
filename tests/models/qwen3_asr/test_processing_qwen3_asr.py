cda158962cd923ca57697219ab2a9d7a01463f43
Eric B <ebezzam@gmail.com>
2026-07-22 14:46:43 +0200

    Update after checkpoint merges.

--- tests/models/qwen3_asr/test_processing_qwen3_asr.py
+++ tests/models/qwen3_asr/test_processing_qwen3_asr.py
@@ -37,10 +37,9 @@ class Qwen3ASRProcessorTest(ProcessorTesterMixin, unittest.TestCase):
     @require_torch
     def setUpClass(cls):
         cls.checkpoint = "Qwen/Qwen3-ASR-0.6B-hf"
-        cls.revision = "refs/pr/3"  # TODO: set to main after merge
         cls.tmpdirname = tempfile.mkdtemp()
 
-        processor = Qwen3ASRProcessor.from_pretrained(cls.checkpoint, revision=cls.revision)
+        processor = Qwen3ASRProcessor.from_pretrained(cls.checkpoint)
         processor.save_pretrained(cls.tmpdirname)
 
     @require_torch
@@ -61,14 +60,14 @@ class Qwen3ASRProcessorTest(ProcessorTesterMixin, unittest.TestCase):
 
     @require_torch
     def test_can_load_various_tokenizers(self):
-        processor = Qwen3ASRProcessor.from_pretrained(self.checkpoint, revision=self.revision)
-        tokenizer = AutoTokenizer.from_pretrained(self.checkpoint, revision=self.revision)
+        processor = Qwen3ASRProcessor.from_pretrained(self.checkpoint)
+        tokenizer = AutoTokenizer.from_pretrained(self.checkpoint)
         self.assertEqual(processor.tokenizer.__class__, tokenizer.__class__)
 
     @require_torch
     def test_save_load_pretrained_default(self):
-        tokenizer = AutoTokenizer.from_pretrained(self.checkpoint, revision=self.revision)
-        processor = Qwen3ASRProcessor.from_pretrained(self.checkpoint, revision=self.revision)
+        tokenizer = AutoTokenizer.from_pretrained(self.checkpoint)
+        processor = Qwen3ASRProcessor.from_pretrained(self.checkpoint)
         feature_extractor = processor.feature_extractor
 
         processor = Qwen3ASRProcessor(tokenizer=tokenizer, feature_extractor=feature_extractor)
@@ -84,7 +83,7 @@ class Qwen3ASRProcessorTest(ProcessorTesterMixin, unittest.TestCase):
 
     @require_torch
     def test_chat_template(self):
-        processor = AutoProcessor.from_pretrained(self.checkpoint, revision=self.revision)
+        processor = AutoProcessor.from_pretrained(self.checkpoint)
         expected_prompt = (
             "<|im_start|>system\n"
             "<|im_end|>\n"
@@ -108,7 +107,7 @@ class Qwen3ASRProcessorTest(ProcessorTesterMixin, unittest.TestCase):
 
     @require_torch
     def test_apply_transcription_request_with_language(self):
-        processor = AutoProcessor.from_pretrained(self.checkpoint, revision=self.revision)
+        processor = AutoProcessor.from_pretrained(self.checkpoint)
 
         audio_url = "https://huggingface.co/datasets/bezzam/audio_samples/resolve/main/librispeech_mr_quilter.wav"
         outputs = processor.apply_transcription_request(audio=audio_url, language="English")
@@ -122,7 +121,7 @@ class Qwen3ASRProcessorTest(ProcessorTesterMixin, unittest.TestCase):
 
     @require_torch
     def test_apply_transcription_request_with_prompt(self):
-        processor = AutoProcessor.from_pretrained(self.checkpoint, revision=self.revision)
+        processor = AutoProcessor.from_pretrained(self.checkpoint)
 
         audio_url = "https://huggingface.co/datasets/bezzam/audio_samples/resolve/main/librispeech_mr_quilter.wav"
         context = "Vocabulary: Quilter, apostle, gospel."
@@ -136,7 +135,7 @@ class Qwen3ASRProcessorTest(ProcessorTesterMixin, unittest.TestCase):
     @require_torch
     def test_apply_transcription_request_mixed_batch(self):
         """Mixed batch: forced-language samples get the prefill, auto-detect samples a bare generation prompt."""
-        processor = AutoProcessor.from_pretrained(self.checkpoint, revision=self.revision)
+        processor = AutoProcessor.from_pretrained(self.checkpoint)
 
         audio_url = "https://huggingface.co/datasets/bezzam/audio_samples/resolve/main/librispeech_mr_quilter.wav"
         outputs = processor.apply_transcription_request(audio=[audio_url, audio_url], language=[None, "zh"])
@@ -148,7 +147,7 @@ class Qwen3ASRProcessorTest(ProcessorTesterMixin, unittest.TestCase):
 
     @require_torch
     def test_decode_formats(self):
-        processor = AutoProcessor.from_pretrained(self.checkpoint, revision=self.revision)
+        processor = AutoProcessor.from_pretrained(self.checkpoint)
 
         raw_text = "language English<asr_text>Mr. Quilter is the apostle of the middle classes."
 
