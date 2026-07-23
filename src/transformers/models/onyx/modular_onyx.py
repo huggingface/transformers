ab69f3c1c6c99e6948c547193cb941349663bb0b
Pedro Cuenca <pedro@huggingface.co>
2026-07-23 14:39:07 +0200

    Use one of the supported layer_types
    We only check for "full_attention"

--- src/transformers/models/onyx/modular_onyx.py
+++ src/transformers/models/onyx/modular_onyx.py
@@ -141,7 +141,7 @@ class OnyxVisionConfig(PreTrainedConfig):
             stride = 4
             self.layer_types = [
                 "full_attention" if (i + 1) % stride == 0 or i == self.num_hidden_layers - 1
-                else "windowed_attention"
+                else "sliding_attention"
                 for i in range(self.num_hidden_layers)
             ]
         super().__post_init__(**kwargs)
