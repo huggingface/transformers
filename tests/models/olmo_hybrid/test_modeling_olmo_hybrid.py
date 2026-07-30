82bfadecd0b136c8a75f73efe75ab949d2802165
vasqu <antonprogamer@gmail.com>
2026-07-30 15:04:46 +0000

    conv1ds across other models

--- tests/models/olmo_hybrid/test_modeling_olmo_hybrid.py
+++ tests/models/olmo_hybrid/test_modeling_olmo_hybrid.py
@@ -69,7 +69,11 @@ class OlmoHybridModelTest(CausalLMModelTest, unittest.TestCase):
         conv_kernel = config.linear_conv_kernel_dim
         key_dim = config.linear_key_head_dim * config.linear_num_key_heads
         value_dim = config.linear_value_head_dim * config.linear_num_value_heads
-        return (batch_size, key_dim * 2 + value_dim, conv_kernel,)
+        return (
+            batch_size,
+            key_dim * 2 + value_dim,
+            conv_kernel,
+        )
 
     def _get_recurrent_state_shape(self, batch_size: int, config):
         return (batch_size, config.linear_num_value_heads, config.linear_key_head_dim, config.linear_value_head_dim)
