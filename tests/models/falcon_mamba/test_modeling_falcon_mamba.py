fcf7292f176f3c2957003eecb6d7e5b506064713
vasqu <antonprogamer@gmail.com>
2026-08-03 21:21:16 +0000

    style

--- tests/models/falcon_mamba/test_modeling_falcon_mamba.py
+++ tests/models/falcon_mamba/test_modeling_falcon_mamba.py
@@ -518,9 +518,9 @@ class FalconMambaIntegrationTests(unittest.TestCase):
                     "Hello my name is Younes and today I will be talking about the importance of the internet in our lives.\nThe internet is a global",
                 ],
                 ("cuda", 9): [
-                    'Hello today I am going to talk about the “Theory of Relativity” by Albert Einstein.\n',
-                    'Hello my name is Younes and today I will be talking about the importance of the internet in our lives.\nThe internet is a global'
-                ]
+                    "Hello today I am going to talk about the “Theory of Relativity” by Albert Einstein.\n",
+                    "Hello my name is Younes and today I will be talking about the importance of the internet in our lives.\nThe internet is a global",
+                ],
             }
         )
         EXPECTED_OUTPUT = EXPECTED_OUTPUTS.get_expectation()
