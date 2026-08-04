2481a7b96f83c69148c39dbe8c0a16d7866a97af
Dhanush Pillay <dhanushpillay28@gmail.com>
2026-08-04 20:01:24 +0530

    clean up reverse_op fixme in compressed_tensors (#47701)

--- src/transformers/integrations/compressed_tensors.py
+++ src/transformers/integrations/compressed_tensors.py
@@ -16,7 +16,7 @@
 import torch
 from torch import nn
 
-from ..core_model_loading import ConversionOps
+from ..core_model_loading import ConversionOps, _IdentityOp
 from ..utils import logging
 from ..utils.import_utils import is_torch_greater_or_equal
 
@@ -166,7 +166,7 @@ class DecompressExperts(ConversionOps):
 
     @property
     def reverse_op(self) -> "ConversionOps":
-        return None  # FIXME
+        return _IdentityOp()
 
 
 _FP8_DTYPE = torch.float8_e4m3fn
