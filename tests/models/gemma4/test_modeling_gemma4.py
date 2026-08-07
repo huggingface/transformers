eace7e3bd00ab76e8a18fde0cb02adecd46bac4b
kaixuanliu <kaixuan.liu@intel.com>
2026-08-07 06:20:33 +0000

    update
    Signed-off-by: kaixuanliu <kaixuan.liu@intel.com>

--- tests/models/gemma4/test_modeling_gemma4.py
+++ tests/models/gemma4/test_modeling_gemma4.py
@@ -142,12 +142,6 @@ class Gemma4TextModelTest(CausalLMModelTest, unittest.TestCase):
     def test_flash_attn_2_inference_equivalence_right_padding(self):
         pass
 
-    @unittest.skip(
-        "QuantizedCache is only supported for models with only full attention layers, but Gemma4 has sliding_attention."
-    )
-    def test_generate_with_quant_cache(self):
-        pass
-
     def test_all_bidirectional_attention_uses_bidirectional_mask(self):
         self.model_tester.use_bidirectional_attention = "all"
         config, inputs_dict = self.model_tester.prepare_config_and_inputs_for_common()
@@ -322,12 +316,6 @@ class Gemma4Audio2TextModelTest(ModelTesterMixin, GenerationTesterMixin, unittes
 
     @unittest.skip("Gemma4 needs correct embeddings for per-layer-input computation, random won't work!")
     def test_generate_from_random_inputs_embeds(self):
-        pass
-
-    @unittest.skip(
-        "QuantizedCache is only supported for models with only full attention layers, but Gemma4 has sliding_attention."
-    )
-    def test_generate_with_quant_cache(self):
         pass
 
     @unittest.skip(GEMMA4_RANDOM_MOE_FA2_SKIP_REASON)
@@ -560,12 +548,6 @@ class Gemma4Vision2TextModelTest(ModelTesterMixin, GenerationTesterMixin, unitte
 
     @unittest.skip("Gemma4 needs correct embeddings for per-layer-input computation, random won't work!")
     def test_generate_from_random_inputs_embeds(self):
-        pass
-
-    @unittest.skip(
-        "QuantizedCache is only supported for models with only full attention layers, but Gemma4 has sliding_attention."
-    )
-    def test_generate_with_quant_cache(self):
         pass
 
     @unittest.skip(
