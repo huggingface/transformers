837961643ad235d86498483281b631f77285bf8d
ydshieh <ydshieh@users.noreply.github.com>
2026-08-10 17:03:45 +0200

    Trigger tests_processors CI run

--- tests/models/qwen3_asr/test_processing_qwen3_asr.py
+++ tests/models/qwen3_asr/test_processing_qwen3_asr.py
@@ -29,6 +29,7 @@ from transformers.testing_utils import require_torch
 from ...test_processing_common import ProcessorTesterMixin
 
 
+# trigger tests_processors
 class Qwen3ASRProcessorTest(ProcessorTesterMixin, unittest.TestCase):
     processor_class = Qwen3ASRProcessor
     tiny_model_id = "hf-internal-testing/tiny-processor-qwen3_asr"
