3467cc05f20b12192fdfa962cfa961961f36eca6
Raushan <raushan@huggingface.co>
2026-08-10 18:36:28 +0000

    skip

--- tests/models/cohere_compass/test_modeling_cohere_compass.py
+++ tests/models/cohere_compass/test_modeling_cohere_compass.py
@@ -380,3 +380,12 @@ class CohereCompassModelTest(VLMModelTest, unittest.TestCase):
                 mm_token_type_ids=mm_token_type_ids,
             )
         self.assertEqual(output.logits.shape[:2], input_ids.shape)
+
+    @unittest.skip("TODO: compass configures RoPE per layer type")
+    def test_model_rope_scaling_frequencies(self):
+        pass
+
+    @parameterized.expand([("linear",), ("dynamic",), ("yarn",)])
+    @unittest.skip("TODO: compass configures RoPE per layer type")
+    def test_model_rope_scaling_from_config(self):
+        pass
