8740a89e9f1ae66bd4f5f351b2646de1da57b2e3
raushan <raushan@huggingface.co>
2026-08-25 16:50:10 +0200

    fix style

--- src/transformers/models/pixtral/modeling_pixtral.py
+++ src/transformers/models/pixtral/modeling_pixtral.py
@@ -21,14 +21,14 @@ from torch import nn
 from ...activations import ACT2FN
 from ...modeling_layers import GradientCheckpointingLayer
 from ...modeling_outputs import BaseModelOutput
-from ...modeling_rope_utils import dynamic_rope_update
+from ...modeling_rope_utils import ROPE_INIT_FUNCTIONS, dynamic_rope_update
 from ...modeling_utils import ALL_ATTENTION_FUNCTIONS, PreTrainedModel
 from ...processing_utils import Unpack
-from ...vision_utils import get_vision_position_ids
 from ...utils import TransformersKwargs, auto_docstring, logging
 from ...utils.deprecation import deprecate_kwarg
 from ...utils.generic import is_flash_attention_requested, maybe_autocast, merge_with_config_defaults
 from ...utils.output_capturing import capture_outputs
+from ...vision_utils import get_vision_position_ids
 from .configuration_pixtral import PixtralVisionConfig
 
 
