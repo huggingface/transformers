93ebf6b11127967f2725cf4d012aae55c3654f5a
Anton Vlasjuk <73884904+vasqu@users.noreply.github.com>
2026-09-10 13:06:37 +0000

    [`MoE`] Fix eager EP (#48653)
    * fix
    
    * fix

--- src/transformers/models/qwen3_omni_moe/modeling_qwen3_omni_moe.py
+++ src/transformers/models/qwen3_omni_moe/modeling_qwen3_omni_moe.py
@@ -1333,7 +1333,7 @@ class Qwen3OmniMoeThinkerTextExperts(nn.Module):
     ) -> torch.Tensor:
         final_hidden_states = torch.zeros_like(hidden_states)
         with torch.no_grad():
-            expert_mask = torch.nn.functional.one_hot(top_k_index, num_classes=self.num_experts)
+            expert_mask = torch.nn.functional.one_hot(top_k_index, num_classes=self.num_experts + 1)
             expert_mask = expert_mask.permute(2, 1, 0)
             expert_hit = torch.greater(expert_mask.sum(dim=(-1, -2)), 0).nonzero()
 
@@ -2782,7 +2782,7 @@ class Qwen3OmniMoeTalkerTextExperts(nn.Module):
     ) -> torch.Tensor:
         final_hidden_states = torch.zeros_like(hidden_states)
         with torch.no_grad():
-            expert_mask = torch.nn.functional.one_hot(top_k_index, num_classes=self.num_experts)
+            expert_mask = torch.nn.functional.one_hot(top_k_index, num_classes=self.num_experts + 1)
             expert_mask = expert_mask.permute(2, 1, 0)
             expert_hit = torch.greater(expert_mask.sum(dim=(-1, -2)), 0).nonzero()
 
