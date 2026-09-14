2a22cf894c99ac6ce304740d98e53ade85109cdd
Marc Sun <marc.sun@hotmail.fr>
2026-09-14 23:42:02 +0200

    up

--- src/transformers/generation/configuration_utils.py
+++ src/transformers/generation/configuration_utils.py
@@ -379,7 +379,6 @@ class GenerationConfig(PushToHubMixin):
     _pad_token_tensor: "torch.Tensor | None"
     _decoder_start_token_tensor: "torch.Tensor | None"
 
-    # Whether the inputs of this call are padded, worked out once by generate() at generation time
     _can_drop_attention_mask: bool = False
 
     # Hash to detect whether the instance was modified after loading
