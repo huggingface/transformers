8577504ca9a00a6c982981f5b3d54dc7cca448b7
IlyasMoutawwakil <moutawwakil.ilyas.tsi@gmail.com>
2026-09-15 15:46:20 +0200

    fix

--- tests/models/tapas/test_modeling_tapas.py
+++ tests/models/tapas/test_modeling_tapas.py
@@ -1001,7 +1001,7 @@ class TapasUtilitiesTest(unittest.TestCase):
         np.testing.assert_array_equal(
             col_index_flat.indices.numpy(), [0, 0, 1, 0, 0, 1, 0, 0, 1, 3, 4, 5, 3, 4, 5, 3, 4, 5]
         )
-        self.assertEqual(batched_index_flat.num_segments.numpy(), np.prod(shape))
+        self.assertEqual(batched_index_flat.num_segments, np.prod(shape))
         np.testing.assert_array_equal(batched_index_flat.indices.numpy(), range(np.prod(shape)))
 
     def test_range_index_map(self):
@@ -1064,7 +1064,7 @@ class TapasUtilitiesTest(unittest.TestCase):
 
         np.testing.assert_allclose(sums.numpy(), [3.0, 3.0])
         np.testing.assert_array_equal(new_index.indices.numpy(), [0, 1])
-        np.testing.assert_array_equal(new_index.num_segments.numpy(), 2)
+        np.testing.assert_array_equal(new_index.num_segments, 2)
         np.testing.assert_array_equal(new_index.batch_dims, 0)
 
     def test_gather(self):
