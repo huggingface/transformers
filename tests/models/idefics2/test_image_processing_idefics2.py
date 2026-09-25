575668e82aad7bdf9470eb74d667958d51e1c84e
raushan <raushan@huggingface.co>
2026-09-25 11:05:18 +0200

    fix

--- tests/models/idefics2/test_image_processing_idefics2.py
+++ tests/models/idefics2/test_image_processing_idefics2.py
@@ -17,7 +17,7 @@ import unittest
 import numpy as np
 
 from transformers.testing_utils import require_torch, require_vision
-from transformers.utils import is_torch_available, is_vision_available
+from transformers.utils import is_torch_available, is_torchvision_available, is_vision_available
 
 from ...test_image_processing_common import ImageProcessingTester, ImageProcessingTestMixin
 
